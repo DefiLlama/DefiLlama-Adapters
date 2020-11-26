c249b9e8323dda7c0be2a588d37a45908288fab0
aggre <hiroyuki.aggre@gmail.com>
2020-11-26 15:44:24 +0900

    apply prettier

--- projects/dev.js
+++ projects/dev.js
@@ -1,76 +1,79 @@
-var Web3 = require('web3');
+var Web3 = require('web3')
 const env = require('dotenv').config()
-const utils = require('./helper/utils');
-const BigNumber = require("bignumber.js");
+const utils = require('./helper/utils')
+const BigNumber = require('bignumber.js')
 
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${env.parsed.INFURA_KEY}`));
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    `https://mainnet.infura.io/v3/${env.parsed.INFURA_KEY}`
+  )
+)
 
 const ADDRESS_CONFIG_ADDRESS = '0x1D415aa39D647834786EB9B5a333A50e9935b796'
 const TOKEN_ADDRESS = '0x5cAf454Ba92e6F2c929DF14667Ee360eD9fD5b26'
 const TOKEN_ID = 'dev-protocol'
 
-const ADDRESS_CONFIG_ABI = `
-[
+const ADDRESS_CONFIG_ABI = [
   {
-    "constant": true,
-    "inputs": [],
-    "name": "lockup",
-    "outputs": [
+    constant: true,
+    inputs: [],
+    name: 'lockup',
+    outputs: [
       {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
 ]
-`
 
-const LOCKUP_ABI = `
-[
+const LOCKUP_ABI = [
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "_config",
-        "type": "address"
-      }
+        internalType: 'address',
+        name: '_config',
+        type: 'address',
+      },
     ],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "constructor"
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor',
   },
   {
-    "constant": true,
-    "inputs": [],
-    "name": "getAllValue",
-    "outputs": [
+    constant: true,
+    inputs: [],
+    name: 'getAllValue',
+    outputs: [
       {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
 ]
-`
 
 const KEYS = [
   {
-    TOKEN_ADDRESS: TOKEN_ID
-  }
+    TOKEN_ADDRESS: TOKEN_ID,
+  },
 ]
 
 async function fetch() {
-  const addressConfigInstance = await new web3.eth.Contract(JSON.parse(ADDRESS_CONFIG_ABI), ADDRESS_CONFIG_ADDRESS)
+  const addressConfigInstance = await new web3.eth.Contract(
+    ADDRESS_CONFIG_ABI,
+    ADDRESS_CONFIG_ADDRESS
+  )
   const lockupAddress = await addressConfigInstance.methods.lockup().call()
-  const lockupInstance = await new web3.eth.Contract(JSON.parse(LOCKUP_ABI), lockupAddress)
+  const lockupInstance = await new web3.eth.Contract(LOCKUP_ABI, lockupAddress)
   const allValue = await lockupInstance.methods.getAllValue().call()
   const priceFeed = await utils.getPrices(KEYS)
   const price = priceFeed.data[TOKEN_ID].usd
@@ -80,5 +83,5 @@ async function fetch() {
 }
 
 module.exports = {
-  fetch
+  fetch,
 }
