113420ffd396e75ed63ebbabf1a4997c957a0521
Author Name <email@address.com>
2021-02-04 11:25:04 +0000

    fix: extra bracket remove

--- projects/finnexus.js
+++ projects/finnexus.js
@@ -1,7 +1,6 @@
 var Web3 = require('web3');
 const env = require('dotenv').config()
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${env.parsed.INFURA_KEY}`));
-//const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/75c431806c0d49ee9868d4fdcef025bd"));
 
 const web3wan = new Web3(new Web3.providers.HttpProvider('https://gwan-ssl.wandevs.org:56891'));
 
@@ -64,7 +63,7 @@ async function fetch() {
 
 }
 
-}
+
 
 
 module.exports = {
