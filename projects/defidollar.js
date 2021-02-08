59e3ffbe8035ed3bb9abf0ffb93c17754b0757a7
Manthankumar Satani <satanimanthan@gmail.com>
2021-02-08 19:58:36 +0530

    chg: improved fetch method

--- projects/defidollar.js
+++ projects/defidollar.js
@@ -2,8 +2,7 @@
  * Modules
  * ************************** */
 var Web3 = require("web3");
-const BigNumber = require("bignumber.js");
-const CORE = require("./config/defidollar/abi.json");
+const DUSD = require("./config/defidollar/abi.json");
 const ENV = require("dotenv").config();
 
 /* ************************** *
@@ -15,19 +14,13 @@ const WEB3 = new Web3(
   )
 );
 const BASE = "0x5bc25f649fc4e26069ddf4cf4010f9f706c23831";
-const DECIMAL = new BigNumber(18);
-const TEN = new BigNumber(10);
 
 /* ************************** *
  * Methods
  * ************************** */
 async function fetch() {
-  var core = new WEB3.eth.Contract(CORE.abi, BASE);
-
-  let tlv = await core.methods.totalSupply().call();
-  tlv = BigNumber(tlv).dividedBy(TEN.pow(new BigNumber(DECIMAL)));
-
-  return tlv.toFixed(4);
+  var dusd = new WEB3.eth.Contract(DUSD.abi, BASE);
+  return WEB3.utils.fromWei(await dusd.methods.totalSupply().call());
 }
 
 module.exports = {
