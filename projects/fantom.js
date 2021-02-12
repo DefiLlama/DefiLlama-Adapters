6c4772c792c606a42ed81715bfd6fe187480311d
quan8 <quan.nguyen@fantom.foundation>
2021-02-12 15:08:22 +1100

    update Fantom's TVL

--- projects/fantom.js
+++ projects/fantom.js
@@ -1,14 +1,46 @@
 const retry = require('async-retry')
 const axios = require("axios");
+const {GraphQLClient, gql} = require('graphql-request')
 const BigNumber = require("bignumber.js");
 
 async function fetch() {
-  let price_feed = await retry(async bail => await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=fantom&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true'))
-  let response = await retry(async bail => await axios.get('https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0x4e15361fd6b4bb609fa63c81a2be19d873717870&apikey=H6NGIGG7N74TUH8K2X31J1KB65HFBH2E82'))
-  let tvl = new BigNumber(response.data.result).div(10 ** 8).toFixed(2);
-  return (tvl * price_feed.data.bitcoin.usd);
+    const endpoint = 'https://xapi3.fantom.network/api'
+    const graphQLClient = new GraphQLClient(endpoint)
+
+    const query = gql`
+        query getToken($token: Address!) {
+            erc20Token(token: $token) {
+                address
+                name
+                symbol
+                decimals
+                totalSupply
+            }
+        }
+        `;
+
+    var tokens = [
+        {
+            address: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
+            symbol: 'WFTM',
+        },
+        {
+            address: '0x69c744d3444202d35a2783929a0f930f2fbb05ad',
+            symbol: 'SFTM',
+        }
+    ]
+
+    let price_feed = await retry(async bail => await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=fantom&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true'))
+
+    const wData = await graphQLClient.request(query, {token: tokens[0].address});
+    const sData = await graphQLClient.request(query, {token: tokens[1].address});
+
+    let tvl = new BigNumber(wData.erc20Token.totalSupply).plus(new BigNumber(sData.erc20Token.totalSupply)).div(10 ** 18).toFixed(2);
+    const result = tvl * price_feed.data.fantom.usd;
+    //console.log(result)
+    return result;
 }
 
 module.exports = {
-  fetch
+    fetch
 }
