dc281b1a279c152842254d1e6cfea981bc592ca6
jiaqinggang1 <forestjqg@126.com>
2021-03-12 15:01:28 +0800

    added frax pool

--- projects/finnexus.js
+++ projects/finnexus.js
@@ -25,6 +25,7 @@ async function fetch() {
 
     let ethUsdcPool = '0x120f18f5b8edcaa3c083f9464c57c11d81a9e549';
     let ethFnxPool = '0xfdf252995da6d6c54c03fc993e7aa6b593a57b8d';
+    let ethFraxPool = '0xa2904fd151c9d9d634dfa8ecd856e6b9517f9785';
 
     let wanFnxPool = '0xae85f191698483d36ff9fb29b36fb12f67a0c253';
     let wanUsdcPool = '0xA6A12974196aB9dE7AA3f998e0D690F1A80A4c11';
@@ -34,11 +35,15 @@ async function fetch() {
     try {
         let ethUsdcTotal = await getEthTotalCollateral(abis.abis.fnxOracle,ethUsdcPool);
         let ethFnxTotal = await getEthTotalCollateral(abis.abis.fnxOracle,ethFnxPool);
+        let ethFraxTotal = await getEthTotalCollateral(abis.abis.fnxOracle,ethFraxPool);
 
         ethUsdcTotal = new BN(ethUsdcTotal).div(new BN(10 ** 26)).toFixed(2);
         ethFnxTotal = new BN(ethFnxTotal).div(new BN(10 ** 26)).toFixed(2);
-        //console.log(ethUsdcTotal,ethFnxTotal);
-        tlv = parseFloat(ethUsdcTotal) + parseFloat(ethFnxTotal);
+        ethFraxTotal = new BN(ethFraxTotal).div(new BN(10 ** 26)).toFixed(2);
+
+        //console.log(ethUsdcTotal,ethFnxTotal,ethFraxTotal);
+
+        tlv = parseFloat(ethUsdcTotal) + parseFloat(ethFnxTotal) + parseFloat(ethFraxTotal);
 
     }  catch (e) {
         console.log(e);
@@ -52,7 +57,7 @@ async function fetch() {
         wanFnxTotal = new BN(wanFnxTotal).div(new BN(10 ** 26)).toFixed(2);
         wanUsdcTotal = new BN(wanUsdcTotal).div(new BN(10 ** 26)).toFixed(2);
 
-       // console.log(wanFnxTotal, wanUsdcTotal);
+        //console.log(wanFnxTotal, wanUsdcTotal);
         tlv = tlv + parseFloat(wanFnxTotal) + parseFloat(wanUsdcTotal);
 
     } catch (e) {
@@ -61,9 +66,6 @@ async function fetch() {
 
     //console.log(tlv.toFixed(2))
     return tlv.toFixed(2);
-
-}
-
 }
 
 
