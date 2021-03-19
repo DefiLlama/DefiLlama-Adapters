fea4e98dc4463a7246a00d2cd59fdbfc69350314
0xngmi <0xngmi@protonmail.com>
2021-03-19 03:32:23 +0100

    Add energiswap

---
+++ projects/energiswap.js
@@ -0,0 +1,23 @@
+const { GraphQLClient, gql } = require('graphql-request')
+
+async function fetch() {
+    const endpoint = 'https://graphnode.energiswap.info:8000/subgraphs/name/energi/energiswap'
+    const graphQLClient = new GraphQLClient(endpoint)
+
+    const query = gql`
+      query energiswapFactories {
+        energiswapFactories(
+        where: { id: "0xa2dAE9e70D5A08B781DeE134617Cf5E7D23043c2" }) { 
+          totalLiquidityUSD
+        }
+      }`;
+
+    const data = await graphQLClient.request(query);
+
+    return data.energiswapFactories[0].totalLiquidityUSD;;
+}
+fetch().then(console.log)
+
+module.exports = {
+    fetch
+}
