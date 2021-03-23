61621e2ec01613b0d54c52527e21b4beffe4bb1c
Frank Yin <ukby.1234@gmail.com>
2021-03-23 14:42:48 +0800

    use total supply

--- projects/ac.js
+++ projects/ac.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 async function fetch() {
   let response = await retry(async bail => await axios.get('https://api.coingecko.com/api/v3/coins/acoconut'));
-  return (response.data.market_data.current_price.usd * response.data.market_data.total_supply);
+  return Number(response.data.market_data.total_supply);
 }
 
 
