94d08b38d9c6c0e4cacf22cc593a05571e3b7a9a
lavender78 <lindawu8134@gmail.com>
2021-03-30 02:04:16 +0800

    feat: fetch total data

--- projects/xdefi/xdefi.js
+++ projects/xdefi/xdefi.js
@@ -1,13 +1,13 @@
-const retry = require('async-retry')
-const axios = require("axios");
-const { GraphQLClient, gql } = require('graphql-request')
+const BigNumber = require('bignumber.js')
+const { GraphQLClient, gql, request } = require('graphql-request')
+
 
 async function fetch() {
     var xdexApi = 'https://api.thegraph.com/subgraphs/name/xdefilab/xdefidex';
     var xdexGraphQLClient = new GraphQLClient(xdexApi)
     var xdexQuery = gql`
     {
-        xdefi(first: 1) {
+        xdefis(first: 1) {
             id,
             version,
             totalLiquidity,
@@ -15,8 +15,8 @@ async function fetch() {
         }
     }
   `;
-    var results = await retry(async bail => await xdexGraphQLClient.request(xdexQuery))
-    const xdexTotalLiquidity = parseFloat(results.xdefi[0].totalLiquidity);
+    const { xdefis } = await xdexGraphQLClient.request(xdexQuery)
+    const xdexTotalLiquidity = new BigNumber(xdefis[0].totalLiquidity);
 
     var xhalflifeApi = 'https://api.thegraph.com/subgraphs/name/xdefilab/xhalflife';
     var xhalflifeGraphQLClient = new GraphQLClient(xhalflifeApi)
@@ -36,15 +36,56 @@ async function fetch() {
         }
      }
     `;
-    results = await retry(async bail => await xhalflifeGraphQLClient.request(xhalflifeQuery))
-    var xhalflifeTotalLiquidity = 0
+    const {streamTotalDatas} = await xhalflifeGraphQLClient.request(xhalflifeQuery)
 
-    //query per token's prices
+    const tokenDataWithLocked = streamTotalDatas.reduce((all, current) => {
+        return {
+            ...all,
+            [current.id]: {
+                id: current.id,
+                locked: current.locked,
+                symbol: current.token.symbol
+            }
+        }
+    },{})
 
-    //xhalflifeTotalLiquidity += per token's liquidity
+    const tokenPriceQuery = gql`
+    query getTokenPrices($ids: [String!]){
+        tokenPrices(first:100,where:{
+            id_in: $ids
+        }){
+            id
+            symbol
+            price
+        }
+    }
+    `
+    const ethAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+    const wethAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 
-    //return xdexTotalLiquidity + xhalflifeTotalLiquidity
+    const {tokenPrices} = await request(xdexApi, tokenPriceQuery,{ids: Object.keys(tokenDataWithLocked).concat(wethAddress)})
+
+    Object.keys(tokenDataWithLocked).forEach(token => {
+        const findToken = tokenPrices.find(tokenPrice => tokenPrice.id == token);
+        if(findToken){
+            tokenDataWithLocked[token].price = findToken.price
+        }else{
+            if(token === ethAddress){
+                tokenDataWithLocked[token].price = tokenPrices.find(tokenPrice => tokenPrice.id == wethAddress).price;
+            }else{
+                console.debug(token, tokenDataWithLocked[token].symbol, 1);  //这里显示取不到价格的token
+                tokenDataWithLocked[token].price = 1
+            }
+        }
+    })
+
+    const tokenTotalValue = Object.keys(tokenDataWithLocked).reduce((all, current) => {
+        return all.plus(new BigNumber(tokenDataWithLocked[current].price).times(tokenDataWithLocked[current].locked));
+    },new BigNumber(0))
+    return tokenTotalValue.plus(xdexTotalLiquidity).toString();
 }
+
+fetch();
 
 module.exports = {
     fetch
