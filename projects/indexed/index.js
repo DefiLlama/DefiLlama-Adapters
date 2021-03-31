3c44a5279c2211822a8b0c4aaf2009bc7be2a4d2
0xngmi <0xngmi@protonmail.com>
2021-03-31 17:28:50 +0200

    Add new indexed adapter

---
+++ projects/indexed/index.js
@@ -0,0 +1,40 @@
+const BigNumber = require("bignumber.js");
+const { request, gql } = require("graphql-request");
+
+const graphUrl = 'https://api.thegraph.com/subgraphs/name/indexed-finance/indexed'
+const graphQuery = gql`
+query get_indexes($block: Int) {
+  indexPools(
+    block: { number: $block }
+  ) {
+    name
+    totalValueLockedUSD
+  }
+}
+`;
+const usdtAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+
+async function tvl(timestamp, block) {
+    const { indexPools } = await request(
+        graphUrl,
+        graphQuery,
+        {
+            block,
+        }
+    );
+    const usdTvl = indexPools.reduce((total, p) => total + Number(p.totalValueLockedUSD), 0)
+
+    return {
+        [usdtAddress]: (usdTvl*1e6).toFixed(0)
+    }
+}
+
+
+module.exports = {
+    name: 'Indexed',
+    token: 'NDX',
+    category: 'Indexes',
+    eth: tvl,
+    start: 0, // WRONG!
+    tvl
+}
