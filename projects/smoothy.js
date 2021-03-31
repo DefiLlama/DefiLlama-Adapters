5a9a61d60d6edacaddccaf5ab33f43d4f31411d2
DefiLlama <defillama@protonmail.com>
2021-03-31 10:18:19 +0100

    merge smoothy adapters

--- projects/smoothy.js
+++ projects/smoothy.js
@@ -2,7 +2,10 @@ const Web3 = require('web3')
 const env = require('dotenv').config()
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${env.parsed.INFURA_KEY}`));
 const abis = require('./config/smoothy/abis.js')
+const abisbsc = require('./config/smoothy-bsc/abis.js')
+
 const BigNumber = require("bignumber.js");
+const web3bsc = new Web3(new Web3.providers.HttpProvider(`https://bsc-dataseed.binance.org/`));
 
 async function getGWeiFromWei(wei, decimals = 18) {
     return BigNumber(wei)
@@ -11,6 +14,13 @@ async function getGWeiFromWei(wei, decimals = 18) {
 }
 
 async function fetch() {
+  let bsctvl = await fetchETH();
+  let tvlETH = await fetchBSC();
+  return bsctvl + tvlETH
+}
+
+
+async function fetchETH() {
     const imp = '0xe5859f4efc09027a9b718781dcb2c6910cac6e91';
     const dacontract = new web3.eth.Contract(abis.abis.smoothy, imp)
     const allTokens = abis.abis.tokens;
@@ -26,6 +36,28 @@ async function fetch() {
     tvl = tvl.toFixed(2);
     return parseFloat(tvl);
 }
+
+
+
+
+async function fetchBSC() {
+    const imp = '0xe5859f4efc09027a9b718781dcb2c6910cac6e91';
+    const dacontract = new web3bsc.eth.Contract(abisbsc.abis.smoothy, imp)
+    const allTokens = abisbsc.abis.bsctokens;
+
+    let tvl = BigNumber(0);
+    await Promise.all(
+        allTokens.map(async (token) => {
+            const result = await dacontract.methods.getTokenStats(token.id).call();
+            let balance = result[2];
+            tvl = tvl.plus(await getGWeiFromWei(balance, token.decimals));
+        })
+    )
+    tvl = tvl.toFixed(2);
+    return parseFloat(tvl);
+}
+
+
 
 module.exports = {
     fetch
