299275eff5d5f438073e46bbe108f08461f2fae2
0xngmi <0xngmi@protonmail.com>
2021-04-01 17:29:23 +0200

    accept alchemy keys

--- projects/config/web3.js
+++ projects/config/web3.js
@@ -1,4 +1,10 @@
 const Web3 = require('web3');
 const env = require("dotenv").config();
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${env.parsed.INFURA_KEY}`));
+let web3RpcUrl;
+if(process.env && process.env.ALCHEMY_API){
+    web3RpcUrl = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API}`
+} else if(env && env.parsed && env.parsed.INFURA_KEY){
+    web3RpcUrl = `https://mainnet.infura.io/v3/${env.parsed.INFURA_KEY}`
+}
+const web3 = new Web3(new Web3.providers.HttpProvider(web3RpcUrl));
 module.exports = web3;
