67940c25be17cd16510796c2ebe1833d0e8eef06
0xngmi <0xngmi@protonmail.com>
2021-04-01 17:20:33 +0200

    Add env

--- projects/config/web3.js
+++ projects/config/web3.js
@@ -1,3 +1,4 @@
 const Web3 = require('web3');
+const env = require("dotenv").config();
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${env.parsed.INFURA_KEY}`));
 module.exports = web3;
