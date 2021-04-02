8f9c32c57beb9c7e93d788c571fc6237041067b6
0xngmi <0xngmi@protonmail.com>
2021-04-03 00:37:03 +0200

    fix rari

--- projects/rari.js
+++ projects/rari.js
@@ -1,7 +1,7 @@
 const fetchUrl = require("node-fetch");
 
 async function fetch() {
-  const { tvl } = fetchUrl("https://app.rari.capital/api/stats").then((res) =>
+  const { tvl } = await fetchUrl("https://app.rari.capital/api/stats").then((res) =>
     res.json()
   );
 
