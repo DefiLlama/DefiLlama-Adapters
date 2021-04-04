9df2b5eaa3d784876809bd99edac5ab411b2c4f7
Artem Alagizov <alagizov@gmail.com>
2021-04-04 13:46:49 +0200

    minor change

--- projects/mirror.finance.js
+++ projects/mirror.finance.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const BigNumber = require("bignumber.js");
 
 async function fetch() {
-  return await axios.get("https://graph.mirror.finance/graphql?query={statgistic{totalValueLocked}}")
+  return await axios.get("https://graph.mirror.finance/graphql?query={statistic{totalValueLocked}}")
     .then(response => new BigNumber(response.data.data.statistic.totalValueLocked).div(10 ** 6).toFixed(2))
     .catch(exception => {
       console.log("Failed to fetch mirror.finance TVL, error: " + exception);
