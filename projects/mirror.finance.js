9652454234e93629813c96d809dc996a561349b1
ArtemAlagizov <alagizov@gmail.com>
2021-04-04 11:46:06 +0200

    added mirror.finance tvl

---
+++ projects/mirror.finance.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+const BigNumber = require("bignumber.js");
+
+async function fetch() {
+  return await axios.get("https://graph.mirror.finance/graphql?query={statgistic{totalValueLocked}}")
+    .then(response => new BigNumber(response.data.data.statistic.totalValueLocked).div(10 ** 6).toFixed(2))
+    .catch(exception => {
+      console.log("Failed to fetch mirror.finance TVL, error: " + exception);
+
+      return 0;
+    });
+}
+
+module.exports = {
+  fetch
+}
