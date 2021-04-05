47c331d9ac038ec8c2317fbcf6938c0bf11f29c6
0xngmi <0xngmi@protonmail.com>
2021-04-05 14:49:27 +0200

    fix powerindex

--- projects/powerindex/index.js
+++ projects/powerindex/index.js
@@ -12,6 +12,8 @@ const abi = require('./abi');
   TVL
   ==================================================*/
 
+const gusd = '0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd'
+
 async function tvl(timestamp, block) {
   let balances = {
     '0x0000000000000000000000000000000000000000': '0', // ETH
@@ -56,15 +58,31 @@ async function tvl(timestamp, block) {
     })
   });
 
-  const [{output: poolBalances}, {output: tokensUnderlyings}] = await Promise.all([
+  const [{output: poolBalances}, {output: tokensUnderlyings}, {output: pricesPerFullShare}, {output: tokens}] = await Promise.all([
     sdk.api.abi.multiCall({ block, calls: poolCalls, abi: 'erc20:balanceOf' }),
-    sdk.api.abi.multiCall({ block, calls: poolCalls.map(c => ({target: c.target})), abi: abi.underlying })
+    sdk.api.abi.multiCall({ block, calls: poolCalls.map(c => ({target: c.target})), abi: abi.underlying }),
+    sdk.api.abi.multiCall({ block, calls: poolCalls.map(c => ({target: c.target})), abi: abi.getPricePerFullShare }),
+    sdk.api.abi.multiCall({ block, calls: poolCalls.map(c => ({target: c.target})), abi: abi.token })
   ]);
 
   for (let i = 0; i < poolBalances.length; i++) {
     const balanceOf = poolBalances[i];
     const tokenAddress = balanceOf.input.target;
-    const underlying = _.find(tokensUnderlyings, t => t.input.target === tokenAddress);
+    let underlying = _.find(tokensUnderlyings, t => t.input.target === tokenAddress);
+    const pricePerFullShare = pricesPerFullShare[i];
+    if(pricePerFullShare.success){
+      underlying = tokens[i];
+      if(underlying.output.toLowerCase() === '0xd2967f45c4f384deea880f807be904762a3dea07'){
+        const virtualPrice = await sdk.api.abi.call({
+          target: '0x4f062658EaAF2C1ccf8C8e36D6824CDf41167956',
+          abi: abi.get_virtual_price,
+          block
+        })
+        underlying.output = gusd
+        balanceOf.output = BigNumber(balanceOf.output).times(virtualPrice.output).div(BigNumber(10).pow(18+18-2)).toFixed(0) // 2 decimals for GUSD
+      }
+      balanceOf.output = BigNumber(balanceOf.output).times(pricePerFullShare.output).div(BigNumber(10).pow(18)).toFixed(0)
+    }
     if (balanceOf.success) {
       const balance = balanceOf.output;
       const address = underlying.success ? underlying.output : tokenAddress;
