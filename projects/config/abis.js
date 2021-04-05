da724d3ef2ab875012bf864081b740ac199930f9
akshaynexus <akshaycm@hotmail.com>
2021-04-05 22:49:45 +0300

    feat: add support for new transmuter

--- projects/config/abis.js
+++ projects/config/abis.js
@@ -1,33 +1,51 @@
-
 let abis = {};
 
 abis.minABI = [
   // balanceOf
   {
-    "constant":true,
-    "inputs":[{"name":"_owner","type":"address"}],
-    "name":"balanceOf",
-    "outputs":[{"name":"balance","type":"uint256"}],
-    "type":"function"
+    constant: true,
+    inputs: [{ name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "balance", type: "uint256" }],
+    type: "function"
   },
   // decimals
   {
-    "constant":true,
-    "inputs":[],
-    "name":"decimals",
-    "outputs":[{"name":"","type":"uint8"}],
-    "type":"function"
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ name: "", type: "uint8" }],
+    type: "function"
   },
   //totalSupply
   {
-    "constant":true,
-    "inputs":[],
-    "name":"totalSupply",
-    "outputs":[{"name":"","type":"uint256"}],
-    "type":"function"
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ name: "", type: "uint256" }],
+    type: "function"
   }
 ];
 
+// Basic yearn vault abi
+abis.minYvV2 = [
+  // balanceOf
+  {
+    constant: true,
+    inputs: [{ name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "balance", type: "uint256" }],
+    type: "function"
+  },
+  //pricePerShare
+  {
+    constant: true,
+    inputs: [],
+    name: "pricePerShare",
+    outputs: [{ name: "", type: "uint256" }],
+    type: "function"
+  }
+];
 module.exports = {
   abis
-}
+};
