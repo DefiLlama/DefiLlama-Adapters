06fb8c5bef9cefc8d938b9264796c42e41d21c40
0xngmi <0xngmi@protonmail.com>
2021-04-07 16:26:12 +0200

    merge bluna into lido

--- projects/lido.js
+++ projects/lido.js
@@ -14,9 +14,20 @@ async function fetch() {
       let dacontract = new web3.eth.Contract(abis.abis.lido, contract)
       let balances = await dacontract.methods.getTotalPooledEther().call();
       let lidoBalance = new BigNumber(balances).div(10 ** 18).toFixed(2);
-        tvl += (parseFloat(lidoBalance) * price_feed.data.ethereum.usd)
+      tvl += (parseFloat(lidoBalance) * price_feed.data.ethereum.usd)
     })
   )
+  let { total_bond_amount } = (
+    await utils.fetchURL("https://lcd.terra.dev/wasm/contracts/terra1mtwph2juhj0rvjz7dy92gvl6xvukaxu8rfv8ts/store?query_msg=%7B%22state%22%3A%20%7B%7D%7D")
+  ).data.result;
+
+  let { amount } = (
+    await utils.fetchURL("https://lcd.terra.dev/market/swap?offer_coin=1000000uluna&ask_denom=uusd")
+  ).data.result;
+
+  tvl += Math.floor(
+    (amount * 1.0 / 1000000) * (total_bond_amount / 1000000)
+  );
 
   return tvl;
 }
