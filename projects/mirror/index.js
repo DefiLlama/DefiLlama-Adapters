412b788a4f1d2e0eaaba3ca1b281fe941d4aeae6
0xngmi <0xngmi@protonmail.com>
2021-04-08 18:30:49 +0200

    add mirror pool2

--- projects/mirror/index.js
+++ projects/mirror/index.js
@@ -38,7 +38,21 @@ const getTotalLiq = assets => assets.reduce((acc, asset)=>acc+ust(asset.statisti
 const getPool2Liq = liq => ust(liq.find(asset=>asset.symbol==="MIR").statistic.liquidity)
 const ustAddress = "0xa47c8bf37f92abed4a126bda807a7b7498661acd"
 
-async function Terra() {
+async function terraPool2() {
+  const pool2TerraLiq = getPool2Liq(await getMirLiquidity('TERRA'))
+  return {
+    [ustAddress] : pool2TerraLiq.toFixed(0),
+  }
+}
+
+async function ethereumPool2() {
+  const pool2EthLiq = getPool2Liq(await getMirLiquidity('ETH'))
+  return {
+    [ustAddress] : pool2EthLiq.toFixed(0),
+  }
+}
+
+async function terraTvl() {
   const totalTvl = request(graphUrl,tvlQuery).then(data=>ust(data.statistic.totalValueLocked))
   const ethLiquidity = getMirLiquidity('ETH')
   const terraLiquidity = getMirLiquidity('TERRA')
@@ -50,7 +64,7 @@ async function Terra() {
   }
 }
 
-async function Ethereum() {
+async function ethereumTvl() {
   const ethLiquidity = getMirLiquidity('ETH')
   const totalEthLiquidity = getTotalLiq(await ethLiquidity)
   const pool2EthLiq = getPool2Liq(await ethLiquidity)
@@ -62,8 +76,8 @@ async function Ethereum() {
 
 const getTvl = balances => Number(balances[ustAddress])
 async function tvl(timestamp, block) {
-  const eth = getTvl(await Ethereum())
-  const terr = getTvl(await Terra())
+  const eth = getTvl(await ethereumTvl())
+  const terr = getTvl(await terraTvl())
   return {
     [ustAddress]: (eth+terr).toFixed(0)
   }
@@ -72,6 +86,12 @@ async function tvl(timestamp, block) {
 
 module.exports = {
   tvl,
-  Ethereum,
-  Terra
+  ethereum: {
+    tvl: ethereumTvl,
+    pool2: ethereumPool2
+  },
+  terra: {
+    tvl: terraTvl,
+    pool2: terraPool2
+  }
 }
