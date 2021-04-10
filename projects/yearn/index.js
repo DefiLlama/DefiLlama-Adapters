e1c07a8f1ef003ad9079cf6621bbfb95e8f25d44
0xngmi <0xngmi@protonmail.com>
2021-04-11 00:19:43 +0200

    fix yearn hourly values

--- projects/yearn/index.js
+++ projects/yearn/index.js
@@ -2,8 +2,15 @@ const axios = require('axios')
 const BigNumber = require('bignumber.js')
 
 const usdtAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+const toUSDT = value => BigNumber(value).times(1e6).toFixed(0)
 
 async function tvl(timestamp) {
+    if(Math.abs(timestamp-Date.now()/1000)<3600){
+        const tvl = await axios.get('https://yearn.science/v1/tvl/latest')
+        return{
+            [usdtAddress]:toUSDT(tvl.data.tvl)
+        }
+    }
     const historicalTvls = Object.entries((await axios.get('https://yearn.science/v1/tvl')).data)
         .map(([date, tvl]) => [Date.parse(date)/1000, tvl]).sort(([date1], [date2]) => date1 - date2);
     let high = historicalTvls.length;
@@ -18,7 +25,7 @@ async function tvl(timestamp) {
         }
     }
     return {
-        [usdtAddress]: BigNumber(historicalTvls[low][1]).times(1e6).toFixed(0)
+        [usdtAddress]: toUSDT(historicalTvls[low][1])
     }
 }
 
