fbbdd6e57e89f15aa4a5137172e8c1011b973a44
0xngmi <0xngmi@protonmail.com>
2021-04-11 14:59:14 +0200

    add dpi on migrator

--- projects/basketdao/index.js
+++ projects/basketdao/index.js
@@ -8,12 +8,18 @@ const bDPISLP = '0x8d782C5806607E9AAFB2AC38c1DA3838Edf8BD03'
 const bDPIToken = '0x0309c98B1bffA350bcb3F9fB9780970CA32a5060'
 const masterChef = '0xDB9daa0a50B33e4fe9d0ac16a1Df1d335F96595e'
 const weth = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+const continuousMigrator = '0x3f436dE9ef3f07b770c4DB45F60f9f1d323Bbf36'
 
 async function tvl(timestamp, block) {
   let balances = {};
   const dpiLocked = sdk.api.erc20.balanceOf({
     target: dpiToken,
     owner: masterChef,
+    block
+  })
+  const dpiLockedOnMigrator = sdk.api.erc20.balanceOf({
+    target: dpiToken,
+    owner: continuousMigrator,
     block
   })
   const dpiLPLocked = sdk.api.erc20.balanceOf({
@@ -46,6 +52,7 @@ async function tvl(timestamp, block) {
   const wethOnLockedSLP = BigNumber((await totalWethOnSlp).output).times((await bDPILPLocked).output).div((await slpSupply).output)
   sdk.util.sumSingleBalance(balances, weth, wethOnLockedSLP.toFixed(0))
   sdk.util.sumSingleBalance(balances, dpiToken, (await dpiLocked).output)
+  sdk.util.sumSingleBalance(balances, dpiToken, (await dpiLockedOnMigrator).output)
   sdk.util.sumSingleBalance(balances, bDPIToken, (await bdpiSupply).output)
   return balances
 }
