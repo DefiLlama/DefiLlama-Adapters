e221431e656b5fe3b9e5aefa1ca62524960cbea2
Oliver Rose <oliver.rose.bsc@users.noreply.github.com>
2021-04-11 21:02:12 +0400

    Update TVL for OliveCash

--- projects/olivecash/index.js
+++ projects/olivecash/index.js
@@ -3,26 +3,42 @@ const graphUrl = 'https://api.thegraph.com/subgraphs/name/olive-rose/olivecash'
 
 const graphQuery = gql`
 query get_tvl($block: Int) {
-  uniswapFactory(
+  pangolinFactory(
     id: "0x4Fe4D8b01A56706Bc6CaD26E8C59D0C7169976b3",
     block: { number: $block }
   ) {
-    totalVolumeUSD
-    totalLiquidityUSD
+        totalLiquidityETH
+  },
+  tokens(where: { symbol: "USDT" }, first:1) {
+    derivedETH
   }
 }
 `;
-const usdtAddress = '0xde3A24028580884448a5397872046a019649b084'
+// Response should be the following:
+// {
+//   "data": {
+//   "tokens": [
+//     {
+//       "derivedETH": "0.03153220767265978413381371295488958"
+//     }
+//   ],
+//       "pangolinFactory": {
+//     "totalLiquidityETH": "132833.6232868980185536992835742923"
+//   }
+// }
+// }
+//  132833/ 0.031 = ~4.2M TVL - its correct now
 
 async function tvl(timestamp, block) {
-  const {uniswapFactory} = await request(
+  const {response} = await request(
     graphUrl,
     graphQuery,
     {
       block,
     }
   );
-  const usdTvl = Number(uniswapFactory.totalLiquidityUSD)
+
+  const usdTvl = Number(response.pangolinFactory.totalLiquidityETH / response.tokens.derivedETH)
 
   return {
     [usdtAddress]: (usdTvl * 1e6).toFixed(0)
