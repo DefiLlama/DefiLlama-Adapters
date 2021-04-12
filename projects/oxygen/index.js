d08777e6eef8cef8776220ccdeb758975d824d60
0xngmi <0xngmi@protonmail.com>
2021-04-12 21:37:22 +0200

    add oxygen

---
+++ projects/oxygen/index.js
@@ -0,0 +1,22 @@
+const axios = require("axios")
+
+const endpoint = "https://history-api.oxygen.org/oxygen-history/public/api/v1/protocols/FUa77Li8RJAn28dQZJvYb9Xb5mmMRQbXN2eA5Q6pU4h7/pools?pageNumber=1&pageSize=1000&sortDirection=DESC&sortField=CREATED_AT&showZeroBalance=false"
+const usdtAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+
+async function tvl() {
+    const pools = await axios.get(endpoint)
+    const tvl = pools.data.data.reduce((acc, pool)=>{
+        return acc+pool.assetValue-pool.liabilityValue
+    }, 0)
+    return {
+        [usdtAddress]: (tvl*1e6).toFixed(0)
+    }
+}
+
+module.exports = {
+    solana: {
+        tvl,
+    },
+    timeTravelUnsupported: true,
+    tvl
+}
