3dd9c32451fa4c238c55f64d0f629b97b0ac9dab
0xngmi <0xngmi@protonmail.com>
2021-04-16 01:36:31 +0200

    fix pool2 mistake

--- projects/penguin/index.js
+++ projects/penguin/index.js
@@ -3,8 +3,8 @@ const {unwrapUniswapLPs} = require('../helper/unwrapLPs')
 const BigNumber = require('bignumber.js')
 
 const masterchefIgloos = "0x8AC8ED5839ba269Be2619FfeB3507baB6275C257"
-const pool2Token = '0x1bb5541EcCdA68A352649954D4C8eCe6aD68338d'
-const iglooTokens = ['0x494Dd9f783dAF777D3fb4303da4de795953592d0', '0x0b9753D73e1c62933e913e9c2C94f2fFa8236F6C', '0x1aCf1583bEBdCA21C8025E172D8E8f2817343d65']
+const pool2Token = '0x494Dd9f783dAF777D3fb4303da4de795953592d0'
+const iglooTokens = ['0x1bb5541EcCdA68A352649954D4C8eCe6aD68338d', '0x0b9753D73e1c62933e913e9c2C94f2fFa8236F6C', '0x1aCf1583bEBdCA21C8025E172D8E8f2817343d65']
 const nest = '0xD79A36056c271B988C5F1953e664E61416A9820F'
 const pefiToken = '0xe896CDeaAC9615145c0cA09C8Cd5C25bced6384c'
 
@@ -12,7 +12,8 @@ const tokenToCoingeckoId = {
   '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7': 'avalanche-2',
   '0xe896CDeaAC9615145c0cA09C8Cd5C25bced6384c': 'penguin-finance',
   '0xC38f41A296A4493Ff429F1238e030924A1542e50': 'snowball-token',
-  '0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15': 'ethereum'
+  '0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15': 'ethereum',
+  '0x60781C2586D68229fde47564546784ab3fACA982': 'pangolin'
 }
 
 async function convertBalancesToCoingecko(balances){
@@ -39,6 +40,7 @@ async function getTokensInMasterChef(chainBlocks){
       token: output.input.target,
       balance: output.output
     })), chainBlocks['avax'], 'avax')
+    console.log(balances)
     return balances
 }
 
