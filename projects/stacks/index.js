e83dd2d7108b7e8a0ec57b2246fceaf57b8247a6
0xngmi <0xngmi@protonmail.com>
2021-04-22 06:48:54 +0200

    Move stacks to tvl adapter

---
+++ projects/stacks/index.js
@@ -0,0 +1,13 @@
+const axios = require("axios")
+
+async function tvl(timestamp) {
+    const stakedStacks = await axios.get(`https://api.stacking.club/api/tvl?timestamp=${timestamp * 1000}`)
+
+    return {
+        "blockstack": stakedStacks.data
+    };
+}
+
+module.exports = {
+    tvl
+}
