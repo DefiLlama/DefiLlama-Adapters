5a73e1a093db5d9f3f0632bedd566ee39b6b0c25
Rory Murray <21082125+rorychristianmurray@users.noreply.github.com>
2021-04-28 07:35:14 -0400

    Adds xBNTa to tvl adapter

--- projects/xtoken.js
+++ projects/xtoken.js
@@ -10,19 +10,22 @@ const web3 = new Web3(
 );
 const BigNumber = require("bignumber.js");
 const {
-  kncAddr,
   xaaveaAddr,
   xaavebAddr,
+  xbntaAddr,
+  bntAddr,
   xinchaAddr,
   xinchbAddr,
   xkncaAddr,
   xkncbAddr,
+  kncAddr,
   xsnxaAddr,
 } = require("./config/xtoken/constants");
 const xAAVE = require("./config/xtoken/xAAVE.json");
 const xINCH = require("./config/xtoken/xINCH.json");
 const xKNC = require("./config/xtoken/xKNC.json");
 const xSNX = require("./config/xtoken/xSNX.json");
+const xBNT = require("./config/xtoken/xBNT.json");
 
 async function fetch() {
   const xaaveaCtr = new web3.eth.Contract(xAAVE, xaaveaAddr);
@@ -32,12 +35,29 @@ async function fetch() {
   const xkncaCtr = new web3.eth.Contract(xKNC, xkncaAddr);
   const xkncbCtr = new web3.eth.Contract(xKNC, xkncbAddr);
   const xsnxaCtr = new web3.eth.Contract(xSNX, xsnxaAddr);
+  const xbntaCtr = new web3.eth.Contract(xBNT, xbntaAddr);
 
   const xaaveaTvlRaw = await xaaveaCtr.methods.getFundHoldings().call();
   const xaavebTvlRaw = await xaavebCtr.methods.getFundHoldings().call();
 
   const xaaveaTvlToken = new BigNumber(xaaveaTvlRaw).div(10 ** 18).toFixed(2);
   const xaavebTvlToken = new BigNumber(xaavebTvlRaw).div(10 ** 18).toFixed(2);
+
+  const xbntaStakedRaw = await xbntaCtr.methods.totalAllocatedNav().call();
+  const xbntaBufferRaw = await xbntaCtr.methods.getBufferBalance().call();
+  const xbntaPendingRaw = await xbntaCtr.methods
+    .getRewardsContributionToNav()
+    .call();
+
+  const xbntaStakedToken = new BigNumber(xbntaStakedRaw)
+    .div(10 ** 18)
+    .toFixed(2);
+  const xbntaBufferToken = new BigNumber(xbntaBufferRaw)
+    .div(10 ** 18)
+    .toFixed(2);
+  const xbntaPendingToken = new BigNumber(xbntaPendingRaw)
+    .div(10 ** 18)
+    .toFixed(2);
 
   const xinchaTvlRaw = await xinchaCtr.methods.getNav().call();
   const xinchbTvlRaw = await xinchbCtr.methods.getNav().call();
@@ -57,11 +77,17 @@ async function fetch() {
     .toFixed(2);
 
   const priceAave = await utils.getPricesfromString("aave");
+  const priceBnt = await retry(
+    async (bail) =>
+      await axios.get(
+        `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${bntAddr}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true`
+      )
+  );
   const priceOneInch = await utils.getPricesfromString("1inch");
   const priceKnc = await retry(
     async (bail) =>
       await axios.get(
-        "https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xdd974D5C2e2928deA5F71b9825b8b646686BD200&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true"
+        `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${kncAddr}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true`
       )
   );
   const priceXsnxa = await retry(
@@ -73,14 +99,31 @@ async function fetch() {
 
   const xaaveaTvl = xaaveaTvlToken * priceAave.data.aave.usd;
   const xaavebTvl = xaavebTvlToken * priceAave.data.aave.usd;
+
+  const xbntaStakedTvl =
+    xbntaStakedToken *
+    priceBnt.data["0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c"].usd;
+
+  const xbntaBufferTvl =
+    xbntaBufferToken *
+    priceBnt.data["0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c"].usd;
+
+  const xbntaPendingTvl =
+    xbntaPendingToken *
+    priceBnt.data["0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c"].usd;
+
+  const xbntaTvl = xbntaStakedTvl + xbntaBufferTvl + xbntaPendingTvl;
+
   const xinchaTvl = xinchaTvlToken * priceOneInch.data["1inch"].usd;
   const xinchbTvl = xinchbTvlToken * priceOneInch.data["1inch"].usd;
+
   const xkncaTvl =
     xkncaTvlToken *
     priceKnc.data["0xdd974d5c2e2928dea5f71b9825b8b646686bd200"].usd;
   const xkncbTvl =
     xkncbTvlToken *
     priceKnc.data["0xdd974d5c2e2928dea5f71b9825b8b646686bd200"].usd;
+
   const xsnxaTvl =
     xsnxaTotalSupply *
     priceXsnxa.data["0x2367012ab9c3da91290f71590d5ce217721eefe4"].usd;
@@ -88,6 +131,7 @@ async function fetch() {
   const tvl =
     xaaveaTvl +
     xaavebTvl +
+    xbntaTvl +
     xinchaTvl +
     xinchbTvl +
     xkncaTvl +
