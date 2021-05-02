9c33a577437446f370801f911496971ccbdc52e1
0xngmi <0xngmi@protonmail.com>
2021-05-02 13:26:18 +0200

    update badger

--- projects/badgerdao.js
+++ projects/badgerdao.js
@@ -6,8 +6,9 @@ const utils = require('./helper/utils');
 *****************/
 
 async function fetch() {
-  let response = await utils.fetchURL('https://api.sett.vision/protocol/value')
-  return response.data.totalValue;
+  let eth = await utils.fetchURL('https://api.badger.finance/v2/value?chain=eth')
+  let bsc = await utils.fetchURL('https://api.badger.finance/v2/value?chain=bsc')
+  return eth.data.totalValue + bsc.data.totalValue;
 }
 
 module.exports = {
