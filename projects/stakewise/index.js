6f795dd4fa94ab8e228fdf7179d551901fe79e5c
0xngmi <0xngmi@protonmail.com>
2021-05-02 13:22:11 +0200

    update stakewise

--- projects/stakewise/index.js
+++ projects/stakewise/index.js
@@ -1,4 +1,5 @@
 const sdk = require('@defillama/sdk');
+const BigNumber = require('bignumber.js')
 
 const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 
@@ -8,8 +9,17 @@ async function tvl(timestamp, block) {
     block
   })
 
+  const solosValidators = await sdk.api.util.getLogs({
+    target: '0xEadCBA8BF9ACA93F627F31fB05470F5A0686CEca',
+    topic: 'ValidatorRegistered(bytes32,bytes,uint256,address)',
+    fromBlock: 0,
+    toBlock: block,
+    keys:[]
+  })
+  const ethOnValidators = BigNumber(solosValidators.output.length).times(32e18)
+
   return {
-    [wethAddress]: supply.output
+    [wethAddress]: ethOnValidators.plus(supply.output).toFixed(0)
   }
 }
 
