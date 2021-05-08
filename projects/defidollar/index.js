2e995d9d24a1373acccee91fc7844e98270bfbf3
atvanguard <93arpit@gmail.com>
2021-05-08 23:08:03 +0530

    abb ibbtc tvl

---
+++ projects/defidollar/index.js
@@ -0,0 +1,45 @@
+/* ************************** *
+ * Modules
+ * ************************** */
+const TOKEN = require("../config/defidollar/tokenABI.json");
+const ORACLE = require("../config/defidollar/EACAggregatorABI.json");
+const WEB3 = require('../config/web3.js');
+
+/* ************************** *
+ * Constants
+ * ************************** */
+const DUSD = "0x5bc25f649fc4e26069ddf4cf4010f9f706c23831";
+const IBBTC = "0x5bc25f649fc4e26069ddf4cf4010f9f706c23831";
+const BTC_USD_ORACLE = '0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c'
+
+/* ************************** *
+ * Methods
+ * ************************** */
+async function tvl() {
+  const dusd = new WEB3.eth.Contract(TOKEN.abi, DUSD);
+  const ibBTC = new WEB3.eth.Contract(TOKEN.abi, IBBTC);
+  const btcUsdOracle = new WEB3.eth.Contract(ORACLE.abi, BTC_USD_ORACLE)
+
+  const [ dusdSupply, ibbtcSupply, pps, btcPrice ] = (await Promise.all([
+    dusd.methods.totalSupply().call(),
+    ibBTC.methods.totalSupply().call(),
+    ibBTC.methods.pricePerShare().call(),
+    btcUsdOracle.methods.latestAnswer().call()
+  ])).map(WEB3.utils.toBN)
+
+  const _1e26 = WEB3.utils.toBN(10).pow(WEB3.utils.toBN(26))
+  return WEB3.utils.fromWei(
+    ibbtcSupply
+    .mul(pps)
+    .mul(btcPrice)
+    .div(_1e26)
+    .add(dusdSupply)
+  )
+}
+
+module.exports = {
+  name: 'DefiDOllar',
+  token: 'DFD',
+  tvl
+}
+
