1aa5e1326567a928f4e233cd6564a785b6ef3be7
0xngmi <0xngmi@protonmail.com>
2021-05-10 21:05:50 +0200

    fix maker

--- projects/maker/index.js
+++ projects/maker/index.js
@@ -3,93 +3,106 @@ const utils = require('web3-utils');
 const sdk = require('@defillama/sdk');
 const MakerSCDConstants = require("./abis/makerdao.js");
 const MakerMCDConstants = require("./abis/maker-mcd.js");
+const { unwrapUniswapLPs } = require('../helper/unwrapLPs')
 
 async function getJoins(block) {
-    let rely = utils.sha3("rely(address)").substr(0, 10);
-    let relyTopic = utils.padRight(rely, 64);
-  
-    let joins = {};
-  
-    // get list of auths
+  let rely = utils.sha3("rely(address)").substr(0, 10);
+  let relyTopic = utils.padRight(rely, 64);
 
-    const logs = (
-        await sdk.api.util
-          .getLogs({
-            keys: [],
-            toBlock: block,
-            target: MakerMCDConstants.VAT,
-            fromBlock: MakerMCDConstants.STARTBLOCK,
-            topics: [relyTopic],
-          })
-      ).output;
-  
-    let auths = logs.map(auth => {
-      return `0x${auth.topics[1].substr(26)}`;
-    });
+  let joins = [];
 
-    const ilks = (await sdk.api.abi.multiCall({
-      abi: MakerMCDConstants.ilk,
-      calls: auths.map((auth) => ({
-        target: auth,
-      })),
-      block
-    })).output;
+  // get list of auths
+  const logs = (
+    await sdk.api.util
+      .getLogs({
+        keys: [],
+        toBlock: block,
+        target: MakerMCDConstants.VAT,
+        fromBlock: MakerMCDConstants.STARTBLOCK,
+        topics: [relyTopic],
+      })
+  ).output;
 
-    for (let ilk of ilks) {
-      if (ilk.output) {
-        let name = utils.hexToString(ilk.output);
-        if (name.substr(0, 3) !== 'PSM') {
-          joins[name.toString()] = ilk.input.target
-        }
+  let auths = logs.map(auth => {
+    return `0x${auth.topics[1].substr(26)}`;
+  });
+
+  const ilks = (await sdk.api.abi.multiCall({
+    abi: MakerMCDConstants.ilk,
+    calls: auths.map((auth) => ({
+      target: auth,
+    })),
+    block
+  })).output;
+
+  for (let ilk of ilks) {
+    if (ilk.output) {
+      let name = utils.hexToString(ilk.output);
+      if (name.substr(0, 3) !== 'PSM') {
+        joins.push(ilk.input.target)
       }
     }
-  
-    return joins;
   }
 
+  return joins;
+}
+
 async function tvl(timestamp, block) {
-    let balances = {};
-    balances[MakerSCDConstants.WETH_ADDRESS] = new BigNumber((await sdk.api.erc20.balanceOf({
-      block,
-      target: MakerSCDConstants.WETH_ADDRESS,
-      owner: MakerSCDConstants.TUB_ADDRESS
-    })).output);
+  let balances = {};
+  balances[MakerSCDConstants.WETH_ADDRESS] = (await sdk.api.erc20.balanceOf({
+    block,
+    target: MakerSCDConstants.WETH_ADDRESS,
+    owner: MakerSCDConstants.TUB_ADDRESS
+  })).output;
 
-    if (block >= MakerMCDConstants.STARTBLOCK) {
-      let joins = await getJoins(block);
+  if (block >= MakerMCDConstants.STARTBLOCK) {
+    let joins = await getJoins(block);
 
-      for (let join in joins) {
-        let gem = (await sdk.api.abi.call({
-          block, 
-          target: joins[join],
+    await Promise.all(joins.map(async join => {
+      try {
+        const gem = (await sdk.api.abi.call({
+          block,
+          target: join,
           abi: MakerMCDConstants.gem
         })).output;
-        console.log(gem, join)
-        let balance = (await sdk.api.erc20.balanceOf({
+        const balance = (await sdk.api.erc20.balanceOf({
           target: gem,
-          owner: joins[join],
+          owner: join,
           block
         })).output;
-        balances[gem] = balances[gem] ? balances[gem].plus(balance) : new BigNumber(balance);
+
+        const symbol = await sdk.api.erc20.symbol(gem)
+        if (symbol.output === "UNI-V2") {
+          await unwrapUniswapLPs(balances, [{
+            token: gem,
+            balance
+          }],
+            block)
+        } else {
+          sdk.util.sumSingleBalance(balances, gem, balance);
+        }
+      } catch (e) {
+        return
       }
-  
-      let pie = (await sdk.api.abi.call({
-        block,
-        target: MakerMCDConstants.POT,
-        abi: MakerMCDConstants.Pie
-      })).output;
-  
-      balances[MakerMCDConstants.DAI] = balances[MakerMCDConstants.DAI] ? balances[MakerMCDConstants.DAI].plus(pie) : new BigNumber(pie);
-    }
+    }))
 
-    return balances;
+    let pie = (await sdk.api.abi.call({
+      block,
+      target: MakerMCDConstants.POT,
+      abi: MakerMCDConstants.Pie
+    })).output;
 
+    sdk.util.sumSingleBalance(balances, MakerMCDConstants.DAI, pie);
+  }
+  console.log(balances)
+
+  return balances;
 }
 
 module.exports = {
-    name: 'Maker',
-    token: 'MKR',
-    category: 'lending',
-    start: 1513566671, // 12/18/2017 @ 12:00am (UTC)
-    tvl,
-  };
+  name: 'Maker',
+  token: 'MKR',
+  category: 'lending',
+  start: 1513566671, // 12/18/2017 @ 12:00am (UTC)
+  tvl,
+};
