3f89aff7f4c002da3580e470ecc71163d98b4109
0xngmi <0xngmi@protonmail.com>
2021-05-13 14:25:31 +0200

    Rate limit serum.js

--- projects/serum.js
+++ projects/serum.js
@@ -3,6 +3,7 @@ const { Connection, PublicKey } = require('@solana/web3.js');
 const { Market } = require('@project-serum/serum');
 
 const serumProgramId = '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'
+const step = 2;
 
 async function fetch() {
   let tvl = 0;
@@ -12,42 +13,43 @@ async function fetch() {
   const connection = new Connection('https://solana-api.projectserum.com/');
   const programId = new PublicKey(serumProgramId);
   const markets = await axios.get('https://wallet-api.bonfida.com/cached/market-table')
-  await Promise.all(markets.data.data.map(async marketData=>{
-    const marketAddress = new PublicKey(marketData.address);
-    const market = await Market.load(connection, marketAddress, {}, programId);
-    const bids = await market.loadBids(connection);
-    const asks = await market.loadAsks(connection);
-    const ceil = Math.min(...Array.from(asks).map(ask=>ask.price))
-    const floor = Math.max(...Array.from(bids).map(ask=>ask.price))
-    if(floor === -Infinity || ceil === Infinity) return
-    const price = (floor + ceil)/2
-    let marketSize = 0;
-    for (let order of Array.from(asks).concat(Array.from(bids))) {
-      //console.log(order.size, order.side, order.price)
-      marketSize += order.size
-    }
-    const [buyCurrency, quoteCurrency] = marketData.market.split('/')
+  for (let i = 0; i < markets.data.data.length; i += step) {
+    await Promise.all(markets.data.data.slice(i, i + step).map(async marketData => {
+      const marketAddress = new PublicKey(marketData.address);
+      const market = await Market.load(connection, marketAddress, {}, programId);
+      const bids = await market.loadBids(connection);
+      const asks = await market.loadAsks(connection);
+      const ceil = Math.min(...Array.from(asks).map(ask => ask.price))
+      const floor = Math.max(...Array.from(bids).map(ask => ask.price))
+      if (floor === -Infinity || ceil === Infinity) return
+      const price = (floor + ceil) / 2
+      let marketSize = 0;
+      for (let order of Array.from(asks).concat(Array.from(bids))) {
+        //console.log(order.size, order.side, order.price)
+        marketSize += order.size
+      }
+      const [buyCurrency, quoteCurrency] = marketData.market.split('/')
 
-    if(quoteCurrency === 'USDT'){
-      tokenPrices[buyCurrency] = price
-    }
-    balances[quoteCurrency] = (balances[quoteCurrency] || 0) + marketSize*price
-  }))
+      if (quoteCurrency === 'USDT') {
+        tokenPrices[buyCurrency] = price
+      }
+      balances[quoteCurrency] = (balances[quoteCurrency] || 0) + marketSize * price
+    }))
+  }
   tokenPrices['USDT'] = 1
   tokenPrices['USDC'] = 1
-  Object.entries(balances).forEach(([token, balance])=>{
+  Object.entries(balances).forEach(([token, balance]) => {
     const price = tokenPrices[token]
-    if(price === undefined){
+    if (price === undefined) {
       return
     }
     //balance error in here somewhere /Alex
     if (balance < 1000000000) {
-      tvl += price*balance
+      tvl += price * balance
     }
   })
   return tvl;
 }
-
 
 module.exports = {
   fetch
