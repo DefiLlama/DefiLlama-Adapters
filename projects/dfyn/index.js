c6612e8169e3d1ccf5f8822ada820cf3ba8ac098
0xngmi <0xngmi@protonmail.com>
2021-05-15 14:00:46 +0200

    Add dexes

---
+++ projects/dfyn/index.js
@@ -0,0 +1,35 @@
+const { request, gql } = require("graphql-request");
+const { toUSDTBalances } = require('../helper/balances');
+
+const graphUrl = 'https://api.thegraph.com/subgraphs/name/ss-sonic/dfyn-v4'
+const graphQuery = gql`
+query get_tvl($block: Int) {
+  uniswapFactories(
+    block: { number: $block }
+  ) {
+    totalVolumeUSD
+    totalLiquidityUSD
+  }
+}
+`;
+
+async function tvl(timestamp, block, chainBlocks) {
+  const {uniswapFactories} = await request(
+    graphUrl,
+    graphQuery,
+    {
+      block: chainBlocks['polygon'] - 60,
+    }
+  );
+  console.log(uniswapFactories)
+  const usdTvl = Number(uniswapFactories[0].totalLiquidityUSD)
+
+  return toUSDTBalances(usdTvl)
+}
+
+module.exports = {
+  polygon:{
+    tvl,
+  },
+  tvl
+}
