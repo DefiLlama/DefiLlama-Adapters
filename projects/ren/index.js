83986419a9bfbc81ac99e8cfe0a35117944db786
0xngmi <0xngmi@protonmail.com>
2021-05-16 18:43:39 +0200

    Change ren

--- projects/ren/index.js
+++ projects/ren/index.js
@@ -4,44 +4,49 @@ const BigNumber = require("bignumber.js");
 const { toUSDT } = require('../helper/balances');
 
 
-const graphUrl = 'https://api.thegraph.com/subgraphs/name/renproject/renvm'
+const ethGraphUrl = 'https://api.thegraph.com/subgraphs/name/renproject/renvm'
+const bscGraphUrl = 'https://api.bscgraph.org/subgraphs/name/renproject/renvm'
 const graphQuery = gql`
-query get_tvl($block: Int) {
-  assets(
-    block: { number: $block }
-  ) {
-    priceInUsd
+{
+  assets {
     symbol
     tokenAddress
   }
 }
 `;
-const usdtAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7';
 const darkNodeStakingContract = "0x60Ab11FE605D2A2C3cf351824816772a131f8782";
 const renToken = '0x408e41876cCCDC0F92210600ef50372656052a38'
 
-async function tvl(timestamp, block) {
+async function getAssetBalance(block, graphUrl, transformAddr, chain){
   const balances = {}
   const {assets} = await request(
     graphUrl,
-    graphQuery,
-    {
-      block,
-    }
+    graphQuery
   );
   const assetCalls = assets.map(asset=>({
     target: asset.tokenAddress
   }))
-  const decimals = sdk.api.abi.multiCall({
-    abi: 'erc20:decimals',
-    block,
-    calls: assetCalls
-  });
   const totalSupplies = sdk.api.abi.multiCall({
     abi: 'erc20:totalSupply',
     block,
+    chain,
     calls: assetCalls
   });
+
+  const resolvedSupplies = (await totalSupplies).output
+  assets.forEach((asset, index)=>{
+    sdk.util.sumSingleBalance(balances, transformAddr(asset.tokenAddress), resolvedSupplies[index].output)
+  })
+  return balances
+}
+
+async function bsc(timestamp, ethBlock, chainBlocks) {
+  return getAssetBalance(chainBlocks['bsc'], bscGraphUrl, ad=>`bsc:${ad}`, 'bsc')
+}
+
+
+async function eth(timestamp, block) {
+  const balances = await getAssetBalance(block, ethGraphUrl, a=>a, 'ethereum')
   const stakedRen = await sdk.api.abi.call({
     target: renToken,
     abi: 'erc20:balanceOf',
@@ -49,24 +54,15 @@ async function tvl(timestamp, block) {
     block
   });
   balances[renToken] = stakedRen.output;
-
-  const resolvedDecimals = (await decimals).output;
-  const resolvedSupplies = (await totalSupplies).output
-  const assetsUsdTvl = assets.reduce((acc, asset, index)=>{
-    const dec = Number(resolvedDecimals[index].output)
-    const supply = BigNumber(resolvedSupplies[index].output)
-    const usdAmount = supply.div(BigNumber(10).pow(dec)).times(asset.priceInUsd).toNumber()
-    return acc + usdAmount;
-  }, 0)
-  balances[usdtAddress]= toUSDT(assetsUsdTvl)
-
   return balances
 }
 
 module.exports = {
-  name: 'REN',
-  token: 'REN',
-  category: 'Assets',
-  start: 0, // WRONG!
-  tvl
+  ethereum:{
+    tvl: eth
+  },
+  bsc: {
+    tvl: bsc
+  },
+  tvl: sdk.util.sumChainTvls([eth, bsc])
 }
