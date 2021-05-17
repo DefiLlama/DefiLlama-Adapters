8fcc429a4bf9fe78c2e99d8e68abecc509e2328f
0xngmi <0xngmi@protonmail.com>
2021-05-17 11:17:54 +0200

    split aave by chain

--- projects/aave.js
+++ projects/aave.js
@@ -1,14 +1,27 @@
 const utils = require('./helper/utils');
 
-/* * * * * * * *
-* ==> Correct adapter needs to be created.
-*
-*****************/
 async function fetch() {
   var totalTvl = await utils.fetchURL('https://aave-api-v2.aave.com/data/tvl')
   return totalTvl.data.totalTvl.tvlInUsd;
 }
 
+async function eth() {
+  var totalTvl = await utils.fetchURL('https://aave-api-v2.aave.com/data/tvl')
+  return totalTvl.data.totalTvl.tvlInUsd - totalTvl.data.matic.tvlInUsd;
+}
+
+async function polygon() {
+  var totalTvl = await utils.fetchURL('https://aave-api-v2.aave.com/data/tvl')
+  return totalTvl.data.matic.tvlInUsd;
+}
+
+
 module.exports = {
+  ethereum: {
+    fetch: eth
+  },
+  polygon: {
+    fetch: polygon
+  },
   fetch
 }
