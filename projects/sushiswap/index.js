a57c3e2ba85d5acd0a4a519e95ab78b6945bdeaa
0xngmi <0xngmi@protonmail.com>
2021-05-17 12:31:02 +0200

    sushi on polygon

--- projects/sushiswap/index.js
+++ projects/sushiswap/index.js
@@ -1,5 +1,7 @@
 const { request, gql } = require("graphql-request");
 const { toUSDTBalances } = require('../helper/balances');
+const {calculateUniTvl} = require('../helper/calculateUniTvl.js')
+const sdk = require('@defillama/sdk')
 
 const graphUrl = 'https://api.thegraph.com/subgraphs/name/zippoxer/sushiswap-subgraph-fork'
 const graphQuery = gql`
@@ -14,7 +16,7 @@ query get_tvl($block: Int) {
 }
 `;
 
-async function tvl(timestamp, block) {
+async function eth(timestamp, block) {
   const {uniswapFactory} = await request(
     graphUrl,
     graphQuery,
@@ -27,9 +29,21 @@ async function tvl(timestamp, block) {
   return toUSDTBalances(usdTvl)
 }
 
+async function polygon(timestamp, ethBlock, chainBlocks){
+  return calculateUniTvl(addr=>{
+    if(addr === '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619'){
+      return '0x0000000000000000000000000000000000000000'
+    }
+    return `polygon:${addr}`
+  }, chainBlocks['polygon'], 'polygon', '0xc35DADB65012eC5796536bD9864eD8773aBc74C4', 11333218);
+}
+
 module.exports = {
   ethereum:{
-    tvl,
+    tvl: eth,
   },
-  tvl
+  polygon: {
+    tvl: polygon
+  },
+  tvl: sdk.util.sumChainTvls([eth, polygon])
 }
