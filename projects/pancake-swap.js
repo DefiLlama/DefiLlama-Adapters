2271bbb6e20d4164f446f348eb9e3b55c1362720
0xngmi <0xngmi@protonmail.com>
2021-05-20 13:29:21 +0200

    pancakeswap progress

--- projects/pancake-swap.js
+++ projects/pancake-swap.js
@@ -1,15 +1,19 @@
 const utils = require('./helper/utils');
-
-/* * * * * * * *
-* ==> Correct adapter needs to be created.
-*
-*****************/
+const {calculateUniTvl} = require('./helper/calculateUniTvl.js')
 
 async function fetch() {
   let response = await utils.fetchURL('https://api.pancakeswap.finance/api/v1/stat')
   return response.data.total_value_locked_all;
 }
 
+async function tvl(timestamp, ethBlock, chainBlocks){
+  const a= await calculateUniTvl(addr=>{
+    return `bsc:${addr}`
+  }, undefined, 'bsc', '0xBCfCcbde45cE874adCB698cC183deBcF17952812', 586899, true);
+  console.log(a)
+  return a
+}
+
 module.exports = {
-  fetch
+  tvl
 }
