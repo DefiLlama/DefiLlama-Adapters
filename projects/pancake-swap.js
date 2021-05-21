f0350357ea1f320a3fb6f3584caefd75d74108a3
0xngmi <0xngmi@protonmail.com>
2021-05-21 21:26:32 +0200

    Add staked cake

--- projects/pancake-swap.js
+++ projects/pancake-swap.js
@@ -6,12 +6,21 @@ async function fetch() {
   return response.data.total_value_locked_all;
 }
 
+const factory = '0xBCfCcbde45cE874adCB698cC183deBcF17952812'
+const cakeToken = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82'
+const masterChef = '0x73feaa1eE314F8c655E354234017bE2193C9E24E'
 async function tvl(timestamp, ethBlock, chainBlocks){
-  const a= await calculateUniTvl(addr=>{
+  const stakedCake = sdk.api.erc20.balanceOf({
+    target: cakeToken,
+    owner: masterChef,
+    chain: 'bsc'
+  })
+  const balances= await calculateUniTvl(addr=>{
     return `bsc:${addr}`
-  }, undefined, 'bsc', '0xBCfCcbde45cE874adCB698cC183deBcF17952812', 586899, true);
-  console.log(a)
-  return a
+  }, undefined, 'bsc', factory, 586899, true);
+  
+  sdk.util.sumSingleBalance(balances, 'bsc:'+cakeToken, (await stakedCake).output)
+  return balances
 }
 
 module.exports = {
