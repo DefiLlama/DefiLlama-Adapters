03df1facf3224e22a8e11c5c5dd3ca6275110f63
0xngmi <0xngmi@protonmail.com>
2021-05-22 02:55:21 +0200

    pancake

--- projects/pancake/index.js
+++ projects/pancake/index.js
@@ -4,11 +4,11 @@ const token0 = require('../helper/abis/token0.json');
 const token1 = require('../helper/abis/token1.json');
 const getReserves = require('../helper/abis/getReserves.json');
 
-const factory = '0xBCfCcbde45cE874adCB698cC183deBcF17952812'
+const FACTORY = '0xBCfCcbde45cE874adCB698cC183deBcF17952812'
 const cakeToken = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82'
 const masterChef = '0x73feaa1eE314F8c655E354234017bE2193C9E24E'
 
-async function processPairs(balances, pairNums){
+async function processPairs(balances, pairNums, chain, block, factory){
   const pairs = (await sdk.api.abi.multiCall({
     abi: abi.allPairs,
     chain: 'bsc',
@@ -18,8 +18,6 @@ async function processPairs(balances, pairNums){
     }))
   })).output
   const pairAddresses = pairs.map(result => result.output.toLowerCase())
-  const chain = 'bsc'
-  const block = undefined
   const [token0Addresses, token1Addresses, reserves] = await Promise.all([
     sdk.api.abi
       .multiCall({
@@ -62,19 +60,21 @@ async function tvl(timestamp, ethBlock, chainBlocks){
   const pairLength = Number((await sdk.api.abi.call({
       target: factory,
       abi: abi.allPairsLength,
-      chain: 'bsc'
+      chain: 'bsc',
+      block: chainBlocks['bsc']
   })).output)
   const allPairNums = Array.from(Array(pairLength).keys())
   const reqs = []
   for(let i=0; i< pairLength; i+=500){
     const pairNums = allPairNums.slice(i, i+500);
-    reqs.push(processPairs(balances, pairNums))
+    reqs.push(processPairs(balances, pairNums, 'bsc', chainBlocks['bsc'], FACTORY))
   }
   await Promise.all(reqs)
   const stakedCake = await sdk.api.erc20.balanceOf({
     target: cakeToken,
     owner: masterChef,
-    chain: 'bsc'
+    chain: 'bsc',
+    block: chainBlocks['bsc']
   })
   sdk.util.sumSingleBalance(balances, 'bsc:'+cakeToken, stakedCake.output)
   return balances
