9b329a3f48594fcf65401ea9fb9c20e0b2eaae0b
0xngmi <0xngmi@protonmail.com>
2021-05-25 08:37:40 +0200

    fix autofarm

--- projects/autofarm.js
+++ projects/autofarm.js
@@ -9,7 +9,7 @@ const bscEndpoint = "https://static.autofarm.network/bsc/farm_data.json"
 
 async function fetch() {
   var bscPools = await utils.fetchURL(bscEndpoint)
-  let tvl = Object.values(bscPools.data.pools).reduce((total, pool) => total + pool.poolWantTVL, 0)
+  let tvl = Object.values(bscPools.data.pools).reduce((total, pool) => total + (pool.poolWantTVL || 0), 0)
   //var hecototalTvl = await utils.fetchURL('https://api.autofarm.network/heco/get_stats')
   return tvl// + hecototalTvl.data.platformTVL;
 }
