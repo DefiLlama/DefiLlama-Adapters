da475b20a47ca95795c24d1ba4df5c1e6b323d25
0xngmi <0xngmi@protonmail.com>
2021-05-25 21:52:17 +0200

    Separate tvl by chain

--- projects/beefy/index.js
+++ projects/beefy/index.js
@@ -1,20 +1,50 @@
 const utils = require('./helper/utils');
 
+async function fetchChain(chainId) {
+  return async () => {
+    const response = await utils.fetchURL('https://api.beefy.finance/tvl');
+
+    let tvl = 0;
+    const chain = response.data[chainId];
+    for (vault in chain) {
+      tvl += chain[vault];
+    }
+
+    return tvl;
+  }
+}
+
+
 async function fetch() {
   const response = await utils.fetchURL('https://api.beefy.finance/tvl');
-  
+
   let tvl = 0;
   for (chainId in response.data) {
     const chain = response.data[chainId];
 
     for (vault in chain) {
       tvl += chain[vault];
-    } 
+    }
   }
 
   return tvl;
 }
 
 module.exports = {
+  bsc:{
+    fetch: fetchChain(56)
+  },
+  heco:{
+    fetch: fetchChain(128)
+  },
+  polygon:{
+    fetch: fetchChain(137)
+  },
+  fantom:{
+    fetch: fetchChain(250)
+  },
+  avalanche:{
+    fetch: fetchChain(43114)
+  },
   fetch
 }
