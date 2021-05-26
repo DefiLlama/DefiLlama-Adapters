5ab0e7f0c46d4840a0a91fbc73f42058d0ffcbba
0xngmi <0xngmi@protonmail.com>
2021-05-26 07:45:16 +0200

    Transform into sdk

--- projects/spookyswap/index.js
+++ projects/spookyswap/index.js
@@ -1,25 +1,11 @@
-const utils = require('../helper/utils');
 const {calculateUniTvl} = require('../helper/calculateUniTvl.js')
+const {transformFantomAddress} = require('../helper/portedTokens.js')
 
 const factory = '0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3'
 async function tvl(_timestamp, _ethBlock, chainBlocks){
-  const multichainTokens = (await utils.fetchURL('https://netapi.anyswap.net/bridge/v2/info')).data.bridgeList
+  const transform = await transformFantomAddress();
 
-  const balances = await calculateUniTvl(addr=>{
-    // WFTM
-    if(addr.toLowerCase() === "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83"){
-      return "0x4e15361fd6b4bb609fa63c81a2be19d873717870"
-    }
-    const srcToken = multichainTokens.find(token=>token.chainId === "250" && token.token === addr.toLowerCase())
-    if(srcToken !== undefined){
-      if(srcToken.srcChainId === '1'){
-        return srcToken.srcToken;
-      } else if(srcToken.srcChainId === '56'){
-        return `bsc:${srcToken.srcToken}`;
-      }
-    }
-    return `fantom:${addr}`
-  }, chainBlocks['fantom'], 'fantom', factory, 3795376, true);
+  const balances = await calculateUniTvl(transform, chainBlocks['fantom'], 'fantom', factory, 3795376, true);
   return balances
 }
 
