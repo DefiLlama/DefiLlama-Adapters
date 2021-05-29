bd1b777fe899e3e041e5b45fbf2371e0a98bea9f
0xngmi <0xngmi@protonmail.com>
2021-05-29 16:10:39 +0200

    fix syntax

--- projects/aquarius/index.js
+++ projects/aquarius/index.js
@@ -4,7 +4,7 @@ const getEntireSystemCollAbi = require("./getEntireSystemColl.abi.json")
 const FTM_ADDRESS = '0x4e15361fd6b4bb609fa63c81a2be19d873717870';
 const TROVE_MANAGER_ADDRESS = "0xC87D230B3239d1A90463463d8adDFD70709D391b";
 
-async function tvl(_, _, chainBlocks) {
+async function tvl(_, _ethBlock, chainBlocks) {
   const balance = (
     await sdk.api.abi.call({
       target: TROVE_MANAGER_ADDRESS,
