f8a51d5e00b0cb4b3a7b3be85d6af35780b7013f
0xngmi <0xngmi@protonmail.com>
2021-06-01 00:41:08 +0200

    add o3swap

---
+++ projects/o3swap.js
@@ -0,0 +1,11 @@
+const {fetchURL} = require('./helper/utils')
+
+async function fetch() {
+    const tvl = await fetchURL('https://monitor.api.o3swap.com/v1/statistics')
+  return tvl.data.data.pool_tvl;
+}
+fetch().then(console.log)
+
+module.exports = {
+  fetch
+}
