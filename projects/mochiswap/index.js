b5ca766a56433849badcfdee54fb72762609fe50
frankydev1 <72719344@users.noreply.github.com>
2021-06-01 23:12:05 -0400

    Include MochiSwap Harmony Graph Data

--- projects/mochiswap/index.js
+++ projects/mochiswap/index.js
@@ -2,7 +2,11 @@ const { request, gql } = require("graphql-request");
 const sdk = require('@defillama/sdk');
 const { toUSDTBalances } = require('../helper/balances');
 
+// BSC Graph
 const graphUrl = 'https://api.thegraph.com/subgraphs/name/mochiswapdev/mochiswap3'
+// Harmony ONE Graph
+const graphUrlHarmony = 'https://api.mochiswap.io/subgraphs/name/mochiswap/mochiswap1'
+
 const graphQuery = gql`
 query get_tvl($block: Int) {
   uniswapFactory(
@@ -14,7 +18,19 @@ query get_tvl($block: Int) {
 }
 `;
 
+const graphQueryHarmony = gql`
+query get_tvl($block: Int) {
+  uniswapFactory(
+    id: "0x3bEF610a4A6736Fd00EBf9A73DA5535B413d82F6",
+    block: { number: $block }
+  ) {
+    totalLiquidityUSD
+  },
+}
+`;
+
 async function tvl(timestamp) {
+  // BSC lookup
   const {block} = await sdk.api.util.lookupBlock(timestamp,{
     chain: 'bsc'
   })
@@ -25,9 +41,21 @@ async function tvl(timestamp) {
       block,
     }
   );
+  // Harmony ONE lookup
+  const {block} = await sdk.api.util.lookupBlock(timestamp,{
+    chain: 'harmony'
+  })
+  const responseHarmony = await request(
+    graphUrlHarmony,
+    graphQueryHarmony,
+    {
+      block,
+    }
+  );
 
-  const usdTvl = Number(response.uniswapFactory.totalLiquidityUSD)
+  const usdTvl = Number(response.uniswapFactory.totalLiquidityUSD) + Number(responseHarmony.uniswapFactory.totalLiquidityUSD)
 
+  // Return Total MochiSwap Cross Chain Liquidity
   return toUSDTBalances(usdTvl)
 }
 
