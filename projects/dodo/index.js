85f3fca6f7e0ca8b62498b6abd592fbee28d9e14
0xngmi <0xngmi@protonmail.com>
2021-06-03 15:05:35 +0200

    update dodo

--- projects/dodo/index.js
+++ projects/dodo/index.js
@@ -1,12 +1,16 @@
 const { request, gql } = require("graphql-request");
 const sdk = require('@defillama/sdk');
 
-const graphEndpoint = "https://api.thegraph.com/subgraphs/name/dodoex/dodoex-v2"
+const graphEndpoints = {
+    'ethereum': "https://api.thegraph.com/subgraphs/name/dodoex/dodoex-v2",
+    "bsc": "https://pq.hg.network/subgraphs/name/dodoex-v2-bsc/bsc"
+}
 const graphQuery = gql`
-query get_pairs($block: Int) {
+query get_pairs($block: Int, $lastId: String) {
     pairs(
       first: 1000,
-      block: { number: $block }
+      block: { number: $block },
+      where: {id_gt: $lastId}
     ) {
         id
         baseReserve
@@ -14,46 +18,73 @@ query get_pairs($block: Int) {
         baseToken{
           id
           symbol
+          usdPrice
         }
         quoteToken{
           id
           symbol
+          usdPrice
         }
     }
 }
 `
 
-async function tvl(timestamp, block) {
-    const response = await request(
-        graphEndpoint,
-        graphQuery,
-        {
-            block,
-        }
-    );
+async function getChainTvl(chain, block, transformAddr) {
+    let allPairs = []
+    let lastId = ""
+    let response;
+    do {
+        response = await request(
+            graphEndpoints[chain],
+            graphQuery,
+            {
+                block,
+                lastId
+            }
+        );
+        allPairs = allPairs.concat(response.pairs)
+        lastId = response.pairs[response.pairs.length-1].id
+    } while (response.pairs.length >= 1000);
 
-    const balanceCalls = response.pairs.map(pair=>[{
-        target: pair.quoteToken.id,
-        params: [pair.id]
-    },{
-        target: pair.baseToken.id,
-        params: [pair.id]
-    }]).flat()
+    const balanceCalls = allPairs.map(pair => {
+        if (pair.id.includes('-')) {
+            return null
+        }
+        return [{
+            target: pair.quoteToken.id,
+            params: [pair.id]
+        }, {
+            target: pair.baseToken.id,
+            params: [pair.id]
+        }]
+    }).filter(pair => pair !== null).flat()
 
     const balanceResults = await sdk.api.abi.multiCall({
-        abi:'erc20:balanceOf',
+        abi: 'erc20:balanceOf',
         calls: balanceCalls,
-        block
+        block,
+        chain
     })
     const balances = {}
-    sdk.util.sumMultiBalanceOf(balances, balanceResults)
+    sdk.util.sumMultiBalanceOf(balances, balanceResults, transformAddr)
 
     return balances
 }
 
+function bsc(timestamp, ethBlock, chainBlocks){
+    return getChainTvl('bsc', chainBlocks['bsc'], addr=>`bsc:${addr}`)
+}
+
+function eth(timestamp, ethBlock, chainBlocks){
+    return getChainTvl('ethereum', ethBlock, addr=>addr)
+}
+
 module.exports = {
     ethereum: {
-        tvl,
+        tvl:eth,
     },
-    tvl
+    bsc:{
+        tvl: bsc
+    },
+    tvl: sdk.util.sumChainTvls([eth, bsc])
 }
