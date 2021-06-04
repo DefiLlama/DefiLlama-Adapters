bdadaf9e6d1a7aacbd378b3e61ae9fb884315b35
petrovska-petro <84875062+petrovska-petro@users.noreply.github.com>
2021-06-04 02:08:18 +0100

    MDEX tvl adapter (bsc chain data broken per token & heco chain using graphql endpoint

--- projects/mdex/index.js
+++ projects/mdex/index.js
@@ -1,10 +1,22 @@
-// starting block as reference aprox 1050000 in heco chain
 const { request, gql } = require("graphql-request");
 const sdk = require("@defillama/sdk");
 const BigNumber = require("bignumber.js");
 
+// --- Similar abi's required = pancake script, similar logic! ---
+const abi = require("./abi.json");
+const token0 = require("../helper/abis/token0.json");
+const token1 = require("../helper/abis/token1.json");
+const getReserves = require("../helper/abis/getReserves.json");
 
-const REDUCE_BLOCK = 10;
+// --> bsc addresses found here:    https://github.com/mdexSwap/bscswap
+// --> heco addresses found here:   https://github.com/mdexSwap/contracts
+
+const factories = {
+  heco: "0xb0b670fc1F7724119963018DB0BfA86aDb22d941",
+  bsc: "0x3CD1C46068dAEa5Ebb0d3f55F6915B10648062B8",
+};
+
+const REDUCE_BLOCK = 60;
 
 const graphUrls = {
   heco: "https://graph.mdex.com/subgraphs/name/mdex/swap",
@@ -43,6 +55,132 @@ function getMDEXLiquidity(block, chain) {
   }).then((data) => data);
 }
 
+async function processPairs(
+  balances,
+  pairNums,
+  chain,
+  block,
+  factory,
+  preAdressSignaling
+) {
+  const pairs = (
+    await sdk.api.abi.multiCall({
+      abi: abi.allPairs,
+      chain: `${preAdressSignaling}`,
+      calls: pairNums.map((num) => ({
+        target: factory,
+        params: [num],
+      })),
+    })
+  ).output;
+
+  // --- In case pairs returns empty array, does not make sense to move down (avoid errs output) ---
+  if (pairs.length == 0) return;
+
+  // --- In case that output is null will not make sense to move further (avoid errs output) ---
+  if (pairs[0].output == null) return;
+
+  const pairAddresses = pairs.map((result) => result.output.toLowerCase());
+  const [token0Addresses, token1Addresses, reserves] = await Promise.all([
+    sdk.api.abi
+      .multiCall({
+        abi: token0,
+        chain,
+        calls: pairAddresses.map((pairAddress) => ({
+          target: pairAddress,
+        })),
+        block,
+      })
+      .then(({ output }) => output),
+    sdk.api.abi
+      .multiCall({
+        abi: token1,
+        chain,
+        calls: pairAddresses.map((pairAddress) => ({
+          target: pairAddress,
+        })),
+        block,
+      })
+      .then(({ output }) => output),
+    sdk.api.abi
+      .multiCall({
+        abi: getReserves,
+        chain,
+        calls: pairAddresses.map((pairAddress) => ({
+          target: pairAddress,
+        })),
+        block,
+      })
+      .then(({ output }) => output),
+  ]);
+
+  for (let n = 0; n < pairNums.length; n++) {
+    // --- Added try/catch block for both outputs from reserve and check null vals before ---
+    try {
+      if (reserves[n] != null) {
+        sdk.util.sumSingleBalance(
+          balances,
+          `${preAdressSignaling}:${token1Addresses[n].output}`,
+          reserves[n].output[1]
+        );
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+    try {
+      if (reserves[n] != null) {
+        sdk.util.sumSingleBalance(
+          balances,
+          `${preAdressSignaling}:${token0Addresses[n].output}`,
+          reserves[n].output[0]
+        );
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
+  const balances = {};
+  const pairLength = Number(
+    (
+      await sdk.api.abi.call({
+        target: factories["bsc"],
+        abi: abi.allPairsLength,
+        chain: "bsc",
+        block: chainBlocks["bsc"],
+      })
+    ).output
+  );
+
+  // 0 to pairLength - 1
+  const allPairNums = Array.from(Array(pairLength).keys());
+  const reqs = [];
+
+  for (let i = 0; i < pairLength; i++) {
+    const pairNums = allPairNums.slice(i, i + 1);
+
+    const output = processPairs(
+      balances,
+      pairNums,
+      "bsc",
+      chainBlocks["bsc"],
+      factories["bsc"],
+      "bsc"
+    );
+
+    if (output != undefined) {
+      reqs.push(output);
+    }
+  }
+
+  await Promise.all(reqs);
+
+  return balances;
+};
+
 const hecoTvl = async (timestamp, ethBlock, chainBlocks) => {
   let block = chainBlocks["heco"];
 
@@ -62,29 +200,12 @@ const hecoTvl = async (timestamp, ethBlock, chainBlocks) => {
   };
 };
 
-const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
-  let block = chainBlocks["bsc"];
-
-  if (block === undefined) {
-    block = (await sdk.api.util.lookupBlock(timestamp, { chain: "bsc" })).block;
-  }
-
-  const results = await getMDEXLiquidity(block - REDUCE_BLOCK, "bsc");
-
-  return {
-    // --- Arrange to account the decimals as it was usdt (decimals = 6) ---
-    [usdtToken]: BigNumber(results.mdexFactory.totalLiquidityUSD)
-      .multipliedBy(10 ** 6)
-      .toFixed(0),
-  };
-};
-
 module.exports = {
-  heco: {
-    tvl: hecoTvl,
-  },
   bsc: {
-    tvl: bscTvl,
+    tvl: bscTvl, //   individually outputs >1B    ---   breakdown per token             (OK)
+  },
+  heco: {
+    tvl: hecoTvl, //  individually outputs >1B    ---   simply using graphql endpoint   (OK)
   },
   tvl: sdk.util.sumChainTvls([hecoTvl, bscTvl]),
 };
