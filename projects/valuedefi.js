ad3c642cbbc953f3c15fbbfe2b09496e7ae4b9b9
0xngmi <0xngmi@protonmail.com>
2021-06-04 23:36:43 +0200

    separation+

--- projects/valuedefi.js
+++ projects/valuedefi.js
@@ -1,15 +1,27 @@
 const utils = require('./helper/utils');
 
-/* * * * * * * *
-* ==> Correct adapter needs to be created.
-*
-*****************/
-
 async function fetch() {
   let response = await utils.fetchURL('https://api-stats.valuedefi.io/api/common-stat/get-total-locked')
   return response.data.data.total
 }
 
+async function eth() {
+  let response = await utils.fetchURL('https://api-stats.valuedefi.io/api/common-stat/get-total-locked')
+  return response.data.data.ethTotal
+}
+
+async function bsc() {
+  let response = await utils.fetchURL('https://api-stats.valuedefi.io/api/common-stat/get-total-locked')
+  return response.data.data.bscTotal
+}
+
+
 module.exports = {
+  ethereum:{
+    fetch: eth,
+  },
+  bsc: {
+    fetch: bsc
+  },
   fetch
 }
