8e7d3261f6f073d7a623e1334bf861cae175c0a5
0xngmi <0xngmi@protonmail.com>
2021-06-05 15:46:44 +0200

    format

--- projects/nsure/index.js
+++ projects/nsure/index.js
@@ -21,75 +21,75 @@ const constant = {
   },
 };
 
-async function purchase(block){
+async function purchase(block) {
   let weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
   let _purchasePool;
-    const [  surplus, treasury] = await Promise.all([
-      sdk.api.abi.call({
-        block,
-        target: weth,
-        abi: abi["balanceOf"],
-        params: constant.surplusPool.address,
-      }),
+  const [surplus, treasury] = await Promise.all([
+    sdk.api.abi.call({
+      block,
+      target: weth,
+      abi: abi["balanceOf"],
+      params: constant.surplusPool.address,
+    }),
 
-      sdk.api.abi.call({
-        block,
-        target: weth,
-        abi: abi["balanceOf"],
-        params: constant.treasuryPool.address,
-      }),
-    ]);
+    sdk.api.abi.call({
+      block,
+      target: weth,
+      abi: abi["balanceOf"],
+      params: constant.treasuryPool.address,
+    }),
+  ]);
 
-    _purchasePool =BigNumber(surplus.output).plus(BigNumber(treasury.output)).toFixed();
+  _purchasePool = BigNumber(surplus.output).plus(BigNumber(treasury.output)).toFixed();
 
-  return {[weth]:_purchasePool};
+  return { [weth]: _purchasePool };
 
 }
 
 async function underwriting(block) {
   let nsure = '0x20945ca1df56d237fd40036d47e866c7dccd2114';
   let balanceOf;
-    balanceOf = await sdk.api.abi.call({
-      block,
-      target: nsure,
-      abi: abi["balanceOf"],
-      params: constant.stakePool.address,
-    });
-    balanceOf = balanceOf.output
+  balanceOf = await sdk.api.abi.call({
+    block,
+    target: nsure,
+    abi: abi["balanceOf"],
+    params: constant.stakePool.address,
+  });
+  balanceOf = balanceOf.output
 
-  return {[nsure]: balanceOf}
+  return { [nsure]: balanceOf }
 }
 
 async function startEth(block) {
   let capitalAddr = '0xa6b658Ce4b1CDb4E7d8f97dFFB549B8688CAFb84'
   let ethAddr = '0x0000000000000000000000000000000000000000'
   let trueBalance = 0;
-    let _ethBalance = await sdk.api.eth.getBalance({target: capitalAddr, block: block});
+  let _ethBalance = await sdk.api.eth.getBalance({ target: capitalAddr, block: block });
 
-    let _totalSupply = await sdk.api.abi.call({
-      block,
-      target: capitalAddr,
-      abi: abi["totalSupply"],
-    });
-    let _poolBalance = await sdk.api.abi.call({
-      block,
-      target: capitalAddr,
-      abi: abi["balanceOf"],
-      params: constant.capitalStakePool.address
-    });
-    _totalSupply = BigNumber(_totalSupply.output);
-    _ethBalance = BigNumber(_ethBalance.output);
-    _poolBalance = BigNumber(_poolBalance.output);
-    trueBalance = _ethBalance.times(_poolBalance).div(_totalSupply).toFixed();
+  let _totalSupply = await sdk.api.abi.call({
+    block,
+    target: capitalAddr,
+    abi: abi["totalSupply"],
+  });
+  let _poolBalance = await sdk.api.abi.call({
+    block,
+    target: capitalAddr,
+    abi: abi["balanceOf"],
+    params: constant.capitalStakePool.address
+  });
+  _totalSupply = BigNumber(_totalSupply.output);
+  _ethBalance = BigNumber(_ethBalance.output);
+  _poolBalance = BigNumber(_poolBalance.output);
+  trueBalance = _ethBalance.times(_poolBalance).div(_totalSupply).toFixed();
 
 
-  return {[ethAddr]:trueBalance}
+  return { [ethAddr]: trueBalance }
 }
 
 async function tvl(timestamp, block) {
   let ethPool = await startEth(block);
   let underwritingPool = await underwriting(block);
-  let purchasePool  = await purchase(block);
+  let purchasePool = await purchase(block);
   let balances = {
     ...purchasePool,
     ...underwritingPool,
