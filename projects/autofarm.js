a9a976d3de956ea8778af060ba34bf14b0d59d5d
0xngmi <0xngmi@protonmail.com>
2021-06-06 05:12:35 +0200

    multichain autofarm

--- projects/autofarm.js
+++ projects/autofarm.js
@@ -1,19 +1,40 @@
 const utils = require('./helper/utils');
 
-/* * * * * * * *
-* ==> Correct adapter needs to be created.
-*
-*****************/
-
 const bscEndpoint = "https://static.autofarm.network/bsc/farm_data.json"
+const polygonEndpoint = "https://static.autofarm.network/polygon/stats.json"
+const hecoEndpoint = "https://api2.autofarm.network/heco/get_stats"
 
-async function fetch() {
-  var bscPools = await utils.fetchURL(bscEndpoint)
-  let tvl = Object.values(bscPools.data.pools).reduce((total, pool) => total + (pool.poolWantTVL || 0), 0)
-  //var hecototalTvl = await utils.fetchURL('https://api.autofarm.network/heco/get_stats')
-  return tvl// + hecototalTvl.data.platformTVL;
+async function polygon() {
+  const data = await utils.fetchURL(polygonEndpoint)
+  return data.data.platformTVL
 }
 
+async function heco() {
+  const data = await utils.fetchURL(hecoEndpoint)
+  return data.data.platformTVL
+}
+
+
+async function bsc() {
+  var bscPools = await utils.fetchURL(bscEndpoint)
+  let tvl = Object.values(bscPools.data.pools).reduce((total, pool) => total + (pool.poolWantTVL || 0), 0)
+  return tvl
+}
+
+async function fetch() {
+  return (await polygon())+(await heco())+(await bsc())
+}
+fetch().then(console.log)
+
 module.exports = {
+  bsc:{
+    fetch:bsc
+  },
+  heco:{
+    fetch: heco
+  },
+  polygon:{
+    fetch: polygon
+  },
   fetch
 }
