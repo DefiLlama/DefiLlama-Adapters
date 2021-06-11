6dc3f48d16c0eb3462bdd16ae57a7f68843d2a57
0xngmi <0xngmi@protonmail.com>
2021-06-11 21:14:30 +0200

    add v2+v1 keeperDAO

--- projects/keeper-dao/index.js
+++ projects/keeper-dao/index.js
@@ -5,11 +5,10 @@ const { GraphQLClient, gql } = require('graphql-request')
 const oldEndpoint = 'https://api.thegraph.com/subgraphs/name/keeperdao/keeperdao';
 const newEndpoint = 'https://api.thegraph.com/subgraphs/name/keeperdao/keeperdao-v2'
 
-async function liquidtyPoolTvl(endpoint, ethBlock){
-  const balances= {}
+async function liquidtyPoolTvl(balances, endpoint, ethBlock) {
   var graphQLClient = new GraphQLClient(endpoint)
 
-    var query = gql`
+  var query = gql`
     {
       liquidityPoolSupplies(block: { number: ${ethBlock}}) {
         id
@@ -18,20 +17,16 @@ async function liquidtyPoolTvl(endpoint, ethBlock){
     }
     `;
 
-    const results = await retry(async bail => await graphQLClient.request(query))
-    results.liquidityPoolSupplies.map(token => {
-      sdk.util.sumSingleBalance(balances, token.id === "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" ? "0x0000000000000000000000000000000000000000" : token.id, token.supply)
-    })
-  return balances
+  const results = await retry(async bail => await graphQLClient.request(query))
+  results.liquidityPoolSupplies.map(token => {
+    sdk.util.sumSingleBalance(balances, token.id === "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" ? "0x0000000000000000000000000000000000000000" : token.id, token.supply)
+  })
 }
 
 async function tvl(timestamp, ethBlock) {
-  let balances;
-  try{
-    balances = await liquidtyPoolTvl(newEndpoint, ethBlock)
-  } catch(e){
-    balances = await liquidtyPoolTvl(oldEndpoint, ethBlock)
-  }
+  const balances = {};
+  await liquidtyPoolTvl(balances, newEndpoint, ethBlock)
+  await liquidtyPoolTvl(balances, oldEndpoint, ethBlock)
 
   return balances
 }
