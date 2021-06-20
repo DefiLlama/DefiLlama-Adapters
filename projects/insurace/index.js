145df46d26b500ef0d555e826234508b44283b6f
Sum <sum.wei@insurace.io>
2021-06-20 23:15:20 +0800

    trick insur price in bsc to ethereum

--- projects/insurace/index.js
+++ projects/insurace/index.js
@@ -74,7 +74,9 @@ async function bsc(timestamp, ethBlock, chainBlocks){
     _.each(_tvlList, (element) => {
         let address = element.input.params[0].toLowerCase();
         if (address == "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") {
-            address = "bsc:0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+            address = "bsc:0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";
+        }else if (address == "0x3192ccddf1cdce4ff055ebc80f3f0231b86a7e30") {
+            address = "0x544c42fbb96b39b21df61cf322b5edc285ee7429";
         }else{
             address = `bsc:${address}`;
         }
@@ -84,7 +86,6 @@ async function bsc(timestamp, ethBlock, chainBlocks){
         }
         balances[address] = BigNumber(balances[address] || 0).plus(balance).toFixed();
     })
-    console.log(balances);
     return balances;
 }
 
