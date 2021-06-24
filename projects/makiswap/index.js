afdb55970c9c928178f4793a3e36e3a2ae6b6acd
Donnie Kim <mobaddicts@hotmail.com>
2021-06-23 21:37:47 -0400

    Add TVL for Makiswap in HECO network

---
+++ projects/makiswap/index.js
@@ -0,0 +1,41 @@
+const { request, gql } = require("graphql-request");
+const sdk = require("@defillama/sdk");
+const BigNumber = require("bignumber.js");
+
+const abi = require("./abi.json");
+
+// We use USDT for TVL for all farm pairs
+const usdtToken = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+
+const tvl = async (timestamp, ethBlock, chainBlocks) => {
+  let block = chainBlocks["heco"];
+
+  if (block === undefined) {
+    block = (await sdk.api.util.lookupBlock(timestamp, { chain: "heco" }))
+      .block;
+  }
+
+  const gqlEndpoint = 'https://q.hg.network/subgraphs/name/maki-exchanges/heco';
+  const gqlQuery = `
+    query tvl($block: Int){
+      uniswapFactories (
+        block:{ number: $block }
+      ) {
+        totalLiquidityUSD
+      }
+    }
+  `;
+  const result = await request(gqlEndpoint, gqlQuery, { block })
+    .then((data) => data.uniswapFactories[0].totalLiquidityUSD);
+
+  return {
+    // --- Arrange to account the decimals as it was usdt (decimals = 6) ---
+    [usdtToken]: BigNumber(result)
+      .multipliedBy(10 ** 6)
+      .toFixed(0),
+  };
+};
+
+module.exports = {
+  tvl
+};
