b3e85dbc74fe0ebe4735aeec453519d1e469f6d0
The Crow <cooperdombeck@gmail.com>
2021-07-05 19:51:17 -0500

    updated reaperfarm tvl fetch

--- projects/reaper.farm/index.js
+++ projects/reaper.farm/index.js
@@ -1,8 +1,10 @@
 const retry = require('async-retry')
 const axios = require("axios");
+const REAPER_API = "https://www.reaper.farm/api";
 
 async function fetch() {
-  let tvl = await retry(async bail => await axios.get());
+  const tvlMsg = await retry(async bail => await axios.get(REAPER_API+"/tvlTotal"));
+  const tvl = tvlMsg.data.data.tvlTotal;
   return tvl;
 }
 
