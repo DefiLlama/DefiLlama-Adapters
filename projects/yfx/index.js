dde8c285e497379c501c29bc21b734ae9d10b847
fooogs <86871242+fooogs@users.noreply.github.com>
2021-07-15 11:24:24 +0800

    update chain date
    update chain date

--- projects/yfx/index.js
+++ projects/yfx/index.js
@@ -2,7 +2,8 @@ const sdk = require("@defillama/sdk");
 const BigNumber = require("bignumber.js");
 const helper = require("../helper/utils.js");
 
-
+const usdtNum = 1000000;
+const usdt = '0xdac17f958d2ee523a2206206994597c13d831ec7';
 const url = 'https://info.yfx.com/Lineapi/fundDistribution?chain=EVERY&type=ALL&token=ALL';
 
 
@@ -25,28 +26,38 @@ async function getYFXLiquidity(block, chain) {
 
 
 const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
-  return await getYFXLiquidity(1, 'BSC');
+  let res = {};
+  res[usdt] = await getYFXLiquidity(1, 'BSC')*usdtNum;
+  return res;
 };
 
 const hecoTvl = async (timestamp, ethBlock, chainBlocks) => {
-  return await getYFXLiquidity(1, 'HECO');
+  let res = {};
+  res[usdt] = await getYFXLiquidity(1, 'HECO')*usdtNum;
+  return res;
 };
 
 const xdaiTvl = async (timestamp, ethBlock, chainBlocks) => {
-  return await getYFXLiquidity(1, 'XDAI');
+  let res = {};
+  res[usdt] = await getYFXLiquidity(1, 'XDAI')*usdtNum;
+  return res;
 }
 
 const okTvl = async (timestamp, ethBlock, chainBlocks) => {
-  return await getYFXLiquidity(1, 'OK');
+  let res = {};
+  res[usdt] = await getYFXLiquidity(1, 'OK')*usdtNum;
+  return res;
 }
 
 const totalTvl = async (timestamp, ethBlock, chainBlocks) => {
   let total = 0;
-  total += await bscTvl(1,1,1)*10**6;
-  total += await hecoTvl(1,1,1)*10**6;
-  total += await okTvl(1,1,1)*10**6;
-  total += await xdaiTvl(1,1,1)*10**6;
-  return {'0xdac17f958d2ee523a2206206994597c13d831ec7':total};
+  total +=  (await bscTvl(1,1,1))[usdt];
+  total +=  (await hecoTvl(1,1,1))[usdt];
+  total +=  (await okTvl(1,1,1))[usdt];
+  total +=  (await xdaiTvl(1,1,1))[usdt];
+  let res = {}
+  res[usdt] = total;
+  return res;
 }
 
 module.exports = {
@@ -62,6 +73,5 @@ module.exports = {
   ok: {
     tvl: okTvl,
   },
-//   tvl: sdk.util.sumChainTvls([hecoTvl, bscTvl, xdaiTvl, okTvl]),
   tvl: totalTvl,
 };
