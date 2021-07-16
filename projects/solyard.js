1a7504afb5be5a44500d4ec1b77ae552b1d95767
0xngmi <0xngmi@protonmail.com>
2021-07-16 06:31:40 +0100

    separate solyard tvls

--- projects/solyard.js
+++ projects/solyard.js
@@ -4,9 +4,27 @@ const axios = require("axios");
 async function fetch() {
   var response = await retry(async bail => await axios.get('https://solyard.finance/tvl'))
 
-  return response.total || {};
+  return response.data.total - response.data['YARDv1'] - response.data['YARDv1-USDC'];
+}
+
+async function staking() {
+  var response = await retry(async bail => await axios.get('https://solyard.finance/tvl'))
+
+  return response.data['YARDv1'];
+}
+
+async function pool2() {
+  var response = await retry(async bail => await axios.get('https://solyard.finance/tvl'))
+
+  return response.data['YARDv1-USDC'];
 }
 
 module.exports = {
+  staking:{
+    fetch:staking
+  },
+  pool2:{
+    fetch: pool2
+  },
   fetch
 }
