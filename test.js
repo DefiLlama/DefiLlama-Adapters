4c568ff2e80c0a5ae4f285a648668b35c121db1a
0xngmi <0xngmi@protonmail.com>
2021-07-16 05:53:28 +0100

    report tvl sections separately

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const path = require("path");
 require('dotenv').config();
-const {default: computeTVL} = require("@defillama/sdk/build/computeTVL");
+const { default: computeTVL } = require("@defillama/sdk/build/computeTVL");
 const { getCurrentBlocks } = require("@defillama/sdk/build//computeTVL/blocks");
 const { humanizeNumber } = require("@defillama/sdk/build//computeTVL/humanizeNumber");
 
@@ -14,21 +14,24 @@ if (process.argv.length < 3) {
 const passedFile = path.resolve(process.cwd(), process.argv[2]);
 
 (async () => {
-    const moduleToTest = require(passedFile);
+    const importedModule = require(passedFile);
     let usdTvl;
     const { timestamp, ethereumBlock, chainBlocks } = await getCurrentBlocks();
-    if (moduleToTest.fetch) {
-        usdTvl = await moduleToTest.fetch(timestamp);
-    } else if (moduleToTest.tvl) {
-        let tvl = await moduleToTest.tvl(timestamp, ethereumBlock, chainBlocks);
-        if (typeof tvl !== "object") {
-            throw new Error("TVL returned is not a balances object");
+    for(const tvlSection of Object.keys(importedModule)){
+        const moduleToTest = tvlSection === 'tvl' ? importedModule : importedModule[tvlSection]
+        console.log(`--- ${tvlSection} ---`)
+        if (moduleToTest.fetch) {
+            usdTvl = await moduleToTest.fetch(timestamp);
+        } else if (moduleToTest.tvl) {
+            let tvl = await moduleToTest.tvl(timestamp, ethereumBlock, chainBlocks);
+            if (typeof tvl !== "object") {
+                throw new Error("TVL returned is not a balances object");
+            }
+            usdTvl = (await computeTVL(tvl, "now", true)).usdTvl;
+        } else {
+            throw new Error("File must export either a property called tvl or one called fetch")
         }
-
-        usdTvl = (await computeTVL(tvl, "now", true)).usdTvl;
-    } else {
-        throw new Error("File must export either a property called tvl or one called fetch")
+        console.log("Total:", humanizeNumber(usdTvl));
     }
-    console.log("Total:", humanizeNumber(usdTvl));
     process.exit(0);
 })();
