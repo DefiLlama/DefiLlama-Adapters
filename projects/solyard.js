7762296fd5bb10e54c25eda2e253cded08b7736a
twenty <twenty@twenty>
2021-07-16 12:41:34 +0800

    Update Feature fetch data

--- projects/solyard.js
+++ projects/solyard.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 async function fetch() {
   var response = await retry(async bail => await axios.get('https://solyard.finance/tvl'))
 
-  return response.tvl || 0;
+  return response.total || {};
 }
 
 module.exports = {
