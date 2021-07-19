1371dac65434fe10069377a40aecce74d636506a
Hung Nguyen <hung.nguyen.thanh@sotatek.com>
2021-07-19 15:51:26 +0700

    fix feedback

--- projects/bunicorn/index.js
+++ projects/bunicorn/index.js
@@ -3,14 +3,15 @@ const {toUSDTBalances} = require('../helper/balances');
 const sdk = require("@defillama/sdk");
 const { request, gql } = require("graphql-request");
 
-const tokenSubgraphUrl = 'https://api.thegraph.com/subgraphs/name/bunicorndefi/buni-token';
+const tokenSubgraphUrl = 'https://graph.bunicorn.exchange/subgraphs/name/bunicorndefi/buni-token';
 const stableSubgraphUrl = 'https://api.thegraph.com/subgraphs/name/bunicorndefi/buni-stablecoins';
 
 
 const graphTotalTokenTVLQuery = gql`
-query GET_TOTAL_TOKEN_TVL {
+query GET_TOTAL_TOKEN_TVL($block: Int) {
   bunis(
-    where: { id: 1 }
+    where: { id: 1 },
+    block: { number: $block }
   ) {
     totalLiquidity
   }
@@ -18,20 +19,22 @@ query GET_TOTAL_TOKEN_TVL {
 `;
 
 const graphTotalStableTVLQuery = gql`
-query GET_TOTAL_STABLE_TVL {
-  buniCornFactories(where: {id: "0x86873f85bc12ce40321340392c0ff39c3bdb8d68"}) {
+query GET_TOTAL_STABLE_TVL($block: Int) {
+  buniCornFactories(where: {id: "0x86873f85bc12ce40321340392c0ff39c3bdb8d68"}, block: { number: $block }) {
     id
     totalLiquidityUSD
   }
 }
 `;
 
-async function getTotalTokenTVL() {
+async function getTotalTokenTVL(timestamp, ethBlock, chainBlocks) {
   try {
     const { bunis } = await request(
       tokenSubgraphUrl,
       graphTotalTokenTVLQuery,
-      {}
+      {
+        block: chainBlocks.bsc
+      }
     );
 
     return bunis[0] && bunis[0].totalLiquidity || 0;
@@ -40,12 +43,14 @@ async function getTotalTokenTVL() {
   }
 }
 
-async function getTotalStableTVL() {
+async function getTotalStableTVL(timestamp, ethBlock, chainBlocks) {
   try {
     const { buniCornFactories } = await request(
       stableSubgraphUrl,
       graphTotalStableTVLQuery,
-      {}
+      {
+        block: chainBlocks.bsc
+      }
     );
 
     return buniCornFactories[0] && buniCornFactories[0].totalLiquidityUSD || 0;
@@ -54,10 +59,10 @@ async function getTotalStableTVL() {
   }
 }
 
-async function getTotalTVL() {
+async function getTotalTVL(timestamp, ethBlock, chainBlocks) {
   const [tokensSummary, stableSummary] = await Promise.all([
-    getTotalTokenTVL(),
-    getTotalStableTVL()
+    getTotalTokenTVL(timestamp, ethBlock, chainBlocks),
+    getTotalStableTVL(timestamp, ethBlock, chainBlocks)
   ]);
 
   return toUSDTBalances(new BigNumber(tokensSummary).plus(stableSummary));
