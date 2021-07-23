6912b369741f69de79a535b2f3243e58d38198a2
Ibai-NexxyoLabs <ibai.castanon@nexxyolabs.com>
2021-07-23 14:07:02 +0200

    Added BambooDeFi TVL Tracker

---
+++ projects/bamboodefi/index.js
@@ -0,0 +1,27 @@
+const sdk = require("@defillama/sdk");
+const tvlOnPairs = require("../helper/processPairs.js");
+
+const factoryETH = "0x3823Ac41b77e51bf0E6536CE465479cBdedcaEa9";
+const factoryBSC = "0xFC2604a3BCB3BA6016003806A288E7aBF75c8Aa3";
+
+const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
+  const balances = {};
+  await tvlOnPairs("bsc", chainBlocks, factoryBSC, balances);
+  return balances;
+};
+
+const ethTvl = async (timestamp, ethBlock, chainBlocks) => {
+    const balances = {};
+    await tvlOnPairs("ethereum", chainBlocks, factoryETH, balances);
+    return balances;
+  };
+
+module.exports = {
+  bsc: {
+    tvl: bscTvl, //   individually outputs >1B    ---   breakdown per token             (OK)
+  },
+  ethereum: {
+    tvl: ethTvl, //   individually outputs >1B    ---   breakdown per token             (OK)
+  },
+  tvl: sdk.util.sumChainTvls([bscTvl, ethTvl]),
+};
