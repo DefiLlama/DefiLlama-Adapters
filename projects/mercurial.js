3abadb100ebb5a0dea80fed41b3e67d3d2e755b6
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-07-22 19:33:59 -0500

    added ust 3pool to mercurial

--- projects/mercurial.js
+++ projects/mercurial.js
@@ -1,14 +1,21 @@
 const {getTokenBalance} = require('./helper/solana')
 
-const holder = "5Q5vuzvECUYibHqDxNLH1Rk72d9NmpJwR5jivhxQ1Sks"
+
 
 async function fetch() {
-    const [usdcAmount, usdtAmount, paiAmount] = await Promise.all([
-        getTokenBalance("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", holder),
-        getTokenBalance("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", holder),
-        getTokenBalance("Ea5SjE2Y6yvCeW5dYTn7PYMuW5ikXkvbGdcmSnXeaLjS", holder),
+    const [usdcAmount, usdtAmount, paiAmount, ustPoolUsdc, ustPoolUsdt, ustPoolUst] = await Promise.all([
+        //pai3pool
+        getTokenBalance("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", "2dc3UgMuVkASzW4sABDjDB5PjFbPTncyECUnZL73bmQR" ),
+        getTokenBalance("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", "2dc3UgMuVkASzW4sABDjDB5PjFbPTncyECUnZL73bmQR" ),
+        getTokenBalance("Ea5SjE2Y6yvCeW5dYTn7PYMuW5ikXkvbGdcmSnXeaLjS", "2dc3UgMuVkASzW4sABDjDB5PjFbPTncyECUnZL73bmQR" ),
+        //ust3Pool
+        getTokenBalance("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", "FDonWCo5RJhx8rzSwtToUXiLEL7dAqLmUhnyH76F888D" ),
+        getTokenBalance("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", "FDonWCo5RJhx8rzSwtToUXiLEL7dAqLmUhnyH76F888D" ),
+        getTokenBalance("CXLBjMMcwkc17GfJtBos6rQCo1ypeH6eDbB82Kby4MRm", "FDonWCo5RJhx8rzSwtToUXiLEL7dAqLmUhnyH76F888D" ),
     ])
-    return usdcAmount + usdtAmount + paiAmount
+    return usdcAmount + usdtAmount + paiAmount + ustPoolUsdc + ustPoolUsdt + ustPoolUst
+    
+    
 }
 
 module.exports = {
