40dcad38ec42a28e82f5b09e04bdeabe84a2ed66
0xngmi <0xngmi@protonmail.com>
2021-07-27 03:23:55 +0100

    add block retries

--- test.js
+++ test.js
@@ -5,6 +5,15 @@ const { default: computeTVL } = require("@defillama/sdk/build/computeTVL");
 const { getCurrentBlocks } = require("@defillama/sdk/build//computeTVL/blocks");
 const { humanizeNumber } = require("@defillama/sdk/build//computeTVL/humanizeNumber");
 
+async function getBlocks(){
+    for(let i=0; i<5; i++){
+        try{
+            return await getCurrentBlocks();
+        } catch(e){
+            throw new Error("Couln't get block heights", e)
+        }
+    }
+}
 
 if (process.argv.length < 3) {
     console.error(`Missing argument, you need to provide the filename of the adapter to test.
@@ -16,7 +25,7 @@ const passedFile = path.resolve(process.cwd(), process.argv[2]);
 (async () => {
     const importedModule = require(passedFile);
     let usdTvl;
-    const { timestamp, ethereumBlock, chainBlocks } = await getCurrentBlocks();
+    const { timestamp, ethereumBlock, chainBlocks } = await getBlocks();
     for(const tvlSection of Object.keys(importedModule)){
         if(tvlSection === "methodology"){
             return
