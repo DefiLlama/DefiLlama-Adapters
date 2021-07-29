d14097bb040de82910e533422c495e6d758e14e5
Hung Nguyen <hung.nguyen.thanh@sotatek.com>
2021-07-29 08:12:37 +0700

    add tvl farm

--- projects/bunicorn/index.js
+++ projects/bunicorn/index.js
@@ -2,10 +2,12 @@ const BigNumber = require("bignumber.js");
 const {toUSDTBalances} = require('../helper/balances');
 const sdk = require("@defillama/sdk");
 const { request, gql } = require("graphql-request");
+const abi = require("./abi.json");
 
 const tokenSubgraphUrl = 'https://graph.bunicorn.exchange/subgraphs/name/bunicorndefi/buni-token';
 const stableSubgraphUrl = 'https://api.thegraph.com/subgraphs/name/bunicorndefi/buni-stablecoins';
-
+const BUNI_CONTRACT_ADDRESS = '0x0e7beec376099429b85639eb3abe7cf22694ed49';
+const MASTERCHET_CONTRACT_ADDRESS = '0xA12c974fE40ea825E66615bA0Dc4Fd19be4D7d24';
 
 const graphTotalTokenTVLQuery = gql`
 query GET_TOTAL_TOKEN_TVL($block: Int) {
@@ -26,6 +28,58 @@ query GET_TOTAL_STABLE_TVL($block: Int) {
   }
 }
 `;
+
+const graphBuniPriceQuery = gql`
+query GET_BUNI_PRICE($block: Int) {
+  tokenPrices(where: {id: "0x0e7beec376099429b85639eb3abe7cf22694ed49"}) {
+    id
+    symbol
+    name
+    decimals
+    price
+  }
+}
+`;
+
+async function getBuniPrice(chainBlocks) {
+  const { tokenPrices } = await request(
+    tokenSubgraphUrl,
+    graphBuniPriceQuery,
+    {
+      block: chainBlocks.bsc
+    }
+  );
+  if (!tokenPrices || !tokenPrices[0]) {
+    throw new Error('Empty price');
+  }
+
+  return parseFloat(tokenPrices[0].price);
+}
+
+
+async function getTotalFarmTVL(timestamp, ethBlock, chainBlocks) {
+  try {
+    const block = chainBlocks.bsc
+    const result = await sdk.api.abi.multiCall({
+      calls: [
+        {
+          target: BUNI_CONTRACT_ADDRESS,
+          params: [MASTERCHET_CONTRACT_ADDRESS]
+        }
+      ],
+      block,
+      abi: abi.balanceOf,
+      chain: 'bsc'
+    })
+    const balance = result.output[0] && result.output[0].output;
+    const buniPrice = await getBuniPrice(chainBlocks);
+
+    return new BigNumber(balance).div(1e18).times(buniPrice);
+  } catch (e) {
+    throw new Error('getTotalFarmTVL has exception:' + e.message);
+  }
+}
+
 
 async function getTotalTokenTVL(timestamp, ethBlock, chainBlocks) {
   try {
@@ -60,18 +114,19 @@ async function getTotalStableTVL(timestamp, ethBlock, chainBlocks) {
 }
 
 async function getTotalTVL(timestamp, ethBlock, chainBlocks) {
-  const [tokensSummary, stableSummary] = await Promise.all([
+  const [tokensSummary, stableSummary, farmSummary] = await Promise.all([
     getTotalTokenTVL(timestamp, ethBlock, chainBlocks),
-    getTotalStableTVL(timestamp, ethBlock, chainBlocks)
+    getTotalStableTVL(timestamp, ethBlock, chainBlocks),
+    getTotalFarmTVL(timestamp, ethBlock, chainBlocks)
   ]);
 
-  return toUSDTBalances(new BigNumber(tokensSummary).plus(stableSummary));
+  return toUSDTBalances(new BigNumber(tokensSummary).plus(stableSummary).plus(farmSummary));
 }
 
 module.exports = {
   bsc:{
     tvl: getTotalTVL
   },
-  tvl: sdk.util.sumChainTvls([getTotalTVL])
+  tvl: getTotalTVL
 }
 
