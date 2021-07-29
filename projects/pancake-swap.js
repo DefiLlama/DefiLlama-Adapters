56d8812f240a8cd85d954c9801fd30a805495458
0xngmi <0xngmi@protonmail.com>
2021-07-29 01:22:20 +0100

    Fix pancakeswap

--- projects/pancake-swap.js
+++ projects/pancake-swap.js
@@ -52,6 +52,7 @@ const factory = '0xBCfCcbde45cE874adCB698cC183deBcF17952812'
 const cakeToken = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82'
 const masterChef = '0x73feaa1eE314F8c655E354234017bE2193C9E24E'
 async function staking(timestamp, ethBlock, chainBlocks) {
+  const balances = {}
   const stakedCake = sdk.api.erc20.balanceOf({
     target: cakeToken,
     owner: masterChef,
