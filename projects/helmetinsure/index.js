22399035827bc5b071531739c6df1c8a2b3b6f03
0xngmi <0xngmi@protonmail.com>
2021-08-01 01:50:27 +0100

    Enforce block heights on helmet subgraph

--- projects/helmetinsure/index.js
+++ projects/helmetinsure/index.js
@@ -1,5 +1,6 @@
 const retry = require('../helper/retry')
 const { GraphQLClient, gql } = require('graphql-request')
+const sdk = require('@defillama/sdk')
 const BigNumber = require("bignumber.js");
 const CHAIN_POLYGON = 'polygon'
 const CHAIN_BSC = 'bsc'
@@ -13,8 +14,8 @@ const THEGARPH_API = {
 }
 
 
-function transform(str){
-  switch (str){
+function transform(str) {
+  switch (str) {
     case "bsc:0xaf90e457f4359adcc8b37e8df8a27a1ff4c3f561": // SHIB
       return "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE"
     case "bsc:0xf218184af829cf2b0019f8e6f0b2423498a36983": // MATH
@@ -26,12 +27,17 @@ function transform(str){
   }
 }
 
-async function fetch(chain) {
-  var endpoint = THEGARPH_API[chain]
-  var graphQLClient = new GraphQLClient(endpoint)
-  var query = gql`
-    {
-      options(first: 1000) {
+function fetch(chain) {
+  return async (_timestamp, _ethBlock, chainBlocks) => {
+
+    var endpoint = THEGARPH_API[chain]
+    var graphQLClient = new GraphQLClient(endpoint)
+    var query = gql`
+    query tvl($block: Int){
+      options(
+        first: 1000,
+        block: { number: $block }
+      ) {
         collateral
         asks {
           volume
@@ -40,36 +46,36 @@ async function fetch(chain) {
       }
     }
     `;
-  const results = await retry(async bail => await graphQLClient.request(query))
-  const {options} = results
+    const results = await retry(async bail => await graphQLClient.request(query, {
+      block: chainBlocks[chain]
+    }))
+    const { options } = results
 
-  const data = options.flatMap(o => {
-    return o.asks.map(ask => {
-      return Object.assign(ask, {
-        collateral: o.collateral
+    const data = options.flatMap(o => {
+      return o.asks.map(ask => {
+        return Object.assign(ask, {
+          collateral: o.collateral
+        })
       })
-    })
-  }).reduce((_data, ask) => {
-    const key = transform(`${chain}:${ask.collateral}`)
-    if(typeof _data[key] === 'undefined'){
-      _data[key] = ask.volume
-    }else {
-      _data[key] = new BigNumber(_data[key]).plus(new BigNumber(ask.volume)).toString()
-    }
-    return _data
-  }, {})
-  return data
+    }).reduce((_data, ask) => {
+      const key = transform(`${chain}:${ask.collateral}`)
+      if (typeof _data[key] === 'undefined') {
+        _data[key] = ask.volume
+      } else {
+        _data[key] = new BigNumber(_data[key]).plus(new BigNumber(ask.volume)).toFixed()
+      }
+      return _data
+    }, {})
+    return data
+  }
 }
-
-async function tvl(timestamp, block) {
-  const bsc = await fetch(CHAIN_BSC)
-  const polygon = await fetch(CHAIN_POLYGON)
-  const balances = Object.assign({}, bsc, polygon)
-  console.log(balances)
-  return balances;
-}
-
 
 module.exports = {
-  tvl
+  [CHAIN_POLYGON]:{
+    tvl: fetch(CHAIN_POLYGON)
+  },
+  [CHAIN_BSC]:{
+    tvl: fetch(CHAIN_BSC)
+  },
+  tvl: sdk.util.sumChainTvls([CHAIN_POLYGON, CHAIN_BSC].map(fetch))
 }
