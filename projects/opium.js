b05dd9ee608d92000784cbf382e6a7a1fcba4135
0xngmi <0xngmi@protonmail.com>
2021-08-04 19:50:40 +0100

    fail hard on opium

--- projects/opium.js
+++ projects/opium.js
@@ -47,12 +47,10 @@ const tvl = (url, chain) => async (timestamp, blockETH, chainBlocks) => {
 
   // Sum all balances
   _.each(balanceOfResults.output, (balanceOf) => {
-    if(balanceOf.success) {
       const address = balanceOf.input.target;
       const balance = balances[address] ? BigNumber(balanceOf.output).plus(BigNumber(balances[address])).toFixed().toString(): balanceOf.output;
 
       balances[address] = balance;
-    }
   });
 
   // Iterate over final balances
@@ -88,7 +86,7 @@ const tvl = (url, chain) => async (timestamp, blockETH, chainBlocks) => {
   await unwrapUniswapLPs(
     balances,
     lpPositions,
-    chain === CHAINS.ETHEREUM ? blockETH : chainBlocks[chain],
+    chainBlocks[chain],
     chain
   )
 
