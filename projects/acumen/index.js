7c6013c6bbafdb8faa55b20a8e7b245f32d69768
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-08-06 17:48:46 -0500

    added tulip and sbr to Acumen

--- projects/acumen/index.js
+++ projects/acumen/index.js
@@ -1,14 +1,25 @@
 const {getTokenBalance} = require('../helper/solana')
 
 async function tvl() {
-    const [usdcAmount, btcAmount, srmAmount, ethAmount, rayAmount, stepAmount, solAmount] = await Promise.all([
+    const [usdcAmount, btcAmount, srmAmount, ethAmount, rayAmount, stepAmount, solAmount, tulipAmount, sbrAmount] = await Promise.all([
+        //usdc
         getTokenBalance("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", "5Jo61FWkhG2jsaHfENdp7zz2G1SU6z5a2enpQWBEj83T"),
+        //btc
         getTokenBalance("9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E", "5Jo61FWkhG2jsaHfENdp7zz2G1SU6z5a2enpQWBEj83T"),
+        //srm
         getTokenBalance("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt", "5Jo61FWkhG2jsaHfENdp7zz2G1SU6z5a2enpQWBEj83T"),
+        //eth
         getTokenBalance("2FPyTwcZLUg1MDrwsyoP4D6s1tM7hAkHYRjkNb5w6Pxk", "5Jo61FWkhG2jsaHfENdp7zz2G1SU6z5a2enpQWBEj83T"),
+        //ray
         getTokenBalance("4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R", "5Jo61FWkhG2jsaHfENdp7zz2G1SU6z5a2enpQWBEj83T"),
+        //step
         getTokenBalance("StepAscQoEioFxxWGnh2sLBDFp9d8rvKz2Yp39iDpyT", "5Jo61FWkhG2jsaHfENdp7zz2G1SU6z5a2enpQWBEj83T"),
+        //sol
         getTokenBalance("So11111111111111111111111111111111111111112", "5Jo61FWkhG2jsaHfENdp7zz2G1SU6z5a2enpQWBEj83T"),
+        //tulip
+        getTokenBalance("TuLipcqtGVXP9XR62wM8WWCm6a9vhLs7T1uoWBk6FDs", "5Jo61FWkhG2jsaHfENdp7zz2G1SU6z5a2enpQWBEj83T"),
+        //sbr   
+        getTokenBalance("Saber2gLauYim4Mvftnrasomsv6NvAuncvMEZwcLpD1", "5Jo61FWkhG2jsaHfENdp7zz2G1SU6z5a2enpQWBEj83T"),
 
     ])
 
@@ -20,6 +31,8 @@ async function tvl() {
         'step-finance':stepAmount,
         'raydium': rayAmount,
         'usd-coin': usdcAmount,
+        'saber': sbrAmount,
+        'solfarm': tulipAmount,
     }
 }
 
