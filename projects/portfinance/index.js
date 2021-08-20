962a2bf66c905f8376713fcb6bf3758ef38a4c1a
Wang Ge <gw980531@gmail.com>
2021-08-20 17:02:55 +0800

    Changed to owner.

--- projects/portfinance/index.js
+++ projects/portfinance/index.js
@@ -2,13 +2,13 @@ const {getTokenBalance} = require('../helper/solana')
 
 async function tvl() {
     const [usdcAmount, usdtAmount, solAmount, paiAmount, merAmount, btcAmount, srmAmount] = await Promise.all([
-        getTokenBalance("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", "2xPnqU4bWhUSjZ74CibY63NrtkHHw5eKntsxf8dzwiid"),
-        getTokenBalance("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", "QyvfrbqH7Mo8W5tHN31nzbfNiwFwqPqahjm9fnzo5EJ"),
-        getTokenBalance("So11111111111111111111111111111111111111112", "BLAFX12cDmsumyB6k3L6whJZqNqySaWeCmS5rVuzy3SS"),
-        getTokenBalance("Ea5SjE2Y6yvCeW5dYTn7PYMuW5ikXkvbGdcmSnXeaLjS", "42kNZrAuwZHLtuc7jvVX7zMfkfgwbPynqzFB3zdkAEGM"),
-        getTokenBalance("MERt85fc5boKw3BW1eYdxonEuJNvXbiMbs6hvheau5K", "6UmrawFZgdPvMe6BLZdZCNRFz9u2TWsu5enFbTufA3a1"),
-        getTokenBalance("9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E", "FZKP27Zxz9GbW86hhq3d1egzpBH5ZnYkyjQZVf86NQJ8"),
-        getTokenBalance("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt", "DjhMNdgdbxNud1gmc4DUwrQqJxNbjhxiwNnhc4usSXmQ"),
+        getTokenBalance("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", "8x2uay8UgrLiX8AAYyF6AkK9z91nNtN6aLwfqPkf6TAQ"),
+        getTokenBalance("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", "8x2uay8UgrLiX8AAYyF6AkK9z91nNtN6aLwfqPkf6TAQ"),
+        getTokenBalance("So11111111111111111111111111111111111111112", "8x2uay8UgrLiX8AAYyF6AkK9z91nNtN6aLwfqPkf6TAQ"),
+        getTokenBalance("Ea5SjE2Y6yvCeW5dYTn7PYMuW5ikXkvbGdcmSnXeaLjS", "8x2uay8UgrLiX8AAYyF6AkK9z91nNtN6aLwfqPkf6TAQ"),
+        getTokenBalance("MERt85fc5boKw3BW1eYdxonEuJNvXbiMbs6hvheau5K", "8x2uay8UgrLiX8AAYyF6AkK9z91nNtN6aLwfqPkf6TAQ"),
+        getTokenBalance("9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E", "8x2uay8UgrLiX8AAYyF6AkK9z91nNtN6aLwfqPkf6TAQ"),
+        getTokenBalance("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt", "8x2uay8UgrLiX8AAYyF6AkK9z91nNtN6aLwfqPkf6TAQ"),
     ])
     return {
         'usd-coin': usdcAmount,
