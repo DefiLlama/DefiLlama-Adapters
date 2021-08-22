66b493865c73400b7a0b7f0136c01413f0c7e937
0xngmi <0xngmi@protonmail.com>
2021-08-22 20:39:23 +0100

    fix fetch adapters web3

--- projects/config/web3.js
+++ projects/config/web3.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3');
-const env = require("dotenv").config();
-let web3RpcUrl = process.env.ETHEREUM_RPC;
+require("dotenv").config();
+let web3RpcUrl = process.env.ETHEREUM_RPC.split(',')[0];
 const web3 = new Web3(new Web3.providers.HttpProvider(web3RpcUrl));
 module.exports = web3;
