d2b3991a95d9e4534713a01c8d12ac540c328bf0
pedrobergamini <pedrohgbergamini@gmail.com>
2021-08-23 14:03:37 -0300

    Adds ILV and ILV/ETH core pools to tvl

--- projects/illuvium/index.js
+++ projects/illuvium/index.js
@@ -8,6 +8,8 @@ const snxstk = "0x9898d72c2901D09E72A426d1c24b6ab90eB100e7"
 const snx = "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f"
 const axsstk = "0x099A3B242dceC87e729cEfc6157632d7D5F1c4ef"
 const axs = "0xbb0e17ef65f82ab018d8edd776e8dd940327b28b"
+const xyz = "0x618679df9efcd19694bb1daa8d00718eacfa2883"
+const xyzstk = "0x4C6997D462b0146fA54b747065411C1Ba0248595"
 
 async function getTvl(tokens, block){
   const balances = {}
@@ -30,14 +32,17 @@ async function pool2(timestamp, block) {
 
 async function tvl(timestamp, block) {
   return getTvl([
+    [ilv, ilvstk],
+    [slp, slpstk, true],
     [snx, snxstk],
-    [axs, axsstk]
+    [axs, axsstk],
+    [xyz, xyzstk],
   ], block)
 }
 
 module.exports = {
-  methodology: `Counts the tokens staked on the flash pools, currently SNX on 0x9898d72c2901D09E72A426d1c24b6ab90eB100e7 and AXS on 0x099A3B242dceC87e729cEfc6157632d7D5F1c4ef
-  pool2 are the SLP tokens locked on 0x8B4d8443a0229349A9892D4F7CbE89eF5f843F72 and staking are the ILV tokens on 0x25121EDDf746c884ddE4619b573A7B10714E2a36`,
+  methodology: `Counts the tokens staked on the core pools (ILV and ILV/ETH SLP) and flash pools, currently SNX on 0x9898d72c2901D09E72A426d1c24b6ab90eB100e7, AXS on 0x099A3B242dceC87e729cEfc6157632d7D5F1c4ef and XYZ on 
+  0x4C6997D462b0146fA54b747065411C1Ba0248595 pool2 are the SLP tokens locked on 0x8B4d8443a0229349A9892D4F7CbE89eF5f843F72 and staking are the ILV tokens on 0x25121EDDf746c884ddE4619b573A7B10714E2a36`,
   staking:{
     tvl: staking
   },
