ee1484f8d12556426a8a51f86c05bba990d97cf5
0xngmi <0xngmi@protonmail.com>
2021-08-24 23:35:07 +0100

    add bsc shibance

--- projects/shibance/index.js
+++ projects/shibance/index.js
@@ -1,4 +1,5 @@
 const { getChainTvl } = require('../helper/getUniSubgraphTvl');
+const {calculateUniTvl} = require('../helper/calculateUniTvl.js')
 const sdk = require('@defillama/sdk')
 
 const graphUrls = {
@@ -7,16 +8,18 @@ const graphUrls = {
 }
 const chainTvl = getChainTvl(graphUrls)
 
+async function bsc(_timestamp, _ethBlock, chainBlocks){
+  return calculateUniTvl(addr=>`bsc:${addr}`, chainBlocks['bsc'], 'bsc', '0x092EE062Baa0440B6df6BBb7Db7e66D8902bFdF7', 0, true);
+}
+
 module.exports = {
   misrepresentedTokens: true,
   methodology: "We count liquidity on the dexes, pulling data from subgraphs",
   kcc: {
     tvl: chainTvl('kcc'),
   },
-  /* outdated
-  bsc: {
-    tvl: chainTvl('bsc'),
+  bsc:{
+    tvl: bsc
   },
-  */
-  tvl: sdk.util.sumChainTvls(['kcc'].map(chainTvl))
+  tvl: sdk.util.sumChainTvls([chainTvl('kcc'), bsc])
 }
