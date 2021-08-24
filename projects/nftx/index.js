62e713154272a7ce449ad0753211321550306139
0xngmi <0xngmi@protonmail.com>
2021-08-25 00:06:12 +0100

    add nftx

---
+++ projects/nftx/index.js
@@ -0,0 +1,73 @@
+const { request, gql } = require("graphql-request");
+const sdk = require('@defillama/sdk')
+
+async function tvl(_timestamp, block){
+  const vaults = await request(graph, query)
+  const supplies = await sdk.api.abi.multiCall({
+    abi: 'erc20:totalSupply',
+    calls: vaults.vaults.map(v=>({
+      target: v.token.id
+    }))
+  })
+  const balances = {}
+  sdk.util.sumMultiBalanceOf(balances, supplies)
+  return balances
+}
+
+module.exports = {
+  methodology: "Counts total value of all vaults",
+  tvl
+}
+
+const graph = "https://api.thegraph.com/subgraphs/name/nftx-project/nftx-v2"
+const query = gql`
+{
+    vaults(first: 1000, where: { vaultId_gte: 0  }) {
+      vaultId
+      id
+      is1155
+      isFinalized
+      totalHoldings
+      totalMints
+      totalRedeems
+      holdings(first: 1000) {
+        id
+        tokenId
+        amount
+      }
+      token {
+        id
+        name
+        symbol
+      }
+      fees {
+        mintFee
+        randomRedeemFee
+        targetRedeemFee
+        swapFee
+      }
+      asset {
+        id
+        name
+        symbol
+      }
+      manager {
+        id
+      }
+      createdBy {
+        id
+      }
+      eligibilityModule {
+        id
+        eligibleIds
+        eligibleRange
+      }
+      features {
+        enableMint
+        enableRandomRedeem
+        enableTargetRedeem
+        enableSwap
+      }
+    }
+  }
+`
