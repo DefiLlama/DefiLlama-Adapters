e4147e522c41f3af2c92f47d16ee2fd192a050cd
0xngmi <0xngmi@protonmail.com>
2021-08-25 15:42:29 +0100

    Fix curve for really old blocks

--- projects/curve-old/index.js
+++ projects/curve-old/index.js
@@ -1,215 +1,187 @@
-const sdk = require("@defillama/sdk");
-const _ = require("underscore");
-_.flatMap = _.compose(_.flatten, _.map);
-const BN = require('bignumber.js');
 
-const abi = require("./abi.json");
-const { object } = require("underscore");
+  const sdk = require('@defillama/sdk');
+  const _ = require('underscore');
+  _.flatMap = _.compose(_.flatten, _.map);
 
-const CurveAddressProvider = "0x0000000022d53366457f9d5e68ec105046fc4383";
-const etherAddress = "0x0000000000000000000000000000000000000000";
-const curveEtherAddress = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
-const CurveRegistryStart = 11218787;
+/*==================================================
+  TVL
+  ==================================================*/
 
-const factoryAddress = "0x0959158b6040D32d04c301A72CBFD6b39E21c9AE";
-const factoryStartBlock = 11942404;
-
-const triCrypto = "0x80466c64868E1ab14a1Ddf27A676C3fcBE638Fe5";
-
-async function tvl(timestamp, block) {
-  let curvePools = [
-    "0xeDf54bC005bc2Df0Cc6A675596e843D28b16A966",
-    "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7",
-    "0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27",
-    "0xA2B47E3D5c44877cca798226B7B8118F9BFb7A56",
-    "0x4f062658EaAF2C1ccf8C8e36D6824CDf41167956",
-    "0x4CA9b3063Ec5866A4B82E437059D2C43d1be596F",
-    "0x3eF6A01A0f81D6046290f3e2A8c5b843e738E604",
-    "0xE7a24EF0C5e95Ffb0f6684b813A78F2a3AD7D171",
-    "0x8474DdbE98F5aA3179B3B3F5942D724aFcdec9f6",
-    "0x06364f10B501e868329afBc005b3492902d6C763",
-    "0x93054188d876f558f4a66B2EF1d97d16eDf0895B",
-    "0xC18cC39da8b11dA8c3541C598eE022258F9744da",
-    "0x7fC77b5c7614E1533320Ea6DDc2Eb61fa00A9714",
-    "0xA5407eAE9Ba41422680e2e00537571bcC53efBfD",
-    "0xC25099792E9349C7DD09759744ea681C7de2cb66",
-    "0x3E01dD8a5E1fb3481F0F589056b428Fc308AF0Fb",
-    "0x0f9cb53Ebe405d49A0bbdBD291A65Ff571bC83e1",
-    "0x52EA46506B9CC5Ef470C5bf89f17Dc28bB35D85C",
-    "0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51",
-    "0x8038C01A0390a8c547446a0b2c18fc9aEFEcc10c",
-    "0x071c661B4DeefB59E2a3DdB20Db036821eeE8F4b",
-    "0xd81dA8D904b52208541Bade1bD6595D8a251F8dd",
-    "0x7F55DDe206dbAD629C080068923b36fe9D6bDBeF",
-    "0x890f4e345B1dAED0367A877a1612f86A1f86985f",
-    "0x0Ce6a5fF5217e38315f87032CF90686C96627CAA",
-    "0xc5424B857f758E906013F3555Dad202e4bdB4567",
-    "0xDeBF20617708857ebe4F679508E7b7863a8A8EeE",
-  ];
-
-  let registryStart = block > CurveRegistryStart;
-  let factoryStart = block > factoryStartBlock;
-
-  let balances = {};
-  balances[curveEtherAddress] = 0;
-  balances["0x6B175474E89094C44Da98b954EedeAC495271d0F"] = 0;
-  balances["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"] = 0;
-  let poolInfo = {};
-  let CurveRegistryAddress;
-
-  if (registryStart) {
-    CurveRegistryAddress = (await sdk.api.abi.call({
-      block,
-      target: CurveAddressProvider,
-      abi: abi["get_registry"]
-    })).output
-  }
-
-  let poolCount = registryStart
-    ? (
-        await sdk.api.abi.call({
-          block,
-          target: CurveRegistryAddress,
-          abi: abi["pool_count"],
-        })
-      ).output
-    : curvePools.length;
-
-  for (let i = 0; i < poolCount; i++) {
-    let poolAddress = registryStart ? (
-      await sdk.api.abi.call({
-        block,
-        target: CurveRegistryAddress,
-        abi: abi["pool_list"],
-        params: i,
-      })
-    ).output : curvePools[i];
-    poolInfo[poolAddress] = true;
-
-    let coins = (await sdk.api.abi.call({
-      block,
-      target: CurveRegistryAddress,
-      abi: abi["get_coins"],
-      params: poolAddress
-    })).output;
-
-    let coinBalances = (await sdk.api.abi.call({
-      block,
-      target: CurveRegistryAddress,
-      abi: abi["get_balances"],
-      params: poolAddress
-    })).output
-
-    for (let i = 0; i < coins.length; i++) {
-      let coin = coins[i];
-      let balance = coinBalances[i];
-      if (coin !== "0x0000000000000000000000000000000000000000") {
-        balances[coin] = balances[coin] ? new BN(balances[coin]).plus(balance) : balance
-      }
+  async function tvl(timestamp, block) {
+    if(block > 11218787){
+      throw new Error("This only works for old blocks")
     }
-  }
+    let balances = {};
 
-  if (factoryStart) {
-    poolCount = (
-        await sdk.api.abi.call({
-          block,
-          target: factoryAddress,
-          abi: abi["pool_count"],
-        })
-      ).output
+    let swaps = [
+      '0xe5FdBab9Ad428bBB469Dee4CB6608C0a8895CbA5',
+      '0x2e60CF74d81ac34eB21eEff58Db4D385920ef419',
+      '0xA2B47E3D5c44877cca798226B7B8118F9BFb7A56',
+      '0x52EA46506B9CC5Ef470C5bf89f17Dc28bB35D85C',
+      '0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51',
+      '0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27',
+      '0xeDf54bC005bc2Df0Cc6A675596e843D28b16A966',
+      '0xA5407eAE9Ba41422680e2e00537571bcC53efBfD',
+      '0x06364f10B501e868329afBc005b3492902d6C763',
+      '0x93054188d876f558f4a66B2EF1d97d16eDf0895B',
+      '0x7fC77b5c7614E1533320Ea6DDc2Eb61fa00A9714',
+      '0x4CA9b3063Ec5866A4B82E437059D2C43d1be596F',
+      '0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7',
+      '0x4f062658EaAF2C1ccf8C8e36D6824CDf41167956',
+      '0x3eF6A01A0f81D6046290f3e2A8c5b843e738E604',
+      '0x8474DdbE98F5aA3179B3B3F5942D724aFcdec9f6',
+      '0xC18cC39da8b11dA8c3541C598eE022258F9744da',
+      '0xC25099792E9349C7DD09759744ea681C7de2cb66',
+      '0x3E01dD8a5E1fb3481F0F589056b428Fc308AF0Fb',
+      '0x0f9cb53Ebe405d49A0bbdBD291A65Ff571bC83e1',
+      '0x8038C01A0390a8c547446a0b2c18fc9aEFEcc10c'
+    ]
 
-  for (let i = 0; i < poolCount; i++) {
-    let poolAddress = (
-      await sdk.api.abi.call({
-        block,
-        target: factoryAddress,
-        abi: abi["pool_list"],
-        params: i,
-      })
-    ).output;
-    if (poolInfo[poolAddress]) {
-      continue;
-    }
-    poolInfo[poolAddress] = true;
+    let coins = [2, 2, 2, 3, 4, 4, 2, 4, 4, 2, 3, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2]
 
-    let coins = (await sdk.api.abi.call({
+    let balancesCalls = _.flatMap(swaps, (token, i) => {
+      return Array.from(Array(coins[i]), (e, idx) =>({target: token, params: idx}))
+    })
+    balancesCalls = balancesCalls.filter(call => !(call.target == '0xeDf54bC005bc2Df0Cc6A675596e843D28b16A966' && call.params == 1))
+
+    let balancesResults = await sdk.api.abi.multiCall({
       block,
-      target: factoryAddress,
-      abi: abi["get_coinsf"],
-      params: poolAddress
-    })).output;
+      calls: balancesCalls.slice(0, balancesCalls.length-21),
+      abi: {
+        "name": "balances",
+        "outputs": [
+         {
+          "type": "uint256",
+          "name": "out"
+         }
+        ],
+        "inputs": [
+         {
+          "type": "int128",
+          "name": "arg0"
+         }
+        ],
+        "constant": true,
+        "payable": false,
+        "type": "function",
+        "gas": 2250
+      },
+    })
 
-    let coinBalances = (await sdk.api.abi.call({
+    let balancesResults2 = await sdk.api.abi.multiCall({
       block,
-      target: factoryAddress,
-      abi: abi["get_balancesf"],
-      params: poolAddress
-    })).output
+      calls: balancesCalls.slice(-21),
+      abi: {
+        "name": "balances",
+        "outputs": [
+         {
+          "type": "uint256",
+          "name": "out"
+         }
+        ],
+        "inputs": [
+         {
+          "type": "uint256",
+          "name": "arg0"
+         }
+        ],
+        "constant": true,
+        "payable": false,
+        "type": "function",
+        "gas": 2250
+      },
+    })
 
-    for (let i = 0; i < coins.length; i++) {
-      let coin = coins[i];
-      let balance = coinBalances[i];
-      if (coin !== "0x0000000000000000000000000000000000000000") {
-        balances[coin] = balances[coin] ? new BN(balances[coin]).plus(balance) : balance
-      }
+    let coinsCalls = _.flatMap(swaps, (token, i) => {
+      return Array.from(Array(coins[i]), (e, idx) =>({target: token, params: idx}))
+    })
+    coinsCalls = coinsCalls.filter(call => !(call.target == '0xeDf54bC005bc2Df0Cc6A675596e843D28b16A966' && call.params == 1))
+
+    let coinsResults = await sdk.api.abi.multiCall({
+      block,
+      calls: coinsCalls.slice(0, coinsCalls.length-21),
+      abi:  {
+        "name": "coins",
+        "outputs": [
+         {
+          "type": "address",
+          "name": "out"
+         }
+        ],
+        "inputs": [
+         {
+          "type": "int128",
+          "name": "arg0"
+         }
+        ],
+        "constant": true,
+        "payable": false,
+        "type": "function",
+        "gas": 2190
+      },
+    })
+
+    let coinsResults2 = await sdk.api.abi.multiCall({
+      block,
+      calls: coinsCalls.slice(-21),
+      abi:  {
+        "name": "coins",
+        "outputs": [
+         {
+          "type": "address",
+          "name": "out"
+         }
+        ],
+        "inputs": [
+         {
+          "type": "uint256",
+          "name": "arg0"
+         }
+        ],
+        "constant": true,
+        "payable": false,
+        "type": "function",
+        "gas": 2190
+      },
+    })
+
+    for(let [i, balance] of balancesResults.output.entries()) {
+      if(!balance || !balance.output) continue;
+      // Balance doesn't exist yet
+      const out = coinsResults.output[i].output;
+      if(!balances[out]) balances[out] = "0";
+      // Update balance
+      sdk.util.sumSingleBalance(balances, out, balance.output)
+      //balances[out] = String(parseFloat(balances[out]) + parseFloat(balance.output));
     }
-  }
-  }
 
-  delete balances['0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490'];
-  delete balances['0x075b1bb99792c9E1041bA13afEf80C91a1e70fB3'];
-  delete balances['0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8']
+    for(let [i, balance] of balancesResults2.output.entries()) {
+      if(!balance || !balance.output) continue;
+      // Balance doesn't exist yet
+      const out = coinsResults2.output[i].output;
+      if(!balances[out]) balances[out] = "0";
+      // Update balance
+      sdk.util.sumSingleBalance(balances, out, balance.output)
+      //balances[out] = String(parseFloat(balances[out]) + parseFloat(balance.output));
+    }
 
-  const yTokens = [
-    {
-      symbol: "ySUSD",
-      underlying: "SUSD",
-      contract: "0x57ab1ec28d129707052df4df418d58a2d46d5f51",
-    },
-    {
-      symbol: "yUSDC",
-      underlying: "USDC",
-      contract: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-    },
-    {
-      symbol: "ycDAI",
-      underlying: "cDAI",
-      contract: "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643",
-    },
-    {
-      symbol: "yUSDT",
-      underlying: "USDT",
-      contract: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-    },
-    {
-      symbol: "ycUSDC",
-      underlying: "cUSDC",
-      contract: "0x39aa39c021dfbae8fac545936693ac917d5e7563",
-    },
-    {
-      symbol: "ycUSDT",
-      underlying: "cUSDT",
-      contract: "0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9",
-    },
-    {
-      symbol: "yBUSD",
-      underlying: "BUSD",
-      contract: "0x4fabb145d64652a948d72533023f6e7a623c7c53",
-    },
-    {
-      symbol: "yDAI",
-      underlying: "DAI",
-      contract: "0x6b175474e89094c44da98b954eedeac495271d0f",
-    },
-    {
-      symbol: "yTUSD",
-      underlying: "TUSD",
-      contract: "0x0000000000085d4780b73119b644ae5ecd22b376",
-    },
-  ];
+    delete balances['0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490'];
+    delete balances['0x075b1bb99792c9E1041bA13afEf80C91a1e70fB3'];
 
-  balance[etherAddress] = balances["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]
-  delete balances["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]
+    const yTokens = [
+      { symbol: 'ySUSD', underlying: 'SUSD', contract: '0x57ab1ec28d129707052df4df418d58a2d46d5f51' },
+      { symbol: 'yUSDC', underlying: 'USDC', contract: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' },
+      { symbol: 'ycDAI', underlying: 'cDAI' },
+      { symbol: 'yUSDT', underlying: 'USDT', contract: '0xdac17f958d2ee523a2206206994597c13d831ec7' },
+      { symbol: 'ycUSDC', underlying: 'cUSDC' },
+      { symbol: 'ycUSDT', underlying: 'cUSDT' },
+      { symbol: 'yBUSD', underlying: 'BUSD', contract: '0x4fabb145d64652a948d72533023f6e7a623c7c53' },
+      { symbol: 'yDAI', underlying: 'DAI', contract: '0x6b175474e89094c44da98b954eedeac495271d0f' },
+      { symbol: 'yTUSD', underlying: 'TUSD', contract: '0x0000000000085d4780b73119b644ae5ecd22b376' },
+    ]
+
+  //balances["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"] = balances["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]
+  //delete balances["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]
 
   return balances
 }
