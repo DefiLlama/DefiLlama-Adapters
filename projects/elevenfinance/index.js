426a50136f7b6c68e9c29ad0d0ac1cc5dfb681f2
Morazzela <jacquet2.clement@gmail.com>
2021-08-26 22:24:42 +0200

    add elevenfinance adapter

---
+++ projects/elevenfinance/index.js
@@ -0,0 +1,14 @@
+const retry = require('async-retry');
+const axios = require('axios');
+
+const apiUrl = 'https://eleven.finance/api.json';
+
+async function fetch() {
+    const res = await retry(async bail => await axios.get(apiUrl));
+    const tvl = parseFloat(res.totalvaluelocked);;
+    return Math.round(tvl);
+}
+
+module.exports = {
+    fetch,
+}
