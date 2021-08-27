fab3681cdb9f370a4a5a8243cae601b195ced8b5
Morazzela <jacquet2.clement@gmail.com>
2021-08-27 18:28:29 +0200

    style

--- projects/elevenfinance/index.js
+++ projects/elevenfinance/index.js
@@ -5,7 +5,7 @@ const apiUrl = 'https://eleven.finance/api.json';
 
 async function fetch() {
     const res = await retry(async bail => await axios.get(apiUrl));
-    const tvl = parseFloat(res.totalvaluelocked);;
+    const tvl = parseFloat(res.totalvaluelocked);
     return Math.round(tvl);
 }
 
