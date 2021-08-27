ed782310a6aaa8317bd62c49fca8470b34ec179f
0xngmi <0xngmi@protonmail.com>
2021-08-28 00:57:18 +0100

    add blocks to nftx

--- projects/nftx/index.js
+++ projects/nftx/index.js
@@ -2,12 +2,15 @@ const { request, gql } = require("graphql-request");
 const sdk = require('@defillama/sdk')
 
 async function tvl(_timestamp, block){
-  const vaults = await request(graph, query)
+  const vaults = await request(graph, query, {
+    block
+  })
   const supplies = await sdk.api.abi.multiCall({
     abi: 'erc20:totalSupply',
     calls: vaults.vaults.map(v=>({
       target: v.token.id
-    }))
+    })),
+    block
   })
   const balances = {}
   sdk.util.sumMultiBalanceOf(balances, supplies)
@@ -21,8 +24,12 @@ module.exports = {
 
 const graph = "https://api.thegraph.com/subgraphs/name/nftx-project/nftx-v2"
 const query = gql`
-{
-    vaults(first: 1000, where: { vaultId_gte: 0  }) {
+query get_vaults($block: Int) {
+    vaults(first: 1000, where: { 
+      vaultId_gte: 0
+    },
+      block: { number: $block }  
+    ) {
       vaultId
       id
       is1155
