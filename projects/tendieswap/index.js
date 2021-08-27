e18152106bd2e74b2e7bfb5d624b886709ca6198
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-08-27 02:10:08 +0000

    add staking to tendieswap

--- projects/tendieswap/index.js
+++ projects/tendieswap/index.js
@@ -2,6 +2,8 @@ const sdk = require("@defillama/sdk");
 const tvlOnPairs = require("../helper/processPairs.js");
 
 const factoryBSC = "0xb5b4aE9413dFD4d1489350dCA09B1aE6B76BD3a8";
+const masterchef = "0x6dDb25ca46656767f8f2385D653992dC1cdb4470";
+const tendie = "0x9853A30C69474BeD37595F9B149ad634b5c323d9"
 
 const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
   const balances = {};
@@ -9,11 +11,25 @@ const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
   return balances;
 };
 
+async function stakingTvl(timestamp, ethBlock, chainBlocks) {
+  const balances = {};
+  const stakedBanana = sdk.api.erc20.balanceOf({
+    target: tendie,
+    owner: masterchef,
+    chain: 'bsc',
+    block: chainBlocks.bsc
+  })
+  sdk.util.sumSingleBalance(balances, 'bsc:' + tendie, (await stakedBanana).output)
+  return balances;
+}
 
 module.exports = {
   bsc: {
     tvl: bscTvl, 
   },
-  tvl: sdk.util.sumChainTvls([bscTvl]),
+  staking: {
+    tvl: stakingTvl,
+  },
+  tvl: sdk.util.sumChainTvls([bscTvl, stakingTvl]),
   methodology: 'TVL counts the liquidity in each of the Tendie swap pairs. Pairs are found using the factory address'
 };
