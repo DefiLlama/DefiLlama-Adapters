d531873169740a57dd930d7ed0030ec185cf1489
0xngmi <0xngmi@protonmail.com>
2021-08-29 00:01:37 +0100

    change quickswap subgraph

--- projects/quickswap/index.js
+++ projects/quickswap/index.js
@@ -1,7 +1,7 @@
 const { request, gql } = require("graphql-request");
 const { toUSDTBalances } = require('../helper/balances');
 
-const graphUrl = 'https://api.thegraph.com/subgraphs/name/sameepsi/quickswap'
+const graphUrl = 'https://api.thegraph.com/subgraphs/name/sameepsi/quickswap06'
 const graphQuery = gql`
 query get_tvl($block: Int) {
   uniswapFactory(
