de291401d4b4902aaeafa27821a1e39f1c32a4fc
Eric Nakagawa <eric.nakagawa@clabs.co>
2021-08-30 12:40:29 -0700

    switch to tvl function

--- projects/mento/index.js
+++ projects/mento/index.js
@@ -2,7 +2,7 @@ const retry = require('async-retry')
 const axios = require("axios");
 const BigNumber = require("bignumber.js");
 
-async function fetch() {
+async function tvl(timestamp, block) {
 
     /*
         Mento is an automated market maker (AMM) type of decentralized exchange that’s native to the Celo platform. It’s responsible 
@@ -15,15 +15,16 @@ async function fetch() {
         More info here: https://medium.com/celoorg/zooming-in-on-the-celo-expansion-contraction-mechanism-446ca7abe4f
     */
 
-    let mento_contract_address = '0x9380fA34Fd9e4Fd14c06305fd7B6199089eD4eb9';
+    const mento_contract_address = '0x9380fA34Fd9e4Fd14c06305fd7B6199089eD4eb9';
 
-    let celo_price = (await retry(async bail => await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=celo&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true'))).data.celo.usd;
+    let mento_pooled = (new BigNumber((await retry(async bail => await axios.get('https://explorer.celo.org/api/?module=account&action=balance&address=' + mento_contract_address))).data.result)).div(10 ** 18).toFixed(2);
 
-    let mento_pooled = (new BigNumber((await retry(async bail => await axios.get('https://explorer.celo.org/api/?module=account&action=balance&address='+mento_contract_address))).data.result)).div(10 ** 18).toFixed(2);
-    
-    return mento_pooled * celo_price;
+    return {
+        [mento_contract_address]: mento_pooled
+    };
 }
 
 module.exports = {
-    fetch
+    celo: { tvl },
+    tvl
 }
