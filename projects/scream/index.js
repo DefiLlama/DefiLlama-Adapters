6b19c9caab5b48a22f7636e0a8615eda6e7ecfea
0xngmi <0xngmi@protonmail.com>
2021-08-30 01:27:09 +0100

    add fusd

--- projects/scream/index.js
+++ projects/scream/index.js
@@ -3,11 +3,16 @@ const {transformFantomAddress} = require('../helper/portedTokens')
 
 async function tvl(...params) {
     const transformAdress= await transformFantomAddress()
-    const balances = await getCompoundV2Tvl("0x260e596dabe3afc463e75b6cc05d8c46acacfb09", "fantom", transformAdress)(...params)
+    const balances = await getCompoundV2Tvl("0x260e596dabe3afc463e75b6cc05d8c46acacfb09", "fantom", addr=>{
+        if(addr === "0xAd84341756Bf337f5a0164515b1f6F993D194E1f"){
+            return "0x0000000000085d4780b73119b644ae5ecd22b376"
+        }
+        return transformAdress(addr)
+    })(...params)
     return Object.fromEntries(Object.entries(balances).filter(b=>Number(b[1])>1))
 }
 
 module.exports={
-    methodology: "Same as compound, we just get all the collateral (not borrowed money) on the lending markets",
+    methodology: "Same as compound, we just get all the collateral (not borrowed money) on the lending markets. fUSD is returned as TUSD",
     tvl
 }
