189b4cc824b5bee6a023b25b52253b1f36adb598
0xngmi <0xngmi@protonmail.com>
2021-08-31 02:05:02 +0100

    add cream staking

--- projects/scream/index.js
+++ projects/scream/index.js
@@ -1,5 +1,6 @@
 const {getCompoundV2Tvl} = require('../helper/compound')
 const {transformFantomAddress} = require('../helper/portedTokens')
+const staking = require('../helper/staking')
 
 async function tvl(...params) {
     const transformAdress= await transformFantomAddress()
@@ -14,5 +15,8 @@ async function tvl(...params) {
 
 module.exports={
     methodology: "Same as compound, we just get all the collateral (not borrowed money) on the lending markets. fUSD is returned as TUSD",
+    staking:{
+        tvl: staking("0xe3d17c7e840ec140a7a51aca351a482231760824", "0xe0654C8e6fd4D733349ac7E09f6f23DA256bF475", "fantom")
+    },
     tvl
 }
