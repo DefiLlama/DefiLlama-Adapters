1085bf93f4e78a6bb100a06644b20496938493f1
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-08-31 03:20:19 +0000

    add methodology to polycat

--- projects/polycat/index.js
+++ projects/polycat/index.js
@@ -3,5 +3,6 @@ const v1 = require('./v1')
 const v2 = require('./v2')
 
 module.exports = {
+  methodology: 'TVL considers v1 and v2 deposits made to the farming strategies as well as the liquidity on the DEX that is calculated using the factory address (0xEAA98F7b5f7BfbcD1aF14D0efAa9d9e68D82f640)',
   tvl: sdk.util.sumChainTvls([v1.tvl, v2.tvl]),
 };
