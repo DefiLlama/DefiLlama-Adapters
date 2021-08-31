7422fab02cb51d9fa35b208a99bd010b9f547200
Rooter <0xrooter@solend.fi>
2021-08-31 14:58:21 +0800

    Add USDT

--- projects/solend/index.js
+++ projects/solend/index.js
@@ -5,12 +5,14 @@ async function tvl() {
           btcAmount, 
           ethAmount, 
           srmAmount,          
+          usdtAmount,
           solAmount, ] = await Promise.all([
         
         getTokenBalance("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", "DdZR6zRFiUt4S5mg7AV1uKB2z1f1WzcNYCaTEEWPAuby"),
         getTokenBalance("9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E", "DdZR6zRFiUt4S5mg7AV1uKB2z1f1WzcNYCaTEEWPAuby"),
         getTokenBalance("2FPyTwcZLUg1MDrwsyoP4D6s1tM7hAkHYRjkNb5w6Pxk", "DdZR6zRFiUt4S5mg7AV1uKB2z1f1WzcNYCaTEEWPAuby"),
         getTokenBalance("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt", "DdZR6zRFiUt4S5mg7AV1uKB2z1f1WzcNYCaTEEWPAuby"),
+        getTokenBalance("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", "DdZR6zRFiUt4S5mg7AV1uKB2z1f1WzcNYCaTEEWPAuby"),
         getTokenBalance("So11111111111111111111111111111111111111112", "DdZR6zRFiUt4S5mg7AV1uKB2z1f1WzcNYCaTEEWPAuby"),
             
 
@@ -21,6 +23,7 @@ async function tvl() {
         'usd-coin': usdcAmount,
         'ethereum': ethAmount,
         'serum': srmAmount,
+        'usdt': usdtAmount,
         'solana': solAmount,
     }
 }
