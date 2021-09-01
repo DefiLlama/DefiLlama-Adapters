3ef4cdf444cf27a953d9ea4bb83bdfeee491632b
Adam Feather <dakramystic@sirenmarkets.com>
2021-09-01 04:50:45 -0700

    Updated Siren Markets tvl to include polygon and mainnet

--- projects/siren/index.js
+++ projects/siren/index.js
@@ -3,61 +3,116 @@
   ==================================================*/
   const { request, gql } = require("graphql-request");
   const BigNumber = require('bignumber.js');
-  const sdk = require("@defillama/sdk")
+  const sdk = require("@defillama/sdk");
 
-  const GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/sirenmarkets/protocol';
+  const MAINNET_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/sirenmarkets/protocol';
+
+  const POLYGON_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/sirenmarkets/protocol-v2-matic'
+
+  const POLYGON_SERIES_ADDRESS = '0x716c543b39a85aac0240ba7ed07e79f06e1fed48'
 
 /*==================================================
   TVL
   ==================================================*/
+async function calculateMainnetTVL(timestamp, block, chainBlocks) { 
 
-  async function tvl(timestamp, block) {
-    const {
-      amms,
-    } = await request(GRAPH_URL, GET_POOLS, {
-      block,
-    });
+  const ethereumBlock = chainBlocks['ethereum'];
 
-    const result = {
-      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': '0'
-    };
+  const mainnet_amms = await request(MAINNET_GRAPH_URL, GET_POOLS_MAINNET, {
+    ethereumBlock,
+  });
 
-    for (let i = 0; i < amms.length; i++) {
-      const amm = amms[i];
+  const mainnet_result = {
+    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': '0'
+  };
+
+      for (let i = 0; i < mainnet_amms['amms'].length; i++) {
+
+      const amm = mainnet_amms['amms'][i];
+
       const collateralToken = amm['collateralToken']['id'];
 
       // Get collateral in AMM
       const response = await sdk.api.erc20.balanceOf({
-        block,
+        block: ethereumBlock,
         target: collateralToken,
         owner: amm['id'],
+        chain: "ethereum"
       });
 
-      if (!result[collateralToken]) {
-        result[collateralToken] = '0';
+      if (!mainnet_result[collateralToken]) {
+        mainnet_result[collateralToken] = '0';
       }
-      result[collateralToken] = BigNumber(result[collateralToken]).plus(response.output).toFixed();
+      mainnet_result[collateralToken] = BigNumber(mainnet_result[collateralToken]).plus(response.output).toFixed();
 
       // Get collateral in Markets
       for (let im = 0; im < amm.markets.length; im++) {
         const market = amm.markets[im];
 
         const response = await sdk.api.erc20.balanceOf({
-          block,
+          block: ethereumBlock,
           target: collateralToken,
           owner: market['id'],
+          chain: "ethereum"
         });
 
-        result[collateralToken] = BigNumber(result[collateralToken]).plus(response.output).toFixed();
+        mainnet_result[collateralToken] = BigNumber(mainnet_result[collateralToken]).plus(response.output).toFixed();
       }
     }
+    return mainnet_result;
 
-    return result;
+}
+
+async function calculatePolygonTVL(timestamp, block, chainBlocks) { 
+
+  const polygonBlock = chainBlocks['polygon'];
+
+  const polygon_amms = await request(POLYGON_GRAPH_URL, GET_POOLS_POLYGON, {
+    polygonBlock,
+  });
+
+  const polygon_result = {
+    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': '0'
+  };
+
+  for (let i = 0; i < polygon_amms['amms'].length; i++) {
+    const amm = polygon_amms['amms'][i];
+    const collateralToken = amm['collateralToken']['id'];
+
+    // Get collateral in AMM
+    const response = await sdk.api.erc20.balanceOf({
+        block: polygonBlock,
+        target: collateralToken,
+        owner: amm['id'],
+        chain: "polygon"
+      });
+    
+
+    if (!polygon_result['polygon:'+collateralToken]) {
+      polygon_result['polygon:'+collateralToken] = '0';
+    }
+    polygon_result['polygon:'+collateralToken] = BigNumber(polygon_result['polygon:'+collateralToken]).plus(response.output).toFixed();
+
+    // Get collateral in Series
+    for (let im = 0; im < amm.series.length; im++) {
+
+      const response = await sdk.api.erc20.balanceOf({
+          block: polygonBlock,
+          target: collateralToken,
+          owner: POLYGON_SERIES_ADDRESS,
+          chain: "polygon"
+        });
+
+      polygon_result['polygon:'+collateralToken] = BigNumber(polygon_result['polygon:'+collateralToken]).plus(response.output).toFixed();
+    }
   }
+  return polygon_result;
 
-  const GET_POOLS = gql`
-    query Pools($block: Int) {
-      amms(block: { number: $block }) {
+}
+
+  const GET_POOLS_MAINNET = gql`
+    query Pools($ethereumBlock: Int) {
+      amms(block: { number: $ethereumBlock }) {
         id
         collateralToken {
           id
@@ -69,6 +124,26 @@
     }
   `
 
+const GET_POOLS_POLYGON = gql`
+query Pools($polygonBlock: Int) {
+  amms(block: { number: $polygonBlock }) {
+    id
+    collateralToken {
+      id
+    }
+    series{
+        	id,
+          series{
+            id,
+            seriesId
+          }
+        }
+  }
+}
+`
+
+// async function calculateUniTvl()
+
 /*==================================================
   Exports
   ==================================================*/
@@ -78,5 +153,11 @@
     token: 'SI',
     category: 'derivatives',
     start: 1605574800, // Nov-17-2020 01:00:00 AM +UTC
-    tvl
+    polygon: {
+      tvl: calculatePolygonTVL,
+    },
+    ethereum: { 
+      tvl: calculateMainnetTVL
+    },
+    tvl: sdk.util.sumChainTvls([calculatePolygonTVL, calculateMainnetTVL]),
   }
