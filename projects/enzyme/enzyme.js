7436e7b59b7335a3f14b694d7bc52f1191409199
0xngmi <0xngmi@protonmail.com>
2021-09-01 05:55:01 +0100

    fix enzyme

--- projects/enzyme/enzyme.js
+++ projects/enzyme/enzyme.js
@@ -1,10 +1,10 @@
 const sdk = require('@defillama/sdk');
 const axios = require('axios');
-const { sumTokensAndLPsSharedOwners } = require('../helper/unwrapLPs');
-let balances = {};
+const { sumTokensAndLPsSharedOwners, unwrapCrv } = require('../helper/unwrapLPs');
 // node test.js projects/enzyme/index2.js
 
 async function v1(timestamp, block) {
+  const balances = {}
     let v1tokens = [
         "0x0d8775f648430679a709e98d2b0cb6250d2887ef", // BAT
         "0x0f5d2fb29fb7d3cfee444a200298f468908cc942", // MANA
@@ -56,11 +56,18 @@ async function v1(timestamp, block) {
       ).output
     vaults = vaults.map(log => `0x${log[2].substr(26,40)}`);
 
-    v1tokens = v1tokens.map(token => [token, false])
     await sumTokensAndLPsSharedOwners(balances, v1tokens, vaults, block)
+    return balances
 };
+
+const crvStEth = "0x06325440d014e39736583c165c2963ba99faf14e"
+const crvGauge = "0x182b723a58739a9c974cfdb385ceadb237453c28"
+
 async function v2(timestamp, block) {
-    let v2tokens = (await axios.post(
+  const balances = {}
+    let v2tokens = (await axios.get('https://data.enzyme.finance/api/asset/list')).data.data.map(token => token.id)
+    /*
+    (await axios.post(
         'https://api.thegraph.com/subgraphs/name/enzymefinance/enzyme', {
             query: `query {
             assets { id }}` 
@@ -68,6 +75,7 @@ async function v2(timestamp, block) {
             headers: {'Content-Type': 'application/json'}}))
             .data.data.assets
             .map(r => r.id);
+            */
 
     var vaults = (
         await sdk.api.util.getLogs({
@@ -82,69 +90,13 @@ async function v2(timestamp, block) {
 
     v2tokens = v2tokens.map(token => [token, false])
     await sumTokensAndLPsSharedOwners(balances, v2tokens, vaults, block)
-};
-async function test(timestamp, block) {
-  const v1tokens = [
-    "0x0d8775f648430679a709e98d2b0cb6250d2887ef", // BAT
-    "0x0f5d2fb29fb7d3cfee444a200298f468908cc942", // MANA
-    "0x1985365e9f78359a9b6ad760e32412f4a445e862", // REP
-    "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
-    "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
-    "0x408e41876cccdc0f92210600ef50372656052a38", // REN
-    "0x4f3afec4e5a3f2a6a1a411def7d7dfe50ee057bf", // DGX
-    "0x514910771af9ca656af840dff83e8264ecf986ca", // LINK
-    "0x607f4c5bb672230e8672085532f7e901544a7375", // RLC
-    "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
-    "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359", // SAI
-    "0x960b236a07cf122663c4303350609a66a7b288c0", // ANT
-    "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", // MKR
-    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
-    "0xa117000000f279d81a1d3cc75430faa017fa5a2e", // ANTv2
-    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
-    "0xd26114cd6ee289accf82350c8d8487fedb8a0c07", // OMG
-    "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
-    "0xdd974d5c2e2928dea5f71b9825b8b646686bd200", // KNC
-    "0xe41d2489571d322189246dafa5ebde1f4699f498", // ZRX
-    "0xec67005c4e498ec7f55e092bd1d35cbc47c91892", // MLN
-    "0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4", // ENG
-];
-  const v1vaults = [
-    '0x8652071d9a81ca1768110c4255ae781154ff640b', 
-    '0xc63f032ce6040511dde03f3767883ee78fcc1856', 
-    '0x7ca06fa3ef13cef620539f92c050108e3bb285bb', 
-    '0x4de31a86c978a067f198b3da3a44aba1117058bf', 
-    '0x8a5c9de3ee219ad88cf69bbc5b1dd3527af2f815', 
-    '0xb563cca60a62a151bb66a82b8ac4a6e2f0e44736', 
-    '0x932c37613a1084d94a18f98239c269cd8b2c820d', 
-    '0x6ede695a788b9594b615f0a0241f7403b1e25854', 
-    '0xbbe917bc7dcc1e2b781cc3c9f8dcbd6d9b2319ca', 
-    '0x01f4af941046067fa912189560418ba26583660f', 
-    '0x5a02f86ad1ea800ad5ffefbfa76bcea45b37417d', 
-    '0xca203923022d077cf8c88bfdac5d7ad22ef8bd52', 
-    '0x2f723abe81787743d11b9c1a091b7a49cd69dcab', 
-    '0xfde8c0077c5d0e179d7979947f65706c7c91c10e', 
-    '0x3cb783977475b29ef429ef74ab682c76e59dd93b', 
-    '0x45df23d3d74826578a15f9a25ac13d663ca7928e', 
-    '0xce3c4e64c923c8779baed6e431019cb31d65f064', 
-    '0xf418709c8caaab24b55229207dc2c91303cf0bab', 
-    '0xe767b68744a5cc26843a1099816b2c2df9c05bcf', 
-    '0x73a9eeae5904750b9678d31c610e5b61be25c37b', 
-    '0xafed83095f64d757d2e8581b85cee47b7674cf00'
-  ];
 
-  const arg2 = v1tokens.map(token => [token, false])
-  await sumTokensAndLPsSharedOwners(balances, arg2, v1vaults, block)
-    console.log(balances);
-};
-async function tvl(timestamp, block) {
-    await Promise.all([
-        v1(timestamp, block),
-        v2(timestamp, block)
-        //test(timestamp, block)
-      ])
+    await unwrapCrv(balances, crvStEth, balances[crvGauge], block)
+    delete balances[crvGauge]
+
     return balances
 };
 
 module.exports = {
-    tvl
+    tvl: sdk.util.sumChainTvls([v1, v2])
 };
