dc3a059a749176962997fa28d69c7a44a5228722
Mohammed18062020 <mohamed_el_hamdaoui93@hotmail.com>
2021-09-03 12:36:29 +0100

    Kudex Finance && Kswap Finance && Koffee Swap TVL Adapters

--- projects/helper/portedTokens.js
+++ projects/helper/portedTokens.js
@@ -23,6 +23,9 @@ async function transformFantomAddress() {
             if (srcToken.srcChainId === '1') {
                 return srcToken.srcToken;
             } else if (srcToken.srcChainId === '56') {
+                if(srcToken.srcToken === ''){
+                    return 'bsc:0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
+                }
                 return `bsc:${srcToken.srcToken}`;
             }
         }
@@ -42,6 +45,9 @@ async function transformAvaxAddress() {
     ]);
 
     return (addr) => {
+        if(compareAddresses(addr, "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7")){ //WAVAX
+          return `avax:${addr}`
+        }
         const srcToken = bridgeTokensOld.data.find(token => compareAddresses(token["Avalanche Token Address"], addr))
         if (srcToken !== undefined && srcToken["Ethereum Token Decimals"] === srcToken["Avalanche Token Decimals"]) {
             return srcToken["Ethereum Token Address"]
@@ -123,6 +129,9 @@ async function transformXdaiAddress() {
 async function transformOkexAddress() {
     const okexBridge = (await utils.fetchURL("https://www.okex.com/v2/asset/cross-chain/currencyAddress")).data.data.tokens
     // TODO
+    return (addr) => {
+      return `okexchain:${addr}`;
+    };
 }
 
 async function transformHecoAddress() {
@@ -131,11 +140,72 @@ async function transformHecoAddress() {
   };
 }
 
+async function transformHarmonyAddress() {
+    const bridge = (await utils.fetchURL("https://be4.bridge.hmny.io/tokens/?page=0&size=1000")).data.content
+
+    return (addr) => {
+        if(compareAddresses(addr, "0x6983D1E6DEf3690C4d616b13597A09e6193EA013")){
+            return "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+        }
+        const srcToken = bridge.find(token => compareAddresses(addr, token.hrc20Address))
+        if (srcToken !== undefined) {
+            const prefix = srcToken.network === "BINANCE"?"bsc:":""
+            return prefix+srcToken.erc20Address
+        }
+        return `harmony:${addr}`
+    }
+}
+
+async function transformOptimismAddress() {
+    const bridge = (await utils.fetchURL("https://static.optimism.io/optimism.tokenlist.json")).data.tokens
+
+    return (addr) => {
+        const dstToken = bridge.find(token => compareAddresses(addr, token.address))
+        if (dstToken !== undefined) {
+            const srcToken = bridge.find(token => dstToken.logoURI === token.logoURI && token.chainId === 1)
+            if(srcToken !== undefined){
+                return srcToken.address
+            }
+        }
+        return `optimism:${addr}`
+    }
+}
+
+function fixAvaxBalances(balances){
+    for(const representation of ["avax:0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7", '0x9dEbca6eA3af87Bf422Cea9ac955618ceb56EfB4']){
+        if(balances[representation] !== undefined){
+            balances['avalanche-2'] = Number(balances[representation])/1e18
+            delete balances[representation]
+        }
+    }
+}
+
+function fixHarmonyBalances(balances){
+    for(const representation of ["harmony:0xcf664087a5bb0237a0bad6742852ec6c8d69a27a", '0xcf664087a5bb0237a0bad6742852ec6c8d69a27a', 'harmony:0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a', '0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a', 'bsc:0xdE976f3344cd9F06E451aF3A94a324afC3E154F4', 'bsc:0xde976f3344cd9f06e451af3a94a324afc3e154f4']){
+        if(balances[representation] !== undefined){
+            balances['harmony'] = Number(balances[representation])/1e18
+            delete balances[representation]
+        }
+    }
+}
+
+async function transformKccAddress() {
+    return (addr) => {
+      return `kcc:${addr}`;
+    };
+  }
+
 module.exports = {
     transformFantomAddress,
     transformBscAddress,
     transformPolygonAddress,
     transformXdaiAddress,
     transformAvaxAddress,
-    transformHecoAddress
+    transformOkexAddress,
+    transformHecoAddress,
+    transformHarmonyAddress,
+    transformOptimismAddress,
+    fixAvaxBalances,
+    fixHarmonyBalances,
+    transformKccAddress
 };
