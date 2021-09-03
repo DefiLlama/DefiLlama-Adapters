4fd73ac684a404d1e94e721957eda57092de47b7
Peter Chung <touhonoob@gmail.com>
2021-09-04 01:38:56 +0800

    add Kolibri
    Kolibri is an Tezos based stablecoin built on Collateralized Debt Positions (CDPs) known as Ovens.
    Kolibri uses CDPs (referred to as an Oven) to collateralize a soft pegged USD-stable value asset, kUSD.

---
+++ projects/kolibri/index.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const BigNumber = require("bignumber.js");
+
+const priceUrl = 'https://oracle-data.kolibri.finance/data.json';
+const tvlUrl = 'https://kolibri-data.s3.amazonaws.com/mainnet/totals.json';
+
+async function fetch() {
+    const price = new BigNumber((await axios.get(priceUrl)).data.prices.XTZ);
+    const tvl = new BigNumber((await axios.get(tvlUrl)).data.totalBalance);
+    return tvl.multipliedBy(price).dividedBy(1000000).toFixed(0);
+}
+
+module.exports = {
+    fetch
+};
