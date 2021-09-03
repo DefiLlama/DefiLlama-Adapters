7df11eae7c955f591ac06d156219b7725008ca65
Denis <carriere.denis@gmail.com>
2021-09-03 13:36:29 +0200

    Update Defibox lend methodology

--- projects/defibox/index.js
+++ projects/defibox/index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const utils = require("../helper/utils");
+const {lendingMarket} = require("../helper/methodologies")
 
 const eosEndpoint = "https://dapp.defibox.io/api/"
 const bscEndpoint = "https://bsc.defibox.io/api/"
@@ -9,16 +10,16 @@ async function eos() {
   const swap = await utils.fetchURL(eosEndpoint + "swap/get24HInfo")
   const lend = await utils.fetchURL(eosEndpoint + "lend/getGlobalOpenPositionStat")
   const usn = await utils.fetchURL(eosEndpoint + "st/open/getGlobalOpenStat")
-  const tvl = Number(swap.data.data.eosBalance) * eosPrice * 2 +
-              lend.data.data.practicalBalance +
-              usn.data.globalOpenStat.totalMortgage
+  const tvl = Number(swap.data.data.eosBalance) * eosPrice * 2 + // swap TVL
+              lend.data.data.practicalBalance - lend.data.data.totalBorrowsVariable + // lend TVL
+              usn.data.globalOpenStat.totalMortgage // usn (stable token) TVL
   return tvl
 }
 
 async function bsc() {
   const bnbPrice = (await utils.getPricesfromString("binancecoin")).data.binancecoin.usd;
   const swap = await axios.default.post(bscEndpoint + "swap/get24HInfo", {}, { headers: { chainid: 56 }})
-  const tvl = swap.data.data.usd_balance + swap.data.data.wbnb_balance * bnbPrice
+  const tvl = swap.data.data.usd_balance + swap.data.data.wbnb_balance * bnbPrice // swap TVL
   return tvl
 }
 
@@ -27,7 +28,7 @@ async function fetch() {
 }
 
 module.exports = {
-  methodology: 'Defibox TVL is achieved by making a call to its API: https://dapp.defibox.io/api/.',
+  methodology: `${lendingMarket}. Defibox TVL is achieved by making a call to its API: https://dapp.defibox.io/api/.`,
   name: 'Defibox',
   token: 'BOX',
   category: 'dexes',
