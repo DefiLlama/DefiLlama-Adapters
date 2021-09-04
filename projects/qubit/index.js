675b6c46dde6e9919b5ea55a6f7d909cdd0a7f1e
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-09-04 22:33:23 +0000

    add methodology to qubit

--- projects/qubit/index.js
+++ projects/qubit/index.js
@@ -37,6 +37,7 @@ async function bsc(timestamp, ethBlock, chainBlock) {
 }
 
 module.exports = {
+    methodology: 'Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There are multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending',
     misrepresentedTokens: true,
     bsc: {
         tvl: bsc
