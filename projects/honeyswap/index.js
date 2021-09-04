721e4e9c17a4dc41430e8acbdb25fe98b72a3f1a
0xngmi <0xngmi@protonmail.com>
2021-09-04 18:24:28 +0100

    add historical honeyswap

--- projects/honeyswap/index.js
+++ projects/honeyswap/index.js
@@ -1,35 +1,19 @@
 const sdk = require('@defillama/sdk');
-const BigNumber = require("bignumber.js");
 const { request, gql } = require("graphql-request");
-const { toUSDT, toUSDTBalances } = require('../helper/balances');
+const { getChainTvl } = require('../helper/getUniSubgraphTvl');
 
-const xdaiGraphUrl = 'https://api.thegraph.com/subgraphs/name/1hive/honeyswap-xdai'
-const polygonGraphUrl = 'https://api.thegraph.com/subgraphs/name/1hive/honeyswap-polygon'
-const graphQuery = gql`
-{
-  honeyswapFactories(first: 1) {
-    totalLiquidityUSD
-  }
-}
-`
-
-async function xdaiTvl() {
-  const { honeyswapFactories } = await request(xdaiGraphUrl, graphQuery)
-  return toUSDTBalances(honeyswapFactories[0]['totalLiquidityUSD'])
-}
-
-async function polygonTvl() {
-  const { honeyswapFactories } = await request(polygonGraphUrl, graphQuery)
-  return toUSDTBalances(honeyswapFactories[0]['totalLiquidityUSD'])
-}
+const chainTvl = getChainTvl({
+  xdai: 'https://api.thegraph.com/subgraphs/name/1hive/honeyswap-xdai',
+  polygon: 'https://api.thegraph.com/subgraphs/name/1hive/honeyswap-polygon'
+}, "honeyswapFactories")
 
 module.exports = {
   misrepresentedTokens: true,
   xdai: {
-    tvl: xdaiTvl
+    tvl: chainTvl('xdai')
   },
   polygon: {
-    tvl: polygonTvl
+    tvl: chainTvl('polygon')
   },
-  tvl: sdk.util.sumChainTvls([xdaiTvl, polygonTvl])
+  tvl: sdk.util.sumChainTvls([chainTvl('xdai'), chainTvl('polygon')])
 }
