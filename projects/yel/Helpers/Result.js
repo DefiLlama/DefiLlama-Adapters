3b4ed951ccaf13a1aed43170b1a5cf08b410a050
YieldEnhancementLabs <info@yel.finance>
2021-09-06 02:56:58 +0300

    TVL calculation for YEL token
    Added sdk adapter for YEL token

---
+++ projects/yel/Helpers/Result.js
@@ -0,0 +1,25 @@
+const BigNumber = require("bignumber.js");
+module.exports = class Result {
+    result = {}
+
+    constructor(obj) {
+        this.result = obj;
+    }
+
+    append(obj) {
+        for (const key in obj) {
+            if (this.result.hasOwnProperty(key)) {
+                BigNumber.config({EXPONENTIAL_AT: 100});
+                const sum = (new BigNumber(this.result[key])).plus(new BigNumber(obj[key]));
+                this.result[key] = sum.valueOf();
+            } else {
+                this.result[key] = obj[key];
+            }
+        }
+        return this;
+    }
+
+    render() {
+        return this.result;
+    }
+}
