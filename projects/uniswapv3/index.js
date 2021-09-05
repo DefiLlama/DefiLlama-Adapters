cff258aadaa8f82fbe32783af079ae0be30cc1df
fun <lljxx1@gmail.com>
2021-09-05 21:32:59 +0800

    fix uniswapv3

--- projects/uniswapv3/index.js
+++ projects/uniswapv3/index.js
@@ -3,7 +3,8 @@ const sdk = require('@defillama/sdk')
 
 const graphUrls = {
   ethereum: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
-  optimism: "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-optimism-dev"
+  optimism: "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-optimism-dev",
+  arbitrum: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-arbitrum-one',
 }
 const chainTvl = getChainTvl(graphUrls, "factories", "totalValueLockedUSD")
 
