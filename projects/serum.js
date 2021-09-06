9d5b5248647041a97623d4d931a9253909d41f49
jhl <77697590+jhlx@users.noreply.github.com>
2021-09-07 06:00:34 +0800

    Update serum.js

--- projects/serum.js
+++ projects/serum.js
@@ -6,55 +6,55 @@ const serumProgramId = '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'
 const step = 1;
 
 function add(balances, token, amount) {
-    balances[token] = (balances[token] || 0) + amount
+  balances[token] = (balances[token] || 0) + amount
 }
 
 async function fetch() {
-    let tvl = 0;
-    const balances = {}
-    const tokenPrices = {}
+  let tvl = 0;
+  const balances = {}
+  const tokenPrices = {}
 
-    const connection = new Connection('https://solana-api.projectserum.com/');
-    const programId = new PublicKey(serumProgramId);
-    const response = await axios.get('https://swap.sollet.io/api/markets');
-    const markets = response.data.result;
-    for (let i = 0; i < markets.length; i += step) {
-        await Promise.all(markets.slice(i, i + step).map(async marketData => {
-            const marketAddress = new PublicKey(marketData.address);
-            const market = await Market.load(connection, marketAddress, {}, programId);
-            const bids = await market.loadBids(connection);
-            const asks = await market.loadAsks(connection);
-            const ceil = Math.min(...Array.from(asks).map(ask => ask.price))
-            const floor = Math.max(...Array.from(bids).map(ask => ask.price))
-            if (floor === -Infinity || ceil === Infinity) return
-            const price = (floor + ceil) / 2
-            const [buyCurrency, quoteCurrency] = marketData.market.split('/')
-            for (let order of Array.from(asks)) {
-                add(balances, buyCurrency, order.size)
-            }
-            for (let order of Array.from(bids)) {
-                add(balances, quoteCurrency, order.size * order.price)
-            }
+  const connection = new Connection('https://solana-api.projectserum.com/');
+  const programId = new PublicKey(serumProgramId);
+  const response = await axios.get('https://swap.sollet.io/api/markets');
+  const markets = response.data.result;
+  for (let i = 0; i < markets.length; i += step) {
+    await Promise.all(markets.slice(i, i + step).map(async marketData => {
+      const marketAddress = new PublicKey(marketData.address);
+      const market = await Market.load(connection, marketAddress, {}, programId);
+      const bids = await market.loadBids(connection);
+      const asks = await market.loadAsks(connection);
+      const ceil = Math.min(...Array.from(asks).map(ask => ask.price))
+      const floor = Math.max(...Array.from(bids).map(ask => ask.price))
+      if (floor === -Infinity || ceil === Infinity) return
+      const price = (floor + ceil) / 2
+      const [buyCurrency, quoteCurrency] = marketData.market.split('/')
+      for (let order of Array.from(asks)) {
+        add(balances, buyCurrency, order.size)
+      }
+      for (let order of Array.from(bids)) {
+        add(balances, quoteCurrency, order.size * order.price)
+      }
 
-            if (quoteCurrency === 'USDT' || quoteCurrency === 'USDC') {
-                tokenPrices[buyCurrency] = price
-            }
-        }))
+      if (quoteCurrency === 'USDT' || quoteCurrency === 'USDC') {
+        tokenPrices[buyCurrency] = price
+      }
+    }))
+  }
+  tokenPrices['USDT'] = 1
+  tokenPrices['USDC'] = 1
+
+  Object.entries(balances).forEach(([token, balance]) => {
+    const price = tokenPrices[token]
+    if (price === undefined) {
+      console.error(`There's no price for ${token}`)
+      return
     }
-    tokenPrices['USDT'] = 1
-    tokenPrices['USDC'] = 1
-
-    Object.entries(balances).forEach(([token, balance]) => {
-        const price = tokenPrices[token]
-        if (price === undefined) {
-            console.error(`There's no price for ${token}`)
-            return
-        }
-        tvl += price * balance
-    })
-    return tvl;
+    tvl += price * balance
+  })
+  return tvl;
 }
 
 module.exports = {
-    fetch
+  fetch
 }
