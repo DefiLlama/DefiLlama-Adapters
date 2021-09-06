02d2492151433aa28fead41c5b3c6dd5b44581f5
jhl <77697590+jhlx@users.noreply.github.com>
2021-09-07 05:39:36 +0800

    Change to Sollet Markets endpoint

--- projects/serum.js
+++ projects/serum.js
@@ -5,55 +5,56 @@ const { Market } = require('@project-serum/serum');
 const serumProgramId = '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'
 const step = 1;
 
-function add(balances, token, amount){
-  balances[token] = (balances[token] || 0) + amount
+function add(balances, token, amount) {
+    balances[token] = (balances[token] || 0) + amount
 }
 
 async function fetch() {
-  let tvl = 0;
-  const balances = {}
-  const tokenPrices = {}
+    let tvl = 0;
+    const balances = {}
+    const tokenPrices = {}
 
-  const connection = new Connection('https://solana-api.projectserum.com/');
-  const programId = new PublicKey(serumProgramId);
-  const markets = await axios.get('https://wallet-api.bonfida.com/cached/market-table')
-  for (let i = 0; i < markets.data.data.length; i += step) {
-    await Promise.all(markets.data.data.slice(i, i + step).map(async marketData => {
-      const marketAddress = new PublicKey(marketData.address);
-      const market = await Market.load(connection, marketAddress, {}, programId);
-      const bids = await market.loadBids(connection);
-      const asks = await market.loadAsks(connection);
-      const ceil = Math.min(...Array.from(asks).map(ask => ask.price))
-      const floor = Math.max(...Array.from(bids).map(ask => ask.price))
-      if (floor === -Infinity || ceil === Infinity) return
-      const price = (floor + ceil) / 2
-      const [buyCurrency, quoteCurrency] = marketData.market.split('/')
-      for (let order of Array.from(asks)) {
-        add(balances, buyCurrency, order.size)
-      }
-      for (let order of Array.from(bids)) {
-        add(balances, quoteCurrency, order.size * order.price)
-      }
+    const connection = new Connection('https://solana-api.projectserum.com/');
+    const programId = new PublicKey(serumProgramId);
+    const response = await axios.get('https://swap.sollet.io/api/markets');
+    const markets = response.data.result;
+    for (let i = 0; i < markets.length; i += step) {
+        await Promise.all(markets.slice(i, i + step).map(async marketData => {
+            const marketAddress = new PublicKey(marketData.address);
+            const market = await Market.load(connection, marketAddress, {}, programId);
+            const bids = await market.loadBids(connection);
+            const asks = await market.loadAsks(connection);
+            const ceil = Math.min(...Array.from(asks).map(ask => ask.price))
+            const floor = Math.max(...Array.from(bids).map(ask => ask.price))
+            if (floor === -Infinity || ceil === Infinity) return
+            const price = (floor + ceil) / 2
+            const [buyCurrency, quoteCurrency] = marketData.market.split('/')
+            for (let order of Array.from(asks)) {
+                add(balances, buyCurrency, order.size)
+            }
+            for (let order of Array.from(bids)) {
+                add(balances, quoteCurrency, order.size * order.price)
+            }
 
-      if (quoteCurrency === 'USDT' || quoteCurrency === 'USDC') {
-        tokenPrices[buyCurrency] = price
-      }
-    }))
-  }
-  tokenPrices['USDT'] = 1
-  tokenPrices['USDC'] = 1
-
-  Object.entries(balances).forEach(([token, balance]) => {
-    const price = tokenPrices[token]
-    if (price === undefined) {
-      console.error(`There's no price for ${token}`)
-      return
+            if (quoteCurrency === 'USDT' || quoteCurrency === 'USDC') {
+                tokenPrices[buyCurrency] = price
+            }
+        }))
     }
-    tvl += price * balance
-  })
-  return tvl;
+    tokenPrices['USDT'] = 1
+    tokenPrices['USDC'] = 1
+
+    Object.entries(balances).forEach(([token, balance]) => {
+        const price = tokenPrices[token]
+        if (price === undefined) {
+            console.error(`There's no price for ${token}`)
+            return
+        }
+        tvl += price * balance
+    })
+    return tvl;
 }
 
 module.exports = {
-  fetch
+    fetch
 }
