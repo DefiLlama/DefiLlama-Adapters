315ed791450f027578e2012901a4e83b72be5a0b
0xngmi <0xngmi@protonmail.com>
2021-09-07 23:38:42 +0100

    separate new shibance

--- projects/shibance/index.js
+++ projects/shibance/index.js
@@ -1,113 +1,25 @@
-const BigNumber = require("bignumber.js");
-const { getBalanceNumber } = require('./format');
-const sdk = require("@defillama/sdk");
-const chains = require('./constants/chain');
+const { getChainTvl } = require('../helper/getUniSubgraphTvl');
+const {calculateUniTvl} = require('../helper/calculateUniTvl.js')
+const sdk = require('@defillama/sdk')
 
-require("dotenv").config();
-
-const fetchPublicVaultData = require("./vault");
-const fetchFarms = require('./farm');
-const { fetchPoolsTotalStaking } = require('./pool');
-
-function getChainTvl(chain) {
-  return function (timestamp, _ethBlock, chainBlocks) {
-    return getTvl(chain, timestamp, _ethBlock, chainBlocks);
-  };
+const graphUrls = {
+  kcc: 'https://info.shibance.com/subgraphs/name/shibance/exchange-kcc',
+  bsc: 'https://api.thegraph.com/subgraphs/name/shibance/exchange-backup'
 }
+const chainTvl = getChainTvl(graphUrls)
 
-async function getTvl(chain, timestamp, _ethBlock, chainBlocks) {
-  const block = chainBlocks ? chainBlocks[chain] : null;
-  const chainPid = {
-    kcc: 1,
-    bsc: 3
-  };
-
-  const chainId = chains[chain];
-  // console.log("block", block);
-  const [vData, pools, farmsLP] = await Promise.all([
-    fetchPublicVaultData(chain, block),
-    fetchPoolsTotalStaking(chain, block),
-    fetchFarms(chain, block),
-  ]);
-
-  const totalCakeInVault = new BigNumber(vData.totalCakeInVault);
-  const woofLp = farmsLP.filter(_ => _.pid == chainPid[chain])[0];
-  const woofPrice = new BigNumber(woofLp.token.busdPrice);
-
-  const farmsLiquidity = farmsLP
-    .map((farm) => {
-      if (
-        farm.pid !== 0 &&
-        farm.multiplier !== "0X" &&
-        farm.lpTotalInQuoteToken &&
-        farm.quoteToken.busdPrice
-      ) {
-        return new BigNumber(farm.lpTotalInQuoteToken).times(
-          farm.quoteToken.busdPrice
-        );
-      }
-      return null;
-    })
-    .filter((liquidity) => !!liquidity)
-    .reduce((a, b) => a.plus(b));
-
-  let poolsStaked = null;
-
-  if (woofPrice.gt(0) && totalCakeInVault.gt(0)) {
-    const staked = pools.map((pool) => {
-      if (pool.sousId === 0) {
-        return new BigNumber(pool.totalStaked)
-          .minus(totalCakeInVault)
-          .times(woofPrice);
-      }
-      return new BigNumber(pool.totalStaked).times(woofPrice);
-    });
-    poolsStaked = staked.reduce((a, b) => a.plus(b));
-  }
-
-  const tvl =
-    farmsLiquidity && totalCakeInVault && poolsStaked
-      ? farmsLiquidity
-          .plus(
-            getBalanceNumber(
-              totalCakeInVault.times(woofPrice).plus(poolsStaked),
-              18
-            )
-          )
-      : null;
-
-  const balances = {};
-  const usdMappings = {
-    'bsc' : '0x4fabb145d64652a948d72533023f6e7a623c7c53'
-  }
-  const baseToken = usdMappings[chain];
-  balances[`${chain}:${baseToken}`] = tvl.toNumber();
-
-  if (chainBlocks) return balances;
-  return tvl.toNumber();
+async function bsc(_timestamp, _ethBlock, chainBlocks){
+  return calculateUniTvl(addr=>`bsc:${addr}`, chainBlocks['bsc'], 'bsc', '0x092EE062Baa0440B6df6BBb7Db7e66D8902bFdF7', 0, true);
 }
-
-// (async () => {
-//   try {
-//     console.log(
-//       await getTvl("bsc", null, null, {
-//         bsc: 10700328,
-//       })
-//     );
-//   } catch(e) {
-//     console.log(e.stack)
-//   }
-// })();
 
 module.exports = {
   misrepresentedTokens: true,
-  methodology: "We count liquidity on the dexes, pulling data from onchain",
-  fetch: getChainTvl("bsc"),
-  // kcc: {
-  //   tvl: getChainTvl("kcc"),
-  // },
-  // bsc: {
-  //   tvl: getChainTvl("bsc"),
-  // },
-  // tvl: getChainTvl("bsc"),
-};
+  methodology: "We count liquidity on the dexes, pulling data from subgraphs",
+  kcc: {
+    tvl: chainTvl('kcc'),
+  },
+  bsc:{
+    tvl: bsc
+  },
+  tvl: sdk.util.sumChainTvls([chainTvl('kcc'), bsc])
+}
