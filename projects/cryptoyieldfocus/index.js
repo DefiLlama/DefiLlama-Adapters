fbe96a5cf428df32a786a6e3b7053237f76409f8
0xngmi <0xngmi@protonmail.com>
2021-09-07 21:53:22 +0100

    separate staking and pool2 from cryptoyieldfocus

--- projects/cryptoyieldfocus/index.js
+++ projects/cryptoyieldfocus/index.js
@@ -1,76 +1,48 @@
 const sdk = require("@defillama/sdk");
-const erc20 = require("../helper/abis/erc20.json");
 const abi = require("./abi.json");
-const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
 const { transformAvaxAddress } = require("../helper/portedTokens");
+const {addFundsInMasterChef} = require('../helper/masterchef')
+const staking = require('../helper/staking')
+const {sumTokensAndLPsSharedOwners} = require('../helper/unwrapLPs')
 
 const STAKING_CONTRACT = "0xaB0141F81b3129f03996D0679b81C07F6A24c435";
+const cyf = "0x411491859864797792308723Fc417f11BbA18D1b"
+const pool2s = [
+  "0x8dcb95A8CD13A734A470A1808a2472bD6B3A7A56",
+    "0x3437Bf22e261c79328e3B91a1F299e057fA12Cb6",
+    "0x0e79B2F73461D682174b00e676b68237eF8583F7",
+    "0x60ef780FB54373088b93db6600BbBAA90Eb14243"
+]
 
 const avaxTvl = async (timestamp, ethBlock, chainBlocks) => {
   const balances = {};
 
-  const lengthPool = (
-    await sdk.api.abi.call({
-      abi: abi.poolLength,
-      target: STAKING_CONTRACT,
-      chain: "avax",
-      block: chainBlocks["avax"],
-    })
-  ).output;
+  const transformAddress = await transformAvaxAddress();
+  await addFundsInMasterChef(balances, STAKING_CONTRACT, chainBlocks.avax, 'avax', transformAddress, abi, [
+    cyf,
+    ...pool2s
+  ])
 
-  const lpPositions = [];
+  return balances;
+};
 
-  for (let index = 0; index < lengthPool; index++) {
-    const lpOrTokens = (
-      await sdk.api.abi.call({
-        abi: abi.poolInfo,
-        target: STAKING_CONTRACT,
-        params: index,
-        chain: "avax",
-        block: chainBlocks["avax"],
-      })
-    ).output[0];
-
-    const lpOrToken_bal = (
-      await sdk.api.abi.call({
-        abi: erc20.balanceOf,
-        target: lpOrTokens,
-        params: STAKING_CONTRACT,
-        chain: "avax",
-        block: chainBlocks["avax"],
-      })
-    ).output;
-
-    if (
-      index == 7 ||
-      (index >= 8 && index <= 13) ||
-      (index >= 15 && index <= 16) || 
-      index == 18
-    ) {
-      sdk.util.sumSingleBalance(balances, `avax:${lpOrTokens}`, lpOrToken_bal);
-    } else {
-      lpPositions.push({
-        token: lpOrTokens,
-        balance: lpOrToken_bal,
-      });
-    }
-  }
+const pool2 = async (timestamp, ethBlock, chainBlocks) => {
+  const balances = {};
 
   const transformAddress = await transformAvaxAddress();
-
-  await unwrapUniswapLPs(
-    balances,
-    lpPositions,
-    chainBlocks["avax"],
-    "avax",
-    transformAddress
-  );
+  await sumTokensAndLPsSharedOwners(balances, pool2s.map(p=>[p, true]), [STAKING_CONTRACT], chainBlocks.avax, 'avax', transformAddress)
 
   return balances;
 };
 
 module.exports = {
-  avax: {
+  staking:{
+    tvl:staking(STAKING_CONTRACT, cyf, 'avax', `avax:${cyf}`)
+  },
+  pool2:{
+    tvl: pool2
+  },
+  avalanche: {
     tvl: avaxTvl,
   },
   tvl: sdk.util.sumChainTvls([avaxTvl]),
