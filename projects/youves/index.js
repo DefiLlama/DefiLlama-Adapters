26d0f94a6dca0633ab1ff3698e6c9f14b2c9829d
0xngmi <0xngmi@protonmail.com>
2021-09-07 15:35:35 +0100

    use coingecko for youves

--- projects/youves/index.js
+++ projects/youves/index.js
@@ -1,6 +1,4 @@
-const axios = require('axios')
 const { GraphQLClient, gql } = require("graphql-request");
-const { default: BigNumber } = require('bignumber.js');
 const retry = require('../helper/retry')
 
 const indexer = "http://youves-indexer.prod.gke.papers.tech/v1/graphql"
@@ -13,22 +11,15 @@ var query = gql`
     }
   }
 `
-async function fetch() {
-    const priceData = await retry(async req => await axios.get("https://api.better-call.dev/v1/contract/mainnet/KT1RC22chBZGJtWv82e5pSeyqyBLEyDRqobz/storage"))
+async function tvl() {
     const graphQLClient = new GraphQLClient(indexer);
     const oracleData = await retry(async req => await graphQLClient.request(query))
 
-    result = new BigNumber(0)
-    for (const val of priceData.data[0].children){
-            if (val["name"] === "price"){
-                price = val["value"]
-                break
-            }
-        }
-
-    return price * oracleData["vault_aggregate"]["aggregate"]["sum"]["balance"] / 1000000 / 1000000
+    return {
+        "tezos": oracleData["vault_aggregate"]["aggregate"]["sum"]["balance"] / 1000000 
+    }
 }
 
 module.exports = {
-    fetch
+    tvl
 }
