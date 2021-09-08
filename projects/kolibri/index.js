3a1cac0298c2698e3f07cc4e73fb9b8597669990
Keefer Taylor <keefer@keefertaylor.com>
2021-09-08 14:24:39 -0700

    update

--- projects/kolibri/index.js
+++ projects/kolibri/index.js
@@ -1,24 +1,13 @@
 const axios = require("axios");
 const BigNumber = require("bignumber.js");
 
-const priceUrl = 'https://oracle-data.kolibri.finance/data.json';
 const tvlUrl = 'https://kolibri-data.s3.amazonaws.com/mainnet/totals.json';
 
 async function fetch() {
-    const price = new BigNumber((await axios.get(priceUrl)).data.prices.XTZ);
-    const data = (await axios.get(tvlUrl)).data
-
-    const tvl = new BigNumber(data.totalBalance);
-    const ovenTvl = tvl.multipliedBy(price).dividedBy(1000000).toFixed(0);
-
-    const liquidityPoolTvl = new BigNumber(data.liquidityPoolBalance)
-
-    const farmsTvl = new BigNumber(data.totalFarmBalanceUSD)
-
-    return ovenTvl.plus(liquidityPoolTvl).plus(farmsTvl)
+    return new BigNumber((await axios.get(tvlUrl)).data.tvlUSD);
 }
 
 module.exports = {
-    methodology: 'TVL counts the XTZ tokens that are deposited to mint kUSD, borrowed tokens are not counted.',
+    methodology: 'TVL counts the XTZ tokens that are deposited to mint kUSD, kUSD in the liquidity pool, and value locked in Kolibri Protocol\'s farms. Borrowed tokens are not counted.',
     fetch
 };
