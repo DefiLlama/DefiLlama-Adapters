17ec3b8fccc84e1c40dcab8963bf7f33583944ea
0xngmi <0xngmi@protonmail.com>
2021-09-11 02:45:15 +0100

    fix pylon

--- projects/pylon/index.js
+++ projects/pylon/index.js
@@ -1,11 +1,11 @@
 const {fetchURL} = require('../helper/utils')
 
-// Pool with all the coins is on https://finder.terra.money/columbus-4/address/terra1z5j60wct88yz62ylqa4t8p8239cwx9kjlghkg2 but idk how to unwrap aUST into UST so we're using the api instead
+// Pools in https://gateway.pylon.money/
 async function tvl(){
-    const pools = await fetchURL("https://api.pylon.money/api/launchpad/v1/projects/mine/")
+    const staked = await fetchURL(stakedEnpoint)
 
     return {
-        'terrausd': pools.data.project.pools.reduce((t,p)=>t+p.totalDepositsInUst, 0)
+        'terrausd': staked.data.totalValueLockedUST - staked.data.liquidityInfo.ustReserve 
     }
 }
 
