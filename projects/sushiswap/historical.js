6b3d0e0a29faa4045be444376bd88807143f2af5
0xngmi <0xngmi@protonmail.com>
2021-09-13 04:49:26 +0100

    sushi historical

--- projects/sushiswap/historical.js
+++ projects/sushiswap/historical.js
@@ -1,15 +1,35 @@
-const HTMLParser = require("node-html-parser");
-const axios = require("axios");
+const sdk = require('@defillama/sdk')
+const { getChainTvl } = require('../helper/getUniSubgraphTvl')
 
-const analytics = [
-    ["avalanche", ""],
-    ["harmony", "https://analytics-harmony.sushi.com/"],
-    ["polygon", "https://analytics-polygon.sushi.com/"],
-    ["fantom", "https://analytics-ftm.sushi.com/"],
-]
+const subgraphs = {
+    'ethereum': 'sushiswap/exchange',
+    'xdai': 'sushiswap/xdai-exchange',
+    'polygon': 'sushiswap/matic-exchange',
+    'fantom': 'sushiswap/fantom-exchange',
+    'bsc': 'sushiswap/bsc-exchange',
+    'harmony': 'sushiswap/harmony-exchange',
+    'okexchain': 'sushiswap/okex-exchange',
+    'avax': 'sushiswap/avalanche-exchange',
+    'celo': 'sushiswap/celo-exchange',
+    'arbitrum': 'sushiswap/arbitrum-exchange',
+    //'okexchain': 'okex-exchange/oec',
+    'heco': 'heco-exchange/heco',
+}
 
-async function chainTvl(url) {
-    const html = await axios.get("https://analytics-harmony.sushi.com/")
-    const data = HTMLParser.parse(html.data).querySelector("#__NEXT_DATA__")
-    const dayDatas = Object.entries(JSON.parse(data.innerHTML).props.pageProps.initialApolloState).filter(([name]) => name.startsWith("DayData"))
+const chainTvl = getChainTvl(
+    Object.fromEntries(Object.entries(subgraphs).map(s => [s[0], "https://api.thegraph.com/subgraphs/name/" + s[1]])),
+    "factories",
+    "liquidityUSD"
+)
+
+const subgraphChainTvls = Object.keys(subgraphs).reduce((obj, chain) => ({
+    ...obj,
+    [chain === 'avax' ? 'avalanche' : chain]: {
+        tvl:chainTvl(chain)
+    }
+}), {})
+
+module.exports={
+    ...subgraphChainTvls,
+    tvl: sdk.util.sumChainTvls(Object.values(subgraphChainTvls).map(tvl=>tvl.tvl))
 }
