13a7347ad41b87c4e2a67e17c8cfdd7f7d24ebc2
0xngmi <0xngmi@protonmail.com>
2021-09-13 05:09:29 +0100

    add univ3 arbitrum

--- projects/uniswapv3/index.js
+++ projects/uniswapv3/index.js
@@ -4,7 +4,7 @@ const sdk = require('@defillama/sdk')
 const graphUrls = {
   ethereum: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
   optimism: "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-optimism-dev",
-  arbitrum: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-arbitrum-one',
+  arbitrum: 'https://api.thegraph.com/subgraphs/name/ianlapham/arbitrum-dev',
 }
 const chainTvl = getChainTvl(graphUrls, "factories", "totalValueLockedUSD")
 
@@ -17,10 +17,8 @@ module.exports = {
   optimism: {
     tvl: chainTvl('optimism'),
   },
-  /*
   arbitrum: {
     tvl: chainTvl('arbitrum'),
   },
-  */
-  tvl: sdk.util.sumChainTvls(['ethereum', 'optimism'].map(chainTvl))
+  tvl: sdk.util.sumChainTvls(['ethereum', 'optimism', 'arbitrum'].map(chainTvl))
 }
