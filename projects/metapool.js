ebb50ecb75cd7707d200908112610c4726181a79
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-09-16 03:39:13 +0000

    add adapter for metapool on near

---
+++ projects/metapool.js
@@ -0,0 +1,26 @@
+const utils = require('./helper/utils');
+const axios = require("axios");
+const retry = require("async-retry");
+
+
+
+
+async function fetch() {
+    let totalTvl = await utils.fetchURL('http://validators.narwallets.com:7000/metrics_json')
+
+    const priceNear = await retry(
+        async (bail) =>
+          await axios.get(
+            `https://api.coingecko.com/api/v3/simple/price?ids=near&vs_currencies=usd`
+          )
+      );
+
+  
+  
+  return totalTvl.data.tvl * priceNear.data.near.usd;
+}
+
+module.exports = {
+    methodology: 'TVL counts the NEAR tokens that are staked.',
+    fetch,
+  }
