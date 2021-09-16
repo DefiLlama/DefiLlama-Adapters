dc71839bffa70556e57229fe9abe463d507f5c7d
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-09-16 03:59:03 +0000

    update syntax metapool

--- projects/metapool.js
+++ projects/metapool.js
@@ -6,10 +6,10 @@ const retry = require("async-retry");
 
 
 async function fetch() {
-    let totalTvl = await utils.fetchURL('http://validators.narwallets.com:7000/metrics_json')
+    const totalTvl = await utils.fetchURL('http://validators.narwallets.com:7000/metrics_json')
 
     const priceNear = await retry(
-        async (bail) =>
+        async () =>
           await axios.get(
             `https://api.coingecko.com/api/v3/simple/price?ids=near&vs_currencies=usd`
           )
