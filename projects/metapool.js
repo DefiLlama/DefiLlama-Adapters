da200763f589ec87dd25a02a421622bdc18bd05e
0xngmi <0xngmi@protonmail.com>
2021-09-16 08:26:52 +0100

    return near balance

--- projects/metapool.js
+++ projects/metapool.js
@@ -1,26 +1,17 @@
 const utils = require('./helper/utils');
-const axios = require("axios");
-const retry = require("async-retry");
 
 
 
 
-async function fetch() {
+async function tvl() {
     const totalTvl = await utils.fetchURL('http://validators.narwallets.com:7000/metrics_json')
 
-    const priceNear = await retry(
-        async () =>
-          await axios.get(
-            `https://api.coingecko.com/api/v3/simple/price?ids=near&vs_currencies=usd`
-          )
-      );
-
-  
-  
-  return totalTvl.data.tvl * priceNear.data.near.usd;
+  return {
+    near: totalTvl.data.tvl
+  }
 }
 
 module.exports = {
     methodology: 'TVL counts the NEAR tokens that are staked.',
-    fetch,
+    tvl,
   }
