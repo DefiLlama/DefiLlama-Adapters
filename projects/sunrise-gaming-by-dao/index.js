31f25e2e8f29732001c414239e8e4680ba29e129
sunrisedao <sunrisedao@gmail.com>
2021-09-19 13:54:07 +0700

    show total tvl

--- projects/sunrise-gaming-by-dao/index.js
+++ projects/sunrise-gaming-by-dao/index.js
@@ -3,6 +3,7 @@ const {staking} = require('../helper/staking')
 const {pool2} = require('../helper/pool2')
 
 const suncAddress = '0x692aCCdD8b86692427E0aa4752AE917Df01CC56F';
+const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 const lpWethSunc = '0xaf5a7469cf2571b973aeee9ae2f8aad00e1337d2';
 const stakingAddress = '0x7dbE40ac6bB41A5FE4Fa2C74f31d7DEFBC793B58';
 
@@ -18,11 +19,16 @@ async function tvl(timestamp, block) {
         params: [lpWethSunc]
       },
       {
+        target: wethAddress,
+        params: [lpWethSunc]
+      },
+      {
         target: suncAddress,
         params: [stakingAddress]
       }
     ]
   })
+
   sdk.util.sumMultiBalanceOf(balances, results);
 
   return balances
@@ -35,5 +41,5 @@ module.exports = {
   pool2:{
     tvl: pool2(stakingAddress, lpWethSunc)
   },
-  tvl: async ()=>({})
+  tvl
 }
