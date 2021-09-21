ac77a2bc62d7d93fae4a539227f5359871c806bc
rleifke <rleifke@umich.edu>
2021-09-21 01:08:53 -0400

    added usdc eth address and fixed tvl reporting

--- projects/mobius/index.js
+++ projects/mobius/index.js
@@ -5,6 +5,8 @@
   const sdk = require('@defillama/sdk');
   const BigNumber = require('bignumber.js');
   const _ = require('underscore');
+  const {getBlock} = require('../helper/getBlock') //added module
+
 
 /*==================================================
   Addresses
@@ -13,44 +15,63 @@
   const usdcPoolAddress = "0x0ff04189Ef135b6541E56f7C638489De92E9c778"
 
   const tokens = {
-    // cusd
-    "0x765de816845861e75a25fca122bb6898b8b1282a": [usdcPoolAddress],
-    // // cusdc
-    // "0x93DB49bE12B864019dA9Cb147ba75cDC0506190e": [usdcPoolAddress],
+    "0x2A3684e9Dc20B857375EA04235F2F7edBe818FA7": ["usdcETH-optics"], // USDC from Ethereum (Optics)
+    "0x93DB49bE12B864019dA9Cb147ba75cDC0506190e": ["cUSDC-moss"], // USDC from Ethereum (Moss)
   }
 
 /*==================================================
   TVL
   ==================================================*/
 
-  async function tvl(timestamp, block) {
-    let balances = {};
-    let calls = [];
-
-    for (const token in tokens) {
-      for(const poolAddress of tokens[token])
-      calls.push({
-        target: token,
-        params: poolAddress
-      })
+  async function tvl(timestamp, ethBlock, chainBlocks) {
+    const chain = 'celo'
+    const block = await getBlock(timestamp, chain, chainBlocks);
+    const balances = {
+        "celo": toNumber((await sdk.api.eth.getBalance({
+          block,
+          chain,
+          target: holder
+        })).output)
     }
 
-    // Pool Balances
-    let balanceOfResults = await sdk.api.abi.multiCall({
-      block,
-      calls: calls,
-      abi: 'erc20:balanceOf'
-    });
+    // let balances = {};
+    // let calls = [];
 
-    // Compute Balances
-    _.each(balanceOfResults.output, (balanceOf) => {
-        let address = balanceOf.input.target
-        let amount =  balanceOf.output
-        balances[address] = BigNumber(balances[address] || 0).plus(amount).toFixed()
-    });
+    
 
-    return balances;
-  }
+    for (const token of tokens) {
+      const bal = await sdk.api.erc20.balanceOf({
+        block,
+        chain,
+        target:token[0],
+        owner: holder
+      })
+      sdk.util.sumSingleBalance(balances, token[1], toNumber(bal.output))
+    }
+    return balances
+}
+
+// module.exports={
+//     methodology: "Same as compound, we just get all the collateral (not borrowed money) on the lending markets",
+//     tvl
+// }
+
+//     // Pool Balances
+//     let balanceOfResults = await sdk.api.abi.multiCall({
+//       block,
+//       calls: calls,
+//       abi: 'erc20:balanceOf'
+//     });
+
+//     // Compute Balances
+//     _.each(balanceOfResults.output, (balanceOf) => {
+//         let address = balanceOf.input.target
+//         let amount =  balanceOf.output
+//         balances[address] = BigNumber(balances[address] || 0).plus(amount).toFixed()
+//     });
+
+//     return balances;
+//   }
 
 /*==================================================
   Exports
@@ -59,8 +80,11 @@
   module.exports = {
     name: 'Mobius',   // project name
     website: "https://mobius.money",
-    token: null,              // null, or token symbol if project has a custom token
-    category: 'DEXes',        // allowed values as shown on DefiPulse: 'Derivatives', 'DEXes', 'Lending', 'Payments', 'Assets'
+    token: MOBI,              // Token symbol
+    category: 'DEXes',        // allowed values as shown on DefiPulse:
     start: 8606077,        // January 19, 2021 11:51:30 AM
     tvl                       // tvl adapter
   }
+
+  ///
+
