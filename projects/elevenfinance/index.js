86c24ea0afd79acef7bf198381fb62f9f4967d3c
Morazzela <jacquet2.clement@gmail.com>
2021-09-22 22:16:05 +0200

    exclude ELE pools

--- projects/elevenfinance/index.js
+++ projects/elevenfinance/index.js
@@ -2,15 +2,65 @@ const utils = require('../helper/utils');
 
 const apiUrl = 'https://eleven.finance/api.json';
 
+const excludedPools = {
+    'polygon': [
+        'ELE ',
+        'ELE-MUST cLP',
+        'ELE-QUICK qLP',
+        'ELE-MATIC cLP',
+        'ELE-MATIC qLP',
+        'ELE-MATIC SLP',
+        'ELE-MATIC WLP',
+        'ELE-USDC DLP',
+        'ELE-DFYN DLP',
+    ],
+    'fantom': [
+        'ELE-WFTM SLP',
+    ],
+    'bsc': [
+        'ELE',
+        'ELE-BNB WLP',
+        'ELE-BNB LP V2',
+    ],
+    'avax': [
+        'ELE-WAVAX TLP',
+        'ELE-WAVAX PLP',
+        'ELE-PNG PLP',
+        'ELE  ',
+    ],
+    'okexchain': [
+        'ELE-USDT PLP',
+    ],
+};
+
 async function fetchChain(chainId) {
     const response = await utils.fetchURL(apiUrl);
-    const tvl = parseFloat(response.tvlinfo[chainId]);
+    let tvl = parseFloat(response.data.tvlinfo[chainId]);
+
+    if (excludedPools[chainId] !== undefined) {
+        excludedPools[chainId].forEach((pool) => {
+            tvl -= parseFloat(response.data[pool]['tvl']);
+        })
+    }
+
     return Math.round(tvl);
 }
 
 async function fetch() {
     const response = await utils.fetchURL(apiUrl);
-    const tvl = parseFloat(response.totalvaluelocked);
+    let tvl = parseFloat(response.data.totalvaluelocked);
+
+    const chains = Object.keys(excludedPools)
+    chains.forEach((chainId) => {
+        if (excludedPools[chainId] === undefined) {
+            return;
+        }
+
+        excludedPools[chainId].forEach((pool) => {
+            tvl -= parseFloat(response.data[pool]['tvl']);
+        })
+    })
+
     return Math.round(tvl);
 }
 
