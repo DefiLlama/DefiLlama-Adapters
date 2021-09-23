3cd15f58711b962cdc9f38ce61a9a35ff405bc28
Gajesh2007 <robogajesh@gmail.com>
2021-09-23 16:12:31 +0530

    seperated non-native and native

--- projects/GajFinance/index.js
+++ projects/GajFinance/index.js
@@ -12,9 +12,8 @@ const NFTFARM_GAJ = '0xce52df6E9ca6db41DC4776B1735fdE60f5aD5012'
 const NFTFARM_GAJ_AVAX = '0x65096f7dB56fC27C7646f0aBb6F9bC0CEA2d8765'
 const JUNGLEPOOL = '0xD45AB9b5655D1A3d58162ed1a311df178C04ddDe'
 
-const GAJDFYNVAULT = '0xbf26b582680e7525da0e27ea9527bb0bf4f22de9'
-
-const endpoint = "https://gajvaultapi.herokuapp.com/vaults"
+const nativeEndpoint = "https://gajvaultapi.herokuapp.com/native"
+const nonNativeEndpoint = "https://gajvaultapi.herokuapp.com/nonNative"
 
 async function staking(timestamp, ethBlock, chainBlocks) {
   const balances = {};
@@ -23,15 +22,49 @@ async function staking(timestamp, ethBlock, chainBlocks) {
   return balances
 }
 
-async function vaults(timestamp, ethBlock, chainBlocks) {
+async function vaultsNonNative(timestamp, ethBlock, chainBlocks) {
     const balances = {};
-    const vaults = (await fetchURL(endpoint)).data.filter(v => {
+    const vaults = (await fetchURL(nativeEndpoint)).data.filter(v => {
       const pool2 = v.vaultName.includes("GAJ")
-      if(includePool2){
-        return pool2
-      } else{
-        return !pool2
-      }
+      return !pool2
+    })
+    const balance = (
+    await sdk.api.abi.multiCall({
+      abi: abi.balanceOfPool,
+      calls: vaults.map((vault) => ({
+        target: vault.contractAddress,
+      })),
+      block,
+      ...((chain == "avax" || chain == "polygon") && { chain }),
+    })
+  ).output.map((bal) => bal.output);
+  
+  
+  const lpPositions = [];
+  for (const vault of vaults) {
+      lpPositions.push({
+        token: vault.contractAddress,
+        balance: balance[vault.id],
+      });
+   }
+ 
+  await unwrapUniswapLPs(
+    balances,
+    lpPositions,
+    block,
+    chain,
+    chain == "avax"
+      ? await transformAvaxAddress()
+      : await transformPolygonAddress()
+  );
+  return balances
+}
+
+async function vaultsNative(timestamp, ethBlock, chainBlocks) {
+    const balances = {};
+    const vaults = (await fetchURL(nonNativeEndpoint)).data.filter(v => {
+      const pool2 = v.vaultName.includes("GAJ")
+      return pool2
     })
     const balance = (
     await sdk.api.abi.multiCall({
@@ -68,11 +101,11 @@ async function vaults(timestamp, ethBlock, chainBlocks) {
 module.exports = {
   misrepresentedTokens: true,
   methodology: "TVL comes from NFT Farming, Jungle Pools, MasterChef and Vaults",
-  tvl: vaults,
+  tvl: vaultsNonNative,
   staking: {
     tvl: staking
   },
   pool2:{
-    tvl: vaults(true)
+    tvl: vaultsNative
   }
 }
