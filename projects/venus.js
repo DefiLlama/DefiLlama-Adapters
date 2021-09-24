ebf3861d9ec3864dbd7838fc4955e43a96f29cd3
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-09-24 06:44:56 +0000

    add methodology to venus
    add methodology to venus

--- projects/venus.js
+++ projects/venus.js
@@ -15,5 +15,6 @@ async function fetch() {
 
 
 module.exports = {
-  fetch
+  methodology: 'TVL is comprised of tokens deposited to the protocol as collateral, similar to Compound Finance and other lending protocols the borrowed tokens are not counted as TVL.',
+  fetch,
 }
