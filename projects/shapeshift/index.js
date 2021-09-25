dd733f9d51000873811163e4db4db9f7b6d8fd63
Mohammed18062020 <mohamed_el_hamdaoui93@hotmail.com>
2021-09-25 22:47:16 +0100

    Shapeshift Protocol updating && Fractional-art abi.json file deleting

--- projects/shapeshift/index.js
+++ projects/shapeshift/index.js
@@ -1,5 +1,6 @@
 const sdk = require("@defillama/sdk");
-const { sumTokensAndLPsSharedOwners } = require("../helper/unwrapLPs");
+const { pool2 } = require("../helper/pool2");
+const {staking} = require('../helper/staking');
 
 // Contracts
 const StakingYieldContract = "0xDd80E21669A664Bce83E3AD9a0d74f8Dad5D9E72";
@@ -8,35 +9,14 @@ const StakingYieldContract = "0xDd80E21669A664Bce83E3AD9a0d74f8Dad5D9E72";
 const ETH_FOX_UNIV2 = "0x470e8de2eBaef52014A47Cb5E6aF86884947F08c";
 const FOX = "0xc770eefad204b5180df6a14ee197d99d808ee52d";
 
-const calcTvl = async (tokenOrLp, bool) => {
-  const balances = {};
-  let chainBlocks = {};
-
-  await sumTokensAndLPsSharedOwners(
-    balances,
-    [[tokenOrLp, bool]],
-    [StakingYieldContract],
-    chainBlocks["ethereum"],
-    "ethereum",
-    (addr) => addr
-  );
-
-  return balances;
-};
-
-/*** Staking of native token (FOX) TVL portion ***/
-const staking = async () => {
-  return calcTvl(FOX, false);
-};
-
-const ethTvl = async () => {
-  return calcTvl(ETH_FOX_UNIV2, true);
+const ethTvl = async (...params) => {
+  return pool2(StakingYieldContract, ETH_FOX_UNIV2)(...params);
 };
 
 module.exports = {
   misrepresentedTokens: true,
   staking: {
-    tvl: staking,
+    tvl: staking(StakingYieldContract, FOX)
   },
   ethereum: {
     tvl: ethTvl,
