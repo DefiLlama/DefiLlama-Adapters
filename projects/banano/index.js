03870568353dd917aecae73a9062c4588d8a89fe
boopowo <cody1774@gmail.com>
2021-09-26 19:54:34 -0500

    Rewrote the code to use the sdk and helpers

--- projects/banano/index.js
+++ projects/banano/index.js
@@ -1,52 +1,61 @@
-const retry = require('async-retry');
-const axios = require("axios");
-const BigNumber = require("bignumber.js");
+const sdk = require("@defillama/sdk");
+const {unwrapUniswapLPs} = require('../helper/unwrapLPs')
 
+const ownerPolygon = "0xefa4aED9Cf41A8A0FcdA4e88EfA2F60675bAeC9F"
+const polygonLp = "0xb556feD3B348634a9A010374C406824Ae93F0CF8"
 
+async function polygonTvl(timestamp, block, chainBlocks) {
+    const balances = {}
+    
+    const lpBalance = await sdk.api.erc20.balanceOf({
+        target: polygonLp,
+        owner: ownerPolygon,
+        block: chainBlocks.polygon,
+        chain: 'polygon'
+    })
 
-async function polygon(){
+    await unwrapUniswapLPs(balances, [{
+        token: polygonLp,
+        balance: lpBalance.output
+    }], chainBlocks.polygon, 'polygon', addr=>`polygon:${addr}`)
 
-    const contract = "0xb556feD3B348634a9A010374C406824Ae93F0CF8";
-    const wBan = "0xe20b9e246db5a0d21bf9209e4858bc9a3ff7a034";
-    const wEth = "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
-
-    let banPrice = await retry(async bail => await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=banano&vs_currencies=usd"));
-    let ethPrice = await retry(async bail => await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"));
-    let wBanBalance = await retry(async bail => await axios.get(`https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=${wBan}&address=${contract}&tag=latest&apikey=API_KEY`));
-    let wEthBalance = await retry(async bail => await axios.get(`https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=${wEth}&address=${contract}&tag=latest&apikey=API_KEY`));
-    let wBanTotal = new BigNumber(wBanBalance.data.result).div(10 ** 18).toFixed(2) * banPrice.data.banano.usd;
-    let wEthTotal = new BigNumber(wEthBalance.data.result).div(10 ** 18).toFixed(2) * ethPrice.data.ethereum.usd;
-
-    return wBanTotal + wEthTotal;   
+  return balances;
 }
 
-async function bsc() {
-    const contract = "0x7898466CACf92dF4a4e77a3b4d0170960E43b896";
-    const wBan = "0xe20b9e246db5a0d21bf9209e4858bc9a3ff7a034";
-    const bUsd = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
+ownerBsc = "0x1E30E12e82956540bf870A40FD1215fC083a3751";
+// ENDED POOLS
+// apeswapWbanBnb = "0x6011c6BAe36F2a2457dC69Dc49068a1E8Ad832DD";
+// apeswapWbanBusd = "0x7898466CACf92dF4a4e77a3b4d0170960E43b896";
+pancakeSwapWbanBusd = "0x351A295AfBAB020Bc7eedcB7fd5A823c01A95Fda";
 
-    let banPrice = await retry(async bail => await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=banano&vs_currencies=usd"));
-    let bUsdPrice = await retry(async bail => await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=binance-usd&vs_currencies=usd"));
-    let wBanBalance = await retry(async bail => await axios.get(`https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${wBan}&address=${contract}&tag=latest&apikey=API_KEY`))
-    let bUsdBalance = await retry(async bail => await axios.get(`https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${bUsd}&address=${contract}&tag=latest&apikey=API_KEY`))
-    let wBanTotal = new BigNumber(wBanBalance.data.result).div(10 ** 18).toFixed(2) * banPrice.data.banano.usd; 
-    let wUsdTotal = new BigNumber(bUsdBalance.data.result).div(10 ** 18).toFixed(2) * bUsdPrice.data['binance-usd'].usd;
+const bscLps = [pancakeSwapWbanBusd];
 
-    return wBanTotal + wUsdTotal;
+async function bscTvl(timestamp, block, chainBlocks) {
+    const balances = {}
 
-}
+    for(const pool of bscLps) {
+        const lpBalance = await sdk.api.erc20.balanceOf({
+            target: pool,
+            owner: ownerBsc,
+            block: chainBlocks.bsc,
+            chain: 'bsc'
+        })
 
-async function fetch() {
-    return new BigNumber(await bsc()).plus(await polygon()).toFixed(2);
+        await unwrapUniswapLPs(balances, [{
+            token: pool,
+            balance: lpBalance.output
+        }], chainBlocks.bsc, 'bsc', addr=>`bsc:${addr}`)
+    }
+
+    return balances;
 }
 
 module.exports = {
-    bsc: {
-        fetch: bsc
-    },
     polygon: {
-        fetch: polygon
+        tvl: polygonTvl
     },
-    fetch,
-    
+    bsc: {
+        tvl: bscTvl
+    },
+    tvl: sdk.util.sumChainTvls([polygonTvl, bscTvl])
 }
