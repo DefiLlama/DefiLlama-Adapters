cf32652265bd78d7938b771dac7b6581c27344cf
0xngmi <0xngmi@protonmail.com>
2021-09-27 05:22:21 +0100

    make bakeryswap use calculateUniTvl

--- projects/bakeryswap/index.js
+++ projects/bakeryswap/index.js
@@ -1,14 +1,10 @@
 const sdk = require("@defillama/sdk");
-const tvlOnPairs = require("../helper/processPairs.js");
+const {calculateUniTvl} = require("../helper/calculateUniTvl");
 
 const factory = "0x01bF7C66c6BD861915CdaaE475042d3c4BaE16A7";
 
 const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
-  const balances = {};
-
-  await tvlOnPairs("bsc", chainBlocks, factory, balances);
-
-  return balances;
+  return calculateUniTvl(addr=>`bsc:${addr}`, chainBlocks.bsc, "bsc", factory, 0, true);
 };
 
 module.exports = {
