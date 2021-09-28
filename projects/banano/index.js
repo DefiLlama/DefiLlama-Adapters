c1fe8bf4338133c07098210d55d987ccb9b73494
boopowo <cody1774@gmail.com>
2021-09-27 22:03:59 -0500

    uses pool2.js to calculate tvl

--- projects/banano/index.js
+++ projects/banano/index.js
@@ -1,61 +1,19 @@
 const sdk = require("@defillama/sdk");
-const {unwrapUniswapLPs} = require('../helper/unwrapLPs')
+const {pool2} = require("../helper/pool2.js");
 
-const ownerPolygon = "0xefa4aED9Cf41A8A0FcdA4e88EfA2F60675bAeC9F"
+//Polygon and BSC owners
+const polygonContract = "0xefa4aED9Cf41A8A0FcdA4e88EfA2F60675bAeC9F";
+const bscContract = "0x1E30E12e82956540bf870A40FD1215fC083a3751";
+
+//Polygon and BSC LPs
 const polygonLp = "0xb556feD3B348634a9A010374C406824Ae93F0CF8"
+const bscLp = "0x351A295AfBAB020Bc7eedcB7fd5A823c01A95Fda";
 
-async function polygonTvl(timestamp, block, chainBlocks) {
-    const balances = {}
-    
-    const lpBalance = await sdk.api.erc20.balanceOf({
-        target: polygonLp,
-        owner: ownerPolygon,
-        block: chainBlocks.polygon,
-        chain: 'polygon'
-    })
-
-    await unwrapUniswapLPs(balances, [{
-        token: polygonLp,
-        balance: lpBalance.output
-    }], chainBlocks.polygon, 'polygon', addr=>`polygon:${addr}`)
-
-  return balances;
-}
-
-ownerBsc = "0x1E30E12e82956540bf870A40FD1215fC083a3751";
-// ENDED POOLS
-// apeswapWbanBnb = "0x6011c6BAe36F2a2457dC69Dc49068a1E8Ad832DD";
-// apeswapWbanBusd = "0x7898466CACf92dF4a4e77a3b4d0170960E43b896";
-pancakeSwapWbanBusd = "0x351A295AfBAB020Bc7eedcB7fd5A823c01A95Fda";
-
-const bscLps = [pancakeSwapWbanBusd];
-
-async function bscTvl(timestamp, block, chainBlocks) {
-    const balances = {}
-
-    for(const pool of bscLps) {
-        const lpBalance = await sdk.api.erc20.balanceOf({
-            target: pool,
-            owner: ownerBsc,
-            block: chainBlocks.bsc,
-            chain: 'bsc'
-        })
-
-        await unwrapUniswapLPs(balances, [{
-            token: pool,
-            balance: lpBalance.output
-        }], chainBlocks.bsc, 'bsc', addr=>`bsc:${addr}`)
-    }
-
-    return balances;
-}
+let polygonPool2 = pool2(polygonContract, polygonLp, "polygon", addr=>`polygon:${addr}`);
+let bscPool2= pool2(bscContract, bscLp, "bsc", addr=>`bsc:${addr}`);
 
 module.exports = {
-    polygon: {
-        tvl: polygonTvl
-    },
-    bsc: {
-        tvl: bscTvl
-    },
-    tvl: sdk.util.sumChainTvls([polygonTvl, bscTvl])
+    pool2:{
+        tvl: sdk.util.sumChainTvls([polygonPool2,bscPool2])
+    }
 }
