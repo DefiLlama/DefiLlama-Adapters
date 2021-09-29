47d331d05eac74686466b6d50da5502fa2bfaee6
0xngmi <0xngmi@protonmail.com>
2021-09-29 05:02:16 +0100

    add waves exchange

---
+++ projects/waves-exchange/index.js
@@ -0,0 +1,7 @@
+const {wavesAdapter} = require('../helper/wavesAdapter')
+
+const endpoint = "http://51.158.191.108:8002/api/v1/history/waves-exchange"
+
+module.exports={
+    tvl: wavesAdapter(endpoint, item => item.investmentsLocked)
+}
