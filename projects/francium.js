f35621252556d9568cd5a55d1003e999cd13741e
Francium-DeFi <84317711+Francium-DeFi@users.noreply.github.com>
2021-09-29 20:45:38 +0800

    create francium

---
+++ projects/francium.js
@@ -0,0 +1,18 @@
+const retry = require("./helper/retry");
+const axios = require("axios");
+
+async function fetch() {
+  const response = (
+    await retry(async (bail) => await axios.get("https://francium.io/api/pools/liquidity"))
+  ).data.data;
+
+  const poolLiqArray = response.farm.map(pool => pool.liquidityLocked);
+  const lendArray = response.lend.map(pool => pool.available);
+  const tvl = [...poolLiqArray, ...lendArray, response.old].reduce((a, b) => a + b, 0);
+
+  return tvl || 0;
+}
+
+module.exports = {
+  fetch,
+};
