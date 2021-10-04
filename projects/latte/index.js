f4bc4573d3a446e0b272d31783c345b52e3c6c4e
cappuccino <cappuccino.kub@gmail.com>
2021-10-04 16:29:40 +0700

    feat: separate TVL and Latte Pool TVL

--- projects/latte/index.js
+++ projects/latte/index.js
@@ -1,8 +1,9 @@
 const utils = require('../helper/utils');
 
 async function fetch() {
-  var totalTvl = await utils.fetchURL('https://api.latteswap.com/api/v1/amm/total-value-locked')
-  return totalTvl.data
+  var totalTvl = await utils.fetchURL('https://api.latteswap.com/api/v1/amm/defi-llama/tvl-exclude-latte')
+  var latteTvl = await utils.fetchURL('https://api.latteswap.com/api/v1/amm/defi-llama/tvl-latte-pool')
+  return Number(totalTvl.data) + Number(latteTvl.data)
 }
 
 module.exports = {
