7f01049354bef2756baf511d7ba6120367bad31d
cappuccino <cappuccino.kub@gmail.com>
2021-10-04 16:42:40 +0700

    refactor: format string URL

--- projects/latte/index.js
+++ projects/latte/index.js
@@ -2,8 +2,8 @@ const utils = require('../helper/utils');
 
 const baseURL = 'https://api.latteswap.com/api'
 async function fetch() {
-  const totalTvl = await utils.fetchURL(baseURL.concat('/v1/amm/defi-llama/tvl-exclude-latte'))
-  const latteTvl = await utils.fetchURL(baseURL.concat('/v1/amm/defi-llama/tvl-latte-pool'))
+  const totalTvl = await utils.fetchURL(`${baseURL}/v1/amm/defi-llama/tvl-exclude-latte`)
+  const latteTvl = await utils.fetchURL(`${baseURL}/v1/amm/defi-llama/tvl-latte-pool`)
   return Number(totalTvl.data) + Number(latteTvl.data)
 }
 
