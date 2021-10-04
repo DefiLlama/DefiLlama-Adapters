1d79d8bddeed6a40127a42d7523c8d78c3700af5
waynebruce0x <waynebruce0x@gmail.com>
2021-10-04 22:57:38 +0100

    avaware patch

--- projects/adamantfinance/index.js
+++ projects/adamantfinance/index.js
@@ -9,47 +9,7 @@ const vaultsUrl = {
   "polygon": "https://raw.githubusercontent.com/eepdev/vaults/main/current_vaults.json",
   "arbitrum": "https://raw.githubusercontent.com/eepdev/vaults/main/arbitrum_vaults.json"
 };
-const current_vaults_url = "https://raw.githubusercontent.com/eepdev/vaults/main/current_vaults.json"
-const polygonTvl3 = async (timestamp, block, chainBlocks) => {
-  const balances = {};
 
-  let vaults = (await utils.fetchURL(current_vaults_url)).data.filter(vault=>vault.token0!==vault.token1).map((vault) => ({
-    vaultAddress: vault.vaultAddress,
-    lpAddress: vault.lpAddress,
-  }));
-
-  const vault_balances = (
-    await sdk.api.abi.multiCall({
-      chain: "polygon",
-      block: chainBlocks["polygon"],
-      calls: vaults.map((vault) => ({
-        target: vault.vaultAddress,
-      })),
-      abi: abi.balance,
-    })
-  ).output.map((val) => val.output);
-
-  const lpPositions = [];
-
-  vaults.forEach((v, idx) => {
-    lpPositions.push({
-      balance: vault_balances[idx],
-      token: v.lpAddress,
-    });
-  });
-
-  const transformAddress = await transformPolygonAddress();
-
-  await unwrapUniswapLPs(
-    balances,
-    lpPositions,
-    chainBlocks["polygon"],
-    "polygon",
-    transformAddress
-  );
-
-  return balances;
-};
 async function polygonTvl(timestamp, block, chainBlocks) {
   const transformAddress = await transformPolygonAddress();
   return await tvl(timestamp, 'polygon', chainBlocks, transformAddress);
@@ -62,9 +22,11 @@ const tvl = async (timestamp, chain, chainBlocks, transformAddress=a=>a) => {
   const block = await getBlock(timestamp, chain, chainBlocks);
   const balances = {};
 
-  let vaults = (await utils.fetchURL(vaultsUrl[chain])).data.filter(vault=>vault.token0!==vault.token1 && vault.vaultAddress !== "" && vault.platform !== "dodo").map((vault) => ({
-    vaultAddress: vault.vaultAddress,
-    lpAddress: vault.lpAddress,
+  let vaults = (await utils.fetchURL(vaultsUrl[chain])).data.filter(
+    vault => vault.token0 !== vault.token1 && vault.vaultAddress !== "" 
+    && vault.platform !== "dodo").map((vault) => ({
+      vaultAddress: vault.vaultAddress,
+      lpAddress: vault.lpAddress,
   }));
 
   const vault_balances = (
@@ -105,7 +67,6 @@ module.exports = {
   arbitrum: {
     tvl: arbitrumTvl,
   },
-  tvl: sdk.util.sumChainTvls([polygonTvl]),//, arbitrumTvl]),
+  tvl: sdk.util.sumChainTvls([polygonTvl, arbitrumTvl]),
   methodology: 'The current vaults on Adamant Finance are found on the Github. Once we have the vaults, we filter out the LP addresses of each vault and unwrap the LPs so that each token can be accounted for. Coingecko is used to price the tokens and the sum of all tokens is provided as the TVL'
 };
-// node test.js projects/adamantfinance/index.js
