1e0d4abc78c81858ff5546e88603ead4510a2075
amir nouri <anasvsfs@gmail.com>
2021-10-06 09:19:36 +0330

    better error handling

--- projects/totemfi/index.js
+++ projects/totemfi/index.js
@@ -16,36 +16,41 @@ const graphQuery = gql`
   }
   `;
 
-async function tvl(timestamp, block) {
-  let balances = {};
-  try {
-    let allPrizePools = []
-    for (const graphUrl of graphUrls) {
-      const { stakingPools } = await request(
-        graphUrl,
-        graphQuery,
-        {
-          block,
-          timestamp
-        }
-      );
-      allPrizePools = allPrizePools.concat(stakingPools)
-    }
-    let sumTOTM = BigNumber.from(0)
-    for (let i = 0; i < allPrizePools.length; i++) {
-      if (allPrizePools[i]["totalUnStaked"] == null) {
-        allPrizePools[i]["totalUnStaked"] = 0
+  async function tvl(timestamp, block) {
+    let balances = {};
+    try {
+      let allPrizePools = []
+      for (const graphUrl of graphUrls) {
+        const { stakingPools } = await request(
+          graphUrl,
+          graphQuery,
+          {
+            block,
+            timestamp
+          }
+        );
+        allPrizePools = allPrizePools.concat(stakingPools)
       }
-      let balance = BigNumber.from(allPrizePools[i]["totalStaked"]).sub(BigNumber.from(allPrizePools[i]["totalUnStaked"]))
-      sumTOTM = sumTOTM.add(balance)
+      let sumTOTM = BigNumber.from(0)
+      for (let i = 0; i < allPrizePools.length; i++) {
+        if (allPrizePools[i]["totalUnStaked"] == null) {
+          allPrizePools[i]["totalUnStaked"] = 0
+        }
+        let balance = BigNumber.from(allPrizePools[i]["totalStaked"]).sub(BigNumber.from(allPrizePools[i]["totalUnStaked"]))
+        sumTOTM = sumTOTM.add(balance)
+      }
+      balances['TOTM'] = sumTOTM.toString()
+      return balances
+    } catch (error) {
+      if (error.message !== undefined) {
+        if (error.message.includes("decode") && error.response?.status == 200) {
+          balances['TOTM'] = "0"
+          return balances
+        }
+      }
+      throw error
     }
-    balances['TOTM'] = sumTOTM.toString()
-    return balances
-  } catch (error) {
-    balances['TOTM'] = "0"
-    return balances
   }
-}
 
 
 module.exports = {
