731445408ee27a5879a0215474ffc19dde047b8a
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-10-06 15:15:06 +0000

    add methodology to Aave
    add methodology to Aave

--- projects/aave/index.js
+++ projects/aave/index.js
@@ -307,6 +307,7 @@ const {getV2Reserves, getV2Tvl, aaveChainTvl} = require('../helper/aave')
   }
 
   module.exports = {
+    methodology: `Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There's multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending`,
     ethereum:{
       staking,
       tvl: ethereum
