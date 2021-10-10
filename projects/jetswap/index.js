2d53f829168132127d285eac94113f907208b49f
0xngmi <0xngmi@protonmail.com>
2021-10-11 00:00:52 +0100

    simplify jetswap

--- projects/jetswap/index.js
+++ projects/jetswap/index.js
@@ -1,77 +1,23 @@
-const sdk = require("@defillama/sdk");
-const retry = require("async-retry");
-const { GraphQLClient, gql } = require("graphql-request");
-const { toUSDTBalances } = require("../helper/balances");
+const { staking } = require("../helper/staking");
+const { getChainTvl } = require("../helper/getUniSubgraphTvl");
+const {uniTvlExport} = require('../helper/calculateUniTvl')
 
-async function bscTvl() {
-  var endpoint =
-    "https://api.thegraph.com/subgraphs/name/smartcookie0501/jetswap-subgraph";
-  var graphQLClient = new GraphQLClient(endpoint);
-
-  var query = gql`
-    {
-      uniswapFactories(first: 5) {
-        totalLiquidityUSD
-      }
-    }
-  `;
-  const results = await retry(
-    async (bail) => await graphQLClient.request(query)
-  );
-  return toUSDTBalances(
-    Number(results.uniswapFactories[0].totalLiquidityUSD)
-  );
-}
-
-async function polygonTvl() {
-  var endpoint =
-    "https://api.thegraph.com/subgraphs/name/smartcookie0501/jetswap-subgraph-polygon";
-  var graphQLClient = new GraphQLClient(endpoint);
-
-  var query = gql`
-    {
-      uniswapFactories(first: 5) {
-        totalLiquidityUSD
-      }
-    }
-  `;
-  const results = await retry(
-    async (bail) => await graphQLClient.request(query)
-  );
-  return toUSDTBalances(
-    Number(results.uniswapFactories[0].totalLiquidityUSD)
-  );
-}
-
-async function fantomTvl() {
-  var endpoint =
-    "https://api.thegraph.com/subgraphs/name/smartcookie0501/jetswap-subgraph-fantom";
-  var graphQLClient = new GraphQLClient(endpoint);
-
-  var query = gql`
-    {
-      uniswapFactories(first: 5) {
-        totalLiquidityUSD
-      }
-    }
-  `;
-  const results = await retry(
-    async (bail) => await graphQLClient.request(query)
-  );
-  return toUSDTBalances(
-    Number(results.uniswapFactories[0].totalLiquidityUSD)
-  );
-}
+const chainTvls = getChainTvl({
+  fantom: "https://api.thegraph.com/subgraphs/name/smartcookie0501/jetswap-subgraph-fantom",
+  polygon: "https://api.thegraph.com/subgraphs/name/smartcookie0501/jetswap-subgraph-polygon",
+  bsc: "https://api.thegraph.com/subgraphs/name/smartcookie0501/jetswap-subgraph"
+})
 
 module.exports = {
+  misrepresentedTokens: true,
   bsc: {
-    tvl: bscTvl,
+    tvl: chainTvls('bsc'),
   },
   polygon: {
-    tvl: polygonTvl,
+    tvl: uniTvlExport('0x668ad0ed2622c62e24f0d5ab6b6ac1b9d2cd4ac7','polygon'),
   },
   fantom: {
-    tvl: fantomTvl,
+    staking: staking("0x9180583C1ab03587b545629dd60D2be0bf1DF4f2", "0x3D8f1ACCEe8e263F837138829B6C4517473d0688","fantom", "bsc:0x0487b824c8261462f88940f97053e65bdb498446"),
+    tvl: chainTvls('fantom'),
   },
-  tvl: sdk.util.sumChainTvls([bscTvl, polygonTvl, fantomTvl]),
 };
