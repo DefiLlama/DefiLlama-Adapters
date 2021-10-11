4e13511691ca61025398de8c5e85ad1eac3742f0
0xngmi <0xngmi@protonmail.com>
2021-10-11 02:45:01 +0100

    move only1 to staking

--- projects/only1.js
+++ projects/only1.js
@@ -1,21 +1,17 @@
 const axios = require("axios")
 const retry = require('async-retry')
 
-async function fetch() {
-  const [tvlResponse, coingeckoResponse] = await Promise.all([
-    "https://us-central1-only1-staking-stats.cloudfunctions.net/tvl",
-    "https://api.coingecko.com/api/v3/simple/price?ids=only1&vs_currencies=usd"
-  ].map((url) => retry(async () => {
-    const { data } = await axios.get(url)
+async function staking() {
+  const { data } = await axios.get("https://us-central1-only1-staking-stats.cloudfunctions.net/tvl")
 
-    return data
-  })))
-  const tvl = tvlResponse.totalTvl
-  const tokenPrice = coingeckoResponse.only1.usd
-
-  return tokenPrice * tvl
+  return {
+    'only1': data.totalTvl
+  }
 }
 
 module.exports = {
-  fetch
+  solana: {
+    staking,
+    tvl: async () => ({})
+  }
 }
