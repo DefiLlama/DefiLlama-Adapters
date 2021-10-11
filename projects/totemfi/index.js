257213a7121b84059ad32b01a7f85e0acb500261
amir nouri <anasvsfs@gmail.com>
2021-10-11 08:56:23 +0330

    using bscBlock

--- projects/totemfi/index.js
+++ projects/totemfi/index.js
@@ -15,42 +15,43 @@ const graphQuery = gql`
     }
   }
   `;
-const TOTM = "0x6FF1BFa14A57594a5874B37ff6AC5efbD9F9599A"
-  async function tvl(timestamp, block) {
-    let balances = {};
-    try {
-      let allPrizePools = []
-      for (const graphUrl of graphUrls) {
-        const { stakingPools } = await request(
-          graphUrl,
-          graphQuery,
-          {
-            block,
-            timestamp
-          }
-        );
-        allPrizePools = allPrizePools.concat(stakingPools)
-      }
-      let sumTOTM = BigNumber.from(0)
-      for (let i = 0; i < allPrizePools.length; i++) {
-        if (allPrizePools[i]["totalUnStaked"] == null) {
-          allPrizePools[i]["totalUnStaked"] = 0
+const TOTM = "bsc:0x6FF1BFa14A57594a5874B37ff6AC5efbD9F9599A"
+async function tvl(timestamp, ethBlock, chainBlocks) {
+  const block = chainBlocks["bsc"];
+  let balances = {};
+  try {
+    let allPrizePools = []
+    for (const graphUrl of graphUrls) {
+      const { stakingPools } = await request(
+        graphUrl,
+        graphQuery,
+        {
+          block,
+          timestamp
         }
-        let balance = BigNumber.from(allPrizePools[i]["totalStaked"]).sub(BigNumber.from(allPrizePools[i]["totalUnStaked"]))
-        sumTOTM = sumTOTM.add(balance)
-      }
-      balances[TOTM] = sumTOTM.toString()
-      return balances
-    } catch (error) {
-      if (error.message !== undefined) {
-        if (error.message.includes("decode") && error.response?.status == 200) {
-          balances[TOTM] = "0"
-          return balances
-        }
-      }
-      throw error
+      );
+      allPrizePools = allPrizePools.concat(stakingPools)
     }
+    let sumTOTM = BigNumber.from(0)
+    for (let i = 0; i < allPrizePools.length; i++) {
+      if (allPrizePools[i]["totalUnStaked"] == null) {
+        allPrizePools[i]["totalUnStaked"] = 0
+      }
+      let balance = BigNumber.from(allPrizePools[i]["totalStaked"]).sub(BigNumber.from(allPrizePools[i]["totalUnStaked"]))
+      sumTOTM = sumTOTM.add(balance)
+    }
+    balances[TOTM] = sumTOTM.toString()
+    return balances
+  } catch (error) {
+    if (error.message !== undefined) {
+      if (error.message.includes("decode") && error.response?.status == 200) {
+        balances[TOTM] = "0"
+        return balances
+      }
+    }
+    throw error
   }
+}
 
 
 module.exports = {
