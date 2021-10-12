d116dc6d0a07b66fe4fceb5921e6cbccb212a28a
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-10-12 16:36:37 +0000

    add methodology to almond
    add methodology to almond

--- projects/almond.js
+++ projects/almond.js
@@ -14,5 +14,6 @@ async function fetch() {
 }
 
 module.exports = {
+  methodology: "The pools on https://almond.so/ are included in TVL. Standard tokens (SOL, BTC, ETH, USDC, etc..), no pool 2. The API fetches the balances of the Solana accounts holding the staked tokens, then uses Coingecko prices to determine dollar values.",
   fetch,
 };
