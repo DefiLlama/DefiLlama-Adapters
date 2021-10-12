6040ffd35300ee56a9636cf13a1a6f525518a83f
0xngmi <0xngmi@protonmail.com>
2021-10-12 05:40:37 +0100

    fail hard on serum

--- projects/serum.js
+++ projects/serum.js
@@ -2,7 +2,6 @@ const retry = require('./helper/retry');
 const axios = require('axios');
 
 async function fetch() {
-    try {
         const response = (
             await retry(
                 async (bail) => await axios.get('https://api.serum-data.com/tvl')
@@ -11,10 +10,7 @@ async function fetch() {
 
         const tvl = response.result.tvl;
 
-        return tvl || 0;
-    } catch (e) {
-        return 0;
-    }
+        return tvl
 }
 
 module.exports = {
