025beb26426d3d30ac8a1278a6fc79529e87efa2
0xngmi <0xngmi@protonmail.com>
2021-10-13 03:45:47 +0100

    simplify artemis

--- projects/artemis/index.js
+++ projects/artemis/index.js
@@ -1,36 +1,31 @@
 const { default: axios } = require("axios");
+const { toUSDTBalances } = require("../helper/balances");
 const retry = require("../helper/retry");
 
 const farmApi = "https://api.elision.farm/getFarmStats/harmony/artemis";
 
-async function harmonyTvl() {
-  let tvl = 0;
-  let { data: farms } = await retry(async (bail) => await axios.get(farmApi));
-  for (let i = 0; i < farms.length; i++) {
-    if (!farms[i].name.startsWith("MIS") && !farms[i].name.endsWith("MIS")) {
-      tvl += farms[i].farm_liquidity_usd;
+function tvl(pool2) {
+  return async () => {
+    let tvl = 0;
+    let { data: farms } = await retry(async (bail) => await axios.get(farmApi));
+    for (let i = 0; i < farms.length; i++) {
+      if (farms[i].name.startsWith("MIS") || farms[i].name.endsWith("MIS")) {
+        if(pool2){
+          tvl += farms[i].farm_liquidity_usd;
+        }
+      } else {
+        if(!pool2){
+          tvl += farms[i].farm_liquidity_usd;
+        }
+      }
     }
+    return toUSDTBalances(tvl);
   }
-  return tvl;
-}
-
-async function harmonyPool2Tvl() {
-  let tvl = 0;
-  let { data: farms } = await retry(async (bail) => await axios.get(farmApi));
-  for (let i = 0; i < farms.length; i++) {
-    if (farms[i].name.startsWith("MIS") || farms[i].name.endsWith("MIS")) {
-      tvl += farms[i].farm_liquidity_usd;
-    }
-  }
-  return tvl;
 }
 
 module.exports = {
   harmony: {
-    fetch: harmonyTvl,
-  },
-  pool2: {
-    fetch: harmonyPool2Tvl,
-  },
-  fetch: harmonyTvl,
+    pool2: tvl(true),
+    tvl: tvl(false),
+  }
 };
