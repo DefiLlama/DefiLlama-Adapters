0c864466b0bfd386cb78704d9974270149c9b1e0
Pasta Ghost <pastaghost@2520.io>
2021-10-13 11:27:35 -0600

    Track balances of both ShapeShift staking contracts

--- projects/shapeshift/index.js
+++ projects/shapeshift/index.js
@@ -3,17 +3,18 @@ const {staking} = require('../helper/staking');
 
 // Contracts
 const StakingYieldContract = "0xDd80E21669A664Bce83E3AD9a0d74f8Dad5D9E72";
+const StakingYieldContractV2 = "0xc54b9f82c1c54e9d4d274d633c7523f2299c42a0";
 
 // Tokens Or LPs
 const ETH_FOX_UNIV2 = "0x470e8de2eBaef52014A47Cb5E6aF86884947F08c";
 const FOX = "0xc770eefad204b5180df6a14ee197d99d808ee52d";
 
 module.exports = {
-  staking: {
-    tvl: staking(StakingYieldContract, FOX)
-  },
-  pool2:{
+  stakingV1: {
     tvl: pool2(StakingYieldContract, ETH_FOX_UNIV2)
+  },
+  stakingV2:{
+    tvl: pool2(StakingYieldContractV2, ETH_FOX_UNIV2)
   },
   tvl: async ()=>({}),
   methodology:
