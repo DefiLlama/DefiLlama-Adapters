76a23521ee05a75d02b1e58eed495950304e9ae4
0xngmi <0xngmi@protonmail.com>
2021-10-14 19:34:24 +0100

    add thales staking and pool2

--- projects/helper/pool2.js
+++ projects/helper/pool2.js
@@ -1,4 +1,7 @@
 const { sumTokensAndLPs } = require('./unwrapLPs')
+const {_BASE_TOKEN_, _QUOTE_TOKEN_} = require('./abis/dodo.json')
+const sdk = require('@defillama/sdk')
+const { default: BigNumber } = require('bignumber.js')
 
 function pool2(stakingContract, lpToken, chain = "ethereum", transformAddress) {
     return async (_timestamp, _ethBlock, chainBlocks) => {
@@ -25,7 +28,33 @@ function pool2Exports(stakingContract, lpTokens, chain = "ethereum", transformAd
     }
 }
 
+function dodoPool2(stakingContract, lpToken, chain = "ethereum", transformAddress=addr=>addr) {
+    return async (_timestamp, _ethBlock, chainBlocks) => {
+        const balances = {}
+        const block = chainBlocks[chain]
+        const [baseToken, quoteToken, totalSupply] = await Promise.all([_BASE_TOKEN_, _QUOTE_TOKEN_, "erc20:totalSupply"].map(abi => sdk.api.abi.call({
+            target: lpToken,
+            chain,
+            block,
+            abi
+        }).then(r=>r.output)))
+        const [baseTokenBalance, quoteTokenBalance, stakedLPBalance] = await Promise.all([
+            [baseToken, lpToken], [quoteToken, lpToken], [lpToken, stakingContract]
+        ].map(token => sdk.api.abi.call({
+            target: token[0],
+            params: [token[1]],
+            chain,
+            block,
+            abi: 'erc20:balanceOf'
+        }).then(r=>r.output)))
+        sdk.util.sumSingleBalance(balances, baseToken, BigNumber(baseTokenBalance).times(stakedLPBalance).div(totalSupply).toFixed(0))
+        sdk.util.sumSingleBalance(balances, quoteToken, BigNumber(quoteTokenBalance).times(stakedLPBalance).div(totalSupply).toFixed(0))
+        return balances
+    }
+}
+
 module.exports = {
     pool2,
-    pool2Exports
+    pool2Exports,
+    dodoPool2
 }
