5ffcfadd59b74b43e13b9f9a120355b212327295
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-10-14 09:53:40 +0000

    correct new pools for mercurial

--- projects/mercurial.js
+++ projects/mercurial.js
@@ -55,28 +55,28 @@ async function tvl() {
     //wusd4pool
     getTokenBalance(
       "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
-      "54q2ct7kTknGvADuHSXjtnKqMbmNQ4xpDVK2xgcnh1xv"
+      "3m15qNJDM5zydsYNJzkFYXE7iGCVnkKz1mrmbawrDUAH"
     ),
     getTokenBalance(
       "A9mUU4qviSctJVPJdBJWkb28deg915LYJKrzQ19ji3FM",
-      "5cvqiPREvEYmhvBt3cZ7fmrCE6tbYvwkAiuvf1pHUPBq"
+      "3m15qNJDM5zydsYNJzkFYXE7iGCVnkKz1mrmbawrDUAH"
     ),
     getTokenBalance(
       "Dn4noZ5jgGfkntzcQSUZ8czkreiZ1ForXYoV2H8Dm7S1",
-      "9gVstb8HkuYX8PqjLSc9b9zLMhFZwWX7k3ofLcWy7wyS"
+      "3m15qNJDM5zydsYNJzkFYXE7iGCVnkKz1mrmbawrDUAH"
     ),
     getTokenBalance(
       "EjmyN6qEC1Tf1JxiG1ae7UTJhUxSwk1TCWNWqxWV4J6o",
-      "HLdcfovcXkHKm4iQWNQZhJypySmuGa1PGoTuB6L68hhZ"
+      "3m15qNJDM5zydsYNJzkFYXE7iGCVnkKz1mrmbawrDUAH"
     ),
     //stsol2pool
     getTokenBalance(
       "7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj",
-      "FqLtqRJvoVNYU5Xpcgp5FNPv8ciXCZw7CiNddjQ4nqkRo"
+      "pG6noYMPVR9ykNgD4XSNa6paKKGGwciU2LckEQPDoSW"
     ),
     getTokenBalance(
       "So11111111111111111111111111111111111111112",
-      "9gizzFG33czvcTzn5N4V23uunV6YR55UNW8ow6qNPoX3"
+      "pG6noYMPVR9ykNgD4XSNa6paKKGGwciU2LckEQPDoSW"
     ),
   ]);
   return {
