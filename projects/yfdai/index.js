44131fd2e05e035925058ba37d984d56e7f85245
waynebruce0x <waynebruce0x@gmail.com>
2021-10-14 19:58:52 +0100

    YfDai fix

--- projects/yfdai/index.js
+++ projects/yfdai/index.js
@@ -6,7 +6,6 @@ const YfDaiStakingAdddress = "0x44d771D0C998f524ff39aB6Df64B72bce1d09566";
 const YfDaiSafetradeStakingAddress = "0x4599cDa238Fb71573fd5A0076C199320e09BCfF0";
 const weth = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 const dai = "0x6b175474e89094c44da98b954eedeac495271d0f"
-const YfDaiETHLP = "0x84E34df6F8F85f15D24Ec8e347D32F1184089a14"
 const wethVault = "0x290e5484601986667dC6cA72119f2B85260Ca92E"
 const daiVault = "0x7e537E8B5028a32166F06C8664cdE9D608487428"
 const YfDaiETHLP72HRSVault = "0x75E9F410e8d1D7240b67ec6FE35FA37580b814d9";
@@ -21,23 +20,21 @@ async function eth(_timestamp, ethBlock, chainBlocks) {
     [yfdaiTokenAddress, YfDaiSafetradeStakingAddress],
     [weth, wethVault],
     [dai, daiVault],
-    [yfdaiTokenAddress, YfDaiETHLP72HRSVault],
-    [yfdaiTokenAddress, YfDaiETHLP30DayVault],
-    [yfdaiTokenAddress, YfDaiETHLP60DayVault],
-    [yfdaiTokenAddress, YfDaiETHLP90DayVault],
-    [yfdaiTokenAddress, impulsevenStakingAddress]
   ], ethBlock)
   return balances
 }
 
 async function staking(_timestamp, ethBlock, chainBlocks) {
-    return {
-        [yfdaiTokenAddress]: (await sdk.api.erc20.balanceOf({
-            target: yfdaiTokenAddress,
-            owner: YfDaiStakingAdddress,
-            block: ethBlock
-        })).output
-    }
+    const balances = {}
+  await sumTokens(balances, [
+    [yfdaiTokenAddress, YfDaiETHLP72HRSVault],
+    [yfdaiTokenAddress, YfDaiETHLP30DayVault],
+    [yfdaiTokenAddress, YfDaiETHLP60DayVault],
+    [yfdaiTokenAddress, YfDaiETHLP90DayVault],
+    [yfdaiTokenAddress, impulsevenStakingAddress],
+    [yfdaiTokenAddress, YfDaiStakingAdddress]
+  ], ethBlock)
+  return balances
 }
 
 
