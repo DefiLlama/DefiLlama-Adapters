b144d2cefb1670f1684b15e6d524377aa012d6fd
Locutus of BORG <75694813+locutusofborg111@users.noreply.github.com>
2021-10-14 14:09:18 -0400

    Update index.js

--- projects/borgswap/index.js
+++ projects/borgswap/index.js
@@ -9,7 +9,10 @@ const BigNumber = require("bignumber.js");
 const MASTERCHEF = "0x4de6c2de6b9eBD974738686C9be7a31597146Ac6";
 const MASTERCHEF2 = "0x92eEd89eeC81d992FF9135Ee451008E93b83dD86";
 const MASTERCHEF3 = "0xC8FF977ee4e5EdA2D650C0e2706995a1DbB4926b";
+const MASTERCHEF3 = "0xaDb6C60f0D62d6e7583e4b7B3697aAAd723d4a85";
 
+const GPL = "0xdDa7DA47f3b53aA1FcB341650C614DaF554f3e57"
+const EARS = "0x0EC4B89462557150302AC6e81270a081F2e3BD20"
 
 const masterchefTvl = async (timestamp, ethBlock, chainBlocks) => {
   const balances = {};
@@ -62,10 +65,31 @@ const masterchefTvl3 = async (timestamp, ethBlock, chainBlocks) => {
   return balances;
 };
 
+const masterchefTvl4 = async (timestamp, ethBlock, chainBlocks) => {
+    const balances = {};
+  
+    const transformAddress = await transformFantomAddress();
+  
+    await addFundsInMasterChef(
+      balances,
+      MASTERCHEF4,
+      chainBlocks.fantom,
+      "fantom",
+      transformAddress,
+      abi.poolInfo
+    );
+  
+    return balances;
+  };
+
 module.exports = {
+  staking:{
+    tvl: staking(GPL, EARS, 'fantom')
+  },
   tvl: sdk.util.sumChainTvls([
     masterchefTvl,
     masterchefTvl2,
     masterchefTvl3,
+    masterchefTvl4,
   ]),
 };
