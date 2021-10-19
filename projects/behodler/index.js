70e4d54f469ee865bb2396526bd611ab9c003356
Arren Vidal <arren@agreemi.com>
2021-10-19 19:11:51 +1000

    Added Behodler AMM

---
+++ projects/behodler/index.js
@@ -0,0 +1,37 @@
+const { request, gql } = require("graphql-request");
+const { toUSDTBalances } = require('../helper/balances');
+
+const graphUrl = 'https://api.thegraph.com/subgraphs/name/arrenv/behodler'
+const graphQuery = gql`
+query get_tvl($block: Int) {
+  behodlers(
+    block: { number: $block }
+  ) {
+    id
+    usdVolume
+    usdLiquidity
+  }
+}
+`;
+
+async function tvl(timestamp, block) {
+  const {behodlers} = await request(
+    graphUrl,
+    graphQuery,
+    {
+      block,
+    }
+  );
+  const usdTvl = Number(behodlers[0].usdLiquidity)
+
+  return toUSDTBalances(usdTvl)
+}
+
+module.exports = {
+  misrepresentedTokens: true,
+  methodology: `Counts the tokens locked on AMM pools, pulling the data from the 'arrenv/behodler' subgraph`,
+  ethereum:{
+    tvl,
+  },
+  tvl
+}
