deb46534548d06cabb4fc72e3f86bd7f4a7a38e7
Dev.vee.finance <90096926+DevForDefi@users.noreply.github.com>
2021-10-19 16:13:29 +0800

    Add fetch adapter for Vee finance

--- projects/vee-finance/index.js
+++ projects/vee-finance/index.js
@@ -1,10 +1,21 @@
-const {getCompoundV2Tvl} = require('../helper/compound')
-const sdk = require('@defillama/sdk')
+const axios = require('axios');
+const BN = require('bignumber.js');
 
-module.exports={
-    methodology: 'As in Compound Finance, TVL counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted.',
-    tvl: sdk.util.sumChainTvls([
-        getCompoundV2Tvl("0xA67DFeD73025b0d61F2515c531dd8D25D4Cfd0Db", "avax", addr=>`avax:${addr}`),
-        getCompoundV2Tvl("0x43AAd7d8Bc661dfA70120865239529ED92Faa054", "avax", addr=>`avax:${addr}`, "0x6481490DBb6Bd0e8b7CB7E1317470f6d08aDa5A2", "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7"),
-    ])
+async function fetch() {   
+    var tvl = 0;
+    try {
+        var responseV1   = await axios.get('https://api-prod.vee.finance/getv1tvl');
+        var responseV2   = await axios.get('https://api-prod.vee.finance/getv2tvl');
+        
+        if(responseV1.status == 200 && responseV2.status == 200){            
+            tvl = new BN(responseV1.data.data['tvl']).plus(responseV2.data.data['tvl']).toFixed(2);           
+        }
+    } catch (error) {
+       //console.error("tvl()-error: ===> " + error);
+  }
+  return tvl;    
+}
+
+module.exports = {
+    fetch
 }
