abd224827f276a69ad09db003ddb0b0c4db1c819
0xngmi <0xngmi@protonmail.com>
2021-10-20 00:37:37 +0100

    kolibri fixes

--- projects/kolibri/index.js
+++ projects/kolibri/index.js
@@ -1,21 +1,18 @@
 const axios = require("axios");
 const {toUSDTBalances} = require('../helper/balances')
 
-const priceUrl = 'https://oracle-data.kolibri.finance/data.json';
 const tvlUrl = 'https://kolibri-data.s3.amazonaws.com/mainnet/totals.json';
 
 async function tvl() {
     const response = await axios.get(tvlUrl)
 
-    // Balance of Liquidity Pool
-    const liquidityPoolBalance = new BigNumber(response.data.liquidityPoolBalance)
-
-    // Balance of locked XTZ, in terms of USD
-    const price = new BigNumber((await axios.get(priceUrl)).data.prices.XTZ);
-    const xtzInOvens = new BigNumber(response.data.totalBalance);
-    const usdInOvens = xtzInOvens.multipliedBy(price).dividedBy(1000000).toFixed(0);
+    const xtzInOvens = Number(response.data.totalBalance)/1000000;
+    const liquidityPoolBalance = Number(response.data.liquidityPoolBalance)
     
-    return toUSDTBalances(usdInOvens.plus(liquidityPoolBalance))
+    return {
+        "tezos": xtzInOvens,
+        "true-usd": liquidityPoolBalance
+    }
 }
 async function pool2() {
     return toUSDTBalances((await axios.get(tvlUrl)).data.quipuswapFarmBalanceUSD);
