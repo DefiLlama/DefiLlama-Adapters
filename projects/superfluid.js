384d4b66e6e446cdc03e2e4c9abdf98c907c9c75
Jonathan Chemla <jchemla@iconem.com>
2021-10-20 15:56:19 +0200

    Refactoring superfluid.js
    Refactoring some functions enhancing readability

--- projects/superfluid.js
+++ projects/superfluid.js
@@ -1,13 +1,11 @@
 const sdk = require("@defillama/sdk");
-const { GraphQLClient, request, gql } = require("graphql-request");
-const abi = require('./erc20-abi.json')
-const { getBlock } = require("../helper/getBlock");
-const { transformPolygonAddress, transformXdaiAddress } = require("../helper/portedTokens");
+const { request, gql } = require("graphql-request"); // GraphQLClient
+const { transformPolygonAddress, transformXdaiAddress } = require("./helper/portedTokens");
+// const abi = require('./erc20-abi.json')
 
-// Superfluid Supertokens can be retrieved using GraphQl API
+// Superfluid Supertokens can be retrieved using GraphQl API - cannot use block number to retrieve historical data at the moment though
 const polygonGraphUrl = 'https://api.thegraph.com/subgraphs/name/superfluid-finance/superfluid-matic'
 const xdaiGraphUrl = 'https://api.thegraph.com/subgraphs/name/superfluid-finance/superfluid-xdai'
-
 const supertokensQuery = gql`
 query get_supertokens {
   tokens(first: 1000) {
@@ -19,10 +17,10 @@ query get_supertokens {
 }
 `;
 
-// callback for lockedTokens
+// Callback for lockedTokens - XDAI chain transform is async but polygon transform is sync
 function lockedTokensCallback_sync(call, transform) {
   const token = allTokens.find(token => token.id === call.input.target)
-  mainnetUnderlyingAddress = transform(token.underlyingAddress) // For XDAI chain, need an async but not for polygon
+  mainnetUnderlyingAddress = transform(token.underlyingAddress) 
   token.mainnetUnderlyingAddress = mainnetUnderlyingAddress
   return {
     target: mainnetUnderlyingAddress, 
@@ -32,7 +30,7 @@ function lockedTokensCallback_sync(call, transform) {
 }
 async function lockedTokensCallback_async(call, transform) {
   const token = allTokens.find(token => token.id === call.input.target)
-  mainnetUnderlyingAddress = await transform(token.underlyingAddress) // For XDAI chain, need an async but not for polygon
+  mainnetUnderlyingAddress = await transform(token.underlyingAddress) 
   token.mainnetUnderlyingAddress = mainnetUnderlyingAddress
   return {
     target: mainnetUnderlyingAddress, 
@@ -65,7 +63,7 @@ async function getChainBalances(allTokens, chain, block, transform = a => a) {
   if (chain === 'xdai') mainnetDecimalsCalls = lockedTokensOutput.output.map( call => lockedTokensCallback_async(call, transform) )
   else mainnetDecimalsCalls = lockedTokensOutput.output.map( call => lockedTokensCallback_sync(call, transform) )
   
-  // Execute decimals ABI (future proof), otherwise could simply store them in a variable
+  // Execute decimals ABI (future proof), otherwise could simply store decimals count in an array
   const decimals = await sdk.api.abi.multiCall({
     abi: 'erc20:decimals', // abi['decimals'],
     calls: mainnetDecimalsCalls,
@@ -82,7 +80,6 @@ async function getChainBalances(allTokens, chain, block, transform = a => a) {
       token.decimals = decimals
     })
   })
-
   // Going forward, each token of allTokens has been appended mainnetUnderlyingAddress and decimals  
   // console.log('\n\n', allTokens, '\n\n')
 
@@ -108,61 +105,48 @@ async function getChainBalances(allTokens, chain, block, transform = a => a) {
 
 const selectedTokens = ['USDCx', 'ETHx', 'DAIx', 'WBTCx', 'USDTx', 'SDTx', 'QIx', 'MOCAx']
 
-async function polygon(timestamp, block, chainBlocks) {
+async function retrieveSupertokensBalances(chain, timestamp, block, chainBlocks) {
   // Retrieve supertokens from graphql API
-  const { tokens } = await request(polygonGraphUrl, supertokensQuery)
+  if (chain === 'polygon') {
+    graphUrl = polygonGraphUrl
+    block = chainBlocks.polygon
+    transform = await transformPolygonAddress()
+  }
+  else if (chain === 'xdai') {
+    graphUrl = xdaiGraphUrl
+    block = chainBlocks.xdai
+    transform = await transformXdaiAddress()
+  }
+
+  const { tokens } = await request(graphUrl, supertokensQuery)
 
   // Filter out some tokens for easier debugging
-  allTokens = tokens.filter(t => selectedTokens.includes(t.symbol)) 
+  //allTokens = tokens.filter(t => selectedTokens.includes(t.symbol)) 
   allTokens = tokens.filter(t => t.symbol.length > 0)
   // console.log(allTokens)
 
-  return getChainBalances(allTokens, 'polygon', chainBlocks.polygon, await transformPolygonAddress())
+  return getChainBalances(allTokens, chain, block, transform)
+}
+async function polygon(timestamp, block, chainBlocks) {
+  return retrieveSupertokensBalances('polygon', timestamp, block, chainBlocks)
 }
 
 async function xdai(timestamp, block, chainBlocks) {
-  // Retrieve supertokens from graphql API
-  const { tokens } = await request(xdaiGraphUrl, supertokensQuery)
-
-  // Filter out some tokens for easier debugging
-  allTokens = tokens.filter(t => selectedTokens.includes(t.symbol)) 
-  allTokens = tokens.filter(t => t.symbol.length > 0)
-  // console.log(allTokens)
-
-  transform = await transformXdaiAddress()
-  return getChainBalances(allTokens, 'xdai', chainBlocks.xdai, transform)
+  return retrieveSupertokensBalances('xdai', timestamp, block, chainBlocks)
 }
 
 
 module.exports = {
-  /*
+  
   polygon: {
     tvl: polygon
   }, 
   xdai: {
     tvl: xdai
-  },*/
-  tvl: sdk.util.sumChainTvls([polygon, polygon]),
+  },
+  tvl: sdk.util.sumChainTvls([xdai, polygon]),
   
   //tvl: xdai,
   methodology: `TVL is the total quantity of tokens locked in Super Tokens from Superfluid, on Polygon and xDai (most important being weth, dai, usdc and wbtc, as well as QiDAO and MOCA)`
-}
-
-
-
-
-
-// Polygon address to Mainnet address could be stored in this script instead
-decimalCoeff = {
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': 6, // USDC 
-  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599': 8, // WBTC
-  '0x6b175474e89094c44da98b954eedeac495271d0f': 18 // DAI
-}
-transform_jo = addr => {
-  if(addr === "0x2791bca1f2de4661ed88a30c99a7a9449aa84174") return "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48" // USDC
-  if(addr === "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063") return "0x6b175474e89094c44da98b954eedeac495271d0f" // DAI
-  if(addr === "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619") return "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2" // WETH
-  if(addr === "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6") return "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599" // WBTC
-  return addr
 }
 
