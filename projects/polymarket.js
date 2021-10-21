ba70c887a7496948976d28384319c2972d68d705
Jonathan Chemla <jchemla@iconem.com>
2021-10-21 20:09:53 +0200

    Polymarket refactoring + accumulating liquidity instead of volume
    Polymarket refactoring + accumulating liquidity instead of volume

--- projects/polymarket.js
+++ projects/polymarket.js
@@ -4,9 +4,11 @@ const { BigNumber } = require("bignumber.js");
 const utils = require('./helper/utils');
 
 
-// Could retrieve https://strapi-matic.poly.market/markets?active=true&_sort=end_date:desc but it is not up to date after end of september
+// Can retrieve https://strapi-matic.poly.market/markets?active=true&_sort=end_date:desc 
 // Old graphql https://api.thegraph.com/subgraphs/name/tokenunion/polymarket-matic 
 // New graphql https://api.thegraph.com/subgraphs/name/polymarket/matic-markets-5
+
+// Useful resources : https://polymarketwhales.info/markets and https://polymarket.com/
 
 // # block: { number: $block }
 const graphUrl = 'https://api.thegraph.com/subgraphs/name/polymarket/matic-markets-5'
@@ -37,9 +39,7 @@ query GET_POLYMARKET($skip: Int, $block: Int) {
 `;
 
 async function polygon_graphql(timestamp, block) {
-  balances = {}
-  scaledCollateralVolumeSum = 0
-  scaledCollateralVolumes = []
+  let scaledCollateralVolumeSum = 0
   skip = 0
   // Continue querying graph while end not reached. Hard cap: skip = 5000
   while (skip !== -1) { 
@@ -64,16 +64,27 @@ async function polygon_graphql(timestamp, block) {
   }
   console.log(`${conditions.length} conditions found`)
   
-  return {'polygon:0x2791bca1f2de4661ed88a30c99a7a9449aa84174': scaledCollateralVolumes * 1e6};;
+  return {'polygon:0x2791bca1f2de4661ed88a30c99a7a9449aa84174': scaledCollateralVolumeSum * 1e6};;
 }
 
 // After a market resolves, then the market participants can withdraw their share based on the redemption rate and their contribution at the closing of the market. All participants do not do it immediately though, so volume of every market should be accounted for in TVL
-const polymarket_api_url = 'https://strapi-matic.poly.market/markets?_limit=-1&_sort=closed_time:desc' // &active=true
+// const polymarket_api_url = 'https://strapi-matic.poly.market/markets?_limit=-1&_sort=closed_time:desc' // &active=true
+
+const polymarket_api_url = 'https://strapi-matic.poly.market/markets?_limit=-1&_sort=volume:desc' // &active=true
 async function polygon(timestamp, block) {
   let markets = await utils.fetchURL(polymarket_api_url)
-  const marketsUsdcVolumes = markets.data.reduce((acc, market) => acc + Number(market.volume) || 0, 0); 
-  console.log(marketsUsdcVolumes)
-  return {'polygon:0x2791bca1f2de4661ed88a30c99a7a9449aa84174': marketsUsdcVolumes * 1e6};
+
+  // Filter out unwanted fields
+  markets = markets.data.map(({ id, slug, volume, liquidity, created_at, closed }) => ({id, slug, volume, liquidity, created_at, closed})); 
+  markets.forEach(market => {
+    market.volume = Number(market.volume) || 0
+    market.liquidity = Number(market.liquidity) || 0
+    if (market.liquidity < 0) market.liquidity = 0
+  }); 
+  
+  const tvl = markets.reduce((acc, market) => acc + market.liquidity, 0); 
+  
+  return {'polygon:0x2791bca1f2de4661ed88a30c99a7a9449aa84174': tvl * 1e6};
 }
 
 
@@ -81,6 +92,40 @@ module.exports = {
   polygon: {
     tvl: polygon
   },
-  tvl: polygon,
+  //tvl: polygon,
   methodology: `TVL is the total quantity of USDC collateral submitted to every polymarket' markets ever opened - once the markets resolve, participants can withdraw theire share given the redeption rate and their input stake, but they do not all do it`
 }
+
+/*
+const largeMarkets = markets.filter(market => market.volume > 1e5)
+console.log('\n\n', largeMarkets, `\n\n${largeMarkets.length} markets with volume above 100k` )
+
+const bad_liquidity = [
+  'how-many-tweets-will-donald-j-trump-jr-post-from-jan-18th-to-25th', 
+  'will-the-buffalo-bills-or-the-kansas-city-chiefs-win-the-afc-championship',
+  'will-weekly-jobless-claims-exceed-348k-for-the-week-ending-on-august-21', 
+  'who-will-win-suns-v-clippers-western-conference-finals-game-3', 
+  'will-fabiano-caruana-win-the-2021-sinquefield-cup',
+  'will-f9-gross-more-than-62m-domestically-on-opening-weekend',
+  'will-the-mavericks-or-the-jazz-win-their-january-29th-matchup',
+  'will-fewociouss-his-name-is-victor-have-a-final-sale-price-of-more-than-850k-at-christies',
+  'will-fewociouss-my-mamas-dream-have-a-final-sale-price-of-more-than-650k-at-christies',
+]
+
+
+const csvString = [
+  [ 'id', 'slug', 'volume', 'liquidity', 'created_at', 'closed'],
+  ...markets.map(m => [m.id, m.slug, m.volume, m.liquidity, m.created_at, m.closed])
+]
+ .map(e => e.join(',')) 
+ .join('\n');
+console.log(csvString);
+const fs = require('fs')
+fs.writeFile('D:/Users/TERRAIN-PC/Downloads/polymarket_tvl.csv', csvString, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  //file written successfully
+})
+*/
