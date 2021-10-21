002366f827c63eaf6054fb06b002b7212a284779
0xngmi <0xngmi@protonmail.com>
2021-10-21 02:51:23 +0100

    removed mainnet calls

--- projects/superfluid.js
+++ projects/superfluid.js
@@ -19,15 +19,6 @@ query get_supertokens {
 `;
 // An upcoming superfluid graphql subgraph will be published soon and provide token supplies. 
 
-async function lockedTokensCallback(call, transform, allTokens) {
-  const token = allTokens.find(token => token.id === call.input.target)
-  mainnetUnderlyingAddress = await transform(token.underlyingAddress) 
-  token.mainnetUnderlyingAddress = mainnetUnderlyingAddress
-  return {
-    target: mainnetUnderlyingAddress,
-  }
-}
-
 // Main function for all chains to get balances of superfluid tokens
 async function getChainBalances(allTokens, chain, block, transform = a => a) {
   // Init empty balances
@@ -44,23 +35,23 @@ async function getChainBalances(allTokens, chain, block, transform = a => a) {
     chain
   })
   //console.log(chain, lockedTokensOutput.output, allTokens)
-
-  const mainnetDecimalsCalls = await Promise.all(lockedTokensOutput.output.map( call => lockedTokensCallback(call, transform, allTokens) ))
   
   // Execute decimals ABI (future proof), otherwise could simply store decimals count in an array
   const decimals = await sdk.api.abi.multiCall({
     abi: 'erc20:decimals', // abi['decimals'],
-    calls: mainnetDecimalsCalls,
+    calls: allTokens.map(token => ({
+      target: token.underlyingAddress,
+      })),
     block,
-    chain: 'ethereum'
+    chain
   })
   decimals.output.forEach(call => {
-    const tokens = allTokens.filter(token => token.mainnetUnderlyingAddress === call.input.target)
+    const tokens = allTokens.filter(token => token.underlyingAddress === call.input.target)
     let decimals = call.output
     tokens.forEach(token => {
       // Some abi calls error out, replace known decimals count 
-      if (token.mainnetUnderlyingAddress === '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599') decimals = '8'; // WBTC
-      else if (token.mainnetUnderlyingAddress === '0x0000000000000000000000000000000000000000') decimals = '18'; // ETH
+      if (token.underlyingAddress === '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599') decimals = '8'; // WBTC
+      else if (token.underlyingAddress === '0x0000000000000000000000000000000000000000') decimals = '18'; // ETH
       token.decimals = decimals
     })
   })
@@ -71,23 +62,18 @@ async function getChainBalances(allTokens, chain, block, transform = a => a) {
   lockedTokensOutput.output.forEach(call => {
     // Find corresponding token and retrieve mainnetUnderlyingAddress and decimals stored previously
     const token = allTokens.find(token => token.id === call.input.target)
-    let mainnetUnderlyingAddress = token.mainnetUnderlyingAddress;
 
     // Edit balance given decimal ABI multicall output and decimalCount. Note: all super tokens have 18 decimals, regardless of underlying asset, but need to adjust balance based on decimal of underlying
     let decimals = token.decimals || 18;
     let underlyingTokenBalance = BigNumber(call.output).div( 10 ** (18 - decimals) ).toFixed(0)
     
     // Accumulate to balances, the balance for tokens on mainnet or sidechain
-    prefixedUnderlyingAddress = chain + ':' + token.underlyingAddress
+    let prefixedUnderlyingAddress = chain + ':' + token.underlyingAddress
     if (tokensNativeToSidechain.includes(token.id)) prefixedUnderlyingAddress = chain + ':' + token.id
     sdk.util.sumSingleBalance(balances, prefixedUnderlyingAddress, underlyingTokenBalance)
-    console.log('Token:', token.symbol, '- decimals:', decimals, '- underlyingTokenBalance:', underlyingTokenBalance, '- mainnetUnderlyingAddress:', mainnetUnderlyingAddress)
+    //console.log('Token:', token.symbol, '- decimals:', decimals, '- underlyingTokenBalance:', underlyingTokenBalance, '- mainnetUnderlyingAddress:', mainnetUnderlyingAddress)
   })
 
-  // Remove null balances
-  balances = Object.assign({}, ...
-    Object.entries(balances).filter(([k,v]) => v > 0).map(([k,v]) => ({[k]:v}))
-  );
   return balances
 }
 
