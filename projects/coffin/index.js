b65f2673d74419071b2dc3e0b6dcad8d28a5b5fa
Mohammed18062020 <mohamed_el_hamdaoui93@hotmail.com>
2021-10-22 19:37:29 +0100

    Chainge Finance && TTswap TVL Adapters && Updating other protocols

--- projects/coffin/index.js
+++ projects/coffin/index.js
@@ -1,8 +1,11 @@
 const sdk = require("@defillama/sdk");
+const { staking } = require("../helper/staking");
 const { addFundsInMasterChef } = require("../helper/masterchef");
 const { transformFantomAddress } = require("../helper/portedTokens");
 
 const MasterChefContract = "0x155482Bd4e5128082D61a2384935D4BBDcb0E7a7";
+const stakingContract = "0x61Befe6E5f20217960bD8659cd3113CC1ca67d2F";
+const WFTM = "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83";
 
 const ftmTvl = async (timestamp, ethBlock, chainBlocks) => {
   const balances = {};
@@ -22,10 +25,12 @@ const ftmTvl = async (timestamp, ethBlock, chainBlocks) => {
 
 module.exports = {
   misrepresentedTokens: true,
+  staking: {
+    tvl: staking(stakingContract, WFTM, "fantom"),
+  },
   fantom: {
     tvl: ftmTvl,
   },
   tvl: sdk.util.sumChainTvls([ftmTvl]),
-  methodology:
-    "We count liquidity on the Farms through MasterChef Contract",
+  methodology: "We count liquidity on the Farms through MasterChef Contract",
 };
