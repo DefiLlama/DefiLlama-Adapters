941d78f0e05d43c37eaccdafc78fef74599d4c85
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-10-22 15:03:20 -0400

    Separated KogeCoin farms and vaults from TVL in staking/pool2

--- projects/kogefarm/index.js
+++ projects/kogefarm/index.js
@@ -25,10 +25,107 @@ const ftm_BalancerForks = [
   },
 ]
 
-const polygonTvl = async (timestamp, block, chainBlocks) => {
+const polygonMasterChef = (masterChef, pid) => async (
+  timestamp,
+  block,
+  chainBlocks,
+) => {
+  const balances = {}
+
+  const lp_addresses = (
+    await sdk.api.abi.multiCall({
+      chain: 'polygon',
+      block: chainBlocks['polygon'],
+      calls: [
+        {
+          target: masterChef,
+          params: [pid],
+        },
+      ],
+      abi: abi.poolInfo,
+    })
+  ).output.map((val) => val.output.lpToken)
+
+  const lp_symbols = (
+    await sdk.api.abi.multiCall({
+      chain: 'polygon',
+      block: chainBlocks['polygon'],
+      calls: lp_addresses.map((address) => ({
+        target: address,
+      })),
+      abi: abi.symbol,
+    })
+  ).output.map((val) => val.output)
+
+  const vault_balances = (
+    await sdk.api.abi.multiCall({
+      chain: 'polygon',
+      block: chainBlocks['polygon'],
+      calls: lp_addresses.map((lp) => ({
+        target: lp,
+        params: masterChef,
+      })),
+      abi: 'erc20:balanceOf',
+    })
+  ).output.map((val) => val.output)
+
+  const lpPositions = []
+  const singlePositions = []
+
+  lp_addresses.forEach((v, idx) => {
+    if (lp_symbols[idx] === 'UNI-V2') {
+      lpPositions.push({
+        vaultAddr: lp_addresses[idx],
+        balance: vault_balances[idx],
+        token: lp_addresses[idx],
+      })
+    } else if (lp_addresses[idx] !== '') {
+      singlePositions.push({
+        vaultAddr: lp_addresses[idx],
+        balance: vault_balances[idx],
+        token: lp_addresses[idx],
+      })
+    }
+  })
+
+  const transformAddress = transformAddressKF()
+
+  await unwrapUniswapLPs(
+    balances,
+    lpPositions,
+    chainBlocks['polygon'],
+    'polygon',
+    transformAddress,
+  )
+
+  await getSinglePositions(
+    balances,
+    singlePositions,
+    chainBlocks['polygon'],
+    'polygon',
+    transformAddress,
+  )
+
+  return balances
+}
+
+const polygonTvl = ({ include, exclude }) => async (
+  timestamp,
+  block,
+  chainBlocks,
+) => {
   const balances = {}
 
   let vaults = (await utils.fetchURL(current_polygon_vaults_url)).data
+
+  if (!!include) {
+    vaults = include
+  }
+  if (!!exclude) {
+    vaults = vaults.filter(
+      (v) => !exclude.find((e) => e.toLowerCase() === v.toLowerCase()),
+    )
+  }
 
   const lp_addresses = (
     await sdk.api.abi.multiCall({
@@ -204,13 +301,46 @@ const fantomTvl = async (timestamp, block, chainBlocks) => {
   return balances
 }
 
+const kogeMasterChefAddr = '0x6275518a63e891b1bC54FEEBBb5333776E32fAbD'
+
+// vKogeKoge
+const _polygonStaking = polygonMasterChef(kogeMasterChefAddr, 1)
+
+// Pool2
+const kogecoinVaultAddr = '0x992Ae1912CE6b608E0c0d2BF66259ab1aE62A657'
+const kogecoinMaticVaultAddr = '0xb7D3e1C5cb26D088d619525c6fD5D8DDC1B543d1'
+const kogecoinSageVaultAddr = '0x4792b5943a05fc6AF3B20B5F1D1d7dDe33C42980'
+
+const _kogePool2 = [
+  kogecoinVaultAddr,
+  kogecoinMaticVaultAddr,
+  kogecoinSageVaultAddr,
+]
+const _polygonPool2 = async (timestamp, block, chainBlocks) => {
+  return {
+    ...(await polygonTvl({
+      include: _kogePool2.filter((p) => p !== kogecoinVaultAddr),
+    })(timestamp, block, chainBlocks)),
+    ...(await polygonMasterChef(kogeMasterChefAddr, 0)(
+      timestamp,
+      block,
+      chainBlocks,
+    )),
+  }
+}
+const _polygonTvl = polygonTvl({
+  exclude: _kogePool2,
+})
+
 module.exports = {
   methodology: `The vaults are obtained through the following links: polygon:"${current_polygon_vaults_url}", fantom:"${current_fantom_vaults_url}". By getting the vaults, we can then pull LP token deposit amounts. We then take the LP token deposits and unwrap them to count each token individually.`,
   polygon: {
-    tvl: polygonTvl,
+    tvl: _polygonTvl,
+    staking: _polygonStaking,
+    pool2: _polygonPool2,
   },
   fantom: {
     tvl: fantomTvl,
   },
-  tvl: sdk.util.sumChainTvls([fantomTvl, polygonTvl]),
+  tvl: sdk.util.sumChainTvls([fantomTvl, _polygonTvl]),
 }
