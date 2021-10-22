527941c9c65a42aa06b36c4c91c3fb7faa80ecde
Jonathan Chemla <jchemla@iconem.com>
2021-10-22 17:31:53 +0200

    Agave: edit exports
    Agave: edit exports

--- projects/agave.js
+++ projects/agave.js
@@ -95,8 +95,8 @@ async function staking(timestamp, block, chainBlocks){
 
 module.exports = {
   methodology: `Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There's multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending`,
-  staking:{
-    tvl: staking
-  },
-  tvl: xdai
+  xdai:{
+    tvl,
+    staking
+  }
 };
