df27253580bc67448bb4732466b04eb9e7486628
0xngmi <0xngmi@protonmail.com>
2021-10-26 06:08:01 +0100

    revert getBlock bypass

--- projects/helper/getBlock.js
+++ projects/helper/getBlock.js
@@ -2,13 +2,13 @@ const sdk = require('@defillama/sdk')
 const retry = require('async-retry')
 const axios = require("axios");
 
-async function getBlock(timestamp, chain, chainBlocks) {
-    if(chain === "celo"){
-        return Number((await retry(async bail => await axios.get("https://explorer.celo.org/api?module=block&action=getblocknobytime&timestamp=" + timestamp + "&closest=before"))).data.result.blockNumber);
-    }
-    if (chainBlocks[chain] !== undefined || process.env.HISTORICAL === undefined) {
+async function getBlock(timestamp, chain, chainBlocks, undefinedOk = false) {
+    if (chainBlocks[chain] !== undefined || (process.env.HISTORICAL === undefined && undefinedOk)) {
         return chainBlocks[chain]
     } else {
+        if(chain === "celo"){
+            return Number((await retry(async bail => await axios.get("https://explorer.celo.org/api?module=block&action=getblocknobytime&timestamp=" + timestamp + "&closest=before"))).data.result.blockNumber);
+        }
         return sdk.api.util.lookupBlock(timestamp, { chain }).then(blockData => blockData.block)
     }
 }
