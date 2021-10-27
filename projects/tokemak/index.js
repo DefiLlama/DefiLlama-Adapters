bb70355b26807f6f915d2d32c2123956fe48db59
Nemusona <nemusonaneko@protonmail.com>
2021-10-27 11:44:52 -0500

    add ohm, alcx, fx, and tcr pool to tokemak

--- projects/tokemak/index.js
+++ projects/tokemak/index.js
@@ -6,6 +6,14 @@ const wethPool = "0xD3D13a578a53685B4ac36A1Bab31912D2B2A2F36"
 const usdcPool = "0x04bda0cf6ad025948af830e75228ed420b0e860d"
 const usdc = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
 const weth = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+const ohmPool = "0xe7a7D17e2177f66D035d9D50A7f48d8D8E31532D";
+const ohm = "0x383518188C0C6d7730D91b2c03a03C837814a899";
+const alcxPool = "0xD3B5D9a561c293Fb42b446FE7e237DaA9BF9AA84";
+const alcx = "0xdBdb4d16EdA451D0503b854CF79D55697F90c8DF";
+const fxsPool = "0xADF15Ec41689fc5b6DcA0db7c53c9bFE7981E655";
+const fxs = "0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0";
+const tcrPool = "0x15A629f0665A3Eb97D7aE9A7ce7ABF73AeB79415";
+const tcr = "0x9C4A4204B79dd291D6b6571C5BE8BbcD0622F050";
 const rtoke = "0xa760e26aA76747020171fCF8BdA108dFdE8Eb930"
 const toke = "0x2e9d63788249371f1dfc918a52f8d799f4a38c94"
 
@@ -15,7 +23,11 @@ async function tvl(timestamp, block) {
     [weth, degenesisContract],
     [usdc, degenesisContract],
     [weth, wethPool],
-    [usdc, usdcPool]
+    [usdc, usdcPool],
+    [ohm, ohmPool],
+    [alcx, alcxPool],
+    [fxs, fxsPool],
+    [tcr, tcrPool]
   ], block)
   return balances
 }
