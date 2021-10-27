1b0bb25ea0b0ac72b3a8ca752b81e1bff86d40bf
Horus <zhr-xc@163.com>
2021-10-27 15:58:30 +0800

    Add Project solo.top

---
+++ projects/solo/index.js
@@ -0,0 +1,40 @@
+const sdk = require('@defillama/sdk');
+const abi = require('./abi.json');
+
+const CHAIN_HECO = "heco"
+const CHAIN_BSC = "bsc"
+const CHAIN_POLYGON = "polygon"
+const CHAIN_OEC = "okexchain"
+
+const VAULT_HECO = "0x1cF73836aE625005897a1aF831479237B6d1e4D2"
+const VAULT_BSC = "0x7033A512639119C759A51b250BfA461AE100894b"
+const VAULT_POLYGON = "0xE95876787B055f1b9E4cfd5d3e32BDe302BF789d"
+const VAULT_OEC = "0xa8AF3199aCE72E47c1DEb56E58BEA1CD41C37c22"
+
+const USDT_HECO = "0xa71EdC38d189767582C38A3145b5873052c3e47a"
+const USDT_BSC = "0x55d398326f99059fF775485246999027B3197955"
+const USDT_POLYGON = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F"
+const USDT_OEC = "0x382bB369d343125BfB2117af9c149795C6C65C50"
+
+const tvlHeco = async (timestamp, blockETH, chainBlocks) => {
+    console.log("timestamp: " + timestamp + ", blockETH:" + blockETH, ", chainBlocks:" + chainBlocks)
+    let block = chainBlocks[CHAIN_HECO];
+    console.log("block: " + block)
+    if (block === undefined) {
+        block = (await sdk.api.util.lookupBlock(timestamp, { chain: CHAIN_HECO })).block;
+    }
+    console.log("block: " + block)
+    const statistics = (await sdk.api.abi.call({ target: VAULT_HECO, abi: abi["getGlobalStatistics"], block: block, chain: CHAIN_HECO })).output;
+    console.log("statistics: " + statistics + ", tvl: " + statistics[0])
+    const balances = {}
+    balances[CHAIN_HECO + ":" + USDT_HECO] = statistics[0]
+    console.log("balances: " + balances)
+    return balances;
+};
+
+module.exports = {
+    heco: {
+       tvl: tvlHeco
+    },
+    tvl: tvlHeco
+};
--- pull_request_template.md
+++ pull_request_template.md
@@ -1,42 +1,42 @@
 ##### Twitter Link:
-
+https://twitter.com/solo_top
 
 ##### List of audit links if any:
-
+https://www.certik.com/projects/solotop
 
 ##### Website Link:
-
+https://solo.top/
 
 ##### Logo (High resolution, preferably in .svg and .png, for application on both white and black backgrounds. Will be shown with rounded borders):
-
+https://solo.top/solo-logo-200x200.png
 
 ##### Current TVL:
-
+128,915,987.42 USDT
 
 ##### Chain:
-
+BSC, HECO, Polygon, OEC
 
 ##### Coingecko ID (so your TVL can appear on Coingecko): (https://api.coingecko.com/api/v3/coins/list)
-
+None
 
 ##### Coinmarketcap ID (so your TVL can appear on Coinmarketcap): (https://api.coinmarketcap.com/data-api/v3/map/all?listing_status=active,inactive,untracked&start=1&limit=10000)
-
+None
 
 ##### Short Description (to be shown on DefiLlama):
-
+Solo.top is a single-asset LP mining aggregator aims to earn liquidity incentive with zero impermanent loss, and higher yield and safer to all users.
 
 ##### Token address and ticker if any:
-
+None
 
 ##### Category (Yield/Dexes/Lending/Minting/Assets/Insurance/Options/Indexes/Staking) *Please choose only one:
-
+Yield
 
 ##### Oracle used (Chainlink/Band/API3/TWAP or any other that you are using):
-
+Chainlink
 
 ##### forkedFrom (Does your project originate from another project):
-
+https://github.com/DefiLlama/DefiLlama-Adapters
 
 ##### methodology (what is being counted as tvl, how is tvl being calculated):
-
+Directly read the TVL from vault's smart contracts on all chains (BSC, HECO, Polygon, OEC).
 
