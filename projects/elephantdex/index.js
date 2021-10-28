cd16cc6ceef9b99d106133966fee36cc518c5750
elephantproject <elephantproject@protonmail.com>
2021-10-28 16:02:13 -0700

    add elephant dex on harmony

---
+++ projects/elephantdex/index.js
@@ -0,0 +1,39 @@
+const { request, gql } = require("graphql-request");
+const sdk = require('@defillama/sdk');
+const { toUSDTBalances } = require('../helper/balances');
+
+const graphUrl = 'https://graph.t.hmny.io/subgraphs/name/elephantproject/subgraph12'
+const graphQuery = gql`
+query get_tvl($block: Int) {
+  uniswapFactory(
+    id: "0x0Dea90EC11032615E027664D2708BC292Bbd976B",
+    block: { number: $block }
+  ) {
+    totalLiquidityUSD
+  },
+}
+`;
+
+async function tvl(timestamp) {
+  const {block} = await sdk.api.util.lookupBlock(timestamp,{
+    chain: 'harmony'
+  })
+  const response = await request(
+    graphUrl,
+    graphQuery,
+    {
+      block,
+    }
+  );
+
+  const usdTvl = Number(response.uniswapFactory.totalLiquidityUSD)
+
+  return toUSDTBalances(usdTvl)
+}
+
+module.exports = {
+  harmony:{
+    tvl,
+  },
+  tvl
+}
