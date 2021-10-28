69301343a5488ed05a97efff2d03ac00daf1d929
0xngmi <0xngmi@protonmail.com>
2021-10-28 15:05:31 +0100

    switch agave to registry

--- projects/agave.js
+++ projects/agave.js
@@ -197,7 +197,7 @@ async function xdai_registry(timestamp, ethBlock, chainBlocks) {
 module.exports = {
   methodology: `Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There's multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending`,
   xdai:{
-    tvl: xdai_graphql,
+    tvl: xdai_registry,
     staking: staking(agaveStakingContract, agaveTokenAddress, chain="xdai")
   }
 };
