3196954bd85969deac7d29a1550feaea767aee52
Aaron Fitzpatrick <a.fitzpatrick20@nuigalway.ie>
2021-10-29 16:23:59 +0200

    added tvl for decubate

--- projects/decubate/index.js
+++ projects/decubate/index.js
@@ -1,28 +1,45 @@
 const sdk = require("@defillama/sdk");
+const getPools = require("./getPools.json");
 
 const DCBToken = "0xEAc9873291dDAcA754EA5642114151f3035c67A2";
-const stakingPool = "0x22B551fE288c93A3Ac9172aD998A1D9ce1A882e5";
+const Staking = "0x22B551fE288c93A3Ac9172aD998A1D9ce1A882e5";
 
-async function staking (timestamp, block, chainBlocks) {
-    let balances = {};
+async function staking(timestamp, block, chainBlocks) {
+  let balances = {};
 
-    let {output: balance} =  await sdk.api.erc20.balanceOf({
-        target: DCBToken,
-        owner: stakingPool,
-        block: chainBlocks.bsc,
-        chain: "bsc"
-    });
+  let { output: balance } = await sdk.api.erc20.balanceOf({
+    target: DCBToken,
+    owner: Staking,
+    block: chainBlocks.bsc,
+    chain: "bsc",
+  });
 
-    sdk.util.sumSingleBalance(balances, `bsc:${DCBToken}`, balance)
+  sdk.util.sumSingleBalance(balances, `bsc:${DCBToken}`, balance);
 
-    return balances;
+  return balances;
+}
+
+async function tvl(timestamp, block, chainBlocks) {
+  let balances = {};
+
+  const pools = await sdk.api.abi.call({
+    target: Staking,
+    abi: getPools,
+    block: chainBlocks.bsc,
+    chain: "bsc",
+  });
+
+  pools.output.forEach(({ totalDeposit }) =>
+    sdk.util.sumSingleBalance(balances, `bsc:${DCBToken}`, totalDeposit)
+  );
+
+  return balances;
 }
 
 module.exports = {
-    bsc: {
-        tvl: async () => ({}), 
-        staking
-    },
-    tvl: async () => ({})
-    
-}
+  bsc: {
+    tvl,
+    staking,
+  },
+  tvl,
+};
