9aad623822a5b03b238baffb1cb4a7cdf7892735
Mohammed18062020 <mohamed_el_hamdaoui93@hotmail.com>
2021-10-31 14:48:57 +0000

    Atlantis Loans && BobaDAO && NileRiver TVL Adapters

---
+++ projects/atlantisloans/index.js
@@ -0,0 +1,40 @@
+const sdk = require("@defillama/sdk");
+const { staking } = require("../helper/staking");
+const { pool2 } = require("../helper/pool2");
+const { getCompoundV2Tvl } = require("../helper/compound");
+const { transformBscAddress } = require("../helper/portedTokens");
+
+const comptroller = "0xE7E304F136c054Ee71199Efa6E26E8b0DAe242F3";
+
+const vaultStakingContract = "0x9aFc9877b1621e414E907F13A8d3ED9511bE03de";
+const ATL = "0x1fD991fb6c3102873ba68a4e6e6a87B3a5c10271";
+
+const lpVaultStakingContract = "0xC7A5Bb6FCd603309D7a010de44dcBDe26fD45B58";
+const ALT_BUSD_CakeLP = "0xaa40dc3ec6ad76db3254b54443c4531e3dfe6bdb";
+
+async function bscTvl(...params) {
+  const transformAddress = await transformBscAddress();
+  return getCompoundV2Tvl(
+    comptroller,
+    "bsc",
+    transformAddress,
+    "0x5A9A90983A369b6bB8F062f0AFe6219Ac01caF63",
+    "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+  )(...params);
+}
+
+module.exports = {
+  misrepresentedTokens: true,
+  staking: {
+    tvl: staking(vaultStakingContract, ATL, "bsc"),
+  },
+  pool2: {
+    tvl: pool2(lpVaultStakingContract, ALT_BUSD_CakeLP, "bsc"),
+  },
+  bsc: {
+    tvl: bscTvl,
+  },
+  tvl: sdk.util.sumChainTvls([bscTvl]),
+  methodology:
+    "Same as compound, we just get all the collateral (not borrowed money) on the lending markets",
+};
