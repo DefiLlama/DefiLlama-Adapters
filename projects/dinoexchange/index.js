3664f035065cf61895ab7fe13f2180c5e55be207
0xngmi <0xngmi@protonmail.com>
2021-11-01 20:18:56 +0000

    simplify dinoexchange

--- projects/dinoexchange/index.js
+++ projects/dinoexchange/index.js
@@ -1,5 +1,5 @@
-const sdk = require("@defillama/sdk");
-const {calculateUniTvl} = require('../helper/calculateUniTvl.js')
+const {calculateUniTvl} = require('../helper/calculateUniTvl.js');
+const { staking } = require("../helper/staking.js");
 
 const DINO_TOKEN = '0xf317932ee2c30fa5d0e14416775977801734812d'
 const MASTER_DINO = '0x26CB55795Cff07Df3a1Fa9Ad0f51d6866a80943b'
@@ -10,25 +10,9 @@ async function bscTvl(timestamp, block, chainBlocks) {
   return rs;
 }
 
-async function stakingTvl(timestamp, ethBlock, chainBlocks) {
-  const balances = {};
-  const stakedDino = sdk.api.erc20.balanceOf({
-    target: DINO_TOKEN,
-    owner: MASTER_DINO,
-    chain: 'bsc',
-    block: chainBlocks.bsc
-  })
-  sdk.util.sumSingleBalance(balances, 'bsc:' + DINO_TOKEN, (await stakedDino).output)
-  return balances
-}
-
-
 module.exports = {
   bsc:{
     tvl: bscTvl,
+    staking: staking(MASTER_DINO, DINO_TOKEN, "bsc")
   },
-  staking:{
-    tvl: stakingTvl,
-  },
-  tvl: sdk.util.sumChainTvls([bscTvl])
 }
