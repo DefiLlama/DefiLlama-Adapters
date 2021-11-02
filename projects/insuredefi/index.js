24851d20727eceff140be65f0980d1a1ae438711
inSure DeFi <52913685+inSureToken@users.noreply.github.com>
2021-11-01 21:29:13 -0400

    Update index.js

--- projects/insuredefi/index.js
+++ projects/insuredefi/index.js
@@ -1,16 +1,37 @@
-const sdk = require('@defillama/sdk')
+const sdk = require("@defillama/sdk");
+const abi = require("./abi.json");
+const _ = require('underscore');
+const BigNumber = require("bignumber.js");
 
-const SURE = "0xcb86c6a22cb56b6cf40cafedb06ba0df188a416e"
-const contract = "0xF4b2aa60Cd469717857a8A4129C3dB9108f54D74"
-const currentTvlAbi = {"inputs":[],"name":"currentTVL","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}
-async function tvl(time, block){
-    return {
-        [SURE]:(await sdk.api.abi.call({
-            target: contract,
-            block,
-            abi: currentTvlAbi
-        })).output
-    }
+const constant = {
+  treasuryPool: {
+    address: "0xF4b2aa60Cd469717857a8A4129C3dB9108f54D74",
+  }
+};
+
+
+async function underwriting(block) {
+  let SURE = '0xcb86c6a22cb56b6cf40cafedb06ba0df188a416e';
+  let balanceOf;
+  balanceOf = await sdk.api.abi.call({
+    block,
+    target: nsure,
+    abi: abi["balanceOf"],
+    params: constant.treasuryPool.address,
+  });
+  balanceOf = balanceOf.output
+
+  return { [SURE]: balanceOf }
+}
+
+async function tvl(timestamp, block) {
+  let underwritingPool = await underwriting(block);
+  
+  let balances = {
+    ...underwritingPool
+  }
+
+  return balances;
 }
 
 
