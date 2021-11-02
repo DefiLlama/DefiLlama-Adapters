df3bb6fec6c11bccf0b9423adce72cabd4a66517
0xngmi <0xngmi@protonmail.com>
2021-11-02 17:07:51 +0000

    add psi staking to nexus

--- projects/nexus-protocol/index.js
+++ projects/nexus-protocol/index.js
@@ -9,11 +9,25 @@ async function tvl() {
     const balances = {}
     await Promise.all(vaults.map(async vault => {
         const deposited = await fetchURL(vault[0])
-        balances[vault[1]]=Number(deposited.data.result.balance)/1e6
+        balances[vault[1]] = Number(deposited.data.result.balance) / 1e6
     }))
     return balances
 }
 
-module.exports={
-    tvl
+async function staking() {
+    const staked = await fetchURL(
+        `https://lcd.terra.dev/wasm/contracts/terra12897djskt9rge8dtmm86w654g7kzckkd698608/store?query_msg=%7B%22balance%22:%7B%22address%22:%22terra1xrk6v2tfjrhjz2dsfecj40ps7ayanjx970gy0j%22%7D%7D`
+    )
+    console.log(Number(staked.data.result.balance) / 1e6)
+    return {
+        "nexus-governance-token": Number(staked.data.result.balance) / 1e6
+    }
+}
+
+module.exports = {
+    terra: {
+        // Token not on coingecko yet
+        //staking,
+        tvl
+    }
 }
