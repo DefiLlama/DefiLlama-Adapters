f50aadbfea01defd9b8300ee3f05142b91a11d35
0xngmi <0xngmi@protonmail.com>
2021-11-03 17:05:32 +0000

    add huckleberry staking

--- projects/huckleberry/index.js
+++ projects/huckleberry/index.js
@@ -1,6 +1,10 @@
 const {getChainTvl} = require('../helper/getUniSubgraphTvl')
+const { staking } = require('../helper/staking')
 
 module.exports={
     methodology: "Liquidity on DEX",
-    tvl: getChainTvl({moonriver: "https://graph-node.huckleberry.finance/subgraphs/name/huckleberry/huckleberry-subgraph"})('moonriver')
+    moonriver:{
+        staking: staking("0x37619cC85325aFea778830e184CB60a3ABc9210B", "0x9A92B5EBf1F6F6f7d93696FCD44e5Cf75035A756", "moonriver"),
+        tvl: getChainTvl({moonriver: "https://graph-node.huckleberry.finance/subgraphs/name/huckleberry/huckleberry-subgraph"})('moonriver')
+    }
 }
