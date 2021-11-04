faac1de5822b87be859b7e1eda9af9204743ca36
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-11-04 04:48:24 +0000

    add missing LP pools

--- projects/apricot.js
+++ projects/apricot.js
@@ -10,6 +10,7 @@ async function tvl() {
         rayAmount,
         orcaAmount,          
         solAmount,
+        ustAmount,
         /*
         orcaUsdcUsdt,
         orcaSolUsdc,
@@ -27,14 +28,29 @@ async function tvl() {
         getTokenBalance("4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"),
         getTokenBalance("orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"),
         getTokenBalance("So11111111111111111111111111111111111111112", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"),
+        getTokenBalance("9vMJfxuKxXBoEa7rM12mYLMwTacLMLDJqHozw96WQL8i", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"),
     ])
     const orcaPoolsTVL = await sumOrcaLPs([
+        //usdt/usdc
         ["GjpXgKwn4VW4J2pZdS3dovM58hiXWLJtopTfqG83zY2f", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
+        //SOL/USDC
         ["FFdjrSvNALfdgxANNpt3x85WpeVMdQSH5SEP2poM8fcK", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
+        //ETH/USDC
         ["HDP2AYFmvLz6sWpoSuNS62JjvW4HjMKp7doXucqpWN56", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
+        //SOL/USDT
         ["71vZ7Jvu8fTyFzpX399dmoSovoz24rVbipLrRn2wBNzW", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
+        //ORCA/USDC
         ["Gc7W5U66iuHQcC1cQyeX9hxkPF2QUVJPTf1NWbW8fNrt", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
+        //ORCA/SOL
         ["B5waaKnsmtqFawPspUwcuy1cRjAC7u2LrHSwxPSxK4sZ", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
+        //mSOL/USDC[aquafarm]
+        ["9y3QYM5mcaB8tU7oXRzAQnzHVa75P8riDuPievLp64cY", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
+        //BTC/mSOL[aquafarm]
+        ["6uA1ADUJbvwYJZpzUn9z9LuyKoRVngBKcQTKdXsSivA8", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
+        //ETH/SOL
+        ["CGFTRh4jKLPbS9r4hZtbDfaRuC7qcA8rZpbLnVTzJBer", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
+        //mSOL/SOL[stable][aquafarm]
+        ["576ABEdvLG1iFU3bLC8AMJ3mo5LhfgPPhMtTeVAGG6u7", "7Ne6h2w3LpTNTa7CNYcUs7UkjeJT3oW7jcrXWfVScTXW"],
     ])
     return {
         'bitcoin': btcAmount,
@@ -45,6 +61,7 @@ async function tvl() {
         'orca': orcaAmount, //+ (orcaOrcaSol/2) + (orcaOrcaUsdc/2),
         'solana': solAmount, //+ (orcaSolUsdt/2) + (orcaSolUsdc/2),
         'msol': msolAmount,
+        'terrausd': ustAmount,  
     }
 }
 
