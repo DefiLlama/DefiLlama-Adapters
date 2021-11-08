5d522e5f17db2a395d61f951258daa59545f8361
0xngmi <0xngmi@protonmail.com>
2021-11-08 02:37:36 +0000

    add otterclam

---
+++ projects/otterclam.js
@@ -0,0 +1,6 @@
+const { ohmTvl } = require('./helper/ohm')
+
+module.exports=ohmTvl("0xab328Ca61599974b0f577d1F8AB0129f2842d765", [
+    ["0xa3fa99a148fa48d14ed51d610c367c61876997f1", false],
+    ["0x8094f4c9a4c8ad1ff4c6688d07bd90f996c7ca21", true],
+], "polygon", "0xcF2A11937A906e09EbCb8B638309Ae8612850dBf", "0x4d6a30efbe2e9d7a9c143fce1c5bb30d9312a465")
