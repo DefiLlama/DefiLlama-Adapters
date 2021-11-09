75199c39c0fcf134cadec8f48ebe058eda4fc294
0xngmi <0xngmi@protonmail.com>
2021-11-09 05:11:57 +0000

    remove lyra double counting

--- projects/lyra/index.js
+++ projects/lyra/index.js
@@ -6,7 +6,7 @@ const v1_0_Pools = ['0x7Af4e1cE484f40D927b9C90fB6905Df4376fc3F6', '0xd7d974E8138
 const v1_1_LiquidityPool = ['0x2935CD347B79C319A6464fe3b1087170f142418C', '0x69B4B35504a8c1d6179fef7AdDCDB37A8c663BC9', '0x788843DE0Be1598155bFFaAB7Cfa2eCBd542E7f1']
 const v1_1_ShortCollateral = ['0x2935CD347B79C319A6464fe3b1087170f142418C', '0x69B4B35504a8c1d6179fef7AdDCDB37A8c663BC9', '0x0A68E15f8E289b9f1Ad1BCAD524FeA30C6125c2D']
 
-const pools = [...v1_0_Pools, ...v1_1_LiquidityPool, ...v1_1_ShortCollateral]
+const pools = [...new Set([...v1_0_Pools, ...v1_1_LiquidityPool, ...v1_1_ShortCollateral].map(t=>t.toLowerCase()))]
 
 const tokens = ['0x8c6f28f2f1a3c87f0f938b96d27520d9751ec8d9', '0xe405de8f52ba7559f9df3c368500b6e6ae6cee49']
 
