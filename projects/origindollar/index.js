4b4014dc8505816a46ad43439f30e928c5ec7727
0xngmi <0xngmi@protonmail.com>
2021-11-11 07:27:53 +0000

    fix origin dollar

--- projects/origindollar/index.js
+++ projects/origindollar/index.js
@@ -4,15 +4,6 @@ const abi = require("./abi.json");
 
 const vault = "0xE75D77B1865Ae93c7eaa3040B038D7aA7BC02F70";
 
-const strategies = [
-  // Compound
-  "0xD5433168Ed0B1F7714819646606DB509D9d8EC1f",
-  // Aave
-  "0x9f2b18751376cF6a3432eb158Ba5F9b1AbD2F7ce",
-  // Curve 3Pool
-  "0x3c5fe0a3922777343CBD67D3732FCdc9f2Fa6f2F",
-];
-
 const ethTvl = async (timestamp, ethBlock) => {
   const balances = {};
 
@@ -25,44 +16,17 @@ const ethTvl = async (timestamp, ethBlock) => {
     })
   ).output;
 
-  for (let i = 0; i < strategies.length; i++) {
-    for (let j = 0; j < stablecoins.length; j++) {
-      const supported = (
-        await sdk.api.abi.call({
-          abi: abi.supportsAsset,
-          target: strategies[i],
-          params: stablecoins[j],
-          block: ethBlock,
-        })
-      ).output;
-
-      if (supported) {
-        const balance_stablecoin = (
-          await sdk.api.abi.call({
-            abi: abi.checkBalance,
-            target: strategies[i],
-            params: stablecoins[j],
-            block: ethBlock,
-          })
-        ).output;
-
-        sdk.util.sumSingleBalance(balances, stablecoins[j], balance_stablecoin);
-      }
-    }
-  }
-
-  // Idle stablecoins in vault
   for (let i = 0; i < stablecoins.length; i++) {
-    const idle_balance = (
+    const balance_stablecoin = (
       await sdk.api.abi.call({
-        abi: erc20.balanceOf,
-        target: stablecoins[i],
-        params: vault,
+        abi: abi.checkBalance,
+        target: vault,
+        params: stablecoins[i],
         block: ethBlock,
       })
     ).output;
 
-    sdk.util.sumSingleBalance(balances, stablecoins[i], idle_balance);
+    sdk.util.sumSingleBalance(balances, stablecoins[i], balance_stablecoin);
   }
 
   return balances;
@@ -72,5 +36,4 @@ module.exports = {
   ethereum: {
     tvl: ethTvl,
   },
-  tvl: sdk.util.sumChainTvls([ethTvl]),
 };
