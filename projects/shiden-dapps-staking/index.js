565b6375645999351f398e01c865807d274565b9
Hyungsuk Kang <hskang9@github.com>
2021-11-15 19:28:23 +0900

    add shiden dapps-staking defillama adapter

---
+++ projects/shiden-dapps-staking/index.js
@@ -0,0 +1,22 @@
+
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+const { options } = require("@acala-network/api");
+
+const SDN_DECIMALS = 18;
+
+
+async function tvl() {
+  const provider = new WsProvider("wss://rpc.shiden.astar.network");
+  const api = await ApiPromise.create(options({ provider }));
+  const era = (await api.query.dappsStaking.currentEra())
+  const SDNLocked = (await api.query.dappsStaking.eraRewardsAndStakes(era)).toJSON() / 10 ** SDN_DECIMALS;
+
+  return {
+    'shiden': SDNLocked
+  }
+}
+
+module.exports = {
+  methodology: 'TVL considers SDN tokens deposited to the Dapps-Staking program',
+  tvl
+}
