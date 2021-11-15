26627939f7a662dcc192c739c78f9e5a3ede6838
Dan Lilienblum <dlilienblum@gmail.com>
2021-11-15 07:57:09 -0600

    Only1: include TVL

--- projects/only1.js
+++ projects/only1.js
@@ -1,18 +1,18 @@
 const axios = require("axios")
 const retry = require('async-retry')
 
-async function staking() {
+async function tvl() {
   const { data } = await axios.get("https://us-central1-only1-staking-stats.cloudfunctions.net/tvl")
 
   return {
-    'only1': data.totalTvl
+    only1: data.totalTvl
   }
 }
 
 module.exports = {
   methodology: "TVL is the sum of all tokens in the staking pools",
+  tvl,
   solana: {
-    staking,
-    tvl: async () => ({})
+    tvl
   }
 }
