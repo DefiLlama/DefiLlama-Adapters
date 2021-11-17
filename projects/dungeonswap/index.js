7fd6a15ea1785b023ee56cf372aac794d3e4e190
Mohammed18062020 <mohamed_el_hamdaoui93@hotmail.com>
2021-11-17 15:00:28 +0000

    Updating DungeonSwap

--- projects/dungeonswap/index.js
+++ projects/dungeonswap/index.js
@@ -1,12 +1,10 @@
 const sdk = require("@defillama/sdk");
-//const abi = require("./abi.json");
 const { addFundsInMasterChef } = require("../helper/masterchef");
 const { transformBscAddress } = require("../helper/portedTokens");
 
 const MasterChefContract = "0x3720F1F9a02BFB4dD6afb9030eB826B4392D321F";
-//const BABY = "";
 
-const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
+const bscTvl = async (chainBlocks) => {
   const balances = {};
 
   let transformAddress = await transformBscAddress();
@@ -17,11 +15,6 @@ const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
     chainBlocks["bsc"],
     "bsc",
     transformAddress,
-    /*abi.poolInfo,
-    [
-      "0x53E562b9B7E5E94b81f10e96Ee70Ad06df3D2657",
-      "0xdfAa0e08e357dB0153927C7EaBB492d1F60aC730",
-    ]*/
   );
 
   return balances;
