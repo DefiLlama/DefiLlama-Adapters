34c2a184d420905d2005354da26046d89303be2f
0xngmi <0xngmi@protonmail.com>
2021-11-19 16:53:22 +0000

    return coingecko ids directly on newdex

--- projects/newdex/index.js
+++ projects/newdex/index.js
@@ -2,11 +2,6 @@ const axios = require("axios");
 const retry = require('../helper/retry')
 const { get_currency_balance } = require("../helper/eos");
 
-async function simple_price(ids) {
-  const response = await retry(async () => await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=usd`));
-  return response.data;
-}
-
 const tokens = [
   ["eosio.token", "EOS", "eos"],
   ["tethertether", "USDT", "tether"],
@@ -22,13 +17,12 @@ const tokens = [
 ]
 
 async function get_account_tvl(account) {
-  let tvl = 0;
-  const price_feed = await simple_price(tokens.map(row => row[2]).join(","));
+  const balances = {}
   for ( const [ code, symbol, id ] of tokens ) {
     const balance = await get_currency_balance(code, account, symbol);
-    tvl += balance * price_feed[id].usd;
+    balances[id] = balance
   }
-  return tvl;
+  return balances;
 }
 
 // https://newdex.io
@@ -37,14 +31,9 @@ async function eos() {
   return await get_account_tvl("newdexpublic");
 }
 
-async function fetch() {
-  return await eos();
-}
-
 module.exports = {
   methodology: `NewDex TVL is achieved by querying token balances from NewDex's limit orderbook smart contract.`,
   eos: {
-    fetch: eos
+    tvl: eos
   },
-  fetch
 }
