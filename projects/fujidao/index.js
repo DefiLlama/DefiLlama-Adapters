eb89458cb3c340e4cbc0f93ef9947807d9845314
0xngmi <0xngmi@protonmail.com>
2021-11-20 01:56:53 +0000

    add fujidao fantom

--- projects/fujidao/index.js
+++ projects/fujidao/index.js
@@ -3,7 +3,7 @@ const abi = require('./abi.json')
 const sdk = require('@defillama/sdk')
 const { default: BigNumber } = require('bignumber.js')
 
-const contracts = [
+const ethContracts = [
     {
         name: "F1155 Core",
         address: "0x1Cf24e4eC41DA581bEe223E1affEBB62a5A95484",
@@ -20,44 +20,65 @@ const weth = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 const wbtc = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
 const ftm = "0x4E15361FD6b4BB609Fa63C81A2be19d873717870";
 
-async function tvl(_timestamp, block){
-
-    const marketsupply = async (contract) => {
-        return await sdk.api.abi.multiCall(
-            {
-                abi: abi.totalSupply,
-                calls: (contract.ids).map( id => ({
-                    target:(contract.address),
-                    params: [id]
-                })),
-                block
-            }
-        );
-    }
-
-    const allMarketSupplies = async () => {
-        let allMarkets;
-        for (let index = 0; index < contracts.length; index++) {
-            if (!allMarkets) {
-                allMarkets = await marketsupply(contracts[index]);
-            } else {
-                let temp = allMarkets.output;
-                let response = await marketsupply(contracts[index]);
-                response = response.output;
-                allMarkets.output = temp.concat(response);
-            } 
+const marketsupply = async (contract, block, chain) => {
+    return await sdk.api.abi.multiCall(
+        {
+            abi: abi.totalSupply,
+            calls: (contract.ids).map( id => ({
+                target:(contract.address),
+                params: [id]
+            })),
+            block,
+            chain
         }
-        return allMarkets;
-    }
+    );
+}
 
-    const supplies = await allMarketSupplies();
+const allMarketSupplies = async (contracts, block, chain) => {
+    let allMarkets;
+    for (let index = 0; index < contracts.length; index++) {
+        if (!allMarkets) {
+            allMarkets = await marketsupply(contracts[index], block, chain);
+        } else {
+            let temp = allMarkets.output;
+            let response = await marketsupply(contracts[index], block, chain);
+            response = response.output;
+            allMarkets.output = temp.concat(response);
+        } 
+    }
+    return allMarkets;
+}
+
+async function eth(_timestamp, block){
+    const supplies = await allMarketSupplies(ethContracts, block, "ethereum");
 
     return {
         [weth]: supplies.output.reduce((t,v) => t.plus(v.output), BigNumber(0)).toFixed(0)
     }
 }
 
+const fantomContracts = [
+    {
+        name: "F1155 Core",
+        address: "0xB4E2eC87f8E6E166929A900Ed433c4589d721D70",
+        ids: [0, 2, 4]
+    },
+];
+
+async function fantom(_timestamp, ethBlock, chainBlocks){
+    const supplies = await allMarketSupplies(fantomContracts, chainBlocks.fantom, "fantom");
+
+    return {
+        [ftm]: supplies.output.reduce((t,v) => t.plus(v.output), BigNumber(0)).toFixed(0)
+    }
+}
+
 module.exports = {
-    tvl,
+    ethereum:{
+        tvl:eth
+    },
+    fantom:{
+        tvl: fantom
+    },
     methodology: "Counts balance of receipt tokens in F1155 Contract on all vaults."
 }
