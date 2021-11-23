f2c01a455b6bf03d6f985205d5e70bf5dfca7eec
Aron Turner <aron.r.turner@gmail.com>
2021-11-22 20:16:30 -0800

    changed as per defillamas specs

--- projects/yieldly/index.js
+++ projects/yieldly/index.js
@@ -2,20 +2,50 @@
 const axios = require("axios");
 
 async function tvl() {
-  let usdTVL = 0;
+  let nllTVL = await axios
+    .get("https://data.yieldly.finance/tvl/nll")
+    .then((body) => {
+      return body.data.nllTVL;
+    })
+    .catch(() => {
+      return 0;
+    });
 
-  await axios.get("https://data.yieldly.finance/tvl").then((body) => {
-    usdTVL = body.data.usdTVL;
-  });
+  let algoPrice = await axios
+    .get("https://api1.binance.com/api/v3/ticker/price?symbol=ALGOUSDT")
+    .then((response) => {
+      return response.data.price;
+    })
+    .catch(() => {
+      return 0;
+    });
 
   return {
-    tvl: usdTVL,
+    tvl: (algoPrice * nllTVL) / 10 ** 6,
   };
 }
 
+async function staking() {
+  // Get the total TVL (Both NLL & Staking)
+  let totalTVLUSD = await axios
+    .get("https://data.yieldly.finance/tvl")
+    .then((body) => {
+      return body.data.usdTVL;
+    })
+    .catch(() => {
+      return 0;
+    });
+
+  // Get NLL TVL in USD
+  let nllTVLUSD = await tvl();
+
+  // Get the staking amount TVL
+  return totalTVLUSD - nllTVLUSD.tvl;
+}
+
 module.exports = {
-  algorand: {
-    tvl: tvl,
+  staking: {
+    tvl: staking,
   },
-  tvl: tvl,
+  tvl,
 };
