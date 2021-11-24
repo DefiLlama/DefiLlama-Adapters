bb1f7bdb5a336b64401411278903fa393648f5bf
0xngmi <0xngmi@protonmail.com>
2021-11-24 06:31:10 +0000

    fix defikingdoms

--- projects/defikingdoms/index.js
+++ projects/defikingdoms/index.js
@@ -1,6 +1,8 @@
 const { request, gql } = require("graphql-request");
-const sdk = require("@defillama/sdk");
-const { toUSDTBalances } = require("../helper/balances");
+const { calculateUniTvl } = require("../helper/calculateUniTvl");
+const { staking } = require("../helper/staking");
+const { getBlock } = require("../helper/getBlock");
+const { fixHarmonyBalances, transformHarmonyAddress } = require("../helper/portedTokens");
 
 const graphUrl =
   "https://graph.defikingdoms.com/subgraphs/name/defikingdoms/dex";
@@ -14,50 +16,31 @@ const graphQuery = gql`
     }
   }
 `;
-const jewelLiquidity = gql`
-  query get_tvl($block: Int) {
-    token(id: "0x72cb10c6bfa5624dd07ef608027e366bd690048f") {
-      symbol
-      tokenDayData(orderDirection: desc) {
-        totalLiquidityUSD
-      }
-    }
-  }
-`;
 
-async function tvl(timestamp) {
-  const { block } = await sdk.api.util.lookupBlock(timestamp, {
-    chain: "harmony",
-  });
-  const response = await request(graphUrl, graphQuery, {
-    block,
-  });
-
-  const usdTvl = Number(response.uniswapFactory.totalLiquidityUSD);
-
-  return toUSDTBalances(usdTvl);
-
+const transforms = {
+  "0x224e64ec1bdce3870a6a6c777edd450454068fec": "0xa47c8bf37f92abed4a126bda807a7b7498661acd", // ust
+  "0xb12c13e66ade1f72f71834f2fc5082db8c091358": "avax:0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7", //avax
+  "0x735abe48e8782948a37c7765ecb76b98cde97b0f": "0x4e15361fd6b4bb609fa63c81a2be19d873717870", //ftm
+  "0xfbdd194376de19a88118e84e279b977f165d01b8": "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0", //matic
+  // missing luna
 }
 
-async function stakingTvl(timestamp) {
-  const { block } = await sdk.api.util.lookupBlock(timestamp, {
-    chain: "harmony",
-  });
-  const response = await request(graphUrl, jewelLiquidity, {
-    block,
-  });
+const factory = "0x9014B937069918bd319f80e8B3BB4A2cf6FAA5F7"
+async function tvl(timestamp, _ethBlock, chainBlocks) {
+  const block = await getBlock(timestamp, "harmony", chainBlocks);
+  const transformAddress = addr=>{
+    return transforms[addr.toLowerCase()] ?? `harmony:${addr}`;
+  }
+  const balances = await calculateUniTvl(transformAddress, block, "harmony", factory, 0, true)
+  fixHarmonyBalances(balances)
 
-  const usdTvl = Number(
-    response.token.tokenDayData.slice(-1)[0].totalLiquidityUSD
-  );
+  return balances
 
-  return toUSDTBalances(usdTvl);
 }
 
 module.exports = {
   harmony: {
     tvl,
-    staking: stakingTvl,
+    staking: staking("0xa9ce83507d872c5e1273e745abcfda849daa654f", "0x72cb10c6bfa5624dd07ef608027e366bd690048f", "harmony"),
   },
-  tvl,
 };
