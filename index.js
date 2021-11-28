9a9febda3e10e80de5aace150bb3cbfcc0e4326f
Define101 <93603962+Define101@users.noreply.github.com>
2021-11-28 18:14:40 +0000

    pidao finance listing

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const { ohmTvl } = require('../helper/ohm')
 
-const treasury = "0xa3904e99b6012eb883db1090d02d4e954539ec61"
+const treasury = "0x458B737d87C40252EC11b160C76bb1C53F248b28"
 module.exports = ohmTvl(treasury, [
-    ["0xcde0a00302cf22b3ac367201fbd114cefa1729b4", true],
-    ["0x4dabf6c57a8bea012f1eaa1259ceed2a62ac7df2", true],
-    ["0xf8838fcc026d8e1f40207acf5ec1da0341c37fe2", true],
-    ["0x224e64ec1bdce3870a6a6c777edd450454068fec", false],
-], "harmony", "0xeea71889c062c135014ec34825a1958c87a2ac61", "0xed0b4b0f0e2c17646682fc98ace09feb99af3ade")
+    //busd 
+    ["0xe9e7cea3dedca5984780bafc599bd69add087d56", false],
+    //busd-usd
+    ["0x55d398326f99059ff775485246999027b3197955", false],
+    //TrueUSD
+    ["0x14016e85a25aeb13065688cafb43044c2ef86784", false],
+    //MDEX LP
+    ["0x149a712378aba63882c1d372739eb47a6fd0e12e", true],
+    //Pancake LPs
+    ["0x3991e0988a69e4c8fde46c011dafe55e26fdd18d", true],
+    //Pancake LPs 1
+    ["0xc0d02c7fc87e50a6580c4091d9d76bb7e38b05b8", true],
+], "bsc", "0x22d1704817D42bdE262d778bFE097e287151371D", "0x1215ed20aa507578ca352e195016f289e7a17f3a")
