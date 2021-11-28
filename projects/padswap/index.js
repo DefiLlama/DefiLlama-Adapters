053efb6d5c88c2cb8a095794c0126cd38c2c3d0a
0xngmi <0xngmi@protonmail.com>
2021-11-28 08:56:59 +0000

    simplify pad

--- projects/padswap/index.js
+++ projects/padswap/index.js
@@ -1,7 +1,6 @@
 const { gql, GraphQLClient } = require("graphql-request");
-const { toUSDTBalances } = require("../helper/balances");
-const sdk = require("@defillama/sdk");
-const BigNumber = require("bignumber.js");
+const { stakings } = require("../helper/staking");
+const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
 
 const TOAD_ADDRESS = "0x463e737d8f740395abf44f7aac2d9531d8d539e9";
 const TOAD_FARM_ADDRESS = "0xe1F1EDfBcEfB1E924e4a031Ed6B4CAbC7e570154";
@@ -22,95 +21,20 @@ const movrClient = new GraphQLClient(
   "https://api.thegraph.com/subgraphs/name/toadguy/padswap-subgraph-moonriver"
 );
 
-const tvlQuery = gql`
-  query get_tvl($factoryId: String, $block: Int) {
-    uniswapFactory(id: $factoryId, block: { number: $block }) {
-      totalVolumeUSD
-      totalLiquidityUSD
-    }
-  }
-`;
-
-const toadPriceQuery = gql`
-  query get_toad_reserve($block: Int) {
-    pair(
-      id: "0xef6421325f7ed6ebeeaa2b092f9cc64ee2aa3b3b"
-      block: { number: $block }
-    ) {
-      reserve0
-      reserveUSD
-    }
-  }
-`;
-
-async function getToadPrice(chainBlocks) {
-  const { pair } = await bscClient.request(toadPriceQuery, {
-    block: chainBlocks["bsc"],
-  });
-  const toadReserve = pair.reserveUSD / 2;
-  const toadPrice = toadReserve / pair.reserve0;
-  return toadPrice;
-}
-
-async function bscTvl(timestamp, block, chainBlocks) {
-  const { uniswapFactory } = await bscClient.request(tvlQuery, {
-    factoryId: PADSWAP_BSC_FACTORY_ADDRESS,
-    block: chainBlocks["bsc"],
-  });
-  const usdTvl = Number(uniswapFactory.totalLiquidityUSD);
-  return toUSDTBalances(usdTvl);
-}
-
-async function movrTvl(timestamp, block, chainBlocks) {
-  const movrBlock = await sdk.api.util.lookupBlock(timestamp, {
-    chain: "moonriver",
-  });
-  const { uniswapFactory } = await movrClient.request(tvlQuery, {
-    factoryId: PADSWAP_MOONRIVER_FACTORY_ADDRESS,
-    block: movrBlock.block,
-  });
-  const usdTvl = Number(uniswapFactory.totalLiquidityUSD);
-  return toUSDTBalances(usdTvl);
-}
-
-async function staking(timestamp, block, chainBlocks) {
-  const { output: toadFarmOutput } = await sdk.api.erc20.balanceOf({
-    block: chainBlocks["bsc"],
-    target: TOAD_ADDRESS,
-    owner: TOAD_FARM_ADDRESS,
-    chain: "bsc",
-  });
-  const { output: padswapV1Output } = await sdk.api.erc20.balanceOf({
-    block: chainBlocks["bsc"],
-    target: TOAD_ADDRESS,
-    owner: TOAD_PADSWAP_FARM_V1_ADDRESS,
-    chain: "bsc",
-  });
-  const { output: padswapV2Output } = await sdk.api.erc20.balanceOf({
-    block: chainBlocks["bsc"],
-    target: TOAD_ADDRESS,
-    owner: TOAD_PADSWAP_FARM_V2_ADDRESS,
-    chain: "bsc",
-  });
-  const toadBalanceBn = BigNumber(toadFarmOutput)
-    .plus(BigNumber(padswapV1Output))
-    .plus(BigNumber(padswapV2Output));
-  const toadBalance = toadBalanceBn.times(10 ** -18).toNumber();
-  const toadPrice = await getToadPrice(chainBlocks);
-  const usdTvl = toadBalance * toadPrice;
-  const balances = toUSDTBalances(usdTvl);
-  return balances;
-}
-
 module.exports = {
   methodology: `TVL accounts for the liquidity on all AMM pools (see https://info.padswap.exchange/ and https://movr-info.padswap.exchange/). Staking includes all TOAD staked in TOAD farms.`,
   misrepresentedTokens: true,
   bsc: {
-    tvl: bscTvl,
-    staking,
+    tvl: calculateUsdUniTvl(PADSWAP_BSC_FACTORY_ADDRESS, "bsc", "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", [
+      "0x463e737d8f740395abf44f7aac2d9531d8d539e9", //toad
+      "0xc0888d80ee0abf84563168b3182650c0addeb6d5", //pad
+    ], "wbnb"),
+    staking: stakings([TOAD_PADSWAP_FARM_V1_ADDRESS, TOAD_PADSWAP_FARM_V2_ADDRESS], TOAD_ADDRESS, "bsc"),
   },
-  moonriver: {
-    tvl: movrTvl,
-  },
-  tvl: sdk.util.sumChainTvls([bscTvl, movrTvl]),
+  moonriver:{
+    tvl: calculateUsdUniTvl(PADSWAP_MOONRIVER_FACTORY_ADDRESS, "moonriver", "0x663a07a2648296f1a3c02ee86a126fe1407888e5", [
+      "0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d", //usdc
+      "0x45488c50184ce2092756ba7cdf85731fd17e6f3d", //pad
+    ], "moonriver"),
+  }
 };
