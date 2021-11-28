182f5919609b59af1d2a92246693235b08092a35
nemusona <nemusonaneko@protonmail.com>
2021-11-28 14:05:28 -0600

    polycat uses onchain

--- projects/polycat/index.js
+++ projects/polycat/index.js
@@ -1,79 +1,21 @@
-const sdk = require("@defillama/sdk");
-const { request, gql } = require("graphql-request");
-const queryUrl =
-  "https://api.thegraph.com/subgraphs/name/polycatfi/polycat-finance-amm";
-const { BigNumber } = require("bignumber.js");
+const { calculateUniTvl } = require("../helper/calculateUniTvl");
+const factory = "0x477Ce834Ae6b7aB003cCe4BC4d8697763FF456FA";
 
-const queryFieldPolygonTvl = gql`
-  query getPools($block: Int) {
-    pairs(orderBy: reserveUSD, orderDirection: desc) {
-      token0 {
-        symbol
-      }
-      token1 {
-        symbol
-      }
-      reserveUSD
-    }
-  }
-`;
-
-async function polygonTvl(timestmap, blocks, chainBlocks) {
-  let balances = {};
-  let { pairs: result } = await request(queryUrl, queryFieldPolygonTvl);
-  for (let i = 0; i < result.length; i++) {
-    let symbol0 = result[i].token0.symbol;
-    let symbol1 = result[i].token1.symbol;
-    if (
-      symbol0 === "PAW" ||
-      symbol1 === "PAW" ||
-      symbol0 === "FISH" ||
-      symbol1 === "FISH"
-    ) {
-      continue;
-    }
-    let reserve = BigNumber(result[i].reserveUSD)
-      .times(10 ** 6)
-      .toFixed(0);
-    sdk.util.sumSingleBalance(
-      balances,
-      `polygon:${"0x2791bca1f2de4661ed88a30c99a7a9449aa84174"}`,
-      reserve
-    );
-  }
-  return balances;
-}
-
-async function polygonPool2Tvl(timestamp, blocks, chainBlocks) {
-  let balances = {};
-  let { pairs: result } = await request(queryUrl, queryFieldPolygonTvl);
-  for (let i = 0; i < result.length; i++) {
-    let symbol0 = result[i].token0.symbol;
-    let symbol1 = result[i].token1.symbol;
-    if (
-      symbol0 === "PAW" ||
-      symbol1 === "PAW" ||
-      symbol0 === "FISH" ||
-      symbol1 === "FISH"
-    ) {
-      let reserve = BigNumber(result[i].reserveUSD)
-        .times(10 ** 6)
-        .toFixed(0);
-      sdk.util.sumSingleBalance(
-        balances,
-        `polygon:${"0x2791bca1f2de4661ed88a30c99a7a9449aa84174"}`,
-        reserve
-      );
-    }
-  }
-  return balances;
+async function tvl(timestamp, block, chainBlocks) {
+  return calculateUniTvl(
+    (addr) => `polygon:${addr}`,
+    chainBlocks.polygon,
+    "polygon",
+    factory,
+    0,
+    true
+  );
 }
 
 module.exports = {
-  methodology: "Pool2 TVL are pairs that include PAW or FISH and regular TVL are the rest. TVL for each pool are the result of the reserveUSD call to the graph.",
+  methodology: "TVL are from the pools created by the factory",
   polygon: {
-    tvl: polygonTvl,
-    pool2: polygonPool2Tvl,
+    tvl,
   },
-  tvl: polygonTvl
+  tvl,
 };
