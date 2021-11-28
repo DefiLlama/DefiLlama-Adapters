6cbdcacdf03bc4084258bb6a4db753d019b29883
0xngmi <0xngmi@protonmail.com>
2021-11-28 19:56:21 +0000

    fix xensa

--- projects/xensa/index.js
+++ projects/xensa/index.js
@@ -1,84 +1,52 @@
 const sdk = require("@defillama/sdk");
 const _us = require('underscore');
 const abi = require('./abi.json');
-const {transformAddress} = require('../helper/portedTokens')
+const { getBlock } = require("../helper/getBlock");
 
 const _xensaCoreAddress = '0xd1242313461dd533279f0cac0dbc06ecdb878a79';
 
-async function tvl(_, _ethBlock, chainBlocks) {
-
+async function tvl(timestamp, _ethBlock, chainBlocks) {
+  const block = await getBlock(timestamp, "okexchain", chainBlocks, true)
   const reserves_xensa = (
     await sdk.api.abi.call({
       target: _xensaCoreAddress,
       abi: abi["getReserves"],
-      block: chainBlocks['okexchain'],
+      block,
       chain: 'okexchain'
     })
-  ).output;
-
-  const decimalsOfReserve = (
-    await sdk.api.abi.multiCall({
-      calls: _us.map(reserves_xensa, (reserve) => ({
-        target: reserve
-      })),
-      abi: "erc20:decimals",
-      block: chainBlocks['okexchain'],
-      chain: 'okexchain'
-    })
-  ).output;
-
-  const assets = [];
-  reserves_xensa.map((reserve, i) => {
-
-    let symbol;
-
-    let decimals = decimalsOfReserve[i];
-    if (decimals.success) {
-      assets.push({
-        address: reserve,
-      })
-    } else {
-      throw new Error("Call failed")
-    }
-  })
+  ).output.filter(addr=>addr!=="0x1111111111111111111111111111111111111111");
 
   const balance_okt = (
     await sdk.api.eth.getBalance({
       target: _xensaCoreAddress,
-      block: chainBlocks['okexchain'],
+      block,
       chain: 'okexchain'
     })
   ).output;
 
 
   const balanceOfResults = await sdk.api.abi.multiCall({
-    block: chainBlocks['okexchain'],
+    block,
     chain: 'okexchain',
-    calls: _us.map(assets, (reserve) => ({
-      target: reserve.address,
+    calls: _us.map(reserves_xensa, (reserve) => ({
+      target: reserve,
       params: _xensaCoreAddress,
     })),
     abi: "erc20:balanceOf",
   })
 
   const balances = {};
-  const transform = await transformAddress()
-
-  assets.forEach((_item,_i)=>{
-    balances[transform(_item.address)] = balanceOfResults.output[_i].output;
-  })
-
   balances['okexchain:0x8f8526dbfd6e38e3d8307702ca8469bae6c56c15'] = balance_okt
+  const transform = addr=>`okexchain:${addr}`// await transformAddress()
+  sdk.util.sumMultiBalanceOf(balances, balanceOfResults, true, transform)
 
   return balances;
 
 }
 
 module.exports = {
-  misrepresentedTokens: true,
   methodology: 'Using the same methodology applied to other lending platforms, TVL for Xensa consists deposits made to the protocol and borrowed tokens are not counted.',
   okexchain:{
     tvl,
   },
-  tvl
 };
