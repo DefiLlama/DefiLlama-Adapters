8a55aef1d150735ee38029c0754934ad263c28c9
0xngmi <0xngmi@protonmail.com>
2021-12-01 16:28:33 +0000

    make flamedefi return tvl in texos

--- projects/flamedefi/index.js
+++ projects/flamedefi/index.js
@@ -11,15 +11,16 @@ const tvlQuery = gql`
   }
 `;
 
-async function fetch() {
+async function tvl() {
     const data = (await request(graphUrl, tvlQuery));
     const totalLiquidity = new BigNumber(data.spaceFarm.totalStackTez);
-    const xtzUsdQuote = new BigNumber((await axios('https://api.coinbase.com/v2/prices/XTZ-USD/spot')).data.data.amount);
-    return totalLiquidity.multipliedBy(xtzUsdQuote).toFixed(0);
+    return {
+      tezos: totalLiquidity
+    }
 }
 
 module.exports = {
     methodology: 'TVL counts the liquidity of FlameDefi farms. Data is pulled from:"https://api2.spacefarm.xyz/api".',
     misrepresentedTokens: true,
-    fetch
+    tvl
 }
