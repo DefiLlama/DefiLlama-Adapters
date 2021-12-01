ad47831e9247cbfeeee3499a40dacb1c991cefb1
0xngmi <0xngmi@protonmail.com>
2021-12-01 15:05:48 +0000

    add chains to o3swap

--- projects/o3swap.js
+++ projects/o3swap.js
@@ -4,42 +4,28 @@ async function fetch() {
     const tvl = await fetchURL('https://monitor.api.o3swap.com/v1/statistics')
     return tvl.data.data.pool_tvl
 }
-async function fetchEthereum(poolIndex) {
-    const tvl = await fetchURL('https://monitor.api.o3swap.com/v1/statistics')
-    return tvl.data.data.pools[0].tvl
-}
-async function fetchBsc() {
-    const tvl = await fetchURL('https://monitor.api.o3swap.com/v1/statistics')
-    return tvl.data.data.pools[1].tvl
-}
-async function fetchHeco() {
-    const tvl = await fetchURL('https://monitor.api.o3swap.com/v1/statistics')
-    return tvl.data.data.pools[2].tvl
-}
-async function fetchPolygon() {
-    const tvl = await fetchURL('https://monitor.api.o3swap.com/v1/statistics')
-    return tvl.data.data.pools[3].tvl
-}
-async function fetchArbitrum() {
-    const tvl = await fetchURL('https://monitor.api.o3swap.com/v1/statistics')
-    return tvl.data.data.pools[4].tvl
+
+const chainsToIndex = {
+    ethereum: 0,
+    bsc: 1,
+    heco: 2,
+    polygon: 3,
+    arbitrum: 4,
+    okexchain: 5,
+    avalanche: 6,
 }
 
 module.exports = {
-    ethereum: {
-        fetch: fetchEthereum,
-    },
-    bsc: {
-        fetch: fetchBsc,
-    },
-    heco: {
-        fetch: fetchHeco,
-    },
-    polygon: {
-        fetch: fetchPolygon,
-    },
-    arbitrum: {
-        fetch: fetchArbitrum,
-    },
+    ...Object.entries(chainsToIndex).reduce((exp, [chain, id]) => {
+        return {
+            ...exp,
+            [chain]: {
+                fetch: async () => {
+                    const tvl = await fetchURL('https://monitor.api.o3swap.com/v1/statistics')
+                    return tvl.data.data.pools[id].tvl
+                }
+            }
+        }
+    }, {}),
     fetch,
 }
