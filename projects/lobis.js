cf7107a8879aab91c28a532507bf55779c582f20
Define101 <93603962+Define101@users.noreply.github.com>
2021-12-01 13:29:24 +0000

    Update Lobis
    Update Lobis

--- projects/lobis.js
+++ projects/lobis.js
@@ -1,26 +1,11 @@
-const sdk = require("@defillama/sdk")
+const { ohmTvl } = require('../helper/ohm')
 
-const stakingContract = '0x3818eff63418e0a0ba3980aba5ff388b029b6d90'
-const LOBI = '0xdec41db0c33f3f6f3cb615449c311ba22d418a8d'
-
-async function staking(timestamp, ethBlock, chainBlocks) {  
-  const stakedLOBI = (await sdk.api.abi.call({
-      target: LOBI, 
-      params: stakingContract,
-      abi: 'erc20:balanceOf',
-      block: ethBlock,
-      chain: 'ethereum'
-    })).output
-  const balances = {
-    [LOBI]: stakedLOBI
-  }
-  return balances
-}
-
-module.exports = {
-  methodology: "LOBI can be staked in a staking contract.",
-  ethereum: {
-    staking,
-    tvl: () => ({})
-  },
-}
+const treasury = "0x873ad91fA4F2aA0d557C0919eC3F6c9D240cDd05"
+module.exports = ohmTvl(treasury, [
+    //curve 
+    ["0xD533a949740bb3306d119CC777fa900bA034cd52", false],
+    //frax
+    ["0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0", false],
+    //Sushi LP
+    ["0x2734f4a846d1127f4b5d3bab261facfe51df1d9a", true],
+], "eth", "0x3818eff63418e0a0ba3980aba5ff388b029b6d90", "0xdec41db0c33f3f6f3cb615449c311ba22d418a8d")
