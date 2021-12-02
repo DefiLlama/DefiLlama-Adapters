a8080b6e82fca15de27cbb432ed9635dedbd31ff
bananafarmlab <bananafarmlab@gmail.com>
2021-12-03 06:52:51 +0800

    fixed(projects/banana): api split and add total

--- projects/banana/index.js
+++ projects/banana/index.js
@@ -3,6 +3,12 @@ const axios = require("axios");
 // const share = "0xc67b9b1b0557aeafa10aa1ffa1d7c87087a6149e";
 // const pool2lp = "0x59b901160bb8eeec517ed396ab68e0da81707c12";
 
+const fetch = async () => {
+  const { data = {} } =
+    (await axios.get("https://bananafarm.io/api/boba/tvl/total")) || {};
+  return data.tvl || 0;
+};
+
 const tvl = async () => {
   const { data = {} } =
     (await axios.get("https://bananafarm.io/api/boba/tvl")) || {};
@@ -23,6 +29,12 @@ const pool2 = async () => {
   // console.log(2, data);
   return data;
 };
+const totalInUsd = async () => {
+  const { data = {} } =
+    (await axios.get("https://bananafarm.io/api/boba/tvl/pool2")) || {};
+  // console.log(2, data);
+  return data;
+};
 
 module.exports = {
   methodology: `TVL is calculated by summing up the values of all LP tokens and our project's rewards token.`,
@@ -30,8 +42,9 @@ module.exports = {
   token: "BANA",
   start: 1638237600,
   boba: {
-    tvl,
     pool2,
     staking,
+    tvl,
   },
+  fetch,
 };
