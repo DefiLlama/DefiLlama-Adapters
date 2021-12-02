3b7d6a44719756d115b0c7d8d1e3ebbf27714673
bananafarmlab <bananafarmlab@gmail.com>
2021-12-02 09:24:56 +0800

    fix(projects): add banana pool2 staking

--- projects/banana/index.js
+++ projects/banana/index.js
@@ -1,10 +1,33 @@
 const axios = require("axios");
+const BigNumber = require("bignumber.js");
 
 const tvlUrl = "https://bananafarm.io/api/boba/tvl";
 
+const stakingPid = "0";
+const poolPid = "1";
+
+const share = "0xc67b9B1B0557aeAfA10AA1fFa1d7c87087a6149E";
+const pool2lp = "0x59b901160bb8eeEc517eD396Ab68e0DA81707c12";
+
 const fetch = async () => {
-  const { data } = await axios.get(tvlUrl) || {};
+  const { data } = (await axios.get(tvlUrl)) || {};
   return data?.tvl || 0;
+};
+
+const staking = async () => {
+  const { data = {} } = (await axios.get(tvlUrl)) || {};
+  const tokenAddress = `boba:${share}`;
+  return {
+    [tokenAddress]: new BigNumber(data[stakingPid] * 1e18).toFixed(0),
+  };
+};
+
+const pool2 = async () => {
+  const { data = {} } = (await axios.get(tvlUrl)) || {};
+  const tokenAddress = `boba:${pool2lp}`;
+  return {
+    [tokenAddress]: new BigNumber(data[poolPid] * 1e18).toFixed(0),
+  };
 };
 
 module.exports = {
@@ -13,7 +36,11 @@ module.exports = {
   token: "BANA",
   start: 1638237600,
   boba: {
-    fetch
+    staking,
+    pool2,
+    fetch,
   },
-  fetch
+  staking,
+  pool2,
+  fetch,
 };
