bc068f7c6ab7a58921cece77155b656c06afe1a6
bananafarmlab <bananafarmlab@gmail.com>
2021-12-03 07:14:51 +0800

    fixed(projects/banana): use sdk

--- projects/banana/index.js
+++ projects/banana/index.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
+const { pool2BalanceFromMasterChefExports } = require("../helper/pool2.js");
+const masterchefAbi = require("../helper/abis/masterchef.json");
 
-// const share = "0xc67b9b1b0557aeafa10aa1ffa1d7c87087a6149e";
+const share = "0xc67b9b1b0557aeafa10aa1ffa1d7c87087a6149e";
 // const pool2lp = "0x59b901160bb8eeec517ed396ab68e0da81707c12";
+const masterChef = "0x0e69359B4783094260abFaD7dD904999fc1d6Fd0";
 
 const fetch = async () => {
   const { data = {} } =
@@ -29,12 +32,6 @@ const pool2 = async () => {
   // console.log(2, data);
   return data;
 };
-const totalInUsd = async () => {
-  const { data = {} } =
-    (await axios.get("https://bananafarm.io/api/boba/tvl/pool2")) || {};
-  // console.log(2, data);
-  return data;
-};
 
 module.exports = {
   methodology: `TVL is calculated by summing up the values of all LP tokens and our project's rewards token.`,
@@ -42,9 +39,14 @@ module.exports = {
   token: "BANA",
   start: 1638237600,
   boba: {
-    pool2,
-    staking,
-    tvl,
+    pool2: pool2BalanceFromMasterChefExports(
+      masterChef,
+      share,
+      "boba",
+      (addr) => `boba:${addr}`,
+      masterchefAbi.poolInfo
+    ),
+    staking: staking(masterChef, share, "boba"),
   },
   fetch,
 };
