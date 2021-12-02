90db8f561cb128629b1e4cfc36a957caaef5b611
0xngmi <0xngmi@protonmail.com>
2021-12-02 23:16:50 +0000

    fix apeswap

--- projects/apeswap/index.js
+++ projects/apeswap/index.js
@@ -1,5 +1,6 @@
 const sdk = require("@defillama/sdk");
-const {calculateUniTvl} = require('../helper/calculateUniTvl.js')
+const {calculateUniTvl} = require('../helper/calculateUniTvl.js');
+const { staking } = require("../helper/staking.js");
 
 const BANANA_TOKEN = '0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a95'
 const MASTER_APE = '0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9'
@@ -10,36 +11,23 @@ const SUBGRAPH_BSC = "https://graph2.apeswap.finance/subgraphs/name/ape-swap/ape
 const SUBGRAPH_POLYGON = "https://api.thegraph.com/subgraphs/name/apeswapfinance/dex-polygon" 
 
 async function bscTvl(timestamp, block, chainBlocks) {
-  return calculateUniTvl(addr=>`bsc:${addr}`, chainBlocks['bsc'], 'bsc', FACTORY_BSC, 0, true);
+  const balances = await calculateUniTvl(addr=>`bsc:${addr}`, chainBlocks['bsc'], 'bsc', FACTORY_BSC, 0, true);
+  delete balances["bsc:0x95e7c70b58790a1cbd377bc403cd7e9be7e0afb1"] //YSL - coingecko price broken
+  return balances
 }
 
 async function polygonTvl(timestamp, block, chainBlocks) {
   return calculateUniTvl(addr=>`polygon:${addr}`, chainBlocks['polygon'], 'polygon', FACTORY_POLYGON, 0, true);
 }
 
-async function poolsTvl(timestamp, ethBlock, chainBlocks) {
-    const balances = {};
-    const stakedBanana = sdk.api.erc20.balanceOf({
-      target: BANANA_TOKEN,
-      owner: MASTER_APE,
-      chain: 'bsc',
-      block: chainBlocks.bsc
-    })
-    sdk.util.sumSingleBalance(balances, 'bsc:' + BANANA_TOKEN, (await stakedBanana).output)
-    return balances
-}
-
 module.exports = {
   misrepresentedTokens: true,
   bsc:{
     tvl: bscTvl,
+    staking: staking(MASTER_APE, BANANA_TOKEN, "bsc")
   },
   polygon:{
     tvl: polygonTvl,
   },
-  staking:{
-    tvl: poolsTvl,
-  },
   methodology: "TVL comes from the DEX liquidity pools, staking TVL is accounted as the banana on 0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9",
-  tvl: sdk.util.sumChainTvls([polygonTvl, bscTvl])
 }
