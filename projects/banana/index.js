1dd761e7f530a6e2fe481298ced00fb5af859a1f
bananafarmlab <bananafarmlab@gmail.com>
2021-12-03 07:16:33 +0800

    fixed(projects/banana): use sdk

--- projects/banana/index.js
+++ projects/banana/index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const { pool2BalanceFromMasterChefExports } = require("../helper/pool2.js");
+const { staking } = require("../helper/staking");
 const masterchefAbi = require("../helper/abis/masterchef.json");
 
 const share = "0xc67b9b1b0557aeafa10aa1ffa1d7c87087a6149e";
@@ -12,26 +13,26 @@ const fetch = async () => {
   return data.tvl || 0;
 };
 
-const tvl = async () => {
-  const { data = {} } =
-    (await axios.get("https://bananafarm.io/api/boba/tvl")) || {};
-  // console.log(0, data);
-  return data;
-};
+// const tvl = async () => {
+//   const { data = {} } =
+//     (await axios.get("https://bananafarm.io/api/boba/tvl")) || {};
+//   // console.log(0, data);
+//   return data;
+// };
 
-const staking = async () => {
-  const { data = {} } =
-    (await axios.get("https://bananafarm.io/api/boba/tvl/staking")) || {};
-  // console.log(1, data);
-  return data;
-};
+// const staking = async () => {
+//   const { data = {} } =
+//     (await axios.get("https://bananafarm.io/api/boba/tvl/staking")) || {};
+//   // console.log(1, data);
+//   return data;
+// };
 
-const pool2 = async () => {
-  const { data = {} } =
-    (await axios.get("https://bananafarm.io/api/boba/tvl/pool2")) || {};
-  // console.log(2, data);
-  return data;
-};
+// const pool2 = async () => {
+//   const { data = {} } =
+//     (await axios.get("https://bananafarm.io/api/boba/tvl/pool2")) || {};
+//   // console.log(2, data);
+//   return data;
+// };
 
 module.exports = {
   methodology: `TVL is calculated by summing up the values of all LP tokens and our project's rewards token.`,
