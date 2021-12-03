ffba3f592cafb920fbddff15264b29f4efa2f7d9
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-03 00:45:26 +0000

    re-add dai-ftm lp to fantohm

--- projects/fantOHM.js
+++ projects/fantOHM.js
@@ -10,7 +10,7 @@ module.exports=ohmTvl("0xA3b52d5A6d2f8932a5cD921e09DA840092349D71",
         ["0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e", false], // DAI
         ["0x82f0b8b456c1a451378467398982d4834b6829c1", false], // MIM
         ["0xbB29D2A58d880Af8AA5859e30470134dEAf84F2B", false], // sSPELL
-        //["0xd77fc9c4074b56ecf80009744391942fbfddd88b", true], // DAI/FHM
+        ["0xd77fc9c4074b56ecf80009744391942fbfddd88b", true], // DAI/FHM
         ["0x46622913cE40c54Ec14857f72968d4BAAF963947", true] // MIM/FHM
     ], 
     "fantom", 
