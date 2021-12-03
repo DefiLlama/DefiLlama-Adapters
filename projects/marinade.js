902a9ece8dfb1842b828003bb6d41636c7dcec29
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-03 05:56:47 +0000

    timetravel: false
    timetravel: false

--- projects/marinade.js
+++ projects/marinade.js
@@ -10,6 +10,7 @@ async function tvl() {
 }
 
 module.exports = {
+    timetravel: false,
     tvl,
     methodology: `To obtain the Marinade Finance TVL we make a dedicated API endpoint in our REST server. It is using values from the database with a separate update process. The *_sol fields of returned JSON object contains a number of SOL tokens held in our contract for us to then use Coingecko to get the price of SOL token in USD and export it. We are counting only SOL tokens because all other tokens used in our contract are mintable by us and represents a value of locked SOL tokens to our customers`,
 }
