160f90acee0acd882975fc3abc33ab3421f92534
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-04 00:54:08 +0000

    add adapter for stellarx

---
+++ projects/stellarx/index.js
@@ -0,0 +1,11 @@
+const utils = require('./helper/utils');
+
+async function fetch() {
+  var totalTvl = await utils.fetchURL('https://amm-api.stellarx.com/api/pools/30d-statistic/?pool_string=');
+  return totalTvl.data[totalTvl.data.length-1].liquidity;
+}
+
+module.exports = {
+  fetch,
+  
+}
