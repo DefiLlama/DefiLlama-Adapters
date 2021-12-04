ef60aaae23084ddf4fd9b4996140fa0b4c8b9d7d
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-04 10:24:23 +0000

    timetravel: false
    timetravel: false

--- projects/almond.js
+++ projects/almond.js
@@ -14,6 +14,7 @@ async function fetch() {
 }
 
 module.exports = {
+  timetravel: false,
   methodology: "The pools on https://almond.so/ are included in TVL. Standard tokens (SOL, BTC, ETH, USDC, etc..), no pool 2. The API fetches the balances of the Solana accounts holding the staked tokens, then uses Coingecko prices to determine dollar values.",
   fetch,
 };
