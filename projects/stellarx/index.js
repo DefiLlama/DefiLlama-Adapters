4b0d0c1763a1167963920475c35cfb54c2a8f486
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-04 07:00:04 +0000

    update adapter

--- projects/stellarx/index.js
+++ projects/stellarx/index.js
@@ -1,4 +1,4 @@
-const utils = require('./helper/utils');
+const utils = require('../helper/utils');
 
 async function fetch() {
   var totalTvl = await utils.fetchURL('https://amm-api.stellarx.com/api/pools/30d-statistic/?pool_string=');
