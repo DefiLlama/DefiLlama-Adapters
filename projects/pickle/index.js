1dd8fa98860ab832fb7011f9ce62be49640548ee
mrmard <mistermard@protonmail.com>
2021-12-07 01:10:26 +0800

    Added pool2

--- projects/pickle/index.js
+++ projects/pickle/index.js
@@ -5,6 +5,7 @@ const { unwrapUniswapLPs, unwrapCrv } = require("../helper/unwrapLPs");
 const { getChainTransform } = require("../helper/portedTokens");
 const { toUSDT, usdtAddress } = require("../helper/balances");
 const { staking } = require("../helper/staking");
+const { GraphQLClient, gql } = require("graphql-request");
 
 const excluded = ["pbamm", "pickle-eth", "sushi-pickle-eth"];
 const jars_url =
@@ -13,6 +14,59 @@ const jars_url =
 // Contracts
 const dillAddress = "0xbBCf169eE191A1Ba7371F30A1C344bFC498b29Cf";
 const pickleAddress = "0x429881672B9AE42b8EbA0E26cD9C73711b891Ca5";
+
+const pool2s = [
+  {
+    contract: "0xfAA267C3Bb25a82CFDB604136a29895D30fd3fd8", // Gauge
+    lpToken: "0xdc98556Ce24f007A5eF6dC1CE96322d65832A819", // Uni Pickle-Eth
+    chain: "ethereum",
+  },
+  {
+    contract: "0xbD17B1ce622d73bD438b9E658acA5996dc394b0d", // Masterchef
+    lpToken: "0xdc98556Ce24f007A5eF6dC1CE96322d65832A819", // Uni Pickle-Eth
+    chain: "ethereum",
+  },
+  {
+    contract: "0xEF0881eC094552b2e128Cf945EF17a6752B4Ec5d", // Sushi MC2
+    lpToken: "0x269Db91Fc3c7fCC275C2E6f22e5552504512811c", // Sushi Pickle-Eth
+    chain: "ethereum",
+  },
+  {
+    contract: "0xBA12222222228d8Ba445958a75a0704d566BF2C8", // Balancer Vault
+    lpToken: "0xc2f082d33b5b8ef3a7e3de30da54efd3114512ac", // Balancer Pickle-Eth
+    chain: "arbitrum",
+  },
+];
+
+async function getBalancerPoolLiquidity(poolAddress, block) {
+  // delayed by around 5 mins to allow subgraph to update
+  block -= 25;
+  var endpoint = `https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-arbitrum-v2`;
+  var graphQLClient = new GraphQLClient(endpoint);
+  var query = gql`{
+      pools(
+        first: 1,
+        skip: 0,
+        block: {number: ${block}},
+        where: {address_in: ["${poolAddress}"]}
+      ) {
+        address,
+        totalLiquidity
+      }
+    }
+    `;
+
+  const results = await graphQLClient.request(query, {
+    block,
+  });
+
+  const result = results.pools[0];
+  const filtered = {
+    address: result.address,
+    totalLiquidity: result.totalLiquidity,
+  };
+  return filtered;
+}
 
 function chainTvl(chain) {
   return async (_timestamp, _ethBlock, chainBlocks) => {
@@ -100,13 +154,68 @@ function chainTvl(chain) {
   };
 }
 
+function chainPool2(chain) {
+  return async (_timestamp, _ethBlock, chainBlocks) => {
+    const block = chainBlocks[chain];
+    const transformAddress = await getChainTransform(chain);
+    const balances = {};
+
+    const chainPool2s = pool2s.filter((pool) => pool.chain === chain);
+
+    if (chain === "ethereum") {
+      const pool2Balances = await Promise.all(
+        chainPool2s.map(async (pool) => {
+          return (
+            await sdk.api.erc20.balanceOf({
+              target: pool.lpToken,
+              owner: pool.contract,
+            })
+          ).output;
+        })
+      );
+
+      const lpPositions = chainPool2s.map((pool, idx) => {
+        return {
+          balance: pool2Balances[idx],
+          token: pool.lpToken,
+        };
+      });
+
+      await unwrapUniswapLPs(
+        balances,
+        lpPositions,
+        block,
+        chain,
+        transformAddress
+      );
+    } else if (chain === "arbitrum") {
+      const pool2Balances = await Promise.all(
+        chainPool2s.map(async (pool) => {
+          return await getBalancerPoolLiquidity(pool.lpToken, block);
+        })
+      );
+
+      pool2Balances.forEach((pool) => {
+        sdk.util.sumSingleBalance(
+          balances,
+          usdtAddress,
+          toUSDT(pool.totalLiquidity)
+        );
+      });
+    }
+    return balances;
+  };
+}
+
 module.exports = {
   arbitrum: {
     tvl: chainTvl("arbitrum"),
+    pool2: chainPool2("arbitrum"),
   },
   ethereum: {
     tvl: chainTvl("ethereum"),
     staking: staking(dillAddress, pickleAddress),
+    pool2: chainPool2("ethereum"),
   },
   polygon: {
     tvl: chainTvl("polygon"),
