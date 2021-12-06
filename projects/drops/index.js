a4a650b20bfd0ce9be51c827c9cad9a48040239a
freelife2010 <snowman190011@gmail.com>
2021-12-06 01:27:55 -0800

    Add Drops adapter

--- projects/drops/index.js
+++ projects/drops/index.js
@@ -1,26 +1,120 @@
-// const Web3 = require('web3');
-// const BigNumber = require('bignumber.js');
+const BigNumber = require("bignumber.js");
 
 const utils = require("../helper/utils");
-// const { getContractInstance } = require('../config/eurxb/utils.js');
-const masterchefABI = require("./abis/masterchef.json");
-const dop_Ether = "0x00aa1c57e894c4010fe44cb840ae56432d7ea1d1";
-const NDR = "0x739763a258640919981F9bA610AE65492455bE53";
-const masterchef = "0x8A78011bf2c42df82cC05F198109Ea024B554df9";
+const { Pools, tokensAddress } = require("./constants.js");
+const { getContractInstance } = require("./utils.js");
 
-// const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.ETHEREUM_RPC}`))
+const masterchefABI = require("./abis/masterchef.json");
+const lptokenABI = require("./abis/lpToken.json");
+const erc20TokenABI = require("./abis/ERC20.json");
 
 const staking = async () => {
-  // const masterchefContract = await getContractInstance(masterchefABI, masterchef);
-  // const poolLength = await masterchefContract.methods.poolLength().call();
-  // console.log('poolLength', poolLength)
-  return 0;
+  let stakingTVL = new BigNumber(0);
+  const fromWei = (value, decimals = 18) =>
+    decimals < 18
+      ? new BigNumber(value).div(10 ** decimals).toFixed(decimals, 0)
+      : new BigNumber(value).div(10 ** 18).toString(10);
+
+  const masterchefContract = await getContractInstance(
+    masterchefABI,
+    tokensAddress.masterchef
+  );
+  const { poolLength, poolInfo } = masterchefContract.methods;
+
+  const length = await poolLength().call();
+  const poolInfos = [];
+  const res = await utils.getTokenPricesFromString(tokensAddress.Comp);
+  const dopPrice = res.data
+    ? res.data[tokensAddress.Comp.toLowerCase()].usd
+    : 0;
+
+  for (let i = 0; i < length; i += 1) {
+    let _info = await poolInfo(i).call();
+    poolInfos.push({ ..._info, poolId: i });
+  }
+  for (let j = 0; j < poolInfos.length; j += 1) {
+    let _totalLp;
+    let _reserves;
+    let _token0;
+    let _token1;
+    let totalLpSupply;
+    let info = poolInfos[j];
+
+    const pool = Pools.find(
+      (pool) => pool.lpToken.toLowerCase() === info.lpToken.toLowerCase()
+    );
+    if (!pool) return resolve(null);
+    const poolType = pool.type;
+    let methods;
+
+    if (poolType === "LP") {
+      const lpTokenContract = await getContractInstance(
+        lptokenABI,
+        info.lpToken
+      );
+      methods = lpTokenContract.methods;
+      _totalLp = await methods.totalSupply().call();
+      _reserves = await methods.getReserves().call();
+      _token0 = await methods.token0().call();
+      _token1 = await methods.token1().call();
+      totalLpSupply = await methods.balanceOf(tokensAddress.masterchef).call();
+    } else {
+      const erc20TokenContract = await getContractInstance(
+        erc20TokenABI,
+        info.lpToken
+      );
+      methods = erc20TokenContract.methods;
+      _totalLp = await methods.totalSupply().call();
+      _reserves = "0";
+      const res = await utils.getTokenPricesFromString(info.lpToken);
+      _token0 = res.data ? res.data[info.lpToken.toLowerCase()].usd : 0;
+      _token1 = await methods.decimals().call();
+      totalLpSupply = await methods.balanceOf(tokensAddress.masterchef).call();
+    }
+
+    let totalLocked = new BigNumber(0);
+    let isLp = _reserves !== "0";
+    const tokenDecimal = isLp ? 18 : _token1;
+    let tokenPrice = _token0 === "0" ? 1 : Number(_token0);
+
+    if (isLp) {
+      if (_token0.toLowerCase() === tokensAddress.Comp.toLowerCase()) {
+        totalLocked = new BigNumber(_reserves._reserve0)
+          .div(1e18)
+          .times(2)
+          .times(dopPrice);
+      } else {
+        totalLocked = new BigNumber(_reserves._reserve1)
+          .div(1e18)
+          .times(2)
+          .times(dopPrice);
+      }
+      tokenPrice = new BigNumber(totalLocked)
+        .div(new BigNumber(_totalLp).div(10 ** 18))
+        .toNumber();
+    } else {
+      totalLocked = new BigNumber(totalLpSupply)
+        .div(10 ** tokenDecimal)
+        .times(tokenPrice);
+    }
+    const result = {
+      ...(Pools.find((pool) => pool.id === info.poolId) || {}),
+      ...info,
+      totalLp: fromWei(_totalLp, tokenDecimal),
+      totalLpSupply: fromWei(totalLpSupply, tokenDecimal),
+      lpPrice: tokenPrice,
+    };
+    stakingTVL = stakingTVL.plus(
+      new BigNumber(result.totalLpSupply).times(result.lpPrice)
+    );
+  }
+  return stakingTVL.toString(10);
 };
 
 const fetch = async () => {
   var res = await utils.fetchURL("https://drops.co/status");
-  // const stakingTVL = await staking()
-  return res.data ? res.data.totalSupply : 0;
+  const stakingTVL = await staking();
+  return res.data ? new BigNumber(res.data.totalSupply).plus(stakingTVL) : 0;
 };
 
 module.exports = {
