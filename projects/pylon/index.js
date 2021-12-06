d9c5559a84e51bdc683343f39806176c9d146972
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-06 23:12:12 +0000

    timetravel: false,
    timetravel: false,

--- projects/pylon/index.js
+++ projects/pylon/index.js
@@ -28,6 +28,7 @@ async function staking(){
 }
 
 module.exports = {
+    timetravel: false,
     methodology: 'TVL counts the UST that has been deposted to the Protocol. Data is pulled from the Pylon API:"https://api.pylon.money/api/launchpad/v1/projects/mine".',
     historical: false,
     pool2:{
