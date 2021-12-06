142d4ea365dc73029cdd1745a19f1b72f6ff8487
0xngmi <0xngmi@protonmail.com>
2021-12-06 21:36:26 +0000

    update cbridge contracts

--- projects/celerbridge/index.js
+++ projects/celerbridge/index.js
@@ -1,17 +1,18 @@
 const { getBlock } = require("../helper/getBlock")
 const { chainExports } = require("../helper/exports")
 const sdk = require('@defillama/sdk')
+const { sumTokens } = require("../helper/unwrapLPs")
 
 const bridgeContractV1 = "0x841ce48F9446C8E281D3F1444cB859b4A6D0738C"
 
 const bridgeContractsV2 = {
-    ethereum: "0xc578Cbaf5a411dFa9F0D227F97DaDAa4074aD062",
-    bsc: "0x5d96d4287D1ff115eE50faC0526cf43eCf79bFc6",
-    arbitrum: "0xdd90E5E87A2081Dcf0391920868eBc2FFB81a1aF",
-    polygon: "0xa251c4691C1ffd7d9b128874C023427513D8Ac5C",
-    avax: "0xBB7684Cc5408F4DD0921E5c2Cadd547b8f1AD573",
-    fantom: "0x3795C36e7D12A8c252A20C5a7B455f7c57b60283",
-    optimism: "0x6De33698e9e9b787e09d3Bd7771ef63557E148bb"
+    ethereum: ["0xc578Cbaf5a411dFa9F0D227F97DaDAa4074aD062", "0x5427FEFA711Eff984124bFBB1AB6fbf5E3DA1820"],
+    bsc: ["0x5d96d4287D1ff115eE50faC0526cf43eCf79bFc6", "0xdd90E5E87A2081Dcf0391920868eBc2FFB81a1aF"],
+    arbitrum: ["0xdd90E5E87A2081Dcf0391920868eBc2FFB81a1aF", "0x1619DE6B6B20eD217a58d00f37B9d47C7663feca"],
+    polygon: ["0xa251c4691C1ffd7d9b128874C023427513D8Ac5C", "0x88DCDC47D2f83a99CF0000FDF667A468bB958a78"],
+    avax: ["0xBB7684Cc5408F4DD0921E5c2Cadd547b8f1AD573", "0xef3c714c9425a8F3697A9C969Dc1af30ba82e5d4"],
+    fantom: ["0x3795C36e7D12A8c252A20C5a7B455f7c57b60283", "0x374B8a9f3eC5eB2D97ECA84Ea27aCa45aa1C57EF"],
+    optimism: ["0x6De33698e9e9b787e09d3Bd7771ef63557E148bb", "0x9D39Fc627A6d9d9F8C831c16995b209548cc3401"]
 }
 
 // From https://cbridge-docs.celer.network/#/FAQ?id=what-are-the-token-addresses-on-each-chain
@@ -92,13 +93,7 @@ function chainTvl(chain) {
             const tokenAddress = chainsWithDifferentDecimals.includes(chain) ? chain + ':' + token[chain] : token.ethereum
             sdk.util.sumSingleBalance(balances, tokenAddress, balanceV1.output)
             if (bridgeContractsV2[chain] !== undefined) {
-                const balanceV2 = await sdk.api.erc20.balanceOf({
-                    chain,
-                    block,
-                    target: token[chain],
-                    owner: bridgeContractsV2[chain]
-                })
-                sdk.util.sumSingleBalance(balances, tokenAddress, balanceV2.output)
+                await sumTokens(balances, bridgeContractsV2[chain].map(b=>[token[chain], b]), block, chain, ()=>tokenAddress)
             }
         }))
         return balances
