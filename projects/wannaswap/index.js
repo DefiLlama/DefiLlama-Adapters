ca865bbcdf6fcf43c677396db07590acf6d3f9f5
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-06 09:28:18 +0000

    remove staking code

--- projects/wannaswap/index.js
+++ projects/wannaswap/index.js
@@ -1,10 +1,10 @@
 const {calculateUsdUniTvl} = require('../helper/getUsdUniTvl');
-const { staking } = require('../helper/staking.js');
+const { staking, stakingPricedLP, stakingUnknownPricedLP } = require('../helper/staking.js');
 
 module.exports = {
     misrepresentedTokens: true,
     aurora: {
-        staking: staking("0x5205c30bf2E37494F8cF77D2c19C6BA4d2778B9B", "0x7faA64Faf54750a2E3eE621166635fEAF406Ab22", "aurora"),
+        //staking: stakingPricedLP("0x5205c30bf2E37494F8cF77D2c19C6BA4d2778B9B", "0x7faA64Faf54750a2E3eE621166635fEAF406Ab22", "aurora", "0x7E9EA10E5984a09D19D05F31ca3cB65BB7df359d",),
         tvl:calculateUsdUniTvl(
             // factory
             "0x7928D4FeA7b2c90C732c10aFF59cf403f0C38246",
