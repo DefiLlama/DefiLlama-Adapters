f58d7a2bb8747213d91b879041e02296b219f501
waynebruce0x <waynebruce0x@gmail.com>
2021-12-08 21:34:02 +0000

    arkadiko hardcoded DIKO to 1

---
+++ projects/arkadiko.js
@@ -0,0 +1,42 @@
+const retry = require("./helper/retry");
+const axios = require("axios");
+
+async function tvl() {
+  let balances = {};
+  const dikoPrice = 1;
+
+  const response = (
+    await retry(
+      async (bail) =>
+        await axios.get("https://arkadiko-api.herokuapp.com/api/v1/pools")
+    )
+  ).data;
+
+  for (pool of response.pools) {
+    balances[pool.token_y_name] =
+      pool.token_y_name in balances
+        ? Number(balances[pool.token_y_name]) + Number(pool.tvl_token_y)
+        : Number(pool.tvl_token_y);
+    balances[pool.token_x_name] =
+      pool.token_x_name in balances
+        ? Number(balances[pool.token_x_name]) + Number(pool.tvl_token_x)
+        : Number(pool.tvl_token_x);
+  }
+
+  balances["blockstack"] = balances["wrapped-stx-token"] / 10 ** 6;
+  delete balances["wrapped-stx-token"];
+  balances["usd-coin"] = balances["usda-token"] / 10 ** 6;
+  delete balances["usda-token"];
+  balances["usd-coin"] =
+    Number(balances["usd-coin"]) +
+    Number(balances["arkadiko-token"] / 10 ** 6) * dikoPrice;
+  delete balances["arkadiko-token"];
+
+  return balances;
+}
+
+module.exports = {
+  misrepresentedTokens: true,
+  tvl,
+};
+// node test.js projects/arkadiko.js
