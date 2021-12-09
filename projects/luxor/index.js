10bb221eade4601294aa4f7d46887dcad7f354a7
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-09 05:32:45 +0000

    update code to include staking

--- projects/luxor/index.js
+++ projects/luxor/index.js
@@ -1,29 +1,25 @@
-const { ohmTvl } = require("../helper/ohm");
+const { stakingPricedLP } = require('../helper/staking')
+const {sumTokensAndLPsSharedOwners} = require('../helper/unwrapLPs')
+const {transformFantomAddress} = require('../helper/portedTokens')
 
-const LuxorStaking = "0xf3F0BCFd430085e198466cdCA4Db8C2Af47f0802";
 
-const LUX = "0x6671E20b83Ba463F270c8c75dAe57e3Cc246cB2b";
+const treasury = "0xDF2A28Cc2878422354A93fEb05B41Bd57d71DB24"
+const LUX = "0x6671e20b83ba463f270c8c75dae57e3cc246cb2b"
 
-const treasuryAddresses = "0xDF2A28Cc2878422354A93fEb05B41Bd57d71DB24";
+async function tvl(time, ethBlock, chainBlocks){
+    const balances = {}
+    const transform = await transformFantomAddress()
+    await sumTokensAndLPsSharedOwners(balances, [
+        ["0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e", false],
+        ["0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83", false],
+        ["0x46729c2AeeabE7774a0E710867df80a6E19Ef851", true]
+    ], [treasury], chainBlocks.fantom, "fantom", transform)
+    return balances
+}
 
-const DAI = "0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e";
-const LUX_DAI = "0x46729c2AeeabE7774a0E710867df80a6E19Ef851";
-const WFTM = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83";
-
-module.exports = ohmTvl(
-  treasuryAddresses,
-  [
-    [DAI, false],
-    [LUX_DAI, true],
-    [WFTM, false],
-  ],
-  "fantom",
-  LuxorStaking,
-  LUX,
-  (addr) => {
-    if (addr === "0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e") {
-      return "0x6b175474e89094c44da98b954eedeac495271d0f";
+module.exports={
+    fantom:{
+        tvl,
+        staking: stakingPricedLP("0xf3F0BCFd430085e198466cdCA4Db8C2Af47f0802", LUX, "fantom", "0x46729c2AeeabE7774a0E710867df80a6E19Ef851", "dai", true)
     }
-    return `fantom:${addr}`;
-  }
-);
+}
