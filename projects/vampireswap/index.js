b742e3d7fd047406414333ce5710a5fba55b4e6d
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-10 03:48:51 +0000

    update staking function

--- projects/vampireswap/index.js
+++ projects/vampireswap/index.js
@@ -1,6 +1,6 @@
 const {calculateUniTvl} = require('../helper/calculateUniTvl.js')
 const {transformFantomAddress} = require('../helper/portedTokens.js');
-const { staking } = require('../helper/staking.js');
+const { staking, stakingPricedLP } = require('../helper/staking.js');
 
 const factory = '0xdf0a0a62995ae821d7a5cf88c4112c395fc41358'
 async function tvl(_timestamp, _ethBlock, chainBlocks){
@@ -12,7 +12,7 @@ async function tvl(_timestamp, _ethBlock, chainBlocks){
 
 module.exports = {
   fantom:{
-    staking: staking("0xa9d452E3CEA2b06d7DBE812A6C3ec81cf52334dD", "0x97058c0B5ff0E0E350e241EBc63b55906a9EADbc", "fantom"),
+    staking: stakingPricedLP("0xa9d452E3CEA2b06d7DBE812A6C3ec81cf52334dD", "0x97058c0B5ff0E0E350e241EBc63b55906a9EADbc", "fantom", "0x8fa291be663a069e6289f844944752cd011ec719", "wrapped-fantom", false),
     tvl,
   },
 }
