931037285b2dc7c55d610aed91958c90bfea8578
nemusona <nemusonaneko@protonmail.com>
2021-12-10 15:51:20 -0600

    colony modification

--- projects/colony/index.js
+++ projects/colony/index.js
@@ -1,22 +1,32 @@
-const sdk = require('@defillama/sdk')
+const sdk = require("@defillama/sdk");
 
-const stakingContract = '0x5B0d74C78F2588B3C5C49857EdB856cC731dc557'
-const colonyGovernanceToken = '0xec3492a2508DDf4FDc0cD76F31f340b30d1793e6'
+const stakingContract = "0x5B0d74C78F2588B3C5C49857EdB856cC731dc557";
+const colonyGovernanceToken = "0xec3492a2508DDf4FDc0cD76F31f340b30d1793e6";
 
-async function tvl (timestamp, block) {
-    const valueLocked = (await sdk.api.erc20.balanceOf({
-        block,
-        target: colonyGovernanceToken,
-        owner: stakingContract,
-        chain: 'avax'
-    })).output
-    return { [colonyGovernanceToken]: valueLocked }
+async function staking(timestamp, block, chainBlocks) {
+  let balances = {};
+  const valueLocked = (
+    await sdk.api.erc20.balanceOf({
+      block: chainBlocks.avax,
+      target: colonyGovernanceToken,
+      owner: stakingContract,
+      chain: "avax",
+    })
+  ).output;
+  sdk.util.sumSingleBalance(
+    balances,
+    `avax:${colonyGovernanceToken}`,
+    valueLocked
+  );
+  return balances;
 }
 
 module.exports = {
-    methodology: 'TVL is calculated based on CLY tokens locked on Colony staking contract',
-    avalanche: {
-        tvl
-    },
-    tvl
-}
+  methodology:
+    "Staking is calculated based on CLY tokens locked on Colony staking contract",
+  avalanche: {
+    tvl: async () => ({}),
+    staking,
+  },
+  tvl: async () => ({}),
+};
