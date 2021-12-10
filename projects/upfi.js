089134a18a130b46dc5d88a6164b6b01975c0ffb
inti25 <nqhung92@hotmail.com>
2021-12-10 21:18:18 +0700

    Update upfi.js

--- projects/upfi.js
+++ projects/upfi.js
@@ -2,16 +2,20 @@ const { getTokenAccountBalance } = require("./helper/solana");
 const axios = require("axios");
 const retry = require("./helper/retry");
 
-async function fetch() {
+async function pool2() {
   const response = (
     await retry(async (bail) => await axios.get("https://api.upfi.network/tvl"))
   ).data;
-  return response.tvl || 0;
+  return { 
+    'upfi-network': response.UPS_USDC,
+    'usd-coin': response.USDC_UPFI + response.UPFI_3Pool,
+  }
 }
 
 module.exports = {
   timetravel: false,
-  methodology:
-    'TVL data is pulled from the UPFI API "https://api.upfi.network/tvl".',
-  fetch,
+  methodology: 'TVL data is pulled from the UPFI API "https://api.upfi.network/tvl".',
+  pool2: {
+    tvl: pool2,
+  }
 };
