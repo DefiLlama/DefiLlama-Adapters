433f14c96f51ddecca4f6890f799019afdce5c8b
0xMadxi <0xMadxi>
2021-12-11 15:07:21 +0800

    Add calculation for PNG-WAVAX PGL

--- projects/maximizer/index.js
+++ projects/maximizer/index.js
@@ -9,6 +9,7 @@ const DAI = "0xd586E7F844cEa2F87f50152665BCbc2C279D8d70";
 const WAVAX = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7";
 const MAXI_DAI_JLP = "0xfBDC4aa69114AA11Fae65E858e92DC5D013b2EA9";
 const MAXI_WAVAX_PGL = "0xbb700450811a30c5ee0dB80925Cf1BA53dBBd60A";
+const PNG_WAVAX_PGL = "0xd7538cABBf8605BdE1f4901B47B8D42c61DE0367";
 
 const staking = async (timestamp, ethBlock, chainBlocks) => {
   const balances = {};
@@ -36,6 +37,7 @@ async function tvl(timestamp, block, chainBlocks) {
       [WAVAX, false],
       [MAXI_DAI_JLP, true],
       [MAXI_WAVAX_PGL, true],
+      [PNG_WAVAX_PGL, true],
     ],
     [Treasury],
     chainBlocks.avax,
