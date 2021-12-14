5dbc858828a6988d306f138cec3b7bdbf0b64ca1
waynebruce0x <waynebruce0x@gmail.com>
2021-12-14 16:49:02 +0000

    elephant dex

--- projects/elephantdex/index.js
+++ projects/elephantdex/index.js
@@ -1,39 +1,28 @@
-const { request, gql } = require("graphql-request");
-const sdk = require('@defillama/sdk');
-const { toUSDTBalances } = require('../helper/balances');
+const { getBlock } = require("../helper/getBlock");
+const { transformHarmonyAddress } = require("../helper/portedTokens");
+const { calculateUniTvl } = require("../helper/calculateUniTvl");
 
-const graphUrl = 'https://elephantgraph.com/subgraphs/name/elephantproject/subgraph21'
-const graphQuery = gql`
-query get_tvl($block: Int) {
-  uniswapFactory(
-    id: "0x0Dea90EC11032615E027664D2708BC292Bbd976B",
-    block: { number: $block }
-  ) {
-    totalLiquidityUSD
-  },
-}
-`;
+const wONE = "bsc:0xdE976f3344cd9F06E451aF3A94a324afC3E154F4";
+async function tvl(timestamp, block, chainBlocks) {
+  block = await getBlock(timestamp, "harmony", chainBlocks);
+  const transform = await transformHarmonyAddress();
 
-async function tvl(timestamp) {
-  const {block} = await sdk.api.util.lookupBlock(timestamp,{
-    chain: 'harmony'
-  })
-  const response = await request(
-    graphUrl,
-    graphQuery,
-    {
-      block,
-    }
+  let balances = await calculateUniTvl(
+    transform,
+    block,
+    "harmony",
+    "0x0Dea90EC11032615E027664D2708BC292Bbd976B",
+    18105518,
+    true
   );
 
-  const usdTvl = Number(response.uniswapFactory.totalLiquidityUSD)
-
-  return toUSDTBalances(usdTvl)
+  balances["harmony"] = balances[wONE] / 10 ** 18;
+  delete balances[wONE];
+  return balances;
 }
 
 module.exports = {
-  harmony:{
+  harmony: {
     tvl,
   },
-  tvl
-}
+};
