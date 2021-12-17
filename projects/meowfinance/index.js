7bca4bfd26f8c580dd3a5683fa5b47c9ce6c255b
meow-finance <admin@meowfinance.org>
2021-12-17 11:51:46 +0700

    fix miss merge

--- projects/meowfinance/index.js
+++ projects/meowfinance/index.js
@@ -1,143 +1,136 @@
 const sdk = require("@defillama/sdk");
-const BigNumber = require("bignumber.js");
-const axios = require("axios");
-const { unwrapUniswapLPs } = require("../helper/unwrapLPs")
-const address = require("./addressFTM.json");
 const abi = require("./abi.json");
+const { pool2Exports } = require("../helper/pool2");
+const { stakingUnknownPricedLP } = require("../helper/staking");
 
-const allValut = Object.keys(address).filter( x=> x.toUpperCase().substring(0,5) == "VAULT").map(x=> address[x]);
-const allWorker = Object.keys(address).filter( x=> x.toUpperCase().substring(x.length - 6) == "WORKER").map(x=> address[x]);
-function getFTMAddress(address) {
-    return `fantom:${address.toUpperCase()}`
-  }
+const meow = "0x41F4CC9613E31d4E77C428b40D53537Da24264Ee";
+const meowMining = "0xba1a3dACa919616aA462E93A80EFbe82753f9087";
+const meowFtm = "0x150Aeb5389d56E258c2bbb42c7e67e944EDEE913";
+const treasuryContract = "0x7d25f49C648B2a12B5f530Df929204352cb6080e";
 
-async function tvl(timestamp, ethBlock, chainBlocks) {
-    const balances = {}
-    const block = chainBlocks.fantom;
-  
-    //#region  WORKER
-    var stakingCall = [];
-    var promise = [];
-    for await (const workerAddress of allWorker)
-    {
-        let res = (new Promise(async (resolve,reject)=>
-            {
-                const lp = (await sdk.api.abi.call(
-                    {
-                        abi: abi.WORKER.lpToken,
-                        target: workerAddress,
-                        params: [],
-                        block: block,
-                        chain: "fantom"
-                  })).output;
-                const masterChef = (await sdk.api.abi.call(
-                    {
-                        abi: abi.WORKER.masterchef,
-                        target: workerAddress,
-                        params: [],
-                        block: block,
-                        chain: "fantom"
-                  })).output;
-                const pid = (await sdk.api.abi.call(
-                  {
-                      abi: abi.WORKER.pid,
-                      target: workerAddress,
-                      params: [],
-                      block: block,
-                      chain: "fantom"
-                })).output;
-                resolve({lp,masterChef,pid,workerAddress});
-            }));
-        promise.push(res);
-    }
-    stakingCall = await Promise.all(promise);
-    // console.log(stakingCall);
-    const resUserInfo = (await sdk.api.abi.multiCall({
-        block,
-        abi: abi.MASRERCHEF.userinfo,
-        chain: 'fantom',
-        calls: stakingCall.map(x => { return {  target: x.masterChef, params: [x.pid, x.workerAddress] }})
-      })).output
-    //  console.log(resUserInfo);
-     const stakingInfo = await stakingCall.map(x=> { 
-        let o = Object.assign({}, x);
-        let userInfo = resUserInfo.find(y=>y.input.params[0] == o.pid && y.input.params[1] == o.workerAddress && y.input.target == o.masterChef  );
-        if (userInfo != undefined)
-        {
-            o.amount = userInfo.output.amount;
-            o.rewardDebt =  userInfo.output.rewardDebt;
-        }
-         return  o;
-     })
-    //  console.log(stakingInfo);
-    await unwrapUniswapLPs(balances,
-        stakingInfo.map( x => {
-            return {token: x.lp, balance: x.amount}
-        })
-      , block, 'fantom', (addr) => getFTMAddress(addr))
-    //#endregion
-
-    //#region  VALUT
-    const getBaseToken = async ()=>
-    {
-        let res = (await sdk.api.abi.multiCall(
-        {
-            abi: abi.VALUT.baseToken,
-            calls : allValut.map((valutAddress) => { return { target: valutAddress }} ),
-            block: block,
-            chain: "fantom"
-        })).output;
-        return res.map(x=> { return {valut: x.input.target , token:x.output}});
-    }
-    const getBalance = async (valutInfo)=>
-    {
-        let res = (await sdk.api.abi.multiCall(
-        {
-            abi: "erc20:balanceOf",
-            calls : valutInfo.map((info) => { return { target: info.token , params: [info.valut]  }} ),
-            block: block,
-            chain: "fantom"
-        })).output;
-        return res.map(x=> { return {token: x.input.target , amount:x.output}});
-    }
-    let baseToken = await getBaseToken();
-    let valutAmount = await getBalance(baseToken);
-    // console.log(baseToken);
-    // console.log(valutAmount);
-    for await (const detail of valutAmount)
-    {
-        balances[getFTMAddress(detail.token)] =   BigNumber( balances[getFTMAddress(detail.token)] || 0 ).plus(detail.amount);
-    }
-    //#endregion
-
-    //#region  MEOW Token
-    /// MEOW-FTM Liquidity
-    let lpTotal = (await sdk.api.abi.call(
-        {
-            abi: "erc20:totalSupply",
-            target: address.MeowLP,
-            params: [],
-            block: block,
-            chain: "fantom"
-        })).output;
-    let lpBalance = {};
-    await unwrapUniswapLPs(lpBalance,
-        [{token: address.MeowLP, balance: lpTotal}]
-        , block, 'fantom', (addr) => (addr))
-    // console.log(lpBalance);
-    for await (const obj of Object.keys(lpBalance))
-    {
-        balances[getFTMAddress(obj)] =   BigNumber( balances[getFTMAddress(obj)] || 0 ).plus(lpBalance[obj]);
-    }
-    //#endregion
-
-    return balances;
-
+const translate = {
+  "0x049d68029688eabf473097a2fc38ef61633a3c7a":
+    "0xdac17f958d2ee523a2206206994597c13d831ec7",
+  "0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e":
+    "0x6b175474e89094c44da98b954eedeac495271d0f",
 };
 
+async function calcTvl(block, chain, borrow) {
+  let balances = {};
+  const poolLength = (
+    await sdk.api.abi.call({
+      target: meowMining,
+      abi: abi.poolLength,
+      block,
+      chain,
+    })
+  ).output;
+  const poolInfo = (
+    await sdk.api.abi.multiCall({
+      calls: Array.from({ length: Number(poolLength) }, (_, k) => ({
+        target: meowMining,
+        params: k,
+      })),
+      abi: abi.poolInfo,
+      block,
+      chain,
+    })
+  ).output;
+  const symbols = (
+    await sdk.api.abi.multiCall({
+      calls: poolInfo.map((p) => ({
+        target: p.output.stakeToken,
+      })),
+      abi: "erc20:symbol",
+      block,
+      chain,
+    })
+  ).output;
+  let ibTokens = [];
+  symbols.forEach((p) => {
+    if (p.output.startsWith("ib")) {
+      ibTokens.push(p.input.target);
+    }
+  });
+  const underlyingtoken = (
+    await sdk.api.abi.multiCall({
+      calls: ibTokens.map((p) => ({
+        target: p,
+      })),
+      abi: abi.token,
+      block,
+      chain,
+    })
+  ).output;
+  const totalToken = (
+    await sdk.api.abi.multiCall({
+      calls: ibTokens.map((p) => ({
+        target: p,
+      })),
+      abi: abi.totalToken,
+      block,
+      chain,
+    })
+  ).output;
+  const vaultDebtVal = (
+    await sdk.api.abi.multiCall({
+      calls: ibTokens.map((p) => ({
+        target: p,
+      })),
+      abi: abi.vaultDebtVal,
+      block,
+      chain,
+    })
+  ).output;
+  for (let i = 0; i < ibTokens.length; i++) {
+    let token = underlyingtoken[i].output.toLowerCase();
+    let total = Number(totalToken[i].output);
+    let debt = Number(vaultDebtVal[i].output);
+    if (translate[token] !== undefined) {
+      token = translate[token];
+    } else {
+      token = `fantom:${token}`;
+    }
+    if (!borrow) {
+      sdk.util.sumSingleBalance(balances, token, total - debt);
+    } else {
+      sdk.util.sumSingleBalance(balances, token, debt);
+    }
+  }
+  return balances;
+}
+
+async function tvl(timestamp, block, chainBlocks) {
+  return await calcTvl(chainBlocks.fantom, "fantom", false);
+}
+
+async function borrowed(timestamp, block, chainBlocks) {
+  return await calcTvl(chainBlocks.fantom, "fantom", true);
+}
+
 module.exports = {
-    fantom:{
-        tvl,
-      }
-  };
-    
+  fantom: {
+    tvl,
+    borrowed,
+    pool2: pool2Exports(
+      meowMining,
+      [meowFtm],
+      "fantom",
+      (addr) => `fantom:${addr}`
+    ),
+    staking: stakingUnknownPricedLP(
+      meowMining,
+      meow,
+      "fantom",
+      meowFtm,
+      (addr) => `fantom:${addr}`
+    ),
+    treasury: stakingUnknownPricedLP(
+      treasuryContract,
+      meow,
+      "fantom",
+      meowFtm,
+      (addr) => `fantom:${addr}`
+    ),
+  },
+};
