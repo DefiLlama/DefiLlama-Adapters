9dd99e989852f7dc178470acbe052b40e00fbf80
0xngmi <0xngmi@protonmail.com>
2021-12-17 21:35:59 +0000

    fix mfinance

--- projects/mfinance/index.js
+++ projects/mfinance/index.js
@@ -1,7 +1,7 @@
 const abi = require("./abi.json");
 const { staking } = require("../helper/staking");
 const { pool2BalanceFromMasterChefExports } = require("../helper/pool2");
-const { addFundsInMasterChef } = require("../helper/masterchef");
+const { masterChefExports } = require("../helper/masterchef");
 
 const masterChef = "0x342A8A451c900158BA4f1367C55955b5Fbcb7CCe";
 const MG = "0x06b0c26235699b15e940e8807651568b995a8e01";
@@ -25,17 +25,4 @@ const ethTvl = async (chainBlocks) => {
   return balances;
 };
 
-module.exports = {
-  ethereum: {
-    staking: staking(masterChef, MG),
-    pool2: pool2BalanceFromMasterChefExports(
-      masterChef,
-      MG,
-      "ethereum",
-      (addr) => addr,
-      abi.poolInfo
-    ),
-    tvl: ethTvl,
-  },
-  methodology: "TVL includes all farms in MasterChef contract",
-};
+module.exports = masterChefExports(masterChef, "ethereum", MG, false, abi.poolInfo)
