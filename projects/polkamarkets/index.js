0e2174cfcfe79e979fb523eb85960c21aa008dea
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-12-20 16:40:59 +0800

    add polk bonds to tvl calculation

--- projects/polkamarkets/index.js
+++ projects/polkamarkets/index.js
@@ -1,18 +1,39 @@
 const sdk = require("@defillama/sdk");
 const BigNumber = require("bignumber.js");
 
-const contractAddress = "0xdcbe79f74c98368141798ea0b7b979b9ba54b026";
+const predictionMarketContractAddress =
+  "0xdcbe79f74c98368141798ea0b7b979b9ba54b026";
+const realtioContractAddress = "0x60d7956805ec5a698173def4d0e1ecdefb06cc57";
+const polkMoonriverAddress = "0x8b29344f368b5fa35595325903fe0eaab70c8e1f";
 
 async function tvl(timestamp, ethBlock, chainBlocks) {
-  const balance = (
+  const balances = {};
+
+  const movrBalance = (
     await sdk.api.eth.getBalance({
-      target: contractAddress,
+      target: predictionMarketContractAddress,
       block: chainBlocks.moonriver,
       chain: "moonriver",
     })
   ).output;
 
-  return { moonriver: new BigNumber(balance).div(10 ** 18) };
+  const polkBalance = (
+    await sdk.api.erc20.balanceOf({
+      target: polkMoonriverAddress,
+      owner: realtioContractAddress,
+      chain: "moonriver",
+      block: chainBlocks.moonriver,
+    })
+  ).output;
+
+  sdk.util.sumSingleBalance(balances, "moonriver", Number(movrBalance) / 1e18);
+  sdk.util.sumSingleBalance(
+    balances,
+    "polkamarkets",
+    Number(polkBalance) / 1e18
+  );
+
+  return balances;
 }
 
 module.exports = {
