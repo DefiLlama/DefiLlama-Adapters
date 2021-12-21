6e6bd2262a94ae88b1cf94d4aff3848e95056ff7
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-21 15:19:41 +0000

    update weth address

--- projects/optinyan/index.js
+++ projects/optinyan/index.js
@@ -1,7 +1,7 @@
 const sdk = require('@defillama/sdk')
 const {getBlock} = require('../helper/getBlock')
 
-const weth = '0x4200000000000000000000000000000000000006'
+const weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 async function tvl(time, _ethBlock, chainBlocks){
     const block = await getBlock(time, 'optimism', chainBlocks)
     const eth = await sdk.api.eth.getBalance({
