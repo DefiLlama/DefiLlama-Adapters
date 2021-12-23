75b7f44967fd41029897a8508afba1536c0ac282
haike <haike0513@gmail.com>
2021-12-23 13:27:54 +0800

    add veth description

--- projects/zenlink/index.js
+++ projects/zenlink/index.js
@@ -4,7 +4,7 @@ const stakeList = [
 ];
 
 const tokenChainMap = {
-    '0x3b25bc1dc591d24d60560d0135d6750a561d4764': '0xc3d088842dcf02c13699f936bb83dfbbc6f721ab'
+    '0x3b25bc1dc591d24d60560d0135d6750a561d4764': '0xc3d088842dcf02c13699f936bb83dfbbc6f721ab', // vETH
 }
 
 module.exports = {
