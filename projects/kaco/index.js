ddf6c11752fb7b481f419041f7edd839cbe93259
hailiting <hailiting@yeah.net>
2021-12-24 16:06:55 +0800

    add

--- projects/kaco/index.js
+++ projects/kaco/index.js
@@ -33,6 +33,24 @@ const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
   return balances;
 };
 
+const shidenTvl = async (timestamp, ethBlock, chainBlocks) => {
+  const balances = {};
+
+  let transformAddress = await transformBscAddress();
+
+  await addFundsInMasterChef(
+    balances,
+    KACMasterChefContract["shiden"],
+    chainBlocks["shiden"],
+    "shiden",
+    transformAddress,
+    abi.poolInfo,
+    [KAC["shiden"], KACMasterChefContract["shiden"]]
+  );
+
+  return balances;
+};
+
 module.exports = {
   timetravel: true,
   misrepresentedTokens: true,
@@ -43,21 +61,13 @@ module.exports = {
     tvl: bscTvl,
   },
   shiden: {
-    tvl: calculateUsdUniTvl(
+    staking: staking(
       KACMasterChefContract["shiden"],
+      KAC["shiden"],
       "shiden",
-      "0x0f933Dc137D21cA519ae4C7E93f87a4C8EF365Ef",
-      [
-        // USDC
-        "0xfA9343C3897324496A05fC75abeD6bAC29f8A40f",
-        // USDT
-        "0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b",
-        // JPYC
-        "0x735aBE48e8782948a37C7765ECb76b98CdE97B0F",
-        // STND
-        "0x722377A047e89CA735f09Eb7CccAb780943c4CB4",
-      ],
-      "shiden"
+      KAC["bsc"],
+      0
     ),
+    tvl: shidenTvl,
   },
 };
