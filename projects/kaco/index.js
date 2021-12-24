e1497d359f7adc4abb08140ec6f76262152c7b1c
hailiting <hailiting@yeah.net>
2021-12-24 17:48:20 +0800

    add

--- projects/kaco/index.js
+++ projects/kaco/index.js
@@ -1,46 +1,53 @@
 const sdk = require("@defillama/sdk");
-const abi = require("./abi.json");
 const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
-
-const { addFundsInMasterChef } = require("../helper/masterchef");
-const { transformBscAddress } = require("../helper/portedTokens");
+const { calculateUniTvl } = require("../helper/calculateUniTvl.js");
+const { pool2 } = require("../helper/pool2");
 const { staking } = require("../helper/staking");
 
 const KACMasterChefContract = {
   bsc: "0x81b71D0bC2De38e37978E6701C342d0b7AA67D59",
   shiden: "0x293A7824582C56B0842535f94F6E3841888168C8",
 };
+const KACFactory = {
+  bsc: "0xa5e48a6E56e164907263e901B98D9b11CCB46C47",
+  shiden: "0xcd8620889c1dA22ED228e6C00182177f9dAd16b7",
+};
 const KAC = {
   bsc: "0xf96429A7aE52dA7d07E60BE95A3ece8B042016fB",
   shiden: "0xb12c13e66ade1f72f71834f2fc5082db8c091358",
 };
-
-// node test.js projects/kaco/index.js
-const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
-  const balances = {};
-
-  let transformAddress = await transformBscAddress();
-
-  await addFundsInMasterChef(
-    balances,
-    KACMasterChefContract["bsc"],
+async function bscTvl(timestamp, block, chainBlocks) {
+  return calculateUniTvl(
+    (addr) => `bsc:${addr}`,
     chainBlocks["bsc"],
     "bsc",
-    transformAddress,
-    abi.poolInfo,
-    [KAC["bsc"], KACMasterChefContract["bsc"]]
+    KACFactory["bsc"],
+    0,
+    true
   );
-
+}
+async function poolsTvl(timestamp, ethBlock, chainBlocks) {
+  const balances = {};
+  const stakedKAC = sdk.api.erc20.balanceOf({
+    target: KAC["bsc"],
+    owner: KACMasterChefContract["bsc"],
+    chain: "bsc",
+    block: chainBlocks["bsc"],
+  });
+  sdk.util.sumSingleBalance(
+    balances,
+    "bsc:" + KAC["bsc"],
+    (await stakedKAC).output
+  );
   return balances;
-};
+}
 
 module.exports = {
-  timetravel: true,
   misrepresentedTokens: true,
   methodology:
     "Fast growing Defi on BSC and Shiden! Fractionalize and farm NFTs.",
   bsc: {
-    staking: staking(KACMasterChefContract["bsc"], KAC["bsc"], "bsc"),
+    staking: poolsTvl,
     tvl: bscTvl,
   },
   shiden: {
@@ -52,7 +59,7 @@ module.exports = {
       0
     ),
     tvl: calculateUsdUniTvl(
-      "0xa5e48a6E56e164907263e901B98D9b11CCB46C47",
+      "0xcd8620889c1dA22ED228e6C00182177f9dAd16b7",
       "shiden",
       "0x0f933Dc137D21cA519ae4C7E93f87a4C8EF365Ef",
       [
