af21ba6142cf783371e58fda6573118162c570cf
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2021-12-24 17:29:44 -0800

    rm formatting

--- projects/uniswap/index.js
+++ projects/uniswap/index.js
@@ -1,56 +1,42 @@
-const { getChainTvl } = require("../helper/getUniSubgraphTvl");
-const sdk = require("@defillama/sdk");
-const { optimism, ethereum: v3Ethereum } = require("./v3/index");
+const { getChainTvl } = require('../helper/getUniSubgraphTvl');
+const sdk = require('@defillama/sdk')
+const {optimism, ethereum:v3Ethereum} = require('./v3/index')
 
-const v1graph = getChainTvl(
-  {
-    ethereum: "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap",
-  },
-  "uniswaps",
-  "totalLiquidityUSD"
-);
+const v1graph = getChainTvl({
+  ethereum: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap'
+}, "uniswaps", "totalLiquidityUSD")
+
 
 const v2graph = getChainTvl({
-  ethereum: "https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2",
-});
+  ethereum: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2'
+})
 
-const v3Graphs = getChainTvl(
-  {
-    ethereum: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
-    optimism:
-      "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-optimism-dev",
-    arbitrum:
-      "https://api.thegraph.com/subgraphs/name/ianlapham/arbitrum-minimal",
-    polygon:
-      "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon",
-  },
-  "factories",
-  "totalValueLockedUSD"
-);
+const v3Graphs = getChainTvl({
+  ethereum: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
+  optimism: "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-optimism-dev",
+  arbitrum: 'https://api.thegraph.com/subgraphs/name/ianlapham/arbitrum-minimal',
+  polygon: "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon",
+}, "factories", "totalValueLockedUSD")
 
 module.exports = {
   timetravel: true,
   misrepresentedTokens: true,
   methodology: `Counts the tokens locked on AMM pools, pulling the data from the 'ianlapham/uniswapv2' subgraph`,
-  ethereum: {
-    tvl: sdk.util.sumChainTvls([
-      v1graph("ethereum"),
-      v2graph("ethereum"),
-      v3Ethereum.tvl,
-    ]),
+  ethereum:{
+    tvl: sdk.util.sumChainTvls([v1graph("ethereum"), v2graph('ethereum'), v3Ethereum.tvl]),
   },
-  arbitrum: {
-    tvl: v3Graphs("arbitrum"),
+  arbitrum:{
+    tvl: v3Graphs('arbitrum')
   },
-  polygon: {
-    tvl: v3Graphs("polygon"),
+  polygon:{
+    tvl: v3Graphs('polygon')
   },
   optimism,
-  hallmarks: [
+  hallmarks:[
     [1598412107, "SushiSwap launch"],
     [1599535307, "SushiSwap migration"],
     [1600226507, "LM starts"],
     [1605583307, "LM ends"],
-    [1617333707, "FEI launch"],
-  ],
-};
+    [1617333707, "FEI launch"]
+  ]
+}
