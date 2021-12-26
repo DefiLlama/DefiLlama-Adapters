97069b60a567f80d3deafba804955fcc82b24d6a
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-26 10:51:48 +0000

    add staked tokens to afra

--- projects/afraswap/index.js
+++ projects/afraswap/index.js
@@ -1,4 +1,5 @@
 const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
+const { stakingPricedLP } = require('../helper/staking');
 
 module.exports = {
   misrepresentedTokens: true,
@@ -14,5 +15,6 @@ module.exports = {
       ],
       "wbnb"
     ),
+    staking: stakingPricedLP("0x259C852834375864b65202375558AB11B2d330fd", "0x5badD826AeFa700446Fa6d784e6ff97eD6eeDca9", "bsc", "0x1Da189c1BA3d718Cc431a2ed240a3753f89CD47A", "wbnb")
   },
 };
