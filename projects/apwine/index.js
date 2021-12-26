8608c431ceb21351a1a119b0e06a14732cf3124b
Jonathan Chemla <jchemla@iconem.com>
2021-12-26 21:17:32 +0100

    APWine minor comments

--- projects/apwine/index.js
+++ projects/apwine/index.js
@@ -9,13 +9,13 @@ const registry = '0x72d15eae2cd729d8f2e41b1328311f3e275612b9' // same address fo
 // Mapping from tokens locked in APWine to ones that can be read by coingecko
 const transformMapping_ethereum = {
   '0xaC14864ce5A98aF3248Ffbf549441b04421247D3': '0x73968b9a57c6e53d41345fd57a6e6ae27d6cdb2f', // xSDT -> SDT
-  '0x6b1D394Ca67fDB9C90BBd26FE692DdA4F4f53ECD': '0xcafe001067cdef266afb7eb5a286dcfd277f3de5', // sPSP_PP4
-  '0xA991356d261fbaF194463aF6DF8f0464F8f1c742': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // tfUSDC
-  '0x24E79e946dEa5482212c38aaB2D0782F04cdB0E0': '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9', // palStkAave -> aave
+  '0x6b1D394Ca67fDB9C90BBd26FE692DdA4F4f53ECD': '0xcafe001067cdef266afb7eb5a286dcfd277f3de5', // sPSP_PP4 -> PSP
+  '0xA991356d261fbaF194463aF6DF8f0464F8f1c742': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // tfUSDC -> USDC
+  '0x24E79e946dEa5482212c38aaB2D0782F04cdB0E0': '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9', // palStkAave -> AAVE
 }
 const transformMapping_polygon = {
-  '0xDDe43710DefEf6CbCf820B18DeBfC3cF9a4f449F': '', // miFARM_4eur-f (bf4eur-f)
-  '0x5A0801BAd20B6c62d86C566ca90688A6b9ea1d3f': 'polygon:0xd7d4a4c67e9c1f5a913bc38e87e228f4b8820e8a' // Moo Curve aTriCrypto3 
+  '0xDDe43710DefEf6CbCf820B18DeBfC3cF9a4f449F': 'polygon:0xDDe43710DefEf6CbCf820B18DeBfC3cF9a4f449F', // miFARM_4eur-f (bf4eur-f) -> ?
+  '0x5A0801BAd20B6c62d86C566ca90688A6b9ea1d3f': 'polygon:0xd7d4a4c67e9c1f5a913bc38e87e228f4b8820e8a' // Moo Curve aTriCrypto3 -> ?
 }
 const transform  = {
   'ethereum': addr => transformMapping_ethereum[addr] || addr,
