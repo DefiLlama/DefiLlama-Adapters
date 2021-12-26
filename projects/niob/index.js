e2808e234bd43ddf37aeee06f37eb6fc9e5c957a
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2021-12-26 12:50:02 +0000

    add missing parameter

--- projects/niob/index.js
+++ projects/niob/index.js
@@ -3,4 +3,4 @@ const { masterChefExports } = require("../helper/masterchef");
 const NIOB = "0x5ac5e6Af46Ef285B3536833E65D245c49b608d9b";
 const masterchef = "0xD18B23ad6c8ACc4AD32AAd6a5dF750ce28C8C772";
 
-module.exports = masterChefExports(masterchef, "bsc", NIOB);
+module.exports = masterChefExports(masterchef, "bsc", NIOB, false);
