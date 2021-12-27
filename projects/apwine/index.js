88c5dd239dff37931639880107e5caec1fe29cfd
Jonathan Chemla <jchemla@iconem.com>
2021-12-27 23:50:23 +0100

    Handle APWine wrapped curve pool tokens
    Handle APWine wrapped curve pool tokens

--- projects/apwine/index.js
+++ projects/apwine/index.js
@@ -1,8 +1,8 @@
-const sdk = require("@defillama/sdk");
-const abi = require("./abi.json");
-const {sumTokens} = require("../helper/unwrapLPs.js");
-const {staking} = require("../helper/staking.js");
-const {pool2s} = require("../helper/pool2.js");
+const sdk = require("@defillama/sdk")
+const abi = require("./abi.json")
+const {sumTokens, unwrapCrv} = require("../helper/unwrapLPs.js")
+const {staking} = require("../helper/staking.js")
+const {pool2s} = require("../helper/pool2.js")
 
 const registry = '0x72d15eae2cd729d8f2e41b1328311f3e275612b9' // same address for polygon and mainnet
 
@@ -14,8 +14,10 @@ const transformMapping_ethereum = {
   '0x24E79e946dEa5482212c38aaB2D0782F04cdB0E0': '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9', // palStkAave -> AAVE
 }
 const transformMapping_polygon = {
-  '0xDDe43710DefEf6CbCf820B18DeBfC3cF9a4f449F': 'polygon:0xDDe43710DefEf6CbCf820B18DeBfC3cF9a4f449F', // miFARM_4eur-f (bf4eur-f) -> ?
-  '0x5A0801BAd20B6c62d86C566ca90688A6b9ea1d3f': 'polygon:0xd7d4a4c67e9c1f5a913bc38e87e228f4b8820e8a' // Moo Curve aTriCrypto3 -> ?
+}
+const tokensToUnwrap =  {
+  'polygon:0xDDe43710DefEf6CbCf820B18DeBfC3cF9a4f449F': {'type': 'crv', 'unwrapTo': '0xad326c253a84e9805559b73a08724e11e49ca651'},  // miFARM_4eur-f (bf4eur-f) -> 4eur-f
+  'polygon:0x5A0801BAd20B6c62d86C566ca90688A6b9ea1d3f': {'type': 'crv', 'unwrapTo': '0xdad97f7713ae9437fa9249920ec8507e5fbb23d3'}, // Moo Curve aTriCrypto3 -> aTriCrypto3
 }
 const transform  = {
   'ethereum': addr => transformMapping_ethereum[addr] || addr,
@@ -31,11 +33,10 @@ const APW_WETH_cometh = '0x70797fc5b1c04541113b5ac20ea05cb390392e30'
 const APW_MUST_cometh = '0x174f902194fce92ef3a51079b531f1e5073de335'
 const APW_WETH_cometh_staking = '0x4e2114f7fa11dc0765ddd51ad98b6624c3bf1908'
 const APW_MUST_cometh_staking = '0xb7ae78f49ac9bd9388109a4c5f53c6b79be4deda'
-// const APW_WETH_sushi = '0x53162d78dca413d9e28cf62799d17a9e278b60e8'
 
 const tvl_from_registry = (chain) => {
   return async (timestamp, ethBlock, chainBlocks) => {
-    const balances = {};
+    const balances = {}
     const block = chainBlocks[chain]
     // Get vaults count, vault addresses, and IBT tokens held by each vault
     const {output: futureVaultCount} = await sdk.api.abi.call({
@@ -64,10 +65,21 @@ const tvl_from_registry = (chain) => {
 
     // Unwrap tokens amount held by each vault
     const tokensAndOwners = IBTAddressTokens.map(t => [t.output, t.input.target])
-    await sumTokens(balances, tokensAndOwners, block, chain, transform[chain]) // id=>`${chain}:${id}`
+    await sumTokens(balances, tokensAndOwners, block, chain, transform[chain]) 
     console.log(`balances for chain ${chain}`, balances) 
+
+    // Handle wrapped pools in balances - like curvePools, etc
+    for (const token of Object.keys(balances)) {
+      if (Object.keys(tokensToUnwrap).includes(token)) {
+        if (tokensToUnwrap[token].type === 'crv') {
+          await unwrapCrv(balances, tokensToUnwrap[token].unwrapTo, balances[token], block, chain, transform[chain]) 
+        }
+        balances[token] = 0 // Once unwrapped, set balance of wrapped curve token to zero
+      }
+    }
+
     return balances
-  };
+  }
 }
 
 
@@ -81,4 +93,4 @@ module.exports = {
     pool2: pool2s([APW_WETH_cometh_staking, APW_MUST_cometh_staking], [APW_WETH_cometh, APW_MUST_cometh], "polygon", id=>`polygon:${id}`)
   },
   methodology: `Use the registry to retrieve futureVaults, and get for each vault the IBT which is the token that this vault holds - the user locked collateral`
-};
+}
