8ef51f91f9d8d721a6e4b6ff72caf8141875b7be
0xlyrix <92387419+0xlyrix@users.noreply.github.com>
2021-12-27 11:35:03 +0700

    single staking

--- projects/wannaswap/index.js
+++ projects/wannaswap/index.js
@@ -4,7 +4,7 @@ const { staking } = require('../helper/staking.js');
 module.exports = {
     misrepresentedTokens: true,
     aurora: {
-        staking: staking("0x5205c30bf2E37494F8cF77D2c19C6BA4d2778B9B", "0x7faA64Faf54750a2E3eE621166635fEAF406Ab22", "aurora"),
+        staking: staking("0x5205c30bf2E37494F8cF77D2c19C6BA4d2778B9B", "0x7faA64Faf54750a2E3eE621166635fEAF406Ab22", "aurora"), // single staking
         tvl:calculateUsdUniTvl(
             // factory
             "0x7928D4FeA7b2c90C732c10aFF59cf403f0C38246",
