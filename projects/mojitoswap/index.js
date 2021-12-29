00f03b753eb376088ce44447d9bc8a9f99766906
bbelly <bawei3600@gmail.com>
2021-12-29 13:56:33 +0800

    add mojitoswap

---
+++ projects/mojitoswap/index.js
@@ -0,0 +1,15 @@
+const { getChainTvl } = require('../helper/getUniSubgraphTvl');
+
+const graphUrls = {
+  kcc: 'https://thegraph.kcc.network/subgraphs/name/mojito/swap',
+};
+const chainTvl = getChainTvl(graphUrls);
+
+module.exports = {
+  misrepresentedTokens: true,
+  methodology: 'We count liquidity on the dexes, pulling data from subgraphs',
+  kcc: {
+    tvl: chainTvl('kcc'),
+  },
+  tvl: chainTvl('kcc'),
+};
