3976d05924e6a69588fd1498c963eb89797a725e
0xngmi <0xngmi@protonmail.com>
2022-01-01 13:12:16 +0000

    add muesliswap on cardano

--- projects/muesliswap/index.js
+++ projects/muesliswap/index.js
@@ -1,9 +1,40 @@
 const {calculateUsdUniTvl} = require('../helper/getUsdUniTvl')
+const { fetchURL } = require('../helper/utils')
+
+async function adaTvl(){
+    const tokens = (await fetchURL("https://orders.muesliswap.com/tokens-info")).data
+    let totalAda = 0
+    await Promise.all(tokens.map(async t=>{
+        const orders = (await fetchURL(`https://orders.muesliswap.com/orderbook/?policy-id=${t.policyId}&tokenname=${t.name}`)).data
+        if(orders.fromToken !== "."){
+            throw new Error("Tokens paired against something other than ADA")
+        }
+        orders.buy.forEach(o=>{
+            totalAda += o.totalPrice
+        })
+        if(orders.buy.length === 0 || orders.sell.length === 0){
+            return
+        }
+        const topPrice = orders.buy[0].price
+        let totalAmountOtherToken = 0
+        orders.sell.forEach(o=>{
+            totalAmountOtherToken += o.amount
+        })
+        totalAda += totalAmountOtherToken * topPrice
+    }))
+    return {
+        cardano: totalAda
+    }
+}
 
 module.exports={
     misrepresentedTokens: true,
+    timetravel: false,
     methodology: "Factory address (0x72cd8c0B5169Ff1f337E2b8F5b121f8510b52117) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
     smartbch: {
         tvl:calculateUsdUniTvl("0x72cd8c0B5169Ff1f337E2b8F5b121f8510b52117", "smartbch", "0x3743eC0673453E5009310C727Ba4eaF7b3a1cc04", ["0xc8E09AEdB3c949a875e1FD571dC4b3E48FB221f0"], "bitcoin-cash")
+    },
+    cardano:{
+        tvl: adaTvl
     }
 }
