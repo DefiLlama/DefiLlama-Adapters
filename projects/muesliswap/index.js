4a8a2e3756b75146edb008b8c8c800e687b42400
0xngmi <0xngmi@protonmail.com>
2022-01-01 13:33:03 +0000

    methodology update

--- projects/muesliswap/index.js
+++ projects/muesliswap/index.js
@@ -32,7 +32,7 @@ async function adaTvl(){
 module.exports={
     misrepresentedTokens: true,
     timetravel: false,
-    methodology: "Factory address (0x72cd8c0B5169Ff1f337E2b8F5b121f8510b52117) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
+    methodology: "Factory address (0x72cd8c0B5169Ff1f337E2b8F5b121f8510b52117) is used to find the LP pairs. TVL is equal to the liquidity on the AMM. For cardano we calculate the tokens on resting orders",
     smartbch: {
         tvl:calculateUsdUniTvl("0x72cd8c0B5169Ff1f337E2b8F5b121f8510b52117", "smartbch", "0x3743eC0673453E5009310C727Ba4eaF7b3a1cc04", ["0xc8E09AEdB3c949a875e1FD571dC4b3E48FB221f0"], "bitcoin-cash")
     },
