63c67211a8cecb67103f211093354a03923aaac6
Denis Carriere <carriere.denis@gmail.com>
2022-01-04 11:05:48 -0500

    update pizza

--- projects/obswap/index.js
+++ projects/obswap/index.js
@@ -7,12 +7,13 @@ const tokens = [
   ["token.defi", "BOX", "defibox"],
   ["minedfstoken", "DFS", "defis-network"],
   ["emanateoneos", "EMT", "emanate"],
+  ["dappservices", "DAPP", "dapp"],
   ["token.newdex", "DEX", "newdex-token"],
   ["chexchexchex", "CHEX", "chex-token"],
   ["everipediaiq", "IQ", "everipedia"],
   ["eosiotptoken", "TPT", "token-pocket"],
   ["core.ogx", "OGX", "organix"],
-  ["pizzatotoken", "PIZZA", "pizza"],
+  ["pizzatotoken", "PIZZA", "pizza-usde"],
 ]
 
 // https://swap.defiob.com
@@ -20,8 +21,6 @@ const tokens = [
 async function eos() {
   return await get_account_tvl("swap.ob", tokens);
 }
-
-eos().then(a => console.log(a))
 
 module.exports = {
   methodology: `OBSwap TVL is achieved by querying token balances from their swap pool smart contract.`,
