b7c7b3a3dc0b0b413820f64c4b0759bb80effd0a
Boba Fett <bobafettirl@pm.me>
2022-01-04 10:07:38 -0800

    Updated to match tomb

--- projects/nacho-finance/index.js
+++ projects/nacho-finance/index.js
@@ -1,78 +1,56 @@
 const sdk = require("@defillama/sdk");
-const axios = require("axios");
-const { transformPolygonAddress } = require("../helper/portedTokens");
 const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
+const { staking } = require("../helper/staking");
 
 const tombTokenAddress = "0xcD86152047e800d67BDf00A4c635A8B6C0e5C4c2";
 const tshareTokenAddress = "0x948D0a28b600BDBd77AF4ea30E6F338167034181";
-
-const tombFtmLpAddress = "0x8d25fec513309f2d329d99d6f677d46c831fdee8";
-const tshareFtmLpAddress = "0x1c84cd20ea6cc100e0a890464411f1365ab1f664";
-
-const masonryAddress = "0x1ad667aCe03875fe48534c65BFE14191CF81fd64";
 const tshareRewardPoolAddress = "0xdD694F459645eb6EfAE934FE075403760eEb9aA1";
+const masonryAddress = "0x1ad667aCe03875fe48534c65BFE14191CF81fd64";
 
-async function tvl(timestamp, block, chainBlocks) {
-  const balances = {};
+const ftmLPs = [
+  "0x8d25fec513309f2d329d99d6f677d46c831fdee8", // tombFtmLpAddress
+  "0x1c84cd20ea6cc100e0a890464411f1365ab1f664", //tshareFtmLpAddress
+];
+
+async function calcPool2(masterchef, lps, block, chain) {
+  let balances = {};
+  const lpBalances = (
+    await sdk.api.abi.multiCall({
+      calls: lps.map((p) => ({
+        target: p,
+        params: masterchef,
+      })),
+      abi: "erc20:balanceOf",
+      block,
+      chain,
+    })
+  ).output;
   let lpPositions = [];
-  let transformAddress = await transformPolygonAddress();
-
-  // Masonry TVL
-  const masonryBalance = sdk.api.erc20
-    .balanceOf({
-      target: tshareTokenAddress,
-      owner: masonryAddress,
-      block: chainBlocks["polygon"],
-      chain: "polygon",
+  lpBalances.forEach((p) => {
+    lpPositions.push({
+      balance: p.output,
+      token: p.input.target,
     });
-  sdk.util.sumSingleBalance(
-    balances,
-    transformAddress(tshareTokenAddress),
-    (await masonryBalance).output
-  );
-
-  // Cemetery TOMB-FTM LP TVL
-  const tombFtmLpCemeteryBalance = sdk.api.erc20
-    .balanceOf({
-      target: tombFtmLpAddress,
-      owner: tshareRewardPoolAddress,
-      block: chainBlocks["polygon"],
-      chain: "polygon",
-    });
-
-  lpPositions.push({
-    token: tombFtmLpAddress,
-    balance: (await tombFtmLpCemeteryBalance).output,
   });
-
-  // Cemetery TSHARE-FTM LP TVL
-  const tshareFtmLpCemeteryBalance = sdk.api.erc20
-    .balanceOf({
-      target: tshareFtmLpAddress,
-      owner: tshareRewardPoolAddress,
-      block: chainBlocks["polygon"],
-      chain: "polygon",
-    });
-
-  lpPositions.push({
-    token: tshareFtmLpAddress,
-    balance: (await tshareFtmLpCemeteryBalance).output,
-  });
-
   await unwrapUniswapLPs(
     balances,
     lpPositions,
-    chainBlocks["polygon"],
-    "polygon",
-    transformAddress
+    block,
+    chain,
+    (addr) => `${chain}:${addr}`
   );
   return balances;
 }
 
+async function ftmPool2(timestamp, block, chainBlocks) {
+  return await calcPool2(tshareRewardPoolAddress, ftmLPs, chainBlocks.polygon, "polygon");
+}
+
 module.exports = {
-  methodology: 'The TVL of Nacho Finance is calculated using the Quickswap LP token deposits(NACHO/ETH and NSHARE/MATIC), and the NSHSARE deposits found in the Bowl contract.',
+  methodology: "Pool2 deposits consist of NACHO/ETH and NSHAER/MATIC LP tokens deposits while the staking TVL consists of the NSHARE tokens locked within the Bowl contract.",
   polygon: {
-    tvl,
+    tvl: async () => ({}),
+    pool2: ftmPool2,
+    staking: staking(masonryAddress, tshareTokenAddress, "polygon"),
   },
-  tvl,
 };
