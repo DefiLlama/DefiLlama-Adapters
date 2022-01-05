f3abffcba7377e10500c139e14f78a429678c71a
Jonathan Chemla <jchemla@iconem.com>
2022-01-05 15:20:07 +0100

    Update Jarvis TVL computation for v5
    Update Jarvis TVL computation for v5
    Only thing needed now is the synthpoolRegistry for each chain

--- projects/jarvis/index.js
+++ projects/jarvis/index.js
@@ -1,6 +1,7 @@
 const sdk = require("@defillama/sdk");
+const abi = require("./abi.json");
 
-const contracts = {
+const contracts_old = {
   ethereum:{
     usdcToken : '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     synthCollateralContracts : ['0x48546bdd57d34cb110f011cdd1ccaae75ee17a70', '0x182d5993106573a95a182ab3a77c892713ffda56', '0x496b179d5821d1a8b6c875677e3b89a9229aab77', "0x911f0Dfc9d98Fcf6E4d07410E7aC460F19843599", "0xF47Ff36956105255E64455BfEDe4538768439066", "0x2431b64cDD6D7E9A630046d225BA4F01B3AC9D3b", "0x4e3Decbb3645551B8A19f0eA1678079FCB33fB4c", "0x767058F11800FBA6A682E73A6e79ec5eB74Fac8c", "0xbD1463F02f61676d53fd183C2B19282BFF93D099", "0xCbbA8c0645ffb8aA6ec868f6F5858F2b0eAe34DA", "0xeF4Db4AF6189aae295a680345e07E00d25ECBAAb", "0x10d00f5788c39a2bf248adfa2863fa55d83dce36"],
@@ -13,11 +14,11 @@ const contracts = {
   }
 }
 
-function chainTvl(chain) {
+function chainTvl_old(chain) {
   return async (timestamp, ethBlock, chainBlocks) => {
     let balances = {};
     const block = chainBlocks[chain]
-    const {synthCollateralContracts, liquidityPools, usdcToken} = contracts[chain]
+    const {synthCollateralContracts, liquidityPools, usdcToken} = contracts_old[chain]
 
     const collateralTokens = await sdk.api.abi.multiCall({
       abi: 'erc20:balanceOf',
@@ -34,6 +35,89 @@ function chainTvl(chain) {
 }
 
 
+// The synthpoolRegistry addresses can be found in this repo
+// https://gitlab.com/jarvis-network/apps/exchange/mono-repo/-/tree/dev/libs/contracts/networks
+const contracts = {
+  polygon: { // 137
+    version: 4, synthpoolRegistry: '0xdCE12741DF9d2CcF2A8bB611684C8151De91a7d2', 
+  },
+  ethereum: { // 1
+    version: 4, synthpoolRegistry: '0xaB77024DdC68A3Fe942De8dDb0014738ED01A5e5', 
+  },
+  bsc: { // 56
+    version: 5, synthpoolRegistry: '0x02c72Fc6C231eE0f0e313c966e2da5cb7a52B503', 
+  },
+  xdai: { // 100
+    version: 5, synthpoolRegistry: '0x43a98e5C4A7F3B7f11080fc9D58b0B8A80cA954e', 
+  }
+}
+function chainTvl(chain) {
+  return async (timestamp, ethBlock, chainBlocks) => {
+    const { synthpoolRegistry, version } = contracts[chain]
+    const block = chainBlocks[chain]
+    console.log(chain, block, 'synthpoolRegistry', synthpoolRegistry)
+
+    // Get liquidityPools by calling getElements(synth, collateral, version)
+    // For v4, these are not AMM LP pools, just pools where money is waiting to be used for minting
+    // For v5, the collateral is stored in the liquidity pools directly
+    // Get collaterals, usually single collat, USDC on polygon, BUSD on BSC, but might be multiple collats later on
+    const {output: collaterals} = await sdk.api.abi.call({
+      abi: abi["SynthereumPoolRegistry_getCollaterals"],
+      target: synthpoolRegistry,
+      block,
+      chain
+    })
+    // Get synth token symbols - jEUR, jCHF etc
+    const {output: syntheticTokens} = await sdk.api.abi.call({
+      abi: abi["SynthereumPoolRegistry_getSyntheticTokens"],
+      target: synthpoolRegistry,
+      block,
+      chain
+    })
+    // Get liquidityPools by calling SynthereumPoolRegistry_getElements
+    const params_list = collaterals.map(collateral => syntheticTokens.map(synth => [synth, collateral, version])).flat()
+    const {output: elements_obj} = await sdk.api.abi.multiCall({
+      abi: abi["SynthereumPoolRegistry_getElements"],
+      calls: params_list.map(params => ({
+        target: synthpoolRegistry,
+        params: params
+      })),
+      block,
+      chain
+    })
+    const liquidityPools = elements_obj.map(e => e.output).flat(2)
+
+    // Retrieve synthCollateralContracts where most of the collateral is held for v4 
+    if (version <= 4) {
+      const synthCollateralContracts = (await sdk.api.abi.multiCall({
+        abi: abi['LiquidityPool_getAllDerivatives'],
+        calls: liquidityPools.map(a => ({target: a})),
+        block,
+        chain
+      })).output.map(e => e.output).flat().filter(c => c) // filter out null contracts
+      liquidityPools.push(...synthCollateralContracts)
+    } 
+    
+    // Get balances of every LiquidityPool and SynthToken Contracts
+    const tokenBalances_calls = collaterals.map(collat => 
+      liquidityPools.map(contract => ({
+        target: collat,
+        params: [contract]
+      }))
+    ).flat()
+    const collateralTokens = await sdk.api.abi.multiCall({
+      abi: 'erc20:balanceOf',
+      calls: tokenBalances_calls,
+      block,
+      chain
+    })
+    const balances = {};
+    sdk.util.sumMultiBalanceOf(balances, collateralTokens, true, t=>`${chain}:${t}`)
+    return balances
+  }
+}
+
+
 module.exports = {
   ethereum: {
     tvl: chainTvl('ethereum')
@@ -41,5 +125,11 @@ module.exports = {
   polygon: {
     tvl: chainTvl('polygon')
   },
-  tvl: sdk.util.sumChainTvls([chainTvl('ethereum'), chainTvl('polygon')])
+  xdai: {
+    tvl: chainTvl('xdai')
+  },
+  bsc: {
+    tvl: chainTvl('bsc')
+  }, 
+  'Methodology': 'Count collateral used to mint jFIAT - in v4 it is held by liquidityPools and synthCollateralContracts, in v5 only in liquidityPools'
 }
