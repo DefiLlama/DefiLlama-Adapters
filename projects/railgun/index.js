425953f3e5f3fc608cbfa9346726ea4f4948198a
liucougar <liucougar@gmail.com>
2022-01-05 22:56:28 -0800

    Add balances in treasury in the TVL for railgun

--- projects/railgun/index.js
+++ projects/railgun/index.js
@@ -3,6 +3,21 @@ const abi = require('./ABI.json');
 
 const STAKING_RAIL = '0xee6a649aa3766bd117e12c161726b693a1b2ee20';
 const RAIL = '0xe76C6c83af64e4C60245D8C7dE953DF673a7A33D';
+const TREASURY = '0xc851fbe0f07a326ce0326ccc70c2a62732e74d6c';
+async function treasury(block, balances) {
+    const tokens = await sdk.api.util.tokenList();
+    const result = await sdk.api.abi.multiCall({
+        calls: tokens.map(t => ({target: t.contract, params: TREASURY})),
+        abi: 'erc20:balanceOf',
+        block: block
+    });
+    for(const r of result.output) {
+        if(r.success && r.output && r.output !== '0') {
+            balances[r.input.target] = r.output;
+        }
+    }
+}
+
 async function tvl(timestamp, block) {
     let balances = {};
 
@@ -26,6 +41,7 @@ async function tvl(timestamp, block) {
         balances[RAIL] = 0;
     }
 
+	await treasury(block, balances);
 
     return balances;
 }
