7b229cdaf03ded0eb3dc27ff0838fd3711b7b291
impelcrypto <impelcrypto@gmail.com>
2022-01-07 15:51:05 +0800

    feat: calculate TVL in Shiden network

--- projects/shiden-dapps-staking/index.js
+++ projects/shiden-dapps-staking/index.js
@@ -1,22 +1,26 @@
-
-const { ApiPromise, WsProvider } = require("@polkadot/api");
-const { options } = require("@PlasmNetwork/api");
+const { ApiPromise, WsProvider } = require('@polkadot/api');
 
 const SDN_DECIMALS = 18;
 
-
 async function tvl() {
-  const provider = new WsProvider("wss://rpc.shiden.astar.network");
-  const api = await ApiPromise.create(options({ provider }));
-  const era = (await api.query.dappsStaking.currentEra())
-  const SDNLocked = (await api.query.dappsStaking.eraRewardsAndStakes(era)).toJSON() / 10 ** SDN_DECIMALS;
+  const provider = new WsProvider('wss://rpc.shiden.astar.network');
+  const api = new ApiPromise({
+    provider,
+  });
+
+  await api.isReady;
+  const era = await api.query.dappsStaking.currentEra();
+  const result = await api.query.dappsStaking.eraRewardsAndStakes(era);
+  const tvl = result.unwrap().staked.valueOf();
+  const SDNLocked = tvl / 10 ** SDN_DECIMALS;
 
   return {
-    'shiden': SDNLocked
-  }
+    shiden: SDNLocked,
+  };
 }
 
 module.exports = {
-  methodology: 'TVL considers SDN tokens deposited to the Dapps-Staking program',
-  tvl
-}
+  methodology:
+    'TVL considers SDN tokens deposited to the Dapps-Staking program',
+  tvl,
+};
