c7a607f2c05be19a371949b228de3f5a0e3da9be
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2022-01-06 22:37:21 -0800

    wip balancer custom backfill

--- dexVolumes/balancer/index.js
+++ dexVolumes/balancer/index.js
@@ -1,18 +1,22 @@
 const { getChainVolume } = require("../helper/getUniSubgraphVolume");
+const { ARBITRUM, ETHEREUM, POLYGON } = require("../helper/chains");
+const { getStartTimestamp } = require("../helper/getStartTimestamp");
+const { getBlock } = require("../helper/getBlock");
+const { ethereum } = require("../../projects/balancer");
 
 const endpoints = {
-  ethereum: "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer",
-  polygon:
+  [ETHEREUM]: "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer",
+  [POLYGON]:
     "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-polygon-v2",
-  arbitrum:
+  [ARBITRUM]:
     "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-arbitrum-v2",
 };
 
 const graphs = getChainVolume({
   graphUrls: {
-    ethereum: endpoints.ethereum,
-    polygon: endpoints.polygon,
-    arbitrum: endpoints.arbitrum,
+    [ETHEREUM]: endpoints[ETHEREUM],
+    [POLYGON]: endpoints[POLYGON],
+    [ARBITRUM]: endpoints[ARBITRUM],
   },
   totalVolume: {
     factory: "balancers",
@@ -21,8 +25,16 @@ const graphs = getChainVolume({
   hasDailyVolume: false,
 });
 
+// const ethStart = getBlock(9747796, ethereum);
+
 module.exports = {
   volume: {
-    ethereum: graphs("ethereum"),
+    [ETHEREUM]: {
+      fetch: graphs(ETHEREUM),
+      start: ethStart,
+      customBackfill: () => {},
+    },
   },
 };
+
+// TODO custom backfill have to get specific block at start of each day
