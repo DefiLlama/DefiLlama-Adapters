e60e19fc7511c1ca1147a79b867bc5b475a9ad40
ugolino <dan.ugolini@gmail.com>
2022-01-10 15:36:56 +0100

    updated controller add

--- projects/opyn/squeeth.js
+++ projects/opyn/squeeth.js
@@ -2,7 +2,7 @@ const sdk = require('@defillama/sdk');
 const BigNumber = require('bignumber.js');
 
 const START_BLOCK = 13944813;
-const controller = "0x4c1fd946A082d26b40154EabD12F7A15a0cB3020";
+const controller = "0x0344f8706947321fa87881d3dad0eb1b8c65e732";
 const ETH = '0x0000000000000000000000000000000000000000';
 
 module.exports.tvl = async function tvl(timestamp, block) {  
