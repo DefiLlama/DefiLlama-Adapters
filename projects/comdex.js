ebea9d01bb0e5896f18d0a378ff29d64921bff30
Chandragupta Singh <chandragupta@MacBook-Air.local>
2022-01-12 11:31:59 +0530

    add cmdx

---
+++ projects/comdex.js
@@ -0,0 +1,23 @@
+const utils = require('./helper/utils')
+const {getApiTvl} = require('./helper/historicalApi')
+
+function tvl(timestamp){
+    return getApiTvl(timestamp, 
+    async()=>{
+        const data = await utils.fetchURL("https://api-osmosis.imperator.co/tokens/v1/CMDX")
+        return data.data[0].liquidity
+    },
+    async()=>{
+        const data = await utils.fetchURL("https://api-osmosis.imperator.co/tokens/v1/liquidity/CMDX/chart")
+        return data.data.map(d=>({
+            date: Math.round(new Date(d.time).getTime()/1000),
+            totalLiquidityUSD: d.value
+        }))
+    })
+}
+
+module.exports = {
+    misrepresentedTokens: true,
+    methodology: "Counts the liquidity on all AMM pools. Metrics come from https://info.osmosis.zone/",
+    tvl
+}
