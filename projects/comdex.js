5e0b3c3ccfa78aca1054d3f2b38262a400607d17
Chandragupta Singh <chandragupta@MacBook-Air.local>
2022-01-13 11:23:00 +0530

    TVL changed

--- projects/comdex.js
+++ projects/comdex.js
@@ -1,24 +1,25 @@
-const utils = require('./helper/utils')
-const {getApiTvl} = require('./helper/historicalApi')
+const retry = require('./helper/retry')
+const axios = require("axios");
+const {
+    humanizeNumber,
+} = require("@defillama/sdk/build/computeTVL/humanizeNumber");
 
-function tvl(timestamp){
-    return getApiTvl(timestamp, 
-    async()=>{
-        const data = await utils.fetchURL("https://api-comdex.zenchainlabs.io/staking/pool")
-        bonded = data.data.result.bonded_tokens
-        return bonded / Math.pow(10,6)
-    },
-    async()=>{
-        const data = await utils.fetchURL("https://api-osmosis.imperator.co/tokens/v1/liquidity/CMDX/chart")
-        return data.data.map(d=>({
-            date: Math.round(new Date(d.time).getTime()/1000),
-            totalLiquidityUSD: d.value
-        }))
-    })
+async function fetch() {
+    var response = await retry(async bail => await axios.get('https://api-comdex.zenchainlabs.io/staking/pool'))
+    var response2 = await retry(async bil => await axios.get('https://api-osmosis.imperator.co/tokens/v1/CMDX'))
+
+    var price = response2.data[0].price
+    var liquid = response2.data[0].liquidity
+
+    var bonded_amt = (response.data.result.bonded_tokens / Math.pow(10, 6)) * price
+
+    console.log("---- tvl ----")
+    console.log("\Bonded Tokens".padEnd(25, " "), humanizeNumber(bonded_amt), "\n")
+    console.log("\Liquidity in Osmosis".padEnd(25, " "), humanizeNumber(liquid), "\n")
+
+    return bonded_amt + liquid;
 }
 
 module.exports = {
-    misrepresentedTokens: true,
-    methodology: "Counts the liquidity on all AMM pools. Metrics come from https://info.osmosis.zone/",
-    tvl
+    fetch
 }
