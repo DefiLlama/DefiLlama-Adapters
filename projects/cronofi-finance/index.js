292a06c87a4a44c7a6b3ce21173dd73f41f2ef9f
nemusona <nemusonaneko@protonmail.com>
2022-01-14 15:55:21 -0600

    add misrepresented export

--- projects/cronofi-finance/index.js
+++ projects/cronofi-finance/index.js
@@ -5,5 +5,6 @@ const masterchef = "0xdc0E1690c594dDD1654544e74036Ee0b0029573d";
 const token = "0x3Df064069Ba2c8B395592E7834934dBC48BbB955";
 
 module.exports = {
+    misrepresentedTokens: true,
     ...masterChefExports(masterchef, "cronos", token, false)
 }
