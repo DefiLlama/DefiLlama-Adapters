99054e9164b51831c4f535592e282ceff5b408b3
nemusona <nemusonaneko@protonmail.com>
2022-01-14 15:54:41 -0600

    cronofi-finance

---
+++ projects/cronofi-finance/index.js
@@ -0,0 +1,9 @@
+const {masterChefExports} = require('../helper/masterchef');
+
+
+const masterchef = "0xdc0E1690c594dDD1654544e74036Ee0b0029573d";
+const token = "0x3Df064069Ba2c8B395592E7834934dBC48BbB955";
+
+module.exports = {
+    ...masterChefExports(masterchef, "cronos", token, false)
+}
