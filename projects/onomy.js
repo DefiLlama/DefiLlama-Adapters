0c68ec5c4d52714d432094705d73dde11cbfbfc0
AlexOnomy <alex@onomy.io>
2022-01-18 22:49:15 +0000

    Onomy placeholder fetch adapter

---
+++ projects/onomy.js
@@ -0,0 +1,8 @@
+async function fetch() {
+  // Onomy chain is in development.  Once deployed this will be updated to fetch TVL from Onomy as well as EVM deployments
+  return 0;
+}
+​
+module.exports = {
+  fetch
+}
