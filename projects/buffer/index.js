a3e20ec336cf966f387f789616bf4da236ecae5a
Wer Heisenberg <heisenberg@buffer.finance>
2022-01-18 22:01:32 +0530

    Update index.js

--- projects/buffer/index.js
+++ projects/buffer/index.js
@@ -3,13 +3,15 @@ const sdk = require("@defillama/sdk");
 const tokens = {
     IBFR: "0xa296aD1C47FE6bDC133f39555C1D1177BD51fBc5",
     POOL: "0x7338ee5535F1E0f1a210a6Ef6dB34f5357EB9860",
+    STAKING: "0xE6C2cDD466Eb1Fa6bDFDb8af1BD072d4A57734C2",
+    WBNB: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
 };
 
 async function tvl(_timestamp, ethBlock, chainBlocks) {
 
     const ibfrV1 = (await sdk.api.erc20.balanceOf({
         target: tokens.IBFR,
-        owner: '0xE6C2cDD466Eb1Fa6bDFDb8af1BD072d4A57734C2',
+        owner: tokens.STAKING,
         block: chainBlocks["bsc"],
         chain: "bsc"
     })).output;
@@ -23,8 +25,8 @@ async function tvl(_timestamp, ethBlock, chainBlocks) {
       ).output;
 
     const balances = {
-        'bsc:0xa296aD1C47FE6bDC133f39555C1D1177BD51fBc5': ibfrV1,
-        'bsc:0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c': bnbV1
+        `bsc:${tokens.IBFR}`: ibfrV1,
+        `bsc:${tokens.WBNB}`: bnbV1
     }
 
     return balances
