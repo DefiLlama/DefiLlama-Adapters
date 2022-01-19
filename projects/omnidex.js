82b281c8e47dc313178010d751c50cb67776ea34
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2022-01-19 08:24:37 +0000

    fix methodology

--- projects/omnidex.js
+++ projects/omnidex.js
@@ -1,6 +1,6 @@
 const { calculateUsdUniTvl } = require("./helper/getUsdUniTvl");
 module.exports = {
-    methodology: `OmniDex is an automated market-making (AMM) decentralized exchange. Swap, Farm and earn rewards on the first native DEX built on the Telos EVM!`,
+    methodology: `Uses factory(0xF9678db1CE83f6f51E5df348E2Cc842Ca51EfEc1) address and whitelisted tokens to find and price Liquidity Pool pairs`,
     misrepresentedTokens: true,
     doublecounting: false,
     timeTravel: true,
