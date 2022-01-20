8ba69cd327c4544977289b053a9fbd3b530cc140
starstream <starstream@starstream.finance>
2022-01-20 11:01:01 +0000

    Add Starstream Finance adapter

---
+++ projects/starstream/index.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const STARSTREAM_API = "https://starstream.finance/api/";
+
+const client = axios.create({
+  baseURL: STARSTREAM_API
+});
+
+async function fetch() {
+  const tvlMsg = await client.get("/tvlTotal");
+  return tvlMsg.data.tvlTotal;
+}
+
+module.exports = {
+  fetch
+}
