ac2d6f586c4470bb7183f27bead64dba5aaf90d3
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2022-01-21 23:27:03 +0000

    change to use factory

--- projects/babyswap/index.js
+++ projects/babyswap/index.js
@@ -1,4 +1,4 @@
-const sdk = require("@defillama/sdk");
+/*const sdk = require("@defillama/sdk");
 const abi = require("./abi.json");
 
 const { addFundsInMasterChef } = require("../helper/masterchef");
@@ -26,7 +26,7 @@ const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
       "0x53E562b9B7E5E94b81f10e96Ee70Ad06df3D2657",
       "0xdfAa0e08e357dB0153927C7EaBB492d1F60aC730",
     ]
-  );
+  );0x86407bEa2078ea5f5EB5A52B2caA963bC1F889Da
 
   return balances;
 };
@@ -39,4 +39,32 @@ module.exports = {
   },
   methodology:
     "We count liquidity on the Farms (LP tokens) and Pools (single tokens) seccions threw MasterChef Contract",
+};*/
+const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
+module.exports = {
+    methodology: `Uses factory(0x86407bEa2078ea5f5EB5A52B2caA963bC1F889Da) address and whitelisted tokens address to find and price Liquidity Pool pairs`,
+    misrepresentedTokens: true,
+    doublecounting: false,
+    timeTravel: true,
+    incentivized: true,
+    moonriver: {
+        tvl: calculateUsdUniTvl(
+            "0x86407bEa2078ea5f5EB5A52B2caA963bC1F889Da",
+            "bsc",
+            "0x55d398326f99059ff775485246999027b3197955",
+            [
+                "0x53E562b9B7E5E94b81f10e96Ee70Ad06df3D2657",
+                "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
+                "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+                "0x2170ed0880ac9a755fd29b2688956bd959f933f8",
+                "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+                "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3",
+                "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c",
+                "0xba2ae424d960c26247dd6c32edc70b295c744c43"
+            ],
+            "tether"
+        ),
+    }
 };
+
+
