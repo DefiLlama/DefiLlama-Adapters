320e0861609ffd354a6d0cb58d1247d3214833f7
0xngmi <0xngmi@protonmail.com>
2022-01-21 21:16:34 +0100

    add 0xdao

---
+++ projects/0xDAO/index.js
@@ -0,0 +1,4 @@
+const {masterChefExports, standardPoolInfoAbi} = require('../helper/masterchef')
+
+module.exports=masterChefExports("0xa7821c3e9fc1bf961e280510c471031120716c3d", "fantom", "0xc165d941481e68696f43ee6e99bfb2b23e0e3114", false,
+standardPoolInfoAbi)
