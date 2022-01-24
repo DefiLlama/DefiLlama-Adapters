b5d6465d1dfead553ef281a9af4cac8a6f8ef384
Mohammed18062020 <mohamed_el_hamdaoui93@hotmail.com>
2022-01-24 20:03:37 +0000

    Updating Geist Finance

--- projects/geist/index.js
+++ projects/geist/index.js
@@ -1,9 +1,13 @@
 const { staking } = require("../helper/staking");
+const { pool2 } = require("../helper/pool2");
 const { aaveChainTvl } = require("../helper/aave");
 const { transformFantomAddress } = require("../helper/portedTokens");
 
 const stakingContract = "0x49c93a95dbcc9A6A4D8f77E59c038ce5020e82f8";
 const GEIST = "0xd8321aa83fb0a4ecd6348d4577431310a6e0814d";
+
+const stakingContractPool2 = "0xE40b7FA6F5F7FB0Dc7d56f433814227AAaE020B5";
+const GEIST_WFTM_spLP = "0x668AE94D0870230AC007a01B471D02b2c94DDcB9";
 
 function lending(borrowed) {
   return async (timestamp, ethBlock, chainBlocks) => {
@@ -26,5 +30,6 @@ module.exports = {
     tvl: lending(false),
     borrowed: lending(true),
     staking: staking(stakingContract, GEIST, "fantom"),
+    pool2: pool2(stakingContractPool2, GEIST_WFTM_spLP, "fantom"),
   },
 };
