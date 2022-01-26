fdf02565c7da3bde6fe6dbabdb78bd6d353ef647
nahom2357 <debebenahom2357@gmail.com>
2022-01-26 11:05:19 +0300

    fix: better price computation

--- projects/cryptomate.js
+++ projects/cryptomate.js
@@ -36,20 +36,23 @@ async function tvl() {
            ft1Name,
            ft1Decimals,
            tvlFt2,
+           ft2Name,
            ft2Decimals] = tokensMetadata[pool.x_token.split(".")[1]].name in tokensPrice
       ? [pool.tvl_x_token,
         tokensMetadata[pool.x_token.split(".")[1]].name,
         Math.pow(10, tokensMetadata[pool.x_token.split(".")[1]].decimals),
         pool.tvl_y_token,
-        tokensMetadata[pool.y_token.split(".")[1]].decimals]
+        tokensMetadata[pool.y_token.split(".")[1]].name,
+        Math.pow(10, tokensMetadata[pool.y_token.split(".")[1]].decimals)]
       : [pool.tvl_y_token,
         tokensMetadata[pool.y_token.split(".")[1]].name,
         Math.pow(10, tokensMetadata[pool.y_token.split(".")[1]].decimals),
         pool.tvl_x_token,
-        tokensMetadata[pool.x_token.split(".")[1]].decimals];
+        tokensMetadata[pool.x_token.split(".")[1]].name,
+        Math.pow(10, tokensMetadata[pool.x_token.split(".")[1]].decimals)];
     const token_type = ft1Name === "wrapped STX"? "blockstack": "usd-coin";
-    balances[token_type] += ft1Name === "wrapped STX"? tvlFt1/ft1Decimals: tokensPrice[ft1Name].price * tvlFt1/ft1Decimals;
-    balances["usd-coin"] += (tvlFt1 * tokensPrice[ft1Name].price / ft1Decimals) / (tvlFt2 / ft2Decimals);
+    balances[token_type] += ft1Name === "wrapped STX"? tvlFt1/ ft1Decimals: tokensPrice[ft1Name].price * tvlFt1/ft1Decimals;
+    balances[token_type] += ft2Name === "wrapped STX"? tvlFt2/ ft2Decimals: tokensPrice[ft2Name].price * tvlFt2/ft2Decimals;
   })
   return balances;
 }
