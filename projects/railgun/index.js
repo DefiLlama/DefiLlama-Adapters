9346291f29c8e2b8d3fe8a1e533527eb271f6a84
liucougar <liucougar@gmail.com>
2022-01-25 21:41:24 -0800

    separate railgun staking out from TVL

--- projects/railgun/index.js
+++ projects/railgun/index.js
@@ -4,22 +4,27 @@ const abi = require('./ABI.json');
 const STAKING_RAIL = '0xee6a649aa3766bd117e12c161726b693a1b2ee20';
 const RAIL = '0xe76C6c83af64e4C60245D8C7dE953DF673a7A33D';
 const TREASURY = '0xc851fbe0f07a326ce0326ccc70c2a62732e74d6c';
-async function treasury(block, balances) {
+
+async function tvl(timestamp, block) {
+    const balances = {};
     const tokens = await sdk.api.util.tokenList();
     const result = await sdk.api.abi.multiCall({
         calls: tokens.map(t => ({target: t.contract, params: TREASURY})),
         abi: 'erc20:balanceOf',
         block: block
     });
+
     for(const r of result.output) {
         if(r.success && r.output && r.output !== '0') {
             balances[r.input.target] = r.output;
         }
     }
+
+    return balances;
 }
 
-async function tvl(timestamp, block) {
-    let balances = {};
+async function treasury(timestamp, block) {
+    const balances = {};
 
     try {
         const interval = await sdk.api.abi.call({
@@ -41,15 +46,14 @@ async function tvl(timestamp, block) {
         balances[RAIL] = 0;
     }
 
-	await treasury(block, balances);
-
     return balances;
 }
 
 module.exports = {
     website: 'https://railgun.ch',
     ethereum: {
-        tvl,
+	staking: treasury,
+        tvl: tvl,
     }
 }
 
