b7dcdcbcc3213257b9d0813835d15f308010d523
sameretto <samer603m@yahoo.com>
2022-02-01 14:13:17 -0500

    removed unsured addresses

--- projects/hector/index.js
+++ projects/hector/index.js
@@ -1,9 +1,6 @@
 const { GraphQLClient, gql } = require("graphql-request");
 const retry = require("async-retry");
 
-const bank = "0xBB97c2875e640f75297da0147914C8f10d7c5174";
-const lpPool = "0x24699312CB27C26Cfc669459D670559E5E44EE60";
-
 async function fetch() {
   var endpoint =
     "https://api.thegraph.com/subgraphs/name/hectordao-hec/hector-dao";
