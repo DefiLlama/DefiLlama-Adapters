b076d94b98842c9cd1cb710032d058ebcb801900
Nayanjyoti Das <nd@tranche.financem>
2022-02-02 10:23:29 +0530

    fetch tvl for all network

--- projects/tranche/index.js
+++ projects/tranche/index.js
@@ -2,6 +2,29 @@ const sdk = require('@defillama/sdk');
 const axios = require('axios');
 const _ = require('underscore');
 
+
+
+async function getTvl (network) {
+  const resp = await axios.get(`https://dev.tranche.finance/api/v1/common/tvl/${ network }`);
+  return resp.data.result;
+}
+
+async function ethTvl () {
+  return getTvl('ethereum');
+}
+async function polygonTvl () {
+  return getTvl('polygon');
+}
+
+async function fantomTvl () {
+  return getTvl('ftm');
+}
+
+async function avaxTvl () {
+  return getTvl('avax');
+}
+
+
 async function tvl(timestamp, block) {
   const balances = {};
 
@@ -26,12 +49,30 @@ async function tvl(timestamp, block) {
     calls,
     abi: 'erc20:balanceOf'
   });
-  sdk.util.sumMultiBalanceOf(balances, balanceOfResults)
+  sdk.util.sumMultiBalanceOf(balances, balanceOfResults);
+  console.log(balances);
 
   return balances;
 }
 
+
+async function fetch () {
+  return await ethTvl() + await polygonTvl() + await fantomTvl() + await avaxTvl();
+}
+
 module.exports = {
   start: 1621340071,
-  tvl,
+  ethereum: {
+    fetch: ethTvl
+  },
+  polygon: {
+    fetch: polygonTvl
+  },
+  avax: {
+    fetch: avaxTvl
+  },
+  fantom: {
+    fetch: fantomTvl
+  },
+  fetch
 }
