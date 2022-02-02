7a56933e644216b29472810daa7c41d1932de726
waynebruce0x <waynebruce0x@gmail.com>
2022-02-02 14:24:00 +0000

    agora tidy

--- projects/agora.js
+++ projects/agora.js
@@ -2,21 +2,22 @@ const sdk = require("@defillama/sdk");
 const { compoundExports } = require("./helper/compound");
 const { transformMetisAddress } = require("./helper/portedTokens");
 
-const {tvl: agoraTvl, borrowed: agoraBorrowed } = compoundExports(
+const { tvl: agoraTvl, borrowed: agoraBorrowed } = compoundExports(
   "0x3fe29D7412aCDade27e21f55a65a7ddcCE23d9B3",
   "metis",
   "0xcFd482DcE13cA1d27834D381AF1b570E9E6C6810",
   "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
   transformMetisAddress()
-)
-// node test.js projects/agora.js
-const {tvl: agoraPlusTvl, borrowed: agoraPlusBorrowed } = compoundExports(
+);
+
+const { tvl: agoraPlusTvl, borrowed: agoraPlusBorrowed } = compoundExports(
   "0x92DcecEaF4c0fDA373899FEea00032E8E8Da58Da",
   "metis",
   "0xE85A1ae1A2A21135c49ADEd398D3FD5Ed032B28e",
   "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
   transformMetisAddress()
-)
+);
+
 module.exports = {
   timeTravel: true,
   incentivized: true,
