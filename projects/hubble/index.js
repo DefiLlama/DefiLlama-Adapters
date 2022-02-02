1ecadaef4f6d06ca3f110321a4d35e3c1f2da38e
peroxy <peroxy@users.noreply.github.com>
2022-02-02 22:09:04 +0100

    Add staking

--- projects/hubble/index.js
+++ projects/hubble/index.js
@@ -5,11 +5,21 @@ const client = axios.create({
   baseURL: HUBBLE_API,
 });
 
-async function fetch() {
+async function tvl() {
   const metrics = await client.get("/metrics");
   return metrics.data.totalValueLocked;
 }
 
+async function staking() {
+  const metrics = await client.get("/metrics");
+  return metrics.data.hbb.staked * metrics.data.hbb.price;
+}
+
 module.exports = {
-  fetch,
+  methodology: `To obtain the Hubble Protocol TVL we use the formula 'TVL = Total HBB Staked * Current HBB Price + Total Collateral Value + Total Stablecoin (USDH) in Stability Pool'.`,
+  solana: {
+    fetch: tvl,
+  },
+  fetch: tvl,
+  staking: {fetch: staking}
 };
