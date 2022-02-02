245fbfa76202c64e7f8e5c9b78cebc9ba5487b57
Nayanjyoti Das <nd@tranche.financem>
2022-02-02 10:37:32 +0530

    production api url added

--- projects/tranche/index.js
+++ projects/tranche/index.js
@@ -5,7 +5,7 @@ const _ = require('underscore');
 
 
 async function getTvl (network) {
-  const resp = await axios.get(`https://dev.tranche.finance/api/v1/common/tvl/${ network }`);
+  const resp = await axios.get(`https://api.tranche.finance/api/v1/common/tvl/${ network }`);
   return resp.data.result;
 }
 
@@ -23,38 +23,6 @@ async function fantomTvl () {
 async function avaxTvl () {
   return getTvl('avax');
 }
-
-
-async function tvl(timestamp, block) {
-  const balances = {};
-
-  // returns undefined in test script
-  let tokenList = await axios.get('https://api.tranche.finance/api/v1/common/token-address?network=ethereum');
-  let holderList = await axios.get('https://api.tranche.finance/api/v1/common/holder-address?network=ethereum');
-  tokenList = tokenList.data.result;
-  holderList = holderList.data.result;
-
-  const calls = [];
-  _.each(tokenList, (token) => {
-    _.each(holderList, (contract) => {
-      calls.push({
-        target: token,
-        params: contract
-      })
-    })
-  });
-
-  const balanceOfResults = await sdk.api.abi.multiCall({
-    block,
-    calls,
-    abi: 'erc20:balanceOf'
-  });
-  sdk.util.sumMultiBalanceOf(balances, balanceOfResults);
-  console.log(balances);
-
-  return balances;
-}
-
 
 async function fetch () {
   return await ethTvl() + await polygonTvl() + await fantomTvl() + await avaxTvl();
