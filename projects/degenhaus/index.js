be42bf25025f3c3953ebe456c508ef6d55d81ff9
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2022-02-03 10:26:51 +0000

    fix factory addr and staking function

--- projects/degenhaus/index.js
+++ projects/degenhaus/index.js
@@ -1,8 +1,8 @@
 const {calculateUniTvl} = require('../helper/calculateUniTvl.js')
 const {transformFantomAddress} = require('../helper/portedTokens.js')
-const {staking} = require('../helper/staking')
+const {staking, stakingPricedLP} = require('../helper/staking')
 
-const factory = '0xA01C3d7607381c79e10334408aE59684Aa36B1131'
+const factory = '0xA01C3d760738c79e10334408aE59684Aa36B1131'
 async function tvl(_timestamp, _ethBlock, chainBlocks){
   const transform = await transformFantomAddress();
 
@@ -13,6 +13,6 @@ async function tvl(_timestamp, _ethBlock, chainBlocks){
 module.exports = {
   fantom:{
     tvl,
-    staking: staking("0x72A7A3770B4BC999026F3663F1534581E0c59f2a", "0xd948efcc99be419ca9bdace89b2bec31edf13adb", 'fantom')
+    staking: stakingPricedLP("0x72A7A3770B4BC999026F3663F1534581E0c59f2a", "0xd948efcc99be419ca9bdace89b2bec31edf13adb", 'fantom', "0x1758d21f2915583f49cc2b3e583df3e55f0dd2c0", "fantom")
   },
 }
