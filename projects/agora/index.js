76af4f865c719c90657c6688ef6509f3535782d7
waynebruce0x <waynebruce0x@gmail.com>
2022-02-03 22:35:53 +0000

    agora puff LPs

--- projects/agora/index.js
+++ projects/agora/index.js
@@ -1,13 +1,13 @@
 const sdk = require("@defillama/sdk");
-const { compoundExports } = require("./helper/compound");
-const { transformMetisAddress } = require("./helper/portedTokens");
+const { compoundExports } = require("../helper/compound");
+const { transformMetisAddress } = require("../helper/portedTokens");
 
 const { tvl: agoraTvl, borrowed: agoraBorrowed } = compoundExports(
   "0x3fe29D7412aCDade27e21f55a65a7ddcCE23d9B3",
   "metis",
   "0xcFd482DcE13cA1d27834D381AF1b570E9E6C6810",
   "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
-  transformMetisAddress()
+  transformMetisAddress(),
 );
 
 const { tvl: agoraPlusTvl, borrowed: agoraPlusBorrowed } = compoundExports(
@@ -15,7 +15,8 @@ const { tvl: agoraPlusTvl, borrowed: agoraPlusBorrowed } = compoundExports(
   "metis",
   "0xE85A1ae1A2A21135c49ADEd398D3FD5Ed032B28e",
   "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
-  transformMetisAddress()
+  transformMetisAddress(),
+  symbol => symbol.indexOf('appuffNetswap') > -1
 );
 
 module.exports = {
