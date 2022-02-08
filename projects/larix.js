8bf1aa83b70384fec04bf296dd176f7458b361e4
nemusona <nemusonaneko@protonmail.com>
2022-02-08 11:26:28 -0600

    larix fetch

--- projects/larix.js
+++ projects/larix.js
@@ -1,27 +1,13 @@
-const {sumTokens} = require("./helper/solana")
+const { fetchURL } = require("./helper/utils");
 
-const getTokenBalance = (a,b)=>[a,b]
+const api = "https://api.projectlarix.com/market";
 
-const contract = "BxnUi6jyYbtEEgkBq4bPLKzDpSfWVAzgyf3TF2jfC1my"
-
-async function tvl() {  
-  return sumTokens([
-    //btcAmount
-    getTokenBalance("9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E","BxnUi6jyYbtEEgkBq4bPLKzDpSfWVAzgyf3TF2jfC1my"),
-    //ethAmount
-    getTokenBalance("2FPyTwcZLUg1MDrwsyoP4D6s1tM7hAkHYRjkNb5w6Pxk","BxnUi6jyYbtEEgkBq4bPLKzDpSfWVAzgyf3TF2jfC1my"),
-    //solAmount
-    getTokenBalance("So11111111111111111111111111111111111111112","BxnUi6jyYbtEEgkBq4bPLKzDpSfWVAzgyf3TF2jfC1my"),
-    //usdtAmount
-    getTokenBalance("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB","BxnUi6jyYbtEEgkBq4bPLKzDpSfWVAzgyf3TF2jfC1my"),
-    getTokenBalance("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v","BxnUi6jyYbtEEgkBq4bPLKzDpSfWVAzgyf3TF2jfC1my"), //usdc
-    getTokenBalance("AGFEad2et2ZJif9jaGpdMixQqvW5i81aBdvKe7PHNfz3","BxnUi6jyYbtEEgkBq4bPLKzDpSfWVAzgyf3TF2jfC1my"), // ftt
-    getTokenBalance("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt", contract), // serum
-    ["mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So", contract, 'solana'],
-  ])
+async function fetch() {
+  const data = (await fetchURL(api)).data.total_available_value;
+  console.log(data)
+  return data;
 }
 
 module.exports = {
-  tvl,
-  methodology: 'To obtain the TVL of Larix we make on-chain calls using the function getTokenBalance() that uses the address of the token and the address of the contract where the tokens are held. These pools addresses are hard-coded. Making these calls returns the amount of tokens held in the smart contract. We then use Coingecko to get the price of each token in USD and export the sum of all tokens.',
+  fetch
 }
