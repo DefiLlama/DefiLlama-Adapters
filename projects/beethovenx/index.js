6c7192e10ef3640bee074f132e47674db9abb38c
0xSkly <skly.beets@protonmail.com>
2022-02-08 07:36:25 +0100

    switch to more reliable source of tvl

--- projects/beethovenx/index.js
+++ projects/beethovenx/index.js
@@ -1,7 +1,29 @@
-const {getBalancerSubgraphTvl} = require('../helper/balancer')
+const { request, gql } = require("graphql-request");
+const {toUSDTBalances} = require("../helper/balances");
 
-module.exports={
-    fantom:{
-        tvl: getBalancerSubgraphTvl('https://graph-node.beets-ftm-node.com/subgraphs/name/beethovenx', 'fantom')
+const graphUrl =
+    'https://backend.beets-ftm-node.com/graphql';
+
+const graphQuery = gql`
+    query get_tvl {
+            data: beetsGetProtocolData {
+                totalLiquidity
+            }
     }
+`;
+
+
+async function tvl() {
+    const { data } = await request(
+        graphUrl,
+        graphQuery,
+    );
+    return toUSDTBalances(data.totalLiquidity)
 }
+
+module.exports = {
+    fantom:{
+        tvl,
+    },
+    tvl,
+};
