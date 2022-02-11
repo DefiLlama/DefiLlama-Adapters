6b255caa7313684b86df1a0257067243bc1a6582
waynebruce0x <waynebruce0x@gmail.com>
2022-02-11 15:58:51 +0000

    fastyield pool2 and staking

--- projects/fastyield/index.js
+++ projects/fastyield/index.js
@@ -1,8 +1,9 @@
 const sdk = require("@defillama/sdk");
 const abi = require("./abi.json");
+const { stakings, staking } = require("../helper/staking");
 const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
 const { transformFantomAddress } = require("../helper/portedTokens");
-
+// node test.js projects/fastyield/index.js
 const NATIVE_CONTRACT = "0xe5AFC91CEA5df74748A2b07e1d48E4e01aacF52B";
 
 const fantomTvl = async (timestamp, block, chainBlocks) => {
@@ -19,7 +20,7 @@ const fantomTvl = async (timestamp, block, chainBlocks) => {
 
   const lpPositions = [];
 
-  for (let index = 0; index < poolLenth; index++) {
+  for (let index = 2; index < poolLenth; index++) {
     const poolInfo = (
       await sdk.api.abi.call({
         abi: abi.poolInfo,
@@ -75,9 +76,36 @@ const fantomTvl = async (timestamp, block, chainBlocks) => {
   return balances;
 };
 
+async function pool2(timestamp, block, chainBlocks) {
+  const balances = {};
+  const transform = await transformFantomAddress();
+
+  const lpBalance = (
+    await sdk.api.abi.call({
+      abi: 'erc20:balanceOf',
+      target: '0xe836997c9f3665986580fe98f79999fa876fc271',
+      params: '0xc0c84eFeB8290BA5Ac6bed682Cfdf2896cf26566',
+      chain: "fantom",
+      block: chainBlocks["fantom"],
+    })
+  ).output;
+
+  await unwrapUniswapLPs(
+    balances,
+    [{balance: lpBalance, token: '0xe836997c9f3665986580fe98f79999fa876fc271' }],
+    chainBlocks["fantom"],
+    "fantom",
+    transform
+  );
+
+  balances['0x4e15361fd6b4bb609fa63c81a2be19d873717870'] *= 2;
+  delete balances['fantom:0x0299461ee055bbb6de11fafe5a0636a0c3bd5e8d'];
+  return balances;
+};
 module.exports = {
   fantom: {
     tvl: fantomTvl,
+    staking: stakings(['0x1Fb33cB822bD554890242F4765505FA6340B1Fb9'], '0x0299461ee055bbb6de11fafe5a0636a0c3bd5e8d', 'fantom'),
+    pool2
   },
-  tvl: sdk.util.sumChainTvls([fantomTvl]),
 };
