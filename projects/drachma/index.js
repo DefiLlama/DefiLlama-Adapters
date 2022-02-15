69e08cf2d3501f8b817dd92a53fa84d9918143b6
agoraagora <team@agoradefi.io>
2022-02-15 09:29:53 +0530

    added changes for drachma farm

--- projects/drachma/index.js
+++ projects/drachma/index.js
@@ -2,6 +2,16 @@ const sdk = require("@defillama/sdk");
 const { transformMetisAddress } = require("../helper/portedTokens");
 const contracts = require('./contracts.json');
 
+const { compoundExports } = require("../helper/compound");
+const comptroller = "0xB6ef08Ffbbb0691a3D9E6c41db4b1d2F97D8D49a";
+
+//tvl for drachma farm
+const { tvl: drachmaTvl, borrowed: drachmaBorrowed } = compoundExports(
+  comptroller,
+  "metis"
+);
+
+//tvl for drachma app
 function tvl(chain) {
   return async (timestamp, block, chainBlocks) => {
     const transform = await transformMetisAddress()
@@ -39,7 +49,11 @@ function tvl(chain) {
 };
 
 module.exports = {
+  timeTravel: true,
+  incentivized: true,
+  misrepresentedTokens: true,
   metis: {
-    tvl: tvl("metis"),
+    tvl: sdk.util.sumChainTvls([drachmaTvl, tvl("metis")]),
+    borrowed: drachmaBorrowed,
   },
 };
