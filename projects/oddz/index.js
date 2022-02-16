82ce1b3aa4558477ab86acf4dc723485777435a8
manti <manti.rvce@gmail.com>
2022-02-16 10:32:52 +0530

    oddz avax support and related data

--- projects/oddz/index.js
+++ projects/oddz/index.js
@@ -46,15 +46,44 @@ const bscPool2 = [
     "bsc:0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
   ], // ODDZ-BNB
 ];
+
+
+const avaxPools = [
+  // pool, token, representation
+  [
+    "0x6a165bA195D9d331b2A1C9648328d409aA599465",
+    "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664",
+    "avax:0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664",
+  ], // oUSD
+];
+
+const avaxStakingPool = [
+  [
+    "0xd0A145aF8F200Fc8e4d118c6e4d4a77eE1ba8E2e",
+    "0xB0a6e056B587D0a85640b39b1cB44086F7a26A1E",
+    "avax:0xB0a6e056B587D0a85640b39b1cB44086F7a26A1E",
+  ], // sODDZ
+];
+
+
+
 async function bsc(_timestamp, block, chainBlocks) {
   return getTotalCollateral(bscPools, "bsc", chainBlocks["bsc"]);
 }
 async function pool2(timestamp, block, chainBlocks) {
   return getTotalCollateral(bscPool2, "bsc", chainBlocks["bsc"]);
 }
-async function staking(timestamp, block, chainBlocks) {
+async function bscStaking(timestamp, block, chainBlocks) {
   return getTotalCollateral(bscStakingPool, "bsc", chainBlocks["bsc"]);
 }
+
+async function avax(_timestamp, block, chainBlocks) {
+  return getTotalCollateral(avaxPools, "avax", chainBlocks["avax"]);
+}
+async function avaxStaking(timestamp, block, chainBlocks) {
+  return getTotalCollateral(avaxStakingPool, "avax", chainBlocks["avax"]);
+}
+
 function mergeBalances(balances, balancesToMerge) {
   Object.entries(balancesToMerge).forEach((balance) => {
     sdk.util.sumSingleBalance(balances, balance[0], balance[1]);
@@ -62,7 +91,7 @@ function mergeBalances(balances, balancesToMerge) {
 }
 async function tvl(timestamp, block, chainBlocks) {
   const balances = {};
-  await Promise.all([bsc(timestamp, block, chainBlocks)]).then((poolBalances) =>
+  await Promise.all([bsc(timestamp, block, chainBlocks), avax(timestamp, block, chainBlocks)]).then((poolBalances) =>
     poolBalances.forEach((pool) => mergeBalances(balances, pool))
   );
   return balances;
@@ -72,7 +101,11 @@ module.exports = {
   bsc: {
     tvl: bsc,
     pool2,
-    staking,
+    bscStaking,
+  },
+  avax: {
+    tvl: avax,
+    avaxStaking,
   },
   tvl,
 };
