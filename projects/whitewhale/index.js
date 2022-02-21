9b0f5e792375f76c03c39530d770f3acee2e591c
0xFable <0xfable@protonmail.com>
2022-02-21 18:24:36 +0000

    feat(tvl): Update query for TVL

--- projects/whitewhale/index.js
+++ projects/whitewhale/index.js
@@ -1,16 +1,17 @@
 const { fetchURL } = require('../helper/utils')
 
 const vaults = [
-    ["https://lcd.terra.dev/wasm/contracts/terra1w0p5zre38ecdy3ez8efd5h9fvgum5s206xknrg/store?query_msg=%7B%22borrower%22:%7B%22address%22:%22terra1ec3r2esp9cqekqqvn0wd6nwrjslnwxm7fh8egy%22%7D%7D", "ust"]
+    ["https://lcd.terra.dev/wasm/contracts/terra1ec3r2esp9cqekqqvn0wd6nwrjslnwxm7fh8egy/store?query_msg=%7B%22pool_state%22:%7B%7D%7D"]
 ]
 
 async function tvl() {
-    const balances = {}
+    const pool_state = {}
+    const tvl = {}
     await Promise.all(vaults.map(async vault => {
-        const deposited = await fetchURL(vault[0])
-        balances[vault[1]] = Number(deposited.data.result.balance) / 1e6
+        pool_state = await fetchURL(vault)
+        tvl = Number(pool_state.total_share_in_ust) / 1e6
     }))
-    return balances
+    return tvl
 }
 
 async function staking() {
@@ -27,7 +28,7 @@ module.exports = {
     timetravel: false,
     terra: {
         // Token not on coingecko yet
-        //staking,
+        // staking,
         tvl
     }
 }
