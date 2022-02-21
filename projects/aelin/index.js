ba593ffed1fe441557c761ed46cd22857f49ff0c
Jonathan Chemla <jchemla@iconem.com>
2022-02-21 10:27:24 +0100

    Fix typo and sum pool token instead of rewards
    Fix typo and sum pool token instead of rewards

--- projects/aelin/index.js
+++ projects/aelin/index.js
@@ -25,7 +25,7 @@ const aelin_data = {
 }
 // https://thegraph.com/hosted-service/subgraph/aelin-xyz/optimism
 const graphQuery = gql`
-query GET_SUMMONED_GOTCHIS ($minGotchiId: Int, $block: Int) {
+query GET_AELIN_POOLS ($minGotchiId: Int, $block: Int) {
   poolCreateds(
     first: 1000
     skip: 0
@@ -39,95 +39,92 @@ query GET_SUMMONED_GOTCHIS ($minGotchiId: Int, $block: Int) {
   }
 }`
 
-tvl = chain => async (timestamp, ethBlock, chainBlocks) => {
-  const block = await getBlock(timestamp, chain, chainBlocks, false) - 100; // graph out of sync
-  
-  const { poolCreateds, totalPoolsCreateds } = await request(
-    aelin_data[chain]['graphUrl'],
-    graphQuery, 
-    {block}
-  );
+function tvl(chain) {
+  return async (timestamp, ethBlock, chainBlocks) => {
+    const block = await getBlock(timestamp, chain, chainBlocks, false) - 100; // graph out of sync
+    
+    const { poolCreateds, totalPoolsCreateds } = await request(
+      aelin_data[chain]['graphUrl'],
+      graphQuery, 
+      {block}
+    );
 
-  const n_pools = totalPoolsCreateds[0]['count']
-  const poolsAddressesCalls = poolCreateds.map(p => ({ target: p.id }))
-  console.log(`${chain} - ${block}: n_pools: ${poolsAddressesCalls.length} / ${n_pools}`, poolsAddressesCalls)
-
-  const [{output: purchaseToken}, {output: rewardsAddress}] = await Promise.all([
-    sdk.api.abi.multiCall({
+    const n_pools = totalPoolsCreateds[0]['count']
+    const poolsAddressesCalls = poolCreateds.map(p => ({ target: p.id }))
+    console.log(`${chain} - ${block}: n_pools: ${poolsAddressesCalls.length} / ${n_pools}`) // , poolsAddressesCalls)
+    
+    const {output: purchaseTokens} = await sdk.api.abi.multiCall({
       calls: poolsAddressesCalls,
       abi: abi["aelinPool_purchaseToken"],
       chain,
       block,
-    }),
-    sdk.api.abi.multiCall({
-      calls: poolsAddressesCalls,
-      abi: abi["aelinPool_aelinRewardsAddress"],
-      chain,
+    })
+    const balancesCall = purchaseTokens.map((purchaseToken, i) => 
+      ({ target: purchaseToken.output, params: [poolCreateds[i].id] }))
+    const tokenBalances = await sdk.api.abi.multiCall({
+      calls: balancesCall,
+        abi: "erc20:balanceOf",
+        chain,
       block,
     })
-  ]);
-  const balancesCall = rewardsAddress.map((rewardsContract, i) => 
-    ({ target: purchaseToken[i].output, params: [rewardsContract.output] }))
-  console.log(balancesCall)
-  const rewardsBalances = await sdk.api.abi.multiCall({
-    calls: balancesCall,
-      abi: "erc20:balanceOf",
-      chain,
-    block,
-  })
-  
-  const balances = {};
-  sdk.util.sumMultiBalanceOf(balances, rewardsBalances, true, x => `${chain}:${x}`);
-  return balances
-}
 
-stakingTVL = chain => async (timestamp, ethBlock, chainBlocks) => {
-  if (chain === 'ethereum') {
-    return {}
-  } 
-
-  const staked = await staking(
-    aelin_data[chain]['AELIN_staking'], // stakingContract, 
-    aelin_data[chain]['AELIN'],         // stakingToken, 
-    chain
-  ) (timestamp, ethBlock, chainBlocks)
-  return staked
-}
-
-pool2TVL = chain => async (timestamp, ethBlock, chainBlocks) => {
-  const stakingContract = aelin_data[chain]['AELIN_ETH_staking']
-  const lpToken = aelin_data[chain]['AELIN_ETH_LP']
-  const block = await getBlock(timestamp, chain, chainBlocks, false) - 100; // graph out of sync
-
-  if (chain === 'ethereum') {
-    const staked = await pool2(stakingContract, lpToken, chain) (timestamp, ethBlock, chainBlocks)
-    const aelin_addr = `ethereum:${aelin_data[chain]['AELIN']}`
-    staked['AELIN'] = BigNumber(staked[aelin_addr]).div(1e18).toFixed(0)
-    staked[aelin_addr] = 0
-    return staked
-  } 
-  else if (chain === 'optimism') {
-    const balances = {}
-    const transformAddress = (addr)=>`${chain}:${addr}`;
-    const {output: heldLPshares} = await sdk.api.abi.call({
-        abi: 'erc20:balanceOf',
-        target: lpToken,
-        params: stakingContract,
-        chain,
-        block,
-    })
-    const lpBalances = [
-      {
-        balance: heldLPshares, 
-        token: lpToken
-      }
-    ]
-    // Unwrao Gelato pools
-    await unwrapUniswapLPs(balances, lpBalances, block, chain, transformAddress, [], false, 'gelato')
+    // console.log(rewardsBalances.output.map(b => b.input.params + ' aelinRewardsAddress pool has: ' + BigNumber(b.output).div(1e18).toFixed(0) + ' of purchaseToken ' + b.input.target))
+    const balances = {};
+    sdk.util.sumMultiBalanceOf(balances, tokenBalances, true, x => `${chain}:${x}`);
     return balances
   }
 }
 
+function stakingTVL(chain) {
+  return async (timestamp, ethBlock, chainBlocks) => {
+    if (chain === 'ethereum') {
+      return {}
+    } 
+
+    const staked = await staking(
+      aelin_data[chain]['AELIN_staking'], 
+      aelin_data[chain]['AELIN'], 
+      chain
+    ) (timestamp, ethBlock, chainBlocks)
+    return staked
+  }
+}
+
+function pool2TVL(chain) {
+  return async (timestamp, ethBlock, chainBlocks) => {
+    const stakingContract = aelin_data[chain]['AELIN_ETH_staking']
+    const lpToken = aelin_data[chain]['AELIN_ETH_LP']
+    const block = await getBlock(timestamp, chain, chainBlocks, false) - 100; // graph out of sync
+
+    if (chain === 'ethereum') {
+      const staked = await pool2(stakingContract, lpToken, chain) (timestamp, ethBlock, chainBlocks)
+      const aelin_addr = `ethereum:${aelin_data[chain]['AELIN']}`
+      staked['AELIN'] = BigNumber(staked[aelin_addr]).div(1e18).toFixed(0)
+      staked[aelin_addr] = 0
+      return staked
+    } 
+    else if (chain === 'optimism') {
+      const balances = {}
+      const transformAddress = (addr) => `${chain}:${addr}`;
+      const {output: heldLPshares} = await sdk.api.abi.call({
+          abi: 'erc20:balanceOf',
+          target: lpToken,
+          params: stakingContract,
+          chain,
+          block,
+      })
+      const lpBalances = [
+        {
+          balance: heldLPshares, 
+          token: lpToken
+        }
+      ]
+      // Unwrao Gelato pools
+      await unwrapUniswapLPs(balances, lpBalances, block, chain, transformAddress, [], false, 'gelato')
+      return balances
+    }
+  }
+}
 
 module.exports = {
   ethereum: {
@@ -139,5 +136,5 @@ module.exports = {
     staking: stakingTVL('optimism'), 
     pool2: pool2TVL('optimism'), 
   },
-  methodology: 'Aelin TVL consists of tokens held by pools rewardContracts',
+  methodology: 'Aelin TVL consists of purchaseTokens held by pools, as well as AELIN token (staking) and LP (pool2) staked to receive a share of the revenue',
 }
