8b02e1b51dd51769fdb050b6e45edf8e329b140f
sameretto <samer603m@yahoo.com>
2022-02-28 09:19:53 -0500

    added borrowed method

--- projects/hector/index.js
+++ projects/hector/index.js
@@ -1,19 +1,5 @@
 const { GraphQLClient, gql } = require("graphql-request");
 const retry = require("async-retry");
-const { getBlock } = require("../helper/getBlock");
-const sdk = require("@defillama/sdk");
-const erc20 = require("../helper/abis/erc20.json");
-
-const hectorStakingv1 = "0x9ae7972BA46933B3B20aaE7Acbf6C311847aCA40";
-const hectorStakingv2 = "0xD12930C8deeDafD788F437879cbA1Ad1E3908Cc5";
-const hec = "0x5C4FDfc5233f935f20D2aDbA572F770c2E377Ab0";
-
-const HectorStakings = [
-  // V1
-  hectorStakingv1,
-  // V2
-  hectorStakingv2,
-];
 
 async function treasury() {
   var endpoint =
@@ -47,9 +33,28 @@ async function treasuryInvestments() {
   const results = await retry(
     async (bail) => await graphQLClient.request(query)
   );
+
   return +results.protocolMetrics[0].treasuryInvestments;
 }
-async function bank() {
+async function bankSupplied() {
+  var endpoint =
+    "https://api.thegraph.com/subgraphs/name/hectordao-hec/hector-dao";
+  var graphQLClient = new GraphQLClient(endpoint);
+
+  var query = gql`
+    query {
+      protocolMetrics(first: 1, orderBy: timestamp, orderDirection: desc) {
+        bankSupplied
+      }
+    }
+  `;
+  const results = await retry(
+    async (bail) => await graphQLClient.request(query)
+  );
+
+  return +results.protocolMetrics[0].bankSupplied;
+}
+async function borrowed() {
   var endpoint =
     "https://api.thegraph.com/subgraphs/name/hectordao-hec/hector-dao";
   var graphQLClient = new GraphQLClient(endpoint);
@@ -58,17 +63,14 @@ async function bank() {
     query {
       protocolMetrics(first: 1, orderBy: timestamp, orderDirection: desc) {
         bankBorrowed
-        bankSupplied
       }
     }
   `;
   const results = await retry(
     async (bail) => await graphQLClient.request(query)
   );
-  const bankTotal =
-    +results.protocolMetrics[0].bankBorrowed +
-    +results.protocolMetrics[0].bankSupplied;
-  return bankTotal;
+
+  return +results.protocolMetrics[0].bankBorrowed;
 }
 async function torCurveLP() {
   var endpoint =
@@ -127,16 +129,20 @@ const staking = async () => {
 
 async function fetch() {
   const total =
-    (await treasury()) + (await eth()) + (await torCurveLP()) + (await bank());
+    (await treasury()) +
+    (await eth()) +
+    (await torCurveLP()) +
+    (await bankSupplied());
   return total;
 }
 
 module.exports = {
   fantom: {
     fetch: staking,
+    fetch: borrowed,
   },
   bank: {
-    fetch: bank,
+    fetch: bankSupplied,
   },
   torCurveLP: {
     fetch: torCurveLP,
