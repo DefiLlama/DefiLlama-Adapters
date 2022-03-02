2f4780169de1624d37ace3271ebe02a59b411206
kenchangh <guanhao3797@gmail.com>
2022-03-02 14:20:03 +0800

    Prettier, add solana to total tvl

--- projects/ribbon/index.js
+++ projects/ribbon/index.js
@@ -1,9 +1,16 @@
-const evm = require('./evm')
-const solana = require('./solana')
+const sdk = require("@defillama/sdk");
+const evm = require("./evm");
+const solana = require("./solana");
 
 module.exports = {
-    doublecounted: true,
-    ...evm,
-    ...solana,
-    methodology: "Sums the totalBalance of all Ribbon Theta Vaults"
-}
+  doublecounted: true,
+  ...evm,
+  ...solana,
+  tvl: sdk.util.sumChainTvls([
+    evm.ethereum.tvl,
+    evm.avalanche.tvl,
+    evm.aurora.tvl,
+    solana.solana.tvl,
+  ]),
+  methodology: "Sums the totalBalance of all Ribbon Theta Vaults",
+};
