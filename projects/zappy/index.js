95875484a8637d51132bb92afbe83d6f655021ed
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2022-03-02 07:05:25 +0000

    update chain export

--- projects/zappy/index.js
+++ projects/zappy/index.js
@@ -4,7 +4,7 @@ module.exports = {
   misrepresentedTokens: true,
   methodology:
     "Factory address (0x4be5Bf2233a0fd2c7D1472487310503Ec8E857be) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
-  cronos: {
+  telos: {
     tvl: calculateUsdUniTvl(
       "0x4be5Bf2233a0fd2c7D1472487310503Ec8E857be",
       "telos",
