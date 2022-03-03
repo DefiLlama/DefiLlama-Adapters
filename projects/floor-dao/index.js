76dc8c801d5021173e66aada9db415a3637ac935
Jonathan Chemla <jchemla@iconem.com>
2022-03-03 16:45:20 +0100

    Final edit of FloorDAO TVL adding xPUNKWETH SLP

--- projects/floor-dao/index.js
+++ projects/floor-dao/index.js
@@ -1,7 +1,8 @@
 const { ohmTvl } = require('../helper/ohm')
+const sdk = require("@defillama/sdk");
+const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
 const BigNumber = require('bignumber.js');
 // https://docs.floor.xyz/fundamentals/treasury
-const { unwrapUniswapV3LPs, sumTokens } = require("../helper/unwrapLPs");
 
 // Tokens in treasury
 const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
@@ -16,28 +17,42 @@ const FLOOR = '0xf59257E961883636290411c11ec5Ae622d19455e'
 const treasuryTokens = [
     [WETH, false], [PUNK, false], [xPUNK, false], // WETH, PUNK and xPUNK
     [PUNKWETH, true],  // PUNK-ETH SLP
-    [xPUNKWETH, false],  // xPUNKWETH is 1:1 with PUNK-WETH-SLP
 ]
 module.exports = ohmTvl(treasury, treasuryTokens, 'ethereum', stakingAddress, FLOOR)
 
 const tvl = module.exports.ethereum.tvl 
 const transform = a => `ethereum:${a}`
 module.exports.ethereum.tvl = async (time, ethBlock, chainBlocks) => {
+    // Get OHM default TVL balances
     const balances = await tvl(time, ethBlock, chainBlocks)
-    console.log('balances', balances)
+    
+    // Replace xPUNK by PUNK which is 1:1
     balances[transform(PUNK)] = BigNumber(balances[transform(PUNK)]).plus(BigNumber(balances[transform(xPUNK)])).toFixed(0)
-    balances['ethereum:' + xPUNK] = 0
+    balances[transform(xPUNK)] = 0
+
+    // Unwrap xPUNKWETH which is 1:1 with PUNK-WETH Sushi LP
+    const {output: xPUNKWETH_bal} = await sdk.api.abi.call({
+        target: xPUNKWETH,
+        params: [treasury],
+        abi: 'erc20:balanceOf',
+        ethBlock,
+        chain: 'ethereum'
+      })
+    const lpPositions = [{
+        token: PUNKWETH,
+        balance: xPUNKWETH_bal,
+    }]
+    await unwrapUniswapLPs( balances, lpPositions, ethBlock, "ethereum", transform );
     return balances
 }
 
+/*
 const dao_treasury = '0xA9d93A5cCa9c98512C8C56547866b1db09090326'
 module.exports.ethereum.treasury = async (time, ethBlock, chainBlocks) => {
     const balances = {}
-
     await sumTokens(balances, [[FLOOR, dao_treasury]], ethBlock, "ethereum", transform)
     // const univ3_Positions = []
     // await unwrapUniswapV3LPs(balances, univ3_Positions, ethBlock, 'ethereum', transform)
-
     return balances
 }
-// https://gnosis-safe.io/app/eth:/balances
+*/
