1cbdadebe11311b5380b81d790fb291e13f41300
theparalleldotio <93149101+theparalleldotio@users.noreply.github.com>
2022-03-03 18:58:54 +0700

    Update index.js

--- projects/the-parallel/index.js
+++ projects/the-parallel/index.js
@@ -1,4 +1,6 @@
 const sdk = require("@defillama/sdk")
+const BigNumber = require("bignumber.js")
+const { pool2s } = require("../helper/pool2")
 const { transformBscAddress } = require("../helper/portedTokens")
 
 // Token PRL
@@ -13,16 +15,20 @@ const PRL_BUSD_KYBER_LP = "0x3E95e07550E9798272130AB65b58f2f17b3f7c57"
 const PRL_LOCKED = '0x9A5AC21399A6Fd7D6232CA0B52A6b0658727A3d2'
 const PRL_MINING = '0x21EFC3DDE8a69Fb8A5403406ebDd23e08C924785'
 
-const lpContracts = [
-    PRL_TOKEN
-];
-
 const coins = {
     //token addresses
     PRL: "0xd07e82440A395f3F3551b42dA9210CD1Ef4f8B24",
     WBNB: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
     BUSD: "0xe9e7cea3dedca5984780bafc599bd69add087d56"
 }
+
+const lpContracts = [
+    PRL_TOKEN
+];
+const lpAddresses = [
+    PRL_WBNB_PANCAKE_LP,
+    PRL_BUSD_PANCAKE_LP,
+]
 
 /**
 * TVLCALC
@@ -39,18 +45,8 @@ async function tvl(timestamp, block) {
     const PRL_WBNB_PANCAKE_LP_wbnb_bal = await sdk.api.erc20.balanceOf({ target: coins.WBNB, owner: PRL_WBNB_PANCAKE_LP, block, chain: 'bsc' })
     sdk.util.sumSingleBalance(balances, transform(coins.WBNB), PRL_WBNB_PANCAKE_LP_wbnb_bal.output);
 
-    console.log({
-        PRL_WBNB_PANCAKE_LP_wbnb_bal,
-        balances
-    });
-
     const PRL_WBNB_PANCAKE_LP_prl_bal = await sdk.api.erc20.balanceOf({ target: coins.PRL, owner: PRL_WBNB_PANCAKE_LP, block, chain: 'bsc' })
     sdk.util.sumSingleBalance(balances, transform(coins.PRL), PRL_WBNB_PANCAKE_LP_prl_bal.output);
-
-    console.log({
-        PRL_WBNB_PANCAKE_LP_prl_bal,
-        balances
-    });
 
 
     // for PRL_BUSD_PANCAKE_LP
@@ -58,59 +54,39 @@ async function tvl(timestamp, block) {
     const PRL_BUSD_PANCAKE_LP_prl_bal = await sdk.api.erc20.balanceOf({ target: coins.PRL, owner: PRL_BUSD_PANCAKE_LP, block, chain: 'bsc' })
     sdk.util.sumSingleBalance(balances, transform(coins.PRL), PRL_BUSD_PANCAKE_LP_prl_bal.output);
 
-    console.log({
-        PRL_BUSD_PANCAKE_LP_prl_bal,
-        balances
-    });
-
     const PRL_BUSD_PANCAKE_LP_busd_bal = await sdk.api.erc20.balanceOf({ target: coins.BUSD, owner: PRL_BUSD_PANCAKE_LP, block, chain: 'bsc' })
     sdk.util.sumSingleBalance(balances, transform(coins.BUSD), PRL_BUSD_PANCAKE_LP_busd_bal.output);
-
-    console.log({
-        PRL_BUSD_PANCAKE_LP_busd_bal,
-        balances
-    });
-
 
     // for PRL_BUSD_KYBER_LP
 
     const PRL_BUSD_KYBER_LP_prl_bal = await sdk.api.erc20.balanceOf({ target: coins.PRL, owner: PRL_BUSD_KYBER_LP, block, chain: 'bsc' })
     sdk.util.sumSingleBalance(balances, transform(coins.PRL), PRL_BUSD_KYBER_LP_prl_bal.output);
 
-    console.log({
-        PRL_BUSD_KYBER_LP_prl_bal,
-        balances
-    });
-
     const PRL_BUSD_KYBER_LP_busd_bal = await sdk.api.erc20.balanceOf({ target: coins.BUSD, owner: PRL_BUSD_KYBER_LP, block, chain: 'bsc' })
     sdk.util.sumSingleBalance(balances, transform(coins.BUSD), PRL_BUSD_KYBER_LP_busd_bal.output);
 
-    console.log({
-        PRL_BUSD_KYBER_LP_busd_bal,
-        balances
-    });
-
     // for PRL_LOCKED
-
     const PRL_LOCKED_prl_bal = await sdk.api.erc20.balanceOf({ target: coins.PRL, owner: PRL_LOCKED, block, chain: 'bsc' })
     sdk.util.sumSingleBalance(balances, transform(coins.PRL), PRL_LOCKED_prl_bal.output);
-
-    console.log({
-        PRL_LOCKED_prl_bal,
-        balances
-    });
-
 
     // for PRL_MINING
     const PRL_MINING_prl_bal = await sdk.api.erc20.balanceOf({ target: coins.PRL, owner: PRL_MINING, block, chain: 'bsc' })
     sdk.util.sumSingleBalance(balances, transform(coins.PRL), PRL_MINING_prl_bal.output);
 
-    console.log({
-        PRL_MINING_prl_bal,
-        balances
-    });
-
     return balances;
+}
+
+async function staking(timestamp, ethBlock, chainBlocks) {
+
+    // for PRL_LOCKED
+
+    const PRL_LOCKED_prl_bal = await sdk.api.erc20.balanceOf({ target: coins.PRL, owner: PRL_LOCKED, ethBlock, chain: 'bsc' })
+
+    // for PRL_MINING
+    const PRL_MINING_prl_bal = await sdk.api.erc20.balanceOf({ target: coins.PRL, owner: PRL_MINING, ethBlock, chain: 'bsc' })
+
+
+    return { 'bsc:0xd07e82440A395f3F3551b42dA9210CD1Ef4f8B24': new BigNumber(PRL_LOCKED_prl_bal.output).plus(new BigNumber(PRL_MINING_prl_bal.output)).toString(10) }
 }
 
 
@@ -119,6 +95,7 @@ module.exports = {
     token: 'PRL',
     website: 'https://theparallel.io/',
     bsc: {
+        staking: staking,
         tvl: tvl,
     }
 }
