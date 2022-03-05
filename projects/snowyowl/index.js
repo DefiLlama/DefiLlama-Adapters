e09e4acb869a66310a847cdc2956d0031f737b0e
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2022-03-05 00:24:22 +0000

    update to correct addresses

--- projects/snowyowl/index.js
+++ projects/snowyowl/index.js
@@ -1,15 +1,16 @@
 const { tombTvl } = require("../helper/tomb");
 
-const sno = "0x9e6832d13b29d0b1c1c3465242681039b31c7a05";
-const snoshare = "0x924157B5dbB387A823719916B25256410a4Ad470";
+const sno = "0x1fE4869f2C5181b9CD780a7E16194FA2c4C4293D";
+const snoshare = "0xe7A102Fbc8AB3581d62830DdB599eCCaae5e7875";
 const boardroom = "0x264C36747b6cC5243d8999345FFf8F220B7CCc77";
-const rewardPool = "0xb762Ece3Bc3571376BE73D2e6F3bBf4d108ED8b1";
+const rewardPool = "0xb7d3dc568F7dF54d516D37739912Fc2E541Ba2fF";
 const lps = [
-    "0xE63b66A8CF7811525cd15daB15F17fb62aa5af2F",
-    "0x061349a57b702ebE3139CA419457bb23f7e0D8A2"
+    "0xe63b66a8cf7811525cd15dab15f17fb62aa5af2f", //joe/sno
+    "0x3e262be2339069cec95552683c1eb3f513adcc66", //sno/snoshare
+    "0x061349a57b702ebe3139ca419457bb23f7e0d8a2"
 ]
 
 module.exports = {
     misrepresentedTokens: true,
-    ...tombTvl(sno, snoshare, rewardPool, boardroom, lps, "avax", undefined, false, lps[1])
+    ...tombTvl(sno, snoshare, rewardPool, boardroom, lps, "avax")
 }
