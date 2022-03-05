194b33cc985e2ab181b86a9ae9730d6bf289a95e
Nguyen Le Vu Long <vulongvn98@gmail.com>
2022-03-05 19:00:22 +0700

    update TVL function for Minswap mainnet contract
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>

--- projects/minswap/index.js
+++ projects/minswap/index.js
@@ -1,19 +1,34 @@
-const axios = require('axios')
+const axios = require("axios");
 
-async function tvl(){
-    const adaLocked = (await axios.get("https://cardano-mainnet.blockfrost.io/api/v0/addresses/addr1vxs9zqu2lcddy98wml0d7qtpfy2m7shtz0gzkj93ghgkckg373c0h", {
-        headers:{
-            project_id: "mainnetTV9qV3mfZXbE6e44TVGMe1UoRlLrpSQt"
-        }
-    })).data.amount.find(token=>token.unit==="lovelace").quantity
-    return {
-        "cardano": adaLocked*2/1e6
-    }
+async function tvl() {
+  const liquidityPoolLocked = (
+    await axios.get(
+      "https://cardano-mainnet.blockfrost.io/api/v0/addresses/addr1z9tu3ecccgqlhgg2nkshfrt8td2zs8fmrwvrchgksl78x96j2c79gy9l76sdg0xwhd7r0c0kna0tycz4y5s6mlenh8pq26n58l",
+      {
+        headers: {
+          project_id: "mainnetTV9qV3mfZXbE6e44TVGMe1UoRlLrpSQt",
+        },
+      }
+    )
+  ).data.amount.find((token) => token.unit === "lovelace").quantity;
+  const batchOrderLocked = (
+    await axios.get(
+      "https://cardano-mainnet.blockfrost.io/api/v0/addresses/addr1wyx22z2s4kasd3w976pnjf9xdty88epjqfvgkmfnscpd0rg3z8y6v",
+      {
+        headers: {
+          project_id: "mainnetTV9qV3mfZXbE6e44TVGMe1UoRlLrpSQt",
+        },
+      }
+    )
+  ).data.amount.find((token) => token.unit === "lovelace").quantity;
+  return {
+    cardano: (liquidityPoolLocked * 2) / 1e6 + batchOrderLocked / 1e6,
+  };
 }
 
-module.exports={
-    timetravel: false,
-    cardano:{
-        tvl
-    }
-}
+module.exports = {
+  timetravel: false,
+  cardano: {
+    tvl,
+  },
+};
