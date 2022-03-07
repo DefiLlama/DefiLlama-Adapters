05740077fb1a23c6c0fb751cd977a6aab4397e8a
waynebruce0x <waynebruce0x@gmail.com>
2022-03-07 18:05:37 +0000

    onx edit

--- projects/onx.js
+++ projects/onx.js
@@ -1,4 +1,5 @@
-const { getEthereumTvl, getEthereumStaking, getEthereumPoolTvl, getEthereumBorrows } = require('./config/onx/ethereum');
+const sdk = require("@defillama/sdk");
+const { getEthereumTvl, getEthereumStaking, getEthereumPoolTvl, getEthereumBorrows, getEthereumVautsTvl } = require('./config/onx/ethereum');
 const { getFantomTvl } = require('./config/onx/fantom');
 const { getPolygonTvl } = require('./config/onx/polygon');
 const { getAvalancheTvl } = require('./config/onx/avalanche');
@@ -7,7 +8,7 @@ module.exports = {
   timetravel: false,
   doublecounted: true,
   ethereum: {
-    tvl: getEthereumTvl,
+    tvl: sdk.util.sumChainTvls([getEthereumTvl, getEthereumVautsTvl]),
     staking: getEthereumStaking,
     pool: getEthereumPoolTvl,
     borrows: getEthereumBorrows,
