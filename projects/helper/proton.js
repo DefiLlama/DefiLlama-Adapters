9ee2c723706702349644743f138f7f43fa8a9fa5
jafri <syedjafri97@gmail.com>
2022-03-09 12:55:25 -0700

    Add single side staking TVL

--- projects/helper/proton.js
+++ projects/helper/proton.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
+const retry = require('./retry')
 
 const CHAIN_RPC = 'https://proton.greymass.com';
+const SWAP_CONTRACT = 'proton.swaps';
+const ORACLES_CONTRACT = 'oracles';
 
-async function get_table_rows({
+async function getTableRows({
   json = true,
   code,
   scope,
@@ -11,7 +14,7 @@ async function get_table_rows({
   upper_bound = '',
   index_position = 1,
   key_type = '',
-  limit = 10,
+  limit = -1,
   reverse = false,
   show_payer = false,
 }) {
@@ -38,7 +41,37 @@ async function get_table_rows({
   }
 }
 
-async function get_currency_balance(code, account, symbol) {
+
+async function getFullTable ({
+  code,
+  scope,
+  table,
+  lower_bound,
+  upper_bound,
+}) {
+  let { rows, more, next_key } = await getTableRows({
+    code,
+    scope,
+    table,
+    lower_bound,
+    upper_bound,
+    limit: -1
+  })
+
+  if (more) {
+    rows = rows.concat(await getFullTable({
+      code,
+      scope,
+      table,
+      lower_bound: next_key,
+      upper_bound
+    }))
+  }
+
+  return rows
+}
+
+async function getCurrencyBalance(code, account, symbol) {
   try {
     const { data } = await axios.default.post(
       CHAIN_RPC + '/v1/chain/get_currency_balance',
@@ -56,26 +89,43 @@ async function get_currency_balance(code, account, symbol) {
 
 async function getAllOracleData(lower_bound) {
   try {
-    let { rows, more, next_key } = await get_table_rows({
-      code: 'oracles',
-      scope: 'oracles',
+    return await getFullTable({
+      code: ORACLES_CONTRACT,
+      scope: ORACLES_CONTRACT,
       table: 'data',
       limit: -1,
       lower_bound: lower_bound,
     });
-
-    if (more) {
-      rows = rows.concat(await getAllOracleData(next_key));
-    }
-
-    return rows;
   } catch (e) {
     return [];
   }
 }
 
+async function getAllSwapPools(lower_bound) {
+  try {
+    return await getFullTable({
+      code: SWAP_CONTRACT,
+      scope: SWAP_CONTRACT,
+      table: 'pools',
+      limit: -1,
+      lower_bound: lower_bound,
+    });
+  } catch (e) {
+    return [];
+  }
+}
+
+async function getTokenPriceUsd(tokenSymbol, tokenContract) {
+  const { data: tokens } = await axios.get('https://api.protonchain.com/v1/chain/exchange-rates/info')
+  const token = tokens.find(token => token.symbol === tokenSymbol && token.contract === tokenContract)
+  const exchangeRate = token.rates.find(rate => rate.counterCurrency === 'USD')
+  return exchangeRate.price
+}
+
 module.exports = {
-  get_table_rows,
-  get_currency_balance,
+  getTableRows,
+  getCurrencyBalance,
   getAllOracleData,
+  getAllSwapPools,
+  getTokenPriceUsd
 }
