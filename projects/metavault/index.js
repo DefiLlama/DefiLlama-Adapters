3a738c098a8f764b4723448f113897aec3b7eacd
0xR <99395193+0xrmvdao@users.noreply.github.com>
2022-03-09 08:52:30 +0300

    TVL calculation changed to use ohmTVL helper.
    we have made a change to use ohmTVL helper for TVL calculation.

--- projects/metavault/index.js
+++ projects/metavault/index.js
@@ -1,58 +1,15 @@
-const { GraphQLClient, gql } = require("graphql-request");
-const retry = require("async-retry");
-const { getBlock } = require("../helper/getBlock");
-const sdk = require("@defillama/sdk");
-const erc20 = require("../helper/abis/erc20.json");
+const {ohmTvl} = require("../helper/ohm");
 
-async function fetch() {
-  var endpoint =
-    "https://api.thegraph.com/subgraphs/name/metavaultorg/metavault-subgraph";
-  var graphQLClient = new GraphQLClient(endpoint);
+const treasury = "0x7bE9BbB7373B675aBd25fA6d58085C8dACF6cc4a";
 
-  var query = gql`
-    query {
-      protocolMetrics(first: 1, orderBy: timestamp, orderDirection: desc) {
-        treasuryMV
-        totalValueLocked
-      }
-    }
-  `;
-  const results = await retry(
-    async (bail) => await graphQLClient.request(query)
-  );
-  return results.protocolMetrics[0].totalValueLocked;
-}
+const treasuryTokens = [
+    ["0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E", false], // DAI
+    ["0x738F57002c3Ff4C3b418c2d37024996608f8E281", true], // MVD-DAI LP
+];
 
-const stakingContract = "0x6eA8de8f643ba65D8be39bd8D3B72f6DaAda7E77";
-const mvdContract = "0x27746007e821aeec6F9C65CBFda04870c236346c";
-
-const Stakings = [stakingContract];
-const staking = async (timestamp, ethBlock, chainBlocks) => {
-  const balances = {};
-  const chain = "fantom";
-  let stakingBalance,
-    totalBalance = 0;
-  const block = await getBlock(timestamp, chain, chainBlocks);
-  for (const stakings of Stakings) {
-    stakingBalance = await sdk.api.abi.call({
-      abi: erc20.balanceOf,
-      target: mvdContract,
-      params: stakings,
-      chain: chain,
-      block: block,
-    });
-    totalBalance += Number(stakingBalance.output);
-  }
-  const address = `${chain}:${mvdContract}`;
-
-  return {
-    [address]: totalBalance,
-  };
-};
+const stakingAddress = "0x6eA8de8f643ba65D8be39bd8D3B72f6DaAda7E77";
+const stakingToken = "0x27746007e821aeec6F9C65CBFda04870c236346c";
 
 module.exports = {
-  fantom: {
-    staking
-  },
-  fetch
-};
+    ...ohmTvl(treasury, treasuryTokens, "fantom", stakingAddress, stakingToken)
+}
