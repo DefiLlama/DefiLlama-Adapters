b508f8996913e36fb30c116b5658cf70f0161b58
RickOhFinance <86678747+RickOhFinance@users.noreply.github.com>
2022-03-09 13:21:56 -0500

    Update index.js
    add usdt.e, dai.e banks for avax, added moonriver banks for USDT, and USDC

--- projects/oh-finance/index.js
+++ projects/oh-finance/index.js
@@ -15,9 +15,21 @@ function getBankTvl(bankAddress, chain){
 
 module.exports={
     ethereum:{
-        tvl: getBankTvl("0xa528639AAe2E765351dcd1e0C2dD299D6279dB52", "ethereum"),
+        tvl: getBankTvl("0xa528639aae2e765351dcd1e0c2dd299d6279db52", "ethereum"),
     },
-    avalanche:{
+    avalanche_usdc:{
         tvl: getBankTvl("0x8B1Be96dc17875ee01cC1984e389507Bb227CaAB", "avax"),
+    },
+    avalanche_usdt:{
+        tvl: getBankTvl("0xd96AbEcf6AA022735CFa9CB512d63645b0834720", "avax"),
+    },
+    avalanche_dai:{
+        tvl: getBankTvl("0xF74303DD14E511CCD90219594e8069d36Da01DCD", "avax"),
+    },        
+    moonriver_usdc:{
+        tvl: getBankTvl("0x4C211F45876d8EC7bAb54CAc0e32AAD15095358A","moonriver"),
+    },
+    moonriver_usdt:{
+        tvl:  getBankTvl("0xdeA7Ff1D84B7E54587b434C1A585718857CF61d1","moonriver"),
     }
 }
