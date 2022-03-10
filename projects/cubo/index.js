b078a3802647d1ab25bbb553fdcb200f09a88b10
g1nt0ki <gintama@ulysses.se>
2022-03-10 16:41:58 +0100

    split TVL between treasury and dao contract

--- projects/cubo/index.js
+++ projects/cubo/index.js
@@ -1,18 +1,34 @@
 const { sumTokens, unwrapCrv, } = require('../helper/unwrapLPs')
 
-async function polygon_tvl(timestamp, block) {
+const chain = 'polygon'
+const DAI = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063'
 
-  const chain = 'polygon'
-  const DAI = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063'
+const treasuryAddress = '0xb495ffc5acd7e2fd909c23c30d182e6719fbe9ec'
+const daoContract = '0xb05d0da5253e77a8ad37232e8235c712e10edee8'
+
+async function polygon_tvl(timestamp, block) {
+  const daoTokens = [DAI,].map(token => [token, daoContract,])
+
+  const balances = {}
+  const transformPolygonAddress = addr => `${chain}:${addr}`
+  await sumTokens(
+    balances,
+    daoTokens,
+    block,
+    chain,
+    transformPolygonAddress
+  )
+  return balances
+}
+
+async function polygon_treasury(timestamp, block) {
+
   const DRAGON_QUICK = '0xf28164a485b0b2c90639e47b0f377b4a438a16b1'
   const CUBO_TOKEN = '0x381d168DE3991c7413d46e3459b48A5221E3dfE4'
   const MOO_CRV_TriCrypto = '0x5A0801BAd20B6c62d86C566ca90688A6b9ea1d3f'
   const MOO_AM3CRV = '0xAA7C2879DaF8034722A0977f13c343aF0883E92e'
   const AM3CRV = '0xe7a24ef0c5e95ffb0f6684b813a78f2a3ad7d171'
   const CRV_TriCrypto = '0xdad97f7713ae9437fa9249920ec8507e5fbb23d'
-
-  const treasuryAddress = '0xb495ffc5acd7e2fd909c23c30d182e6719fbe9ec'
-  const daoContract = '0xb05d0da5253e77a8ad37232e8235c712e10edee8'
 
   const UnwrapTokenMapping = [
     { from: MOO_AM3CRV, unwrapTo: AM3CRV },
@@ -25,13 +41,12 @@ async function polygon_tvl(timestamp, block) {
   }, {})
 
   const treasuryTokens = [DAI, DRAGON_QUICK, CUBO_TOKEN, MOO_CRV_TriCrypto, MOO_AM3CRV,].map(token => [token, treasuryAddress,])
-  const daoTokens = [DAI,].map(token => [token, daoContract,])
 
   const balances = {}
   const transformPolygonAddress = addr => `${chain}:${addr}`
   await sumTokens(
     balances,
-    [...treasuryTokens, ...daoTokens,],
+    treasuryTokens,
     block,
     chain,
     transformPolygonAddress
@@ -54,6 +69,7 @@ async function polygon_tvl(timestamp, block) {
 module.exports = {
   polygon: {
     tvl: polygon_tvl,
+    treasury: polygon_treasury,
   },
   methodology: `TVL on polygon is sum of all collateralTokens (dai only atm) provided to mint nodes`,
 }
