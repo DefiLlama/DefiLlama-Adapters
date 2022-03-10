d286fdeb4b4f961e7540dff8eebb54ca74ec1adb
bbelly <bawei3600@gmail.com>
2022-03-10 20:59:35 +0800

    add staking for mojitoswap

--- projects/mojitoswap/index.js
+++ projects/mojitoswap/index.js
@@ -1,16 +1,20 @@
-const { getChainTvl } = require("./utils");
+const { MJT_GRAPH } = require("./query");
+const { getChainTvl, getStakeLockValue } = require("./utils");
 
 const graphUrls = {
-  kcc: "https://thegraph.kcc.network/subgraphs/name/mojito/swap",
+  kcc: MJT_GRAPH,
 };
 
 const chainTvl = getChainTvl(graphUrls);
+const stakeValue = getStakeLockValue();
 
 module.exports = {
   misrepresentedTokens: true,
   methodology: "We count liquidity on the dexes, pulling data from subgraphs",
   kcc: {
     tvl: chainTvl("kcc"),
+    staking: stakeValue,
   },
   tvl: chainTvl("kcc"),
+  staking: stakeValue,
 };
