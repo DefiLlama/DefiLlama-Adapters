21387dc5c755fce032ffae0e8bf112787b9f9304
0xngmi <0xngmi@protonmail.com>
2022-03-12 11:35:11 +0000

    fix ankr

--- projects/config/ankr/ethereum/index.js
+++ projects/config/ankr/ethereum/index.js
@@ -5,30 +5,22 @@ const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 
 const getaETHcTvl = async () => {
   const totalSupply = await aETHcTokenContract.methods.totalSupply().call();
-  console.log(111, `aETHc ${totalSupply}`);
-  // return { [tokenAddresses.aETHc]: totalSupply };
   return { [wethAddress]: totalSupply };
 }
 
 const getaMATICbTvl = async () => {
   const totalSupply = await aMATICbTokenContract.methods.totalSupply().call();
-  console.log(111, `aMATICb ${totalSupply}`)
-  // return { [tokenAddresses.aMATICb]: totalSupply };
-  return { [wethAddress]: totalSupply };
+  return { "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0": totalSupply };
 }
 
 const getaDOTbTvl = async () => {
   const totalSupply = await aDOTbTokenContract.methods.totalSupply().call();
-  console.log(111, `aDOTb ${totalSupply}`)
-  // return { [tokenAddresses.aDOTb]: totalSupply };
-  return { [wethAddress]: totalSupply };
+  return { "bsc:0x7083609fce4d1d8dc0c979aab8c869ea2c873402": totalSupply };
 }
 
 const getaKSMbTvl = async () => {
   const totalSupply = await aKSMbTokenContract.methods.totalSupply().call();
-  console.log(111, `aKSMb ${totalSupply}`)
-  // return { [tokenAddresses.aKSMb]: totalSupply };
-  return { [wethAddress]: totalSupply };
+  return { kusama: Number(totalSupply)/1e18 };
 }
 
 module.exports = {
