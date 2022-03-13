4c40eaec151049fa5b325a05751dab5accb3fe62
Justin Schuldt <justinschuldt@gmail.com>
2022-03-13 14:40:47 -0500

    Wormhole TVL per chain

--- projects/wormhole.js
+++ projects/wormhole.js
@@ -2,13 +2,42 @@ const retry = require('async-retry')
 const axios = require("axios");
 const BigNumber = require("bignumber.js");
 
-async function fetch() {
-  const url = 'https://europe-west3-wormhole-315720.cloudfunctions.net/mainnet-notionaltvl'
-  const res = await retry(async bail => await axios.get(url))
-  const tvl = res.data.AllTime["*"]["*"].Notional
-  return new BigNumber(tvl).toFixed(2)
+function fetch(chainId) {
+  return async () => {
+    const url = 'https://europe-west3-wormhole-315720.cloudfunctions.net/mainnet-notionaltvl'
+    const res = await retry(async bail => await axios.get(url))
+    if (chainId in res.data.AllTime) {
+      const tvl = res.data.AllTime[chainId]["*"].Notional
+      return new BigNumber(tvl).toFixed(2)
+    } else {
+      throw "invalid ChainId supplied"
+    }
+  }
 }
 
 module.exports = {
-  fetch
+  solana: {
+    fetch: fetch("1")
+  },
+  ethereum: {
+    fetch: fetch("2")
+  },
+  bsc: {
+    fetch: fetch("3")
+  },
+  terra: {
+    fetch: fetch("4")
+  },
+  polygon: {
+    fetch: fetch("5")
+  },
+  avax: {
+    fetch: fetch("6")
+  },
+  oasis: {
+    fetch: fetch("7")
+  },
+  fantom: {
+    fetch: fetch("10")
+  },
 }
