d4c879598de26019146e62fc5320a4560dd3e7d4
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2022-03-13 09:06:59 +0000

    add staking tvl

--- projects/mojitoswap/index.js
+++ projects/mojitoswap/index.js
@@ -1,5 +1,6 @@
 const { MJT_GRAPH } = require("./query");
 const { getChainTvl, getStakeLockValue } = require("./utils");
+const { staking } = require("../helper/staking.js");
 
 const graphUrls = {
   kcc: MJT_GRAPH,
@@ -14,7 +15,7 @@ module.exports = {
     "We count liquidity and staking on the dexes, pulling data from subgraphs",
   kcc: {
     tvl: chainTvl("kcc"),
-    staking: stakeValue,
+    staking: staking("0x25c6d6a65c3ae5d41599ba2211629b24604fea4f","0x2ca48b4eea5a731c2b54e7c3944dbdb87c0cfb6f", "kcc"),
   },
-  tvl: chainTvl("kcc"),
+  
 };
