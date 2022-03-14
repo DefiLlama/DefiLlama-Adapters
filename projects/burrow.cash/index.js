c20cde52259b918b17bd02c50609a075568673b3
g1nt0ki <gintama@ulysses.se>
2022-03-14 08:38:00 +0100

    code refactor

--- projects/burrow.cash/index.js
+++ projects/burrow.cash/index.js
@@ -1,14 +1,6 @@
-const { call, getTokenBalance, } = require('../helper/near')
-const BigNumber = require('bignumber.js')
-const sdk = require('@defillama/sdk')
-const { transformNearAddress } = require('../helper/portedTokens')
-
-const transformAddress = transformNearAddress()
+const { call, addTokenBalances, } = require('../helper/near')
 
 const BURROW_BETA_CONTRACT = 'contract.beta.burrow.near'
-const PROBLEMATIC_TOKENS = [
-  'aurora',    // I am not sure what aurora token is, but unfortunately it is valued too high (decimal issue?), maybe it is aurora-near but see it being displayed as eth in the UI
-]
 
 async function tvl() {
   const balances = {}
@@ -17,28 +9,10 @@ async function tvl() {
   const assets = assetsCallResponse
     .map(([asset]) => asset)
     .filter(asset => !/\.burrow\./.test(asset))  // Ignore all assets that can be considered native tokens
-    .filter(asset => !PROBLEMATIC_TOKENS.includes(asset))
-  await Promise.all(assets.map(asset => addAsset(balances, asset)))
+  await addTokenBalances(assets, BURROW_BETA_CONTRACT, balances)
   return balances
 }
 
-const tokenMapping = {
-  'wrap.near': { name: 'near', reduceDecimals: 24, },
-  'meta-pool.near': { name: 'staked-near', reduceDecimals: 24, },
-}
-
-async function addAsset(balances, token) {
-  let balance = await getTokenBalance(token, BURROW_BETA_CONTRACT)
-  const { name, reduceDecimals, } = tokenMapping[token] || {}
-
-  if (name) {
-    if (reduceDecimals)
-      balance = balance.slice(0, balance.length - reduceDecimals)
-    token = name
-  }
-
-  sdk.util.sumSingleBalance(balances, transformAddress(token), balance)
-}
 
 module.exports = {
   near: {
