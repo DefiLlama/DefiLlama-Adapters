90d3172ddf5305670c38e8903fa9100fc5e7a570
waynebruce0x <waynebruce0x@gmail.com>
2022-03-15 17:57:25 +0000

    enzyme

--- projects/enzyme/index.js
+++ projects/enzyme/index.js
@@ -1,178 +1,70 @@
-const sdk = require("@defillama/sdk");
-const { request, gql } = require("graphql-request");
-const queryUrl = "https://api.thegraph.com/subgraphs/name/enzymefinance/enzyme";
-const { BigNumber } = require("bignumber.js");
-const {unwrapUniswapLPs, unwrapCrv} = require("../helper/unwrapLPs")
+const axios = require('axios');
+const retry = require("async-retry");
+const { getBlock } = require('../helper/getBlock')
+const { sumTokensAndLPsSharedOwners } = require('../helper/unwrapLPs');
 
-const mln = "0xec67005c4e498ec7f55e092bd1d35cbc47c91892";
-const mlnWethPool = "0x15ab0333985fd1e289adf4fbbe19261454776642";
+let tokens = [
+  "0x0d8775f648430679a709e98d2b0cb6250d2887ef", // BAT
+  "0x0f5d2fb29fb7d3cfee444a200298f468908cc942", // MANA
+  "0x1985365e9f78359a9b6ad760e32412f4a445e862", // REP
+  "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
+  "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
+  "0x408e41876cccdc0f92210600ef50372656052a38", // REN
+  "0x4f3afec4e5a3f2a6a1a411def7d7dfe50ee057bf", // DGX
+  "0x514910771af9ca656af840dff83e8264ecf986ca", // LINK
+  "0x607f4c5bb672230e8672085532f7e901544a7375", // RLC
+  "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+  "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359", // SAI
+  "0x960b236a07cf122663c4303350609a66a7b288c0", // ANT
+  "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", // MKR
+  "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+  "0xa117000000f279d81a1d3cc75430faa017fa5a2e", // ANTv2
+  "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
+  "0xd26114cd6ee289accf82350c8d8487fedb8a0c07", // OMG
+  "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+  "0xdd974d5c2e2928dea5f71b9825b8b646686bd200", // KNC
+  "0xe41d2489571d322189246dafa5ebde1f4699f498", // ZRX
+  "0xec67005c4e498ec7f55e092bd1d35cbc47c91892", // MLN
+  "0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4", // ENG
+];
 
-const derivatives = [null, "Aave", "Alpha", "Chai", "Compound", "CurvePool", "Idle", "Stakehound", "Synthetix", "UniswapPool", "Yearn"];
+// node test.js projects/enzyme/historical.js
+async function tvl(timestamp, blockEth, chainBlocks) {
+  const block = await getBlock(timestamp, 'ethereum', chainBlocks);
+  const balances = {};
 
-async function query(block, derivative) {
-  const field = gql`
-    query getData($block: Int) {
-      assets(first: 1000, where: {derivativeType: ${derivative}}, block: {number: $block}) {
-        id
-        decimals
-        networkAssetHolding{
-          amount
-        }
-        underlyingAsset{
-          id
-          decimals
-        }
-      }
-    }
-  `
-  const result = (await request(queryUrl, field, {block})).assets;
-  return result;
-}
+  // V1
+  let vaults = (await retry(
+    async (bail) =>
+      await axios.get(
+        "https://app.enzyme.finance/api/v1/vault-list?network=ethereum"
+      )
+  )).data.map(v => v.id);
 
-async function tvl(timestamp, block) {
-  let balances = {};
-  let lpPositions = [];
-  let crvAssets = [];
-  for (let i = 0; i < derivatives.length; i++) {
-    let derivative = derivatives[i];
-    let results = await query(block, derivative);
-    results.forEach(asset => {
-      if (asset.networkAssetHolding === null || asset.networkAssetHolding.amount === "0") {
-        return;
-      }
-      let token;
-      let decimals;
-      let balance;
-      switch (derivative) {
-        case null:
-        case "Aave":
-        case "Alpha":
-        case "Compound":
-        case "Idle":
-        case "Stakehound":
-        case "Synthetix":
-          token = asset.id;
-          if (token === mln) {
-            return;
-          }
-          // If sXTZ convert to regular Tezos
-          else if (token === "0x2e59005c5c0f0a4d77cca82653d48b46322ee5cd") {
-            sdk.util.sumSingleBalance(balances, ["tezos"], BigNumber(asset.networkAssetHolding.amount).toFixed(0));
-            return;
-          }
-          decimals = asset.decimals;
-          balance = BigNumber(asset.networkAssetHolding.amount).times(10 ** Number(decimals)).toFixed(0);
-          sdk.util.sumSingleBalance(balances, token, balance);
-          break;
-        case "Chai":
-          // No results for Chai
-          break;
-        case "CurvePool":
-          token = asset.id;
-          decimals = asset.decimals;
-          balance = BigNumber(asset.networkAssetHolding.amount).times(10 ** Number(decimals)).toFixed(0);
-          // Handles eCRV Gauge
-          if (token === "0x3c0ffff15ea30c35d7a85b85c0782d6c94e1d238") {
-            sdk.util.sumSingleBalance(balances, ["ethereum"], BigNumber(asset.networkAssetHolding.amount).div(2).toFixed(0));
-            sdk.util.sumSingleBalance(balances, "0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb", BigNumber(asset.networkAssetHolding.amount).times(10 ** 18).div(2).toFixed(0));
-          }
-          // Handles steCRV Gauge
-          else if (token === "0x182b723a58739a9c974cfdb385ceadb237453c28") {
-            sdk.util.sumSingleBalance(balances, ["ethereum"], BigNumber(asset.networkAssetHolding.amount).div(2).toFixed(0));
-            sdk.util.sumSingleBalance(balances, "0xae7ab96520de3a18e5e111b5eaab095312d7fe84", BigNumber(asset.networkAssetHolding.amount).times(10 ** 18).div(2).toFixed(0));
-          }
-          else{
-            crvAssets.push({
-              token,
-              balance
-            });
-          }
-          break;
-        case "UniswapPool":
-          token = asset.id;
-          if (token === mlnWethPool) {
-            return;
-          }
-          decimals = asset.decimals;
-          balance = BigNumber(asset.networkAssetHolding.amount).times(10 ** Number(decimals)).toFixed(0);
-          lpPositions.push({
-            balance,
-            token
-          });
-          break;
-        case "Yearn":
-          token = asset.underlyingAsset.id;
-          decimals = asset.underlyingAsset.decimals;
-          balance = BigNumber(asset.networkAssetHolding.amount).times(10 ** Number(decimals)).toFixed(0);
-          sdk.util.sumSingleBalance(balances, token, balance);
-          break;
-        default:
-          break;
-      }
-    });
-  }
-  
-  for (let i = 0; i < crvAssets.length; i++) {
-    let asset = crvAssets[i];
-    // Handles eursCRV Gauge
-    if (asset.token === "0x90bb609649e0451e5ad952683d64bd2d1f245840") {
-      await unwrapCrv(balances, "0x194ebd173f6cdace046c53eacce9b953f28411d1", asset.balance);
-    }
-    // Handles a3CRV Gauge
-    else if (asset.token === "0xd662908ada2ea1916b3318327a97eb18ad588b5d") {
-      await unwrapCrv(balances, "0xfd2a8fa60abd58efe3eee34dd494cd491dc14900", asset.balance);
-    }
-    else {
-      await unwrapCrv(balances, asset.token, asset.balance);
-    }
-  }
-  await unwrapUniswapLPs(balances, lpPositions, block);
+  tokens = tokens.map(token => [token, false]);
 
-  return balances;
-}
+  for (let i = 0; i < vaults.length / 50; i++) {
+    await sumTokensAndLPsSharedOwners(
+      balances, 
+      tokens, 
+      vaults.slice(25 * i, 25 * (i + 1)), 
+      block
+    );
+    await delay(2000);
+  };
 
-// Gets MLN TVL
-async function staking(timestamp, block) {
-  let balances = {};
-  const field = gql`
-  query getBalance($block: Int) {
-    asset(id:"0xec67005c4e498ec7f55e092bd1d35cbc47c91892", block: {number: $block}) {
-      networkAssetHolding{
-        amount
-      }
-    }
-  }
-  `
-  let balance = (await request(queryUrl, field, {block})).asset.networkAssetHolding.amount;
-  sdk.util.sumSingleBalance(balances, mln, BigNumber(balance).times(10 ** 18).toFixed(0));
-  return balances;
-}
+  balances['0xa117000000f279d81a1d3cc75430faa017fa5a2e'] = balances['0x960b236a07cf122663c4303350609a66a7b288c0'];
+  delete balances['0x960b236a07cf122663c4303350609a66a7b288c0'];
 
-// Calculates TVL locked in the MLN-WETH UNI LP
-async function pool2(timestamp, block) {
-  let balances = {};
-  let token = mlnWethPool;
-  const field = gql`
-  query getBalance($block: Int) {
-    asset(id:"0x15ab0333985fd1e289adf4fbbe19261454776642", block: {number: $block}) {
-      networkAssetHolding{
-        amount
-      }
-    }
-  }
-  `
-  let balance = (await request(queryUrl, field, {block})).asset.networkAssetHolding.amount;
-  balance = BigNumber(balance).times(10 ** 18).toFixed(0);
-  await unwrapUniswapLPs(balances, [{balance, token}], block);
-  return balances;
-}
-
+  return balances
+};
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time)
+  });
+};
 module.exports = {
-  methodology: "Uses subgraph to retrieve asset information and balances then uses onchain calls to calculate TVL.",
-  misrepresentedTokens: true,
   ethereum: {
-    tvl,
-    staking,
-    pool2
+    tvl
   }
 };
