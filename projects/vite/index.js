30abfe0442d1435d8c0b61ba583a10bb2c63a28e
allen <allen@vite.org>
2022-03-16 23:28:43 +0800

    feat: add adapter for vite

---
+++ projects/vite/index.js
@@ -0,0 +1,38 @@
+const retry = require('async-retry')
+const utils = require('../helper/utils');
+
+async function getData() {
+  const res = await retry(async bail => await utils.fetchURL('https://vite-info-api.xvite.workers.dev/'))
+  return res.data;
+}
+
+async function vitex() {
+  const data = await getData()
+  return data.tvlRankings.filter((i) => {
+    return i.name == 'ViteX'
+  })[0].amount
+}
+
+async function viva() {
+  const data = await getData()
+  return data.tvlRankings.filter((i) => {
+    return i.name == 'Viva'
+  })[0].amount
+}
+
+async function fetch() {
+  const data = await getData()
+  return data.tvl
+}
+
+module.exports = {
+  timetravel: false,
+  misrepresentedTokens: true,
+  vitex:{
+    fetch: vitex
+  },
+  viva:{
+    fetch: viva
+  },
+  fetch
+}
