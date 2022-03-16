878426b13b5baaf802a1daccee58a76f5b8613ae
LpcAries <291482574@qq.com>
2022-03-16 18:50:53 +0800

    add izumi adapter

---
+++ projects/izumi/index.js
@@ -0,0 +1,13 @@
+const retry = require('async-retry')
+const axios = require("axios");
+const BigNumber = require("bignumber.js");
+
+async function fetch() {
+  let response = await retry(async bail => await axios.get('https://izumi.finance/api/v1/farm/stat/tvl'))
+  let tvl = new BigNumber(response.data.data.tvl).toFixed(2);
+  return tvl;
+}
+
+module.exports = {
+  fetch
+}
