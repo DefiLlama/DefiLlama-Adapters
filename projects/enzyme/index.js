a2907715891abe1fa6e74c5cf558182c5527c072
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-03-16 17:42:23 +0100

    Query enzyme api for their tvl

--- projects/enzyme/index.js
+++ projects/enzyme/index.js
@@ -1,58 +1,18 @@
 const axios = require('axios')
-const retry = require('async-retry')
-const sdk = require('@defillama/sdk')
-const BigNumber = require("bignumber.js")
 const { toUSDTBalances } = require('../helper/balances')
 
+
 async function tvl() {
-  const balances = {
-    value: BigNumber(0)
-  }
-
-  // V1
-  let vaults = (await retry(
-    async (bail) =>
-      await axios.get(
-        'https://app.enzyme.finance/api/v1/vault-list?network=ethereum'
-      )
-  )).data.map(v => v.id)
-
-  const pageSize = 10
-  const pageCount = vaults.length / pageSize
-
-  for (let i = 0; i <= pageCount ; i++) {
-    const items = vaults.slice(pageSize * i, pageSize * (i + 1))
-    await Promise.all(items.map(address => addAllTokens(address, balances)))
-    
-    await delay(2000)
-  }
-
-  return toUSDTBalances(balances.value)
-}
-
-async function addAllTokens(address, balances = {}) {
-  let tokens = (
-    await axios.get(
-      `https://api.covalenthq.com/v1/1/address/${address}/balances_v2/?&key=ckey_72cd3b74b4a048c9bc671f7c5a6`
-    )
-  ).data.data.items
-
-  tokens
-    .forEach(({ contract_address: token, balance, contract_decimals: decimals, quote_rate: price, }) => {
-      if (!balance || !decimals || !price || !token) return;
-      const bigNumber = BigNumber(balance).shiftedBy(-1 * decimals).multipliedBy(price)
-      if (bigNumber.toFixed(2) > 10 ** 7) {
-        return; // Ignoring values that return more than 10 Million
-      }
-      balances.value  = bigNumber.plus(balances.value)
-    })
-  return balances
-}
-
-function delay(time) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, time)
+  let response = await axios.post('https://app.enzyme.finance/api/graphql' ,{
+    'operationName': 'NetworkLatestStats',
+    'variables': {
+      'currency': 'usd',
+      'network': 'ethereum'
+    },
+    'query': 'query NetworkLatestStats($currency: Currency!, $network: Network!) {\n  networkLatestStats(currency: $currency, network: $network) {\n    gav\n    vaults\n    deposits\n    __typename\n  }\n}'
   })
+
+  return toUSDTBalances(response.data.data.networkLatestStats.gav)
 }
 
 module.exports = {
