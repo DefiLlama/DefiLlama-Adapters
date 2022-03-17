84e8298801bb5316ca6e827baae6b77703231ba6
waynebruce0x <waynebruce0x@gmail.com>
2022-03-17 17:34:07 +0000

    zunami fix

--- projects/zunami/index.js
+++ projects/zunami/index.js
@@ -9,9 +9,9 @@ const zunamiHoldingsDecimals = 18;
 const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 const usdtDecimals = 6;
 
-const ethTvl = async () => {
+async function ethTvl(timestamp, block) {
   const totalHoldings = (await sdk.api.abi.call({
-    block: startBlock,
+    block,
     abi: abi.totalHoldings,
     target: zunamiContract,
   })).output / 10 ** (zunamiHoldingsDecimals - usdtDecimals);
