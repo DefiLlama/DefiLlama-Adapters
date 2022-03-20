aee7c33585b1ee1082f86c0b38984d48ae1e6f3f
Frederick Ngoiya <null@DESKTOP-MBG29RS.localdomain>
2022-03-20 13:30:41 +0300

    kaddex/indexjs

--- projects/kaddex/index.js
+++ projects/kaddex/index.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const Pact = require("pact-lang-api");
 const { toUSDTBalances } = require("../helper/balances");
 
-const chainId = "3";
+const chainId = "2";
 const network = `https://api.chainweb.com/chainweb/0.0/mainnet01/chain/${chainId}/pact`;
 const GAS_PRICE = 0.00000001;
 const creationTime = () => Math.round(new Date().getTime() / 1000) - 10;
@@ -32,13 +32,13 @@ const getPairList = async () => {
       let data = await Pact.fetch.local(
         {
           pactCode: `
-            (use free.exchange)
+            (use kswap.exchange)
             (let*
               (
                 (p (get-pair ${pair.token0.code} ${pair.token1.code}))
                 (reserveA (reserve-for p ${pair.token0.code}))
                 (reserveB (reserve-for p ${pair.token1.code}))
-                (totalBal (free.tokens.total-supply (free.exchange.get-pair-key ${pair.token0.code} ${pair.token1.code})))
+                (totalBal (kswap.tokens.total-supply (kswap.exchange.get-pair-key ${pair.token0.code} ${pair.token1.code})))
               )[totalBal reserveA reserveB])
              `,
           meta: Pact.lang.mkMeta(
@@ -52,8 +52,9 @@ const getPairList = async () => {
         },
         network
       );
-
+       
       return {
+        
         reserves: [
           getReserve(data.result.data[1]),
           getReserve(data.result.data[2]),
@@ -86,13 +87,8 @@ async function fetch() {
   const kdaPrice = await fetchKdaPrice();
   const kdaTotal = await fetchKdaTotal(pairList);
   const kdaInFluxPair = pairList[0].reserves[0];
-  
-
-  
-  const tvl =
-    kdaPrice *
-    (kdaTotal + kdaInFluxPair);
-    return toUSDTBalances(tvl);
+  const tvl=kdaPrice * (kdaTotal + kdaInFluxPair);
+  return toUSDTBalances(tvl);
 }
 
 module.exports = {
