7092aac0f8932c4bc53200b0a01b4c1579176cc2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-03-22 18:16:55 +0100

    query uniswap pair data in chunks

--- projects/helper/calculateUniTvl.js
+++ projects/helper/calculateUniTvl.js
@@ -5,6 +5,7 @@ const token1 = require('./abis/token1.json');
 const getReserves = require('./abis/getReserves.json');
 const factoryAbi = require('./abis/factory.json');
 const {getBlock} = require('./getBlock')
+const { getChainTransform } = require("./portedTokens");
 
 async function calculateUniTvl(getAddress, block, chain, FACTORY, START_BLOCK, useMulticall = false, abis={
   getReserves
@@ -21,16 +22,24 @@ async function calculateUniTvl(getAddress, block, chain, FACTORY, START_BLOCK, u
       throw new Error("allPairsLength() failed")
     }
     const pairNums = Array.from(Array(Number(pairLength)).keys())
-    const pairs = (await sdk.api.abi.multiCall({
-      abi: factoryAbi.allPairs,
-      chain,
-      calls: pairNums.map(num => ({
-        target: FACTORY,
-        params: [num]
-      })),
-      block,
-      requery: true
-    })).output
+    const pairs = []
+
+    const chunkSize = 100;
+    for (let i = 0; i < pairNums.length; i += chunkSize) {
+      const pairsChunk = pairNums.slice(i, i + chunkSize);
+      const pairsChunkResponse = (await sdk.api.abi.multiCall({
+        abi: factoryAbi.allPairs,
+        chain,
+        calls: pairsChunk.map(num => ({
+          target: FACTORY,
+          params: [num]
+        })),
+        block,
+        requery: true
+      })).output
+      pairs.push(...pairsChunkResponse)
+    }
+    
     pairAddresses = pairs.map(result => result.output.toLowerCase())
   } else {
     const logs = (
@@ -54,106 +63,113 @@ async function calculateUniTvl(getAddress, block, chain, FACTORY, START_BLOCK, u
       .map((pairAddress) => pairAddress.toLowerCase());
   }
 
-  const [token0Addresses, token1Addresses, reserves] = await Promise.all([
-    sdk.api.abi
-      .multiCall({
-        abi: token0,
-        chain,
-        calls: pairAddresses.map((pairAddress) => ({
-          target: pairAddress,
-        })),
-        block,
-        requery: true
-      })
-      .then(({ output }) => output),
-    sdk.api.abi
-      .multiCall({
-        abi: token1,
-        chain,
-        calls: pairAddresses.map((pairAddress) => ({
-          target: pairAddress,
-        })),
-        block,
-        requery: true
-      })
-      .then(({ output }) => output),
-    sdk.api.abi
-      .multiCall({
-        abi: abis.getReserves,
-        chain,
-        calls: pairAddresses.map((pairAddress) => ({
-          target: pairAddress,
-        })),
-        block,
-        requery: true
-      }).then(({ output }) => output),
-  ]);
-
-  const pairs = {};
-  // add token0Addresses
-  token0Addresses.forEach((token0Address) => {
-    const tokenAddress = token0Address.output.toLowerCase();
-
-    const pairAddress = token0Address.input.target.toLowerCase();
-    pairs[pairAddress] = {
-      token0Address: getAddress(tokenAddress),
-    }
-  });
-
-  // add token1Addresses
-  token1Addresses.forEach((token1Address) => {
-    const tokenAddress = token1Address.output.toLowerCase();
-    const pairAddress = token1Address.input.target.toLowerCase();
-    pairs[pairAddress] = {
-      ...(pairs[pairAddress] || {}),
-      token1Address: getAddress(tokenAddress),
-    }
-  });
-
-  const balances = reserves.reduce((accumulator, reserve, i) => {
-      const pairAddress = reserve.input.target.toLowerCase();
-      const pair = pairs[pairAddress] || {};
-
-      // handle reserve0
-      if (pair.token0Address) {
-        const reserve0 = new BigNumber(reserve.output['0']);
-        if (!reserve0.isZero()) {
-          const existingBalance = new BigNumber(
-            accumulator[pair.token0Address] || '0'
-          );
-
-          accumulator[pair.token0Address] = existingBalance
-            .plus(reserve0)
-            .toFixed()
-        }
-      }
-
-      // handle reserve1
-      if (pair.token1Address) {
-        const reserve1 = new BigNumber(reserve.output['1']);
-
-        if (!reserve1.isZero()) {
-          const existingBalance = new BigNumber(
-            accumulator[pair.token1Address] || '0'
-          );
-
-          accumulator[pair.token1Address] = existingBalance
-            .plus(reserve1)
-            .toFixed()
-        }
-      }
-
-    return accumulator
-  }, {})
+  const balances = {}
+  const addressesChunkSize = 100;
+  for (let i = 0; i < pairAddresses.length; i += addressesChunkSize)
+    await addToBalances(balances, pairAddresses.slice(i, i + addressesChunkSize))
 
   return balances
+
+  async function addToBalances(balances, pairAddresses) {
+    const [token0Addresses, token1Addresses, reserves] = await Promise.all([
+      sdk.api.abi
+        .multiCall({
+          abi: token0,
+          chain,
+          calls: pairAddresses.map((pairAddress) => ({
+            target: pairAddress,
+          })),
+          block,
+          requery: true
+        })
+        .then(({ output }) => output),
+      sdk.api.abi
+        .multiCall({
+          abi: token1,
+          chain,
+          calls: pairAddresses.map((pairAddress) => ({
+            target: pairAddress,
+          })),
+          block,
+          requery: true
+        })
+        .then(({ output }) => output),
+      sdk.api.abi
+        .multiCall({
+          abi: abis.getReserves,
+          chain,
+          calls: pairAddresses.map((pairAddress) => ({
+            target: pairAddress,
+          })),
+          block,
+          requery: true
+        }).then(({ output }) => output),
+    ]);
+  
+    const pairs = {};
+    // add token0Addresses
+    token0Addresses.forEach((token0Address) => {
+      const tokenAddress = token0Address.output.toLowerCase();
+  
+      const pairAddress = token0Address.input.target.toLowerCase();
+      pairs[pairAddress] = {
+        token0Address: getAddress(tokenAddress),
+      }
+    });
+  
+    // add token1Addresses
+    token1Addresses.forEach((token1Address) => {
+      const tokenAddress = token1Address.output.toLowerCase();
+      const pairAddress = token1Address.input.target.toLowerCase();
+      pairs[pairAddress] = {
+        ...(pairs[pairAddress] || {}),
+        token1Address: getAddress(tokenAddress),
+      }
+    });
+  
+    return reserves.reduce((accumulator, reserve, i) => {
+        const pairAddress = reserve.input.target.toLowerCase();
+        const pair = pairs[pairAddress] || {};
+  
+        // handle reserve0
+        if (pair.token0Address) {
+          const reserve0 = new BigNumber(reserve.output['0']);
+          if (!reserve0.isZero()) {
+            const existingBalance = new BigNumber(
+              accumulator[pair.token0Address] || '0'
+            );
+  
+            accumulator[pair.token0Address] = existingBalance
+              .plus(reserve0)
+              .toFixed()
+          }
+        }
+  
+        // handle reserve1
+        if (pair.token1Address) {
+          const reserve1 = new BigNumber(reserve.output['1']);
+  
+          if (!reserve1.isZero()) {
+            const existingBalance = new BigNumber(
+              accumulator[pair.token1Address] || '0'
+            );
+  
+            accumulator[pair.token1Address] = existingBalance
+              .plus(reserve1)
+              .toFixed()
+          }
+        }
+  
+      return accumulator
+    }, balances)
+  }
 };
 
 function uniTvlExport(factory, chain, transformAddressOriginal=undefined, abis){
   return async (timestamp, _ethBlock, chainBlocks)=>{
     let transformAddress;
     if(transformAddressOriginal === undefined){
-      transformAddress = addr=>`${chain}:${addr}`;
+      transformAddress = await getChainTransform(chain)
     }else{
       transformAddress = await transformAddressOriginal()
     }
--- projects/pippifinance/index.js
+++ projects/pippifinance/index.js
@@ -1,19 +1,10 @@
 const sdk = require("@defillama/sdk");
-const { transformHecoAddress } = require("../helper/portedTokens");
-const {calculateUniTvl} = require('../helper/calculateUniTvl.js')
+const { uniTvlExport } = require('../helper/calculateUniTvl.js')
 
-const MASTERCHEF_CONTRACT = "0xa02fF30986211B7ca571AcAE5AD4D25ab1e58426";
-const PIPPI_FACTORY  = "0x979efE7cA072b72d6388f415d042951dDF13036e";
-
-const hecoTvl = async (timestamp, ethBlock, chainBlocks) => {
-
-    const transformAddress = await transformHecoAddress();
-
-    return calculateUniTvl(transformAddress, chainBlocks['heco'], 'heco', PIPPI_FACTORY, 0, true);
-}
+const PIPPI_FACTORY = "0x979efE7cA072b72d6388f415d042951dDF13036e";
 
 module.exports = {
-    heco: {
-        tvl: hecoTvl,
-    },
+	heco: {
+		tvl: uniTvlExport(PIPPI_FACTORY, "heco")
+	},
 };
