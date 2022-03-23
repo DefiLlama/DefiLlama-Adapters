79c123e738f6a5b4b62378fdb13a94e18219305b
Dylan Davis <dylandav@usc.edu>
2022-03-23 03:16:39 -0500

    Removed staking and pool2 -- this is done by a partner for now

--- projects/penguin-finance/index.js
+++ projects/penguin-finance/index.js
@@ -6,8 +6,6 @@ const { MintLayout, TOKEN_PROGRAM_ID } = require("@solana/spl-token");
 const SOLANA_API_URL = "https://solana-api.projectserum.com";
 const PENGUIN_SWAP_PROGRAM_ADDRESS =
   "PSwapMdSai8tjrEXcxFeQth87xC4rRsa4VA5mhGhXkP";
-const STAKING_ACCOUNT_ADDRESS = "F7AkpGvjsXaUVW49mMMgyKQzToFxJvHSZjwB6EUta1W";
-const BUD_TOKEN_SYMBOL = "BUD";
 
 const TokenSwapLayout = BufferLayout.struct([
   BufferLayout.u8("version"),
@@ -34,39 +32,7 @@ const TokenSwapLayout = BufferLayout.struct([
 
 const connection = new Connection(SOLANA_API_URL);
 
-// Derives BUD price as a ratio compared to another token it is paired with that has been listed on Coingecko
-function getBUDPrice(poolsTokenAccounts, tokenMap, validTokenAddresses) {
-  for (tokenAccounts of poolsTokenAccounts) {
-    const poolTokens = tokenAccounts.value.filter((account) =>
-      validTokenAddresses.has(account.account.data.parsed.info.mint)
-    );
-
-    const token1 = tokenMap.get(poolTokens[0].account.data.parsed.info.mint);
-    const token2 = tokenMap.get(poolTokens[1].account.data.parsed.info.mint);
-    const amount1 = poolTokens[0].account.data.parsed.info.tokenAmount.uiAmount;
-    const amount2 = poolTokens[1].account.data.parsed.info.tokenAmount.uiAmount;
-
-    // Checks if BUD has been added to Coingecko after the time this code was written
-    if (token1.symbol === BUD_TOKEN_SYMBOL && token1.extensions.coingeckoId) {
-      return { rate: amount1, symbol: token1.extensions.coingeckoId };
-    }
-    if (token2.symbol === BUD_TOKEN_SYMBOL && token2.extensions.coingeckoId) {
-      return { rate: amount2, symbol: token2.extensions.coingeckoId };
-    }
-
-    if (token1.symbol === BUD_TOKEN_SYMBOL && token2.extensions.coingeckoId) {
-      return { rate: amount2 / amount1, symbol: token2.extensions.coingeckoId };
-    }
-
-    if (token2.symbol === BUD_TOKEN_SYMBOL && token1.extensions.coingeckoId) {
-      return { rate: amount1 / amount2, symbol: token1.extensions.coingeckoId };
-    }
-  }
-
-  return { rate: undefined, symbol: undefined };
-}
-
-async function getSharedState() {
+async function tvl(time, ethBlock, chainBlocks) {
   const tokenList = await new TokenListProvider()
     .resolve()
     .then((tokens) => tokens.filterByClusterSlug("mainnet-beta").getList());
@@ -115,111 +81,47 @@ async function getSharedState() {
     )
   );
 
-  return { validTokenAddresses, tokenMap, poolsTokenAccounts };
-}
+  const balances = {};
 
-async function staking(time, ethBlock, chainBlocks) {
-  const { validTokenAddresses, tokenMap, poolsTokenAccounts } =
-    await getSharedState();
-
-  const stakingAccountPubKey = new PublicKey(STAKING_ACCOUNT_ADDRESS);
-  const stakingAccount = await connection.getParsedAccountInfo(
-    stakingAccountPubKey
-  );
-
-  const { symbol, rate } = getBUDPrice(
-    poolsTokenAccounts,
-    tokenMap,
-    validTokenAddresses
-  );
-
-  return {
-    [symbol]: rate * stakingAccount.value.data.parsed.info.tokenAmount.uiAmount,
-  };
-}
-
-function getFilteredPools(
-  poolsTokenAccounts,
-  validTokenAddresses,
-  tokenMap,
-  calculatePool2
-) {
-  return poolsTokenAccounts.filter((tokenAccounts) => {
+  poolsTokenAccounts.forEach((tokenAccounts) => {
     const poolTokens = tokenAccounts.value.filter((account) =>
       validTokenAddresses.has(account.account.data.parsed.info.mint)
     );
 
-    const symbol1 = tokenMap.get(
-      poolTokens[0].account.data.parsed.info.mint
-    ).symbol;
-    const symbol2 = tokenMap.get(
-      poolTokens[1].account.data.parsed.info.mint
-    ).symbol;
+    const token1 = tokenMap.get(poolTokens[0].account.data.parsed.info.mint);
+    const token2 = tokenMap.get(poolTokens[1].account.data.parsed.info.mint);
+    const symbol1 = token1.extensions.coingeckoId;
+    const symbol2 = token2.extensions.coingeckoId;
 
-    if (calculatePool2) {
-      return symbol1 === BUD_TOKEN_SYMBOL || symbol2 === BUD_TOKEN_SYMBOL;
-    }
+    let amount1 = poolTokens[0].account.data.parsed.info.tokenAmount.uiAmount;
+    let amount2 = poolTokens[1].account.data.parsed.info.tokenAmount.uiAmount;
 
-    return symbol1 !== BUD_TOKEN_SYMBOL && symbol2 !== BUD_TOKEN_SYMBOL;
-  });
-}
-
-function tvl(calculatePool2) {
-  return async (time, ethBlock, chainBlocks) => {
-    const { validTokenAddresses, tokenMap, poolsTokenAccounts } =
-      await getSharedState();
-
-    const filteredPools = getFilteredPools(
-      poolsTokenAccounts,
-      validTokenAddresses,
-      tokenMap,
-      calculatePool2
-    );
-
-    const balances = {};
-
-    filteredPools.forEach((tokenAccounts) => {
-      const poolTokens = tokenAccounts.value.filter((account) =>
-        validTokenAddresses.has(account.account.data.parsed.info.mint)
-      );
-
-      const token1 = tokenMap.get(poolTokens[0].account.data.parsed.info.mint);
-      const token2 = tokenMap.get(poolTokens[1].account.data.parsed.info.mint);
-      const symbol1 = token1.extensions.coingeckoId;
-      const symbol2 = token2.extensions.coingeckoId;
-
-      let amount1 = poolTokens[0].account.data.parsed.info.tokenAmount.uiAmount;
-      let amount2 = poolTokens[1].account.data.parsed.info.tokenAmount.uiAmount;
-
-      // Future proofing - only add this pool's tokens to balances if one of the pool's tokens are listed on Coingecko
-      // As of March 23, 2022 all of Penguin Finance pools have at least one token listed on Coingecko
-      if (symbol1 || symbol2) {
-        // If one of the pool's tokens is not on Coingecko we will double the amount of the other token to get a pool TVL estimation
-        if (!symbol1 && symbol2) {
-          amount2 *= 2;
-          balances[symbol2] = (balances[symbol2] ?? 0) + amount2;
-        } else if (symbol1 && !symbol2) {
-          amount1 *= 2;
-          balances[symbol1] = (balances[symbol1] ?? 0) + amount1;
-        } else {
-          balances[symbol1] = (balances[symbol1] ?? 0) + amount1;
-          balances[symbol2] = (balances[symbol2] ?? 0) + amount2;
-        }
+    // Future proofing - only add this pool's tokens to balances if one of the pool's tokens are listed on Coingecko
+    // As of March 23, 2022 all of Penguin Finance pools have at least one token listed on Coingecko
+    if (symbol1 || symbol2) {
+      // If one of the pool's tokens is not on Coingecko we will double the amount of the other token to get a pool TVL estimation
+      if (!symbol1 && symbol2) {
+        amount2 *= 2;
+        balances[symbol2] = (balances[symbol2] ?? 0) + amount2;
+      } else if (symbol1 && !symbol2) {
+        amount1 *= 2;
+        balances[symbol1] = (balances[symbol1] ?? 0) + amount1;
+      } else {
+        balances[symbol1] = (balances[symbol1] ?? 0) + amount1;
+        balances[symbol2] = (balances[symbol2] ?? 0) + amount2;
       }
-    });
+    }
+  });
 
-    return balances;
-  };
+  return balances;
 }
 
 module.exports = {
   timetravel: false,
   misrepresentedTokens: true,
   methodology:
-    "TVL includes the total token value inside the protocol's liquidity pools and governance token staking contract. Pools that have one side BUD token have been separated into the pool2 filter.",
+    "TVL includes the total token value inside the protocol's liquidity pools.",
   solana: {
-    staking,
-    tvl: tvl(false),
-    pool2: tvl(true),
+    tvl,
   },
 };
