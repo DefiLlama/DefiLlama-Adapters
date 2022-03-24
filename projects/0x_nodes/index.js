dd52c2122636fe7db233d9217494c16b0efef812
August G <august.gran@gmail.com>
2022-03-25 00:05:52 +0100

    Create code structure, similar to curve's index.js

--- projects/0x_nodes/index.js
+++ projects/0x_nodes/index.js
@@ -1,31 +1,80 @@
-const sdk = require('@defillama/sdk');
-const { transformBscAddress } = require('../helper/portedTokens');
-const MINT_TOKEN_CONTRACT = '0x1f3Af095CDa17d63cad238358837321e95FC5915';
-const MINT_CLUB_BOND_CONTRACT = '0x8BBac0C7583Cc146244a18863E708bFFbbF19975';
+// almost a hardcopy of the curve adapter index.js structure, maybe we could use a similar structure? just hook the functions up to our API
+const utils = require('../helper/utils');
 
-async function tvl(timestamp, block, chainBlocks) {
-  const balances = {};
-  const transform = await transformBscAddress();
-
-  const collateralBalance = (await sdk.api.abi.call({
-    abi: 'erc20:balanceOf',
-    chain: 'bsc',
-    target: MINT_TOKEN_CONTRACT,
-    params: [MINT_CLUB_BOND_CONTRACT],
-    block: chainBlocks['bsc'],
-  })).output;
-
-  await sdk.util.sumSingleBalance(balances, transform(MINT_TOKEN_CONTRACT), collateralBalance)
-
-  return balances;
+//one function per chain and strategy(?) Can probably make a Iteration on this later using a URL and chain array.
+async function eth() {
+  const tvl = await utils.fetchURL('https://api.curve.fi/api/getTVLPolygon') //get from our API via URL? need to replace these with ours obviously.
+  console.log(tvl.data.data.tvl)
+  return tvl.data.data.tvl
+}
+async function bsc() {
+  const tvl = await utils.fetchURL('') //curve isnt on bsc
+  return tvl.data.data.tvl
+}
+async function polygon() {
+  const tvl = await utils.fetchURL('https://api.curve.fi/api/getTVLPolygon')
+  return tvl.data.data.tvl
+}
+async function fantom() {
+  const tvl = await utils.fetchURL('https://api.curve.fi/api/getTVLFantom')
+  return tvl.data.data.tvl
+}
+async function andromeda() {
+  const tvl = await utils.fetchURL('') //curve isnt on andromeda
+  return tvl.data.data.tvl
+}
+async function avax() {
+  const tvl = await utils.fetchURL('https://api.curve.fi/api/getTVLAvalanche')
+  return tvl.data.data.tvl
+}
+//example strategy functions
+async function saal_main() {
+  const tvl = 3443234.02; //somehow wire up our API here and return strategy tvl to this variable.Via URL like above?
+  return tvl
+}
+async function saal_gnb() {
+  const tvl = 6443234.02;
+  return tvl
 }
 
+//fetching chains and strategies all together when called. Can probably use Iteration on this later using a chain array.
+async function fetch() {
+  return (await eth()) +
+   // (await bsc()) +
+   (await polygon()) +
+   (await fantom()) +
+   // (await andromeda()) +
+   (await avax()) +
+   (await saal_main()) +
+   (await saal_gnb())
+
+}
+//the module export construction, so it can make readable numbers, put them into the right place and calc "combined TVL":
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  methodology: 'counts the number of MINT tokens in the Club Bonding contract.'
-  start: 1000235,
-  bsc: {
-    tvl,
-  }
-}; 
+  timetravel: false,
+  misrepresentedTokens: true,
+
+  //takes out from the fetch function call done at the bottom here and places returned tvl in the right place. Iteration?
+  ethereum:{
+    fetch: eth,
+  },
+  // bsc:{
+  //   fetch: bsc
+  // },
+  polygon:{
+    fetch:polygon
+  },
+  // andromeda:{
+  //   fetch:andromeda
+  // },
+  avalanche:{
+    fetch: avax
+  },
+  SAAL_main:{
+    fetch: saal_main
+  },
+  SAAL_gnb:{
+    fetch: saal_gnb
+  },
+  fetch
+}
--- projects/0x_nodes/readme.md
+++ projects/0x_nodes/readme.md
@@ -1,1 +1,6 @@
-init adapter
+How to test if TVLs is showing correctly:
+
+Once you are done writing it you can verify that it returns the correct value by running the following code:
+
+$ npm install
+$ node test.js projects/0x_nodes/index.js
