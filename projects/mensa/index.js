44344d6545b55e9cd42e8871da875c1d3966fcbc
realShaman <realShaman@protonmail.com>
2022-03-25 01:12:59 +0000

    add rekt: true export

--- projects/mensa/index.js
+++ projects/mensa/index.js
@@ -75,6 +75,7 @@ async function tvl(_, _ethBlock, chainBlocks) {
 }
 
 module.exports = {
+  rekt: true,
   misrepresentedTokens: true,
   methodology: 'Using the same methodology applied to other lending platforms, TVL for Mensa consists deposits made to the protocol and borrowed tokens are not counted.',
   fantom:{
