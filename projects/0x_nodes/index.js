541e3720e7a18baec5fc3b8327051aff896a980c
August G <august.gran@gmail.com>
2022-03-28 00:11:49 +0200

    added graphql calls via URL. missing tvl for bsc, poly, ftm, metis, avax

--- projects/0x_nodes/index.js
+++ projects/0x_nodes/index.js
@@ -1,52 +1,40 @@
 // almost a hardcopy of the curve adapter index.js structure, maybe we could use a similar structure? just hook the functions up to our API
-
 //functions written by defiLlama
 const utils = require('../helper/utils');
 const { staking } = require('../helper/staking');
-var tvl;
-//one function per chain, Can probably make a Iteration on this later an chain array.
-async function eth() {
-  const tvl_strategies = [500000, 20000]; //replace this with an array with tvls from the strats using our API
-  tvl = 0;
-  tvl_strategies.forEach(sum);
-  return tvl
+var url = 'https://system11-api-kwhdxoi2yq-uw.a.run.app/graphql?query='
+// var query =  `query {networkStats(chainId: ${chainId}) {tvl}}` hoping to make the query so it has a variable field being chainId
+
+async function eth(/*chainId*/) { //chainId ends up as something crazy like 1648417255 even if I pass '1' when calling it at line 50. not sure why.
+  const r = await utils.fetchURL(url+'query {networkStats(chainId:1) {tvl}}')
+  // const r = await utils.fetchURL(url+query) .
+  return r.data.data.networkStats.tvl / 1e18
   }
-
-async function bsc() {
-  const tvl_strategies = [500000, 20000];
-  tvl = 0;
-  tvl_strategies.forEach(sum);
-  return tvl
-
+async function bsc(chainId) {
+  const r = await utils.fetchURL(url+'query {networkStats(chainId:56) {tvl}}') // no other than mainnet spits out TVL? just gets null.
+  // return r.data.data.networkStats.tvl / 1e18
+  return 1000002
 }
-async function polygon() {
-  const tvl_strategies = [500000, 20000];
-  tvl = 0;
-  tvl_strategies.forEach(sum);
-  return tvl
+async function polygon(chainId) {
+  const r = await utils.fetchURL(url+'query {networkStats(chainId:137) {tvl}}')
+  // return r.data.data.networkStats.tvl / 1e18
+  return 1000002
 }
-
-async function fantom() {
-  const tvl_strategies = [500000, 20000];
-  tvl = 0;
-  tvl_strategies.forEach(sum);
-  return tvl
+async function fantom(chainId) {
+  const r = await utils.fetchURL(url+'query {networkStats(chainId:250) {tvl}}')
+  // return r.data.data.networkStats.tvl / 1e18
+  return 1000002
 }
-
-async function andromeda() {
-  const tvl_strategies = [500000, 20000];
-  tvl = 0;
-  tvl_strategies.forEach(sum);
-  return tvl
+async function andromeda(chainId) {
+  const r = await utils.fetchURL(url+'query {networkStats(chainId:1088) {tvl}}')
+  // return r.data.data.networkStats.tvl / 1e18
+  return 1000002
 }
-
-async function avax() {
-  const tvl_strategies = [500000, 20000];
-  tvl = 0;
-  tvl_strategies.forEach(sum);
-  return tvl
+async function avax(chainId) {
+  const r = await utils.fetchURL(url+'query {networkStats(chainId:431134) {tvl}}')
+  // return r.data.data.networkStats.tvl / 1e18
+  return 1000002
 }
-
 // 0xnodes treasury
 async function treasury() {
   const tvl = 1256789
@@ -60,57 +48,29 @@ async function pfa() {
 function sum(value) {
   tvl = tvl + value;
 }
-
-//fetching chains and strategies all together when called. Can probably use Iteration on this later using a chain array.
+//+ing all TVLs all together when called. Can probably use some sorta iteration on this later using a chain array.(?)
 async function fetch() {
-  return (await eth()) +
-  (await bsc()) +
-  (await polygon()) +
-  (await fantom()) +
-  (await andromeda()) +
-  (await avax()) +
-  (await treasury()) +
-  (await pfa())
-
+  return (await eth(/*chainId = '1'*/)) + (await bsc(/*chainId = '56'*/)) + (await polygon(/*chainId = '137'*/)) +
+  (await fantom(/*chainId = '250'*/)) + (await andromeda(/*chainId = '1088'*/)) + (await avax(/*chainId = '431134'*/)) +
+  (await treasury()) + (await pfa())
 }
-
-//the module export construction, so it can make readable numbers, put them into the right place and calc "combined TVL":
+//the module export construction:  makes the TVLs into readable numbers and sums them together.
 module.exports = {
   timetravel: false,
   misrepresentedTokens: true,
-  methodology: 'TVL based on Treasury, staking assets (PFA), {{ nubmer_of_strategies }} yield strategies and a total of {{ number_of_chains }} chains',
-  //takes out from the fetch function call done at the bottom here and places returned tvl in the right place. Iteration?
-  ethereum:{
-    fetch: eth,
-    // staking: staking("0xAACa86B876ca011844b5798ECA7a67591A9743C8", "0xcfcff4eb4799cda732e5b27c3a36a9ce82dbabe0")
-  },
-  bsc:{
-    fetch: bsc
-  },
-  polygon:{
-    fetch:polygon
-  },
-  fantom:{
-    fetch:fantom
-  },
-  andromeda:{
-    fetch:andromeda
-  },
-  avalanche:{
-    fetch: avax
-  },
-  treasury:{
-    fetch: treasury
-  },
-  PFA:{
-    fetch: pfa
-  },
+  methodology: 'TVL based on Treasury, staking assets (PFA), {{ nubmer_of_strategies }} yield strategies and on a total of {{ number_of_chains }} chains',
+  //takes out from the fetch function call and places returned tvl in the right place.
+  ethereum:{fetch: eth},
+  bsc:{fetch: bsc},
+  polygon:{fetch:polygon},
+  fantom:{fetch:fantom},
+  andromeda:{fetch:andromeda},
+  avalanche:{fetch: avax},
+  treasury:{fetch: treasury},
+  PFA:{fetch: pfa},
   fetch
 }
-
 /* How to test if TVLs is showing correctly:
-
 Once you are done writing it you can verify that it returns the correct value by running the following code:
-
 $ npm install
 $ node test.js projects/0x_nodes/index.js */
