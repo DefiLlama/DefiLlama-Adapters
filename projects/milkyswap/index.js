9d15da782e7921ea77535a281357c582ad11adc2
realShaman <realShaman@protonmail.com>
2022-03-30 00:27:03 +0100

    change chain export to milkomeda

--- projects/milkyswap/index.js
+++ projects/milkyswap/index.js
@@ -4,7 +4,7 @@ module.exports = {
   misrepresentedTokens: true,
   methodology:
     "Factory address (0xD6Ab33Ad975b39A8cc981bBc4Aaf61F957A5aD29) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
-  cronos: {
+  milkomeda: {
     tvl: calculateUsdUniTvl(
       "0xD6Ab33Ad975b39A8cc981bBc4Aaf61F957A5aD29",
       "milkomeda",
