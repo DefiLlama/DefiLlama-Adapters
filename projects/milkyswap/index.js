a753b0c338172d4fef7f68d8fc31a4b49421ea30
realShaman <realShaman@protonmail.com>
2022-03-30 00:26:22 +0100

    whitelist weth and wbtc

--- projects/milkyswap/index.js
+++ projects/milkyswap/index.js
@@ -12,6 +12,9 @@ module.exports = {
       [
         "0x063A5E4cD5e15ac66ea47134Eb60e6b30A51B2bf",
         "0xB44a9B6905aF7c801311e8F4E76932ee959c663C",
+        "0x80A16016cC4A2E6a2CACA8a4a498b1699fF0f844",
+        "0x6aB6d61428fde76768D7b45D8BFeec19c6eF91A8",
+        "0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D"
       ],
       "cardano"
     ),
