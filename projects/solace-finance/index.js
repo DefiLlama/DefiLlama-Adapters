f95faaf2638b9395bc263959a22439175dffd8ef
selcuk sozuer <selcuk.sozuer@gmail.com>
2022-03-29 13:35:40 +0300

    Defillama adapter chain separation

--- projects/solace-finance/index.js
+++ projects/solace-finance/index.js
@@ -1,13 +1,24 @@
 const retry = require("async-retry")
 const axios = require("axios");
 
-async function fetch() {
+async function tvl(chain) {
   // github repository https://github.com/solace-fi/solace-stats
-  var res = await retry(async bail => await axios.get('https://stats.solace.fi/tvl'));
+  var res = await retry(async bail => await axios.get(`https://stats.solace.fi/tvl?chain=${chain}`));
   var tvl = parseFloat(res["data"]["tvl_usd"]);
   return tvl;
 }
 
 module.exports = {
-  fetch
+  ethereum: {
+    tvl("mainnet")
+  },
+  bsc: {
+    tvl("bsc")
+  },
+  aurora: {
+    tvl("aurora")
+  },
+  polygon: {
+    tvl("polygon")
+  }
 }
