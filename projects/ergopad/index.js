8ac471e9657aa6c88c7dddf15c727566158ea9fb
Luivatra <luivatra@gmail.com>
2022-03-30 21:23:38 +0200

    fixed import

--- projects/ergopad/index.js
+++ projects/ergopad/index.js
@@ -1,5 +1,5 @@
-const utils = require('./helper/utils');
-const {toUSDTBalances} = require('./helper/balances')
+const utils = require('../helper/utils');
+const {toUSDTBalances} = require('../helper/balances')
 
 async function tvl() {
   var totalTvl = await utils.fetchURL('https://ergopad.io/api/blockchain/tvl/d71693c49a84fbbecd4908c94813b46514b18b67a99952dc1e6e4791556de413');
