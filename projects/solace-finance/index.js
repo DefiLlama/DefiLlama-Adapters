03f5f5bcfe7c5e248b75a9361744a8748ea75e12
selcuk sozuer <selcuk.sozuer@gmail.com>
2022-03-30 21:52:07 +0300

    test fixes

--- projects/solace-finance/index.js
+++ projects/solace-finance/index.js
@@ -1,24 +1,46 @@
 const retry = require("async-retry")
 const axios = require("axios");
 
-async function tvl(chain) {
-  // github repository https://github.com/solace-fi/solace-stats
-  var res = await retry(async bail => await axios.get(`https://stats.solace.fi/tvl?chain=${chain}`));
+
+async function fetch() {
+  // github repository https://github.com/solace-fi/solace-stats/tree/main/api/tvl
+  var res = await retry(async bail => await axios.get(`https://stats.solace.fi/tvl`));
   var tvl = parseFloat(res["data"]["tvl_usd"]);
   return tvl;
 }
 
 module.exports = {
   ethereum: {
-    tvl("mainnet")
+    fetch: async () => {
+        // github repository https://github.com/solace-fi/solace-stats/tree/main/api/tvl
+        var res = await retry(async bail => await axios.get(`https://stats.solace.fi/tvl?chain=mainnet`));
+        var tvl = parseFloat(res["data"]["tvl_usd"]);
+        return tvl;
+    }
   },
   bsc: {
-    tvl("bsc")
+    fetch: async () => {
+      // github repository https://github.com/solace-fi/solace-stats/tree/main/api/tvl
+      var res = await retry(async bail => await axios.get(`https://stats.solace.fi/tvl?chain=bsc`));
+      var tvl = parseFloat(res["data"]["tvl_usd"]);
+      return tvl;
+    }
   },
   aurora: {
-    tvl("aurora")
+    fetch: async () => {
+      // github repository https://github.com/solace-fi/solace-stats/tree/main/api/tvl
+      var res = await retry(async bail => await axios.get(`https://stats.solace.fi/tvl?chain=aurora`));
+      var tvl = parseFloat(res["data"]["tvl_usd"]);
+      return tvl;
+    }
   },
   polygon: {
-    tvl("polygon")
-  }
+    fetch: async () => {
+      // github repository https://github.com/solace-fi/solace-stats/tree/main/api/tvl
+      var res = await retry(async bail => await axios.get(`https://stats.solace.fi/tvl?chain=polygon`));
+      var tvl = parseFloat(res["data"]["tvl_usd"]);
+      return tvl;
+    }
+  },
+  fetch
 }
