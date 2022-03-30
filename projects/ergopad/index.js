3269dd077540245a8e26e0dbd0e8a94745c40dbf
Luivatra <luivatra@gmail.com>
2022-03-30 20:14:09 +0200

    add ergopad

---
+++ projects/ergopad/index.js
@@ -0,0 +1,16 @@
+const utils = require('./helper/utils');
+const {toUSDTBalances} = require('./helper/balances')
+
+async function tvl() {
+  var totalTvl = await utils.fetchURL('https://ergopad.io/api/blockchain/tvl/d71693c49a84fbbecd4908c94813b46514b18b67a99952dc1e6e4791556de413');
+  return toUSDTBalances(totalTvl.data.tvl.total);
+}
+
+module.exports = {
+    misrepresentedTokens: true,
+    timetravel: false,
+    ergo:{
+        tvl
+    },
+    methodology: `Ergopad TVL is achieved by making a call to its API: https://ergopad.io/api/blockchain/tvl/d71693c49a84fbbecd4908c94813b46514b18b67a99952dc1e6e4791556de413 and consists of both staked and vested ergopad tokens.`
+}
