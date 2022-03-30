e7197256754c2bf2b885e6d9104d9455b5c6fd9d
Jonathan Chemla <jchemla@iconem.com>
2022-03-30 19:59:59 +0200

    Remove Nested graphql calls
    Mid-april 2022, an update to the nested backend will allow retrieval of all token addresses held by all nested records nfts, without requiring this intensive on-chain call
    
    In the meantime, add a 200 limit calls to get the tokens.

--- projects/nested/index.js
+++ projects/nested/index.js
@@ -1,98 +1,14 @@
 const sdk = require('@defillama/sdk')
 const { getBlock } = require('../helper/getBlock')
-const { request, gql } = require("graphql-request")
 const {chainExports} = require('../helper/exports')
 const abi = require('./abi.json')
 
-
 const nestRecords_contract = '0x3Ee96E771D5E56b34245b023E8B31ffDf36dFafd'
 const nestReserve_contract = '0x150fb0Cfa5bF3D4023bA198C725b6DCBc1577f21'
+// const graphUrl = 'https://api.nested.finance/graphql'
+// Mid-april 2022, an update to the nested backend will allow retrieval of all token addresses held by all nested records nfts, without requiring this intensive on-chain call
 
-
-const graphUrl = 'https://api.nested.finance/graphql'
-const graphQuery = gql`
-query GET_NESTED_NFTS ($chainId: Chain, $offset: Int, $limit: Int) {
-  nfts (filter: {
-      order: replications,
-      isReplicate: false,
-      minUsdValue: 5,
-      limit: $limit,
-      offset: $offset, 
-      chain: $chainId
-  }) {
-    id
-    chain
-    name
-    owner
-    replicationCount
-    creationDate
-    holdings {
-        token {
-          id
-          symbol
-      }
-    }
-  }
-}
-`
-
-function chainTvl_graphql(chain) {
-  return async (timestamp, ethBlock, chainBlocks) => {
-    const block = await getBlock(timestamp, chain, chainBlocks, false)
-    const balances = {}
-    const transformAddress = (addr) => `${chain}:${addr}`
-    
-    const limit = 15
-    const chainId = chain == 'polygon'? 'poly' : chain // avax and bsc as-is
-    const allNestedNFTs = [];
-    const nft_ids = []
-    let offset = 0;
-    while ((offset !== -1) && offset <= 30) {
-      console.log('yo')
-      const { nfts: nestedNFTs_i } = await request(
-      // const nfts = await request(
-        graphUrl,
-        graphQuery, 
-        {chainId, offset, limit}
-      );
-      console.log(offset, 'nfts', nestedNFTs_i.length) // , nestedNFTs_i)
-      offset += limit
-      if (nestedNFTs_i && nestedNFTs_i.length > 0) {
-        allNestedNFTs.push(...nestedNFTs_i);
-        nft_ids.push(...allNestedNFTs.map(nft => nft.id))
-        // Could add another check on nestedNFTs_i.length to be == limit otherwise stop condition 
-      } else {
-        offset = -1;
-      }
-    }
-    console.log('allNestedNFTs', allNestedNFTs)
-    let recordsTokens = allNestedNFTs.map(nft => nft.holdings.map(
-      t => t.token.id
-    )).flat()
-    recordsTokens = recordsTokens.map(t => t.substring(t.indexOf(':') + 1))
-    const reserveTokens = [...new Set(recordsTokens)]
-    console.log('reserveTokens', reserveTokens)
-    
-    console.log('nft_ids', nft_ids)
-
-    
-
-    const tokenBalances = await sdk.api.abi.multiCall({
-      calls: reserveTokens.map(t => ({
-        target: t,
-        params: nestReserve_contract,
-      })),
-      abi: 'erc20:balanceOf',
-      chain,
-      block,
-    });
-    sdk.util.sumMultiBalanceOf(balances, tokenBalances, true, transformAddress);
-    
-    return balances
-  }
-}
-
-const max_calls = 100
+const max_calls = 50
 function chainTvl_onchain(chain) {
   return async (timestamp, ethBlock, chainBlocks) => {
     const block = await getBlock(timestamp, chain, chainBlocks, false)
@@ -135,6 +51,5 @@ function chainTvl_onchain(chain) {
   }
 }
 
-// module.exports = chainExports(chainTvl_graphql, ['polygon', 'bsc', 'avax'])
-module.exports = chainExports(chainTvl_graphql, ['bsc'])
+module.exports = chainExports(chainTvl_onchain, ['polygon', 'bsc', 'avax'])
 module.exports.methodology = 'Nested TVL consists of tokens held by NestedReserve, for which we can get the address using the nestedRecords contract'
