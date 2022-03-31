b7b6ddbb55e6464fa37bfeb5b24cdbfb79a92dbd
Jonathan Chemla <jchemla@iconem.com>
2022-03-31 13:35:15 +0200

    Query CVX intermediary addresses onchain so you can simply input the BaseRewardPool contract
    Query CVX intermediary addresses onchain so you can simply input the BaseRewardPool contract

--- projects/concave/index.js
+++ projects/concave/index.js
@@ -1,7 +1,8 @@
 const sdk = require('@defillama/sdk')
 const BigNumber = require("bignumber.js");
 const { ohmTvl } = require('../helper/ohm')
-const abi = require('./abi.json')
+const cvx_abi = require('./cvx_abi.json')
+const crv_abi = require('./crv_abi.json')
 
 // Treasury backing the CNV price, similar to OHM so using the ohm wrapper
 const treasury = '0x226e7af139a0f34c6771deb252f9988876ac1ced' 
@@ -13,14 +14,15 @@ const treasuryTokens = [
     ['0x0ab87046fBb341D058F17CBC4c1133F25a20a52f', false], //gOHM
 ]
 
+// Generic CRV position unwrapping, useful for a CVX position unwrapping
+const cvxBoosterAddress = "0xF403C135812408BFbE8713b5A23a04b3D48AAE31";
 // CVX treasury position parameters
 const cvxUST_whv23CRV_BaseRewardPool = '0x7e2b9b5244bcfa5108a76d5e7b507cfd5581ad4a'
 const cvxUST_whv23CRV_f = '0x2d2006135e682984a8a2eb74f5c87c2251cc71e9' // CVX LP, base reward pool holds 100% of that token plus some crv. Can be queried via stakingToken method on baseRewardPool
 const UST_whv23CRV_f = '0xceaf7747579696a2f0bb206a14210e3c9e6fb269' // Crv LP, Best would be to derive it from the baseRewardPool or cvx contract
 
 
-// Generic CRV position unwrapping, useful for a CVX position unwrapping
-const abi_crvLP_coins = {'stateMutability':'view','type':'function','name':'coins','inputs':[{'name':'arg0','type':'uint256'}],'outputs':[{'name':'','type':'address'}],'gas':3123}
+
 async function genericUnwrapCrv(balances, crvToken, lpBalance, block, chain) {
   const {output: resolvedCrvTotalSupply} = await sdk.api.erc20.totalSupply({
     target: crvToken,
@@ -37,7 +39,7 @@ async function genericUnwrapCrv(balances, crvToken, lpBalance, block, chain) {
   const LP_tokens_count = 1 + (crv_symbol.match(/_/g) || []).length
   const coins_indices = Array.from(Array(LP_tokens_count).keys())
   const coins = (await sdk.api.abi.multiCall({
-    abi: abi_crvLP_coins, 
+    abi: crv_abi['crvLP_coins'], 
     calls: coins_indices.map(i => ({params: [i]})),
     target: crvToken,
     chain,
@@ -60,6 +62,38 @@ async function genericUnwrapCrv(balances, crvToken, lpBalance, block, chain) {
   sdk.util.sumMultiBalanceOf(balances, crvLP_token_balances);
 }
 
+async function genericUnwrapCvx(balances, holder, cvx_BaseRewardPool, block, chain)  {
+    // Compute the balance of the treasury of the CVX position and unwrap
+  const {output: cvx_LP_bal} = await sdk.api.abi.call({
+    abi: cvx_abi['cvxBRP_balanceOf'], // cvx_balanceOf cvx_earned cvx_rewards cvx_userRewardPerTokenPaid
+    target: cvx_BaseRewardPool,
+    params: [holder],
+    chain,
+    block: block,
+  })
+  const {output: stakingToken} = await sdk.api.abi.call({
+    abi: cvx_abi['cvxBRP_stakingToken'], 
+    target: cvx_BaseRewardPool,
+    chain,
+    block: block,
+  })
+  console.log('stakingToken', stakingToken)
+  // const {output: crvPool} = await sdk.api.abi.call({
+  //   abi: cvx_abi['cvxBooster_poolInfo'],
+  //   target: cvxBoosterAddress,
+  //   params: [stakingToken],
+  //   chain,
+  //   block: block,
+  // })
+  // console.log(crvPool)
+
+  await genericUnwrapCrv(balances, UST_whv23CRV_f, cvx_LP_bal, block, chain)
+  // TODO: replace by crvPool queried on-chain
+  // await genericUnwrapCrv(balances, crvPool, cvx_LP_bal, block, chain) 
+  
+}
+
+
 async function tvl(timestamp, ethBlock, chainBlocks) {
   // Get ether balance
   const balances = {
@@ -68,15 +102,8 @@ async function tvl(timestamp, ethBlock, chainBlocks) {
 
   // Compute the balance of the treasury of the CVX position and unwrap
   const chain = 'ethereum'
-  const {output: cvx_LP_bal} = await sdk.api.abi.call({
-    abi: abi['cvx_balanceOf'], // cvx_balanceOf cvx_earned cvx_rewards cvx_userRewardPerTokenPaid
-    target: cvxUST_whv23CRV_BaseRewardPool,
-    params: [treasury],
-    chain,
-    block: ethBlock,
-  })
-  await genericUnwrapCrv(balances, UST_whv23CRV_f, cvx_LP_bal, ethBlock, chain)
-  
+  await genericUnwrapCvx(balances, treasury, cvxUST_whv23CRV_BaseRewardPool, ethBlock, chain)
+
   return balances
 }
 
