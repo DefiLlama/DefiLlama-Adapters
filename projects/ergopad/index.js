03807aefcef21411d0f0fc97654c36543fb38614
Luivatra <luivatra@gmail.com>
2022-03-31 17:19:20 +0200

    filed under staking

--- projects/ergopad/index.js
+++ projects/ergopad/index.js
@@ -1,7 +1,7 @@
 const utils = require('../helper/utils');
 const {toUSDTBalances} = require('../helper/balances')
 
-async function tvl() {
+async function staking() {
   var totalTvl = await utils.fetchURL('https://ergopad.io/api/blockchain/tvl/d71693c49a84fbbecd4908c94813b46514b18b67a99952dc1e6e4791556de413');
   return toUSDTBalances(totalTvl.data.tvl.staked);
 }
@@ -10,7 +10,8 @@ module.exports = {
     misrepresentedTokens: true,
     timetravel: false,
     ergo:{
-        tvl
-    },
-    methodology: `Ergopad TVL is achieved by making a call to its API: https://ergopad.io/api/blockchain/tvl/d71693c49a84fbbecd4908c94813b46514b18b67a99952dc1e6e4791556de413 and consists of both staked and vested ergopad tokens.`
+        tvl: (async) => ({}),
+        staking
+    },  
+    methodology: `Ergopad TVL is achieved by making a call to its API: https://ergopad.io/api/blockchain/tvl/d71693c49a84fbbecd4908c94813b46514b18b67a99952dc1e6e4791556de413 and consists of both staked ergopad tokens.`
 }
