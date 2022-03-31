edd5fa19654365917fad871ce322062ceacab0db
Luivatra <luivatra@gmail.com>
2022-03-31 10:21:34 +0200

    remove vested tokens from tvl

--- projects/ergopad/index.js
+++ projects/ergopad/index.js
@@ -3,7 +3,7 @@ const {toUSDTBalances} = require('../helper/balances')
 
 async function tvl() {
   var totalTvl = await utils.fetchURL('https://ergopad.io/api/blockchain/tvl/d71693c49a84fbbecd4908c94813b46514b18b67a99952dc1e6e4791556de413');
-  return toUSDTBalances(totalTvl.data.tvl.total);
+  return toUSDTBalances(totalTvl.data.tvl.staked);
 }
 
 module.exports = {
