4d07efe6e8f81cafc61f2897b35253af95d68591
svrapidinnovation <sv@rapidinnovation.dev>
2022-04-01 11:14:07 +0530

    updated eth balance

--- projects/relayChain/index.js
+++ projects/relayChain/index.js
@@ -171,6 +171,9 @@ async function ethTvl(timestamp, ethBlock) {
       await sdk.util.sumSingleBalance(balances, "ethereum:"+ethTokenAddress[i], tokenBalance)
     }
 
+    tokenBalance=(await sdk.api.eth.getBalance({target:eth1Addr})).output;
+    await sdk.util.sumSingleBalance(balances, "ethereum:"+"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", tokenBalance)
+
       return balances
   };
 
