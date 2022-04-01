5f3bdf2373d15e9b7d8ff7af524b3c6690bec0ea
0xngmi <0xngmi@protonmail.com>
2022-04-02 00:53:15 +0100

    add crystal staking

--- projects/crystalvale/index.js
+++ projects/crystalvale/index.js
@@ -1,10 +1,12 @@
 const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
+const { staking } = require("../helper/staking");
 
 module.exports = {
   misrepresentedTokens: true,
   methodology:
     "Factory address (0x794C07912474351b3134E6D6B3B7b3b4A07cbAAa) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
   dfk: {
+    staking: staking("0x6E7185872BCDf3F7a6cBbE81356e50DAFFB002d2", "0x04b9dA42306B023f3572e106B11D82aAd9D32EBb", "dfk", "avax:0x04b9da42306b023f3572e106b11d82aad9d32ebb"),
     tvl: calculateUsdUniTvl(
       "0x794C07912474351b3134E6D6B3B7b3b4A07cbAAa",
       "dfk",
