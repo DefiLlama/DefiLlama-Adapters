3fcac1f80a4d0811e43d3f354d5b4e5b6f55f487
LendFlare <lendflareofficial@gmail.com>
2022-04-03 03:07:16 +0800

    Add LendFlare project
    Add LendFlare

---
+++ projects/lendflare/index.js
@@ -0,0 +1,21 @@
+const sdk = require('@defillama/sdk');
+const convexBooster = require('./convexBooster.js');
+const supplyBooster = require('./supplyBooster.js');
+const { toUSDTBalances } = require('./utils.js');
+
+async function tvl(timestamp, block, chainBlocks) {
+    const borrow = await convexBooster.tvl()
+    const supply = await supplyBooster.tvl()
+
+    // console.log(`borrow ${borrow.toString()}`)
+    // console.log(`supply ${supply.toString()}`)
+
+    return toUSDTBalances(borrow.plus(supply))
+}
+
+module.exports = {
+    timetravel: true,
+    ethereum: {
+        tvl,
+    }
+};
