a31648daf2eed07291ed50db535132e64fa088d6
Jonathan Chemla <jchemla@iconem.com>
2022-04-04 21:06:21 +0200

    Edit Nested to use REST endpoint to query tokens held
    Using the REST endpoint (live today) to query a list of all tokens that are part of a nested portfolio. Reduces the number of on-chain queries by a lot - previously was using a long while-loop

--- projects/nested/index.js
+++ projects/nested/index.js
@@ -1,12 +1,20 @@
 const sdk = require('@defillama/sdk')
 const { getBlock } = require('../helper/getBlock')
+const axios = require("axios");
+const retry = require('../helper/retry');
 const {chainExports} = require('../helper/exports')
-const abi = require('./abi.json')
+// const abi = require('./abi.json')
 
-const nestRecords_contract = '0x3Ee96E771D5E56b34245b023E8B31ffDf36dFafd'
 const nestReserve_contract = '0x150fb0Cfa5bF3D4023bA198C725b6DCBc1577f21'
+const tokens_url = 'https://api.nested.finance/tvl-tokens'
+// const nestRecords_contract = '0x3Ee96E771D5E56b34245b023E8B31ffDf36dFafd'
 // const graphUrl = 'https://api.nested.finance/graphql'
-// Mid-april 2022, an update to the nested backend will allow retrieval of all token addresses held by all nested records nfts, without requiring this intensive on-chain call
+
+const chainTransform = {
+  'polygon': 'poly', 
+  'bsc': 'bsc',
+  'avax': 'avax',
+}
 
 const max_calls = 50
 function chainTvl_onchain(chain) {
@@ -15,26 +23,14 @@ function chainTvl_onchain(chain) {
     const balances = {}
     const transformAddress = (addr) => `${chain}:${addr}`
 
-    let record_idx = 1
-    const recordsTokens = []
-    while (record_idx < max_calls) { // Could batch calls by bundles of 10 or 50 for example
-      const {output: nestedRecords_i} = await sdk.api.abi.call({
-          abi: abi['nestedRecords_getAssetTokens'],
-          target: nestRecords_contract,
-          params: record_idx,
-          chain,
-          block,
-      })
-      record_idx++
-      if (false && ((!nestedRecords_i) || nestedRecords_i.length == 0))
-      {
-        record_idx = max_calls // Stop criteria, no more records info
-      } else {
-        recordsTokens.push(nestedRecords_i)
-      }
-    }
+    
+    const tokens_response = await retry(async () => await axios.get(tokens_url));
+    const recordsTokens = tokens_response.data
+      .filter(t => t.startsWith(chainTransform[chain]))
+      .map(t => t.substring(t.indexOf(':') + 1))
+
     const reserveTokens = [...new Set(recordsTokens.flat())]
-    console.log(`${chain}: All tokens from every nested records`, reserveTokens)
+    // console.log(`${chain}: All tokens from every nested records`, reserveTokens)
     
     const tokenBalances = await sdk.api.abi.multiCall({
       calls: reserveTokens.map(t => ({
@@ -52,4 +48,4 @@ function chainTvl_onchain(chain) {
 }
 
 module.exports = chainExports(chainTvl_onchain, ['polygon', 'bsc', 'avax'])
-module.exports.methodology = 'Nested TVL consists of tokens held by NestedReserve, for which we can get the address using the nestedRecords contract'
+module.exports.methodology = 'Nested TVL consists of tokens held by NestedReserve, for which we can get the address using the nestedRecords contract or better, the rest api endpoint'
