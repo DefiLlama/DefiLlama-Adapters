eec576b6e8f466599f668b828a93239628c0e935
Jonathan Chemla <jchemla@iconem.com>
2022-04-07 11:49:13 +0200

    Ambire Wallet TVL - staking only
    Ambire Wallet TVL - staking only

---
+++ projects/ambire-wallet/index.js
@@ -0,0 +1,12 @@
+const { staking } = require("../helper/staking");
+
+const WALLET = "0x88800092ff476844f74dc2fc427974bbee2794ae";
+const WALLET_staking = "0x47cd7e91c3cbaaf266369fe8518345fc4fc12935";
+
+module.exports = {
+  methodology: `TVL for Ambire Wallet consists of the staking of WALLET.`, 
+  ethereum:{
+    tvl: () => ({}),
+    staking: staking(WALLET_staking, WALLET, "ethereum"), 
+  }
+}
