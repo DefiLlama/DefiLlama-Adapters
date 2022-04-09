41b695eddf7a7204fe5ea2b7fa4f5e684c044f5e
waynebruce0x <waynebruce0x@gmail.com>
2022-04-09 16:57:09 +0100

    dddx onchain and nord finance refactor

--- projects/nord-finance/index.js
+++ projects/nord-finance/index.js
@@ -4,22 +4,32 @@ const STATS_URL = 'https://api.nordfinance.io/tvl/statistics';
 
 async function fetch() {
   var totalTvl = await utils.fetchURL(STATS_URL);
-  return totalTvl.data.tvl.totalTvl;
+  return totalTvl.data.tvl.totalTvl - totalTvl.data.tvl.lpStaking.totalLpStaking - totalTvl.data.tvl.staking.totalStaking;
 }  
 
 async function ethereum() {
   var totalTvl = await utils.fetchURL(STATS_URL);
-  return totalTvl.data.tvl.ethereum;
+  return totalTvl.data.tvl.ethereum - totalTvl.data.tvl.lpStaking.ethereum - totalTvl.data.tvl.staking.ethereum;
 }
 
 async function polygon() {
   var totalTvl = await utils.fetchURL(STATS_URL);
-  return totalTvl.data.tvl.polygon;
+  return totalTvl.data.tvl.polygon - totalTvl.data.tvl.lpStaking.polygon - totalTvl.data.tvl.staking.polygon;
 }
 
 async function avalanche() {
   var totalTvl = await utils.fetchURL(STATS_URL);
-  return totalTvl.data.tvl.avalanche;
+  return totalTvl.data.tvl.avalanche - totalTvl.data.tvl.lpStaking.avalanche - totalTvl.data.tvl.staking.avalanche;
+}
+
+async function pool2() {
+  var totalTvl = await utils.fetchURL(STATS_URL);
+  return totalTvl.data.tvl.lpStaking.totalLpStaking;
+}
+
+async function staking() {
+  var totalTvl = await utils.fetchURL(STATS_URL);
+  return totalTvl.data.tvl.staking.totalStaking;
 }
 
 module.exports = {
@@ -33,5 +43,11 @@ module.exports = {
   },
   avalanche: {
     fetch: avalanche,
+  },
+  pool2: {
+    fetch: pool2
+  },
+  staking: {
+    fetch: staking
   }
-};
+}; // node test.js projects/nord-finance/index.js
