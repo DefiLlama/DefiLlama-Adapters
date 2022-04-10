626be12aeed3ad937906feaf005ca6c6f525da03
BluebitFinance <bluebit86@gmail.com>
2022-04-10 10:32:30 +0800

    update bluebit.fi adapter

--- projects/bluebit/index.js
+++ projects/bluebit/index.js
@@ -1,9 +1,12 @@
 const sdk = require("@defillama/sdk");
 const abi = require("./abis.json");
+const { staking } = require("../helper/staking");
 const { toUSDTBalances } = require("../helper/balances");
 
 const account = "0x0000000000000000000000000000000000000000";
 const stats = "0x1B77F6f962270010B8bb8F5CECEE47C4CF4741a2";
+const token = "0x4148d2Ce7816F0AE378d98b40eB3A7211E1fcF0D";
+const veToken = "0xdf7C547f332351A86DB0D89a89799A7aB4eC9dEB";
 
 const tvl = async (timestamp, block, chainBlocks) => {
   let tvl = (
@@ -24,6 +27,7 @@ module.exports = {
   misrepresentedTokens: false,
   start: 62936418,
   aurora: {
-    tvl,
+    tvl: tvl,
+    staking: staking(veToken, token, "aurora"),
   },
 };
