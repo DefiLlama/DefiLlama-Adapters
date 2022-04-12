54a828181bc3e20590227f37bdf17e415098c09e
byeonggoon <dla6762@gmail.com>
2022-04-12 17:50:17 +0900

    update stakedNR

--- projects/neuronswap/index.js
+++ projects/neuronswap/index.js
@@ -6,14 +6,23 @@ async function NRtvl() {
   const response = await retry(
     async (bail) => await axios.get("https://core.neuronswap.com/api/dashboard")
   );
-  return response.data.data.totalValueLocked;
+  return response.data.data.totalLp;
+}
+
+async function stakedNR() {
+  const response = await retry(
+    async (bail) => await axios.get("https://core.neuronswap.com/api/dashboard")
+  );
+  return response.data.data.totalStakedNeuronPrice;
 }
 
 module.exports = {
   methodology: `Tvl counts the tokens locked on AMM pools and staking counts the NR that has been staked. Data is pulled from the 'https://core.neuronswap.com/api/dashboard'`,
   klaytn: {
     fetch: NRtvl,
+    staking: stakedNR,
   },
 
   fetch: async () => await NRtvl(),
+  staking: async () => await stakedNR(),
 };
