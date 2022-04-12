e21df4043b2d946c3bf0b550ddf23b885d753966
alex <alexangel.j@gmail.com>
2022-04-11 17:37:37 -0700

    fix(primitive-adapter): adds v1 and rmm tvl to adapter

--- projects/primitive/index.js
+++ projects/primitive/index.js
@@ -1,8 +1,19 @@
 const rmmTVL = require('./rmm')
+const v1TVL = require('./v1')
+
+function sumObjectsByKey(...objs) {
+  return objs.reduce((a, b) => {
+    for (let k in b) {
+      if (b.hasOwnProperty(k)) a[k] = (a[k] || 0) + b[k]
+    }
+    return a
+  }, {})
+}
 
 async function tvl(timestamp, block) {
   const [rmm] = await Promise.all([rmmTVL(timestamp, block)])
-  return rmm
+  const [v1] = await Promise.all([v1TVL(timestamp, block)])
+  return sumObjectsByKey(rmm, v1)
 }
 
 module.exports = {
