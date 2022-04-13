b8666a3f8f7475ba170620d625284b9021dff552
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-04-13 22:55:43 +0200

    update scrub adapter

--- projects/scrub/index.js
+++ projects/scrub/index.js
@@ -1,29 +1,16 @@
+const { tombTvl } = require("./helper/tomb")
 
-const { sumTokens } = require("../helper/unwrapLPs")
-const { staking } = require('../helper/staking')
+const lion = '0x49fB98F9b4a3183Cd88e7a115144fdf00fa6fB95'
+const share = "0xD6597AA36DD90d7fCcBd7B8A228F2d5CdC88eAd0";
 
-const share = '0xD6597AA36DD90d7fCcBd7B8A228F2d5CdC88eAd0' // TIGER
-const rewardPool = '0x44B4a1e8f34Bb52ed39854aD218FF94D2D5b4800'
-const scrub = '0x05CaB739FDc0A4CE0642604c78F307C6c543cD6d'
-const treasury = '0x101785025A90ae6a865E3161FF37a7483a088ADf'
-const chain = 'cronos'
-async function pool2(ts, _, chainBlocks) {
-  const block = chainBlocks[chain]
-  const balances = {}
-  await sumTokens(balances, [
-    ['0xD440433dAA33b3e3f2b5421046EAf84bEe6F40D0', rewardPool,], // LION-SVN LP
-    ['0xaDeC6aaAa0765472EE9eBe524BD3454Fd733BAB9', rewardPool,], // TIGER-SVN LP
-  ], block, chain, undefined, { resolveLP: true })
+const masonry = "0x05CaB739FDc0A4CE0642604c78F307C6c543cD6d";
+const rewardPool = "0x44B4a1e8f34Bb52ed39854aD218FF94D2D5b4800";
 
-  return balances
-}
+const pool2lps = [
+	'0xD440433dAA33b3e3f2b5421046EAf84bEe6F40D0', // LION-SVN LP
+	'0xaDeC6aaAa0765472EE9eBe524BD3454Fd733BAB9', // TIGER-SVN LP
+]
 
 module.exports = {
-  cronos: {
-    pool2,
-    treasury: staking(treasury, share, 'cronos'),
-    staking: staking(scrub, share, 'cronos'),
-    tvl: async () => ({}),
-  },
-  methodology: 'amount staked on the platform and, the dao treasury',
+    ...tombTvl(lion, share, rewardPool, masonry, pool2lps, "cronos", undefined, false, pool2lps[1])
 }
