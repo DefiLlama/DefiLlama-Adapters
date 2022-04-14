62ef3e4f13e75f9ab6b6c66fc743951063d0e803
Dario <medariox@users.noreply.github.com>
2022-04-14 15:26:21 +0200

    Add TVL for TempleDAO using subgraph

--- projects/templedao/index.js
+++ projects/templedao/index.js
@@ -1,32 +1,39 @@
 const { staking } = require("../helper/staking");
-const { sumTokensAndLPsSharedOwners } = require("../helper/unwrapLPs");
+const { gql, request } = require("graphql-request");
+const { toUSDTBalances } = require("../helper/balances");
 
 const templeStakingContract = "0x4D14b24EDb751221B3Ff08BBB8bd91D4b1c8bc77";
 const TEMPLE = "0x470ebf5f030ed85fc1ed4c2d36b9dd02e77cf1b7";
 
-const templeTreasuryContract = "0x22c2fE05f55F81Bf32310acD9a7C51c4d7b4e443";
-const FRAX = "0x853d955acef822db058eb8505911ed77f175b99e";
+const protocolQuery = gql`
+query get_tvl($block: Int) {
+  protocolMetrics(
+    first: 1, orderBy: timestamp, orderDirection: desc
+  ) {
+    lockedStables
+    timestamp
+  }
+}
+`
 
-async function treasuryTvl() {
-  const balances = {};
-
-  await sumTokensAndLPsSharedOwners(
-    balances,
-    [
-      [FRAX, false],
-    ],
-    [templeTreasuryContract]
-  );
-
-  return balances;
+/*** TVL Portion (Treasury) ***
+ * Treasury TVL consists of stables from TEMPLE-FRAX AMM LP, Temple Defend and FRAX+3CRV LP
+ ***/
+async function ethTvl(timestamp, block) {
+  const queriedData = await request("https://api.thegraph.com/subgraphs/name/templedao/templedao-metrics", protocolQuery, {block})
+  const metric = queriedData.protocolMetrics[0]
+  if(Date.now()/1000 - metric.timestamp > 3600*24){
+    throw new Error("outdated")
+  }
+  return toUSDTBalances(metric.lockedStables)
 }
 
 module.exports = {
   misrepresentedTokens: true,
   ethereum: {
     staking: staking(templeStakingContract, TEMPLE),
-    tvl: treasuryTvl,
+    tvl: ethTvl,
   },
   methodology:
-    "Counts tvl on the treasury through TempleTreasury Contract",
+    "Counts stables from TEMPLE-FRAX AMM LP, Temple Defend and FRAX+3CRV LP as treasury TVL",
 };
