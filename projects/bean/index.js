d1461bea3f0c1de4152de4fea8657abe400c98ea
Timo Angerer <timo.angerer@tum.de>
2022-04-14 11:01:40 +0200

    WIP: get data from old blocks

--- projects/bean/index.js
+++ projects/bean/index.js
@@ -1,6 +1,9 @@
 const sdk = require('@defillama/sdk');
-const { unwrapUniswapLPs } = require('../helper/unwrapLPs');
+const utils = require('../helper/utils')
+const { unwrapCrv } = require('../helper/resolveCrvTokens');
+const { unwrapUniswapLPs, sumTokens } = require('../helper/unwrapLPs');
 const { bean_abi, unwrapCrvSimple } = require ("./bean-utils.js");
+// const { getChainTransform } = require('../portedTokens')
 
 const BEAN_DIA_ADDR = "0xC1E088fC1323b20BCBee9bd1B9fC9546db5624C5";
 
@@ -14,45 +17,134 @@ const BEAN_CRV_POOLS = [
     { addr: "0x3a70DfA7d2262988064A2D051dd47521E43c9BdD", numToken: 2 },
 ]
 
-async function tvl(time, block){
-    const balances = {};
-
+async function tvl(time, ethBlock) {
+    
     const beanBalance = (await sdk.api.abi.call({
-        abi: bean_abi["totalDepositedBeans"],
-        target: BEAN_DIA_ADDR,
-        block,
+        abi: 'erc20:balanceOf',
+        chain: "ethereum",
+        target: BEAN_TOKEN_ADDR,
+        params: [BEAN_DIA_ADDR],
+        block: 14582500,
     })).output;
-
-    const beanEthLpBalance = (await sdk.api.abi.call({
-        abi: bean_abi["totalDepositedLP"],
-        target: BEAN_DIA_ADDR,
-        block,
+    console.log("14582500:",beanBalance);
+    const beanBalance2 = (await sdk.api.abi.call({
+        abi: 'erc20:balanceOf',
+        chain: "ethereum",
+        target: BEAN_TOKEN_ADDR,
+        params: [BEAN_DIA_ADDR],
+        block: 14582544,
     })).output;
-
-    const lpPositions = [
-        {
-            balance: beanEthLpBalance,
-            token: BEAN_ETH_ADDR,
-        }
-    ]
-
-    // add balance of siloed Beans
-    await sdk.util.sumSingleBalance(balances, BEAN_TOKEN_ADDR, beanBalance);
-    // add balance of siloed BEAN:ETH from uniswap pool
-    await unwrapUniswapLPs(balances, lpPositions, block);
-
-    // add balances of all siloed curve pools
-    await Promise.all(BEAN_CRV_POOLS.map(async (pool) => {
-        const lpBalance = (await sdk.api.abi.call({
-            abi: bean_abi["getTotalDeposited"],
-            target: BEAN_DIA_ADDR,
-            params: pool.addr,
-        })).output;
-        await unwrapCrvSimple(balances, pool.addr, lpBalance, block, "ethereum", pool.numToken);
-    }));
-
-    return balances;
+    console.log("14582544:",beanBalance2);
+    const beanBalance3 = (await sdk.api.abi.call({
+        abi: 'erc20:balanceOf',
+        chain: "ethereum",
+        target: BEAN_TOKEN_ADDR,
+        params: [BEAN_DIA_ADDR],
+        block: ethBlock,
+    })).output;
+    console.log("current:",beanBalance3);
+    return {};
 }
+
+//tvl(0, 14579517);
+
+// async function tvl(time, block){
+//     const chain = "ethereum";
+//     console.log("Calculating TVL for block", block);
+//     const balances = {};
+
+//     const beanPrice = await utils.getPrices({BEAN_TOKEN_ADDR : 'bean'});
+//     console.log(beanPrice.data);
+
+//     await sumTokens(balances, [[BEAN_TOKEN_ADDR, BEAN_DIA_ADDR]], block);
+//     console.log(balances);
+
+//     const beanEthLpBalance = (await sdk.api.abi.call({
+//         abi: 'erc20:balanceOf',
+//         target: BEAN_ETH_ADDR,
+//         params: BEAN_DIA_ADDR,
+//         block,
+//         chain,
+//     })).output;
+
+//     const beanBalance = (await sdk.api.abi.call({
+//         abi: 'erc20:balanceOf',
+//         target: BEAN_TOKEN_ADDR,
+//         params: BEAN_DIA_ADDR,
+//         block: block,
+//     })).output;
+//     // console.log("Bean:Eth balance:", beanEthLpBalance2);
+
+//     tokensAndOwners = [
+//         [BEAN_TOKEN_ADDR, BEAN_DIA_ADDR],
+//         [BEAN_ETH_ADDR, BEAN_DIA_ADDR],
+//     ]
+
+//     const balanceOfTokens = await sdk.api.abi.multiCall({
+// 		calls: tokensAndOwners.map(t => ({
+// 			target: t[0],
+// 			params: t[1]
+// 		})),
+// 		abi: 'erc20:balanceOf',
+// 		block,
+// 		chain
+// 	})
+
+//     // const beanBalance = (await sdk.api.abi.call({
+//     //     abi: bean_abi["totalDepositedBeans"],
+//     //     chain: 'ethereum',
+//     //     target: BEAN_DIA_ADDR,
+//     //     block: block,
+//     // })).output;
+//     // console.log("Bean balance:", beanBalance);
+
+//     // const beanEthLpBalance = (await sdk.api.abi.call({
+//     //     abi: bean_abi["totalDepositedLP"],
+//     //     target: BEAN_DIA_ADDR,
+//     //     block: block,
+//     // })).output;
+//     // console.log("Bean:Eth balance:", beanEthLpBalance);
+
+//     const lpPositions = [
+//         {
+//             balance: beanEthLpBalance,
+//             token: BEAN_ETH_ADDR,
+//         }
+//     ]
+
+//     // add balance of siloed Beans
+//     await sdk.util.sumSingleBalance(balances, BEAN_TOKEN_ADDR, beanBalance);
+//     console.log("Added Bean:",balances)
+//     // add balance of siloed BEAN:ETH from uniswap pool
+//     await unwrapUniswapLPs(balances, lpPositions, block);
+//     console.log("Added BEAN:ETH LP:",balances)
+
+//     if (block >= 14218934) {
+//         // add balances of all siloed curve pools
+//         // await Promise.all(BEAN_CRV_POOLS.map(async (pool) => {
+//         //     const lpBalance = (await sdk.api.abi.call({
+//         //         abi: bean_abi["getTotalDeposited"],
+//         //         target: BEAN_DIA_ADDR,
+//         //         params: pool.addr,
+//         //         block: block,
+//         //     })).output;
+//         //     await unwrapCrvSimple(balances, pool.addr, lpBalance, block, "ethereum", pool.numToken);
+//         // }));
+//         await Promise.all(BEAN_CRV_POOLS.map(async (pool) => {
+//             const lpBalance = (await sdk.api.abi.call({
+//                 abi: 'erc20:balanceOf',
+//                 target: pool.addr,
+//                 params: BEAN_DIA_ADDR,
+//                 block: block,
+//             })).output;
+//             await unwrapCrvSimple(balances, pool.addr, lpBalance, block, "ethereum", pool.numToken);
+//         }));
+        
+//     }
+//     console.log(balances);
+
+//     return balances;
+// }
 
 module.exports={
     timetravel: true,
