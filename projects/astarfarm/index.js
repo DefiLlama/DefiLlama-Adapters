75eaff9611289cb8870193632f1e3d9911882084
lilpacy <revivedtomorrow@gmail.com>
2022-04-14 17:29:32 +0900

    fix astarfarm fetch function to pass test

--- projects/astarfarm/index.js
+++ projects/astarfarm/index.js
@@ -1,12 +1,16 @@
 const ethers = require("ethers")
 
 const fetch = async () => {
-    const contract = await ethers.getContractAt("AstarFarm", "0x0BdD0C5406B9420B4d4251671F9F045e4FBEb758");
+    const rpcUrl = "https://rpc.astar.network:8545";
+    const provider = await ethers.getDefaultProvider(rpcUrl);
+    const contract = new ethers.Contract(
+      "0x0BdD0C5406B9420B4d4251671F9F045e4FBEb758",
+      ["function staked() external view returns (uint128)"],
+      provider
+    );
     return await contract.staked(); // tvl as wei
 }
 
 module.exports = {
-    timetravel: true,
-    methodology: "AstarFarm Tvl Calculation",
-    fetch: fetch
+    fetch
 }
