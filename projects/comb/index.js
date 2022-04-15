0165a5ee593c64296bce11e5e7422184fffc0fc9
Philip <pvu2002@outlook.com>
2022-04-14 21:59:49 -0700

    move zcomb to staking

--- projects/comb/index.js
+++ projects/comb/index.js
@@ -6,20 +6,28 @@ function fetch() {
     let tvl = 0;
     const pools = await utils.fetchURL('https://comb-breakdown.herokuapp.com/pools');
     const vaults = await utils.fetchURL('https://comb-breakdown.herokuapp.com/vaults');
-    const zcomb = await utils.fetchURL('https://comb-breakdown.herokuapp.com/zcomb');
+
 
     pools.data.forEach(pool => tvl += pool.tvl);
     vaults.data.forEach(vault => tvl += vault.tvl);
-    tvl += zcomb.data.tvl;
 
     return toUSDTBalances(tvl);
   }
 }
 
+function staking() {
+  return async () => {
+    let tvl = 0;
+    const zcomb = await utils.fetchURL('https://comb-breakdown.herokuapp.com/zcomb');
+    tvl += zcomb.data.tvl;
+    return toUSDTBalances(tvl);
+  }
+}
 
 module.exports = {
   methodology: 'Fetches pools (masterchef), vaults, and zcomb data from external APIs and sums up the total locked values (TVL). The TVLs are calculated by taking the lp balances and its price of the strategies and adding them up. The zcomb tvl is calculated by taking the total locked comb and multiplying it by its market value.',
   fantom: {
-    tvl: fetch()
+    tvl: fetch(),
+    staking: staking()
   }
 }
