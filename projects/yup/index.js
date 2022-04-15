05ec86736782655bf7bafe809adf5eea50d93891
Slava <90375322+Slava-g1@users.noreply.github.com>
2022-04-15 13:40:54 -0700

    Update index.js

--- projects/yup/index.js
+++ projects/yup/index.js
@@ -1,21 +1,21 @@
 const sdk = require('@defillama/sdk');
-const { transformBscAddress } = require('../helper/portedTokens');
-const MINT_TOKEN_CONTRACT = '0x1f3Af095CDa17d63cad238358837321e95FC5915';
-const MINT_CLUB_BOND_CONTRACT = '0x8BBac0C7583Cc146244a18863E708bFFbbF19975';
+const { transformPolygonAddress } = require('../helper/portedTokens');
+const TOKEN_CONTRACT = '0x69bbc3f8787d573f1bbdd0a5f40c7ba0aee9bcc9';
+//const MINT_CLUB_BOND_CONTRACT = '0x8BBac0C7583Cc146244a18863E708bFFbbF19975';
 
 async function tvl(timestamp, block, chainBlocks) {
   const balances = {};
-  const transform = await transformBscAddress();
+  const transform = await transformPolygonAddress();
 
   const collateralBalance = (await sdk.api.abi.call({
     abi: 'erc20:balanceOf',
-    chain: 'bsc',
-    target: MINT_TOKEN_CONTRACT,
-    params: [MINT_CLUB_BOND_CONTRACT],
-    block: chainBlocks['bsc'],
+    chain: 'polygon',
+    target: TOKEN_CONTRACT,
+    //params: [MINT_CLUB_BOND_CONTRACT],
+    block: chainBlocks['polygon'],
   })).output;
 
-  await sdk.util.sumSingleBalance(balances, transform(MINT_TOKEN_CONTRACT), collateralBalance)
+  await sdk.util.sumSingleBalance(balances, transform(TOKEN_CONTRACT), collateralBalance)
 
   return balances;
 }
@@ -23,7 +23,7 @@ async function tvl(timestamp, block, chainBlocks) {
 module.exports = {
   timetravel: true,
   misrepresentedTokens: false,
-  methodology: 'counts the number of MINT tokens in the Club Bonding contract.'
+  methodology: ''
   start: 1000235,
   bsc: {
     tvl,
