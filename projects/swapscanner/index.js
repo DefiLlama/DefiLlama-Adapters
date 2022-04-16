d93fe99b3a020eb6eb7c2bd69bbcb05252337e39
Luther <luther@swapscanner.io>
2022-04-16 11:59:33 +0900

    [Swapscanner] Separately classify Governance staked amount as Staking

--- projects/swapscanner/index.js
+++ projects/swapscanner/index.js
@@ -3,28 +3,41 @@ const { toUSDTBalances } = require("../helper/balances");
 const { RPC_ENDPOINT_URL, SCNR, KLAY, ABIs } = require("./constants");
 const { getTokenPriceCoinGecko } = require("../config/bella/utilities.js");
 
+const getTokenPrice = (staked, klayPrice) => {
+  const { reservesForSCNR, reservesForKLAY } = staked;
+  const amountOfSCNRStaked = reservesForSCNR / 10 ** SCNR.decimals;
+  const amountOfKLAYStaked = reservesForKLAY / 10 ** KLAY.decimals;
+
+  const exchangeRatio = amountOfKLAYStaked / amountOfSCNRStaked;
+  return exchangeRatio * klayPrice;
+};
+
+const getStakedReserves = async (provider) => {
+  const lp = new provider.klay.Contract(ABIs.LP, SCNR.lp.KLAY);
+  const { 0: reservesForSCNR, 1: reservesForKLAY } = await lp.methods
+    .getReserves()
+    .call();
+  return { reservesForSCNR, reservesForKLAY };
+};
+
 const tvl = async () => {
   const caver = new Caver(RPC_ENDPOINT_URL);
-  const lp = new caver.klay.Contract(ABIs.LP, SCNR.lp.KLAY);
-
-  const getStakedReserves = async () => {
-    const { 0: reservesForSCNR, 1: reservesForKLAY } = await lp.methods
-      .getReserves()
-      .call();
-    return { reservesForSCNR, reservesForKLAY };
-  };
-
-  const getTokenPrice = (staked, klayPrice) => {
-    const { reservesForSCNR, reservesForKLAY } = staked;
-    const amountOfSCNRStaked = reservesForSCNR / 10 ** SCNR.decimals;
-    const amountOfKLAYStaked = reservesForKLAY / 10 ** KLAY.decimals;
-
-    const exchangeRatio = amountOfKLAYStaked / amountOfSCNRStaked;
-    return exchangeRatio * klayPrice;
-  };
 
   // For now, SCNR/KLAY LP exists only on Swapscanner
   const getStakedInLPs = (staked) => Number(staked.reservesForSCNR);
+
+  const [staked, klayPrice] = await Promise.all([
+    getStakedReserves(caver),
+    getTokenPriceCoinGecko("usd")("klay-token"),
+  ]);
+
+  const tokenPrice = getTokenPrice(staked, klayPrice);
+  const totalStaked = getStakedInLPs(staked) / 10 ** SCNR.decimals;
+  return toUSDTBalances(totalStaked * tokenPrice);
+};
+
+const staking = async () => {
+  const caver = new Caver(RPC_ENDPOINT_URL);
 
   const getStakedInGovernance = async (address) => {
     const staking = new caver.klay.Contract(ABIs.Staking, SCNR.staking);
@@ -34,23 +47,19 @@ const tvl = async () => {
     return Number(stakedBalance);
   };
 
-  const [staked, stakedInGovernance, klayPrice] = await Promise.all([
-    getStakedReserves(),
+  const [stakedInLP, totalStakedInGovernance, klayPrice] = await Promise.all([
+    getStakedReserves(caver),
     getStakedInGovernance(SCNR.address),
     getTokenPriceCoinGecko("usd")("klay-token"),
   ]);
-  const tokenPrice = getTokenPrice(staked, klayPrice);
 
-  const totalStakedByProtocols = [getStakedInLPs(staked), stakedInGovernance];
-  const totalStaked =
-    totalStakedByProtocols.reduce((acc, staked) => acc + staked, 0) /
-    10 ** SCNR.decimals;
-
-  return toUSDTBalances(totalStaked * tokenPrice);
+  const tokenPrice = getTokenPrice(stakedInLP, klayPrice);
+  return toUSDTBalances(totalStakedInGovernance * tokenPrice);
 };
 
 module.exports = {
   klaytn: {
     tvl,
+    staking,
   },
 };
