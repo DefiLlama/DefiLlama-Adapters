c53cfe870ce878ba51a4630860aad91fb51ca22f
zavodil <support@zavodil.ru>
2022-04-16 22:43:56 -0400

    export borrowed

--- projects/burrow.cash/index.js
+++ projects/burrow.cash/index.js
@@ -1,4 +1,4 @@
-const { call, addTokenBalances, } = require('../helper/near')
+const { call, addTokenBalances, sumSingleBalance} = require('../helper/near')
 
 const BURROW_CONTRACT = 'contract.main.burrow.near'
 
@@ -7,16 +7,30 @@ async function tvl() {
 
   const assetsCallResponse = await call(BURROW_CONTRACT, 'get_assets_paged', {})
   const assets = assetsCallResponse
-    .map(([asset]) => asset)
-    .filter(asset => !/\.burrow\./.test(asset))  // Ignore all assets that can be considered native tokens
+      .map(([asset]) => asset)
+      .filter(asset => !/\.burrow\./.test(asset))  // Ignore all assets that can be considered native tokens
   await addTokenBalances(assets, BURROW_CONTRACT, balances)
   return balances
 }
 
+async function borrowed() {
+  const balances = {}
+  const assetsCallResponse = await call(BURROW_CONTRACT, 'get_assets_paged', {})
+  assetsCallResponse
+      .map((asset_info) => {
+        const token_id = asset_info[0];
+        const asset = asset_info[1];
+        if(!/\.burrow\./.test(token_id)) { // Ignore all assets that can be considered native tokens
+          sumSingleBalance(balances, token_id, asset.borrowed.balance)
+        }
+      });
+  return balances
+}
 
 module.exports = {
   near: {
     tvl,
+    borrowed
   },
   misrepresentedTokens: true,
   methodology: 'Summed up all the tokens deposited in their main lending contract'
