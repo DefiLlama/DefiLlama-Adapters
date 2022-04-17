53cd2e706b9292eaf17c9459c425cdaf6ebf0f8c
astrit11 <astritmaliku@yahoo.com>
2022-04-17 20:38:06 +0200

    Added smartcredit tvl

--- projects/smartcredit.js
+++ projects/smartcredit.js
@@ -1,28 +1,25 @@
 const retry = require('async-retry')
 const { GraphQLClient, gql } = require('graphql-request')
 
-async function tvl() {
-  var endpoint ='https://d1oejh0y3ae31s.cloudfront.net/graphql';
+var endpoint ='https://d2c7awq32ho327.cloudfront.net/graphql';
   var graphQLClient = new GraphQLClient(endpoint)
 
   var query = gql`
   {
     getTVL {
-        borrowerVolume,
-        lenderVolume
+      platformTVL,
     }
   }
   `;
-  const results = await retry(async bail => await graphQLClient.request(query))
-  const borrowerVolume = parseFloat(results.getTVL[0].borrowerVolume)
-  const lendingVolume = parseFloat(results.getTVL[0].lenderVolume)
-
-  const tvl = borrowerVolume + lendingVolume 
-
-  return tvl
-      
-}
+async function fetch() {
+  try {
+    const {getTVL} = await retry(async bail => await graphQLClient.request(query))
+    return Promise.resolve(getTVL.platformTVL)
+  } catch (err) {
+    Promise.reject(err)
+  }
+}  
 
 module.exports = {
-  tvl
+    fetch
 }
