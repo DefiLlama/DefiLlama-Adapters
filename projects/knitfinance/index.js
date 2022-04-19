0cce5b2c3108a90d50064b14708e8ac628eade1d
sruthi23 <sruthi@lightrains.com>
2022-04-19 18:10:45 +0530

    KnitFinance tvl

--- projects/knitfinance/index.js
+++ projects/knitfinance/index.js
@@ -1,30 +1,80 @@
 const axios = require("axios");
 
-let url = "https://adminv1.knit.finance/api/tvl";
+let url = "http://127.0.0.1:3001/api/tvl";
 
 async function fetchBsc() {
   let bsc = await axios.get(`${url}/bsc`);
-  return bsc.data.data.data.tvl.bsc;
+  return bsc?.data?.data?.data?.tvl?.bsc;
 }
 
 async function fetchPoly() {
   let poly = await axios.get(`${url}/matic`);
-  return poly.data.data.data.tvl.matic;
+  return poly?.data?.data?.data?.tvl?.matic;
 }
 
 async function fetchEth() {
   let eth = await axios.get(`${url}/eth`);
-  return eth.data.data.data.tvl.eth;
+  return eth?.data?.data?.data?.tvl?.eth;
 }
 
 async function fetchFantom() {
   let fantom = await axios.get(`${url}/fantom`);
-  return fantom.data.data.data.tvl.fantom;
+  return fantom?.data?.data?.data?.tvl?.fantom;
 }
 
 async function fetchHeco() {
   let heco = await axios.get(`${url}/heco`);
-  return heco.data.data.data.tvl.heco;
+  return heco?.data?.data?.data?.tvl?.heco;
+}
+
+async function fetchAvax() {
+  let avax = await axios.get(`${url}/avalanche`);
+  return avax?.data?.data?.data?.tvl?.avalanche;
+}
+
+async function fetchKcc() {
+  let kcc = await axios.get(`${url}/kcc`);
+  return kcc?.data?.data?.data?.tvl?.kcc;
+}
+
+async function fetchHarmony() {
+  let harmony = await axios.get(`${url}/harmony`);
+  return harmony?.data?.data?.data?.tvl?.harmony;
+}
+
+async function fetchOkx() {
+  let okx = await axios.get(`${url}/okexchain`);
+  return okx?.data?.data?.data?.tvl?.okexchain;
+}
+
+async function fetchGc() {
+  let gc = await axios.get(`${url}/gnosis`);
+  return gc?.data?.data?.data?.tvl?.gnosis;
+}
+
+async function fetchSysCoin() {
+  let sys = await axios.get(`${url}/syscoin`);
+  return sys?.data?.data?.data?.tvl?.syscoin;
+}
+
+async function fetchTlos() {
+  let tlos = await axios.get(`${url}/telos`);
+  return tlos?.data?.data?.data?.tvl?.telos;
+}
+
+async function fetchMoonriver() {
+  let moonriver = await axios.get(`${url}/moonriver`);
+  return moonriver?.data?.data?.data?.tvl?.moonriver;
+}
+
+async function fetchMilkoMeda() {
+  let moonriver = await axios.get(`${url}/moonriver`);
+  return moonriver?.data?.data?.data?.tvl?.moonriver;
+}
+
+async function fetchMoonbeam() {
+  let moonbeam = await axios.get(`${url}/moonbeam`);
+  return moonbeam?.data?.data?.data?.tvl?.moonbeam;
 }
 
 async function fetch() {
@@ -32,13 +82,34 @@ async function fetch() {
     bsc = await axios.get(`${url}/bsc`),
     eth = await axios.get(`${url}/eth`),
     fantom = await axios.get(`${url}/fantom`),
-    heco = await axios.get(`${url}/heco`);
+    heco = await axios.get(`${url}/heco`),
+    avax = await axios.get(`${url}/avalanche`),
+    kcc = await axios.get(`${url}/kcc`),
+    harmony = await axios.get(`${url}/harmony`),
+    okx = await axios.get(`${url}/okexchain`),
+    gc = await axios.get(`${url}/gnosis`),
+    sys = await axios.get(`${url}/syscoin`),
+    tlos = await axios.get(`${url}/telos`),
+    moonriver = await axios.get(`${url}/moonriver`),
+    milkomeda = await axios.get(`${url}/milkomeda`),
+    moonbeam = await axios.get(`${url}/moonbeam`);
   const tvl =
-    bsc.data.data.data.tvl.bsc +
-    poly.data.data.data.tvl.matic +
-    eth.data.data.data.tvl.eth +
-    fantom.data.data.data.tvl.fantom +
-    heco.data.data.data.tvl.heco;
+    bsc?.data?.data?.data?.tvl?.bsc +
+    poly?.data?.data?.data?.tvl?.matic +
+    eth?.data?.data?.data?.tvl?.eth +
+    fantom?.data?.data?.data?.tvl?.fantom +
+    heco?.data?.data?.data?.tvl?.heco +
+    avax?.data?.data?.data?.tvl?.avalanche +
+    kcc?.data?.data?.data?.tvl?.kcc +
+    harmony?.data?.data?.data?.tvl?.harmony +
+    okx?.data?.data?.data?.tvl?.okexchain +
+    gc?.data?.data?.data?.tvl?.gnosis +
+    sys?.data?.data?.data?.tvl?.syscoin +
+    tlos?.data?.data?.data?.tvl?.telos +
+    moonriver?.data?.data?.data?.tvl?.moonriver +
+    milkomeda?.data?.data?.data?.tvl?.milkomeda +
+    moonbeam?.data?.data?.data?.tvl?.moonbeam;
+
   return tvl;
 }
 
@@ -57,6 +128,36 @@ module.exports = {
   },
   fantom: {
     fetch: fetchFantom,
+  },
+  avalanche: {
+    fetch: fetchAvax,
+  },
+  kcc: {
+    fetch: fetchKcc,
+  },
+  harmony: {
+    fetch: fetchHarmony,
+  },
+  okexchain: {
+    fetch: fetchOkx,
+  },
+  gnosis: {
+    fetch: fetchGc,
+  },
+  syscoin: {
+    fetch: fetchSysCoin,
+  },
+  telos: {
+    fetch: fetchTlos,
+  },
+  moonriver: {
+    fetch: fetchMoonriver,
+  },
+  milkomeda: {
+    fetch: fetchMilkoMeda,
+  },
+  moonbeam: {
+    fetch: fetchMoonbeam,
   },
 
   fetch,
