b3d8daa68b2a43c8954ab0a620f5baee235a79c7
astrit11 <astritmaliku@yahoo.com>
2022-04-20 16:02:40 +0200

    Added borrowed volume and fixed tvl calculation

--- projects/smartcredit.js
+++ projects/smartcredit.js
@@ -4,22 +4,47 @@ const { GraphQLClient, gql } = require('graphql-request')
 var endpoint ='https://d2c7awq32ho327.cloudfront.net/graphql';
   var graphQLClient = new GraphQLClient(endpoint)
 
-  var query = gql`
-  {
-    getTVL {
-      platformTVL,
+  var platformTVLQuery = gql`
+    {
+      getTVL {
+        platformTVL,
+        borrowingTVL
     }
+
+  }
+  `;
+
+
+  var borrowingTVLQuery = gql`
+    {
+      getTVL {
+        borrowingTVL
+    }
+
   }
   `;
 async function fetch() {
   try {
-    const {getTVL} = await retry(async bail => await graphQLClient.request(query))
-    return Promise.resolve(getTVL.platformTVL)
+    const {getTVL} = await retry(async bail => await graphQLClient.request(platformTVLQuery))
+    return Promise.resolve(getTVL.platformTVL-getTVL.borrowingTVL)
+  } catch (err) {
+    Promise.reject(err)
+  }
+}  
+
+
+async function borrowed() {
+  try {
+    const {getTVL} = await retry(async bail => await graphQLClient.request(borrowingTVLQuery))
+    return Promise.resolve(getTVL.borrowingTVL)
   } catch (err) {
     Promise.reject(err)
   }
 }  
 
 module.exports = {
-    fetch
+  borrowed:{
+    fetch:borrowed
+  },
+  fetch
 }
