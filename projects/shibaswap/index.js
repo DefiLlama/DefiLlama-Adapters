fbebd394ca723455d4e8d9890e23841bc2a5c691
waynebruce0x <waynebruce0x@gmail.com>
2022-04-22 16:46:22 +0100

    shiba revert

--- projects/shibaswap/index.js
+++ projects/shibaswap/index.js
@@ -5,7 +5,6 @@ const FACTORY = '0x115934131916c8b277dd010ee02de363c09d037c';
 async function tvl(_, ethBlock, chainBlocks) {
   const chain = 'ethereum'
   const balances = await calculateUniTvl(id=>id, ethBlock, chain, FACTORY, 0, true)
-  delete balances['0x3f7aff0ef20aa2e646290dfa4e67611b2220c597']
   return balances
 };
 
