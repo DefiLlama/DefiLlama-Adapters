461e051ded0e2d94bfdddbbf0c9b34c175819894
Julian Rachman <julianrachman@julians-mbp.lan>
2022-04-24 20:04:47 -0700

    added milkomeda and moonbeam proxycontracts

--- projects/nomad/index.js
+++ projects/nomad/index.js
@@ -1,63 +1,88 @@
-const sdk = require('@defillama/sdk');
-// const { transformBscAddress } = require('../helper/portedTokens');
-const NOMAD_ETH_BRIDGE_CONTRACT = '0x88A69B4E698A4B090DF6CF5Bd7B2D47325Ad30A3';
+// proxy contract:
+//     https://github.com/nomad-xyz/configuration/blob/main/configs/production.json
+// token holdings:
+//     https://etherscan.io/tokenholdings?a=PUT CONTRACT ADDRESS HERE
 
-const HOME_CHAINS = ['ethereum'];
+const sdk = require("@defillama/sdk");
+const { getBlock } = require("../helper/getBlock");
+const { chainExports } = require("../helper/exports");
 
-const TOKEN_ADDRESSES = {
-    'ethereum': [
-      '0x853d955aCEf822Db058eb8505911ED77F175b99e', //FRAX
-      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', //USDC
-      '0x6B175474E89094C44Da98b954EedeAC495271d0F', //DAI
-      '0xdAC17F958D2ee523a2206206994597C13D831ec7', //UST
-      '0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0', //FXS
-      '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', //WETH
-      '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', //WBTC
-    ]
+const HOME_CHAINS = {
+  'ethereum': '0x88A69B4E698A4B090DF6CF5Bd7B2D47325Ad30A3', 
+  'moonbeam': '0xD3dfD3eDe74E0DCEBC1AA685e151332857efCe2d', 
+  'milkomedaC1': '0x9faF7f27c46ACdeCEe58Eb4B0Ab6489E603EC251'
 };
 
-async function tvl(timestamp, block, chainBlocks) {
-  const balances = {};
+const TOKEN_ADDRESSES = [
+  {
+    // FRAX
+    'ethereum': '0x853d955aCEf822Db058eb8505911ED77F175b99e'
+  },
+  {
+    // USDC
+    'ethereum': '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+  },
+  {
+    // DAI
+    'ethereum': '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+  },
+  {
+    // USDT
+    'ethereum': '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+  },
+  {
+    // FXS
+    'ethereum': '0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0'
+  },
+  {
+    // WETH
+    'ethereum': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+  },
+  {
+    // WBTC
+    'ethereum': '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
+  },
+  {
+    // IAG
+    'ethereum': '0x40EB746DEE876aC1E78697b7Ca85142D178A1Fc8'
+  },
+  {
+    // CQT
+    'ethereum': '0xD417144312DbF50465b1C641d016962017Ef6240'
+  }
+];
 
-  console.log(block);
+function tvl(chain) {
+  return async (time, _, chainBlocks) => {
+    const balances = {};
+    const block = await getBlock(time, chain, chainBlocks, true);
+    // console.log(block);
   
-  // const block = await getBlock(timestamp, 'ethereum', chainBlocks, true);
- 
-  await Promise.all(
-    HOME_CHAINS.map(async (chain) => {
-      await Promise.all(
-        TOKEN_ADDRESSES[chain].map(async (target) => {
+    await Promise.all(
+      TOKEN_ADDRESSES.map(async (token) => {
+        const target = token[chain];
+
+        if (target != undefined) {
+          const owner = HOME_CHAINS[chain];
+          
           const tokenBalance = await sdk.api.erc20.balanceOf({
             chain,
             block,
             target,
-            owner: NOMAD_ETH_BRIDGE_CONTRACT,
+            owner,
           });
 
+          // console.log(tokenBalance.output);
+
           await sdk.util.sumSingleBalance(balances, target, tokenBalance.output);
-        })
-      ) 
-    })
-  
-    // let tokenAddress;
-    // // Use the Ethereum address if possible.
-    // // Also do special handling for tokens with different decimals on some chains.
-    // if (token.coingecko) {
-    //   // Some tokens don't have the Ethereum address registered on Coingecko.
-    //   tokenAddress = token.coingecko;
-    // } else if (
-    //   (!token.ethereum ||
-    //     (tokensWithDifferentDecimals.includes(token.ethereum) &&
-    //       chainsWithDifferentDecimals.includes(chain))) &&
-    //   chainToCoingeckoId[chain]
-    // ) {
-    //   tokenAddress = chain + ":" + token[chain];
-    // } else {
-    //   tokenAddress = token.ethereum;
-    // }
-  
-  )
-  return balances;
+
+          // console.log(balances);
+        }
+      })
+    );
+    
+    return balances;
+  }
 }
 
 module.exports = {
@@ -65,7 +90,5 @@ module.exports = {
   misrepresentedTokens: false,
   methodology: 'counts the total amount of assets locked in the Nomad token bridge.',
   start: 13983843,
-  ethereum: {
-    tvl,
-  }
+  ...chainExports(tvl, Object.keys(HOME_CHAINS))
 }; 
