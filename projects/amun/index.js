14c49edf949974dc5b2c5137218ec589b5cf5ff8
Victor Genin <victor@fanai.io>
2022-04-25 18:35:45 +0300

    clean and pretty

--- projects/amun/index.js
+++ projects/amun/index.js
@@ -1,20 +1,23 @@
-const sdk = require('@defillama/sdk');
+const sdk = require("@defillama/sdk");
 const { default: computeTVL } = require("@defillama/sdk/build/computeTVL");
 const { util } = require("@defillama/sdk");
-const abi = require('./abi.json');
-const axios = require('axios');
-const solana = require('../helper/solana');
-const utils = require('../helper/utils')
+const abi = require("./abi.json");
+const axios = require("axios");
+const solana = require("../helper/solana");
+const utils = require("../helper/utils");
 const BigNumber = require("bignumber.js");
 
-const PECO_ADDRESS = '0xA9536B9c75A9E0faE3B56a96AC8EdF76AbC91978';
-const DFI_ADDRESS = '0xA9536B9c75A9E0faE3B56a96AC8EdF76AbC91978';
-const SOLI_ADDRESS = '8JnNWJ46yfdq8sKgT1Lk4G7VWkAA8Rhh7LhqgJ6WY41G';
+const PECO_ADDRESS = "0xA9536B9c75A9E0faE3B56a96AC8EdF76AbC91978";
+const DFI_ADDRESS = "0xA9536B9c75A9E0faE3B56a96AC8EdF76AbC91978";
+const SOLI_ADDRESS = "8JnNWJ46yfdq8sKgT1Lk4G7VWkAA8Rhh7LhqgJ6WY41G";
 
 const knownTokenPrices = {};
 
 async function getSolanaTokenRate(address) {
-    return await axios.get("https://lima-api.amun.com/tokenRate/APygy51vntbQQsYmgkF1CeMsAn5Tv1rsVUB6nee3wJpY?chain=solana&address="+address);
+  return await axios.get(
+    "https://lima-api.amun.com/tokenRate/APygy51vntbQQsYmgkF1CeMsAn5Tv1rsVUB6nee3wJpY?chain=solana&address=" +
+      address
+  );
 }
 
 const locks = [];
@@ -37,87 +40,96 @@ setInterval(() => {
 const maxCoingeckoRetries = 5;
 
 function chainTvl(chain, tokens) {
-    if (chain === 'solana') {
-        const decimals = 6;
-        return async (timestamp, ethBlock, chainBlocks) => {
-            const balances = {}
-            for (const token of tokens) {
-                const rate = await getSolanaTokenRate(token);
-                const tokenSupply = await solana.getTokenSupply(token);
-                const balance = Math.trunc(
-                    rate.data.value * tokenSupply
-                  );
-
-                sdk.util.sumSingleBalance(balances, chain+':'+token, balance);
-            }
-
-            let total = 0;
-            for (var key in balances) {
-                total += balances[key];
-            }
-
-            return total;
-        }
-    }
-
+  if (chain === "solana") {
     return async (timestamp, ethBlock, chainBlocks) => {
-        const block = chainBlocks[chain]
-        const balances = {};
-        for (const address of tokens) {
-            const underlyings = await sdk.api.abi.call({
-                block,
-                target: address,
-                abi: abi.getTokens,
-                chain
-            });
-            for (const token of underlyings.output) {
-                const held = await sdk.api.erc20.balanceOf({
-                    block,
-                    target: token,
-                    owner: address,
-                    chain
-                });
-                sdk.util.sumSingleBalance(balances, chain + ':' + token, held.output);
-            };
-        }
+      const balances = {};
+      for (const token of tokens) {
+        const rate = await getSolanaTokenRate(token);
+        const tokenSupply = await solana.getTokenSupply(token);
+        const balance = Math.trunc(rate.data.value * tokenSupply);
 
-        const tvlResults = await computeTVL(
-            balances,
-            "now",
-            false,
-            knownTokenPrices,
-            getCoingeckoLock,
-            maxCoingeckoRetries
-          );
+        sdk.util.sumSingleBalance(balances, chain + ":" + token, balance);
+      }
 
-        let total = tvlResults["usdTvl"];
-        return total;
+      let total = 0;
+      for (var key in balances) {
+        total += balances[key];
+      }
+
+      return total;
+    };
+  }
+
+  return async (timestamp, ethBlock, chainBlocks) => {
+    const block = chainBlocks[chain];
+    const balances = {};
+    for (const address of tokens) {
+      const underlyings = await sdk.api.abi.call({
+        block,
+        target: address,
+        abi: abi.getTokens,
+        chain,
+      });
+      for (const token of underlyings.output) {
+        const held = await sdk.api.erc20.balanceOf({
+          block,
+          target: token,
+          owner: address,
+          chain,
+        });
+        sdk.util.sumSingleBalance(balances, chain + ":" + token, held.output);
+      }
     }
+
+    const tvlResults = await computeTVL(
+      balances,
+      "now",
+      false,
+      knownTokenPrices,
+      getCoingeckoLock,
+      maxCoingeckoRetries
+    );
+
+    let total = tvlResults["usdTvl"];
+    return total;
+  };
 }
 
 async function fetch(timestamp, ethBlock, chainBlocks) {
-	const ethereumTVL = await chainTvl("ethereum", [DFI_ADDRESS])(timestamp, ethBlock, chainBlocks);
-	const polygonTVL = await chainTvl("polygon", [PECO_ADDRESS])(timestamp, ethBlock, chainBlocks);
-	const solanaTVL = await chainTvl("solana", [SOLI_ADDRESS])(timestamp, ethBlock, chainBlocks);
+  const ethereumTVL = await chainTvl("ethereum", [DFI_ADDRESS])(
+    timestamp,
+    ethBlock,
+    chainBlocks
+  );
+  const polygonTVL = await chainTvl("polygon", [PECO_ADDRESS])(
+    timestamp,
+    ethBlock,
+    chainBlocks
+  );
+  const solanaTVL = await chainTvl("solana", [SOLI_ADDRESS])(
+    timestamp,
+    ethBlock,
+    chainBlocks
+  );
 
-	// sum up tvl on all chains
-    
-    const tvl = solanaTVL + ethereumTVL + polygonTVL;
+  // sum up tvl on all chains
 
-	return tvl;
+  const tvl = solanaTVL + ethereumTVL + polygonTVL;
+
+  return tvl;
 }
 
 module.exports = {
-    timetravel: false,
-    ethereum: {
-        fetch: chainTvl("ethereum", [DFI_ADDRESS])
-    },
-    polygon: {
-        fetch: chainTvl("polygon", [PECO_ADDRESS])
-    },
-    solana: {
-        fetch: chainTvl("solana", [SOLI_ADDRESS])
-    },
-    fetch,
-    methodology: `Amun Tokens has three investment strategies available, which are the Defi Token Index(DFI), the Polygon Ecosystem Index (PECO) and Solana Ecosystem Index (SOLI). Each strategy has its own address where the underlying tokens are held. To get the TVL for the DFI and PECO, first of all, an on-chain call is made using the function 'tvl()', which first retrieves each token that is held within the strategy addresses and then calls 'balanceOf()' to get the balances of these tokens which are added and used as TVL. For SOLI, getTokenSupply helper method is called to get the total supply of the token, and then multiplied at the current market rate of the token, retrieved from our API endpoint.`
+  timetravel: false,
+  ethereum: {
+    fetch: chainTvl("ethereum", [DFI_ADDRESS]),
+  },
+  polygon: {
+    fetch: chainTvl("polygon", [PECO_ADDRESS]),
+  },
+  solana: {
+    fetch: chainTvl("solana", [SOLI_ADDRESS]),
+  },
+  fetch,
+  methodology: `Amun Tokens has three investment strategies available, which are the Defi Token Index(DFI), the Polygon Ecosystem Index (PECO) and Solana Ecosystem Index (SOLI). Each strategy has its own address where the underlying tokens are held. To get the TVL for the DFI and PECO, first of all, an on-chain call is made using the function 'tvl()', which first retrieves each token that is held within the strategy addresses and then calls 'balanceOf()' to get the balances of these tokens which are added and used as TVL. For SOLI, getTokenSupply helper method is called to get the total supply of the token, and then multiplied at the current market rate of the token, retrieved from our API endpoint.`,
 };
