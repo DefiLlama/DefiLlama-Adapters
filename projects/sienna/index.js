c567f8b54405e1f79f077445243b52f78693d7fd
Ionut Moldovan <moldovan.ionut.89@gmail.com>
2022-04-26 15:56:27 +0300

    grab all data from the chain

--- projects/sienna/index.js
+++ projects/sienna/index.js
@@ -1,72 +1,60 @@
 const { CosmWasmClient } = require("secretjs");
-const axios = require("axios");
-const retry = require("../helper/retry");
 const BigNumber = require("bignumber.js");
 const sdk = require('@defillama/sdk');
-
-const BACKEND_URL = "https://ethereumbridgebackend.azurewebsites.net/";
-const SECRET_NODE_URL = "https://bridgeapi.azure-api.net/proxy/";
-const SIENNA_CONTRACT_ADDRESS = "secret1rgm2m5t530tdzyd99775n6vzumxa5luxcllml4";
-const LEND_OVERSEER_CONTRACT = null;
-
 const utils = require("./utils");
 
+const factoryContract = "secret1zvk7pvhtme6j8yw3ryv0jdtgg937w0g0ggu8yy";
+const pairCodeId = 111;
+const factoryContractV2 = "secret18sq0ux28kt2z7dlze2mu57d3ua0u5ayzwp6v2r";
+const pairCodeIdV2 = 361;
+const SIENNA_SINGLE_SIDED_POOLS = [
+    { address: "secret1ja57vrpqusx99rgvxacvej3vhzhh4rhlkdkd7w", version: 1 },
+    { address: "secret109g22wm3q3nfys0v6uh7lqg68cn6244n2he4t6", version: 2 },
+    { address: "secret1uta9zf3prn7lvc6whp8sqv7ynxmtz3jz9xkyu7", version: 3 }
+];
+const SIENNA_TOKEN_ADDRESS = "secret1rgm2m5t530tdzyd99775n6vzumxa5luxcllml4";
+const LEND_OVERSEER_CONTRACT = null;
+
+const SECRET_NODE_URL = "https://bridgeapi.azure-api.net/proxy/";
 const queryClient = new CosmWasmClient(SECRET_NODE_URL);
 
-async function Tokens() {
-
-    return (await retry(
-        async () =>
-            axios({
-                method: "GET",
-                baseURL: BACKEND_URL,
-                url: "tokens"
-            })
-    )).data.tokens;
-}
-
 async function Pairs() {
-    return (await retry(
-        async () =>
-            axios({
-                method: "GET",
-                baseURL: BACKEND_URL,
-                url: "secretswap_pairs"
-            }))
-    ).data.pairs.map(pair => pair.contract_addr);
+    return (await PairsV1()).concat(await PairsV2());
 }
 
-
-async function SiennaSingleSidedPools() {
-    return (await retry(
-        async () =>
-            axios({
-                method: "GET",
-                baseURL: BACKEND_URL,
-                url: "rewards"
-            })
-    )).data.pools.filter(pool => pool.inc_token.address === SIENNA_CONTRACT_ADDRESS);
+async function PairsV1() {
+    const pairs = await queryClient.getContracts(pairCodeId);
+    return pairs.filter((p) => p.label.endsWith(`${factoryContract}-${pairCodeId}`));
 }
 
-function TokenByAddress(tokenAddress, tokens) {
-    return tokens.find(token => token.dst_address === tokenAddress);
+async function PairsV2() {
+    const pairs = await queryClient.getContracts(pairCodeIdV2);
+    return pairs.filter((p) => p.label.endsWith(`${factoryContractV2}-${pairCodeIdV2}`));
 }
 
-async function PairsVolumes(pairs, tokens) {
+async function TokenInfo(tokenAddress) {
+    const result = await queryClient.queryContractSmart(tokenAddress, { token_info: {} });
+    return result.token_info;
+}
+
+async function PairsVolumes() {
     const volumes = []
-    await Promise.all(pairs.map(async contract => {
-        const pair_info = (await queryClient.queryContractSmart(contract, "pair_info")).pair_info;
 
-        const token1 = TokenByAddress(pair_info.pair.token_0.custom_token.contract_addr, tokens)
+    const pairs = await Pairs();
+
+    await Promise.all(pairs.map(async contract => {
+        const pair_info = (await queryClient.queryContractSmart(contract.address, "pair_info")).pair_info;
+
+        const token1 = await TokenInfo(pair_info.pair.token_0.custom_token.contract_addr);
         volumes.push({
             tokens: new BigNumber(pair_info.amount_0).div(new BigNumber(10).pow(token1.decimals)).toNumber(),
-            tokenSymbol: token1.display_props.symbol
+            symbol: token1.symbol
         });
 
-        const token2 = TokenByAddress(pair_info.pair.token_1.custom_token.contract_addr, tokens)
+        const token2 = await TokenInfo(pair_info.pair.token_1.custom_token.contract_addr);
         volumes.push({
             tokens: new BigNumber(pair_info.amount_1).div(new BigNumber(10).pow(token2.decimals)).toNumber(),
-            tokenSymbol: token2.display_props.symbol
+            symbol: token2.symbol
         });
     }));
     return volumes;
@@ -94,7 +82,7 @@ async function getLendMarkets() {
     return markets;
 }
 
-async function Lend(tokens) {
+async function Lend() {
     const markets = await getLendMarkets();
     const block = await queryClient.getHeight();
     return Promise.all(markets.map(async (market) => {
@@ -109,25 +97,24 @@ async function Lend(tokens) {
             }
         });
         const underlying_asset = await queryClient.queryContractSmart(market.contract.address, { underlying_asset: {} });
-        const token = TokenByAddress(underlying_asset.address, tokens);
+        const token = await TokenInfo(underlying_asset.address);
         return {
-            tokenSymbol: token.display_props.symbol,
+            symbol: token.symbol,
             tokens_supplied: new BigNumber(marketState.total_supply).times(exchange_rate).div(new BigNumber(10).pow(token.decimals)).toNumber(),
             tokens_borrowed: new BigNumber(marketState.total_borrows).div(new BigNumber(10).pow(token.decimals).toNumber()).toNumber(),
         };
     }));
 }
 
-async function StakedTokens(tokens) {
-    const contracts = await SiennaSingleSidedPools();
-    const siennaToken = TokenByAddress(SIENNA_CONTRACT_ADDRESS, tokens);
-    const stakedTokens = await Promise.all(contracts.map(async (contract) => {
+async function StakedTokens() {
+    const siennaToken = await TokenInfo(SIENNA_TOKEN_ADDRESS);
+    const stakedTokens = await Promise.all(SIENNA_SINGLE_SIDED_POOLS.map(async (pool) => {
         let total_locked;
-        if (contract.version === "3") {
-            const fetchedPool = await queryClient.queryContractSmart(contract.rewards_contract, { rewards: { pool_info: { at: new Date().getTime() } } });
+        if (pool.version === 3) {
+            const fetchedPool = await queryClient.queryContractSmart(pool.address, { rewards: { pool_info: { at: new Date().getTime() } } });
             total_locked = fetchedPool.rewards.pool_info.staked;
         } else {
-            const fetchedPool = await queryClient.queryContractSmart(contract.rewards_contract, { pool_info: { at: new Date().getTime() } });
+            const fetchedPool = await queryClient.queryContractSmart(pool.address, { pool_info: { at: new Date().getTime() } });
             total_locked = fetchedPool.pool_info.pool_locked;
         }
         return new BigNumber(total_locked).div(new BigNumber(10).pow(siennaToken.decimals)).toNumber();
@@ -138,78 +125,28 @@ async function StakedTokens(tokens) {
 async function TVL() {
     const balances = {};
 
-    const tokens = await Tokens();
-    const pairs = await Pairs();
-
-    const pairs_volumes = await PairsVolumes(pairs, tokens);
+    const pairs_volumes = await PairsVolumes();
     await Promise.all(pairs_volumes.map(async volume => {
-        if (utils.symbolsMap[volume.tokenSymbol]) await sdk.util.sumSingleBalance(balances, utils.symbolsMap[volume.tokenSymbol], volume.tokens);
+        if (utils.symbolsMap[volume.symbol]) await sdk.util.sumSingleBalance(balances, utils.symbolsMap[volume.symbol], volume.tokens);
     }));
 
-    const staked_tokens = await StakedTokens(tokens);
+    const staked_tokens = await StakedTokens();
     if (staked_tokens) await sdk.util.sumSingleBalance(balances, "sienna", staked_tokens);
 
-    const lend_data = await Lend(tokens);
+    const lend_data = await Lend();
 
     await Promise.all(lend_data.map(async volume => {
-        if (utils.symbolsMap[volume.tokenSymbol]) {
-            
-            await sdk.util.sumSingleBalance(balances, utils.symbolsMap[volume.tokenSymbol], volume.tokens_supplied);
-        }
+        if (utils.symbolsMap[volume.symbol]) await sdk.util.sumSingleBalance(balances, utils.symbolsMap[volume.symbol], volume.tokens_supplied);
     }));
 
     return balances;
 }
-
-async function Staked() {
-    const balances = {};
-
-    const tokens = await Tokens();
-    const staked_tokens = await StakedTokens(tokens);
-    if (staked_tokens) await sdk.util.sumSingleBalance(balances, "sienna", staked_tokens);
-
-    return balances;
-}
-
-async function Borrowed() {
-    const balances = {};
-
-    const tokens = await Tokens();
-    
-    const lend_data = await Lend(tokens);
-    await Promise.all(lend_data.map(async volume => {
-        if (utils.symbolsMap[volume.tokenSymbol]) {
-            await sdk.util.sumSingleBalance(balances, utils.symbolsMap[volume.tokenSymbol], volume.tokens_borrowed);
-        }
-    }));
-
-    return balances;
-}
-
-async function Pool2() {
-    const balances = {};
-
-    const tokens = await Tokens();
-    const pairs = await Pairs();
-
-    const pairs_volumes = await PairsVolumes(pairs, tokens);
-    await Promise.all(pairs_volumes.map(async volume => {
-        if (utils.symbolsMap[volume.tokenSymbol]) await sdk.util.sumSingleBalance(balances, utils.symbolsMap[volume.tokenSymbol], volume.tokens);
-    }));
-
-    return balances;
-}
-
 
 module.exports = {
     misrepresentedTokens: true,
     timetravel: false,
-    methodology: 'All tokens locked in SIENNA Network pairs + All the supplied tokens to Sienna Lend Markets + Staked Sienna; borrowed tokens across all markets',
+    methodology: 'All tokens locked in SIENNA Network pairs + All the supplied tokens to Sienna Lend Markets + Staked Sienna;',
     secret: {
-        staking: Staked,
-        pool2: Pool2,
-        borrowed: Borrowed,
         tvl: TVL
-    },
-
+    }
 };
