392b62ad093d836af451c0c9b4e40ed76197172b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-04-28 22:58:39 +0530

    Fix mahadao adapter

--- projects/mahadao/ethereum.js
+++ projects/mahadao/ethereum.js
@@ -1,7 +1,5 @@
-const { balanceOf, totalSupply } = require("@defillama/sdk/build/erc20");
-const { sumTokensAndLPsSharedOwners } = require("../helper/unwrapLPs.js");
+const { unwrapTroves, sumTokens } = require("../helper/unwrapLPs.js");
 const BigNumber = require("bignumber.js");
-const getEntireSystemCollAbi = require("../helper/abis/getEntireSystemColl.abi.json");
 const sdk = require("@defillama/sdk");
 
 const chain = "ethereum";
@@ -19,117 +17,41 @@ const eth = {
   frxArthStaking: "0x7B2F31Fe97f32760c5d6A4021eeA132d44D22039"
 }
 
-const getBalanceOfLP = async (
-  balances,
-  lpAddress,
-  tokenAddresses,
-  tokenNames,
-) => {
+Object.keys(eth).forEach(k => eth[k] = eth[k].toLowerCase())
 
-  const token1Balance = await balanceOf({
-    target: tokenAddresses[0],
-    owner: lpAddress,
-    chain: 'ethereum'
-  })
+async function pool2(_, block) {
+  const balances = {};
+  const tokensAndOwners = [
+    [eth.ethMahaSLP, eth.ethMahaSushiStaking,],
+  ]
+  const fraxArthbalances = {};
+  const fraxArthTokensAndOwners = [
+    [eth.frxArthLP, eth.frxArthStaking,],
+    [eth.frax, eth.frxArthLP,],
+    // [eth["arth.usd"], eth.frxArthLP,],
+  ]
 
-  const token2Balance = await balanceOf({
-    target: tokenAddresses[1],
-    owner: lpAddress,
-    chain: 'ethereum'
-  })
+  await sumTokens(balances, tokensAndOwners, block, chain, undefined, { resolveLP: true })
+  await sumTokens(fraxArthbalances, fraxArthTokensAndOwners, block, chain, undefined)
+  const { output: totalSupply } = await sdk.api.erc20.totalSupply({ target: eth.frxArthLP, block })
 
-  if (tokenNames[0] == 'arth.usd')
-    balances['arth'] = balances[tokenNames[0]] ? balances[tokenNames[0]] + (token1Balance.output / 1e18) / 2 : (token1Balance.output / 1e18) / 2
-  else
-    balances[tokenNames[0]] = balances[tokenNames[0]] ? balances[tokenNames[0]] + token1Balance.output / 1e18 : token1Balance.output / 1e18
+  const stakedRatio = BigNumber(fraxArthbalances[eth.frxArthLP]).dividedBy(totalSupply)
+  sdk.util.sumSingleBalance(balances, eth.frax, stakedRatio.multipliedBy(2).multipliedBy(fraxArthbalances[eth.frax]).toFixed(0))
+  // sdk.util.sumSingleBalance(balances, eth["arth.usd"], stakedRatio.multipliedBy(fraxArthbalances[eth["arth.usd"]]).toFixed(0))
 
-  if (tokenNames[1] == 'arth.usd')
-    balances['arth'] = balances[tokenNames[1]] ? balances[tokenNames[1]] + (token2Balance.output / 1e18) / 2 : (token2Balance.output / 1e18) / 2
-  else
-    balances[tokenNames[1]] = balances[tokenNames[1]] ? balances[tokenNames[1]] + token2Balance.output / 1e18 : token2Balance.output / 1e18
-
-};
-
-
-function pool2s() {
-  return async (_timestamp, _ethBlock, chainBlocks) => {
-    const balances = {};
-
-    // calculate tvl for regular uniswap lp tokens
-    await getBalanceOfLP(
-      balances,
-      eth.ethMahaSLP,
-      [eth.weth, eth.maha],
-      ['weth', 'mahadao']
-    );
-
-    await getBalanceOfLP(
-      balances,
-      eth.ethMahaSLP2,
-      [eth.weth, eth.maha],
-      ['weth', 'mahadao']
-    );
-
-    await getBalanceOfLP(
-      balances,
-      eth.frxArthLP,
-      [eth.frax, eth['arth.usd']],
-      ['frax', 'arth.usd']
-    );
-
-    return balances;
-  };
+  return balances;
 }
 
-async function getTVLv1(ret, troves, collaterals, chainBlocks) {
-  const block = chainBlocks[chain];
-
-  const tvls = await Promise.all(
-    troves.map((trove) =>
-      sdk.api.abi.call({
-        target: trove,
-        abi: getEntireSystemCollAbi,
-        block,
-        chain,
-      })
-    )
-  );
-
-  collaterals.forEach((collateral, index) => {
-    let key = chain + ":" + collateral;
-    let val = tvls[index].output;
-
-    if (ret[key] == undefined) ret[key] = BigNumber(0);
-    ret[key] = ret[key].plus(BigNumber(val));
-  });
-
-  return ret;
+async function tvl(_, block) {
+  const balances = {};
+  const troves = [
+    "0x4a47a8EB52c6213963727BF93baaa1CF66CBdF38", // FRAX Trove
+  ]
+  await unwrapTroves({ balances, chain, block, troves })
+  return balances;
 }
 
-function getEthereumTvl() {
-  return async (_, ethBlock, chainBlocks) => {
-    const ret = {};
-
-
-    await getTVLv1(
-      ret,
-      [
-        // trove
-        "0x4a47a8EB52c6213963727BF93baaa1CF66CBdF38", // FRAX Trove
-      ],
-      [
-        // collaterals
-        "0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0", // FRAX shares
-      ],
-      chainBlocks
-    );
-
-    return ret;
-  };
-}
 module.exports = {
-  pool2: pool2s(),
-  tvl: getEthereumTvl(
-
-  )
+  pool2,
+  tvl
 };
