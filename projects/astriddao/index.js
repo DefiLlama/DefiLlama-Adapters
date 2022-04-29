be1286fe38b5997da5f5e4940fbef7fa89fd2b34
KayAstrid <kevin@astriddao.xyz>
2022-04-28 22:40:06 -0700

    Make it display total locked ATID first

--- projects/astriddao/index.js
+++ projects/astriddao/index.js
@@ -1,52 +1,64 @@
 const sdk = require("@defillama/sdk");
+// In case BigInt is not available.
+const BN = require("bn.js");
 
-const ATID_ABI = require("./abi/WASTR.json");
-const BAI_ABI = require("./abi/WASTR.json");
-const WASTR_ABI = require("./abi/WASTR.json");
+const ActivePoolABI = require("./abi/ActivePool.abi.json");
+const DefaultPoolABI = require("./abi/ActivePool.abi.json");
 
-const NATIVE_TOKEN_WASTAR = "0x19574c3c8FaFc875051b665Ec131b7E60773d2C9";
-const START_BLOCK = 912772;
+const DECIMAL_PRECISION = new BN("1000000000000000000");
 
-const TOKENS_ADDRESSES = {
-  BAI: "0x733ebcC6DF85f8266349DEFD0980f8Ced9B45f35",
-  ATID: "0x5271D85CE4241b310C0B34b7C2f1f036686A6d7C",
-  WASTR: NATIVE_TOKEN_WASTAR,
+function getBNFromCallResponse(response) {
+  if ("output" in response) {
+    return new BN(response.output);
+  }
+  return new BN(0);
+}
+
+const CONTRACT_ADDRESSES = {
+  // Pools holding ASTR.
+  ACTIVE_POOL: "0x70724b57618548eE97623146F76206033E67086e",
+  DEFAULT_POOL: "0x2fE3FDf91786f75C92e8AB3B861588D3D051D83F",
 };
 
-const chain = "astar";
+const CHAIN = "astar";
 
-const ATID_CONTRACT = (
-  await sdk.api.abi.call({
-    target: TOKENS_ADDRESSES.ATID,
-    abi: ATID_ABI,
-    block,
-    chain,
-  })
-).output;
+// Once BAI is listed on CoinGecko, add locked BAIs into the system as TVL.
+async function getTotalASTRCollateralLockedInEthers(block) {
+  const [
+    activePoolASTRCollateralResponse,
+    defaultPoolASTRCollateralResponse
+  ] = await Promise.all([
+    sdk.api.abi.call({
+      chain: CHAIN,
+      target: CONTRACT_ADDRESSES.ACTIVE_POOL,
+      abi: ActivePoolABI.getCOL,
+      block,
+    }),
+    sdk.api.abi.call({
+      chain: CHAIN,
+      target: CONTRACT_ADDRESSES.DEFAULT_POOL,
+      abi: DefaultPoolABI.getCOL,
+      block,
+    })
+  ]);
+  let activePoolASTRCollateral = getBNFromCallResponse(activePoolASTRCollateralResponse);
+  let defaultPoolASTRCollateral = getBNFromCallResponse(defaultPoolASTRCollateralResponse);
+  return activePoolASTRCollateral.add(defaultPoolASTRCollateral).div(DECIMAL_PRECISION);
+}
 
-const BAI_CONTRACT = (
-  await sdk.api.abi.call({
-    target: TOKENS_ADDRESSES.BAI,
-    abi: BAI_ABI,
-    block,
-    chain,
-  })
-).output;
+async function tvl(timestamp, block, chainBlocks) {
+  const balances = {
+    astar: (await getTotalASTRCollateralLockedInEthers(block)).toString()
+  };
 
-const WASTR_CONTRACT = (
-  await sdk.api.abi.call({
-    target: TOKENS_ADDRESSES.WASTR,
-    abi: WASTR_ABI,
-    block,
-    chain,
-  })
-).output;
+  return balances;
+}
 
 module.exports = {
   timetravel: true,
-  methodology: "AstridDAO TVL Calculation and Total Supply of BAI",
+  start: 915830,
+  methodology: "Total locked ASTR (in wrapped ERC-20 form) in ActivePool and DefaultPool",
   astar: {
-    basicTVL: "",
-    Borrows: "",
+    tvl,
   },
 };
