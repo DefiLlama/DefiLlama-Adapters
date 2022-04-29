7d1590754debb0b89bd3ecdb974a9dc9ff05b905
graham01u <104616898+graham01u@users.noreply.github.com>
2022-04-29 16:03:12 +0900

    Add kleva adapter

---
+++ projects/kleva/index.js
@@ -0,0 +1,21 @@
+const retry = require('async-retry')
+const axios = require("axios");
+const BigNumber = require("bignumber.js");
+const { toUSDTBalances } = require('../helper/balances');
+
+async function fetchLiquidity() {
+  const tvl = await retry(async bail => await axios.get('https://bbs5kn3x0j.execute-api.ap-southeast-1.amazonaws.com/dev/klay/ksInfo/tvl'))
+
+  return toUSDTBalances(tvl.data)
+}
+
+(async() => {
+  await fetchLiquidity()
+})()
+
+module.exports = {
+  klaytn: {
+    tvl: fetchLiquidity,
+  },
+  timetravel: false,
+}
