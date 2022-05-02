ad957b64d53c6dbb84c423df736171fdc7501b5d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-05-02 18:06:34 +0530

    update pole adapter

--- projects/pole/index.js
+++ projects/pole/index.js
@@ -1,24 +1,21 @@
-const { Port } = require("@port.finance/port-sdk");
-const { WalletId } = require("@port.finance/port-sdk/lib/models/WalletId");
+const { sumTokensUnknown, getSolBalance } = require("../helper/solana")
+const BigNumber = require("bignumber.js")
+const tokens = require('./tokens.json')
+
+const contractAddress = '8x2uay8UgrLiX8AAYyF6AkK9z91nNtN6aLwfqPkf6TAQ'
+const tokensAndAccounts = tokens.map((token) => [
+  token.address,
+  contractAddress,
+  token.coingeckoId,
+]);
 
 async function tvl() {
-  const port = Port.forMainNet();
-  const reserveContext = await port.getReserveContext();
-  const portBalance = await port.getPortBalance(
-    WalletId.fromBase58("HNS2DL8GQRBk4ju2kHzfQDyCmkd5B4eeU3sjDp24f28i"),
-    reserveContext
-  );
-  const usdcCollateral = portBalance
-    .getCollaterals()[0]
-    .getRecordedValue()
-    .toNumber();
-  const usdcBorrow = portBalance.getLoans()[0].getRecordedValue().toNumber();
-  return {
-    "usd-coin": usdcCollateral - usdcBorrow,
-  };
+  return sumTokensUnknown(tokensAndAccounts);
 }
 
 module.exports = {
   timetravel: false,
-  tvl,
+  solana: {
+    tvl
+  },
 };
