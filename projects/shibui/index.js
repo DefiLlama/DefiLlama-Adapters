6d3b2e4082d5f3198d2f64946327c7745598f847
Nejc Drobnic <ethoarx@gmail.com>
2022-05-04 11:46:21 +0200

    refactor: remove unused token

--- projects/shibui/index.js
+++ projects/shibui/index.js
@@ -5,7 +5,6 @@ const { staking } = require('../helper/staking');
 
 const Boba_SHIBUI = "0xf08ad7c3f6b1c6843ba027ad54ed8ddb6d71169b";
 
-const Boba_WETH = "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000";
 const Boba_BOBA = "0xa18bF3994C0Cc6E3b63ac420308E5383f53120D7";
 const Boba_USDT = "0x5de1677344d3cb0d7d465c10b72a8f60699c062d";
 const Boba_SHIBUI_WETH = "0xcE9F38532B3d1e00a88e1f3347601dBC632E7a82";
@@ -14,7 +13,6 @@ const Boba_SHIBUI_USDT = "0x3f714fe1380ee2204ca499d1d8a171cbdfc39eaa";
 const CHAIN_ORGANISED_DATA = {
   boba: {
     treasuryTokens: [
-      [Boba_WETH, false],
       [Boba_BOBA, false],
       [Boba_USDT, false],
       [Boba_SHIBUI_WETH, true],
@@ -37,7 +35,8 @@ const chainTVL = (chain) => {
       balances,
       data.treasuryTokens,
       data.treasuryAddresses,
-      block
+      block,
+      chain
     );
 
     return balances;
@@ -46,3 +45,5 @@ const chainTVL = (chain) => {
 
 module.exports = chainExports(chainTVL, ["boba"]);
 module.exports.boba.staking = staking("0xabAF0A59Bd6E937F852aC38264fda35EC239De82", Boba_SHIBUI);
+
+module.exports.start = 394825;
