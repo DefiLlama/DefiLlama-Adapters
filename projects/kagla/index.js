19f50a3a26196ab522095dd1d441b01c61852822
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-05-05 23:12:57 +0530

    Removed unused @acala-network/api

--- projects/kagla/index.js
+++ projects/kagla/index.js
@@ -2,7 +2,7 @@ const { getBalances } = require("./pools");
 
 const START_BLOCK = 724359
 
-async function astar(_timestamp, block) {
+async function astar(_timestamp, _block, {astar: block}) {
   const chain = "astar"
   const balances = await getBalances(chain, block)
   return balances
