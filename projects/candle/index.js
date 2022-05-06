2fff843d36ce6f0c5e3b9e154757d258c3e3daa6
waynebruce0x <waynebruce0x@gmail.com>
2022-05-06 17:03:32 +0100

    candle

--- projects/candle/index.js
+++ projects/candle/index.js
@@ -18,12 +18,12 @@ module.exports = {
   ethereum: {
     staking: stakings(stakingContracts, CNDL),
     pool2: pool2s(stakingContracts, [USDC_CNDL_UNIV2]),
-    tvl: (async) => ({}),
+    tvl: () => ({}),
   },
   candle: {
-    staking: stakings(stakingContracts, CNDL),
-    pool2: pool2s(stakingContracts, [USDC_CNDL_UNIV2]),
-    tvl: (async) => ({}),
+    staking: stakings(stakingContracts, CNDL, 'candle'),
+    pool2: pool2s(stakingContracts, [USDC_CNDL_UNIV2], 'candle'),
+    tvl: () => ({}),
   },
   methodology: "Counts liquidty on the staking and pool2s only",
 
