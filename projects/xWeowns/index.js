a425b76e6fb3e10281e6fec02cc1b6cfe7a28ed3
kunalgoyal9 <kunalgoyal.goyal9@gmail.com>
2022-05-13 19:58:34 +0530

    xWeown pegged fix

--- projects/xWeowns/index.js
+++ projects/xWeowns/index.js
@@ -1,10 +1,23 @@
 const sdk = require("@defillama/sdk");
-const abi = require('./abi.json');
 
+const abi = {
+    "name": "exactPeggedAmount",
+    "type": "function",
+    "inputs": [],
+    "outputs": [
+        {
+            "name": "",
+            "type": "uint256",
+            "internalType": "uint256"
+        }
+    ],
+    "stateMutability": "view"
+}
 
 const contracts = {
     "xWeowns": "0xaBA0Bb586335B938a7a817A900017D891268d32c"
 };
+const xWeowns = "0x53f5f2D63999C497ac7bD996E59145BA588f16fD"
 
 async function tvl(timestamp, block, chainBlocks) {
     // console.log("abi:: ", abi)
@@ -14,34 +27,41 @@ async function tvl(timestamp, block, chainBlocks) {
         calls: Object.values(contracts).map((c) => ({
             target: c
         })),
-        abi: abi.totalSupply,
+        abi: 'erc20:totalSupply',
         block: chainBlocks.lachain,
         chain: 'lachain'
-    })).output;
+    })).output[0].output;
 
-    console.log("print lol")
+    console.log("supplies:: ", supplies)
 
-    // const decimals = (await sdk.api.abi.multiCall({
-    //     calls: Object.values(contracts).map((c) => ({
-    //         target: c
-    //     })),
-    //     abi: abi.decimals,
-    //     block: chainBlocks.lachain,
-    //     chain: 'lachain'
-    // })).output;
+    const decimals = (await sdk.api.abi.multiCall({
+        calls: Object.values(contracts).map((c) => ({
+            target: c
+        })),
+        abi: 'erc20:decimals',
+        block: chainBlocks.lachain,
+        chain: 'lachain'
+    })).output[0].output;
 
-    // const pegged = (await sdk.api.abi.multiCall({
-    //     calls: Object.values(contracts).map((c) => ({
-    //         target: c
-    //     })),
-    //     abi: abi.exactPeggedAmount,
-    //     block: chainBlocks.lachain,
-    //     chain: 'lachain'
-    // })).output;
+    console.log("decimals:: ", decimals)
+
+    const pegged = (await sdk.api.abi.multiCall({
+        calls: Object.values(contracts).map((c) => ({
+            target: c
+        })),
+        abi: abi,
+        block: chainBlocks.lachain,
+        chain: 'lachain'
+    })).output[0].output;
+
+    console.log("pegged:: ", pegged)
+
+    sdk.util.sumSingleBalance(balances, `lachain:${xWeowns}`, supplies);
+    
 
     // for (let i = 0; i < Object.keys(contracts).length; i++) {
         
-    //     console.log("supplies:: ", pegged[i].output)
+    //     // console.log("supplies:: ", pegged[i].output)
 
     //     sdk.util.sumSingleBalance(
     //         balances, 
@@ -49,6 +69,8 @@ async function tvl(timestamp, block, chainBlocks) {
     //         supplies[i].output / 10 ** decimals[i].output
     //         );
     // };
+
+    console.log("balances:: ", balances)
 
     return balances;
 };
