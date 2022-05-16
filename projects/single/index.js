b433fb5aeaa11695079891f55221315c201b4dbc
Benicio Takahashi <benicio@singlefinance.io>
2022-05-16 18:11:11 +0800

    add single dynamic wMasterChef & vaults data

--- projects/single/index.js
+++ projects/single/index.js
@@ -1,9 +1,10 @@
 const { sumTokens } = require("../helper/unwrapLPs")
 const { getChainTransform, getFixBalances } = require("../helper/portedTokens")
 const { getUserMasterChefBalances } = require("../helper/masterchef")
+const { getUserCraftsmanV2Balances } = require("./helpers")
 const { pools } = require("./cronos/pools")
-const { vaults } = require("./cronos/vaults")
 const vvsPoolInfoABI = require('./cronos/vvsPoolInfo.json')
+const { fetchURL } = require("../helper/utils")
 
 const chain = 'cronos'
 const VVS_WMASTERCHEF_ADDR = '0x05e0A288441dd6Ae69e412e4EB16f812498a4e32'
@@ -11,6 +12,8 @@ const VVS_MASTERCHEF = '0xDccd6455AE04b03d785F12196B492b18129564bc'
 const MMF_MASTERCHEF = '0x6bE34986Fdd1A91e4634eb6b9F8017439b7b5EDc'
 const MMF_WMASTERCHEF_ADDR = '0x539E14063fd3B5fC0C0ccf1E965397a284bC48cf'
 const SINGLE_TOKEN = '0x0804702a4e749d39a35fde73d1df0b1f1d6b8347'
+
+const API = 'https://api.singlefinance.io'
 
 async function staking(timestamp, _block, chainBlocks) {
   let balances = {}
@@ -27,8 +30,16 @@ async function tvl(tx, _block, chainBlocks) {
   const balances = {}
   const block = chainBlocks[chain]
   const fixBalances = await getFixBalances(chain)
-  await getUserMasterChefBalances({ balances, masterChefAddress:VVS_MASTERCHEF, userAddres: VVS_WMASTERCHEF_ADDR, block, chain, poolInfoABI: vvsPoolInfoABI, excludePool2: true, pool2Tokens: [ SINGLE_TOKEN ] })
-  await getUserMasterChefBalances({ balances, masterChefAddress:MMF_MASTERCHEF, userAddres: MMF_WMASTERCHEF_ADDR, block, chain, poolInfoABI: vvsPoolInfoABI, excludePool2: true, pool2Tokens: [ SINGLE_TOKEN ] })
+  const { data: { vaults, wmasterchefs } } = await fetchURL(`${BASE_API_URL}/api/protocol/contracts`)
+
+  for (const { masterChef: masterChefAddress, wMasterChef, name, ...rest } of wmasterchefs) {
+    if (name === "vvsMultiYield") {
+      await getUserCraftsmanV2Balances({ balances, masterChefAddress, userAddres: wMasterChef, block, chain, poolInfoABI: vvsPoolInfoABI, excludePool2: true, pool2Tokens: [ SINGLE_TOKEN ], craftsmanV1: rest.craftsmanV1 })
+      continue;
+    }
+    await getUserMasterChefBalances({ balances, masterChefAddress, userAddres: wMasterChef, block, chain, poolInfoABI: vvsPoolInfoABI, excludePool2: true, pool2Tokens: [ SINGLE_TOKEN ] })
+  }
+
   const tokenAndOwners = vaults.map(({token, address}) => [token, address])
   await sumTokens(balances, tokenAndOwners, block, chain) // Add lending pool tokens to balances
   fixBalances(balances)
@@ -41,8 +52,16 @@ async function pool2(tx, _block, chainBlocks) {
   const fixBalances = await getFixBalances(chain)
   const tokenAndOwners = pools.filter(pool => pool.isLP).map(pool => [pool.tokenContract, pool.address])
   await sumTokens(balances, tokenAndOwners, block, chain, undefined, { resolveLP: true }) // Add staked lp tokens to balances
-  await getUserMasterChefBalances({ balances, masterChefAddress:VVS_MASTERCHEF, userAddres: VVS_WMASTERCHEF_ADDR, block, chain, poolInfoABI: vvsPoolInfoABI, onlyPool2: true, pool2Tokens: [ SINGLE_TOKEN ] })
-  await getUserMasterChefBalances({ balances, masterChefAddress:MMF_MASTERCHEF, userAddres: MMF_WMASTERCHEF_ADDR, block, chain, poolInfoABI: vvsPoolInfoABI, onlyPool2: true, pool2Tokens: [ SINGLE_TOKEN ] })
+  const { data: { wmasterchefs } } = await fetchURL(`${BASE_API_URL}/api/protocol/contracts`)
+
+  for (const { masterChef: masterChefAddress, wMasterChef, name, ...rest } of wmasterchefs) {
+    if (name === "vvsMultiYield") {
+      await getUserCraftsmanV2Balances({ balances, masterChefAddress, userAddres: wMasterChef, block, chain, poolInfoABI: vvsPoolInfoABI, onlyPool2: true, pool2Tokens: [ SINGLE_TOKEN ], craftsmanV1: rest.craftsmanV1 })
+      continue;
+    }
+    await getUserMasterChefBalances({ balances, masterChefAddress, userAddres: wMasterChef, block, chain, poolInfoABI: vvsPoolInfoABI, onlyPool2: true, pool2Tokens: [ SINGLE_TOKEN ] })
+  }
+
   fixBalances(balances)
   return balances
 }
@@ -57,3 +76,5 @@ module.exports = {
     tvl, pool2, staking,
   },
 } // see if single will run with updated unwrapLPs
+
+
