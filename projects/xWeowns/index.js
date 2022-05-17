82e4306d2ab952e68e96abaf6c5abab40a019d51
waynebruce0x <waynebruce0x@gmail.com>
2022-05-17 14:18:16 +0100

    xweowns

--- projects/xWeowns/index.js
+++ projects/xWeowns/index.js
@@ -1,53 +1,22 @@
 const sdk = require("@defillama/sdk");
-const abi = require('./abi2.json');
-
 
 const contracts = {
-    "xWeowns": "0xaBA0Bb586335B938a7a817A900017D891268d32c"
+    "xWeowns": "0xaBA0Bb586335B938a7a817A900017D891268d32c",
+    "USDT": "0x32D2b9bBCf25525b8D7E92CBAB14Ca1a5f347B14",
+    "USDTethereum": "0xdac17f958d2ee523a2206206994597c13d831ec7"
 };
-// node test.js projects/xWeowns/index.js
+
 async function tvl(timestamp, block, chainBlocks) {
-    const balances = {};
-
-    const supplies = (await sdk.api.abi.multiCall({
-        calls: Object.values(contracts).map((c) => ({
-            target: c
-        })),
-        abi: 'erc20:totalSupply',
-        block: chainBlocks.lachain,
-        chain: 'lachain'
-    })).output;
-
-    const decimals = (await sdk.api.abi.multiCall({
-        calls: Object.values(contracts).map((c) => ({
-            target: c
-        })),
-        abi: 'erc20:decimals',
-        block: chainBlocks.lachain,
-        chain: 'lachain'
-    })).output;
-
-    const pegged = (await sdk.api.abi.multiCall({
-        calls: Object.values(contracts).map((c) => ({
-            target: c
-        })),
-        abi: abi.exactPeggedAmount,
-        block: chainBlocks.lachain,
-        chain: 'lachain'
-    })).output;
-
-    for (let i = 0; i < Object.keys(contracts).length; i++) {
-        
-        console.log("supplies:: ", pegged[i].output)
-
-        sdk.util.sumSingleBalance(
-            balances, 
-            Object.keys(contracts)[i], 
-            supplies[i].output / 10 ** decimals[i].output
-            );
+    return {
+        [ contracts.USDTethereum ]:
+        (await sdk.api.abi.call({
+            target: contracts.USDT,
+            params: contracts.xWeowns,
+            abi: 'erc20:balanceOf',
+            block: chainBlocks.lachain,
+            chain: 'lachain'
+        })).output
     };
-
-    return balances;
 };
 
 module.exports = {
