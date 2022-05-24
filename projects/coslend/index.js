df8e6837c0372d208384f1faaf82ab180fbb2a10
coslendteam <coslendteam@gmail.com>
2022-05-24 10:59:15 +0800

    Add Coslend

---
+++ projects/coslend/index.js
@@ -0,0 +1,8 @@
+const { usdCompoundExports } = require("../helper/compound");
+module.exports = {
+  evmos: usdCompoundExports(
+    "0x2C8b48Dc777C26dc857E1040D8ef3Bdd3B1ef499",
+    "evmos",
+    ""
+  ),
+};
