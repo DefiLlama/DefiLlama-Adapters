8bade4f335edf946b05ba8bef917134565836ede
BitByTheByte <73377045+BitByTheByte@users.noreply.github.com>
2022-05-24 11:28:19 +0200

    fix: subtract the amount reserved for the staking rewards

--- projects/cake-monster/index.js
+++ projects/cake-monster/index.js
@@ -1,4 +1,5 @@
 const sdk = require("@defillama/sdk");
+const BigNumber = require("bignumber.js");
 const { transformBscAddress } = require("../helper/portedTokens");
 const CM_TOKEN_CONTRACT = "0x8A5d7FCD4c90421d21d30fCC4435948aC3618B2f";
 const CM_STAKING_CONTRACT = "0xF7CDDF60CD076d4d64c613489aA00dCCf1E518F6";
@@ -17,11 +18,17 @@ async function staking(timestamp, block, chainBlocks) {
     })
   ).output;
 
+  let stakingBalanceCorrected = new BigNumber(stakingBalance)
+    .minus(new BigNumber(1000000000).times(10 ** 18))
+    .toFixed();
+
   await sdk.util.sumSingleBalance(
     balances,
     transform(CM_TOKEN_CONTRACT),
-    stakingBalance
+    stakingBalanceCorrected
   );
+
+  console.log(typeof stakingBalance);
 
   return balances;
 }
@@ -30,7 +37,7 @@ module.exports = {
   timetravel: true,
   misrepresentedTokens: false,
   methodology:
-    "counts the number of $MONSTA tokens in the Cake Monster Staking contract.",
+    "counts the number of $MONSTA tokens in the Cake Monster Staking contract, excluding the amount reserved for the staking rewards.",
   start: 15765654,
   bsc: {
     tvl: () => ({}),
