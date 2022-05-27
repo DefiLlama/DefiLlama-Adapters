4bff606b7dd3506a39c4677351a532527b7657b2
Stefano Faieta <9802152+stefanofa@users.noreply.github.com>
2022-05-27 16:35:58 +0200

    Delete newline

--- projects/helper/algorand.js
+++ projects/helper/algorand.js
@@ -40,5 +40,4 @@ module.exports = {
   lookupApplications: withLimiter(lookupApplications),
   lookupAccountByID: withLimiter(lookupAccountByID),
   searchAccounts: withLimiter(searchAccounts),
-
 }
