4666a0df841dbf1d32e3a5bf54605f4ec3236db2
Peter Miklos <pmiklos@sirokko.net>
2022-05-29 13:08:57 -0700

    added support to calculate TVL for non-native assets locked in Oswap

--- projects/helper/obyte.js
+++ projects/helper/obyte.js
@@ -1,29 +1,118 @@
 const utils = require('./utils')
 
 /**
- * @param {number} timestamp - unix timestamp in seconds from epoch of the moment in time for which the TVL is calculated
- * @param {string} address - the Obyte address of the base AA for which the total TVL is calculated
+ * @param {number} timestamp - unix timestamp in seconds from epoch of the moment in time for which the balances are requested
+ * @param {string} address - the Obyte address of the base AA for which the balances are fetched
  *
- * @return the total TVL of all AAs that are based on the base AA identified by address
+ * @return {Promise<object>} the balances of all assets of all AAs that are based on the base AA identified by address
  */
-async function fetchBaseAATvl(timestamp, address) {
+async function fetchBaseAABalances(timestamp, address) {
   /*
-   * Example result:
    * {
-   *   "type": "spot",
-   *   "series": "tvl",
    *   "subject": "GS23D3GQNNMNJ5TL4Z5PINZ5626WASMA",
-   *   "unit": "USD_MA",
-   *   "data": {
-   *     "2022-05-20T06:16:18.169Z": 8419.285627948508
+   *   "addresses": {
+   *     "67XYBBBME57DZMJPLOYNXSIMAIDHGUDW": {
+   *       "assets": {
+   *         "3XF+1slNoFxIVPvRupR5uf9AXluOm92nobzKyCCSE3c=": {
+   *           "balance": 829,
+   *           "burned": false,
+   *           "selfIssued": false,
+   *           "selfIssuedUncapped": false
+   *         },
+   *         "base": {
+   *           "balance": 36262,
+   *           "burned": false,
+   *           "selfIssued": false,
+   *           "selfIssuedUncapped": false
+   *         }
+   *       }
+   *     },
+   *     "TBLLH5DGDX6KU5UGHG4WDH4N7IC5FPKP": {
+   *        ...
+   *     },
    *   }
    * }
    */
-  const fetched = await utils.fetchURL(` https://api.charts.obyte.app/baseagents/${address}/tvl?ts=${timestamp}`)
-  const totalTvlResponse = fetched.data
-  return Object.values(totalTvlResponse.data)[0]
+  const fetched = await utils.fetchURL(` https://api.charts.obyte.app/baseagents/${address}/balances?ts=${timestamp}`)
+  return fetched.data
+}
+
+/**
+ * @return {Promise<object>} fetches all exchange rates traded on Oswap v1 and v2 plus a few externally defined tokens such as GBYTE-USD or BTC-USD
+ */
+async function fetchOswapExchangeRates() {
+  /*
+   * {
+   *   "BTC_USD": 29832,
+   *   "GBYTE_BTC": 0.0004509,
+   *   "GBYTE_USD": 13.4512488,
+   *   "+X9n1ni9OpH/0PFXdmeB4f16wSxSivW4/qcyOt1UEDI=_USD": 88.2777158012621,
+   *   "/1ReF/OW7wud1rqomgWMSeaetx8WjyD6eSTnGurTftU=_USD": 0.22874160911121644
+   * }
+   */
+  const fetched = await utils.fetchURL("https://v2-data.oswap.io/api/v1/exchangeRates")
+  return fetched.data
+}
+
+/**
+ * @return {Promise<object>} fetches assets traded on Oswap v1
+ */
+async function fetchOswapV1Assets() {
+  /*
+   * {
+   *  "O2-GBYTE-USDC": {
+   *    "asset_id": "cQZVAFFh0Aaj5kMMydWoTqcMDxpfzGzEZhhEaQSVbHA=",
+   *    "decimals": 0,
+   *    "description": "Oswap v2 LP shares GBYTE-USDC",
+   *    "symbol": "O2-GBYTE-USDC",
+   *    "supply": 5026551
+   *  }
+   * }
+   */
+  const fetched = await utils.fetchURL("https://v1-data.oswap.io/api/v1/assets")
+  const assets = fetched.data
+  return Object.values(assets).reduce((map, asset) => {
+    map[asset.asset_id] = asset
+    return map
+  }, {})
+}
+
+/**
+ * @return {Promise<object>} fetches assets traded on Oswap v2
+ */
+async function fetchOswapV2Assets() {
+  /*
+   * {
+   *  "O2-GBYTE-USDC": {
+   *    "asset_id": "cQZVAFFh0Aaj5kMMydWoTqcMDxpfzGzEZhhEaQSVbHA=",
+   *    "decimals": 0,
+   *    "description": "Oswap v2 LP shares GBYTE-USDC",
+   *    "symbol": "O2-GBYTE-USDC",
+   *    "supply": 5026551
+   *  }
+   * }
+   */
+  const fetched = await utils.fetchURL("https://v2-data.oswap.io/api/v1/assets")
+  const assets = fetched.data
+  return Object.values(assets).reduce((map, asset) => {
+    map[asset.asset_id] = asset
+    return map
+  }, {})
+}
+
+async function fetchOswapAssets() {
+  const [assets1, assets2] = await Promise.all([
+      fetchOswapV1Assets(),
+      fetchOswapV2Assets()
+  ])
+  return {
+    ...assets1,
+    ...assets2
+  }
 }
 
 module.exports = {
-  fetchBaseAATvl
+  fetchBaseAABalances,
+  fetchOswapExchangeRates,
+  fetchOswapAssets
 }
