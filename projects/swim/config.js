7315a8806c4b58d5817144debd252381a555fdf7
Kirito <kirito@exsphere.com>
2022-05-30 09:05:17 -1000

    add gst and gmt pools and modify config

--- projects/swim/config.js
+++ projects/swim/config.js
@@ -11,31 +11,73 @@ module.exports = {
               'Dn4noZ5jgGfkntzcQSUZ8czkreiZ1ForXYoV2H8Dm7S1', // Eth-USDT
               '5RpUwQ8wtdPCZHhu6MERp2RGrpobsbZ6MH5dDHkUjs2', // Bsc-BUSD
               '8qJSyQprMC57TWKaYEmetUR3UUiTP2M3hXdcvFhkZdmv', // Bsc-USDT
-            ]
+            ],
+            symbols: [
+              "usd-coin",
+              "tether",
+              "usd-coin",
+              "tether",
+              "binance-usd",
+              "tether",
+            ],
           },
           {
             pool: 'Ha7YEA5wRWyH2htfyMXw3VfLbtBHm4UoVXMpq8Ev6zJh', // Meta Avax-USDC
             tokens: [
               'FHfba3ov5P3RjaiLVgh8FTv4oirxQDoVXuoUUDvHuXax', // Avax-USDC
-            ]
+            ],
+            symbols: [
+              "usd-coin",
+            ],
           },
           {
             pool: 'EpvBni7vTfbTG95zf9sNcS9To1NEKnVMpCwZdb21tKsg', // Meta Avax-USDT
             tokens: [
               'Kz1csQA91WUGcQ2TB3o5kdGmWmMGp8eJcDEyHzNDVCX', // Avax-USDT
-            ]
+            ],
+            symbols: [
+              "tether",
+            ],
           },
           {
             pool: '2iLTifF3JDP65AjFKZ3t4mgfJdQVSmVCiM8Zca3TgvpU', // Meta Polygon-USDC
             tokens: [
               'E2VmbootbVCBkMNNxKQgCLMS1X3NoGMaYAsufaAsf7M', // Polygon-USDC
-            ]
+            ],
+            symbols: [
+              "usd-coin",
+            ],
           },
           {
             pool: '3uxBU3fRZzp3V7v9MTNZiDmjxDkKh3rZutLwFtnjJ2pQ', // Meta Polygon-USDT
             tokens: [
               '5goWRao6a3yNC4d6UjMdQxonkCMvKBwdpubU3qhfcdf1', // Polygon-USDT
+            ],
+            symbols: [
+              "tether",
+            ],
+          },
+          {
+            pool: '57k3vNmCivSYn7EwQNjcNFcCWAdohZ9xACfMhJGwKiBq', // GSTBNB-GSTSOL
+            tokens: [
+              'GDuUFXEhUm4jG71vPxYRX3VxUMJ5etGvHTR1iKwTdb6p', // GSTBNB
+              '8YYBkTNhpY9mFdCdZWM6mHNf8J6A9hGfimb33LEiiZ3x', // GSTSOL
+            ],
+            symbols: [
+              "green-satoshi-token-bsc",
+              "green-satoshi-token",
             ]
+          },
+          {
+            pool: 'HZr3bF8YEJWMV75Wi3aFEHEyLLk61VyQduXtunWtXNVQ', // GMTBNB-GMTSOL
+            tokens: [
+              '7dzFD8xQ3FDmVLxwn75UA9WhVnBsUdRAexASVvpXX3Bo', // GMTBNB
+              '2x7MjgopLXd3qETGLpY19cyZjHvVnGkrwVjTkJnBza4A', // GMTSOL
+            ],
+            symbols: [
+              "stepn",
+              "stepn",
+            ],
           },
         ],
       }
