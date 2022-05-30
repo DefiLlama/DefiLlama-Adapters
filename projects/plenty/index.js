7e40646069cbfd3fc9709b34cc8754db3f4754ed
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-05-30 18:59:04 +0200

    [plenty] minor refactor

--- projects/plenty/index.js
+++ projects/plenty/index.js
@@ -1,6 +1,5 @@
 const axios = require('axios')
 const { getTokenBalances, sumTokens, convertBalances } = require('../helper/tezos')
-const sdk = require('@defillama/sdk')
 const { getFixBalances } = require('../helper/portedTokens')
 
 async function tvl() {
@@ -19,10 +18,7 @@ async function tvl() {
   return balances
 }
 
-
-
 async function staking() {
-  const response = {}
   const fixBalances = await getFixBalances('tezos')
   const stakingAddress = 'KT1PxkrCckgh5fA5v2cZEE2bX5q2RV1rv8dj'
   const balances = await getTokenBalances(stakingAddress, false)
@@ -37,16 +33,16 @@ async function getConfig() {
 
   const a = data.slice(data.indexOf(': IConfig')) // find start of config file
 
-  const b = a.slice(a.indexOf('{'), a.lastIndexOf('}') + 1) // find first { and last } and get all data in between
-    .split('\n')  //remove all comment lines 
-    .map(i => {
-      if (i.includes('// ')) return i.slice(0, i.indexOf('// '))
-      return i
-    }).join('\n')
-    .replace(/\,(\s*[\}\]])/g, '$1')  // remove trailing commas
-    .replaceAll('\'', '"')  // convert single quotation to double
-    .replace(/(\s?)(\w+)\:([^\/])/g, '$1"$2":$3') // cover keys with qoutes, eg. key1: "value" -> "key1":"value"
-  return JSON.parse(b)
+  let text = a.slice(a.indexOf('{'), a.lastIndexOf('}') + 1) // find first { and last } and get all data in between
+  text = text.split('\n')  //remove all comment lines 
+  text = text.map(i => {
+    if (i.includes('// ')) return i.slice(0, i.indexOf('// '))
+    return i
+  }).join('\n')
+  text = text.replace(/\,(\s*[\}\]])/g, '$1')  // remove trailing commas
+  text = text.replaceAll('\'', '"')  // convert single quotation to double
+  text = text.replace(/(\s?)(\w+)\:([^\/])/g, '$1"$2":$3') // cover keys with qoutes, eg. key1: "value" -> "key1":"value"
+  return JSON.parse(text)
 }
 
 module.exports = {
