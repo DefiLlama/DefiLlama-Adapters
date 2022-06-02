5c3eb2b5cafcf844960fe7d81cabdbcb38580a30
Imran <sp18-rcs-007@cuilahore.edu.pk>
2022-06-03 00:48:31 +0500

    comments added

--- projects/algodex.js
+++ projects/algodex.js
@@ -5,7 +5,18 @@ async function tvl(){
     const data = await fetchURL("https://app.algodex.com/algodex-backend/tvl.php/")
     return toUSDTBalances(data.data.total_liquidity_in_usd)
 }
+//    (asaAmount * tinyMan)
+//    +
+// Sum(algoAmount * tinyMan_USD_ALGO_Price)
 
+// i need tinyman prices 
+// https://mainnet.analytics.tinyman.org/api/v1/current-asset-prices/
+// algoexplorer prices 
+// https://price.algoexplorerapi.io/price/algo-usd
+
+// for all assets whose usd prices are not available in tinyman , find prices from this api endpoint 
+//  and multiply with price of asset 0 in tinyman api
+// https://app.algodex.com/algodex-backend/assets.php
 module.exports={
     misrepresentedTokens:true,
     algodex:{
