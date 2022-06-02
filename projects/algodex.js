8a9997824d7c7275036f3b2588dd117105c030d4
Imran <sp18-rcs-007@cuilahore.edu.pk>
2022-06-03 01:58:16 +0500

    work in progress

--- projects/algodex.js
+++ projects/algodex.js
@@ -1,16 +1,25 @@
 const {fetchURL} = require('./helper/utils')
 const {toUSDTBalances} = require('./helper/balances')
 
+const usdPriceUrl  =  "https://mainnet.analytics.tinyman.org/api/v1/current-asset-prices/"
+const algoPriceUrl =  "https://app.algodex.com/algodex-backend/assets.php"
+const assetVolURL =   "https://app.algodex.com/algodex-backend/tvl.php/"
+
 async function tvl(){
-    const data = await fetchURL("https://app.algodex.com/algodex-backend/tvl.php/")
+    const volumData = await fetchURL(assetVolURL)
+    const usdPriceData = await fetchURL(usdPriceUrl)
+    const algoPriceData = await fetchURL(algoPriceUrl)
+    
     return toUSDTBalances(data.data.total_liquidity_in_usd)
 }
+
 //    (asaAmount * tinyMan)
 //    +
 // Sum(algoAmount * tinyMan_USD_ALGO_Price)
 
 // i need tinyman prices 
 // https://mainnet.analytics.tinyman.org/api/v1/current-asset-prices/
+
 // algoexplorer prices 
 // https://price.algoexplorerapi.io/price/algo-usd
 
