90fba489360adb08ee15758f957f93696832ac49
hirorylabo <penya.shk@gmail.com>
2022-06-03 17:56:22 +0900

    naming and ref fix

--- projects/bridge-mutual_clone/index.js
+++ projects/bridge-mutual_clone/index.js
@@ -1,21 +1,15 @@
 const sdk = require("@defillama/sdk");
 const abi = require("./abi.json");
-const BigNumber = require('bignumber.js');
 
 const chain = "ethereum";
 
 // addresses pools
-const CapitalPool = "0x131fb74c6fede6d6710ff224e07ce0ed8123f144";
-const insureStaking = "0x55978a6f6a4cfa00d5a8b442e93e42c025d0890c";
-const ClaimVoting = "0x81d73999fabec7e8355d76d1010afbe3068f08fa";
-// addresses tokens
 
 const usdc = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 const insure = "0xd83AE04c9eD29d6D3E6Bf720C71bc7BeB424393E";
 
-// addresses getters
-const PolicyBookRegistry = "0x131fb74c6fede6d6710ff224e07ce0ed8123f144";
-const ShieldMining = "0x3dc07E60ecB3d064d20c386217ceeF8e3905916b";
+const Vault = "0x131fb74c6fede6d6710ff224e07ce0ed8123f144";
+const VotingEscrow = "0x3dc07E60ecB3d064d20c386217ceeF8e3905916b";
 const vlINSURE = "0xA12ab76a82D118e33682AcB242180B4cc0d19E29";
 
 
@@ -25,7 +19,7 @@ async function tvl(timestamp, block) {
   // =================== GET usdc BALANCES =================== //
   const vusdcBalances = (
     await sdk.api.abi.call({
-      target: CapitalPool,
+      target: Vault,
       abi: abi["valueAll"],
       chain: chain,
       block: block,
@@ -43,7 +37,7 @@ async function staking(timestamp, block) {
 
   const veinsureBalances = (
     await sdk.api.abi.call({
-      target: ShieldMining,
+      target: VotingEscrow,
       abi: abi["supply"],
       chain: chain,
       block: block,
@@ -66,16 +60,9 @@ async function staking(timestamp, block) {
   
   sdk.util.sumSingleBalance(balances, insure ,veinsureBalances);
   sdk.util.sumSingleBalance(balances, insure ,vlinsureBalances);
-  
+
   console.log(balances);
   
-  // let totalStakingAmount = BigNumber();
-  // totalStakingAmount = BigNumber(veinsureBalances).plus(vlinsureBalances);
-
-  // console.log(BigNumber(totalStakingAmount));
-
-  // sdk.util.sumSingleBalance(balances, insure ,totalStakingAmount);
-
   return balances;
 }
 
