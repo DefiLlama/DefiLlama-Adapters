37d36005d970334971bd83e395108c6789013046
Eugene Lepeico <eugenelepeico@gmail.com>
2022-06-03 12:14:45 +0300

    fix: rewrite to direct blockchain calls

--- projects/phuture/index.js
+++ projects/phuture/index.js
@@ -1,25 +1,44 @@
-const { request, gql } = require("graphql-request");
+const { getBlock } = require("../helper/getBlock");
+const sdk = require("@defillama/sdk");
+const vTokenAbi = require("./abis/vToken.abi.json");
+const BigNumber = require("bignumber.js");
 
-const graphUrl = "https://api.thegraph.com/subgraphs/name/phuture-finance/phuture-v1";
-const graphQuery = gql`
-  query GetVaultReserves($block: Int) {
-    vTokens(block: { number: $block }) {
-      asset {
-        id
-      }
-      assetReserve
+const networks = {
+  ethereum: {
+    vTokenFactory: {
+      address: "0x24aD48f31CAb5E35D0E9CDfa9213b5451f22FB92",
+      blockNumber: 14832754
     }
   }
-`;
+};
 
 const tvl = (chain) => async (timestamp, block, chainBlocks) => {
-  const { vTokens } = await request(graphUrl, graphQuery, {
-    block: chainBlocks[chain]
+  const vTokenFactory = networks[chain].vTokenFactory;
+  const toBlock = await getBlock(timestamp, chain, chainBlocks);
+  const { output: logs } = await sdk.api.util.getLogs({
+    target: vTokenFactory.address,
+    topic: "VTokenCreated(address,address)",
+    keys: [],
+    fromBlock: vTokenFactory.blockNumber,
+    toBlock,
+    chain
   });
 
-  return Object.fromEntries(vTokens
-    .filter(({ assetReserve }) => assetReserve !== "0")
-    .map(({ asset, assetReserve }) => [asset.id, assetReserve])
+  const vTokens = logs.map(({ data }) => ({
+    target: `0x${data.substr(26, 40)}`.toLowerCase(),
+    asset: `0x${data.substr(-40)}`.toLowerCase()
+  }));
+
+  const { output } = await sdk.api.abi.multiCall({
+    abi: vTokenAbi.virtualTotalAssetSupply,
+    calls: vTokens.map(({ target }) => ({ target })),
+    block,
+    chain
+  });
+
+  return Object.fromEntries(output
+    .filter(({ output }) => output)
+    .map(({ output }, index) => [vTokens[index].asset, output])
   );
 };
 
