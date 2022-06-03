3fd0e9b9f26c0df84e99942e03fa7123da9a15bc
ocabafox <ocabafox@gmail.com>
2022-06-04 02:27:06 +0800

    update implementation

--- projects/starbank/index.js
+++ projects/starbank/index.js
@@ -3,8 +3,9 @@ const retry = require('async-retry')
 const {toUSDTBalances} = require("../helper/balances");
 const axios = require("axios")
 
+const seconds = Math.floor(parseInt(Date.now().toString()) / 1000 / 5); // 5sec cache
 const starbankTVLUrl =
-    `https://starbank-storage-api.s3.ap-south-1.amazonaws.com/api/consolidatedDataV1.json?timestamp=${Date.now()}`;
+    `https://starbank-storage-api.s3.ap-south-1.amazonaws.com/api/consolidatedDataV1.json?timestamp=${seconds}`;
 
 async function tvl() {
   const { data } = await retry(
