31999829c0ba689806addc946ba06bbc6766ff26
TakuyaObata <takuya.obata.wltc@gmail.com>
2022-06-04 17:36:33 +0900

    fix: Not working properly since the last update.

--- projects/nodoka/index.js
+++ projects/nodoka/index.js
@@ -3,21 +3,23 @@ const utils = require("../helper/utils");
 const nethabi = require("./neth.json");
 const uniswapV3abi = require("./uniswapV3.json");
 
+
 const BigNumber = require("bignumber.js");
 
 const nethContract = "0xf2e51185caaded6c63d587943369f0b5df169344";
 const nethPool = "0xdb5c6D2DE362606C2Fa84bc948fE0401ea11e207";
 
-async function tvl(ts, block) {
-  const nethTotalSupply = await sdk.api.abi.call({
+async function fetch(ts, block) {
+  const nethTotalSupply = (await sdk.api.abi.call({
     block,
     target: nethContract,
     abi: nethabi["totalSupply"],
-  });
+  })).output;
 
   let price = await getNethPrice();
-  let supply = new BigNumber(nethTotalSupply.output).div(10 ** 18);
-  return supply * price;
+  let supply = new BigNumber(nethTotalSupply).div(10**18);
+
+  return price * supply;
 }
 
 async function getNethPrice(block) {
@@ -38,6 +40,7 @@ async function getNethPrice(block) {
 
 module.exports = {
   ethereum: {
-    tvl,
+    fetch
   },
+  fetch
 };
