8af1feaecbfda934cea1aa6bb98a2aac247a640c
Gerald <gm@gerald.host>
2022-06-06 18:19:21 +0100

    feat: union finance TVL

--- projects/union-finance/index.js
+++ projects/union-finance/index.js
@@ -1,40 +1,148 @@
-const sdk = require('@defillama/sdk')
+const sdk = require("@defillama/sdk");
 
-const PURE_TOKEN_ADAPTER = "0x62DD06026F5f8e874eEfF362b1280CD9A2057b7d"
-const UNION_TOKEN = "0x5Dfe42eEA70a3e6f93EE54eD9C321aF07A85535C"
-const UDAI = "0x954F20DF58347b71bbC10c94827bE9EbC8706887"
+const config = {
+  ethereum: {
+    userManager: "0x49c910Ba694789B58F53BFF80633f90B8631c195",
+    DAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    uDAI: "0x954F20DF58347b71bbC10c94827bE9EbC8706887",
+  },
+  arbitrum: {
+    userManager: "0xb71F3D4342AaE0b8D531E14D2CF2F45d6e458A5F",
+    DAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    uDAI: "0x954F20DF58347b71bbC10c94827bE9EbC8706887",
+  },
+};
 
+const abi = {
+  totalStaked: {
+    inputs: [],
+    name: "totalStaked",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  totalRedeemable: {
+    inputs: [],
+    name: "totalRedeemable",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  totalReserves: {
+    inputs: [],
+    name: "totalReserves",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  totalBorrows: {
+    inputs: [],
+    name: "totalBorrows",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+};
 
-async function tvl(_timestamp, ethBlock){
-    const supply = await sdk.api.erc20.totalSupply({
-        target: UNION_TOKEN,
-        block: ethBlock
-    })
+function tvl(chain) {
+  return async function (_, __, chainBlocks) {
+    const chainBlock = chainBlocks[chain];
+
+    const { userManager, uDAI } = config[chain];
+
+    const totalStaked = (
+      await sdk.api.abi.call({
+        abi: abi.totalStaked,
+        target: userManager,
+        params: [],
+        chain,
+        block: chainBlock,
+      })
+    ).output;
+
+    const totalRedeemable = (
+      await sdk.api.abi.call({
+        abi: abi.totalRedeemable,
+        target: uDAI,
+        params: [],
+        chain,
+        block: chainBlock,
+      })
+    ).output;
+
+    const totalReserves = (
+      await sdk.api.abi.call({
+        abi: abi.totalReserves,
+        target: uDAI,
+        params: [],
+        chain,
+        block: chainBlock,
+      })
+    ).output;
+
+    const total = [totalStaked, totalRedeemable, totalReserves].reduce(
+      (acc, n) => Number(n) + Number(acc),
+      0
+    );
+
     return {
-        [UNION_TOKEN]: supply.output
-    }
+      [config[chain].DAI]: total,
+    };
+  };
+}
+
+function borrowing(chain) {
+  return async function (_, __, chainBlocks) {
+    const chainBlock = chainBlocks[chain];
+
+    const { uDAI } = config[chain];
+
+    const totalBorrows = (
+      await sdk.api.abi.call({
+        abi: abi.totalBorrows,
+        target: uDAI,
+        params: [],
+        chain,
+        block: chainBlock,
+      })
+    ).output;
+
+    return {
+      [config[chain].DAI]: totalBorrows,
+    };
+  };
 }
 
 module.exports = {
-    ethereum:{
-        tvl
-    },
-}
-
-// async function tvl(_timestamp, ethBlock){
-//     const supply = await sdk.api.abi.call({
-//         target: PURE_TOKEN_ADAPTER,
-//         block: ethBlock,
-//         abi: 'erc20:balanceOf',
-//         params: [UNION_TOKEN]
-//     })
-//     return {
-//         [PURE_TOKEN_ADAPTER]: supply.output
-//     }
-// }
-
-// module.exports = {
-//     ethereum:{
-//         tvl
-//     },
-// }
+  timetravel: true,
+  misrepresentedTokens: false,
+  methodology: "Sums total DAI staked in the UserManager and uDAI contracts",
+  ethereum: {
+    tvl: tvl("ethereum"),
+    staking: tvl("ethereum"),
+    borrowed: borrowing("ethereum"),
+  },
+  arbitrum: {
+    tvl: tvl("arbitrum"),
+    staking: tvl("arbitrum"),
+    borrowed: borrowing("arbitrum"),
+  },
+};
