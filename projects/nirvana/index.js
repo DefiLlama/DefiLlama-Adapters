94503d06d1d41e64da2a7a2334c56efb878c1e05
nirvana-sid <102767601+nirvana-sid@users.noreply.github.com>
2022-06-07 13:32:26 -0500

    Add Nirvana metrics
    Twitter: https://twitter.com/nirvana_fi
    Website: nirvana.finance
    Logo: https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/ANAxByE6G2WjFp7A4NqtWYXb3mgruyzZYg3spfxe6Lbo/logo.png
    Current TVL: $19841303.613561314
    Chain: Solana
    Coingecko ID: nirvana-ana
    Coinmarket Cap ID: nirvana-ana
    Short Description: ANA is a partially-collateralized asset with a built-in rising floor price & renewable yield.  It allows for zero liquidation risk loans in the form of the NIRV superstable token.
    Token Address: ANAxByE6G2WjFp7A4NqtWYXb3mgruyzZYg3spfxe6Lbo 
    Ticker: ANA
    Category: CDP
    Oracle Used: Switchboard
    methodology: The total value of ANA tokens locked in the protocol, either as staking or as collateral for loans.

---
+++ projects/nirvana/index.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const {toUSDTBalances} = require('../helper/balances');
+const NIRVANA_API = "https://us-central1-nirvana-91051.cloudfunctions.net";
+
+const client = axios.create({
+  baseURL: NIRVANA_API,
+});
+
+async funtion getTvl(){
+  const metrics = client.get("/getLlama")
+  const { tvl } = metrics
+  return toUSDTBalances(tvl);
+}
+
+const tvl = await getTvl();
+
+module.exports = {
+  methodology: 'The total value of ANA tokens locked in the protocol, either as staking or as collateral for loans.',
+  solana: {
+    tvl
+  }
+}
