53ea3f6941d57edc5bb04bcb9f66e15520d7a2c0
kunalgoyal9 <kunalgoyal.goyal9@gmail.com>
2022-06-08 18:46:00 +0530

    tvl from beefy

--- projects/lachain-yield-market.js
+++ projects/lachain-yield-market.js
@@ -2,9 +2,9 @@ const utils = require('./helper/utils');
 const sdk = require("@defillama/sdk");
 
 async function tvl(timestamp, block, chainBlocks) {
-    const balances = {};
-
     const response = await utils.fetchURL(`https://raw.githubusercontent.com/LATOKEN/farm-list/main/latoken.farmlist.json`);
+    const address_usdt = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+    var tvl_sum = 0;
 
     for (let i = 0; i < response.data.farms.length; i++) {
         const f = response.data.farms[i];
@@ -18,27 +18,31 @@ async function tvl(timestamp, block, chainBlocks) {
         })
         ).output;
         
+        const decimals = (await sdk.api.abi.call({
+            abi: "erc20:decimals",
+            target: f.wrappedTokenAddress,
+            params: [],
+            block: chainBlocks.lachain,
+            chain: 'lachain'
+        })).output;
+
         const oracleId = f.oracleId;
         var rate = 0;
-
+        
         if(f.oracle == 'lps'){
-            const lp_rate = await utils.fetchURL(`https://api.beefy.finance/lps`).data;
-            // rate = lp_rate[oracleId]
-            // console.log("lp_rate: ", lp_rate);
+            const lp_rate = await utils.fetchURL(`https://api.beefy.finance/lps`);
+            rate = lp_rate.data[oracleId]
         }
         else if(f.oracle == 'tokens'){
-            const token_rate = await utils.fetchURL(`https://api.beefy.finance/prices`).data;
-            // console.log("token_rate: ", token_rate);
-            
-            rate = token_rate[token_rate]
-            console.log("Avax:: ", token_rate.AVAX);
+            const token_rate = await utils.fetchURL(`https://api.beefy.finance/prices`);
+            rate = token_rate.data[oracleId]
         }
-        console.log("oracleId: ", oracleId);
-        console.log("rate: ", rate);
+
+        tvl_sum += (supplies/10**decimals)*rate;
         
     };
-
-    return balances;
+    
+    return {[address_usdt]: tvl_sum * 10**6};
 };
 
 module.exports = {
