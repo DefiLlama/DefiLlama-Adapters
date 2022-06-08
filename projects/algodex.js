40c1c819e805868768e9c9f04677721dbe1ffdc3
Imran <sp18-rcs-007@cuilahore.edu.pk>
2022-06-08 16:38:41 +0500

    price=0 for asset not found on tinyman

--- projects/algodex.js
+++ projects/algodex.js
@@ -6,38 +6,6 @@ const usdPriceUrl = "https://mainnet.analytics.tinyman.org/api/v1/current-asset-
 const algoPriceUrl = "https://app.algodex.com/algodex-backend/assets.php"
 const assetVolURL = "https://app.algodex.com/algodex-backend/tvl.php/"
 
-// const rtprices = fs.readFileSync("/home/imran/Projects/algodex/DefiLlama-Adapters/tinyman_prices.json")
-// const tp = JSON.parse(rtprices)
-
-// const raprices = fs.readFileSync("/home/imran/Projects/algodex/DefiLlama-Adapters/algo_prices.json")
-// const ap = JSON.parse(raprices)
-// const algoPrices = ap.data.reduce((obj, item) => {
-//     if (item.hasOwnProperty("price")) {
-//         obj[item.id] = item.price
-//     }
-//     return obj
-// }, {})
-
-// const rawAlgoAmount = fs.readFileSync("/home/imran/Projects/algodex/DefiLlama-Adapters/algo_amounts.json")
-// const algoAmount = JSON.parse(rawAlgoAmount)
-
-// function getPrice(assetId) {
-//     var assetPrice = 0
-//     if (tp.hasOwnProperty(assetId)) {
-//         // console.log("Tinyprice: ", assetId)
-//         assetPrice = tp[assetId].price
-
-//     } else {
-
-//         if (algoPrices.hasOwnProperty(assetId)) {
-//             // console.log("Algoprice: ", assetId)
-//             assetPrice = algoPrices[assetId]
-//         }
-
-//     }
-//     return assetPrice
-// }
-
 async function tvl() {
     const volumData = await fetchURL(assetVolURL)
     const usdPriceData = await fetchURL(usdPriceUrl)
@@ -62,16 +30,14 @@ async function tvl() {
             assetPrice = usdPriceData[asset.assetid].price
 
         } else {
+            // price for some asset is not found on tinyman then
+            // set its price to zero
 
-            if (algoPrices.hasOwnProperty(asset.assetid)) {
-                // console.log("Algoprice: ", assetId)
-                assetPrice = algoPrices[asset.assetid]
-            }
+            assetPrice = 0
 
         }
 
 
-        // var price = getPrice(asset.assetid)
         var total = assetPrice * asset.asaAmountTotal
         total_liquidity_in_usd = total_liquidity_in_usd + total
     })
@@ -79,29 +45,6 @@ async function tvl() {
     return toUSDTBalances(data.data.total_liquidity_in_usd)
 }
 
-
-// query for api 
-// SELECT assetid,   Sum(asaAmount) asaAmountTotal FROM algodex.orderbook
-// group by assetid
-// union all 
-// select 0 as assetId,  sum(algoAmount) from algodex.orderbook
-// order by assetid
-
-
-
-//    (asaAmount * tinyMan)
-//    +
-// Sum(algoAmount * tinyMan_USD_ALGO_Price)
-
-// i need tinyman prices 
-// https://mainnet.analytics.tinyman.org/api/v1/current-asset-prices/
-
-// algoexplorer prices 
-// https://price.algoexplorerapi.io/price/algo-usd
-
-// for all assets whose usd prices are not available in tinyman , find prices from this api endpoint 
-//  and multiply with price of asset 0 in tinyman api
-// https://app.algodex.com/algodex-backend/assets.php
 module.exports = {
     misrepresentedTokens: true,
     algodex: {
