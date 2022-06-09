51fbf15dc63d4380ade4caa380dc6e0046f64381
photonswap-dev <94353805+photonswap-dev@users.noreply.github.com>
2022-06-09 15:38:52 +0400

    added kava and evmos adapters for Photonswap

--- projects/photonswap/index.js
+++ projects/photonswap/index.js
@@ -2,7 +2,7 @@ const {calculateUsdUniTvl} = require('../helper/getUsdUniTvl')
 
 module.exports={
     misrepresentedTokens: true,
-    methodology: "Factory address (0x462C98Cae5AffEED576c98A55dAA922604e2D875) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
+    methodology: "Factory addresses (0x462C98Cae5AffEED576c98A55dAA922604e2D875 for cronos, 0x4FD2c40c25Dd40e9Bf0CE8479bA384178b8671b5 for kava, 0x1c671d6fEC45Ec0de88C82e6D8536bFe33F00c8a for evmos) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
     cronos: {
         tvl:calculateUsdUniTvl(
             "0x462C98Cae5AffEED576c98A55dAA922604e2D875", 
@@ -15,5 +15,31 @@ module.exports={
                 "0xc21223249ca28397b4b6541dffaecc539bff0c59"
             ], 
             "crypto-com-chain")
+    },
+    evmos: {
+        tvl:calculateUsdUniTvl(
+            "0x1c671d6fEC45Ec0de88C82e6D8536bFe33F00c8a", 
+            "evmos", 
+            "0xD4949664cD82660AaE99bEdc034a0deA8A0bd517", 
+            [
+                //PHOTON
+                "0xA8eFf8BB28c6193CBE8BcFb8276e9b1dD3380B13",
+                //USDC 
+                "0x51e44FfaD5C2B122C8b635671FCC8139dc636E82"
+            ], 
+            "evmos")
+    },
+    kava: {
+        tvl:calculateUsdUniTvl(
+            "0x4FD2c40c25Dd40e9Bf0CE8479bA384178b8671b5", 
+            "kava", 
+            "0xc86c7C0eFbd6A49B35E8714C5f59D99De09A225b", 
+            [
+                //PHOTON
+                "0xA8eFf8BB28c6193CBE8BcFb8276e9b1dD3380B13",
+                //USDC 
+                "0xfA9343C3897324496A05fC75abeD6bAC29f8A40f"
+            ], 
+            "kava")
     }
 }
