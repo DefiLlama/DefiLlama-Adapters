fa85fbb097b7fae6018fb797337c07fb4dfcd1d1
nirvana-sid <102767601+nirvana-sid@users.noreply.github.com>
2022-06-09 17:45:45 -0500

    update adapter to staking, fix 0 return

--- projects/nirvana/index.js
+++ projects/nirvana/index.js
@@ -1,20 +1,24 @@
 const axios = require("axios");
 const NIRVANA_API = "https://us-central1-nirvana-91051.cloudfunctions.net";
+const { toUSDTBalances } = require("../helper/balances");
 
 const client = axios.create({
   baseURL: NIRVANA_API,
 });
 
-async function tvl(){
-  const metrics = await client.get("/getLlama")
-  const { tvl } = metrics.data
-  return tvl;
+async function staking() {
+  const metrics = await client.get("/getLlama");
+  const { tvl } = metrics.data;
+  console.log("tvl", tvl);
+  return toUSDTBalances(tvl);
 }
 
 module.exports = {
   timetravel: false,
-  methodology: 'The total value of ANA tokens locked in the protocol, either as staking or as collateral for loans.',
+  methodology:
+    "The total value of ANA tokens locked in the protocol, either as staking or as collateral for loans.",
   solana: {
-    tvl
-  }
-}
+    tvl: () => ({}),
+    staking,
+  },
+};
