13c27f2e7a64c595b0e7df0ef355806196ad48c5
waynebruce0x <waynebruce0x@gmail.com>
2022-06-10 17:43:55 +0800

    tidy so we can merge mole

--- projects/astroport/index.js
+++ projects/astroport/index.js
@@ -1,15 +1,18 @@
 const {getFactoryTvl} = require('../terraswap/factoryTvl')
 
-const oldFactory = "terra1fnywlw4edny3vw44x04xd67uzkdqluymgreu7g"
-const factory = "terra14x9fr055x5hvr48hzy2t4q7kvjvfttsvxusa4xsdcy702mnzsvuqprer8r"
 module.exports = {
     timetravel: true,
     misrepresentedTokens: true,
     methodology: "Liquidity on the DEX",
     terra: {
-        tvl: getFactoryTvl(oldFactory)
+        tvl: getFactoryTvl(
+            "terra1fnywlw4edny3vw44x04xd67uzkdqluymgreu7g"
+        )
     },
-    terra2: {
-        tvl: getFactoryTvl(factory)
-    }
-}
+    // terra2: {
+    //     tvl: getFactoryTvl(
+    //         "terra14x9fr055x5hvr48hzy2t4q7kvjvfttsvxusa4xsdcy702mnzsvuqprer8r", 
+    //         true
+    //     )
+    // }
+} // node test.js projects/astroport/index.js
