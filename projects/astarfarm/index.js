cf740e063602b0f615a8bb082373d9408b8ad637
lilpacy <revivedtomorrow@gmail.com>
2022-06-13 21:57:40 +0900

    fix to use DefiLlama adapter

--- projects/astarfarm/index.js
+++ projects/astarfarm/index.js
@@ -1,16 +1,13 @@
-const ethers = require("ethers")
+const abi = require("./abi.json");
+const sdk = require('@defillama/sdk')
 
-const fetch = async () => {
-    const rpcUrl = "https://rpc.astar.network:8545";
-    const provider = await ethers.getDefaultProvider(rpcUrl);
-    const contract = new ethers.Contract(
-      "0x0BdD0C5406B9420B4d4251671F9F045e4FBEb758",
-      ["function staked() external view returns (uint128)"],
-      provider
-    );
-    return await contract.staked(); // tvl as wei
-}
+const stakingContract = '0x992bad137Fc8a50a486B5C6375f581964b4A15FC'
 
 module.exports = {
-    fetch
+    astar: {
+        tvl: async (ts, _block, { astar: block }) => {
+            const { output: lockedAstar } = await sdk.api.abi.call({ target: stakingContract, block, abi: abi.find(i => i.name === 'staked'), chain: 'astar' })
+            return { 'astar': +lockedAstar / 1e18 }
+        }
+    }
 }
