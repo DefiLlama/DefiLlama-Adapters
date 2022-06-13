dea9b8dbd5521bfafe4eb33e9ada51868cc7662f
mytrade-dex <76690427+mytrade-dex@users.noreply.github.com>
2022-06-13 18:51:32 +0800

    Add fetch adapter for Mytrade
    Add fetch adapter for Mytrade

--- projects/MyTrade/index.js
+++ projects/MyTrade/index.js
@@ -1,1 +1,15 @@
+const utils = require("../helper/utils")
+const { toUSDTBalances } = require('../helper/balances')
 
+async function fetch() {
+  var endpoint ='https://www.mytrade.org/b/totalMap/list?totalItem=Total%20Liquidity&pages=0&pageSize=10';
+  const response = await utils.fetchURL(endpoint);
+  return toUSDTBalances(parseFloat(response?.data[0].totalValue));
+}
+
+module.exports = {
+  timetravel: true,
+  polygon: {
+    tvl: fetch
+  },
+}
