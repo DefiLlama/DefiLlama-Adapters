e979c6e905d561651b243cc5e179727f05a412c7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-06-14 09:56:02 +0200

    Use on-chain calls to determine tvl

--- projects/MyTrade/index.js
+++ projects/MyTrade/index.js
@@ -1,15 +1,14 @@
-const utils = require("../helper/utils")
-const { toUSDTBalances } = require('../helper/balances')
+const { getUniTVL } = require("../helper/unknownTokens");
 
-async function fetch() {
-  var endpoint ='https://www.mytrade.org/b/totalMap/list?totalItem=Total%20Liquidity&pages=0&pageSize=10';
-  const response = await utils.fetchURL(endpoint);
-  return toUSDTBalances(parseFloat(response?.data[0].totalValue));
-}
-
-module.exports = {
-  timetravel: true,
-  polygon: {
-    tvl: fetch
-  },
-}
+ module.exports = {
+   polygon: {
+     tvl: getUniTVL({
+       factory: '0x3ee4154c7f42d94e1092ad8ce5debb4b743ed0b2',
+       chain: 'polygon',
+       coreAssets: [
+        '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0', // wmatic
+        '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', // usdc
+       ]
+     })
+   },
+ }
