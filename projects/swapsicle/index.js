0b34e1e7ebffb6e1e87cd9fc718369082aa95526
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-06-14 16:38:06 +0200

    refactor code

--- projects/swapsicle/index.js
+++ projects/swapsicle/index.js
@@ -1,34 +1,16 @@
-const { request, gql } = require("graphql-request");
-const { toUSDTBalances } = require('../helper/balances');
-const graphUrl = 'https://api.thegraph.com/subgraphs/name/billy93/exchange'
+const { getUniTVL } = require("../helper/unknownTokens");
 
-const graphQuery = gql`
-query gettvl($block: Int) {
-  factory(
-    id: "0x9c60c867ce07a3c403e2598388673c10259ec768",
-    block: { number: $block }
-  ) {
-    liquidityUSD
-  }
-}
-`;
-
-async function tvl(timestamp, ethBlock, chainBlocks) {
-  const response = await request(
-    graphUrl,
-    graphQuery,
-    {
-      block:chainBlocks.avax,
-    }
-  );
-
-  return toUSDTBalances(Number(response.factory.liquidityUSD));
-}
+const factory = "0x9c60c867ce07a3c403e2598388673c10259ec768";
 
 module.exports = {
-  misrepresentedTokens: true,
-  methodology: 'Using the Swapsicle factory subgraph and contract address we are able to retrieve the total balance held in all liquidity pairs.',
-  avalanche:{
-    tvl,
-  }
-}
+  avax: {
+    tvl: getUniTVL({
+      factory,
+      chain: 'avax',
+      coreAssets: [
+        '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7', // WAVAX
+        '0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664', // USDC.e
+      ]
+    }),
+  },
+};
