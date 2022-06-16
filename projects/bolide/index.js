61b51c10a9d10f4f4803ee675cb5d8aa34073343
Nikita Bezhin <bezhin.nikita@gmail.com>
2022-06-16 09:26:08 +0300

    New TVL fetch logic

--- projects/bolide/index.js
+++ projects/bolide/index.js
@@ -1,39 +1,10 @@
-const sdk = require('@defillama/sdk')
-const abi = require('./abi.json')
-const BigNumber = require('bignumber.js')
-const { ethers } = require('ethers')
-
-const address = "0xf1f25A26499B023200B3f9A30a8eCEE87b031Ee1";
-
-function toBN(value) {
-    let data = value ? value : 0;
-    data = !Number.isNaN(data) ? data : 0;
-
-    return new BigNumber(data);
-};
-
-function fromWei(value, decimals) {
-    const prepValue = value.toString(10).split('.')[0];
-    const eth = ethers.utils.formatUnits(prepValue, decimals).toString();
-
-    return toBN(eth);
-};
-
-function safeBN(v) {
-    const value = v.toString(10);
-  
-    return value.includes('.') ? value.split('.')[0] : value;
-};
+const retry = require('./helper/retry')
+const axios = require("axios");
 
 async function fetch() {
-    const { output } = await sdk.api.abi.call({
-        abi: abi.getTotalDeposit,
-        target: address,
-        params: [],
-        chain: "bsc",
-    });
-    
-    return safeBN(fromWei(output, 18).toString(10));
+    var response = await retry(async bail => await axios.get('https://bolide.fi/api/tvl'))
+
+    return response.data.total;
 };
 
 module.exports = {
