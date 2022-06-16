59ff2597e4291e52ca3848439524565a00bffa6f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-06-16 16:25:16 +0200

    Seperate staking and pool2 from tvl

--- projects/revest/index.js
+++ projects/revest/index.js
@@ -1,6 +1,10 @@
-const { sumTokens2 } = require("../helper/unwrapLPs")
+const { sumTokens2, unwrapLPsAuto, } = require("../helper/unwrapLPs")
 const { get } = require("../helper/http")
 const sdk = require('@defillama/sdk')
+const { getChainTransform } = require('../helper/portedTokens')
+const { staking } = require('../helper/staking')
+const { pool2 } = require('../helper/pool2')
+const { getUniqueAddresses } = require('../helper/utils')
 
 const tokenAddress = 'https://defi-llama-feed.vercel.app/api/address'
 const config = {
@@ -8,6 +12,8 @@ const config = {
     holder: '0xA81bd16Aa6F6B25e66965A2f842e9C806c0AA11F',
     graph: 'https://api.thegraph.com/subgraphs/name/revest-finance/revest-mainnet-subgraph',
     chainId: 1,
+    revest: '0x120a3879da835a5af037bb2d1456bebd6b54d4ba',
+    lp: '0x6490828bd87be38279a36f029f3b9af8b4e14b49',
   },
   polygon: {
     holder: '0x3cCc20d960e185E863885913596b54ea666b2fe7',
@@ -56,12 +62,17 @@ module.exports = {
 
 
 Object.keys(config).forEach(chain => {
+  const { chainId, holder, revest, lp } = config[chain]
   module.exports[chain] = {
     tvl: async (_, _b, { [chain]: block }) => {
-      const { chainId, graph, holder } = config[chain]
+      const blacklist = []
+      if (revest) blacklist.push(revest.toLowerCase())
+      if (lp) blacklist.push(lp.toLowerCase())
+      const transform = await getChainTransform(chain)
       const tokenURL = `${tokenAddress}?chainId=${chainId}`
-      const { body: tokens } = await get(tokenURL)
-      const balances = await sumTokens2({ chain, block, owner: holder, tokens })
+      let { body: tokens } = await get(tokenURL)
+      tokens = getUniqueAddresses(tokens).filter(t => !blacklist.includes(t)) // filter out staking and LP tokens
+      // const balances = await sumTokens2({ chain, block, owner: holder, tokens })
       const { output: tokenRes } = await sdk.api.abi.multiCall({
         target: holder,
         abi: tokenTrackersABI,
@@ -69,9 +80,12 @@ Object.keys(config).forEach(chain => {
         chain, block,
       })
       const balance2 = {}
-      tokenRes.forEach(i => balance2[i.input.params[0]] = i.output.lastBalance)
-      console.log(chain, balances, balance2)
-      return balances
-    }
+      tokenRes.forEach(i => sdk.util.sumSingleBalance(balance2, transform(i.input.params[0]), i.output.lastBalance))
+      await unwrapLPsAuto({ balances: balance2, block, chain, transformAddress: transform, })
+      // console.log(chain, balances, balance2)
+      return balance2
+    },
+    pool2: lp ? pool2(holder, lp, chain) : undefined,
+    staking: revest ? staking(holder, revest, chain): undefined,
   }
 })
