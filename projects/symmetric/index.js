847d0b7507383d6168579da2d44e9af272e9162a
0xCoDeFi <cimran@gmail.com>
2022-06-21 00:28:07 +0100

    support added for Symmetric v2 and xDai > Gnosis

--- projects/symmetric/index.js
+++ projects/symmetric/index.js
@@ -3,12 +3,22 @@ const BigNumber = require("bignumber.js");
 const { GraphQLClient, gql } = require('graphql-request')
 const { toUSDTBalances } = require('../helper/balances');
 const { getBlock } = require('../helper/getBlock');
-
-async function getTVL(subgraphName, block) {
+async function getTVL(subgraphName, block, version = 'v1') {
   var endpoint = `https://api.thegraph.com/subgraphs/name/centfinance/${subgraphName}`
   var graphQLClient = new GraphQLClient(endpoint)
 
-  var query = gql`
+  var v1 = gql`
+  query get_tvl($block: Int) {
+    symmetrics(
+      first: 5,
+      block: { number: $block }
+    ) {
+      totalLiquidity,
+      totalSwapVolume
+    }
+  }
+  `;
+  var v2 = gql`
   query get_tvl($block: Int) {
     balancers(
       first: 5,
@@ -19,32 +29,31 @@ async function getTVL(subgraphName, block) {
     }
   }
   `;
-  const results = await graphQLClient.request(query, {
+  const results = await graphQLClient.request(version === 'v1' ? v1 : v2, {
     block
   })
-  return results.balancers[0].totalLiquidity;
+  return version ==='v1'? results.symmetrics[0].totalLiquidity : results.balancers[0].totalLiquidity;
 }
 
-async function xdai(timestamp, ethBlock, chainBlocks) {
-  const [v1,v2] = await Promise.all([getTVL("cent-swap-xdai", ethBlock), getTVL("symmetric-v2-gnosis", ethBlock)])
+async function gnosis(timestamp, ethBlock, chainBlocks) {
+  const [v1,v2] = await Promise.all([getTVL("symmetric-xdai", await getBlock(timestamp, "xdai", chainBlocks), 'v1'), getTVL("symmetric-v2-gnosis", await getBlock(timestamp, "xdai", chainBlocks), 'v2')])
   return toUSDTBalances(BigNumber(v1).plus(v2))
 }
 
 async function celo(timestamp, ethBlock, chainBlocks) {
-  const [v1,v2] = await Promise.all([getTVL("cent-swap-celo", ethBlock), getTVL("symmetric-v2-celo", ethBlock)])
+  const [v1,v2] = await Promise.all([getTVL("symmetric-celo", await getBlock(timestamp, "celo", chainBlocks), 'v1'), getTVL("symmetric-v2-celo", await getBlock(timestamp, "celo", chainBlocks), 'v2')])
   return toUSDTBalances(BigNumber(v1).plus(v2))
 }
 
 module.exports = {
-  timetravel: true,
   misrepresentedTokens: true,
   methodology: `Symmetric is an Automated Market Maker (AMM) and a Decentralized Exchange (DEX), running on the Celo and Gnosis networks.
-  Symmetric TVL is pulled from the Symmetric subgraph and includes deposits made to Symmetric Gnosis and Symmetric Celo on V1 & V2 liquidity pools.`,
+  Symmetric TVL is pulled from the Symmetric subgraph and includes deposits made to Symmetric Gnosis and Symmetric Celo V1 & V2 liquidity pools.`,
   celo:{
     tvl: celo
   },
-  xdai:{
-    tvl: xdai
+  gnosis:{
+    tvl: gnosis
   },
-  tvl: sdk.util.sumChainTvls([xdai, celo])
+  tvl: sdk.util.sumChainTvls([gnosis, celo])
 }
