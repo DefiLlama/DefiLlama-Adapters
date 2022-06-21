3b10ccc147b5b83d0ca89852822ab682d8665784
dacarva <davidfelipec88@gmail.com>
2022-06-21 09:34:48 -0500

    Updated contract to show the real TVL

--- projects/tropykus/index.js
+++ projects/tropykus/index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 const fetch = async function () {
   const url = 'https://us-central1-tropycofinance.cloudfunctions.net/getTVL';
-  const comptroller = '0x962308fef8edfadd705384840e7701f8f39ed0c0';
+  const comptroller = '0x962308fEf8edFaDD705384840e7701F8f39eD0c0';
 
   const tropykusTVL = await retry(async bail => await axios.get(`${url}?comptroller=${comptroller}`));
   return tropykusTVL.data.tvl;
