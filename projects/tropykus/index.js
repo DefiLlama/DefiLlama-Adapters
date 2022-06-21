f7093fdbb31c423ce073f8170a6f211bb8d35049
dacarva <davidfelipec88@gmail.com>
2022-06-21 08:58:19 -0500

    Update comptroller contract
    The comptroller contract in line 6 was updated to the new address: 0x962308fef8edfadd705384840e7701f8f39ed0c0

--- projects/tropykus/index.js
+++ projects/tropykus/index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 const fetch = async function () {
   const url = 'https://us-central1-tropycofinance.cloudfunctions.net/getTVL';
-  const comptroller = '0x10d6d9cfacd77ec2f3b422a4ad7face58197c2e9';
+  const comptroller = '0x962308fef8edfadd705384840e7701f8f39ed0c0';
 
   const tropykusTVL = await retry(async bail => await axios.get(`${url}?comptroller=${comptroller}`));
   return tropykusTVL.data.tvl;
