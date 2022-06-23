fd3e93c4a398a14935d04c89e677cca40eee7484
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-06-23 13:56:23 +0200

    [mango-markets] update MSOL gecko id

--- projects/mango-markets/index.js
+++ projects/mango-markets/index.js
@@ -32,11 +32,12 @@ const coingeckoIds = {
     'RAY': 'raydium',
     'COPE': 'cope',
     'FTT': 'ftx-token',
-    'MSOL': 'marinade-staked-sol',
+    'MSOL': 'msol',
     'BNB': 'binance-coin',
     'AVAX': 'avalanche',
     'LUNA': 'terra-luna',
-    'GMT': 'stepn'
+    'LUNA': 'terra-luna',
+    'GMT': 'stepn',
 }
 
 
