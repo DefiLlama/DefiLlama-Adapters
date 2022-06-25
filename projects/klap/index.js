0fe79c71acc4a226a5bb33b610268c6648c67968
0xinsanity <noahsahar@gmail.com>
2022-06-24 20:02:46 -0400

    fixed export

--- projects/klap/index.js
+++ projects/klap/index.js
@@ -53,7 +53,7 @@ module.exports = {
   timetravel: true,
   methodology:
     "Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There's multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending",
-  fantom: {
+  klaytn: {
     tvl: lending(false),
     borrowed: lending(true)
   },
