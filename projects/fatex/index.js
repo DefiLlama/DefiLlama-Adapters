2540f1b79a87de966ce8213ccdd38fca7f7c5b15
Corey Caplan <coreycaplan3@gmail.com>
2022-06-26 12:55:18 -0400

    fixed index.js

--- projects/fatex/index.js
+++ projects/fatex/index.js
@@ -1,59 +1,24 @@
-const sdk = require("@defillama/sdk");
-const { transformPolygonAddress } = require("../helper/portedTokens");
-const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
-const BigNumber = require("bignumber.js");
+const { stakingUnknownPricedLP } = require('../helper/staking')
+const { getUniTVL } = require('../helper/unknownTokens')
+
 
 const FACTORY_CONTRACT = "0x937e0c67d21Df99eaEa0e6a1055A5b783291DC8f";
 const FATE_TOKEN = "0x4853365bC81f8270D902076892e13F27c27e7266";
-const USDC_TOKEN = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
 const FATE_USDC_PAIR_TOKEN = "0x69c894Dce6FA2E3b89D3111d29167F0484AC0b2A";
 const X_FATE_TOKEN = "0x56BE76031A4614370fA1f188e01e18a1CF16E642";
 
-async function xFateTvl(timestamp, block, chainBlocks) {
-  const balances = {};
-
-  const fateBalanceRaw = (await sdk.api.abi.call({
-    abi: "erc20:balanceOf",
-    chain: "polygon",
-    target: FATE_TOKEN,
-    params: [X_FATE_TOKEN],
-    block: chainBlocks["polygon"]
-  })).output;
-
-  const fateReservesRaw = (await sdk.api.abi.call({
-    abi: "erc20:balanceOf",
-    chain: "polygon",
-    target: FATE_TOKEN,
-    params: [FATE_USDC_PAIR_TOKEN],
-    block: chainBlocks["polygon"]
-  })).output;
-
-  const usdcReservesRaw = (await sdk.api.abi.call({
-    abi: "erc20:balanceOf",
-    chain: "polygon",
-    target: USDC_TOKEN,
-    params: [FATE_USDC_PAIR_TOKEN],
-    block: chainBlocks["polygon"]
-  })).output;
-
-  balances['usd-coin'] = new BigNumber(fateBalanceRaw).times(usdcReservesRaw).div(fateReservesRaw).div(1e6).toNumber();
-
-  return balances;
-}
-
-const pool2 = calculateUsdUniTvl(FACTORY_CONTRACT, "polygon", USDC_TOKEN, [FATE_TOKEN], "usd-coin", 6);
-
 module.exports = {
-  misrepresentedTokens: false,
-  methodology: "Gets the value of all tokens in FATExFi liquidity pools and staking contracts",
-  timetravel: true,
   polygon: {
-    tvl: async (timestamp, block, chainBlocks) => {
-      const balances = await pool2(timestamp, block, chainBlocks);
-      balances['usd-coin'] = balances['usd-coin'] + (await xFateTvl(timestamp, block, chainBlocks))['usd-coin'];
-      return balances;
-    },
-    pool2: pool2,
-    staking: xFateTvl
-  }
-};
+    tvl: getUniTVL({
+      factory: FACTORY_CONTRACT,
+      chain: 'polygon',
+      coreAssets: [
+        '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', // WMATIC
+        '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', // USDC
+        '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', // USDT
+        '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063', // DAI
+      ],
+    }),
+    staking: stakingUnknownPricedLP(X_FATE_TOKEN, FATE_TOKEN, 'polygon', FATE_USDC_PAIR_TOKEN)
+  },
+}
