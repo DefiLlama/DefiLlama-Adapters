02292954f37c9ae9e5608ea33f63e57a8f870790
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-06-27 10:49:47 +0200

    [fractal-protocol] refactor code

--- projects/fractal-protocol/index.js
+++ projects/fractal-protocol/index.js
@@ -1,36 +1,22 @@
-const sdk = require('@defillama/sdk');
-const vaultAbi = require('./vaultAbi.json');
-const usdfAbi = require('./usdfAbi.json');
-
+const sdk = require('@defillama/sdk')
+const getTokenPrice = {"inputs":[],"name":"getTokenPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}
 const USDF_TOKEN_CONTRACT = '0x51acB1ea45c1EC2512ae4202B9076C13016dc8aA';
 const FRACTAL_VAULT_CONTRACT = '0x3eB82f2eD4d992dc0Bed328214A0907250f4Ec82';
-const ONE_MWEI = 1000000;
 
+async function tvl(timestamp, block) {
+  const { output: totalSupply } = await sdk.api.erc20.totalSupply({ target: USDF_TOKEN_CONTRACT, block }) 
+  const { output: price } = await sdk.api.abi.call({ target: FRACTAL_VAULT_CONTRACT, block, abi: getTokenPrice }) 
 
-async function tvl (timestamp, block) {
-
-    const usdfTotalSupply = (await sdk.api.usdfAbi.call({
-        block,
-        target: USDF_TOKEN_CONTRACT,
-        abi: abi['totalSupply']
-    })).output;
-
-    const usdfTokenPrice = (await sdk.api.vaultAbi.call({
-        block,
-        target: FRACTAL_VAULT_CONTRACT,
-        abi: abi['getTokenPrice']
-    })).output;
-
-    totalTvl = (usdfTotalSupply/ONE_MWEI) * (usdfTokenPrice/ONE_MWEI);
-    
-    return totalTvl;
+  return {
+    'usd-coin': totalSupply * price / 1e12
+  }
 }
 
 module.exports = {
-    ethereum: {
-      methodology: "USDF is minted when USDC is deposited into the Fractal Vault. TVL = totalSupply * usdfPrice.",
-      tvl,
-    }
+  ethereum: {
+    methodology: "USDF is minted when USDC is deposited into the Fractal Vault. TVL = totalSupply * usdfPrice.",
+    tvl,
   }
+}
 
 
