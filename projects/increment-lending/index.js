f800a446558d7cbe14b084fbc29b0d83a9040f27
Jing Li <swordc@126.com>
2022-06-30 17:47:43 +0800

    1.add flow chain 2.add borrow in increment-lending

--- projects/increment-lending/index.js
+++ projects/increment-lending/index.js
@@ -1,17 +1,27 @@
 const { fetchURL } = require("../helper/utils");
 
+
+// increment lending info: https://app.increment.fi/markets
 async function tvl() {
-    // info: https://app.increment.fi/markets
+    
     const { data: tvls } = await fetchURL(
         "https://app.increment.fi/info/tvl"
     );
-
     return tvls.LendingTVL;
+}
+
+async function borrowed() {
+    
+    const { data: tvls } = await fetchURL(
+        "https://app.increment.fi/info/tvl"
+    );
+    return tvls.LendingBorrow;
 }
 
 module.exports = {
     methodology: "This is the first lending protocol on the flow blockchain , and temporarily uses the project's own endpoint.",
     flow: {
-        tvl
+        tvl,
+        borrowed
     }
 }
