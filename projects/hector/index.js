84dedbaf4486d276ac2da52f355424bff75d69ea
waynebruce0x <waynebruce0x@gmail.com>
2022-06-30 17:58:32 +0100

    staking

--- projects/hector/index.js
+++ projects/hector/index.js
@@ -251,13 +251,28 @@ function tvl(chain) {
     return balances;
   };
 }
+const staking = async () => {
+  var endpoint =
+    "https://api.thegraph.com/subgraphs/name/hectordao-hec/hector-dao";
+  var graphQLClient = new GraphQLClient(endpoint);
 
+  var query = gql`
+    query {
+      protocolMetrics(first: 1, orderBy: timestamp, orderDirection: desc) {
+        totalValueLocked
+      }
+    }
+  `;
+  const results = await retry(async bail => await graphQLClient.request(query));
+  return toUSDTBalances(+results.protocolMetrics[0].totalValueLocked);
+};
 module.exports = {
   ethereum: {
     tvl: tvl("ethereum")
   },
   fantom: {
-    tvl: sdk.util.sumChainTvls([tvl("fantom"), hectorBank])
+    tvl: sdk.util.sumChainTvls([tvl("fantom"), hectorBank]),
+    staking
   },
   bsc: {
     tvl: tvl("bsc")
