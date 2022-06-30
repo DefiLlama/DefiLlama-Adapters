e8356b857a6a8704aac018e027a26c246fcc98fe
Arsenii <backend@cryptouniverse.io>
2022-06-30 18:43:26 +0300

    format

--- projects/minto/index.js
+++ projects/minto/index.js
@@ -1,17 +1,13 @@
 const axios = require("axios");
 const MINTO_API_TVL = "https://stats-dev.minto.org/v1";
 
-
 const client = axios.create({
   baseURL: MINTO_API_TVL
 });
 
-
-
 async function fetch() {
   const tvlRes = await client.get("general/tapp-tvl");
-  const tvl = tvlRes.data.value;
-  return tvl;
+  return tvlRes.data.value;
 }
 
 module.exports = {
