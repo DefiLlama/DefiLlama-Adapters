5ba2cec60fe0c5aa14b6b703f96b764a8e2f448f
jaredWu0805 <jared810805@gmail.com>
2022-07-04 12:20:56 +0800

    Update tvl url

--- projects/xy-finance/index.js
+++ projects/xy-finance/index.js
@@ -15,57 +15,12 @@ const chains = {
   43114: 'avax',
 }
 
-const url = ''
-
-markData = {
-    "bridgeTokenList": [
-        {
-            "chainId": 1,
-            "tokenAddress": "",
-            "symbol": "USDC",
-            "decimals": 6,
-            "ypoolContractAddress": "",
-            "price": 2.16,
-            "balance": "245616176457139196363406",
-            "tvl": 50.5,
-        },
-        {
-            "chainId": 1,
-            "tokenAddress": "",
-            "symbol": "USDT",
-            "decimals": 6,
-            "ypoolContractAddress": "",
-            "price": 0.260124,
-            "balance": "53623913481087880337446071",
-            "tvl": 60.6,
-        },
-        {
-            "chainId": 56,
-            "tokenAddress": "",
-            "symbol": "USDC",
-            "decimals": 6,
-            "ypoolContractAddress": "",
-            "price": 2.16,
-            "balance": "245616176457139196363406",
-            "tvl": 7.005,
-        },
-        {
-            "chainId": 56,
-            "tokenAddress": "",
-            "symbol": "USDT",
-            "decimals": 6,
-            "ypoolContractAddress": "",
-            "price": 0.260124,
-            "balance": "53623913481087880337446071",
-            "tvl": 7.003,
-        },
-    ]
-}
+const url = 'https://api.xy.finance/stats/v1/tvl'
 
 function fetchChain(chain) {
   return async () => {
-    const { data } = await utils.fetchURL(url)
-    // const data = markData
+    let req_url = chain === null ? url : url + '?chainId=' + chain
+    const { data } = await utils.fetchURL(req_url)
     const tokensInChain = chain === null ? data.bridgeTokenList : data.bridgeTokenList.filter(p => p.chainId === chain)
     let total = 0
     tokensInChain.forEach((item) => {
@@ -75,8 +30,7 @@ function fetchChain(chain) {
     return total
   }
 }
-// t = fetchChain(null)
-// console.log(t())
+
 const chainTvls = {}
 Object.keys(chains).forEach((chain) => {
   const chainName = chains[chain]
@@ -84,12 +38,11 @@ Object.keys(chains).forEach((chain) => {
     fetch: fetchChain(Number(chain))
   }
 })
-// Object.values(chainTvls).forEach((f) => console.log(f.fetch()))
 
-// module.exports = {
-//   misrepresentedTokens: true,
-//   timetravel: false,
-//   methodology: 'Added up all the stable coins deposited in different chains',
-//   ...chainTvls,
-//   fetch: fetchChain(null),
-// }
+module.exports = {
+  misrepresentedTokens: true,
+  timetravel: false,
+  methodology: 'Added up all the stable coins deposited in different chains',
+  ...chainTvls,
+  fetch: fetchChain(null),
+}
