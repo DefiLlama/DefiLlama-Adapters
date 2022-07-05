ddcea31114ad98456a220bc0ff68a35ddfcbca45
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-07-05 14:24:21 +0200

    [minto] code refactor

--- projects/minto/index.js
+++ projects/minto/index.js
@@ -1,19 +1,18 @@
-const axios = require("axios");
-const MINTO_API_TVL = "https://stats-prod.minto.org/v1";
+const { stakings } = require('../helper/staking')
 
-const client = axios.create({
-  baseURL: MINTO_API_TVL
-});
-
-async function fetch() {
-  const tvlRes = await client.get("general/tapp-tvl");
-  return tvlRes.data.value;
-}
+const minto = '0x410a56541bD912F9B60943fcB344f1E3D6F09567'
+const hminto = '0x410a56541bd912f9b60943fcb344f1e3d6f09567'
+const stackingContracts = [
+  '0x78ae303182fca96a4629a78ee13235e6525ebcfb',
+  '0xe742FCE58484FF7be7835D95E350c23CE55A7E12',
+]
 
 module.exports = {
-  doublecounted: true,
-  timetravel: false,
-  misrepresentedTokens: true,
-  methodology: "Return TVL from minto api",
-  fetch
+  bsc: {
+    staking: stakings(stackingContracts, minto, 'bsc')
+  },
+  heco: {
+    tvl: () => ({}),
+    staking: stakings(stackingContracts, hminto, 'heco')
+  }
 }
