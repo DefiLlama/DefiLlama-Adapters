46e7129f64f9379b8c54c5c7a1ea6aebc1b26988
FredCoen <friedrich.kaspar.coen@gmail.com>
2022-07-08 15:31:51 +0200

    correct atlendis tvl

--- projects/atlendis/index.js
+++ projects/atlendis/index.js
@@ -3,7 +3,7 @@ const { getBlock } = require("../helper/getBlock.js");
 const { transformPolygonAddress } = require('../helper/portedTokens');
 
 
-async function fetchData(block, balances, transform, borrowed=false) {
+async function fetchData(block, balances, transform) {
   const baseUrl = 'https://api.thegraph.com/subgraphs/name/atlendis';
   const urlPolygon = `${baseUrl}/atlendis-hosted-service-polygon`;
   const graphQLClient = new GraphQLClient(urlPolygon)
@@ -30,51 +30,32 @@ async function fetchData(block, balances, transform, borrowed=false) {
   const data = await graphQLClient.request(query, {
     block: block
   });
+  for (let i=0; i < data.poolStatuses.length; i++) {
+    let amount = parseInt(data.poolStatuses[i].normalizedAvailableAmount)
+              + parseInt(data.poolStatuses[i].adjustedPendingAmount)
+              + parseInt(data.poolStatuses[i].normalizedBorrowedAmount);
+    let assetAddress = data.poolStatuses[i].pool.parameters.underlyingToken;
 
-  // calculate TVL
-  if (!borrowed) {
-    for (let i=0; i < data.poolStatuses.length; i++) {
-      let amount = parseInt(data.poolStatuses[i].normalizedAvailableAmount)
-                + parseInt(data.poolStatuses[i].adjustedPendingAmount);
-      let assetAddress = data.poolStatuses[i].pool.parameters.underlyingToken;
-
-      assetAddress = transform(assetAddress);
-      balances[assetAddress] = (balances[assetAddress] || 0) + amount / 1e12;
-    }
-  } else {
-    for (let i=0; i < data.poolStatuses.length; i++) {
-      let amount = parseInt(data.poolStatuses[i].normalizedBorrowedAmount)
-      let assetAddress = data.poolStatuses[i].pool.parameters.underlyingToken;
-
-      assetAddress = transform(assetAddress);
-      balances[assetAddress] = (balances[assetAddress] || 0) + amount / 1e12;
-    }
+    assetAddress = transform(assetAddress);
+    balances[assetAddress] = (balances[assetAddress] || 0) + amount / 1e12;
   }
-}
+} 
 
 
 async function tvl (timestamp, block, chainBlocks) {
   const balances = {};
+  console.log("f")
   const transform = await transformPolygonAddress();
   block = await getBlock(timestamp, "polygon", chainBlocks);
   await fetchData(block, balances, transform);
   return balances;
 }
 
-async function borrowed (timestamp, block, chainBlocks) {
-  const balances = {};
-  const transform = await transformPolygonAddress();
-  block = await getBlock(timestamp, "polygon", chainBlocks);
-  await fetchData(block, balances, transform, true);
-  return balances;
-}
-
-
 module.exports = {
   timetravel: true,
   misrepresentedTokens: false,
   polygon: {
     tvl: tvl,
-    borrowed: borrowed,
-  }
+  },
+  methodology: "Counts the tokens deposited in the contracts. Borrowed coins are not taken out of TVL as they represent an asset of the lenders."
 };
