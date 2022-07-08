8bb5a2ca8bcbcb57009d9324d22d0cca27268f6e
realdealshaman <realdealshaman11@gmail.com>
2022-07-09 00:19:26 +0100

    cashio hallmark

--- projects/cashio/index.js
+++ projects/cashio/index.js
@@ -133,6 +133,9 @@ async function tvl() {
 
 module.exports = {
   timetravel: false,
+  hallmarks: [
+    [1647993600, "Infinite mint glitch"]
+],
   methodology:
     "TVL counts LP token deposits made to Cashio and accrued reward tokens to its bank. CoinGecko is used to find the price of tokens in USD.",
   solana: { tvl },
