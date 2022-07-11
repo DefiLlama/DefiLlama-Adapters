7b7a0b998b85772b38e9a00e8573dde7960a3291
Rabus <rabus@dxsale.network>
2022-07-11 06:09:51 +0200

    add more wrapper to secure web3 calls

--- projects/dx/index.js
+++ projects/dx/index.js
@@ -36,174 +36,179 @@ function getTVLTotal(args) {
     let lptokens = [];
     const chain = args.chain;
     const block = chainBlocks[chain];
-    const transform = await getChainTransform(chain);
 
-    //Get Liquidity Locks new from storage
-    try {
-      for (let i = 0; i < args.storageLiquidityLocks.length; i++) {
-        //Get Amount of Locks on Contract
-        const { output: totalLocks } = await sdk.api.abi.call({
-          target: args.storageLiquidityLocks[i],
-          abi: getStorageLockCountV33,
-          chain,
-          block,
-        });
+    try{
+      const transform = await getChainTransform(chain);
 
-        for (let j = 0; j < totalLocks; j++) {
-          //Get Wallet at Lock ID
-          const { output: lockData } = await sdk.api.abi.call({
+      //Get Liquidity Locks new from storage
+      try {
+        for (let i = 0; i < args.storageLiquidityLocks.length; i++) {
+          //Get Amount of Locks on Contract
+          const { output: totalLocks } = await sdk.api.abi.call({
             target: args.storageLiquidityLocks[i],
-            params: j,
-            abi: getStorageLockDataV33,
+            abi: getStorageLockCountV33,
             chain,
             block,
           });
-          const getTokenBalance = (
-            await sdk.api.abi.call({
-              abi: "erc20:balanceOf",
-              target: lockData[2],
-              params: lockData[4],
+
+          for (let j = 0; j < totalLocks; j++) {
+            //Get Wallet at Lock ID
+            const { output: lockData } = await sdk.api.abi.call({
+              target: args.storageLiquidityLocks[i],
+              params: j,
+              abi: getStorageLockDataV33,
               chain,
               block,
-            })
-          ).output;
+            });
+            const getTokenBalance = (
+              await sdk.api.abi.call({
+                abi: "erc20:balanceOf",
+                target: lockData[2],
+                params: lockData[4],
+                chain,
+                block,
+              })
+            ).output;
 
-          lptokens.push({
-            token: lockData[2],
-            balance: getTokenBalance,
-          });
-        }
-      }
-    } catch (e) {
-      console.log(e);
-    }
-    //Get Token Locks new from storage
-    try {
-      for (let i = 0; i < args.storageTokenLocks.length; i++) {
-        //Get Amount of Locks on Contract
-        const { output: totalLocks } = await sdk.api.abi.call({
-          target: args.storageTokenLocks[i],
-          abi: getStorageLockCountV33,
-          chain,
-          block,
-        });
-
-        for (let j = 0; j < totalLocks; j++) {
-          //Get Wallet at Lock ID
-          const { output: lockData } = await sdk.api.abi.call({
-            target: args.storageTokenLocks[i],
-            params: j,
-            abi: getStorageLockDataV33,
-            chain,
-            block,
-          });
-          const getTokenBalance = (
-            await sdk.api.abi.call({
-              abi: "erc20:balanceOf",
-              target: lockData[2],
-              params: lockData[4],
-              chain,
-              block,
-            })
-          ).output;
-
-          tokens.push({
-            token: lockData[2],
-            balance: getTokenBalance,
-          });
-        }
-      }
-    } catch (e) {
-      console.log(e);
-    }
-    //Get Locks from Archives
-    try {
-      for (let i = 0; i < args.locks.length; i++) {
-        let preventDuplicates = [];
-        //Get Amount of Locks on Contract
-        const { output: totalLocks } = await sdk.api.abi.call({
-          target: args.locks[i],
-          abi: getLockCountPerContractV3,
-          chain,
-          block,
-        });
-
-        for (let j = 0; j < totalLocks; j++) {
-          //Get Wallet at Lock ID
-          const { output: walletToId } = await sdk.api.abi.call({
-            target: args.locks[i],
-            params: j,
-            abi: getLockerWalletWithIdV3,
-            chain,
-            block,
-          });
-          //Get Count of Locks per wallet
-          const { output: walletLockCount } = await sdk.api.abi.call({
-            target: args.locks[i],
-            params: walletToId,
-            abi: getLockerPerWalletV3,
-            chain,
-            block,
-          });
-
-          if (preventDuplicates.includes(walletToId)) {
-            continue;
+            lptokens.push({
+              token: lockData[2],
+              balance: getTokenBalance,
+            });
           }
-          preventDuplicates.push(walletToId);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+      //Get Token Locks new from storage
+      try {
+        for (let i = 0; i < args.storageTokenLocks.length; i++) {
+          //Get Amount of Locks on Contract
+          const { output: totalLocks } = await sdk.api.abi.call({
+            target: args.storageTokenLocks[i],
+            abi: getStorageLockCountV33,
+            chain,
+            block,
+          });
 
-          for (let k = 0; k < walletLockCount; k++) {
-            try {
-              //Try resolving as LP Lock
-              const { output: returnFromDataStruct } = await sdk.api.abi.call({
-                target: args.locks[i],
-                params: [walletToId, k],
-                abi: getLockerLPDataV3,
+          for (let j = 0; j < totalLocks; j++) {
+            //Get Wallet at Lock ID
+            const { output: lockData } = await sdk.api.abi.call({
+              target: args.storageTokenLocks[i],
+              params: j,
+              abi: getStorageLockDataV33,
+              chain,
+              block,
+            });
+            const getTokenBalance = (
+              await sdk.api.abi.call({
+                abi: "erc20:balanceOf",
+                target: lockData[2],
+                params: lockData[4],
                 chain,
                 block,
-              });
+              })
+            ).output;
 
-              if (returnFromDataStruct[1]) {
-                lptokens.push({
-                  token: returnFromDataStruct[6],
-                  balance: returnFromDataStruct[3],
-                });
-              }
-            } catch (e) {
-              console.log("No LP Lock", e);
-              //If not a LP Lock it's a token Lock
-              const { output: returnFromDataStruct } = await sdk.api.abi.call({
-                target: args.locks[i],
-                params: [walletToId, k],
-                abi: getLockerTokenDataV3,
-                chain,
-                block,
-              });
+            tokens.push({
+              token: lockData[2],
+              balance: getTokenBalance,
+            });
+          }
+        }
+      } catch (e) {
+        console.log(e);
+      }
+      //Get Locks from Archives
+      try {
+        for (let i = 0; i < args.locks.length; i++) {
+          let preventDuplicates = [];
+          //Get Amount of Locks on Contract
+          const { output: totalLocks } = await sdk.api.abi.call({
+            target: args.locks[i],
+            abi: getLockCountPerContractV3,
+            chain,
+            block,
+          });
 
-              if (returnFromDataStruct[1]) {
-                tokens.push({
-                  token: returnFromDataStruct[12],
-                  balance: returnFromDataStruct[3],
+          for (let j = 0; j < totalLocks; j++) {
+            //Get Wallet at Lock ID
+            const { output: walletToId } = await sdk.api.abi.call({
+              target: args.locks[i],
+              params: j,
+              abi: getLockerWalletWithIdV3,
+              chain,
+              block,
+            });
+            //Get Count of Locks per wallet
+            const { output: walletLockCount } = await sdk.api.abi.call({
+              target: args.locks[i],
+              params: walletToId,
+              abi: getLockerPerWalletV3,
+              chain,
+              block,
+            });
+
+            if (preventDuplicates.includes(walletToId)) {
+              continue;
+            }
+            preventDuplicates.push(walletToId);
+
+            for (let k = 0; k < walletLockCount; k++) {
+              try {
+                //Try resolving as LP Lock
+                const { output: returnFromDataStruct } = await sdk.api.abi.call({
+                  target: args.locks[i],
+                  params: [walletToId, k],
+                  abi: getLockerLPDataV3,
+                  chain,
+                  block,
                 });
+
+                if (returnFromDataStruct[1]) {
+                  lptokens.push({
+                    token: returnFromDataStruct[6],
+                    balance: returnFromDataStruct[3],
+                  });
+                }
+              } catch (e) {
+                console.log("No LP Lock", e);
+                //If not a LP Lock it's a token Lock
+                const { output: returnFromDataStruct } = await sdk.api.abi.call({
+                  target: args.locks[i],
+                  params: [walletToId, k],
+                  abi: getLockerTokenDataV3,
+                  chain,
+                  block,
+                });
+
+                if (returnFromDataStruct[1]) {
+                  tokens.push({
+                    token: returnFromDataStruct[12],
+                    balance: returnFromDataStruct[3],
+                  });
+                }
               }
             }
           }
         }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
-    }
 
-    try {
-      await unwrapUniswapLPs(balances, lptokens, block, chain, transform);
+      try {
+        await unwrapUniswapLPs(balances, lptokens, block, chain, transform);
 
-      for (let i = 0; i < tokens.length; i++) {
-        sdk.util.sumSingleBalance(
-          balances,
-          transform(tokens[i].token),
-          tokens[i].balance
-        );
+        for (let i = 0; i < tokens.length; i++) {
+          sdk.util.sumSingleBalance(
+            balances,
+            transform(tokens[i].token),
+            tokens[i].balance
+          );
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
+    }catch (e) {
       console.log(e);
     }
 
