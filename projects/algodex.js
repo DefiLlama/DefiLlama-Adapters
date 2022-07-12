9eb6e65ca20c19c1c39d5cbeb571508d36066fbd
Alexander Trefonas <atrefonas@gmail.com>
2022-07-12 09:58:00 -0400

    Remove unnecessary call

--- projects/algodex.js
+++ projects/algodex.js
@@ -2,20 +2,11 @@ const { fetchURL } = require('./helper/utils')
 const { toUSDTBalances } = require('./helper/balances')
 
 const usdPriceUrl = "https://mainnet.analytics.tinyman.org/api/v1/current-asset-prices/"
-const algoPriceUrl = "https://eu-central-1.algodex.com/algodex-backend/assets.php"
 const assetTVLURL = "https://eu-central-1.algodex.com/algodex-backend/tvl.php"
 
 async function fetch() {
   const tvlData = await fetchURL(assetTVLURL)
   const usdPriceData = await fetchURL(usdPriceUrl)
-  const algoPriceData = await fetchURL(algoPriceUrl)
-
-  const algoPrices = algoPriceData.data.data.reduce((obj, item) => {
-    if (item.hasOwnProperty("price")) {
-        obj[item.id] = item.price
-    }
-    return obj
-  }, {})
 
   const total_liquidity_in_usd = tvlData.data.reduce((sum, asset) => {
     var assetPrice = 0
