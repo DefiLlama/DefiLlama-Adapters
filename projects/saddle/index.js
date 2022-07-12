4c53fe6df266d88dec388f875eb4803c03d074bf
Weston Nelson <westontnelson@outlook.com>
2022-07-12 00:13:45 -0500

    Update

--- projects/saddle/index.js
+++ projects/saddle/index.js
@@ -345,3 +345,35 @@ module.exports={
   },
       tvl: sdk.util.sumChainTvls([eth_tvl, optTvl, arbTvl, tvlFantom, evmTvl])
 }
+
+const { SDL, veSDL, veSDLStaking } = require("./index");
+
+
+async function staking(timestamp, chainBlocks) {
+  const balances = {};
+
+  await staking(
+    balances,
+    [
+      [SDL, false],
+      [veSDL, false],
+      [veSDLStaking, false],
+    ],
+    staking,
+    chainBlocks["ethereum"],
+    "ethereum"
+  );
+
+  return balances;
+}
+exports.evmTvl = evmTvl;
+
+module.exports={
+  methodology: "Staking TVL",
+  ethereum:{
+      tvl: eth_tvl,
+  },
+      tvl: sdk.util.sumChainTvls([eth_tvl, // SDL Staking TVL
+          staking(veSDLStaking, veSDL, "ethereum", SDL),
+      ]),
+}
