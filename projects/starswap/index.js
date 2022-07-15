6934faaedc65b64153ed0dfc03e74192b50b4c96
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-07-15 18:27:28 +0200

    fix starswap

--- projects/starswap/index.js
+++ projects/starswap/index.js
@@ -1,5 +1,6 @@
-const { getUniTVL } = require('../helper/unknownTokens')
-const { stakingPricedLP } = require("../helper/staking")
+const { getUniTVL, getTokenPrices, } = require('../helper/unknownTokens')
+const { getFixBalances } = require('../helper/portedTokens')
+const sdk = require('@defillama/sdk')
 
 const FACTORIES = "0x0b657e81a0C3E903cbe1228579fBd49AC5D81Ac1"
 
@@ -9,9 +10,9 @@ const TOKENS = {
   STAR: "0x8489f4554790F5A103F2B0398537eAEe68B73884"
 }
 
-const STAKING_CONTRACTS = {
-  astar: "0x0262592d5f489e19afe070abc88a0808afc75250"
-}
+const STAKING_CONTRACT = "0x0262592d5f489e19afe070abc88a0808afc75250"
+const ASTAR_LP = '0x4a0e3b2a0c35737d1c2a78fb76470ce31836024c'
+const chain = 'astar'
 
 module.exports = {
   misrepresentedTokens: true,
@@ -19,7 +20,7 @@ module.exports = {
   astar: {
     tvl: getUniTVL({
       factory: FACTORIES,
-      chain: 'astar',
+      chain,
       coreAssets:
         [
           NATIVE_TOKEN_WASTAR,
@@ -31,13 +32,68 @@ module.exports = {
           "0x75364D4F779d0Bd0facD9a218c67f87dD9Aff3b4"
         ],
     }),
-    staking: stakingPricedLP(
-      STAKING_CONTRACTS.astar,
-      TOKENS.STAR,
-      "astar",
-      "0x4a0e3b2a0c35737d1c2a78fb76470ce31836024c",
-      "astar",
-      true,
-    ),
+    staking:  async (_, _b, { [chain]: block }) => {
+      const { output: { totalStakedTokens } } = await sdk.api.abi.call({
+        target: STAKING_CONTRACT,
+        params: 0,
+        abi: {
+          "type": "function",
+          "stateMutability": "view",
+          "outputs": [
+            {
+              "type": "address",
+              "name": "lpToken",
+              "internalType": "contract IERC20"
+            },
+            {
+              "type": "uint256",
+              "name": "allocPoint",
+              "internalType": "uint256"
+            },
+            {
+              "type": "uint256",
+              "name": "lastRewardBlock",
+              "internalType": "uint256"
+            },
+            {
+              "type": "uint256",
+              "name": "accStarPerShare",
+              "internalType": "uint256"
+            },
+            {
+              "type": "uint16",
+              "name": "depositFeeBP",
+              "internalType": "uint16"
+            },
+            {
+              "type": "uint256",
+              "name": "harvestInterval",
+              "internalType": "uint256"
+            },
+            {
+              "type": "uint256",
+              "name": "totalStakedTokens",
+              "internalType": "uint256"
+            }
+          ],
+          "name": "poolInfo",
+          "inputs": [
+            {
+              "type": "uint256",
+              "name": "",
+              "internalType": "uint256"
+            }
+          ]
+        },
+        chain, block,
+      })
+
+      const balances = { [chain + ':' + TOKENS.STAR] : totalStakedTokens }
+      const transform = await getFixBalances(chain)
+      const { updateBalances } = await getTokenPrices({ chain, block, coreAssets: [NATIVE_TOKEN_WASTAR], lps: [ ASTAR_LP ], allLps: true })
+      await updateBalances(balances)
+      transform(balances)
+      return balances
+    }
   }
 }
