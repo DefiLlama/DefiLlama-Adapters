37721e909fc4607edbf21e4f91c2496271ebd134
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-07-15 16:47:48 +0200

    refactor code

--- projects/vovo/index.js
+++ projects/vovo/index.js
@@ -1,7 +1,18 @@
 const sdk = require("@defillama/sdk");
-
-const usdcVaultAbi = require("../helper/abis/vovoUsdc.json");
-const glpVaultAbi = require("../helper/abis/vovoGlp.json");
+const chain = 'arbitrum'
+const getGlpPriceABI = {
+  "inputs": [],
+  "name": "getGlpPrice",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+}
 
 const usdcVaults = [
   "0x9ba57a1D3f6C61Ff500f598F16b97007EB02E346",
@@ -17,54 +28,43 @@ const glpVaults = [
   "0x46d6dEE922f1d2C6421895Ba182120C784d986d3",
 ];
 
-async function usdcVaultsTVL(chainBlocks) {
-  const tvls = await Promise.all(
-    usdcVaults.map(async (vaultAddress) => {
-      const { output: tvl } = await sdk.api.abi.call({
-        abi: usdcVaultAbi.totalSupply,
-        chain: "arbitrum",
-        target: vaultAddress,
-        block: chainBlocks.arbitrum,
-      });
+async function usdcVaultsTVL(block) {
 
-      return +tvl / 1e6;
-    })
-  );
+  const { output: tvls } = await sdk.api.abi.multiCall({
+    abi: 'erc20:totalSupply',
+    calls: usdcVaults.map(i => ({ target: i })),
+    chain, block,
+  })
 
-  const totalTvl = tvls.reduce((prev, current) => current + prev, 0);
-  return totalTvl;
+  let tvl = 0
+  tvls.forEach(i => tvl += i.output / 1e6)
+  return tvl;
 }
 
-async function glpVaultsTVL(chainBlocks) {
+async function glpVaultsTVL(block) {
   const { output: glpPrice } = await sdk.api.abi.call({
-    abi: glpVaultAbi.getGlpPrice,
-    chain: "arbitrum",
+    abi: getGlpPriceABI,
     target: glpVaults[0],
-    block: chainBlocks.arbitrum,
+    chain, block,
   });
 
-  const tvls = await Promise.all(
-    glpVaults.map(async (vaultAddress) => {
-      const { output: tvl } = await sdk.api.abi.call({
-        abi: glpVaultAbi.totalSupply,
-        chain: "arbitrum",
-        target: vaultAddress,
-        block: chainBlocks.arbitrum,
-      });
-
-      return +tvl / 1e18;
-    })
-  );
-
-  const totalTvl = tvls.reduce((prev, current) => current + prev, 0);
-  return totalTvl * (+glpPrice / 1e18);
+  const { output: tvls } = await sdk.api.abi.multiCall({
+    abi: 'erc20:totalSupply',
+    calls: glpVaults.map(i => ({ target: i })),
+    chain, block,
+  })
+  let tvl = 0
+  tvls.forEach(i => tvl += i.output / 1e18)
+  return tvl * glpPrice / 1e18
 }
 
-async function tvl(timestamp, block, chainBlocks) {
+async function tvl(ts, _, { [chain]: block }) {
   const totalTvl =
-    (await usdcVaultsTVL(chainBlocks)) + (await glpVaultsTVL(chainBlocks));
+    (await usdcVaultsTVL(block)) + (await glpVaultsTVL(block));
 
-  return totalTvl;
+  return {
+    'usd-coin': totalTvl,
+  };
 }
 
 module.exports = {
