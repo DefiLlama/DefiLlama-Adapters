afff9162d548b67b01f631e9050e2cd52ec5fa84
samisbakedham <samsafahi@gmail.com>
2022-07-14 21:48:02 -0700

    Create index.js

---
+++ projects/carthage/index.js
@@ -0,0 +1,35 @@
+const { request, gql } = require("graphql-request");
+const { toUSDTBalances } = require('../helper/balances');
+const graphUrl = 'https://thegraph.cndlchain.com/subgraphs/name/ianlapham/uniswap-v3'
+
+const graphQuery = gql`
+query get_tvl($block: Int) {
+  pangolinFactory(
+    id: "0x5Bb7BAE25728e9e51c25466D2A15FaE97834FD95",
+    block: { number: $block }
+  ) {
+    totalLiquidityUSD
+  }
+}
+`;
+
+async function tvl(timestamp, ethBlock, chainBlocks) {
+  const response = await request(
+    graphUrl,
+    graphQuery,
+    {
+      block:chainBlocks.avax,
+    }
+  );
+
+  return toUSDTBalances(Number(response.carthageFactory.totalLiquidityUSD));
+}
+
+module.exports = {
+  misrepresentedTokens: true,
+  methodology: 'The Carthage subgraph and the Carthage factory contract address are used to obtain the balance held in every LP pair.',
+  candle:{
+    tvl,
+  },
+  start: 1612715300, // 7th-Feb-2021
+}
