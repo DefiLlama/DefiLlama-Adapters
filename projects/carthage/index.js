06646fda352e86cab43ecf7c78cf582fde2ae564
samisbakedham <samsafahi@gmail.com>
2022-07-16 00:49:19 -0700

    j

--- projects/carthage/index.js
+++ projects/carthage/index.js
@@ -1,11 +1,26 @@
-const {calculateUsdUniTvl} = require('../helper/getUsdUniTvl')
+const sdk = require("@defillama/sdk");
+const { calculateUniTvl } = require("../helper/calculateUniTvl");
+const { stakingUnknownPricedLP } = require("../helper/staking");
 
-module.exports={
-    timetravel: true,
-    misrepresentedTokens: true,
-    methodology: "Factory address (0x5Bb7BAE25728e9e51c25466D2A15FaE97834FD95) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
-    candle: {
-        tvl:calculateUsdUniTvl("0x5Bb7BAE25728e9e51c25466D2A15FaE97834FD95", "candle", "0xcC237fa0A4B80bA47992d102352572Db7b96A6B5", ["0x95A0A7953F9292838C0614D690005D5c716E718E", "0xb750990F953B36F806d0327678eCFB4eEFd16979","0x5c17C48F127D6aE5794b2404F1F8A5CeED419eDf",
-                                "0xa018034190943D6c8E10218d9F8E8Af491272411", "0xad43669cbAC863e33449d423261E525de8da0Ff4"], "candle")
-    }
+const FACTORY_ADDRESS = "0x5Bb7BAE25728e9e51c25466D2A15FaE97834FD95";
+
+async function candleTvl(timestamp, block, chainBlocks) {
+  let balances = await calculateUniTvl(
+    (addr) => `candle:${addr}`,
+    chainBlocks.candle,
+    "candle",
+    FACTORY_ADDRESS,
+    0,
+    true
+  );
+  return balances;
 }
+
+
+module.exports = {
+  candle: {
+    tvl: candleTvl,
+  },
+  misrepresentedTokens: true,
+  methodology: "TVL comes from the DEX liquidity pools.",
+};
