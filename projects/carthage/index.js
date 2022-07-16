65bf41ef0519da1abfc317f5c8999d505efd09d5
samisbakedham <samsafahi@gmail.com>
2022-07-16 00:56:24 -0700

    Update index.js

--- projects/carthage/index.js
+++ projects/carthage/index.js
@@ -1,26 +1,38 @@
-const sdk = require("@defillama/sdk");
-const { calculateUniTvl } = require("../helper/calculateUniTvl");
-const { stakingUnknownPricedLP } = require("../helper/staking");
+const { request, gql } = require("graphql-request");
+const { toUSDTBalances } = require('../helper/balances');
+const graphUrl = 'https://thegraph.cndlchain.com/subgraphs/name/ianlapham/uniswap-v3-test'
 
-const FACTORY_ADDRESS = "0x5Bb7BAE25728e9e51c25466D2A15FaE97834FD95";
+const graphQuery = gql`
+query get_tvl($block: Int, $number_gte: Int = 10) {
+  factory(
+    id: "0x5Bb7BAE25728e9e51c25466D2A15FaE97834FD95"
+    block: {number_gte: $number_gte}
+  ) {
+    totalValueLockedETHUntracked
+    totalValueLockedETH
+    totalValueLockedUSD
+    totalValueLockedUSDUntracked
+  }
+}
+`;
 
-async function candleTvl(timestamp, block, chainBlocks) {
-  let balances = await calculateUniTvl(
-    (addr) => `candle:${addr}`,
-    chainBlocks.candle,
-    "candle",
-    FACTORY_ADDRESS,
-    0,
-    true
+async function tvl(timestamp, ethBlock, chainBlocks) {
+  const response = await request(
+    graphUrl,
+    graphQuery,
+    {
+      block:chainBlocks.cndl,
+    }
   );
-  return balances;
+
+  return toUSDTBalances(Number(response.factory.totalValueLockedUSD));
 }
 
-
 module.exports = {
-  candle: {
-    tvl: candleTvl,
-  },
   misrepresentedTokens: true,
-  methodology: "TVL comes from the DEX liquidity pools.",
-};
+  methodology: 'The Carthage subgraph and the Carthage factory contract address are used to obtain the balance held in every LP pair.',
+  candle:{
+    tvl,
+  },
+  start: 1612715300, // 7th-Feb-2021
+}
