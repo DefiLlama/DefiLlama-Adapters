36f2ece895c603f3316f70752edfcc605a5c5c93
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2022-07-16 01:28:30 -0700

    Update speedTest.js

--- speedTest.js
+++ speedTest.js
@@ -21,7 +21,6 @@ function createProvider(name, url, chainId) {
   const providers = {
     ethereum: createProvider("ethereum", "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161", 1),
     bsc: createProvider("bsc", "https://bsc-dataseed4.binance.org", 56),
-    candle: createProvider("candle", "https://candlelabs.org", 534),
     polygon: createProvider("polygon", "https://rpc-mainnet.maticvigil.com/", 137),
     heco: createProvider("heco", "https://http-mainnet.hecochain.com", 128),
     fantom: createProvider("fantom", "https://rpc.ftm.tools", 250),
