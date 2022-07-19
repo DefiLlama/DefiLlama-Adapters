7b68ad824445ff11634c3095f2c69386b0d6271b
Raphael Santo <raphael.esanto@gmail.com>
2022-07-19 09:54:10 -0300

    Calculation Update
    We are now making an open call to the Portfolio directly through the Cardano network and disregarding the tokens within that portfolio for now.
    
    We will add a call where we calculate these tokens later.

--- projects/CardanceSwap/index.js
+++ projects/CardanceSwap/index.js
@@ -1,9 +1,13 @@
 const axios = require('axios')
 
 async function tvl(){
-    const amount = (await axios.get("https://api.cardance.finance/dance/dex/balance")).data.value
+    const ammLocked = (await axios.get("https://cardano-mainnet.blockfrost.io/api/v0/addresses/addr1w9qzpelu9hn45pefc0xr4ac4kdxeswq7pndul2vuj59u8tqaxdznu", {
+        headers:{
+            project_id: "mainnetTV9qV3mfZXbE6e44TVGMe1UoRlLrpSQt"
+        }
+    })).data.amount.find(token=>token.unit==="lovelace").quantity
     return {
-        "cardano":amount
+        "cardano": ammLocked/1e6
     }
 }
 
