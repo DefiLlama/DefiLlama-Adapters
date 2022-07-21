90277f3a463517fab17046ac9d422e79b6abcad9
0xTheCrow <16514232+0xTheCrow@users.noreply.github.com>
2022-07-21 12:06:08 -0700

    change to add reaperfarm optimism tvl

--- projects/reaper.farm/index.js
+++ projects/reaper.farm/index.js
@@ -1,17 +1,32 @@
 const axios = require("axios");
-const REAPER_API = "http://api.reaper.farm/api/";
+const REAPER_API = "http://api.reaper.farm/api";
 
 const client = axios.create({
   baseURL: REAPER_API
 });
 
-async function fetch() {
-  const tvlMsg = await client.get("/tvlTotal");
+async function fetchTvl(chainName) {
+  let tvlMsg;
+  switch (chainName) {
+    case 'fantom':
+      tvlMsg = await client.get('/tvlTotal');
+      break;
+    case 'optimism':
+      tvlMsg = await client.get('/optimism/tvlTotal');
+      break;
+    default:     
+      tvlMsg = await client.get('/tvlTotal');
+  }
   const tvl = tvlMsg.data.data.tvlTotal;
   return tvl;
 }
 
 module.exports = {
   methodology: `TVL is fetched from the Reaper API(http://api.reaper.farm/api)`,
-  fetch
+  fantom: { 
+    tvl: fetchTvl('fantom')
+  },
+  optimism: {
+    tvl: fetchTvl('optimism')
+  }
 }
