fa954ba51fa38219747849220386845ef64d8b9d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-07-21 17:26:56 +0200

    [nftx] add arbitrum

--- projects/nftx/index.js
+++ projects/nftx/index.js
@@ -3,66 +3,78 @@ const { blockQuery } = require('../helper/graph')
 const { getTokenPrices } = require('../helper/unknownTokens')
 const sdk = require('@defillama/sdk')
 
-const weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-async function tvl(_timestamp, block) {
-  const { vaults } = await blockQuery(graphUrl, graphQuery, block, 100)
-  const LPs = new Set(vaults.map(v => v.lpStakingPool.stakingToken.id))
-  const tokens = new Set(vaults.map(v => v.token.id))
-
-  const weth_balances = await sdk.api.abi.multiCall({
-    abi: 'erc20:balanceOf',
-    calls: Array.from(LPs).map(lp => ({
-      target: weth,
-      params: lp
-    })),
-    block
-  })
-
-  const token_balances = await sdk.api.abi.multiCall({
-    abi: 'erc20:totalSupply',
-    calls: Array.from(tokens).map(token => ({
-      target: token,
-    })),
-    block
-  })
-
-  const balances = {}
-
-  sdk.util.sumMultiBalanceOf(balances, token_balances)
-  //sdk.util.sumMultiBalanceOf(balances, weth_balances)
-
-  const lps = weth_balances.output
-    .filter(({ output }) => +output > 2 * 1e18) // only pick pools with minimum 2 eth in it
-    .map(({ input }) => input.params[0])
-
-  const { updateBalances, prices } = await getTokenPrices({
-    block, coreAssets: [weth], lps, allLps: true,
-  })
-
-  const print = []
-  vaults.forEach(vault => {
-    const price = prices[vault.token.id]
-    const balance = (balances[vault.token.id] || 0) / 1e18
-    if (!price || !balance) return;
-    const total = balance * price[1]
-    if (total < 50) return;
-    print.push({ id: vault.token.id, balance, name: vault.token.name, val: total })
-  })
-  print.sort((a, b) => b.val - a.val)
-  console.table(print)
-  console.log(print.reduce((a, i) => a + i.val, 0))
-
-  updateBalances(balances)
-
-  return balances
+const config = {
+  ethereum: {
+    weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    graphUrl: 'https://graph-proxy.nftx.xyz/c/nftx-indexer/subgraphs/id/QmTCMt7aWvxCnEwTQAB1r9a5QMk9cpoFTMfefm4D1c2ewT'
+  },
+  arbitrum: {
+    weth: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    graphUrl: 'https://graph-proxy.nftx.xyz/nftx-arbitrum/subgraphs/name/nftx-hosted/nftx-v2-arbitrum'
+  },
+}
+function getTvl(chain) {
+  const { weth, graphUrl } = config[chain]
+  return async (_timestamp, block) => {
+    const { vaults } = await blockQuery(graphUrl, graphQuery, block, 100)
+    const LPs = new Set(vaults.map(v => v.lpStakingPool.stakingToken.id))
+    const tokens = new Set(vaults.map(v => v.token.id))
+  
+    const weth_balances = await sdk.api.abi.multiCall({
+      abi: 'erc20:balanceOf',
+      calls: Array.from(LPs).map(lp => ({
+        target: weth,
+        params: lp
+      })),
+      block, chain, 
+    })
+  
+    const token_balances = await sdk.api.abi.multiCall({
+      abi: 'erc20:totalSupply',
+      calls: Array.from(tokens).map(token => ({
+        target: token,
+      })),
+      block, chain, 
+    })
+  
+    const balances = {}
+  
+    sdk.util.sumMultiBalanceOf(balances, token_balances)
+    //sdk.util.sumMultiBalanceOf(balances, weth_balances)
+  
+    const lps = weth_balances.output
+      .filter(({ output }) => +output > 2 * 1e18) // only pick pools with minimum 2 eth in it
+      .map(({ input }) => input.params[0])
+  
+    const { updateBalances, prices } = await getTokenPrices({
+      block, coreAssets: [weth], lps, allLps: true, chain, 
+    })
+  
+    const print = []
+    vaults.forEach(vault => {
+      const price = prices[vault.token.id]
+      const balance = (balances[vault.token.id] || 0) / 1e18
+      if (!price || !balance) return;
+      const total = balance * price[1]
+      if (total < 50) return;
+      print.push({ id: vault.token.id, balance, name: vault.token.name, val: total })
+    })
+    print.sort((a, b) => b.val - a.val)
+    console.table(print)
+    console.log(print.reduce((a, i) => a + i.val, 0))
+  
+    updateBalances(balances)
+  
+    return balances
+  }
 }
 
 module.exports = {
   methodology: "Counts total value of all vaults",
-  ethereum: { tvl }
+  ethereum: { tvl: getTvl('ethereum') },
+  arbitrum: { tvl: getTvl('arbitrum') },
 }
 
-const graphUrl = "https://graph-proxy.nftx.xyz/c/nftx-indexer/subgraphs/id/QmTCMt7aWvxCnEwTQAB1r9a5QMk9cpoFTMfefm4D1c2ewT"
 const graphQuery = gql`
 query get_vaults($block: Int) {
   vaults(
