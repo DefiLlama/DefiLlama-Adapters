bbb6618de1b2c8f591e0c70e53bf19abcdca09e0
admin <admin@admins-Mac-mini.local>
2022-07-24 22:37:03 -0400

    add staking

--- projects/radiant/index.js
+++ projects/radiant/index.js
@@ -1,9 +1,13 @@
 const { staking } = require("../helper/staking");
+const { pool2 } = require("../helper/pool2");
 const { aaveChainTvl } = require("../helper/aave");
 const { transformArbitrumAddress } = require("../helper/portedTokens");
 
 const stakingContract = "0xc2054A8C33bfce28De8aF4aF548C48915c455c13";
 const RADIANT = "0x0C4681e6C0235179ec3D4F4fc4DF3d14FDD96017";
+
+const stakingContractPool2 = "0xc963ef7d977ECb0Ab71d835C4cb1Bf737f28d010";
+const RADIANT_WETH_sushiLP = "0x24704aFF49645D32655A76Df6d407E02d146dAfC";
 
 function lending(borrowed) {
   return async (timestamp, ethBlock, chainBlocks) => {
@@ -26,5 +30,6 @@ module.exports = {
     tvl: lending(false),
     borrowed: lending(true),
     staking: staking(stakingContract, RADIANT, "arbitrum"),
+    pool2: pool2(stakingContractPool2, RADIANT_WETH_sushiLP, "arbitrum"),
   },
 };
