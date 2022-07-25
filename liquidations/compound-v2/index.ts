06c5cfbe6171efd89214651fb0f4a25d46c480f9
strobie <0xstrobe@protonmail.com>
2022-07-25 08:41:56 +0000

    no need to flat again kek

--- liquidations/compound-v2/index.ts
+++ liquidations/compound-v2/index.ts
@@ -91,67 +91,65 @@ const positions = async () => {
   const ethPriceInUsd = Number(await uniswapAnchoredView.price("ETH")) / 1e6;
 
   // all positions across all users
-  const positions = accounts
-    .flatMap((account) => {
-      const { totalBorrowValueInEth, totalCollateralValueInEth } = account;
+  const positions = accounts.flatMap((account) => {
+    const { totalBorrowValueInEth, totalCollateralValueInEth } = account;
 
-      const debts = account.tokens
-        .filter(
-          (token) =>
-            !(
-              Number(token.borrowBalanceUnderlying) === 0 &&
-              Number(token.supplyBalanceUnderlying) === 0
-            )
-        )
-        .map((token) => {
-          const decimals = token.market.underlyingDecimals;
-          const price = Number(token.market.underlyingPrice) * ethPriceInUsd;
-          const collateralFactor = Number(token.market.collateralFactor); // equivalent to liqThreshold in aave
-          let debt = new BigNumber(token.borrowBalanceUnderlying);
-          if (token.enteredMarket) {
-            const factoredSupply = new BigNumber(
-              token.supplyBalanceUnderlying
-            ).times(collateralFactor);
-            debt = debt.minus(factoredSupply);
-          }
-          debt = debt.times(price);
+    const debts = account.tokens
+      .filter(
+        (token) =>
+          !(
+            Number(token.borrowBalanceUnderlying) === 0 &&
+            Number(token.supplyBalanceUnderlying) === 0
+          )
+      )
+      .map((token) => {
+        const decimals = token.market.underlyingDecimals;
+        const price = Number(token.market.underlyingPrice) * ethPriceInUsd;
+        const collateralFactor = Number(token.market.collateralFactor); // equivalent to liqThreshold in aave
+        let debt = new BigNumber(token.borrowBalanceUnderlying);
+        if (token.enteredMarket) {
+          const factoredSupply = new BigNumber(
+            token.supplyBalanceUnderlying
+          ).times(collateralFactor);
+          debt = debt.minus(factoredSupply);
+        }
+        debt = debt.times(price);
+        return {
+          debt,
+          price,
+          token: token.market.underlyingAddress,
+          totalBal: token.supplyBalanceUnderlying,
+          decimals,
+        };
+      });
+
+    const liquidablePositions = debts
+      .filter(({ debt }) => debt.lt(0))
+      .map((pos) => {
+        const usdPosNetCollateral = pos.debt.negated();
+        const otherCollateral = new BigNumber(totalCollateralValueInEth)
+          .times(ethPriceInUsd)
+          .minus(usdPosNetCollateral);
+        const diffDebt = new BigNumber(totalBorrowValueInEth)
+          .times(ethPriceInUsd)
+          .minus(otherCollateral);
+        if (diffDebt.gt(0)) {
+          const amountCollateral = usdPosNetCollateral.div(pos.price);
+          const liqPrice = diffDebt.div(amountCollateral);
           return {
-            debt,
-            price,
-            token: token.market.underlyingAddress,
-            totalBal: token.supplyBalanceUnderlying,
-            decimals,
+            owner: account.id,
+            liqPrice: Number(liqPrice.toFixed(6)),
+            collateral: "ethereum:" + pos.token,
+            collateralAmount: new BigNumber(pos.totalBal)
+              .times(10 ** pos.decimals)
+              .toFixed(0),
           };
-        });
+        }
+      })
+      .filter((t) => t !== undefined);
 
-      const liquidablePositions = debts
-        .filter(({ debt }) => debt.lt(0))
-        .map((pos) => {
-          const usdPosNetCollateral = pos.debt.negated();
-          const otherCollateral = new BigNumber(totalCollateralValueInEth)
-            .times(ethPriceInUsd)
-            .minus(usdPosNetCollateral);
-          const diffDebt = new BigNumber(totalBorrowValueInEth)
-            .times(ethPriceInUsd)
-            .minus(otherCollateral);
-          if (diffDebt.gt(0)) {
-            const amountCollateral = usdPosNetCollateral.div(pos.price);
-            const liqPrice = diffDebt.div(amountCollateral);
-            return {
-              owner: account.id,
-              liqPrice: Number(liqPrice.toFixed(6)),
-              collateral: "ethereum:" + pos.token,
-              collateralAmount: new BigNumber(pos.totalBal)
-                .times(10 ** pos.decimals)
-                .toFixed(0),
-            };
-          }
-        })
-        .filter((t) => t !== undefined);
-
-      return liquidablePositions;
-    })
-    .flat();
+    return liquidablePositions;
+  });
 
   return positions;
 };
