70d67279078c45327f53347687b53a75debade17
strobie <0xstrobe@protonmail.com>
2022-07-25 08:38:34 +0000

    remove unused stuff

--- liquidations/compound-v2/index.ts
+++ liquidations/compound-v2/index.ts
@@ -66,122 +66,6 @@ type Market = {
   underlyingAddress: string;
 };
 
-const getUnderwaterAccounts = (accounts: Account[]) => {
-  const underwaterAccounts = accounts.filter((account) => {
-    const { health, totalBorrowValueInEth } = account;
-    return (
-      Number(health) < 1 &&
-      Number(health) > 0 &&
-      Number(totalBorrowValueInEth) > 0
-    );
-  });
-  return underwaterAccounts;
-};
-
-const getCTokenData = (token: Token) => {
-  const { market } = token;
-  const { underlyingAddress, underlyingPrice, underlyingDecimals } = market;
-
-  return {
-    underlyingAddress,
-    underlyingPrice,
-    underlyingDecimals,
-  };
-};
-
-const getBorrowValueInEth = (token: Token) => {
-  const { borrowBalanceUnderlying, market } = token;
-  const { underlyingPrice } = market;
-  return parseFloat(borrowBalanceUnderlying) * parseFloat(underlyingPrice);
-};
-
-const getSupplyValueInEth = (token: Token) => {
-  const { supplyBalanceUnderlying, market } = token;
-  const { underlyingPrice } = market;
-  return parseFloat(supplyBalanceUnderlying) * parseFloat(underlyingPrice);
-};
-
-/**
- * Find seizable supply position with conditions:
- * 1. enteredMarket === true
- * 2. supplyValue >= borrowValue * 0.5
- */
-const findSupplyPositionToSeize = (
-  tokens: Token[],
-  borrowId: string,
-  borrowValueInEth: number
-) => {
-  for (const token of tokens) {
-    const { enteredMarket, id: supplyId } = token;
-
-    // Borrow and supply position can't be the same token
-    if (!enteredMarket || borrowId === supplyId) {
-      continue;
-    }
-
-    const supplyValueInEth = getSupplyValueInEth(token);
-    // Must have enough supply to seize 50% of borrow value
-    if (supplyValueInEth >= borrowValueInEth * 0.5) {
-      return { token, supplyValueInEth };
-    }
-  }
-  return null;
-};
-
-/**
- * Finds a supply position to seize given a borrow position to repay given
- * the tokens of an underwater account.
- */
-const findBorrowAndSupplyPosition = (tokens: Token[]) => {
-  for (const token of tokens) {
-    const { id: borrowId } = token;
-    const borrowValueInEth = getBorrowValueInEth(token);
-    if (borrowValueInEth > 0) {
-      const supplyPositionToSeize = findSupplyPositionToSeize(
-        tokens,
-        borrowId,
-        borrowValueInEth
-      );
-      if (supplyPositionToSeize !== null) {
-        return {
-          borrowPositionToRepay: { token, borrowValueInEth },
-          supplyPositionToSeize,
-        };
-      }
-    }
-  }
-  return null;
-};
-
-const getLiquidatablePosition = (account: Account) => {
-  const { tokens } = account;
-
-  const borrowAndSupplyPosition = findBorrowAndSupplyPosition(tokens);
-  if (!borrowAndSupplyPosition) {
-    return null;
-  }
-
-  const { supplyPositionToSeize } = borrowAndSupplyPosition;
-
-  const {
-    underlyingAddress: collateralAddress,
-    underlyingPrice: liqPriceInEth,
-    underlyingDecimals,
-  } = getCTokenData(supplyPositionToSeize.token);
-  const { id: owner } = account;
-
-  return {
-    owner,
-    // supplyBalanceUnderlying is also calculated from cToken balance while indexing, so there's fractional
-    collateralAmount: (
-      Number(supplyPositionToSeize.token.supplyBalanceUnderlying) *
-      10 ** underlyingDecimals
-    ).toFixed(),
-    collateral: "ethereum:" + collateralAddress,
-    liqPriceInEth,
-  };
-};
-
 // price oracle used in comptroller
 const uniswapAnchoredView = new ethers.Contract(
   "0x65c816077C29b557BEE980ae3cC2dCE80204A0C5",
@@ -197,14 +81,12 @@ const uniswapAnchoredView = new ethers.Contract(
   providers.ethereum
 );
 
-const liqs = async () => {
+const positions = async () => {
   const accounts = (await getPagedGql(
     subgraphUrl,
     accountsQuery,
     "accounts"
   )) as Account[];
-  // const accounts = (await request(subgraphUrl, accountsQuery))
-  //   .accounts as Account[];
 
   const ethPriceInUsd = Number(await uniswapAnchoredView.price("ETH")) / 1e6;
 
@@ -276,6 +158,6 @@ const liqs = async () => {
 
 module.exports = {
   ethereum: {
-    liquidations: liqs,
+    liquidations: positions,
   },
 };
