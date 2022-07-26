535772c9e1183642c00acc7c4b3811836e7f1326
Michalis Kargakis <kargakis@protonmail.com>
2022-07-26 18:54:24 +0200

    Add Toucan Protocol support

---
+++ projects/toucan-protocol/config.js
@@ -0,0 +1,10 @@
+const CONFIG_DATA = {
+    polygon: {
+        bct: "0x2F800Db0fdb5223b3C3f354886d907A671414A7F",
+        nct: "0xD838290e877E0188a4A44700463419ED96c16107",
+    },
+};
+  
+module.exports = {
+    CONFIG_DATA,
+};
