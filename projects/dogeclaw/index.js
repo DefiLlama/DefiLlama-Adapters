498378263e6fedd374a9fba8c747789d5cef1adf
DogeClaw <109725451+DogeClaw@users.noreply.github.com>
2022-07-26 15:59:35 +0800

    Add files via upload

--- projects/dogeclaw/index.js
+++ projects/dogeclaw/index.js
@@ -1,8 +1,9 @@
+
 const { masterchefExports, } = require('../helper/unknownTokens')
 
 const chain = 'okexchain'
 const contract = '0x1bb44D416620902a7f8AdF521422751A9f86d213'
-const claw = '0x796294Cd49c81923391e869684Cf463646587133'
+const claw = '0xc2f1a8570361DAA6994936d1Dd397e1434F2E2B3'
 
 module.exports = masterchefExports({ chain, masterchef: contract, nativeToken: claw, coreAssets: [
   '0x382bb369d343125bfb2117af9c149795c6c65c50', // tether
