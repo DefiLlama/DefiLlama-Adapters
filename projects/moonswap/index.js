bf1f317dc27f87ffde4c342b303a7b23ee242577
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-07-29 11:48:44 +0200

    fix moonswap

--- projects/moonswap/index.js
+++ projects/moonswap/index.js
@@ -1,10 +1,25 @@
-const {getChainTvl} = require('../helper/getUniSubgraphTvl')
+const { getChainTvl } = require('../helper/getUniSubgraphTvl')
+const { getUniTVL } = require('../helper/unknownTokens')
 
 const tvl = getChainTvl({
-    'moonriver': 'https://api.thegraph.com/subgraphs/name/moonfarmin/moonswap-dex'
+  'moonriver': 'https://api.thegraph.com/subgraphs/name/moonfarmin/moonswap-dex'
 })
 
-module.exports={
-    tvl: tvl('moonriver'),
-    methodology: "We get the tvl from the subgraph at graph.moonfarm.in/subgraphs/name/moonswap, it only includes liquidity on the DEX"
+module.exports = {
+  moonriver: {
+    tvl: getUniTVL({
+      chain: 'moonriver',
+      factory: '0x056973f631a5533470143bb7010c9229c19c04d2',
+      coreAssets: [
+        '0xf50225a84382c74CbdeA10b0c176f71fc3DE0C4d', // moonriver
+        "0xb44a9b6905af7c801311e8f4e76932ee959c663c", // usdt
+        "0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d", // usdc
+        "0x639a647fbe20b6c8ac19e48e2de44ea792c62c5c", // eth
+      ],
+      blacklist: [
+        '0xfd301ca82d007880e678bb750a771550c5104ff2', // ANKR, bad decimal?
+      ]
+    })
+  },
+  misrepresentedTokens: true,
 } // node test.js projects/moonswap/index.js
