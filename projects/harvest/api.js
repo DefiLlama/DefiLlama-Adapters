89cb9849d72498fdd6f3cd34e47cf9f92b09b482
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-07-29 17:19:28 +0200

    added heroku exports

---
+++ projects/harvest/api.js
@@ -0,0 +1,8 @@
+const { getExports } = require('../helper/heroku-api')
+const indexExports = require('../harvest')
+const chainKeys = Object.keys(indexExports).filter(chain => typeof indexExports[chain] === 'object' && indexExports[chain].tvl)
+
+module.exports = {
+  timetravel: false,
+  ...getExports("harvest", chainKeys),
+}
---
+++ projects/hydradex/api.js
@@ -0,0 +1,8 @@
+const { getExports } = require('../helper/heroku-api')
+const indexExports = require('../hydradex')
+const chainKeys = Object.keys(indexExports).filter(chain => typeof indexExports[chain] === 'object' && indexExports[chain].tvl)
+
+module.exports = {
+  timetravel: false,
+  ...getExports("harvest", chainKeys),
+}
