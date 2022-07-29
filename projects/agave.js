804665552c49c7518c5564eae6dd8f0919f98918
Luigy-Lemon <luigy-lemon@protonmail.com>
2022-07-29 12:22:13 +0100

    Update agave.js

--- projects/agave.js
+++ projects/agave.js
@@ -5,8 +5,8 @@ const { aaveChainTvl} = require('./helper/aave.js');
 const agaveTokenAddress = '0x3a97704a1b25F08aa230ae53B352e2e72ef52843'
 const agaveStakingContract = '0x610525b415c1BFAeAB1a3fc3d85D87b92f048221'
 
-const addressesProviderRegistryXDAI = "0xa5E80AEAa020Ae41b1cBEe75dE7826297F7D803E"
-const protocolDataHelper = '0xa874f66342a04c24b213BF0715dFf18818D24014'
+const addressesProviderRegistryXDAI = "0x4BaacD04B13523D5e81f398510238E7444E11744"
+const protocolDataHelper = '0x24dCbd376Db23e4771375092344f5CbEA3541FC0'
 
 module.exports = {
   hallmarks: [
