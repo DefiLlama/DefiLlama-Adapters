2a299c8f0f40cd80b9e2730d5e127d6d015dd91e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-07-29 11:36:06 +0200

    [fiatdao] use multicall

--- projects/fiatdao/index.js
+++ projects/fiatdao/index.js
@@ -14,46 +14,51 @@ function resolveUnderlier(underlier) {
 async function tvl(timestamp, block) {
   const balances = {};
 
-  const metadata = Object.entries(
-    (await fetchURL('https://raw.githubusercontent.com/fiatdao/changelog/main/metadata/metadata-mainnet.json')).data
-  )
+  const metadata = (await fetchURL('https://raw.githubusercontent.com/fiatdao/changelog/main/metadata/metadata-mainnet.json')).data
+  const allVaults = Object.keys(metadata)
+  const { output: tokensAll } = await sdk.api.abi.multiCall({ abi: abi.token, calls: allVaults.map(i => ({ target: i })), block, })
+  const tokens = []
+  const vaults = []
 
-  await Promise.all(metadata.map(async ([vault, { tokenIds }]) => {
-    const token = await sdk.api.abi.call({target: vault, block, abi: abi.token})
-    if (token.output === '0x0000000000000000000000000000000000000000') return
+  tokensAll.forEach(({ output, input: { target } }) => {
+    if (output !== '0x0000000000000000000000000000000000000000') {
+      vaults.push(target)
+      tokens.push(output)
+    }
+  })
 
-    return Promise.all(tokenIds.map(async (tokenId) => {
-      // ERC20 based vaults
-      if (tokenId === '0') {
-        // TODO: use multicall
-        const tokenBalance = await sdk.api.erc20.balanceOf({target: token.output, owner: vault, block})
-        const tokenScale = await sdk.api.abi.call({target:vault, block, abi: abi.tokenScale})
-        const underlier = await sdk.api.abi.call({target:vault, block, abi: abi.underlierToken})
-        const underlierScale = await sdk.api.abi.call({target:vault, block, abi: abi.underlierScale})
-        const price = await sdk.api.abi.call({target:vault, block, abi: abi.fairPrice, params: [0, false, false]})
+  const vaultCalls = vaults.map(i => ({ target: i }))
+  const { output: tokenScales } = await sdk.api.abi.multiCall({ abi: abi.tokenScale, calls: vaultCalls, block, })
+  const { output: underliers } = await sdk.api.abi.multiCall({ abi: abi.underlierToken, calls: vaultCalls, block, })
+  const { output: underlierScales } = await sdk.api.abi.multiCall({ abi: abi.underlierScale, calls: vaultCalls, block, })
 
-        sdk.util.sumSingleBalance(
-          balances,
-          resolveUnderlier(underlier.output),
-          BigNumber(tokenBalance.output).times(underlierScale.output).div(tokenScale.output).times(price.output).div(1e18).toFixed(0)
-        )
-      // ERC1155 based vaults
-      } else {
-        // TODO: use multicall
-        const tokenBalance = await sdk.api.abi.call({target: token.output, block, abi: abi.balanceOf, params: [vault, tokenId]})
-        const tokenScale = await sdk.api.abi.call({target:vault, block, abi: abi.tokenScale})
-        const underlier = await sdk.api.abi.call({target:vault, block, abi: abi.underlierToken})
-        const underlierScale = await sdk.api.abi.call({target:vault, block, abi: abi.underlierScale})
-        const price = await sdk.api.abi.call({target:vault, block, abi: abi.fairPrice, params: [tokenId, false, false]})
-    
-        sdk.util.sumSingleBalance(
-          balances,
-          resolveUnderlier(underlier.output),
-          BigNumber(tokenBalance.output).times(underlierScale.output).div(tokenScale.output).times(price.output).div(1e18).toFixed(0)
-        )
+  const erc20Metadata = []
+  const erc1155Metadata = []
+
+  underliers.forEach(({ output: token, input: { target: vault } }, i) => {
+    const underlier = resolveUnderlier(token)
+    const scale = BigNumber(underlierScales[i].output / (tokenScales[i].output * 1e18))
+    metadata[vault].tokenIds.forEach(id => {
+      if (id === '0') {
+        erc20Metadata.push({ vault, scale, underlier, tokenCall: { target: tokens[i], params: vault }, priceCall: { target: vault, params: [0, false, false] } })
+        return;
       }
-    }))
-  }))
+      erc1155Metadata.push({ vault, scale, underlier, tokenCall: { target: tokens[i], params: [vault, id] }, priceCall: { target: vault, params: [id, false, false] } })
+    })
+  })
+
+  const { output: erc20Balances } = await sdk.api.abi.multiCall({ abi: 'erc20:balanceOf', calls: erc20Metadata.map(i => i.tokenCall), block, })
+  const { output: erc20Prices } = await sdk.api.abi.multiCall({ abi: abi.fairPrice, calls: erc20Metadata.map(i => i.priceCall), block, })
+  const { output: erc1155Balances } = await sdk.api.abi.multiCall({ abi: abi.balanceOf, calls: erc1155Metadata.map(i => i.tokenCall), block, })
+  const { output: erc1155Prices } = await sdk.api.abi.multiCall({ abi: abi.fairPrice, calls: erc1155Metadata.map(i => i.priceCall), block, })
+
+  erc20Balances.forEach(({ output, }, i) => {
+    sdk.util.sumSingleBalance(balances, erc20Metadata[i].underlier, erc20Metadata[i].scale.times(output).times(erc20Prices[i].output).toFixed(0))
+  })
+
+  erc1155Balances.forEach(({ output, }, i) => {
+    sdk.util.sumSingleBalance(balances, erc1155Metadata[i].underlier, erc1155Metadata[i].scale.times(output).times(erc1155Prices[i].output).toFixed(0))
+  })
 
   return balances
 }
