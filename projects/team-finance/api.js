06c0751af10eae5a00e72aa9a3a20057b85befae
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-07-29 17:59:10 +0200

    harvest fetch -> tvl

--- projects/team-finance/api.js
+++ projects/team-finance/api.js
@@ -3,6 +3,7 @@ const indexExports = require('./index')
 const chainKeys = Object.keys(indexExports).filter(chain => typeof indexExports[chain] === 'object' && indexExports[chain].tvl)
 
 module.exports = {
+  misrepresentedTokens: true,
   timetravel: false,
   ...getExports("team-finance", chainKeys),
 }
