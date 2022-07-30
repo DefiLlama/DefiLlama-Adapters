1d30f98397ef49f88aae6a95aa5a42abbc844eb1
Define101 <93603962+Define101@users.noreply.github.com>
2022-07-30 09:28:52 +0100

    fix it
    fix it

--- projects/moon-swap/index.js
+++ projects/moon-swap/index.js
@@ -11,8 +11,6 @@ module.exports = {
         '0x1f545487c62e5acfea45dcadd9c627361d1616d8', // WBTC
         '0xa47f43de2f9623acb395ca4905746496d2014d57', // ETH
         '0x6963efed0ab40f6c3d7bda44a05dcf1437c44372', // USDC
-        '0xfe97e85d13abd9c1c33384e796f10b73905637ce', // USDT
-        '0x8e28460074f2ff545053e502e774dddc97125110'  //cMOON
       ]
     }),
   }
