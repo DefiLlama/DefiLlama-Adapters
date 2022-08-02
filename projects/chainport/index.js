b2615a39824b45a0c8c742e59257d793d4bbad4b
Eitan Seri-Levi <eitan@Eitans-MacBook-Pro.local>
2022-08-02 13:38:45 +0300

    tvl per chain

--- projects/chainport/index.js
+++ projects/chainport/index.js
@@ -1,12 +1,23 @@
 const utils = require('../helper/utils');
 
-async function fetch() {
-    let tvl = await utils.fetchURL('https://api.chainport.io/tvl/get')
+async function fetchByNetwork(network_name) {
+    let tvl = await utils.fetchURL('https://api.chainport.io/api/tvl_per_chain')
 
-    return tvl.data["tvl_usd"]
+    return tvl.data[network_name]
 }
 
 module.exports = {
     methodology: "assets in liquidity are counted as TVL",
-    fetch
+    ethereum: {
+        tvl: fetchByNetwork('ETHEREUM')
+    },
+    polygon: {
+        tvl: fetchByNetwork('POLYGON')
+    },
+    bsc: {
+        tvl: fetchByNetwork('BSC')
+    },
+    fantom: {
+        tvl: fetchByNetwork('FANTOM')
+    }
 }
