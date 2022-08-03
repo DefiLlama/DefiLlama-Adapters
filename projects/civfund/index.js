b9020e1e9bd52eceef11677286121b7dacb95378
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-03 23:32:19 +0530

    [civfund] code refactor

--- projects/civfund/index.js
+++ projects/civfund/index.js
@@ -1,27 +1,21 @@
-const sdk = require('@defillama/sdk');
-const { calculateUsdUniTvlPairs } = require('../helper/getUsdUniTvl')
-const { staking } = require('../helper/staking')
-
-const civFarmContract = "0x8a774F790aBEAEF97b118112c790D0dcccA61099";
+const { sumTokens2 } = require('../helper/unwrapLPs')
+const civFarmContract = "0x8a774F790aBEAEF97b118112c790D0dcccA61099"
 const stakingTokens = [
-  "0x73a83269b9bbafc427e76be0a2c1a1db2a26f4c2",
-  "0x37fe0f067fa808ffbdd12891c0858532cfe7361d",
-  "0x6c406daeca809382e649d6c8f768450bf8dbc1dd",
-  "0xa65653bb6e1338dbce69191bb1328700881fc051",
-  "0xed247449a7ca06db5b27b44b2c092f0b48bbdb77",
+  "0x37fE0f067FA808fFBDd12891C0858532CFE7361d",
+  "0x73A83269b9bbAFC427E76Be0A2C1a1db2a26f4C2",
 ];
 
-
-const stonePoolTVL = staking(civFarmContract, stakingTokens[0], "ethereum")
-const civPoolTVL = staking(civFarmContract, stakingTokens[1], "ethereum")
-const stoneEthTVL = staking(civFarmContract, stakingTokens[2], "ethereum")
-const stoneCivTVL = staking(civFarmContract, stakingTokens[3], "ethereum")
-const civEthTVL = staking(civFarmContract, stakingTokens[4], "ethereum")
-const totalTVLs = sdk.util.sumChainTvls([stonePoolTVL, civPoolTVL,stoneEthTVL,stoneCivTVL,civEthTVL])
+const pool2Tokens = [
+  '0xED247449A7CA06DB5b27B44B2c092f0B48bbDB77',
+  '0x6C406daecA809382E649d6c8f768450bF8Dbc1dD',
+  '0xA65653BB6e1338dbCe69191bb1328700881fC051',
+]
 
 module.exports = {
     misrepresentedTokens: true,
     ethereum: {
-        tvl: totalTVLs
+        tvl: () => ({}),
+        staking: async (_, block) => sumTokens2({ block, owner: civFarmContract, tokens: stakingTokens}),
+        pool2: async (_, block) => sumTokens2({ block, owner: civFarmContract, tokens: pool2Tokens, resolveLP: true }),
     },
 }
