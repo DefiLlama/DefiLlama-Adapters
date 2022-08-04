0eac0056e02b9f70be90046eaf170bae9cd5bbc5
khiemkino <87562184+khiemsoft@users.noreply.github.com>
2022-08-04 14:24:43 +0700

    Updated Feedback Baryon

--- projects/baryon/index.js
+++ projects/baryon/index.js
@@ -1,15 +1,16 @@
-const axios = require('axios')
-
-const endpoint = "https://api.baryon.network/program/pools"
-
-async function fetch() {
-  const pools = await axios.get(endpoint);
-  return pools.data.reduce((sum, pool) =>
-    sum + pool.liquidity
-    , 0);
-}
+const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
-  timetravel: false,
-  fetch
+  bsc: {
+    misrepresentedTokens: true,
+    tvl: getUniTVL({
+      chain: 'bsc',
+      factory: '0x03879e2a3944fd601e7638dfcbc9253fb793b599',
+      coreAssets: [
+        '0x23c5d1164662758b3799103effe19cc064d897d6', // AURA
+        '0xe9e7cea3dedca5984780bafc599bd69add087d56',  // BUSD
+        '0xaEC945e04baF28b135Fa7c640f624f8D90F1C3a6',  // C98
+      ]
+    })
+  }
 }
