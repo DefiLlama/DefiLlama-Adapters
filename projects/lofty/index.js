91396720af452f9064476a3fe7da15970e590725
codinronan <patrick@engineerapart.com>
2022-08-03 19:13:24 -0500

    [lofty] adding lofty

---
+++ projects/lofty/index.js
@@ -0,0 +1,36 @@
+
+const sdk = require('@defillama/sdk');
+const { toUSDTBalances } = require('../helper/balances');
+const retry = require("async-retry");
+const axios = require("axios");
+const { lookupApplications, } = require("../helper/algorand");
+
+const LoftyTVLApi = "https://api.lofty.ai/prod/properties/v2/valuations";
+
+async function loftyTVL() {
+  const response = (
+    await retry(
+      async (bail) =>
+        await axios.get(LoftyTVLApi, {
+          headers: {
+            'X-API-Client': 'DEFILLAMA'
+          }
+        })
+    )
+  ).data.data;
+
+  const total = response.reduce((acc, item) => {
+    acc = acc + item.propertyPrice.currentInvestment;
+    return acc;
+  }, 0);
+
+  return toUSDTBalances(total);
+}
+
+module.exports = {
+  algorand: {
+    tvl: loftyTVL,
+  }
+};
+
+// node test.js projects/lofty/index.js
