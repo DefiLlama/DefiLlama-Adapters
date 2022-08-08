6884822b85b5acf0279b19e48b0436b69bb6405c
0xtawa <0xtawanofi@gmail.com>
2022-08-08 19:39:34 +0200

    Add balancer customBackfill function

--- dexVolumes/balancer/index.ts
+++ dexVolumes/balancer/index.ts
@@ -1,6 +1,9 @@
-import { DexVolumeAdapter } from "../dexVolume.type";
+import { DexVolumeAdapter, Fetch } from "../dexVolume.type";
 import { getChainVolume } from "../helper/getUniSubgraphVolume";
 import { ARBITRUM, ETHEREUM, POLYGON } from "../helper/chains";
+import { Chain } from "@defillama/sdk/build/general";
+import { getChainBlocks } from "@defillama/sdk/build/computeTVL/blocks";
+import { getBlock } from "../../projects/helper/getBlock"
 
 const endpoints = {
   [ETHEREUM]: "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer",
@@ -23,24 +26,38 @@ const graphs = getChainVolume({
   hasDailyVolume: false,
 });
 
+const customBackfill = (chain: Chain): Fetch => async (timestamp: number, chainBlocks: ChainBlocks) => {
+  const fetchGetVolume = graphs(chain)
+  const resultDayN = await fetchGetVolume(timestamp, chainBlocks)
+  const timestampPreviousDay = timestamp - 60 * 60 * 24
+  const chainBlocksPreviousDay = (await getBlock(timestampPreviousDay, chain, {})) - 20
+  const resultPreviousDayN = await fetchGetVolume(timestampPreviousDay, { [chain]: chainBlocksPreviousDay })
+  return {
+    block: resultDayN.block,
+    timestamp: resultDayN.timestamp,
+    totalVolume: resultDayN.totalVolume,
+    dailyVolume: `${Number(resultDayN.totalVolume) - Number(resultPreviousDayN.totalVolume)}`,
+  }
+}
+
 const adapter: DexVolumeAdapter = {
   volume: {
     [ETHEREUM]: {
       fetch: graphs(ETHEREUM),
       start: 0,
-      customBackfill: () => {},
+      customBackfill: customBackfill(ETHEREUM),
     },
     // POLYGON
     [POLYGON]: {
       fetch: graphs(POLYGON),
       start: 0,
-      customBackfill: () => {},
+      customBackfill: customBackfill(POLYGON),
     },
     // ARBITRUM
-    [POLYGON]: {
-      fetch: graphs(POLYGON),
+    [ARBITRUM]: {
+      fetch: graphs(ARBITRUM),
       start: 0,
-      customBackfill: () => {},
+      customBackfill: customBackfill(ARBITRUM),
     },
   },
 };
