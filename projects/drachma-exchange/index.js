82176a236bd1eac030d0810a59dfa717b0dd6740
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-10 11:05:42 +0200

    refactor code

--- projects/drachma-exchange/index.js
+++ projects/drachma-exchange/index.js
@@ -1,31 +1,19 @@
-const llama = [
-  {
-    coingeckoID: "tether",
-    account: "z7dYPG6f37n5DVt2tVzebdoXdvsnp1axj3iP3cTMtPk",
-  },
-  {
-    coingeckoID: "usd-coin",
-    account: "5U9E5wZZT4WeCswKCQ5xZ1JZJ33hGdmNjBJmkxQcrbTb",
-  },
-  {
-    coingeckoID: "usdh",
-    account: "6xVXwpwTkyLYkC3j8vHgX1zp7eUSB79f4ugjokKZTHVS",
-  },
-];
+const { sumTokens2, } = require("../helper/solana");
 
 async function tvl() {
-  return Promise.all(
-    llama.map(async ({ coingeckoID, account }) => {
-      return {
-        coingeckoID,
-        amount: await getTokenAccountBalance(account),
-      };
-    })
-  );
+  return sumTokens2({
+    tokensAndOwners: [
+      ['Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB', 'J1bMZJc4aj6RZVTf5vjDBwrwgoRAnvttkhU4TviuqrtL'],
+      ['EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v', 'J1bMZJc4aj6RZVTf5vjDBwrwgoRAnvttkhU4TviuqrtL'],
+      ['USDH1SM1ojwWUga67PGrgFWUHibbjqMvuMaDkRJTgkX', 'J1bMZJc4aj6RZVTf5vjDBwrwgoRAnvttkhU4TviuqrtL'],
+    ]
+  })
 }
 
 module.exports = {
   methodology:
     "drachma exchange TVL is computed by looking at the balances of the accounts holding the tokens backing the drachma vault user. The data comes from https://drachma.exchange/pool.",
-  tvl,
+  solana: {
+    tvl
+  },
 };
