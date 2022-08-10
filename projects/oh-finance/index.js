49c5522afe867edf24d4f4cb182f79ab32636cb2
Odysseus The King <info@liquid.software>
2022-08-10 14:38:55 -0700

    Add Metis and Avax Banks

--- projects/oh-finance/index.js
+++ projects/oh-finance/index.js
@@ -21,15 +21,23 @@ module.exports={
     },
     avax:{
         tvl: sdk.util.sumChainTvls([
-            getBankTvl("0x8B1Be96dc17875ee01cC1984e389507Bb227CaAB", "avax"), // usdc
-            getBankTvl("0xd96AbEcf6AA022735CFa9CB512d63645b0834720", "avax"), // usdt
-            getBankTvl("0xF74303DD14E511CCD90219594e8069d36Da01DCD", "avax"), // dai
+            getBankTvl("0x8B1Be96dc17875ee01cC1984e389507Bb227CaAB", "avax"), // usdc.e
+            getBankTvl("0xd96AbEcf6AA022735CFa9CB512d63645b0834720", "avax"), // usdt.e
+            getBankTvl("0xF74303DD14E511CCD90219594e8069d36Da01DCD", "avax"), // dai.e
+            getBankTvl("0xe001DeCc1763F8BadBbc1b10c2D6db0900f9B928", "avax"), // usdc
+            getBankTvl("0xB3ce618F43b53Cdc12077FB937f9fF465BcE1f60", "avax"), // usdt
         ])
     },
     moonriver: {
         tvl: sdk.util.sumChainTvls([
             getBankTvl("0x4C211F45876d8EC7bAb54CAc0e32AAD15095358A","moonriver"), // usdc
             getBankTvl("0xdeA7Ff1D84B7E54587b434C1A585718857CF61d1","moonriver"), // usdt
+        ])
+    },
+    metis: {
+        tvl: sdk.util.sumChainTvls([
+            getBankTvl("0x4C211F45876d8EC7bAb54CAc0e32AAD15095358A", "metis"),
+            getBankTvl("0xc53bC2517Fceff56308b492AFad4A53d96d16ed8", "metis")
         ])
     }
 }
