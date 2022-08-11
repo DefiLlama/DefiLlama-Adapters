c440ac4da03e1b2e9318f9bdce8774723f3f5f0a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-11 22:23:47 +0200

    revert sushiswap change

--- projects/sushiswap/index.js
+++ projects/sushiswap/index.js
@@ -1,15 +1,12 @@
 const {staking} = require('../helper/staking')
-const { getExports } = require('../helper/heroku-api')
-const indexExports = require('./api')
+const modulesToExport = require('./api')
 
 const xSUSHI = "0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272"
 const SUSHI = "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"
-const chainKeys = Object.keys(indexExports).filter(chain => typeof indexExports[chain] === 'object' && indexExports[chain].tvl)
 
+modulesToExport.ethereum.staking = staking(xSUSHI, SUSHI, 'ethereum')
 module.exports = {
-  timetravel: false,
-  misrepresentedTokens: true,
-  ...getExports("sushiswap", chainKeys),
+    misrepresentedTokens: true,
+    ...modulesToExport,
 }
-
-module.exports.ethereum.staking = staking(xSUSHI, SUSHI, 'ethereum')
+// node test.js projects/sushiswap/index.js
