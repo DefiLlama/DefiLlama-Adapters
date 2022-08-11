8f41a337a2a45e1a1f43a1f77beae836c63a5f27
xiaodongyu <xiaodongyu@yangqianguan.com>
2022-08-11 17:00:23 +0800

    Update Earnmos adapter url

--- projects/earnmos/index.js
+++ projects/earnmos/index.js
@@ -2,7 +2,7 @@ const retry = require('async-retry')
 const axios = require("axios");
 
 async function fetch() {
-    const {data: {tvl}} = await retry(async () => await axios.get('http://localhost:3004/defi-llama/tvl'));
+    const {data: {tvl}} = await retry(async () => await axios.get('https://app.earnmos.fi/defi-llama/tvl'));
 
     return tvl;
 }
