7efbefd13eb0227510779c634a0fc7d1dfbe941b
vitalii427 <vitalii427@yahoo.com>
2022-08-12 17:50:21 +0300

    Minor refactoring

--- projects/pembrock.js
+++ projects/pembrock.js
@@ -1,17 +1,19 @@
 const { BigNumber } = require('bignumber.js');
-const {call, addTokenBalances, sumSingleBalance} = require('./helper/near')
+const { call, addTokenBalances, sumSingleBalance } = require('./helper/near')
 
 const PEMBROCK_CONTRACT = "v1.pembrock.near"
 const REF_FINANCE_CONTRACT = "v2.ref-finance.near"
 const REF_BOOST_CONTRACT = "boostfarm.ref-labs.near"
 
-function addFarmBalances(farms, seeds, balances) {
+async function addFarmBalances(farms, seeds, balances) {
   return Promise.all(Object.values(farms).map(async value => {
-    const pool = await call(REF_FINANCE_CONTRACT, "get_pool", {"pool_id": value.ref_pool_id});
-    const nonStakedShares = await call(REF_FINANCE_CONTRACT, "mft_balance_of", {token_id:  `:${value.ref_pool_id}`, account_id: PEMBROCK_CONTRACT});
-    const stakedFreeShares = seeds[`v2.ref-finance.near@${value.ref_pool_id}`].free_amount;
-    const stakedLockedShares = seeds[`v2.ref-finance.near@${value.ref_pool_id}`].locked_amount;
-    const shares = BigNumber(nonStakedShares).plus(stakedFreeShares).plus(stakedLockedShares);
+    const [pool, nonStakedShares] = await Promise.all([
+      call(REF_FINANCE_CONTRACT, "get_pool", {"pool_id": value.ref_pool_id}),
+      call(REF_FINANCE_CONTRACT, "mft_balance_of", {token_id:  `:${value.ref_pool_id}`, account_id: PEMBROCK_CONTRACT})
+    ]);
+    const seed = seeds[`${REF_FINANCE_CONTRACT}@${value.ref_pool_id}`];
+    
+    const shares = BigNumber(nonStakedShares).plus(seed.free_amount).plus(seed.locked_amount);
 
     const firstTokenAmount = shares.multipliedBy(pool.amounts[0]).dividedBy(pool.shares_total_supply);
     const secondTokenAmount = shares.multipliedBy(pool.amounts[1]).dividedBy(pool.shares_total_supply);
