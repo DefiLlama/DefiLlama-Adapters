be47a3ec5035369556ba4cab0eb0457cf3060acb
qudacki <qudacki@gmail.com>
2022-08-13 20:09:00 +0900

    refactor

--- projects/starlay/index.js
+++ projects/starlay/index.js
@@ -1,7 +1,7 @@
 const { getReserves, getStarlayTvl } = require("./starlay");
 const BigNumber = require("bignumber.js");
 const { getBorrowed } = require("../helper/aave");
-const { TOKENS, DOT_TOKEN, DOT_DECIMALS, AUSD_TOKEN, AUSD_DECIMALS, DEFAULT_DECIMALS, LAY_ADDRESS } = require("./constanrs");
+const { TOKENS, TOKEN_INFO, DEFAULT_DECIMALS, LAY_ADDRESS } = require("./constanrs");
 const { getLockedLAY } = require("./ve");
 
 const transferFromAddress = (underlying) => TOKENS[underlying]
@@ -36,10 +36,10 @@ function astar(borrowed) {
    return Object.keys(balances).reduce((res, key) => {
       if (key.startsWith("0x"))
         return { ...res, [key]: balances[key] };
-      if (key === DOT_TOKEN)
-          return { ...res, [key]: new BigNumber(balances[key]).shiftedBy(-DOT_DECIMALS) };
-      if (key === AUSD_TOKEN)
-        return { ...res, [key]: new BigNumber(balances[key]).shiftedBy(-AUSD_DECIMALS) };
+      for (const token of Object.values(TOKEN_INFO)) {
+        if (key === token.key)
+          return { ...res, [key]: new BigNumber(balances[key]).shiftedBy(-token.decimals) };
+      }
       return { ...res, [key]: new BigNumber(balances[key]).shiftedBy(-DEFAULT_DECIMALS) };
     }, {});
   };
