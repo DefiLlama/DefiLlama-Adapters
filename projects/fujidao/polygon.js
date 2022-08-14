efea134ec1e25ae04c9eb1d659a0d7e05e874932
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-08-14 08:08:44 -0400

    fix(fujidao): fix address and added export params per latest DefiLlama docs

--- projects/fujidao/polygon.js
+++ projects/fujidao/polygon.js
@@ -1,22 +1,22 @@
 const polygonContracts = {
   matic: [{
     name: "PolygonF1155Core_VaultsMATIC",
-    address: "0xB4E2eC87f8E6E166929A900Ed433c4589d721D70",
+    address: "0x03BD587Fe413D59A20F32Fc75f31bDE1dD1CD6c9",
     ids: [0, 2],
   }],
   wbtc: [{
     name: "PolygonF1155Core_VaultsWBTC",
-    address: "0xB4E2eC87f8E6E166929A900Ed433c4589d721D70",
+    address: "0x03BD587Fe413D59A20F32Fc75f31bDE1dD1CD6c9",
     ids: [4, 6],
   }],
   weth: [{
     name: "PolygonF1155Core_VaultsWETH",
-    address: "0xB4E2eC87f8E6E166929A900Ed433c4589d721D70",
+    address: "0x03BD587Fe413D59A20F32Fc75f31bDE1dD1CD6c9",
     ids: [8, 10],
   }],
   usdc: [{
     name: "PolygonF1155Core_VaultsUSDC",
-    address: "0xB4E2eC87f8E6E166929A900Ed433c4589d721D70",
+    address: "0x03BD587Fe413D59A20F32Fc75f31bDE1dD1CD6c9",
     ids: [16, 18],
   }],
 };
