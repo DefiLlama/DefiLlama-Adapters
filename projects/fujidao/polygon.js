5969cea2847fd65e198c7fdd89442b8de4fd5af1
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-08-14 07:26:17 -0400

    feat(fujidao): added polygon and refactor

---
+++ projects/fujidao/polygon.js
@@ -0,0 +1,26 @@
+const polygonContracts = {
+  matic: [{
+    name: "PolygonF1155Core_VaultsMATIC",
+    address: "0xB4E2eC87f8E6E166929A900Ed433c4589d721D70",
+    ids: [0, 2],
+  }],
+  wbtc: [{
+    name: "PolygonF1155Core_VaultsWBTC",
+    address: "0xB4E2eC87f8E6E166929A900Ed433c4589d721D70",
+    ids: [4, 6],
+  }],
+  weth: [{
+    name: "PolygonF1155Core_VaultsWETH",
+    address: "0xB4E2eC87f8E6E166929A900Ed433c4589d721D70",
+    ids: [8, 10],
+  }],
+  usdc: [{
+    name: "PolygonF1155Core_VaultsUSDC",
+    address: "0xB4E2eC87f8E6E166929A900Ed433c4589d721D70",
+    ids: [16, 18],
+  }],
+};
+
+module.exports = {
+  polygonContracts,
+};
