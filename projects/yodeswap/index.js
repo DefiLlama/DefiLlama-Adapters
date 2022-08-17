e830c47d3dc07668eff3a6fea85041c051a42b57
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-17 19:28:38 +0530

    Update index.js

--- projects/yodeswap/index.js
+++ projects/yodeswap/index.js
@@ -1,18 +1,23 @@
-const { getUniTVL } = require('../helper/unknownTokens')
-const { staking } = require('../helper/staking');
-const sdk = require('@defillama/sdk');
-const yode = "0x6FC4563460d5f45932C473334d5c1C5B4aEA0E01"
-const xyode = "0x707Ee40734454162b7720B665Fa2aA0f7c2C6983"
+const { getUniTVL, staking, } = require('../helper/unknownTokens')
+
+const chain = 'dogechain'
+const YodeDEX = '0x6FC4563460d5f45932C473334d5c1C5B4aEA0E01'
+const coreAssets = ['0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101']
+const lps = ['0x2Dd53abcFEBE71a9Cd5dFF4f44458E14707F9280']
+
 
 module.exports = {
   misrepresentedTokens: true,
-  doublecounted: false,
   dogechain: {
     tvl: getUniTVL({
-      chain: 'dogechain',
-      coreAssets: ['0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101', '0x6FC4563460d5f45932C473334d5c1C5B4aEA0E01', '0x765277EebeCA2e31912C9946eAe1021199B39C61', '0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D'],
+      chain, coreAssets,
       factory: '0xAaA04462e35f3e40D798331657cA015169e005d7',
     }),
-    staking: staking(xyode, yode, "dogechain")
+    staking: staking({
+      chain, coreAssets,
+      owner: '0x707Ee40734454162b7720B665Fa2aA0f7c2C6983',
+      tokens: [YodeDEX],
+      lps,
+     })
   }
 }
