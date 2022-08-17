724313f97c325fadf15c3eaf2f3bb677cb47ef23
sergocap <systemofadown.2013@yandex.ru>
2022-08-17 20:06:45 +0400

    update aliumswap index.js
    added new shp address

--- projects/aliumswap/index.js
+++ projects/aliumswap/index.js
@@ -2,7 +2,8 @@ const sdk = require("@defillama/sdk");
 const { calculateUniTvl } = require("../helper/calculateUniTvl.js");
 
 const ALMToken = "0x7C38870e93A1f959cB6c533eB10bBc3e438AaC11";
-const stakingPool = "0x4f388167F8B52F89C87A4E46706b9C1408F2c137"; // Strong Holder Pool
+const stakingPool0 = "0x95CDf618b6aF0ec1812290A777955D3609B0508d"; // Strong Holder Pool
+const stakingPool1 = "0x4f388167F8B52F89C87A4E46706b9C1408F2c137"; // Old Strong Holder Pool
 
 const bscFactory = "0xbEAC7e750728e865A3cb39D5ED6E3A3044ae4B98";
 
@@ -20,14 +21,22 @@ async function bscTvl(timestamp, block, chainBlocks) {
 async function bscStaking(timestamp, block, chainBlocks) {
   let balances = {};
 
-  let { output: balance } = await sdk.api.erc20.balanceOf({
+  let { output: balance0 } = await sdk.api.erc20.balanceOf({
     target: ALMToken,
-    owner: stakingPool,
+    owner: stakingPool0,
+    block: chainBlocks.bsc,
+    chain: "bsc",
+  });
+  
+  let { output: balance1 } = await sdk.api.erc20.balanceOf({
+    target: ALMToken,
+    owner: stakingPool1,
     block: chainBlocks.bsc,
     chain: "bsc",
   });
 
-  sdk.util.sumSingleBalance(balances, `bsc:${ALMToken}`, balance);
+  sdk.util.sumSingleBalance(balances, `bsc:${ALMToken}`, balance0);
+  sdk.util.sumSingleBalance(balances, `bsc:${ALMToken}`, balance1);
 
   return balances;
 }
