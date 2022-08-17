dff42ad52114eaa17c39c2850da3d60612305cb2
strobie <0xstrobe@protonmail.com>
2022-08-17 12:09:34 +0200

    add contracts

--- liquidations/maker/index.ts
+++ liquidations/maker/index.ts
@@ -12,14 +12,26 @@ function toHex(str: string) {
     .join("");
 }
 
+// all maker contracts: https://chainlog.makerdao.com/api/mainnet/active.json
+
+const cdpManager = new ethers.Contract(
+  "0x5ef30b9986345249bc32d8928B7ee64DE9435E39",
+  [
+    "function cdpi() external view returns (uint256)",
+    "function ilks(uint256) external view returns (bytes32)",
+    "function owns(uint256) external view returns (address)",
+    "function urns(uint256) external view returns (address)",
+  ],
+  providers.ethereum
+);
+
+const ilkRegistry = new ethers.Contract(
+  "0x5a464C28D19848f44199D003BeF5ecc87d090F87",
+  ["function gem(bytes32) view returns (address)"],
+  providers.ethereum
+);
+
 const positions = async (): Promise<Liq[]> => {
-  // all maker contracts: https://chainlog.makerdao.com/api/mainnet/active.json
-  const ilkRegistry = new ethers.Contract(
-    "0x5a464C28D19848f44199D003BeF5ecc87d090F87",
-    ["function gem(bytes32) view returns (address)"],
-    providers.ethereum
-  );
-  let ilks = new Set<string>();
   return [];
 };
 
