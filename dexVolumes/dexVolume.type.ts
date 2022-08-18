dd52afee9072a05659b46bc7c896550b101ca90a
0xtawa <0xtawanofi@gmail.com>
2022-08-18 18:41:36 +0200

    Improve types

--- dexVolumes/dexVolume.type.ts
+++ dexVolumes/dexVolume.type.ts
@@ -15,7 +15,7 @@ export type Fetch = (
 ) => Promise<FetchResult>;
 
 export type VolumeAdapter = {
-  [x: string]: {
+  [chain: string]: {
     start: () => Promise<number>
     fetch: Fetch;
     runAtCurrTime?: boolean;
@@ -24,7 +24,7 @@ export type VolumeAdapter = {
 };
 
 export type BreakdownAdapter = {
-  [x: string]: VolumeAdapter;
+  [version: string]: VolumeAdapter;
 };
 
 export type DexVolumeAdapter = {
