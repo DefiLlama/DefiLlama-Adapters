ae7e6ff2e601a240c76113fa9025ff268fabdf9d
Michalis Kargakis <kargakis@protonmail.com>
2022-08-18 19:59:04 +0200

    Track almost all JPEG vaults
    Track all JPEG vaults for which a Chainlink
    oracle exists (CryptoPunks, BAYC, MAYC).

---
+++ projects/jpeg-d/helper/abis.js
@@ -0,0 +1,44 @@
+const VAULT_ABI = [{
+    "inputs": [],
+    "name": "totalPositions",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },{
+    "inputs": [],
+    "name": "floorOracle",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+}];
+
+const PRICEORACLE_ABI = [{
+    "inputs": [],
+    "name": "latestAnswer",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+}];
+
+module.exports = {
+    VAULT_ABI,
+    PRICEORACLE_ABI
+};
