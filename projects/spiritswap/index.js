0b38f76aa4e7619a3b7cbe94b3d144d20a4ad86a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-19 15:07:34 +0200

    Alpha-homora: add optimism

--- projects/spiritswap/index.js
+++ projects/spiritswap/index.js
@@ -12,8 +12,8 @@ const unitroller_fantom = "0x892701d128d63c9856A9Eb5d967982F78FD3F2AE"
 const factory = '0xEF45d134b73241eDa7703fa787148D9C9F4950b0'
 const factory2 = '0x9d3591719038752db0c8bEEe2040FfcC3B2c6B9c'
 const chain = 'fantom'
-const ammTvl = getUniTVL({ chain, factory, useDefaultCoreAssets: true, })
-const ammTvl2 = getUniTVL({ chain, factory: factory2, useDefaultCoreAssets: true, })
+const ammTvl = getUniTVL({ chain, factory, useDefaultCoreAssets: false, })
+const ammTvl2 = getUniTVL({ chain, factory: factory2, useDefaultCoreAssets: false, })
 const olalending = usdCompoundExports(unitroller_fantom, "fantom", "0xed8F2C964b47D4d607a429D4eeA972B186E6f111", abis)
 
 module.exports = {
