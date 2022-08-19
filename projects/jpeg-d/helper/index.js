9746aa5d0b45f1d3ebe8b6cc80fc5ac0873ac635
Michalis Kargakis <kargakis@protonmail.com>
2022-08-19 23:11:45 +0200

    Add support for EtherRocks in JPEG-d

--- projects/jpeg-d/helper/index.js
+++ projects/jpeg-d/helper/index.js
@@ -1,4 +1,5 @@
 const sdk = require("@defillama/sdk");
+const { ethers } = require("ethers");
 const abi = require("./abis");
 
 const WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
@@ -7,20 +8,22 @@ const ETHER_ROCKS_VAULT = "0x6837a113aa7393ffbd5f7464e7313593cd2dd560";
 const BAYC_VAULT = "0x271c7603aaf2bd8f68e8ca60f4a4f22c4920259f";
 const MAYC_VAULT = "0x7b179f9bfbe50cfa401c1cdde3cb2c339c6635f3";
 
-const NFT_ARRAY = [
+const NFT_CHAINLINKED_ARRAY = [
     CRYPTO_PUNK_VAULT,
-    // No Chainlink oracle for EtherRocks
-    // ETHER_ROCKS_VAULT,
     BAYC_VAULT,
     MAYC_VAULT
+];
+
+const NFT_DAO_SET_ARRAY = [
+    ETHER_ROCKS_VAULT
 ];
 
 
 async function getTVL(balances, chain, timestamp, chainBlocks) {
     // Fetch positions and oracles from vaults
-    const [totalPositions, floorOracles] = await Promise.all([
+    const [totalChainlinkedPositions, floorOracles] = await Promise.all([
         sdk.api.abi.multiCall({
-            calls: NFT_ARRAY.map((address) => ({
+            calls: NFT_CHAINLINKED_ARRAY.map((address) => ({
                 target: address,
             })),
             abi: abi.VAULT_ABI.find(
@@ -30,7 +33,7 @@ async function getTVL(balances, chain, timestamp, chainBlocks) {
             chain,
         }),
         sdk.api.abi.multiCall({
-            calls: NFT_ARRAY.map((address) => ({
+            calls: NFT_CHAINLINKED_ARRAY.map((address) => ({
                 target: address,
             })),
             abi: abi.VAULT_ABI.find(
@@ -56,8 +59,39 @@ async function getTVL(balances, chain, timestamp, chainBlocks) {
     ).output;
 
     let collateralValueETH = 0;
-    for(let i=0; i<totalPositions.output.length; i++){
+    for(let i=0; i<totalChainlinkedPositions.output.length; i++){
         const floorPrice = floorPrices[i].output;
+        const position = totalChainlinkedPositions.output[i].output;
+        collateralValueETH += position * floorPrice;
+    }
+
+    // Fetch DAO-set prices
+    const [totalPositions, daoPrices] = await Promise.all([
+        sdk.api.abi.multiCall({
+            calls: NFT_DAO_SET_ARRAY.map((address) => ({
+                target: address,
+            })),
+            abi: abi.VAULT_ABI.find(
+                (a) => a.name === "totalPositions"
+            ),
+            block: chainBlocks[chain],
+            chain,
+        }),
+        sdk.api.abi.multiCall({
+            calls: NFT_DAO_SET_ARRAY.map((address) => ({
+                target: address,
+                params: [ethers.utils.formatBytes32String("")],
+            })),
+            abi: abi.VAULT_ABI.find(
+                (a) => a.name === "nftTypeValueETH"
+            ),
+            block: chainBlocks[chain],
+            chain,
+        })
+    ]);
+
+    for(let i=0; i<totalPositions.output.length; i++){
+        const floorPrice = daoPrices.output[i].output;
         const position = totalPositions.output[i].output;
         collateralValueETH += position * floorPrice;
     }
