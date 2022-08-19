36182d66b8ecb864933e1f551c4e1537889947f5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-19 21:50:38 +0530

    Update index.js

--- projects/KibbleSwap/index.js
+++ projects/KibbleSwap/index.js
@@ -2,19 +2,21 @@ const { getUniTVL, staking, } = require('../helper/unknownTokens')
 
 const chain = 'dogechain'
 const KIB = '0x1e1026ba0810e6391b0F86AFa8A9305c12713B66'
-const coreAssets = ['0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101']
-const lps = ['0x1709b8076F9ecb6b30567A6980754A9E5c33F837']
+const lps = ['0xC1C10b8BeeC82E840990A2c60A54ccdB39b2153F']
 
 
 module.exports = {
   misrepresentedTokens: true,
   dogechain: {
     tvl: getUniTVL({
-      chain, coreAssets,
+      chain, useDefaultCoreAssets: true,
       factory: '0xF4bc79D32A7dEfd87c8A9C100FD83206bbF19Af5',
+      blacklist: [
+        '0xe52CE89E16c4C596D30D111f3b5c137da412dEb8',
+      ]
     }),
     staking: staking({
-      chain, coreAssets,
+      chain, useDefaultCoreAssets: true,
       owner: '0x8ffBD442F246964A0d2E87C9b2551095bdA6EEb3',
       tokens: [KIB],
       lps,
