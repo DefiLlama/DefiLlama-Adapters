1854105303d9443cfd4b5c1d313fd477787a2b2b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-19 22:06:10 +0200

    fix dogmoney

--- projects/dogmoney/index.js
+++ projects/dogmoney/index.js
@@ -1,24 +1,17 @@
-const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
-const { staking } = require('../helper/staking')
+const { getUniTVL, staking } = require('../helper/unknownTokens')
 
 const xDOGMONEY = "0xC5c70fA7A518bE9229eB0Dc84e70a91683694562";
 const DOGMONEY = "0x93C8a00416dD8AB9701fa15CA120160172039851";
-const USDC = "0x765277EebeCA2e31912C9946eAe1021199B39C61";
-const WWDOGE = "0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101";
 const FACTORY = "0xaF85e6eD0Da6f7F5F86F2f5A7d595B1b0F35706C";
 
 module.exports = {
   misrepresentedTokens: true,
-  methodology:
-    "Factory address (0xaF85e6eD0Da6f7F5F86F2f5A7d595B1b0F35706C) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
-    dogechain: {
-    tvl: calculateUsdUniTvl(
-      FACTORY,
-      "dogechain",
-      WWDOGE,
-      [DOGMONEY, USDC],
-      "dogecoin"
-    ),
-    staking: staking(xDOGMONEY, DOGMONEY, 'dogechain', 'dogecoin', 18)
-  },
-};
+  dogechain: {
+    tvl: getUniTVL({
+      chain: 'dogechain',
+      useDefaultCoreAssets: true,
+      factory: FACTORY,
+    }),
+    staking: staking({ owner: xDOGMONEY, tokens: [ DOGMONEY ], chain: 'dogechain', lps: ['0x9ab710cd0bfbee60e14115d19c76213c4d4b1687'], useDefaultCoreAssets: true, })
+  }
+}
