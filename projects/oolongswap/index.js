5b2960ecfceb157f436a21351f5899233ec72a3a
p7m <perpetuum7@protonmail.com>
2022-08-19 14:31:02 +0200

    add oolongswap hallmark

--- projects/oolongswap/index.js
+++ projects/oolongswap/index.js
@@ -1,7 +1,12 @@
-const {getChainTvl} = require('../helper/getUniSubgraphTvl')
+const { getChainTvl } = require("../helper/getUniSubgraphTvl");
 
-module.exports={
-    boba:{
-        tvl: getChainTvl({boba: "https://api.thegraph.com/subgraphs/name/oolongswap/oolongswap-mainnet"})("boba")
-    }
-}
+module.exports = {
+  boba: {
+    tvl: getChainTvl({
+      boba: "https://api.thegraph.com/subgraphs/name/oolongswap/oolongswap-mainnet",
+    })("boba"),
+  },
+  hallmarks: [
+    [1658312537, "Alameda Research exits"],
+  ],
+};
