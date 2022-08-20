2a89cb7d08b51d543cad96f913d1de70f76947a1
PolyPup <polypup.farm@gmail.com>
2022-08-20 04:55:34 -0400

    add dogepup

---
+++ projects/dogepup/index.js
@@ -0,0 +1,9 @@
+const { masterChefExports } = require("../helper/masterchef");
+
+const token = "0x1b15b9446b9f632a78396a1680DAaE17f74Ce8d9";
+const masterchef = "0xc6dEcf90D8171B0E17f367C9f2fA4560C73845da";
+const chain = "dogechain";
+
+module.exports = {
+  ...masterChefExports(masterchef, chain, token),
+};
