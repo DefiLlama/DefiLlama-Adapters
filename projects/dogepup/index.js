b7e7edb07e479aea269edf822ed5eb8976e2dca1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-20 17:46:11 +0530

    Update index.js

--- projects/dogepup/index.js
+++ projects/dogepup/index.js
@@ -1,9 +1,7 @@
-const { masterChefExports } = require("../helper/masterchef");
+const { masterchefExports } = require("../helper/unknownTokens");
 
 const token = "0x1b15b9446b9f632a78396a1680DAaE17f74Ce8d9";
 const masterchef = "0xc6dEcf90D8171B0E17f367C9f2fA4560C73845da";
 const chain = "dogechain";
 
-module.exports = {
-  ...masterChefExports(masterchef, chain, token),
-};
+module.exports = masterchefExports({ chain, masterchef, nativeTokens: [token], useDefaultCoreAssets: true, })
