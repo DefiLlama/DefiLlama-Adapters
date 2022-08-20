25fe628bf8b27db2a0bca650745651b6cd758b59
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-20 19:59:13 +0200

    Canto: add forte/foodex

---
+++ projects/canto-forte/index.js
@@ -0,0 +1,12 @@
+const { getUniTVL } = require('../helper/unknownTokens')
+
+module.exports = {
+  misrepresentedTokens: true,
+  canto: {
+    tvl: getUniTVL({
+      chain: 'canto',
+      factory: '0x759e390D946249c63e0A1d8a810C5A577a591719',
+      useDefaultCoreAssets: true,
+    })
+  }
+}
