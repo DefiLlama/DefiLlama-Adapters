a432aa4b88c7c56725d39df6f6f86748b651d4df
fountainchen <fountainchen@yahoo.com>
2022-08-22 14:19:28 +0800

    [project]add StarSwap project of starcoin

---
+++ projects/starswap.js
@@ -0,0 +1,23 @@
+const utils = require('./helper/utils');
+
+async function farmingTvl() {
+    var totalTvl = await utils.fetchURL('https://swap-api.starcoin.org/main/v1/farmingTvlInUsd');
+    return totalTvl.data;
+}
+
+async function stakeTvl() {
+    const totalTvl = await utils.fetchURL('https://swap-api.starcoin.org/main/v1/syrupPoolTvlInUsd')
+    return totalTvl.data;
+}
+
+async function fetch() {
+    return (await farmingTvl()) + (await stakeTvl())
+}
+
+module.exports = {
+    methodology: `StarSwap's TVL is achieved by making a call to it's API: https://swap-api.starcoin.org/main/v1/farmingTvlInUsd (Farming) and https://swap-api.starcoin.org/main/v1/syrupPoolTvlInUsd (Stake).`,
+    doublecounted: false,
+    timetravel: false,
+    incentivized: true,
+    fetch,
+}
