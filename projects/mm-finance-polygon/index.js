dae22ea6aa06108aec930103ce551cc69a857365
CaVang <khietnt93@gmail.com>
2022-08-22 08:35:57 +0700

    feat: revert

--- projects/mm-finance-polygon/index.js
+++ projects/mm-finance-polygon/index.js
@@ -2,9 +2,11 @@ const sdk = require("@defillama/sdk");
 const { calculateUniTvl } = require("../helper/calculateUniTvl.js");
 const { staking, stakingPricedLP } = require('../helper/staking')
 
+
 const factory = '0x7cFB780010e9C861e03bCbC7AC12E013137D47A5'
 const mmfToken = '0x22a31bD4cB694433B6de19e0aCC2899E553e9481'
 const masterChef = '0xa2B417088D63400d211A4D5EB3C4C5363f834764'
+
 
 async function polyTvl(timestamp, block, chainBlocks) {
   return calculateUniTvl(
