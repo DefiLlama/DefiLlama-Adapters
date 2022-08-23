d47d00ef49799cea4c821801100e43a237086679
belbix <7453635@gmail.com>
2022-08-23 19:35:43 +0300

    add bsc network for tetu

--- projects/tetu/index.js
+++ projects/tetu/index.js
@@ -10,6 +10,10 @@ const FTM_BOOKKEEPER = "0x00379dD90b2A337C4652E286e4FBceadef940a21";
 const FTM_CONTRACT_READER = "0xa4EB2E1284D9E30fb656Fe6b34c1680Ef5d4cBFC";
 const FTM_USDC = "fantom:0x04068da6c83afcfa0e13ba15a6696662335d5b75";
 
+const BSC_BOOKKEEPER = "0x8A571137DA0d66c2528DA3A83F097fbA10D28540";
+const BSC_CONTRACT_READER = "0xE8210A2d1a7B56115a47B8C06a72356773f6838E";
+const BSC_USDC = "bsc:0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d";
+
 const EXCLUDED_PLATFORMS = {
   "12": true, // TETU_SWAP
   "29": true // TETU_SF
@@ -145,6 +149,71 @@ const fantomTvl = async (timestamp, ethBlock, chainBlocks) => {
   return balances;
 };
 
+const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
+  const block = chainBlocks["bsc"];
+  let balances = {};
+
+  const vaultLength = (
+      await sdk.api.abi.call({
+        abi: abi.vaultsLength,
+        target: BSC_BOOKKEEPER,
+        block,
+        chain: "bsc",
+      })
+  ).output;
+
+  for (let i = 0; i < vaultLength; i++) {
+    const vaultAddress = (
+        await sdk.api.abi.call({
+          block,
+          target: BSC_BOOKKEEPER,
+          params: i,
+          abi: abi.vaults,
+          chain: "bsc",
+        })
+    ).output;
+
+    const strategy = (
+        await sdk.api.abi.call({
+          block,
+          target: vaultAddress,
+          abi: abi.strategy,
+          chain: "bsc",
+        })
+    ).output;
+
+    const platform = (
+        await sdk.api.abi.call({
+          block,
+          target: strategy,
+          abi: abi.platform,
+          chain: "bsc",
+        })
+    ).output;
+
+    if (EXCLUDED_PLATFORMS[platform] === true) {
+      continue;
+    }
+
+    // Tetu is using our own PriceCalculator for determination prices of each assets
+    // ContractReader uses this solution for calculation vault TVL with USDC price
+    const usdcBal = (
+        await sdk.api.abi.call({
+          block,
+          target: BSC_CONTRACT_READER,
+          params: vaultAddress,
+          abi: abi.vaultTvlUsdc,
+          chain: "bsc",
+        })
+    ).output;
+    // vaultTvlUsdc returns 18 decimals but USDC has 6
+    sdk.util.sumSingleBalance(balances, BSC_USDC,
+        BigNumber(usdcBal).toFixed(0));
+  }
+
+  return balances;
+};
+
 module.exports = {
   start: 1628024400,  //Tue Aug 03 2021 21:00:00 GMT+0000
   misrepresentedTokens: true,
@@ -153,5 +222,8 @@ module.exports = {
   },
   fantom: {
     tvl: fantomTvl,
+  },
+  bsc: {
+    tvl: bscTvl,
   },
 };
