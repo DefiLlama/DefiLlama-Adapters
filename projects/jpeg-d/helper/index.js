aab92a140a0696451f82a4664445fe4fb4a02a1f
Michalis Kargakis <kargakis@protonmail.com>
2022-08-23 11:23:07 +0200

    Track JPEG'd treasury assets

--- projects/jpeg-d/helper/index.js
+++ projects/jpeg-d/helper/index.js
@@ -1,8 +1,19 @@
 const sdk = require("@defillama/sdk");
 const { ethers } = require("ethers");
 const abi = require("./abis");
+const { sumTokens } = require("../../helper/unwrapLPs");
 
+// Treasury
+const TREASURY = "0x51C2cEF9efa48e08557A361B52DB34061c025a1B";
+
+// Tokens
 const WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+const USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+const DAI = "0x6b175474e89094c44da98b954eedeac495271d0f";
+const USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+const TUSD = "0x0000000000085d4780B73119b644AE5ecd22b376";
+
+// NFT vaults
 const CRYPTO_PUNK_VAULT = "0xD636a2fC1C18A54dB4442c3249D5e620cf8fE98F";
 const ETHER_ROCKS_VAULT = "0x6837a113aa7393ffbd5f7464e7313593cd2dd560";
 const BAYC_VAULT = "0x271c7603aaf2bd8f68e8ca60f4a4f22c4920259f";
@@ -100,6 +111,23 @@ async function getTVL(balances, chain, timestamp, chainBlocks) {
     return balances;
 }
 
+async function getTreasury(balances, chain, timestamp, chainBlocks) {
+    await sumTokens(
+        balances,
+        [
+            [WETH, TREASURY],
+            [USDC, TREASURY],
+            [DAI, TREASURY],
+            [USDT, TREASURY],
+            [TUSD, TREASURY],
+        ],
+        chainBlocks[chain]
+    );
+
+    return balances;
+}
+
 module.exports = {
     getTVL,
+    getTreasury,
 };
