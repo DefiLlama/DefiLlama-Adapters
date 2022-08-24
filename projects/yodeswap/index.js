a83e17cbc1f06f426e57c20689345eed4c102a24
yodedex <yodedex@protonmail.com>
2022-08-24 11:22:17 +0200

    add multichain USDC

--- projects/yodeswap/index.js
+++ projects/yodeswap/index.js
@@ -2,7 +2,8 @@ const { getUniTVL, staking, } = require('../helper/unknownTokens')
 
 const chain = 'dogechain'
 const YodeDEX = '0x6FC4563460d5f45932C473334d5c1C5B4aEA0E01'
-const coreAssets = ['0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101']
+const USDC = '0x765277EebeCA2e31912C9946eAe1021199B39C61' //bridged Multichain USDC
+const coreAssets = ['0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101', USDC]
 const lps = ['0x2Dd53abcFEBE71a9Cd5dFF4f44458E14707F9280']
 
 
@@ -18,6 +19,6 @@ module.exports = {
       owner: '0x707Ee40734454162b7720B665Fa2aA0f7c2C6983',
       tokens: [YodeDEX],
       lps,
-     })
+    })
   }
 }
