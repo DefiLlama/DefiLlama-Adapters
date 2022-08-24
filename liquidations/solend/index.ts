88bedf5b4c681213182b99846d81f3055179c39d
strobie <0xstrobe@protonmail.com>
2022-08-24 16:42:53 +0200

    wip check

--- liquidations/solend/index.ts
+++ liquidations/solend/index.ts
@@ -5,6 +5,7 @@ import {
   SolendObligation,
   parseObligation,
 } from "@solendprotocol/solend-sdk";
+import { BigNumber } from "bignumber.js";
 
 const connection = new Connection("https://solend.genesysgo.net/", "confirmed");
 
@@ -12,36 +13,57 @@ const SOLEND_PROGRAM_ID = "So1endDq2YkqhipRh3WViPa8hdiSpxWy6z3Z6tMCpAo";
 const LENDING_MARKET_MAIN = "4UpD2fh7xH3VP9QQaXtsS1YY3bxzWhtfpks7FatyKvdY";
 const OBLIGATION_LEN = 1300;
 
-const func = async (): Promise<void> => {
+type MarketConfig = {
+  name: string;
+  symbol: string;
+  mintAddress: string;
+  reserveAddress: string;
+  decimals: number;
+  cTokenExchangeRate?: number;
+  loanToValueRatio?: number;
+  liquidationThreshold?: number; // safe to
+  assetPriceUSD?: number;
+};
+
+type ExtendedPosition = {
+  mintAddress: string;
+  amount: string;
+  token: MarketConfig;
+};
+
+const func = async () => {
   const market = await SolendMarket.initialize(connection);
   await market.loadReserves();
 
   const solendReserves = market.reserves;
 
-  const marketConfigs = market.reserves
-    .map((r) => ({ ...r.config, ltv: r.stats?.loanToValueRatio }))
+  const marketConfigs: MarketConfig[] = market.reserves
+    .map((r) => ({
+      ...r.config,
+      ...r.stats,
+    }))
     .map((c) => ({
       name: c.name,
       symbol: c.symbol,
-      collateral: c.mintAddress, // token address
+      loanToValueRatio: c.loanToValueRatio, // collateralFactor
+      assetPriceUSD: c.assetPriceUSD, // underlyingPriceUSD
+      mintAddress: c.mintAddress, // underlyingAddress
+      cTokenExchangeRate: c.cTokenExchangeRate, // exchangeRate
+      decimals: c.decimals, // underlyingDecimals
+      liquidationThreshold: c.liquidationThreshold,
       reserveAddress: c.address, // reserve address
-      decimals: c.decimals,
-      ltv: c.ltv,
     }));
 
   const marketConfigsMap: Map<
     string, // reserve address
-    {
-      name: string;
-      symbol: string;
-      collateral: string;
-      reserveAddress: string;
-      decimals: number;
-      ltv?: number;
-    }
+    MarketConfig
   > = new Map(marketConfigs.map((c) => [c.reserveAddress, c]));
 
-  // console.log(marketConfigsMap); // mintAddress issa token address
+  const tokenInfosMap: Map<string, MarketConfig> = new Map(
+    marketConfigs.map((c) => [c.mintAddress, c])
+  );
+
+  console.log(marketConfigsMap);
 
   const accounts = await connection.getProgramAccounts(
     new PublicKey(SOLEND_PROGRAM_ID),
@@ -76,21 +98,89 @@ const func = async (): Promise<void> => {
         borrows: so.borrows.map((b) => ({
           mintAddress: b.mintAddress,
           amount: b.amount.toString(),
+          token: tokenInfosMap.get(b.mintAddress),
         })),
         deposits: so.deposits.map((d) => ({
           mintAddress: d.mintAddress,
           amount: d.amount.toString(),
+          token: tokenInfosMap.get(d.mintAddress),
         })),
         ...so.obligationStats,
       };
     })
     .filter((o) => o.borrowUtilization < 1);
 
-  console.log(
-    obligations.find(
-      (o) => o.owner === "3oSE9CtGMQeAdtkm2U3ENhEpkFMfvrckJMA8QwVsuRbE"
-    )
+  const positions = obligations.map((o) => {
+    const { userTotalBorrow, userTotalDeposit } = o; // true decimals in USD, not wad etc
+
+    const tokens = new Map<
+      string,
+      // TODO: no need for array cuz u cant have 2 borrow positions ova same token
+      // will refactor later
+      { borrows: ExtendedPosition[]; deposits: ExtendedPosition[] }
+    >();
+
+    o.borrows.forEach((b) => {
+      const token = b.token!;
+      const tokenBorrows = tokens.get(token.mintAddress) || {
+        borrows: [],
+        deposits: [],
+      };
+      tokenBorrows.borrows.push({
+        mintAddress: b.mintAddress,
+        amount: b.amount,
+        token,
+      });
+      tokens.set(token.mintAddress, tokenBorrows);
+    });
+
+    o.deposits.forEach((d) => {
+      const token = d.token!;
+      const tokenDeposits = tokens.get(token.mintAddress) || {
+        borrows: [],
+        deposits: [],
+      };
+      tokenDeposits.deposits.push({
+        mintAddress: d.mintAddress,
+        amount: d.amount,
+        token,
+      });
+      tokens.set(token.mintAddress, tokenDeposits);
+    });
+
+    const aggregatedDebts = new Map<string, string>(); // mintAddress, amount
+    for (const token of tokens) {
+      const { borrows, deposits } = token[1];
+      const borrowsTotal = borrows.reduce(
+        (acc, b) => acc.plus(new BigNumber(b.amount).negated()),
+        new BigNumber(0)
+      );
+      const depositsTotal = deposits.reduce(
+        (acc, d) => acc.plus(new BigNumber(d.amount)),
+        new BigNumber(0)
+      );
+      const total = borrowsTotal.plus(depositsTotal);
+      aggregatedDebts.set(token[0], total.toString());
+    }
+
+    for (const aggregatedDebt of aggregatedDebts) {
+      const [mintAddress, amount] = aggregatedDebt;
+      const token = tokenInfosMap.get(mintAddress)!;
+      const { name, symbol, decimals } = token;
+      const amountInUSD = new BigNumber(amount).dividedBy(10 ** decimals);
+      console.log(
+        `${name} (${symbol}) - ${amountInUSD.toFixed(2)} USD - ${amount} wad`
+      );
+    }
+  });
+
+  const target = obligations.find(
+    (o) => o.owner === "3oSE9CtGMQeAdtkm2U3ENhEpkFMfvrckJMA8QwVsuRbE"
   );
+
+  const liquidables = target;
+
+  console.log(liquidables);
 };
 
 func();
