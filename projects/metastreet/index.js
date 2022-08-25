897c183ee53af889598a8fb24a8c983dcf321273
abitwhaleish <abitwhaleish@ante.xyz>
2022-08-25 11:35:36 -0700

    updated metastreet adapter

--- projects/metastreet/index.js
+++ projects/metastreet/index.js
@@ -1,25 +1,21 @@
 const sdk = require('@defillama/sdk');
 const abi = require("./abi.json");
-const BigNumber = require('bignumber.js')
+const BigNumber = require('bignumber.js');
+const { getTvl } = require('../helper/aave');
 
 // Constants
 const METASTREET_VAULT_REGISTRY = '0x07AB40311B992c8C75c4813388eDf95420e8f80A';
 
-// Gets the array of all MetaStreet Vaults registered in VaultRegistry
-async function getAllVaults(block) {
-    return (await sdk.api.abi.call({
+// Gets all MetaStreet Vaults registered in VaultRegistry and their
+// corresponding currency token each vault's balance is denoted in
+async function getAllVaultsAndTokens(block) {
+    const vaults = (await sdk.api.abi.call({
         target: METASTREET_VAULT_REGISTRY,
         abi: abi.getVaultList,
         block,
     })).output;
-}
 
-// Calculates the TVL or borrowed funds across all MetaStreet vaults
-async function getTVL(balances, block, isBorrowed) {
-    const vaults = await getAllVaults(block);
-
-    // Gets the currency token each vault's balance is denoted in
-    const vaultTokens = (await sdk.api.abi.multiCall({
+    const tokens = (await sdk.api.abi.multiCall({
         abi: abi.currencyToken,
         calls: vaults.map((vault) => ({
             target: vault
@@ -29,10 +25,33 @@ async function getTVL(balances, block, isBorrowed) {
         response.output
     ));
 
-    // Vault TVL = cash reserves = vault balance - admin fee - pending withdrawals
-    // TODO: Could rewrite using the ERC20 balance and subtracting admin fees/
-    // withdrawals to slightly reduce reliance on vault's internal state keeping
-    // TODO: Could incorporate NFT collateral value into TVL
+    return [vaults, tokens];
+}
+
+// Calculates the TVL across all MetaStreet vaults
+async function getTVL(balances, block, vaults, tokens) {
+    // TODO: Could incorporate NFT loan collateral value into TVL?
+    const vaultBalances = (await sdk.api.abi.multiCall({
+        calls: vaults.map((vault, index) => ({
+            target: tokens[index],
+            params: vault,
+        })),
+        abi: 'erc20:balanceOf',
+        block
+    })).output.map((response) => (
+        response.output
+    ));
+    
+    // Sum up token balances
+    for (let i = 0; i < vaults.length; i++) {
+        sdk.util.sumSingleBalance(balances, tokens[i], vaultBalances[i]);
+    };
+
+    return balances;
+}
+
+// Calculates loan balance across all MetaStreet vaults
+async function getBorrowed(balances, block, vaults, tokens) {
     const vaultBalances = (await sdk.api.abi.multiCall({
         abi: abi.balanceState,
         calls: vaults.map((vault) => ({
@@ -40,37 +59,38 @@ async function getTVL(balances, block, isBorrowed) {
         })),
         block
     })).output.map((response) => (
-        // If borrows, return loan balance; otherwise, return cash balance
-        isBorrowed ? response.output.totalLoanBalance : response.output.totalCashBalance
+        response.output.totalLoanBalance
     ));
     
     // Sum up token balances
     for (let i = 0; i < vaults.length; i++) {
-        sdk.util.sumSingleBalance(balances, vaultTokens[i], vaultBalances[i]);
+        sdk.util.sumSingleBalance(balances, tokens[i], vaultBalances[i]);
     };
 
     return balances;
 }
 
-async function borrowed(timestamp, block) {
-    const balances = {};
-    await getTVL(balances, block, true);
-    return balances;
-}
-
-async function tvl(timestamp, block) {
-    const balances = {};
-    await getTVL(balances, block, false);
-    return balances;
+function getMetaStreetTVL(isBorrowed) {
+    return async (block) => {
+        const balances = {}    
+        // Get all vaults and tokens
+        const [vaults, tokens] = await getAllVaultsAndTokens(block);
+        if (isBorrowed) {
+            await getBorrowed(balances, block, vaults, tokens);
+        } else {
+            await getTVL(balances, block, vaults, tokens);
+        }
+        return balances;
+    }
 }
 
 module.exports = {
     ethereum:{
-        tvl,
-        borrowed,
+        tvl: getMetaStreetTVL(false),
+        borrowed: getMetaStreetTVL(true),
     },
     timetravel: true,
     misrepresentedTokens: false,
-    methodology: 'TVL is calculated by getting the totalCashBalance of each vault, which counts currency tokens deposited into contracts for earning yield but not NFT collateral tokens. Borrowed tokens are also not counted towards TVL.',
+    methodology: 'TVL is calculated by getting the ERC20 balance of each vault, which counts tokens deposited into contracts for earning yield but not the value of any NFT loan note collateral the vault has purchased. Borrowed tokens are also not counted towards TVL.',
     start: 14878205
 };
