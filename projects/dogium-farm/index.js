15b17df36eed63fa2dcc612ebb44895e855ad9f7
pepperstepper <pepperstepper21@protonmail.com>
2022-08-28 00:57:37 +0100

    Switch masterchef helper utility

--- projects/dogium-farm/index.js
+++ projects/dogium-farm/index.js
@@ -1,6 +1,6 @@
-const { masterChefExports } = require("../helper/masterchef");
+const { masterchefExports } = require("../helper/unknownTokens");
 
-const dogiumToken  = "0x55BD2a3904C09547c3A5899704f1207eE61878Be";
+const dogiumToken  = "0x55bd2a3904c09547c3a5899704f1207ee61878be";
 const masterchef = "0x579BACCd9DdF3D9e652174c0714DBC0CD4700dF2";
 
-module.exports = masterChefExports(masterchef, "dogechain", dogiumToken);
+module.exports = masterchefExports({ chain: 'dogechain', masterchef, useDefaultCoreAssets: true, nativeTokens: [dogiumToken],  })
