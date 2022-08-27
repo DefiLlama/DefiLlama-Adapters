2006e39ae2de68d84886139719c98ee919459fb9
MusketPosition <musket.gun@position.exchange>
2022-08-27 13:14:11 +0700

    remove TVL with native coin in posichain

--- projects/positionexchange/index.js
+++ projects/positionexchange/index.js
@@ -10,46 +10,11 @@ const nftMiningProxy = "0xd8b6E267a0A46E13047056F105787dB9aFFb4b9a";
 
 const {bsc: chefExport} = (masterChefExports(masterchef, 'bsc', token, true))
 
-const client = axios.create({
-  baseURL: "https://api.posichain.org/",
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-});
-
-async function stakingPosiChain() {
-
-  let balances = {};
-  const response = await client.post('/', bodyParams("hmyv2_getAllValidatorInformation", 0));
-  if (response === null || response.data === null || response.data.result === null) {
-    return {output : 0}
-  }
-
-  let totalStaking = 0;
-  for (let validator of response.data.result) {
-    totalStaking += Number(validator["total-delegation"]);
-  }
-
-  sdk.util.sumSingleBalance(balances, `bsc:${token}`, totalStaking);
-  return balances;
-
-}
-const bodyParams = (method, params) => `{
-      "jsonrpc": "2.0",
-      "method": "${method}",
-      "params": ${params !== undefined ? `[${params}]` : '[]'},
-      "id": 1
-    }`;
 module.exports = {
-  methodology: "TVL in Binance Smart Chain is calculated by value locked in the MasterChef contract, cast NFT and treasury value is the POSI in the treasury contract. TVL in PosiChain is calculated by delegating into the validator node.",
+  methodology: "TVL in Binance Smart Chain is calculated by value locked in the MasterChef contract, cast NFT and treasury value is the POSI in the treasury contract.",
   bsc: {
     tvl: chefExport.tvl,
     pool2: chefExport.pool2,
     staking: sdk.util.sumChainTvls([chefExport.staking, staking(nftMiningProxy, token, 'bsc')])
-  },
-  posichain: {
-    staking: stakingPosiChain,
-    tvl : stakingPosiChain
   }
 };
