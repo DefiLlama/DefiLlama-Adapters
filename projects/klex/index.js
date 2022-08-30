a2846c5ea9219b0a7d319704068b44ebfe20d5e0
0xdoobybrothers <0xdoobybrothers@gmail.com>
2022-08-30 18:18:15 -0400

    updated to just be in index

--- projects/klex/index.js
+++ projects/klex/index.js
@@ -32,33 +32,7 @@ async function getTVL() {
     .reduce((acc, i) => acc + i, 0)
 }
 
-async function getTVLFromPools() {
-  var endpoint = `https://graph-prod.klex.finance/subgraphs/name/klex-staging-2-mainnet`
-  var graphQLClient = new GraphQLClient(endpoint)
-
-  var query = gql`
-  query get_tvl {
-    pools (
-      orderBy: totalLiquidity
-      orderDirection: desc
-    ) {
-      totalLiquidity
-      name
-      tokens
-    }
-  }
-  `;
-  const results = await graphQLClient.request(query)
-  results.pools.forEach(i => {
-    if (+i.totalLiquidity > 1e10) console.log('bad pool: ', i)
-  })
-  return results.pools
-    .map(i => +i.totalLiquidity)
-    .filter(i => i < 1e10)  // we filter out if totalLiquidity is higher than 10B as it is unlikely/error
-    .reduce((acc, i) => acc + i, 0)
-}
-
-async function klaytn(timestamp, ethBlock, chainBlocks) {
+async function klaytn() {
   return toUSDTBalances(await getTVL())
 }
 
