de4fa5dfd86ae200de456ac13a15a4b6a0c49850
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-30 23:44:22 +0530

    Update index.js

--- projects/helio-money/index.js
+++ projects/helio-money/index.js
@@ -1,14 +1,10 @@
-const retry = require('async-retry')
-const axios = require("axios");
-const BigNumber = require("bignumber.js");
-
-async function fetch() {
-  let price_feed = await retry(async bail => await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true'))
-  let response = await retry(async bail => await axios.get('https://api.bscscan.com/api?module=stats&action=tokenCsupply&contractaddress=0x563282106A5B0538f8673c787B3A16D3Cc1DbF1a'))
-  let tvl = new BigNumber(response.data.result).div(10 ** 18).toFixed(3);
-  return (tvl * price_feed.data.binancecoin.usd);
-}
+const { sumTokensExport, } = require('../helper/unknownTokens')
 
 module.exports = {
-  fetch
+  methodology: 'count the amount of ankr BNB in the vault', 
+  bsc: {
+    tvl: sumTokensExport({ chain: 'bsc', owner: '0x25b21472c073095bebC681001Cbf165f849eEe5E', tokens: [
+      '0xE85aFCcDaFBE7F2B096f268e31ccE3da8dA2990A', // ankrBNB
+    ] }),
+  }
 }
