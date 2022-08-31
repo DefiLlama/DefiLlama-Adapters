4c626609e8fdf3283cf6c87eddada7b58091d805
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-31 14:23:53 +0530

    Update index.js

--- projects/cantoswap/index.js
+++ projects/cantoswap/index.js
@@ -7,7 +7,6 @@ module.exports = {
       chain: "canto",
       factory: "0x6eE19E5e5F1018c26bDa107cB2CCd9dA461A698c",
       useDefaultCoreAssets: true,
-      blacklist: ["0x7264610a66eca758a8ce95cf11ff5741e1fd0455"],
     }),
   },
 };
