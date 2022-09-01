a95db66883d075a80f0a964838541a536770df8c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-09-01 11:47:47 +0200

    klex: count token balances

--- projects/klex/index.js
+++ projects/klex/index.js
@@ -1,46 +1,36 @@
-const sdk = require('@defillama/sdk');
-const BigNumber = require("bignumber.js");
-const { GraphQLClient, gql } = require('graphql-request')
-const { toUSDTBalances } = require('../helper/balances');
+const { request, gql } = require("graphql-request");
+const { sumTokens2 } = require('../helper/unwrapLPs');
 
-async function getTVL() {
-  var endpoint = `https://graph-prod.klex.finance/subgraphs/name/klex-staging-2-mainnet`
-  var graphQLClient = new GraphQLClient(endpoint)
+const V2_ADDRESS = '0xb519Cf56C63F013B0320E89e1004A8DE8139dA27'; // shared by all networks
 
-  var query = gql`
-  query get_tvl {
-    pools (
-      orderBy: totalLiquidity
-      orderDirection: desc
-    ){
-      id
-      name
-      owner
-      address
-      totalLiquidity
+function v2(chain) {
+  return async (time, ethBlock, chainBlocks) => {
+    const block = chainBlocks[chain]
+    const tokensApi = `https://graph-prod.klex.finance/subgraphs/name/klex-staging-2-mainnet`;
+    const POOL_TOKENS = gql`
+    {
+      pools {
+        name
+        tokensList
+        address
+      }
+    }`;
+
+    const v2Tokens = await request(tokensApi, POOL_TOKENS, {
+      block,
+    });
+    const tokens = [];
+    const blacklist = []
+    for (const { address, tokensList } of v2Tokens.pools) {
+      tokens.push(...tokensList)
+      blacklist.push(address)
     }
+    return sumTokens2({ chain, block, owner: V2_ADDRESS, tokens, blacklistedTokens: blacklist, })
   }
-  `;
-  const results = await graphQLClient.request(query)
-
-  results.pools.forEach(i => {
-    if (+i.totalLiquidity > 1e10) console.log('bad pool: ', i)
-  })
-  return results.pools
-    .map(i => +i.totalLiquidity)
-    .filter(i => i < 1e10)  // we filter out if liquidity is higher than 10B as it is unlikely/error
-    .reduce((acc, i) => acc + i, 0)
-}
-
-async function klaytn() {
-  return toUSDTBalances(await getTVL())
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: true,
-  methodology: `KLEX TVL is pulled from the KLEX subgraph and includes deposits made to KLEX liquidity pools.`,
   klaytn:{
-    tvl: klaytn
+    tvl: v2("klaytn")
   },
 }
