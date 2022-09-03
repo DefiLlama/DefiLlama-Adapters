246fb2a822ab345036e269e57d8c9b7e6797cbf4
Ojin Timo <89027644+OjinTimo@users.noreply.github.com>
2022-09-03 22:24:05 +0200

    Update index.js
    Updated to staked LP tokens

--- projects/puli/index.js
+++ projects/puli/index.js
@@ -1,7 +1,7 @@
 const sdk = require('@defillama/sdk');
 const { transformBscAddress } = require('../helper/portedTokens');
 const PULI_TOKEN_CONTRACT = '0xaef0a177c8c329cbc8508292bb7e06c00786bbfc';
-const PULI_TOKEN_LP_CONTRACT = '0x3ff5cbe338153063d8251d2b6a22a437ec09eef3';
+const PULI_TOKEN_STAKING_CONTRACT = '0x864d434308997e9648838d23f3eedf5d0fd17bea';
 
 async function tvl(timestamp, block, chainBlocks) {
     const balances = {};
@@ -11,7 +11,7 @@ async function tvl(timestamp, block, chainBlocks) {
         abi: 'erc20:balanceOf',
         chain: 'bsc',
         target: PULI_TOKEN_CONTRACT,
-        params: [PULI_TOKEN_LP_CONTRACT],
+        params: [PULI_TOKEN_STAKING_CONTRACT],
         block: chainBlocks['bsc'],
     })).output;
     
