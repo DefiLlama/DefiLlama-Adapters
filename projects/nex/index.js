4b02467416d274318ce927e892713dd146f4e33b
hukudome <hukudome@nex.market>
2022-09-05 18:46:58 +0900

    add Optimism

--- projects/nex/index.js
+++ projects/nex/index.js
@@ -1,30 +1,64 @@
-const sdk = require('@defillama/sdk');
-const axios = require('axios')
+const sdk = require("@defillama/sdk");
+const axios = require("axios");
 
-const { transformAuroraAddress } = require("../helper/portedTokens");
+const {
+  transformAuroraAddress,
+  transformOptimismAddress,
+} = require("../helper/portedTokens");
 
-const auroraApiEndpoint = 'https://stats.nex.market/api/tokens'
-const auroraVault = '0x5827094484b93989D1B75b12a57989f49e3b88B0';
+const auroraApiEndpoint = "https://stats.nex.market/api/tokens";
+const auroraVault = "0x5827094484b93989D1B75b12a57989f49e3b88B0";
+
+const optimismApiEndpoint = "https://stats-optimism.nex.market/api/tokens";
+const optimismVault = "0x5827094484b93989D1B75b12a57989f49e3b88B0";
 
 const auroraTVL = async (timestamp, block, chainBlocks) => {
-    const transform = await transformAuroraAddress();
-    const balances = {};
-    const allTokens = (await axios.get(auroraApiEndpoint)).data;
-    const tokenBalances = await sdk.api.abi.multiCall({
-        block: chainBlocks.aurora,
-        calls: allTokens.map(token => ({
-            target: token,
-            params: [auroraVault]
-        })),
-        abi: 'erc20:balanceOf',
-        chain: 'aurora',
-    });
-    sdk.util.sumMultiBalanceOf(balances, tokenBalances, true, transform);
-    return balances;
+  const transform = await transformAuroraAddress();
+  const balances = {};
+
+  const allTokens = (await axios.get(auroraApiEndpoint)).data;
+
+  const tokenBalances = await sdk.api.abi.multiCall({
+    block: chainBlocks.aurora,
+    calls: allTokens.map((token) => ({
+      target: token,
+      params: [auroraVault],
+    })),
+    abi: "erc20:balanceOf",
+    chain: "aurora",
+  });
+
+  sdk.util.sumMultiBalanceOf(balances, tokenBalances, true, transform);
+
+  return balances;
+};
+
+const optimismTVL = async (timestamp, block, chainBlocks) => {
+  const transform = await transformOptimismAddress();
+  const balances = {};
+
+  const allTokens = (await axios.get(optimismApiEndpoint)).data;
+
+  const tokenBalances = await sdk.api.abi.multiCall({
+    block: chainBlocks.optimism,
+    calls: allTokens.map((token) => ({
+      target: token,
+      params: [optimismVault],
+    })),
+    abi: "erc20:balanceOf",
+    chain: "optimism",
+  });
+
+  sdk.util.sumMultiBalanceOf(balances, tokenBalances, true, transform);
+
+  return balances;
 };
 
 module.exports = {
-  aurora:{
-    tvl: auroraTVL
-  }
+  aurora: {
+    tvl: auroraTVL,
+  },
+  optimism: {
+    tvl: optimismTVL,
+  },
 };
