eb10f15a0fc860a0f5d1dcc8e1e7fc6eb7fe640a
realdealshaman <realdealshaman11@gmail.com>
2022-09-05 22:55:48 +0100

    update tvl function

--- projects/versedex/index.js
+++ projects/versedex/index.js
@@ -1,4 +1,5 @@
 const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
+const { getUniTVL } = require('../helper/unknownTokens')
 
 const FLEXUSD = "0x7b2B3C5308ab5b2a1d9a94d20D35CCDf61e05b72";
 const WBCH = "0x3743eC0673453E5009310C727Ba4eaF7b3a1cc04";
@@ -13,21 +14,9 @@ module.exports = {
   methodology:
     "Factory address (0x16bc2B187D7C7255b647830C05a6283f2B9A3AF8) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
   ethereum: {
-    tvl: calculateUsdUniTvl(
-      ETH_FACTORY,
-      "ethereum",
-      WETH,
-      [DAI],
-      "weth"
-    ),
+    tvl: getUniTVL({ chain: 'ethereum', useDefaultCoreAssets: true, factory: ETH_FACTORY}),
   },
   smartbch: {
-    tvl: calculateUsdUniTvl(
-      SBCH_FACTORY,
-      "smartbch",
-      WBCH,
-      [FLEXUSD],
-      "bitcoin-cash"
-    ),
+    tvl: getUniTVL({ chain: 'smartbch', useDefaultCoreAssets: true, factory: SBCH_FACTORY})
   },
-};
+}
