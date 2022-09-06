43a9c00afda7de6f575093d265b69db0af487273
alexei <alexei@interlay.io>
2022-09-06 14:51:10 +0200

    Split Interlay chain into apps: btc wrapped and collateral locked. Added vote escrow

--- projects/interlay-btc/api.js
+++ projects/interlay-btc/api.js
@@ -20,27 +20,13 @@ async function tvl(){
     }
   }
 
-  // Fetch collateral locked to secure kBTC
-  const collaterals = await api.query.vaultRegistry.totalUserVaultCollateral.entries()
-
-  const collateralStorageKeys = {
-    polkadot : "0x8402aaa79721798ff725d48776181a44a42c51e2c6fef3b5d83341e8096ff7bfd6bfa4fbbbb302d0f4e13a890467318100000001",
-  }
-
-  for (const col of collaterals){
-    // check against DOT collateral storage key
-    if(col[0] == collateralStorageKeys.polkadot){
-      interlayTVL.polkadot = parseInt(col[1]/1e10)
-      continue;
-    }
-  }
   return interlayTVL
 }
 
 
 module.exports = {
   timetravel: false,
-  methodology: "Tracks BTC wrapped and assets locked as collateral by Vaults in Interlay's interBTC bridge protocol.",
+  methodology: "Tracks BTC wrapped in Interlay's interBTC bridge protocol.",
   interlay: { tvl }
 };
 
