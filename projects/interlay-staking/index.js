7128ce75c1fe3a804afafb28f6ce68bdf91ce5d3
0xngmi <0xngmi@protonmail.com>
2022-09-06 23:46:20 +0100

    export interlat staking under staking

--- projects/interlay-staking/index.js
+++ projects/interlay-staking/index.js
@@ -2,5 +2,8 @@ const { getExports } = require('../helper/heroku-api')
 
 module.exports = {
   timetravel: false,
-  ...getExports('interlay-staking', ['interlay']),
+  interlay:{
+    staking: getExports('interlay-staking', ['interlay']).interlay.tvl,
+    tvl: ()=>({}),
+  }
 }
