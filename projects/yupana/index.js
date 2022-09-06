51412391e5c47ce731eab300cbfe627166a8f7fc
Illia Likhoshva <ilyaliko64@gmail.com>
2022-09-06 14:08:25 +0300

    Refactor to use native helpers. Add helper to get bigmap by id from storage response

--- projects/yupana/index.js
+++ projects/yupana/index.js
@@ -1,93 +1,61 @@
-const { GraphQLClient, gql } = require("graphql-request");
-const retry = require('../helper/retry')
 const sdk = require("@defillama/sdk")
-const { usdtAddress } = require('../helper/balances')
-const { convertBalances, } = require('../helper/tezos')
+const {
+  convertBalances,
+  getTokenBalances,
+  getStorage,
+  getBigMapById,
+} = require("../helper/tezos");
 const { getFixBalances } = require('../helper/portedTokens')
-const { PromisePool } = require('@supercharge/promise-pool');
 const { default: BigNumber } = require("bignumber.js");
-const indexer = "https://mainnet-api.yupana.finance/v1/graphql";
-const graphQLClient = new GraphQLClient(indexer);
 const YUPANA_CORE = "KT1Rk86CX85DjBKmuyBhrCyNsHyudHVtASec";
-
-const tvl_query = gql`
-  {
-    tvl: asset {
-      contractAddress
-      isFa2
-      tokenId
-      tokens {
-        name
-        decimals
-      }
-      totalLiquid
-      totalBorrowed
-      reserves
-    }
-  }
-`;
-
-const borrows_query = `
-{
-  borrows: asset {
-    contractAddress
-    isFa2
-    tokenId
-    tokens {
-      name
-      decimals
-    }
-    totalBorrowed
-  }
-}`;
-
+const wTEZ = "KT1UpeXdK6AJbX58GJ92pLZVCucn2DR8Nu4b";
 
 async function tvl() {
-  const balances = {};
   const fixBalances = await getFixBalances("tezos");
-  const tvl_by_markets = await retry(
-    async (req) => await graphQLClient.request(tvl_query)
-  );
-  tvl_by_markets.tvl.forEach(entry => {
-    const underlying = entry.tokens[0];
-    const token_tvl = new BigNumber(entry.totalLiquid)
-      .plus(entry.reserves)
-      .div(10 ** 18);
-    if (underlying.name.includes("XTZ"))
-      sdk.util.sumSingleBalance(balances, "tezos", +token_tvl.div(10**6).toFixed(18));
-    else sdk.util.sumSingleBalance(
+  const balances = await getTokenBalances(YUPANA_CORE, true);
+  if (wTEZ in balances) {
+    // calculation of wrapped XTZ. Price always 1 to 1 to XTZ.
+    const wTez_balance = new BigNumber(balances[wTEZ]);
+    delete balances[wTEZ];
+    sdk.util.sumSingleBalance(
       balances,
-      entry.contractAddress + (entry.isFa2 ? "-" + entry.tokenId : ""),
-      +token_tvl.toFixed(18)
+      "tezos",
+      +wTez_balance.div(10 ** 6).toFixed(18)
     );
-  });
+  }
   return fixBalances(await convertBalances(balances));
 }
 
 async function borrowed() {
   const balances = {};
   const fixBalances = await getFixBalances("tezos");
-  const borrows_by_markets = await retry(
-    async (req) => await graphQLClient.request(borrows_query)
-  );
-  borrows_by_markets.borrows.forEach((entry) => {
-    const underlying = entry.tokens[0];
-    const token_borrows = new BigNumber(entry.totalBorrowed)
-      .div(10 ** 18);
-    if (underlying.name.includes("XTZ"))
-      sdk.util.sumSingleBalance(
-        balances,
-        "tezos",
-        +token_borrows.div(10 ** 6).toFixed(18)
-      );
-    else
-      sdk.util.sumSingleBalance(
-        balances,
-        entry.contractAddress + (entry.isFa2 ? "-" + entry.tokenId : ""),
-        +token_borrows.toFixed(18)
-      );
-  });
-
+  const storage = await getStorage(YUPANA_CORE);
+  const tokens_map = await getBigMapById(storage.storage.tokens);
+  for (const id in tokens_map) {
+    const token = tokens_map[id];
+    const token_borrows = new BigNumber(token.totalBorrowsF).div(10 ** 18);
+      if (token.mainToken.fA2 && token.mainToken.fA2.address === wTEZ)
+        // calculation of wrapped XTZ. Price always 1 to 1 to XTZ.
+        sdk.util.sumSingleBalance(
+          balances,
+          "tezos",
+          +token_borrows.div(10 ** 6).toFixed(18)
+        );
+      else {
+        let token_address;
+        if (token.mainToken.fA2)
+          token_address =
+            `${token.mainToken.fA2.address}` +
+            (token.mainToken.fA2.nat > 0 ? "-" + token.mainToken.fA2.nat : "");
+        else
+          token_address = `${token.mainToken.fA12}`;
+        sdk.util.sumSingleBalance(
+          balances,
+          token_address,
+          +token_borrows.toFixed(18)
+        );
+      }
+  }
   return fixBalances(await convertBalances(balances));
 }
 
