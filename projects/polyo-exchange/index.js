3ae66ac8503a3a5a01d955c9d8481bcffefd620d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-09-07 13:02:43 +0530

    code refactor

--- projects/polyo-exchange/index.js
+++ projects/polyo-exchange/index.js
@@ -1,4 +1,4 @@
-const sdk = require("@defillama/sdk");
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
 // polyo Asset Address
 const Asset_P01_BUSD = "0xC72e6f8041183EaD6f85839bD463F7aD0f14A40D";
@@ -12,28 +12,15 @@ const DAI = "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3";
 const USDC = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
 const USDT = "0x55d398326f99059fF775485246999027B3197955";
 
-async function balanceOf(owner, target, block) {
-  const chain = "bsc";
-  let decimals = (await sdk.api.erc20.decimals(target, chain)).output;
-  let balance = (
-    await sdk.api.erc20.balanceOf({
-      owner,
-      target,
-      block,
-      chain,
-    })
-  ).output;
-  return Number(balance) / 10 ** decimals;
-}
-
 async function tvl(timestamp, ethereumBlock, chainBlocks) {
+  const chain = 'bsc'
   const block = chainBlocks["bsc"];
-  let balances = {};
-  balances["binance-usd"] = await balanceOf(Asset_P01_BUSD, BUSD, block);
-  balances["dai"] = await balanceOf(Asset_P01_DAI, DAI, block);
-  balances["usd-coin"] = await balanceOf(Asset_P01_USDC, USDC, block);
-  balances["tether"] = await balanceOf(Asset_P01_USDT, USDT, block);
-  return balances;
+  return sumTokens2({ chain, block, tokensAndOwners: [
+    [BUSD, Asset_P01_BUSD],
+    [DAI, Asset_P01_DAI],
+    [USDC, Asset_P01_USDC],
+    [USDT, Asset_P01_USDT],
+  ]})
 }
 
 module.exports = {
