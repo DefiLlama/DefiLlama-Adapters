f433b18d960acf278103d7b55910e219189efdda
Joxes Work <91908708+Joxess@users.noreply.github.com>
2022-09-08 00:50:32 -0400

    Update index.js

--- projects/meanfinance/index.js
+++ projects/meanfinance/index.js
@@ -111,7 +111,7 @@ module.exports = {
     [1643602958, "V2 full launch"],
     [1646367758, "Deployment on Polygon"],
     [1650082958, "Protocol is paused because a non-critical vulnerability"],
-    [1653366158, "V2 Relaunch"]
+    [1653366158, "V2 Relaunch"],
     [1654057358, "OP launch bring more users into Optimism and benefit Mean"]
   ]
 };
