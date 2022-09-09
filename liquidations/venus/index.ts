baf21c096be37e29fbcd4a2549c21019c309b583
strobie <0xstrobe@protonmail.com>
2022-09-09 22:44:37 +0200

    wip

---
+++ liquidations/venus/index.ts
@@ -0,0 +1,140 @@
+import { gql } from "graphql-request";
+import { getPagedGql } from "../utils/gql";
+import BigNumber from "bignumber.js";
+import { Liq } from "../utils/binResults";
+import {
+  Account,
+  borrowBalanceUnderlying,
+  getMarkets,
+  getUnderlyingPrices,
+  supplyBalanceUnderlying,
+  totalBorrowValueInUsd,
+  totalCollateralValueInUsd,
+} from "../utils/compound-helpers";
+
+const subgraphUrl = "https://api.thegraph.com/subgraphs/name/venusprotocol/venus-subgraph";
+
+const accountsQuery = gql`
+  query accounts($lastId: ID) {
+    accounts(first: 1000, where: { hasBorrowed: true, id_gt: $lastId }) {
+      id
+      tokens {
+        vTokenBalance
+        accountBorrowIndex
+        storedBorrowBalance
+        market {
+          name
+          symbol
+
+          exchangeRate
+          collateralFactor
+          borrowIndex
+
+          underlyingAddress
+        }
+        enteredMarket
+      }
+      hasBorrowed
+    }
+    _meta {
+      block {
+        number
+      }
+    }
+  }
+`;
+
+const EXPLORER_BASE_URL = "https://bscscan.com/address/";
+
+const positions = async () => {
+  const accounts = (await getPagedGql(subgraphUrl, accountsQuery, "accounts")).map((x) => {
+    const clone = { ...x };
+    clone["tokens"]["cTokenBalance"] = clone["tokens"]["vTokenBalance"];
+    return clone;
+  }) as Account[];
+  const markets = await getMarkets(subgraphUrl);
+  const prices = await getUnderlyingPrices(markets, "bsc:");
+
+  // all positions across all users
+  const positions = accounts.flatMap((account) => {
+    const _totalBorrowValueInUsd = totalBorrowValueInUsd(account, prices, "bsc:");
+    const _totalCollateralValueInUsd = totalCollateralValueInUsd(account, prices, "bsc:");
+
+    const debts = account.tokens
+      .filter((token) => {
+        const _borrowBalanceUnderlying = borrowBalanceUnderlying(token);
+        const _supplyBalanceUnderlying = supplyBalanceUnderlying(token);
+        return !(_borrowBalanceUnderlying.eq(0) && _supplyBalanceUnderlying.eq(0));
+      })
+      .map((token) => {
+        const _borrowBalanceUnderlying = borrowBalanceUnderlying(token);
+        const _supplyBalanceUnderlying = supplyBalanceUnderlying(token);
+        const _price = prices["bsc:" + token.market.underlyingAddress];
+        if (!_price) {
+          console.log("no price for", "bsc:" + token.market.underlyingAddress);
+          return {
+            debt: new BigNumber(0),
+            price: 0,
+            token: "bsc:" + token.market.underlyingAddress,
+            totalBal: _supplyBalanceUnderlying,
+            decimals: 0,
+          };
+        }
+        const decimals = _price.decimals;
+        const price = _price.price;
+        const collateralFactor = Number(token.market.collateralFactor); // equivalent to liqThreshold in aave
+        let debt = _borrowBalanceUnderlying;
+        if (token.enteredMarket) {
+          const factoredSupply = _supplyBalanceUnderlying.times(collateralFactor);
+          debt = debt.minus(factoredSupply);
+        }
+        debt = debt.times(price);
+        return {
+          debt,
+          price,
+          token: "bsc:" + token.market.underlyingAddress,
+          totalBal: _supplyBalanceUnderlying,
+          decimals,
+        };
+      });
+
+    const liquidablePositions = debts
+      .filter(({ debt }) => debt.lt(0))
+      .map((pos) => {
+        const usdPosNetCollateral = pos.debt.negated();
+        const otherCollateral = _totalCollateralValueInUsd.minus(usdPosNetCollateral);
+        const diffDebt = _totalBorrowValueInUsd.minus(otherCollateral);
+        if (diffDebt.gt(0)) {
+          const amountCollateral = usdPosNetCollateral.div(pos.price);
+          const liqPrice = diffDebt.div(amountCollateral);
+          return {
+            owner: account.id,
+            liqPrice: Number(liqPrice.toFixed(6)),
+            collateral: pos.token,
+            collateralAmount: pos.totalBal.times(10 ** pos.decimals).toFixed(0),
+            extra: {
+              url: EXPLORER_BASE_URL + account.id,
+            },
+          } as Liq;
+        } else {
+          return {
+            owner: "",
+            liqPrice: 0,
+            collateral: "",
+            collateralAmount: "",
+          };
+        }
+      })
+      .filter((t) => !!t.owner);
+
+    return liquidablePositions;
+  });
+
+  return positions;
+};
+
+module.exports = {
+  bsc: {
+    liquidations: positions,
+  },
+};
