a76b1e319ecf3bad515bfe0a64fd981dec74c025
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-09-12 18:20:16 -0400

    feat(fujidao): added optimism contracts missing tests

---
+++ projects/fujidao/optimism.js
@@ -0,0 +1,16 @@
+const optimismContracts = {
+  weth: [{
+    name: "optimismF1155Core_VaultsWETH",
+    address: "0xaAb2AAfBFf7419Ff85181d3A846bA9045803dd67",
+    ids: [0],
+  }],
+  usdc: [{
+    name: "optimismF1155Core_VaultsUSDC",
+    address: "0x76E77921341e4414e60BdE4a3F56822c70e3ADf6",
+    ids: [2],
+  }],
+};
+
+module.exports = {
+  optimismContracts,
+};
