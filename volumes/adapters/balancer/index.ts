cf7bdca5be6877b84175a9a684594f23d0e56b25
0xngmi <0xngmi@protonmail.com>
2022-09-12 20:43:05 +0100

    fix balancer volumes

--- volumes/adapters/balancer/index.ts
+++ volumes/adapters/balancer/index.ts
@@ -1,38 +1,56 @@
-import { ChainEndpoints, SimpleVolumeAdapter } from "../../dexVolume.type";
+import { ChainEndpoints, BreakdownVolumeAdapter, Adapter } from "../../dexVolume.type";
 import { getChainVolume } from "../../helper/getUniSubgraphVolume";
 import customBackfill from "../../helper/customBackfill";
 import { CHAIN } from "../../helper/chains";
 import { Chain } from "@defillama/sdk/build/general";
 
 const endpoints: ChainEndpoints = {
-  [CHAIN.ETHEREUM]: "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer",
+  [CHAIN.ETHEREUM]: "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-v2",
   [CHAIN.POLYGON]:
     "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-polygon-v2",
   [CHAIN.ARBITRUM]:
     "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-arbitrum-v2",
 };
 
-const graphs = getChainVolume({
-  graphUrls: endpoints,
+const graphParams = {
   totalVolume: {
     factory: "balancers",
     field: "totalSwapVolume",
   },
   hasDailyVolume: false,
+}
+
+const graphs = getChainVolume({
+  graphUrls: endpoints,
+  ...graphParams
 });
 
-const adapter: SimpleVolumeAdapter = {
-  volume: Object.keys(endpoints).reduce((acc, chain) => {
-    return {
-      ...acc,
-      [chain]: {
-        fetch: graphs(chain as Chain),
-        start: async () => 0,
-        customBackfill: customBackfill(CHAIN.ETHEREUM, graphs),
+const v1graphs = getChainVolume({
+  graphUrls: {
+    [CHAIN.ETHEREUM]: "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer"
+  },
+  ...graphParams
+});
+
+const adapter: BreakdownVolumeAdapter = {
+  breakdown: {
+    v1: {
+      [CHAIN.ETHEREUM]: {
+        fetch: v1graphs(CHAIN.ETHEREUM),
+        start: async () => 1582761600,
+      },
+    },
+    v2: Object.keys(endpoints).reduce((acc, chain) => {
+      return {
+        ...acc,
+        [chain]: {
+          fetch: graphs(chain as Chain),
+          start: async () => 0,
+        }
       }
-    }
-  }, {} as SimpleVolumeAdapter['volume'])
-};
+    }, {} as Adapter)
+  }
+}
 
 export default adapter;
 
