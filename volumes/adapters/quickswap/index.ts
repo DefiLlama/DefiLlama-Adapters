68c39561125ebf801f7cadb66bbcdf41ed96bac5
0xtawa <0xtawanofi@gmail.com>
2022-09-12 19:08:49 +0200

    Fix quickswap volume adapter

--- volumes/adapters/quickswap/index.ts
+++ volumes/adapters/quickswap/index.ts
@@ -1,22 +1,34 @@
 import { SimpleVolumeAdapter } from "../../dexVolume.type";
-
-const { getChainVolume } = require("../../helper/getUniSubgraphVolume");
+import { CHAIN } from "../../helper/chains";
+import { getStartTimestamp } from "../../helper/getStartTimestamp";
+import { DEFAULT_TOTAL_VOLUME_FIELD, getChainVolume } from "../../helper/getUniSubgraphVolume";
 
 const endpoints = {
-  polygon: "https://api.thegraph.com/subgraphs/name/sameepsi/quickswap05",
+  [CHAIN.POLYGON]: "https://api.thegraph.com/subgraphs/name/sameepsi/quickswap-v3",
 };
 
 const graphs = getChainVolume({
-  graphUrls: {
-    polygon: endpoints.polygon,
+  graphUrls: endpoints,
+  totalVolume: {
+    factory: "factories",
+    field: DEFAULT_TOTAL_VOLUME_FIELD,
+  },
+  dailyVolume: {
+    factory: "algebraDayData",
+    field: 'volumeUSD',
   },
 });
 
 const adapter: SimpleVolumeAdapter = {
   volume: {
-    polygon: {
-      fetch: graphs("polygon"),
-      start: async () => 1602115200,
+    [CHAIN.POLYGON]: {
+      fetch: graphs(CHAIN.POLYGON),
+      start: getStartTimestamp({
+        endpoints: endpoints,
+        chain: CHAIN.POLYGON,
+        volumeField: 'volumeUSD',
+        dailyDataField: 'algebraDayDatas'
+      })
     },
   },
 };
