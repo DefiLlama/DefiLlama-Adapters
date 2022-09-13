3fa6805dd6abfac35b28d217d5acec1d607d2bd6
0xngmi <0xngmi@protonmail.com>
2022-09-13 21:41:08 +0100

    add biswap

---
+++ volumes/adapters/biswap/index.js
@@ -0,0 +1,5 @@
+import { univ2Adapter } from "../../helper/getUniSubgraphVolume";
+
+export default univ2Adapter({
+    "bsc": "https://api.thegraph.com/subgraphs/name/biswapcom/exchange5"
+}, "pancakeFactories");
