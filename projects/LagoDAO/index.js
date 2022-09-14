055f40952ecd8228d5e00d64b923ad2a1e65334e
matt-lago-finance <103604072+matt-lago-finance@users.noreply.github.com>
2022-09-14 19:56:54 -0400

    Update index.js

--- projects/LagoDAO/index.js
+++ projects/LagoDAO/index.js
@@ -8,11 +8,8 @@ let net = `https://api.chainweb.com/chainweb/0.0/mainnet01/chain/1/pact`
 
 async function tvl_kadena() {
     
-    let usd2supply;
     let kwUSDCsupply;
-    let wBTCSupply;
     let kadenaPrice;
-    let bitcoinPrice;
 
     await fetch('https://api.coingecko.com/api/v3/simple/price?ids=kadena&vs_currencies=usd', {
 
@@ -23,23 +20,6 @@ async function tvl_kadena() {
         })
         .catch(error => console.error(error))
 
-    await fetch('https://api.coingecko.com/api/v3/simple/price?ids=kadena&vs_currencies=btc', {
-
-        }).then(response => response.json()
-      )
-      .then(data => {
-        bitcoinPrice = data.kadena.btc
-      })
-      .catch(error => console.error(error))
-
-    let data = await fetchLocal({
-        pactCode: `(lago.USD2-wrapper.return-supply "lago.USD2") `,
-        meta: mkMeta("", "1" ,0.01,100000000, 28800, creationTime()),
-    }, net)
-    if (data.result.status === "success"){
-        usd2supply = parseFloat(data.result.data) / kadenaPrice
-    } 
-
     let data2 = await fetchLocal({
         pactCode: `(lago.bridge.get-supply "lago.kwUSDC") `,
         meta: mkMeta("", "1" ,0.01,100000000, 28800, creationTime()),
@@ -49,25 +29,17 @@ async function tvl_kadena() {
         kwUSDCsupply = parseFloat(data2.result.data.amount) / kadenaPrice
     } 
 
-    let data3 = await fetchLocal({
-        pactCode: `(lago.bridge.get-supply "lago.kwBTC") `,
-        meta: mkMeta("", "1" ,0.01,100000000, 28800, creationTime()),
-    }, net)
-
-    if (data3.result.status === "success"){
-        wBTCSupply = parseFloat(data3.result.data.amount) / bitcoinPrice
-    }
 
 
     return {
-        kadena: usd2supply + kwUSDCsupply + wBTCSupply
+        kadena: kwUSDCsupply
     }
 };
 
 module.exports = {
     timetravel: false,
     misrepresentedTokens: true,
-    methodology: "Tracks funds locked in Lago Finance",
+    methodology: "Tracks the kwUSDC collateral which backs the USD2 stablecoin on Kadena.",
     kadena: {
         tvl: tvl_kadena
     }
