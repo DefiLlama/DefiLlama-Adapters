8140b5a43a9b08150d8c0df631ea348b6e96c513
vrtnd <vrotend@proton.me>
2022-09-14 17:03:52 +0300

    Add Compound v3

--- projects/compound-onchain/index.js
+++ projects/compound-onchain/index.js
@@ -4,6 +4,7 @@ const abi = require('./abi.json');
 const v1abi = require('./v1Abi.json');
 const BigNumber = require('bignumber.js');
 const {lendingMarket} = require('../helper/methodologies')
+const {v3Abi} = require('./v3abi')
 
 // cache some data
 const markets = [
@@ -183,12 +184,77 @@ async function v2Tvl(balances, block, borrowed){
   });
   return balances;
 }
+const USDCV3 = "0xc3d688B66703497DAA19211EEdff47f25384cdc3";
 
-async function borrowed(timestamp, block){
+async function v3Tvl(balances, block, borrowed) {
+  const numMarkets = +(
+    await sdk.api.abi.call({
+      target: USDCV3,
+      block,
+      abi: v3Abi.find(({ name }) => name === "numAssets"),
+    })
+  ).output;
+
+  const markets = (
+    await sdk.api.abi.multiCall({
+      abi: v3Abi.find(({ name }) => name === "getAssetInfo"),
+      block,
+      calls: Array.from({ length: numMarkets }).map((_, i) => ({
+        target: USDCV3,
+        params: i,
+      })),
+    })
+  ).output.map(({ output }) => output.asset);
+
+  const collateral = (
+    await sdk.api.abi.multiCall({
+      abi: v3Abi.find(({ name }) => name === "totalsCollateral"),
+      block,
+      calls: markets.map((market) => ({
+        target: USDCV3,
+        params: market,
+      })),
+    })
+  ).output.map(({ output }) => output.totalSupplyAsset);
+
+  const borrows = (
+    await sdk.api.abi.call({
+      target: USDCV3,
+      block,
+      abi: v3Abi.find(({ name }) => name === "totalBorrow"),
+    })
+  ).output;
+  const borrowedToken = (
+    await sdk.api.abi.call({
+      target: USDCV3,
+      block,
+      abi: v3Abi.find(({ name }) => name === "baseToken"),
+    })
+  ).output;
+
+  if (borrowed) {
+    balances[borrowedToken] = BigNumber(balances[borrowedToken] || 0)
+      .plus(borrows)
+      .toFixed();
+  } else {
+    markets.forEach((market, i) => {
+      const reserves = collateral[i];
+
+      balances[market] = BigNumber(balances[market] || 0)
+        .plus(reserves)
+        .toFixed();
+    });
+  }
+
+  return balances;
+}
+
+async function borrowed(timestamp, block) {
   const balances = {};
-  await v1Tvl(balances, block, true)
-  await v2Tvl(balances, block, true)
-  return balances
+  await v1Tvl(balances, block, true);
+  await v2Tvl(balances, block, true);
+  await v3Tvl(balances, block, true);
+  return balances;
 }
 
 async function tvl(timestamp, block) {
@@ -201,23 +267,23 @@ async function tvl(timestamp, block) {
       target: market.underlying,
       params: v1Contract,
     })),
-    abi: 'erc20:balanceOf',
+    abi: "erc20:balanceOf",
   });
 
   sdk.util.sumMultiBalanceOf(balances, v1Locked);
 
-  await v2Tvl(balances, block, false)
+  await v2Tvl(balances, block, false);
+  await v3Tvl(balances, block, false);
+
   return balances;
 }
 
 module.exports = {
-  hallmarks: [
-    [1632873600,"Comptroller vulnerability exploit"]
-  ],
+  hallmarks: [[1632873600, "Comptroller vulnerability exploit"]],
   timetravel: true,
   ethereum: {
     tvl,
-    borrowed
+    borrowed,
   },
   methodology: `${lendingMarket}. TVL is calculated by getting the market addresses from comptroller and calling the getCash() on-chain method to get the amount of tokens locked in each of these addresses, then we get the price of each token from coingecko.`,
 };
