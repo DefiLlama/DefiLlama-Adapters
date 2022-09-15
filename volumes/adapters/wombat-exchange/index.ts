68a4ac3439ce7519ee44163241c2e2aa384d1764
0xngmi <0xngmi@protonmail.com>
2022-09-15 02:24:26 +0100

    fix wombat vol start

--- volumes/adapters/wombat-exchange/index.ts
+++ volumes/adapters/wombat-exchange/index.ts
@@ -5,9 +5,13 @@ const endpoints = {
   [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/wombat-exchange/wombat-exchange",
 };
 
-export default univ2Adapter(endpoints, {
+const adapter = univ2Adapter(endpoints, {
   factoriesName: "protocols",
   dayData: "protocolDayData",
   dailyVolume: "dailyTradeVolumeUSD",
   totalVolume: "totalTradeVolumeUSD"
 });
+
+adapter.volume.bsc.start = async()=>1650243600
+
+export default adapter
