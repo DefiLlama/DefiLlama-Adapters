8a646cace71597b133dd43ac7628be780fb214be
rainbowfirestudios <rainbowfirestudios@gmail.com>
2022-09-18 07:55:15 -0400

    Create index.ts

---
+++ volumes/adapters/radioshack/index.ts
@@ -0,0 +1,8 @@
+import { univ2Adapter } from "../../helper/getUniSubgraphVolume";
+
+export default univ2Adapter({
+    "bsc": "https://thegraph.com/hosted-service/subgraph/radioshackcreator/radioshack-bsc",
+    "polygon": "https://thegraph.com/hosted-service/subgraph/radioshackcreator/radioshack-polygon",
+    "ethereum": "https://thegraph.com/hosted-service/subgraph/shayha271097/mainnet-radioshack-swap",
+    "avax": "https://thegraph.com/hosted-service/subgraph/shayha271097/avax-radioshack-swap",
+}, {});
