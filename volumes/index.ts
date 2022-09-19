90faad9fde79cb46a3d2f70d5df6e60639b6fc9b
dtmkeng <phumaisut@gmail.com>
2022-09-20 00:48:55 +0700

    add mdex dexs

--- volumes/index.ts
+++ volumes/index.ts
@@ -40,6 +40,7 @@ import platypus from "./adapters/platypus";
 import wombatExchange from "./adapters/wombat-exchange";
 import wingriders from "./adapters/wingriders";
 import minswap from "./adapters/minswap";
+import mdex from "./adapters/mdex";
 
 export default {
   mooniswap,
@@ -83,6 +84,7 @@ export default {
   platypus,
   "wombat-exchange": wombatExchange,
   wingriders,
-  minswap
+  minswap,
+  mdex
   // lyra -> OPTIONS DEX, not enable for now
 };
