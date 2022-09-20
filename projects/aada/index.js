026eb6f45456dc441d176c0b98e9ff612e700871
Elvis Arabadjiyski <dreemsuncho@gmail.com>
2022-09-20 14:42:57 +0300

    APT-384 Update defillama repo

--- projects/aada/index.js
+++ projects/aada/index.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const { default: BigNumber } = require('bignumber.js');
-const { getAddressesUTXOs, getTxsMetadata } = require("../helper/cardano/blockfrost");
+const { getAddressesUTXOs } = require("../helper/cardano/blockfrost");
 
 async function staking() {
   const aadaLocked = (await axios.get("https://cardano-mainnet.blockfrost.io/api/v0/addresses/addr1wyvej5rmcrhfpcwrwmnqsjtwvf8gv3dn64vwy3xzekp95wqqhdkwa", {
@@ -9,12 +9,10 @@ async function staking() {
     }
   })).data.amount.find(token => token.unit === "8fef2d34078659493ce161a6c7fba4b56afefa8535296a5743f6958741414441").quantity;
 
-
   const topPrice = (await axios.get("https://orders.muesliswap.com/orderbook/?policy-id=8fef2d34078659493ce161a6c7fba4b56afefa8535296a5743f69587&tokenname=AADA")
   ).data.buy[0].price;
 
   const cardano = aadaLocked * topPrice;
-  // console.log({cardanoStaking: cardano})
   return { cardano };
 }
 
@@ -25,21 +23,44 @@ const scriptAdresses = [
   'addr1zxcjtxuc7mj8w6v9l3dfxvm30kxf78nzw387mqjqvszxr4mfjcnq9fczt4qkxgec2hz6x7f38vnj8xuxywk4x4qgzh9sp92046', //liquidation.hs -- Funds were liquidated. Borrower to claim
 ];
 const tvl = async () => {
-  let totalLovelaceLocked = 0;
-  const mapLovelaceUtxo = ({ amount }) => amount.filter(({ unit }) => unit === 'lovelace').map(({ quantity }) => quantity);
-  const sumUtxos = (a, b) => BigNumber.sum(a, b);
+  let tvlLocked = {};
+  const tokenPairsUrl = 'https://api-mainnet-prod.minswap.org/coinmarketcap/v2/pairs';
+  const res = await axios.get(tokenPairsUrl);
+  let pairs;
+  if (res.data) {
+    pairs = res.data;
+  }
+
   for (const scriptAddress of scriptAdresses) {
     const utxos = await getAddressesUTXOs(scriptAddress);
-    const addressTvl = utxos
-      .map(mapLovelaceUtxo)
-      .reduce(sumUtxos, 0);
-    totalLovelaceLocked = BigNumber.sum(totalLovelaceLocked, addressTvl).toNumber();
+    utxos.map(utxo => utxo.amount)
+      .reduce((a, b) => a.concat(b), [])
+      .forEach(amount => {
+        const { unit, quantity } = amount;
+        if (!tvlLocked[unit]) {
+          tvlLocked[unit] = {
+            value: 0,
+            valueLovelace: 0,
+          };
+        }
+        const value = BigNumber.sum(quantity, tvlLocked[unit].value);
+        tvlLocked[unit] = { value: value.toNumber(), };
+        const pair = pairs[`${unit}_lovelace`];
+        if (pair) {
+          if (!tvlLocked[unit].valueLovelace) {
+            tvlLocked[unit].valueLovelace = 0;
+          }
+          const valueLovelace = BigNumber.sum(new BigNumber(quantity).multipliedBy(pair.last_price), tvlLocked[unit].valueLovelace);
+          tvlLocked[unit].valueLovelace = valueLovelace.toNumber();
+        }
+      });
   }
+
+  const totalLovelaceLocked = Object.entries(tvlLocked).map(([id, values]) => id === 'lovelace' ? values.value : values.valueLovelace).reduce((a, b) => BigNumber.sum(a, b).toNumber(), 0);
+  console.log({ totalLovelaceLocked });
   const cardano = totalLovelaceLocked / 1e6;
-  // console.log({ cardano });
   return { cardano, };
 };
-
 
 module.exports = {
   misrepresentedTokens: true,
