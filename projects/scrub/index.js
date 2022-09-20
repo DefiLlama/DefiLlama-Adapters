51392c6f9d5dc8caade3129e9b9f86f06f2850d9
TheG <theg@gmail.com>
2022-09-21 00:41:44 +0100

    Update index.js

--- projects/scrub/index.js
+++ projects/scrub/index.js
@@ -3,7 +3,6 @@ const { unknownTombs } = require("../helper/unknownTokens")
 const token = "0x49fB98F9b4a3183Cd88e7a115144fdf00fa6fB95"
 const rewardPool = ["0x44B4a1e8f34Bb52ed39854aD218FF94D2D5b4800","0x14103f4Fc36daCeaCDE4c5313a2b1a462e00B1e8"]
 const lps = Object.values({
-    'LION-USDC-LP': '0xf2059ed015ec4ecc80f902d9fdbcd2a227bfe037',
     'TIGER-USDC-LP': '0xf6464c80448d6ec4deb7e8e5ec95b8eb768fbf69',
     'BEAR-WBTC-LP': '0x3d9e539fa44b970605658e25d18f816ce78c4007',
 })
