677eaa28f57b8b27b8a2fbb6a7983fd8f6577c4a
gaspare100 <gaspare.marchese.it@gmail.com>
2022-09-21 00:25:19 +0100

    Added single stake pool

--- projects/scrub/index.js
+++ projects/scrub/index.js
@@ -1,7 +1,7 @@
 const { unknownTombs } = require("../helper/unknownTokens")
 
 const token = "0x49fB98F9b4a3183Cd88e7a115144fdf00fa6fB95"
-const rewardPool = "0x44B4a1e8f34Bb52ed39854aD218FF94D2D5b4800"
+const rewardPool = ["0x44B4a1e8f34Bb52ed39854aD218FF94D2D5b4800","0x14103f4Fc36daCeaCDE4c5313a2b1a462e00B1e8"]
 const lps = Object.values({
     'LION-USDC-LP': '0xf2059ed015ec4ecc80f902d9fdbcd2a227bfe037',
     'TIGER-USDC-LP': '0xf6464c80448d6ec4deb7e8e5ec95b8eb768fbf69',
