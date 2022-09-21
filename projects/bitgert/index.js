e7efd4e074d25e0e851fd952ec9ca1137ff41f33
box25 <simon@klein-homepage.de>
2022-09-21 16:08:58 +0200

    added Bitgert Brise staking pseudo code

--- projects/bitgert/index.js
+++ projects/bitgert/index.js
@@ -3,6 +3,7 @@ const { staking } = require('../helper/staking')
 module.exports = {
   bsc: {
   tvl: () =>  ({}),
-    staking: staking('0xD578BF8Cc81A89619681c5969D99ea18A609C0C3', '0x8FFf93E810a2eDaaFc326eDEE51071DA9d398E83', 'bsc')
+    staking: staking('0xD578BF8Cc81A89619681c5969D99ea18A609C0C3', '0x8FFf93E810a2eDaaFc326eDEE51071DA9d398E83', 'bsc') + 
+    staking('0x8Ed91b2f3d9f6a5Ee426B4705F981090a7403795', '0x0000000000000000000000000000000000000000', 'bitgert')
   },
 };
