bb1b4972b2f2b39bacbdf5a99737126243354ab3
dtmkeng <phumaisut@gmail.com>
2022-09-23 00:56:00 +0700

    add mdex dexs (#3929)
    * add mdex dexs
    
    * add sunswap dexs
    
    * Add customBackfill function + improve helper
    
    * Remove runAtCurrTime flag
    
    * add customerbackfil
    
    Co-authored-by: 0xtawa <0xtawanofi@gmail.com>
    Co-authored-by: dtmkeng <dtmkeng@gmail.com>
    Co-authored-by: 0xtawa <106731946+0xtawa@users.noreply.github.com>

--- volumes/index.ts
+++ volumes/index.ts
@@ -40,6 +40,7 @@ import platypus from "./adapters/platypus";
 import wombatExchange from "./adapters/wombat-exchange";
 import wingriders from "./adapters/wingriders";
 import minswap from "./adapters/minswap";
+import mdex from "./adapters/mdex";
 import meshswap from "./adapters/meshswap";
 import vvsFinance from "./adapters/vvs-finance";
 import mmStableswapPolygon from "./adapters/mm-stableswap-polygon";
@@ -89,6 +90,7 @@ export default {
   "wombat-exchange": wombatExchange,
   wingriders,
   minswap,
+  mdex,
   meshswap,
   "vvs-finance": vvsFinance,
   "mm-stableswap-polygon": mmStableswapPolygon,
