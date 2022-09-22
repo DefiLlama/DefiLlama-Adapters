6131fd6faed9e8303f69646c564d2fe3e3abf740
Umar Salim <me@umarsalim.com>
2022-09-22 17:27:52 +0100

    feat: updated Flashstake TVL metrics

--- projects/flashstake/index.js
+++ projects/flashstake/index.js
@@ -1,28 +1,24 @@
-const { request, gql } = require("graphql-request");
+const retry = require('async-retry')
+const axios = require("axios");
+const BigNumber = require("bignumber.js");
 const { toUSDTBalances } = require("../helper/balances");
 
-const graphUrl =
-  "https://api.thegraph.com/subgraphs/name/blockzerohello/flash-stake-stats-v2-subgraph";
-const graphQuery = gql`
-  query get_tvl($block: Int) {
-    flashFactories(block: { number: $block }) {
-      totalVolumeUSD
-      totalLiquidityUSD
-    }
-  }
-`;
-
 async function tvl(timestamp, block) {
-  const { flashFactories } = await request(graphUrl, graphQuery, {
-    block,
-  });
-  const usdTvl = Number(flashFactories[0].totalLiquidityUSD);
-  return toUSDTBalances(usdTvl);
+  // Retrieve TVL Data
+  const endpoint = 'https://api.flashstake.io/protocol/tvl/' + block;
+  let response = await retry(async bail => await axios({
+    method: "GET",
+    url: endpoint,
+    timeout: 30000, // at most only wait 30 seconds
+  }));
+  return toUSDTBalances(new BigNumber(response.data['totalTVL']));
 }
 
 module.exports = {
-  misrepresentedTokens: true,
+  misrepresentedTokens: false,
   ethereum: {
     tvl,
   },
+  timetravel: true,
+  start: 15450000
 };
