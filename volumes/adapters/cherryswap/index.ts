8de1572689b747c2b1f0ca44ecf5c30c774dc342
0xtawa <106731946+0xtawa@users.noreply.github.com>
2022-09-22 19:36:16 +0200

    Add okexchain constant

--- volumes/adapters/cherryswap/index.ts
+++ volumes/adapters/cherryswap/index.ts
@@ -1,7 +1,8 @@
-import { univ2Adapter } from "../../helper/getUniSubgraphVolume";
+import { univ2Adapter } from "../../helper/chains";
+import { CHAIN } from "../../helper/getUniSubgraphVolume";
 
 const adapter = univ2Adapter({
-  "okexchain": "https://okinfo.cherryswap.net/subgraphs/name/cherryswap/cherrysubgraph"
+  [CHAIN.OKEXCHAIN]: "https://okinfo.cherryswap.net/subgraphs/name/cherryswap/cherrysubgraph"
 }, {
   factoriesName: "uniswapFactories",
   dayData: "uniswapDayData",
