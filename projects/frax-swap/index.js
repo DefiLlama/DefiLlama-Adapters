996aa2d529dcbc46acbf3409ab44c7007e8165eb
0xngmi <0xngmi@protonmail.com>
2022-09-23 22:02:11 +0100

    all fraxswap pools

--- projects/frax-swap/index.js
+++ projects/frax-swap/index.js
@@ -1,19 +1,34 @@
 const { getUniTVL } = require('../helper/unknownTokens')
+const sdk = require("@defillama/sdk")
+
+const fx = (chain, factory1, factory2) => ({
+  [chain]: {
+    tvl: factory2 === undefined ?
+      getUniTVL({
+        chain,
+        factory: factory1,
+      })
+      : sdk.util.sumChainTvls([
+        getUniTVL({
+          chain,
+          factory: factory1,
+        }),
+        getUniTVL({
+          chain,
+          factory: factory2,
+        }),
+      ])
+  },
+})
 
 module.exports = {
   misrepresentedTokens: true,
-  ethereum: {
-    tvl: getUniTVL({
-      chain: 'ethereum',
-      factory: '0xb076b06f669e682609fb4a8c6646d2619717be4b',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  moonbeam: {
-    tvl: getUniTVL({
-      chain: 'moonbeam',
-      factory: '0x5Ca135cB8527d76e932f34B5145575F9d8cbE08E',
-      useDefaultCoreAssets: true,
-    }),
-  },
+  ...fx("ethereum", "0xb076b06f669e682609fb4a8c6646d2619717be4b", "0x43ec799eadd63848443e2347c49f5f52e8fe0f6f"),
+  ...fx("arbitrum", "0x5Ca135cB8527d76e932f34B5145575F9d8cbE08E"),
+  ...fx("avax", "0x5Ca135cB8527d76e932f34B5145575F9d8cbE08E"),
+  ...fx("bsc", "0xa007a9716dba05289df85A90d0Fd9D39BEE808dE"),
+  ...fx("dogechain", "0x67b7DA7c0564c6aC080f0A6D9fB4675e52E6bF1d"),
+  ...fx("fantom", "0xF55C563148cA0c0F1626834ec1B8651844D76792"),
+  ...fx("moonbeam", "0x5Ca135cB8527d76e932f34B5145575F9d8cbE08E"),
+  ...fx("polygon", "0xc2544A32872A91F4A553b404C6950e89De901fdb"),
 };
