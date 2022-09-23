1429b7c2fd05a95c919f4af3b55b4237e1cb6117
twenty <twenty@twenty>
2022-09-23 16:50:13 +0800

    Add CTR Lp Tvl

--- projects/concentrator/index.js
+++ projects/concentrator/index.js
@@ -17,6 +17,7 @@ const concentratorAFXS = '0xDAF03D70Fe637b91bA6E521A32E1Fb39256d3EC9';
 const cvxcrvAddress = '0x62b9c7356a2dc64a1969e19c23e4f579f9810aa7';
 
 const concentratorNewVault = '0x3Cf54F3A1969be9916DAD548f3C084331C4450b5';
+const usdtAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7";
 const addressZero = "0x0000000000000000000000000000000000000000"
 const ethAddress = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
@@ -30,8 +31,16 @@ const replacements = [
   "0x73a052500105205d34Daf004eAb301916DA8190f"
 ].map(i => i.toLowerCase())
 
+async function getBalancerLpTvl(balances) {
+  const res = await fetchURL('https://concentrator-api.aladdin.club/data/aladdin/initInfo')
+  const lpTvl = res.data.data.balancerCtrTvl.tvl;
+  const usdtTVL = toUSDTBalances(lpTvl)
+  sdk.util.sumSingleBalance(balances, usdtAddress, usdtTVL[usdtAddress])
+}
+
 async function tvl(timestamp, block) {
   let balances = {}
+  await getBalancerLpTvl(balances, block)
   await getAFXSInfo(balances, block);
   const acrvTotalUnderlying = (await sdk.api.abi.call({
     target: concentratorAcrv,
@@ -116,9 +125,6 @@ async function getTokenTvl(balances, poolData, totalUnderlying, resolvedLPSupply
   }
   let coinBalances = []
   const tokens = coins.output.map(i => {
-    if (i.output.toLowerCase() == wethAddress.toLowerCase()) {
-      return ethAddress
-    }
     return i.output;
   })
   let tempBalances = await sumTokens2({ block, owner: swapAddress, tokens })
