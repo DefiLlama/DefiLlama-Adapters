6492133254a74c66b744093add1c28efb05d1973
0xtawa <0xtawanofi@gmail.com>
2022-09-23 09:24:43 +0200

    Fix type

--- volumes/helper/customBackfill.ts
+++ volumes/helper/customBackfill.ts
@@ -5,14 +5,14 @@ import { Chain } from "@defillama/sdk/build/general";
 
 const ONE_HOUR_IN_SECONDS = 60 * 60 * 24
 
-export type IGraphs = (chain: string) => (timestamp: number, chainBlocks: ChainBlocks) => Promise<{
+export type IGraphs = (chain: Chain) => (timestamp: number, chainBlocks: ChainBlocks) => Promise<{
     timestamp: number;
     block?: number;
     totalVolume: string;
     dailyVolume?: string;
 }>
 
-export default (chain: string, graphs: IGraphs): Fetch => async (timestamp: number, chainBlocks: ChainBlocks) => {
+export default (chain: Chain, graphs: IGraphs): Fetch => async (timestamp: number, chainBlocks: ChainBlocks) => {
     const fetchGetVolume = graphs(chain)
     const resultDayN = await fetchGetVolume(timestamp, chainBlocks)
     const timestampPreviousDay = timestamp - ONE_HOUR_IN_SECONDS
