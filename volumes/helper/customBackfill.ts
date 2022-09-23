e38143c02023e470a4f71c0e1850451acde2b989
strobie <0xstrobe@protonmail.com>
2022-09-23 09:13:44 +0200

    fix type error

--- volumes/helper/customBackfill.ts
+++ volumes/helper/customBackfill.ts
@@ -5,19 +5,19 @@ import { Chain } from "@defillama/sdk/build/general";
 
 const ONE_HOUR_IN_SECONDS = 60 * 60 * 24
 
-export type IGraphs = (chain: string) => (timestamp: number, chainBlocks: ChainBlocks) => Promise<{
+export type IGraphs = (chain: Chain) => (timestamp: number, chainBlocks: ChainBlocks) => Promise<{
     timestamp: number;
     block?: number;
     totalVolume: string;
     dailyVolume?: string;
 }>
 
-export default (chain: string, graphs: IGraphs): Fetch => async (timestamp: number, chainBlocks: ChainBlocks) => {
+export default (chain: Chain, graphs: IGraphs): Fetch => async (timestamp: number, chainBlocks: ChainBlocks) => {
     const fetchGetVolume = graphs(chain)
     const resultDayN = await fetchGetVolume(timestamp, chainBlocks)
     const timestampPreviousDay = timestamp - ONE_HOUR_IN_SECONDS
     let chainBlocksPreviousDay = {}
-    if (chainsForBlocks.includes(chain as Chain) || chain === "ethereum")
+    if (chainsForBlocks.includes(chain) || chain === "ethereum")
         chainBlocksPreviousDay = { [chain]: await getBlock(timestampPreviousDay, chain, {}).catch(() => { }) }
     const resultPreviousDayN = await fetchGetVolume(timestampPreviousDay, chainBlocksPreviousDay)
     return {
