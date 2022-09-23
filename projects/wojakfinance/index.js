90d91ac5664fe92a877040ee87c7948d6536984e
wojakfinance <113054160+wojakfinance@users.noreply.github.com>
2022-09-23 10:11:05 +0800

    fix: import error

--- projects/wojakfinance/index.js
+++ projects/wojakfinance/index.js
@@ -1,11 +1,10 @@
-const { stakings } = require('./helper/staking')
+
+const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
+const { stakings } = require('../helper/staking')
 
 const MasterChefContract = "0x065AAE6127D2369C85fE3086b6707Ac5dBe8210a";
 const WojkPoolContract = "0xDF21058099e69D3635005339721C4826c4c47F8A";
 const WOJK = "0x570C41a71b5e2cb8FF4445184d7ff6f78A4DbcBD";
-
-
-const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
 module.exports = {
     methodology: `Uses factory(0xc7c86B4f940Ff1C13c736b697e3FbA5a6Bc979F9) address and whitelisted tokens address to find and price Liquidity Pool pairs`,
     misrepresentedTokens: true,
