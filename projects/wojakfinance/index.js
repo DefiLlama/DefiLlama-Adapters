36b405083814e86238546a5cb04879f79d7b6114
wojakfinance <113054160+wojakfinance@users.noreply.github.com>
2022-09-23 11:04:07 +0800

    fix: update wojak tvl to get from subgraph

--- projects/wojakfinance/index.js
+++ projects/wojakfinance/index.js
@@ -1,36 +1,63 @@
-
-const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
+const { request, gql } = require("graphql-request");
+const { toUSDTBalances } = require('../helper/balances')
 const { stakings } = require('../helper/staking')
 
 const MasterChefContract = "0x065AAE6127D2369C85fE3086b6707Ac5dBe8210a";
 const WojkPoolContract = "0xDF21058099e69D3635005339721C4826c4c47F8A";
 const WOJK = "0x570C41a71b5e2cb8FF4445184d7ff6f78A4DbcBD";
+
+
+
+const graphEndpoint = 'https://api.dogechainhealth.com/subgraphs/name/wojakswap/exchange'
+const currentQuery = gql`
+query pancakeFactories {
+  pancakeFactories(first: 1) {
+    totalLiquidityUSD
+  }
+}
+`
+const historicalQuery = gql`
+query pancakeDayDatas {
+pancakeDayDatas(
+  first: 1000
+  orderBy: date
+  orderDirection: asc
+  ) {
+    date
+    dailyVolumeUSD
+    totalLiquidityUSD
+    __typename
+  }
+}
+`
+async function tvl(timestamp, ethBlock, chainBlocks) {
+  if (Math.abs(timestamp - Date.now() / 1000) < 3600) {
+    const tvl = await request(graphEndpoint, currentQuery, {}, {
+      "referer": "https://wojak.fi/",
+      "origin": "https://wojak.fi",
+    })
+    return toUSDTBalances(tvl.pancakeFactories[0].totalLiquidityUSD)
+  } else {
+    const tvl = (await request(graphEndpoint, historicalQuery)).pancakeDayDatas
+    let closest = tvl[0]
+    tvl.forEach(dayTvl => {
+      if (Math.abs(dayTvl.date - timestamp) < Math.abs(closest.date - timestamp)) {
+        closest = dayTvl
+      }
+    })
+    return toUSDTBalances(closest.totalLiquidityUSD)
+  }
+}
 module.exports = {
-    methodology: `Uses factory(0xc7c86B4f940Ff1C13c736b697e3FbA5a6Bc979F9) address and whitelisted tokens address to find and price Liquidity Pool pairs`,
     misrepresentedTokens: true,
     doublecounted: false,
     timetravel: true,
     incentivized: true,
+    methodology: 'TVL accounts for the liquidity on all AMM pools, using the TVL chart on https://wojak.fi/info as the source. Staking accounts for the WOJK locked in MasterChef (0x065AAE6127D2369C85fE3086b6707Ac5dBe8210a)',
     dogechain: {
-        tvl: calculateUsdUniTvl(
-            "0xc7c86B4f940Ff1C13c736b697e3FbA5a6Bc979F9",
-            "dogechain",
-            "0x765277EebeCA2e31912C9946eAe1021199B39C61",
-            [
-                "0x570C41a71b5e2cb8FF4445184d7ff6f78A4DbcBD",
-                "0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101",
-                "0x765277EebeCA2e31912C9946eAe1021199B39C61",
-                "0x332730a4F6E03D9C55829435f10360E13cfA41Ff",
-                "0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D",
-                "0xB44a9B6905aF7c801311e8F4E76932ee959c663C",
-                "0xfA9343C3897324496A05fC75abeD6bAC29f8A40f",
-                "0x7b4328c127b85369d9f82ca0503b000d09cf9180",
-                "0xa649325aa7c5093d12d6f98eb4378deae68ce23f",
-            ],
-            "usd-coin"
-        ),
+        tvl,
         staking: stakings([MasterChefContract, WojkPoolContract], WOJK, "dogechain"),
-    }
+    },
 };
 
 
