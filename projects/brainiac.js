2a68775073c22c51ad83666815bdcb0f0085d280
Brainiac <brainiacfinance@protonmail.com>
2022-09-26 15:34:28 +0530

    Add https://brainiac.fi/ money market adapters

---
+++ projects/brainiac.js
@@ -0,0 +1,10 @@
+const { fullCoumpoundExports } = require('./helper/brainiac');
+
+// params =  comptroller , chain , brCKB , CKB
+
+module.exports = fullCoumpoundExports(
+  "0x5c68BDBba7151c486faCB66dc39b891030e46725",
+  "godwoken_v1",
+  "0x352d09567dE5A02415670723D09E006F623fE62e",
+  "0x7538C85caE4E4673253fFd2568c1F1b48A71558a"
+)
