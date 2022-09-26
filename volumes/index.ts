d55a4f7113f48e5f4cbd116ffdd1541c082d80aa
realdealshaman <realdealshaman11@gmail.com>
2022-09-26 04:06:54 +0100

    update frax-swap name

--- volumes/index.ts
+++ volumes/index.ts
@@ -70,7 +70,7 @@ import wanswapDex from "./adapters/wanswap-dex";
 import solarbeam from "./adapters/solarbeam";
 import tombFinance from "./adapters/tomb-finance";
 import dfxFinance from "./adapters/dfx-finance";
-import fraxswap from "./adapters/fraxswap";
+import fraxswap from "./adapters/frax-swap";
 import izumiFinance from "./adapters/izumi-finance";
 import tinyman from "./adapters/tinyman";
 import junoswap from "./adapters/junoswap";
@@ -147,7 +147,7 @@ export default {
   solarbeam,
   "tomb-finance": tombFinance,
   "dfx-finance": dfxFinance,
-  fraxswap,
+  "frax-swap": fraxswap,
   "izumi-finance": izumiFinance,
   tinyman,
   junoswap,
