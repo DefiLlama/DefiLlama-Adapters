91d7c1d0f51e5c500f88dd4fffb627f337648543
Mount Junga <jungamount@gmail.com>
2022-09-27 10:12:42 +0700

    Task: Hectagon adapter update

--- projects/hectagon/index.js
+++ projects/hectagon/index.js
@@ -1,38 +1,36 @@
 const sdk = require("@defillama/sdk");
-
 const { transformBscAddress } = require("../helper/portedTokens");
-const getReserves = require("../helper/abis/getReserves.json");
 
 const BUSD_ADDRESS = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
-
 const HECTA_ADDRESS = "0x343915085b919fbd4414F7046f903d194c6F60EE";
-const TREASURY_ADDRESS = "0x4059c4a0b8a2B528C4f2E101A3bB8fB169aBa4fB";
+const ASSET_LOCKED_ADDRESS = "0x4059c4a0b8a2B528C4f2E101A3bB8fB169aBa4fB";
 const HECTA_BUSD_ADDRESS = "0xc7cee4cea7c76e11e9f5e5e5cbc5e3b798a1c4d0";
+const GHECTA_ADDRESS = "0x7D31ed03c2442F9ffc4f22d90772EE1f32fa9b0D";
 
 async function tvl(timestamp, block, chainBlocks) {
   const balances = {};
   const transform = await transformBscAddress();
 
-  const [busdBalance, hectaBalance, hectaBusdBalance] = await Promise.all([
+  const [busdBalance, hectaBusdBalance, hectaInGHecta] = await Promise.all([
     sdk.api.abi.call({
       abi: "erc20:balanceOf",
       chain: "bsc",
       target: BUSD_ADDRESS,
-      params: [TREASURY_ADDRESS],
-      block: chainBlocks["bsc"],
-    }),
-    sdk.api.abi.call({
-      abi: "erc20:balanceOf",
-      chain: "bsc",
-      target: HECTA_ADDRESS,
-      params: [TREASURY_ADDRESS],
+      params: [ASSET_LOCKED_ADDRESS],
       block: chainBlocks["bsc"],
     }),
     sdk.api.abi.call({
       abi: "erc20:balanceOf",
       chain: "bsc",
       target: HECTA_BUSD_ADDRESS,
-      params: [TREASURY_ADDRESS],
+      params: [ASSET_LOCKED_ADDRESS],
+      block: chainBlocks["bsc"],
+    }),
+    sdk.api.abi.call({
+      abi: "erc20:balanceOf",
+      chain: "bsc",
+      target: HECTA_ADDRESS,
+      params: [GHECTA_ADDRESS],
       block: chainBlocks["bsc"],
     }),
   ]);
@@ -45,14 +43,14 @@ async function tvl(timestamp, block, chainBlocks) {
 
   sdk.util.sumSingleBalance(
     balances,
-    transform(HECTA_ADDRESS),
-    hectaBalance.output
+    transform(HECTA_BUSD_ADDRESS),
+    hectaBusdBalance.output
   );
 
   sdk.util.sumSingleBalance(
     balances,
-    transform(HECTA_BUSD_ADDRESS),
-    hectaBusdBalance.output
+    transform(HECTA_ADDRESS),
+    hectaInGHecta.output
   );
 
   return balances;
