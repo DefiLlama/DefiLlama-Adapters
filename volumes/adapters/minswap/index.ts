d9b24867b04978118b9894490b0166a8847ccb8e
dtmkeng <phumaisut@gmail.com>
2022-09-27 18:19:16 +0700

    update functon get start time

--- volumes/adapters/minswap/index.ts
+++ volumes/adapters/minswap/index.ts
@@ -1,31 +1,50 @@
 import axios from "axios";
 import type { SimpleVolumeAdapter } from "../../dexVolume.type";
+import { CHAIN } from "../../helper/chains";
 import { getUniqStartOfTodayTimestamp } from "../../helper/getUniSubgraphVolume";
 
-const getVol = async()=>axios.get("https://api-mainnet-prod.minswap.org/defillama/volume-series")
-  .then((data) => data.data);
+
+interface IVolumeall {
+  time: number;
+  volume: number;
+};
+
+const historicalVolumeEndpoint = "https://api-mainnet-prod.minswap.org/defillama/volume-series";
+
+const fetch = async (timestamp: number) => {
+  const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+  const vols: IVolumeall[] = (await axios.get(historicalVolumeEndpoint))?.data;
+  const totalVolume = vols
+    .filter(volItem => (new Date(volItem.time).getTime() / 1000) <= dayTimestamp)
+    .reduce((acc, { volume }) => acc + Number(volume), 0);
+
+  const dailyVolume = vols
+    .find(dayItem => (new Date(dayItem.time).getTime() / 1000) === dayTimestamp)?.volume
+
+  const prices = await axios.post('https://coins.llama.fi/prices', {
+    "coins": [
+      "coingecko:cardano",
+    ],
+    timestamp: dayTimestamp
+  });
+
+  return {
+    timestamp: dayTimestamp,
+    totalVolume: totalVolume ? String(totalVolume/1e6 * prices.data.coins["coingecko:cardano"].price) : "0",
+    dailyVolume: dailyVolume ? String(dailyVolume/1e6 * prices.data.coins["coingecko:cardano"].price) : "0"
+  }
+}
+
+const getStartTimestamp = async () => {
+  const historicalVolume: IVolumeall[] = (await axios.get(historicalVolumeEndpoint))?.data;
+  return (new Date(historicalVolume[0].time).getTime()) / 1000;
+}
 
 const adapter: SimpleVolumeAdapter = {
   volume: {
-    "cardano": {
-      start: async () => {
-        return getVol().then(data=>data[0].time/1e3)
-      },
-      fetch: async (ts) => {
-        const cleanTimestamp = getUniqStartOfTodayTimestamp(new Date(ts * 1000))
-        const vols = await getVol()
-        const vol = vols.find((v:any)=>v.time/1e3===cleanTimestamp)
-        const prices = await axios.post('https://coins.llama.fi/prices', {
-          "coins": [
-            "coingecko:cardano",
-          ],
-          timestamp: cleanTimestamp
-        })
-        return {
-          timestamp: cleanTimestamp,
-          dailyVolume: String(vol.volume/1e6 * prices.data.coins["coingecko:cardano"].price)
-        }
-      }
+    [CHAIN.CARDADO]: {
+      start: getStartTimestamp,
+      fetch: fetch,
     }
   }
 };
