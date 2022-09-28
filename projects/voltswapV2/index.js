043021d4991c0a3bed3b1f8017cd0e8216924396
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-09-28 11:06:52 +0200

    refactor voltswap v2

--- projects/voltswapV2/index.js
+++
@@ -1,6 +0,0 @@
-const { uniTvlExport } = require('../helper/calculateUniTvl.js')
-module.exports = {
-  meter: {
-    tvl: uniTvlExport("0xb33dE8C0843F90655ad6249F20B473a627443d21", "meter"),
-  },
-}
