17d85b7cc3b9a83744a051d2227cee145e59ba5b
0xtawa <0xtawanofi@gmail.com>
2022-09-29 17:43:10 +0200

    Force/cast options type

--- volumes/utils/fetchURL.ts
+++ volumes/utils/fetchURL.ts
@@ -4,5 +4,5 @@ import AsyncRetry = require("async-retry")
 export default async function fetchURL(url: string) {
     return AsyncRetry(async () => await axios.get(url), {
         retries: 3
-    })
+    } as AsyncRetry.Options)
 }
