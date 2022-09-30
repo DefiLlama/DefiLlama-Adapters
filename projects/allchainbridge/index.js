3a24385cf72287b90782b31a834967f9182b64a6
Boris <1955765262@qq.com>
2022-09-30 12:17:37 +0800

    add allchainbridge other chain

--- projects/allchainbridge/index.js
+++ projects/allchainbridge/index.js
@@ -1,5 +1,6 @@
 const sdk = require("@defillama/sdk");
 const BigNumber = require("bignumber.js");
+const { sumTokens2, nullAddress, } = require('../helper/unwrapLPs')
 
 const getBridgeContract = (chain) => {
   switch (chain) {
@@ -27,8 +28,9 @@ const getBridgeContract = (chain) => {
  * ADD TOKENS AND NETWORKS HERE
  */
 
-const tokens = {
+const tokensConf = {
   ethereum: {
+    ETH: nullAddress,
     BUSD: "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
     USDC: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
     USDT: "0xdac17f958d2ee523a2206206994597c13d831ec7",
@@ -39,6 +41,7 @@ const tokens = {
     HT: "0x6f259637dcd74c767781e37bc6133cd6a68aa161",
   },
   bsc: {
+    BNB: nullAddress,
     BUSD: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
     USDC: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
     USDT: "0x55d398326f99059fF775485246999027B3197955",
@@ -50,6 +53,7 @@ const tokens = {
     DOGE: "0xba2ae424d960c26247dd6c32edc70b295c744c43",
   },
   heco: {
+    HT: nullAddress,
     USDC: "0x9362bbef4b8313a8aa9f0c9808b80577aa26b73b",
     USDT: "0xa71edc38d189767582c38a3145b5873052c3e47a",
     HUSD: "0x0298c2b32eae4da002a15f36fdf7615bea3da047",
@@ -57,54 +61,45 @@ const tokens = {
     SWFTC: "0x329dda64Cbc4DFD5FA5072b447B3941CE054ebb3",
   },
   okexchain: {
+    OKT: nullAddress,
     USDC: "0xc946daf81b08146b1c7a8da2a851ddf2b3eaaf85",
     USDT: "0x382bb369d343125bfb2117af9c149795c6c65c50",
     ETH: "0xef71ca2ee68f45b9ad6f72fbdb33d707b872315c",
     OKB: "0xdf54b6c6195ea4d948d03bfd818d365cf175cfc2",
   },
   polygon: {
+    MATIC: nullAddress,
     USDC: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
     USDT: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
     WMATIC: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
   },
   fantom: {
+    FTM: nullAddress,
     USDC: "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75",
     USDT: "0x049d68029688eabf473097a2fc38ef61633a3c7a",
     DAI: "0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e",
     WETH: "0x74b23882a30290451A17c44f4F05243b6b58C76d",
-  }
+  },
+  kava: {
+    KAVA: nullAddress,
+  },
 };
 
 /*
  * END OF CONFIGURATION
  */
 
-const getAddrPrefix = (chain) => (chain === "ethereum" ? "" : `${chain}:`);
-
 const createTvlFunction = (chain) => async (timestamp, block, chainBlocks) => {
-  const balances = {};
   const bridgeContract = getBridgeContract(chain);
-
-  for (const [symbol, address] of Object.entries(tokens[chain])) {
-    let tokenBalance = BigNumber(0);
-    // Get balance of token in bridge contract
-    const result = await sdk.api.erc20.balanceOf({
-      target: address,
-      owner: bridgeContract,
-      chain,
-      block: chainBlocks[chain],
-    });
-    tokenBalance = tokenBalance.plus(result.output);
-
-    balances[`${getAddrPrefix(chain)}${address}`] = tokenBalance.toFixed();
-  }
-  return balances;
+  const tokens = Object.values(tokensConf[chain])
+  const owners = [bridgeContract]
+  return sumTokens2({ chain, block:chainBlocks[chain], tokens, owners, })
 };
 
 const toExport = {
   methodology: "Assets staked in the pool and trading contracts",
 };
-for (const network of Object.keys(tokens)) {
+for (const network of Object.keys(tokensConf)) {
   toExport[network] = {
     tvl: createTvlFunction(network),
   };
