3f7df6480413253826f4e1b9954e60a90a84aace
TheG <theg@gmail.com>
2022-10-03 19:13:30 +0100

    Kava Deploy Draft

---
+++ projects/scrubKava/index.js
@@ -0,0 +1,92 @@
+const { getChainTransform, stripTokenHeader, getFixBalances, transformBalances, } = require('../helper/portedTokens')
+const { getTokenPrices } = require("../helper/unknownTokens")
+
+const { requery, } = require('../helper/getUsdUniTvl')
+const { getCoreAssets } = require('../helper/tokenMapping')
+const { sumTokens, sumTokens2, nullAddress, } = require('../helper/unwrapLPs')
+const { isLP, getUniqueAddresses, DEBUG_MODE, sliceIntoChunks, sleep, log } = require('../helper/utils')
+const token = ["0x96b451770772e64a582685d5EDCdfe53A5CD8718","0x96b451770772e64a582685d5EDCdfe53A5CD8718"]
+const rewardPool = ["0x69F451a527484159F27F34f6A5bd21727434027e"]
+const lps = Object.values({
+    'LION-USDC-LP': '0xf2059ed015ec4ecc80f902d9fdbcd2a227bfe037',
+    'TIGER-USDC-LP': '0xf6464c80448d6ec4deb7e8e5ec95b8eb768fbf69',
+    'BEAR-WBTC-LP': '0x3d9e539fa44b970605658e25d18f816ce78c4007',
+})
+
+function scrubTomb({ token = [], shares = [], rewardPool = [], masonry = [], lps, chain = "ethereum", coreAssets = [],
+  useDefaultCoreAssets = false, }) {
+  let getPrices
+  if (!coreAssets.length && useDefaultCoreAssets)
+    coreAssets = getCoreAssets(chain)
+
+  if (!Array.isArray(shares) && typeof shares === 'string')
+    shares = [shares]
+  if (!Array.isArray(masonry) && typeof masonry === 'string')
+    masonry = [masonry]
+  if (!Array.isArray(rewardPool) && typeof rewardPool === 'string')
+    rewardPool = [rewardPool]
+  if (!Array.isArray(token) && typeof token === 'string')
+    token = [token]
+
+  const pool2 = async (timestamp, _block, chainBlocks) => {
+    let balances = {};
+    const block = chainBlocks[chain]
+    if (!getPrices)
+      getPrices = getTokenPrices({ block, chain, lps, coreAssets, allLps: true })
+
+    const { updateBalances } = await getPrices
+
+    const tao = []
+    lps.forEach(token => rewardPool.forEach(owner => tao.push([token, owner])))
+
+    await sumTokens(balances, tao, block, chain, undefined, { resolveLP: true, skipFixBalances: true })
+    const fixBalances = await getFixBalances(chain)
+    await updateBalances(balances)
+    fixBalances(balances)
+    return balances
+  }
+  
+  const staking = async (timestamp, _block, chainBlocks) => {
+    let balances = {};
+    const block = chainBlocks[chain]
+    if (!getPrices)
+      getPrices = getTokenPrices({ block, chain, lps, coreAssets, allLps: true })
+
+    const { updateBalances } = await getPrices
+
+    const tao = []
+    shares.forEach(token => masonry.forEach(owner => tao.push([token, owner])))
+    token.forEach(token => rewardPool.forEach(owner => tao.push([token, owner])))
+
+    await sumTokens(balances, tao, block, chain, undefined, { skipFixBalances: true })
+    const fixBalances = await getFixBalances(chain)
+    await updateBalances(balances)
+    fixBalances(balances)
+    return balances
+  }
+
+  return {
+    [chain === "avax" ? "avalanche" : chain]: {
+      staking,
+      pool2,
+      tvl: async () => ({}),
+
+    }
+  }
+
+}
+
+module.exports = scrubTomb({
+  lps,
+  token,
+  shares: [
+    '0x398046624dF74d5038F2Fae49fDFC8d6bedC74f3', //Tiger
+  ],
+  rewardPool,
+  masonry: [
+    '0x1810F07671fFF4D03110Ec3bA9B3C8E88D88Ed89',
+  ],
+  chain: 'kava',
+  useDefaultCoreAssets: true,
+  misrepresentedTokens: true
+})
