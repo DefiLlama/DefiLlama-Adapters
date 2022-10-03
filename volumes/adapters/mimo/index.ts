a923f8584e64dd2bb64020ad62716d69ad0847fc
dtmkeng <phumaisut@gmail.com>
2022-10-03 18:47:20 +0700

    change function

--- volumes/adapters/mimo/index.ts
+++ volumes/adapters/mimo/index.ts
@@ -1,6 +1,37 @@
+import { Chain } from "@defillama/sdk/build/general";
+import { SimpleVolumeAdapter } from "../../dexVolume.type";
 import { CHAIN } from "../../helper/chains";
-import { univ2Adapter } from "../../helper/getUniSubgraphVolume";
 
-export default univ2Adapter({
-  [CHAIN.IOTEX]: "https://graph-cache.mimo.exchange/subgraphs/name/mimo/mainnet",
-}, {});
+const {
+  getChainVolume,
+} = require("../../helper/getUniSubgraphVolume");
+const endpoints = {
+  [CHAIN.IOTEX]: "https://graph-cache.mimo.exchange/subgraphs/name/mimo/mainnet"
+};
+
+const graphs = getChainVolume({
+  graphUrls: endpoints,
+  totalVolume: {
+    factory: "uniswapFactories",
+    field: "totalVolumeUSD",
+  },
+  dailyVolume: {
+    factory: "uniswapDayData",
+    field: "dailyVolumeUSD",
+  }
+});
+
+
+const adapter: SimpleVolumeAdapter = {
+  volume: Object.keys(endpoints).reduce((acc, chain: any) => {
+    return {
+      ...acc,
+      [chain]: {
+        fetch: graphs(chain as Chain),
+        start: async () => 1624332218
+      }
+    }
+  }, {} as SimpleVolumeAdapter['volume'])
+};
+
+export default adapter;
