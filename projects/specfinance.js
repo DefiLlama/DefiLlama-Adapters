549e9116467a4c87249bf4f8b486c31bad5dec53
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-03 15:16:55 +0530

    [Spectrum Finance] add terra2 tvl

--- projects/specfinance.js
+++ projects/specfinance.js
@@ -1,20 +1,29 @@
 const axios = require('axios')
 
 async function tvl(){
-    const tvl = await axios.get('https://spec-api.azurefd.net/api/data?type=tvl')
+    const tvl = await axios.get('https://api.spec.finance/api/stat')
     return {
         'terrausd': tvl.data.tvl,
     }
 }
 
+async function tvl(){
+    const tvl = await axios.get('https://spec-api.azurefd.net/api/data?type=tvl')
+    return {
+        'tether': tvl.data.tvl,
+    }
+}
+
 module.exports = {
+    misrepresentedTokens: true,
     timetravel: false,
-    terra2:{
+    terra:{
         tvl
+    },
+    terra2:{
+        tvl: terra2
     },
     hallmarks:[
     [1651881600, "UST depeg"],
   ]
-}  
-
-//add the new chain terra2
+}
