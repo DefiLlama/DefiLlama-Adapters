dbb769c74bd47f155491c63861d26b5b0e6a7952
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-03 14:31:45 +0200

    [xspswap] refactor code

--- projects/xspswap/index.js
+++ projects/xspswap/index.js
@@ -1,78 +1,12 @@
-const { request, gql } = require("graphql-request");
-const { toUSDTBalances } = require('../helper/balances')
-
-
-
-const graphEndpoint = 'https://graph-node.yodaplus.net:8000/subgraphs/name/pro100skm/factory'
-const currentQuery = gql`
-    query uniswapFactories {
-        uniswapFactories(first: 1) {
-            totalLiquidityUSD
-        }
-    }
-`
-const historicalQuery = gql`
-    query uniswapDayDatas {
-        uniswapDayDatas(
-            first: 1000
-            orderBy: date
-            orderDirection: asc
-        ) {
-            date
-            dailyVolumeUSD
-            totalLiquidityUSD
-            __typename
-        }
-    }
-`
-
-const graphUrl = 'https://graph-node.yodaplus.net:8000/subgraphs/name/pro100skm/factory'
-const graphQuery = gql`
-    query get_tvl($block: Int) {
-        uniswapFactories(
-            block: { number: $block }
-        ) {
-            totalVolumeUSD
-            totalLiquidityUSD
-        }
-    }
-`;
-async function tvl(timestamp, ethBlock, chainBlocks) {
-    if (Math.abs(timestamp - Date.now() / 1000) < 3600) {
-        const tvl = await request(graphEndpoint, currentQuery, {}, {
-            "referer": "https://app.xspswap.finance/",
-            "origin": "https://app.xspswap.finance/",
-        })
-        return toUSDTBalances(tvl.uniswapFactories[0].totalLiquidityUSD)
-    } else {
-        const tvl = (await request(graphEndpoint, historicalQuery)).uniswapDayDatas
-        let closest = tvl[0]
-        tvl.forEach(dayTvl => {
-            if (Math.abs(dayTvl.date - timestamp) < Math.abs(closest.date - timestamp)) {
-                closest = dayTvl
-            }
-        })
-        if(Math.abs(closest.date - timestamp) > 3600*24){ // Oldest data is too recent
-            const {uniswapFactories} = await request(
-                graphUrl,
-                graphQuery,
-                {
-                    block: chainBlocks['xdc'],
-                }
-            );
-            const usdTvl = Number(uniswapFactories[0].totalLiquidityUSD)
-
-            return toUSDTBalances(usdTvl)
-        }
-        return toUSDTBalances(closest.totalLiquidityUSD)
-    }
-}
+const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
-    timetravel: true,
-    misrepresentedTokens: true,
-    methodology: 'TVL accounts for the liquidity on all AMM pools, using the TVL chart on https://analytics.xspswap.finance/ as the source',
-    xdc: {
-        tvl
-    },
+  misrepresentedTokens: true,
+  xdc: {
+    tvl: getUniTVL({
+      factory: '0x347D14b13a68457186b2450bb2a6c2Fd7B38352f',
+      chain: 'xdc',
+      useDefaultCoreAssets: true,
+    })
+  },
 }
