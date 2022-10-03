04d812aa3987517addf21f2d8402def536749d03
ravirasadiya <ravirasadiya1994@gmail.com>
2022-10-03 16:34:50 +0530

    pudate

---
+++ projects/uniwswap/get-token-price.js
@@ -0,0 +1,23 @@
+const { fetchURL } = require("../helper/utils");
+
+const coingeckoMap = {
+  WETH: "ethereum-pow-iou",
+  UNIW: "uniwswap",
+};
+
+async function getTokenPrice(token) {
+  const id = coingeckoMap[token];
+  if (!id) {
+    throw new Error(`Id not available for token: ${token}`);
+  }
+
+  const prices = (
+    await fetchURL(
+      `https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd`
+    )
+  ).data;
+
+  return prices[id].usd;
+}
+
+module.exports = getTokenPrice;
--- projects/uniwswap/index.js
+++ projects/uniwswap/index.js
@@ -1,12 +1,66 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const sdk = require("@defillama/sdk");
+const abi = require("./abi.json");
+const calculateLpTokenPrice = require("./calculate-lp-token-price");
+const getTokenPrice = require("./get-token-price");
+
+const MASTERCHEF = "0xC07707C7AC7E383CE344C090F915F0a083764C94";
+
+async function tvl() {
+  let amount = 0;
+
+  const length = (
+    await sdk.api.abi.call({
+      abi: abi.poolLength,
+      chain: "ethpow",
+      target: MASTERCHEF,
+      params: [],
+    })
+  ).output;
+
+  const borkPrice = await getTokenPrice("UNIW");
+  for (let i = 0; i < length; i++) {
+    const poolInfo = (
+      await sdk.api.abi.call({
+        abi: abi.poolInfo,
+        chain: "ethpow",
+        target: MASTERCHEF,
+        params: [i],
+      })
+    ).output;
+
+    const token = (
+      await sdk.api.abi.call({
+        abi: "erc20:symbol",
+        chain: "ethpow",
+        target: poolInfo.lpToken,
+        params: [],
+      })
+    ).output;
+
+    const balance = (
+      await sdk.api.abi.call({
+        abi: abi.lpTokenAmount,
+        chain: "ethpow",
+        target: MASTERCHEF,
+        params: [i],
+      })
+    ).output;
+
+    let price = 0;
+
+    if (token === "UNIW") {
+      price = await getTokenPrice("UNIW");
+    } else if (token === "LP") {
+      price = await calculateLpTokenPrice(poolInfo.lpToken, borkPrice);
+    } else {
+      price = await getTokenPrice(token);
+    }
+    amount += (balance / 1e18) * price;
+  }
+
+  return amount;
+}
 
 module.exports = {
-  misrepresentedTokens: true,
-  ethpow: {
-    tvl: getUniTVL({
-      chain: 'ethpow',
-      useDefaultCoreAssets: true,
-      factory: '0xaBC4325bAD182076EAa5877c68437833d596D3Ee',
-    })
-  }
+  fetch: tvl,
 };
