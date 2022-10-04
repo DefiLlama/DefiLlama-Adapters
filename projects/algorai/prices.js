b294790c176da0ee34438d25b921be9e96980db6
Vitali <vitali@undercurrent.tech>
2022-10-04 07:29:50 +0400

    Add comments to algorai project.

--- projects/algorai/prices.js
+++ projects/algorai/prices.js
@@ -7,7 +7,7 @@ const {
 } = require("./utils");
 
 
-async function getTellorPrice(medianizerAppID) {
+async function getPriceFromMedianizer(medianizerAppID) {
     const state = await readGlobalState(medianizerAppID,
         ["median_value_1", "median_value_2", "median_value_3", "median_value_4", "median_value_5"]
     );
@@ -19,7 +19,7 @@ async function getPrices() {
 
     let price;
     for (const [depositID, medianizerID] of Object.entries(price_feeds)) {
-        price = await getTellorPrice(medianizerID)
+        price = await getPriceFromMedianizer(medianizerID)
         prices[depositID] = Number(price) / 10 ** priceDecimals;
     }
 
