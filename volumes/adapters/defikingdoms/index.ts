f3705f6f05fd01d9106be58fc11a47b166c330d1
0xtawa <0xtawanofi@gmail.com>
2022-10-04 12:19:29 +0200

    Add defikingdoms adapter

--- volumes/adapters/defikingdoms/index.ts
+++ volumes/adapters/defikingdoms/index.ts
@@ -1,11 +1,36 @@
-import { DISABLED_ADAPTER_KEY, SimpleVolumeAdapter } from "../../dexVolume.type";
+import axios from "axios";
+import { SimpleVolumeAdapter } from "../../dexVolume.type";
 import { CHAIN } from "../../helper/chains";
-import disabledAdapter from "../../helper/disabledAdapter";
+import { getUniqStartOfTodayTimestamp } from "../../helper/getUniSubgraphVolume";
+
+const API = "https://nomics.com/data/exchange-volume-history?convert=USD&exchange=defikingdoms&interval=all"
+
+interface IAPIResponse {
+  items: Array<{
+    timestamp: string
+    volume: string
+    spot_volume: string
+    derivative_volume: string
+  }>
+}
 
 const adapter: SimpleVolumeAdapter = {
   volume: {
-    [DISABLED_ADAPTER_KEY]: disabledAdapter,
-    [CHAIN.HARMONY]: disabledAdapter,
+    [CHAIN.HARMONY]: {
+      start: async () => {
+        const data = (await axios.get(API)).data as IAPIResponse
+        return new Date(data.items[0].timestamp).getTime() / 1000
+      },
+      fetch: async (timestamp: number) => {
+        const data = (await axios.get(API)).data as IAPIResponse
+        const cleanTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
+        return {
+          timestamp: cleanTimestamp,
+          dailyVolume: data.items.find((item) => (new Date(item.timestamp).getTime() / 1000) === cleanTimestamp)?.spot_volume,
+          totalVolume: String(data.items.reduce((acc, item)=>acc+=+item.spot_volume, 0))
+        }
+      }
+    },
   },
 };
 
