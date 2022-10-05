84147be4ef5feb22d911e08c51254719ce6d0e3b
Tim Jones <t27.jones@connect.qut.edu.au>
2022-10-05 16:31:23 +1000

    WIP add Staking TVL

--- projects/mycelium/index.js
+++ projects/mycelium/index.js
@@ -1,7 +1,11 @@
-const { gmxExports } = require('../helper/gmx')
+const { mycExports } = require("../helper/mycelium");
 
 module.exports = {
   arbitrum: {
-    tvl: gmxExports({ chain: 'arbitrum', vault: '0xDfbA8AD57d2c62F61F0a60B2C508bCdeb182f855', })
+    tvl: mycExports({
+      chain: "arbitrum",
+      vault: "0xDfbA8AD57d2c62F61F0a60B2C508bCdeb182f855",
+      staking: "0x9B225FF56C48671d4D04786De068Ed8b88b672d6",
+    }),
   },
-}
+};
