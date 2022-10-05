9324ff8998faaa6eb920bc6fcff58c3672e8aab2
Alex <alex@mtrlabs.com>
2022-10-05 19:50:09 +0800

    add metadata state balance

--- projects/amulet-protocol.js
+++ projects/amulet-protocol.js
@@ -1,7 +1,29 @@
-const retry = require('async-retry')
+const retry = require('async-retry');
 const axios = require("axios");
+const BN = require("bn.js");
+const { PublicKey } = require("@solana/web3.js")
+const { getConnection } = require("./helper/solana");
 
 async function tvl() {
+    const connection = getConnection();
+    const metadataState = await connection.getAccountInfo(new PublicKey("DEg9mNnxwqJQiYz2mXpVJmq8Yyx14oZ2SaYpHS7mB2mx"));
+    const posSolVirtualAmount = metadataState.data.slice(129, 137);
+    posSolVirtualAmount.reverse();
+    console.log(new BN(posSolVirtualAmount).toString());
+    const liqSolAmount = metadataState.data.slice(307, 315);
+    liqSolAmount.reverse();
+    console.log(new BN(liqSolAmount).toString());
+    const posState = await connection.getAccountInfo(new PublicKey("CaxYodGBcen2MVPSM1SKdAMHC2csnPSvLAZu2vkVT1H2"));
+    const totalActiveBalance = posState.data.slice(160, 168);
+    totalActiveBalance.reverse();
+    console.log(new BN(totalActiveBalance).toString());
+    const premiumPoolAccountBalance = new BN((await connection.getAccountInfo(new PublicKey("632JbevHriKtPvxncKJYa5asT76Rm77Tnrvncscufusg"))).lamports);
+    console.log(premiumPoolAccountBalance.toString());
+    const claimProgramSolBalance = new BN((await connection.getAccountInfo(new PublicKey("3eXy94aq5sGN3pZiJBzM6LVP4W7vNPoNbyPkpqSwBn8u"))).lamports);
+    console.log(claimProgramSolBalance.toString());
+    const pcuvSolBalance = new BN((await connection.getAccountInfo(new PublicKey("3snom88cEW4WtXH7pHEyYHr76oMVtYqAMdJ4BtBWWaC9"))).lamports);
+    console.log(pcuvSolBalance.toString());
+
     var response = await retry(async bail => await axios.post('https://gql.amulet.org/tvl?code=POhj0JlawECAf6r7TANSZdXWelMSL1L4cbZZjCS9CzJkAzFurBl76A==', {
             query: `
                 query {
