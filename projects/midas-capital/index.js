84fd64ecdc2787a8d7be1a002eab64d243636227
Rahul Sethuram <rahul@sethuram.com>
2022-10-05 16:19:32 +0400

    DefiLlama updates

--- projects/midas-capital/index.js
+++ projects/midas-capital/index.js
@@ -1,6 +1,11 @@
-const { compoundExportsWithAsyncTransform } = require("../helper/compound");
-const { transformBscAddress, transformMoonbeamAddress } = require("../helper/portedTokens");
 const sdk = require("@defillama/sdk");
+
+const { compoundExportsWithAsyncTransform } = require("../helper/compound");
+const {
+  transformBscAddress,
+  transformMoonbeamAddress,
+  transformPolygonAddress,
+} = require("../helper/portedTokens");
 
 function getTvl(chain) {
   const pools = {
@@ -12,6 +17,9 @@ function getTvl(chain) {
         "0x5373C052Df65b317e48D6CAD8Bb8AC50995e9459", // BOMB
         "0xfeB4f9080Ad40ce33Fd47Ff6Da6e4822fE26C7d5", // Ankr
         "0xd3E5AAFebBF06A071509cf894f665710dDaa800d", // Tester
+        "0x35F3a59389Dc3174A98610727C2e349E275Dc909", // Ellipsis
+        "0x3F239A5C45849391E7b839190597B5130780790d", // PancakeStack
+        "0x7f8B5fCA1a63C632776ffc9936D2e323c14B57f8", // tbd
       ],
       transform: transformBscAddress,
     },
@@ -19,9 +27,19 @@ function getTvl(chain) {
       pools: [
         "0xeB2D3A9D962d89b4A9a34ce2bF6a2650c938e185", // xDot
         "0x0fAbd597BDecb0EEE1fDFc9B8458Fe1ed0E35028", // BeamSwap
+        "0xCc248E6106CB7B05293eF027D5c1c05BF3E39F21", // StellaSwap
       ],
       transform: transformMoonbeamAddress,
-    }
+    },
+    polygon: {
+      pools: [
+        "0xCc248E6106CB7B05293eF027D5c1c05BF3E39F21", // Jarvis
+        "0xB08A309eFBFFa41f36A06b2D0C9a4629749b17a2", // Arrakis
+        "0xBd82D36B9CDfB9747aA12025CeCE3782EDe767FE", // tbd
+        "0xF1ABd146B4620D2AE67F34EA39532367F73bbbd2", // MIMO
+      ],
+      transform: transformPolygonAddress,
+    },
   };
   const config = pools[chain] ?? { pools: [] };
   const tvls = config.pools.map((pool) =>
