24847d3650e12df69c1d7cde86930f5b12850072
yym-mtrlabs <yumeng@mtrlabs.com>
2022-10-05 20:18:07 +0800

    update tvl calculation

--- projects/amulet-protocol.js
+++ projects/amulet-protocol.js
@@ -1,5 +1,3 @@
-const retry = require('async-retry');
-const axios = require("axios");
 const BN = require("bn.js");
 const { PublicKey } = require("@solana/web3.js")
 const { getConnection } = require("./helper/solana");
@@ -9,41 +7,22 @@ async function tvl() {
     const metadataState = await connection.getAccountInfo(new PublicKey("DEg9mNnxwqJQiYz2mXpVJmq8Yyx14oZ2SaYpHS7mB2mx"));
     const posSolVirtualAmount = metadataState.data.slice(129, 137);
     posSolVirtualAmount.reverse();
-    console.log(new BN(posSolVirtualAmount).toString());
+
     const liqSolAmount = metadataState.data.slice(307, 315);
     liqSolAmount.reverse();
-    console.log(new BN(liqSolAmount).toString());
+
     const posState = await connection.getAccountInfo(new PublicKey("CaxYodGBcen2MVPSM1SKdAMHC2csnPSvLAZu2vkVT1H2"));
     const totalActiveBalance = posState.data.slice(160, 168);
     totalActiveBalance.reverse();
-    console.log(new BN(totalActiveBalance).toString());
-    const premiumPoolAccountBalance = new BN((await connection.getAccountInfo(new PublicKey("632JbevHriKtPvxncKJYa5asT76Rm77Tnrvncscufusg"))).lamports);
-    console.log(premiumPoolAccountBalance.toString());
-    const claimProgramSolBalance = new BN((await connection.getAccountInfo(new PublicKey("3eXy94aq5sGN3pZiJBzM6LVP4W7vNPoNbyPkpqSwBn8u"))).lamports);
-    console.log(claimProgramSolBalance.toString());
-    const pcuvSolBalance = new BN((await connection.getAccountInfo(new PublicKey("3snom88cEW4WtXH7pHEyYHr76oMVtYqAMdJ4BtBWWaC9"))).lamports);
-    console.log(pcuvSolBalance.toString());
 
-    var response = await retry(async bail => await axios.post('https://gql.amulet.org/tvl?code=POhj0JlawECAf6r7TANSZdXWelMSL1L4cbZZjCS9CzJkAzFurBl76A==', {
-            query: `
-                query {
-                    tvl(input:{
-                        first: 1
-                    })
-                    {
-                        tvl_sol_amount
-                    }
-                }
-            `
-        }, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    )
+    const accountList = ["632JbevHriKtPvxncKJYa5asT76Rm77Tnrvncscufusg", "3eXy94aq5sGN3pZiJBzM6LVP4W7vNPoNbyPkpqSwBn8u", "3snom88cEW4WtXH7pHEyYHr76oMVtYqAMdJ4BtBWWaC9"]
+    const accountSolBalances = await Promise.all(accountList.map(async(x) => new BN((await connection.getAccountInfo(new PublicKey(x))).lamports)))
+    const solAmount = [new BN(posSolVirtualAmount), new BN(liqSolAmount), new BN(totalActiveBalance), ...accountSolBalances]
+
+    const tvl = solAmount.reduce((acc,x) => acc.add(x), new BN(0)).div(new BN(1000000000)).toNumber()
 
     return {
-        'solana': Number(response.data.data.tvl.tvl_sol_amount)
+        'solana': tvl
     }
 }
 
