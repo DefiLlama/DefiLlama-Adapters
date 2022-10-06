9a42b9ef49fa8df0ea552a3c510c6481d4beaa46
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-06 16:59:10 +0200

    Minor code refactor

--- projects/algorai-finance/index.js
+++ projects/algorai-finance/index.js
@@ -1,9 +1,10 @@
 const { toUSDTBalances } = require("../helper/balances");
+const sdk = require('@defillama/sdk')
 
 const {
   vaults,
 } = require("./constants");
-const { readGlobalState} = require("./utils");
+const { readGlobalState } = require("./utils");
 const { getPrices } = require("./prices");
 
 /**
@@ -14,14 +15,10 @@ const { getPrices } = require("./prices");
  */
 async function getTotalTvl(prices) {
   const promises = vaults.map(async (vault) => {
-    try {
-      const state = await readGlobalState(vault.vaultID, ["vtv"]);
-      const totalTvl = state[0] ? state[0] / 10 ** vault.assetDecimals : 0;
+    const state = await readGlobalState(vault.vaultID, ["vtv"]);
+    const totalTvl = state[0] ? state[0] / 10 ** vault.assetDecimals : 0;
 
-      return vault.stable ? totalTvl : totalTvl * prices[vault.depositAssetID];
-    } catch (e) {
-      return 0;
-    }
+    return vault.stable ? totalTvl : totalTvl * prices[vault.depositAssetID];
   });
 
   const totalTvl = await Promise.all(promises);
@@ -34,6 +31,7 @@ async function getTotalTvl(prices) {
  * @returns {Promise<{"[usdtAddress]": *}>}
  */
 async function tvl() {
+  /*
   const prices = await getPrices();
 
   const [
@@ -42,12 +40,24 @@ async function tvl() {
     getTotalTvl(prices),
   ]);
   return toUSDTBalances(
-      tvl
+    tvl
   );
+  
+  */
+  const balances = {}
+  const promises = vaults.map(async (vault) => {
+    const state = await readGlobalState(vault.vaultID, ["vtv"]);
+    const totalTvl = state[0] ? state[0] / 10 ** vault.assetDecimals : 0;
+    sdk.util.sumSingleBalance(balances, vault.coingecko, totalTvl)
+  });
+  await Promise.all(promises)
+
+  return balances
 }
 
 
 module.exports = {
+  timetravel: false,
   algorand: {
     tvl,
   },
