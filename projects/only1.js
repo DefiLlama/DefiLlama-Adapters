3ca3a1c5449289c4b4fb94012a522fdafb86e04c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-08 14:22:00 +0200

    revert only1

--- projects/only1.js
+++ projects/only1.js
@@ -1,14 +1,12 @@
 const axios = require("axios")
-const { sumTokens2 } = require('./helper/solana')
+const retry = require('async-retry')
 
 async function staking() {
-  // const { data } = await axios.get("https://us-central1-only1-staking-stats.cloudfunctions.net/tvl")
-  const { data: { data } } = await axios.get("https://api.only1.app/staking-pools?limit=100&sort=numTokensLocked%3Adesc")
-  const { data: { data: data1 } } = await axios.get("https://api.only1.app/staking-pools?limit=100&page=2&sort=numTokensLocked%3Adesc")
+  const { data } = await axios.get("https://us-central1-only1-staking-stats.cloudfunctions.net/tvl")
 
-  data.push(...data1)
-  const owners = data.map(i => i.address)
-  return sumTokens2({ tokens: ['3bRTivrVsitbmCTGtqwp7hxXPsybkjn4XLNtPsHqa3zR'], owners, })
+  return {
+    'only1': data.totalTvl
+  }
 }
 
 module.exports = {
