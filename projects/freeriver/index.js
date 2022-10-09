afd26ca6cdbff16dd60d1d6084e570e772724654
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-09 02:13:49 +0200

    bugfix

--- projects/freeriver/index.js
+++ projects/freeriver/index.js
@@ -2,5 +2,7 @@ const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports={
     misrepresentedTokens: true,
-    tvl: getUniTVL({ factory: '0x52abD262B13bef4E65Ff624880E8A0595a17af48', chain: 'moonriver', useDefaultCoreAssets: true }),
+    moonriver: {
+        tvl: getUniTVL({ factory: '0x52abD262B13bef4E65Ff624880E8A0595a17af48', chain: 'moonriver', useDefaultCoreAssets: true }),
+    }
 }
--- projects/versedex/index.js
+++ projects/versedex/index.js
@@ -1,5 +1,4 @@
 const { getUniTVL } = require('../helper/unknownTokens')
-const { getUniTVL } = require('../helper/unknownTokens')
 
 const FLEXUSD = "0x7b2B3C5308ab5b2a1d9a94d20D35CCDf61e05b72";
 const WBCH = "0x3743eC0673453E5009310C727Ba4eaF7b3a1cc04";
