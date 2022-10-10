aab30da4cee53c04386cf26f7832cc84e4721f14
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-10 11:37:32 +0200

    export treasury as seperate field

--- projects/peardao/index.js
+++ projects/peardao/index.js
@@ -1,7 +1,6 @@
-const sdk = require('@defillama/sdk');
-const { transformBscAddress } = require('../helper/portedTokens');
 const { stakings } = require('../helper/staking');
 const { pool2 } = require('../helper/pool2');
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 const PEX_TOKEN_CONTRACT = '0x6a0b66710567b6beb81A71F7e9466450a91a384b';
 const BUSD_TOKEN_CONTRACT = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
@@ -16,45 +15,16 @@ const PEX_BNB_LP_MASTER_CHEF = '0xa5f8C5Dbd5F286960b9d90548680aE5ebFf07652';
 const PEX_STAKING_POOL_CONTRACT = '0x5F57dCa7D4f81D56C777E060D9dC81AF112d23eb';
 const DOTC_CONTRACT = '0x8996Da635aFabd360fbABB80e7Be5028324B8323'; // P2P trade escrow contract
 const TREASURY_ADDRESS = '0x263e0910C8c1B77B80CB9947B0FAC3735a6FEf4C';
-
-async function tvl(timestamp, block, chainBlocks) {
-  const balances = {};
-  const transform = await transformBscAddress();
-
-  async function sumBalance(token, address) {
-    const collateralBalance = (await sdk.api.abi.call({
-      abi: 'erc20:balanceOf',
-      chain: 'bsc',
-      target: token,
-      params: [address],
-      block: chainBlocks['bsc'],
-    })).output;
-    sdk.util.sumSingleBalance(balances, transform(token), collateralBalance);
-  }
-
-  // Calculate the value of assets in the treasury
-  await sumBalance(BUSD_TOKEN_CONTRACT, TREASURY_ADDRESS);
-  await sumBalance(USDT_TOKEN_CONTRACT, TREASURY_ADDRESS);
-  await sumBalance(USDC_TOKEN_CONTRACT, TREASURY_ADDRESS);
-  await sumBalance(WBNB_TOKEN_CONTRACT, TREASURY_ADDRESS);
-
-  // Calculate the value of locked assets in P2P trade escrow contract
-  await sumBalance(BUSD_TOKEN_CONTRACT, DOTC_CONTRACT);
-  await sumBalance(USDT_TOKEN_CONTRACT, DOTC_CONTRACT);
-  await sumBalance(USDC_TOKEN_CONTRACT, DOTC_CONTRACT);
-  await sumBalance(BTCB_TOKEN_CONTRACT, DOTC_CONTRACT);
-  await sumBalance(USTC_TOKEN_CONTRACT, DOTC_CONTRACT);
-
-  return balances;
-}
+const tokens =  [
+  BUSD_TOKEN_CONTRACT, USDC_TOKEN_CONTRACT, USDT_TOKEN_CONTRACT, WBNB_TOKEN_CONTRACT, USTC_TOKEN_CONTRACT, BTCB_TOKEN_CONTRACT,
+]
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'Counts the value of LP tokens and PEX tokens in the staking contracts, assets locked in the P2P escrow contract, and assets in the treasury contract.',
   start: 15966251, // Mar-11-2022 01:00:01 PM +UTC
   bsc: {
-    tvl,
+    treasury: sumTokensExport({ chain: 'bsc', tokens, owner: TREASURY_ADDRESS, }),
+    tvl: sumTokensExport({ chain: 'bsc', tokens, owner: DOTC_CONTRACT, }),
     staking: stakings([TREASURY_ADDRESS, PEX_STAKING_POOL_CONTRACT], PEX_TOKEN_CONTRACT, "bsc"),
     pool2: pool2(PEX_BNB_LP_MASTER_CHEF, PEX_BNB_LP_CONTRACT, 'bsc')
   }
