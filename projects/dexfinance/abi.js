c68b29bd047a7ef9c9d572aa74adb28f2a6d1c64
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-11 22:57:16 +0200

    [dexfinance] code refactor

--- projects/dexfinance/abi.js
+++ projects/dexfinance/abi.js
@@ -13,83 +13,9 @@ const ETF_ABI = {
     "type": "function",
     "constant": true
   },
-  'getBalance': {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "token",
-        "type": "address"
-      }
-    ],
-    "name": "getBalance",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  'decimals': {
-    "inputs": [],
-    "name": "decimals",
-    "outputs": [
-      {
-        "internalType": "uint8",
-        "name": "",
-        "type": "uint8"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-}
-
-const ETF_ORACLE_ABI = {
-  'computeAverageTokenPrices': {
-    "inputs": [
-      {
-        "internalType": "address[]",
-        "name": "tokens",
-        "type": "address[]"
-      },
-      {
-        "internalType": "uint256",
-        "name": "minTimeElapsed",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "maxTimeElapsed",
-        "type": "uint256"
-      }
-    ],
-    "name": "computeAverageTokenPrices",
-    "outputs": [
-      {
-        "components": [
-          {
-            "internalType": "uint224",
-            "name": "_x",
-            "type": "uint224"
-          }
-        ],
-        "internalType": "struct FixedPoint.uq112x112[]",
-        "name": "averagePrices",
-        "type": "tuple[]"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  }
 }
 
 
 module.exports = {
   ETF_ABI,
-  ETF_ORACLE_ABI
 }
--- projects/dexfinance/index.js
+++ projects/dexfinance/index.js
@@ -1,14 +1,13 @@
 const sdk = require("@defillama/sdk");
 const BigNumber = require("bignumber.js");
-const { fetchURL } = require("../helper/utils");
-const { ETF_ABI, ETF_ORACLE_ABI } = require('./abi');
-const { getChainTransform } = require('../helper/portedTokens');
+const { ETF_ABI, } = require('./abi');
+const { sumTokens2, sumTokensExport, } = require('../helper/unwrapLPs')
 
 const REGULATION_STAKING_POOL = '0xd69db827939e26511068aa2bf742e7463b292190'
 const FARM = '0xcc180bfa5d2c3ac191758b721c9bbbb263b3fd1c'
+const TREASURY = '0xa5f3d6a33c5a5bcdff8f81c88ca00f457b699e0f'
 
 const ETF_INDEX_POOL = '0x60ebfd605cb25c7796f729c78a4453acecb1ce03'
-const ETF_ORACLE = '0x3b186d534c714679cf9d0504d1fbfd56c2339e7c'
 
 const TOKENS = {
   USDEX_USDC_LP: '0x79f3bb5534b8f060b37b3e5dea032a39412f6b10',
@@ -24,8 +23,6 @@ const commonCallOptions = {
   chain: 'bsc',
   decimals: 18
 }
-
-const tokenPrices = {}
 
 async function getWdexDexsharePrice(dexIraPrice, dexSharePrice) {
   const [
@@ -56,139 +53,50 @@ async function getWdexDexsharePrice(dexIraPrice, dexSharePrice) {
 
   return (balanceDexIraInUsd + balanceDexShareInUsd) / wdexTotalSupply;
 }
-async function getTokenPrices(transformAddress) {
-  const allTokens = Object.values(TOKENS)
-    .reduce((a, b) => `${a},${transformAddress(b)}`, '');
 
-  const { coins } = (await fetchURL("https://coins.llama.fi/prices/current/" + allTokens)).data;
-
-  Object.values(TOKENS).forEach((address) => {
-    const priceObj = coins[transformAddress(address.toLowerCase())];
-    if (!priceObj) { return }
-    tokenPrices[address] = priceObj.price
-  }, {})
-
-  const wdexDexsharePrice = await getWdexDexsharePrice(tokenPrices[TOKENS.DEXIRA], tokenPrices[TOKENS.DEXSHARE])
-
-  tokenPrices[TOKENS.WDEX_DEXSHARE] = wdexDexsharePrice
-};
-
-async function regulationPoolTvl() {
-
-  const { output: dexShareBalance } = await sdk.api.erc20.balanceOf({
-    target: TOKENS.DEXSHARE,
-    owner: REGULATION_STAKING_POOL,
-    ...commonCallOptions,
-  })
-  return dexShareBalance * tokenPrices[TOKENS.DEXSHARE]
-};
-
-async function farmsTvl() {
-  let tvl = 0
-  const [
-    { output: USDEX_USDC_LPFarmBalance },
-    { output: DEXSHARE_BNB_LPFarmBalance },
-    { output: WDEX_DEXSHAREFarmBalance },
-  ] = await Promise.all([
-    sdk.api.erc20.balanceOf({
-      target: TOKENS.USDEX_USDC_LP,
-      owner: FARM,
-      ...commonCallOptions,
-    }),
-    sdk.api.erc20.balanceOf({
-      target: TOKENS.DEXSHARE_BNB_LP,
-      owner: FARM,
-      ...commonCallOptions,
-    }),
-    sdk.api.erc20.balanceOf({
-      target: TOKENS.WDEX_DEXSHARE,
-      owner: FARM,
-      ...commonCallOptions,
-    }),
-  ])
-  tvl += USDEX_USDC_LPFarmBalance * tokenPrices[TOKENS.USDEX_USDC_LP]
-  tvl += DEXSHARE_BNB_LPFarmBalance * tokenPrices[TOKENS.DEXSHARE_BNB_LP]
-  tvl += WDEX_DEXSHAREFarmBalance * tokenPrices[TOKENS.WDEX_DEXSHARE]
-  return tvl
-};
-
-async function dexEtfTvl() {
-  const bnbPrice = tokenPrices[TOKENS.BNB]
-  const { output: tokenAddresses } = await sdk.api.abi.call({
+const chain = 'bsc'
+async function tvl(_, _b, { bsc: block}) {
+  const { output: tokens } = await sdk.api.abi.call({
     target: ETF_INDEX_POOL,
     abi: ETF_ABI['getCurrentTokens'],
     chain: 'bsc',
     params: []
   })
-  const tokensBalanceWeight = (await sdk.api.abi.multiCall({
-    abi: ETF_ABI['getBalance'],
-    calls: tokenAddresses.map(ta => ({ target: ETF_INDEX_POOL, params: [ta] })),
-    chain: 'bsc',
-  })).output.map(({ output }) => output)
+  const balances = await sumTokens2({ chain, block, tokens, owner: ETF_INDEX_POOL, })
+  return balances
+} 
 
-  const decimals = (await Promise.all(
-    tokenAddresses.map((ta) => sdk.api.abi.call({
-      target: ta,
-      abi: 'erc20:decimals',
-      chain: 'bsc',
-    })))
-  ).map(({ output }) => output)
-
-  const tokenPricesOracle = (await sdk.api.abi.call({
-    target: ETF_ORACLE,
-    abi: ETF_ORACLE_ABI['computeAverageTokenPrices'],
-    chain: 'bsc',
-    params: [tokenAddresses, '1', '172800']
-  })).output.map(([price]) => price)
-
-  const tvl = tokenPricesOracle.reduce((acc, tokenPrice, index) => {
-    const balance = tokensBalanceWeight[index];
-    const decimalsNum = Number(decimals[index]);
-    const tokenPriceInBnb = new BigNumber(tokenPrice).div(2 ** 112).div(10 ** (18 - decimalsNum));
-    const tokenBnbValue = new BigNumber(balance).div(10 ** decimalsNum).times(tokenPriceInBnb);
-    return acc.plus(tokenBnbValue.times(bnbPrice));
-  }, new BigNumber(0));
-
-  return Number(tvl.toFixed(0))
-}
-
-async function lpPairsTvl() {
-  const lps = [
-    TOKENS.DEXIRA_BNB_LP,
-    TOKENS.DEXSHARE_BNB_LP,
-    TOKENS.USDEX_USDC_LP,
-  ]
-
-  const totalSupplies = (await sdk.api.abi.multiCall({
-    abi: 'erc20:totalSupply',
-    calls: lps.map(ta => ({ target: ta, params: [] })),
-    chain: 'bsc',
-  }))
-  const balances = {}
-  sdk.util.sumMultiBalanceOf(balances, totalSupplies, true);
-
-  const tvlUsd = lps.reduce((a, i) => {
-    const balance = balances[i]
-    const priceUsd = tokenPrices[i]
-    return a.plus(new BigNumber(balance).times(priceUsd).div(10 ** 18))
-  }, new BigNumber(0));
-  return tvlUsd.toNumber()
-}
-
-async function fetch() {
-  const transformAddress = await getChainTransform('bsc')
-
-  await getTokenPrices(transformAddress)
-  const [etfTvlUsd, farmsTvlUsd, regulationPoolTvlUsd, lpPairsTvlUsd] = await Promise.all([
-    dexEtfTvl(),
-    farmsTvl(),
-    regulationPoolTvl(),
-    lpPairsTvl(transformAddress)
+async function farmWDEX_DEXSHARE(_, _b, { bsc: block}) {
+  const [
+    { output: bal },
+    { output: iraBal },
+    { output: shareBal },
+    { output: totalSupply },
+  ] = await Promise.all([
+    sdk.api.abi.call({ chain, block, abi: 'erc20:balanceOf', target: TOKENS.WDEX_DEXSHARE, params: FARM}),
+    sdk.api.abi.call({ chain, block, abi: 'erc20:balanceOf', target: TOKENS.DEXIRA, params: TOKENS.WDEX_DEXSHARE}),
+    sdk.api.abi.call({ chain, block, abi: 'erc20:balanceOf', target: TOKENS.DEXSHARE, params: TOKENS.WDEX_DEXSHARE }),
+    sdk.api.abi.call({ chain, block, abi: 'erc20:totalSupply', target: TOKENS.WDEX_DEXSHARE, }),
   ])
-
-  return etfTvlUsd + farmsTvlUsd + regulationPoolTvlUsd + lpPairsTvlUsd
+  const ratio = bal/totalSupply
+  const balances = {}
+  sdk.util.sumSingleBalance(balances, 'bsc:'+TOKENS.DEXIRA, BigNumber(iraBal * ratio).toFixed(0))
+  sdk.util.sumSingleBalance(balances, 'bsc:'+TOKENS.DEXSHARE, BigNumber(shareBal * ratio).toFixed(0))
+  return balances
 }
 
 module.exports = {
-  fetch
+  bsc: {
+    tvl,
+    pool2: sdk.util.sumChainTvls([
+      sumTokensExport({ chain, tokens: [TOKENS.USDEX_USDC_LP, TOKENS.DEXSHARE_BNB_LP, ], owner: FARM, }),
+      farmWDEX_DEXSHARE
+    ]),
+    treasury: sumTokensExport({ chain, tokens: [
+      TOKENS.DEXIRA_BNB_LP,
+      TOKENS.DEXSHARE_BNB_LP,
+      TOKENS.USDEX_USDC_LP,
+    ], owner: TREASURY, }),
+    staking: sumTokensExport({ chain, tokensAndOwners: [[TOKENS.DEXSHARE, REGULATION_STAKING_POOL,], ], }),
+  }
 };
