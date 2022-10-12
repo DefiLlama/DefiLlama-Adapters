ae476f907eb76395e322e7ac53af76bf1d8483fa
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-10-12 17:25:42 -0300

    TVL MoreMoney

--- projects/moremoney/index.js
+++ projects/moremoney/index.js
@@ -1,201 +1,71 @@
-const sdk = require("@defillama/sdk");
-const { ethers } = require("ethers");
-const { getProvider } = require("@defillama/sdk/build/general");
-
-const CurvePoolRewards = "0x9727D535165e19590013bdDea8Fd85dd618b9aF7";
-const account = "0x0000000000000000000000000000000000000000";
+const { ethers, BigNumber } = require("ethers");
 const methodology = "";
-const BigNumber = require("bignumber.js");
-const addresses = require("./addresses.json");
-const StrategyViewer = require("./StrategyViewer.json");
-const StrategyRegistry = require("./StrategyRegistry.json");
-const IStrategy = require("./IStrategy.json");
-
-function useAddresses() {
-  return addresses[43114];
-}
+const useStrategyMetadata = require("./StrategyMetadata").useStrategyMetadata;
+const useLegacyIsolatedStrategyMetadata =
+  require("./StrategyMetadata").useLegacyIsolatedStrategyMetadata;
+const getTokenDecimalsAndValue1e18 =
+  require("./tokens").getTokenDecimalsAndValue1e18;
+const useParsedStakingMetadata =
+  require("./StakingMetadata").useParsedStakingMetadata;
 
 async function tvl(timestamp, block) {
-  const addresses = useAddresses();
-  const provider = getProvider('avax');
+  const allStratMeta = await useStrategyMetadata();
+  const legacyStratMeta = await useLegacyIsolatedStrategyMetadata();
+  const { tokenDecimals, tokenValuePer1e18 } =
+    await getTokenDecimalsAndValue1e18([...allStratMeta, ...legacyStratMeta]);
 
-  const stratRegistry = new ethers.Contract(
-    addresses.StrategyRegistry,
-    StrategyRegistry.abi,
-    provider
-  );
+  const stakeMeta = await useParsedStakingMetadata();
+  console.log("stakeMeta", stakeMeta);
 
-  const stratViewer = new ethers.Contract(
-    addresses.StrategyViewer,
-    StrategyViewer.abi,
-    provider
-  );
+  const tvlsFarm = stakeMeta
+    .filter((x) => x)
+    .filter(([x]) => x)
+    .reduce((tvl, row) => {
+      return tvl.add(row.tvl);
+    }, BigNumber.from(0));
 
-  const enabledStrategies = await stratRegistry.allEnabledStrategies();
-
-  const approvedTokens = await Promise.all(
-    enabledStrategies.map((address) => {
-      const contract = new ethers.Contract(address, IStrategy.abi, provider);
-      return contract.viewAllApprovedTokens();
-    })
-  );
-  let token2Strat2 = {};
-
-  for (let i = 0; i < enabledStrategies.length; i++) {
-    const strategy = enabledStrategies[i];
-    const tokens = approvedTokens[i];
-    for (let j = 0; j < tokens.length; j++) {
-      if (token2Strat2[tokens[j]] == undefined) {
-        token2Strat2[tokens[j]] = strategy;
-      }
-    }
-  }
-
-  const tokens = Object.keys(token2Strat2);
-  const strats = Object.values(token2Strat2);
-
-  const noHarvestBalanceResults =
-    await stratViewer.viewMetadataNoHarvestBalance(
-      addresses.StableLending,
-      addresses.OracleRegistry,
-      addresses.Stablecoin,
-      tokens,
-      strats
-    );
-
-  const stratMeta = [...noHarvestBalanceResults];
-  const stakingMeta = (
-    await sdk.api.abi.call({
-      target: CurvePoolRewards,
-      block,
-      chain: "avax",
-      params: [account],
-      abi: {
-        inputs: [
-          {
-            internalType: "address",
-            name: "account",
-            type: "address",
-          },
-        ],
-        name: "stakingMetadata",
-        outputs: [
-          {
-            components: [
-              {
-                internalType: "address",
-                name: "stakingToken",
-                type: "address",
-              },
-              {
-                internalType: "address",
-                name: "rewardsToken",
-                type: "address",
-              },
-              {
-                internalType: "uint256",
-                name: "totalSupply",
-                type: "uint256",
-              },
-              {
-                internalType: "uint256",
-                name: "tvl",
-                type: "uint256",
-              },
-              {
-                internalType: "uint256",
-                name: "aprPer10k",
-                type: "uint256",
-              },
-              {
-                internalType: "uint256",
-                name: "vestingCliff",
-                type: "uint256",
-              },
-              {
-                internalType: "uint256",
-                name: "periodFinish",
-                type: "uint256",
-              },
-              {
-                internalType: "uint256",
-                name: "stakedBalance",
-                type: "uint256",
-              },
-              {
-                internalType: "uint256",
-                name: "vestingStart",
-                type: "uint256",
-              },
-              {
-                internalType: "uint256",
-                name: "earned",
-                type: "uint256",
-              },
-              {
-                internalType: "uint256",
-                name: "rewards",
-                type: "uint256",
-              },
-              {
-                internalType: "uint256",
-                name: "vested",
-                type: "uint256",
-              },
-            ],
-            internalType: "struct VestingStakingRewards.StakingMetadata",
-            name: "",
-            type: "tuple",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-    })
-  ).output;
-
-  const tokensMeta = Object.values(stratMeta).map((strat) => {
-    return strat.token;
-  });
-  const decimals = await Promise.all(
-    tokensMeta.map((token) => {
-      return sdk.api.erc20.decimals(token, "avax");
-    })
-  );
-
-  const tokenList = [
-    tokensMeta.map((token, index) => {
-      return { address: token, decimals: decimals[index].output };
-    }),
-  ];
-
-  const tvlsFarm = stakingMeta.tvl;
-
-  const tvlNoFarm = Object.values(stratMeta)
+  const tvlNoFarm = Object.values(allStratMeta)
     .map((row) => {
-      let decimals = tokenList.filter((t) => t.address === row.token)[0]
-        ?.decimals;
+      let decimals = tokenDecimals[row.token];
+      let valuePer1e18 = tokenValuePer1e18[row.token];
       const trueOne = ethers.utils.parseUnits("1", decimals);
+
       const valPerOne = trueOne
-        .mul(row.valuePer1e18)
+        .mul(valuePer1e18)
         .div(ethers.utils.parseEther("1"));
       return {
         ...row,
         tvlInPeg: row.tvl.mul(valPerOne).div(trueOne),
       };
     })
-    .reduce((tvl, row) => {
-      return tvl.plus(row.tvlInPeg.toString());
-    }, BigNumber(0));
-  const tvl = tvlNoFarm.plus(tvlsFarm);
+    .reduce((tvl, row) => tvl.add(row.tvlInPeg), BigNumber.from(0));
+
+  const legacyTvlNoFarm = Object.values(legacyStratMeta)
+    .map((row) => {
+      let decimals = tokenDecimals[row.token];
+      let valuePer1e18 = tokenValuePer1e18[row.token];
+      const trueOne = ethers.utils.parseUnits("1", decimals);
+      const valPerOne = trueOne
+        .mul(valuePer1e18)
+        .div(ethers.utils.parseEther("1"));
+      return {
+        ...row,
+        tvlInPeg: row.tvl.mul(valPerOne).div(trueOne),
+      };
+    })
+    .reduce((tvl, row) => tvl.add(row.tvlInPeg), BigNumber.from(0));
+  const tvl = tvlNoFarm.add(tvlsFarm).add(legacyTvlNoFarm);
+
   return {
-    "avax:0xd586e7f844cea2f87f50152665bcbc2c279d8d70": tvl.toFixed(0),
+    "avax:0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E": parseFloat(
+      ethers.utils.formatUnits(tvl, 12)
+    ),
   };
 }
 
 module.exports = {
   methodology: methodology,
-  avax:{
+  avax: {
     tvl,
   },
 };
