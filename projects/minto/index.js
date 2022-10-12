9b0cbee5ccfd38faa0ff543c8572e215f10884f3
Arsenii <backend@cryptouniverse.io>
2022-10-12 16:36:21 +0300

    update adapter

--- projects/minto/index.js
+++ projects/minto/index.js
@@ -1,4 +1,8 @@
 const { stakings } = require('../helper/staking')
+const {getBlock} = require("../helper/getBlock");
+const sdk = require("@defillama/sdk");
+const {default: BigNumber} = require("bignumber.js");
+const {getChainTransform} = require("../helper/portedTokens");
 
 const minto = '0x410a56541bD912F9B60943fcB344f1E3D6F09567'
 const hminto = '0x410a56541bd912f9b60943fcb344f1e3d6f09567'
@@ -7,13 +11,57 @@ const stackingContracts = [
   '0xe742FCE58484FF7be7835D95E350c23CE55A7E12',
 ]
 
+const abiBalanceOfSum = {
+  "inputs": [
+    {
+      "internalType": "address",
+      "name": "account",
+      "type": "address"
+    }
+  ],
+  "name": "balanceOfSum",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+}
+
+function mintoStaking(stakingContracts, stakingToken, chain = "ethereum", transformedTokenAddress = undefined, decimals = undefined) {
+
+  return async (timestamp, _ethBlock, chainBlocks) => {
+    const block = await getBlock(timestamp, chain, chainBlocks, true)
+    const bal = (await sdk.api.abi.multiCall({
+      calls: stakingContracts.map(c => ({target: stakingToken, params: [c]})),
+      chain,
+      block,
+      abi: abiBalanceOfSum
+    })).output.reduce((total, call) => BigNumber(total).plus(call.output).toFixed(0), "0")
+    let address = stakingToken;
+    if (transformedTokenAddress) {
+      address = transformedTokenAddress
+    } else {
+      address = (await getChainTransform(chain))(stakingToken)
+    }
+    if (decimals !== undefined) {
+      return {
+        [address]: Number(bal) / (10 ** decimals)
+      }
+    }
+    return {
+      [address]: bal
+    }
+  }
+}
 module.exports = {
   bsc: {
-    tvl: stakings(stackingContracts, minto, 'bsc')
-    staking: stakings(stackingContracts, minto, 'bsc')
+    tvl: mintoStaking(stackingContracts, minto, 'bsc'),
   },
   heco: {
-    tvl: stakings(stackingContracts, hminto, 'heco'),
-    staking: stakings(stackingContracts, hminto, 'heco')
+    tvl: mintoStaking(stackingContracts, hminto, 'heco'),
   }
 }
