bca22940e20d282539ec55682b2ff3498c3727a8
thomasrunner <thomas.lock.personal@gmail.com>
2022-10-12 22:11:41 -0400

    Updated index with fetch and staking

--- projects/neuy/index.js
+++ projects/neuy/index.js
@@ -2,59 +2,76 @@ const BigNumber = require('bignumber.js')
 const sdk = require('@defillama/sdk')
 const { fetchURL } = require("../helper/utils");
 
+const { staking } = require("../helper/staking");
+
 const neuy = {
   "ethereum": "0xa80505c408C4DEFD9522981cD77e026f5a49FE63",
   "polygon": "0x62a872d9977Db171d9e213A5dc2b782e72ca0033",
 }
 
-
-const lp = {
-  "polygon100": "0x8574f0F28Bbd7BCfFec50B00cc4D153C564bfC05",
-  "polygon10": "0x83139cf662df4fee8797Dc916EF2B5aaFE86eB16",
-  "uniswapUSDC":"0x140ae14be4b5e86aa149f76e84953746e0bc04f1",
-  "uniswapNEUY":"0xabebc245a9a47166ecd10933d43817c8ef6fb825",
-  "uniswapNEUY2":"0xfb55e94d02d2309a62fbd2bb64c27772ae9d900a",
-
+const ethLP = {
+  "ethereum100": "0x83833e3c5363b51db01e74ccc97e98a09ce86dcc",
+  "ethereum10": "0x82bcd0c19acb970a75771b370f2a3adea1702a44",
 }
 
-async function fetch() {
+const polyLP = {
+  "polygon100": "0x8574f0F28Bbd7BCfFec50B00cc4D153C564bfC05",
+  "polygon10": "0x83139cf662df4fee8797Dc916EF2B5aaFE86eB16",
+}
 
-  block = (await sdk.api.util.lookupBlock(Math.floor(Date.now() / 1000), { chain: "polygon" }))
+async function eth() {
+
+  ethBlock = (await sdk.api.util.lookupBlock(Math.floor(Date.now() / 1000), { chain: "ethereum" }))
       .block;
-  const lps = [
+  const ethLPS = [
     {
-      "target": neuy.polygon,
-      "params": lp.polygon100
+      "target": neuy.ethereum,
+      "params": ethLP.ethereum100
     },
     {
-      "target": neuy.polygon,
-      "params": lp.polygon10
-    },
-    {
-      "target": neuy.polygon,
-      "params": lp.uniswapUSDC
-    },
-    {
-      "target": neuy.polygon,
-      "params": lp.uniswapNEUY
-    },
-    {
-      "target": neuy.polygon,
-      "params": lp.uniswapNEUY2
+      "target": neuy.ethereum,
+      "params": ethLP.ethereum10
     }
   ]
-  let balances = await stakingBalanceTvl(Math.floor(Date.now() / 1000), block, "polygon", lps)
+  let ethBalances = await stakingBalanceTvl(Math.floor(Date.now() / 1000), ethBlock, "ethereum", ethLPS)
   const pricesData = (await fetchURL(
         `https://api.coingecko.com/api/v3/simple/price?ids=neuy&vs_currencies=usd`
     )).data;
     
-  const amount = (Object.values(balances)[0] / Math.pow(10, 18))
+  amount = (Object.values(ethBalances)[0] / Math.pow(10, 18))
   let tvl = pricesData.neuy.usd * amount;
+  tvl = Math.round(tvl * 100) / 100.0;
+  console.log(tvl);
+  return tvl;
+}
+
+async function poly() {
+
+  polyBlock = (await sdk.api.util.lookupBlock(Math.floor(Date.now() / 1000), { chain: "polygon" }))
+      .block;
+  const polyLPS = [
+    {
+      "target": neuy.polygon,
+      "params": polyLP.polygon100
+    },
+    {
+      "target": neuy.polygon,
+      "params": polyLP.polygon10
+    }
+  ]
+  let polyBalances = await stakingBalanceTvl(Math.floor(Date.now() / 1000), polyBlock, "polygon", polyLPS)
+  const pricesData = (await fetchURL(
+        `https://api.coingecko.com/api/v3/simple/price?ids=neuy&vs_currencies=usd`
+    )).data;
+    
+  amount = (Object.values(polyBalances)[0] / Math.pow(10, 18))
+  let tvl = pricesData.neuy.usd * amount;
+  tvl = Math.round(tvl * 100) / 100.0;
+  console.log(tvl);
   return tvl;
 }
 
 async function stakingBalanceTvl(timestamp, block, chain, lps) {
-
   const balancesOfResult = await sdk.api.abi.multiCall({
     calls: lps.map((lp) => ({
       target: lp.target,
@@ -69,6 +86,22 @@ async function stakingBalanceTvl(timestamp, block, chain, lps) {
   return balances
 }
 
-module.exports = {
-fetch: fetch
+async function fetch() {
+  let polyAmount = await poly();
+  let ethAmount = await eth();
+  return polyAmount + ethAmount;
 }
+
+module.exports = {
+ fetch: fetch,
+ ethereum: {
+    tvl: () => ({}),
+    staking: staking(ethLP.ethereum100, neuy.ethereum, 'ethereum'),
+    staking: staking(ethLP.ethereum10, neuy.ethereum, 'ethereum')
+  },
+ polygon: {
+    tvl: () => ({}),
+    staking: staking(polyLP.polygon100, neuy.polygon, 'polygon'),
+    staking: staking(polyLP.polygon10, neuy.polygon, 'polygon')
+  }
+} 
