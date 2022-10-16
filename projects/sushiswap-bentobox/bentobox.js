ae60ab736a82bfb127f6b5401365e7a75c0ffa53
HHK <hhk.contact@protonmail.com>
2022-10-16 12:24:59 +0200

    feat: add bentobox tvl adapter

--- projects/sushiswap-bentobox/bentobox.js
+++ projects/sushiswap-bentobox/bentobox.js
@@ -1,15 +1,36 @@
 const sdk = require("@defillama/sdk");
-const { request, gql } = require("graphql-request");
+const { BigNumber } = require("ethers");
 const { getChainTransform } = require("../helper/portedTokens");
-const { getFuroTokens, getKashiTokens, getTridentTokens } = require("./helper");
+const {
+  getFuroTokens,
+  getKashiTokens,
+  getTridentTokens,
+  getBentoboxTokensArray,
+} = require("./helper");
 
 function bentobox(chain) {
   return async (timestamp, ethBlock, chainBlocks) => {
     const balances = {};
     const transform = await getChainTransform(chain);
 
-    const tokens = await getTridentTokens(chain);
-    console.log(tokens);
+    const bentoTokens = await getBentoboxTokensArray(chain); //array with shares and amount
+    const tridentTokens = await getTridentTokens(chain); //mapping with amount
+    const kashiTokens = await getKashiTokens(chain); //mapping with amount
+    const furoTokens = await getFuroTokens(chain); //mapping with amount
+    bentoTokens.map((token) => {
+      let amount = BigNumber.from(token.rebase.elastic);
+      if (tridentTokens[token.id]) {
+        amount = amount.sub(tridentTokens[token.id]);
+      }
+      if (kashiTokens[token.id]) {
+        amount = amount.sub(kashiTokens[token.id]);
+      }
+      if (furoTokens[token.id]) {
+        amount = amount.sub(furoTokens[token.id]);
+      }
+
+      sdk.util.sumSingleBalance(balances, transform(token.id), amount);
+    });
 
     return balances;
   };
@@ -17,5 +38,5 @@ function bentobox(chain) {
 
 module.exports = {
   bentobox,
-  methodology: `TVL of Sushiswap BentoBox consist of tokens deposited in it minus Trident, Furo and Kashi TVL since they are built on it.`,
+  methodology: `TVL of BentoBox consist of tokens deposited in it minus Trident, Furo and Kashi TVL since they are built on it.`,
 };
