81fb1b1d701e9597decd1c71f12d0446894106e5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-17 14:07:42 +0200

    fix jpeg-d

--- projects/helper/eos.js
+++ projects/helper/eos.js
@@ -7,13 +7,14 @@ const RPC_ENDPOINTS = {
     'telos': 'https://telos.greymass.com',
 }
 
+async function getEosBalance(account_name, chain = "eos") {
+    const { data } = await retry(async () => await axios.default.post(`${RPC_ENDPOINTS[chain]}/v1/chain/get_account`, { account_name, }));
+    return data;
+}
+
 async function get_currency_balance(code, account, symbol, chain = "eos") {
-    const response = await retry(async () => await axios.default.post(`${RPC_ENDPOINTS[chain]}/v1/chain/get_currency_balance`, {code, account, symbol}));
-    try {
-        return Number(response.data[0].split(" ")[0]);
-    } catch (e) {
-        return 0;
-    }
+    const response = await retry(async () => await axios.default.post(`${RPC_ENDPOINTS[chain]}/v1/chain/get_currency_balance`, { code, account, symbol }));
+    return Number(response.data[0].split(" ")[0]);
 }
 
 /**
@@ -28,12 +29,11 @@ async function get_currency_balance(code, account, symbol, chain = "eos") {
  * get_precision("8,WAX");
  * // => 8
  */
-function get_precision( symbol )
-{
-    if ( symbol.includes(",") ) return symbol.split(",")[0];
-    if ( symbol == "EOS") return 4;
-    if ( symbol == "TLOS") return 4;
-    if ( symbol == "WAX") return 8;
+function get_precision(symbol) {
+    if (symbol.includes(",")) return symbol.split(",")[0];
+    if (symbol == "EOS") return 4;
+    if (symbol == "TLOS") return 4;
+    if (symbol == "WAX") return 8;
     return 4;
 }
 
@@ -41,7 +41,7 @@ function get_precision( symbol )
 async function get_staked(account_name, symbol, chain = "eos") {
     const response = await retry(async () => await axios.default.post(`${RPC_ENDPOINTS[chain]}/v1/chain/get_account`, { account_name }));
     try {
-        return response.data.voter_info.staked / (10 ** get_precision( symbol ))
+        return response.data.voter_info.staked / (10 ** get_precision(symbol))
     } catch (e) {
         return 0;
     }
@@ -51,15 +51,15 @@ async function get_account_tvl(accounts, tokens, chain = "eos") {
     const balances = {}
 
     // support single or multiple accounts
-    for ( const account of Array.isArray(accounts) ? accounts : [ accounts ] ) {
-        for ( const [ code, symbol, id ] of tokens ) {
+    for (const account of Array.isArray(accounts) ? accounts : [accounts]) {
+        for (const [code, symbol, id] of tokens) {
             const balance = await get_currency_balance(code, account, symbol, chain);
 
             // support native staking as balance
             const staked = code == "eosio.token" ? await get_staked(account, symbol, chain) : 0;
 
             // support adding same balance from multiple accounts
-            if ( balances[id] ) balances[id] += balance + staked;
+            if (balances[id]) balances[id] += balance + staked;
             else balances[id] = balance + staked;
         }
     }
@@ -67,6 +67,7 @@ async function get_account_tvl(accounts, tokens, chain = "eos") {
 }
 
 module.exports = {
+    getEosBalance,
     get_staked,
     get_currency_balance,
     get_account_tvl,
