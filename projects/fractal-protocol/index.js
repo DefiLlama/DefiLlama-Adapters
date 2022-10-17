7ccae8e86deaef267d4e990a89e1991d5059d019
adam.xyz <66784124+adamxyzxyz@users.noreply.github.com>
2022-10-17 16:06:35 -0400

    update tvl calculations. add whitelabel moonriver calculation

--- projects/fractal-protocol/index.js
+++ projects/fractal-protocol/index.js
@@ -2,58 +2,58 @@ const sdk = require('@defillama/sdk')
 const Web3 = require('web3');
 const axios = require("axios");
 
-const vaultAbi = [
+const vaultAbi = 
   {
     inputs: [],
     name: 'getTokenPrice',
     outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
-  },
-];
-const usdfAbi = [
+  }
+;
+const usdfAbi = 
   {
     inputs: [],
     name: 'totalSupply',
     outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
-  },
-];
+  }
+;
 
 const USDF_TOKEN_CONTRACT = '0x51acB1ea45c1EC2512ae4202B9076C13016dc8aA';
 const FRACTAL_VAULT_CONTRACT = '0x3EAa4b3e8967c02cE1304C1EB35e8C5409838DFC';
 
-const web3 = new Web3(process.env.INFURA_CONNECTION);
-
-const usdfEthereumMetrics = async () => {
+async function usdfEthereumTvl(timestamp, block) {
   //set contracts
-  const usdfContract = new web3.eth.Contract(usdfAbi, USDF_TOKEN_CONTRACT);
-  const vaultContract = new web3.eth.Contract(vaultAbi, FRACTAL_VAULT_CONTRACT);
 
-  const usdfTotalSupply = await usdfContract.methods.totalSupply().call();
-  const usdfPrice = await vaultContract.methods.getTokenPrice().call();
+  const {output: usdfTotalSupply} = await sdk.api.abi.call({ target: USDF_TOKEN_CONTRACT, block, abi: usdfAbi})
+  const {output: usdfPrice} = await sdk.api.abi.call({ target: FRACTAL_VAULT_CONTRACT, block, abi: vaultAbi})
 
   const usdfTvl = (usdfTotalSupply * usdfPrice) / 1e12;
-  return usdfTvl
+  return {
+    'usd-coin': usdfTvl 
+  }
 
 }
 
-const whitelabelMoonriverMetrics = async () => {
+ async function whitelabelMoonriverTvl(timestamp, block) {
   const data = await axios.get('https://api.fractalprotocol.org/api/vault/total-tvl')
   const whitelabelTvlMoonriver = data.data.whitelabelTvlMoonriver
-  return whitelabelTvlMoonriver
+  return {
+    'usd-coin': whitelabelTvlMoonriver 
+  } 
 }
 
 
 module.exports = {
   ethereum: {
     methodology: "Total Value Locked in Fractal via USDF.",
-    tvl: usdfEthereumMetrics
+    tvl: usdfEthereumTvl
   },
   moonriver: {
     methodology: "Total Value Locked in Fractal White Label Strategies on Moonriver.",
-    tvl: whitelabelMoonriverMetrics
+    tvl: whitelabelMoonriverTvl
   } 
 }
 
