80aec866d5f8f59dc2a63532df2b9bc7dc203ea4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-17 15:09:57 +0200

    remove dependency on eosflare

--- projects/vigor/index.js
+++ projects/vigor/index.js
@@ -1,28 +1,7 @@
-const {lendingMarket} = require("../helper/methodologies")
-const { getEosBalance, get_account_tvl } = require("../helper/eos")
-const axios = require("axios");
-
-const EOSFLARE_ENDPOINT = "https://api.eosflare.io";
-
-// async function get_account_tvl(account) {
-//   const response = await axios.default.post(EOSFLARE_ENDPOINT + "/v1/eosflare/get_account", {account});
-//   const { token_value, balance_total, eos_price } = response.data.account;
-//   return token_value + // sum of all alt tokens
-//          balance_total * eos_price; // sum of EOS balance * price
-// }
-
-// https://app.vigor.ai/health
-async function eos() {
-  return await getEosBalance("vigorlending");
-}
-
-async function fetch() {
-  return await eos();
-}
+const { get_account_tvl } = require("../helper/eos")
 
 module.exports = {
   timetravel: false,
-  methodology: `${lendingMarket}. Vigor TVL is achieved by querying token balances from Vigor's lending smart contract via https://eosflare.io/api.`,
   eos: {
     tvl: async () => {
       const account = 'vigorlending'
