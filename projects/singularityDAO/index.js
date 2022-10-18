b24c7c58ab4a5375d37839e0abd02d4b94438a0d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-18 18:12:07 +0200

    singularityDAO: code refactor

--- projects/singularityDAO/index.js
+++ projects/singularityDAO/index.js
@@ -2,9 +2,13 @@ const abi = require("./abi.json");
 const http = require("../helper/http");
 const sdk = require("@defillama/sdk");
 
-const { sumTokens } = require("../helper/unwrapLPs");
+const { sumTokens, sumTokens2 } = require("../helper/unwrapLPs");
 const { addFundsInMasterChef } = require("./masterchef");
+const { getParamCalls } = require('../helper/utils')
 
+
+const AGIX_TOKEN = "0x5B7533812759B45C2B44C19e320ba2cD2681b542";
+const NuNet_TOKEN = "0xF0d33BeDa4d734C72684b5f9abBEbf715D0a7935";
 const SDAO_TOKEN = "0x993864E43Caa7F7F12953AD6fEb1d1Ca635B875F";
 const SDAO_TOKEN_BNB = "0x90Ed8F1dc86388f14b64ba8fb4bbd23099f18240";
 
@@ -32,9 +36,9 @@ const graphEndpoint =
 // TVL
 
 async function tvl(ts, block) {
+  const blacklistedTokens = [SDAO_TOKEN, LP_TOKEN_SDAO_ETH, LP_TOKEN_SDAO_USDT, AGIX_TOKEN, NuNet_TOKEN]
   // DYNASETS
 
-  const balances = {};
   const response = await http.graphQuery(graphEndpoint, getDynasetQuery);
   const dynasets = response.dynaset
     .map((d) => d.addresses.map((a) => a.value))
@@ -53,40 +57,28 @@ async function tvl(ts, block) {
       )
     );
 
-  // Dynaset Contribution
-  DYNASET_FORGES.forEach((forge) =>
-    tokensAndOwners.push(
-      ["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", forge],
-      ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", forge],
-      ["0xdAC17F958D2ee523a2206206994597C13D831ec7", forge],
-      ["0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", forge],
-      ["0x514910771AF9Ca656af840dff83E8264EcF986CA", forge]
-    )
-  );
-
-  // VAULTS
-
-  tokensAndOwners.push([
-    "0xF0d33BeDa4d734C72684b5f9abBEbf715D0a7935",
-    "0x502B965d3D51d4FD531E6A1c1fA9bFA50337bA55",
-  ]);
-  await addFundsInMasterChef({
-    balances,
+  let { output: forgetCount } = await sdk.api.abi.multiCall({
+    abi: abis.totalForges,
+    calls: DYNASET_FORGES.map(i => ({ target: i})),
     block,
-    masterchef: "0xfB85B9Ec50560e302Ab106F1E2857d95132120D0",
-    skipTokens: [SDAO_TOKEN, LP_TOKEN_SDAO_ETH, LP_TOKEN_SDAO_USDT],
-  });
+  })
 
-  await addFundsInMasterChef({
-    balances,
-    block,
-    masterchef: "0xb267deaace0b8c5fcb2bb04801a364e7af7da3f4",
-    skipTokens: [SDAO_TOKEN],
-  });
+  const calls = []
+  forgetCount = forgetCount.map(({input: { target}, output}) => {
+    let arry = getParamCalls(output)
+    arry.forEach(i => i.target = target)
+    calls.push(...arry)
+  })
 
-  await sumTokens(balances, tokensAndOwners, block);
+  let { output: tokenInfo } = await sdk.api.abi.multiCall({
+    abi: abis.forgeInfo, calls,    block,
+  })
 
-  return balances;
+  tokenInfo.forEach(({input: { target}, output}) => {
+    tokensAndOwners.push([output.contributionToken, target])
+  })
+
+  return sumTokens2({ tokensAndOwners, block, blacklistedTokens, })
 }
 
 // LP Pools ERC
@@ -94,27 +86,28 @@ async function tvl(ts, block) {
 // Staked LP tokens where one side of the market is the platform's own governance token.
 
 async function pool2(ts, block) {
-  const balances = {};
   const tokensAndOwners = [
     [LP_TOKEN_SDAO_ETH, "0xfB85B9Ec50560e302Ab106F1E2857d95132120D0"], // Unbonded
     [LP_TOKEN_SDAO_USDT, "0xfB85B9Ec50560e302Ab106F1E2857d95132120D0"], // Bonded 6M
   ];
-  await sumTokens(balances, tokensAndOwners, block);
-  return balances;
+  return sumTokens2({tokensAndOwners, block});
 }
 
 // Staking pools ERC
 // The platform's own tokens
 
 async function staking(ts, block) {
-  const balances = {};
   const tokensAndOwners = [
     [SDAO_TOKEN, "0xfB85B9Ec50560e302Ab106F1E2857d95132120D0"], // Unbonded
     [SDAO_TOKEN, "0x74641ed232dbB8CBD9847484dD020d44453F0368"], // Bonded 6M
     [SDAO_TOKEN, "0xF5738B4aD2f8302b926676692a0C09603d930b42"], // Bonded 12M
+    [NuNet_TOKEN, "0x502B965d3D51d4FD531E6A1c1fA9bFA50337bA55"],
+    [NuNet_TOKEN, "0xfB85B9Ec50560e302Ab106F1E2857d95132120D0"],
+    [NuNet_TOKEN, "0xb267deaace0b8c5fcb2bb04801a364e7af7da3f4"],
+    [AGIX_TOKEN, "0xfB85B9Ec50560e302Ab106F1E2857d95132120D0"],
+    [AGIX_TOKEN, "0xb267deaace0b8c5fcb2bb04801a364e7af7da3f4"],
   ];
-  await sumTokens(balances, tokensAndOwners, block);
-  return balances;
+  return sumTokens2({ tokensAndOwners, block, })
 }
 
 //////////////////////////////////
@@ -123,39 +116,23 @@ async function staking(ts, block) {
 
 // Staking pools BNB
 
-async function stakingBNB(ts, EthBlock, chainBlocks) {
-  const balances = {};
-  const block = chainBlocks[chain];
+async function stakingBNB(ts, EthBlock, { bsc: block }) {
   const tokensAndOwners = [
     [SDAO_TOKEN_BNB, "0x79292c62f593e08d9b850b790b07e7a0903fd007"], // Unbonded
     [SDAO_TOKEN_BNB, "0x17de46760F4c18C26eEc36117C23793299F564A8"], // Bonded
   ];
-  const transform = await transformBscAddress();
-  await sumTokens(balances, tokensAndOwners, block, chain, transform);
-  return balances;
+  return sumTokens2({ tokensAndOwners, block, chain: 'bsc' })
 }
 
 // LP Pools BNB
 
-async function pool2BNB(ts, EthBlock, chainBlocks) {
-  const balances = {};
-  const block = chainBlocks[chain];
-
-  await addFundsInMasterChef({
-    balances,
-    block,
-    masterchef: "0x79292c62f593e08d9b850b790b07e7a0903fd007",
-    skipTokens: [SDAO_TOKEN_BNB],
-    chain,
-  });
-  await addFundsInMasterChef({
-    balances,
-    block,
-    masterchef: "0xBa1933bc47e6Ad050A4E4485F6f4b16b9CcdB806",
-    skipTokens: [],
-    chain,
-  });
-  return balances;
+async function pool2BNB(ts, EthBlock, { bsc: block }) {
+  const tokensAndOwners = [
+    ['0x6c805d2077025eaaa42fae7f764e61df42aadb14', "0xba1933bc47e6ad050a4e4485f6f4b16b9ccdb806"],
+    ['0x43b95976cf0929478bc13332c9cd2d63bf060976', "0x79292c62f593e08d9b850b790b07e7a0903fd007"],
+    ['0x3d12e4381901a6b94438758b90881cb03f10b01e', "0x79292c62f593e08d9b850b790b07e7a0903fd007"],
+  ];
+  return sumTokens2({ tokensAndOwners, block, chain: 'bsc' })
 }
 
 module.exports = {
@@ -170,3 +147,8 @@ module.exports = {
     pool2: pool2BNB,
   },
 };
+
+const abis = {
+  totalForges: {"inputs":[],"name":"totalForges","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  forgeInfo: {"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"forgeInfo","outputs":[{"internalType":"bool","name":"isEth","type":"bool"},{"internalType":"address","name":"contributionToken","type":"address"},{"internalType":"uint256","name":"dynasetLp","type":"uint256"},{"internalType":"uint256","name":"totalContribution","type":"uint256"},{"internalType":"uint256","name":"minContribution","type":"uint256"},{"internalType":"uint256","name":"maxContribution","type":"uint256"},{"internalType":"uint256","name":"maxCap","type":"uint256"},{"internalType":"uint256","name":"contributionPeriod","type":"uint256"},{"internalType":"bool","name":"withdrawEnabled","type":"bool"},{"internalType":"bool","name":"depositEnabled","type":"bool"},{"internalType":"bool","name":"forging","type":"bool"},{"internalType":"uint256","name":"nextForgeContributorIndex","type":"uint256"}],"stateMutability":"view","type":"function"},
+}
