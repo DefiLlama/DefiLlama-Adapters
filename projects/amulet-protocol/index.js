3e97db1e994b52ed4f1869eefdec97fdb65fdbb0
yym-mtrlabs <yumeng@mtrlabs.com>
2022-10-19 15:54:50 +0800

    update tvl calculation for amulet-protocol

--- projects/amulet-protocol/index.js
+++ projects/amulet-protocol/index.js
@@ -3,24 +3,6 @@ const { PublicKey } = require("@solana/web3.js")
 const { getConnection } = require("../helper/solana");
 
 async function tvl() {
-    const connection = getConnection();
-    const metadataState = await connection.getAccountInfo(new PublicKey("DEg9mNnxwqJQiYz2mXpVJmq8Yyx14oZ2SaYpHS7mB2mx"));
-    const posSolVirtualAmount = metadataState.data.slice(129, 137);
-    posSolVirtualAmount.reverse();
-
-    const liqSolAmount = metadataState.data.slice(307, 315);
-    liqSolAmount.reverse();
-
-    const posState = await connection.getAccountInfo(new PublicKey("CaxYodGBcen2MVPSM1SKdAMHC2csnPSvLAZu2vkVT1H2"));
-    const totalActiveBalance = posState.data.slice(160, 168);
-    totalActiveBalance.reverse();
-
-    const accountList = ["632JbevHriKtPvxncKJYa5asT76Rm77Tnrvncscufusg", "3eXy94aq5sGN3pZiJBzM6LVP4W7vNPoNbyPkpqSwBn8u", "3snom88cEW4WtXH7pHEyYHr76oMVtYqAMdJ4BtBWWaC9"]
-    const accountSolBalances = await Promise.all(accountList.map(async(x) => new BN((await connection.getAccountInfo(new PublicKey(x))).lamports)))
-    const solAmount = [new BN(posSolVirtualAmount), new BN(liqSolAmount), new BN(totalActiveBalance), ...accountSolBalances]
-
-    const tvlSOL = solAmount.reduce((acc,x) => acc.add(x), new BN(0)).div(new BN(1000000000)).toNumber()
-
     const stakingInstanceState = await connection.getAccountInfo(new PublicKey("HNhPNHkp3RobeJzepNzyVyewtAaoF3QCCvtBTxKJVnRX"));
     const liqStakedAmtSOLAmount = stakingInstanceState.data.slice(203, 211);
     liqStakedAmtSOLAmount.reverse();
@@ -33,7 +15,6 @@ async function tvl() {
     const tvlAmtSOL = amtSOLAmount.reduce((acc,x) => acc.add(x), new BN(0)).div(new BN(1000000000)).toNumber()
 
     return {
-        'solana': tvlSOL,
         'amulet-staked-sol': tvlAmtSOL
     }
 }
@@ -43,5 +24,5 @@ module.exports = {
     solana:{
         tvl
     },
-    methodology: `Amulet provides solution which allows user to to earn rewards from underwriting covers and liquidity staking. SOL derivatives token (amtSOL) will be used to underwrite covers, SOL will be used to delegate to validators. Hence, the amount of SOL and SOL derivatives staked are counted as our TVL which the value are calculated based on the price get from Coingecko.`
+    methodology: `Amulet provides solution which allows user to to earn rewards from underwriting covers with SOL derivatives token (amtSOL). Hence, the amount of SOL derivatives staked are counted as our TVL which the value are calculated based on the price get from Coingecko.`
 }
