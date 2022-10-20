1b1980e5d301c24890b8fb0eb54216ddf3bf0f80
dtmkeng <phumaisut@gmail.com>
2022-10-20 18:26:34 +0700

    update const chain name biswap

--- volumes/adapters/biswap/index.ts
+++ volumes/adapters/biswap/index.ts
@@ -1,7 +1,8 @@
+import { CHAIN } from "../../helper/chains";
 import { univ2Adapter } from "../../helper/getUniSubgraphVolume";
 
 export default univ2Adapter({
-    "bsc": "https://api.thegraph.com/subgraphs/name/biswapcom/exchange5"
+    [CHAIN.BSC]: "https://api.thegraph.com/subgraphs/name/biswapcom/exchange5"
 }, {
     factoriesName: "pancakeFactories",
     dayData: "pancakeDayData",
