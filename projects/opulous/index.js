cfe694c70bd4d83798e4e362099005d6cd998f28
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2022-10-20 20:29:11 -0300

    Add Opulous
    Adds Opulous in the Algorand blockchain

---
+++ projects/opulous/index.js
@@ -0,0 +1,38 @@
+const { lookupApplications, } = require("../helper/algorand");
+
+async function staking() {
+
+    //OPUL Staking Pool, expires on timestamp 1668988799 (November 20th)
+    const poolId = 843061415
+    let response = await lookupApplications(poolId)
+    var poolAmount
+
+    for (const y of response.application.params["global-state"]) {
+        let decodedKey = Buffer.from(y.key, 'base64').toString('binary')
+        if (decodedKey === "pool_size") {
+          //OPUL has 10 digits
+          poolAmount = y.value.uint / 10000000000
+        }
+      }
+
+    return {
+        'opulous': poolAmount
+    }
+
+}
+
+async function tvl() {
+    return staking()
+}
+
+module.exports = {
+        //timetravel: true,
+        //start: 1660827158,
+        methodology: `Counts the number of OPUL tokens locked in the staking pool.`,
+        algorand: {
+            tvl,
+            staking: staking
+        }
+}
+
+// node test.js projects/opulous/index.js
