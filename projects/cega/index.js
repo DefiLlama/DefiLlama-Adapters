b594f477f06a1845ba6c85a8200e9a19883e41e1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-21 14:31:54 +0200

    cega: separate borrowed from tvl

--- projects/cega/index.js
+++ projects/cega/index.js
@@ -1,9 +1,12 @@
 const { Program } = require("@project-serum/anchor");
-const { getProvider, transformBalances } = require("../helper/solana");
+const { getProvider, transformBalances, sumTokens2, getTokenBalance } = require("../helper/solana");
 const sdk = require("@defillama/sdk");
 const idl = require("./idl.json");
 
-async function tvl() {
+const vaultMintAuthority = '4nhbsUdKEwVQXuYDotgdQHoMWW83GvjXENwLsf9QrRJT'
+const usdcToken = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
+
+async function borrowed() {
   const provider = getProvider();
   const programId = "3HUeooitcfKX1TSCx2xEpg2W31n6Qfmizu7nnbaEWYzs";
   const program = new Program(idl, programId, provider);
@@ -11,18 +14,21 @@ async function tvl() {
   const balances = {};
   accounts.forEach(({ account: i }) => {
     if (Buffer.from(i.productName).toString().trim().includes("test")) return;
-    sdk.util.sumSingleBalance(
-      balances,
-      i.underlyingMint.toString(),
-      +i.underlyingAmount / 1e6
-    );
+    sdk.util.sumSingleBalance(balances, i.underlyingMint.toString(), +i.underlyingAmount / 1e6);
   });
+  const usdcBalance = await getTokenBalance(usdcToken, vaultMintAuthority)
+  sdk.util.sumSingleBalance(balances, usdcToken, -1 * usdcBalance);
   return transformBalances({ tokenBalances: balances });
+}
+
+async function tvl() {
+  return sumTokens2({ owner: vaultMintAuthority, tokens: [usdcToken]})
 }
 
 module.exports = {
   timetravel: false,
   solana: {
     tvl,
+    borrowed,
   },
 };
