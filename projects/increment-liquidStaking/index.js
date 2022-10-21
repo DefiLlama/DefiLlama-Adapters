021f1d8dc2e64575362cafdfb3e1c37827d96719
Jing Li <swordc@126.com>
2022-10-21 23:34:46 +0800

    replace to tvl query script on chain

--- projects/increment-liquidStaking/index.js
+++ projects/increment-liquidStaking/index.js
@@ -1,18 +1,44 @@
 const { fetchURL } = require("../helper/utils");
 
 // increment liquid staking link: https://app.increment.fi/staking
+const axios = require("axios");
+
+let queryLiquidStakingTVLCode =
+"import DelegatorManager from 0xd6f80565193ad727\
+pub fun main(): UFix64 {\
+    return DelegatorManager.getTotalValidStakingAmount()\
+}";
+
+const queryCodeBase64 = Buffer.from(queryLiquidStakingTVLCode, 'utf-8').toString('base64');
+
 
 async function tvl() {
-    const { data: tvls } = await fetchURL(
-        "https://app.increment.fi/info/tvl"
-    );
-    return tvls.LiquidStakingTVL;
+    try {
+        const response = await axios({
+            url: "https://rest-mainnet.onflow.org/v1/scripts",
+            method: 'post',
+            headers: { "content-type": "application/json" },
+            data: {
+                "script": queryCodeBase64
+            }
+        });
+        let resEncoded = response.data;
+        // resString = {"type":"UFix64","value":"96014.17736711"}
+        let resString = Buffer.from(resEncoded, 'base64').toString('utf-8');
+        let resJson = JSON.parse(resString);
+        let flowTokenTVL = Number(resJson.value);
+        
+        let resPrice = await fetchURL("https://www.binance.com/api/v3/ticker/price?symbol=FLOWUSDT");
+        let flowPrice = Number(resPrice.data.price);
+
+        return flowTokenTVL * flowPrice;
+    } catch (error) {
+        throw new Error(`Couln't query scripts of increment liquid staking for flow chain`, error);
+    }
 }
+
 async function fetch() {
-    const { data: tvls } = await fetchURL(
-        "https://app.increment.fi/info/tvl"
-    );
-    return tvls.LiquidStakingTVL;
+    return await tvl();
 }
 
 module.exports = {
