561c5bd76e2a7f5ad35286e920ee04955cffbc81
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-22 14:58:59 +0200

    code refactor

--- projects/stlos-liquid-staking/index.js
+++ projects/stlos-liquid-staking/index.js
@@ -1,22 +1,21 @@
 const sdk = require("@defillama/sdk");
 const sTlosAbi = require("./sTlos.json");
-const { ethers } = require("ethers");
 
 const sTLOS = "0xb4b01216a5bc8f1c8a33cd990a1239030e60c905";
 
-async function tvl(timestamp, block, chainBlocks) {
+async function tvl(timestamp, _, { telos: block }) {
     const pooledTLOS = await sdk.api.abi.call({
         target: sTLOS,
         abi: sTlosAbi.totalAssets,
-        chain: "telos",
+        chain: "telos", block,
     });
-    
+
     return {
-        telos: ethers.utils.formatEther(pooledTLOS.output)
+        telos: pooledTLOS.output / 1e18
     };
 }
 
-module.exports={
+module.exports = {
     telos: {
         tvl,
     },
