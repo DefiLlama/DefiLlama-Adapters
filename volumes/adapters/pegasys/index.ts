af2a4dab68bf34c3f6facf74a6fa49f97e930090
dtmkeng <phumaisut@gmail.com>
2022-10-22 09:40:57 +0700

    update subgraph

--- volumes/adapters/pegasys/index.ts
+++ volumes/adapters/pegasys/index.ts
@@ -2,7 +2,7 @@ import { CHAIN } from "../../helper/chains";
 import { univ2Adapter } from "../../helper/getUniSubgraphVolume";
 
 export default univ2Adapter({
-    [CHAIN.SYSCOIN]: "https://graph.pegasys.exchange/subgraphs/name/pollum-io/pegasys"
+    [CHAIN.SYSCOIN]: "https://graph.pegasys.finance/subgraphs/name/pollum-io/pegasys"
 }, {
     factoriesName: "pegasysFactories",
     dayData: "pegasysDayData",
