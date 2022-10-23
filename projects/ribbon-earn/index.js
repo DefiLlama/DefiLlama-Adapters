8c243acceb0b0a6ef040718c2c812e906c2cbce0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-23 15:55:16 +0200

    ribbon-earn: count amount lent out to MM as borrowed

--- projects/ribbon-earn/index.js
+++ projects/ribbon-earn/index.js
@@ -1,11 +1,13 @@
 const sdk = require("@defillama/sdk")
 const abi = require("../ribbon/abi.json")
+const { sumTokensExport } = require('../helper/unwrapLPs');
+const { default: BigNumber } = require("bignumber.js");
 
 // Ribbon Earn vaults
 const rearnUSDC = "0x84c2b16FA6877a8fF4F3271db7ea837233DFd6f0";
 
 // Ethereum Assets
-const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
 async function addVaults({ balances, chain, vaults, block, transformAddress = a => a }) {
   const { output: balanceRes } = await sdk.api.abi.multiCall({
     abi: abi.totalBalance,
@@ -16,7 +18,14 @@ async function addVaults({ balances, chain, vaults, block, transformAddress = a 
   balanceRes.forEach((data, i) => sdk.util.sumSingleBalance(balances, transformAddress(vaults[i][0]), data.output))
 }
 
-async function tvl(_, block) {
+async function getUSDCOnVault(block) {
+  const { output: usdcBalance } = await sdk.api.abi.call({
+    target: usdc, abi: 'erc20:balanceOf', params: rearnUSDC, block,
+  })
+  return usdcBalance
+}
+
+async function borrowed(_, block) {
   const balances = {};
   const vaults = [
     // ribbon earn
@@ -24,11 +33,13 @@ async function tvl(_, block) {
   ]
   
   await addVaults({ balances, block, vaults, })
+  sdk.util.sumSingleBalance(balances, usdc, BigNumber(-1 * (await getUSDCOnVault(block))).toFixed(0))
   return balances
 }
 
 module.exports = {
   ethereum: {
-    tvl,
+    tvl: sumTokensExport({ owner: rearnUSDC, tokens: [usdc]}),
+    borrowed,
   },
 }
