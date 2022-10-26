d6a8e07bd262b6953eec2afe4a85862a57e6a833
headline-dev <aaron@headline-inc.com>
2022-10-26 18:11:18 +0000

    silo init 2

--- projects/silodefi/index.js
+++ projects/silodefi/index.js
@@ -1,38 +1,43 @@
-const call = 913906096
+async function tvl() {
+  const call = 913906096;
+ 
+  const put = 913951447;
 
-const put = 913951447
+  const apps = [call,put]
 
+  const usdc = 31566704;
 
-async function getAccounts(apps) {
   const path = `https://algoindexer.algoexplorerapi.io/v2/accounts?application-id=`;
-  let localStates = []
+  let tvlAlgo = 0;
+  let tvlUSDC = 0;
 
   for (let i = 0; i < apps.length; i++) {
-    let data = await fetch(path + apps[i])
-    let dataJSON = await data.json()
+    let data = await fetch(path + apps[i]);
+    let dataJSON = await data.json();
     //console.log(dataJSON.accounts)
-    dataJSON.accounts.forEach(account => {
-      account["apps-local-state"].forEach((state) => {
-        if (state?.id === apps[i]) {
-          if (state["key-value"]) {
-            state["key-value"].forEach((entry) => {
-              if (entry.key) {
-                let key = window.atob(entry.key)
-                if (key) {
-                  localStates.push(key + " " + entry.value.uint)
-                }
+    dataJSON.accounts.forEach((account) => {
+      if (account["created-assets"]) {
+        account["created-assets"].forEach((asset) => {
+          let name = asset.params["unit-name"];
+          if (name === "SILO") {
+            tvlAlgo += account.amount;
+            account.assets.forEach((asset) => {
+              if (asset["asset-id"] === usdc) {
+                tvlUSDC += asset.amount;
               }
-            })
+            });
           }
-
-        }
-      })
-    })
-
-
+        });
+      }
+    });
   }
-  console.log(localStates)
-  return localStates
-}
 
-getAccounts([call, put])
+  let data2 = await fetch("https://price.algoexplorerapi.io/price/algo-usd");
+  let data2JSON = await data2.json();
+  let price = data2JSON.price;
+  let total = tvlAlgo * price + tvlUSDC;
+  console.log(total);
+  return total;
+}
+//example usage:
+tvl();
