667d027d4c4b3ff19698749b991b91e352cb1779
L0stic <k0ndratev.da@yandex.ru>
2022-10-26 20:25:28 +0300

    Add Milkomeda A1 to Blueshift protocol

--- projects/blueshift/index.js
+++ projects/blueshift/index.js
@@ -3,45 +3,40 @@ const { default: BigNumber } = require('bignumber.js');
 const { getChainTransform } = require('../helper/portedTokens');
 
 const abi = require('./abi.json');
+const config = require("./config.json");
 
 
-const REGISTRY_CONTRACT = '0x83E384d119adA05195Caca26396B8f56fdDA1c91';
-const MANUAL_POOL_CONTRACT = '0xA4f0e3C80C77b347250B9D3999478E305FF814A4';
-// temporary solution using BLUES/ADA price
-const BLUESHIFT_INDEX_PORTFOLIO = '0xB2A76Ce2D5eD32aD7F8B93a1098C1Fee473e27bA';
-
-
-async function staking(timestamp, block, chainBlocks) {
+async function staking(chain, timestamp, block, chainBlocks) {
   const balances = {};
-  const transform = await getChainTransform('milkomeda');
+  const transform = await getChainTransform(chain);
 
   const value = (await sdk.api.abi.call({
     abi: abi.BlueshiftEarning.getAccDeposit,
-    chain: 'milkomeda',
-    target: MANUAL_POOL_CONTRACT,
+    chain: chain,
+    target: config.manualPool[chain],
     params: [],
-    block: chainBlocks['milkomeda'],
+    block: chainBlocks[chain],
   })).output;
 
   const tokenAddress = (await sdk.api.abi.call({
     abi: abi.BlueshiftEarning.getToken,
-    chain: 'milkomeda',
-    target: MANUAL_POOL_CONTRACT,
+    chain: chain,
+    target: config.manualPool[chain],
     params: [],
-    block: chainBlocks['milkomeda'],
+    block: chainBlocks[chain],
   })).output;
 
 
-  // temporary solution using BLUES/ADA price
+  // temporary solution using BLUES/(ADA or ALGO) price
   const portfolios = (await sdk.api.abi.call({
     abi: abi.BlueshiftRegistry.getPortfolios,
-    chain: 'milkomeda',
-    target: REGISTRY_CONTRACT,
+    chain: chain,
+    target: config.registry[chain],
     params: [],
-    block: chainBlocks['milkomeda'],
+    block: chainBlocks[chain],
   })).output;
 
-  const bluesPortfolio = portfolios.filter(portfolio => portfolio.contractAddress === BLUESHIFT_INDEX_PORTFOLIO)[0];
+  const bluesPortfolio = portfolios.filter(portfolio => portfolio.contractAddress === config.blueshiftPortfolio[chain])[0];
   if (!bluesPortfolio) {
     return balances;
   }
@@ -52,29 +47,36 @@ async function staking(timestamp, block, chainBlocks) {
   const valueInBaseToken = BigNumber(value).multipliedBy(tokenPrice).div(baseTokenPrice);
 
   sdk.util.sumSingleBalance(balances, transform(baseTokenAddress), valueInBaseToken.toNumber());
+
+  if (balances["algorand"] !== undefined) {
+    balances["algorand"] = BigNumber(balances["algorand"]).div(10**18);
+  }
   // ----------------------------------------
 
   // CoinGecko solution
   // sdk.util.sumSingleBalance(balances, transform(tokenAddress), value);
-
   return balances;
 }
 
-async function tvl(timestamp, block, chainBlocks) {
+async function tvl(chain, timestamp, block, chainBlocks) {
   const balances = {};
-  const transform = await getChainTransform('milkomeda');
+  const transform = await getChainTransform(chain);
 
   const portfolios = (await sdk.api.abi.call({
     abi: abi.BlueshiftRegistry.getPortfolios,
-    chain: 'milkomeda',
-    target: REGISTRY_CONTRACT,
+    chain: chain,
+    target: config.registry[chain],
     params: [],
-    block: chainBlocks['milkomeda'],
+    block: chainBlocks[chain],
   })).output;
 
   for (let portfolio of portfolios) {
     const value = portfolio.totalValue;
     sdk.util.sumSingleBalance(balances, transform(portfolio.baseTokenAddress), value);
+  }
+
+  if (balances["algorand"] !== undefined) {
+    balances["algorand"] = BigNumber(balances["algorand"]).div(10**18);
   }
 
   return balances;
@@ -84,9 +86,14 @@ module.exports = {
   timetravel: true,
   misrepresentedTokens: true,
   methodology: 'Accumulates TVL of all Blueshift portfolios calculated in base tokens. Adds TVL of BLUES tokens staked in Blueshift yield pools.',
-  start: 2023331,
   milkomeda: {
-    staking,
-    tvl
+    start: 2023331,
+    staking: (timestamp, block, chainBlocks) => staking('milkomeda', timestamp, block, chainBlocks),
+    tvl: (timestamp, block, chainBlocks) => tvl('milkomeda', timestamp, block, chainBlocks)
+  },
+  milkomeda_a1: {
+    start: 1300,
+    // staking: (timestamp, block, chainBlocks) => staking('milkomeda_a1', timestamp, block, chainBlocks),
+    tvl: (timestamp, block, chainBlocks) => tvl('milkomeda_a1', timestamp, block, chainBlocks)
   }
 };
