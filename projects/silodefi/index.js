2bc3451b195dbee69e9f7d066e2112ce3890832f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-27 11:10:09 +0200

    [silodefi] code refactor

--- projects/silodefi/index.js
+++ projects/silodefi/index.js
@@ -1,43 +1,26 @@
+const { searchAccountsAll } = require('../helper/algorand')
+const sdk = require('@defillama/sdk')
+
 async function tvl() {
+  const balances = {}
   const call = 913906096;
- 
   const put = 913951447;
-
-  const apps = [call,put]
-
   const usdc = 31566704;
+  let accounts = await Promise.all([call, put].map(appId => searchAccountsAll({ appId })))
+  accounts = accounts.flat().filter(i => i["created-assets"] && 
+  i["created-assets"].some(asset => asset.params["unit-name"] === 'SILO')
+  )
+  accounts.forEach(a => {
+    sdk.util.sumSingleBalance(balances, 'algorand', a.amount/1e6)
+    a.assets.filter(i => i['asset-id'] === usdc).forEach(i => sdk.util.sumSingleBalance(balances, 'usd-coin', i.amount/1e6))
 
-  const path = `https://algoindexer.algoexplorerapi.io/v2/accounts?application-id=`;
-  let tvlAlgo = 0;
-  let tvlUSDC = 0;
-
-  for (let i = 0; i < apps.length; i++) {
-    let data = await fetch(path + apps[i]);
-    let dataJSON = await data.json();
-    //console.log(dataJSON.accounts)
-    dataJSON.accounts.forEach((account) => {
-      if (account["created-assets"]) {
-        account["created-assets"].forEach((asset) => {
-          let name = asset.params["unit-name"];
-          if (name === "SILO") {
-            tvlAlgo += account.amount;
-            account.assets.forEach((asset) => {
-              if (asset["asset-id"] === usdc) {
-                tvlUSDC += asset.amount;
-              }
-            });
-          }
-        });
-      }
-    });
-  }
-
-  let data2 = await fetch("https://price.algoexplorerapi.io/price/algo-usd");
-  let data2JSON = await data2.json();
-  let price = data2JSON.price;
-  let total = tvlAlgo * price + tvlUSDC;
-  console.log(total);
-  return total;
+  })
+  return balances
 }
-//example usage:
-tvl();
+
+module.exports = {
+  timetravel: false,
+  algorand: {
+    tvl,
+  }
+}
