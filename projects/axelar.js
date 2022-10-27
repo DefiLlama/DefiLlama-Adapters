ca68d1099682be9729ca8a9856ab81a460d55eb9
Aditya Kumar Verma <aditya.verma@flipkart.com>
2022-10-27 19:42:24 +0530

    add fetch adapter for axelar

---
+++ projects/axelar.js
@@ -0,0 +1,18 @@
+const retry = require('async-retry')
+const axios = require("axios");
+const BigNumber = require("bignumber.js");
+
+async function fetch() {
+    var res = await retry(async bail => await axios.get('https://api.axelarscan.io/cross-chain/tvl'))
+
+    var tvl = 0;
+    for (const asset of res.data.data) {
+        console.log(asset.total)
+        tvl = tvl + (parseFloat(asset.total)) * (parseFloat(asset.price))
+    }
+    return new BigNumber(tvl);
+}
+
+module.exports = {
+    fetch
+}
