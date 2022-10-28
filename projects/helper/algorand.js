fc347d8b52c31649a5e123c52c58831a25a7f0e8
0xngmi <80541789+0xngmi@users.noreply.github.com>
2022-10-28 19:53:51 +0100

    Revert "Refactor Algofi adapter (#4459)"
    This reverts commit 9e5b98181fa9d8b090aca13c9c4b8239d07e0614.

--- projects/helper/algorand.js
+++ projects/helper/algorand.js
@@ -7,11 +7,8 @@ const { fixBalancesTokens } = require('../helper/tokenMapping')
 const { getFixBalancesSync } = require('../helper/portedTokens')
 const sdk = require('@defillama/sdk');
 const { default: BigNumber } = require('bignumber.js');
-const stateCache = {}
-const accountCache = {}
-const assetCache = {}
 
-const geckoMapping = fixBalancesTokens.algorand
+
 const axiosObj = axios.create({
   baseURL: 'https://algoindexer.algoexplorerapi.io',
   timeout: 300000,
@@ -74,18 +71,13 @@ async function sumTokens({ owner, owners = [], tokens = [], token, balances = {}
 }
 
 async function getAssetInfo(assetId) {
-  if (!assetCache[assetId]) assetCache[assetId] = _getAssetInfo()
-  return assetCache[assetId]
-
-  async function _getAssetInfo() {
-    const { data: { asset } } = await axiosObj.get(`/v2/assets/${assetId}`)
-    const reserveInfo = await getAccountInfo(asset.params.reserve)
-    const assetObj = { ...asset.params, ...asset, reserveInfo, }
-    assetObj.circulatingSupply = assetObj.total - reserveInfo.assetMapping[assetId].amount
-    assetObj.assets = { ...reserveInfo.assetMapping }
-    delete assetObj.assets[assetId]
-    return assetObj
-  }
+  const { data: { asset } } = await axiosObj.get(`/v2/assets/${assetId}`)
+  const reserveInfo = await getAccountInfo(asset.params.reserve)
+  const assetObj = { ...asset.params, ...asset, reserveInfo, }
+  assetObj.circulatingSupply = assetObj.total - reserveInfo.assetMapping[assetId].amount
+  assetObj.assets = { ...reserveInfo.assetMapping }
+  delete assetObj.assets[assetId]
+  return assetObj
 }
 
 async function resolveTinymanLp({ balances, lpId, unknownAsset, blacklistedTokens, }) {
@@ -113,77 +105,32 @@ async function resolveTinymanLp({ balances, lpId, unknownAsset, blacklistedToken
 }
 
 async function getAccountInfo(accountId) {
-  if (!accountCache[accountId]) accountCache[accountId] = _getAccountInfo()
-  return accountCache[accountId]
-
-  async function _getAccountInfo() {
-    const { data: { account } } = await axiosObj.get(`/v2/accounts/${accountId}`)
-    if (account.amount) account.assets.push({ amount: account.amount, 'asset-id': '1', })
-    account.assetMapping = {}
-    account.assets.forEach(i => {
-      i['asset-id'] = '' + i['asset-id']
-      account.assetMapping[i['asset-id']] = i
-    })
-    return account
-  }
+  const { data: { account } } = await axiosObj.get(`/v2/accounts/${accountId}`)
+  account.assetMapping = {}
+  account.assets.forEach(i => {
+    i['asset-id'] = '' + i['asset-id']
+    account.assetMapping[i['asset-id']] = i
+  })
+  return account
 }
 
 const tokens = {
   usdc: 31566704,
   goUsd: 672913181,
   usdcGoUsdLp: 885102318,
-  gard: 684649988,
 }
 
 // store all asset ids as string
 Object.keys(tokens).forEach(t => tokens[t] = '' + tokens[t])
 
-async function getAppGlobalState(marketId) {
-  if (!stateCache[marketId]) stateCache[marketId] = _getAppGlobalState()
-  return stateCache[marketId]
-
-  async function _getAppGlobalState() {
-    let response = await lookupApplications(marketId);
-    let results = {}
-    response.application.params["global-state"].forEach(x => {
-      let decodedKey = Buffer.from(x.key, "base64").toString("binary")
-      results[decodedKey] = x.value.uint
-    })
-
-    return results
-  }
-}
-
-async function getPriceFromAlgoFiLP(lpAssetId, unknownAssetId) {
-  let lpInfo = await getAssetInfo(lpAssetId)
-  if (lpInfo['unit-name'] !== 'AF-POOL')  throw new Error('No, this is not an AlgoFi LP')
-
-  const unknownAssetQuantity = lpInfo.reserveInfo.assets.find(i => i['asset-id'] === ''+unknownAssetId).amount
-  for (const i of lpInfo.reserveInfo.assets) {
-    const id = i['asset-id']
-    if (geckoMapping[id]) {
-      const { coingeckoId, decimals } = geckoMapping[id]
-      return {
-        price: i.amount / unknownAssetQuantity,
-        geckoId: coingeckoId, 
-        decimals,
-      }
-    }
-  }
-  
-  throw new Error('Not mapped with any whitelisted assets')
-}
-
 module.exports = {
   tokens,
-  getAssetInfo: withLimiter(getAssetInfo),
   searchAccountsAll,
+  getAssetInfo,
   getAccountInfo,
   sumTokens,
   getApplicationAddress,
   lookupApplications: withLimiter(lookupApplications),
   lookupAccountByID: withLimiter(lookupAccountByID),
   searchAccounts: withLimiter(searchAccounts),
-  getAppGlobalState: getAppGlobalState,
-  getPriceFromAlgoFiLP,
 }
