866156328803205237d4bf9a565a6c5cd4306012
wufeng <phpwufeng@163.com>
2022-10-28 14:57:47 +0800

    Add MetaTdex

---
+++ projects/metatdex/index.js
@@ -0,0 +1,31 @@
+const retry = require('async-retry');
+const axios = require("axios");
+
+async function fetch(chainId) {
+    return (await retry(async () => 
+        await axios.get(`http://public.tdex.cz/tvl?chain_id=${chainId}`)
+    )).data.result
+    .map(p => p.tvl)
+    .reduce((a, b) => a + parseFloat(b), 0);
+}; 
+
+async function heco() { 
+    return (await fetch(128));
+};
+async function bsc() { 
+    return (await fetch(56));
+};
+async function total() {
+    return (await fetch(128)) + (await fetch(56));
+};
+
+module.exports = {
+    timetravel: false,
+    heco: {
+        fetch: heco
+    },
+    bsc: {
+        fetch: bsc
+    },
+    fetch: total
+};
