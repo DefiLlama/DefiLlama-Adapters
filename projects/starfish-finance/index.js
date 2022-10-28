467dc6233ab0b13dbada6b1cc1041e19a41851c5
europa-starfish <europa@starfish.finance>
2022-10-28 15:15:45 +0800

    refactor: remove commentted

--- projects/starfish-finance/index.js
+++ projects/starfish-finance/index.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 const retry = require('async-retry')
 const BigNumber = require("bignumber.js");
 
-// const chef = "0x2639779d6ca9091483a2a7b9a1fe77ab83b90281";
 const client = axios.create({
   baseURL: 'https://api.starfish.finance/v1'
 })
