ad0adbdad0024fb97d074567c8cdfca8c64b2a20
Vitali <vitali@undercurrent.tech>
2022-11-01 08:30:08 +0400

    Add v1.1 vaults.

--- projects/algorai-finance/constants.js
+++ projects/algorai-finance/constants.js
@@ -3,6 +3,7 @@
  * @type {[{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number},{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number},{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number}]}
  */
 const vaults = [
+    // v1.0
     {
         vaultID: 878144513,
         depositAssetID: 0,
@@ -12,6 +13,21 @@ const vaults = [
     },
     {
         vaultID: 878140320,
+        depositAssetID: 31566704,
+        assetDecimals: 6,
+        coingecko: 'usd-coin',
+        stable: true
+    },
+    // v1.1
+    {
+        vaultID: 919531421,
+        depositAssetID: 0,
+        assetDecimals: 6,
+        coingecko: 'algorand',
+        stable: false
+    },
+    {
+        vaultID: 919539218,
         depositAssetID: 31566704,
         assetDecimals: 6,
         coingecko: 'usd-coin',
