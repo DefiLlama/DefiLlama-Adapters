c2de6335d471592fb45c57e42b579b89eee8354b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-01 15:30:08 +0100

    cleanup crolend

--- projects/crolend/index.js
+++ projects/crolend/index.js
@@ -1,16 +1,5 @@
-const {compoundExports} = require('../helper/compound')
-
-const wCRO = "0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23";
-const cCRO = "0xff024211741059a2540b01f5Be2e75fC0c1b3d82";
+const { compoundExports } = require('../helper/compound')
 
 module.exports = {
-    cronos: {
-        ...compoundExports(
-            "0x30dF4C58ADaf1FcF388B7Bf775840DEc086dcB98",
-            "cronos",
-            cCRO,
-            wCRO,
-            addr => `cronos:${addr}`,
-        ),
-    }
+    cronos: compoundExports("0x30dF4C58ADaf1FcF388B7Bf775840DEc086dcB98", "cronos",)
 }
