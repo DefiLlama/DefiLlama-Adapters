e6f66dbbe707b001a7cdee59f9cdb89e8148f99b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-01 15:15:36 +0100

    add hallmark

--- projects/armorfinance/index.js
+++ projects/armorfinance/index.js
@@ -43,4 +43,7 @@ module.exports = {
     tvl: () =>  ({}), // Project rebranded. arNXM is now being managed by EASE.
     staking: staking('0x5afedef11aa9cd7dae4023807810d97c20791dec', '0x1337def16f9b486faed0293eb623dc8395dfe46a')
   },
+  hallmarks: [
+    [Math.floor(new Date('2022-10-31')/1e3), 'Project rebranded as Ease. Stopped doublecounting Nexus Mutual tvl'],
+  ],
 };
