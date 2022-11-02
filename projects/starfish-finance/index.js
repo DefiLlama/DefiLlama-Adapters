9d2fe6edecb76dc68e9c0d8825489dc64bd46865
europa-starfish <europa@starfish.finance>
2022-11-02 13:54:01 +0800

    feat: calculate without ext api

--- projects/starfish-finance/index.js
+++ projects/starfish-finance/index.js
@@ -1,27 +1,32 @@
-const sdk = require("@defillama/sdk");
-const axios = require("axios");
 const retry = require('async-retry')
 const BigNumber = require("bignumber.js");
-
-const client = axios.create({
-  baseURL: 'https://api.starfish.finance/v1'
-})
-
-async function fetchEthTvl() {
-  const { data } = await retry(await client.get('/starfish/ethereum/tvl'))
-  return new BigNumber(data).toFixed(2)
+const ethers = require("ethers");
+const { staking } = require("../helper/staking.js");
+const { fetchSeanPrice } = require("./api/coingecko");
+const SEAN_STAKING_ABI = require("./abis/SeanStaking.json");
+const { SupportedChain, Providers } = require("./providers");
+ 
+const addresses = {
+  [SupportedChain.Astar]: {
+    seanStaking: '0xa86dc743efBc24AF4c1FC5d150AaDb4DCF52c868',
+    seanToken: '0xEe8138B3bd03905cF84aFE10cCD0dCcb820eE08E'
+  }
 }
 
 async function fetchAstarTvl() {
-  const { data } = await retry(await client.get('/starfish/astar/tvl'))
-  return new BigNumber(data).toFixed(2)
+  const seanPrice = await retry(await fetchSeanPrice())
+  const SeanStakingContract = new ethers.Contract(addresses.astar.seanStaking, SEAN_STAKING_ABI)
+
+  const stakedSean = await (
+    await SeanStakingContract.internalSeanBalance()
+  ).div(BigNumber.from(10).pow(await SeanContract.decimals()));
+  
+  return stakedSean.toNumber() * seanPrice;
 }
 
 module.exports = {
-  ethereum: {
-    fetch: fetchEthTvl,
-  },
   astar: {
-    fetch: fetchAstarTvl,
+    tvl: fetchAstarTvl,
+    staking: staking(addresses[SupportedChain.Astar].seanStaking, addresses[SupportedChain.Astar].seanToken, "astar", 'starfish-finance', 18)
   }
 };
