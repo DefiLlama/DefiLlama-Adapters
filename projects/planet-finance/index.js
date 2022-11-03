0510db752372c6f680d87833cbf4e1b233e527a9
defininja <hello@planet.finance>
2022-11-04 01:50:47 +0530

    new farm address added

--- projects/planet-finance/index.js
+++ projects/planet-finance/index.js
@@ -1,5 +1,6 @@
 const sdk = require("@defillama/sdk");
 const abi = require("./abi.json");
+const abiNew = require("./abiNew.json");
 const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
 const { staking } = require("../helper/staking.js");
 const { getPoolInfo } = require("../helper/masterchef.js");
@@ -16,13 +17,37 @@ const replacements = {
     "0x2170ed0880ac9a755fd29b2688956bd959f933f8", //beltETH->
 };
 
+const gammaInifnityVaultStratAddress = "0xA160F381EA8B3a4EcF32273b7a73393fCdA57aa7";
+const gGammaAddress = "0xCD221e1504442671671D3330CB8E916a5EDc3FC7";
+
+const aquaInifnityVaultStratAddress = "0xddd0626BB795BdF9CfA925da5102eFA5E7008114";
+const gAquaAddress = "0x2f5d7A9D8D32c16e41aF811744DB9f15d853E0A5";
+
+
+
 // liquidity pools
 async function tvl(timestamp, ethBlock, chainBlocks) {
+  const gGammaTokenExchangeRate = (await sdk.api.abi.call({
+    target: gGammaAddress,
+    abi: abi.exchangeRateStored,
+    block: chainBlocks.bsc,
+    chain: "bsc"
+  })).output;
+
+  const gAquaTokenExchangeRate = (await sdk.api.abi.call({
+    target: gAquaAddress,
+    abi: abi.exchangeRateStored,
+    block: chainBlocks.bsc,
+    chain: "bsc"
+  })).output;
+  
+  console.log('gTokenExchangeRate', gGammaTokenExchangeRate, gAquaTokenExchangeRate);
+
   let balances = {};
   const lps = [];
   const transform = await transformBscAddress();
 
-  const [poolInfo1, poolInfo2] = await Promise.all([
+  const [poolInfo1, poolInfo2, poolInfo3] = await Promise.all([
     await getPoolInfo(
       "0x0ac58Fd25f334975b1B61732CF79564b6200A933",
       chainBlocks.bsc,
@@ -35,9 +60,14 @@ async function tvl(timestamp, ethBlock, chainBlocks) {
       "bsc",
       abi.poolInfo
     ),  // newFarm
+    await getPoolInfo(
+      "0x9EBce8B8d535247b2a0dfC0494Bc8aeEd7640cF9",
+      chainBlocks.bsc,
+      "bsc",
+      abiNew.poolInfo
+    ),  // newFarm
   ]);
-  const allPoolInfos = poolInfo1.concat(poolInfo2);
-
+  const allPoolInfos = poolInfo1.concat(poolInfo2).concat(poolInfo3);
   const [poolTvl, wantSymbol] = await Promise.all([
     sdk.api.abi.multiCall({
       calls: allPoolInfos.map(p => ({
@@ -57,6 +87,8 @@ async function tvl(timestamp, ethBlock, chainBlocks) {
     })
   ]);
 
+  //console.log('poolTvl', poolTvl.output);
+
   for (let i = 0; i < poolTvl.output.length; i++) {
     if (wantSymbol.output[i].output.endsWith("LP")) {
       lps.push({
@@ -67,13 +99,35 @@ async function tvl(timestamp, ethBlock, chainBlocks) {
     } else {
       const addr = replacements[allPoolInfos[i].output.want]
         ?? allPoolInfos[i].output.want;
+      //const addr = allPoolInfos[i].output.want;
+      let bal = poolTvl.output[i].output
+
+      //balance in gamma infinity vault
+      if(allPoolInfos[i].output.strat.toLowerCase() == gammaInifnityVaultStratAddress.toLowerCase()) {
+        bal = (
+          (poolTvl.output[i].output * gGammaTokenExchangeRate) /
+          1e18
+        ).toLocaleString("fullwide", { useGrouping: false });
+        console.log('bal', bal);
+      }
+
+      //balance in aqua infinity vault
+      if(allPoolInfos[i].output.strat.toLowerCase() == aquaInifnityVaultStratAddress.toLowerCase()) {
+        bal = (
+          (poolTvl.output[i].output * gAquaTokenExchangeRate) /
+          1e18
+        ).toLocaleString("fullwide", { useGrouping: false });
+        console.log('bal', bal);
+      }
+
       sdk.util.sumSingleBalance(
         balances,
         transform(addr),
-        poolTvl.output[i].output
+        bal
       );
     };
   };
+  console.log('balances', balances);
 
   await unwrapUniswapLPs(
     balances,
@@ -82,7 +136,6 @@ async function tvl(timestamp, ethBlock, chainBlocks) {
     "bsc",
     transform
   );
-
   return balances;
 };
 
@@ -93,7 +146,7 @@ module.exports = {
   bsc: {
     tvl,
     staking: staking(
-      "0xb7eD4A5AF620B52022fb26035C565277035d4FD7",
+      "0x2f5d7A9D8D32c16e41aF811744DB9f15d853E0A5",
       "0x72B7D61E8fC8cF971960DD9cfA59B8C829D91991",
       "bsc",
     )
