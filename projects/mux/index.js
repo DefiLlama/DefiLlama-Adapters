847a5dc0b12632a5792d0ffce3b3bef4b6225d74
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-05 10:19:04 +0100

    code refactor

--- projects/mux/index.js
+++ projects/mux/index.js
@@ -1,8 +1,6 @@
-const {toUSDTBalances} = require('../helper/balances');
-const BigNumber = require("bignumber.js");
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
 const sdk = require("@defillama/sdk");
-const utils = require("../helper/utils");
 const abi = [
   {
     inputs: [],
@@ -181,92 +179,25 @@ const abi = [
   },
 ]
 
-const keys = [
-  {
-    ETH: "ethereum",
-    BTC: "bitcoin",
-    DAI: "dai",
-    USDC: "usd-coin",
-    'USDC.e': "usd-coin",
-    AVAX: "avalanche-2",
-    USDT: "tether",
-    fUSDT: "tether",
-    'USDT.e': "tether",
-    BUSD: "binance-usd",
-    'BUSD.e': "binance-usd",
-    BNB: "binancecoin",
-    FTM: "fantom",
-  },
-];
-
-const liquidityPoolContract = {
+const config = {
   arbitrum: '0x3e0199792Ce69DC29A0a36146bFa68bd7C8D6633',
   bsc: '0x855E99F768FaD76DD0d3EB7c446C0b759C96D520',
   avax: '0x0bA2e492e8427fAd51692EE8958eBf936bEE1d84',
   fantom: '0x2e81F443A11a943196c88afcB5A0D807721A88E6',
 }
 
-function hexToStr(hex) {
-  const trimedStr = hex.trim();
-  const rawStr = trimedStr.substr(0,2).toLowerCase() === "0x" ? trimedStr.substr(2) : trimedStr;
-  const len = rawStr.length;
-  if(len % 2 !== 0) {
-    return "";
-  }
-  let curCharCode;
-  const resultStr = [];
-  for(let i = 0; i < len;i = i + 2) {
-    curCharCode = parseInt(rawStr.substr(i, 2), 16);
-    if (curCharCode !== 0) {
-      resultStr.push(String.fromCharCode(curCharCode));
+module.exports = {
+  methodology: `This is the total value of all tokens in the MUXLP Pool. The liquidity pool consists of a token portfolio used for margin trading and third-party DEX mining.`,
+}
+
+
+Object.keys(config).forEach(chain => {
+  const owner = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, {[chain]: block}) => {
+      const { output } = await sdk.api.abi.call({ target: owner, chain, block, abi: abi.find(i => i.name === 'getAllAssetInfo') })
+      const tokens = output.map(i => i.tokenAddress)
+      return sumTokens2({ chain, block, owner, tokens})
     }
   }
-  return resultStr.filter(str => str !== '').join("");
-}
-
-async function tvl(chain) {
-  const { output } = await sdk.api.abi.call({ target: liquidityPoolContract[chain], chain: chain, abi: abi.find(i => i.name === 'getAllAssetInfo') })
-  const prices = (await utils.getPrices(keys)).data;
-  let tvl = new BigNumber(0)
-  output.map(token => {
-    const symbol = hexToStr(token.symbol)
-    const amount = new BigNumber(token.spotLiquidity).shiftedBy(-18)
-    const price = prices[keys[0][symbol]]?.usd || 0
-    tvl = tvl.plus(amount.times(price))
-  })
-
-  return toUSDTBalances(tvl)
-}
-
-async function arbitrum() {
-  return await tvl('arbitrum')
-}
-
-async function bsc() {
-  return await tvl('bsc')
-}
-
-async function fantom() {
-  return await tvl('fantom')
-}
-
-async function avax() {
-  return await tvl('avax')
-}
-
-module.exports = {
-  misrepresentedTokens: true,
-  methodology: `This is the total value of all tokens in the MUXLP Pool. The liquidity pool consists of a token portfolio used for margin trading and third-party DEX mining.`,
-  arbitrum: {
-    tvl: arbitrum,
-  },
-  bsc: {
-    tvl: bsc
-  },
-  fantom: {
-    tvl: fantom
-  },
-  avax: {
-    tvl: avax
-  }
-}
+})
