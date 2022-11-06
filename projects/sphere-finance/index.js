11855c5a5d642fbc3fe0ceebf9414c5fe2166c0f
EthanHCahill <117314339+EthanHCahill@users.noreply.github.com>
2022-11-06 22:33:28 +1000

    Add Staked Tokens in S.P.H.E.R.E GAMES.

--- projects/sphere-finance/index.js
+++ projects/sphere-finance/index.js
@@ -1,15 +1,34 @@
 const utils = require("../helper/utils");
+const { ohmTvl } = require('../helper/ohm')
 
-function fetch() {
+function ohmTvlMultiTreasuries(treasuries, treasuryTokens, chain, stakingAddress, stakingToken, transformOriginal, fix = id=>id, tokenOnCoingecko = true) {
+  // Accumulate tvl for multiple treasuries, executing simply ohmTvl
+  const tvl_per_treasury = treasuries.map(treasury =>
+      ohmTvl(treasury, treasuryTokens, chain, stakingAddress, stakingToken, transformOriginal, fix, tokenOnCoingecko)
+  )
+
+  // Edit TVL of object to be the cumulative tvl
+  const tvl_object = tvl_per_treasury[0]
+  const tvls = tvl_per_treasury.map(o => o[chain].tvl)
+  tvl_object[chain].tvl = sdk.util.sumChainTvls(tvls)
+  return tvl_object
+}
+
+function sphereTVL() {
   return async () =>
     utils
       .fetchURL("https://spheretvl.simsalacrypto.workers.dev/")
       .then((d) => ({ "usd-coin": d.data["portfolio"]["net_worth"] }));
 }
 
+function stakedTokens() {
+  return ohmTvlMultiTreasuries(undefined, undefined, "polygon", "0x284eba456e27ec9d07a656ce7cf68f2c78578f2e", "0x62f594339830b90ae4c084ae7d223ffafd9658a7", undefined, undefined, true)
+}
+
 module.exports = {
   timetravel: false,
   polygon: {
-    tvl: fetch(),
+    tvl: sphereTVL(),
+    staking: stakedTokens(),
   },
 };
