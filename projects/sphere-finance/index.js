ea78a8da7148031541c1a7b1ed1fd0959f98a824
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-06 13:27:50 +0100

    minor fix

--- projects/sphere-finance/index.js
+++ projects/sphere-finance/index.js
@@ -5,6 +5,8 @@ const sphere_token = "0x62f594339830b90ae4c084ae7d223ffafd9658a7"
 const stakingAddress = "0x284eba456e27ec9d07a656ce7cf68f2c78578f2e" // Sphere Games StakePrizePool
 
 module.exports = {
+  timetravel: false,
+  misrepresentedTokens: true,
   polygon: {
     tvl: async () => {
       const data = await get('https://spheretvl.simsalacrypto.workers.dev/')
