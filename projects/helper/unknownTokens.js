3715493bb189ee9e6b5f5e35b48f7fab029e39cd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-07 12:45:13 +0100

    improve token pricing logic

--- projects/helper/unknownTokens.js
+++ projects/helper/unknownTokens.js
@@ -6,7 +6,7 @@ const token1 = require('./abis/token1.json');
 const masterchefAbi = require('./abis/masterchef.json')
 const getReserves = require('./abis/getReserves.json');
 const kslpABI = require('./abis/kslp.js');
-const { getChainTransform, stripTokenHeader, getFixBalances, transformBalances, } = require('./portedTokens')
+const { getChainTransform, stripTokenHeader, getFixBalances, transformBalances, transformDexBalances, } = require('./portedTokens')
 const { requery, } = require('./getUsdUniTvl')
 const { getCoreAssets } = require('./tokenMapping')
 const { sumTokens, sumTokens2, nullAddress, } = require('./unwrapLPs')
@@ -136,6 +136,7 @@ async function getTokenPrices({
 
   const prices = {}
   const pairBalances = {}
+  const pairBalances2 = []
 
   for (let i = 0; i < reserves.length; i++) {
     const pairAddress = reserves[i].input.target.toLowerCase();
@@ -144,6 +145,12 @@ async function getTokenPrices({
     const token0Address = pair.token0Address.toLowerCase()
     const token1Address = pair.token1Address.toLowerCase()
     const reserveAmounts = reserves[i].output
+    pairBalances2.push({
+      token0:token0Address,
+      token1:token1Address,
+      token0Bal: reserveAmounts[0],
+      token1Bal: reserveAmounts[1],
+    })
     if (coreAssets.includes(token0Address) && coreAssets.includes(token1Address)) {
       sdk.util.sumSingleBalance(pairBalances[pairAddress], token0Address, Number(reserveAmounts[0]))
       sdk.util.sumSingleBalance(pairBalances[pairAddress], token1Address, Number(reserveAmounts[1]))
@@ -228,6 +235,7 @@ async function getTokenPrices({
     pairBalances,
     prices,
     balances,
+    pairBalances2,
   }
 
   function setPrice(prices, address, coreAmount, tokenAmount, coreAsset) {
@@ -362,10 +370,16 @@ function getUniTVL({ chain = 'ethereum', coreAssets = [], blacklist = [], whitel
   abis = {},
   restrictTokenRatio, // while computing tvl, an unknown token value can max be x times the pool value, default 100 times pool value
   fetchInChunks = 0,  // if there are too many pairs, we might want to query in batches to avoid multicall failing and crashing
+  version = '1',
 }) {
   if (!coreAssets.length && useDefaultCoreAssets) {
     coreAssets = getCoreAssets(chain)
   }
+  const isVersion2 = version === '2'
+  if (fetchInChunks && isVersion2) {
+    throw new Error ('Not yet supported!')
+  }
+
   return async (ts, _block, { [chain]: block }) => {
 
     // get factory from LP
@@ -389,6 +403,7 @@ function getUniTVL({ chain = 'ethereum', coreAssets = [], blacklist = [], whitel
         block, chain, coreAssets, blacklist, lps: pairAddresses, transformAddress, whitelist, allLps: true,
         minLPRatio, log_coreAssetPrices, log_minTokenValue, restrictTokenRatio, abis,
       })
+      if (isVersion2) return transformDexBalances({ chain, data: response.pairBalances2, })
       return withMetaData ? response : response.balances
     } else {
       let i = 0
--- projects/helper/utils.js
+++ projects/helper/utils.js
@@ -91,6 +91,7 @@ function isLP(symbol, token, chain) {
   if (chain === 'ethereum' && ['SUDO-LP'].includes(symbol)) return false
   if (chain === 'dogechain' && ['DST-V2'].includes(symbol)) return true
   if (chain === 'harmony' && ['HLP'].includes(symbol)) return true
+  if (chain === 'fantom' && ['HLP'].includes(symbol)) return true
   if (chain === 'songbird' && ['FLRX', 'OLP'].includes(symbol)) return true
   if (chain === 'metis' && ['NLP', 'ALP'].includes(symbol)) return true // Netswap/Agora LP Token
   if (['fantom', 'nova',].includes(chain) && ['NLT'].includes(symbol)) return true
