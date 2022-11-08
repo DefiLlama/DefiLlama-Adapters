4af7fea407b9f67bfdcfafe901bc88aa810665aa
adamrturman <adamrturman@gmail.com>
2022-11-08 15:07:03 -0600

    handle unknown token

--- projects/kavaBoost.js
+++ projects/kavaBoost.js
@@ -1,20 +1,24 @@
-const utils = require('./helper/utils');
-const sdk = require('@defillama/sdk')
+const utils = require("./helper/utils");
+const sdk = require("@defillama/sdk");
 
 async function tvl(timestamp) {
   let balances = {};
   let url = `https://api2.kava.io/kava/savings/v1beta1/total_supply`;
 
-  if (Math.abs(Date.now()/1000 - timestamp) > 3600){
-    const block = await sdk.api.util.lookupBlock(timestamp, {chain:'kava'})
-    url += `?height=${block.block}`
+  if (Math.abs(Date.now() / 1000 - timestamp) > 3600) {
+    const block = await sdk.api.util.lookupBlock(timestamp, { chain: "kava" });
+    url += `?height=${block.block}`;
   }
 
   const response = await utils.fetchURL(url);
 
   for (let coin of response.data.result) {
     const tokenInfo = generic(coin.denom);
-    const tokenName = coin.denom === 'bkava' ? 'bkava' : tokenInfo[0];
+    if (!tokenInfo) {
+      console.log("unknown token", coin.denom);
+      continue;
+    }
+    const tokenName = coin.denom === "bkava" ? "bkava" : tokenInfo[0];
     balances[tokenName] = coin.amount / 10 ** tokenInfo[1];
   }
 
@@ -23,13 +27,16 @@ async function tvl(timestamp) {
 
 function generic(ticker) {
   switch (ticker) {
-    case 'bkava': return ['kava', 6];
-    case 'ukava': return ['kava', 6];
-    case 'erc20/multichain/usdc': return ['usd-coin',6];
+    case "bkava":
+      return ["kava", 6];
+    case "ukava":
+      return ["kava", 6];
+    case "erc20/multichain/usdc":
+      return ["usd-coin", 6];
   }
 }
 
 module.exports = {
   timetravel: false,
   kava: { tvl }
-}
+};
