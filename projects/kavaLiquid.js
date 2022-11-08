4dbdfb5bcfa031d1ababad4e8bfd4f8720e3649f
adamrturman <adamrturman@gmail.com>
2022-11-08 15:15:22 -0600

    implement liquid adapter

--- projects/kavaLiquid.js
+++ projects/kavaLiquid.js
@@ -3,22 +3,34 @@ const sdk = require("@defillama/sdk");
 
 async function tvl(timestamp) {
   let totalValueLocked = {};
-  let url = `https://api2.kava.io/swap/pools`;
+  let url = `https://api2.kava.io/kava/liquid/v1beta1/total_supply`;
   if (Math.abs(Date.now() / 1000 - timestamp) > 3600) {
     const block = await sdk.api.util.lookupBlock(timestamp, { chain: "kava" });
     url += `?height=${block.block}`;
   }
 
-  const tokenInfo = ['kava',6]
+  const response = await utils.fetchURL(url);
 
-  // const response = await utils.fetchURL(url);
-  //
-  // if (response) {
-    totalValueLocked[tokenInfo[0]] = 100
-  // }
+  for (let coin of response.data.result) {
+    const tokenInfo = generic(coin.denom);
+    if (!tokenInfo) {
+      console.log("unknown token", coin.denom);
+      continue;
+    }
 
+    totalValueLocked[tokenInfo[0]] = coin.amount / 10 ** tokenInfo[1];
+  }
   return totalValueLocked;
 }
+
+
+function generic(ticker) {
+  switch (ticker) {
+    case "ukava":
+      return ["kava", 6];
+  }
+}
+
 
 module.exports = {
   timetravel: false,
