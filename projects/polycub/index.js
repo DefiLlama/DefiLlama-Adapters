b096a70d4153d6da71baabf17093919ae0ac1f00
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-09 17:48:49 +0100

    polycub: add staking

--- projects/polycub/index.js
+++ projects/polycub/index.js
@@ -1,5 +1,6 @@
-
+const sdk = require('@defillama/sdk')
 const { yieldHelper } = require("../helper/yieldHelper");
+const { staking } = require("../helper/staking");
 const MASTERCHEF = "0xef79881df640b42bda6a84ac9435611ec6bb51a4";
 const POLYCUB_TOKEN = "0x7cc15fef543f205bf21018f038f591c6bada941c";
 
@@ -9,3 +10,5 @@ module.exports = yieldHelper({
   masterchef: MASTERCHEF,
   nativeToken: POLYCUB_TOKEN,
 })
+
+module.exports.polygon.staking = staking('0x905E21f6C4CB1Ad789CeD61CD0734590a4542346', POLYCUB_TOKEN, 'polygon')
