8356aa0a2cdae7fd0ddba168818e54c8110c26c6
Ioana Roceanu <ioanaroceanu@gmail.com>
2022-11-09 12:46:45 +0000

    voltz project adapter

---
+++ projects/voltz/poolsData.js
@@ -0,0 +1,82 @@
+module.exports =  [
+    {
+      underlying: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      pool_name: 'aUSDC',
+      marginEngine: '0x9ea5cfd876260edadab461f013c24092ddbd531d',
+    },
+    {
+      underlying: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      pool_name: 'aUSDC v2',
+      marginEngine: '0x0bc09825ce9433b2cdf60891e1b50a300b069dd2',
+    },
+    {
+      underlying: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+      pool_name: 'cUSDT borrow',
+      marginEngine: '0x111a75e91625142e85193b67b10e53acf82838cd',
+    },
+    {
+      underlying: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      pool_name: 'aUSDC v3',
+      marginEngine: '0xb785e7e71f099ada43222e1690ee0bf701f80396',
+    },
+    {
+      underlying: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      pool_name: 'aUSDC borrow',
+      marginEngine: '0x33ba6a0b16750206195c777879edd8706204154b',
+    },
+    {
+      underlying: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      pool_name: 'aDAI',
+      marginEngine: '0x317916f91050ee7e4f53f7c94e83fbd4ecadec7e',
+    },
+    {
+      underlying: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      pool_name: 'cDAI',
+      marginEngine: '0x641a6e03fa9511bde6a07793b04cb00aba8305c0',
+    },
+    {
+      underlying: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      pool_name: 'stETH',
+      marginEngine: '0x21f9151d6e06f834751b614c2ff40fc28811b235',
+    },
+    {
+      underlying: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      pool_name: 'rETH',
+      marginEngine: '0xb1125ba5878cf3a843be686c6c2486306f03e301',
+    },
+    {
+      underlying: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      pool_name: 'aDAI v2',
+      marginEngine: '0x654316a63e68f1c0823f0518570bc108de377831',
+    },
+    {
+      underlying: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      pool_name: 'cDAI v2',
+      marginEngine: '0xf2ccd85a3428d7a560802b2dd99130be62556d30',
+    },
+    {
+      underlying: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      pool_name: 'aETH borrow',
+      marginEngine: '0x9b76b4d09229c339b049053f171bfb22cbe50092',
+    },
+    {
+      underlying: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      pool_name: 'aDAI v3',
+      marginEngine: '0x0f533f6b042593c00c9f4a2ad28106f524fceb94',
+    },
+    {
+      underlying: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      pool_name: 'aETH',
+      marginEngine: '0x6f7ccb0cfd6130e75e88e4c72168fd8a6926c943',
+    },
+    {
+      underlying: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      pool_name: 'cDAI v3',
+      marginEngine: '0x75cdbd0e66fdf4e2c80334f72b39628105fbeb20',
+    },
+    {
+      underlying: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      pool_name: 'aETH borrow v2',
+      marginEngine: '0x3d2821e59f330105c1c3aab8c289bce15f688f8a',
+    }
+  ];
