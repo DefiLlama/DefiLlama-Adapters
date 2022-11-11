3de0f5c79205b0a9f3f92b0bb83d6fe1e9e92902
Define101 <Lovelovenow@protonmail.com>
2022-11-11 20:50:11 +0000

    bitfinex, cosmos, aptos, avalanche, algorand, card

--- projects/bitfinex/index.js
+++ projects/bitfinex/index.js
@@ -29,7 +29,25 @@ const config = {
       '0x742d35Cc6634C0532925a3b844Bc454e4438f44e', // Avalanche (C-Chain) cold wallet
       '0x876EabF441B2EE5B5b0554Fd502a8E0600950cFa', // Avalanche (old) hot wallet
     ]
-  }
+  },
+  cardano: {
+    owners: [
+      'DdzFFzCqrhstFM5XQYA28G2ekCkvpb6bPdhUT5vcsZtqYT3i7wAtjNPCTEGNbUmgL4ym9udeV5k6Utc3vCmAbky6Bvda72r1SQknZH9L', //Cardano ADA hot wallet
+      'addr1qxrlkh6yh0km5m5n7923syel0yqqvc3pjrnqrzrz3gwpxd70prfqwehanuxzkwmv55ff9gr7tjx5vymykd2galr9chaqlwjwm9',//Cardano ADA cold wallet
+    ]
+  },
+  algorand: {
+    owners: ['JDQ7EW3VY2ZHK4DKUHMNP35XLFPRJBND6M7SZ7W5RCFDNYAA47OC5IS62I'],
+  },
+  aptos: {
+    owners: ['0xfd9192f8ad8dc60c483a884f0fbc8940f5b8618f3cf2bbf91693982b373dfdea']
+  },
+  cosmos: {
+    owners: [
+      'cosmos1h9ymfm2fxrqgd257dlw5nku3jgqjgpl59sm5ns', //Cosmos hot wallet
+      'cosmos1jtdkj8hxhj88jxv8lul9xvdpnwsl00evvvpnhj', //Cosmos cold wallet
+    ]
+  },
 }
 
 module.exports = cexExports(config)
