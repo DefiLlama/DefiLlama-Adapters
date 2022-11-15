9e288be517e633b485409facbcf47789c5f53077
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-15 09:48:50 +0100

    Fix how abi is chosen

--- projects/ellipsis/index.js
+++ projects/ellipsis/index.js
@@ -47,14 +47,17 @@ async function tvl(_, _b, { [chain]: block }) {
   const tokensAndOwners = []
   const poolInfo = await get('https://api.ellipsis.finance/api/getPoolsCrypto')
   const wrappedCoinPools = [
-    '0x19EC9e3F7B21dd27598E7ad5aAe7dC0Db00A806d'
+    '0xab499095961516f058245c1395f9c0410764b6cd',
+    '0x245e8bb5427822fb8fd6ce062d8dd853fbcfabf5',
+    '0xfa715e7c8fa704cf425dd7769f4a77b81420fbf2',
+    '0x19ec9e3f7b21dd27598e7ad5aae7dc0db00a806d',
   ].map(i => i.toLowerCase())
   const lpTokens = poolInfo.data.allPools.map(i => i.lpToken.address.toLowerCase())
   const { errors } = await PromisePool.withConcurrency(20)
     .for(poolInfo.data.allPools)
     .process(async pool => {
       const target = pool.address.toLowerCase()
-      let abi = !wrappedCoinPools.includes(target) ? abis.coins : abis.wrapped_coins
+      let abi = wrappedCoinPools.includes(target) ? abis.wrapped_coins : abis.coins
       try {
         for (let i = 0; i < pool.tokens.length; i++) {
           const { output: token } = await sdk.api.abi.call({
