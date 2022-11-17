cd52b88c3f55049f70beb005b982d14fa1fabcd4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-17 14:33:54 +0100

    voltz: refactor code

--- projects/voltz/index.js
+++ projects/voltz/index.js
@@ -1,34 +1,30 @@
 
 const sdk = require('@defillama/sdk');
-const BigNumber = require('bignumber.js');
-const irsPools = require('./poolsData.js');
+const ethers = require("ethers");
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
-async function tvl(timestamp, block) {
-  const balances = {};
+async function getTokensAndOwners(block) {
+  const Factory = '0x6a7a5c3824508D03F0d2d24E0482Bea39E08CcAF'
+  const { output: logs } = await sdk.api.util.getLogs({
+    keys: [],
+    toBlock: block,
+    target: Factory,
+    fromBlock: 14878442,
+    topic: 'IrsInstance(address,address,uint256,uint256,int24,address,address,address,uint8,uint8)',
+  })
 
-  const marginBalance = (await sdk.api.abi.multiCall({
-    abi: 'erc20:balanceOf',
-    calls: irsPools.map((pool) => ({
-        params: [pool.marginEngine],
-        target: pool.underlying,
-    })),
-    block: block,
-  })).output;
+  let iface = new ethers.utils.Interface(['event IrsInstance (address indexed underlyingToken, address indexed rateOracle, uint256 termStartTimestampWad, uint256 termEndTimestampWad, int24 tickSpacing, address marginEngine, address vamm, address fcm, uint8 yieldBearingProtocolID, uint8 underlyingTokenDecimals)'])
+  return logs.map((log) => iface.parseLog(log).args).map(i => ([i[0], i[5]]))
+}
 
-  irsPools.forEach((pool) => {
-    const margin = marginBalance.find(
-        (result) => result.input.params[0] === pool.marginEngine
-    );
-    balances[pool.underlying] = BigNumber(balances[pool.underlying] || 0)
-        .plus(margin.output)
-        .toFixed();
+async function tvl(_, block) {
+  return sumTokens2({
+    tokensAndOwners: await getTokensAndOwners(block),
+    block,
   });
-
-  return balances;
 }
 
 module.exports = {
-  timetravel: true,
   ethereum: {
     tvl,
   },
