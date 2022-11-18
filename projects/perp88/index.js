fd4171eed946d3c38d12dcce8682067e1349c6dd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-18 16:37:40 +0100

    code refactor

--- projects/perp88/index.js
+++ projects/perp88/index.js
@@ -1,4 +1,6 @@
-const { tvl } = require('./tvl')
+const { sumTokensExport } = require('../helper/unwrapLPs')
+const tokens = require('./tokens')
+const POOL_DIAMOND_CONTRACT = '0xE7D96684A56e60ffBAAe0fC0683879da48daB383';
 
 module.exports = {
   timetravel: false,
@@ -6,6 +8,10 @@ module.exports = {
   methodology: 'Count every tokens under PoolDiamond management.',
   start: 1668684025,
   polygon: {
-    tvl,
+    tvl: sumTokensExport({
+      owner: POOL_DIAMOND_CONTRACT,
+      tokens: Object.values(tokens),
+      chain: 'polygon',
+    }),
   }
 }
