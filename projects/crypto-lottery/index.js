6935ead1085ca862abb755931a02494c95c1cc76
Anton Masanavets <a.masanovets@gmail.com>
2022-11-20 23:03:51 +0300

    fix: bsc

--- projects/crypto-lottery/index.js
+++ projects/crypto-lottery/index.js
@@ -1,7 +1,7 @@
 const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs');
 
 const CSC_POLYGON_CONTRACT = '0x80b313Be000c42f1f123C7FBd74654544818Ba7c';
-const BCS_CONTRACT = '0xbB183c396392d08B5f4b19909C7Ce58f9c86F637';
+const BSC_CONTRACT = '0xbB183c396392d08B5f4b19909C7Ce58f9c86F637';
 
 module.exports = {
   start: 14005585,
@@ -13,6 +13,6 @@ module.exports = {
     tvl: sumTokensExport({ chain: 'polygon', owner: CSC_POLYGON_CONTRACT, tokens: [nullAddress] }),
   },
   bsc: {
-    tvl: sumTokensExport({ chain: 'bsc', owner: BCS_CONTRACT, tokens: [nullAddress] }),
+    tvl: sumTokensExport({ chain: 'bsc', owner: BSC_CONTRACT, tokens: [nullAddress] }),
   }
 }; 
