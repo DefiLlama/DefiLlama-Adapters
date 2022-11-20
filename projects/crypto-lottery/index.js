5d9691986170c131610eb3add64ad8bcb61fa25c
Anton Masanavets <a.masanovets@gmail.com>
2022-11-21 00:39:50 +0300

    fix: tvl old style

--- projects/crypto-lottery/index.js
+++ projects/crypto-lottery/index.js
@@ -1,10 +1,26 @@
 const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs');
+const sdk = require('@defillama/sdk');
+const { transformBscAddress } = require('../helper/portedTokens');
 
 const CSC_POLYGON_CONTRACT = '0x80b313Be000c42f1f123C7FBd74654544818Ba7c';
 const BSC_CONTRACT = '0xbB183c396392d08B5f4b19909C7Ce58f9c86F637';
 
+async function tvl(chainBlocks) {
+  const balances = {};
+  const transform = await transformBscAddress();
+
+  const balance = await sdk.api.eth.getBalance({
+    target: BSC_CONTRACT,
+    block: chainBlocks['bsc'],
+    chain: 'bsc'
+  });
+  
+  sdk.util.sumSingleBalance(balances, transform(nullAddress), balance.output)
+
+  return balances;
+}
+
 module.exports = {
-  start: 14005585,
   methodology: "We count of smart contract balance in coins",
   csc: {
     tvl: sumTokensExport({ chain: 'csc', owner: CSC_POLYGON_CONTRACT, tokens: [nullAddress]}),
@@ -13,6 +29,7 @@ module.exports = {
     tvl: sumTokensExport({ chain: 'polygon', owner: CSC_POLYGON_CONTRACT, tokens: [nullAddress] }),
   },
   bsc: {
-    tvl: sumTokensExport({ chain: 'bsc', owner: BSC_CONTRACT, tokens: [nullAddress] }),
+    tvl
   }
 }; 
+
