416b23afedb2cefeb6991a4adaec70aff998e319
Anton Masanavets <a.masanovets@gmail.com>
2022-11-20 23:00:24 +0300

    ref: add bsc

--- projects/crypto-lottery/index.js
+++ projects/crypto-lottery/index.js
@@ -1,13 +1,18 @@
-const SMART_CONTRACT = '0x80b313Be000c42f1f123C7FBd74654544818Ba7c'
-const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs')
+const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs');
+
+const CSC_POLYGON_CONTRACT = '0x80b313Be000c42f1f123C7FBd74654544818Ba7c';
+const BCS_CONTRACT = '0xbB183c396392d08B5f4b19909C7Ce58f9c86F637';
 
 module.exports = {
   start: 14005585,
   methodology: "We count of smart contract balance in coins",
   csc: {
-    tvl: sumTokensExport({ chain: 'csc', owner: SMART_CONTRACT, tokens: [nullAddress]}),
+    tvl: sumTokensExport({ chain: 'csc', owner: CSC_POLYGON_CONTRACT, tokens: [nullAddress]}),
   },
   polygon: {
-    tvl: sumTokensExport({ chain: 'polygon', owner: SMART_CONTRACT, tokens: [nullAddress] }),
+    tvl: sumTokensExport({ chain: 'polygon', owner: CSC_POLYGON_CONTRACT, tokens: [nullAddress] }),
+  },
+  bsc: {
+    tvl: sumTokensExport({ chain: 'bsc', owner: BCS_CONTRACT, tokens: [nullAddress] }),
   }
 }; 
