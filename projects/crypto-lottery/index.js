5f9b6df5a0bf3e23000af18ec79317520fc0966b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-21 14:29:30 +0530

    Update index.js

--- projects/crypto-lottery/index.js
+++ projects/crypto-lottery/index.js
@@ -1,24 +1,7 @@
 const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs');
-const sdk = require('@defillama/sdk');
-const { transformBscAddress } = require('../helper/portedTokens');
 
 const CSC_POLYGON_CONTRACT = '0x80b313Be000c42f1f123C7FBd74654544818Ba7c';
 const BSC_CONTRACT = '0xbB183c396392d08B5f4b19909C7Ce58f9c86F637';
-
-async function tvl(chainBlocks) {
-  const balances = {};
-  const transform = await transformBscAddress();
-
-  const balance = await sdk.api.eth.getBalance({
-    target: BSC_CONTRACT,
-    block: chainBlocks['bsc'],
-    chain: 'bsc'
-  });
-  
-  sdk.util.sumSingleBalance(balances, transform(nullAddress), balance.output)
-
-  return balances;
-}
 
 module.exports = {
   methodology: "We count of smart contract balance in coins",
@@ -29,7 +12,6 @@ module.exports = {
     tvl: sumTokensExport({ chain: 'polygon', owner: CSC_POLYGON_CONTRACT, tokens: [nullAddress] }),
   },
   bsc: {
-    tvl
-  }
-}; 
-
+    tvl: sumTokensExport({ chain: 'bsc', owner: BSC_CONTRACT, tokens: [nullAddress] }),
+  },
+}
