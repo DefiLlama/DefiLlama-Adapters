28383e20fad9f81a7e837136c6b06d21c5b2de43
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2022-11-22 03:26:42 -0800

    rename var

--- projects/vesta/index.js
+++ projects/vesta/index.js
@@ -13,6 +13,8 @@ const VaultTokens = {
   GLP: "0x2f546ad4edd93b956c8999be404cdcafde3e89ae"
 }
 
+const FEGLP_ADDRESS = "0x4277f8F2c384827B5273592FF7CeBd9f2C1ac258"
+
 const VSTA_FARMING_ADDRESS = "0x65207da01293C692a37f59D1D9b1624F0f21177c";
 const LP_VSTA_ETH_ADDRESS = "0xc61ff48f94d801c1ceface0289085197b5ec44f0";
 
@@ -21,6 +23,7 @@ const LP_VST_FRAX_ADDRESS = "0x59bF0545FCa0E5Ad48E13DA269faCD2E8C886Ba4";
 
 // TroveManager holds total system collateral for each individual vaults
 const TROVE_MANAGER_ADDRESS = "0x100EC08129e0FD59959df93a8b914944A3BbD5df";
+
 
 const chain = "arbitrum";
 
@@ -36,7 +39,7 @@ async function tvl(_, block, chainBlocks) {
   output.forEach(({ input: { params: [token] }, output }) => {
     if (token.toLowerCase() === VaultTokens.renBTC) output /= 1e10 // fix renBTC balance
 
-    const llamaTokenAddress = transform(token.toLowerCase() === VaultTokens.GLP ? '0x4277f8F2c384827B5273592FF7CeBd9f2C1ac258' : token) // convert sGLP to feGLP address for price api
+    const llamaTokenAddress = transform(token.toLowerCase() === VaultTokens.GLP ? FEGLP_ADDRESS : token) // convert sGLP to feGLP address for price api
    
     sdk.util.sumSingleBalance(balances, llamaTokenAddress, output)
   })
