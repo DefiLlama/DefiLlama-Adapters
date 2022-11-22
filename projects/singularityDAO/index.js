5e8605c651b031bc5de19b5b35b63a3cc8445be8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-22 17:40:49 +0100

    Fixed singularityDAO

--- projects/singularityDAO/index.js
+++ projects/singularityDAO/index.js
@@ -2,8 +2,7 @@ const abi = require("./abi.json");
 const http = require("../helper/http");
 const sdk = require("@defillama/sdk");
 
-const { sumTokens, sumTokens2 } = require("../helper/unwrapLPs");
-const { addFundsInMasterChef } = require("./masterchef");
+const { sumTokens2 } = require("../helper/unwrapLPs");
 const { getParamCalls } = require('../helper/utils')
 
 
@@ -21,11 +20,8 @@ const DYNASET_FORGES = [
 const LP_TOKEN_SDAO_ETH = "0x424485f89ea52839fdb30640eb7dd7e0078e12fb";
 const LP_TOKEN_SDAO_USDT = "0x3a925503970d40d36d2329e3846e09fcfc9b6acb";
 
-const { transformBscAddress } = require("../helper/portedTokens");
-const chain = "bsc";
-
 const getDynasetQuery =
-  "{ dynaset { addresses { chainId value __typename } } }";
+  "{ dynaset { address } }";
 const graphEndpoint =
   "https://dev-onchain-server.singularitydao.ai/dynaset-server/api/graphql";
 
@@ -41,7 +37,7 @@ async function tvl(ts, block) {
 
   const response = await http.graphQuery(graphEndpoint, getDynasetQuery);
   const dynasets = response.dynaset
-    .map((d) => d.addresses.map((a) => a.value))
+    .map((d) => d.address)
     .flat();
   const { output: tokens } = await sdk.api.abi.multiCall({
     calls: dynasets.map((addr) => ({ target: addr })),
@@ -90,7 +86,7 @@ async function pool2(ts, block) {
     [LP_TOKEN_SDAO_ETH, "0xfB85B9Ec50560e302Ab106F1E2857d95132120D0"], // Unbonded
     [LP_TOKEN_SDAO_USDT, "0xfB85B9Ec50560e302Ab106F1E2857d95132120D0"], // Bonded 6M
   ];
-  return sumTokens2({tokensAndOwners, block});
+  return sumTokens2({tokensAndOwners, block, resolveLP: true,});
 }
 
 // Staking pools ERC
@@ -132,7 +128,7 @@ async function pool2BNB(ts, EthBlock, { bsc: block }) {
     ['0x43b95976cf0929478bc13332c9cd2d63bf060976', "0x79292c62f593e08d9b850b790b07e7a0903fd007"],
     ['0x3d12e4381901a6b94438758b90881cb03f10b01e', "0x79292c62f593e08d9b850b790b07e7a0903fd007"],
   ];
-  return sumTokens2({ tokensAndOwners, block, chain: 'bsc' })
+  return sumTokens2({ tokensAndOwners, block, chain: 'bsc', resolveLP: true, })
 }
 
 module.exports = {
