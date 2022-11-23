5c72261ac34ad8b0acf2f25f62121f36dd2da188
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2022-11-23 23:12:03 +0000

    misrepresentedTokens: true ???

--- projects/VAX/index.js
+++ projects/VAX/index.js
@@ -1,7 +1,7 @@
-/*const { getUniTVL } = require('../helper/unknownTokens')
+const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
-  misrepresentedTokens: false,
+  misrepresentedTokens: true,
   multivac: {
     tvl: getUniTVL({
       chain: 'multivac',
@@ -10,7 +10,8 @@ module.exports = {
     })
   }
 };
-*/
+/*
 const { uniTvlExport } = require('../helper/unknownTokens')
 
 module.exports = uniTvlExport('multivac', '0xbaC576111e2BC5EfBbE7c5d765b9DC44083901fD')
+*/
