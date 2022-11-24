70513c7bbcec584c266dd560b15aa8b7318b702f
0xtawa <0xtawanofi@gmail.com>
2022-11-24 09:31:37 +0100

    Remove volumes folder (see README)

--- volumes/adapters/protofi/index.ts
+++
@@ -1,9 +0,0 @@
-import { CHAIN } from "../../helper/chains";
-import { univ2Adapter } from "../../helper/getUniSubgraphVolume";
-
-export default univ2Adapter({
-  [CHAIN.FANTOM]: "https://api.thegraph.com/subgraphs/name/theothercrypto/protofi-dex-fantom",
-}, {
-  factoriesName: "pancakeFactories",
-  dayData: "pancakeDayData",
-});
--- volumes/adapters/ultronswap/index.ts
+++
@@ -1,8 +0,0 @@
-import { CHAIN } from "../../helper/chains";
-import { univ2Adapter } from "../../helper/getUniSubgraphVolume";
-
-const adapters = univ2Adapter({
-  [CHAIN.ULTRON]: "https://graph-node.ultron-rpc.net/subgraphs/name/root/ultronswap-exchange"
-}, {});
-adapters.volume.ultron.start = async () => 1659323793;
-export default adapters;
