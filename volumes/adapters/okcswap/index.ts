98be35528665cfc9cb00b540181d3541c3068630
0xtawa <0xtawanofi@gmail.com>
2022-11-24 09:31:37 +0100

    Remove volumes folder (see README)

--- volumes/adapters/okcswap/index.ts
+++
@@ -1,8 +0,0 @@
-import { CHAIN } from "../../helper/chains";
-import { univ2Adapter } from "../../helper/getUniSubgraphVolume";
-
-const adapters = univ2Adapter({
-  [CHAIN.OKEXCHAIN]: "https://www.okx.com/okc/subgraph/name/okcswap/okc-swap-subgraph-v3",
-}, {});
-
-export default adapters;
