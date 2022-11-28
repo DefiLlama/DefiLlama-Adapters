aa92603b2458d6527e1d15aa2230859296eca337
ramverma-oro <ram.verma.gt@gmail.com>
2022-11-28 19:04:40 +0530

    setu push

--- projects/helper/unifarm.js
+++ projects/helper/unifarm.js
@@ -1,8 +1,28 @@
+const { call } = require("@defillama/sdk/build/abi");
 const axios = require("axios");
+const BigNumber = require("bignumber.js");
 
 const ETH_CHAIN = "ethereum";
 const BSC_CHAIN = "bsc";
 const POLYGON_CHAIN = "polygon";
+
+const liquidityContract = {
+  ethereum: "0xD13dF6B426358C2471bC6dea75167c3c106Ef881",
+  bsc: "0x7423Af05D11e7363cF5Ea5Ef2eA55c7E7AEA3f7a",
+  polygon: "0x62CEfDaDd37C8169034b2efD4401bc770B7B92D3",
+};
+
+const UFARM = {
+  ethereum: "0x40986a85b4cfcdb054a6cbfb1210194fee51af88", //Ufarm ethereum
+  bsc: "0x0a356f512f6fce740111ee04ab1699017a908680", // ufarm BSC
+  polygon: "0xa7305ae84519ff8be02484cda45834c4e7d13dd6", //ufarm Polygon
+  avax: "0xd60effed653f3f1b69047f2d2dc4e808a548767b", // ufarm avax
+};
+
+const ORO = {
+  ethereum: "0xc3eb2622190c57429aac3901808994443b64b466", // ethereum
+  bsc: "0x9f998d62B81AF019E3346AF141f90ccCD679825E", //BSC
+};
 
 const ETH_TOKENLIST =
   "https://raw.githubusercontent.com/InfernalHeir/tokenlist/mainnet-01/unifarm-tokenlist.json";
@@ -22,6 +42,21 @@ query GetCohort($where: CohortGroupWhereClause!) {
 }
 `;
 
+const v2Query = `
+query MyQuery {
+  cohorts {
+    cohortVersion
+    numberOfFarms,
+    id,
+    tokens {
+      decimals
+      farmToken
+      fid
+    }
+  }
+}
+`;
+
 const getCohortTokens = async (url) => {
   const results = await axios.get(url);
   return results.data.tokenlist;
@@ -37,6 +72,40 @@ const getCohortTokensByChainName = async (chain) => {
       return await getCohortTokens(POLYGON_TOKENLIST);
     default:
       break;
+  }
+};
+
+const getV2GraphEndPoint = (chain) => {
+  switch (chain) {
+    case ETH_CHAIN:
+      return "https://api.thegraph.com/subgraphs/name/themohitmadan/unifarm-eth";
+    case BSC_CHAIN:
+      return "https://api.thegraph.com/subgraphs/name/themohitmadan/unifarm-bsc";
+    case POLYGON_CHAIN:
+      return "https://api.thegraph.com/subgraphs/name/themohitmadan/unifarm-polygon";
+    default:
+      break;
+  }
+};
+
+const getV2Cohorts = async (chain) => {
+  const V2_GRAPH_ENDPOINT = getV2GraphEndPoint(chain);
+  try {
+    const results = await axios.post(
+      V2_GRAPH_ENDPOINT,
+      JSON.stringify({
+        query: v2Query,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return results.data.data.cohorts;
+  } catch (err) {
+    console.log(err.message);
   }
 };
 
@@ -86,6 +155,15 @@ const createCalls = (tokens, cohortId) => {
   });
 };
 
+const createV2Calls = (tokens, cohortId) => {
+  return tokens.map((items) => {
+    return {
+      target: items.farmToken,
+      params: [cohortId],
+    };
+  });
+};
+
 const createMulticalls = (cohortAndProxies, tokens) => {
   let calls = [];
   for (var u = 0; u < cohortAndProxies.length; u++) {
@@ -102,8 +180,53 @@ const createMulticalls = (cohortAndProxies, tokens) => {
   return calls;
 };
 
+const createMulticallForV2 = (cohorts) => {
+  let calls = [];
+  for (let i = 0; i < cohorts.length; i++) {
+    calls.push(...createV2Calls(cohorts[i].tokens, cohorts[i].id));
+  }
+
+  return calls;
+};
+
+const createCallForSetu = (chain) => {
+  let calls = [];
+  calls.push({
+    target: UFARM[chain],
+    params: liquidityContract[chain],
+  });
+
+  if (chain === "ethereum" || chain === "bsc") {
+    calls.push({
+      target: ORO[chain],
+      params: liquidityContract[chain],
+    });
+  }
+  return calls;
+};
+
+const getFormattedBalance = (input, decimals) => {
+  return new BigNumber(input).dividedBy(new BigNumber(10 ** Number(decimals)));
+};
+
+const getBigNumberBalance = (input, decimals) => {
+  return new BigNumber(input)
+    .multipliedBy(new BigNumber(10).pow(decimals))
+    .toFixed(0);
+};
+
+// const fetchBridge = async () => {
+//   const results = await axios.get("https://api.setu.unifarm.co/api/tokens");
+//   return results.data;
+// };
+
 module.exports = {
   createMulticalls,
   getCohortTokensByChainName,
   getCohortsAndProxiesByChain,
+  getV2Cohorts,
+  createMulticallForV2,
+  createCallForSetu,
+  getFormattedBalance,
+  getBigNumberBalance,
 };
--- projects/unifarm/index.js
+++ projects/unifarm/index.js
@@ -1,19 +1,30 @@
 const sdk = require("@defillama/sdk");
 const erc20Abi = require("./erc20.json");
 const BigNumber = require("bignumber.js");
+const _ = require("lodash");
 const {
   getCohortTokensByChainName,
   getCohortsAndProxiesByChain,
   createMulticalls,
+  createMulticallForV2,
+  getV2Cohorts,
+  getBigNumberBalance,
+  createCallForSetu,
+  getFormattedBalance,
 } = require("../helper/unifarm");
-const { 
-  transformBscAddress, 
-  transformPolygonAddress 
-} = require('../helper/portedTokens');
+const {
+  transformBscAddress,
+  transformPolygonAddress,
+} = require("../helper/portedTokens");
 // node test.js projects/unifarm/index.js
-const _tvl = async (timestamp, ethBlock, chainBlocks, chain, transform = a => a) => {
+const _tvl = async (
+  timestamp,
+  ethBlock,
+  chainBlocks,
+  chain,
+  transform = (a) => a
+) => {
   const block = chainBlocks[chain];
-
   const tokens = await getCohortTokensByChainName(chain);
   const cohortAndProxies = await getCohortsAndProxiesByChain(chain);
 
@@ -27,6 +38,7 @@ const _tvl = async (timestamp, ethBlock, chainBlocks, chain, transform = a => a)
   });
 
   var balances = {};
+  let decimalsWithChainV1 = {};
 
   for (var e = 0; e < tokens.length; e++) {
     const token = tokens[e];
@@ -53,6 +65,7 @@ const _tvl = async (timestamp, ethBlock, chainBlocks, chain, transform = a => a)
       }
     }
 
+    decimalsWithChainV1[`${chain}:${token.address}`] = token.decimals;
     balances[`${chain}:${token.address}`] = new BigNumber(
       cohortBalances.reduce((a, b) => a + b, 0)
     )
@@ -60,23 +73,217 @@ const _tvl = async (timestamp, ethBlock, chainBlocks, chain, transform = a => a)
       .toFixed(0);
   }
 
-  return balances;
+  return { balanceV1: balances, decimalsWithChainV1 };
 };
 
-const ethereum = async (timestamp, ethBlock, chainBlocks) => {
-  let balance = await _tvl(timestamp, ethBlock, chainBlocks, "ethereum");
+const _tvl_v2 = async (
+  timestamp,
+  ethBlock,
+  chainBlocks,
+  chain,
+  transform = (a) => a
+) => {
+  const block = chainBlocks[chain];
+  const v2Cohorts = await getV2Cohorts(chain);
+
+  let calls = createMulticallForV2(v2Cohorts);
+
+  const multiCallResult = await sdk.api.abi.multiCall({
+    abi: erc20Abi[0],
+    calls,
+    chain,
+    block,
+  });
+
+  let balances = {};
+  let decimalsWithChainV2 = {};
+  for (var e = 0; e < v2Cohorts.length; e++) {
+    const token = v2Cohorts[e];
+    const balance = multiCallResult.output.map((element) => {
+      for (let i = 0; i < token.tokens.length; i++) {
+        if (
+          String(element.input.target).toLowerCase() ===
+          String(token.tokens[i].farmToken).toLowerCase()
+        ) {
+          return {
+            ...element,
+            decimals: token.tokens[i].decimals,
+          };
+        }
+      }
+    });
+
+    const balancesArray = balance.filter((e) => e != undefined);
+    var cohortBalances = [];
+
+    if (balancesArray[0] !== undefined) {
+      let j = 0;
+      while (j < balancesArray.length) {
+        var x = new BigNumber(balancesArray[j].output);
+        var parseBalance = Number(
+          x.dividedBy(new BigNumber(10 ** Number(balancesArray[j].decimals)))
+        );
+
+        cohortBalances.push({
+          token: balancesArray[j].input.target,
+          balance: parseBalance,
+          decimals: Number(balancesArray[j].decimals),
+        });
+
+        j++;
+      }
+    }
+
+    let sumOfBalances = {};
+    let decimalsOfToken = {};
+
+    for (let i = 0; i < cohortBalances.length; i++) {
+      sumOfBalances[`${cohortBalances[i].token}`] = _.add(
+        sumOfBalances[`${cohortBalances[i].token}`] >= 0
+          ? sumOfBalances[`${cohortBalances[i].token}`]
+          : 0,
+        Number(cohortBalances[i].balance)
+      );
+      decimalsOfToken[`${cohortBalances[i].token}`] =
+        cohortBalances[i].decimals;
+    }
+
+    Object.entries(sumOfBalances).map((item) => {
+      balances[`${chain}:${item[0]}`] = new BigNumber(item[1])
+        .multipliedBy(new BigNumber(10).pow(decimalsOfToken[item[0]]))
+        .toFixed(0);
+    });
+
+    Object.entries(decimalsOfToken).map((item) => {
+      decimalsWithChainV2[`${chain}:${item[0]}`] = item[1];
+    });
+  }
+  return { balancesV2: balances, decimalsWithChainV2 };
+};
+
+const fetchBridgeTVL = async (timestamp, ethBlock, chainBlocks, chain) => {
+  const block = chainBlocks[chain];
+  const calls = createCallForSetu(chain);
+
+  const multiCallResult = await sdk.api.abi.multiCall({
+    abi: erc20Abi[0],
+    calls,
+    chain,
+    block,
+  });
+
+  const setuBalance = {};
+  multiCallResult.output.map((element) => {
+    setuBalance[`${chain}:${element.input.target}`] = element.output;
+  });
+
+  return setuBalance;
+};
+
+const chainTVL = async (chain, timestamp, ethBlock, chainBlocks) => {
+  let balance = {};
+  const setuBalance = await fetchBridgeTVL(
+    timestamp,
+    ethBlock,
+    chainBlocks,
+    chain
+  );
+
+  Object.entries(setuBalance).map((item) => {
+    balance[item[0]] = item[1];
+  });
+
+  let { balancesV2, decimalsWithChainV2 } = await _tvl_v2(
+    timestamp,
+    ethBlock,
+    chainBlocks,
+    chain
+  );
+
+  Object.entries(balancesV2).map((item) => {
+    if (balance[item[0]]) {
+      const getFormatBalance1 = getFormattedBalance(
+        balance[item[0]],
+        decimalsWithChainV2[item[0]]
+      );
+      const getFormatBalance2 = getFormattedBalance(
+        item[1],
+        decimalsWithChainV2[item[0]]
+      );
+      const sumOfBalace = Number(getFormatBalance1) + Number(getFormatBalance2);
+      balance[item[0]] = getBigNumberBalance(
+        sumOfBalace,
+        decimalsWithChainV2[item[0]]
+      );
+    } else {
+      balance[item[0]] = item[1];
+    }
+  });
+
+  let { balanceV1, decimalsWithChainV1 } = await _tvl(
+    timestamp,
+    ethBlock,
+    chainBlocks,
+    chain
+  );
+
+  Object.entries(balanceV1).map((item) => {
+    if (balance[item[0]]) {
+      const getFormatBalance1 = getFormattedBalance(
+        balance[item[0]],
+        decimalsWithChainV1[item[0]]
+      );
+      const getFormatBalance2 = getFormattedBalance(
+        item[1],
+        decimalsWithChainV1[item[0]]
+      );
+      const sumOfBalace = Number(getFormatBalance1) + Number(getFormatBalance2);
+      balance[item[0]] = getBigNumberBalance(
+        sumOfBalace,
+        decimalsWithChainV1[item[0]]
+      );
+    } else {
+      balance[item[0]] = item[1];
+    }
+  });
+
+  console.log({ balance });
   return balance;
 };
 
+// const ethereum = async (timestamp, ethBlock, chainBlocks) => {
+//   const transform = await transformBscAddress();
+//   let balance = await chainTVL(
+//     timestamp,
+//     ethBlock,
+//     chainBlocks,
+//     "ethereum",
+//     transform
+//   );
+//   return balance;
+// };
+
 const bsc = async (timestamp, ethBlock, chainBlocks) => {
   const transform = await transformBscAddress();
-  let balance = await _tvl(timestamp, ethBlock, chainBlocks, "bsc", transform);
+  let balance = await chainTVL(
+    timestamp,
+    ethBlock,
+    chainBlocks,
+    "bsc",
+    transform
+  );
   return balance;
 };
 
 const polygon = async (timestamp, ethBlock, chainBlocks) => {
   const transform = await transformPolygonAddress();
-  let balance = await _tvl(timestamp, ethBlock, chainBlocks, "polygon", transform);
+  let balance = await chainTVL(
+    timestamp,
+    ethBlock,
+    chainBlocks,
+    "polygon",
+    transform
+  );
   return balance;
 };
 
