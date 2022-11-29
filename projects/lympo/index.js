9a1df773f06b4655aba28994128d9dde282071c5
romuch <neverkiller2@gmail.com>
2022-11-29 16:36:44 +0200

    lympo: fix tvl

--- projects/lympo/index.js
+++ projects/lympo/index.js
@@ -4,7 +4,7 @@ const { sumTokens2 } = require('../helper/unwrapLPs')
 const poolsUrl = 'https://api.lympo.io/pools/poolsV2/pools.json';
 const sportTokenAddress = "0x503836c8c3A453c57f58CC99B070F2E78Ec14fC0"
 
-async function staking(timestamp, _, { polygon: block }) {
+async function tvl(timestamp, _, { polygon: block }) {
     const pools = await get(poolsUrl)
     const owners = pools.map(i => i.address)
     return sumTokens2({ chain: 'polygon', block, owners, tokens: [sportTokenAddress]})
@@ -12,8 +12,7 @@ async function staking(timestamp, _, { polygon: block }) {
 
 module.exports = {
     polygon: {
-        tvl: () => ({}),
-        staking,
+        tvl: tvl,
     },
     methodology: "TVL is calculated as value of SPORT tokens in Lympo pools staking",
 }
