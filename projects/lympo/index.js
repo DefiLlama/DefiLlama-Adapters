7b3f58fffdc59b796d8b2f3b90b6f30d9d6f29a4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-29 14:56:15 +0100

    lympo: code refactor

--- projects/lympo/index.js
+++ projects/lympo/index.js
@@ -1,39 +1,19 @@
-const axios = require('axios');
-const sdk = require("@defillama/sdk");
+const { get } = require('../helper/http')
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
 const poolsUrl = 'https://api.lympo.io/pools/poolsV2/pools.json';
 const sportTokenAddress = "0x503836c8c3A453c57f58CC99B070F2E78Ec14fC0"
 
-async function getBalances(pools, block) {
-
-    const balances = {};
-
-    for(pool of pools) {
-        const balanceResponse = await sdk.api.abi.call({
-            target: sportTokenAddress,
-            abi: "erc20:balanceOf",
-            params: pool.address,
-            chain: "polygon"
-        })
-
-        sdk.util.sumSingleBalance(balances, sportTokenAddress, balanceResponse.output);
-    }
-
-    return balances
-}
-
-async function tvl(timestamp, block) {
-    const pools = (await axios.get(poolsUrl)).data;
-    const balances = await getBalances(pools, block);
-
-    return {
-        "sport": balances[sportTokenAddress] / 1e18
-    };
+async function staking(timestamp, _, { polygon: address }) {
+    const pools = await get(poolsUrl)
+    const owners = pools.map(i => i.address)
+    return sumTokens2({ chain: 'polygon', block, owners, tokens: [sportTokenAddress]})
 }
 
 module.exports = {
     polygon: {
-        tvl
+        tvl: () => ({}),
+        staking,
     },
     methodology: "TVL is calculated as value of SPORT tokens in Lympo pools staking",
 }
