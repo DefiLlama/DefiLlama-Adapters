20137cc51785a04bab6098959f0ad1484f0bdbbe
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-01 17:24:18 +0100

    neptune mutual: turn into on chain adapter

--- projects/neptune-mutual/index.js
+++ projects/neptune-mutual/index.js
@@ -1,14 +1,33 @@
-const { fetchURL } = require("../helper/utils");
+const sdk = require('@defillama/sdk')
+const { ethers } = require("ethers");
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
-async function tvl(timestamp, block) {
-  const url = "https://api.adapters.neptunemutual.com/defillama/ethereum/tvl";
-  const { data } = await fetchURL(url);
+const abi = {
+  sc: { "inputs": [], "name": "sc", "outputs": [{ "internalType": "contract IStore", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
+}
 
-  return data;
+async function tvl(_, block) {
+  const {output: logs} = await sdk.api.util.getLogs({
+    target: "0x0150b57aa8cc6fcbc110f07eef0c85731d8aacf4", // vault factory
+    topic: "VaultDeployed(address,bytes32,string,string)",
+    keys: [],
+    fromBlock: 15912005,
+    toBlock: block,
+  });
+
+  let iface = new ethers.utils.Interface(['event VaultDeployed (address vault, bytes32 coverKey, string name, string symbol)'])
+  const vaults = logs.map((log) => iface.parseLog(log).args.vault)
+  const { output: tokens } = await sdk.api.abi.multiCall({
+    abi: abi.sc,
+    calls: vaults.map(i => ({ target: i})),
+    block,
+  })
+  const toa = tokens.map(i => ([i.output, i.input.target]))
+
+  return sumTokens2({ tokensAndOwners: toa, block, })
 }
 
 module.exports = {
-  timetravel: false,
   methodology: "TVL consists of the total liquidity available in the cover pools",
   start: 1667260800, // Nov 01 2022 @ 12:00am (UTC)
   ethereum: { tvl },
