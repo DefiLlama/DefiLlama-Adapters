d1e22d0ff9ff6f58db38b3516144fe35408153c2
realdealshaman <realdealshaman11@gmail.com>
2022-12-02 18:14:05 +0000

    add hallmark

--- projects/crolend/index.js
+++ projects/crolend/index.js
@@ -1,5 +1,8 @@
 const { compoundExports } = require('../helper/compound')
 
 module.exports = {
+    hallmarks: [
+        [1670004805, "Project rugged"]
+    ],
     cronos: compoundExports("0x30dF4C58ADaf1FcF388B7Bf775840DEc086dcB98", "cronos",)
 }
