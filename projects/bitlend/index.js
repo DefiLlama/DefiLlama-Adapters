33502e985f046794d3e7d126d27329518229d2f6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-03 19:27:40 +0100

    code refactor

--- projects/bitlend/index.js
+++ projects/bitlend/index.js
@@ -1,4 +1,4 @@
-const sdk = require('@defillama/sdk')
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 const CHAIN = 'bittorrent'
 
@@ -12,47 +12,17 @@ const bETH = '0xb65103C4B2Af563F9bBD8ad8CA8387d700673B6E'
 const bUSDC_T = '0xd1dA3881282a954E7FCd5a23c89AF2978FCEFcF0'
 const bTRX = '0xE73fb086C7Aa48b83372b028f0f35B06E77C7511'
 
-async function tvl (timestamp, ethBlock, { bittorrent: block }) {
-  const ethBalance = (
-    await sdk.api.erc20.balanceOf({
-      block,
-      target: ETH,
-      chain: CHAIN,
-      owner: bETH
-    })
-  ).output
-
-  const usdctBalance = (
-    await sdk.api.erc20.balanceOf({
-      block,
-      target: USDC_T,
-      chain: CHAIN,
-      owner: bUSDC_T
-    })
-  ).output
-
-  const trxBalance = (
-    await sdk.api.erc20.balanceOf({
-      block,
-      target: TRX,
-      chain: CHAIN,
-      owner: bTRX
-    })
-  ).output
-
-  return {
-    '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': ethBalance,
-    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': usdctBalance,
-    tron: trxBalance / 10e5
-  }
-}
-
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'Total staked tokens in Bitlend protocol.',
   start: 14069919,
   bittorrent: {
-    tvl
+    tvl: sumTokensExport({
+      chain: CHAIN,
+      tokensAndOwners: [
+        [ETH, bETH],
+        [USDC_T, bUSDC_T],
+        [TRX, bTRX],
+      ],
+    })
   }
 }
