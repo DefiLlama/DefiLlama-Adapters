290f2ef25c0762cb928c861caf14096fbbf2dd6d
Michael Murphy <mike@rivval.com>
2022-12-07 13:36:56 +0000

    fix : filter lp tokens

--- projects/x7/index.js
+++ projects/x7/index.js
@@ -1,57 +1,56 @@
 const sdk = require("@defillama/sdk");
 
 const ethContracts = [
-    /**
-    * X7DAO
-    * V2 Governance token
-    */
-    '0x7105e64bf67eca3ae9b123f0e5ca2b83b2ef2da0',
-    /**
-    * X7R
-    * Merged Reward token
-    */
-    '0x70008f18fc58928dce982b0a69c2c21ff80dca54',
-    /**
-    * X7101
-    * V2 First of price consistent collection
-    */
-    '0x7101a9392eac53b01e7c07ca3baca945a56ee105',
-    /**
-    * X7102
-    * V2 Second of price consistent collection
-    */
-    '0x7102dc82ef61bfb0410b1b1bf8ea74575bf0a105',
-    /**
-    * X7103
-    * V2 Third of price consistent collection
-    */
-    '0x7103ebdbf1f89be2d53eff9b3cf996c9e775c105',
-    /**
-    * X7104
-    * V2 Fourth of price consistent collection
-    */
-    '0x7104d1f179cc9cc7fb5c79be6da846e3fbc4c105',
-    /**
-    * X7105
-    * V2 Fifth of price consistent collection
-    */
-    '0x7105faa4a26ed1c67b8b2b41bec98f06ee21d105',
+    /*** X7DAO LP - V2 Governance token */
+    {
+        lpAddress: '0x75311ee016c82e7770E4ACa73a0d142f96ddB969', erc20Address: '0x7105E64bF67ECA3Ae9b123F0e5Ca2b83b2eF2dA0'
+    },
+    /*** X7R LP - Merged Reward token */
+    {
+        lpAddress: '0x6139240A5907e4CE74673257c320ea366c521AEA', erc20Address: '0x70008F18Fc58928dcE982b0A69C2c21ff80Dca54'
+    },
+    /*** X7101 - V2 First of price consistent collection */
+    {
+        lpAddress: '0x81b786ED4B2F1118E0fa0343AD4760E15448e3e8', erc20Address: '0x7101a9392EAc53B01e7c07ca3baCa945A56EE105'
+    },
+    /*** X7102 - V2 Second of price consistent collection */
+    {
+        lpAddress: '0x49C838c60170C36E90CFA6021a57f2268dda3254', erc20Address: '0x7102DC82EF61bfB0410B1b1bF8EA74575bf0A105'
+    },
+    /*** X7103 - V2 Third of price consistent collection */
+    {
+        lpAddress: '0xcecf54EDC42c5C9f6Ee10cb1eFcc23E49F7D5A5d', erc20Address: '0x7103eBdbF1f89be2d53EFF9B3CF996C9E775c105'
+    },
+    /*** X7104 - V2 Fourth of price consistent collection */
+    {
+        lpAddress: '0x7d0D7c088233cBC08ee2400B96D10BF24C40E93a', erc20Address: '0x7104D1f179Cc9cc7fb5c79Be6Da846E3FBC4C105'
+    },
+    /*** X7105 - V2 Fifth of price consistent collection */
+    {
+        lpAddress: '0x6d9D1B6B4D53f090639ae8D9E9C83B796Da694eE', erc20Address: '0x7105FAA4a26eD1c67B8B2b41BEc98F06Ee21D105'
+    },
 ];
 
-const ethTvls = ethContracts.map((contractAddress) => {
-    return async (timestamp, block) => {
-        return {
-            [contractAddress]: (
-                await sdk.api.erc20.totalSupply({
-                    block,
-                    target: contractAddress,
-                })
-            ).output,
-        };
-    };
-});
+async function tvl(timestamp, block, chainBlocks) {
+    const balances = {};
+    const underlying = await sdk.api.abi.multiCall({
+        calls: ethContracts.map(address => ({
+            target: address.erc20Address,
+            params: [address.lpAddress]
+        })),
+        abi: 'erc20:balanceOf',
+        block,
+    });
+
+    sdk.util.sumMultiBalanceOf(balances, underlying);
+    return balances;
+}
 
 module.exports = {
-    ethereum: { tvl: sdk.util.sumChainTvls(ethTvls), },
-    methodology: 'The TVL consists of the underlying project tokens.'
+    timetravel: true,
+    misrepresentedTokens: false,
+    methodology: 'The TVL consists of the underlying project tokens.',
+    ethereum: {
+        tvl,
+    }
 };
