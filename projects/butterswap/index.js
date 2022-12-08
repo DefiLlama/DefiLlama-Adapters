0b2b9496ea9e572e68804b975f3c51dee5eea421
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-08 15:45:21 +0100

    fix broken adapters

--- projects/butterswap/index.js
+++ projects/butterswap/index.js
@@ -1,5 +1,5 @@
-const sdk = require("@defillama/sdk");
 const {uniTvlExport} = require("../helper/unknownTokens");
+const {staking} = require("../helper/staking");
 
 //HECO ADDRESSES
 const hecoFactory = "0x874D01CA682C9c26BA7E6D9f6F801d1a1fb49201";
@@ -12,39 +12,13 @@ const bscButter = "0x5eF7814f4cB17b38408F1F641e4b5b61c5D023a8";
 const bscHButter = "0x2f3bca2631fff538b8a55207f6c2081457e229f7";
 const bscChef = "0xa49f4CF57eaFE0098D398DF3eD3A7dF10EAaBfAB";
 
-async function bscStaking(timestamp, block, chainBlocks) {
-    let balances = {};
-    let stakingBalance = (await sdk.api.erc20.balanceOf({
-        target: bscButter,
-        owner: bscChef,
-        block: chainBlocks.bsc,
-        chain: "bsc"
-    })).output;
-    sdk.util.sumSingleBalance(balances, `heco:${hecoButter}`, stakingBalance);
-    return balances;
-}
-
-async function hecoStaking(timestamp, block, chainBlocks) {
-    let balances = {};
-    let stakingBalance = (await sdk.api.erc20.balanceOf({
-        target: hecoButter,
-        owner: hecoChef,
-        block: chainBlocks.heco,
-        chain: "heco"
-    })).output;
-    sdk.util.sumSingleBalance(balances, `heco:${hecoButter}`, stakingBalance);
-    return balances;
-}
-
-
-
 module.exports = {
     heco: {
-        tvl: uniTvlExport('heco', hecoFactory),
-        staking: hecoStaking
+        tvl: uniTvlExport('heco', hecoFactory).heco.tvl,
+        staking: staking(hecoChef, hecoButter, 'heco'),
     },
     bsc: {
-        tvl: uniTvlExport('bsc', bscFactory),
-        staking: bscStaking
+        tvl: uniTvlExport('bsc', bscFactory).bsc.tvl,
+        staking: staking(bscChef, bscButter, 'bsc'),
     },
 }
