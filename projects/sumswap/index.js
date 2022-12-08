8dc64b488d19717bafcd4fc3498da4abbb0ee74c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-08 08:38:11 +0100

    minor fix

--- projects/sumswap/index.js
+++ projects/sumswap/index.js
@@ -1,8 +1,7 @@
 const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
-  misrepresentedTokens: true,
   ethereum: {
-    tvl: getUniTVL({ chain: 'ethereum', factory: '0x96FF042f8c6757fCE515d171F194b5816CAFEe11', useDefaultCoreAssets: true, }), 
+    tvl: getUniTVL({ chain: 'ethereum', factory: '0x96FF042f8c6757fCE515d171F194b5816CAFEe11', }), 
   },
 };
