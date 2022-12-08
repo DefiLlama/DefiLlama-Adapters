bfe310f47307889195905e7c2d159e9400541f7e
Britt Cyr <cyrbritt@gmail.com>
2022-12-08 18:33:04 -0400

    Reset logging

--- projects/helper/solana.js
+++ projects/helper/solana.js
@@ -136,6 +136,7 @@ async function getTokenAccountBalances(tokenAccounts, { individual = false, chun
           log('Null account: skipping it')
           return;
         }
+        console.log(data.data.map(i => i.result.value)[i], tokenAccounts[i].toString())
       }
       if (!value) {
         return;
