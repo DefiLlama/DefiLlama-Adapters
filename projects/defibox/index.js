09ffa525423d8a40e95eff7fc45259c97ece0b6c
Denis <carriere.denis@gmail.com>
2022-12-10 10:30:19 -0500

    Update TVL for Defibox (#4839)
    * Update Defibox adapter
    
    * add TODO for BSC
    
    * fix formatting
    
    * fix total borrowed
    
    * minor fix
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/defibox/index.js
+++ projects/defibox/index.js
@@ -1,62 +1,71 @@
-const axios = require("axios");
 const utils = require("../helper/utils");
-const {get_account_tvl} = require('../helper/chain/eos')
 const {lendingMarket} = require("../helper/methodologies")
 
-const eosEndpoint = "https://dapp.defibox.io/api/"
+const eosEndpoint = "https://eos.defibox.io/api/"
+const waxEndpoint = "https://wax.defibox.io/defibox/api/"
 const bscEndpoint = "https://bsc.defibox.io/api/"
 
 async function eos() {
-  const swap = await utils.fetchURL(eosEndpoint + "swap/get24HInfo")
-  const lend = await utils.fetchURL(eosEndpoint + "lend/getGlobalOpenPositionStat")
-  const usn = await utils.fetchURL(eosEndpoint + "st/open/getGlobalOpenStat")
-  const tvl = lend.data.data.practicalBalance - lend.data.data.totalBorrowsVariable + // lend TVL
-              usn.data.globalOpenStat.totalMortgage // usn (stable token) TVL
+  const swap = await utils.fetchURL(eosEndpoint + "swap/get24HInfo");
+  const lend = await utils.postURL(eosEndpoint + "lend/getGlobalOpenPositionStat")
+  const usn = await utils.postURL(eosEndpoint + "st/open/getGlobalOpenStat")
+  const bal = await utils.fetchURL(eosEndpoint + "bal/get24HInfo");
+  const vault = await utils.postURL(eosEndpoint + "vault/getVaultStat");
+
+  // Calculate TVL (in $EOS value)
+  const eos = Number(swap.data.data.eosBalance) * 2; // swap TVL (dual sided 50/50 AMM pool)
+
+  // Calculate TVL (in $USDT value)
+  const lend_tvl = Number(lend.data.data.practicalBalance) - Number(lend.data.data.totalBorrowsVariable); // lending protocol (deposits - borrowed assets)
+  const usn_tvl = Number(usn.data.globalOpenStat.totalMortgage) // USN (over-collaterized stable token)
+  const bal_tvl = Number(bal.data.data.balUsdtBalance) // balance protocol TVL (total in USDT value)
+  const vault_tvl = Number(vault.data.data) // vault protocol TVL (time deposit EOS & USDT valued in $USD)
+  const tether = lend_tvl + usn_tvl + bal_tvl + vault_tvl;
+
   return {
-    tether: tvl,
-    eos: swap.data.data.eosBalance * 2, // swap TVL
+    tether,
+    eos,
   }
 }
 
 async function borrowed() {
-  const lend = await utils.fetchURL(eosEndpoint + "lend/getGlobalOpenPositionStat")
-  const tvl = lend.data.data.totalBorrowsVariable // lend TVL
+  const lend = await utils.postURL(eosEndpoint + "lend/getGlobalOpenPositionStat")
+  const tether = Number(lend.data.data.totalBorrowsVariable) // total TVL that is borrowed from lend protocol
   
-  return {
-    tether: tvl,
-  }
+  return {tether};
 }
 
 async function wax() {
-  const tokens = [
-    ["eosio.token", "WAX", "wax"],
-    ["alien.worlds", "TLM", "alien-worlds"],
-    // ["e.rplanet", "AETHER", null], // no CoinGecko price support
-    // ["e.rplanet", "RDAO", null], // no CoinGecko price support
-    // ["prospectorsw", "PGL", null], // no CoinGecko price support
-  ];
-  return get_account_tvl("swap.box", tokens, "wax");
+  const swap = await utils.fetchURL(waxEndpoint + "swap/get24HInfo");
+  const wax = Number(swap.data.data.waxBalance) * 2; // swap TVL (dual sided 50/50 AMM pool)
+
+  return {
+    wax,
+  }
 }
 
+// TODO: BSC endpoint not available at the moment
 async function bsc() {
-  const swap = await axios.default.post(bscEndpoint + "swap/get24HInfo", {}, { headers: { chainid: 56 }})
+  const swap = await utils.fetchURL(bscEndpoint + "swap/get24HInfo");
+  const bnb = Number(swap.data.data.bnbBalance) * 2; // swap TVL (dual sided 50/50 AMM pool)
+
   return {
-    tether: swap.data.data.usd_balance,
-    binancecoin: swap.data.data.wbnb_balance,
+    bnb,
   }
 }
 
 module.exports = {
   methodology: `${lendingMarket}. Defibox TVL is achieved by making a call to its API: https://dapp.defibox.io/api/.`,
   timetravel: false,
+  misrepresentedTokens: true,
   eos: {
     tvl: eos,
     borrowed,
   },
-  bsc: {
-    tvl: bsc
-  },
   wax: {
     tvl: wax
   },
+  bsc: {
+    tvl: () => ({}) // TODO: BSC endpoint not available at the moment
+  }
 }
