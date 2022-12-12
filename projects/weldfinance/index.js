53e5a05b2e7150aeb9c9c59207b1a4e7143d804a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-12 20:33:16 +0100

    Add usdk mapping

--- projects/weldfinance/index.js
+++ projects/weldfinance/index.js
@@ -1,4 +1,39 @@
 const { masterchefExports, } = require("../helper/unknownTokens")
+const sdk = require('@defillama/sdk')
+
+async function verifyTvl() {
+  const usdkSupply = await sdk.api2.abi.call({
+    target: '0x472402d47Da0587C1cf515DAfbAFc7bcE6223106',
+    abi: 'erc20:totalSupply',
+    chain: 'kava',
+  })
+
+  const fireBlockAccount = '0x07B8F3e3D3fCf5b6D8cf1a49B92047008EE991E8'
+
+  const bals = await sdk.api2.abi.multiCall({
+    abi: 'erc20:balanceOf',
+    calls: [
+      { target: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', params: fireBlockAccount },
+      { target: '0xdac17f958d2ee523a2206206994597c13d831ec7', params: fireBlockAccount },
+    ],
+  })
+
+  const balsPoly = await sdk.api2.abi.multiCall({
+    abi: 'erc20:balanceOf',
+    chain: 'polygon',
+    calls: [
+      { target: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', params: fireBlockAccount },
+      { target: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', params: fireBlockAccount },
+    ],
+  })
+
+  const backing = [...bals, ...balsPoly].reduce((a, i) => a + i/1e6, 0)
+
+  sdk.log('usdk supply: ', usdkSupply / 1e18)
+  sdk.log('usdk backing: ', backing)
+  if (usdkSupply > backing) throw new Error('USDk supply is higher than backing')
+  return {}
+}
 
 module.exports = masterchefExports({
   chain: 'kava',
@@ -6,3 +41,5 @@ module.exports = masterchefExports({
   masterchef: '0xAbF3edbDf79dAfBBd9AaDBe2efEC078E557762D7',
   nativeToken: '0xa0EEDa2e3075092d66384fe8c91A1Da4bcA21788'
 })
+
+module.exports.kava.tvl = sdk.util.sumChainTvls([module.exports.kava.tvl, verifyTvl])
