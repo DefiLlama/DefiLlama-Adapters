053e9bab6cedb69b937032706607655f052cf2de
崔棉大师 <1046166@qq.com>
2022-12-14 00:50:24 +0800

    update ampl vault

--- projects/meterBridge/index.js
+++ projects/meterBridge/index.js
@@ -2,6 +2,8 @@ const sdk = require('@defillama/sdk');
 const addresses = require("./addresses.json");
 const BigNumber = require("bignumber.js");
 
+const ampl = "0xD46bA6D942050d489DBd938a2C909A5d5039A161";
+const amplVault = "0x805c7Ecba41f9321bb098ec1cf31d86d9407de2F"
 
 function getTvl(chain) {
     return async (timestamp, _block, chainBlocks) => {
@@ -13,7 +15,15 @@ function getTvl(chain) {
             [json.Wrapped]:
                 (await sdk.api.eth.getBalance({ target, chain: chain })).output
         };
-        // console.log("balances:",balances,chain)
+        if (chain == "ethereum") {
+            sdk.util.sumSingleBalance(balances, ampl, (await sdk.api.abi.call({
+                target: ampl,
+                params: amplVault,
+                abi: 'erc20:balanceOf',
+                block: block,
+                chain: chain
+            })).output)
+        }
         const holdTokens = await sdk.api.abi.multiCall({
             calls: json.Tokens.map(token => ({
                 target: token,
@@ -41,25 +51,25 @@ module.exports = {
     ethereum: {
         tvl: getTvl('ethereum'),
     },
-    bsc: {
-        tvl: getTvl('bsc'),
-    },
-    moonriver: {
-        tvl: getTvl('moonriver'),
-    },
-    meter: {
-        tvl: getTvl('meter'),
-    },
-    theta: {
-        tvl: getTvl('theta'),
-    },
-    avax: {
-        tvl: getTvl('avax'),
-    },
-    moonbeam: {
-        tvl: getTvl('moonbeam'),
-    },
-    polygon: {
-        tvl: getTvl('polygon'),
-    }
+    // bsc: {
+    //     tvl: getTvl('bsc'),
+    // },
+    // moonriver: {
+    //     tvl: getTvl('moonriver'),
+    // },
+    // meter: {
+    //     tvl: getTvl('meter'),
+    // },
+    // theta: {
+    //     tvl: getTvl('theta'),
+    // },
+    // avax: {
+    //     tvl: getTvl('avax'),
+    // },
+    // moonbeam: {
+    //     tvl: getTvl('moonbeam'),
+    // },
+    // polygon: {
+    //     tvl: getTvl('polygon'),
+    // }
 }; 
