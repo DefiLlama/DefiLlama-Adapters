af77fe9daed2c85cbb4420634098a9ec6acb9812
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2022-12-13 11:46:50 -0500

    Update index.js

--- projects/ABCDEFX/index.js
+++ projects/ABCDEFX/index.js
@@ -2,7 +2,7 @@ const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
   misrepresentedTokens: true,
-  methodoogy: "Using DefiLlama's SDK to make on-chain calls to a Uniswap-like Factory Contract to iterate over Liquidity Pools.",
+  methodology: "Using DefiLlama's SDK to make on-chain calls to a Uniswap-like Factory Contract to iterate over Liquidity Pools.",
   xdai:     { tvl: getUniTVL({ chain: 'xdai',     factory: '0xbBD6e78961b13498F313883632D315ebD4Adbd6D',  useDefaultCoreAssets: true, }) },
   fantom:   { tvl: getUniTVL({ chain: 'echelon',  factory: '0xbBD6e78961b13498F313883632D315ebD4Adbd6D',  useDefaultCoreAssets: true, }) },
   kcc:      { tvl: getUniTVL({ chain: 'echelon',  factory: '0xbBD6e78961b13498F313883632D315ebD4Adbd6D',  useDefaultCoreAssets: true, }) },
