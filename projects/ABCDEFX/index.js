f1881e8658b7679b554da27e53bd469dda683b51
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2022-12-13 11:48:49 -0500

    Update index.js

--- projects/ABCDEFX/index.js
+++ projects/ABCDEFX/index.js
@@ -3,9 +3,9 @@ const { getUniTVL } = require('../helper/unknownTokens')
 module.exports = {
   misrepresentedTokens: true,
   methodology: "Using DefiLlama's SDK to make on-chain calls to a Uniswap-like Factory Contract to iterate over Liquidity Pools.",
-  xdai:     { tvl: getUniTVL({ chain: 'xdai',     factory: '0xbBD6e78961b13498F313883632D315ebD4Adbd6D',  useDefaultCoreAssets: true, }) },
-  fantom:   { tvl: getUniTVL({ chain: 'echelon',  factory: '0xbBD6e78961b13498F313883632D315ebD4Adbd6D',  useDefaultCoreAssets: true, }) },
-  kcc:      { tvl: getUniTVL({ chain: 'echelon',  factory: '0xbBD6e78961b13498F313883632D315ebD4Adbd6D',  useDefaultCoreAssets: true, }) },
-  echelon:  { tvl: getUniTVL({ chain: 'echelon',  factory: '0xbBD6e78961b13498F313883632D315ebD4Adbd6D',  useDefaultCoreAssets: true, }) },
-  multivac: { tvl: getUniTVL({ chain: 'echelon',  factory: '0xbBD6e78961b13498F313883632D315ebD4Adbd6D',  useDefaultCoreAssets: true, }) },
+  xdai:     { tvl: getUniTVL({ chain: 'xdai',     factory: '0x01F43D2A7F4554468f77e06757e707150e39130c',  useDefaultCoreAssets: true, }) },
+  fantom:   { tvl: getUniTVL({ chain: 'echelon',  factory: '0x01F43D2A7F4554468f77e06757e707150e39130c',  useDefaultCoreAssets: true, }) },
+  kcc:      { tvl: getUniTVL({ chain: 'echelon',  factory: '0x01F43D2A7F4554468f77e06757e707150e39130c',  useDefaultCoreAssets: true, }) },
+  echelon:  { tvl: getUniTVL({ chain: 'echelon',  factory: '0x01F43D2A7F4554468f77e06757e707150e39130c',  useDefaultCoreAssets: true, }) },
+  multivac: { tvl: getUniTVL({ chain: 'echelon',  factory: '0x01F43D2A7F4554468f77e06757e707150e39130c',  useDefaultCoreAssets: true, }) },
 };
