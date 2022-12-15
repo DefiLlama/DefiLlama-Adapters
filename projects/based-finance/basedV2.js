dd534993d77478cbc52200a0f02b2173126b58dc
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-15 18:11:42 +0100

    based finance: minor code refactor

--- projects/based-finance/basedV2.js
+++ projects/based-finance/basedV2.js
@@ -1,41 +1,23 @@
-const sdk = require("@defillama/sdk");
 const { sumTokens2, nullAddress, } = require("../helper/unwrapLPs");
 const { stakings } = require("../helper/staking");
 const chain = 'fantom'
 
 //BASED V2
-const obol = "0x1539C63037D95f84A5981F96e43850d1451b6216";
 const smelt = "0x141FaA507855E56396EAdBD25EC82656755CD61e";
 const BoardroomV2 = "0x8ff9eFB99D522fAC6a21363b7Ca54d25477637F6";
-const SmeltRewardPool = "0x7A1f47c8a26fD895228947ffc0482F3dD9c2cA29";
 const usdc = "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75";
-const usdcFtmLp = "0x7a6C9B27e20560253d4080944A252494C702f1a2";
 const wftm = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83";
-const godNft = '0xc5777AD2250D7b12a3f2383afB4464b9E49bE297';
 const treasuryAddress = '0x0A10daD90b9C6FB8B87BFf3857A4B012890C53A5';
-const WFTM_SMELT_LP = '0x432a4654BC67ed86B3119cD256c490F2CEA1e42a'
-const WFTM_OBOL_LP = '0xE3E26Bd9cD2F32a8F60BfFf5DF88bB3b3C5Eb9f9'
 
 //BASED V2 Twisted Nodes
 const shortNodes = "0xAEbfF260074782a3DfD8981352b44767A05fa2eD";
 const mediumNodes = "0x525ca3877a78c6AE12292D0a55765775e3943379";
 const longNodes = "0x62A2Ff4BcCC5dD5316C358cDF079EC5e5c0851fe";
 
-async function pool2(_, _b, {fantom: block }) {
-  return sumTokens2({ owner: SmeltRewardPool, tokens: [
-    WFTM_SMELT_LP,
-    WFTM_OBOL_LP,
-  ], block, chain, })
-}
-
 async function treasury(_, _b, { fantom: block }) {
   return sumTokens2({ owner: treasuryAddress, tokens: [
     usdc,
-    WFTM_SMELT_LP,
-    WFTM_OBOL_LP,
     wftm,
-    obol,
-    smelt,
     nullAddress,
   ], block, chain, })
 }
@@ -43,7 +25,6 @@ async function treasury(_, _b, { fantom: block }) {
 module.exports = {
   fantom: {
     tvl: async () => ({}),
-    pool2,
     treasury,
     staking: stakings([BoardroomV2, shortNodes, mediumNodes, longNodes, ], smelt, "fantom"),
   },
--- projects/based-finance/index.js
+++ projects/based-finance/index.js
@@ -1,53 +1,25 @@
-const sdk = require("@defillama/sdk");
-const { mergeExports } = require('../helper/utils')
 const { sumTokens2, } = require("../helper/unwrapLPs");
 const { staking } = require("../helper/staking");
-const basedV2Exports = require('./basedV2')
 const { getUniTVL } = require("../helper/unknownTokens")
-const { getALLTVL } = require("../helper/yieldHelper")
 const chain = 'fantom'
 
-const usdcRewardPoolAddress = "0x3A554736b2aF99F5574Ee630Db5Ae33Af118235e";
 const acropolisAddress = "0xe5009dd5912a68b0d7c6f874cd0b4492c9f0e5cd";
 const treasuryAddress = "0xa0e0f462d66de459711bc721ce1fdcc3d9405831";
 
 // Token Addresses
 const basedTokenAddress = "0x8d7d3409881b51466b483b11ea1b8a03cded89ae";
 const bshareTokenAddress = "0x49c290ff692149a4e16611c694fded42c954ab7a";
-const bbondAddress = "0xC078285F16665B3F4bCe74AbDCF0f4C877de3E9f";
 const usdcAddress = "0x04068da6c83afcfa0e13ba15a6696662335d5b75";
 const usdtAddress = "0x049d68029688eAbF473097a2fC38ef61633A3C7A";
 const maiAddress = "0xfB98B335551a418cD0737375a2ea0ded62Ea213b";
 const wftmAddress = "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83";
-const tshareAddress = "0x4cdF39285D7Ca8eB3f090fDA0C069ba5F4145B37";
-
-// LP Addresses
-const basedFtmLpAddress = "0x0981916Bd96d375dAd0067Fd24E19120D2fcF5e6";
-const bshareFtmLpAddress = "0x52A5B9E36F53b54Ed9ddd7a4e66Ac26696E9F0be";
-const ftmUsdcLpAddress = "0x7a6C9B27e20560253d4080944A252494C702f1a2";
-
-const poolLPs = [
-  basedFtmLpAddress,
-  bshareFtmLpAddress,
-  bbondAddress
-];
 
 const treasuryTokens = [
   usdcAddress,
   usdtAddress,
   maiAddress,
   wftmAddress,
-  basedTokenAddress,
-  bshareTokenAddress,
-  bbondAddress,
-  basedFtmLpAddress,
-  bshareFtmLpAddress,
-  ftmUsdcLpAddress
 ];
-
-async function pool2(_, _b, {fantom: block }) {
-  return sumTokens2({ owner: usdcRewardPoolAddress, tokens: poolLPs, block, chain, })
-}
 
 async function treasury(_, _b, { fantom: block }) {
   return sumTokens2({ owner: treasuryAddress, tokens: treasuryTokens, block, chain, })
@@ -59,24 +31,11 @@ module.exports = {
     tvl: getUniTVL({
              factory: '0x407C47E3FDB7952Ee53aa232B5f28566A024A759',
              chain: 'fantom',
-             useDefaultCoreAssets: true,
            }),
-    pool2,
     staking: staking(acropolisAddress, bshareTokenAddress, "fantom"),
     treasury
   },
 };
-
-// module.exports = {
-//   methodology: "Counts the tokens locked on AMM pools using the factory contract(0xE236f6890F1824fa0a7ffc39b1597A5A6077Cfe9) to find all of the pairs",
-//   fantom: {
-//     tvl: getUniTVL({
-//       factory: '0x407C47E3FDB7952Ee53aa232B5f28566A024A759',
-//       chain: 'fantom',
-//       useDefaultCoreAssets: true,
-//     })
-//   },
-// };
 
 module.exports.hallmarks = [
   [Math.floor(new Date('2022-09-30')/1e3), 'Added Based V2'],
