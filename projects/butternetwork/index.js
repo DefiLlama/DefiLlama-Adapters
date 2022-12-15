1a4177681ae2fc2e84095373f3097cc24451ea4b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-15 23:08:12 +0100

    code refactor

--- projects/butternetwork/index.js
+++ projects/butternetwork/index.js
@@ -1,5 +1,4 @@
-const sdk = require('@defillama/sdk');
-const near = require('../helper/chain/near');
+const { sumTokensExport } = require('../helper/sumTokens')
 
 const config = {
   'bsc': {
@@ -22,59 +21,13 @@ const config = {
   }
 }
 
-async function bscTvl(timestamp, block, chainBlocks) {
-  return await evmTvl('bsc', timestamp, block, chainBlocks)
-}
-
-async function polygonTvl(timestamp, block, chainBlocks) {
-  return await evmTvl('polygon', timestamp, block, chainBlocks)
-}
-
-async function nearTvl(timestamp, block, chainBlocks) {
-  const chain = 'near';
-  const balances = {};
-
-  const mosAddress = config[chain].mosAddress;
-  const tokenHoldingList = config[chain].tokenHoldingList;
-
-  for (let token of tokenHoldingList) {
-    const balance = await near.getTokenBalance(token, mosAddress);
-    sdk.util.sumSingleBalance(balances, token, balance)
-  }
-
-  return balances;
-}
-
-async function evmTvl(chain, timestamp, block, chainBlocks) {
-  const mosAddress = config[chain].mosAddress;
-  const tokenHoldingList = config[chain].tokenHoldingList;
-
-  const tokenBalances = await sdk.api.abi.multiCall({
-    calls: tokenHoldingList.map((tokenAddress) => ({
-      target: tokenAddress,
-      params: mosAddress
-    })),
-    abi: 'erc20:balanceOf',
-    block: chainBlocks[chain],
-    chain: chain
-  })
-
-  const balances = {};
-  sdk.util.sumMultiBalanceOf(balances, tokenBalances)
-  return balances;
-}
-
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'get the amount of token deposited in MOS contract on each supported chain.',
-  bsc: {
-    tvl: bscTvl,
-  },
-  polygon: {
-    tvl: polygonTvl
-  },
-  near: {
-    tvl: nearTvl
-  }
 };
+
+Object.keys(config).forEach(chain => {
+  const { mosAddress, tokenHoldingList } = config[chain]
+  module.exports[chain] = {
+    tvl:sumTokensExport({ owner: mosAddress, tokens: tokenHoldingList, chain, })
+  }
+})
