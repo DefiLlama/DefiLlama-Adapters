d3c3557bc88111f6b3462c28d0795ccbc1b9a82b
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-12-17 17:59:51 +0200

    TVL and TVS from xexchange.com

--- projects/maiar/index.js
+++ projects/maiar/index.js
@@ -1,6 +1,5 @@
 const { request, gql } = require("graphql-request");
 const { toUSDTBalances } = require('../helper/balances')
-const { sumTokensExport } = require('../helper/sumTokens')
 
 const LiquidityQuery = gql`
 {
@@ -11,22 +10,31 @@ const LiquidityQuery = gql`
   }
 `
 
+const StakeQuery = gql`
+{
+    factory {
+      totalValueStakedUSD
+    }
+  }
+`
+
 async function tvl() {
   const results = await request("https://graph.maiar.exchange/graphql", LiquidityQuery)
 
   return toUSDTBalances(results.factory.totalValueLockedUSD)
 }
 
-const stakingContracts = [
-  "erd1qqqqqqqqqqqqqpgq7qhsw8kffad85jtt79t9ym0a4ycvan9a2jps0zkpen",
-  "erd1qqqqqqqqqqqqqpgqv4ks4nzn2cw96mm06lt7s2l3xfrsznmp2jpsszdry5"
-]
+async function tvs() {
+  const results = await request("https://graph.maiar.exchange/graphql", StakeQuery)
+
+  return toUSDTBalances(results.factory.totalValueStakedUSD)
+}
 
 module.exports = {
   misrepresentedTokens: true,
   timetravel: false,
   elrond: {
     tvl,
-    staking: sumTokensExport({ chain: 'elrond', owners: stakingContracts, token: 'MEX-455c57' })
+    staking: tvs
   },
 }
