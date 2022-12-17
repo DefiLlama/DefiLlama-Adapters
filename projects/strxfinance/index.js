565370ae5eb1bb5a81fe55b3c304611ca401ed62
STRX FINANCE <116388041+strxfinance@users.noreply.github.com>
2022-12-18 01:19:42 +0530

    Use web3 call

--- projects/strxfinance/index.js
+++ projects/strxfinance/index.js
@@ -1,19 +1,28 @@
-const { getTrxBalance } = require('../helper/chain/tron');
-const STAKINGPOOL_ADDRESS = 'TGrdCu9fu8csFmQptVE25fDzFmPU9epamH';
-const FREEZE_ADDRESS = 'TSTrx3UteLMBdeGe9Edwwi2hLeQCmLPZ5g';
+const {
+	postURL
+} = require('../helper/utils');
+
+async function getCurrentStake() {
+	let postdata = {
+		"contract_address": "414b8a2c619bccb710206b3d11e28dce62d8d72a8b",
+		"owner_address": "4128fb7be6c95a27217e0e0bff42ca50cd9461cc9f",
+		"function_selector": "reservedTRX()",
+		"parameter": "",
+		"call_value": 0
+	};
+	let result = await postURL('https://api.trongrid.io/wallet/triggerconstantcontract', postdata);
+	let trx = parseInt(result.data.constant_result[0], 16) / 10 ** 6;
+	return trx;
+}
 
 async function tvl() {
-  const [stakingBalance, freezeBalance] = await Promise.all([
-    getTrxBalance(STAKINGPOOL_ADDRESS),
-    getTrxBalance(FREEZE_ADDRESS),
-  ]);
-  return  {
-    "tron": (stakingBalance + freezeBalance) / 10 ** 6,
-  };
+	return {
+		"tron": await getCurrentStake()
+	}
 }
 
 module.exports = {
-  tron: {
-    tvl,
-  },
+	tron: {
+		tvl,
+	},
 }
