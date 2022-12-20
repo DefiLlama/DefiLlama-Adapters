796ea6ed02553dc939a1bc535d1b698c981cdb4f
rohitpoly <99714571+rohitpoly@users.noreply.github.com>
2022-12-20 16:15:07 +0530

    TVL added

--- projects/polytrade/index.js
+++ projects/polytrade/index.js
@@ -4,18 +4,18 @@ const TRADE_TOKEN_CONTRACT = '0x63168f8335B0CF03d58C80e182b9aEa4558064Da';
 const LENDER_POOL_CONTRACT = '0xE544a0Ca5F4a01f137AE5448027471D6a9eC9661';
 
 
-async function tvl(timestamp, block, chainBlocks) {
+async function tvl(_, _block, chainBlocks) {
   const balances = {};
   const transform = await transformBscAddress();
 
-  const collateralBalance = (await sdk.api.abi.call({
+  const liquidityBalance = (await sdk.api.abi.call({
     abi: ABI,
     chain: 'polygon',
     target: LENDER_POOL_CONTRACT,
     block: chainBlocks['polygon'],
   })).output;
 
-  await sdk.util.sumSingleBalance(balances, `polygon:${TRADE_TOKEN_CONTRACT}`, collateralBalance)
+  await sdk.util.sumSingleBalance(balances, `polygon:${TRADE_TOKEN_CONTRACT}`, liquidityBalance)
 
   return balances;
 }
@@ -25,7 +25,7 @@ module.exports = {
   misrepresentedTokens: false,
   methodology: 'gets the amount in liquidity pool',
   start: 1657074187,
-  bsc: {
+  polygon: {
     tvl,
   }
 }; 
