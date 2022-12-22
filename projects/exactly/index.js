b41e5164cfb2eca3f1d4018314bc21782352f269
danilo neves cruz <cruzdanilo@gmail.com>
2022-12-22 13:59:53 -0500

    👔 exactly: fetch all fixed pools

--- projects/exactly/index.js
+++ projects/exactly/index.js
@@ -2,39 +2,37 @@ const { api2 } = require("@defillama/sdk");
 const {
   MULTICALL_ADDRESS_MAINNET,
 } = require("@defillama/sdk/build/abi/multicall");
-const {
-  utils: { Interface },
-  Contract,
-} = require("ethers");
+const { Contract } = require("@ethersproject/contracts");
+const { Interface, FormatTypes } = require("@ethersproject/abi");
 
-const marketABI = new Interface([
-  "function asset() view returns (address)",
-  "function fixedPools(uint256) view returns ((uint256 borrowed, uint256 supplied, uint256, uint256))",
-  "function maxFuturePools() view returns (uint8)",
-  "function totalAssets() view returns (uint256)",
-  "function totalFloatingBorrowAssets() view returns (uint256)",
-]);
+/** @type {Record<string, { start: number, auditor: string, multicall: string }>} */
+const chains = {
+  ethereum: {
+    start: 15868410,
+    auditor: "0x310A2694521f75C7B2b64b5937C16CE65C3EFE01",
+    multicall: MULTICALL_ADDRESS_MAINNET,
+  },
+};
 
 /** @type {(chain: string, timestamp: number, block: number) => Promise<Market[]>} */
 const markets = async (chain, timestamp, block) => {
   const { [chain]: provider } = api2.config.providers;
   const auditor = new Contract(
-    {
-      ethereum: "0x310A2694521f75C7B2b64b5937C16CE65C3EFE01",
-    }[chain],
+    chains[chain].auditor,
     ["function allMarkets() view returns (address[])"],
     provider
   );
   const multicall = new Contract(
-    {
-      ethereum: MULTICALL_ADDRESS_MAINNET,
-    }[chain],
+    chains[chain].multicall,
     ["function aggregate((address,bytes)[]) view returns (uint256, bytes[])"],
     provider
   );
 
-  /** @type {string[]} */
-  const markets = await auditor.allMarkets({ blockTag: block });
+  /** @type {[string[], { timestamp: number }]} */
+  const [markets, { timestamp: startTimestamp }] = await Promise.all([
+    auditor.allMarkets({ blockTag: block }),
+    provider.getBlock(chains[chain].start),
+  ]);
 
   const getters = [
     "asset",
@@ -45,7 +43,7 @@ const markets = async (chain, timestamp, block) => {
   /** @type {[BigNumber, string[]]} */
   const [, gettersData] = await multicall.aggregate(
     markets.flatMap((market) =>
-      getters.map((fn) => [market, marketABI.encodeFunctionData(fn)])
+      getters.map((fn) => [market, abi.encodeFunctionData(fn)])
     ),
     { blockTag: block }
   );
@@ -56,7 +54,7 @@ const markets = async (chain, timestamp, block) => {
     const [asset, maxFuturePools, totalAssets, totalFloatingBorrowAssets] =
       gettersData
         .slice(i * getters.length, (i + 1) * getters.length)
-        .map((d, j) => marketABI.decodeFunctionResult(getters[j], d)[0]);
+        .map((d, j) => abi.decodeFunctionResult(getters[j], d)[0]);
     return {
       asset,
       market,
@@ -67,40 +65,37 @@ const markets = async (chain, timestamp, block) => {
   });
 
   const INTERVAL = 86_400 * 7 * 4;
-  const maxFuturePools = marketData.reduce(
-    (max, { maxFuturePools }) => Math.max(max, maxFuturePools),
-    0
-  );
-  /** @type {[BigNumber, string[]]} */
-  const [, fixedPoolsData] = await multicall.aggregate(
-    marketData.flatMap(
-      ({ market }) =>
-        [...Array(maxFuturePools)].map((_, i) => [
-          market,
-          marketABI.encodeFunctionData("fixedPools", [
-            timestamp - (timestamp % INTERVAL) + INTERVAL * (i + 1),
-          ]),
-        ]),
-      { blockTag: block }
-    )
-  );
+  const minMaturity = startTimestamp - (startTimestamp % INTERVAL) + INTERVAL;
+  const maxMaturity =
+    timestamp -
+    (timestamp % INTERVAL) +
+    INTERVAL *
+      marketData.reduce(
+        (max, { maxFuturePools }) => Math.max(max, maxFuturePools),
+        0
+      );
+  const fixedPoolCount = (maxMaturity - minMaturity) / INTERVAL + 1;
+  /** @type {{ borrowed: string, supplied: string }[]} */
+  const fixedPools = await api2.abi.multiCall({
+    abi: JSON.parse(abi.getFunction("fixedPools").format(FormatTypes.json)),
+    calls: marketData.flatMap(({ market }) =>
+      [...Array(fixedPoolCount)].map((_, i) => ({
+        target: market,
+        params: [minMaturity + INTERVAL * i],
+      }))
+    ),
+    block,
+  });
 
   return marketData.map(
     ({ asset, totalFloatingBorrowAssets, totalFloatingDepositAssets }, i) => ({
       asset,
-      fixedPools: fixedPoolsData
-        .slice(i * maxFuturePools, (i + 1) * maxFuturePools)
-        .map((data) => {
-          /** @type {{ borrowed: BigNumber, supplied: BigNumber }} */
-          const { borrowed, supplied } = marketABI.decodeFunctionResult(
-            "fixedPools",
-            data
-          )[0];
-          return {
-            borrowed: borrowed.toBigInt(),
-            supplied: supplied.toBigInt(),
-          };
-        }),
+      fixedPools: fixedPools
+        .slice(i * fixedPoolCount, (i + 1) * fixedPoolCount)
+        .map(({ borrowed, supplied }) => ({
+          borrowed: BigInt(borrowed),
+          supplied: BigInt(supplied),
+        })),
       totalFloatingBorrowAssets,
       totalFloatingDepositAssets,
     })
@@ -110,26 +105,24 @@ const markets = async (chain, timestamp, block) => {
 /** @type {(chain: string) => (timestamp: number, block: number, chainBlocks: Record<string, number>) => Promise<Record<string, bigint>>} */
 const tvl =
   (chain) =>
-  async (timestamp, _, { [chain]: block }) =>
-    Object.fromEntries(
-      await Promise.all(
-        Object.values(await markets(chain, timestamp, block)).map(
-          async ({
+  (timestamp, _, { [chain]: block }) =>
+    markets(chain, timestamp, block).then((data) =>
+      Object.fromEntries(
+        data.map(
+          ({
             asset,
             fixedPools,
             totalFloatingBorrowAssets,
             totalFloatingDepositAssets,
-          }) => {
-            return [
-              asset,
-              fixedPools.reduce(
-                (total, { borrowed, supplied }) => total + supplied - borrowed,
-                0n
-              ) +
-                totalFloatingDepositAssets -
-                totalFloatingBorrowAssets,
-            ];
-          }
+          }) => [
+            asset,
+            fixedPools.reduce(
+              (total, { borrowed, supplied }) => total + supplied - borrowed,
+              0n
+            ) +
+              totalFloatingDepositAssets -
+              totalFloatingBorrowAssets,
+          ]
         )
       )
     );
@@ -137,18 +130,14 @@ const tvl =
 /** @type {(chain: string) => (timestamp: number, block: number, chainBlocks: Record<string, number>) => Promise<Record<string, bigint>>} */
 const borrowed =
   (chain) =>
-  async (timestamp, _, { [chain]: block }) =>
-    Object.fromEntries(
-      await Promise.all(
-        Object.values(await markets(chain, timestamp, block)).map(
-          async ({ asset, fixedPools, totalFloatingBorrowAssets }) => {
-            return [
-              asset,
-              fixedPools.reduce((total, { borrowed }) => total + borrowed, 0n) +
-                totalFloatingBorrowAssets,
-            ];
-          }
-        )
+  (timestamp, _, { [chain]: block }) =>
+    markets(chain, timestamp, block).then((data) =>
+      Object.fromEntries(
+        data.map(({ asset, fixedPools, totalFloatingBorrowAssets }) => [
+          asset,
+          fixedPools.reduce((total, { borrowed }) => total + borrowed, 0n) +
+            totalFloatingBorrowAssets,
+        ])
       )
     );
 
@@ -156,14 +145,23 @@ module.exports = {
   timetravel: true,
   methodology:
     "Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There are multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending.",
-  ethereum: {
-    start: 15868410,
-    tvl: tvl("ethereum"),
-    borrowed: borrowed("ethereum"),
-  },
+  ...Object.fromEntries(
+    Object.entries(chains).map(([chain, { start }]) => [
+      chain,
+      { start, tvl: tvl(chain), borrowed: borrowed(chain) },
+    ])
+  ),
 };
 
-/** @typedef {import("ethers").BigNumber} BigNumber */
+const abi = new Interface([
+  "function asset() view returns (address)",
+  "function fixedPools(uint256) view returns ((uint256 borrowed, uint256 supplied, uint256, uint256))",
+  "function maxFuturePools() view returns (uint8)",
+  "function totalAssets() view returns (uint256)",
+  "function totalFloatingBorrowAssets() view returns (uint256)",
+]);
+
+/** @typedef {import("@ethersproject/bignumber").BigNumber} BigNumber */
 /** @typedef {{ borrowed: bigint, supplied: bigint }} FixedPool */
 /**
  * @typedef Market
