57f369b8e92b875a63795721a04d1b3b4f76bc12
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-23 15:45:28 +0100

    symbiosis-finance: code refactor

--- projects/symbiosis-finance/index.js
+++ projects/symbiosis-finance/index.js
@@ -1,7 +1,5 @@
-const sdk = require('@defillama/sdk');
-const erc20Abi = require("../helper/abis/erc20.json")
-const {getChainTransform} = require("../helper/portedTokens");
 const config = require("./config");
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
   methodology: 'Counts the amount of stables locked in Symbiosis protocol contracts: Portals V1, NervePools V1, Portals V2, OmniPool V2',
@@ -10,20 +8,6 @@ module.exports = {
 config.chains.forEach(chainInfo => {
   const {name: chain, stable, holders} = chainInfo
   module.exports[chain] = {
-    tvl: async (_, _b, {[chain]: block}) => {
-      const calls = holders.map((holder) => {
-        return {target: stable, params: holder}
-      })
-      const output = await sdk.api.abi.multiCall({
-        chain,
-        block,
-        abi: erc20Abi.balanceOf,
-        calls,
-      })
-      const transformAddress = await getChainTransform(chain)
-      const tvl = {}
-      sdk.util.sumMultiBalanceOf(tvl, output, true, transformAddress)
-      return tvl
-    }
+    tvl: sumTokensExport({ chain, tokens: [ stable ], owners: holders })
   }
 })
