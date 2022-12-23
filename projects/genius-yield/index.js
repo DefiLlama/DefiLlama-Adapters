1cccd51b3257b8e47d61f53877d5eb1b639d3295
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-23 12:11:23 +0100

    lago bridge: add hallmark

--- projects/genius-yield/index.js
+++ projects/genius-yield/index.js
@@ -1,25 +1,23 @@
-const axios = require("axios");
+const { getTokenBalance, getTokenPrice } = require('../helper/chain/cardano')
+
+const asset = 'dda5fdb1002f7389b33e036b6afee82a8189becb6cba852e8b79b4fb'
+const asset1 = 'dda5fdb1002f7389b33e036b6afee82a8189becb6cba852e8b79b4fb0014df1047454e53'
+const owner = 'addr1w8r99sv75y9tqfdzkzyqdqhedgnef47w4x7y0qnyts8pznq87e4wh'
 
 async function staking() {
+  const [ bal, price] = await Promise.all([
+    getTokenBalance(asset1, owner),
+    getTokenPrice(asset)
+  ])
+
   return {
-    "genius-yield":
-      (await axios.get(
-        "https://cardano-mainnet.blockfrost.io/api/v0/addresses/addr1w8r99sv75y9tqfdzkzyqdqhedgnef47w4x7y0qnyts8pznq87e4wh",
-        {
-          headers: {
-            project_id: "mainnetTV9qV3mfZXbE6e44TVGMe1UoRlLrpSQt"
-          }
-        }
-      )).data.amount.find(
-        token =>
-          token.unit ===
-          "dda5fdb1002f7389b33e036b6afee82a8189becb6cba852e8b79b4fb0014df1047454e53"
-      ).quantity / 1e6
+    cardano: bal * price / 1e6
   };
 }
 
 module.exports = {
   timetravel: false,
+  misrepresentedTokens: true,
   cardano: {
     staking,
     tvl: () => ({})
