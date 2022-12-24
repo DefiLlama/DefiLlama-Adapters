0fc0b99d71355b0918aee95e2a9c7454f54c6066
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-24 16:26:06 +0100

    [exactly] fix bug

--- projects/exactly/index.js
+++ projects/exactly/index.js
@@ -8,22 +8,23 @@ module.exports = {
 const config = {
   ethereum: {
     auditor: '0x310A2694521f75C7B2b64b5937C16CE65C3EFE01',
+    startTimestamp: 1667223731,
   }
 }
 
 Object.keys(config).forEach(chain => {
-  const { auditor } = config[chain]
+  const { auditor, startTimestamp, } = config[chain]
   module.exports[chain] = {
     tvl: async (_, _b, _cb, { api, }) => {
 
       const balances = {}
-      const data = await markets(api, auditor)
+      const data = await markets(api, auditor, startTimestamp)
 
       data[0].forEach((_, i) => {
         const asset = data[0][i]
         const totalAssets = data[1][i]
         const totalFloatingBorrowAssets = data[2][i]
-        const fixedPools = data[3][i]
+        const fixedPools = data[4][i]
 
         sdk.util.sumSingleBalance(balances, asset, totalAssets, chain)
         sdk.util.sumSingleBalance(balances, asset, totalFloatingBorrowAssets * -1, chain)
@@ -37,12 +38,12 @@ Object.keys(config).forEach(chain => {
     borrowed: async (_, _b, _cb, { api, }) => {
 
       const balances = {}
-      const data = await markets(api, auditor)
+      const data = await markets(api, auditor, startTimestamp)
 
       data[0].forEach((_, i) => {
         const asset = data[0][i]
         const totalFloatingBorrowAssets = data[2][i]
-        const fixedPools = data[3][i]
+        const fixedPools = data[4][i]
 
         sdk.util.sumSingleBalance(balances, asset, totalFloatingBorrowAssets, chain)
         fixedPools.forEach(({ borrowed }) => {
@@ -54,16 +55,33 @@ Object.keys(config).forEach(chain => {
   }
 })
 
-async function markets(api, target) {
+const INTERVAL = 86_400 * 7 * 4;
+
+async function markets(api, target, startTimestamp) {
   const markets = await api.call({ abi: abis.allMarkets, target })
+  const timestamp = api.timestamp
 
   const getters = [
     "asset",
     "totalAssets",
     "totalFloatingBorrowAssets",
+    "maxFuturePools",
   ]
   const gettersData = await Promise.all(getters.map(key => api.multiCall({ abi: abis[key], calls: markets })))
-  const fixedPools = await Promise.all(markets.map(target => api.fetchList({ target, lengthAbi: abis.maxFuturePools, itemAbi: abis.fixedPools, })))
+  const params = []
+  const futurePoolsMax = gettersData[3].reduce((a, i) => a > +i ? a : +i, 0)
+
+  markets.forEach((_, i) => {
+    params[i] = []
+    const minMaturity = startTimestamp - (startTimestamp % INTERVAL) + INTERVAL;
+    const maxMaturity = timestamp - (timestamp % INTERVAL) + INTERVAL * futurePoolsMax
+    const fixedPoolCount = (maxMaturity - minMaturity) / INTERVAL + 1;
+
+    for (let j = 0; j < fixedPoolCount; j++)
+      params[i].push(minMaturity + INTERVAL * j)
+  })
+
+  const fixedPools = await Promise.all(markets.map((target, i) => api.multiCall({ target, abi: abis.fixedPools, calls: params[i]})))
 
   gettersData.push(fixedPools)
   return gettersData
