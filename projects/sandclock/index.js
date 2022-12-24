1a7416e00194b05b133f5a5a02364382809a8fcd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-24 15:51:48 +0100

    refactor sandclock

--- projects/sandclock/index.js
+++ projects/sandclock/index.js
@@ -1,5 +1,5 @@
 const sdk = require('@defillama/sdk');
-const abi = require("./abi.json");
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
 const LUSD = '0x5f98805A4E8be255a32880FDeC7F6728C6568bA0';
 const YEARN_VAULT = '0x4fe4bf4166744bcbc13c19d959722ed4540d3f6a';
@@ -10,29 +10,17 @@ const LIQUITY_STRATEGY = '0x537ee18ca78c42e1e7fb87fecfe9fc811c3d3575';
 const vaults = [YEARN_VAULT, LIQUITY_VAULT];
 const strategies = [YERAN_STRATEGY, LIQUITY_STRATEGY];
 
-async function tvl(timestamp, block) {
-  const balances = {};
-
-  const { output: vaultBalances } = await sdk.api.abi.multiCall({
-    abi: 'erc20:balanceOf',
-    target: LUSD,
-    block,
-    calls: vaults.map(vault => ({ params: [vault] })),
-  });
-  vaultBalances.forEach((data) => sdk.util.sumSingleBalance(balances, LUSD, data.output));
-
-  const { output: strategyBalances } = await sdk.api.abi.multiCall({
-    abi: abi.investedAssets,
-    block,
-    calls: strategies.map(strategy => ({ target: strategy })),
-  });
-  strategyBalances.forEach((data) => sdk.util.sumSingleBalance(balances, LUSD, data.output));
-
-  return balances;
+async function tvl(_, _b, _cb, { api, chain, block, }) {
+  const balances = {}
+  const strategyBalances = await api.multiCall({
+    abi: 'uint256:investedAssets',
+    calls: strategies,
+  })
+  strategyBalances.forEach(i => sdk.util.sumSingleBalance(balances, LUSD, i, chain))
+  return sumTokens2({ balances, chain, block, tokens: [LUSD], owners: vaults, })
 }
 
 module.exports = {
-  timetravel: true,
   misrepresentedTokens: false,
   methodology: 'add underlying asset balances in all the vaults and strategies together.',
   doublecounted: true,
