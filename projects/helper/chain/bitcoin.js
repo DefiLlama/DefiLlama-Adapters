b02b67e2d273f10a3aeff53d9074d5c9426e6b6a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-25 17:01:05 +0100

    Added support fetch historical btc balances

--- projects/helper/chain/bitcoin.js
+++ projects/helper/chain/bitcoin.js
@@ -1,11 +1,9 @@
 const sdk = require('@defillama/sdk')
 const { get } = require('../http')
-const { getCache, setCache } = require('../cache')
 const env = require('../env')
 
 const url = addr => 'https://blockstream.info/api/address/' + addr
 
-const project = 'bitcoin'
 const delay = 3 * 60 * 60 // 3 hours
 
 async function getBalanceNow(addr) {
@@ -19,40 +17,23 @@ async function getBalanceNow(addr) {
 async function sumTokens({ balances = {}, owners = [], timestamp }) {
   const bitBals = []
   for (const addr of owners)
-    bitBals.push(getBalance(addr, timestamp))
+    bitBals.push(await getBalance(addr, timestamp))
   sdk.util.sumSingleBalance(balances, 'bitcoin', bitBals.reduce((a, i) => a + i, 0))
   return balances
 }
 
 async function getBalance(addr, timestamp) {
   const now = Date.now() / 1e3
-  if (!timestamp || (now - timestamp) < delay) return getBalanceNow(addr)
+  let balance = await getBalanceNow(addr)
 
-  let cache = await getCache(project, addr)
-  if (!Array.isArray(cache)) cache = [] // initialize cache
+  if (!timestamp || (now - timestamp) < delay) return balance
 
-  const currentCacheLength = cache.length
-  sdk.log('bitcoin', addr, currentCacheLength)
-  let endpoint = `https://btc.getblock.io/${env.GETBLOCK_KEY}/mainnet/blockbook/api/v2/balancehistory/${addr}?fiatcurrency=btc&groupBy=86400`
+  let endpoint = `https://btc.getblock.io/${env.GETBLOCK_KEY}/mainnet/blockbook/api/v2/balancehistory/${addr}?fiatcurrency=btc&groupBy=86400&from=${timestamp}`
 
-  if (cache.length) {
-    const startTime = cache.reduce((a, i) => a > i.time ? a : i.time, 0)
-    endpoint += `&from=${startTime}`
-  }
-  
   const response = await get(endpoint)
-
-  const timeKeys = new Set(cache.map(i => i.time))
-  response.filter(i => !timeKeys.has(i.time)).forEach(i => cache.push(i))
-  if (cache.length > currentCacheLength)  await setCache(project, addr, cache)
-
-  let balance = 0
-  cache.filter(i => (!timestamp || i.time < timestamp))
-    .forEach(({ sent, received }) => balance += +received - +sent)
-
-  sdk.log('bitcoin balance', addr, balance/1e8)
-  return balance / 1e8
-
+  response.forEach(({ sent, received }) => balance += sent / 1e8 - received / 1e8)
+  sdk.log('bitcoin balance', addr, balance)
+  return balance
 }
 
 module.exports = {
