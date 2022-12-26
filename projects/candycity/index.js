bbbb545e8e247b2434d109ea054828d72aa2fee6
DramaCrypto <51626000+DramaCrypto@users.noreply.github.com>
2022-12-26 10:21:48 -0300

    Feat: add staking and vesting value

--- projects/candycity/index.js
+++ projects/candycity/index.js
@@ -23,16 +23,28 @@ const STAKING_CONTRACTS = [
   '0x7CeA583ea310b3A8a72Ed42B3364aff16d24B3A2', // candy lock
   '0xE56C1A8D4E90d82BA06F3f49efEc69f736a32070', // candy => wcro pool
   '0xc568Ce4C714c5Ec819eA8F52596a6Fd9523A2B81', // candy => warz pool,
-  '0x427f1230A547566a51F5Ffd5698BB65c06acA2D2', // candy vesting
 ];
 
+const VESTING_CONTRACTS = [
+  '0x427f1230A547566a51F5Ffd5698BB65c06acA2D2', // candy vesting
+]
+
 async function fetchTvl(timestamp, ethBlock, chainBlocks) {
-    let cmc_response = await get(CMC_API_URL)
-    const candyPrice = cmc_response.data.marketPairs[0].price
-    const balances = await stakings(STAKING_CONTRACTS, CANDY_TOKEN, "cronos")(timestamp, ethBlock, chainBlocks)
-    const candyBalances = Object.values(balances)[0]
-    
-    return toUSDTBalances(BigNumber(candyBalances).times(BigNumber(candyPrice)).div(1e18));
+  let cmc_response = await get(CMC_API_URL)
+  const candyPrice = cmc_response.data.marketPairs[0].price
+  const balances = await stakings(STAKING_CONTRACTS, CANDY_TOKEN, "cronos")(timestamp, ethBlock, chainBlocks)
+  const candyBalances = Object.values(balances)[0]
+
+  return toUSDTBalances(BigNumber(candyBalances).times(BigNumber(candyPrice)).div(1e18));
+};
+
+async function fetchVesting(timestamp, ethBlock, chainBlocks) {
+  let cmc_response = await get(CMC_API_URL)
+  const candyPrice = cmc_response.data.marketPairs[0].price
+  const balances = await stakings(VESTING_CONTRACTS, CANDY_TOKEN, "cronos")(timestamp, ethBlock, chainBlocks)
+  const candyBalances = Object.values(balances)[0]
+
+  return toUSDTBalances(BigNumber(candyBalances).times(BigNumber(candyPrice)).div(1e18));
 };
 
 module.exports = {
@@ -40,7 +52,9 @@ module.exports = {
   methodology:
     "Factory address (0x84343b84EEd78228CCFB65EAdEe7659F246023bf) is used to find the LP pairs. TVL is equal to the liquidity on the AMM and the candy tokens in the staking pools / vault / vesting contract / lock contract.",
   cronos: {
-    tvl: sdk.util.sumChainTvls([chainTvl, fetchTvl]),
-    
+    tvl:  sdk.util.sumChainTvls([chainTvl, fetchTvl]),
+    staking: fetchTvl,
+    vesting: fetchVesting
+
   },
 }; // node test.js projects/crodex/index.js
