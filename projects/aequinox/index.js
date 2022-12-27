48a1aba393b80797e0d0bc50b3cbd8abf8a195af
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-27 21:38:47 +0100

    refactor getLogs code

--- projects/aequinox/index.js
+++ projects/aequinox/index.js
@@ -6,7 +6,7 @@ const vault = '0xee1c8dbfbf958484c6a4571f5fb7b99b74a54aa7'
 
 async function tvl(_, _b, _cb, { api, }) {
   const data = await getLogs({
-    ...api,
+    api,
     target: vault,
     topics: ['0xf5847d3f2197b16cdcd2098ec95d0905cd1abdaf415f07bb7cef2bba8ac5dec4'],
     fromBlock: 20457369,
