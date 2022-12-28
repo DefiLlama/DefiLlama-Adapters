820c66ed16caa76be09529ac6b5f6fbbe655c979
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-28 01:16:26 +0100

    refactor getlogs method

--- projects/bigdataprotocol/index.js
+++ projects/bigdataprotocol/index.js
@@ -3,6 +3,7 @@ const abi = require("./abi.json");
 const { staking } = require("../helper/staking");
 const { addFundsInMasterChef } = require("../helper/masterchef");
 const { sumTokens2 } = require('../helper/unwrapLPs')
+const { getLogs } = require('../helper/cache/getLogs')
 
 const BDPMasterContract = "0x0De845955E2bF089012F682fE9bC81dD5f11B372";
 const BDP = "0xf3dcbc6d72a4e1892f7917b7c43b74131df8480e";
@@ -25,18 +26,24 @@ const getCurrentTokensABI = {
   "type": "function"
 }
 
-const ethTvl = async (_, block) => {
-
-
-  const logs = (
-    await sdk.api.util.getLogs({
-      keys: [],
-      toBlock: block,
-      target: BFactory,
-      fromBlock: 11105585,
-      topic: 'BPoolCreated(address,address)',
-    })
-  ).output
+const ethTvl = async (_, block, _1, { api }) => {
+  const balances = {};
+  /*** BDP Seed Pools (Data Vault seccion) TVL portion ***/
+  await addFundsInMasterChef(
+    balances,
+    BDPMasterContract,
+    block,
+    chain,
+    addr => addr,
+    abi.poolInfo,
+  );
+  return balances
+  const logs = await getLogs({
+    api,
+    target: BFactory,
+    fromBlock: 11105585,
+    topic: 'BPoolCreated(address,address)',
+  })
 
   const pools = logs
     .map((log) => `0x${log.topics[1].substring(26)}`)
@@ -47,22 +54,10 @@ const ethTvl = async (_, block) => {
     chain, block,
   })
 
-  const balances = {};
   const toa = []
 
   tokens.forEach(({ output, input: { target } }) => output.forEach(t => toa.push([t, target])))
 
-  /*** BDP Seed Pools (Data Vault seccion) TVL portion ***/
-  await addFundsInMasterChef(
-    balances,
-    BDPMasterContract,
-    block,
-    chain,
-    addr => addr,
-    abi.poolInfo,
-  );
-
-  // console.log(toa.length)
 
   return sumTokens2({ balances, tokensAndOwners: toa, chain, block, });
 };
@@ -72,7 +67,5 @@ module.exports = {
     staking: staking(BDPMasterContract, BDP),
     tvl: ethTvl,
   },
-  methodology: `Counts liquidity on the Pools of Data Vault and Data Market seccions through BDPMaster Contract
-  and using Subgraph API 'https://subgraph.mainnet.bigdataprotocolmarket.com:8000/subgraphs/name/oceanprotocol/ocean-subgraph'
-  to pulling data`
+  methodology: `Counts liquidity in masterchef`
 }
