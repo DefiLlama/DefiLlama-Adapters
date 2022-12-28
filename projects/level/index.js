69d0c1e3eb5f6c27760435d0f8d118710bd8f9cc
20xlevel <20xlevel@protonmail.com>
2022-12-28 13:36:39 +0700

    update logic calc tvl, pool2

--- projects/level/index.js
+++ projects/level/index.js
@@ -1,56 +1,85 @@
-const BigNumber = require('bignumber.js');
 const sdk = require('@defillama/sdk');
-const { toUSDTBalances } = require('../helper/balances')
+const { transformBalances } = require('../helper/portedTokens')
+const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
 
+const Contracts = {
+    Pool: "0xA5aBFB56a78D2BD4689b25B8A77fd49Bb0675874",
+    Chef: "0x1Ab33A7454427814a71F128109fE5B498Aa21E5d",
+    Tokens: {
+        BTC: "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
+        ETH: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+        WBNB: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+        CAKE: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
+        BUSD: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+        USDT: "0x55d398326f99059fF775485246999027B3197955"
+    },
+    LVL_BNB_LP: "0x70f16782010fa7dDf032A6aaCdeed05ac6B0BC85"
+}
 
 const abi = {
-    getPoolValue: {
-        "inputs": [{ "internalType": "contract IPoolForLens", "name": "_pool", "type": "address" }],
-        "name": "getPoolValue",
-        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    getPoolAsset: {
+        "inputs": [{ "internalType": "address", "name": "_token", "type": "address" }],
+        "name": "getPoolAsset",
+        "outputs": [
+            {
+                "components": [
+                    { "internalType": "uint256", "name": "poolAmount", "type": "uint256" },
+                    { "internalType": "uint256", "name": "reservedAmount", "type": "uint256" },
+                    { "internalType": "uint256", "name": "guaranteedValue", "type": "uint256" },
+                    { "internalType": "uint256", "name": "totalShortSize", "type": "uint256" }
+                ],
+                "internalType": "struct AssetInfo",
+                "name": "",
+                "type": "tuple"
+            }
+        ],
         "stateMutability": "view",
         "type": "function"
     },
 }
 
-const tvl = async () => {
-    const sum = {};
-    const [poolValue, wrapNativeBalanceLVLNativePair, wrapNativeBalanceStableNativePair, stableBalanceStableNativePair] = await Promise.all([
-        sdk.api.abi.call({
-            target: "0x1f0F5b5eE986bD513cDEcFD9E25B72457aA5aD57", // pool lens
-            abi: abi.getPoolValue,
-            params: ["0xA5aBFB56a78D2BD4689b25B8A77fd49Bb0675874"], // Pool
-            chain: 'bsc',
-        }),
-        sdk.api.abi.call({
-            target: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", // WBNB
-            abi: "erc20:balanceOf",
-            params: ["0x70f16782010fa7ddf032a6aacdeed05ac6b0bc85"], // LVL/BNB pair
-            chain: 'bsc',
-        }),
-        sdk.api.abi.call({
-            target: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", // WBNB
-            abi: "erc20:balanceOf",
-            params: ["0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16"], // BNB/BUSD pair
-            chain: 'bsc',
-        }),
-        sdk.api.abi.call({
-            target: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", // BUSD
-            abi: "erc20:balanceOf",
-            params: ["0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16"], // BNB/BUSD pair
-            chain: 'bsc',
-        }),
-    ]);
-    const wrapNativePrice = new BigNumber(stableBalanceStableNativePair.output)
-        .times(new BigNumber(10).pow(12))
-        .div(new BigNumber(wrapNativeBalanceStableNativePair.output));
+const calcTvl = async (_, _b, chainBlocks, { chain }) => {
+    const balances = {};
+    const { output: data } = await sdk.api.abi.multiCall({
+        target: Contracts.Pool,
+        abi: abi.getPoolAsset,
+        calls: Object.values(Contracts.Tokens).map(t => ({
+            params: [t],
+        })),
+        block: chainBlocks.bsc,
+        chain,
+    })
+    Object.values(Contracts.Tokens).forEach((token, i) => {
+        sdk.util.sumSingleBalance(balances, token, data[i].output.poolAmount || 0)
+    })
+    return transformBalances(chain, balances)
+}
 
-    const tvl = wrapNativePrice.times(wrapNativeBalanceLVLNativePair.output).plus(new BigNumber(poolValue.output)).div(new BigNumber(10).pow(30));
-    return toUSDTBalances(tvl)
+const calcPool2 = async (_, _b, chainBlocks, { chain }) => {
+    const balances = {}
+    const lpBalance = (await sdk.api.abi.call({
+        target: Contracts.LVL_BNB_LP,
+        abi: "erc20:balanceOf",
+        params: Contracts.Chef,
+        block: chainBlocks.bsc,
+        chain
+    })).output;
+    await unwrapUniswapLPs(
+        balances,
+        [{
+            balance: lpBalance,
+            token: Contracts.LVL_BNB_LP,
+        }],
+        chainBlocks.bsc,
+        chain,
+        (addr) => `${chain}:${addr}`
+    );
+    return balances;
 }
 
 module.exports = {
     bsc: {
-        tvl,
+        tvl: calcTvl,
+        pool2: calcPool2
     },
 };
