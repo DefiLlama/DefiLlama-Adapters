2391306d2289a79e6e259099d5f2c6bbedb5505f
Haythem Sellami <sellamihaythem96@gmail.com>
2022-12-29 16:03:32 +0100

    improve Opyn dashboard

--- projects/opyn-gamma/gamma_avax.js
+++ projects/opyn-gamma/gamma_avax.js
@@ -3,7 +3,7 @@ const sdk = require('@defillama/sdk');
 const marginPool = "0xCCF6629aEaB734E621Cc59EBb0297196774fDb9D";
 const wavax = '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7'.toLowerCase()
 
-module.exports.tvl = async function tvl(timestamp, block) {
+module.exports.tvl = async function avaxTvl(timestamp, block) {
 
   let balances = {};
 
