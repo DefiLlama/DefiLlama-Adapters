1013169dc990deb06131cf28de2da668cae0d113
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-31 11:58:46 +0100

    refactor dehive

--- projects/symmetric/index.js
+++ projects/symmetric/index.js
@@ -5,17 +5,15 @@ const { toUSDTBalances } = require('../helper/balances');
 const { getBlock } = require('../helper/http');
 async function getTVL(subgraphName, block, version = 'v1') {
   // delayed by around 5 mins to allow subgraph to update
-  block -= 25;
   var endpoint = `https://api.thegraph.com/subgraphs/name/centfinance/${subgraphName}`
   var kavaEndPoint = 'https://subgraph.symmetric.exchange/subgraphs/name/centfinance/kava' 
   var graphQLClient = new GraphQLClient(endpoint)
   var graphQLClientKava = new GraphQLClient(kavaEndPoint)
 
   var v1 = gql`
-  query get_tvl($block: Int) {
+  query get_tvl {
     symmetrics(
       first: 5,
-      block: { number: $block }
     ) {
       totalLiquidity,
       totalSwapVolume
@@ -23,10 +21,9 @@ async function getTVL(subgraphName, block, version = 'v1') {
   }
   `;
   var v2 = gql`
-  query get_tvl($block: Int) {
+  query get_tvl {
     balancers(
       first: 5,
-      block: { number: $block }
     ) {
       totalLiquidity,
       totalSwapVolume
@@ -34,24 +31,22 @@ async function getTVL(subgraphName, block, version = 'v1') {
   }
   `;
   const results = subgraphName ==='kava' ? await graphQLClientKava.request(v2, {
-    block
   }) : await graphQLClient.request(version === 'v1' ? v1 : v2, {
-    block
   })
   return version ==='v1'? results.symmetrics[0].totalLiquidity : results.balancers[0].totalLiquidity;
 }
 
 async function xdai(timestamp, ethBlock, chainBlocks) {
-  const [v1,v2] = await Promise.all([getTVL("symmetric-xdai", await getBlock(timestamp, "xdai", chainBlocks), 'v1'), getTVL("symmetric-v2-gnosis", await getBlock(timestamp, "xdai", chainBlocks), 'v2')])
+  const [v1,v2] = await Promise.all([getTVL("symmetric-xdai", null, 'v1'), getTVL("symmetric-v2-gnosis", null, 'v2')])
   return toUSDTBalances(BigNumber(v1).plus(v2))
 }
 
 async function celo(timestamp, ethBlock, chainBlocks) {
-  const [v1,v2] = await Promise.all([getTVL("symmetric-celo", await getBlock(timestamp, "celo", chainBlocks), 'v1'), getTVL("symmetric-v2-celo", await getBlock(timestamp, "celo", chainBlocks), 'v2')])
+  const [v1,v2] = await Promise.all([getTVL("symmetric-celo", null, 'v1'), getTVL("symmetric-v2-celo", null, 'v2')])
   return toUSDTBalances(BigNumber(v1).plus(v2))
 }
 async function kava(timestamp, ethBlock, chainBlocks) {
-  const [v2] = await Promise.all([getTVL("kava", await getBlock(timestamp, "kava", chainBlocks), 'v2')])
+  const [v2] = await Promise.all([getTVL("kava", await null, 'v2')])
   return toUSDTBalances(BigNumber(v2))
 }
 
