9e4a4dcd4acbeb1bd9e93a725640dfd7e7e2728a
Jack Smith <business@bluemove.net>
2023-01-04 16:42:04 +0700

    Adding methodology for calculating the total value locked

--- projects/blue-move-mart/index.js
+++ projects/blue-move-mart/index.js
@@ -6,6 +6,7 @@ async function query(api) {
 
 module.exports = {
   timetravel: false,
+  methodology: "The total value locked is calculated by adding each value locked for each collection listed on the BlueMove NFT marketplace. The collection's value locked is calculated by the total NFT locked on the platform including listed and staked NFTs with the NFT collection floor price respectively",
   aptos: {
     tvl: async () => {
       const {tvl: { total_aptos } } = await query(bluemoveApi);
