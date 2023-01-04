1bf04ce40c175a895b5bd03e62095850d41f975c
Stefano Faieta <9802152+stefanofa@users.noreply.github.com>
2023-01-04 17:08:35 +0100

    Calculate tvl of folks finance v2

--- projects/folks-finance/index.js
+++ projects/folks-finance/index.js
@@ -1,16 +1,20 @@
-const { v1TvlUsdt, v1BorrowedBalancesUsdt } = require("./v1");
+const { toUSDTBalances } = require("../helper/balances");
+const { v1TvlUsd, v1BorrowedBalancesUsd } = require("./v1");
+const { v2BorrowedBalancesUsd, v2TvlUsd } = require("./v2");
 
 /* Get total deposits */
 async function tvl() {
-  const v1Tvl = await v1TvlUsdt();
+  const v1Tvl = await v1TvlUsd();
+  const v2Tvl = await v2TvlUsd();
 
-  return v1Tvl;
+  return toUSDTBalances(v1Tvl + v2Tvl);
 }
 
 async function borrowedBalances() {
-  const v1BorrowedBalances = await v1BorrowedBalancesUsdt();
+  const v1BorrowedBalances = await v1BorrowedBalancesUsd();
+  const v2BorrowedBalances = await v2BorrowedBalancesUsd();
 
-  return v1BorrowedBalances;
+  return toUSDTBalances(v1BorrowedBalances + v2BorrowedBalances);
 }
 
 module.exports = {
