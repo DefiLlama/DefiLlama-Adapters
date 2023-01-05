679cf015203a36f3528e4131de48b6773a9dde58
Juan Castrejon <jayz@xbacked.io>
2023-01-05 05:05:22 +0000

    UPDATE: use updated domain

--- projects/xbacked/index.js
+++ projects/xbacked/index.js
@@ -5,7 +5,7 @@ async function tvl() {
   const response = (
     await retry(
       async () => await axios.get(
-        'http://xback-LoadB-77J86FESSSEN-2befef5c80c440ac.elb.us-west-2.amazonaws.com:4001/api/v1/getTVL'
+        'http://mainnet.collector.xbacked.io:4001/api/v1/getTVL'
       )
     )
   )
