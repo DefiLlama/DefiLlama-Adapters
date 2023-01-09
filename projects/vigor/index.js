96e6fece51351cdb34ac3a5b0c6cc9b6ed7cde69
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-10 04:17:58 +0530

    Update index.js

--- projects/vigor/index.js
+++ projects/vigor/index.js
@@ -1,27 +1,26 @@
 const { get_account_tvl } = require("../helper/chain/eos")
+const sdk = require('@defillama/sdk')
+const account1 = 'vigorlending'
+const tokens1 = [
+  ["eosio.token", "EOS", "eos"],
+  ["tethertether", "USDT", "tether"],
+  ["everipediaiq", "IQ", "everipedia"],
+  ["btc.ptokens", "PBTC", "bitcoin"],
+  ["eth.ptokens", "PETH", "ethereum"],
+];
+
+const account2 = 'vigorstaking'
+const tokens2 = [
+  ["eosio.token", "EOS", "eos"],
+  ["tethertether", "USDT", "tether"],
+];
 
 module.exports = {
   timetravel: false,
   eos: {
-    tvl: async () => {
-      const account1 = 'vigorlending'
-      const tokens1 = [
-        ["eosio.token", "EOS", "eos"],
-        ["tethertether", "USDT", "tether"],
-        ["everipediaiq", "IQ", "everipedia"],
-        ["btc.ptokens", "PBTC", "bitcoin"],
-        ["eth.ptokens", "PETH", "ethereum"],
-      ];
-      var tvl1 = await get_account_tvl(account1, tokens1);
-      
-      const account2 = 'vigorstaking'
-      const tokens2 = [
-        ["eosio.token", "EOS", "eos"],
-        ["tethertether", "USDT", "tether"],
-      ];
-      var tvl2 = await get_account_tvl(account2, tokens2);
-      
-      return tvl1+tvl2
-    }
+    tvl: sdk.util.sumChainTvls([
+      () => get_account_tvl(account1, tokens1),
+      () => get_account_tvl(account2, tokens2),
+    ])
   },
 }
