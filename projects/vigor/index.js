d7f1d03b4807f89aac6f58d71f2a13af0a1e8d28
yogevmas <48476699+yogevmas@users.noreply.github.com>
2023-01-09 15:40:10 +0200

    Await for tvl per account

--- projects/vigor/index.js
+++ projects/vigor/index.js
@@ -5,15 +5,23 @@ module.exports = {
   eos: {
     tvl: async () => {
       const account1 = 'vigorlending'
-      const account2 = 'vigorstaking'
-      const tokens = [
+      const tokens1 = [
         ["eosio.token", "EOS", "eos"],
         ["tethertether", "USDT", "tether"],
         ["everipediaiq", "IQ", "everipedia"],
         ["btc.ptokens", "PBTC", "bitcoin"],
         ["eth.ptokens", "PETH", "ethereum"],
       ];
-      return get_account_tvl(account1, tokens)+get_account_tvl(account2, tokens)
+      var tvl1 = await get_account_tvl(account1, tokens1);
+      
+      const account2 = 'vigorstaking'
+      const tokens2 = [
+        ["eosio.token", "EOS", "eos"],
+        ["tethertether", "USDT", "tether"],
+      ];
+      var tvl2 = await get_account_tvl(account2, tokens2);
+      
+      return tvl1+tvl2
     }
   },
 }
