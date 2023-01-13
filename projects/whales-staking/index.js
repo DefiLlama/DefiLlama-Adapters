d9059543362bd6d229566cf0043b4ab01d9a38fc
mrmld <die.marmelade@yandex.ru>
2023-01-13 18:56:48 +0400

    wip(whales-staking): rewrite to sdk

--- projects/whales-staking/index.js
+++ projects/whales-staking/index.js
@@ -1,10 +1,17 @@
 const { get } = require('../helper/http')
+const { transformBalances } = require('../helper/portedTokens')
 
-async function fetch() {
+async function tvl() {
   const resp = await get('https://connect.tonhubapi.com/stats/staking')
-  return resp.tvl;
+  return transformBalances('ton', {
+    // 'USD': resp.tvlUsd,
+    'Ef8zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzM0vF': resp.tvlTon
+  });
 }
 
 module.exports = {
-  fetch,
-};
+  timetravel: false,
+  ton: {
+    tvl
+  }
+}
