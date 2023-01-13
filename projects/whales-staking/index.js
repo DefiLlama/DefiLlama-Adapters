0c097455e19199a73d6be43f10369b9905d67dfe
mrmld <die.marmelade@yandex.ru>
2023-01-13 18:13:14 +0400

    fix(whales-staking): invalid path to tvl

--- projects/whales-staking/index.js
+++ projects/whales-staking/index.js
@@ -2,7 +2,7 @@ const { get } = require('../helper/http')
 
 async function fetch() {
   const resp = await get('https://connect.tonhubapi.com/stats/staking')
-  return resp.data.tvl;
+  return resp.tvl;
 }
 
 module.exports = {
