ac397f754971a8c8e3079e90c7de8efa21227d92
bakedwatermelon <doggytomb@gmail.com>
2023-01-13 21:43:01 +0100

    add FlareWatermelon miner

--- projects/flareWatermelon/index.js
+++ projects/flareWatermelon/index.js
@@ -3,7 +3,7 @@ const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
 const contract = "0x4d86Aa688ba56e1971Baf8C2CBCb44D980587894"
 
 async function tvl(time, ethBlock, { flare: block }) {
-  return sumTokens2({ tokens: [nullAddress], owner: contract, block, chain: 'dogechain', })
+  return sumTokens2({ tokens: [nullAddress], owner: contract, block, chain: 'flare', })
 }
 
 module.exports = {
