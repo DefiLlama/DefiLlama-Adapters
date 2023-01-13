1906649e97fdc3859e686e6246d7715df96e731b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-13 13:59:51 +0100

    changex: query balance in hydra

--- projects/changex/index.js
+++ projects/changex/index.js
@@ -1,5 +1,5 @@
-const sdk = require("@defillama/sdk");
 const { staking } = require("../helper/staking");
+const { get } = require("../helper/http");
 const CHANGEX_TOKEN_CONTRACT_ETH = "0x7051faed0775f664a0286af4f75ef5ed74e02754";
 const CHANGEX_TOKEN_STAKING_CONTRACT_ETH =
   "0x48309699c488ad207Dd9d228bBb013cF848a6e50";
@@ -7,28 +7,15 @@ const CHANGEX_TOKEN_CONTRACT_HYDRA = "bd3c617d271b3467bd9b83dda73c9288de2fb0c9";
 const CHANGEX_TOKEN_STAKING_CONTRACT_HYDRA =
   "72c9a79baa83e698bf1dbf44d26e5bdca2d2bab1";
 
-
 async function tvl(_, _1, _2, { api }) {
-  const balances = {};
-
-  const totalBalance = await api.call({
-    abi: "function totalBalance() view returns (uint256)",
-    target: CHANGEX_TOKEN_CONTRACT_HYDRA,
-    params: [CHANGEX_TOKEN_STAKING_CONTRACT_HYDRA],
-  });
-
-  await sdk.util.sumSingleBalance(
-    balances,
-    CHANGEX_TOKEN_CONTRACT_HYDRA,
-    totalBalance,
-    api.chain
-  );
-
-  return balances;
+  const data = await get('https://explorer.hydrachain.org/7001/contract/'+CHANGEX_TOKEN_STAKING_CONTRACT_HYDRA)
+  return {
+    changex: data.qrc20Balances.find(i => i.addressHex === CHANGEX_TOKEN_CONTRACT_HYDRA)?.balance / 1e18
+  }
 }
 
 module.exports = {
-  start: 1000235,
+  timetravel: false,
   ethereum: {
     tvl: () => ({}),
     staking: staking(
@@ -37,11 +24,6 @@ module.exports = {
     ),
   },
   hydra: {
-    tvl,
-    // tvl: () => ({}),
-    // staking: staking(
-    //   CHANGEX_TOKEN_STAKING_CONTRACT_HYDRA,
-    //   CHANGEX_TOKEN_CONTRACT_HYDRA,
-    // ),
+    staking: tvl,
   },
 };
