2dd8418393299c5301c987dd23092afbc41fd769
raidshift <laurenz.zielinski@gmail.com>
2023-01-13 09:36:12 +0100

    flatcoin

--- projects/flatcoin/index.js
+++ projects/flatcoin/index.js
@@ -4,8 +4,8 @@ const owner = 'TV8ndiKP98SF537BM9XvEbzkY2TerXNzEs'
 const token = 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t'
 
 async function tvl() {
-    const tokensAndOwner = [[token, owner]]
-    return sumTokens({ tokensAndOwners: tokensAndOwner, })
+    const tokenAndOwner = [[token, owner]]
+    return sumTokens({ tokensAndOwners: tokenAndOwner, })
 }
 
 module.exports = {
