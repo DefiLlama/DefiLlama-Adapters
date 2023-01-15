7a8b6b2bcd87756ceb7aa462cbac264feacebc8a
Juan Castrejon <jayz@xbacked.io>
2023-01-15 11:16:18 +0000

    FIX: use requested changes

--- projects/xbacked/index.js
+++ projects/xbacked/index.js
@@ -1,24 +1,12 @@
-const axios = require('axios');
-const retry = require('../helper/retry');
-
-async function fetch() {
-  const response = (
-    await retry(
-      async () => await axios.get(
-        'http://mainnet.collector.xbacked.io:4001/api/v1/getTVL'
-      )
-    )
-  )
-  const data = response.data
-  return parseFloat(data);
-}
+const { get } = require('../helper/http');
 
 module.exports = {
   timetravel: false,
   misrepresentedTokens: true,
   methodology: "Sums the total locked collateral value in usd across all vaults.",
   algorand: {
-    fetch
+    tvl: async () => {
+      return { tether: await get('http://mainnet.collector.xbacked.io/api/v1/getTVL') };
+    }
   },
-  fetch
 }
