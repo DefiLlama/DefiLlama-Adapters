7d678e40ef2283b91fd8caf467aee3f74a61ecba
Eric Lee <eric.lee@themarsolutions.io>
2023-01-17 08:31:10 -0500

    update mainnet address

--- projects/unlockd/helper/address.js
+++ projects/unlockd/helper/address.js
@@ -1,9 +1,9 @@
 const UiPoolDataProvider = {
-  ethereum: "", // TODO Mainnet Address
+  ethereum: "0x2CF74101dF653E166Cbf699EeED6FaB599293BcD",
 };
 
 const LendPoolAddressProvider = {
-  ethereum: "", // TODO Mainnet Address
+  ethereum: "0xE6cd031FB0D9A79FD8CD3035B9228F78ff459B07",
 };
 
 module.exports = {
