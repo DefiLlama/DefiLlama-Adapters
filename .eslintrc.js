508b55dc2f3b9b776336af5ec0814c9b9294a003
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-18 13:41:42 +0530

    add ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-unused-vars": "off",
+    }
+}
