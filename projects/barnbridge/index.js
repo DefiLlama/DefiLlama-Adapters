5a0cd21ec5e9dccd99ed8f6cc1323e16936df554
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-18 20:05:43 +0530

    barnbridge: code refactor

--- projects/barnbridge/index.js
+++ projects/barnbridge/index.js
@@ -1,8 +1,5 @@
 const sdk = require("@defillama/sdk");
-const BigNumber = require("bignumber.js");
 const { request, gql } = require("graphql-request");
-
-const ERC20 = require("./abis/ERC20.js");
 
 const CHAINS = [
   {
@@ -10,14 +7,12 @@ const CHAINS = [
     name: "ethereum",
     url: "https://api.thegraph.com/subgraphs/name/barnbridge/bb-sy-mainnet",
     address: "0x8A897a3b2dd6756fF7c17E5cc560367a127CA11F",
-    abi: require("./abis/SmartYieldaV2.js"),
   },
   {
     id: 42161,
     name: "arbitrum",
     url: "https://api.thegraph.com/subgraphs/name/barnbridge/bb-sy-arbitrum",
     address: "0x1ADDAbB3fAc49fC458f2D7cC24f53e53b290d09e",
-    abi: require("./abis/SmartYieldaV3.js"),
   },
 ];
 
@@ -29,64 +24,31 @@ const termsQuery = (timestamp) => gql`
     }
   `;
 
-const _underlying = async (chain) =>
-  (
-    await sdk.api.abi.call({
-      abi: chain.abi.find((i) => i.name === "underlying"),
-      chain: chain.name,
-      target: chain.address,
-    })
-  ).output;
+const _underlying = async (chain, api) =>
+  api.call({
+    abi: "address:underlying",
+    target: chain.address,
+  })
 
-const _decimals = async (chain, target) =>
-  (
-    await sdk.api.abi.call({
-      abi: ERC20.find((i) => i.name === "decimals"),
-      chain: chain.name,
-      target: target,
-    })
-  ).output;
-
-const _symbol = async (chain, target) =>
-  (
-    await sdk.api.abi.call({
-      abi: ERC20.find((i) => i.name === "symbol"),
-      chain: chain.name,
-      target: target,
-    })
-  ).output;
-
-const _liquidityProviderBalance = async (chain) => {
-  const balance = (
-    await sdk.api.abi.call({
-      abi: chain.abi.find((i) => i.name === "liquidityProviderBalance"),
-      chain: chain.name,
-      target: chain.address,
-    })
-  ).output;
-
-  return BigNumber(balance);
-};
+const _liquidityProviderBalance = async (chain, api) =>
+  api.call({
+    abi: "uint256:liquidityProviderBalance",
+    target: chain.address,
+  })
 
 const tvl = (chain) => {
-  return async (timestamp, _1, _2, _3) => {
-    const underlying = await _underlying(chain);
-    const decimals = await _decimals(chain, underlying);
-    const symbol = await _symbol(chain, underlying);
-    const liquidity = await _liquidityProviderBalance(chain);
+  return async (timestamp, _1, _2, { api, }) => {
+    const balances = {}
+    const underlying = await _underlying(chain, api);
+    const liquidity = await _liquidityProviderBalance(chain, api);
 
     const { terms } = await request(chain.url, termsQuery(timestamp));
 
-    const deposits = terms.reduce(
-      (acc, term) => acc.plus(BigNumber(term.depositedAmount)),
-      BigNumber(0)
-    );
+    const deposits = terms.reduce((acc, term) => acc + +term.depositedAmount, 0);
+    sdk.util.sumSingleBalance(balances, underlying, liquidity, api.chain)
+    sdk.util.sumSingleBalance(balances, underlying, deposits, api.chain)
 
-    const total = deposits
-      .div(`1e${decimals}`)
-      .plus(liquidity.div(`1e${decimals}`));
-
-    return { [symbol]: total };
+    return balances
   };
 };
 
