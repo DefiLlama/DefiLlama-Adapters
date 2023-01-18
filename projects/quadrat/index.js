db1e8506ffd2f9bdb277c7ee120d1bce1582c383
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-18 16:20:22 +0530

    quadrat: minor fix

--- projects/quadrat/index.js
+++ projects/quadrat/index.js
@@ -1,7 +1,7 @@
 const { GraphQLClient, gql } = require('graphql-request')
 const sdk = require('@defillama/sdk')
 
-const CHAINS = ['polygon', 'optimism', 'ethereum', 'arbitrum'];
+const HALF_DAY = 12 * 3600
 
 const GRAPH_CLIENTS = {
   polygon: new GraphQLClient('https://api.thegraph.com/subgraphs/name/krivochenko/plasma-quadrat-matic'),
@@ -11,7 +11,7 @@ const GRAPH_CLIENTS = {
 };
 
 const GRAPH_QUERY = gql`
-    query strategies ($timestamp: Int! $skip: Int! $first: Int!) {
+    query strategies ($timestampStart: Int! $timestampEnd: Int! $skip: Int! $first: Int!) {
         strategies (skip:$skip first:$first) {
             token0 {
                 id
@@ -19,7 +19,7 @@ const GRAPH_QUERY = gql`
             token1 {
                 id
             }
-            daysData (orderBy: createdAt orderDirection: desc skip:0 first: 1,where: {createdAt_gte:$timestamp}) {
+            daysData (orderBy: createdAt orderDirection: desc skip:0 first: 1,where: {createdAt_gte:$timestampStart createdAt_lt:$timestampEnd}) {
                 amount0
                 amount1
             }
@@ -28,9 +28,9 @@ const GRAPH_QUERY = gql`
 `;
 
 function chainTvlFactory(chain) {
-  return async (timestamp) => {
+  return async (timestamp, _, _1, { api }) => {
     const graphQLClient = GRAPH_CLIENTS[chain];
-    const result = await graphQLClient.request(GRAPH_QUERY, { timestamp, skip: 0, first: 1000 });
+    const result = await graphQLClient.request(GRAPH_QUERY, { timestampStart: timestamp - HALF_DAY, timestampEnd: timestamp + HALF_DAY, skip: 0, first: 1000 });
     if (!Array.isArray(result.strategies)) {
       return {}
     }
@@ -39,8 +39,8 @@ function chainTvlFactory(chain) {
     result.strategies.forEach(strategy => {
       const dayData = strategy.daysData[0];
       if (dayData)  {
-        sdk.util.sumSingleBalance(balances, strategy.token0.id, dayData.amount0);
-        sdk.util.sumSingleBalance(balances, strategy.token1.id, dayData.amount1);
+        sdk.util.sumSingleBalance(balances, strategy.token0.id, dayData.amount0, api.chain);
+        sdk.util.sumSingleBalance(balances, strategy.token1.id, dayData.amount1, api.chain);
       }
     });
     return balances
@@ -48,14 +48,13 @@ function chainTvlFactory(chain) {
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'Counts the tokens locked in Strategy Vaults in Uniswap v3 Pools.',
   start: 1667197843, // Mon Oct 31 2022 06:30:43 GMT+0000
-  ...CHAINS.reduce((tvls, chain) => {
-    tvls[chain] = {
-      tvl: chainTvlFactory(chain),
-    };
-    return tvls;
-  }, {}),
-}
+};
+
+Object.keys(GRAPH_CLIENTS).forEach(chain => {
+  module.exports[chain] = {
+    tvl: chainTvlFactory(chain)
+  }
+})
+
