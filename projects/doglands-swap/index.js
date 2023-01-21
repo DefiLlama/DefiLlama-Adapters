9d243a897575638bed2e93266c40f4dd4fe04bb5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-21 09:21:01 +0530

    doglands-swap rugged

--- projects/doglands-swap/index.js
+++ projects/doglands-swap/index.js
@@ -6,11 +6,15 @@ module.exports = {
   methodology: "Count TVL as liquidity on the dex",
   misrepresentedTokens: true,
   dogechain: {
-    tvl: getUniTVL({
-      chain: 'dogechain',
-      factory: FACTORY,
-      useDefaultCoreAssets: true,
-      fetchBalances: true,
-    }),
-  }
+    // tvl: getUniTVL({
+    //   chain: 'dogechain',
+    //   factory: FACTORY,
+    //   useDefaultCoreAssets: true,
+    //   fetchBalances: true,
+    // }),
+    tvl: () => 0
+  },
+  hallmarks: [
+    [Math.floor(new Date('2023-01-20')/1e3), 'Project rugged'],
+  ],
 } 
