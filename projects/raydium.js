3b2982dac661732a3bb0d9f340d803521f3d0600
Steven Enamakel <enamakel@mahadao.com>
2023-01-21 16:16:57 +0530

    fixed the raydium bug

--- projects/raydium.js
+++ projects/raydium.js
@@ -1,20 +1,18 @@
-const { get } = require('./helper/http')
+const { get } = require("./helper/http");
+const sdk = require("@defillama/sdk");
 
 async function fetch() {
-  const response = (
-    await get("https://api.raydium.io/pairs"))
+  const response = await get("https://api.raydium.io/pairs");
 
   return response.reduce((a, b) => {
-    if (b.amm_id === '5UCve2rYxMTe2xyCoqREsMXoTCCqr57XpnDsPq45r9dc') return a
-    if (b.liquidity > 1e8) sdk.log(b)
-    return a + b.liquidity
+    if (b.amm_id === "5UCve2rYxMTe2xyCoqREsMXoTCCqr57XpnDsPq45r9dc") return a;
+    if (b.liquidity > 1e8) sdk.log(b);
+    return a + b.liquidity;
   }, 0);
 }
 
 module.exports = {
-  hallmarks:[
-    [1667865600, "FTX collapse"]
-],
+  hallmarks: [[1667865600, "FTX collapse"]],
   timetravel: false,
   fetch,
 };
