c4c5dbbc06b6667afb0ae2a7677ebb68925fae60
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-22 10:34:49 +0530

    polysynth: cache http response

--- projects/polysynth/index.js
+++ projects/polysynth/index.js
@@ -1,67 +1,39 @@
 const sdk = require("@defillama/sdk");
-const bpsABI = require('./bps.json');
-const utils = require('../helper/utils');
-const {BigNumber} = require("ethers")
+const { getCache } = require('../helper/http');
 
 let _response;
-const GLP_BEEFY_VAULT="0x9dbbBaecACEDf53d5Caa295b8293c1def2055Adc";
 const GLP_TOKEN = "0x4277f8F2c384827B5273592FF7CeBd9f2C1ac258";
 
-function fetchTVL(chainId) {
-  return async (chainBlocks) => {
-    const ethTVL = {};
-    const polygonTVL = {};
-    const arbTVL = {};
-    
-    if (!_response) _response = utils.fetchURL('https://h.oliveapp.finance/api/rest/vaults/tvl')
-    const response = await _response;
+async function getVaults(chain) {
+  if (!_response) _response = getCache('https://h.oliveapp.finance/api/rest/vaults/tvl')
+  return (await _response).vault_meta_data
+    .map(i => i.vault_object)
+    .filter(i => i.chain_id === chains[chain])
+}
 
-    const vaults = response.data["vault_meta_data"];
-    for (const v in vaults) {      
-      if (chainId == vaults[v]['vault_object']['chain_id']) {
-        const asset = vaults[v]['vault_object']['underlying_asset'];
-        const balance = vaults[v]['vault_object']['total_balance'];
-
-        switch (chainId) {
-          case 1:
-            sdk.util.sumSingleBalance(ethTVL,asset,balance);
-            break;
-          case 137:
-            sdk.util.sumSingleBalance(polygonTVL,'polygon:'+asset,balance);
-            break;
-          case 42161:            
-            if (vaults[v]["name"] == "GLP") {
-              // Get BPS for beefy vault
-              const bps = await sdk.api.abi.call({
-                target: GLP_BEEFY_VAULT,
-                abi: bpsABI['bps'],
-                block: chainBlocks['arbitrum'],
-                chain: 'arbitrum'
-            })
-
-            const glp = BigNumber.from(bps.output).mul(BigNumber.from(balance)).div(BigNumber.from('1000000000000000000')).toString();
-            sdk.util.sumSingleBalance(arbTVL,'arbitrum:'+GLP_TOKEN,glp)
-            } else {
-              sdk.util.sumSingleBalance(arbTVL,'arbitrum:'+asset,balance)
-            }            
-            break;
-          default:
-            break;
-        }
-      }
-    }    
-    return chainId == 1 ?ethTVL: chainId == 137 ? polygonTVL : arbTVL;
-  }  
+function transform(address) {
+  if (address.toLowerCase() === '0x5402b5f40310bded796c7d0f3ff6683f5c0cffdf') return GLP_TOKEN
+  return address
 }
 
 const chains = {
-  ethereum: 1,
-  polygon: 137,
-  arbitrum: 42161
+  ethereum: '1',
+  polygon: '137',
+  arbitrum: '42161'
 }
 
-module.exports = {  
-  ...Object.fromEntries(Object.entries(chains).map(chain => [chain[0], {
-    tvl: fetchTVL(chain[1])    
-  }]))
-}
+module.exports = {
+};
+
+Object.keys(chains).forEach(chain => {
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const balances = {}
+      const vaults = await getVaults(chain)
+      const calls = vaults.map(i => i.id)
+      const bals = await api.multiCall({  abi: 'uint256:totalBalance', calls })
+      bals.forEach((vaule, i) => sdk.util.sumSingleBalance(balances,transform(vaults[i].underlying_asset),vaule, chain))
+      return balances
+    }
+  }
+})
