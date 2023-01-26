5e58904c9edab0c2ad9835f9d21373fc37c95811
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-27 00:00:53 +0530

    move tvl to pool2

--- projects/citadao/index.js
+++ projects/citadao/index.js
@@ -1,14 +1,20 @@
-const { staking } = require('../helper/staking');
+const { staking } = require('../helper/staking')
+const { unwrapUniswapV3NFTs } = require('../helper/unwrapLPs')
 
-const UNISWAP_V3_ETH_POOL = "0x03bc32cbaf0516945db77a32f2479379585aa17b";
-const CITADAO_TOKEN = "0x3541A5C1b04AdABA0B83F161747815cd7B1516bC";
-
+async function tvl(_, _b, _cb, { api, }) {
+  return unwrapUniswapV3NFTs({
+    owners: [
+      '0xc8b1039928a98d7a272f6942d86814ed9d8f9f17',
+      '0x3f96c580436dd59404ba612bf6d8079dc10f6f7e',
+      '0xda62d109064138c14d45085b6e49568e1c0b4e23',
+    ]
+  })
+}
 
 module.exports = {
-  timetravel: true,
-  methodology:
-    "This is calculated as the amount of tokens staked in the liquidity pool on Uniswap v3",
   ethereum: {
-    tvl: staking(UNISWAP_V3_ETH_POOL, CITADAO_TOKEN, "ethereum"),
-  },
+    tvl: () => 0,
+    pool2: tvl,
+    staking: staking('0x20891b408c35e0b7ece14df59f259be3c763f120', '0x3541a5c1b04adaba0b83f161747815cd7b1516bc'),
+  }
 }
