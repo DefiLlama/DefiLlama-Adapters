f20b06c3d14323adb674543e6afd9ca6378033b7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-27 00:03:56 +0530

    Update index.js

--- projects/traddify/index.js
+++ projects/traddify/index.js
@@ -1,14 +1,10 @@
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs')
 
 const contract = "0xA7f3d2dEa7a53E7A9FEbBdE5Cf7C69d39D065030"
-
-async function tvl(time, ethBlock, { kava: block }) {
-  return sumTokens2({ tokens: [nullAddress], owner: contract, block, chain: 'kava', })
-}
 
 module.exports = {
   methodology: `We count the WKAVA on ${contract}`,
   kava: {
-    tvl
+    tvl: sumTokensExport({ tokens: [nullAddress, '0xc86c7C0eFbd6A49B35E8714C5f59D99De09A225b', ], owner: contract})
   }
 }
