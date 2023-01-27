2387684c1388460d4d6d8885b67754836f1ed1b1
Elliot Shiu <sandbochs@users.noreply.github.com>
2023-01-26 23:32:17 -0800

    add new real yield usd cellar to sommelier project

--- projects/sommelier/index.js
+++ projects/sommelier/index.js
@@ -1,5 +1,6 @@
 const v0815 = require("./v0-8-15");
 const v0816 = require("./v0-8-16");
+const v2 = require("./v2");
 const chain = "ethereum";
 
 // v0.8.15 Cellars (Cellar 1.0)
@@ -22,6 +23,9 @@ const cellarsV0816 = [
   { id: STEADY_MATIC, startBlock: 16192732 },
 ];
 
+const REAL_YIELD_USD = "0x97e6e0a40a3d02f12d1cec30ebfbae04e37c119e";
+const cellarsV2 = [{ id: REAL_YIELD_USD, startBlock: 16431804 }];
+
 async function tvl(timestamp, block, chainBlocks) {
   const balances = {};
   const baseOptions = { balances, chainBlocks };
@@ -36,6 +40,11 @@ async function tvl(timestamp, block, chainBlocks) {
   await v0816.sumTvl({
     ...baseOptions,
     cellars: filterActiveCellars(cellarsV0816, block),
+  });
+
+  await v2.sumTvl({
+    ...baseOptions,
+    cellars: filterActiveCellars(cellarsV2, block),
   });
 
   return balances;
@@ -59,5 +68,6 @@ module.exports = {
     [1658419200, "aave2 Cellar Launch"],
     [1666886400, "ETH-BTC Trend & Momentum Cellars Launch"],
     [1669741200, "Steady ETH & BTC Cellars Launch"],
+    [1674671068, "Real Yield USD Cellar Launch"],
   ],
 };
