0422a5fc5db147f71e72f1f9d1ad02c2cf9a1000
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-29 08:39:01 +0530

    Update index.js

--- projects/bitindi/index.js
+++ projects/bitindi/index.js
@@ -6,12 +6,4 @@ module.exports = {
     tvl: () => ({}),
     staking: staking('0xf559BF9c0563Ed5322E7F493C02ea2275d68A367', '0x77fc65deda64f0cca9e3aea7b9d8521f4151882e', 'bsc'),
   },
-  bitindi: {
-    staking: async (_, _b, { bitindi: block }) => sumTokens2({
-      chain: 'bitindi',
-      block,
-      owner: '0x7E9dA0832BDB880a2663203c212d933f4c3668C6',
-      tokens: [nullAddress],
-    })
-  }
 };
