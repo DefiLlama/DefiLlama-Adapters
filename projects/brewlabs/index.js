0910192aee4bdc84790336fe0420d926ed71b80a
brainstormk <109654392+brainstormk@users.noreply.github.com>
2023-02-01 02:08:09 +0800

    fix: test issue

--- projects/brewlabs/index.js
+++ projects/brewlabs/index.js
@@ -3,12 +3,11 @@ const { fetchChain } = require("./brewlabs.js");
 const chains = ["ethereum", "bsc", "polygon"];
 
 module.exports = {
-  url: "https://brewlabs.info",
   methodology: `Brewlabs is a Multi-Chain utility project responsible for a number of community tools and platforms within the cryptocurrency space.`,
-  ...chains.map((chain) => [
+  ...Object.fromEntries(chains.map((chain) => [
     chain,
     {
       tvl: fetchChain(chain),
     },
-  ]),
+  ])),
 };
