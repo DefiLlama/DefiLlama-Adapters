eaa9b1c29f782acb0b71fd7698a1b2790d232e02
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-31 12:05:04 +0530

    minor refactor

--- projects/perennial/index.js
+++ projects/perennial/index.js
@@ -1,22 +1,14 @@
-const { sumTokens2 } = require("../helper/unwrapLPs");
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
-const DSU = {
-  ethereum: "0x605D26FBd5be761089281d5cec2Ce86eeA667109",
-};
-
-const collateral = {
-  ethereum: "0x2d264EBDb6632A06A1726193D4d37FeF1E5dbDcd",
-};
-
-function tvl(chain) {
-  return async (_ts, _block, _cb) => {
-    const tokensAndOwners = [[DSU[chain], collateral[chain]]];
-    return sumTokens2({ chain, tokensAndOwners });
-  };
+const config = {
+  ethereum: { DSU: '0x605D26FBd5be761089281d5cec2Ce86eeA667109', collateral: '0x2d264EBDb6632A06A1726193D4d37FeF1E5dbDcd', },
 }
 
-module.exports = {
-  ethereum: {
-    tvl: tvl("ethereum"),
-  },
-};
+module.exports = {};
+
+Object.keys(config).forEach(chain => {
+  const { DSU, collateral} = config[chain]
+  module.exports[chain] = {
+    tvl: sumTokensExport({ owner: collateral, tokens: [DSU] })
+  }
+})
