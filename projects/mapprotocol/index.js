37e4480735c580ea22bb3aff847678fe25f94872
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-01-31 11:58:55 +0530

    minor refactor

--- projects/mapprotocol/index.js
+++ projects/mapprotocol/index.js
@@ -1,4 +1,4 @@
-const { sumTokens } = require("../helper/sumTokens");
+const { sumTokensExport } = require("../helper/sumTokens");
 const config = require("./config");
 
 const MOS_CONTRACT = "0x630105189c7114667a7179aa57f07647a5f42b7f";
@@ -6,16 +6,8 @@ const MOS_CONTRACT = "0x630105189c7114667a7179aa57f07647a5f42b7f";
 module.exports = {};
 
 Object.keys(config).forEach(chain => {
-  let { tokens } = config[chain];
+  const { tokens } = config[chain]
   module.exports[chain] = {
-    tvl: async (_, _1, _2, { chain }) => {
-      const toa = [];
-      Object.values(tokens).forEach((t) => {
-        if (t) {
-          toa.push([t, MOS_CONTRACT]);
-        }
-      });
-      return sumTokens({ chain, tokensAndOwners: toa });
-    }
-  };
-});
+    tvl: sumTokensExport({ owner: MOS_CONTRACT, tokens: Object.values(tokens) })
+  }
+})
