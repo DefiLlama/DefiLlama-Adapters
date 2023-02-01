224b471a6e86822966481b3b9908ae35faa9c749
keccakdog <keccakdog@gmail.com>
2023-01-31 19:42:41 -0800

    Update index.js

--- projects/Monolith/index.js
+++ projects/Monolith/index.js
@@ -3,7 +3,7 @@ const { staking } = require('../helper/staking')
 
 module.exports = {
   ethereum:{
-    tvl: uniTvlExport("0x822EF744C568466D40Ba28b0f9e4A4961837a46a", "ethereum"),
+    tvl: uniTvlExport("0x777de5Fe8117cAAA7B44f396E93a401Cf5c9D4d6", "ethereum"),
     staking: staking("0x848578e351d25b6ec0d486e42677891521c3d743", "0x3Fd3A0c85B70754eFc07aC9Ac0cbBDCe664865A6","ethereum"),
   },
 }
