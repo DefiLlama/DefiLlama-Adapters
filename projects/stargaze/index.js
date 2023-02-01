7cac5d2861e4467c680594ef3802f8d805d21b13
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-01 23:33:37 +0530

    stargaze: code refactor

--- projects/stargaze/index.js
+++ projects/stargaze/index.js
@@ -1,58 +1,26 @@
-const utils = require("../helper/utils");
 const sdk = require("@defillama/sdk");
+const { get } = require('../helper/http')
 
 async function tvl() {
-  const bondedTokens = {};
+  const balances = {};
 
   /**
    * The denom is the official API.  bids was custom made for this use.  @pen.so on #Stargaze
    * Bids has already been converted from ustars to stars.
    */
 
-  const denomURL = `https://rest.stargaze-apis.com/cosmos/staking/v1beta1/params`;
+  // const denomURL = `https://rest.stargaze-apis.com/cosmos/staking/v1beta1/params`;
   const bidsURL = `https://metabase.constellations.zone/api/public/card/4bd16e60-7e77-4206-8ad2-8b04f362afed/query`
 
-  /* 
-   * Fetach and store the JSON data 
-  */
-  const bidsResponse = await utils.fetchURL(bidsURL);
-  const denomResponse = await utils.fetchURL(denomURL);
+  const bidsResponse = await get(bidsURL);
+  // const denomResponse = await utils.fetchURL(denomURL);
+  sdk.util.sumSingleBalance(balances, 'ustars', bidsResponse.data.rows[0] * 1e6, 'stargaze')
 
-  const tokenInfo = denomFix(denomResponse.data.params.bond_denom);
-  
-  /**
-   * Debugging
-   */
-  // console.log(bidsResponse.data.data.rows[0]);
-  // console.log(tokenInfo);
-
-  /**
-   * This stores the tvl in bids into bondedTokens and sets coingecko id to stargaze
-   */
-  sdk.util.sumSingleBalance(bondedTokens, tokenInfo[1], parseFloat(bidsResponse.data.data.rows[0]))
-
-  /**
-   * Debugging
-   */
-  // console.log(bondedTokens);
-
-  return bondedTokens;
+  return balances;
 }
-
-
-function denomFix(ticker) {
-  switch (ticker) {
-    case "ustars":
-      /**
-       * denome, coingecko, decimal
-       */
-      return ["stars", "stargaze", 6];
-  }
-}
-
 
 module.exports = {
   timetravel: false,
   methodology: 'Queries the chain API for the how many STARS are locked in bids.',
-  stars: { tvl }
+  stargaze: { tvl }
 };
