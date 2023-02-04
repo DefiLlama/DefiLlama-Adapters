73ec34b2f09247a7d7fc59826f1540fd32f38d43
larry <26318510+larry0x@users.noreply.github.com>
2023-02-04 00:10:11 +0000

    subtract community delegations

--- projects/mars/index.js
+++ projects/mars/index.js
@@ -13,7 +13,25 @@ const queryBondedTokens = async (timestamp, block) => {
 
   // Staked MARS tokens can be in one of three states: bonded, unbonding, or
   // unbonded. Here we only include those in the bonded state in the TVL.
-  const bondedTokensRaw = BigNumber(res.data.pool.bonded_tokens);
+  let bondedTokensRaw = BigNumber(res.data.pool.bonded_tokens);
+
+  // For the first month after Mars Hub's launch, in order to bootstrap the
+  // chain's security, 50M MARS from the community pool are delegated to the
+  // genesis validators via a smart contract. These 50M should not be counted in
+  // the TVL.
+  //
+  // After the 1 month mark, this delegation will be unstaked and returned to
+  // the community pool.
+  //
+  // TODO: Once unstaked, delete this part
+  const res2 = await axios.get(
+    "https://rest.marsprotocol.io/cosmos/staking/v1beta1/delegations/mars1nc5tatafv6eyq7llkr2gv50ff9e22mnf70qgjlv737ktmt4eswrqhnhf0l"
+  );
+  const communityDelegationRaw = res2.data.delegation_responses.reduce(
+    (acc, curr) => acc.plus(BigNumber(curr.balance.amount)),
+    BigNumber(0)
+  );
+  bondedTokensRaw = bondedTokensRaw.minus(communityDelegationRaw);
 
   // MARS token has 6 decimal places
   const bondedTokens = bondedTokensRaw.div(1e6);
