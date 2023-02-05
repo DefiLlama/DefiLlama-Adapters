330d56ee613f4fde3f58491baf77270c77bf8092
Dizz0 <83427844+Dizz02@users.noreply.github.com>
2023-02-05 09:42:18 +0100

    Add files via upload

---
+++ projects/Hopers.io/index.js
@@ -0,0 +1,21 @@
+const { get } = require('../helper/http')
+const { transformDexBalances } = require('../helper/portedTokens')
+
+module.exports = {
+  timetravel: false,
+  misrepresentedTokens: true,
+  juno: {
+    tvl: async () => {
+      const data = await get('https://api.xxxxxxxxxxxxxxxxxxx')
+      return transformDexBalances({
+        chain: 'juno',
+        data: Object.values(data).map(i => ({
+          token0: i[0].token ?? i[0].native,
+          token0Bal: i[0].amount ?? 0,
+          token1: i[1].token ?? i[1].native,
+          token1Bal: i[1].amount ?? 0,
+        }))
+      })
+    }
+  }
+}
