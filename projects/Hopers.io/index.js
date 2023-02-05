b27c397ec0b499b1901cb87a618999c15939181a
Dizz0 <83427844+Dizz02@users.noreply.github.com>
2023-02-05 11:31:57 +0100

    Update index.js

--- projects/Hopers.io/index.js
+++ projects/Hopers.io/index.js
@@ -6,7 +6,8 @@ module.exports = {
   misrepresentedTokens: true,
   juno: {
     tvl: async () => {
-      const data = await get('https://api.hopers.io')
+      const data = await get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=hopers-io&order=market_cap_desc&per_page=100&page=1&sparkline=false
+')
       return transformDexBalances({
         chain: 'juno',
         data: Object.values(data).map(i => ({
