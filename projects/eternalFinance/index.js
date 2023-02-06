fad60600fec46e23680fe82beaa6b4fbf99e0bc9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-06 20:39:34 +0530

    minor fix

--- projects/eternalFinance/index.js
+++ projects/eternalFinance/index.js
@@ -1,21 +1,18 @@
 const { lyfTvl } = require("./lyf");
 const { lendingTvl } = require("./lending");
-const { default: BigNumber } = require("bignumber.js");
+const sdk = require('@defillama/sdk')
 
 async function tvl() {
     const lyf = await lyfTvl();
     const lending = await lendingTvl();
-    /// @dev aggregate tvl from lending and farming
-    Object.keys(lending).forEach((key) => {
-        const tvlFromLyf = lyf[key] || '0';
-        lyf[key] = new BigNumber(lending[key]).plus(tvlFromLyf).toFixed();
-    })
-
-    return lyf
+    const balances = {}
+    sdk.util.mergeBalances(balances, lyf)
+    sdk.util.mergeBalances(balances, lending)
+    return balances
 }
 
 module.exports = {
-    start: 1675411748,
+    timetravel: false,
     aptos: {
         tvl,
     }
