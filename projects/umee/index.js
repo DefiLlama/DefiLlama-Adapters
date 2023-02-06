74b8ba9652d7d452b97ae447162f32d1f7d188a0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-06 08:43:41 +0530

    umee: add eth network

--- projects/umee/index.js
+++ projects/umee/index.js
@@ -1,6 +1,8 @@
 const { get } = require('../helper/http')
 const sdk = require('@defillama/sdk')
 const { transformBalances } = require('../helper/portedTokens')
+const { getLogs, getAddress } = require('../helper/cache/getLogs')
+const { sumTokens2 } = require('../helper/unwrapLPs')
 let data
 
 async function getData() {
@@ -22,11 +24,27 @@ async function borrowed() {
   return transformBalances('umee', balances)
 }
 
+async function ethTvl(_, _b, _cb, { api, }) {
+  const logs = await getLogs({
+    api,
+    target: '0xe296db0a0e9a225202717e9812bf29ca4f333ba6',
+    topics: ['0x3a0ca721fc364424566385a1aa271ed508cc2c0949c2272575fb3013a163a45f'],
+    fromBlock: 14216544,
+    eventAbi: 'event ReserveInitialized (address indexed asset, address indexed aToken, address stableDebtToken, address variableDebtToken, address interestRateStrategyAddress)',
+    onlyArgs: true,
+  })
+  const tokensAndOwners = logs.map(i => ([i.asset, i.aToken]))
+  return sumTokens2({ api, tokensAndOwners })
+}
+
 module.exports = {
   timetravel: false,
   methodology: "Total supplied assets - total borrowed assets",
   umee: {
     tvl,
     borrowed,
+  },
+  ethereum: {
+    tvl: ethTvl
   },
 };
