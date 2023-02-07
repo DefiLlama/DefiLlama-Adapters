41255199e9564036506d573101d834df6cc38d2b
Avedith <avedith@avedith.dev>
2023-02-08 01:22:47 +1000

    replace Sphere Games contract with ylSPHERE contract

--- projects/sphere-finance/index.js
+++ projects/sphere-finance/index.js
@@ -2,7 +2,7 @@ const { staking } = require('../helper/staking')
 const { get } = require('../helper/http')
 
 const sphere_token = "0x62f594339830b90ae4c084ae7d223ffafd9658a7"
-const stakingAddress = "0x284eba456e27ec9d07a656ce7cf68f2c78578f2e" // Sphere Games StakePrizePool
+const stakingAddress = "0x4Af613f297ab00361D516454E5E46bc895889653" // ylSPHERE
 
 module.exports = {
   timetravel: false,
