3ce833ef2615a63269cbe3820cf6d4917239e66e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-07 23:16:56 +0530

    remove treasury

--- projects/notiboy/index.js
+++ projects/notiboy/index.js
@@ -1,23 +1,6 @@
-const scTreasury  = "KGNCP3PMGIJUAMD7NO5G3SSTYOO74HGYAS72EE34YTXYLRN3ZVQBYXZA3U";
-const notiboyTreasury = "NOTILXUG675YH2JBO3NP5BXADEWRWHPOM5VBIWE6Z3AQU3QKGKMEPNZJRE";
-
-const { sumTokens} = require('../helper/chain/algorand')
-
-async function treasury() {
-  const balances = await sumTokens({ 
-    owners: [scTreasury,notiboyTreasury],
-  })
-  return balances
-}
-
-async function tvl(){
-  return 0;
-}
-
 module.exports = {
   timetravel: false,
   algorand: {
-    tvl,
-    treasury
+    tvl: () => 0
   }
 }
