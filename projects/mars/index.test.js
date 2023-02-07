d57b080cd833e02c836a121156309d03437a21b7
larry <26318510+larry0x@users.noreply.github.com>
2023-02-07 16:46:14 +0000

    remove mars tvl test

--- projects/mars/index.test.js
+++ projects/mars/index.test.js
@@ -1,9 +1,6 @@
 const { mars, osmosis } = require("./index.js");
 
 (async function () {
-  console.log("querying mars tvl...");
-  console.log(await mars.tvl());
-
   console.log("querying osmosis tvl...");
   console.log(await osmosis.tvl());
 
