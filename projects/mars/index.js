d2da274e32d85f0cdb6f3e422a7838df9b264826
larry <26318510+larry0x@users.noreply.github.com>
2023-02-07 16:45:29 +0000

    remove mars hub staking for now

--- projects/mars/index.js
+++ projects/mars/index.js
@@ -24,24 +24,6 @@ async function queryBondedTokens(timestamp, block) {
   // unbonded. Here we only include those in the bonded state in the TVL.
   let bondedTokensRaw = BigNumber(res.data.pool.bonded_tokens);
 
-  // For the first month after Mars Hub's launch, in order to bootstrap the
-  // chain's security, 50M MARS from the community pool are delegated to the
-  // genesis validators via a smart contract. These 50M should not be counted in
-  // the TVL.
-  //
-  // After the 1 month mark, this delegation will be unstaked and returned to
-  // the community pool.
-  //
-  // TODO: Once unstaked, delete this part
-  const res2 = await axios.get(
-    "https://rest.marsprotocol.io/cosmos/staking/v1beta1/delegations/mars1nc5tatafv6eyq7llkr2gv50ff9e22mnf70qgjlv737ktmt4eswrqhnhf0l"
-  );
-  const communityDelegationRaw = res2.data.delegation_responses.reduce(
-    (acc, curr) => acc.plus(BigNumber(curr.balance.amount)),
-    BigNumber(0)
-  );
-  bondedTokensRaw = bondedTokensRaw.minus(communityDelegationRaw);
-
   // MARS token has 6 decimal places
   const bondedTokens = bondedTokensRaw.div(1e6);
 
@@ -112,10 +94,10 @@ async function queryOsmosis(timestamp, block) {
 module.exports = {
   timetravel: false,
   methodology: "We query Mars protocol smart contracts to get the amount of assets deposited and borrowed, then use CoinGecko to price the assets in USD.",
-  mars: {
-    tvl: queryBondedTokens,
-    staking: queryBondedTokens,
-  },
+  // mars: {
+  //   tvl: queryBondedTokens,
+  //   staking: queryBondedTokens,
+  // },
   osmosis: {
     tvl: () => queryOsmosis().then((result) => result.tvl),
     borrowed: () => queryOsmosis().then((result) => result.borrowed),
