b25f0b2729a645943f7057712527d259d89667a1
Vidhyanand CS <vithushaji@gmail.com>
2023-02-07 14:08:44 +0530

    added treasury

--- projects/notiboy/index.js
+++ projects/notiboy/index.js
@@ -1,16 +1,23 @@
-const notiboyTreasury  = "KGNCP3PMGIJUAMD7NO5G3SSTYOO74HGYAS72EE34YTXYLRN3ZVQBYXZA3U";
+const scTreasury  = "KGNCP3PMGIJUAMD7NO5G3SSTYOO74HGYAS72EE34YTXYLRN3ZVQBYXZA3U";
+const notiboyTreasury = "NOTILXUG675YH2JBO3NP5BXADEWRWHPOM5VBIWE6Z3AQU3QKGKMEPNZJRE";
+
 const { sumTokens} = require('../helper/chain/algorand')
 
-async function tvl() {
+async function treasury() {
   const balances = await sumTokens({ 
-    owner: notiboyTreasury,
+    owners: [scTreasury,notiboyTreasury],
   })
   return balances
+}
+
+async function tvl(){
+  return 0;
 }
 
 module.exports = {
   timetravel: false,
   algorand: {
-    tvl
+    tvl,
+    treasury
   }
 }
