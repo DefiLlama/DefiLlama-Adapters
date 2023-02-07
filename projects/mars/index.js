c522bd95712109fbf3a7c28bbb4cf62a0d0c5483
larry <26318510+larry0x@users.noreply.github.com>
2023-02-07 16:51:54 +0000

    cleanly remove mars token staking

--- projects/mars/index.js
+++ projects/mars/index.js
@@ -13,26 +13,6 @@ function encodeBase64(msg) {
 }
 
 /**
- * Query the total amount of MARS tokens staked on Mars Hub app-chain.
- */
-async function queryBondedTokens(timestamp, block) {
-  const res = await axios.get(
-    "https://rest.marsprotocol.io/cosmos/staking/v1beta1/pool"
-  );
-
-  // Staked MARS tokens can be in one of three states: bonded, unbonding, or
-  // unbonded. Here we only include those in the bonded state in the TVL.
-  let bondedTokensRaw = BigNumber(res.data.pool.bonded_tokens);
-
-  // MARS token has 6 decimal places
-  const bondedTokens = bondedTokensRaw.div(1e6);
-
-  return {
-    "mars-protocol": bondedTokens.toNumber(),
-  };
-}
-
-/**
  * Query the TVL of a Red Bank deployment.
  */
 async function queryRedBankTvl(restUrl, contractAddr, assets) {
@@ -94,10 +74,6 @@ async function queryOsmosis(timestamp, block) {
 module.exports = {
   timetravel: false,
   methodology: "We query Mars protocol smart contracts to get the amount of assets deposited and borrowed, then use CoinGecko to price the assets in USD.",
-  // mars: {
-  //   tvl: queryBondedTokens,
-  //   staking: queryBondedTokens,
-  // },
   osmosis: {
     tvl: () => queryOsmosis().then((result) => result.tvl),
     borrowed: () => queryOsmosis().then((result) => result.borrowed),
