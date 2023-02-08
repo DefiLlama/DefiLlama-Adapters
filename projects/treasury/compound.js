81d380a905693f9bacda8acdadeed98a859c485b
realdealshaman <realdealshaman11@gmail.com>
2023-02-08 01:25:17 +0000

    add treasury adapters

--- projects/treasury/compound.js
+++ projects/treasury/compound.js
@@ -1,18 +1,15 @@
 const { nullAddress, treasuryExports } = require("../helper/treasury");
 
-const treasury = "0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B";
-const COMP = "0xc00e94Cb662C3520282E6f5717214004A7f26888";
+const treasury = "0x9467cfADC9DE245010dF95Ec6a585A506A8ad5FC";
+const INDEX = "0x0954906da0Bf32d5479e25f46056d22f08464cab";
 
 
 module.exports = treasuryExports({
   ethereum: {
     tokens: [ 
-        nullAddress,
-        '0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9',//cUSDT
-        '0xE41d2489571d322189246DaFA5ebDe1F4699F498',//zrx
-        '0x39AA39c021dfbaE8faC545936693aC917d5E7563',//cUSDC
+        nullAddress
      ],
     owners: [treasury],
-    ownTokens: [COMP],
+    ownTokens: [INDEX],
   },
 })
