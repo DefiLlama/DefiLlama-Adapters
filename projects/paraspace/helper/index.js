8dc03681923cb9aadd21b4ae06dde27d2075e66b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-08 17:20:29 +0530

    paraspace: code refactor

--- projects/paraspace/helper/index.js
+++ projects/paraspace/helper/index.js
@@ -1,162 +1,47 @@
 const sdk = require("@defillama/sdk");
-const { default: BigNumber } = require("bignumber.js");
+const { sumTokens2, unwrapUniswapV3NFTs, } = require('../../helper/unwrapLPs')
 
 const abi = require("./abis");
 const address = require("./address");
 
-async function getTVL(chain, chainBlocks) {
-  const block = chainBlocks[chain];
-
-  const {
-    output: [reservesData],
-  } = await sdk.api.abi.call({
-    target: address.UiPoolDataProvider[chain],
-    params: [address.PoolAddressProvider[chain]],
+async function tvl(_, _1, _cb, { api, }) {
+  const { UiPoolDataProvider, PoolAddressProvider, UniV3Pos, } = address[api.chain]
+  let [reservesData] = await api.call({
+    target: UiPoolDataProvider,
+    params: PoolAddressProvider,
     abi: abi.UiPoolDataProvider.getReservesData,
-    block,
-    chain,
-  });
+  })
 
   const balances = {};
+  if (UniV3Pos) {
+    const isUniV3XToken = i => i.underlyingAsset.toLowerCase() === UniV3Pos.toLowerCase()
 
-  reservesData
-    .filter((d) => d.assetType === "0")
-    .forEach((d) => {
-      balances["ETHEREUM"] = new BigNumber(balances["ETHEREUM"] || 0).plus(
-        new BigNumber(d.availableLiquidity)
-          .multipliedBy(d.priceInMarketReferenceCurrency)
-          .shiftedBy(-(18 + new BigNumber(d.decimals).toNumber()))
-      );
-    });
-
-  const NFTPrices = await Promise.all(
-    reservesData
-      .filter((d) => d.assetType === "1")
-      .map(async (data) => {
-        const { output: supported } = await sdk.api.abi.call({
-          target: address.BNftOracle[chain],
-          abi: abi.BNFTOracle.nftPriceFeedMap,
-          params: [data.underlyingAsset],
-          block,
-          chain,
-        });
-        if (supported) {
-          const { output: price } = await sdk.api.abi.call({
-            target: address.BNftOracle[chain],
-            abi: abi.BNFTOracle.getAssetPrice,
-            params: [data.underlyingAsset],
-            block,
-            chain,
-          });
-          return new BigNumber(price).gt(data.priceInMarketReferenceCurrency)
-            ? price
-            : data.priceInMarketReferenceCurrency;
-        } else {
-          return data.priceInMarketReferenceCurrency;
-        }
-      })
-  );
-
-  reservesData
-    .filter((d) => d.assetType === "1")
-    .forEach((d, index) => {
-      balances["ETHEREUM"] = new BigNumber(balances["ETHEREUM"] || 0).plus(
-        new BigNumber(d.availableLiquidity)
-          .multipliedBy(NFTPrices[index])
-          .shiftedBy(-(18 + new BigNumber(d.decimals).toNumber()))
-      );
-    });
-
-  const uniV3PosNToken = reservesData.find(
-    (data) =>
-      data.underlyingAsset.toLowerCase() ===
-      address.UniV3Pos[chain].toLowerCase()
-  )?.xTokenAddress;
-
-  if (uniV3PosNToken) {
-    const uniV3Tvl = await getUniV3Tvl(uniV3PosNToken, chain, block);
-    balances["ETHEREUM"] = new BigNumber(balances["ETHEREUM"]).plus(uniV3Tvl);
+    const uniswapOwners = reservesData.filter(isUniV3XToken).map(i => i.xTokenAddress)
+    reservesData = reservesData.filter(i => !isUniV3XToken(i))
+    await unwrapUniswapV3NFTs({ ...api, balances, owners: uniswapOwners })
   }
-
-  return balances;
+  const toa = reservesData.map(i => ([i.underlyingAsset, i.xTokenAddress]))
+  return sumTokens2({ balances, tokensAndOwners: toa, blacklistedTokens: ['0x0000000000000000000000000000000000000001'] })
 }
 
-async function getUniV3Tvl(nToken, chain, block) {
-  const bal = await sdk.api.erc20.balanceOf({
-    target: address.UniV3Pos[chain],
-    owner: nToken,
-    block,
-    chain,
-  });
-
-  const tokenIds = (
-    await Promise.all(
-      new Array(bal).map((_, index) =>
-        sdk.api.abi.call({
-          target: address.UniV3Pos[chain],
-          abi: abi.ERC721.tokenOfOwnerByIndex,
-          params: [nToken, index],
-          block,
-          chain,
-        })
-      )
-    )
-  ).map(({ output: tokenId }) => tokenId);
-
-  const uniV3Prices = (
-    await Promise.all(
-      tokenIds.map((id) =>
-        sdk.api.abi.call({
-          target: address.UiPoolDataProvider[chain],
-          abi: abi.UiPoolDataProvider.getUniswapV3LpTokenData,
-          params: [
-            address.PoolAddressProvider[chain],
-            address.UniV3Pos[chain],
-            id,
-          ],
-          block,
-          chain,
-        })
-      )
-    )
-  ).map(({ output: data }) => data.tokenPrice);
-
-  return uniV3Prices
-    .reduce(
-      (pv, cv) => new BigNumber(pv).plus(new BigNumber(cv)),
-      new BigNumber(0)
-    )
-    .shiftedBy(-18);
-}
-
-async function getBorrowed(chain, chainBlocks) {
-  const block = chainBlocks[chain];
-
-  const {
-    output: [reservesData],
-  } = await sdk.api.abi.call({
-    target: address.UiPoolDataProvider[chain],
-    params: [address.PoolAddressProvider[chain]],
+async function borrowed(_, _1, _cb, { api, }) {
+  const { UiPoolDataProvider, PoolAddressProvider, } = address[api.chain]
+  let [reservesData] = await api.call({
+    target: UiPoolDataProvider,
+    params: PoolAddressProvider,
     abi: abi.UiPoolDataProvider.getReservesData,
-    block,
-    chain,
-  });
+  })
 
   const balances = {};
 
   reservesData.forEach((d) => {
-    balances["ETHEREUM"] = new BigNumber(balances["ETHEREUM"] || 0).plus(
-      new BigNumber(d.totalScaledVariableDebt)
-        .multipliedBy(d.variableBorrowIndex)
-        .multipliedBy(d.priceInMarketReferenceCurrency)
-        .shiftedBy(-(18 + 27 + new BigNumber(d.decimals).toNumber()))
-    );
-  });
+    sdk.util.sumSingleBalance(balances, d.underlyingAsset, d.totalScaledVariableDebt * d.variableBorrowIndex * 1e-27, api.chain)
+  })
 
   return balances;
 }
 
 module.exports = {
-  getTVL,
-  getBorrowed,
+  tvl,
+  borrowed,
 };
--- projects/paraspace/index.js
+++ projects/paraspace/index.js
@@ -1,17 +1,6 @@
-const sdk = require("@defillama/sdk");
-
-const { getTVL, getBorrowed } = require("./helper/index");
-
-async function tvl(timestamp, block, chainBlocks) {
-  return await getTVL("ethereum", chainBlocks);
-}
-
-async function borrowed(timestamp, block, chainBlocks) {
-  return await getBorrowed("ethereum", chainBlocks);
-}
+const { tvl, borrowed } = require("./helper/index");
 
 module.exports = {
-  timetravel: true,
   methodology: `TVL includes ERC-20 and ERC-721 tokens that have been supplied as well as ERC-20 tokens that are supplied for lending.`,
   ethereum: {
     tvl,
