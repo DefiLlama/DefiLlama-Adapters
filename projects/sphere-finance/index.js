9e08b8d01c54f9aaf8210132f80596dfeea1d0a8
Avedith <avedith@avedith.dev>
2023-02-09 15:19:27 +1000

    remove block query

--- projects/sphere-finance/index.js
+++ projects/sphere-finance/index.js
@@ -17,7 +17,6 @@ async function optimismTvl(timestamp, block, chainBlocks) {
         tokens: [0x4200000000000000000000000000000000000006, 0x4200000000000000000000000000000000000042, 0x73cb180bf0521828d8849bc8CF2B920918e23032, 0x7F5c764cBc14f9669B88837ca1490cCa17c31607],
         owners: [OptimismGnosisContract],
         chain: "optimism",
-        block: chainBlocks.optimism
     })
 }
 
@@ -27,7 +26,6 @@ async function arbitrumTvl(timestamp, block, chainBlocks) {
         tokens: [0x82aF49447D8a07e3bd95BD0d56f35241523fBab1, 0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a, 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8,0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9, 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f],
         owners: [ArbitrumGnosisContract],
         chain: "arbitrum",
-        block: chainBlocks.arbitrum
     })
 }
 
@@ -37,7 +35,6 @@ async function binanceTvl(timestamp, block, chainBlocks) {
         tokens: [0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56, 0xAD6742A35fB341A9Cc6ad674738Dd8da98b94Fb1, 0x14016E85a25aeb13065688cAFB43044C2ef86784, 0x52F24a5e03aee338Da5fd9Df68D2b6FAe1178827, 0xA60205802E1B5C6EC1CAFA3cAcd49dFeECe05AC9, 0x9f8BB16f49393eeA4331A39B69071759e54e16ea, 0xD7FbBf5CB43b4A902A8c994D94e821f3149441c7],
         owners: [BinanceGnosisContract],
         chain: "binance",
-        block: chainBlocks.binance
     })
 }
 
