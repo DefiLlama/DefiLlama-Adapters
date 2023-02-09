b2e422957e1ec5e8f4e87381300ca4be18450c76
define <Lovelovenow@protonmail.com>
2023-02-09 16:19:33 +0000

    ens treasury

---
+++ projects/treasury/ens.js
@@ -0,0 +1,17 @@
+const { nullAddress, treasuryExports } = require("../helper/treasury");
+
+const treasury = "0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7";
+
+const ENS= "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72";
+
+
+module.exports = treasuryExports({
+  ethereum: {
+    tokens: [ 
+        nullAddress,
+        "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", //USDC
+     ],
+    owners: [treasury],
+    ownTokens: [ENS],
+  },
+})
