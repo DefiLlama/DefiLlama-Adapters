1a1101e0172be9a0a137c3f377a9641e508e3b0f
clarencesea <clarence.sea01@gmail.com>
2023-02-09 14:31:11 +0800

    Add: LENLEN tvl

---
+++ projects/lenlen/index.js
@@ -0,0 +1,16 @@
+const retry = require('async-retry')
+const axios = require("axios");
+
+async function fetch() {
+  let response = await retry(async bail => await axios.get('https://api.v-token.io/api/lenen/defiLlama'))
+  let tvl = 0
+  if (response.code === 200 && response.data) {
+    tvl = parseFloat(response.data.total_value_locked ?? '0') + parseFloat(response.data.total_borrow ?? '0')
+  }
+  
+  return tvl;
+}
+
+module.exports = {
+  fetch
+}
