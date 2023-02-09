47ca9b7e0eaeede7e1858d85ed92a5183f8d6ef5
Avedith <avedith@avedith.dev>
2023-02-09 15:16:55 +1000

    Add Binance TVL

--- projects/sphere-finance/index.js
+++ projects/sphere-finance/index.js
@@ -13,7 +13,6 @@ const PolygonGnosisContracts = ["0x20D61737f972EEcB0aF5f0a85ab358Cd083Dd56a", "0
 
 //Optimism TVL consists of investments/tokens on gnosis safe
 async function optimismTvl(timestamp, block, chainBlocks) {
-    let balance
     return sumTokensExport({
         tokens: [0x4200000000000000000000000000000000000006, 0x4200000000000000000000000000000000000042, 0x73cb180bf0521828d8849bc8CF2B920918e23032, 0x7F5c764cBc14f9669B88837ca1490cCa17c31607],
         owners: [OptimismGnosisContract],
@@ -24,7 +23,6 @@ async function optimismTvl(timestamp, block, chainBlocks) {
 
 //Arbitrum TVL consists of investments/tokens on gnosis safe
 async function arbitrumTvl(timestamp, block, chainBlocks) {
-    let balance
     return sumTokensExport({
         tokens: [0x82aF49447D8a07e3bd95BD0d56f35241523fBab1, 0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a, 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8,0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9, 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f],
         owners: [ArbitrumGnosisContract],
@@ -33,11 +31,25 @@ async function arbitrumTvl(timestamp, block, chainBlocks) {
     })
 }
 
+//Binance TVL consists of investments/tokens on gnosis safe
+async function binanceTvl(timestamp, block, chainBlocks) {
+    return sumTokensExport({
+        tokens: [0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56, 0xAD6742A35fB341A9Cc6ad674738Dd8da98b94Fb1, 0x14016E85a25aeb13065688cAFB43044C2ef86784, 0x52F24a5e03aee338Da5fd9Df68D2b6FAe1178827, 0xA60205802E1B5C6EC1CAFA3cAcd49dFeECe05AC9, 0x9f8BB16f49393eeA4331A39B69071759e54e16ea, 0xD7FbBf5CB43b4A902A8c994D94e821f3149441c7],
+        owners: [BinanceGnosisContract],
+        chain: "binance",
+        block: chainBlocks.binance
+    })
+}
+
+
 module.exports = {
     optimism: {
         tvl: optimismTvl
     },
     arbitrum: {
         tvl: arbitrumTvl
+    },
+    binance: {
+        tvl: binanceTvl
     },
 }
