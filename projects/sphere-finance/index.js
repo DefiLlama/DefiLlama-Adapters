3c008e106f585bbac80f8ef9c41f8024a95f3355
Avedith <avedith@avedith.dev>
2023-02-09 17:55:50 +1000

    add polygon tokens

--- projects/sphere-finance/index.js
+++ projects/sphere-finance/index.js
@@ -3,27 +3,6 @@ const { sumTokensExport } = require("../helper/unwrapLPs");
 const SphereToken = "0x62f594339830b90ae4c084ae7d223ffafd9658a7";
 const SphereLP = "0xf3312968c7D768C19107731100Ece7d4780b47B2"; // SPHERE/MATIC LP
 
-const PolygonGnosisContracts = [
-  "0x20D61737f972EEcB0aF5f0a85ab358Cd083Dd56a",
-  "0x1a2ce410a034424b784d4b228f167a061b94cff4",
-  "0x826b8d2d523e7af40888754e3de64348c00b99f4",
-];
-
-//Binance TVL consists of investments/tokens in gnosis safe
-async function binanceTvl(timestamp, block) {
-  return sumTokensExport({
-    tokens: [
-      "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", //BUSD
-      "0xAD6742A35fB341A9Cc6ad674738Dd8da98b94Fb1", //WOM
-      "0x14016E85a25aeb13065688cAFB43044C2ef86784", //TUSD
-      "0x52F24a5e03aee338Da5fd9Df68D2b6FAe1178827", //ankrBNB
-      "0xA60205802E1B5C6EC1CAFA3cAcd49dFeECe05AC9", //CONE
-      "0x9f8BB16f49393eeA4331A39B69071759e54e16ea", //MDB+
-      "0xD7FbBf5CB43b4A902A8c994D94e821f3149441c7", //UNKWN
-    ],
-    owner: "0x124E8498a25EB6407c616188632D40d80F8e50b0",
-  });
-}
 
 module.exports = {
   // optimism TVL consists of investments/tokens in gnosis safe
@@ -64,6 +43,29 @@ module.exports = {
         "0xD7FbBf5CB43b4A902A8c994D94e821f3149441c7", //UNKWN
       ],
       owner: "0x124E8498a25EB6407c616188632D40d80F8e50b0",
+    }),
+  },
+  polygon: {
+    tvl: sumTokensExport({
+      tokens: [
+        "0xfa68FB4628DFF1028CFEc22b4162FCcd0d45efb6", //MATICx
+        "0x255707B70BF90aa112006E1b07B9AeA6De021424", //TETU
+        "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", //USDC
+        "0x236eec6359fb44cce8f97e99387aa7f8cd5cde1f", //USD+
+        "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619", //WETH
+        "0x0000000000000000000000000000000000001010", //MATIC
+        "0x9246a5F10A79a5a939b0C2a75A3AD196aAfDB43b", //BETS
+        "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6", //WBTC
+        "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", //USDT
+        "0x1d734A02eF1e1f5886e66b0673b71Af5B53ffA94", //STADER
+        "0x80487b4f8f70e793A81a42367c225ee0B94315DF", //CASH
+        "",
+      ],
+      owner: [
+        "0x20D61737f972EEcB0aF5f0a85ab358Cd083Dd56a",
+        "0x1a2ce410a034424b784d4b228f167a061b94cff4",
+        "0x826b8d2d523e7af40888754e3de64348c00b99f4",
+      ],
     }),
   },
 };
