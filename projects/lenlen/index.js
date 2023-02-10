971e674440b6797958975066321bff662ccc8f79
clarencesea <clarence.sea01@gmail.com>
2023-02-10 15:25:28 +0800

    Fix: LENLEN fetch

--- projects/lenlen/index.js
+++ projects/lenlen/index.js
@@ -2,7 +2,7 @@ const retry = require('async-retry')
 const axios = require("axios");
 
 async function fetch() {
-  let response = await retry(async bail => await axios.get('https://api.v-token.io/api/lenen/defiLlama'))
+  let {data: response} = await axios.get('https://api.v-token.io/api/lenen/defiLlama')
   let tvl = 0
   if (response.code === 200 && response.data) {
     tvl = parseFloat(response.data.total_value_locked ?? '0') + parseFloat(response.data.total_borrow ?? '0')
