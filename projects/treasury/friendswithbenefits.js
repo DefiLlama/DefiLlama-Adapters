230e08fdbc106de8eb56a0838f9c4ebbaaa86063
realdealshaman <realdealshaman11@gmail.com>
2023-02-11 01:49:34 +0000

    add usdc address

--- projects/treasury/friendswithbenefits.js
+++ projects/treasury/friendswithbenefits.js
@@ -9,7 +9,7 @@ module.exports = treasuryExports({
   ethereum: {
     tokens: [ 
         nullAddress,
-        '0x35bD01FC9d6D5D81CA9E055Db88Dc49aa2c699A8',//usdc
+        '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',//usdc
         '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',//weth
         '0xBcca60bB61934080951369a648Fb03DF4F96263C',//ausdc
         '0x9355372396e3F6daF13359B7b607a3374cc638e0',//whale
