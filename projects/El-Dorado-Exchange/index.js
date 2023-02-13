65b354f81350d4b4d07e624c0d4efc1d5ec5dc5c
Roy <hfers@msn.com>
2023-02-13 11:50:37 +0800

    add ELP2 TVL

--- projects/El-Dorado-Exchange/index.js
+++ projects/El-Dorado-Exchange/index.js
@@ -6,6 +6,7 @@ module.exports = {
   bsc: {
     tvl: sdk.util.sumChainTvls([
       gmxExports({ chain, vault: '0x7f90C8De425e2E21F6d152e881713DE5Fe37dEAB', }),
+      gmxExports({ chain, vault: '0x2c7077cF9bd07C3BC45B4E5b8C27f8B95c6550B3', }),
     ])
   }
 }
