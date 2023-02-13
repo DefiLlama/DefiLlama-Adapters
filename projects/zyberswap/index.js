95cc34b10243f0f66c3fc25a98dc2a37038be35c
zyberswap <arbadex@protonmail.com>
2023-02-13 17:10:27 +0100

    fixed mistake

--- projects/zyberswap/index.js
+++ projects/zyberswap/index.js
@@ -7,7 +7,7 @@ const ZYBER = "0x3B475F6f2f41853706afc9Fa6a6b8C5dF1a2724c";
 const stableSwapPools = {
     '3pool': {
         name: '3pool',
-        address: '0x1A90A043751A364447110FF95CCa05AE752d85BE',
+        address: '0x969f7699fbB9C79d8B61315630CDeED95977Cfb8',
         assets: ['USDC', 'USDT', 'DAI'],
         lpToken: '3pool'
     },
