8c6ee5510dea956bf567bcfe0668da3d88114846
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-13 12:55:25 +0800

    Add PancakeSwap stableswap tvl

--- projects/pancake-swap/index.js
+++ projects/pancake-swap/index.js
@@ -1,8 +1,8 @@
 const { request,  } = require("graphql-request");
-const { toUSDTBalances } = require('./helper/balances')
-const { stakings } = require('./helper/staking')
-const { getUniTVL } = require('./helper/unknownTokens')
-const { dexExport } = require('./helper/chain/aptos')
+const { toUSDTBalances } = require('../helper/balances')
+const { stakings } = require('../helper/staking')
+const { getUniTVL } = require('../helper/unknownTokens')
+const { dexExport } = require('../helper/chain/aptos')
 
 
 const graphEndpoint = 'https://proxy-worker.pancake-swap.workers.dev/bsc-exchange'
@@ -73,7 +73,7 @@ async function tvl(timestamp, ethBlock, chainBlocks) {
 module.exports = {
   timetravel: false,
   misrepresentedTokens: true,
-  methodology: 'TVL accounts for the liquidity on all AMM pools, using the TVL chart on https://pancakeswap.info/ as the source. Staking accounts for the CAKE locked in MasterChef (0x73feaa1eE314F8c655E354234017bE2193C9E24E)',
+  methodology: 'TVL accounts for the liquidity on all AMM pools, using the TVL chart on https://pancakeswap.finance/info as the source. Staking accounts for the CAKE locked in MasterChef (0x73feaa1eE314F8c655E354234017bE2193C9E24E)',
   bsc: {
     staking: stakings(["0x73feaa1eE314F8c655E354234017bE2193C9E24E", "0x45c54210128a065de780c4b0df3d16664f7f859e"], "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82", "bsc"),
     tvl
