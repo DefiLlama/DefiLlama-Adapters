e4309454fbb1e0a4f3f3d67b2f2c6f52e6b9ca76
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-13 12:55:25 +0800

    Add PancakeSwap stableswap tvl

---
+++ projects/pancake-swap-stableswap/index.js
@@ -0,0 +1,18 @@
+const { getChainTvl } = require("../helper/getUniSubgraphTvl");
+
+const stableGraph = getChainTvl(
+  {
+    bsc: "https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-stableswap",
+  },
+  "factories"
+);
+
+
+module.exports = {
+  misrepresentedTokens: true,
+  methodology:
+    "TVL accounts for the liquidity on all StableSwap pools, using the TVL chart on https://pancakeswap.finance/info?type=stableSwap as the source.",
+  bsc: {
+    tvl: stableGraph("bsc"),
+  },
+};
