4126c1c23b2b5f3096e1bae9b0eb5829f58600c4
Orion-starfish <orion@starfish.finance>
2023-02-13 15:09:22 +0800

    feat: add starfish-finance tvl

--- projects/starfish-finance/index.js
+++ projects/starfish-finance/index.js
@@ -1,15 +1,30 @@
 const { staking } = require("../helper/staking.js");
- 
-const addresses = {
-  astar: {
-    seanStaking: '0xa86dc743efBc24AF4c1FC5d150AaDb4DCF52c868',
-    seanToken: '0xEe8138B3bd03905cF84aFE10cCD0dCcb820eE08E'
-  }
+const { getLiquidityTvl, getFarmTvl, addresses } = require("./helper.js");
+
+function tvl(chain) {
+  return async (timestamp, ethBlock, chainBlocks) => {
+    const block = chainBlocks[chain];
+    const [liquidityTvl, farmTvl] = await Promise.all([
+      getLiquidityTvl(chain, block),
+      getFarmTvl(chain, block),
+    ]);
+    // console.log({
+    //   liquidityTvl: liquidityTvl.toNumber(),
+    //   farmTvl: farmTvl.toNumber(),
+    // });
+    return { tether: liquidityTvl.plus(farmTvl) };
+  };
 }
 
 module.exports = {
   astar: {
-    tvl: () => ({}),
-    staking: staking(addresses.astar.seanStaking, addresses.astar.seanToken, "astar", 'starfish-finance', 18)
-  }
-}
+    tvl: tvl("astar"),
+    staking: staking(
+      addresses.astar.seanStaking,
+      addresses.astar.seanToken,
+      "astar",
+      "starfish-finance",
+      18
+    ),
+  },
+};
