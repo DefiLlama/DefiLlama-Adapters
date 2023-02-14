726a7cf6f59893c87ebbc75960058bb5d6c16cb5
Gaspare Marchese <gaspare.marchese@mercedes-benz.io>
2023-02-14 16:40:49 +0000

    fix tvl

--- projects/lioncommerce/index.js
+++ projects/lioncommerce/index.js
@@ -3,16 +3,16 @@ const { unknownTombs, sumTokensExport } = require("../helper/unknownTokens");
 const { mergeExports } = require("../helper/utils");
 
 const stakingValue = sumTokensExport({
-	chain: "kava",
-	owner: "0x52b18024e084150e001a34be9c7a41706517d79f",
-	tokens: ["0x990e157fC8a492c28F5B50022F000183131b9026"],
-	lps: ["0x59e38a5799B64fE17c5fAb7E0E5396C15E2acb7b"],
-	useDefaultCoreAssets: true,
-  });
+  chain: "kava",
+  owner: "0x52b18024e084150e001a34be9c7a41706517d79f",
+  tokens: ["0x990e157fC8a492c28F5B50022F000183131b9026"],
+  lps: ["0x59e38a5799B64fE17c5fAb7E0E5396C15E2acb7b"],
+  useDefaultCoreAssets: true,
+});
 const lionStaking = {
   kava: {
     staking: stakingValue,
-	tvl: 0.
+    tvl: async () => ({}),
   },
 };
 
