1eb4a2cb2dfad87d90d93b7b4466999c0b66c501
Alderik <alderikvanderheyde@Alderiks-MacBook-Pro.local>
2023-02-13 23:02:38 -0500

    add ww

---
+++ projects/White Whale/index.js
@@ -0,0 +1,28 @@
+const { getFactoryTvl } = require("../terraswap/factoryTvl");
+
+module.exports = {
+  timetravel: false,
+  misrepresentedTokens: true,
+  methodology: "Liquidity on the DEX",
+  terra2: {
+    tvl: getFactoryTvl(
+      "terra14x9fr055x5hvr48hzy2t4q7kvjvfttsvxusa4xsdcy702mnzsvuqprer8r",
+      true
+    ),
+  },
+  injective: {
+    tvl: getFactoryTvl("inj1x22q8lfhz7qcvtzs0dakhgx2th64l79kfye5lk", true),
+  },
+  comdex: {
+    tvl: getFactoryTvl(
+      "comdex1gurgpv8savnfw66lckwzn4zk7fp394lpe667dhu7aw48u40lj6jswv4mft",
+      true
+    ),
+  },
+  juno: {
+    tvl: getFactoryTvl(
+      "juno14m9rd2trjytvxvu4ldmqvru50ffxsafs8kequmfky7jh97uyqrxqs5xrnx",
+      true
+    ),
+  },
+}; // node test.js projects/White-Whale/index.js
