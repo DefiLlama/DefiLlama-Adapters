af48bdf2ed8c0fd8a5de212436ac6a33f1b914dd
David Vittori <me@vittoridavid.com>
2023-02-16 10:55:13 +0100

    updating TVL calculation to include Pylon

--- projects/zircon-gamma/index.js
+++ projects/zircon-gamma/index.js
@@ -1,8 +1,40 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const sdk = require('@defillama/sdk');
+const { request, gql } = require("graphql-request"); // GraphQLClient
+
+
+async function fetch() {
+  const GAMMA_SUBGRAPH_URI = 'https://api.thegraph.com/subgraphs/name/reshyresh/zircon-finance'
+
+  const tokensQuery = gql`
+      query {
+          tokens(first: 100, where: {totalLiquidity_gt: "0.01"}) {
+              id
+              symbol
+              totalLiquidity
+              decimals
+          }
+      }
+  `
+
+  const { tokens } = await request(
+    GAMMA_SUBGRAPH_URI,
+    tokensQuery
+  )
+
+  const allBalances = {}
+  tokens.forEach(token => {sdk.util.sumSingleBalance(allBalances,
+    token.symbol === 'LDO' ? '0x5a98fcbea516cf06857215779fd812ca3bef1b32' :
+      token.symbol === 'xcKSM' ? 'kusama' :
+        token.symbol === 'ZRG' ? 'zircon-gamma-token' :
+          `moonriver:${token.id}`,
+    token.symbol === 'xcKSM' || token.symbol === 'ZRG' ? parseInt(token.totalLiquidity) :
+      BigInt(parseFloat(token.totalLiquidity).toFixed(0)*(10**token.decimals)))})
+  return allBalances;
+}
+
 
 module.exports = {
-  misrepresentedTokens: true,
   moonriver: {
-    tvl: getUniTVL({ chain: 'moonriver', factory: '0x6B6071Ccc534fcee7B699aAb87929fAF8806d5bd', useDefaultCoreAssets: true, }),
-  },
+    tvl: fetch,
+  }
 };
