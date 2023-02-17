4107a4c0b6efa5466d6bdf763aa45aca514471f2
bankofcronos <122385645+bankofcronos@users.noreply.github.com>
2023-02-16 20:17:50 -0500

    Update bankofcronos.js

--- projects/treasury/bankofcronos.js
+++ projects/treasury/bankofcronos.js
@@ -1,9 +1,9 @@
 const {  nullAddress,treasuryExports } = require("../helper/treasury");
 
-const ohmTreasury = "0xBacF28BF21B374459C738289559EF89978D08102";
+const teamTreasury = "0x1087234fe877721F30016ebeD5BEd061397C8851";
 
-const OHM = "0xe5786DDFc4D6DcA0973D1c5b02987cBbac66ed87";
-
+const BOC = "0xe5786DDFc4D6DcA0973D1c5b02987cBbac66ed87";
+const pBOC = "0xF93fB4CDB0e40dbF33d2cDbf11D9516f6aDd7e8e";
 
 module.exports = treasuryExports({
   cronos: {
@@ -15,8 +15,8 @@ module.exports = treasuryExports({
         '0xe44Fd7fCb2b1581822D0c862B68222998a0c299a',//WETH
         '0x062E66477Faf219F25D27dCED647BF57C3107d52',//WBTC
      ],
-    owners: [ohmTreasury],
-    ownTokenOwners: [ohmTreasury],
-    ownTokens: [OHM],
+    owners: [teamTreasury],
+    ownTokenOwners: [teamTreasury],
+    ownTokens: [BOC, pBOC],
   },
 })
