290ecb99f55cbc6592ab218741bfdc166da62e2a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-20 13:57:13 +0530

    benddao: code refactor

--- projects/benddao/index.js
+++ projects/benddao/index.js
@@ -1,6 +1,5 @@
 const sdk = require("@defillama/sdk");
 const { staking } = require("../helper/staking");
-const { pool2 } = require("../helper/pool2");
 const { tvl, borrowed } = require("./helper/index");
 const address = require("./helper/address");
 
@@ -10,20 +9,7 @@ module.exports = {
   ethereum: {
     tvl,
     borrowed,
-    staking: staking(
-      address.ethereum.VeBend,
-      address.ethereum.Bend,
-      "ethereum"
-    ),
-    pool2: pool2(
-      address.ethereum.StakedBUNI,
-      address.ethereum.UniswapV2PairWETH,
-      "ethereum"
-    ),
-    treasury: staking(
-      address.ethereum.Treasury,
-      [address.ethereum.USDC, address.ethereum.ApeCoin],
-      "ethereum"
-    ),
+    staking: staking(address.ethereum.VeBend, address.ethereum.Bend,),
+    pool2: staking(address.ethereum.StakedBUNI, address.ethereum.UniswapV2PairWETH,),
   },
 };
