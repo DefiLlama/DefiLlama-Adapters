6e137c2f55a073a279397d5253fcfe9ea6b190d0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-20 14:39:10 +0530

    sean: code refactor

--- projects/starfish-finance/index.js
+++ projects/starfish-finance/index.js
@@ -1,30 +1,13 @@
 const { staking } = require("../helper/staking.js");
-const { getLiquidityTvl, getFarmTvl, addresses } = require("./helper.js");
+const { getLiquidityTvl, addresses } = require("./helper.js");
 
-function tvl(chain) {
-  return async (timestamp, ethBlock, chainBlocks) => {
-    const block = chainBlocks[chain];
-    const [liquidityTvl, farmTvl] = await Promise.all([
-      getLiquidityTvl(chain, block),
-      getFarmTvl(chain, block),
-    ]);
-    // console.log({
-    //   liquidityTvl: liquidityTvl.toNumber(),
-    //   farmTvl: farmTvl.toNumber(),
-    // });
-    return { tether: liquidityTvl.plus(farmTvl) };
-  };
-}
+async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
+  return getLiquidityTvl(api)
+};
 
 module.exports = {
   astar: {
-    tvl: tvl("astar"),
-    staking: staking(
-      addresses.astar.seanStaking,
-      addresses.astar.seanToken,
-      "astar",
-      "starfish-finance",
-      18
-    ),
+    tvl,
+    staking: staking(addresses.astar.seanStaking, addresses.astar.seanToken,),
   },
 };
