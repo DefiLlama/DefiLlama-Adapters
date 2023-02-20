bdce6d37ae83967d36385ae3da370735f800918b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-20 13:57:13 +0530

    benddao: code refactor

--- projects/benddao/helper/index.js
+++ projects/benddao/helper/index.js
@@ -3,63 +3,35 @@ const { sumTokens2 } = require("../../helper/unwrapLPs");
 
 const abi = require("./abis");
 const address = require("./address");
-const BigNumber = require("bignumber.js");
 
 async function tvl(chain, timestamp, chainBlocks, { api }) {
-  const block = chainBlocks[api.chain];
   const addressMap = address[api.chain];
 
-  const [{ output: simpleReservesData }, { output: bnftAssetList }] =
+  const [simpleReservesData, bnftAssetList] =
     await Promise.all([
-      sdk.api.abi.call({
+      api.call({
         target: addressMap.UiPoolDataProvider,
         params: [addressMap.LendPoolAddressProvider],
         abi: abi.UiPoolDataProvider.getSimpleReservesData,
       }),
-      sdk.api.abi.call({
+      api.call({
         target: addressMap.BNFTRegistry,
-        params: [],
         abi: abi.BNFTRegistry.getBNFTAssetList,
       }),
     ]);
 
-  const { output: bnftProxyList } = await sdk.api.abi.multiCall({
-    calls: bnftAssetList.map((d) => ({
-      target: addressMap.BNFTRegistry,
-      params: [d],
-    })),
+  const bnftProxyList = await api.multiCall({
+    calls: bnftAssetList,
+    target: addressMap.BNFTRegistry,
     abi: abi.BNFTRegistry.bNftProxys,
   });
 
   const toa = [
-    ...bnftAssetList.map((i, idx) => [i, bnftProxyList[idx].output]),
+    ...bnftAssetList.map((i, idx) => [i, bnftProxyList[idx]]),
     ...simpleReservesData.map((i) => [i.underlyingAsset, i.bTokenAddress]),
   ];
 
-  const balances = await sumTokens2({ api, tokensAndOwners: toa });
-
-  const [{ output: apeStakingStakedTotal }] = await Promise.all([
-    sdk.api.abi.multiCall({
-      calls: bnftProxyList.map((d) => {
-        return {
-          target: addressMap.ApeCoinStaking,
-          params: [d.output],
-        };
-      }),
-      abi: abi.ApeCoinStaking.stakedTotal,
-    }),
-  ]);
-
-  apeStakingStakedTotal.forEach((d) => {
-    sdk.util.sumSingleBalance(
-      balances,
-      addressMap.ApeCoin,
-      d.output,
-      api.chain
-    );
-  });
-
-  return balances;
+  return sumTokens2({ api, tokensAndOwners: toa });
 }
 
 async function borrowed(chain, timestamp, chainBlocks, { api }) {
