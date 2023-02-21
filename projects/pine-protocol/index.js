9b43182d417aa47a110d7f136939c3e77066584f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-21 12:54:49 +0530

    pine-protocol: turn into onchain adapter

--- projects/pine-protocol/index.js
+++ projects/pine-protocol/index.js
@@ -1,83 +1,33 @@
-const axios = require("axios");
-const BigNumber = require("bignumber.js");
-const ethers = require("ethers");
-const ERC20 = require("./ERC20.json");
-const { gql, request } = require("graphql-request");
+const { getLogs, getAddress } = require('../helper/cache/getLogs')
+const { sumTokens2 } = require('../helper/unwrapLPs')
+const sdk = require('@defillama/sdk')
 
-const subgraph_url =
-  "https://api.thegraph.com/subgraphs/name/pinedefi/open-loans";
+async function _getLogs(api) {
+  const logs = await getLogs({
+    api,
+    target: '0x19c56cb20e6e9598fc4d22318436f34981e481f9',
+    topics: ['0xfe735a4766d74f0c8b076be3f254cfc301a851f3ac49c4ae35e7d48ab75cd69d'],
+    fromBlock: 16423090,
+  })
+  return logs.map(i => i.topics.slice(1).map(getAddress))
+}
 
-const GQL_POOLS = gql`
-  query pools {
-    pools(first: 1000) {
-      id
-      totalUtilization
-      collection
-      supportedCurrency
-      target
-      fundSource
-      duration
-      interestBPS1000000XBlock
-      collateralFactorBPS
-      lenderAddress
-    }
-  }
-`;
+async function tvl(_, _b, _cb, { api, }) {
+  const logs = await _getLogs(api)
+  return sumTokens2({ api, tokensAndOwners: logs.map(i => [i[1], i[0]]) })
+}
 
-const provider = new ethers.providers.JsonRpcProvider(
-  "https://eth-mainnet.g.alchemy.com/v2/UN5z0qze-a7hJuNOqFMUs9t2blMsxcN7"
-);
-
-const tvl = async () => {
-  const { data: pools } = await axios.get(
-    "http://localhost:8080/v0/pools/addresses?ethereum=1"
-  );
-
-  const { pools: subgraphPools } = await request(subgraph_url, GQL_POOLS);
-
-  let tvl = new BigNumber(0);
-
-  await Promise.all(
-    pools.map(async (pool) => {
-      let balanceWei = new BigNumber(0);
-      try {
-        if (pool.version === 1) {
-          balanceWei = await provider.getBalance(pool.address);
-        } else {
-          const contract = new ethers.Contract(
-            pool.tokenAddress,
-            ERC20,
-            provider
-          );
-          balanceWei = await contract.functions.balanceOf(pool.fundSource);
-        }
-        balanceWei = ethers.utils.formatEther(balanceWei.toString());
-        const utilization =
-          subgraphPools.find((p) => p.id === pool.address.toLowerCase())
-            ?.totalUtilization || "0";
-        const utilizationBN = ethers.utils.formatEther(utilization);
-        const capacityBN = new BigNumber(balanceWei.toString());
-        const valueLockedEth = capacityBN
-          .plus(utilizationBN)
-          .gt(new BigNumber(Number(pool.ethLimit) || Number.POSITIVE_INFINITY))
-          ? new BigNumber(pool.ethLimit ?? "0")
-          : capacityBN.plus(utilizationBN);
-        tvl = tvl.plus(valueLockedEth);
-      } catch (err) {
-        console.log(err);
-      }
-    })
-  );
-
-  return {
-    "coingecko:ethereum": tvl.toString(),
-  };
-};
+async function borrowed(_, _b, _cb, { api, }) {
+  const balances = {}
+  const logs = await _getLogs(api)
+  const loans = await api.multiCall({ abi: 'uint256:_currentLoanAmount', calls: logs.map(i => i[0]) })
+  loans.forEach((val, i) => sdk.util.sumSingleBalance(balances, logs[i][2], val, api.chain))
+  return balances
+}
 
 module.exports = {
-  methodology:
-    "Non-custodial decentralized asset-backed lending protocol that allows borrowers to borrow fungible digital tokens from lenders using non-fungible tokens as collateral",
+  methodology: 'value of NFTs locked in pools as taken as tvl, tokens borrowed against it is counted towards borrowed',
   ethereum: {
-    tvl,
-  },
-};
+    tvl, borrowed
+  }
+}
