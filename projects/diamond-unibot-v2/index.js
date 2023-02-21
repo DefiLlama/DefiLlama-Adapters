5d0dd360c3d40d9d8fad796ae9c043c230bfda28
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-21 16:03:21 +0530

    add lending pool and vault tvl

--- projects/diamond-unibot-v2/index.js
+++ projects/diamond-unibot-v2/index.js
@@ -1,8 +1,22 @@
 const { getLogs, getAddress } = require('../helper/cache/getLogs')
 const { sumTokens2, unwrapUniswapV3NFTs } = require('../helper/unwrapLPs')
 
+const lendingPools = [
+  ['0x82af49447d8a07e3bd95bd0d56f35241523fbab1', '0xedd1efa76fe59e9106067d824b89b59157c5223c'], // WETH
+]
+
+const vaults = [
+  [
+    [
+      '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8', // USDC
+      '0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a', // GMX
+    ],
+    '0x8610D60f5329B0560c8F0CEb80175F342fe943F3' // vault
+  ]
+]
+
 async function tvl(_, _b, _cb, { api, }) {
-  const balances = {}
+  const balances = await sumTokens2({ api, ownerTokens: vaults })
   const logs = await getLogs({
     api,
     target: '0x8a908ec03e2610fa8dcaec93bb010560780ec860',
@@ -13,8 +27,10 @@ async function tvl(_, _b, _cb, { api, }) {
   const pools = logs.map(i => getAddress(i.topics[1]))
   const wantTokens = logs.map(i => getAddress(i.data))
 
-  await unwrapUniswapV3NFTs({ ...api, balances, owners: pools})
-  return sumTokens2({ balances, api, tokensAndOwners: wantTokens.map((i, idx) => [i, pools[idx]])})
+  await unwrapUniswapV3NFTs({ ...api, balances, owners: pools })
+  const tokensAndOwners = wantTokens.map((i, idx) => [i, pools[idx]])
+  tokensAndOwners.push(...lendingPools)
+  return sumTokens2({ balances, api, tokensAndOwners })
 }
 
 module.exports = {
