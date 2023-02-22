6689079ee213b82a0f5d38387e7923284fb5e23b
kokosro <kokos@kokos.ro>
2023-02-22 16:12:39 +0200

    cleanup logs

--- projects/ante/index.js
+++ projects/ante/index.js
@@ -1,61 +1,61 @@
-const sdk = require("@defillama/sdk");
-const { sumTokens2, nullAddress } = require("../helper/unwrapLPs");
-const { createIncrementArray } = require("../helper/utils");
+const sdk = require('@defillama/sdk');
+const { sumTokens2, nullAddress } = require('../helper/unwrapLPs');
+const { createIncrementArray } = require('../helper/utils');
 
 const CONFIG = {
   ethereum: {
     factories: [
       {
-        factory: "0xa03492A9A663F04c51684A3c172FC9c4D7E02eDc",
-        version: "0.5.0",
+        factory: '0xa03492A9A663F04c51684A3c172FC9c4D7E02eDc',
+        version: '0.5.0',
       },
       {
-        factory: "0x89E583A67B5FA1B39b8CE7E77654071c3a34cc48",
-        version: "0.6.0",
+        factory: '0x89E583A67B5FA1B39b8CE7E77654071c3a34cc48',
+        version: '0.6.0',
       },
     ],
     startBlock: 13234803,
-    gasToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    gasToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
   },
   avax: {
     factories: [
       {
-        factory: "0x18aB6357f673696375018f006B86fE44F195DE1f",
-        version: "0.5.1",
+        factory: '0x18aB6357f673696375018f006B86fE44F195DE1f',
+        version: '0.5.1',
       },
     ],
     startBlock: 16037331,
-    gasToken: "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
+    gasToken: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
   },
   polygon: {
     factories: [
       {
-        factory: "0xb4FD0Ce108e196d0C9844c48174d4C32Cd42F7bC",
-        version: "0.5.1",
+        factory: '0xb4FD0Ce108e196d0C9844c48174d4C32Cd42F7bC',
+        version: '0.5.1',
       },
     ],
     startBlock: 32245577,
-    gasToken: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+    gasToken: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
   },
   bsc: {
     factories: [
       {
-        factory: "0xb4FD0Ce108e196d0C9844c48174d4C32Cd42F7bC",
-        version: "0.5.1",
+        factory: '0xb4FD0Ce108e196d0C9844c48174d4C32Cd42F7bC',
+        version: '0.5.1',
       },
     ],
     startBlock: 20928838,
-    gasToken: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    gasToken: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
   },
   fantom: {
     factories: [
       {
-        factory: "0xb4FD0Ce108e196d0C9844c48174d4C32Cd42F7bC",
-        version: "0.5.1",
+        factory: '0xb4FD0Ce108e196d0C9844c48174d4C32Cd42F7bC',
+        version: '0.5.1',
       },
     ],
     startBlock: 46604874,
-    gasToken: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+    gasToken: '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83',
   },
 };
 
@@ -70,27 +70,21 @@ Object.keys(CONFIG).forEach((chain) => {
       const length = 4;
 
       for (const factory of factories) {
-        console.log(factory);
-        if (factory.version === "0.6.0") {
-          try {
-            const controller = await sdk.api.abi.call({
-              target: factory.factory,
-              abi: abis.getController,
-              chain,
-              block,
-            });
-            const allowedTokens = await sdk.api.abi.call({
-              target: controller.output,
-              abi: abis.getAllowedTokens,
-              chain,
-              block,
-            });
+        if (factory.version.split('.').slice(0,2).join('.') === '0.6') {
+          const controller = await sdk.api.abi.call({
+            target: factory.factory,
+            abi: abis.getController,
+            chain,
+            block,
+          });
+          const allowedTokens = await sdk.api.abi.call({
+            target: controller.output,
+            abi: abis.getAllowedTokens,
+            chain,
+            block,
+          });
 
-            tokens = [...new Set([...tokens, ...allowedTokens.output])];
-            console.log(tokens);
-          } catch (e) {
-            console.log(e);
-          }
+          tokens = [...new Set([...tokens, ...allowedTokens.output])];
         }
         do {
           calls = createIncrementArray(4).map((j) => ({
@@ -123,7 +117,7 @@ Object.keys(CONFIG).forEach((chain) => {
 });
 
 const abis = {
-  allPools: "function allPools(uint256) view returns (address)",
-  getController: "function controller() view returns (address)",
-  getAllowedTokens: "function getAllowedTokens() view returns (address[])",
+  allPools: 'function allPools(uint256) view returns (address)',
+  getController: 'function controller() view returns (address)',
+  getAllowedTokens: 'function getAllowedTokens() view returns (address[])',
 };
