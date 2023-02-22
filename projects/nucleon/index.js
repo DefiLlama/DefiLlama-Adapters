1e0672297c5c108693bee05346fdf7778426b1e2
n1c0 <111755752+0xn1c0@users.noreply.github.com>
2023-02-22 17:58:42 -0300

    Fixed TVL
    Fixed the TVL calculation from Amount to CFX Staked, to Total Value of CFX Staked

--- projects/nucleon/index.js
+++ projects/nucleon/index.js
@@ -1,8 +1,10 @@
 const sdk = require("@defillama/sdk")
 const token = '0x889138644274a7dc602f25a7e7d53ff40e6d0091'
 const chain = 'conflux'
+const axios = require("axios");
+const { BigNumber } = require("ethers");
 
-function xcfxSupply(target) {
+function stakedValue(target) {
   return async (timestamp, block, chainBlocks) => {
       let supply = {};
       supply = { everscale: (await sdk.api.abi.call({
@@ -11,14 +13,41 @@ function xcfxSupply(target) {
           block: chainBlocks[chain],
           chain
       })).output / 10 ** 17 };
-      console.log(supply);
-      return supply ;
+      let price = (await getCFXPrice());
+
+      supply.everscale = supply.everscale * price;
+      
+      return supply;
   };
 }
 
+
+async function getCFXPrice() {
+  const tokenId = "wrapped-conflux";
+  // https://api.coingecko.com/api/v3/simple/price?ids=wrapped-conflux&vs_currencies=usd
+  let result;
+  let price = 0;
+  try {
+      result =
+          await axios({
+              method: "GET",
+              url: "https://api.coingecko.com/api/v3/simple/price",
+              params: {
+                  ids: tokenId,
+                  vs_currencies: "usd",
+              },
+          });
+  } catch (e) {
+      return price;
+  }
+  const data = result.data;
+  price = data[tokenId].usd;
+  return price;
+}
+
 module.exports = {
-  methodology: 'TVL accounts for XCFX Supply, which represents the amount of CFX staked on the Protocol.',
+  methodology: 'TVL accounts for XCFX Supply * CFX Price, which represents the amount of CFX staked on the Protocol.',
   conflux: {
-    tvl: xcfxSupply(token, "conflux", () => "wrapped-conflux"),
+    tvl: stakedValue(token, "conflux",() => "wrapped-conflux"),
   },
 }
