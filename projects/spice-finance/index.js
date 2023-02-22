d6bdb0e6be8a0207338436e3049eac5bc40e3262
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-22 23:41:59 +0530

    Update index.js

--- projects/spice-finance/index.js
+++ projects/spice-finance/index.js
@@ -1,24 +1,12 @@
-const sdk = require("@defillama/sdk");
-
 const VAULT = "0x6110d61DD1133b0f845f1025d6678Cd22A11a2fe";
 
-async function tvl(_1, _2, chainBlocks) {
-  const totalAssets = (
-    await sdk.api.abi.call({
-      target: VAULT,
-      abi: "function totalAssets() view returns (uint256 totalAssets)",
-      block: chainBlocks.ethereum,
-      chain: "ethereum",
-    })
-  ).output;
-  const balances = {
-    ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"]: totalAssets,
+async function tvl(_1, _2, chainBlocks, { api }) {
+  return {
+    "ethereum:0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": await api.call({ target: VAULT, abi: 'uint256:totalAssets'}),
   };
-  return balances;
 }
 
 module.exports = {
-  timetravel: true,
   misrepresentedTokens: true,
   methodology: `Counts WETH deposited to earn yield via NFT lending.`,
   ethereum: {
