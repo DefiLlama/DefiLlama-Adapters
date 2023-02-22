a38cdef2e966561b65503b38f6bfb04f018c456b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-22 11:08:30 +0530

    move to staking

--- projects/perion/index.js
+++ projects/perion/index.js
@@ -1,24 +1,9 @@
-const sdk = require("@defillama/sdk");
 const PERC = "0x60bE1e1fE41c1370ADaF5d8e66f07Cf1C2Df2268";
-
-async function tvl(_1, _2, chainBlocks) {
-  const totalAssets = (
-    await sdk.api.abi.call({
-      target: PERC,
-      abi: "function balanceOf(address account) view returns (uint256 balance)",
-      block: chainBlocks.ethereum,
-      chain: "ethereum",
-      params: ["0xf64F48A4E27bBC299273532B26c83662ef776b7e"]
-    })
-  ).output;
-  return {
-    [PERC]: totalAssets,
-  };
-}
+const { staking } = require('../helper/staking')
 
 module.exports = {
-  methodology: `Counts staked PERC.`,
   ethereum: {
-    tvl,
+    tvl: () => 0,
+    staking: staking('0xf64F48A4E27bBC299273532B26c83662ef776b7e', PERC)
   },
 };
