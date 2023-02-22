2f5fd9c04eeff0edf11c69ae483e9fbf520fdfe6
MrLightspeed <95941244+MrLightspeed@users.noreply.github.com>
2023-02-22 13:58:07 -0500

    Update puli.js
    Updated the wallets holding TVL tokens and staked lp

--- projects/treasury/puli.js
+++ projects/treasury/puli.js
@@ -1,5 +1,5 @@
 const { sumTokensExport, nullAddress, } = require('../helper/unknownTokens')
-// const PULI_TOKEN_STAKING_CONTRACT = '0x864d434308997e9648838d23f3eedf5d0fd17bea';
+const { POOL2S } = require("../helper/pool2")
 const chain = 'bsc'
 const TREASURY1 = '0xc569C21b0862B112Ed69bA9d2C6e9Ed86A036f9C'
 const TREASURY2 = '0xA017862ADba59aA030b8aA0433eD91D9d909B8B1'
@@ -13,7 +13,11 @@ module.exports = {
         [nullAddress, TREASURY2],
         ['0xe9e7cea3dedca5984780bafc599bd69add087d56', TREASURY1],
         ['0xC17c30e98541188614dF99239cABD40280810cA3', TREASURY1],
-        ['0x3FF5cbE338153063D8251d2B6a22A437EC09Eef3', TREASURY2],
+        ['0x55d398326f99059fF775485246999027B3197955', TREASURY1],
+        ['0xe9e7cea3dedca5984780bafc599bd69add087d56', TREASURY2],
+        ['0xC17c30e98541188614dF99239cABD40280810cA3', TREASURY2],
+        ['0x55d398326f99059fF775485246999027B3197955', TREASURY2],
+        ['0x3FF5cbE338153063D8251d2B6a22A437EC09Eef3', POOL2S],
       ],
     })
   }
