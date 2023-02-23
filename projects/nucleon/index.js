de47e1284dcf4591affc69d2835021b32879b401
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-23 11:51:19 +0530

    code refactor

--- projects/nucleon/index.js
+++ projects/nucleon/index.js
@@ -1,10 +1,5 @@
-const { stakingPricedLP } = require("../helper/staking");
-const sdk = require("@defillama/sdk")
-const { pool2 } = require("../helper/pool2");
-const {sumLPWithOnlyOneToken} = require("../helper/unwrapLPs.js");
-
-const axios = require("axios");
-const { BigNumber } = require("ethers");
+const { staking, } = require("../helper/staking");
+const { sumTokensExport, getTokenPrices, } = require("../helper/unknownTokens");
 
 const MasterchefV2 = "0xeced26633b5c2d7124b5eae794c9c32a8b8e7df2";
 const NUT_TokenAddress = "0xfe197e7968807b311d476915db585831b43a7e3b";
@@ -15,86 +10,17 @@ const CFX_XCFX_LP_TokenAddress = "0x949b78ef2c8d6979098e195b08f27ff99cb20448";
 
 const WCFX = "0x14b2d3bc65e74dae1030eafd8ac30c533c976a9b";
 
-const chain = 'conflux';
-
-function stakedValue(target) {
-  return async (timestamp, block, chainBlocks) => {
-      let supply = {};
-      supply = { usd: (await sdk.api.abi.call({
-        target,
-          abi: 'erc20:totalSupply',
-          block: chainBlocks[chain],
-          chain
-      })).output / 10 ** 18 };
-      let price = (await getCFXPrice());
-      
-      let cfx_nut_balance = {};
-      await sumLPWithOnlyOneToken(cfx_nut_balance, CFX_NUT_LP_TokenAddress,MasterchefV2, WCFX, undefined, chain, () => "wrapped-conflux");
-      cfx_nut_balance['wrapped-conflux'] = Number(cfx_nut_balance['wrapped-conflux'])*price/(10 ** 18);
-      
-      let cfx_xcfx_balance = {};
-      await sumLPWithOnlyOneToken(cfx_xcfx_balance, CFX_XCFX_LP_TokenAddress,MasterchefV2, WCFX, undefined, chain, () => "wrapped-conflux");
-      cfx_xcfx_balance['wrapped-conflux'] = Number(cfx_xcfx_balance['wrapped-conflux'])*price/(10 ** 18);
-
-      supply.usd = supply.usd * price 
-      supply.usd += cfx_xcfx_balance["wrapped-conflux"];
-      supply.usd += cfx_nut_balance["wrapped-conflux"];
-      
-
-      return supply;
-  };
-}
-
-
-async function getCFXPrice() {
-  const tokenId = "wrapped-conflux";
-  // https://api.coingecko.com/api/v3/simple/price?ids=wrapped-conflux&vs_currencies=usd
-  let result;
-  let price = 0;
-  try {
-      result =
-          await axios({
-              method: "GET",
-              url: "https://api.coingecko.com/api/v3/simple/price",
-              params: {
-                  ids: tokenId,
-                  vs_currencies: "usd",
-              },
-          });
-  } catch (e) {
-      return price;
-  }
-  const data = result.data;
-  price = data[tokenId].usd;
-  return price;
-}
-
-
-
-function nut_cfx_pool(chain = "ethereum", transformAddress = (addr) => addr) {
-  return async (_timestamp, _ethBlock, chainBlocks) => {
-      let balances = {};
-      await sumLPWithOnlyOneToken(balances, CFX_NUT_LP_TokenAddress,MasterchefV2, WCFX, undefined, chain, transformAddress);
-      balances['wrapped-conflux'] = Number(balances['wrapped-conflux'])/(10 ** 18);
-      return balances;
-  }
-}
-
-
-function xcfx_cfx_pool(chain = "ethereum", transformAddress = (addr) => addr) {
-  return async (_timestamp, _ethBlock, chainBlocks) => {
-      let balances = {};
-      await sumLPWithOnlyOneToken(balances, CFX_XCFX_LP_TokenAddress,MasterchefV2, WCFX, undefined, chain, transformAddress);
-      balances['wrapped-conflux'] = Number(balances['wrapped-conflux'])/(10 ** 18);
-      return balances;
-  }
-}
-
 module.exports = {
-  methodology: 'TVL accounts for total amount of CFX Staked on the protocol + the value of All LP Tokens staked on the Masterchef or Pools.',
   conflux: {
-    tvl: stakedValue(XCFX_TokenAddress, "conflux",() => "wrapped-conflux"),
-    staking: xcfx_cfx_pool("conflux", () => "wrapped-conflux"),
-    pool2: nut_cfx_pool("conflux", () => "wrapped-conflux"),
-  },
+    tvl: async (_, _1, _2, { api }) => {
+      const balances = {
+        ['conflux:' + XCFX_TokenAddress]: await api.call({ target: XCFX_TokenAddress, abi: 'erc20:totalSupply' })
+      }
+      const { updateBalances, } = await getTokenPrices({ ...api, coreAssets: [WCFX], lps: [CFX_XCFX_LP_TokenAddress] })
+      console.log(await api.call({ target: CFX_XCFX_LP_TokenAddress, abi: 'address:factory'}))
+      return updateBalances(balances)
+    },
+    pool2: sumTokensExport({ owner: MasterchefV2, coreAssets: [WCFX], tokens: [CFX_NUT_LP_TokenAddress, CFX_XCFX_LP_TokenAddress] }),
+    staking: staking(MasterchefV2, NUT_TokenAddress),
+  }
 }
