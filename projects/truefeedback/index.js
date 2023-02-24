6930b34209ea6bb91689d101e75dbe8256868e17
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-24 17:35:37 +0530

    code refactor

--- projects/truefeedback/index.js
+++ projects/truefeedback/index.js
@@ -1,35 +1,12 @@
-const { ethers } = require("ethers");
-const sdk = require("@defillama/sdk");
-
-const stakeContractAddresses = {
-  celo: '0x588069878442856b683ab39f410ed96b72fb542a',
-  kava: '0x067543c3D97753dDA22A2cF6a806f47BD6A17B6A',
-}
-
-function makeTvlFunction(chain) {
-  return async function tvl(timestamp, block, chainBlocks) {
-    var stakeBalance = (await sdk.api.abi.call({
-      target: stakeContractAddresses[chain],
-      abi: 'erc20:totalSupply',
-      block: chainBlocks[chain],
-      chain: chain
-    })).output
-    
-    //providerBalance = [balance, totalCap]
-    var staked = ethers.utils.formatEther(stakeBalance)
-    return {
-      'truefeedbackchain': staked,
-    };
-  }
-}
+const { staking } = require('../helper/staking')
 
 module.exports = {
-  methodology: 'TVL counts staked TFBX coins on the platform itself. CoinGecko is used to find the price of tokens in USD.',
   celo: {
-    tvl: makeTvlFunction('celo'),
+    tvl: () => 0,
+    staking: staking('0x588069878442856b683ab39f410ed96b72fb542a', '0xbdd31effb9e9f7509feaac5b4091b31645a47e4b', undefined, 'truefeedbackchain', 18),
   },
   kava: {
-    tvl: makeTvlFunction('kava'),
+    staking: staking('0x067543c3D97753dDA22A2cF6a806f47BD6A17B6A', '0xbd10F04B8b5027761fCAAd42421aD5d0787211Ee', undefined, 'truefeedbackchain', 18),
   },
 };
 
