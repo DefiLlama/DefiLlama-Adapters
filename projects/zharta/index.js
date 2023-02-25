fa62bbd15440f9a83f3b75ae6b1628e722598748
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-25 08:56:59 +0530

    zharta: add weth pool tvl

--- projects/zharta/index.js
+++ projects/zharta/index.js
@@ -1,14 +1,19 @@
-const { getWhitelistedNFTs, } = require('../helper/tokenMapping');
+const { getWhitelistedNFTs, tokensBare} = require('../helper/tokenMapping');
 const { sumTokensExport, } = require('../helper/unwrapLPs');
 
 // Vaults
 const collateralVault = "0xA79da8c90Aa480B3716C23145154CA6eF5Fc29C1";
-const punkVault = "0xA79da8c90Aa480B3716C23145154CA6eF5Fc29C1";
+const punkVault = "0x94925030F48aDfc3e4A65a2E0A7444733406c144";
+const LP_CORE = "0xe3c959bc97b92973d5367dbf4ce1b7b9660ee271";
 
 module.exports = {
   misrepresentedTokens: true,
   methodology: `Counts the floor value of all deposited NFTs with Chainlink price feeds. Borrowed coins are not counted towards the TVL`,
   ethereum: {
-    tvl: sumTokensExport({ owners: [collateralVault, punkVault], tokens: getWhitelistedNFTs(),}),
+    tvl: sumTokensExport({ ownerTokens: [
+      [getWhitelistedNFTs(), collateralVault], 
+      [getWhitelistedNFTs(), punkVault], 
+      [[tokensBare.weth], LP_CORE],
+    ]}),
   }
 }
