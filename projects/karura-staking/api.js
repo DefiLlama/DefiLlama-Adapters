aad644df6815d2a1196c43ead120e17de251b9d3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-26 00:51:14 +0530

    fix doublecounting

--- projects/karura-staking/api.js
+++ projects/karura-staking/api.js
@@ -3,5 +3,4 @@ const { staking } = require('../helper/acala/liquidStaking')
 
 module.exports = {
   karura: { tvl: async () => staking('karura') },
-  acala: { tvl: async () => staking('acala') },
 };
