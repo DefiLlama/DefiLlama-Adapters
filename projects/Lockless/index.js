729ad52a04b02f71a354115d383214693ac973c7
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2023-02-26 17:09:07 -0500

    using sdk.api.abi

--- projects/Lockless/index.js
+++ projects/Lockless/index.js
@@ -2,15 +2,37 @@ const sdk = require("@defillama/sdk")
 const swkava  = '0x9d9682577CA889c882412056669bd936894663fd'
 const swech   = '0x86e4D91800c03e803d4c8FA3293d1C7d612A7300'
 
+
+async function kava(timestamp, ethBlock, {kava: block}) {
+  const chain = "kava"
+  const pooledCoin = await sdk.api.abi.call({
+    block,
+    chain,
+    target: swkava,
+    abi: "uint256:totalSupply",
+  })
+  return {
+    'kava': Number(pooledCoin.output)/1e18,
+  }
+}
+
+async function echelon(timestamp, ethBlock, {echelon: block}) {
+  const chain = "echelon"
+  const pooledCoin = await sdk.api.abi.call({
+    block,
+    chain,
+    target: swech,
+    abi: "uint256:totalSupply",
+  })
+  return {
+    'echelon': Number(pooledCoin.output)/1e18,
+  }
+}
+
+
+
 module.exports = {
   kava: {
-    tvl: async (_, block) => ({
-      ["kava"]: (await sdk.api.erc20.totalSupply({ target: swkava, block})).output
-    })
-  },
-  echelon: {
-    tvl: async (_, block) => ({
-      ["echelon"]: (await sdk.api.erc20.totalSupply({ target: swech, block})).output
-    })
+    tvl: kava
   }
 }
