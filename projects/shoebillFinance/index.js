39140dcdd427e5f66e84707bdbbe37e49c4e0de9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-27 08:50:38 +0530

    code refacor

--- projects/shoebillFinance/index.js
+++ projects/shoebillFinance/index.js
@@ -1,61 +1,39 @@
-const { ethers } = require("ethers/lib");
 const { toUSDTBalances } = require("../helper/balances");
-const { protocolDataProviderAbi } = require("./abi");
 
-const shoebillDataProviderAddress =
-  "0xBdc26Ba6a0ebFD83c76CEf76E8F9eeb7714A5884";
+const shoebillDataProviderAddress = "0xBdc26Ba6a0ebFD83c76CEf76E8F9eeb7714A5884";
+const protocolDataProviderAbi = "function getAllAggregatedReservesData() view returns (tuple(tuple(string internalSymbol, address internalAddress, string externalSymbol, address externalAddress, address aTokenAddress, address stableDebtTokenAddress, address variableDebtTokenAddress) token, tuple(uint256 ltv, uint256 liquidationThreshold, uint256 liquidationBonus, uint256 reserveFactor, bool usageAsCollateralEnabled, bool borrowingEnabled, bool stableBorrowRateEnabled, bool isActive, bool isFrozen, uint256 decimals) configuration, tuple(uint256 availableLiquidity, uint256 totalStableDebt, uint256 totalVariableDebt, uint256 liquidityRate, uint256 variableBorrowRate, uint256 stableBorrowRate, uint256 averageStableBorrowRate, uint256 liquidityIndex, uint256 variableBorrowIndex, uint40 lastUpdateTimestamp, bool isCollateral, address yieldAddress) overview, uint256 oraclePrice)[])"
 
-const ethersProvider = new ethers.providers.JsonRpcProvider(
-  "https://klaytn.blockpi.network/v1/rpc/public"
-);
+async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
+  const aggregatedData = await api.call({ target: shoebillDataProviderAddress, abi: protocolDataProviderAbi })
 
-const dataProvider = new ethers.Contract(
-  shoebillDataProviderAddress,
-  protocolDataProviderAbi,
-  ethersProvider
-);
-
-async function getTvl(timestamp, ethBlock, chainBlocks, { api }) {
-  const aggregatedData = await dataProvider.getAllAggregatedReservesData();
-
-  const data = aggregatedData.map((e) => {
+  const data = aggregatedData.filter(i => i.overview).map((e) => {
     return {
       address: e.token.externalAddress,
-      tvl: e.overview.availableLiquidity
-        .add(e.overview.totalVariableDebt)
-        .mul(e.oraclePrice)
-        .toString(),
-      decimal: e.configuration.decimals,
-      borrowed: e.overview.totalVariableDebt.mul(e.oraclePrice),
+      tvl: e.overview.availableLiquidity * e.oraclePrice / (10 ** e.configuration.decimals),
     };
   });
-  const totalSupplyTvl =
-    data.reduce((a, b) => a + b.tvl / 10 ** b.decimal, 0) / 1e8;
-  const borrowed =
-    data.reduce((a, b) => a + b.borrowed / 10 ** b.decimal, 0) / 1e8;
+  const totalSupplyTvl = data.reduce((a, b) => a + +b.tvl, 0) / 1e8;
 
   return toUSDTBalances(totalSupplyTvl);
 }
-async function getBorrowed(timestamp, ethBlock, chainBlocks, { api }) {
-  const aggregatedData = await dataProvider.getAllAggregatedReservesData();
+async function borrowed(timestamp, ethBlock, chainBlocks, { api }) {
+  const aggregatedData = await api.call({ target: shoebillDataProviderAddress, abi: protocolDataProviderAbi })
 
   const data = aggregatedData.map((e) => {
     return {
-      decimal: e.configuration.decimals,
-      borrowed: e.overview.totalVariableDebt.mul(e.oraclePrice),
+      borrowed: e.overview.totalVariableDebt * e.oraclePrice / (10 ** e.configuration.decimals),
     };
   });
-  const borrowed =
-    data.reduce((a, b) => a + b.borrowed / 10 ** b.decimal, 0) / 1e8;
+  const borrowed = data.reduce((a, b) => a + b.borrowed, 0) / 1e8;
 
   return toUSDTBalances(borrowed);
 }
 module.exports = {
-  timetravel: true,
+  misrepresentedTokens: true,
   methodology:
     "Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There's multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending",
   klaytn: {
-    tvl: getTvl,
-    borrowed: getBorrowed,
+    tvl,
+    borrowed,
   },
 };
