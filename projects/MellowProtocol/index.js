e14b5b0d22d972f96ef15c29e9a39b5a27e58c4a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-02-28 11:29:22 +0530

    mellow: use get logs to fetch vault addresses

--- projects/MellowProtocol/index.js
+++ projects/MellowProtocol/index.js
@@ -1,74 +1,60 @@
-const sdk = require('@defillama/sdk');
-const abi = require("./abi.json");
+const { getLogs } = require('../helper/cache/getLogs')
+const sdk = require('@defillama/sdk')
 
-const ADDRESSES = ['0xB17a8d440c4e0A206Fc1dE76F3D0531F70bF6d42', '0x336ce0084D1aCF7c32578924b13F7abCed47Ac3e', '0x5e6f99876b709E0C8384dd2A7F9F12771E00d240', '0x3AF84EC55b9A696C650C2a7Cab7D2555dbf45892', '0xedF3cb10dE6Ad6449B9F9Ee561f18e50b6B79234', '0x87065A6e0e6609583976404fd33F670FaA310390', '0x6F0e45AA2B7D936F88166F92Be82AB162788Ed9b', '0x02B9f3AfB742Ce533f6d3cc8900b588674C8B795', '0x0c43098E8aF3D3d27555c3FD4cCf62638d269C68', '0xfd89E1274D96884381601D533e8d051bCf20fC71', '0x6D3F5363bB30BB5CAE7F30c74689Ee6a7154350E', '0xC99c70492Bc15c056813d1ddA95C89Bb285Cdc86', '0x6A2Dd3B817F0364e7603e781dDA9c62f62c440E1', '0x78ba57594656400d74a0c5ea80f84750cb47f449', '0xA33a068645E228Db11c42e9d187EDC72361B7BC0', '0x34c277B0c690936168cF436B904B2242a11E7eeA', '0xD3442BA55108d33FA1EB3F1a3C0876F892B01c44', '0xa2607696699dbF3c4de584e244f4E3df58cdf69c', '0x13c7bCc2126d6892eEFd489Ad215A1a09F36AA9f', '0x1FCD3926b6DFa2A90Fe49A383C732b31f1ee54eB'];
-const ADDRESSES_POLYGON = ['0x87eF296A29652245Ca13EcE7dF11C17e5a5559df', '0xF44FeC6b2d0dF38106490e34af6dB1Ea2FB42334', '0xDC188Fa71F077Ac39cE5383A73Cb2ea1687409e8', '0x4bAc81e668ac82A0382b987A27bbc5E65c70A64c', '0x37E336D573EC3b05eA55fF80dCD29D619a2A9795', '0xCb555775D73C187bdC8C3082dCac5794Bf0d22BB', '0xEDDA71479f5E889b85bF48BE0eF38255481da4b1', '0x0F07904D2EBcEa8a416174427380406B7251F701'];
-
-async function tvl(_, _1, _2, { api }) {
-  const balances = {};
-
-  for (let i = 0; i < ADDRESSES.length; ++i) {
-
-    const balancesVault = await api.call({
-      abi: abi.tvl,
-      target: ADDRESSES[i],
-      params: [],
-    });
-
-    const tokens = await api.call({
-      abi: abi.vaultTokens,
-      target: ADDRESSES[i],
-      params: [],
-    }); 
-
-    for (let i = 0; i < tokens.length; ++i) {
-      if (!(tokens[i] in balances)) {
-          balances[tokens[i]] = 0;
-      }
-      balances[tokens[i]] += Number(balancesVault[1][i]);
-    }
-
+const config = {
+  ethereum: {
+    registry: '0xfd23f971696576331fcf96f80a20b4d3b31ca5b2',
+    fromBlock: 15237714,
+  },
+  polygon: {
+    registry: '0xd3d0e85f225348a2006270daf624d8c46cae4e1f',
+    fromBlock: 31243728,
   }
-
-  return balances;
-}
-
-async function tvlPolygon(_, _1, _2, { api }) {
-  const balances = {};
-
-  for (let i = 0; i < ADDRESSES_POLYGON.length; ++i) {
-
-    const balancesVault = await api.call({
-      abi: abi.tvl,
-      target: ADDRESSES_POLYGON[i],
-      params: [],
-    });
-
-    const tokens = await api.call({
-      abi: abi.vaultTokens,
-      target: ADDRESSES_POLYGON[i],
-      params: [],
-    }); 
-
-    for (let i = 0; i < tokens.length; ++i) {
-      const line = "polygon:" + tokens[i];
-      if (!(line in balances)) {
-          balances[line] = 0;
-      }
-      balances[line] += Number(balancesVault[1][i]);
-    }
-
-  }
-
-  return balances;
 }
 
 module.exports = {
-  methodology: 'counts the tvl over all Mellow Procotol Root Vaults calling their corresponding tvl functions',
-  ethereum: {
-    tvl,
-  },
-  polygon: {
-    tvl: tvlPolygon,
+  doublecounted: true,
+};
+
+const ignored = [
+  '0x728fD7ec3cBB50D809735879318d04D8CbEb7157'
+]
+
+Object.keys(config).forEach(chain => {
+  const { registry, fromBlock, } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const balances = {}
+      const logs = await getLogs({
+        api,
+        target: registry,
+        topic: 'VaultRegistered(address,address,uint256,address,address)',
+        fromBlock,
+        eventAbi: 'event VaultRegistered (address indexed origin, address indexed sender, uint256 indexed nft, address vault, address owner)',
+        onlyArgs: true,
+        extraKey: 'VaultRegistered'
+      })
+      const vaultKeys = {}
+      logs.forEach(i => vaultKeys[i.nft] = i.vault)
+      const tokenLockedLogs = await getLogs({
+        api,
+        target: registry,
+        topic: 'TokenLocked(address,address,uint256)',
+        fromBlock,
+        eventAbi: 'event TokenLocked (address indexed origin, address indexed sender, uint256 indexed nft)',
+        onlyArgs: true,
+        extraKey: 'TokenLocked'
+      })
+      tokenLockedLogs.forEach(i => delete vaultKeys[i.nft])
+      let vaults = Object.values(vaultKeys).filter(i => !ignored.includes(i))
+      const tokens = await api.multiCall({ abi: 'function vaultTokens() view returns (address[])', calls: vaults })
+      const bals = await api.multiCall({ abi: 'function tvl() view returns (uint256[] minTokenAmounts, uint256[] maxTokenAmounts)', calls: vaults })
+      tokens.forEach((tokens, i) => {
+        if (!bals[i]) return;
+        let balsInner = bals[i].minTokenAmounts
+        tokens.forEach((v, i) => sdk.util.sumSingleBalance(balances, v, balsInner[i], api.chain))
+      })
+      return balances
+    }
   }
-}; 
+})
