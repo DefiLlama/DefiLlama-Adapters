96f9fd2d90a2fa5b8ba48a5d3f6fa1cc89832b19
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-02 13:34:25 +0100

    mnor fix

--- projects/mare-finance/index.js
+++ projects/mare-finance/index.js
@@ -1,10 +1,12 @@
 const { compoundExports } = require("../helper/compound");
+const { staking } = require("../helper/staking");
 
-const maWKAVA = "0x24149e2D0D3F79EBb7Fc464b09e3628dE395b39D";
 const unitroller = "0x4804357AcE69330524ceb18F2A647c3c162E1F95";
-const wkava = "0xc86c7C0eFbd6A49B35E8714C5f59D99De09A225b";
 
 module.exports = {
-    methodology: "Same as Compound Finance, we just count all the tokens supplied (not borrowed money) on the lending markets",
-    kava: compoundExports(unitroller, "optimism", maWKAVA, wkava),
-};
+  methodology: "Same as Compound Finance, we just count all the tokens supplied (not borrowed money) on the lending markets",
+  kava: {
+    ...compoundExports(unitroller, "kava"),
+    staking: staking(['0x2c4A1f47c3E15F468399A87c4B41ec0d19297772', '0x194AAd54F363D28aDEaE53A7957d63B9BCf8a6b2'], '0xd86C8d4279CCaFbec840c782BcC50D201f277419')
+  },
+}
