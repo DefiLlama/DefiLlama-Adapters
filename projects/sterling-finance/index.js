2234ef217820cdc73905e9b60f13000c9227fbb0
Avedith <avedith@avedith.dev>
2023-03-02 22:29:26 +1000

    remove unused code

--- projects/sterling-finance/index.js
+++ projects/sterling-finance/index.js
@@ -1,18 +1,4 @@
 const {uniTvlExport} = require('../helper/calculateUniTvl.js')
-const { sumTokens2 } = require('../helper/unwrapLPs');
-
-async function arbitrumTVL(timestamp, block, chainBlocks) {
-    let balances = {};
-
-    await sumTokens2({
-        balances,
-        owners: ["0x3Bb9372989c81d56db64e8aaD38401E677b91244"],
-        tokens: ["0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1"],
-        chain: 'arbitrum',
-        block: chainBlocks.arbitrum
-    })
-}
-
 
 module.exports = {
   arbitrum:{
