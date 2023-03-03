c854dc445eaa3d5a8d219cfc958b31903865c600
Avedith <avedith@avedith.dev>
2023-03-03 20:50:19 +1000

    update bullionFX adapter

--- projects/bullionFX/index.js
+++ projects/bullionFX/index.js
@@ -1,3 +1,11 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
+const { getUniTVL } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('ethereum', '0x5E7CfE3DB397d3DF3F516d79a072F4C2ae5f39bb')
+module.exports = {
+    ethereum: {
+        tvl: getUniTVL({
+            factory: '0x5E7CfE3DB397d3DF3F516d79a072F4C2ae5f39bb',
+            fetchBalances: true,
+            useDefaultCoreAssets: true,
+        })
+    }
+}
