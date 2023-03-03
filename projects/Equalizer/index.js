23a8889ca3ca6f1054bb88b2033377d15351bf27
Avedith <avedith@avedith.dev>
2023-03-03 21:17:32 +1000

    update Equalizer adapter

--- projects/Equalizer/index.js
+++ projects/Equalizer/index.js
@@ -1,9 +1,13 @@
-const {uniTvlExport} = require('../helper/calculateUniTvl.js')
 const { staking } = require('../helper/staking')
+const { getUniTVL } = require("../helper/unknownTokens");
 
 module.exports = {
-  fantom:{
-    tvl: uniTvlExport("0xc6366EFD0AF1d09171fe0EBF32c7943BB310832a", "fantom"),
+  fantom: {
+    tvl: getUniTVL({
+      factory: '0xc6366EFD0AF1d09171fe0EBF32c7943BB310832a',
+      fetchBalances: true,
+      useDefaultCoreAssets: true,
+    }),
     staking: staking("0x8313f3551C4D3984FfbaDFb42f780D0c8763Ce94", "0x3Fd3A0c85B70754eFc07aC9Ac0cbBDCe664865A6","fantom"),
-  },
+  }
 }
