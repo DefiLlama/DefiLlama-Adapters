a4ed28610ad3cd55c7c91cfd88f1a47d6d5153cd
Avedith <avedith@avedith.dev>
2023-03-03 20:11:21 +1000

    update hswap adapter

--- projects/hswap.js
+++ projects/hswap.js
@@ -1,7 +1,20 @@
-const { uniTvlExport } = require("./helper/calculateUniTvl")
+// const { uniTvlExport } = require("./helper/calculateUniTvl")
+
+// module.exports = {
+//     heco: {
+//         tvl: uniTvlExport('0x13D1EA691C8153F5bc9a491d41b927E2baF8A6b1', "heco")
+//     }
+// }
+
+const {getUniTVL} = require('./helper/unknownTokens')
 
 module.exports = {
-    heco: {
-        tvl: uniTvlExport('0x13D1EA691C8153F5bc9a491d41b927E2baF8A6b1', "heco")
-    }
+  misrepresentedTokens: true,
+  heco:{
+    tvl: getUniTVL({
+      factory: '0x13D1EA691C8153F5bc9a491d41b927E2baF8A6b1',
+      fetchBalances: true,
+      useDefaultCoreAssets: true,
+    })
+  },
 }
