32f46455e9bb50104a16a5315a25e60bfbb92336
Avedith <avedith@avedith.dev>
2023-03-03 21:31:21 +1000

    update hiveswap adapter

--- projects/hiveswap/index.js
+++ projects/hiveswap/index.js
@@ -1,3 +1,11 @@
-const { uniTvlExport } = require("../helper/unknownTokens")
+const { getUniTVL } = require("../helper/unknownTokens");
 
-module.exports = uniTvlExport('map', '0x29c3d087302e3fCb75F16175A09E4C39119459B2')
+module.exports = {
+    map: {
+        tvl: getUniTVL({
+            factory: '0x29c3d087302e3fCb75F16175A09E4C39119459B2',
+            fetchBalances: true,
+            useDefaultCoreAssets: true,
+        }),
+    }
+}
