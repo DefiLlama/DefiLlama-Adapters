3659e7d9278cc84a8e65b92b2075a2f9c7a4e926
Avedith <avedith@avedith.dev>
2023-03-03 21:54:16 +1000

    update levinswap adapter

--- projects/levinswap/index.js
+++ projects/levinswap/index.js
@@ -1,13 +1,16 @@
-const { uniTvlExport } = require("../helper/calculateUniTvl");
+const { getUniTVL } = require("../helper/unknownTokens");
 const { staking } = require("../helper/staking");
 
-const factory = "0x965769C9CeA8A7667246058504dcdcDb1E2975A5";
 const levin = "0x1698cD22278ef6E7c0DF45a8dEA72EDbeA9E42aa";
 const xlevin = "0xafa57Fb9d8D63Ff8124E17c1495C73bc3a7678D0";
 
 module.exports = {
   xdai: {
-    tvl: uniTvlExport(factory, 'xdai'),
+    tvl: getUniTVL({
+      factory: '0x965769C9CeA8A7667246058504dcdcDb1E2975A5',
+      fetchBalances: true,
+      useDefaultCoreAssets: true,
+    }),
     staking: staking(xlevin, levin, "xdai")
   }
 }
