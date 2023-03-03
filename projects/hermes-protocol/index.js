8e42f651589d3e8afd6997aaf0247412dc15604c
Avedith <avedith@avedith.dev>
2023-03-03 21:30:05 +1000

    update hermes-protocol adapter

--- projects/hermes-protocol/index.js
+++ projects/hermes-protocol/index.js
@@ -1,9 +1,13 @@
-const {uniTvlExport} = require('../helper/calculateUniTvl.js')
+const { getUniTVL } = require("../helper/unknownTokens");
 const { staking } = require('../helper/staking')
 
 module.exports = {
   metis:{
-    tvl: uniTvlExport("0x633a093C9e94f64500FC8fCBB48e90dd52F6668F", "metis"),
+    tvl: getUniTVL({
+      factory: '0x633a093C9e94f64500FC8fCBB48e90dd52F6668F',
+      fetchBalances: true,
+      useDefaultCoreAssets: true,
+    }),
     staking: staking("0xa4C546c8F3ca15aa537D2ac3f62EE808d915B65b", "0xb27bbeaaca2c00d6258c3118bab6b5b6975161c8","metis"),
   },
 }
