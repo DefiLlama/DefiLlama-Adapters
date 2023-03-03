57ce62f826d9baf1c0d8977a23dee7d8b89fde4f
Avedith <avedith@avedith.dev>
2023-03-03 21:05:34 +1000

    update cobraswap adpater

--- projects/cobraswap/index.js
+++ projects/cobraswap/index.js
@@ -1,5 +1,11 @@
-const { uniTvlExport } = require("../helper/unknownTokens");
+const { getUniTVL } = require("../helper/unknownTokens");
 
-const factory = "0x3165d94dd2f71381495cb897832de02710a0dce5";
-
-module.exports = uniTvlExport('bsc', factory)
+module.exports = {
+    bsc: {
+        tvl: getUniTVL({
+            factory: '0x3165d94dd2f71381495cb897832de02710a0dce5',
+            fetchBalances: true,
+            useDefaultCoreAssets: true,
+        }),
+    }
+}
