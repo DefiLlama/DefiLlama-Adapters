e1e558cdecee77097ca95e1a4c72dce3f1a09efb
Avedith <avedith@avedith.dev>
2023-03-03 21:14:26 +1000

    update dinosaureggs adapter

--- projects/dinosaureggs/index.js
+++ projects/dinosaureggs/index.js
@@ -1,7 +1,11 @@
-const {uniTvlExport} = require("../helper/calculateUniTvl");
-
-const factory = "0x73d9f93d53505cb8c4c7f952ae42450d9e859d10";
+const { getUniTVL } = require("../helper/unknownTokens");
 
 module.exports = {
-  bsc: { tvl: uniTvlExport(factory, 'bsc') }
-};
+    bsc: {
+        tvl: getUniTVL({
+            factory: '0x73d9f93d53505cb8c4c7f952ae42450d9e859d10',
+            fetchBalances: true,
+            useDefaultCoreAssets: true,
+        }),
+    }
+}
