c68574b27042ebf288d0544381050e769c2618ce
Avedith <avedith@avedith.dev>
2023-03-03 20:14:58 +1000

    remove unused code

--- projects/hswap.js
+++ projects/hswap.js
@@ -1,11 +1,3 @@
-// const { uniTvlExport } = require("./helper/calculateUniTvl")
-
-// module.exports = {
-//     heco: {
-//         tvl: uniTvlExport('0x13D1EA691C8153F5bc9a491d41b927E2baF8A6b1', "heco")
-//     }
-// }
-
 const {getUniTVL} = require('./helper/unknownTokens')
 
 module.exports = {
