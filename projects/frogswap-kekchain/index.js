d1d940e535a40e80ecddd384dde1bcbc8f6fb08c
Avedith <avedith@avedith.dev>
2023-03-03 21:21:55 +1000

    update frogswap-kekchain adapter

--- projects/frogswap-kekchain/index.js
+++ projects/frogswap-kekchain/index.js
@@ -1,3 +1,11 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
+const { getUniTVL } = require("../helper/unknownTokens");
 
-module.exports = uniTvlExport('kekchain', '0xfe0139503a1B97F7f6c2b72f4020df7A6c1EE399')
+module.exports = {
+    kekchain: {
+        tvl: getUniTVL({
+            factory: '0xfe0139503a1B97F7f6c2b72f4020df7A6c1EE399',
+            fetchBalances: true,
+            useDefaultCoreAssets: true,
+        }),
+    }
+}
