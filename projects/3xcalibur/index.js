db71e8b3a6cbad54938f1882f08423cfebb73bc4
Avedith <avedith@avedith.dev>
2023-03-03 20:22:07 +1000

    update 3xcalibur adapter

--- projects/3xcalibur/index.js
+++ projects/3xcalibur/index.js
@@ -1,11 +1,16 @@
-const { uniTvlExport } = require("../helper/calculateUniTvl.js");
+const {getUniTVL} = require('../helper/unknownTokens')
+
 
 module.exports = {
   doublecounted: false,
   timetravel: true,
   start: 1667689200,
   arbitrum: {
-    tvl: uniTvlExport("0xD158bd9E8b6efd3ca76830B66715Aa2b7Bad2218", "arbitrum"),
+    tvl: getUniTVL({
+      factory: "0xD158bd9E8b6efd3ca76830B66715Aa2b7Bad2218",
+      fetchBalances: true,
+      useDefaultCoreAssets: true,
+    }),
   },
   hallmarks: [[1668038400, "Emissions started"]],
 };
