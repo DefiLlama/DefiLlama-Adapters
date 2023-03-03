759364617b4ab6f0565239d36bbb30e1eb677d83
Avedith <avedith@avedith.dev>
2023-03-03 21:48:06 +1000

    update latte adapter

--- projects/latte/index.js
+++ projects/latte/index.js
@@ -1,8 +1,7 @@
 
 const sdk = require("@defillama/sdk");
-const { uniTvlExport } = require("../helper/calculateUniTvl");
+const { getUniTVL } = require("../helper/unknownTokens");
 
-const factory = "0x4DcE5Bdb81B8D5EdB66cA1b8b2616A8E0Dd5f807";
 const latteToken = "0x8D78C2ff1fB4FBA08c7691Dfeac7bB425a91c81A";
 const lattev2Token = "0xa269A9942086f5F87930499dC8317ccC9dF2b6CB";
 const masterchef = "0xbCeE0d15a4402C9Cc894D52cc5E9982F60C463d6";
@@ -34,7 +33,11 @@ async function staking(timestamp, block, chainBlocks) {
 
 module.exports = {
   bsc: {
-    tvl: uniTvlExport(factory, 'bsc'),
+    tvl: getUniTVL({
+      factory: '0x4DcE5Bdb81B8D5EdB66cA1b8b2616A8E0Dd5f807',
+      fetchBalances: true,
+      useDefaultCoreAssets: true,
+    }),
     staking,
   },
 };
