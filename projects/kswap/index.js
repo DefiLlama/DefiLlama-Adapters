3b4c5d17d2781030635757cd111ea3b68c07af3f
Avedith <avedith@avedith.dev>
2023-03-03 21:46:18 +1000

    update kswap adapter

--- projects/kswap/index.js
+++ projects/kswap/index.js
@@ -1,4 +1,11 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
-const factory = '0xEFD3ad14E5cF09b0EbE435756337fb2e9D10Dc1a' // v2 factory address
+const { getUniTVL } = require("../helper/unknownTokens");
 
-module.exports = uniTvlExport('kava', factory)
+module.exports = {
+    kava: {
+        tvl: getUniTVL({
+            factory: '0xEFD3ad14E5cF09b0EbE435756337fb2e9D10Dc1a',
+            fetchBalances: true,
+            useDefaultCoreAssets: true,
+        }),
+    }
+}
