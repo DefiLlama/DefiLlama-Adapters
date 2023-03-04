00eabd81c2c3a053794baac46e1da89a3a13df49
Elliot Shiu <sandbochs@users.noreply.github.com>
2023-03-03 16:51:55 -0800

    count vaulted nfts in tvl for arcade.xyz

---
+++ projects/arcade-xyz/constants.js
@@ -0,0 +1,24 @@
+const CHAIN = 'ethereum';
+const LOAN_CORE = '0x81b2f8fc75bab64a6b144aa6d2faa127b4fa7fd9';
+const VAULT_FACTORY_A = '0x6e9b4c2f6bd57b7b924d29b5dcfca1273ecc94a2';
+const VAULT_FACTORY_APE_STAKING_A = '0x666faa632e5f7ba20a7fce36596a6736f87133be';
+
+const VAULT_FACTORIES = new Set();
+const vf = VAULT_FACTORIES;
+vf.add(VAULT_FACTORY_A);
+vf.add(VAULT_FACTORY_APE_STAKING_A);
+
+const START_BLOCKS = new Map();
+const sb = START_BLOCKS;
+sb.set(VAULT_FACTORY_A, 15045028);
+sb.set(VAULT_FACTORY_APE_STAKING_A, 16128500);
+
+module.exports = {
+  CHAIN,
+  LOAN_CORE,
+  START_BLOCKS,
+  VAULT_FACTORY_A,
+  VAULT_FACTORY_APE_STAKING_A,
+  VAULT_FACTORIES,
+}
+
