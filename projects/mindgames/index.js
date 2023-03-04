658b489074fbfc8c2c629d4a3bbe3f3f335b2243
Hugh <109151284+thatborghugh@users.noreply.github.com>
2023-03-04 03:23:10 +0200

    mindgames arbitrum relaunch
    the project uses its own WETH9 contract so it had to be added as a side tvl input

--- projects/mindgames/index.js
+++ projects/mindgames/index.js
@@ -1,26 +1,33 @@
 const { getUniTVL, staking } = require('../helper/unknownTokens')
+const sdk = require('@defillama/sdk')
+const uniswapAbi = require('../helper/abis/uniswap')
 
-const xCELL = "0xbe334d7208366B3E4Fb40348576227b524d8CBA0";
-const CELL = "0xa685F488DEe49b75469E9e866965daBc8Ed6083d";
-const factory = "0x78f406B41C81eb4144C321ADa5902BBF5de28538";
-const coreAssets = [
-  "0xC42C30aC6Cc15faC9bD938618BcaA1a1FaE8501d",
-]
+const WETH_MG = "0xd75528908c0981b92758aD6cE7d18C0Dd163f8ae";
+const xCRX = "0x35AfE95662fdf442762a11E4eD5172C81fBceF7e";
+const CRX = "0xb21Be1Caf592A5DC1e75e418704d1B6d50B0d083";
+const factory = "0x7C7F1c8E2b38d4C06218565BC4C9D8231b0628c0";
 
-const tvl = getUniTVL({ factory, chain: 'aurora', coreAssets })
+async function tvlETH(_, _b, { arbitrum: block }) {
+  const reserves = await sdk.api2.abi.call({ abi: uniswapAbi.getReserves, target: WETH_MG, chain: 'arbitrum', block})
+  let balance = reserves[0] / 10 ** 18
+  return {['WETH'] : balance};
+}
+
+const tvl = getUniTVL({ factory, chain: 'arbitrum', useDefaultCoreAssets: false, fetchBalances: true })
 
 module.exports = {
-  misrepresentedTokens: true,
+  doublecounted: false,
   methodology:
-    "Factory address (0x78f406B41C81eb4144C321ADa5902BBF5de28538) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
-  aurora: {
-    tvl,
-    staking: staking({
-      owner: xCELL,
-      tokens: [CELL],
-      chain: 'aurora',
-      coreAssets,
-      lps: ['0xcA5eD5745890C66026400F9b805466fB3984A345']
-    })
+    "Factory address (0x7C7F1c8E2b38d4C06218565BC4C9D8231b0628c0) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
+  arbitrum: {
+    tvl : sdk.util.sumChainTvls([tvlETH, tvl]),
+    staking: staking(
+      {
+        owner: xCRX,
+        tokens: [CRX],
+        chain: 'arbitrum',
+        coreAssets: ['0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8'],
+        lps: ['0xf7305D209BFeCF40Bd53ccBdbe5303B3153d0660']
+      })
   },
 };
