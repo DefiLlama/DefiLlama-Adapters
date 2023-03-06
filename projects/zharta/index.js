8b04227a7041686d9925e1178dfc55dca4478050
codingsh <codingsh@pm.me>
2023-03-06 17:18:53 +0000

    chore(zharta): update logic

--- projects/zharta/index.js
+++ projects/zharta/index.js
@@ -1,12 +1,10 @@
 const { default: axios } = require("axios");
 const { getWhitelistedNFTs, tokensBare} = require('../helper/tokenMapping');
-const { sumTokensExport } = require('../helper/unwrapLPs');
+const { sumTokensExport, } = require('../helper/unwrapLPs')
+
 
 // https://api.zharta.io/collaterals/v2/doc#/collaterals/get_collateral_from_pool_collaterals_v2_lending_pool__token_symbol__get
 // WETH
-let totalAppraisal = 0;
-fetchTotalAppraisal();
-
 async function fetchCollateralData() {
   try {
     const response = await axios.get("https://api.zharta.io/collaterals/v2/lending_pool/WETH");
@@ -29,6 +27,10 @@ async function fetchTotalAppraisal() {
   totalAppraisal = getTotalAppraisal(collaterals);
 }
 
+let totalAppraisal = 0;
+fetchTotalAppraisal();
+
+
 // Vaults
 const collateralVault = "0xA79da8c90Aa480B3716C23145154CA6eF5Fc29C1";
 const punkVault = "0x94925030F48aDfc3e4A65a2E0A7444733406c144";
@@ -37,13 +39,11 @@ const LP_CORE = "0xe3c959bc97b92973d5367dbf4ce1b7b9660ee271";
 module.exports = {
   methodology: `Counts the floor value of all deposited NFTs with Chainlink price feeds. Borrowed coins are not counted towards the TVL`,
   ethereum: {
-    tvl: sumTokensExport({
-      ownerTokens: [
-        [getWhitelistedNFTs(), collateralVault], 
-        [getWhitelistedNFTs(), punkVault], 
-        [[tokensBare.weth], LP_CORE],
-        [[tokensBare.weth], totalAppraisal],
-      ],
-    }),
-  },
-};
+    tvl: sumTokensExport({ ownerTokens: [
+      [getWhitelistedNFTs(), collateralVault], 
+      [getWhitelistedNFTs(), punkVault], 
+      [[tokensBare.weth], LP_CORE],
+      [[tokensBare.weth], totalAppraisal],
+    ]}),
+  }
+}
