679f426a415f1ca9cb03b58bd808786a7e6a0e3e
Elliot Shiu <sandbochs@users.noreply.github.com>
2023-03-06 12:35:31 -0800

    add borrowed value to arcade xyz

--- projects/arcade-xyz/queries.js
+++ projects/arcade-xyz/queries.js
@@ -1,6 +1,38 @@
 const { gql, request } = require('graphql-request');
 const { SUBGRAPH_URL } = require('./constants');
 
+// Max recursion depth
+const MAX_DEPTH = 100;
+
+// Fetch from the SG until there are no results
+async function fetchPaginated(query, key = '', limit = 100, cursor = 0, depth = 0) {
+  // Defensive so we don't give DeFiLlama engineers a headache
+  if (cursor < 0 || depth >= MAX_DEPTH) {
+    return [];
+  }
+
+  const first = limit ?? 100;
+  const start = cursor ?? 0;
+
+  let q  = query.replace('<FIRST>', first);
+  q = q.replace('<CURSOR>', start);
+
+  const res = await request(SUBGRAPH_URL, q);
+  const results = res[key] ?? [];
+
+  // There could be more results if the length matches the query limit
+  if (results .length >= first) {
+    // Get the cursor for the last vault
+    const lastCursor = results [first - 1].cursor ?? -1;
+
+    // Fetch more results starting from the last cursor + 1, inclusive
+    const moreResults = await fetchPaginated(query, key, limit, lastCursor + 1, depth + 1);
+    return results .concat(moreResults );
+  }
+
+  return results ;
+}
+
 // Fetches vaults that have tokens deposited and are available to be used on the platform.
 // Broken vaults (withdrawEnabled) cannot be used in a loan.
 const fetchVaultQuery = gql`
@@ -11,44 +43,27 @@ const fetchVaultQuery = gql`
   }
 `;
 
-// Helper function to fetch the first 1000 vaults
+// Helper function to fetch the first 1000 active
 async function fetchVaults() {
-  return fetchVaultsPaginated(1000);
+  return fetchPaginated(fetchVaultQuery, 'arcadeVaults', 1000);
 }
 
-// Max recursion depth
-const MAX_DEPTH = 100;
-
-// Continues to fetch vaults from the subgraph until there are no results
-async function fetchVaultsPaginated(limit = 100, cursor = 0, depth = 0) {
-  // Defensive so we don't give DeFiLlama engineers a headache
-  if (cursor < 0 || depth >= MAX_DEPTH) {
-    return [];
+const fetchLoansQuery = gql`
+  query {
+    arcadeLoans(first: <FIRST>, where :{ state: "Active", cursor_gte: <CURSOR> }) {
+      payableCurrency
+      principal
+    }
   }
+`
 
-  const first = limit ?? 100;
-  const start = cursor ?? 0;
-
-  let query = fetchVaultQuery.replace('<FIRST>', first);
-  query = query.replace('<CURSOR>', start);
-
-  const res = await request(SUBGRAPH_URL, query);
-  const vaults = res.arcadeVaults ?? [];
-
-  // There could be more vaults if the number of results matches the query limit
-  if (vaults.length >= first) {
-    // Get the cursor for the last vault
-    const lastCursor = vaults[first - 1].cursor ?? -1;
-
-    // Fetch more vaults starting from the last cursor + 1, inclusive
-    const moreVaults = await fetchVaultsPaginated(limit, lastCursor + 1, depth + 1);
-    return vaults.concat(moreVaults);
-  }
-
-  return vaults;
+// Helper function to fetch the first 1000 active loans
+async function fetchLoans() {
+  return fetchPaginated(fetchLoansQuery, 'arcadeLoans', 1000);
 }
 
 module.exports = {
+  fetchLoans,
+  fetchPaginated,
   fetchVaults,
-  fetchVaultsPaginated,
 }
