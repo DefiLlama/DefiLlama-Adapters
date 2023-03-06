4a62306d1326dc60a2f5fb3a5b108d4849a85a7d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-06 13:07:00 +0100

    fix anyhedge

--- projects/helper/solana.js
+++ projects/helper/solana.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const http = require('./http')
+const cache = require('./cache')
 const env = require('./env')
 const { transformBalances: transformBalancesOrig, transformDexBalances, } = require('./portedTokens.js')
 const { tokens, getUniqueAddresses } = require('./tokenMapping')
@@ -69,13 +70,6 @@ async function getGeckoSolTokens() {
   const tokenSet = new Set()
   tokens.filter(i => i.extensions?.coingeckoId).forEach(i => tokenSet.add(i.address))
   return tokenSet
-}
-
-async function getSolTokenMap() {
-  const tokenList = await getTokenList()
-  let map = {}
-  tokenList.forEach(i => map[i.address] = i)
-  return map
 }
 
 async function getTokenDecimals(tokens) {
@@ -304,10 +298,10 @@ function exportDexTVL(DEX_PROGRAM_ID, getTokenAccounts) {
 }
 
 async function getSaberPools() {
-  return http.get('https://registry.saber.so/data/llama.mainnet.json')
+  return cache.getConfig('solana/saber-pools', 'https://registry.saber.so/data/llama.mainnet.json')
 }
 async function getQuarryData() {
-  return http.get('https://raw.githubusercontent.com/QuarryProtocol/rewarder-list-build/master/mainnet-beta/tvl.json')
+  return cache.getConfig('solana/quarry', 'https://raw.githubusercontent.com/QuarryProtocol/rewarder-list-build/master/mainnet-beta/tvl.json')
 }
 
 async function sumTokens2({
@@ -350,6 +344,8 @@ async function sumTokens2({
     const solBalance = await getSolBalances(solOwners)
     sdk.util.sumSingleBalance(balances, tokenMapping.solana, solBalance)
   }
+
+  blacklistedTokens.forEach(i => delete balances['solana:'+i])
 
   return balances
 
@@ -402,7 +398,6 @@ module.exports = {
   getGeckoSolTokens,
   getTokenAccountBalances,
   getTokenList,
-  getSolTokenMap,
   readBigUInt64LE,
   decodeAccount,
 };
