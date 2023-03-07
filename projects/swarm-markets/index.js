0eeb4d8ab6b839d398c3033be47a48fb440480dc
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-07 09:45:53 +0100

    update balancer forks

--- projects/swarm-markets/index.js
+++ projects/swarm-markets/index.js
@@ -1,55 +1,20 @@
-const sdk = require("@defillama/sdk");
-const { request, gql } = require("graphql-request");
+const { v1Tvl } = require("../helper/balancer")
+const sdk = require('@defillama/sdk')
 
-const SUBGRAPH_URL =
-    "https://api.thegraph.com/subgraphs/name/protofire/swarm-markets";
-const XTokenWrapperContractAddress =
-    "0x2b9dc65253c035eb21778cb3898eab5a0ada0cce";
-
-async function getTokens() {
-    let tokens = [];
-
-    const tokensQuery = gql`
-      query Tokens {
-        tokens(where: { symbol_not: "SPT" }) {
-          id
-        }
-      }
-    `;
-    const tokensResponse = await request(SUBGRAPH_URL, tokensQuery);
-    tokens = tokensResponse.tokens.map((token) => token.id);
-
-    return tokens;
-}
-
-async function getBalances(tokens, block) {
-    const balanceCalls = tokens.map((token) => ({
-        target: token,
-        params: XTokenWrapperContractAddress,
-    }));
-
-    const balanceResponses = (
-        await sdk.api.abi.multiCall({
-            block,
-            calls: balanceCalls,
-            abi: "erc20:balanceOf",
-        })
-    );
-
-    const balances = {};
-    sdk.util.sumMultiBalanceOf(balances, balanceResponses, true);
-    return balances;
-}
-
-async function tvl(timestamp, block) {
-    const tokens = await getTokens();
-    const balances = await getBalances(tokens, block);
-
-    return balances;
+async function tvl(_, _b, _cb, { api, }) {
+  const tvlFunc = v1Tvl('0x991fd11876a438af7bcadc98f4b3cf201bdf6191', 23158523)
+  const balances_ = await tvlFunc(...arguments)
+  const tokens = Object.keys(balances_)
+  const uTokens = await api.multiCall({ abi: 'function xTokenToToken(address) view returns (address)', target: '0x6cdde4ed9165405c1914b229d3cd4ac9c354c331', calls: tokens.map(i => i.replace('polygon:', '')) })
+  const balances = {}
+  tokens.forEach((v, i) => {
+    sdk.util.sumSingleBalance(balances, uTokens[i], balances_[v], api.chain)
+  })
+  return balances
 }
 
 module.exports = {
-    ethereum: {
-        tvl
-    }
+  polygon: {
+    tvl
+  }
 }
