6492c67bb6b885b325be071721261d522b14e973
realdealshaman <realdealshaman11@gmail.com>
2023-03-09 18:00:04 +0000

    add adapter for lfgswap-core

--- projects/lfgswap-core/index.js
+++ projects/lfgswap-core/index.js
@@ -2,10 +2,10 @@ const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
   misrepresentedTokens: true,
-  ethpow: {
+  core: {
     tvl: getUniTVL({
       useDefaultCoreAssets: true,
-      factory: '0xf66cef53c518659bFA0A9a4Aa07445AF08bf9B3a',
+      factory: '0xA1ADD165AED06D26fC1110b153ae17a5A5ae389e',
     })
   }
 }
