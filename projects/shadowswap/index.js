03cce52ff86115ea9d11ba8b6650eaf5a830e562
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-09 09:24:49 +0100

    add shadowswap.xyz

--- projects/shadowswap/index.js
+++ projects/shadowswap/index.js
@@ -1,13 +1,12 @@
 const { getUniTVL } = require('../helper/unknownTokens')
 
-
 module.exports = {
   misrepresentedTokens: true,
   core: {
     tvl: getUniTVL({
       chain: 'core',
       useDefaultCoreAssets: true,
-      factory: '0xA1ADD165AED06D26fC1110b153ae17a5A5ae389e',
+      factory: '0x326Ee96748E7DcC04BE1Ef8f4E4F6bdd54048932',
     })
   }
 }
