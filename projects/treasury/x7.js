f7ce456921ff91aedfa3c9f9e18059c218179f30
Adz1DoubleD <86771605+Adz1DoubleD@users.noreply.github.com>
2023-03-09 12:59:52 +0000

    added lending and rewards pools

--- projects/treasury/x7.js
+++ projects/treasury/x7.js
@@ -4,8 +4,8 @@ const ethTreasury = {
   'X7TreasurySplitterAddress': '0x70006b785aa87821331a974c3d5af81cde5bb999',
   'DeveloperMultiSigWalletAddress': '0x5cf4288bf373bbe17f76948e39baf33b9f6ac2e0',
   'CommmunityMultiSigWalletAddress': '0x7063E83dF5349833A21f744398fD39D42fbC00f8',
-  'MarketingWalletAddress': '0x5Bacb575b88888D08231b65B243419eDe49D1795',
-  'DeveloperWalletAddress': '0x76685a61585010B7855436906E50c05f91d316F9',
+  'LendingPoolReserveAddress': '0x7Ca54e9Aa3128bF15f764fa0f0f93e72b5267000',
+  'RewardsPoolAddress': '0x70000299ee8910ccacd97b1bb560e34f49c9e4f7',
 };
 
 module.exports = {
