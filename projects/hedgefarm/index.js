27d43e8b8f374fa2092e27dc5c8f38c9dfcf5aa2
tudorhedged <113336700+tudorhedged@users.noreply.github.com>
2023-03-09 13:07:38 +0100

    Add Alpha1 v2 migration

--- projects/hedgefarm/index.js
+++ projects/hedgefarm/index.js
@@ -1,24 +1,30 @@
 const sdk = require("@defillama/sdk")
-const { pool2 } = require("../helper/pool2");
-const { stakings } = require("../helper/staking");
-const { getCompoundV2Tvl } = require("../helper/compound");
 
-const ALPHA1_CONTRACT = '0xdE4133f0CFA1a61Ba94EC64b6fEde4acC1fE929E';
+const ALPHA1_V1_CONTRACT = '0xdE4133f0CFA1a61Ba94EC64b6fEde4acC1fE929E';
+const ALPHA1_V2_CONTRACT = '0x60908a71fbc9027838277f9f98e458bef2a201da';
 const ALPHA2_CONTRACT = '0x3C390b91Fc2f248E75Cd271e2dAbF7DcC955B1A3';
 
 const BTCB = '0x152b9d0FdC40C096757F570A51E494bd4b943E50';
-const USDC = '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E'
+const USDC = '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E';
 
-const ALPHA1_ABI = "uint256:totalBalance"
-const ALPHA2_ABI = "uint256:getLastUpdatedModulesBalance"
+const ALPHA1_ABI = "uint256:totalBalance";
+const ALPHA2_ABI = "uint256:getLastUpdatedModulesBalance";
 
 async function tvl(timestamp, block, chainBlocks) {
   const balances = {};
 
-  const alpha1Balance = (await sdk.api.abi.call({
+  const alpha1V1Balance = (await sdk.api.abi.call({
     abi: ALPHA1_ABI,
     chain: 'avax',
-    target: ALPHA1_CONTRACT,
+    target: ALPHA1_V1_CONTRACT,
+    params: [],
+    block: chainBlocks['avax'],
+  })).output;
+
+  const alpha1V2Balance = (await sdk.api.abi.call({
+    abi: ALPHA1_ABI,
+    chain: 'avax',
+    target: ALPHA1_V2_CONTRACT,
     params: [],
     block: chainBlocks['avax'],
   })).output;
@@ -31,8 +37,9 @@ async function tvl(timestamp, block, chainBlocks) {
     block: chainBlocks['avax'],
   })).output;
 
-  await sdk.util.sumSingleBalance(balances, `avax:${USDC}`, alpha1Balance)
-  await sdk.util.sumSingleBalance(balances, `avax:${BTCB}`, alpha2Balance)
+  await sdk.util.sumSingleBalance(balances, `avax:${USDC}`, alpha1V1Balance);
+  await sdk.util.sumSingleBalance(balances, `avax:${USDC}`, alpha1V2Balance);
+  await sdk.util.sumSingleBalance(balances, `avax:${BTCB}`, alpha2Balance);
 
   return balances;
 }
