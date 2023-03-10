9a2bd1cbe03ecd7177109e55493ffb29ac06ed1d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-10 17:07:33 +0100

    fix nexus

--- projects/nexus/index.js
+++ projects/nexus/index.js
@@ -3,11 +3,17 @@ const { sumTokens2 } = require('../helper/unwrapLPs');
 const pools = [
   '0xcafea112Db32436c2390F5EC988f3aDB96870627'   // current pool
 ];
+const yieldPools = [
+  '0x27f23c710dd3d878fe9393d93465fed1302f2ebd'
+]
 const getAssetsABI = "function getAssets() view returns (tuple(address assetAddress, bool isCoverAsset, bool isAbandoned)[])"
 
 async function tvl(timestamp, block, _, { api }) {
   const assets = await api.multiCall({ abi: getAssetsABI, calls: pools})
-  return sumTokens2({ api, ownerTokens: assets.map((v, i) => [v.map(i => i.assetAddress), pools[i]])})
+  const ownerTokens = assets.map((v, i) => [v.map(i => i.assetAddress), pools[i]])
+  const assets2 = await api.multiCall({  abi: 'address[]:getTrackedAssets', calls: yieldPools }) 
+  assets2.forEach((v, i) => ownerTokens.push([v, yieldPools[i]]))
+  return sumTokens2({ api, ownerTokens, blacklistedTokens: yieldPools})
 }
 
 module.exports = {
