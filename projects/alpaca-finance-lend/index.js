f10271aaeea4cf32409c2f093e328b7fc565c98c
jr-alpaca <uviveramrom@gmail.com>
2023-03-11 16:05:40 +0700

    feat: alpaca lending

---
+++ projects/alpaca-finance-lend/index.js
@@ -0,0 +1,37 @@
+const { calLendingTvl, calBorrowingTvl } = require("./lend");
+
+async function bscLendingTvl(timestamp, ethBlock, chainBlocks) {
+  const bscLendingTvl = await calLendingTvl("bsc", chainBlocks.bsc);
+  return { ...bscLendingTvl };
+}
+
+async function bscCalBorrowingTvl(timestamp, ethBlock, chainBlocks) {
+  const bscBorrowingTvl = await calBorrowingTvl("bsc", chainBlocks.bsc);
+  return { ...bscBorrowingTvl };
+}
+
+async function ftmLendingTvl(timestamp, ethBlock, chainBlocks) {
+  const ftmLendingTvl = await calLendingTvl("fantom", chainBlocks.fantom);
+  return { ...ftmLendingTvl };
+}
+
+async function ftmCalBorrowingTvl(timestamp, ethBlock, chainBlocks) {
+  const ftmBorrowingTvl = await calBorrowingTvl("fantom", chainBlocks.fantom);
+  return { ...ftmBorrowingTvl };
+}
+
+// node test.js projects/alpaca-finance-lend/index.js
+module.exports = {
+  start: 1602054167,
+  timetravel: true,
+  doublecounted: false,
+  methodology: "Sum total token and vaultDebt value in each vault",
+  bsc: {
+    tvl: bscLendingTvl,
+    borrowed: bscCalBorrowingTvl,
+  },
+  fantom: {
+    tvl: ftmLendingTvl,
+    borrowed: ftmCalBorrowingTvl,
+  },
+};
---
+++ projects/alpaca-finance-lend/lend.js
@@ -0,0 +1,113 @@
+const { getConfig } = require("../helper/cache");
+const abi = require("./abi.json");
+const sdk = require("@defillama/sdk");
+const BigNumber = require("bignumber.js");
+
+async function getProtocolAddresses(chain) {
+  if (chain == "bsc") {
+    return await getConfig(
+      "alpaca-finance/lyf-bsc",
+      "https://raw.githubusercontent.com/alpaca-finance/bsc-alpaca-contract/main/.mainnet.json"
+    );
+  }
+  if (chain == "fantom") {
+    return await getConfig(
+      "alpaca-finance/lyf-fantom",
+      "https://raw.githubusercontent.com/alpaca-finance/bsc-alpaca-contract/main/.fantom_mainnet.json"
+    );
+  }
+}
+
+async function calLendingTvl(chain, block) {
+  /// @dev Initialized variables
+  const balances = {};
+
+  /// @dev Getting all addresses from Github
+  const addresses = await getProtocolAddresses(chain);
+
+  const vaultsTotalToken = (
+    await sdk.api.abi.multiCall({
+      block,
+      abi: abi.totalToken,
+      calls: addresses["Vaults"].map((v) => {
+        return { target: v.address };
+      }),
+      chain,
+    })
+  ).output;
+
+  const vaultsPendingInterest = await pendingInterest(addresses, block, chain)
+
+  for (let i = 0; i < addresses["Vaults"].length; i++) {
+    const vault = addresses["Vaults"][i];
+
+    const vaultPendingInterest = new BigNumber(vaultsPendingInterest[i].output)
+    const vaultTotalToken = new BigNumber(vaultsTotalToken[i].output).plus(vaultPendingInterest)
+
+    await sdk.util.sumSingleBalance(
+      balances,
+      vault.baseToken,
+      vaultTotalToken.toFixed(0),
+      chain
+    );
+  }
+  return balances;
+}
+
+async function calBorrowingTvl(chain, block) {
+  /// @dev Initialized variables
+  const balances = {};
+
+  /// @dev Getting all addresses from Github
+  const addresses = await getProtocolAddresses(chain);
+
+  const vaultsDebtVal = (
+    await sdk.api.abi.multiCall({
+      block,
+      abi: abi.vaultDebtVal,
+      calls: addresses["Vaults"].map((v) => {
+        return { target: v.address };
+      }),
+      chain,
+    })
+  ).output;
+
+  const vaultsPendingInterest = await pendingInterest(addresses, block, chain)
+
+
+  for (let i = 0; i < addresses["Vaults"].length; i++) {
+    const vault = addresses["Vaults"][i];
+
+    const vaultPendingInterest = new BigNumber(vaultsPendingInterest[i].output)
+    const vaultDebtValue = new BigNumber(vaultsDebtVal[i].output).plus(vaultPendingInterest)
+
+    await sdk.util.sumSingleBalance(
+      balances,
+      vault.baseToken,
+      vaultDebtValue.toFixed(0),
+      chain
+    );
+  }
+  return balances;
+}
+
+async function pendingInterest(addresses,block,chain) {
+  return (
+    await sdk.api.abi.multiCall({
+      block,
+      abi: abi.pendingInterest,
+      calls: addresses["Vaults"].map((v) => {
+        return { target: v.address, 
+          params : [0]
+        };
+      }),
+      chain,
+    })
+  ).output;
+
+}
+
+module.exports = {
+  calLendingTvl,
+  calBorrowingTvl,
+};
