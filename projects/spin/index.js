212faa764179bdae5d39a48fce579953363d3c0f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-12 11:32:29 +0100

    split spin adapter

--- projects/spin/index.js
+++ projects/spin/index.js
@@ -6,13 +6,6 @@ const NATIVE_NEAR = 'near.near';
 const FT_NEAR = 'wrap.near';
 const FT_AURORA = 'd9c2d319cd7e6177336b0a9c93c21cb48d84fb54.factory.bridge.near';
 
-const PERP_PROJECT_CONTRACT = 'v2_0_2.perp.spin-fi.near';
-const GET_BASE_CUURENCY_METHOD = 'get_base_currency';
-
-const VAULT_PROJECT_CONTRACT = 'v1.vault.spin-fi.near'
-const VAULT_GET_ALL = 'vault_get_all';
-
-
 async function tvl() {
     let spotFtCurrencies = (await call(SPOT_PROJECT_CONTRACT, GET_CURRENCIES_METHOD, {}))
         .filter(token => token['address'] !== NATIVE_NEAR)
@@ -28,18 +21,6 @@ async function tvl() {
     // NOTE: add near balance for spot
     const spot_contract_state = await view_account(SPOT_PROJECT_CONTRACT);
     sumSingleBalance(balances, FT_NEAR, spot_contract_state['amount']);
-
-    let perpFtCurrency = (await call(PERP_PROJECT_CONTRACT, GET_BASE_CUURENCY_METHOD, {}))['address'];
-    
-    // NOTE: add collateral balance (only USDC right now) for perp
-    balances = await addTokenBalances(perpFtCurrency, PERP_PROJECT_CONTRACT, balances);
-
-    // NOTE: add TVL for vaults
-    const execution_assets = (await call(VAULT_PROJECT_CONTRACT, VAULT_GET_ALL, {"limit": "100", "offset": "0"}))
-        .map(vault => vault['invariant']['execution_asset']);
-
-    balances = await addTokenBalances(execution_assets, VAULT_PROJECT_CONTRACT, balances);
-
     return balances;
 }
 
