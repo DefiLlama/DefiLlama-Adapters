dacd102a164eed97007cbf50a2f1b14704c88973
realdealshaman <realdealshaman11@gmail.com>
2023-03-13 20:39:03 +0000

    use unknown token helper

--- projects/0xacid/index.js
+++ projects/0xacid/index.js
@@ -1,4 +1,4 @@
-const { staking } = require("../helper/staking");
+const { staking, stakingUnknownPricedLP } = require("../helper/staking");
 
 const ACID_STAKING = "0x00a842038a674616f6a97e62f80111a536778282";
 const ACID_TOKEN = "0x29C1EA5ED7af53094b1a79eF60d20641987c867e";
@@ -7,6 +7,6 @@ module.exports = {
   start: 1678417200,
   arbitrum: {
     tvl: () => ({}),
-    staking: staking(ACID_STAKING, ACID_TOKEN)
+    staking: stakingUnknownPricedLP(ACID_STAKING, ACID_TOKEN, "arbitrum", "0x73474183a94956cd304c6c5a504923d8150bd9ce")
   },
 }
