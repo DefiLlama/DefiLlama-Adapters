79e1ed347181369807108474644e1aec5d309ef0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-13 13:04:25 +0100

    minor fix

--- projects/0xacid/index.js
+++ projects/0xacid/index.js
@@ -1,14 +1,12 @@
-const sdk = require("@defillama/sdk");
 const { staking } = require("../helper/staking");
 
 const ACID_STAKING = "0x00a842038a674616f6a97e62f80111a536778282";
 const ACID_TOKEN = "0x29C1EA5ED7af53094b1a79eF60d20641987c867e";
 
 module.exports = {
-  timetravel: true,
   start: 1678417200,
   arbitrum: {
     tvl: () => ({}),
-    staking: staking(ACID_STAKING, ACID_TOKEN, "arbitrum")
+    staking: staking(ACID_STAKING, ACID_TOKEN)
   },
 }
