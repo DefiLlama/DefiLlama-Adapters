5953e61d1e80bcffc6769c8f907b96bb69a41d23
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-14 15:37:38 +0100

    update dxsale

--- projects/dxsale/apiCache.js
+++ projects/dxsale/apiCache.js
@@ -16,6 +16,10 @@ const {
   moonriverArchives,
   milkomedaArchives,
   smartbchArchives,
+  dogeArchives,
+  dexitArchives,
+  coreDaoArchives,
+  bitgertArchives,
 } = require("./config");
 const {
   getStorageLPLockDataV33,
@@ -200,4 +204,16 @@ module.exports = {
   smartbch: {
     tvl: getTVLTotal(smartbchArchives),
   },
+  dogechain: {
+    tvl: getTVLTotal(dogeArchives),
+  },
+  dexit: {
+    tvl: getTVLTotal(dexitArchives),
+  },
+  core: {
+    tvl: getTVLTotal(coreDaoArchives),
+  },
+  bitgert: {
+    tvl: getTVLTotal(bitgertArchives),
+  }
 };
