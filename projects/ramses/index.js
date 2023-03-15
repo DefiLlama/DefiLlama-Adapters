82f82c445ddcf99ddebee9890826743a36be960f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-15 08:41:58 +0100

    minor fix

--- projects/ramses/index.js
+++ projects/ramses/index.js
@@ -1,9 +1,10 @@
-const {uniTvlExport} = require('../helper/calculateUniTvl.js')
+const {getUniTVL} = require('../helper/unknownTokens')
 const { staking } = require('../helper/staking')
 
 module.exports = {
+  misrepresentedTokens: true,
   arbitrum:{
-    tvl: uniTvlExport("0xAAA20D08e59F6561f242b08513D36266C5A29415", "arbitrum"),
-    staking: staking("0xAAA343032aA79eE9a6897Dab03bef967c3289a06", "0xaaa6c1e32c55a7bfa8066a6fae9b42650f262418","arbitrum"),
+    tvl: getUniTVL({ factory: '0xAAA20D08e59F6561f242b08513D36266C5A29415', useDefaultCoreAssets: true, fetchBalances: true, }),
+    staking: staking("0xAAA343032aA79eE9a6897Dab03bef967c3289a06", "0xaaa6c1e32c55a7bfa8066a6fae9b42650f262418"),
   },
 }
