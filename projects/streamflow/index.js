2b783a363051d0c51d24960a86ce96a116b79418
Yolley <Yolley@users.noreply.github.com>
2023-03-15 23:19:54 +0900

    use metabase api wrapper in streamflow adapter

--- projects/streamflow/index.js
+++ projects/streamflow/index.js
@@ -1,27 +1,22 @@
 const { getCache } = require('../helper/http')
 
-const TVL_KEY = "TVL (unlocked) (L1+stables)";
-const VESTING_KEY = "TVL (locked) (vesting)";
+const SOLANA = "SOLANA";
+const TVL_KEY = "tvl";
+const VESTING_KEY = "tvl_vested";
 const api =
-  "https://app.datawisp.io/api/exec/sheet_results/8fd98286-f0e1-4a32-91cd-d88d827ae9d7";
-
-const flatten = (arr) => {
-  return arr.reduce((acc, next) => [...acc, ...next], []);
-}
+  "https://metabase.internal-streamflow.com/_public/api/v1/stats/accumulated";
 
 const getValueForKey = (arr, key) => {
   for (let i = 0; i < arr.length; i++) {
-    const el = arr[i];
-    if (el[key] !== undefined) {
-      return el[key];
+    if (arr[i].chain === SOLANA && arr[i][key] !== undefined) {
+      return arr[i][key];
     }
   }
   return null;
 }
 
 async function getCachedApiRespnse() {
-  let apiResponse = (await getCache(api)).data;
-  apiResponse = flatten(Object.values(apiResponse));
+  let apiResponse = (await getCache(api));
 
   return apiResponse;
 }
