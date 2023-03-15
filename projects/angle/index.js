9030b89beda90d815b724a99996762bae4e1854f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-15 19:46:48 +0100

    angle: update code for euler hack

--- projects/angle/index.js
+++ projects/angle/index.js
@@ -2,6 +2,7 @@ const sdk = require("@defillama/sdk");
 const { staking } = require("../helper/staking");
 const { getConfig } = require("../helper/cache");
 const { sumTokens2 } = require("../helper/unwrapLPs");
+const { getUniqueAddresses } = require("../helper/tokenMapping");
 
 const ANGLE = "0x31429d1856ad1377a8a0079410b297e1a9e214c2";
 const veANGLE = "0x0C462Dbb9EC8cD1630f1728B2CFD2769d09f0dd5";
@@ -35,54 +36,39 @@ async function getVaultManagersFromAPI(chain) {
   return calls;
 }
 
-async function tvl(chain, block) {
+async function tvl(_, _1, _2, { api }) {
+  const chain = api.chain
   const balances = {};
   const tokensAndOwners = [];
   if (chain === "ethereum") {
     // Registry will be released in next sdk of Angle + graphql endpoint to come
-    const collaterals = {
-      dai: "0x6b175474e89094c44da98b954eedeac495271d0f",
-      usdc: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-      frax: "0x853d955acef822db058eb8505911ed77f175b99e",
-      fei: "0x956F47F50A910163D8BF957Cf5846D573E7f87CA",
-      weth: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    };
-
     const agEUR = {
       contract: "0x1a7e4e63778B4f12a199C062f3eFdD288afCBce8",
       stableMasterFront: "0x5adDc89785D75C86aB939E9e15bfBBb7Fc086A87",
       poolManagers: {
         dai: "0xc9daabC677F3d1301006e723bD21C60be57a5915", // DAI
-        usdc: "0xe9f183FC656656f1F17af1F2b0dF79b8fF9ad8eD", // USDC
+        // usdc: "0xe9f183FC656656f1F17af1F2b0dF79b8fF9ad8eD", // USDC disabled because of EULER hack
         fei: "0x53b981389Cfc5dCDA2DC2e903147B5DD0E985F44", // FEI
         frax: "0x6b4eE7352406707003bC6f6b96595FD35925af48", // FRAX
         weth: "0x3f66867b4b6eCeBA0dBb6776be15619F73BC30A2", // WETH
       },
     };
 
-    const agTokens = [agEUR];
-    const tokenMapping = {};
-    agTokens.map((t) => {
-      return Object.entries(t.poolManagers).forEach(([key, value]) => {
-        tokenMapping[value] = collaterals[key];
-      });
-    });
+    // count the USDC in pool manager contract
+    tokensAndOwners.push(['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', '0xe9f183FC656656f1F17af1F2b0dF79b8fF9ad8eD']) // add USDC in USDC manager
+    const poolManagers = getUniqueAddresses([agEUR].map(i => Object.values(i.poolManagers)).flat())
 
-    let { output: assets } = await sdk.api.abi.multiCall({
-      calls: Object.keys(tokenMapping).map((i) => ({ target: i })),
+    let assets = await api.multiCall({
+      calls: poolManagers,
       abi: poolManagers_abi["getTotalAsset"],
-      chain,
-      block,
     });
-    let { output: tokens } = await sdk.api.abi.multiCall({
-      calls: Object.keys(tokenMapping).map((i) => ({ target: i })),
+    let tokens = await api.multiCall({
+      calls: poolManagers,
       abi: poolManagers_abi["token"],
-      chain,
-      block,
     });
 
-    assets.forEach(({ output }, i) =>
-      sdk.util.sumSingleBalance(balances, tokens[i].output, output)
+    assets.forEach((output, i) =>
+      sdk.util.sumSingleBalance(balances, tokens[i], output)
     );
 
     // AMOs
@@ -92,45 +78,21 @@ async function tvl(chain, block) {
     const cvxagEUREUROCstaker = "0xA91fccC1ec9d4A2271B7A86a7509Ca05057C1A98";
     const AngleagEUREUROCStaker = "0xC1e8Dba1cbF29f1CaA8343CAe96d5AdFD9bca736";
     // pool TVL
-    let agEURTVL = await sdk.api.erc20.balanceOf({
-      owner: curvePool,
-      target: agEUR.contract,
-      block: block,
-    });
-    let EUROCTVL = await sdk.api.erc20.balanceOf({
-      owner: curvePool,
-      target: EUROC,
-      block: block,
-    });
-    const totPoolTokenSupply = await sdk.api.erc20.totalSupply({
-      target: curvePool,
-      block: block,
-    });
-    // Angle holdings of Curve agEUREUROC LP tokens (staked on Stake DAO and Convex)
-    let sdagEUREUROCTVL = await sdk.api.erc20.balanceOf({
-      owner: AngleagEUREUROCStaker,
-      target: sdagEUREUROC,
-      block: block,
-    });
-    let cvxagEUREUROCstakerTVL = await sdk.api.erc20.balanceOf({
-      owner: AngleagEUREUROCStaker,
-      target: cvxagEUREUROCstaker,
-      block: block,
-    });
-    const AnglePoolTokenSupply =
-      Number(sdagEUREUROCTVL.output) + Number(cvxagEUREUROCstakerTVL.output);
-    let AngleagEURTVL =
-      (AnglePoolTokenSupply / totPoolTokenSupply.output) * agEURTVL.output;
-    let AngleEUROCTVL =
-      (AnglePoolTokenSupply / totPoolTokenSupply.output) * EUROCTVL.output;
-
-    sdk.util.sumSingleBalance(balances, agEUR.contract, AngleagEURTVL);
-    sdk.util.sumSingleBalance(balances, EUROC, AngleEUROCTVL);
+    const [agEurBal, eurocBal, totPoolTokenSupply, sdagEUREUROCTVL, cvxagEUREUROCstakerTVL] = await Promise.all([
+      api.call({ abi: 'erc20:balanceOf', target: agEUR.contract, params: curvePool }),
+      api.call({ abi: 'erc20:balanceOf', target: EUROC, params: curvePool }),
+      api.call({ abi: 'erc20:totalSupply', target: curvePool }),
+      // Angle holdings of Curve agEUREUROC LP tokens (staked on Stake DAO and Convex)
+      api.call({ abi: 'erc20:balanceOf', target: sdagEUREUROC, params: AngleagEUREUROCStaker }),
+      api.call({ abi: 'erc20:balanceOf', target: cvxagEUREUROCstaker, params: AngleagEUREUROCStaker }),
+    ])
+    const eurocBalance = eurocBal * (+sdagEUREUROCTVL + +cvxagEUREUROCstakerTVL) / totPoolTokenSupply
+    sdk.util.sumSingleBalance(balances, EUROC, eurocBalance);
   }
 
   // Borrowing module
   tokensAndOwners.push(...(await getVaultManagersFromAPI(chain)));
-  return sumTokens2({ balances, chain, block, tokensAndOwners });
+  return sumTokens2({ balances, api, tokensAndOwners });
 }
 
 /*
@@ -139,6 +101,9 @@ If not, the API call defaults to mainnet and the blockchain calls fail and retur
 */
 
 module.exports = {
+  hallmarks: [
+    [Math.floor(new Date('2023-03-13') / 1e3), 'Euler was hacked'],
+  ],
   ethereum: {
     staking: staking(veANGLE, ANGLE, "ethereum"),
   },
@@ -147,6 +112,5 @@ module.exports = {
 
 ["ethereum", "polygon", "optimism", "arbitrum", "avax"].forEach((chain) => {
   if (!module.exports[chain]) module.exports[chain] = {};
-  module.exports[chain].tvl = async (_, _b, { [chain]: block }) =>
-    tvl(chain, block);
+  module.exports[chain].tvl = tvl
 });
