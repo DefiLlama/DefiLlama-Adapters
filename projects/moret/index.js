bb5577516eef77369e298166612d6eb955765e88
eightwoods <45482848+eightwoods@users.noreply.github.com>
2023-03-16 20:45:28 +0000

    Update moret index
    add new pools

--- projects/moret/index.js
+++ projects/moret/index.js
@@ -1,8 +1,10 @@
 // const sdk = require('@defillama/sdk');
 const { sumTokens2 } = require('../helper/unwrapLPs')
 
-const markets = ['0xE851E0628A9F0D62393C7105A0eb112a6C3572fE', // ETH market pool
-                '0xdfA196542929Ae0C468915b9f2Ae6A713DFf8D0b' // BTC market pool
+const markets = ['0x01885b7a81B9699684DF563206FfaE5A19F9eaaC', // ETH pool
+                '0xD8FB918669E95395a94C1f857766C124098Bb828', // BTC pool
+                '0xE851E0628A9F0D62393C7105A0eb112a6C3572fE', // ETH market pool 2
+                '0xdfA196542929Ae0C468915b9f2Ae6A713DFf8D0b' // BTC market pool 2
             ]
 const tvlTokens = ['0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', // WETH
                 '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6', // WBTC
