6cdc138d468411f7a588181ce9ba42b88f0f6e0c
alikonuk1 <alikonuk1@gmail.com>
2023-03-18 14:51:39 +0300

    add pxswap

---
+++ projects/pxswap/index.js
@@ -0,0 +1,9 @@
+const sdk = require('@defillama/sdk')
+const { sumTokensExport } = require('../helper/unwrapLPs')
+
+const contract = "0x638B06F80FB28F109E65C9d5cC585aDf9A0c3f9f"
+const usdcToken = "0xfA9343C3897324496A05fC75abeD6bAC29f8A40f"
+
+module.exports = {
+    ethereum: { tvl: sumTokensExport({ owner: contract, tokens: [usdcToken]}) },
+}
