3542b8e0f4e077e85ae4483f20c2ec7a81d82b68
Kevin <10713090+fractalityy@users.noreply.github.com>
2023-03-17 21:35:28 -0400

    Remove hallmarks

--- projects/ArbitrumExchange/index.js
+++ projects/ArbitrumExchange/index.js
@@ -5,8 +5,6 @@ const dexTVL = getUniTVL({ factory: '0x1C6E968f2E6c9DEC61DB874E28589fd5CE3E1f2c'
 
 
 module.exports = {
-      hallmarks: [
-     [1677744000,"Rug Pull"]
     methodology: `Uses factory(0x1C6E968f2E6c9DEC61DB874E28589fd5CE3E1f2c) address and whitelisted tokens address to find and price Liquidity Pool pairs. We also have our native token $ARX staking.`,
     arbitrum: {
         tvl: dexTVL,
