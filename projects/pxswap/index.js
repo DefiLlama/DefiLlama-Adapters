8549170c14582eae355650d7819ffcf08c3a215f
ali konuk <alikonuk1@protonmail.com>
2023-03-18 15:20:45 +0300

    Update index.js

--- projects/pxswap/index.js
+++ projects/pxswap/index.js
@@ -5,5 +5,5 @@ const contract = "0x638B06F80FB28F109E65C9d5cC585aDf9A0c3f9f"
 const usdcToken = "0xfA9343C3897324496A05fC75abeD6bAC29f8A40f"
 
 module.exports = {
-    ethereum: { tvl: sumTokensExport({ owner: contract, tokens: [usdcToken]}) },
+    kava: { tvl: sumTokensExport({ owner: contract, tokens: [usdcToken]}) },
 }
