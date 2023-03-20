7cb5be0489b83a4148ca0bcbd605bd4c2c60d3dc
BlockChain Engineer <posninja1008@gmail.com>
2023-03-21 06:01:00 +0900

    included addresses

--- projects/sohei/index.js
+++ projects/sohei/index.js
@@ -1,20 +1,7 @@
 const { compoundExports } = require("../helper/compound");
 
-const comptroller = "0x9f750cf10034f3d7a18221aec0bddab7fc6f32ba";
-const cether = "0x685d1f1a83ff64e75fe882e7818e4ad9173342ca";
-const cetheEquivalent = "0x82af49447d8a07e3bd95bd0d56f35241523fbab1";
-const blacklistedTokens = ["0x6e002cb9bf8c17409eeb6c593ef6548faddd2985"];
-
 module.exports = {
   methodology:
     "Same as compound, we just get all the collateral (not borrowed money) on the lending markets.",
-  arbitrum: compoundExports(
-    comptroller,
-    'arbitrum',
-    cether,
-    cetheEquivalent,
-    undefined,
-    undefined,
-    { blacklistedTokens: blacklistedTokens }
-  ),
+  arbitrum: compoundExports('0x9f750cf10034f3d7a18221aec0bddab7fc6f32ba', 'arbitrum', '0x685d1f1a83ff64e75fe882e7818e4ad9173342ca', '0x82af49447d8a07e3bd95bd0d56f35241523fbab1', undefined, undefined, { blacklistedTokens: ['0x6e002cb9bf8c17409eeb6c593ef6548faddd2985'] }),
 };
