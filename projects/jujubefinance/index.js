f763d40062afc4cb8d748f8cb09d0542473ff2a8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-20 15:57:24 +0100

    jujube.fi: update resource account address

--- projects/jujubefinance/index.js
+++ projects/jujubefinance/index.js
@@ -1,6 +1,6 @@
 const { dexExport } = require('../helper/chain/aptos')
 
 module.exports = dexExport({
-  account: '0x88888888f2f4b1daeec09653917d5c5364b60d6d2a14eb649f5cf54b1277f9d9',
+  account: '0x4a45434525e4fc243071250a449494d915719d2b3f9f7b92242196c7f2e88346',
   poolStr: 'liquidity_pool::LiquidityPool',
 })
