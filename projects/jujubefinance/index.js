c39d8b04f03ec1c0a94f39e466e6473a0e1edcad
admin <nezatchancefa@gmail.com>
2023-03-20 11:46:42 +0800

    Add jujubefinance

---
+++ projects/jujubefinance/index.js
@@ -0,0 +1,6 @@
+const { dexExport } = require('../helper/chain/aptos')
+
+module.exports = dexExport({
+  account: '0x88888888f2f4b1daeec09653917d5c5364b60d6d2a14eb649f5cf54b1277f9d9',
+  poolStr: 'liquidity_pool::LiquidityPool',
+})
