7ec03ff2080938c4df516c7975c3a1210ec7a6e2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-21 12:50:23 +0100

    improve cardano token support

--- projects/djed/index.js
+++ projects/djed/index.js
@@ -1,15 +1,8 @@
-const { getAdaInAddress } = require("../helper/chain/cardano");
-
-async function tvl() {
-  const adaTvl = await getAdaInAddress("addr1z9s3v9vyyctzr4xagvrayw87yvzre6qcq7qw2uvqfznf92qm5kjdmrpmng059yellupyvwgay2v0lz6663swmds7hp0q2jjlf4")
-  return {
-    cardano: adaTvl,
-  };
-}
+const { sumTokensExport } = require("../helper/chain/cardano");
 
 module.exports = {
   timetravel: false,
   cardano: {
-    tvl,
+    tvl: sumTokensExport({ owner: 'addr1z9s3v9vyyctzr4xagvrayw87yvzre6qcq7qw2uvqfznf92qm5kjdmrpmng059yellupyvwgay2v0lz6663swmds7hp0q2jjlf4', tokens: ['lovelace']}),
   },
 };
