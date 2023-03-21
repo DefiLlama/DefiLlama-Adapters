1597f8ca2403fb41c6849e69d2e323c9846c0eb2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-21 09:34:49 +0100

    code refactor

--- projects/waterdendy/index.js
+++ projects/waterdendy/index.js
@@ -1,21 +1,5 @@
-const { transformArbitrumAddress } = require("../helper/portedTokens");
-const { addFundsInMasterChef } = require('../helper/masterchef');
+const { masterchefExports  } = require('../helper/unknownTokens')
 
 const STAKING_CONTRACT_ARBITRUM = "0xD0834fF6122FF8dcf38E3eB79372C00FAeAFa08B"; //MASTERCHEF ARBITRUM
 
-const arbitrumTvl = async (timestamp, ethBlock, chainBlocks) => {
-    const balances = {};
-    const transformAddress = await transformArbitrumAddress();
-
-    await addFundsInMasterChef(
-        balances, STAKING_CONTRACT_ARBITRUM, chainBlocks.arbitrum, 'arbitrum', transformAddress);
-        delete balances['0x88692aD37c48e8F4c821b71484AE3C2878C2A2C6']; //TOKEN ADDRESS
-        return balances;
-    };
-
-    module.exports={
-    timetravel: true,
-    arbitrum: {
-        tvl: arbitrumTvl
-    },
-}
+module.exports = masterchefExports({ chain: 'arbitrum', masterchef: STAKING_CONTRACT_ARBITRUM, nativeToken: '0x88692aD37c48e8F4c821b71484AE3C2878C2A2C6', useDefaultCoreAssets: true, })
