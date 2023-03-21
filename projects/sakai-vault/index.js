634c4bd0c54a6dca883ba6682c48dc00536c7b06
admin <122627006+SakaiVault@users.noreply.github.com>
2023-03-21 14:46:32 +0300

    added sakai-vault to projects

---
+++ projects/sakai-vault/index.js
@@ -0,0 +1,17 @@
+const contracts  = {
+    "SAKAI": "0x43b35e89d15b91162dea1c51133c4c93bdd1c4af",
+    "Sakai-SP": "0xc20A079c7962D9fc92173cda349e80D484dFA42A" //Sakai Staking Protocol Contract
+};
+
+
+async function getTVL() {
+    const response = await fetch("https://lp.sakaivault.io/tvl"); 
+    const tvl = await response.json() //Check locked value : https://app.sakaivault.io/
+    const lastIndex = await tvl[tvl.length-1]
+    return {...lastIndex}
+  }
+  
+  module.exports = {
+    getTVL,
+  };
+  
