48597075346e3fcc35e82c65b962627205ada0fb
admin <122627006+SakaiVault@users.noreply.github.com>
2023-03-21 15:12:49 +0300

    Update index.js

--- projects/sakai-vault/index.js
+++ projects/sakai-vault/index.js
@@ -1,3 +1,5 @@
+const utils = require('../helper/utils');
+
 const contracts  = {
     "SAKAI": "0x43b35e89d15b91162dea1c51133c4c93bdd1c4af",
     "Sakai-SP": "0xc20A079c7962D9fc92173cda349e80D484dFA42A" //Sakai Staking Protocol Contract
@@ -5,7 +7,7 @@ const contracts  = {
 
 
 async function fetch() {
-    const response = await fetch("https://lp.sakaivault.io/tvl"); 
+    const response = await utils.fetchURL("https://lp.sakaivault.io/tvl"); 
     const tvl = await response.json() //Check locked value : https://app.sakaivault.io/
     const lastIndex = await tvl[tvl.length-1]
     return {...lastIndex}
