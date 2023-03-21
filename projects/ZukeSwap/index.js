1793511afa4333a18b5592f3a3da1f397e4b520b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-21 09:38:22 +0100

    refactor code

--- projects/ZukeSwap/index.js
+++ projects/ZukeSwap/index.js
@@ -2,9 +2,9 @@ const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
   misrepresentedTokens: true,
-  dogechain: {
+  loop: {
     tvl: getUniTVL({
-      chain: 'loop',
+      fetchBalances: true,
       useDefaultCoreAssets: true,
       factory: '0xc738bE6fE1A54d7B0F6e4C3262a46a002aC2508e',
     })
