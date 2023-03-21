a4e40ec40895c4cfe3c20bf2af5b2e3d9f508a99
4TT1L4 <2914096+4TT1L4@users.noreply.github.com>
2023-03-21 08:49:12 +0100

    Updated Genius Yield Adapter
     - Added NMKR staking to the TVL.

--- projects/genius-yield/index.js
+++ projects/genius-yield/index.js
@@ -1,11 +1,13 @@
 const { getTokenBalance,  } = require('../helper/chain/cardano')
 
-const asset1 = 'dda5fdb1002f7389b33e036b6afee82a8189becb6cba852e8b79b4fb0014df1047454e53'
+const gens = 'dda5fdb1002f7389b33e036b6afee82a8189becb6cba852e8b79b4fb0014df1047454e53'
+const nmkr = '5dac8536653edc12f6f5e1045d8164b9f59998d3bdc300fc928434894e4d4b52'
 const owner = 'addr1w8r99sv75y9tqfdzkzyqdqhedgnef47w4x7y0qnyts8pznq87e4wh'
 
 async function staking() {
   return {
-    'genius-yield': (await getTokenBalance(asset1, owner)) / 1e6
+    'genius-yield': (await getTokenBalance(gens, owner)) / 1e6,
+    'nmkr' : (await getTokenBalance(nmkr, owner)) / 1e6
   };
 }
 
