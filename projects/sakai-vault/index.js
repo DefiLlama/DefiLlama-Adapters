575091b7674201d0880d8844cea7fc996762fb91
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-21 13:25:51 +0100

    code refactor

--- projects/sakai-vault/index.js
+++ projects/sakai-vault/index.js
@@ -1,19 +1,13 @@
-const utils = require('../helper/utils');
+const { staking } = require('../helper/staking');
 
-const contracts  = {
-    "SAKAI": "0x43b35e89d15b91162dea1c51133c4c93bdd1c4af",
-    "Sakai-SP": "0xc20A079c7962D9fc92173cda349e80D484dFA42A" //Sakai Staking Protocol Contract
-};
+const contracts = {
+  "SAKAI": "0x43b35e89d15b91162dea1c51133c4c93bdd1c4af",
+  "Sakai-SP": "0xc20A079c7962D9fc92173cda349e80D484dFA42A" //Sakai Staking Protocol Contract
+}
 
-
-async function fetch() {
-    const response = await utils.fetchURL("https://lp.sakaivault.io/tvl"); 
-    const tvlData = await response.data //Check locked value : https://app.sakaivault.io/
-    const lastIndex = await tvlData[tvlData.length-1]
-    return Number(lastIndex.balanceInUSDT)
-  }
-  
-  module.exports = {
-    fetch,
-  };
-  
+module.exports = {
+  bsc: {
+    tvl: () => 0,
+    staking: staking(contracts['Sakai-SP'], contracts.SAKAI)
+  },
+}
