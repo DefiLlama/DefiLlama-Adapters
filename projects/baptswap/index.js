09ec30c6f01d9c8455b943fd271a0b8ea65ebd3f
AceyAdapter <nicholaskreitz@gmail.com>
2023-03-21 21:58:11 -0400

    add baptswap

---
+++ projects/baptswap/index.js
@@ -0,0 +1,6 @@
+const { dexExport } = require('../helper/chain/aptos')
+
+module.exports = dexExport({
+  account: '0x2ad8f7e64c7bffcfe94d7dea84c79380942c30e13f1b12c7a89e98df91d0599b',
+  poolStr: 'swap::TokenPairMetadata',
+})
