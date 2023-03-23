11699405a0031e4a9623ec3cd51b33be8dd67c26
babayega <shubh800@gmail.com>
2023-03-23 21:30:59 +0530

    Added mvlp vault

--- projects/polysynth/index.js
+++ projects/polysynth/index.js
@@ -1,11 +1,14 @@
 const sdk = require("@defillama/sdk");
 const bpsABI = require('./bps.json');
 const utils = require('../helper/utils');
+const { sumTokens } = require('../helper/unwrapLPs')
 const {BigNumber} = require("ethers")
 
 let _response;
 const GLP_BEEFY_VAULT="0x9dbbBaecACEDf53d5Caa295b8293c1def2055Adc";
+const MVLP_BEEFY_VAULT = "0xa0FdCDDA62C4C6a0109A702a7Efe59B4E8807e3f";
 const GLP_TOKEN = "0x4277f8F2c384827B5273592FF7CeBd9f2C1ac258";
+const MVLP_TOKEN = "0x9F4f8bc00F48663B7C204c96b932C29ccc43A2E8";
 
 function fetchTVL(chainId) {
   return async (chainBlocks) => {
@@ -27,7 +30,20 @@ function fetchTVL(chainId) {
             sdk.util.sumSingleBalance(ethTVL,asset,balance);
             break;
           case 137:
-            sdk.util.sumSingleBalance(polygonTVL,'polygon:'+asset,balance);
+            if (vaults[v]["name"] == "MVLP") {
+                // Get BPS for beefy vault
+                const bps = await sdk.api.abi.call({
+                  target: MVLP_BEEFY_VAULT,
+                  abi: bpsABI['bps'],
+                  block: chainBlocks['polygon'],
+                  chain: 'polygon'
+              })
+
+              const mvlp = BigNumber.from(bps.output).mul(BigNumber.from(balance)).div(BigNumber.from('1000000000000000000')).toString();
+              sdk.util.sumSingleBalance(polygonTVL,'polygon:'+MVLP_TOKEN,mvlp);
+            } else {
+              sdk.util.sumSingleBalance(polygonTVL,'polygon:'+asset,balance);
+            }
             break;
           case 42161:            
             if (vaults[v]["name"] == "GLP") {
