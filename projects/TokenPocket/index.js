cc5b0bc0769fb464c72698c067ff03ca7a78eb43
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-23 11:00:16 +0100

    minor fix

--- projects/TokenPocket/index.js
+++ projects/TokenPocket/index.js
@@ -1,21 +1,11 @@
 const axios = require("axios");
 
 async function tvl() {
-  const tvlData = (
-    await axios.get(
-      "https://preserver.mytokenpocket.vip/v1/pledge/method_any_v2",
-      {
-        headers: {
-          api: "/eth2/v2/partner/dashboard",
-        },
-      }
-    )
-  )
-  let total_amount = 0
-  if (tvlData?.data?.data?.staking) {
-    total_amount = tvlData?.data?.data?.staking.total_amount || 0
-  }
-  return { ethereum: total_amount} 
+  const tvlData = (await axios.get(
+    "https://preserver.mytokenpocket.vip/v1/pledge/method_any_v2", {
+    headers: { api: "/eth2/v2/partner/dashboard", }
+  }))
+  return { ethereum: tvlData.data.data.staking.total_amount }
 }
 
 module.exports = {
