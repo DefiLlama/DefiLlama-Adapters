12f7de69937211fce55859901cad2d4b71e0a319
Avedith <avedith@avedith.dev>
2023-03-24 09:12:03 +1000

    update chain and add treasury

--- projects/flair-dex/index.js
+++ projects/flair-dex/index.js
@@ -2,7 +2,7 @@ const { getUniTVL } = require("../helper/unknownTokens");
 
 module.exports = {
   misrepresentedTokens: true,
-  polygon: {
+  avax: {
     tvl: getUniTVL({
       factory: "0x634e02EB048eb1B5bDDc0CFdC20D34503E9B362d",
       fetchBalances: true,
