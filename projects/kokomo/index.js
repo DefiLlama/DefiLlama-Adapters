d97aa5612e63e0504d9eec45e241cc54517d91e6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-23 07:32:35 +0100

    minor fix

--- projects/kokomo/index.js
+++ projects/kokomo/index.js
@@ -1,12 +1,6 @@
-const { getCompoundV2Tvl } = require('../helper/compound')
+const { compoundExports, } = require('../helper/compound')
 
 module.exports = {
-    optimism: {
-        tvl: getCompoundV2Tvl("0x91c471053bA4697B13d62De1E850Cc89EbE23633", "optimism"),
-        borrowed: ()=>({})
-    },
-    arbitrum: {
-        tvl: getCompoundV2Tvl("0x91c471053bA4697B13d62De1E850Cc89EbE23633", "arbitrum"),
-        borrowed: ()=>({})
-    }
+    optimism: compoundExports('0x91c471053bA4697B13d62De1E850Cc89EbE23633', 'optimism'),
+    arbitrum: compoundExports('0x91c471053bA4697B13d62De1E850Cc89EbE23633', 'arbitrum'),
 }
