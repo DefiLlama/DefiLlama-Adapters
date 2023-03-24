240e986820a7c423b73bf574a9671b3ef2941e20
0xngmi <0xngmi@protonmail.com>
2023-03-24 17:09:41 +0000

    avoid using rpc for mute

--- projects/mute/index.js
+++ projects/mute/index.js
@@ -1,11 +1,20 @@
-const { getChainTvl } = require('../helper/getUniSubgraphTvl');
+const { default: request, gql } = require('graphql-request');
+const { toUSDTBalances } = require('../helper/balances');
+
+async function tvl(){
+    const res = await request("https://graph.mute.io/subgraphs/name/mattt21/muteswitch_mainnet", gql`{
+        muteSwitchFactories{
+          totalLiquidityUSD
+        }
+      }`)
+    return toUSDTBalances(res["muteSwitchFactories"][0].totalLiquidityUSD)
+}
 
 module.exports = {
     misrepresentedTokens: true,
+    timetravel: false,
     era: {
-      tvl: getChainTvl({
-        era: "https://graph.mute.io/subgraphs/name/mattt21/muteswitch_mainnet"
-      }, "muteSwitchFactories")("era"),
+      tvl
     },
     methodology:
       "Counts liquidity in pools",
