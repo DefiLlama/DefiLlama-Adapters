72327491ea0945edc5373fcd3b4d0ae969dfa32b
adamrturman <adamrturman@gmail.com>
2023-03-24 14:37:50 -0500

    add axlUSDC to Kava Lend TVL

--- projects/hard.js
+++ projects/hard.js
@@ -9,6 +9,7 @@ const BUSD_DENOM = "busd";
 const XRPB_DENOM = "xrpb";
 const ATOM_DENOM = "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"
 const AKT_DENOM = "ibc/799FDD409719A1122586A629AE8FCA17380351A51C1F47A80A1B8E7F2A491098"
+const AXLUSDC_DENOM = "erc20/axelar/usdc"
 const coingeckoIds = {
     [KAVA_DENOM]: 'kava',
     [HARD_DENOM]:'kava-lend',
@@ -18,7 +19,8 @@ const coingeckoIds = {
     [BUSD_DENOM]:'binance-usd',
     [XRPB_DENOM]:'ripple',
     [ATOM_DENOM]:'cosmos',
-    [AKT_DENOM]:'akash-network'
+    [AKT_DENOM]:'akash-network',
+    [AXLUSDC_DENOM]: 'axlusdc'
 }
 const decimals = {
     [KAVA_DENOM]: 6,
@@ -29,7 +31,8 @@ const decimals = {
     [BUSD_DENOM]:8,
     [XRPB_DENOM]:8,
     [ATOM_DENOM]:6,
-    [AKT_DENOM]:6
+    [AKT_DENOM]:6,
+    [AXLUSDC_DENOM]:6,
 }
 
 var tvl = async () => {
@@ -40,6 +43,7 @@ var tvl = async () => {
         const borrowed = Number(totalBorrowed.result.find(item=>item.denom === coin.denom)?.amount || 0);
         balances[coingeckoIds[coin.denom]]=(Number(coin.amount)-borrowed)/(10**decimals[coin.denom]);
     }
+
     return balances;
 }
 
