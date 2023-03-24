ce90a86d01316c0880b8f87d7c907b2257e0137f
Elliot Shiu <sandbochs@users.noreply.github.com>
2023-03-23 20:36:53 -0700

    add more comments

--- eslint-rules/missing-defined-addresses-require.js
+++ eslint-rules/missing-defined-addresses-require.js
@@ -11,10 +11,10 @@ module.exports = {
   },
   create: (context) => {
     const cwd = context.getCwd();
-    const fileDir = path.parse(context.getPhysicalFilename()).dir;
-    const constantsPath = path.resolve(cwd, 'utils/constants.js');
-    const relativePath = path.relative(fileDir, constantsPath);
-    let isRequired = false;
+    const fileDir = path.parse(context.getPhysicalFilename()).dir; // the path to the file being linted (without filename)
+    const constantsPath = path.resolve(cwd, 'utils/constants.js'); // full path to constants.js
+    const relativePath = path.relative(fileDir, constantsPath); // relative path from file being linted to utils/constants.js
+    let isImported = false; // flag, is utils/constants.js imported already
 
     return {
       // Checks all require statements to see if utils/constants.js is required and DEFINED_ADDRESSES is imported
@@ -27,7 +27,7 @@ module.exports = {
           for (const prop of props) {
             // check each imported property for a match
             if (prop.value.name === 'DEFINED_ADDRESSES') {
-              isRequired = true;
+              isImported = true;
             }
           }
         }
@@ -37,10 +37,10 @@ module.exports = {
       MemberExpression: (node) => {
         // Get the parent object's name
         const parentObject = node.property.parent?.object?.name;
-        if (parentObject === 'DEFINED_ADDRESSES' && !isRequired) {
+        if (parentObject === 'DEFINED_ADDRESSES' && !isImported) {
           // Fix if DEFINED_ADDRESSES is the parent object and utils/constants is not required
           const fix = (fixer) => {
-            isRequired = true;
+            isImported = true;
 
             // Insert the require statement at the top of the file
             const requireStatement = `const { DEFINED_ADDRESSES } = require('${relativePath}');\n`;
--- eslint-rules/no-contract-address-literals.js
+++ eslint-rules/no-contract-address-literals.js
@@ -11,7 +11,7 @@ module.exports = {
   meta: {
     type: 'problem',
     docs: {
-      description: 'Disallow use of string literals of addresses with a defined constant.',
+      description: 'Disallow use of string literals of addresses with a constant defined in utils/constants.js.',
       recommended: true
     },
     fixable: 'code'
@@ -20,20 +20,27 @@ module.exports = {
     return {
       Literal(node) {
         if (typeof node.value !== 'string') {
+          // Skip nodes that are not strings
           return
-        } 
+        }
 
-        const nodeVal = node.value.toLowerCase();
+        const nodeVal = node.value.toLowerCase(); // potential address literal
         if (addresses.has(nodeVal)) {
+          // check if literal is an object key, the fix will include brackets
           const isObjectKey = node.parent?.type === 'Property' && node.parent.key === node;
 
+          // fetch the symbol of the address (object key)
           const symbol = addressToSymbol.get(nodeVal);
 
           const fix = (fixer) => {
             let constantReference = `DEFINED_ADDRESSES.${symbol}`;
             if (isObjectKey) {
+              // object keys require brackets eg:
+              // { [DEFINED_ADDRESSES.USDC]: 'stable' }
               constantReference = `[${constantReference}]`;
             }
+
+            // the character range of the detected node
             const start = node.range[0];
             const end = node.range[1];
 
