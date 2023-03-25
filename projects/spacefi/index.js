fa26880e1615e360dbc2c77bc9a10743759674e1
terry <terry>
2023-03-26 00:31:58 +0800

    SpaceFi Add ZkSync Info

--- projects/spacefi/index.js
+++ projects/spacefi/index.js
@@ -9,4 +9,8 @@ module.exports = {
       useDefaultCoreAssets: true,
     })
   },
+  era: {
+    tvl: getUniTVL({ factory: '0x0700Fb51560CfC8F896B2c812499D17c5B0bF6A7', useDefaultCoreAssets: true,})
+  },
 }; 
+
