e9f090f20b49299f55ec59ff21f85779dbd5504b
ArrayCapital <128329694+ArrayCapital@users.noreply.github.com>
2023-03-27 23:15:13 +0200

    Create index.js
    adapter

---
+++ projects/array.capital/index.js
@@ -0,0 +1,11 @@
+const sdk = require('@defillama/sdk');
+const { staking } = require('./staking')
+
+module.exports = {
+  arbitrum: {
+  // polygon: {
+    tvl: staking('0xA55Cb77E8CeBc3fe517044d0AaA923d541a69e71', '0x53Fd70B568e5C8DACe2cE3c38E650F5924BeB1c1')
+    // tvl: staking('0x136B45d68352F45737222693bBE54a667B9111F6', '0xf60d1C5476da2405EA83cc8d10Cd1cb27d7D6F1E')
+
+  }
+}
