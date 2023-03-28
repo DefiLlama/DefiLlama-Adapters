1401874016c1d1b3c9715cc524aaa95ab339f043
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-28 20:54:37 +0200

    code refactor

--- projects/tegisto/index.js
+++ projects/tegisto/index.js
@@ -1,32 +1,8 @@
-const { ethers } = require("ethers");
-const sdk = require("@defillama/sdk");
-
-// Tegisto stake addresses
-const stakeContractAddresses = {
-  kava: '0x744Dd9f79b80437a9e5eb0292128045F51C48b6d',
-};
-
-function stakeFunction(chain) {
-  return async function tvl(timestamp, block, chainBlocks) {
-    var stakeBalance = (await sdk.api.abi.call({
-      target: stakeContractAddresses[chain],
-      abi: 'erc20:totalSupply',
-      block: chainBlocks[chain],
-      chain: chain
-    })).output;
-
-    var staked = ethers.utils.formatEther(stakeBalance);
-    return {
-      'tegisto': staked,
-    };
-  }
-}
+const { staking } = require("../helper/staking")
 
 module.exports = {
-  methodology: 'TVL counts staked TGS tokens on the platform itself. CoinGecko is used to find the price of tokens in USD.',
-  timetravel: false,
   kava: {
-    tvl:() => 0,
-    staking: stakeFunction('kava'),
+    tvl: () => 0,
+    staking: staking('0x744Dd9f79b80437a9e5eb0292128045F51C48b6d', '0x87F1E00d6bcD3712031e5edD26DFcdB0FEd35D20', undefined, 'tegisto', 18),
   }
-};
+}
