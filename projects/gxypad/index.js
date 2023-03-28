8c73ca2c687c1f2a5bacf5e4d6cf100afc5710b8
SpaceDogeChain <contact@gxypad.com>
2023-03-29 02:58:13 +0800

    Update index.js

--- projects/gxypad/index.js
+++ projects/gxypad/index.js
@@ -4,7 +4,7 @@ module.exports = {
   misrepresentedTokens: true,
   era: {
     tvl: getUniTVL({
-      factory: '0x065c8703132F2A38Be3d2dbF7Be6BE455930560c',
+      factory: '0x2da10A1e27bF85cEdD8FFb1AbBe97e53391C0295',
       chain: 'era',
       useDefaultCoreAssets: true,
     })
