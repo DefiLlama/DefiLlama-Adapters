119f40aac27e9643634c87199e726bb3419f39a8
My MetaTrader <124645892+mymetatrader@users.noreply.github.com>
2023-03-28 15:03:46 +0700

    Update staking for mymetatrader

--- projects/mymetatrader/index.js
+++ projects/mymetatrader/index.js
@@ -1,13 +1,21 @@
-const { sumTokensExport } = require('../helper/unwrapLPs')
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const { staking } = require("../helper/staking");
 
 const vault = "0x992EB7040b66b13abEa94E2621D4E61d5CE608BD";
 const usdc = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8";
 
+const stakingPool = "0xBdB00022030C9D715A10F2fCeDb19e99020Aa357";
+const mmtToken = "0x27d8de4c30ffde34e982482ae504fc7f23061f61";
+
 module.exports = {
   arbitrum: {
-    tvl: sumTokensExport({ tokens: [usdc], owner: vault }),
+    tvl: sumTokensExport({
+      tokensAndOwners: [
+        [usdc, vault],
+        [mmtToken, stakingPool],
+      ],
+    }),
+    staking: staking(stakingPool, mmtToken),
   },
-  hallmarks: [
-    [1677765600, "Closed mainnet"],
-  ],
+  hallmarks: [[1677765600, "Closed mainnet"]],
 };
