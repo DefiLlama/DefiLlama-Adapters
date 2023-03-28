933d95eff505a483034af8342100f99f88ae9524
ArrayCapital <128329694+ArrayCapital@users.noreply.github.com>
2023-03-28 16:13:02 +0200

    Update index.js
    Fixed tvl calculation

--- projects/array.capital/index.js
+++ projects/array.capital/index.js
@@ -1,9 +1,23 @@
 const sdk = require('@defillama/sdk');
-const { staking } = require('./staking')
+
+const token = "0x53Fd70B568e5C8DACe2cE3c38E650F5924BeB1c1";
+const masterchef = "0xA55Cb77E8CeBc3fe517044d0AaA923d541a69e71";
+
+const stakingToken = token.toLowerCase();
+async function getTvl(chainBlocks) {
+
+  let locked = (await sdk.api.erc20.balanceOf({
+    target: token,
+    owner: masterchef,
+    block: chainBlocks.arbitrum,
+    chain: 'arbitrum'
+  })).output;
+
+  return locked;
+}
 
 module.exports = {
   arbitrum: {
-    tvl: staking('0xA55Cb77E8CeBc3fe517044d0AaA923d541a69e71', '0x53Fd70B568e5C8DACe2cE3c38E650F5924BeB1c1')
-
+    tvl: getTvl
   }
 }
