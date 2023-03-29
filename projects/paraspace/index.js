1fbf25e46890331dfbd4671962a79ccbaa235a25
0xngmi <0xngmi@protonmail.com>
2023-03-29 13:34:27 +0100

    paraspace hack

--- projects/paraspace/index.js
+++ projects/paraspace/index.js
@@ -2,7 +2,7 @@ const { tvl, borrowed } = require("./helper/index");
 
 module.exports = {
   hallmarks: [
-    [Math.floor(new Date('2023-03-17') / 1e3), 'Protocol was hacked'],
+    [Math.floor(new Date('2023-03-17') / 1e3), 'Whitehat hack'],
   ],
   methodology: `TVL includes ERC-20 and ERC-721 tokens that have been supplied as well as ERC-20 tokens that are supplied for lending.`,
   ethereum: {
