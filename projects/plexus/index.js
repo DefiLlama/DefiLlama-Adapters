69c360c4a8bae7ef8b0fec3a24dce8194829a4ca
Plexus-Olivia <olivia@plexus.app>
2023-03-30 13:39:22 +0900

    Modify daily totalfee by chain

--- projects/plexus/index.js
+++ projects/plexus/index.js
@@ -7,14 +7,32 @@ const CHAIN = {
   250: "fantom",
 };
 
-const getFee = async (chainId) => {
-  const { data } = await axios.get("https://api.plexus.app/v1/dashboard/fee");
+const getFee = async (chainId, formattedDate) => {
+  const { data } = await axios.get(
+    `https://api.plexus.app/v1/dashboard/fee?date=${formattedDate}`
+  );
   return data.data[chainId];
+};
+
+const timeStampToDate = (timestamp) => {
+  const date = new Date(timestamp);
+
+  const year = date.getUTCFullYear();
+  const month = ("0" + (date.getUTCMonth() + 1)).slice(-2);
+  const day = ("0" + date.getUTCDate()).slice(-2);
+
+  return `${year}-${month}-${day}`;
 };
 
 module.exports = Object.entries(CHAIN).reduce(
   (result, [chainId, chainName]) => {
-    result[chainName] = { fetch: () => getFee(chainId) };
+    result[chainName] = {
+      fetch: async (timestamp) => {
+        const formattedDate = timeStampToDate(timestamp);
+
+        return await getFee(chainId, formattedDate);
+      },
+    };
     return result;
   },
   {}
