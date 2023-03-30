028a06aba769f2c3a7f91e948d689dacd4ff8ba8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-30 10:56:27 +0200

    yama: code refactor

--- projects/yama-finance/index.js
+++ projects/yama-finance/index.js
@@ -1,13 +1,26 @@
-const { treasuryExports } = require("../helper/treasury");
+const { getLogs } = require('../helper/cache/getLogs')
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
-module.exports = treasuryExports({
-  arbitrum: {
-    tokens: [ 
-        '0x9dbbbaecacedf53d5caa295b8293c1def2055adc',//mooGLP
-        "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",//usdt
-     ],
-    owners: ["0x1cd97ee98f3423222f7b4cddb383f2ee2907e628", "0x0e1Ddf8D61f0570Bf786594077CD431c727335A9"],
-  },
-})
+async function tvl(_, _b, _cb, { api, }) {
+  const CDP = '0x1cd97ee98f3423222f7b4cddb383f2ee2907e628'
+  const logs = await getLogs({
+    api,
+    target: CDP,
+    topic: 'AddCollateralType(uint256,address,address,uint256,uint256,uint256,uint256,bool,bool)',
+    eventAbi: 'event AddCollateralType(uint256 indexed collateralTypeId, address indexed token, address priceSource, uint256 debtFloor, uint256 debtCeiling, uint256 collateralRatio, uint256 interestRate, bool borrowingEnabled, bool allowlistEnabled)',
+    onlyArgs: true,
+    fromBlock: 72920436,
+  })
+
+  const ownerTokens = [
+    [['0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9'], '0x0e1Ddf8D61f0570Bf786594077CD431c727335A9'], // psm module
+  ]
+
+  ownerTokens.push([logs.map(i => i.token), CDP])
+
+  return sumTokens2({ api, ownerTokens})
+}
+
+module.exports = { arbitrum: { tvl }}
 module.exports.methodology = "mooGmxGLP in CDP Module and USDT in PSM";
 
