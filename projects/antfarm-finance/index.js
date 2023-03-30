2f9c944960405aa8f8d8a70f07816c0c1364f6e5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-30 15:29:18 +0200

    antfarm code refactor

--- projects/antfarm-finance/index.js
+++ projects/antfarm-finance/index.js
@@ -1,32 +1,18 @@
-const { uniTvlExport } = require("../helper/calculateUniTvl.js");
+const { getUniTVL } = require('../helper/unknownTokens')
+
+const config = {
+  arbitrum: '0x61f4ECD130291e5D5D7809A112f9F9081b8Ed3A5',
+  ethereum: '0xE48AEE124F9933661d4DD3Eb265fA9e153e32CBe',
+  polygon_zkevm: '0x8aF94528FBE3c4C148523E7aAD48BcEbcC0A71d7',
+}
 
 module.exports = {
   misrepresentedTokens: true,
-  arbitrum: {
-    tvl: uniTvlExport(
-      "0x61f4ECD130291e5D5D7809A112f9F9081b8Ed3A5",
-      "arbitrum",
-      undefined,
-      undefined,
-      { hasStablePools: false, useDefaultCoreAssets: true }
-    ),
-  },
-  ethereum: {
-    tvl: uniTvlExport(
-      "0xE48AEE124F9933661d4DD3Eb265fA9e153e32CBe",
-      "ethereum",
-      undefined,
-      undefined,
-      { hasStablePools: false, useDefaultCoreAssets: true }
-    ),
-  },
-  polygon_zkevm: {
-    tvl: uniTvlExport(
-      "0x8aF94528FBE3c4C148523E7aAD48BcEbcC0A71d7",
-      "polygon_zkevm",
-      undefined,
-      undefined,
-      { hasStablePools: false, useDefaultCoreAssets: true }
-    ),
-  },
 };
+
+Object.keys(config).forEach(chain => {
+  const factory = config[chain]
+  module.exports[chain] = {
+    tvl: getUniTVL({ factory, useDefaultCoreAssets: true, fetchBalances: true, })
+  }
+})
