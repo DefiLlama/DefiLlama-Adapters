d7ea0313575979bb879bd05a1e2b0c0de35cbe93
Vitali <vitali@undercurrent.tech>
2023-03-30 20:41:16 +0400

    Update tvl for ethereum on algorai-finance.

--- projects/algorai-finance/constants.js
+++ projects/algorai-finance/constants.js
@@ -63,6 +63,20 @@ const vaults = [
     },
 ];
 
+const state = {
+    ethereum: {
+        contractAddress: "0x2103dE019522B65BbbD051C8Ce9426AA9005A655",
+        tokens: [
+            {
+                name: "usd-coin",
+                address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+                decimals: 6
+            },
+        ]
+    },
+}
+
 module.exports = {
     vaults,
+    state
 };
