fd6e2dda74af687de1aa534c8330e39dcd08a13f
Vitali <vitali@undercurrent.tech>
2023-03-31 07:00:22 +0400

    Update tvl for ethereum on algorai-finance.

--- projects/algorai-finance/index.js
+++ projects/algorai-finance/index.js
@@ -2,6 +2,7 @@ const sdk = require('@defillama/sdk')
 
 const {
   vaults,
+  state
 } = require("./constants");
 const { readGlobalState } = require("./utils");
 
@@ -22,10 +23,43 @@ async function tvl() {
   return balances
 }
 
+/**
+ *
+ * @param decimals
+ * @param n
+ * @returns {number}
+ */
+const toNumber = (decimals, n) => Number(n)/Math.pow(10, decimals)
+
+/**
+ *
+ * @param chain
+ * @returns {function(*, *, *): Promise<{}>}
+ */
+function getTVLFunction(chain)
+{
+  return async function tvl(timestamp, ethBlock, chainBlocks) {
+    const balances = {}
+    const chainData = state[chain];
+
+    const block = chainBlocks[chain];
+    for (const token of chainData.tokens) {
+      const balance = await sdk.api.erc20.balanceOf({
+        block, chain, target: token.address, owner: chainData.contractAddress
+      });
+
+      sdk.util.sumSingleBalance(balances, token.name, toNumber(token.decimals, balance.output));
+    }
+    return balances
+  }
+}
 
 module.exports = {
   timetravel: false,
   algorand: {
     tvl,
+  },
+  ethereum: {
+    tvl: getTVLFunction('ethereum'),
   },
 };
