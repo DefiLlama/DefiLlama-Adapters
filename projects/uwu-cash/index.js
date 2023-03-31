7bc0f951cc5626cb4ccab3acf80410042d273067
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-31 12:26:40 +0200

    add zksync to deri

--- projects/uwu-cash/index.js
+++ projects/uwu-cash/index.js
@@ -4,7 +4,7 @@ const { sumTokensExport, nullAddress } = require('../helper/sumTokens')
 module.exports = {
   stacks: {
     tvl: sumTokensExport({
-      owners: ['SP2AKWJYC7BNY18W1XXKPGP0YVEK63QJG4793Z2D4.uwu-factory-v1'],
+      owners: ['SP2AKWJYC7BNY18W1XXKPGP0YVEK63QJG4793Z2D4.uwu-factory-v1-1-0'],
       tokens: [nullAddress]
     }),
   }
