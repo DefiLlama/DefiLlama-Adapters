541f4f2c008de3dbc518e587757ed9a8d2ca253b
velocore <velocoredao@gmail.com>
2023-03-31 14:13:47 +0900

    velocore

--- projects/velocore/index.js
+++ projects/velocore/index.js
@@ -3,7 +3,7 @@ const { getUniTVL } = require('../helper/unknownTokens');
 module.exports = {
   misrepresentedTokens: true,
   era: {
-    tvl: getUniTVL({ factory: '', useDefaultCoreAssets: true, hasStablePools: true })
+    tvl: getUniTVL({ factory: '0xF5719b1Ea3C9bF6491E22C49379E31060d0FbFc1', useDefaultCoreAssets: true, hasStablePools: true })
   },
   methodology: "Counts liquidity in pools",
 };
