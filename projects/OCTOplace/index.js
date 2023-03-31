f0608c5d0ca42b83041d9d59db109b0d6b0447bd
OmiAkk <badzan00@mail.ru>
2023-03-31 23:19:07 +0600

    Added USDC
    OCTOplace TVL now also includes USDC

--- projects/OCTOplace/index.js
+++ projects/OCTOplace/index.js
@@ -3,7 +3,8 @@ const { sumTokensExport } = require("../helper/unwrapLPs");
 
 const contract = "0x61F91266F6abEA61447E00EB781A3c38a3D1b925";
 const KAVA = "0x0000000000000000000000000000000000000000";
+const USDC = "0xfa9343c3897324496a05fc75abed6bac29f8a40f";
 
 module.exports = {
-  kava: { tvl: sumTokensExport({ owner: contract, tokens: [KAVA] }) },
+  kava: { tvl: sumTokensExport({ owner: contract, tokens: [KAVA, USDC] }) },
 };
