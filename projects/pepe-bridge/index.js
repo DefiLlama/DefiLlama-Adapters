77ac9ca877e8204af2486dbd07ce87bfba095c4e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-03 17:46:26 +0200

    minor refactor

--- projects/pepe-bridge/index.js
+++ projects/pepe-bridge/index.js
@@ -1,22 +1,12 @@
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs');
-const { chainAddresses } = require('./constants');
+const { nullAddress, sumTokensExport, } = require('../helper/unwrapLPs');
 
-const getTVLFunction = (chain) => async (_timestamp, _block, chainBlocks) => {
-  const contracts = Object.keys(chainAddresses[chain]);
-  // tokens by chain must be unique
-  return contracts.reduce(async (balances, contract) => {
-    const tokens = chainAddresses[chain][contract];
-    return { ...(await balances), ...(await sumTokens2({ chain, block: chainBlocks[chain], tokens, owner: contract })) };
-  }, {});
-};
+const config = {
+  ethereum: [
+    [[nullAddress], '0x882260324AD5A87bF5007904B4A8EF87023c856A']
+  ]
+}
+module.exports = {};
 
-module.exports = {
-  methodology: "All tokens locked in Bridge contracts by PepeTeam.",
-  timetravel: false,
-};
-
-Object.keys(chainAddresses).forEach(chain => {
-  module.exports[chain] = {
-    tvl: getTVLFunction(chain),
-  }
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = { tvl: sumTokensExport({ ownerTokens: config[chain] }) }
 })
