a48adbde4a9e64155af2143453d3881a0dae525c
0xngmi <0xngmi@protonmail.com>
2023-04-04 03:02:41 +0100

    fetch token list automatically

--- projects/helper/treasury.js
+++ projects/helper/treasury.js
@@ -1,14 +1,24 @@
 const { sumTokensExport, nullAddress, } = require('../helper/sumTokens')
+const { covalentGetTokens } = require('./http')
 
 function treasuryExports(config) {
   const chains = Object.keys(config)
   const exportObj = {  }
   chains.forEach(chain => {
-    let { ownTokenOwners = [], ownTokens, owners = [],  } = config[chain]
+    let { ownTokenOwners = [], ownTokens, owners = [], fetchTokens = false } = config[chain]
     if (chain === 'solana')  config[chain].solOwners = owners
     const tvlConfig = { ...config[chain] }
     tvlConfig.blacklistedTokens = ownTokens
-    exportObj[chain] = { tvl: sumTokensExport(tvlConfig) }
+    if(fetchTokens === true){
+      exportObj[chain] = { tvl: async (_, _b, _cb, { api }) => {
+        const tokens = await Promise.all(owners.map(address=>covalentGetTokens(address, chain)))
+        const uniqueTokens = new Set([...config[chain].tokens, ...tokens.flat()])
+        tvlConfig.tokens = Array.from(uniqueTokens)
+        return sumTokensExport(tvlConfig)(_, _b, _cb, api)
+      }}
+    } else {
+      exportObj[chain] = { tvl: sumTokensExport(tvlConfig) }
+    }
 
     if (ownTokens) {
       const { solOwners, ...otherOptions } = config[chain]
