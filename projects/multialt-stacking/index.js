bc3786e39046b751433b47864b535fa0ad6fc465
devcexdex <dev.cexdex@gmail.com>
2023-04-04 09:41:30 +0530

    Added new stacking for Multialt

--- projects/multialt-stacking/index.js
+++ projects/multialt-stacking/index.js
@@ -1,14 +1,14 @@
 const { sumTokensExport } = require("../helper/unwrapLPs");
 
-const token_USDC = "0xfA9343C3897324496A05fC75abeD6bAC29f8A40f";
-const token_USDT = "0xB44a9B6905aF7c801311e8F4E76932ee959c663C";
 const masterchef = "0xFFd33A71411fbA8B989Eca9F99EE7a908a2dEf4F";
+const token_USDT = "0xB44a9B6905aF7c801311e8F4E76932ee959c663C";
+const token_USDC = "0xfA9343C3897324496A05fC75abeD6bAC29f8A40f";
 
 module.exports = {
   kava: {
     tvl: sumTokensExport({
       owner: masterchef,
-      tokens: [token_USDC, token_USDT]
+      tokens: [token_USDT, token_USDC]
     })
   }
 }
