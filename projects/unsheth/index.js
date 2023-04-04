eef195473e9793f8fbc5364fc97630ca527c29fa
thepiandao <piandao@unsheth.xyz>
2023-04-05 00:51:34 +0530

    updated comments

--- projects/unsheth/index.js
+++ projects/unsheth/index.js
@@ -1,6 +1,9 @@
 const sdk = require("@defillama/sdk");
+
+// upgraded LSDVault V2
 const LSDVAULT_CONTRACT = "0x51A80238B5738725128d3a3e06Ab41c1d4C05C74";
 
+// list of all the supported LSDs deposited in vault
 const wstETHAddress = "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0";
 const sfrxETHAddress = "0xac3e018457b222d93114458476f3e3416abbe38f";
 const rETHAddress = "0xae78736cd615f374d3085123a210448e74fc6393";
