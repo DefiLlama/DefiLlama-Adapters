8add95e9b318cd844eccaa091976e7315e489aa9
David Josephs <davidjosephs12@gmail.com>
2023-04-04 14:19:03 -0700

    Update parcl tvl.

--- projects/parcl/index.js
+++ projects/parcl/index.js
@@ -7,6 +7,13 @@ const collateralVaults = [
   "9rUUoPJhdbZp4GqraxpjvNHJRayAX3ao2ZvBQqeNpQZ4",
   "HuxKhSY4ZjAFLS1PQMrTavrmXQHtRjtAPyEfRsuTMhgL",
   "EJ77XK6z1RhHdPgRAP3RuYDeTdKF5grb929awqM3Qo7k",
+  "5hHfPfyvgqLUy7YKNifmkRLGR3TsEtkPePcD4fK3Cf3U",
+  "7Si7o3jSvTkd5XKfLAG1j5jJsVyf9xGF41VAfZ5J2WMH",
+  "6vPhHq5dmxQ1TReVbP6KZLuZSWBTCZYD5xewgodiQtpr",
+  "FAkAaRUwGBXWcVXgKPm7tLrtKRumDKL85o2sc6GiJB2q",
+  "CAYRqwXJnfY6rvdEr5764Mzpx3PwNvh8kjidKYPkFwmq",
+  "3mE1oiFdLs8zntSk8bPLLY6Cq1vf5tbHpnyywunFysGZ",
+  "AWoQgYmaNDUERUePyo6f5LkxA9ozmjKg8MmKtfJn33Hb",
 ];
 
 async function tvl() {
