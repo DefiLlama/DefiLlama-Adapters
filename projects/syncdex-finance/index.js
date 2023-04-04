e36e539850ed4fef87783322bca93914e4a343ff
devsyncdex <dev@syncdex.finance>
2023-04-04 22:23:11 +0700

    The purpose of the project will be shifted to focus on staking and farming, therefore, the TVL will be rebuilt by counting the assets in the staking contract.

--- projects/syncdex-finance/index.js
+++ projects/syncdex-finance/index.js
@@ -1,5 +1,4 @@
 const sdk = require("@defillama/sdk");
-const abi = require("./abi.json");
 
 const staking_contract = "0xa8CD01322Ad632c9656879e99Fd7FbC11ca8E3BB";
 
@@ -9,30 +8,6 @@ const assets = [
   "0x3a34FA9a1288597Ad6C1Da709f001D37FeF8b19e",
   "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
 ];
-
-const stakingTvl = async (timestamp, eraBlock, chainBlocks) => {
-  const balances = {};
-
-  const token = (
-    await sdk.api.abi.call({
-      abi: abi.token,
-      target: staking_contract,
-      block: eraBlock,
-    })
-  ).output;
-
-  const currentTotalStake = (
-    await sdk.api.abi.call({
-      abi: abi.currentTotalStake,
-      target: staking_contract,
-      block: eraBlock,
-    })
-  ).output;
-
-  sdk.util.sumSingleBalance(balances, token, currentTotalStake);
-
-  return balances;
-};
 
 async function eraTvl(timestamp, eraBlock, chainBlocks) {
   let balances = {};
@@ -57,6 +32,5 @@ module.exports = {
   methodology: `Counts SWAP tokens locked int the staking contract(0xa8CD01322Ad632c9656879e99Fd7FbC11ca8E3BB). Regular TVL counts SDYX, USDC, and WETH that are also in the staking contract(these tokens may have been sent to the contract by accident).`,
   era: {
     tvl: eraTvl,
-    staking: stakingTvl
   },
 };
