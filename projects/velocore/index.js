87da2bb2b623de20bda95928e9c2fe5911b0bf37
velocore <128014130+velocore@users.noreply.github.com>
2023-04-05 15:17:54 +0900

    Velocore Dex contract v2 update (#5807)
    * velocore
    
    * velocore
    
    * velocore
    
    * velocore
    
    * v2 contract

--- projects/velocore/index.js
+++ projects/velocore/index.js
@@ -1,9 +1,11 @@
 const { getUniTVL } = require('../helper/unknownTokens');
-
+const sdk = require('@defillama/sdk')
+const factv1=getUniTVL({ factory: '0xF5719b1Ea3C9bF6491E22C49379E31060d0FbFc1', useDefaultCoreAssets: true, hasStablePools: true });
+const factv2=getUniTVL({ factory: '0xE140EaC2bB748c8F456719a457F26636617Bb0E9', useDefaultCoreAssets: true, hasStablePools: true });
 module.exports = {
   misrepresentedTokens: true,
   era: {
-    tvl: getUniTVL({ factory: '0xF5719b1Ea3C9bF6491E22C49379E31060d0FbFc1', useDefaultCoreAssets: true, hasStablePools: true })
+    tvl: sdk.util.sumChainTvls([factv1, factv2]),
   },
   methodology: "Counts liquidity in pools",
 };
