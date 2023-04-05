28045626ad7ff53b2631b88f0a00f504869e1e49
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2023-04-05 21:25:00 +0100

    Update index.js
    refactor code to use staking helper

--- projects/TWTStake/index.js
+++ projects/TWTStake/index.js
@@ -1,20 +1,6 @@
-const sdk = require('@defillama/sdk');
+const { staking } = require("../helper/staking");
 const TWT_STAKE_CONTRACT = '0x5e7c3c55eb5c0ee10817d70e414f4b1ee22d5ce3';
 const TWT_TOKEN_CONTRACT = '0x4b0f1812e5df2a09796481ff14017e6005508003';
-
-async function tvl(_, _1, _2, { api }) {
-  const balances = {};
-
-  const contractBalance = await api.call({
-    abi: 'contract:getContractBalance',
-    target: TWT_STAKE_CONTRACT,
-    params: [],
-  });
-
-  await sdk.util.sumSingleBalance(balances, TWT_TOKEN_CONTRACT, contractBalance, api.chain)
-
-  return balances;
-}
 
 module.exports = {
   timetravel: true,
@@ -22,6 +8,6 @@ module.exports = {
   methodology: 'Counts the number of TWT tokens in the TWT Stake contract.',
   start: 1000235,
   bsc: {
-    tvl,
+    tvl: staking(TWT_STAKE_CONTRACT,TWT_TOKEN_CONTRACT,"bsc")
   }
 };
