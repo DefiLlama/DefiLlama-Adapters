e974bca26e9d23ce747f8edc0d3aae865c7f7333
0xctrlaltape <orbitalapes@gmail.com>
2023-04-05 22:15:29 +0400

    add forge

---
+++ projects/forge/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  evmos: { factory: '0xf544365e7065966f190155F629cE0182fC68Eaa2', fromBlock: 12367456, },
+})
