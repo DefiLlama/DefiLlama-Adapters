8f81dd9bd9c029cad5fbdd83ab2b27def116b79c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-06 17:19:38 +0200

    syncdex: refactor code

--- projects/syncdex-finance/index.js
+++ projects/syncdex-finance/index.js
@@ -10,13 +10,15 @@ const sydx_usdc_farming_contract = "0x75aC887df149076b7DF4deAA0267711475b71572";
 const assets = [
   "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
   "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
-  "0x3a34FA9a1288597Ad6C1Da709f001D37FeF8b19e", // We have SYDX pool so we add here
   "0x0000000000000000000000000000000000000000" // This is address of native token (ETH), you can check native_staking_contract
 ];
+const SYNC_DEX = '0x3a34FA9a1288597Ad6C1Da709f001D37FeF8b19e'
+
+const owners = [native_staking_contract, usdc_staking_contract, sydx_staking_contract, native_usdc_farming_contract, native_sydx_farming_contract, sydx_usdc_farming_contract]
 
 module.exports = {
   era: {
-    tvl: sumTokensExport({ owners: [native_staking_contract, usdc_staking_contract, sydx_staking_contract, native_usdc_farming_contract, native_sydx_farming_contract, sydx_usdc_farming_contract], tokens: assets}),
-    staking: sumTokensExport({ owners: [native_staking_contract, usdc_staking_contract, sydx_staking_contract, native_usdc_farming_contract, native_sydx_farming_contract, sydx_usdc_farming_contract], tokens: assets}),
+    tvl: sumTokensExport({ owners, tokens: assets }),
+    staking: sumTokensExport({ owners, tokens: [SYNC_DEX] }),
   },
 };
