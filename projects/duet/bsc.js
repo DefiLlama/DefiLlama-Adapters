3930f5d768eda9d80eb95356d2e18c2a3d543608
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-06 12:40:37 +0200

    Separate own tokens from tvl

--- projects/duet/bsc.js
+++ projects/duet/bsc.js
@@ -5,9 +5,11 @@ const { getUniqueAddresses } = require('../helper/utils')
 
 const BigNumber = require("bignumber.js");
 const { sumTokens2 } = require("../helper/unwrapLPs");
+const { staking } = require("../helper/staking");
 
 const TOKEN_LIST_URL = "https://app.duet.finance/tokens.json";
 const COLLATERAL_READER_CONTRACT = "0xFfB8FD0E5eA13bb71401B19e00f9F934746f0b7A";
+const DUET = '0x95EE03e1e2C5c4877f9A298F1C0D6c98698FAB7B'
 
 async function getEBCakeTvl(api) {
   const EBCAKE_READER_CONTRACT = "0x243F8da5893E534CBd25220b6E277420dd9dE77B";
@@ -19,7 +21,7 @@ async function getEBCakeTvl(api) {
   api.add('0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', ret.allEbStacked)
 }
 
-async function fetch(_, _1, _2, { api }) {
+async function tvl(_, _1, _2, { api }) {
   const ret = await getConfig("duet-fi", TOKEN_LIST_URL);
   const vaultList = [];
   for (const token of ret) {
@@ -52,7 +54,7 @@ async function fetch(_, _1, _2, { api }) {
   // api.add('tether', (await getEBCakeTvl(api)) / 1e8, { skipChain: true })
 
   await getEBCakeTvl(api)
-  return sumTokens2({ tokensAndOwners, api, resolveLP: true,})
+  return sumTokens2({ tokensAndOwners, api, resolveLP: true, blacklistedTokens: [DUET]})
 
   // const tokenTvls = await api.multiCall({ abi: collateralReaderContractABI.depositVaultValues, target: COLLATERAL_READER_CONTRACT, calls: uniqueVaults.map(i => ({ params: [[i], false] })) })
   // api.add('tether', (await getEBCakeTvl(api)) / 1e8, { skipChain: true })
@@ -61,4 +63,6 @@ async function fetch(_, _1, _2, { api }) {
   // })
 }
 
-module.exports = fetch;
+module.exports = {
+  tvl, staking: staking('0x29fd5bdca277eb5b8c31314e259c7feabd1badd4', DUET)
+}
