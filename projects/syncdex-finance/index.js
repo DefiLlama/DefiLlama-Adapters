4a4440d00adc2a2f1759bece6a50691dbe629e15
Dev SyncDEX <129781989+devsyncdex@users.noreply.github.com>
2023-04-06 15:24:11 +0700

    Add Adapter For SyncDEX Finance (#5823)
    * Add Contract Address
    
    * add syncdex-finance
    
    * The purpose of the project will be shifted to focus on staking and farming, therefore, the TVL will be rebuilt by counting the assets in the staking contract.
    
    * The purpose of the project will be shifted to focus on staking and farming, therefore, the TVL will be rebuilt by counting the assets in the staking contract.
    
    * code refactor
    
    * Remove the project's token.
    
    * Remove the project token owner
    
    * Add USDC token for staking contract
    
    * Add USDC, WETH token for staking contract
    
    * Update index.js
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ projects/syncdex-finance/index.js
@@ -0,0 +1,15 @@
+const { sumTokensExport } = require("../helper/unwrapLPs");
+
+const staking_contract = "0xa8CD01322Ad632c9656879e99Fd7FbC11ca8E3BB";
+
+const assets = [
+  "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
+  "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
+];
+
+module.exports = {
+  era: {
+    tvl: sumTokensExport({ owner: staking_contract, tokens: assets}),
+    staking: sumTokensExport({ owner: staking_contract, tokens: ['0x3a34FA9a1288597Ad6C1Da709f001D37FeF8b19e']}),
+  },
+};
