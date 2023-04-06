ca00afa5ac5d177fdbfc330fed2ae1f1cd54212a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-06 12:34:00 +0200

    dual: refactor code

--- projects/duet/bsc.js
+++ projects/duet/bsc.js
@@ -1,47 +1,64 @@
 const { getConfig } = require("../helper/cache");
-const sdk = require("@defillama/sdk");
 const collateralReaderContractABI = require("./abis/collateral-reader.json");
 const ebcakeReaderContractABI = require("./abis/ebcake-reader.json");
 const { getUniqueAddresses } = require('../helper/utils')
 
 const BigNumber = require("bignumber.js");
-const proPoolABI = require("./abis/pro-pool.json");
+const { sumTokens2 } = require("../helper/unwrapLPs");
 
 const TOKEN_LIST_URL = "https://app.duet.finance/tokens.json";
 const COLLATERAL_READER_CONTRACT = "0xFfB8FD0E5eA13bb71401B19e00f9F934746f0b7A";
 
-async function getEBCakeTvl() {
+async function getEBCakeTvl(api) {
   const EBCAKE_READER_CONTRACT = "0x243F8da5893E534CBd25220b6E277420dd9dE77B";
-  const ret = await sdk.api.abi.call({
+  const ret = await api.call({
     abi: ebcakeReaderContractABI.extendableBondGroupInfo,
-    chain: "bsc",
     target: EBCAKE_READER_CONTRACT,
     params: ["yearly"]
   });
-  return ret.output.faceUsdValue;
+  api.add('0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', ret.allEbStacked)
 }
 
 async function fetch(_, _1, _2, { api }) {
   const ret = await getConfig("duet-fi", TOKEN_LIST_URL);
-  const tokens = ret;
   const vaultList = [];
-  for (const token of tokens) {
+  for (const token of ret) {
     if (!token.vaults || token.vaults.length < 1) {
       continue;
     }
     vaultList.push(...token.vaults);
   }
   const uniqueVaults = getUniqueAddresses(vaultList
-    .filter((vault) => vault.displayPosition !== "DASSETS")
+    .filter((vault) => vault.dAssetsType !== "DASSETS")
     .map((vault) => vault.vaultAddress && vault.vaultAddress.trim())
     .filter(i => i))
 
-  
-  const tokenTvls = await api.multiCall({  abi: collateralReaderContractABI.depositVaultValues, target: COLLATERAL_READER_CONTRACT, calls: [...uniqueVaults].map(i => ({ params: [[i], false]}))}) 
-  api.add('tether', (await getEBCakeTvl(api))/1e8, { skipChain: true } )
-  tokenTvls.forEach(v => {
-    if (v) api.add('tether', v[1][0]/1e8, { skipChain: true })
+  const tokensAndOwners = []
+  const underlyings = await api.multiCall({ abi: 'address:underlying', calls: uniqueVaults })
+  const underlyingMap = uniqueVaults.reduce((acc, v, i) =>{
+    acc[v] = underlyings[i]
+    return acc
+  }, {})
+  const pairs = await api.multiCall({ abi: 'address:pair', calls: uniqueVaults })
+  const maybeIsSingle = uniqueVaults.filter((v, i) => {
+    if (!pairs[i]) return true
+    tokensAndOwners.push([pairs[i], underlyingMap[v]])
   })
+  const tokens = await api.multiCall({ abi: 'address:underlyingToken', calls: maybeIsSingle })
+  maybeIsSingle.forEach((v, i) => {
+    if (!tokens[i]) return;
+    tokensAndOwners.push([tokens[i], underlyingMap[v]])
+  })
+  // api.add('tether', (await getEBCakeTvl(api)) / 1e8, { skipChain: true })
+
+  await getEBCakeTvl(api)
+  return sumTokens2({ tokensAndOwners, api, resolveLP: true,})
+
+  // const tokenTvls = await api.multiCall({ abi: collateralReaderContractABI.depositVaultValues, target: COLLATERAL_READER_CONTRACT, calls: uniqueVaults.map(i => ({ params: [[i], false] })) })
+  // api.add('tether', (await getEBCakeTvl(api)) / 1e8, { skipChain: true })
+  // tokenTvls.forEach(v => {
+  //   if (v) api.add('tether', v[1][0] / 1e8, { skipChain: true })
+  // })
 }
 
 module.exports = fetch;
--- projects/duet/index.js
+++ projects/duet/index.js
@@ -2,7 +2,6 @@ const bsc = require('./bsc')
 const arbitrum = require('./arbitrum')
 
 module.exports = {
-  misrepresentedTokens: true,
   bsc: { tvl: bsc },
-  // arbitrum: { tvl: arbitrum },
+  arbitrum: { tvl: arbitrum },
 };
