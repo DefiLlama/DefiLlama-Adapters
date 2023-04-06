5b4239168a3152fe7bfb29ad4cb8ec9280f017b0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-06 17:01:32 +0200

    barnbridge: purge graph usage

--- projects/barnbridge/index.js
+++ projects/barnbridge/index.js
@@ -1,44 +1,32 @@
-const { getPastAndActiveTerms } = require("./subgraph.js");
-const { liquidityProviderBalancesForTerms } = require("./numbers.js");
+const { getLogs } = require('../helper/cache/getLogs')
+const { getUniqueAddresses } = require('../helper/utils')
 
-const CHAINS = [
-  {
-    id: 1,
-    name: "ethereum",
-    url: "https://api.thegraph.com/subgraphs/name/barnbridge/sy-mainnet",
-    address: "0xc67cb09d08521cD1dE6BAAC46824261eb1dB8800",
-  },
-  {
-    id: 42161,
-    name: "arbitrum",
-    url: "https://api.thegraph.com/subgraphs/name/barnbridge/sy-arbitrum",
-    address: "0xf878a060D4d51704B14e8f68B51185bF5DbFE3A1",
-  },
-  {
-    id: 10,
-    name: "optimism",
-    url: "https://api.thegraph.com/subgraphs/name/barnbridge/sy-optimism",
-    address: "0x45c158E0ee76c76E525BaB941991268249e95331",
-  },
-];
-
-async function tvl(_timestamp, _1, _2, { api }) {
-  const chain = api.chain
-  const config = CHAINS.find(i => i.name === chain)
-  const pastAndActiveTerms = await getPastAndActiveTerms(config.url);
-
-  await liquidityProviderBalancesForTerms(api, pastAndActiveTerms)
-  return api.getBalances()
-};
+const config = {
+  ethereum: { factory: '0xc67cb09d08521cD1dE6BAAC46824261eb1dB8800', fromBlock: 16828337, },
+  arbitrum: { factory: '0xf878a060D4d51704B14e8f68B51185bF5DbFE3A1', fromBlock: 69857947, },
+  optimism: { factory: '0x45c158E0ee76c76E525BaB941991268249e95331', fromBlock: 80641123, },
+}
 
 module.exports = {
-  timetravel: false,
-  methodology:
-    "BarnBridge TVL is an aggregated TVL (user liquidity + DAO deposits) of active Smart Yield pools across available networks.",
 };
 
-CHAINS.forEach((chain) => {
-  module.exports[chain.name] = {
-    tvl,
-  };
-});
+Object.keys(config).forEach(chain => {
+  const { factory, fromBlock, } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const logs = await getLogs({
+        api,
+        target: factory,
+        topics: ['0x4f2ce4e40f623ca765fc0167a25cb7842ceaafb8d82d3dec26ca0d0e0d2d4896'],
+        eventAbi: 'event PoolCreated(address indexed controller, address provider)',
+        onlyArgs: true,
+        fromBlock,
+      })
+      const providers = getUniqueAddresses(logs.map(i => i.provider))
+      const tokens = await api.multiCall({  abi: 'address:underlying', calls: providers}) 
+      const balances = await api.multiCall({  abi: 'uint256:underlyingBalance', calls: providers}) 
+      api.addTokens(tokens, balances)
+      return api.getBalances()
+    }
+  }
+})
