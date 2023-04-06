d33f2db4f0424079428ccdac20de14db8b0f93da
realdealshaman <realdealshaman11@gmail.com>
2023-04-06 01:19:31 +0100

    add sushiswap v3 adapter

---
+++ projects/sushiswap-v3/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  ethereum: { factory: '0xbACEB8eC6b9355Dfc0269C18bac9d6E2Bdc29C4F', fromBlock: 16955547, },
+})
