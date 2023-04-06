a44ed3a04a6d8f0f2da43a13257d6ad5754e0efc
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-06 12:34:00 +0200

    dual: refactor code

--- projects/duet/arbitrum.js
+++ projects/duet/arbitrum.js
@@ -1,20 +1,9 @@
-const sdk = require("@defillama/sdk");
 const proPoolABI = require("./abis/pro-pool.json");
-const BigNumber = require("bignumber.js");
 const PRO_POOL_CONTRACT = "0xdE57c591de8B3675C43fB955725b62e742b1c0B4";
 
-async function fetch() {
-
-  try {
-    const proPoolTvl = await sdk.api.abi.call({
-      abi: proPoolABI.liquidity, chain: "arbitrum", target: PRO_POOL_CONTRACT, params: []
-    });
-    const tvl = new BigNumber(proPoolTvl.output).div(Math.pow(10, 18));
-    return { usd: tvl.toNumber() };
-  } catch (e) {
-    // console.error("arbitrum", e);
-    return { usd: 0 };
-  }
+async function fetch(_, _1, _2, { api }) {
+  const proPoolTvl = await api.call({ abi: proPoolABI.liquidity, target: PRO_POOL_CONTRACT })
+  return { 'usd-coin': proPoolTvl / 1e18 };
 }
 
 module.exports = fetch;
