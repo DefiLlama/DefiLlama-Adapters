390dda9e443fe0b891b78f1bd541a03e817a1a34
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-06 16:43:17 +0200

    barnbridge: simplify code

--- projects/barnbridge/numbers.js
+++ projects/barnbridge/numbers.js
@@ -1,75 +1,13 @@
-const BigNumber = require("bignumber.js");
-const { uniqBy, groupBy } = require("lodash");
+const { getUniqueAddresses } = require('../helper/utils')
 
-const { poolByProvider } = require("./SmartYield.js");
-
-const userDepositsForTerms = async (terms) => {
-  const groupedUserDeposits = groupBy(terms, (term) => term.provider);
-
-  const values = Object.entries(groupedUserDeposits).map(
-    ([provider, terms]) => {
-      const value = terms.reduce(
-        (prev, term) => prev.plus(BigNumber(term.currentDepositedAmount)),
-        BigNumber(0)
-      );
-
-      return { provider, value };
-    }
-  );
-
-  return values;
-};
-
-const liquidityProviderBalancesForTerms = async (chain, terms) => {
-  const values = await Promise.all(
-    uniqBy(terms, "provider")
-      .map((term) => term.provider)
-      .map(async (provider) => {
-        const liquidityProviderBalance = (await poolByProvider(chain, provider))
-          .liquidityProviderBalance;
-
-        const value = BigNumber(liquidityProviderBalance);
-
-        return { provider, value };
-      })
-  );
-
-  return values;
-};
-
-const totalValueLockedForTerms = async (chain, terms) => {
-  const liquidityProviderBalances = await liquidityProviderBalancesForTerms(
-    chain,
-    terms
-  );
-
-  const userDeposits = await userDepositsForTerms(terms);
-
-  const indexedLiquidityProviderBalances = Object.fromEntries(
-    liquidityProviderBalances.map(({ provider, value }) => [provider, value])
-  );
-
-  const indexeduserDeposits = Object.fromEntries(
-    userDeposits.map(({ provider, value }) => [provider, value])
-  );
-
-  const providers = uniqBy(terms, "provider").map((term) => ({
-    provider: term.provider,
-    underlying: term.underlying,
-  }));
-
-  const values = providers.map(({ provider, underlying }) => {
-    return {
-      underlying: underlying,
-      value: indexedLiquidityProviderBalances[provider]
-        .plus(indexeduserDeposits[provider])
-        .toFixed(),
-    };
-  });
-
-  return values;
+const liquidityProviderBalancesForTerms = async (api, terms) => {
+  const providers = getUniqueAddresses(terms.map(i => i.provider))
+  const tokens = await api.multiCall({  abi: 'address:underlying', calls: providers}) 
+  const balances = await api.multiCall({  abi: 'uint256:underlyingBalance', calls: providers}) 
+  console.log(tokens, balances)
+  api.addTokens(tokens, balances)
 };
 
 module.exports = {
-  totalValueLockedForTerms,
+  liquidityProviderBalancesForTerms,
 };
