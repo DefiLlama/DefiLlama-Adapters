1d5ff18bccba942cbbd8031b01eb99df4591a45d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-06 11:55:10 +0200

    code refactor

--- projects/duet/bsc.js
+++ projects/duet/bsc.js
@@ -2,6 +2,7 @@ const { getConfig } = require("../helper/cache");
 const sdk = require("@defillama/sdk");
 const collateralReaderContractABI = require("./abis/collateral-reader.json");
 const ebcakeReaderContractABI = require("./abis/ebcake-reader.json");
+const { getUniqueAddresses } = require('../helper/utils')
 
 const BigNumber = require("bignumber.js");
 const proPoolABI = require("./abis/pro-pool.json");
@@ -11,21 +12,16 @@ const COLLATERAL_READER_CONTRACT = "0xFfB8FD0E5eA13bb71401B19e00f9F934746f0b7A";
 
 async function getEBCakeTvl() {
   const EBCAKE_READER_CONTRACT = "0x243F8da5893E534CBd25220b6E277420dd9dE77B";
-  try {
-    const ret = await sdk.api.abi.call({
-      abi: ebcakeReaderContractABI.extendableBondGroupInfo,
-      chain: "bsc",
-      target: EBCAKE_READER_CONTRACT,
-      params: ["yearly"]
-    });
-    return ret.output.faceUsdValue;
-  } catch (e) {
-    // console.error(e);
-    return 0;
-  }
+  const ret = await sdk.api.abi.call({
+    abi: ebcakeReaderContractABI.extendableBondGroupInfo,
+    chain: "bsc",
+    target: EBCAKE_READER_CONTRACT,
+    params: ["yearly"]
+  });
+  return ret.output.faceUsdValue;
 }
 
-async function fetch() {
+async function fetch(_, _1, _2, { api }) {
   const ret = await getConfig("duet-fi", TOKEN_LIST_URL);
   const tokens = ret;
   const vaultList = [];
@@ -35,31 +31,17 @@ async function fetch() {
     }
     vaultList.push(...token.vaults);
   }
-  const uniqueVaults = new Set(vaultList
+  const uniqueVaults = getUniqueAddresses(vaultList
     .filter((vault) => vault.displayPosition !== "DASSETS")
     .map((vault) => vault.vaultAddress && vault.vaultAddress.trim())
-    .filter(Boolean));
+    .filter(i => i))
 
-  const tokenTvls = await Promise.all([...uniqueVaults].map(async (vault) => {
-    try {
-      return (await sdk.api.abi.call({
-        abi: collateralReaderContractABI.depositVaultValues,
-        chain: "bsc",
-        target: COLLATERAL_READER_CONTRACT,
-        params: [[vault], false]
-      })).output[1][0];
-    } catch (e) {
-      // console.error(e, vault);
-      return null;
-    }
-  }));
-  await getEBCakeTvl();
-  const tvl = tokenTvls
-    .filter(Boolean)
-    .reduce((previous, current) => new BigNumber(previous).plus(current), new BigNumber(0))
-    .plus(await getEBCakeTvl())
-    .div(Math.pow(10, 8));
-  return { usd: tvl.toNumber() };
+  
+  const tokenTvls = await api.multiCall({  abi: collateralReaderContractABI.depositVaultValues, target: COLLATERAL_READER_CONTRACT, calls: [...uniqueVaults].map(i => ({ params: [[i], false]}))}) 
+  api.add('tether', (await getEBCakeTvl(api))/1e8, { skipChain: true } )
+  tokenTvls.forEach(v => {
+    if (v) api.add('tether', v[1][0]/1e8, { skipChain: true })
+  })
 }
 
 module.exports = fetch;
