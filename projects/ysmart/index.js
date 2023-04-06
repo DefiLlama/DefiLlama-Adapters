28d9d8ba6d6b71249b324929f62837a221c5164c
ysmart <dev.smart360@gmail.com>
2023-04-06 18:42:01 +0530

    corrected master chef address

--- projects/ysmart/index.js
+++ projects/ysmart/index.js
@@ -1,6 +1,6 @@
 const { sumTokensExport } = require("../helper/unwrapLPs");
 
-const masterchef = "0x13C9e00871ce38bA2F11fa0C03E32a9DdD767893";
+const masterchef = "0x705A71efadD12faDac67600b666D8c0347848BD3";
 const token_USDC = "0xfA9343C3897324496A05fC75abeD6bAC29f8A40f";
 const token_USDT = "0xB44a9B6905aF7c801311e8F4E76932ee959c663C";
 
