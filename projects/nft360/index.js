8fcf950cca24f6d99c71ce8519df7740998d9ce9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-06 20:56:56 +0530

    Update index.js

--- projects/nft360/index.js
+++ projects/nft360/index.js
@@ -1,6 +1,7 @@
 const { sumTokensExport } = require("../helper/unwrapLPs");
 
 const token_USDC = "0xfA9343C3897324496A05fC75abeD6bAC29f8A40f";
+const token_USDT = "0xB44a9B6905aF7c801311e8F4E76932ee959c663C";
 const masterchef = "0x061362b6c0676fA9995A15D7fFcaef0323A9dF2F";
 
 module.exports = {
