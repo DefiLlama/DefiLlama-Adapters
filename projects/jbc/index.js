07ee6a589a616c061b22669ed1524a719ed6c13e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-07 16:40:28 +0200

    minor refactor

--- projects/jbc/index.js
+++ projects/jbc/index.js
@@ -1,6 +1,5 @@
-const { pool2 } = require("../helper/unknownTokens");
+const { pool2, staking } = require("../helper/unknownTokens");
 const { nullAddress, sumTokensExport, } = require("../helper/unwrapLPs");
-const sdk = require("@defillama/sdk");
 
 const tokensAndOwners = [
   ['0x1addd80e6039594ee970e5872d247bf0414c8903', '0xe964b6083F24dBC06e94C662b195c22C76923b22'], // GLP
@@ -8,28 +7,12 @@ const tokensAndOwners = [
   ['0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f', '0xCC13E077F54577cE3Ea52916fDd70305C461A3ED'], // WBTC
   ['0xff970a61a04b1ca14834a43f5de4533ebddb5cc8', '0xcA2F482B067D354B3cdB6926911f42F5d1f0e872'], // USDC
 ]
-
-function staking(stakingAddr, token, chain) {
-  return async (_timestamp, _block, chainBlocks) => {
-    let balances = {};
-    let balance = (
-      await sdk.api.erc20.balanceOf({
-        target: token,
-        owner: stakingAddr,
-        block: chainBlocks[chain],
-        chain,
-      })
-    ).output;
-    sdk.util.sumSingleBalance(balances, `arbitrum:${token}`, balance);
-    return balances;
-  };
-}
+const lpToken = '0x85c6da933a7451bf2a6d836304b30967f3e76e11'
 
 module.exports = {
   arbitrum: {
     tvl: sumTokensExport({ tokensAndOwners }),
-    pool2: pool2({ stakingContract: '0x0F6f73c7ecCE4FB9861E25dabde79CBA112550b3', lpToken: '0x85c6da933a7451bf2a6d836304b30967f3e76e11', chain: 'arbitrum', useDefaultCoreAssets: true, }),
-    staking: staking("0xaF70e6DF6d34dbcd284BC4CCa047Bd232110A2CF", "0xb67c175701fD60cD670cB9D331368367BF072e47", "arbitrum")
+    pool2: pool2({ stakingContract: '0x0F6f73c7ecCE4FB9861E25dabde79CBA112550b3', lpToken, useDefaultCoreAssets: true, }),
+    staking: staking({owner: "0xaF70e6DF6d34dbcd284BC4CCa047Bd232110A2CF", tokens: ["0xb67c175701fD60cD670cB9D331368367BF072e47"], lps: [lpToken]})
   }
-};
-
+}
