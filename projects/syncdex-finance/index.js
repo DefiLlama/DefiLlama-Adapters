596f4a6a0969cea664fedf68865e1b2bbc65df6b
Dev SyncDEX <129781989+devsyncdex@users.noreply.github.com>
2023-04-09 02:42:34 +0700

    Add USDC/USD+ farming pool for SyncDEX Finance (#5850)
    * Add Contract Address
    
    * add syncdex-finance
    
    * The purpose of the project will be shifted to focus on staking and farming, therefore, the TVL will be rebuilt by counting the assets in the staking contract.
    
    * The purpose of the project will be shifted to focus on staking and farming, therefore, the TVL will be rebuilt by counting the assets in the staking contract.
    
    * code refactor
    
    * Remove the project's token.
    
    * Remove the project token owner
    
    * Add USDC token for staking contract
    
    * Add USDC, WETH token for staking contract
    
    * Update index.js
    
    * Add more pools for SyncDEX Finance
    
    * syncdex: refactor code
    
    * Add USDC/USD+ farming pool for SyncDEX Finance
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/syncdex-finance/index.js
+++ projects/syncdex-finance/index.js
@@ -6,15 +6,17 @@ const sydx_staking_contract = "0xB2bD1427b04d84eA14214582C59CB16D4F1d676E";
 const native_usdc_farming_contract = "0x3F90A802B5D6E984A6f296F697AB847C5fE23F31";
 const native_sydx_farming_contract = "0xDA24F9528102A5A126Cd7eeE8c6e22A786cAE844";
 const sydx_usdc_farming_contract = "0x75aC887df149076b7DF4deAA0267711475b71572";
+const usdc_usdplus_farming_contract = "0xF0B42d393eAB886bd9310AaF0A2B1eDbaaD31fdE"
 
 const assets = [
   "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
   "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
-  "0x0000000000000000000000000000000000000000" // This is address of native token (ETH), you can check native_staking_contract
+  "0x0000000000000000000000000000000000000000", // This is address of native token (ETH), you can check native_staking_contract
+  "0x8E86e46278518EFc1C5CEd245cBA2C7e3ef11557"
 ];
 const SYNC_DEX = '0x3a34FA9a1288597Ad6C1Da709f001D37FeF8b19e'
 
-const owners = [native_staking_contract, usdc_staking_contract, sydx_staking_contract, native_usdc_farming_contract, native_sydx_farming_contract, sydx_usdc_farming_contract]
+const owners = [native_staking_contract, usdc_staking_contract, sydx_staking_contract, native_usdc_farming_contract, native_sydx_farming_contract, sydx_usdc_farming_contract, usdc_usdplus_farming_contract]
 
 module.exports = {
   era: {
