adf02ce7d0fa29a50cf6118715b64d0f3e97b736
Roman <romangolovlyov@gmail.com>
2023-04-08 14:56:51 +0500

    added ZKSYNC network

--- projects/overnight-usdPlus/index.js
+++ projects/overnight-usdPlus/index.js
@@ -5,6 +5,7 @@ const m2m = {
     bsc: "0x9Af655c4DBe940962F776b685d6700F538B90fcf",
     optimism: "0x9Af655c4DBe940962F776b685d6700F538B90fcf",
     arbitrum: "0x9Af655c4DBe940962F776b685d6700F538B90fcf",
+    zksync: "0x240aad990FFc5F04F11593fF4dCF1fF714d6fc80",
 }
 
 const assets = {
@@ -12,6 +13,7 @@ const assets = {
     bsc: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", //BUSD
     optimism: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", //USDC
     arbitrum: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", //USDC
+    zksync: "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
 }
 
 const abi = "uint256:totalNetAssets"
