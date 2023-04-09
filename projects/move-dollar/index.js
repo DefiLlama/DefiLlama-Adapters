0e76a2c6bd2a51289bf8fb128d4e8cc0aa3a37c8
Lawson Graham <lawson.graham8@gmail.com>
2023-04-08 17:53:42 -0700

    updated Thala TVL logic

--- projects/move-dollar/index.js
+++ projects/move-dollar/index.js
@@ -3,7 +3,6 @@ const { getResources, } = require("../helper/chain/aptos");
 const { transformBalances } = require("../helper/portedTokens");
 
 const moveDollarAddress = "0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01";
-const MODType = "0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::mod_coin::MOD";
 
 let resourcesCache;
 
@@ -13,12 +12,11 @@ async function _getResources() {
 }
 const extractVaultType = resource => resource.type.split('<')[1].replace('>', '').split(', ');
 const vaultsFilter = resource => resource.type.includes(`${moveDollarAddress}::vault::Vaults<`)
-const stabilityPoolsFilter = resource => resource.type.includes(`${moveDollarAddress}::stability_pool::StabilityPool<`)
 
 module.exports = {
   timetravel: false,
   methodology:
-    "Aggregates all collateral backing Move Dollar in Thala's CDP Vaults and Stability Pools.",
+    "Aggregates all collateral backing Move Dollar in Thala's CDP Vaults.",
   aptos: {
     tvl: async () => {
       const balances = {};
@@ -30,14 +28,6 @@ module.exports = {
 
       vaults.forEach(({ asset_type, total_collateral }) => {
           sdk.util.sumSingleBalance(balances, asset_type, total_collateral);
-      });
-
-      const stabilityPools = resources.filter(stabilityPoolsFilter).map(pool => ({
-          total_value: pool.data.stability.value,
-        }));
-      
-      stabilityPools.forEach(({ total_value }) => {
-          sdk.util.sumSingleBalance(balances, MODType, total_value);
       });
 
       return transformBalances("aptos", balances);
