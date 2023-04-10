f45ed77163978758a63b90809fe7b15878780b5f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-10 21:04:31 +0200

    add uni v3 subgraph helper

--- projects/forge/index.js
+++ projects/forge/index.js
@@ -1,5 +1,8 @@
-const { uniV3Export } = require('../helper/uniswapV3')
+const { uniV3GraphExport } = require('../helper/uniswapV3')
+// evmos: { factory: '0xf544365e7065966f190155F629cE0182fC68Eaa2', fromBlock: 12367456, },
 
-module.exports = uniV3Export({
-  evmos: { factory: '0xf544365e7065966f190155F629cE0182fC68Eaa2', fromBlock: 12367456, },
-})
+module.exports = {
+  evmos: {
+    tvl: uniV3GraphExport({ graphURL: 'https://api.orbitmarket.io/subgraphs/name/forge-trade/v3-subgraph'})
+  }
+}
