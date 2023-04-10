eed513ea1641b37faa49b96fe36f28c8ee993f47
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-10 14:10:37 +0200

    code refactor

--- projects/monopoly-finance/index.js
+++ projects/monopoly-finance/index.js
@@ -1,153 +1,59 @@
+const { staking } = require("../helper/staking");
 const abi = require("./abi.json");
-const { getUniTVL } = require("../helper/unknownTokens");
-const { addFundsInMasterChef } = require("../helper/masterchef");
-const { stakings } = require("../helper/staking");
-const sdk = require("@defillama/sdk");
-const { unwrapLPsAuto } = require("../helper/unwrapLPs");
-const { default: BigNumber } = require("bignumber.js");
 const chef = "0xfBb572B2F67746fe3A0Cb7DbDeE6717581a790cc";
-const mono = "0xbC0a588120AB8b913436D342a702C92611C9af6a";
+const mono = "0xbC0a588120AB8b913436D342a702C92611C9af6a".toLowerCase();
 const ACC_MONO_PRECISION = 1e18;
 
-const bentoLps = [
-  {
-    pid: 4,
-    lp: "0x79bf7147eBCd0d55e83Cb42ed3Ba1bB2Bb23eF20",
-    lpDecimals: 18,
-    token0Decimals: 6,
-    token1Decimals: 6,
-  },
-  {
-    pid: 5,
-    lp: "0x6DBE389142E40b01aA10Fb069ae448Fc4460DaE4",
-    lpDecimals: 18,
-    token0Decimals: 18,
-    token1Decimals: 6,
-  },
+const bentoPools = [
+  "0x79bf7147eBCd0d55e83Cb42ed3Ba1bB2Bb23eF20".toLowerCase(),
+  "0x6DBE389142E40b01aA10Fb069ae448Fc4460DaE4".toLowerCase(),
 ];
 
-async function getTokensInMasterChef(time, ethBlock, chainBlocks) {
-  const chain = "arbitrum";
-  const block = chainBlocks[chain];
-  const transformAddress = (addr) => `arbitrum:${addr}`;
-  const ignoreAddresses = [mono].map((i) => i.toLowerCase());
+async function getTokensInMasterChef(time, ethBlock, chainBlocks, { api }) {
+  const poolInfo = await api.fetchList({ lengthAbi: abi.poolLength, itemAbi: abi.poolInfo, target: chef })
 
-  const balances = {};
-  const poolLength = (
-    await sdk.api.abi.call({
-      abi: abi.poolLength,
-      target: chef,
-      block,
-      chain,
-    })
-  ).output;
+  const [bentoLpTotalSupply, bentoLpReserves, bentoLpAssets] = await Promise.all([
+    api.multiCall({ calls: bentoPools, abi: abi.totalSupply, }),
+    api.multiCall({ calls: bentoPools, abi: abi.getReserves, }),
+    api.multiCall({ calls: bentoPools, abi: abi.getAssets, }),
+  ])
+  const lpTokens = bentoLpAssets.flat().map(i => i.toLowerCase())
+  const lpDecimals = await api.multiCall({ abi: 'erc20:decimals', calls: lpTokens })
 
-  const poolInfo = (
-    await sdk.api.abi.multiCall({
-      block,
-      calls: Array.from(Array(Number(poolLength)).keys()).map((i) => ({
-        target: chef,
-        params: i,
-      })),
-      abi: abi.poolInfo,
-      chain,
-    })
-  ).output;
+  const bentoLpData = bentoPools.map((lp, i) => {
+    const supply = (bentoLpTotalSupply[i] / 1e18)
+    let [token0, token1] = bentoLpAssets[i].map(i => i.toLowerCase())
+    const token0Decimals = lpDecimals[lpTokens.indexOf(token0)]
+    const token1Decimals = lpDecimals[lpTokens.indexOf(token1)]
 
-  const bentoLpTotalSupply = (
-    await sdk.api.abi.multiCall({
-      block,
-      calls: bentoLps.map((lp) => ({
-        target: lp.lp,
-      })),
-      abi: abi.totalSupply,
-      chain,
-    })
-  ).output;
-
-  const bentoLpReserves = (
-    await sdk.api.abi.multiCall({
-      block,
-      calls: bentoLps.map((lp) => ({
-        target: lp.lp,
-      })),
-      abi: abi.getReserves,
-      chain,
-    })
-  ).output;
-  const bentoLpAssets = (
-    await sdk.api.abi.multiCall({
-      block,
-      calls: bentoLps.map((lp) => ({
-        target: lp.lp,
-      })),
-      abi: abi.getAssets,
-      chain,
-    })
-  ).output;
-
-  const bentoLpData = bentoLps.map((lp, i) => {
     return {
-      ...lp,
-      lpPerToken0:
-        bentoLpReserves[i].output._reserve0 /
-        10 ** lp.token0Decimals /
-        (bentoLpTotalSupply[i].output / 1e18),
-      lpPerToken1:
-        bentoLpReserves[i].output._reserve1 /
-        10 ** lp.token1Decimals /
-        (bentoLpTotalSupply[i].output / 1e18),
-      token0: bentoLpAssets[i].output[0],
-      token1: bentoLpAssets[i].output[1],
+      lp,
+      lpPerToken0: bentoLpReserves[i]._reserve0 / (supply * 10 ** token0Decimals),
+      lpPerToken1: bentoLpReserves[i]._reserve1 / (supply * 10 ** token1Decimals),
+      token0, token1, token0Decimals, token1Decimals,
     };
-  });
+  })
 
-  poolInfo.forEach(({ output: pool }, pid) => {
-    if (pid === 4 || pid === 5) {
-      const lpData = bentoLpData.find((lp) => lp.pid === pid);
-
-      const token0 = lpData.token0.toLowerCase();
-      const token1 = lpData.token1.toLowerCase();
-
-      const balance = BigNumber(pool.totalShares)
-        .times(pool.lpPerShare)
-        .div(ACC_MONO_PRECISION)
-        .toFixed(0);
-
-      const token0Balance = BigNumber(balance)
-        .multipliedBy(lpData.lpPerToken0)
-        .div(Number(10 ** (18 - lpData.token0Decimals)))
-        .toFixed(0);
-      const token1Balance = BigNumber(balance)
-        .multipliedBy(lpData.lpPerToken1)
-        .div(Number(10 ** (18 - lpData.token1Decimals)))
-        .toFixed(0);
-      sdk.util.sumSingleBalance(
-        balances,
-        transformAddress(token0),
-        token0Balance
-      );
-      sdk.util.sumSingleBalance(
-        balances,
-        transformAddress(token1),
-        token1Balance
-      );
-    } else {
-      const token = pool[0].toLowerCase();
-      const balance = BigNumber(pool.totalShares)
-        .times(pool.lpPerShare)
-        .div(ACC_MONO_PRECISION)
-        .toFixed(0);
-      sdk.util.sumSingleBalance(balances, transformAddress(token), balance);
+  poolInfo.forEach(pool => {
+    let { lpToken, totalShares, lpPerShare, } = pool
+    lpToken = lpToken.toLowerCase()
+    if (lpToken === mono) {
+      return;
     }
-  });
-
-  return balances;
+    let bals = totalShares * lpPerShare / ACC_MONO_PRECISION
+    if (bentoPools.includes(lpToken)) {
+      const { token0, token1, lpPerToken0, lpPerToken1, token0Decimals, token1Decimals, } = bentoLpData.find(i => i.lp === lpToken)
+      api.add(token0, bals * lpPerToken0 / (10 ** (18 - token0Decimals)))
+      api.add(token1, bals * lpPerToken1 / (10 ** (18 - token1Decimals)))
+    } else {
+      api.add(lpToken, bals)
+    }
+  })
 }
 module.exports = {
+  methodology: "TVL includes all farms in MasterChef contract, as well as staking pools.",
   arbitrum: {
-    methodology:
-      "TVL includes all farms in MasterChef contract, as well as staking pools.",
     tvl: getTokensInMasterChef,
+    staking: staking('0x7779b827832bBD9050917Ab29787B7DC3C3D5974', mono)
   },
 };
