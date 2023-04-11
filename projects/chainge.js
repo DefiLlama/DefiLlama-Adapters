159fd1c60320c49b2b938a6f7a864ecb2d0f16f0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-11 14:50:51 +0200

    chainge: code refactor

--- projects/chainge.js
+++ projects/chainge.js
@@ -1,13 +1,16 @@
+const { getUniTVL } = require('./helper/unknownTokens');
 const utils = require('./helper/utils');
 
 // fusion
 async function fetch() {
-    let tvl = await utils.fetchURL('https://info.chainge.finance/api/v1/info/getInfoOuterTvl')
+  let tvl = await utils.fetchURL('https://info.chainge.finance/api/v1/info/getInfoOuterTvl')
 
-    return tvl.data.data.totalTvl
+  return tvl.data.data.totalTvl
 }
 
 module.exports = {
-    methodology: "assets in liquidity are counted as TVL",
-    fetch
+  methodology: "assets in liquidity are counted as TVL",
+  fusion: {
+    tvl: getUniTVL({ factory: '0xb260b0474B29642f8317530591B99b052Ce4788f', useDefaultCoreAssets: true, fetchBalances: true, })
+  }
 }
