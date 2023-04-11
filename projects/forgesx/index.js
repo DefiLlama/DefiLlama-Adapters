1c39ac693d2a9104c1506ba8f71d7f14ec6e741b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-11 17:41:11 +0200

    forge sx: refactor code

--- projects/forgesx/index.js
+++ projects/forgesx/index.js
@@ -1,24 +1,11 @@
-const sdk = require('@defillama/sdk')
+const { sumTokensExport } = require('../helper/unwrapLPs')
 const FORGE_SOL = '0x4938D2016e7446a24b07635611bD34289Df42ECb'
 const USDC_TOKEN = '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8'
 
-async function tvl(_, _1, _2, { api }) {
-  const balances = {}
-  const collateralBalance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: USDC_TOKEN,
-    params: [FORGE_SOL],
-  })
-  await sdk.util.sumSingleBalance(balances, USDC_TOKEN, collateralBalance, api.chain)
-  return balances
-}
-
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'counts the number of USDC tokens deposited as collateral in the Forge.sol contract.',
   start: 1680643295,
   arbitrum: {
-    tvl,
+    tvl: sumTokensExport({ owner: FORGE_SOL, tokens: [USDC_TOKEN]}),
   }
 };
