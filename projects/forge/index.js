f0f4695ca703fb08bbaa368cda02a64e47ca12f8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-11 11:00:28 +0200

    add doveswap v3

--- projects/forge/index.js
+++ projects/forge/index.js
@@ -1,8 +1,5 @@
-const { uniV3GraphExport } = require('../helper/uniswapV3')
-// evmos: { factory: '0xf544365e7065966f190155F629cE0182fC68Eaa2', fromBlock: 12367456, },
+const { uniV3Export } = require('../helper/uniswapV3')
 
-module.exports = {
-  evmos: {
-    tvl: uniV3GraphExport({ graphURL: 'https://api.orbitmarket.io/subgraphs/name/forge-trade/v3-subgraph'})
-  }
-}
+module.exports = uniV3Export({
+  evmos: { factory: '0xf544365e7065966f190155F629cE0182fC68Eaa2', fromBlock: 12367456, },
+})
