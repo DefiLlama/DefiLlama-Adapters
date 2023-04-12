17cefc190947c8448a0ec186980c19a4e0d0cdf8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-12 10:25:37 +0200

    native.fi: refactor code

--- projects/native/index.js
+++ projects/native/index.js
@@ -1,69 +1,32 @@
-const sdk = require("@defillama/sdk");
-const axios = require("axios");
-
-async function tvl(_, _1, _2, { api }) {
-  const balances = {};
-
-  const pools = await axios.get(
-    `http://chain-monitoring.native.org/analytics/pools?chain=${api.chain}`
-  );
-
-  const combinedTreasuryTokenAddresses = pools.data.reduce(
-    (accumulator, item) => {
-      const existingEntry = accumulator.find(
-        (entry) => entry.treasury_address === item.treasury_address
-      );
-
-      if (existingEntry) {
-        if (!existingEntry.token_addresses.includes(item.token0.address)) {
-          existingEntry.token_addresses.push(item.token0.address);
-        }
-        if (!existingEntry.token_addresses.includes(item.token1.address)) {
-          existingEntry.token_addresses.push(item.token1.address);
-        }
-      } else {
-        accumulator.push({
-          treasury_address: item.treasury_address,
-          token_addresses: [item.token0.address, item.token1.address],
-        });
-      }
-
-      return accumulator;
-    },
-    []
-  );
-
-  for (const entry of combinedTreasuryTokenAddresses) {
-    const { treasury_address, token_addresses } = entry;
-
-    for (const token_address of token_addresses) {
-      const tokenBalance = await api.call({
-        abi: "erc20:balanceOf",
-        target: token_address,
-        params: [treasury_address],
-      });
-
-      await sdk.util.sumSingleBalance(
-        balances,
-        token_address,
-        tokenBalance,
-        api.chain
-      );
-    }
-  }
-
-  return balances;
-}
+const { getLogs } = require('../helper/cache/getLogs');
+const { sumTokens2, nullAddress, } = require("../helper/unwrapLPs");
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  methodology:
-    "includes the liquidity provided to the infrasturcture and ecosystem of Native",
-  bsc: {
-    tvl,
-  },
-  ethereum: {
-    tvl,
-  },
-};
+  methodology: "includes the liquidity provided to the infrasturcture and ecosystem of Native",
+}
+
+const config = {
+  bsc: { factory: '0x85b0f66e83515ff4e825dfcaa58e040e08278ef9', fromBlock: 27103796, },
+  ethereum: { factory: '0x85b0f66e83515ff4e825dfcaa58e040e08278ef9', fromBlock: 16995923, },
+}
+
+Object.keys(config).forEach(chain => {
+  const { factory, fromBlock, } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const logs = await getLogs({
+        api,
+        target: factory,
+        topic: 'PoolCreated(address,address,address,address,address)',
+        eventAbi: 'event PoolCreated(address treasury, address owner, address signer, address pool, address impl)',
+        onlyArgs: true,
+        fromBlock,
+      })
+      const treasuries = logs.map(i => i.treasury)
+      const pools = logs.map(i => i.pool)
+      const tokenAs = await api.multiCall({ abi: 'address[]:getTokenAs', calls: pools })
+      const tokenBs = await api.multiCall({ abi: 'address[]:getTokenBs', calls: pools })
+      return sumTokens2({ api, ownerTokens: treasuries.map((v, i) => [[nullAddress, ...tokenAs[i], ...tokenBs[i]], v]) })
+    }
+  }
+})
