6e8bfa8d336634cbcaf47182aa0491aee85dafc0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-12 15:52:59 +0200

    minor fix

--- projects/agilitylsd/index.js
+++ projects/agilitylsd/index.js
@@ -1,6 +1,6 @@
 const { sumTokens2, nullAddress } = require("../helper/unwrapLPs");
 const { getLogs } = require("../helper/cache/getLogs");
-const { pool2s } = require("../helper/pool2");
+const { staking } = require("../helper/staking");
 
 const FACTORY_CONTRACT = "0xe4a51ec59233ba1f62b71f84554622a532b584ed";
 const WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
@@ -11,26 +11,16 @@ async function tvl(timestamp, block, chainBlocks, { api }) {
   const logs = await getLogs({
     api,
     target: FACTORY_CONTRACT,
-    topics: [
-      "0x489ab9065c597368f4a678fadcb323bf4c848713ea7d5a296d16ec97203eae83",
-    ],
-    eventAbi:
-      "event StakingPoolDeployed(address indexed poolAddress,address indexed stakingToken,uint256 startTime,uint256 roundDurationInDays)",
+    topics: ["0x489ab9065c597368f4a678fadcb323bf4c848713ea7d5a296d16ec97203eae83",],
+    eventAbi: "event StakingPoolDeployed(address indexed poolAddress,address indexed stakingToken,uint256 startTime,uint256 roundDurationInDays)",
     onlyArgs: true,
     fromBlock: 17015686,
   });
 
-  const lsdAddresses = logs.map((i) =>
-    i.stakingToken === nullAddress ? WETH_ADDRESS : i.stakingToken
-  );
+  const lsdAddresses = logs.map((i) => i.stakingToken === nullAddress ? WETH_ADDRESS : i.stakingToken);
   const poolAddresses = logs.map((i) => i.poolAddress);
 
-  return sumTokens2({
-    api,
-    tokens: lsdAddresses,
-    owners: poolAddresses,
-    blacklistedTokens: [AGI_ETH_LP_ADDRESS],
-  });
+  return sumTokens2({ api, tokensAndOwners2: [lsdAddresses, poolAddresses], blacklistedTokens: [AGI_ETH_LP_ADDRESS], });
 }
 
 module.exports = {
@@ -38,6 +28,6 @@ module.exports = {
     methodology:
       "TVL of Staked ETH & LSD tokens, with pool2 including value of staked AGI/ETH Uniswap-V2 LP tokens",
     tvl,
-    pool2: pool2s([AGI_ETH_STAKING_POOL], [AGI_ETH_LP_ADDRESS]),
+    pool2: staking([AGI_ETH_STAKING_POOL], [AGI_ETH_LP_ADDRESS]),
   },
 };
