4b607edd6737fc81ad843d66cd1a7b13b1060556
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-13 22:11:51 +0200

    minor fix

--- projects/satoshiswap/index.js
+++ projects/satoshiswap/index.js
@@ -4,7 +4,7 @@ module.exports = {
   misrepresentedTokens: true,
   core: {
     tvl: getUniTVL({
-      chain: 'core',
+      fetchBalances: true,
       useDefaultCoreAssets: true,
       factory: '0x8f5c03a1c86bf79ae0baC0D72E75aee662083e26',
     })
