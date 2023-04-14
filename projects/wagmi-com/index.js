2a3ec47fd2b34b7e5a8bead6ba41a5b9c4b2ff94
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-14 23:17:16 +0200

    add wagmi.com

--- projects/wagmi-com/index.js
+++ projects/wagmi-com/index.js
@@ -1,5 +1,5 @@
 const { uniV3Export } = require("../helper/uniswapV3");
 
 module.exports = uniV3Export({
-  era: { factory: '0x32a4fB9CC08f24639533178da9BEf36f6436994b', fromBlock: 869504, },
+  era: { factory: '0x31be61CE896e8770B21e7A1CAFA28402Dd701995', fromBlock: 1351075, },
 })
