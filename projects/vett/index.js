f4c53963419e0343097041b24b1a5373e3b2d158
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-14 09:38:09 +0200

    minor refactor

--- projects/vett/index.js
+++ projects/vett/index.js
@@ -1,22 +1,8 @@
-const sdk = require("@defillama/sdk");
-// const ethers = require("ethers");
-
-async function tvl(_timestamp, _b, { thundercore: block }) {
-  const { output: ttBalances } = await sdk.api.eth.getBalances({
-    targets: ["0xC3C857a9E5Be042C8acF4F2827Aa053e93b5d039"],
-    chain: "thundercore",
-    block,
-  });
-  const ttTvl = ttBalances[0].balance;
-  return {
-    "thunder-token": ttTvl / 1e18,
-  };
-}
+const { nullAddress, sumTokensExport } = require('../helper/unwrapLPs');
 
 module.exports = {
-  timetravel: true,
-  methodology: "calculate the total amount of TT locked in the veTT contract",
+  methodology: 'calculate the total amount of TT locked in the veTT contract',
   thundercore: {
-    tvl,
+    tvl: sumTokensExport({ owners: ['0xC3C857a9E5Be042C8acF4F2827Aa053e93b5d039'], tokens: [nullAddress], })
   },
-};
+}
