9599aa75d02f2832d38a4452e00094a6e4baef6d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-14 11:23:32 +0200

    collection.xyz: refactor to be on chain

--- projects/collectionxyz/index.js
+++ projects/collectionxyz/index.js
@@ -1,33 +1,24 @@
-const { get } = require("../helper/http");
-const sdk = require("@defillama/sdk");
+const { getLogs } = require('../helper/cache/getLogs')
+const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+const factory = '0x03b51826a4868780db375ee27e5b0adaac5274ee'
 
-let nextCursor = "";
-let shouldContinue = true;
-let tvlEthSum = 0;
-const balances = {};
-
+async function tvl(_, _b, _cb, { api, }) {
+  const logs = await getLogs({
+    api,
+    target: factory,
+    topics: ['0x77948cb83ef3caff9ac13dfab1ea1f8a6875c98370287ce587f5dbc74cc5b6b0'],
+    eventAbi: 'event NewPool (address indexed collection, address indexed poolAddress)',
+    onlyArgs: true,
+    fromBlock: 16932638,
+  })
+  api.log('Pool length: ', logs.length)
+  const tokensAndOwners = logs.map(i => [[i.collection, i.poolAddress], [nullAddress, i.poolAddress]]).flat()
+  return sumTokens2({ api, tokensAndOwners})
+  
+}
 module.exports = {
+  start: 16945809,
   ethereum: {
-    start: 16945809,
-    tvl: async (timestamp, block, chainBlocks, { api }) => {
-      // keep calling until tvlEth is 0
-      while (shouldContinue) {
-        const resp = await get(
-          `https://api.collection.xyz/networks/1/collections?duration=1d&limit=10&sort=tvl:desc&cursor=${nextCursor}`
-        );
-        const { data, cursor } = resp;
-        data?.forEach(({ address, tvlEth }) => {
-          if (tvlEth <= 0) {
-            shouldContinue = false;
-            return;
-          }
-          tvlEthSum += tvlEth;
-        });
-        nextCursor = cursor;
-      }
-      balances.ethereum = tvlEthSum;
-
-      return balances;
-    },
+    tvl,
   },
 };
