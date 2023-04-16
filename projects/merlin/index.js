142d5e07f7d213d0b546eb1d9e7af2cdf9302c99
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-16 20:39:51 +0200

    minor fix

--- projects/merlin/index.js
+++ projects/merlin/index.js
@@ -1,9 +1,7 @@
-const { uniTvlExport } = require("../helper/calculateUniTvl");
+const { getUniTVL } = require("../helper/unknownTokens");
 module.exports = {
-  doublecounted: false,
-  timetravel: true,
   start: 1682899200,
   era: {
-    tvl: uniTvlExport("0x63E6fdAdb86Ea26f917496bEEEAEa4efb319229F", "era"),
+    tvl: getUniTVL({ factory: '0x63E6fdAdb86Ea26f917496bEEEAEa4efb319229F', useDefaultCoreAssets: true, fetchBalances: true, }),
   },
 };
