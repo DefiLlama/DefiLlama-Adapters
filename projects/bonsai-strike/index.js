6f0ed05ad75f3acf02d443c344a4367d6fb89f18
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-18 11:36:48 +0200

    bonsai strike: code refactor

--- projects/bonsai-strike/index.js
+++ projects/bonsai-strike/index.js
@@ -1,24 +1,7 @@
-const sdk = require("@defillama/sdk");
-const { sumTokens2 } = require("../helper/unwrapLPs");
-
-const bnbCallVault = '0x01cEF5B79044E1CCd9b6Ad76c3d0985b5A33F769';
-const bnb = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
-
-async function tvl(_, _1, _2, { api }) {
-  const balances = {};
-
-  const totalBalance = await api.call({
-    abi: "uint256:totalBalance",
-    target: bnbCallVault,
-  });
-
-  await sdk.util.sumSingleBalance(balances, bnb, totalBalance, api.chain)
-
-  return balances;
-}
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
 module.exports = {
   bsc: {
-    tvl,
+    tvl: sumTokensExport({ owners: ['0x01cEF5B79044E1CCd9b6Ad76c3d0985b5A33F769', '0x4b9d1cf13129a79d92bef13dac908e9ffc665ad8'], tokens: ['0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c']}),
   }
 }; 
