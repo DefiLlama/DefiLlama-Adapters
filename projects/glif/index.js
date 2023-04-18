67a38128b95230db546dad6f7810425ad5d31581
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-18 09:28:01 +0200

    glif: minor refactor

--- projects/glif/index.js
+++ projects/glif/index.js
@@ -1,27 +1,12 @@
-const sdk = require("@defillama/sdk");
+const { nullAddress } = require("../helper/tokenMapping");
+
 const IFIL_TOKEN_CONTRACT = "0x690908f7fa93afC040CFbD9fE1dDd2C2668Aa0e0";
 
 async function tvl(_, _1, _2, { api }) {
-  const balances = {};
-
-  const collateralBalance = await api.call({
-    abi: "erc20:totalSupply",
-    target: IFIL_TOKEN_CONTRACT,
-  });
-
-  await sdk.util.sumSingleBalance(
-    balances,
-    IFIL_TOKEN_CONTRACT,
-    collateralBalance,
-    api.chain
-  );
-
-  return balances;
+  api.add(nullAddress, await api.call({ abi: "erc20:totalSupply", target: IFIL_TOKEN_CONTRACT, }))
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology:
     "The Infinity Pool is accepting early depositers by minting 1 iFIL token per 1 FIL deposited. This adapter returns the total supply of iFIL tokens, representing the total amount of FIL and WFIL deposited into the early deposit contract.",
   filecoin: {
