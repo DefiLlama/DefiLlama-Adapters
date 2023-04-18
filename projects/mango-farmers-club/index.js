16b95cdd2f9f7a729fbaa960dee51f8b3f06b1fd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-18 10:11:24 +0200

    refactor mango-farmers-club

--- projects/mango-farmers-club/index.js
+++ projects/mango-farmers-club/index.js
@@ -1,21 +1,7 @@
-const sdk = require('@defillama/sdk');
-const mprAbi = require('./mprAbi.json');
-const BigNumber = require("bignumber.js");
-const { toUSDTBalances } = require("../helper/balances");
-const BASE = BigNumber(10 ** 6);
-
-async function tvl() {
-  const { output: totalDeposit } = await sdk.api.abi.call({
-    target: '0x05Cd3c8Ded966BE2556A8A83190aEF845f24eB76',
-    abi: mprAbi['totalDeposit'],
-    chain: 'polygon_zkevm'
-  });
-  const stakedValue = BigNumber(totalDeposit.toString()).div(BASE);
-  return toUSDTBalances(stakedValue);
-}
+const { sumTokensExport } = require('../helper/unwrapLPs');
 
 module.exports = {
   polygon_zkevm: {
-    tvl
+    tvl: sumTokensExport({ owner: '0x05Cd3c8Ded966BE2556A8A83190aEF845f24eB76', tokens: ['0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035']})
   }
 };
