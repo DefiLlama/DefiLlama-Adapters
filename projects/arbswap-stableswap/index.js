b947b7b7c75150c45653cee8265439a9bc136a15
StagArbswap <104505886+StagArbswap@users.noreply.github.com>
2023-04-19 08:27:14 +0800

    Add Arbswap Stableswap tvl on Arbitrum Nova

--- projects/arbswap-stableswap/index.js
+++ projects/arbswap-stableswap/index.js
@@ -13,8 +13,23 @@ async function tvl(_, _b, _cb, { api, }) {
   return sumTokens2({ api, ownerTokens: logs.map(({ tokenA, tokenB, tokenC, swapContract}) => ([[tokenA, tokenB, tokenC], swapContract]))})
 }
 
+async function arbitrumNovaTvl(_, _b, _cb, { api, }) {
+  const logs = await getLogs({
+    api,
+    target: '0x11f6206AEF570aE946C0117C8f4B298B0A706f18',
+    topics: ['0x48dc7a1b156fe3e70ed5ed0afcb307661905edf536f15bb5786e327ea1933532'],
+    fromBlock: 3732144,
+    eventAbi: 'event NewStableSwapPair (address indexed swapContract, address tokenA, address tokenB, address tokenC, address LP)',
+    onlyArgs: true,
+  })
+  return sumTokens2({ api, ownerTokens: logs.map(({ tokenA, tokenB, tokenC, swapContract}) => ([[tokenA, tokenB, tokenC], swapContract]))})
+}
+
 module.exports = {
   arbitrum: {
     tvl
+  },
+  arbitrum_nova: {
+    tvl: arbitrumNovaTvl
   }
 };
