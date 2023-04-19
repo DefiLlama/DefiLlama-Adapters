2bd9117650e0d7f4bc3cccf52cba338d9877cc0c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-19 14:52:28 +0200

    apex: minor fix

--- projects/apex/index.js
+++ projects/apex/index.js
@@ -1,42 +1,13 @@
-const sdk = require('@defillama/sdk');
-
-const BigNumber = require("bignumber.js");
-
-
+const { sumTokensExport } = require('../helper/unwrapLPs')
 const tokens = [
   '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
 ];
 
-const ownerAddress = [
+const owners = [
   '0xA1D5443F2FB80A5A55ac804C948B45ce4C52DCbb'
 ]
-
-async function tvl (timestamp, block) {
-  const balances = {};
-
-  let balanceOfCalls = [];
-  ownerAddress.forEach((contract) => {
-    balanceOfCalls = [
-      ...balanceOfCalls,
-      ...tokens.map((token) => ({
-        target: token,
-        params: contract
-      }))
-    ];
-  });
-
-  const balanceOfResult = (await sdk.api.abi.multiCall({
-    block,
-    calls: balanceOfCalls,
-    abi: 'erc20:balanceOf',
-  }));
-
-  sdk.util.sumMultiBalanceOf(balances, balanceOfResult, true)
-
-  return balances;
-}
-
 module.exports = {
   start: 15402867,
-  ethereum: { tvl },
+  ethereum: { tvl: sumTokensExport({ owners, tokens, }) },
+  arbitrum: { tvl: () => 0 }
 };
