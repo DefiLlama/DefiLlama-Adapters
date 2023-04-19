3f12dd5084a4aaa64b6cc64f4bb1e36c8bfd80c1
Junwei Chen <kngzhi@hotmail.com>
2023-04-19 15:50:01 -0400

    feat: add lp pool into TVL

---
+++ projects/alchemix/cvx_abi.js
@@ -0,0 +1,5 @@
+module.exports = {
+    abi: {
+        getBalance: "function balanceOf(address account) view returns (uint256)",
+      }
+};
--- projects/alchemix/index.js
+++ projects/alchemix/index.js
@@ -1,7 +1,8 @@
-const { getChainTransform } = require('../helper/portedTokens.js');
-const { sumTokensSharedOwners } = require('../helper/unwrapLPs');
+const sdk = require("@defillama/sdk");
+const { getChainTransform } = require("../helper/portedTokens.js");
+const { sumTokensSharedOwners, genericUnwrapCvx } = require("../helper/unwrapLPs");
 const { staking } = require("../helper/staking.js");
-const contracts = require('./contracts');
+const contracts = require("./contracts");
 
 function tvl(chain) {
   return async (timestamp, block, chainBlocks) => {
@@ -9,14 +10,41 @@ function tvl(chain) {
     const transform = await getChainTransform(chain);
 
     await sumTokensSharedOwners(
-      balances, 
-      Object.values(contracts[chain].underlyingTokens)
-        .concat(Object.values(contracts[chain].yvTokens)), 
-        Object.values(contracts[chain].tokenHolders), 
-        chainBlocks[chain],
-        chain, 
-        transform
+      balances,
+      Object.values(contracts[chain].underlyingTokens).concat(
+        Object.values(contracts[chain].yvTokens)
+      ),
+      Object.values(contracts[chain].tokenHolders),
+      chainBlocks[chain],
+      chain,
+      transform
+    );
+
+    const lpPoolKeys = Object.keys(contracts.cvxLPpools);
+    const lpBalances = await Promise.all(
+      lpPoolKeys.map(async (lpPoolName) => {
+        const { poolAddress, holder, tokenAddress } =
+          contracts.cvxLPpools[lpPoolName];
+        const lpTokenBalance = await sdk.api.abi.call({
+          target: poolAddress,
+          abi: "erc20:balanceOf",
+          params: holder,
+          chain,
+          block: chainBlocks[chain],
+        });
+
+        return lpTokenBalance.output;
+      })
+    );
+
+    lpPoolKeys.forEach((lpPoolName, i) => {
+      const { tokenAddress  } = contracts.cvxLPpools[lpPoolName];
+      sdk.util.sumSingleBalance(
+        balances,
+        tokenAddress,
+        lpBalances[i].toString()
       );
+    });
 
     return balances;
   };
@@ -24,13 +52,16 @@ function tvl(chain) {
 
 module.exports = {
   ethereum: {
-    tvl: tvl('ethereum'),
-    staking: staking(contracts.ethereum.staking.holder, contracts.ethereum.staking.token)
+    tvl: tvl("ethereum"),
+    // staking: staking(
+    //   contracts.ethereum.staking.holder,
+    //   contracts.ethereum.staking.token
+    // ),
   },
-  fantom: {
-    tvl: tvl('fantom')
-  },
-  optimism: {
-    tvl: tvl('optimism')
-  }
+  // fantom: {
+  //   tvl: tvl("fantom"),
+  // },
+  // optimism: {
+  //   tvl: tvl("optimism"),
+  // },
 };
