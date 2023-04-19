6732926861db31d82aa3f1988aef5298d4b8049a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-19 23:08:28 +0200

    zkfox: code refactor

--- projects/zkfox/index.js
+++ projects/zkfox/index.js
@@ -1,103 +1,60 @@
-const sdk = require('@defillama/sdk')
 const abi = require('./abi.json')
 
-
-const chain = 'era'
-
 const contractAddress = {
-    bankHall: '0xA4357F550e034B0f899463A56B346BF38967a194',
-    pool: '0x60E2B7e4893cf40a11ec4Df1B8aD22deBab20218'
+  bankHall: '0xA4357F550e034B0f899463A56B346BF38967a194',
+  pool: '0x60E2B7e4893cf40a11ec4Df1B8aD22deBab20218'
 }
 
+const { compoundExports } = require('../helper/compound')
 
-async function tvl(_, _b, { [chain]: block}) {
-    const { output: bankLen } = await sdk.api.abi.call({
-        abi: abi.boxesLength,
-        target: contractAddress.bankHall,
-        chain, block
-    })
-    const { output: poolAddressList } = await sdk.api.abi.multiCall({
-        abi: abi.boxInfo,
-        calls: Array(parseInt(bankLen)).fill(0).map((item, i) => ({ target: contractAddress.bankHall, params: [i] })),
-        chain, block,
-    })
-    const { output: tokenList } = await sdk.api.abi.multiCall({
-        abi: abi.baseToken,
-        calls: poolAddressList.map(item=> ({ target: item.output })),
-        chain, block,
-    })
-    const { output: depositTotalList } = await sdk.api.abi.multiCall({
-        abi: abi.getDepositTotal,
-        calls: poolAddressList.map(item=> ({ target: item.output })),
-        chain, block
-    })
 
-    const { output: borrowTotalList } = await sdk.api.abi.multiCall({
-        abi: abi.getBorrowTotal,
-        calls: poolAddressList.map(item=> ({ target: item.output })),
-        chain, block
-    })
+async function tvl(_, _b, _1, { api }) {
+  const poolAddressList = await api.fetchList({ lengthAbi: abi.boxesLength, itemAbi: abi.boxInfo, target: contractAddress.bankHall })
+  const tokenList = await api.multiCall({ abi: abi.baseToken, calls: poolAddressList, })
+  const depositTotalList = await api.multiCall({ abi: abi.getDepositTotal, calls: poolAddressList, })
+  const borrowTotalList = await api.multiCall({ abi: abi.getBorrowTotal, calls: poolAddressList, })
 
-    
-    const balances = {}
-    for (let i = 0; i < depositTotalList.length; i++) {
-        const token = tokenList[i].output
-        const depositTotal = depositTotalList[i].output
-        const borrowTotal = borrowTotalList[i].output
-        await sdk.util.sumSingleBalance(balances, token, depositTotal, chain)
-        await sdk.util.sumSingleBalance(balances, token, borrowTotal, chain)
+  for (let i = 0; i < depositTotalList.length; i++) {
+    const token = tokenList[i]
+    const depositTotal = depositTotalList[i]
+    const borrowTotal = borrowTotalList[i]
+    api.add(token, depositTotal)
+    // api.add(token, borrowTotal) // we do not consider borrowed tokens as tvl
+  }
+
+  const fpoolAllInfoList = await api.fetchList({ lengthAbi: abi.poolLength, itemAbi: abi.poolInfo, target: contractAddress.pool })
+  const tokenSymbolList = await api.multiCall({ abi: 'erc20:symbol', calls: fpoolAllInfoList.map(item => item.lpToken), })
+  const fPoolIndex = []
+  for (let i = 0; i < tokenSymbolList.length; i++) {
+    const symbol = tokenSymbolList[i]
+    if (symbol.startsWith('f')) {
+      fPoolIndex.push(i)
     }
-    const { output: poolLen } = await sdk.api.abi.multiCall({
-        abi: abi.poolLength,
-        calls: [{ target: contractAddress.pool }],
-        chain, block,
-    })
-    
-    const { output: fpoolAllInfoList } = await sdk.api.abi.multiCall({
-        abi: abi.poolInfo,
-        calls: Array(parseInt(poolLen[0].output)).fill(0).map((item, i) => ({ target: contractAddress.pool, params: [i] })),
-        chain, block,
-    })
-    const { output: tokenSymbolList } = await sdk.api.abi.multiCall({
-        abi: 'erc20:symbol',
-        calls: fpoolAllInfoList.map(item=>({ target: item.output.lpToken })),
-        chain, block,
-    })
-    const fPoolIndex = []
-    for(let i=0;i<tokenSymbolList.length;i++) {
-        const symbol = tokenSymbolList[i].output
-        if (symbol.startsWith('f')) {
-            fPoolIndex.push(i)
-        }
-    }
-   const { output: poolTokenList } = await sdk.api.abi.multiCall({
-        abi: abi.baseToken,
-        calls: fPoolIndex.map(item=>({ target: fpoolAllInfoList[item].output.lpToken })),
-        chain, block,
-   })
-   const { output: coefficientList } = await  sdk.api.abi.multiCall({
-        abi: abi.getBaseTokenPerLPToken,
-        calls: fPoolIndex.map(item=>({ target: fpoolAllInfoList[item].output.lpToken })),
-        chain, block,
-    })
+  }
+  const poolTokenList = await api.multiCall({
+    abi: abi.baseToken,
+    calls: fPoolIndex.map(item => ({ target: fpoolAllInfoList[item].lpToken })),
+  })
+  const coefficientList = await api.multiCall({
+    abi: abi.getBaseTokenPerLPToken,
+    calls: fPoolIndex.map(item => ({ target: fpoolAllInfoList[item].lpToken })),
+  })
 
-    for(let i=0;i<fPoolIndex.length;i++) {
-        const totalAmount = fpoolAllInfoList[fPoolIndex[i]].output.totalAmount
-        const coefficient = coefficientList[i].output
-        const token = poolTokenList[i].output
-        await sdk.util.sumSingleBalance(balances, token, (totalAmount * coefficient)/1e18, chain)
-    }
-    
-    return balances
+  for (let i = 0; i < fPoolIndex.length; i++) {
+    const totalAmount = fpoolAllInfoList[fPoolIndex[i]].totalAmount
+    const coefficient = coefficientList[i]
+    const token = poolTokenList[i]
+    api.add(token, (totalAmount * coefficient) / 1e18)
+  }
 }
 
-
+module.exports = {
+  era: {
+    tvl
+  }
+}
 
 
 module.exports = {
-    methodology: "Data is retrieved from the api at https://zkfox.io",
-    era: {
-        tvl
-    }
+  era: compoundExports("0xc3D157Ee5D602E9CEAF6eA4c15C9b52B313A1364"),
 }
-
