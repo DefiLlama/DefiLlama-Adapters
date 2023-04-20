4a86731284a4fa939ee5e8576a061c3339eb7782
cheng <chenchengalex2014@gmail.com>
2023-04-20 15:27:55 +0200

    feat:Remove veth1.0 and update bifrost tvl

--- projects/bifrost-staking/index.js
+++ projects/bifrost-staking/index.js
@@ -1,18 +1,43 @@
 const { getExports } = require('../helper/heroku-api')
 const { nullAddress } = require('../helper/unwrapLPs')
-const BigNumber = require("bignumber.js");
 
 module.exports = {
 	timetravel: false,
-	...getExports("bifrost-staking", ['bifrost']),
+	bifrost: {
+		tvl: async () => {
+			const { bifrost } = getExports("bifrost-staking", ['bifrost'])
+			const tvl = await bifrost.tvl()
+			return { 'bifrost-native-coin': tvl['bifrost-native-coin'], polkadot: tvl.polkadot, kusama: tvl.kusama }
+		}
+	},
 	ethereum: {
 		tvl: async (_, _1, _2, { api }) => {
 			const vETH = await api.call({ target: '0x4bc3263eb5bb2ef7ad9ab6fb68be80e43b43801f', abi: 'uint256:totalSupply' })
-			const vETH_v1 = await api.call({ target: '0xc3d088842dcf02c13699f936bb83dfbbc6f721ab', abi: 'uint256:totalSupply' })
 
 			return {
-				[nullAddress]: new BigNumber(vETH).plus(vETH_v1).toString(),
+				[nullAddress]: vETH,
 			}
+		}
+	},
+	filecoin: {
+		tvl: async () => {
+			const { bifrost } = getExports("bifrost-staking", ['bifrost'])
+			const { filecoin } = await bifrost.tvl()
+			return { filecoin }
+		}
+	},
+	moonbeam: {
+		tvl: async () => {
+			const { bifrost } = getExports("bifrost-staking", ['bifrost'])
+			const { moonbeam } = await bifrost.tvl()
+			return { moonbeam }
+		}
+	},
+	moonriver: {
+		tvl: async () => {
+			const { bifrost } = getExports("bifrost-staking", ['bifrost'])
+			const { moonriver } = await bifrost.tvl()
+			return { moonriver }
 		}
 	}
 }
