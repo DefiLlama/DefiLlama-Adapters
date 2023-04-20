d085cee8b82dd6e0a47f7640467a21e138c424fd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-20 22:13:21 +0200

    fix shadeswap

--- projects/shadeswap/index.js
+++ projects/shadeswap/index.js
@@ -2,8 +2,8 @@ const { get } = require("../helper/http")
 
 async function tvl(_, _b, _cb, { api, }) {
   const data = await get('https://na36v10ce3.execute-api.us-east-1.amazonaws.com/API-mainnet-STAGE/shadeswap/pairs')
-  return { 
-    tether: data.map(i =>i.liquidity_usd/1e12 > 1e8 ? i.liquidity_usd/1e16 : i.liquidity_usd/1e12 ).reduce((a, i) => a+i)
+  return {
+    tether: data.map(i => +i.liquidity_usd).reduce((a, i) => a + i)
   }
 
 }
