1e90c9958a889eb611b30a867cec39af95406075
EVMlord <85406816+EVMlord@users.noreply.github.com>
2023-04-20 13:56:17 +0100

    Update index.js
    Refactor individual chain tvl calculations

--- projects/kimberlite/index.js
+++ projects/kimberlite/index.js
@@ -4,8 +4,8 @@ const { vestingHelper } = require("../helper/unknownTokens");
 const { config } = require('./config')
 
 async function calculateTvl(contract, chain, block) {
-  
-  const blacklist = ["0xC95056c9c9f5CcaEbc2F343CB4fBc75BFf64Eb78"];
+	
+  const blacklist = [];
   
   const { output: lengths } = await sdk.api.abi.multiCall({
     abi: abi.depositId,
@@ -14,6 +14,10 @@ async function calculateTvl(contract, chain, block) {
   })
   
   const contractBalance = await getBalances(contract, lengths[0].output);
+  
+  const contractBalance = await getBalances(contract, lengths[0].output);
+	console.log("contractBalance:", contractBalance);
+
   Object.entries(contractBalance).forEach(([token, val]) => {
   sdk.util.sumSingleBalance(balances, token, val);
   })
@@ -33,58 +37,35 @@ async function calculateTvl(contract, chain, block) {
       owner: vault,
       tokens,
       block, chain,
-      log_coreAssetPrices: [
-        300/ 1e18,
-        1/ 1e18,
-        1/ 1e18,
-        1/ 1e18,
-      ],
-      log_minTokenValue: 1e6,
     })
   }
 };
 
-/* async function getTotalTvl(block) {
-  const networks = ['ethereum', 'bsc', 'metis', 'polygon', 'core', 'dogechain', 'arbitrum'];
-  const promises = networks.map(async (network) => {
-    const tvl = await module.exports[network].tvl(null, null, { [network]: block });
-    return tvl;
-  });
+const chains = [
+  'arbitrum',
+  'bsc',
+  'ethereum',
+  'polygon',
+  'core',
+  'dogechain',
+  "metis"
+]
 
-  const tvlArray = await Promise.all(promises);
-  const totalTvl = tvlArray.reduce((accumulator, tvl) => {
-    for (const [token, value] of Object.entries(tvl)) {
-      sdk.util.sumSingleBalance(accumulator, token, value);
-    }
-    return accumulator;
-  }, {});
+module.exports = {}
 
-  return totalTvl;
-} */
+chains.forEach(chain => {
+  let contract = config.kimberliteSafeBSC.locker
 
-module.exports = {
-  methodology:
-    "Counts TVL of all the tokens locked on the Kimberlite Safe locker smart contracts.",
-  //tvl: getTotalTvl,
-  ethereum: {
-    tvl: async (_, _b, { ethereum: block }) => calculateTvl(config.kimberliteSafeETH.locker, config.kimberliteSafeETH.chain, block)
-  },
-  bsc: {
-    tvl: async (_, _b, { bsc: block }) => calculateTvl(config.kimberliteSafeBSC.locker, config.kimberliteSafeBSC.chain, block)
-  },
-  metis: {
-    tvl: async (_, _b, { metis: block }) => calculateTvl(config.kimberliteSafeMETIS.locker, config.kimberliteSafeMETIS.chain, block)
-  },
-  polygon: {
-    tvl: async (_, _b, { polygon: block }) => calculateTvl(config.kimberliteSafeMATIC.locker, config.kimberliteSafeMATIC.chain, block)
-  },
-  core: {
-    tvl: async (_, _b, { core: block }) => calculateTvl(config.kimberliteSafeCORE.locker, config.kimberliteSafeCORE.chain, block)
-  },
-  dogechain: {
-    tvl: async (_, _b, { dogechain: block }) => calculateTvl(config.kimberliteSafeDOGE.locker, config.kimberliteSafeDOGE.chain, block)
-  },
-  arbitrum: {
-    tvl: async (_, _b, { arbitrum: block }) => calculateTvl(config.kimberliteSafeARB.locker, config.kimberliteSafeARB.chain, block)
-  },
-}
+  switch (chain) {
+    case 'arbitrum': contract = config.kimberliteSafeARB.locker; break;
+    case 'ethereum': contract = config.kimberliteSafeETH.locker; break;
+    case 'metis': contract = config.kimberliteSafeMETIS.locker; break;
+    case 'polygon': contract = config.kimberliteSafeMATIC.locker ; break;
+	case 'core': contract = config.kimberliteSafeCORE.locker ; break;
+	case 'dogechain': contract = config.kimberliteSafeDOGE.locker ; break;
+  }
+
+  module.exports[chain] = {
+    tvl: async (_, _b, { [chain]: block }) => calculateTvl(contract, block, chain),
+  }
+})
