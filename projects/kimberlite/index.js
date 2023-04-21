594d758d9688cfc1c7a2de10a03cecffee1639b3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-21 22:21:35 +0200

    refactor kimberlite

--- projects/kimberlite/index.js
+++ projects/kimberlite/index.js
@@ -1,152 +1,18 @@
-const sdk = require('@defillama/sdk')
-const {config} = require('./config')
+const { config } = require('./config')
 const abi = require('./abi.json')
-const {sumTokens2} = require('../helper/unwrapLPs')
-const {vestingHelper} = require('../helper/unknownTokens')
+const { sumUnknownTokens, } = require('../helper/unknownTokens')
 
-async function calculateTvl(contract, chain, block) {
-    const {output: lengths} = await sdk.api.abi.call({
-        target: contract,
-        abi: abi.depositId,
-        chain,
-        block,
-    })
+module.exports = {
+};
 
-    console.log('Chain:', chain) // Add this line to see the value of chain
-    console.log('Contract:', contract) // Add this line to see the value of contract
-    console.log('Lengths:', lengths) // Add this line to see the content of lengths object
-    console.log('Block:', block) // Add this line to see the value of block
-
-    const balancesA = await getBalances(contract, chain, block, lengths)
-
-    console.log('BalancesA:', balancesA) // Add this line to see the value of balancesA
-
-    let tokensAndOwners = await mapTokensToContract(
-        contract,
-        chain,
-        block,
-        lengths
-    )
-
-    console.log('TokensAndContract:', tokensAndOwners) // Add this line to see the value of tokensAndContract
-
-    const balancesB = await sumTokens2({tokensAndOwners, chain, block})
-
-    console.log('BalancesB:', balancesB) // Add this line to see the value of balancesB
-
-    const balancesC = {}
-
-    // Iterate through the keys of balancesA and add them to balancesC
-    for (const key in balancesA) {
-        balancesC[key] = BigInt(balancesA[key]).toString()
+Object.values(config).forEach(({ chain, locker, startBlock }) => {
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const data = await api.fetchList({ lengthAbi: abi.depositId, itemAbi: abi.lockedToken, target: locker })
+      const tokensAndOwners = data
+        // .filter(i => !i.withdrawn)
+        .map((i) => [i.tokenAddress, locker])
+      return sumUnknownTokens({ api, tokensAndOwners, useDefaultCoreAssets: true, resolveLP: true, })
     }
-
-    // Iterate through the keys of balancesB
-    for (const key in balancesB) {
-        // If the key already exists in balancesC, add the balance from balancesB
-        if (balancesC.hasOwnProperty(key)) {
-            balancesC[key] = (
-                BigInt(balancesC[key]) + BigInt(balancesB[key])
-            ).toString()
-        } else {
-            // If the key doesn't exist in balancesC, add it with the balance from balancesB
-            balancesC[key] = BigInt(balancesB[key]).toString()
-        }
-    }
-    console.log('BalancesC:', balancesC) // Add this line to see the value of balancesC
-
-    return balancesC
-}
-
-async function mapTokensToContract(contract, chain, block, length) {
-    const calls = []
-    const tokensArray = []
-
-    for (let i = 1; i <= length; i++) {
-        calls.push({target: contract, params: [i]}) // Pass the parameter as an array
-    }
-
-    console.log('Calls:', calls) // Add this line to see the content of calls array
-
-    const {output} = await sdk.api.abi.multiCall({
-        abi: abi.lockedToken,
-        requery: true,
-        calls,
-        chain,
-        block,
-    })
-
-    for (const item of output) {
-        tokensArray.push(item.output.tokenAddress)
-    }
-
-    const uniqueTokens = filterDuplicates(tokensArray)
-    const mappedTokens = uniqueTokens.map((token, i) => [token, contract])
-    return mappedTokens
-}
-
-async function getBalances(contract, chain, block, length) {
-    const calls = []
-    for (let i = 1; i <= length; i++) calls.push({target: contract, params: i})
-
-    console.log('Calls:', calls) // Add this line to see the content of calls array
-
-    const {output} = await sdk.api.abi.multiCall({
-        abi: abi.lockedToken,
-        requery: true,
-        calls,
-        chain,
-        block,
-    })
-
-    const tokens = output.map((i) => i.output.tokenAddress)
-
-    const blacklist = []
-
-    return await vestingHelper({
-        useDefaultCoreAssets: true,
-        blacklist,
-        owner: contract,
-        tokens,
-        block,
-        chain,
-        log_coreAssetPrices: [300 / 1e18, 1 / 1e18, 1 / 1e18, 1 / 1e18],
-        log_minTokenValue: 1e6,
-    })
-}
-
-function filterDuplicates(tokensArray) {
-    return [...new Set(tokensArray.map((token) => token.toLowerCase()))]
-}
-
-const chains = ['arbitrum', 'bsc', 'polygon', 'core', 'dogechain', 'metis']
-
-module.exports = {}
-
-chains.forEach((chain) => {
-    let contract = config.kimberliteSafeBSC.locker
-
-    switch (chain) {
-        case 'arbitrum':
-            contract = config.kimberliteSafeARB.locker
-            break
-        case 'metis':
-            contract = config.kimberliteSafeMETIS.locker
-            break
-        case 'polygon':
-            contract = config.kimberliteSafeMATIC.locker
-            break
-        case 'core':
-            contract = config.kimberliteSafeCORE.locker
-            break
-        case 'dogechain':
-            contract = config.kimberliteSafeDOGE.locker
-            break
-    }
-
-    module.exports[chain] = {
-        misrepresentedTokens: true,
-        tvl: async (_, _b, {[chain]: block}) =>
-            calculateTvl(contract, chain, block),
-    }
+  }
 })
