321f3eb6d85a592b559831218c69e6535cc4fad1
rf <hello@reactorfusion.xyz>
2023-04-24 01:24:59 +0900

    debut or debug?

---
+++ projects/reactorfusion/index.js
@@ -0,0 +1,6 @@
+const { compoundExports } = require('../helper/compound');
+const { nullAddress } = require('../helper/tokenMapping');
+
+module.exports = {
+  era: compoundExports('0x23848c28Af1C3AA7B999fA57e6b6E8599C17F3f2', 'era', '0xC5db68F30D21cBe0C9Eac7BE5eA83468d69297e6', nullAddress),
+}; 
