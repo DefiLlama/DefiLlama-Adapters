33141a9475466f07b8a218571aa26defb317eb1e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-23 19:35:45 +0200

    reactor fusion: minor refactor

--- projects/reactorfusion/index.js
+++ projects/reactorfusion/index.js
@@ -1,6 +1,8 @@
-const { compoundExports } = require('../helper/compound');
-const { nullAddress } = require('../helper/tokenMapping');
+const { compoundExports2 } = require('../helper/compound');
 
 module.exports = {
-  era: compoundExports('0x23848c28Af1C3AA7B999fA57e6b6E8599C17F3f2', 'era', '0xC5db68F30D21cBe0C9Eac7BE5eA83468d69297e6', nullAddress),
+  era: compoundExports2({
+    comptroller: '0x23848c28Af1C3AA7B999fA57e6b6E8599C17F3f2',
+    cether: '0xC5db68F30D21cBe0C9Eac7BE5eA83468d69297e6',
+  })
 }; 
