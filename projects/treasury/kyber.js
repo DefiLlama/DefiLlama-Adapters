61a14aa9256cfeeb59d05cf2a302fd9694d17ea8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-23 22:38:28 +0200

    Add arbitrum project treasuries

---
+++ projects/treasury/kyber.js
@@ -0,0 +1,9 @@
+const {  nullAddress,treasuryExports } = require("../helper/treasury");
+
+module.exports = treasuryExports({
+  arbitrum: {
+    tokens: ['0x3F56e0c36d275367b8C502090EDF38289b3dEa0d'],
+    owners: ['0x91c9d4373b077ef8082f468c7c97f2c499e36f5b'],
+    ownTokens: ['0xe4dddfe67e7164b0fe14e218d80dc4c08edc01cb'],
+  },
+})
