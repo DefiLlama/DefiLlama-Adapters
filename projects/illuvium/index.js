14ef8ad3e1b44de84fc66888179304d9f05b2e37
Chris Gutkowski <kgutko@gmail.com>
2023-04-24 09:36:27 +0200

    separate into pool2 and staking categories

--- projects/illuvium/index.js
+++ projects/illuvium/index.js
@@ -1,5 +1,6 @@
 const { api } = require("@defillama/sdk")
-const BigNumber = require("bignumber.js")
+
+const { staking } = require("../helper/staking")
 
 const ILV_POOL_V2_ADDRESS = "0x7f5f854FfB6b7701540a00C69c4AB2De2B34291D"
 const ILV_TOKEN_ADDRESS = "0x767FE9EDC9E0dF98E07454847909b5E959D7ca0E"
@@ -7,27 +8,14 @@ const SLP_TOKEN_ADDRESS = "0x6a091a3406E0073C3CD6340122143009aDac0EDa"
 const LP_POOL_V1_ADDRESS = "0x8B4d8443a0229349A9892D4F7CbE89eF5f843F72"
 const LP_POOL_V2_ADDRESS = "0xe98477bDc16126bB0877c6e3882e3Edd72571Cc2"
 
-async function tvl() {
-  const [{ output: ilvPoolTvl }, { output: lpPoolLpBalance }, { output: lpPoolV1LpBalance }] = await Promise.all([
-    // ILVPool
-    api.abi.call({
-      abi: "function getTotalReserves() view returns (uint256 totalReserves)",
-      target: ILV_POOL_V2_ADDRESS,
-    }),
-    // SushiLPPool
-    api.erc20.balanceOf({
-      owner: LP_POOL_V2_ADDRESS,
-      target: SLP_TOKEN_ADDRESS,
-    }),
-    api.erc20.balanceOf({
-      owner: LP_POOL_V1_ADDRESS,
-      target: SLP_TOKEN_ADDRESS,
-    })
-  ])
+async function ilvStaking() {
+  const { output: ilvPoolTvl } = await api.abi.call({
+    abi: "function getTotalReserves() view returns (uint256 totalReserves)",
+    target: ILV_POOL_V2_ADDRESS,
+  })
 
   return {
-    [ILV_TOKEN_ADDRESS]: new BigNumber(ilvPoolTvl),
-    [SLP_TOKEN_ADDRESS]: new BigNumber(lpPoolLpBalance).plus(lpPoolV1LpBalance),
+    [ILV_TOKEN_ADDRESS]: ilvPoolTvl,
   }
 }
 
@@ -35,6 +23,8 @@ module.exports = {
   timetravel: true,
   methodology: `Based on ILV reserves in the ILVPool contract and SLP in SushiLPPool contracts.`,
   ethereum:{
-    tvl,
+    tvl: () => 0,
+    pool2: staking([LP_POOL_V1_ADDRESS, LP_POOL_V2_ADDRESS], SLP_TOKEN_ADDRESS),
+    staking: ilvStaking,
   },
 }
