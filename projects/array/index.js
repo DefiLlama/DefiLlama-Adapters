8ff299ec19744cf11128f8c8d6df9de8b09c4646
ArrayCapital <128329694+ArrayCapital@users.noreply.github.com>
2023-04-25 16:16:17 +0200

    Update index.js

--- projects/array/index.js
+++ projects/array/index.js
@@ -1,23 +1,11 @@
-const sdk = require('@defillama/sdk');
+const { staking } = require('../helper/staking');
 
-const token = "0x164731CD270daA4A94bc70761E53320e48367B8B";
+const token = "0x164731cd270daa4a94bc70761e53320e48367b8b";
 const masterchef = "0x1b91b24d12C934383f25aa07C2c9C9666accf39e";
-
-const stakingToken = token.toLowerCase();
-async function getTvl(chainBlocks) {
-
-  let locked = (await sdk.api.erc20.balanceOf({
-    target: token,
-    owner: masterchef,
-    block: chainBlocks.arbitrum,
-    chain: 'arbitrum'
-  })).output;
-
-  return locked;
-}
 
 module.exports = {
   arbitrum: {
-    tvl: getTvl
+    tvl: () => 0,
+    staking: staking(masterchef, token)
   }
 }
