1e8e4984f0e55394e7596c009debb962640e97fc
dickensodera <dickens@laevitas.ch>
2023-04-25 09:29:59 +0300

    feat: Added Ajira Pay Finance Staking TVL on KAVA network

--- projects/ajira-pay-finance/index.js
+++ projects/ajira-pay-finance/index.js
@@ -1,11 +1,13 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { staking } = require("../helper/staking");
 
-const FACTORIES = "0x9DBC0Ad09184226313FbDe094E7c3DD75c94f997"
+const AJP_CONTRACT_ADDRESS = "0x9DBC0Ad09184226313FbDe094E7c3DD75c94f997"
+const STAKING_CONTRACT = "0x894E327f11b09ab87Af86876dCfCEF40eA086f34"
 
 module.exports = {
     misrepresentedTokens: true,
-    methodology: "Ajira Pay Finance Tvl Calculation",
+    methodology: "Ajira Pay Finance Tvl Calculations are based on AJP Staking statistics on KAVA",
     kava: {
-        tvl: getUniTVL({ factory: FACTORIES, chain: 'kava', useDefaultCoreAssets: true }),
-    }
+        staking: staking(STAKING_CONTRACT, AJP_CONTRACT_ADDRESS, "kava"),
+        tvl: () => 0,
+    },
 };
