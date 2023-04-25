0faf42a66b2ac52376ca53dbb2000120f28de6d8
dickensodera <dickens@laevitas.ch>
2023-04-25 10:12:07 +0300

    update: Update tvl functions

--- projects/ajira-pay-finance/index.js
+++ projects/ajira-pay-finance/index.js
@@ -4,10 +4,11 @@ const AJP_CONTRACT_ADDRESS = "0x9DBC0Ad09184226313FbDe094E7c3DD75c94f997"
 const STAKING_CONTRACT = "0x894E327f11b09ab87Af86876dCfCEF40eA086f34"
 
 module.exports = {
+    timetravel: true,
     misrepresentedTokens: true,
-    methodology: "Ajira Pay Finance Tvl Calculations are based on AJP Staking statistics on KAVA",
+    methodology: "Ajira Pay Finance Tvl Calculations are based on AJP Staking pool",
     kava: {
         staking: staking(STAKING_CONTRACT, AJP_CONTRACT_ADDRESS, "kava"),
-        tvl: () => 0,
+        tvl: () => ({})
     },
 };
