4a8f29e4cf6a2c6bb90f5a03fbd116f1a2c6cf23
Sergey <itsib.su@gmail.com>
2023-04-25 19:51:18 +0700

    Added Binance support

--- projects/quadrat/index.js
+++ projects/quadrat/index.js
@@ -7,54 +7,74 @@ module.exports = {
   start: 1667197843, // Mon Oct 31 2022 06:30:43 GMT+0000
 };
 
-const config = {
-  ethereum: {
-    factory: '0x823db50c56d8a994af0ceb3f7dc421852cf6fbff',
-    fromBlock: 15891335,
-  },
-  optimism: {
-    factory: '0x3ae3b506a39ffc1aa3964f6dc888891ea10671ed',
-    fromBlock: 33415677,
-  },
-  polygon: {
-    factory: '0x1a639e9249d26c70edf0b7410a8495d9b72140ff',
-    fromBlock: 35028277,
-  },
-  arbitrum: {
-    factory: '0xb8d498f025c45a8a7a63277cb1cca36c2599bbd7',
-    fromBlock: 34510988,
-  },
+const configs = {
+  ethereum: [
+    {
+      factory: '0x823db50c56d8a994af0ceb3f7dc421852cf6fbff',
+      fromBlock: 15891335,
+    }
+  ],
+  polygon: [
+    {
+      factory: '0x1a639e9249d26c70edf0b7410a8495d9b72140ff',
+      fromBlock: 35028277,
+    }
+  ],
+  optimism: [
+    {
+      factory: '0x3ae3b506a39ffc1aa3964f6dc888891ea10671ed',
+      fromBlock: 33415677,
+    }
+  ],
+  arbitrum: [
+    {
+      factory: '0xb8d498f025c45a8a7a63277cb1cca36c2599bbd7',
+      fromBlock: 34510988,
+    }
+  ],
+  bsc: [
+    {
+      factory: '0x28e9f86690449059734e079eaaa66d8913263bed', // For Uniswap V3
+      fromBlock: 26497758,
+    },
+    {
+      factory: '0xce5ee90894f79a184919a5d3afd1f754b35a919d', // For PancakeSwap V3
+      fromBlock: 27461875,
+    }
+  ],
 }
 
-Object.keys(config).forEach(chain => {
-  const { factory, fromBlock, } = config[chain]
+Object.keys(configs).forEach(chain => {
+  const config = configs[chain]
   module.exports[chain] = {
     tvl: async (_, _b, _cb, { api, }) => {
       const balances = {}
 
-      const logs = await getLogs({
-        api,
-        target: factory,
-        topics: ['0x9c5d829b9b23efc461f9aeef91979ec04bb903feb3bee4f26d22114abfc7335b'],
-        fromBlock,
-        onlyArgs: true,
-        eventAbi: 'event PoolCreated (address indexed uniPool, address indexed manager, address indexed pool)'
-      })
+      for (const { factory, fromBlock } of config) {
+        const logs = await getLogs({
+          api,
+          target: factory,
+          topics: ['0x9c5d829b9b23efc461f9aeef91979ec04bb903feb3bee4f26d22114abfc7335b'],
+          fromBlock,
+          onlyArgs: true,
+          eventAbi: 'event PoolCreated (address indexed uniPool, address indexed manager, address indexed pool)'
+        })
 
-      const calls = logs.map(i => i.pool)
+        const calls = logs.map(i => i.pool)
 
-      const [token0, token1, bals] = await Promise.all([
-        api.multiCall({ abi: 'address:token0', calls }),
-        api.multiCall({ abi: 'address:token1', calls }),
-        api.multiCall({ abi: 'function getUnderlyingBalances() view returns (uint256 amount0, uint256 amount1)', calls }),
-      ])
+        const [token0, token1, bals] = await Promise.all([
+          api.multiCall({ abi: 'address:token0', calls }),
+          api.multiCall({ abi: 'address:token1', calls }),
+          api.multiCall({ abi: 'function getUnderlyingBalances() view returns (uint256 amount0, uint256 amount1)', calls }),
+        ])
 
-      bals.forEach((val, i) => {
-        if (!val) return;
-        const { amount0, amount1 }  = val
-        sdk.util.sumSingleBalance(balances, token0[i], amount0, api.chain)
-        sdk.util.sumSingleBalance(balances, token1[i], amount1, api.chain)
-      })
+        bals.forEach((val, i) => {
+          if (!val) return;
+          const { amount0, amount1 }  = val
+          sdk.util.sumSingleBalance(balances, token0[i], amount0, api.chain)
+          sdk.util.sumSingleBalance(balances, token1[i], amount1, api.chain)
+        })
+      }
 
       return balances
     }
