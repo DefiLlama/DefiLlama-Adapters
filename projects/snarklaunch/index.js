d9cad80831a7cfc1ef2d3d801007c3cf3592648d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-25 20:56:33 +0200

    Update index.js

--- projects/snarklaunch/index.js
+++ projects/snarklaunch/index.js
@@ -6,6 +6,7 @@ const SNRK_TOKEN = "0x533b5F887383196C6bc642f83338a69596465307";
 module.exports = {
   methodology: 'TVL for SNRK staking is computed by summing the balance of SNRK tokens held by the staking contract.',
   era: {
-    tvl: sumTokensExport({ owners: [STAKING_SNRK_CONTRACT], tokens: [SNRK_TOKEN] })
+    staking: sumTokensExport({ owners: [STAKING_SNRK_CONTRACT], tokens: [SNRK_TOKEN] }),
+    tvl: () => 0,
   },
 }
