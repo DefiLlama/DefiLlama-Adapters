cc7472e9d4ecc10e1e7a58f7b4c4399d4561edbf
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-26 08:36:28 +0200

    minor fix

--- projects/zkBoost/index.js
+++ projects/zkBoost/index.js
@@ -12,7 +12,6 @@ Object.keys(config).forEach(chain => {
         const data = await api.fetchList({  lengthAbi: abi.getTotalLockCount, itemAbi: abi.getLockAt, target: vault })
         await sumTokens2({ api, owner: vault, tokens: data.map(i => i.token), resolveLP: true, })
       }
-      return api.getBalances()
     }
   }
 })
