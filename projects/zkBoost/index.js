a89708785f7a0ed7fc69c2aad2f529896a995a79
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-26 08:36:16 +0200

    zkboost: reafctor code

--- projects/zkBoost/index.js
+++ projects/zkBoost/index.js
@@ -1,4 +1,3 @@
-const sdk = require("@defillama/sdk")
 const abi = require('./abi')
 const config = require('./config')
 const { sumTokens2 } = require("../helper/unwrapLPs")
@@ -7,38 +6,13 @@ module.exports = {}
 
 Object.keys(config).forEach(chain => {
   module.exports[chain] = {
-    tvl: async (timestamp, _block, { [chain]: block }) => {
-      const balances = {}
-      const { vaults, blacklist, log_coreAssetPrices, log_minTokenValue, } = config[chain]
+    tvl: async (timestamp, _block, { [chain]: block }, { api }) => {
+      const { vaults, } = config[chain]
       for (const vault of vaults) {
-
-        let calls = []
-        const { output: size } = await sdk.api.abi.call({
-          target: vault,
-          abi: abi.getTotalLockCount,
-          chain, block,
-        })
-
-        const isLastVault = vault === vaults[vaults.length - 1]
-        const lockAbi = isLastVault ? abi.getLockAt : abi.getLock
-        for (let i = 0; i < +size; i++)
-          calls.push({ params: i })
-        let { output: tokens } = await sdk.api.abi.multiCall({
-          target: vault,
-          abi: lockAbi,
-          calls,
-          chain, block,
-        })
-
-        tokens = tokens.map(i => i.output[1])
-
-        const balance = await sumTokens2({ chain, block, owner: vault, tokens: tokens })
-
-        Object.entries(balance).forEach(([token, bal]) => sdk.util.sumSingleBalance(balances, token, bal))
+        const data = await api.fetchList({  lengthAbi: abi.getTotalLockCount, itemAbi: abi.getLockAt, target: vault })
+        await sumTokens2({ api, owner: vault, tokens: data.map(i => i.token), resolveLP: true, })
       }
-
-      return balances
+      return api.getBalances()
     }
-
   }
 })
