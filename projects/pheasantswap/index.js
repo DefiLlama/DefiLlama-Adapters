59e760e696acbbc7802ca4420476fb2b484bc242
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-26 17:58:19 +0530

    Update index.js

--- projects/pheasantswap/index.js
+++ projects/pheasantswap/index.js
@@ -1,14 +1,5 @@
-const { get } = require("../helper/http")
-async function fetch() {
-  const {liquidityList} = await get('http://info.pheasantswap.com/swap/operate/getSwapStatisticsList')
-  const liquidity = liquidityList[liquidityList.length -1]
-  return liquidity.amount*1
-}
+const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
-  methodology: "PheasantSwap - A Trustworthy Decentralized Exchange Built on ENULS.",
-  enuls: {
-    fetch
-  },
-  fetch
+  enuls: { tvl: getUniTVL({ factory: '0x7bf960B15Cbd9976042257Be3F6Bb2361E107384', useDefaultCoreAssets: true, fetchBalances: true }), },
 }
