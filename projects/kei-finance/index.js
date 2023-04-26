09c494861c3906e60e6bc3f24cfb527b5f271a36
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-26 16:56:10 +0200

    fix kei staking

--- projects/kei-finance/index.js
+++ projects/kei-finance/index.js
@@ -1,10 +1,14 @@
-const { staking } = require('../helper/staking')
 const TOKEN_ADDRESS = "0xF75C7a59bCD9bd207C4Ab1BEB0b32EEd3B6392f3";
-const STAKING_CONTRACT = "...";
+const STAKING_CONTRACT = '0x463beb323b229d52F530Be23C2E829C78904D8a8'
+
+async function staking(_, _b, _cb, { api, }) {
+  const bal = await api.call({  abi: 'uint256:totalValue', target: STAKING_CONTRACT})
+  api.add(TOKEN_ADDRESS, bal)
+}
 
 module.exports = {
   ethereum: {
     tvl: () => 0,
-    staking: staking(STAKING_CONTRACT, TOKEN_ADDRESS)
+    staking
   }
 };
