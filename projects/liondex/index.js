644fee3bac611792b7e6bb266c2b799d11ae2b2b
LionDEX <131145154+LionDEXDev@users.noreply.github.com>
2023-04-26 19:28:55 +0800

    exclude treasury from tvl

--- projects/liondex/index.js
+++ projects/liondex/index.js
@@ -1,7 +1,7 @@
 const { staking } = require('../helper/staking')
 const { sumTokens2,sumTokensExport } = require('../helper/unwrapLPs')
 
-const treasure = "0x7fca3bf8adc4e143bd789aecda36c0ce34f1d75b";
+//const treasure = "0x7fca3bf8adc4e143bd789aecda36c0ce34f1d75b";
 const lionDEXVault = "0x8eF99304eb88Af9BDe85d58a35339Cb0e2a557B6";
 const lionStaking = "0x154E2b1dBE9F493fF7938E5d686366138ddCE017";
 const LION = "0x8ebb85d53e6955e557b7c53acde1d42fd68561ec";
@@ -10,9 +10,9 @@ const USDC = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8";
 
 
 module.exports = {
-  methodology: `Counts USDC in treasure and fsGLP deposited to lionDEXVault. Staking counts Lion deposited to stakingPool.`,
+  methodology: `Counts fsGLP and USDC deposited to lionDEXVault. Staking counts Lion deposited to stakingPool.`,
   arbitrum: {
-    tvl:  sumTokensExport({ chain: 'arbitrum', owners: [treasure, lionDEXVault], tokens: [ fsGLP, USDC]}),
+    tvl:  sumTokensExport({ chain: 'arbitrum', owners: [lionDEXVault], tokens: [ fsGLP, USDC]}),
     staking: staking(lionStaking, LION, 'arbitrum')
   }
 }
