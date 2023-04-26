f170574b88ad184dac44450275be0d4e671a2484
MIMIEYES <pierreluohao@gmail.com>
2023-04-26 17:28:32 +0800

    update nerve bridge

--- projects/nerveswap/index.js
+++ projects/nerveswap/index.js
@@ -1,23 +1,8 @@
 const utils = require('../helper/utils');
 const { BigNumber } = require("bignumber.js");
-const baseURL = "https://api.swap.nerve.network/swap";
 
 async function fetch() {
-  let total = await utils.fetchURL(baseURL + '/total/info');
-  let tvl = new BigNumber(total.data.data.tvl);
-  tvl = tvl.shiftedBy(-18).toFixed(6);
-  return tvl;
-}
-
-async function nuls() {
-  let total = await utils.fetchURL(baseURL + '/nuls/info');
-  let tvl = new BigNumber(total.data.data.tvl);
-  tvl = tvl.shiftedBy(-18).toFixed(6);
-  return tvl;
-}
-
-async function kava() {
-  let total = await utils.fetchURL(baseURL + '/kava/info');
+  let total = await utils.fetchURL('https://api.swap.nerve.network/swap/total/info');
   let tvl = new BigNumber(total.data.data.tvl);
   tvl = tvl.shiftedBy(-18).toFixed(6);
   return tvl;
@@ -26,10 +11,7 @@ async function kava() {
 module.exports = {
   methodology: "A NerveDeFi platform that integrates consensus, swap, cross-chain swap,liquidity, farm and cross-chain bridge.",
   nuls: {
-    fetch: nuls
-  },
-  kava: {
-    fetch: kava
+    fetch
   },
   fetch
 }
