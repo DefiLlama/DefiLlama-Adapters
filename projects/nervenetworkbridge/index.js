dfcaf7cd7c9d19cef9d99b6a129cc201e54ba50b
MIMIEYES <pierreluohao@gmail.com>
2023-04-26 22:49:20 +0800

    update nerve bridge (#6078)
    * add tvl of kava chain
    
    * update nerve bridge
    
    * fix chain name issue
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/nervenetworkbridge/index.js
+++ projects/nervenetworkbridge/index.js
@@ -24,7 +24,9 @@ const getBridgeContract = {
   'smartbch': '0x3758AA66caD9F2606F1F501c9CB31b94b713A6d5',
   'enuls': '0x3758AA66caD9F2606F1F501c9CB31b94b713A6d5',
   'kava': '0x3758AA66caD9F2606F1F501c9CB31b94b713A6d5',
-  'ethpow': '0x67b3757f20DBFa114b593dfdAc2b3097Aa42133E'
+  'ethpow': '0x67b3757f20DBFa114b593dfdAc2b3097Aa42133E',
+  'rei': '0x3758AA66caD9F2606F1F501c9CB31b94b713A6d5',
+  'era': '0x54C4A99Ee277eFF14b378405b6600405790d5045'
 }
 const tronBridgeContract = 'TXeFBRKUW2x8ZYKPD13RuZDTd9qHbaPGEN';
 
@@ -36,10 +38,19 @@ async function getTokensConf() {
   return tokensConfTest
 }
 
+function getChain(chain) {
+  const chainMapping = {
+    era: 'zksync'
+  }
+
+  return chainMapping[chain] ?? chain
+}
+
 async function tvl(_, _b, _cb, { api, }) {
   let conf = await getTokensConf();
+
   const bridgeContract = getBridgeContract[api.chain];
-  const tokens = Object.values(conf[api.chain])
+  const tokens = Object.values(conf[getChain(api.chain)])
   const owners = [bridgeContract]
   return sumTokens2({ api, tokens, owners, })
 }
