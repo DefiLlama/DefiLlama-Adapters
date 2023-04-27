ffb6e9522938b0ba4877ac39c755f0475a2201bf
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-27 09:36:54 +0200

    minor refactor

--- projects/funbear/index.js
+++ projects/funbear/index.js
@@ -1,36 +1,19 @@
-const { ethers } = require("ethers");
-const sdk = require("@defillama/sdk");
-
-const chain = 'kava';
+const FUNB = 'ethereum:0xd86c0b9b686f78a7a5c3780f03e700dbbad40e01';
 const stakeContractAddresses = {
   unlockedStake: '0x1C4f227A2c7F62f88a7907cBF027403603A81A64',
   yearlyStake: '0xb991FAeF710f2ae699c425a92482Fc5D3Ae0cCD7',
-};
-
-async function fetchStakedAmount(stakeContract, block) {
-  var stakeBalance = (await sdk.api.abi.call({
-    target: stakeContract,
-    abi: 'erc20:totalSupply',
-    block: block,
-    chain: chain
-  })).output;
-  return stakeBalance;
 }
 
-async function getTotalStake(timestamp, block, chainBlocks) {
-  var unlocked = await fetchStakedAmount(stakeContractAddresses.unlockedStake, chainBlocks[chain]);
-  var yearly = await fetchStakedAmount(stakeContractAddresses.yearlyStake, chainBlocks[chain]);
-
-  return {
-    'ethereum:0xd86c0b9b686f78a7a5c3780f03e700dbbad40e01': ethers.BigNumber.from(unlocked).add(yearly),
-  };
+async function staking(timestamp, block, chainBlocks, { api }) {
+  const supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: Object.values(stakeContractAddresses) })
+  supplies.forEach(i => api.add(FUNB, i, { skipChain: true }))
+  return api.getBalances()
 }
 
 module.exports = {
   methodology: 'TVL counts staked FUNB coins on the platform itself.',
-  timetravel: false,
   kava: {
-    tvl:() => 0,
-    staking: getTotalStake,
+    tvl: () => 0,
+    staking,
   }
 };
