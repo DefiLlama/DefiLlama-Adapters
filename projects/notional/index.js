b379ecec7c80af17fbaf070dddb2cde9a3c7412e
Jeff Wu <jeff@notional.finance>
2023-04-28 08:28:45 -0700

    making notional v2 tvl dynamic

--- projects/notional/index.js
+++ projects/notional/index.js
@@ -1,36 +1,35 @@
 const abi = require('./abi');
 const sdk = require('@defillama/sdk');
-const { sumTokensAndLPsSharedOwners } = require('../helper/unwrapLPs');
 
-const escrowContract = '0x9abd0b8868546105F6F48298eaDC1D9c82f7f683';
 const v2Contract = "0x1344A36A1B56144C3Bc62E7757377D288fDE0369"
 
 async function tvl (timestamp, block) {
   const maxCurrencyId = (await sdk.api.abi.call({
     block,
-    target: escrowContract,
-    abi: abi['maxCurrencyId']
+    target: v2Contract,
+    abi: abi['getMaxCurrencyId']
   })).output;
 
   const addressCalls = []
-  for (let i = 0; i <= maxCurrencyId; i++) {
+  for (let i = 1; i <= maxCurrencyId; i++) {
     addressCalls.push({
-      target: escrowContract,
+      target: v2Contract,
       params: i
     })
   }
 
   const supportedTokens = (await sdk.api.abi.multiCall({
     calls: addressCalls,
-    target: escrowContract,
-    abi: abi['currencyIdToAddress'],
+    target: v2Contract,
+    abi: abi['getCurrency'],
     block,
   })).output
 
   const balanceCalls = supportedTokens.map((s) => {
     return {
-      target: s.output,
-      params: escrowContract
+      // Target is the asset token address, first parameter, first slot in tuple
+      target: s.output[0][0],
+      params: v2Contract
     }
   })
 
@@ -44,13 +43,6 @@ async function tvl (timestamp, block) {
     obj[b.input.target] = b.output
     return obj
   }, {})
-
-  await sumTokensAndLPsSharedOwners(balanceMap, [
-    "0x39aa39c021dfbae8fac545936693ac917d5e7563",
-    "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643",
-    "0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5",
-    "0xccf4429db6322d5c611ee964527d42e5d685dd6a"
-  ].map(a=>[a, false]), [v2Contract], block)
 
   return balanceMap
 }
