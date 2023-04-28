ebcc88d78e351308791aa7b19ba9f268d15f3da6
zeus <Zeus@onsenswap.xyz>
2023-04-28 14:06:36 +0900

    update starttime

--- projects/onsenswap/index.js
+++ projects/onsenswap/index.js
@@ -2,7 +2,7 @@ const { getUniTVL } = require("../helper/unknownTokens");
 
 module.exports = {
   misrepresentedTokens: true,
-  start: 1669075200,
+  start: 1682168400,
   era: {
     tvl: getUniTVL({
       factory: "0x0E15a1a03bD356B17F576c50d23BF7FC00305590",
