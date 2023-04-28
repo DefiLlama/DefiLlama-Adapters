fa422d1253325e80a04835ab5770cced2e335aaf
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-28 11:42:37 +0200

    zkdefi: minor fix

--- projects/zkdefi/index.js
+++ projects/zkdefi/index.js
@@ -4,7 +4,7 @@ module.exports = {
   misrepresentedTokens: true,
   polygon_zkevm: {
     tvl: getUniTVL({
-      chain: "polygon_zkevm",
+      fetchBalances: true,
       useDefaultCoreAssets: true,
       factory: "0xfDC8ec444F482Fe8aFe0a00114548DC9ff729568",
     }),
