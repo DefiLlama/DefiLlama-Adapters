d480af6edd678655ecd4a722f60f20f3f47a9143
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-29 00:33:31 +0200

    code refactor

--- projects/rising-undead/index.js
+++ projects/rising-undead/index.js
@@ -1,7 +1,5 @@
-const sdk = require('@defillama/sdk');
-const BigNumber = require('bignumber.js');
+const { sumTokensExport } = require('../helper/unwrapLPs');
 
-const chain = "kava";
 const owner = "0xc732471083342a842a728221878327c8DeE8aEDB";
 const tokens = [
     {symbol: "WKAVA", address: "0xc86c7C0eFbd6A49B35E8714C5f59D99De09A225b" },
@@ -10,19 +8,8 @@ const tokens = [
     {symbol: "USDT", address: "0xB44a9B6905aF7c801311e8F4E76932ee959c663C" },
 ]
 
-async function getTVL() {
-    let balance = 0;
-    tokens.forEach( async(token) => {
-        const target = token.address;
-        const tokenDecimal = await sdk.api.erc20.decimals(token.address, chain);
-        const tokenBal = await sdk.api.erc20.balanceOf({ target, owner, chain });
-        balance += BigNumber(tokenBal.output).dividedBy(BigNumber(10).pow(tokenDecimal.output)).toNumber();
-    });
-    return balance;
-}
-
 module.exports = {
     kava: {
-      tvl: getTVL()
+      tvl: sumTokensExport({ owner, tokens: tokens.map(i => i.address)})
     }
 }
