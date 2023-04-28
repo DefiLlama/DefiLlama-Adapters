b7145489340fc567a9264979ede5d0ade8936aab
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-04-28 10:59:26 +0200

    hydradex: refactor code

--- projects/hydradex.js
+++ projects/hydradex.js
@@ -1,21 +1,27 @@
-const cloudscraper = require('cloudscraper')
+const { blockQuery } = require('./helper/http')
+const { getBlock } = require('./hydradex/getHydraV3SubgraphTvl')
 
-async function fetch() {
-  const uri = 'https://info.hydradex.org/graphql'
-  const body = { "operationName": "Query", "variables": {}, "query": "query Query {\n  hydraswapFactories(where: {id: \"5a2a927bea6c5f4a48d4e0116049c1e36d52a528\"}) {\n    totalLiquidityUSD\n  }\n}\n" }
-  const reserves = (
-    await cloudscraper.post(uri, {
-      json: body
-    })
-  )
-  return { tether: +reserves.data.hydraswapFactories[0].totalLiquidityUSD }
+async function tvl(timestamp) {
+  const endpoint = 'https://info.hydradex.org/graphql'
+  const block = await getBlock(endpoint, timestamp)
+  const query = `query ($block: Float!){
+    hydraswapFactories (block: { number: $block }) {
+      totalLiquidityUSD
+    }
+    }`
+  const { hydraswapFactories: [{ totalLiquidityUSD }] } = await blockQuery(endpoint, query, {
+    api: {
+      getBlock: () => block,
+      block
+    }
+  })
+  return { tether: totalLiquidityUSD }
 }
 
 module.exports = {
   misrepresentedTokens: true,
-  timetravel: false,
   methodology: "We count liquidity on the dex, pulling data from subgraph",
   hydra: {
-    tvl: fetch,
+    tvl,
   },
 };
