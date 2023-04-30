8d52f115d5bdb1c52108f2be928cb1ca83bc823c
define <Lovelovenow@protonmail.com>
2023-04-30 13:09:21 +0100

    remove hastablepools from crhonos

--- projects/chronos/index.js
+++ projects/chronos/index.js
@@ -5,6 +5,6 @@ module.exports = {
     misrepresentedTokens: true,
     methodology: `Uses factory(0xCe9240869391928253Ed9cc9Bcb8cb98CB5B0722) address to count liquidity in pools as TVL.`,
     arbitrum: {
-        tvl: getUniTVL({ factory: '0xCe9240869391928253Ed9cc9Bcb8cb98CB5B0722',  useDefaultCoreAssets: true, hasStablePools: true }),
+        tvl: getUniTVL({ factory: '0xCe9240869391928253Ed9cc9Bcb8cb98CB5B0722',  useDefaultCoreAssets: true, hasStablePools: false }),
     }
 };
