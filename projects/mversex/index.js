29b21528649dc812ec86bd4ec1d3b088cfb673f5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-01 00:30:22 +0200

    minor refactor

---
+++ projects/mversex/index.js
@@ -0,0 +1,8 @@
+const { getUniTVL } = require('../helper/unknownTokens')
+module.exports = {
+  misrepresentedTokens: true,
+  kava: {
+    tvl: getUniTVL({ factory: '0xEFD3ad14E5cF09b0EbE435756337fb2e9D10Dc1a', fetchBalances: true, useDefaultCoreAssets: true, })
+  },
+}
+
