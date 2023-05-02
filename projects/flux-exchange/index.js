305643cfc5fd3c1463f451ca799697bbd6728dc6
0xfluxdev <david@fluxexchange.io>
2023-05-02 11:15:32 +0100

    feat: Add flux ftm adapter

---
+++ projects/flux-exchange/index.js
@@ -0,0 +1,13 @@
+const { staking } = require("../helper/staking");
+const { gmxExports } = require("../helper/gmx");
+
+const FTMVault = "0xc050733A325eEe50E544AcCbD38F6DACEd60ea6D";
+const FTMStaking = "0x136F1bD4Bb930cD931Ed30310142c2f03a946AC0";
+const WFTM = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83";
+
+module.exports = {
+  fantom: {
+    staking: staking(FTMStaking, WFTM, "fantom", WFTM, 18),
+    tvl: gmxExports({ vault: FTMVault })
+  }
+};
