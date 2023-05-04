08f8d72a7812e4a33070bde71326872aadfdc2e1
Egor Prudnikov <prudkaa@yandex.ru>
2023-05-04 13:53:02 +0500

    Change tvl counter

--- projects/EZZY-game-BSC-token/index.js
+++ projects/EZZY-game-BSC-token/index.js
@@ -1,15 +1,14 @@
-const { sumTokensExport } = require('../helper/unwrapLPs')
+const { staking } = require("../helper/staking")
 
-const owner = '0xc056124727540Fc26C3AF44d2b6BA4FbF59C6909'
-const tokens = [
-  '0xb452bc9cead0b08c4ef99da0feb8e10ef6bb86bb'
-]
+const contract = "0xc056124727540Fc26C3AF44d2b6BA4FbF59C6909"
+const token = "0xb452bc9cead0b08c4ef99da0feb8e10ef6bb86bb"
 
 module.exports = {
   timetravel: true,
   misrepresentedTokens: false,
   start: 1674950,
   bsc: {
-    tvl: sumTokensExport({ tokens, owner })
-  }
-}
+    staking: staking(contract, token, "bsc"),
+    tvl: (async) => ({}),
+  },
+};
