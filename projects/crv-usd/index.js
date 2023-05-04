a1fa9dc86e744e7d5c64862e4776681857949d57
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-04 16:16:40 +0200

    add new crv-usd

--- projects/crv-usd/index.js
+++ projects/crv-usd/index.js
@@ -2,26 +2,37 @@ const { getLogs } = require('../helper/cache/getLogs')
 const { sumTokens2 } = require('../helper/unwrapLPs')
 
 const config = {
-  ethereum: { factory: '0x818709b85052ddc521fae9c78737b27316337e3a', fromBlock: 17182152 },
+  ethereum: {
+    pools: [
+      { factory: '0x818709b85052ddc521fae9c78737b27316337e3a', fromBlock: 17182152 },
+      { factory: '0x30a2F3c3AA6D12C0a36Bed210dCF1B32EF6228Cc', fromBlock: 17187330 },
+    ]
+  },
 }
 
 module.exports = {
 };
 
+
 Object.keys(config).forEach(chain => {
-  const { factory, fromBlock } = config[chain]
+  const { pools} = config[chain]
   module.exports[chain] = {
     tvl: async (_, _b, _cb, { api, }) => {
-      const logs = await getLogs({
-        api,
-        target: factory,
-        topics: ['0xebbe0dfde9dde641808b7a803882653420f3a5b12bb405d238faed959e1e3aa3'],
-        eventAbi: 'event AddMarket (address indexed collateral, address controller, address amm, address monetary_policy, uint256 ix)',
-        onlyArgs: true,
-        fromBlock,
-      })
+      const logs = await Promise.all(pools.map(getLogs_))
 
-      return sumTokens2({ api, tokensAndOwners: logs.map(i => [i.collateral, i.amm]) })
+      console.log(logs)
+      return sumTokens2({ api, tokensAndOwners: logs.flat().map(i => [i.collateral, i.amm]) })
+
+      async function getLogs_({ factory, fromBlock }) {
+        return getLogs({
+          api,
+          target: factory,
+          topics: ['0xebbe0dfde9dde641808b7a803882653420f3a5b12bb405d238faed959e1e3aa3'],
+          eventAbi: 'event AddMarket (address indexed collateral, address controller, address amm, address monetary_policy, uint256 ix)',
+          onlyArgs: true,
+          fromBlock,
+        })
+      }
     }
   }
 })
