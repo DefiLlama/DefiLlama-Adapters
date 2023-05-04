95723ce1d4bd526ebf673d95a38a458564f5f564
SolBlaze <contact@solblaze.org>
2023-05-03 19:49:57 -0700

    Fix import

--- projects/blazestake/index.js
+++ projects/blazestake/index.js
@@ -1,4 +1,4 @@
-const { getTokenSupply } = require("./helper/solana")
+const { getTokenSupply } = require("../helper/solana")
 
 async function tvl() {
   const supply = await getTokenSupply("bSo13r4TkiE4KumL71LsHTPpL2euBYLFx6h9HP3piy1");
