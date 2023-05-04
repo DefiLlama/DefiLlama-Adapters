198d9af86a944a0183a0a130d9a0a882ca5b334e
Kevin Upton <k_upton@live.com>
2023-05-04 15:10:34 +1000

    updated kei index

--- projects/kei-finance/index.js
+++ projects/kei-finance/index.js
@@ -1,10 +1,55 @@
-const { staking } = require('../helper/staking')
+const sdk = require("@defillama/sdk");
+const { request,  } = require("graphql-request");
+const { BigNumber } = require("bignumber.js");
+
 const TOKEN_ADDRESS = "0xF75C7a59bCD9bd207C4Ab1BEB0b32EEd3B6392f3";
-const STAKING_CONTRACT = "...";
+const TREASURY_ADDRESS = "0x3D027824a9Eb4cc5E8f24D97FD8495eA9DC7026F";
+const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+
+const config = {
+  ethereum: {
+    subgraph: 'https://api.thegraph.com/subgraphs/name/kei-finance/core',
+  }
+}
+
+const graphQuery = `
+  query GetStakingDetails {
+    staking(id:"staking") {
+      totalPrincipal
+      totalRewards
+    }
+  }
+`;
+
+async function tvl(_, _1, _2, { api }) {
+  // const tokenBalance = await sdk.api.erc20.balanceOf({
+  //   target: TOKEN_ADDRESS,
+  //   owner: TREASURY_ADDRESS
+  // });
+
+  const wethBalance = await sdk.api.erc20.balanceOf({
+    target: WETH_ADDRESS,
+    owner: TREASURY_ADDRESS
+  });
+
+  console.log(wethBalance)
+
+  return {
+    [WETH_ADDRESS]: wethBalance.output,
+    // [TOKEN_ADDRESS]: tokenBalance.output
+  };
+}
 
 module.exports = {
   ethereum: {
-    tvl: () => 0,
-    staking: staking(STAKING_CONTRACT, TOKEN_ADDRESS)
+    tvl,
+    staking: async () => {
+      const {  staking } = await request(
+        config.ethereum.subgraph,
+        graphQuery
+      )
+
+      return { [TOKEN_ADDRESS]: BigNumber(staking.totalPrincipal).plus(BigNumber(staking.totalRewards)) };
+    },
   }
 };
