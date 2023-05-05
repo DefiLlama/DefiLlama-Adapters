5054e3f7c22e67ec4b0019b6dab9853adb61b85f
Mo Shaikjee <shaikjeemohammed@gmail.com>
2023-05-05 14:47:23 +0200

    +- v2 prod url

--- projects/bubbleswap-v2/index.js
+++ projects/bubbleswap-v2/index.js
@@ -2,7 +2,7 @@ const { default: axios } = require('axios');
 
 async function fetch(){
 
-    const response = await axios.get(' https://api.testnet.bubbleswap.io/v2/backend/api/v1/tvl', {
+    const response = await axios.get('https://api.bubbleswap.io/v2/backend/api/v1/tvl', {
         headers: {
             //I would prefer the user agengt to be set to something like axios, or DefiLlama, but our WAF only allows the bellow
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
@@ -22,7 +22,7 @@ async function fetch(){
 module.exports = {
   fetch,
   timetravel: false,
-  methodology: "Data is retrieved from the api at https://api.testnet.bubbleswap.io/",
+  methodology: "Data is retrieved from the api at https://api.bubbleswap.io/",
   hallmarks: [
     [1676644131, "V2 Launch"],
   ]
