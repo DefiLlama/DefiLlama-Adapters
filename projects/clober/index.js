35739f016f4ad71b481456d01791a4875f680f1f
graykode <nlkey2022@gmail.com>
2023-05-08 23:29:54 +0900

    feat: add polygon zkevm mainnet for clober

--- projects/clober/index.js
+++ projects/clober/index.js
@@ -1,10 +1,17 @@
 const abi = require("./abi.json");
 const { sumTokens2 } = require('../helper/unwrapLPs')
 
-const FACTORY = "0x93A43391978BFC0bc708d5f55b0Abe7A9ede1B91"
+function factoryAddress(chainId){
+    if(chainId === 1101){
+        return "0x24aC0938C010Fb520F1068e96d78E0458855111D"
+    }else{
+        return "0x93A43391978BFC0bc708d5f55b0Abe7A9ede1B91"
+    }
+}
 
 async function tvl(_, _b, _cb, { api }) {
-    let tokenAddresses = await api.fetchList({  lengthAbi: abi.nonce, itemAbi: abi.computeTokenAddress, target: FACTORY}) 
+    const chainId = await api.getChainId()
+    let tokenAddresses = await api.fetchList({  lengthAbi: abi.nonce, itemAbi: abi.computeTokenAddress, target: factoryAddress(chainId)})
     tokenAddresses = tokenAddresses.flat()
     const markets = await api.multiCall({  abi: abi.market, calls: tokenAddresses })
     const base = await api.multiCall({  abi: abi.baseToken, calls: markets}) 
@@ -22,5 +29,6 @@ module.exports = {
     methodology: "TVL consists of assets deposited into market contracts",
     ethereum: { tvl },
     polygon: { tvl },
-    arbitrum: { tvl }
+    arbitrum: { tvl },
+    polygon_zkevm: { tvl }
 }
