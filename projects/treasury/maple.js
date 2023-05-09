c4d17abc654ba95ea748e262ba68a667c24464e2
0xngmi <0xngmi@protonmail.com>
2023-05-09 02:35:42 +0100

    owntokens on treasury

--- projects/treasury/maple.js
+++ projects/treasury/maple.js
@@ -12,5 +12,6 @@ module.exports = treasuryExports({
       "0xa9466EaBd096449d650D5AEB0dD3dA6F52FD0B19",
       "0xd6d4Bcde6c816F17889f1Dd3000aF0261B03a196"
     ],
+    ownTokens: ["0x33349b282065b0284d756f0577fb39c158f935e6"],
   },
 })
