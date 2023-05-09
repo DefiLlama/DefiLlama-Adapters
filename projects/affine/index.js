5d8444cd29a395ef5a51a12ed42049dd29307b4a
kiwi0401 <kiwi04011@gmail.com>
2023-05-09 14:04:08 -0700

    use correct tokens on polygon baskets and add eth baskets

--- projects/affine/index.js
+++ projects/affine/index.js
@@ -1,57 +1,33 @@
-const sdk = require('@defillama/sdk');
-const TOKEN_CONTRACT_ADDRESS = "0x829363736a5A9080e05549Db6d1271f070a7e224";
-const OWNER_ADDRESS = "0x829363736a5A9080e05549Db6d1271f070a7e224";
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
-async function tvl(_, _1, _2, { api }) {
-  const balances = {};
+async function tvl(_, _b, _cb, { api, }) {
+  const vaults = ['0x829363736a5A9080e05549Db6d1271f070a7e224']
+  const tokens = await api.multiCall({ abi: 'address:asset', calls: vaults })
+  const bals = await api.multiCall({ abi: 'uint256:totalAssets', calls: vaults })
+  api.addTokens(tokens, bals)
+  const ownerTokens = [
+    [['0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6'], '0x1F9b1057cd93fb2d07d18810903B791b56acc2E1']
+  ]
+  return sumTokens2({ api, ownerTokens })
+}
 
-  const totalSupplyBigInt = await api.call({
-    abi: "erc20:totalSupply",
-    target: TOKEN_CONTRACT_ADDRESS,
-  });
-
-  const supplyDecimals = await api.call({
-    abi: {
-      constant: true,
-      inputs: [],
-      name: "decimals",
-      outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
-      stateMutability: "view",
-      type: "function",
-    },
-    target: TOKEN_CONTRACT_ADDRESS,
-  });
-
-  const totalSupply = Number(totalSupplyBigInt) / 10 ** supplyDecimals;
-
-  const { num, decimals } = await api.call({
-    abi: {
-      constant: true,
-      inputs: [],
-      name: "detailedPrice",
-      outputs: [
-        { internalType: "uint256", name: "num", type: "uint256" },
-        { internalType: "uint8", name: "decimals", type: "uint8" },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    target: TOKEN_CONTRACT_ADDRESS,
-  });
-
-  const tokenPriceInUSD = num / (10 ** decimals);
-  balances["usd"] = tokenPriceInUSD * totalSupply;
-  return balances;
+async function ethTvl(_, _b, _cb, { api, }) {
+  const ethBaskets = [
+    '0x61A18EE9d6d51F838c7e50dFD750629Fd141E944', '0x78Bb94Feab383ccEd39766a7d6CF31dED177Ad0c', '0x72D51B2233c5feA8a702FDd0E51B0adE95638f2c'
+  ]
+  for(const basket of ethBaskets) {
+    const vaults = [basket]
+    const tokens = await api.multiCall({ abi: 'address:asset', calls: vaults })
+    const bals = await api.multiCall({ abi: 'uint256:totalAssets', calls: vaults })
+    api.addTokens(tokens, bals)
+  }
+  return sumTokens2({ api })
 }
 
 module.exports = {
+  doublecounted: true,
   timetravel: true,
-  misrepresentedTokens: false,
-  methodology: 'The TVL is calculated by fetching the total supply of the token from the contract,' +
-    ' retrieving the tokens price in USD using the contracts "detailedPrice" function,' +
-    ' and then multiplying the total supply by the tokens price',
-  start: 1000235,
-  polygon: {
-    tvl,
-  }
-};
+  methodology: 'Counts the tokens in the Affine baskets',
+  polygon: { tvl },
+  ethereum: { tvl: ethTvl }
+}
