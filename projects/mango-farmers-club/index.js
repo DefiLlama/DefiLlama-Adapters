dd412a89c23f03dd36a2193fbb81517af394d2fd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-09 20:04:49 +0200

    minor fix

--- projects/mango-farmers-club/index.js
+++ projects/mango-farmers-club/index.js
@@ -1,20 +1,17 @@
-const { default: BigNumber } = require('bignumber.js');
-const sdk = require("@defillama/sdk");
 const { fetchURL } = require("../helper/utils");
-const { transformBalances } = require("../helper/portedTokens");
 
 module.exports = {
   polygon_zkevm: {
-    staking: async () => {
-      const {output: stakingBalance} = await sdk.api.abi.call({
-        abi: 'erc20:totalSupply',
-        target: "0xdd38211f2973dc41cd6fC4DB681596Fd6118D894",
-        chain: 'polygon_zkevm'
+    staking: async (_, _1, _2, { api }) => {
+      const stakingBalance = await api.call({
+        abi: 'erc20:balanceOf',
+        target: '0x1fa03edb1b8839a5319a7d2c1ae6aae492342bad',
+        params: "0xdd38211f2973dc41cd6fC4DB681596Fd6118D894",
       });
       const { price : mangoPrice } = (await fetchURL('https://prod.clober-api.com/1101/markets/0x1FC38BA10E741F357b1c8B69DC08eA654c21Ae37/trades?limit=1')).data.trades[0]
-      const balances = {}
-      sdk.util.sumSingleBalance(balances, "0x1fA03eDB1B8839a5319A7D2c1Ae6AAE492342bAD", BigNumber(stakingBalance).times(mangoPrice).div(1e18).toFixed(0))
-      return transformBalances('polygon_zkevm', balances)
+      return {
+        tether: mangoPrice * stakingBalance /1e18
+      }
     },
     tvl: () => ({})
   }
