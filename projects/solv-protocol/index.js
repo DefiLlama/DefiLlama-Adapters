45585222b0b1a1e9934bf689baa0170579b4813e
charq <73696209+buchaoqun@users.noreply.github.com>
2023-05-10 23:50:16 +0800

    Add solv v3 (#6219)
    * solv-protocal update v2
    		      add v3
    
    * try catch error
    
    * solv: code refactor
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/solv-protocol/index.js
+++ projects/solv-protocol/index.js
@@ -1,84 +1,115 @@
-const { sumTokens, } = require('../helper/unwrapLPs')
-const { staking } = require('../helper/staking');
-const { getChainTransform } = require("../helper/portedTokens");
-const { getConfig } = require('../helper/cache')
+const { default: BigNumber } = require("bignumber.js");
+const abi = require("./abi.json");
+const { getConfig, cachedGraphQuery } = require("../helper/cache");
+const { sumTokens2 } = require("../helper/unwrapLPs");
 
 // token list
 const tokenListsApiEndpoint = "https://token-list.solv.finance/vouchers-prod.json"
 
-// Staking Tvls
-const solvEthereumTokenAddress = '0x256F2d67e52fE834726D2DDCD8413654F5Eb8b53'
-const solvEthereumPoolAddress = '0x7D0C93DcAD6f6B38C81431d7262CF0E48770B81a'
-const solvBscTokenAddress = '0xC073c4eD65622A9423b5e5BDe2BFC8B81EBC471c'
-const solvBscPoolAddress = '0xE5742912EDb4599779ACC1CE2acB6a06E01f1089'
-
-const ethereumTVL = async (timestamp, block, chainBlocks) => {
-    const transform = await getChainTransform('ethereum');
-    return tvl(timestamp, block, chainBlocks, "ethereum", 1, transform);
+// The Graph
+const graphUrlList = {
+  ethereum: 'https://api.studio.thegraph.com/query/40045/solv-payable-factory-prod/v0.0.1',
+  bsc: 'https://api.thegraph.com/subgraphs/name/slov-payable/solv-v3-earn-factory',
+  arbitrum: 'https://api.studio.thegraph.com/query/40045/solv-payable-factory-arbitrum/v0.0.1',
 }
 
-const polygonTVL = async (timestamp, block, chainBlocks) => {
-    const transform = await getChainTransform('polygon');
-    return tvl(timestamp, block, chainBlocks, "polygon", 137, transform);
+async function tvl() {
+  const { api } = arguments[3]
+  const chainId = api.getChainId()
+  const tokens = await tokenList(chainId);
+  await sumTokens2({ api, tokensAndOwners: tokens.map(i => [i.address, i.pool]), ignoreFailed: true })
+  await graphEarn(api)
 }
 
-const arbitrumTVL = async (timestamp, block, chainBlocks) => {
-    const transform = await getChainTransform('arbitrum');
-    return tvl(timestamp, block, chainBlocks, "arbitrum", 42161, transform);
-};
+async function graphEarn(api) {
+  const network = api.chain
+  if (!graphUrlList[network]) return;
+  const slots = await getSlot(api.timestamp, network);
 
-const bscTVL = async (timestamp, block, chainBlocks) => {
-    const transform = await getChainTransform('bsc');
-    return tvl(timestamp, block, chainBlocks, "bsc", 56, transform);
-};
+  const concretes = await concrete(slots, api);
 
-async function tvl(timestamp, block, chainBlocks, network, chainId, transform) {
-    let balances = {}; // Setup the balances object
-    const tokens = await tokenList(chainId);
-    let tokenPairs = []
-    for (let i = 0; i < tokens.length; i++) {
-        const token = tokens[i];
-        tokenPairs.push([
-            token.address,
-            token.pool
-        ])
-    }
+  const totalValues = await api.multiCall({
+    abi: abi.slotTotalValue,
+    calls: slots.map((index) => ({
+      target: concretes[index.contractAddress],
+      params: [index.slot]
+    })),
+  })
 
-    await sumTokens(balances, tokenPairs, chainBlocks[network], network, transform)
+  const baseInfos = await api.multiCall({
+    abi: abi.slotBaseInfo,
+    calls: slots.map((index) => ({
+      target: concretes[index.contractAddress],
+      params: [index.slot]
+    })),
+  })
 
-    return balances;
+  const decimalList = await api.multiCall({
+    abi: abi.decimals,
+    calls: baseInfos.map(i => i[1]),
+  })
+
+  for (let i = 0; i < totalValues.length; i++) {
+    const decimals = decimalList[i];
+    const balance = BigNumber(totalValues[i]).div(BigNumber(10).pow(18 - decimals)).toNumber();
+    api.add(baseInfos[i][1], balance)
+  }
 }
 
 async function tokenList(chainId) {
-    let tokens = [];
-    const allTokens = (await getConfig('solv-protocol', tokenListsApiEndpoint)).tokens;
-    for (let token of allTokens) {
-        if (chainId == token.chainId) {
-            if (token.extensions.voucher.underlyingToken && token.extensions.voucher.underlyingToken.symbol != "SOLV") {
-                tokens.push({
-                    address: token.extensions.voucher.underlyingToken.address,
-                    pool: token.extensions.voucher.vestingPool
-                })
-            }
+  let tokens = [];
+  const allTokens = (await getConfig('solv-protocol', tokenListsApiEndpoint)).tokens;
+  for (let token of allTokens) {
+    if (chainId == token.chainId) {
+      if (token.extensions.voucher.underlyingToken != undefined) {
+        if (token.extensions.voucher.underlyingToken.symbol != "SOLV" && token.extensions.voucher.underlyingToken.symbol.indexOf("_") == -1) {
+          tokens.push({
+            address: token.extensions.voucher.underlyingToken.address,
+            pool: token.extensions.voucher.vestingPool
+          })
         }
+      }
     }
+  }
 
-    return tokens;
+  return tokens;
 }
-// node test.js projects/solv-protocol/index.js
-module.exports = {
-    ethereum: {
-        tvl: ethereumTVL,
-        staking: staking(solvEthereumPoolAddress, solvEthereumTokenAddress)
-    },
-    bsc: {
-        tvl: bscTVL,
-        staking: staking(solvBscPoolAddress, solvBscTokenAddress, "bsc")
-    },
-    polygon: {
-        tvl: polygonTVL
-    },
-    arbitrum: {
-        tvl: arbitrumTVL
+
+async function concrete(slots, api) {
+  var slotsList = [];
+  var only = {};
+  for (var i = 0; i < slots.length; i++) {
+    if (!only[slots[i].contractAddress]) {
+      slotsList.push(slots[i]);
+      only[slots[i].contractAddress] = true;
     }
+  }
+
+  const concreteLists = await api.multiCall({
+    calls: slotsList.map((index) => index.contractAddress),
+    abi: abi.concrete,
+  })
+
+  let concretes = {};
+  for (var k = 0; k < concreteLists.length; k++) {
+    concretes[slotsList[k].contractAddress] = concreteLists[k];
+  }
+
+  return concretes;
 }
+
+
+async function getSlot(timestamp, chain) {
+  const slotDataQuery = `query BondSlotInfos {
+            bondSlotInfos(first: 1000, where:{maturity_gt:${timestamp}}) {
+                contractAddress
+                slot
+            }
+        }`;
+  const slots = (await cachedGraphQuery(`solv-protocol/graph-data/${chain}`, graphUrlList[chain], slotDataQuery)).bondSlotInfos;
+  return slots;
+}
+
+['ethereum', 'bsc', 'polygon', 'arbitrum'].forEach(chain => {
+  module.exports[chain] = { tvl }
+})
