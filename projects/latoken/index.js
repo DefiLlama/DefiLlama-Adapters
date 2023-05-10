9cbb3baa478b642eeb5533f62cfc54cab3457f8c
semyon.zhulikov <semyon.zhulikov@latoken.com>
2023-05-10 17:05:11 +0300

    add btc address

--- projects/latoken/index.js
+++ projects/latoken/index.js
@@ -4,7 +4,8 @@ const config = {
   bitcoin: {
     owners: [
       "bc1q48amr6l7dvacdppgucvnswwuyleaqh4dus8z8h",
-      "bc1q2cgh9nxn7cqmqhk4hc5fu6mju8nzy9a20qqqh8" //cold
+      "bc1q2cgh9nxn7cqmqhk4hc5fu6mju8nzy9a20qqqh8", //cold
+      "bc1qw5fc9ml9vm4xq5c6xkcdu3vtwyw4gdn8lw0uwq"
     ]
   },
   ethereum: {
