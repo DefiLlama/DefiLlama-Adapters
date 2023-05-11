9d48dbad41280e569547a306bba5677dcb5f035c
Teddy Leno <lenoteddy@gmail.com>
2023-05-11 10:29:43 +0700

    Update index.js
    Add Staking V3 Pool

--- kommunitas/index.js
+++ kommunitas/index.js
@@ -2,7 +2,8 @@ const sdk = require('@defillama/sdk');
 const KOM_TOKEN_CONTRACT = '0xC004e2318722EA2b15499D6375905d75Ee5390B8';
 const KOM_STAKING_CONTRACT = [
     "0x453d0a593d0af91e77e590a7935894f7ab1b87ec",
-    "0x8d37b12DB32E07d6ddF10979c7e3cDECCac3dC13"
+    "0x8d37b12DB32E07d6ddF10979c7e3cDECCac3dC13",
+    "0x8d34Bb43429c124E55ef52b5B1539bfd121B0C8D"
 ];
 
 async function tvl(timestamp, block, chainBlocks) {
