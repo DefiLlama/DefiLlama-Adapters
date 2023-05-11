760c4a9935c34f3fadd9077df2541f16c874a2e9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-11 22:58:35 +0200

    code refactor

--- projects/KyborgExchange/index.js
+++ projects/KyborgExchange/index.js
@@ -1,47 +1,20 @@
-const sdk = require("@defillama/sdk");
-const { request, gql } = require("graphql-request");
+const { getLogs } = require('../helper/cache/getLogs');
+const { sumTokens2 } = require('../helper/unwrapLPs');
 
-const KYBORG_HUB = "0x25620d76654caC426229C85bE8eAEB010Ea25c8F";
-
-async function tvl(_, block) {
-  const graphQuery = gql`
-    query poolQuery {
-      tokens {
-        id
-      }
-    }
-  `;
-  const tokens = (
-    await request(
-      "https://api.thegraph.com/subgraphs/name/kyborgexchange/kyborgexchange",
-      graphQuery
-    )
-  ).tokens.map(({ id }) => id);
-
-  const tokensBalances = await Promise.all(
-    tokens.map(
-      async (token) =>
-        (
-          await sdk.api.abi.call({
-            target: token,
-            abi: "erc20:balanceOf",
-            params: KYBORG_HUB,
-            block,
-          })
-        ).output
-    )
-  );
-
-  const balances = {};
-
-  tokens.forEach((token, i) => (balances[token] = tokensBalances[i]));
-  return balances;
+const config = {
+  arbitrum: { factory: '0x25620d76654caC426229C85bE8eAEB010Ea25c8F', fromBlock: 89067435 },
 }
+
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: true,
   methodology: `Counts the tokens balances of the KyborgHub contract`,
-  arbitrum: {
-    tvl,
-  },
 };
+
+Object.keys(config).forEach(chain => {
+  const { factory, fromBlock } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const logs = await getLogs({ api, target: factory, onlyArgs: true, eventAbi: 'event PoolCreated (address indexed token0, address indexed token1, bytes32 indexed poolId)', topics: ['0xec5dc6309c83a50f60f4a1fae9422b2c406da78c579b9b12b92d033db37c7194'], fromBlock, })
+      return sumTokens2({ api, owner: factory, tokens: logs.map(i => [i.token0, i.token1]).flat()})
+    }
+  }
+})
