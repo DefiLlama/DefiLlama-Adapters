a01da360d7adbefcec5e190c8dce0c26ddd7c5d2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-11 08:51:57 +0200

    meld-gold: code refactor

--- projects/helper/chain/algorand.js
+++ projects/helper/chain/algorand.js
@@ -2,24 +2,21 @@
 
 const axios = require('axios')
 const { getApplicationAddress } = require('./algorandUtils/address')
-const { RateLimiter } = require('limiter')
+const { RateLimiter } = require("limiter");
 const coreAssets = require('../coreAssets.json')
-const sdk = require('@defillama/sdk')
-const { default: BigNumber } = require('bignumber.js')
+const sdk = require('@defillama/sdk');
+const { default: BigNumber } = require('bignumber.js');
 const stateCache = {}
 const accountCache = {}
 const assetCache = {}
 
 const geckoMapping = coreAssets.algorand ?? {}
 const axiosObj = axios.create({
-  baseURL: 'https://mainnet-idx.algonode.cloud',
+  baseURL: "https://mainnet-idx.algonode.cloud",
   timeout: 300000,
-})
+});
 
-const indexerLimiter = new RateLimiter({
-  tokensPerInterval: 10,
-  interval: 'second',
-})
+const indexerLimiter = new RateLimiter({ tokensPerInterval: 10, interval: "second" });
 
 async function lookupApplications(appId) {
   return (await axiosObj.get(`/v2/applications/${appId}`)).data
@@ -29,52 +26,36 @@ async function lookupAccountByID(accountId) {
   return (await axiosObj.get(`/v2/accounts/${accountId}`)).data
 }
 
-async function searchAccounts({ appId, limit = 1000, nexttoken, searchParams }) {
-  const response = await axiosObj.get('/v2/accounts', {
+async function searchAccounts({ appId, limit = 1000, nexttoken, searchParams, }) {
+  const response = (await axiosObj.get('/v2/accounts', {
     params: {
       ...searchParams,
       'application-id': appId,
       limit,
       next: nexttoken,
-    },
-  })
+    }
+  }))
   return response.data
 }
+
 
 async function searchAccountsAll({ appId, limit = 1000, searchParams = {} }) {
   const accounts = []
   let nexttoken
   do {
-    const res = await searchAccounts({
-      appId,
-      limit,
-      nexttoken,
-      searchParams,
-    })
+    const res = await searchAccounts({ appId, limit, nexttoken, searchParams, })
     nexttoken = res['next-token']
     accounts.push(...res.accounts)
   } while (nexttoken)
   return accounts
 }
 
-const withLimiter =
-  (fn, tokensToRemove = 1) =>
-  async (...args) => {
-    await indexerLimiter.removeTokens(tokensToRemove)
-    return fn(...args)
-  }
+const withLimiter = (fn, tokensToRemove = 1) => async (...args) => {
+  await indexerLimiter.removeTokens(tokensToRemove);
+  return fn(...args);
+}
 
-async function sumTokens({
-  owner,
-  owners = [],
-  tokens = [],
-  token,
-  balances = {},
-  blacklistedTokens = [],
-  tinymanLps = [],
-  blacklistOnLpAsWell = false,
-  tokensAndOwners = [],
-}) {
+async function sumTokens({ owner, owners = [], tokens = [], token, balances = {}, blacklistedTokens = [], tinymanLps = [], blacklistOnLpAsWell = false, tokensAndOwners = [], }) {
   if (owner) owners = [owner]
   if (token) tokens = [token]
   if (tokensAndOwners.length) owners = tokensAndOwners.map(i => i[1])
@@ -88,16 +69,7 @@ async function sumTokens({
     })
   })
   if (tinymanLps.length) {
-    await Promise.all(
-      tinymanLps.map(([lp, unknown]) =>
-        resolveTinymanLp({
-          balances,
-          lpId: lp,
-          unknownAsset: unknown,
-          blacklistedTokens: blacklistOnLpAsWell ? blacklistedTokens : [],
-        }),
-      ),
-    )
+    await Promise.all(tinymanLps.map(([lp, unknown]) => resolveTinymanLp({ balances, lpId: lp, unknownAsset: unknown, blacklistedTokens: blacklistOnLpAsWell ? blacklistedTokens : [] })))
   }
   return balances
 }
@@ -107,11 +79,9 @@ async function getAssetInfo(assetId) {
   return assetCache[assetId]
 
   async function _getAssetInfo() {
-    const {
-      data: { asset },
-    } = await axiosObj.get(`/v2/assets/${assetId}`)
+    const { data: { asset } } = await axiosObj.get(`/v2/assets/${assetId}`)
     const reserveInfo = await getAccountInfo(asset.params.reserve)
-    const assetObj = { ...asset.params, ...asset, reserveInfo }
+    const assetObj = { ...asset.params, ...asset, reserveInfo, }
     assetObj.circulatingSupply = assetObj.total - reserveInfo.assetMapping[assetId].amount
     assetObj.assets = { ...reserveInfo.assetMapping }
     delete assetObj.assets[assetId]
@@ -119,37 +89,27 @@ async function getAssetInfo(assetId) {
   }
 }
 
-async function resolveTinymanLp({ balances, lpId, unknownAsset, blacklistedTokens }) {
-  const lpBalance = balances['algorand:' + lpId]
+async function resolveTinymanLp({ balances, lpId, unknownAsset, blacklistedTokens, }) {
+  const lpBalance = balances['algorand:'+lpId]
   if (lpBalance && lpBalance !== '0') {
     const lpInfo = await getAssetInfo(lpId)
     let ratio = lpBalance / lpInfo.circulatingSupply
     if (unknownAsset && lpInfo.assets[unknownAsset]) {
       ratio = ratio * 2
-      Object.keys(lpInfo.assets).forEach(token => {
+      Object.keys(lpInfo.assets).forEach((token) => {
         if (!blacklistedTokens.length || !blacklistedTokens.includes(token))
           if (token !== unknownAsset)
-            sdk.util.sumSingleBalance(
-              balances,
-              token,
-              BigNumber(lpInfo.assets[token].amount * ratio).toFixed(0),
-              'algorand',
-            )
+            sdk.util.sumSingleBalance(balances, token, BigNumber(lpInfo.assets[token].amount * ratio).toFixed(0), 'algorand')
       })
     } else {
-      Object.keys(lpInfo.assets).forEach(token => {
+      Object.keys(lpInfo.assets).forEach((token) => {
         if (!blacklistedTokens.length || !blacklistedTokens.includes(token))
-          sdk.util.sumSingleBalance(
-            balances,
-            token,
-            BigNumber(lpInfo.assets[token].amount * ratio).toFixed(0),
-            'algorand',
-          )
+          sdk.util.sumSingleBalance(balances, token, BigNumber(lpInfo.assets[token].amount * ratio).toFixed(0), 'algorand')
       })
     }
   }
   delete balances[lpId]
-  delete balances['algorand:' + lpId]
+  delete balances['algorand:'+lpId]
   return balances
 }
 
@@ -158,11 +118,9 @@ async function getAccountInfo(accountId) {
   return accountCache[accountId]
 
   async function _getAccountInfo() {
-    const {
-      data: { account },
-    } = await axiosObj.get(`/v2/accounts/${accountId}`)
+    const { data: { account } } = await axiosObj.get(`/v2/accounts/${accountId}`)
     if (!account.assets) account.assets = []
-    if (account.amount) account.assets.push({ amount: account.amount, 'asset-id': '1' })
+    if (account.amount) account.assets.push({ amount: account.amount, 'asset-id': '1', })
     account.assetMapping = {}
     account.assets.forEach(i => {
       i['asset-id'] = '' + i['asset-id']
@@ -182,17 +140,17 @@ const tokens = {
 }
 
 // store all asset ids as string
-Object.keys(tokens).forEach(t => (tokens[t] = '' + tokens[t]))
+Object.keys(tokens).forEach(t => tokens[t] = '' + tokens[t])
 
 async function getAppGlobalState(marketId) {
   if (!stateCache[marketId]) stateCache[marketId] = _getAppGlobalState()
   return stateCache[marketId]
 
   async function _getAppGlobalState() {
-    let response = await lookupApplications(marketId)
+    let response = await lookupApplications(marketId);
     let results = {}
-    response.application.params['global-state'].forEach(x => {
-      let decodedKey = Buffer.from(x.key, 'base64').toString('binary')
+    response.application.params["global-state"].forEach(x => {
+      let decodedKey = Buffer.from(x.key, "base64").toString("binary")
       results[decodedKey] = x.value.uint
     })
 
