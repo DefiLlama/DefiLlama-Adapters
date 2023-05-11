96721b1f8456c3beb9c7e42f690915d44a3f8c68
wangxi <wangxi8206@sina.com>
2023-05-11 16:23:15 +0800

    udpate

--- projects/nutbox/index.js
+++ projects/nutbox/index.js
@@ -2,7 +2,9 @@
 const sdk = require('@defillama/sdk');
 const { GraphQLClient, gql } = require('graphql-request');
 const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutinbsc')
-const { getTokenPrices } = require('../helper/cache/sumUnknownTokens')
+const { getTokenPrices, getLPData } = require('../helper/cache/sumUnknownTokens')
+const  { sumTokensAndLPs } = require('../helper/unwrapLPs')
+const { isLP } = require('../helper/utils')
 
 async function getBSCPools() {
     const query = gql`
@@ -53,29 +55,118 @@ async function getENULSPools() {
 async function bscTvl(_, _1, _2, { api }) {
     let balances = {};
 
-    const pools = await getBSCPools();
+    let pools = await getBSCPools();
+    const wbnb = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
 
-    const prices = await getTokenPrices({
+    const res = await getTokenPrices({
         block: 'latest',
         chain: 'bsc',
         lps: pools.map(p=>p.asset),
-        coreAssets: ['0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'],
+        coreAssets: [],
         minLPRatio: 0.001
     })
 
-    balances = await prices.updateBalances(prices.balances);
-    return balances;
+    let { pairBalances } = res;
+    let tempTokens = [];
+    let prices = {}
+    for (let lp of Object.keys(pairBalances)) {
+      const pair = pairBalances[lp]
+      if (pair[wbnb]) {
+        let t1,t2,a1,a2;
+        for(let token of Object.keys(pair)){
+          if (token === wbnb){
+            t2 = token;
+            a2 = pair[token];
+          }else{
+            t1 = token;
+            a1 = pair[token];
+          }
+        }
+        prices[t1] = a2 / a1;
+      }else{
+        tempTokens.push(pair);
+      }
+    }
+
+    for (let pair of tempTokens) {
+      let price;
+      let t1,t2,a1,a2;
+      for (let token of Object.keys(pair)) {
+        if (prices[token]){
+          price = prices[token]
+          t2 = token;
+          a2 = pair[token]
+        }else {
+          t1 = token;
+          a1 = pair[token];
+        }
+      }
+      prices[t1] = a2 / a1 * price
+    }
+    let bnbTvl = 0;
+    pools.map(p => {
+      if (!prices[p.asset]) return;
+      bnbTvl += prices[p.asset] * parseFloat(p.totalAmount.toString() / 1e18)
+    })
+    balances['bsc:'+wbnb] = bnbTvl * 1e18;
+
+    return balances
 }
 
 async function enulsTvl(_, _1, _2, {api}) {
     const pools = await getENULSPools();
-    const prices = await getTokenPrices({
+    const wnuls = '0x217dffF57E3b855803CE88a1374C90759Ea071bD'
+    const res = await getTokenPrices({
         block: 'latest',
         chain: 'enuls',
         lps: pools.map(p => p.asset),
-        coreAssets: ['0x217dffF57E3b855803CE88a1374C90759Ea071bD'],
+        coreAssets: [],
     })
-    return await prices.updateBalances(prices.balances);
+    let { pairBalances } = res;
+    let tempTokens = [];
+    let prices = {}
+    for (let lp of Object.keys(pairBalances)) {
+      const pair = pairBalances[lp]
+      if (pair[wnuls]) {
+        let t1,t2,a1,a2;
+        for(let token of Object.keys(pair)){
+          if (token === wnuls){
+            t2 = token;
+            a2 = pair[token];
+          }else{
+            t1 = token;
+            a1 = pair[token];
+          }
+        }
+        prices[t1] = a2 / a1;
+      }else{
+        tempTokens.push(pair);
+      }
+    }
+
+    for (let pair of tempTokens) {
+      let price;
+      let t1,t2,a1,a2;
+      for (let token of Object.keys(pair)) {
+        if (prices[token]){
+          price = prices[token]
+          t2 = token;
+          a2 = pair[token]
+        }else {
+          t1 = token;
+          a1 = pair[token];
+        }
+      }
+      prices[t1] = a2 / a1 * price
+    }
+    let enulsTvl = 0;
+    pools.map(p => {
+      if (!prices[p.asset]) return;
+      enulsTvl += prices[p.asset] * parseFloat(p.totalAmount.toString() / 1e18)
+    })
+    balances['enuls:'+wnuls] = enulsTvl * 1e18;
+
+    return balances
 }
 
 module.exports = {
