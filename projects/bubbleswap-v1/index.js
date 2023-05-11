98f63ecdc10333c84efa86105551cd4a797cd216
Mo Shaikjee <shaikjeemohammed@gmail.com>
2023-05-11 10:31:41 +0200

    rename folder name, add v1 deprecation hallmark

---
+++ projects/bubbleswap-v1/index.js
@@ -0,0 +1,15 @@
+async function fetch(){
+
+    return 0;
+
+}
+
+module.exports = {
+  fetch,
+  timetravel: false,
+  methodology: "Data is retrieved from the api at https://api.bubbleswap.io",
+  hallmarks: [
+    [1666569600, "Start V1 Rewards"],
+    [1683813600, "V1 Deprecation"],
+  ]
+}
