ffdd65484465fd1cf705965ccf2ed3fd9fbe0d89
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-11 16:18:23 +0200

    minor fix

--- projects/starmaker/index.js
+++ projects/starmaker/index.js
@@ -1,9 +1,9 @@
-const { uniTvlExport } = require("../helper/calculateUniTvl");
+const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
   misrepresentedTokens: true,
   era: {
-    tvl: uniTvlExport("0x7096Cebc52012e2611a1E88c45bC54ee2A88dcB4", "era"),
+    tvl: getUniTVL({ factory: '0x7096Cebc52012e2611a1E88c45bC54ee2A88dcB4', useDefaultCoreAssets: true, fetchBalances: true, }),
   },
   methodology: "Counts liquidity in pools",
 };
