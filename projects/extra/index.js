5449609a9647fa9ff9cd113837456b8a1ffd8c97
Martin <momo@extrafi.io>
2023-05-11 22:57:02 +0900

    add extra finance tvl

---
+++ projects/extra/index.js
@@ -0,0 +1,46 @@
+const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
+const { graphQuery } = require('../helper/http');
+const BigNumber = require("bignumber.js");
+
+const subgraphUrls = {
+  optimism: `https://api.thegraph.com/subgraphs/name/extrafi/extrasubgraph`,
+};
+
+async function tvl(timestamp, block) {
+  const chain = 'optimism';
+  const balances = {};
+  const vaultsQuery = `{
+    vaults {
+      pair
+      totalLp
+    }
+  }`;
+  const vaultsQueryResult = await graphQuery(subgraphUrls[chain], vaultsQuery);
+  const lpPositions = vaultsQueryResult.vaults.map(vaultPool => {
+    return {
+      token: vaultPool.pair,
+      balance: vaultPool.totalLp
+    };
+  });
+
+  const lendingQuery = `{
+    lendingReservePools {
+      totalLiquidity
+      totalBorrows
+      underlyingTokenAddress
+    }
+  }`;
+  const lendingQueryResult = await graphQuery(subgraphUrls[chain], lendingQuery);
+  lendingQueryResult.lendingReservePools.forEach(lendingPool => {
+    const remainAmount = new BigNumber(lendingPool.totalLiquidity).minus(new BigNumber(lendingPool.totalBorrows)).toFixed(0);
+    balances[`optimism:${lendingPool.underlyingTokenAddress}`] = remainAmount;
+  });
+  await unwrapUniswapLPs(balances, lpPositions, block, chain);
+  return balances
+}
+
+module.exports = {
+  optimism: {
+    tvl,
+  },
+};
