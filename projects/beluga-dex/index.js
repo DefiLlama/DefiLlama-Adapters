eee2468cc07a38752ecea801a0bad457e9758485
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-12 09:55:25 +0200

    code refactor

--- projects/beluga-dex/index.js
+++ projects/beluga-dex/index.js
@@ -1,19 +1,13 @@
-const sdk = require("@defillama/sdk")
 const { staking } = require("../helper/staking");
 const { sumTokens2 } = require('../helper/unwrapLPs')
-const assetsAbi = require("./abi")
 const asssetsContract = "0xF6Eb0eE167e3b8a43E74999C47720140A9431448"
 
-const chain = "arbitrum"
-
-async function tvl(timestamp, ethereumBlock, chainBlocks) {
-  const block = chainBlocks[chain];
-  const { output: toa } = await sdk.api.abi.call({
+async function tvl(timestamp, ethereumBlock, chainBlocks, { api }) {
+  const tokensAndOwners  = await api.call({
     target: asssetsContract,
-    abi: assetsAbi.getAssets,
-    chain, block,
+    abi: "function getAssets() view returns (address[][])",
   })
-  return sumTokens2({ chain: chain, block, tokensAndOwners: toa, })
+  return sumTokens2({ api, tokensAndOwners, })
 }
 
 module.exports = {
@@ -22,7 +16,6 @@ module.exports = {
     staking: staking(
       "0x7fbdEb84D5966c1C325D8CB2E01593D74c9A41Cd", //vetoken
       "0x09090e22118b375f2c7b95420c04414E4bf68e1A", //bela
-      "arbitrum"
     ),
   },
 };
