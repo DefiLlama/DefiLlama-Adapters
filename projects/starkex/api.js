cca7a542e3833afad86b546a81a30e5bf13e7d5d
cantlee <133316389+cantlee@users.noreply.github.com>
2023-05-14 21:26:05 +0800

    Update api.js

--- projects/starkex/api.js
+++ projects/starkex/api.js
@@ -5,7 +5,7 @@ const { transformDexBalances } = require('../helper/portedTokens')
 const { getParamCalls } = require('../helper/utils')
 
 async function tvl() {
-  const factory = '0x00d018832f3b2b082f7ebaa3eae2a5323708a7bb7598db620c0dba0e985e9a53'
+  const factory = '0x07df3bce30857e8f9c08bcd9d9668df34166e94dd968db6e2920b870c4410e34'
   let pairLength = await call({ target: factory, abi: abi.factory.allPairsLength})
   let pairs = await multiCall({ abi: abi.factory.allPairs, target: factory, calls: getParamCalls(+pairLength)})
   
