25c60585aae3e4acb302a0702a66e95ba44fd59c
define <Lovelovenow@protonmail.com>
2023-05-15 15:52:53 +0100

    treasuries

--- projects/treasury/aavegotchi.js
+++ projects/treasury/aavegotchi.js
@@ -2,6 +2,7 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const { nullAddress, treasuryExports } = require("../helper/treasury");
 
 const aavegotchiTreasury2 = "0xFFE6280ae4E864D9aF836B562359FD828EcE8020";
+const treasury2 = "0xfb76e9be55758d0042e003c1e46e186360f0627e"
 const GHST = "0x3F382DbD960E3a9bbCeaE22651E88158d2791550";
 
 
@@ -11,7 +12,7 @@ module.exports = treasuryExports({
         nullAddress,
         ADDRESSES.ethereum.DAI//DAI
      ],
-    owners: [aavegotchiTreasury2],
+    owners: [aavegotchiTreasury2, treasury2],
     ownTokens: [GHST],
   },
 })
