2a01d1530f0c7552d8f6711fb41878e734bfd885
graykode <nlkey2022@gmail.com>
2023-05-15 16:34:11 +0900

    feat: update v1.1.0 contracts for Clober DEX

--- projects/clober/index.js
+++ projects/clober/index.js
@@ -2,29 +2,38 @@ const abi = require("./abi.json");
 const { ethers } = require("ethers");
 const { sumTokens2 } = require('../helper/unwrapLPs')
 
+const contractV1DeployedChainIds = [1, 137, 42161]
+const contractV1_1DeployedChainIds = [137, 1101, 42161]
+
 function calculateSalt(chainId, nonce) {
     return ethers.utils.solidityKeccak256(['uint256', 'uint256'], [chainId, nonce])
 }
 
-async function fetchTokenAddresses(api, chainId){
-    if(chainId === 1 || chainId === 137 || chainId === 42161) {
-        return await api.fetchList({
-            lengthAbi: abi.nonce,
-            itemAbi: "function computeTokenAddress(uint256) view returns (address)",
-            target: "0x93A43391978BFC0bc708d5f55b0Abe7A9ede1B91"
-        })
-    }else if(chainId === 1101){
-        const maxNonce = await api.call({ abi: abi.nonce, target: "0x24aC0938C010Fb520F1068e96d78E0458855111D" })
-        return await api.multiCall({
-            abi: "function computeTokenAddress(bytes32) view returns (address)",
-            calls: Array.from({length: maxNonce}, (_, i) => i ).map((i, v) => ({ target: "0x58ed1f4913e652baF17C154551bd8E9dbc73fC56", params: calculateSalt(chainId, v) })),
-        })
+async function fetchTokenAddressesV1_0(api, chainId){
+    if(!contractV1DeployedChainIds.includes(chainId)){
+        return []
     }
+    return await api.fetchList({
+        lengthAbi: abi.nonce,
+        itemAbi: "function computeTokenAddress(uint256) view returns (address)",
+        target: "0x93A43391978BFC0bc708d5f55b0Abe7A9ede1B91"
+    })
+}
+
+async function fetchTokenAddressesV1_1(api, chainId){
+    if (!contractV1_1DeployedChainIds.includes(chainId)) {
+        return []
+    }
+    const maxNonce = await api.call({ abi: abi.nonce, target: "0x24aC0938C010Fb520F1068e96d78E0458855111D" })
+    return await api.multiCall({
+        abi: "function computeTokenAddress(bytes32) view returns (address)",
+        calls: Array.from({length: maxNonce}, (_, i) => i ).map((i, v) => ({ target: "0x58ed1f4913e652baF17C154551bd8E9dbc73fC56", params: calculateSalt(chainId, v) })),
+    })
 }
 
 async function tvl(_, _b, _cb, { api }) {
     const chainId = await api.getChainId()
-    let tokenAddresses = await fetchTokenAddresses(api, chainId)
+    let tokenAddresses = [...await fetchTokenAddressesV1_0(api, chainId), ...await fetchTokenAddressesV1_1(api, chainId)]
     tokenAddresses = tokenAddresses.flat()
     const markets = await api.multiCall({  abi: abi.market, calls: tokenAddresses })
     const base = await api.multiCall({  abi: abi.baseToken, calls: markets})
