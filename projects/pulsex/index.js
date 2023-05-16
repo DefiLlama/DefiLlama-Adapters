3df8d98113384186c7ba0da96a702decbb06777a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-16 23:07:08 +0200

    add support for pulse chain

--- projects/pulsex/index.js
+++ projects/pulsex/index.js
@@ -4,7 +4,7 @@ module.exports = {
   misrepresentedTokens: true,
   methodology:
     "Factory address (0x1715a3E4A142d8b698131108995174F37aEBA10D) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
-  pulse: {
-    tvl: getUniTVL({ factory: '0x1715a3E4A142d8b698131108995174F37aEBA10D', chain: 'pulse', useDefaultCoreAssets: true }),
+    pulse: {
+    tvl: getUniTVL({ factory: '0x1715a3E4A142d8b698131108995174F37aEBA10D', useDefaultCoreAssets: true }),
   },
 }; // node test.js projects/pulsex/index.js
