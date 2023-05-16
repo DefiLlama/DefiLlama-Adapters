3ffbe041dac54ac24b042a04aa28ac51d42e22d8
allush <al.lushnikov@yandex.ru>
2023-05-16 08:21:16 +0300

    feat: symbiosis refactors config, adds SIS token as trackable liquidity token on Ethereum, add USDC as a trackeble token on BNB chain

--- projects/symbiosis-finance/config.js
+++ projects/symbiosis-finance/config.js
@@ -3,7 +3,10 @@ module.exports = {
   chains: [
     {
       name: 'ethereum',
-      stable: ADDRESSES.ethereum.USDC, // USDC
+      tokens: [
+        ADDRESSES.ethereum.USDC,
+        '0xd38bb40815d2b0c2d2c866e0c72c5728ffc76dd9', // SIS
+      ],
       holders: [
         '0xb80fDAA74dDA763a8A158ba85798d373A5E84d84', // portal v1
         '0xb8f275fBf7A959F4BCE59999A2EF122A099e81A8', // portal v2
@@ -11,7 +14,10 @@ module.exports = {
     },
     {
       name: 'bsc',
-      stable: ADDRESSES.bsc.BUSD, // BUSD
+      tokens: [
+        ADDRESSES.bsc.BUSD,
+        ADDRESSES.bsc.USDC,
+      ],
       holders: [
         '0xD7F9989bE0d15319d13d6FA5d468211C89F0b147', // portal v1
         '0x5Aa5f7f84eD0E5db0a4a85C3947eA16B53352FD4', // portal v2
@@ -20,7 +26,9 @@ module.exports = {
     },
     {
       name: 'avax',
-      stable: ADDRESSES.avax.USDC_e, // USDC.e
+      tokens: [
+        ADDRESSES.avax.USDC_e,
+      ],
       holders: [
         '0xD7F9989bE0d15319d13d6FA5d468211C89F0b147', // portal v1
         '0xE75C7E85FE6ADd07077467064aD15847E6ba9877', // portal v2
@@ -30,7 +38,9 @@ module.exports = {
     },
     {
       name: 'polygon',
-      stable: ADDRESSES.polygon.USDC, // USDC
+      tokens: [
+        ADDRESSES.polygon.USDC,
+      ],
       holders: [
         '0xD7F9989bE0d15319d13d6FA5d468211C89F0b147', // portal v1
         '0xb8f275fBf7A959F4BCE59999A2EF122A099e81A8', // portal v2
@@ -41,7 +51,9 @@ module.exports = {
     },
     {
       name: 'telos',
-      stable: ADDRESSES.telos.USDC, // USDC
+      tokens: [
+        ADDRESSES.telos.USDC,
+      ],
       holders: [
         '0x17A0E3234f00b9D7028e2c78dB2caa777F11490F', // portal v1
         '0xb8f275fBf7A959F4BCE59999A2EF122A099e81A8', // portal v2
@@ -50,23 +62,20 @@ module.exports = {
     },
     {
       name: 'aurora',
-      stable: ADDRESSES.aurora.USDC_e, // USDC
+      tokens: [
+        ADDRESSES.aurora.USDC_e,
+      ],
       holders: [
         '0x17A0E3234f00b9D7028e2c78dB2caa777F11490F', // portal v1
         '0x7Ff7AdE2A214F9A4634bBAA4E870A5125dA521B8', // v1 pool with BNB chain
         '0x7F1245B61Ba0b7D4C41f28cAc9F8637fc6Bec9E4', // v1 pool with Polygon
       ]
     },
-    // {
-    //   name: 'milkomeda',
-    //   stable: ADDRESSES.milkomeda.sUSDC, // sUSDC
-    //   holders: [
-    //     '0x3Cd5343546837B958a70B82E3F9a0E857d0b5fea', // portal v1
-    //   ]
-    // },
     {
       name: 'boba',
-      stable: ADDRESSES.boba.USDC, // USDC
+      tokens: [
+        ADDRESSES.boba.USDC,
+      ],
       holders: [
         '0xD7F9989bE0d15319d13d6FA5d468211C89F0b147', // portal v1
         '0xb8f275fBf7A959F4BCE59999A2EF122A099e81A8', // portal v2
@@ -76,42 +85,54 @@ module.exports = {
     },
     {
       name: 'boba_avax',
-      stable: ADDRESSES.boba_avax.USDC_e, // USDC.e
+      tokens: [
+        ADDRESSES.boba_avax.USDC_e,
+      ],
       holders: [
         '0xd8db4fb1fEf63045A443202d506Bcf30ef404160', // portal v2
       ]
     },
     {
       name: 'boba_bnb',
-      stable: ADDRESSES.boba_bnb.USDC, // USDC
+      tokens: [
+        ADDRESSES.boba_bnb.USDC,
+      ],
       holders: [
         '0x6148FD6C649866596C3d8a971fC313E5eCE84882', // pool v2
       ]
     },
     {
       name: 'kava',
-      stable: ADDRESSES.telos.ETH, // USDC
+      tokens: [
+        ADDRESSES.telos.ETH,
+      ],
       holders: [
         '0x292fC50e4eB66C3f6514b9E402dBc25961824D62', // portal v2
       ]
     },
     {
       name: 'era',
-      stable: ADDRESSES.era.USDC, // USDC
+      tokens: [
+        ADDRESSES.era.USDC,
+      ],
       holders: [
         '0x39dE19C9fF25693A2311AAD1dc5C790194084A39', // portal v2
       ]
     },
     {
       name: 'arbitrum',
-      stable: ADDRESSES.arbitrum.USDC, // USDC
+      tokens: [
+        ADDRESSES.arbitrum.USDC,
+      ],
       holders: [
         '0x01A3c8E513B758EBB011F7AFaf6C37616c9C24d9', // portal v2
       ]
     },
     {
       name: 'optimism',
-      stable: ADDRESSES.optimism.USDC, // USDC
+      tokens: [
+        ADDRESSES.optimism.USDC,
+      ],
       holders: [
         '0x292fC50e4eB66C3f6514b9E402dBc25961824D62', // portal v2
       ]
