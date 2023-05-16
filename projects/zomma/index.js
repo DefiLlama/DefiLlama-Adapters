a8f93c68e46a7365233729e161e5f37002b2ca07
Wayne Chen <xwaynec@gmail.com>
2023-05-16 16:58:58 +0800

    fixed export methodolgy position

--- projects/zomma/index.js
+++ projects/zomma/index.js
@@ -4,8 +4,8 @@ const ZOMMA_CONTRACT = "0x709051774c60c0527dbaf880f41425eae036efaf";
 const USDC_CONTRACT = "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4";
 
 module.exports = {
+  methodolgy: "TVL is calculated as the sum of USDC deposited by traders and the liquidity in the Pools.",
   era: {
-    methodolgy: "TVL is calculated as the sum of USDC deposited by traders and the liquidity in the Pools.",
     tvl: sumTokensExport({
       owner: ZOMMA_CONTRACT,
       tokens: [USDC_CONTRACT],
