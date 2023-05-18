c1cfac80df0bf95bd64b1696c293d2f73c7ab2d0
zkDX-OliverRichards <zkdxtrade@gmail.com>
2023-05-18 18:22:17 +0800

    update staking

--- projects/zkdx/index.js
+++ projects/zkdx/index.js
@@ -1,4 +1,3 @@
-const sdk = require("@defillama/sdk");
 const ADDRESSES = require('../helper/coreAssets.json')
 const {staking} = require("../helper/staking");
 const {gmxExports} = require("../helper/gmx");
@@ -9,14 +8,11 @@ module.exports = {
     methodology: 'zkDX counts the vault and staking values as tvl',
     start: 3744214,
     era: {
-        tvl: sdk.util.sumChainTvls(
-            [
-                gmxExports({vault: "0x6a29d14176248E84760473d28973F53821fB6287"}),
-                staking(
-                    ["0x79033C597B7d8e752a7511cF24512f4A7217C0B8", "0xd6cce119B45Efcb378a4735a96aE08826A37ca1c"],
-                    [ADDRESSES.era.WETH, ADDRESSES.era.USDC]
-                )
-            ]
+        tvl: gmxExports({vault: "0x6a29d14176248E84760473d28973F53821fB6287"}),
+        staking: staking(
+            ["0x79033C597B7d8e752a7511cF24512f4A7217C0B8", "0xd6cce119B45Efcb378a4735a96aE08826A37ca1c"],
+            [ADDRESSES.era.WETH, ADDRESSES.era.USDC]
         )
+
     }
 };
