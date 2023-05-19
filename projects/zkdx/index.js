0501a9d8bb3fc544790d80768acbc16a03393edd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-19 10:33:11 +0200

    minor fix

--- projects/zkdx/index.js
+++ projects/zkdx/index.js
@@ -2,8 +2,6 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const {staking} = require("../helper/staking");
 
 module.exports = {
-    timetravel: false,
-    misrepresentedTokens: false,
     methodology: 'zkDX counts the staking values as tvl',
     start: 3744214,
     era: {
