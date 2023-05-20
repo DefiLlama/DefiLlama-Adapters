a78d4d44006031c7fa6adafcbc6ac77a931a36a7
qudacki <qudacki@gmail.com>
2023-05-20 23:05:49 +0900

    fix calculation

--- projects/kagla/addresses.js
+++ projects/kagla/addresses.js
@@ -5,17 +5,20 @@ const ADDRESS_PROVIDER_ADDRESS = "0x5a0ad8337E5C6895b3893E80c8333859DAcf7c01"
 const KGL_ADDRESS = ADDRESSES.astar.KGL
 const VOTING_ESCROW_ADDRESS = "0x432c8199F548425F7d5746416D98126E521e8174"
 
-const transformTokenAddress = (address) => TOKENS[address]
+const transformTokenAddress = (address) => TOKENS[address.toLowerCase()]
 
 const TOKEN_INFO = {
-  ausd: { key: "acala-dollar", decimals: 12 }
-}
+  ausd: { key: "acala-dollar", decimals: 12 },
+  muKGL: { address: "0x5eaAe8435B178d4677904430BAc5079e73aFa56e" },
+  muLAY: { address: "0xDDF2ad1d9bFA208228166311FC22e76Ea7a4C44D" },
+  astar: { address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE" },
+};
 
 const TOKENS = {
   // KGL
   [KGL_ADDRESS]: "kagla-finance",
   // muKGL: staked KGL by Muuu Finance
-  "0x5eaAe8435B178d4677904430BAc5079e73aFa56e": "kagla-finance",
+  [TOKEN_INFO.muKGL.address.toLowerCase()]: "kagla-finance",
   // USDC
   [ADDRESSES.moonbeam.USDC]: ADDRESSES.ethereum.USDC,
   // Starlay lUSDC -> USDC
@@ -33,15 +36,15 @@ const TOKENS = {
   // BAI
   [ADDRESSES.astar.BAI]: "bai-stablecoin",
   // aUSD
-  "0xfFFFFfFF00000000000000010000000000000001": TOKEN_INFO.ausd.key,
+  [ADDRESSES.astar.aUSD]: TOKEN_INFO.ausd.key,
   // ASTR
-  "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE" : "astar",
+  [TOKEN_INFO.astar.address.toLowerCase()]: "astar",
   // nASTR: staked ASTR by Algem
   [ADDRESSES.astar.nASTR]: "astar",
   // LAY
   [ADDRESSES.astar.LAY]: "starlay-finance",
   // muLAY: staked LAY by Muuu Finance
-  "0xDDF2ad1d9bFA208228166311FC22e76Ea7a4C44D": "starlay-finance",
+  [TOKEN_INFO.muLAY.address.toLowerCase()]: "starlay-finance",
 };
 
 
