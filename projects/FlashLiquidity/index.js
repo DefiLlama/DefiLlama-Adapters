7537f381903c265c4c6080e75a90f5cb70259a9d
flashliquidity.finance <flashliquidity.finance@protonmail.com>
2023-05-20 13:54:08 +0200

    Add files via upload

---
+++ projects/FlashLiquidity/index.js
@@ -0,0 +1,21 @@
+const { getUniTVL } = require("../helper/unknownTokens");
+
+const factory = "0x6e553d5f028bD747a27E138FA3109570081A23aE"
+
+module.exports = {
+  misrepresentedTokens: true,
+  polygon: {
+    tvl: getUniTVL({
+      factory: factory,
+      useDefaultCoreAssets: true,
+    }),
+  },
+  polygon_zkevm: {
+    tvl: getUniTVL({
+      factory: factory,
+      useDefaultCoreAssets: true,
+    }),
+  },
+  methodology:
+    "TVL comes from the DEX liquidity pools"
+};
