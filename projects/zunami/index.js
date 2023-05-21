a73b7bc7b67077cf32fd761aee7470347a4b5a9e
fextr <extr1811@gmail.com>
2023-05-21 12:02:47 +0400

    zunami: add zunami aps totalHoldings

--- projects/zunami/index.js
+++ projects/zunami/index.js
@@ -3,6 +3,7 @@ const sdk = require("@defillama/sdk");
 const abi = require("./abi.json");
 
 const zunamiContract = "0x2ffCC661011beC72e1A9524E12060983E74D14ce";
+const zunamiApsContract = "0xCaB49182aAdCd843b037bBF885AD56A3162698Bd";
 const zunamiHoldingsDecimals = 18;
 
 const usdt = ADDRESSES.ethereum.USDT;
@@ -14,9 +15,16 @@ async function ethTvl(timestamp, block) {
     abi: abi.totalHoldings,
     target: zunamiContract,
   })).output / 10 ** (zunamiHoldingsDecimals - usdtDecimals);
+  const totalHoldingsAps = (await sdk.api.abi.call({
+    block,
+    abi: abi.totalHoldings,
+    target: zunamiApsContract,
+  })).output / 10 ** (zunamiHoldingsDecimals - usdtDecimals);
+
+  const totalHoldings = totalHoldingsOmnipool + totalHoldingsAps;
 
   return {
-    [usdt]: totalHoldingsOmnipool,
+    [usdt]: totalHoldings,
   };
 }
 
