524ced72e973611dce0baf53910ba30aa118f10d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-23 14:03:43 +0200

    cells: add arbitrum

--- projects/cells/index.js
+++ projects/cells/index.js
@@ -1,14 +1,23 @@
 const { yieldHelper, } = require("../helper/yieldHelper")
+const { mergeExports } = require('../helper/utils')
 
-const contract = '0xAcb845A2a46F6De8cbAe8eA234b632a99ef93D31'
-const cells = '0x3022d80e02075F5A2a442A318229487f9Ea66D82'
-
-module.exports = yieldHelper({
-  project: 'cells-finance',
-  chain: 'bsc',
-  masterchef: contract,
-  nativeToken: cells,
-    abis: {
-    poolInfo: 'function poolInfo(uint256) view returns (address want, uint256 , uint256 , uint256 , uint256 , uint256 , uint256 , uint256 amount, uint256 , address strat, address )',
-  }
-})
+module.exports = mergeExports([
+  yieldHelper({
+    project: 'cells-finance',
+    chain: 'bsc',
+    masterchef: '0xAcb845A2a46F6De8cbAe8eA234b632a99ef93D31',
+    nativeToken: '0x3022d80e02075F5A2a442A318229487f9Ea66D82',
+      abis: {
+      poolInfo: 'function poolInfo(uint256) view returns (address want, uint256 , uint256 , uint256 , uint256 , uint256 , uint256 , uint256 amount, uint256 , address strat, address )',
+    }
+  }),
+  yieldHelper({
+    project: 'cells-finance',
+    chain: 'arbitrum',
+    masterchef: '0xAcb845A2a46F6De8cbAe8eA234b632a99ef93D31',
+    nativeToken: '0x3022d80e02075f5a2a442a318229487f9ea66d82',
+      abis: {
+      poolInfo: 'function poolInfo(uint256) view returns (address want, uint256 , uint256 , uint256 , uint256 , uint256 , uint256 , uint256 amount, uint256 , address strat, address )',
+    }
+  }),
+])
