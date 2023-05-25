d4fdcf41a4ce58db87fb696f3b154128aa3aeb3a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-25 11:34:19 +0200

    code refactor

--- projects/MFIL/index.js
+++ projects/MFIL/index.js
@@ -1,29 +1,17 @@
 const MFILPOOLCONTRACT = '0x72A57760aE548B8d1B3a1b29bE25D2f09a6cB558';
-const MFILREGULARPOOLCONTRACT = '0xDcECF046dd21A7298Eb3c0a3c70d716999E7A607';
+const MFILREGULARPOOLCONTRACT = '0xDcECF046dd21A7298Eb3c0a3c70d716999E7A607'
+const ADDRESSES = require('../helper/coreAssets.json')
 
 async function mfilTvl(_, _1, _2, { api }) {
-    console.log("api = ", api);
-
   const mfilPoolStakeMfil = await api.call({
     target: MFILPOOLCONTRACT,
-    abi: "erc20:balanceOf",
-    params: [MFILPOOLCONTRACT],
+    abi: "erc20:totalSupply",
   })
 
-  const mfilRegularPoolStakeMfil = await api.call({
-    target: MFILPOOLCONTRACT,
-    abi: "erc20:balanceOf",
-    params: [MFILREGULARPOOLCONTRACT],
-  })
-
-  return {
-    "filecoin:0x0000000000000000000000000000000000000000": mfilPoolStakeMfil*1 + mfilRegularPoolStakeMfil*1,
-  }
+  api.add(ADDRESSES.null, mfilPoolStakeMfil)
 }
 
 module.exports = {
-  doublecounted: true,
-
   filecoin: {
     tvl: mfilTvl
   }, 
