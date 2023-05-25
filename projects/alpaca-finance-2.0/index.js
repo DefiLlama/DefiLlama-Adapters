61dce7c57d6c29999534eef317ac8112f66fde3e
m100alpaca <129822378+m100alpaca@users.noreply.github.com>
2023-05-25 15:51:27 +0700

    refactor: af2.0

--- projects/alpaca-finance-2.0/index.js
+++ projects/alpaca-finance-2.0/index.js
@@ -1,12 +1,5 @@
 const {lendingTvl, borrowTvl} = require("./moneyMarket");
 
-async function lendingCal(ts, _, _1, {api}) {
-  return await lendingTvl(api.block, api.chain)
-}
-
-async function borrowCal(ts, _, _1, {api}) {
-  return await borrowTvl(api.block, api.chain)
-}
 
 module.exports = {
     start: 1602054167,
@@ -14,7 +7,7 @@ module.exports = {
     doublecounted: false,
     methodology: "Sum floating balance and borrow for each token",
     bsc: {
-      tvl: lendingCal,
-      borrowed: borrowCal
+      tvl: lendingTvl,
+      borrowed: borrowTvl
     },
   };
