f56755454636e16acfb2f8dade6c40c9cf2ab350
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-25 16:40:45 +0200

    code refactor

--- projects/alpaca-finance-v2/index.js
+++ projects/alpaca-finance-v2/index.js
@@ -1,13 +1,10 @@
-const {lendingTvl, borrowTvl} = require("./moneyMarket");
-
+const { lendingTvl, borrowTvl } = require("./moneyMarket");
 
 module.exports = {
-    start: 1602054167,
-    timetravel: true,
-    doublecounted: false,
-    methodology: "Sum floating balance and borrow for each token",
-    bsc: {
-      tvl: lendingTvl,
-      borrowed: borrowTvl
-    },
-  };
+  start: 1602054167,
+  methodology: "Sum floating balance and borrow for each token",
+  bsc: {
+    tvl: lendingTvl,
+    borrowed: borrowTvl
+  },
+};
--- projects/alpaca-finance-v2/moneyMarket.js
+++ projects/alpaca-finance-v2/moneyMarket.js
@@ -1,57 +1,27 @@
 const sdk = require("@defillama/sdk");
 const abi = require("./abi.json");
 const { getConfig } = require("../helper/cache");
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
 async function getMoneyMarketData() {
     return (await getConfig("alpaca-finance-2.0", "https://raw.githubusercontent.com/alpaca-finance/alpaca-v2-money-market/main/.mainnet.json")).moneyMarket;
 }
 
 async function lendingTvl(ts, _, _1, {api}) {
-
-  const balances = {};
-  const moneyMarket = await getMoneyMarketData();
-
-  const lendingSupplies = (await sdk.api.abi.multiCall({
-    block: api.block,
-    chain: api.chain,
-    abi: abi.getFloatingBalance,
-    calls: moneyMarket.markets.map( (market) => {
-      return {
-        target: moneyMarket.moneyMarketDiamond,
-        params: [market.token]
-      }
-    }),
-  })).output
-
-  for(let i = 0; i < moneyMarket.markets.length; i++) {
-    sdk.util.sumSingleBalance(balances,moneyMarket.markets[i].token, lendingSupplies[i].output, api.chain);
-  }
-
-  return balances;
+  const { moneyMarketDiamond, markets } = await getMoneyMarketData();
+  return sumTokens2({ api, owner: moneyMarketDiamond, tokens: markets.map(i => i.token)})
 }
 
 async function borrowTvl(ts, _, _1, {api}) {
-  const balances = {};
-  const moneyMarket = await getMoneyMarketData();
+  const  { moneyMarketDiamond, markets } = await getMoneyMarketData();
+  const tokens = markets.map(i => i.token)
 
-  const borrows = (await sdk.api.abi.multiCall({
-    block: api.block,
-    chain: api.chain,
+  const borrows = (await api.multiCall({
     abi: abi.getGlobalDebtValueWithPendingInterest,
-    calls: moneyMarket.markets.map((market) => {
-      return {
-        target: moneyMarket.moneyMarketDiamond,
-        params: [market.token]
-      }
-    })
-  })).output
-
-
-  for(let i = 0; i < moneyMarket.markets.length; i++) {
-    sdk.util.sumSingleBalance(balances, moneyMarket.markets[i].token, borrows[i].output, api.chain);
-  }
-
-  return balances;
+    target: moneyMarketDiamond,
+    calls: tokens
+  }))
+  api.addTokens(tokens, borrows)
 }
 
 module.exports = {
