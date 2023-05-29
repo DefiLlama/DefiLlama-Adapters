d67ccdfa822221aa7e68a83d4df8b618179e1672
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-29 13:34:01 +0200

    code refactor

--- projects/range/index.js
+++ projects/range/index.js
@@ -1,65 +1,38 @@
 const { getLogs } = require('../helper/cache/getLogs')
 
-const sdk = require('@defillama/sdk');
 const ABI = require('./abi.json');
 const config = {
-  ethereum: {
-    factory: '0xf1e70677fb1f49471604c012e8B42BA11226336b',
-    fromBlock: 17266660,
-  },
-  arbitrum: {
-    factory: '0xB9084c75D361D1d4cfC7663ef31591DeAB1086d6',
-    fromBlock: 88503603,
-  },
-  bsc: {
-    factory: '0xad2b34a2245b5a7378964BC820e8F34D14adF312',
-    fromBlock: 28026886,
-  },
-  polygon: {
-    factory: '0xad2b34a2245b5a7378964BC820e8F34D14adF312',
-    fromBlock: 42446548,
-  }
+  ethereum: { factory: '0xf1e70677fb1f49471604c012e8B42BA11226336b', fromBlock: 17266660, },
+  arbitrum: { factory: '0xB9084c75D361D1d4cfC7663ef31591DeAB1086d6', fromBlock: 88503603, },
+  bsc: { factory: '0xad2b34a2245b5a7378964BC820e8F34D14adF312', fromBlock: 28026886, },
+  polygon: { factory: '0xad2b34a2245b5a7378964BC820e8F34D14adF312', fromBlock: 42446548, }
 }
-
-async function tvl(timestamp, blocks, chainBlocks, { api }) {
-  const balances = {};
-  const chain = api.chain;
-  const { factory, fromBlock } = config[chain];
-  const logs = await getLogs({
-    target: factory,
-    topic: 'VaultCreated(address,address)',
-    api,
-    fromBlock,
-    chain: chain,
-  });
-  for (const log of logs) {
-    const uni_pool = `0x${log.topics[1].substring(26)}`.toLowerCase();
-    const vault =`0x${log.topics[2].substring(26)}`.toLowerCase();
-    const token0 = (await sdk.api.abi.call({target: uni_pool,abi: ABI.token0,chain: chain}))['output'];
-    const token1 = (await sdk.api.abi.call({target: uni_pool,abi: ABI.token1,chain: chain}))['output'];
-    const underLyingBalance = (await sdk.api.abi.call({target: vault, abi: ABI.underlyingBalance, chain: chain, block:chainBlocks[chain]})).output;
-    await sdk.util.sumSingleBalance(balances, token0, underLyingBalance['amount0Current'],chain);
-    await sdk.util.sumSingleBalance(balances, token1, underLyingBalance['amount1Current'],chain);
-  }
-return balances;
-}
-
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'assets deployed on DEX as LP + asset balance of vaults',
   start: 1683965157,
-  ethereum: {
-    tvl,
-  },
-  arbitrum: {
-    tvl,
-  },
-  bsc: {
-    tvl,
-  },
-  polygon: {
-    tvl,
-  }
 };
+
+Object.keys(config).forEach(chain => {
+  const { factory, fromBlock } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const logs = await getLogs({
+        api,
+        target: factory,
+        topic: 'VaultCreated(address,address)',
+        eventAbi: 'event VaultCreated(address indexed uniPool, address indexed vault)',
+        onlyArgs: true,
+        fromBlock,
+      })
+      const vaults = logs.map(log => log.vault)
+      const token0s = await api.multiCall({ abi: "address:token0", calls: vaults, })
+      const token1s = await api.multiCall({ abi: "address:token1", calls: vaults, })
+      const bals = await api.multiCall({ abi: ABI.underlyingBalance, calls: vaults, })
+      bals.forEach(({ amount0Current, amount1Current }, i) => {
+        api.add(token0s[i], amount0Current)
+        api.add(token1s[i], amount1Current)
+      })
+    }
+  }
+})
