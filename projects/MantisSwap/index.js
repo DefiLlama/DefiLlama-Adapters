698c2468bc8d0faf3f1114777c25c7801710e9f3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-29 20:41:08 +0200

    code refactor

--- projects/MantisSwap/index.js
+++ projects/MantisSwap/index.js
@@ -1,64 +1,26 @@
-const ADDRESSES = require("../helper/coreAssets.json");
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
-const polygonTokenDict = {
-  "0x62Ba5e1AB1fa304687f132f67E35bFC5247166aD": [
-    ADDRESSES.polygon.USDC,
-    ADDRESSES.polygon.USDT,
-    ADDRESSES.polygon.DAI,
-  ],
-};
+const config = {
+  polygon: { vaults: ['0x62Ba5e1AB1fa304687f132f67E35bFC5247166aD'] },
+  polygon_zkevm: { vaults: ['0x12d41b6DF938C739F00c392575e3FD9292d98215'] },
+}
 
-const zkevmTokenDict = {
-  "0x12d41b6DF938C739F00c392575e3FD9292d98215": [
-    ADDRESSES.polygon_zkevm.USDC,
-    "0x1E4a5963aBFD975d8c9021ce480b42188849D41d", // USDT
-    "0xC5015b9d9161Dca7e18e32f6f25C4aD850731Fd4", // DAI
-  ],
-};
+const MAX_LP_TOKENS = 3
 
-async function getTotalAssets(pool, api, chain) {
-  let tokens = [];
-  if (chain == "polygon") {
-    tokens = polygonTokenDict[pool];
-  } else if (chain == "polygon_zkevm") {
-    tokens = zkevmTokenDict[pool];
+Object.keys(config).forEach(chain => {
+  const { vaults } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const calls = vaults.map(vault => {
+        let res = []
+        for (let i = 0; i < MAX_LP_TOKENS; i++)
+          res.push({ target: vault, params: i })
+        return res
+      }).flat()
+      let lpVaults = await api.multiCall({ abi: 'function lpList(uint256) view returns (address)', calls, permitFailure: true })
+      lpVaults = lpVaults.filter(v => v)
+      const tokens = await api.multiCall({ abi: 'address:underlier', calls: lpVaults })
+      return sumTokens2({ api, tokensAndOwners2: [tokens, lpVaults] })
+    }
   }
-  const owners = await api.multiCall({
-    abi: "function tokenLPs(address) view returns (address)",
-    calls: tokens,
-    target: pool,
-  });
-  return tokens.map((val, i) => [val, owners[i]]);
-}
-
-async function polygonTvl(timestamp, ethereumBlock, chainBlocks, { api }) {
-  const totalAssets = (
-    await Promise.all(
-      Object.keys(polygonTokenDict).map((i) =>
-        getTotalAssets(i, api, "polygon")
-      )
-    )
-  ).flat();
-  return sumTokens2({ api, tokensAndOwners: totalAssets });
-}
-
-async function zkevmTvl(timestamp, ethereumBlock, chainBlocks, { api }) {
-  const totalAssets = (
-    await Promise.all(
-      Object.keys(zkevmTokenDict).map((i) =>
-        getTotalAssets(i, api, "polygon_zkevm")
-      )
-    )
-  ).flat();
-  return sumTokens2({ api, tokensAndOwners: totalAssets });
-}
-
-module.exports = {
-  polygon: {
-    tvl: polygonTvl,
-  },
-  polygon_zkevm: {
-    tvl: zkevmTvl,
-  },
-};
+})
