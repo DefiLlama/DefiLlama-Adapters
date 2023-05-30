8cf8c782a2228b633bad63ff2e62f1ce3287af25
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-30 16:13:05 +0200

    fix sablier

--- projects/sablier/index.js
+++ projects/sablier/index.js
@@ -1,6 +1,7 @@
-const { GraphQLClient, gql } = require('graphql-request')
 const { sumTokens2 } = require('../helper/unwrapLPs')
+const { covalentGetTokens } = require('../helper/http')
 const { isWhitelistedToken } = require('../helper/streamingHelper')
+const { getUniqueAddresses } = require('../helper/utils')
 
 const sablierAddresses = {
   "v1.0.0": "0xA4fc358455Febe425536fd1878bE67FfDBDEC59a",
@@ -9,29 +10,36 @@ const sablierAddresses = {
 
 const blacklistedTokens = [
   '0x57ab1e02fee23774580c119740129eac7081e9d3', // sUSD legacy
+  '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+  '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
+  '0x57ab1e02fee23774580c119740129eac7081e9d3',
 ]
 
-async function getTokens(isVesting) {
-  const graphQLClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/sablierhq/sablier')
-  var query = gql`{tokens (first: 1000) { id name symbol }}`
-  const results = await graphQLClient.request(query)
-  return results.tokens.filter(i => isWhitelistedToken(i.symbol, i.id, isVesting)).map(i => i.id)
+async function getTokens(api, isVesting) {
+  // const graphQLClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/sablierhq/sablier')
+  // var query = gql`{tokens (first: 1000) { id name symbol }}`
+  // const results = await graphQLClient.request(query)
+  let tokens = (await Promise.all(Object.values(sablierAddresses).map(i => covalentGetTokens(i, 'ethereum')))).flat().filter(i => !blacklistedTokens.includes(i))
+  tokens = getUniqueAddresses(tokens)
+  const symbols = await api.multiCall({ abi: 'erc20:symbol', calls: tokens })
+  return tokens.filter((v, i) => isWhitelistedToken(symbols[i], v, isVesting))
 }
 
-async function tvl(_, block) {
-  const tokens = await getTokens(false)
-  return sumTokens2({ block, owners: Object.values(sablierAddresses), tokens: tokens, blacklistedTokens, })
+async function tvl(_, block, _1, { api }) {
+  const tokens = await getTokens(api, false)
+  return sumTokens2({ api, owners: Object.values(sablierAddresses), tokens, blacklistedTokens, })
 }
 
-async function vesting(_, block) {
-  const tokens = await getTokens(true)
-  return sumTokens2({ block, owners: Object.values(sablierAddresses), tokens: tokens, blacklistedTokens, })
+async function vesting(_, block, _1, { api }) {
+  const tokens = await getTokens(api, true)
+  return sumTokens2({ api, owners: Object.values(sablierAddresses), tokens, blacklistedTokens, })
 }
 
 module.exports = {
   hallmarks: [
-    [Math.floor(new Date('2022-10-03')/1e3), 'Vesting tokens are not included in tvl'],
+    [Math.floor(new Date('2022-10-03') / 1e3), 'Vesting tokens are not included in tvl'],
   ],
   start: 1573582731,
+  timetravel: false,
   ethereum: { tvl, vesting, }
 };
