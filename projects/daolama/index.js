f15a659359fcafb0ff9b0d3748fee45a1db02c7b
Mpower <mpower.public@yandex.ru>
2023-05-31 11:30:50 +0300

    feat: add DAOLama project
    - change the TVL methodology

--- projects/daolama/index.js
+++ projects/daolama/index.js
@@ -2,7 +2,7 @@ const { tvl } = require('./tvl');
 const { borrowed } = require('./borrowed');
 
 module.exports = {
-  methodology: 'Counts minimum values of all pledged NFTs as borrowed TON coins (50% of actual prices) plus the pool size. Actual prices of total NFTs may be higher. Doesn\'t count NFTs that were not bought back.',
+  methodology: 'Counts the pool size as the TVL. Borrowed coins are not counted towards the TVL.',
   timetravel: false,
   misrepresentedTokens: true,
   ton: {
--- projects/daolama/tvl.js
+++ projects/daolama/tvl.js
@@ -2,13 +2,11 @@ const sdk = require("@defillama/sdk");
 const { get } = require("../helper/http");
 const { transformBalances } = require("../helper/portedTokens");
 const ADDRESSES = require("../helper/coreAssets.json");
-const { BASE_API_URL, POOL_ADDRESS } = require("./constants");
+const { POOL_ADDRESS } = require("./constants");
 const nullAddress = ADDRESSES.null;
 
 async function tvl(chain, timestamp, chainBlocks, { api }) {
   const balances = {};
-  const result = await get(`${BASE_API_URL}/api/v1/analytics/borrowed`);
-  sdk.util.sumSingleBalance(balances, nullAddress, result.value, api.chain);
   const balance = await get(`https://toncenter.com/api/v2/getAddressBalance?address=${encodeURIComponent(POOL_ADDRESS)}`)
   sdk.util.sumSingleBalance(balances, nullAddress, balance.result, api.chain);
   return transformBalances(api.chain, balances);
