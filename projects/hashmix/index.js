2bf9e2c248670e5d5a0c9e37e07d94fd472b532f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-31 10:00:47 +0200

    code refactor

--- projects/hashmix/index.js
+++ projects/hashmix/index.js
@@ -1,42 +1,16 @@
-const sdk = require("@defillama/sdk");
-// const {get} = require("../helper/http");
-const BigNumber = require('bignumber.js');
 const POOL = "0x587A7eaE9b461ad724391Aa7195210e0547eD11d";
+const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
 
 async function tvl(_, _1, _2, { api }) {
+  const totalBorrows = await api.call({    target: POOL,    abi: "uint256:totalBorrows",  });
+    const totalReserves = await api.call({    target: POOL,    abi: "uint256:totalReserves",  });
+    api.add(nullAddress, totalBorrows)
+    api.add(nullAddress, totalReserves * -1)
 
-  // let price = await get("https://api.coingecko.com/api/v3/simple/price?ids=filecoin&vs_currencies=usd");
-  // let priceFloat = price.filecion.usd;
-  
-  const balances = {};
-  const bal = await sdk.api2.eth.getBalance({target: POOL, chain: api.chain, decimals: api.decimals});
-  const totalBorrows = await sdk.api2.abi.call({
-    target: POOL,
-    abi: "uint256:totalBorrows",
-    chain: api.chain,
-  });
-  const totalReserves = await sdk.api2.abi.call({
-    target: POOL,
-    abi: "uint256:totalReserves",
-    chain: api.chain,
-  });
-
-  let b = new BigNumber.BigNumber(bal.output);
-  b = b.plus(new BigNumber.BigNumber(totalBorrows)).minus(new BigNumber.BigNumber(totalReserves));
-
-  sdk.util.sumSingleBalance(
-    balances,
-    "0x0000000000000000000000000000000000000000",
-    b.toFixed(0),
-    api.chain
-  );
-
-  return balances;
+  return sumTokens2({ api, owner: POOL, tokens: [nullAddress]});
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology:
     "HashMix FIL Liquid Staking Protocol is a decentralized staking protocol on Filecoin, connecting FIL holders and miners in the ecosystem.",
   filecoin: {
