600c668770310962cf12c4a7f160ba4f8a2d4f6d
Ivan <76028641+block4chain226@users.noreply.github.com>
2023-05-31 19:37:01 +0300

    Create index.js

---
+++ projects/dubble-exchange/index.js
@@ -0,0 +1,37 @@
+const sdk = require("@defillama/sdk");
+const { api } = require("@defillama/sdk");
+const abi = require("./abi.json");
+
+const VAULT_CONTRACT = "0xD522395dfD017F47a932D788eC7CB058aDBbc783";
+
+async function tvl(timestamp, ethBlock) {
+  const balances = {};
+
+  const stablecoins = (
+    await sdk.api.abi.call({
+      abi: abi.getAllAssets,
+      target: VAULT_CONTRACT,
+      block: ethBlock,
+    })
+  ).output;
+
+  for (let i = 0; i < stablecoins.length; i++) {
+    const balance_stablecoin = (
+      await sdk.api.abi.call({
+        abi: abi.checkBalance,
+        target: VAULT_CONTRACT,
+        params: stablecoins[i],
+        block: ethBlock,
+      })
+    ).output;
+
+    sdk.util.sumSingleBalance(balances, stablecoins, balance_stablecoin);
+  }
+  return balances;
+}
+
+module.exports = {
+  arbitrum: {
+    tvl: tvl,
+  },
+};
