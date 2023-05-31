1e8a54db0f6b4b208adaf2148ea88ca93a25c5ea
Rentero <weixiurenyuanyong@MacBook-Pro-2.local>
2023-05-31 13:33:54 +0800

    ADD:letsfil

---
+++ projects/letsfil/index.js
@@ -0,0 +1,24 @@
+const sdk = require('@defillama/sdk');
+const LETSFIL_CONTRACT = '0xd08F7A4544a7C21283B4a063ebb53ADFc0506562';
+
+async function tvl(_, _1, _2, { api }) {
+  const balances = {};
+  const collateralBalance = await api.call({
+    abi: 'function getTvl() external view returns (uint256)',
+    target: LETSFIL_CONTRACT,
+  });
+
+  await sdk.util.sumSingleBalance(balances, LETSFIL_CONTRACT, collateralBalance, api.chain)
+  return balances;
+}
+
+
+
+module.exports = {
+  timetravel: false,
+  misrepresentedTokens: false,
+  methodology: 'counts the number of pledge filecoins in the letsfil contract',
+  filecoin: {
+    tvl
+  }
+};
