4b1e273306db023a48364ceee272ea2e75ed1dcf
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-01 16:55:26 +0200

    xpla: code refactor

--- projects/xpla/index.js
+++ projects/xpla/index.js
@@ -1,5 +1,10 @@
-const { xpla } = require('./factoryTvl')
+const { getFactoryTvl } = require('../terraswap/factoryTvl')
 
 module.exports = {
-  xpla: { tvl: xpla() }
-}; 
+  timetravel: false,
+  misrepresentedTokens: true,
+  methodology: "Liquidity on the DEX",
+  xpla: {
+    tvl: getFactoryTvl('xpla1j33xdql0h4kpgj2mhggy4vutw655u90z7nyj4afhxgj4v5urtadq44e3vd')
+  },
+}
