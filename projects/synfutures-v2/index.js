dd9810f815497fd2fd81c79427e872bcd9b06cab
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-02 23:48:38 +0200

    code refactor

--- projects/synfutures-v2/index.js
+++ projects/synfutures-v2/index.js
@@ -1,37 +1,26 @@
-// SynFutures-v1 TVL from chain
-const { request, gql } = require("graphql-request");
 const { sumTokens2 } = require('../helper/unwrapLPs')
+const { getLogs, getAddress } = require('../helper/cache/getLogs');
 
-const QUERY_UNDERLYINGS = gql`{
-  underlyings{
-    id
-    symbol
-    quote{
-      id
+const config = {
+  polygon: { factory: '0x1267c6e5d4048318ae48f936130c292e2e0edd73', fromBlock:   32799818  },
+}
+
+Object.keys(config).forEach(chain => {
+  const { factory, fromBlock, } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const logs = await getLogs({
+        api,
+        target: factory,
+        topics: ['0x3e54f626b503b6952f636f8f05b35a3a2c6345dd88a99285dede80fc89961706'],
+        fromBlock,
+      })
+      const tokensAndOwners =  logs.map((i) => {
+        const token = getAddress(i.data.slice(0, 64 + 2));
+        const pool = getAddress(i.data.slice(64 * 3, 64 * 4 + 2));
+        return [token, pool];
+      });
+      return sumTokens2({ tokensAndOwners, api, })
     }
   }
-}`;
-
-const info = {
-  polygon: {
-    subgraph: 'https://api.thegraph.com/subgraphs/name/synfutures/v2-polygon',
-  }
-}
-
-function chainTvl(chain) {
-  return async (_, _b, {[chain]: block}) => {
-    const data = await request(info[chain].subgraph,QUERY_UNDERLYINGS,{ block });
-    const toa = []
-
-    for (let underlying of data.underlyings)
-      toa.push([underlying.quote.id, underlying.id])
-    return sumTokens2({ chain, block, tokensAndOwners: toa, })
-  }
-}
-
-module.exports = {
-  timetravel: false,
-  polygon: {
-    tvl: chainTvl('polygon'),
-  }
-}
+})
