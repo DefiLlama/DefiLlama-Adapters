0ebb7247aea37ed403e110e230f3f68498de5350
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-04 10:01:42 +0200

    minor fix

--- projects/neutra-finance/index.js
+++ projects/neutra-finance/index.js
@@ -1,3 +1,5 @@
+const ADDRESSES = require('../helper/coreAssets.json')
+
 async function tvl(_, _b, _cb, { api }) {
   const nGlpVaults = "0x6Bfa4F1DfAfeb9c37E4E8d436E1d0C5973E47e25";
   const nUSDCVault = "0x2a958665bC9A1680135241133569C7014230Cb21";
@@ -11,11 +13,11 @@ async function tvl(_, _b, _cb, { api }) {
     target: nUSDCVault,
   });
 
-  const tvl = nUSDCTotalValue + (nGlpTotalValue / 1e24).toFixed();
-
-  return tvl; // tvl in 6 decimal
+  const tvl = +nUSDCTotalValue + nGlpTotalValue / 1e24;
+  api.add(ADDRESSES.arbitrum.USDC, tvl)
 }
 module.exports = {
+  misrepresentedTokens: true,
   arbitrum: {
     tvl,
   },
