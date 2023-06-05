12d5275ffdf7cadb1ea2fce1524fdab17424546a
hoomp3 <hoomp3@pm.me>
2023-06-05 09:47:08 -0400

    Update Shade Protocol (#6475)
    * add new shade protocol adapters
    
    * fix linting

---
+++ projects/shadeprotocol-lend/index.js
@@ -0,0 +1,16 @@
+const { get } = require("../helper/http")
+
+// Total Collateral Deposited in Vaults + Silk in Earn Pool
+async function tvl(_, _b, _cb, { api, }) {
+  const data = await get('https://ruvzuawwz7.execute-api.us-east-1.amazonaws.com/prod-analytics-v1/lend')
+  return {
+    tether: data.totalUsd
+  }
+}
+
+module.exports = {
+  misrepresentedTokens: true,
+  secret: {
+    tvl
+  }
+}
