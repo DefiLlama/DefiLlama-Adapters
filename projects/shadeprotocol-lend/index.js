609f507200f7062cf1fd859c60dc0ba238ec8c67
realdealshaman <realdealshaman11@gmail.com>
2023-06-05 23:44:28 +0100

    count value of each collateral as tvl

--- projects/shadeprotocol-lend/index.js
+++ projects/shadeprotocol-lend/index.js
@@ -1,10 +1,18 @@
 const { get } = require("../helper/http")
 
-// Total Collateral Deposited in Vaults + Silk in Earn Pool
+// Total Collateral Deposited in Vaults 
 async function tvl(_, _b, _cb, { api, }) {
   const data = await get('https://ruvzuawwz7.execute-api.us-east-1.amazonaws.com/prod-analytics-v1/lend')
+
+  let totalValue = 0;
+
+  for (let i = 0; i < data.collaterals.length; i++) {
+    // Add the value of each collateral to the total value
+    totalValue += data.collaterals[i].value;
+  }
+
   return {
-    tether: data.totalUsd
+    tether: totalValue
   }
 }
 
