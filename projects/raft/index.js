34ec4cc085648287bd60cdfc3f235f9e8f267894
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-06 08:44:24 +0200

    minor refactor

--- projects/raft/index.js
+++ projects/raft/index.js
@@ -1,30 +1,10 @@
-const sdk = require("@defillama/sdk");
+const { sumTokensExport } = require('../helper/unwrapLPs')
+const ADDRESSES = require('../helper/coreAssets.json')
 
 const RAFT_POSITION_MANAGER = '0x5f59b322eb3e16a0c78846195af1f588b77403fc';
-const WSTETH_ADDRESS = '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0';
-
-async function tvl(timestamp, block) {
-    let balances = {};
-    const lockedBalance = (
-    await sdk.api.abi.call({
-        target: WSTETH_ADDRESS,
-        abi: 'erc20:balanceOf',
-        params: [RAFT_POSITION_MANAGER],
-        block
-    })
-    ).output
-
-    sdk.util.sumSingleBalance(
-      balances,
-      WSTETH_ADDRESS,
-      lockedBalance
-    );
-  
-  return balances;
-}
 
 module.exports = {
   ethereum: {
-    tvl,
+    tvl: sumTokensExport({ owner: RAFT_POSITION_MANAGER, tokens: [ADDRESSES.ethereum.WSTETH]}),
   }
-};
+}
