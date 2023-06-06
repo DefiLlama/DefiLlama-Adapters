d36de0848ec8ba8c2be915dd46f67b4437365cca
stacey <stacey@zklend.com>
2023-06-06 08:11:38 +0000

    feat: replace data source with contract call

--- projects/zklend/index.js
+++ projects/zklend/index.js
@@ -1,8 +1,6 @@
-const { fetchURL } = require('../helper/utils')
 const { BigNumber } = require("bignumber.js");
-
-
-const endpoint = "https://data.app.zklend.com/pools"
+const { multiCall, sumTokens } = require('../helper/chain/starknet')
+const { marketAbi } = require('./abi');
 
 const valueToBigNumber = (amount) => {
     if (amount instanceof BigNumber) {
@@ -10,64 +8,30 @@ const valueToBigNumber = (amount) => {
     }
     return new BigNumber(amount);
 }
+const market = '0x4c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05'
 
-const normalizeBN = (n, decimals) => {
-    return valueToBigNumber(n).shiftedBy(decimals * -1);
+const assets = [
+    '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac',
+    '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+    '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
+    '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3',
+    '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8'
+]
+
+async function tvl(_, _1, _2, { api }) {
+    return sumTokens({ api, owner: market, tokens: assets })
 }
 
-const getUnderlyingAssets = (token) => {
-    return {
-        'ETH': '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
-        'USDC': '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
-        'WBTC': '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac',
-        'USDT': '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8',
-        'DAI': '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3',
-    }[token]
-}
-
-async function fetch() {
-    const markets = await fetchURL(endpoint)
-    let tvl = 0;
-    markets.data.forEach(market => {
-        const tokenPrice = normalizeBN(
-            market.price.price,
-            market.price.decimals
-        );
-        const supplyVolume = normalizeBN(
-            market.supply_amount,
-            market.token.decimals
-        ).multipliedBy(tokenPrice);
-        tvl += supplyVolume.toNumber()
-    })
-
-    return tvl
-}
-
-const borrowed = async (_, _1, _2, { api }) => {
-    let markets = await fetchURL(endpoint)
-    const underlyings = [
-        '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac',
-        '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
-        '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
-        '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3',
-        '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8'
-    ]
-    let data = {}
-    markets.data.forEach(market => {
-        const borrowVolume = valueToBigNumber(market.debt_amount)
-        data[getUnderlyingAssets(market.token.symbol)] = borrowVolume.toNumber()
-    })
-
-    let sortedData = [] 
-    underlyings.map((underlying) => { sortedData.push(data[underlying]) })
-    api.addTokens(underlyings, sortedData)
+async function borrowed(_, _1, _2, { api }) {
+    let data = await multiCall({ calls: assets, target: market, abi: marketAbi.get_total_debt_for_token });
+    data = data.map(i => valueToBigNumber(i).toNumber())
+    api.addTokens(assets, data)
 }
 
 module.exports = {
     methodology: 'Value of user supplied asset on zkLend is considered as TVL',
     starknet: {
-        fetch,
+        tvl,
         borrowed
     },
-    fetch
 }
