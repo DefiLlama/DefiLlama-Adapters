af403fd0ec5f6ea728ed63e5d650a83d5ad1c9b4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-06 17:58:22 +0200

    code refactor

--- projects/dubble-exchange/index.js
+++ projects/dubble-exchange/index.js
@@ -1,24 +1,13 @@
-const sdk = require('@defillama/sdk');
-const { transformArbitrumAddress } = require('../helper/portedTokens');
 const VAULT_CONTRACT = "0xD522395dfD017F47a932D788eC7CB058aDBbc783";
-const USDC = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
+const ADDRESSES = require('../helper/coreAssets.json')
 
-async function tvl(timestamp, block, chainBlocks){
-  const balances = {};
-  const transform = await transformArbitrumAddress();
-
-  const collateralBalance = (await sdk.api.abi.call({
-    abi: "uint256:checkBalance",
-    chain: 'arbitrum',
-    target: VAULT_CONTRACT,
-    block: chainBlocks['arbitrum'],
-  })).output;
-
-  sdk.util.sumSingleBalance(balances, transform(USDC), collateralBalance);
-  return balances;
+async function tvl(timestamp, block, chainBlocks, { api }) {
+  const bal = await api.call({ abi: 'uint256:checkBalance', target: VAULT_CONTRACT })
+  api.add(ADDRESSES.arbitrum.USDC, bal)
 }
 
 module.exports = {
+  misrepresentedTokens: true,
   arbitrum: {
     tvl
   }
