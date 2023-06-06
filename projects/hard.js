31e70aceca89bac3dd2bdc7bf52fa090c97f5a24
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-06 14:47:35 +0200

    fix kava

--- projects/hard.js
+++ projects/hard.js
@@ -1,6 +1,4 @@
 const { queryV1Beta1 } = require('./helper/chain/cosmos');
-const { transformBalances } = require('./helper/portedTokens')
-
 const chain = 'kava'
 
 const tvl = async (_, _1, _2, { api }) => {
@@ -8,17 +6,15 @@ const tvl = async (_, _1, _2, { api }) => {
     queryV1Beta1({ chain, url: 'hard/v1beta1/total-deposited' }),
     queryV1Beta1({ chain, url: 'hard/v1beta1/total-borrowed' })
   ])
-  deposited.supplied_coins.forEach(({ denom, amount }) => api.add(denom, amount, { skipChain: true }))
-  borrowed.borrowed_coins.forEach(({ denom, amount }) => api.add(denom, amount * -1, { skipChain: true }))
-  return transformBalances(chain, api.getBalances())
+  deposited.supplied_coins.forEach(({ denom, amount }) => api.add(denom, amount))
+  borrowed.borrowed_coins.forEach(({ denom, amount }) => api.add(denom, amount * -1))
 }
 
 
 
 const borrowed = async (_, _1, _2, { api }) => {
   const borrowed = await queryV1Beta1({ chain, url: 'hard/v1beta1/total-borrowed' })
-  borrowed.borrowed_coins.forEach(({ denom, amount }) => api.add(denom, amount, { skipChain: true }))
-  return transformBalances(chain, api.getBalances())
+  borrowed.borrowed_coins.forEach(({ denom, amount }) => api.add(denom, amount))
 }
 
 module.exports = {
--- projects/kava-earn/index.js
+++ projects/kava-earn/index.js
@@ -1,12 +1,10 @@
 const { queryV1Beta1 } = require('../helper/chain/cosmos');
-const { transformBalances } = require('../helper/portedTokens')
 
 const chain = 'kava'
 
 async function tvl(_, _1, _2, { api }) {
   const { result: pools } = await queryV1Beta1({ chain, url: '/earn/v1beta1/total_supply' });
-  pools.forEach(({ denom, amount }) => api.add(denom, amount, { skipChain: true }))
-  return transformBalances(chain, api.getBalances())
+  pools.forEach(({ denom, amount }) => api.add(denom, amount))
 }
 
 module.exports = {
