6fb30f6776c68d2d0170f06103aaafdc26a823f0
wshino <wshinohara@gmail.com>
2023-06-06 14:53:39 +0900

    breakdown predy versions

--- projects/predy-v320/index.js
+++ projects/predy-v320/index.js
@@ -3,9 +3,6 @@ const sdk = require('@defillama/sdk');
 const { sumTokensExport } = require('../helper/unwrapLPs')
 const BigNumber = require("bignumber.js");
 
-const v2Address = '0xc7ec02AEeCdC9087bf848c4C4f790Ed74A93F2AF';
-const v202Address = '0xAdBAeE9665C101413EbFF07e20520bdB67C71AB6';
-const v3Address = '0x4006A8840F8640A7D8F46D2c3155a58c76eCD56e';
 const v320Address = '0x68a154fB3e8ff6e4DA10ECd54DEF25D9149DDBDE';
 
 const WETH_CONTRACT = ADDRESSES.arbitrum.WETH;
@@ -24,12 +21,6 @@ const abi = {
 async function borrowed(_time, _ethBlock, chainBlocks, { api }) {
     let balances = {};
 
-    // V3
-    const v3TokenState = await api.call({ abi: abi.v3.getTokenState, target: v3Address, })
-
-    await sdk.util.sumSingleBalance(balances, WETH_CONTRACT, v3TokenState[0]['totalNormalBorrowed'], api.chain);
-    await sdk.util.sumSingleBalance(balances, USDC_CONTRACT, v3TokenState[1]['totalNormalBorrowed'], api.chain);
-
     // V3.2
     const v320USDCState = await api.call({ abi: abi.v320.getAsset, target: v320Address, params: 1})
     const v320ETHState = await api.call({ abi: abi.v320.getAsset, target: v320Address, params: 2 })
@@ -47,7 +38,7 @@ async function borrowed(_time, _ethBlock, chainBlocks, { api }) {
 module.exports = {
     methodology: "USDC and WETH locked on predy contracts",
     arbitrum: {
-        tvl: sumTokensExport({ owners: [v202Address, v2Address, v3Address, v320Address], tokens: [USDC_CONTRACT, WETH_CONTRACT,] }),
+        tvl: sumTokensExport({ owners: [v320Address], tokens: [USDC_CONTRACT, WETH_CONTRACT,] }),
         borrowed
     },
     hallmarks: [
