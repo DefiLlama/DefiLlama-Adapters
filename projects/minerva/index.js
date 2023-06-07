966c90d9ab0b24f5b34571beeaae0392413b9dff
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-07 17:59:31 +0200

    code refactor

--- projects/minerva/index.js
+++ projects/minerva/index.js
@@ -1,13 +1,9 @@
 const {staking} = require('../helper/staking')
 const { gmxExports } = require('../helper/gmx')
 
-const arbitrumVault = '0x7EF6f8abAc00689e057C9ec14E34aC232255a2fb';
-const arbitrumStaking = '0x06177a05704C8156f8b3ae9391365497C432260A';
-const arbitrumGMX = '0xE4d8701C69b3B94A620ff048e4226C895b67b2c0';
-
 module.exports = {
-  fantom: {
-    staking: staking(arbitrumStaking, arbitrumGMX),
-    tvl: gmxExports({ vault: arbitrumVault, })
+  optimism: {
+    staking: staking('0x21563764f5641ffcb89f25560644e39947b21be0', '0xE4d8701C69b3B94A620ff048e4226C895b67b2c0'),
+    tvl: gmxExports({ vault: '0x7EF6f8abAc00689e057C9ec14E34aC232255a2fb', })
   }
 }
