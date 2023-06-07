3c1c836a7682a5472911f406097754a4ad602013
Alex Rubio <alejandro@flykube.com>
2023-06-07 09:29:21 +0200

    compute cardano tvl

--- projects/kubecoin/index.js
+++ projects/kubecoin/index.js
@@ -1,14 +1,14 @@
 const { toUSDTBalances } = require("../helper/balances");
 const { get } = require("../helper/http");
 
-async function staking() {
+async function value() {
   const url = "https://api.kubepay.io/kubecoin/staking-usd/";
   return toUSDTBalances(await get(url));
 }
 
 module.exports = {
   cardano: {
-    tvl: () => ({}),
-    staking,
+    tvl: value,
+    staking: value,
   },
 };
