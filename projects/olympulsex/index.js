c2b8e21acec6ab7a06b86b5e80782cacc63eca02
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-08 10:18:20 +0200

    code refactor

--- projects/olympulsex/index.js
+++ projects/olympulsex/index.js
@@ -1,22 +1,12 @@
-const { ohmTvl } = require("../helper/ohm");
-const ADDRESSES = require("../helper/coreAssets.json");
+const { sumTokensExport } = require('../helper/unknownTokens')
 
 const stakingToken = "0xBB366A397D7d4d2BEDABD9139D4c32a8826605Ed"; // SEX
 const staking = "0xf1Bc988e7EaBA7a2dbF0121E6ad9BEA82A1AB1ff";
-const treasury = "0x767d028d6d49ac86Aba52d23746c6dC5285C4852";
-const treasuryTokens = [
-  // DAI from Ethereum
-  [ADDRESSES.pulse.DAI, false],
-  // DAI-SEX PulseX LP V1
-  ["0x2d593b3472d6a5439bC1523a04C2aec314CBc44c", true],
-];
-module.exports = ohmTvl(
-  treasury,
-  treasuryTokens,
-  "pulse",
-  staking,
-  stakingToken,
-  (addr) => `pulse:${addr}`,
-  false,
-  false
-);
+const LP = '0x2d593b3472d6a5439bC1523a04C2aec314CBc44c'
+
+module.exports = {
+  pulse: {
+    tvl: () => 0,
+    staking: sumTokensExport({ owner: staking, tokens: [stakingToken], lps: [LP], useDefaultCoreAssets: true, }),
+  }
+}
--- projects/treasury/olympulsex.js
+++ projects/treasury/olympulsex.js
@@ -1,4 +1,11 @@
-const index = require('../hector/index')
-const { ohmTreasury } = require('../helper/treasury')
+const ADDRESSES = require("../helper/coreAssets.json");
+const { sumTokensExport } = require('../helper/unknownTokens')
 
-module.exports = ohmTreasury(index)
+const treasury = "0x767d028d6d49ac86Aba52d23746c6dC5285C4852";
+const LP = '0x2d593b3472d6a5439bC1523a04C2aec314CBc44c'
+
+module.exports = {
+  pulse: {
+    tvl: sumTokensExport({ owner: treasury, tokens: [ADDRESSES.pulse.DAI, LP], useDefaultCoreAssets: true, }),
+  }
+}
