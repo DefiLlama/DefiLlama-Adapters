406c21b85ca9ebfb207a8e486067afb0d2e625b7
Denis Ignatovich <denis.ignatovich.m@gmail.com>
2023-06-08 23:33:37 +0200

    Add hallmarks for Toros

--- projects/toros/index.js
+++ projects/toros/index.js
@@ -1,25 +1,27 @@
-
 const { DHEDGE_FACTORY_ABI, TOROS_POOL_ABI } = require("./abis");
 const { CONFIG_DATA } = require("./config");
 
 async function tvl(_, _b, _cb, { api, chain }) {
-  const { dhedgeFactory, torosMultisigManager, } = CONFIG_DATA[chain];
+  const { dhedgeFactory, torosMultisigManager } = CONFIG_DATA[chain];
 
   const pools = await api.call({
     abi: DHEDGE_FACTORY_ABI,
     target: dhedgeFactory,
     params: [torosMultisigManager],
-  })
+  });
 
   const poolSummaries = await api.multiCall({
     abi: TOROS_POOL_ABI,
-    calls: pools
-  })
+    calls: pools,
+  });
 
-  const totalValue = poolSummaries.reduce((acc, i) => acc + +i.totalFundValue, 0)
+  const totalValue = poolSummaries.reduce(
+    (acc, i) => acc + +i.totalFundValue,
+    0
+  );
 
   return {
-    tether: totalValue/1e18
+    tether: totalValue / 1e18,
   };
 }
 
@@ -28,11 +30,12 @@ module.exports = {
   misrepresentedTokens: true,
   start: 1627776000, // Sunday, August 1, 2021 12:00:00 AM
   methodology:
-    "Aggregates total value of each Toros product both on Polygon and Optimism",
+    "Aggregates total value of each Toros vault both on Polygon and Optimism",
   polygon: {
-    tvl
+    tvl,
   },
   optimism: {
-    tvl
+    tvl,
   },
+  hallmarks: [[1674003600, "Optimism Incentives Start"]],
 };
