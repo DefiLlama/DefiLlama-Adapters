d8012693baf4a821b5c3c17a0e3ccba07cf45be3
eZKalibur <130600962+eZKalibur@users.noreply.github.com>
2023-06-09 15:55:48 +0200

    ezkalibur (#6523)
    * ezkalibur
    
    * minor fix
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ projects/ezkalibur/index.js
@@ -0,0 +1,9 @@
+const { getUniTVL } = require("../helper/unknownTokens");
+
+module.exports = {
+  misrepresentedTokens: true,
+  start: 1686309181,
+  era: {
+    tvl: getUniTVL({ factory: '0x15C664A62086c06D43E75BB3fddED93008B8cE63', useDefaultCoreAssets: true,fetchBalances: true, }),
+  },
+};
