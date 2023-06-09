c101ed15acbc03deef611b83afcbedf4f584dcb3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-09 15:55:08 +0200

    minor fix

--- projects/ezkalibur/index.js
+++ projects/ezkalibur/index.js
@@ -4,6 +4,6 @@ module.exports = {
   misrepresentedTokens: true,
   start: 1686309181,
   era: {
-    tvl: getUniTVL({ factory: '0x15C664A62086c06D43E75BB3fddED93008B8cE63', useDefaultCoreAssets: true,}),
+    tvl: getUniTVL({ factory: '0x15C664A62086c06D43E75BB3fddED93008B8cE63', useDefaultCoreAssets: true,fetchBalances: true, }),
   },
 };
