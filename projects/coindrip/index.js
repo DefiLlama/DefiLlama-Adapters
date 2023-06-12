ce2516b6cf5b8b4566c25c68acd42678d107b74f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-12 17:21:35 +0200

    code refactor

--- projects/coindrip/index.js
+++ projects/coindrip/index.js
@@ -1,5 +1,6 @@
 const { toUSDTBalances } = require("../helper/balances");
 const { get } = require("../helper/http");
+const { sumTokensExport } = require('../helper/sumTokens')
 
 const API_URL = "https://app.coindrip.finance/api/tvl";
 
@@ -11,6 +12,6 @@ async function tvl() {
 module.exports = {
   timetravel: false,
   elrond: {
-    tvl,
+    tvl: sumTokensExport({ owner: 'erd1qqqqqqqqqqqqqpgqqnm3x37972323nuv3l3kywev0n8q5n6gyc8qwljqz9', })
   },
 };
