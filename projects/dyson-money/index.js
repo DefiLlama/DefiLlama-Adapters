8195039da3b4ac72c16cacac2746bcc96a731917
Avedith <avedith@avedith.dev>
2023-06-13 01:24:21 +1000

    refactor dyson & add avax chain (#6531)

--- projects/dyson-money/index.js
+++ projects/dyson-money/index.js
@@ -1,19 +1,8 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { staking } = require('../helper/staking')
 const { sumTokens2 } = require('../helper/unwrapLPs');
-const sdk = require('@defillama/sdk');
-const { get } = require('../helper/http')
 const utils = require('../helper/utils');
 const { toUSDTBalances } = require('../helper/balances');
-
-//contract the deposits into official GNS Staking Contract
-const gnsDysonVault = "0x035001DdC2f6DcF2006565Af31709f8613a7D70C"
-
-//grab GNS price from DefiLlama Price API
-const getGNSPrice = async () => {
-  const response = await get('https://coins.llama.fi/prices/current/polygon:0xE5417Af564e4bFDA1c483642db72007871397896')
-  return response.coins['polygon:0xE5417Af564e4bFDA1c483642db72007871397896'].price
-}
 
 const sphere_token = "0x62F594339830b90AE4C084aE7D223fFAFd9658A7"
 const ylSPHEREvault = "0x4Af613f297ab00361D516454E5E46bc895889653"
@@ -25,17 +14,17 @@ async function polygonTvl(timestamp, block, chainBlocks) {
   await sumTokens2({
     balances,
     owners: [ylSPHEREvault],
-    tokens: ["0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", ADDRESSES.polygon.USDC, ADDRESSES.polygon.WETH_1, ADDRESSES.polygon.USDT, "0x4Af613f297ab00361D516454E5E46bc895889653", ADDRESSES.polygon.WBTC],
+    tokens: [
+      ADDRESSES.polygon.WMATIC_2,
+      ADDRESSES.polygon.USDC,
+      ADDRESSES.polygon.WETH_1,
+      ADDRESSES.polygon.USDT,
+      ADDRESSES.polygon.WBTC,
+      "0x172370d5Cd63279eFa6d502DAB29171933a610AF"
+    ],
     chain: 'polygon',
     block: chainBlocks.polygon
   })
-
-  const gnsDysonVaultSupply = (await sdk.api.erc20.totalSupply({
-    target: gnsDysonVault,
-    chain: 'polygon',
-    block: chainBlocks.polygon
-  })).output / 1e18  * ((await getGNSPrice()) * 1e18) / 6.24 //Don't even ask why 6.24 is needed, it just is the only way to get the correct TVL
-  balances["polygon:0xE5417Af564e4bFDA1c483642db72007871397896"] = gnsDysonVaultSupply
 
   // calculate TVL for polygon from API
   const dysonTvl = await fetchChain(137)()
@@ -66,12 +55,6 @@ function fetchChain(chainId) {
   }
 }
 
-const chains = {
-  optimism: 10,
-  polygon: 137,
-  arbitrum: 42161,
-}
-
 module.exports = {
   doublecounted: true,
   misrepresentedTokens: false,
@@ -89,4 +72,7 @@ module.exports = {
   bsc: {
     tvl: fetchChain(56)
   },
+  avax: {
+    tvl: fetchChain(43114)
+  }
 };
