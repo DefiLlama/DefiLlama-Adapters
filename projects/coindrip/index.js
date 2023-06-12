4fb5e63c679d5f78f2339910c7288a8486ff77df
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-12 20:53:04 +0530

    Coindrip (#6535)
    * add coindrip tvl
    
    * remove misrepresentedTokens
    
    * code refactor
    
    ---------
    
    Co-authored-by: Razvan Statescu <razvan@statescu.net>

---
+++ projects/coindrip/index.js
@@ -0,0 +1,17 @@
+const { toUSDTBalances } = require("../helper/balances");
+const { get } = require("../helper/http");
+const { sumTokensExport } = require('../helper/sumTokens')
+
+const API_URL = "https://app.coindrip.finance/api/tvl";
+
+async function tvl() {
+  const data = await get(API_URL);
+  return toUSDTBalances(data);
+}
+
+module.exports = {
+  timetravel: false,
+  elrond: {
+    tvl: sumTokensExport({ owner: 'erd1qqqqqqqqqqqqqpgqqnm3x37972323nuv3l3kywev0n8q5n6gyc8qwljqz9', })
+  },
+};
