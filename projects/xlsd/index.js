bf7432b0fce67aa727f9513ba40edf05f8b1e289
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-14 15:31:58 +0200

    minor refactor

--- projects/xlsd/index.js
+++ projects/xlsd/index.js
@@ -1,18 +1,12 @@
-const { sumTokens2 } = require("../helper/unwrapLPs");
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
-const BLACKLISTED_TOKENS = [];
 const LSD_TOKENS = ['0x22aB6bC52CAD84AAe84bD5781258677a6E0cFaB0', '0xce3a75572bc375476438206f6F29f46F8466EcC2', '0xC56dC613C8EF3C57314171d73EAEFE87Aa469186'];
 const STAKING_POOL_ADDRESS = ['0x675B74A939fF8b16bA90Af9Cc9e6981a7975401E', '0x7931bfCeeD6FC04D35de75608F2977F3d3a69bc5', '0x77b16981d200dFe89Dd3AD5dc3238967646DDB28'];
 
-
-async function calcTvl(timestamp, block, chainBlocks, { api }) {
-  return sumTokens2({ api, tokensAndOwners2: [LSD_TOKENS, STAKING_POOL_ADDRESS], chain: 'arbitrum', blacklistedTokens: BLACKLISTED_TOKENS});
-}
-
 module.exports = {
+  methodology: "TVL of Staked ETH & LSD tokens in the StakingPool contracts",
   arbitrum: {
-    methodology:
-      "TVL of Staked ETH & LSD tokens in the StakingPool contracts",
-    tvl: calcTvl
+    tvl: () => ({}),
+    staking: sumTokensExport({ tokensAndOwners2: [LSD_TOKENS, STAKING_POOL_ADDRESS], }),
   },
 };
