09baabea1d98a23d57c50e2a22c07bad1e678870
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-14 23:19:02 +0200

    update eigenlayer

--- projects/eigenlayer/index.js
+++ projects/eigenlayer/index.js
@@ -1,19 +1,34 @@
-const { sumTokensExport } = require("../helper/unwrapLPs")
+const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+const { getLogs } = require('../helper/cache/getLogs')
 
-// eigen pods are paused for now but theyre missing
+async function tvl(_, _b, _cb, { api, }) {
+  const logs = await getLogs({
+    api,
+    target: '0x858646372CC42E1A627fcE94aa7A7033e7CF075A',
+    topic: 'StrategyAddedToDepositWhitelist(address)',
+    eventAbi: "event StrategyAddedToDepositWhitelist(address strategy)",
+    onlyArgs: true,
+    fromBlock: 17445564,
+  })
+  const podLogs = await getLogs({
+    api,
+    target: '0x91E677b07F7AF907ec9a428aafA9fc14a0d3A338',
+    topics: ['0x21c99d0db02213c32fff5b05cf0a718ab5f858802b91498f80d82270289d856a'],
+    eventAbi: "event PodDeployed (address indexed eigenPod, address indexed podOwner)",
+    onlyArgs: true,
+    fromBlock: 17445564,
+  })
+  const strategies = logs.map(log => log.strategy)
+  const pods = podLogs.map(log => log.eigenPod)
+  const restakedStatus = await api.multiCall({ abi: 'bool:hasRestaked', calls: pods })
+  restakedStatus.filter(i => i).forEach(() => api.add(nullAddress, 32 * 1e18))
+  const tokens = await api.multiCall({ abi: 'address:underlyingToken', calls: strategies })
+  return sumTokens2({ api, tokensAndOwners2: [tokens, strategies] })
+}
+
+// https://github.com/Layr-Labs/eigenlayer-contracts/blob/master/script/output/M1_deployment_mainnet_2023_6_9.json
 module.exports = {
   ethereum: {
-    tvl: sumTokensExport({
-      owners: [
-        '0x93c4b944d05dfe6df7645a86cd2206016c51564d', //steth
-        '0x1bee69b7dfffa4e2d53c2a2df135c388ad25dcd2', //reth
-        '0x54945180db7943c0ed0fee7edab2bd24620256bc', //cbeth
-      ],
-      tokens: [
-        '0xae7ab96520de3a18e5e111b5eaab095312d7fe84',
-        '0xae78736cd615f374d3085123a210448e74fc6393',
-        '0xbe9895146f7af43049ca1c1ae358b0541ea49704',
-      ]
-    }),
+    tvl,
   },
 }
