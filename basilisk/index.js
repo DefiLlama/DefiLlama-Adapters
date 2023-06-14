a36d6917197078c4e8c59dc09c46316c16a98357
basilisk-protocol <130878946+basilisk-protocol@users.noreply.github.com>
2023-06-14 19:01:32 -0300

    Add files via upload

---
+++ basilisk/index.js
@@ -0,0 +1,8 @@
+const { compoundExports2 } = require('../helper/compound');
+
+module.exports = {
+  era: compoundExports2({
+    comptroller: '0x4085f99720e699106bc483dAb6CAED171EdA8D15',
+    cether: '0x1e8F1099a3fe6D2c1A960528394F4fEB8f8A288D',
+  })
+}; 
