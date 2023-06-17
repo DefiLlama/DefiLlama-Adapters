d2b378060770da221851daad680671bc27f30696
realdealshaman <realdealshaman11@gmail.com>
2023-06-17 05:53:21 +0100

    add back 1inch

---
+++ projects/1inch.js
@@ -0,0 +1,27 @@
+const { sumTokens2, } = require('./helper/unwrapLPs')
+const { getLogs } = require('./helper/cache/getLogs')
+
+const config = require("./1inch/config");
+
+module.exports = {}
+
+Object.keys(config).forEach(chain => {
+  const { blacklistedTokens = [], factories } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _2, { api }) => {
+      const ownerTokens = []
+      for (const { MooniswapFactory, fromBlock} of factories) {
+        const logs = await getLogs({
+          api,
+          target: MooniswapFactory,
+          topic: 'Deployed(address,address,address)',
+          eventAbi: 'event Deployed(address indexed mooniswap, address indexed token1, address indexed token2)',
+          onlyArgs: true,
+          fromBlock,
+        })
+        logs.forEach(i => ownerTokens.push([[i.token1, i.token2], i.mooniswap]))
+      }
+      return sumTokens2({ api, ownerTokens, blacklistedTokens, })
+    }
+  }
+})
