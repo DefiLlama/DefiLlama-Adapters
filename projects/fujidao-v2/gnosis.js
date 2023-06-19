05a2e394e27ada215a013db43b2c60b09037ba2d
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-06-19 10:22:38 +0100

    refactor(fujidao-v2): add debt and move supply function to gno file

--- projects/fujidao-v2/gnosis.js
+++ projects/fujidao-v2/gnosis.js
@@ -1,11 +1,43 @@
-// The provided address should be the all the BorrowingVaults and YieldVault contracts that returns
-// totalAssets() for the indicated asset.
-const gnosisContracts = {
+const {
+  marketSupply,
+  marketDebt,
+  WETH,
+  USDC
+} = require('./utils');
+
+// Collateral accounting by asset in all BorrowingVaults and YieldVault contracts
+// using totalAssets() method.
+const gnosisVaultContracts = {
   weth: [
-    "0x1f9137C0007341A78b83097027CE99cF540BD0E0",
+    "0x1f9137C0007341A78b83097027CE99cF540BD0E0", // WETH-USDC-1
   ],
 };
 
+// Debt accounting by debt asset in all BorrowingVaults contracts
+// using totalDebt() method.
+const gnosisContractsDebt = {
+  usdc: [
+    "0x1f9137C0007341A78b83097027CE99cF540BD0E0",
+  ]
+}
+
+async function gnosisSupply(_timestamp, ethBlock, chainBlocks){
+  const wethSupplies = await marketSupply(gnosisVaultContracts.weth, chainBlocks.xdai, "xdai");
+
+  return {
+      [WETH]: wethSupplies,
+  }
+}
+
+async function gnosisDebt(_timestamp, ethBlock, chainBlocks){
+  const usdcDebt = await marketDebt(gnosisContractsDebt.weth, chainBlocks.xdai, "xdai");
+
+  return {
+      [USDC]: usdcDebt,
+  }
+}
+
 module.exports = {
-  gnosisContracts,
+  gnosisSupply,
+  gnosisDebt
 };
