30105274dc2f019edcfd05e73679930d4f1ebe22
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-06-19 10:22:14 +0100

    refactor(fujidao-v2): simplify index file

--- projects/fujidao-v2/index.js
+++ projects/fujidao-v2/index.js
@@ -1,61 +1,23 @@
-const ADDRESSES = require('../helper/coreAssets.json');
-const abi = require('./abi.json');
-const sdk = require('@defillama/sdk');
-const { default: BigNumber } = require('bignumber.js');
-const {polygonContracts} = require('./polygon');
-const {arbitrumContracts} = require('./arbitrum');
-const {optimismContracts} = require('./optimism');
-const {gnosisContracts} = requre('./gnosis');
-
-
-const marketSupply = async (contracts, block, chain) => {
-    const supply = await sdk.api.abi.multiCall(
-        {
-            abi: abi.totalAssets,
-            calls: (contracts).map( vault => ({
-                target: vault,
-                params: []
-            })),
-            block,
-            chain
-        }
-    );
-    return supply.output.reduce((t,v) => t.plus(v.output), BigNumber(0)).toFixed(0);
-}
-
-async function polygon(_timestamp, ethBlock, chainBlocks){
-    const wethSupplies = await marketSupply(polygonContracts.weth, chainBlocks.polygon, "polygon");
-
-    return {
-        [ADDRESSES.polygon.WETH_1]: wethSupplies,
-        [ADDRESSES.polygon.MATICX]: usdcSupplies,
-    }
-}
-
-// async function arbitrum(_timestamp, ethBlock, chainBlocks){
-//     const wethSupplies = await allMarketSupplies(arbitrumContracts.weth, chainBlocks.arbirtum, "arbitrum");
-//     const usdcSupplies = await allMarketSupplies(arbitrumContracts.usdc, chainBlocks.arbirtum, "arbitrum");
-
-//     return {
-//         [weth]: wethSupplies,
-//         [usdc]: usdcSupplies,
-//     }
-// }
-
-// async function optimism(_timestamp, ethBlock, chainBlocks){
-//     const wethSupplies = await allMarketSupplies(optimismContracts.weth, chainBlocks.optimism, "optimism");
-//     const usdcSupplies = await allMarketSupplies(optimismContracts.usdc, chainBlocks.optimism, "optimism");
-//     return {
-//         [weth]: wethSupplies,
-//         [usdc]: usdcSupplies,
-//     }
-// }
+const {polygonSupply, polygonDebt} = require('./polygon');
+const {arbitrumSupply, arbitrumDebt} = require('./arbitrum');
+const {optimismSupply, optimismDebt} = require('./optimism');
+// const {gnosisSupply, gnosisDebt} = require('./gnosis');
 
 module.exports = {
-    timetravel: false,
-    misrepresentedTokens: false,
-    methodology: "Counts on-chain balance of assets deposits in all vaults.",
+    methodology: "Counts on-chain balance of underlying asset deposited and borrowed debt for all Fuji V2 vaults.",
     polygon: {
-        tvl: polygon
-    }
+        tvl: polygonSupply,
+        borrowed: polygonDebt
+    },
+    arbitrum: {
+        tvl: arbitrumSupply,
+        borrowed: arbitrumDebt
+    },
+    optimism: {
+        tvl: optimismSupply,
+        borrowed:  optimismDebt
+    },
+    // xdai: {
+    //     tvl: gnosisSupply
+    // }
 }
