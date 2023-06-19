e48278df2dbef49723bf5870f4b33fb2d4b16d33
Kyli Bing <kylidboy@hashmix.org>
2023-06-19 17:31:07 +0800

    feat: change tvl cal

--- projects/hashmix/index.js
+++ projects/hashmix/index.js
@@ -1,28 +1,29 @@
 const sdk = require("@defillama/sdk");
-// const {get} = require("../helper/http");
+const { get } = require("../helper/http");
 const BigNumber = require('bignumber.js');
 const POOL = "0x587A7eaE9b461ad724391Aa7195210e0547eD11d";
 
 async function tvl(_, _1, _2, { api }) {
 
-  // let price = await get("https://api.coingecko.com/api/v3/simple/price?ids=filecoin&vs_currencies=usd");
-  // let priceFloat = price.filecion.usd;
-  
-  const balances = {};
-  const bal = await sdk.api2.eth.getBalance({target: POOL, chain: api.chain, decimals: api.decimals});
-  const totalBorrows = await sdk.api2.abi.call({
-    target: POOL,
-    abi: "uint256:totalBorrows",
-    chain: api.chain,
-  });
-  const totalReserves = await sdk.api2.abi.call({
-    target: POOL,
-    abi: "uint256:totalReserves",
-    chain: api.chain,
-  });
+  let tvl = await get("https://fvm.hashmix.org/fevmapi/tvl");
+  let b = new BigNumber.BigNumber(tvl.data, 10);
 
-  let b = new BigNumber.BigNumber(bal.output);
-  b = b.plus(new BigNumber.BigNumber(totalBorrows)).minus(new BigNumber.BigNumber(totalReserves));
+  const balances = {};
+  // const bal = await sdk.api2.eth.getBalance({ target: POOL, chain: api.chain, decimals: api.decimals });
+  // const totalBorrows = await sdk.api2.abi.call({
+  //   target: POOL,
+  //   abi: "uint256:totalBorrows",
+  //   chain: api.chain,
+  // });
+  // const totalReserves = await sdk.api2.abi.call({
+  //   target: POOL,
+  //   abi: "uint256:totalReserves",
+  //   chain: api.chain,
+  // });
+
+
+  // let b = new BigNumber.BigNumber(bal.output);
+  // b = b.plus(new BigNumber.BigNumber(totalBorrows)).minus(new BigNumber.BigNumber(totalReserves));
 
   sdk.util.sumSingleBalance(
     balances,
