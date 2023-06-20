7f87cb73b8caf6bf6b1a4a3e7d296e481f23d15d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-20 08:58:16 +0200

    minor fix

--- projects/gmd/index.js
+++ projects/gmd/index.js
@@ -5,7 +5,7 @@ const abi = 'uint256:totalUSDvaults'
 const vault = "0xA7Ce4434A29549864a46fcE8662fD671c06BA49a";
 const vault2 = "0x8080B5cE6dfb49a6B86370d6982B3e2A86FBBb08";
 const stakingAdd = "0x48c81451d1fddeca84b47ff86f91708fa5c32e93";
-// const EsGMDAddress = "0x49E050dF648E9477c7545fE1779B940f879B787A";
+const EsGMDAddress = "0x49E050dF648E9477c7545fE1779B940f879B787A";
 const GMDaddress = "0x4945970EfeEc98D393b4b979b9bE265A3aE28A8B";
 
 const Tvl = async (ts, _, { arbitrum: block }) => {
@@ -22,7 +22,7 @@ module.exports = {
   misrepresentedTokens: true,
   methodology: "staked gmd + vault balance",
   arbitrum: {
-    staking: staking(stakingAdd, GMDaddress, "arbitrum"),
+    staking: staking(stakingAdd, [EsGMDAddress, GMDaddress]),
     tvl: Tvl,
   },
 };
