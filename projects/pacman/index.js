b39e90afc8c26755520b6f1ac1275c9be7e6369d
PacmanFinance <pacmanfinance7@gmail.com>
2023-06-21 15:44:05 +0800

    add tvl

--- projects/pacman/index.js
+++ projects/pacman/index.js
@@ -1,9 +1,48 @@
-const { getUniTVL } = require("../helper/unknownTokens");
+const { request, gql } = require('graphql-request');
+const BigNumber = require("bignumber.js");
+
+const subgraphUrl = "https://api.thegraph.com/subgraphs/name/pacmanfinance/pacman-arbitrum";
+
+const vaultsQuery = gql`
+  query {
+    vaults {
+      baseToken {
+        id
+        priceByUsd
+      }
+      id
+      availableAmount
+    }
+  }
+`;
+
+async function tvl(chain) {
+  const balances = {};
+  const vaultsInfo = await request(subgraphUrl, vaultsQuery);
+
+  for (let i = 0; i < vaultsInfo["vaults"].length; i++) {
+    var token = vaultsInfo["vaults"][i]["baseToken"]
+    
+    balances[`${chain}:${token["id"]}`] = BigNumber(
+      balances[`${chain}:${token["id"]}`] || 0
+      )
+      .plus(
+        BigNumber(vaultsInfo["vaults"][i]["availableAmount"])
+        .multipliedBy(
+        BigNumber(token["priceByUsd"])
+        )
+      ).toFixed(0)
+    ;
+  }
+  return balances;
+}
 
 module.exports = {
-  misrepresentedTokens: true,
-  start: 1669075200,
+  // timetravel: true,
+  // misrepresentedTokens: false,
+  // methodology: 'get tvl',
+  //start: 91889820,
   arbitrum: {
-    tvl: getUniTVL({ factory: '0x6EcCab422D763aC031210895C81787E87B43A652', useDefaultCoreAssets: true,}),
-  },
-};
+    tvl,
+  }
+}; 
