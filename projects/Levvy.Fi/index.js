a104f86dd18767eb2b627601dfdad623d548ecdf
theapesociety <98607857+theapesociety@users.noreply.github.com>
2023-06-24 21:39:38 -0700

    Update index.js

--- projects/Levvy.Fi/index.js
+++ projects/Levvy.Fi/index.js
@@ -4,7 +4,7 @@ module.exports = {
   timetravel: false,
   cardano: {
     tvl: async () => {
-      const data = await post('https://citizens.theapesociety.io/api/getLevvyData')
+      const data = await post('https://citizens.theapesociety.io/api/getLevvyData', {})
       return {
         cardano: data.cardano
       }
