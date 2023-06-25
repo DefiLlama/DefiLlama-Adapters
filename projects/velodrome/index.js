2280b78e79c189997bd1a6a8268bf71eab884cd3
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-25 14:43:36 -0400

    test velo v2 update

--- projects/velodrome/index.js
+++ projects/velodrome/index.js
@@ -6,10 +6,11 @@ module.exports = {
   timetravel: true,
   incentivized: true,
   optimism: {
-    tvl: uniTvlExport("0x25CbdDb98b35ab1FF77413456B31EC81A6B6B746", "optimism"),
+    tvl: uniTvlExport(["0x25CbdDb98b35ab1FF77413456B31EC81A6B6B746","0xF1046053aa5682b4F9a81b5481394DA16BE5FF5a"], "optimism"),
   },
   hallmarks:[
     [1657760400, "First OP grant awarded"],
     [1659618000, "Loss $350k Operational Funds"],
+    [1687465883, "v2 Migration on OP Mainnet"],
   ]
 }
