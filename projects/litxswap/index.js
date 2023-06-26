16ac8dea05d466db1019e933fdecf3051ba20bf2
moonbrownie <litx@netmail.dk>
2023-06-26 22:04:42 +0530

    Update index.js

--- projects/litxswap/index.js
+++ projects/litxswap/index.js
@@ -1,12 +1,14 @@
-const { getUniTVL } = require("../helper/unknownTokens");
-module.exports = {
-  misrepresentedTokens: true,
-  methodology:
-    "Factory address (0x24398b6ea5434339934D999E431807B1C157b4Fd) is used to find the LP pairs. TVL is equal to the liquidity on the AMM. Staking balance is equal to the balance of $EAZY in $xEAZY contract",
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
   pulse: {
-    tvl: getUniTVL({
-      factory: "0x24398b6ea5434339934D999E431807B1C157b4Fd",
-      useDefaultCoreAssets: true,
-    }),
+    factory: "0x24398b6ea5434339934D999E431807B1C157b4Fd",
+    fromBlock: 17449439,
+    isAlgebra: true,
   },
-};
+  bsc: {
+    factory: "0xaA37Bea711D585478E1c04b04707cCb0f10D762a",
+    fromBlock: 28719189,
+    isAlgebra: true,
+  },
+});
