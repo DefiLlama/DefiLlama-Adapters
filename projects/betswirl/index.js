2541d26fa9c8bdfcb230f988926ecdf09bfaeae7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-26 23:38:56 +0530

    Betswirl (#6650)
    * feat(betswirl): Add Staking contracts TVLs
    
    * minor fix
    
    ---------
    
    Co-authored-by: Romuald <romuald@betswirl.com>

--- projects/betswirl/index.js
+++ projects/betswirl/index.js
@@ -1,10 +1,19 @@
+const { staking } = require('../helper/staking')
 
 module.exports = {
-  methodology:
-    "BetSwirl has no users TVL yet. However, it includes the bankrolls amounts (each tokens amount in the bank allowing players to bet).",
-  // The first Bank was deployed on Polygon at tx 0x6b99f617946d2f8c23adcd440cd3309d2da750e52d135853f38a0da11cdc3233
-  start: 1648344312, // new Date(Date.UTC(2022, 2, 27, 1, 25, 12)).getTime() / 1e3,
+  methodology: "TVL counts BETS tokens deposited on the Staking contracts.",
+  start: 1687715559,
   bsc: {
     tvl: () => ({}),
+    staking: staking('0xeb5F6571861EAA6de9F827519B48eFe979d4d913', '0x3e0a7C7dB7bB21bDA290A80c9811DE6d47781671'),
+  },
+  polygon: {
+    staking: staking('0xa184468972c71209BC31a5eF39b7321d2A839225', '0x9246a5f10a79a5a939b0c2a75a3ad196aafdb43b'),
+  },
+  avax: {
+    staking: staking('0x31EDcD915e695AdAF782c482b9816613b347AC8c', '0xc763f8570a48c4c00c80b76107cbe744dda67b79'),
+  },
+  arbitrum: {
+    staking: staking('0xD4BFB259D8785228e5D2c19115D5DB342E2eE064', '0xe26ae3d881f3d5def58d795f611753804e7a6b26'),
   },
 };
