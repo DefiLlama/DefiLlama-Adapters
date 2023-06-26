f4327306e8740e47dded19106de8cda962a4859e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-26 20:07:42 +0200

    minor fix

--- projects/betswirl/index.js
+++ projects/betswirl/index.js
@@ -4,15 +4,16 @@ module.exports = {
   methodology: "TVL counts BETS tokens deposited on the Staking contracts.",
   start: 1687715559,
   bsc: {
-    tvl: staking('0xeb5F6571861EAA6de9F827519B48eFe979d4d913', '0x3e0a7C7dB7bB21bDA290A80c9811DE6d47781671'),
+    tvl: () => ({}),
+    staking: staking('0xeb5F6571861EAA6de9F827519B48eFe979d4d913', '0x3e0a7C7dB7bB21bDA290A80c9811DE6d47781671'),
   },
   polygon: {
-    tvl: staking('0xa184468972c71209BC31a5eF39b7321d2A839225', '0x9246a5f10a79a5a939b0c2a75a3ad196aafdb43b'),
+    staking: staking('0xa184468972c71209BC31a5eF39b7321d2A839225', '0x9246a5f10a79a5a939b0c2a75a3ad196aafdb43b'),
   },
   avax: {
-    tvl: staking('0x31EDcD915e695AdAF782c482b9816613b347AC8c', '0xc763f8570a48c4c00c80b76107cbe744dda67b79'),
+    staking: staking('0x31EDcD915e695AdAF782c482b9816613b347AC8c', '0xc763f8570a48c4c00c80b76107cbe744dda67b79'),
   },
   arbitrum: {
-    tvl: staking('0xD4BFB259D8785228e5D2c19115D5DB342E2eE064', '0xe26ae3d881f3d5def58d795f611753804e7a6b26'),
+    staking: staking('0xD4BFB259D8785228e5D2c19115D5DB342E2eE064', '0xe26ae3d881f3d5def58d795f611753804e7a6b26'),
   },
 };
