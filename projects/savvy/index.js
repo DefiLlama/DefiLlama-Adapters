ea3c8a55c98ce19466fc1e12a31f25a8883a3529
Akshay Gupta <akshaygupta54321@gmail.com>
2023-06-27 15:20:36 -0400

    tvl test passed

--- projects/savvy/index.js
+++ projects/savvy/index.js
@@ -1,48 +1,16 @@
-const sdk = require('@defillama/sdk');
-const abi = require("./abi.json");
 const contracts = require("./contracts.json");
+const { sumTokens2 } = require("../helper/unwrapLPs");
 
-async function tvl(_, _1, _2, { api }) {
-  let balances = {};
-  let balance = (await sdk.api.abi.call({
-      abi: abi.totalValueLocked,
-      chain: "arbitrum",
-      target: contracts.infoAggregator
-    })
-  ).output;
-  await sdk.util.sumSingleBalance(balances, `0x43aB8f7d2A8Dd4102cCEA6b438F6d747b1B9F034`, balance, api.chain);
-  console.log("tvlVal", balances);
-  return balances;
-}
-
-async function staking() {
-  let balances = {};
-  let balance  = (await sdk.api.abi.call({
-      abi: abi.totalStaked,
-      chain: "arbitrum",
-      target: contracts.infoAggregator
-    })
-  ).output;
-  sdk.util.sumSingleBalance(balances, `arbitrum:${contracts.infoAggregator}`, balance);
-  console.log("stakingVal", balances);
-  return balances;
-}
-
-async function borrowed() {
-  const borrowedVal = (await sdk.api.abi.call({
-      abi: abi.totalDebt,
-      target: contracts.infoAggregator
-    })
-  ).output;
-  console.log("borrowedVal", borrowedVal);
-  return Math.min(borrowedVal, 0);
+function tvl(chain) {
+  return async (timestamp, block, chainBlocks, { api }) => {
+    const tokens = Object.values(contracts.savvyTokens).concat(Object.values(contracts.baseTokens)).concat(Object.values(contracts.yieldTokens));
+    await sumTokens2({ tokens, api, owners: Object.values(contracts.tokenHolders) })
+  };
 }
 
 module.exports = {
   methodology: 'The calculated TVL is the current USD sum of all user deposits and SVY tokens staked in veSVY.',
   arbitrum: {
-    tvl,
-    //staking,
-    //borrowed
+    tvl: tvl("arbitrum")
   }
 }
