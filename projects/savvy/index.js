d47ed2b82b47aab503df9e874d6622cd7d9d3aad
Akshay Gupta <akshaygupta54321@gmail.com>
2023-06-27 15:48:12 -0400

    address feedback

--- projects/savvy/index.js
+++ projects/savvy/index.js
@@ -3,7 +3,7 @@ const { sumTokens2 } = require("../helper/unwrapLPs");
 
 function tvl(chain) {
   return async (timestamp, block, chainBlocks, { api }) => {
-    const tokens = Object.values(contracts.savvyTokens).concat(Object.values(contracts.baseTokens)).concat(Object.values(contracts.yieldTokens));
+    const tokens = Object.values(contracts.baseTokens).concat(Object.values(contracts.yieldTokens));
     await sumTokens2({ tokens, api, owners: Object.values(contracts.tokenHolders) })
   };
 }
