235a1eb829719369730fd7d9fbbfc007ae06c161
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-27 10:12:49 +0200

    savvy: code refactor

--- projects/savvy/index.js
+++ projects/savvy/index.js
@@ -1,48 +1,15 @@
-const sdk = require('@defillama/sdk');
-const abi = require("./abi.json");
-const contracts = require("./contracts.json");
-
-async function tvl(_, _1, _2, { api }) {
-  let balances = {};
-  let balance = (await sdk.api.abi.call({
-      abi: abi.totalValueLocked,
-      chain: "arbitrum",
-      target: contracts.infoAggregator
-    })
-  ).output;
-  await sdk.util.sumSingleBalance(balances, `0x43aB8f7d2A8Dd4102cCEA6b438F6d747b1B9F034`, balance, api.chain);
-  console.log("tvlVal", balances);
-  return balances;
-}
-
-async function staking() {
-  let balances = {};
-  let balance  = (await sdk.api.abi.call({
-      abi: abi.totalStaked,
-      chain: "arbitrum",
-      target: contracts.infoAggregator
-    })
-  ).output;
-  sdk.util.sumSingleBalance(balances, `arbitrum:${contracts.infoAggregator}`, balance);
-  console.log("stakingVal", balances);
-  return balances;
-}
-
-async function borrowed() {
-  const borrowedVal = (await sdk.api.abi.call({
-      abi: abi.totalDebt,
-      target: contracts.infoAggregator
-    })
-  ).output;
-  console.log("borrowedVal", borrowedVal);
-  return Math.min(borrowedVal, 0);
-}
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
   methodology: 'The calculated TVL is the current USD sum of all user deposits and SVY tokens staked in veSVY.',
   arbitrum: {
-    tvl,
-    //staking,
-    //borrowed
+    tvl: sumTokensExport({
+      tokensAndOwners: [
+        ['0x078f358208685046a11c85e8ad32895ded33a249', '0xDD7e69c478288A9596DECc2230A39c7b922413Dd'], // aArbWBTC
+        ['0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8', '0xCaB3886D48bAd0F749ba7e7c7A255b636c64F937'], // aArbWETH
+        ['0x625e7708f30ca75bfd92586e17077590c60eb4cd', '0xCdFF8576683E22621f512fc2E6d347A9D241DAd3'], // aArbUSDC
+      ]
+    }),
+    staking: sumTokensExport({ owners: ['0x9aEEe4656F67034B06D99294062feBA1015430ad', '0xbf837f2C7894cfF859a01d078d7AAa8c47221a2F'], tokens: ['0x43ab8f7d2a8dd4102ccea6b438f6d747b1b9f034'] }),
   }
 }
