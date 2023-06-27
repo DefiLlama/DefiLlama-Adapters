758d4c2bd2a47eef4e8e95363088328ebdb7ddaa
Josh Weintraub <jhweintraub@gmail.com>
2023-06-27 15:02:07 -0400

    cleaned up and added comments and staking for tokenVaultV2

--- projects/revest/index.js
+++ projects/revest/index.js
@@ -1,12 +1,10 @@
 const { sumTokens2, } = require("../helper/unwrapLPs")
 const { covalentGetTokens, } = require("../helper/http")
 const { getUniqueAddresses } = require('../helper/utils')
-const axios = require('axios')
 const sdk = require("@defillama/sdk");
 
 const CutoffABI = "function FNFT_CUTOFF() external view returns (uint256)"
 const numFNFTSABI = "function fnftsCreated() external view returns (uint256)"
-const resonateFNFTSEndpoint = "https://app.resonate.finance/api/fnfts?chainId="
 const getFNFTSupplyABI = "function getSupply(uint256 fnftId) external view returns (uint256)"
 const getFNFTConfigABI = "function getFNFT(uint256 fnftId) external view returns (tuple(address asset,,,,,,,) memory)"
 const balanceOfABI = "function balanceOf(address account) view returns (uint256)"
@@ -38,15 +36,11 @@ const config = {
   optimism: {
     tokenVaultV2: '0x490867a64746AC33f721A778dD8C30BBb0074055',
     fnftHandler: '0xA002Dc3E3C163732F4F5e6F941C87b61B5Afca74',
-
-    //TODO: Fix Later
     holder: '0x490867a64746AC33f721A778dD8C30BBb0074055'
   },
   arbitrum: {
     tokenVaultV2: '0x209F3F7750d4CC52776e3e243717b3A8aDE413eB',
     fnftHandler: '0xd90D465631a1718FDB3eA64C39F41290Addf70da',
-
-    //TODO: Fix Later
     holder: '0x209F3F7750d4CC52776e3e243717b3A8aDE413eB'
   }
 }
@@ -71,9 +65,6 @@ Object.keys(config).forEach(chain => {
       let owners = []
 
       if (tokenVaultV2 != null) {
-        //console.log(`------${chain}---------`)
-
-        //console.log(`CALL SUCCCEEDED: ${chain}`)
 
         let info = await api.batchCall([
           {
@@ -91,29 +82,15 @@ Object.keys(config).forEach(chain => {
         let cutoff = Number(info[0])
         let numFNFTS = Number(info[1])
 
-        //console.log(`cutoff: ${cutoff}`)
-        //console.log(`Num FNFTS: ${numFNFTS}`)
-
-        //Get and filter out all the FNFTs from Resonate
-        let resonateFNFTS = {}
-        let resonateFNFTList = await axios.get(resonateFNFTSEndpoint + api.chainId);
-        //console.log(`resonateFNFTList: ${resonateFNFTList.data.length}`)
-        resonateFNFTList.data.forEach(fnft => {
-          resonateFNFTS[Number(fnft.fnftId)] = true
-        })
-
-        //Get all the FNFTS that aren't from resonate, so now we try to filter out burned ones
+        //Build the Multicall to get the supply of each FNFT to check for ones that still exist
         let fnftSupplyCalls = []
         for(let x = cutoff; x < numFNFTS; x++) {
-          if (resonateFNFTS[x]) continue;
           fnftSupplyCalls.push({
             target: fnftHandler,
             params: x,
           })
         }
 
-        // console.log(`GOT HERE`)
-        
         //Get the supply of each to determine if an FNFT is still alive by supply > 0
         let fnftSupplys = await sdk.api.abi.multiCall({
           calls: fnftSupplyCalls,
@@ -123,12 +100,10 @@ Object.keys(config).forEach(chain => {
           chain: chain
         })
 
-        //console.log(`AND ALSO HERE`)
-
+        //If the FNFT still exists, track it's ID
         let aliveFNFTCalls = []
         let aliveFNFTS = {}
         fnftSupplys.output.forEach(supply => {
-          //console.log(`FNFT Supply: ${supply.output}`)
           if (supply.output != 0) {
 
             //Track the supply of the FNFT
@@ -142,7 +117,6 @@ Object.keys(config).forEach(chain => {
         })
 
         //Get the asset of the FNFT
-        // console.log("alive FNFTS: " + aliveFNFTCalls.length)
         let fnftConfigs = await sdk.api.abi.multiCall({
           calls: aliveFNFTCalls,
           abi: getFNFTConfigABI,
@@ -155,9 +129,7 @@ Object.keys(config).forEach(chain => {
         //Link each FNFTId to its underlying token
         let tokenAddressesPerFNFT = {}
         let walletCalls = []
-        // console.log(`config 0: ${JSON.stringify(fnftConfigs.output[0])}`)
         fnftConfigs.output.forEach(config => {
-          // console.log(`token: ${config.output[0]}`)
           if (!blacklist.includes(config.output[0].toLowerCase())) {
             tokenAddressesPerFNFT[config.input.params[0]] = config.output[0]
           
@@ -169,6 +141,7 @@ Object.keys(config).forEach(chain => {
 
         })
 
+        //Get the wallet for each FNFT
         let wallets = await sdk.api.abi.multiCall({
           abi: getWalletABI,
           calls: walletCalls,
@@ -177,18 +150,14 @@ Object.keys(config).forEach(chain => {
           permitFailure: false,
         })
 
+        //Prepare Calldata to check the balance of each token in each wallet
         let balanceOfMultiCalls = []
         wallets.output.forEach(wallet => {
-          // console.log(`wallet: ${wallet.output}`)
-          // console.log(`token address: ${tokenAddressesPerFNFT[wallet.input.params[0]]}`)
           balanceOfMultiCalls.push({
             target: tokenAddressesPerFNFT[wallet.input.params[0]],
             params: wallet.output,
           })
         })
-
-        // console.log(`------------------------------------------`)
-        // console.log(`balance multicall info: ${JSON.stringify(balanceOfMultiCalls[0])}`)
 
         let multiCallBalances = await sdk.api.abi.multiCall({
           abi: balanceOfABI,
@@ -198,6 +167,7 @@ Object.keys(config).forEach(chain => {
           permitFailure: false,
         })
 
+        //Record balances, increasing stored amount if necessary
         multiCallBalances.output.forEach(balance => {
           if (balances[balance.input.target] == undefined) {
             balances[balance.input.target] = 0
@@ -209,37 +179,68 @@ Object.keys(config).forEach(chain => {
         
       }
 
+      //Get values in tokenVaultV1
       owners.push(holder);
-      
       let tokens = await covalentGetTokens(holder, api.chain)
-
       tokens = getUniqueAddresses(tokens).filter(t => !blacklist.includes(t)) // filter out staking and LP tokens
-
-      //console.log(`tokens: ${tokens.length}\n`)
-      //console.log(`owners: ${owners.length}\n`)
-
       let tokenVaultV1Balances = await sumTokens2({ api, owners: owners, tokens, })
-      //console.log(`tokenVaultV1Balances: ${JSON.stringify(tokenVaultV1Balances)}`)
 
       //For each key in V1 Balances append to regular balances and return
       Object.keys(balances).forEach(key => {
-        //console.log(`key: ${key}`)
         if (tokenVaultV1Balances[key] == undefined) {
           tokenVaultV1Balances[`${chain}:${key}`] = 0
         }
-
         tokenVaultV1Balances[`${chain}:${key}`] += balances[key]
       })
-
       return tokenVaultV1Balances;
     },
   }
 
   if (revest)
     module.exports[chain].staking = async (_, _b, _1, { api }) => {
-      //TODO: Get Wallets from FNFTHandler -> Is easy since only 1 token to track
+      //Get the number of FNFTS for the TokenVaultV2
+      let info = await api.batchCall([
+          {
+            target: config[chain].tokenVaultV2,
+            abi: CutoffABI,
+            chain: chain
+          },
+          {
+            target: config[chain].fnftHandler,
+            abi: numFNFTSABI,
+            chain: chain
+          }
+        ])
 
-      return sumTokens2({ api, owner: holder, tokens: [revest] })
+        let cutoff = Number(info[0])
+        let numFNFTS = Number(info[1])
+
+        //Build Multicall Data
+        let calls = []
+        for(let x = cutoff; x < numFNFTS; x++) {
+          calls.push({
+            target: config[chain].tokenVaultV2,
+            params: [x]
+          })
+        }
+
+        //Get the wallet address for each FNFT via multicall
+        let walletMultiCallList = await sdk.api.abi.multiCall({
+          abi: getWalletABI,
+          calls: calls,
+          chain: chain,
+          requery: true,
+          permitFailure: false 
+        })
+        let wallets = []
+        walletMultiCallList.output.forEach(wallet => {
+          // console.log(`output: ${JSON.stringify(wallet)}`)
+          wallets.push(wallet.output)
+        })
+
+        wallets.push(holder);
+
+        //Return the balance of revest in every wallet.
+        return sumTokens2({ api, owners: wallets, tokens: [revest] })
     }
 })
-
