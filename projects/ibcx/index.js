28a75cdd04cafbae56cbf3e747b85d128e0ed2cd
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2023-06-27 14:30:53 +0700

    fixing

--- projects/ibcx/index.js
+++ projects/ibcx/index.js
@@ -4,7 +4,7 @@ const { transformBalances } = require('../helper/portedTokens')
 const chain = 'osmosis'
 
 const contractAddresses = [
-    "osmo1yhd9tzp09d833u7ray4pc6wwp72aewtt2xwakszn3lzlf2klnlwscjwhxt",
+    "osmo1yhd9tzp09d833u7ray4pc6wwp72aewtt2xwakszn3lzlf2klnlwscjwhxt", // uibcx 
     "osmo1fw7wc4x7leyf3keud4zdhdnyaanw0sajudg5sgnr93ydjhzmxk6sx84rxe",
     "osmo1xqw2sl9zk8a6pch0csaw78n4swg5ws8t62wc5qta4gnjxfqg6v2qcs243k", // stIBCX
     "osmo1fd33mtk06awg0v2hhd6djcjvvzcv90s8u03qujcjnlu87wm65xjs305mr4",
@@ -21,6 +21,29 @@ async function tvl() {
             chain: 'osmosis',
             data: { 'get_total_supply': {} }
         });
+        console.log("totalSupply", totalSupply)
+
+        let config = await queryContract({
+            contract: contractAddress,
+            chain: 'osmosis',
+            data: { 'get_config': {} }
+        });
+        console.log("config", config)
+
+        let rebalance = await queryContract({
+            contract: contractAddress,
+            chain: 'osmosis',
+            data: { 'get_rebalance': {} }
+        });
+        console.log("rebalance", rebalance)
+
+        let balance = await queryContract({
+            contract: contractAddress,
+            chain: 'osmosis',
+            data: { 'get_balance': {'account': 'osmo1k8re7jwz6rnnwrktnejdwkwnncte7ek7gt29gvnl3sdrg9mtnqkse6nmqm'} }
+        });
+        console.log("balance", balance)
+
         if (typeof amounts['uion'] !== "undefined") {
             amounts['uion'] = totalSupply
         } else {
