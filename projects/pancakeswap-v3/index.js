cde2d5fb20e3040c6d60c47d2d50ce9d6cf08012
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-28 15:53:02 +0200

    fix pancakeswap v3

--- projects/pancakeswap-v3/index.js
+++ projects/pancakeswap-v3/index.js
@@ -7,6 +7,7 @@ module.exports = uniV3Export({
     '0xf8c7f403829cc0f9a37f126a3da41358c232acdf',
     '0x95e7c70b58790a1cbd377bc403cd7e9be7e0afb1',
     '0x454f4597582df557c2757403f47d3f3bbb890d43',
+    '0x121a3fba8456ebce13964363ba35fea00c2aa3d2',
   ] },
   ethereum: { factory: '0x0bfbcf9fa4f9c56b0f40a671ad40e0805a091865', fromBlock: 16950685, },
 })
