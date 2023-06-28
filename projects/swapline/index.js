48063d67ffed8bb05e4c4037f110b6d8ee63c973
0xjosaphat <120175614+0xjosaphat@users.noreply.github.com>
2023-06-28 13:15:37 +0200

    added new chain polygon_zkevm (#6668)
    * added new chain polygon_zkevm
    
    * added marker for new chain launch on Polygon zkEVM

--- projects/swapline/index.js
+++ projects/swapline/index.js
@@ -3,6 +3,7 @@ const { sumTokens2 } = require('../helper/unwrapLPs')
 const factories = {
   fantom: '0x640801a6983c109805E928dc7d9794080C21C88E',
   optimism: '0xd08C98F6409fCAe3E61f3157B4147B6595E60cf3',
+  polygon_zkevm: '0x5A5c0C4832828FF878CE3ab4fEc44d21200b1496',
 }
 
 async function tvl(_, _b, _cb, { api, }) {
@@ -29,13 +30,17 @@ async function tvl(_, _b, _cb, { api, }) {
 
 module.exports = {
   hallmarks: [
-    [1682298000,"Launch on Optimism"]
+    [1682298000,"Launch on Optimism"],
+    [1687827600,"Launch on Polygon zkEVM"]
   ],
   methodology: 'We count the token balances in in different liquidity book contracts',
   fantom:{
     tvl,
   },
   optimism:{
+    tvl,
+  },
+  polygon_zkevm:{
     tvl,
   },
 };
