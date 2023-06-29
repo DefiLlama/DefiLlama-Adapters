d8dd507935026cd02ffec80074d641fd2e7a5ad0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-29 13:18:38 +0200

    fix sushiswap

--- projects/sushiswap/api.js
+++ projects/sushiswap/api.js
@@ -2,8 +2,6 @@ const { request, gql } = require("graphql-request");
 const { toUSDTBalances } = require('../helper/balances');
 const { getBlock } = require('../helper/http')
 const { getUniTVL } = require("../helper/unknownTokens")
-const sdk = require('@defillama/sdk');
-const { getChainTransform } = require("../helper/portedTokens");
 
 const graphUrl = 'https://api.thegraph.com/subgraphs/name/zippoxer/sushiswap-subgraph-fork'
 const graphQuery = gql`
@@ -62,187 +60,42 @@ function getChainTVL(chain) {
 }
 
 const factory = '0xc35DADB65012eC5796536bD9864eD8773aBc74C4'
+const tvl = getUniTVL({
+  factory, useDefaultCoreAssets: true, blacklist: [
+    '0xed0b4b0f0e2c17646682fc98ace09feb99af3ade', // RVRS
+    '0x00598f74DA03489d4fFDb7Fde54db8E3D3AA9a61', // GSHIB
+    '0xE38928cd467AD7347465048b3637893124187d02', // GSHIB
+    '0xc0e39cbac6a5c5cdcdf2c1a1c29cbf5917754943', // GSHIB
+  ],
+})
 
 module.exports = {
-  telos: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'telos',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  palm: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'palm',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  moonriver: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'moonriver',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  celo: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'celo',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  okexchain: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'okexchain',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  arbitrum: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'arbitrum',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  xdai: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'xdai',
-      useDefaultCoreAssets: true,
-    })
-  },
-  harmony: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'harmony',
-      useDefaultCoreAssets: true,
-      blacklist: [
-        '0xed0b4b0f0e2c17646682fc98ace09feb99af3ade', // RVRS
-      ],
-    })
-  },
-  ethereum: {
-    tvl: eth,
-  },
-  polygon: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'polygon',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  fantom: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'fantom',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  bsc: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'bsc',
-      useDefaultCoreAssets: false,
-      blacklist: [
-        '0x00598f74DA03489d4fFDb7Fde54db8E3D3AA9a61', // GSHIB
-        '0xE38928cd467AD7347465048b3637893124187d02', // GSHIB
-        '0xc0e39cbac6a5c5cdcdf2c1a1c29cbf5917754943', // GSHIB
-      ]
-    }),
-  },
-  heco: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'heco',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  boba: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'boba',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  boba_avax: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'boba_avax',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  boba_bnb: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'boba_bnb',
-      useDefaultCoreAssets: true,
-    }),
-  },
-  avax: {
-    tvl: getUniTVL({
-      factory,
-      chain: 'avax',
-      useDefaultCoreAssets: true,
-    }),
-  },
+  telos: { tvl, },
+  palm: { tvl, },
+  moonriver: { tvl, },
+  celo: { tvl, },
+  okexchain: { tvl, },
+  arbitrum: { tvl, },
+  xdai: { tvl, },
+  harmony: { tvl, },
+  ethereum: { tvl: eth, },
+  polygon: { tvl, },
+  fantom: { tvl, },
+  bsc: { tvl, },
+  heco: { tvl, },
+  boba: { tvl, },
+  boba_avax: { tvl, },
+  boba_bnb: { tvl, },
+  avax: { tvl, },
   fuse: {
-    tvl: getUniTVL({
-      factory: '0x43eA90e2b786728520e4f930d2A71a477BF2737C',
-      chain: 'fuse',
-      useDefaultCoreAssets: true,
-    }),
+    tvl: getUniTVL({ factory: '0x43eA90e2b786728520e4f930d2A71a477BF2737C', useDefaultCoreAssets: true, }),
   },
-  arbitrum_nova: {
-    tvl: getUniTVL({
-      chain: 'arbitrum_nova',
-      useDefaultCoreAssets: true,
-      factory,
-    })
-  },
+  arbitrum_nova: { tvl, },
   //moved kava to trident adapter
-  //kava: {
-  //  tvl: kavaTridentTvl,
-  //},
+  //kava: {  //  tvl: kavaTridentTvl,  //},
 }
 
-module.exports.polygon.tvl = getChainTVL('polygon')
+// module.exports.polygon.tvl = getChainTVL('polygon')
 // module.exports.bsc.tvl = getChainTVL('bsc')
-module.exports.fantom.tvl = getChainTVL('fantom')
+// module.exports.fantom.tvl = getChainTVL('fantom')
 // module.exports.harmony.tvl = getChainTVL('harmony')
-
-async function kavaTridentTvl(ts, _b, cb) {
-  const chain = 'kava'
-  const graph = 'https://pvt.graph.kava.io/subgraphs/name/sushiswap/trident-kava'
-  const query = `query get_tvl($block: Int){
-      pairs(
-        block: { number: $block }
-        size: 1000
-      ){
-        id
-        name
-        type
-        reserve0
-        reserve1
-        token0 {
-          id
-        }
-        token1 {
-          id
-        }
-      }
-  }`
-  const block = await getBlock(ts, chain, cb, false)
-  const { pairs } = await request(graph, query, {
-    block: block - 100,
-  })
-  // const bentoBox = '0xc35DADB65012eC5796536bD9864eD8773aBc74C4'
-  const balances = {}
-  const transform = await getChainTransform(chain)
-  pairs.forEach(i => {
-    sdk.util.sumSingleBalance(balances, transform(i.token0.id), i.reserve0)
-    sdk.util.sumSingleBalance(balances, transform(i.token1.id), i.reserve1)
-  } )
-  return balances
-}
