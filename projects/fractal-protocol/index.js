3d540341d251d3a96c79a2d4520043f1487c05dd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-30 09:10:11 +0200

    fractral refactor: use sdk v2

--- projects/fractal-protocol/index.js
+++ projects/fractal-protocol/index.js
@@ -1,7 +1,5 @@
-const sdk = require('@defillama/sdk');
 const { userInfo } = require('../pendle/abi.json');
-const { subscribe } = require('graphql');
-const { sumTokens2 } = require("../helper/unwrapLPs");
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
 //Fractal Addresses ETH
 const FRACTAL_VAULT_CONTRACT_ETH = '0x3EAa4b3e8967c02cE1304C1EB35e8C5409838DFC';
@@ -34,11 +32,11 @@ const ETH_LOANS = [
 
 //Loans AVAX
 const AVAX_LOANS = [
-  '0x9297c3B0Eb3DB1Cc72E99cF641CE418a2c3791D9', 
-  '0x1952ae434FA8B5E9c9a1374951e5fd9ae7BD75C7', 
-  '0x9C7629E3bAD32631F3977eF7710415158208E057', 
-  '0x3f2ebE04329B96CBfF6E9911c64fC0c1Fc609d9C', 
-  '0x337dBC3d0C948F8Ef516F6678C3D63d11300Dc10', 
+  '0x9297c3B0Eb3DB1Cc72E99cF641CE418a2c3791D9',
+  '0x1952ae434FA8B5E9c9a1374951e5fd9ae7BD75C7',
+  '0x9C7629E3bAD32631F3977eF7710415158208E057',
+  '0x3f2ebE04329B96CBfF6E9911c64fC0c1Fc609d9C',
+  '0x337dBC3d0C948F8Ef516F6678C3D63d11300Dc10',
   '0x595b16Aab6E43664Bf5F2DF0E34fEF1A8e7a2692'
 ]
 
@@ -53,239 +51,88 @@ const abis = {
 }
 
 // Define the function
-async function getLoanDebt(loans, block, chain) {
-
-  let totalDebt = 0;
-  
-  for (const address of loans) {
-    try {
-      const { output: loanDebt } = await sdk.api.abi.call({ block, target: address, abi: abis.getDebt, chain })
-      totalDebt += loanDebt.principalDebtAmount / 1e6
-    } catch (error) {
-      console.error(`Error while processing address ${address}:`, error);
-    }
-  }
-  // Output the total sum of data
-  return totalDebt
+async function getLoanDebt(loans, api) {
+  const loanDebts = await api.multiCall({ abi: abis.getDebt, calls: loans })
+  loanDebts.map((loanDebt) => api.add(USDC_ETH, loanDebt.principalDebtAmount, { skipChain: true }))
 }
 
-// Define the function
-function getLpTokenPositionUsd(
-  token0Balance, 
-  token1Balance, 
-  token0Price, 
-  token1Price, 
-  lpTokenTotalSupply, 
-  lpTokenBalance) {
-
-  let token0ValueUsd = (token0Balance / 1e6) * (token0Price / 1e6)
-  let token1ValueUsd = (token1Balance / 1e6) * (token1Price / 1e6)
-
-  let lpBalanceRatio = lpTokenBalance / lpTokenTotalSupply
-
-  token0ValueUsd = token0ValueUsd * lpBalanceRatio
-  token1ValueUsd = token1ValueUsd * lpBalanceRatio
-
-  return token0ValueUsd + token1ValueUsd
-}
-
-
-const getEthTvl = async (_, ethBlock) => {
-  try {
-
-    const chain = 'ethereum'
-
-    let { output: vaultBalance } = await sdk.api.erc20.balanceOf({
-      target: USDC_ETH,
-      owner: FRACTAL_VAULT_CONTRACT_ETH,
-      block: ethBlock
-    });
-
-    let { output: yieldReserveBalance } = await sdk.api.erc20.balanceOf({
-      target: USDC_ETH,
-      owner: FRACTAL_YIELD_RESERVE_ETH,
-      block: ethBlock
-    });
-    
-    vaultBalance  = vaultBalance / 1e6
-    yieldReserveBalance = yieldReserveBalance / 1e6
-
-    //seems like there is an error here, I shouldn't be dividing the vault balance by 1e18
-    return {
-      tether: vaultBalance + yieldReserveBalance
-    }
-
-  } catch (error) {
-    console.error('Error calculating ETH TVL:', error);
-    throw error;
-  }
+const getEthTvl = async (_, ethBlock, __, { api }) => {
+  return sumTokens2({ owners: [FRACTAL_YIELD_RESERVE_ETH, FRACTAL_VAULT_CONTRACT_ETH], tokens: [USDC_ETH,], api })
 };
 
-
-const getEthLoans = async (_, ethBlock) => {
-
-  const chain = 'ethereum'
-
-  const loanDebt = await getLoanDebt(ETH_LOANS, ethBlock, chain)
-
-  return { tether: loanDebt}
-
+const getEthLoans = async (_, ethBlock, __, { api }) => {
+  return getLoanDebt(ETH_LOANS, api)
 }
 
-const getArbTvl = async (_, _b, { arbitrum: block }) => {
+const getArbTvl = async (_, _b, { arbitrum: block }, { api }) => {
+  // const usdfPrice = await api.call({    target: FRACTAL_VAULT_CONTRACT_ARB,    abi: abis.getTokenPrice,  })
 
-  try {
+  const [
+    chronosLpBalance, usdcBalanceChronosPool, usdfBalanceChronosPool, usdcBalanceCamelotPool, usdfBalanceCamelotPool,
+  ] = await api.multiCall({
+    abi: 'erc20:balanceOf', calls: [
+      { target: CHRONOS_GAUGE, params: FRACTAL_FIREBLOCKS_OWNER },
+      { target: USDC_ARB, params: FRACTAL_CHRONOS_POOL },
+      { target: USDF_ARB, params: FRACTAL_CHRONOS_POOL },
+      { target: USDC_ARB, params: FRACTAL_CAMELOT_POOL },
+      { target: USDF_ARB, params: FRACTAL_CAMELOT_POOL },
+    ]
+  })
 
-    const chain = 'arbitrum'
+  const camelotUserInfo = await api.call({
+    target: FRACTAL_CAMELOT_NITRO_POOL,
+    abi: abis.userInfo,
+    params: FRACTAL_FIREBLOCKS_OWNER,
+  })
 
-    const usdfPrice = await sdk.api.abi.call({
-      target: FRACTAL_VAULT_CONTRACT_ARB,
-      abi: abis.getTokenPrice, 
-      chain, 
-      block
-    })
+  const [
+    chronosLpTokenSupply, camelotLpTokenSupply,
+  ] = await api.multiCall({
+    abi: 'erc20:totalSupply', calls: [
+      FRACTAL_CHRONOS_POOL, FRACTAL_CAMELOT_POOL,
+    ]
+  })
 
-    const chronosLpBalance= await sdk.api.erc20.balanceOf({
-      target: CHRONOS_GAUGE,
-      owner: FRACTAL_FIREBLOCKS_OWNER,
-      block: block,
-      chain: chain
-    })
+  api.add(USDC_ARB, usdcBalanceChronosPool * chronosLpBalance / chronosLpTokenSupply)
+  api.add(USDC_ARB, usdcBalanceCamelotPool * camelotUserInfo.totalDepositAmount / camelotLpTokenSupply)
+  // api.add(USDF_ARB, usdfBalanceChronosPool * chronosLpBalance / chronosLpTokenSupply)
+  // api.add(USDF_ARB, usdfBalanceCamelotPool * camelotUserInfo.totalDepositAmount / camelotLpTokenSupply)
 
-    const chronosLpTokenSupply = await sdk.api.erc20.totalSupply({
-      target: FRACTAL_CHRONOS_POOL,
-      block: block,
-      chain: chain
-    })
-
-    const usdcBalanceChronosPool = await sdk.api.erc20.balanceOf({
-      target: USDC_ARB,
-      owner: FRACTAL_CHRONOS_POOL,
-      block: block,
-      chain: chain
-    })
-
-    const usdfBalanceChronosPool = await sdk.api.erc20.balanceOf({
-      target: USDF_ARB,
-      owner: FRACTAL_CHRONOS_POOL,
-      block: block,
-      chain: chain
-    })
-
-    const chronosPositionBalance = getLpTokenPositionUsd(
-      usdfBalanceChronosPool.output, 
-      usdcBalanceChronosPool.output, 
-      usdfPrice.output, 
-      1000000, 
-      chronosLpTokenSupply.output, 
-      chronosLpBalance.output)
-
-    const camelotUserInfo = await sdk.api.abi.call({
-      target: FRACTAL_CAMELOT_NITRO_POOL,
-      abi: abis.userInfo,
-      params: FRACTAL_FIREBLOCKS_OWNER,
-      block: block,
-      chain: chain
-    })
-
-    const camelotLpTokenSupply = await sdk.api.erc20.totalSupply({
-      target: FRACTAL_CAMELOT_POOL,
-      block: block,
-      chain: chain
-    })
-
-    const usdcBalanceCamelotPool = await sdk.api.erc20.balanceOf({
-      target: USDC_ARB,
-      owner: FRACTAL_CAMELOT_POOL,
-      block: block,
-      chain: chain
-    })
-
-    const usdfBalanceCamelotPool = await sdk.api.erc20.balanceOf({
-      target: USDF_ARB,
-      owner: FRACTAL_CAMELOT_POOL,
-      block: block,
-      chain: chain
-    })
-
-    const camelotPositionBalance = getLpTokenPositionUsd(
-      usdfBalanceCamelotPool.output, 
-      usdcBalanceCamelotPool.output, 
-      usdfPrice.output, 
-      1000000, 
-      camelotLpTokenSupply.output, 
-      camelotUserInfo.output.totalDepositAmount
-    )
-        
-    let { output: vaultBalance } = await sdk.api.erc20.balanceOf({
-      target: USDC_ARB,
-      owner: FRACTAL_VAULT_CONTRACT_ARB,
-      block: block,
-      chain: chain
-    });
-
-    let { output: yieldReserveBalance } = await sdk.api.erc20.balanceOf({
-      target: USDC_ARB,
-      owner: FRACTAL_YIELD_RESERVE_ARB ,
-      block: block,
-      chain: chain
-    });
-
-    vaultBalance  = vaultBalance / 1e6
-    yieldReserveBalance = yieldReserveBalance / 1e6
-
-    //seems like there is an error here, I shouldn't be dividing the vault balance by 1e18
-    return {
-      tether: vaultBalance + yieldReserveBalance + chronosPositionBalance + camelotPositionBalance
-    }
-
-  } catch (error) {
-    console.error('Error calculating ARB TVL:', error);
-    throw error;
-  }
+  return sumTokens2({ api, tokens: [USDC_ARB], owners: [FRACTAL_YIELD_RESERVE_ARB, FRACTAL_VAULT_CONTRACT_ARB] })
 };
 
-const getAvaxTvl = async (_, _b, { avax: block }) => {
-    const chain = 'avax'
-    const strategy = '0x9fea225c7953869e68b8228d2c90422d905e5117'
-    const nUSDLP = '0xCA87BF3ec55372D9540437d7a86a7750B42C02f4'
-    const nUSDSwap = '0xed2a7edd7413021d440b09d654f3b87712abab66'
-    const synapseMiniChef = '0x3a01521f8e7f012eb37eaaf1cb9490a5d9e18249'
-    const { output: { amount } } = await sdk.api.abi.call({
-      target: synapseMiniChef, params: [1, strategy],
-      abi: userInfo, chain, block,
-    })
-    const { output: price } = await sdk.api.abi.call({
-      target: nUSDSwap, abi: abis.getVirtualPrice, chain, block,
-    })
-    const synapseBalance = amount * price / 1e36
-    const loanDebt = await getLoanDebt(AVAX_LOANS, block, chain)
+const getAvaxTvl = async (_, _b, { avax: block }, { api }) => {
+  const strategy = '0x9fea225c7953869e68b8228d2c90422d905e5117'
+  const nUSDLP = '0xCA87BF3ec55372D9540437d7a86a7750B42C02f4'
+  const nUSDSwap = '0xed2a7edd7413021d440b09d654f3b87712abab66'
+  const synapseMiniChef = '0x3a01521f8e7f012eb37eaaf1cb9490a5d9e18249'
+  const { amount } = await api.call({
+    target: synapseMiniChef, params: [1, strategy],
+    abi: userInfo,
+  })
+  const price = await api.call({
+    target: nUSDSwap, abi: abis.getVirtualPrice,
+  })
+  const synapseBalance = amount * price / 1e36
 
-    return { tether: synapseBalance}
+  return { tether: synapseBalance }
 }
 
-const getAvaxLoans= async (_, _b, { avax: block }) => {
-
-  const chain = 'avax'
-
-  const loanDebt = await getLoanDebt(AVAX_LOANS, block, chain)
-
-  return { tether: loanDebt}
-
+const getAvaxLoans = async (_, _b, { avax: block }, { api }) => {
+  return getLoanDebt(AVAX_LOANS, api)
 }
 
-const getPolygonTvl = async (_, _b, { polygon: block }) => {
-  const chain = 'polygon'
+const getPolygonTvl = async (_, _b, { polygon: block }, { api }) => {
   const strategy = '0x894cB5e24DDdD9ececb27831647ae869541Af28F'
   const nUSDLP = '0x7479e1bc2f2473f9e78c89b4210eb6d55d33b645'
   const nUSDSwap = '0x85fcd7dd0a1e1a9fcd5fd886ed522de8221c3ee5'
   const synapseMiniChef = '0x7875af1a6878bda1c129a4e2356a3fd040418be5'
-  const { output: { amount } } = await sdk.api.abi.call({
+  const { amount } = await api.call({
     target: synapseMiniChef, params: [1, strategy],
-    abi: userInfo, chain, block,
+    abi: userInfo,
   })
-  const { output: price } = await sdk.api.abi.call({
-    target: nUSDSwap, abi: abis.getVirtualPrice, chain, block,
+  const price = await api.call({
+    target: nUSDSwap, abi: abis.getVirtualPrice,
   })
   return { tether: amount * price / 1e36 }
 }
