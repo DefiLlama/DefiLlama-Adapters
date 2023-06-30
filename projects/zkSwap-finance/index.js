f8bbc71153e917670c50c858ae19d9b1e2414f8e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-06-30 11:14:53 +0200

    minor fix

--- projects/zkSwap-finance/index.js
+++ projects/zkSwap-finance/index.js
@@ -6,6 +6,7 @@ module.exports = {
     tvl: getUniTVL({
       factory: '0x3a76e377ED58c8731F9DF3A36155942438744Ce3',
       useDefaultCoreAssets: true,
+      fetchBalances: true,
     })
   },
   methodology: "TVL is total liquidity of all liquidity pools."
