ab8f0ae4710fbe62ff5d032067a537a9587765b8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-01 19:04:12 +0200

    minor fix

--- projects/soarpulse/index.js
+++ projects/soarpulse/index.js
@@ -1,18 +1,9 @@
-const { sumTokens2, nullAddress } = require("../helper/unwrapLPs");
+const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
 
 const contract = "0xb29201EBB420b28Bb43052Ad58C23F43d6899F51";
 
-async function tvl(time, ethBlock, { bsc: block }) {
-  return sumTokens2({
-    tokens: [nullAddress],
-    owner: contract,
-    block,
-    chain: "pulse",
-  });
-}
-
 module.exports = {
   pulse: {
-    tvl,
+    tvl: sumTokensExport({ owner: contract, tokens: [nullAddress]}),
   },
 };
