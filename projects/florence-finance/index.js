4eec8dd9af119c6d43616ba9184527bf5c366d21
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-03 18:36:47 +0200

    code refactor

--- projects/florence-finance/index.js
+++ projects/florence-finance/index.js
@@ -1,37 +1,20 @@
-const sdk = require("@defillama/sdk");
 
 const FACTORY_CONTRACT_ETH = "0xD6348E8EacE62Eb3Eb77fBbA8D8c363e375fC455";
 const EURS_CONTRACT_ETH= "0xdb25f211ab05b1c97d595516f45794528a807ad8";
 
 async function tvl(_, _1, _2, { api }) {
-  
-  const balances = {};
-
   // Get all vaults
   const vaultIds = await api.call({ 
     abi: "function getLoanVaultIds() external view returns (string[])",
     target: FACTORY_CONTRACT_ETH
-  });
-
-  // Get the sum of loansOutstanding (TVL)
-  for(const vaultId of vaultIds) {
-
-    const VAULT_CONTRACT = await api.call({ abi: "function getLoanVault (string loanVaultId) external view returns (address)", target: FACTORY_CONTRACT_ETH, params: [vaultId] });
-
-    const loansOutstanding = await api.call({ abi: "function loansOutstanding() external view returns (uint256)", target: VAULT_CONTRACT });
-
-    //api.add(VAULT_CONTRACT, loansOutstanding)
-
-    // Take the sum of all vault tokens in terms of EURS (1 Loan Vault Token = 1 EURS Statis) on the platform | 18-2 = 16 atomic units (LV-EURS)
-    sdk.util.sumSingleBalance(balances, EURS_CONTRACT_ETH, Number(loansOutstanding) / Math.pow(10, 16));
-  }
-
-  return balances
+  })
+  const vaultContracts = await api.multiCall({  abi: "function getLoanVault (string loanVaultId) external view returns (address)", target: FACTORY_CONTRACT_ETH, calls: vaultIds })
+  const loans = await api.multiCall({  abi: "function loansOutstanding() external view returns (uint256)", calls: vaultContracts})
+  // Take the sum of all vault tokens in terms of EURS (1 Loan Vault Token = 1 EURS Statis) on the platform | 18-2 = 16 atomic units (LV-EURS
+  loans.forEach((val, i) => api.add(EURS_CONTRACT_ETH, val / 1e16))
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: true,
   start: 16077400,
   methodology: "Data is retrieved on-chain by taking the total sum of all loans outstanding (dominated in EURS Statis) from all platform vaults.",
   ethereum: { tvl }
