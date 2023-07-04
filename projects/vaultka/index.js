207ea362cb7e96145da446ce1a95f3d589687c06
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-07-04 10:00:39 +0200

    add tvl for new pool sake and whiskey

--- projects/vaultka/index.js
+++ projects/vaultka/index.js
@@ -11,8 +11,67 @@ module.exports = {
         calls: vaults,
       });
 
+      const addresses = {
+        whiskey: "0x6532eFCC1d617e094957247d188Ae6d54093718A",
+        whiskeyWater: "0xa100E02e861132C4703ae96D6868664f27Eaa431",
+        sake: "0x45BeC5Bb0EE87181A7Aa20402C66A6dC4A923758",
+        sakeWater: "0x6b367F9EB22B2E6074E9548689cddaF9224FC0Ab",
+      };
+
+      const contractAbis = {
+        gainsBalance: "function getGainsBalance() view returns (uint256)",
+        gTokenPrice: "function gTokenPrice() view returns (uint256)",
+        wWaterBalance: "function balanceOfDAI() public view returns (uint256)",
+        vlpBalance: "function getVlpBalance() public view returns (uint256)",
+        stakedVlpBalance:
+          "function getStakedVlpBalance() public view returns (uint256)",
+        vlpPrice: "function getVLPPrice() public view returns (uint256)",
+        waterUSDCBal: "function balanceOfUSDC() public view returns (uint256)",
+      };
+
+      const whiskeyGainsBalance = await api.call({
+        abi: contractAbis.gainsBalance,
+        target: addresses.whiskey,
+      });
+
+      const whiskeyGTokenPrice = await api.call({
+        abi: contractAbis.gTokenPrice,
+        target: addresses.whiskey,
+      });
+
+      const whiskeyWaterDaiBal = await api.call({
+        abi: contractAbis.wWaterBalance,
+        target: addresses.whiskeyWater,
+      });
+
+      const sakeWaterUSDCBal = await api.call({
+        abi: contractAbis.waterUSDCBal,
+        target: addresses.sakeWater,
+      });
+
+      const vlpBal = await api.call({
+        abi: contractAbis.vlpBalance,
+        target: addresses.sake,
+      });
+
+      const StakedVLPBal = await api.call({
+        abi: contractAbis.stakedVlpBalance,
+        target: addresses.sake,
+      });
+
+      const sakeVLPPrice = await api.call({
+        abi: contractAbis.vlpPrice,
+        target: addresses.sake,
+      });
+
       return {
         tether: bals.reduce((a, i) => a + i / 1e6, 0),
+        dai:
+          (whiskeyGainsBalance * whiskeyGTokenPrice) / 1e36 +
+          whiskeyWaterDaiBal / 1e18,
+        "usd-coin":
+          ((vlpBal + StakedVLPBal) * sakeVLPPrice) / 1e18 / 1e5 +
+          sakeWaterUSDCBal / 1e6,
       };
     },
   },
