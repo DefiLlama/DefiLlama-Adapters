d69a28d312a319b103ff4907452f6e395453a88b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-04 17:29:04 +0200

    minor fix

--- projects/tashi/index.js
+++ projects/tashi/index.js
@@ -1,11 +1,7 @@
-const {compoundExports} = require('../helper/compound')
+const { compoundExports } = require('../helper/compound')
 const ADDRESSES = require('../helper/coreAssets.json')
 
-module.exports={
-    timetravel: true,
-    doublecounted: false,
-    methodology: "Same as compound, we just get all the collateral (not borrowed money) on the lending markets",    
-    evmos:{
-        ...compoundExports("0x053841Bd1D291380726a007eA834Ecd296923260", "evmos", "0x1cd248D72248A0618932F77093Dc4ceC9757759d", ADDRESSES.evmos.WEVMOS)
-    }
+module.exports = {
+  methodology: "Same as compound, we just get all the collateral (not borrowed money) on the lending markets",
+  evmos: compoundExports("0x053841Bd1D291380726a007eA834Ecd296923260", "evmos", "0x1cd248D72248A0618932F77093Dc4ceC9757759d", ADDRESSES.null, undefined, undefined, { fetchBalances: true, })
 }
