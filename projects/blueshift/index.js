67a1485357bb28c62bbc3a2211c4c43bfb31ca4c
L0stic <k0ndratev.da@yandex.ru>
2023-07-04 09:13:40 +0300

    Update registry abi and add blueschain tvl

--- projects/blueshift/index.js
+++ projects/blueshift/index.js
@@ -31,18 +31,38 @@ async function staking(chain, chainBlocks) {
 async function tvl(chain, chainBlocks) {
   const balances = {};
 
-  const portfolios = (await sdk.api.abi.call({
-    abi: abi.BlueshiftRegistry.getPortfolios,
-    chain: chain,
-    target: config.registry[chain],
-    params: [],
-    block: chainBlocks[chain],
-  })).output;
+  // Blueschain reserves
+  if (config.blueschain[chain]) {
+    const tokenBalances = await Promise.all(
+      config.blueschain[chain].tokens.map(async token => (await sdk.api.abi.call({
+        abi: abi.ERC20.balanceOf,
+        chain: chain,
+        target: token,
+        params: [config.blueschain[chain].reserve],
+        block: chainBlocks[chain],
+      })).output)
+    );
 
-  for (let portfolio of portfolios) {
-    const value = portfolio.totalValue;
-    sdk.util.sumSingleBalance(balances, portfolio.baseTokenAddress, value);
+    for (let i = 0; i < tokenBalances.length; ++i) {
+      sdk.util.sumSingleBalance(balances, config.blueschain[chain].tokens[i], tokenBalances[i]);
+    }
   }
+
+  // Local reserves
+  if (config.registry[chain]) {
+    const portfolios = (await sdk.api.abi.call({
+      abi: abi.BlueshiftRegistry.getPortfolios,
+      chain: chain,
+      target: config.registry[chain],
+      params: [],
+      block: chainBlocks[chain],
+    })).output;
+
+    for (let i = 0; i < portfolios.contractAddress.length; ++i) {
+      sdk.util.sumSingleBalance(balances, portfolios.baseTokenAddress[i], portfolios.totalValue[i]);
+    }
+  }
+
   return transformBalances(chain, balances);
 }
 
@@ -62,5 +82,9 @@ module.exports = {
     start: 2499737,
     staking: (timestamp, block, chainBlocks) => staking('kava', chainBlocks),
     tvl: (timestamp, block, chainBlocks) => tvl('kava', chainBlocks)
+  },
+  polygon: {
+    start: 43057589,
+    tvl: (timestamp, block, chainBlocks) => tvl('polygon', chainBlocks)
   }
 };
