6350bed8b7f8152bede211b294ca843fdea866d8
chainupk <121842069+chainupk@users.noreply.github.com>
2023-07-05 23:14:48 +0800

    remove nodedao filecoin and created folder for hashking (#6728)

--- projects/node-dao/index.js
+++ projects/node-dao/index.js
@@ -7,14 +7,5 @@ module.exports = {
       const totalEth = await api.call({  abi: 'uint256:getTotalEthValue', target: owner })
       api.add(nullAddress,totalEth)
     }
-  },
-  filecoin: {
-    tvl: async (_, _1, _2, { api }) => {
-      const owner = '0xe012f3957226894b1a2a44b3ef5070417a069dc2'
-      const validators = await api.call({ target: owner, abi: 'function beneficiarys() public view returns (address [] memory)'})
-      const bals = await api.multiCall({  abi: 'uint256:totalStakingFil', calls: validators})
-      bals.forEach(i => api.add(nullAddress, i))
-      return sumTokens2({ api, owners: [owner], tokens: [nullAddress]})
-    }
   }
 }
