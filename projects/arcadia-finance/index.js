0d7d363ae531622cd2a3c4afc27a016472afe85d
- <->
2023-07-06 11:59:03 +0200

    :tada: Add tvl

---
+++ projects/arcadia-finance/index.js
@@ -0,0 +1,23 @@
+const { addVaultAssets, addPoolTVL } = require("./helper");
+async function tvlOptimism(timestamp, ethBlock, { optimism: block }, { api }) {
+  await addPoolTVL(timestamp, ethBlock, "optimism", api)
+  await addVaultAssets(timestamp, ethBlock, "optimism", api)
+}
+
+
+
+async function tvlEthereum(timestamp, ethBlock, { ethereum: block }, { api }) {
+  await addPoolTVL(timestamp, ethBlock, "ethereum", api)
+  await addVaultAssets(timestamp, ethBlock, "ethereum", api)
+}
+
+module.exports = {
+  methodology:
+    "TVL includes ERC-20 tokens that have been supplied as well as ERC-20 tokens that are supplied for lending.",
+  optimism:{
+    tvl:tvlOptimism
+  },
+  ethereum:{
+    tvl:tvlEthereum
+  },
+};
