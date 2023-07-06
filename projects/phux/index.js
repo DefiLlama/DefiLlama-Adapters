b676ebb515b47c869bde1b60da90db96103d7f80
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-06 09:32:01 +0200

    minor fix

--- projects/phux/index.js
+++ projects/phux/index.js
@@ -1,1 +1,16 @@
-module.exports = require('./onchain')
+const { onChainTvl } = require('../helper/balancer')
+
+const VAULT_ADDRESS = '0x7F51AC3df6A034273FB09BB29e383FCF655e473c';
+
+const config = {
+  pulse: { fromBlock: 17500116, },
+}
+
+module.exports = {};
+
+Object.keys(config).forEach(chain => {
+  const { fromBlock } = config[chain]
+  module.exports[chain] = {
+    tvl: onChainTvl(VAULT_ADDRESS, fromBlock)
+  }
+})
