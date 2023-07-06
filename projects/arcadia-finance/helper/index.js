7406470600ab3c18b7ff499ad9d71c466fba2247
g1nt0ki <llama@llama.fi>
2023-07-06 22:09:10 +0200

    code refactor

--- projects/arcadia-finance/helper/index.js
+++ projects/arcadia-finance/helper/index.js
@@ -1,63 +1,38 @@
-const ADDRESSES = require("../../helper/coreAssets.json");
-const ARCADIA_ADDRESSES = {
+const config = {
   optimism: {
     factory: "0x00CB53780Ea58503D3059FC02dDd596D0Be926cB",
-    wethPool: "0xD417c28aF20884088F600e724441a3baB38b22cc",
-    usdcPool: "0x9aa024D3fd962701ED17F76c17CaB22d3dc9D92d"
+    pools: {
+      wethPool: "0xD417c28aF20884088F600e724441a3baB38b22cc",
+      usdcPool: "0x9aa024D3fd962701ED17F76c17CaB22d3dc9D92d"
+    }
   },
   ethereum: {
     factory: "0x00CB53780Ea58503D3059FC02dDd596D0Be926cB",
-    wethPool: "0xD417c28aF20884088F600e724441a3baB38b22cc",
-    usdcPool: "0x9aa024D3fd962701ED17F76c17CaB22d3dc9D92d"
-  }
-}
-const abi = require("./abi.json");
-
-
-async function addPoolTVL(timestamp, ethBlock, chain, api) {
-
-  const [usdc_trl, weth_trl, usdc_turl, weth_turl] = await Promise.all([
-    api.call({ target: ARCADIA_ADDRESSES[chain].usdcPool, abi: abi['totalRealisedLiquidity'], }),
-    api.call({ target: ARCADIA_ADDRESSES[chain].wethPool, abi: abi['totalRealisedLiquidity'], }),
-    api.call({ target: ARCADIA_ADDRESSES[chain].usdcPool, abi: abi['totalUnrealisedLiquidity'], }),
-    api.call({ target: ARCADIA_ADDRESSES[chain].wethPool, abi: abi['totalUnrealisedLiquidity'], }),
-  ])
-
-  api.addTokens([
-    ADDRESSES[chain].WETH,
-    ADDRESSES[chain].WETH,
-    ADDRESSES[chain].USDC,
-    ADDRESSES[chain].USDC,
-  ], [
-    weth_trl,
-    weth_turl,
-    usdc_trl,
-    usdc_turl])
-}
-
-async function addVaultAssets(timestamp, ethBlock, chain, api) {
-
-  const vaultsLength = await api.call({target: ARCADIA_ADDRESSES[chain].factory, abi: abi['vaultLength']})
-  const vaultIds = Array.from(Array(parseInt(vaultsLength)).keys())
-  const vaultAddresses = await api.multiCall({target: ARCADIA_ADDRESSES[chain].factory, calls: vaultIds, abi: abi['vaultAddress']})
-  const assetsInVaults = await api.multiCall({abi: abi['assetData'], calls: vaultAddresses.map((v) => ({
-      target: v,
-      params: []
-    })),
-    ethBlock,
-    chain
-  })
-  for (let i = 0; i < assetsInVaults.length; i++) {
-    const assetAddresses_ = assetsInVaults[i][0];
-    for (let j = 0; j < assetAddresses_.length; j++) {
-      const assetAddress_ = assetAddresses_[j]
-      const assetAmount_ = assetsInVaults[i][2][j]
-      api.add(assetAddress_, assetAmount_)
+    pools: {
+      wethPool: "0xD417c28aF20884088F600e724441a3baB38b22cc",
+      usdcPool: "0x9aa024D3fd962701ED17F76c17CaB22d3dc9D92d",
     }
   }
 }
 
+const abi = require("./abi.json");
+
+async function tvl(_, _b, _cb, { api, }) {
+  const { chain } = api
+  let { factory, pools } = config[chain]
+  pools = Object.values(pools)
+  const poolAssets = await api.multiCall({ abi: 'address:asset', calls: pools })
+  const totalRealisedLiquidity = await api.multiCall({ abi: abi.totalRealisedLiquidity, calls: pools })
+  const totalUnrealisedLiquidity = await api.multiCall({ abi: abi.totalUnrealisedLiquidity, calls: pools })
+  api.addTokens(poolAssets, totalRealisedLiquidity)
+  api.addTokens(poolAssets, totalUnrealisedLiquidity)
+
+  const vaults = await api.fetchList({ lengthAbi: abi.vaultLength, itemAbi: abi.vaultAddress, target: factory, })
+  const assetData = await api.multiCall({ abi: abi.assetData, calls: vaults, })
+
+  assetData.forEach((assetsInVaults) => api.addTokens(assetsInVaults[0], assetsInVaults[2]))
+}
+
 module.exports = {
-  addPoolTVL,
-  addVaultAssets,
+  tvl,
 };
--- projects/arcadia-finance/index.js
+++ projects/arcadia-finance/index.js
@@ -1,23 +1,8 @@
-const { addVaultAssets, addPoolTVL } = require("./helper");
-async function tvlOptimism(timestamp, ethBlock, { optimism: block }, { api }) {
-  await addPoolTVL(timestamp, ethBlock, "optimism", api)
-  await addVaultAssets(timestamp, ethBlock, "optimism", api)
-}
-
-
-
-async function tvlEthereum(timestamp, ethBlock, { ethereum: block }, { api }) {
-  await addPoolTVL(timestamp, ethBlock, "ethereum", api)
-  await addVaultAssets(timestamp, ethBlock, "ethereum", api)
-}
+const { tvl, } = require("./helper");
 
 module.exports = {
   methodology:
     "TVL includes ERC-20 tokens that have been supplied as collateral as well as ERC-20 tokens that are supplied by liquidity providers.",
-  optimism:{
-    tvl:tvlOptimism
-  },
-  ethereum:{
-    tvl:tvlEthereum
-  },
-};
+  optimism: { tvl },
+  ethereum: { tvl },
+}
