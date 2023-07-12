5e0d450166cd03ad6e2c3a697445175885cf5145
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-12 17:09:49 +0200

    jewelswap: coide refactor

--- projects/jewelswap/index.js
+++ projects/jewelswap/index.js
@@ -1,4 +1,5 @@
 const { toUSDTBalances } = require("../helper/balances");
+const { sumTokens } = require("../helper/chain/elrond");
 const { get } = require("../helper/http");
 
 const API_URL = "https://api-nfts.jewelswap.io/tvl";
@@ -8,9 +9,16 @@ async function tvl() {
   return toUSDTBalances(data);
 }
 
+const LENDING_POOL = 'erd1qqqqqqqqqqqqqpgqhpauarfmx75nf4pwxh2fuy520ym03p8e8jcqt466up'
+const LENDING_POOL_FARMS = 'erd1qqqqqqqqqqqqqpgq96n4gxvmw8nxgxud8nv8qmms5namspc5vmusg930sh'
+const FARMS = 'erd1qqqqqqqqqqqqqpgqlnxy2hmvs8qxr6ezq2wmggn7ev62cjp6vmusvdral4'
+async function tvl1(_, _b, _cb, { api, }) {
+  return sumTokens({ owners: [LENDING_POOL, LENDING_POOL_FARMS, FARMS]})
+  
+}
 module.exports = {
   timetravel: false,
   elrond: {
-    tvl: tvl,
+    tvl: tvl1,
   },
 };
