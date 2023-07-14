84984505a8cc7e1361951ae94e51c625372f8063
not found <notfound@gmail.com>
2023-07-14 15:08:32 +0700

    seprate TVL tokens

--- projects/zkSwap-finance/index.js
+++ projects/zkSwap-finance/index.js
@@ -1,13 +1,28 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { getLogs } = require('../helper/cache/getLogs')
+const { sumTokens2 } = require('../helper/unwrapLPs')
+
+const FACTORY = '0x3a76e377ed58c8731f9df3a36155942438744ce3'
+const CHAIN = 'era'
+
+async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
+  const factoryLogs = await getLogs({
+    api,
+    target: FACTORY,
+    fromBlock: 4182273,
+    topic: 'PairCreated(address,address,address,uint256)',
+    eventAbi: 'event PairCreated(address indexed token0, address indexed token1, address pair, uint256)',
+    onlyArgs: true,
+  })
+  
+  let balanceRequests = []
+  factoryLogs.forEach(({ token0, token1, pair}) => {
+    balanceRequests.push([token0, pair])
+    balanceRequests.push([token1, pair])
+  })
+  return sumTokens2({ chain: CHAIN, ethBlock, tokensAndOwners: balanceRequests })
+}
 
 module.exports = {
-  misrepresentedTokens: true,
-  era: {
-    tvl: getUniTVL({
-      factory: '0x3a76e377ED58c8731F9DF3A36155942438744Ce3',
-      useDefaultCoreAssets: true,
-      fetchBalances: true,
-    })
-  },
+  era: { tvl, },
   methodology: "TVL is total liquidity of all liquidity pools."
-};
+}
