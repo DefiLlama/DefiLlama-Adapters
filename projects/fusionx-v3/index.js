ac263becdb293c4ade48a3a365feaf0731c8e7b3
0xngmi <80541789+0xngmi@users.noreply.github.com>
2023-07-17 13:45:54 +0100

    Update fusion v3

--- projects/fusionx-v3/index.js
+++ projects/fusionx-v3/index.js
@@ -1,5 +1,5 @@
 const { uniV3Export } = require('../helper/uniswapV3')
 
 module.exports = uniV3Export({
-  mantle: { factory: '0x0c69e6B21cD183e2A268649178A66d53d738E760', fromBlock: 2646 },
+  mantle: { factory: '0x530d2766D1988CC1c000C8b7d00334c14B69AD71', fromBlock: 2646 },
 })
