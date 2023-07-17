e5695408059c89f83d1bc71004c42c08cd2135ae
amilcarrey <amilcarrey.ar@gmail.com>
2023-07-17 18:09:54 -0300

    Adding liquid staking

--- projects/hatom-liquid-staking/index.js
+++ projects/hatom-liquid-staking/index.js
@@ -1,13 +1,34 @@
-async function tvl() {
+const { getTokenPrices, getLiquidStakingAddress } = require("../helper/hatom/hatom-graph");
+const {
+   getLiquidStakingCashReserve,
+} = require("../helper/hatom/on-chain");
 
-}
+const ADDRESSES = require('../helper/coreAssets.json');
+const BigNumber = require("bignumber.js");
+
+
+const tlv = async () => {
+   // Fetching data off chain
+   const [tokenPrices, liquidStakingAddress] = await Promise.all([
+      getTokenPrices(),
+      getLiquidStakingAddress()
+   ]);
+
+   // Fetching data on chain
+   const liquidStakingCashReserve = await getLiquidStakingCashReserve(liquidStakingAddress)
+
+   // Formatting data
+   const liquidStakingCashReserveUSD = BigNumber(liquidStakingCashReserve)
+      .dividedBy(1e18)
+      .multipliedBy(tokenPrices?.EGLD || 0)
+
+   return { [ADDRESSES.ethereum.USDC]: liquidStakingCashReserveUSD.multipliedBy(1e6).toNumber() }
+};
 
 module.exports = {
    misrepresentedTokens: true,
    timetravel: false,
-   methodology:
-      "The Total Value Locked (TVL) is computed as the aggregate sum of the EGLD reserve held within the liquid staking protocol, in conjunction with the USD reserve held within the lending protocol. This calculation encompasses not only the liquid balance but also takes into consideration the borrowing activity.",
    elrond: {
-      tvl: tvl,
+      tvl: tlv,
    },
 };
