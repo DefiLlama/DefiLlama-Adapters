47787500acbef1033270dfbb21664591cf60f662
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-18 22:17:46 +0200

    ondo: track polygon

--- projects/ondofinance/index.js
+++ projects/ondofinance/index.js
@@ -1,18 +1,26 @@
-const sdk = require("@defillama/sdk");
-const funds = [
-  "0x1B19C19393e2d034D8Ff31ff34c81252FcBbee92", // OUSG
-];
 
 module.exports = {
   methodology: "Sums Ondo's fund supplies.",
   misrepresentedTokens: true,
   doublecounted: true,
-  ethereum: {
-    tvl: async (_, __, ___, { api }) => {
-      const supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: funds })
-      const balances = {}
-      supplies.forEach((v, i) => sdk.util.sumSingleBalance(balances, funds[i], v, api.chain))
-      return balances
-    }
-  },
 };
+
+const config = {
+  ethereum: {
+    OUSG: '0x1B19C19393e2d034D8Ff31ff34c81252FcBbee92',
+  },
+  polygon: {
+    OUSG: '0xbA11C5effA33c4D6F8f593CFA394241CfE925811',
+  }
+}
+
+Object.keys(config).forEach(chain => {
+  let funds = config[chain]
+  funds = Object.values(funds)
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: funds })
+      api.addTokens(funds, supplies)
+    }
+  }
+})
