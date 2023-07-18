7d178d691a8aac78b1b27d9f47d46c7cf3f98e86
Yash Patil <yash.patil@ondo.finance>
2023-07-18 17:34:49 -0400

    add polygon

--- projects/ondofinance/index.js
+++ projects/ondofinance/index.js
@@ -1,26 +1,29 @@
+const sdk = require("@defillama/sdk");
+const funds_eth = [
+  "0x1B19C19393e2d034D8Ff31ff34c81252FcBbee92", // OUSG_ETH
+];
+const funds_polygon = [
+  "0xbA11C5effA33c4D6F8f593CFA394241CfE925811", // OUSG_POLY
+]
 
 module.exports = {
   methodology: "Sums Ondo's fund supplies.",
   misrepresentedTokens: true,
   doublecounted: true,
-};
-
-const config = {
   ethereum: {
-    OUSG: '0x1B19C19393e2d034D8Ff31ff34c81252FcBbee92',
+    tvl: async (_, __, ___, { api }) => {
+      const supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: funds_eth })
+      const balances = {}
+      supplies.forEach((v, i) => sdk.util.sumSingleBalance(balances, funds_eth[i], v, api.chain))
+      return balances
+    }
   },
   polygon: {
-    OUSG: '0xbA11C5effA33c4D6F8f593CFA394241CfE925811',
-  }
-}
-
-Object.keys(config).forEach(chain => {
-  let funds = config[chain]
-  funds = Object.values(funds)
-  module.exports[chain] = {
-    tvl: async (_, _b, _cb, { api, }) => {
-      const supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: funds })
-      api.addTokens(funds, supplies)
+    tvl: async (_, __, ___, { api }) => {
+      const supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: funds_polygon })
+      const balances = {}
+      supplies.forEach((v, i) => sdk.util.sumSingleBalance(balances, funds_polygon[i], v, api.chain))
+      return balances
     }
   }
-})
+};
