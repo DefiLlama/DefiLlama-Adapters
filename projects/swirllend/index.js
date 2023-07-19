026f727770c2019968103753ba4d8b56492a701b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-19 20:26:26 +0200

    minor fix

--- projects/swirllend/index.js
+++ projects/swirllend/index.js
@@ -3,9 +3,7 @@ const { compoundExports2 } = require("../helper/compound");
 module.exports = {
   linea: compoundExports2({
     comptroller: "0xcAABf6Ffb76634183254B0a4a3D5fb36180FdAE4",
-    abis: {
-      getAllMarkets: "address[]:allMarkets",
-      totalBorrows: "uint256:totalBorrow",
-    },
+    fetchBalances: true,
+    cether: '0x91067b912f89dfca50c3f5529dbeb78b63c6a2e5',
   }),
 };
