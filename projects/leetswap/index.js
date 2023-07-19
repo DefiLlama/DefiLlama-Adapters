659c00d796d6db2fa60538ea2d32f89aebec90b9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-19 21:34:12 +0200

    leetswap: track linea

--- projects/leetswap/index.js
+++ projects/leetswap/index.js
@@ -4,4 +4,5 @@ module.exports = {
   misrepresentedTokens: true,
   polygon_zkevm: { tvl: getUniTVL({ factory: '0xcE87E0960f4e2702f4bFFE277655E993Ae720e84', useDefaultCoreAssets: true,  }), },
   canto: { tvl: getUniTVL({ factory: '0x116e8a41E8B0A5A87058AF110C0Ddd55a0ed82B7', useDefaultCoreAssets: true,  }), },
+  linea: { tvl: getUniTVL({ factory: '0x4DDf0fa98B5f9Bd7Cb0645c25bA89A574fe9Be8c', useDefaultCoreAssets: true,  }), },
 }
