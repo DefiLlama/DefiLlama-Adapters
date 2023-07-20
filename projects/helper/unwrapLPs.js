5e5289eea1f7423b3141d10321bf6d80b5f1fdb8
LPX <yoon.hansup1@gmail.com>
2023-07-21 06:31:59 +0900

    add(revert-evmos) (#6872)

--- projects/helper/unwrapLPs.js
+++ projects/helper/unwrapLPs.js
@@ -200,6 +200,7 @@ async function unwrapUniswapV3NFTs({ balances = {}, nftsAndOwners = [], block, c
         case 'optimism':
         case 'arbitrum': nftAddress = '0xC36442b4a4522E871399CD717aBDD847Ab11FE88'; break;
         case 'bsc': nftAddress = [PANCAKE_NFT_ADDRESS, '0x7b8a01b39d58278b5de7e48c8449c9f4f5170613']; break;
+        case 'evmos': nftAddress = '0x5fe5daaa011673289847da4f76d63246ddb2965d'; break;
         default: throw new Error('missing default uniswap nft address')
       }
 
