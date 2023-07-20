fdbd821cb613a92e25e8d84ab5e18afc68ca554a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-20 13:18:35 +0200

    code refactor

--- projects/narwhal-finance/index.js
+++ projects/narwhal-finance/index.js
@@ -1,46 +1,19 @@
-const coreAssets = require('../helper/coreAssets.json');
-const { sumTokens2 } = require('../helper/unwrapLPs');
-
-const tokens = {
-  arbitrum: {
-    USDT: coreAssets.arbitrum.USDT,
-  },
-  bsc: {
-    USDT: coreAssets.bsc.USDT,
-  },
-};
-
-const narwhal = {
-  arbitrum: {
-    NarwhalTradingVault: '0x14559479DC1041Ef6565f44028D454F423d2b9E6',
-  },
-  bsc: {
-    NarwhalTradingVault: '0x71AF984f825C7BEf79cAEE5De14565ca8A29Fe93',
-  },
-};
-
-async function arbTvl() {
-  const tokensAndOwners = [
-    [tokens.arbitrum.USDT, narwhal.arbitrum.NarwhalTradingVault],
-  ];
-  return sumTokens2({ chain: "arbitrum", tokensAndOwners });
-}
-
-async function bscTvl() {
-  const tokensAndOwners = [
-    [tokens.bsc.USDT, narwhal.bsc.NarwhalTradingVault],
-  ];
-  return sumTokens2({ chain: "bsc", tokensAndOwners });
-}
+const { sumTokensExport } = require('../helper/unwrapLPs')
+const ADDRESSES = require('../helper/coreAssets.json');
 
 module.exports = {
   hallmarks: [
     [1689034511, "Launch on Arbitrum & BSC"],
   ],
-  arbitrum: {
-    tvl: arbTvl,
-  },
-  bsc: {
-    tvl: bscTvl,
-  },
 };
+
+const config = {
+  bsc: { owner: '0x71AF984f825C7BEf79cAEE5De14565ca8A29Fe93', tokens: [ADDRESSES.bsc.USDT,]},
+  arbitrum: { owner: '0x14559479DC1041Ef6565f44028D454F423d2b9E6', tokens: [ADDRESSES.arbitrum.USDT,]},
+}
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = {
+    tvl: sumTokensExport(config[chain])
+  }
+})
