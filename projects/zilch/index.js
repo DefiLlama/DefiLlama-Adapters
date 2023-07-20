5f6ff1ddc1b8c13ac23589ee7a6f978bfb53d4a2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-20 23:27:42 +0200

    refactor code

--- projects/zilch/index.js
+++ projects/zilch/index.js
@@ -1,57 +1,21 @@
-const sdk = require('@defillama/sdk')
 const abi = require('./abi')
-const { getChainTransform } = require('../helper/portedTokens')
 const { sumTokens2 } = require('../helper/unwrapLPs')
-const chain = 'era'
-
 const farm = "0xa65D04f79633BeBdC4Dd785498269e8ABD6A1476"
 
-async function tvl(_, _b, {[chain]: block }, { api }) {
-
-  let { output: poolLength } = await sdk.api.abi.multiCall({
-    abi: abi.poolLength,
-    calls: [{ target: farm}],
-    chain, block,
-  })
-  poolLength = poolLength[0].output
-
-  let poolAssets = []
-  for(var i = 0; i < poolLength; i ++) {
-    poolAssets.push(i)
-  }
-
-  let { output: poolInfos } = await sdk.api.abi.multiCall({
-    abi: abi.poolInfo,
-    calls: poolAssets.map((item) => {return { target: farm, params: item}}),
-    chain, block,
-  })
-
-  poolAssets = poolInfos.map((item) => {return item.output.lpToken})
-  let { output: tokens } = await sdk.api.abi.multiCall({
-    abi: abi.underlyingToken,
-    calls: poolAssets.map((item, index) => {if(index < 3) return { target: item}}).filter((item) => {return item}),
-    chain, block,
-  })
-
-  const tokenAndOwner = []
-
-  for(var i = 0; i < tokens.length; i++) {
-    tokenAndOwner.push([tokens[i].output, poolAssets[i]])
-  }
-  const output = await sumTokens2({api, tokensAndOwners: tokenAndOwner})
-
-  const valutsToken = await api.multiCall({ abi: "uint256:balanceOf", calls: poolAssets.map((item, index) => {if(index >= 3) return { target: item}}).filter((item) => {return item}), })
-  const lps = await api.multiCall({ abi: "address:want", calls: poolAssets.map((item, index) => {if(index >= 3) return { target: item}}).filter((item) => {return item}), })
-  valutsToken.forEach((data, i) => {
-    sdk.util.sumSingleBalance(output, lps[i], data, api.chain)
-  })
-
-  return output
-
+async function tvl(_, _b, _cb, { api }) {
+  const poolInfos = await api.fetchList({ lengthAbi: abi.poolLength, itemAbi: abi.poolInfo, target: farm })
+  let pools = poolInfos.map(i => i.lpToken)
+  const stakingPools = pools.slice(0, 3)
+  pools = pools.slice(3)
+  const stakingTokens = await api.multiCall({ abi: abi.underlyingToken, calls: stakingPools })
+  const tokens = await api.multiCall({ abi: 'address:want', calls: pools })
+  const bals = await api.multiCall({ abi: 'uint256:balanceOf', calls: pools })
+  api.addTokens(tokens, bals)
+  return sumTokens2({ api, tokensAndOwners2: [stakingTokens, stakingPools] })
 }
 
 module.exports = {
-  era: { 
+  era: {
     tvl,
   }
 }
