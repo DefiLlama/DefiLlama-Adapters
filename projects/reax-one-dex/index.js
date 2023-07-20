a9ddbb99f9cf7df551593bcf20393664e10089f7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-20 17:33:52 +0200

    track reax.one

---
+++ projects/reax-one-dex/index.js
@@ -0,0 +1,13 @@
+const { onChainTvl } = require('../helper/balancer')
+
+module.exports = {
+  mantle: {
+    tvl: onChainTvl('0x1AA7f1f5b51fe22478e683466232B5C8fc49407f', 10790, {
+      blacklistedTokens: [
+        '0xf7dfe223e19701a514e78f3ce7ba98f2c5fbb5b2',
+        '0xa84bdecd44e6cee1c588a3c97fcc4482831fde05',
+        '0x62959ad021402f48d0d8067bc5c4c03f63fceaa4',
+      ]
+    }),
+  }
+}
