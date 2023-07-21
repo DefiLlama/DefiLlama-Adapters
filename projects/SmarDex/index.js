7a2443c6b9f8ae0243a6011e97d4be3c8ce2ea7e
Esteban <gonzalesteb@gmail.com>
2023-07-21 08:42:58 +0200

    feat(smardex): update multichain tvl

--- projects/SmarDex/index.js
+++ projects/SmarDex/index.js
@@ -1,9 +1,45 @@
-const { getUniTVL, } = require('../helper/unknownTokens');
-const { staking } = require('../helper/staking')
+const { getUniTVL } = require("../helper/unknownTokens");
+const { stakings } = require("../helper/staking");
+
+const ethereumPairsTvl = getUniTVL({
+  chain: "ethereum",
+  factory: "0x7753F36E711B66a0350a753aba9F5651BAE76A1D",
+  fetchBalances: true,
+});
+const polygonPairsTvl = getUniTVL({
+  factory: "0x9A1e1681f6D59Ca051776410465AfAda6384398f",
+  chain: "polygon",
+  fetchBalances: true,
+});
+const bscPairsTvl = getUniTVL({
+  factory: "0xA8EF6FEa013034E62E2C4A9Ec1CDb059fE23Af33",
+  chain: "bsc",
+  fetchBalances: true,
+});
+const arbitrumPairsTvl = getUniTVL({
+  factory: "0x41A00e3FbE7F479A99bA6822704d9c5dEB611F22",
+  chain: "arbitrum",
+  fetchBalances: true,
+});
 
 module.exports = {
   ethereum: {
-    tvl: getUniTVL({ factory: '0x7753F36E711B66a0350a753aba9F5651BAE76A1D',  abis: { getReserves: 'function getReserves() view returns (uint112 _reserve0, uint112 _reserve1)'}  }),
-    staking: staking('0xB940D63c2deD1184BbdE059AcC7fEE93654F02bf', '0x5de8ab7e27f6e7a1fff3e5b337584aa43961beef')
+    tvl: ethereumPairsTvl,
+    staking: stakings(
+      [
+        "0xB940D63c2deD1184BbdE059AcC7fEE93654F02bf",
+        "0x80497049b005Fd236591c3CD431DBD6E06eB1A31",
+      ],
+      "0x5de8ab7e27f6e7a1fff3e5b337584aa43961beef"
+    ),
   },
-}
+  polygon: {
+    tvl: polygonPairsTvl,
+  },
+  arbitrum: {
+    tvl: arbitrumPairsTvl,
+  },
+  bsc: {
+    tvl: bscPairsTvl,
+  },
+};
