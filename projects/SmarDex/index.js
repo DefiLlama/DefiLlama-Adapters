33b1569cb6bd0215b7d1f97f3ab4856d16c7f9b0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-21 09:21:24 +0200

    code refactor

--- projects/SmarDex/index.js
+++ projects/SmarDex/index.js
@@ -1,45 +1,18 @@
 const { getUniTVL } = require("../helper/unknownTokens");
 const { stakings } = require("../helper/staking");
 
-const ethereumPairsTvl = getUniTVL({
-  chain: "ethereum",
-  factory: "0x7753F36E711B66a0350a753aba9F5651BAE76A1D",
-  fetchBalances: true,
-});
-const polygonPairsTvl = getUniTVL({
-  factory: "0x9A1e1681f6D59Ca051776410465AfAda6384398f",
-  chain: "polygon",
-  fetchBalances: true,
-});
-const bscPairsTvl = getUniTVL({
-  factory: "0xA8EF6FEa013034E62E2C4A9Ec1CDb059fE23Af33",
-  chain: "bsc",
-  fetchBalances: true,
-});
-const arbitrumPairsTvl = getUniTVL({
-  factory: "0x41A00e3FbE7F479A99bA6822704d9c5dEB611F22",
-  chain: "arbitrum",
-  fetchBalances: true,
-});
+const config = {
+  ethereum: '0x7753F36E711B66a0350a753aba9F5651BAE76A1D',
+  bsc: '0xA8EF6FEa013034E62E2C4A9Ec1CDb059fE23Af33',
+  polygon: '0x9A1e1681f6D59Ca051776410465AfAda6384398f',
+  arbitrum: '0x41A00e3FbE7F479A99bA6822704d9c5dEB611F22',
+}
 
-module.exports = {
-  ethereum: {
-    tvl: ethereumPairsTvl,
-    staking: stakings(
-      [
-        "0xB940D63c2deD1184BbdE059AcC7fEE93654F02bf",
-        "0x80497049b005Fd236591c3CD431DBD6E06eB1A31",
-      ],
-      "0x5de8ab7e27f6e7a1fff3e5b337584aa43961beef"
-    ),
-  },
-  polygon: {
-    tvl: polygonPairsTvl,
-  },
-  arbitrum: {
-    tvl: arbitrumPairsTvl,
-  },
-  bsc: {
-    tvl: bscPairsTvl,
-  },
-};
+Object.keys(config).forEach(chain => {
+  const factory = config[chain]
+  module.exports[chain] = {
+    tvl: getUniTVL({ factory, fetchBalances: true, })
+  }
+})
+
+module.exports.ethereum.staking = stakings(["0xB940D63c2deD1184BbdE059AcC7fEE93654F02bf", "0x80497049b005Fd236591c3CD431DBD6E06eB1A31",], "0x5de8ab7e27f6e7a1fff3e5b337584aa43961beef")
