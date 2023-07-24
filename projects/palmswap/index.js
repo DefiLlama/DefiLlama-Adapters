3eee9b4e6ac4f7883fab29ec2055a598d01643d4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-24 07:44:28 +0200

    code refactor

--- projects/palmswap/index.js
+++ projects/palmswap/index.js
@@ -1,44 +1,14 @@
-const { request } = require("graphql-request");
-
-const subgraph =
-  "https://api.thegraph.com/subgraphs/name/palmswap/synthetic-raw-mainnet";
-
-async function fetchTotalStaked() {
-  const graphQLTVL = `
-          {
-            stakedTotals {
-              palmStakedTotal
-              plpStakedTotal
-            }
-          }
-        `;
-
-  const res = await request(subgraph, graphQLTVL);
-  const totalPALMStaked = res.stakedTotals[0]?.palmStakedTotal;
-
-  return {
-    PALM: totalPALMStaked,
-  };
-}
-
-async function staked(_, _1, _2) {
-  const { PALM } = await fetchTotalStaked();
-
-  const palmFormatted = PALM / 1e18;
-  return palmFormatted;
-}
-
 const { gmxExports } = require("../helper/gmx");
 const { staking } = require("../helper/staking");
 
 const palmswapVault = "0x806f709558CDBBa39699FBf323C8fDA4e364Ac7A"; //Vault
 
-const palmTokenStakingContract = "0xF14fDA6c354b5Ec53027F131f2e8fBDD192A3095"; //RewardRouter
+const palmTokenStakingContract = "0x95fc6f7df412040a815494cf27fbc82be6c7585c"; //RewardTracker
 const palmToken = "0x29745314B4D294B7C77cDB411B8AAa95923aae38"; //PALM token address
 
 module.exports = {
   bsc: {
-    tvl: gmxExports({ vault: palmswapVault }), //works
-    staking: staking(palmTokenStakingContract, palmToken), // does not work
+    tvl: gmxExports({ vault: palmswapVault }),
+    staking: staking(palmTokenStakingContract, palmToken),
   },
 };
