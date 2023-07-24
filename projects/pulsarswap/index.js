855a5c99ab35b1fa579a60f4cc06cbecf9185e88
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-24 09:08:53 +0200

    pulsar: track mantle

--- projects/pulsarswap/index.js
+++ projects/pulsarswap/index.js
@@ -2,6 +2,7 @@ const { sumTokens2 } = require('../helper/unwrapLPs')
 const config = {
   arbitrum: '0x336a2f76d2be24e7cb6f468665a4277d4d617d00',
   ethereum: '0x408f66057163d829a30d4d466092c6b0eebb692f',
+  mantle: '0xB5B03706C24c79D3F7a368b30562a1711d74F688',
 }
 
 module.exports = {};
