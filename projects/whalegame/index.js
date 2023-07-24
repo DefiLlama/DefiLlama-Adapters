5b3ee7d79664509abecea1ae0db5268215e0323e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-24 07:40:06 +0200

    code refactor

--- projects/whalegame/index.js
+++ projects/whalegame/index.js
@@ -1,61 +1,26 @@
-const abi = require("./abi.json");
-const sdk = require("@defillama/sdk");
+
 const ADDRESSES = require("../helper/coreAssets.json");
 
 const GAME_CONTRACT_ADDRESS = "0xF1baf16Db25405856f5379246Beba2B694e1449D";
 
 async function tvl(_, _1, _2, { api }) {
-  const whaleTokenAddress = await api.call({
-    abi: abi.whaleToken,
-    target: GAME_CONTRACT_ADDRESS,
-  });
+  const whaleTokenAddress = await api.call({ abi: abi.whaleToken, target: GAME_CONTRACT_ADDRESS, });
+  const owners = [GAME_CONTRACT_ADDRESS, whaleTokenAddress]
 
-  //Count amount in Game Pot Contract
-  const ethBalance = await sdk.api.eth.getBalance({
-    target: GAME_CONTRACT_ADDRESS,
-    chain: "ethereum",
-  });
-  api.add(ADDRESSES.null, ethBalance.output);
-
-  //Count amount in WHALE Rewards Contract
-  const ethBalanceInWhaleToken = await sdk.api.eth.getBalance({
-    target: whaleTokenAddress,
-    chain: "ethereum",
-  });
-
-  const currentRound = await api.call({
-    abi: abi.round,
-    target: GAME_CONTRACT_ADDRESS,
-  });
-
-  const round = currentRound;
-  const roundsComplete = Array.from({ length: round - 1 }, () => 0);
-
-  //Account for balances in vesting contracts that have not yet been claimed
-  await Promise.all(
-    roundsComplete.map(async (_, i) => {
-      const vestingContract = await api.call({
-        abi: abi.vestingContractforRound,
-        target: GAME_CONTRACT_ADDRESS,
-        params: [i + 1],
-      });
-
-      const ethBalanceInVestingContract = await sdk.api.eth.getBalance({
-        target: vestingContract,
-        chain: "ethereum",
-      });
-      api.add(ADDRESSES.null, ethBalanceInVestingContract.output);
-    })
-  );
-
-  api.add(ADDRESSES.null, ethBalanceInWhaleToken.output);
+  owners.push(...await api.fetchList({ itemAbi: abi.vestingContractforRound, lengthAbi: abi.round, target: GAME_CONTRACT_ADDRESS, }))
+  return api.sumTokens({ tokens: [ADDRESSES.null], owners, blacklistedOwners: [ADDRESSES.null] })
 }
+
 module.exports = {
-  timetravel: false,
-  misrepresentedTokens: false,
   methodology:
     "Counts the amount of ETH in the Game Pot, WHALE Rewards Contract and unclaimed ETH in vesting contracts",
   ethereum: {
     tvl,
   },
 };
+
+const abi = {
+  "vestingContractforRound": "function vestingContractForRound(uint256) view returns (address)",
+  "round": "uint256:round",
+  "whaleToken": "address:whaleToken"
+}
