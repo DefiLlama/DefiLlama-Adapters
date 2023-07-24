51fdeaaa3fdbbb15bf7558dafc30bf6d66ef16df
yashvik <39464729+waverune@users.noreply.github.com>
2023-07-24 19:40:52 +0530

    Timeswap: zkevm chain added (#6893)
    * timeswap: adds mantle chain
    
    * update: polygon_zkevm chain added

--- projects/timeswap-v2/index.js
+++ projects/timeswap-v2/index.js
@@ -80,6 +80,10 @@ const config = {
   mantle: {
     factory: "0xf8F5e4B7825d484FBDFDC36fc915E79f30b02f9E",
     fromBlock: 3563,
+  },
+  polygon_zkevm: {
+    factory: "0x406d3Dfcbe20b642c2262b29b960822975371502",
+    fromBlock: 1787343,
   }
 };
 
