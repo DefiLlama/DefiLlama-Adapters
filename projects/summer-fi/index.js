5a8049d9b7de9bc82d24979955b5275971ab257e
Marcin Ciarka <marcin@oazoapps.com>
2023-07-25 14:49:31 +0200

    ☀️ Summer.fi - adapter fixes (#6907)
    * new confirmedSummerFiMakerVaults handling, change the MCDView contract
    
    * sync

--- projects/summer-fi/index.js
+++ projects/summer-fi/index.js
@@ -1,33 +1,33 @@
 const { automationTvl, dpmPositions, makerTvl } = require("./handlers");
 const { getAutomationCdpIdList, setCallCache } = require("./helpers");
-const sdk = require('@defillama/sdk')
+const sdk = require("@defillama/sdk");
 const { getConfig, getCache, setCache } = require("../helper/cache");
 const { endpoints } = require("./constants/endpoints");
 
 module.exports = {
   doublecounted: true,
-  ethereum: { tvl, },
+  ethereum: { tvl },
 };
 
 async function tvl(_, _1, _2, { api }) {
-  await api.getBlock()
+  await api.getBlock();
   const executionStart = Date.now() / 1000;
-  const [ confirmedSummerFiMakerVaults, cdpIdList, cache ] = await Promise.all([
-    getConfig('summer-fi/maker-vaults', endpoints.makerVaults),
+  const [confirmedSummerFiMakerVaults, cdpIdList, cache] = await Promise.all([
+    await getConfig("summer-fi/maker-vaults", endpoints.makerVaults),
     getAutomationCdpIdList({ api }),
-    getCache('summer-fi/cache', api.chain),
+    getCache("summer-fi/cache", api.chain),
   ]);
 
-  setCallCache(cache)
+  setCallCache(cache);
 
-  sdk.log([...cdpIdList].length, 'cdpIdList')
+  sdk.log([...cdpIdList].length, "cdpIdList");
 
   await Promise.all([
     dpmPositions({ api }),
     automationTvl({ api, cdpIdList }),
-    makerTvl({ api, cdpIdList, confirmedSummerFiMakerVaults, }),
+    makerTvl({ api, cdpIdList, confirmedSummerFiMakerVaults }),
   ]);
 
-  await setCache('summer-fi/cache', api.chain, cache);
+  await setCache("summer-fi/cache", api.chain, cache);
   sdk.log("Execution time", Date.now() / 1000 - executionStart, "seconds");
 }
