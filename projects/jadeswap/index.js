ea5138771e0b3f5dbeb3795f93903f38a26b44fe
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-25 19:19:12 +0200

    minor fix

--- projects/jadeswap/index.js
+++ projects/jadeswap/index.js
@@ -4,6 +4,7 @@ const sdk = require("@defillama/sdk");
 const uniTvl = getUniTVL({
   factory: "0x210D0c3d885C5D296e8F32Ad4B7a9a2Fe39E983b",
   useDefaultCoreAssets: true,
+  fetchBalances: true,
 });
 const masterchef = "0xcC507803A4b832684154C4E395D92A6EDbEAfE52";
 const jade = "0x7c70229F108D3d506Cff8Ea243FFA57344Fc4cE1";
