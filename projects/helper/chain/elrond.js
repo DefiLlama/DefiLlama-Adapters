559aec88ed35e4cfea3d2d88818a22b8a9c9b716
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-25 18:48:26 +0200

    jewelswap: code refactor

--- projects/helper/chain/elrond.js
+++ projects/helper/chain/elrond.js
@@ -67,11 +67,16 @@ async function sumTokens({ owners = [], tokens = [], balances = {}, blacklistedT
       const bal = await getElrondBalance(i)
       sdk.util.sumSingleBalance(balances, nullAddress, bal, chain)
     }))
-  console.log(balances)
   return transformBalances(chain, balances)
+}
+
+async function getNFTs(address) {
+  const res = await get(`${getEnv('MULTIVERSX_RPC')}/accounts/${address}/nfts?size=1000`)
+  return res
 }
 
 module.exports = {
   sumTokens,
   call,
+  getNFTs,
 }
--- projects/jewelswap-nft/index.js
+++ projects/jewelswap-nft/index.js
@@ -1,21 +1,11 @@
-const { toUSDTBalances } = require("../helper/balances");
-const { sumTokens } = require("../helper/chain/elrond");
-const { get } = require("../helper/chain/elrond");
-const axios = require("axios");
-const BigNumber = require("bignumber.js");
 const { ResultsParser, AbiRegistry, SmartContract, Address, OptionalValue } = require("@multiversx/sdk-core/out");
 const { ProxyNetworkProvider } = require("@multiversx/sdk-network-providers/out");
-const JEWEL_ONEDEX_FARM_SC_ABI = require("../jewelswap-lev-farming/jewel-onedex-farm.abi.json");
 const NFT_LENDING_SC_ABI_JSON = require("./jewel-nft-lending.abi.json");
-const { sleep } = require("../helper/utils");
+const { sumTokens } = require("../helper/chain/elrond");
+const { nullAddress } = require("../helper/tokenMapping");
 
-const JEWEL_ONEDEX_FARM_SC_ADDRESS = "erd1qqqqqqqqqqqqqpgqm7exdla3rzshywy99pvlxzkr45wt9kjsdfys7qqpn0";
 
 const LENDING_POOL = "erd1qqqqqqqqqqqqqpgqhpauarfmx75nf4pwxh2fuy520ym03p8e8jcqt466up";
-const LENDING_POOL_FARMS = "erd1qqqqqqqqqqqqqpgq96n4gxvmw8nxgxud8nv8qmms5namspc5vmusg930sh";
-const FARMS = "erd1qqqqqqqqqqqqqpgqlnxy2hmvs8qxr6ezq2wmggn7ev62cjp6vmusvdral4";
-const ASHSWAP_STAKE = "erd1qqqqqqqqqqqqqpgqhw2s04kx5crn2yvx5p253aa8fmganjjqdfysjvnluz";
-const LIQUID_STAKE = "erd1qqqqqqqqqqqqqpgqx6833qjac6uqztgsa8jhlztexucke0hrdfys6wd7qt";
 
 const abi = AbiRegistry.create(NFT_LENDING_SC_ABI_JSON);
 const nftLendingSmartContract = new SmartContract({
@@ -23,40 +13,7 @@ const nftLendingSmartContract = new SmartContract({
   abi: abi
 });
 
-const ASHSWAP_API_GRAPHQL_URL = "https://api-v2.ashswap.io/graphql";
-const ASHSWAP_API_GRAPHQL_PARAM = {
-  "query": "\n                query ashBaseStateQuery($accAddress: String = \"\") {\n                    farms(address: $accAddress) {\n                        address\n                        farmToken {\n                            ...allTokenProps\n                        }\n                        rewardToken {\n                            ...allTokenProps\n                        }\n                        farmingToken {\n                            ...allTokenProps\n                        }\n                        farmTokenSupply\n                        rewardPerSec,\n                        rewardPerShare\n                        state\n                        lastRewardBlockTs\n                        divisionSafetyConstant\n                        farmingTokenBalance\n                        produceRewardEnabled\n                        account {\n                            slopeBoosted\n                        }\n                        shard\n                 }\n                    pools {\n                        address\n                        lpToken {\n                            ...allTokenProps\n                        }\n                        tokens {\n                            ...allTokenProps\n                        }\n                        reserves\n                        totalSupply\n                        swapFeePercent\n                        adminFeePercent\n                        ampFactor\n                        state\n                    }\n                    poolsV2 {\n                        address\n                        lpToken {\n                            ...allTokenProps\n                        }\n                        totalSupply\n                        reserves\n                        priceScale\n                        ampFactor\n                        gamma\n                        xp\n                        futureAGammaTime\n                        d\n                        midFee\n                        outFee\n                        feeGamma\n                    }\n                    tokens {\n                        ...allTokenProps\n                    }\n                    votingEscrows(address: $accAddress) {\n                        address\n                        lockedToken {\n                            ...allTokenProps\n                        }\n                        totalLock\n                        veSupply\n                        account {\n                            locked {\n                                amount\n                                end\n                            }\n                        }\n                    }\n                    feeDistributor(address: $accAddress) {\n                        address\n                        rewardToken {\n                            ...allTokenProps\n                        }\n                        account {\n                            reward\n                        }\n                    }\n                    blockchain {\n                        blockShards {\n                            shard\n                            nonce\n                        }\n                    }\n\n                    \n\n\n\n\n\n\n        ashSupply\n    \n\n\n\n        rewarder {\n            address\n            rewardPerSec\n        }\n    \n                }\n\n                fragment allTokenProps on Token {\n                    id\n                    price\n                }\n            ",
-  "variables": {
-    "accAddress": "erd1f0dwxpl3vxe936cla2mkky7nym4g3xn4vgfz497dpupqul8uktzshxqj5l"
-  },
-  "operationName": "ashBaseStateQuery"
-};
-const jewelOnedexFarmAbiRegistry = AbiRegistry.create(JEWEL_ONEDEX_FARM_SC_ABI);
-const jewelOnedexFarmSmartContract = new SmartContract({
-  address: new Address(JEWEL_ONEDEX_FARM_SC_ADDRESS),
-  abi: jewelOnedexFarmAbiRegistry
-});
-
-
 const networkConfigs = {
-  devnet: {
-    id: "devnet",
-    chainId: "D",
-    apiUrl: "https://devnet-api.multiversx.com",
-    gatewayUrl: "https://devnet-gateway.multiversx.com",
-    explorerUrl: "https://devnet-explorer.multiversx.com",
-    apiTimeout: 10000
-  },
-
-  testnet: {
-    id: "testnet",
-    chainId: "T",
-    apiUrl: "https://testnet-api.multiversx.com",
-    gatewayUrl: "https://testnet-gateway.multiversx.com",
-    explorerUrl: "https://testnet-explorer.multiversx.com",
-    apiTimeout: 10000
-  },
-
   mainnet: {
     id: "mainnet",
     chainId: "1",
@@ -80,93 +37,43 @@ module.exports = {
   misrepresentedTokens: true,
   timetravel: false,
   elrond: {
-    tvl: () => getNftsTvl()
+    tvl: () => sumTokens({ owners: [LENDING_POOL] }),
+    borrowed,
   }
 };
 
-async function getNftsTvl() {
-  const egldPrice = (await getPriceFromXex("WEGLD-bd4d79")).price;
+async function borrowed() {
+  const { api } = arguments[3]
   const commonSettings = await queryNFTDataFromSc();
-  let lendingTvl1InUsd = 0;
-  if (commonSettings) {
-    const lendingTvl = convertWeiToEsdt(new BigNumber(commonSettings.total_lending_amount).plus(commonSettings.total_collateral_amount));
-    lendingTvl1InUsd = lendingTvl.multipliedBy(new BigNumber(egldPrice));
-  } else {
-    // retrying once
-    await sleep(1000);
-    const commonSettings = await queryNFTDataFromSc();
-    if (commonSettings) {
-      const lendingTvl = convertWeiToEsdt(new BigNumber(commonSettings.total_lending_amount).plus(commonSettings.total_collateral_amount));
-      lendingTvl1InUsd = lendingTvl.multipliedBy(new BigNumber(egldPrice));
-    }
-  }
-  return toUSDTBalances(lendingTvl1InUsd);
+  api.add(nullAddress, commonSettings.total_loan_amount)
+
+  return api.getBalances()
 }
 
 async function queryNFTDataFromSc() {
+  const args = [
+    OptionalValue.newMissing()
+  ];
+  const interaction = nftLendingSmartContract.methodsExplicit.viewCommonSettings(args);
+  const query = interaction.check().buildQuery();
+  const queryResponse = await proxyProvider.queryContract(query);
+  const endpointDefinition = interaction.getEndpoint();
+  const {
+    firstValue,
+    returnCode,
+    returnMessage
+  } = new ResultsParser().parseQueryResponse(queryResponse, endpointDefinition);
 
-  try {
-    const args = [
-      OptionalValue.newMissing()
-    ];
-    const interaction = nftLendingSmartContract.methodsExplicit.viewCommonSettings(args);
-    const query = interaction.check().buildQuery();
-    const queryResponse = await proxyProvider.queryContract(query);
-    const endpointDefinition = interaction.getEndpoint();
-    const {
-      firstValue,
-      returnCode,
-      returnMessage
-    } = new ResultsParser().parseQueryResponse(queryResponse, endpointDefinition);
-
-    if (!firstValue || !returnCode.isSuccess()) {
-      console.error(returnMessage);
-      return undefined;
-    }
-
-    const value = firstValue.valueOf();
-    const decoded = {
-      total_lending_amount: value.total_lending_amount.toFixed(),
-      total_collateral_amount: value.total_collateral_amount.toFixed()
-    };
-    return decoded;
-  } catch (e) {
-    console.error(e);
+  if (!firstValue || !returnCode.isSuccess()) {
+    console.error(returnMessage);
     return undefined;
   }
+
+  const value = firstValue.valueOf();
+  const decoded = {
+    total_lending_amount: value.total_lending_amount.toFixed(),
+    total_collateral_amount: value.total_collateral_amount.toFixed(),
+    total_loan_amount: value.total_loan_amount.toFixed(),
+  };
+  return decoded;
 }
-
-async function getPriceFromXex(identifier) {
-  const configUrl = `https://api.multiversx.com/mex/tokens/${identifier}`;
-  try {
-    const { data } = await axios.get(configUrl);
-    return data;
-  } catch (e) {
-    return null;
-  }
-}
-
-function getTokenDecimal(tokenId) {
-  console.log(tokenId);
-  if (tokenId === "EGLD") {
-    return 18;
-  }
-
-  if (tokenId.includes("-")) {
-    const tokenTicker = tokenId.split("-")[0];
-
-    if (tokenTicker === "WEGLD") return 18;
-    if (tokenTicker === "USDT") return 6;
-    if (tokenTicker === "USDC") return 6;
-    if (tokenTicker === "BUSD") return 18;
-    if (tokenTicker === "ASH") return 18;
-    if (tokenTicker === "ALP") return 18;
-    if (tokenTicker === "JWLASH") return 18;
-  }
-}
-
-function convertWeiToEsdt(amount, decimals, precision) {
-  if (!amount) amount = "0";
-  return new BigNumber(amount).shiftedBy(decimals == null ? -18 : -decimals).decimalPlaces(precision == null ? 4 : precision, BigNumber.ROUND_FLOOR);
-}
-
