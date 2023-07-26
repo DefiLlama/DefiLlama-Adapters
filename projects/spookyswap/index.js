4b72b0a5392dcaf2c5b7f7db435b293b0a8d92a4
franz <franzns11@gmail.com>
2023-07-26 16:44:30 +0200

    don't use default core assets

--- projects/spookyswap/index.js
+++ projects/spookyswap/index.js
@@ -2,6 +2,6 @@ const { getUniTVL } = require("../helper/unknownTokens")
 module.exports={
   misrepresentedTokens: true,
   fantom:{
-      tvl: getUniTVL({ useDefaultCoreAssets: true, factory: '0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3' }),
+      tvl: getUniTVL({ useDefaultCoreAssets: false, factory: '0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3' }),
   },
 }  
