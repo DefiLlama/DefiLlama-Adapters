79e705f935184b6b9b137916e9a01de83e18af38
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-27 15:28:36 +0200

    minor fix

--- projects/salsa/index.js
+++ projects/salsa/index.js
@@ -2,11 +2,10 @@ const { call } = require("../helper/chain/elrond");
 
 const ADDRESSES = require('../helper/coreAssets.json');
 
-const tvl = async () => {
+const tvl = async (_, _1, _2, { api }) => {
   const tokenPrice = await call({ target: 'erd1qqqqqqqqqqqqqpgqaqxztq0y764dnet95jwtse5u5zkg92sfacts6h9su3', abi: 'getTokenPrice', responseTypes: ['number'] });
   const lsTokenSupply = await call({ target: 'erd1qqqqqqqqqqqqqpgqaqxztq0y764dnet95jwtse5u5zkg92sfacts6h9su3', abi: 'getLiquidTokenSupply', responseTypes: ['number'] });
-
-  return { ['elrond:' + ADDRESSES.null]: (tokenPrice * lsTokenSupply) / 1000000000000000000 };
+  api.add(ADDRESSES.null, tokenPrice * lsTokenSupply / 1e18)
 };
 
 module.exports = {
