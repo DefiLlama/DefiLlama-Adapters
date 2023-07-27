5eda51ca7612e4b9ad1afd9080ccba1c56fab91e
Aaron Hunt <aahunter1990@gmail.com>
2023-07-27 12:01:34 +0300

    Tegisto dex factory address updates

--- projects/tegisto/index.js
+++ projects/tegisto/index.js
@@ -3,7 +3,10 @@ const { getUniTVL } = require("../helper/unknownTokens")
 
 module.exports = {
   kava: {
-    tvl: getUniTVL({ chain: 'kava', factory: '0x07fA706528c0bb721327798B5686B620BCcf5b99', useDefaultCoreAssets: true, fetchBalances: true }), 
+    tvl: getUniTVL({ chain: 'kava', factory: '0xfdF832fe60a5145909e7b24Cff225830c1850917', useDefaultCoreAssets: true, fetchBalances: true }), 
     staking: staking('0x744Dd9f79b80437a9e5eb0292128045F51C48b6d', '0x87F1E00d6bcD3712031e5edD26DFcdB0FEd35D20', undefined, 'tegisto', 18),
+  },
+  celo: {
+    tvl: getUniTVL({ chain: 'celo', factory: '0x1FA136Ba715889B691305a687A0fbD82e6287A67', useDefaultCoreAssets: true, fetchBalances: true }),
   }
 }
