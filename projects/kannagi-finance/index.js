ba44815911ed60f030da79a8f8760e7f7b216323
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-31 10:24:39 +0200

    use token labels

--- projects/kannagi-finance/index.js
+++ projects/kannagi-finance/index.js
@@ -1,8 +1,9 @@
+const ADDRESSES = require('../helper/coreAssets.json')
 const abi = require("./abi.json");
 const {yieldHelper} = require('../helper/yieldHelper');
 const KANA_ADDRESS = "0x26aC1D9945f65392B8E4E6b895969b5c01A7B414";
 const YIELD_ADDRESS= "0x6E415ba5a37761256D13E84B45f4822c179DEF47";
-const USDT_ADDRESS = "era:0x493257fd37edb34451f62edf8d2a0c418852ba4c";
+const USDT_ADDRESS = "era:" + ADDRESSES.era.USDT;
 
 
 function yieldHelperWrapper(){
