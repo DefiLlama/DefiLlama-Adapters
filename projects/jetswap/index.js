10d60289e475f8235cac7f7dfef6190cd7f54411
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-31 17:57:00 +0200

    fix jetswap

--- projects/jetswap/index.js
+++ projects/jetswap/index.js
@@ -1,5 +1,5 @@
-const sdk = require("@defillama/sdk");
 const { uniTvlExport } = require("../helper/calculateUniTvl");
+const { staking } = require('../helper/staking')
 
 const bscFactory = "0x0eb58E5c8aA63314ff5547289185cC4583DfCBD5";
 const polygonFactory = "0x668ad0ed2622C62E24f0d5ab6B6Ac1b9D2cD4AC7";
@@ -12,22 +12,6 @@ const WINGS_TOKEN_FANTOM = "0x3D8f1ACCEe8e263F837138829B6C4517473d0688";
 const MASTER_BSC = "0x63d6EC1cDef04464287e2af710FFef9780B6f9F5";
 const MASTER_POLYGON = "0x4e22399070aD5aD7f7BEb7d3A7b543e8EcBf1d85";
 const MASTER_FANTOM = "0x9180583C1ab03587b545629dd60D2be0bf1DF4f2";
-
-function staking(masterchef, token, chain) {
-  return async (_timestamp, _block, chainBlocks) => {
-    let balances = {};
-    let balance = (
-      await sdk.api.erc20.balanceOf({
-        target: token,
-        owner: masterchef,
-        block: chainBlocks[chain],
-        chain,
-      })
-    ).output;
-    sdk.util.sumSingleBalance(balances, `bsc:${WINGS_TOKEN_BSC}`, balance);
-    return balances;
-  };
-}
 
 module.exports = {
   bsc: {
