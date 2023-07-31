0a4ff0e1529ecb8ef1ebafefbd7a0a677661d02b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-07-31 20:30:27 +0200

    minor fix

--- projects/brine/index.js
+++ projects/brine/index.js
@@ -1,5 +1,5 @@
 const ADDRESSES = require("../helper/coreAssets.json");
-const { sumTokens2 } = require("../helper/unwrapLPs");
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
 const contracts = ["0x1390f521A79BaBE99b69B37154D63D431da27A07"];
 
@@ -8,13 +8,11 @@ const tokens = [
   ADDRESSES.ethereum.USDC,
   ADDRESSES.ethereum.USDT,
   ADDRESSES.ethereum.WBTC,
+  ADDRESSES.ethereum.MATIC,
+  '0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1'
 ];
-
-async function tvl(timestamp, block) {
-  return sumTokens2({ owner: contracts[0], tokens: tokens, block });
-}
 
 module.exports = {
   start: 1685817000,
-  ethereum: { tvl },
+  ethereum: { tvl: sumTokensExport({ owners: contracts, tokens, }) },
 };
