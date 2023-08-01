3e16bb64c074a84e4fb0dca99ccdbbaaa9d576d5
define <Lovelovenow@protonmail.com>
2023-08-01 13:29:47 +0100

    add base chain to oasisswap

--- projects/oasisswapdex/index.js
+++ projects/oasisswapdex/index.js
@@ -13,4 +13,8 @@ module.exports = {
     ]),
     staking: staking("0x73c1fb66b4e183bc101b98d4c17431b667d85958", "0x602eb0d99a5e3e76d1510372c4d2020e12eaea8a", "arbitrum")
   },
-}; // node test.js projects/oasisswapdex/index.js
+  base: {
+    tvl: sdk.util.sumChainTvls([
+      getUniTVL({ factory: '0xc8126578093366968199707b7f8edff258f473b6', chain: 'base', useDefaultCoreAssets: true }),
+    ]),
+} }// node test.js projects/oasisswapdex/index.js
