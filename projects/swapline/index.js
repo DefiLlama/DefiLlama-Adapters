ee1378bbf5ae165997c4dc4dc9579e3c56f09452
0xchiado <128964553+0xchiado@users.noreply.github.com>
2023-08-02 08:54:02 +0200

    Added new chain Base (#6980)
    * test
    
    * added SwapLine adapter
    
    * added SwapLine adapter
    
    * added new Fantom factory address; added Optimism Factory details
    
    * added new Base factory address

--- projects/swapline/index.js
+++ projects/swapline/index.js
@@ -5,8 +5,8 @@ const factories = {
   optimism: '0xd08C98F6409fCAe3E61f3157B4147B6595E60cf3',
   polygon_zkevm: '0x5A5c0C4832828FF878CE3ab4fEc44d21200b1496',
   arbitrum: '0xEE0616a2DEAa5331e2047Bc61E0b588195A49cEa',
+  base: '0x5A5c0C4832828FF878CE3ab4fEc44d21200b1496',
 }
-
 async function tvl(_, _b, _cb, { api, }) {
   let blacklistedTokens = []
   if (api.chain === 'fantom') blacklistedTokens = ['0xdc6ff44d5d932cbd77b52e5612ba0529dc6226f1']
@@ -35,7 +35,8 @@ module.exports = {
   hallmarks: [
     [1682298000,"Launch on Optimism"],
     [1687827600,"Launch on Polygon zkEVM"],
-    [1689037200,"Launch on Arbitrum"]
+    [1689037200,"Launch on Arbitrum"],
+    [1690848000,"Launch on Base"]
   ],
   methodology: 'We count the token balances in in different liquidity book contracts',
   fantom:{
@@ -48,6 +49,9 @@ module.exports = {
     tvl,
   },
   arbitrum:{
+    tvl,
+  },
+  base:{
     tvl,
   },
 };
