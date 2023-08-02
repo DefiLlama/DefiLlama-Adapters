0e79a746c8d87e29659269db5f2f10b649b8ac02
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-02 08:43:38 +0200

    minor fix

--- projects/gambit/index.js
+++ projects/gambit/index.js
@@ -1,28 +1,12 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokens2 } = require("../helper/unwrapLPs");
-const { staking } = require("../helper/staking");
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
 const SimpleGToken = "0x0729e806f57CE71dA4464c6B2d313E517f41560b"; // SimpleGToken
-
-const tokens = {
-    era: {
-        USDC: ADDRESSES.era.USDC
-    },
-};
-
-async function eraTvl(_, _b, _cb, { api }) {
-    // timestamp, block, chainBlocks, chainApi
-    const tokensAndOwners = [
-        [tokens.era.USDC, SimpleGToken],
-    ];
-    return sumTokens2({ api, tokensAndOwners });
-}
 
 module.exports = {
     methodology: `Count the USDC that has been deposited on Gambit`,
     era: {
-        // staking: staking('contract', tokens.era.CNG),
-        tvl: eraTvl,
+        tvl: sumTokensExport({ owner: SimpleGToken, tokens: [ADDRESSES.era.USDC], }),
     },
 };
 
