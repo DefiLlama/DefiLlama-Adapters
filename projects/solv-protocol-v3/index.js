b2d61f98924729b1d2c7297e6e6803e2242e5762
buchaoqun <bu736554084@163.com>
2023-08-03 00:15:50 +0800

    update the graph arbitrum url

--- projects/solv-protocol-v3/index.js
+++ projects/solv-protocol-v3/index.js
@@ -6,7 +6,7 @@ const { cachedGraphQuery } = require("../helper/cache");
 const graphUrlList = {
   ethereum: 'https://api.studio.thegraph.com/query/40045/solv-payable-factory-prod/version/latest',
   bsc: 'https://api.thegraph.com/subgraphs/name/slov-payable/solv-v3-earn-factory',
-  arbitrum: 'https://api.studio.thegraph.com/query/29232/test-3/version/latest',
+  arbitrum: 'https://api.studio.thegraph.com/query/40045/solv-payable-factory-arbitrum/version/latest',
 }
 
 const filterSlot = [
