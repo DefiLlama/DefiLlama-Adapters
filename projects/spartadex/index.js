fd59e079d06f56948166bdd2afd356d6c255753d
define <Lovelovenow@protonmail.com>
2023-08-03 15:03:09 +0100

    adapter spartadex

---
+++ projects/spartadex/index.js
@@ -0,0 +1,6 @@
+const { getUniTVL } = require('../helper/unknownTokens')
+
+module.exports = {
+  misrepresentedTokens: true,
+  arbitrum: { tvl: getUniTVL({ factory: '0xFe8EC10Fe07A6a6f4A2584f8cD9FE232930eAF55', useDefaultCoreAssets: true, fetchBalances: true, }), },
+}
