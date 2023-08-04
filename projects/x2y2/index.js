c71b90847e4d46bffc335675be7611752c82126d
aichitutu <1217033134@qq.com>
2023-08-04 16:23:37 +0800

    add x2y2 lending v3 contract

--- projects/x2y2/index.js
+++ projects/x2y2/index.js
@@ -8,10 +8,11 @@ const X2Y2_staking = "0xb329e39ebefd16f40d38f07643652ce17ca5bac1";
 
 // Lending vaults
 const XY3_v1 = "0xC28F7Ee92Cd6619e8eEC6A70923079fBAFb86196";
-const XY3_v2 = "0xFa4D5258804D7723eb6A934c11b1bd423bC31623"
+const XY3_v2 = "0xFa4D5258804D7723eb6A934c11b1bd423bC31623";
+const XY3_V3 = "0xb81965ddfdda3923f292a47a1be83ba3a36b5133"
 
 async function tvl(timestamp, block, chainBlocks, { api }) {
-  return sumTokens2({ api, owners: [XY3_v1, XY3_v2], resolveNFTs: true})
+  return sumTokens2({ api, owners: [XY3_v1, XY3_v2, XY3_V3], resolveNFTs: true})
 }
 
 module.exports = {
