9e0846bdeab740ce57783b1cce93c5f4a97f5af1
Felix Philip <felixphilipk@gmail.com>
2023-08-05 20:02:28 +0530

    Update index.js

--- projects/gnoerli/index.js
+++ projects/gnoerli/index.js
@@ -1,7 +1,6 @@
 const ethers = require('ethers');
 const sdk = require('@defillama/sdk');
 
-
 // Assuming you have the ABI for both contracts
 const fs = require('fs');
 
@@ -10,8 +9,6 @@ const primaryIssueManagerABI = primaryIssueManagerData.abi;
 const secondaryIssueManagerData = JSON.parse(fs.readFileSync('./SecondaryIssueManager.json', 'utf8'));
 const secondaryIssueManagerABI = secondaryIssueManagerData.abi;
 
-
-// Define the addresses of the contracts
 const contracts = {
     goerli: {
         primary: '0x63D95635938857Ad202f0684dfd91dc71C9d111e',
@@ -23,26 +20,31 @@ async function tvl(timestamp, ethBlock, chainBlocks) {
     let totalTVL = ethers.BigNumber.from(0);
     const balances = {};
     const block = chainBlocks.goerli;
-   // Create a provider
-   const infuraProvider = new ethers.providers.JsonRpcProvider('https://goerli.infura.io/v3/324d7d968bb245e39b4edcda5a16c7a4');
-
+    const infuraProvider = new ethers.providers.JsonRpcProvider('https://goerli.infura.io/v3/324d7d968bb245e39b4edcda5a16c7a4');
 
     const primaryContract = new ethers.Contract(contracts.goerli.primary, primaryIssueManagerABI, infuraProvider);
     const secondaryContract = new ethers.Contract(contracts.goerli.secondary, secondaryIssueManagerABI, infuraProvider);
-  
-    // Fetch the 'subscription' events from the contract and calculate TVL based on those
-    const primaryEvents = await primaryContract.queryFilter('subscribers');
-    primaryEvents.forEach(event => {
+
+    const filter = primaryContract.filters.subscribers();
+    const startBlock = 0; 
+    const endBlock = 'latest'; 
+
+    const primaryEvents = await primaryContract.queryFilter(filter, startBlock, endBlock);
+    await Promise.all(primaryEvents.map(async event => {
         const cashSwapped = ethers.BigNumber.from(event.args.cashSwapped);
-        totalTVL= totalTVL.add(cashSwapped);
-        sdk.util.sumSingleBalance(balances, event.args.settlements, cashSwapped.toString());
-    });
-    const secondaryEvents = await secondaryContract.queryFilter('subscribers');
+        const currency = event.args.currency;
+        totalTVL = totalTVL.add(cashSwapped);
+        sdk.util.sumSingleBalance(balances, currency, cashSwapped.toString());
+    }));
+
+    const filterSecondary = secondaryContract.filters.subscribers();
+    const secondaryEvents = await secondaryContract.queryFilter(filterSecondary, startBlock, endBlock);
     secondaryEvents.forEach(event => {
         const amount = ethers.BigNumber.from(event.args.amount);
         totalTVL = totalTVL.add(amount);
-        sdk.util.sumSingleBalance(balances, event.args.settlements, amount.toString());
+        sdk.util.sumSingleBalance(balances, event.args.currencySettled, amount.toString());
     });
+    
     balances.totalTVL = totalTVL.toString();
     console.log('Primary events:', primaryEvents);
     console.log('Secondary events:', secondaryEvents);
@@ -57,4 +59,3 @@ module.exports = {
 tvl('testTimestamp', 'testEthBlock', { goerli: 'testBlock' })
   .then(result => console.log(result))
   .catch(error => console.error(error));
-
