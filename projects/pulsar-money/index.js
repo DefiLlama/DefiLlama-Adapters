e637f7e1c2adec13b363f221e8cd0dfdb35dd0b1
astrarizon <astrarizon@gmail.com>
2023-08-05 10:05:00 +0300

    added owner address

--- projects/pulsar-money/index.js
+++ projects/pulsar-money/index.js
@@ -1,12 +1,19 @@
 const { toUSDTBalances } = require("../helper/balances");
 const { get } = require("../helper/http");
+const ADDRESSES = require("../helper/coreAssets.json");
+const { sumTokens, sumTokensExport } = require("../helper/chain/elrond");
 
 const API_URL = "https://pulsar-money-prod.herokuapp.com/metrics/tvl";
 
 async function tvl() {
   const data = await get(API_URL);
 
-  return toUSDTBalances(data);
+  const tokensAndOwners = {
+    owners: ["erd1qqqqqqqqqqqqqpgq39rqpn2xvm0ykl2ccaa4h5zk5c9r647wdteswmap9l"],
+    balances: [data],
+  };
+
+  return sumTokens(tokensAndOwners);
 }
 
 module.exports = {
