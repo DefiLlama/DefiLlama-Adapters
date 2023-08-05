e6834216924a846f15f0de22af63d9c421ae5cf0
0xtekgrinder <72015889+0xtekgrinder@users.noreply.github.com>
2023-08-05 01:01:24 +0000

    feat: add paladin dullahan

---
+++ projects/paladinfinance-dullahan/index.js
@@ -0,0 +1,29 @@
+const sdk = require("@defillama/sdk");
+const abi = require("./abi.json");
+const { BigNumber } = require("ethers");
+const ADDRESSES = require('../helper/coreAssets.json')
+
+const DULLAHAN_VAULT = "0x167c606be99DBf5A8aF61E1983E5B309e8FA2Ae7";
+
+async function ethTvl(timestamp, block, _, { api },) {
+  const balances = {};
+
+  let totalAssets = await sdk.api.abi.call({
+    abi: abi["assets"],
+    block: block,
+    target: DULLAHAN_VAULT
+  });
+
+  sdk.util.sumSingleBalance(balances, ADDRESSES.ethereum.AAVE, totalAssets.output)
+  console.log(balances)
+
+  return balances;
+}
+
+module.exports = {
+  methodology: "Counts the total locked tokens inside the lockers contracts",
+  ethereum: {
+    tvl: ethTvl,
+  },
+  start: 17824291
+};
