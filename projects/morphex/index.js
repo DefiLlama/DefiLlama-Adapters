219a88af80ac7b8f4be7655061c80cc64d600721
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-07 15:24:23 +0530

    Morphex (#7047)
    * Add BSC for Morphex
    
    * Add new Fantom contract
    
    * merge listings
    
    * minor fix
    
    ---------
    
    Co-authored-by: daedboi <87483308+daedboi@users.noreply.github.com>

--- projects/morphex/index.js
+++ projects/morphex/index.js
@@ -1,10 +1,12 @@
 const { staking } = require("../helper/staking");
 const { gmxExports } = require("../helper/gmx");
+const sdk = require('@defillama/sdk')
 
-// Fantom
-const vaultAddressFantom = "0x3CB54f0eB62C371065D739A34a775CC16f46563e";
+// Fantom (New MLP with Axelar and LayerZero assets)
+const vaultAddressFantom = "0x245cD6d33578de9aF75a3C0c636c726b1A8cbdAa";
 const stakingAddressFantom = "0xa4157E273D88ff16B3d8Df68894e1fd809DbC007";
 const tokenAddressMPXFantom = "0x66eEd5FF1701E6ed8470DC391F05e27B1d0657eb";
+const vaultAddressFantomMultichain = "0x3CB54f0eB62C371065D739A34a775CC16f46563e";
 
 // BNB Chain
 const vaultAddressBSC = "0x46940Dc651bFe3F2CC3E04cf9dC5579B50Cf0765";
@@ -15,7 +17,7 @@ module.exports = {
     methodology: "Morphex liquidity is calculated by the value of tokens in the MLP pool. TVL also includes MPX staked.",
     fantom: {
         staking: staking(stakingAddressFantom, tokenAddressMPXFantom, "fantom"),
-        tvl: gmxExports({ vault: vaultAddressFantom })
+        tvl: sdk.util.sumChainTvls([gmxExports({ vault: vaultAddressFantom }), gmxExports({ vault: vaultAddressFantomMultichain })])
     },
     bsc: {
         staking: staking(stakingAddressBSC, tokenAddressMPXBSC, "bsc"),
