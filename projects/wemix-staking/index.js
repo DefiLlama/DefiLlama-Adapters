e3487144c2fba036cf3be1f4c0e8c32fba361310
define <Lovelovenow@protonmail.com>
2023-08-07 12:23:55 +0100

    add wemix new address

--- projects/wemix-staking/index.js
+++ projects/wemix-staking/index.js
@@ -1,11 +1,14 @@
 const ADDRESSES = require("../helper/coreAssets.json");
 const { staking } = require("../helper/staking");
+const { nullAddress } = require("../helper/unwrapLPs");
 
 const wwemix = ADDRESSES.wemix.WWEMIX;
-const stakingContract = "0x6F3f44B0Cf7C751f2a44Faf6bFdd08e499Eb0973";
+const stakingContractDIOS = "0x6F3f44B0Cf7C751f2a44Faf6bFdd08e499Eb0973";
+const stakingContractGRAND = "0xBEd789c6008F788a28fc222C83082D67033Daf7F"
+
 
 module.exports = {
   wemix: {
-    tvl: staking(stakingContract, wwemix),
+    tvl: staking([stakingContractDIOS, stakingContractGRAND],[wwemix, nullAddress]),
   },
 };
