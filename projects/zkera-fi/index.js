6cb79d7b17778a52fd5e0e9626f02fecb5df7325
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-07 15:29:57 +0200

    bugfix

--- projects/zkera-fi/index.js
+++ projects/zkera-fi/index.js
@@ -2,15 +2,13 @@ const { staking } = require("../helper/staking");
 const { gmxExports } = require("../helper/gmx");
 
 //ZKera
-const ZkVault = "0xBC918775C20959332c503d51a9251C2405d9cF88";
+const zkVault = "0xBC918775C20959332c503d51a9251C2405d9cF88";
 const zkStaking = "0xA258D1CfeCaDD96C763dfa50284525f1529cfB35";
 const zkZKE = "0x7b3e1236c39ddD2e61cF6Da6ac6D11193238ccB0";
 
-
 module.exports = {
- 
-  ZKera: {
-    staking: staking(zkStaking, zkZKE, "ZKera"),
+  era: {
+    staking: staking(zkStaking, zkZKE),
     tvl: gmxExports({ vault: zkVault })
- };
- 
+  }
+}
