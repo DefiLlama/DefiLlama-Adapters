37cdc4de6a7c5f7fe5f7ac031cd4a0878bdb837f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-07 16:16:22 +0200

    code refactor

--- projects/unlockd/helper/index.js
+++ projects/unlockd/helper/index.js
@@ -1,61 +1,30 @@
-const sdk = require("@defillama/sdk");
-const abi = require("./abi/abi"); 
+const abi = require("./abi/abi");
 const address = require("./addresses");
-const { sumTokens2 } = require("../../helper/unwrapLPs") 
-const BN = require('bignumber.js');
+const { sumTokens2 } = require("../../helper/unwrapLPs")
 
 async function tvl(chain, timestamp, chainBlocks, { api }) {
+  const tokens = await api.call({ target: address.ethereum.UNFTRegistry, abi: abi.UNFTRegistry.getUNFTAssetList, })
+  const owners = (await api.multiCall({  abi: 'function getUNFTAddresses(address) view returns (address proxy, address impl)', calls: tokens, target: address.ethereum.UNFTRegistry,})).map(i => i.proxy)
 
-    const [reservesData, uNFTAssetList] = await Promise.all([ 
-      api.call({
-        target: address.ethereum.UiPoolDataProvider,
-        params: [address.ethereum.LendPoolAddressProvider],
-        abi: abi.UiPoolDataProvider.getSimpleReservesData,
-      }),
-      api.call({
-        target: address.ethereum.UNFTRegistry,
-        abi: abi.UNFTRegistry.getUNFTAssetList,
-      }),
-    ]);
+  const reservesData = await api.call({
+    target: address.ethereum.UiPoolDataProvider,
+    params: [address.ethereum.LendPoolAddressProvider],
+    abi: abi.UiPoolDataProvider.getSimpleReservesData
+  })
 
-    const balances = await sumTokens2({ api, tokensAndOwners: address.uTokens });
+  reservesData.forEach(({ underlyingAsset, availableLiquidity }) => api.add(underlyingAsset, availableLiquidity))
 
-    reservesData.forEach(reserve => {
-      const availableLiquidity = new BN(reserve.availableLiquidity);
-      const totalVariableDebt = new BN(reserve.totalVariableDebt);
-      const sum = availableLiquidity.plus(totalVariableDebt);
-
-      sdk.util.sumSingleBalance(
-        balances, 
-        reserve.underlyingAsset, 
-        sum.toFixed(), 
-        api.chain
-      )
-    });
+  return sumTokens2({ api, tokensAndOwners2: [tokens, owners] });
 }
 
 async function borrowed(chain, timestamp, chainBlocks, { api }) {
-  const balances = {}
+  const reservesData = await api.call({
+    target: address.ethereum.UiPoolDataProvider,
+    params: [address.ethereum.LendPoolAddressProvider],
+    abi: abi.UiPoolDataProvider.getSimpleReservesData
+  })
 
-    const [reservesData] = await Promise.all([
-      api.call({
-        target: address.ethereum.UiPoolDataProvider,
-        params: [address.ethereum.LendPoolAddressProvider],
-        abi: abi.UiPoolDataProvider.getSimpleReservesData
-      })
-    ]);
-      
-    reservesData.map(
-      reserve => sdk.util.sumSingleBalance(
-        balances, 
-        reserve.underlyingAsset, 
-        reserve.totalVariableDebt, 
-        api.chain
-      )
-    )
-
-  return balances;
-
+  reservesData.forEach(({ underlyingAsset, totalVariableDebt }) => api.add(underlyingAsset, totalVariableDebt))
 }
 
 module.exports = {
--- projects/unlockd/index.js
+++ projects/unlockd/index.js
@@ -1,7 +1,6 @@
 const { tvl, borrowed } = require("./helper/index.js");
 
 module.exports = {
-    timetravel: true,
     methodology: `Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL`,
     ethereum: {
       tvl,
