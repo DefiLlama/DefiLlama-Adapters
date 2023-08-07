31da3bef0292bfe90925501671b145292b768b0c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-07 18:07:48 +0200

    minor fix

--- projects/lsdFil/index.js
+++ projects/lsdFil/index.js
@@ -1,15 +1,9 @@
 const MINT_TOKEN_CONTRACT = '0xcE5805CF6C84F71D2897f632E0Aa60d2430cCd2A';
 const MINT_CLUB_BOND_CONTRACT = '0x7187b3B1314375909B775d72fB7214Cb71a7D907';
-const {sumTokens2} = require('../helper/unwrapLPs');
+const { sumTokensExport } = require('../helper/unwrapLPs');
 
-
-  module.exports = {
-    filecoin: {
-        tvl: async (_, _1, _2, {api}) => {
-
-            let balances = {};
-            await sumTokens2({balances, owner: MINT_CLUB_BOND_CONTRACT, MINT_TOKEN_CONTRACT, api, });
-            return balances;
-        }
-    }
+module.exports = {
+  filecoin: {
+    tvl: sumTokensExport({ owner: MINT_TOKEN_CONTRACT, tokens: [MINT_CLUB_BOND_CONTRACT] })
+  }
 }
