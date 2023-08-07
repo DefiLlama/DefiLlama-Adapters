f30a65e495fbefe41b025d84d59528ad5643be28
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-07 13:00:11 +0200

    minor fix

--- projects/pulsar-money/index.js
+++ projects/pulsar-money/index.js
@@ -1,21 +1,7 @@
-const { toUSDTBalances } = require("../helper/balances");
-const { get } = require("../helper/http");
-const ADDRESSES = require("../helper/coreAssets.json");
-const { sumTokens, sumTokensExport } = require("../helper/chain/elrond");
+const { sumTokensExport } = require("../helper/chain/elrond");
 const { getCoreAssets } = require('../helper/tokenMapping')
 
-const API_URL = "https://pulsar-money-prod.herokuapp.com/metrics/tvl";
-
-async function tvl() {
-  const data = await get(API_URL);
-
-  const tokensAndOwners = {
-    owners: ["erd1qqqqqqqqqqqqqpgq39rqpn2xvm0ykl2ccaa4h5zk5c9r647wdteswmap9l"],
-    balances: [data],
-  };
-
-  return sumTokens(tokensAndOwners);
-}
+// const API_URL = "https://pulsar-money-prod.herokuapp.com/metrics/tvl";
 
 const vault = 'erd1qqqqqqqqqqqqqpgq39rqpn2xvm0ykl2ccaa4h5zk5c9r647wdteswmap9l'
 
