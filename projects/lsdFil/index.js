b3637e74e309f36a9651a8667c8c9a776fee25fc
daniel-kung <dannielkung@gmail.com>
2023-08-08 23:58:02 +0800

    add lsdFil

--- projects/lsdFil/index.js
+++ projects/lsdFil/index.js
@@ -1,15 +1,22 @@
 const MINT_TOKEN_CONTRACT = '0xcE5805CF6C84F71D2897f632E0Aa60d2430cCd2A';
 const MINT_CLUB_BOND_CONTRACT = '0x7187b3B1314375909B775d72fB7214Cb71a7D907';
-const {sumTokens2} = require('../helper/unwrapLPs');
 
 
-  module.exports = {
-    filecoin: {
-        tvl: async (_, _1, _2, {api}) => {
+async function tvl(_, _1, _2, { api }) {
+  const collateralBalance = await api.call({
+    abi: 'erc20:balanceOf',
+    target: MINT_TOKEN_CONTRACT,
+    params: [MINT_CLUB_BOND_CONTRACT],
+  });
 
-            let balances = {};
-            await sumTokens2({balances, owner: MINT_CLUB_BOND_CONTRACT, MINT_TOKEN_CONTRACT, api, });
-            return balances;
-        }
-    }
+  api.add(MINT_TOKEN_CONTRACT, collateralBalance)
 }
+
+module.exports = {
+  timetravel: true,
+  misrepresentedTokens: false,
+  methodology: 'counts the number of MINT tokens in the Club Bonding contract.',
+  filecoin: {
+    tvl,
+  }
+}; 
