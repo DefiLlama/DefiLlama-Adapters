18a1999678006a25de15bc9075b635c743e41be6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-08 20:41:10 +0200

    track https://www.aquarius.loan/

---
+++ projects/aquarius-loan/index.js
@@ -0,0 +1,5 @@
+const { compoundExports2 } = require("../helper/compound");
+
+module.exports = {
+  core: compoundExports2({ comptroller: '0x6056Eb6a5634468647B8cB892d3DaA5F816939FC', cether: '0x03ef96f537a7cda4411c8643afd9d8814d5b4906'})
+};
