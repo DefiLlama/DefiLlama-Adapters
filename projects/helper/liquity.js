f70898747514859743efe8836731a605c906afc4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-09 17:14:03 +0200

    fix broken adapters

--- projects/helper/liquity.js
+++ projects/helper/liquity.js
@@ -1,11 +1,12 @@
 const { nullAddress } = require("./tokenMapping")
 
-function getLiquityTvl(TROVE_MANAGER_ADDRESS, { nonNativeCollateralToken = false, abis = {} } = {}) {
+function getLiquityTvl(TROVE_MANAGER_ADDRESS, { nonNativeCollateralToken = false, abis = {}, collateralToken, } = {}) {
   return async (_, ethBlock, chainBlocks, { api }) => {
     const activePool = await api.call({ target: TROVE_MANAGER_ADDRESS, abi: abis.activePool ?? "address:activePool", })
     const defaultPool = await api.call({ target: TROVE_MANAGER_ADDRESS, abi: "address:defaultPool", })
     let token = nullAddress
-    if (nonNativeCollateralToken) token = await api.call({ target: TROVE_MANAGER_ADDRESS, abi: abis.collateralToken ?? "address:collateralToken", })
+    if (collateralToken) token = collateralToken
+    else if (nonNativeCollateralToken) token = await api.call({ target: TROVE_MANAGER_ADDRESS, abi: abis.collateralToken ?? "address:collateralToken", })
     return api.sumTokens({ owners: [activePool, defaultPool], tokens: [token] })
   }
 }
