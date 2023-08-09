41cc864164c4be04c237f8771dab74fd0bc64c0d
trung2891 <trungnv280201@gmail.com>
2023-08-09 10:19:13 +0700

    add Orchai Protocol

---
+++ projects/orchai/index.js
@@ -0,0 +1,31 @@
+const { queryContract } = require("../helper/chain/cosmos");
+
+const addresses = {
+  orai: {
+    tvlContract:
+      "orai1ez359uvv5p7l2ssgadzjk2pfuune9rrhu72ehwdsu4h2qhqf9jlsxw7e0f",
+  },
+};
+
+// Orai
+async function oraiTVL() {
+  let tokensDelegated = await queryContract({
+    contract: addresses.orai.tvlContract,
+    chain: "orai",
+    data: { liquid_stake_tvl: {} },
+  });
+
+  let balances = {};
+  for (let item of tokensDelegated) {
+    balances[`${item.token}`] = item.amount;
+  }
+
+  return balances;
+}
+
+module.exports = {
+  timetravel: false,
+  orai: {
+    tvl: oraiTVL,
+  },
+};
