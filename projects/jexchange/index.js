a92f86aa81c93bfadc8043cbbac4f8936e201e41
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-10 04:05:15 +0200

    code refactor

--- projects/jexchange/index.js
+++ projects/jexchange/index.js
@@ -1,13 +1,12 @@
 const { sumTokens } = require("../helper/chain/elrond");
+const { getConfig } = require('../helper/cache')
 
 async function tvl() {
+  const pools = await getConfig('jexchange', 'https://api.jexchange.io/pools')
   const owners = [
+    ...pools.map(pool => pool.sc_address),
     "erd1qqqqqqqqqqqqqpgqmmxzmktd09gq0hldtczerlv444ykt3pz6avsnys6m9",
     "erd1qqqqqqqqqqqqqpgqdh6jeeyamfhq66u7rmkyc48q037kk8n26avs400gg8",
-    "erd1qqqqqqqqqqqqqpgqpa3pdmemt5l2ex80g7pksr2ettt955d66avsz76hyt",
-    "erd1qqqqqqqqqqqqqpgq5nmzpdkpr6c5q894f553n49t0uc67vc96avsxgjl3v",
-    "erd1qqqqqqqqqqqqqpgqru6tkedsjjszrkkanctgq9m6rm2ple436avs0qj07m",
-    "erd1qqqqqqqqqqqqqpgquenuwz852khuxcau49md27wk2qp03v4s6avsdvmxkc",
   ];
   return sumTokens({ owners });
 }
