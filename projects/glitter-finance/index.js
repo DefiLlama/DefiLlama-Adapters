b1c55f8e587ff4bc683a49c636bcb6db4c70ee4b
Andrew Carl <andrewc@glitter.finance>
2023-08-10 10:13:18 -0400

    Testing Update

--- projects/glitter-finance/index.js
+++ projects/glitter-finance/index.js
@@ -1,9 +1,18 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 //Solana Helpers
 const { sumTokensExport, } = require("../helper/sumTokens")
+const { getTokenBalance,sumTokens } = require("../helper/solana");
 //addresses
 const SOLANA_VAULT = "7xCU4nvqu3Nz3BBQckKzibp3kBav4xbkuqQ3WM9CBHdJ";
+const XSOL_ADDRESS = "xALGoH1zUfRmpCriy94qbfoMXHtK6NDnMKzT4Xdvgms";
 const ALGO_VAULT = "R7VCOR74LCUIFH5WKCCMZOS7ADLSDBQJ42YURFPDT3VGYTVNBNG7AIYTCQ";
+
+async function solanaTVL(){
+    const tokensAndOwners = []
+    tokensAndOwners.push([ADDRESSES.SOLANA, SOLANA_VAULT])
+    return sumTokens(tokensAndOwners);
+
+}
 
 module.exports = {
     hallmarks: [
@@ -12,6 +21,6 @@ module.exports = {
     timetravel: false,
     methodology:
         "TVL counts tokens and native assets locked in Glitter-Finance bridge vaults. CoinGecko is used to find the price of tokens in USD.",
-    solana: { tvl: sumTokensExport({ solOwners: [SOLANA_VAULT], }) },
+    solana: { tvl: solanaTVL },
     algorand: { tvl: sumTokensExport({ owners: [ALGO_VAULT] }) },
 };
