5b2d4456b4ff7191e031a73ca8a6c17146770a0e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-11 00:27:01 +0530

    minor refactor (#7110)
    * gravita: add Arbitrum deployment
    
    * minor refactor
    
    ---------
    
    Co-authored-by: lutr0 <>

--- projects/gravita-protocol/index.js
+++ projects/gravita-protocol/index.js
@@ -1,28 +1,23 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
-const ADMIN_CONTRACT_ADDRESS = "0xf7Cc67326F9A1D057c1e4b110eF6c680B13a1f53";
-
-/**
- * Returns an array of addresses containing the collateral tokens accepted by the platform.
- */
-async function _getCollateralAddresses(api) {
-  return api.call({
-    abi: "function getValidCollateral() external view returns (address[])",
-    target: ADMIN_CONTRACT_ADDRESS,
-  });
-}
+const ADMIN_ADDRESSES = {
+  ethereum: "0xf7Cc67326F9A1D057c1e4b110eF6c680B13a1f53",
+  arbitrum: "0x4928c8F8c20A1E3C295DddBe05095A9aBBdB3d14",
+};
 
 async function tvl(_, _1, _2, { api }) {
-  const collAddresses = await _getCollateralAddresses(api);
-  const pool = await api.call({  abi: 'address:activePool', target: ADMIN_CONTRACT_ADDRESS})
-  return sumTokens2({ api, tokens: collAddresses, owner: pool})
+  const chainAddress = ADMIN_ADDRESSES[api.chain];
+  const collAddresses = await api.call({ abi: "address[]:getValidCollateral", target: chainAddress, });
+  const pool = await api.call({ abi: 'address:activePool', target: chainAddress })
+  return sumTokens2({ api, tokens: collAddresses, owner: pool })
 }
 
 module.exports = {
   methodology:
     "Adds up the total value locked as collateral on the Gravita platform",
   start: 1684256400, // Tuesday, May 15, 2023 17:00 GMT
-  ethereum: {
-    tvl,
-  },
 };
+
+Object.keys(ADMIN_ADDRESSES).forEach(chain => {
+  module.exports[chain] = { tvl }
+})
