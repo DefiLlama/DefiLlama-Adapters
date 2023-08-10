10b987f34110a93a1fe30b11bf097b3c6d214008
Arthur Ribeiro <arthurmr13@hotmail.com>
2023-08-10 12:07:08 -0300

    feat: integrated sweep n' flip TVL

--- projects/sweep-n-flip/index.js
+++ projects/sweep-n-flip/index.js
@@ -1,33 +1,66 @@
-const { request, GraphQLClient, } = require("graphql-request")
+const { GraphQLClient, } = require('graphql-request')
+const BigNumber = require("bignumber.js");
 
 const backendEndpoint = 'https://us-east1-nftfy-backend.cloudfunctions.net/v2/graphql'
+
+const graphQLClient = new GraphQLClient(backendEndpoint)
 
 const query = `
   query AmmTopPools($chainId: Int!) {
     topPools(chainId: $chainId) {
       liquidity
+      erc20Token {
+        id
+      }
     }
   }
 `
 
-const polygonQuery = request(query, { chainId: 137})
-const ethereumQuery = request(query, { chainId: 1})
-const arbitrumQuery = request(query, { chainId: 42161})
+const polygonQuery = graphQLClient.request(query, { chainId: 137})
+const ethereumQuery = graphQLClient.request(query, { chainId: 1})
+const arbitrumQuery = graphQLClient.request(query, { chainId: 42161})
 
-const obtainLiquidity = async () => {
+const obtainPools = async () => {
   const { topPools: polygonTopPools } = await polygonQuery
   const { topPools: ethereumTopPools } = await ethereumQuery
   const { topPools: arbitrumTopPools } = await arbitrumQuery
 
   return {
-    polygon: polygonTopPools.reduce((acc, pool) => acc + Number(pool.liquidity), 0),
-    ethereum: ethereumTopPools.reduce((acc, pool) => acc + Number(pool.liquidity), 0),
-    arbitrum: arbitrumTopPools.reduce((acc, pool) => acc + Number(pool.liquidity), 0),
+    polygon: polygonTopPools,
+    ethereum: ethereumTopPools,
+    arbitrum: arbitrumTopPools,
   }
 }
 
 module.exports = {
+  timetravel: false,
+  misrepresentedTokens: true,
+  methodology: 'TVL counts the liquidity of the pools on each chain.',
   arbitrum: {
-    tvl
-  }
+    tvl: async (_, _1, _2, { api }) => {
+      const { topPools } = await ethereumQuery
+      topPools.forEach(pool => {
+        const liquidityInWei = BigInt(Number(pool.liquidity.replace(',', '')) * 1e18)
+        api.add(pool.erc20Token.id, liquidityInWei.toString())
+      })
+    },
+  },
+  ethereum: {
+    tvl: async (_, _1, _2, { api }) => {
+      const { topPools } = await ethereumQuery
+      topPools.forEach(pool => {
+        const liquidityInWei = BigInt(Number(pool.liquidity.replace(',', '')) * 1e18)
+        api.add(pool.erc20Token.id, liquidityInWei.toString())
+      })
+    },
+  },
+  polygon: {
+    tvl: async (_, _1, _2, { api }) => {
+      const { topPools } = await polygonQuery
+      topPools.forEach(pool => {
+        const liquidityInWei = BigInt(Number(pool.liquidity.replace(',', '')) * 1e18)
+        api.add(pool.erc20Token.id, liquidityInWei.toString())
+      })
+    },
+  },
 }
