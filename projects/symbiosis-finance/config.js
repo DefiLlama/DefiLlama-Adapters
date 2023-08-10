349c1ee5f6099bb3eac39115f7e5f4b47d27f39e
Aleksei Lushnikov <al.lushnikov@yandex.ru>
2023-08-10 13:31:59 +0100

    symbiosis adds USDC to linea (#7100)

--- projects/symbiosis-finance/config.js
+++ projects/symbiosis-finance/config.js
@@ -179,6 +179,7 @@ module.exports = {
        name: 'linea',
        tokens: [
          ADDRESSES.linea.WETH,
+         ADDRESSES.linea.USDC,
        ],
        holders: [
          '0x292fC50e4eB66C3f6514b9E402dBc25961824D62', // portal v2
