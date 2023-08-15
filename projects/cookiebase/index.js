324fd540cf9537cc30b445e744c7fd5c34544773
Cookiedev <cookiebasedev@gmail.com>
2023-08-15 20:26:55 +0200

    updated with masterchef contract

--- projects/cookiebase/index.js
+++ projects/cookiebase/index.js
@@ -1,6 +1,10 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { masterchefExports } = require("../helper/unknownTokens");
 
-module.exports = {
-  misrepresentedTokens: true,
-  base: { tvl: getUniTVL({ factory: '0x05b00D63DbC67874ad44d039213b7DAfB999d184', useDefaultCoreAssets: true, fetchBalances: true, }), },
-}
+module.exports = masterchefExports({
+  chain: "base",
+  masterchef: "0x0544b381F24eaC255ED1e2Ab2a67f10D2502921a",
+  nativeTokens: ["0x614747C53CB1636b4b962E15e1D66D3214621100"],
+  useDefaultCoreAssets: true,
+  poolInfoABI: "function poolInfo(uint256) view returns (address)",
+  getToken: (i) => i,
+});
