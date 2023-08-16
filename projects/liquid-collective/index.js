1a2794f9d53441f04a3d45307fb16723e85ede87
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-16 11:53:14 +0530

    Liquid-collective (#7167)

---
+++ projects/liquid-collective/index.js
@@ -0,0 +1,14 @@
+const ADDRESSES = require('../helper/coreAssets.json')
+const token = '0x8c1BEd5b9a0928467c9B1341Da1D7BD5e10b6549'
+
+async function tvl(_, _1, _2, { api }) {
+  const supply = await api.call({ abi: 'uint256:totalUnderlyingSupply', target: token, });
+  api.add(ADDRESSES.null, supply)
+}
+
+module.exports = {
+  methodology: 'Retrieve the total underlying ETH supply',
+  ethereum: {
+    tvl
+  }
+}
