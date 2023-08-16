eb64df75c682ae35fbc852d8fc1833739ea5dfc4
firebomb1 <88804546+firebomb1@users.noreply.github.com>
2023-08-16 08:41:44 +0200

    chore: add shapeshift fox farm v7 (#7161)

--- projects/shapeshift/index.js
+++ projects/shapeshift/index.js
@@ -9,6 +9,7 @@ const stakingUNIv2Contracts = [
   "0x24FD7FB95dc742e23Dc3829d3e656FEeb5f67fa0", // v4
   "0xC14eaA8284feFF79EDc118E06caDBf3813a7e555", // v5
   "0xEbB1761Ad43034Fd7FaA64d84e5BbD8cB5c40b68", // v6
+  "0x5939783dbf3e9f453a69bc9ddc1e492efac1fbcb", // v7
 ];
 const stakingFoxy = "0xee77aa3Fd23BbeBaf94386dD44b548e9a785ea4b";
 
