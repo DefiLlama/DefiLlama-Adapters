b91bdabdca74fb5fa88e42aa3f29a97d4dda7f52
findolor <anakisci@gmail.com>
2023-08-16 22:44:49 +0300

    Add seaswap adapter

---
+++ projects/seaswap/index.js
@@ -0,0 +1,36 @@
+const { queryContracts, getDenomBalance } = require("../helper/chain/cosmos");
+const { transformDexBalances } = require("../helper/portedTokens");
+
+const POOL_CONTRACT_CODE_ID = 64;
+
+module.exports = {
+  timetravel: false,
+  methodology: "Liquidity on the DEX",
+  sei: {
+    tvl: async (_, _1, _2, { chain }) => {
+      const contracts = await queryContracts({ chain, codeId: POOL_CONTRACT_CODE_ID })
+
+      let totalBalance = 0;
+
+      for (const contract of contracts) {
+        const balance = await getDenomBalance({
+          chain,
+          owner: contract,
+          denom: "usei",
+        });
+
+        totalBalance += balance;
+      }
+
+      const data = [
+        {
+          token0: 'usei',
+          token0Bal: totalBalance,
+          token1: 'uusd',
+          token1Bal: 0
+        },
+      ]
+      return transformDexBalances({ chain, data });
+    },
+  },
+};
