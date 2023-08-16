d4e1360d17e79ca1220f3c94717e4e2a7957a53a
Geunwoo Baek (Richard) <39114544+geunwoobaek@users.noreply.github.com>
2023-08-16 17:23:20 +0900

    Update index.js

--- projects/vivaleva/index.js
+++ projects/vivaleva/index.js
@@ -1,60 +1,18 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const vaults = [
-  {
-    symbol: "ibETH",
-    decimals: 18,
-    baseTokenAddress: ADDRESSES.era.WETH,
-    address: "0x23FDd6487a17abB8360E8Da8b1B370C94ee94Cc2",
-  },
-  {
-    symbol: "ibUSDC",
-    decimals: 6,
-    baseTokenAddress: ADDRESSES.era.USDC,
-    address: "0x0755DA5D9e9A722A9e5cc4bb83742387ae2990a5",
-  },
-];
-const syncswapWorkers = [
-  {
-    name: "ETH-USDC Syncswap Farm",
-    address: "0x39356ed5dC2F7Ea897296e07E97b59Af9C8153Ec",
-    farmingTokenAddress: ADDRESSES.era.WETH,
-    baseTokenAddress: ADDRESSES.era.USDC,
-    lpToken: "0x80115c708E12eDd42E504c1cD52Aea96C547c05c",
-  },
-  {
-    name: "USDC-ETH Syncswap Farm",
-    address: "0x3A613EFAe4a6A6447A9D784E398730811a57af6e",
-    farmingTokenAddress: ADDRESSES.era.USDC,
-    baseTokenAddress: ADDRESSES.era.WETH,
-    lpToken: "0x80115c708E12eDd42E504c1cD52Aea96C547c05c",
-  },
-  {
-    name: "USDT-ETH Syncswap Farm",
-    address: "0x95C78e21Beb54314fe5A4571E7361f6c6A144B2f",
-    farmingTokenAddress: ADDRESSES.era.USDT,
-    baseTokenAddress: ADDRESSES.era.WETH,
-    lpToken: "0xd3D91634Cf4C04aD1B76cE2c06F7385A897F54D3",
-  },
-  {
-    name: "BUSD-ETH Syncswap Farm",
-    address: "0x8468AB9e88550f725439A7f128E2E31a5E6b753f",
-    farmingTokenAddress: ADDRESSES.era.BUSD,
-    baseTokenAddress: ADDRESSES.era.WETH,
-    lpTokenAddress: "0xaD86486f1d225D624443e5DF4B2301d03bBe70f6",
-  },
-  {
-    name: "USDT-USDC Syncswap Farm",
-    address: "0xfbF4BcD3266Af20B72dc484F6D7Dc13855885ba0",
-    farmingTokenAddress: ADDRESSES.era.USDT,
-    baseTokenAddress: ADDRESSES.era.USDC,
-    lpToken: "0x0E595bfcAfb552F83E25d24e8a383F88c1Ab48A4",
-  },
-];
+const axios = require('axios');
+
+async function fetch() {
+  const url = "https://sbb.sooho.io/api/v1/external/vivaleva/defiLlama";
+  var response = (await axios.get(url)).data;
+  return response;
+}
 
 module.exports = {
   timetravel: false,
   era: {
     async tvl(_, _1, _2, { api }) {
+      const data = await fetch();
+      const vaults = data.vaults;
+      const syncswapWorkers = data.syncSwapWorkers;
       const vaultBalances = await api.multiCall({
         abi: "uint256:vaultBalance",
         calls: vaults.map((v) => v.address),
@@ -75,11 +33,11 @@ module.exports = {
         }),
         api.multiCall({
           abi: "function getReserves() view returns (uint256, uint256)",
-          calls: syncswapWorkers.map((v) => v.lpToken),
+          calls: syncswapWorkers.map((v) => v.lpTokenAddress),
         }),
         api.multiCall({
           abi: "uint256:totalSupply",
-          calls: syncswapWorkers.map((v) => v.lpToken),
+          calls: syncswapWorkers.map((v) => v.lpTokenAddress),
         }),
       ]);
 
