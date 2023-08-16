4bbf3e00e3154cc887a5a5228dffa2607977bb82
g1nt0ki <llama@llama.fi>
2023-08-16 14:51:53 +0200

    minor fix

--- projects/vivaleva/index.js
+++ projects/vivaleva/index.js
@@ -1,22 +1,13 @@
-const axios = require('axios');
-
-async function fetch() {
-  const url = "https://sbb.sooho.io/api/v1/external/vivaleva/defiLlama";
-  var response = (await axios.get(url)).data;
-  return response;
-}
+const { getConfig } = require('../helper/cache')
 
 module.exports = {
   timetravel: false,
   era: {
     async tvl(_, _1, _2, { api }) {
-      const data = await fetch();
+      const data = await getConfig('vivaleva', "https://sbb.sooho.io/api/v1/external/vivaleva/defiLlama")
       const vaults = data.vaults;
       const syncswapWorkers = data.syncSwapWorkers;
-      const vaultBalances = await api.multiCall({
-        abi: "uint256:vaultBalance",
-        calls: vaults.map((v) => v.address),
-      });
+      const vaultBalances = await api.multiCall({ abi: "uint256:vaultBalance", calls: vaults.map((v) => v.address), });
 
       vaults.forEach((v, i) => {
         api.add(v.baseTokenAddress, vaultBalances[i]);
@@ -26,26 +17,19 @@ module.exports = {
         syncswapWorkerBalances,
         syncswapReserves,
         syncswapLpTotalSupplies,
+        token0s,
+        token1s,
       ] = await Promise.all([
-        api.multiCall({
-          abi: "uint256:totalStakedLpBalance",
-          calls: syncswapWorkers.map((v) => v.address),
-        }),
-        api.multiCall({
-          abi: "function getReserves() view returns (uint256, uint256)",
-          calls: syncswapWorkers.map((v) => v.lpTokenAddress),
-        }),
-        api.multiCall({
-          abi: "uint256:totalSupply",
-          calls: syncswapWorkers.map((v) => v.lpTokenAddress),
-        }),
+        api.multiCall({ abi: "uint256:totalStakedLpBalance", calls: syncswapWorkers.map((v) => v.address), }),
+        api.multiCall({ abi: "function getReserves() view returns (uint256, uint256)", calls: syncswapWorkers.map((v) => v.lpTokenAddress), }),
+        api.multiCall({ abi: "uint256:totalSupply", calls: syncswapWorkers.map((v) => v.lpTokenAddress), }),
+        api.multiCall({ abi: "address:token0", calls: syncswapWorkers.map((v) => v.lpTokenAddress), }),
+        api.multiCall({ abi: "address:token1", calls: syncswapWorkers.map((v) => v.lpTokenAddress), }),
       ]);
 
       syncswapWorkers.forEach((w, i) => {
-        const [token0, token1] =
-          w.baseTokenAddress.toLowerCase() < w.farmingTokenAddress.toLowerCase()
-            ? [w.baseTokenAddress, w.farmingTokenAddress]
-            : [w.farmingTokenAddress, w.baseTokenAddress];
+        const token0 = token0s[i]
+        const token1 = token1s[i]
         const lpBalance = BigInt(syncswapWorkerBalances[i])
         const totalSupply = BigInt(syncswapLpTotalSupplies[i])
         const [r0, r1] = syncswapReserves[i].map(BigInt);
