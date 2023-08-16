469969195dd1c4fcad40261c48cba6e0751d2ddd
Geunwoo Baek (Richard) <39114544+geunwoobaek@users.noreply.github.com>
2023-08-16 17:14:28 +0900

    Update index.js

--- projects/vivaleva/index.js
+++ projects/vivaleva/index.js
@@ -42,13 +42,13 @@ const syncswapWorkers = [
     baseTokenAddress: ADDRESSES.era.USDC,
     lpToken: "0x0E595bfcAfb552F83E25d24e8a383F88c1Ab48A4",
   },
-  // {
-  //   name: "BUSD-ETH Syncswap Farm",
-  //   address: "0x8468AB9e88550f725439A7f128E2E31a5E6b753f",
-  //   farmingTokenAddress: ADDRESSES.era.BUSD,
-  //   baseTokenAddress: ADDRESSES.era.WETH,
-  //   lpTokenAddress: "0xaD86486f1d225D624443e5DF4B2301d03bBe70f6"
-  // },
+  {
+    name: "BUSD-ETH Syncswap Farm",
+    address: "0x8468AB9e88550f725439A7f128E2E31a5E6b753f",
+    farmingTokenAddress: ADDRESSES.era.BUSD,
+    baseTokenAddress: ADDRESSES.era.WETH,
+    lpTokenAddress: "0xaD86486f1d225D624443e5DF4B2301d03bBe70f6",
+  },
 ];
 
 module.exports = {
