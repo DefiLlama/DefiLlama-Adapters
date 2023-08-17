030f824dfcbc376f97e2eb150b447e98bb6145c4
Tempest Capital <129153004+TempestGit@users.noreply.github.com>
2023-08-17 18:35:25 +0800

    Add Tempest on OP (#7189)

---
+++ projects/tempest/index.js
@@ -0,0 +1,8 @@
+const { compoundExports2 } = require('../helper/compound');
+
+module.exports = {
+    optimism: compoundExports2({
+        comptroller: '0x505E91a70aA5036677A71b2Af8D1881B85181EAE',
+        cether: '0x736440033C4C1C8A21e0B238989FE24bEC502014',
+    })
+}; 
