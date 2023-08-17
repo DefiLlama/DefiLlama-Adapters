32535d91fb8a5aae6cb17444b12f5fe0bc4f180f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-17 16:37:38 +0200

    code refactor

--- projects/FuzioNetwork/index.js
+++ projects/FuzioNetwork/index.js
@@ -1,39 +1,9 @@
-const { queryContracts, getDenomBalance } = require("../helper/chain/cosmos");
-const { transformDexBalances } = require("../helper/portedTokens");
-
-const POOL_CONTRACT_CODE_ID = 86;
+const { getSeiDexTvl } = require('../terraswap/factoryTvl')
 
 module.exports = {
   timetravel: false,
   methodology: "Liquidity on the DEX",
   sei: {
-    tvl: async (_, _1, _2, { chain }) => {
-      const contracts = await queryContracts({ chain, codeId: POOL_CONTRACT_CODE_ID })
-
-      let totalBalance = 0;
-
-      for (const contract of contracts) {
-        const balance = await getDenomBalance({
-          chain,
-          owner: contract,
-          denom: "usei",
-        });
-
-        totalBalance += balance;
-      }
-
-
-
-
-      const data = [
-        {
-          token0: 'usei',
-          token0Bal: totalBalance,
-          token1: 'uusd',
-          token1Bal: 0
-        },
-      ]
-      return transformDexBalances({ chain, data });
-    },
+    tvl: getSeiDexTvl(86),
   },
-};
+}
--- projects/seaswap/index.js
+++ projects/seaswap/index.js
@@ -1,45 +1,9 @@
-const { queryContracts, queryContract, sumTokens, } = require("../helper/chain/cosmos")
-const { transformDexBalances } = require('../helper/portedTokens')
-const { PromisePool } = require('@supercharge/promise-pool');
-const { sliceIntoChunks, sleep } = require("../helper/utils");
-
-const POOL_CONTRACT_CODE_ID = 64;
-
-function extractTokenInfo(asset) {
-  const { native_token, token, native, cw20, } = asset
-  for (const tObject of [native_token, token, native, cw20]) {
-    if (!tObject) continue
-    if (typeof tObject === 'string') return tObject
-    const token = tObject.denom || tObject.contract_addr
-    if (token) return token
-  }
-}
+const { getSeiDexTvl } = require('../terraswap/factoryTvl')
 
 module.exports = {
   timetravel: false,
   methodology: "Liquidity on the DEX",
   sei: {
-    tvl: async (_, _1, _2, { api, }) => {
-      const chain = api.chain
-      const contracts = await queryContracts({ chain, codeId: POOL_CONTRACT_CODE_ID })
-      /* const data = []
-      const getPairPool = (async (pair) => {
-        const res = await queryContract({ contract: pair, chain, data: { info: {} } })
-        const { token1_denom, token2_denom, token1_reserve, token2_reserve } = res
-        data.push({
-          token0: extractTokenInfo(token1_denom),
-          token1: extractTokenInfo(token2_denom),
-          token0Bal: token1_reserve,
-          token1Bal: token2_reserve,
-        })
-      })
-      await PromisePool
-        .withConcurrency(25)
-        .for(contracts)
-        .process(getPairPool)
-      return transformDexBalances({ chain, data }) */
-      
-      return sumTokens({ chain, owners: contracts })
-    },
+    tvl: getSeiDexTvl(64),
   },
 };
