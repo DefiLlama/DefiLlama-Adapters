17da1c3e0027b9c0bf4a210a0e4238291e5538a1
Opankeswap <142486128+Opankeswap@users.noreply.github.com>
2023-08-17 16:02:09 +0700

    OpankeSwap (#7186)
    Co-authored-by: tinhp115 <tinhp115@gmail.com>

---
+++ projects/OpankeSwap/index.js
@@ -0,0 +1,10 @@
+const { getUniTVL } = require("../helper/unknownTokens");
+
+module.exports = {
+  op_bnb: {
+    tvl: getUniTVL({
+      factory: "0x4523A7f5414bAc9BfbDfc6eF0932Bf580C3cf9f1",
+      useDefaultCoreAssets: true,
+    }),
+  },
+};
