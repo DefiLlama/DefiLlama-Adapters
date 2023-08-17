8ddfdfdbc1c277e490337d31d63f9b276e5c1dba
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-17 10:32:25 +0200

    fix

--- projects/FlitSwap/index.js
+++ projects/FlitSwap/index.js
@@ -2,9 +2,9 @@ const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
   misrepresentedTokens: true,
-  dogechain: {
+  op_bnb: {
     tvl: getUniTVL({
-      chain: 'op_bnb',
+      fetchBalances: true,
       useDefaultCoreAssets: true,
       factory: '0x243f0218EF4cB8FC922ddd6d44e2DdE5b95DCa89',
     })
