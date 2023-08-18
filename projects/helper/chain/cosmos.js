f7473f7c63a441a285ff43d5fd3c93b84bba8cb3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-18 08:28:49 +0200

    fix broken adapters

--- projects/helper/chain/cosmos.js
+++ projects/helper/chain/cosmos.js
@@ -10,7 +10,7 @@ const { log } = require("../utils");
 // https://cosmos-chain.directory/chains
 const endPoints = {
   crescent: "https://mainnet.crescent.network:1317",
-  osmosis: "https://lcd.osmosis.zone",
+  osmosis: "https://osmosis-api.polkachu.com",
   cosmos: "https://cosmoshub-lcd.stakely.io",
   kujira: "https://kuji-api.kleomedes.network",
   comdex: "https://rest.comdex.one",
@@ -20,8 +20,8 @@ const endPoints = {
   orai: "https://lcd.orai.io",
   juno: "https://lcd-juno.cosmostation.io",
   cronos: "https://lcd-crypto-org.cosmostation.io",
-  // chihuahua: "https://rest.cosmos.directory/chihuahua",
-  chihuahua: "https://api.chihuahua.wtf",
+  chihuahua: "https://rest.cosmos.directory/chihuahua",
+  // chihuahua: "https://api.chihuahua.wtf",
   injective: "https://lcd-injective.whispernode.com:443",
   migaloo: "https://migaloo-api.polkachu.com",
   fxcore: "https://fx-rest.functionx.io",
--- projects/ibcx/index.js
+++ projects/ibcx/index.js
@@ -1,4 +1,6 @@
 const axios = require('axios')
+const { endPoints } = require('../helper/chain/cosmos')
+
 
 const contractAddresses = [
     "osmo1yhd9tzp09d833u7ray4pc6wwp72aewtt2xwakszn3lzlf2klnlwscjwhxt", // uibcx 
@@ -12,23 +14,23 @@ const contractAddresses = [
 async function tvl() {
     const { api } = arguments[3]
     for (const contractAddress of contractAddresses) {
-        const contractBalances = (await axios.get(`https://lcd.osmosis.zone/cosmos/bank/v1beta1/balances/${contractAddress}`)).data.balances
+        const contractBalances = (await axios.get(`${endPoints.osmosis}/cosmos/bank/v1beta1/balances/${contractAddress}`)).data.balances
         for (const coin of contractBalances) {
             // stUMEE not be listed on coingecko so we convert stUMEE to the corresponding amount of UMEE base on pool stUMEE/UMEE
             if (coin.denom == "ibc/02F196DA6FD0917DD5FEA249EE61880F4D941EE9059E7964C5C9B50AF103800F") {
-                const spotPrice = (await axios.get(`https://lcd.osmosis.zone/osmosis/gamm/v2/pools/1035/prices?base_asset_denom=ibc/67795E528DF67C5606FC20F824EA39A6EF55BA133F4DC79C90A8C47A0901E17C&quote_asset_denom=${coin.denom}`)).data.spot_price
+                const spotPrice = (await axios.get(`${endPoints.osmosis}/osmosis/gamm/v2/pools/1035/prices?base_asset_denom=ibc/67795E528DF67C5606FC20F824EA39A6EF55BA133F4DC79C90A8C47A0901E17C&quote_asset_denom=${coin.denom}`)).data.spot_price
                 coin.denom = "ibc/67795E528DF67C5606FC20F824EA39A6EF55BA133F4DC79C90A8C47A0901E17C" // UMEE 
                 coin.amount = coin.amount * spotPrice
             }
             // stEVMOS not be listed on coingecko so we convert stEVMOS to the corresponding amount of EVMOS base on pool stEVMOS/EVMOS
             if (coin.denom == "ibc/C5579A9595790017C600DD726276D978B9BF314CF82406CE342720A9C7911A01") {
-                const spotPrice = (await axios.get(`https://lcd.osmosis.zone/osmosis/gamm/v2/pools/922/prices?base_asset_denom=ibc/6AE98883D4D5D5FF9E50D7130F1305DA2FFA0C652D1DD9C123657C6B4EB2DF8A&quote_asset_denom=${coin.denom}`)).data.spot_price
+                const spotPrice = (await axios.get(`${endPoints.osmosis}/osmosis/gamm/v2/pools/922/prices?base_asset_denom=ibc/6AE98883D4D5D5FF9E50D7130F1305DA2FFA0C652D1DD9C123657C6B4EB2DF8A&quote_asset_denom=${coin.denom}`)).data.spot_price
                 coin.denom = "ibc/6AE98883D4D5D5FF9E50D7130F1305DA2FFA0C652D1DD9C123657C6B4EB2DF8A" // EVMOS 
                 coin.amount = coin.amount * spotPrice
             }
             api.add(coin.denom, coin.amount)
         }
-        
+
     }
 }
 
