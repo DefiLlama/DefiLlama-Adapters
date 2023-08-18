8b08a8ff3117e9791c6cf2c8e031da4b67256ecc
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-18 10:52:56 +0200

    minor fix

--- projects/LuigiSwap/index.js
+++ projects/LuigiSwap/index.js
@@ -4,7 +4,7 @@ module.exports = {
   op_bnb: {
     tvl: getUniTVL({
       factory: "0x0dAe6d22182c20AB9150a4DCB3160591Dc41027a",
-      useDefaultCoreAssets: false,
+      useDefaultCoreAssets: true,
       fetchBalances: true,
     }),
   },
