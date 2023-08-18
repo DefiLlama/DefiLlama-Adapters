8cb2f1ffd4f4efea3cb5b42e8af8666a5cf04f45
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-18 10:57:49 +0200

    minor fix

--- projects/OpankeSwap/index.js
+++ projects/OpankeSwap/index.js
@@ -1,10 +1,11 @@
 const { getUniTVL } = require("../helper/unknownTokens");
 
 module.exports = {
+  misrepresentedTokens: true,
   op_bnb: {
     tvl: getUniTVL({
       factory: "0x4523A7f5414bAc9BfbDfc6eF0932Bf580C3cf9f1",
-      useDefaultCoreAssets: false,
+      useDefaultCoreAssets: true,
       fetchBalances: true,
     }),
   },
