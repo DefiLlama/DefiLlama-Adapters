90f7edd4019b52c175633b1c818bcd5168bd24b5
xasai <xasai@denet.pro>
2023-08-18 11:19:29 +0600

    count DE tokens insidie DeNet contracts as staking

--- projects/DeNet/index.js
+++ projects/DeNet/index.js
@@ -2,6 +2,7 @@ const { sumTokensExport } = require("../helper/unwrapLPs");
 const DE_TOKEN_ADDRESS = '0x081Ec4c0e30159C8259BAD8F4887f83010a681DC';
 
 const owners = [
+  '0x6261e1aac369cd694093455f9e2b65b31acedda1', // veDE
   '0x1a9b54A3075119f1546C52cA0940551A6ce5d2D0', // Payments for DeNet storage
 ]
 
@@ -12,7 +13,8 @@ module.exports = {
   misrepresentedTokens: false,
 
   polygon: {
-    tvl: sumTokensExport({ 
+	tvl: () => ({}),
+    staking: sumTokensExport({ 
       owners: owners,
       tokens: [DE_TOKEN_ADDRESS]
     }),
