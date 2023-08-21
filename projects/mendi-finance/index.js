28deb74914a66dbfb2a05c65b218a1d6e45b5ef6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-21 15:22:16 +0200

    minor fix

--- projects/mendi-finance/index.js
+++ projects/mendi-finance/index.js
@@ -1,13 +1,11 @@
-const { compoundExports } = require("../helper/compound");
+const { compoundExports2 } = require("../helper/compound");
 const { staking } = require("../helper/staking");
-
-const unitroller = "0x1b4d3b0421dDc1eB216D230Bc01527422Fb93103";
 
 module.exports = {
   methodology:
     "Same as Compound Finance, we just count all the tokens supplied (not borrowed money) on the lending markets",
   linea: {
-    ...compoundExports(unitroller, "linea"),
+    ...compoundExports2({ comptroller: '0x1b4d3b0421dDc1eB216D230Bc01527422Fb93103'}),
     staking: staking(
       [
         "0x150b1e51738cdf0ccfe472594c62d7d6074921ca",
