85b9b7295b7581dd2420466e67fdf441069a4ea1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-21 15:33:23 +0200

    minor fix

--- projects/orange-finance/index.js
+++ projects/orange-finance/index.js
@@ -1,30 +1,18 @@
 const ADDRESSES = require("../helper/coreAssets.json");
-const sdk = require("@defillama/sdk");
 
 const VAULTS = [
   "0xb9c5425084671221d7d5a547dbf1bdcec26c8b7d", //Camelot ETH-USDC.e DN Vault
   "0x1c99416c7243563ebEDCBEd91ec8532fF74B9a39", //UniswapV3 ETH-USDC.e Dynamic Hedge Vault
 ];
-const USDC_ARB = ADDRESSES.arbitrum.USDC;
 
 async function tvl(_, _1, _2, { api }) {
-  const totalAssets = (
-    await sdk.api.abi.multiCall({
-      abi: "function totalAssets() external view returns (uint256)",
-      calls: VAULTS.map((addr) => ({ target: addr })),
-      chain: "arbitrum",
-    })
-  ).output;
+  const totalAssets = await api.multiCall({ abi: "uint256:totalAssets", calls: VAULTS })
 
-  totalAssets.forEach((totalAsset) => {
-    api.add(USDC_ARB, totalAsset.output);
-  });
+  totalAssets.forEach((i) => api.add(ADDRESSES.arbitrum.USDC, i))
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  methodology: "",
+  doublecounted: true,
   start: 107356480,
   arbitrum: {
     tvl,
