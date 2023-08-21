fa5439a6219921e5360ec20e660e0123f2030ece
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-21 16:10:09 +0200

    intswap code refactor

--- projects/intswap/index.js
+++ projects/intswap/index.js
@@ -1,34 +1,23 @@
-const sdk = require('@defillama/sdk')
-const graphql = require('../helper/utils/graphql')
-const returnEthBalance = require('../helper/utils.js')
+const ADDRESSES = require('../helper/coreAssets.json')
+const { getLogs } = require('../helper/cache/getLogs')
 
-const query = `
-query getPairs {
-  pairs(first: 1000) {
-    id
-    address
-    tvl
-  }
-}`
+async function tvl(_, _b, _cb, { api, }) {
+  const logs = await getLogs({
+    api,
+    target: '0xb9a8213d237c768e88bf89d690a9222df439dcc1',
+    topics: ['0xbc9687dc21b69bf8e01370b9f0ae1625f6bf5244fc230fa3ad4c06350deb634d'], // NewPair event
+    onlyArgs: true,
+    fromBlock: 9675402,
+  })
+
+  await api.sumTokens({ owners: logs.map(i => '0x' + i.data.slice(-40)), tokens: [ADDRESSES.null] })
+  const bal = api.getBalances()
+  api.add(ADDRESSES.null, bal[api.chain + ':'+ADDRESSES.null ?? 0])
+  return bal
+}
 
 module.exports = {
+  misrepresentedTokens: true,
   methodology: 'Sum up all the ETH in pools',
-  era: {
-    tvl: async (timestamp, block, chainBlocks, { api }) => {
-
-      const data = await graphql.request(
-        'https://api.studio.thegraph.com/query/47106/intswap-zksync-era/version/latest',
-        query,
-      )
-
-      const balances = {};
-      console.log(data);
-      data.pairs.forEach(({ address, tvl }) => {
-        console.log(tvl);
-        sdk.util.sumSingleBalance(balances, 'ethereum', tvl / 1e18)
-      });
-
-      return balances;
-    }
-  }
+  era: { tvl, }
 }
