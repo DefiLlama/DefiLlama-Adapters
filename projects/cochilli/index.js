d93f5dcf5a433042946df0db5ed136e05c535eee
cochilli <138674364+cochilli@users.noreply.github.com>
2023-08-21 07:44:11 +0000

    update the cochilli contract address

--- projects/cochilli/index.js
+++ projects/cochilli/index.js
@@ -1,7 +1,7 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport } = require('../helper/unwrapLPs');
 
-const owner = "0x8D5b64b8D8904E4aEc79F10468F347534D2A1b79"; // vault address
+const owner = "0xcE42C112e617c9BB1Bd8238D6Bb83B89e112fDbc"; // vault address
 const tokens = [ADDRESSES.base.USDbC]; // USDC
 
 module.exports = {
