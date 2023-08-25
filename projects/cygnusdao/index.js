5a62a2d83961e4ada6ccc06bab30d0f591103c26
0xsnowman <eth.snowman@gmail.com>
2023-08-25 22:54:52 +0700

    update-deployed-shuttles-getter-dynamic

--- projects/cygnusdao/index.js
+++ projects/cygnusdao/index.js
@@ -1,12 +1,22 @@
 const sdk = require("@defillama/sdk");
 const abi = require("./abi.json");
-const shuttleIds = [2, 3, 4];
 const FACTORY_CONTRACT = "0x94faE55669327e71E9EC579067ad6C3C3b84e574";
 const POLYGON_USDC_ADDRESS = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
 
-function cygnalytics(category) { // futuristic parameter
-  return async (timestamp, block) => {
+function cygnalytics(category) {
+  // futuristic parameter
+  return async (timestamp, block, _, { api }) => {
     let balances = {};
+
+    const shuttlesDeployed = await api.call({
+      abi: abi.shuttlesDeployed,
+      target: FACTORY_CONTRACT,
+    });
+
+    let shuttleIds = Array.from(
+      { length: shuttlesDeployed - 2 },
+      (_, i) => i + 2
+    );
 
     const shuttleTvlUsds = await sdk.api.abi.multiCall({
       abi: abi.shuttleTvlUsd,
