260c930ba596683eecee62b5402912aa1b45b107
realdealshaman <realdealshaman11@gmail.com>
2023-08-25 04:06:46 +0100

    update to new vault contract

--- projects/gplx/index.js
+++ projects/gplx/index.js
@@ -5,6 +5,6 @@ module.exports = {
     [1687320000, "Token supply compromise"]
   ],
   pulse:{
-    tvl: gmxExports({ vault: '0xC2311efFE60b0dC2491148Ff1bd46F08D64ADC98', })
+    tvl: gmxExports({ vault: '0x4a305E6F8724Cb5F0106C8CdC90e9C6CA6429083', })
   }
 };
