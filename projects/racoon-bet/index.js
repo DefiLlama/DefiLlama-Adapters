6b232a0442063a89b623f18a02395f9f405d3437
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-25 22:18:21 +0200

    code refactor

--- projects/racoon-bet/index.js
+++ projects/racoon-bet/index.js
@@ -1,36 +1,16 @@
-const { getBalance2 } = require('../helper/chain/cosmos');
+const { sumTokensExport } = require('../helper/sumTokens')
 
-async function tvl(chain, contract) {
-  let balances = await getBalance2({
-    owner: contract,
-    chain
-  });
-  return rename_balance(balances)
-}
-
-function rename_balance(obj) {
-  const keyMappings = {
-    uwhale: 'white-whale',
-    ujuno: 'juno-network',
-    uhuahua: 'chihuahua-token'
-  };
-
-  const new_balance = {};
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      const newKey = keyMappings[key] || key;
-      const originalValue = parseFloat(obj[key]);
-      // All 6 decimals for juno migaloo and huahua
-      const to_big_denom = (originalValue / 1000000).toString();
-      new_balance[newKey] = to_big_denom;
-    }
-  }
-  return new_balance;
+const config = {
+  juno: { owner: 'juno1puyjxrxkkwc9ms63a297vx2aln4kqsaeegnclknt99py59elandses9f3j', },
+  migaloo: { owner: 'migaloo1q6vmqprwvay5p3l0d763v50ufunt7fwfnfwp85wne5xan4meeqpsdvzyvy', },
+  chihuahua: { owner: 'chihuahua18s2dazpmva4t38rtnrlj3gjpsntmcdrk6v9220kt4yxckhqus3vssqsrgp', },
 }
 
 module.exports = {
   timetravel: false,
-  juno: { tvl: () => tvl("juno", "juno1puyjxrxkkwc9ms63a297vx2aln4kqsaeegnclknt99py59elandses9f3j") },
-  migaloo: { tvl: () => tvl("migaloo", "migaloo1q6vmqprwvay5p3l0d763v50ufunt7fwfnfwp85wne5xan4meeqpsdvzyvy") },
-  chihuahua: { tvl: () => tvl("chihuahua", "chihuahua18s2dazpmva4t38rtnrlj3gjpsntmcdrk6v9220kt4yxckhqus3vssqsrgp") },
 }
+
+Object.keys(config).forEach(chain => {
+  const { owner } = config[chain]
+  module.exports[chain] = { tvl: sumTokensExport({ owner }) }
+})
