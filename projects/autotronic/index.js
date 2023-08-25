d47b0ab71d9000b5641cb25273a2a6acc694fedf
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-25 22:52:02 +0200

    code refactor

--- projects/autotronic/index.js
+++ projects/autotronic/index.js
@@ -1,9 +1,7 @@
-const { uniTvlExport } = require("../helper/calculateUniTvl");
+const { getUniTVL } = require('../helper/unknownTokens')
 module.exports = {
-  doublecounted: false,
-  timetravel: true,
   start: 1692842880,
   base: {
-    tvl: uniTvlExport("0x55b3409335B81E7A8B7C085Bbb4047DDc23f7257", "base", true),
+    tvl: getUniTVL({ factory: '0x55b3409335B81E7A8B7C085Bbb4047DDc23f7257', useDefaultCoreAssets: true, fetchBalances: true, }),
   },
 };
