cbb9bff9181d929c77ce0bf3eff9913b63e4d520
realdealshaman <realdealshaman11@gmail.com>
2023-08-25 05:41:51 +0100

    fix date

--- projects/gplx/index.js
+++ projects/gplx/index.js
@@ -3,7 +3,7 @@ const { gmxExports } = require('../helper/gmx')
 module.exports = {
   hallmarks: [
     [1687320000, "Token supply compromise"],
-    [1692921600, "Protocol relauch"]
+    [1692835200, "Protocol relauch"]
   ],
   pulse:{
     tvl: gmxExports({ vault: '0x4a305E6F8724Cb5F0106C8CdC90e9C6CA6429083', })
