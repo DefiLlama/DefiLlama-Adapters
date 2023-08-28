8c5bc102f094188c8d2966269ddadded6d35df26
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-28 15:13:36 +0200

    code refactor

--- projects/alienbase/index.js
+++ projects/alienbase/index.js
@@ -1,23 +1,12 @@
 const { getUniTVL } = require('../helper/unknownTokens')
-const ADDRESSES = require("../helper/coreAssets.json");
-const sdk = require("@defillama/sdk");
-const BigNumber = require("bignumber.js");
 const ALB = "0x1dd2d631c92b1aCdFCDd51A0F7145A50130050C4";
 const FACTORY = "0x3E84D913803b02A4a7f027165E8cA42C14C0FdE7"
-
-
-const dexTVL = getUniTVL({
-    factory: FACTORY,
-    useDefaultCoreAssets: true,
-    fetchBalances: true,
-})
-
 
 
 module.exports = {
     misrepresentedTokens: true,
     methodology: `Uses Uniswap-style factory address to find and price liquidity pairs.`,
     base: {
-        tvl: dexTVL,
+        tvl: getUniTVL({ factory: FACTORY, useDefaultCoreAssets: true, fetchBalances: true, }),
     }
 };
