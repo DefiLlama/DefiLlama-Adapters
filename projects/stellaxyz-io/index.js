cc4eec4a57507c62697f39f0ef7680d6d70a72ad
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-28 10:10:19 +0200

    update stellaxyz

--- projects/stellaxyz-io/index.js
+++ projects/stellaxyz-io/index.js
@@ -1,36 +1,29 @@
 const { sumTokens2 } = require('../helper/unwrapLPs')
 const { getLogs, getAddress } = require('../helper/cache/getLogs');
+const { cachedGraphQuery } = require('../helper/cache')
 
 const config = {
   arbitrum: {
-    lendingContracts: [
-      '0x3251f402cc06b33e742f08e1adbe0d2e4c1ea2fa',
-      '0x36569fbc5a9d4c59d71e81d46db24256a09d1ad6',
-      '0x5ecb93b3ef882bf42fee65541942d50a7dab4b33',
-      '0x8e57143d14bae132210cfeec58d0c48875f7d415',
-      '0xab416e57ec74e87295b8a1507745a954b0bb9f02',
-    ],
-    factory: '0x573a89fBc6b4a5B11a55DC9814A1018a3A9cD0CA',
-    fromBlock: 101291920,
+    endpoint: 'https://graph.stellaxyz.io/v1/graphql',
+    // factory: '0x573a89fBc6b4a5B11a55DC9814A1018a3A9cD0CA',
+    // fromBlock: 101291920,
   }
 }
 
-module.exports = {
-};
+const query = `query GET_ALL_LENDING_POOLS{  pools: lending_pool_registry(    where: {_or: [{is_launched: {_eq: true}}], chain_id: {_eq: 42161}, is_deprecated: {_eq: false}}  ) {    tokenName: lending_token_name      poolAddress: pool_address     }}`
+const strategyQuery = `query Strategy {  strategy(    where: {chain_id: {_eq: 42161 }, _or: [{is_launched: {_eq: true}}]}   order_by: {display_priority: asc}  ) {    id    name    strategyAddress: strategy_address  }}`
 
 Object.keys(config).forEach(chain => {
-  const { lendingContracts, factory, fromBlock, } = config[chain]
+  const { endpoint, factory, fromBlock, } = config[chain]
   module.exports[chain] = {
     tvl: async (_, _b, _cb, { api, }) => {
-      const logs = await getLogs({
-        api,
-        target: factory,
-        topics: ['0x0803371633b57311f58d10924711080d2dae75ab17c5c0c262af3887cfca00bb'],
-        fromBlock,
-      })
+      const { pools } = await cachedGraphQuery('stellaxyz/lending-pool/'+api.chain, endpoint, query)
+      const { strategy } = await cachedGraphQuery('stellaxyz/strategy/'+api.chain, endpoint, strategyQuery)
+      const lendingContracts = pools.map(i => i.poolAddress)
+      const strategies = strategy.map(i => i.strategyAddress)
+      // const logs = await getLogs({        api,        target: factory,        topics: ['0x0803371633b57311f58d10924711080d2dae75ab17c5c0c262af3887cfca00bb'],        fromBlock,      })
 
-      const strategies = logs.map(i => getAddress(i.data.slice(64, 64 * 2 + 2)))
-      const positionManagers = await api.multiCall({  abi: 'address:positionManager', calls: strategies})
+      const positionManagers = await api.multiCall({ abi: 'address:positionManager', calls: strategies })
 
       const tokens = await api.multiCall({ abi: 'address:depositToken', calls: lendingContracts })
       await sumTokens2({ api, tokensAndOwners2: [tokens, lendingContracts] })
