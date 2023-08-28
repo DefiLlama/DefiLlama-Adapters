b7b2898dee5cfc3e5ef0173e82ccb0ba99c6e116
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-08-28 16:51:52 +0200

    minor fix

--- projects/moonbiz/index.js
+++ projects/moonbiz/index.js
@@ -2,4 +2,4 @@ const { uniTvlExport } = require("../helper/unknownTokens");
 
 const factory = "0x9B2593839E1390ECee3B348a47B3D93b2Ec2834C";
 
-module.exports = uniTvlExport('bsc', factory)
+module.exports = uniTvlExport('bsc', factory, { fetchBalances: true, })
