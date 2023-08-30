0b2d525f6f00a38c37fb610bbad3dabd14e53b18
byyyyyyyyy <byronbarra09@gmail.com>
2023-08-30 17:31:06 +0800

    finalized tvl field

--- projects/wise-lending/index.js
+++ projects/wise-lending/index.js
@@ -1,15 +1,13 @@
-const sdk = require('@defillama/sdk');
 const axios = require('axios');
 
-async function tvl() {
+async function fetch() {
     const tvlJson = await axios.get('https://data.wisetoken.net/WISE/globals/tvl.json');
     return tvlJson.data.tvlLending;
 }
 
 module.exports = {
-  misrepresentedTokens: true,
-  timetravel: false,
   ethereum: {
-    tvl,
-  }
+    fetch
+  },
+  fetch
 };
