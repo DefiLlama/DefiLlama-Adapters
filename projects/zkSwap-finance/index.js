b2be2f35384160f8183d5e6f57b11cc72abef7c9
not found <notfound@gmail.com>
2023-08-31 10:53:08 +0700

    update TVL ZF token and staking pools

--- projects/zkSwap-finance/index.js
+++ projects/zkSwap-finance/index.js
@@ -1,30 +1,18 @@
-const { getLogs } = require('../helper/cache/getLogs')
-const { sumTokens2 } = require('../helper/unwrapLPs')
+const ADDRESSES = require('./helper/coreAssets.json')
+const { getUniTVL } = require('../helper/unknownTokens')
+const {stakingPricedLP} = require("../helper/staking")
 
 const FACTORY = '0x3a76e377ed58c8731f9df3a36155942438744ce3'
 const CHAIN = 'era'
 
-async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
-  const factoryLogs = await getLogs({
-    api,
-    target: FACTORY,
-    fromBlock: 4182273,
-    topic: 'PairCreated(address,address,address,uint256)',
-    eventAbi: 'event PairCreated(address indexed token0, address indexed token1, address pair, uint256)',
-    onlyArgs: true,
-  })
-  
-  let balanceRequests = []
-  factoryLogs.forEach(({ token0, token1, pair}) => {
-    balanceRequests.push([token0, pair])
-    balanceRequests.push([token1, pair])
-  })
-  return sumTokens2({ chain: CHAIN, ethBlock, tokensAndOwners: balanceRequests })
-}
-
-
 module.exports = {
-  era: { tvl, },
+  era: {
+    tvl: getUniTVL({
+      factory: FACTORY,
+      useDefaultCoreAssets: true,
+    }),
+    staking: stakingPricedLP('0x9F9D043fB77A194b4216784Eb5985c471b979D67','0x31C2c031fDc9d33e974f327Ab0d9883Eae06cA4A', CHAIN, ADDRESSES.era.WETH, 'weth', false)
+  },
   methodology: "TVL is total liquidity of all liquidity pools."
 }
 
