83914a5ad70fdde88b99748ac1a2aac162f435a6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-01 15:08:29 +0200

    minor fix

--- projects/sonne-finance/index.js
+++ projects/sonne-finance/index.js
@@ -1,29 +1,13 @@
-const { compoundExports } = require('../helper/compound')
+const { compoundExports2 } = require('../helper/compound')
 const { staking } = require('../helper/staking');
 
-const optimismConfig = {
-  unitroller: '0x60CF091cD3f50420d50fD7f707414d0DF4751C58',
-  chain: 'optimism'
+
+const config = {
+	optimism: '0x60CF091cD3f50420d50fD7f707414d0DF4751C58',
+	base: '0x1DB2466d9F5e10D7090E7152B68d62703a2245F0'
 }
 
-const baseConfig = {
-  unitroller: '0x1DB2466d9F5e10D7090E7152B68d62703a2245F0',
-  chain: 'base'
-}
-
-// OPTIMISM CHAIN
-const optimismTVL = compoundExports(optimismConfig.unitroller, optimismConfig.chain)
-
-// BASE CHAIN
-const baseTVL = compoundExports(baseConfig.unitroller, baseConfig.chain)
-
-module.exports = {
-	optimism: { ...optimismTVL,
-				staking: staking(
-					["0xdc05d85069dc4aba65954008ff99f2d73ff12618",
-					 "0x41279e29586eb20f9a4f65e031af09fced171166"],
-					"0x1DB2466d9F5e10D7090E7152B68d62703a2245F0"
-					),
-	     	},
-	base: { ...baseTVL }
-}
+Object.keys(config).forEach(chain => {
+	module.exports[chain] = compoundExports2({ comptroller: config[chain] })
+})
+module.exports.optimism.staking = staking(["0xdc05d85069dc4aba65954008ff99f2d73ff12618", "0x41279e29586eb20f9a4f65e031af09fced171166"], "0x1DB2466d9F5e10D7090E7152B68d62703a2245F0")
