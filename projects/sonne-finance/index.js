a945f3c78d74069ec4c7c4e3aa037eb83058ea23
halfdoctor <nmanura@gmail.com>
2023-09-01 12:39:50 +0100

    Update index.js

--- projects/sonne-finance/index.js
+++ projects/sonne-finance/index.js
@@ -20,10 +20,7 @@ const baseTVL = compoundExports(baseConfig.unitroller, baseConfig.chain)
 module.exports = {
   optimism: { ...optimismTVL,
 				staking: staking(
-				  [
-					"0xdc05d85069dc4aba65954008ff99f2d73ff12618",
-					"0x41279e29586eb20f9a4f65e031af09fced171166",
-				  ],
+				  ["0xdc05d85069dc4aba65954008ff99f2d73ff12618","0x41279e29586eb20f9a4f65e031af09fced171166"],
 				  "0x1DB2466d9F5e10D7090E7152B68d62703a2245F0"
 				),
 	          },
