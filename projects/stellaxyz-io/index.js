48ac08c0a56c3dd4fb82f2766fa59f725a575729
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-01 18:08:39 +0200

    stealla: track traderjoe positions

--- projects/stellaxyz-io/index.js
+++ projects/stellaxyz-io/index.js
@@ -1,6 +1,8 @@
 const { sumTokens2 } = require('../helper/unwrapLPs')
 const { getLogs, getAddress } = require('../helper/cache/getLogs');
 const { cachedGraphQuery } = require('../helper/cache')
+const { graphQuery } = require('../helper/http');
+const ADDRESSES = require('../helper/coreAssets.json')
 
 const config = {
   arbitrum: {
@@ -10,6 +12,10 @@ const config = {
   }
 }
 
+module.exports = {
+  misrepresentedTokens: true,
+}
+
 const query = `query GET_ALL_LENDING_POOLS{  pools: lending_pool_registry(    where: {_or: [{is_launched: {_eq: true}}], chain_id: {_eq: 42161}, is_deprecated: {_eq: false}}  ) {    tokenName: lending_token_name      poolAddress: pool_address     }}`
 const strategyQuery = `query Strategy {  strategy(    where: {chain_id: {_eq: 42161 }, _or: [{is_launched: {_eq: true}}]}   order_by: {display_priority: asc}  ) {    id    name    strategyAddress: strategy_address  }}`
 
@@ -17,17 +23,48 @@ Object.keys(config).forEach(chain => {
   const { endpoint, factory, fromBlock, } = config[chain]
   module.exports[chain] = {
     tvl: async (_, _b, _cb, { api, }) => {
-      const { pools } = await cachedGraphQuery('stellaxyz/lending-pool/'+api.chain, endpoint, query)
-      const { strategy } = await cachedGraphQuery('stellaxyz/strategy/'+api.chain, endpoint, strategyQuery)
+      await getStraegyTvl2()
+      const { pools } = await cachedGraphQuery('stellaxyz/lending-pool/' + api.chain, endpoint, query)
       const lendingContracts = pools.map(i => i.poolAddress)
-      const strategies = strategy.map(i => i.strategyAddress)
-      // const logs = await getLogs({        api,        target: factory,        topics: ['0x0803371633b57311f58d10924711080d2dae75ab17c5c0c262af3887cfca00bb'],        fromBlock,      })
-
-      const positionManagers = await api.multiCall({ abi: 'address:positionManager', calls: strategies })
-
       const tokens = await api.multiCall({ abi: 'address:depositToken', calls: lendingContracts })
       await sumTokens2({ api, tokensAndOwners2: [tokens, lendingContracts] })
-      return sumTokens2({ api, owners: positionManagers, resolveUniV3: true, })
+
+      async function getStraegyTvl2() {
+        let hasMore = false
+        let offset = 0
+        do {
+          const { position } = await graphQuery(endpoint, `query GetPositionsDefillama($offset:Int!) {
+            position(order_by:{opened_at:asc},where: {status: {_eq: "Active"}}, limit: 1000, offset: $offset) {
+            id
+              position_value_usd
+              strategy{
+                id
+                strategy_address
+                lp_address
+                pool_address
+              }
+              opened_at
+              closed_at
+              status
+            }
+          }`, {offset})
+          hasMore = position.length === 1000
+          position.forEach(i => {
+            api.add(ADDRESSES.arbitrum.USDC, i.position_value_usd * 1e6)
+          })
+        } while (hasMore)
+      }
+
+      async function getStategyTvl() {
+        const { strategy } = await cachedGraphQuery('stellaxyz/strategy/' + api.chain, endpoint, strategyQuery)
+        const strategies = strategy.map(i => i.strategyAddress)
+        // const logs = await getLogs({        api,        target: factory,        topics: ['0x0803371633b57311f58d10924711080d2dae75ab17c5c0c262af3887cfca00bb'],        fromBlock,      })
+
+        const positionManagers = await api.multiCall({ abi: 'address:positionManager', calls: strategies })
+        return sumTokens2({ api, owners: positionManagers, resolveUniV3: true, })
+
+
+      }
     }
   }
 })
