dcab12d672f801fa3f391a982ad4c6e032d11597
LastChance <104254800+lastchancedegen@users.noreply.github.com>
2023-09-04 17:09:00 +0100

    Add ComfySwap to Shibarium (#7370)
    * Add ComfySwap
    
    * minor fix
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -64,6 +64,7 @@ const fixBalancesTokens = {
   },
   shibarium: {
     '0x8ed7d143ef452316ab1123d28ab302dc3b80d3ce':  { coingeckoId: "ethereum", decimals: 18 },
+    '0xc76f4c819d820369fb2d7c1531ab3bb18e6fe8d8':  { coingeckoId: "bone-shibaswap", decimals: 18 },
     '0x213c25900f365f1be338df478cd82bef7fd43f85':  { coingeckoId: "bone-shibaswap", decimals: 18 },
     '0x6c19a35875217b134e963ca9e61b005b855cad21':  { coingeckoId: "bone-shibaswap", decimals: 18 },
     '0x1b2f364032f12bd8a4c89e672e6272de03ae2680':  { coingeckoId: "bone-shibaswap", decimals: 18 },
