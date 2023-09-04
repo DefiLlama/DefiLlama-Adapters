8c0bf356e79153332fd771851a9c29ade27d062f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-04 17:20:00 +0200

    Update index.js

--- projects/parallax/index.js
+++ projects/parallax/index.js
@@ -1,30 +1,13 @@
 const sdk = require("@defillama/sdk");
 const { default: BigNumber } = require("bignumber.js");
 
+const { staking } = require('../helper/staking')
+const { sumTokensExport } = require('../helper/unwrapLPs')
 const { getPriceMIM, getPriceAura, getPriceSushi } = require("./getPrice");
 
 const prllxERC20 = require("./abis/prllxERC20.json");
 const contracts = require("./contracts.json");
 
-const arbitrumPools = [
-  // pool, token, representation
-
-  [
-    "0x82FD636D7A28a20635572EB8ec0603ee264B8651",
-    "0xc8CCBd97b96834b976C995a67BF46e5754e2C48E",
-    "arbitrum:0xc8CCBd97b96834b976C995a67BF46e5754e2C48E",
-  ], // 3-Months Vault
-  [
-    "0xA3CE2c0d1cfB29F398f8f4800bA202Aba39dbbfe",
-    "0xc8CCBd97b96834b976C995a67BF46e5754e2C48E",
-    "arbitrum:0xc8CCBd97b96834b976C995a67BF46e5754e2C48E",
-  ], // 12-Months Vault
-  [
-    "0xEb370470Afd74d8a9BBC4fF0C94371C310fF9D3e",
-    "0xc8CCBd97b96834b976C995a67BF46e5754e2C48E",
-    "arbitrum:0xc8CCBd97b96834b976C995a67BF46e5754e2C48E",
-  ], // Farming Vault
-];
 async function ethTvl(time, _ethBlock, { ethereum: block }, { api }) {
   const strategyId = await api.call({
     target: contracts.eth.parallaxAddress,
@@ -162,22 +145,6 @@ async function arbitrumTvl(time, _ethBlock, { arbitrum: block }, { api }) {
     totalStakedTVL
   );
 
-  // Vaults
-  await Promise.all(
-    arbitrumPools.map((pool) =>
-      sdk.api.erc20
-        .balanceOf({
-          target: pool[1],
-          owner: pool[0],
-          chain: "arbitrum",
-          block,
-        })
-        .then((result) =>
-          sdk.util.sumSingleBalance(balances, pool[2], result.output)
-        )
-    )
-  );
-
   return balances;
 }
 
@@ -185,6 +152,8 @@ module.exports = {
   methodology: "TVL comes from the Staking Vaults",
   arbitrum: {
     tvl: arbitrumTvl,
+    staking: staking(['0x82FD636D7A28a20635572EB8ec0603ee264B8651', '0xA3CE2c0d1cfB29F398f8f4800bA202Aba39dbbfe', '0xEb370470Afd74d8a9BBC4fF0C94371C310fF9D3e', ], '0xc8CCBd97b96834b976C995a67BF46e5754e2C48E'),
+    pool2: sumTokensExport({ owner: '0xEb370470Afd74d8a9BBC4fF0C94371C310fF9D3e', resolveUniV3: true, })
   },
   ethereum: {
     tvl: ethTvl,
