abede607ae9f71ddc5a0062a030eeaf4a5fd4196
PolyLend <144064665+polylendxyz@users.noreply.github.com>
2023-09-05 12:12:56 +0300

    Added polylend market (#7374)

---
+++ projects/polylend/index.js
@@ -0,0 +1,7 @@
+const { aaveExports } = require("../helper/aave");
+
+module.exports = {
+  polygon_zkevm: aaveExports("polygon_zkevm", undefined, undefined, [
+    "0x2C21bA5b8795A1efeC48b34d2FD7787d7547b494",
+  ]),
+};
