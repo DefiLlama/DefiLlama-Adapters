1449d22d57e996cd50401b8f31b6a9c51d25d4f4
PolyLend <144064665+polylendxyz@users.noreply.github.com>
2023-09-06 03:50:33 +0300

    Update index.js

--- projects/polylend/index.js
+++ projects/polylend/index.js
@@ -2,6 +2,6 @@ const { aaveExports } = require("../helper/aave");
 
 module.exports = {
   polygon_zkevm: aaveExports("polygon_zkevm", undefined, undefined, [
-    "0x2C21bA5b8795A1efeC48b34d2FD7787d7547b494",
+    "0x25D99D76710266f32A6E6E4CbDCD47A61FE1f004",
   ]),
 };
