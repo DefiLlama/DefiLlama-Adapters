dcc129e988a8eaab75fbb9e58458e3f1bd5015e8
layerbank <135123153+layerbank@users.noreply.github.com>
2023-09-06 13:39:26 +0900

    Add LayerBank

---
+++ projects/layerbank/index.js
@@ -0,0 +1,11 @@
+const { compoundExports2 } = require("../helper/compound");
+
+module.exports = {
+  linea: compoundExports2({
+    comptroller: "0x009a0b7C38B542208936F1179151CD08E2943833",
+    abis: {
+      getAllMarkets: "address[]:allMarkets",
+      totalBorrows: "uint256:totalBorrow",
+    },
+  }),
+};
