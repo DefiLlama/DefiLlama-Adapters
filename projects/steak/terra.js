49f65f25d74188ccc30ab4e871615130fc6350ec
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-06 13:14:20 +0200

    reduce reliance on fetch

--- projects/steak/terra.js
+++ projects/steak/terra.js
@@ -1,9 +1,3 @@
-const axios = require("axios");
-
-const { encodeBase64 } = require("./helpers.js");
-
-const GRPC_API_URL = "https://lcd.terra.dev";
-const STEAK_HUB = "terra15qr8ev2c0a0jswjtfrhfaj5ucgkhjd7la2shlg";
 
 async function tvl() {
   return {}
