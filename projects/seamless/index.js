35537dde59c1d52202527dc86ef61c9179948c6c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-08 08:34:58 +0200

    code refactor

--- projects/seamless/index.js
+++ projects/seamless/index.js
@@ -1,192 +1,49 @@
-const sdk = require("@defillama/sdk");
-const BigNumber = require("bignumber.js");
 const { aaveExports } = require("../helper/aave");
 const { sumTokens2 } = require("../helper/unwrapLPs");
 const abi = require("./abis.json");
+const { mergeExports } = require("../helper/utils");
 
-const AAVE_ADDRESSES_PROVIDER_REGISTRY =
-  "0x90C5055530C0465AbB077FA016a3699A3F53Ef99";
+const AAVE_ADDRESSES_PROVIDER_REGISTRY = "0x90C5055530C0465AbB077FA016a3699A3F53Ef99";
 const AAVE_POOL_DATA_PROVIDER = "0x2A0979257105834789bC6b9E1B00446DFbA8dFBa";
 const GEYSER_REGISTRY = "0xD5815fC3D736120d07a1fA92bA743c1167dA89d8";
 
-function geyserExports(chain) {
-  return async (_, _1, _2, { block }) => {
-    const geyserCount = (
-      await sdk.api.abi.call({
-        target: GEYSER_REGISTRY,
-        abi: abi["instanceCount"],
-        chain,
-        block,
-      })
-    ).output;
+async function geyserTvl(_, _1, _2, { api }) {
+  const aTokens = await api.call({  abi: "function getAllATokens() view returns (tuple(string symbol, address tokenAddress)[])", target: AAVE_POOL_DATA_PROVIDER })
+  const aTokenSet = new Set(aTokens.map(t => t.tokenAddress.toLowerCase()))
+  const geysers = await api.fetchList({ lengthAbi: abi.instanceCount, itemAbi: abi.instanceAt, target: GEYSER_REGISTRY })
+  const geysersData = await api.multiCall({ calls: geysers, abi: abi["getGeyserData"], })
+  const stakingTokens = geysersData.map(d => d.stakingToken)
+  const wrappedUnderlying = await api.multiCall({ calls: stakingTokens, abi: abi["underlying"], permitFailure: true, })
+  const uTokens = wrappedUnderlying.filter(i => i && !aTokenSet.has(i.toLowerCase()))
+  const uOwners = stakingTokens.filter((_, i) => wrappedUnderlying[i]&& !aTokenSet.has(wrappedUnderlying[i].toLowerCase()))
 
-    console.log("geyserCount: ", geyserCount);
+  const notWrappedTokensAndOwners = wrappedUnderlying.map((w, i) => {
+    if (w) return;
+    return [stakingTokens[i], geysers[i]];
+  }).filter(i => i)
 
-    const calls = [];
-    for (let i = 0; i < geyserCount; i++) {
-      calls.push({
-        target: GEYSER_REGISTRY,
-        params: [i],
-        chain,
-        block,
-      });
-    }
+  const aTokenUnderlying = await api.multiCall({ calls: uTokens, abi: abi["UNDERLYING_ASSET_ADDRESS"], })
 
-    console.log("calls[]: ", calls);
+  const notATokenTokensAndOwners = aTokenUnderlying.map((w, i) => {
+    if (w) return;
+    return [uTokens[i], uOwners[i]];
+  }).filter(i => i)
 
-    const geysers = (
-      await sdk.api.abi.multiCall({
-        calls,
-        abi: abi["instanceAt"],
-        chain,
-        block,
-      })
-    ).output;
+  const underlyingBalance = await api.multiCall({ calls: uOwners, abi: abi["totalUnderlying"], permitFailure: true, });
 
-    console.log("geysers: ", geysers);
+  await sumTokens2({ api, tokensAndOwners: [notATokenTokensAndOwners, notWrappedTokensAndOwners].flat(), });
 
-    const geysersData = (
-      await sdk.api.abi.multiCall({
-        calls: geysers.map((g) => ({
-          target: g.output,
-          params: [],
-        })),
-        abi: abi["getGeyserData"],
-        chain,
-        block,
-      })
-    ).output;
+  underlyingBalance.forEach((bal, i) => {
+    if (!bal) return;
+    api.add(aTokenUnderlying[i], bal)
+  });
 
-    console.log("geysersData: ", geysersData);
-
-    const wrappedUnderlying = (
-      await sdk.api.abi.multiCall({
-        calls: geysersData.map((d) => ({
-          target: d.output.stakingToken,
-          params: [],
-        })),
-        abi: abi["underlying"],
-        chain,
-        block,
-        permitFailure: true,
-      })
-    ).output;
-
-    const notWrappedTokensAndOwners = wrappedUnderlying
-      .map((w, i) => {
-        if (w.success) {
-          return;
-        }
-
-        return [w.input.target, geysersData[i].input.target];
-      })
-      .filter((f) => f);
-
-    console.log("notWrappedTokensAndOwners: ", notWrappedTokensAndOwners);
-    console.log("wrappedUnderlying: ", wrappedUnderlying);
-
-    const aTokenUnderlying = (
-      await sdk.api.abi.multiCall({
-        calls: wrappedUnderlying.map((d) => ({
-          target: d.output,
-          params: [],
-        })),
-        abi: abi["UNDERLYING_ASSET_ADDRESS"],
-        permitFailure: true,
-        chain,
-        block,
-      })
-    ).output;
-
-    const notATokenTokensAndOwners = aTokenUnderlying
-      .map((w, i) => {
-        if (w.success) {
-          return;
-        }
-
-        return [w.input.target, wrappedUnderlying[i].input.target];
-      })
-      .filter((f) => f);
-
-    console.log("notATokenTokensAndOwners: ", notATokenTokensAndOwners);
-    console.log("aTokenUnderlying: ", aTokenUnderlying);
-
-    const underlyingBalance = await sdk.api.abi.multiCall({
-      calls: geysersData.map((d) => ({
-        target: d.output.stakingToken,
-        params: [],
-      })),
-      abi: abi["totalUnderlying"],
-      chain,
-      block,
-      permitFailure: true,
-    });
-
-    console.log("underlyingBalance: ", JSON.stringify(underlyingBalance));
-
-    let balances = await sumTokens2({
-      chain,
-      block,
-      tokensAndOwners: notWrappedTokensAndOwners.concat(
-        notATokenTokensAndOwners
-      ),
-    });
-
-    aTokenUnderlying
-      .forEach((t, i) => {
-        if (!t.success) {
-          return;
-        }
-
-        let address = t.output;
-        const balance = BigNumber(underlyingBalance.output[i].output);
-
-        if (!address || balance.lte(0)) {
-          return;
-        }
-
-        address = `${chain}:${address}`;
-
-        balances[address] = BigNumber(balances[address] ?? 0)
-          .plus(balance)
-          .toString();
-      });
-
-    console.log("balances: ", balances);
-
-    return balances;
-  };
+  return api.getBalances()
 }
 
-function aggregate() {
-  const { tvl: tvlAave, borrowed } = aaveExports(
-    "base",
-    AAVE_ADDRESSES_PROVIDER_REGISTRY,
-    undefined,
-    [AAVE_POOL_DATA_PROVIDER],
-    { v3: true }
-  );
-  const tvlGeyser = geyserExports("base");
+const baseAAVE = aaveExports("base", AAVE_ADDRESSES_PROVIDER_REGISTRY, undefined, [AAVE_POOL_DATA_PROVIDER], { v3: true });
 
-  return {
-    tvl: async (_, _1, _2, { api, block }) => {
-      const balances = {};
-      sdk.util.mergeBalances(balances,
-        await tvlGeyser(_, _1, _2, { api, block })
-      );
-      sdk.util.mergeBalances(balances,
-        await tvlAave(_, _1, _2, { api, block })
-      );
-      console.log("merged balances: ", balances);
-      return balances;
-    },
-    borrowed,
-  };
-}
-
-module.exports = {
+module.exports = mergeExports([{
   methodology: `Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There's multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending`,
-  base: aggregate(),
-  start: 1,
-};
-// node test.js projects/seamless/index.js
+  base: baseAAVE,
+}, { base: { tvl: geyserTvl } }]);
