3f638f8632d5d19c1c0d31d6acece0b1b5cd64a9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-11 09:43:45 +0200

    minor refactor

--- projects/dackieswap-v2/index.js
+++ projects/dackieswap-v2/index.js
@@ -1,15 +1,6 @@
-const { getUniTVL, sumTokensExport } = require('../helper/unknownTokens')
-const DACKIE = "0xc2BC7A73613B9bD5F373FE10B55C59a69F4D617B";
-const FACTORY = "0x591f122D1df761E616c13d265006fcbf4c6d6551"
-
-
-const dexTVL = getUniTVL({ factory: FACTORY, useDefaultCoreAssets: true })
-
+const { getUniTVL, } = require('../helper/unknownTokens')
 
 module.exports = {
-    misrepresentedTokens: true,
-    methodology: `Uses factory(${FACTORY}) address and whitelisted tokens address to find and price Liquidity Pool pairs.`,
-    base: {
-        tvl: dexTVL
-    }
+  misrepresentedTokens: true,
+  base: { tvl: getUniTVL({ factory: '0x591f122D1df761E616c13d265006fcbf4c6d6551', useDefaultCoreAssets: true, fetchBalances: true }) }
 };
