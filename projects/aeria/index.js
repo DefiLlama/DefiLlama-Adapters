9a5d4e4eecb2d696a1ef96c348483767d19e5e44
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-11 17:47:34 +0200

    update aeria

--- projects/aeria/index.js
+++ projects/aeria/index.js
@@ -5,8 +5,10 @@ const FACTORY = "0x649b80892ef773bd64cc3c663950dea3a604f660";
 async function tvl(_, _1, _2, { api }) {
   let vaults = await api.fetchList({ lengthAbi: 'uint256:vaultCount', itemAbi: 'function vaults(uint256) view returns (address)', target: FACTORY, startFromOne: true, })
   vaults = vaults.filter(i => i !== nullAddress)
+  console.log(vaults)
   const tokens = await api.multiCall({ abi: 'address:depositToken', calls: vaults })
-  return api.sumTokens({ tokensAndOwners2: [tokens, vaults] })
+  const bals = await api.multiCall({ abi: 'uint256:totalSupply', calls: vaults })
+  api.addTokens(tokens, bals)
 }
 
 module.exports = {
