edf5c551a75c29658c557fc27caf2d20b3f3c40c
manukyanmari <144628067+manukyanmari@users.noreply.github.com>
2023-09-11 23:21:26 +0200

    Create index.js
    I want to list my token on defillama

---
+++ projects/SyncSwap-Finance/index.js
@@ -0,0 +1,11 @@
+const SNCF_TOKEN_CONTRACT = '0xAff861770fD8BF01492B47a9e438Be0836C63054';
+
+module.exports = {
+  timetravel: true,
+  misrepresentedTokens: false,
+  methodology: 'counts the number of SNCF tokens in the SYNCSWAP FINANCE contract.',
+  start: 1000235,
+  bsc: {
+    tvl,
+  }
+};
