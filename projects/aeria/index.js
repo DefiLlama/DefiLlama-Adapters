0668bb1a9b5bc502e2f4b2b9025f48c59e651197
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-11 21:45:22 +0200

    update aeria (#7452)
    * update aeria
    
    * add funds in team wallet

--- projects/aeria/index.js
+++ projects/aeria/index.js
@@ -1,12 +1,19 @@
 const { nullAddress } = require("../helper/tokenMapping");
+const ADDRESSES = require('../helper/coreAssets.json')
 
 const FACTORY = "0x649b80892ef773bd64cc3c663950dea3a604f660";
 
-async function tvl(_, _1, _2, { api }) {
+async function tvl(timestamp, _1, _2, { api }) {
   let vaults = await api.fetchList({ lengthAbi: 'uint256:vaultCount', itemAbi: 'function vaults(uint256) view returns (address)', target: FACTORY, startFromOne: true, })
   vaults = vaults.filter(i => i !== nullAddress)
+  
   const tokens = await api.multiCall({ abi: 'address:depositToken', calls: vaults })
-  return api.sumTokens({ tokensAndOwners2: [tokens, vaults] })
+
+  // needs to move funds to Multisig in under a month
+  if (timestamp * 1e3 < +new Date('10-10-2023')) await api.sumTokens({ owner: '0x8C8bA29f177CDEC445F4B5451B57946268D044Fa', tokens: [ADDRESSES.base.USDC]})
+  return api.sumTokens({ tokensAndOwners2: [tokens, vaults]})
+  // const bals = await api.multiCall({ abi: 'uint256:totalSupply', calls: vaults })
+  // api.addTokens(tokens, bals)
 }
 
 module.exports = {
