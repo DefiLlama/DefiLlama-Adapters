2a1c7752505532e7dd815bb0a8dc01cd1d99f847
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-11 17:32:53 +0200

    code refactor

--- projects/messinaone-ls/index.js
+++ projects/messinaone-ls/index.js
@@ -1,16 +1,13 @@
-const sdk = require('@defillama/sdk')
-const { readGlobalState } = require("./utils");
+const { getAppGlobalState } = require("../helper/chain/algorand");
 
 const contractId = 1189577109
 const STATE_DEPOSIT_ALGOS = "deposit_algos"
 
 async function tvl() {
-  const depositAlgos = await readGlobalState(contractId, STATE_DEPOSIT_ALGOS);
-  const totalTvl = depositAlgos ? depositAlgos / 10 ** 6 : 0;
-  const balances = {}
-  sdk.util.sumSingleBalance(balances, "algorand", totalTvl)
-
-  return balances
+  const state = await getAppGlobalState(contractId);
+  return {
+    algorand: state[STATE_DEPOSIT_ALGOS]/1e6
+  }
 }
 
 module.exports = {
