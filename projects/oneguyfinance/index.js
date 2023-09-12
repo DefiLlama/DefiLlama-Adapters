1c620284baa7984b77d5597df52ce731809212ff
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-12 21:02:45 +0200

    lint fix

--- projects/oneguyfinance/index.js
+++ projects/oneguyfinance/index.js
@@ -1,22 +1,13 @@
-const PREDICTION_CONTRACT = "0x4e9f8e71DBbd9aCca9b4c7ae1c647FC1B45065FD";
-const sdk = require("@defillama/sdk");
-const ADDRESSES = require("../helper/coreAssets.json");
+const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
 
-async function polygon(timestamp, block, chainBlocks, { api }) {
-  let maticBalance = await sdk.api.eth.getBalance({
-    target: PREDICTION_CONTRACT,
-    block: chainBlocks["polygon"],
-    chain: "polygon",
-  });
-
-  return {
-    ["polygon:" + ADDRESSES.polygon.WMATIC_1]: maticBalance.output,
-  };
+const config = {
+  polygon: { owners: ['0x4e9f8e71DBbd9aCca9b4c7ae1c647FC1B45065FD'], tokens: [nullAddress] }
 }
 
 module.exports = {
-  polygon: {
-    tvl: polygon,
-  },
   methodology: `TVL is the total amount of MATIC held on smart-contracts.`,
-};
+}
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = { tvl: sumTokensExport(config[chain]) }
+})
