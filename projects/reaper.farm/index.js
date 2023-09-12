3f219ccf0fb08d2cb5970907b225a9e379732afd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-09-12 08:21:42 +0200

    code refactor

--- projects/reaper.farm/index.js
+++ projects/reaper.farm/index.js
@@ -1,51 +1,24 @@
-const axios = require("axios");
-const REAPER_API = "https://2ch9hbg8hh.execute-api.us-east-1.amazonaws.com/dev/api/";
-
-const client = axios.create({
-  baseURL: REAPER_API
-});
-
-function fetchTvl(chainName) {
-  return async () => {
-    let tvlMsg = await client.get('/tvls/chains');
-    let tvl = 0;
-    switch (chainName) {
-      case 'fantom':
-        tvl = tvlMsg.data.data.find((item) => item.chain_hex === '0xfa').tvl;
-        break;
-      case 'optimism':
-        tvl = tvlMsg.data.data.find((item) => item.chain_hex === '0xa').tvl;
-        break;
-      case 'arbitrum':
-        tvl = tvlMsg.data.data.find((item) => item.chain_hex === '0xa4b1').tvl;
-        break;
-      case 'bsc':
-        tvl = tvlMsg.data.data.find((item) => item.chain_hex === '0x38').tvl;
-        break;
-      default:
-        break;
-    }
-    return { tether: +tvl };
-  }
-}
+const REAPER_API = "https://2ch9hbg8hh.execute-api.us-east-1.amazonaws.com/dev/api/vaults/";
+const { getConfig } = require('../helper/cache');
+const { sumERC4626Vaults } = require('../helper/erc4626');
+const { sumTokens2 } = require('../helper/unwrapLPs');
 
 module.exports = {
-  misrepresentedTokens: false,
-  methodology: `TVL is fetched from the Reaper API`,
-  timetravel: false,
-  fantom: {
-    tvl: fetchTvl('fantom')
-  },
-  optimism: {
-    tvl: fetchTvl('optimism')
-  },
-  arbitrum: {
-    tvl: fetchTvl('arbitrum')
-  },
-  bsc: {
-    tvl: fetchTvl('bsc')
-  },
   hallmarks:[
     [1659441956, "$1.7m Exploit"],
   ]
 }
+
+const chains = ['optimism', 'arbitrum', 'fantom', 'bsc']
+
+chains.forEach(chain => {
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const hexId = '0x'+ api.chainId.toString(16)
+      const {data} = await getConfig('reaper/'+api.chain, REAPER_API + hexId)
+      const vaults = Object.values(data).map(i => i.address).filter(i => !['0x85A21D07a3DeEfe58EcD841198D7f774e6444654'].includes(i))
+      await sumERC4626Vaults({ api, vaults, abi: { asset: 'address:token', balance: 'uint256:balance' } })
+      return sumTokens2({ api, resolveLP: true, })
+    }
+  }
+})
