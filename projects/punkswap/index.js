d7b7f8e5634ff5932f425761182d7ac70e5c6d6e
ShibaPunkArmy <35100816+ShibaPunkArmy@users.noreply.github.com>
2023-09-14 13:32:11 +0200

    Added PunkSwap Factory
    Added PunkSwap Factory and Masterchef on Shibarium
    
    opBNB has only Factory for now and was also added into the index.js file

---
+++ projects/punkswap/index.js
@@ -0,0 +1,16 @@
+const { getUniTVL } = require('../helper/unknownTokens')
+const { stakings } = require('../helper/staking')
+
+module.exports = {
+
+    misrepresentedTokens: true,
+      shibarium: {
+      staking: stakings("0x336726832bbE10FBED4C80Bc201F728B1404B073", "shibarium"),
+      tvl: getUniTVL({ factory: '0x5640113EA7F369E6DAFbe54cBb1406E5BF153E90', useDefaultCoreAssets: true, })
+    },
+
+    op_bnb: {
+      tvl: getUniTVL({ factory: '0x5640113EA7F369E6DAFbe54cBb1406E5BF153E90', useDefaultCoreAssets: true, })
+    },
+
+}
