74f893ff70ce250e26935ec9e118f34c32cb90a8
realdealshaman <realdealshaman11@gmail.com>
2023-09-15 19:52:30 +0100

    add bsharerewardpool

--- projects/basedfarm/index.js
+++ projects/basedfarm/index.js
@@ -21,11 +21,18 @@ async function tvl(_, _b, _cb, { api, }) {
 }
 
 async function pool2(timestamp, ethBlock, chainBlocks) {
-  const pools = [{
+  const pools = [
+    { // BasedRewardPool
     'pool2Address': '0x5F45e48F9C053286cE9Ca08Db897f8b7eb3f7992',
     'pairToken': ADDRESSES.base.WETH,
     'stakingContract': '0x8A75C6EdD19d9a72b31774F1EE2BC45663d30733'
-  }];
+    },
+    { // bShareRewardPool
+      'pool2Address': '0xbE23ce31C665225E27521D3d7DB9Bb7E5A76aeb8',
+      'pairToken': ADDRESSES.base.WETH,
+      'stakingContract': '0x227F33775f1320959bAA17280310Fab9ACc4Aa6C'
+    }
+];
 
   const tokensAndOwners = pools.map(i => ([i.pool2Address, i.stakingContract]));
   const coreAssets = pools.map(i => i.pairToken);
@@ -34,6 +41,6 @@ async function pool2(timestamp, ethBlock, chainBlocks) {
 }
 
 module.exports = {
-  methodology: 'TVL is based on value of the single-sided staked tokens inside of the Genesis reward pool. Pool2 TVL represents the value of specific LP tokens staked in designated pool2 staking contract(BasedRewardPool)',
+  methodology: 'TVL is based on value of the single-sided staked tokens inside of the Genesis reward pool. Pool2 TVL represents the value of specific LP tokens staked in designated pool2 staking contracts(BasedRewardPool & bShareRewardPool)',
   base: { tvl,pool2 },
 };
