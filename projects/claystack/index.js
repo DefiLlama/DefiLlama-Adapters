22c288cd58747e0dc4b302e53d8907bf3c339313
Sagar Chaurasia <sagar.chaurasia@claystack.com>
2023-09-15 16:05:38 +0530

    added csETH tvl support

--- projects/claystack/index.js
+++ projects/claystack/index.js
@@ -4,14 +4,15 @@ const abi = require('./clayABIs/clayMain.json');
 
 const clayAddresses = {
   clayMatic: "0x91730940DCE63a7C0501cEDfc31D9C28bcF5F905",
+  clayEth: "0x331312DAbaf3d69138c047AaC278c9f9e0E8FFf8"
 };
 
 const coinAddresses = {
   matic: ADDRESSES.ethereum.MATIC,
 };
 
-async function getClayMaticTvl(_,block) {
-  const deposits = (
+async function getClayTvlOnEthereum(_, block) {
+  const maticDeposits = (
     await sdk.api.abi.call({
       target: clayAddresses.clayMatic,
       abi: abi.funds,
@@ -19,13 +20,26 @@ async function getClayMaticTvl(_,block) {
       block
     })
   ).output;
-  return { [coinAddresses.matic]: deposits.currentDeposit };
+
+  const ethDeposits = (
+    await sdk.api.abi.call({
+      target: clayAddresses.clayEth,
+      abi: abi.funds,
+      chain: "ethereum",
+      block
+    })
+  ).output;
+
+  return {
+    [ADDRESSES.null]: ethDeposits.currentDeposit,
+    [coinAddresses.matic]: maticDeposits.currentDeposit
+  };
 }
 
 module.exports = {
 
   ethereum: {
-    tvl: getClayMaticTvl,
+    tvl: getClayTvlOnEthereum,
   },
   methodology: `We get the total token deposited in clay contracts and convert it to USD.`
 }
