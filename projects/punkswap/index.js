c058fe8ba4e6b642dcf48c918727fa5ff026d509
ShibaPunkArmy <35100816+ShibaPunkArmy@users.noreply.github.com>
2023-09-19 09:43:03 +0200

    update index.js PunkSwap
    updated index.js for Punkswap DEX

--- projects/punkswap/index.js
+++ projects/punkswap/index.js
@@ -1,16 +1,10 @@
-const { getUniTVL } = require('../helper/unknownTokens')
-const { stakings } = require('../helper/staking')
+const {getUniTVL} = require("../helper/unknownTokens");
 
 module.exports = {
-
-    misrepresentedTokens: true,
-      shibarium: {
-      staking: stakings("0x336726832bbE10FBED4C80Bc201F728B1404B073", "shibarium"),
-      tvl: getUniTVL({ factory: '0x5640113EA7F369E6DAFbe54cBb1406E5BF153E90', useDefaultCoreAssets: true, })
-    },
-
-    op_bnb: {
-      tvl: getUniTVL({ factory: '0x5640113EA7F369E6DAFbe54cBb1406E5BF153E90', useDefaultCoreAssets: true, })
-    }
-
-}
+  op_bnb: {
+    tvl: getUniTVL({ factory: "0x5640113EA7F369E6DAFbe54cBb1406E5BF153E90", useDefaultCoreAssets: true,})
+  },
+  shibarium: {
+    tvl: getUniTVL({ factory: "0x5640113EA7F369E6DAFbe54cBb1406E5BF153E90", useDefaultCoreAssets: true,})
+  },
+};
