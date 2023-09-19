170262d591feb091cbf26048cc67b7b2716a562d
Mihai Barca <mihai.barca@elrond.com>
2023-09-19 10:25:24 +0300

    renamed bStake to staking

--- projects/onedex/index.js
+++ projects/onedex/index.js
@@ -5,7 +5,7 @@ const ADDRESSES = require('../helper/coreAssets.json');
 const swapsSC = 'erd1qqqqqqqqqqqqqpgqqz6vp9y50ep867vnr296mqf3dduh6guvmvlsu3sujc';
 const boostedStakingSC = 'erd1qqqqqqqqqqqqqpgq8nlmvjm8gum6y2kqe0v296kgu8cm4jlemvlsays3ku';
 
-const bStake = async (_, _1, _2, { api }) => {
+const staking = async (_, _1, _2, { api }) => {
   // Swaps SC
   const oneTokenPrice = await call({ target: swapsSC, abi: 'getEquivalent', params: ['WEGLD-bd4d79', 'ONE-f9954f', 1e18], responseTypes: ['number']});
 
@@ -20,6 +20,6 @@ module.exports = {
   timetravel: false,
   elrond: {
     tvl: sumTokensExport({ owner: swapsSC, }),
-    bStake,
+    staking,
   },
 };
