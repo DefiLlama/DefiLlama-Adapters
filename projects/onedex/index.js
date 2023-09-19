a2112daf84cdc895bd00f5c3cc704cf2826ec747
Mihai Barca <mihai.barca@elrond.com>
2023-09-19 10:19:42 +0300

    added BoostedStaking sc

--- projects/onedex/index.js
+++ projects/onedex/index.js
@@ -1,8 +1,25 @@
 const { sumTokensExport } = require('../helper/sumTokens')
+const { call } = require("../helper/chain/elrond");
+const ADDRESSES = require('../helper/coreAssets.json');
+
+const swapsSC = 'erd1qqqqqqqqqqqqqpgqqz6vp9y50ep867vnr296mqf3dduh6guvmvlsu3sujc';
+const boostedStakingSC = 'erd1qqqqqqqqqqqqqpgq8nlmvjm8gum6y2kqe0v296kgu8cm4jlemvlsays3ku';
+
+const tvl = async (_, _1, _2, { api }) => {
+  // Swaps SC
+  api.add(ADDRESSES.null, sumTokensExport({ owner: swapsSC, }));
+  const oneTokenPrice = await call({ target: swapsSC, abi: 'getEquivalent', params: ['WEGLD-bd4d79', 'ONE-f9954f', 1e18], responseTypes: ['number']});
+
+  // Boosted Staking SC
+  const totalBoostedStaking = await call({ target: boostedStakingSC, abi: 'getTotalStaking', responseTypes: ['number']});
+  api.add(ADDRESSES.null, totalBoostedStaking * oneTokenPrice / 1e18);
+  const boostedStakingRewards = await call({ target: boostedStakingSC, abi: 'getDepositedRewards', responseTypes: ['number']});
+  api.add(ADDRESSES.null, boostedStakingRewards * oneTokenPrice / 1e18);
+};
 
 module.exports = {
   timetravel: false,
   elrond: {
-    tvl: sumTokensExport({ owner: 'erd1qqqqqqqqqqqqqpgqqz6vp9y50ep867vnr296mqf3dduh6guvmvlsu3sujc', }),
+    tvl,
   },
 };
