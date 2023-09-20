60d611d16abd7a79c9b5a9c0c343d0c0ef4f4a3b
moonbasefi <143190385+moonbasefi@users.noreply.github.com>
2023-09-20 17:26:00 +0200

    Update index.js

--- projects/moonbase/index.js
+++ projects/moonbase/index.js
@@ -1,11 +1,11 @@
 const { getUniTVL } = require("../helper/unknownTokens");
-const MOON = "*";
 module.exports = {
   methodology:
     "Uses Uniswap-style factory address to find and price liquidity pairs.",
   base: {
     tvl: getUniTVL({
-      factory: "0xff0655dDdFE887e7bAECD7E731A755Bf9f386cEd",
+      factory: "0xA4CB5E815cC3919C279869Dda058D72976085fee",
     }),
   },
 };
+
