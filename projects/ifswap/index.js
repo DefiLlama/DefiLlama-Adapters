a9ad77b161409ed87dd65967900d82b75b114d8e
0xngmi <0xngmi@protonmail.com>
2023-09-20 02:45:08 +0100

    fix ifswap

--- projects/ifswap/index.js
+++ projects/ifswap/index.js
@@ -1,5 +1,5 @@
 const { getUniTVL } = require('../helper/unknownTokens')
-const { stakingPricedLP } = require("../helper/staking");
+const { staking } = require("../helper/staking");
 
 const IFS = "0x6925435f9D1CB710abeb680Ec7EF3f8C5E1B2639";
 const FACTORY = "0x44b7864D360BFf7879402E3B860aF47e6e371208";
@@ -16,12 +16,10 @@ module.exports = {
     "We count liquidity of all paris through Factory Contract and Pools (single tokens) seccions through MasterChef Contract.",
   csc: {
     tvl: ifswapDexTvl,
-    staking: stakingPricedLP(
+    staking: staking(
       MASTERCHEF,
       IFS,
-      "csc",
-      IFS_USDT_LP,
-      COREASSETNAME
+      "csc"
     ),
   },
 };
