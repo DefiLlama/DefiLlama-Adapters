648330835569542e526f258658625d52d7aa6680
g1nt0ki <llama@llama.fi>
2023-09-21 17:19:28 +0200

    track shoebill v2

---
+++ projects/shoebillFinance-v2/index.js
@@ -0,0 +1,5 @@
+const { compoundExports2 } = require('../helper/compound')
+
+module.exports = {
+  klaytn: compoundExports2({ comptroller: '0xEE3Db1711ef46C04c448Cb9F5A03E64e7aa22814', cether: '0xac6a4566d390a0da085c3d952fb031ab46715bcf'})
+}
