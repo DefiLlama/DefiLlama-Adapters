24394a098902033bc73611605fe44df0491a5ddd
g1nt0ki <llama@llama.fi>
2023-09-21 13:01:39 +0200

    code refactor

--- projects/baki/index.js
+++ projects/baki/index.js
@@ -1,34 +1,18 @@
-const { ethers } = require("ethers");
-const path = require("path");
-const fs = require("fs");
-
 const bakiAddress = "0x8c1278D8b20ecEe71736F27181D3018E9a15652B";
-const treasury = "0x6F996Cb36a2CB5f0e73Fc07460f61cD083c63d4b";
-const usdc = "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E";
 
-const provider = new ethers.providers.JsonRpcProvider(
-  "https://api.avax.network/ext/bc/C/rpc"
-);
-const abiPath = path.resolve("./projects/baki/abi.json");
-const rawData = fs.readFileSync(abiPath);
-const contractABI = JSON.parse(rawData);
-
-
-const tokenABIPath = path.resolve("./projects/baki/usdc.json");
-const tokenRawData = fs.readFileSync(tokenABIPath);
-
-
-const tokenABI = JSON.parse(tokenRawData);
-
-const contract = new ethers.Contract(bakiAddress, contractABI, provider);
-const tokenContract = new ethers.Contract(usdc, tokenABI, provider);
+const ADDRESSES = require('../helper/coreAssets.json')
+const abi = require("./abi.json");
 
 module.exports = {
   methodology: "The baki protocol keeps track of the total collateral locked in the vault",
   avax:{
-    tvl: async () => {
-      let tvl = await contract?.totalCollateral();
-      return parseInt(ethers.utils.formatUnits(tvl, 0))/10**18;
+    tvl: async (_, _1, _2, { api }) => {
+      // const usdcBal = await api.call({  abi: abi.totalCollateral, target: bakiAddress })
+      // api.add(usdc, usdcBal)
+      // return api.getBalances()
+      const treasury = await api.call({  abi: abi.treasuryWallet, target: bakiAddress })
+      return api.sumTokens({ owner: treasury, tokens: [ADDRESSES.avax.USDC]})
+      
     }
   },
  
