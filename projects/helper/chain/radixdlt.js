1917b77391580e39b5d41da6e4333b1dd4b4c5f6
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-09-21 13:51:44 -0300

    removing logs and converting amounts to the right type

--- projects/helper/chain/radixdlt.js
+++ projects/helper/chain/radixdlt.js
@@ -8,7 +8,7 @@ async function getBalance(component) {
   const data = await post(`https://rcnet-v3.radixdlt.com/state/entity/details`, body)
   const fungible_resources =  data["items"][0]["fungible_resources"]["items"]
   for (const item of fungible_resources) {
-    balance += item.amount
+    balance += parseFloat(item.amount)
   }
   return balance;
 }
