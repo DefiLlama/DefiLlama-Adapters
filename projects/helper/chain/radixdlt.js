cad837c06c0d5726313f149f8fd2ff29c431de42
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-09-22 15:36:28 -0300

    fixing the way we iterate over the resources

--- projects/helper/chain/radixdlt.js
+++ projects/helper/chain/radixdlt.js
@@ -1,19 +1,43 @@
 const { post } = require('../http')
 
-async function getBalance(component) {
-  body = {
-    "addresses": [component]
+function getNameFromMetadata(metadata) {
+  for(const item of metadata.items) {
+    if (item.key == "name") {
+      return item.value.typed.value
+    }
   }
-  let balance = 0;  
-  const data = await post(`https://rcnet-v3.radixdlt.com/state/entity/details`, body)
-  const fungible_resources =  data["items"][0]["fungible_resources"]["items"]
-  for (const item of fungible_resources) {
-    balance += parseFloat(item.amount)
-  }
-  return balance;
+  return ""
 }
 
+// TODO: update the url below with the final url after babylon
+const ENTITY_DETAILS_URL = `https://rcnet-v3.radixdlt.com/state/entity/details`
+
+async function getBalances(component) {
+  const body = {
+    "addresses": [component]
+  }
+  const data = await post(ENTITY_DETAILS_URL, body)
+  const fungibleResources =  data["items"][0]["fungible_resources"]["items"]
+
+  const balancesByName = {}
+  const balancesByResource = {}
+  const resourceAddresses = []
+  fungibleResources.forEach(resource => {
+    resourceAddresses.push(resource.resource_address)
+    balancesByResource[resource.resource_address] = resource.amount
+  });
+
+  body["addresses"] = resourceAddresses
+
+  const resourcesDetails = await post(ENTITY_DETAILS_URL, body)
+  resourcesDetails.items.forEach(resource => {
+    const resourceName = getNameFromMetadata(resource.metadata)
+    balancesByName[resourceName] = parseFloat(balancesByResource[resource.address])
+  });
+
+  return balancesByName
+}
 
 module.exports = {
-  getBalance,
+  getBalances,
 }
--- projects/radix-test/index.js
+++ projects/radix-test/index.js
@@ -1,11 +1,13 @@
-const { getBalance } = require('../helper/chain/radixdlt');
+const { getBalances } = require('../helper/chain/radixdlt');
+
+/*
+Call the getBalances method with the address of your component
+*/
 
 module.exports = {
   radixdlt: {
     tvl: async () => {
-      return {
-        radixdlt: await getBalance('component_tdx_e_1cq7m3qzds938gxjl8kzryqjke8p9wr4qcn0qmxw3xcvychexuxqmv0')
-      }
+      return await getBalances('component_tdx_e_1cq7m3qzds938gxjl8kzryqjke8p9wr4qcn0qmxw3xcvychexuxqmv0')
     },
   },
   timetravel: false
