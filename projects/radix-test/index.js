d0f4a3420fc460b87c710e4e4cf841a6bab844e7
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-09-25 12:50:16 -0300

    add docstring

--- projects/radix-test/index.js
+++ projects/radix-test/index.js
@@ -1,7 +1,18 @@
 const { getBalances } = require('../helper/chain/radixdlt');
 
 /*
-Call the getBalances method with the address of your component
+Call the getBalances method with the addresses of your components
+
+Note that all the fungible resources within each of the components,
+need to be listed in CoinGecko, as that's how the sdk get's the resource value
+and calculates the TVL
+
+Radix is using the resource metadata to get the resource name and it uses that name
+to pass it to the defillama sdk.
+
+If the resource is not listed on CoinGecko or if the resource name within the resource
+metadata does not match the resource name at CoinGecko, then it will not be included in
+the TVL report
 */
 
 module.exports = {
