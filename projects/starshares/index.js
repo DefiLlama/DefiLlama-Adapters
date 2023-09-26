8ce45d791e96919dda355bca3b4e08ae4d82d98e
0xngmi <0xngmi@protonmail.com>
2023-09-26 23:36:28 +0100

    add starshares

---
+++ projects/starshares/index.js
@@ -0,0 +1,14 @@
+const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+
+const contract = "0xa481b139a1a654ca19d2074f174f17d7534e8cec"
+
+async function tvl(time, ethBlock, _b, {api}) {
+  return sumTokens2({ tokens: [nullAddress], owner: contract, api })
+}
+
+module.exports = {
+  methodology: `We count the AVAX on ${contract}`,
+  avax: {
+    tvl
+  }
+}
