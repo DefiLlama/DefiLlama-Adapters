66701c1be2ce13c6c75a2550e292d90e4afa8132
bbjansen <bobjansen@pm.me>
2023-09-26 20:05:49 +0200

    bridge implementation

--- projects/florence-finance/index.js
+++ projects/florence-finance/index.js
@@ -1,49 +1,38 @@
-
-const CONTRACT_ETH_VAULTS = "0xD6348E8EacE62Eb3Eb77fBbA8D8c363e375fC455";
 const CONTRACT_ETH_BRIDGE= "0xcee284f754e854890e311e3280b767f80797180d";
-const CONTRACT_ETH_EURS = "0xdb25f211ab05b1c97d595516f45794528a807ad8";
 const CONTRACT_ETH_FLR= "0x5e5d9aeec4a6b775a175b883dca61e4297c14ecb";
-const CONTRACT_ARB_VAULTS = "0x19A2106475b29F0ff5053AE026190ce7A9227cE3";
-const CONTRACT_ARB_EURS = "0xd22a58f79e9481d1a88e00c343885a588b34b68b";
+const CONTRACT_ETH_EURS = "0xdb25f211ab05b1c97d595516f45794528a807ad8"; //EURS
+
+const CONTRACT_ARB_BRIDGE= "0x";
+const CONTRACT_ARB_FLR= "0x9B6226dd0191a77d032F56A6d383044EE99944C3";
+const CONTRACT_ARB_EURS = "0xfa5ed56a203466cbbc2430a43c66b9d8723528e7"; //agEUR 
+
+async function getBorrowed(tokenAddress, bridgeAddress, { api }) {
+  const totalSupply = await api.call({ 
+    abi: "function totalSupply() external view returns (uint256)",
+    target: tokenAddress
+  });
+
+  const bridgeBalance = await api.call({ 
+    abi: "function balanceOf(address account) view returns (uint256)",
+    target: tokenAddress,
+    params: [bridgeAddress]
+  });
+
+  return (totalSupply - bridgeBalance);
+}
 
 async function borrowedETH(_, _1, _2, { api }) {
-  // Get all vaults
-  const vaultIds = await api.call({ 
-    abi: "function getLoanVaultIds() external view returns (string[])",
-    target: CONTRACT_ETH_VAULTS
-  })
-  const vaultContracts = await api.multiCall({  abi: "function getLoanVault (string loanVaultId) external view returns (address)", target: CONTRACT_ETH_VAULTS, calls: vaultIds })
-  const loans = await api.multiCall({  abi: "function loansOutstanding() external view returns (uint256)", calls: vaultContracts})
-  
-  let sumLoans = 0;
-  loans.forEach((val, i) => sumLoans += Number(val))
-
-  // Subtract Bridged FLR (EURS) to Arbitrum from Ethereum (migration)
-  const migratedAmount = await api.call({ 
-    abi: "function balanceOf(address account) view returns (uint256)",
-    target: CONTRACT_ETH_FLR,
-    params: [CONTRACT_ETH_BRIDGE]
-  })
-
-  // Subtract migratedAmount from sumLoans and express it in terms of EURS (1 Vault Token = 1 EURS)
-  api.add(CONTRACT_ETH_EURS, (sumLoans - migratedAmount) / 1e16)
+  const borrowed = await getBorrowed(CONTRACT_ETH_FLR, CONTRACT_ETH_BRIDGE, { api });
+  api.add(CONTRACT_ETH_EURS, (borrowed / 1e16));
 }
 
 async function borrowedARB(_, _1, _2, { api }) {
-  // Get all vaults
-  const vaultIds = await api.call({ 
-    abi: "function getLoanVaultIds() external view returns (string[])",
-    target: CONTRACT_ARB_VAULTS
-  })
-  const vaultContracts = await api.multiCall({  abi: "function getLoanVault (string loanVaultId) external view returns (address)", target: CONTRACT_ARB_VAULTS, calls: vaultIds })
-  const loans = await api.multiCall({  abi: "function loansOutstanding() external view returns (uint256)", calls: vaultContracts})
-
-  // Take the sum of all vault tokens in terms of EURS (1 Loan Vault Token = 1 EURS Statis) on the platform | 18-2 = 16 atomic units (LV-EURS)
-  loans.forEach((val, i) => api.add(CONTRACT_ARB_EURS, val / 1e16))
+  const borrowed = await getBorrowed(CONTRACT_ARB_FLR, CONTRACT_ARB_BRIDGE, { api });
+  api.add(CONTRACT_ARB_EURS, (borrowed / 1e16));
 }
-
+ 
 module.exports = {
-  methodology: "Data is retrieved on-chain by taking the total sum of all loans outstanding (dominated in EURS Statis) from all platform vaults. Florence Finance is currently migrating from Ethereum to Arbitrum.",
+  methodology: "Data is retrieved on-chain by subtracting the balance of FlorinToken in the BridgeContract from the total supply of FlorinToken. The result is the total amount borrowed.",
   ethereum: { start: 16077400, borrowed: borrowedETH, tvl: () => ({}) },
   //arbitrum: { start: 126183410, borrowed: borrowedARB, tvl: () => ({}) },
 }
