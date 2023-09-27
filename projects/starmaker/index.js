d97abde2dc6b974c496df18a892458958d340057
define <Lovelovenow@protonmail.com>
2023-09-27 10:11:54 +0100

    add mantle chain to starmaker

--- projects/starmaker/index.js
+++ projects/starmaker/index.js
@@ -5,5 +5,8 @@ module.exports = {
   era: {
     tvl: getUniTVL({ factory: '0x7096Cebc52012e2611a1E88c45bC54ee2A88dcB4', useDefaultCoreAssets: true, fetchBalances: true, }),
   },
+  mantle: {
+    tvl: getUniTVL({ factory: '0x2a34936cd9441B7E7FB152c9C4e304be58e14830', useDefaultCoreAssets: true, fetchBalances: true, }),
+  },
   methodology: "Counts liquidity in pools",
 };
