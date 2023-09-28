efb42319a84737937aebdaa0ccc41ac2cf83b0f0
bbjansen <bobjansen@pm.me>
2023-09-28 09:25:38 +0200

    rewrite

--- projects/florence-finance/index.js
+++ projects/florence-finance/index.js
@@ -1,38 +1,38 @@
-const CONTRACT_ETH_BRIDGE= "0xcee284f754e854890e311e3280b767f80797180d";
-const CONTRACT_ETH_FLR= "0x5e5d9aeec4a6b775a175b883dca61e4297c14ecb";
-const CONTRACT_ETH_EURS = "0xdb25f211ab05b1c97d595516f45794528a807ad8"; //EURS
+const ETH_ARB_BRIDGE = "0xcee284f754e854890e311e3280b767f80797180d";
+const ETH_FLR = "0x5e5d9aeec4a6b775a175b883dca61e4297c14ecb";
+const ETH_EURS = "0xdb25f211ab05b1c97d595516f45794528a807ad8";
 
-const CONTRACT_ARB_BRIDGE= "0x";
-const CONTRACT_ARB_FLR= "0x9B6226dd0191a77d032F56A6d383044EE99944C3";
-const CONTRACT_ARB_EURS = "0xfa5ed56a203466cbbc2430a43c66b9d8723528e7"; //agEUR 
+const ARB_FLR = "0x9b6226dd0191a77d032f56a6d383044ee99944c3";
+const ARB_AGEUR = "0xfa5ed56a203466cbbc2430a43c66b9d8723528e7";
 
-async function getBorrowed(tokenAddress, bridgeAddress, { api }) {
-  const totalSupply = await api.call({ 
+async function getTotalSupply(token, { api }) {
+  const totalSupply = await api.call({
     abi: "function totalSupply() external view returns (uint256)",
-    target: tokenAddress
+    target: token,
   });
+  return totalSupply;
+}
 
-  const bridgeBalance = await api.call({ 
+async function getBorrowedOnEthereum(_, _1, _2, { api }) {
+  const [flrSupply, flrInBridgeCustody] = await Promise.all([getTotalSupply(ETH_FLR, { api }), await api.call({
     abi: "function balanceOf(address account) view returns (uint256)",
-    target: tokenAddress,
-    params: [bridgeAddress]
-  });
+    target: ETH_FLR,
+    params: [ETH_ARB_BRIDGE],
+  })]);
 
-  return (totalSupply - bridgeBalance);
+  const borrowed = flrSupply - flrInBridgeCustody; //Subtract FLR in bridge custody to avoid double counting accross chains
+
+  api.add(ETH_EURS, borrowed / 1e16); //18 decimals (FLR) -> 2 decimals (EURS)
 }
 
-async function borrowedETH(_, _1, _2, { api }) {
-  const borrowed = await getBorrowed(CONTRACT_ETH_FLR, CONTRACT_ETH_BRIDGE, { api });
-  api.add(CONTRACT_ETH_EURS, (borrowed / 1e16));
+async function getBorrowedOnArbitrum(_, _1, _2, { api }) {
+  const borrowed = await getTotalSupply(ARB_FLR, { api }); //FLR that are bridged to Ethereum are burned by the Arbitrum bridge. So there is no need to subtract here.
+  api.add(ARB_AGEUR, borrowed); //Decimals of FLR and agEUR are both 18. No conversion needed.
 }
 
-async function borrowedARB(_, _1, _2, { api }) {
-  const borrowed = await getBorrowed(CONTRACT_ARB_FLR, CONTRACT_ARB_BRIDGE, { api });
-  api.add(CONTRACT_ARB_EURS, (borrowed / 1e16));
-}
- 
 module.exports = {
-  methodology: "Data is retrieved on-chain by subtracting the balance of FlorinToken in the BridgeContract from the total supply of FlorinToken. The result is the total amount borrowed.",
-  ethereum: { start: 16077400, borrowed: borrowedETH, tvl: () => ({}) },
-  //arbitrum: { start: 126183410, borrowed: borrowedARB, tvl: () => ({}) },
-}
+  methodology:
+    "The Florin token (FLR) is minted whenever a new loan is funded and burned when a loan matures and is repaid. Since the Florin token is 1:1 redeemable for EUR the borrowed amount is denominated in the protocols treasuries EUR stablecoin of the respective chain. Consequently the total supply of Florin equals the amount borrowed through the protocol. To avoid double counting, the amount of FLR held in the bridge contract is subtracted from the total supply. ",
+  ethereum: { start: 16077400, borrowed: getBorrowedOnEthereum, tvl: () => ({}) },
+  arbitrum: { start: 126183369, borrowed: getBorrowedOnArbitrum, tvl: () => ({}) },
+};
