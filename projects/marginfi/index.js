2937bc06971585434d47d38e59d52ab2c57ab4b2
g1nt0ki <llama@llama.fi>
2023-09-28 16:45:25 +0200

    split margin fi

--- projects/marginfi/index.js
+++ projects/marginfi/index.js
@@ -1,29 +1,14 @@
 const { Program } = require("@project-serum/anchor");
-const { getProvider, sumTokens2, getConnection, } = require("../helper/solana");
-const { PublicKey } = require("@solana/web3.js");
+const { getProvider, sumTokens2, } = require("../helper/solana");
 
 const idl = require('./idl')
 
 async function tvl() {
-  // mrgnlend
   const provider = getProvider()
   const program = new Program(idl, 'MFv2hWf31Z9kbCa1snEPYctwafyhdvnV7FZnsebVacA', provider)
   const banks = await program.account.bank.all()
 
-  const tvl = sumTokens2({ tokenAccounts: banks.map(i => i.account.liquidityVault.toString()) });
-
-  // LST
-  const connection = getConnection();
-  const account = await connection.getAccountInfo(new PublicKey('DqhH94PjkZsjAqEze2BEkWhFQJ6EyU6MdtMphMgnXqeK'));
-  const lstTvlSolana = Number(account.data.readBigUint64LE(258))/1e9;
-
-  if (!tvl.solana) {
-    tvl.solana = 0;
-  }
-
-  tvl.solana += lstTvlSolana;
-  
-  return tvl
+  return sumTokens2({ tokenAccounts: banks.map(i => i.account.liquidityVault.toString()) });
 }
 
 module.exports = {
