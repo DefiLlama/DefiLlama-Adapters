1b2d59a53eca1b8a29bf9e58789ece75178cd070
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-09-29 14:37:56 -0300

    pointing to the mainnet

--- projects/helper/chain/radixdlt.js
+++ projects/helper/chain/radixdlt.js
@@ -9,20 +9,40 @@ function getNameFromMetadata(metadata) {
   return ""
 }
 
-// TODO: update the url below with the final url after babylon
 const ENTITY_DETAILS_URL = `https://mainnet.radixdlt.com/state/entity/details`
+
+/**
+ * getBalances expects a list of components addresses like
+ * [
+ *  component_rdx1cptxxxxxxxxxajhkjahkaj,
+ *  component_rdx1cptxxxxxxxxxlkjljljlkkj
+ * ]
+ *
+ * Each component has a list of fungible resources so we call state/entity/details
+ * with all the components addresses as params and we get a list of items as result
+ *
+ * We iterate over all items and we add up the amount for each fungible resource
+ * DefiLlama sdk expects an input in the for of
+ *
+ * {
+ *  token_one_name_in_coin_gecko: amount_token_one,
+ *  token_two_name_in_coin_gecko: amount_token_two
+ * }
+ *
+ */
 
 async function getBalances(components) {
   const body = {
     "addresses": components,
-    "opt_ins": {
-      "explicit_metadata": [
-        "name"
-      ]
-    }
+    "opt_ins": {"explicit_metadata": ["name"]}
   }
-  const data = await post(ENTITY_DETAILS_URL, body)
-
+  let data;
+  try {
+    data = await post(ENTITY_DETAILS_URL, body)
+  } catch (error) {
+    console.log(error)
+    return {}
+  }
   const resourceAmounts = {}
   data.items.forEach((item) => {
     item.fungible_resources.items.forEach((resource) => {
@@ -30,21 +50,30 @@ async function getBalances(components) {
       const amount = resource.amount;
 
       if (resourceAmounts.hasOwnProperty(resourceAddress)) {
-        resourceAmounts[resourceAddress] += amount;
+        resourceAmounts[resourceAddress] += parseFloat(amount);
       } else {
-        resourceAmounts[resourceAddress] = amount;
+        resourceAmounts[resourceAddress] = parseFloat(amount);
       }
     });
   });
 
-  const balancesByName = {}
+  let balancesByName = {}
   body["addresses"] = Object.keys(resourceAmounts);
-  const resourcesDetails = await post(ENTITY_DETAILS_URL, body)
+
+  let resourcesDetails
+  try {
+    resourcesDetails = await post(ENTITY_DETAILS_URL, body)
+  } catch (error) {
+    console.log(error)
+    resourcesDetails.items = []
+  }
+
   resourcesDetails.items.forEach(resource => {
     const resourceName = getNameFromMetadata(resource.explicit_metadata)
-    balancesByName[resourceName] = parseFloat(resourceAmounts[resource.address])
+    balancesByName[resourceName] = resourceAmounts[resource.address]
   });
 
+  console.log(balancesByName)
   return balancesByName
 }
 
