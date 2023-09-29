0935307294232293d9f182236875f7a1506c770f
g1nt0ki <llama@llama.fi>
2023-09-29 03:54:14 +0200

    code refactor

--- projects/gammaswap/index.js
+++ projects/gammaswap/index.js
@@ -1,44 +1,23 @@
-const sdk = require('@defillama/sdk')
-const { request, gql } = require("graphql-request")
+const { getLogs } = require('../helper/cache/getLogs')
 const { sumTokens2 } = require('../helper/unwrapLPs')
 
-async function fetchPoolsInfo() {
-  const subgraphUrl = 'https://api.thegraph.com/subgraphs/name/gammaswap/gammaswap-v1-arbitrum'
-  const query = gql`
-    query {
-      gammaPools {
-        address
-        cfmm
-        token0 {
-          id
-        }
-        token1 {
-          id
-        }
-      }
+const config = {
+  arbitrum: { factory: '0xe048ccE443E787c5b6FA886236De2981D54E244f', fromBlock: 132429931 },
+}
+
+Object.keys(config).forEach(chain => {
+  const { factory, fromBlock } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const logs = await getLogs({
+        api,
+        target: factory,
+        eventAbi: 'event PoolCreated (address indexed pool, address indexed cfmm, uint16 indexed protocolId, address implementation, address[] tokens, uint256 count)',
+        onlyArgs: true,
+        fromBlock,
+      })
+      const ownerTokens = logs.map(i => [[...i.tokens, i.cfmm], i.pool])
+      return sumTokens2({ ownerTokens, api, resolveLP: true, })
     }
-  `
-  const { gammaPools } = await request(subgraphUrl, query);
-
-  return gammaPools.map(pool => ({
-    address: pool.address,
-    cfmm: pool.cfmm,
-    token0: pool.token0.id,
-    token1: pool.token1.id,
-  }))
-}
-
-async function tvl(timestamp, block, _, { api }) {
-  const pools = await fetchPoolsInfo();
-  const poolAddresses = pools.map(pool => pool.address);
-  const tokenAddresses = Array.from(new Set(pools.map(pool => [pool.token0, pool.token1]).flat()));
-  const lpTokens = pools.map(pool => pool.cfmm);
-
-  return sumTokens2({ api, owners: poolAddresses, tokens: [...tokenAddresses, ...lpTokens], resolveLP: true });
-}
-
-module.exports = {
-  arbitrum: {
-    tvl,
   }
-}
+})
