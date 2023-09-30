a3b4cbe4fe86474c030cc80d312458c75fea57ad
sevane <sevane@tangleswap.exchange>
2023-09-30 19:56:16 +0200

    add tangleswap SDK adapter (#7608)

---
+++ projects/tangleswap/index.js
@@ -0,0 +1,8 @@
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+  shimmer_evm: {
+    factory: "0xdf7bA717FB0D5ce579252f05167cD96d0fA77bCb",
+    fromBlock: 32598,
+  },
+});
