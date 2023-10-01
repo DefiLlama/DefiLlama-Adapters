4af4116d674220710d2f4a638353a1e2150ae6da
disco chuck <uv@househeads.org>
2023-10-01 11:53:13 -0500

    Update Glacier v2 tvl metrics
    Attempting to take the llama magick 0xngmi provided for our v3
    factory and apply it to our v2.

--- projects/glacier-finance/index.js
+++ projects/glacier-finance/index.js
@@ -1,10 +1,35 @@
 const { getUniTVL } = require("../helper/unknownTokens");
 const { staking, stakingPricedLP } = require('../helper/staking');
+const { getLogs } = require('../helper/cache/getLogs')
+const { sumTokens2 } = require('../helper/unwrapLPs')
+
+const FACTORY =  '0xaC7B7EaC8310170109301034b8FdB75eCa4CC491';
+
+const startBlocks = {
+    avax: 28094493
+}
+
+function chainTvl(chain) {
+  return async (timestamp, ethBlock, chainBlocks, { api }) => {
+    const  START_BLOCK = startBlocks[chain]
+    const logs = await getLogs({
+        api,
+        target: FACTORY,
+        fromBlock: START_BLOCK,
+        topic: 'Pool(address,address,address)',
+        eventAbi: 'event Pool(address indexed token0, address indexed token1, address pool)',
+        onlyArgs: true,
+      })
+    const toa = []
+    logs.forEach(({ token0, token1, pool}) => toa.push([token0, pool], [token1, pool]))
+    return sumTokens2({ api, tokensAndOwners: toa })
+  }
+}
 
 module.exports = {
   misrepresentedTokens: true,
   avax: {
-    tvl: getUniTVL({ factory: '0xaC7B7EaC8310170109301034b8FdB75eCa4CC491', useDefaultCoreAssets: true,  hasStablePools: true }),
+    tvl: chainTvl('avax'),
     staking: stakingPricedLP("0xed1eE3f892fe8a13A9BE02F92E8FB7410AA84739", "0x3712871408a829C5cd4e86DA1f4CE727eFCD28F6", "avax", "0x2071a39da7450d68e4f4902774203df208860da2", "avalanche-2"),
   },
 };
