194930381bb1fa0977ac960ffa7b945538977a39
llama <llama@llama.fi>
2023-10-02 15:05:18 +0200

    fix broken adapters

--- projects/sifchain.js
+++ projects/sifchain.js
@@ -1,16 +1,17 @@
 const { get } = require('./helper/http')
+const { endPoints } = require('./helper/chain/cosmos')
 
 async function tvl() {
   // const getPoolsRes = await get('https://api.sifchain.finance/clp/getPools')
-  const getPoolsRes = await get('https://sifchain-api.polkachu.com/clp/getPools')
+  const getPoolsRes = await get(endPoints.sifchain + '/clp/getPools')
 
   const total = getPoolsRes.result.pools
     .map(pool => +pool.native_asset_balance)
     .reduce((sum, current) => sum + current, 0)
 
-    return {
-      sifchain: total * 2 / 1e18
-    }
+  return {
+    sifchain: total * 2 / 1e18
+  }
 }
 
 module.exports = {
