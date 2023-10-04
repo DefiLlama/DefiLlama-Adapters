71791d0967d252d2202752d9061acfb64723a82c
0xngmi <0xngmi@protonmail.com>
2023-10-04 17:32:35 +0100

    fix cbeth

--- projects/cbETH/index.js
+++ projects/cbETH/index.js
@@ -1,10 +1,16 @@
-const sdk = require("@defillama/sdk")
+const { default: axios } = require("axios")
 const token = '0xbe9895146f7af43049ca1c1ae358b0541ea49704'
 
 module.exports = {
   ethereum: {
-    tvl: async (_, block) => ({
-      [token]: (await sdk.api.erc20.totalSupply({ target: token, block})).output
-    })
+    tvl: async (timestamp, block) => {
+      if(timestamp < Date.now()/1e3 - 3600){
+        throw new Error("Only works for current info")
+      }
+      const data = await axios.get("https://api.exchange.coinbase.com/wrapped-assets/CBETH")
+      return {
+        [token]: data.data.circulating_supply * 1e18
+      }
+    }
   }
 }
