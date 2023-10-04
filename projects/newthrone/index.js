640896837ab62ba4fb85f0717b1e4920cbb41293
llama <llama@llama.fi>
2023-10-04 10:10:43 +0200

    minor fix

--- projects/newthrone/index.js
+++ projects/newthrone/index.js
@@ -1,21 +1,11 @@
-const GAME_CONTRACT = '0x61C3A357bc3ca51b80eCD36CB1Ae37e5465C6701';
-const WETH = '0x4200000000000000000000000000000000000006';
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const ADDRESSES= require("../helper/coreAssets.json");
 
-async function tvl(_, _1, _2, { api }) {
-  const gameContractBalance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: WETH,
-    params: [GAME_CONTRACT],
-  });
-
-  api.add(WETH, gameContractBalance)
-}
+const GAME_CONTRACT = '0x61C3A357bc3ca51b80eCD36CB1Ae37e5465C6701'
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'WETH tokens in the Game contract, which represents the current balance of the game (total spent - total claimed).',
   base: {
-    tvl,
+    tvl: sumTokensExport({ owner: GAME_CONTRACT, tokens: [ADDRESSES.base.WETH]}),
   }
 }; 
