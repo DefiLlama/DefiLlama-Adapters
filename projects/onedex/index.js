9e313603a4c22b3d75fc511139e55f23032dd799
Damian <damian.ovidiu27@gmail.com>
2023-10-04 22:18:01 +0300

    update onedex

--- projects/onedex/index.js
+++ projects/onedex/index.js
@@ -1,8 +1,18 @@
-const { sumTokensExport } = require('../helper/sumTokens')
+const { sumTokensExport } = require("../helper/sumTokens");
+const axios = require("axios");
+
+async function farmsValue() {
+  const initialResult = await axios.get("https://api.onedex.app/tvs");
+  const tvs = initialResult.total_tvs;
+  return tvs;
+}
 
 module.exports = {
   timetravel: false,
   elrond: {
-    tvl: sumTokensExport({ owner: 'erd1qqqqqqqqqqqqqpgqqz6vp9y50ep867vnr296mqf3dduh6guvmvlsu3sujc', }),
+    tvl: sumTokensExport({
+      owner: "erd1qqqqqqqqqqqqqpgqqz6vp9y50ep867vnr296mqf3dduh6guvmvlsu3sujc",
+    }),
+    staking: farmsValue,
   },
 };
