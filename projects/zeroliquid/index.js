489369cfb8320eec7002c60acb7dc064ecce2cac
llama <llama@llama.fi>
2023-10-05 13:46:08 +0200

    minor refacot

--- projects/zeroliquid/index.js
+++ projects/zeroliquid/index.js
@@ -1,50 +1,15 @@
 const { staking } = require('../helper/staking')
-
-const wstETH ="0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
-const rETH= "0xae78736cd615f374d3085123a210448e74fc6393"
-const sfrxETH= "0xac3E018457B222d93114458476f3E3416Abbe38F"
-
-const endpoint = 'https://api.thegraph.com/subgraphs/name/zackzeroliquid/zeroliquid'
-
-const query = `query {
-  zeroLiquids {
-    amountRETH
-    amountSfrxETH
-    amountWstETH
-  }
-}`
-
-async function tvl(_, _1, _2, { api }) {
-  const data = await fetch(endpoint,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        query,
-      }),
-    },
-  )
-    .then((r) => r.json())
-    .then((data) => {
-      return data.data.zeroLiquids[0];
-    });
-
-  api.add(wstETH, data.amountWstETH)
-  api.add(rETH, data.amountRETH)
-  api.add(sfrxETH, data.amountSfrxETH)
-
-}
+const  ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  methodology: 'Getting TVL data from the GraphQL',
+  methodology: 'Value of LSD tokens in the vault',
   ethereum: {
-    tvl,
+    tvl: sumTokensExport({
+      owner: '0x0246e28C6B161764492E54CBF852e28A4DA2D672',
+      tokens: [ADDRESSES.ethereum.WSTETH, ADDRESSES.ethereum.RETH, ADDRESSES.ethereum.sfrxETH],
+    }),
+    staking: staking("0xb9b2d7a712402acb37a97f0ea6356ff271e065e2", '0xb0ed33f79d89541dfdcb04a8f04bc2c6be025ecc')
   }
 }; 
 
-module.exports.ethereum.staking = staking("0xb9b2d7a712402acb37a97f0ea6356ff271e065e2", '0xb0ed33f79d89541dfdcb04a8f04bc2c6be025ecc')
