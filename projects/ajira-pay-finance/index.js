5af37d1723a3e12d78a3557fe011e368f83c7de6
dickensodera <dickens@laevitas.ch>
2023-10-05 11:59:05 +0300

    update: Add celo and polygon tvl

--- projects/ajira-pay-finance/index.js
+++ projects/ajira-pay-finance/index.js
@@ -3,6 +3,8 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const ASSETS = require('./assets.json')
 
 const KAVA_STREAM_VAULT  = '0xd8FDE1F90895AB64E74efD376129Ae3e79F1B9f9'
+const CELO_STREAM_VAULT = "0xE19Dab2511C59341b37656F235F2A722953DcA09"
+const POLYGON_POS_STREAM_VAULT = "0x015E0622F4311eA67dEcB5b433EFd611EF7600c2"
 
 const KAVA_ASSETS = [
   ADDRESSES.kava.SUSHI,
@@ -41,10 +43,29 @@ const KAVA_ASSETS_STABLE = [
   ASSETS.kava.BNB,
 ]
 
+const CELO_ASSET_STABLES = [
+  nullAddress,
+  ASSETS.celo.cUSD,
+  ASSETS.celo.CELO,
+  ASSETS.celo.cEUR
+]
+
+const POLYGON_ASSET_STABLES = [
+  nullAddress,
+  ASSETS.polygon.USDT,
+  ASSETS.polygon.USDC
+]
+
 module.exports = {
   methodology: "TVL is based on the active balances of assets deposited at the stream vault for token streaming.",
   kava: {
     tvl: sumTokensExport({ owners: [KAVA_STREAM_VAULT], tokens: KAVA_ASSETS_STABLE}),
     vesting: sumTokensExport({ owners: [KAVA_STREAM_VAULT], tokens: KAVA_ASSETS}),
+  },
+  celo: {
+    tvl: sumTokensExport({ owners: [CELO_STREAM_VAULT], tokens: CELO_ASSET_STABLES })
+  },
+  polygon: {
+    tvl: sumTokensExport({ owners: [POLYGON_POS_STREAM_VAULT], tokens: POLYGON_ASSET_STABLES})
   }
 };
