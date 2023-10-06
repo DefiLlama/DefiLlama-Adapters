83cf198d94189766b7385a3ab7051bfbdbfea896
llama <llama@llama.fi>
2023-10-06 05:55:01 +0200

    minor fix

--- projects/escrow/index.js
+++ projects/escrow/index.js
@@ -1,15 +1,10 @@
-const { sumTokens2, nullAddress } = require("../helper/unwrapLPs");
+const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
 
 const contract = "0x2a412Df7e18B847e953c2Bc6ae90581dEe8571e3";
 
-async function tvl(time, ethBlock, _b, { api }) {
-  return sumTokens2({ tokens: [nullAddress], owner: contract, api });
-}
-
-//not sure on methodology 
 module.exports = {
-  methodology: `We count the eth on ${contract}`,
-  eth: {
-    volume,
+  methodology: `We count the eth escrowed in ${contract}`,
+  ethereum: {
+    tvl: sumTokensExport({ owner: contract, tokens: [nullAddress]}),
   },
 };
