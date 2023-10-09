877275af25ca1b1cfa52ef57853c0eecd032df6a
daveai <dave@pm.me>
2023-10-09 12:32:07 +0100

    add latest Gnosis addresses for PoolAddress and Data Provider

--- projects/spark-fi/index.js
+++ projects/spark-fi/index.js
@@ -4,5 +4,5 @@ const { aaveExports } = require('../helper/aave');
 
 module.exports = {
   ethereum: aaveExports('ethereum', '0x03cFa0C4622FF84E50E75062683F44c9587e6Cc1', undefined, ["0xFc21d6d146E6086B8359705C8b28512a983db0cb"], { v3: true, blacklistedTokens: [ADDRESSES.ethereum.DAI]}),
-  xdai: aaveExports('xdai', '0x545eeEc8Ca599085cE86ada51eb8c0c35Af1e9d6', undefined, ["0x61B989D473a977884Ac73A3726e1d2f7A6b50e07"], { v3: true})
+  xdai: aaveExports('xdai', '0xA98DaCB3fC964A6A0d2ce3B77294241585EAbA6d', undefined, ["0x2a002054A06546bB5a264D57A81347e23Af91D18"], { v3: true})
 };
