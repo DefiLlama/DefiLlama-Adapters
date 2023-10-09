80d1667489911f3425e38e30cab61917ed8847aa
legend524 <thecrazydev524@gmail.com>
2023-10-10 05:03:42 +0900

    updated tvl calculation function in wildbase

--- projects/wildbase/index.js
+++ projects/wildbase/index.js
@@ -1,12 +1,13 @@
-const { getUniTVL } = require('../helper/unknownTokens')
-const WILDx = "0xbCDa0bD6Cd83558DFb0EeC9153eD9C9cfa87782E";
-const FACTORY = "0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E"
+const { sumTokensExport } = require("../helper/unknownTokens");
 
+const masterchefAddress = "0x3eAB0C9716b0aA98CdC4c3ae317d69dE301ef247";
+const wildxToken = "0xbCDa0bD6Cd83558DFb0EeC9153eD9C9cfa87782E";
+const wildx_weth_lp = "0xeAA13b4f85A98E6CcaF65606361BD590e98DE2Cb"
 
 module.exports = {
     misrepresentedTokens: true,
-    methodology: `Uses Uniswap-style factory address to find and price liquidity pairs.`,
-    base: {
-        tvl: getUniTVL({ factory: FACTORY, useDefaultCoreAssets: true, fetchBalances: true, }),
-    }
+    bsc: {
+        tvl: sumTokensExport({ tokens: [wildxToken, wildx_weth_lp], owner: masterchefAddress, useDefaultCoreAssets: true })
+    },
+    methodology: `Total amount of tokens in treasury and masterchef contract`,
 };
