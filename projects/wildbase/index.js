4fa58bdf4ddf9a6d1578e77dbe22a91064ec027c
legend524 <thecrazydev524@gmail.com>
2023-10-11 01:58:29 +0900

    fixed wildbase tvl

--- projects/wildbase/index.js
+++ projects/wildbase/index.js
@@ -1,8 +1,9 @@
 const { masterchefExports } = require("../helper/unknownTokens");
 
 module.exports = masterchefExports({
-  chain: 'base',
-  masterchef: '0x3eAB0C9716b0aA98CdC4c3ae317d69dE301ef247',
-  useDefaultCoreAssets: true,
-  nativeToken: '0xbCDa0bD6Cd83558DFb0EeC9153eD9C9cfa87782E'
+    chain: 'base',
+    masterchef: '0x3eAB0C9716b0aA98CdC4c3ae317d69dE301ef247',
+    useDefaultCoreAssets: true,
+    tokens: ['0xbCDa0bD6Cd83558DFb0EeC9153eD9C9cfa87782E'],
+    lps: ['0xeAA13b4f85A98E6CcaF65606361BD590e98DE2Cb']
 })
