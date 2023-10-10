bc4d80a403d30aac94a7c0e1803b2112b5cc8d3d
realdealshaman <realdealshaman11@gmail.com>
2023-10-10 07:14:56 +0100

    add distressed asset

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -25,7 +25,8 @@ const distressedAssts = new Set(Object.values({
   aBNBb: ADDRESSES.bsc.aBNBb,
   XRPC: '0xd4ca5c2aff1eefb0bea9e9eab16f88db2990c183',
   FLEET: '0xfd56a3dcfc0690881a466ae432d71bb2db588083',
-  YAKU: 'NGK3iHqqQkyRZUj4uhJDQqEyKKcZ7mdawWpqwMffM3s'
+  YAKU: 'NGK3iHqqQkyRZUj4uhJDQqEyKKcZ7mdawWpqwMffM3s',
+  JEFI: '0x80fa6d5384bdde296a28a321f73ab70977575129'
 }).map(i => i.toLowerCase()))
 
 const transformTokens = {
