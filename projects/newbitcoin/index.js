bf4cd575354c51ba14799972cb675b5274418f08
llama <llama@llama.fi>
2023-10-10 09:52:22 +0200

    fetch tvl on chain

--- projects/newbitcoin/index.js
+++ projects/newbitcoin/index.js
@@ -1,12 +1,8 @@
-const fetch = require("node-fetch");
-
-const tvl = async () => {
-  const resp = await fetch("https://perp-api.fprotocol.io/api/tvl");
-  const rs = await resp.json();
-  return rs.result
-}
+const ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
-  methodology: "New Bitcoin's Total Value Locked (TVL) can be calculated as the sum of the BTC amount locked in all creators' smart contracts. Every content creator has their own smart contract (this is similar to Uniswap's pool structure). Once a key holder buys key(s) of a creator, their purchased BTC is locked in the creator's contract.",
-  fetch: tvl
-} 
+  nos: {
+    tvl: sumTokensExport({ owner: '0xea21fbBB923E553d7b98D14106A104665BA57eCd', tokens: [ADDRESSES.nos.BTC] })
+  }
+}
