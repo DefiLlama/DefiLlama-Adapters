6d13044b0467283c082a3af78fac344de46481d8
llama <llama@llama.fi>
2023-10-11 07:53:50 +0200

    code refactor

--- projects/redemptionfi/index.js
+++ projects/redemptionfi/index.js
@@ -1,8 +1,6 @@
-const { masterChefExports } = require("../helper/masterchef");
+const { masterchefExports } = require("../helper/unknownTokens");
 
-const chef = "0x5c6c79Ff5a58bBC3D7903f439b3A75415685eca3"
-const rdmp = "0x41E99e0F73a88947C52070FF67C19B7aBc171A54"
+const masterchef = "0x5c6c79Ff5a58bBC3D7903f439b3A75415685eca3"
+const nativeToken = "0x41E99e0F73a88947C52070FF67C19B7aBc171A54"
 
-module.exports = {
-  ...masterChefExports(chef, "base", rdmp, false),
-}
+module.exports = masterchefExports({ chain: 'base', masterchef, nativeToken, useDefaultCoreAssets: true,})
