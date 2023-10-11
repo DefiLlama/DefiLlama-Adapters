82fb639f4b5fd36a2af094e70118b3276b3c42c7
llama <llama@llama.fi>
2023-10-11 15:31:34 +0200

    code refactor

--- projects/deepp/index.js
+++ projects/deepp/index.js
@@ -1,27 +1,12 @@
-const BetLiquidityPoolABI = [
-    "function getTokenBalance(address tokenAdd) view returns (uint256)",
-    "function getFullBalance(address tokenAdd) view returns (uint256)",
-];
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const ADDRESSES = require("../helper/coreAssets.json");
 
-const USDC_TOKEN_CONTRACT = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831';
 const BET_LP_CONTRACT = '0x84a512E120294C2017a88a8f1af2219Ec250CBaa';
 
-async function tvl(_, _1, _2, { api }) {
-    const fullPoolBalance = await api.call({
-        abi: BetLiquidityPoolABI[1],
-        target: BET_LP_CONTRACT,
-        params: [USDC_TOKEN_CONTRACT],
-    });
-
-    api.add(USDC_TOKEN_CONTRACT, fullPoolBalance)
-}
-
 module.exports = {
-    timetravel: true,
-    misrepresentedTokens: false,
-    methodology: 'Lists the number of owned USDC tokens in the Deepp LP and BetLock contracts.',
-    start: 1696118400,
-    arbitrum: {
-        tvl,
-    }
+  methodology: 'Lists the number of owned USDC tokens in the Deepp LP and BetLock contracts.',
+  start: 1696118400,
+  arbitrum: {
+    tvl: sumTokensExport({ owners: [BET_LP_CONTRACT], tokens: [ADDRESSES.arbitrum.USDC_CIRCLE] }),
+  }
 }; // node test.js projects/deepp/index.js
