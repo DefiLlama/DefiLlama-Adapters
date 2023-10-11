7ac0695efff404607f038ac7380bc7ce79796e08
llama <llama@llama.fi>
2023-10-11 11:44:06 +0200

    use token labels

--- projects/arcanum/index.js
+++ projects/arcanum/index.js
@@ -1,7 +1,8 @@
+const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport } = require('../helper/unwrapLPs')
 const ARBI_CONTRACT = '0xfc2f1678f7c0d78c3911090c92b86bca7cc3a8b7';
 const ASSETS_CONTRACTS = [
-    '0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a',
+    ADDRESSES.arbitrum.GMX,
     '0x11cdb42b0eb46d95f990bedd4695a6e3fa034978',
     '0x18c11fd286c5ec11c3b683caa813b77f5163a122',
     '0xb64e280e9d1b5dbec4accedb2257a87b400db149',
