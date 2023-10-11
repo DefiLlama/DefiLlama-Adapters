fcc03b0165d0a8d126e2c85d864923dcc183e8eb
llama <llama@llama.fi>
2023-10-11 10:02:41 +0200

    refactor code

--- projects/crayon/index.js
+++ projects/crayon/index.js
@@ -1,6 +1,3 @@
-const sdk = require('@defillama/sdk');
-const { sumTokens2 } = require('../helper/unwrapLPs')
-
 // desks
 const owners = [
   '0x8351483e30928D1Fe1f80eD5062c6438faa85b88',
@@ -9,44 +6,20 @@ const owners = [
   '0x343d5F534C4C1fB83cdDf0875cC91591cCf69416',
   '0x635b2fE7bF8d41B0477A492f953f57b40E385Cfb',
   '0xfE2A45BF13965393c863460F063bDD4a9874c415'
-]; 
-
-// all tokens, all desks
-const tokens = [
-  "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
-  "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
-  "0x912CE59144191C1204E64559FE8253a0e49E6548",
-  "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a",
-  "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
-  "0x3082CC23568eA640225c2467653dB90e9250AaA0"
 ];
 
-async function borrowed(_, _1, _2, { api } ) {
-  const base_tokens = await api.multiCall({
-    calls: owners.map(desk => ({
-      target: desk
-    })),
-    abi: 'address:base_coin',
-  });
+async function borrowed(_, _1, _2, { api }) {
+  const tokens = await api.multiCall({ calls: owners, abi: 'address:base_coin', });
+  const bals = await api.multiCall({ calls: owners, abi: 'uint256:total_loans', });
 
-  const bals = await api.multiCall({
-    calls: owners.map(desk => ({
-      target: desk
-    })),
-    abi: 'uint256:total_loans',
-  });
+  api.addTokens(tokens, bals)
 
-  const borrowed_bals = {};
-  bals.forEach((v, i) => sdk.util.sumSingleBalance(borrowed_bals, base_tokens[i], v));
-  
-  return borrowed_bals;
+  return api.getBalances();
 }
 
-async function tvl(_, _1, _2, { api } ) {
-  // owners are the desks
-  const desk_owned_bals = sumTokens2({owners, tokens, api});
-
-  return desk_owned_bals;
+async function tvl(_, _1, _2, { api }) {
+  const tokens = await api.multiCall({ calls: owners, abi: 'address:base_coin', });
+  return api.sumTokens({ tokensAndOwners2: [tokens, owners] });
 }
 
 module.exports = {
