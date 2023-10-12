563954484a7331d8a26fb36f1ef2456e31cfd4ea
ALPHADA <48852104+ALPHADA@users.noreply.github.com>
2023-10-12 10:09:10 +0900

    Update Gambit Tvl

--- projects/gambit/index.js
+++ projects/gambit/index.js
@@ -1,12 +1,28 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokensExport } = require("../helper/unwrapLPs");
+const { sumTokens2 } = require("../helper/unwrapLPs");
+const { staking } = require("../helper/staking");
 
 const SimpleGToken = "0x0729e806f57CE71dA4464c6B2d313E517f41560b"; // SimpleGToken
+const Treasury = "0x1fb8611064a09469F808263C398623A86e7Aa883"; // Treasury
+
+const tokens = {
+    era: {
+        USDC: ADDRESSES.era.USDC
+    },
+};
+
+async function eraTvl(_, _b, _cb, { api }) {
+    const tokensAndOwners = [
+        [tokens.era.USDC, SimpleGToken],
+        [tokens.era.USDC, Treasury],
+    ];
+    return sumTokens2({ api, tokensAndOwners });
+}
 
 module.exports = {
     methodology: `Count the USDC that has been deposited on Gambit`,
     era: {
-        tvl: sumTokensExport({ owner: SimpleGToken, tokens: [ADDRESSES.era.USDC], }),
+        tvl: eraTvl,
     },
 };
 
