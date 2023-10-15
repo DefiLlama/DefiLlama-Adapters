781f929d6f4893308b6b0bd02f66260501373cc5
realdealshaman <realdealshaman11@gmail.com>
2023-10-15 05:55:45 +0100

    add adapter

--- projects/spacefi/index.js
+++ projects/spacefi/index.js
@@ -7,8 +7,5 @@ module.exports = {
       factory: '0x868A71EbfC46B86a676768C7b7aD65055CC293eE',
       useDefaultCoreAssets: true,
     })
-  },
-  scroll: {
-    tvl: getUniTVL({ factory: '0x6cC370Ed99f1C11e7AC439F845d0BA6aed55cf50', useDefaultCoreAssets: true, })
   }
-}; 
+};
