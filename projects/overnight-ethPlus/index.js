67d148462cf9d003fc6cd4520fcee93ee757a105
rrr-source <84952562+rrr-source@users.noreply.github.com>
2023-10-16 18:12:39 +0500

    added overnight ETH+ on arbitrum (#7719)
    and updated USDT+

--- projects/overnight-ethPlus/index.js
+++ projects/overnight-ethPlus/index.js
@@ -2,15 +2,11 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const sdk = require("@defillama/sdk");
 
 const m2m = {
-  bsc: "0xF3434f6a11AA950150AF3e4962E39E6281496EF9",
-  linea: "0x3d67655A49Adb0F44530233Cbf8375D33FfAde41",
-  arbitrum: "0xf3607bB88738c3388b543d390a90B6ABF4046E3b",
+  arbitrum: "0x672F0f9ECF78406E4E31cd531b0CefE32f0A84B5",
 }
 
 const assets = {
-  bsc: ADDRESSES.bsc.USDT, //USDT
-  linea: ADDRESSES.linea.USDT, //USDT
-  arbitrum: ADDRESSES.arbitrum.USDT //USDT
+  arbitrum: ADDRESSES.arbitrum.WETH, //WETH
 }
 
 const abi = "uint256:totalNetAssets"
