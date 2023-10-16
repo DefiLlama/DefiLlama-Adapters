1e80ac4afcde91ee7ea7d86d5f1d0f5b112859b5
llama <llama@llama.fi>
2023-10-16 08:36:33 +0200

    code refactor

--- projects/factor-leverage-vault/index.js
+++ projects/factor-leverage-vault/index.js
@@ -1,82 +1,27 @@
-/* eslint-disable no-useless-catch */
+const { blockQuery } = require('../helper/http')
 
-const sdk = require("@defillama/sdk");
-const { request, gql } = require("graphql-request");
-
-/*//////////////////////////////////////////////////////////////////////////////
-                                   Constants                                             
-//////////////////////////////////////////////////////////////////////////////*/
-
-const queryBlock = gql`
-  {
-    leverageVaultPoolTokenStates(block: { number: $block }) {
-      underlyingAssetAddress
-      balanceRaw
-    }
+const queryBlock = `query  data($block: Int){
+  leverageVaultPoolTokenStates(where: { balanceRaw_gt: 0} block: { number: $block }) {
+    id type underlyingAssetAddress      balanceRaw
   }
-`;
-
-const queryLatest = gql`
-  {
-    leverageVaultPoolTokenStates {
-      underlyingAssetAddress
-      balanceRaw
-    }
-  }
-`;
+}`
 
 const SUBGRAPH_URL =
   "https://api.thegraph.com/subgraphs/name/dimasriat/factor-leverage-vault";
 
-/*//////////////////////////////////////////////////////////////////////////////
-                                    Helpers                                            
-//////////////////////////////////////////////////////////////////////////////*/
+async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
+  const { leverageVaultPoolTokenStates } = await blockQuery(SUBGRAPH_URL, queryBlock, { api })
 
-async function subgraphCall(url, query, queryFail, variable) {
-  let result;
-
-  try {
-    result = await request(url, query, variable);
-  } catch (err) {
-    result = await request(url, queryFail, variable);
+  for (let poolTokenState of leverageVaultPoolTokenStates) {
+    let { underlyingAssetAddress, balanceRaw } = poolTokenState;
+    if (poolTokenState.type === 'debt') balanceRaw *= -1
+    api.add(underlyingAssetAddress, balanceRaw)
   }
 
-  return result;
+  return api.getBalances()
 }
 
-/*//////////////////////////////////////////////////////////////////////////////
-                                  TVL Handler                                              
-//////////////////////////////////////////////////////////////////////////////*/
-
-async function tvl(timestamp, ethBlock, chainBlocks) {
-  try {
-    const balances = {};
-
-    const { leverageVaultPoolTokenStates } = await subgraphCall(
-      SUBGRAPH_URL,
-      queryBlock,
-      queryLatest,
-      { block: chainBlocks["arbitrum"] }
-    );
-
-    for (let poolTokenState of leverageVaultPoolTokenStates) {
-      const { underlyingAssetAddress, balanceRaw } = poolTokenState;
-
-      sdk.util.sumSingleBalance(
-        balances,
-        `arbitrum:${underlyingAssetAddress}`,
-        balanceRaw
-      );
-    }
-
-    return balances;
-  } catch (err) {
-    throw err;
-  }
-}
-
-/*//////////////////////////////////////////////////////////////////////////////
-                                 Module Exports                                               
-//////////////////////////////////////////////////////////////////////////////*/
-
-module.exports = { arbitrum: { tvl } };
+module.exports = {
+  doublecounted: true,
+  arbitrum: { tvl }
+};
