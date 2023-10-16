54e6453c146c193503cb216a66a9889d19932c39
llama <llama@llama.fi>
2023-10-16 15:49:43 +0200

    minor fix

--- projects/entities/arrington-xrp-capital.js
+++ projects/entities/arrington-xrp-capital.js
@@ -8,8 +8,8 @@ const config = {
         "0xaea97b739dcd6f5b586586cb7a538d60ba96a757",
         "0x1f9e17D8C21b00d2BbdfE637C33db21c3B1F4E4e"
     ],
-    tokens: ["0x29C827Ce49aCCF68A1a278C67C9D30c52fBbC348", "0xC36442b4a4522E871399CD717aBDD847Ab11FE88"], //sushi lp , univ3 position
-    resolveLP: true
+    tokens: ["0x29C827Ce49aCCF68A1a278C67C9D30c52fBbC348"], //sushi lp
+    resolveUniV3: true,
   },
   avax: {
     owners: [
