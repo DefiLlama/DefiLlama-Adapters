b28883c84ab3bb7cd44060fa023df09849266b08
Brdd Mnk <ilchuksergey@gmail.com>
2023-10-17 14:39:36 +0300

    new zkSync Era strategies (#7732)

--- projects/parallax/index.js
+++ projects/parallax/index.js
@@ -1,8 +1,8 @@
 const sdk = require("@defillama/sdk");
 const { default: BigNumber } = require("bignumber.js");
 
-const { staking } = require('../helper/staking')
-const { sumTokensExport } = require('../helper/unwrapLPs')
+const { staking } = require("../helper/staking");
+const { sumTokensExport } = require("../helper/unwrapLPs");
 const { getPriceMIM, getPriceAura, getPriceSushi } = require("./getPrice");
 
 const prllxERC20 = require("./abis/prllxERC20.json");
@@ -148,30 +148,55 @@ async function arbitrumTvl(time, _ethBlock, { arbitrum: block }, { api }) {
   return balances;
 }
 
-async function eraTvl(_, _b, _cb, { api, }) {
+async function eraTvl(_, _b, _cb, { api }) {
+  if (contracts.era.length > 0) {
+    for (let i = 0; i < contracts.era.length; i++) {
+      const strItem = contracts.era[i];
+      const strategyId = await api.call({
+        target: strItem.parallaxAddress,
+        params: strItem.strategyAddress,
+        abi: prllxERC20["strategyToId"],
+      });
 
-  const strategyId = await api.call({ target: contracts.era.parallaxAddress, params: contracts.era.strategyAddress, abi: prllxERC20["strategyToId"], });
-
-  const strategy = await api.call({ target: contracts.era.parallaxAddress, params: strategyId, abi: prllxERC20.strategiesERA, });
-  const pair = contracts.era.lpAddress
-  const [token0, token1, reserves, totalSupply] = await Promise.all([
-    api.call({ target: pair, abi: 'address:token0' }),
-    api.call({ target: pair, abi: 'address:token1' }),
-    api.call({ target: pair, abi: "function getReserves() view returns (uint256 _reserve0, uint256 _reserve1)" }),
-    api.call({ target: pair, abi: 'uint256:totalSupply' }),
-  ])
-  const ratio = strategy.totalStaked / totalSupply
-  api.add(token0, reserves._reserve0 * ratio)
-  api.add(token1, reserves._reserve1 * ratio)
-  return api.getBalances()
+      const strategy = await api.call({
+        target: strItem.parallaxAddress,
+        params: strategyId,
+        abi: prllxERC20.strategiesERA,
+      });
+      const pair = strItem.lpAddress;
+      const [token0, token1, reserves, totalSupply] = await Promise.all([
+        api.call({ target: pair, abi: "address:token0" }),
+        api.call({ target: pair, abi: "address:token1" }),
+        api.call({
+          target: pair,
+          abi: "function getReserves() view returns (uint256 _reserve0, uint256 _reserve1)",
+        }),
+        api.call({ target: pair, abi: "uint256:totalSupply" }),
+      ]);
+      const ratio = strategy.totalStaked / totalSupply;
+      api.add(token0, reserves._reserve0 * ratio);
+      api.add(token1, reserves._reserve1 * ratio);
+    }
+    return api.getBalances();
+  }
 }
 
 module.exports = {
   methodology: "TVL comes from the Staking Vaults",
-   arbitrum: {
+  arbitrum: {
     tvl: arbitrumTvl,
-    staking: staking(['0x82FD636D7A28a20635572EB8ec0603ee264B8651', '0xA3CE2c0d1cfB29F398f8f4800bA202Aba39dbbfe', '0xEb370470Afd74d8a9BBC4fF0C94371C310fF9D3e', ], '0xc8CCBd97b96834b976C995a67BF46e5754e2C48E'),
-    pool2: sumTokensExport({ owner: '0xEb370470Afd74d8a9BBC4fF0C94371C310fF9D3e', resolveUniV3: true, })
+    staking: staking(
+      [
+        "0x82FD636D7A28a20635572EB8ec0603ee264B8651",
+        "0xA3CE2c0d1cfB29F398f8f4800bA202Aba39dbbfe",
+        "0xEb370470Afd74d8a9BBC4fF0C94371C310fF9D3e",
+      ],
+      "0xc8CCBd97b96834b976C995a67BF46e5754e2C48E"
+    ),
+    pool2: sumTokensExport({
+      owner: "0xEb370470Afd74d8a9BBC4fF0C94371C310fF9D3e",
+      resolveUniV3: true,
+    }),
   },
   ethereum: {
     tvl: ethTvl,
