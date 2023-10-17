0679c3574073065f284caf2d3bc0a34a09940c91
richard-luigiswap <142534010+richard-luigiswap@users.noreply.github.com>
2023-10-17 18:37:54 +0700

    [luigiswap] track scroll (#7734)

--- projects/LuigiSwap/index.js
+++ projects/LuigiSwap/index.js
@@ -9,4 +9,11 @@ module.exports = {
       fetchBalances: true,
     }),
   },
+  scroll: {
+    tvl: getUniTVL({
+      factory: "0x0dAe6d22182c20AB9150a4DCB3160591Dc41027a",
+      useDefaultCoreAssets: true,
+      fetchBalances: true,
+    }),
+  },
 };
