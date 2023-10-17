c7ae0802414b1a26b799f82cf8d0279538dd8bb8
llama <llama@llama.fi>
2023-10-18 00:15:30 +0200

    track ociswap: https://ociswap.com/

--- projects/caviarnine/index.js
+++ projects/caviarnine/index.js
@@ -5,9 +5,7 @@ module.exports = {
     tvl: async (_, _1, _2, { api }) => {
       const { shapeliquidity } = await getConfig('caviarnine', 'https://api-core.caviarnine.com/v1.0/shapeliquidity/get_components')
       const owners = shapeliquidity.map(i => i.component_address)
-      return sumTokens({
-        owners, api,
-      })
+      return sumTokens({ owners, api, })
     },
   },
   timetravel: false
