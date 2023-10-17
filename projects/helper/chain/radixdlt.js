5ed79350d119cc0dc3352ec22c9f467bf83781cb
llama <llama@llama.fi>
2023-10-17 23:33:24 +0200

    track caviarnine tvl

--- projects/helper/chain/radixdlt.js
+++ projects/helper/chain/radixdlt.js
@@ -1,82 +1,40 @@
 const { post } = require('../http')
-
-function getNameFromMetadata(metadata) {
-  for(const item of metadata.items) {
-    if (item.key == "name") {
-      return item.value.typed.value.replace(/ /g, "-")
-    }
-  }
-  return ""
-}
+const { getUniqueAddresses, } = require('../tokenMapping')
+const { getFixBalancesSync } = require('../portedTokens')
+const { sliceIntoChunks } = require('../utils')
+const chain = 'radixdlt'
 
 const ENTITY_DETAILS_URL = `https://mainnet.radixdlt.com/state/entity/details`
 
-/**
- * getBalances expects a list of components addresses like
- * [
- *  component_rdx1cptxxxxxxxxxajhkjahkaj,
- *  component_rdx1cptxxxxxxxxxlkjljljlkkj
- * ]
- *
- * Each component has a list of fungible resources so we call state/entity/details
- * with all the components addresses as params and we get a list of items as result
- *
- * We iterate over all items and we add up the amount for each fungible resource
- * DefiLlama sdk expects an input in the for of
- *
- * {
- *  token_one_name_in_coin_gecko: amount_token_one,
- *  token_two_name_in_coin_gecko: amount_token_two
- * }
- *
- */
+async function sumTokens({ owner, owners = [], api, }) {
+  const fixBalances = getFixBalancesSync(chain)
 
-async function getBalances(components) {
-  const body = {
-    "addresses": components,
-    "opt_ins": {"explicit_metadata": ["name"]}
+  if (owner) owners.push(owner)
+  owners = getUniqueAddresses(owners)
+  if (!owners.length) return api.getBalances()
+  let items = []
+  const chunks  = sliceIntoChunks(owners, 20)
+  for (const chunk of chunks) {
+    const body = {
+      "addresses": chunk,
+      "opt_ins": { "explicit_metadata": ["name"] }
+    }
+    let data = await post(ENTITY_DETAILS_URL, body)
+    items.push(...data.items)
   }
-  let data;
-  try {
-    data = await post(ENTITY_DETAILS_URL, body)
-  } catch (error) {
-    console.log(error)
-    return {}
-  }
-  const resourceAmounts = {}
-  data.items.forEach((item) => {
-    item.fungible_resources.items.forEach((resource) => {
-      const resourceAddress = resource.resource_address;
-      const amount = resource.amount;
-
-      if (resourceAmounts.hasOwnProperty(resourceAddress)) {
-        resourceAmounts[resourceAddress] += parseFloat(amount);
-      } else {
-        resourceAmounts[resourceAddress] = parseFloat(amount);
-      }
+  items.forEach((item) => {
+    item.fungible_resources.items.forEach(({ resource_address, amount }) => {
+      api.add(resource_address, +amount)
     });
   });
+  fixBalances(api.getBalances())
+}
 
-  let balancesByName = {}
-  body["addresses"] = Object.keys(resourceAmounts);
-
-  let resourcesDetails
-  try {
-    resourcesDetails = await post(ENTITY_DETAILS_URL, body)
-  } catch (error) {
-    console.log(error)
-    resourcesDetails.items = []
-  }
-
-  resourcesDetails.items.forEach(resource => {
-    const resourceName = getNameFromMetadata(resource.explicit_metadata)
-    balancesByName[resourceName] = resourceAmounts[resource.address]
-  });
-
-  console.log(balancesByName)
-  return balancesByName
+function sumTokensExport(...args) {
+  return async (_, _1, _2, { api }) => sumTokens({ ...args, api, })
 }
 
 module.exports = {
-  getBalances,
+  sumTokens,
+  sumTokensExport,
 }
