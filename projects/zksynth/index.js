2bc63fd38dbe3fa16391c40d5e2c160526f362d0
prasad-kumkar <pk111_@live.com>
2023-10-19 01:19:03 +0530

    TVL fix

--- projects/zksynth/index.js
+++ projects/zksynth/index.js
@@ -1,21 +1,33 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokensExport } = require('../helper/unwrapLPs')
+// const retry = require('async-retry')
+const { GraphQLClient, gql } = require('graphql-request')
+
+async function fetch() {
+  var endpoint = 'https://api.thegraph.com/subgraphs/name/prasad-kumkar/zksynth-mainnet';
+  var graphQLClient = new GraphQLClient(endpoint)
+
+  var query = gql`
+  {
+    pools(first: 10) {
+      collaterals{
+        totalDeposits
+        priceUSD
+        token{
+          decimals
+        }
+      }
+    }
+  }
+  `;
+  const results = await graphQLClient.request(query);
+  let tvl = 0;
+  for(let i = 0; i < results.pools.length; i++){
+    for(let j = 0; j < results.pools[i].collaterals.length; j++){
+      tvl += results.pools[i].collaterals[j].totalDeposits * results.pools[i].collaterals[j].priceUSD / Math.pow(10, results.pools[i].collaterals[j].token.decimals);
+    }
+  }
+  return tvl;
+}
 
 module.exports = {
-  mantle: {
-    tvl: sumTokensExport({
-      owner: '0x78B2fa94A94bF3E96fcF9CE965bed55bE49FA9E7',
-      tokens: [
-        ADDRESSES.scroll.WETH,
-        "0xcA77eB3fEFe3725Dc33bccB54eDEFc3D9f764f97", // DAI
-        "0xeDEAbc3A1e7D21fE835FFA6f83a710c70BB1a051", // LUSD
-        "0x53878B874283351D26d206FA512aEcE1Bef6C0dD", // rETH
-        "0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4", // USDC
-        "0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df", // USDT
-        "0x3C1BCa5a656e69edCD0D4E36BEbb3FcDAcA60Cf1", // WBTC
-        "0xf610A9dfB7C89644979b4A0f27063E9e7d7Cda32" // wstETH
-
-      ]
-    }),
-  }
+  fetch
 }
