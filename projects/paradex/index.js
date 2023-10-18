e7481231c1a1e5cef27b77508248293ac2cc0a52
llama <llama@llama.fi>
2023-10-18 11:36:56 +0200

    code refactor

--- projects/paradex/index.js
+++ projects/paradex/index.js
@@ -1,40 +1,10 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-  const sdk = require('@defillama/sdk');
+const { sumTokensExport } = require('../helper/unwrapLPs');
 
-  const contracts = [
-    '0xE3cbE3A636AB6A754e9e41B12b09d09Ce9E53Db3'
-  ];
+const contracts = [
+  '0xE3cbE3A636AB6A754e9e41B12b09d09Ce9E53Db3'
+];
 
-  const tokens = [
-    ADDRESSES.ethereum.USDC
-  ];
-
-  async function tvl (timestamp, block) {
-    const balances = {};
-
-    let balanceOfCalls = [];
-    contracts.forEach((contract) => {
-      balanceOfCalls = [
-        ...balanceOfCalls,
-        ...tokens.map((token) => ({
-          target: token,
-          params: contract
-        }))
-      ];
-    });
-
-    const balanceOfResult = (await sdk.api.abi.multiCall({
-      block,
-      calls: balanceOfCalls,
-      abi: 'erc20:balanceOf',
-    }));
-
-    sdk.util.sumMultiBalanceOf(balances, balanceOfResult, true)
-
-    return balances;
-  }
-
-  module.exports = {
-    start: 1696118400,  // 09/29/2018 @ 12:00am (UTC)
-    ethereum: { tvl }
-  };
+module.exports = {
+  ethereum: { tvl: sumTokensExport({ owners: contracts, tokens: [ADDRESSES.ethereum.USDC] }) }
+};
