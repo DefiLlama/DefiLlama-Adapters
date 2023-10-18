94902cf865578e515ec59ce6094f9a90ddcc58c3
llama <llama@llama.fi>
2023-10-18 11:42:01 +0200

    fix jojo

--- projects/jojo/index.js
+++ projects/jojo/index.js
@@ -6,7 +6,7 @@ module.exports = {
     bsc: { tvl: sumTokensExport({ tokens: [ADDRESSES.bsc.USDC], owners: ['0x25173BB47CB712cFCDFc13ECBebDAd753090801E'], }) },
     arbitrum: { tvl: sumTokensExport(
         {tokens: [ADDRESSES.arbitrum.USDC, ADDRESSES.arbitrum.WETH, ADDRESSES.arbitrum.WBTC, 
-                ADDRESSES.arbitrum.ARB, ADDRESSES.arbitrum.GMX, ADDRESSES.arbitrum.WSTETH, ADDRESSES.arbitrum.Magic], 
+                ADDRESSES.arbitrum.ARB, ADDRESSES.arbitrum.GMX, ADDRESSES.arbitrum.WSTETH, '0x539bde0d7dbd336b79148aa742883198bbf60342'], 
             owners: ['0xcDf9eED57Fe8dFaaCeCf40699E5861517143bcC7','0x747282eadcd331e3a8725DcD9e358514D723b3a3']}
         ) },
 };
