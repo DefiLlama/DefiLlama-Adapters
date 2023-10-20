12e13114fd92489d9322564d1b6fcce7c0ee469b
Eugene Mormul <zhenya.mormul@gmail.com>
2023-10-20 09:23:33 +0300

    feat: atokens for tvl (#7789)

--- projects/primex-finance/index.js
+++ projects/primex-finance/index.js
@@ -15,13 +15,20 @@ const config = {
       USDT: ADDRESSES.polygon.USDT,
       EPMX: "0xDc6D1bd104E1efa4A1bf0BBCf6E0BD093614E31A"
     },
+    aaveTokens: {
+      [ADDRESSES.polygon.WETH_1]: "0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8",
+      [ADDRESSES.polygon.WBTC]: "0x078f358208685046a11C85e8ad32895DED33A249",
+      [ADDRESSES.polygon.WMATIC_2]: "0x6d80113e533a2C0fe82EaBD35f1875DcEA89Ea97",
+      [ADDRESSES.polygon.USDC]: "0x625E7708f30cA75bfd92586e17077590C60eb4cD",
+      [ADDRESSES.polygon.USDT]: "0x6ab707Aca953eDAeFBc4fD23bA73294241490620",
+    },
   },
 }
 
 module.exports = {}
 
 Object.keys(config).forEach(chain => {
-  const { bucketsFactory, positionManager, traderBalanceVault, defaultTokens } = config[chain]
+  const { bucketsFactory, positionManager, traderBalanceVault, defaultTokens, aaveTokens } = config[chain]
 
   module.exports[chain] = {
     tvl: async (_, _b, _cb, { api, }) => {
@@ -29,13 +36,13 @@ Object.keys(config).forEach(chain => {
       const borrowedTokensAddresses = await api.multiCall({ abi: abi.borrowedAsset, calls: buckets })
 
       const tokensAndOwnersBuckets = buckets.map((b, i) => [borrowedTokensAddresses[i], b])
+      const aTokensAndOwnersBuckets = buckets.map((b, i) => [aaveTokens[borrowedTokensAddresses[i].toLowerCase()], b]).filter((p) => p[0])
       const tokensAndOwnersPM = Object.values(defaultTokens).map(t => [t, positionManager])
       const tokensAndOwnersTBV = Object.values(defaultTokens).map(t => [t, traderBalanceVault])
 
-      const tokensAndOwners = tokensAndOwnersBuckets.concat(tokensAndOwnersPM, tokensAndOwnersTBV)
+      const tokensAndOwners = tokensAndOwnersBuckets.concat(aTokensAndOwnersBuckets, tokensAndOwnersPM, tokensAndOwnersTBV)
 
       return sumTokens2({ api, tokensAndOwners })
     }
   }
 })
-
