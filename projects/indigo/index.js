453298070bbeca5758d67628237851cafbb381a9
Cody Butz <codyjbutz@gmail.com>
2023-10-20 15:58:24 -0400

    Add staked INDY to TVL

--- projects/indigo/index.js
+++ projects/indigo/index.js
@@ -4,9 +4,14 @@ module.exports = {
   timetravel: false,
   cardano: {
     tvl: async () => {
-      const data = await get('https://analytics.indigoprotocol.io/api/cdps')
+      const cdpData = await get('https://analytics.indigoprotocol.io/api/cdps')
+      const cdpTvl = cdpData.reduce((a, i) => a + (i.collateralAmount/1e6), 0);
+      const stakingManagerData = await get('https://analytics.indigoprotocol.io/api/staking-manager')
+      const totalIndyStaked = stakingManagerData.total_stake / 10 ** 6;
+      const indyPriceData = await get('https://analytics.indigoprotocol.io/api/indy-price')
+      const totalIndyStakedTvl = indyPriceData.ada_price * totalIndyStaked;
       return {
-        cardano: data.reduce((a, i) => a + (i.collateralAmount/1e6), 0)
+        cardano: cdpTvl + totalIndyStakedTvl
       }
     }
   },
