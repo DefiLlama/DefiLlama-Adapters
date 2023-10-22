30c7634141a41137d688d3f3aa28e71190e1c6c5
llama <llama@llama.fi>
2023-10-22 06:49:52 +0200

    WIP

---
+++ projects/defisaver/config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  ethereum: {
+    subscriptions: [
+      { fromBlock: 14242966, target: "0x1612fc28Ee0AB882eC99842Cde0Fc77ff0691e90" },
+    ]
+  },
+  /* optimism: {
+    subscriptions: [
+      { fromBlock: 13688707, target: "0xb944291Ed31886b20030d0d4C47c7838d1d9eb97" },
+    ]
+  },
+  arbitrum: {
+    subscriptions: [
+      { fromBlock: 19126047, target: "0x24ab68395660b910BfBF1cc88BaA316BA06354eE" },
+    ]
+  }, */
+
+}
--- projects/defisaver/index.js
+++ projects/defisaver/index.js
@@ -1,9 +1,11 @@
-const Web3 = require('web3');
+// const Web3 = require('web3');
 const DefiLlamaSdk = require('@defillama/sdk');
-const DfsAutomationSdk = require('@defisaver/automation-sdk');
-const DfsPositionsSdk = require('@defisaver/positions-sdk');
+const { getLogs } = require('../helper/cache/getLogs')
+// const DfsAutomationSdk = require('@defisaver/automation-sdk');
+// const DfsPositionsSdk = require('@defisaver/positions-sdk');
 
-const { sliceIntoChunks } = require('./helper/utils');
+const { sliceIntoChunks } = require('../helper/utils');
+const config = require('./config');
 
 function strategiesMapping(chainId, provider) {
   return ({
@@ -43,8 +45,10 @@ function accountBalancesMapping(protocolIdentifier) {
   })[protocolIdentifier];
 }
 
-async function tvl(timestamp, ethBlock, networkBlockMapping, chainApi) {
+async function _tvl(timestamp, ethBlock, networkBlockMapping, chainApi) {
   const balances = {};
+
+  return balances
 
   const rpcUrl = chainApi.api.provider.providerConfigs[0].provider.connection.url;
   const provider = new Web3(rpcUrl);
@@ -112,7 +116,113 @@ async function tvl(timestamp, ethBlock, networkBlockMapping, chainApi) {
 module.exports = {
   doublecounted: true,
   methodology: 'TVL accounts for all assets deposited into the automated strategies.',
-  ethereum: { tvl },
-  arbitrum: { tvl },
-  optimism: { tvl },
 };
+
+Object.keys(config).forEach(chain => {
+  const { subscriptions } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const logs = await Promise.all(subscriptions.map(sub => getSubscriptions(api, sub)))
+      console.log(logs[0].subscribeLogs.length, logs[0].updateLogs.length)
+      console.log(logs[0].subscribeLogs[0], logs[0].updateLogs[0], chain, logs[0].updateLogs[0].args.subId)
+      // const allLogs = logs.flat()
+      // console.log(allLogs[0], chain)
+      // console.log(allLogs.length)
+      return {}
+    }
+  }
+})
+
+async function getSubscriptions(api, { target, fromBlock }) {
+  const subscribeLogs = await getLogs({ api, target, eventAbi: subscriptionAbis.Subscribe, onlyArgs: true, fromBlock, topics: ['0x7791eea7fd5a877940fd0654c6b1e05911293cedabdfe1f99b7c2cb0308dbaf7'], extraKey: 'Subscribe' })
+  const updateLogs = await getLogs({ api, target, eventAbi: subscriptionAbis.UpdateData, onlyArgs: false, fromBlock, topics: ['0x1fdee328afa41b70ae10ae58d8e54117600357f6737bca0764ddb6fcad6ac9dd'], extraKey: 'UpdateData' })
+  updateLogs.sort((a, b) => a.blockNumber - b.blockNumber)
+  console.log(JSON.stringify(updateLogs[0].blockNumber, null, 2),JSON.stringify(updateLogs[1].blockNumber, null, 2), api.chain)
+  const subIds = subscribeLogs.map(log => +log.subId)
+  const strategiesSubs = await api.multiCall({  abi: subscriptionAbis.strategiesSubs, calls: subIds, target,})
+  console.log(strategiesSubs[0], api.chain)
+  const subscriptions = strategiesSubs.map((sub, i) => {
+    let latestUpdate = subscribeLogs[i];
+
+    if (latestUpdate.subHash !== sub?.strategySubHash) {
+      const lastUpdate = updateLogs.find(log => log.args.subId === latestUpdate.subId);
+      latestUpdate = {
+        ...latestUpdate, // Update is missing proxy, hence this
+        ...lastUpdate,
+      };
+    }
+    return getParsedSubscriptions({
+      chainId: api.chainId,
+      subscriptionEventData: latestUpdate,
+      strategiesSubsData: sub,
+    });
+  })
+
+  return { subscribeLogs, updateLogs }
+}
+
+const subscriptionAbis = {
+  "Subscribe": "event Subscribe(uint256 indexed subId, address indexed proxy, bytes32 indexed subHash, tuple(uint64 strategyOrBundleId, bool isBundle, bytes[] triggerData, bytes32[] subData) subStruct)",
+  "UpdateData": "event UpdateData(uint256 indexed subId, bytes32 indexed subHash, tuple(uint64 strategyOrBundleId, bool isBundle, bytes[] triggerData, bytes32[] subData) subStruct)",
+  "ActivateSub": "event ActivateSub(uint256 indexed subId)",
+  "DeactivateSub": "event DeactivateSub(uint256 indexed subId)",
+  "activateSub": "function activateSub(uint256 _subId)",
+  "adminVault": "address:adminVault",
+  "deactivateSub": "function deactivateSub(uint256 _subId)",
+  "getSub": "function getSub(uint256 _subId) view returns (tuple(bytes20 userProxy, bool isEnabled, bytes32 strategySubHash))",
+  "getSubsCount": "uint256:getSubsCount",
+  "kill": "function kill()",
+  "registry": "address:registry",
+  "strategiesSubs": "function strategiesSubs(uint256) view returns (bytes20 userProxy, bool isEnabled, bytes32 strategySubHash)",
+  "subscribeToStrategy": "function subscribeToStrategy(tuple(uint64 strategyOrBundleId, bool isBundle, bytes[] triggerData, bytes32[] subData) _sub) returns (uint256)",
+  "updateSubData": "function updateSubData(uint256 _subId, tuple(uint64 strategyOrBundleId, bool isBundle, bytes[] triggerData, bytes32[] subData) _sub)",
+  "withdrawStuckFunds": "function withdrawStuckFunds(address _token, address _receiver, uint256 _amount)"
+}
+
+function getParsedSubscriptions(parseData){
+  const {
+    chainId, subscriptionEventData, strategiesSubsData,
+  } = parseData;
+  const {
+    subStruct, proxy, subId, subHash,
+  } = subscriptionEventData;
+  const { isEnabled } = strategiesSubsData;
+
+  const id = subStruct.strategyOrBundleId;
+
+  const strategyOrBundleInfo = (
+    subStruct.isBundle
+      ? BUNDLES_INFO[chainId][id]
+      : STRATEGIES_INFO[chainId][id]
+  );
+
+  if (!strategyOrBundleInfo) return null;
+
+  const position = {
+    isEnabled,
+    chainId,
+    subHash,
+    subId: Number(subId),
+    owner: proxy,
+    protocol: {
+      ...strategyOrBundleInfo.protocol,
+    },
+    strategy: {
+      isBundle: subStruct.isBundle,
+      ...strategyOrBundleInfo,
+    },
+    strategyData: {
+      encoded: {
+        triggerData: subStruct.triggerData,
+        subData: subStruct.subData,
+      },
+      decoded: {
+        triggerData: null,
+        subData: null,
+      },
+    },
+    specific: {},
+  };
+
+  return getParsingMethod(position.protocol.id, position.strategy)(position, parseData, chainId);
+}
