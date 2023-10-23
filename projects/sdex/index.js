5c77b82a3987bf51700a3e3fc94f26d1fc04376a
liu941125 <84958447+liu941125@users.noreply.github.com>
2023-10-23 12:14:14 +0800

    Update index.js

--- projects/sdex/index.js
+++ projects/sdex/index.js
@@ -2,7 +2,7 @@ const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
   misrepresentedTokens: true,
-  arbitrum: {
+  ethf: {
     tvl: getUniTVL({ factory: '0x425D988a209125C58d16A9376f997D24009f569b', useDefaultCoreAssets: true, }),
   }
 }
