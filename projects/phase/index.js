b64d027efd1325e1f258934f27cb8b3c8eb2b363
llama <llama@llama.fi>
2023-10-23 11:02:21 +0200

    code refactor

--- projects/phase/index.js
+++ projects/phase/index.js
@@ -1,64 +1,45 @@
-const sdk = require("@defillama/sdk");
 const config = require("./config");
+const { staking } = require('../helper/staking')
+const { getLogs } = require('../helper/cache/getLogs')
+
+async function getVaults(api) {
+  const { factory, fromBlock } = config[api.chain]
+  const logs = await getLogs({
+    api,
+    target: factory,
+    eventAbi: 'event ContractCreated (address indexed creator, address created)',
+    onlyArgs: true,
+    fromBlock,
+  })
+  return logs.map(i => i.created)
+}
 
 async function tvl(timestamp, block, chainBlocks, { api }) {
-  const { vaults } = config[api.chain];
-
-  for (const vault of vaults) {
-    const asset = await api.call({
-      abi: "function asset() external view returns (address)",
-      target: vault,
-    });
-
-    const collateralBalance = await api.call({
-      abi: "function collectiveCollateral() external view returns (uint256)",
-      target: vault,
-    });
-
-    api.add(asset, collateralBalance);
-  }
+  const vaults = await getVaults(api)
+  const assets = await api.multiCall({ abi: 'address:asset', calls: vaults })
+  const bals = await api.multiCall({ abi: 'uint256:collectiveCollateral', calls: vaults })
+  api.addTokens(assets, bals)
+  return api.getBalances()
 }
 
 async function borrowed(timestamp, block, chainBlocks, { api }) {
-  const { vaults, cash } = config[api.chain];
-  const balances = {};
-
-  for (const vault of vaults) {
-    const totalBorrowed = await api.call({
-      abi: "function collectiveDebt() external view returns (uint256)",
-      target: vault,
-    });
-
-    sdk.util.sumSingleBalance(balances, cash, totalBorrowed);
-  }
-
-  return balances;
-}
-
-async function staking(timestamp, block, chainBlocks, { api }) {
-  const { cash, bond } = config[api.chain];
-
-  const totalBonded = await api.call({
-    abi: "erc20:balanceOf",
-    target: cash,
-    params: [bond],
-  });
-
-  return {
-    [cash]: totalBonded,
-  };
+  const vaults = await getVaults(api)
+  const assets = vaults.map(_ => config[api.chain].cash)
+  const bals = await api.multiCall({ abi: 'uint256:collectiveDebt', calls: vaults })
+  api.addTokens(assets, bals)
+  return api.getBalances()
 }
 
 module.exports = {
-  timetravel: true,
   methodology:
     "Counts the TVL of the Phase vaults. Counts total borrows. Counts $CASH in the bond.",
 };
 
 Object.keys(config).forEach((chain) => {
+  const { cash, bond } = config[chain];
   module.exports[chain] = {
     tvl,
-    borrowed,
-    staking,
+    // borrowed,
+    staking: staking(bond, cash),
   };
 });
