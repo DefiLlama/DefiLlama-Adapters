8fbf9edf148baecde00f0a6a86c7767c6d086815
0xngmi <0xngmi@protonmail.com>
2023-10-24 04:18:39 +0100

    solidly update

--- projects/solidly-v3/index.js
+++ projects/solidly-v3/index.js
@@ -1,5 +1,5 @@
 const { uniV3Export } = require("../helper/uniswapV3");
 
 module.exports = uniV3Export({
-  ethereum: { factory: "0x735bb16affe83a3dc4dc418abccf179617cf9ff2", fromBlock: 18044650, },
+  ethereum: { factory: "0x70fe4a44ea505cfa3a57b95cf2862d4fd5f0f687", fromBlock: 18044650, },
 });
