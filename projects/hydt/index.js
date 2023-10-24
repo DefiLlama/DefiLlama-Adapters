bc80ef44d64aa94f275ed2fabe11f69d7f6e40d5
llama <llama@llama.fi>
2023-10-24 14:11:57 +0200

    bugfix

--- projects/hydt/index.js
+++ projects/hydt/index.js
@@ -6,9 +6,9 @@ const earnAddress = "0x8e48d5b2Ac80d9861d07127F06BbF02F73520Ced";
 const controlPairAddress = "0xBB8ae522F812E9E65239A0e5db87a9D738ce957a";
 
 async function tvl(_, _b, _cb, { api, }) {
-  const hydtBal = await api.call({  abi: 'erc20:balanceOf', target: shydtAddress, params: earnAddress})
+  const hydtBal = await api.call({ abi: 'erc20:balanceOf', target: shydtAddress, params: earnAddress })
   api.add(hydtAddress, hydtBal)
-  return sumUnknownTokens({ api, lps: [controlPairAddress], useDefaultCoreAssets: true, })
+  return sumUnknownTokens({ api, lps: [controlPairAddress], useDefaultCoreAssets: true, })
 }
 
 module.exports = {
