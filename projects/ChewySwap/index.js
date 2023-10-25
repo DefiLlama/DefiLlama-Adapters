6e3c55df13f743b8111dfb35f717f3fdb46c3687
llama <llama@llama.fi>
2023-10-25 08:07:17 +0200

    update chewyswap

--- projects/ChewySwap/index.js
+++ projects/ChewySwap/index.js
@@ -1,3 +1,10 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
+const { getUniTVL } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('shibarium', '0xEDedDbde5ffA62545eDF97054edC11013ED72125', { fetchBalances: true, })
+module.exports = {
+  shibarium: {
+    tvl: getUniTVL({ factory: "0xEDedDbde5ffA62545eDF97054edC11013ED72125", useDefaultCoreAssets: true,})
+  },
+  dogechain: {
+    tvl: getUniTVL({ factory: "0x7C10a3b7EcD42dd7D79C0b9d58dDB812f92B574A", useDefaultCoreAssets: true,})
+  }
+}
