7c2721a8a713900345855865d677a02e1e174466
llama <llama@llama.fi>
2023-10-27 15:41:07 +0200

    code refactor

--- projects/vivaleva/index.js
+++ projects/vivaleva/index.js
@@ -1,59 +1,14 @@
+const { getConfig } = require('../helper/cache')
+const getAllPositionsByIdsABI = "function getAllPositionsByIds(address vaultAddress, uint256[] positionIds) view returns (tuple(address worker, uint256 positionId, uint256 positionDebtAmount, uint256 positionHealth, uint256[] positionIngredients, uint16 positionKillFactorBps)[] positionInfos)"
+
 module.exports = {
   timetravel: false,
   era: {
     async tvl(_, _1, _2, { api }) {
+      const _data = await getConfig('vivaleva', "https://sbb.sooho.io/api/v1/external/vivaleva/defiLlama")
+
       const data = {
-        vaults: [
-          {
-            symbol: "ibETH",
-            decimals: 18,
-            baseTokenAddress: "0x5aea5775959fbc2557cc8789bc1bf90a239d9a91",
-            address: "0x23FDd6487a17abB8360E8Da8b1B370C94ee94Cc2"
-          },
-          {
-            symbol: "ibUSDC",
-            decimals: 6,
-            baseTokenAddress: "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
-            address: "0x0755DA5D9e9A722A9e5cc4bb83742387ae2990a5"
-          }
-        ],
-        syncSwapWorkers: [
-          {
-            name: "BUSD-ETH SyncSwap Farm",
-            address: "0x8468AB9e88550f725439A7f128E2E31a5E6b753f",
-            farmingTokenAddress: "0x2039bb4116B4EFc145Ec4f0e2eA75012D6C0f181",
-            baseTokenAddress: "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
-            lpTokenAddress: "0xaD86486f1d225D624443e5DF4B2301d03bBe70f6"
-          },
-          {
-            name: "USDC-ETH SyncSwap Farm",
-            address: "0x3A613EFAe4a6A6447A9D784E398730811a57af6e",
-            farmingTokenAddress: "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
-            baseTokenAddress: "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
-            lpTokenAddress: "0x80115c708E12eDd42E504c1cD52Aea96C547c05c"
-          },
-          {
-            name: "USDT-ETH SyncSwap Farm",
-            address: "0x95C78e21Beb54314fe5A4571E7361f6c6A144B2f",
-            farmingTokenAddress: "0x493257fD37EDB34451f62EDf8D2a0C418852bA4C",
-            baseTokenAddress: "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
-            lpTokenAddress: "0xd3D91634Cf4C04aD1B76cE2c06F7385A897F54D3"
-          },
-          {
-            name: "ETH-USDC SyncSwap Farm",
-            address: "0x39356ed5dC2F7Ea897296e07E97b59Af9C8153Ec",
-            farmingTokenAddress: "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
-            baseTokenAddress: "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
-            lpTokenAddress: "0x80115c708E12eDd42E504c1cD52Aea96C547c05c"
-          },
-          {
-            name: "USDT-USDC SyncSwap Farm",
-            address: "0xfbF4BcD3266Af20B72dc484F6D7Dc13855885ba0",
-            farmingTokenAddress: "0x493257fD37EDB34451f62EDf8D2a0C418852bA4C",
-            baseTokenAddress: "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
-            lpTokenAddress: "0x0E595bfcAfb552F83E25d24e8a383F88c1Ab48A4"
-          }
-        ],
+        ..._data,
         pancakeSwapV3Worker: [
           {
             name: "USDC-WETH PancakeSwapV3 Farm Worker",
@@ -93,62 +48,7 @@ module.exports = {
       for (const [i, v] of vaults.entries()) {
         const positionLength = positionLengths[i];
         const positions = Array.from(Array(Number(positionLength)).keys());
-        const getAllPositionsByIdsABI = {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "vaultAddress",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256[]",
-              "name": "positionIds",
-              "type": "uint256[]"
-            }
-          ],
-          "name": "getAllPositionsByIds",
-          "outputs": [
-            {
-              "components": [
-                {
-                  "internalType": "address",
-                  "name": "worker",
-                  "type": "address"
-                },
-                {
-                  "internalType": "uint256",
-                  "name": "positionId",
-                  "type": "uint256"
-                },
-                {
-                  "internalType": "uint256",
-                  "name": "positionDebtAmount",
-                  "type": "uint256"
-                },
-                {
-                  "internalType": "uint256",
-                  "name": "positionHealth",
-                  "type": "uint256"
-                },
-                {
-                  "internalType": "uint256[]",
-                  "name": "positionIngredients",
-                  "type": "uint256[]"
-                },
-                {
-                  "internalType": "uint16",
-                  "name": "positionKillFactorBps",
-                  "type": "uint16"
-                }
-              ],
-              "internalType": "struct CommonCalculator.PositionInfo[]",
-              "name": "positionInfos",
-              "type": "tuple[]"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        };
+
         const positionIngredients = await api.call({ abi: getAllPositionsByIdsABI, params: [v.address, positions], target: commonCalculator });
         // => using multiCall
         for (const cur of positionIngredients) {
