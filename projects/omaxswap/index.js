adc6073683315e87e01ba98742e9d9fccba89be9
OMAXCHAIN <103971478+OMAXCHAIN@users.noreply.github.com>
2023-10-29 16:23:11 +0530

    Update index.js

--- projects/omaxswap/index.js
+++ projects/omaxswap/index.js
@@ -1,3 +1,3 @@
 const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('omax', '0x0e149Ff38Cd5B5c0F1004D08A14C9653485ad5fA')
+module.exports = uniTvlExport('omax', '0x90fC9D17FD4E44FD83FFEbBba1d703997cF791D8')
