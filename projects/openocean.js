1255597a80b23a883a6aa6f8e2036fb7746056b6
tony <cform4982@gmail.com>
2023-10-30 19:15:26 +0800

    add manta chain and fixed bug

--- projects/openocean.js
+++ projects/openocean.js
@@ -6,7 +6,7 @@ const config = {
   polygon: { factory: '0xd76d8C2A7CA0a1609Aea0b9b5017B3F7782891bf', },
   kava: { factory: '0x6dd434082EAB5Cd134B33719ec1FF05fE985B97b', },
   arbitrum: { factory: '0x01Ec93c289cB003e741f621cDD4FE837243f8905', },
-  manta: { factory: '0x2B5F704aB7061FB4DBfc5876B024F4BDB2f5E8b6', },
+  manta: { factory: '0x8D2B7e5501Eb6D92F8e349f2FEbe785DD070bE74', },
   avax: { factory: '0x042AF448582d0a3cE3CFa5b65c2675e88610B18d', s: ['0x4C431b568e8baAB20F004BB16E44570e8E0cD6D7', '0x0ebd9537a25f56713e34c45b38f421a1e7191469'], },
   ethereum: { factory: '0x1f8c25f8da3990ecd3632ee4f02c2ea37755c3c6', s: ['0xb99d38eb69214e493b1183ffa3d561fc9f75d519', '0x7778360f035c589fce2f4ea5786cbd8b36e5396b'], },
 }
