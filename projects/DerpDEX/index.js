b84983b1d4a3bd26f476f0ab194eea9221894604
geckocoding <133345507+geckocoding@users.noreply.github.com>
2023-10-30 21:01:24 +0800

    Update DerpDEX TVL calculations

--- projects/DerpDEX/index.js
+++ projects/DerpDEX/index.js
@@ -1,16 +1,29 @@
-const { uniV3Export } = require("../helper/uniswapV3");
+const { getUniTVL } = require('../helper/unknownTokens')
 
-module.exports = uniV3Export({
+module.exports = {
+  misrepresentedTokens: true,
   era: {
-    factory: "0x52a1865eb6903bc777a02ae93159105015ca1517",
-    fromBlock: 7790768,
+    tvl: getUniTVL({
+      factory: "0x52a1865eb6903bc777a02ae93159105015ca1517",
+      useDefaultCoreAssets: true, 
+      fetchBalances: true, 
+      permitFailure: true
+    })
   },
   base: {
-    factory: "0xeddef4273518b137cdbcb3a7fa1c6a688303dfe2",
-    fromBlock: 2753388
+    tvl: getUniTVL({
+      factory: "0xeddef4273518b137cdbcb3a7fa1c6a688303dfe2",
+      useDefaultCoreAssets: true, 
+      fetchBalances: true, 
+      permitFailure: true
+    })
   },
   op_bnb: {
-    factory: "0xb91331Ea9539ee881e3A45191076c454E482dAc7",
-    fromBlock: 3521514
+    tvl: getUniTVL({
+      factory: "0xb91331Ea9539ee881e3A45191076c454E482dAc7",
+      useDefaultCoreAssets: true, 
+      fetchBalances: true, 
+      permitFailure: true
+    })
   }
-});
+};
