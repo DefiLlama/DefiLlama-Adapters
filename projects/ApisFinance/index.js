0eac94ec0ec5e569d2e62c62463f8c003d8f17af
APISF <149051599+APISF@users.noreply.github.com>
2023-10-30 11:25:09 +0800

    Update index.js

--- projects/ApisFinance/index.js
+++ projects/ApisFinance/index.js
@@ -1,16 +1,13 @@
-
-
-const { sumTokensExport } = require("./helper/unwrapLPs");
-
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
 
 
 module.exports = {
 
   methodology: 'counts the number of MINT tokens in the Club Bonding contract.',
-  opbnb: {
+  op_bnb: {
     tvl:sumTokensExport({ 
-    	owner:'0xC72Be17AFD278F918752bcc3D73a36E1901023BC',
+    	owner:'0xa7caf3af138d2f26749a0e6be22c7647b14c7c87',
     	tokens:['0x4200000000000000000000000000000000000006','0x8e0c7C33637e0c6BcE51A34e961C04C35aAc2706'],
 
     }),
