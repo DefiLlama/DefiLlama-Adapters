ca25a9ae7d29c2fc458136bc1a4b17c0311ae1ad
APISF <149051599+APISF@users.noreply.github.com>
2023-10-30 12:26:16 +0800

    Update index.js

--- projects/ApisFinance/index.js
+++ projects/ApisFinance/index.js
@@ -5,7 +5,7 @@ APIS='0x8e0c7C33637e0c6BcE51A34e961C04C35aAc2706';
 
 module.exports = {
 
-  methodology: 'counts the number of MINT tokens in the Club Bonding contract.',
+  methodology: 'Summed up  APIS tokens Liquidity  in DEX and all the tokens deposited in their main lending contract.',
   op_bnb: {
     tvl:sumTokensExport({ 
     	owner:'0xa7caf3af138d2f26749a0e6be22c7647b14c7c87',
