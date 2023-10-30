14f285f7275ae44464f5322255dd69b04914bc2c
APISF <149051599+APISF@users.noreply.github.com>
2023-10-30 11:28:58 +0800

    Update index.js

--- projects/ApisFinance/index.js
+++ projects/ApisFinance/index.js
@@ -1,6 +1,7 @@
 const { sumTokensExport } = require("../helper/unwrapLPs");
 
-
+WBNB='0x4200000000000000000000000000000000000006';
+APIS='0x8e0c7C33637e0c6BcE51A34e961C04C35aAc2706';
 
 module.exports = {
 
@@ -8,7 +9,7 @@ module.exports = {
   op_bnb: {
     tvl:sumTokensExport({ 
     	owner:'0xa7caf3af138d2f26749a0e6be22c7647b14c7c87',
-    	tokens:['0x4200000000000000000000000000000000000006','0x8e0c7C33637e0c6BcE51A34e961C04C35aAc2706'],
+    	tokens:[WBNB, APIS],
 
     }),
   }
