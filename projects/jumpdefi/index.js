85f037a8c3920c1895aa3b5acd96349109933bf8
ymc182 <ymc182@gmail.com>
2023-10-31 22:27:42 +1100

    jumpdefi

--- projects/jumpdefi/index.js
+++ projects/jumpdefi/index.js
@@ -1,59 +1,9 @@
-const { call } = require("../helper/chain/near");
 const axios = require("axios");
+const sdk = require("@defillama/sdk");
+const { sumTokens } = require("../helper/unwrapLPs");
 const { BigNumber } = require("bignumber.js");
-const COLLECTION_IDS = [
-  "nft.classykangaroosv2.near",
-  "mp.nephilim.near",
-  "realbirds.near",
-  "nfc.enleap.near",
-  "asac.near",
-  "mrbrownproject.near",
-  "tinkerunion_nft.enleap.near",
-  "thebullishbulls.near",
-  "cartelgen1.neartopia.near",
-];
-const NFT_STAKING_CONTRACT = "nftstaking.jumpfinance.near";
-async function tvl() {
-  let balances = {
-    near: "0",
-  };
-  let promises = [];
+const { uniTvlExport } = require("../helper/unknownTokens");
 
-  COLLECTION_IDS.forEach((collectionId) => {
-    promises.push(getValueLockCollection(collectionId));
-  });
-  const values = await Promise.allSettled(promises);
-
-  values.forEach((value) => {
-    if (value.status === "fulfilled") {
-      balances.near = parseFloat(balances.near) + parseFloat(value.value);
-    }
-  });
-
-  return balances;
-}
-async function getValueLockCollection(collectionId) {
-  const fpFetchURL =
-    "https://api-v2-mainnet.paras.id/collections?collection_id=";
-  const fpFetch = await axios.get(fpFetchURL + collectionId);
-  const fpData = fpFetch.data;
-  const nearValuePerNFT = parseFloat(
-    new BigNumber(fpData.data.results[0].floor_price)
-      .dividedBy(10 ** 24)
-      .toFixed(2),
-  );
-  const nftStaked = await call(collectionId, "nft_supply_for_owner", {
-    account_id: NFT_STAKING_CONTRACT,
-  });
-  const totalValue = parseFloat(nftStaked) * parseFloat(nearValuePerNFT);
-  return totalValue;
-}
-
-module.exports = {
-  timetravel: false,
-  near: {
-    tvl,
-  },
-  methodology:
-    "Summed up all the tokens deposited in JumpDefi including NFT Staking",
-};
+const chain = "telos";
+const factory = "0xff59EBFf3e3F72E8162eA2aB0a0d1C9258692dF5";
+module.exports = uniTvlExport(chain, factory);
