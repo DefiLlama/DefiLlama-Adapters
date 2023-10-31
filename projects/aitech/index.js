717a319062a8f8efa26d21bd9b7a461106f88909
llama <llama@llama.fi>
2023-10-31 08:59:06 +0100

    code refactor

--- projects/aitech/index.js
+++ projects/aitech/index.js
@@ -1,26 +1,11 @@
-const sdk = require("@defillama/sdk");
+const { staking } = require('../helper/staking')
 
 const aitechStakingContract = '0x2C4dD7db5Ce6A9A2FB362F64fF189AF772C31184';
 const aitechTokenContract = '0x2D060Ef4d6BF7f9e5edDe373Ab735513c0e4F944';
 
-async function staking (timestamp, block, chainBlocks) {
-    let balances = {};
-
-    let {output: balance} =  await sdk.api.erc20.balanceOf({
-        target: aitechTokenContract,
-        owner: aitechStakingContract,
-        block: chainBlocks.bsc,
-        chain: "bsc"
-    });
-
-    sdk.util.sumSingleBalance(balances, `bsc:${aitechTokenContract}`, balance)
-
-    return balances;
-}
-
 module.exports = {
-    bsc: {
-        tvl: staking
-    },
-    
+  bsc: {
+    tvl: () => ({}),
+    staking: staking(aitechStakingContract, aitechTokenContract,)
+  },
 }
