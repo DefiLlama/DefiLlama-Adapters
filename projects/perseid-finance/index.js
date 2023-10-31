7e15fc7b18365cd63e8e14923b3f8f5364370e12
llama <llama@llama.fi>
2023-10-31 10:30:03 +0100

    code refactor

--- projects/perseid-finance/index.js
+++ projects/perseid-finance/index.js
@@ -1,65 +1,47 @@
-const sdk = require("@defillama/sdk");
-const abi = require("./abi.json");
-const { getLPData } = require("../helper/unknownTokens");
-const { unwrapLPsAuto } = require("../helper/unwrapLPs");
-const { getTokenPrices } = require("../helper/cache/sumUnknownTokens");
-const BigNumber = require("bignumber.js");
+const { sumUnknownTokens } = require('../helper/unknownTokens')
+const ADDRESSES = require('../helper/coreAssets.json')
 
-NATIVE_ADDRES = "0xfeC65bFB6e5BbCC9ab8aE98f62A8AaB2EA51c495";
-NATIVE_USDT_ADDRESS = "0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df";
-NATIVE_LP_ADDRESS = "0x47ED4E0a52716e91a4F37914b04B2252B5B5fcDF";
-FARM_ADDRES = "0xC692CA3066C84012C616989Bc7fD9659f16DDCFd";
+const NATIVE_ADDRES = "0xfec65bfb6e5bbcc9ab8ae98f62a8aab2ea51c495";
+const NATIVE_LP_ADDRESS = "0x47ED4E0a52716e91a4F37914b04B2252B5B5fcDF"; // iziswap LP
+const FARM_ADDRES = "0xC692CA3066C84012C616989Bc7fD9659f16DDCFd";
 
-async function calculateNativeToken(balances, token, tvl, api) {
-  let [usdtBalance, pedBalance] = await api.multiCall({
-    abi: "erc20:balanceOf",
-    calls: [
-      { target: NATIVE_USDT_ADDRESS, params: [NATIVE_LP_ADDRESS] },
-      { target: NATIVE_ADDRES, params: [NATIVE_LP_ADDRESS] },
-    ],
-  });
-  let ret = BigNumber(tvl).div(pedBalance).times(usdtBalance).toFixed(0);
-  sdk.util.sumSingleBalance(balances, token, ret);
-}
 
-async function tvl(_, _1, _2, { api }) {
-  const transform = (i) => `scroll:${i.toLowerCase()}`;
-  const balances = {
-    tvl: {},
-  };
-  let pools = await api.call({
-    abi: abi.getPoolTotalTvl,
-    target: FARM_ADDRES,
-  });
+const getTvl = isStaking => async (_, _1, _2, { api }) => {
+  let pools = await api.call({ abi: abi.getPoolTotalTvl, target: FARM_ADDRES, });
+  if (isStaking)
+    pools = pools.find(i => i[1].toLowerCase() === NATIVE_ADDRES)
+  else
+    pools = pools.filter(i => i[1].toLowerCase() !== NATIVE_ADDRES)
 
-  const poolTvls = pools.map((pool) => pool.tvl);
-  const tokens = pools.map((pool) => pool.assets);
-  tokens.forEach(async (token, index) => {
-    if (NATIVE_ADDRES === token) {
-      await calculateNativeToken(balances.tvl, transform(NATIVE_USDT_ADDRESS), poolTvls[index], api);
-    } else {
-      sdk.util.sumSingleBalance(balances.tvl, transform(token), poolTvls[index]);
-    }
-  });
-
-  await unwrapLPsAuto({ api, balances: balances.tvl, abis: { getReservesABI: abi.getReserves } });
-
-  const pairInfos = await getLPData({
-    chain: "scroll",
-    lps: tokens,
-    abis: { getReservesABI: abi.reserves },
-  });
-  const lps = Object.keys(pairInfos);
-  if (lps.length) {
-    const { updateBalances } = await getTokenPrices({ lps, ...api, abis: { getReservesABI: abi.getReserves } });
-    balances.tvl = await updateBalances(balances.tvl);
+  if (!isStaking) {
+    pools.forEach(([_, token, bal]) => api.add(token, bal))
+    return sumUnknownTokens({
+      api, useDefaultCoreAssets: true, resolveLP: true, abis: {
+        getReservesABI: !isStaking ? abi.getReserves : null,
+      }
+    })
   }
 
-  return balances.tvl;
+  if (!pools) return {}
+  const pedBal = pools[2]
+  const [usdtLPBal, pedLPBal] = await api.multiCall({  abi: 'erc20:balanceOf', calls: [
+    { target: ADDRESSES.scroll.USDT, params: NATIVE_LP_ADDRESS },
+    { target: NATIVE_ADDRES, params: NATIVE_LP_ADDRESS },
+  ]})
+
+  api.add(ADDRESSES.scroll.USDT, pedBal * usdtLPBal / pedLPBal)
+  return api.getBalances()
 }
 
 module.exports = {
+  misrepresentedTokens: true,
   scroll: {
-    tvl,
+    tvl: getTvl(false),
+    staking: getTvl(true),
   },
 };
+
+const abi = {
+  "getPoolTotalTvl": "function getPoolTotalTvl() view returns (tuple(uint256 pid, address assets, uint256 tvl)[])",
+  "getReserves": "function getReserves() external view returns (uint _reserve0, uint _reserve1)"
+}
