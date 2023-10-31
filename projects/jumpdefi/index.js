4006986f60011073125ee4e98f290957cfdbd6bc
llama <llama@llama.fi>
2023-10-31 18:16:51 +0100

    code refactor

--- projects/jumpdefi/index.js
+++ projects/jumpdefi/index.js
@@ -1,9 +1,3 @@
-const axios = require("axios");
-const sdk = require("@defillama/sdk");
-const { sumTokens } = require("../helper/unwrapLPs");
-const { BigNumber } = require("bignumber.js");
 const { uniTvlExport } = require("../helper/unknownTokens");
 
-const chain = "telos";
-const factory = "0xff59EBFf3e3F72E8162eA2aB0a0d1C9258692dF5";
-module.exports = uniTvlExport(chain, factory);
+module.exports = uniTvlExport("telos", "0xff59EBFf3e3F72E8162eA2aB0a0d1C9258692dF5", { fetchBalances: true });
