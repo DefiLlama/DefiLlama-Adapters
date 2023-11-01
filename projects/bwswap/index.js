a257e85db958f8843b4665aca69f1355639a9500
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-01 07:25:45 +0100

    minor fix

--- projects/bwswap/index.js
+++ projects/bwswap/index.js
@@ -1,8 +1,3 @@
-const { uniV3Export } = require('../helper/uniswapV3')
+const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports = {
-    methodology: 'TVL accounts for the liquidity on all AMM pools taken from the factory contract',
-    ...uniV3Export({
-        base: { factory: '0x67233C258BAeE28b2a7d42ec19fBD0b750a77Cd1', fromBlock: 2048745 },
-    })
-}
+module.exports = uniTvlExport('base', '0x67233C258BAeE28b2a7d42ec19fBD0b750a77Cd1', { fetchBalances: true })
