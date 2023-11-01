96f86527654dcd36ea6969b0da3d25c7814bcc48
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-01 07:38:28 +0100

    code refactor

--- projects/quantum-unit/index.js
+++ projects/quantum-unit/index.js
@@ -1,20 +1,10 @@
-const sdk = require("@defillama/sdk");
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const ADDRESSES = require('../helper/coreAssets.json')
 const QUANTUM_UNIT_CONTRACT = "0x81889aec6fdc400ec9786516c3adc14d59fc361e";
-const WBNB_CONTRACT = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
-async function tvl(timestamp, block, chainBlocks, { api }) {
-  const collateralBalance = await sdk.api.eth.getBalance({
-    target: QUANTUM_UNIT_CONTRACT,
-    block: chainBlocks.bsc,
-    chain: "bsc"
-  })
-  api.add(WBNB_CONTRACT, collateralBalance.output)
-}
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'count all contract balance',
   bsc: {
-    tvl,
+    tvl: sumTokensExport({ owner: QUANTUM_UNIT_CONTRACT, tokens: [ADDRESSES.null] }),
   }
 };
