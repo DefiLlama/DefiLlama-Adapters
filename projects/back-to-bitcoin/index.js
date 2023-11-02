a5ebeab222de8b39cc24e096dfd33639974cf7e3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-02 07:52:04 +0100

    minor refactor

--- projects/back-to-bitcoin/index.js
+++ projects/back-to-bitcoin/index.js
@@ -1,15 +1,11 @@
-const { sumTokens2 } = require('../helper/unwrapLPs')
+const { sumTokensExport } = require('../helper/unwrapLPs')
+const ADDRESSES = require('../helper/coreAssets.json')
 
 const contract = "0x1EE28d16C380B2137E63EBf92a9F5B42e63E9500"
-const BTCB = '0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c'
-
-async function tvl(time, ethBlock, _b, {api}) {
-  return sumTokens2({ tokens: [BTCB], owner: contract, api })
-}
 
 module.exports = {
   methodology: `We count the BTCB on ${contract}`,
   bsc: {
-    tvl
+    tvl: sumTokensExport({ owner: contract, tokens: [ADDRESSES.bsc.BTCB]}),
   }
 }
