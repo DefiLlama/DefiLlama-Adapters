d6b1f3e6f9298df0833e067877f7cfac22b183a8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-02 20:03:46 +0100

    minor fix

--- projects/baki/index.js
+++ projects/baki/index.js
@@ -1,19 +1,10 @@
-const bakiAddress = "0x8c1278D8b20ecEe71736F27181D3018E9a15652B";
-
+const bakiAddress = "0x98e76679064d50a45647c452a0b2c4cb1137f28c";
+const { sumTokensExport } = require('../helper/unwrapLPs')
 const ADDRESSES = require('../helper/coreAssets.json')
-const abi = require("./abi.json");
 
 module.exports = {
   methodology: "The baki protocol keeps track of the total collateral locked in the vault",
-  avax:{
-    tvl: async (_, _1, _2, { api }) => {
-      // const usdcBal = await api.call({  abi: abi.totalCollateral, target: bakiAddress })
-      // api.add(usdc, usdcBal)
-      // return api.getBalances()
-      const treasury = await api.call({  abi: abi.treasuryWallet, target: bakiAddress })
-      return api.sumTokens({ owner: treasury, tokens: [ADDRESSES.avax.USDC]})
-      
-    }
+  avax: {
+    tvl: sumTokensExport({ owner: bakiAddress, tokens: [ADDRESSES.avax.USDC] })
   },
- 
 };
