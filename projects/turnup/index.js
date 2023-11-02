eb00fb73e0a544f17c2077bf79bb7947a959b1b0
Define101 <Lovelovenow@protonmail.com>
2023-11-02 19:35:08 +0000

    typo

--- projects/turnup/index.js
+++ projects/turnup/index.js
@@ -7,7 +7,7 @@ async function tvl(time, ethBlock, _b, {api}) {
 }
 
 module.exports = {
-  methodology: `We count the ETH on ${contract}`,
+  methodology: `We count the BNB on ${contract}`,
   bsc: {
     tvl
   }
