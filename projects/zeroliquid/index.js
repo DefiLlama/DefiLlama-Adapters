b222de41148f2d0f2dfa791db7a8c909494d696b
Barroso <bluebirdyatchclub@gmail.com>
2023-11-02 15:12:56 +0500

    new LSDs added

--- projects/zeroliquid/index.js
+++ projects/zeroliquid/index.js
@@ -3,6 +3,9 @@ const { staking } = require('../helper/staking')
 const wstETH ="0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
 const rETH= "0xae78736cd615f374d3085123a210448e74fc6393"
 const sfrxETH= "0xac3E018457B222d93114458476f3E3416Abbe38F"
+const swETH= "0xf951e335afb289353dc249e82926178eac7ded78"
+const unshETH= "0x0ae38f7e10a43b5b2fb064b42a2f4514cba909ef"
+
 
 const endpoint = 'https://api.thegraph.com/subgraphs/name/zackzeroliquid/zeroliquid'
 
@@ -11,6 +14,8 @@ const query = `query {
     amountRETH
     amountSfrxETH
     amountWstETH
+    amountSwETH
+    amountUnshETH
   }
 }`
 
@@ -35,13 +40,13 @@ async function tvl(_, _1, _2, { api }) {
   api.add(wstETH, data.amountWstETH)
   api.add(rETH, data.amountRETH)
   api.add(sfrxETH, data.amountSfrxETH)
+  api.add(swETH, data.amountSwETH)
+  api.add(unshETH, data.amountUnshETH)
 
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  methodology: 'Getting TVL data from the GraphQL',
+  methodology: 'Getting TVL data from The Graph',
   ethereum: {
     tvl,
   }
