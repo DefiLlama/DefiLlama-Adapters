97298f3842632e29198bccf12739c987a4e6f23b
Aleksei Lushnikov <al.lushnikov@yandex.ru>
2023-11-02 14:53:02 +0000

    symbiosis adds Manta chain support (#7933)

--- projects/symbiosis-finance/config.js
+++ projects/symbiosis-finance/config.js
@@ -223,5 +223,14 @@ module.exports = {
          '0x5Aa5f7f84eD0E5db0a4a85C3947eA16B53352FD4', // portal v2
        ]
      },
+     {
+       name: 'manta',
+       tokens: [
+         ADDRESSES.manta.WETH,
+       ],
+       holders: [
+         '0x5Aa5f7f84eD0E5db0a4a85C3947eA16B53352FD4', // portal v2
+       ]
+     },
   ]
 }
