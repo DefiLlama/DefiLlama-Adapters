11c6a97a3705e2444863d79a20332c31db8753a8
Steve0day <143409435+Steve0day@users.noreply.github.com>
2023-11-05 23:28:10 +0800

    Update index.js, add opBNB chain

--- projects/friend3/index.js
+++ projects/friend3/index.js
@@ -1,14 +1,26 @@
 const { sumTokens2, nullAddress } = require("../helper/unwrapLPs");
-
-const contract = "0x1e70972EC6c8a3FAe3aC34C9F3818eC46Eb3BD5D";
-
-async function tvl(time, ethBlock, _b, { api }) {
-  return sumTokens2({ tokens: [nullAddress], owner: contract, api });
+​
+const contractBsc = "0x1e70972EC6c8a3FAe3aC34C9F3818eC46Eb3BD5D";
+const contractOpbnb = "0x2C5bF6f0953ffcDE678A35AB7d6CaEBC8B6b29F0";
+​
+async function tvl(time, ethBlock, _b, { api, chain }) {
+  if (chain === "bsc") {
+    return sumTokens2({ tokens: [nullAddress], owner: contractBsc, api });
+  } else if (chain === "op_bnb") {
+    return sumTokens2({
+      tokens: [nullAddress],
+      owner: contractOpbnb,
+      api,
+    });
+  }
 }
-
+​
 module.exports = {
-  methodology: `We count the ETH on ${contract}`,
+  methodology: `We count the BNB on ${contractBsc} ${contractOpbnb}`,
   bsc: {
+    tvl,
+  },
+  op_bnb: {
     tvl,
   },
 };
