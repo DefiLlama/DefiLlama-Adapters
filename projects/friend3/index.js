a6669afda47bbe1caa2b370d92124ef672fdcf1f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-06 18:07:14 +0100

    minor fix

--- projects/friend3/index.js
+++ projects/friend3/index.js
@@ -1,26 +1,12 @@
-const { sumTokens2, nullAddress } = require("../helper/unwrapLPs");
-​
-const contractBsc = "0x1e70972EC6c8a3FAe3aC34C9F3818eC46Eb3BD5D";
-const contractOpbnb = "0x2C5bF6f0953ffcDE678A35AB7d6CaEBC8B6b29F0";
-​
-async function tvl(time, ethBlock, _b, { api, chain }) {
-  if (chain === "bsc") {
-    return sumTokens2({ tokens: [nullAddress], owner: contractBsc, api });
-  } else if (chain === "op_bnb") {
-    return sumTokens2({
-      tokens: [nullAddress],
-      owner: contractOpbnb,
-      api,
-    });
-  }
+const { nullAddress, sumTokensExport } = require("../helper/unwrapLPs");
+
+const config = {
+  bsc: '0x1e70972EC6c8a3FAe3aC34C9F3818eC46Eb3BD5D',
+  op_bnb: '0x2C5bF6f0953ffcDE678A35AB7d6CaEBC8B6b29F0',
 }
-​
-module.exports = {
-  methodology: `We count the BNB on ${contractBsc} ${contractOpbnb}`,
-  bsc: {
-    tvl,
-  },
-  op_bnb: {
-    tvl,
-  },
-};
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = {
+    tvl: sumTokensExport({ tokens: [nullAddress], owner: config[chain] })
+  }
+})
