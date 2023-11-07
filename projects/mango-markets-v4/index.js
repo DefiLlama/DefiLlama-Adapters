90ffb1086b0c988dde916d16bd9ea4694df486cf
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-07 10:02:22 +0100

    minor fix

--- projects/mango-markets-v4/index.js
+++ projects/mango-markets-v4/index.js
@@ -1,18 +1,8 @@
 const { ordersTvl } = require("./orders");
 const { depositsTvl, borrowed } = require("./deposits");
 const sdk = require("@defillama/sdk");
-const idl = require("./idl.json");
-
-async function tvl() {
-  const orders = await ordersTvl();
-  const deposits = await depositsTvl();
-  const balances = {};
-  sdk.util.mergeBalances(balances, orders);
-  sdk.util.mergeBalances(balances, deposits);
-  return balances;
-}
 
 module.exports = {
   timetravel: false,
-  solana: { tvl, borrowed },
+  solana: { tvl: sdk.util.sumChainTvls([ordersTvl, depositsTvl ]), borrowed },
 };
