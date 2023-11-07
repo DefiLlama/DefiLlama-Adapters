e7f4035cc974d153437f31612946c2e8c54c9dcf
psychoter <the.psycho.writer@gmail.com>
2023-11-07 10:19:44 +0330

    Add DavinciGraph TVL Adaptor

---
+++ projects/davincigraph/index.js
@@ -0,0 +1,14 @@
+const { fetchURL } = require("../helper/utils");
+
+module.exports = {
+  timetravel: false,
+  methodology:
+    "The displayed value represents the Total Value Locked (TVL) in USD of all assets secured within the davincigraph token locker smart contracts, accessible at https://davincigraph.io/devs/locks/contracts",
+  fetch: async () => {
+    const response = await fetchURL(
+      "https://davincigraph.network/api/v1/locks/statistics"
+    );
+
+    return response.data.tvl.locked;
+  },
+};
