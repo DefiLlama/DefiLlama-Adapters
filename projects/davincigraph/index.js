f4b200f6b0f9cdd7242d132685ffda7108d9a719
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-08 11:13:23 +0100

    lint fix

--- projects/davincigraph/index.js
+++ projects/davincigraph/index.js
@@ -1,22 +1,17 @@
 const { fetchURL } = require("../helper/utils")
 
-async function hedera() {
-  const response = await fetchURL(
-    "https://davincigraph.network/api/v1/locks/statistics"
-  );
-
-  return response.data.tvl.locked.liquidity;
-};
 async function fetch() {
-  return await hedera()
-};
+  const response = await fetchURL("https://davincigraph.network/api/v1/locks/statistics")
+
+  return response.data.tvl.locked.liquidity
+}
 
 module.exports = {
   timetravel: false,
   methodology:
     "The displayed value represents the Total Value Locked (TVL) in USD of all assets secured within the davincigraph token locker smart contracts, accessible at https://davincigraph.io/devs/locks/contracts",
   hedera: {
-    fetch: hedera
-    },
     fetch
+  },
+  fetch
 };
