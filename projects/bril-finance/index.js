aa31146af39789a2ff40f0eab2a4f1dfb28e27ef
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-09 14:19:43 +0100

    code refactor

--- projects/bril-finance/index.js
+++ projects/bril-finance/index.js
@@ -1,44 +1,39 @@
-const strategy_abi = require("./strategy-abi.json");
+const { getLogs } = require('../helper/cache/getLogs')
 
-// Bril stratagies for PancakeSwap Position Manager
-const strategies = [
-  '0xb3842f8038f2f7c1a83B6529308da8fdc0E53019',
-  '0xafdC0eA6d2995B37B055712ce9875Ae9ED4fec27',
-  '0xB7fc5C8D8318ABAd47D2F4d74895ae52f5765e7f',
-  '0xBcB0DAf89999C135E6B0570c9B99ddf8De0b77c0',
-  '0xccD9Af42f84B05aBE3DB79D0Ea33BF67643b1926',
-  '0xde60E440775B5Dd427c4f2BF1f2FBd9A14130A14',
-  '0x06d473e85110548e73caAfbED28d616EF8fa1011',
-  '0x864D323eE8FB2a491d425e12Aa49288c2D33Dd15',
-] 
-
-async function tvl(_, _1, _2, { api }) {
-  const balances = await api.multiCall({
-    abi: strategy_abi.vaultAmounts,
-    calls: strategies,
-    params: [],
-  });
-
-  const summaries = await api.multiCall({
-    abi: strategy_abi.vaultSummary,
-    calls: strategies,
-    params: [],
-  });
-
-  for (let i=0; i <balances.length; i++){
-    api.add(summaries[i].baseToken_, balances[i].baseTotal_);
-    api.add(summaries[i].scarceToken_, balances[i].scarceTotal_);
-  }
-
+const config = {
+  bsc: { factory: '0xfaa2e7c69F7F34195D3Ef6CF98B6B009A6A07F30', fromBlock: 29703589, }
 }
-  
+
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'Count tokens managed by Bril automated liquidity management stratagies',
   start: 30131926,
-  bsc: {
-    tvl,
+};
+
+Object.keys(config).forEach(chain => {
+  const { factory, fromBlock, } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const logs = await getLogs({
+        api,
+        target: factory,
+        eventAbi: 'event StrategyInstanceDeployed (uint256 indexed typeId, address indexed strategyInstance, address indexed owner, address vault, bytes initData)',
+        onlyArgs: true,
+        fromBlock,
+      })
+      const strategies = logs.map(log => log.strategyInstance).filter(i => i !== '0x06Fc93C05614e711b4842B83E3e83C6da5c8547e')
+      const balances = await api.multiCall({ abi: abi.vaultAmounts, calls: strategies, });
+
+      const summaries = await api.multiCall({ abi: abi.vaultSummary, calls: strategies, });
+
+      for (let i = 0; i < balances.length; i++) {
+        api.add(summaries[i].baseToken_, balances[i].baseTotal_);
+        api.add(summaries[i].scarceToken_, balances[i].scarceTotal_);
+      }
+    }
   }
-}; 
-// node test.js projects/bril-finance/index.js
+})
+
+const abi = {
+  "vaultAmounts": "function vaultAmounts() view returns (uint256 baseTotal_, uint256 scarceTotal_ )",
+  "vaultSummary": "function vaultSummary() view returns (address vault_, address baseToken_, address scarceToken_, bool inverted_, int24 tickSpacing_)"
+}
