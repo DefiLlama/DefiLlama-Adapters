5d7849777d1653cb9922d9cbaaf4df1d7606fb40
shutsuwei <shu.tsu.wei88@gmail.com>
2023-11-09 17:43:25 +0700

    chore: add GM tokens to HMX's tvl

--- projects/perp88/index.js
+++ projects/perp88/index.js
@@ -28,6 +28,8 @@ module.exports = {
         ADDRESSES.arbitrum.WBTC,
         ADDRESSES.arbitrum.USDT,
         ADDRESSES.arbitrum.fsGLP,
+        ADDRESSES.arbitrum.GM_BTCUSDC,
+        ADDRESSES.arbitrum.GM_ETHUSDC,
       ]
     })
   }
