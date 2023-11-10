5360db787f612a9987ca4c0693bfe8ae0f87831e
Holly-S33 <56821487+Holly-S33@users.noreply.github.com>
2023-11-10 12:35:53 +0800

    Update T2T2/index.js remove the wrong address

--- projects/T2T2/index.js
+++ projects/T2T2/index.js
@@ -1,8 +1,7 @@
 const { nullAddress, sumTokensExport } = require("../helper/unwrapLPs");
 
 const config = {
-  ethereum: '0x390e61F798267fe7aa9BBE61Be8BB1776250D44C',
-  base: '0xE173A25C522385BB117b3044C79F534cD0a895EC',
+  base: '0xE173A25C522385BB117b3044C79F534cD0a895EC'
 }
 
 Object.keys(config).forEach(chain => {
