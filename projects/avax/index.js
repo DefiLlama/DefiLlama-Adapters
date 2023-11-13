6fa6b4be9dba359ed588d2d2e63c7509f9076efc
waynebruce0x <waynebruce0x@gmail.com>
2023-11-13 18:27:53 +0000

    more contracts

--- projects/avax/index.js
+++ projects/avax/index.js
@@ -1,3 +1,4 @@
+const { call } = require("@defillama/sdk/build/abi/abi2");
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
 module.exports = {
@@ -8,5 +9,17 @@ module.exports = {
         owner: "0x8EB8a3b98659Cce290402893d0123abb75E3ab28",
         fetchCoValentTokens: true,
       }),
+  },
+  bitcoin: {
+    tvl: async (_, _b, chainBlocks, { logArray }) => ({
+      bitcoin:
+        (await call({
+          chain: "avax",
+          abi: "erc20:totalSupply",
+          target: "0x152b9d0FdC40C096757F570A51E494bd4b943E50",
+          block: chainBlocks.avax,
+        })) /
+        10 ** 8,
+    }),
   },
 };
