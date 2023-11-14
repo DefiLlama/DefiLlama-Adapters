50f28a022e4b8134887b80a92dd39fe18187c298
Holly-S33 <56821487+Holly-S33@users.noreply.github.com>
2023-11-14 20:34:27 +0800

    T2T2 update adapter (#8062)
    * T2T2 project SDK adapter
    
    * Update T2T2/index.js remove the wrong address
    
    * Update T2T2 adapter. add ethereum contracr address
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/T2T2/index.js
+++ projects/T2T2/index.js
@@ -1,6 +1,7 @@
 const { nullAddress, sumTokensExport } = require("../helper/unwrapLPs");
 
 const config = {
+  ethereum: '0xA875755b23a38E06B669219AFf9bb79845F43EFd',
   base: '0xE173A25C522385BB117b3044C79F534cD0a895EC'
 }
 
