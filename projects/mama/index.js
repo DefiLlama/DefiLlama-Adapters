fa885382e17426cb97086d260cb90c70f4ff642b
ivan lysakov <ivl@aryze.io>
2023-11-14 15:01:25 +0200

    add mama project

---
+++ projects/mama/index.js
@@ -0,0 +1,8 @@
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+  polygon: {
+    factory: "0xE7aE959bbC94BDF2E9FF28a214840aB3285d7765",
+    fromBlock: 46977039,
+  }
+});
