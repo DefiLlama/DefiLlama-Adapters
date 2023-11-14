41ea42cc270b265faf9b4bfd4b9b3e914911ea69
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-14 14:19:07 +0100

    cache graph response

--- projects/frankencoin/index.js
+++ projects/frankencoin/index.js
@@ -1,48 +1,19 @@
-const { graphQuery } = require("../helper/http");
-const { sumTokens2 } = require("../helper/unwrapLPs");
+const { sumTokens2 } = require('../helper/unwrapLPs')
+const { cachedGraphQuery } = require('../helper/cache')
 
 const MintingHub = "0x7bbe8F18040aF0032f4C2435E7a76db6F1E346DF";
 const Collaterals = ["0xb4272071ecadd69d933adcd19ca99fe80664fc08"]; // XCHF
 
-const fetchPositions = async () => {
-  const url =
-    "https://api.thegraph.com/subgraphs/name/frankencoin-zchf/frankencoin-subgraph";
-  const query = `
-    query {
-      positions {
-        id
-        position
-        collateral
-      }
-    }`;
-  const { positions } = await graphQuery(url, query);
-
-  const pos = [];
-  const collaterals = [];
-  positions.forEach((position) => {
-    pos.push(position.position);
-    collaterals.push(position.collateral);
-  });
-
-  return {
-    positions: pos,
-    collaterals,
-  };
-};
-
-const getTvl = async (_, ethBlock, __, { api }) => {
-  const { positions, collaterals } = await fetchPositions();
-
-  return sumTokens2({
-    owners: [...positions, MintingHub],
-    tokens: [...collaterals, ...Collaterals],
-    api,
-  });
-};
+async function tvl(_, _b, _cb, { api, }) {
+  const tokensAndOwners = Collaterals.map(i => [i, MintingHub])
+  const { positions } = await cachedGraphQuery('frankencoin', 'https://api.thegraph.com/subgraphs/name/frankencoin-zchf/frankencoin-subgraph', '{ positions { position collateral } }')
+  positions.forEach(i => tokensAndOwners.push([i.collateral, i.position]))
+  return sumTokens2({ api, tokensAndOwners, })
+}
 
 module.exports = {
   ethereum: {
-    tvl: getTvl,
+    tvl,
   },
   start: 1698487043,
 };
