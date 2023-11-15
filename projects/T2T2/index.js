5df5ffe577b9cc6693bb1ae83c573b66ccf76a72
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-15 16:11:32 +0100

    blacklist own token

--- projects/T2T2/index.js
+++ projects/T2T2/index.js
@@ -5,9 +5,11 @@ const config = {
   base: '0xE173A25C522385BB117b3044C79F534cD0a895EC'
 }
 
+const T2_T2 = '0x390e61f798267fe7aa9bbe61be8bb1776250d44c'
+
 module.exports = {
   ethereum: {
-    tvl: sumTokensExport({ owner: config.ethereum, fetchCoValentTokens: true })
+    tvl: sumTokensExport({ owner: config.ethereum, fetchCoValentTokens: true, blacklistedTokens: [T2_T2] })
   },
   base: {
     tvl: sumTokensExport({ owner: config.base, tokens: [nullAddress] })
