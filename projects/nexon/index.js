7ecaefcb9f18ed6e9a11897a69dc0822b1b8fd41
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-15 15:01:43 +0100

    nexon: track telos tvl

--- projects/nexon/index.js
+++ projects/nexon/index.js
@@ -1,4 +1,4 @@
-const { compoundExports } = require('../helper/compound');
+const { compoundExports, compoundExports2 } = require('../helper/compound');
 const { nullAddress } = require('../helper/tokenMapping');
 
 const comptroller = "0x0171cA5b372eb510245F5FA214F5582911934b3D"
@@ -18,5 +18,6 @@ module.exports = {
   era: {
     tvl: lendingMarket.tvl,
     borrowed: ()=>({})
-  }
+  },
+  telos: compoundExports2({ comptroller: '0x5D2f22856DfEB4b5d6A4422FE0182B70D49cAFCa'})
 }; 
