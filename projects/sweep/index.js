c093687cac9512b492c19a173eb1934ab5b990af
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-15 22:30:34 +0100

    sweep: minor fix

--- projects/sweep/index.js
+++ projects/sweep/index.js
@@ -1,43 +1,28 @@
-const sdk = require('@defillama/sdk')
 const ADDRESSES = require('../helper/coreAssets.json');
 const SWEEP_ADDRESS = '0xB88a5Ac00917a02d82c7cd6CEBd73E2852d43574';
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
 const config = {
-  ethereum: {
-    fromBlock: 18017036,
-  },
-  arbitrum: {
-    fromBlock: 124179091,
-  },
-  optimism: {
-    fromBlock: 110684392,
-  }
+  ethereum: {},
+  arbitrum: {},
+  optimism: {},
 };
 
 async function tvl(_a, _b, _c, { api }) {
-  const balances = {}
   const USDC_ADDRESS = ADDRESSES[api.chain].USDC;
-  const minters = await api.multiCall({ abi: 'address[]:getMinters', calls: [SWEEP_ADDRESS] });
-
-  const balanceOfCalls = minters[0].map((minter) => {
-    return api.multiCall({
-      abi: 'function balanceOf(address) external view returns(uint256)',
-      calls: [{ target: USDC_ADDRESS, params: minter }]
-    });
-  });
-
-  const assetValueCalls = minters[0].map((minter) => {
-    return api.multiCall({ abi: 'uint256:assetValue', calls: [minter] });
-  });
-
-  const valueCalls = await Promise.all([...balanceOfCalls, ...assetValueCalls]);
-  const totalBalance = valueCalls.reduce((sum, value) => sum + parseInt(value[0]), 0);
-
-  sdk.util.sumSingleBalance(balances, `${api.chain}:${USDC_ADDRESS}`, totalBalance);
-
-  return balances;
+  const [minters] = await api.multiCall({ abi: 'address[]:getMinters', calls: [SWEEP_ADDRESS] })
+  // await sumTokens2({ api, owners: minters, tokens: [USDC_ADDRESS]})
+  const names = await api.multiCall({ abi: 'string:name', calls: minters })
+  const uniswapMinters = minters.filter((_, i) => /uniswap/i.test(names[i]))
+  const otherMinters = minters.filter((_, i) => !/uniswap/i.test(names[i]))
+  await sumTokens2({ api, owners: uniswapMinters, resolveUniV3: true, blacklistedTokens: [SWEEP_ADDRESS], tokens: [USDC_ADDRESS], })
+  const bals = await api.multiCall({ abi: 'uint256:assetValue', calls: otherMinters })
+  bals.forEach(bal => api.add(USDC_ADDRESS, bal))
+  return api.getBalances()
 }
 
 Object.keys(config).forEach((chain) => {
   module.exports[chain] = { tvl };
 });
+
+module.exports.misrepresentedTokens = true
