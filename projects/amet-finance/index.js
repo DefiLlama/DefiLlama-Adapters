08de49d6f1e09926950bee49a5386b46928e35c4
TheUnconstrainedMind <141995559+amet-finance-core@users.noreply.github.com>
2023-11-16 10:24:20 +0400

    Update index.js

--- projects/amet-finance/index.js
+++ projects/amet-finance/index.js
@@ -1,4 +1,5 @@
 const { getLogs } = require("../helper/cache/getLogs");
+const BN = require("bn.js");
 
 const config = {
   manta: {
@@ -12,21 +13,24 @@ async function tvl(_, _1, _2, { api }) {
   const { issuer, fromBlock, issuerEventABI } = config[api.chain];
 
   const logs = await getLogs({ api, target: issuer, fromBlock, eventAbi: issuerEventABI, onlyArgs: true });
-  const calls = logs.map(item => item.contractAddress)
-  const res = await api.multiCall({  abi: ZCB_Issuer_V1.getInfo, calls})
-  const ownerTokens = res.map((v, i) => [[v.investmentToken, v.interestToken], calls[i]])
-  return api.sumTokens({ ownerTokens })
+  const calls = logs.map(item => item.contractAddress);
+  const res = await api.multiCall({ abi: ZCB_Issuer_V1.getInfo, calls });
+  const ownerTokens = res.map((v, i) => {
+    api.add(v.interestToken, new BN(v.purchased).mul(new BN(v.investmentTokenAmount)));
+    return [[v.investmentToken], calls[i]];
+  });
+  return api.sumTokens({ ownerTokens });
 }
 
 module.exports = {
   start: 1700036718369,
-  methodology: "get the logs of the issuer contract to understand what bonds were issued and summing what is locked in the contract and adding the purchased amount",
-}
+  methodology: "get the logs of the issuer contract to understand what bonds were issued and summing what is locked in the contract and adding the purchased amount"
+};
 
 Object.keys(config).forEach(chain => {
-  module.exports[chain] = { tvl }
-})
+  module.exports[chain] = { tvl };
+});
 
 const ZCB_Issuer_V1 = {
-  getInfo: "function getInfo() view returns (address, uint256, uint256 purchased, uint256, uint256, address investmentToken, uint256 investmentTokenAmount, address interestToken, uint256, uint16, uint256)",
-}
+  getInfo: "function getInfo() view returns (address, uint256, uint256 purchased, uint256, uint256, address investmentToken, uint256 investmentTokenAmount, address interestToken, uint256, uint16, uint256)"
+};
