ba0f2a4c59ce50cf0612ddb7b38efd6ff2f65a98
elykct <elykct@nbc>
2023-11-17 16:47:46 +0700

    fix Error: Protocol doesn't have total tvl

--- projects/newbitcoin/index.js
+++ projects/newbitcoin/index.js
@@ -2,10 +2,6 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport } = require('../helper/unwrapLPs')
 const { uniV3Export } = require('../helper/uniswapV3')
 
-module.exports = {
-  nos: { tvl: BigInt(alphaKeys.tvl) + BigInt(alphaCoins) },
-}
-
 let alphaCoins = uniV3Export({
   nos: { factory: '0x1d12AC81710da54A50e2e9095E20dB2D915Ce3C8', fromBlock: 320282, },
 })
@@ -14,3 +10,6 @@ let alphaKeys = {
   tvl: sumTokensExport({ owner: '0xea21fbBB923E553d7b98D14106A104665BA57eCd', tokens: [ADDRESSES.nos.BTC] })
 }
 
+module.exports = {
+  nos: { tvl: BigInt(alphaKeys.tvl) + BigInt(alphaCoins) },
+}
