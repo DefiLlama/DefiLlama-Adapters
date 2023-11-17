64fa6daf1e8cc274f4a1a62173a64211328807bb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-17 15:06:05 +0100

    merge listings

--- projects/newbitcoin/index.js
+++ projects/newbitcoin/index.js
@@ -1,8 +1,20 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport } = require('../helper/unwrapLPs')
+const sdk = require('@defillama/sdk')
+
+const { uniV3Export } = require('../helper/uniswapV3')
+
+uniExports = uniV3Export({
+  nos: {
+    factory: '0x1d12AC81710da54A50e2e9095E20dB2D915Ce3C8', fromBlock: 320282, sumChunkSize: 50, filterFn: async (api, logs) => {
+      const tokens = logs.map(i => [i.token0, i.token1]).flat()
+      return tokens.filter(t => t.toLowerCase() !== ADDRESSES.nos.BTC.toLowerCase())
+    }
+  },
+})
 
 module.exports = {
   nos: {
-    tvl: sumTokensExport({ owner: '0xea21fbBB923E553d7b98D14106A104665BA57eCd', tokens: [ADDRESSES.nos.BTC] })
+    tvl: sdk.util.sumChainTvls([sumTokensExport({ owner: '0xea21fbBB923E553d7b98D14106A104665BA57eCd', tokens: [ADDRESSES.nos.BTC] }), uniExports.nos.tvl])
   }
 }
--- projects/newbitcoincity-dex/index.js
+++
@@ -1,5 +0,0 @@
-const { uniV3Export } = require('../helper/uniswapV3')
-
-module.exports = uniV3Export({
-  nos: { factory: '0x1d12AC81710da54A50e2e9095E20dB2D915Ce3C8', fromBlock: 320282, sumChunkSize: 50 },
-})
