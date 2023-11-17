63ad1094b886da4de151d2ef7b439916d15e63df
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-17 11:56:02 +0100

    sumer: minor fix

--- projects/sumer/index.js
+++ projects/sumer/index.js
@@ -1,5 +1,5 @@
-const { compoundExports } = require('../helper/compound')
+const { compoundExports2 } = require('../helper/compound')
 
 module.exports = {
-  meter: compoundExports("0xcB4cdDA50C1B6B0E33F544c98420722093B7Aa88", "meter"),
+  meter: compoundExports2({ comptroller: '0xcB4cdDA50C1B6B0E33F544c98420722093B7Aa88' }),
 }
