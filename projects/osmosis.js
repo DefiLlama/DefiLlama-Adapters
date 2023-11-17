fd02cfed8248773c79983ef920683da1fffcb152
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-17 15:41:38 +0100

    osmosis: include CL tvl

--- projects/osmosis.js
+++ projects/osmosis.js
@@ -1,60 +1,62 @@
-const { queryV1Beta1 } = require('./helper/chain/cosmos')
+const { queryV1Beta1, sumTokens } = require('./helper/chain/cosmos')
 const ADDRESSES = require('./helper/coreAssets.json')
 
 const coreAssets = new Set()
 Object.values(ADDRESSES.osmosis).forEach(i => coreAssets.add(i))
 Object.values(ADDRESSES.ibc).forEach(i => coreAssets.add('ibc/' + i))
 
-const sdk = require('@defillama/sdk')
-
 const chain = 'osmosis'
 
 async function tvl(_, _b, _cb, { api, }) {
   // // https://api-osmosis.imperator.co/pools/v2/all?low_liquidity=true
   // https://lcd.osmosis.zone/osmosis/gamm/v1beta1/pools?pagination.limit=100&pagination.count_total=true
-  let offset = 0
-  let hasMore = true
-  const data = []
-  let allPools = []
+  const owners = []
   const blacklistedTokens = new Set()
 
-  do {
-    const pools = await queryV1Beta1({ chain, url: '/gamm/v1beta1/pools?pagination.offset=' + offset })
-    allPools.push(...pools.pools)
-    sdk.log(data.length, pools.pools.length, offset)
-    offset += pools.pools.length
-    hasMore = pools.pools.length === 100
-  } while (hasMore)
+  const { pools } = await queryV1Beta1({ chain, url: '/poolmanager/v1beta1/all-pools' })
 
-  allPools.forEach((i) => {
+  pools.forEach((i) => {
     const poolId = i?.total_shares?.denom
     if (poolId) blacklistedTokens.add(poolId)
     if (i.address) blacklistedTokens.add(i.address)
+    if (i.contract_address) blacklistedTokens.add(i.contract_address)
   })
 
-  allPools.forEach((i) => {
-    const isStableSwap = i['@type'] === '/osmosis.gamm.poolmodels.stableswap.v1beta1.Pool'
-    if (isStableSwap) {
-      i.pool_liquidity.forEach(i => !blacklistedTokens.has(i.denom) && api.add(i.denom, i.amount))
-      return;
-    } else {
-      const coreData = i.pool_assets.find(i => coreAssets.has(i.token.denom))
-
-      i.pool_assets.forEach(i => {
-        if (blacklistedTokens.has(i.token.denom)) return;
-        if (!coreAssets.has(i.token.denom) && coreData) {
-          api.add(coreData.token.denom, coreData.token.amount)
-          return;
-        } else if (!coreAssets.has(i.token.denom)) {
-          api.add(i.token.denom, i.token.amount)
-          return;
-        }
-        api.add(i.token.denom, i.token.amount)
-      })
+  pools.forEach((i) => {
+    switch (i['@type']) {
+      case '/osmosis.gamm.poolmodels.stableswap.v1beta1.Pool':
+        i.pool_liquidity.forEach(i => !blacklistedTokens.has(i.denom) && api.add(i.denom, i.amount))
+        break;
+      case '/osmosis.gamm.v1beta1.Pool': addDefaultPoolData(i); break;
+      case '/osmosis.concentratedliquidity.v1beta1.Pool': addCLPool(i); break;
+      case '/osmosis.cosmwasmpool.v1beta1.CosmWasmPool': break;
+      default: throw new Error('Unknown pool type' + i['@type'])
     }
   })
-  return;
+  return sumTokens({ balances: api.getBalances(), chain, owners, blacklistedTokens: [...blacklistedTokens], })
+
+  function addCLPool(i) {
+    owners.push(i.address)
+  }
+
+  function addDefaultPoolData(i) {
+    const coreData = i.pool_assets.find(i => coreAssets.has(i.token.denom))
+
+    i.pool_assets.forEach(i => {
+      if (blacklistedTokens.has(i.token.denom)) return;
+      if (!coreAssets.has(i.token.denom) && coreData) {
+        api.add(coreData.token.denom, coreData.token.amount)
+        return;
+      } else if (!coreAssets.has(i.token.denom)) {
+        api.add(i.token.denom, i.token.amount)
+        return;
+      }
+      api.add(i.token.denom, i.token.amount)
+    })
+  }
 }
+
+
 
 module.exports = {
   misrepresentedTokens: true,
