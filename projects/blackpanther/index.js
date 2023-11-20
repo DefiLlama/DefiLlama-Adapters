401e6d5a3901944fd71f10315eea7c460f691100
Black Panther <blackwhalemoney@gmail.com>
2023-11-20 19:22:43 +0800

    combine tvl from vault and incentive contracts

--- projects/blackpanther/index.js
+++ projects/blackpanther/index.js
@@ -1,9 +1,15 @@
 const { sumTokens, endPoints, queryContracts } = require('../helper/chain/cosmos')
 
+async function getOwners() {
+    let vaultContracts = await queryContracts({ codeId: 181, chain: 'injective' });
+    let incentiveContracts = await queryContracts({ codeId: 132, chain: 'injective' });
+
+    let combinedContracts = [...vaultContracts, ...incentiveContracts];
+    return combinedContracts;
+}
+
 async function tvl() {
-  const vaultContractsTvl = sumTokens({ owners: await queryContracts({ codeId: 181, chain: 'injective' }), chain: 'injective' })
-  const incentiveContractsTvl = sumTokens({ owners: await queryContracts({ codeId: 132, chain: 'injective' }), chain: 'injective' })
-  return vaultContractsTvl + incentiveContractsTvl
+  return sumTokens({ owners: await getOwners(), chain: 'injective' })
 }
 
 module.exports = {
