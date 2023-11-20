9ca1dbedf6e02b7991069c29dd31234a5974fd5f
Black Panther <blackwhalemoney@gmail.com>
2023-11-20 19:17:48 +0800

    add tvl from incentive contract

--- projects/blackpanther/index.js
+++ projects/blackpanther/index.js
@@ -1,7 +1,9 @@
 const { sumTokens, endPoints, queryContracts } = require('../helper/chain/cosmos')
 
 async function tvl() {
-  return sumTokens({ owners: await queryContracts({ codeId: 181, chain: 'injective' }), chain: 'injective' })
+  const vaultContractsTvl = sumTokens({ owners: await queryContracts({ codeId: 181, chain: 'injective' }), chain: 'injective' })
+  const incentiveContractsTvl = sumTokens({ owners: await queryContracts({ codeId: 132, chain: 'injective' }), chain: 'injective' })
+  return vaultContractsTvl + incentiveContractsTvl
 }
 
 module.exports = {
