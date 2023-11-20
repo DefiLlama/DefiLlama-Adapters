d535a37117c6f7c2f8f9f0ed8bfeda2d0457773f
Black Panther <blackwhalemoney@gmail.com>
2023-11-20 19:42:20 +0800

    use codeId of contract

--- projects/blackpanther/index.js
+++ projects/blackpanther/index.js
@@ -1,14 +1,16 @@
-const { sumTokens, endPoints, queryContracts } = require('../helper/chain/cosmos')
+const { sumTokens, queryContracts } = require('../helper/chain/cosmos')
 
 
 async function tvl() {
-  const owners = [
-    "inj1xug34v8e887uzcc9mr2pql06h2ztvhxxm6asll",
-  ]
-  return sumTokens({ owners, chain: 'injective' })
+  let vaultContracts = await queryContracts({ codeId: 181, chain: 'injective' });
+  let incentiveContracts = await queryContracts({ codeId: 132, chain: 'injective' });
+
+  let combinedContracts = vaultContracts.concat(incentiveContracts);
+  return sumTokens({ owners: combinedContracts, chain: 'injective' })
 }
 
 module.exports = {
+  doublecounted: false,
   injective: {
     tvl,
   },
