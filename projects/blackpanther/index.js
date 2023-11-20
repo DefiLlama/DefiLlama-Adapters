7a60aca9b0c8ac0f741ab7e0eebef081913c4dfa
Black Panther <blackwhalemoney@gmail.com>
2023-11-20 19:35:50 +0800

    update owner

--- projects/blackpanther/index.js
+++ projects/blackpanther/index.js
@@ -2,15 +2,13 @@ const { sumTokens, endPoints, queryContracts } = require('../helper/chain/cosmos
 
 
 async function tvl() {
-  let vaultContracts = await queryContracts({ codeId: 181, chain: 'injective' });
-  let incentiveContracts = await queryContracts({ codeId: 132, chain: 'injective' });
-
-  let combinedContracts = [...vaultContracts, ...incentiveContracts];
-  return sumTokens({ owners: combinedContracts, chain: 'injective' })
+  const owners = [
+    "inj1xug34v8e887uzcc9mr2pql06h2ztvhxxm6asll",
+  ]
+  return sumTokens({ owners, chain: 'injective' })
 }
 
 module.exports = {
-  doublecounted: false,
   injective: {
     tvl,
   },
