a2159acd7904bbf334aa50a0dee0ad3051731885
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-20 10:39:03 +0100

    code refactor

--- projects/handlefi-hsp/index.js
+++ projects/handlefi-hsp/index.js
@@ -1,24 +1,13 @@
-const { sumTokens2 } = require("../helper/unwrapLPs")
+const { sumTokensExport } = require("../helper/unwrapLPs")
 
-const chain = 'arbitrum'
-
-// Arbitrum TVL.
 const hspPool = "0x5CE8dDD04F3576C93eDdDf0eb58bf2c7f643Ad0A"
 const fxTokens = {
   usd: "0x8616E8EA83f048ab9A5eC513c9412Dd2993bcE3F",
 }
 
-function tvl(_, _b, {[chain]: block }) {
-  return sumTokens2({
-    chain,
-    block, tokensAndOwners: [
-    [fxTokens.usd, hspPool],
-  ] })
-}
-
 module.exports = {
   arbitrum: {
-    tvl,
+    tvl: sumTokensExport({ owner: hspPool, tokens: [fxTokens.usd] }),
   },
   methodology: "TVL on arbitrum is the sum of all handle synthetic perpetuals (hSP) deposits",
 }
