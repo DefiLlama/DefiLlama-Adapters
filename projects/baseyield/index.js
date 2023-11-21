49b1f434a87e2d247eb801485f8d83ad1ebbaaf7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-21 09:34:17 +0100

    baseyield: skip own token from tvl

--- projects/baseyield/index.js
+++ projects/baseyield/index.js
@@ -1,7 +1,5 @@
-const { yieldHelper } = require("../helper/unknownTokens");
-
+const { sumTokens2 } = require('../helper/unwrapLPs')
 const MiniChefV2 = "0x3db771B933aC5d824a2411866F1a395DbB026528";
-const tokenAPI = "address:want"
 
 const abis = {
   lpToken: 'function lpToken(uint256) public view returns (address)',
@@ -12,7 +10,13 @@ module.exports = {
   base: {
     tvl: async (_, _b, _c, { api }) => {
       const vaults = await api.fetchList({ target: MiniChefV2, lengthAbi: abis.poolLength, itemAbi: abis.lpToken, })
-      return yieldHelper({ ...api, vaults, tokenAPI });
+      const tokens = await api.multiCall({ abi: 'address:want', calls: vaults })
+      const bals = await api.multiCall({ abi: 'uint256:balance', calls: vaults })
+      tokens.forEach((token, i) => {
+        if (token.toLowerCase() === '0xf45116c2e5be608152ea8a6a73917dfe75b707dc') return;
+        api.add(token, bals[i])
+      })
+      return sumTokens2({ api, resolveLP: true })
     }
   }
 }
