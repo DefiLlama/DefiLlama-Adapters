ef575fea821e889938a6732cbfe380a661e1a047
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-21 17:35:49 +0100

    code refactor

--- projects/lend-lord/index.js
+++ projects/lend-lord/index.js
@@ -1,47 +1,23 @@
-const sdk = require("@defillama/sdk");
-
-const chain = "manta";
+const { sumTokens2 } = require('../helper/unwrapLPs')
 const LEND_LORD_VAULT_CONTRACT = "0x05c576786B1B96C69917eDc4E8162D39aFe03cD0";
 
-async function tvl(_, _b, { [chain]: block }) {
-  const { output: tokens } = await sdk.api.abi.call({
-    abi: abi.getAssets,
-    target: LEND_LORD_VAULT_CONTRACT,
-    chain,
-    block,
-  });
+async function tvl(_, _b, _cb, { api, }) {
+  const tokens = await api.call({ abi: abi.getAssets, target: LEND_LORD_VAULT_CONTRACT })
+  return sumTokens2({ api, owner: LEND_LORD_VAULT_CONTRACT, tokens })
+}
 
-  const assetsInfo = await Promise.all(
-    tokens.map(async (token) => {
-      const { output: liquidity } = await sdk.api.abi.call({
-        abi: abi.getAssetTotalLiquidity,
-        target: LEND_LORD_VAULT_CONTRACT,
-        params: token,
-        chain,
-        block,
-      });
-
-      return {
-        [`${chain}:${token}`]: liquidity,
-      };
-    })
-  );
-
-  let balances = {};
-
-  assetsInfo.map((i) => {
-    balances = { ...balances, ...i };
-  });
-
-  return balances;
+async function borrowed(_, _b, _cb, { api, }) {
+  const tokens = await api.call({ abi: abi.getAssets, target: LEND_LORD_VAULT_CONTRACT })
+  const balances = await api.multiCall({ abi: abi.getAssetTotalBorrowBalance, calls: tokens, target: LEND_LORD_VAULT_CONTRACT })
+  api.addTokens(tokens, balances)
+  return api.getBalances()
 }
 
 const abi = {
   getAssets: "address[]:getAssets",
-  getAssetTotalLiquidity:
-    "function getAssetTotalLiquidity(address) view returns (uint256 amount)",
+  getAssetTotalBorrowBalance: "function  getAssetTotalBorrowBalance(address) view returns (uint256 amount)",
 };
 
 module.exports = {
-  manta: { tvl },
+  manta: { tvl, borrowed },
 };
