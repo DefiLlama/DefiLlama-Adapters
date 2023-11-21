9c2069df6634af8e4b8522c203a9890734620d62
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-21 09:21:56 +0100

    code refactor

--- projects/basemax-finance/index.js
+++ projects/basemax-finance/index.js
@@ -1,15 +1,14 @@
 const ADDRESSES = require("../helper/coreAssets.json");
 const { staking } = require("../helper/staking");
-const { pool2Exports } = require("../helper/pool2");
-
+const { sumTokensExport } = require("../helper/unknownTokens");
+const LP = '0xd2eb1de935fe66501aece023b0437fa7b9c40a25'
 
 module.exports = {
   methodology: "Counts USDC deposited to trade and to mint BLP. Staking counts BSM and esBSM deposited to earn esBSM",
-
+  start: 1700488,
   base: {
-    start: 1700488,
     tvl: staking("0xEDFFF5d0C68cFBd44FA12659Fd9AD55F04748874", ADDRESSES.base.USDbC),
-    staking: pool2Exports("0xe2cb504d51fd16d8bdf533c58553ed3f4f755f00", ["0xC5Dc1b9413c47089641D811B6336c0f2fE440883","0xd2eb1de935fe66501aece023b0437fa7b9c40a25"], "base"),
+    pool2: sumTokensExport({ owner: "0xe2cb504d51fd16d8bdf533c58553ed3f4f755f00", tokens: [LP], useDefaultCoreAssets: true, }),
+    staking: sumTokensExport({ owner: "0x957e6844aa7e963dc26447646be268932b785200", tokens: ['0xc5dc1b9413c47089641d811b6336c0f2fe440883'], useDefaultCoreAssets: true, lps: [LP] }),
   }
-
 };
