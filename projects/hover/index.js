0d0ddbe9b0c4a470532959b19e3f8d0d8e115fab
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-21 19:15:00 +0100

    minor fix

--- projects/hover/index.js
+++ projects/hover/index.js
@@ -1,11 +1,6 @@
-const {compoundExports} = require('../helper/compound')
-const ADDRESSES = require('../helper/coreAssets.json')
+const { compoundExports2 } = require('../helper/compound')
 
-module.exports={
-    timetravel: true,
-    doublecounted: false,
-    methodology: "Same as compound, we just get all the collateral (not borrowed money) on the lending markets",    
-    kava:{
-        ...compoundExports("0x3A4Ec955a18eF6eB33025599505E7d404a4d59eC", "kava", "0xb51eFaF2f7aFb8a2F5Be0b730281E414FB487636", ADDRESSES.kava.WKAVA)
-    }
+module.exports = {
+  methodology: "Same as compound, we just get all the collateral (not borrowed money) on the lending markets",
+  kava: compoundExports2({ comptroller: '0x3A4Ec955a18eF6eB33025599505E7d404a4d59eC', cether: '0xb51eFaF2f7aFb8a2F5Be0b730281E414FB487636' }),
 }
