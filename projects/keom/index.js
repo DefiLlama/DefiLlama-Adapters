b6e34deacf96c7a103a483ba6a7a3050e16f8c6b
dlock <0xdlock@proton.me>
2023-11-21 20:51:10 +0200

    Update addresses to unitroller

--- projects/keom/index.js
+++ projects/keom/index.js
@@ -2,11 +2,11 @@ const { compoundExports2 } = require('../helper/compound')
 
 const addresses = {
   polygon: {
-    comptroller: '0xc145700AC0d8A59B1f64DcE35687dD7CA2BEB26A',
+    comptroller: '0x5B7136CFFd40Eee5B882678a5D02AA25A48d669F',
     cether: '0x7854D4Cfa7d0B877E399bcbDFfb49536d7A14fc7',
   },
   manta: {
-    comptroller: '0xBd5Cd926EB73B48905091fFf9996Bba832FEAc71',
+    comptroller: '0x91e9e99AC7C39d5c057F83ef44136dFB1e7adD7d',
     cether: '0x8903Dc1f4736D2FcB90C1497AebBABA133DaAC76',
   }
 }
