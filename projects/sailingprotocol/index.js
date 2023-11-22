80534e4170e2a84666f6602473c2cce45510ec78
Nicholas Szerman <nicszerman@gmail.com>
2023-11-21 22:16:25 -0300

    polished

--- projects/sailingprotocol/index.js
+++ projects/sailingprotocol/index.js
@@ -1,56 +1,44 @@
 const sdk = require('@defillama/sdk');
 const axios = require('axios');
 
-// Fetches pegs from the API
 const PEGS_API_URL = 'https://sailingprotocol.org/api/sailingprotocol/public_analytics/get_onchain_instruments';
-const HISTORICAL_DATA_API_URL = 'https://sailingprotocol.org/api/sailingprotocol/market_data/historical_daily_SPY';
+const HISTORICAL_PRICING_API_URL = 'https://sailingprotocol.org/api/sailingprotocol/market_data/historical_daily_SPY';
 
-// Obtiene los pegs de la API
-async function fetchPegs() {
-    const response = await axios.post(PEGS_API_URL);
-    return response.data.pegs;
+async function getPegsInformation() {
+    const pegsInformationResponse = await axios.post(PEGS_API_URL);
+    return pegsInformationResponse.data.pegs;
 }
 
-// Fetches the total supply of a peg
-async function getPegTVL(networkName, pegAddress) {
-    const response = await sdk.api.abi.call({
+async function getPegSupply(api, pegAddress) {
+    return await api.call({
         target: pegAddress,
         abi: "erc20:totalSupply",
-        chain: networkName,
-    });
-
-    if (isNaN(response.output)) {
-        console.error(`Invalid total supply for contract ${pegAddress}: ${response.output}`);
-        return 0;
-    }
-
-    return response.output / 1e18;
+    }) / 1e18;
 }
 
-// Fetches the TVL in USD for the chain
-async function getChainTvlusd(_0, _1, _2, { api }) {
-    try {
-        const response = await axios.post(HISTORICAL_DATA_API_URL);
-        const historicalData = response.data;
-        const pegs = await fetchPegs();
-        const address = pegs[0].address;
-        const tvlValue = await getPegTVL('kava', address);
+async function tvl(_0, _1, _2, { api }) {
+    const pegs = await getPegsInformation();
+    
+    // initial version supports SPY (S&P500)
+    const spyHistoricalPricing = (await axios.post(HISTORICAL_PRICING_API_URL)).data;
+    if (spyHistoricalPricing.length === 0) {
+        throw new Error('No SPY pricing information.');
+    }
+    spyHistoricalPricing.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-        if (historicalData.length > 0) {
-            historicalData.sort((a, b) => new Date(b.date) - new Date(a.date));
-            const lastPrice = historicalData[0].close;
-
-            const tokenTvl = tvlValue * lastPrice * 1e6;
-            api.add("erc20/tether/usdt", tokenTvl)
-        } else {
-            console.error('No historical data was found in the response.');
+    for (const peg of pegs) {
+        if (peg.ticker !== 'SPYs' || peg.network.toLowerCase() !== api.chain.toLowerCase()) {
+            continue;
         }
-    } catch (error) {
-        console.error('Error fetching data from the API:', error);
+        const pegSupply = await getPegSupply(api, peg.address);
+        const spyPrice = spyHistoricalPricing[0].close;
+
+        const pegTvl = pegSupply * spyPrice * 1e6;
+        api.add("erc20/tether/usdt", pegTvl)
     }
 }
 
 module.exports = {
-    kava: { tvl: getChainTvlusd },
+    kava: { tvl, },
     methodology: 'The total supply is extracted from the contract found in the Sailing Protocol API, and this value is multiplied by the latest closing price of the S&P500 to obtain the TVL in USD.'
 }
