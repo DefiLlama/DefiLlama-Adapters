7c0caaaf35775a3f531a03c1ea448118176a0047
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-22 18:42:49 +0100

    code refactor

--- projects/sailingprotocol/index.js
+++ projects/sailingprotocol/index.js
@@ -1,43 +1,19 @@
-const axios = require('axios');
-
-const PEGS_API_URL = 'https://sailingprotocol.org/api/sailingprotocol/public_analytics/get_onchain_instruments';
-const HISTORICAL_PRICING_API_URL = 'https://sailingprotocol.org/api/sailingprotocol/market_data/historical_daily_SPY';
-
-async function getPegsInformation() {
-    const pegsInformationResponse = await axios.post(PEGS_API_URL);
-    return pegsInformationResponse.data.pegs;
-}
-
-async function getPegSupply(api, pegAddress) {
-    return await api.call({
-        target: pegAddress,
-        abi: "erc20:totalSupply",
-    }) / 1e18;
-}
+const sdk = require('@defillama/sdk')
 
 async function tvl(_0, _1, _2, { api }) {
-    const pegs = await getPegsInformation();
-    
-    // initial version supports SPY (S&P500)
-    const spyHistoricalPricing = (await axios.post(HISTORICAL_PRICING_API_URL)).data;
-    if (spyHistoricalPricing.length === 0) {
-        throw new Error('No SPY pricing information.');
-    }
-    spyHistoricalPricing.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-    for (const peg of pegs) {
-        if (peg.ticker !== 'SPYs' || peg.network.toLowerCase() !== api.chain.toLowerCase()) {
-            continue;
-        }
-        const pegSupply = await getPegSupply(api, peg.address);
-        const spyPrice = spyHistoricalPricing[0].close;
-
-        const pegTvl = pegSupply * spyPrice * 1e6;
-        api.add("erc20/tether/usdt", pegTvl)
-    }
+  const SPY = '0x75B5DACEc8DACcb260eA47549aE882513A21CE01'
+  const arbitrumApi = new sdk.ChainApi({ chain: 'arbitrum' })
+  const totalSupply = await api.call({ abi: 'erc20:totalSupply', target: SPY })
+  // https://pyth.network/developers/price-feed-ids
+  const { price, expo } = await arbitrumApi.call({ abi: 'function getPriceUnsafe(bytes32 id) view returns (tuple(int64 price, uint64 conf, int32 expo, uint256 publishTime) price)', target: '0xff1a0f4744e8582df1ae09d5611b887b6a12925c', params: '0x19e09bb805456ada3979a7d1cbb4b6d63babc3a0f8e8a9509f68afa5c4c11cd5' })
+  return {
+    tether: (totalSupply * price) / 10 ** (18 - expo)
+  }
 }
 
 module.exports = {
-    kava: { tvl, },
-    methodology: 'The total supply is extracted from the contract found in the Sailing Protocol API, and this value is multiplied by the latest closing price of the S&P500 to obtain the TVL in USD.'
+  misrepresentedTokens: true,
+  timetravel: false,
+  kava: { tvl, },
+  methodology: 'The total supply is extracted from the contract found in the Sailing Protocol API, and this value is multiplied by the latest closing price of the S&P500 to obtain the TVL in USD.'
 }
