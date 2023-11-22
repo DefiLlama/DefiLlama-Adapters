b826b91ab6da01245ef2690e729024bcef385390
EdgarRBL <99257947+EdgarRBL@users.noreply.github.com>
2023-11-22 17:06:29 -0300

    chore: remove Hover methodology description (#8159)
    * chore: remove Hover methodology description
    
    * chore: remove Hover methodology description

--- projects/hover/index.js
+++ projects/hover/index.js
@@ -1,6 +1,5 @@
 const { compoundExports2 } = require('../helper/compound')
 
 module.exports = {
-  methodology: "Same as compound, we just get all the collateral (not borrowed money) on the lending markets",
   kava: compoundExports2({ comptroller: '0x3A4Ec955a18eF6eB33025599505E7d404a4d59eC', cether: '0xb51eFaF2f7aFb8a2F5Be0b730281E414FB487636' }),
 }
