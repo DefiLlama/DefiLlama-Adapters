a67cb3141879ac30163b0fe463c0006842027fb6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-24 08:17:08 +0100

    code refactor

--- projects/artcpaclub/index.js
+++ projects/artcpaclub/index.js
@@ -1,16 +1,15 @@
-const { sumTokens } = require("../helper/chain/elrond");
+const { sumTokens, sumTokensExport } = require("../helper/chain/elrond");
+const owners = [
+  "erd1qqqqqqqqqqqqqpgqfken0exk7jpr85dx6f8ym3jgcagesfcqkqys0xnquf",
+  "erd1qqqqqqqqqqqqqpgqj8exjpz38agu78sxh5rlxcp2kmxy35m6kqysscypf3",
+];
 
-async function tvl() {
-  const owners = [
-    "erd1qqqqqqqqqqqqqpgqfken0exk7jpr85dx6f8ym3jgcagesfcqkqys0xnquf",
-    "erd1qqqqqqqqqqqqqpgqj8exjpz38agu78sxh5rlxcp2kmxy35m6kqysscypf3",
-  ];
-  return sumTokens({ owners });
-}
+const CPA = 'CPA-97530a'
 
 module.exports = {
   timetravel: false,
   elrond: {
-    tvl,
+    tvl: sumTokensExport({ owners, blacklistedTokens: [CPA]}),
+    staking: sumTokensExport({ owners, tokens: [CPA]}),
   },
 };
