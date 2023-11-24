5335c89f1c7be4cf8ecdf2396bfbd666b98f6beb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-24 16:41:34 +0100

    code refactor

--- projects/meridian/index.js
+++ projects/meridian/index.js
@@ -1,13 +1,7 @@
 const { getLiquityTvl } = require('../helper/liquity')
 
-const options = { nonNativeCollateralToken: false}
-
 module.exports = {
-    methodology: "Deposited ETH and TLOS on the Base and Telos network",
-    base: {
-      tvl: getLiquityTvl("0x56a901FdF67FC52e7012eb08Cfb47308490A982C", options)
-    },
-    telos: {
-      tvl: getLiquityTvl("0xF58e1a49168FC7f16C441b38701CFC9D8e29A074", options)
-    },
-  };
+  methodology: "Deposited ETH and TLOS on the Base and Telos network",
+  base: { tvl: getLiquityTvl("0x56a901FdF67FC52e7012eb08Cfb47308490A982C") },
+  telos: { tvl: getLiquityTvl("0xF58e1a49168FC7f16C441b38701CFC9D8e29A074") },
+};
