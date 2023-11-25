be4428a2ddd39e39d64f6e73688c46574cd18271
0xngmi <0xngmi@protonmail.com>
2023-11-25 12:01:00 +0000

    remove event

--- projects/solidly-v3/index.js
+++ projects/solidly-v3/index.js
@@ -2,7 +2,4 @@ const { uniV3Export } = require("../helper/uniswapV3");
 
 module.exports = uniV3Export({
   ethereum: { factory: "0x70fe4a44ea505cfa3a57b95cf2862d4fd5f0f687", fromBlock: 18044650, },
-  hallmarks:[
-    [1693526400, "Solidly V3 launch"],
-  ]
 });
