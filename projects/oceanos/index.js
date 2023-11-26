2b55671b6a23d6c59fde99a814076ef61af37d47
oceanos-labs <149803375+oceanos-labs@users.noreply.github.com>
2023-11-27 04:03:40 +0900

    add oceanos (#8188)
    * add oceanos
    
    * code refactor
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ projects/oceanos/index.js
@@ -0,0 +1,14 @@
+const poolAddresses = [
+  '0xD50766f79Ecef0BA96E1d7DD7ccB56E1b2Ba1120', // eth,
+  '0x2F453e781E3A474290fad60a22fEa6f155B69fBD', // usdc
+  '0x836280846adc84f28918Cec30A7dCe791D17b72C', // wsteth
+]
+
+async function tvl(_, _1, _2, { api }) {
+  const tokens = await api.multiCall({ abi: 'address:collateralAsset', calls: poolAddresses })
+  return api.sumTokens({ tokensAndOwners2: [tokens, poolAddresses] })
+}
+
+module.exports = {
+  manta: { tvl }
+}
