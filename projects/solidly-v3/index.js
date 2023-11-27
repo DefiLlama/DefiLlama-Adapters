e03941da70a65f74903bca590ef4dbb727359227
realdealshaman <realdealshaman11@gmail.com>
2023-11-27 22:04:30 +0000

    fix exports

--- projects/solidly-v3/index.js
+++ projects/solidly-v3/index.js
@@ -1,8 +1,8 @@
 const { uniV3Export } = require("../helper/uniswapV3");
 
+module.exports.hallmarks=[
+  [1693526400, "Solidly V3 launch"],
+],
 module.exports = uniV3Export({
   ethereum: { factory: "0x70fe4a44ea505cfa3a57b95cf2862d4fd5f0f687", fromBlock: 18044650, },
-  hallmarks:[
-    [1693526400, "Solidly V3 launch"],
-  ]
 });
