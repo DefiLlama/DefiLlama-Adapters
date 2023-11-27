42267757cd922c0113aafaf2ed4779ee93353318
Arham Chordia <43543921+arhamchordia@users.noreply.github.com>
2023-11-27 14:49:35 +0530

    Update Quasar index.js

--- projects/quasar/index.js
+++ projects/quasar/index.js
@@ -1,23 +1,12 @@
-const sdk = require("@defillama/sdk")
 const {getConfig} = require('../helper/cache')
 
 async function tvl() {
-    const data = await getConfig('quasar-vaults', 'https://api.quasar.fi/vaults')
-    const vaults = data.filter(i => i.chainId === 'osmosis-1').map(i => i.address)
-    let balances = {}
-    for (const vault of vaults) {
-        let tvl_api = "https://api.quasar.fi/vaults/" + vault + "/tvl?time_resolution=day"
-        let tvl_data = await getConfig('quasar-vaults-tvl' + vault, tvl_api)
-        let usdAmounts = tvl_data.map(entry => entry.tvl.usd);
-        if (usdAmounts.length > 0) {
-            sdk.util.sumSingleBalance(
-                balances,
-                "usd",
-                usdAmounts[0],
-            )
-        }
-    }
-    return balances
+  try {
+    return await getConfig('quasar-vaults', 'https://api.quasar.fi/vaults/total_tvl')
+  } catch (error) {
+    console.error('Error fetching total TVL:', error);
+    return {};
+  }
 }
 
 module.exports = {
