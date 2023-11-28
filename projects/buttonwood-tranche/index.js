b4fbcd10e3db8463c296669be1046b813af60393
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-28 10:22:08 +0100

    code refactor

--- projects/buttonwood-tranche/index.js
+++ projects/buttonwood-tranche/index.js
@@ -10,38 +10,8 @@ const config = {
       "0x72799ffd1f4ccf92ea2b1ee0cada16a5461c4d96",
       "0xfc74f7b04c620dad6a750d4f60f13586e2b1ef54",
     ],
-    fromBlock: 13779573 },
-}
-
-async function getTranche(api, bond, index) {
-  const trancheOutput = await api.call({ abi: abi.tranches, target: bond, params: [index] });
-  return trancheOutput.token;
-}
-
-async function getTranches(api, bond, trancheCount) {
-  const trancheArray = await Promise.all(
-    [...Array(trancheCount).keys()].map(index => getTranche(api, bond, index))
-  );
-  return trancheArray;
-}
-async function getBondTranches(api, bonds) {
-  return new Map(
-    await Promise.all(
-    bonds.map(
-      bond => api.call({ abi: abi.trancheCount, target: bond})
-        .then(trancheCount => getTranches(api, bond, parseInt(trancheCount)))
-        .then(tranches => [bond, tranches])
-    )
-  ));
-}
-
-async function getBondCollateralTokens(api, bonds) {
-  return new Map(await Promise.all(
-    bonds.map(
-      bond => api.call({ abi: abi.collateralToken, target: bond})
-        .then(collateralToken => [bond, collateralToken])
-    )
-  ));
+    fromBlock: 13779573
+  },
 }
 
 async function getAllBondsFromBondFactory(api, bondFactory, fromBlock) {
@@ -67,26 +37,20 @@ Object.keys(config).forEach(chain => {
       ).then(bonds => bonds.flat(1));
 
       // Collecting all of the collateralTokens for each bond in a map
-      const collateralTokenMap = await getBondCollateralTokens(api, allBonds);
-
-      // Collecting all of the tranches for each bond in a map
-      const trancheTokenMap = await getBondTranches(api, allBonds);
-
-      // Creating ownerToken pairs from bonds and their collateralTokens
-      const bondOwnerTokens = allBonds.map(
-        bond => [[collateralTokenMap.get(bond)], bond]
-      )
-
-      // Creating ownerToken pairs from tranches and the collateralTokens of their parent bond
-      const trancheOwnerTokens = allBonds.flatMap(
-        bond => trancheTokenMap.get(bond).map(
-          tranche => [[collateralTokenMap.get(bond)], tranche]
-        )
-      );
-
-      // Concatenating ownerToken pairs
-      const ownerTokens = bondOwnerTokens.concat(trancheOwnerTokens);
-      return sumTokens2({ api, ownerTokens, })
+      const collateralTokens = await api.multiCall({ abi: abi.collateralToken, calls: allBonds })
+      const trancheCounts = await api.multiCall({ abi: abi.trancheCount, calls: allBonds })
+      const owners = [...allBonds]
+      const tokens = [...collateralTokens]
+      const calls = []
+      trancheCounts.forEach((trancheCount, i) => {
+        for (let j = 0; j < trancheCount; j++) {
+          tokens.push(collateralTokens[i])
+          calls.push({ target: allBonds[i], params: j })
+        }
+      })
+      const tranches = await api.multiCall({ abi: abi.tranches, calls })
+      tranches.forEach(tranche => owners.push(tranche.token))
+      return sumTokens2({ api, tokensAndOwners2: [tokens, owners], permitFailure: true, blacklistedTokens: ['0xf0406c59da4880fe41ded41990af0ac594a6ef53'] })
     }
   }
 })
