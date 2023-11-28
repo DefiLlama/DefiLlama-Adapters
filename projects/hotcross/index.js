33f924dadc44b4cd96742d8194487cd1b7a1787e
waynebruce0x <waynebruce0x@gmail.com>
2023-11-28 10:35:18 +0000

    hotcross

---
+++ projects/hotcross/index.js
@@ -0,0 +1,34 @@
+const { sumTokens2 } = require("../helper/unwrapLPs");
+const { fetchURL } = require("../helper/utils");
+
+const chainKeys = {
+  ethereum: {
+    asset: "erc20",
+    chain: "eth",
+  },
+  bsc: {
+    asset: "bep20",
+    chain: "bsc",
+  },
+};
+
+function tvl(chain, get) {
+  return async (_t, _e, { [chain]: block }, { logArray }) => {
+    const assets = (await get).data;
+    const tokensAndOwners = assets.map((data) => [
+      data[chainKeys[chain].asset],
+      data[`${chainKeys[chain].chain}BridgeAddress`],
+    ]);
+    return sumTokens2({ tokensAndOwners, block, chain, logArray });
+  };
+}
+
+const chainTypeExports = () => {
+  const get = fetchURL("https://api.hotcross.com/bridges");
+  return Object.keys(chainKeys).reduce(
+    (obj, chain) => ({ ...obj, [chain]: { tvl: tvl(chain, get) } }),
+    {}
+  );
+};
+
+module.exports = chainTypeExports();
