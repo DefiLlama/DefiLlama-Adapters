c036a903c8820fb891a22d781945f74abc685e82
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-29 11:45:25 +0100

    code refactor

--- projects/csix/index.js
+++ projects/csix/index.js
@@ -1,47 +1,20 @@
-const sdk = require("@defillama/sdk");
-const ethers = require("ethers");
-
 const CSIX_TOKEN = "0x04756126F044634C9a0f0E985e60c88a51ACC206";
 const STAKING_CONTRACT_V1 = "0xadc743298F6339Cd8ebC0Dc58D4E19C2065D6b4f";
 const STAKING_CONTRACT_V2 = "0xA4f55D251b8fa8e0C291CC539F020c5Cbe4a9FA8";
 
-const ABI = {
-  inputs: [],
-  name: "totalStaked",
-  outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-  stateMutability: "view",
-  type: "function",
-};
-
 async function staking(_, _1, _2, { api }) {
-  const totalStakes = await api.batchCall([
-    {
-      abi: ABI,
-      target: STAKING_CONTRACT_V1,
-    },
-    {
-      abi: ABI,
-      target: STAKING_CONTRACT_V2,
-    },
-  ]);
-
-  const totalStaked = totalStakes.reduce(
-    (prev, curr) => prev.add(curr),
-    ethers.BigNumber.from(0)
-  );
-
-  api.add(CSIX_TOKEN, totalStaked);
+  const staked = await api.multiCall({ abi: "uint256:totalStaked", calls: [STAKING_CONTRACT_V1, STAKING_CONTRACT_V2,] })
+  staked.forEach(i => api.add(CSIX_TOKEN, i))
+  return api.getBalances()
 }
 
 module.exports = {
-  timetravel: false,
-  misrepresentedTokens: false,
   hallmarks: [
     [1676338485, "Staking V1"],
     [1700652398, "Staking V2"],
   ],
   bsc: {
-    tvl: staking,
+    tvl: () => ({}),
     staking,
   },
   start: 25647232,
