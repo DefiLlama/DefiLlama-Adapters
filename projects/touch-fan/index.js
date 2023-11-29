8feb802676803089bd8a2b824225ac4f7b901f68
lwaisyj <lwaisyj@163.com>
2023-11-29 10:32:34 +0800

    touch

--- projects/touch-fan/index.js
+++ projects/touch-fan/index.js
@@ -1,7 +1,8 @@
 const { sumTokensExport } = require('../helper/unknownTokens')
-const fan_contract = "0x9842114F1d9c5286A6b8e23cF0D8142DAb2B3E9b"
+
 const { nullAddress } = require('../helper/unwrapLPs')
 
+const fan_contract = "0x9842114F1d9c5286A6b8e23cF0D8142DAb2B3E9b"
 const touch_contract = "0xC612eD7a1FC5ED084C967bD71F1e0F0a338Cf816"
 const tft_address = "0x14acccd04393f26ba155e5402aa6fddbb8e2254a"
 const zk_sync_swap_pool = "0xB075F49F4Bea5204d22B42E8d442e8E9e3400AF8"
@@ -16,15 +17,16 @@ module.exports = {
     methodology: `We count the ETH on ${fan_contract} and ${touch_contract}`,
     misrepresentedTokens: true,
     era: {
-        tvl: sumTokensExport({ owners: [fan_contract, touch_contract], tokens: [nullAddress] }),
-        staking: sumTokensExport({
-            owners: [lock_tft_address, vester_address, stake_tft_contract_address], tokens: [tft_address],
+        tvl: sumTokensExport({
+            tokensAndOwners: [
+                [nullAddress, fan_contract],
+                [nullAddress, touch_contract],
+                [tft_address, lock_tft_address],
+                [tft_address, vester_address],
+                [tft_address, stake_tft_contract_address],
+                [zk_sync_swap_pool, lock_lp_address]
+            ],
             lps: [zk_sync_swap_pool],
-            abis: { getReservesABI },
-            useDefaultCoreAssets: true
-        }),
-        pool2: sumTokensExport({
-            owners: [lock_lp_address], tokens: [zk_sync_swap_pool], resolveLP: true,
             abis: { getReservesABI },
             useDefaultCoreAssets: true
         })
