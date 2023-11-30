091746bcd9f416ab1f6ddae065c8f80f333db664
amilcarrey <amilcarrey.ar@gmail.com>
2023-11-29 23:18:13 -0300

    feat: :sparkles: Add suggestions by g1nt0nki

--- projects/hatom-booster/index.js
+++ projects/hatom-booster/index.js
@@ -1,44 +1,21 @@
 const { call } = require('../helper/chain/elrond')
-const { cachedGraphQuery } = require('../helper/cache')
-const { nullAddress } = require('../helper/tokenMapping')
 
 const boosterContractAddress = 'erd1qqqqqqqqqqqqqpgqw4dsh8j9xafw45uwr2f6a48ajvcqey8s78sstvn7xd'
-
-async function getMoneyMarkets() {
-   const { queryMoneyMarket: res } = await cachedGraphQuery('hatom-TVLLendingProtocolQuery', 'https://mainnet-api.hatom.com/graphql', `
-    query QueryMoneyMarket {
-      queryMoneyMarket {
-        address
-        underlying {
-          symbol
-          name
-          decimals
-          id
-        }
-      }
-    }
-  `)
-   res.forEach(i => {
-      if (i.underlying.id === 'EGLD') i.underlying.id = nullAddress
-   })
-   return res
-}
+const htm = 'HTM-f51d55'
 
 const staking = async (_, _1, _2, { api }) => {
-   const moneyMarkets = await getMoneyMarkets()
-   const htmTokenId = moneyMarkets.find(i => i.underlying.symbol === 'HTM').underlying.id
    const totalStaked = await call({
       target: boosterContractAddress,
       abi: 'getTotalStake',
       responseTypes: ['number']
    })
-   api.addTokens([htmTokenId], [totalStaked.toString()])
+   api.addTokens([htm], [totalStaked.toString()])
 };
 
 module.exports = {
    timetravel: false,
    elrond: {
-      tvl: staking,
+      tvl: () => ({}),
       staking
    },
 };
