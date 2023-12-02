444110453694d7d3da0449cdd48b63bd777980b3
Clark Alesna <clark_alesna@hotmail.com>
2023-12-02 15:20:00 +0800

    added teddyswap

---
+++ projects/teddyswap/index.js
@@ -0,0 +1,21 @@
+const utils = require('../helper/utils');
+
+async function cardanoTVL() {
+    let response = await utils.fetchURL('https://analytics.teddyswap.org/v1/pools/overview?after=0')
+    let data = response.data;
+
+    let totalTvl = 0;
+    for (let i = 0; i < data.length; i++) {
+        totalTvl += data[i].tvl;
+    }
+
+    return { cardano: totalTvl };
+}
+
+module.exports = {
+    timetravel: false,
+    misrepresentedTokens: true,
+    cardano: {
+        tvl: cardanoTVL
+    }
+}
