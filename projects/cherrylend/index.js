eb4102ed404c306f1cd9f98a1e07b4819e06908e
chadle-git <60274638+chadle-git@users.noreply.github.com>
2023-12-03 12:42:08 -0600

    updated index.js with address

--- projects/cherrylend/index.js
+++ projects/cherrylend/index.js
@@ -1,20 +1,16 @@
-const utils = require('../helper/utils');
+const { sumTokensExport, } = require("../helper/chain/cardano");
 
-async function cardanoTVL() {
-    let response = await utils.fetchURL('https://app.cherrylend.org/api/tvl')
-    let data = response.data;
-
-    let totalTvl = 0;
-    for (let i = 0; i < data.length; i++) {
-        totalTvl += data[i].tvl;
-    }
-
-    return { cardano: totalTvl };
-}
+const scriptAdresses = [
+  'addr1z87jq3sr5psnv5vz2s799ycawj845ny7d2d25ayq7cgywd5nc4gwrvu5dcuccayqddwpx0l492czzxp7929792qv4grql4zqxt', 
+  'addr1zy7z86jxa4dyflaf3rwpt5jlnrdahkt2r4737khf7huv9q5nc4gwrvu5dcuccayqddwpx0l492czzxp7929792qv4grqnc0lf0',
+  'addr1zxczgk9j5z67tjeer7ryhzgznm6zrkjqgm004m2gxymzfnvnc4gwrvu5dcuccayqddwpx0l492czzxp7929792qv4grq2d0zxe',
+  'addr1zytlwd9nvg9qujwtrgmecp4kghelj258d9gwu373cn8eefvnc4gwrvu5dcuccayqddwpx0l492czzxp7929792qv4grqp3aqvs',
+];
 
 module.exports = {
-    timetravel: false,
-    cardano: {
-        tvl: cardanoTVL
-    }
-}
+  methodology: 'TVL are assets either locked as collateral, or payment on interest to be reclaimed',
+  timetravel: false,
+  cardano: {
+    tvl: sumTokensExport({ scripts: scriptAdresses, })
+  }
+};
