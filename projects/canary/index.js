0def6900c47e394fe616bd4b837d6d1484a5c63b
Canary Dev <84072229+canarydeveloper@users.noreply.github.com>
2023-12-07 15:44:35 +0300

    Added Staking Contract index.js (#8304)

--- projects/canary/index.js
+++ projects/canary/index.js
@@ -1,10 +1,24 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { staking } = require("../helper/staking");
+const { getUniTVL } = require('../helper/unknownTokens');
+
+const contracts = {
+  avax: {
+    factory: "0xCFBA329d49C24b70F3a8b9CC0853493d4645436b",
+    cnr: "0x8D88e48465F30Acfb8daC0b3E35c9D6D7d36abaf",
+    stakingContract: "0x39124Af473501Ccd83a5791eA1eFBc2e6dd78f10",
+  },
+  scroll: {
+    factory: "0x8D88e48465F30Acfb8daC0b3E35c9D6D7d36abaf",
+  },
+};
+
 module.exports = {
   misrepresentedTokens: true,
   avax:{
-    tvl: getUniTVL({ chain: 'avax', useDefaultCoreAssets: true, factory: '0xCFBA329d49C24b70F3a8b9CC0853493d4645436b', }),
+    tvl: getUniTVL({ chain: 'avax', useDefaultCoreAssets: true, factory: contracts.avax.factory, }),
+    staking: staking(contracts.avax.stakingContract, contracts.avax.cnr, "avax"),
   },
   scroll:{
-    tvl: getUniTVL({ chain: 'scroll', useDefaultCoreAssets: true, factory: '0x8D88e48465F30Acfb8daC0b3E35c9D6D7d36abaf', }),
+    tvl: getUniTVL({ chain: 'scroll', useDefaultCoreAssets: true, factory: contracts.scroll.factory, }),
   },
 }
