ddccc0892c05b9aa5e80e0c697506fbf446972f1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-08 14:16:41 +0100

    code refactor

--- projects/dvol/index.js
+++ projects/dvol/index.js
@@ -1,66 +1,59 @@
 const sdk = require('@defillama/sdk')
 
 const vaults = [
-    '0x54781C6aa884297369A55A79eF7Fd1FD7B3bBD32',
-    '0x6652f1B0531C4C75B523e74BCf5D0CD009b7BBB8'
+  '0x54781C6aa884297369A55A79eF7Fd1FD7B3bBD32',
+  '0x6652f1B0531C4C75B523e74BCf5D0CD009b7BBB8'
 ]
 
 const abi = {
-    "existingVaultAmount": "uint256:existingVaultAmount",
-    "existingVaultIds": "function existingVaultIds(uint256 index) view returns (uint256)",
-    "idVaultInfoMap": "function idVaultInfoMap(uint256 id) view returns (uint256 id, address depositToken, uint256 maxVaultCapacity, uint256 minVaultLimit, uint256 saleStartTime, uint256 saleEndTime, uint256 termStartTime, uint256 termEndTime, address organization, address transferSigner)",
-    "idVaultStateMap": "function idVaultStateMap(uint256 id) view returns (address lpTokenContract, uint256 soldAmount, bool hasTransferred, bool hasSettled)"
+  "existingVaultAmount": "uint256:existingVaultAmount",
+  "existingVaultIds": "function existingVaultIds(uint256 index) view returns (uint256)",
+  "idVaultInfoMap": "function idVaultInfoMap(uint256 id) view returns (uint256 id, address depositToken, uint256 maxVaultCapacity, uint256 minVaultLimit, uint256 saleStartTime, uint256 saleEndTime, uint256 termStartTime, uint256 termEndTime, address organization, address transferSigner)",
+  "idVaultStateMap": "function idVaultStateMap(uint256 id) view returns (address lpTokenContract, uint256 soldAmount, bool hasTransferred, bool hasSettled)"
 }
 
+let vaultsTvls = {}
 
-const vaultsTvl = async (timestamp, block, _, { api }) => {
-    const tokens = []
-    const bals = []
-    const balances = {}
+async function getVaultsTvl(api) {
+  let key = api.chain + '_' + api.block
+  if (!vaultsTvls[key])
+  vaultsTvls[key] = vaultsTvlFn(api)
 
-    for(var i = 0; i < vaults.length; i++){
-        let vault = vaults[i];
-        const ids = []
-        const total = await api.call({ target: vault, abi: abi["existingVaultAmount"] });
-        for(var j = 0; j < Number(total); j++) {
-            ids.push(
-                await api.call({
-                    target: vault,
-                        abi: abi["existingVaultIds"],
-                        params: j
-                })
-            )
-        }
+  return vaultsTvls[key]
+}
 
-        for(i = 0; i < Number(ids.length); i++) {
-            tokens.push(
-                (await api.call({
-                    target: vault,
-                    abi: abi["idVaultInfoMap"],
-                    params: ids[i]
-                })).depositToken
-            )
-        }
+async function vaultsTvlFn(api) {
+  const borrowApi = new sdk.ChainApi({ chain: api.chain, block: api.block})
+  const ownerTokens = []
+  for (const target of vaults) {
+    const ids = await api.fetchList({  lengthAbi: abi.existingVaultAmount, itemAbi: abi.existingVaultIds, target})
+    const tokens = (await api.multiCall({  abi: abi.idVaultInfoMap, calls: ids, target })).map(i => i.depositToken)
+    const soldAmounts = (await api.multiCall({  abi: abi.idVaultStateMap, calls: ids, target })).map(i => i.soldAmount)
+    borrowApi.addTokens(tokens, soldAmounts)
+    ownerTokens.push([tokens, target])
+  }
+  
+  await api.sumTokens({ ownerTokens })
+  const tvlBalances = api.getBalances()
+  Object.entries(tvlBalances).forEach(b => borrowApi.addToken(b[0], b[1] * -1, { skipChain: true }))
+  return {
+    tvl: tvlBalances,
+    borrowed: borrowApi.getBalances(),
+  }
+}
 
-        for(i = 0; i < Number(ids.length); i++) {
-            bals.push(
-                (await api.call({
-                    target: vault,
-                    abi: abi["idVaultStateMap"],
-                    params: ids[i]
-                })).soldAmount
-            )
-        }
-    }
+async function tvl(_, _b, _cb, { api, }) {
+  return (await getVaultsTvl(api)).tvl
+}
 
-    tokens.forEach((v, i) => sdk.util.sumSingleBalance(balances, v, bals[i], api.chain))
-
-    return balances
+async function borrowed(_, _b, _cb, { api, }) {
+  return (await getVaultsTvl(api)).borrowed
 }
 
 module.exports = {
-    start: 33674950,
-    bsc: {
-        tvl: vaultsTvl
-    },
+  start: 33674950,
+  timetravel: false,
+  bsc: {
+    tvl, borrowed,
+  },
 }
