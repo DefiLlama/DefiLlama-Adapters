c955b0d5226c0bec69b0dad82280793899c15edb
XBANKING <126655346+xbankingorg@users.noreply.github.com>
2023-12-09 13:06:42 +0700

    Update index.js

--- projects/xbanking/index.js
+++ projects/xbanking/index.js
@@ -2,25 +2,25 @@ const BigNumber = require('bignumber.js')
 const axios = require('axios');
 const fetch = require('node-fetch');
 
-async function EthereumTvl() {
-  const myHeaders = new fetch.Headers();
-  myHeaders.append("accept", "application/json");
-  const requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
-  const validatorBalance = fetch("https://docs-demo.quiknode.pro/eth/v1/beacon/states/head/validator_balances?id=0x91104f28e17de8c6bec26c5a8e64a149ba3ed2a35273197c33ed2f2bc74b8dbda96f9098517a6e946247c169c89deb34", requestOptions)
-    .then(response => response.text())
-  const dat = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum`)
-  const nodebalance = JSON.parse(await validatorBalance).data[0].balance / 1000000000
-  const ves = nodebalance * dat.data[0].current_price
-  return parseFloat(ves.toFixed(4));
-}
 
 module.exports = {
-  methodology: 'Counts total value in usd ethereum staked',
-  ethereum: {
-    EthereumTvl,
-  },
+    timetravel: false,
+    methodology: "xbanking tvl",
+    ethereum: {
+        tvl: async () => {
+          const myHeaders = new fetch.Headers();
+          myHeaders.append("accept", "application/json");
+          const requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+          };
+          const validatorBalance = fetch("https://docs-demo.quiknode.pro/eth/v1/beacon/states/head/validator_balances?id=0x91104f28e17de8c6bec26c5a8e64a149ba3ed2a35273197c33ed2f2bc74b8dbda96f9098517a6e946247c169c89deb34", requestOptions)
+            .then(response => response.text())
+          const dat = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum`)
+          const nodebalance = JSON.parse(await validatorBalance).data[0].balance / 1000000000
+          const ves = nodebalance * dat.data[0].current_price
+          return parseFloat(ves.toFixed(4));
+        }
+    }
 }
