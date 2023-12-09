d6d78efdc47b962770e1dfa40be267bc0c9a299f
XBANKING <126655346+xbankingorg@users.noreply.github.com>
2023-12-09 13:08:45 +0700

    Update index.js

--- projects/xbanking/index.js
+++ projects/xbanking/index.js
@@ -1,26 +1,80 @@
-const BigNumber = require('bignumber.js')
-const axios = require('axios');
-const fetch = require('node-fetch');
+const sdk = require("@defillama/sdk");
+const abi = require("./abi.json");
+const contracts = require("./contracts.json");
+const { staking } = require("../helper/staking");
 
+async function vaultTvl(balances, block, abi, target, coin) {
+  const ftmStaked = ((await sdk.api.abi.call({
+    chain: 'fantom',
+    block,
+    target,
+    abi,
+  })).output);
+
+  sdk.util.sumSingleBalance(balances, coin, ftmStaked);
+}
+async function tvl(timestamp, block, chainBlocks) {
+  const balances = {};
+
+  await vaultTvl(
+    balances, 
+    chainBlocks.fantom, 
+    abi.see_s1ftm_circ, 
+    contracts.ftmVault, 
+    contracts.FTM
+    );
+  await vaultTvl(
+    balances, 
+    chainBlocks.fantom, 
+    abi.see_s1ftm_circ, 
+    contracts.ethVault, 
+    contracts.WETH
+    );
+  await vaultTvl(
+    balances, 
+    chainBlocks.fantom, 
+    abi.see_s1tomb_circ, 
+    contracts.avaxVault, 
+    contracts.AVAX
+    );
+  await vaultTvl(
+    balances, 
+    chainBlocks.fantom, 
+    abi.see_s1tomb_circ, 
+    contracts.tombVault, 
+    contracts.TOMB
+    );
+
+  const daiBalances = (await sdk.api.abi.multiCall({
+    chain: 'fantom',
+    block: chainBlocks.fantom,
+    calls: [
+        contracts.ftmVault, 
+        contracts.ethVault, 
+        contracts.avaxVault, 
+        contracts.tombVault
+      ].map((v) => ({
+        target: contracts.Collateral,
+        params: [v],
+    })),
+    abi: 'erc20:balanceOf'
+  })).output
+  .map(b => b.output)
+  .reduce((acc, el) => Number(acc) + Number(el), 0);
+
+  sdk.util.sumSingleBalance(balances, contracts.DAI, daiBalances);
+
+  return balances;
+}
 
 module.exports = {
-    timetravel: false,
-    methodology: "xbanking tvl",
-    ethereum: {
-        tvl: async () => {
-          const myHeaders = new fetch.Headers();
-          myHeaders.append("accept", "application/json");
-          const requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow'
-          };
-          const validatorBalance = fetch("https://docs-demo.quiknode.pro/eth/v1/beacon/states/head/validator_balances?id=0x91104f28e17de8c6bec26c5a8e64a149ba3ed2a35273197c33ed2f2bc74b8dbda96f9098517a6e946247c169c89deb34", requestOptions)
-            .then(response => response.text())
-          const dat = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum`)
-          const nodebalance = JSON.parse(await validatorBalance).data[0].balance / 1000000000
-          const ves = nodebalance * dat.data[0].current_price
-          return parseFloat(ves.toFixed(4));
-        }
-    }
-}
+  fantom: {
+    tvl,
+    // hitting pool2 staking contract twice while stake1 isnt on coingecko
+    pool2: staking(
+      [contracts.pool2, contracts.pool2], 
+      [contracts.daiPool2, contracts.ftmPool2], 
+      'fantom'
+      )
+  }
+}; // node test.js projects/stake1/index.js
