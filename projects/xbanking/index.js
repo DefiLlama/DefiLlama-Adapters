6ca7c6d49e086759ae1fb5dca3ed9fac98119870
XBANKING <126655346+xbankingorg@users.noreply.github.com>
2023-12-09 11:58:31 +0700

    Update index.js

--- projects/xbanking/index.js
+++ projects/xbanking/index.js
@@ -16,7 +16,7 @@ module.exports = {
                 throw new Error("Unknown");
             }
             const result = response.result
-            console.log(response.result);
+            const tonTotalSupply = parseInt(result.stack[0][1], 16)
             return {"coingecko:the-open-network":  BigNumber(tonTotalSupply).div(1e9).toFixed(0)};
         }
     }
