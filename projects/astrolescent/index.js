a80dea41de14fb2692528b9b0ac228cf8cb94a0a
Timan Rebel <timan@trebel.nl>
2023-12-09 12:30:21 +0100

    add Astrolescent pool to Radix

--- projects/astrolescent/index.js
+++ projects/astrolescent/index.js
@@ -1,12 +1,13 @@
 const { sumTokens, queryAddresses } = require('../helper/chain/radixdlt');
 
+const POOL_COMPONENT_ADDRESS = 'component_rdx1cqg95d25q8sa26k6996yxl3qg95edwqn4a775nf3s2zxy6tt22s9xf';
 const STAKING_COMPONENT_ADDRESS = 'component_rdx1cz6ed6wksa2u4a3da5qfeg4tg453tczexulcf0z7vs885rkr6r6363';
 
 module.exports = {
    radixdlt: {
       tvl: async (_, _1, _2, { api }) => {
-         const data = await queryAddresses({ addresses: [STAKING_COMPONENT_ADDRESS] });
-         const owners = data.map((i) => i.details.state.fields.find((i) => i.type_name === 'GlobalOneResourcePool').value);
+         const data = await queryAddresses({ addresses: [STAKING_COMPONENT_ADDRESS, POOL_COMPONENT_ADDRESS] });
+         const owners = data.map((i) => i.details.state.fields.find((i) => i.type_name === 'GlobalOneResourcePool' || i.type_name == 'GlobalMultiResourcePool').value);
          return sumTokens({ owners, api });
       },
    },
