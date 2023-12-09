3e40bab3a9daa82d6456245fe19c1ce75ddf1ed6
Arpit Singh <arpit140940@gmail.com>
2023-12-09 06:03:35 +0530

    LogX TVL

---
+++ projects/logx/index.js
@@ -0,0 +1,12 @@
+const ADDRESSES = require("../helper/coreAssets.json");
+const { sumTokensExport } = require("../helper/unwrapLPs");
+
+module.exports = {
+  methodology: "USDC.e in the vault",
+  linea: {
+    tvl: sumTokensExport({
+      owners: ["0xC5f444D25D5013C395F70398350d2969eF0F6AA0"],
+      tokens: [ADDRESSES.linea.USDC],
+    }),
+  },
+};
