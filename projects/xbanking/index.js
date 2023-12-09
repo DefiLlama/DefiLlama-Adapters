43ab4b4d32001f9634ab9defbaf885a3c00ddead
XBANKING <126655346+xbankingorg@users.noreply.github.com>
2023-12-09 11:54:56 +0700

    Update index.js

--- projects/xbanking/index.js
+++ projects/xbanking/index.js
@@ -1,13 +1,23 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { call } = require("@defillama/sdk/build/abi/abi2");
-const { sumTokensAccount } = require("../helper/chain/ton.js");
+const BigNumber = require("bignumber.js");
+const { post } = require('../helper/http')
 
 module.exports = {
-  ton: {
-    tvl: (_, _b, _c, { api, logArray }) =>
-        sumTokensAccount({
-            api,
-            addr: "Ef-WMmizoLk4CvqTKs-mDrGJwW4fiH5zVd4SaHih7PObxP_0",
-      }),
-  },
-};
+    timetravel: false,
+    methodology: "xbanking",
+    ton: {
+        tvl: async () => {
+            const requestBody = {
+                "address": "Ef-WMmizoLk4CvqTKs-mDrGJwW4fiH5zVd4SaHih7PObxP_0",
+                "method": "get_pool_full_data",
+                "stack": []
+            }
+            const response = await post('https://toncenter.com/api/v2/runGetMethod', requestBody)
+            if (! response.ok)  {
+                throw new Error("Unknown");
+            }
+            const result = response.result
+            const tonTotalSupply = parseInt(result.stack[2][1], 16)
+            return {"coingecko:the-open-network":  BigNumber(tonTotalSupply).div(1e9).toFixed(0)};
+        }
+    }
+}
