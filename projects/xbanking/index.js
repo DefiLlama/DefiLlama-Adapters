5c9168bfc72b89a2a24f11f2f55e503b5bd9a154
XBANKING <126655346+xbankingorg@users.noreply.github.com>
2023-12-09 13:02:54 +0700

    Update index.js

--- projects/xbanking/index.js
+++ projects/xbanking/index.js
@@ -1,23 +1,24 @@
-const BigNumber = require("bignumber.js");
-const { post } = require('../helper/http')
+const BigNumber = require('bignumber.js')
+const axios = require('axios');
+async function EthereumTvl() {
+  const myHeaders = new Headers();
+  myHeaders.append("accept", "application/json");
+  const requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  const validatorBalance = fetch("https://docs-demo.quiknode.pro/eth/v1/beacon/states/head/validator_balances?id=0x91104f28e17de8c6bec26c5a8e64a149ba3ed2a35273197c33ed2f2bc74b8dbda96f9098517a6e946247c169c89deb34", requestOptions)
+    .then(response => response.text())
+  const dat = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum`)
+  const nodebalance = JSON.parse(await validatorBalance).data[0].balance / 1000000000
+  const ves = nodebalance * dat.data[0].current_price
+  return parseFloat(ves.toFixed(4));
+}
 
 module.exports = {
-    timetravel: false,
-    methodology: "xbanking",
-    ton: {
-        tvl: async () => {
-            const requestBody = {
-                "address": "Ef-WMmizoLk4CvqTKs-mDrGJwW4fiH5zVd4SaHih7PObxP_0",
-                "method": "get_pool_full_data",
-                "stack": []
-            }
-            const response = await post('https://toncenter.com/api/v2/runGetMethod', requestBody)
-            if (! response.ok)  {
-                throw new Error("Unknown");
-            }
-            const result = response.result
-            const tonTotalSupply = parseInt(result.stack[0][1], 16)
-            return {"coingecko:the-open-network":  BigNumber(tonTotalSupply).div(1e9).toFixed(0)};
-        }
-    }
+  methodology: 'Counts total value in usd ethereum staked',
+  ethereum: {
+    tvl: EthereumTvl,
+  },
 }
