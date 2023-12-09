d58b7797d0d5803d311b39438513314c45b717b5
realdealshaman <realdealshaman11@gmail.com>
2023-12-09 17:02:54 +0000

    update methodology

--- projects/logx/index.js
+++ projects/logx/index.js
@@ -2,7 +2,7 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport } = require('../helper/unwrapLPs');
 
 module.exports = {
-    methodology: 'To get TVL we call the getAumInUsdl function from contract address: 0xf024541569796286d58fFF4b5898A3C3f1635bd1',
+    methodology: 'USDC.e in the vault',
     linea: {
         tvl: sumTokensExport({ owner: '0xc5f444d25d5013c395f70398350d2969ef0f6aa0', tokens: [ADDRESSES.linea.USDC]}),
   },
