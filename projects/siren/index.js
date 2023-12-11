a3e757f78a3f17cfb9920fb60c299cc287a93d75
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-11 22:15:01 +0100

    refactor

--- projects/siren/index.js
+++ projects/siren/index.js
@@ -1,40 +1,22 @@
-const abis = require('../helper/abis/siren.json');
-const sdk = require('@defillama/sdk');
-const { sumTokens2, sumTokens } = require("../helper/unwrapLPs");
 const { getLogs, getAddress } = require('../helper/cache/getLogs');
+
 const config = {
-  arbitrum: {hedgePools: ["0x07835De4f96164758fE68283a5466E066c1885DC"]},
+  arbitrum: { hedgePools: ["0x07835De4f96164758fE68283a5466E066c1885DC"] },
   polygon: { fromBlock: 17842705, factory: '0x0cdaa64b47474e02cdfbd811ec9fd2d265cd3a0a', vault: '0xc40a31bd9fed1569ce647bb7de7ff93facca36e9', },
   ethereum: { fromBlock: 11272343, factory: '0xb8623477ea6f39b63598ceac4559728dca81af63', vault: '0x7b63ecbc78402553a2d7f01ea3d10079c3aaa469', },
 }
+
 Object.keys(config).forEach(chain => {
-  const hedgePools = config[chain].hedgePools;
+  const { factory, fromBlock, vault, hedgePools } = config[chain]
   module.exports[chain] = {
     tvl: async (_, _b, _cb, { api, }) => {
-      if(chain == 'arbitrum') {
-        let balances = {}
-        for (const hedgePool of hedgePools) {
-          const totalValueCached = (
-            await sdk.api.abi.call({
-              target: hedgePool,
-              abi: abis.getTotalPoolValueCached,
-              chain: 'arbitrum'
-            })
-          ).output;
-          const collateralToken = (
-            await sdk.api.abi.call({
-              abi: abis.collateralToken,
-              target: hedgePool,
-              chain: 'arbitrum'
-            })
-          ).output;
-
-          balances[`arbitrum:${collateralToken}`] = parseInt(totalValueCached)
-        }
-        return sumTokens2({ balances, owner: "0x07835De4f96164758fE68283a5466E066c1885DC" })
+      const tokensAndOwners = []
+      if (hedgePools) {
+        const tokens = await api.multiCall({ abi: 'address:collateralToken', calls: hedgePools })
+        tokensAndOwners.push(...tokens.map((v, i) => [v, hedgePools[i]]))
       }
-      else {
-        const { factory, fromBlock, vault, } = config[chain]
+
+      if (factory) {
         const logs = await getLogs({
           api,
           target: factory,
@@ -42,9 +24,12 @@ Object.keys(config).forEach(chain => {
           fromBlock,
         })
         const pools = logs.map(i => getAddress(i.data))
-        const tokens = await api.multiCall({  abi: 'address:collateralToken', calls: pools})
-        return sumTokens2({ api, tokensAndOwners: tokens.map((v, i) => [[v, pools[i]], [v, vault]]).flat()})
+        const tokens = await api.multiCall({ abi: 'address:collateralToken', calls: pools })
+        const toa = tokens.map((v, i) => [[v, pools[i]], [v, vault]]).flat()
+        tokensAndOwners.push(...toa)
       }
+
+      return api.sumTokens({ tokensAndOwners })
     }
   }
 });
