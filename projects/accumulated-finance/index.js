467d08007695f2eca77421f880d2cae2caaa0f75
Evgeny Rozanov <e.rosanov@gmail.com>
2023-12-12 00:23:21 +0400

    accumulated.finance integration

---
+++ projects/accumulated-finance/index.js
@@ -0,0 +1,42 @@
+const sdk = require('@defillama/sdk');
+const config = require('./config.json');
+
+async function getLsdPoolsTvl(api, filteredPools) {
+
+  const calls = filteredPools.map(pool => ({ target: pool.stToken }));
+
+  const balanceOfResults = await sdk.api.abi.multiCall({
+    abi: 'erc20:totalSupply',
+    chain: api.chain,
+    calls
+  });
+
+  const balances = {};
+  balanceOfResults.output.forEach(balanceOf => {
+    const pool = filteredPools.find(item => item.stToken === balanceOf.input.target);
+    if (pool) {
+      // baseToken - a wrapped token that can be obtained from coingecko.
+      // stToken - a staked token. The price of stToken is equal to the price of baseToken
+      balances[pool.baseToken] = balanceOf.output;
+    }
+  });
+  return balances;
+}
+
+const chains = config.chains;
+
+module.exports = {
+  timetravel: false,
+  methodology:
+    "We aggregated the assets staked across Accumulate finance protocol",
+}
+
+Object.keys(config.chains).forEach(chain => {
+  module.exports[chain] = {
+    tvl: async (timestamp, block, chainBlocks, { api }) => {
+      const chainId = chains[api.chain];
+      const filteredPools = config.lsdPools.filter(item => item.chainID === chainId);
+      return await getLsdPoolsTvl(api, filteredPools);
+    }
+  }
+});
