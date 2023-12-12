bdde24e1bc9c23e0257c6a1b9998533ff77e33b5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-12 10:55:44 +0100

    refactor

--- projects/weft-finance/index.js
+++ projects/weft-finance/index.js
@@ -1,49 +1,32 @@
-const { sumTokens, queryAddresses } = require('../helper/chain/radixdlt');
-const { getConfig } = require('../helper/cache')
-const { get, post } = require('../helper/http')
-const sdk = require('@defillama/sdk')
+const { queryAddresses } = require('../helper/chain/radixdlt');
 
 const pools = [
-    'component_rdx1cq8mm5z49x6lyet44a0jd7zq52flrmykwwxszq65uzfn6pk3mvm0k4',
-    'component_rdx1cq7qd9vnmmu5sjlnarye09rwep2fhnq9ghj6eafj6tj08y7358z5pu',
+  'component_rdx1cq8mm5z49x6lyet44a0jd7zq52flrmykwwxszq65uzfn6pk3mvm0k4',
+  'component_rdx1cq7qd9vnmmu5sjlnarye09rwep2fhnq9ghj6eafj6tj08y7358z5pu',
 ]
 
-async function tvl() {
+async function tvl(_, _b, _cb, { api, }) {
+  const data = await queryAddresses({ addresses: pools })
 
-    let tvl = {}
+  data.forEach((item) => {
+    console.log(JSON.stringify(item, null, 2))
+    api.add(item.fungible_resources.items[0].resource_address, +item.fungible_resources.items[0].amount)
+  })
 
-    const data = await queryAddresses({
-        addresses: pools
-    })
-
-    data.forEach((item) => {
-        tvl['radixdlt:' + item.fungible_resources.items[0].resource_address] = parseFloat(item.fungible_resources.items[0].amount) + parseFloat(item.details.state.fields[1].value)
-    })
-
-    console.log('tvl', tvl)
-
-    return tvl
+  return api.getBalances()
 }
 
+async function borrowed(_, _b, _cb, { api, }) {
+  const data = await queryAddresses({ addresses: pools })
 
-async function borrowed() {
+  data.forEach((item) => {
+    api.add(item.fungible_resources.items[0].resource_address, +item.details.state.fields[1].value)
+  })
 
-    let borrowed = {}
-
-    const data = await queryAddresses({
-        addresses: pools
-    })
-
-    data.forEach((item) => {
-        borrowed['radixdlt:' + item.fungible_resources.items[0].resource_address] = parseFloat(item.details.state.fields[1].value)
-    })
-
-    console.log(borrowed)
-
-    return borrowed
+  return api.getBalances()
 }
 
 module.exports = {
-    radixdlt: { tvl, borrowed },
-    timetravel: true
+  radixdlt: { tvl, borrowed },
+  timetravel: false
 }
