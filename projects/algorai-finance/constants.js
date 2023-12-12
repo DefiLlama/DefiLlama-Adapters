9ac11c0348bc06fa738771e39a96bb35ad807536
vito-ucc <45604845+vito-makhatadze@users.noreply.github.com>
2023-12-12 14:45:27 +0400

    Add new vaults for algorai-finance.

--- projects/algorai-finance/constants.js
+++ projects/algorai-finance/constants.js
@@ -1,6 +1,6 @@
 /**
  *
- * @type {[{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number},{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number},{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number}]}
+ * @type {[{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number},{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number},{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number, originCoin: string}]}
  */
 const vaults = [
     // v1.0
@@ -8,67 +8,98 @@ const vaults = [
         vaultID: 878144513,
         assetDecimals: 6,
         coingecko: 'algorand',
+        originCoin: ''
     },
     {
         vaultID: 878140320,
         assetDecimals: 6,
         coingecko: 'usd-coin',
+        originCoin: ''
     },
     // v1.1
     {
         vaultID: 919531421,
         assetDecimals: 6,
         coingecko: 'algorand',
+        originCoin: ''
     },
     {
         vaultID: 919539218,
         assetDecimals: 6,
         coingecko: 'usd-coin',
+        originCoin: ''
     },
     {
         vaultID: 1004309470,
         assetDecimals: 8,
         coingecko: 'bitcoin',
+        originCoin: ''
     },
     {
         vaultID: 1004325987,
         assetDecimals: 6,
         coingecko: 'usd-coin',
+        originCoin: ''
     },
     {
         vaultID: 1004326861,
         assetDecimals: 8,
         coingecko: 'ethereum',
+        originCoin: ''
     },
     {
         vaultID: 1004374058,
         assetDecimals: 6,
         coingecko: 'usd-coin',
+        originCoin: ''
     },
     {
         vaultID: 1097368119,
         assetDecimals: 4,
         coingecko: 'pepe',
+        originCoin: ''
     },
     {
         vaultID: 1146813039,
         assetDecimals: 6,
         coingecko: 'algorand',
+        originCoin: ''
     },
     {
         vaultID: 1170260887,
         assetDecimals: 6,
         coingecko: 'usd-coin',
+        originCoin: ''
     },
     {
         vaultID: 1170261298,
         assetDecimals: 6,
         coingecko: 'usd-coin',
+        originCoin: ''
     },
     {
         vaultID: 1190994349,
         assetDecimals: 6,
         coingecko: 'meld-gold',
+        originCoin: ''
+    },
+    {
+        vaultID: 1255766337,
+        assetDecimals: 6,
+        coingecko: 'usd-coin',
+        originCoin: ''
+    },
+    {
+        vaultID: 1202846474,
+        assetDecimals: 6,
+        coingecko: 'algorand',
+        originCoin: ''
+    },
+    {
+        vaultID: 1190874303,
+        assetDecimals: 6,
+        coingecko: 'algorand',
+        originCoin: 'mAlgo'
     }
 ];
 
