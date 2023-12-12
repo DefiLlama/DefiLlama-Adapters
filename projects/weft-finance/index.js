071756a177cc51aa72e8f3171ddb3aae398811e3
atoumbre <atoumbre@gmail.com>
2023-12-12 08:08:40 +0000

    Adding borrowed

--- projects/weft-finance/index.js
+++ projects/weft-finance/index.js
@@ -1,19 +1,49 @@
 const { sumTokens, queryAddresses } = require('../helper/chain/radixdlt');
 const { getConfig } = require('../helper/cache')
-const { get } = require('../helper/http')
+const { get, post } = require('../helper/http')
 const sdk = require('@defillama/sdk')
 
+const pools = [
+    'component_rdx1cq8mm5z49x6lyet44a0jd7zq52flrmykwwxszq65uzfn6pk3mvm0k4',
+    'component_rdx1cq7qd9vnmmu5sjlnarye09rwep2fhnq9ghj6eafj6tj08y7358z5pu',
+]
+
+async function tvl() {
+
+    let tvl = {}
+
+    const data = await queryAddresses({
+        addresses: pools
+    })
+
+    data.forEach((item) => {
+        tvl['radixdlt:' + item.fungible_resources.items[0].resource_address] = parseFloat(item.fungible_resources.items[0].amount) + parseFloat(item.details.state.fields[1].value)
+    })
+
+    console.log('tvl', tvl)
+
+    return tvl
+}
+
+
+async function borrowed() {
+
+    let borrowed = {}
+
+    const data = await queryAddresses({
+        addresses: pools
+    })
+
+    data.forEach((item) => {
+        borrowed['radixdlt:' + item.fungible_resources.items[0].resource_address] = parseFloat(item.details.state.fields[1].value)
+    })
+
+    console.log(borrowed)
+
+    return borrowed
+}
+
 module.exports = {
-    radixdlt: {
-        tvl: async (_, _1, _2, { api }) => {
-            return sumTokens({
-                owners: [
-                    'component_rdx1cpuzsp2aqkjzg504s8h8hxg57wnaqpcp9r802jjcly5x3j5nape40l',
-                    'component_rdx1cq8mm5z49x6lyet44a0jd7zq52flrmykwwxszq65uzfn6pk3mvm0k4',
-                    'component_rdx1cq7qd9vnmmu5sjlnarye09rwep2fhnq9ghj6eafj6tj08y7358z5pu'
-                ], api,
-            })
-        },
-    },
+    radixdlt: { tvl, borrowed },
     timetravel: true
 }
