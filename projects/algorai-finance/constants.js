330e51c92237fd0762d2c175b7d333e37ba24d6a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-12 14:06:08 +0100

    code refactor. Price tokens server side

--- projects/algorai-finance/constants.js
+++ projects/algorai-finance/constants.js
@@ -3,107 +3,27 @@
  * @type {[{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number},{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number},{vaultID: number, stable: boolean, assetDecimals: number, depositAssetID: number, originCoin: string}]}
  */
 const vaults = [
-    // v1.0
-    {
-        vaultID: 878144513,
-        assetDecimals: 6,
-        coingecko: 'algorand',
-        originCoin: ''
-    },
-    {
-        vaultID: 878140320,
-        assetDecimals: 6,
-        coingecko: 'usd-coin',
-        originCoin: ''
-    },
-    // v1.1
-    {
-        vaultID: 919531421,
-        assetDecimals: 6,
-        coingecko: 'algorand',
-        originCoin: ''
-    },
-    {
-        vaultID: 919539218,
-        assetDecimals: 6,
-        coingecko: 'usd-coin',
-        originCoin: ''
-    },
-    {
-        vaultID: 1004309470,
-        assetDecimals: 8,
-        coingecko: 'bitcoin',
-        originCoin: ''
-    },
-    {
-        vaultID: 1004325987,
-        assetDecimals: 6,
-        coingecko: 'usd-coin',
-        originCoin: ''
-    },
-    {
-        vaultID: 1004326861,
-        assetDecimals: 8,
-        coingecko: 'ethereum',
-        originCoin: ''
-    },
-    {
-        vaultID: 1004374058,
-        assetDecimals: 6,
-        coingecko: 'usd-coin',
-        originCoin: ''
-    },
-    {
-        vaultID: 1097368119,
-        assetDecimals: 4,
-        coingecko: 'pepe',
-        originCoin: ''
-    },
-    {
-        vaultID: 1146813039,
-        assetDecimals: 6,
-        coingecko: 'algorand',
-        originCoin: ''
-    },
-    {
-        vaultID: 1170260887,
-        assetDecimals: 6,
-        coingecko: 'usd-coin',
-        originCoin: ''
-    },
-    {
-        vaultID: 1170261298,
-        assetDecimals: 6,
-        coingecko: 'usd-coin',
-        originCoin: ''
-    },
-    {
-        vaultID: 1190994349,
-        assetDecimals: 6,
-        coingecko: 'meld-gold',
-        originCoin: ''
-    },
-    {
-        vaultID: 1255766337,
-        assetDecimals: 6,
-        coingecko: 'usd-coin',
-        originCoin: ''
-    },
-    {
-        vaultID: 1202846474,
-        assetDecimals: 6,
-        coingecko: 'algorand',
-        originCoin: ''
-    },
-    {
-        vaultID: 1190874303,
-        assetDecimals: 6,
-        coingecko: 'algorand',
-        originCoin: 'mAlgo'
-    }
+  // v1.0
+  878144513,
+  878140320,
+  // v1.1
+  919531421,
+  919539218,
+  1004309470,
+  1004325987,
+  1004326861,
+  1004374058,
+  1097368119,
+  1146813039,
+  1170260887,
+  1170261298,
+  1190994349,
+  1255766337,
+  1202846474,
+  1190874303,
 ];
 
 
 module.exports = {
-    vaults,
+  vaults,
 };
--- projects/algorai-finance/index.js
+++ projects/algorai-finance/index.js
@@ -1,9 +1,5 @@
-const sdk = require('@defillama/sdk')
-
-const {
-  vaults,
-} = require("./constants");
-const { getAppGlobalState } = require("../helper/chain/algorand");
+const {  vaults, } = require("./constants");
+const { sumTokens } = require("../helper/chain/algorand");
 const axios = require("axios");
 
 async function fetchMAlgoToAlgoBalance() {
@@ -16,22 +12,7 @@ async function fetchMAlgoToAlgoBalance() {
  * @returns {Promise<{"[usdtAddress]": *}>}
  */
 async function tvl() {
-  const balances = {}
-  const promises = vaults.map(async (vault) => {
-    const state = await getAppGlobalState(vault.vaultID);
-    let totalTvl = state.vtv ? state.vtv / 10 ** vault.assetDecimals : 0;
-    if (vault.originCoin !== '') {
-      const mAlgoToAlgo = await fetchMAlgoToAlgoBalance();
-      if (mAlgoToAlgo && mAlgoToAlgo.algo) {
-        // Calculate mAlgo to algo price from messina
-        totalTvl = totalTvl * Number(mAlgoToAlgo.algo)
-      }
-    }
-    sdk.util.sumSingleBalance(balances, vault.coingecko, totalTvl)
-  });
-  await Promise.all(promises)
-
-  return balances
+  return sumTokens({ owners: vaults, blacklistedTokens: ['1145958888', '1145959061', ''] })
 }
 
 module.exports = {
