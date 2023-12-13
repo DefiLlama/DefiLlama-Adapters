a220be7ece4c875b02399c7daea328436f8192af
Damian <damian.ovidiu27@gmail.com>
2023-12-13 17:46:13 +0200

    onedex simple app staking and boosted staking

--- projects/onedex/index.js
+++ projects/onedex/index.js
@@ -1,13 +1,4 @@
 const { sumTokensExport } = require("../helper/sumTokens");
-const axios = require("axios");
-
-async function farmsValue() {
-  const initialResult = await axios.get(
-    "https://api.onedex.app/value-in-stake"
-  );
-  const tvs = initialResult.total_value;
-  return tvs;
-}
 
 module.exports = {
   timetravel: false,
@@ -15,6 +6,11 @@ module.exports = {
     tvl: sumTokensExport({
       owner: "erd1qqqqqqqqqqqqqpgqqz6vp9y50ep867vnr296mqf3dduh6guvmvlsu3sujc",
     }),
-    staking: farmsValue,
+    staking: sumTokensExport({
+      owner: "erd1qqqqqqqqqqqqqpgql9z9vm8d599ya2r9seklpkcas6qmude4mvlsgrj7hv",
+    }),
+    staking2: sumTokensExport({
+      owner: "erd1qqqqqqqqqqqqqpgq8nlmvjm8gum6y2kqe0v296kgu8cm4jlemvlsays3ku",
+    }),
   },
 };
