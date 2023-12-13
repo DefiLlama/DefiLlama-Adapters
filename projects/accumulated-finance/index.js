50169841e0244fcc40e077e84e92af9aba71eda6
Evgeny Rozanov <e.rosanov@gmail.com>
2023-12-13 22:10:18 +0400

    accumulated-finance

--- projects/accumulated-finance/index.js
+++ projects/accumulated-finance/index.js
@@ -1,6 +1,11 @@
 const sdk = require('@defillama/sdk');
 const config = require('./config.json');
 
+const curvePoolAbi = require('./curvePoolAbi.json');
+
+const { staking } = require('../helper/staking');
+const { sumTokens2 } = require('../helper/unwrapLPs');
+
 const chains = {
   ethereum: 1,
   arbitrum: 42161,
@@ -8,9 +13,8 @@ const chains = {
 };
 
 async function getLsdPoolsTvl(api, filteredPools) {
-  const calls = filteredPools.map(pool => ({
-    target: pool.stAddress
-  }));
+
+  const calls = filteredPools.map(pool => ({ target: pool.stToken }));
 
   const balanceOfResults = await sdk.api.abi.multiCall({
     abi: 'erc20:totalSupply',
@@ -20,7 +24,7 @@ async function getLsdPoolsTvl(api, filteredPools) {
 
   const balances = {};
   balanceOfResults.output.forEach(balanceOf => {
-    const pool = filteredPools.find(item => item.stAddress === balanceOf.input.target);
+    const pool = filteredPools.find(item => item.stToken === balanceOf.input.target);
     if (pool) {
       balances[pool.baseToken] = balanceOf.output;
     }
@@ -36,5 +40,12 @@ Object.keys(chains).forEach(chain => {
       const filteredPools = config.lsdPools.filter(item => item.chainID === chainId);
       return await getLsdPoolsTvl(api, filteredPools);
     }
-  };
+  }
 });
+
+module.exports['ethereum'].staking = staking(
+  '0x4424b4a37ba0088d8a718b8fc2ab7952c7e695f5',
+  ['0xdf4ef6ee483953fe3b84abd08c6a060445c01170', '0x7AC168c81F4F3820Fa3F22603ce5864D6aB3C547'],
+  'ethereum',
+  '0xdf4ef6ee483953fe3b84abd08c6a060445c01170'
+);
