7336d5fbcded7d762d9f68ccb30ebba47402b287
jscrui <juandesndr@gmail.com>
2023-12-13 17:22:25 -0300

    Add fix to correctly display TVL

--- projects/vault-tech/index.js
+++ projects/vault-tech/index.js
@@ -7,9 +7,10 @@ const staking60days = "0xf671d40a8011e7868c0a7e4ad2908bdc41c519cb"
 const staking90days = "0xcD2d59d9597e782858483f3bC78FB7A9f47Df3ae"
 
 module.exports = {
-  ethereum: {    
-    tvl: () => 0,
-    staking: sumTokensExport({ owners: [staking30days, staking60days, staking90days], tokens: [vault] }),    
+  misrepresentedTokens: true,
+  ethereum: {      
+    staking: sumTokensExport({ owners: [staking30days, staking60days, staking90days], tokens: [vault] }),
+    tvl: sumTokensExport({ owners: [staking30days, staking60days, staking90days], tokens: [vault] }),        
   },
   methodology:
     "Calculate the TVL of Vault-Tech staking protocol by getting the amount of $VAULT staked in the 3 pools",
