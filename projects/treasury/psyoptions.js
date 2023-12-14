6652555fe9e4ee1d0b8c2699161801bc880f4c5a
Define101 <Lovelovenow@protonmail.com>
2023-12-14 11:44:23 +0000

    fix psyoptions

--- projects/treasury/psyoptions.js
+++ projects/treasury/psyoptions.js
@@ -3,12 +3,12 @@ const { treasuryExports } = require("../helper/treasury");
 module.exports = treasuryExports({
   solana: {
     owners: [
-      'CcNUW7KDCdaUY6rNqYJBmTKYn66RjYTVyPUqCNEiALdp',
+      'ENSuopuKKCDgdmT6dXHqJSjeDjUoLXUNikr33e21bNtp',
       'CyDnoEMVuf21v23bxoS2wXxPdCvRR2yFLfymegMH1WY4',
-      'E4tuwwYvmB9XWKTmhxeRmywhMmttyDLtMok6jQVAbDLG'
+      'E4tuwwYvmB9XWKTmhxeRmywhMmttyDLtMok6jQVAbDLG',
     ],
     ownTokens: [
-      'PsyFiqqjiv41G7o5SMRzDJCu4psptThNR2GtfeGHfSq', // PSY
+      'PsyFiqqjiv41G7o5SMRzDJCu4psptThNR2GtfeGHfSq', // PSY fffff
     ]
   },
 })
