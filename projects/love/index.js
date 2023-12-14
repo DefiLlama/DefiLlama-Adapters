98c7ddc88bcc3ed69959adbf8a8c934a67719707
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-14 19:59:03 +0100

    code refactor

--- projects/love/index.js
+++ projects/love/index.js
@@ -1,4 +1,4 @@
-const LIQUIDITY_ON_ETH = [
+/* const LIQUIDITY_ON_ETH = [
   "0xb55EE890426341FE45EE6dc788D2D93d25B59063", // LOVE
   "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // WETH
 ];
@@ -119,17 +119,26 @@ async function tvl_BSC(_, _1, _2, { api }) {
   ]);
 
   api.add(LIQUIDITY_ON_ETH[0], [stakingLockedTokens]);
-}
+} */
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology:
     "The liquidity on these three pools + the tokens staked on all three chains (PulseChain, Ethereum, and Binance Smart Chain)",
   start: 1000235,
-  ethereum: {
-    tvl: tvl_ETH,
-    staking: stakingETH,
-  },
-  pulse: { tvl: tvl_PLS, staking: stakingPLS },
-  bsc: { tvl: tvl_BSC, staking: stakingBSC },
 };
+
+const config = {
+  ethereum: { staking: '0xE639E9DC0E302f5dB025713009868c8adE4Ced26' },
+  pulse: { staking: '0xDd91E607C919Db74e18C2845e4cfb22793c30b2f' },
+  bsc: { staking: '0x1781e00780AfD93a03Bf5f9dED088a8578cE9B09' },
+}
+
+Object.keys(config).forEach(chain => {
+  const { staking} = config[chain]
+  module.exports[chain] = {
+    tvl: () => ({}),
+    staking: async (_, _b, _cb, { api, }) => {
+      const stakingToken = await api.call({  abi: 'address:stakingToken', target: staking})
+      return api.sumTokens({ owner: staking, tokens: [stakingToken]})
+    }
+  }
+})
