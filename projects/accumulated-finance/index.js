3194fe0a7e6bfbbe1e5cced18e537a61d46d5607
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-14 23:44:42 +0100

    code refactor

--- projects/accumulated-finance/index.js
+++ projects/accumulated-finance/index.js
@@ -1,43 +1,41 @@
-const sdk = require('@defillama/sdk');
-const config = require('./config.json');
-
-async function getLsdPoolsTvl(api, filteredPools) {
-
-  // baseToken - a token staked into Accumulated Finance liquid staking
-  // stToken - a liquid staking token
-  const calls = filteredPools.map(pool => ({ target: pool.stToken }));
-
-  // calculate total supply of existing LST tokens
-  const balanceOfResults = await sdk.api.abi.multiCall({
-    abi: 'erc20:totalSupply',
-    chain: api.chain,
-    calls
-  });
-
-  const balances = {};
-  balanceOfResults.output.forEach(balanceOf => {
-    const pool = filteredPools.find(item => item.stToken === balanceOf.input.target);
-    if (pool) {
-      balances[pool.baseToken] = balanceOf.output;
-    }
-  });
-  return balances;
+const ADDRESSES = require('../helper/coreAssets.json')
+const config = {
+  "ethereum": {
+    "baseToken": "0xdf4ef6ee483953fe3b84abd08c6a060445c01170",
+    "stToken": "0x7AC168c81F4F3820Fa3F22603ce5864D6aB3C547"
+  },
+  "arbitrum": {
+    "baseToken": "0xdf4ef6ee483953fe3b84abd08c6a060445c01170",
+    "stToken": "0x7AC168c81F4F3820Fa3F22603ce5864D6aB3C547"
+  },
+  "velas": {
+    "baseToken": "0x8c543aed163909142695f2d2acd0d55791a9edb9",
+    "LST": "0x3557371afed82dd683de278924bd0e1a790a3c49"
+  }
 }
 
-const chains = config.chains;
-
 module.exports = {
-  timetravel: false,
   methodology:
     "We aggregated liquid staking tokens issued by Accumulated Finance",
 }
 
-Object.keys(config.chains).forEach(chain => {
+
+Object.keys(config).forEach(chain => {
+  const { LST, stToken, baseToken } = config[chain]
   module.exports[chain] = {
-    tvl: async (timestamp, block, chainBlocks, { api }) => {
-      const chainId = chains[api.chain];
-      const filteredPools = config.lsdPools.filter(item => item.chainID === chainId);
-      return await getLsdPoolsTvl(api, filteredPools);
+    tvl: async (_, _b, _cb, { api, }) => {
+      if (!LST)
+        return {}
+      const supply = await api.call({ abi: 'uint256:totalSupply', target: LST })
+      api.add(ADDRESSES.null, supply)
+      return api.getBalances()
+    },
+    staking: async (_, _b, _cb, { api, }) => {
+      if (!stToken || !baseToken)
+        return {}
+      const supply = await api.call({ abi: 'uint256:totalSupply', target: stToken })
+      api.add(baseToken, supply)
+      return api.getBalances()
     }
   }
-});
+})
