b396bf04412b0af972306cf639ac97e1420b8d8c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-14 19:39:26 +0100

    bugfix

--- projects/pharaoh-exchange/index.js
+++ projects/pharaoh-exchange/index.js
@@ -1,5 +1,5 @@
 const { uniV3Export } = require('../helper/uniswapV3')
 
 module.exports = uniV3Export({
-  avax: { factory: '0xAAA32926fcE6bE95ea2c51cB4Fcb60836D320C42', fromBlock: 3876459, },
+  avax: { factory: '0xAAA32926fcE6bE95ea2c51cB4Fcb60836D320C42', fromBlock: 38764597, },
 })
