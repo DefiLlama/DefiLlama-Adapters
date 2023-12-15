d13a1d0431e5aaad0f745cc4c4cf7cc0939605cc
allush <al.lushnikov@yandex.ru>
2023-12-15 16:35:14 +0000

    symbiosis adds new portal on zksync

--- projects/symbiosis-finance/config.js
+++ projects/symbiosis-finance/config.js
@@ -131,6 +131,7 @@ module.exports = {
       ],
       holders: [
         '0x39dE19C9fF25693A2311AAD1dc5C790194084A39', // portal v2
+        '0x4f5456d4d0764473DfCA1ffBB8524C151c4F19b9', // new portal v2
         '0x97b99f47b086a074f214f4A62A3b041599726DC2', // portal Teleport
       ]
     },
