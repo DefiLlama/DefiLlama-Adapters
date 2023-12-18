3547eb7349467e70e6a143e728d456e93cfa71f1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-18 02:03:30 +0100

    code refactor

--- projects/jibswap/index.js
+++ projects/jibswap/index.js
@@ -1,11 +1,3 @@
-const sdk = require('@defillama/sdk')
-const { uniTvlExport } = require('../helper/calculateUniTvl.js')
+const { uniTvlExport } = require('../helper/unknownTokens')
 
-const FACTORY = '0x4BBdA880C5A0cDcEc6510f0450c6C8bC5773D499'
-
-module.exports = {
-  jibchain: {
-    tvl: uniTvlExport(FACTORY, 'jibchain')
-  },
-  methodology: "Includes liquidity on all the pools on the uniswap fork",
-}
+module.exports = uniTvlExport('jbc', '0x4BBdA880C5A0cDcEc6510f0450c6C8bC5773D499')
