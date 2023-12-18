fa21d59f6853739f7664df58d95650fd28790d70
ultrasecr.eth <241804+ultrasecreth@users.noreply.github.com>
2023-12-18 10:38:56 +0000

    Extract out

--- projects/contango-v2/index.js
+++ projects/contango-v2/index.js
@@ -1,26 +1,38 @@
-const { getLogs } = require("../helper/cache/getLogs")
+const { getLogs } = require("../helper/cache/getLogs");
+
+const CONTANGO_PROXY = "0x6Cae28b3D09D8f8Fc74ccD496AC986FC84C0C24E";
 
 const config = {
   arbitrum: {
-    contango: '0x6Cae28b3D09D8f8Fc74ccD496AC986FC84C0C24E',
+    contango: CONTANGO_PROXY,
     fromBlock: 120103720,
+    grapUrl: "https://api.thegraph.com/subgraphs/name/contango-xyz/v2-arbitrum",
   },
+};
+
+Object.keys(config).forEach((chain) => {
+  const { contango, fromBlock } = config[chain];
+  module.exports[chain] = {
+    tvl: async (_1, _2, _3, { api }) => vaultTvl(api, contango, fromBlock),
+  };
+});
+
+async function vaultTvl(api, contango, fromBlock) {
+  const logs = await getLogs({
+    api,
+    target: contango,
+    eventAbi: abis.InstrumentCreated,
+    onlyArgs: true,
+    fromBlock,
+  });
+  const vault = await api.call({ abi: "address:vault", target: contango });
+  return api.sumTokens({
+    owner: vault,
+    tokens: logs.flatMap((log) => [log.base, log.quote]),
+  });
 }
 
-Object.keys(config).forEach(chain => {
-  const { contango, fromBlock } = config[chain]
-  module.exports[chain] = {
-    tvl: async (timestamp, _b, chainBlocks, { api }) => {
-      const logs = await getLogs({
-        api,
-        target: contango,
-        eventAbi: "event InstrumentCreated(bytes16 indexed symbol, address base, address quote)",
-        onlyArgs: true,
-        fromBlock,
-      })
-      const vault = await api.call({  abi: 'address:vault', target: contango})
-      return api.sumTokens({ owner: vault, tokens: logs.map(log => [log.base, log.quote]).flat()})
-      
-    }
-  }
-})
+const abis = {
+  InstrumentCreated:
+    "event InstrumentCreated(bytes16 indexed symbol, address base, address quote)",
+};
