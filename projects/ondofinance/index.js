75b6d75fd3f63427e6bc714aa78fe160d3e42ccd
Lilian Ludford <lilian@ondo.finance>
2023-12-19 13:42:00 -0500

    adding ousg + usdy on solana

--- projects/ondofinance/index.js
+++ projects/ondofinance/index.js
@@ -1,4 +1,4 @@
-const sdk = require('@defillama/sdk');
+const sdk = require("@defillama/sdk");
 
 module.exports = {
   methodology: "Sums Ondo's fund supplies.",
@@ -7,17 +7,21 @@ module.exports = {
 
 const config = {
   ethereum: {
-    OUSG: '0x1B19C19393e2d034D8Ff31ff34c81252FcBbee92',
-    USDY: '0x96F6eF951840721AdBF46Ac996b59E0235CB985C',
-    USDYc: '0xe86845788d6e3e5c2393ade1a051ae617d974c09',
+    OUSG: "0x1B19C19393e2d034D8Ff31ff34c81252FcBbee92",
+    USDY: "0x96F6eF951840721AdBF46Ac996b59E0235CB985C",
+    USDYc: "0xe86845788d6e3e5c2393ade1a051ae617d974c09",
   },
   polygon: {
-    OUSG: '0xbA11C5effA33c4D6F8f593CFA394241CfE925811',
+    OUSG: "0xbA11C5effA33c4D6F8f593CFA394241CfE925811",
   },
   mantle: {
     USDY: "0x5bE26527e817998A7206475496fDE1E68957c5A6",
-  }
-}
+  },
+  solana: {
+    OUSG: "i7u4r16TcsJTgq1kAG8opmVZyVnAKBwLKu6ZPMwzxNc",
+    USDY: "A1KLoBrKBde8Ty9qtNQUtq3C2ortoC3u7twggz7sEto6",
+  },
+};
 
 Object.keys(config).forEach((chain) => {
   let fundsMap = config[chain];
@@ -26,12 +30,15 @@ Object.keys(config).forEach((chain) => {
   module.exports[chain] = {
     tvl: async (_, _b, _cb, { api }) => {
       const balances = {};
-      const supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: fundAddresses });
+      const supplies = await api.multiCall({
+        abi: "erc20:totalSupply",
+        calls: fundAddresses,
+      });
 
       supplies.forEach((supply, index) => {
         const tokenAddress = fundAddresses[index];
-        const key = `${chain}:${tokenAddress}`; 
-        balances[key] = supply; 
+        const key = `${chain}:${tokenAddress}`;
+        balances[key] = supply;
       });
 
       return balances;
