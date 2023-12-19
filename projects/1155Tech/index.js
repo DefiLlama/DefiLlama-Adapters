b16bf56a87ae615fba8723ee9293738e1fb1b5b1
Abdul Qawi Bhoira <aqawi72@gmail.com>
2023-12-19 17:36:37 +0530

    1155tech tvl

--- projects/1155Tech/index.js
+++ projects/1155Tech/index.js
@@ -2,21 +2,21 @@ const TOKEN_CONTRACT_NOTE = '0x4e71A2E537B7f9D9413D3991D37958c0b5e1e503';
 const MARKET_1155TECH_CONTRACT = '0x33b77fAf955Ed3eDAf939ae66C4D7a2D78bc30C6';
 
 async function tvl(_, _1, _2, { api }) {
-  const balance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: TOKEN_CONTRACT_NOTE,
-    params: [MARKET_1155TECH_CONTRACT],
-  });
+    const balance = await api.call({
+        abi: 'erc20:balanceOf',
+        target: TOKEN_CONTRACT_NOTE,
+        params: [MARKET_1155TECH_CONTRACT],
+    });
 
-  api.add(TOKEN_CONTRACT_NOTE, balance)
+    api.add(TOKEN_CONTRACT_NOTE, balance)
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  methodology: 'the amount of Note tokens that are in the 1155Tech contract',
-  start: 7280880,
-  canto: {
-    tvl,
-  }
+    timetravel: true,
+    misrepresentedTokens: false,
+    methodology: 'the amount of Note tokens that are in the 1155Tech contract',
+    start: 7280880,
+    canto: {
+        tvl
+    }
 };
