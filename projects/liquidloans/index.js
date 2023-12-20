53fb9897b1692a22ed2ed470c2fa8b9c689168ca
Antonio Jose Lopez Morillo <antoniojlopez@gmail.com>
2023-12-19 20:21:51 -0400

    Implementation corrections for LiquidLoans

--- projects/liquidloans/index.js
+++ projects/liquidloans/index.js
@@ -2,33 +2,25 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const sdk = require("@defillama/sdk");
 const { staking } = require("../helper/staking.js");
 
-const PLS_ADDRESS = ADDRESSES.null;
-// VaultManager holds total system collateral (deposited PLS)
 const VAULT_MANAGER_ADDRESS = "0xD79bfb86fA06e8782b401bC0197d92563602D2Ab";
 const STAKING_ADDRESS = "0x853F0CD4B0083eDf7cFf5Ad9A296f02Ffb71C995";
 const LOAN_ADDRESS = "0x9159f1D2a9f51998Fc9Ab03fbd8f265ab14A1b3B"
 
-async function tvl(_, block) {
+async function tvl(timestamp, block, chainBlocks, { api }) {
 
-  const vaultPlsTvl = (
-    await sdk.api.abi.call({
-      target: VAULT_MANAGER_ADDRESS,
-      abi: "uint256:getEntireSystemColl",
-      block,
-    })
-  ).output;
+  const systemCollateral = await api.call({
+    abi: 'function getEntireSystemColl() view returns (uint256)',
+    target: VAULT_MANAGER_ADDRESS
+  });
 
-  return {
-    [PLS_ADDRESS]: vaultPlsTvl,
-  };
+  return api.add(ADDRESSES.pulse.WPLS, systemCollateral)
 }
 
 module.exports = {
   timetravel: true,
-  start: 18971008,
+  start : 18971003,
   pulse: {
     tvl,
-    staking: staking(STAKING_ADDRESS, LOAN_ADDRESS, "pulse")
-  }
-  
+    staking: staking(STAKING_ADDRESS, LOAN_ADDRESS, "pulse")    
+  },
 };
