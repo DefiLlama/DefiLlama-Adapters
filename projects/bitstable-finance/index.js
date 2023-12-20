6df4c690935ae72edce5437b2532a299d1de2eb0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-20 18:23:29 +0100

    code refactor

--- projects/bitstable-finance/index.js
+++ projects/bitstable-finance/index.js
@@ -4,19 +4,13 @@ const { sumTokensExport } = require("../helper/unwrapLPs");
 const owner = "0x103dd1184599c7511a3016E0a383E11F84AE7173";
 const tokens = {
   ethereum: [ADDRESS.ethereum.USDT],
-  bsc: [ADDRESS.bsc.FDUSD],
+  bsc: ['0xc5f0f7b66764F6ec8C8Dff7BA683102295E16409'],
 };
 
-function getTvls() {
-  return Object.keys(tokens).reduce((prev, next) => {
-    prev[next] = {
-      tvl: sumTokensExport({ owner, tokens: tokens[next] }),
-    };
-    return prev;
-  }, {});
-}
+Object.keys(tokens).map((chain) => {
+  module.exports[chain] = {
+    tvl: sumTokensExport({ owner, tokens: tokens[chain] }),
+  };
+});
 
-module.exports = getTvls();
 module.exports.methodology = "Staking tokens via BitStable counts as TVL";
-module.exports.misrepresentedTokens = true;
-module.exports.hallmarks = [[1651881600, "UST depeg"]];
