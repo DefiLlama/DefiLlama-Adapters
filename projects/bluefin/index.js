d7e9850c009af6ac0457712edaff4ead1f3e5ced
Hemant Singhal <hemant@seed.im>
2023-12-20 10:22:46 +0000

    Removing Arbitrum from the TVL

--- projects/bluefin/index.js
+++ projects/bluefin/index.js
@@ -1,13 +1,8 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokensExport } = require('../helper/unwrapLPs')
 const sui = require("../helper/chain/sui");
 
 const SUI_BANK_ID =
   "0x39c65abefaee0a18ffa0e059a0074fcc9910216fa1a3550aa32c2e0ec1c03043";
-
-const Arbitrum_Config = {
-  "endpoint": "0x52b5471d04487fb85B39e3Ae47307f115fe8733F",
-}
 
 async function suiTvl() {
   const { api } = arguments[3];
@@ -20,12 +15,6 @@ async function suiTvl() {
 }
 
 module.exports = {
-  arbitrum: {
-    tvl: sumTokensExport({
-      owners: [Arbitrum_Config.endpoint],
-      tokens: [ADDRESSES.arbitrum.USDC],
-    })
-  },
   sui: {
     tvl: suiTvl
   },
