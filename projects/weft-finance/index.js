3b68b9174e1ab3ac620a952ab4778a43cd59bd74
atoumbre <atoumbre@gmail.com>
2023-12-20 11:54:27 +0000

    Adding LSULP Lending pool (#8440)

--- projects/weft-finance/index.js
+++ projects/weft-finance/index.js
@@ -3,6 +3,7 @@ const { queryAddresses } = require('../helper/chain/radixdlt');
 const pools = [
   'component_rdx1cq8mm5z49x6lyet44a0jd7zq52flrmykwwxszq65uzfn6pk3mvm0k4',
   'component_rdx1cq7qd9vnmmu5sjlnarye09rwep2fhnq9ghj6eafj6tj08y7358z5pu',
+  'component_rdx1cr5cnuzre63whe4yhnemeyvjj2yaq7tqg0j6q4xxtcyajf8rv0hw26',
 ]
 
 async function tvl(_, _b, _cb, { api, }) {
