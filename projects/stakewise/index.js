35bd486b3d5d8883a0abf68577713f76ee023f8d
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2023-12-20 16:59:22 +0200

    Fix stakewise ETH and GNO TVL

--- projects/stakewise/index.js
+++ projects/stakewise/index.js
@@ -1,60 +1,43 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const sdk = require('@defillama/sdk');
-const BigNumber = require('bignumber.js')
-const { getLogs } = require('../helper/cache/getLogs')
+const ADDRESSES = require("../helper/coreAssets.json");
+const sdk = require("@defillama/sdk");
+const BigNumber = require("bignumber.js");
+const axios = require("axios");
 
-const wethAddress = ADDRESSES.ethereum.WETH
+const wethAddress = ADDRESSES.ethereum.WETH;
 
-async function tvl(timestamp, block, _1, { api }) {
-  const supply = await sdk.api.erc20.totalSupply({
-    target: '0xFe2e637202056d30016725477c5da089Ab0A043A',
-    block
-  })
-
-  const solosValidators = await getLogs({
-    target: '0xEadCBA8BF9ACA93F627F31fB05470F5A0686CEca',
-    topic: 'ValidatorRegistered(bytes32,bytes,uint256,address)',
-    fromBlock: 11726299,
-    api
-  })
-  const ethOnValidators = BigNumber(solosValidators.length).times(32e18)
-  const vaults = await getLogs({
-    target: '0x3a0008a588772446f6e656133c2d5029cc4fc20e',
-    topic: 'VaultAdded(address,address)',
-    fromBlock: 18470078,
-    api
-  })
-  const assets = await api.multiCall({
-    calls: vaults.map(v=>({target:"0x"+v.topics[2].slice(26)})),
-    abi: "uint256:totalAssets"
-  })
-
+async function ethereumTvl(timestamp, block, _1, _2) {
+  const tvl = await axios.get("https://mainnet-api.stakewise.io/api/tvl");
   return {
-    [wethAddress]: ethOnValidators.plus(supply.output).plus(assets.reduce((sum, b)=>sum.plus(b), BigNumber(0))).toFixed(0)
-  }
+    [wethAddress]: new BigNumber(tvl)
+  };
 }
 
-async function xdaiTvl(timestamp, ethBlock, { xdai: block }) {
-  const chain = "xdai"
-  const supply = await sdk.api.erc20.totalSupply({
-    target: '0xA4eF9Da5BA71Cc0D2e5E877a910A37eC43420445',
+async function gnoTvl(timestamp, ethBlock, { xdai: block }) {
+  const chain = "xdai";
+  const sGNO = await sdk.api.erc20.totalSupply({
+    target: "0xA4eF9Da5BA71Cc0D2e5E877a910A37eC43420445",
     block,
     chain
-  })
+  });
+
+  const rGNO = await sdk.api.erc20.totalSupply({
+    target: "0x6aC78efae880282396a335CA2F79863A1e6831D4",
+    block,
+    chain
+  });
 
   return {
-    [ADDRESSES.ethereum.GNO]: supply.output
-  }
+    [ADDRESSES.ethereum.GNO]: new BigNumber(sGNO.output).plus(new BigNumber(rGNO.output))
+  };
 }
-
 
 
 module.exports = {
-  methodology: 'Counts ETH staked',
+  methodology: "Counts ETH and GNO staked",
   ethereum: {
-    tvl,
+    tvl: ethereumTvl
   },
-  xdai:{
-    tvl: xdaiTvl
+  xdai: {
+    tvl: gnoTvl
   }
-}
+};
