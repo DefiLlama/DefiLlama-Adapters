cddba3f0db731d7e5ec342c66bb9a4a789fa16f9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-21 15:51:34 +0100

    code refactor

--- projects/flamincome.js
+++ projects/flamincome.js
@@ -36,6 +36,3 @@ module.exports = {
     tvl,
   }
 }
-
-
-
--- projects/kelp-dao/index.js
+++ projects/kelp-dao/index.js
@@ -1,23 +1,11 @@
 const DEPOSIT_POOL = "0x036676389e48133B63a802f8635AD39E752D375D";
-const ETHx = "0xA35b1B31Ce002FBF2058D22F30f95D405200A15b";
-const stETH = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84";
-
-async function getTotalAssetDeposits(api, tokenAddress) {
-  return await api.call({
-    abi: "function getTotalAssetDeposits(address) external view returns (uint256)",
-    target: DEPOSIT_POOL,
-    params: [tokenAddress],
-  });
-}
 
 async function tvl(_, _1, _2, { api }) {
-  const tokens = [ETHx, stETH];
-  const depositPromises = tokens.map((token) =>
-    getTotalAssetDeposits(api, token)
-  );
-  const depositValues = await Promise.all(depositPromises);
-
-  api.addTokens(tokens, depositValues);
+  const config = await api.call({  abi: 'address:lrtConfig', target: DEPOSIT_POOL})
+  const tokens= await api.call({  abi: 'address[]:getSupportedAssetList', target: config})
+  const bals = await api.multiCall({  abi: 'function getTotalAssetDeposits(address) external view returns (uint256)', calls: tokens, target: DEPOSIT_POOL})
+  api.addTokens(tokens, bals);
+  return api.getBalances()
 }
 
 module.exports = {
