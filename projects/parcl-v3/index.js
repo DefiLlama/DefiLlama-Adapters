0663e14ad2750fc6a0c238aa091a0d79e4aca639
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-22 01:58:11 +0100

    minor fix

--- projects/parcl-v3/index.js
+++ projects/parcl-v3/index.js
@@ -1,6 +1,7 @@
 const { sumTokens2, } = require('../helper/solana')
 
 async function tvl() {
+  // https://docs.parcl.co/addresses
   return sumTokens2({ tokenAccounts: ['Ai9AuTfGncuFxEknjZT4HU21Rkv98M1QyXpbW9Xct6LK'] })
 }
 
