d273e9daa13d681007217be3b273664a9aaafe7b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-22 16:10:06 +0100

    code refactor

--- projects/starknet/index.js
+++ projects/starknet/index.js
@@ -1,11 +1,11 @@
+const { get } = require("../helper/http");
 const { sumTokens2 } = require("../helper/unwrapLPs");
-const axios = require("axios");
 
 async function tvl(_, _b, _c, { api }) {
-  const mapping = await axios.get(
+  const mapping = await get(
     `https://raw.githubusercontent.com/starknet-io/starknet-addresses/master/bridged_tokens/mainnet.json`
   );
-  const tokensAndOwners = mapping.data.map((t) => [
+  const tokensAndOwners = mapping.map((t) => [
     t.l1_token_address == "0x0000000000000000000000000000000000455448"
       ? "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
       : t.l1_token_address,
