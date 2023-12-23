eb0f923c885552104080058e839d684aa039edeb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-23 17:53:20 +0100

    code refactor

--- projects/skydrome/index.js
+++ projects/skydrome/index.js
@@ -1,50 +1,12 @@
 const { getUniTVL } = require('../helper/unknownTokens')
+const sdk = require('@defillama/sdk')
 
 const V1_FACTORY = '0x2516212168034b18a0155FfbE59f2f0063fFfBD9'
 const V2_FACTORY = '0x74B8738862E4814C6E6D6e0202F8386685ca7B9D'
 
-async function mergePromises(elements) {
-  let results = await Promise.all(elements);
-  return merge(results);
-}
-
-function merge(elements) {
-  return elements.reduce((combined, current) => {
-    for (const [id, amount] of Object.entries(current)) {
-      if (!combined[id]) {
-        combined[id] = 0;
-      }
-      combined[id] += amount;
-    }
-
-    return combined;
-  }, {});
-}
-
-function getCombinedTVL() {
-  return async (_, _b, cb, { api, chain } = {}) => {
-    const tvlV1 = getUniTVL({
-      factory: V1_FACTORY,
-      useDefaultCoreAssets: true,
-      hasStablePools: true,
-    })(_, _b, cb, {api, chain})
-    const tvlV2 = getUniTVL({
-      factory: V2_FACTORY,
-      useDefaultCoreAssets: true,
-      hasStablePools: true,
-    })(_, _b, cb, {api, chain})
-
-    return await mergePromises([
-      tvlV1,
-      tvlV2
-    ])
-  }
-}
-
+const tvl = sdk.util.sumChainTvls([V1_FACTORY, V2_FACTORY].map((factory) => getUniTVL({ factory, useDefaultCoreAssets: true, hasStablePools: true, })))
 
 module.exports = {
   misrepresentedTokens: true,
-  scroll: {
-    tvl: getCombinedTVL()
-  },
+  scroll: { tvl },
 }
