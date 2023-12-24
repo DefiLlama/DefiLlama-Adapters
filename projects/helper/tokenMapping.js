a74119c3c2f1fcdc27e1cae89c9258337d64953d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-24 22:30:21 +0100

    track jbc USDT

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -136,7 +136,10 @@ const fixBalancesTokens = {
     'cxb49d82c46be6b61cab62aaf9824b597c6cf8a25d': { coingeckoId: 'binance-usd', decimals: 18 },
     'cx5b5a03cb525a1845d0af3a872d525b18a810acb0': { coingeckoId: 'bitcoin-bep2', decimals: 18 },
     'cx288d13e1b63563459a2ac6179f237711f6851cb5': { coingeckoId: 'ethereum', decimals: 18 },
-  }
+  },
+  jbc: {
+    [ADDRESSES.jbc.USDT]: { coingeckoId: 'tether', decimals: 18 },
+  },
 }
 
 ibcChains.forEach(chain => fixBalancesTokens[chain] = { ...ibcMappings, ...(fixBalancesTokens[chain] || {}) })
