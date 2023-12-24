3a52b2d36e11977d15a21cc9e64b3783de714cda
otto <150408489+PoorExchange@users.noreply.github.com>
2023-12-24 15:55:13 +0700

    Update index.js
    I have updated the chain.

--- projects/PoorExchange/index.js
+++ projects/PoorExchange/index.js
@@ -1,3 +1,3 @@
 const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('Arbitrum One', '0x968d22160F9036e1679F0BA72488dbBf07470b98', { fetchBalances: true, })
+module.exports = uniTvlExport('arbitrum', '0x968d22160F9036e1679F0BA72488dbBf07470b98', { fetchBalances: true, })
