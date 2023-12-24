fa5b7bbbe6d01f8c2b6699d8ace5a94640c7bbc4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-24 22:20:13 +0100

    code refactor

--- projects/ckbtc/index.js
+++ projects/ckbtc/index.js
@@ -1,26 +1,16 @@
-//const { sumTokensExport } = require('../helper/sumTokens')
-const axios = require("axios")
+const { get } = require('../helper/http')
 
-async function tvl() {
-  var end = Math.floor(Date.now()/1000);
-  let start = end - 10000;
-  console.log(end);
-  console.log(start);
-  const { data, status } = await axios.get(
-    `https://icrc-api.internetcomputer.org/api/v1/ledgers/mxzaz-hqaaa-aaaar-qaada-cai/total-supply?start=${start}&end=${end}&step=1`,
-    {
-        headers: {
-            Accept: 'application/json',
-        },
-    },
-);
-let tokens = data.data.slice(-1)[0][1]
-return tokens;
+async function tvl(ts) {
+  var end = ts;
+  let start = end - 24 * 60 * 60;
+  const { data } = await get(`https://icrc-api.internetcomputer.org/api/v1/ledgers/mxzaz-hqaaa-aaaar-qaada-cai/total-supply?start=${start}&end=${end}&step=1`);
+  let [_, bal] = data.pop()
+  return {
+    'coingecko:bitcoin': bal / 1e8
+  };
 }
-
-
 
 module.exports = {
   methodology: `We count the BTC as the collateral for the ckBTC`,
-  icp: { tvl: tvl },
+  bitcoin: { tvl: tvl },
 }
