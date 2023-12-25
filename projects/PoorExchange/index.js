817cf0ba57ae253438fda3110684ceb868f5665b
otto <150408489+PoorExchange@users.noreply.github.com>
2023-12-25 08:35:18 +0700

    Update index.js new

--- projects/PoorExchange/index.js
+++ projects/PoorExchange/index.js
@@ -1,3 +1,3 @@
 const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('arbitrum', '0xa0eFcB6311633E450871b5bf274470582F91d613', { fetchBalances: true, })
+module.exports = uniTvlExport('arbitrum', '0x9fA0988D9e4b6362e0aaA02D1A09196a78c177e1', { fetchBalances: true, })
