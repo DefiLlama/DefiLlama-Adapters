442675985e34a0a30a442ceab1d5b5b0642bce9e
Abdurrezak Kandemir <info@sakaivault.io>
2023-12-26 05:41:39 +0300

    sakai dao contract added

--- projects/sakai-vault/index.js
+++ projects/sakai-vault/index.js
@@ -1,22 +1,3 @@
-<<<<<<< Updated upstream
-const contracts  = {
-    "SAKAI": "0x43b35e89d15b91162dea1c51133c4c93bdd1c4af",
-    "Sakai-SP": "0xc20A079c7962D9fc92173cda349e80D484dFA42A" //Sakai Staking Protocol Contract
-};
-
-
-async function fetch() {
-    const response = await fetch("https://lp.sakaivault.io/tvl"); 
-    const tvl = await response.json() //Check locked value : https://app.sakaivault.io/
-    const lastIndex = await tvl[tvl.length-1]
-    return {...lastIndex}
-  }
-  
-  module.exports = {
-    fetch
-  };
-  
-=======
 const { staking } = require("../helper/staking");
 
 const contracts = {
@@ -32,4 +13,3 @@ module.exports = {
     dao: dao(contracts["SakaiDAO"], contracts.SAKAI),
   },
 };
->>>>>>> Stashed changes
