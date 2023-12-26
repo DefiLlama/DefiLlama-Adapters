c54b5c508387a65488246410af551e019b17357a
cerraio <147749662+cerraio@users.noreply.github.com>
2023-12-26 09:24:52 +0200

    Update index.js

--- projects/cerra/index.js
+++ projects/cerra/index.js
@@ -1,15 +1,14 @@
-const { post } = require('../helper/http')
+const { sumTokens2 } = require("../helper/chain/cardano");
+
+const LENDING_SCRIPT = "addr1w8z7wt5leqj5cv9x887a4mw257f63ze4h64cput99cr373qfjgjwn";
 
 async function tvl() {
-    const { tvl } = await post('https://pab.cerra.io/', {
-        endpoint: 'getTvl'
-    })
-    return {cardano: Number(tvl.ADA)}
+  return sumTokens2({ scripts: [LENDING_SCRIPT] })
 }
 
-module.exports={
-    timetravel: false,
-    cardano: {
-        tvl
-    }
-}
+module.exports = {
+  timetravel: false,
+  cardano: {
+    tvl,
+  }
+};
