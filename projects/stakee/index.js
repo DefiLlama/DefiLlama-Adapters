b5c9582c0723b8413b2c06b537d56c966646a5ba
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-26 11:13:31 +0100

    code refactor

--- projects/stakee/index.js
+++ projects/stakee/index.js
@@ -1,23 +1,13 @@
-const BigNumber = require("bignumber.js");
-const { post } = require('../helper/http')
+const { call } = require("../helper/chain/ton");
 
 module.exports = {
-    timetravel: false,
-    methodology: "STAKED",
-    ton: {
-        tvl: async () => {
-            const requestBody = {
-                "address": "EQD2_4d91M4TVbEBVyBF8J1UwpMJc361LKVCz6bBlffMW05o",
-                "method": "get_pool_full_data",
-                "stack": []
-            }
-            const response = await post('https://toncenter.com/api/v2/runGetMethod', requestBody)
-            if (! response.ok)  {
-                throw new Error("Unknown");
-            }
-            const result = response.result
-            const tonTotalSupply = parseInt(result.stack[2][1], 16)
-            return {"coingecko:the-open-network":  BigNumber(tonTotalSupply).div(1e9).toFixed(0)};
-        }
+  timetravel: false,
+  ton: {
+    tvl: async () => {
+      const result = await call({ target: "EQD2_4d91M4TVbEBVyBF8J1UwpMJc361LKVCz6bBlffMW05o", abi: "get_pool_full_data"})
+      console.log(result)
+      const tonTotalSupply = parseInt(result.stack[2][1], 16)
+      return { "coingecko:the-open-network": tonTotalSupply/1e9 };
     }
+  }
 }
