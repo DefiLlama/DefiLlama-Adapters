1302f42ea5d05e34a3cff49d2a95844cfad882e4
kirillmelcin <153381644+kirillmelcin@users.noreply.github.com>
2023-12-26 13:15:33 +0300

    Added Stakee (#8501)
    * Added Stakee
    
    * code refactor
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/stakee/index.js
+++ projects/stakee/index.js
@@ -4,7 +4,7 @@ module.exports = {
   timetravel: false,
   ton: {
     tvl: async () => {
-      const result = await call({ target: "EQCkWxfyhAkim3g2DjKQQg8T5P4g-Q1-K_jErGcDJZ4i-vqR", abi: "get_pool_full_data"})
+      const result = await call({ target: "EQD2_4d91M4TVbEBVyBF8J1UwpMJc361LKVCz6bBlffMW05o", abi: "get_pool_full_data"})
       console.log(result)
       const tonTotalSupply = parseInt(result.stack[2][1], 16)
       return { "coingecko:the-open-network": tonTotalSupply/1e9 };
