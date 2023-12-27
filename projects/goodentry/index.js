802143fc0e8442c1b3480e4a1390667fd7068956
Nicolas <lapingarou@gmail.com>
2023-12-27 18:31:10 +0800

    feat: gev2

--- projects/goodentry/index.js
+++ projects/goodentry/index.js
@@ -12,77 +12,20 @@ const token1Abi = "function TOKEN1() view returns (address token, uint8 decimals
 const vaultReservesAbi = "function getReserves() view returns (uint baseAmount, uint quoteAmount, uint valueX8)";
 
 
-// V1 supported tokens
-const v1tokens = [
-  "0x912CE59144191C1204E64559FE8253a0e49E6548", 
-  "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", 
-  "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", 
-  "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a", 
-  "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f"
-]
-
-
 // Existing v2 vaults: { vaultAddress: {name, base, quote} }
 const v2vaults = [
-  {
-    address: "0x1ba92C53BFe8FD1D81d84B8968422192B73F4475", 
-    name: "ARB-USDC.e UNIv3", base: "0x912CE59144191C1204E64559FE8253a0e49E6548", quote: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
-  },
-  {
-    address: "0xd5fE1A54fA642400ef559d866247cCE66049141B",
-    name: "ARB-USDC.e Camelot", base: "0x912CE59144191C1204E64559FE8253a0e49E6548", quote: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
-  },
-  {
-    address: "0x419ae989a629Cc71834BDf6E3e8E33c9c3ED3Bb4", 
-    name: "ARB-USDC Camelot", base: "0x912CE59144191C1204E64559FE8253a0e49E6548", quote: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
-  },
-  {
-    address: "0x36003A975bFC56f650590C26B1479ba423217931", 
-    name: "ETH-USDC.e Camelot", base: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", quote: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
-  },
-  {
-    address: "0xd666156C473Cc9539CAaCc112B3A3590a895C861", 
-    name: "ETH-USDC Camelot", base: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", quote: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
-  },
-  {
-    address: "0x21EB68Cc5a5d51b48e0DE743f321151523b7A15D", 
-    name: "GMX-USDC Camelot", base: "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a", quote: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
-  },
-  {
-    address: "0x5f6aB9b043C43FaB8D2A51EA85b70495B5EeFD15",
-    name: "WBTC-USDC Camelot", base: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f", quote: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
-  },
+  { address: "0x1ba92C53BFe8FD1D81d84B8968422192B73F4475", name: "ARB-USDC.e UNIv3", base: ADDRESSES.arbitrum.ARB, quote: ADDRESSES.arbitrum.USDC },
+  { address: "0xd5fE1A54fA642400ef559d866247cCE66049141B", name: "ARB-USDC.e Camelot", base: ADDRESSES.arbitrum.ARB, quote: ADDRESSES.arbitrum.USDC },
+  { address: "0x419ae989a629Cc71834BDf6E3e8E33c9c3ED3Bb4", name: "ARB-USDC Camelot", base: ADDRESSES.arbitrum.ARB, quote: ADDRESSES.arbitrum.USDC_CIRCLE },
+  { address: "0x36003A975bFC56f650590C26B1479ba423217931", name: "ETH-USDC.e Camelot", base: ADDRESSES.arbitrum.WETH, quote: ADDRESSES.arbitrum.USDC },
+  { address: "0xd666156C473Cc9539CAaCc112B3A3590a895C861", name: "ETH-USDC Camelot", base: ADDRESSES.arbitrum.WETH, quote: ADDRESSES.arbitrum.USDC_CIRCLE },
+  { address: "0x21EB68Cc5a5d51b48e0DE743f321151523b7A15D", name: "GMX-USDC Camelot", base: ADDRESSES.arbitrum.GMX, quote: ADDRESSES.arbitrum.USDC_CIRCLE },
+  { address: "0x5f6aB9b043C43FaB8D2A51EA85b70495B5EeFD15", name: "WBTC-USDC Camelot", base: ADDRESSES.arbitrum.WBTC, quote: ADDRESSES.arbitrum.USDC_CIRCLE },
 ]
-
-
-
-const getTvlV2 = async (chain, block) => {
-  const balances = {}
-  // GoodEntry v2: call getReserves() on each vault
-  const balsv2 = (await Promise.all(
-    v2vaults.map( async (vault) => {
-      var res = {}
-      try {
-        let reserves = (await sdk.api.abi.call({target: vault.address, abi: vaultReservesAbi, block, chain})).output;
-        res[chain+":"+vault.base] = parseInt(reserves.baseAmount);
-        res[chain+":"+vault.quote] = parseInt(reserves.quoteAmount);
-      } catch(e) {}
-      return res
-    })
-  )).flat(2)
-
-  //aggregate tokens balances
-  for (let k of balsv2){
-    for( const [key, bal] of Object.entries(k)){
-      balances[key] = balances.hasOwnProperty(key) ? bal + balances[key] : bal
-    }
-  }
-
-  return balances
-}
 
 
 async function tvl(timestamp, ethBlock, _, { api }) {
+  // GoodEntry v1
   const addressesProviders = await api.call({ target: addressesProviderRegistry, abi: abi["getAddressesProvidersList"], })
   const validAddressesProviders = addressesProviders.filter((ap) => ap != ADDRESSES.null)
   const lendingPools = await api.multiCall({ calls: validAddressesProviders, abi: getLpAbi, })
@@ -111,6 +54,13 @@ async function tvl(timestamp, ethBlock, _, { api }) {
       })
     })
   )
+
+  // GoodEntry v2
+  let reserves = (await api.multiCall({ calls: v2vaults.map(v => v.address), abi: vaultReservesAbi})).map((v, i) => {return { res: v, ...v2vaults[i]} })
+  reserves.forEach( v => {
+    api.add(v.base, v.res.baseAmount)
+    api.add(v.quote, v.res.quoteAmount)
+  })
 
 }
 
