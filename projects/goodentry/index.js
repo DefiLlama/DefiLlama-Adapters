d5b10f648d7d91b01dc3164e90e5675d6f611621
Nicolas <lapingarou@gmail.com>
2023-12-27 15:35:25 +0800

    feat: ge v2

--- projects/goodentry/index.js
+++ projects/goodentry/index.js
@@ -9,14 +9,88 @@ const getReserveDataAbi = "function getReserveData(address asset) view returns (
 const getUnderlyingAbi = "function getTokenAmounts(uint amount) external view returns (uint token0Amount, uint token1Amount)";
 const token0Abi = "function TOKEN0() view returns (address token, uint8 decimals)";
 const token1Abi = "function TOKEN1() view returns (address token, uint8 decimals)";
+// v2 getReserves ABI
+const vaultReservesAbi = "function getReserves() view returns (uint baseAmount, uint quoteAmount, uint valueX8)";
 
-// Aave helper doesnt recognize tokenized Uniswap positions, need to manually
+
+// V1 supported tokens
+const v1tokens = [
+  "0x912CE59144191C1204E64559FE8253a0e49E6548", 
+  "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", 
+  "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", 
+  "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a", 
+  "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f"
+]
+
+
+// Existing v2 vaults: { vaultAddress: {name, base, quote} }
+const v2vaults = [
+  {
+    address: "0x1ba92C53BFe8FD1D81d84B8968422192B73F4475", 
+    name: "ARB-USDC.e UNIv3", base: "0x912CE59144191C1204E64559FE8253a0e49E6548", quote: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
+  },
+  {
+    address: "0xd5fE1A54fA642400ef559d866247cCE66049141B",
+    name: "ARB-USDC.e Camelot", base: "0x912CE59144191C1204E64559FE8253a0e49E6548", quote: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
+  },
+  {
+    address: "0x419ae989a629Cc71834BDf6E3e8E33c9c3ED3Bb4", 
+    name: "ARB-USDC Camelot", base: "0x912CE59144191C1204E64559FE8253a0e49E6548", quote: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
+  },
+  {
+    address: "0x36003A975bFC56f650590C26B1479ba423217931", 
+    name: "ETH-USDC.e Camelot", base: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", quote: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
+  },
+  {
+    address: "0xd666156C473Cc9539CAaCc112B3A3590a895C861", 
+    name: "ETH-USDC Camelot", base: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", quote: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
+  },
+  {
+    address: "0x21EB68Cc5a5d51b48e0DE743f321151523b7A15D", 
+    name: "GMX-USDC Camelot", base: "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a", quote: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
+  },
+  {
+    address: "0x5f6aB9b043C43FaB8D2A51EA85b70495B5EeFD15",
+    name: "WBTC-USDC Camelot", base: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f", quote: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
+  },
+]
+
+
+
+const getTvlV2 = async (chain, block) => {
+  const balances = {}
+  // GoodEntry v2: call getReserves() on each vault
+  const balsv2 = (await Promise.all(
+    v2vaults.map( async (vault) => {
+      var res = {}
+      try {
+        let reserves = (await sdk.api.abi.call({target: vault.address, abi: vaultReservesAbi, block, chain})).output;
+        res[chain+":"+vault.base] = parseInt(reserves.baseAmount);
+        res[chain+":"+vault.quote] = parseInt(reserves.quoteAmount);
+      } catch(e) {}
+      return res
+    })
+  )).flat(2)
+
+  //aggregate tokens balances
+  for (let k of balsv2){
+    for( const [key, bal] of Object.entries(k)){
+      balances[key] = balances.hasOwnProperty(key) ? bal + balances[key] : bal
+    }
+  }
+
+  return balances
+}
+
+
 
 
 function geTvl() {
   const chain = "arbitrum";
   return async (timestamp, ethBlock, { [chain]: block }) => {
-    const balances = {}
+    const balances = await getTvlV2(chain, block)
+
+    // GoodEntry v1: loop on lending pools and tickers to get underlying assets
     const addressesProviders = (
       await sdk.api.abi.call({
         target: addressesProviderRegistry,
@@ -65,24 +139,31 @@ function geTvl() {
               aTokenAddress = (await sdk.api.abi.call({target: pool, abi: getReserveDataAbi, block, chain, params: atn})).output.aTokenAddress;
               balance = (await sdk.api.abi.call({target: atn, abi: balanceOfAbi, block, chain, params: aTokenAddress})).output;
 
-              // if it's a ticker, can call underlying
-              const underlying = (await sdk.api.abi.call({target: atn, abi: getUnderlyingAbi, block, chain, params: balance})).output;
-              const token0 = (await sdk.api.abi.call({target: atn, abi: token0Abi, block, chain})).output.token;
-              const token1 = (await sdk.api.abi.call({target: atn, abi: token1Abi, block, chain})).output.token;
+              // if this is a support ERC20 (and not a tokenised range)
+              if (v1tokens.includes(atn)){
+                res[chain+":"+atn] = parseInt(balance);
+              }
+              else {
+                // ignore low balance assets to save calls, since anyway tickers are initialized with value around ~$1/1e18
+                if (balance < 10e18) return res;
+                
+                // if it's a ticker, can call underlying
+                const underlying = (await sdk.api.abi.call({target: atn, abi: getUnderlyingAbi, block, chain, params: balance})).output;
+                const token0 = (await sdk.api.abi.call({target: atn, abi: token0Abi, block, chain})).output.token;
+                const token1 = (await sdk.api.abi.call({target: atn, abi: token1Abi, block, chain})).output.token;
 
-              res[chain+":"+token0] = parseInt(underlying.token0Amount);
-              res[chain+":"+token1] = parseInt(underlying.token1Amount);
+                res[chain+":"+token0] = parseInt(underlying.token0Amount);
+                res[chain+":"+token1] = parseInt(underlying.token1Amount);
+              }
             }
-            catch(e){
-              // in case of error, return the base asset
-              res[chain+":"+atn] = parseInt(balance);
-            }
+            catch(e){}
             return res;
           }).flat(2)
         )
         return atnsRes;
       })
     )).flat(2)
+
     //aggregate tokens balances
     for (let k of bals){
       for( const [key, bal] of Object.entries(k)){
@@ -96,7 +177,10 @@ function geTvl() {
 
 module.exports = {
   methodology:
-    "Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There's multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending",
+    "For GoodEntry v1, counts the tokens locked in the Aave lending pool fork. For v2, calls a dedicated getReserves() function on the vault.",
+  hallmarks: [
+    [1701376109, "V2 Launch"]
+  ],
   arbitrum: {
     tvl: geTvl(),
   }
