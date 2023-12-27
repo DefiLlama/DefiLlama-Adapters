c759cb1bad8ef7902b66e8bab2fbc74633cb54b8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-27 16:47:39 +0100

    code refactor

--- projects/dojoswap/index.js
+++ projects/dojoswap/index.js
@@ -1,11 +1,10 @@
-const { getFactoryTvl } = require("./factoryTvl");
+const { getFactoryTvl } = require("../terraswap/factoryTvl");
 
 const factory = {
   classic: "inj1pc2vxcmnyzawnwkf03n2ggvt997avtuwagqngk",
 };
 
 module.exports = {
-  timetravel: false,
   misrepresentedTokens: true,
   methodology: "Liquidity on the DEX",
   injective: { tvl: getFactoryTvl(factory.classic) },
