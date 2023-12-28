51a0ac02043c8d05bc3216f3848143e43547defe
0xkorin <0xkorin@proton.me>
2023-12-28 22:45:09 +0100

    Fix abi

--- projects/yearn-ether/index.js
+++ projects/yearn-ether/index.js
@@ -3,7 +3,7 @@ const YETH_POOL = '0x2cced4ffA804ADbe1269cDFc22D7904471aBdE63';
 
 async function tvl(_, _1, _2, { api }) {
   const vb_prod_sum = await api.call({
-    abi: 'uint256:vb_prod_sum',
+    abi: {"stateMutability":"view","type":"function","name":"vb_prod_sum","inputs":[],"outputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"}]},
     target: YETH_POOL,
   });
 
