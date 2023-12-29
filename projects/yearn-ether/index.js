cf89ed07a9f8a78fcc04d57ae236dae712109777
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-12-29 12:36:36 +0100

    code refactor

--- projects/yearn-ether/index.js
+++ projects/yearn-ether/index.js
@@ -1,20 +1,11 @@
-const { nullAddress } = require("../helper/unwrapLPs");
 const YETH_POOL = '0x2cced4ffA804ADbe1269cDFc22D7904471aBdE63';
 
 async function tvl(_, _1, _2, { api }) {
-  const vb_prod_sum = await api.call({
-    abi: {"stateMutability":"view","type":"function","name":"vb_prod_sum","inputs":[],"outputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"}]},
-    target: YETH_POOL,
-  });
-
-  return {
-    [nullAddress]: vb_prod_sum[1]
-  };
+  const tokens = await api.fetchList({  lengthAbi: 'num_assets', itemAbi: 'assets', target: YETH_POOL})
+  return api.sumTokens({ tokens, owner: YETH_POOL })
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: 'counts the total amount of ETH underlying the LSTs deposited into the yETH pool.',
   start: 1693971707,
   ethereum: { tvl }
