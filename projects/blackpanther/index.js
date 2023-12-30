3a179c1831f3cc7bedc1cda2d16fdc880ec241a3
Black Panther <blackwhalemoney@gmail.com>
2023-12-30 15:00:40 +0800

    update to include spot vaults

--- projects/blackpanther/index.js
+++ projects/blackpanther/index.js
@@ -20,6 +20,11 @@ const config = {
       "inj1wf0n4jr5hz8962xszgt9s479h80zh722kp40ky",
       "inj1u8llm9l24s54e4hk7ac5wswggfr8w8nkn0ae9f",
       "inj1kepxjx47dh87ytws2fkl6j99gnhu65rzrl9m4q",
+      "inj1v4hl4s7lqw84usxqhh9mhjewlwvfe5r8zygvc3",
+      "inj1enh7ulash5gj0098qlcfltw5w8uyygavc8r4el",
+      "inj140crz68lje9chnlyl4ewtgpcmvnnlns39t5tf0",
+      "inj1z274paln0lwdl5erfcdzsmgwkt4gtv7fx39326",
+      "inj1xd5w08ezq4c6eq6cjsy5zqrvtvqf7hpkp3swcw",
     ],
   },
 };
