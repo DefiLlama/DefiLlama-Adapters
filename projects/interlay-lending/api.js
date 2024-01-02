72cc07f6656bbd077cbbf652cddd66851ec641d9
Brendon Votteler <bvotteler@users.noreply.github.com>
2024-01-02 10:31:37 +0200

    feat: add interlay-lending tvl

---
+++ projects/interlay-lending/api.js
@@ -0,0 +1,43 @@
+const { getAPI, addTokenBalance } = require('../helper/acala/interlay-api');
+
+// for exchange rate decoding
+const FIXEDI128_SCALING_FACTOR = 18;
+
+async function tvl() {
+  const chain = "interlay";
+  const api = await getAPI(chain);
+  const balances = {};
+  
+  const data = (await api.query.loans.markets.entries());
+
+  const markets = [];
+  for (let i = 0; i < data.length; i++) {
+    const [underlyingId, marketData] = data[i];
+
+    const isActive = marketData?.toJSON().state == "Active";
+    if (!isActive) {
+      continue;
+    }
+
+    const lendTokenId = marketData.toJSON().lendTokenId;
+
+    const [exchangeRate, totalIssuance] = Promise.all([
+      api.query.loans.exchangeRate(underlyingId).then((rawExchangeRate) => {
+        parseInt(rawExchangeRate) / (10 ** FIXEDI128_SCALING_FACTOR);
+      }),
+      api.query.tokens.totalIssuance(lendTokenId)
+    ]);
+
+    const atomicAmount = totalIssuance * exchangeRate;
+    addTokenBalance({ balances, chain, atomicAmount, ccyArg: underlyingId });
+  }
+
+  return balances;
+}
+
+
+module.exports = {
+  timetravel: false,
+  methodology: "Tracks TVL for Interlay's lending protocol.",
+  interlay: { tvl }
+};
