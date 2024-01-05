1fec0ab8c4287bc2b2dc25b0bb1f85427ec57390
stephen <stephen@lumiterra.net>
2024-01-05 11:27:21 +0800

    fix: lumi-finance tvl

--- projects/lumi-finance/index.js
+++ projects/lumi-finance/index.js
@@ -6,12 +6,11 @@ const Arb2CRVGauge = "0xCE5F24B7A95e9cBa7df4B54E911B4A3Dc8CDAf6f";
 const Arb2CRVLUAUSDLP = "0xD2239B95890018a8f52fFD17d7F94C3A82f05389";
 
 const staking = sumTokensExport({ ownerTokens: [[[Arb2CRVGauge, Arb2CRVLUAUSDLP], StakeContract]] });
-const tvl = sumTokensExport({ ownerTokens: [[[Arb2CRVGauge, Arb2CRVLUAUSDLP], AMMContract], [[Arb2CRVGauge, Arb2CRVLUAUSDLP], StakeContract]] });
 
 module.exports = {
   methodology: `We count the 2CRV and 2CRVLUAUSD-LP on ${AMMContract}, ${StakeContract}`,
   arbitrum: {
     staking: staking,
-    tvl: tvl
+    tvl: sumTokensExport({ ownerTokens: [[[Arb2CRVGauge, Arb2CRVLUAUSDLP], AMMContract]] })
   }
 }
