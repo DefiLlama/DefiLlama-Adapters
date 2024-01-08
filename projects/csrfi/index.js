e2dfd7c953eac365d9e15af32b58fbde620bf50f
Jakub Semik <kuba.semik@gmail.com>
2024-01-08 19:51:17 +0000

    add csr canto

--- projects/csrfi/index.js
+++ projects/csrfi/index.js
@@ -1,19 +1,20 @@
-const MINT_TOKEN_CONTRACT = '0xbe1Be54f6251109d5fB2532b85d7eE9Cb375C43f';
+const MINT_TOKEN_CONTRACT = "0xbe1Be54f6251109d5fB2532b85d7eE9Cb375C43f";
 
 async function tvl(_, _1, _2, { api }) {
-    const collateralBalance = await api.call({
-      abi: 'erc20:totalSupply',
-      target: MINT_TOKEN_CONTRACT,
-    });
-  
-    api.add(MINT_TOKEN_CONTRACT, collateralBalance)
-  }
+  const balance = await api.call({
+    abi: "erc20:totalSupply",
+    target: MINT_TOKEN_CONTRACT,
+  });
+
+  api.add(MINT_TOKEN_CONTRACT, balance);
+}
 
 module.exports = {
-    timetravel: true,
-    misrepresentedTokens: false,
-    start: 4859484,
-    canto: {
-      tvl,
-    }
-  }; 
+  timetravel: true,
+  misrepresentedTokens: false,
+  methodology: "Value of all Canto wrapped in CSR protocol",
+  start: 4859484,
+  canto: {
+    tvl,
+  },
+};
