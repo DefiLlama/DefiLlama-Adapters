635f3929eb40e6723b454cdc1cad2ddbff9a67a3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-08 11:43:48 +0100

    code refactor

--- projects/y24/index.js
+++ projects/y24/index.js
@@ -1,4 +1,5 @@
-const { staking } = require('../helper/staking')
+const { staking } = require('../helper/staking');
+const { sumTokensExport } = require('../helper/unknownTokens');
 
 const TOKEN_ADDRESS = '0x652000ba3c230d83279AEC84A49d41d706AFB0F1';
 const StakingContract1 = '0x8aCE17bAadBbAfb8178330d4C87C224a08826520';
@@ -9,7 +10,7 @@ module.exports = {
   start: 35011373,
   bsc: {
     tvl: () => ({}),
-    staking: staking([StakingContract1, StakingContract2], TOKEN_ADDRESS)
+    staking: sumTokensExport({owners: [StakingContract1, StakingContract2], tokens: [TOKEN_ADDRESS], lps: ['0x44628669C0F888b2884d20b94C22af465AA11f05'], useDefaultCoreAssets: true,})
   }
 };
 
