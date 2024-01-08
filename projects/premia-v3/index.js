ce4a6b3ff690b0678df239a225abda2b4a58739b
froggiedev <froggie@premia.finance>
2024-01-08 10:05:48 -1000

    Update premia-v3 subgraph endpoint

--- projects/premia-v3/index.js
+++ projects/premia-v3/index.js
@@ -1,5 +1,5 @@
-const { getLogs } = require('../helper/cache/getLogs')
-const { cachedGraphQuery } = require('../helper/cache')
+const { getLogs } = require("../helper/cache/getLogs");
+const { cachedGraphQuery } = require("../helper/cache");
 
 const getAddresses = `
   query PoolAddresses {
@@ -10,30 +10,46 @@ const getAddresses = `
       }
     }
   }
-`
+`;
 
 const config = {
-  arbitrum: { poolFactory: '0xae4fb6622f25f397587f11638da8ce88c27b5645', poolFromBlock: 119998935, vaultGraph: 'https://api.thegraph.com/subgraphs/name/premian-labs/premia-blue' },
-}
+  arbitrum: {
+    poolFactory: "0xae4fb6622f25f397587f11638da8ce88c27b5645",
+    poolFromBlock: 119998935,
+    vaultGraph:
+      "https://subgraph.satsuma-prod.com/5d8f840fce6d/premia/premia-v3-arbitrum/api",
+  },
+};
 
-Object.keys(config).forEach(chain => {
-  const { poolFactory, poolFromBlock, vaultGraph, } = config[chain]
+Object.keys(config).forEach((chain) => {
+  const { poolFactory, poolFromBlock, vaultGraph } = config[chain];
   module.exports[chain] = {
-    tvl: async (_, _b, _cb, { api, }) => {
+    tvl: async (_, _b, _cb, { api }) => {
       const logs = await getLogs({
         api,
         target: poolFactory,
-        eventAbi: "event PoolDeployed(address indexed base, address indexed quote, address oracleAdapter, uint256 strike, uint256 maturity, bool isCallPool, address poolAddress)",
+        eventAbi:
+          "event PoolDeployed(address indexed base, address indexed quote, address oracleAdapter, uint256 strike, uint256 maturity, bool isCallPool, address poolAddress)",
         onlyArgs: true,
         fromBlock: poolFromBlock,
-      })
-      const ownerTokens = logs.map(log => [[log.base, log.quote], log.poolAddress])
+      });
+      const ownerTokens = logs.map((log) => [
+        [log.base, log.quote],
+        log.poolAddress,
+      ]);
       if (vaultGraph) {
-        const { vaults } = await cachedGraphQuery('premia/v3/vaults/'+api.chain, vaultGraph, getAddresses)
-        const vaultTokens = vaults.map(log => [[log.asset.address], log.address])
-        ownerTokens.push(...vaultTokens)
+        const { vaults } = await cachedGraphQuery(
+          "premia/v3/vaults/" + api.chain,
+          vaultGraph,
+          getAddresses
+        );
+        const vaultTokens = vaults.map((log) => [
+          [log.asset.address],
+          log.address,
+        ]);
+        ownerTokens.push(...vaultTokens);
       }
-      return api.sumTokens({ ownerTokens })
-    }
-  }
-})
+      return api.sumTokens({ ownerTokens });
+    },
+  };
+});
