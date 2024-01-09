6a0ad4837a1ffefa8511ab009ea690937ce7e111
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-09 22:08:50 +0100

    update tangleswap graph url

--- projects/tangleswap/index.js
+++ projects/tangleswap/index.js
@@ -3,7 +3,7 @@ const { sumTokens2, } = require('../helper/unwrapLPs')
 const { uniV3Export, } = require('../helper/uniswapV3')
 
 const config = {
-  shimmer_evm: { endpoint: 'https://shimmer.subgraph.tangleswap.space/subgraphs/name/tangleswap/shimmer' },
+  shimmer_evm: { endpoint: 'https://shimmer.subgraph.tangleswap.space/subgraphs/name/tangleswap/shimmer-v3' },
 }
 const query = `query getPools($lastId: String!) {
   pools(
@@ -32,6 +32,5 @@ Object.keys(config).forEach(chain => {
   module.exports[chain] = { tvl }
 })
 
-module.exports.milkomeda = uniV3Export({ 
-  milkomeda: { factory: "0xda2f048C128506e720b0b0b32F20432157dde1c7", fromBlock: 19701714, },
-}).milkomeda
+module.exports.milkomeda = uniV3Export({  milkomeda: { factory: "0xda2f048C128506e720b0b0b32F20432157dde1c7", fromBlock: 19701714, },}).milkomeda
+// module.exports.shimmer_evm = uniV3Export({  milkomeda: { factory: "0xdf7bA717FB0D5ce579252f05167cD96d0fA77bCb", fromBlock: 19701714, },}).milkomeda
