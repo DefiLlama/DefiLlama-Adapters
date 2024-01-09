2bc1f87ee60db0009535591b92951598354d9063
pavelGotbit <pavel.grigoriev@gotbit.io>
2024-01-09 11:30:26 +0300

    removed vesting adapter

--- projects/creo/index.js
+++ projects/creo/index.js
@@ -1,22 +1,15 @@
 const STAKING = "0x077bd3104413c555Aa985a585CE9D2174349ddc3";
 
-const VESTING = "0xeaEd594B5926A7D5FBBC61985390BaAf936a6b8d";
-
 const TOKEN = "0x9521728bF66a867BC65A93Ece4a543D817871Eb7";
-
-async function vesting(time, ethBlock, _b, { api }) {
-  return api.sumTokens({ tokens: [TOKEN], owner: VESTING });
-}
 
 async function staking(time, ethBlock, _b, { api }) {
   return api.sumTokens({ tokens: [TOKEN], owner: STAKING });
 }
 
 module.exports = {
-  methodology: "TVL counted from staking and vesting contracts",
+  methodology: "TVL counted from the vesting contract",
   bsc: {
     tvl: () => ({}),
     staking,
-    vesting,
   },
 };
