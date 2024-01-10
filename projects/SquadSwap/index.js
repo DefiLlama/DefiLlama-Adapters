ff178673f4d9d1e0b10e51f831e1c8a3da69d6a9
Eyyub Ensar MERMER <98224771+eyyubmermer@users.noreply.github.com>
2024-01-10 22:29:18 +0300

    Update index.js

--- projects/SquadSwap/index.js
+++ projects/SquadSwap/index.js
@@ -2,7 +2,7 @@ const { stakings } = require("../helper/staking");
 const { getUniTVL } = require('../helper/unknownTokens')
 const masterchefV2Address = '0x2e881a10f682a3b2CBaaF8fc5A9a94E98D4879B4'; // SquadSwap's MasterChef V2 contract
 const masterchefV3Address = '0x44eC8143EB368cAbB00c4EfF085AF276260202B5'; // SquadSwap's MasterChef V3 contract
-const squadTokenAddress = '0x0fEAdcC3824E7F3c12f40E324a60c23cA51627fc'; // SquadSwap token contract
+const squadTokenAddress = '0x2d2567dec25c9795117228adc7fd58116d2e310c'; // SquadSwap token contract
 
 module.exports = {
     methodology: "TVL is calculated from total liquidity of SquadSwap's active pools",
