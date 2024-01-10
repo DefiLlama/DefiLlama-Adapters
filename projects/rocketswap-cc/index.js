984145a08f1e00759902b7384d990665a2a68d4c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-10 13:40:31 +0100

    fix rocketswap

--- projects/rocketswap-cc/index.js
+++ projects/rocketswap-cc/index.js
@@ -2,5 +2,5 @@ const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
   misrepresentedTokens: true,
-  base: { tvl: getUniTVL({ factory: '0x1b8128c3a1b7d20053d10763ff02466ca7ff99fc', useDefaultCoreAssets: true, fetchBalances: true, }), },
+  base: { tvl: getUniTVL({ factory: '0x1b8128c3a1b7d20053d10763ff02466ca7ff99fc', useDefaultCoreAssets: true, }), },
 }
