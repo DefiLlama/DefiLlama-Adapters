88c1d527af62c6b38b6f2bb4c8f6113033b46957
0xN2 <155973521+0xN2@users.noreply.github.com>
2024-01-11 16:11:20 -0300

    Update index.js

--- projects/sizev1/index.js
+++ projects/sizev1/index.js
@@ -1,29 +1,26 @@
 const sdk = require("@defillama/sdk");
-const { eth } = require("@defillama/sdk/build/api");
 
-const SIZE_S_C ="0x044d9B2C4d8A696Fe83FBB723F6006bd2d7a0E7e";
-const USDC_ADDRESS = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
-const blockNumber = 9061457
-async function tvl(timestamp,chainBlocks) {
-const usdcBalance = (
-await sdk.api.erc20.balanceOf({
-target: USDC_ADDRESS,
-owner: SIZE_S_C,
-block: blockNumber,
-chain: "base",
-})
-).output;
+lendingPoolCore = "0xA238Dd80C259a72e81d7e4664a9801593F98d1c5"
+async function tvl(lendingPoolCore, block) {
 
-return {
-    "0x044d9B2C4d8A696Fe83FBB723F6006bd2d7a0E7e": usdcBalance, // USDC address
-};
+  abi={"getUserAccountData": "function getUserAccountData(address user) view returns (uint256 totalCollateralBase, uint256 totalDebtBase)"}
+  const reserves = (
+      await sdk.api.abi.call({
+          target: "0xA238Dd80C259a72e81d7e4664a9801593F98d1c5",
+          params: ["0x01ae5e1e0fed8369aef5c4e6ff1ab62bd0381b9b"],
+          abi: abi["getUserAccountData"],
+          block: 9104473,
+          chain: "base",
+          skipCache: true
+      })
+  ).output;
+console.log(reserves)
+  return reserves
 }
 
-module.exports = {
-methodology: "USDC tokens",
-base: {
-tvl,
-borrowed
-},
-};
-
+  module.exports = {
+    methodology: "Counts assets (ETH and USDC) deposited ",
+    base: {
+      tvl,
+    },
+  };
