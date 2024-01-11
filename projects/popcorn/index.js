ff0119090ec5ba699d11811bbb6673c5197342c4
ruhum <90156563+0xruhum@users.noreply.github.com>
2024-01-11 18:25:03 +0100

    popcorn: add frax vaults on arbi (#8633)

--- projects/popcorn/index.js
+++ projects/popcorn/index.js
@@ -1,11 +1,16 @@
 const { ADDRESSES } = require("./constants");
 const { addVaultToTVL } = require("./vault");
+const { addFraxVaultToTVL } = require("./fraxVault");
 
 function getTVL(chain = undefined) {
   return async (timestamp, block, chainBlocks, { api }) => {
     let balances = {};
 
     await addVaultToTVL(balances, api, ADDRESSES[chain].vaultRegistry);
+
+    if (chain === "arbitrum") {
+      await addFraxVaultToTVL(balances, api);
+    }
 
     return balances;
   }
