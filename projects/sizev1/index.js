f8b7320a1c4146fb1fd870bdb4b59d9d5aa4a978
0xN2 <155973521+0xN2@users.noreply.github.com>
2024-01-12 12:25:31 -0300

    parse result

--- projects/sizev1/index.js
+++ projects/sizev1/index.js
@@ -1,26 +1,38 @@
 const sdk = require("@defillama/sdk");
+const { chainExports } = require("../helper/exports");
 
-lendingPoolCore = "0xA238Dd80C259a72e81d7e4664a9801593F98d1c5"
-async function tvl(lendingPoolCore, block) {
+const lendingPoolCore = "0xA238Dd80C259a72e81d7e4664a9801593F98d1c5";
 
-  abi={"getUserAccountData": "function getUserAccountData(address user) view returns (uint256 totalCollateralBase, uint256 totalDebtBase)"}
-  const reserves = (
-      await sdk.api.abi.call({
-          target: "0xA238Dd80C259a72e81d7e4664a9801593F98d1c5",
-          params: ["0x01ae5e1e0fed8369aef5c4e6ff1ab62bd0381b9b"],
-          abi: abi["getUserAccountData"],
-          block: 9104473,
-          chain: "base",
-          skipCache: true
-      })
-  ).output;
-console.log(reserves)
-  return reserves
+async function tvl(_, _1, _2,{api}) {
+    abi={"getUserAccountData": "function getUserAccountData(address user) view returns (uint256 totalCollateralBase, uint256 totalDebtBase)"}
+
+    const reserves = await sdk.api.abi.call({
+        target: lendingPoolCore,
+        params: ["0x01ae5e1e0fed8369aef5c4e6ff1ab62bd0381b9b"],
+        abi: abi["getUserAccountData"],
+        chain: "base",
+       // block: block,
+        skipCache: true
+    });
+
+    const totalCollateralBase = reserves.output[0];
+    let tvlOk = parseInt(totalCollateralBase[0])
+
+    
+    const balances = {
+        '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913': totalCollateralBase
+    };
+console.log(tvlOk)
+  //return  tvlOk;
+  api.add("0x01ae5e1e0fed8369aef5c4e6ff1ab62bd0381b9b", tvlOk)
+  /* blockNumber = sdk.api.util.getLatestBlock(base)
+  sdk. */
 }
 
-  module.exports = {
-    methodology: "Counts assets (ETH and USDC) deposited ",
+module.exports = {
+    methodology: "Counts assets (ETH and USDC) deposited",
     base: {
-      tvl,
-    },
-  };
+        tvl,
+    },      
+    
+};
