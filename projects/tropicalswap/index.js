01067549ac0a87d6fe106feff48bb4f6e44e659c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-14 10:52:29 +0100

    Update index.js

--- projects/tropicalswap/index.js
+++ projects/tropicalswap/index.js
@@ -1,5 +1,3 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
-const chain = 'mantle'
-const factory = '0x5B54d3610ec3f7FB1d5B42Ccf4DF0fB4e136f249' // v2 factory address
+const { uniTvlExport } = require('../helper/unknownTokens');
 
-module.exports = uniTvlExport(chain, factory)
+module.exports = uniTvlExport('mantle', '0x5B54d3610ec3f7FB1d5B42Ccf4DF0fB4e136f249', { fetchBalances: true, })
