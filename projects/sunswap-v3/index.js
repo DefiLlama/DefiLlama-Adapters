9b10b9c4ace26eea2e2bec94c5f538969b1618ca
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-15 14:04:16 +0100

    track genesis lrt

--- projects/sunswap-v3/index.js
+++ projects/sunswap-v3/index.js
@@ -1,6 +1,5 @@
 const { getConfig } = require('../helper/cache')
 
-/** @type {import('../types').tvlFunction} */
 async function tvl(_, _b, _cb, { api, }) {
   const { data } = await getConfig('sunswap-v3', 'https://sbc.endjgfsv.link/scan/getPoolList?version=v3')
   const ownerTokens = data.map(i => [[i.token0Address, i.token1Address], i.pairAddress])
