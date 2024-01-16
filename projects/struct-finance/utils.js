3f142eaf3ae125fc3224b273b18238558c8205d3
AntwuanDixon <evan@struct.fi>
2024-01-16 11:35:30 +0800

    feature: create constants and utils files

---
+++ projects/struct-finance/utils.js
@@ -0,0 +1,28 @@
+const {autopoolsMetaData} = require("./constants");
+
+function mergeAndSum(obj1, obj2) {
+  let result = {};
+
+  new Set([...Object.keys(obj1), ...Object.keys(obj2)]).forEach((key) => {
+    result[key] = (obj1[key] || 0) + (obj2[key] || 0);
+  });
+
+  return result;
+}
+
+function sumTokens(sums, response) {
+  const { tokenX, tokenY } = autopoolsMetaData[response.input.target];
+  const autopoolTokenXWithAvax = `avax:${tokenX.toLowerCase()}`;
+  const autopoolTokenYWithAvax = `avax:${tokenY.toLowerCase()}`;
+  const { amountX, amountY } = response.output;
+
+  sums[autopoolTokenXWithAvax] = (sums[autopoolTokenXWithAvax] || 0) + amountX;
+  sums[autopoolTokenYWithAvax] = (sums[autopoolTokenYWithAvax] || 0) + amountY;
+
+  return sums;
+}
+
+module.exports = {
+  mergeAndSum,
+  sumTokens,
+};
