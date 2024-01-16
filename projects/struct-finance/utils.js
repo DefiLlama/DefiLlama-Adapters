fb8ce3c217fc98322410cff5ee7dd13149304124
AntwuanDixon <evan@struct.fi>
2024-01-16 19:35:34 +0800

    fix: convert strings to floats

--- projects/struct-finance/utils.js
+++ projects/struct-finance/utils.js
@@ -4,7 +4,7 @@ function mergeAndSum(obj1, obj2) {
   let result = {};
 
   new Set([...Object.keys(obj1), ...Object.keys(obj2)]).forEach((key) => {
-    result[key] = (obj1[key] || 0) + (obj2[key] || 0);
+    result[key] = (+obj1[key] || 0) + (+obj2[key] || 0);
   });
 
   return result;
@@ -16,8 +16,8 @@ function sumTokens(sums, response) {
   const autopoolTokenYWithAvax = `avax:${tokenY.toLowerCase()}`;
   const { amountX, amountY } = response.output;
 
-  sums[autopoolTokenXWithAvax] = (sums[autopoolTokenXWithAvax] || 0) + amountX;
-  sums[autopoolTokenYWithAvax] = (sums[autopoolTokenYWithAvax] || 0) + amountY;
+  sums[autopoolTokenXWithAvax] = (sums[autopoolTokenXWithAvax] || 0) + +amountX;
+  sums[autopoolTokenYWithAvax] = (sums[autopoolTokenYWithAvax] || 0) + +amountY;
 
   return sums;
 }
