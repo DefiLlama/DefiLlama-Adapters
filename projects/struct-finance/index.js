673147bbe442d7a8fd45347d6490338e94e5755a
AntwuanDixon <evan@struct.fi>
2024-01-16 11:34:22 +0800

    feature: add logic to fetch TVL of TJAP struct vaults and value of AP tokens belonging to yield source contracts - TODO: fix GMX sumTokens2 call and merge results with TJAP

--- projects/struct-finance/index.js
+++ projects/struct-finance/index.js
@@ -1,30 +1,88 @@
-const ADDRESSES = require('../helper/coreAssets.json');
+const ADDRESSES = require("../helper/coreAssets.json");
 const { sumTokens2 } = require("../helper/unwrapLPs");
-
-const addresses = {
-  struct: {
-    gmxYieldSource: "0x6aE12b0adF9716181c07D19dfe76442AB1b3817b",
-    gmxFactory: "0x46f8765781ac36e5e8f9937658fa311af9d735d7",
-  },
-  token: {
-    fsGlp: "0x9e295b5b976a184b14ad8cd72413ad846c299660",
-  },
-};
+const { mergeAndSum, sumTokens } = require("./utils");
+const {
+  addresses,
+  autoPoolABI,
+  aptFarmUserInfoCalls,
+  aptFarmAddress,
+  aptFarmABI
+} = require("./constants");
+const sdk = require("@defillama/sdk");
 
 async function tvl(ts, _, __, { api }) {
-  const vaults = await api.fetchList({  lengthAbi: 'uint256:totalProducts', itemAbi: "function allProducts(uint256) external view returns (address)", target: addresses.struct.gmxFactory})
-  vaults.push(addresses.struct.gmxYieldSource);
+  const vaultsGmx = await api.fetchList({
+    lengthAbi: "uint256:totalProducts",
+    itemAbi: "function allProducts(uint256) external view returns (address)",
+    target: addresses.struct.gmx.factory,
+  });
+  const vaultsTjap = await api.fetchList({
+    lengthAbi: "uint256:totalProducts",
+    itemAbi: "function allProducts(uint256) external view returns (address)",
+    target: addresses.struct.tjap.factory,
+  });
 
-  return sumTokens2({
+  // call userInfo on APT farm, passing in the farm ID and the yield source address
+  // first value returned in the array is the amount of AP tokens owned by the yield source contract
+  const { output: responsesFarmUserInfo } = await sdk.api.abi.multiCall({
+    target: aptFarmAddress,
+    calls: aptFarmUserInfoCalls,
+    chain: "avax",
+    abi: aptFarmABI,
+  });
+
+  // then take the amount and pass it to previewAmounts on each respective TJAP Autovault contract, which will give
+  // back the amounts redeemable by the yield source in token X and token Y
+  const tokenXYCalls = Object.values(addresses.token.tjap).map(
+    (autovaultAddress, i) => {
+      return {
+        target: autovaultAddress,
+        params: responsesFarmUserInfo[i].output.amount,
+        abi: autoPoolABI,
+      };
+    }
+  );
+
+  const { output: responsesAmountsXY } = await sdk.api.abi.multiCall({
+    target: "", // @note - not used, but must be supplied
+    calls: tokenXYCalls,
+    chain: "avax",
+    abi: autoPoolABI,
+  });
+
+  const tjapYieldSourcesTokenSums = responsesAmountsXY.reduce(sumTokens, {});
+
+  vaultsGmx.push(addresses.struct.yieldSource);
+  // @todo - figure out why calling this throws an error
+  // const gmxSumTokens = sumTokens2({
+  //   api,
+  //   owners: vaultsGmx,
+  //   tokens: [
+  //     ADDRESSES.avax.BTC_b,
+  //     ADDRESSES.avax.USDC,
+  //     ADDRESSES.avax.WETH_e,
+  //     addresses.token.gmx.fsGlp,
+  //   ],
+  // });
+
+  const tjapVaultsTokenSums = sumTokens2({
     api,
-    owners: vaults,
+    owners: vaultsTjap,
     tokens: [
       ADDRESSES.avax.BTC_b,
       ADDRESSES.avax.USDC,
       ADDRESSES.avax.WETH_e,
-      addresses.token.fsGlp,
+      ADDRESSES.avax.EURC,
+      ADDRESSES.avax.WAVAX,
     ],
   });
+
+  const tjapTokenSums = mergeAndSum(
+    tjapVaultsTokenSums,
+    tjapYieldSourcesTokenSums
+  );
+
+  return tjapTokenSums;
 }
 
 module.exports = {
