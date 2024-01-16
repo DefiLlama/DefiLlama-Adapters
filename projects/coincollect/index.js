b17a80a342b2d9cb212a391e1f130d96f3556d95
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-16 15:26:11 +0100

    minor fix

--- projects/coincollect/index.js
+++ projects/coincollect/index.js
@@ -9,7 +9,7 @@ module.exports = {
     misrepresentedTokens: true,
     incentivized: true,
     polygon: {
-        tvl: getUniTVL({ factory: '0x2bc17223A99B6e2857796a5F64A1ED91067b5657', useDefaultCoreAssets: true }),
+        tvl: getUniTVL({ factory: '0x2bc17223A99B6e2857796a5F64A1ED91067b5657', useDefaultCoreAssets: true, fetchBalances: true }),
         staking: staking(MasterChefContract, COLLECT),
     }
 };
