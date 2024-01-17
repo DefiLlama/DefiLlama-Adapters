e03ac0e4b65e853f9918624239435f1165aca03d
realdealshaman <realdealshaman11@gmail.com>
2024-01-17 03:41:36 +0000

    update to kim factory

--- projects/kizuna/index.js
+++ projects/kizuna/index.js
@@ -2,6 +2,6 @@ const { getUniTVL } = require("../helper/unknownTokens");
 module.exports = {
   misrepresentedTokens: true,
   mode: {
-    tvl: getUniTVL({ factory: "0x05CDC3ec49C623dCE7947172fECFc5d3cD8d16cD", useDefaultCoreAssets: true, }),
+    tvl: getUniTVL({ factory: "0xc02155946dd8C89D3D3238A6c8A64D04E2CD4500", useDefaultCoreAssets: true, }),
   },
 };
