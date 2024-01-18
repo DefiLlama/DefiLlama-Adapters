abe34b7826fadc39c9d4a61f75cca8dd63b6448c
shafu <shafu0x@gmail.com>
2024-01-18 21:41:51 +0100

    Update index.js (#8703)

--- projects/dyad/index.js
+++ projects/dyad/index.js
@@ -1,9 +1,19 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokensExport } = require('../helper/unwrapLPs')
+const ADDRESSES = require("../helper/coreAssets.json");
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
 module.exports = {
   ethereum: {
-    tvl: sumTokensExport({ owners: ['0xdc400bbe0b8b79c07a962ea99a642f5819e3b712', '0xcF97cEc1907CcF9d4A0DC4F492A3448eFc744F6c'],
-      tokens: [ADDRESSES.null, ADDRESSES.ethereum.WETH] }),
+    tvl: sumTokensExport({
+      owners: [
+        "0xdc400bbe0b8b79c07a962ea99a642f5819e3b712",
+        "0xcF97cEc1907CcF9d4A0DC4F492A3448eFc744F6c",
+        "0x7aE80418051b2897729Cbdf388b07C5158C557A1",
+      ],
+      tokens: [
+        ADDRESSES.null,
+        ADDRESSES.ethereum.WETH,
+        ADDRESSES.ethereum.WSTETH,
+      ],
+    }),
   },
 };
