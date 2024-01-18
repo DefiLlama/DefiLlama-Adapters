515be3511efe2eb2e3fc6835bc7f37dc72d918bd
codehans <94654388+codehans@users.noreply.github.com>
2024-01-18 08:39:51 +0000

    Explicity read the deposit balances from Bow pools

--- projects/bow/index.js
+++ projects/bow/index.js
@@ -1,16 +1,27 @@
 
 const { getConfig } = require("../helper/cache");
-const { sumTokens } = require('../helper/chain/cosmos')
+const { queryContract } = require("../helper/chain/cosmos");
 
-
-async function tvl() {
+async function tvl(_, _1, _2, { api }) {
+  const chain = api.chain;
   const contracts = await getConfig("kujira/contracts", "https://raw.githubusercontent.com/Team-Kujira/kujira.js/master/src/resources/contracts.json");
-  const bowPools = contracts["kaiyo-1"].bow.map(x => x.address)
-  const owners = [
-  ...bowPools
-]
+  const bowPools = contracts["kaiyo-1"].bow;
+  for (const contract of bowPools) {
+    try {
+      const pool = await queryContract({
+        contract: contract.address,
+        chain,
+        data: { pool: {} },
+      });
 
-  return await sumTokens({ owners, chain: 'kujira' })
+      api.add(contract.config.denoms[0], pool.balances[0])
+      api.add(contract.config.denoms[1], pool.balances[1])
+    } catch (error) {
+      continue;
+    }
+  }
+
+  return api.getBalances();
 }
 
 module.exports = {
