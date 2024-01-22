6dbfd37d04d253f9d32af9f7edf9d9e6bf9455bb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-22 15:40:15 +0100

    code refactor

--- projects/struct-finance/index.js
+++ projects/struct-finance/index.js
@@ -1,6 +1,6 @@
 const ADDRESSES = require("../helper/coreAssets.json");
 const { sumTokens2 } = require("../helper/unwrapLPs");
-const { mergeAndSum, sumAutoPoolTokenXY } = require("./utils");
+const { sumAutoPoolTokenXY } = require("./utils");
 const {
   addresses,
   autoPoolABI,
@@ -8,26 +8,16 @@ const {
   aptFarmAddress,
   aptFarmABI
 } = require("./constants");
-const sdk = require("@defillama/sdk");
 
 async function tvl(ts, _, __, { api }) {
-  const vaultsGmx = await api.fetchList({
-    lengthAbi: "uint256:totalProducts",
-    itemAbi: "function allProducts(uint256) external view returns (address)",
-    target: addresses.struct.gmx.factory,
-  });
-  const vaultsTjap = await api.fetchList({
-    lengthAbi: "uint256:totalProducts",
-    itemAbi: "function allProducts(uint256) external view returns (address)",
-    target: addresses.struct.tjap.factory,
-  });
+  const vaultsGmx = await api.fetchList({ lengthAbi: "totalProducts", itemAbi: "allProducts", target: addresses.struct.gmx.factory, });
+  const vaultsTjap = await api.fetchList({ lengthAbi: "totalProducts", itemAbi: "allProducts", target: addresses.struct.tjap.factory, });
 
   // call userInfo on APT farm, passing in the farm ID and the yield source address
   // first value returned in the array is the amount of AP tokens staked by the yield source contract
-  const { output: responsesFarmUserInfo } = await sdk.api.abi.multiCall({
+  const responsesFarmUserInfo  = await api.multiCall({
     target: aptFarmAddress,
     calls: aptFarmUserInfoCalls,
-    chain: api.chain,
     abi: aptFarmABI,
   });
 
@@ -37,27 +27,23 @@ async function tvl(ts, _, __, { api }) {
     (autovaultAddress, i) => {
       return {
         target: autovaultAddress,
-        params: responsesFarmUserInfo[i].output.amount,
-        abi: autoPoolABI,
+        params: responsesFarmUserInfo[i].amount,
       };
     }
   );
 
-  const { output: responsesAmountsXY } = await sdk.api.abi.multiCall({
-    target: "", // not used, but must be supplied
+  const responsesAmountsXY  = await api.multiCall({
     calls: amountsXYCalls,
-    chain: api.chain,
     abi: autoPoolABI,
-  });
+  })
 
-  const tjapYieldSourcesTokenSums = responsesAmountsXY.reduce(
-    sumAutoPoolTokenXY,
-    {}
-  );
+  responsesAmountsXY.forEach((response, i) => {
+    sumAutoPoolTokenXY(api, response, amountsXYCalls[i].target)
+  })
 
   vaultsGmx.push(addresses.struct.gmx.yieldSource);
 
-  const vaultTokenSums = await sumTokens2({
+  return sumTokens2({
     api,
     owners: [vaultsTjap, vaultsGmx].flat(),
     tokens: [
@@ -69,10 +55,6 @@ async function tvl(ts, _, __, { api }) {
       addresses.token.gmx.fsGlp,
     ],
   });
-
-  const allTokenSums = mergeAndSum(vaultTokenSums, tjapYieldSourcesTokenSums);
-
-  return allTokenSums;
 }
 
 module.exports = {
--- projects/struct-finance/utils.js
+++ projects/struct-finance/utils.js
@@ -1,28 +1,12 @@
-const {autopoolsMetaData} = require("./constants");
+const { autopoolsMetaData } = require("./constants");
 
-function mergeAndSum(obj1, obj2) {
-  let result = {};
-
-  new Set([...Object.keys(obj1), ...Object.keys(obj2)]).forEach((key) => {
-    result[key] = (+obj1[key] || 0) + (+obj2[key] || 0);
-  });
-
-  return result;
-}
-
-function sumAutoPoolTokenXY(sums, response) {
-  const { tokenX, tokenY } = autopoolsMetaData[response.input.target];
-  const autopoolTokenXWithAvax = `avax:${tokenX.toLowerCase()}`;
-  const autopoolTokenYWithAvax = `avax:${tokenY.toLowerCase()}`;
-  const { amountX, amountY } = response.output;
-
-  sums[autopoolTokenXWithAvax] = (sums[autopoolTokenXWithAvax] || 0) + +amountX;
-  sums[autopoolTokenYWithAvax] = (sums[autopoolTokenYWithAvax] || 0) + +amountY;
-
-  return sums;
+function sumAutoPoolTokenXY(api, response, target) {
+  const { tokenX, tokenY } = autopoolsMetaData[target];
+  const { amountX, amountY } = response;
+  api.add(tokenX, amountX);
+  api.add(tokenY, amountY);
 }
 
 module.exports = {
-  mergeAndSum,
   sumAutoPoolTokenXY,
 };
