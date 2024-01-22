5e8e5f26bb260bacccb51402466a0c39d58e2844
Jazz Bashara <110779103+JazzBashara@users.noreply.github.com>
2024-01-22 19:48:03 +0700

    Add FerdyFlip TVL (#8719)
    * Add FerdyFlip TVL
    
    * Update index.js
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ projects/ferdyflip/index.js
@@ -0,0 +1,18 @@
+const ADDRESSES = require("../helper/coreAssets.json");
+const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
+
+// https://docs.ferdyflip.xyz/developers/contracts/platform
+const config = {
+  avax: {
+    tokens: [ADDRESSES.avax.WAVAX, nullAddress], owners: ['0x20AfbaC35B333dA4fE7230CC60946F88ee87aAA3'],
+  },
+  base: {
+    tokens: [ADDRESSES.base.WETH, nullAddress], owners: ['0x3b6014e4b38791444a352D687022D6d6d79Eb99c'],
+  },
+};
+
+Object.keys(config).forEach((chain) => {
+  module.exports[chain] = {
+    tvl: sumTokensExport(config[chain]),
+  };
+});
