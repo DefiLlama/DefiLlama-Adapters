0dae608410faae205d50e544060ac3834fa7407f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-22 13:47:53 +0100

    Update index.js

--- projects/ferdyflip/index.js
+++ projects/ferdyflip/index.js
@@ -2,25 +2,17 @@ const ADDRESSES = require("../helper/coreAssets.json");
 const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
 
 // https://docs.ferdyflip.xyz/developers/contracts/platform
-const owners = [
-  "0x20AfbaC35B333dA4fE7230CC60946F88ee87aAA3",
-  "0x3b6014e4b38791444a352D687022D6d6d79Eb99c",
-];
-
 const config = {
   avax: {
-    tokens: [ADDRESSES.avax.WAVAX, nullAddress],
+    tokens: [ADDRESSES.avax.WAVAX, nullAddress], owners: ['0x20AfbaC35B333dA4fE7230CC60946F88ee87aAA3'],
   },
   base: {
-    tokens: [ADDRESSES.base.WETH, nullAddress],
+    tokens: [ADDRESSES.base.WETH, nullAddress], owners: ['0x3b6014e4b38791444a352D687022D6d6d79Eb99c'],
   },
 };
 
-module.exports = {};
-
 Object.keys(config).forEach((chain) => {
-  const { tokens } = config[chain];
   module.exports[chain] = {
-    tvl: sumTokensExport({ owners, tokens }),
+    tvl: sumTokensExport(config[chain]),
   };
 });
