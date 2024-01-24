a24b04d320c377ce69eb78c6b4e784d757636e56
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-24 13:13:53 +0100

    code refactor

--- projects/dojoswap-lsd/index.js
+++ projects/dojoswap-lsd/index.js
@@ -1,19 +1,4 @@
-const BigNumber = require("bignumber.js");
-// const { getFactoryTvl } = require("../terraswap/factoryTvl");
-
-const toQueryMsg = (msg) => {
-  try {
-    return JSON.stringify(JSON.parse(msg))
-  } catch (error) {
-    return ""
-  }
-}
-
-const getUrl = (contract, msg) => {
-  const query_msg =
-    typeof msg === "string" ? toQueryMsg(msg) : JSON.stringify(msg)
-  return `${`https://sentry.lcd.injective.network:443`}/cosmwasm/wasm/v1/contract/${contract}/smart/${btoa(query_msg)}`
-}
+const { queryContract } = require('../helper/chain/cosmos')
 
 const ADDRESSES = {
   LSD_CONTRACT: 'inj17glv5mk2pvhpwkdjljacmr2fx9pfc3djepy6xh',
@@ -21,49 +6,15 @@ const ADDRESSES = {
   MULTICAL_CONTRACT: 'inj1578zx2zmp46l554zlw5jqq3nslth6ss04dv0ee'
 }
 
-async function stateLSD() {
-  const query = {
-    address: ADDRESSES.LSD_CONTRACT,
-    data: btoa(JSON.stringify({ state: {} }))
-  };
-
-  const url = getUrl(ADDRESSES.MULTICAL_CONTRACT, { aggregate: { queries: [query] } });
-  const reponse = await fetch(url);
-  const data = await reponse.json();
-  const data2 = data.data.return_data?.map((e) => {
-    return e.length == 0
-      ? null
-      : JSON.parse(Buffer.from(e.data, 'base64').toString())
-  });
-  return data2
-}
-
-async function getLDP() {
-  const [state] = await Promise.all([
-    stateLSD()
-  ]);
-  return state[0].tvl_utoken
-}
-
-async function staking() {
-  /// LSD
-  const data2 = await getLDP();
-  const state = (new BigNumber(data2)).div(new BigNumber(10 ** 18)) || new BigNumber(0);
-
-  /// INJ price
-  const response3 = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=injective-protocol&vs_currencies=usd');
-  const data3 = await response3.json();
-  const injPrice = data3['injective-protocol'].usd;
-
-  const lsdTvl = state.multipliedBy(injPrice).toNumber();
+async function tvl(_, _1, _2, { api }) {
+  const data = await queryContract({chain: api.chain, contract: ADDRESSES.LSD_CONTRACT,  data: { state: {} }})
 
   return {
-    tether: lsdTvl,
+    'injective-protocol': data.tvl_utoken / 1e18,
   }
 }
 
 module.exports = {
-  misrepresentedTokens: true,
   methodology: "Liquidity on LSD staking",
-  injective: { tvl: staking },
+  injective: { tvl },
 };
