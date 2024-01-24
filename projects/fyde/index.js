22403f488369165e9cb029c9637adf32fcc8e193
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-24 17:40:25 +0100

    update fyde

--- projects/fyde/index.js
+++ projects/fyde/index.js
@@ -2,7 +2,9 @@ const FYDE_CONTRACT = "0x87Cc45fFF5c0933bb6aF6bAe7Fc013b7eC7df2Ee";
 
 async function tvl(_, _1, _2, { api }) {
   const tokens = await api.fetchList({ lengthAbi: 'getAssetsListLength', itemAbi: 'assetsList', target: FYDE_CONTRACT })
-  return api.sumTokens({ tokens, owner: FYDE_CONTRACT })
+  const bals = await api.multiCall({  abi: 'function totalAssetAccounting(address) view returns (uint256)', calls: tokens, target: FYDE_CONTRACT })
+  api.addTokens(tokens, bals)
+  // return api.sumTokens({ tokens, owner: FYDE_CONTRACT })
 }
 
 module.exports = {
