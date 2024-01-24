afc8956dbbfb50d41fe5eb55fd56c25ac4cfe16b
Dayaa <m.d.e.boutalbi@me.com>
2024-01-24 12:11:02 +0100

    fix(Sperax USD): Fix TVL Calculation

--- projects/sperax/index.js
+++ projects/sperax/index.js
@@ -1,7 +1,7 @@
 const sdk = require("@defillama/sdk");
 const abi = require("../sperax/abi.json");
 const {staking} = require("../helper/staking.js");
-
+const { gql, request }= require ("graphql-request");
 const ethStakingAddr = "0xbF82a3212e13b2d407D10f5107b5C8404dE7F403";
 const arbStakingAddr = "0x2e2071180682Ce6C247B1eF93d382D509F5F6A17";
 
@@ -10,21 +10,21 @@ const vaultcore = '0xF783DD830A4650D2A8594423F123250652340E3f';
 const SPA = '0x5575552988a3a80504bbaeb1311674fcfd40ad4b';
 const ethSPA = '0xB4A3B0Faf0Ab53df58001804DdA5Bfc6a3D59008';
 const USDsAddress = '0xD74f5255D557944cf7Dd0E45FF521520002D5748';
+const v2SubgraphUrl= 'https://api.thegraph.com/subgraphs/name/sperax/usds-v2'
+const tvlQuery = gql`
+{
+    totalSupplyUsds(first: 1,orderBy:transactionData__blockNumber,orderDirection:desc) {
+        id
+        TVL
+      }
+}
+`;
 
-async function tvl(timestamp, ethBlock, chainBlocks) {
-    const chain = 'arbitrum';
-    const block = chainBlocks[chain];
-
-    let tvlInUSD = (await sdk.api.abi.call({
-        abi: abi.totalValueLocked,
-        chain: chain,
-        target: vaultcore,
-        params: [],
-        block: block
-    })).output;
+async function tvl() {
+    const query = await request(v2SubgraphUrl, tvlQuery);
 
     return {
-      tether: tvlInUSD / 1e18
+      tether: query.totalSupplyUsds[0].TVL
     }
 }
 
