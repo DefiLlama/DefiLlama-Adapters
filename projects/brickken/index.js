4836dd74d34764809fa303a1d50cabf6e2726069
xaler <dariolobuglio@gmail.com>
2024-01-25 10:02:34 +0100

    generalize for payTokens with more than 6 decimals

--- projects/brickken/index.js
+++ projects/brickken/index.js
@@ -5,17 +5,14 @@ async function tvl(_, _1, _2, { api }) {
   const tokens = await api.fetchList({  lengthAbi: 'idSTOs', itemAbi: 'stoTokens', target: FACTORY_ETH_CONTRACT, startFromOne: true })
   const escrows = await api.fetchList({  lengthAbi: 'idSTOs', itemAbi: 'stoEscrows', target: FACTORY_ETH_CONTRACT, startFromOne: true })
   const supplies = await api.multiCall({  abi: 'erc20:totalSupply', calls: tokens})
-  // const names = await api.multiCall({  abi: 'string:name', calls: tokens})
   const issuanceIndex = await api.multiCall({  abi: 'uint256:issuanceIndex', calls: escrows})
   const payTokens = await api.multiCall({  abi: 'address:paymentToken', calls: escrows})
+  const decimals = await api.multiCall({abi: 'erc20:decimals', calls: payTokens})
   const lastPrice = await api.multiCall({  abi:ESCROW_LATEST_PRICE_ABI, calls: escrows.map((escrow, idx) => ({target: escrow, params: issuanceIndex[idx]}))})
 
-  // const print = []
   payTokens.forEach((token, idx) => {
-    // print.push({ name: names[idx], token: tokens[idx], supply: supplies[idx]/1e18, price: lastPrice[idx][9]/1e18, tvl: supplies[idx] * lastPrice[idx][9]/ 1e42 })
-    api.add(token, supplies[idx] * lastPrice[idx][9]/ 1e30)
+    api.add(token, supplies[idx] * lastPrice[idx][9]/ 10**(36-decimals[idx]))
   })
-  // console.table(print)
 }
 
 module.exports = {
