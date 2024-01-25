2254b139da0720ce5cfa6008b7fd6a1134f939a4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-25 11:21:12 +0100

    code refactor

--- projects/peapods-finance/index.js
+++ projects/peapods-finance/index.js
@@ -1,6 +1,8 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
-const indexManager = "0x0Bb39ba2eE60f825348676f9a87B7CD1e3B4AE6B";
+const config = {
+  ethereum: { indexManager: "0x0Bb39ba2eE60f825348676f9a87B7CD1e3B4AE6B", peasToken: "0x02f92800F57BCD74066F5709F1Daa1A4302Df875" }
+}
 
 const indexManagerABI =
   "function allIndexes() view returns (tuple(address index, bool verified)[])";
@@ -9,8 +11,10 @@ const indexABIassets =
   "function getAllAssets() view returns (tuple(address token, uint256 weighting, uint256 basePriceUSDX96, address c1, uint256 q1)[])";
 const stakingPoolABI = { stakingToken: "address:stakingToken" };
 
-const getTvl = async (api, indexes) => {
+const getTvl = async (api, isStaking) => {
+  const { indexManager, peasToken } = config[api.chain]
   const ownerTokens = []
+  const indexes = (await api.call({ abi: indexManagerABI, target: indexManager, })).map(i =>i.index);
 
   //get staking pool contract for index (spp)
   const stakingPools = await api.multiCall({ abi: indexABI.lpStakingPool, calls: indexes, });
@@ -25,24 +29,22 @@ const getTvl = async (api, indexes) => {
   stakingTokens.forEach((stakingToken, i) => ownerTokens.push([[stakingToken], stakingPools[i]]))
   await sumTokens2({ api, ownerTokens, blacklistedTokens: indexes, resolveLP: true, })
   indexes.forEach(i => api.removeTokenBalance(i))
+  Object.keys(api.getBalances()).forEach(token => {
+    let remove = (new RegExp(peasToken, "i")).test(token)
+    if (isStaking) remove = !remove
+    if (remove)
+      api.removeTokenBalance(token)
+  })
   return api.getBalances()
 };
 
-async function tvl(_, _b, _cb, { api, }) {
-  //retrieve all available indexes
-  const allIndexes = await api.call({ abi: indexManagerABI, target: indexManager, });
-  //this array contains all contracts that own TVL
-  const indexes = allIndexes.map(i => i.index).filter(i => i.toLowerCase() !== '0x515e7fd1C29263DFF8d987f15FA00c12cd10A49b'.toLowerCase())
-  return getTvl(api, indexes)
-}
-
-async function staking(_, _b, _cb, { api, }) {
-  return getTvl(api, ['0x515e7fd1C29263DFF8d987f15FA00c12cd10A49b'])
-}
-
 module.exports = {
-  ethereum: {
-    tvl, staking,
-  },
   methodology: "Aggregates TVL in all Peapods Finance indexes created",
-};
+}
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => getTvl(api, false),
+    staking: async (_, _b, _cb, { api, }) => getTvl(api, true),
+  }
+})
