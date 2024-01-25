58c5b0a88bf69aabe4812936824d27cf72d8d20c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-25 11:12:37 +0100

    Revert "Separate PEAS tvl to staking and prepare for crosschain"
    This reverts commit c7f1b7c09f4104c69cb1173a68e344c675e4800d.

--- projects/peapods-finance/index.js
+++ projects/peapods-finance/index.js
@@ -1,11 +1,6 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
-const chainSettings = {
-  ethereum: {
-    indexManager: "0x0Bb39ba2eE60f825348676f9a87B7CD1e3B4AE6B",
-    peasToken: "0x02f92800F57BCD74066F5709F1Daa1A4302Df875"
-  }
-}
+const indexManager = "0x0Bb39ba2eE60f825348676f9a87B7CD1e3B4AE6B";
 
 const indexManagerABI =
   "function allIndexes() view returns (tuple(address index, bool verified)[])";
@@ -13,102 +8,41 @@ const indexABI = { lpStakingPool: "address:lpStakingPool" };
 const indexABIassets =
   "function getAllAssets() view returns (tuple(address token, uint256 weighting, uint256 basePriceUSDX96, address c1, uint256 q1)[])";
 const stakingPoolABI = { stakingToken: "address:stakingToken" };
-const stakingTokenABI = { token0: "address:token0", token1: "address:token1" };
 
-const getTVL = async (api, isStaking) => {
-  const indexManager = chainSettings[api.chain].indexManager;
-  const peasToken = chainSettings[api.chain].peasToken;
-
-  //retrieve all available indexes
-  const allIndexes = await api.call({
-    abi: indexManagerABI,
-    target: indexManager,
-  });
-
-  //this array contains all contracts that own TVL
-  const ownersArray = [];
-
-  //this array determines which tokens to be counted as TVL, so it doesn't count the index wrapper token itself, just the underlying value and paired token in LP
-  const tokensArray = [];
-
-  //collect all index data
-  const indexStakingPools = [];
-
-  for (const i of allIndexes) {
-    //add index to owners
-    ownersArray.push(i.index);
-  }
+const getTvl = async (api, indexes) => {
+  const ownerTokens = []
 
   //get staking pool contract for index (spp)
-  stakingPools = await api.multiCall({
-    abi: indexABI.lpStakingPool,
-    calls: ownersArray,
-  });
+  const stakingPools = await api.multiCall({ abi: indexABI.lpStakingPool, calls: indexes, });
 
   //get assets this index consists of
-  const assetsResult = await api.multiCall({
-    abi: indexABIassets,
-    calls: ownersArray,
-  });
-  for (const ar of assetsResult) {
-    for (const asset of ar) {
-      //add asset to whitelisted tokens to be counted for tvl
-      tokensArray.push(asset.token);
-    }
-  }
+  const assetsResult = await api.multiCall({ abi: indexABIassets, calls: indexes, });
+  const stakingTokens = await api.multiCall({ abi: stakingPoolABI.stakingToken, calls: stakingPools, });
 
-  //get staking token for index (UNI-V2 LP)
-  stakingTokens = await api.multiCall({
-    abi: stakingPoolABI.stakingToken,
-    calls: stakingPools,
-  });
-
-  //add UNI pool to owners
-  ownersArray.push(...stakingTokens);
-
-  //get tokens from pair
-  allToken0 = await api.multiCall({
-    abi: stakingTokenABI.token0,
-    calls: stakingTokens,
-  });
-  allToken1 = await api.multiCall({
-    abi: stakingTokenABI.token1,
-    calls: stakingTokens,
-  });
-
-  token0 = [];
-  for (const [i, value] of allToken0.entries()) {
-    token0[i] = value;
-  }
-
-  for (const [i, token1] of allToken1.entries()) {
-    //determine which token the index token is paired against and add token to whitelisted tokens to be counted for tvl
-    tokensArray.push(token1 != ownersArray[i] ? token1 : token0[i]);
-  }
-
-  //remove duplicate tokens
-  const uniqueTokensArray = [...new Set(tokensArray)];
-
-  //blacklisting ownersArray so that all index tokens won't be counted towards TVL when added to other pods to prevent double counting
-  return sumTokens2({
-    owners: ownersArray,
-    tokens: (isStaking ? [peasToken] : uniqueTokensArray),
-    blacklistedTokens: (isStaking ? [] : [...ownersArray,peasToken]),
-  });
+  assetsResult.forEach((assets, i) => {
+    ownerTokens.push([assets.map(i => i.token), indexes[i]])
+  })
+  stakingTokens.forEach((stakingToken, i) => ownerTokens.push([[stakingToken], stakingPools[i]]))
+  await sumTokens2({ api, ownerTokens, blacklistedTokens: indexes, resolveLP: true, })
+  indexes.forEach(i => api.removeTokenBalance(i))
+  return api.getBalances()
 };
 
 async function tvl(_, _b, _cb, { api, }) {
-  return getTVL(api, false);
+  //retrieve all available indexes
+  const allIndexes = await api.call({ abi: indexManagerABI, target: indexManager, });
+  //this array contains all contracts that own TVL
+  const indexes = allIndexes.map(i => i.index).filter(i => i.toLowerCase() !== '0x515e7fd1C29263DFF8d987f15FA00c12cd10A49b'.toLowerCase())
+  return getTvl(api, indexes)
 }
 
 async function staking(_, _b, _cb, { api, }) {
-  return getTVL(api, true);
+  return getTvl(api, ['0x515e7fd1C29263DFF8d987f15FA00c12cd10A49b'])
 }
 
 module.exports = {
   ethereum: {
-    tvl,
-    staking
+    tvl, staking,
   },
-  methodology: "Aggregates TVL in all Peapods Finance pods created. PEAS token in all pods are calculated towards staking conform DefiLlama guidelines.",
+  methodology: "Aggregates TVL in all Peapods Finance indexes created",
 };
