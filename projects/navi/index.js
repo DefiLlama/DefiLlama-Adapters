00fa119628801a7ea98b55518bbf2fce86111371
xiaodong <373232355@qq.com>
2024-01-25 12:02:06 +0800

    navi protocol merge volosui tvl

--- projects/navi/index.js
+++ projects/navi/index.js
@@ -14,7 +14,7 @@ async function borrow() {
   const storageObject = await sui.getObject(storageId);
   const reservesId = storageObject.fields.reserves.fields.id.id
 
-  const dynamicFields = await sui.getDynamicFieldObjects({parent: reservesId})
+  const dynamicFields = await sui.getDynamicFieldObjects({ parent: reservesId })
   dynamicFields.forEach((data) => {
     const coin = '0x' + data.fields.value.fields.coin_type
     const borrowed = data.fields.value.fields.borrow_balance.fields.total_supply
@@ -29,14 +29,39 @@ async function tvl() {
   const storageObject = await sui.getObject(storageId);
   const reservesId = storageObject.fields.reserves.fields.id.id
 
-   const dynamicFields = await sui.getDynamicFieldObjects({parent: reservesId})
+  const dynamicFields = await sui.getDynamicFieldObjects({ parent: reservesId })
+  const voloSuiTvl = await voloTvl()
+  console.log(voloSuiTvl)
+
   dynamicFields.forEach(object => {
     const coin = '0x' + object.fields.value.fields.coin_type
     const total_supply = object.fields.value.fields.supply_balance.fields.total_supply
     const borrowed = object.fields.value.fields.borrow_balance.fields.total_supply
-    const amount = (total_supply - borrowed) * (10 ** (decimalShift[coin] ?? 0))
+    var amount = (total_supply - borrowed) * (10 ** (decimalShift[coin] ?? 0))
+    if (coin === "0x549e8b69270defbfafd4f94e17ec44cdbdd99820b33bda2278dea3b9a32d3f55::cert::CERT") {
+      console.log(amount)
+      amount = amount + voloSuiTvl
+    }
     api.add(coin, amount)
   })
+}
+
+async function voloTvl() {
+  const nativePoolObj = await sui.getObject('0x7fa2faa111b8c65bea48a23049bfd81ca8f971a262d981dcd9a17c3825cb5baf');
+
+  const totalStakedValue = +(await sui.getDynamicFieldObject(
+    nativePoolObj.fields.total_staked.fields.id.id,
+    nativePoolObj.fields.staked_update_epoch,
+    {
+      idType: 'u64'
+    })).fields.value + +nativePoolObj.fields.pending.fields.balance;
+
+  const totalPendingRewards = +nativePoolObj.fields.total_rewards - nativePoolObj.fields.collected_rewards;
+  const unstakeTicketsSupply = +nativePoolObj.fields.ticket_metadata.fields.total_supply;
+
+  const totalStakedSui = totalStakedValue + totalPendingRewards - unstakeTicketsSupply;
+
+  return totalStakedSui
 }
 
 module.exports = {
