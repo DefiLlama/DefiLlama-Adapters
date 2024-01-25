d03c7e05553ca018c40a7d7d9726bfd12db63a95
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-25 11:51:40 +0100

    code refactor

--- projects/minu/index.js
+++ projects/minu/index.js
@@ -1,15 +1,9 @@
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+const { nullAddress, sumTokensExport } = require('../helper/unwrapLPs')
 const MINER_CONTRACT = "0x0754088499311a3FC2A9D2B759Dab2b6c0dB4A15"
 
-async function tvl(_, _1, _2, { bsc: block }) {
-  return sumTokens2({ tokens: [nullAddress], owner: MINER_CONTRACT, block, chain: 'bsc', })
-
-}
-
 module.exports = {
-
   bsc:{
-    tvl
+    tvl: sumTokensExport({ tokens: [nullAddress], owner: MINER_CONTRACT, })
   },
   methodology: "We count the BNB on the Miner contract"
 }
