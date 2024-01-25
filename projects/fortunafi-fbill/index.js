6794010844fb64a86a2f65b9cddef9da3c6e530e
eukadish <eugene.kadish@gmail.com>
2024-01-25 10:55:17 -0500

    add mulitchain support

--- projects/fortunafi-fbill/index.js
+++ projects/fortunafi-fbill/index.js
@@ -1,7 +1,22 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const contracts = {
+    arbitrum: {
+        token: '0x79ECCE8E2D17603877Ff15BC29804CbCB590EC08'
+    },
+    base: {
+        token: '0x79ECCE8E2D17603877Ff15BC29804CbCB590EC08'
+    },
     canto: {
+        token: '0x79ECCE8E2D17603877Ff15BC29804CbCB590EC08'
+    },
+    ethereum: {
+        token: '0x108Ec61bd5A91F5596F824832524C6b6002E3F03'
+    },
+    polygon: {
+        token: '0xB61c107Bf88F01A57F9aCFaB7Fa7BCF9637a950F'
+    },
+    polygon_zkevm: {
         token: '0x79ECCE8E2D17603877Ff15BC29804CbCB590EC08'
     }
 }
@@ -9,11 +24,26 @@ const contracts = {
 const tvl = async (_, _1, _2, { api }) => {
     const nav = await api.call({ target: contracts[api.chain].token, abi: 'function nav() external view returns (uint256)' });
 
-    api.add(ADDRESSES.canto.USDC, nav/1e12)
+    api.add(ADDRESSES[api.chain].USDC, (nav + 1) / 1e12)
 }
 
 module.exports = {
+    arbitrum: {
+        tvl
+    },
+    base: {
+        tvl
+    },
     canto: {
+        tvl
+    },
+    ethereum: {
+        tvl
+    },
+    polygon: {
+        tvl
+    },
+    polygon_zkevm: {
         tvl
     }
 };
--- projects/fortunafi-ifbill/index.js
+++ projects/fortunafi-ifbill/index.js
@@ -1,7 +1,22 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const contracts = {
+    arbitrum: {
+        token: '0x45bafad5a6a531Bc18Cf6CE5B02C58eA4D20589b'
+    },
+    base: {
+        token: '0x45bafad5a6a531Bc18Cf6CE5B02C58eA4D20589b'
+    },
     canto: {
+        token: '0x45bafad5a6a531Bc18Cf6CE5B02C58eA4D20589b'
+    },
+    ethereum: {
+        token: '0x4B57e1E3fd684d3bb82A0652c77FD7412dF6a2A5'
+    },
+    polygon: {
+        token: '0x8fC1F841b02d7C64b889EDC7Dbe50c9CB5860025'
+    },
+    polygon_zkevm: {
         token: '0x45bafad5a6a531Bc18Cf6CE5B02C58eA4D20589b'
     }
 }
@@ -9,11 +24,26 @@ const contracts = {
 const tvl = async (_, _1, _2, { api }) => {
     const nav = await api.call({ target: contracts[api.chain].token, abi: 'function nav() external view returns (uint256)' });
 
-    api.add(ADDRESSES.canto.USDC, nav / 1e12)
+    api.add(ADDRESSES[api.chain].USDC, (nav + 1) / 1e12)
 }
 
 module.exports = {
+    arbitrum: {
+        tvl
+    },
+    base: {
+        tvl
+    },
     canto: {
+        tvl
+    },
+    ethereum: {
+        tvl
+    },
+    polygon: {
+        tvl
+    },
+    polygon_zkevm: {
         tvl
     }
 };
