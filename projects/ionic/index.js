30c33cafd946ae98e75b6d748ac360efb2120baf
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-26 18:14:16 +0100

    code refactor

--- projects/ionic/index.js
+++ projects/ionic/index.js
@@ -1,25 +1,5 @@
-const sdk = require("@defillama/sdk");
+const { compoundExports2 } = require("../helper/compound");
 
-const { compoundExportsWithAsyncTransform } = require("../helper/compound");
-
-const pools = {
-  mode: {
-    pools: ["0xfb3323e24743caf4add0fdccfb268565c0685556"],
-  },
-};
-
-function getTvl(chain) {
-  const config = pools[chain] ?? { pools: [] };
-  const tvls = config.pools.map((pool) =>
-    compoundExportsWithAsyncTransform(pool, chain, undefined, undefined, {
-      resolveLPs: true,
-    })
-  );
-  let _tvl = sdk.util.sumChainTvls(tvls.map((t) => t.tvl));
-  let _borrowed = sdk.util.sumChainTvls(tvls.map((t) => t.borrowed));
-  let tvl = _tvl;
-  let borrowed = _borrowed;
-  return { tvl, borrowed };
+module.exports = {
+  mode: compoundExports2({ comptroller: '0xfb3323e24743caf4add0fdccfb268565c0685556'})
 }
-
-Object.keys(pools).forEach((chain) => (module.exports[chain] = getTvl(chain)));
