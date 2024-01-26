c65f1282ce2036350676252d6628ca5c68045b72
defichad23 <alex@possumfinance.com>
2024-01-26 14:26:51 +0100

    Fix export and contract address

--- projects/PossumLabs/index.js
+++ projects/PossumLabs/index.js
@@ -1,4 +1,4 @@
-const HLP_Portal = '0x1f3Af095CDa17d63cad238358837321e95FC5915';
+const HLP_Portal = '0x24b7d3034C711497c81ed5f70BEE2280907Ea1Fa';
 const HLP_PRICE_ADAPTER = '0x0266868d1c144a7534513F38B816c1aadE4030A2';
 // const TIME_RIFT = '0x6df4EF024089ab148078fdD88f5BF0Ee63248D3E';
 
@@ -9,33 +9,29 @@ async function tvl(_, _1, _2, { api }) {
         target: HLP_Portal,
     });
 
-    const PrincipalPrice = await api.call({
-        abi: 'uint256:getPrice',
-        target: HLP_PRICE_ADAPTER,
-    });
+    // const PrincipalPrice = await api.call({
+    //     abi: 'uint256:getPrice',
+    //     target: HLP_PRICE_ADAPTER,
+    // });
 
     // const TimeRiftStakedFLASH = await api.call({
     //     abi: 'uint256:stakedTokensTotal',
     //     target: TIME_RIFT,
     // });
 
-
-    api.add(HLP_Portal, stakeBalanceHLP * PrincipalPrice)
+    api.add(HLP_Portal, stakeBalanceHLP)
 }
 
-module.exports = {
+
+module.exports[arbitrum] = {
     timetravel: true,
     misrepresentedTokens: false,
     methodology: 'Get the number of HLP tokens staked and HLP price to calculate USD TVL.',
     start: 1701313010,
     arbitrum: {
         tvl,
-    }
-};
-
-module.exports = {
+    },
     doublecounted: true,
-    start: 1701313010,
     hallmarks: [
         [1701313010, "HLP Portal Launch"],
         [1704270118, "Time Rift Launch"]
