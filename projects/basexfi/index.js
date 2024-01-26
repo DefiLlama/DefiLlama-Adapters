02bae82e2f24788d87a56b8cb3a9f5be520f42c8
Jack <herrymarry@proton.me>
2024-01-26 14:49:26 +0800

    tvl

--- projects/basexfi/index.js
+++ projects/basexfi/index.js
@@ -1,11 +1,8 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { uniV3Export } = require('../helper/uniswapV3')
 
 module.exports = {
-  misrepresentedTokens: true,
-  base: {
-    tvl: getUniTVL({
-      useDefaultCoreAssets: true,
-      factory: '0xdC323d16C451819890805737997F4Ede96b95e3e'
-    })
-  }
-};
+  methodology: 'TVL accounts for the liquidity on all AMM pools taken from the factory contract',
+  ...uniV3Export({
+    base: { factory: '0xdC323d16C451819890805737997F4Ede96b95e3e' },
+  })
+}
