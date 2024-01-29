20d73bb322cbea3975020ac9024ee0a514f7fe48
royvardhan <royvardhan.official@gmail.com>
2024-01-29 22:20:39 +0530

    feat: add telos to vapordex

--- projects/vapordex-v2/index.js
+++ projects/vapordex-v2/index.js
@@ -1,8 +1,12 @@
 const { uniV3Export } = require("../helper/uniswapV3");
-const factory = "0x62B672E531f8c11391019F6fba0b8B6143504169";
+const FACTORY = "0x62B672E531f8c11391019F6fba0b8B6143504169";
 module.exports = uniV3Export({
   avax: {
-    factory,
+    FACTORY,
     fromBlock: 36560289,
+  },
+  telos: {
+    FACTORY,
+    fromBlock: 312329030,
   },
 });
