516380bf1d084a6c467f5887f19de37eab8113b4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-30 10:07:43 +0100

    Remove unnecessary files and update Blueberry Lending Market TVL calculation

--- projects/blueberry/index.js
+++ projects/blueberry/index.js
@@ -1,25 +1,6 @@
-const ComptrollerAbi = require('./ComptrollerAbi.json');
-const BTokenAbi = require('./BTokenAbi.json');
-
-const Comptroller = '0xfFadB0bbA4379dFAbFB20CA6823F6EC439429ec2';
-
-async function tvl(timestamp, block, chainBlocks, { api }) {
-  let underlyingTokens = [];
-  let underlyingBalances = [];
-
-  const bTokens = await api.call({ target: Comptroller, abi: ComptrollerAbi['getAllMarkets'] });
-
-  await Promise.all(bTokens.map(async bToken => {
-    const cashBalance = await api.call({ target: bToken, abi: BTokenAbi['internalCash'] });
-    const underlying = await api.call({ target: bToken, abi: BTokenAbi['underlying'] });
-    underlyingTokens.push(underlying);
-    underlyingBalances.push(cashBalance);
-  }));
-
-  api.addTokens(underlyingTokens, underlyingBalances);
-}
+const { compoundExports2 } = require('../helper/compound')
 
 module.exports = {
   methodology: 'Gets the total value locked in the Blueberry Lending Market',
-  ethereum: { tvl, },
-};
+  ethereum: compoundExports2({ comptroller: '0xfFadB0bbA4379dFAbFB20CA6823F6EC439429ec2'}),
+}
