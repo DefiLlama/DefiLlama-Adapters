edf18932f4d7cf8065163cdae4a89e1bb37c7408
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-30 10:02:58 +0100

    Update index.js

--- projects/aspida/index.js
+++ projects/aspida/index.js
@@ -1,20 +1,8 @@
-const sdk = require("@defillama/sdk");
-const ADDRESSES = require("../helper/coreAssets.json");
-
-const aETHContractAddress = "0xFC87753Df5Ef5C368b5FBA8D4C5043b77e8C5b39";
-
-
 module.exports = {
   ethereum: {
-    tvl: async (_, _1, _2) => {
-      const totalSupply = await sdk.api.abi.call({
-        target: aETHContractAddress,
-        abi: "uint256:totalSupply"
-      });
-
-      return {
-        [ADDRESSES.null]: totalSupply.output
-      };
+    tvl: async (_, _1, _2, { api }) => {
+      const totalSupply = await api.call({ target: "0xFC87753Df5Ef5C368b5FBA8D4C5043b77e8C5b39", abi: "uint256:totalSupply" });
+      api.addGasToken(totalSupply)
     }
   }
-};
+}
