e807f234ba9332faf8fa0b9fa3b3b8341974f0cd
CristianRicharte6 <cristianricharte6@gmail.com>
2024-01-31 16:26:29 +0100

    Refactor: Methodology updated

--- projects/3a-dao/index.js
+++ projects/3a-dao/index.js
@@ -3,8 +3,23 @@ const EURO3 = require("./EURO3Price");
 const protocol = require("./protocolTVL");
 const Provider = new ethers.JsonRpcProvider("https://polygon-rpc.com");
 
+
+// Make sure to use async functions and await when necessary
+async function test() {
+    console.log({
+        methodology: 'TVL counts the value for all the collateral tokens stored in the protocol vaults',
+        tvl: await protocol.tvl(Provider),
+        EURO3: {
+            totalSupply: await EURO3.totalSupply(Provider),
+            price: await EURO3.price(Provider),
+        },
+    });
+}
+
+test();
+
 module.exports = {
-    methodology: 'Counts the total value locked of all the collaterals assets in the protocol.',
+    methodology: 'Calculates the cumulative value of locked collateral across all assets within the protocol.',
     EURO3: {
         totalSupply: EURO3.totalSupply(Provider),
         price: EURO3.price(Provider)
