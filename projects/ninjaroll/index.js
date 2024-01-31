af55074cac7a24028071710a50861c3730150e33
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-31 14:30:06 +0100

    Refactor staking function and update dependencies

--- projects/ninjaroll/index.js
+++ projects/ninjaroll/index.js
@@ -1,22 +1,11 @@
-const axios = require("axios");
-const BigNumber = require("bignumber.js");
+const { queryContract } = require('../helper/chain/cosmos')
 
-async function staking() {
-  const rollPrice = await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=ninjaroll&vs_currencies=usd");
-  const rollPoolState = await axios.get(
-    `https://sentry.lcd.injective.network/cosmwasm/wasm/v1/contract/inj1fuf8u3d8fk2p34anz3f72tct6q8sr5hvxxv4x4/smart/${btoa(JSON.stringify({ state: {} }))}`
-  );
-  const rollTvl = new BigNumber(rollPrice?.data?.ninjaroll?.usd)
-    .times(rollPoolState?.data?.data?.total_bond_amount)
-    .div(new BigNumber(10).pow(18))
-    .toNumber()
-  return {
-    tether: rollTvl
-  }
+async function staking(_, _2, _1, { api }) {
+  const res = await queryContract({ chain: api.chain, contract: "inj1fuf8u3d8fk2p34anz3f72tct6q8sr5hvxxv4x4", data: { state: {} }})
+  api.add('coingecko:ninjaroll', res.total_bond_amount/1e18, { skipChain: true, })
 }
 
 module.exports = {
-  misrepresentedTokens: true,
   methodology: "TVL counts the ROLL tokens staked in the Ninjaroll staking contract",
-  injective: { tvl: staking },
+  injective: { tvl:() => ({}), staking },
 };
