87e582b393dbc6e760ba36f54642454d4206728f
ninja-roll <158177385+ninja-roll@users.noreply.github.com>
2024-01-31 10:53:02 +0700

    feat: call directly onchain

--- projects/ninjaroll/index.js
+++ projects/ninjaroll/index.js
@@ -1,9 +1,17 @@
+const axios = require("axios");
+const BigNumber = require("bignumber.js");
+
 async function staking() {
-  const response = await fetch('https://analytics.dojo.trading/dashboard/ninjaroll/pooltvl');
-  const data = await response.json();
-  const rollValue = data['ROLL'].tvl;
+  const rollPrice = await axios.get("https://analytics.dojo.trading/dashboard/pairs/prices?tokenaddress=inj1qv98cmfdaj5f382a0klq7ps4mnjp6calzh20h3");
+  const rollPoolState = await axios.get(
+    `https://sentry.lcd.injective.network/cosmwasm/wasm/v1/contract/inj1fuf8u3d8fk2p34anz3f72tct6q8sr5hvxxv4x4/smart/${btoa(JSON.stringify({ state: {} }))}`
+  );
+  const rollTvl = new BigNumber(rollPrice?.data?.price)
+    .times(rollPoolState?.data?.data?.total_bond_amount)
+    .div(new BigNumber(10).pow(18))
+    .toNumber()
   return {
-    tether: rollValue
+    tether: rollTvl
   }
 }
 
