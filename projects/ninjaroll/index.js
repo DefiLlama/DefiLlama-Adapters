ef1ac1b7e7440ceb8bf624e1590a93b20df8ec6f
ninja-roll <158177385+ninja-roll@users.noreply.github.com>
2024-01-31 11:05:05 +0700

    feat: update api for fetching roll price

--- projects/ninjaroll/index.js
+++ projects/ninjaroll/index.js
@@ -2,11 +2,11 @@ const axios = require("axios");
 const BigNumber = require("bignumber.js");
 
 async function staking() {
-  const rollPrice = await axios.get("https://analytics.dojo.trading/dashboard/pairs/prices?tokenaddress=inj1qv98cmfdaj5f382a0klq7ps4mnjp6calzh20h3");
+  const rollPrice = await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=ninjaroll&vs_currencies=usd");
   const rollPoolState = await axios.get(
     `https://sentry.lcd.injective.network/cosmwasm/wasm/v1/contract/inj1fuf8u3d8fk2p34anz3f72tct6q8sr5hvxxv4x4/smart/${btoa(JSON.stringify({ state: {} }))}`
   );
-  const rollTvl = new BigNumber(rollPrice?.data?.price)
+  const rollTvl = new BigNumber(rollPrice?.data?.ninjaroll?.usd)
     .times(rollPoolState?.data?.data?.total_bond_amount)
     .div(new BigNumber(10).pow(18))
     .toNumber()
