35167fadb08194a4fcb349a5f545fd9e168dbe5f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-01 13:51:42 +0100

    Refactor TVL and staking functions in Match-Finance index.js

--- projects/Match-Finance/index.js
+++ projects/Match-Finance/index.js
@@ -8,30 +8,31 @@ const MATCH_FINANCE_CONTRACT = '0x04b9ce11da7323aEf03f6e6c16C0b93cFB44C55c';
 const VLMATCH_STAKING_CONTRACT = '0x7D027083e55724A1082b8cDC51eE90781f41Ff14';
 
 const totalSuppliedAbi = 'function depositedAsset(address) external view returns (uint256)'
-const vlMatchStakedAbi = 'function totalStaked() external view returns (uint256)'
 
 async function tvl(_, _1, _2, { api }) {
   const [
     totalSTETHSupplied,
     lpStaked,
-    vlMatchStaked
   ] = await Promise.all([
     api.call({ abi: totalSuppliedAbi, params: MATCH_FINANCE_CONTRACT, target: LYBRA_STETH_VAULT }),
     api.call({ abi: 'erc20:balanceOf', params: MATCH_FINANCE_CONTRACT, target: LYBRA_REWARD_VAULT }),
-    api.call({ abi: vlMatchStakedAbi, params: null, target: VLMATCH_STAKING_CONTRACT })
   ]);
 
   api.add(ADDRESSES.ethereum.STETH, totalSTETHSupplied)
   api.add(ETH_LBR_LP, lpStaked)
-  api.add(MATCH_TOKEN, vlMatchStaked)
 
   return api.getBalances()
 }
 
+async function staking(_, _b, _cb, { api, }) {
+  const vlMatchStaked = await api.call({ abi: 'uint256:totalStaked', target: VLMATCH_STAKING_CONTRACT })
+  api.add(MATCH_TOKEN, vlMatchStaked)
+}
+
 module.exports = {
   doublecounted: true,
-  methodology: 'Total amount of stETH supplied, ETH-LBR LP staked and MATCH locked in Match Finance',
+  methodology: 'Get total amount of stETH supplied and ETH-LBR LP staked to Match Finance',
   ethereum: {
-    tvl,
+    tvl, staking,
   }
 }; 
