e8e5436e08e00a73722850db9952fa2be3eaa75f
CristianRicharte6 <cristianricharte6@gmail.com>
2024-02-01 09:34:39 +0100

    Fix: Removed test

--- projects/3a-dao/index.js
+++ projects/3a-dao/index.js
@@ -4,20 +4,6 @@ const protocol = require("./protocolTVL");
 const Provider = new ethers.JsonRpcProvider("https://polygon-rpc.com");
 
 
-// Make sure to use async functions and await when necessary
-async function test() {
-    console.log({
-        methodology: 'TVL counts the value for all the collateral tokens stored in the protocol vaults',
-        tvl: await protocol.tvl(Provider),
-        EURO3: {
-            totalSupply: await EURO3.totalSupply(Provider),
-            price: await EURO3.price(Provider),
-        },
-    });
-}
-
-test();
-
 module.exports = {
     methodology: 'Calculates the cumulative value of locked collateral across all assets within the protocol.',
     polygon: {
