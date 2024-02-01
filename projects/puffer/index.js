64b364a5b79d453b1a6646041ec1ecfb56dcece0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-01 13:44:46 +0100

    code refactor

--- projects/puffer/index.js
+++ projects/puffer/index.js
@@ -1,28 +1,14 @@
 const PUF_ETH = '0xD9A442856C234a39a81a089C06451EBAa4306a72';
-const ETH = "0x0000000000000000000000000000000000000000";
-
-const pufETHAbi = {
-    "totalAssets": "function totalAssets() view returns (uint256)"
-}
 
 async function tvl(_, _1, _2, { api }) {
-    const totalAssets = await api.call({
-        abi: pufETHAbi.totalAssets,
-        target: PUF_ETH,
-        params: [],
-    });
-
-    // The vault holds stETH which which has a 1:1 exchange rate with ETH, WETH, ETH
-    // This means all of the assets held by the vault are ETH
-    api.add(ETH, totalAssets)
+  return api.erc4626Sum({ calls: [PUF_ETH], isOG4626: true })
 }
 
 module.exports = {
-    timetravel: true,
-    misrepresentedTokens: false,
-    methodology: 'Returns the total assets owned by the Puffer Vault on Ethereum.',
-    start: 19128047,
-    ethereum: {
-        tvl,
-    }
+  doublecounted: true,
+  methodology: 'Returns the total assets owned by the Puffer Vault on Ethereum.',
+  start: 19128047,
+  ethereum: {
+    tvl,
+  }
 }; 
