7a3f733819d7c431597b86fc9e210fc403c4357d
haluk <haluk@bionluk.com>
2024-02-02 15:25:50 +0300

    updated salvor project info

--- projects/salvorlending/index.js
+++
@@ -1,6 +0,0 @@
-const ADDRESSES = require("../helper/coreAssets.json");
-const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
-
-module.exports.avax = {
-  tvl: sumTokensExport({ owners: ["0xab4fe2d136efd7f8dfce3259a5e3c5e4c0130c80"], tokens: [ADDRESSES.avax.WAVAX, nullAddress] }),
-};
