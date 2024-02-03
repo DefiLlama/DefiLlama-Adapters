0f890694d6e0bb4ed9fa840bea447242b66cc54d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-03 13:39:08 +0100

    Refactor TVL calculation using sumTokensExport

--- projects/pike/index.js
+++ projects/pike/index.js
@@ -1,35 +1,27 @@
 const ADDRESSES = require('../helper/coreAssets.json');
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
-async function tvl(timestamp, block, _, { api }) {
-    console.log(block);
-    if (api.chainId === 1) {
-        const ethBalance = (await api.provider.getBalance('0xFC7599cfFea9De127a9f9C748CCb451a34d2F063', block));
-        const usdcBalance = await api.call({ abi: 'erc20:balanceOf', params: ['0x54FD7bA87DDBDb4b8a28AeE34aB8ffC4004687De'], target: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', block: block })
-        api.addTokens([ADDRESSES.ethereum.WETH, ADDRESSES.ethereum.USDC], [ethBalance, usdcBalance])
-    }
-    if (api.chainId === 8453) {
-        const ethBalance = (await api.provider.getBalance('0xFC7599cfFea9De127a9f9C748CCb451a34d2F063', block));
-        const usdcBalance = await api.call({ abi: 'erc20:balanceOf', params: ['0xA9452cA8281556DAfA4C0d3DA3dcaAa184941032'], target: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', block: block })
-        api.addTokens([ADDRESSES.base.WETH, ADDRESSES.base.USDC], [ethBalance, usdcBalance])
-    }
-    if (api.chainId === 10) {
-        const opBalance = await api.call({ abi: 'erc20:balanceOf', params: ['0x1E65e48532f6Cf9747774777F3f1F6dC6cf0D81b'], target: '0x4200000000000000000000000000000000000042', block: block })
-        const usdcBalance = await api.call({ abi: 'erc20:balanceOf', params: ['0x7856493B59cdb1685757A6DcCe12425F6a6666a0'], target: '0x0b2c639c533813f4aa9d7837caf62653d097ff85', block: block })
-        api.addTokens([ADDRESSES.optimism.OP, ADDRESSES.optimism.USDC], [opBalance, usdcBalance])
-    }
-    if (api.chainId === 42161) {
-        const arbBalance = await api.call({ abi: 'erc20:balanceOf', params: ['0x1E65e48532f6Cf9747774777F3f1F6dC6cf0D81b'], target: '0x912ce59144191c1204e64559fe8253a0e49e6548', block: block })
-        const usdcBalance = await api.call({ abi: 'erc20:balanceOf', params: ['0x7856493B59cdb1685757A6DcCe12425F6a6666a0'], target: '0xaf88d065e77c8cc2239327c5edb3a432268e5831', block: block })
-        api.addTokens([ADDRESSES.arbitrum.ARB, ADDRESSES.arbitrum.USDC], [arbBalance, usdcBalance])
-    }
+const config = {
+  ethereum: { tokensAndOwners: [
+    [ADDRESSES.null, '0xFC7599cfFea9De127a9f9C748CCb451a34d2F063'],
+    [ADDRESSES.ethereum.USDC, '0x54FD7bA87DDBDb4b8a28AeE34aB8ffC4004687De']
+  ]},
+  optimism: { tokensAndOwners: [
+    [ADDRESSES.optimism.OP, '0x1E65e48532f6Cf9747774777F3f1F6dC6cf0D81b'],
+    [ADDRESSES.optimism.USDC_CIRCLE, '0x7856493B59cdb1685757A6DcCe12425F6a6666a0']
+  ]},
+  arbitrum: { tokensAndOwners: [
+    [ADDRESSES.arbitrum.ARB, '0x1E65e48532f6Cf9747774777F3f1F6dC6cf0D81b'],
+    [ADDRESSES.arbitrum.USDC_CIRCLE, '0x7856493B59cdb1685757A6DcCe12425F6a6666a0']
+  ]},
+  base: { tokensAndOwners: [
+    // [ADDRESSES.base.WETH, '0xFC7599cfFea9De127a9f9C748CCb451a34d2F063'],
+    [ADDRESSES.base.USDC, '0xA9452cA8281556DAfA4C0d3DA3dcaAa184941032']
+  ]}
 }
 
-module.exports = {
-    doublecounted: false,
-    methodology:
-        "Counts the amount of tokens using PikeTokens",
-    ethereum: { tvl, },
-    base: { tvl, },
-    optimism: { tvl, },
-    arbitrum: { tvl, },
-};
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = {
+    tvl: sumTokensExport(config[chain])
+  }
+})
