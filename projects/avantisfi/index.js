a5e069125824d1acff81dc39bd82aa740c387781
skords <skords@gmail.com>
2024-02-05 21:16:21 +0530

    add avantis tvl

--- projects/avantisfi/index.js
+++ projects/avantisfi/index.js
@@ -2,21 +2,22 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const VaultManager = "0xe9fB8C70aF1b99F2Baaa07Aa926FCf3d237348DD"
 
 async function tvl(_, _1, { base: block }, { api }) {
-    const vaultManagerBalance = await api.call({
-        target: VaultManager,
-        abi: 'function currentBalanceUSDC() view returns (uint256)',
-        block
-    });
+
+    const [vaultManagerBalance, collateralBalance] = await Promise.all([
+        api.call({
+            target: VaultManager,
+            abi: 'function currentBalanceUSDC() view returns (uint256)',
+            block
+        }),
+        api.call({
+            abi: 'erc20:balanceOf',
+            target: ADDRESSES.base.USDC,
+            params: [VaultManager],
+            block
+        })
+    ]);
 
     api.add(ADDRESSES.base.USDC, vaultManagerBalance)
-
-    const collateralBalance = await api.call({
-        abi: 'erc20:balanceOf',
-        target: ADDRESSES.base.USDC,
-        params: [VaultManager],
-        block
-    });
-
     api.add(ADDRESSES.base.USDC, collateralBalance)
 }
 
