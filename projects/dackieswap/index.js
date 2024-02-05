f187928baf2781869139741cb868c79c0bc65518
DackieSwap <132213730+DackieSwap@users.noreply.github.com>
2024-02-05 18:18:47 +0700

    add DackieSwap Optimism (#8851)
    * add DackieSwap Optimism
    
    * update v3 Factory DackieSwap Optimism
    
    ---------
    
    Co-authored-by: andreapn.eth <fifi.hate.lonely@gmail.com>

--- projects/dackieswap/index.js
+++ projects/dackieswap/index.js
@@ -1,5 +1,6 @@
 const { uniV3Export } = require("../helper/uniswapV3");
 
 module.exports = uniV3Export({
-  base: { factory: "0x3D237AC6D2f425D2E890Cc99198818cc1FA48870", fromBlock: 2031627, },
+  base: { factory: "0x3D237AC6D2f425D2E890Cc99198818cc1FA48870", fromBlock: 2031627 },
+  optimism: { factory: "0xc2BC7A73613B9bD5F373FE10B55C59a69F4D617B", fromBlock: 115172042 }
 });
