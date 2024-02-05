0778acb4780bbe4bfb90750763a80a04e77236ac
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-05 12:13:45 +0100

    Refactor TVL calculation in Omnipool index.js

--- projects/omnipool/index.js
+++ projects/omnipool/index.js
@@ -1,22 +1,8 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const sdk = require('@defillama/sdk')
-
 async function tvl(_, _1, _2, { api }) {
-    const pooledZeta = await sdk.api.abi.call({
-        chain: 'zeta',
-        target: "0x82bbc3f521E5313cf5e8401797d7BaB6c030C908",
-        abi: "uint256:getTotalPooledZeta"
-    })
-
-    return {
-        [ADDRESSES.null]: pooledZeta.output,
-    }
+  const pooledZeta = await api.call({ target: "0x82bbc3f521E5313cf5e8401797d7BaB6c030C908", abi: "uint256:getTotalPooledZeta" })
+  api.addGasToken(pooledZeta)
 }
 
 module.exports = {
-    timetravel: true,
-    doublecounted: true,
-    zeta: {
-        tvl
-    }
+  zeta: { tvl }
 }
