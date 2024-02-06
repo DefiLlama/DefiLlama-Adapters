1cb03b675db06ae7524a5039fd7c9bba2b02ea08
sgmntp <146187995+sgmntp@users.noreply.github.com>
2024-02-06 11:07:49 +0100

    add (Segment Finance) cEther address

--- projects/segment-finance/index.js
+++ projects/segment-finance/index.js
@@ -4,7 +4,10 @@ const { staking } = require("../helper/staking");
 module.exports = {
   methodology: "Same as Compound Finance, we just count all the tokens supplied (not borrowed money) on the lending markets",
   bsc: {
-    ...compoundExports2({ comptroller: "0x57E09c96DAEE58B77dc771B017de015C38060173" }),
+    ...compoundExports2({
+        comptroller: "0x57E09c96DAEE58B77dc771B017de015C38060173",
+        cether: "0xC6BcBe182b0F85dBfF6b49DC81CecEe02A16fE57",
+    }),
     staking: staking(
       [
         "0xC6BcBe182b0F85dBfF6b49DC81CecEe02A16fE57",
