79f16a188b013a6459f711c074beb3b131e9fc72
unknown <ujjwalg056@gmail.com>
2024-02-06 16:27:39 +0530

    solana: tvl

--- projects/flashtrade/index.js
+++ projects/flashtrade/index.js
@@ -6,14 +6,17 @@ async function tvl(_, _b, _cb, { api }) {
     "BC5xAUpEbfeSWi5fJdvhFQhM3eMbTok2c7SY62daB3da", // USDC
     "FuFoCkfnrDjNmwPr54JEAYTUshXA4gQojevfvv3KXdx7", // ETH
     "55UmrYacpb8v7gbKswDofmWjLS8TSP3VB8NKjNfxu11d", // BTC
-    "7b2jY9CeCWCnyKBvaLSnsV7qwUhbJGsJTPdyCsspPY7Q" // Virtual Pool - USDC 
+    "7b2jY9CeCWCnyKBvaLSnsV7qwUhbJGsJTPdyCsspPY7Q", // Virtual Pool - USDC
   ];
 
   return sumTokens2({ tokenAccounts: vaults });
-};  
+}
 
 module.exports = {
-    timetravel: false,
-    methodology: "tvl is the usd equivalent value of all the assets in our pools.",
-    tvl,
+  timetravel: false,
+  methodology:
+    "tvl is the usd equivalent value of all the assets in our pools.",
+  solana: { 
+    tvl 
+},
 };
