a1bdacb5d00a7e5f54e72753b06aa50e60cdd1b7
Tom Rpl <tomireppelin@gmail.com>
2024-02-06 14:08:09 +0100

    feat(morpho-blue): adding more addresses

--- projects/morpho-blue/addresses.js
+++ projects/morpho-blue/addresses.js
@@ -1,12 +1,24 @@
-const wethWstethChainlinkAdaptiveCurveIRM945 =
+const wstethWethChainlinkAdaptiveCurveIRM945 =
   "0xc54d7acf14de29e0e5527cabd7a576506870346a78a11a6762e2cca66322ec41";
-const usdcWstethChainlinkAdaptiveCurveIRM860 =
+const wstethUsdcChainlinkAdaptiveCurveIRM860 =
   "0xb323495f7e4148be5643a4ea4a8221eef163e4bccfdedc2a6f4696baacbc86cc";
+const weethWethChainlinkAdaptiveCurveIRM860 =
+  "0x698fe98247a40c5771537b5786b2f3f9d78eb487b4ce4d75533cd0e94d88a115";
+const wbib01UsdcChainlinkAdaptiveCurveIRM965 =
+  "0x495130878b7d2f1391e21589a8bcaef22cbc7e1fbbd6866127193b3cc239d8b1";
+const osethWethChainlinkAdaptiveCurveIRM860 =
+  "0xd5211d0e3f4a30d5c98653d988585792bb7812221f04801be73a44ceecb11e89";
+const idleWETH =
+  "0x58e212060645d18eab6d9b2af3d56fbc906a92ff5667385f616f662c70372284";
 
 module.exports = {
   morphoBlue: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
   whitelistedIds: [
-    wethWstethChainlinkAdaptiveCurveIRM945,
-    usdcWstethChainlinkAdaptiveCurveIRM860,
+    wstethWethChainlinkAdaptiveCurveIRM945,
+    wstethUsdcChainlinkAdaptiveCurveIRM860,
+    weethWethChainlinkAdaptiveCurveIRM860,
+    wbib01UsdcChainlinkAdaptiveCurveIRM965,
+    osethWethChainlinkAdaptiveCurveIRM860,
+    idleWETH,
   ],
 };
