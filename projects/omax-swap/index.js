4ecd241f1918d5c6570f7f80bfa17d9c9b00006e
pro100skm <pro100skm@yandex.ru>
2024-02-08 20:00:16 +0300

    - native omax staking

--- projects/omax-swap/index.js
+++ projects/omax-swap/index.js
@@ -1,3 +1,13 @@
 const { uniTvlExport } = require('../helper/unknownTokens')
+const { staking } = require("../helper/staking");
+const ADDRESSES = require("../helper/coreAssets.json");
 
-module.exports = uniTvlExport('omax', '0x441b9333D1D1ccAd27f2755e69d24E60c9d8F9CF')
+
+const stakingContractAddress = 0x3A2DcDc705031eDBD94254ef7CEFB93D8066cC8D
+
+module.exports = {
+  polygon: {
+    tvl: uniTvlExport('omax', '0x441b9333D1D1ccAd27f2755e69d24E60c9d8F9CF'),
+    staking: staking(stakingContractAddress, ADDRESSES.null, "omax")
+  }
+}
