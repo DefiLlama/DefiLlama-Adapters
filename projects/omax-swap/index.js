ec3c6db87f81ef3b793309652cef5326acabc80c
pro100skm <pro100skm@yandex.ru>
2024-02-08 22:08:20 +0300

    - with getBalance

--- projects/omax-swap/index.js
+++ projects/omax-swap/index.js
@@ -1,14 +1,14 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
+const { uniTvlExport, getUniTVL } = require('../helper/unknownTokens')
 const ADDRESSES = require("../helper/coreAssets.json");
 const sdk = require("@defillama/sdk");
 
 const WOMAX = ADDRESSES.omax.WOMAX;
-const stakingContractAddress = 0x3A2DcDc705031eDBD94254ef7CEFB93D8066cC8D
+const stakingContractAddress = '0x3A2DcDc705031eDBD94254ef7CEFB93D8066cC8D'
 
-async function staking(time, _ethBlock, {omax: block}) {
+async function staking(time, _ethBlock, chainBlocks) {
   const resp = await sdk.api.eth.getBalance({
     target: stakingContractAddress,
-    block,
+    block: chainBlocks.omax,
     chain: "omax",
   });
   return {
@@ -17,8 +17,9 @@ async function staking(time, _ethBlock, {omax: block}) {
 }
 
 module.exports = {
-  polygon: {
-    tvl: uniTvlExport('omax', '0x441b9333D1D1ccAd27f2755e69d24E60c9d8F9CF'),
+  misrepresentedTokens: true,
+  omax: {
+    tvl: getUniTVL({ factory: '0x441b9333D1D1ccAd27f2755e69d24E60c9d8F9CF', useDefaultCoreAssets: true, chain: 'omax' }),
     staking
   }
 }
