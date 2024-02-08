2dfc92e5797b268a4b9907ce642947dfdd3b0611
0xBluePingu <156801910+0xBluePingu@users.noreply.github.com>
2024-02-08 14:27:09 +0100

    add start timestamp (#8897)
    Co-authored-by: 0xBluePingu <0xBluePingu@pingu.exchange>

--- projects/pingu/index.js
+++ projects/pingu/index.js
@@ -5,8 +5,9 @@ const PINGU = "0x4615fa30fFA5716984d4372030ce28D99fCB702f"; // PINGU
 const assets = [nullAddress, "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"] // ETH, USDC
 
 module.exports = {
-  arbitrum: {
-    tvl: sumTokensExport({ owners: [fundStore], tokens: assets}),
-    staking: sumTokensExport({ owners: [fundStore], tokens: [PINGU]}),
-  },
+	start: 1704844800,
+	arbitrum: {
+		tvl: sumTokensExport({ owners: [fundStore], tokens: assets }),
+		staking: sumTokensExport({ owners: [fundStore], tokens: [PINGU] }),
+	},
 }
