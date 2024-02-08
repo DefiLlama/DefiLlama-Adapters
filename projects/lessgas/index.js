35cfcbf08eb048c4611161ce9760520e9a91a317
evan <evan@gmail.com>
2024-02-08 14:25:46 +0800

    Change calculation method

--- projects/lessgas/index.js
+++ projects/lessgas/index.js
@@ -2,8 +2,8 @@ const sdk = require("@defillama/sdk");
 const axios = require('axios')
 
 const BigNumber = require("bignumber.js");
-const HOST='https://api2.lessgas.xyz'
-// const HOST='http://127.0.0.1:9912'
+// const HOST='https://api2.lessgas.xyz'
+const HOST='http://127.0.0.1:9912'
 const request_staking = async () => {
   let config = {
     method: 'get',
@@ -22,11 +22,25 @@ const request_tvl = async () => {
     maxBodyLength: Infinity,
     url: `${HOST}/llama/tvl`,
   };
-  let response =  await axios.request(config)
-  console.log(response.data);
-  let result = response.data.data;
-
-  return { tether: new BigNumber(result.result).multipliedBy(new BigNumber(result.price)) };
+  //Get supported tokens
+  let response =  await axios.request(config);
+  let tokens = response.data.data.tokens;
+  let total = 0;
+  for (const token of tokens) {
+    //Obtain the quantity that has been minted
+    let {output} = await sdk.api.abi.call({
+      abi: 'erc20:totalSupply',
+      target: token.contract,
+      params: [],
+      chain: "map",
+      // block: chainBlocks["arbitrum"],
+    });
+    //Accumulate the quantity
+    total = new BigNumber(output).div(new BigNumber(10**(token.decimals))).multipliedBy(new BigNumber(token.price)).plus(new BigNumber(total));
+    // console.log(output);
+  }
+  // return { tether: new BigNumber(result.result).multipliedBy(new BigNumber(result.price)) };
+  return { tether: total };
 };
 
 module.exports = {
