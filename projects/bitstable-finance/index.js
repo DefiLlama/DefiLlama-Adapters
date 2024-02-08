10172fda6b0dd2ed85e55374a097d11637065abc
pro100skm <pro100skm@yandex.ru>
2024-02-08 22:29:18 +0300

    - put error on bitstable back

--- projects/bitstable-finance/index.js
+++ projects/bitstable-finance/index.js
@@ -7,7 +7,7 @@ const {
 const owner = "0x103dd1184599c7511a3016E0a383E11F84AE7173";
 const tokens = {
   ethereum: [ADDRESS.ethereum.USDT],
-  bsc: [ADDRESS.ethereum.FDUSD],
+  bsc: [ADDRESSES.ethereum.FDUSD],
 };
 
 module.exports = {
