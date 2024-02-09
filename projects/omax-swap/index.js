9bde6d4b67a1facf587eb8fd20d5a435ecefa98c
pro100skm <pro100skm@yandex.ru>
2024-02-09 14:52:14 +0300

    - magic

--- projects/omax-swap/index.js
+++ projects/omax-swap/index.js
@@ -6,20 +6,19 @@ const WOMAX = ADDRESSES.omax.WOMAX;
 const stakingContractAddress = '0x3A2DcDc705031eDBD94254ef7CEFB93D8066cC8D'
 
 async function staking(time, _ethBlock, chainBlocks) {
-  const resp = await sdk.api.eth.getBalance({
+  const { output } = await sdk.api.eth.getBalance({
     target: stakingContractAddress,
-    block: chainBlocks.omax,
     chain: "omax",
   });
-  return {
-    [WOMAX]: resp.output,
-  };
+  const balances = {}
+  sdk.util.sumSingleBalance(balances, WOMAX, output, 'omax')
+  return balances;
 }
 
 module.exports = {
   misrepresentedTokens: true,
   omax: {
     tvl: getUniTVL({ factory: '0x441b9333D1D1ccAd27f2755e69d24E60c9d8F9CF', useDefaultCoreAssets: true, chain: 'omax' }),
-    staking
+    staking,
   }
 }
