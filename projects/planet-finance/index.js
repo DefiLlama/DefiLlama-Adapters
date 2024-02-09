f4976414a31bdb3081cbdbdb2b0ef4e548afd286
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-09 19:33:15 +0100

    Update ABI files for Planet Finance

--- projects/planet-finance/index.js
+++ projects/planet-finance/index.js
@@ -1,167 +1,53 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const sdk = require("@defillama/sdk");
 const abi = require("./abi.json");
-const abiNew = require("./abiNew.json");
 const abiV3 = require("./abiV3.json");
-const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
+const abiNew = require("./abiNew.json");
+const { sumTokens2 } = require("../helper/unwrapLPs");
 const { staking } = require("../helper/staking.js");
-const { getPoolInfo } = require("../helper/masterchef.js");
 
-const replacements = {
-  [ADDRESSES.bsc.beltBNB]:
-    ADDRESSES.bsc.WBNB, //beltBNB -> wbnb
-  [ADDRESSES.bsc.Belt4]:
-    ADDRESSES.bsc.USDT, // 4belt -> usdt
-  [ADDRESSES.bsc.beltBTC]:
-    ADDRESSES.bsc.BTCB, //beltBTC->
-  [ADDRESSES.bsc.beltETH]:
-    ADDRESSES.bsc.ETH, //beltETH->
-};
-
-const transform = i => `bsc:${i}`;
 // liquidity pools
-async function tvl(timestamp, ethBlock, chainBlocks) {
-  let balances = {};
-  const lps = [];
-  const thenaLps = [];
-  const transform = i => `bsc:${i}`;
-
-  const [ poolInfo1, poolInfo2, poolInfo3, poolInfo4] = await Promise.all([
-    await getPoolInfo(
-      "0x0ac58Fd25f334975b1B61732CF79564b6200A933",
-      chainBlocks.bsc,
-      "bsc",
-      abi.poolInfo
-    ), // aquaFarm
-    await getPoolInfo(
-      "0xB87F7016585510505478D1d160BDf76c1f41b53d",
-      chainBlocks.bsc,
-      "bsc",
-      abi.poolInfo
-    ),  // newFarm
-    await getPoolInfo(
-      "0x9EBce8B8d535247b2a0dfC0494Bc8aeEd7640cF9",
-      chainBlocks.bsc,
-      "bsc",
-      abiNew.poolInfo
-    ),  // newFarm
-    await getPoolInfo(
-      "0x405960AEAad7Ec8B419DEdb511dfe9D112dFc22d",
-      chainBlocks.bsc,
-      "bsc",
-      abiV3.poolInfo
-    ),  // v3 Farm
-  ]);
+async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
+  const poolInfo1 = await api.fetchList({ lengthAbi: abi.poolLength, itemAbi: abi.poolInfo, target: '0x0ac58Fd25f334975b1B61732CF79564b6200A933' })
+  const poolInfo2 = await api.fetchList({ lengthAbi: abi.poolLength, itemAbi: abi.poolInfo, target: '0xB87F7016585510505478D1d160BDf76c1f41b53d' })
+  const poolInfo3 = await api.fetchList({ lengthAbi: abiNew.poolLength, itemAbi: abiNew.poolInfo, target: '0x9EBce8B8d535247b2a0dfC0494Bc8aeEd7640cF9' })
+  const poolInfo4 = await api.fetchList({ lengthAbi: abiV3.poolLength, itemAbi: abiV3.poolInfo, target: '0x405960AEAad7Ec8B419DEdb511dfe9D112dFc22d' })
   let allPoolInfos = poolInfo1.concat(poolInfo2).concat(poolInfo3).concat(poolInfo4);
 
-  allPoolInfos = allPoolInfos.filter(function (el) {
-    return el.output.want != "0xC9440dEb2a607A6f6a744a9d142b16eD824A1A3b";
-  });
-
-  const [poolTvl, wantSymbol] = await Promise.all([
-    sdk.api.abi.multiCall({
-      calls: allPoolInfos.map(p => ({
-        target: p.output.strat
-      })),
-      abi: abi.wantLockedTotal,
-      chain: "bsc",
-      block: chainBlocks.bsc,
-    }),
-    sdk.api.abi.multiCall({
-      calls: allPoolInfos.map(p => ({
-        target: p.output.want
-      })),
-      abi: "erc20:symbol",
-      chain: "bsc",
-      block: chainBlocks.bsc,
-    })
-  ]);
-
-
-  for (let i = 0; i < poolTvl.output.length; i++) {
-    if (wantSymbol.output[i].output.endsWith("LP")) {
-      lps.push({
-        token: allPoolInfos[i].output.want,
-        balance: poolTvl.output[i].output,
-      });
-    } else if (wantSymbol.output[i].output.indexOf("a") == 0) {
-      thenaLps.push({
-        token: allPoolInfos[i].output.want,
-        balance: poolTvl.output[i].output,
-      });
-    }else {
-      const addr = replacements[allPoolInfos[i].output.want]
-        ?? allPoolInfos[i].output.want;
-      sdk.util.sumSingleBalance(
-        balances,
-        transform(addr),
-        poolTvl.output[i].output
-      );
+  allPoolInfos = allPoolInfos.filter(el => el.want.toLowerCase() !== "0xC9440dEb2a607A6f6a744a9d142b16eD824A1A3b".toLowerCase() && el.strat.toLowerCase() !== "0x0000000000000000000000000000000000000000".toLowerCase())
+  const tokens = allPoolInfos.map(i => i.want)
+  const bals = (await api.multiCall({ abi: abi.wantLockedTotal, calls: allPoolInfos.map(i => i.strat), permitFailure: true })).map(i => i ?? 0)
+  const _totalAmounts = await api.multiCall({ abi: abiV3.getTotalAmounts, calls: tokens, permitFailure: true })
+  const totalAmounts = []
+  const thetaTokens = []
+  const thetaBals = []
+  _totalAmounts.forEach((amounts, i) => {
+    if (amounts) {
+      totalAmounts.push(amounts)
+      thetaTokens.push(tokens[i])
+      thetaBals.push(bals[i])
+    } else {
+      api.add(tokens[i], bals[i])
     }
+  })
+  const supplies = await api.multiCall({ abi: abiV3.totalSupply, calls: thetaTokens })
+  const token0s = await api.multiCall({ abi: abiV3.token0, calls: thetaTokens })
+  const token1s = await api.multiCall({ abi: abiV3.token1, calls: thetaTokens })
+  for (let i = 0; i < totalAmounts.length; i++) {
+    const totalSupply = supplies[i]
+    const token0 = token0s[i]
+    const token1 = token1s[i]
+    const vaultAmt = totalAmounts[i]
+    const ratio = thetaBals[i] / totalSupply
+    api.add(token0, ratio * vaultAmt.total0)
+    api.add(token1, ratio * vaultAmt.total1)
   }
 
-  await unwrapUniswapLPs(
-    balances,
-    lps,
-    chainBlocks.bsc,
-    "bsc",
-    transform
-  );
-
-  await thenaTvl(timestamp, chainBlocks, thenaLps, balances);
-
-  return balances;
+  return sumTokens2({ api, resolveLP: true, })
 }
 
-async function thenaTvl(timestamp, chainBlocks, thenaLps, balances) {
-  const vaultCalls = thenaLps.map(v => ({ target: v.token }))
-
-  const { output: vaultAmts } = await sdk.api.abi.multiCall({ 
-    abi: abiV3.getTotalAmounts, 
-    calls: vaultCalls, 
-    chain: "bsc",
-    block: chainBlocks.bsc,
-  })
-
-  const { output: token0 } = await sdk.api.abi.multiCall({ 
-    abi: abiV3.token0, 
-    calls: vaultCalls, 
-    chain: "bsc",
-    block: chainBlocks.bsc, 
-  })
-  const { output: token1 } = await sdk.api.abi.multiCall({ 
-    abi: abiV3.token1, 
-    calls: vaultCalls, 
-    chain: "bsc",
-    block: chainBlocks.bsc
-  })
-
-  const { output: totalSupplies } = await sdk.api.abi.multiCall({ 
-    abi: abiV3.totalSupply, 
-    calls: vaultCalls, 
-    chain: "bsc",
-    block: chainBlocks.bsc
-  })
-
-  vaultAmts.map((vaultAmt, i) => {
-    const ratio = thenaLps[i].balance/ totalSupplies[i].output;
-    sdk.util.sumSingleBalance(balances, transform(token0[i].output), ratio * vaultAmt.output.total0);
-    sdk.util.sumSingleBalance(balances, transform(token1[i].output) , ratio * vaultAmt.output.total1);
-  })
-
-  return balances;
-}
-
-
-// node test.js projects/planet-finance/index.js
 module.exports = {
   doublecounted: true,
   bsc: {
     tvl,
-    staking: staking(
-      "0x60A895073AdC0e5F5a22C60bdfc584D79B5219a1",
-      "0xb3cb6d2f8f2fde203a022201c81a96c167607f15",
-      "bsc",
-    )
+    staking: staking("0x60A895073AdC0e5F5a22C60bdfc584D79B5219a1", "0xb3cb6d2f8f2fde203a022201c81a96c167607f15",)
   },
 };
