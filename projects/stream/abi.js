9d3bb368ea0bf37c4b3b47624fee8d182c9f01bd
Alejandro Gala <alegala21@gmail.com>
2024-02-09 20:35:01 -0500

    add new vaults

--- projects/stream/abi.js
+++ projects/stream/abi.js
@@ -1,4 +1,7 @@
 module.exports = {
   currentExchangeRate:
     "function currentExchangeRate() view returns (uint256 vaultToken, uint256 baseToken)",
+  totalBalance:
+    "function totalBalance() public view returns (uint256)"
 };
+
