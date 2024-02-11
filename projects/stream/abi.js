b07309f3f0bff843664c6ef26ce26efef9feb012
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-11 14:39:17 +0100

    Refactor TVL calculation and add support for multiple vaults

--- projects/stream/abi.js
+++ projects/stream/abi.js
@@ -1,7 +1,6 @@
 module.exports = {
-  currentExchangeRate:
-    "function currentExchangeRate() view returns (uint256 vaultToken, uint256 baseToken)",
-  totalBalance:
-    "function totalBalance() public view returns (uint256)"
+  vaultParams: "function vaultParams() view returns (uint8 decimals, address asset, uint56 minimumSupply, uint104 cap )",
+  currentExchangeRate: "function currentExchangeRate() view returns (uint256 vaultToken, uint256 baseToken)",
+  totalBalance: "uint256:totalBalance"
 };
 
