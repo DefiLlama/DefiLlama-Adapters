1e7859b1be21c017d0b0c8ee03c0fa6df62dd47e
Bruno Wu <wubruno@outlook.com>
2024-02-12 11:15:29 +0000

    add the zil address

--- projects/web3war/index.js
+++ projects/web3war/index.js
@@ -1,13 +1,13 @@
 const BSC_FPS_TOKEN_CONTRACT = "0x351dA1E7500aBA1d168b9435DCE73415718d212F";
 const ZQ_FPS_TOKEN_CONTRACT = "0x241c677D9969419800402521ae87C411897A029f";
 const ZQ_FPS_BRIDGE_CONTRACT = "0x6D61eFb60C17979816E4cE12CD5D29054E755948";
+const ZQ_FPS_ZIL_TOKEN_ADDRESS = "zil1j2wrzjljwyjelspmtr63vfl34c467ype2w3mjl";
 
 async function tvl(_, _a, _c, { api, chain }) {
   if (chain === "zilliqa") {
     const supply = await api.call({
       abi: "erc20:totalSupply",
       target: ZQ_FPS_TOKEN_CONTRACT,
-      params: [],
     });
 
     const lockedAmount = await api.call({
@@ -18,7 +18,7 @@ async function tvl(_, _a, _c, { api, chain }) {
     const circulatingSupply = supply - lockedAmount;
     console.log(circulatingSupply);
 
-    api.add("web3war", circulatingSupply);
+    api.add(ZQ_FPS_ZIL_TOKEN_ADDRESS, circulatingSupply);
   } else {
     const supply = await api.call({
       abi: "erc20:totalSupply",
