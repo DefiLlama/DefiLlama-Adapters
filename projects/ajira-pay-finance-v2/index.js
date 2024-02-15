8d1ade6c08b990962dbd56982a6bfce62108e21d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-15 12:05:03 +0100

    code refactor

--- projects/ajira-pay-finance-v2/index.js
+++ projects/ajira-pay-finance-v2/index.js
@@ -1,36 +1,8 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokens2, sumTokensExport } = require('../helper/unwrapLPs')
 const { covalentGetTokens } = require('../helper/token')
-const { isWhitelistedToken } = require('../helper/streamingHelper')
-const { getUniqueAddresses } = require('../helper/utils')
+const { getWhitelistedTokens } = require('../helper/streamingHelper')
 
 const config = {
   linea: { owners: ['0xD2AA294B9A5097F4A09fd941eD0bE665bd85Eab2'], },
-}
-
-const blacklistedTokens = [
-  ADDRESSES.ethereum.sUSD_OLD,
-  ADDRESSES.ethereum.SAI.toLowerCase(),
-  ADDRESSES.ethereum.MKR,
-]
-
-async function getTokens(api, owners, isVesting) {
-  let tokens = (await Promise.all(owners.map(i => covalentGetTokens(i, api, { onlyWhitelisted: false, })))).flat().filter(i => !blacklistedTokens.includes(i))
-  tokens = getUniqueAddresses(tokens)
-  const symbols = await api.multiCall({ abi: 'erc20:symbol', calls: tokens })
-  return tokens.filter((v, i) => isWhitelistedToken(symbols[i], v, isVesting))
-}
-
-async function tvl(_, block, _1, { api }) {
-  const { owners } = config[api.chain]
-  const tokens = await getTokens(api, owners, false)
-  return sumTokens2({ api, owners, tokens, blacklistedTokens, })
-}
-
-async function vesting(_, block, _1, { api }) {
-  const { owners } = config[api.chain]
-  const tokens = await getTokens(api, owners, true)
-  return sumTokens2({ api, owners, tokens, blacklistedTokens, })
 }
 
 module.exports = {
@@ -38,5 +10,16 @@ module.exports = {
 }
 
 Object.keys(config).forEach(chain => {
-  module.exports[chain] = { tvl, vesting }
+  const { owners } = config[chain]
+  module.exports[chain] = {
+    tvl: tvl(false),
+    vesting: tvl(true),
+  }
+
+  function tvl(isVesting) {
+    return async (_, _1, _2, { api }) => {
+      const tokens = (await Promise.all(owners.map(i => covalentGetTokens(i, api, { onlyWhitelisted: false, })))).flat()
+      return api.sumTokens({ owners, tokens: await getWhitelistedTokens({ api, tokens, isVesting})})
+    }
+  }
 })
