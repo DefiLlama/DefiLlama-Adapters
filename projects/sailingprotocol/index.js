c99a493be99c2ff9c94a8b2e3abee463d8d8a7eb
Nicholas Szerman <nicszerman@gmail.com>
2024-02-16 16:00:04 -0500

    added pricing mechanism for tokens without enough LP liquidity

--- projects/sailingprotocol/index.js
+++ projects/sailingprotocol/index.js
@@ -1,19 +1,41 @@
 const sdk = require('@defillama/sdk')
+const axios = require('axios')
 
-const SPY = '0x75B5DACEc8DACcb260eA47549aE882513A21CE01';
-const ARKK = '0x2E4763AdBEe00D5eB3089ec25973599c0e62dD07';
-
+const tokens = [
+  {
+    "address": "0x75B5DACEc8DACcb260eA47549aE882513A21CE01",
+    "ticker": "SPY",
+    "sufficientLiquidityForDefiLlamaIndexer": false,
+  },
+  {
+    "address": "0x2E4763AdBEe00D5eB3089ec25973599c0e62dD07",
+    "ticker": "ARKK",
+    "sufficientLiquidityForDefiLlamaIndexer": false,
+  }
+];
 async function tvl(_0, _1, _2, { api }) {
-  const allTokenAddresses = [SPY, ARKK];
-  for (const tokenAddress of allTokenAddresses) {
-    const tokenTotalSupply = await api.call({ abi: 'erc20:totalSupply', target: tokenAddress });
-    api.add(tokenAddress, tokenTotalSupply);
+  for (const token of tokens) {
+    const tokenTotalSupply = await api.call({ abi: 'erc20:totalSupply', target: token.address });
+    api.add(token.address, tokenTotalSupply);
+    if (!token.sufficientLiquidityForDefiLlamaIndexer) {
+      const tickerPricing = await axios.post(
+        'https://sailingprotocol.org/api/sailingprotocol/market_data/historical_intraday',
+        {
+          ticker: token.ticker
+        }
+      );
+      const tickerPrice = tickerPricing.data.at(-1)[1];
+      api.add(
+        '0x919C1c267BC06a7039e03fcc2eF738525769109c', // usdtKavaAddress
+        tokenTotalSupply * tickerPrice * (1e6 / 1e18)
+      );
+    }
   }
 }
 
 module.exports = {
-  misrepresentedTokens: false,
-  timetravel: true,
+  misrepresentedTokens: true, // false, // until all tokens are indexed by defillama
+  timetravel: false, // true, // until all tokens are indexed by defillama
   kava: { tvl, },
   methodology: 'The total supply of their circulating stocks is extracted from their stock token contracts.'
 }
