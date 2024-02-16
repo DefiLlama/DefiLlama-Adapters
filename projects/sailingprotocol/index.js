e4b8b366539c4cb219f2f87e7f2faab7f1ddf6d0
Nicholas Szerman <nicszerman@gmail.com>
2024-02-16 18:02:03 -0500

    fetches price from liquidity pool

--- projects/sailingprotocol/index.js
+++ projects/sailingprotocol/index.js
@@ -6,28 +6,29 @@ const tokens = [
     "address": "0x75B5DACEc8DACcb260eA47549aE882513A21CE01",
     "ticker": "SPY",
     "sufficientLiquidityForDefiLlamaIndexer": false,
+    "priceLiquidityPool": "0xeA55Df929438f8A1af003A59381ba57aB2f3fb1f",
   },
   {
     "address": "0x2E4763AdBEe00D5eB3089ec25973599c0e62dD07",
     "ticker": "ARKK",
     "sufficientLiquidityForDefiLlamaIndexer": false,
+    "priceLiquidityPool": "0x56742D48Fd18C236aCEd6317c192eCFB2d4bd792",
   }
 ];
+
 async function tvl(_0, _1, _2, { api }) {
   for (const token of tokens) {
-    const tokenTotalSupply = await api.call({ abi: 'erc20:totalSupply', target: token.address });
+    const tokenTotalSupply = await api.call({ target: token.address, abi: 'erc20:totalSupply' });
     api.add(token.address, tokenTotalSupply);
     if (!token.sufficientLiquidityForDefiLlamaIndexer) {
-      const tickerPricing = await axios.post(
-        'https://sailingprotocol.org/api/sailingprotocol/market_data/historical_intraday',
-        {
-          ticker: token.ticker
-        }
-      );
-      const tickerPrice = tickerPricing.data.at(-1)[1];
+      const liquidityPoolAddress = token.priceLiquidityPool;
+      const slot0abi = 'function slot0() view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, uint32 feeProtocol, bool unlocked)';
+      const slot0 = await api.call({ target: liquidityPoolAddress, abi: slot0abi });
+      const tokenPriceUSDT = ((slot0.sqrtPriceX96 / (2 ** 96)) ** 2) * (1e18 / 1e6);
+      // the pool implied token price adjusted for token decimals
       api.add(
         '0x919C1c267BC06a7039e03fcc2eF738525769109c', // usdtKavaAddress
-        tokenTotalSupply * tickerPrice * (1e6 / 1e18)
+        tokenTotalSupply * tokenPriceUSDT * (1e6 / 1e18)
       );
     }
   }
