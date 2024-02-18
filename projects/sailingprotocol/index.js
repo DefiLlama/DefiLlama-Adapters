e52143426586f65174cf5836a41557e99630dc2f
Nicholas Szerman <nicszerman@gmail.com>
2024-02-18 16:20:37 -0500

    added 24 hours price averaging and try catch to support time traveling

--- projects/sailingprotocol/index.js
+++ projects/sailingprotocol/index.js
@@ -15,21 +15,50 @@ const tokens = [
   }
 ];
 
-async function tvl(_0, _1, _2, { api }) {
+const averagePoolPrice24Hrs = async ({
+  poolAddress,
+  lastBlockNumber,
+  api,
+}) => {
+  const slot0abi = 'function slot0() view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, uint32 feeProtocol, bool unlocked)';
+  const getTokenPriceAtBlock = async (queryBlockNumber) => {
+    const slot0 = await api.call({
+      target: poolAddress,
+      abi: slot0abi,
+      block: queryBlockNumber
+    });
+    // below formula is token price in USDT denomination, adjusted for token decimals
+    return ((slot0.sqrtPriceX96 / (2 ** 96)) ** 2) * (1e18 / 1e6);
+  };
+  let priceAverageAggregate = 0;
+  const blocksPerHour = 1833;
+  for (let i = 0; i < 24; i++) {
+    priceAverageAggregate += await getTokenPriceAtBlock(
+      lastBlockNumber - i * blocksPerHour
+    );
+  }
+  return priceAverageAggregate / 24;
+};  
+
+async function tvl(_0, blockNumber, _2, { api }) {
   for (const token of tokens) {
-    const tokenTotalSupply = await api.call({ target: token.address, abi: 'erc20:totalSupply' });
-    api.add(token.address, tokenTotalSupply);
-    if (!token.sufficientLiquidityForDefiLlamaIndexer) {
-      const liquidityPoolAddress = token.priceLiquidityPool;
-      const slot0abi = 'function slot0() view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, uint32 feeProtocol, bool unlocked)';
-      const slot0 = await api.call({ target: liquidityPoolAddress, abi: slot0abi });
-      const tokenPriceUSDT = ((slot0.sqrtPriceX96 / (2 ** 96)) ** 2) * (1e18 / 1e6);
-      // the pool implied token price adjusted for token decimals
-      api.add(
-        '0x919C1c267BC06a7039e03fcc2eF738525769109c', // usdtKavaAddress
-        tokenTotalSupply * tokenPriceUSDT * (1e6 / 1e18)
-      );
-    }
+    try {
+      // try block to support time traveling, as more token tickers are added over time
+      const tokenTotalSupply = await api.call({ target: token.address, abi: 'erc20:totalSupply' });
+      api.add(token.address, tokenTotalSupply);
+      if (!token.sufficientLiquidityForDefiLlamaIndexer) {
+        // pool has low liquidity so price is aggregated over 24 hours
+        const tokenPriceUSDT = await averagePoolPrice24Hrs({
+          poolAddress: token.priceLiquidityPool,
+          lastBlockNumber: blockNumber,
+          api
+        });
+        api.add(
+          '0x919C1c267BC06a7039e03fcc2eF738525769109c', // usdtKavaAddress
+          tokenTotalSupply * tokenPriceUSDT * (1e6 / 1e18)
+        );
+      }
+    } catch {};
   }
 }
 
