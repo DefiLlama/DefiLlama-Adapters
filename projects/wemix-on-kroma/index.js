4b5137f32ef97ba13fb23497c96111a2fbfd93b0
martin-ryunseok <martin@lightscale.io>
2024-02-19 16:47:33 +0900

    fix: change methodology to balance of from total supply

--- projects/wemix-on-kroma/index.js
+++ projects/wemix-on-kroma/index.js
@@ -4,18 +4,20 @@ const sdk = require('@defillama/sdk');
 
 const tvl = async (_, _1, _2, { api }) => {
   const wemixEContract = '0x3720b1dc2c8dde3bd6cfcf0b593d0a2bbcac856e';
+  const stakingContract = '0xA5c7992710A94A2ef2e8E910b441bD70385DBAB8'
 
-  const wemixESupply = (await sdk.api.erc20.totalSupply({
-      target: wemixEContract,
+  const balanceOfStakingContract = (await sdk.api.erc20.balanceOf({
+    target: wemixEContract,
+    owner:stakingContract,
       chain: 'kroma',
       decimals: 18
   })).output;
 
-  return {"wemix-token": wemixESupply}
+  return {"wemix-token": balanceOfStakingContract}
 }
 
 
 module.exports["kroma"] = {
-  methodology: "The total supply of WEMIX.e tokens transferred from the Wemix chain",
+  methodology: "The balance of WEMIX.e tokens staked in Staking Wemix Contract on kroma chain",
   tvl,
 }
