7958231b0e30b3c8958d4e075f590a729c5c23b6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-20 12:00:11 +0100

    code refactor

--- projects/shprd/index.js
+++ projects/shprd/index.js
@@ -6,43 +6,17 @@ const protocolConfig = {
 }
 
 async function tvl(ts, block, _, { api }) {
-    // 1. get all vaults deployed on chain
-    const factoryAddress = protocolConfig.factories[api.chain].address
-    const factoryState = await api.call({
-      abi: "function getFactoryState() view returns (address[], address, address, address, address, address)",
-      target: factoryAddress,
-    });
-    const vaultAddresses = factoryState[0]
+  // 1. get all vaults deployed on chain
+  const factoryAddress = protocolConfig.factories[api.chain].address
+  const factoryState = await api.call({
+    abi: "function getFactoryState() view returns (address[], address, address, address, address, address)",
+    target: factoryAddress,
+  });
+  const vaultAddresses = factoryState[0]
 
-    // 2. get all tradable erc20s for these same vaults
-    const erc20Addresses = await api.call({
-      abi: "function getWhitelistedTokens() view returns (address[])",
-      target: factoryAddress,
-    });
-
-
-    // for each vault
-    for (let vIndex = 0; vIndex < vaultAddresses.length; vIndex++) {
-      // get erc20 balances
-      const multiCallResults = await api.multiCall({
-        calls: erc20Addresses.map(erc20Address => ({
-          target: erc20Address,
-          params: [vaultAddresses[vIndex]]
-        })),
-        abi: 'erc20:balanceOf',
-        withMetadata: true,
-      });
-
-      // then iterate over each erc20
-      for (let eIndex = 0; eIndex < multiCallResults.length; eIndex++) {
-        // prepare add args
-        const erc20Address = multiCallResults[eIndex].input.target
-        const balance = Number(multiCallResults[eIndex].output)
-
-        // add token with respective balance
-        api.add(erc20Address, balance)
-      }
-    }
+  // 2. get all tradable erc20s for these same vaults
+  const tokens = await api.call({ abi: "address[]:getWhitelistedTokens", target: factoryAddress, });
+  return api.sumTokens({ owners: vaultAddresses, tokens, })
 }
 
 
