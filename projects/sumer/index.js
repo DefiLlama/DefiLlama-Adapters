f21124a75be862a6ec040c9293e7944995f370f2
yanfeizuo <69463394+yanfeizuo@users.noreply.github.com>
2024-02-20 18:55:28 +0800

    Add base to project Sumer (#9005)
    * update for sumer
    
    * Revert "update for sumer"
    
    This reverts commit c2d61474251f429ce5d50dc600deb5822bff4379.
    
    * add base to project sumer
    
    ---------
    
    Co-authored-by: yanfeizuo <hh18856856693@gmail.com>

--- projects/sumer/index.js
+++ projects/sumer/index.js
@@ -2,4 +2,5 @@ const { compoundExports2 } = require('../helper/compound')
 
 module.exports = {
   meter: compoundExports2({ comptroller: '0xcB4cdDA50C1B6B0E33F544c98420722093B7Aa88' }),
+  base: compoundExports2({ comptroller: '0x611375907733D9576907E125Fb29704712F0BAfA' }),
 }
