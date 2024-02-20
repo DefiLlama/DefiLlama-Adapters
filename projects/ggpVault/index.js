3e8da037cffbc8a320c39a017e8cf0996202cf94
Bookcliff <bookcliff76@gmail.com>
2024-02-20 08:51:06 +0800

    correct underlying asset

--- projects/ggpVault/index.js
+++ projects/ggpVault/index.js
@@ -1,15 +1,12 @@
 const GGPVAULT_CONTRACT = "0xdF34022e8a280fc79499cA560439Bb6f9797EbD8";
-const ggAVAX = "0xA25EaF2906FA1a3a13EdAc9B9657108Af7B703e3";
+const GGP = "0x69260B9483F9871ca57f81A90D91E2F96c2Cd11d";
 
 async function tvl(_, _b, _cb, { api }) {
-  const token = await api.call({ abi: "address:asset", target: ggAVAX });
-
   const bal = await api.call({
     abi: "uint256:totalAssets",
     target: GGPVAULT_CONTRACT,
   });
-
-  api.add(token, bal);
+  api.add(GGP, bal);
 }
 
 module.exports = {
