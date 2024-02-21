cb928c05ac0a116b306cc370d520549b3e4c09c3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-21 23:36:53 +0100

    code refactor

--- projects/yuzu-market/index.js
+++ projects/yuzu-market/index.js
@@ -1,16 +1,5 @@
-const sdk = require("@defillama/sdk");
-const { aaveChainTvl } = require('../helper/aave');
-
-function v2(chain, v2Registry){
-  const section = borrowed => sdk.util.sumChainTvls([
-    aaveChainTvl(chain, v2Registry, undefined, undefined, borrowed),
-  ])
-  return {
-    tvl: section(false),
-    borrowed: section(true)
-  }
-}
+const { aaveExports } = require('../helper/aave');
 
 module.exports = {
-  eon: v2("eon", "0x7CCE4c055BbC0Da1FeCb09F3d327b125FE19555a"),
+  eon: aaveExports("eon", "0x7CCE4c055BbC0Da1FeCb09F3d327b125FE19555a"),
 }
