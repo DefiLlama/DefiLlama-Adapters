c92a1dba07d1ab5642265d7d6d3116431fa1cbe7
Tomwhh <30361751+Tomwhh@users.noreply.github.com>
2024-02-22 13:18:17 +0000

    Update index.js

--- projects/21-co/index.js
+++ projects/21-co/index.js
@@ -10,6 +10,21 @@ const config = {
     owners: [
       'FvLsZiM3g2ZnehkK42c4EoLASBdchKnqzxyAEmGhViZq'
     ],
+  },
+  ripple: {
+    owners: [
+      'rHTG5htCXSNtcXDhFkaSsvB1s1ah6WYrFW'
+    ],
+  },
+  litecoin: {
+    owners: [
+      'LbgDyHCVc6UwsKuDzt5jKwFdG5TLsN5tbf'
+    ],
+  },
+  polkadot: {
+    owners: [
+      '161fEUkrGhhAog8QG1ik3sfch9UzPdvAJtUwRH6WZnsgqkuw'
+    ],
   }
 }
 
