645b511c4b3c9e68926ab79c620703741015f9da
butcher <butcher_of_blaviken@outlook.com>
2024-02-22 13:52:43 +0800

    update roup tvl to brc20 balance on bitcoin

--- projects/roup/index.js
+++ projects/roup/index.js
@@ -1,64 +1,16 @@
-const sdk = require("@defillama/sdk");
-
-const Tokens = {
-  ROUP: "0x5a1c3f3aaE616146C7b9bf9763E0ABA9bAFc5eaE",
-  MAPO: "0xF73C6a12d70Fde8d1cc42229970A529996B1eb24",
-  STST: "0xF5a59f961a8e86285Dae2e45ac4Ae50E4E47Ba97",
-  ORDI: "0x9CA528368964cFb92cfFdd51dCcED25E27ACCef9",
-  RATS: "0x6369414F2b0e973c7E85A362141aA1430bc30056",
-  MMSS: "0x0fBb3B3Fb1e928f75B3Ed8b506bAb4503373fdca",
-  CSAS: "0x141b30Dd30FAFb87ec10312d52e5dbD86122FE14",
-  BTCS: "0xBE81B9390D894fEBf5e5D4Ea1486a003C1e8dc63",
-  "3518": "0xA332EA6eb28A3cFd630bA402832b8e3d3462870E",
-  SATS: "0x3Dd99381218990A32cc10918d39c20bc70685f40",
-  XDAO: "0x9596a3F29E53aEC45594e8F90A976F163791378C",
-  BLLB: "0x91C0815dA9A41B66Ba5a53739Ce4411a9beaFA8F",
-  LSGS: "0x756af1d3810a01d3292fad62f295bbcc6c200aea",
-  MMQQ: "0x5a91ca4004ada972d50edc5c012080b3158219b1",
-  EEAA: "0x040A66eD7deF1c037c5c9848bc5D44dCd3B0Fc62",
-  MBTC: "0x15BB20280322c9ECb9D4E9103C1CE6365d6a1Fd8"
-}
-
-const Manager = "0x57E9094f501573e6Eb7e19aCe9D8E263D11fc8ba";
-async function tvl(ts, block, _2, { api }) {
-  const names = Object.keys(Tokens);
-  const { output: totalSupplies } = await sdk.api.abi.multiCall({
-    calls: names.map((name) => {
-      return {
-        target: Tokens[name],
-        params: []
-      }
-    }),
-    abi: "uint256:totalSupply",
-    chain: "map"
-  })
-
-  const {output: managerBalances} = await sdk.api.abi.multiCall({
-    calls: names.map((name) => {
-      return {
-        target: Tokens[name],
-        params: [Manager]
-      }
-    }),
-    abi: "erc20:balanceOf",
-    chain: "map"
-  })
-  api.addBalances(names.reduce((acc, name, index) => {
-    const balance = (totalSupplies[index].output / 10 ** 18) + (managerBalances[index].output / 10 ** 18);
-    return {
-      ...acc,
-      [name]: balance
-    }
-  }, {}))
-  return api.getBalances();
-}
-
-
+const { sumTokensExport } = require("../helper/chain/brc20");
 module.exports = {
   timetravel: false,
-  methodology: `Tvl of ROUP on Map Protocol`,
-  map: {
-    tvl,
+  methodology: `Tokens bridged via ROUP are counted as TVL`,
+  bitcoin: {
+    tvl: sumTokensExport({
+      owners: [
+        'bc1pv5lu5aklz64sye9f4zmnjkfg8j6s2tllu3fem4cs9t0hcrnz5e7qy0qw6e',
+        'bc1p2tncs8egnj8e6qt46np3qla70mfx4telu92v4c9hp3pg8khqp37s9lvmfx',
+        'bc1phnxqw4gfq349wm2xcqgqk77544ssqwa6ycuhjh7hdxks4mtjg33qrfenw5',
+        'bc1pfsu3ts4equ7rdy63dgt7shkqlu2n5kw8p0z7p7c8lsrh2yqg40fsvz4ev3'
+      ],
+    }),
   }
 }
 
