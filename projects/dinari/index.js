f55d95201f25579ee7a2f7135d4670f309911064
joshualyguessennd <joshualyguessennd@icloud.com>
2024-02-22 16:25:56 +0000

    update dinari project

--- projects/dinari/index.js
+++ projects/dinari/index.js
@@ -1,43 +1,34 @@
 const { getLogs } = require('../helper/cache/getLogs')
-
-const dinariEventConfig = {
-  eventAbi: 'event DShareAdded(address indexed dShare, address indexed wrappedDShare, string indexed symbol, string name)',
-  topics: ['0x35c7978dcf8171bb46371f6b24d3d16faf27279eafd5e22ae1350a4aa458e977'],
-}
+const { getUniqueAddresses } = require('../helper/utils')
 
 const tokenFactories = {
-  arbitrum: { factory: '0xABCD', fromBlock: 1337 },
+  arbitrum: { factory: '0xB4Ca72eA4d072C779254269FD56093D3ADf603b8', fromBlock: 178460216 },
 };
 
-async function getDinariExport(factoriesConfig) {
-  const exports = {};
-
-  const topics = dinariEventConfig.topics;
-  const eventAbi = dinariEventConfig.eventAbi;
+function getDinariExport(factoriesConfig) {
 
   Object.keys(factoriesConfig).forEach(chain => {
-    const { factory: target, fromBlock } = factoriesConfig[chain];
+    const { factory, fromBlock, } = factoriesConfig[chain];
 
-    exports[chain] = {
+    module.exports[chain] = {
       tvl: async (_, _b, _cb, { api, }) => {
         const logs = await getLogs({
           api,
-          target,
-          topics,
-          fromBlock,
-          eventAbi,
+          target: factory,
+          eventAbi: 'event DShareAdded(address indexed dShare, address indexed wrappedDShare, string indexed symbol, string name)',
+          topic: '0x35c7978dcf8171bb46371f6b24d3d16faf27279eafd5e22ae1350a4aa458e977',
           onlyArgs: true,
+          fromBlock,
+          skipCache: true
         });
-
-        const tokenAddresses = logs.map(i => i.dShare);
+        const tokenAddresses = getUniqueAddresses(logs.map(i => i.dShare))
         supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: tokenAddresses });
-        api.addTokens(tokenAddresses, supplies);
+        api.add(tokenAddresses, supplies);
         return api.getBalances();
       }
     }
   })
 
-  return exports;
 }
 
-module.exports = getDinariExport(tokenFactories, api);
+getDinariExport(tokenFactories);
