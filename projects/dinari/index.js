66596266e42f385d12eb2b54183f694c5860edf8
joshualyguessennd <joshualyguessennd@icloud.com>
2024-02-23 00:01:17 +0000

    update dinari project

--- projects/dinari/index.js
+++ projects/dinari/index.js
@@ -1,5 +1,6 @@
 const { getLogs } = require('../helper/cache/getLogs')
 const { getUniqueAddresses } = require('../helper/utils')
+const { chainlink_oracles } = require('./helpers/utils')
 
 const tokenFactories = {
   arbitrum: { factory: '0xB4Ca72eA4d072C779254269FD56093D3ADf603b8', fromBlock: 178460216 },
@@ -23,8 +24,30 @@ function getDinariExport(factoriesConfig) {
         });
         const tokenAddresses = getUniqueAddresses(logs.map(i => i.dShare))
         supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: tokenAddresses });
-        api.add(tokenAddresses, supplies);
-        return api.getBalances();
+
+        const symbols = await api.multiCall({ abi: 'erc20:symbol', calls: tokenAddresses });
+
+        let prices = {};
+        let totalValue = 0;
+        for (let i = 0; i < symbols.length; i++) {
+          let symbol = symbols[i];
+          if (chainlink_oracles[symbol]) {
+            const oracleAddress = chainlink_oracles[symbol];
+            const priceData = await api.multiCall({
+              abi: 'function latestRoundData() view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)',
+              calls: [{ target: oracleAddress }]
+            });
+            const price = priceData[0].answer / (10 ** 8);
+            prices[symbol] = price;
+
+            const supply = supplies[i] / (10 ** 18);
+            const value = supply * price;
+            totalValue += value;
+          }
+        }
+        return {
+          tether: totalValue
+        };
       }
     }
   })
