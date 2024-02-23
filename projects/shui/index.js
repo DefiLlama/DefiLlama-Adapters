3601d1651638b055d0a1c6c5160c074ace94fa85
larkloss <dingzhao1995@gmail.com>
2024-02-24 02:39:52 +0800

    Fix_TVL_Issues (#9052)
    Readjust TVL calculation

--- projects/shui/index.js
+++ projects/shui/index.js
@@ -1,5 +1,3 @@
-const { sumTokensExport, } = require("../helper/unknownTokens");
-
 const SCFX_TokenAddress = "0x1858a8d367e69cd9E23d0Da4169885a47F05f1bE";
 
 const WCFX = "0x14b2d3bc65e74dae1030eafd8ac30c533c976a9b";
@@ -7,10 +5,17 @@ const WCFX = "0x14b2d3bc65e74dae1030eafd8ac30c533c976a9b";
 module.exports = {
   conflux: {
     tvl: async (_, _1, _2, { api }) => {
-      const [exchangeRate] = await api.call({ target: '0x808f81acc4618a05c8253a7b41240468c08cd64c', abi: 'function XCFX_burn_estim(uint256) returns (uint256,uint256)', params: 1e18 +''})
+      const ratioDepositedBySupply = await api.call({
+        target: SCFX_TokenAddress,
+        abi: "function ratioDepositedBySupply() public view returns (uint256)"
+      });
+      const totalDeposited = await api.call({
+        target: SCFX_TokenAddress,
+        abi: "function totalDeposited() public view returns (uint256)"
+      });
       return {
-        ['conflux:' + WCFX]: (await api.call({ target: SCFX_TokenAddress, abi: 'erc20:totalSupply' })) * exchangeRate / 1e18
-      }
-    },
+        ["conflux:" + WCFX]: totalDeposited * ratioDepositedBySupply / 1e9
+      };
+    }
   }
-}
+};
