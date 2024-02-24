02548a6444b4fd8c75403afd7fccc1ddbd92c27f
Scopuly <39069225+Scopuly@users.noreply.github.com>
2024-02-24 14:36:04 +0300

    Scopuly TVL

--- projects/scopuly/index.js
+++ projects/scopuly/index.js
@@ -1,6 +1,7 @@
 const { get } = require('../helper/http');
 
 async function tvl() {
+  
   var totalTvl = await get('https://api.scopuly.com/api/liquidity_pools_tvl');
   return totalTvl.data.reduce((a, i) => a + Number(i.tvl), 0)
 }
