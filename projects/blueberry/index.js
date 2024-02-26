260d8c495b714f541a59a4643e3e02460d50ccf2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-26 09:41:26 +0100

    code refactor

--- projects/blueberry/index.js
+++ projects/blueberry/index.js
@@ -1,33 +1,21 @@
 const { compoundExports2 } = require('../helper/compound')
-const { abi } = require('./abi.json');
+const abi = {
+  "getNextPositionId": "function getNextPositionId() view returns (uint256)",
+  "getPositionValue": "function getPositionValue(uint256 positionId) view returns (uint256)"
+}
 const sdk = require('@defillama/sdk');
 
 const BANK = '0x9b06eA9Fbc912845DF1302FE1641BEF9639009F7';
 
-async function tvl(timestamp, block, chainBlocks) {
-  let total = 0;
-  const nextPositionId = await sdk.api.abi.call({
-    target: BANK,
-    abi: abi['getNextPositionId'],
-    block
-  }).output;
-
-  for (let i = 0; i < nextPositionId; i++ ) {
-    const positionValue = await sdk.api.abi.call({
-      target: BANK,
-      abi: abi['getPositionValue'],
-      params: i,
-      block
-    }).output;
-    total += positionValue;
-  }
-
-  const totalTvl = total + compoundExports2({ comptroller: '0xfFadB0bbA4379dFAbFB20CA6823F6EC439429ec2'});  
-  
-  return totalTvl;
+async function tvl(timestamp, block, chainBlocks, {api}) {
+  const positionValues = await api.fetchList({  lengthAbi: abi.getNextPositionId, itemAbi: abi.getPositionValue, target: BANK})
+  api.addCGToken('tether', positionValues.reduce((acc, i) => acc + i/1e18, 0))
+  return api.getBalances()
 }
 
 module.exports = {
   methodology: 'Gets the total value locked in the Blueberry Lending Market and Blueberry Earn',
-  ethereum: {tvl},
+  ethereum: compoundExports2({ comptroller: '0xfFadB0bbA4379dFAbFB20CA6823F6EC439429ec2'}),
 }
+
+module.exports.ethereum.tvl = sdk.util.sumChainTvls([module.exports.ethereum.tvl, tvl])
