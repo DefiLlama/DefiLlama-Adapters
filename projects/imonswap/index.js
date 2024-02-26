09098c42a9339bfac588399246660bd403eea8c9
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-02-26 16:49:21 +0300

    Update
    Update

--- projects/imonswap/index.js
+++ projects/imonswap/index.js
@@ -2,4 +2,14 @@ const { uniTvlExport } = require("../helper/unknownTokens");
 
 const factory = "0xA0BB8f9865f732C277d0C162249A4F6c157ae9D0";
 
-module.exports = uniTvlExport('chz', factory)
+module.exports = {
+    misrepresentedTokens: true,
+    methodology:
+      "We count liquidity of all paris through Factory Contract and Pools (single tokens) seccions through Factory Contract.",
+    chz: {
+      tvl: getUniTVL({ factory: factory, useDefaultCoreAssets: true, }),
+    },
+    start: 1701478462, //Dec-2-2023 3:54:26 PM +UTC
+
+  };
+  
