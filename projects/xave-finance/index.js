373eac413d1f0d1b18bfda7c0d227480a8045932
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2024-02-26 14:28:02 +0800

    Adds v1 stats computation

--- projects/xave-finance/index.js
+++ projects/xave-finance/index.js
@@ -2,7 +2,6 @@ const { ethers } = require("ethers");
 const { getLogs } = require("../helper/cache/getLogs");
 
 async function tvl(_, _b, _cb, { api }) {
-  console.log(api.addTokens);
   const factories = config[api.chain];
 
   const promises = factories.map(async ({ factory, fromBlock, name }) => {
@@ -33,97 +32,65 @@ async function tvl(_, _b, _cb, { api }) {
       });
 
       data.forEach((i) => {
-        console.log("v2");
-        console.table({
-          address: i.tokens,
-          balances: i.balances,
-        });
         api.addTokens(i.tokens, i.balances);
       });
     } else if (name.includes("CurveFactory")) {
-      const pools = logs.map((i) => i.curve);
+      try {
+        const pools = logs.map((i) => i.curve);
 
-      const data = await api.multiCall({
-        abi: "function liquidity() view returns (uint256 total_, uint256[] memory individual_)",
-        calls: pools.map((v) => ({ target: v })),
-      });
-      console.log("data", data);
-      console.log("pools", pools);
-      // Curve.derivatives(0)
-      const derivatives0 = await api.multiCall({
-        abi: "function derivatives(uint256 index) view returns (address token_address)",
-        calls: pools.map((v) => ({ target: v, params: "0" })),
-      });
-      console.log("derivatives0", derivatives0);
-      const derivatives0Decimals = await api.multiCall({
-        abi: "function decimals() view returns (uint8 decimals)",
-        calls: derivatives0.map((v) => ({ target: v })),
-      });
-      console.log("derivatives0Decimals", derivatives0Decimals);
-      // Curve.derivatives(1)
-      const derivatives1 = await api.multiCall({
-        abi: "function derivatives(uint256 index) view returns (address token_address)",
-        calls: pools.map((v, i) => ({ target: v, params: "1" })),
-      });
-      console.log("derivatives1", derivatives1);
-      const derivatives1Decimals = await api.multiCall({
-        abi: "function decimals() view returns (uint8 decimals)",
-        calls: derivatives1.map((v) => ({ target: v })),
-      });
-      console.log("derivatives1Decimals", derivatives1Decimals);
-      // TODO: Convert numeraire to native token value
-
-      /**
-       * Steps
-       *
-       * 1. Call assimilator # Curve.assimilator(address derivative_address) for base token
-       * 2. Call ERC20.decimals()
-       * 3. Call getRate() from assimilator
-       * 4. Then get baseTokenBalance by d.individual[0]/rate #
-       *
-       */
-
-      // data.forEach((d, i) =>
-      //   api.addTokens([derivatives0[i], derivatives1[i]], [d.individual[0]/rate, d.individual_[1]])
-      // );
-
-      data.forEach((d, i) => {
-        const divisor0 = ethers.parseUnits(
-          "1",
-          parseInt(derivatives0Decimals[i])
-        );
-
-        const baseTokenBalance =
-          (BigInt(d.individual_[0]) / ethers.parseUnits("1", parseInt(18))) *
-          divisor0;
-        console.log(
-          "baseTokenBalance",
-          baseTokenBalance,
-          ethers.formatEther(baseTokenBalance)
-        );
-
-        const divisor1 = ethers.parseUnits(
-          "1",
-          parseInt(derivatives1Decimals[i])
-        );
-        const quoteTokenBalance =
-          (BigInt(d.individual_[1]) / ethers.parseUnits("1", parseInt(18))) *
-          divisor1;
-        console.log(
-          "quoteTokenBalance",
-          quoteTokenBalance,
-          ethers.formatEther(quoteTokenBalance)
-        );
-
-        console.table({
-          derivatives: [derivatives0[i], derivatives1[i]],
-          balances: [baseTokenBalance.toString(), quoteTokenBalance.toString()],
+        const data = await api.multiCall({
+          abi: "function liquidity() view returns (uint256 total_, uint256[] memory individual_)",
+          calls: pools.map((v) => ({ target: v })),
         });
-        api.addTokens(
-          [derivatives0[i], derivatives1[i]],
-          [baseTokenBalance.toString(), quoteTokenBalance.toString()]
-        );
-      });
+
+        // Curve.derivatives(0)
+        const derivatives0 = await api.multiCall({
+          abi: "function derivatives(uint256 index) view returns (address token_address)",
+          calls: pools.map((v) => ({ target: v, params: "0" })),
+        });
+
+        const derivatives0Decimals = await api.multiCall({
+          abi: "function decimals() view returns (uint8 decimals)",
+          calls: derivatives0.map((v) => ({ target: v })),
+        });
+
+        // Curve.derivatives(1)
+        const derivatives1 = await api.multiCall({
+          abi: "function derivatives(uint256 index) view returns (address token_address)",
+          calls: pools.map((v, i) => ({ target: v, params: "1" })),
+        });
+
+        const derivatives1Decimals = await api.multiCall({
+          abi: "function decimals() view returns (uint8 decimals)",
+          calls: derivatives1.map((v) => ({ target: v })),
+        });
+
+        data.forEach((d, i) => {
+          const divisor0 = ethers.parseUnits(
+            "1",
+            parseInt(derivatives0Decimals[i])
+          );
+
+          const baseTokenBalance =
+            (BigInt(d.individual_[0]) / ethers.parseUnits("1", parseInt(18))) *
+            divisor0;
+
+          const divisor1 = ethers.parseUnits(
+            "1",
+            parseInt(derivatives1Decimals[i])
+          );
+          const quoteTokenBalance =
+            (BigInt(d.individual_[1]) / ethers.parseUnits("1", parseInt(18))) *
+            divisor1;
+
+          api.addTokens(
+            [derivatives0[i], derivatives1[i]],
+            [baseTokenBalance.toString(), quoteTokenBalance.toString()]
+          );
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
   await Promise.all(promises);
@@ -132,21 +99,21 @@ async function tvl(_, _b, _cb, { api }) {
 
 module.exports = {
   methodology: "sum of all the tokens locked in FX Pools",
-  // doublecounted: true, // tokens are stored in balancer vaults
+  doublecounted: true, // tokens are stored in balancer vaults
 };
 
 const config = {
   polygon: [
-    // {
-    //   name: "FX Pool Factory",
-    //   factory: "0x627D759314D5c4007b461A74eBaFA7EBC5dFeD71",
-    //   fromBlock: 32054794,
-    // },
-    // {
-    //   name: "FX Pool Deployer",
-    //   factory: "0xF169c1Ae8De24Da43a3dC5c5F05De412b4848bD3",
-    //   fromBlock: 49368321,
-    // },
+    {
+      name: "FX Pool Factory",
+      factory: "0x627D759314D5c4007b461A74eBaFA7EBC5dFeD71",
+      fromBlock: 32054794,
+    },
+    {
+      name: "FX Pool Deployer",
+      factory: "0xF169c1Ae8De24Da43a3dC5c5F05De412b4848bD3",
+      fromBlock: 49368321,
+    },
     {
       name: "CurveFactory",
       factory: "0x6bfBc74eb37d44DE6E44B0B63F04CbA626009Ff5",
@@ -154,16 +121,16 @@ const config = {
     },
   ],
   ethereum: [
-    // {
-    //   name: "FX Pool Factory",
-    //   factory: "0x81fE9e5B28dA92aE949b705DfDB225f7a7cc5134",
-    //   fromBlock: 15981805,
-    // },
-    // {
-    //   name: "FX Pool Deployer",
-    //   factory: "0xfb23Bc0D2629268442CD6521CF4170698967105f",
-    //   fromBlock: 18469425,
-    // },
+    {
+      name: "FX Pool Factory",
+      factory: "0x81fE9e5B28dA92aE949b705DfDB225f7a7cc5134",
+      fromBlock: 15981805,
+    },
+    {
+      name: "FX Pool Deployer",
+      factory: "0xfb23Bc0D2629268442CD6521CF4170698967105f",
+      fromBlock: 18469425,
+    },
     {
       name: "CurveFactory",
       factory: "0xFA505d02269bF4Ea59355a4e37fBd882122717e5",
@@ -171,16 +138,16 @@ const config = {
     },
   ],
   avax: [
-    // {
-    //   name: "FX Pool Factory",
-    //   factory: "0x81fE9e5B28dA92aE949b705DfDB225f7a7cc5134",
-    //   fromBlock: 32585313,
-    // },
-    // {
-    //   name: "FX Pool Deployer",
-    //   factory: "0x4042dC4110Ea9500338737605A60065c3de152C6",
-    //   fromBlock: 37150792,
-    // },
+    {
+      name: "FX Pool Factory",
+      factory: "0x81fE9e5B28dA92aE949b705DfDB225f7a7cc5134",
+      fromBlock: 32585313,
+    },
+    {
+      name: "FX Pool Deployer",
+      factory: "0x4042dC4110Ea9500338737605A60065c3de152C6",
+      fromBlock: 37150792,
+    },
   ],
 };
 
