df840eb8dcbd9885b10d789d1a891dd18ffbfc64
Daniel <danielnickreal@gmail.com>
2024-02-26 21:38:11 +0800

    chore: update chain name

--- projects/manta-atlantic-mantadex/index.js
+++ projects/manta-atlantic-mantadex/index.js
@@ -2,5 +2,5 @@ const { getExports } = require("../helper/heroku-api");
 
 module.exports = {
   timetravel: false,
-  ...getExports("manta-atlantic-mantadex", ["manta-atlantic"]),
+  ...getExports("manta-atlantic-mantadex", ["manta_atlantic"]),
 };
