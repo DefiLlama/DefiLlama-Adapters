b6b0b95ea7fa3445f31d263f7e89565c372ca598
SteakHut <0xsirloin@steakhut.xyz>
2024-02-26 19:18:26 +1000

    initial commit

--- projects/steakHut-lb/index.js
+++ projects/steakHut-lb/index.js
@@ -1,5 +1,9 @@
 const sdk = require('@defillama/sdk');
 const abi = require('./abi.json')
+const abi_hjoe = require('./abi_hJOE.json')
+const abi_factory = require('./abi_enigmaFactory.json')
+const abi_enigma = require('./abi_enigma.json');
+const { balanceOf } = require('@defillama/sdk/build/erc20');
 
 const _vaults = {
     avax: [
@@ -32,6 +36,15 @@ const _vaults = {
     ],
 }
 
+const enigmaFactory_AVAX = `0xD751E0940CfadC35f84e60075d0f940a2545FB8d`;
+
+async function getTotalEnigmaCount(api){
+    return api.call({
+        target: enigmaFactory_AVAX,
+        abi: abi_factory.enigmaPositionNumber
+    })
+}
+
 async function getVaultShares(api) {
     return api.multiCall({
         calls: _vaults[api.chain],
@@ -47,10 +60,13 @@ async function getDepositTokens(api) {
 }
 
 
+
+
 async function tvl(_, block, _1, { api }) {
     const balances = {};
 
-    //get the total shares from all vaults
+    
+
     const vaultShares = await getVaultShares(api);
     const depositTokens = await getDepositTokens(api);
 
@@ -74,14 +90,147 @@ async function tvl(_, block, _1, { api }) {
     return balances;
 }
 
+function buildQueries(count){
+    const calls = []
+    for (let i = 0; i < count; i++) {
+        calls.push({
+            target: enigmaFactory_AVAX,
+            params: i
+        })
+    }
+    return calls
+}
+
+function buildQueriesOnEnigma(addresses){
+    const calls = []
+    for (let i = 0; i < addresses.length; i++) {
+        calls.push({
+            target: addresses[i]
+        })
+    }
+    return calls
+}
+
+async function getXSteakTVL(_, block, _1, { api }) {
+    const balances = {};
+
+    const XSTEAK_ADDRESS = '0x902Aa4cC3b463c84541C9C1DeDF50620C99950B9';
+    const STEAK_ADDRESS_AVAX = '0xb279f8dd152b99ec1d84a489d32c35bc0c7f5674';
+    
+    const totalSupply = await api.call({
+        target: XSTEAK_ADDRESS,
+        abi: abi_hjoe.totalSupply
+    })
+
+    
+    
+    sdk.util.sumSingleBalance(balances, STEAK_ADDRESS_AVAX, totalSupply, api.chain)
+    return(balances);
+
+}
+
+async function tvlHJoe(_, block, _1, { api }) {
+    const balances = {};
+    const HJOE_ADDRESS = '0xe7250b05bd8dee615ecc681eda1196add5156f2b';
+    const JOE_ADDRESS = '0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd';
+
+    const totalSupply = await api.call({
+        target: HJOE_ADDRESS,
+        abi: abi_hjoe.totalSupply
+    })
+
+    
+    
+    sdk.util.sumSingleBalance(balances,JOE_ADDRESS, totalSupply, api.chain)
+    return(balances);
+}
+
+
+async function getV2LiquidityTVL(_, block, _1, { api }) {
+    const balances = {};
+
+    //get the total shares from all vaults
+    const enigmaCount = await getTotalEnigmaCount(api);
+    
+
+    const enigmaCalls = buildQueries(enigmaCount);
+    
+
+    //get all the enigma contract addresses 
+    const enigmasGenerated = await api.multiCall({
+        calls: enigmaCalls,
+        abi: abi_factory.enigmaAtIndex
+    })
+    
+
+    const enigmaQueries = buildQueriesOnEnigma(enigmasGenerated);
+    
+    
+    ///get the want tokens 
+    const wantToken0sForEnigmas = await api.multiCall({
+        calls: enigmaQueries,
+        abi: abi_enigma.token0
+    })
+
+    const wantToken1sForEnigmas = await api.multiCall({
+        calls: enigmaQueries,
+        abi: abi_enigma.token1
+    })
+
+
+
+    const underlyingAmounts = await api.multiCall({
+        calls: enigmaQueries,
+        abi: abi_enigma.getTotalAmounts
+    })
+
+    //now we can sum the total balances 
+    for(let i = 0; i < underlyingAmounts.length; i++){
+        sdk.util.sumSingleBalance(balances, wantToken0sForEnigmas[i], underlyingAmounts[i][0], api.chain)
+        sdk.util.sumSingleBalance(balances, wantToken1sForEnigmas[i], underlyingAmounts[i][1], api.chain)
+    }
+
+    
+    return(balances);
+}
+function mergeAndSum(obj1, obj2) {
+    const result = { ...obj1 }; // Start with a copy of the first object
+  
+    Object.keys(obj2).forEach(key => {
+      if (result.hasOwnProperty(key)) {
+        // Convert both values to numbers and sum them if the key exists
+        result[key] = (parseFloat(result[key]) + parseFloat(obj2[key])).toString();
+      } else {
+        // Otherwise, just add the key-value pair from obj2 to the result
+        result[key] = obj2[key];
+      }
+    });
+  
+    return result;
+  }
+
+
+async function tvlAVAX(_, block, _1, { api }) {
+    
+    const xSTEAKTVL = await getXSteakTVL(_, block, _1, { api });
+    const hJOETVL = await tvlHJoe(_, block, _1, { api });
+    const v2LiquidityTVL = await getV2LiquidityTVL(_, block, _1, { api });
+
+    const x = mergeAndSum(xSTEAKTVL, v2LiquidityTVL );
+    const y = mergeAndSum(x, hJOETVL);
+
+    return(y);
+}
+
+
+
 module.exports = {
     doublecounted: true,
-    methodology: 'Counts the value of LB tokens staked into SteakHut Liquidity.',
+    methodology: 'Counts the value of underlying SteakHut Pools.',
     avax: {
-        tvl,
+        tvl: tvlAVAX,
     },
     arbitrum: {
         tvl
     }
-
 }; 
