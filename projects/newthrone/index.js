7596c250ccc96da5f1ea6d837ad19fa67bebdb02
Tazz <104685150+ItsMeTazz@users.noreply.github.com>
2024-02-27 13:45:36 -0700

    Update index.js

--- projects/newthrone/index.js
+++ projects/newthrone/index.js
@@ -1,20 +1,20 @@
 const GAME_CONTRACT = '0x61C3A357bc3ca51b80eCD36CB1Ae37e5465C6701';
-const WETH = '0x4200000000000000000000000000000000000006';
+const THRO = '0x0f929C29dcE303F96b1d4104505F2e60eE795caC';
 
 async function tvl(_, _1, _2, { api }) {
   const gameContractBalance = await api.call({
     abi: 'erc20:balanceOf',
-    target: WETH,
+    target: THRO,
     params: [GAME_CONTRACT],
   });
 
-  api.add(WETH, gameContractBalance)
+  api.add(THRO, gameContractBalance)
 }
 
 module.exports = {
   timetravel: true,
   misrepresentedTokens: false,
-  methodology: 'WETH tokens in the Game contract, which represents the current balance of the game (total spent - total claimed).',
+  methodology: 'THRO tokens in the Game contract, which represents the current balance of the game (total spent - total claimed).',
   base: {
     tvl,
   }
