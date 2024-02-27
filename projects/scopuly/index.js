22dbd3e1ba065121f90ad6d92b04944b565f4c83
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-27 11:13:51 +0100

    code refactor

--- projects/scopuly/index.js
+++ projects/scopuly/index.js
@@ -1,11 +1,12 @@
 const { get } = require('../helper/http');
 
 async function tvl() {
-  
   var totalTvl = await get('https://api.scopuly.com/api/liquidity_pools_tvl');
-  return totalTvl.data.reduce((a, i) => a + Number(i.tvl), 0)
+  return { tether: totalTvl.reduce((a, i) => a + Number(i.tvl), 0)}
 }
 
 module.exports = {
-  tvl,
+  timetravel: false,
+  misrepresentedTokens: true,
+  stellar: { tvl },
 }
