609ae5bb09edfadfcd1276f55bd9c99fd1114f87
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-27 16:28:31 +0100

    code refactor

--- projects/velvet-capital-v2/index.js
+++ projects/velvet-capital-v2/index.js
@@ -1,20 +1,7 @@
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
-const { getConfig, } = require('../helper/cache')
-const { getLogs } = require('../helper/cache/getLogs')
+const { sumTokens2, } = require('../helper/unwrapLPs')
 
 async function tvl(_, _b, _cb, { api, }) {
-  const indexes = await api.fetchList({ lengthAbi: 'uint256:indexId', itemAbi: 'function getIndexList(uint256) view returns (address)', target: '0xE61472Ce45e559830ECF12F6a215Cd732F4D798B' })
-  const [tokens, vaults] = await Promise.all([
-    api.multiCall({ abi: 'address[]:getTokens', calls: indexes }),
-    api.multiCall({ abi: 'address:vault', calls: indexes }),
-  ])
-
-  const ownerTokens = tokens.map((tokens, i) => [tokens, vaults[i]]);
-  return sumTokens2({ api, ownerTokens, resolveLP: true });
-}
-
-async function tvl2(_, _b, _cb, { api, }) {
-  const indexes = await api.fetchList({ lengthAbi: 'uint256:indexId', itemAbi: 'function getIndexList(uint256) view returns (address)', target: '0xc4209197ebC3165863d62c4c340C113620414d97' })
+  const indexes = await api.fetchList({ lengthAbi: 'uint256:indexId', itemAbi: 'function getIndexList(uint256) view returns (address)', target: config[api.chain] })
   const [tokens, vaults] = await Promise.all([
     api.multiCall({ abi: 'address[]:getTokens', calls: indexes }),
     api.multiCall({ abi: 'address:vault', calls: indexes }),
@@ -26,10 +13,13 @@ async function tvl2(_, _b, _cb, { api, }) {
 
 module.exports = {
   methodology: 'calculates overall value deposited across different protocol portfolios',
-  bsc: { 
-    tvl:tvl 
-  },
-  arbitrum: { 
-    tvl:tvl2 
-  }
 }
+
+const config = {
+  arbitrum: '0xc4209197ebC3165863d62c4c340C113620414d97',
+  bsc: '0xE61472Ce45e559830ECF12F6a215Cd732F4D798B',
+}
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = { tvl }
+})
