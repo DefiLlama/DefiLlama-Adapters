1969e9e5b01e98c74d9b9cf7b63d2a51d0510c88
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-27 13:11:29 +0100

    Update index.js

--- projects/bakerfi/index.js
+++ projects/bakerfi/index.js
@@ -1,50 +1,21 @@
-/** BakerFI Vault TVL */
-const ADDRESSES = require('../helper/coreAssets.json')
-
 const config = {
-    "arbitrum": {
-        vaults: ["0x5c1b2312FaE6c0d61B6A15A8093842E9fE5b1e44"]
-    },  
-}
-/**
- * BakerFi Function to get the total assets locked on vault
- */
-const VAUL_ABI = {
-    totalAssets: {
-        type: "function",        
-        stateMutability: "view",
-        inputs: [],
-        name: "totalAssets",
-        outputs: [
-          {
-            "internalType": "uint256",
-            "name": "amount",
-            "type": "uint256"
-          }
-        ],      
-    }
+  "arbitrum": {
+    vaults: ["0x5c1b2312FaE6c0d61B6A15A8093842E9fE5b1e44"]
+  },
 }
 
-async function getVaultTVL(api, vaults)  {    
-    for( const vault of vaults) {
-        const tvl =  await api.call({
-            abi: VAUL_ABI.totalAssets,
-            target: vault,
-            params: [],
-        });
-        api.add(ADDRESSES.null, tvl)
-    }
+async function getVaultTVL(api, vaults) {
+  const bals = await api.multiCall({ abi: 'uint256:totalAssets', calls: vaults })
+  api.addGasToken(bals)
 }
 
 module.exports = {
-    methodology: 'Counts the number of assets that are deployed through the protocol',
-    misrepresentedTokens: false,
+  methodology: 'Counts the number of assets that are deployed through the protocol',
 }
 
 Object.keys(config).forEach(chain => {
-    const { vaults  = []} = config[chain]
-    module.exports[chain] = {
-        tvl: async (_, _1, _2, { api }) => await getVaultTVL(api, vaults),    
-    }
+  const { vaults = [] } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _1, _2, { api }) => await getVaultTVL(api, vaults),
+  }
 })
-
