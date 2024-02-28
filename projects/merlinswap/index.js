8472e554f9362985ee988b7f563aa659105b7300
Define101 <93603962+Define101@users.noreply.github.com>
2024-02-28 12:40:51 +0000

    add merlin chain (#9105)

---
+++ projects/merlinswap/index.js
@@ -0,0 +1,8 @@
+
+
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+  merlin: { factory: "0xE29a6620DAc789B8a76e9b9eC8fE9B7cf2B663D5", fromBlock: 26, },
+});
+
