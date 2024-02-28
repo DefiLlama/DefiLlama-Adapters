a41d2263c47dcdcfbfe96fb041aef0e13b6e7d19
joshualyguessennd <joshualyguessennd@icloud.com>
2024-02-28 11:38:18 +0000

    fix lint

--- projects/dinari/index.js
+++ projects/dinari/index.js
@@ -1,6 +1,5 @@
 const { getLogs } = require('../helper/cache/getLogs')
 const { getUniqueAddresses } = require('../helper/utils')
-const { sumUnknownTokens } = require('../helper/unknownTokens');
 
 const tokenFactories = {
   arbitrum: { factory: '0xB4Ca72eA4d072C779254269FD56093D3ADf603b8', fromBlock: 178460216 },
@@ -21,8 +20,8 @@ function getDinariExport(factoriesConfig) {
           skipCache: true,
           onlyArgs: true,
         });
-        tokenAddresses = getUniqueAddresses(logs.map(i => i.dShare))
-        supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: tokenAddresses });
+        const tokenAddresses = getUniqueAddresses(logs.map(i => i.dShare))
+        const supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: tokenAddresses });
         api.addTokens(tokenAddresses, supplies);
         return api.getBalances();
       }
