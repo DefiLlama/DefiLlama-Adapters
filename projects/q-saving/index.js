dd8ac9fa495d901da8d1d82b99e280498cd9df6d
Kyryl Riabov <kyryl.ryabov@gmail.com>
2024-02-28 14:01:06 +0200

    Updated TVL function

--- projects/q-saving/index.js
+++ projects/q-saving/index.js
@@ -6,7 +6,7 @@ const SAVING_CONTRACT = "0x7CCa96c630329c1972547e95B9f3F82eC31A916A"
 
 module.exports = {
     q: {
-        staking: staking(SAVING_CONTRACT, ADDRESSES.q.QUSD),
+        staking: staking(SAVING_CONTRACT, ADDRESSES.q.QUSD, "q", "usd-coin", 18),
         tvl: () => ({})
     },
 };
