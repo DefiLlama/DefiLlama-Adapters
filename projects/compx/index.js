3d5b7f80028fdc1cadd5a4c6c15fdd658ac58071
Ronan <clooneyr17@gmail.com>
2024-02-28 14:06:10 +1100

    feat: Update calc to be more transparent

--- projects/compx/index.js
+++ projects/compx/index.js
@@ -1,9 +1,16 @@
 const { get } = require("../helper/http");
 async function tvl() {
-  const tvl = await get(
+  const compxTvlData = await get(
     "https://compx-backend-e899c.ondigitalocean.app/get-compx-tvl"
   );
-  return { tether: tvl.data.tvlUsd };
+  //Calculating user tvl:
+  //usersFarmPosition = userLpBalance / farmTotalTvlLp
+  //userDollarTvl = farmTvl * usersFarmPosition
+  const tvlUsd = compxTvlData.data.tvlDetails.reduce((total, userTvl) =>{
+    const usersDollarTvl = Number((Number(userTvl.farmTvl) * Number(userTvl.usersFarmPosition)).toFixed(3));
+    return total + usersDollarTvl;
+  }, 0)
+  return { tether: tvlUsd };
 }
 
 module.exports = {
