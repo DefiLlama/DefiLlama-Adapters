056311ec41af46f8224bd600437922ed83a2a055
Kyryl Riabov <kyryl.ryabov@gmail.com>
2024-02-28 13:43:38 +0200

    Updated TVL function

--- projects/q-saving/index.js
+++ projects/q-saving/index.js
@@ -1,5 +1,4 @@
 const { staking } = require('../helper/staking')
-const {getUniTVL} = require("../helper/unknownTokens");
 
 const ADDRESSES = require('../helper/coreAssets.json')
 
@@ -8,6 +7,6 @@ const SAVING_CONTRACT = "0x7CCa96c630329c1972547e95B9f3F82eC31A916A"
 module.exports = {
     q: {
         staking: staking(SAVING_CONTRACT, ADDRESSES.q.QUSD),
-        tvl: getUniTVL({ coreAssets: [ADDRESSES.q.QUSD], factory: '0xfbb4E52FEcc90924c79F980eb24a9794ae4aFFA4', chain: 'q' }),
+        tvl: () => ({})
     },
 };
