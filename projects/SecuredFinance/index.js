d82d1de2a8d214420d4ae517f4554dae6289c3eb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-29 12:31:50 +0100

    Refactor TVL calculation for multiple chains

--- projects/SecuredFinance/index.js
+++ projects/SecuredFinance/index.js
@@ -1,68 +1,18 @@
-const { sumTokens2 } = require('../helper/unwrapLPs');
-const ADDRESSES = require('../helper/coreAssets.json');
+const config = {
+  ethereum: { FIL: '0x6a7b717ae5ed65f85ba25403d5063d368239828e'},
+  arbitrum: {},
+  avax: { FIL: '0xf3bcb00146d1123dd19974de758f83d01e26d3f1'},
+  polygon_zkevm: { vault: '0x0896AC8B9e2DC3545392ff65061E5a8a3eD68824' },
+}
 
-const ethereumOwner = '0xB74749b2213916b1dA3b869E41c7c57f1db69393';
-const arbitrumOwner = '0xB74749b2213916b1dA3b869E41c7c57f1db69393';
-const avaxOwner = '0xB74749b2213916b1dA3b869E41c7c57f1db69393';
-const polygonZkEVMOwner = '0x0896AC8B9e2DC3545392ff65061E5a8a3eD68824';
-
-const ethereumTokens = [
-    ADDRESSES.ethereum.USDC,
-    ADDRESSES.ethereum.WBTC,
-    ADDRESSES.ethereum.WETH,
-    '0x6A7b717aE5Ed65F85BA25403D5063D368239828e',
-];
-
-const arbitrumTokens = [
-    ADDRESSES.arbitrum.WBTC,
-    ADDRESSES.arbitrum.WETH,
-    ADDRESSES.arbitrum.USDC_CIRCLE,
-];
-
-const avaxTokens = [
-    ADDRESSES.avax.BTC_b,
-    ADDRESSES.avax.USDC,
-    ADDRESSES.avax.WETH_e,
-    '0xF3bcB00146d1123dD19974De758F83D01E26D3F1',
-];
-
-const polygonZkEVMTokens = [
-    ADDRESSES.polygon_zkevm.USDC,
-    ADDRESSES.polygon_zkevm.WETH,
-    '0xEA034fb02eB1808C2cc3adbC15f447B93CbE08e1',
-];
-
-module.exports = {
-    ethereum: {
-        tvl: async (_, _1, _2, { api }) => {
-            return sumTokens2({
-                owner: ethereumOwner,
-                tokens: ethereumTokens,
-                api,
-            });
-        },
-    },
-    arbitrum: {
-        tvl: async (_, _1, _2, { api }) => {
-            return sumTokens2({
-                owner: arbitrumOwner,
-                tokens: arbitrumTokens,
-                api,
-            });
-        },
-    },
-    avax: {
-        tvl: async (_, _1, _2, { api }) => {
-            return sumTokens2({ owner: avaxOwner, tokens: avaxTokens, api });
-        },
-    },
-    polygon_zkevm: {
-        tvl: async (_, _1, _2, { api }) => {
-            return sumTokens2({
-                owner: polygonZkEVMOwner,
-                tokens: polygonZkEVMTokens,
-                api,
-            });
-        },
-    },
-};
+Object.keys(config).forEach(chain => {
+  const { vault = '0xB74749b2213916b1dA3b869E41c7c57f1db69393', FIL } = config[chain]
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api, }) => {
+      const bytes = await api.call({ abi: 'function getCollateralCurrencies() view returns (bytes32[])', target: vault })
+      const tokens = await api.multiCall({ abi: 'function getTokenAddress(bytes32) view returns (address)', calls: bytes, target: vault })
+      if (FIL) tokens.push(FIL)
+      return api.sumTokens({ owner: vault, tokens, })
+    }
+  }
+})
