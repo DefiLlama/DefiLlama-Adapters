c5245e3ba173d50330453d380c497d407850a43b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-02-29 23:24:41 +0100

    code refactor

--- projects/cyberblast-v3/index.js
+++ projects/cyberblast-v3/index.js
@@ -2,5 +2,5 @@ const { uniV3Export } = require('../helper/uniswapV3')
 const factory = '0x57eF21959CF9536483bA6ddB10Ad73E2a06b85ff'
 
 module.exports = uniV3Export({
-  base: { factory, fromBlock: 207530 },
+  blast: { factory, fromBlock: 207530 },
 })
