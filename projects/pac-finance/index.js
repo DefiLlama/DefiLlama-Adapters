fb62762255b01a2508392553e058488bc74fb7f7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-01 19:00:01 +0100

    track https://app.orbitlending.io

--- projects/pac-finance/index.js
+++ projects/pac-finance/index.js
@@ -4,5 +4,5 @@ const { aaveExports } = require("../helper/aave");
 module.exports = {
   blast: aaveExports("blast", undefined, undefined, [
     "0x742316f430002D067dC273469236D0F3670bE446",
-  ]),
+  ], { blacklistedTokens: ['0x9be8a40c9cf00fe33fd84eaedaa5c4fe3f04cbc3']}),
 };
