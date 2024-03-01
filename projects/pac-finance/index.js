e606b18a45b78e3e6a42dc87b957713c9261c642
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-01 18:21:00 +0100

    Add support for RING-V2 LP token in blast chain

--- projects/pac-finance/index.js
+++ projects/pac-finance/index.js
@@ -1,7 +1,8 @@
 const { aaveExports } = require("../helper/aave");
 
+// https://docs.pac.finance/developer/mainnet-addresses
 module.exports = {
   blast: aaveExports("blast", undefined, undefined, [
-    "0x688B5fd3C3E3724b4De08C4BCB3A755F9b579c9a",
+    "0x742316f430002D067dC273469236D0F3670bE446",
   ]),
 };
