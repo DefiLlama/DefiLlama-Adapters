e4eecbc76b20e0689cbe31da2f47710f39557b16
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-01 16:26:41 +0100

    track thruster fi v2

--- projects/thruster-fi/index.js
+++ projects/thruster-fi/index.js
@@ -1,5 +1,6 @@
 const { uniV3Export } = require("../helper/uniswapV3");
+const { mergeExports } = require('../helper/utils')
 
-module.exports = uniV3Export({
-  blast: { factory: "0x71b08f13B3c3aF35aAdEb3949AFEb1ded1016127", fromBlock: 10, },
-});
+module.exports = mergeExports([
+  uniV3Export({ blast: { factory: "0x71b08f13B3c3aF35aAdEb3949AFEb1ded1016127", fromBlock: 157106, }, }),
+])
