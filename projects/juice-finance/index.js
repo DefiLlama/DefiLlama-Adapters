3729b8448befbface8e576494d0f2018c9d66e71
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-01 16:39:21 +0100

    code refactor

--- projects/juice-finance/index.js
+++ projects/juice-finance/index.js
@@ -1,7 +1,9 @@
 const ADDRESSES = require('../helper/coreAssets.json')
+const LENDING_POOL = "0x4A1d9220e11a47d8Ab22Ccd82DA616740CF0920a"
+const COLLATERAL_VAULT = "0x6301795aa55B90427CF74C18C8636E0443F2100b"
 
 async function tvl(timestamp, _, _1, { api }) {
-  api.sumTokens({ tokensAndOwners: [[ADDRESSES.blast.USDB, LENDING_POOL], [ADDRESSES.blast.WETH, COLLATERAL_VAULT]] })
+  return api.sumTokens({ tokensAndOwners: [[ADDRESSES.blast.USDB, LENDING_POOL], [ADDRESSES.blast.WETH, COLLATERAL_VAULT]] })
 }
 
 module.exports = {
