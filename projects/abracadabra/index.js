b09ead087487ebd441c513906acc4263a4466403
0xmDreamy <0xmDreamy@proton.me>
2024-03-01 20:26:15 +0100

    feat(abracadabra): Add support for Blast

--- projects/abracadabra/index.js
+++ projects/abracadabra/index.js
@@ -9,6 +9,7 @@ const abi = require('./abi.json');
 const bentoBoxAddresses = {
   "arbitrum": ["0x74c764D41B77DBbb4fe771daB1939B00b146894A", "0x7c8fef8ea9b1fe46a7689bfb8149341c90431d38"],
   "avax": ["0xf4F46382C2bE1603Dc817551Ff9A7b333Ed1D18f", "0x1fC83f75499b7620d53757f0b01E2ae626aAE530"],
+  "blast": ["0xC8f5Eb8A632f9600D1c7BC91e97dAD5f8B1e3748"],
   "bsc": ["0x090185f2135308BaD17527004364eBcC2D37e5F6"],
   "ethereum": ["0xF5BCE5077908a1b7370B9ae04AdC565EBd643966", "0xd96f48665a1410C0cd669A88898ecA36B9Fc2cce"],
   "fantom": ["0xF5BCE5077908a1b7370B9ae04AdC565EBd643966", "0x74A0BcA2eeEdf8883cb91E37e9ff49430f20a616"],
@@ -21,6 +22,7 @@ const underlyingTokens = {
     "0x3477Df28ce70Cecf61fFfa7a95be4BEC3B3c7e75": "0x5402B5F40310bDED796c7D0F3FF6683f5C0cFfdf",
   },
   avax: {},
+  blast: {},
   bsc: {},
   ethereum: {
     "0x5958A8DB7dfE0CC49382209069b00F54e17929C2": "0x903C9974aAA431A765e60bC07aF45f0A1B3b61fb",
@@ -52,7 +54,7 @@ async function tvl(_, _1, _2, { api }) {
   return api.getBalances()
 }
 
-const chains = ['arbitrum', 'avax', 'bsc', 'ethereum', 'fantom', 'kava', 'optimism'];
+const chains = ['arbitrum', 'avax', 'blast', 'bsc', 'ethereum', 'fantom', 'kava', 'optimism'];
 chains.forEach(chain => module.exports[chain] = { tvl }),
 module.exports.hallmarks = [
   [1651881600, "UST depeg"],
