c8dcdddaf0f3aecf1ef6588fda3c0d4aaeac8ac8
MineJay <minejay6712@gmail.com>
2024-03-01 12:12:28 +0800

    Add pac finance

---
+++ projects/pac-finance/index.js
@@ -0,0 +1,7 @@
+const { aaveExports } = require("../helper/aave");
+
+module.exports = {
+  blast: aaveExports("blast", undefined, undefined, [
+    "0x688B5fd3C3E3724b4De08C4BCB3A755F9b579c9a",
+  ]),
+};
