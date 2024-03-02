7793cbc102f03a480883744cecf8612bdfb75d62
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-02 15:52:20 +0100

    code refactor

--- projects/BlastNYAN/index.js
+++ projects/BlastNYAN/index.js
@@ -7,32 +7,14 @@ const stakingBLNYANContract = '0xA76D6dc805d0EbEcb3787c781ce3A18feEF020cb'
 const feeDistro = '0xBC8a7a845cC7A8246EB34856Afe6f1a3d62BD9C6'
 const stakeLpEarnWeth = '0x0a3A757BE3049C2d9444d025E98D37b2a81a0a32'
 const button = '0x00066Ed6c2F7d6CC6e66c678BaEE2C8683B632e6'
+const opts = { useDefaultCoreAssets: true, lps: [BLNYAN_WETH_SLP] }
 
 module.exports = {
   misrepresentedTokens: true,
   blast: {
-    tvl: sumTokensExport({
-      owners: [
-        stakeLpEarnWeth,
-        stakingBLNYANContract,
-        BLNYAN,
-        feeDistro,
-        button,
-      ],
-      tokens: [WETH, BLNYAN],
-      lps: [BLNYAN_WETH_SLP],
-      useDefaultCoreAssets: true,
-    }),
-    pool2: sumTokensExport({
-      owners: [stakeLpEarnWeth],
-      tokens: [BLNYAN_WETH_SLP],
-      useDefaultCoreAssets: true,
-    }),
-    staking: sumTokensExport({
-      owners: [stakingBLNYANContract],
-      tokens: [BLNYAN],
-      useDefaultCoreAssets: true,
-    }),
+    tvl: sumTokensExport({ owners: [feeDistro, button], tokens: [WETH], ...opts, }),
+    pool2: sumTokensExport({ ...opts, owners: [stakeLpEarnWeth], tokens: [BLNYAN_WETH_SLP], }),
+    staking: sumTokensExport({ owners: [stakingBLNYANContract], tokens: [BLNYAN], ...opts, }),
   },
   methodology:
     'Counts as TVL the ETH, blNYAN and LP assets deposited through-out the protocol',
