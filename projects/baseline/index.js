92bd128210dc4c689e3cc10f1b31a9eccd5e6d8a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-04 12:26:15 +0100

    minor refactor

--- projects/baseline/index.js
+++ projects/baseline/index.js
@@ -1,59 +1,20 @@
-const ADDRESSES = require("../helper/coreAssets.json");
-
-const sdk = require("@defillama/sdk");
 const BASELINE_CONTRACT = "0x14eB8d9b6e19842B5930030B18c50B0391561f27";
 
 async function tvl(_, _1, _2, { api }) {
-  const balances = {};
   //floor, anchor, discovery
   const positions = [0, 1, 2];
 
   //return position info from baseline contract
-  const position = await api.multiCall({
-    calls: positions.map((p) => ({
-      target: BASELINE_CONTRACT,
-      params: [p],
-    })),
-    abi: abi.getPosition,
-  });
-
+  const position = await api.multiCall({ target: BASELINE_CONTRACT, calls: positions, abi: abi.getPosition, });
   //return managed positions from baseline contract
-  const baselinePositionBalances = await api.multiCall({
-    calls: position.map((p) => ({
-      target: BASELINE_CONTRACT,
-      params: [p],
-    })),
-    abi: abi.getBalancesForPosition,
-  });
-
-  const reserves = baselinePositionBalances.map((b) => ({ reserves: b[0] }));
-
+  const baselinePositionBalances = await api.multiCall({ target: BASELINE_CONTRACT, calls: position.map(i => ({ params: [i], })), abi: abi.getBalancesForPosition, });
   //sum the reserve balances
-  reserves.map((r) => {
-    sdk.util.sumSingleBalance(
-      balances,
-      ADDRESSES.blast.WETH,
-      r.reserves,
-      "blast"
-    );
-  });
-
-  return balances;
+  api.addGasToken(baselinePositionBalances.map(i => i.reserves))
 }
 
 async function borrowed(_, _1, _2, { api }) {
-  const balances = {};
-  const lentReserves = await api.call({
-    abi: abi.totalLentReserves,
-    target: BASELINE_CONTRACT,
-  });
-  sdk.util.sumSingleBalance(
-    balances,
-    ADDRESSES.blast.WETH,
-    lentReserves,
-    "blast"
-  );
-  return balances;
+  const lentReserves = await api.call({ abi: abi.totalLentReserves, target: BASELINE_CONTRACT, });
+  api.addGasToken(lentReserves)
 }
 
 module.exports = {
