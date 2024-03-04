4828df3c6be52a71fd2c6819c45c276a826a9974
Lim Yee Han <limyeehan@gmail.com>
2024-03-04 11:48:30 +0800

    Remove API server

--- projects/repl/index.js
+++ projects/repl/index.js
@@ -9,26 +9,24 @@ const WFIL_WPFIL_POOL_ADDRESS = '0x443A6243A36Ef0ae1C46523d563c15abD787F4E9';
 const PFIL_CONTRACT = '0xAaa93ac72bECfbBc9149f293466bbdAa4b5Ef68C';
 const WPFIL_CONTRACT = '0x57E3BB9F790185Cfe70Cc2C15Ed5d6B84dCf4aDb';
 const WFIL_CONTRACT = '0x60E1773636CF5E4A227d9AC24F20fEca034ee25A';
+const REPL_HELPER_CONTRACT = '0x52d41C90ce592229F9fC0dA6Ef7b7cc26472B4E9';
 
-const filecoinCoinGeckoAPI =
-  'https://api.coingecko.com/api/v3/simple/price?ids=filecoin&vs_currencies=usd';
-async function query(api) {
-  return http.get(`${api}`);
-}
+// Total Assets of Miners pledged to the protocol
+const getMinerAssets = (agents) => {
+  return agents.reduce((sum, agent) => sum + +agent.minerBalance, 0);
+};
 
 module.exports = {
   filecoin: {
     tvl: async (_, _1, _2, { api }) => {
-      const [FILpriceData, resp] = await Promise.all([
-        query(filecoinCoinGeckoAPI),
-        // Retrieving this information directly on-chain is prohibitively costly.
-        // To alleviate this, we've encapsulated the TVL in a server, retrieve directly from the Filecoin chain
-        // This serves the data in a more efficient format and prevents overloading the Defillama frontend
-        get('http://repl.fi/api/stats'),
+      const [tvlComponents, activeAgents, FILpriceData] = await Promise.all([
+        api.call({ abi: abi.getTVLComponents, target: REPL_HELPER_CONTRACT }),
+        api.call({ abi: abi.getAllActiveAgents, target: REPL_HELPER_CONTRACT }),
       ]);
-      const tvlFIL = ethers.parseEther(resp.tvl_fil.toString());
 
-      api.add(nullAddress, tvlFIL);
+      const minerAssets = getMinerAssets(activeAgents);
+      api.add(nullAddress, minerAssets);
+      api.add(nullAddress, tvlComponents);
     },
   },
 };
