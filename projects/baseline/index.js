57dfd0d211376d5c69e224856487851b5437b890
brightiron <95196612+brightiron@users.noreply.github.com>
2024-03-03 21:07:46 -0600

    add borrowed reserves

--- projects/baseline/index.js
+++ projects/baseline/index.js
@@ -1,9 +1,13 @@
 const ADDRESSES = require("../helper/coreAssets.json");
+
 const sdk = require("@defillama/sdk");
+const BASELINE_CONTRACT = "0x14eB8d9b6e19842B5930030B18c50B0391561f27";
 
 async function tvl(_, _1, _2, { api }) {
-  const balances = {};
-  const BASELINE_CONTRACT = "0x14eB8d9b6e19842B5930030B18c50B0391561f27";
+  const bAsset = await api.call({
+    abi: abi.bAsset,
+    target: BASELINE_CONTRACT,
+  });
 
   //floor, anchor, discovery
   const positions = [0, 1, 2];
@@ -28,6 +32,8 @@ async function tvl(_, _1, _2, { api }) {
 
   const reserves = baselinePositionBalances.map((b) => ({ reserves: b[0] }));
 
+  const balances = {};
+
   //sum the reserve balances
   reserves.map((r) => {
     sdk.util.sumSingleBalance(
@@ -41,14 +47,32 @@ async function tvl(_, _1, _2, { api }) {
   return balances;
 }
 
+async function borrowed(_, _1, _2, { api }) {
+  const balances = {};
+  const lentReserves = await api.call({
+    abi: abi.totalLentReserves,
+    target: BASELINE_CONTRACT,
+  });
+  sdk.util.sumSingleBalance(
+    balances,
+    ADDRESSES.blast.WETH,
+    lentReserves,
+    "blast"
+  );
+  return balances;
+}
+
 module.exports = {
   doublecounted: true,
   blast: {
     tvl,
+    borrowed,
   },
 };
 
 const abi = {
+  bAsset: "function bAsset() view returns (address)",
+  totalLentReserves: "function totalLentReserves() view returns (uint256)",
   getPosition:
     "function getPosition(uint8) view returns (tuple(uint8, int24, int24))",
   getBalancesForPosition:
