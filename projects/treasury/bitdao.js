f4950c832fdd49c61e20ae641732a02ab15b2b7d
Define101 <93603962+Define101@users.noreply.github.com>
2024-03-05 13:18:39 +0000

    add new wallet mantle treasury (#9198)

--- projects/treasury/bitdao.js
+++ projects/treasury/bitdao.js
@@ -1,10 +1,26 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const {  nullAddress,treasuryExports } = require("../helper/treasury");
 
-const bitdaoTreasury1 = "0x78605Df79524164911C144801f41e9811B7DB73D";
+//eth wallets
+const mTreasuryL1 = "0x78605Df79524164911C144801f41e9811B7DB73D";
+const mTreasuryL1SC = "0xCa264A4Adf80d3c390233de135468A914f99B6a5"
+const mTreasuryL1O1 = "0xf0e91a74cb053d79b39837E1cfba947D0c98dd93"
+const mTreasuryL1E1 = "0x1a743BD810dde05fa897Ec41FE4D42068F7fD6b2"
+const mTreasuryL1RB1 = "0x164Cf077D3004bC1f26E7A46Ad8fA54df4449E3F"
+const mTreasuryL1LPE1 = "0xA5b79541548ef2D48921F63ca72e4954e50a4a74"
+
+//mantle wallets
+
+const mTreasuryL2 = "0x94FEC56BBEcEaCC71c9e61623ACE9F8e1B1cf473"
+const mTreasuryL2RB2 = "0x87C62C3F9BDFc09200bCF1cbb36F233A65CeF3e6"
+const mTreasuryL2LPM1 = "0x992b65556d330219e7e75C43273535847fEee262"
+const mTreasuryL2FF1 = "0xcD9Dab9Fa5B55EE4569EdC402d3206123B1285F4"
 
 const BIT = "0x1A4b46696b2bB4794Eb3D4c26f1c55F9170fa4C5";
 const MNT = "0x3c3a81e81dc49a522a592e7622a7e711c06bf354"
+
+const MNTMantle = "0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000"
+const wrappedmantleonmantle = "0x78c1b0C915c4FAA5FffA6CAbf0219DA63d7f4cb8"
 
 module.exports = treasuryExports({
   ethereum: {
@@ -18,9 +34,23 @@ module.exports = treasuryExports({
         '0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272',//xSUSHI
         "0x52A8845DF664D76C69d2EEa607CD793565aF42B8",
      ],
-    owners: [bitdaoTreasury1],
+    owners: [mTreasuryL1, mTreasuryL1SC,mTreasuryL1O1,mTreasuryL1E1, mTreasuryL1RB1, mTreasuryL1LPE1 ],
     ownTokens: [BIT, MNT],
     resolveLP: true,
     resolveUniV3: true,
+  },
+  mantle: {
+    tokens: [ 
+        nullAddress,
+        "0x5bE26527e817998A7206475496fDE1E68957c5A6", //ondo usd
+        "0xcda86a272531e8640cd7f1a92c01839911b90bb0", //mantle staked eth
+        "0xdeaddeaddeaddeaddeaddeaddeaddeaddead1111", //eth
+        "0x09bc4e0d864854c6afb6eb9a9cdf58ac190d0df9", //usdc
+        "0x201EBa5CC46D216Ce6DC03F6a759e8E766e956aE", //tether
+        "0xf52b354ffdb323e0667e87a0136040e3e4d9df33", //lp meth
+     ],
+    owners: [mTreasuryL2, mTreasuryL2RB2, mTreasuryL2LPM1, mTreasuryL2FF1],
+    ownTokens: [MNTMantle, wrappedmantleonmantle],
+    resolveLP: true,
   },
 })
