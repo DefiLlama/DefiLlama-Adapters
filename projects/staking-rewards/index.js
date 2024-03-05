8189c2ca48baaa27f4959a153e30c5c1a66f7afd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-05 16:31:10 +0100

    code refactor

--- projects/staking-rewards/index.js
+++ projects/staking-rewards/index.js
@@ -1,27 +1,24 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const sdk = require('@defillama/sdk')
-const abi = require("./abi.json");
+const abi = {
+  "getSmartVaultAssetBalances": "function getSmartVaultAssetBalances(address, bool) external returns (uint256[] memory)"
+}
 
-const ethContract = ADDRESSES.ethereum.srETH;
+const ethContract = "0x5d6ac99835b0dd42ed9ffc606170e59f75a88fde";
 const spoolLens = '0x8aa6174333F75421903b2B5c70DdF8DA5D84f74F';
 
-async function eth(timestamp, ethBlock, chainBlocks) {
-  const tvlETH = await sdk.api.abi.call({
-    block: ethBlock,
+async function eth(timestamp, ethBlock, chainBlocks, { api }) {
+  const tvlETH = await api.call({
     target: spoolLens,
     abi: abi["getSmartVaultAssetBalances"],
     params: [ethContract, false]
   });
-
-  return {
-    [ADDRESSES.null]: tvlETH.output[0],
-  }
+  api.addGasToken(tvlETH[0])
+  return api.getBalances()
 }
 
 module.exports = {
   methodology: 'TVL is counted as deposits routed to the underlying Liquid Staking protocols in the vault.',
-  timetravel: true,
   doublecounted: true, // tokens are stored in underlying LSDs
+  misrepresentedTokens: true,
   ethereum: {
     tvl: eth
   }
