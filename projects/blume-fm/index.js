930da02aee273222105417f6e2b385369d3cd122
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-05 10:18:33 +0100

    Update index.js

--- projects/blume-fm/index.js
+++ projects/blume-fm/index.js
@@ -3,6 +3,5 @@ const { compoundExports2 } = require("../helper/compound");
 module.exports = {
   blast: compoundExports2({
     comptroller: "0x4EdF556c5664b4f86Ec50dB0F58B58B26210DC31",
-    cether: "0x901d7160C07c2750a254C1f9E2949A0d1860a219",
   }),
 };
