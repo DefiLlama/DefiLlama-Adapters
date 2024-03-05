ee70a19c023c61a2db0a59378e421a2c4c8bee29
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-05 14:47:14 +0100

    code refactor

--- projects/venofinance/index.js
+++ projects/venofinance/index.js
@@ -1,4 +1,3 @@
-const sdk = require("@defillama/sdk");
 const { staking } = require("../helper/staking.js");
 
 const fountain_contract_address = "0xb4be51216f4926ab09ddf4e64bc20f499fd6ca95";
@@ -9,46 +8,23 @@ async function tvlCronos(timestamp, ethBlock, chainBlocks, { api }) {
   const lcro_contract_address = "0x9Fae23A2700FEeCd5b93e43fDBc03c76AA7C08A6";
   const latom_contract_address = "0xac974ee7fc5d083112c809ccb3fce4a4f385750d";
 
-  const block = chainBlocks.cronos;
+  const cro_pooled = await api.call({ abi: "uint256:getTotalPooledCro", target: lcro_contract_address, })
+  const atom_pooled = await api.call({ abi: "uint256:getTotalPooledToken", target: latom_contract_address, })
 
-  const cro_pooled = await sdk.api.abi.call({
-    abi: "uint256:getTotalPooledCro",
-    target: lcro_contract_address,
-    block: block,
-    chain: "cronos",
-  });
-
-  const atom_pooled = await sdk.api.abi.call({
-    abi: "uint256:getTotalPooledToken",
-    target: latom_contract_address,
-    block: block,
-    chain: "cronos",
-  });
-
-  return {
-    "crypto-com-chain": Number(cro_pooled.output) / 1e18,
-    cosmos: Number(atom_pooled.output) / 1e6,
-  };
+  api.addGasToken(cro_pooled)
+  api.addCGToken('cosmos', atom_pooled / 1e6)
+  return api.getBalances()
 }
 
 async function tvlEra(timestamp, ethBlock, chainBlocks, { api }) {
   const leth_contract_address = "0xE7895ed01a1a6AAcF1c2E955aF14E7cf612E7F9d";
 
-  const eth_pooled = await sdk.api.abi.call({
-    abi: "uint256:getTotalPooledToken",
-    target: leth_contract_address,
-    chain: "era",
-    block: chainBlocks.era,
-  });
-
-  return {
-    ethereum: Number(eth_pooled.output) / 1e18,
-  };
+  const eth_pooled = await api.call({ abi: "uint256:getTotalPooledToken", target: leth_contract_address, })
+  api.addGasToken(eth_pooled)
+  return api.getBalances()
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
   methodology: "TVL counts tokens staked by the protocol.",
   cronos: {
     tvl: tvlCronos,
