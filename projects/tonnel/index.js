2c6df12c652161424ae30a6951aa5c3d2cdfaffb
Saber Coder <45599139+SaberDoTcodeR@users.noreply.github.com>
2024-03-05 13:24:43 +0000

    fix staking

--- projects/tonnel/index.js
+++ projects/tonnel/index.js
@@ -2,7 +2,7 @@ const { sumTokensExport } = require('../helper/chain/ton')
 
 const config = require('./config.js')
 const {sleep} = require("../helper/utils");
-const tonnel = "EQDNDv54v_TEU5t26rFykylsdPQsv5nsSZaH_v7JSJPtMitv"
+const tonnel = "0:cd0efe78bff4c4539b76eab17293296c74f42cbf99ec499687fefec94893ed32" //EQDNDv54v_TEU5t26rFykylsdPQsv5nsSZaH_v7JSJPtMitv
 const tonnelHolders = [
   "EQDTs-yjPLn7XzaRRq8pjp7H8Nw4y_OJ51Bk2dcrPlIYgwtV", // 10000 TONNEL
   "EQAgoyECSzCIFTFkMIvDLgdUE3D9RxGfYQQGfxy3lBBc_Ke_", // 1000 TONNEL
@@ -23,5 +23,5 @@ Object.keys(config).forEach(async (chain) => {
   }
   await sleep(30000)
   if (chain === 'ton')
-    module.exports.ton.staking = sumTokensExport({ owners: tonnelHolders, tokenSet: [tonnel], onlyWhitelistedTokens: true,})
+    module.exports.ton.staking = sumTokensExport({ owners: tonnelHolders, tokens: [tonnel], onlyWhitelistedTokens: true,})
 })
