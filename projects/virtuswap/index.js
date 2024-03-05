565fee09a4278f41721990941926ec1e267d1a47
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2024-03-05 11:34:19 +0400

    Add Arbitrum chain for VirtuSwap project

--- projects/virtuswap/index.js
+++ projects/virtuswap/index.js
@@ -1,11 +1,37 @@
 const { staking } = require('../helper/staking');
 
-const FACTORY_CONTRACT = '0xd4E3668A9C39ebB603f02A6987fC915dBC906B43';
-const STAKER = '0x9C58a2B79cd054442D5970b925637B9E88E7ecc2';
-const VRSW_TOKEN_POLYGON = '0x57999936fC9A9EC0751a8D146CcE11901Be8beD0';
+const POLYGON_STAKER = '0x9C58a2B79cd054442D5970b925637B9E88E7ecc2';
+const POLYGON_FACTORY_CONTRACT = '0xd4E3668A9C39ebB603f02A6987fC915dBC906B43';
+const POLYGON_VRSW_TOKEN = '0x57999936fC9A9EC0751a8D146CcE11901Be8beD0';
 
-async function tvl(_, _1, _2, { api }) {
-  const pools = await api.fetchList({ lengthAbi: 'allPairsLength', itemAbi: 'allPairs', target: FACTORY_CONTRACT })
+const ARBITRUM_STAKER = '0x68748818983CD5B4cD569E92634b8505CFc41FE8';
+const ARBITRUM_FACTORY_CONTRACT = '0x389DB0B69e74A816f1367aC081FdF24B5C7C2433';
+const ARBITRUM_VRSW_TOKEN = '0xd1E094CabC5aCB9D3b0599C3F76f2D01fF8d3563';
+
+async function polygon_tvl(_, _1, _2, { api }) {
+  const pools = await api.fetchList({ lengthAbi: 'allPairsLength', itemAbi: 'allPairs', target: POLYGON_FACTORY_CONTRACT })
+  const tokenLength = await api.multiCall({ abi: 'uint256:allowListLength', calls: pools })
+  const nativeTokens = await api.multiCall({ abi: 'function getTokens() view returns (address, address)', calls: pools })
+  const calls = []
+  for (let i = 0; i < pools.length; i++) {
+    const pool = pools[i];
+    const tokensNumber = tokenLength[i];
+    for (let j = 0; j < tokensNumber; j++) {
+      calls.push({ target: pool, params: [j], poolId: i });
+    }
+  }
+  const tokens = await api.multiCall({ abi: 'function allowList(uint256) view returns (address)', calls })
+  const ownerTokens = pools.map((v, i) => [nativeTokens[i], v])
+  tokens.forEach((v, i) => {
+    const tokens = ownerTokens[calls[i].poolId][0]
+    tokens.push(v)
+  })
+
+  return api.sumTokens({ ownerTokens, });
+}
+
+async function arbitrum_tvl(_, _1, _2, { api }) {
+  const pools = await api.fetchList({ lengthAbi: 'allPairsLength', itemAbi: 'allPairs', target: ARBITRUM_FACTORY_CONTRACT })
   const tokenLength = await api.multiCall({ abi: 'uint256:allowListLength', calls: pools })
   const nativeTokens = await api.multiCall({ abi: 'function getTokens() view returns (address, address)', calls: pools })
   const calls = []
@@ -29,8 +55,12 @@ async function tvl(_, _1, _2, { api }) {
 module.exports = {
   methodology: 'Sum of all pools liquidity plus staked VRSW tokens',
   polygon: {
-    tvl,
-    staking: staking(STAKER, VRSW_TOKEN_POLYGON),
+    tvl: polygon_tvl,
+    staking: staking(POLYGON_STAKER, POLYGON_VRSW_TOKEN),
+  },
+  arbitrum: {
+    tvl: arbitrum_tvl,
+    staking: staking(ARBITRUM_STAKER, ARBITRUM_VRSW_TOKEN),
   },
 };
 
