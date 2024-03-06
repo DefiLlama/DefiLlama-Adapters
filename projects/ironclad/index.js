fe78a40c9127a862d045027d2250a970316eaef5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-06 21:53:24 +0100

    code refactor

--- projects/ironclad/index.js
+++ projects/ironclad/index.js
@@ -1,16 +1,5 @@
-const sdk = require("@defillama/sdk");
-const { aaveChainTvl } = require('../helper/aave');
-
-function v2(chain, v2Registry){
-  const section = borrowed => sdk.util.sumChainTvls([
-    aaveChainTvl(chain, v2Registry, undefined, undefined, borrowed),
-  ])
-  return {
-    tvl: section(false),
-    borrowed: section(true)
-  }
-}
+const { aaveExports } = require('../helper/aave')
 
 module.exports = {
-  mode: v2("mode", "0x5C93B799D31d3d6a7C977f75FDB88d069565A55b"),
+  mode: aaveExports("mode", "0x5C93B799D31d3d6a7C977f75FDB88d069565A55b"),
 }
