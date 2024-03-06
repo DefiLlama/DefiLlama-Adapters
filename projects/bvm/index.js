d082ea5009ca4552a44003147e14579da45df2dc
elykct <elykct@nbc>
2024-03-06 16:48:32 +0700

    remove count assets from bridge, add univ3 factory contract

--- projects/bvm/index.js
+++ projects/bvm/index.js
@@ -13,28 +13,14 @@ const uniExports = uniV3Export({
       return tokens.filter(i => !coreAssets.has(i.toLowerCase()))
     }
   },
+  naka: {
+    factory: '0xf6632D6fF6fc71DAf1fA96AbAd1bC269bD507dF8', fromBlock: 49370, sumChunkSize: 50, filterFn: async (api, logs) => {
+      const tokens = logs.map(i => [i.token0, i.token1]).flat()
+      const coreAssets = new Set(getCoreAssets(api.chain))
+      return tokens.filter(i => !coreAssets.has(i.toLowerCase()))
+    }
+  },
 })
-
-const tokenTotalSupply = async(contractAddress) => {
-  return async (timestamp, block) => {
-    return {
-      [contractAddress]: (
-          await sdk.api.erc20.totalSupply({
-            block,
-            target: contractAddress,
-          })
-      ).output,
-    };
-  };
-};
-
-const getTVLNativeTokenBridge = async(chainBlocks) => {
-    const { output } = await sdk.api.eth.getBalance({ target: '0x694A7eF60ACe282E2a55a9bc6AdD56730e5Ee8B6', chain: 'naka', block: chainBlocks.naka })
-    
-    // BTC is native token of Naka ChainChain
-    // BTC TVL: 21M pre_minted - leftBalance (which is unlocked for users)
-    return (BigInt("0x115EEC47F6CF7E35000000") - BigInt(output)).toString();
-}
 
 module.exports = {
   // todo: test below export
@@ -42,10 +28,7 @@ module.exports = {
     tvl: sdk.util.sumChainTvls([
         sumTokensExport({ owner: '0xea21fbBB923E553d7b98D14106A104665BA57eCd', tokens: [ADDRESSES.nos.BTC] }),
         uniExports.nos.tvl,
-        tokenTotalSupply(ADDRESSES.naka.ETH),
-        tokenTotalSupply(ADDRESSES.naka.SATS),
-        tokenTotalSupply(ADDRESSES.naka.ORDI),
-        getTVLNativeTokenBridge
+        uniExports.naka.tvl
     ])
   }
 }
