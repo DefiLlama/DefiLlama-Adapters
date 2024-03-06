67c5d593a68ab561eecac9fb8b2e657cca0cb397
Xa Ek <max@pantherprotocol.io>
2024-03-06 12:19:05 +0000

    updating contracts to include staked + locked on polygon

--- projects/panther-protocol/index.js
+++ projects/panther-protocol/index.js
@@ -5,6 +5,8 @@ const { sumTokens2 } = require('../helper/unwrapLPs')
 const contracts = {
   polygon: {
     core: '0x9A06Db14D639796B25A6ceC6A1bf614fd98815EC',
+    staking: '0x4cec451f63dbe47d9da2debe2b734e4cb4000eac',
+    locked: '0x5e7fda6d9f5024c4ad1c780839987ab8c76486c9'
   },
   ethereum: {
     core: '0x909E34d3f6124C324ac83DccA84b74398a6fa173',
@@ -23,7 +25,7 @@ function getTVLFunc(contractAddress, chain) {
 function getChainTVL(chain) {
   return {
     staking: contracts[chain].staking ? staking(contracts[chain].staking, contracts[chain].core) : undefined,
-    tvl: getTVLFunc(contracts[chain].proxy, chain),
+    tvl: getTVLFunc(contracts[chain].locked, chain),
   };
 }
 
