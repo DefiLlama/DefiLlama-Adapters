5932eb7aa16f2fa72a0ac0d45d81fbc9e8106b4b
Hyungsuk Kang <hskang9@gmail.com>
2024-03-08 06:39:54 +0900

    Add Blast deployment (#9239)

--- projects/standard-mode/index.js
+++ projects/standard-mode/index.js
@@ -1,6 +1,7 @@
 const { getLogs } = require('../helper/cache/getLogs')
 const config = {
   mode: { factory: '0x4D1b18A7BDB8D0a02f692398337aBde8DeB8FB09', fromBlock: 4381503, },
+  blast: { factory: '0x2CC505C4bc86B28503B5b8C450407D32e5E20A9f', fromBlock: 434946, }, 
 }
 
 Object.keys(config).forEach(chain => {
