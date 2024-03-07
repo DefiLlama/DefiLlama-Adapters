668acf9ca803d756f576202660fedef6fd7ac91d
0xngmi <0xngmi@protonmail.com>
2024-03-07 23:12:38 +0000

    add fraxtal

---
+++ projects/ra/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  fraxtal: { factory: '0x95120704f4E2D545Aea8b6B3c16d9Da1fa32E30F', fromBlock: 1352709, },
+})
