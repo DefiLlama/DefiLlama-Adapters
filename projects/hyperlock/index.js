3957276fcf93c6e3a0f791729b88d67b7c17378f
0xngmi <0xngmi@protonmail.com>
2024-03-08 20:42:26 +0000

    fix

--- projects/hyperlock/index.js
+++ projects/hyperlock/index.js
@@ -4,6 +4,6 @@ const { sumTokensExport } = require('../helper/unwrapLPs')
 module.exports = {
   doublecounted: true,
   blast: {
-    tvl: sumTokensExport({ owner: '0xc28EffdfEF75448243c1d9bA972b97e32dF60d06', resolveUniV3: true, })
+    tvl: sumTokensExport({ owners: ['0xc28EffdfEF75448243c1d9bA972b97e32dF60d06', '0xC3EcaDB7a5faB07c72af6BcFbD588b7818c4a40e'], resolveUniV3: true, })
   }
 }
