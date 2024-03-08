5930e893569b23d427a8b80f977ecd1977ab39d2
bd21 <bd21@uw.edu>
2024-03-08 01:30:53 -0800

    add price

--- projects/superstate/index.js
+++ projects/superstate/index.js
@@ -1,6 +1,13 @@
+const { get } = require("../helper/http");
+
 module.exports = {
     methodology: "Sums the total supplies of Superstate's issued tokens.",
 };
+
+async function USTBPrice() {
+    const data = await get("https://api.superstate.co/v1/funds/1/nav-daily")
+    return parseFloat(data[0].net_asset_value) / 10.0
+}
 
 const config = {
     ethereum: {
@@ -16,8 +23,7 @@ Object.keys(config).forEach((chain) => {
         tvl: async (_, _b, _cb, { api }) => {
             let supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: fundAddresses });
             api.addTokens(fundAddresses, supplies);
-            return api.getBalances();
-        },
+            return api.getBalances() * await USTBPrice();
+        }
     };
 });
-
