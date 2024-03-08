f424f3e94a1b66f924d5baa9fdda7889745aeacd
UjjwalGupta49 <ujjwalg056@gmail.com>
2024-03-08 11:18:48 +0530

    added FLP3 (governance pool) tokenAccounts to tvl

--- projects/flashtrade/index.js
+++ projects/flashtrade/index.js
@@ -7,6 +7,10 @@ async function tvl() {
     "FuFoCkfnrDjNmwPr54JEAYTUshXA4gQojevfvv3KXdx7", // ETH
     "55UmrYacpb8v7gbKswDofmWjLS8TSP3VB8NKjNfxu11d", // BTC
     "7b2jY9CeCWCnyKBvaLSnsV7qwUhbJGsJTPdyCsspPY7Q", // Virtual Pool - USDC
+    "CjRKKtT3DCsDh7gvQwkX1aCLpYhhDUKaNrJhUzGtsHUC", // Governance Pool - USDC
+    "CjSYeE668mfLdY7hi1B1ocLMzAVAFFtEhjKrGo8hgvuy", // JUP
+    "5rdgpCu7xFDhyksEAyUxsyuH9q1KQReKg1FwzmGA7mFq", // PYTH
+    "FahFdXRRn1iodaCs9MZyNjSb62TTpaQCD98Xcsdowdgf" // JTO
   ];
 
   return sumTokens2({ tokenAccounts: vaults });
