a42897f4d5b2c46d6203fa91c3623fa2dfe4b9df
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-09 11:56:26 +0100

    bugfix

--- projects/clone/index.js
+++ projects/clone/index.js
@@ -1,28 +1,22 @@
 const anchor = require("@coral-xyz/anchor");
 const { sumTokens2, getProvider, } = require("../helper/solana");
 
-const programId = new anchor.web3.PublicKey(
-    'C1onEW2kPetmHmwe74YC1ESx3LnFEpVau6g2pg4fHycr')
+const programId = new anchor.web3.PublicKey('C1onEW2kPetmHmwe74YC1ESx3LnFEpVau6g2pg4fHycr')
 
 async function tvl() {
   const provider = getProvider()
   const idl = await anchor.Program.fetchIdl(programId, provider)
   const program = new anchor.Program(idl, programId, provider)
 
-  let owners = []
-  let tokens = []
+  let tokenAccounts = []
 
   const cloneAccountAddress = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("clone")], programId)[0]
+    [Buffer.from("clone")], programId)[0]
 
   const cloneAccount = await program.account.clone.fetch(cloneAccountAddress)
-  owners.push(cloneAccount.collateral.vault.toString())
-  tokens.push(cloneAccount.collateral.mint.toString())
+  tokenAccounts.push(cloneAccount.collateral.vault.toString())
 
-  return sumTokens2({ 
-      tokens,
-      owners,
-  })
+  return sumTokens2({ tokenAccounts })
 }
 
 module.exports = {
