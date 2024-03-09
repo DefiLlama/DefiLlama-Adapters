379e95110e6e42701f2ba2a4ee304bc90f209cdf
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-09 09:53:43 +0100

    code refactor

--- projects/gasline/index.js
+++ projects/gasline/index.js
@@ -1,42 +1,17 @@
-const GASLINE_CONTRACT = {
-  81457: '0xD5400cAc1D76f29bBb8Daef9824317Aaf9d3C0a1',
-  56: '0x35138Ddfa39e00C642a483d5761C340E7b954F94',
-};
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
-const WETH = {
-  81457: '0x4300000000000000000000000000000000000004',
-  56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-};
-
-const abi = {
-  balanceOf: "erc20:balanceOf",
-  totalSupply: "function totalSupply() view returns (uint256)",
-};
-
-function tvl(chainId) {
-  return async function(_, _1, _2, { api }) {
-    const deposited = await api.call({ abi: abi.balanceOf, target: WETH[chainId], params: GASLINE_CONTRACT[chainId] });
-
-    api.addGasToken(deposited);
-  }
+const config = {
+  blast: { gasline: '0xD5400cAc1D76f29bBb8Daef9824317Aaf9d3C0a1', weth: '0x4300000000000000000000000000000000000004', },
+  bsc: { gasline: '0x35138Ddfa39e00C642a483d5761C340E7b954F94', weth: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', },
 }
 
-function borrowed(chainId) {
-  return async function(_, _1, _2, { api }) {
-    const borrowed = await api.call({ abi: abi.totalSupply, target: GASLINE_CONTRACT[chainId] });
-
-    api.addGasToken(borrowed);
+Object.keys(config).forEach(chain => {
+  const {gasline, weth} = config[chain]
+  module.exports[chain] = {
+    tvl: sumTokensExport({ owner: gasline, tokens: [weth] }),
+    borrowed: async (_, _b, _cb, { api, }) => {
+      const supply = await api.call({  abi: 'erc20:totalSupply', target: gasline})
+      api.addGasToken(supply)
+    },
   }
-}
-
-module.exports = {
-  doublecounted: true,
-  blast: {
-    tvl: tvl(81457),
-    borrowed: borrowed(81457),
-  },
-  bsc: {
-    tvl: tvl(56),
-    borrowed: borrowed(56),
-  },
-};
+})
