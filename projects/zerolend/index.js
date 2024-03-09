b6c6f48c452b3427ad0f02decf80b109e4790d5f
Deadshot Ryker <ryker@zerolend.xyz>
2024-03-09 18:58:04 -0500

    added linea and the mainnet lrt markets

--- projects/zerolend/index.js
+++ projects/zerolend/index.js
@@ -10,4 +10,10 @@ module.exports = {
   blast: aaveExports("blast", undefined, undefined, [
     "0xc6DF4ddDBFaCb866e78Dcc01b813A41C15A08C10",
   ]),
+  linea: aaveExports("linea", undefined, undefined, [
+    "0x67f93d36792c49a4493652B91ad4bD59f428AD15",
+  ]),
+  ethereum: aaveExports("ethereum", undefined, undefined, [
+    "0x47223D4eA966a93b2cC96FFB4D42c22651FADFcf",
+  ]),
 };
