efb673d3028a02b9d354affcdc088536a1929779
0xkaidev <0xkai.dev@gmail.com>
2024-03-09 15:11:55 +0700

    Update fromBlock

--- projects/moraswap-v3/index.js
+++ projects/moraswap-v3/index.js
@@ -3,7 +3,7 @@ const { uniV3Export } = require('../helper/uniswapV3')
 module.exports = uniV3Export({
   neon_evm: {
     factory: '0x58122246F7e33669cde3486Dd72f95c2e886E375',
-    fromBlock: 0,
+    fromBlock: 237396579,
     isAlgebra: false,
   }
 })
