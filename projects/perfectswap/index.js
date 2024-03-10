516fde361965db0e6a3864d84022b75758a4604f
pandorabok <borkovicbiljana9@gmail.com>
2024-03-10 20:13:16 +0200

    add perfectswap: change contract adrress

--- projects/perfectswap/index.js
+++ projects/perfectswap/index.js
@@ -3,7 +3,7 @@ const { uniTvlExport } = require('../helper/calculateUniTvl.js')
 module.exports = {
   misrepresentedTokens: true,
   arbitrum: {
-    tvl: uniTvlExport("0x9697552931f5f2c7287db7Ee3a6aEB5c8DE21870", undefined, undefined, {
+    tvl: uniTvlExport("0x6df18f08dF448ca2336dA5Fe1db9c6Fef7D5dFD5", undefined, undefined, {
       allPairsLength: 'uint256:allPoolsLength',
       allPairs: 'function allPools(uint256) view returns (address)',
     }, { useDefaultCoreAssets: true, hasStablePools: true, }),
