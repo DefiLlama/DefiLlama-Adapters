b0ebd0e18489d472628fd70d8d7fd84bebc7b89f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-11 23:54:46 +0100

    code refactor

--- projects/suilend/index.js
+++ projects/suilend/index.js
@@ -1,4 +1,3 @@
-const BigNumber = require("bignumber.js");
 const sui = require("../helper/chain/sui");
  
 const SUILEND_LENDING_MARKET_ID = "0x84030d26d85eaa7035084a057f2f11f701b7e2e4eda87551becbc7c97505ece1";
@@ -8,8 +7,7 @@ async function tvl() {
   const object = await sui.getObject(SUILEND_LENDING_MARKET_ID)
   for (const reserve of object.fields.reserves) {
     const coinType = '0x' + reserve.fields.coin_type.fields.name;
-    const amount = new BigNumber(reserve.fields.available_amount)
-    api.add(coinType, amount.toString())
+    api.add(coinType, reserve.fields.available_amount)
   }
 }
 
@@ -18,8 +16,7 @@ async function borrowed() {
   const object = await sui.getObject(SUILEND_LENDING_MARKET_ID)
   for (const reserve of object.fields.reserves) {
     const coinType = '0x' + reserve.fields.coin_type.fields.name;
-    const amount = (new BigNumber(reserve.fields.borrowed_amount.fields.value)).dividedBy(new BigNumber(10 ** 18)).integerValue(BigNumber.ROUND_HALF_CEIL);
-    api.add(coinType, amount.toString())
+    api.add(coinType, reserve.fields.borrowed_amount.fields.value / 1e18)
   }
 }
 
