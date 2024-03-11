7328096bda4ef22db4b34ff314c0a971395cc186
ArthSwap Dev <dev@arthswap.org>
2024-03-11 10:21:46 +0900

    fix staking

--- projects/arthswap/index.js
+++ projects/arthswap/index.js
@@ -1,12 +1,10 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { getUniTVL } = require("../helper/unknownTokens");
-const { stakingPricedLP } = require("../helper/staking");
+const { staking } = require("../helper/staking");
 
 const FACTORIES = "0xA9473608514457b4bF083f9045fA63ae5810A03E";
 
-const TOKENS = {
-  ARSW: ADDRESSES.astar.ARSW,
-};
+const arsw = ADDRESSES.astar.ARSW;
 
 const STAKING_CONTRACTS = {
   astar: "0x42d175a498Cb517Ad29d055ea7DcFD3D99045404",
@@ -20,12 +18,6 @@ module.exports = {
       factory: FACTORIES,
       useDefaultCoreAssets: true,
     }),
-    staking: stakingPricedLP(
-      STAKING_CONTRACTS.astar,
-      TOKENS.ARSW,
-      "astar",
-      "0x50497E7181eB9e8CcD70a9c44FB997742149482a",
-      "arthswap"
-    ),
+      staking: staking(STAKING_CONTRACTS.astar, arsw, 'astar'),
   },
 };
