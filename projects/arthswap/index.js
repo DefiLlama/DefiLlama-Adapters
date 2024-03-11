9c875936d4909e41c5db054557f7f06fd57de63c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-11 11:34:54 +0100

    code refactor

--- projects/arthswap/index.js
+++ projects/arthswap/index.js
@@ -1,23 +1,12 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const { getUniTVL } = require("../helper/unknownTokens");
 const { staking } = require("../helper/staking");
 
 const FACTORIES = "0xA9473608514457b4bF083f9045fA63ae5810A03E";
 
-const arsw = ADDRESSES.astar.ARSW;
-
-const STAKING_CONTRACTS = {
-  astar: "0x42d175a498Cb517Ad29d055ea7DcFD3D99045404",
-};
-
 module.exports = {
-  timetravel: true,
-  methodology: "Arthswap Tvl Calculation",
+  misrepresentedTokens: true,
   astar: {
-    tvl: getUniTVL({
-      factory: FACTORIES,
-      useDefaultCoreAssets: true,
-    }),
-      staking: staking(STAKING_CONTRACTS.astar, arsw, 'astar'),
+    tvl: getUniTVL({ factory: FACTORIES, useDefaultCoreAssets: true, }),
+    staking: staking('0x42d175a498Cb517Ad29d055ea7DcFD3D99045404', '0xde2578edec4669ba7f41c5d5d2386300bcea4678'),
   },
 };
