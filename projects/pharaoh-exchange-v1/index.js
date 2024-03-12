52476b8c2ac5585d1e18fd4a21efc6df3dca22c2
0xshad0w <hugomncs@gmail.com>
2024-03-12 22:54:31 +0000

    fix factory address

--- projects/pharaoh-exchange-v1/index.js
+++ projects/pharaoh-exchange-v1/index.js
@@ -4,11 +4,7 @@ const { staking } = require('../helper/staking')
 module.exports = {
   misrepresentedTokens: true,
   avax:{
-<<<<<<< HEAD
     tvl: getUniTVL({ factory: '0xAAA16c016BF556fcD620328f0759252E29b1AB57', useDefaultCoreAssets: true,  hasStablePools: true, stablePoolSymbol: 'crAMM' }),
-=======
-    tvl: getUniTVL({ factory: '0xAAA91e283126774b3bb513fD5922976d5212dc49', useDefaultCoreAssets: true,  hasStablePools: true, stablePoolSymbol: 'crAMM' }),
->>>>>>> 26da82eec2e4e4e68c0756d6278ca71758c22e9c
     staking: staking("0xAAA3249511DE3E7A5c61FbA8313170c1Bef9A65e", "0xAAAB9D12A30504559b0C5a9A5977fEE4A6081c6b"),
   },
 }
