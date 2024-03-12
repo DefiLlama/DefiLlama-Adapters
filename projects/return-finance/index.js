032427df8ede081bcb4bf83b1a0e8a42b43e4731
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-12 17:52:32 +0100

    code refactor

--- projects/return-finance/index.js
+++ projects/return-finance/index.js
@@ -1,47 +1,19 @@
-const ADDRESSES = require("../helper/coreAssets.json");
-const { sumTokensExport } = require("../helper/unwrapLPs");
-
-// AAVE V3 Polygon
-const ownerPolygon = "0x3B6385493a1d4603809dDbaE647200eF8baA53F5";
-const tokensPolygon = [
-  ADDRESSES.polygon.USDC,
-  "0x625E7708f30cA75bfd92586e17077590C60eb4cD",
-];
-
-// AAVE V3 - Avalanche
-const ownerAvax = "0x3B6385493a1d4603809dDbaE647200eF8baA53F5";
-const tokensAvax = ["0x625E7708f30cA75bfd92586e17077590C60eb4cD"];
-
-// Compound V3 - Base
-const ownerBase = "0x3B6385493a1d4603809dDbaE647200eF8baA53F5";
-const tokensBase = ["0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf"];
-
-// MakerDAO - Ethereum
-const ownerMaker = "0x201254227f9fE57296C257397Be6c617389a8cCb";
-
-// Convex Finance - Ethereum
-const ownerConvex = "0xFD360A096E4a4c3C424fc3aCd85da8010D0Db9a5";
-
-const tokensETH = [
-  "0x201254227f9fE57296C257397Be6c617389a8cCb",
-  "0xFD360A096E4a4c3C424fc3aCd85da8010D0Db9a5",
-];
+const { sumERC4626VaultsExport } = require("../helper/erc4626")
 
 module.exports = {
-  methodology: "Track funds locked in protocols",
-  polygon: {
-    tvl: sumTokensExport({ owner: ownerPolygon, tokens: tokensPolygon }),
-  },
-  avax: {
-    tvl: sumTokensExport({ owner: ownerAvax, tokens: tokensAvax }),
-  },
-  base: {
-    tvl: sumTokensExport({ owner: ownerBase, tokens: tokensBase }),
-  },
-  ethereum: {
-    tvl: sumTokensExport({
-      owners: [ownerMaker, ownerConvex],
-      tokens: tokensETH,
-    }),
-  },
+  doublecounted: true,
 };
+
+const config = {
+  ethereum: ['0xFD360A096E4a4c3C424fc3aCd85da8010D0Db9a5', '0x201254227f9fE57296C257397Be6c617389a8cCb'],
+  avax: ['0x3B6385493a1d4603809dDbaE647200eF8baA53F5'],
+  polygon: ['0x3B6385493a1d4603809dDbaE647200eF8baA53F5'],
+  base: ['0x3B6385493a1d4603809dDbaE647200eF8baA53F5'],
+}
+
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = {
+    tvl: sumERC4626VaultsExport({ vaults: config[chain], isOG4626: true, })
+  }
+})
