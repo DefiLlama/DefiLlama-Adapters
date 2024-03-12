dce98d70f9ab56460bf32ed120082630437e2b88
0xngmi <0xngmi@protonmail.com>
2024-03-12 21:43:05 +0000

    fix double cether

--- projects/orbitlending-io/index.js
+++ projects/orbitlending-io/index.js
@@ -3,7 +3,7 @@ const { compoundExports2 } = require('../helper/compound');
 module.exports = {
   blast: compoundExports2({
     comptroller: '0x1E18C3cb491D908241D0db14b081B51be7B6e652',
-    cether: '0xF9B3B455f5d900f62bC1792A6Ca6e1d47B989389',
-    blacklistedTokens: ['0xf92996ddc677a8dcb032ac5fe62bbf00f92ae2ec', '0x0872b71efc37cb8dde22b2118de3d800427fdba0']
+    cether: ['0xf9b3b455f5d900f62bc1792a6ca6e1d47b989389', '0x0872b71efc37cb8dde22b2118de3d800427fdba0'],
+    blacklistedTokens: ['0xf92996ddc677a8dcb032ac5fe62bbf00f92ae2ec']
   }),
 }; 
