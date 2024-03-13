dc9e4ca26ae4b9d70b3b1024aa312f7bf4f97566
0xShad0w <44497716+0xShad0w@users.noreply.github.com>
2024-03-13 07:04:11 +0000

    Pharaoh v1 (#9318)
    * add phar-v1 pools
    
    * fix factory address
    
    * fix factory address

--- projects/pharaoh-exchange-v1/index.js
+++ projects/pharaoh-exchange-v1/index.js
@@ -4,7 +4,7 @@ const { staking } = require('../helper/staking')
 module.exports = {
   misrepresentedTokens: true,
   avax:{
-    tvl: getUniTVL({ factory: '0xAAA91e283126774b3bb513fD5922976d5212dc49', useDefaultCoreAssets: true,  hasStablePools: true, stablePoolSymbol: 'crAMM' }),
+    tvl: getUniTVL({ factory: '0xAAA16c016BF556fcD620328f0759252E29b1AB57', useDefaultCoreAssets: true,  hasStablePools: true, stablePoolSymbol: 'crAMM' }),
     staking: staking("0xAAA3249511DE3E7A5c61FbA8313170c1Bef9A65e", "0xAAAB9D12A30504559b0C5a9A5977fEE4A6081c6b"),
   },
 }
