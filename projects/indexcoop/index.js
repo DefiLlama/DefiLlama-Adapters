3728000c06f3b34fedb88be6cb2469be34c60a7c
crews <64375250+TheYoungCrews@users.noreply.github.com>
2024-03-13 10:00:44 -0500

    Add new Index Coop Leverage Indices
    We migrated all TVL from the old FLI products into new 2x tokens built on Aave v3 instead of Compound v2
    
    see announcement: https://discord.com/channels/762061559744299010/814879319633428540/1217264332635902134

--- projects/indexcoop/index.js
+++ projects/indexcoop/index.js
@@ -4,9 +4,11 @@ const sdk = require("@defillama/sdk")
 
 const dpiAddress = "0x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b";
 const ethFliAddress = "0xaa6e8127831c9de45ae56bb1b0d4d4da6e5665bd";
+const eth2x = "0x65c4C0517025Ec0843C9146aF266A2C5a2D148A2";
 const mviAddress = "0x72e364f2abdc788b7e918bc238b21f109cd634d7";
 const cgiAddress = "0xada0a1202462085999652dc5310a7a9e2bf3ed42";
 const btcFliAddress = "0x0b498ff89709d3838a063f1dfa463091f9801c2b";
+const btc2x = "0xD2AC55cA3Bbd2Dd1e9936eC640dCb4b745fDe759";
 const bedAddress = "0x2aF1dF3AB0ab157e1E2Ad8F88A7D04fbea0c7dc6";
 const dataAddress = "0x33d63Ba1E57E54779F7dDAeaA7109349344cf5F1";
 const gmiAddress = "0x47110d43175f7f2c2425e7d15792acc5817eb44f";
@@ -18,6 +20,8 @@ const gtcETH = '0x36c833Eed0D376f75D1ff9dFDeE260191336065e'
 const sets = [
   dpiAddress,
   ethFliAddress,
+  btc2x,
+  eth2x,
   mviAddress,
   cgiAddress,
   btcFliAddress,
