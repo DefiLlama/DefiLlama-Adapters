43c619d656d1a91eb3833628ff7d0fa8efac4e06
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-13 08:11:41 +0100

    code refactor

--- projects/maduck/index.js
+++ projects/maduck/index.js
@@ -1,28 +1,4 @@
-const { addFundsInMasterChef } = require("../helper/masterchef");
-const { pool2BalanceFromMasterChefExports} = require("../helper/pool2.js");
 
-const MasterChef = "0x944dFb7f7caB8bbA2F74882784742C39b8495F5e";
+const { masterchefExports } = require('../helper/unknownTokens')
 
-const bscTvl = async (timestamp, ethBlock, chainBlocks) => {
-  const balances = {};
-  let transformAddress = i => `bsc:${i}`;
-
-  await addFundsInMasterChef(
-    balances,
-    MasterChef,
-    chainBlocks["bsc"],
-    "bsc",
-    transformAddress
-  );
-
-  return balances;
-};
-
-module.exports = {
-  misrepresentedTokens: true,
-  bsc: {
-    tvl: bscTvl,
-  },
-  methodology:
-    "We count liquidity on the Farms through MasterChef Contract",
-};
+module.exports = masterchefExports({ chain: 'bsc', masterchef: '0x944dFb7f7caB8bbA2F74882784742C39b8495F5e', nativeToken: '0xb976d9684412f75f7aee24e56d846fd404b1b329', useDefaultCoreAssets: true, })
