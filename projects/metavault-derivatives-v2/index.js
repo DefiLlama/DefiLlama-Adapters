9ea5c80bd25be129cc45cd9d25733428a6a752bd
0xR <0xr@metavault.org>
2024-03-13 16:00:02 +0300

    Update Metavault Derivatives V2 chains

--- projects/metavault-derivatives-v2/index.js
+++ projects/metavault-derivatives-v2/index.js
@@ -2,14 +2,23 @@ const ADDRESSES = require("../helper/coreAssets.json");
 
 const { nullAddress } = require("../helper/unwrapLPs");
 
-async function LineaTvl(_time, _ethBlock, _cb, { api}) {
+async function LineaTvl(_time, _ethBlock, _cb, { api }) {
   const tokens = [nullAddress, ADDRESSES.linea.USDC];
-  const owners = ["0xf3Ef1c95aecf5B5025815014890dC14488599883"];
-  return api.sumTokens({ owners, tokens})
+  const owners = ["0xb514Ee8a1e00B102cE2312048abcbc3E57bfED94"];
+  return api.sumTokens({ owners, tokens });
+}
+
+async function PolygonTvl(_time, _ethBlock, _cb, { api }) {
+  const tokens = [nullAddress, ADDRESSES.polygon.USDC];
+  const owners = ["0xAb36984e4952e5a9d08536C4dE5190ed37725017"];
+  return api.sumTokens({ owners, tokens });
 }
 
 module.exports = {
   linea: {
     tvl: LineaTvl,
+  },
+  polygon: {
+    tvl: PolygonTvl,
   },
 };
