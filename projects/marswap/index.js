a5abe59e983fc5ce37a089b0b006d72ec2009562
Topleft <87454455+topleft1562@users.noreply.github.com>
2024-03-14 11:53:38 -0500

    back. meant to do this locally

--- projects/marswap/index.js
+++ projects/marswap/index.js
@@ -1,9 +1,9 @@
 const { getUniTVL } = require('../helper/unknownTokens')
 
-
+/*
 let TVL_SHIBARIUM_V1 = getUniTVL({factory: "0xBe0223f65813C7c82E195B48F8AAaAcb304FbAe7", useDefaultCoreAssets: true,})
 let TVL_SHIBARIUM_V2 = getUniTVL({factory: "0xd871a3f5d3bB9c00DDB0cC772690351B9712968D", useDefaultCoreAssets: true,})
-let CombinedTVL = TVL_SHIBARIUM_V1.map((arrayV1, index) => arrayV1.concat(TVL_SHIBARIUM_V2[index]));
+*/
 
 module.exports = {
     bsc: {
@@ -13,7 +13,7 @@ module.exports = {
         tvl: getUniTVL({ factory: '0x5e763172d59b3b580af29a1c9fa4ac1cee69c5dd', useDefaultCoreAssets: true, })
     },
     shibarium: {
-        tvl: CombinedTVL,
+        tvl: getUniTVL({factory: "0xBe0223f65813C7c82E195B48F8AAaAcb304FbAe7", useDefaultCoreAssets: true,})
     },
     base: {
         tvl: getUniTVL({factory: "0xeE42fe6d6Be1eF43701DDAbc417AD22d82C5ecC3", useDefaultCoreAssets: true,})
