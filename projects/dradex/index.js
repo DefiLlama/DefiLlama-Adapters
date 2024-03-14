489417f5b5b22c43296e5da0ed7d086e91347fa3
DradexDex <dradex@tuta.io>
2024-03-14 22:03:57 +0300

    add dradex

---
+++ projects/dradex/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    hallmarks: [
+      [1660392000,"Rug Pull"]
+    ],
+    methodology:
+      "dradex exchange TVL is computed by looking at the balances of the accounts holding the tokens backing the dradex vault user. The data comes from https://dradex.xyz/pool.",
+    solana: {
+      tvl: () => 0
+    },
+  };
