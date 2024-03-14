77ff31aad98b6cbb41dc819ade6cdb9ca47668ec
AbelCz <abel.czupor@gmail.com>
2024-03-14 13:32:54 +0100

    Add TOKEN to counting TokenFi's Staking TVL

--- projects/tokenfi/index.js
+++ projects/tokenfi/index.js
@@ -3,17 +3,19 @@ const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
 const contract = "0xb8D2471E35eE033Db509e0456c8eFc4135f4EE43"
 const flokiToken = "0xfb5B838b6cfEEdC2873aB27866079AC55363D37E"
 const ethFlokiToken = "0xcf0C122c6b73ff809C693DB761e7BaeBe62b6a2E"
+const tokenToken = "0x4507cEf57C46789eF8d1a19EA45f4216bae2B528"
+const ethTokenToken = "0x4507cEf57C46789eF8d1a19EA45f4216bae2B528"
 
 async function ethtvl(time, ethBlock, _b, {api}) {
-  return sumTokens2({ tokens: [ethFlokiToken], owner: contract, api })
+  return sumTokens2({ tokens: [ethFlokiToken, ethTokenToken], owner: contract, api })
 }
 
 async function bsctvl(time, ethBlock, _b, {api}) {
-    return sumTokens2({ tokens: [flokiToken], owner: contract, api })
+    return sumTokens2({ tokens: [flokiToken, tokenToken], owner: contract, api })
   }
 
 module.exports = {
-  methodology: `We count the FLOKI on ${contract}`,
+  methodology: `We count the FLOKI & TOKEN on ${contract}`,
   ethereum: {
     tvl:  ()=>({}) ,
     staking: ethtvl
