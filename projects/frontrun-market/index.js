3ec9f7224876ef1480bdf5ae8bd047f9f27cefd1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-16 14:21:36 +0100

    bugfix

--- projects/frontrun-market/index.js
+++ projects/frontrun-market/index.js
@@ -3,21 +3,17 @@ const { sumTokensExport } = require('../helper/unwrapLPs');
 
 const targetAddress = '0x849F4081899305A1Fd24aAC84db5174EB60DC28e';
 const config = {
-  ethereum: [
-    {
-      tokens: [
-        ADDRESSES.ethereum.SDAI,
-        ADDRESSES.ethereum.USDC,
-        ADDRESSES.ethereum.USDT,
-        ADDRESSES.null,
-      ],
-    },
-  ],
-  blast: [
-    {
-      tokens: [ADDRESSES.blast.USDB, ADDRESSES.null,],
-    },
-  ],
+  ethereum: {
+    tokens: [
+      ADDRESSES.ethereum.SDAI,
+      ADDRESSES.ethereum.USDC,
+      ADDRESSES.ethereum.USDT,
+      ADDRESSES.null,
+    ],
+  },
+  blast: {
+    tokens: [ADDRESSES.blast.USDB, ADDRESSES.null,],
+  },
 };
 
 Object.keys(config).forEach((chain) => {
