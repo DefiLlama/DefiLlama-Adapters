c11dcc04c3732c6ec439f8d7fe62b20e0929c8ed
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-16 11:42:58 +0100

    Refactor token and owner configuration in index.js

--- projects/frontrun-market/index.js
+++ projects/frontrun-market/index.js
@@ -1,8 +1,6 @@
 const ADDRESSES = require('../helper/coreAssets.json');
-const { nullAddress } = require('../helper/unwrapLPs');
 const { sumTokensExport } = require('../helper/unwrapLPs');
 
-// The Ethereum address you're interested in
 const targetAddress = '0x849F4081899305A1Fd24aAC84db5174EB60DC28e';
 const config = {
   ethereum: [
@@ -11,27 +9,19 @@ const config = {
         ADDRESSES.ethereum.SDAI,
         ADDRESSES.ethereum.USDC,
         ADDRESSES.ethereum.USDT,
-        nullAddress,
+        ADDRESSES.null,
       ],
-      holders: [targetAddress],
     },
   ],
   blast: [
     {
-      tokens: [ADDRESSES.blast.USDB, nullAddress],
-      holders: [targetAddress],
+      tokens: [ADDRESSES.blast.USDB, ADDRESSES.null,],
     },
   ],
 };
 
-module.exports = {};
 Object.keys(config).forEach((chain) => {
-  const tokensAndOwners = config[chain]
-    .map(({ tokens, holders }) =>
-      holders.map((o) => tokens.map((t) => [t, o])).flat(),
-    )
-    .flat();
   module.exports[chain] = {
-    tvl: sumTokensExport({ tokensAndOwners }),
+    tvl: sumTokensExport({ owner: targetAddress, tokens: config[chain].tokens }),
   };
 });
