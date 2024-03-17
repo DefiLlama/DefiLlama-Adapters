e7408892cf819b421968cc7bbe5dce0f2e39efc3
danetteceola <danetteceola@gmail.com>
2024-03-17 18:43:38 +0800

    refactor using on chain data

--- projects/liquix/index.js
+++ projects/liquix/index.js
@@ -1,16 +1,19 @@
-const { get } = require("../helper/http");
+const { getLogs } = require('../helper/cache/getLogs')
+const { covalentGetTokens } = require('../helper/token')
+const { sleep } = require("../helper/utils");
+const { getUniqueAddresses } = require('../helper/utils');
 
-const endpoint = "https://api.soc.trading/web-service/soc/vault/v1/getSocAccountBalance";
+const config = {
+  arbitrum: {
+    network: "arbitrum",
+    factory: "0x37215Af590CED3f50a4e3Af8BE903E4307C71C1F",
+    helper: "0x6376f97fc9bc9a0ebd03fa89eF8a653A79b41e98",
+    fromBlock: 145653947,
+  },
+};
 
-async function getTvl() {
-  const { api } = arguments[3];
-  const { network } = config[api.chain];
-
-  const tvl = await get(endpoint, { network: [network] });
-  //const tvl = results.stats.totalValueManaged;
-
-  return { "usd-coin": tvl };
-}
+const blacklistedTokens = [
+]
 
 module.exports = {
   doublecounted: true,
@@ -18,10 +21,43 @@ module.exports = {
   timetravel: false,
 };
 
-const config = {
-  arbitrum: { network: "arbitrum" },
-};
+async function getTokens(api, owners) {
+  let tokens = []
+  for (let i = 0; i < owners.length; i++) {
+    const owner = owners[i];
+    let new_tokens = await covalentGetTokens(owner, api, { onlyWhitelisted: true, });
+    tokens.push(...new_tokens);
+    await sleep(300)
+  }
+  console.log(tokens)
+  tokens = getUniqueAddresses(tokens)
+  return tokens
+}
 
 Object.keys(config).forEach((chain) => {
-  module.exports[chain] = { tvl: getTvl };
+  const { factory, helper, fromBlock } = config[chain];
+  module.exports[chain] = {
+    tvl: async (_, _b, _cb, { api }) => {
+      const logs = await getLogs({
+        api,
+        target: factory,
+        topics: [
+          "0x14db5e0167c5e77f0a48cedd835dd30b2dcd630caff2e8e5d2411b892a094324",
+        ],
+        eventAbi:
+          "event VaultCreated(string name, address proxyAddress)",
+        onlyArgs: true,
+        fromBlock,
+      });
+      const vaults = logs.map(i => i.proxyAddress)
+      for (i = 0; i < vaults.length; i++) {
+        const balances = await api.call({ abi: 'function totalUnderlying(address vault_) external view returns ((address,uint256)[] memory)', target: helper, params: vaults[i] });
+        for (j = 0; j < balances.length; j++) {
+          const token0 = balances[j][0];
+          const balance = balances[j][1];
+          api.add(token0, balance)
+        }
+      }
+    }
+  }
 });
