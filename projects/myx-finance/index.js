7a2cb6115236f176d01773a4fefe453c9791f970
jiangmaniu <jiangmaniu@gmail.com>
2024-03-18 14:24:13 +0800

    feat: compute tvl onchain

--- projects/myx-finance/index.js
+++ projects/myx-finance/index.js
@@ -1,30 +1,55 @@
-const { post } = require("../helper/http");
+const { request, gql } = require("graphql-request");
 const { toUSDTBalances } = require("../helper/balances");
 const { default: BigNumber } = require("bignumber.js");
-const { ethers } = require("ethers");
+
+
+const config = {
+  arbitrum: { subgraphUrl: "https://subgraph-arb.myx.finance/subgraphs/name/myx-subgraph" },
+  linea: { subgraphUrl: "https://subgraph-linea.myx.finance/subgraphs/name/myx-subgraph" },
+};
 
 async function getTvl() {
   const { api } = arguments[3];
-  const { endpoint } = config[api.chain];
+  const { subgraphUrl } = config[api.chain];
 
-  const result = await post(endpoint, {"account":ethers.ZeroAddress,"pageNum":1,"pageSize": 10000});
+  const tokensIdResult = await request(subgraphUrl, gql`
+    query MyQuery {
+      tokens {
+        id
+      }
+    }
+  `)
 
-  const tvl =  result?.data?.records?.reduce((acc, item) => {
-    return acc.plus(item?.lpTvl || 0)
+
+  const tokensDataResult = await Promise.all(tokensIdResult.tokens.map(async (token) => {
+    const query = `
+      query MyQuery {
+        token(id: "${token.id}") {
+          decimals
+          id
+          name
+          symbol
+          totalValueLocked
+          totalValueLockedUSD
+          volume
+          volumeUSD
+        }
+      }
+    `
+
+    return await request(subgraphUrl, gql`${query}`)
+  }))
+
+  const tvl = tokensDataResult.reduce((acc, item) => {
+    return acc.plus(item.token.totalValueLockedUSD)
   }, new BigNumber(0))
 
-
-  return toUSDTBalances(tvl?.toFixed() || '0')
+  return toUSDTBalances(tvl.toFixed())
 }
 
 module.exports = {
   misrepresentedTokens: false,
   timetravel: true,
-};
-
-const config = {
-  arbitrum: { endpoint: "https://api-arb.myx.finance/lp/mlpBuy/stat/page" },
-  linea: { endpoint: "https://api-linea.myx.finance/lp/mlpBuy/stat/page" },
 };
 
 Object.keys(config).forEach((chain) => {
