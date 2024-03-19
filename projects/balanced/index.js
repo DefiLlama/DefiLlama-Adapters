8356ffd46022494df2bfbcd5457bb3f09530d344
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-19 13:04:25 +0100

    code refactor

--- projects/balanced/index.js
+++ projects/balanced/index.js
@@ -1,7 +1,6 @@
 const { sumTokens } = require('../helper/chain/icx');
 const { getConfig } = require('../helper/cache');
 const ADDRESSES = require('../helper/coreAssets.json');
-const { get } = require('../helper/http');
 
 const balancedDexContract = 'cxa0af3165c08318e988cb30993b3048335b94af6c';
 
@@ -9,11 +8,6 @@ async function tvl(_, _b, _cb, { api, }) {
   const pools = await getConfig('balancedDex', 'https://balanced.icon.community/api/v1/pools')
   const tokens = pools.map(pool => [pool.base_address, pool.quote_address]).flat().filter(i => i).map(i => i === 'ICX' ? ADDRESSES.null : i)
   return sumTokens({ api, tokens, owner: balancedDexContract })
-  /* const pools = await get('https://balanced.icon.community/api/v1/pools')
-  pools.forEach(({ quote_liquidity, base_liquidity }) => {
-    api.add('tether', base_liquidity, { skipChain: true })
-    api.add('tether', quote_liquidity, { skipChain: true })
-  }) */
 }
 
 module.exports = {
@@ -22,6 +16,6 @@ module.exports = {
     tvl
   },
   archway: {
-    tvl
+    tvl: () => ({})
   },
 };
