ad97f014218131175cdd2577596c39b9c3baff8d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-19 23:16:55 +0100

    code refactor

--- projects/vulcandex/index.js
+++ projects/vulcandex/index.js
@@ -1,11 +1,3 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports = {
-  misrepresentedTokens: true,
-  methodology:
-    "Factory address (0x293f45b6F9751316672da58AE87447d712AF85D7)",
-  polygon: {
-    tvl: getUniTVL({ factory: '0x293f45b6F9751316672da58AE87447d712AF85D7', chain: 'polygon', useDefaultCoreAssets: true }),
-  },
- 
-}; //For some reason, the built-in module functions are not receiving the PYR token and its pools. Manually adding the token?
+module.exports = uniTvlExport('polygon', '0x293f45b6F9751316672da58AE87447d712AF85D7')
