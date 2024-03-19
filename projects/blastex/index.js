84f97ba885ba1efc4e70a202dfea9badd295beba
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-19 11:26:17 +0100

    code refactor

--- projects/blastex/index.js
+++ projects/blastex/index.js
@@ -1,6 +1,13 @@
+const { sumTokensExport } = require('../helper/unknownTokens')
+
+const main = '0xdC0C0746CA0954E6C2284D1a97cC85474B051EbB'
+const token = '0x800ce05BaDE6B87E1552d0301Ea3393ccFf42F4A'
+const lps = ['0x974D36201171cA8D28Cc3F46972349d24Be3A303']
+
 module.exports = {
   blast: {
-    tvl: () => ({})
+    tvl: () => ({}),
+    pool2: sumTokensExport({ owner: main, tokens: lps, useDefaultCoreAssets: true }),
+    staking: sumTokensExport({ owner: main, tokens: [token], lps, useDefaultCoreAssets: true, })
   },
-  methodology: "all TVL is locked on the ThrusterSwap contracts",
 };
