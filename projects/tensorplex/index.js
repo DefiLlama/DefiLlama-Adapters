d5df93991a8ee502f2264109ad2a9326d244ca5b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-20 15:27:52 +0530

    Update index.js

--- projects/tensorplex/index.js
+++ projects/tensorplex/index.js
@@ -1,18 +1,13 @@
-const sdk = require("@defillama/sdk");
-const st_tao_contract_address = "0xB60acD2057067DC9ed8c083f5aa227a244044fD6";
+const st_tao = "0xB60acD2057067DC9ed8c083f5aa227a244044fD6"
 
-async function tvlEthereum(timestamp, ethBlock, chainBlocks, { api }) {
-  const total_supply = await sdk.api.erc20.totalSupply({
-    target: st_tao_contract_address,
-    ethBlock,
-  });
-  api.addCGToken("tensorplex-staked-tao", parseInt(total_supply.output) / 1e9);
-  return api.getBalances();
+async function tvl(_, _1, _2, { api }) {
+  const supply = await api.call({ abi: 'erc20:totalSupply', target: st_tao })
+  api.add(st_tao, supply)
 }
 
 module.exports = {
   methodology: "TVL counts tokens staked by the protocol.",
   ethereum: {
-    tvl: tvlEthereum,
+    tvl,
   },
-};
+}
