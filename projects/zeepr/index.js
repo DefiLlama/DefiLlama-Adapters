bec5b4bac8e3cfb457ec2e9841da4994482275b6
Joey <38412153+JoJo-Joker@users.noreply.github.com>
2024-03-22 00:05:54 +0800

    Update index.js
    Get the margins of tvl

--- projects/zeepr/index.js
+++ projects/zeepr/index.js
@@ -1,22 +1,23 @@
-const MINT_TOKEN_CONTRACT = '0x1c2D10633C78A47786759715d4618296D85D7cD1';
-const MINT_CLUB_BOND_CONTRACT = '0xd6eE9E6be560D8AEd6Caf1bAcF58603fA4b7e475';
+const { sumUnknownTokens } = require("../helper/unknownTokens");
 
-async function tvl(_, _1, _2, { api }) {
-  const collateralBalance = await api.call({
-    abi: 'address:totalMargins',
-    target: MINT_TOKEN_CONTRACT,
-    params: [MINT_CLUB_BOND_CONTRACT],
-  });
-
-  api.add(MINT_TOKEN_CONTRACT, collateralBalance)
+function vaultTvl(vaults) {
+  return async (_, _b, _cb, { api }) => {
+    const [tokens, bals] = await Promise.all([
+      api.multiCall({ abi: "address:totalMargins", calls: vaults }),
+    ]);
+    api.addTokens(tokens, bals);
+    return sumUnknownTokens({
+      api,
+      tokensAndOwners: ["0xA346963be84a215bce16FEd8Aac0e24eca74b25E"],
+      useDefaultCoreAssets: true,
+      lps: [],
+      resolveLP: true,
+    });
+  };
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  methodology: 'counts the number of margin in the contract address.',
-  start: 1679391877,
-  arbitrum: {
-    tvl,
-  }
-}; 
+  bsc: {
+    tvl: vaultTvl(["0x88A72cb97E89b9B4bBfAaE90F123f176C59F1Bbc"]),
+  },
+};
