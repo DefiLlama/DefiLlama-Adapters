f78931357f5bc909c2f43d0d5989a7afc4d056c8
0xngmi <0xngmi@protonmail.com>
2024-03-22 16:13:13 +0000

    add another wbone

--- projects/woofswap/index.js
+++ projects/woofswap/index.js
@@ -1,3 +1,3 @@
 const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('shibarium', '0xB9fbdFA27B7ba8BB2d4bB4aB399e4c55F0F7F83a', {  hasStablePools: true, })
+module.exports = uniTvlExport('shibarium', '0xB9fbdFA27B7ba8BB2d4bB4aB399e4c55F0F7F83a', {  hasStablePools: true })
