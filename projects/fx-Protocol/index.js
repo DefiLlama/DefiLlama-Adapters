9d15a94a394261c38d4c37ebcd9a36522ba1b7c4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-22 16:58:17 +0100

    Refactor tvl function and import sumERC4626VaultsExport

--- projects/fx-Protocol/index.js
+++ projects/fx-Protocol/index.js
@@ -1,24 +1,15 @@
-const ADDRESSES = require("../helper/coreAssets.json");
+const { sumERC4626VaultsExport} = require("../helper/erc4626");
 
-const stETHTreasury = "0x0e5CAA5c889Bdf053c9A76395f62267E653AFbb0";
-const stETH = ADDRESSES.ethereum.STETH;
-const fxUSD_wstETHTreasury = "0xED803540037B0ae069c93420F89Cd653B6e3Df1f";
-const fxUSD_sfrxETHTreasury = "0xcfEEfF214b256063110d3236ea12Db49d2dF2359";
-const wstETH = ADDRESSES.ethereum.WSTETH;
-const sfrxETH = ADDRESSES.ethereum.sfrxETH;
-
-async function tvl(api) {
-  const totalSupply = await api.call({ target: stETH, abi: "erc20:balanceOf", params: stETHTreasury, })
-  const fxUSDWstETHtotalSupply = await api.call({ target: fxUSD_wstETHTreasury, abi: "uint256:totalBaseToken", })
-  const fxUSDSfrxETHtotalSupply = await api.call({ target: fxUSD_sfrxETHTreasury, abi: "uint256:totalBaseToken", })
-  api.add(stETH, totalSupply)
-  api.add(wstETH, fxUSDWstETHtotalSupply)
-  api.add(sfrxETH, fxUSDSfrxETHtotalSupply)
-}
+const treasuries = [
+  "0x0e5CAA5c889Bdf053c9A76395f62267E653AFbb0",
+  "0xED803540037B0ae069c93420F89Cd653B6e3Df1f",
+  "0xcfEEfF214b256063110d3236ea12Db49d2dF2359",
+  "0x781BA968d5cc0b40EB592D5c8a9a3A4000063885",
+]
 
 module.exports = {
   doublecounted: true,
   ethereum: {
-    tvl,
+    tvl: sumERC4626VaultsExport({ vaults: treasuries, tokenAbi: 'baseToken', balanceAbi: 'totalBaseToken' }),
   },
 };
