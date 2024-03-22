ad778898a480d0eaf436476173e4863f75ac644e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-22 15:43:30 +0530

    Update index.js

--- projects/teahouse-v3/index.js
+++ projects/teahouse-v3/index.js
@@ -12,7 +12,7 @@ async function getVaultContractsAddress(chain) {
     // v3 vaults
     if (element.isDeFi == true && element.isActive == true) {
       if (
-        element.share.address === '0xf9b099f8EebCC675B63E4f0a4657c10F246af77A' &&
+        element.share.address.toLowerCase() === '0xf9b099f8EebCC675B63E4f0a4657c10F246af77A'.toLowerCase() &&
         element.chain === "arbitrum"
       ) return;
       if (element.chain === chain) plAddress.push(element.share.address);
