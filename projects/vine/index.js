54f76b4ee59c8d9307ec4f25810c5754d30b3370
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-22 14:00:33 +0530

    Update index.js

--- projects/vine/index.js
+++ projects/vine/index.js
@@ -1,19 +1,8 @@
-const sdk = require('@defillama/sdk')
-const { transformBalances, } = require('../helper/portedTokens')
-
-async function getTVL() {
-    let balances = {}
-    let balance = await sdk.api.eth.getBalance({ target: "0x1882560361578F2687ddfa2F4CEcca7ae2e614FD", chain: "sapphire" })
-
-    balances = {
-        'sapphire:0x8Bc2B030b299964eEfb5e1e0b36991352E56D2D3': balance.output, 
-    }
-    return transformBalances("sapphire", balances)
-}
+const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs')
 
 module.exports = {
-    start: 1706475600,
-    sapphire: {
-        tvl: getTVL,
-    },
+  start: 1706475600,
+  sapphire: {
+    tvl: sumTokensExport({ owner: '0x1882560361578F2687ddfa2F4CEcca7ae2e614FD', tokens: [nullAddress] }),
+  },
 }
