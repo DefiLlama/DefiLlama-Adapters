2d6031078b7258eec714ac1d3e7acfe23d8b571a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-25 13:31:51 +0100

    Refactor PyreSwap index.js to use forEach loop for chain configurations

--- projects/PyreSwap/index.js
+++ projects/PyreSwap/index.js
@@ -1,26 +1,11 @@
 const { getUniTVL } = require('../helper/unknownTokens');
-const factory = "0x045d720873f0260e23da812501a7c5930e510aa4"
+
 module.exports = {
-  misrepresentedTokens: true,
-  fantom: {
-    tvl: getUniTVL({
-      chain: 'fantom',
-      factory: factory,
-      useDefaultCoreAssets: true,
-    }),
-  },
-  bsc: {
-    tvl: getUniTVL({
-      chain: 'bsc', 
-      factory: factory, 
-      useDefaultCoreAssets: true,
-    }),
-  },
-  avax: {
-    tvl: getUniTVL({
-      chain: 'avax', 
-      factory: factory, 
-      useDefaultCoreAssets: true,
-    }),
-  },
+  misrepresentedTokens: true, 
 };
+
+['fantom', 'bsc', 'avax'].forEach(chain => {
+  module.exports[chain] = {
+    tvl: getUniTVL({ factory: '0x045d720873f0260e23da812501a7c5930e510aa4', useDefaultCoreAssets: true, fetchBalances: true, })
+  }
+})
