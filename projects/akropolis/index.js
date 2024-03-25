61ab8a468b32b2868beed91f835141829e43099c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-25 13:41:58 +0100

    Refactor AKRO token staking in index.js

--- projects/akropolis/index.js
+++ projects/akropolis/index.js
@@ -1,16 +1,5 @@
-const { sumTokensExport } = require("../helper/unwrapLPs");
-
-const tokens = require("../helper/coreAssets.json");
-
-const ethereumContract = [
-  "0x3501Ec11d205fa249f2C42f5470e137b529b35D0",
-];
-
-const AKRO = [
-  tokens.null,
-  "0x8Ab7404063Ec4DBcfd4598215992DC3F8EC853d7"
-];
+const { staking } = require('../helper/staking')
 
 module.exports = {
-  ethereum: { tvl: sumTokensExport({ owners: ethereumContract, tokens: AKRO }) }
+  ethereum: { tvl: () => ({}), staking: staking('0x3501Ec11d205fa249f2C42f5470e137b529b35D0', '0x8Ab7404063Ec4DBcfd4598215992DC3F8EC853d7') }
 };
