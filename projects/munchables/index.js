2f072fd21d557a7c6644a4e90d5057b5d11612b7
nelsonmurua913 <sr.muruanelson@gmail.com>
2024-03-25 18:04:50 -0300

    feat: add new project for getting munchables TVL

---
+++ projects/munchables/index.js
@@ -0,0 +1,11 @@
+const ADDRESSES = require("../helper/coreAssets.json");
+const { sumTokensExport } = require("../helper/unwrapLPs");
+
+module.exports = {
+  blast: {
+    tvl: sumTokensExport({
+      owner: "0x29958E8E4d8a9899CF1a0aba5883DBc7699a5E1F",
+      tokens: [ADDRESSES.null, ADDRESSES.blast.USDB, ADDRESSES.blast.WETH],
+    }),
+  },
+};
