46f9cd4f14cf5f2b12410fba65f39b34a312c0e3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-25 22:59:10 +0100

    code refactor

--- projects/aloe/index.js
+++ projects/aloe/index.js
@@ -1,66 +1,37 @@
-const { getLogs } = require("../helper/cache/getLogs");
+const { getLogs } = require("../helper/cache/getLogs")
+const { getUniqueAddresses } = require("../helper/utils")
 
-const factories = {
-  ethereum: {
-    address: "0x000000009efdB26b970bCc0085E126C9dfc16ee8",
-    fromBlock: 18782116,
-  },
-  optimism: {
-    address: "0x000000009efdB26b970bCc0085E126C9dfc16ee8",
-    fromBlock: 113464669,
-  },
-  base: {
-    address: "0x000000009efdB26b970bCc0085E126C9dfc16ee8",
-    fromBlock: 7869252,
-  },
-  arbitrum: {
-    address: "0x000000009efdB26b970bCc0085E126C9dfc16ee8",
-    fromBlock: 159919891,
-  },
+const config = {
+  ethereum: { fromBlock: 18782116, },
+  optimism: { fromBlock: 113464669, },
+  base: { fromBlock: 7869252, },
+  arbitrum: { fromBlock: 159919891, },
 };
 
 async function getVaults(api) {
-  const { address: factory, fromBlock } = factories[api.chain];
+  const { factory = '0x000000009efdB26b970bCc0085E126C9dfc16ee8', fromBlock } = config[api.chain];
   const createMarketEvents = await getLogs({
     api,
     fromBlock,
     target: factory,
-    topic: "CreateMarket(address,address,address)",
-    eventAbi:
-      "event CreateMarket(address indexed pool, address lender0, address lender1)",
+    eventAbi: "event CreateMarket(address indexed pool, address lender0, address lender1)",
     onlyArgs: true,
   });
-  return createMarketEvents.flatMap((eventArgs) => eventArgs.slice(1));
+  const res = createMarketEvents.flatMap(i => [i.lender0, i.lender1])
+  return getUniqueAddresses(res)
 }
 
-async function getTvl(api) {
+async function tvl(api) {
   const vaults = await getVaults(api);
   return api.erc4626Sum({ calls: vaults, isOG4626: true });
 }
 
-async function getBorrows(api) {
-  const vaults = await getVaults(api);
-  const tokens = await api.multiCall({ calls: vaults, abi: "address:asset" });
-  const stats = await api.multiCall({
-    calls: vaults,
-    abi: "function stats() view returns (uint72, uint256, uint256, uint256)",
-  });
-  api.addTokens(
-    tokens,
-    stats.map((x) => x[2])
-  );
-}
-
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  doublecounted: false,
+  doublecounted: true,
   methodology:
-    "Sums up deposits and borrows across Aloe's ERC4626 lending vaults to get TVL and Borrowed amounts, respectively. Does not include collateral value.",
-  ...Object.fromEntries(
-    Object.keys(factories).map((chain) => [
-      chain,
-      { tvl: getTvl, borrowed: getBorrows },
-    ])
-  ),
+    "Sums up deposits and borrows across Aloe's ERC4626 lending vaults to get TVL. Does not include collateral value.",
 };
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = { tvl }
+})
