4a1fe0720d21ac19b35d4826ad332ad0748bd3c4
MiguelGGMM <104460442+MiguelGGMM@users.noreply.github.com>
2024-03-26 23:54:53 +0100

    Create index.js

---
+++ projects/2FAI/index.js
@@ -0,0 +1,19 @@
+const { stakings } = require("./helper/staking");
+
+const revShareContract = [
+    "0x394a95251135274165837be000d771D5DCb08e15",
+];
+
+const tokenVestingUNCX = [
+    "0xA82685520C463A752d5319E6616E4e5fd0215e33"
+]
+
+module.exports = {
+  misrepresentedTokens: true,
+  base: {
+    revSharePool: stakings(revShareContract, twoFAI, 'base'),
+    locked: stakings(tokenVestingUNCX, twoFAI, 'base'),
+    tvl: async () => ({}),
+  },
+  methodology: "Counts 2FAI on the rev share pool and locked by team",
+}
