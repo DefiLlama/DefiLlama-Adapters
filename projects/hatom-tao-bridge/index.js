28ea30a7f4e555fb7cda1c0e5689eeb66d6113da
carlosalv098 <76534971+carlosalv098@users.noreply.github.com>
2024-03-27 08:34:23 -0600

    add Hatom WTAO Bridge SDK Adapter

---
+++ projects/hatom-tao-bridge/index.js
@@ -0,0 +1,21 @@
+const { call } = require("../helper/chain/elrond");
+
+const wrappedTaoContractAddress =
+    "erd1qqqqqqqqqqqqqpgqajq8kvm8qq045s2fvj4sa7dph8kpqx9d78ssxjzjdh";
+const wtao = "WTAO-4f5363";
+
+const tvl = async () => {
+    const totalSupply = await call({
+        target: wrappedTaoContractAddress,
+        abi: "getTokenSupply",
+        responseTypes: ["number"],
+    });
+    api.addTokens([wtao], [totalSupply.toString()]);
+};
+
+module.exports = {
+    timetravel: false,
+    elrond: {
+        tvl,
+    },
+};
