b855d5f0482761234b74b8b32fdbcf3c025d004c
carlosalv098 <76534971+carlosalv098@users.noreply.github.com>
2024-03-27 08:55:13 -0600

    quick fix

--- projects/hatom-tao-bridge/index.js
+++ projects/hatom-tao-bridge/index.js
@@ -4,7 +4,7 @@ const wrappedTaoContractAddress =
     "erd1qqqqqqqqqqqqqpgqajq8kvm8qq045s2fvj4sa7dph8kpqx9d78ssxjzjdh";
 const wtao = "WTAO-4f5363";
 
-const tvl = async () => {
+const tvl = async (api) => {
     const totalSupply = await call({
         target: wrappedTaoContractAddress,
         abi: "getTokenSupply",
