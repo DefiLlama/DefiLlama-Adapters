e686b2045bd2a390c978c11d6feeb32df60613cd
ChazSchmidt <schmidt.864@osu.edu>
2024-03-28 10:51:21 -0400

    added BCT TVL to KlimaDAO

--- projects/klima-dao/index.js
+++ projects/klima-dao/index.js
@@ -1,3 +1,5 @@
+const sdk = require("@defillama/sdk");
+const { default: BigNumber } = require("bignumber.js");
 const ADDRESSES = require('../helper/coreAssets.json')
 const { staking } = require('../helper/staking')
 const { sumTokensAndLPsSharedOwners } = require('../helper/unwrapLPs')
@@ -5,6 +7,7 @@ const { sumTokensAndLPsSharedOwners } = require('../helper/unwrapLPs')
 
 const treasuryAddress = "0x7Dd4f0B986F032A44F913BF92c9e8b7c17D77aD7"
 const daoWallet = "0x65A5076C0BA74e5f3e069995dc3DAB9D197d995c"
+const bctAddress = "0x2F800Db0fdb5223b3C3f354886d907A671414A7F"
 
 async function treasuryBalances(time, ethBlock, chainBlocks) {
   const balances = {}
@@ -17,7 +20,7 @@ async function treasuryBalances(time, ethBlock, chainBlocks) {
     [ADDRESSES.polygon.USDT, false], // USDT
 
     // Carbon
-    ["0x2f800db0fdb5223b3c3f354886d907a671414a7f", false], // BCT
+    [bctAddress, false], // BCT
     ["0xD838290e877E0188a4A44700463419ED96c16107", false], // NCT
     ["0xAa7DbD1598251f856C12f63557A4C4397c253Cea", false], // MCO2
     ["0x03E3369af9390493CB7CC599Cd5233D50e674Da4", false], // MOSS
@@ -34,14 +37,40 @@ async function treasuryBalances(time, ethBlock, chainBlocks) {
     ["0x4D2263FF85e334C1f1d04C6262F6c2580335a93C", true], // KLIMA-CCO2 Sushi LP
     ["0x5786b267d35F9D011c4750e0B0bA584E1fDbeAD1", true], // KLIMA-USDC.e Sushi LP
     ["0x64a3b8cA5A7e406A78e660AE10c7563D9153a739", true], // KLIMA-MCO2 Quick LP
-    ["0xbA3B82585355CcD047A96b86aaBe256d181c23D2", true], // KLIMA-USDC UniV3 LP
+
   ], [treasuryAddress, daoWallet], chainBlocks.polygon, "polygon", transform)
   return balances
 }
 
+const getTVL = (chain) => {
+  return async (timestamp, block, chainBlocks) => {
+    const supplyCall = { target: bctAddress };
+
+    const supply = (
+      await sdk.api.abi.call({
+        abi: 'erc20:totalSupply',
+        target: supplyCall.target,
+        chain,
+        block: chainBlocks[chain],
+      })
+    ).output;
+
+    const bct = BigNumber(supply);
+
+    return {
+      'toucan-protocol-base-carbon-tonne': bct.div(10**18).toFixed(0),
+    };
+  };
+};
+
 module.exports = {
+  methodology: "TVL counts the TCO2 tokens within the BCT pool. Additionally, tokens held by the DAO and within the treasury as well as the staking contracts are counted toward treasury and staking respectively.",
   polygon: {
-    tvl: () => 0,
-    staking: staking("0x25d28a24Ceb6F81015bB0b2007D795ACAc411b4d", "0x4e78011ce80ee02d2c3e649fb657e45898257815", "polygon")
-  }
-}
+    tvl: getTVL("polygon"),
+    staking: staking("0x25d28a24Ceb6F81015bB0b2007D795ACAc411b4d", "0x4e78011ce80ee02d2c3e649fb657e45898257815", "polygon"),
+    treasury: treasuryBalances
+  },
+  hallmarks: [
+    [1709269200, "BCT administrative control transferred to KlimaDAO"],
+  ]
+};
