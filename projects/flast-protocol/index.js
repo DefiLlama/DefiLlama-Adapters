dfe5354042af48139027576f722cda966870b021
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-28 10:09:38 +0100

    minor fix

--- projects/flast-protocol/index.js
+++ projects/flast-protocol/index.js
@@ -1,20 +1,8 @@
-const ADDRESSES = require("../helper/coreAssets.json");
-const sdk = require("@defillama/sdk");
-const CONTRACT_ADDRESS = "0x7474796140775d8719584AA9923102ad7bf56490";
+const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs")
 
-async function tvl() {
-  const eth = await sdk.api.eth.getBalance({
-    target: CONTRACT_ADDRESS,
-    chain: "blast"
-  });
-
-  return { [ADDRESSES.null]: eth.output };
-}
 module.exports = {
-  timetravel: true,
-  methodology:
-    "Value of user deposited ETH on Flast Protocol is considered as TVL",
+  methodology: "Value of user deposited ETH on Flast Protocol is considered as TVL",
   blast: {
-    tvl,
+    tvl: sumTokensExport({ owner: '0x7474796140775d8719584AA9923102ad7bf56490', tokens: [nullAddress] }),
   },
-};
+}
