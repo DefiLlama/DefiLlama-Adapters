29464da80d4550521e5e15fdb366b5e1b735d4b5
Vladislav Petushkov <1732750+vladislavpetushkov@users.noreply.github.com>
2024-03-29 18:53:13 +0400

    add staking amount in governance to adapter (#9613)
    * add staking amount in governance to adapter
    
    * add on chain code as backup
    
    ---------
    
    Co-authored-by: Vladislav Petushkov <vpetushkov@web3tech.ru>
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/swop/index.js
+++ projects/swop/index.js
@@ -1,17 +1,33 @@
+// const { sumTokens, call } = require('../helper/chain/waves');
+// const { getConfig } = require("../helper/cache");
 const { get } = require("../helper/http");
 const { toUSDTBalances } = require("../helper/balances");
 
+
 const swopfiBackendEndpoint = "https://backend.swop.fi";
 
-const getSwopFiTVL = async () => {
+const getSwopFiTVL = async (_, _1, _2, { api }) => {
   const poolsStats = await get(`${swopfiBackendEndpoint}/pools`);
   return toUSDTBalances(poolsStats.overall.liquidity);
+  /* const { pools } = await getConfig('swop', `${swopfiBackendEndpoint}/pools`)
+  const owners = pools.map(i => i.id)
+  console.log(owners.length)
+  for (const pool of pools) {
+    await sumTokens({ owners: [pool.id], api, includeWaves: true, blacklistedTokens: ['Ehie5xYpeN8op1Cctc6aGUrqx8jq3jtf1DSjXDbfm7aT'] })
+  } */
 }
+
 
 module.exports = {
   timetravel: false, // Waves blockchain,
   methodology: "Counts the tokens locked on AMM pools",
   waves: {
     tvl: getSwopFiTVL,
+    staking: async () => {
+      const stakingStats = await get(`${swopfiBackendEndpoint}/staking`);
+      return toUSDTBalances(stakingStats.swop.totalSwopUsdt);
+      /* const res = await call({target:'3PLHVWCqA9DJPDbadUofTohnCULLauiDWhS', key: 'total_GSwop_amount'})
+      return { swop: res / 1e8 } */
+    }
   }
 };
