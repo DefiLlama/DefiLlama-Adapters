e978bee80be0e36c147160b91b43538e149df9e3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-29 17:38:17 +0100

    code refactor

--- projects/hatom-tao-bridge/index.js
+++ projects/hatom-tao-bridge/index.js
@@ -1,25 +1,11 @@
-const { call } = require("../helper/chain/elrond");
-
-const wrappedTaoContractAddress =
-    "erd1qqqqqqqqqqqqqpgqajq8kvm8qq045s2fvj4sa7dph8kpqx9d78ssxjzjdh";
-const wtao = "WTAO-4f5363";
+const { post } = require('../helper/http')
 
 const TREASURY_ADDRESS = "5HZAAREPzwBc4EPWWeTHA2WRcJoCgy4UBk8mwYFWR5BTCNcT";
-const tao = "TAO";
 
-const TAO_STATS_SUBQUERY =
-    "https://api.subquery.network/sq/TaoStats/bittensor-indexer";
+const TAO_STATS_SUBQUERY = "https://api.subquery.network/sq/TaoStats/bittensor-indexer";
 
-const tvl = async (api) => {
-    const totalSupply = await call({
-        target: wrappedTaoContractAddress,
-        abi: "getTokenSupply",
-        responseTypes: ["number"],
-    });
-    api.addTokens([wtao], [totalSupply.toString()]);
-};
 const taoQuery = async () => {
-    const query = `{
+  const query = `{
         query{
             account(id: "${TREASURY_ADDRESS}"){
                 id
@@ -32,34 +18,22 @@ const taoQuery = async () => {
         }
     }`;
 
-    const variables = {};
+  const variables = {};
 
-    const response = await fetch(TAO_STATS_SUBQUERY, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            query,
-            variables,
-        }),
-    });
-
-    return await response.json();
+  return post(TAO_STATS_SUBQUERY, {
+    query,
+    variables,
+  });
 };
 
 module.exports = {
-    timetravel: false,
-    bittensor: {
-        tvl: async (api) => {
-            return taoQuery().then((walletBalanceResponse) => {
-                const totalBalance =
-                    walletBalanceResponse.data.account.balanceTotal;
-                return api.addTokens([tao], [totalBalance]);
-            });
-        },
+  timetravel: false,
+  bittensor: {
+    tvl: async () => {
+      const { data: { query: { account: { balanceTotal } } } } = await taoQuery();
+      return {
+        bittensor: balanceTotal / 1e9,
+      };
     },
-    elrond: {
-        tvl,
-    },
-};
+  },
+}
