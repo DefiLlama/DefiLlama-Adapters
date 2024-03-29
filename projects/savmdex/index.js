16d12619ec807fb556c35ced1a45828ccf779292
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-29 11:37:38 +0100

    Update package-lock.json and tokenMapping.js

--- projects/savmdex/index.js
+++ projects/savmdex/index.js
@@ -2,7 +2,7 @@ const { getUniTVL } = require("../helper/unknownTokens");
 module.exports = {
   misrepresentedTokens: true,
 
-  BEVM: {
+  bevm: {
     tvl: getUniTVL({
       factory: "0xc008f29AaddA007b123919a5a0561c1B2E37864A",
       useDefaultCoreAssets: true,
