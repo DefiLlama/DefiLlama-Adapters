f8737a2567768c6772cedd45fa60898bd05034d5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-03-29 11:39:28 +0100

    minor fix

--- projects/savmdex/index.js
+++ projects/savmdex/index.js
@@ -1,11 +1,2 @@
-const { getUniTVL } = require("../helper/unknownTokens");
-module.exports = {
-  misrepresentedTokens: true,
-
-  bevm: {
-    tvl: getUniTVL({
-      factory: "0xc008f29AaddA007b123919a5a0561c1B2E37864A",
-      useDefaultCoreAssets: true,
-    }),
-  },
-};
+const { uniTvlExport } = require("../helper/unknownTokens");
+module.exports = uniTvlExport('bevm', '0xc008f29AaddA007b123919a5a0561c1B2E37864A')
