167df8d4af86ad148a4669a6657cf97a707e0039
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-01 16:10:14 +0200

    code refactor

--- projects/decubate/index.js
+++ projects/decubate/index.js
@@ -1,31 +1,14 @@
-const sdk = require("@defillama/sdk");
+const { staking } = require('../helper/staking')
 
-const DCBToken = "0xEAc9873291dDAcA754EA5642114151f3035c67A2";
+const DCBToken = "0xEAc9873291dDAcA754EA5642114151f3035c67A2"
 const stakingPools = [
-    "0xD1748192aE1dB982be2FB8C3e6d893C75330884a", // Legacy staking pool contract
-    "0xe740758a8cd372c836857defe8011e4e80e48723"  // New staking pools contract
-];
-
-async function staking(timestamp, block, chainBlocks) {
-    let balances = {};
-
-    for (let pool of stakingPools) {
-        let {output: balance} = await sdk.api.erc20.balanceOf({
-            target: DCBToken,
-            owner: pool,
-            block: chainBlocks.bsc,
-            chain: "bsc"
-        });
-
-        sdk.util.sumSingleBalance(balances, `bsc:${DCBToken}`, balance);
-    }
-
-    return balances;
-}
+  "0xD1748192aE1dB982be2FB8C3e6d893C75330884a", // Legacy staking pool contract
+  "0xe740758a8cd372c836857defe8011e4e80e48723"  // New staking pools contract
+]
 
 module.exports = {
-    bsc: {
-        tvl: async () => ({}),
-        staking
-    },
-};
+  bsc: {
+    tvl: async () => ({}),
+    staking: staking(stakingPools, DCBToken,)
+  },
+}
