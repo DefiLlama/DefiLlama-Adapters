2b3a7fb9e4985f3a8245841b6d185473f9134550
Joey <38412153+JoJo-Joker@users.noreply.github.com>
2024-04-01 19:52:58 +0800

    Update index.js

--- projects/zeepr/index.js
+++ projects/zeepr/index.js
@@ -1,19 +1,29 @@
-const { sumTokensExport } = require("../helper/unwrapLPs");
-const ADDRESSES = require("../helper/coreAssets.json");
+// const { sumTokensExport } = require("../helper/unwrapLPs");
+// const ADDRESSES = require("../helper/coreAssets.json");
 
-const config = {
+// const config = {
+//   arbitrum: {
+//     ownerTokens: [
+//       [
+//         ["0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"],
+//         "0xbB0390Cf2586e9b0A4FAADF720aE188D140E9fd5",
+//       ],
+//     ],
+//   },
+// };
+
+// Object.keys(config).forEach((chain) => {
+//   module.exports[chain] = {
+//     tvl: sumTokensExport(config[chain]),
+//   };
+// });
+
+const { staking } = require("./helper/staking");
+module.exports = {
+  // polygon: {
+  //   staking: staking('0xfb06a737f549eb2512eb6082a808fc7f16c0819d'),
+  // },
   arbitrum: {
-    ownerTokens: [
-      [
-        ["0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"],
-        "0xbB0390Cf2586e9b0A4FAADF720aE188D140E9fd5",
-      ],
-    ],
+    staking: staking('0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13'),
   },
 };
-
-Object.keys(config).forEach((chain) => {
-  module.exports[chain] = {
-    tvl: sumTokensExport(config[chain]),
-  };
-});
