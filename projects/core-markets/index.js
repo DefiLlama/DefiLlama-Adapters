19d4392964087f588ee761184d0e806ae716ba78
Lemon <lemonpac@intentx.io>
2024-04-01 18:51:37 +0200

    fix: Core-Markets adapter

--- projects/core-markets/index.js
+++ projects/core-markets/index.js
@@ -1,18 +1,13 @@
 const { request, gql } = require("graphql-request");
 
-const graphUrl =
-  "https://api.studio.thegraph.com/query/62472/core-analytics-082/version/latest";
+const graphUrl = "https://api.studio.thegraph.com/query/62472/core-analytics-082/version/latest";
 
-const BETA_START = "236678";
+const BETA_START = 236678;
 
 const query = gql`
   query stats($from: String!, $to: String!) {
     dailyHistories(
-      where: {
-        timestamp_gte: $from
-        timestamp_lte: $to
-        accountSource: "0xd6ee1fd75d11989e57B57AA6Fd75f558fBf02a5e"
-      }
+      where: { timestamp_gte: $from, timestamp_lte: $to, accountSource: "0xd6ee1fd75d11989e57B57AA6Fd75f558fBf02a5e" }
     ) {
       timestamp
       platformFee
@@ -27,26 +22,22 @@ const query = gql`
 async function getTVL(toTimestamp) {
   const { dailyHistories } = await request(graphUrl, query, {
     from: BETA_START.toString(),
-    to: toTimestamp.toString(),
+    to: toTimestamp.timestamp.toString(),
   });
 
-  const total = dailyHistories.reduce(
-    (acc, cur) => acc + (Number(cur.deposit) - Number(cur.withdraw)),
-    0
-  );
+  const total = dailyHistories.reduce((acc, cur) => acc + (Number(cur.deposit) - Number(cur.withdraw)), 0);
 
   return {
-    "base:0x4300000000000000000000000000000000000003": new BigNumber(total),
+    "blast:0x4300000000000000000000000000000000000003": total,
   };
 }
 
 module.exports = {
   timetravel: false,
-  start: 236678,
-  base: {
+  start: BETA_START,
+  blast: {
     tvl: async (timestamp) => {
       return getTVL(timestamp);
     },
   },
 };
-
