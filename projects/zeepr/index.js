77d11445099a37e90389ed6c33bd729f00cdd01e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-02 10:04:43 +0200

    Update index.js

--- projects/zeepr/index.js
+++ projects/zeepr/index.js
@@ -1,8 +1,8 @@
-const { staking } = require("./helper/staking");
+const { staking } = require("../helper/staking");
 
 module.exports = {
   arbitrum: {
     tvl: () => ({}),
-    staking: staking(      "0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13",      "0xf75fb73fd1bccd23ce2389169674ce375b43b7a6"    ),
+    staking: staking("0xf75fb73fd1bccd23ce2389169674ce375b43b7a6", "0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"),
   },
-};
+}
