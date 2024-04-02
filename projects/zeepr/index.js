ecb58f4ddfe01c3a8fa2008129dd5a2dbc17dbe8
Joey <38412153+JoJo-Joker@users.noreply.github.com>
2024-04-02 15:41:33 +0800

    Update index.js

--- projects/zeepr/index.js
+++ projects/zeepr/index.js
@@ -2,7 +2,9 @@ const { staking } = require("./helper/staking");
 
 module.exports = {
   arbitrum: {
-    tvl: 0,
-    staking: staking("0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"),
+    staking: staking(
+      "0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13",
+      "0xf75fb73fd1bccd23ce2389169674ce375b43b7a6"
+    ),
   },
 };
