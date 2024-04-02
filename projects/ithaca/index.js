88d92009103f0a42dae32dc34c8550a0d702bd9b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-02 13:33:23 +0200

    minor change

--- projects/ithaca/index.js
+++ projects/ithaca/index.js
@@ -1,8 +1,9 @@
+const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport } = require("../helper/unwrapLPs");
 
 const ITHACA_FUNDLOCK_CONTRACT = '0x4a20d341315b8ead4e5ebecc65d95080a47a7316';
-const WETH_CONTRACT = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1';
-const USDC_CONTRACT = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831';
+const WETH_CONTRACT = ADDRESSES.arbitrum.WETH;
+const USDC_CONTRACT = ADDRESSES.arbitrum.USDC_CIRCLE;
 
 module.exports = {
   timetravel: true,
