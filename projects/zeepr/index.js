fc790e19d605f6bbf6332c1cbbfa921fb439f499
Joey <38412153+JoJo-Joker@users.noreply.github.com>
2024-04-03 22:11:02 +0800

    Update index.js

--- projects/zeepr/index.js
+++ projects/zeepr/index.js
@@ -1,8 +1,28 @@
-const { staking } = require("../helper/staking");
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const ADDRESSES = require("../helper/coreAssets.json");
 
-module.exports = {
+const config = {
   arbitrum: {
-    tvl: () => ({}),
-    staking: staking("0xf75fb73fd1bccd23ce2389169674ce375b43b7a6", "0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"),
+    ownerTokens: [
+      [
+        ["0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"],
+        "0xf75fb73fd1bccd23ce2389169674ce375b43b7a6",
+      ],
+    ],
   },
-}
+};
+
+Object.keys(config).forEach((chain) => {
+  module.exports[chain] = {
+    tvl: sumTokensExport(config[chain]),
+  };
+});
+
+// const { staking } = require("../helper/staking");
+
+// module.exports = {
+//   arbitrum: {
+//     tvl: () => ({}),
+//     staking: staking("0xf75fb73fd1bccd23ce2389169674ce375b43b7a6", "0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"),
+//   },
+// }
