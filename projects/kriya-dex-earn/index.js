e58699494f8b4972b873b693757dbce6e2214f63
Knight-101 <sanskarmohapatra@gmail.com>
2024-04-04 13:30:41 +0530

    added kriya vaults tvl

---
+++ projects/kriya-dex-earn/index.js
@@ -0,0 +1,27 @@
+const sui = require("../helper/chain/sui");
+const { fetchURL } = require("../helper/utils");
+
+const vaultUrl =
+  "https://88ob93rfva.execute-api.ap-southeast-1.amazonaws.com/release/vaults";
+
+async function vaultTVL() {
+  let tvl = 0;
+  const vaults = await fetchURL(vaultUrl);
+  for (const vault of vaults) {
+    tvl += Number(vault?.tvl);
+  }
+
+  return {
+    tvl,
+  };
+}
+
+module.exports = {
+  timetravel: true,
+  misrepresentedTokens: false,
+  methodology:
+    "Collets all the TVL from the KriyaDEX vaults. The TVL is denominated in USD.",
+  sui: {
+    tvl: vaultTVL,
+  },
+};
