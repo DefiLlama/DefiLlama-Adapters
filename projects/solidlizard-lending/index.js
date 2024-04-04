9517d5755f7a9fe919b42b893d6a3abf787cf538
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-04 21:51:11 +0200

    Update index.js

--- projects/solidlizard-lending/index.js
+++ projects/solidlizard-lending/index.js
@@ -8,6 +8,6 @@ module.exports = {
   arbitrum: {
     ...compoundExports2({ comptroller: '0x9FfBdfcc508e76ee2b719eF6218879E938eF056C'}),
     staking: staking( "0x08153c4C19Cb438A3bdC6303aF962a30E9f5e0B1",  "0xe6AF844d5740B6B297B6Dd7Fb2ce299Ee9E3d16F"),
-    pool2: staking( "0x8082F587Ff2B24dadB2220026F4FCa9323Ed8080",  "0xB70005C4980d78e8bE47D4C9ccCd7300D6a02501"),
+    pool2: pool2( "0x8082F587Ff2B24dadB2220026F4FCa9323Ed8080",  "0xB70005C4980d78e8bE47D4C9ccCd7300D6a02501"),
   },
 };
