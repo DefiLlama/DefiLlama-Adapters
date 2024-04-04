145d80d655fe32bb0e90c4b1c58d7a5b37e7276c
CristianRicharte6 <cristianricharte6@gmail.com>
2024-04-04 12:46:08 +0200

    Feat: Linea support

--- projects/3a-dao/index.js
+++ projects/3a-dao/index.js
@@ -1,13 +1,27 @@
-const { getLogs } = require('../helper/cache/getLogs')
+const {
+  getLogs
+} = require('../helper/cache/getLogs')
 
 const config = {
-  polygon: { factory: '0x4760847023fa0833221ae76E01Db1E483A5D20e0', fromBlock: 49852705 },
+  polygon: {
+    factory: '0x4760847023fa0833221ae76E01Db1E483A5D20e0',
+    fromBlock: 49852705
+  },
+  linea: {
+    factory: '0x65c6FD9B3a2A892096881e28f07c732ed128893E',
+    fromBlock: 3045954
+  },
 }
 
 Object.keys(config).forEach(chain => {
-  const { factory, fromBlock, } = config[chain]
+  const {
+    factory,
+    fromBlock,
+  } = config[chain]
   module.exports[chain] = {
-    tvl: async (_, _b, _cb, { api, }) => {
+    tvl: async (_, _b, _cb, {
+      api,
+    }) => {
       const logs = await getLogs({
         api,
         target: factory,
@@ -16,9 +30,14 @@ Object.keys(config).forEach(chain => {
         fromBlock,
       })
       const vaults = logs.map(log => log.vault)
-      const tokens = await api.multiCall({  abi: 'address[]:collaterals', calls: vaults})
+      const tokens = await api.multiCall({
+        abi: 'address[]:collaterals',
+        calls: vaults
+      })
       const ownerTokens = tokens.map((token, i) => [token, vaults[i]])
-      return api.sumTokens({ ownerTokens })
+      return api.sumTokens({
+        ownerTokens
+      })
     }
   }
 })
