182fc4f0d29c2e75434e97c11127eaebf275c036
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-04 16:34:47 +0200

    code refactor

--- projects/fathom-lending/index.js
+++ projects/fathom-lending/index.js
@@ -1,7 +1,10 @@
-const { fathomExports } = require('./fathom');
-const PoolDataProvider_XDC = "0x7fa488a5C88E9E35B0B86127Ec76B0c1F0933191";
 const PoolAddressesProviderRegistry = "0xDAb3B99eb3569466750c436d6F4c99d57850Cc89"
+const { aaveExports } = require('../helper/aave')
 
 module.exports = {
-    xdc: fathomExports('xdc', PoolAddressesProviderRegistry, undefined, [PoolDataProvider_XDC], { v3: true }),
+  xdc: aaveExports(undefined, PoolAddressesProviderRegistry, undefined, ['0x7fa488a5C88E9E35B0B86127Ec76B0c1F0933191'], {
+    v3: true, abis: {
+      getAllATokens: "function getAllFmTokens() view returns (tuple(string symbol, address tokenAddress)[])",
+    }
+  }),
 };
