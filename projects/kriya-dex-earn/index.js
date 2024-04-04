6b441ac7c8e4ebb4a54f372c41956bcce8694d8b
Knight-101 <sanskarmohapatra@gmail.com>
2024-04-04 18:38:15 +0530

    tvl func fix

--- projects/kriya-dex-earn/index.js
+++ projects/kriya-dex-earn/index.js
@@ -5,7 +5,7 @@ const sui = require("../helper/chain/sui");
 const vaultUrl =
   "https://88ob93rfva.execute-api.ap-southeast-1.amazonaws.com/release/vaults";
 
-async function vaultTVL() {
+async function vaultTVL(api) {
   const vaults = (await fetchURL(vaultUrl))?.data;
   for (const vault of vaults) {
     const tokenX = Number(vault?.coinA) / 10 ** vault?.pool?.tokenXDecimals;
@@ -14,9 +14,6 @@ async function vaultTVL() {
     api.add(vault?.pool?.tokenXType, tokenX);
     api.add(vault?.pool?.tokenYType, tokenY);
   }
-  return {
-    tvl,
-  };
 }
 
 module.exports = {
