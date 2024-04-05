04a6744b5ba7fd8b64475d11b4289e03c94d9a8e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-05 10:09:23 +0200

    refactor venom

--- projects/venombridge/index.js
+++ projects/venombridge/index.js
@@ -1,76 +1,21 @@
-const sdk = require("@defillama/sdk");
-const config = require("./config.js");
-
-function totalSupply(venomContracts) {
-  return async (timestamp, block, chainBlocks) => {
-    try {
-      let result = 0;
-      for (const { chain, address, decimals } of venomContracts) {
-        const { output: totalSupply } = await sdk.api.abi.call({
-          target: address,
-          abi: "erc20:totalSupply",
-          block,
-          chain,
-        });
-        result += totalSupply / 10 ** decimals;
-      }
-
-      return { venom: result };
-    } catch (error) {
-      console.error(`Error getting total supply: ${error.message}`);
-      return { venom: 0 };
-    }
-  };
-}
-
-function balanceOf(chain, vaultAddress, tokens) {
-  return async (timestamp, block, chainBlocks) => {
-    try {
-      const balanceCalls = tokens.map(({ address }) => ({
-        target: address,
-        params: [vaultAddress],
-      }));
-      const { output: balances } = await sdk.api.abi.multiCall({
-        calls: balanceCalls,
-        abi: "erc20:balanceOf",
-        block: chainBlocks[chain],
-        chain,
-      });
-
-      const result = {};
-      for (const { input, output } of balances) {
-        const token = tokens.find(({ address }) => address === input.target);
-        if (token) {
-          result[token.coingeckoId] = output / 10 ** token.decimals;
-        }
-      }
-
-      return result;
-    } catch (error) {
-      console.error(`Error getting balances for ${chain}: ${error.message}`);
-      return {};
-    }
-  };
-}
+const sdk = require("@defillama/sdk")
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
   timetravel: false,
   venom: {
-    tvl: totalSupply(config.venomContracts),
+    tvl: venomTvl,
   },
-  ethereum: {
-    tvl: balanceOf("ethereum", config.vaultAddress, config.tokens.ethereum),
-  },
-  bsc: {
-    tvl: balanceOf("bsc", config.vaultAddress, config.tokens.bsc),
-  },
-  fantom: {
-    tvl: balanceOf("fantom", config.vaultAddress, config.tokens.fantom),
-  },
-  polygon: {
-    tvl: balanceOf("polygon", config.vaultAddress, config.tokens.polygon),
-  },
-  avax: {
-    tvl: balanceOf("avax", config.vaultAddress, config.tokens.avax),
-  },
-};
+}
+
+const chains = ['ethereum', 'bsc', 'fantom', 'polygon', 'avax']
+const tvl = sumTokensExport({ owner: '0x4aB740157721105aE503fbad756a578171512525', fetchCoValentTokens: true, })
+
+chains.forEach(chain => module.exports[chain] = { tvl })
+
+async function venomTvl() {
+  const promises = chains.map(chain => sdk.api.erc20.totalSupply({ chain, target: '0x46f84dc6564cdd93922f7bfb88b03d35308d87c9' }))
+  const totalSupplies = await Promise.all(promises)
+  const bal = totalSupplies.reduce((acc, supply) => acc + supply.output / 1e9, 0)
+  return { 'venom': bal }
+}
