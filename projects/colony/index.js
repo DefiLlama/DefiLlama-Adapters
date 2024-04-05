c11c66068ee26d9c65344c59a6f8278e626bc086
hbdgr <hbdgr@protonmail.com>
2024-04-05 12:33:14 +0200

    colony: unlocked tokens from vesting contract

--- projects/colony/index.js
+++ projects/colony/index.js
@@ -1,7 +1,12 @@
+const { clyVesting } = require("./clyVesting")
+
+const colonyGovernanceToken = "0xec3492a2508DDf4FDc0cD76F31f340b30d1793e6";
+
 const stakingV1Contract = "0x5B0d74C78F2588B3C5C49857EdB856cC731dc557";
 const stakingV2Contract = "0x7CcDa6E26dCeD1Ba275c67CD20235790ed615A8D";
 const stakingV3Contract = "0x62685d3EAacE96D6145D35f3B7540d35f482DE5b";
-const colonyGovernanceToken = "0xec3492a2508DDf4FDc0cD76F31f340b30d1793e6";
+
+const vestingContract = "0xEFAc81f709d314604a7DaEe9ca234dA978c2Be20";
 
 async function staking({ api }) {
   const stakingV1 = (
@@ -31,10 +36,12 @@ async function staking({ api }) {
 }
 
 module.exports = {
+  timetravel: true,
   methodology:
     "Staking is calculated based on CLY tokens locked on Colony staking contracts",
   avax:{
     tvl: async () => ({}),
     staking,
+    vesting: clyVesting(colonyGovernanceToken, vestingContract),
   },
 };
