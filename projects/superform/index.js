cfaea7c34a674e6b6966f1196803b41656689ab4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-05 23:18:40 +0200

    refactor to use multicall

--- projects/superform/index.js
+++ projects/superform/index.js
@@ -1,71 +1,21 @@
-const modulesToExport = {};
 const superform_chains = ["ethereum", "polygon", "bsc", "avax", "arbitrum", "optimism", "base"];
 const factory_contract = "0xD85ec15A9F814D6173bF1a89273bFB3964aAdaEC";
 
-const calcTvl = async (api) => {
-    const vaultCount = await api.call({
-        abi: "function getSuperformCount() external view returns(uint256)",
-        target: factory_contract,
-    });
-
-    let balances = [];
-    let tokens = [];
-
-    for (let i = 0; i < vaultCount; i++) {
-            const vault = await api.call({
-                abi: "function superforms(uint256) external view returns(uint256)",
-                target: factory_contract,
-                params: [i],
-            });
-
-            const getSuperformRes = await api.call({
-                abi: "function getSuperform(uint256) external view returns(address, uint32, uint64)",
-                target: factory_contract,
-                params: [vault],
-            });
-
-            const [vaultAddress, assetAddress] = await Promise.all([
-                api.call({
-                    abi: "function vault() external view returns(address)",
-                    target: getSuperformRes[0],
-                }),
-                api.call({
-                    abi: "function asset() external view returns(address)",
-                    target: getSuperformRes[0],
-                })
-            ])
-
-            const balanceOfShares = await api.call({
-                abi: "erc20:balanceOf",
-                target: vaultAddress,
-                params: [getSuperformRes[0]],
-            });
-        
-        try{
-            const balanceOfCollateral = await api.call({
-                abi: "function previewRedeemFrom(uint256) external view returns(uint256)",
-                target: getSuperformRes[0],
-                params: [balanceOfShares],
-            });
-
-            balances.push(balanceOfCollateral);
-            tokens.push(assetAddress);
-        } catch (err) {
-            console.log("previewRedeem reverts");
-        }
-    }
-
-    return api.addTokens(tokens, balances);
-};
-
-superform_chains.forEach((chain) => {
-    modulesToExport[chain] = {
-        tvl: calcTvl
-    }
-})
+async function tvl(api) {
+  const forms = await api.fetchList({ lengthAbi: 'getSuperformCount', itemAbi: "function superforms(uint256) external view returns(uint256)", target: factory_contract })
+  const getSuperformRes = await api.multiCall({ abi: "function getSuperform(uint256) external view returns(address, uint32, uint64)", calls: forms, target: factory_contract })
+  const super4626 = getSuperformRes.map(v => v[0])
+  const vaults = await api.multiCall({ abi: 'address:vault', calls: super4626 })
+  const assets = await api.multiCall({ abi: 'address:asset', calls: super4626 })
+  const vBals = await api.multiCall({ abi: "erc20:balanceOf", calls: vaults.map((v, i) => ({ target: v, params: super4626[i] })) })
+  const bals = await api.multiCall({ abi: "function previewRedeemFrom(uint256) external view returns(uint256)", calls: super4626.map((v, i) => ({ target: v, params: vBals[i] })), permitFailure: true })
+  bals.forEach((bal, i) => {
+    if (bal) api.add(assets[i], bal)
+  })
+}
 
 module.exports = {
-    timetravel: true,
-    methodology: "counts the TVL of each superform across all the supported networks",
-    ...modulesToExport
+  methodology: "counts the TVL of each superform across all the supported networks",
 };
+
+superform_chains.forEach(chain => module.exports[chain] = { tvl })
