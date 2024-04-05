3c76b2c6a3151fc424c9cf868a5d2f11d2d63d03
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-05 14:12:12 +0200

    code refactor

--- projects/lumin-finance/index.js
+++ projects/lumin-finance/index.js
@@ -1,4 +1,5 @@
 const { nullAddress } = require("../helper/tokenMapping");
+const { staking } = require('../helper/staking')
 
 const LUMIN_V1_CONTRACT_ASSET_MANAGER = "0x61c6b185fafd2727ddeac6247e6770f5eadd823a"
 const LUMIN_V2_CONTRACT = "0x1F00009c0310A4804925695F44355dE5110EC074"
@@ -14,62 +15,26 @@ const abi = {
   }
 }
 
-const lutSymbol = {
-  ARB: 'arbitrum',
-  USDC: 'usd-coin',
-  USDT: 'tether'
+async function tvl(api) {
+  const assetIds = await api.call({ abi: abi.v1.assetIds, target: LUMIN_V1_CONTRACT_ASSET_MANAGER });
+  const assetData = await api.multiCall({ abi: abi.v1.asset, calls: assetIds, target: LUMIN_V1_CONTRACT_ASSET_MANAGER })
+  const tokens = assetData.map(asset => asset[0])
+  return api.sumTokens({ tokens, owner: LUMIN_V1_CONTRACT_ASSET_MANAGER })
 }
 
-async function staking({ api }) {
-  const stakingBalance = await api.call({
-    abi: abi.v2.stakeStats,
-    target: LUMIN_V2_CONTRACT,
-    params: [],
-  });
-
-  return {
-    'LUMIN': (BigInt(stakingBalance[0][0]) + BigInt(stakingBalance[1][0])) / BigInt(10 ** 12)
-  }
-}
-
-async function tvl({ api }) {
-  const assetIds = await api.call({
-    abi: abi.v1.assetIds,
-    target: LUMIN_V1_CONTRACT_ASSET_MANAGER
-  });
-
-  const deposits = {}
-
-  for (const assetId of assetIds) {
-    const asset = await api.call({
-      abi: abi.v1.asset,
-      target: LUMIN_V1_CONTRACT_ASSET_MANAGER,
-      params: [assetId],
-    });
-
-    const symbol = await api.call({
-      abi: 'erc20:symbol',
-      target: asset[0]
-    })
-
-    const deposit = await api.call({
-      abi: abi.v1.depositOf,
-      target: LUMIN_V1_CONTRACT_ASSET_MANAGER,
-      params: [assetId, nullAddress],
-    });
-
-    deposits[lutSymbol[symbol] ?? symbol] = deposit[0] / (10 ** asset[3])
-  }
-
-  return deposits
+async function borrowed(api) {
+  const assetIds = await api.call({ abi: abi.v1.assetIds, target: LUMIN_V1_CONTRACT_ASSET_MANAGER });
+  const assetData = await api.multiCall({ abi: abi.v1.asset, calls: assetIds, target: LUMIN_V1_CONTRACT_ASSET_MANAGER })
+  const tokens = assetData.map(asset => asset[0])
+  const deposit = await api.multiCall({ abi: abi.v1.depositOf, target: LUMIN_V1_CONTRACT_ASSET_MANAGER, calls: assetIds.map(i => ({ params: [i, nullAddress] })) });
+  tokens.forEach((token, i) => api.add(token, deposit[i][1]))
 }
 
 module.exports = {
-  timetravel: true,
   methodology: 'Gets v1 total deposits, and v2 staking statistics on-chain.',
   start: 194344665,
   arbitrum: {
-    staking,
-    tvl
+    staking: staking('0x1F5FAB72ED3Be2F1cf8F4E20BC9096C94a04D5c5', '0x1FC01117E196800f416A577350CB1938d10501C2'),
+    tvl, borrowed,
   }
-};
+}
