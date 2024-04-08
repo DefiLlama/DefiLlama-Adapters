bb6e8d7db17448d7e31c741583b3cd7cef004f3b
Jasper <104004836+j-vp@users.noreply.github.com>
2024-04-08 13:24:46 +0200

    📝 update methodology typo

--- projects/arcadia-finance-v2/index.js
+++ projects/arcadia-finance-v2/index.js
@@ -2,7 +2,7 @@ const { tvl } = require("./helper");
 
 module.exports = {
   methodology:
-    "TVL is calculates as the sum of all Account values and the available balance in the liquidity pools. Assets are not double counted.",
+    "TVL is calculated as the sum of all Account values and the available balance in the liquidity pools. Assets are not double counted.",
   base: { tvl },
   start: 1711389600, // Mon Mar 25 2024 18:00:00 GMT+0000
   hallmarks: [
