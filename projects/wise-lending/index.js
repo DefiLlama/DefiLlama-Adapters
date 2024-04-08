d38c1d43e5f641186c4fc8139bdcd8aed01e372a
Byron Kim Barra <byronbarra09@gmail.com>
2024-04-08 20:05:01 +0800

    removed hallmark to from updated wise lending project

--- projects/wise-lending/index.js
+++ projects/wise-lending/index.js
@@ -8,12 +8,6 @@ async function tvl(api) {
   return api.sumTokens({ owner: lendingContract, tokens: [...otherTokens, ...uaTokens] })
 }
 
-module.exports = {
-  hallmarks: [
-    [1705017600,"Project Exploited"]
-  ],
-}
-
 const config = {
   ethereum: { lending: '0x37e49bf3749513A02FA535F0CbC383796E8107E4', feeManager: '0x0bc24e61daad6293a1b3b53a7d01086bff0ea6e5' },
   arbitrum: { lending: '0x9034a49587bd2c1af27598e0f04f30db66c87ebf', feeManager: '0x90a022796798f9dbA1Da0f8645234B284d4E8EC6' },
