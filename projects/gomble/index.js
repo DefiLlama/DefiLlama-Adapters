a140a82c1fc958ca244364d80f29485181785e94
agwab <leesd556@gmail.com>
2024-04-09 19:45:19 +0900

    gomble: add testAuction tvl

--- projects/gomble/index.js
+++ projects/gomble/index.js
@@ -1,9 +1,18 @@
+const sdk = require('@defillama/sdk');
+const ADDRESSES = require('../helper/coreAssets.json')
 const stakingContractAddress = '0xAB8c9Eb287796F075C821ffafBaC5FeDAa4604d5';
+const testAuctionContractAddress = '0xe619F1DF06EeaAF443dFC5c09cc96e2C7C91d130';
 
 async function tvl(api) {
   const tokens = await api.call({ abi: 'address[]:getRegisteredTokens', target: stakingContractAddress })
   const vTokens = await api.multiCall({ abi: 'function getRegisteredVToken(address) view returns (address)', calls: tokens, target: stakingContractAddress })
-  return api.sumTokens({ owner: stakingContractAddress, tokens: vTokens.concat(tokens) })
+  await api.sumTokens({ owner: stakingContractAddress, tokens: vTokens.concat(tokens) });
+
+  const {output: balance} = await sdk.api.eth.getBalance({
+    target: testAuctionContractAddress,
+    chain: "bsc"
+  });
+  await api.add(ADDRESSES.null, balance);
 }
 
 module.exports = {
