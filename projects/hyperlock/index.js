72fe76da8c74aa59a85a7d67df82573dd2e1ec58
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-09 16:01:28 +0200

    Add token addresses and owners for Blast tokens

--- projects/hyperlock/index.js
+++ projects/hyperlock/index.js
@@ -1,3 +1,4 @@
+const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokens2 } = require("../helper/unwrapLPs");
 const { cachedGraphQuery } = require('../helper/cache')
 
@@ -12,7 +13,6 @@ module.exports = {
   },
 }
 
-
 const query = `{
   pools(where:{type: V2}) { id type }
 }`
@@ -20,8 +20,11 @@ const query = `{
 
 async function tvl(api) {
   const { pools } = await cachedGraphQuery('hyperlock/v2', 'https://graph.hyperlock.finance/subgraphs/name/hyperlock/points-blast-mainnet', query)
-  console.log(pools)
+  const tokens = pools.map(i => i.id)
   await sumTokens2({ api, owner: v2Deposits, tokens: pools.map(i => i.id), resolveLP: true, })
+  await sumTokens2({ api, tokensAndOwners: [
+    [ADDRESSES.blast.USDB, '0x390b781BAf1e6Db546cF4e3354b81446947838d2'],
+    [ADDRESSES.blast.WETH, '0x1856c7e0b559e9d7287473cb4b4786398db4032a'],
+  ] })
   return sumTokens2({ api, owner: v3Deposits, resolveUniV3: true })
-
 }
