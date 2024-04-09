5cdc580a50c6a9150a25f71f4e57b26351054706
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-09 13:10:01 +0200

    code refactor

--- projects/0xzebra/index.js
+++ projects/0xzebra/index.js
@@ -1,25 +1,8 @@
-const BigNumber = require("bignumber.js");
-
-const wzetaTroveManager = '0x3aC3d90dc8cB1Cacf1Fd8bc07e4B76c5EA443279'
-const zbrUSD = '0x5d8Dc3b34D7B07BB09495c480AD629900c0A2e02'
-const wzeta = '0x5F0b1a82749cb4E2278EC87F8BF6B618dC71a8bf'
-
-async function tvl(api) {
-	const getEntireSystemColl = await api.call({ target: wzetaTroveManager, abi: "function getEntireSystemColl() public view returns (uint256)" })
-	const price = await api.call({ target: wzetaTroveManager, abi: "function fetchPrice() external returns (uint256)" })
-	const totalSupply = await api.call({ target: zbrUSD, abi: "function totalSupply() public view returns (uint256)" })
-	// zbrUSD price is not available currently, using equivalent WZeta instead
-	const collBalance = new BigNumber(getEntireSystemColl)
-	const zbrUSDBalance = new BigNumber(totalSupply).multipliedBy(1e18).div(price)
-	api.addTokens([wzeta], [collBalance.plus(zbrUSDBalance).toFixed(0)])
-}
+const ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokensExport } = require('../helper/sumTokens')
 
 module.exports = {
-	start: 2197820,
-	misrepresentedTokens: true,
-	methodology: "TVL = TVL(Collateral) + toatalDebt(Supply of zbrUSD)",
-	timetravel: true,
 	zeta: {
-		tvl
+		tvl: sumTokensExport({ owner: '0x3aC3d90dc8cB1Cacf1Fd8bc07e4B76c5EA443279', tokens: [ADDRESSES.zeta.WZETA] }),
 	}
-};
+}
