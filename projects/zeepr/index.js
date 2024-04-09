1c87fa9f92256206aafcdd019969d35954a2d3d7
Joey <38412153+JoJo-Joker@users.noreply.github.com>
2024-04-09 20:58:11 +0800

    Update index.js

--- projects/zeepr/index.js
+++ projects/zeepr/index.js
@@ -1,29 +1,29 @@
-const { staking } = require("../helper/staking");
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const ADDRESSES = require("../helper/coreAssets.json");
 
-module.exports = {
+const config = {
   arbitrum: {
-    tvl: () => ({}),
-    staking: staking("0xf75fb73fd1bccd23ce2389169674ce375b43b7a6", "0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"),
+    ownerTokens: [
+      [
+        ["0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"],
+        "0xbB0390Cf2586e9b0A4FAADF720aE188D140E9fd5",
+      ],
+    ],
   },
-}
-// const { sumTokensExport } = require("../helper/unwrapLPs");
-// const ADDRESSES = require("../helper/coreAssets.json");
+};
 
-// const config = {
+Object.keys(config).forEach((chain) => {
+  module.exports[chain] = {
+    tvl: sumTokensExport(config[chain]),
+  };
+});
+
+
+// const { staking } = require("../helper/staking");
+
+// module.exports = {
 //   arbitrum: {
-//     ownerTokens: [
-//       [
-//         ["0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"],
-//         "0xf75fb73fd1bccd23ce2389169674ce375b43b7a6",
-//       ],
-//     ],
+//     tvl: () => ({}),
+//     staking: staking("0xf75fb73fd1bccd23ce2389169674ce375b43b7a6", "0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"),
 //   },
-// };
-
-// Object.keys(config).forEach((chain) => {
-//   module.exports[chain] = {
-//     tvl: sumTokensExport(config[chain]),
-//   };
-// });
-
-
+// }
