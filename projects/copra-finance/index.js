7a769e06c72876da5e90e49402a21b624ca6b1a5
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2024-04-09 15:56:40 +0800

    Add copra v2 tvl calc (#9755)
    * add copra v2 tvl calc
    
    * improve copra v2 tvl calc

--- projects/copra-finance/index.js
+++ projects/copra-finance/index.js
@@ -3,6 +3,7 @@ const { getLogs, } = require("../helper/cache/getLogs");
 async function tvl(api) {
   const { factory, fromBlock } = config[api.chain];
 
+  // V1
   const logs = await getLogs({
     api,
     target: factory,
@@ -13,11 +14,18 @@ async function tvl(api) {
   const creditAccounts = logs.map((i) => i.creditAccount)
   const status = await api.multiCall({ abi: 'function getStatus() view returns (uint8)', calls: creditAccounts })
   const activeCreditAccounts = creditAccounts.filter((_, i) => status[i] == 0 || status[i] == 1)
-  const tokens = (await api.multiCall({ abi: 'function getTerms() view returns (uint256 tenor, uint256 principalAmount, uint256 interestAmount, uint256 securityDepositAmount, address token)', calls: activeCreditAccounts })).map(i => i.token)
+  const v1Tokens = (await api.multiCall({ abi: 'function getTerms() view returns (uint256 tenor, uint256 principalAmount, uint256 interestAmount, uint256 securityDepositAmount, address token)', calls: activeCreditAccounts })).map(i => i.token)
   const lenderAmounts = await api.multiCall({ abi: 'uint256:getTotalFundedPrincipalAmount', calls: activeCreditAccounts })
   const borrowerAmounts = await api.multiCall({ abi: 'uint256:getBorrowerFundedAmount', calls: activeCreditAccounts })
-  api.add(tokens, lenderAmounts)
-  api.add(tokens, borrowerAmounts)
+  api.add(v1Tokens, lenderAmounts)
+  api.add(v1Tokens, borrowerAmounts)
+
+  // V2
+  const copraRegistryAddr = "0x10D3362BBf04427126c807d28665fA4Da5fBDF14"
+  const [v2LiquidityWarehouses] = await api.multiCall({ abi: 'address[]:getLiquidityWarehouses', calls: [copraRegistryAddr]})
+  const v2Tokens = (await api.multiCall({ abi: 'function getTerms() view returns (address asset, address feeRecipient, uint64 liquidationThreshold, uint64 capacityThreshold, uint64 interestRate, uint64 interestFee, uint64 withdrawalFee)', calls: v2LiquidityWarehouses })).map(i => i.asset)
+  const v2NetAssetValues = await api.multiCall({ abi: "uint256:getNetAssetValue", calls: v2LiquidityWarehouses})
+  api.add(v2Tokens, v2NetAssetValues)
 }
 
 const config = {
