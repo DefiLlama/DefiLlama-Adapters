19785a59f2c05d5fadd4b6e37c5f7da88e9e99f8
VensonLiou <48060449+VensonLiou@users.noreply.github.com>
2024-04-10 21:27:32 +0800

    teahouse: add portfolio vault under permissionless vault (#9791)
    * teahouse: add portfolio vault under permissionless vault
    
    * teahouse: skip doublecount
    
    ---------
    
    Co-authored-by: Venson <venson@teahouse.finance>
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/teahouse-v3/index.js
+++ projects/teahouse-v3/index.js
@@ -6,19 +6,26 @@ const teahouseVaultAPI = "https://vault-content-api.teahouse.finance/vaults";
 
 // get vault contract addresses from teahouse api
 async function getVaultContractsAddress(chain) {
-  let plAddress = [];
+  let pairVault = [];
+  let portVault = []
   const { vaults } = await getConfig("teahouse/v3", teahouseVaultAPI);
   vaults.forEach((element) => {
-    // v3 vaults
+    // permissionless vaults
     if (element.isDeFi == true && element.isActive == true) {
-      if (
-        element.share.address.toLowerCase() === '0xf9b099f8EebCC675B63E4f0a4657c10F246af77A'.toLowerCase() &&
-        element.chain === "arbitrum"
-      ) return;
-      if (element.chain === chain) plAddress.push(element.share.address);
+      if (element.chain === chain) {
+        const type = element.type.toLowerCase();
+        if (type === 'v3pair') {
+          pairVault.push(element.share.address);
+        } else if (type === 'v3port') {
+          portVault.push(element.share.address);
+        }
+      }
     }
   });
-  return plAddress;
+  return {
+    pair: pairVault,
+    port: portVault
+  };
 }
 
 const chains = ["ethereum", "optimism", "arbitrum", 'polygon', 'boba', 'mantle'];
@@ -27,20 +34,26 @@ chains.forEach((chain) => {
   module.exports[chain] = {
     tvl: async (api) => {
       const vaults = await getVaultContractsAddress(chain);
-      const tokens = await api.multiCall({
+
+      let tokens = await api.multiCall({
         abi: abi.assetToken1,
-        calls: vaults,
+        calls: vaults.pair,
       });
-      const bals = await api.multiCall({
+      let bals = await api.multiCall({
         abi: abi.estimatedValueIntoken1,
-        calls: vaults,
+        calls: vaults.pair,
       });
       api.addTokens(tokens, bals);
-      if (['boba', 'mantle'].includes(chain)) {
-        const tvl = await api.getUSDValue()
-        if (+tvl === 0) throw new Error('tvl is 0 Balances:' + JSON.stringify(api.getBalances()))
-      }
-      return api.getBalances();
+
+      tokens = await api.multiCall({ abi: "address[]:getAssets", calls: vaults.port })
+      const allTokens = [...new Set(tokens.flat())]
+      const symbols = await api.multiCall({ abi: 'string:symbol', calls: allTokens })
+      const sMap = Object.fromEntries(allTokens.map((address, i) => [address, symbols[i]]))
+      const ownerTokens = vaults.port.map((v, i) => {
+        const tokenList = tokens[i].filter(t => !sMap[t].startsWith('TEA-'))
+        return [tokenList, v]
+      })
+      return api.sumTokens({ ownerTokens})
     },
   };
 });
