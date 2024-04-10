f04fc8b6b418b99087c3863c2863797f13ee802d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-10 15:06:36 +0200

    code refactor

--- projects/nimbora/index.js
+++ projects/nimbora/index.js
@@ -1,31 +1,28 @@
-const { L1TroveAbi } = require('./abi');
-const { multiCall } = require('@defillama/sdk/build/abi');
-
+const { ChainApi } = require('@defillama/sdk');
+const ADDRESSES = require('../helper/coreAssets.json')
 const TROVE_HIGH_RISK = "0xEF3cf0ede2cA738A8Bd0c38fd5D43DC639B41532";
 const TROVE_MEDIUM_RISK = "0x4cdB2fdE85Da92Dbe9b568dda2Cc22d426b0b642";
 const TROVE_MANAGER = "0xA39739EF8b0231DbFA0DcdA07d7e29faAbCf4bb2";
-const ETH_L2_ADDRESS = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 
-async function tvl(_, _1, _2, { api }) {
-  const calls = [{params: [TROVE_HIGH_RISK]}, {params: [TROVE_MEDIUM_RISK]}];
-    
-    var tvl = 0;
-    const multicallResponse = await multiCall({
-      abi: L1TroveAbi.getEntireDebtAndColl,
-      calls: calls,
-      target: TROVE_MANAGER,
-      chain: 'ethereum'
-    });
+async function tvl(api) {
+  api = new ChainApi({ chain: 'ethereum', timestamp: api.timestamp })
+  await api.getBlock()
 
-    multicallResponse.output.forEach(response => {
-      tvl += Number(response.output.coll);
-    });
-    api.addToken(ETH_L2_ADDRESS, tvl);
+  const multicallResponse = await api.multiCall({
+    abi: "function getEntireDebtAndColl(address _borrower) payable returns (uint256 debt, uint256 coll, uint256, uint256)",
+    calls: [TROVE_HIGH_RISK, TROVE_MEDIUM_RISK],
+    target: TROVE_MANAGER,
+  });
+  multicallResponse.forEach(i => {
+    api.add(ADDRESSES.null, i.coll)
+    api.add(ADDRESSES.ethereum.LUSD, i.debt * -1)
+  })
+  return api.getBalances()
 }
 
 module.exports = {
-    methodology: "The TVL is calculated as a sum of total assets deposited into the Trove contracts.",
-    starknet: {
-        tvl,
-    },
+  methodology: "The TVL is calculated as a sum of total assets deposited into the Trove contracts.",
+  starknet: {
+    tvl,
+  },
 };
