951ecdda0e372895fb7d0865cb43abee245a1a3f
Joey <38412153+JoJo-Joker@users.noreply.github.com>
2024-04-12 00:40:33 +0800

    add core's zeepr (#9812)
    * Update tokenMapping.js
    
    * Update index.js

--- projects/zeepr/index.js
+++ projects/zeepr/index.js
@@ -5,4 +5,8 @@ module.exports = {
     tvl: () => ({}),
     staking: staking("0xbb0390cf2586e9b0a4faadf720ae188d140e9fd5", "0xe46C5eA6Da584507eAF8dB2F3F57d7F578192e13"),
   },
+  core: {
+    tvl: () => ({}),
+    staking: staking("0x60101E4388D1c2B389d78daC29d37Ee2DAc88e07", "0x1281E326C6e4413A98DafBd0D174a4Ae07ff4223"),
+  },
 }
