324362eb41bb5a4f47c40a7baeaaf13480ade5d9
jsmjsm <jsmjsm.eth@gmail.com>
2024-04-11 13:57:45 -0600

    Feat: Update market name

--- projects/avalon-finance/index.js
+++ projects/avalon-finance/index.js
@@ -3,10 +3,10 @@ const methodologies = require('../helper/methodologies')
 
 module.exports = {
 	methodology: methodologies.lendingMarket,
-	merlin: aaveExports('merlin', '0x6aB5d5E96aC59f66baB57450275cc16961219796', {
+	merlin_main_market: aaveExports('merlin', '0x6aB5d5E96aC59f66baB57450275cc16961219796', {
 		v3: true,
 	}),
-	merlin: aaveExports('merlin', '0x91b212e9FaF20117Eae59d6289CB38749DDFc070', {
+	merlin_innovation_market: aaveExports('merlin', '0x91b212e9FaF20117Eae59d6289CB38749DDFc070', {
 		v3: true,
 	}),
 }
