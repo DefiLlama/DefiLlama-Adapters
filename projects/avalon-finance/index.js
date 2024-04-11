e3e103f0cc72a4cdebae18e64a6e1cb0f66b2c18
jsmjsm <jsmjsm.eth@gmail.com>
2024-04-11 14:03:19 -0600

    Feat: Combine the markets tvl and borrowed

--- projects/avalon-finance/index.js
+++ projects/avalon-finance/index.js
@@ -1,12 +1,18 @@
 const { aaveExports } = require('../helper/aave')
 const methodologies = require('../helper/methodologies')
 
+const merlin_main_market_data = aaveExports('merlin', '0x6aB5d5E96aC59f66baB57450275cc16961219796', {
+	v3: true,
+})
+
+const merlin_innovation_market_data = aaveExports('merlin', '0x91b212e9FaF20117Eae59d6289CB38749DDFc070', {
+	v3: true,
+})
+
 module.exports = {
 	methodology: methodologies.lendingMarket,
-	merlin_main_market: aaveExports('merlin', '0x6aB5d5E96aC59f66baB57450275cc16961219796', {
-		v3: true,
-	}),
-	merlin_innovation_market: aaveExports('merlin', '0x91b212e9FaF20117Eae59d6289CB38749DDFc070', {
-		v3: true,
-	}),
+	merlin: {
+		tvl: merlin_main_market_data.tvl + merlin_innovation_market_data.tvl,
+		borrowed: merlin_main_market_data.borrowed + merlin_innovation_market_data.borrowed,
+	},
 }
