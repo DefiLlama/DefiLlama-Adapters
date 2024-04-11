265448cc48fac91ba0f81401529c2ce53036f5d7
jsmjsm <jsmjsm.eth@gmail.com>
2024-04-11 13:42:42 -0600

    Feat: Update chain name

--- projects/avalon-finance/index.js
+++ projects/avalon-finance/index.js
@@ -3,10 +3,10 @@ const methodologies = require('../helper/methodologies')
 
 module.exports = {
 	methodology: methodologies.lendingMarket,
-	Merlin: aaveExports('Merlin', '0x662F1DcFf1124D63e8bb01e2d1b6486428C511B3', {
+	merlin: aaveExports('merlin', '0x662F1DcFf1124D63e8bb01e2d1b6486428C511B3', {
 		v3: true,
 	}),
-	Merlin: aaveExports('Merlin', '0xCB952Df3ed741dfAB56D96dAFeCd2a3A7A1fe0E1', {
+	merlin: aaveExports('merlin', '0xCB952Df3ed741dfAB56D96dAFeCd2a3A7A1fe0E1', {
 		v3: true,
 	}),
 }
