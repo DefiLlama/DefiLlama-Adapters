0c06fded551ac515446d5bdae51194a5b557fa7d
rez <info@sakaivault.io>
2024-04-11 09:48:07 +0300

    added staking v2 contract, deleted dao contract

--- projects/Chocoinu/index.js
+++ projects/Chocoinu/index.js
@@ -1,6 +1,10 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { masterchefExports } = require("../helper/unknownTokens");
 
-module.exports = {
-  misrepresentedTokens: true,
-  avax: { tvl: getUniTVL({ factory: '0x49a5044268A54467a94905d1458A88413695afc1', useDefaultCoreAssets: true, fetchBalances: true, }), },
-}
+module.exports = masterchefExports({
+  chain: "shibarium",
+  masterchef: "0x100A30e31aa03ed85F0854712a1Dff0880e960BE",
+  nativeTokens: ["0xC7cc176b2a098fF7cFd578C9eF0Cc8b1216C8ED1"],
+  useDefaultCoreAssets: true,
+  poolInfoABI: "function poolInfo(uint256) view returns (address)",
+  getToken: (i) => i,
+});
