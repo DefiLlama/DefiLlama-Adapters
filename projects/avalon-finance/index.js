b87e96143910f6790e73527c02454da5a4586c64
jsmjsm <jsmjsm.eth@gmail.com>
2024-04-11 13:28:52 -0600

    Feat: Add Avalon Finance

---
+++ projects/avalon-finance/index.js
@@ -0,0 +1,12 @@
+const { aaveExports } = require('../helper/aave')
+const methodologies = require('../helper/methodologies')
+
+module.exports = {
+	methodology: methodologies.lendingMarket,
+	Merlin_Main: aaveExports('Merlin', '0x662F1DcFf1124D63e8bb01e2d1b6486428C511B3', {
+		v3: true,
+	}),
+	Merlin_Innovation: aaveExports('Merlin', '0xCB952Df3ed741dfAB56D96dAFeCd2a3A7A1fe0E1', {
+		v3: true,
+	}),
+}
