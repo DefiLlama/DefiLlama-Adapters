c32b68923b9b22027bd47b6196d755c44f61f0a0
kdbvier <kdbvier@gmail.com>
2024-04-14 15:24:24 -0600

    fix: lucia adapter fixed

--- projects/luciaprotocol/index.js
+++ projects/luciaprotocol/index.js
@@ -1,15 +1,7 @@
-const { getUniTVL } = require("../helper/unknownTokens");
-const config = {
-  manta: "0x7C23F66DFC733068043060ab04f88B5122042E2a",
-};
-
-module.exports = {
-  misrepresentedTokens: true,
-};
-
-Object.keys(config).forEach((chain) => {
-  const factory = config[chain];
-  module.exports[chain] = {
-    tvl: getUniTVL({ factory, useDefaultCoreAssets: true }),
-  };
+const { uniV3Export } = require("../helper/uniswapV3");
+module.exports = uniV3Export({
+  manta: {
+    factory: "0x7C23F66DFC733068043060ab04f88B5122042E2a",
+    fromBlock: 1747340,
+  },
 });
