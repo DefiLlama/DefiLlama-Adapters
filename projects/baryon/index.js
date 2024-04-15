7480e0a46c3d309d3697e99f401291f10a84755d
khiêm <87562184+khiemsoft@users.noreply.github.com>
2024-04-15 17:30:05 +0700

    feat: submit add more chain to baryon

--- projects/baryon/index.js
+++ projects/baryon/index.js
@@ -1,5 +1,4 @@
-const utils = require('../helper/utils');
-const { stakings, staking } = require('../helper/staking');
+const { stakings } = require('../helper/staking');
 const { getUniTVL } = require('../helper/unknownTokens')
 const { getConfig } = require('../helper/cache')
 
@@ -21,39 +20,42 @@ async function stakingBsc(...args) {
   return stakings(lpTokenTomo.stakeContract, lpTokenTomo.lpToken)(...args)
 }
 
-async function fetchData(chain) {
-  const { data } = await utils.fetchURL('https://rapid.coin98.com/baryon-stake.json')
+async function stakingAncient8(...args) {
+  const lpTokenqAncient8 = await fetchData('ancient8Mainnet')
 
-  return data[chain]
+  return stakings(lpTokenqAncient8.stakeContract, lpTokenqAncient8.lpToken)(...args)
 }
 
-async function stakingTomo(...args) {
-  const lpTokenTomo = await fetchData('tomo')
+async function stakingBitkub(...args) {
+  const lpTokenBitkub = await fetchData('bitkub')
 
-  return stakings(lpTokenTomo.stakeContract, lpTokenTomo.lpToken)(...args)
+  return stakings(lpTokenBitkub.stakeContract, lpTokenBitkub.lpToken)(...args)
 }
 
-async function stakingBsc(...args) {
-  const lpTokenTomo = await fetchData('binanceSmart')
-
-  return stakings(lpTokenTomo.stakeContract, lpTokenTomo.lpToken)(...args)
-}
 
 module.exports = {
   bsc: {
-    misrepresentedTokens: true,
     staking: stakingBsc,
     tvl: getUniTVL({
       factory: '0x03879e2a3944fd601e7638dfcbc9253fb793b599',
-      useDefaultCoreAssets: true,
     })
   },
   tomochain: {
-    misrepresentedTokens: true,
     staking: stakingTomo,
     tvl: getUniTVL({
       factory: '0xFe48A2E66EE2f90334d3565E56E0c9d0081447e8',
-      useDefaultCoreAssets: true,
+    })
+  },
+  ancient8: {
+    staking: stakingAncient8,
+    tvl: getUniTVL({
+      factory: '0xAE12C5930881c53715B369ceC7606B70d8EB229f',
+    })
+  },
+  bitkub: {
+    staking: stakingBitkub,
+    tvl: getUniTVL({
+      factory: '0xf7eEe3A8363731C611A24CdDfCBcaDE9C153Cfe8',
     })
   }
 }
