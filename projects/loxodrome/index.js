ff5a31cd212d2165910632cfdbdcc55ac75a3de2
wangjj250wo <wangjj250wo@qq.com>
2024-04-16 09:58:49 +0800

    remove checking for tokens on gauge

--- projects/loxodrome/index.js
+++ projects/loxodrome/index.js
@@ -1,3 +1,46 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const sdk = require("@defillama/sdk");
 
-module.exports = uniTvlExport('iotex', '0x92bfa051bf12a0aef9a5e1ac8b2aa7dc1b05a406', { hasStablePools: true, })
+const balances = {};
+
+const PAIR_FACTORY = "0x92bfa051bf12a0aef9a5e1ac8b2aa7dc1b05a406";
+// const VOTER = "0x5792c972D8D4b43c0c154Fba8215c28133e5b7f0";
+
+async function getPairs(api, factory) {
+    const pairs = await api.call({
+        abi: "function pairs() external view returns(address[] memory )",
+        target: factory,
+    });
+    sdk.log(pairs);
+    const tokens0 = await api.multiCall({
+        abi: "address:token0",
+        calls: pairs.map((pair) => ({ target: pair })),
+    });
+    const tokens1 = await api.multiCall({
+        abi: "address:token1",
+        calls: pairs.map((pair) => ({ target: pair })),
+    });
+    const ownerTokens = [];
+    if (pairs.length > 0) {
+        pairs.map((pair, idx) => {
+            ownerTokens.push([
+                [tokens0[idx].toLowerCase(), tokens1[idx].toLowerCase()],
+                pair.toLowerCase(),
+            ]);
+        });
+    }
+    return ownerTokens;
+}
+
+module.exports = {
+    iotex: {
+        tvl: async (api) =>
+            sumTokensExport({
+                api,
+                balances,
+                ownerTokens: await getPairs(api, PAIR_FACTORY),
+                resolveLP: true,
+            })(),
+    },
+};
+
