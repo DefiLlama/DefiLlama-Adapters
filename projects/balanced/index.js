ae5883300e9f20781be66aef54cf20e87b7796ed
David <111794534+usr-icon-foundation@users.noreply.github.com>
2024-04-16 11:17:07 +0200

    Configure external chain deposits for cross-chain Balanced DEX
    Placed getExternalChainDeposits and computeTVL functions in balanced/helper.js
    
    Updated balanced/index.js to filter out external deposits from the icon TVL and attribute these external deposits to their origin chains.

--- projects/balanced/index.js
+++ projects/balanced/index.js
@@ -1,13 +1,16 @@
 const { sumTokens } = require('../helper/chain/icx');
 const { getConfig } = require('../helper/cache');
 const ADDRESSES = require('../helper/coreAssets.json');
+const { computeTVL, getExternalChainDeposits } = require('./helper')
 
 const balancedDexContract = 'cxa0af3165c08318e988cb30993b3048335b94af6c';
 
 async function tvl(api) {
   const pools = await getConfig('balancedDex', 'https://balanced.icon.community/api/v1/pools')
   const tokens = pools.map(pool => [pool.base_address, pool.quote_address]).flat().filter(i => i).map(i => i === 'ICX' ? ADDRESSES.null : i)
-  return sumTokens({ api, tokens, owner: balancedDexContract })
+  const deposits = await getExternalChainDeposits();
+  const filteredTokens = tokens.filter(token => !deposits.some(deposit => deposit.iconchaincontract === token));
+  return sumTokens({ api, filteredTokens, owner: balancedDexContract })
 }
 
 module.exports = {
@@ -16,6 +19,12 @@ module.exports = {
     tvl
   },
   archway: {
-    tvl: () => ({})
+    tvl: async () => await computeTVL("archway"),
+  },
+  binance: {
+    tvl: async () => await computeTVL("bnbchain"),
+  },
+  avax: {
+    tvl: async () => await computeTVL("avalanche"),
   },
 };
