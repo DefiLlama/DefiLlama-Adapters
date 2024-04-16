60f5dbecd6e5f14ac9236942fe69c98351f91110
Bill Clippy <bill.clippy@tranchess.com>
2024-04-16 11:20:57 -0400

    add STONE fund TVL

--- projects/tranchess/index.js
+++ projects/tranchess/index.js
@@ -7,6 +7,10 @@ const votingEscrow = '0x95A2bBCD64E2859d40E2Ad1B5ba49Dc0e1Abc6C2'
 const ethChess = '0xD6123271F980D966B00cA4FCa6C2c021f05e2E73'
 const ethVotingEscrow = '0x3FadADF8f443A6DC1E091f14Ddf8d5046b6CF95E'
 
+const scrollChess = '0x9735fb1126B521A913697A541f768376011bCcF9'
+const scrollETH = '0x5300000000000000000000000000000000000004'
+const scrollVotingEscrow = '0xffD17794bF2e3BA798170f358225763F1aF8f5ba'
+
 const funds = new Map([
   ['0xd6B3B86209eBb3C608f3F42Bf52818169944E402', false],  // BTC
   ['0x677B7304Cb944b413D3c9aEbc4D4B5DA1A698A6B', false],  // ETH
@@ -36,6 +40,15 @@ const ETHV2Funds = [
 const ETHV2Swaps = [
   '0xBA919470C7a2983fbcdA6ADC89Be9C43b8298079', // ETH V2 BISHOP Swap (ETH mainnet)
   '0xAD06a2DBd34Da8f8Cf5f85d284A5B93A2057bDb5', // wstETH SWAP (ETH mainnet)
+]
+
+const stone = '0x7122985656e38BDC0302Db86685bb972b145bD3C'
+const ScrollFunds = [
+  '0x289E69E5B611F6193694F6Cfa2F93B7cF161253f', // Stone Fund (Scroll mainnet)
+]
+
+const ScrollSwaps = [
+  '0xD151ce31322aEa25E4779678dF0A3f376f9FFc6f', // Stone SWAP (Scroll mainnet)
 ]
 
 function getBSCAddress(address) {
@@ -166,6 +179,19 @@ async function ethStaking(timestamp, blockETH, chainBlocks) {
   return balances;
 }
 
+async function scrollStaking(timestamp, blockETH, chainBlocks) {
+  let balances = {};
+  let { output: balance } = await sdk.api.erc20.balanceOf({
+    target: scrollChess,
+    owner: scrollVotingEscrow,
+    chain: 'scroll',
+    block: chainBlocks['scroll']
+  });
+  sdk.util.sumSingleBalance(balances, scrollChess, balance, 'scroll');
+
+  return balances;
+}
+
 async function ethereum(timestamp, blockETH, chainBlocks){
   let balances = {};
 
@@ -208,6 +234,51 @@ async function ethereum(timestamp, blockETH, chainBlocks){
   return balances
 }
 
+async function scroll(timestamp, blockETH, chainBlocks){
+  let balances = {};
+
+  for (const fund of ScrollFunds) {
+    const underlyingInFund = (await sdk.api.abi.call({
+      target: fund,
+      abi: abi.getTotalUnderlying,
+      chain: 'scroll',
+      block: chainBlocks['scroll']
+    })).output
+
+    const stonePrice = (await sdk.api.abi.call({
+      target: stone,
+      abi: abi.tokenPrice,
+      chain: 'ethereum',
+      block: chainBlocks['scroll']
+    })).output
+    const ethAmount = underlyingInFund * stonePrice / 1e18
+
+    sdk.util.sumSingleBalance(balances, scrollETH, ethAmount, 'scroll')
+  }
+
+  for (const swap of ScrollSwaps) {
+    const balancesInSwap = (await sdk.api.abi.call({
+      target: swap,
+      abi: abi.allBalances,
+      chain: 'scroll',
+      block: chainBlocks['scroll']
+    })).output
+
+    const stonePrice = (await sdk.api.abi.call({
+      target: stone,
+      abi: abi.tokenPrice,
+      chain: 'ethereum',
+      block: chainBlocks['scroll']
+    })).output
+    console.log(stonePrice)
+    const ethAmount = balancesInSwap[1] * stonePrice / 1e18
+
+    sdk.util.sumSingleBalance(balances, scrollETH, ethAmount, 'scroll')
+  }
+  
+  return balances
+}
+
 module.exports = {
   methodology: `Counts the underlying assets in each fund.`,
   bsc:{
@@ -217,5 +288,9 @@ module.exports = {
   ethereum:{
     staking: ethStaking,
     tvl: ethereum
-  }
+  },
+  scroll:{
+    staking: scrollStaking,
+    tvl: scroll
+  },
 }
