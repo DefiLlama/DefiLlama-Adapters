ca32b15bc19ff267fd706c8524110d543bf3d6fb
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2024-04-16 14:56:04 +0400

    Add new vaults for Storm Trade (#9859)

--- projects/stormtrade/index.js
+++ projects/stormtrade/index.js
@@ -1,9 +1,19 @@
 const { sumTokensExport } = require('../helper/sumTokens')
+const ADDRESSES = require("../helper/coreAssets.json");
 
 module.exports = {
   timetravel: false,
   methodology: 'Total amount of jUSDT locked in the StormTrade vault (EQDynReiCeK8xlKRbYArpp4jyzZuF6-tYfhFM0O5ulOs5H0L)',
   ton: {
-    tvl: sumTokensExport({ owner: 'EQDynReiCeK8xlKRbYArpp4jyzZuF6-tYfhFM0O5ulOs5H0L' })
+    tvl: (
+      sumTokensExport({
+        owners: [
+          'EQDynReiCeK8xlKRbYArpp4jyzZuF6-tYfhFM0O5ulOs5H0L', // USDT vault
+          'EQDpJnZP89Jyxz3euDaXXFUhwCWtaOeRmiUJTi3jGYgF8fnj', // TON-M vault
+          'EQBwfRtqEf3ZzhkeGsmXiC7hzTh1C5zZZzLgDH5VL8gENQ2A'  // Notcoin pre-market vault
+        ],
+        tokens: [ADDRESSES.ton.jUSDT, ADDRESSES.ton.TON]
+      })
+    )
   }
 }
