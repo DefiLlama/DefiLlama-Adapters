1a561da15b0635eda838d8b88bde7e4d9473f22a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-16 10:37:40 +0200

    code refactor

--- projects/areon-swap/index.js
+++
@@ -1,9 +0,0 @@
-const { getUniTVL } = require('../helper/unknownTokens')
-
-module.exports = {
-    misrepresentedTokens: true,
-    methodology: "HyperSwap Tvl Calculation",
-    area: {
-        tvl: getUniTVL({ factory: '0x4df039804873717bff7d03694fb941cf0469b79e', useDefaultCoreAssets: true }),
-    }
-}
