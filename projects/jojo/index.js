f070aab0d34ee72cbff8875d181aab2bcf3ac5d6
JoscelynFarr <130961775+JoscelynFarr@users.noreply.github.com>
2024-04-18 18:24:19 +0800

    add jojo tvl (#9890)
    * add jojo tvl
    
    * Update index.js
    
    ---------
    
    Co-authored-by: JoscelynFarr <fushikun307@outlook.com>
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/jojo/index.js
+++ projects/jojo/index.js
@@ -2,16 +2,23 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
-    era: { tvl: sumTokensExport({ tokens: [ADDRESSES.era.USDC], owners: ['0x47eAD228547db8397398C1D3aAfd0847CBEbddeC'], }) },
-    bsc: { tvl: sumTokensExport({ tokens: [ADDRESSES.bsc.USDC], owners: ['0x25173BB47CB712cFCDFc13ECBebDAd753090801E'], }) },
-    arbitrum: {
-        tvl: sumTokensExport(
-            {
-                tokens: [ADDRESSES.arbitrum.USDC_CIRCLE, ADDRESSES.arbitrum.WETH, ADDRESSES.arbitrum.WBTC,
-                ADDRESSES.arbitrum.ARB, ADDRESSES.arbitrum.GMX, ADDRESSES.arbitrum.WSTETH],
-                owners: ['0xcd85998b31C85040C8BA59288eaA8f9beE115B01', '0xa88603625ad55e25674F46f3c831010D53d35E79', '0x0174f6d813Df42C986ADF75ec473a0162faAfcda']
-            }
-        )
-    },
-};
-
+  era: { tvl: sumTokensExport({ tokens: [ADDRESSES.era.USDC], owners: ['0x47eAD228547db8397398C1D3aAfd0847CBEbddeC'], }) },
+  bsc: { tvl: sumTokensExport({ tokens: [ADDRESSES.bsc.USDC], owners: ['0x25173BB47CB712cFCDFc13ECBebDAd753090801E'], }) },
+  arbitrum: {
+    tvl: sumTokensExport(
+      {
+        tokens: [ADDRESSES.arbitrum.USDC_CIRCLE, ADDRESSES.arbitrum.WETH, ADDRESSES.arbitrum.WBTC,
+        ADDRESSES.arbitrum.ARB, ADDRESSES.arbitrum.GMX, ADDRESSES.arbitrum.WSTETH],
+        owners: ['0xcd85998b31C85040C8BA59288eaA8f9beE115B01', '0xa88603625ad55e25674F46f3c831010D53d35E79', '0x0174f6d813Df42C986ADF75ec473a0162faAfcda']
+      }
+    )
+  },
+  base: {
+    tvl: sumTokensExport(
+      {
+        tokens: [ADDRESSES.base.USDC, ADDRESSES.base.WETH],
+        owners: ['0x2f7c3cf9d9280b165981311b822becc4e05fe635', '0xf8192489A8015cA1690a556D42F7328Ea1Bb53D0']
+      }
+    )
+  },
+}
