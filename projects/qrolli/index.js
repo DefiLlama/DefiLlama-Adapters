a0cc6fafd6783281d980ff84c0e7c3095b17fe5a
qrolli <qrolli@proton.me>
2024-04-18 17:28:40 +0800

    Update Qrolli TVL

--- projects/qrolli/index.js
+++ projects/qrolli/index.js
@@ -3,7 +3,8 @@ const BitsHub = "0x64af2e5b1f6c0194adc5a0f17bc566a6d30f21e1"
 
 async function tvl(api) {
   const pools = await api.fetchList({ lengthAbi: 'getBitsLength', itemAbi: 'bitsClones', target: BitsHub })
-  return api.sumTokens({ owners: pools, tokens: [ADDRESSES.null] })
+  api.sumTokens({ owners: pools, tokens: [ADDRESSES.null] }) // For MATIC amount
+  return api.sumTokens({ owners: pools, tokens: [ADDRESSES.null] }) // For Token amount which is same as MATIC amount
 }
 
 module.exports = {
