d079723b9059e741a103f63813c99d4f3b1b6484
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-18 10:03:28 +0200

    simplify vesting logic

--- projects/colony/index.js
+++ projects/colony/index.js
@@ -1,4 +1,5 @@
 const { clyVesting } = require("./clyVesting")
+const { staking } = require('../helper/staking')
 
 const colonyGovernanceToken = "0xec3492a2508DDf4FDc0cD76F31f340b30d1793e6";
 
@@ -8,46 +9,25 @@ const stakingV3Contract = "0x62685d3EAacE96D6145D35f3B7540d35f482DE5b";
 
 const vestingContract = "0xEFAc81f709d314604a7DaEe9ca234dA978c2Be20";
 
-async function staking({ api }) {
-  const stakingV1 = (
-    await api.call({
-      abi: "function totalStaked() external view returns (uint256)",
-      target: stakingV1Contract,
-    })
-  )
-
-  const stakingV2 = (
-    await api.call({
-      abi: "function totalStake() external view returns (uint256)",
-      target: stakingV2Contract,
-    })
-  )
-
-  const stakingV3 = (
-    await api.call({
-      abi: "function totalStake() external view returns (uint256)",
-      target: stakingV3Contract,
-    })
-  )
-
-  api.add(colonyGovernanceToken, stakingV1)
-  api.add(colonyGovernanceToken, stakingV2)
-  api.add(colonyGovernanceToken, stakingV3)
+async function _staking(api) {
+  const bals = await api.multiCall({ abi: 'uint256:totalStaked', calls: [stakingV1Contract,] })
+  const bals1 = await api.multiCall({ abi: 'uint256:totalStake', calls: [stakingV2Contract, stakingV3Contract] })
+  api.add(colonyGovernanceToken, bals.concat(bals1))
 }
 
 module.exports = {
-  timetravel: true,
   methodology:
     "Staking is calculated based on CLY tokens locked on Colony staking contracts. " +
     "Vesting is calculated as already unlocked and available to claim CLY tokens in the vesting contract",
-  avax:{
+  avax: {
     start: 1638367059, // CLY Token deployment
     hallmarks: [
       [1651241728, "Staking V2 Launch"],
       [1711370069, "Staking V3 Launch"]
     ],
     tvl: async () => ({}),
-    staking,
-    vesting: clyVesting(colonyGovernanceToken, vestingContract),
+    staking: _staking,
+    // vesting: clyVesting(colonyGovernanceToken, vestingContract),
+    vesting: staking(vestingContract, colonyGovernanceToken),
   },
 };
