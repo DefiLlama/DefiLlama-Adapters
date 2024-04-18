28e3a803daa0d906137c569f9e7f2f4ff97657c1
leven <leven890619@gmail.com>
2024-04-18 14:07:09 +0800

    add xrgb

--- projects/xrgb/index.js
+++ projects/xrgb/index.js
@@ -6,7 +6,7 @@ module.exports = {
   ethereum: {
     tvl: sumTokensExport({
       owners: ["0xc50f5f0e2421c307b3892a103b45b54f05259668"],
-      tokens: [ADDRESSES.ethereum.WETH]
+      tokens: [ADDRESSES.ethereum.WETH, "0x5cc5e64ab764a0f1e97f23984e20fd4528356a6a"]
     }),
   },
 };
