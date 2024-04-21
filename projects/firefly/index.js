3dc37611cd2f090a20254fb02caf6b1237bebfe1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-21 20:03:02 +0200

    Update index.js

--- projects/firefly/index.js
+++ projects/firefly/index.js
@@ -1,12 +1,5 @@
-const sdk = require("@defillama/sdk");
-const { uniV3Export } = require("../helper/uniswapV3");
-const { mergeExports } = require("../helper/utils");
+const { uniV3Export } = require("../helper/uniswapV3")
 
-module.exports = mergeExports([
-  uniV3Export({
-    manta: {
-      factory: "0x8666EF9DC0cA5336147f1B11f2C4fC2ecA809B95",
-      fromBlock: 1776716,
-    },
-  }),
-]);
+module.exports = uniV3Export({
+  manta: { factory: "0x8666EF9DC0cA5336147f1B11f2C4fC2ecA809B95", fromBlock: 1776716, },
+})
