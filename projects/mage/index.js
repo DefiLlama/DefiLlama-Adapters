b64d5d165d6e15a901c759d7d760a5f900856377
MageFinance <mage.finance@outlook.com>
2024-04-21 23:18:20 +0800

    Update Mage TVL

--- projects/mage/index.js
+++ projects/mage/index.js
@@ -1,6 +1,18 @@
-const { methodology, compoundExports2 } = require("../helper/compound");
+const { methodology, usdCompoundExports } = require("../helper/compound");
+const { mergeExports } = require("../helper/utils");
 
-module.exports = {
-  merlin: compoundExports2({ comptroller: '0xCE3bcCd2b0A457782f79000Be1b534C04B3F5aDD', cether: '0xe3b51f15dc086fba15426b8d42b4cd6feb46968e' }),
-  methodology,
-}
+const mainHub = usdCompoundExports(
+  "0xCE3bcCd2b0A457782f79000Be1b534C04B3F5aDD",
+  "merlin",
+  "0xe3b51f15dc086fba15426b8d42b4cd6feb46968e"
+);
+
+const merlinHub = usdCompoundExports(
+  "0xe7464Caa3fD31A1A8B458a634e72F94A00695d17",
+  "merlin"
+);
+
+module.exports = mergeExports([
+  { methodology, merlin: mainHub },
+  { merlin: merlinHub },
+]);
