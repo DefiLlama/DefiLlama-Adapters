ebfacb07eab6da699c1b5257addc71ad6d535fba
0xngmi <0xngmi@protonmail.com>
2024-04-22 19:40:13 +0100

    uniswap linea

--- projects/uniswap/index.js
+++ projects/uniswap/index.js
@@ -81,6 +81,7 @@ module.exports = {
     rsk: { factory: "0xAf37Ec98A00fD63689cF3060Bf3b6784e00CaD82", fromBlock: 5829207, },
     scroll: { factory: "0x70C62C8b8e801124A4Aa81ce07b637A3e83cb919", fromBlock: 1367, },
     blast: { factory: "0x792edade80af5fc680d96a2ed80a44247d2cf6fd", fromBlock: 400903, },
+    linea: { factory: "0x31FAfd4889FA1269F7a13A66eE0fB458f27D72A9", fromBlock: 25247, },
   }),
   filecoin: { tvl: filecoinTvl },
 }
