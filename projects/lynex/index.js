c2c1779b0f3a64708224dcb3779e9b2ab8607f6c
Krugo-D <lennardmulder1991@gmail.com>
2024-04-22 22:50:45 +0700

    add lynx->veLYNX staking

--- projects/lynex/index.js
+++ projects/lynex/index.js
@@ -1,9 +1,21 @@
 const { uniV3Export } = require("../helper/uniswapV3");
+const { staking } = require("../helper/staking");
 
-module.exports = uniV3Export({
+const tvl = uniV3Export({
   linea: {
     factory: "0x622b2c98123D303ae067DB4925CD6282B3A08D0F",
     fromBlock: 143660,
     isAlgebra: true,
   },
-});
+}).linea.tvl;
+
+module.exports = {
+  misrepresentedTokens: true,
+  linea: {
+    tvl: tvl,
+    staking: staking(
+      "0x8D95f56b0Bac46e8ac1d3A3F12FB1E5BC39b4c0c",
+      "0x1a51b19CE03dbE0Cb44C1528E34a7EDD7771E9Af"
+    ),
+  },
+};
