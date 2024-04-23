e593a0ebd873841dca7fd64b1afabf1d815bce45
Snail Finance <167850087+SnailFinance@users.noreply.github.com>
2024-04-23 16:15:47 +0800

    Listing of Snail (#9938)

---
+++ projects/Snail Finance/index.js
@@ -0,0 +1,6 @@
+const { methodology, compoundExports2 } = require("../helper/compound");
+
+module.exports = {
+  base: compoundExports2({ comptroller: '0x784E1507193c060bC88e6699adC7c796Ebe0E14e', cether: '0x9649Eb0f03C7e85615cFDC7927c7E6B775Ef284d' }),
+  methodology,
+}
