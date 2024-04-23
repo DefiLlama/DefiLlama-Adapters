a70fa379dd71530924630d7dcc155385d1663c6b
Mouzayan Delbourgo <mouzayan@arcade.xyz>
2024-04-23 13:17:38 -0400

    chore(arcade-staking-tvl): index.js running

--- projects/arcade-xyz/staking_queries.js
+++ projects/arcade-xyz/staking_queries.js
@@ -5,6 +5,7 @@ const { sumTokensAndLPsSharedOwners } = require('../helper/unwrapLPs');
 const { CHAIN, STAKING_REWARDS, SINGLE_SIDED_STAKING, ARCD, ARCD_WETH_LP, WETH } = require('./staking_constants');
 const SINGLE_SIDED_STAKING_ABI = require("./staking_abi_single_sided.json");
 
+
 async function getTokenPriceInUSD(tokenId) {
     try {
         const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${tokenId}&vs_currencies=usd`);
@@ -58,25 +59,27 @@ async function addToTVL(block, chainBlocks) {
 
   // convert balances to USD
   if (balances[normalizedARCDKey] !== undefined) {
-    console.log("ARCD balance before conversion:", balances[normalizedARCDKey]);
+    console.log("StakingRewards ARCD balance:", balances[normalizedARCDKey]);
     balances[normalizedARCDKey] = (balances[normalizedARCDKey] / 1e18) * arcdPriceInUSD;
-    console.log("ARCD balance in USD:", balances[normalizedARCDKey]);
+    console.log("StakingRewards ARCD balance in USD:", balances[normalizedARCDKey]);
     } else {
         console.log("No ARCD balance found");
     }
 
     if (balances[normalizedWETHKey] !== undefined) {
-        console.log("WETH balance before conversion:", balances[normalizedWETHKey]);
+        console.log("StakingRewards WETH balance:", balances[normalizedWETHKey]);
         balances[normalizedWETHKey] = (balances[normalizedWETHKey] / 1e18) * wethPriceInUSD;
-        console.log("WETH balance in USD:", balances[normalizedWETHKey]);
+        console.log("StakingRewards WETH balance in USD:", balances[normalizedWETHKey]);
     } else {
         console.log("No WETH balance found");
   }
 
   // get the SINGLE_SIDED_STAKING total supply
   const totalSupplySingleSided = await getTotalSupply(SINGLE_SIDED_STAKING, SINGLE_SIDED_STAKING_ABI, block);
+  console.log("SingleSidedStaking ARCD balance:", totalSupplySingleSided);
   // convert it to USD
   const totalSupplySingleSidedUSD = (totalSupplySingleSided / 1e18) * arcdPriceInUSD;
+  console.log("SingleSidedStaking ARCD balance in USD:", totalSupplySingleSidedUSD);
   // add it to the balances
   sdk.util.sumSingleBalance(balances, `${CHAIN}:${SINGLE_SIDED_STAKING}`, totalSupplySingleSidedUSD);
 
