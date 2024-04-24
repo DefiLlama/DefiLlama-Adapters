dcc4e3d4488c36e8d3152f0e8ff6cb9415f38451
Egor Polyakov <poegva@mail.ru>
2024-04-24 15:02:14 +0800

    Update Cega V2 Adapter (#9953)
    * Update CegaAdapter
    
    * fix
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/cega-v2/index.js
+++ projects/cega-v2/index.js
@@ -1,17 +1,35 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { nullAddress, sumTokensExport } = require('../helper/unwrapLPs')
+const ADDRESSES = require("../helper/coreAssets.json");
+const { nullAddress, sumTokensExport } = require("../helper/unwrapLPs");
 
 module.exports = {
   ethereum: {
     tvl: sumTokensExport({
-        tokens:[nullAddress, ADDRESSES.ethereum.WSTETH, ADDRESSES.ethereum.USDC, ADDRESSES.ethereum.USDT, ADDRESSES.ethereum.WBTC],
-        owners: ["0xA8AB795731fbBFDd1Fbc57ca11e6f722e7783642"]
-    })
+      tokens: [
+        nullAddress,
+        ADDRESSES.ethereum.WSTETH,
+        ADDRESSES.ethereum.USDC,
+        ADDRESSES.ethereum.USDT,
+        ADDRESSES.ethereum.WBTC,
+        ADDRESSES.ethereum.DAI,
+        ADDRESSES.ethereum.USDe,
+        "0x9D39A5DE30e57443BfF2A8307A4256c8797A3497",
+      ],
+      owners: ["0xA8AB795731fbBFDd1Fbc57ca11e6f722e7783642"],
+    }),
   },
   arbitrum: {
     tvl: sumTokensExport({
-        tokens:[nullAddress, ADDRESSES.arbitrum.WSTETH, ADDRESSES.arbitrum.USDC_CIRCLE, ADDRESSES.arbitrum.USDT, ADDRESSES.arbitrum.WBTC],
-        owners: ["0x475c4af369b28997b25bd756ef92797ad3f69593"]
-    })
-  }
-}
+      tokens: [
+        nullAddress,
+        ADDRESSES.arbitrum.WSTETH,
+        ADDRESSES.arbitrum.USDC_CIRCLE,
+        ADDRESSES.arbitrum.USDT,
+        ADDRESSES.arbitrum.WBTC,
+        ADDRESSES.arbitrum.DAI,
+        ADDRESSES.arbitrum.USDe,
+        '0x211Cc4DD073734dA055fbF44a2b4667d5E5fE5d2',
+      ],
+      owners: ["0x475c4af369b28997b25bd756ef92797ad3f69593"],
+    }),
+  },
+};
