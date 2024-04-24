3fbddc84e756412b1aff6954b4ae05b559193b69
Aaric <aaricchen@gmail.com>
2024-04-24 17:37:14 +0800

    feat: add rize project

--- projects/rize/index.js
+++ projects/rize/index.js
@@ -1,22 +1,20 @@
-const ADDRESSES = require("../helper/coreAssets.json");
-const target = "0x02d88035861f4d6e1b3b7a0f74771fe14ce31d7c";
-const sdk = require("@defillama/sdk");
+const addresses = require('../helper/coreAssets.json')
+const axios = require("axios");
+const BigNumber = require("bignumber.js");
 
-async function eth(timestamp, ethBlock, chainBlocks) {
-  const pooledETH = await sdk.api.abi.call({
-    block: ethBlock,
-    target: target,
-    abi: "uint256:getTotalPooledEther",
-  });
-
-  return {
-    [ADDRESSES.ethereum.ETH]: pooledETH.output,
-  };
+async function tvl(_, _1, _2, {api}) {
+    const result = await axios.get("https://referral-dev.m-safe-dev.link/tvl")
+    const data = result.data
+    return {
+        [`ethereum:${addresses.null}`]: new BigNumber(data.coins.find(it => it.coinType === "ethereum").amount).times(1e18).toString(),
+        [`ethereum:${addresses.ethereum.USDT}`]: new BigNumber(data.coins.find(it => it.coinType === "usdt").amount).times(1e6).toString(),
+        [`ethereum:${addresses.ethereum.USDC}`]: new BigNumber(data.coins.find(it => it.coinType === "usdc").amount).times(1e6).toString(),
+    }
 }
 
 module.exports = {
-  timetravel: false,
-  ethereum: {
-    tvl: eth,
-  },
+    start: 1713892926,
+    ethereum: {
+        tvl
+    },
 };
