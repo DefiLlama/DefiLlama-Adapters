8cfd3d272def0c4d76cf764e4ebf686c92250c99
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-25 10:29:29 +0200

    code refactor

--- projects/bitgenie/index.js
+++ projects/bitgenie/index.js
@@ -1,5 +1,3 @@
-const { stakings } = require("../helper/staking");
-
 const stakingContracts = [
   "0xd47477da78F6252b1029f3041aC4FC749439CD3F",
   "0xe3F7194AB9cDa2E91bBf0AC7F450Ac7a60B40901",
@@ -9,15 +7,15 @@ const stakingContracts = [
   "0x63C8860e93697dB85716C9ee7F15D09EAC62136C"
 ];
 
-const stakingTokens = [
-    "0x5c46bFF4B38dc1EAE09C5BAc65872a1D8bc87378",
-    "0xB880fd278198bd590252621d4CD071b1842E9Bcd",
-    "0xF6D226f9Dc15d9bB51182815b320D3fBE324e1bA"
-];
 
 module.exports = {
   merlin: {
-    staking: stakings(stakingContracts, stakingTokens),
-    tvl: () => ({}),
+    tvl,
   },
-};
+}
+
+async function tvl(api) {
+  const tokens = await api.multiCall({  abi: 'address:stakeToken', calls: stakingContracts})
+  return api.sumTokens({ tokensAndOwners2: [tokens, stakingContracts]})
+  
+}
