c6771fe9d1e669ed5527adf99d9013266d3f70f5
Your Name <you@example.com>
2024-04-26 13:14:32 +0800

    Add new contracts

--- projects/bitgenie/index.js
+++ projects/bitgenie/index.js
@@ -1,23 +1,23 @@
-const { stakings } = require("../helper/staking");
-
 const stakingContracts = [
   "0xd47477da78F6252b1029f3041aC4FC749439CD3F",
   "0xe3F7194AB9cDa2E91bBf0AC7F450Ac7a60B40901",
   "0x53Bd1b82363d93dc9344FfBB764d2713cd5628cC",
   "0x2e1f9186912142341B2fc40b32b99Ea9fE1b730B",
   "0x237826695a575D9Fd2FA7C184E5f37593aBc1529",
-  "0x63C8860e93697dB85716C9ee7F15D09EAC62136C"
+  "0x63C8860e93697dB85716C9ee7F15D09EAC62136C",
+  "0x19a0D0b1734bdB167D393e7BB7a57418e3a8cCA8",
+  "0x1DBa95577DFe76d8e86af00aEB614479cb7917D2"
 ];
 
-const stakingTokens = [
-    "0x5c46bFF4B38dc1EAE09C5BAc65872a1D8bc87378",
-    "0xB880fd278198bd590252621d4CD071b1842E9Bcd",
-    "0xF6D226f9Dc15d9bB51182815b320D3fBE324e1bA"
-];
 
 module.exports = {
   merlin: {
-    staking: stakings(stakingContracts, stakingTokens),
-    tvl: () => ({}),
+    tvl,
   },
-};
+}
+
+async function tvl(api) {
+  const tokens = await api.multiCall({  abi: 'address:stakeToken', calls: stakingContracts})
+  return api.sumTokens({ tokensAndOwners2: [tokens, stakingContracts]})
+  
+}
