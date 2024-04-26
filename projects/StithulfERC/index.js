63171872a7f6c673735f2d90ff87a82ad5a5c32c
Project Stithulf <72882813+stithulf@users.noreply.github.com>
2024-04-27 01:07:31 +0530

    Update index.js

--- projects/StithulfERC/index.js
+++ projects/StithulfERC/index.js
@@ -1,8 +1,16 @@
 const { getUniTVL } = require('../helper/unknownTokens')
+
 const config = {
   bsc: '0x9df9de5ed89adbbd9fa2c14691903a0de9048a87',
 }
 
+async function bscTVL() {
+  // Implement logic to fetch TVL data from the bsc protocol
+  const tvl = await fetchTvlFromBscApi();
+  return tvl;
+}
+
 module.exports = {
+  tvl: bscTVL,
   misrepresentedTokens: false,
 }
