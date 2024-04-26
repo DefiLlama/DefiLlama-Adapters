6594f8e17d613b567fc9644279d436133ed13f91
Project Stithulf <72882813+stithulf@users.noreply.github.com>
2024-04-27 00:52:37 +0530

    Create index.js

---
+++ projects/StithulfERC/index.js
@@ -0,0 +1,8 @@
+const { getUniTVL } = require('../helper/unknownTokens')
+const config = {
+  bsc: '0x9df9de5ed89adbbd9fa2c14691903a0de9048a87',
+}
+
+module.exports = {
+  misrepresentedTokens: false,
+}
