ce1640e78f069ce5766b13252e212e84b850c9cb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-27 18:44:47 +0200

    code refactor

--- projects/eigenpie/index.js
+++ projects/eigenpie/index.js
@@ -1,31 +1,14 @@
 const config = require("./config");
+const ADDRESSES = require('../helper/coreAssets.json')
 
 async function tvl(api) {
   const { eigenConfig, eigenStaking } = config[api.chain];
 
-  const tokens = await api.call({ abi: 'address[]:getSupportedAssetList', target: eigenConfig, });
+  let tokens = await api.call({ abi: 'address[]:getSupportedAssetList', target: eigenConfig, });
   const mlrttokens = await api.multiCall({ abi: 'function mLRTReceiptByAsset(address) view returns (address)', calls: tokens, target: eigenConfig })
-  const tokenBalances = []; 
-  for (let i = 0; i < mlrttokens.length; i++) {
-        const token = mlrttokens[i];
-
-        try {
-            const result = await api.call({ abi: 'uint256:totalSupply', target: token });
-            tokenBalances.push(result);
-        } catch (error) {
-            console.error(`Error fetching balance for token ${token}:`, error);
-        }
-      }
-      const oldAddress = '0xeFEfeFEfeFeFEFEFEfefeFeFefEfEfEfeFEFEFEf';  //convert native token to get price for native restaking
-      const newAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
-      
-      for (let i = 0; i < tokens.length; i++) {
-          const currentAddress = tokens[i];
-          if (currentAddress.toLowerCase() === oldAddress.toLowerCase()) {
-              tokens[i] = newAddress;
-          }
-      }
- api.add(tokens, tokenBalances)
+  const tokenSupplies = await api.multiCall({  abi: 'uint256:totalSupply', calls: mlrttokens})
+  tokens = tokens.map(token => token.toLowerCase() === '0xeFEfeFEfeFeFEFEFEfefeFeFefEfEfEfeFEFEFEf'.toLowerCase() ?  ADDRESSES.null : token)
+  api.add(tokens, tokenSupplies)
 }
 
 Object.keys(config).forEach((chain) => {
