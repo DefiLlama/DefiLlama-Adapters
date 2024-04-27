b4599800f7cf658089b8df4f13e8a7648202a66d
surf-rd <rd@surf.one>
2024-04-27 11:46:13 +0800

    Count all liquidity

--- projects/surfone/index.js
+++ projects/surfone/index.js
@@ -2,6 +2,7 @@ const factoryContract = '0x5FeD7c030a1B3b40988984479Fdd666dE81038A3'
 const positionContract = '0x1fa9702e774D31aB661D84f449b0Aa22c41D6827'
 const merlinPoolContract = '0x69d2AbBCef322afcDAED52238f43882cEe3ACC08'
 const merlinPositionContract = '0x7A3D0eBb547001E769F2A7AE3f0D7a9b3078F3C1'
+const merlinConfigContract = '0xa3979d2A94F17196A9728A17765e5BD315CdacFb'
 const ADDRESSES = require('../helper/coreAssets.json')
 
 async function tvlBase(api) {
@@ -12,8 +13,9 @@ async function tvlBase(api) {
 }
 
 async function merlinTvl(api) {
-  const SolvBTC = "0x41D9036454BE47d3745A823C4aaCD0e29cFB0f71"
-  return api.sumTokens({ owners:[merlinPoolContract,merlinPositionContract], tokens: [ADDRESSES.merlin.WBTC, ADDRESSES.merlin.WBTC_1, SolvBTC]})
+  const tokens = await api.call({ abi: abi.getLiquidityTokens, target: merlinConfigContract })
+  const lpTokens = tokens.map(i => [i.indexToken])
+  return api.sumTokens({owners:[merlinPoolContract,merlinPositionContract], tokens: lpTokens})
 }
 
 module.exports = {
@@ -23,5 +25,6 @@ module.exports = {
 }
 
 const abi = {
-  "getAllPools": "function getAllPools() view returns (tuple(address baseToken, address spotToken, bytes32 spotTokenKey,string baseName,string spotName,uint24 feeP,address pool,uint256 index,uint8 groupIndex, uint256 chainId,bool reverse,uint256 flag)[])"
+  "getAllPools": "function getAllPools() view returns (tuple(address baseToken, address spotToken, bytes32 spotTokenKey,string baseName,string spotName,uint24 feeP,address pool,uint256 index,uint8 groupIndex, uint256 chainId,bool reverse,uint256 flag)[])",
+  "getLiquidityTokens": "function getLiquidityTokens() view returns (tuple(uint256 tokenIndex, string symbol, bytes32 spotTokenKey, address indexToken, uint8 indexTokenDecimals, address lpToken, address[] collateralToken, uint256 tokenWeight, uint256 maxGlobalShortSize, uint256 maxGlobalLongSize, uint256 maxDailyRemoveSize, uint256 minAddLiquiditySize)[])"
 }
