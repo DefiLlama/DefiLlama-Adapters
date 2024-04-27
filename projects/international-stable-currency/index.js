d2af251386074561b7c6f69a7301d1e06edb6121
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-27 18:55:53 +0200

    code refactor

--- projects/international-stable-currency/index.js
+++ projects/international-stable-currency/index.js
@@ -1,22 +1,18 @@
 const { PublicKey } = require("@solana/web3.js");
 const { getConnection, decodeAccount } = require("../helper/solana");
 
-async function tvl() {
-    const connection = getConnection();
-    const mint = await connection.getAccountInfo(
-        new PublicKey("J9BcrQfX4p9D1bvLzRNCbMDv8f44a9LFdeqNE4Yk2WMD")
-    );
-    const mintInfo = decodeAccount("mint", mint);
-    const supply = mintInfo.supply.toString() / 10 ** mintInfo.decimals;
-    return {
-        "international-stable-currency": supply,
-    };
+async function tvl(api) {
+  const connection = getConnection();
+  const ISC = "J9BcrQfX4p9D1bvLzRNCbMDv8f44a9LFdeqNE4Yk2WMD"
+  const mint = await connection.getAccountInfo(new PublicKey(ISC))
+  const mintInfo = decodeAccount("mint", mint);
+  api.add(ISC, mintInfo.supply)
 }
 
 module.exports = {
-    timetravel: false,
-    methodology: "Supply of ISC multiplied by the price of ISC in USD.",
-    solana: {
-        tvl,
-    },
+  timetravel: false,
+  methodology: "Supply of ISC multiplied by the price of ISC in USD.",
+  solana: {
+    tvl,
+  },
 };
