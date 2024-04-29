743c1d4bbd05d7dd242587023d0e85d632243adb
c40003141 <117669273+c40003141@users.noreply.github.com>
2024-04-29 20:51:56 +0200

    d8x: track xlayer (#2)
    Co-authored-by: m66260 <m66260@protonmail.com>

--- projects/d8x/index.js
+++ projects/d8x/index.js
@@ -1,23 +1,34 @@
+const { sumUnknownTokens } = require('../helper/unknownTokens')
 const abi = require("./abi-poolInfo.json");
 
-const D8X_PERPETUALS_CONTRACT = "0xaB7794EcD2c8e9Decc6B577864b40eBf9204720f";
+const config = {
+  polygon_zkevm: { factory: "0xaB7794EcD2c8e9Decc6B577864b40eBf9204720f", fromBlock: 6182628 },
+  xlayer: { factory: "0xb24dB543749277E8625a59C061aE7574C8235475", fromBlock: 385678 },
+};
 
 async function tvl(api) {
+  const { factory } = config[api.chain];
   const exchangeInfo = await api.call({
     abi: abi.getPoolStaticInfo,
-    target: D8X_PERPETUALS_CONTRACT,
+    target: factory,
     params: [1, 255],
+    useDefaultCoreAssets: true,
   });
 
   const marginTokens = exchangeInfo[2];
-  return api.sumTokens({ owner: D8X_PERPETUALS_CONTRACT, tokens: marginTokens });
+  return sumUnknownTokens({ api, owner: factory, tokens: marginTokens, useDefaultCoreAssets: true });
 }
 
-module.exports = {
-  methodology:
-    "adds up the balances of all liquidity pools in the D8X exchange",
-  start: 6182628,
-  polygon_zkevm: {
-    tvl,
-  },
+// Build the exports object
+const exportsData = {
+  methodology: "adds up the balances of all liquidity pools in the D8X exchange",
 };
+
+Object.keys(config).forEach(chain => {
+  exportsData[chain] = {
+    tvl: (api) => tvl(api),
+    start: config[chain].fromBlock,
+  };
+});
+
+module.exports = exportsData;
