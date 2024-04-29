73d13d00c2c6c7197984d891dca5a5065eb1a573
LowPolyDuck <97544154+LowPolyDuck@users.noreply.github.com>
2024-04-29 12:34:10 -0400

    Create index.js
    Adding Mezo tvl

---
+++ projects/Mezo/index.js
@@ -0,0 +1,20 @@
+const ADDRESSES = require('../helper/coreAssets.json');
+const { sumTokens } = require('../helper/sumTokens');
+const { nullAddress } = require("../helper/treasury");
+
+const bridge = "0xAB13B8eecf5AA2460841d75da5d5D861fD5B8A39";
+
+async function tvl(api) {
+    const balances = {};
+    await sumTokens({
+        api,
+        balances,
+        owners: [bridge],
+        tokens: [ADDRESSES.ethereum.tBTC, ADDRESSES.ethereum.WBTC, nullAddress]
+    })
+    return balances
+}
+
+module.exports = {
+    ethereum: { tvl }
+}
