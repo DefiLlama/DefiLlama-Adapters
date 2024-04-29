574684d00e81d125d94947517b71012c52a962aa
sui <>
2024-04-29 11:52:12 +0800

    add zestswap adaptor

---
+++ projects/zestswap/index.js
@@ -0,0 +1,9 @@
+const { getUniTVL } = require('../helper/unknownTokens')
+
+module.exports={
+    misrepresentedTokens: true,
+    methodology: "Factory address (0xda36B416102E0C9Ff562bA2daa7836242caeD9EF) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
+    cronos: {
+        tvl: getUniTVL({ factory: '0xda36B416102E0C9Ff562bA2daa7836242caeD9EF', chain: 'xlayer', useDefaultCoreAssets: true }),
+    }
+}
