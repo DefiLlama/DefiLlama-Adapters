8b5ed2a81c6abc2d3089696d979894b0060d9065
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-30 08:35:07 +0200

    code refactor

--- projects/nxd-protocol/index.js
+++ projects/nxd-protocol/index.js
@@ -1,8 +1,9 @@
+const { sumUnknownTokens } = require('../helper/unknownTokens')
+
 const NXD_PROTOCOL_ADDRESS = '0xE05430D42842C7B757E5633D19ca65350E01aE11';
 const DXN_ERC20_ADDRESS = '0x80f0C1c49891dcFDD40b6e0F960F84E6042bcB6F';
 const NXD_ERC20_ADDRESS = '0x70536D44820fE3ddd4A2e3eEdbC937b8B9D566C7';
 const NXD_STAKING_VAULT_ADDRESS = '0xa1B56E42137D06280E34B3E1352d80Ac3BECAF79';
-const NXD_DXN_UNIV2_POOL = '0x98134CDE70ff7280bb4b9f4eBa2154009f2C13aC';
 const TAX_RECIPIENT = '0x61e0318C8d5A855D63D989b02D859Aae5020b308';
 const ABI = {
   'totalDXNDepositedLMP':
@@ -10,59 +11,23 @@ const ABI = {
   'totalDXNStaked': 'function totalDXNStaked() view returns (uint256)',
   'dxnStaked': 'function dxnStaked() view returns (uint256)',
 };
-const lpReservesAbi =
-  'function getReserves() view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)';
 
-async function tvl(_, _1, _2, { api }) {
-  const nxdStaked = await api.call({
-    abi: 'erc20:balanceOf',
-    target: NXD_ERC20_ADDRESS,
-    params: [NXD_STAKING_VAULT_ADDRESS],
-  });
-  const dxnStakedLMP = await api.call({
-    abi: ABI.totalDXNDepositedLMP,
-    target: NXD_PROTOCOL_ADDRESS,
-    params: [],
-  });
-  const dxnStakedAfterLMP = await api.call({
-    abi: ABI.totalDXNStaked,
-    target: NXD_PROTOCOL_ADDRESS,
-    params: [],
-  });
-  const dxnStakedFoT = await api.call({
-    abi: ABI.dxnStaked,
-    target: TAX_RECIPIENT,
-    params: [],
-  });
+async function staking(api) {
+  const dxnStakedLMP = await api.call({ abi: ABI.totalDXNDepositedLMP, target: NXD_PROTOCOL_ADDRESS, });
+  const dxnStakedAfterLMP = await api.call({ abi: ABI.totalDXNStaked, target: NXD_PROTOCOL_ADDRESS, });
+  const dxnStakedFoT = await api.call({ abi: ABI.dxnStaked, target: TAX_RECIPIENT, });
 
-  // NXD is untracked on DefiLlama, so we need to get the reserves of the pool to get NXD price in DXN.
-  const nxdDxnReserves = await api.call({
-    abi: lpReservesAbi,
-    target: NXD_DXN_UNIV2_POOL,
-    params: [],
-  });
+  await sumUnknownTokens({ api, owner: NXD_STAKING_VAULT_ADDRESS, lps: ['0x98134CDE70ff7280bb4b9f4eBa2154009f2C13aC',], tokens: [NXD_ERC20_ADDRESS], coreAssets: [DXN_ERC20_ADDRESS], })
 
-  const nxdPriceInDXN = nxdDxnReserves._reserve1 / nxdDxnReserves._reserve0;
-
-  const nxdStakedInDXN = nxdStaked * nxdPriceInDXN;
-
-  api.addTokens(
-    [
-      DXN_ERC20_ADDRESS,
-      DXN_ERC20_ADDRESS,
-      DXN_ERC20_ADDRESS,
-      DXN_ERC20_ADDRESS,
-    ],
-    [dxnStakedLMP, dxnStakedAfterLMP, dxnStakedFoT, nxdStakedInDXN]
-  );
+  api.add(DXN_ERC20_ADDRESS, [dxnStakedLMP, dxnStakedAfterLMP, dxnStakedFoT]);
 }
 
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
+  misrepresentedTokens: true,
   methodology:
     'We count the TVL of NXD Protocol by checking the total amount of NXD staked in the Staking Vault in addition to the total DXN staked by the protocol.',
   ethereum: {
-    tvl,
+    tvl: () => ({}),
+    staking,
   },
 };
