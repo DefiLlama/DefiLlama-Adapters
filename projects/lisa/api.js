2edc2c6d92b07d389c50b4273a1fa5047c8bdc8f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-30 07:16:49 +0200

    fix lisa

--- projects/lisa/api.js
+++ projects/lisa/api.js
@@ -8,9 +8,9 @@ module.exports = {
 async function tvl() {
   const reserveAddr = 'SM26NBC8SFHNW4P1Y4DFH27974P56WN86C92HPEHH.token-lqstx';
   const registryAddr = 'SM26NBC8SFHNW4P1Y4DFH27974P56WN86C92HPEHH.lqstx-mint-registry';
-  const reserve = await call({ reserveAddr, abi: 'get-reserve' });
-  const pending = await call({ registryAddr, abi: 'get-mint-requests-pending-amount'});
+  const pending = await call({ target: registryAddr, abi: 'get-mint-requests-pending-amount'});
+  const reserve = await call({ target: reserveAddr, abi: 'get-reserve' });
   return {
-    blockstack: (reserve.value + pending.value) / 1e6
+    blockstack: (+reserve.value + +pending.toString()) / 1e6
   }
 }
