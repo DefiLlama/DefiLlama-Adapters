6b98805cf59c0648e4912d072eeb1ceb18b1f1c9
biest <93846276+1biest@users.noreply.github.com>
2024-04-29 18:23:20 -0600

    token factory address

--- projects/neptune-finance/index.js
+++ projects/neptune-finance/index.js
@@ -1,17 +1,25 @@
 const { queryContract, getToken, sumTokens, getBalance } = require('../helper/chain/cosmos')
 
 const MARKET_ADDR = 'inj1nc7gjkf2mhp34a6gquhurg8qahnw5kxs5u3s4u';
-const CW20_ADDRS = ['inj18luqttqyckgpddndh8hvaq25d5nfwjc78m56lc'];
 
-async function sumCW20Tokens({ balances = {}, tokens, owner, chain } = {}) {
+const CW20_ADDRS = [
+  ['inj18luqttqyckgpddndh8hvaq25d5nfwjc78m56lc', 'factory:inj14ejqjyq8um4p3xfqj74yld5waqljf88f9eneuk:inj18luqttqyckgpddndh8hvaq25d5nfwjc78m56lc']
+];
+
+async function sumCW20Tokens({
+  balances = {},
+  tokens,
+  owner,
+  chain
+} = {}) {
   // Loop through each token in the tokens array
   await Promise.all(
     tokens.map(async (token) => {
       // Fetch the balance for the token and owner
-      const balance = await getBalance({ token, owner, chain });
+      const balance = await getBalance({ token: token[0], owner, chain });
       // Add the balance to the balances object
       if (!isNaN(balance) && typeof balance === 'number' && isFinite(balance)) {
-        balances[`injective:${token.toString()}`] = balance.toString(); // Convert balance to string to match the format
+        balances[`injective:${token[1].toString()}`] = balance.toString(); // Convert balance to string and append to 2nd token in array
       }
     })
   );
@@ -19,21 +27,36 @@ async function sumCW20Tokens({ balances = {}, tokens, owner, chain } = {}) {
 }
 
 async function tvl(api) {
+  let balances = {};
+
   // Sum the balances of all tokens excluding CW20
-  const tokens = await sumTokens({ chain: api.chain, owner: MARKET_ADDR });
-  console.log("sumTokens", tokens);
+  await sumTokens({
+    balances,
+    chain: api.chain,
+    owner: MARKET_ADDR
+  });
+
   // Sum the balances of CW20 tokens
-  const cw20tokens = await sumCW20Tokens({ chain: api.chain, tokens: CW20_ADDRS, owner: MARKET_ADDR });
-  console.log("cw20tokens", cw20tokens);
-  // Add the two sums together to get the total TVL
-  const total = { ...tokens, ...cw20tokens };
-  console.log("total", total)
-  return total
+  await sumCW20Tokens({
+    balances,
+    chain: api.chain,
+    tokens: CW20_ADDRS,
+    owner: MARKET_ADDR
+  });
+  
+  /* console.log("balances", balances) */
+  return balances
 }
 
 async function borrowed(api) {
   // query market-state
-  const { markets, } = await queryContract({ chain: api.chain, contract: MARKET_ADDR, data: { get_state: {} } })
+  const { markets, } = await queryContract({
+    chain: api.chain,
+    contract: MARKET_ADDR,
+    data: {
+      get_state: {}
+    }
+  })
 
   // get all borrowed
   markets.map(market => {
