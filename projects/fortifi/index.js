3e638fcbd00fefbb7e868e88c56e84943609c9e5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-30 12:17:42 +0200

    code refactor

--- projects/fortifi/index.js
+++ projects/fortifi/index.js
@@ -1,26 +1,21 @@
-const { getConfig } = require('../helper/cache')
+const { nullAddress } = require("../helper/tokenMapping")
+
+const vaults = [
+  '0x432963C721599Cd039ff610Fad447D487380D858', // FortiFi AVAX Stability Vault (ffAvaSV)
+  '0x853e7A9dcc5037cD624834DC5f33151AA49d2D73', // FortiFi WAVAX LST MultiYield (ffWavaxLST)
+]
+
+const abi = {
+  getTokenInfo: "function getTokenInfo(uint256 _tokenId) view returns ((uint256 deposit, ((address strategy, address depositToken, address router, address oracle, bool isFortiFi, bool isSAMS, uint16 bps, uint8 decimals) strategy, uint256 receipt)[] positions))",
+}
 
 async function tvl(api) {
-    // FortiFi MultiYields route user deposits to various YieldYak farms.
-    // This API endpoint calculates the total underlying deposits based on the amount of 
-    // YRT being held by each our our MultiYields.
-    const farms = await getConfig('fortifi/avax', 'https://api.fortifi.pro/tvl');
-    let multiYields = Object.keys(farms);
-    let tokens = [];
-    let vals = [];
-
-    for (let i = 0; i < multiYields.length; i ++) {
-        Object.entries(farms[multiYields[i]]).forEach(strategy => {
-            tokens.push(strategy[1].depositToken);
-            vals.push(strategy[1].depositTokensLocked);
-        })
-    }
-
-    tokens.forEach((token, i) => {
-      if (!token || !vals[i]) return;
-      api.add(token, vals[i])
-    })
+  for (const vault of vaults) {
+    const token = await api.call({ abi: 'address:depositToken', target: vault }).catch(() => nullAddress)
+    const data = await api.fetchList({ lengthAbi: 'nextToken', itemAbi: abi.getTokenInfo, target: vault })
+    api.add(token, data.map(d => d.deposit))
   }
+}
 
 module.exports = {
   avax: {
