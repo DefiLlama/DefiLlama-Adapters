bc7e537d27a7d9f3a75681633d30c3ff1c1e0e11
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-04-30 13:05:24 +0200

    Update index.js

--- projects/penpad/index.js
+++ projects/penpad/index.js
@@ -1,12 +1,10 @@
-const { sumTokensExport } = require('../helper/unwrapLPs');
+
 const ADDRESSES = require('../helper/coreAssets.json');
 const { staking } = require('../helper/staking');
-
-const PENPAD_STAKING_CONTRACT = '0x8F53fA7928305Fd4f78c12BA9d9DE6B2420A2188';
 
 module.exports = {
   methodology: 'Counts liquidty on the staking',
   scroll: {
-    tvl: staking([PENPAD_STAKING_CONTRACT], ADDRESSES.null),
+    tvl: staking('0x8F53fA7928305Fd4f78c12BA9d9DE6B2420A2188, ADDRESSES.null),
   },
-};
+}
