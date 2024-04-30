78227eed9d08fe9ae81cf89166b052d3d75d7916
0xngmi <0xngmi@protonmail.com>
2024-04-30 22:54:58 +0100

    add new etherfi vault

--- projects/etherfi-liquid/index.js
+++ projects/etherfi-liquid/index.js
@@ -1,10 +1,15 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 async function tvl(api) {
-  const bal = await api.call({
+  const balETH = await api.call({
     abi: "uint256:totalAssets",
     target: '0xea1a6307d9b18f8d1cbf1c3dd6aad8416c06a221',
   });
-  api.add(ADDRESSES.ethereum.EETH, bal);
+  api.add(ADDRESSES.ethereum.EETH, balETH);
+  const balUSD = await api.call({
+    abi: "uint256:totalSupply",
+    target: '0x08c6f91e2b681faf5e17227f2a44c307b3c1364c',
+  });
+  api.add(ADDRESSES.ethereum.USDC, balUSD);
 }
 
 module.exports = {
