d3ed094516d65e5eaf0e7de047eba1a0888bd0f8
Define101 <93603962+Define101@users.noreply.github.com>
2024-04-30 14:21:31 +0100

    git push origin fix-penpad-server (#10052)

--- projects/penpad/index.js
+++ projects/penpad/index.js
@@ -5,6 +5,6 @@ const { staking } = require('../helper/staking');
 module.exports = {
   methodology: 'Counts liquidty on the staking',
   scroll: {
-    tvl: staking('0x8F53fA7928305Fd4f78c12BA9d9DE6B2420A2188, ADDRESSES.null),
+    tvl: staking('0x8F53fA7928305Fd4f78c12BA9d9DE6B2420A2188', ADDRESSES.null),
   },
 }
