74d71bd19e10a0360f6ba36b5ba2c1232811bbf3
olivblad <olivier.bladier@atlendislabs.io>
2024-04-30 14:49:05 +0200

    new adapter for Atlendis protocol to monitor v2

--- projects/atlendis-v2/index.js
+++ projects/atlendis-v2/index.js
@@ -1,4 +1,4 @@
-const { GraphQLClient, } = require("graphql-request");
+const { GraphQLClient } = require("graphql-request");
 const query = `
   query tvl($chainId: Int!) {
     rcls(chainId: $chainId) {
@@ -19,7 +19,7 @@ const query = `
       }
     }
   }
-`
+`;
 
 // Atlendis' indexer url
 const atlendisUrl = "https://atlendis.herokuapp.com/graphql";
@@ -27,29 +27,29 @@ const atlendisUrl = "https://atlendis.herokuapp.com/graphql";
 const supportedChains = { polygon: 137, mode: 34443 };
 
 async function tvl(api) {
-  const chain = api.chain
+  const chain = api.chain;
   const graphQLClient = new GraphQLClient(atlendisUrl);
   const chainId = supportedChains[chain];
-  const { rcls, } = await graphQLClient.request(query, { chainId });
-  const tokensAndOwners = rcls.map(rcl => [rcl.token.address, rcl.address])
-  return api.sumTokens({ tokensAndOwners })
+  const { rcls } = await graphQLClient.request(query, { chainId });
+  const tokensAndOwners = rcls.map((rcl) => [rcl.token.address, rcl.address]);
+  return api.sumTokens({ tokensAndOwners });
 }
 
 async function borrowed(api) {
-  const chain = api.chain
+  const chain = api.chain;
   const graphQLClient = new GraphQLClient(atlendisUrl);
   const chainId = supportedChains[chain];
   const { rcls, bulletLoans } = await graphQLClient.request(query, { chainId });
 
   for (let rcl of rcls) {
     if (rcl.currentLoan) {
-      api.add(rcl.token.address, rcl.currentLoan.borrowedAmount)
+      api.add(rcl.token.address, rcl.currentLoan.borrowedAmount);
     }
   }
 
   for (let loan of bulletLoans) {
     if (loan.issuedLoan) {
-      api.add(loan.token.address, loan.tvl)
+      api.add(loan.token.address, loan.tvl);
     }
   }
 }
@@ -62,6 +62,6 @@ module.exports = {
   ],
 };
 
-Object.keys(supportedChains).forEach(chain => {
-  module.exports[chain] = { tvl, borrowed }
-})
+Object.keys(supportedChains).forEach((chain) => {
+  module.exports[chain] = { tvl, borrowed };
+});
