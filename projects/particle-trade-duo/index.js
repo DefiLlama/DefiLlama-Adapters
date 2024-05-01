b19aa7c0485bbc67b5f56c05f0b2daaf040b262b
particle-deployer <130306380+particle-deployer@users.noreply.github.com>
2024-05-01 04:58:18 -0700

    when native token is locked, wrapped token is generated for tvl tracking (#10062)
    * when native token is locked, wrapped token is generated for tvl tracking
    
    * minor fix
    
    ---------
    
    Co-authored-by: Wukong Particle <wukong@wukong-mbp.local>
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/particle-trade-duo/index.js
+++ projects/particle-trade-duo/index.js
@@ -9,6 +9,20 @@ const config = {
   },
 }
 
+const wrappedToken = {
+  blast: {
+    duoEth: '0x1Da40C742F32bBEe81694051c0eE07485fC630f6',
+    duoUsd: '0x1A3D9B2fa5c6522c8c071dC07125cE55dF90b253',
+  }
+}
+
+const wrappedNativeTokenMap = {
+  blast: {
+    '0x1Da40C742F32bBEe81694051c0eE07485fC630f6': '0x66714DB8F3397c767d0A602458B5b4E3C0FE7dd1',
+    '0x1A3D9B2fa5c6522c8c071dC07125cE55dF90b253': '0x866f2C06B83Df2ed7Ca9C2D044940E7CD55a06d6',
+  }
+}
+
 Object.keys(config).forEach(chain => {
   module.exports[chain] = {
     tvl: async (api) => {
@@ -20,6 +34,12 @@ Object.keys(config).forEach(chain => {
       const principal = await api.multiCall({  abi: 'uint256:principal', calls: managers})
       api.add(tokens, yields)
       api.add(tokens, principal)
+
+      let wrappedTokens = wrappedToken[chain]
+      wrappedTokens = Object.values(wrappedTokens)
+      const wrappedBalances = await api.multiCall({  abi: 'uint256:totalSupply', calls: wrappedTokens})
+      const nativeTokens = wrappedTokens.map(wrappedToken => wrappedNativeTokenMap[chain][wrappedToken])
+      api.add(nativeTokens, wrappedBalances);
     }
   }
 })
