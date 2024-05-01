80031d31e193e58cbbb8997b2b51aedd7d38456b
ramenforbreakfast <ramenforbreakfast@protonmail.com>
2024-05-01 16:50:39 -0700

    feat: update prePO TVL

--- projects/prePo/index.js
+++ projects/prePo/index.js
@@ -1,34 +1,37 @@
-const { getLogs } = require('../helper/cache/getLogs');
-const { sumTokens2 } = require('../helper/unwrapLPs')
-const { getUniqueAddresses } = require('../helper/utils')
-const config = {
-  arbitrum: { factory: '0x6f889e3fce9b13fe8cefa068a48f4074292e663c', fromBlock: 70478558 },
+const { getLogs } = require("../helper/cache/getLogs")
+const { sumTokens2 } = require("../helper/unwrapLPs")
+const BLAST_MARKETS = "0xB40DBBb7931Cfef8Be73AEEC6c67d3809bD4600B"
+const FROM_BLOCK = 2890000
+
+async function tvl(timestamp, _, _1, { api }) {
+  const logs = await getLogs({
+    api,
+    target: BLAST_MARKETS,
+    topics: [
+      "0xb5a3664443cdf63934025ca85796acceaccbdc33b3e93ba963df1fc863c58086",
+    ],
+    eventAbi: "event MarketCreated(bytes32 indexed, address, address, uint256)",
+    onlyArgs: true,
+    fromBlock: FROM_BLOCK,
+  })
+  const uniqueCollateral = new Set()
+  logs.forEach((log) => uniqueCollateral.add(log[2]))
+  const collateralAddresses = Array.from(uniqueCollateral)
+  // Map each unique address with the BLAST_MARKETS constant
+  const tokensAndOwners = collateralAddresses.map((address) => [
+    address,
+    BLAST_MARKETS,
+  ])
+  // Call sumTokens2 function with dynamically generated tokensAndOwners array
+  return sumTokens2({
+    api,
+    tokensAndOwners: tokensAndOwners,
+  })
 }
 
-module.exports = {}
-
-Object.keys(config).forEach(chain => {
-  const { fromBlock, factory, } = config[chain]
-  module.exports[chain] = {
-    tvl: async (api) => {
-      const logs = await getLogs({
-        api,
-        target: factory,
-        topics: ['0xe56f19ada061bf6161817694d647d94134afe9d3e877db1d8118b3012b744635'],
-        eventAbi: 'event MarketCreation(address, address, address, uint256, uint256, uint256, uint256, uint256 expiryTime)',
-        onlyArgs: true,
-        fromBlock: fromBlock,
-      })
-      const markets = logs.map(i => i[0])
-      let tokens = await api.multiCall({ abi: 'address:getCollateral', calls: markets })
-      let wrappedTokens = getUniqueAddresses(tokens)
-      const tokenNames = await api.multiCall({ abi: 'string:name', calls: wrappedTokens })
-      wrappedTokens = wrappedTokens.filter((v, i) => tokenNames[i].startsWith('prePO'))
-      let baseTokens = await api.multiCall({ abi: 'address:getBaseToken', calls: wrappedTokens })
-      const tokensAndOwners = baseTokens.map((v, i) => [v, wrappedTokens[i]])
-      markets.forEach((v, i) => tokensAndOwners.push([tokens[i], v]))
-      return sumTokens2({ api, tokensAndOwners, blacklistedTokens: wrappedTokens })
-    }
-  }
-})
-
+module.exports = {
+  misrepresentedTokens: false,
+  blast: {
+    tvl,
+  },
+}
