9a293b18f20cf7e8744e1e18af380c01fdc511ce
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-01 10:33:11 +0200

    code refactor

--- projects/atlendis-v2/index.js
+++ projects/atlendis-v2/index.js
@@ -1,113 +1,67 @@
-const { GraphQLClient, gql } = require("graphql-request");
+const { GraphQLClient, } = require("graphql-request");
+const query = `
+  query tvl($chainId: Int!) {
+    rcls(chainId: $chainId) {
+      id
+     address
+      tvl
+      token {
+        address
+      }
+      currentLoan {accruedInterests borrowedAmount }
+    }
+    bulletLoans(chainId: $chainId) {
+      id
+     address
+      tvl
+      token {
+        address
+      }
+    }
+  }
+`
 
 // Atlendis' indexer url
 const atlendisUrl = "https://atlendis.herokuapp.com/graphql";
 // Atlendis V2 currently supports two chains: Polygon and Mode
 const supportedChains = { polygon: 137, mode: 34443 };
 
-async function getTvl(chain) {
+async function tvl(api) {
+  const chain = api.chain
   const graphQLClient = new GraphQLClient(atlendisUrl);
-  const query = gql`
-    query tvl($chainId: Int!) {
-      rcls(chainId: $chainId) {
-        tvl
-        token {
-          address
-        }
-      }
-      bulletLoans(chainId: $chainId) {
-        tvl
-        token {
-          address
-        }
-      }
-    }
-  `;
   const chainId = supportedChains[chain];
-  const { rcls, bulletLoans } = await graphQLClient.request(query, { chainId });
-
-  const balances = {};
-  for (let pool of [...rcls, ...bulletLoans]) {
-    if (!balances[`${chain}:${pool.token.address}`]) {
-      balances[`${chain}:${pool.token.address}`] = pool.tvl;
-    } else {
-      balances[`${chain}:${pool.token.address}`] =
-        balances[`${chain}:${pool.token.address}`] + pool.tvl;
-    }
-  }
-
-  return balances;
+  const { rcls, } = await graphQLClient.request(query, { chainId });
+  const tokensAndOwners = rcls.map(rcl => [rcl.token.address, rcl.address])
+  return api.sumTokens({ tokensAndOwners })
 }
 
-async function getBorrowed(chain) {
+async function borrowed(api) {
+  const chain = api.chain
   const graphQLClient = new GraphQLClient(atlendisUrl);
-  const query = gql`
-    query borrowed($chainId: Int!) {
-      rcls(chainId: $chainId) {
-        currentLoan {
-          borrowedAmount
-        }
-        token {
-          address
-        }
-      }
-      bulletLoans(chainId: $chainId) {
-        issuedLoan {
-          borrowedAmount
-        }
-        token {
-          address
-        }
-      }
-    }
-  `;
-
   const chainId = supportedChains[chain];
   const { rcls, bulletLoans } = await graphQLClient.request(query, { chainId });
-
-  const balances = {};
 
   for (let rcl of rcls) {
     if (rcl.currentLoan) {
-      if (!balances[`${chain}:${rcl.token.address}`]) {
-        balances[`${chain}:${rcl.token.address}`] =
-          rcl.currentLoan.borrowedAmount;
-      } else {
-        balances[`${chain}:${rcl.token.address}`] =
-          balances[`${chain}:${rcl.token.address}`] +
-          rcl.currentLoan.borrowedAmount;
-      }
+      api.add(rcl.token.address, rcl.currentLoan.borrowedAmount)
     }
   }
 
   for (let loan of bulletLoans) {
     if (loan.issuedLoan) {
-      if (!balances[`${chain}:${loan.token.address}`]) {
-        balances[`${chain}:${loan.token.address}`] =
-          loan.issuedLoan.borrowedAmount;
-      } else {
-        balances[`${chain}:${loan.token.address}`] =
-          balances[`${chain}:${loan.token.address}`] +
-          loan.issuedLoan.borrowedAmount;
-      }
+      api.add(loan.token.address, loan.tvl)
     }
   }
-
-  return balances;
 }
 
 module.exports = {
-  start: 1686642643000,
-  polygon: {
-    tvl: () => getTvl("polygon"),
-    borrowed: () => getBorrowed("polygon"),
-  },
-  mode: {
-    tvl: () => getTvl("mode"),
-    borrowed: () => getBorrowed("mode"),
-  },
+  start: 1686642643,
   hallmarks: [
     [1702367571, "Launch of Fluna V2 Pool on Polygon"],
     [1713855195, "Launch of Arjan pool on Mode Network"],
   ],
 };
+
+Object.keys(supportedChains).forEach(chain => {
+  module.exports[chain] = { tvl, borrowed }
+})
