e4160b58ba798eda85c9d553a181e0f8e32a7468
olivblad <olivier.bladier@atlendislabs.io>
2024-05-02 10:29:47 +0200

    changed after refacto

--- projects/atlendis-v2/index.js
+++ projects/atlendis-v2/index.js
@@ -1,21 +1,24 @@
 const { GraphQLClient } = require("graphql-request");
 const query = `
-  query tvl($chainId: Int!) {
+  query tvlAndBorrow($chainId: Int!) {
     rcls(chainId: $chainId) {
-      id
-     address
+      address
       tvl
       token {
         address
       }
-      currentLoan {accruedInterests borrowedAmount }
+      currentLoan {
+        borrowedAmount
+      }
     }
     bulletLoans(chainId: $chainId) {
-      id
-     address
+      address
       tvl
       token {
         address
+      }
+      issuedLoan {
+        borrowedAmount
       }
     }
   }
@@ -30,9 +33,27 @@ async function tvl(api) {
   const chain = api.chain;
   const graphQLClient = new GraphQLClient(atlendisUrl);
   const chainId = supportedChains[chain];
-  const { rcls } = await graphQLClient.request(query, { chainId });
-  const tokensAndOwners = rcls.map((rcl) => [rcl.token.address, rcl.address]);
-  return api.sumTokens({ tokensAndOwners });
+  const { rcls, bulletLoans } = await graphQLClient.request(query, { chainId });
+  const balances = {};
+  for (let rcl of rcls) {
+    const poolTvl = rcl.tvl - rcl.currentLoan?.borrowedAmount || "0x00";
+    if (!balances[`${chain}:${rcl.token.address}`]) {
+      balances[`${chain}:${rcl.token.address}`] = poolTvl;
+    } else {
+      balances[`${chain}:${rcl.token.address}`] =
+        balances[`${chain}:${rcl.token.address}`] + poolTvl;
+    }
+  }
+  for (let loan of bulletLoans) {
+    const poolTvl = loan.tvl - loan.issuedLoan?.borrowedAmount || "0x00";
+    if (!balances[`${chain}:${loan.token.address}`]) {
+      balances[`${chain}:${loan.token.address}`] = poolTvl;
+    } else {
+      balances[`${chain}:${loan.token.address}`] =
+        balances[`${chain}:${loan.token.address}`] + poolTvl;
+    }
+  }
+  return balances;
 }
 
 async function borrowed(api) {
@@ -40,18 +61,30 @@ async function borrowed(api) {
   const graphQLClient = new GraphQLClient(atlendisUrl);
   const chainId = supportedChains[chain];
   const { rcls, bulletLoans } = await graphQLClient.request(query, { chainId });
-
+  const balances = {};
   for (let rcl of rcls) {
     if (rcl.currentLoan) {
-      api.add(rcl.token.address, rcl.currentLoan.borrowedAmount);
+      const { borrowedAmount } = rcl.currentLoan;
+      if (!balances[`${chain}:${rcl.token.address}`]) {
+        balances[`${chain}:${rcl.token.address}`] = borrowedAmount;
+      } else {
+        balances[`${chain}:${rcl.token.address}`] =
+          balances[`${chain}:${rcl.token.address}`] + borrowedAmount;
+      }
     }
   }
-
   for (let loan of bulletLoans) {
     if (loan.issuedLoan) {
-      api.add(loan.token.address, loan.tvl);
+      const { borrowedAmount } = loan.issuedLoan;
+      if (!balances[`${chain}:${loan.token.address}`]) {
+        balances[`${chain}:${loan.token.address}`] = borrowedAmount;
+      } else {
+        balances[`${chain}:${loan.token.address}`] =
+          balances[`${chain}:${loan.token.address}`] + borrowedAmount;
+      }
     }
   }
+  return balances;
 }
 
 module.exports = {
