f549ef20123e454401522f5b00e772639cee5c12
Stone <125890624+Stonepapa@users.noreply.github.com>
2024-05-02 22:40:01 +0800

    Create index.js
    hive protocol

---
+++ projects/hive-protocol/index.js
@@ -0,0 +1,5 @@
+const {uniV3Export} = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+    bsquared: {factory: '0xd5B5f1CA0fa5636ac54b0a0007BA374A1513346e', fromBlock: 747123},
+})
