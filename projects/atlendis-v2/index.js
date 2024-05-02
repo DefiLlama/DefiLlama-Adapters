923fa9bc61e6331f2e444c78432ec9a2c0179056
olivblad <olivier.bladier@atlendislabs.io>
2024-05-02 14:37:16 +0200

    using DefiLlama sdk (refacto) and on chain calls for tvl computation for consistency

--- projects/atlendis-v2/index.js
+++ projects/atlendis-v2/index.js
@@ -34,57 +34,32 @@ async function tvl(api) {
   const graphQLClient = new GraphQLClient(atlendisUrl);
   const chainId = supportedChains[chain];
   const { rcls, bulletLoans } = await graphQLClient.request(query, { chainId });
-  const balances = {};
-  for (let rcl of rcls) {
-    const poolTvl = rcl.tvl - rcl.currentLoan?.borrowedAmount || "0x00";
-    if (!balances[`${chain}:${rcl.token.address}`]) {
-      balances[`${chain}:${rcl.token.address}`] = poolTvl;
-    } else {
-      balances[`${chain}:${rcl.token.address}`] =
-        balances[`${chain}:${rcl.token.address}`] + poolTvl;
-    }
-  }
-  for (let loan of bulletLoans) {
-    const poolTvl = loan.tvl - loan.issuedLoan?.borrowedAmount || "0x00";
-    if (!balances[`${chain}:${loan.token.address}`]) {
-      balances[`${chain}:${loan.token.address}`] = poolTvl;
-    } else {
-      balances[`${chain}:${loan.token.address}`] =
-        balances[`${chain}:${loan.token.address}`] + poolTvl;
-    }
-  }
-  return balances;
+  const tokensAndOwners = [...rcls, ...bulletLoans].map((pool) => [
+    pool.token.address,
+    pool.address,
+  ]);
+  return api.sumTokens({ tokensAndOwners });
 }
 
 async function borrowed(api) {
   const chain = api.chain;
   const graphQLClient = new GraphQLClient(atlendisUrl);
   const chainId = supportedChains[chain];
-  const { rcls, bulletLoans } = await graphQLClient.request(query, { chainId });
-  const balances = {};
+  const { rcls, bulletLoans } = await graphQLClient.request(query, {
+    chainId,
+  });
   for (let rcl of rcls) {
     if (rcl.currentLoan) {
       const { borrowedAmount } = rcl.currentLoan;
-      if (!balances[`${chain}:${rcl.token.address}`]) {
-        balances[`${chain}:${rcl.token.address}`] = borrowedAmount;
-      } else {
-        balances[`${chain}:${rcl.token.address}`] =
-          balances[`${chain}:${rcl.token.address}`] + borrowedAmount;
-      }
+      api.add(rcl.token.address, borrowedAmount);
     }
   }
   for (let loan of bulletLoans) {
     if (loan.issuedLoan) {
       const { borrowedAmount } = loan.issuedLoan;
-      if (!balances[`${chain}:${loan.token.address}`]) {
-        balances[`${chain}:${loan.token.address}`] = borrowedAmount;
-      } else {
-        balances[`${chain}:${loan.token.address}`] =
-          balances[`${chain}:${loan.token.address}`] + borrowedAmount;
-      }
+      api.add(loan.token.address, borrowedAmount);
     }
   }
-  return balances;
 }
 
 module.exports = {
