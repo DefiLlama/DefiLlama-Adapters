b5cc99fce5430a03fc872790d3cc10f52798446b
larkloss <dingzhao1995@gmail.com>
2024-05-04 16:07:04 +0800

    Revert "Added more info for SHUI"
    This reverts commit 6966ad38132566bf467a300a9a9280bf36b60d8a.

--- projects/shui/index.js
+++ projects/shui/index.js
@@ -1,11 +1,7 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const SCFX_TokenAddress = "0x1858a8d367e69cd9E23d0Da4169885a47F05f1bE";
-const { sumTokensExport, } = require("../helper/unknownTokens");
-const MasterchefV2 = "0xeced26633b5c2d7124b5eae794c9c32a8b8e7df2";
 
 const WCFX = ADDRESSES.conflux.WCFX;
-const CFX_SHUI_LP_TokenAddress = "0xF1F6e3Aa98Bac6C13230051e452065DF299a78A7";
-const CFX_SCFX_LP_TokenAddress = "0x1858a8d367e69cd9E23d0Da4169885a47F05f1bE";
 
 module.exports = {
   conflux: {
@@ -21,7 +17,6 @@ module.exports = {
       return {
         ["conflux:" + WCFX]: totalDeposited * ratioDepositedBySupply / 1e9
       };
-    },
-    pool2: sumTokensExport({ owner: MasterchefV2, coreAssets: [WCFX], tokens: [CFX_SHUI_LP_TokenAddress, CFX_SCFX_LP_TokenAddress] }),
+    }
   }
 };
