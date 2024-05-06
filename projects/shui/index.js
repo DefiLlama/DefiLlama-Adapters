d58931ddbeb56921acc940bb886b90779aa500bd
larkloss <dingzhao1995@gmail.com>
2024-05-06 21:31:11 +0800

    Added and more info for SHUI (fixed previous pr)

--- projects/shui/index.js
+++ projects/shui/index.js
@@ -1,11 +1,11 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const SCFX_TokenAddress = "0x1858a8d367e69cd9E23d0Da4169885a47F05f1bE";
 const { sumTokensExport, } = require("../helper/unknownTokens");
-const MasterchefV2 = "0xeced26633b5c2d7124b5eae794c9c32a8b8e7df2";
+const SHUI2CFX = "0xf1f6e3aa98bac6c13230051e452065df299a78a7";
 
 const WCFX = ADDRESSES.conflux.WCFX;
-const CFX_SHUI_LP_TokenAddress = "0xF1F6e3Aa98Bac6C13230051e452065DF299a78A7";
-const CFX_SCFX_LP_TokenAddress = "0x1858a8d367e69cd9E23d0Da4169885a47F05f1bE";
+const CFX_SHUI_LP_TokenAddress = "0x561c1412D926b3D75BaB15ABA1d7c10E31Ffb721";
+const CFX_SCFX_LP_TokenAddress = "0x41e9e50952d8a2e489d0b866b78835bc2ad2a0fa";
 
 module.exports = {
   conflux: {
@@ -22,6 +22,6 @@ module.exports = {
         ["conflux:" + WCFX]: totalDeposited * ratioDepositedBySupply / 1e9
       };
     },
-    pool2: sumTokensExport({ owner: MasterchefV2, coreAssets: [WCFX], tokens: [CFX_SHUI_LP_TokenAddress, CFX_SCFX_LP_TokenAddress] }),
+    pool2: sumTokensExport({ owner: SHUI2CFX, coreAssets: [WCFX], tokens: [CFX_SHUI_LP_TokenAddress, CFX_SCFX_LP_TokenAddress] }),
   }
 };
