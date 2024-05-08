e3f2c33fe139ac4b08e36ce7e5c89cf3211addfd
jihyun song <jihyunsong42@gmail.com>
2024-05-08 13:59:22 +0900

    Update index.js

--- projects/limitless/index.js
+++ projects/limitless/index.js
@@ -3,8 +3,6 @@ const limWethAbi = require("./limWeth.json")
 const vaultAbi = require("./vault.json")
 const dataProviderAbi = require("./dataProvider.json")
 const axios = require("axios")
-// const { SqrtPriceMath, TickMath } = require('@uniswap/v3  -sdk')
-const JSBI = require("jsbi")
 
 const LIM_WETH_CONTRACT = '0x845d629D2485555514B93F05Bdbe344cC2e4b0ce'
 const BASE_WETH_CONTRACT = '0x4200000000000000000000000000000000000006'
@@ -40,18 +38,18 @@ query {
 const LMT_SUBGRAPH_ENDPOINT_BASE = 'https://api.studio.thegraph.com/query/71042/limitless-subgraph-base/version/latest'
 
 async function base_tvl(api) {
-  const tokenBalance = (await sdk.api.abi.call({
+  const tokenBalance = BigInt((await sdk.api.abi.call({
     target: LIM_WETH_CONTRACT,
     abi: limWethAbi.tokenBalance,
     chain: 'base'
-  })).output
+  })).output)
   api.add(BASE_WETH_CONTRACT, tokenBalance)
 
-  const vault = (await sdk.api.abi.call({
+  const vault = BigInt((await sdk.api.abi.call({
     target: VAULT_CONTRACT,
     abi: vaultAbi.totalAssets,
     chain: 'base'
-  })).output
+  })).output)
 
   api.add(VAULT_CONTRACT, vault)
 
@@ -62,7 +60,7 @@ async function base_tvl(api) {
   const withdrawnData = (await axios.post(LMT_SUBGRAPH_ENDPOINT_BASE, {
     query: LIQUIDITY_WITHDRAWN_QUERY
   })).data.data.liquidityWithdrawns
-  
+
   let pools = new Set()
   providedData.forEach((entry) => {
     const pool = entry.pool
@@ -111,82 +109,81 @@ async function base_tvl(api) {
 
 
   const mulShift = (val, mulBy) => {
-    return JSBI.signedRightShift(JSBI.multiply(val, JSBI.BigInt(mulBy)), JSBI.BigInt(128))
+    return val * mulBy >> 128n
   }
 
   const getSqrtRatioAtTick = (tick) => {
     const MIN_TICK = -887272
     const MAX_TICK = -MIN_TICK
-    const MaxUint256 = JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+    const MaxUint256 = 2n ** 256n - 1n
     // invariant(tick >= MIN_TICK && tick <= MAX_TICK && Number.isInteger(tick), 'TICK')
     const absTick = tick < 0 ? tick * -1 : tick
 
     let ratio =
       (absTick & 0x1) != 0
-        ? JSBI.BigInt('0xfffcb933bd6fad37aa2d162d1a594001')
-        : JSBI.BigInt('0x100000000000000000000000000000000')
-    if ((absTick & 0x2) != 0) ratio = mulShift(ratio, '0xfff97272373d413259a46990580e213a')
-    if ((absTick & 0x4) != 0) ratio = mulShift(ratio, '0xfff2e50f5f656932ef12357cf3c7fdcc')
-    if ((absTick & 0x8) != 0) ratio = mulShift(ratio, '0xffe5caca7e10e4e61c3624eaa0941cd0')
-    if ((absTick & 0x10) != 0) ratio = mulShift(ratio, '0xffcb9843d60f6159c9db58835c926644')
-    if ((absTick & 0x20) != 0) ratio = mulShift(ratio, '0xff973b41fa98c081472e6896dfb254c0')
-    if ((absTick & 0x40) != 0) ratio = mulShift(ratio, '0xff2ea16466c96a3843ec78b326b52861')
-    if ((absTick & 0x80) != 0) ratio = mulShift(ratio, '0xfe5dee046a99a2a811c461f1969c3053')
-    if ((absTick & 0x100) != 0) ratio = mulShift(ratio, '0xfcbe86c7900a88aedcffc83b479aa3a4')
-    if ((absTick & 0x200) != 0) ratio = mulShift(ratio, '0xf987a7253ac413176f2b074cf7815e54')
-    if ((absTick & 0x400) != 0) ratio = mulShift(ratio, '0xf3392b0822b70005940c7a398e4b70f3')
-    if ((absTick & 0x800) != 0) ratio = mulShift(ratio, '0xe7159475a2c29b7443b29c7fa6e889d9')
-    if ((absTick & 0x1000) != 0) ratio = mulShift(ratio, '0xd097f3bdfd2022b8845ad8f792aa5825')
-    if ((absTick & 0x2000) != 0) ratio = mulShift(ratio, '0xa9f746462d870fdf8a65dc1f90e061e5')
-    if ((absTick & 0x4000) != 0) ratio = mulShift(ratio, '0x70d869a156d2a1b890bb3df62baf32f7')
-    if ((absTick & 0x8000) != 0) ratio = mulShift(ratio, '0x31be135f97d08fd981231505542fcfa6')
-    if ((absTick & 0x10000) != 0) ratio = mulShift(ratio, '0x9aa508b5b7a84e1c677de54f3e99bc9')
-    if ((absTick & 0x20000) != 0) ratio = mulShift(ratio, '0x5d6af8dedb81196699c329225ee604')
-    if ((absTick & 0x40000) != 0) ratio = mulShift(ratio, '0x2216e584f5fa1ea926041bedfe98')
-    if ((absTick & 0x80000) != 0) ratio = mulShift(ratio, '0x48a170391f7dc42444e8fa2')
+        ? 0xfffcb933bd6fad37aa2d162d1a594001n
+        : 0x100000000000000000000000000000000n
+    if ((absTick & 0x2) != 0) ratio = mulShift(ratio, 0xfff97272373d413259a46990580e213an)
+    if ((absTick & 0x4) != 0) ratio = mulShift(ratio, 0xfff2e50f5f656932ef12357cf3c7fdccn)
+    if ((absTick & 0x8) != 0) ratio = mulShift(ratio, 0xffe5caca7e10e4e61c3624eaa0941cd0n)
+    if ((absTick & 0x10) != 0) ratio = mulShift(ratio, 0xffcb9843d60f6159c9db58835c926644n)
+    if ((absTick & 0x20) != 0) ratio = mulShift(ratio, 0xff973b41fa98c081472e6896dfb254c0n)
+    if ((absTick & 0x40) != 0) ratio = mulShift(ratio, 0xff2ea16466c96a3843ec78b326b52861n)
+    if ((absTick & 0x80) != 0) ratio = mulShift(ratio, 0xfe5dee046a99a2a811c461f1969c3053n)
+    if ((absTick & 0x100) != 0) ratio = mulShift(ratio, 0xfcbe86c7900a88aedcffc83b479aa3a4n)
+    if ((absTick & 0x200) != 0) ratio = mulShift(ratio, 0xf987a7253ac413176f2b074cf7815e54n)
+    if ((absTick & 0x400) != 0) ratio = mulShift(ratio, 0xf3392b0822b70005940c7a398e4b70f3n)
+    if ((absTick & 0x800) != 0) ratio = mulShift(ratio, 0xe7159475a2c29b7443b29c7fa6e889d9n)
+    if ((absTick & 0x1000) != 0) ratio = mulShift(ratio, 0xd097f3bdfd2022b8845ad8f792aa5825n)
+    if ((absTick & 0x2000) != 0) ratio = mulShift(ratio, 0xa9f746462d870fdf8a65dc1f90e061e5n)
+    if ((absTick & 0x4000) != 0) ratio = mulShift(ratio, 0x70d869a156d2a1b890bb3df62baf32f7n)
+    if ((absTick & 0x8000) != 0) ratio = mulShift(ratio, 0x31be135f97d08fd981231505542fcfa6n)
+    if ((absTick & 0x10000) != 0) ratio = mulShift(ratio, 0x9aa508b5b7a84e1c677de54f3e99bc9n)
+    if ((absTick & 0x20000) != 0) ratio = mulShift(ratio, 0x5d6af8dedb81196699c329225ee604n)
+    if ((absTick & 0x40000) != 0) ratio = mulShift(ratio, 0x2216e584f5fa1ea926041bedfe98n)
+    if ((absTick & 0x80000) != 0) ratio = mulShift(ratio, 0x48a170391f7dc42444e8fa2n)
 
-    if (tick > 0) ratio = JSBI.divide(MaxUint256, ratio)
-    const Q32 = JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(32))
-    const ZERO = JSBI.BigInt(0)
-    const ONE = JSBI.BigInt(1)
+    if (tick > 0) ratio = MaxUint256 / ratio
+    const Q32 = 2n ** 32n
+    const ZERO = 0n
+    const ONE = 1n
     // back to Q96
-    return JSBI.greaterThan(JSBI.remainder(ratio, Q32), ZERO)
-      ? JSBI.add(JSBI.divide(ratio, Q32), ONE)
-      : JSBI.divide(ratio, Q32)
-    
+    return ratio % Q32 > ZERO
+      ? ratio / Q32 + ONE
+      : ratio / Q32
   }
 
   const mulDivRoundingUp = (a, b, denominator) => {
-    const ONE = JSBI.BigInt(1)
-    const product = JSBI.multiply(a, b)
-    let result = JSBI.divide(product, denominator)
-    if (JSBI.notEqual(JSBI.remainder(product, denominator), ZERO)) result = JSBI.add(result, ONE)
+    const ONE = 1n
+    const product = a * b
+    let result = product / denominator
+    if (product % denominator !== ZERO) result += ONE
     return result
   }
 
   const getAmount0Delta = (sqrtRatioAX96, sqrtRatioBX96, liquidity, roundUp) => {
-    const ONE = JSBI.BigInt(1)
-    if (JSBI.greaterThan(sqrtRatioAX96, sqrtRatioBX96)) {
+    const ONE = 1n
+    if (sqrtRatioAX96 > sqrtRatioBX96) {
       ;[sqrtRatioAX96, sqrtRatioBX96] = [sqrtRatioBX96, sqrtRatioAX96]
     }
 
-    const numerator1 = JSBI.leftShift(liquidity, JSBI.BigInt(96))
-    const numerator2 = JSBI.subtract(sqrtRatioBX96, sqrtRatioAX96)
+    const numerator1 = liquidity << 96n
+    const numerator2 = sqrtRatioBX96 - sqrtRatioAX96
 
     return roundUp
       ? mulDivRoundingUp(mulDivRoundingUp(numerator1, numerator2, sqrtRatioBX96), ONE, sqrtRatioAX96)
-      : JSBI.divide(JSBI.divide(JSBI.multiply(numerator1, numerator2), sqrtRatioBX96), sqrtRatioAX96)
+      : numerator1 * numerator2 / sqrtRatioBX96 / sqrtRatioAX96
   }
 
   const getAmount1Delta = (sqrtRatioAX96, sqrtRatioBX96, liquidity, roundUp) => {
-    const Q96 = JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(96))
-    if (JSBI.greaterThan(sqrtRatioAX96, sqrtRatioBX96)) {
+    const Q96 = 2n ** 96n
+    if (sqrtRatioAX96 > sqrtRatioBX96) {
       ;[sqrtRatioAX96, sqrtRatioBX96] = [sqrtRatioBX96, sqrtRatioAX96]
     }
 
     return roundUp
-      ? mulDivRoundingUp(liquidity, JSBI.subtract(sqrtRatioBX96, sqrtRatioAX96), Q96)
-      : JSBI.divide(JSBI.multiply(liquidity, JSBI.subtract(sqrtRatioBX96, sqrtRatioAX96)), Q96)
+      ? mulDivRoundingUp(liquidity, sqrtRatioBX96 - sqrtRatioAX96, Q96)
+      : liquidity * (sqrtRatioBX96 - sqrtRatioAX96) / Q96
   }
 
 
@@ -202,7 +199,7 @@ async function base_tvl(api) {
       amount0 = getAmount0Delta(
         getSqrtRatioAtTick(entry.tickLower),
         getSqrtRatioAtTick(entry.tickUpper),
-        JSBI.BigInt(entry.liquidity.toString()),
+        BigInt(entry.liquidity.toString()),
         false
       ).toString()
       amount1 = '0'
@@ -210,20 +207,20 @@ async function base_tvl(api) {
       amount0 = getAmount0Delta(
         getSqrtRatioAtTick(curTick),
         getSqrtRatioAtTick(entry.tickUpper),
-        JSBI.BigInt(entry.liquidity.toString()),
+        BigInt(entry.liquidity.toString()),
         false
       ).toString()
       amount1 = getAmount1Delta(
         getSqrtRatioAtTick(entry.tickLower),
         getSqrtRatioAtTick(curTick),
-        JSBI.BigInt(entry.liquidity.toString()),
+        BigInt(entry.liquidity.toString()),
         false
       ).toString()
     } else {
       amount1 = getAmount1Delta(
         getSqrtRatioAtTick(entry.tickLower),
         getSqrtRatioAtTick(entry.tickUpper),
-        JSBI.BigInt(entry.liquidity.toString()),
+        BigInt(entry.liquidity.toString()),
         false
       ).toString()
       amount0 = '0'
@@ -242,7 +239,7 @@ async function base_tvl(api) {
 
   const ProvidedDataProcessed = providedData?.map(processLiqEntry)
   const WithdrawDataProcessed = withdrawnData?.map(processLiqEntry)
-  
+
   const sumProvidedData = ProvidedDataProcessed.reduce((acc, cur) => {
     if (!acc[cur.pool]) {
       acc[cur.pool] = { 
@@ -272,7 +269,7 @@ async function base_tvl(api) {
     }
     return acc
   }, {})
-  
+
   const diffData = {}
 
   for (const pool in sumProvidedData) {
@@ -292,11 +289,10 @@ async function base_tvl(api) {
       }
     }
   }
-  
-  // console.log("DIFF BY POOL", diffData)
+
   Object.values(diffData).forEach((entry) => {
-    api.add(entry.token0, entry.amount0)
-    api.add(entry.token1, entry.amount1)
+    api.add(entry.token0, entry.amount0.toString())
+    api.add(entry.token1, entry.amount1.toString())
   })
 }
 
