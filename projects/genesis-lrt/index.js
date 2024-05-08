f7ae018991fa92f75e8474395239a10072bdf409
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-08 17:19:32 +0200

    compromise

--- projects/genesis-lrt/index.js
+++ projects/genesis-lrt/index.js
@@ -1,7 +1,8 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
 async function tvl(api) {
-  const staked = await api.call({  abi: 'uint256:totalAssets', target: '0xf073bAC22DAb7FaF4a3Dd6c6189a70D54110525C'})
+  if (api.timestamp > 1715181240) return {}
+  const staked = await api.call({ abi: 'uint256:totalAssets', target: '0xf073bAC22DAb7FaF4a3Dd6c6189a70D54110525C' })
   api.add(ADDRESSES.null, staked)
 }
 
