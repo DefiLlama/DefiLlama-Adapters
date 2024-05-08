9ed909336beef10ba7a5ae6f11f13aebb365fc85
nlx-morpheus <nolomorpheous@gmail.com>
2024-05-08 13:26:24 +0100

    pr to update NLX informations

--- projects/nlx/index.js
+++ projects/nlx/index.js
@@ -2,6 +2,7 @@ const { gmxExportsV2 } = require('../helper/gmx')
 
 module.exports = {
   core: {
+    //use gmxExportsV2 as NLX is a fork of GMXV2
     tvl: gmxExportsV2({ eventEmitter: '0x29792F84224c77e2c672213c4d942fE280D596ef', fromBlock: 13558258, }),
   },
 }
