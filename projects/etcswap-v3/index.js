26d5f8f65e2e4086847fce6f07f132ca2e6fba8f
leon-do <leondo123@gmail.com>
2024-05-07 22:15:25 -0400

    Add ETCSwap V3

---
+++ projects/etcswap-v3/index.js
@@ -0,0 +1,8 @@
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+  ethereumclassic: {
+    factory: "0x2624E907BcC04f93C8f29d7C7149a8700Ceb8cDC",
+    fromBlock: 19714286,
+  },
+});
