01d4fbc22008aadc0df693be3b69ecd1a8e464a8
Calvin Koepke <hello@calvinkoepke.com>
2024-05-09 15:58:52 -0600

    chore: add v3 contract tvl

--- projects/sundaeswap-v3/index.js
+++ projects/sundaeswap-v3/index.js
@@ -2,9 +2,9 @@ const { getAdaInAddress } = require('../helper/chain/cardano')
 
 async function tvl(){
     const ammLocked = await getAdaInAddress("addr1x8srqftqemf0mjlukfszd97ljuxdp44r372txfcr75wrz26rnxqnmtv3hdu2t6chcfhl2zzjh36a87nmd6dwsu3jenqsslnz7e")
-    const orderBookLocked = await getAdaInAddress("addr1w8ax5k9mutg07p2ngscu3chsauktmstq92z9de938j8nqacprc9mw")
+    // const orderBookLocked = await getAdaInAddress("addr1w8ax5k9mutg07p2ngscu3chsauktmstq92z9de938j8nqacprc9mw")
     return {
-        "cardano": ammLocked * 2 + orderBookLocked
+        "cardano": ammLocked * 2
     }
 }
 
