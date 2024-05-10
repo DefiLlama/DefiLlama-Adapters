18dc69011f44f3375a42be92eb78c2354aacef7f
hbdgr <hbdgr@protonmail.com>
2024-05-10 18:44:00 +0200

    colony: tvl from staking rewards

--- projects/colony/index.js
+++ projects/colony/index.js
@@ -1,4 +1,4 @@
-const { clyVesting } = require("./clyVesting")
+// const { clyVesting } = require("./clyVesting")
 const { staking } = require('../helper/staking')
 
 const colonyGovernanceToken = "0xec3492a2508DDf4FDc0cD76F31f340b30d1793e6";
@@ -15,13 +15,81 @@ async function _staking(api) {
   api.add(colonyGovernanceToken, bals.concat(bals1))
 }
 
+async function tvl(_a, _b, _cb, { api }) {
+  const rewardTokensListLength = Number(
+    await api.call({
+      abi: "uint256:rewardTokensListLenght",
+      target: stakingV3Contract,
+    })
+  );
+
+  const customRewardTokensListLength = Number(
+    await api.call({
+      abi: "uint256:customRewardTokensListLenght",
+      target: stakingV3Contract,
+    })
+  );
+
+  const rewardTokens = (await api.multiCall({
+    abi: "function getRewardTokensListIndex(uint256 index) external view returns (address, uint8)",
+    target: stakingV3Contract,
+    calls: [...Array(rewardTokensListLength).keys()].map(i => ({
+      params: i
+    }))
+  })).map(r => ({ address: r[0], category: r[1]}))
+
+  const customRewardTokens = (await api.multiCall({
+    abi: "function getCustomRewardTokensListIndex(uint256 index) external view returns (address, uint8)",
+    target: stakingV3Contract,
+    calls: [...Array(customRewardTokensListLength).keys()].map(i => ({
+      params: i
+    }))
+  })).map(r => ({ address: r[0], category: r[1]}))
+
+  // the same tokens can appear in different categories
+  const uniqueRewardTokensSet = new Set();
+  [...rewardTokens, ...customRewardTokens].forEach(token => {
+    uniqueRewardTokensSet.add(token.address)
+  })
+
+  const handleCLYReward = async () => {
+    const totalStake = BigInt(await api.call({
+      abi: 'uint256:totalStake',
+      target: stakingV3Contract,
+    }))
+
+    const balanceOf = BigInt(await api.call({
+      abi: 'erc20:balanceOf',
+      target: colonyGovernanceToken,
+      params: stakingV3Contract
+    }))
+
+    api.add(colonyGovernanceToken, balanceOf - totalStake)
+  }
+
+  for(const token of uniqueRewardTokensSet) {
+    // CLY has to be handled separately because some of the balance is locked in staking.
+    if (token === colonyGovernanceToken) {
+      await handleCLYReward()
+      continue
+    }
+
+    api.add(token, await api.call({
+      abi: 'erc20:balanceOf',
+      target: token,
+      params: stakingV3Contract
+    }))
+  }
+}
+
 module.exports = {
   methodology:
     "Staking is calculated based on CLY tokens locked on Colony staking contracts. " +
-    "Vesting is calculated as already unlocked and available to claim CLY tokens in the vesting contract",
+    "Vesting is calculated as CLY tokens in the vesting contract." +
+    "TVL includes also rewards in various tokens distributed continuously for stakers.",
   avax: {
     start: 1638367059, // CLY Token deployment
-    tvl: async () => ({}),
+    tvl,
     staking: _staking,
     // vesting: clyVesting(colonyGovernanceToken, vestingContract),
     vesting: staking(vestingContract, colonyGovernanceToken),
