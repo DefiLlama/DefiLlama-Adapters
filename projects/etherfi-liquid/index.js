5174b6e7041bc1b59f30ef5a0a8d936325bb8215
Vaibhav Valecha <vvalecha@uwaterloo.ca>
2024-05-10 00:28:06 -0500

    retrieve weth from liquid not added to etherfi tvl

--- projects/etherfi-liquid/index.js
+++ projects/etherfi-liquid/index.js
@@ -10,6 +10,9 @@ async function tvl(api) {
     target: '0x08c6f91e2b681faf5e17227f2a44c307b3c1364c',
   });
   api.add(ADDRESSES.ethereum.USDC, balUSD);
+  const tvl_breakdown = await fetch('https://nw8dhr9lr1.execute-api.ap-southeast-1.amazonaws.com/prod/tvl')
+  const liquidWeth = (await tvl_breakdown.json())['tvl']['liquid_weth']['tvl']['eth']
+
 }
 
 module.exports = {
