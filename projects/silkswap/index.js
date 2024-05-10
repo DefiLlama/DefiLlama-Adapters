38fc5d381d2b99185e391fe853d83505512b0bd6
madi-blockstars <madi-blockstars@proton.me>
2024-05-10 14:04:42 +0100

    v2

--- projects/silkswap/index.js
+++ projects/silkswap/index.js
@@ -1,9 +1,5 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { uniTvlExport } = require('../helper/unknownTokens')
+const chain = 'bahamut'
+const factory = '0xF660558a4757Fb5953d269FF32E228Ae3d5f6c68' // v2 factory address
 
-module.exports={
-        misrepresentedTokens: false,
-    methodology: "Factory address (0xF660558a4757Fb5953d269FF32E228Ae3d5f6c68) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
-    bahamut: {
-        tvl: getUniTVL({ factory: '0xF660558a4757Fb5953d269FF32E228Ae3d5f6c68', useDefaultCoreAssets: true }),
-    }
-}
+module.exports = uniTvlExport(chain, factory)
