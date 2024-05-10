f30f29671a0dfeb2f8639d57180f177590a96959
imfeng <fantasrulai@gmail.com>
2024-05-10 21:10:54 +0800

    Update Satoshi-Protocol (BEVM) wstBTC tvl (#10166)
    * update Satoshi-Protocol wstBTC tvl
    
    * Update index.js
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/satoshi-protocol/index.js
+++ projects/satoshi-protocol/index.js
@@ -1,8 +1,11 @@
-const TROVE_MANAGER_BEACON_PROXY_ADDRESS = '0x0598Ef47508Ec11a503670Ac3B642AAE8EAEdEFA';
+const TROVE_MANAGER_LIST = [
+  '0x0598Ef47508Ec11a503670Ac3B642AAE8EAEdEFA', // WBTC Collateral
+  '0xa794a7Fd668FE378E095849caafA8C8dC7E84780', // wstBTC Collateral
+]
 
 async function tvl(api) {
-  const token = await api.call({  abi: 'address:collateralToken', target: TROVE_MANAGER_BEACON_PROXY_ADDRESS})
-  return api.sumTokens({ tokens: [token], owner: [TROVE_MANAGER_BEACON_PROXY_ADDRESS] })
+  const tokens = await api.multiCall({  abi: 'address:collateralToken', calls: TROVE_MANAGER_LIST})
+  return api.sumTokens({ tokensAndOwners2: [tokens, TROVE_MANAGER_LIST]})
 }
 
 module.exports = {
