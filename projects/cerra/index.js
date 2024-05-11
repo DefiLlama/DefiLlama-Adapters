8407e11ac09b373b80a3cf564e8fa72abfa5c452
cerraio <147749662+cerraio@users.noreply.github.com>
2024-05-11 01:12:50 -0700

    add batcher script

--- projects/cerra/index.js
+++ projects/cerra/index.js
@@ -2,9 +2,10 @@ const { sumTokens2 } = require("../helper/chain/cardano");
 
 const LENDING_SCRIPT = "addr1z878lgw0676mfkusf0f2h9wl3w5q2zu0klrlcamv6g2werc0pqnxu0fg7wvwgjtgzvg6lg6avzczmwf84aclygrvlkhqfmh5zc";
 const AMM_SCRIPT = "addr1zyl8u7cw93g99g2e68cq3k27ad7rd8p6yy3yg9vesgahrus0pqnxu0fg7wvwgjtgzvg6lg6avzczmwf84aclygrvlkhq2a0paj";
+const BATCHER_SCRIPT = "addr_test1wpw0ds4d59mlr639ya84guh3p8nzyxxmh5cdk6x8fz35agsmnawle";
 
 async function tvl() {
-  return sumTokens2({ scripts: [LENDING_SCRIPT, AMM_SCRIPT] })
+  return sumTokens2({ scripts: [LENDING_SCRIPT, AMM_SCRIPT, BATCHER_SCRIPT] })
 }
 
 module.exports = {
