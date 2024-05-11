1aef8d663429efcdd21afeb1fc4fcf0e773ad61b
gaspare100 <gaspare.marchese.it@gmail.com>
2024-05-11 16:21:52 +0100

    Add new pool to scrub invest

--- projects/scrubinvest/index.js
+++ projects/scrubinvest/index.js
@@ -51,7 +51,7 @@ async function tvl(api) {
       [ADDRESSES.kava.WKAVA, '0xC05021F3b3601BF33aB59dc75577B85985ACaab7'],
       // New Scrub Pool
       ['0x471F79616569343e8e84a66F342B7B433b958154', '0x67041094c4fc1492A1AB988Fb8De0ab4A0a4A080'],
-
+      ['0x990e157fC8a492c28F5B50022F000183131b9026','0xa07deE8FF35fE2e2961a7e1006EAdA98E24aE82E']
       // WETH
       
       // Vaults Equilibre Scrub
