b1aca1171e36f81400d549f314115f3c9ffe9eaa
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-13 08:38:16 +0200

    code refactor

--- projects/equation-v3/index.js
+++ projects/equation-v3/index.js
@@ -1,19 +1,13 @@
-const { sumTokens2 } = require("../helper/unwrapLPs");
-
 const config = {
-  arbitrum: {
-    marketManager: "0xDe2b77bcbAEf4C5ECE3b827B21fbD8556e8Fa5a4",
-    usd: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9"
-  }
-};
+  arbitrum: { marketManager: "0xDe2b77bcbAEf4C5ECE3b827B21fbD8556e8Fa5a4", }
+}
 
 Object.keys(config).forEach(chain => {
-  const { marketManager, usd } = config[chain];
+  const { marketManager, } = config[chain]
   module.exports[chain] = {
-    tvl: async (_, _b, _cb, { api }) => {
-      const ownerTokens = [[[usd], marketManager]];
-      return sumTokens2({ api, ownerTokens });
+    tvl: async (api) => {
+      const token = await api.call({ abi: 'address:USD', target: marketManager })
+      return api.sumTokens({ owner: marketManager, tokens: [token] })
     }
-  };
-});
-
+  }
+})
