7568a432ad1c3defd62d3dbb1525d4862ebc2436
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-14 18:11:21 +0200

    fix adapters

--- projects/kinetix-v3/index.js
+++ projects/kinetix-v3/index.js
@@ -1,8 +1,12 @@
-const { uniV3Export } = require('../helper/uniswapV3')
+const { uniV3GraphExport } = require('../helper/uniswapV3')
 
-module.exports = uniV3Export({
-  kava: { 
-    factory: '0x2dBB6254231C5569B6A4313c6C1F5Fe1340b35C2', 
-    fromBlock: 6069472
-    },
-})
+module.exports = {
+  kava: {
+    tvl: uniV3GraphExport({
+      // // factory: '0x2dBB6254231C5569B6A4313c6C1F5Fe1340b35C2', 
+      // // fromBlock: 6069472
+      graphURL: 'https://kava-graph-node.metavault.trade/subgraphs/name/kinetixfi/v3-subgraph',
+      name: 'kinetixfi/kava-v3',
+  })
+  }
+}
