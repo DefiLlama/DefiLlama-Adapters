49e44723fedd329ae854c0d564dfa20bfae7f050
realdealshaman <realdealshaman11@gmail.com>
2024-05-14 19:58:49 +0100

    add pxeth blast

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -75,6 +75,7 @@ const fixBalancesTokens = {
   },
   blast: {
     '0xf7bc58b8d8f97adc129cfc4c9f45ce3c0e1d2692': { coingeckoId: "wrapped-bitcoin", decimals: 8 },
+    '0x9e0d7d79735e1c63333128149c7b616a0dc0bbdb': { coingeckoId: "weth", decimals: 18 },
   },
   solana: {
     'AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR': { coingeckoId: "guacamole", decimals: 5 },
