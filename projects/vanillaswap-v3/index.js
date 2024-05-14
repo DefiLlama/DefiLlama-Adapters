09e1feeebb1c083962d7916a66425470b978c6bf
igorshelkovenkov <is@topiet.de>
2024-05-14 12:49:55 +0200

    add correct chain vanilla swap v3

--- projects/vanillaswap-v3/index.js
+++ projects/vanillaswap-v3/index.js
@@ -2,5 +2,5 @@ const { uniV3Export } = require("../helper/uniswapV3");
 const factory = "0x79Ea1b897deeF37e3e42cDB66ca35DaA799E93a3";
 
 module.exports = uniV3Export({
-    core: { factory, fromBlock: 512861 },
+    defichain_evm: { factory, fromBlock: 512861 },
 });
