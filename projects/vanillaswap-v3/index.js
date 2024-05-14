33cf11c27c6f9f3830e177383525e8084503eab9
igorshelkovenkov <is@topiet.de>
2024-05-14 12:40:31 +0200

    fix vanilla swap v3

--- projects/vanillaswap-v3/index.js
+++ projects/vanillaswap-v3/index.js
@@ -1,3 +1,6 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
+const { uniV3Export } = require("../helper/uniswapV3");
+const factory = "0x79Ea1b897deeF37e3e42cDB66ca35DaA799E93a3";
 
-module.exports = uniTvlExport('defichain_evm', '0x79Ea1b897deeF37e3e42cDB66ca35DaA799E93a3')
+module.exports = uniV3Export({
+    core: { factory, fromBlock: 512861 },
+});
