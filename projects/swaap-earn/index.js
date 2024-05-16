78870328539ef2df84d972a0368ad3fcaa785108
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-16 17:48:56 +0200

    code refactor

--- projects/swaap-earn/index.js
+++ projects/swaap-earn/index.js
@@ -1,52 +1,20 @@
-const {request, gql} = require("graphql-request");
-const configEarn = [
-    {
-        chain: 'arbitrum',
-        subgraph: 'https://api.thegraph.com/subgraphs/name/borelien/swaap-earn-arbitrum-test',
-    }
-]
-
-const query = gql`
-query FundsTVL{
-  funds {
-    tvlTotal
-    asset{
-      id
-      decimals
-    }
-    id
-  }
-}`
-
-function tvlFunctionGetterEarn(chain) {
-    return async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
-
-
-        const config = configEarn.find(c => c.chain === chain)
-        const {funds} = await request(
-            config.subgraph,
-            query,
-        )
-
-        for (const fund of funds) {
-            const asset = fund.asset.id
-            const decimals = fund.asset.decimals
-            const eighteenDecimalsTotalAssets = fund.tvlTotal
-
-            const totalAssets = (
-                BigInt(eighteenDecimalsTotalAssets) / BigInt(10 ** (18 - decimals))
-            ).toString()
-            api.add(asset, totalAssets)
-        }
-    }
-}
-
+const { cachedGraphQuery } = require('../helper/cache')
+const query = `query FundsTVL{ funds { id  } }`
 
 module.exports = {
-    start: 1713312000, //  Apr 17 2024 00:00:00 GMT+0000
-    methodology: `The TVL is retrieved using TheGraph.`,
-    arbitrum: {
-        tvl: tvlFunctionGetterEarn('arbitrum')
-    }
+  start: 1713312000, //  Apr 17 2024 00:00:00 GMT+0000
 }
 
+const config = {
+  arbitrum: { endpoint: 'https://api.thegraph.com/subgraphs/name/borelien/swaap-earn-arbitrum-test', },
+}
+
+Object.keys(config).forEach(chain => {
+  const { endpoint } = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const { funds } = await cachedGraphQuery('swaap-earn/' + chain, endpoint, query)
+      return api.erc4626Sum({ calls: funds.map(i => i.id), isOG4626: true, })
+    }
+  }
+})
