8db0dbfca6ffb9804baf91a2ee5edf0149b9379d
Yuushin <Shyy>
2024-05-17 23:22:54 +0800

    replace compoundExports2() with compoundExports(), and use WBTC as the fourth argument.

--- projects/enzo/index.js
+++ projects/enzo/index.js
@@ -1,5 +1,5 @@
-const { compoundExports2 } = require("../helper/compound");
+const { compoundExports } = require("../helper/compound");
 
 module.exports = {
-	btr: compoundExports2({ cether: '0xe277Aed3fF3Eb9824EdC52Fe7703DF0c5ED8B313', comptroller: '0xe688a4a94AD1D32CD52A01306fc0a9552749F322',})
+	btr: compoundExports("0xe688a4a94AD1D32CD52A01306fc0a9552749F322", "btr", "0xe277Aed3fF3Eb9824EdC52Fe7703DF0c5ED8B313", "0xfF204e2681A6fA0e2C3FaDe68a1B28fb90E4Fc5F")
 };
