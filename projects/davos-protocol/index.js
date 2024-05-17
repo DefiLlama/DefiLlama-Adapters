3d3cb106f925d39a88d0b72545f977ed52ed110b
Simão Pinto <87367287+Sk1mer12@users.noreply.github.com>
2024-05-17 17:30:24 +0100

    Update index.js

--- projects/davos-protocol/index.js
+++ projects/davos-protocol/index.js
@@ -5,42 +5,42 @@ let aux
 
 async function getARBTvl() {
   aux = await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=42161')
-  return (aux.amount)
+  return (aux.amount.toFixed(0))
 }
 
 async function getBSCTvl() {
   aux = await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=56')
-  return (aux.amount)
+  return (aux.amount.toFixed(0))
 }
 
 async function getETHTvl() {
   aux = await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=1')
-  return (aux.amount)
+  return (aux.amount.toFixed(0))
 }
 
 async function getOpimismTvl() {
   aux = await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=10')
-  return (aux.amount)
+  return (aux.amount.toFixed(0))
 }
 
 async function getPolygonPOSTvl() {
   aux = await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=137')
-  return (aux.amount)
+  return (aux.amount.toFixed(0))
 }
 
 async function getPolygonZkEVMTvl() {
   aux = await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=1101')
-  return (aux.amount)
+  return (aux.amount.toFixed(0))
 }
 
 async function getModeTvl() {
   aux = await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=34443')
-  return (aux.amount)
+  return (aux.amount.toFixed(0))
 }
 
 async function getLineaTvl() {
   aux = await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=59144')
-  return (aux.amount)
+  return (aux.amount.toFixed(0))
 }
 
 module.exports = {
