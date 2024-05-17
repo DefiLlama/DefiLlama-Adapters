a55c844c8f7ff350dcb2f0e29430f39b805050d3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-17 18:44:27 +0200

    minor fix

--- projects/jaspervault/index.js
+++ projects/jaspervault/index.js
@@ -1,13 +1,13 @@
-const { sumTokensExport } = require("../helper/unwrapLPs");
+const ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokensExport } = require("../helper/unwrapLPs")
 
 module.exports = {
-    arbitrum: {
-        tvl: sumTokensExport({
-            owners: [
-                '0xE980FD1dEA4E93c25B7f5B27351CF069C4f63a41', // sell WBTC ETH CALL & PUT
-            ],
-            fetchCoValentTokens: true,
-        })
-    },
-
+  arbitrum: {
+    tvl: sumTokensExport({
+      owners: [
+        '0xE980FD1dEA4E93c25B7f5B27351CF069C4f63a41', // sell WBTC ETH CALL & PUT
+      ],
+      tokens: [ADDRESSES.arbitrum.WBTC, ADDRESSES.null, ADDRESSES.arbitrum.USDT]
+    })
+  },
 }
