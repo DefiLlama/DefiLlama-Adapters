256d13b7c2e2b8a3cb9e85e5f6a172a298a17719
Simão Pinto <87367287+Sk1mer12@users.noreply.github.com>
2024-05-17 17:11:12 +0100

    Update index.js

--- projects/davos-protocol/index.js
+++ projects/davos-protocol/index.js
@@ -6,27 +6,27 @@ const { sumTokensExport, } = require('../helper/unknownTokens')
 module.exports = {
   methodology: 'collateral TVL * collateral price', 
   arbitrum: {
-    tvl: get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=42161'),
+    tvl: await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=42161'),
   },
   bsc: {
-    tvl: get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=56'),
+    tvl: await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=56'),
   },
   ethereum: {
-    tvl: get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=1'),
+    tvl: await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=1'),
   },
   optimism: {
-    tvl: get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=10'),
+    tvl: await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=10'),
   },
   polygon: {
-    tvl: get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=137'),
+    tvl: await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=137'),
   },
   polygon_zkevm: {
-    tvl: get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=1101'),
+    tvl: await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=1101'),
   },
   mode: {
-    tvl: get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=34443'),
+    tvl: await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=34443'),
   },
   linea: {
-    tvl: get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=59144'),
+    tvl: await get('https://api.prod.davos.xyz/cmc/total-tvl?blockchain=59144'),
   },
 }
