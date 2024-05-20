d3cdf9e3a97ef9d5e6d5b4b76ec4e90c2245b50b
Adolf998 <154962952+Adolf998@users.noreply.github.com>
2024-05-20 18:50:26 +0800

    add bouncebit-lsd & bouncebit-premium (#10276)
    * add bouncebit-lsd & bouncebit-premium
    
    * add bouncebit fixBalancesTokens
    
    * minor fix
    
    ---------
    
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ projects/bouncebit-premium/index.js
@@ -0,0 +1,26 @@
+const { sumTokensExport } = require('../helper/sumTokens');
+
+const BBTC = '0xF5e11df1ebCf78b6b6D26E04FF19cD786a1e81dC'
+const BBUSD = '0x77776b40C3d75cb07ce54dEA4b2Fd1D07F865222'
+const stBBTC = '0x7F150c293c97172C75983BD8ac084c187107eA19'
+
+module.exports = {
+  ethereum: {
+    tvl: sumTokensExport({
+      owners: ["0x1ddD6E5eA766511CC0f348DC8d17578a821B680F", "0xa2B283e4dbdFEA5461C36a59E3B94b3ef2883085"],
+      tokens: [BBTC, BBUSD]
+    }),
+  },
+  bsc: {
+    tvl: sumTokensExport({
+      owners: ["0x55a55e8b08b091bD0529bf1af05b69fF5291867D", "0xdAfd8591402c5E57DCa4B1b9e481c08548a2442E"],
+      tokens: [BBTC, BBUSD]
+    }),
+  },
+  bouncebit: {
+    tvl: sumTokensExport({
+      owners: ["0xd4def93a10ada7e14cAdc6920b6CDE01148D1813", "0x426CD147ff93f31BB18F1Acd19DAb9c32d934131", '0x7F26aB9263E33de947654F44C5AB439090cfAaf7'],
+      tokens: [BBTC, BBUSD, stBBTC]
+    }),
+  },
+};
