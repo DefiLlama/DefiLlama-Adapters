650959a10c71dcb4cbf2fb82fb416d010540dbfe
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-20 12:03:32 +0200

    minor fix

--- projects/bouncebit-lsd/index.js
+++ projects/bouncebit-lsd/index.js
@@ -1,39 +1,11 @@
-const sdk = require("@defillama/sdk");
 const ADDRESSES = require('../helper/coreAssets.json')
-const { getFixBalancesSync } = require('../helper/portedTokens')
 const BBTC = '0xF5e11df1ebCf78b6b6D26E04FF19cD786a1e81dC'
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
-async function bouncebitLSDTvl(_timestamp, block, ...params) {  
-  const balances = {}
-  const BBBalance = (await sdk.api.erc20.totalSupply({ 
-    target: '0x22aAC17E571D6651880d057e310703fF4C7c3483', 
-    block,
-    chain: 'bouncebit'
-   })
-  ).output
-
-  sdk.util.sumSingleBalance(
-    balances,
-    ADDRESSES.null,
-    BBBalance,
-    'bouncebit'
-  );
-
-  const BBTCBalance = (
-    await sdk.api.erc20.balanceOf({
-      target: BBTC,
-      owner: "0x7F150c293c97172C75983BD8ac084c187107eA19",
-      block,
-      chain: 'bouncebit'
-    })
-  ).output;
-
-  sdk.util.sumSingleBalance(balances, BBTC, BBTCBalance, 'bouncebit');
-
-  const fixBalances = getFixBalancesSync('bouncebit')
-
-  fixBalances(balances)
-  return balances;
+async function bouncebitLSDTvl(api) {
+  const BBBalance = await api.call({  abi: 'erc20:totalSupply', target: '0x22aAC17E571D6651880d057e310703fF4C7c3483'})  
+  api.add(ADDRESSES.null, BBBalance)
+  return sumTokens2({ owner: '0x7F150c293c97172C75983BD8ac084c187107eA19', tokens: [BBTC], api })
 }
 
 module.exports = {
