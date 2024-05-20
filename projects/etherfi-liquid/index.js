803a07ff98a4ab0c670602e42a9943a1d6de5858
Vaibhav Valecha <vvalecha@uwaterloo.ca>
2024-05-19 19:03:44 -0500

    add sdk

--- projects/etherfi-liquid/index.js
+++ projects/etherfi-liquid/index.js
@@ -1,16 +1,18 @@
 const ADDRESSES = require('../helper/coreAssets.json')
+const sdk = require('@defillama/sdk')
+
 async function tvl(api) {
   const optimismApi = new sdk.ChainApi({ chain: 'optimism' })
   const balETH = await api.call({
     abi: "uint256:totalAssets",
     target: '0xea1a6307d9b18f8d1cbf1c3dd6aad8416c06a221',
   });
+  await optimismApi.getBlock()
   const wethBal = await optimismApi.call({
     target: '0x72Dbdd6B57934339C36F850a4bDAeD8E488E7062',
     abi: 'uint256:getCategoryTVL',
     params: ['liquid-weth']
   });
-  await optimismApi.getBlock()
   api.add(ADDRESSES.ethereum.EETH, balETH-wethBal);
   api.add(ADDRESSES.ethereum.WETH, wethBal)
   const balUSD = await api.call({
