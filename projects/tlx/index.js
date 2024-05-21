4b9c147948224a82ccf1fbb98e34dc335da9d617
pauljpritz-temp <paulpritz96@gmail.com>
2024-05-21 15:42:15 +0100

    Updated staking stats to include staked TLX and added TVL.

--- projects/tlx/index.js
+++ projects/tlx/index.js
@@ -1,9 +1,13 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
+const helperAbi = require("./leveraged-token-helper-abi.json");
 
 const contracts = {
   tlx: "0xD9cC3D70E730503E7f28c1B407389198c4B75FA2",
+  sUSD: "0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9",
   locker: "0xc068c3261522c97ff719dc97c98c63a1356fef0f",
-}
+  staker: "0xc30877315f3b621a8f7bcda27819ec29429f3817",
+  leveragedTokenHelper: "0xBdAF7A2C4ee313Be468B9250609ba8496131B1f0",
+};
 
 async function staking(api) {
   const lockedTlxBalance = await api.call({
@@ -11,15 +15,34 @@ async function staking(api) {
     target: contracts.locker,
   });
 
-  api.addToken(contracts.tlx, lockedTlxBalance)
-  return sumTokens2({ api })
+  const stakedTlxBalance = await api.call({
+    abi: "uint256:totalStaked",
+    target: contracts.staker,
+  });
+
+  api.addToken(contracts.tlx, lockedTlxBalance);
+  api.addToken(contracts.tlx, stakedTlxBalance);
+  return sumTokens2({ api });
+}
+
+async function tvl(api) {
+  const res = await api.call({
+    abi: helperAbi.leveragedTokenData,
+    target: contracts.leveragedTokenHelper,
+  });
+  const totalSusd = res.reduce((acc, token) => {
+    return acc + (token.totalSupply * token.exchangeRate) / 1e18;
+  }, 0);
+  api.addToken(contracts.sUSD, totalSusd);
+  return sumTokens2({ api });
 }
 
 module.exports = {
   start: 1712731500,
-  methodology: "Total TLX locked in the genesis locker contract.",
+  methodology:
+    "Total TLX locked in the genesis locker contract and total TLX staked in the staking contract. TVL is computed as the total margin deposited across the protocol's leveraged tokens.",
   optimism: {
-    tvl: () => ({}),
+    tvl: tvl,
     staking: staking,
   },
 };
