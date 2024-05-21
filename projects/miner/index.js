42e3a21e35c367ba3ca8dbac86e8fdc51ed44e11
barbat0x <florian.chardel@gmail.com>
2024-05-21 23:44:15 +0200

    add base

--- projects/miner/index.js
+++ projects/miner/index.js
@@ -2,19 +2,17 @@ const { stakings } = require('../helper/staking')
 const { sumTokensExport } = require('../helper/unwrapLPs')
 
 const owner = '0x335D87736b4693E5ED3e5C4f6C737A5a87aFA029' // vault address
-const tokens = [
-    '0x23CbB9F0de3258DE03baaD2BCeA4FCCC55233af0', // MINER
-]
+const token = '0x23CbB9F0de3258DE03baaD2BCeA4FCCC55233af0' // MINER
 
 module.exports = {
     misrepresentedTokens: true,
     ethereum: {
-        tvl: sumTokensExport({ owner, tokens }),
+        tvl: sumTokensExport({ owner, token }),
     },
     base: {
-        tvl: stakings(
-            ['0x335D87736b4693E5ED3e5C4f6C737A5a87aFA029'],
-            ['0x18A8BD1fe17A1BB9FFB39eCD83E9489cfD17a022']
-        ),
+        tvl: sumTokensExport({
+            owner: '0x335D87736b4693E5ED3e5C4f6C737A5a87aFA029',
+            token: '0x18A8BD1fe17A1BB9FFB39eCD83E9489cfD17a022',
+        }),
     },
 }
