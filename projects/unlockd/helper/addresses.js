18d636e2dbf442a0c8aa05fe1a4733585d8bde17
fepvenancio <filipeepv@gmail.com>
2024-05-22 17:41:40 +0100

    unlockd updated adapters

--- projects/unlockd/helper/addresses.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  ethereum: {
-    UiPoolDataProvider: "0xD726b93865393fb3B345C66Da3EB5999B5aC4754",
-    LendPoolAddressProvider: "0xE6cd031FB0D9A79FD8CD3035B9228F78ff459B07",
-    UNFTRegistry: "0x255f25335662c88Fcc7deC402cADf2f4f0E08300",
-  },
-};
--- projects/unlockd/index.js
+++ projects/unlockd/index.js
@@ -1,10 +1,42 @@
+const sdk = require("@defillama/sdk");
 const methodologies = require("../helper/methodologies.js");
-const { tvl, borrowed } = require("./helper/index.js");
+const uTokenAbi = {
+    "totalAvailableSupply": "function totalAvailableSupply(address) view returns (uint256)",
+    "getScaledTotalDebtMarket": "function getScaledTotalDebtMarket(address) view returns (uint256)"
+}
+const addresses = {
+    UTokenVault: "0x25299e9Ec27c242465587B8A2Aa70bcE02636cDA",
+    USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+}
+
+async function tvl() {
+    const reserves = await sdk.api.abi.call({
+        target: addresses.UTokenVault,
+        params: addresses.USDC,
+        abi: uTokenAbi.totalAvailableSupply,
+    });
+
+    return {
+        [addresses.USDC]: reserves.output
+    };
+}
+
+async function borrowed() {
+    const scaledDebt = await sdk.api.abi.call({
+        target: addresses.UTokenVault,
+        params: addresses.USDC,
+        abi: uTokenAbi.getScaledTotalDebtMarket
+    });
+
+    return {
+        [addresses.USDC]: scaledDebt.output
+    };
+}
 
 module.exports = {
     methodology: methodologies.lendingMarket,
     ethereum: {
-      tvl,
-      borrowed,
+        tvl,
+        borrowed,
     },
 };
