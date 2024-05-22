40ed3795f9d0b51c49cb113446bddbef01dd082b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-22 21:26:49 +0200

    refactor

--- projects/linehub-v3/index.js
+++ projects/linehub-v3/index.js
@@ -1,11 +1,5 @@
-const { uniV3GraphExport } = require("../helper/uniswapV3");
+const { uniV3Export } = require("../helper/uniswapV3");
 
-module.exports = {
-  linea: {
-    tvl: uniV3GraphExport({
-      graphURL:
-        "https://api.studio.thegraph.com/query/55804/linehub-v3/version/latest",
-      name: "linehub/linehub-v3",
-    }),
-  },
-};
+module.exports = uniV3Export({
+  linea: { factory: '0x6c379d538f2f7cb642851e154a8e572d63238df4', fromBlock: 407280, },
+})
