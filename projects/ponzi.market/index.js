95b8a8b4c5506c7544901536862785ad7b2f7d7b
QueezZ <queezz.dev@gmail.com>
2024-05-22 09:27:46 +0300

    fix: add onchain tvl calculation

--- projects/ponzi.market/index.js
+++ projects/ponzi.market/index.js
@@ -1,13 +1,30 @@
 const { get } = require("../helper/http");
+const sdk = require("@defillama/sdk");
 
+const ETH_ADDRESS = "0x0000000000000000000000000000000000000000";
 const API_URL =
-  "https://ponzi-backend-production.up.railway.app/api/analytics/tvl";
+  "https://ponzi-backend-production.up.railway.app/api/analytics/games/contracts";
+
 async function tvl() {
-  //fetch the sum of all ETH balances of all game contracts
-  const data = await get(API_URL);
-  return {
-    "0x0000000000000000000000000000000000000000": data?.tvl * 1e18,
-  };
+  //fetch all game contracts
+  const contracts = await get(API_URL);
+
+  const gameContractsBalancesData = await sdk.api.eth.getBalances({
+    targets: contracts,
+    chain: "arbitrum",
+  });
+
+  const balances = {};
+
+  gameContractsBalancesData.output.forEach((contractBalanceData) => {
+    sdk.util.sumSingleBalance(
+      balances,
+      ETH_ADDRESS,
+      contractBalanceData.balance
+    );
+  });
+
+  return balances;
 }
 
 module.exports = {
