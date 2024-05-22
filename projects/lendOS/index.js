a46c334e31afa203d43b0519a7d09848cc195915
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-22 17:20:07 +0200

    code refactor

--- projects/lendOS/index.js
+++ projects/lendOS/index.js
@@ -1,20 +1,7 @@
-const { aaveChainTvl } = require('../helper/aave');
+const { aaveExports } = require('../helper/aave');
 const methodologies = require('../helper/methodologies');
-
-const v3params = ["0x550F9Cc46566560D8d608815Aac34305cADaF569", undefined, ["0x3A1ca459F21D8FAcF9A30bC4773f5dBf07C1191d"]]
-
-function v3(chain) {
-  let params = v3params
-  if (chain === 'neon_evm')
-    params = ['0x550F9Cc46566560D8d608815Aac34305cADaF569', undefined, ['0x3A1ca459F21D8FAcF9A30bC4773f5dBf07C1191d']]
-  const section = borrowed => aaveChainTvl(chain, ...params, borrowed, true);
-  return {
-    tvl: section(false),
-    borrowed: section(true)
-  }
-}
 
 module.exports = {
   methodology: methodologies.lendingMarket,
-  neon_evm: v3("neon_evm"),
-};
+  neon_evm: aaveExports('', undefined, undefined, ['0x3A1ca459F21D8FAcF9A30bC4773f5dBf07C1191d'], { v3: true, }),
+}
