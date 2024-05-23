3939056b66897d07a17722d72f45c4ba51140641
Macaron <164980497+Macaromswap@users.noreply.github.com>
2024-05-23 19:59:23 +0800

    Update index.js

--- projects/macaron-xyz/index.js
+++ projects/macaron-xyz/index.js
@@ -1,3 +1,3 @@
 const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('btr', '0x05daB9D11A0e14E68862cd08A73A7362Bb93a331')
+module.exports = uniTvlExport('btr', '0xB0Cc30795f9E0125575742cFA8e73D20D9966f81')
