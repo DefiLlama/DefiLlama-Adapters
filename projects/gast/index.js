11d51725757f1ffa667497b43fbed3d9bd045b7b
HavenCross <havenquan@qq.com>
2024-05-23 12:29:55 +0800

    add gast

--- projects/gast/index.js
+++ projects/gast/index.js
@@ -1,12 +1,22 @@
 const { sumTokens2, } = require('../helper/unwrapLPs')
 
-async function tvl(api) {
-  
-  return sumTokens2({ owners: [
-    '0xcc92b570ef8117af0ed2ec294f635b70644f13ea',
-  ], tokens: ['0xff2e016a79301633eae9395ca51aaeaf2dc0051a', '0xff204e2681a6fa0e2c3fade68a1b28fb90e4fc5f'], api, }) //gast and wbtc
-}
-
 module.exports = {
-  btr: { tvl, }
+  btr: {
+    tvl: async api => {
+      return sumTokens2({
+        owners: [
+          '0xcc92b570ef8117af0ed2ec294f635b70644f13ea', // gast-wbtc lp
+        ], tokens: ['0xff204e2681a6fa0e2c3fade68a1b28fb90e4fc5f'], api,
+      }) //wbtc
+    },
+  },
+  arbitrum: {
+    tvl: async api => {
+      return sumTokens2({
+        owners: [
+          '0xbd7d96b4598E22eC5302232F6e7870a2d095CAcA', // gast-usdt lp
+        ], tokens: ['0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9'], api,
+      }) //usdt
+    },
+  }
 }
