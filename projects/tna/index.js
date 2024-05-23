448c59d2a6c6e733ba9282fef3a74a03c005f8a8
akai <akai@soully.one>
2024-05-23 10:19:17 +0800

    add tna contract

--- projects/tna/index.js
+++ projects/tna/index.js
@@ -2,7 +2,7 @@ const { sumTokens2, } = require('../helper/unwrapLPs')
 async function tvl(api) {
     return sumTokens2({
         owners: [
-            '0x48c78f36a5b42d215dddc94871d354805c485a18',
+            '0x48c78f36a5b42d215dddc94871d354805c485a18','0x048d86f26952ab5e1f601f897bc9512a1e7fa675'
         ], tokens: ['0x48c78f36a5b42d215dddc94871d354805c485a18'], api,
     }) //wbtc
 }
