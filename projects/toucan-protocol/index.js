82e4f812297b7b2462807af1371e86602cca574c
Michalis Kargakis <kargakis@protonmail.com>
2024-05-24 01:14:35 -0400

    Fix processing CHAR and other Toucan tokens (#10353)
    a7f6110 was incorrectly setting supplies in
    the destructured variables (bct, nct, char)
    as for some networks (eg., base) not all
    three tokens are found. Here I am fixing this
    issue so now the final struct returned by
    getCalculationMethod is created dynamically
    and always contains the exact tokens with
    the correct info.

--- projects/toucan-protocol/index.js
+++ projects/toucan-protocol/index.js
@@ -1,26 +1,43 @@
 const sdk = require('@defillama/sdk')
-const { CONFIG_DATA } = require("./config");
+const { CONFIG_DATA, TOKEN_DATA } = require("./config");
 
 const getCalculationMethod = (chain) => {
   return async (api,) => {
     const supplyCalls = []
+    const tokenInfo = []
     Object.keys(CONFIG_DATA[chain]).map((key) => {
       supplyCalls.push(CONFIG_DATA[chain][key]);
+      tokenInfo.push(TOKEN_DATA[key]);
     })
 
-    let [bct, nct, char] = await api.multiCall({ abi: 'erc20:totalSupply', calls: supplyCalls, })
+    const resp = await api.multiCall({ abi: 'erc20:totalSupply', calls: supplyCalls, })
+    const tokensArray = resp.map((obj, i) => {
+      const validUntil = tokenInfo[i].validUntil
+      if (validUntil && api.timestamp > validUntil)
+        tokenInfo[i].totalSupply = 0
+      else
+        tokenInfo[i].totalSupply = obj
+  
+      return {
+        [tokenInfo[i].coingecko]: dropDecimals(tokenInfo[i].totalSupply),
+      };
+    });
 
-    // If the current block is later than the date BCT was transferred to KlimaDAO, return 0
-    if (api.timestamp > 1709828986)
-      bct = 0
+    const tokens = tokensArray.reduce((acc, cur) => {
+      for (const entry of Object.entries(cur)) {
+        const [key, value] = entry;
+        acc[key] = value;
+      }
+      return acc;
+    } , {});
 
-    return {
-      'toucan-protocol-base-carbon-tonne': (bct ?? 0) / 1e18,
-      'toucan-protocol-nature-carbon-tonne': (nct ?? 0) / 1e18,
-      'biochar': (char ?? 0) / 1e18,
-    };
+    return tokens;
   };
 };
+
+const dropDecimals = (num) => {
+  return (num ?? 0) / 1e18;
+}
 
 const getRegenCredits = () => {
   return async () => {
