e4309a8d70c31286444e6d48d56122b95d9af93f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-24 09:28:52 +0200

    code refactor

--- projects/defusion/index.js
+++ projects/defusion/index.js
@@ -1,20 +1,8 @@
-const { getConfig } = require('../helper/cache')
-
-async function fetchData() {
-  const { data } = await getConfig('viction/staking', 'https://server.defusion.xyz/api/v1/pool?page=1&size=100&sort=-apr')
-
-  const totalStaked = data?.data?.map(item => ({amount: item.totalStaked})).reduce((total,stakeData) => total += stakeData.amount, 0)
-
-  return {
-    'tomochain:0xC054751BdBD24Ae713BA3Dc9Bd9434aBe2abc1ce': BigInt(totalStaked).toString(),
-  }
+async function tvl(api) {
+  const supply = await api.call({  abi: 'erc20:totalSupply', target: '0xCdde1f5D971A369eB952192F9a5C367f33a0A891'})
+  api.add('0xC054751BdBD24Ae713BA3Dc9Bd9434aBe2abc1ce', supply)
 }
 
 module.exports.tomochain = {
-  tvl:  () => ({}),
-  staking: async () => {
-    return await fetchData()
-  }
+  tvl
 } 
-
-module.exports.misrepresentedTokens = true
