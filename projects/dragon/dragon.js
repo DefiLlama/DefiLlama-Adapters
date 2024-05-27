6158c59e165dbb7330e948f880209e983970d1db
spzmya <127633250+spzmya@users.noreply.github.com>
2024-05-27 02:45:50 +0200

    Update dragon.js

--- projects/dragon/dragon.js
+++ projects/dragon/dragon.js
@@ -7,7 +7,6 @@ module.exports = {
   misrepresentedTokens: true,
   methodology: 'TVL accounts for the liquidity on all AMM pools, using the TVL chart on https://corgiswap.info/ as the source. Staking accounts for the CORIS locked in MasterChef (0x60E5Cf9111d046E8F986fC98e37d6703607d5Baf)',
   base: {
-    tvl: getUniTVL({ factory, useDefaultCoreAssets: true, }),
     staking: stakingPricedLP(masterChef, dragonToken, "base", "0xD53881CAee96D3A94Fd0e2Eb027a05fD44d8c470", "weth", true)
   },
 }
