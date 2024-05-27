bc4886633b6c03237d4e006ac498d585fafb11fc
spzmya <127633250+spzmya@users.noreply.github.com>
2024-05-27 03:10:23 +0200

    Update dragon.js

--- projects/dragon/dragon.js
+++ projects/dragon/dragon.js
@@ -1,12 +1,19 @@
-const { stakingPricedLP } = require("./helper/staking");
+const { stakings } = require("../helper/staking");
 
-const dragonToken = '0x528757e34a5617aa3aabe0593225fe33669e921c'
-const masterChef = '0xbb595f34190c6ea1add1c78f6d12df181542763c'
+const stakingContracts = [
+  // stakingContract1 =
+  "0xbb595f34190c6ea1add1c78f6d12df181542763c",
+];
+
+const ETH_DRAGON_UNIV2 = "0xd53881caee96d3a94fd0e2eb027a05fd44d8c470";
+const DRAGON = "0x528757e34a5617aa3aabe0593225fe33669e921c";
 
 module.exports = {
   misrepresentedTokens: true,
-  methodology: 'TVL accounts for the liquidity on all AMM pools, using the TVL chart on https://corgiswap.info/ as the source. Staking accounts for the CORIS locked in MasterChef (0x60E5Cf9111d046E8F986fC98e37d6703607d5Baf)',
   base: {
-    staking: stakingPricedLP(masterChef, dragonToken, "base", "0xD53881CAee96D3A94Fd0e2Eb027a05fD44d8c470", "weth", true)
+    staking: stakings(stakingContracts, DRAGON),
+    pool2: stakings(stakingContracts, [ETH_DRAGON_UNIV2]),
+    tvl: (async) => ({}),
   },
-}
+  methodology: "Counts liquidty on the staking and pool2s only",
+};
