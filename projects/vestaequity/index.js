1a7a79dd2b03d6d6e7db6165ae6c744a1e597973
supermonster <ekaydalov@vestaequity.net>
2024-05-27 09:41:52 -0400

    remove outdated libraries

--- projects/vestaequity/index.js
+++ projects/vestaequity/index.js
@@ -1,22 +1,13 @@
-
-const sdk = require('@defillama/sdk');
 const { toUSDTBalances } = require('../helper/balances');
-const retry = require("async-retry");
-const axios = require("axios");
+const { get } = require('../helper/http');
 
 const VestaEquityTVLApi = "https://app.vestaequity.net/api/listings/tvl/";
 
 async function vestaequityTVL() {
+
   const response = (
-    await retry(
-      async (bail) =>
-        await axios.get(VestaEquityTVLApi, {
-          headers: {
-            'X-API-Client': 'DEFILLAMA'
-          }
-        })
-    )
-  ).data;
+    await get(VestaEquityTVLApi)
+  );
 
   const total = response.reduce((acc, item) => {
     acc = acc + item.tvl;
