f41088ebc152b4f8c80130b9ea35a0ae3120a489
0xngmi <0xngmi@protonmail.com>
2024-05-28 18:41:00 +0100

    new puff contracts

--- projects/puff-penthouse/index.js
+++ projects/puff-penthouse/index.js
@@ -1,13 +1,14 @@
 const { sumTokensExport } = require('../helper/unwrapLPs')
-const { staking } = require('../helper/staking')
+const { stakings } = require('../helper/staking')
 
 module.exports = {
     mantle: {
-        staking: staking("0xBeCd6b3D8B06479c83533f0d7E6DF1b0e413AeEa", "0x26a6b0dcdCfb981362aFA56D581e4A7dBA3Be140"),
+        staking: stakings(
+            ["0xBeCd6b3D8B06479c83533f0d7E6DF1b0e413AeEa", "0x1260140fEa31cf920D7D890aD1de85cbAC1Fea12", "0x35Ee9e36804d358A2892FA0De336426cC3Cb18e4"],
+            "0x26a6b0dcdCfb981362aFA56D581e4A7dBA3Be140"),
         tvl: sumTokensExport({
-            tokensAndOwners: [
-                ["0xcDA86A272531e8640cD7F1a92c01839911B90bb0", "0x0CC41C11878254aF8E65ca61C03DD03735F2DC6d"]
-            ]
+            tokens: ["0xcDA86A272531e8640cD7F1a92c01839911B90bb0"],
+            owners: ["0x0CC41C11878254aF8E65ca61C03DD03735F2DC6d", "0x1260140fEa31cf920D7D890aD1de85cbAC1Fea12", "0x35Ee9e36804d358A2892FA0De336426cC3Cb18e4"]
         }),
     },
 };
