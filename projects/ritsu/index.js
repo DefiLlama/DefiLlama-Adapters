61ae285975101204e49d214031997fb109f04d82
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-28 08:50:21 +0200

    code refactor

--- projects/ritsu/index.js
+++ projects/ritsu/index.js
@@ -1,41 +1,21 @@
-const { getLogs } = require('../helper/cache/getLogs')
-const { transformDexBalances } = require('../helper/portedTokens')
-const sdk = require('@defillama/sdk')
+const { sumTokens2 } = require('../helper/unwrapLPs')
+const { getLogs2 } = require('../helper/cache/getLogs')
 
 async function tvl(api) {
   const { fromBlock, basicFactorys, stableFactorys, rhythmFactorys = [] } = config[api.chain]
 
-  const logs = await Promise.all([...basicFactorys, ...stableFactorys, ...rhythmFactorys].map(factory => (getFactoryLogs(api, factory))));
+  let logs = await Promise.all([basicFactorys, stableFactorys, rhythmFactorys].flat().map(getFactoryLogs))
+  logs = logs.flat()
 
-  const balances = {}
-  const data = []
+  const ownerTokens = logs.map(({ token0, token1, pool }) => [[token0, token1], pool])
+  return sumTokens2({ api, ownerTokens})
 
-  const reserves = await Promise.all(logs.map(log => (api.multiCall({ abi: 'function getReserves() external view returns (uint, uint)', calls: log.map(i => i.pool) }))))
-
-  for (let i = 0; i < logs.length; i++) {
-    if (i < basicFactorys.length) {
-      reserves[i].forEach(([token0Bal, token1Bal], j) => {
-        data.push({ token0Bal, token1Bal, token0: logs[i][j].token0, token1: logs[i][j].token1, })
-      })
-    } else {
-      reserves[i].forEach(([reserve0, reserve1], j) => {
-        sdk.util.sumSingleBalance(balances, logs[i][j].token0, reserve0)
-        sdk.util.sumSingleBalance(balances, logs[i][j].token1, reserve1)
-      })
-    }
-  }
-
-
-  return transformDexBalances({ balances, data, chain: api.chain })
-
-  async function getFactoryLogs(api, factory) {
-    return getLogs({
+  async function getFactoryLogs(target) {
+    return getLogs2({
       api,
-      target: factory,
+      target,
       fromBlock,
-      topic: 'PoolCreated(address,address,address)',
       eventAbi: 'event PoolCreated(address indexed token0, address indexed token1, address pool)',
-      onlyArgs: true,
     })
   }
 }
@@ -47,10 +27,6 @@ const config = {
     basicFactorys: ['0xDFFee0ad5C833f2A5E610dfe9FD1aD82743eA74e'],
     rhythmFactorys: ['0x0A78CAB89a069555a18B78537f09fab24c03dECd']
   }
-}
-
-module.exports = {
-  misrepresentedTokens: true,
 }
 
 Object.keys(config).forEach(chain => { module.exports[chain] = { tvl } })
