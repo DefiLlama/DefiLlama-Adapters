029d4b104b60c327081abacdc7fc6d416468d93a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-29 18:02:25 +0200

    minor fix

--- projects/bluelotusdao/index.js
+++ projects/bluelotusdao/index.js
@@ -1,6 +1,3 @@
-const { uniV3Export } = require("../helper/uniswapV3");
-const factory = "0x5c4619104985163b3839dA465232B6D2a9588E7B";
 
-module.exports = uniV3Export({
-    genesys: { factory, fromBlock: 4725924 },
-});
+const { uniTvlExport } = require('../helper/unknownTokens')
+module.exports = uniTvlExport('genesys', '0x5c4619104985163b3839dA465232B6D2a9588E7B')
--- projects/helper/env.js
+++ projects/helper/env.js
@@ -21,7 +21,6 @@ const DEFAULTS = {
   TAIKO_RPC: 'https://rpc.taiko.xyz', // added manually short name is tko-mainnet
   REAL_RPC_MULTICALL: '0xcA11bde05977b3631167028862bE2a173976CA11',
   TAIKO_RPC_MULTICALL: '0xcb2436774C3e191c85056d248EF4260ce5f27A9D',
-  GENESYS_RPC :"https://rpc.genesys.network",
 }
 
 const ENV_KEYS = [
