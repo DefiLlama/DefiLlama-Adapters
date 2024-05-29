bc13783b6f687e49f8c9edf4d037d6c92a6493d5
Aleksei Lushnikov <al.lushnikov@yandex.ru>
2024-05-29 17:16:44 +0100

    Symbiosis adds Taiko chain (#10408)
    * symbiosis adds Core chain support and some new tokens on portals
    
    * symbiosis adds Taiko chain

--- projects/symbiosis-finance/config.js
+++ projects/symbiosis-finance/config.js
@@ -319,5 +319,14 @@ module.exports = {
          '0x292fC50e4eB66C3f6514b9E402dBc25961824D62', // portal v2
        ]
      },
+     {
+       name: 'taiko',
+       tokens: [
+         ADDRESSES.taiko.WETH,
+       ],
+       holders: [
+         '0x5Aa5f7f84eD0E5db0a4a85C3947eA16B53352FD4', // portal v2
+       ]
+     },
   ]
 }
