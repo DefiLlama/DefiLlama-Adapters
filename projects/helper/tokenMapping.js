a2fbed8012cb577cc82b7b581b6507978c16b056
particle-deployer <130306380+particle-deployer@users.noreply.github.com>
2024-05-29 08:34:03 -0700

    Adding a new vault for Ethena (USDe) integration on Blast (#10424)
    * ethena connection
    
    * minor fix
    
    ---------
    
    Co-authored-by: Wukong Particle <wukong@wukong-mbp.local>
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -78,6 +78,7 @@ const fixBalancesTokens = {
     '0xf7bc58b8d8f97adc129cfc4c9f45ce3c0e1d2692': { coingeckoId: "wrapped-bitcoin", decimals: 8 }, 
     '0x9e0d7d79735e1c63333128149c7b616a0dc0bbdb': { coingeckoId: "weth", decimals: 18 }, //pirex eth
     '0xde55b113a27cc0c5893caa6ee1c020b6b46650c0': { coingeckoId: "deus-finance-2", decimals: 18 }, // deus
+    '0x5d3a1Ff2b6BAb83b63cd9AD0787074081a52ef34': { coingeckoId: "ethena-usde", decimals: 18 },
   },
   solana: {
     'AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR': { coingeckoId: "guacamole", decimals: 5 },
