4e9296e05c6c3666b50abbcee97e2c050a3b438f
Tiago Silva <tfn.da.silva@gmail.com>
2024-05-30 21:04:13 +0100

    Add carbondefi sei evm deployment

--- projects/carbondefi/index.js
+++ projects/carbondefi/index.js
@@ -1,21 +1,33 @@
-const { getLogs } = require('../helper/cache/getLogs')
-const { sumTokens2 } = require('../helper/unwrapLPs')
+const { getLogs } = require("../helper/cache/getLogs");
+const { sumTokens2 } = require("../helper/unwrapLPs");
 
-const controller = '0xC537e898CD774e2dCBa3B14Ea6f34C93d5eA45e1'
+const config = {
+  ethereum: {
+    fromBlock: 17087375,
+    controller: "0xC537e898CD774e2dCBa3B14Ea6f34C93d5eA45e1",
+  },
+  sei_evm: {
+    fromBlock: 79146720,
+    controller: "0xe4816658ad10bF215053C533cceAe3f59e1f1087",
+  },
+};
 
-async function tvl(api) {
-	const logs = await getLogs({
-		api,
-		target: controller,
-		topic: 'PairCreated(uint128,address,address)',
-		eventAbi: 'event PairCreated(uint128 indexed pairId, address indexed token0, address indexed token1)',
-		onlyArgs: true,
-		fromBlock: 17087375,
-	})
-	const tokens = logs.map(i => [i.token0, i.token1]).flat()
+Object.keys(config).forEach((chain) => {
+  const { controller, fromBlock } = config[chain];
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const logs = await getLogs({
+        api,
+        target: controller,
+        topic: "PairCreated(uint128,address,address)",
+        eventAbi:
+          "event PairCreated(uint128 indexed pairId, address indexed token0, address indexed token1)",
+        onlyArgs: true,
+        fromBlock,
+      });
+      const tokens = logs.map((i) => [i.token0, i.token1]).flat();
 
-	return sumTokens2({ api, owner: controller, tokens, })
-}
-
-
-module.exports = { ethereum: { tvl } }
+      return sumTokens2({ api, owner: controller, tokens });
+    },
+  };
+});
