8a112d01ae1e79b14946602d4b48b2410da857f3
brianbodollo <maeby1115@gmail.com>
2024-05-31 11:21:29 +0800

    chore: removed redemption_rate and changed methodology

--- projects/stride-hyperlane/index.js
+++ projects/stride-hyperlane/index.js
@@ -1,24 +1,19 @@
 const sdk = require("@defillama/sdk")
 const { get } = require("../helper/http")
 
-function hyperlaneTvl() {
+function tvl() {
   return async () => {
     const hyperlaneAddressDenom = "ibc/BF3B4F53F3694B66E13C23107C84B6485BD2B96296BB7EC680EA77BBA75B4801"
 
-    const [{ balances: hyperlaneBalances }, { host_zone: hostZone }] = await Promise.all([
-      await get(
-        "https://stride-fleet.main.stridenet.co/api/cosmos/bank/v1beta1/balances/stride1h4rhlwcmdwnnd99agxm3gp7uqkr4vcjd73m4586hcuklh3vdtldqgqmjxc"
-      ),
-      await get(
-        "https://stride-fleet.main.stridenet.co/api/Stride-Labs/stride/staketia/host_zone"
-      )
-    ])
+    const { balances: hyperlaneBalances } = await get(
+      "https://stride-fleet.main.stridenet.co/api/cosmos/bank/v1beta1/balances/stride1h4rhlwcmdwnnd99agxm3gp7uqkr4vcjd73m4586hcuklh3vdtldqgqmjxc"
+    )
   
     const hyperlaneBalance = hyperlaneBalances.find((balance) => balance.denom === hyperlaneAddressDenom)
   
     if (hyperlaneBalance == null) throw new Error("Something went wrong with getting the available hyperlane balance")
   
-    // This defaults to 1e6 since the origin denom of `hyperlaneAddressDenom` is utia
+    // This defaults to 1e6 (for now) since the origin denom of `hyperlaneAddressDenom` is utia
     const coinDecimals = 1e6
   
     const amount = hyperlaneBalance.amount / coinDecimals
@@ -28,7 +23,7 @@ function hyperlaneTvl() {
     sdk.util.sumSingleBalance(
       balances,
       "celestia",
-      amount * hostZone.redemption_rate
+      amount
     )
   
     return balances
@@ -37,8 +32,8 @@ function hyperlaneTvl() {
 
 module.exports = {
   timetravel: false,
-  methodology: "TVL of liquid staked tokens on Stride that are on Hyperlane",
-  "stride-hyperlane": {
-    tvl: hyperlaneTvl()
+  methodology: "TVL of bridged tokens on Stride Hyperlane",
+  "celestia": {
+    tvl
   }
 } // node test.js projects/stride-hyperlane/index.js
