fee7d16351fd200c9af9357bfc1409cdc09d5ed7
neko <lubu.manta@gmail.com>
2024-06-01 17:50:59 +0800

    add nekoswap adapters

---
+++ projects/nekoswap/index.js
@@ -0,0 +1,6 @@
+const { uniTvlExport } = require('../helper/unknownTokens')
+const { mergeExports } = require('../helper/utils')
+
+module.exports = mergeExports([
+  uniTvlExport('btr', '0x8beca6f9b2c9a2c26a05114d01eaddf0b57269df')
+])
