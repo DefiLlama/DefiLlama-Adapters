a7a00b0dd559e51d0f30c77ea858271ae175b7ab
Define101 <93603962+Define101@users.noreply.github.com>
2024-06-03 11:48:03 +0100

    move zun to own tokens (#10470)

--- projects/treasury/zunami.js
+++ projects/treasury/zunami.js
@@ -2,6 +2,7 @@ const { nullAddress, treasuryExports } = require("../helper/treasury");
 const ADDRESSES = require("../helper/coreAssets.json");
 
 const treasury = "0xb056b9a45f09b006ec7a69770a65339586231a34";
+const ZUN = "0x6b5204b0be36771253cc38e88012e02b752f0f36"
 
 module.exports = treasuryExports({
   ethereum: {
@@ -16,6 +17,7 @@ module.exports = treasuryExports({
       "0x73968b9a57c6E53d41345FD57a6E6ae27d6CDB2F", // SDT
       "0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0" // FXS
     ],
-    owners: [treasury]
+    owners: [treasury],
+    ownTokens: [ZUN]
   }
 });
