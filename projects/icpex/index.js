cfec20ad59eea170c99e89df1d5c2f7aaf56ed64
OxC0 <147254343+OxC0@users.noreply.github.com>
2024-06-03 23:18:59 +0800

    Update index.js

--- projects/icpex/index.js
+++ projects/icpex/index.js
@@ -9,11 +9,15 @@ module.exports = {
 }
 
 async function tvl() {
-  let result = await get('https://metrics.icpex.org/llama/tvl')
-  if (result.retCode === 1 && result.retMsg === "success") {
-    const tvl = result.data;
-    return toUSDTBalances(tvl)
-  } else {
-    throw `API error! message: ${result.retMsg}`;
+  try {
+    let result = await get('https://metrics.icpex.org/llama/tvl');
+    if (result.retCode === 1 && result.retMsg === "success") {
+      const tvl = result.data;
+      return toUSDTBalances(tvl);
+    } else {
+      throw new Error(`API error! message: ${result.retMsg}`);
+    }
+  } catch (error) {
+    throw error;
   }
 }
