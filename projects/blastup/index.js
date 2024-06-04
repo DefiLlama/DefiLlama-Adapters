17ecf3149d59e079e63aac689a95e1be3c67272d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-04 15:37:25 +0200

    Update index.js

--- projects/blastup/index.js
+++ projects/blastup/index.js
@@ -1,13 +1,10 @@
-const utils = require("../helper/utils");
-const { stakings } = require("../helper/staking");
-const { toUSDTBalances } = require("../helper/balances");
+const { staking } = require("../helper/staking")
+const stakingpool = "0x0E84461a00C661A18e00Cab8888d146FDe10Da8D"
 
-const stakingpool = "0x0E84461a00C661A18e00Cab8888d146FDe10Da8D";
-
-const ADDRESSES = require("../helper/coreAssets.json");
+const ADDRESSES = require("../helper/coreAssets.json")
 
 module.exports = {
   blast: {
-    tvl: stakings([stakingpool], [ADDRESSES.blast.USDB, ADDRESSES.blast.WETH]),
+    tvl: staking(stakingpool, [ADDRESSES.blast.USDB, ADDRESSES.blast.WETH]),
   },
-};
+}
