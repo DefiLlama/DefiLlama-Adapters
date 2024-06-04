522c50a3d1955036b80ae2e0fda652033baa3847
Tiago Silva <tfn.da.silva@gmail.com>
2024-06-04 14:39:00 +0100

    Use contract call to get pairs instead of getLogs

--- projects/carbondefi/index.js
+++ projects/carbondefi/index.js
@@ -1,4 +1,3 @@
-const { getLogs } = require("../helper/cache/getLogs");
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
 const config = {
@@ -16,16 +15,11 @@ Object.keys(config).forEach((chain) => {
   const { controller, fromBlock } = config[chain];
   module.exports[chain] = {
     tvl: async (api) => {
-      const logs = await getLogs({
-        api,
-        target: controller,
-        topic: "PairCreated(uint128,address,address)",
-        eventAbi:
-          "event PairCreated(uint128 indexed pairId, address indexed token0, address indexed token1)",
-        onlyArgs: true,
-        fromBlock,
-      });
-      const tokens = logs.map((i) => [i.token0, i.token1]).flat();
+      const pairs = await api.call({
+        target:controller,
+        abi: 'function pairs() view returns (address[2][])',
+      })
+      const tokens = [...new Set(pairs.flat())];
 
       return sumTokens2({ api, owner: controller, tokens });
     },
