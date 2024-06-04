585dc855a7a87bbbd90f431473b0470311656744
stepollo <stepolloz@protonmail.com>
2024-06-04 13:36:09 +0200

    create helper functions for different chains

--- projects/vfat/index.js
+++ projects/vfat/index.js
@@ -39,23 +39,24 @@ const config = {
   }
 };
 
+// Helper function to fetch sickles
+async function fetchSickles(api, factory, fromBlockSickle) {
+  const deployLogs = await getLogs({
+    api,
+    target: factory,
+    fromBlock: fromBlockSickle,
+    eventAbi: 'event Deploy(address indexed admin, address sickle)',
+  });
+  return deployLogs.map(log => log.args.sickle);
+}
 
-async function tvl(api) {
-  const { factory, gaugeFactory, gaugeFactory2, voter, NonfungiblePositionManager, fromBlock, fromBlockSickle, chainName } = config[api.chain];
-
-  // Fetch logs from both the factory and the voter contracts
-  const [deployLogs, deployAeroLogs] = await Promise.all([
-    getLogs({
-      api,
-      target: factory,
-      fromBlock: fromBlockSickle,
-      eventAbi: 'event Deploy(address indexed admin, address sickle)',
-    }),
-    getLogs({
-      api,
-      target: voter,
-      fromBlock,
-      eventAbi: `event GaugeCreated(
+// Helper function to fetch and process gauges
+async function fetchGauges(api, voter, fromBlock, gaugeFactory, gaugeFactory2) {
+  const deployAeroLogs = await getLogs({
+    api,
+    target: voter,
+    fromBlock,
+    eventAbi: `event GaugeCreated(
                 address indexed poolFactory,
                 address indexed votingRewardsFactory,
                 address indexed gaugeFactory,
@@ -65,15 +66,9 @@ async function tvl(api) {
                 address gauge,
                 address creator
             )`,
-    }),
-  ]);
+  });
 
-
-  // Get the addresses of deployed sickles
-  const sickles = deployLogs.map(log => log.args.sickle);
-
-  // Separate gauges by type
-  const deployedAeroGauges = deployAeroLogs.reduce(
+  return deployAeroLogs.reduce(
     (acc, log) => {
       const gaugeFactoryAddress = log.args.gaugeFactory;
       const gaugeAddress = log.args.gauge;
@@ -86,46 +81,60 @@ async function tvl(api) {
     },
     { lp: [], nft: [] }
   );
+}
 
-  const stakingTokens = await api.multiCall({ abi: 'address:stakingToken', calls: deployedAeroGauges.lp })
-  const gaugeTokenMapping = {}
+// TVL calculation for Base and Optimism
+async function tvlBaseOptimism(api) {
+  const { factory, gaugeFactory, gaugeFactory2, voter, NonfungiblePositionManager, fromBlock, fromBlockSickle, chainName } = config[api.chain];
+
+  const sickles = await fetchSickles(api, factory, fromBlockSickle);
+  const deployedAeroGauges = await fetchGauges(api, voter, fromBlock, gaugeFactory, gaugeFactory2);
+
+  const stakingTokens = await api.multiCall({ abi: 'address:stakingToken', calls: deployedAeroGauges.lp });
+  const gaugeTokenMapping = {};
   stakingTokens.forEach((stakingToken, index) => {
-    gaugeTokenMapping[deployedAeroGauges.lp[index]] = stakingToken
-  })
+    gaugeTokenMapping[deployedAeroGauges.lp[index]] = stakingToken;
+  });
 
-
-  // Prepare balance queries for each gauge-sickle pair
   const balanceCallsLP = [];
   const tokens = [];
-
   for (const gauge of deployedAeroGauges.lp) {
     for (const sickle of sickles) {
       balanceCallsLP.push({ target: gauge, params: [sickle] });
-      tokens.push(gaugeTokenMapping[gauge])
+      tokens.push(gaugeTokenMapping[gauge]);
     }
   }
 
-  const lpBals = await api.multiCall({ abi: 'erc20:balanceOf', calls: balanceCallsLP, })
-  api.add(tokens, lpBals)
+  const lpBals = await api.multiCall({ abi: 'erc20:balanceOf', calls: balanceCallsLP });
+  api.add(tokens, lpBals);
 
-
-  // process NFT gauges 
-  const pools = await api.multiCall({ abi: 'address:pool', calls: deployedAeroGauges.nft })
-  const slot0s = await api.multiCall({ abi: 'function slot0() view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, bool unlocked)', calls: pools })
-  let i = 0
+  const pools = await api.multiCall({ abi: 'address:pool', calls: deployedAeroGauges.nft });
+  const slot0s = await api.multiCall({ abi: 'function slot0() view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, bool unlocked)', calls: pools });
 
   await Promise.all(deployedAeroGauges.nft.map(async (gauge, i) => {
-    const tick = slot0s[i].tick
-    const nftIds = (await api.multiCall({ abi: 'function stakedValues(address depositor) view returns (uint256[])', calls: sickles, target: gauge })).flat()
-    const positions = await api.multiCall({ abi: 'function positions(uint256 tokenId) view returns (uint96 nonce, address operator, address token0, address token1, int24 tickSpacing, int24 tickLower, int24 tickUpper, uint128 liquidity, uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128, uint128 tokensOwed0, uint128 tokensOwed1)', calls: nftIds, target: NonfungiblePositionManager })
-    positions.forEach(position => addUniV3LikePosition({ ...position, tick, api }))
-  }))
+    const tick = slot0s[i].tick;
+    const nftIds = (await api.multiCall({ abi: 'function stakedValues(address depositor) view returns (uint256[])', calls: sickles, target: gauge })).flat();
+    const positions = await api.multiCall({ abi: 'function positions(uint256 tokenId) view returns (uint96 nonce, address operator, address token0, address token1, int24 tickSpacing, int24 tickLower, int24 tickUpper, uint128 liquidity, uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128, uint128 tokensOwed0, uint128 tokensOwed1)', calls: nftIds, target: NonfungiblePositionManager });
+    positions.forEach(position => addUniV3LikePosition({ ...position, tick, api }));
+  }));
 
-  // unwrap uni v2 like LPs
-  return sumTokens2({ api, resolveLP: true, })
+  return sumTokens2({ api, resolveLP: true });
 }
 
+// TVL calculation for Arbitrum and Linea
+async function tvlArbitrumLinea(api) {
+  const { factory, gaugeFactory, fromBlock, fromBlockSickle, chainName } = config[api.chain];
+
+  const sickles = await fetchSickles(api, factory, fromBlockSickle);
+  console.log(sickles);
+
+  return 0;
+}
 
 Object.keys(config).forEach(chain => {
-  module.exports[chain] = { tvl }
-})
+  if (['base', 'optimism'].includes(chain)) {
+    module.exports[chain] = { tvl: tvlBaseOptimism };
+  } else if (['arbitrum', 'linea'].includes(chain)) {
+    module.exports[chain] = { tvl: tvlArbitrumLinea };
+  }
+});
