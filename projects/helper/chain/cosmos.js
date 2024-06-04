fe797f66398ccce6881872e249602530e19dd222
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-04 17:43:16 +0200

    Add Joltify adapter and move mapping to server (#10498)
    * add joltify adapter
    
    * move mapping to server
    
    ---------
    
    Co-authored-by: briangarden <codingbb1015@gmail.com>

--- projects/helper/chain/cosmos.js
+++ projects/helper/chain/cosmos.js
@@ -41,7 +41,8 @@ const endPoints = {
   sifchain: "https://sifchain-api.polkachu.com",
   nolus: "https://pirin-cl.nolus.network:1317",
   nibiru: "https://lcd.nibiru.fi",
-  bostrom: "https://lcd.bostrom.cybernode.ai"
+  bostrom: "https://lcd.bostrom.cybernode.ai",
+  joltify: "https://lcd.joltify.io"
 };
 
 const chainSubpaths = {
@@ -50,6 +51,7 @@ const chainSubpaths = {
   comdex: "comdex",
   umee: "umee",
   kava: "kava",
+  joltify: "joltify",
 };
 
 // some contract calls need endpoint with higher gas limit
