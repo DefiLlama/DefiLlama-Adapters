91036dc0c9eefc966d2cb1eadda6424b45e0c69c
Jason Mansfield <jmansfield@gmail.com>
2024-06-05 13:58:01 -0400

    Add Polkadex staking TVL

--- projects/polkadex/index.js
+++ projects/polkadex/index.js
@@ -1,6 +1,15 @@
 const { getExports } = require('../helper/heroku-api')
 
+function getPolkadexExports() {
+	let tvl = getExports("polkadex", ['polkadex']);
+	if(tvl.polkadex.staking === undefined) {
+		let staking = getExports("polkadex-staking", ['polkadex']);
+		tvl.polkadex.staking = staking.polkadex.tvl;
+	}
+	return tvl;
+}
+
 module.exports = {
 	timetravel: false,
-	...getExports("polkadex", ['polkadex']),
+	...getPolkadexExports(),
 }
