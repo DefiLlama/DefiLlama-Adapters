fea9b5629075a1075cf27e389186665e3920a3be
TsimakuridzeNikolozi <ntsim20@freeuni.edu.ge>
2024-06-05 18:51:16 +0400

    Refactored to remove in-house API usage

--- projects/hinkal/hinkalUtils.js
+++ projects/hinkal/hinkalUtils.js
@@ -1,7 +1,4 @@
-const utils = require("../helper/utils");
 const sdk = require("@defillama/sdk");
-const RELAYER_URLS = require("./relayerUrls.js");
-const { ethers } = require("ethers");
 const ownerByChain = require("./owners.js");
 
 const nullAddress = "0x0000000000000000000000000000000000000000";
@@ -33,29 +30,12 @@ const getAllTokenBalances = async (tokenList, chain) => {
     })
   ).output;
 
-  const decimalCalls = tokenList.map((token) => ({
-    target: token,
-  }));
-
-  const decimals = (
-    await sdk.api.abi.multiCall({
-      calls: decimalCalls,
-      abi: "erc20:decimals",
-      chain,
-    })
-  ).output;
-
   const tokenBalances = balances.map((bal) => {
     const token = bal.input.target;
-
     const tokenBalance = bal.output;
 
-    const tokenDecimal = decimals?.find(
-      (decimalOutput) => decimalOutput.input.target === token
-    ).output;
-
     return {
-      balance: ethers.formatUnits(tokenBalance, Number(tokenDecimal ?? 18)),
+      balance: tokenBalance,
       address: token,
     };
   });
@@ -71,44 +51,12 @@ const getAllTokenBalances = async (tokenList, chain) => {
 
   tokenBalances.push({
     address: nullAddress,
-    balance: ethers.formatUnits(nativeTokenBalance, 18),
+    balance: nativeTokenBalance,
   });
 
   return tokenBalances.filter((tokenBal) => Number(tokenBal.balance) > 0);
 };
 
-const fetchTotalValue = async (tokenBalances, chainName) => {
-  const tokenAddresses = tokenBalances.map((token) => token.address);
-  const prices = (
-    await utils.postURL(`${RELAYER_URLS[chainName]}/get-token-prices`, {
-      erc20Addresses: tokenAddresses,
-    })
-  ).data.prices;
-
-  const total = tokenBalances.map((token, index) => {
-    const price = prices[index];
-    const tokenBalance = Number(token.balance);
-    if (!price || !tokenBalance || isNaN(price) || isNaN(tokenBalance)) {
-      console.log("Some error occured for token", {
-        token: token.address,
-        balance: tokenBalance,
-        price,
-        chainName,
-      });
-      return {
-        tokenAddress: token.address,
-        tokenBalance: 0,
-      };
-    }
-    return {
-      tokenAddress: token.address,
-      tokenBalance: tokenBalance * price,
-    };
-  });
-  return total.filter((token) => token && token.tokenBalance > 0);
-};
-
 module.exports = {
   getAllTokenBalances,
-  fetchTotalValue,
 };
