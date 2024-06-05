17e5163ebd9e31626ec05d5b7ae863c3b8fb859a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-05 12:59:42 +0200

    code refactor

--- projects/sceptre-staked-flr/index.js
+++ projects/sceptre-staked-flr/index.js
@@ -1,28 +1,13 @@
-const ADDRESSES = require('../helper/coreAssets.json');
-const sdk = require('@defillama/sdk');
-const sflrAbi = require("./sflr.json");
-
 const SFLR = "0x12e605bc104e93B45e1aD99F9e555f659051c2BB";
 
-async function tvl(timestamp, block, chainBlocks) {
-    const pooledFlr = await sdk.api.abi.call({
-        target: SFLR,
-        abi: sflrAbi.totalPooledFlr,
-        chain: "flare",
-        block: chainBlocks.flare,
-    });
-
-    const balances = {};
-    const pooledFlrBalance = pooledFlr.output;
-
-    balances[`flare:${SFLR}`] = pooledFlrBalance;
-
-    return balances;
+async function tvl(api) {
+  const pooledFlr = await api.call({ abi: "uint256:totalPooledFlr", target: SFLR })
+  api.addGasToken(pooledFlr)
 }
 
 module.exports = {
-    flare: {
-        tvl,
-    },
-    methodology: "Counts staked FLR tokens.",
-};
+  flare: {
+    tvl,
+  },
+  methodology: "Counts staked FLR tokens.",
+}
