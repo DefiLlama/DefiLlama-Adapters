5a089132ad6f5c598c52b31b2723720f4715390a
0xjosaphat <jo.saphat.metr@proton.me>
2024-06-05 15:19:00 +0200

    added IOTA EVM as new chain to MagicSea DEX

--- projects/shimmersea/index.js
+++ projects/shimmersea/index.js
@@ -1,3 +1,4 @@
 const { uniTvlExport } = require('../helper/unknownTokens');
 
 module.exports = uniTvlExport('shimmer_evm', '0x4fb5d3a06f5de2e88ce490e2e11d22b840d5ac47')
+module.exports = uniTvlExport('iota_evm', '0x349aaAc3a500014981CBA11b64C76c66a6c1e8D0')
