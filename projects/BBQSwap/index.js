4b2012f2846859cd42c4981c17157e0c9c4e54e7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-06 11:23:54 +0200

    code refactor

--- projects/BBQSwap/index.js
+++ projects/BBQSwap/index.js
@@ -1,3 +1,3 @@
 const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('hamchain', '0x7304e5751973113fA7c4FFf677871B926258f27e')
+module.exports = uniTvlExport('ham', '0x7304e5751973113fA7c4FFf677871B926258f27e')
