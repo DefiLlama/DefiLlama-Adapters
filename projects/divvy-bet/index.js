f0f0bff3646353ff11dbdc0ae2ae2885e7802c8f
kdevdivvy <119331575+kdevdivvy@users.noreply.github.com>
2024-06-06 10:05:00 -0400

    chore: house authority PDA and whitelisted house keys (#10534)
    * chore: house authority PDA and whitelisted house keys
    
    * chore: add SOL owner
    
    * bugfix: Max seed length exceeded
    
    * bugfix: Max seed length exceeded
    
    * chore: aggregate house accounts more scalable
    
    * bugfix: update solOwners mapper
    
    * bugfix: missing params in PDA helper
    
    * bugfix: PDA helper

--- projects/divvy-bet/index.js
+++ projects/divvy-bet/index.js
@@ -1,19 +1,42 @@
 const { Program } = require("@project-serum/anchor");
-const { getProvider, sumTokens2, } = require("../helper/solana");
-const { PublicKey, SystemProgram } = require("@solana/web3.js");
+const { PublicKey } = require("@solana/web3.js");
+const { getProvider, sumTokens2 } = require("../helper/solana");
 
 async function tvl() {
+  function findHouseAuthorityAddress(houseKey, programId) {
+    return PublicKey.findProgramAddressSync(
+      [Buffer.from("House Authority"), houseKey.toBuffer()],
+      programId
+    )[0];
+  }
 
-  const programId = new PublicKey('dvyFwAPniptQNb1ey4eM12L8iLHrzdiDsPPDndd6xAR')
-  const provider = getProvider()
-  const idl = await Program.fetchIdl(programId, provider)
-  const program = new Program(idl, programId, provider)
-  const houses = await program.account.house.all()
-  const tokensAndOwners = houses.map(house => {
-    const owner = PublicKey.findProgramAddressSync([house.publicKey.toBuffer(), Buffer.from(house.account.authorityBump.toString())], SystemProgram.programId)[0]
-    return [house.account.currency, owner]
-  })
-  return sumTokens2({ tokensAndOwners })
+  const NATIVE_MINT = "So11111111111111111111111111111111111111112";
+  const DIVVY_ADMIN = "AHf1MX99d31ebLfAydVPe2vVdgzZGuUaW972znWPNzZY";
+
+  const programId = new PublicKey("dvyFwAPniptQNb1ey4eM12L8iLHrzdiDsPPDndd6xAR");
+  const provider = getProvider();
+  const idl = await Program.fetchIdl(programId, provider);
+  const program = new Program(idl, programId, provider);
+  const houses = await program.account.house.all([
+    {
+      memcmp: {
+        offset: 8,
+        bytes: DIVVY_ADMIN, // houses governed by Divvy's multisig
+      },
+    },
+  ]);
+  const tokensAndOwners = houses
+    .filter((house) => house.account.currency.toBase58() !== NATIVE_MINT)
+    .map((house) => {
+      const owner = findHouseAuthorityAddress(house.publicKey, programId);
+      return [house.account.currency, owner];
+    });
+  const solOwners = houses
+    .filter((house) => house.account.currency.toBase58() === NATIVE_MINT)
+    .map((house) => {
+      return findHouseAuthorityAddress(house.publicKey, programId);
+    });
+  return sumTokens2({ tokensAndOwners, solOwners });
 }
 
 module.exports = {
@@ -21,4 +44,4 @@ module.exports = {
   solana: {
     tvl,
   },
-}
+};
