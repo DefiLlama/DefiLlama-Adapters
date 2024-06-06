24944e11fd6105b06676c2f91d2c3a0e64097e69
MathPoem <79817711+MathPoem@users.noreply.github.com>
2024-06-06 22:49:47 +0300

    Update index.js

--- projects/blasterswap-v3/index.js
+++ projects/blasterswap-v3/index.js
@@ -3,7 +3,6 @@ const { uniV3Export } = require("../helper/uniswapV3");
 const { mergeExports } = require('../helper/utils')
 
 module.exports = mergeExports([
-main
 	uniV3Export({ blast: { factory: "0x1A8027625C830aAC43aD82a3f7cD6D5fdCE89d78", fromBlock: 4308657, }, }),
 ])
 
