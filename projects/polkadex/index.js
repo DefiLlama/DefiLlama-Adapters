c540f13512ffe970d8082dd9dd8e1355118eb531
Jason Mansfield <jmansfield@gmail.com>
2024-06-06 09:47:14 -0400

    Change export to include staking

--- projects/polkadex/index.js
+++ projects/polkadex/index.js
@@ -1,15 +1,6 @@
 const { getExports } = require('../helper/heroku-api')
 
-function getPolkadexExports() {
-	let tvl = getExports("polkadex", ['polkadex']);
-	if(tvl.polkadex.staking === undefined) {
-		let staking = getExports("polkadex-staking", ['polkadex']);
-		tvl.polkadex.staking = staking.polkadex.tvl;
-	}
-	return tvl;
-}
-
 module.exports = {
 	timetravel: false,
-	...getPolkadexExports(),
+	...getExports("polkadex", ['polkadex'], ['staking'])
 }
