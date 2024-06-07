56d46ca48d1a5c4f8c8e584bdfe5a4b7eca76469
aiants <aiants@nvidia.com>
2024-06-07 18:30:16 +0400

    [MellowProtocol] update TVL script

--- projects/MellowProtocol/index.js
+++ projects/MellowProtocol/index.js
@@ -3,21 +3,14 @@ const sdk = require('@defillama/sdk')
 
 const config = {
   ethereum: {
-    registry: '0xfd23f971696576331fcf96f80a20b4d3b31ca5b2',
-    fromBlock: 15237714,
+    vaults: [
+      '0xf3bF8720De4741133FC59fb69650A42f4DEdFd58',
+      '0xAeA3824ED5Fe391c05CD96D1042645EE13Cbf745',
+      '0xa77a8D25cEB4B9F38A711850751edAc70d7b91b0',
+      '0xbf0311df31af8b027a12051c00d02aa85a322594'
+    ],
+    fromBlock: 20039678,
   },
-  polygon: {
-    registry: '0xd3d0e85f225348a2006270daf624d8c46cae4e1f',
-    fromBlock: 31243728,
-  },
-  polygon_zkevm: {
-    registry: '0xc02a7B4658861108f9837007b2DF2007d6977116',
-    fromBlock: 2665891,
-  },
-  base: {
-    registry: '0xc02a7B4658861108f9837007b2DF2007d6977116',
-    fromBlock: 2785683,
-  }
 }
 
 module.exports = {
@@ -25,38 +18,16 @@ module.exports = {
 };
 
 Object.keys(config).forEach(chain => {
-  const { registry, fromBlock, } = config[chain]
+  const { vaults, fromBlock, } = config[chain]
   module.exports[chain] = {
     tvl: async (api) => {
       const balances = {}
-      const logs = await getLogs({
-        api,
-        target: registry,
-        topic: 'VaultRegistered(address,address,uint256,address,address)',
-        fromBlock,
-        eventAbi: 'event VaultRegistered (address indexed origin, address indexed sender, uint256 indexed nft, address vault, address owner)',
-        onlyArgs: true,
-        extraKey: 'VaultRegistered'
-      })
-      const vaultKeys = {}
-      logs.forEach(i => vaultKeys[i.nft] = i.vault)
-      const tokenLockedLogs = await getLogs({
-        api,
-        target: registry,
-        topic: 'TokenLocked(address,address,uint256)',
-        fromBlock,
-        eventAbi: 'event TokenLocked (address indexed origin, address indexed sender, uint256 indexed nft)',
-        onlyArgs: true,
-        extraKey: 'TokenLocked'
-      })
-      tokenLockedLogs.forEach(i => delete vaultKeys[i.nft])
-      let vaults = Object.values(vaultKeys)
-      const tokens = await api.multiCall({ abi: 'function vaultTokens() view returns (address[])', calls: vaults })
-      const bals = await api.multiCall({ abi: 'function tvl() view returns (uint256[] minTokenAmounts, uint256[] maxTokenAmounts)', calls: vaults, permitFailure: true })
-      tokens.forEach((tokens, i) => {
-        if (!bals[i]) return;
-        let balsInner = bals[i].minTokenAmounts
-        tokens.forEach((v, i) => sdk.util.sumSingleBalance(balances, v, balsInner[i], api.chain))
+      const baseTvl = await api.multiCall({ abi: 'function baseTvl() public view returns (address[], uint256[])', calls: vaults })
+      baseTvl.forEach((tvl, i) => {
+
+        const tokens = tvl[0]
+        const amounts = tvl[1]
+        tokens.forEach((v, j) => sdk.util.sumSingleBalance(balances, v, amounts[j], api.chain))
       })
       return balances
     }
