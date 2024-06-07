e31729bc6618f0d21fb4c4ea88a70e28c201a46d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-07 16:34:52 +0200

    minor refactor

--- projects/agentfi/index.js
+++ projects/agentfi/index.js
@@ -6,18 +6,12 @@ const { getAllAgent } = require("./utils");
 
 
 async function tvl(api) {
-    const multiCallAddress = "0xcA11bde05977b3631167028862bE2a173976CA11"
     const allAgents = await getAllAgent(api)
     const allAgentsAddress = allAgents.map(i => i.agentAddress)
     const dexBalancerAgents = allAgents.filter(i => i.moduleType === "DexBalancer")
     const concentratedLiquidityAgents = allAgents.filter(i => i.moduleType === "ConcentratedLiquidity")
     const looperAgentsAddresses = allAgents.filter(i => i.moduleType === "Looper").map(i => i.agentAddress)
 
-    const USDB = allAgentsAddress.map(ag => [tokenAddress.USDB, ag])
-    const ETH = allAgentsAddress.map(ag => ({
-        target: multiCallAddress, params: [ag]
-    }))
-    const WETH = allAgentsAddress.map(ag => [tokenAddress.WETH, ag])
 
     const thrusterv2 = dexBalancerAgents.map(i => [uniV2Lp.thruster, i.agentAddress])
     const blasterswapv2 = dexBalancerAgents.map(i => [uniV2Lp.blasterswap, i.agentAddress])
@@ -28,16 +22,10 @@ async function tvl(api) {
     const thrusterV3 = concentratedLiquidityAgents.map(i => [uniV3NftManager.thruster, i.agentAddress])
 
 
-    const ethBalancePromise = api.multiCall({
-        abi: 'function getEthBalance(address addr) view returns (uint256 balance)',
-        calls: ETH,
-    })
-    const dexBalancerPromise = getTvlForDexBalancer(dexBalancerAgents.map(i => i.agentAddress), api)
-    const looperPromise = getTvlForLooperWithOrbit(looperAgentsAddresses, api)
-    const sumTokenPromise = sumTokens2({
+    await getTvlForDexBalancer(dexBalancerAgents.map(i => i.agentAddress), api)
+    await  getTvlForLooperWithOrbit(looperAgentsAddresses, api)
+    await sumTokens2({
         tokensAndOwners: [
-            ...USDB,
-            ...WETH,
             ...thrusterv2,
             ...blasterswapv2,
             ...ringv2,
@@ -50,9 +38,7 @@ async function tvl(api) {
         resolveLP: true,
         api,
     })
-
-    const [ethBalance] = await Promise.all([ethBalancePromise, sumTokenPromise, looperPromise, dexBalancerPromise])
-    ethBalance.forEach(i => api.add(nullAddress, i))
+    await api.sumTokens({ owners: allAgentsAddress, tokens: [tokenAddress.USDB, tokenAddress.WETH, nullAddress], })
 }
 
 module.exports = {
