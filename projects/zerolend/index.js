4da1d47a3b9e31ed10bf2238d30596b300aabf28
Deadshot Ryker <151393124+deadshotryker@users.noreply.github.com>
2024-06-08 18:23:25 +0400

    Added ZERO staking and ZERO/ETH LP staking tvl for zerolend (#10564)
    * added xlayer for zerolend
    
    * added staking and lp for linea chain on zerolend
    
    * added zerolend treasury

--- projects/zerolend/index.js
+++ projects/zerolend/index.js
@@ -1,4 +1,6 @@
 const { aaveExports } = require("../helper/aave");
+const { staking } = require("../helper/staking");
+const { pool2s } = require("../helper/pool2");
 
 const config = {
   era: "0xB73550bC1393207960A385fC8b34790e5133175E",
@@ -9,8 +11,22 @@ const config = {
   ethereum: "0x47223D4eA966a93b2cC96FFB4D42c22651FADFcf",
 };
 
+const linea = {
+  treasury: "0x14aAD4668de2115e30A5FeeE42CFa436899CCD8A",
+  zeroEthNileLP: "0x0040f36784dda0821e74ba67f86e084d70d67a3a",
+  zeroEthNileCLP: "0x179b4B1C19faF18Bed713304c870e9317bc79A84",
+  zeroEthLynexCLP: "0xb88261e0DBAAc1564f1c26D78781F303EC7D319B",
+  zero: "0x78354f8DcCB269a615A7e0a24f9B0718FDC3C7A7",
+  zlpLocker: "0x8bb8b092f3f872a887f377f73719c665dd20ab06",
+  zeroLocker: "0x08D5FEA625B1dBf9Bae0b97437303a0374ee02F8",
+};
+
+const data = {};
 Object.keys(config).forEach((chain) => {
-  module.exports[chain] = aaveExports(chain, undefined, undefined, [
-    config[chain],
-  ]);
+  data[chain] = aaveExports(chain, undefined, undefined, [config[chain]]);
 });
+
+data.linea.staking = staking(linea.zeroLocker, linea.zero, "linea");
+data.linea.pool2 = pool2s([linea.zlpLocker], [linea.zeroEthNileLP], "linea"); // todo add the lynex and nile LPs from the treasury
+
+module.exports = data;
