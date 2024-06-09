6879ad8ca5e64cca9ff139b6f1c47661e71d49b0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-09 10:52:54 +0200

    update penpie

--- projects/penpie/index.js
+++ projects/penpie/index.js
@@ -3,7 +3,7 @@ const config = require("./config");
 const { staking } = require('../helper/staking')
 
 async function tvl(api) {
-  const { masterPenpie, pendleStaking, vePENDLE, PENDLE, mPENDLE, } = config[api.chain];
+  const { masterPenpie, vlPNP } = config[api.chain];
 
   const poolTokens = await api.fetchList({
     lengthAbi: MasterMagpieAbi.poolLength,
@@ -11,13 +11,11 @@ async function tvl(api) {
     target: masterPenpie,
   });
   const poolInfos = await api.multiCall({ abi: 'function getPoolInfo(address) view returns ( uint256 emission, uint256 allocpoint, uint256 sizeOfPool, uint256 totalPoint)', calls: poolTokens, target: masterPenpie, })
-  const symbols = await api.multiCall({ abi: 'erc20:symbol', calls: poolTokens, })
   poolTokens.forEach((token, i) => {
-    if (symbols[i] === 'vlPenpie' || symbols[i] === 'mPendle' || symbols[i] === 'mPendleOFT' || symbols[i] === 'mPendleSV') {
-      token = PENDLE
-    }
     api.add(token, poolInfos[i].sizeOfPool)
   })
+  if (vlPNP)
+    api.removeTokenBalance(vlPNP)
 }
 
 Object.keys(config).forEach((chain) => {
