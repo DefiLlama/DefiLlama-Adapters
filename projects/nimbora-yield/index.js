d60b192c6cde01eabf1e55566e82cd2270e6aec0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-10 10:34:34 +0200

    cache response

--- projects/nimbora-yield/index.js
+++ projects/nimbora-yield/index.js
@@ -1,6 +1,7 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { call, parseAddress } = require('../helper/chain/starknet')
-const axios = require('axios')
+const { getConfig } = require('../helper/cache')
+const { get } = require('../helper/http')
 
 const totalAssetsAbi = {
   "name": "total_assets",
@@ -15,27 +16,27 @@ const totalAssetsAbi = {
   "stateMutability": "view"
 }
 
-
+async function fetcher() {
+  return get('https://backend.nimbora.io/yield-dex/strategies', {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+  })
+}
 
 async function tvl(api) {
-  let endpoint = 'https://backend.nimbora.io/yield-dex/strategies'
-  let strategyData;
-  try {
-    strategyData = (await axios.get(endpoint, {headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}})).data;
-  } catch (error) {
-    console.error('Error fetching strategy data:', error.response ? error.response.data : error.message);
+  const strategyData = await getConfig('nimbora-yield', undefined, { fetcher })
+  for (let index = 0; index < strategyData.length; index++) {
+    const strategyInfo = strategyData[index];
+    const underlying = parseAddress(strategyInfo.underlying) == "0x05574eb6b8789a91466f902c380d978e472db68170ff82a5b650b95a58ddf4ad" ? ADDRESSES.starknet.DAI : parseAddress(strategyInfo.underlying);
+    const strategyTvl = await call({ target: strategyInfo.tokenManager, abi: totalAssetsAbi });
+    api.add(underlying, strategyTvl)
   }
-    for (let index = 0; index < strategyData.length; index++) {
-      const strategyInfo = strategyData[index];
-      const underlying = parseAddress(strategyInfo.underlying) == "0x05574eb6b8789a91466f902c380d978e472db68170ff82a5b650b95a58ddf4ad" ? ADDRESSES.starknet.DAI : parseAddress(strategyInfo.underlying);
-      const strategyTvl = await call({ target: strategyInfo.tokenManager, abi: totalAssetsAbi });
-      api.add(underlying, strategyTvl)
-    }
 }
 
 module.exports = {
-    methodology: 'Computed by summing the total assets held by each strategy.',
-    starknet: {
-        tvl
-    },
+  methodology: 'Computed by summing the total assets held by each strategy.',
+  starknet: {
+    tvl
+  },
 }
