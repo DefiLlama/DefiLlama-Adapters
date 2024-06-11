0673dba55ac0d262067d96ac5cde71ad47ca0f7a
neko <lubu.manta@gmail.com>
2024-06-11 15:07:27 +0800

    replace factory address

--- projects/nekoswap/index.js
+++ projects/nekoswap/index.js
@@ -2,5 +2,5 @@ const { uniTvlExport } = require('../helper/unknownTokens')
 const { mergeExports } = require('../helper/utils')
 
 module.exports = mergeExports([
-  uniTvlExport('btr', '0x8beca6f9b2c9a2c26a05114d01eaddf0b57269df')
+  uniTvlExport('btr', '0x4ABB962572B2057A0Ef1E939243F243f7D9f188c')
 ])
