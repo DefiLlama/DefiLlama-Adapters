080f0c1779b44fca53a0d3e0098ded9bd5d1f088
x <x>
2024-06-11 08:45:19 +0200

    rename

--- projects/thevault/index.js
+++ projects/thevault/index.js
@@ -1,9 +1,9 @@
 const { getTokenSupply } = require("../helper/solana")
 
 async function tvl(api) {
-  const bSOL = 'vSoLxydx6akxyMD9XEcPvGYNGq6Nn66oqVb3UkGkei7'
-  const supply = await getTokenSupply(bSOL)
-  api.add(bSOL, supply * 1e9)
+  const vSOL = 'vSoLxydx6akxyMD9XEcPvGYNGq6Nn66oqVb3UkGkei7'
+  const supply = await getTokenSupply(vSOL)
+  api.add(vSOL, supply * 1e9)
 }
 
 module.exports = {
