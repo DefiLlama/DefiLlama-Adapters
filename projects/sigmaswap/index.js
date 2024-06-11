f19070d0c5aebe60f538fd802add0c6be055e8d4
Szymon Paluch <12szymon.paluch34@gmail.com>
2024-06-11 10:24:30 +0200

    feat: change approach to use prebuilt function

--- projects/sigmaswap/index.js
+++ projects/sigmaswap/index.js
@@ -1,16 +1,13 @@
-const ethers = require("ethers");
+const { sumTokens2, nullAddress } = require("../helper/unwrapLPs");
 
-const BRIDGE_CONTRACT = "0xE58a7D7E726CD266c103cC7C2763f4a3005d78B1";
-const LUKSO_RPC_URL = "https://42.rpc.thirdweb.com";
+const contract = "0xE58a7D7E726CD266c103cC7C2763f4a3005d78B1";
 
 async function tvl(api) {
-  const provider = new ethers.JsonRpcProvider(LUKSO_RPC_URL);
-  const balance = await provider.getBalance(BRIDGE_CONTRACT);
-
-  api.add(BRIDGE_CONTRACT, balance);
+  return sumTokens2({ tokens: [nullAddress], owner: contract, api });
 }
 
 module.exports = {
+  methodology: `We count the LYX on ${contract}`,
   lukso: {
     tvl,
   },
