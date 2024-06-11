1160efe6ddf092ef76d7eb6f9e5d44bede4bc4c4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-11 11:02:43 +0200

    code refactor

--- projects/catalist/index.js
+++ projects/catalist/index.js
@@ -1,24 +1,14 @@
-const ADDRESSES = require("../helper/coreAssets.json");
-const sdk = require("@defillama/sdk");
-
 const aceContract = "0xec46d5a0ee47e585fab59a15976d0f2413bfbb82";
 
-async function ace(timestamp, aceBlock, chainBlocks) {
-  const pooledACE = await sdk.api.abi.call({
-    block: aceBlock,
-    target: aceContract,
-    abi: "uint256:getTotalPooledAce",
-  });
-
-  return {
-    [ADDRESSES.null]: pooledACE.output,
-  };
+async function ace(api) {
+  const pooledACE = await api.call({ target: aceContract, abi: "uint256:getTotalPooledAce", })
+  api.addCGToken('endurance', pooledACE / 1e18)
 }
 
 module.exports = {
   methodology:
     "Staked tokens are counted as TVL based on the chain that they are staked on and where the liquidity tokens are issued.",
-  endurance: {
+  ace: {
     tvl: ace,
   },
 };
