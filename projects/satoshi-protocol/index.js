54053a0d0c83460c3056ea0b6cb7e73969bf7a62
imfeng <fantasrulai@gmail.com>
2024-06-11 17:07:32 +0800

    update satoshi-protocol add bitlayer chain tvl (#10581)

--- projects/satoshi-protocol/index.js
+++ projects/satoshi-protocol/index.js
@@ -1,17 +1,21 @@
 const { sumTokens2 } = require("../helper/unwrapLPs")
 
-const TROVE_MANAGER_LIST = [
-  '0x0598Ef47508Ec11a503670Ac3B642AAE8EAEdEFA', // WBTC Collateral
-  '0xa794a7Fd668FE378E095849caafA8C8dC7E84780', // wstBTC Collateral
-]
-
-async function tvl(api) {
-  const tokens = await api.multiCall({  abi: 'address:collateralToken', calls: TROVE_MANAGER_LIST})
-  return sumTokens2({ api, tokensAndOwners2: [tokens, TROVE_MANAGER_LIST]})
+function createExports(troveList) {
+  return {
+    tvl: async (api) => {
+      const tokens = await api.multiCall({ abi: 'address:collateralToken', calls: troveList })
+      return sumTokens2({ api, tokensAndOwners2: [tokens, troveList] })
+    },
+  }
 }
 
 module.exports = {
-  bevm: {
-    tvl,
-  }
+  bevm: createExports([
+    '0x0598Ef47508Ec11a503670Ac3B642AAE8EAEdEFA', // BEVM WBTC Collateral
+    '0xa794a7Fd668FE378E095849caafA8C8dC7E84780', // BEVM wstBTC Collateral
+  ]),
+  btr: createExports([
+    '0xf1A7b474440702BC32F622291B3A01B80247835E', // BITLAYER WBTC Collateral
+    '0xe9897fe6C8bf96D5ef8B0ECC7cBfEdef9818232c', // BITLAYER stBTC Collateral
+  ]),  
 }
