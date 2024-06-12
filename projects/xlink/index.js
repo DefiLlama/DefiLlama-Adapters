fe7f86bda7cca3a40dbd9faef375cadce629c0f4
fiftyeightandeight <chan.ahn@gmail.com>
2024-06-12 15:31:34 +0800

    handle stacks separately

--- projects/xlink/index.js
+++ projects/xlink/index.js
@@ -7,22 +7,22 @@ const { call } = require('../helper/chain/stacks-api')
 async function tvl(api) {
     const factories = config[api.chain];    
 
-    if(api.chain == 'stacks'){
-        let lqstx = 0;
-        let abtc = 0;
-        let alex = 0;
-        for(const owner of factories.owners){
-            lqstx += Number((await call({ target: 'SM26NBC8SFHNW4P1Y4DFH27974P56WN86C92HPEHH.token-vlqstx', abi: 'get-share', inputArgs: [{ type: 'principal', value: owner}]})).value);
-            abtc += Number((await call({ target: 'SP2XD7417HGPRTREMKF748VNEQPDRR0RMANB7X1NK.token-abtc', abi: 'get-balance', inputArgs: [{ type: 'principal', value: owner}]})).value);
-            alex += Number((await call({ target: 'SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.token-alex', abi: 'get-balance', inputArgs: [{ type: 'principal', value: owner}]})).value);
-        }
+    // if(api.chain == 'stacks'){
+    //     let lqstx = 0;
+    //     let abtc = 0;
+    //     let alex = 0;
+    //     for(const owner of factories.owners){
+    //         lqstx += Number((await call({ target: 'SM26NBC8SFHNW4P1Y4DFH27974P56WN86C92HPEHH.token-vlqstx', abi: 'get-share', inputArgs: [{ type: 'principal', value: owner}]})).value);
+    //         abtc += Number((await call({ target: 'SP2XD7417HGPRTREMKF748VNEQPDRR0RMANB7X1NK.token-abtc', abi: 'get-balance', inputArgs: [{ type: 'principal', value: owner}]})).value);
+    //         alex += Number((await call({ target: 'SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.token-alex', abi: 'get-balance', inputArgs: [{ type: 'principal', value: owner}]})).value);
+    //     }
         
-        return {
-            blockstack: lqstx / 1e6,
-            bitcoin: abtc / 1e8,
-            alexgo: alex / 1e8
-        } 
-    }   
+    //     return {
+    //         blockstack: lqstx / 1e6,
+    //         bitcoin: abtc / 1e8,
+    //         alexgo: alex / 1e8
+    //     } 
+    // }   
     return sumTokens({ chain: api.chain, owners: factories.owners, tokens: factories.tokens });
 }
 
