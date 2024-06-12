ba7c0e03dd0d5153d9b93095e0de90eaa78950d9
HJNJu <hyojin.ju@gmx.net>
2024-06-12 11:19:01 +0900

    fix: chain name

--- projects/catalist/index.js
+++ projects/catalist/index.js
@@ -3,9 +3,11 @@ const sdk = require("@defillama/sdk");
 
 const aceContract = "0xec46d5a0ee47e585fab59a15976d0f2413bfbb82";
 
-async function ace(timestamp, aceBlock, chainBlocks) {
+async function ace(timestamp, ethBlock, { endurance: block }) {
+  const chain = "ace";
   const pooledACE = await sdk.api.abi.call({
-    block: aceBlock,
+    block,
+    chain,
     target: aceContract,
     abi: "uint256:getTotalPooledAce",
   });
