c94ca64dc113df2df9eb8150da41a4f50c3ac600
hbeltik <134815749+hbeltik@users.noreply.github.com>
2024-06-12 17:59:18 -0400

    clean up

--- projects/3jane/emv.js
+++ projects/3jane/emv.js
@@ -1,12 +1,9 @@
-const ADDRESSES = require("../helper/coreAssets.json")
 const sdk = require("@defillama/sdk")
 const abi = require("./abi.json")
-const { sumTokens2, nullAddress } = require("../helper/unwrapLPs")
+const { sumTokens2 } = require("../helper/unwrapLPs")
 
 // Ethereum Vaults
 const eEthCallVault = "0xAcD147A5bbCB7166c5BB13A9354ad7a59b99fB4d"
-
-const threeJaneDeployer = "0x1226858E04b9d077258F153275613734421cD06B"
 
 // Ethereum Assets
 const weEth = "0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee"
@@ -37,13 +34,13 @@ async function addVaults({
 
 async function ethTvl(_, block) {
   const balances = {}
-  const vaults = [[weEth, eEthCallVault]]
+  const vaults = [[eEthCallVault]]
 
   await addVaults({ balances, block, vaults })
-  // pauser holds a variety of coins
+
   return sumTokens2({
     balances,
-    tokens: [nullAddress, weEth],
+    tokens: [weEth],
     block,
   })
 }
