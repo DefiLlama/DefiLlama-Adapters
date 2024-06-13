71a22d8ede68c88e24a4ef82987bb48751008727
realdealshaman <realdealshaman11@gmail.com>
2024-06-13 20:42:28 +0100

    fix syntax

--- projects/prime-protocol/index.js
+++ projects/prime-protocol/index.js
@@ -1,5 +1,4 @@
 const sdk = require("@defillama/sdk");
-const sdk = require('@defillama/sdk');
 const { sumTokens2, } = require('../helper/unwrapLPs');
 const { cachedGraphQuery } = require('../helper/cache');
 
