b97be7fde0a4b048ee9ae5c92ff8d8b37598a9bc
Timo Neumann <timoneumann@timos-mbp.localdomain>
2024-06-13 17:27:12 +0200

    update methodlology

--- projects/fyde/index.js
+++ projects/fyde/index.js
@@ -1,7 +1,6 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const FYDE_CONTRACT = "0x87Cc45fFF5c0933bb6aF6bAe7Fc013b7eC7df2Ee";
 const RESTAKING_AGGREGATOR = "0x3f69F62e25441Cf72E362508f4d6711d53B05341";
-const YIELD_MANAGER = "0xB615A7E4D1Ed426470Ac2Df14F3153fA2DcCC3ba";
 const ORACLE = "0x05198327206123E89c24ABd9A482316449bD2aEe"
 const WETH = ADDRESSES.ethereum.WETH;
 
@@ -19,7 +18,6 @@ async function tvl(api) {
 
   api.add(WETH, amountStakedETH)
 
-  
   // add tokens deployed to yield module. Check which token from tokens is a yield-token, map to the underlying and 
   // add balances to TVL
   const response = (await api.multiCall({
@@ -46,7 +44,7 @@ async function tvl(api) {
 }
 
 module.exports = {
-  methodology: 'Read out balances from internal accounting for each asset in Fyde. Add ETH staked in LRT Aggregator.',
+  methodology: 'Read out balances from internal accounting for each asset in Fyde in the YieldModule. Add ETH staked in LRT Aggregator.',
   ethereum: {
     tvl
   }
