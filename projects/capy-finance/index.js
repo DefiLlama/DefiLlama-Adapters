ae82fa6758d4f667f5a4442485414f51376475f6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-13 18:39:33 +0200

    code refactor

--- projects/capy-finance/index.js
+++ projects/capy-finance/index.js
@@ -1,54 +1,11 @@
-const ADDRESSES = require('../helper/coreAssets.json');
-
-const wbtc = ADDRESSES.bsquared.WBTC;
-const usdt = ADDRESSES.bsquared.USDT;
-const usdc = ADDRESSES.bsquared.USDC;
-const matic = ADDRESSES.bsquared.MATIC;
-const fdusd = ADDRESSES.bsquared.FDUSD;
-const bstone = ADDRESSES.bsquared.BSTONE;
-const ordi = ADDRESSES.bsquared.ORDI;
-const sats = ADDRESSES.bsquared.SATS;
-const ubtc = ADDRESSES.bsquared.UBTC;
-const weth = ADDRESSES.bsquared.ETH;
-
 const CAPY_STAKING_CONTRACT = '0x67D171A673FfDBd5BBce01dE1489f9E57F3d911b';
 const CAPY_RESTAKING_CONTRACT = '0x12178d2B86031dD293274A0E25c8908521F3d27C';
 
-const tokensInCapyStaking = [
-  wbtc,
-  usdt,
-  usdc,
-  matic,
-  fdusd,
-  bstone,
-  ordi,
-  sats,
-  ubtc,
-  weth,
-];
-
-const tokensInCapyReStaking = [wbtc, usdt, usdc, fdusd, weth];
-
 async function tvl(api) {
-  for (const token of tokensInCapyStaking) {
-    const balance = await api.call({
-      abi: 'erc20:balanceOf',
-      target: token,
-      params: [CAPY_STAKING_CONTRACT],
-    });
-
-    api.add(token, balance);
-  }
-
-  for (const token of tokensInCapyReStaking) {
-    const balance = await api.call({
-      abi: 'erc20:balanceOf',
-      target: token,
-      params: [CAPY_RESTAKING_CONTRACT],
-    });
-
-    api.add(token, balance);
-  }
+  const contracts = [CAPY_STAKING_CONTRACT, CAPY_RESTAKING_CONTRACT]
+  const tokens = await api.multiCall({ abi: 'address[]:getSupportedTokens', calls: contracts })
+  const ownerTokens = contracts.map((contract, i) => [tokens[i], contract])
+  return api.sumTokens({ ownerTokens })
 }
 
 module.exports = {
@@ -57,4 +14,4 @@ module.exports = {
   bsquared: {
     tvl,
   },
-};
+}
