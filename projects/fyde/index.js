36f2f389b1be6c491a73abafe0d22496f75fb208
Timo Neumann <timoneumann@timos-mbp.localdomain>
2024-06-13 16:58:18 +0200

    update Fyde TVL to include yieldModule

--- projects/fyde/index.js
+++ projects/fyde/index.js
@@ -1,6 +1,8 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const FYDE_CONTRACT = "0x87Cc45fFF5c0933bb6aF6bAe7Fc013b7eC7df2Ee";
 const RESTAKING_AGGREGATOR = "0x3f69F62e25441Cf72E362508f4d6711d53B05341";
+const YIELD_MANAGER = "0xB615A7E4D1Ed426470Ac2Df14F3153fA2DcCC3ba";
+const ORACLE = "0x05198327206123E89c24ABd9A482316449bD2aEe"
 const WETH = ADDRESSES.ethereum.WETH;
 
 async function tvl(api) {
@@ -16,6 +18,31 @@ async function tvl(api) {
   });
 
   api.add(WETH, amountStakedETH)
+
+  
+  // add tokens deployed to yield module. Check which token from tokens is a yield-token, map to the underlying and 
+  // add balances to TVL
+  const response = (await api.multiCall({
+    calls: tokens.map(token => ({
+      target: ORACLE,
+      params: [token]
+    })),
+    abi: 'function yieldTokenToToken(address) external view returns (address)',
+    withMetadata: true,
+  })).map(x => x.output);
+
+  let tokenInYieldManager = [];
+  let balInYieldManager = []; 
+
+  for (let i = 0; i < tokens.length; i++) {
+    if (response[i] !== '0x0000000000000000000000000000000000000000') {
+      tokenInYieldManager.push(response[i]);
+      balInYieldManager.push(bals[i]);
+    }
+  }
+
+  api.addTokens(tokenInYieldManager, balInYieldManager)
+  
 }
 
 module.exports = {
