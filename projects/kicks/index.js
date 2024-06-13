7fd9ba77218f66e5d72168682d1fe4c7553ea371
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-13 18:00:04 +0200

    minor fix

--- projects/kicks/index.js
+++ projects/kicks/index.js
@@ -1,19 +1,12 @@
-const STAKING = "0x7f435dC15CD9d686C48B6beD2cB8d32D895cfb93";
-const TOKEN = "0xfEB4e9B932eF708c498Cc997ABe51D0EE39300cf";
 
-async function staking(time, ethBlock, _b, { api }) {
-  const supply = await api.call({
-    abi: "function totalSupply() external view returns (uint256)",
-    target: STAKING,
-  });
-
-  return api.add(TOKEN, supply)
-}
+const { staking } = require('../helper/staking')
+const STAKING = "0x7f435dC15CD9d686C48B6beD2cB8d32D895cfb93"
+const TOKEN = "0xfEB4e9B932eF708c498Cc997ABe51D0EE39300cf"
 
 module.exports = {
   methodology: "TVL counted from the KICKS contracts",
   bsc: {
     tvl: () => ({}),
-    staking,
+    staking: staking(STAKING, TOKEN),
   },
-};
+}
