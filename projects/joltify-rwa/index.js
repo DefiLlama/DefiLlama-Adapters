a730591388a366890169281c936efd65ffcdaa88
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-14 10:04:23 +0200

    joltify: exclude borrowed

--- projects/joltify-rwa/index.js
+++ projects/joltify-rwa/index.js
@@ -6,15 +6,7 @@ const tvl = async (api) => {
     queryV1Beta1({ chain, url: `spv/list_pools` }),
   ]);
 
-  const [price_info] = await Promise.all([
-    queryV1Beta1({ chain, url: `third_party/pricefeed/v1beta1/prices` }),
-  ]);
-
   pools.pools_info.forEach(async pool => {
-    const [market, borrowed_denom] = pool.borrowed_amount.denom.split('-');
-    const market_id = `${market}:usd`;
-    const price = price_info.prices.find(price => price.market_id === market_id)?.price;
-    api.add(borrowed_denom, pool.borrowed_amount.amount * price);
     api.add(pool.usable_amount.denom, pool.usable_amount.amount);
     api.add(pool.escrow_principal_amount.denom, pool.escrow_principal_amount.amount);
     api.add(pool.escrow_principal_amount.denom, pool.escrow_interest_amount);
