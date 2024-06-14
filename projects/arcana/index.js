f85effa25e159b70d878fb43d8d42437ed1eb338
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-14 11:38:23 +0200

    code refactor

--- projects/arcana/index.js
+++ projects/arcana/index.js
@@ -1,12 +1,9 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const arcUSD = ADDRESSES.real.arcUSD
-
 module.exports = {
   misrepresentedTokens: true,
   real: { tvl }
 }
 
 async function tvl(api) {
-  const supply = await api.call({ abi: 'uint256:circulatingSupply', target: arcUSD })
-  api.addCGToken('tether', supply/1e18)
+  const supply = await api.call({ abi: 'uint256:circulatingSupply', target: '0xaec9e50e3397f9ddc635c6c429c8c7eca418a143' })
+  api.addCGToken('tether', supply / 1e18)
 }
