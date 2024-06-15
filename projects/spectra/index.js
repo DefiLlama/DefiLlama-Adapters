2bc778787ff1af21d2315581ea9b4efc4dc7bc6f
ulydev <ulydev@live.fr>
2024-06-15 13:18:19 +0200

    fix: add event topics

--- projects/spectra/index.js
+++ projects/spectra/index.js
@@ -7,6 +7,11 @@ module.exports = {
   methodology: `All deposited underlying in Spectra Principal Tokens and all underlying supplied as liquidity in Spectra Markets`,
 };
 
+const curvePoolDeployedTopic =
+  "0x3c7b686d948efcba31c9cfd1aeae78faac70fe0c1ed90d151d49c75e85027a91";
+const ptDeployedTopic =
+  "0xcf50c3e7162cc35f5befd4f0379ddd760d499ca96330c9ae8faa4059919caaee";
+
 Object.keys(config).forEach((chain) => {
   const { factory, fromBlock } = config[chain];
   module.exports[chain] = {
@@ -89,6 +94,7 @@ Object.keys(config).forEach((chain) => {
     const logs = await getLogs({
       api,
       target: factory,
+      topic: curvePoolDeployedTopic,
       eventAbi:
         "event CurvePoolDeployed(address indexed poolAddress, address indexed ibt, address indexed pt)",
       onlyArgs: true,
@@ -101,6 +107,7 @@ Object.keys(config).forEach((chain) => {
     const logs = await getLogs({
       api,
       target: factory,
+      topic: ptDeployedTopic,
       eventAbi:
         "event PTDeployed(address indexed pt, address indexed poolCreator)",
       onlyArgs: true,
