74b9e8c15357c3ca338ee6f1e7cf8168e94447d4
realdealshaman <realdealshaman11@gmail.com>
2024-06-15 18:35:38 +0100

    hasStablePools=true

--- projects/fenix-v2/index.js
+++ projects/fenix-v2/index.js
@@ -1,3 +1,12 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
+const { getUniTVL } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('blast', '0xa19c51d91891d3df7c13ed22a2f89d328a82950f')
+module.exports = {
+  misrepresentedTokens: true,
+  blast: {
+    tvl: getUniTVL({
+      factory: '0xa19c51d91891d3df7c13ed22a2f89d328a82950f', 
+      useDefaultCoreAssets: true,
+      hasStablePools: true
+    })
+  },
+};
