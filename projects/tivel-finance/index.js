eb9c4f029c47768111bbe73e1aee1f48deb27f14
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-17 09:23:49 +0200

    code refactor

--- projects/tivel-finance/index.js
+++ projects/tivel-finance/index.js
@@ -1,24 +1,14 @@
-const { getPoolAddresses, getPoolDetails, tvl } = require('./v1');
-
-function getTvl(chain, factoryAddress) {
-  return async (api)=> {
-    const balances = api.getBalances()
-    const { block } = api
-
-    // get list of pools
-    const poolAddresses = await getPoolAddresses(factoryAddress, block, chain)
-    // get pools detail
-    const [tokens, reserves, withdrawingLiquidities] = await getPoolDetails(poolAddresses, block, chain)
-    // sum tvl
-    tvl(balances, tokens, reserves, withdrawingLiquidities)
-  }
+const config = {
+  era: '0x846FcA826196B3D674fd1691Bb785F3E4216bc0F'
 }
 
-module.exports = {
-  methodology: "Counts the tokens locked in the contracts to earn yield. Withdrawing tokens are not counted towards the TVL.",
-  era: {
-    tvl: getTvl("era", "0x846FcA826196B3D674fd1691Bb785F3E4216bc0F"),
-  },
-};
-
-// node test.js projects/tivel-finance/index.js
+Object.keys(config).forEach(chain => {
+  const factory = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const pools = await api.fetchList({ lengthAbi: 'poolLength', itemAbi: 'pools', target: factory })
+      const tokens = await api.multiCall({ abi: 'address:quoteToken', calls: pools })
+      return api.sumTokens({ tokensAndOwners2: [tokens, pools] })
+    }
+  }
+})
