5d5ae8300c94817431c991bdcd42341308fd8a58
realdealshaman <realdealshaman11@gmail.com>
2024-06-17 20:16:26 +0100

    add sanko

--- projects/camelot/index.js
+++ projects/camelot/index.js
@@ -6,4 +6,7 @@ module.exports = {
   arbitrum: {
     tvl: getUniTVL({ factory: '0x6EcCab422D763aC031210895C81787E87B43A652', useDefaultCoreAssets: true,}),
   },
+  sanko: {
+    tvl: getUniTVL({factory: '0x7d8c6B58BA2d40FC6E34C25f9A488067Fe0D2dB4', useDefaultCoreAssets: true})
+  }
 };
