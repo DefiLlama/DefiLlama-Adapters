4452d3114129377d71af145abe3a789750ac73af
re traca <ggpt98@gmail.com>
2024-06-18 17:39:22 +0100

    fix

--- projects/scoreplay/index.js
+++ projects/scoreplay/index.js
@@ -1,11 +1,31 @@
-const { get } = require("../helper/http");
+const { GraphQLClient, gql } = require("graphql-request");
+const ethers = require("ethers");
 
 async function tvl() {
-  // var response = await get("http://192.168.68.110:3000/api/defillama/tvl");
-  var response = await get("https://www.scoreplay.xyz/api/defillama/tvl");
-  return { ethereum: Number(response.totalEthBalance) };
+  var graphQLClient = new GraphQLClient(
+    "https://api.studio.thegraph.com/query/70998/scoreplay-euro24/version/latest"
+  );
+
+  const getTotalTvl = gql`
+    {
+      tvlActivity(id: "0x01") {
+        totalVolume
+      }
+    }
+  `;
+  const results = await graphQLClient.request(getTotalTvl);
+
+  const totalVolume = parseFloat(
+    ethers.formatEther(results.tvlActivity.totalVolume)
+  );
+
+  return {
+    ethereum: totalVolume,
+  };
 }
 
 module.exports = {
+  methodology: "sums the eth balance in each team for LeagueFactory contract.",
+  start: 15582196,
   base: { tvl },
 };
