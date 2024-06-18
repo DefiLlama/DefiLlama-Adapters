737da50d836649d6cafff17c530d75e424ee99aa
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-18 19:28:42 +0200

    code refactor

--- projects/pac-finance/address.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  blast: {
-    UiPoolDataProvider: "0x1205529dc4fe0844039099cf7125aEF38B7c058F",
-    PoolAddressProvider: "0x688B5fd3C3E3724b4De08C4BCB3A755F9b579c9a",
-  },
-};
