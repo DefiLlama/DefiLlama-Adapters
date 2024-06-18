f1c59f27a1b362c36fd4bfc49d5b8b516f744a7c
jfzhou5 <1241330802@qq.com>
2024-06-18 22:19:32 +0800

    Fix: pac-finance tvl data

--- projects/pac-finance/index.js
+++ projects/pac-finance/index.js
@@ -1,6 +1,49 @@
-const { aaveExports } = require("../helper/aave");
+const sdk = require("@defillama/sdk");
+const { sumTokens2 } = require("../helper/unwrapLPs");
 
-// https://docs.pac.finance/developer/mainnet-addresses
+const UiPoolDataProviderABI = require("./UiPoolDataProvider.json");
+const address = require("./address");
+
+async function tvl(api) {
+  const { UiPoolDataProvider, PoolAddressProvider } = address[api.chain];
+  let [reservesData] = await api.call({
+    target: UiPoolDataProvider,
+    params: PoolAddressProvider,
+    abi: UiPoolDataProviderABI.getReservesData,
+  });
+
+  const balances = {};
+
+  let toa = reservesData.map((i) => [i.underlyingAsset, i.aTokenAddress]);
+
+  return sumTokens2({ chain: api.chain, balances, tokensAndOwners: toa });
+}
+
+async function borrowed(api) {
+  const { UiPoolDataProvider, PoolAddressProvider } = address[api.chain];
+  let [reservesData] = await api.call({
+    target: UiPoolDataProvider,
+    params: PoolAddressProvider,
+    abi: UiPoolDataProviderABI.getReservesData,
+  });
+
+  const balances = {};
+
+  reservesData.forEach((d) => {
+    sdk.util.sumSingleBalance(
+      balances,
+      d.underlyingAsset,
+      d.totalScaledVariableDebt * d.variableBorrowIndex * 1e-27,
+      api.chain
+    );
+  });
+
+  return balances;
+}
+
 module.exports = {
-  blast: aaveExports("blast", undefined, undefined, ["0x742316f430002D067dC273469236D0F3670bE446",], { hasV2LPs: true }),
+  blast: {
+    tvl,
+    borrowed,
+  },
 };
