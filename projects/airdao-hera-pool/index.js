85c35d3b3c6d2387e368346bf60891ccf8da3b54
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-19 07:25:30 +0200

    minor fix

--- projects/airdao-hera-pool/index.js
+++ projects/airdao-hera-pool/index.js
@@ -1,21 +1,10 @@
-const sdk = require('@defillama/sdk');
-
-const POOL_ADDRESS = '0x0E051C8C1cd519d918DB9b631Af303aeC85266BF';
-
 async function tvl(api) {
-  const balances = {};
-
-  const totalStake = await api.call({abi: 'uint256:totalStake', target: POOL_ADDRESS})
-
-  sdk.util.sumSingleBalance(balances, 'amber', totalStake / 10**18)
-
-  return balances;
+  const totalStake = await api.call({abi: 'uint256:totalStake', target: '0x0E051C8C1cd519d918DB9b631Af303aeC85266BF'})
+  api.addCGToken('amber', totalStake/1e18)
 }
 
 module.exports = {
   methodology: `TVL counts deposits made to Hera pool on AirDAO.`,
-  misrepresentedTokens: true,
-  timetravel: false,
   airdao: {
     tvl
   }
