ec50dd2e02a940191f4472a437abc8a154de9d25
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-19 13:15:50 +0200

    fix jax

--- projects/jax-protocol/index.js
+++ projects/jax-protocol/index.js
@@ -1,5 +1,5 @@
 const { compoundExports2 } = require('../helper/compound')
 
 module.exports = {
-  taiko: compoundExports2({ comptroller: '0x7c710A21b2B993Bfe34E5C075B4Cd2594aca60AF' }),
+  taiko: compoundExports2({ comptroller: '0x8D86d4070b9432863FE9522B2c931C410085E1d4', cether: '0xdc1af71e6b9b4572cdf7832496efbea06cbecfc5', }),
 }
