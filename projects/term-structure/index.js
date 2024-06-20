a0fd100009d45b01c0bf4fbd7c84ea4ecc72abf4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-20 17:54:03 +0200

    minor fix

--- projects/term-structure/index.js
+++ projects/term-structure/index.js
@@ -1,35 +1,16 @@
-const ADDRESSES = require('../helper/coreAssets.json')
+const ADDRESSES = require('../helper/coreAssets.json');
+const { sumTokens2 } = require('../helper/unwrapLPs');
 
 const ZkTrueUpContractAddress = "0x09E01425780094a9754B2bd8A3298f73ce837CF9";
-const EthTokenAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
 
 module.exports = {
   ethereum: {
     tvl: async (api) => {
-      const tokenNums = await api.call({
-        target: ZkTrueUpContractAddress,
-        abi: "function getTokenNum() external view returns (uint16)",
-      });
-
-      const assetConfigs = await api.multiCall({
-        target: ZkTrueUpContractAddress,
-        abi: "function getAssetConfig(uint16 tokenId) external view returns (bool isStableCoin, bool isTsbToken, uint8 decimals, uint128 minDepositAmt, address token)",
-        calls: new Array(Number(tokenNums)).fill(0).map((_, i) => ({
-          params: [i+1],
-        }))
-      })
-
-      const tokens = assetConfigs.map(assetConfig => {
-        const isETH2WETH = assetConfig.token ===  EthTokenAddress;
-        return isETH2WETH ? ADDRESSES.ethereum.WETH : assetConfig.token;
-      });
-
-      return await api.sumTokens({
-        owners: [
-          ZkTrueUpContractAddress,
-        ],
-        tokens: tokens,
-      });
+      const infoAbi = "function getAssetConfig(uint16 tokenId) external view returns (bool isStableCoin, bool isTsbToken, uint8 decimals, uint128 minDepositAmt, address token)"
+      const tokenInfo = await api.fetchList({ lengthAbi: 'getTokenNum', itemAbi: infoAbi, target: ZkTrueUpContractAddress, startFrom: 1 })
+      const tokens = tokenInfo.map(i => i.token)
+      tokens.push(ADDRESSES.ethereum.WETH)
+      return sumTokens2({ api, tokens, owner: ZkTrueUpContractAddress })
     },
   },
 };
