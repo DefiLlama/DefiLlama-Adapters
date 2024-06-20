dcef23bddf1f0490d82202ea5a58f5061a618ecd
Abner <fantasrulai@gmail.com>
2024-06-20 18:14:42 +0800

    Update Term Structure TVL

--- projects/term-structure/index.js
+++ projects/term-structure/index.js
@@ -3,13 +3,31 @@ const { sumTokens2 } = require("../helper/unwrapLPs");
 const ZkTrueUpContractAddress = "0x09E01425780094a9754B2bd8A3298f73ce837CF9";
 module.exports = {
   ethereum: {
-    tvl: (api) =>
-      sumTokens2({
+    tvl: async (api) => {
+      const tokenNums = await api.call({
+        target: ZkTrueUpContractAddress,
+        abi: "function getTokenNum() external view returns (uint16)",
+      });
+
+      const assetConfigs = await api.multiCall({
+        target: ZkTrueUpContractAddress,
+        abi: "function getAssetConfig(uint16 tokenId) external view returns (bool isStableCoin, bool isTsbToken, uint8 decimals, uint128 minDepositAmt, address token)",
+        calls: new Array(Number(tokenNums)).fill(0).map((_, i) => ({
+          // target: ZkTrueUpContractAddress,
+          params: [i+1],
+        }))
+      })
+
+      const tokens = assetConfigs.map(assetConfig => assetConfig[4]);
+
+      return sumTokens2({
         api,
+        tokens: tokens,
         owners: [
           ZkTrueUpContractAddress,
         ],
-        fetchCoValentTokens: true,
-      }),
+      });
+
+    },
   },
 };
