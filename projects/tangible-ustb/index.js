dc4cba9f21e8b7ceaf717c1de10a44a96eccf5f6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-20 21:29:14 +0200

    minor fix

--- projects/tangible-ustb/index.js
+++ projects/tangible-ustb/index.js
@@ -1,10 +1,8 @@
+const { sumTokensExport } = require('../helper/unwrapLPs')
 const USTB = '0x83fedbc0b85c6e29b589aa6bdefb1cc581935ecd'
 
 module.exports = {
   ethereum: {
-    tvl: async (api) => {
-      const supply = await api.call({ abi: 'erc20:totalSupply', target: USTB })
-      api.add(USTB, supply)
-    },
+    tvl: sumTokensExport({ owner: USTB, tokens: ["0x59D9356E565Ab3A36dD77763Fc0d87fEaf85508C"]}),
   },
 }
