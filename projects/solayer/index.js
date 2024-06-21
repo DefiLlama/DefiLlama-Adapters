5478fcae78e3b6258cb3e3899b3e08b81cc91462
Jason <lxjpub@gmail.com>
2024-06-21 00:55:38 -0700

    refactor: Update solayer index.js for TVL calculation and methodology

--- projects/solayer/index.js
+++ projects/solayer/index.js
@@ -1,16 +1,28 @@
-const utils = require('../helper/utils');
-const totlaTvlEndpoint = "https://app-staging.solayer.org/api/total_tvl"
+const { sumTokens2, getConnection } = require("../helper/solana");
+const { PublicKey } = require("@solana/web3.js");
 
 async function tvl() {
-  const totalTvl = await utils.fetchURL(totlaTvlEndpoint)
-  const totalTvlValue = parseFloat(totalTvl.data);
-  return totalTvlValue
+  const connection = getConnection();
+  const [account, lst] = await Promise.all([
+    connection.getAccountInfo(new PublicKey('po1osKDWYF9oiVEGmzKA4eTs8eMveFRMox3bUKazGN2')),
+    sumTokens2({
+      tokensAndOwners: [
+        ['J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn', 'GgTE2exWZ36Q82FoVgEEzEHYCfsbGjm3P6zRfx3hLUv4'],
+        ['mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So', 'E9LmYVKU5oyjWs9Zmzv9ji8NkzhJxJQbUEH3FWDKZt8D'],
+        ['bSo13r4TkiE4KumL71LsHTPpL2euBYLFx6h9HP3piy1', '2DRZbbse5b5souvMQkifpS8CRBsDeLt6a9xDqqVJvmdw'],
+        ['5oVNBeEEQvYi1cX3ir8Dx5n1P7pdxydbGF2X4TxVusJm', 'GF8jvNGY44tnCfhnzdoSUBpgfog9YnLc6BRBCnt8j9do']
+      ],
+    })
+  ]);
+
+  return {
+    solana: Number(account.data.readBigUint64LE(258)) / 1e9,
+    ...lst
+  };
 }
 
 module.exports = {
   timetravel: false,
-  solana:{
-    fetch: tvl
-  },
-  methodology: 'TVL consists of deposits made to the protocol',
-}
+  methodology: "TVL is calculated by summing all re-staked assets.",
+  solana: { tvl },
+};
