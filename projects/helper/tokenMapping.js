55caf87ed9b84720f1f3509e0a1e11b4d01c1044
allush <al.lushnikov@yandex.ru>
2024-06-25 13:02:06 +0100

    symbiosis adds Fraxtal chain

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -202,6 +202,9 @@ const fixBalancesTokens = {
     '0xc5ed6c946cdc82f4599f0f2f012e1822502e70e3': { coingeckoId: 'artificial-neural-network-ordinals', decimals: 18 },
     '0x0a3bb08b3a15a19b4de82f8acfc862606fb69a2d': { coingeckoId: 'izumi-bond-usd', decimals: 18 },
   },
+  fraxtal: {
+    [ADDRESSES.fraxtal.WETH]: { coingeckoId: 'weth', decimals: 18 },
+  },
 }
 
 ibcChains.forEach(chain => fixBalancesTokens[chain] = { ...ibcMappings, ...(fixBalancesTokens[chain] || {}) })
