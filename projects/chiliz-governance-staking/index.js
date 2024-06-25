e14f5540bb0281929857092c24ba315af7c4c6fb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-25 15:10:22 +0200

    minor fix

--- projects/chiliz-governance-staking/index.js
+++ projects/chiliz-governance-staking/index.js
@@ -1,20 +1,9 @@
-const sdk = require("@defillama/sdk");
-const ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
 const STAKING_CONTRACT = '0x0000000000000000000000000000000000001000';
-
-async function tvl(api) {
-  const {output: balance} = await sdk.api.eth.getBalance({
-    target: STAKING_CONTRACT,
-    block: api.block,
-    chain: "chz"
-  });
-  api.add(ADDRESSES.null, balance)
-}
 
 module.exports = {
   methodology: 'Total CHZ Locked in Staking System Contract.',
   chz: {
-    tvl,
+    tvl: sumTokensExport({ owner: STAKING_CONTRACT, tokens: [nullAddress] }),
   }
-}; 
-
+}
