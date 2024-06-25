c5f52cf8ef1d1731cffdf91ecf3f3373c24773d0
vspaxo <110825143+vspaxo@users.noreply.github.com>
2024-06-25 14:59:16 +0530

    add boba network to wefi (#10756)
    Co-authored-by: Vinay Sagar <vinaysagar@Vinays-MacBook-Pro.local>

--- projects/paxo-finance/index.js
+++ projects/paxo-finance/index.js
@@ -6,6 +6,7 @@ module.exports = {
   polygon: compoundExports('0x1eDf64B621F17dc45c82a65E1312E8df988A94D3', 'polygon'),
   xdc: compoundExports('0x301C76e7b60e9824E32991B8F29e1c4a03B4F65b', 'xdc'),
   linea: compoundExports('0x301C76e7b60e9824E32991B8F29e1c4a03B4F65b', 'linea'),
+  boba: compoundExports('0x301C76e7b60e9824E32991B8F29e1c4a03B4F65b', 'boba'),
   methodology: `${lendingMarket}. TVL is calculated by getting the market addresses from comptroller and calling the getCash() on-chain method to get the amount of tokens locked in each of these addresses.`,
 }
 
