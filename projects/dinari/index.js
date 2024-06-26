9c96a51979ba4375206ed4ba9dd645385c3e9368
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-26 15:04:16 +0530

    Dinari: Add kinto (#10781)
    * feat: dinari kinto tvl
    
    * minor fix
    
    ---------
    
    Co-authored-by: jaketimothy <jake.a.timothy@gmail.com>

--- projects/dinari/index.js
+++ projects/dinari/index.js
@@ -1,25 +1,26 @@
 const config = {
   arbitrum: {
     factory: "0xB4Ca72eA4d072C779254269FD56093D3ADf603b8",
-    getTokensAbi: "function getDShares() external view returns (address[] memory, address[] memory)",
     usdplus: "0xfc90518D5136585ba45e34ED5E1D108BD3950CFa"
   },
   ethereum: {
     factory: "0x60B5E7eEcb2AEE0382db86491b8cFfA39347c747",
-    getTokensAbi: "function getDShares() external view returns (address[] memory, address[] memory)",
     usdplus: "0x98C6616F1CC0D3E938A16200830DD55663dd7DD3"
   },
   blast: {
     factory: "0x6Aa1BDa7e764BC62589E64F371A4022B80B3c72a",
-    getTokensAbi: "function getDShares() external view returns (address[] memory, address[] memory)"
+  },
+  kinto: {
+    factory: "0xE4Daa69e99F48AD0C4D4843deF4447253248A906",
+    usdplus: "0x6F086dB0f6A621a915bC90295175065c9e5d9b8c"
   }
 }
 
-async function getTokens(api, chain) {
+async function getTokens(api) {
+  const abi = config[api.chain].getTokensAbi ?? "function getDShares() external view returns (address[] memory, address[] memory)"
   return (await api.call({
-    chain: chain,
-    target: config[chain].factory,
-    abi: config[chain].getTokensAbi
+    target: config[api.chain].factory,
+    abi
   }))[0];
 }
 
