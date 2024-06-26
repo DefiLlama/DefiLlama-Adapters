64a3e82092dde5e471e66988e26d3be7d923eede
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-26 10:56:08 +0200

    minor fix

--- projects/wbrockstaking/index.js
+++ projects/wbrockstaking/index.js
@@ -1,19 +1,16 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { staking } = require("../helper/staking");
 
-const WBROCK = ADDRESSES.bitrock.WBR;
 const stakingContract = "0x1a71F508d536c7Ab1D1B53a5D261abD494524C96";
-const BITROCKETH = ADDRESSES.bitrock.BR;
 const BITROCKETHstakingContract = "0x46363C31Be0c677Bd6F3eD429686753794ee8b97";
 
 module.exports = {
   bitrock: {
-    tvl: staking(stakingContract, WBROCK, "bitrock"),
+    tvl: staking(stakingContract, ADDRESSES.bitrock.WBR),
   },
   ethereum: {
-    tvl: staking(BITROCKETHstakingContract, BITROCKETH, "ethereum"),
+    tvl: staking(BITROCKETHstakingContract, ADDRESSES.bitrock.BR),
   },
-  
   methodology: "Wrapped Bitrock tokens locked in staking contract",
 };
 
