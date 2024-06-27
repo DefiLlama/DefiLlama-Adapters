8977cd5948e9a1600994fec98e6aa6da7477c5ec
fextr <extr1811@gmail.com>
2024-06-27 12:32:03 +0400

    use curve oracle for the rlp price

--- projects/resolv/index.js
+++ projects/resolv/index.js
@@ -1,24 +1,13 @@
 const USR = "0x66a1E37c9b0eAddca17d3662D6c05F4DECf3e110";
 const RLP = "0x4956b52aE2fF65D74CA2d61207523288e4528f96";
-const RLPPriceStorage = "0x31319866778a5223633bd745780BB6d59406371E";
 
 const erc20 = {
   "totalSupply": "uint256:totalSupply"
 };
-const rlpPriceStorage = {
-  "lastPrice": "uint256:lastPrice"
-};
 
 async function ethTvl(api) {
   api.add(USR, await api.call({ abi: erc20.totalSupply, target: USR }));
-
-  const rlpSupply = await api.call({ abi: erc20.totalSupply, target: RLP });
-  const rlpPrice = await api.call({
-      abi: rlpPriceStorage.lastPrice,
-      target: RLPPriceStorage
-    }
-  ) / 1e18;
-  api.add(RLP, rlpSupply * rlpPrice);
+  api.add(RLP, await api.call({ abi: erc20.totalSupply, target: RLP }));
 }
 
 module.exports = {
