9ffbc3797239a182f8b6ce6791985c0b53221dc9
fextr <extr1811@gmail.com>
2024-06-27 08:55:31 +0400

    remove staking export

--- projects/resolv/index.js
+++ projects/resolv/index.js
@@ -1,7 +1,6 @@
 const USR = "0x66a1E37c9b0eAddca17d3662D6c05F4DECf3e110";
 const RLP = "0x4956b52aE2fF65D74CA2d61207523288e4528f96";
 const RLPPriceStorage = "0x31319866778a5223633bd745780BB6d59406371E";
-const stUSR = "0x6c8984bc7DBBeDAf4F6b2FD766f16eBB7d10AAb4";
 
 const erc20 = {
   "totalSupply": "uint256:totalSupply"
@@ -22,13 +21,8 @@ async function ethTvl(api) {
   api.add(RLP, rlpSupply * rlpPrice);
 }
 
-async function staking(api) {
-  api.add(USR, await api.call({ abi: erc20.totalSupply, target: stUSR }));
-}
-
 module.exports = {
   ethereum: {
-    tvl: ethTvl,
-    staking
+    tvl: ethTvl
   }
 };
