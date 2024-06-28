b42dc34b99a7b270d4c03467042096e0f29f987f
0xmDreamy <124707942+0xmDreamy@users.noreply.github.com>
2024-06-28 14:49:15 +0200

    feat(mimswap): Add support for Arbitrum (#10807)

--- projects/mimswap/index.js
+++ projects/mimswap/index.js
@@ -1,6 +1,7 @@
 const { getLogs2 } = require('../helper/cache/getLogs')
 
 const config = {
+  arbitrum: { factory: '0x8D0Cd3eEf1794F59F2B3a664Ef07fCAD401FEc73', fromBlock: 205217727 },
   blast: { factory: '0x7E05363E225c1c8096b1cd233B59457104B84908', fromBlock: 1067907 },
 }
 
