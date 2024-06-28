9f76c66d4ecb3ae207f59f0eec25954f75bbca6d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-06-28 23:53:44 +0200

    minor fix

--- projects/affine-defi-liquid/index.js
+++ projects/affine-defi-liquid/index.js
@@ -1,25 +1,14 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-
-async function tvl(api) {
-    const balETHSymbioticVault = await api.call({
-        abi: "uint256:totalAssets",
-        target: '0x0D53bc2BA508dFdf47084d511F13Bb2eb3f8317B',
-    });
-
-    api.add(ADDRESSES.ethereum.WSTETH, balETHSymbioticVault)
-
-
-    const balETHEigenLayerVault = await api.call({
-        abi: "uint256:totalAssets",
-        target: '0x47657094e3AF11c47d5eF4D3598A1536B394EEc4',
-    });
-
-    api.add(ADDRESSES.ethereum.WSTETH, balETHEigenLayerVault)
-}
+const { sumERC4626VaultsExport } = require('../helper/erc4626')
 
 module.exports = {
-    doublecounted: true,
-    ethereum: {
-        tvl,
-    },
-};
+  doublecounted: true,
+  ethereum: {
+    tvl: sumERC4626VaultsExport({
+      vaults: [
+        '0x0D53bc2BA508dFdf47084d511F13Bb2eb3f8317B',
+        '0x47657094e3AF11c47d5eF4D3598A1536B394EEc4',
+      ],
+      isOG4626: true,
+    }),
+  },
+}
