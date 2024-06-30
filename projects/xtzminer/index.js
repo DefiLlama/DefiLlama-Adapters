ecbc3c6a63f18801aee5ae868bdef5906c08a152
gectiga <141640690+gectiga@users.noreply.github.com>
2024-06-30 23:09:42 +0300

    Update index.js

--- projects/xtzminer/index.js
+++ projects/xtzminer/index.js
@@ -1,1 +1,10 @@
+const { sumTokensExport } = require("../helper/unknownTokens")
+const XTZ_MINER_CONTRACT = '0xc0F9a97E46Fb0f80aE39981759eAB4a61eE36459';
 
+module.exports = {
+  methodology: "XTZ Miner is a Defi Miners on Etherlink. A fun platform to generate 8%/Day ROI for Lifetime",
+  start: 838944,
+  etlk: {
+    tvl: sumTokensExport({ owner: XTZ_MINER_CONTRACT, useDefaultCoreAssets: true, }),
+  }
+}
