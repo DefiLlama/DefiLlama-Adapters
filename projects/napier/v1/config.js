563eacc24a5723faeecf86cc282f5911bb957c65
jsh1n <jsh1n.idle@gmail.com>
2024-07-02 17:14:38 +0900

    feat: add pool underlyings

--- projects/napier/v1/config.js
+++ projects/napier/v1/config.js
@@ -1,5 +1,9 @@
 const config = {
   ethereum: {
+    pool_factory: {
+      address: "0xff8BD29e4D61ed0A748ec6F5aF5bd00Cb4d242aD",
+      deployedAt: 20017932,
+    },
     tranche_factory: {
       address: "0xc9FfF65E5810e11a6F089d231ad9C7983C9e0c49",
       deployedAt: 20017932,
--- projects/napier/v1/tranche.js
+++ projects/napier/v1/tranche.js
@@ -4,24 +4,39 @@ const { getLogs } = require("../../helper/cache/getLogs");
 module.exports = {};
 
 Object.keys(config).map((network) => {
-  const { tranche_factory } = config[network];
+  const { tranche_factory, pool_factory } = config[network];
   module.exports[network] = {
     tvl: async (api) => {
       const trancheDeployedLogs = tranche_factory.address
         ? await getLogs({
-            api,
-            target: tranche_factory.address,
-            topic: "TrancheDeployed(uint256,address,address)",
-            eventAbi:
-              "event TrancheDeployed(uint256 indexed maturity, address indexed principalToken, address indexed yieldToken)",
-            onlyArgs: true,
-            fromBlock: tranche_factory.deployedAt,
-          })
+          api,
+          target: tranche_factory.address,
+          topic: "TrancheDeployed(uint256,address,address)",
+          eventAbi:
+            "event TrancheDeployed(uint256 indexed maturity, address indexed principalToken, address indexed yieldToken)",
+          onlyArgs: true,
+          fromBlock: tranche_factory.deployedAt,
+        })
         : [];
 
       const tranches = trancheDeployedLogs.map((event) =>
         event.principalToken.toLowerCase()
       );
+
+      // fetch deployed pools
+      const poolDeployedLogs = pool_factory.address
+        ? await getLogs({
+          api,
+          target: pool_factory.address,
+          topic: "Deployed(address,address,address)",
+          eventAbi:
+            "event Deployed(uint256 indexed basePool, address indexed underlying, address indexed pool)",
+          onlyArgs: true,
+          fromBlock: pool_factory.deployedAt,
+        })
+        : [];
+
+      const pools = poolDeployedLogs.map((event) => event.pool.toLowerCase())
 
       // fetch adapter contracts
       const results = await api.multiCall({
@@ -31,15 +46,28 @@ Object.keys(config).map((network) => {
       const adapters = results.map((r) => r.adapter);
       const underlyingTokens = results.map((r) => r.underlying);
 
-      // fetch underlying balances of adapter contracts
-      const fetchedbalances = await api.multiCall({
-        abi: "function totalAssets() external view returns (uint256 totalManaged)",
-        calls: adapters
-      })
+      // fetch underlying balances of adapters and pools
+      const [adapterBalances, poolBalances, poolUnderlyings] = await Promise.all([
+        api.multiCall({
+          abi: "function totalAssets() external view returns (uint256 totalManaged)",
+          calls: adapters
+        }),
+        api.multiCall({
+          abi: "function totalUnderlying() external view returns (uint128 totalUnderlyings)",
+          calls: pools
+        }),
+        api.multiCall({
+          abi: "function underlying() external view returns (address underlyingAddress)",
+          calls: pools
+        })
+      ])
 
       // sum up the balances
-      fetchedbalances.forEach((totalManaged, i) => {
+      adapterBalances.forEach((totalManaged, i) => {
         api.add(underlyingTokens[i], totalManaged)
+      })
+      pools.forEach((_pool, i) => {
+        api.add(poolUnderlyings[i], poolBalances[i])
       })
     },
   };
