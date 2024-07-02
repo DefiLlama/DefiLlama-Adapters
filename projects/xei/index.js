bfe569474b74e35290bb765a5949e813d7521ca0
vulntor <22735155+algtm@users.noreply.github.com>
2024-07-02 14:59:13 +0800

    fix

--- projects/xei/index.js
+++ projects/xei/index.js
@@ -8,7 +8,7 @@ const { toUSDTBalances } = require('../helper/balances');
 const V3Factory = '0x0596a0469D5452F876523487251BDdE73D4B2597';
 
 async function tvl(api) {
-  console.log(api.timestamp)
+  // console.log(api.timestamp)
   const res=await get("https://app.xei.finance/indexer/1329/xei/dexTvl?endAt="+api.timestamp.toString())
   const tvl=parseInt(res.data)
  return toUSDTBalances(tvl)
