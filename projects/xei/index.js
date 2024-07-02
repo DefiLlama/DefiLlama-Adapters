1d9c18c2f93c3c8d3e6cb7f94f35d85962290c11
vulntor <22735155+algtm@users.noreply.github.com>
2024-07-02 14:58:57 +0800

    add xei

--- projects/xei/index.js
+++ projects/xei/index.js
@@ -1,5 +1,24 @@
-const { uniV3Export } = require('../helper/uniswapV3')
+const { json } = require('starknet');
+const { get } = require('../helper/http');
+const { uniV3Export } = require('../helper/uniswapV3');
+const { toUSDTBalances } = require('../helper/balances');
 
-module.exports = uniV3Export({
-  sei: { factory: '0x0596a0469D5452F876523487251BDdE73D4B2597', fromBlock:79299552},
-})
+
+
+const V3Factory = '0x0596a0469D5452F876523487251BDdE73D4B2597';
+
+async function tvl(api) {
+  console.log(api.timestamp)
+  const res=await get("https://app.xei.finance/indexer/1329/xei/dexTvl?endAt="+api.timestamp.toString())
+  const tvl=parseInt(res.data)
+ return toUSDTBalances(tvl)
+  
+  
+}
+
+module.exports = {
+  methodology:"TVL of LPs",
+  sei:{
+    tvl
+  }
+}; 
