997f0b20136d3e09f295c4f1f6d4f34278627843
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-02 17:01:55 +0530

    Update index.js

--- projects/etherfi-lrt/index.js
+++ projects/etherfi-lrt/index.js
@@ -8,7 +8,7 @@ module.exports = {
   doublecounted: true,
   ethereum: {
     tvl: sumTokensExport({
-      owners: [owners],
+      owners,
       tokens: [ADDRESSES.ethereum.WSTETH, ADDRESSES.ethereum.sfrxETH, ADDRESSES.ethereum.cbETH, ADDRESSES.ethereum.EETH, ADDRESSES.ethereum.RETH, ADDRESSES.null, "0xf951E335afb289353dc249e82926178EaC7DEd78", "0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee", "0xA35b1B31Ce002FBF2058D22F30f95D405200A15b", "0xa2E3356610840701BDf5611a53974510Ae27E2e1", "0xd5F7838F5C461fefF7FE49ea5ebaF7728bB0ADfa" ] 
     }),
   }}
