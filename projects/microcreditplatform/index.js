100edcf7c236e12e48c409e4eab05b0d3dd383f5
Solazan <85551951+sarp07@users.noreply.github.com>
2024-07-03 15:22:15 +0300

    Update index.js

--- projects/microcreditplatform/index.js
+++ projects/microcreditplatform/index.js
@@ -1,31 +1,59 @@
-const { getChainTvl } = require('@defillama/sdk/build/generalUtil');
-const { sumTokens2 } = require('../helper/unwrapLPs')
+const sdk = require('@defillama/sdk');
 
-const creditDatabaseContract = '0xf79B598E856858527573cE0771b872C88887a055'
-const microcreditInvestmentContract = '0x3B724e84Fd7479C1bed10cAf8eed825dad852C1b'
-const microcreditProfitShareContract = '0x78ed6350E3E4A0Fa59C48DA702d66cEe90F38BDB'
+const microcreditInvestmentContract = '0x3B724e84Fd7479C1bed10cAf8eed825dad852C1b';
+const microcreditProfitShareContract = '0x78ed6350E3E4A0Fa59C48DA702d66cEe90F38BDB';
 
-const fakeUsdt = '0xe5CeD8244f9F233932d754A0B1F7268555FBd3B5'
-const fakeMct = '0x829e43f497b8873fA5c83FcF665b96A39a1FBeD6'
+const fakeUsdt = '0xe5CeD8244f9F233932d754A0B1F7268555FBd3B5';
+const fakeMct = '0x829e43f497b8873fA5c83FcF665b96A39a1FBeD6';
 
 async function tvl(timestamp, ethBlock, chainBlocks) {
-  const balances = {}
+  const balances = {};
   const block = chainBlocks.haqq;
-  const transformAddress = addr => `haqq:${addr}`
-  const contracts = [
-    creditDatabaseContract,
-    microcreditInvestmentContract,
-    microcreditProfitShareContract,
-  ]
 
-  await sumTokens2({ balances, block, chain: 'haqq', owners: contracts, tokens: [fakeUsdt, fakeMct], transformAddress })
+  // Mikro kredi yatırım sözleşmesinin token bakiyelerini al
+  const investmentBalances = await sdk.api.abi.multiCall({
+    calls: [
+      {
+        target: fakeUsdt,
+        params: [microcreditInvestmentContract],
+      },
+      {
+        target: fakeMct,
+        params: [microcreditInvestmentContract],
+      }
+    ],
+    abi: 'erc20:balanceOf',
+    block,
+    chain: 'haqq'
+  });
 
-  return balances
+  sdk.util.sumMultiBalanceOf(balances, investmentBalances);
+
+  // Mikro kredi kar payı sözleşmesinin token bakiyelerini al
+  const profitShareBalances = await sdk.api.abi.multiCall({
+    calls: [
+      {
+        target: fakeUsdt,
+        params: [microcreditProfitShareContract],
+      },
+      {
+        target: fakeMct,
+        params: [microcreditProfitShareContract],
+      }
+    ],
+    abi: 'erc20:balanceOf',
+    block,
+    chain: 'haqq'
+  });
+
+  sdk.util.sumMultiBalanceOf(balances, profitShareBalances);
+
+  return balances;
 }
 
 module.exports = {
   haqq: {
     tvl,
   },
-  methodology: 'counts the number of fake USDT and MCT tokens in the CreditDatabase, MicrocreditInvestment, and MicrocreditProfitShare contracts.',
-}
+  methodology: 'counts the number of fake USDT and MCT tokens in the MicrocreditInvestment and MicrocreditProfitShare contracts.',
+};
