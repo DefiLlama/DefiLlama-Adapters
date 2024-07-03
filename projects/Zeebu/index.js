9fa1f8001f7d9bfdbee22f121c0488cf75c260d3
dsarvaiya <114975126+dsarvaiya@users.noreply.github.com>
2024-07-03 16:51:49 +0530

    Update index.js
    update basic adapter configurations

--- projects/Zeebu/index.js
+++ projects/Zeebu/index.js
@@ -1,1 +1,20 @@
-create index.js
+const ZEEBU_TOKEN_CONTRACT = '0x4D3dc895a9EDb234DfA3e303A196c009dC918f84';
+const ZEEBU_USER_ADDRESS = '0xC2855eAc217a5E989FCeEDE93e453bd555FE720e';
+
+async function tvl(api) {
+  const collateralBalance = await api.call({
+    abi: 'erc20:balanceOf',
+    target: ZEEBU_TOKEN_CONTRACT,
+    params: [ZEEBU_USER_ADDRESS],
+  });
+
+  api.add(MINT_TOKEN_CONTRACT, collateralBalance)
+}
+
+module.exports = {
+  methodology: 'counts the number of ZEEBU tokens top user.',
+  start: 1000235,
+  bsc: {
+    tvl,
+  }
+}; 
