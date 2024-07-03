a13d581aad4ec375b6069a2886f0152286006ea8
Define101 <93603962+Define101@users.noreply.github.com>
2024-07-03 22:35:17 +0100

    Remove god dam eth (#10875)
    * remove duplicated vault
    
    * change logic etherfi

--- projects/etherfi-lrt/index.js
+++ projects/etherfi-lrt/index.js
@@ -1,14 +1,17 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokensExport } = require('../helper/sumTokens');
+const ADDRESSES = require('../helper/coreAssets.json');
+const sdk = require('@defillama/sdk');
 
-const owners = ["0x917ceE801a67f933F2e6b33fC0cD1ED2d5909D88"]
+async function tvl(api) {
+  const balETH2 = await api.call({
+    abi: "uint256:totalSupply",
+    target: '0x917ceE801a67f933F2e6b33fC0cD1ED2d5909D88',
+  });
+  api.add(ADDRESSES.ethereum.EETH, BigInt(balETH2));
+}
 
 module.exports = {
-  methodology: "LRTS on 0x917ceE801a67f933F2e6b33fC0cD1ED2d5909D88",
   doublecounted: true,
   ethereum: {
-    tvl: sumTokensExport({
-      owners,
-      tokens: [ADDRESSES.ethereum.WSTETH, ADDRESSES.ethereum.sfrxETH, ADDRESSES.ethereum.cbETH, ADDRESSES.ethereum.EETH, ADDRESSES.ethereum.RETH, ADDRESSES.null, "0xf951E335afb289353dc249e82926178EaC7DEd78", "0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee", "0xA35b1B31Ce002FBF2058D22F30f95D405200A15b", "0xa2E3356610840701BDf5611a53974510Ae27E2e1", "0xd5F7838F5C461fefF7FE49ea5ebaF7728bB0ADfa" ] 
-    }),
-  }}
+    tvl,
+  },
+};
