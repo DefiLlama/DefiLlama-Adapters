87a8e5e0ca4e434116885ab695cac74ad4b53ae7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-03 08:58:02 +0300

    code refactor

--- projects/step-finance/index.js
+++ projects/step-finance/index.js
@@ -1,17 +1,11 @@
 const { sumTokens2 } = require("../helper/solana");
 
-const STEP_MINT = 'StepAscQoEioFxxWGnh2sLBDFp9d8rvKz2Yp39iDpyT';
 const STAKING_VAULT = 'ANYxxG365hutGYaTdtUQG8u2hC4dFX9mFHKuzy9ABQJi';
 
 module.exports = {
 	timetravel: false,
 	solana: {
-		tvl,
+		tvl: () => ({}),
 		staking: () => sumTokens2({ tokenAccounts: [STAKING_VAULT] })
 	},
-};
-
-async function tvl(api) {
-	const tokensAndOwners = [[STEP_MINT, STAKING_VAULT]];
-	return sumTokens2({ tokensAndOwners });
 }
