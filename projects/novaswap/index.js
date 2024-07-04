7336f5d28eb0ec0822f954da9688f75c78a524f1
NovaSwap | Dev <dev@novaswap.finance>
2024-07-04 18:07:04 +0800

    upgrade to Novaswap v2

--- projects/novaswap/index.js
+++ projects/novaswap/index.js
@@ -1,5 +1,5 @@
 const { uniV3Export } = require('../helper/uniswapV3')
 
 module.exports = uniV3Export({
-  zklink: { factory: '0xf8D35842f37800E349A993503372fb9E2CBb7E3d', fromBlock: 1676253 },
+  zklink: { factory: '0x9f94c91b178F5bc9fCcA3e5428b09A3d01CE5AC6', fromBlock: 3416798 },
 })
