79b46369e71d7e4b23b135f0c345002bd2e61661
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-05 08:16:43 +0300

    code refactor

--- projects/astherus/index.js
+++ projects/astherus/index.js
@@ -1,36 +1,21 @@
-const { sumTokensExport } = require("../helper/unwrapLPs");
-const utils = require('../helper/utils');
+const { getConfig } = require('../helper/cache')
 
-async function fetchAssetTokensByChain(chain) {
-  const { data } = await utils.fetchURL(`https://astherus.finance/bapi/futures/v1/public/future/web3/ae-deposit-asset?chainId=${chain}`)
-  return (data.data ?? []).map(i => i.contractAddress)
-}
-
-const ContractMap = {
-  '56': '0x128463A60784c4D3f46c23Af3f65Ed859Ba87974',
-  '1': '0x604DD02d620633Ae427888d41bfd15e38483736E'
-}
-const Chain = {
-  BSC: '56',
-  ETH: '1'
+const config = {
+  bsc: '0x128463A60784c4D3f46c23Af3f65Ed859Ba87974',
+  ethereum: '0x604DD02d620633Ae427888d41bfd15e38483736E'
 }
 
 module.exports = {
-  start: 1706716800000, // 02/01/2024 @ 00:00:00pm (UTC)
-  bsc: {
-    tvl: async function (...rest) {
-      const tokens = await fetchAssetTokensByChain(Chain.BSC)
-      return sumTokensExport({
-        owner: ContractMap[Chain.BSC], tokens
-      })(...rest)
+  start: 1706716800, // 02/01/2024 @ 00:00:00pm (UTC)
+}
+
+Object.keys(config).forEach(chain => {
+  const vault = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const { data } = await getConfig(`astherus/${api.chain}`, `https://astherus.finance/bapi/futures/v1/public/future/web3/ae-deposit-asset?chainId=${api.chainId}`)
+      const tokens = data.map(i => i.contractAddress)
+      return api.sumTokens({ owner: vault, tokens })
     }
-  },
-  ethereum: {
-    tvl: async function (...rest) {
-      const tokens = await fetchAssetTokensByChain(Chain.ETH)
-      return sumTokensExport({
-        owner: ContractMap[Chain.ETH], tokens
-      })(...rest)
-    }
-  },
-};
+  }
+})
