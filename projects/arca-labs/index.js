46303451b06f893c0f6ebfc2276d0608496a2b63
0xpeluche <0xpeluche@protonmail.com>
2024-07-06 17:06:28 +0200

    Adjust Supply

--- projects/arca-labs/index.js
+++ projects/arca-labs/index.js
@@ -10,11 +10,12 @@ module.exports = {
         api.call({target: RCOIN, abi:'erc20:decimals'}),
         api.call({target: RCOIN, abi:'erc20:totalSupply'})
     ])
-
       // Adjusting the total supply of RCOIN to match the decimal places of USDC
       // USDC has 6 decimals, whereas RCOIN has 8 decimals
-      const rcoinToUsdcDecimalAdjustment = Math.pow(10, usdcDecimals) / Math.pow(10, rcoinDecimals);
-      return api.add(ADDRESSES.ethereum.USDC, totalSupply * rcoinToUsdcDecimalAdjustment)
+      const rcoinDecimalAdjustment = Math.pow(10, usdcDecimals) / Math.pow(10, rcoinDecimals);
+      const adjustedSupply = totalSupply * rcoinDecimalAdjustment
+      
+      return api.add(ADDRESSES.ethereum.USDC, adjustedSupply)
     }
   }
 }
