c8f86f9012764f8685ed42ae842cecdb45442ddc
LeeChunHao2000 <64617939+LeeChunHao2000@users.noreply.github.com>
2024-07-07 08:09:48 -0700

    add Blast USDz (#10904)

--- projects/anzen-v2/index.js
+++ projects/anzen-v2/index.js
@@ -4,6 +4,7 @@ const sdk = require('@defillama/sdk')
 
 const USDz = '0xa469b7ee9ee773642b3e93e842e5d9b5baa10067';
 const Base_USDz = '0x04d5ddf5f3a8939889f11e97f8c4bb48317f1938';
+const Blast_USDz = '0x52056ed29fe015f4ba2e3b079d10c0b87f46e8c6';
 const SPCT = '0xf30a29f1c540724fd8c5c4be1af604a6c6800d29'; // Secured collateral
 
 const mainnet_tvl = async (api) => {
@@ -14,6 +15,11 @@ const mainnet_tvl = async (api) => {
 const base_tvl = async (api) => {
   const supply = await api.call({ abi: 'erc20:totalSupply', target: Base_USDz })
   api.add(Base_USDz, supply)
+}
+
+const blast_tvl = async (api) => {
+  const supply = await api.call({ abi: 'erc20:totalSupply', target: Blast_USDz })
+  api.add(Blast_USDz, supply)
 }
 
 const collateral_assets = async (api) => {
@@ -28,5 +34,8 @@ module.exports = {
   },
   base: {
     tvl: base_tvl,
+  },
+  blast: {
+    tvl: blast_tvl,
   },
 };
