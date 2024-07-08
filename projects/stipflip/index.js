5f920ad037d8a3cc73bbd188aefc95cab9f6e5ed
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-08 16:57:00 +0300

    stip: include oracle tvl

--- projects/stipflip/index.js
+++ projects/stipflip/index.js
@@ -1,6 +1,7 @@
 const { getLogs2 } = require('../helper/cache/getLogs')
 const ADDRESSES = require('../helper/coreAssets.json')
 const { uniV3Export } = require('../helper/uniswapV3')
+const ORACLE = "0x4AC635E92801e657F44BDEfcc7660Ea1431DF846";
 
 async function tvl(api) {
   const logs = await getLogs2({
@@ -10,7 +11,7 @@ async function tvl(api) {
     fromBlock: 20172088,
   })
   const synths = logs.map(log => log.synth)
-  await api.sumTokens({ owners: synths, tokens: [ADDRESSES.null] })
+  await api.sumTokens({ owners: [...synths, ORACLE], tokens: [ADDRESSES.null, ADDRESSES.ethereumclassic.WETC] })
   const uniTvl = uniV3Export({
     ethereumclassic: { blacklistedTokens: synths.concat(['0xf09ace63aa1345882a1ca200b7243f5786eb177b']), factory: '0xaCc703c9C8248a141113C672ea71d196E8118210', fromBlock: 20130563, }
   }).ethereumclassic.tvl
