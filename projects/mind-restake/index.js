b4c52658d3e1ef11dd2bb897584c19ab68aedaf6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-08 12:20:23 +0300

    minor fix

--- projects/mind-restake/index.js
+++ projects/mind-restake/index.js
@@ -1,25 +1,21 @@
-const HELPER_CONTRACTS = {
+const config = {
   ethereum: "0x6a5D488EC17d6a7a1872AaB88feC90c1B2Df4196",
   scroll: "0xea3E87699D11B77Fba754Bf0257a25664B97437d"
 };
 
-async function tvl(api) {
-  const target = HELPER_CONTRACTS[api.chain];
-  const [, tokenList, totalList] = await api.call({
-    abi: "function getPoolTotalAssets() view returns (address[] memory,address[] memory, uint256[] memory)",
-    target,
-    params: [],
-  });
-
-  tokenList.map((token, idx) => api.add(token, totalList[idx]))
-}
-
 module.exports = {
   methodology: "Counts the total amount of asset tokens deposited in each of the Strategy contracts registered in the helper contract on each chain.",
-  ethereum: {
-    tvl,
-  },
-  scroll: {
-    tvl
+}
+
+Object.keys(config).forEach(chain => {
+  const target = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const [_vaults, tokens, bals] = await api.call({
+        abi: "function getPoolTotalAssets() view returns (address[] memory,address[] memory, uint256[] memory)",
+        target,
+      })
+      api.add(tokens, bals)
+    }
   }
-};
+})
