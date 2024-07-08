76d60bc7b5713a04ae34c973d5505310d98096b2
0xpeluche <0xpeluche@protonmail.com>
2024-07-08 20:15:28 +0200

    use toUSDTBalances() helper

--- projects/franklinTempleton-finance/index.js
+++ projects/franklinTempleton-finance/index.js
@@ -1,15 +1,15 @@
-const ADDRESSES = require('../helper/coreAssets.json')
+const { toUSDTBalances } = require('../helper/balances');
 
 const BENJI_STELLAR = {ticker: "BENJI", address: 'GBHNGLLIE3KWGKCHIKMHJ5HVZHYIK7WTBE4QF5PLAKL4CJGSEU7HZIW5'}
 const BENJI_POLYGON = '0x408a634b8a8f0de729b48574a3a7ec3fe820b00a'
-const USDC_DECIMALS = 6
 
 const stellarTvl = async (api) => {
     const stellarApi = `https://api.stellar.expert/explorer/public/asset/${BENJI_STELLAR.ticker}-${BENJI_STELLAR.address}`
     const response = await fetch(stellarApi)
     const {supply, toml_info} = await response.json()
-    const adjustedSupply = supply * Math.pow(10, USDC_DECIMALS) / Math.pow(10, toml_info.decimals)
-    return api.add(ADDRESSES.ethereum.USDC, adjustedSupply, { skipChain: true })
+    const adjustedSupply = toUSDTBalances((supply / Math.pow(10, toml_info.decimals)))
+    const [[tokenAddress, tokenBalance]] = Object.entries(adjustedSupply);
+    return api.add(tokenAddress, tokenBalance, { skipChain: true })
 }
 
 const polygonTvl = async (api) => {
@@ -17,8 +17,9 @@ const polygonTvl = async (api) => {
         api.call({target: BENJI_POLYGON, abi:'erc20:decimals'}),
         api.call({target: BENJI_POLYGON, abi:'erc20:totalSupply'})
   ])
-  const adjustedSupply = totalSupply * Math.pow(10, USDC_DECIMALS) / Math.pow(10, decimals)
-  return api.add(ADDRESSES.polygon.USDC,adjustedSupply)
+  const adjustedSupply = toUSDTBalances((totalSupply / Math.pow(10, decimals)))
+  const [[tokenAddress, tokenBalance]] = Object.entries(adjustedSupply);
+  api.add(tokenAddress, tokenBalance, {skipChain: true} )
 }
 
 module.exports = {
