b9a520b50493605b6d6a0e7f03f10ddac94603d6
Ubuntu <ubuntu@ip-172-31-23-176.us-east-2.compute.internal>
2024-07-08 18:18:29 +0000

    Add Landshare SDK adapter

--- projects/landshare/index.js
+++ projects/landshare/index.js
@@ -1,34 +1,35 @@
+const LANDSHARE_TOKEN_CONTRACT = '0xA73164DB271931CF952cBaEfF9E8F5817b42fA5C';
+const LANDSHARE_STAKING_CONTRACT = '0x3f9458892fB114328Bc675E11e71ff10C847F93b';
+const LANDSHARE_LP_TOKEN_CONTRACT = '0x13f80c53b837622e899e1ac0021ed3d1775caefa';
 
-const { toUSDTBalances } = require('../helper/balances');
-const { get } = require('../helper/http');
+async function tvl(api) {
+  // Get the balance of LANDSHARE tokens in the staking contract
+  const landshareTokenBalance = await api.call({
+    abi: 'erc20:balanceOf',
+    target: LANDSHARE_TOKEN_CONTRACT,
+    params: [LANDSHARE_STAKING_CONTRACT],
+  });
 
-const LandshareApi = "https://api.landshare.io/api/properties";
+  // Get the balance of LANDSHARE LP tokens in the LP staking contract
+  const landshareLpTokenBalance = await api.call({
+    abi: 'erc20:balanceOf',
+    target: LANDSHARE_LP_TOKEN_CONTRACT,
+    params: [LANDSHARE_STAKING_CONTRACT],
+  });
 
-async function landshareTVL() {
-  const rentals = await get(LandshareApi + "/rentals");
-  const flips = await get(LandshareApi + "/flips");
+  // Add the LANDSHARE token balance to the balances object
+  api.add(LANDSHARE_TOKEN_CONTRACT, landshareTokenBalance);
 
-  const totalRentals = rentals.reduce((acc, item) => {
-    acc = acc + item.value
-    return acc;
-  }, 0);
-
-  const totalFlips = flips.reduce((acc, item) => {
-    acc = acc + item;
-    return acc;
-  }, 0);
-
-  const total = totalRentals + totalFlips;
-
-  return toUSDTBalances(total);
+  // Add the LANDSHARE LP token balance to the balances object
+  api.add(LANDSHARE_LP_TOKEN_CONTRACT, landshareLpTokenBalance);
 }
 
 module.exports = {
-  timetravel: false,
-  misrepresentedTokens: true,
+  methodology: 'Counts the number of LANDSHARE tokens in the staking contract and LANDSHARE LP tokens in the LP staking contract.',
+  start: 26406467, // First block for Landshare token creator
   bsc: {
-    tvl: landshareTVL,
-  }
+    tvl,
+  },
+  timetravel: true,
+  misrepresentedTokens: false,
 };
-
-// node test.js projects/landshare/index.js
