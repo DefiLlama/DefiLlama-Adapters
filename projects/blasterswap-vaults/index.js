038d8ed21e3157f8724408ea589bec08320fc5e4
lsblk07 <lsblk07@eth.io>
2024-07-09 17:49:01 +0300

    blasterswap vault tvl

--- projects/blasterswap-vaults/index.js
+++ projects/blasterswap-vaults/index.js
@@ -1,18 +1,12 @@
-const config = {
-	blast: {
-		pools: {
-			"xBlast": "0x0464a36beCf9967111D2dCAb57CAf4a2376f6E3F",
-		}, fromBlock: 5692576,
+const stakingContract = '0x0464a36beCf9967111D2dCAb57CAf4a2376f6E3F'
+const blast = '0x0464a36beCf9967111D2dCAb57CAf4a2376f6E3F'
+const { staking } = require('./helper/staking')
+
+
+module.exports = {
+	methodology: "BLAST can be staked in Blasterswap vault",
+	ethereum: {
+		staking: staking(stakingContract, blast),
+		tvl: () => ({})
 	},
 }
-
-
-Object.keys(config).forEach(chain => {
-	let { pools, fromBlock, tokens = [], } = config[chain]
-	pools = Object.values(pools)
-	module.exports[chain] = {
-		tvl: async (api) => {
-			return api.erc4626Sum({ calls: pools[0], isOG4626: true, });
-		}
-	}
-})
