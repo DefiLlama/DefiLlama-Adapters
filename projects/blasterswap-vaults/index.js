af59ee0fef47407e07514266e2bc4aa1cd153b67
lsblk07 <lsblk07@eth.io>
2024-07-09 17:51:03 +0300

    import

--- projects/blasterswap-vaults/index.js
+++ projects/blasterswap-vaults/index.js
@@ -1,6 +1,6 @@
 const stakingContract = '0x0464a36beCf9967111D2dCAb57CAf4a2376f6E3F'
 const blast = '0x0464a36beCf9967111D2dCAb57CAf4a2376f6E3F'
-const { staking } = require('./helper/staking')
+const { staking } = require('../helper/staking')
 
 
 module.exports = {
