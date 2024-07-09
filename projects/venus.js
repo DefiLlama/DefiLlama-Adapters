e8e9814cdad893ec86f512d41290bb2214629849
realdealshaman <realdealshaman11@gmail.com>
2024-07-09 03:23:50 +0100

    add arbitrum

--- projects/venus.js
+++ projects/venus.js
@@ -12,3 +12,4 @@ module.exports = fullCoumpoundExports("0xfd36e2c2a6789db23113685031d7f1632915838
 
 module.exports.ethereum = compoundExports2({ comptroller: '0x67aA3eCc5831a65A5Ba7be76BED3B5dc7DB60796'})
 module.exports.op_bnb = compoundExports2({ comptroller: '0xd6e3e2a1d8d95cae355d15b3b9f8e5c2511874dd'})
+module.exports.arbitrum = compoundExports2({ comptroller: '0x317c1A5739F39046E20b08ac9BeEa3f10fD43326'})
