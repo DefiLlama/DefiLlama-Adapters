29cb88bdc57210940852e9f22e587092162493c2
lsblk07 <lsblk07@eth.io>
2024-07-09 17:22:30 +0300

    blasterswap-vaults-tvl

---
+++ projects/blasterswap-vaults/index.js
@@ -0,0 +1,20 @@
+const { getLogs } = require('../helper/cache/getLogs');
+
+const config = {
+	blast: {
+		pools: {
+			"xBlast": "0x0464a36beCf9967111D2dCAb57CAf4a2376f6E3F",
+		}, fromBlock: 5692576,
+	},
+}
+
+
+Object.keys(config).forEach(chain => {
+	let { pools, fromBlock, tokens = [], } = config[chain]
+	pools = Object.values(pools)
+	module.exports[chain] = {
+		tvl: async (api) => {
+			return api.erc4626Sum({ calls: pools[0], isOG4626: true, });
+		}
+	}
+})
