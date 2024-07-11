d974000fb57a8d0d5f4688e258a43d04f55edc64
atozICT <60227992+atozICT20@users.noreply.github.com>
2024-07-11 15:42:16 -0400

    [Sturdy V2]: added linea, optimism and sei networks (#10963)

--- projects/sturdy-v2/index.js
+++ projects/sturdy-v2/index.js
@@ -4,6 +4,9 @@ const abi = require("./abi.json");
 const config = {
   ethereum: '0x69764E3e0671747A7768A1C1AfB7C0C39868CC9e',
   mode: '0xF0382A9Eca5276d7B4BbcC503e4159C046c120ec',
+  linea: "0xd67Da8636Ae87b0cECBDa2e66dB58d4839722B52",
+  optimism: "0x9dc7B2130e478C5810Dc0cDbD46B9D479b2e1aC4",
+  sei: "0x4534F53A81416a83F6bAF5ac63c94aEd1fea1303",
 }
 
 module.exports = {
