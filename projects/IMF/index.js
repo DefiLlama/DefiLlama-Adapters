359d59365f0aaf368319d310a96e0ebe9dc0dc98
miffybunnyrabbit <miffybunnyrabbit@gmail.com>
2024-07-11 15:25:46 +1000

    Revert "+IMF +MONEY"
    This reverts commit d87dba5b2d8677b1dfdb85478b8828483319b463.

--- projects/IMF/index.js
+++ projects/IMF/index.js
@@ -1,20 +1,20 @@
-const IMF_TOKEN_CONTRACT = '0x05BE1d4c307C19450A6Fd7cE7307cE72a3829A60';
-const IMF_BOND_CONTRACT = '0x2625Bfb6aD9840C2c0ABb48f150Eb9158393c466'; //address of the largest holder of IMF
+const MINT_TOKEN_CONTRACT = '0x1f3Af095CDa17d63cad238358837321e95FC5915';
+const MINT_CLUB_BOND_CONTRACT = '0x8BBac0C7583Cc146244a18863E708bFFbbF19975';
 
 async function tvl(api) {
   const collateralBalance = await api.call({
     abi: 'erc20:balanceOf',
-    target: IMF_TOKEN_CONTRACT,
-    params: [IMF_BOND_CONTRACT],
+    target: MINT_TOKEN_CONTRACT,
+    params: [MINT_CLUB_BOND_CONTRACT],
   });
 
-  api.add(IMF_TOKEN_CONTRACT, collateralBalance)
+  api.add(MINT_TOKEN_CONTRACT, collateralBalance)
 }
 
 module.exports = {
-  methodology: 'counts the number of IMF tokens in the Club Bonding contract.',
+  methodology: 'counts the number of MINT tokens in the Club Bonding contract.',
   start: 1000235,
-  ethereum: {
+  bsc: {
     tvl,
   }
 }; 
