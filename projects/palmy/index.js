41268a5c3f546ecbfc8cb17db3446a72d146c1e9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-12 08:30:34 +0300

    code refactor

--- projects/palmy/index.js
+++ projects/palmy/index.js
@@ -2,11 +2,9 @@ const { aaveExports, methodology, } = require("../helper/aave");
 
 module.exports = {
   methodology,
-  oasys: {
-    ...aaveExports("oasys", '0xf4A3dDC5F629d9CB14DF4e7d5f78326153eA02A3', undefined, undefined, {
-      abis: {
-        getAllATokens: "function getAllLTokens() view returns (tuple(string symbol, address tokenAddress)[])"
-      },      
-    }),
-  }
+  oas: aaveExports("oas", '0xf4A3dDC5F629d9CB14DF4e7d5f78326153eA02A3', undefined, undefined, {
+    abis: {
+      getAllATokens: "function getAllLTokens() view returns (tuple(string symbol, address tokenAddress)[])"
+    },
+  }),
 };
