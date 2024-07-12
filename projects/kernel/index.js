4e9603b3dcb64e66b4e82a6cba43795652d74f16
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-12 09:53:18 +0300

    update sdk

--- projects/kernel/index.js
+++ projects/kernel/index.js
@@ -1,36 +1,23 @@
-const { abi } = require('./abi')
+const abi  =  {
+  tokens: "function tokens(address) view returns (address vaultToken, address rateProvider, uint256 cap, uint256 deposited, bool paused)"
+}
 
 const ASSETS = [
   "0xf02C96DbbB92DC0325AD52B3f9F2b951f972bf00", // krETH
   "0x513D27c94C0D81eeD9DC2a88b4531a69993187cF", // ksETH
   "0x0bB9aB78aAF7179b7515e6753d89822b91e670C4", // kUSD
-];
-
-const range = (length) => Array.from({ length }, (_, i) => i);
-
-const fetchApprovedTokens = async (api, assets) => {
-  const numTokensAllowed = await api.multiCall({calls: assets, abi: abi.numApprovedTokens})
-  const calls = numTokensAllowed.flatMap((numTokens, assetIndex) =>
-    range(numTokens).map(tokenIndex => ({ target: assets[assetIndex], params: [tokenIndex] }))
-  );
-
-  return api.multiCall({ calls, abi: abi.approvedTokens });
-}
-
-const fetchTokenBalances = async (api, assets, approvedTokens) => {
-  const calls = assets.flatMap((asset) => {
-    return approvedTokens.map((token) => ({target: asset, params: token}))
-  })
-  
-  const balances = await api.multiCall({calls, abi: abi.tokens})
-  balances.forEach(({ deposited }, index) => {
-    api.add(calls[index].params, deposited)
-  })
-}
+]
 
 const tvl = async (api) => {
-  const approvedTokens = await fetchApprovedTokens(api, ASSETS);
-  return fetchTokenBalances(api, ASSETS, approvedTokens)
+  const tokens = await api.fetchList({ lengthAbi: 'numApprovedTokens', itemAbi: 'approvedTokens', calls: ASSETS, groupedByInput: true, });
+  const assets = []
+  const balanceCalls = []
+  tokens.forEach((tokens, i) => {
+    assets.push(...tokens)
+    balanceCalls.push(...tokens.map(token => ({ target: ASSETS[i], params: token })))
+  })
+  const balances = (await api.multiCall({ calls: balanceCalls, abi: abi.tokens })).map(i => i.deposited)
+  api.add(assets, balances)
 };
 
 module.exports = {
