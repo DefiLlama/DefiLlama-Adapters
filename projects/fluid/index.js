03772e574ebc567549fb38b9912c5de9bc091b9f
bergben <bergben@tutanota.com>
2024-07-12 19:08:06 +0200

    fix(fluid): update abi for different resolver versions

--- projects/fluid/index.js
+++ projects/fluid/index.js
@@ -1,8 +1,10 @@
 const ADDRESSES = require("../helper/coreAssets.json");
 const methodologies = require("../helper/methodologies");
 const abi = {
-  getOverallTokensData:
+  getOverallTokensDataLegacy:
     "function getOverallTokensData(address[] tokens_) view returns ((uint256 borrowRate, uint256 supplyRate, uint256 fee, uint256 lastStoredUtilization, uint256 storageUpdateThreshold, uint256 lastUpdateTimestamp, uint256 supplyExchangePrice, uint256 borrowExchangePrice, uint256 supplyRawInterest, uint256 supplyInterestFree, uint256 borrowRawInterest, uint256 borrowInterestFree, uint256 totalSupply, uint256 totalBorrow, uint256 revenue, (uint256 version, (address token, uint256 kink, uint256 rateAtUtilizationZero, uint256 rateAtUtilizationKink, uint256 rateAtUtilizationMax) rateDataV1, (address token, uint256 kink1, uint256 kink2, uint256 rateAtUtilizationZero, uint256 rateAtUtilizationKink1, uint256 rateAtUtilizationKink2, uint256 rateAtUtilizationMax) rateDataV2) rateData)[] overallTokensData_)",
+  getOverallTokensData:
+    "function getOverallTokensData(address[] tokens_) view returns ((uint256 borrowRate, uint256 supplyRate, uint256 fee, uint256 lastStoredUtilization, uint256 storageUpdateThreshold, uint256 lastUpdateTimestamp, uint256 supplyExchangePrice, uint256 borrowExchangePrice, uint256 supplyRawInterest, uint256 supplyInterestFree, uint256 borrowRawInterest, uint256 borrowInterestFree, uint256 totalSupply, uint256 totalBorrow, uint256 revenue, uint256 maxUtilization, (uint256 version, (address token, uint256 kink, uint256 rateAtUtilizationZero, uint256 rateAtUtilizationKink, uint256 rateAtUtilizationMax) rateDataV1, (address token, uint256 kink1, uint256 kink2, uint256 rateAtUtilizationZero, uint256 rateAtUtilizationKink1, uint256 rateAtUtilizationKink2, uint256 rateAtUtilizationMax) rateDataV2) rateData)[] overallTokensData_)",
 };
 
 const config = {
@@ -60,7 +62,7 @@ async function borrowed(api) {
   const tokens = await getListedTokens(api);
   const borrowed = await api.call({
     target: config[api.chain].liquidityResolver(api.block),
-    abi: abi.getOverallTokensData,
+    abi: api.block < 19992056 ? abi.getOverallTokensDataLegacy : abi.getOverallTokensData,
     params: [tokens],
   });
   api.add(
