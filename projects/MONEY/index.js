8b900b51e000be080edf3b336a51884f829543c1
miffybunnyrabbit <miffybunnyrabbit@gmail.com>
2024-07-15 09:23:46 +1000

    Update index.js

--- projects/MONEY/index.js
+++ projects/MONEY/index.js
@@ -1,32 +1,15 @@
 const PEPE_TOKEN_CONTRACT = '0x6982508145454Ce325dDbE47a25d4ec3d2311933';
-const MOG_TOKEN_CONTRACT = '0xaaeE1A9723aaDB7afA2810263653A34bA2C21C7a';
-const BITCOIN_TOKEN_CONTRACT = '0x72e4f9f808c49a2a61de9c5896298920dc4eeea9';
-const MONEY_MARKETS_CONTRACT = '0x83b7D15588A01aB37C55290fB600A5fC4d7201bf'; //rn its miffy wallet for testing
+const LOCKED_MONEY_CONTRACT = '0x30F75834cB406b7093208Fda7F689938aCBD1EeB'; //wallet that has all the locked money
 
 async function tvl(api) {
   const collateralBalance = await api.call({
     abi: 'erc20:balanceOf',
     target: PEPE_TOKEN_CONTRACT,
-    params: [MONEY_MARKETS_CONTRACT],
+    params: [LOCKED_MONEY_CONTRACT],
   });
-
-  const collateralBalanceMog = await api.call({
-    abi: 'erc20:balanceOf',
-    target: MOG_TOKEN_CONTRACT,
-    params: [MONEY_MARKETS_CONTRACT],
-  });
-
-  const collateralBalanceBitcoin = await api.call({
-    abi: 'erc20:balanceOf',
-    target: BITCOIN_TOKEN_CONTRACT,
-    params: [MONEY_MARKETS_CONTRACT],
-  });
-
   
 
   api.add(PEPE_TOKEN_CONTRACT, collateralBalance);
-  api.add(MOG_TOKEN_CONTRACT, collateralBalance);
-  api.add(BITCOIN_TOKEN_CONTRACT, collateralBalance);
 }
 
 module.exports = {
