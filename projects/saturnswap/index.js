246f340f12d9f67e2f3514d7d19af75e879b2662
Nicholas Maselli <maselli.nicholas@gmail.com>
2024-07-15 11:27:13 -0400

    adding saturn swap adaptor

---
+++ projects/saturnswap/index.js
@@ -0,0 +1,13 @@
+const { fetchURL } = require("../helper/utils")
+
+async function tvl() {
+  const res = await fetchURL("https://api.saturnswap.io/v1/defillama/tvl");
+  return {
+      cardano: res.data.tvl.tvl
+  };
+}
+
+module.exports = {
+  timetravel: false,
+  tvl
+}
