a738979274cd858934880aa0bba8945db9253e99
Simão Pinto <87367287+Sk1mer12@users.noreply.github.com>
2024-07-15 10:11:53 +0100

    Update index.js

--- projects/inception-lrt/index.js
+++ projects/inception-lrt/index.js
@@ -10,41 +10,10 @@ async function tvl_eth(api) {
   api.add(tokens, total_deposited)
 }
 
-async function tvl_linea(api) {
-    const staked = await api.call({ abi: 'uint256:totalSupply', target: '0xd08C3F25862077056cb1b710937576Af899a4959' })
-    api.add(ADDRESSES.null, staked)
-}
-
-async function tvl_mode(api) {
-    const staked = await api.call({ abi: 'uint256:totalSupply', target: '0x5A32d48411387577c26a15775cf939494dA8064A' })
-    api.add(ADDRESSES.null, staked)
-}
-
-async function tvl_arbitrum(api) {
-    const staked = await api.call({ abi: 'uint256:totalSupply', target: '0xd08C3F25862077056cb1b710937576Af899a4959' })
-    api.add(ADDRESSES.null, staked)
-}
-
-async function tvl_optimsim(api) {
-    const staked = await api.call({ abi: 'uint256:totalSupply', target: '0xd08C3F25862077056cb1b710937576Af899a4959' })
-    api.add(ADDRESSES.null, staked)
-}
-
-async function tvl_bsc(api) {
-    const staked = await api.call({ abi: 'uint256:totalSupply', target: '0x3059a337b134Cc89851c8DE18A00D880fa1D5519' })
-    api.add(ADDRESSES.null, staked)
-}
-
-
 module.exports = {
   hallmarks: [
     [1714953600,"Genesis Merges with InceptionLRT"]
   ],
   doublecounted: true,
   ethereum: { tvl: tvl_eth, } ,
-  linea: { tvl: tvl_linea, }, 
-  mode: { tvl: tvl_mode, }, 
-  arbitrum: { tvl: tvl_arbitrum, },
-  optimism: { tvl: tvl_optimism, },
-  bsc: { tvl: tvl_bsc, },
 }
