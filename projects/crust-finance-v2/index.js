4301a37b6e50b315c161a14cb11a09774ab33056
LaazyPotaato <laazy.potaato@gmail.com>
2024-07-17 00:13:43 +0530

    Updated chain

--- projects/crust-finance-v2/index.js
+++ projects/crust-finance-v2/index.js
@@ -1,7 +1,7 @@
 const { uniV3Export } = require("../helper/uniswapV3");
 
 module.exports = uniV3Export({
-  polygon: {
+  mantle: {
     factory: "0xEaD128BDF9Cff441eF401Ec8D18a96b4A2d25252",
     fromBlock: 62692352,
   },
