c191ded100434709ac593c5cdb225c5a94b0d707
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2024-07-15 20:10:33 -0400

    add ethereum & arbitrum to Pump Markets (#11000)

--- projects/pump-markets/index.js
+++ projects/pump-markets/index.js
@@ -8,4 +8,16 @@ module.exports = {
       tokens: [ADDRESSES.blast.USDB, ADDRESSES.blast.WETH],
     }),
   },
+  ethereum: {
+    tvl: sumTokensExport({
+      owner: "0x360CFF6ca6278954933A8556D1938B4cF3D747B5",
+      tokens: [ADDRESSES.ethereum.USDC, ADDRESSES.ethereum.USDT, ADDRESSES.ethereum.WETH],
+    }),
+  },
+  arbitrum: {
+    tvl: sumTokensExport({
+      owner: "0x360CFF6ca6278954933A8556D1938B4cF3D747B5",
+      tokens: [ADDRESSES.arbitrum.USDC, ADDRESSES.arbitrum.USDT, ADDRESSES.arbitrum.WETH],
+    }),
+  },
 };
