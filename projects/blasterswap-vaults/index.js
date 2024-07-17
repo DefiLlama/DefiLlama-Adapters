6b1fb2c8944c4996d9a119a199d2309a0d248143
lsblk07 <lsblk07@eth.io>
2024-07-17 16:36:24 +0300

    new vaults

--- projects/blasterswap-vaults/index.js
+++ projects/blasterswap-vaults/index.js
@@ -1,12 +1,14 @@
-const stakingContract = '0x0464a36beCf9967111D2dCAb57CAf4a2376f6E3F'
-const blast = '0xb1a5700fa2358173fe465e6ea4ff52e36e88e2ad'
-const { staking } = require('../helper/staking')
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
+const owners = ['0x0464a36beCf9967111D2dCAb57CAf4a2376f6E3F', '0xC52fb7E613e401a0195C2fdB369618580D58C91D', '0x013249266842e078999088807033D80531A84260'];
+const tokens = ['0xb1a5700fa2358173fe465e6ea4ff52e36e88e2ad', '0xd43D8aDAC6A4C7d9Aeece7c3151FcA8f23752cf8', '0x5ffd9EbD27f2fcAB044c0f0a26A45Cb62fa29c06'];
 
 module.exports = {
-	methodology: "BLAST can be staked in Blasterswap vault",
+	methodology: "TVL",
 	blast: {
-		tvl: () => ({}),
-		staking: staking(stakingContract, blast),
+		tvl: sumTokensExport({
+			owners,
+			tokens,
+		}),
 	},
 }
