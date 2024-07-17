7b9fdd12b88bbeb8a1ded65ee4ef308a5b7c1de0
TrKNguyen <nguyenusaco@gmail.com>
2024-07-17 16:57:42 +0700

    add BNRY adapter

---
+++ projects/bnry/index.js
@@ -0,0 +1,20 @@
+const BNRY_TOKEN_CONTRACT = '0xb5090d514bcaca7dafb7e52763658844121f346d';
+const TARGET_CONTRACT = '0xfB1dA2bA2B6c1e73e4Ace7aF2A38Fea4C289508e';
+
+async function tvl(api) {
+  const collateralBalance = await api.call({
+    abi: 'erc20:balanceOf',
+    target: BNRY_TOKEN_CONTRACT,
+    params: [TARGET_CONTRACT],
+  });
+
+  api.add(BNRY_TOKEN_CONTRACT, collateralBalance)
+}
+
+module.exports = {
+  methodology: 'counts the number of BNRY tokens in the specified contract on Optimism.',
+  start: 1709286506,
+  optimism: {
+    tvl,
+  },
+};
