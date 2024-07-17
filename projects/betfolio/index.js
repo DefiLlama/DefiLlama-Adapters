e2f6f50a4bcbb7ca8b11458a7ddfd38a08aa38ab
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2024-07-17 21:44:22 +0200

    betfolio, add soloPrediction (#11030)
    Co-authored-by: 0xpeluche <0xpeluche@protonmail.com>

--- projects/betfolio/index.js
+++ projects/betfolio/index.js
@@ -6,8 +6,11 @@ const config = {
 }
 
 async function getContracts() {
-  const { data: { list } } = await getConfig('betfolio', 'https://api.betfolio.co/api/v1/user/predictionList?limit=1000&duration=&type=')
-  return list.map(i => i.contract_address)
+  const [list0Res, list1Res] = await Promise.all([
+    getConfig('betfolio', 'https://api.betfolio.co/api/v1/user/predictionList?limit=1000&duration=&type='),
+    getConfig('betfolioSoloPrediction', 'https://api.betfolio.co/api/v1/user/getSoloPredictions?limit=1000&type=All&theme=All&solo_type=All')
+  ]);
+  return [...list0Res.data.list, ...list1Res.data.list].map(i => i.contract_address)
 }
 
 Object.keys(config).forEach(chain => {
