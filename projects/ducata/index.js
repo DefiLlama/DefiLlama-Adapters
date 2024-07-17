e550ce2c42c3f36f8a39d3cb559fd3b477db9057
RickVM <vanmelisrick@gmail.com>
2024-07-17 11:16:15 +0200

    feat: index tvl for Ducata

---
+++ projects/ducata/index.js
@@ -0,0 +1,10 @@
+const { onChainTvl } = require('../helper/balancer')
+
+const vault = "0x25898DEe0634106C2FcBB51B3DB5b14aA1c238a4";
+
+module.exports = {
+    methodology: 'counts the number of MINT tokens in the Club Bonding contract.',
+    arbitrum: {
+      tvl: onChainTvl(vault, 230182440),
+    }
+  };
