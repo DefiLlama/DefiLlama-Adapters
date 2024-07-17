167c66ce6cd9dd2e14a86eaf8121834290f96ef3
wgou <gw574813284@sina.com>
2024-07-17 16:16:06 +0800

    update contract

--- projects/layerx-protocol/index.js
+++ projects/layerx-protocol/index.js
@@ -3,11 +3,11 @@ const { sumTokens2, } = require('../helper/unwrapLPs')
 async function tvl(api) {
   
   return sumTokens2({ owners: [
-    '0xB88bcdaF020D963aFB0ED203bc02dB00D1b106C5',
-    '0x983b9d04e7F7De7E81E9520d878D9075657DE03B',
-    '0x7887761179ff0c3AaB0CA7CC794F5BD5e60e1674',
+    '0x6a7821c654e5F8E890ECaa530D7dd744A177A774',
+    '0xD904dF70A69e1550728DdF0659E06a6464b4127d',
+    '0xcA98b38F0fFc98C4Ee7EDfe16D7d611528f1eEc2',
     
-  ], tokens: ['0x7887761179ff0c3AaB0CA7CC794F5BD5e60e1674'], api, }) //lslor
+  ], tokens: ['0x7d2B7603234c7312976C6920f33A84531Ca40940'], api, }) //lslor
 }
 
 module.exports = {
