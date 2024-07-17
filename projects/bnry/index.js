f584da326f649f6dbfd4db53a8be5a9c5bac3b87
TrKNguyen <nguyenusaco@gmail.com>
2024-07-18 00:24:58 +0700

    change name of EOA_WALLET address

--- projects/bnry/index.js
+++ projects/bnry/index.js
@@ -1,11 +1,11 @@
 const BNRY_TOKEN_CONTRACT = '0xb5090d514bcaca7dafb7e52763658844121f346d';
-const TARGET_CONTRACT = '0xfB1dA2bA2B6c1e73e4Ace7aF2A38Fea4C289508e';
+const EOA_WALLET = '0xfB1dA2bA2B6c1e73e4Ace7aF2A38Fea4C289508e';
 
 async function tvl(api) {
   const collateralBalance = await api.call({
     abi: 'erc20:balanceOf',
     target: BNRY_TOKEN_CONTRACT,
-    params: [TARGET_CONTRACT],
+    params: [EOA_WALLET],
   });
 
   api.add(BNRY_TOKEN_CONTRACT, collateralBalance)
