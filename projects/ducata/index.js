d800ea24b7da2c8823dec47d9504d2fe46f3a6fd
RickVM <vanmelisrick@gmail.com>
2024-07-17 11:17:18 +0200

    refactor: clarify number

--- projects/ducata/index.js
+++ projects/ducata/index.js
@@ -1,10 +1,11 @@
 const { onChainTvl } = require('../helper/balancer')
 
 const vault = "0x25898DEe0634106C2FcBB51B3DB5b14aA1c238a4";
+const launchBlock = 230182440;
 
 module.exports = {
     methodology: 'counts the number of MINT tokens in the Club Bonding contract.',
     arbitrum: {
-      tvl: onChainTvl(vault, 230182440),
+      tvl: onChainTvl(vault, launchBlock),
     }
   };
