469556f9fbe438606bcdfdb5ced19533bd7bfbe8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-17 12:40:23 +0530

    code refactor

--- projects/size-credit/index.js
+++ projects/size-credit/index.js
@@ -1,36 +1,23 @@
-const WETH_CONTRACT = '0x4200000000000000000000000000000000000006';
-const AUSDC_CONTRACT = '0x4e65fe4dba92790696d040ac24aa414708f5c0ab';
-const SZDEBT_CONTRACT = '0xb0a00c4b3d77c896f46dc6b204695e22de7a185d';
-const SIZE_PROXY_CONTRACT = '0xC2a429681CAd7C1ce36442fbf7A4a68B11eFF940';
+const { sumTokensExport } = require('../helper/unwrapLPs')
+const ADDRESSES = require('../helper/coreAssets.json')
 
-async function tvl(api) {
-  const tokens = [
-    WETH_CONTRACT,
-    AUSDC_CONTRACT,
-  ]
-  const balances = await api.multiCall({
-    abi: 'erc20:balanceOf',
-    calls: tokens.map(i => ({
-      target: i,
-      params: SIZE_PROXY_CONTRACT
-    }))
-  })
-  return api.addTokens(tokens, balances)
-}
+const AUSDC_CONTRACT = '0x4e65fe4dba92790696d040ac24aa414708f5c0ab'
+const SZDEBT_CONTRACT = '0xb0a00c4b3d77c896f46dc6b204695e22de7a185d'
+const SIZE_PROXY_CONTRACT = '0xC2a429681CAd7C1ce36442fbf7A4a68B11eFF940'
+const tokens = [
+  ADDRESSES.base.WETH,
+  AUSDC_CONTRACT,
+]
 
 async function borrowed(api) {
-  const totalDebt = await api.call({
-    abi: 'erc20:totalSupply',
-    target: SZDEBT_CONTRACT,
-    params: [],
-  });
+  const totalDebt = await api.call({ abi: 'erc20:totalSupply', target: SZDEBT_CONTRACT, });
 
-  return api.add(SZDEBT_CONTRACT, totalDebt)
+  return api.add(ADDRESSES.base.USDbC, totalDebt)
 }
 
 module.exports = {
   base: {
-    tvl,
+    tvl: sumTokensExport({ tokens, owner: SIZE_PROXY_CONTRACT }),
     borrowed
   }
 }
