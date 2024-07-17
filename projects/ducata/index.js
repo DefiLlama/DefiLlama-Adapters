352ff55b2ef62c28ae886e8ed623d7a84ae40572
RickVM <vanmelisrick@gmail.com>
2024-07-17 12:27:08 +0200

    fix: improve methodology description

--- projects/ducata/index.js
+++ projects/ducata/index.js
@@ -4,7 +4,7 @@ const vault = "0x25898DEe0634106C2FcBB51B3DB5b14aA1c238a4";
 const launchBlock = 230182440;
 
 module.exports = {
-    methodology: 'counts the number of MINT tokens in the Club Bonding contract.',
+    methodology: 'TVL counts the external assets deposited in the vaults',
     arbitrum: {
       tvl: onChainTvl(vault, launchBlock),
     }
