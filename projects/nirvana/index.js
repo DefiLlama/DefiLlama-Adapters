7dce0aedcbeeb822179454b64e0ab0330c2f43c9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-19 15:47:43 +0300

    refactor sol adapters

--- projects/nirvana/index.js
+++ projects/nirvana/index.js
@@ -1,21 +1,18 @@
-const { getTokenBalance, getTokenAccountBalance, } = require('../helper/solana');
+const { sumTokens2 } = require('../helper/solana');
 const ANA = "ANAxByE6G2WjFp7A4NqtWYXb3mgruyzZYg3spfxe6Lbo";
 const stakingContract = "CxuuSEv67PzNkMxqCvHeDUr6HKaadoz8NhTfxbQSJnaG";
 
 async function staking() {
-  const balance = await getTokenBalance(ANA, stakingContract);
-  return { 'nirvana-ana': balance };
+  return sumTokens2({ tokensAndOwners: [[ANA, stakingContract]] });
 }
+
 async function tvl() {
-  return { 
-    'tether': await getTokenAccountBalance('EwQa93fpN2a6smD7EBFjQEyDt3DP8EMLHLjgAWxG9Qhj'),
-    'usd-coin': await getTokenAccountBalance('6t7ViNDULtkgvmntHsmi1QV73Tez7ZcX6DJvARHkfUJZ'),
-  };
+  return sumTokens2({ tokenAccounts: ['EwQa93fpN2a6smD7EBFjQEyDt3DP8EMLHLjgAWxG9Qhj', '6t7ViNDULtkgvmntHsmi1QV73Tez7ZcX6DJvARHkfUJZ',] })
 }
 
 module.exports = {
   hallmarks: [
-    [1659009600,"Flash-Loan & Price Manipulation Attack"]
+    [1659009600, "Flash-Loan & Price Manipulation Attack"]
   ],
   timetravel: false,
   methodology:
@@ -24,4 +21,5 @@ module.exports = {
     tvl,
     staking,
   },
+  deadFrom: 1669009600,
 };
