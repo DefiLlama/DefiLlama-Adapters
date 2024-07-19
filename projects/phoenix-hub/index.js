0545ed7ac61c0e16d4f245f29b847fbed244a678
Milan Steiner <69144826+msteiner96@users.noreply.github.com>
2024-07-19 23:23:40 +0200

    Phoenix DeFi Hub TVL Adapter (#11047)
    * Phoenix DeFi Hub TVL Adapter
    
    * Add newline to end of file

---
+++ projects/phoenix-hub/index.js
@@ -0,0 +1,18 @@
+const utils = require("../helper/utils");
+const {toUSDTBalances} = require("../helper/balances");
+
+async function tvl() {
+    // Fetch all tickers
+    const tickers = await utils.fetchURL("https://api-phoenix.decentrio.ventures/tickers");
+
+    // Sum up the tvl
+    const totalTVL = tickers.data.reduce((acc, t) => acc + t.liquidity_in_usd, 0);
+    return toUSDTBalances(totalTVL);
+}
+
+module.exports = {
+    methodology: 'counts the liquidity on all Phoenix Hub AMM Pools.',
+    stellar: {
+        tvl,
+    }
+};
