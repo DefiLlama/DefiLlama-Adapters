9f1af4336fee2d121b0826e21e3b9b45c3335e10
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-19 08:21:03 +0300

    minor fix

--- projects/blasterswap-vaults/index.js
+++ projects/blasterswap-vaults/index.js
@@ -4,9 +4,8 @@ const vaults = ['0x0464a36beCf9967111D2dCAb57CAf4a2376f6E3F', '0xC52fb7E613e401a
 const tokens = ['0xb1a5700fa2358173fe465e6ea4ff52e36e88e2ad', '0xd43D8aDAC6A4C7d9Aeece7c3151FcA8f23752cf8', '0x5ffd9EbD27f2fcAB044c0f0a26A45Cb62fa29c06'];
 
 module.exports = {
-	methodology: "Staking",
 	blast: {
-		tvl: (async) => ({}),
+		tvl: () => ({}),
 		staking: stakings(vaults, tokens)
 	},
 }
