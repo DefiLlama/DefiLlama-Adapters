2e56a14dde5486ab0dd8f065a9343677614674ca
Kostas Tzoumpas <tzoumpas.ks@gmail.com>
2024-07-22 19:47:56 +0300

    fix bug

--- projects/valoro/index.js
+++ projects/valoro/index.js
@@ -1,19 +1,21 @@
 const { sumTokens } = require("../helper/chain/elrond");
 const { get } = require("../helper/http");
 
-const fundsOwnerScAddress =
+const fundsOwnerSc =
     "erd1qqqqqqqqqqqqqpgqqvj2zrdfv4lsc38p8cvh4e0yd4av6njfu7zsj7ztzl";
 
-async function getFundScs(address) {
+async function getFunds(address) {
     const fundsScs = await get(`https://api.multiversx.com/accounts/${address}/contracts`);
     return fundsScs
 }
 
 async function tvl() {
-    const fundScs = await getFundScs(fundsOwnerScAddress);
-    const fundsAddresses = [...fundScs.map(fund => fund.address)];
-    
-    return sumTokens({ fundsAddresses });
+    const funds = await getFunds(fundsOwnerSc);
+    const owners = [
+        ...funds.map(fund => fund.address)
+      ];
+
+    return sumTokens({ owners });
 }
 
 module.exports = {
