2d91e92ceb82aeaf08c3471813bd5bf2b2614c0c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-22 09:42:10 +0300

    code refactor

--- projects/goldlink/index.js
+++ projects/goldlink/index.js
@@ -1,46 +1,10 @@
-const abi = require("./abi.json");
-
-const GOLDLINK_GMX_VAULT_ADDRESS = '0xd8dd54df1a7d2ea022b983756d8a481eea2a382a'
-const USDC_TOKEN_ADDRESS = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831'
-
-async function getTotalDeposits(api, block) {
-    const totalAmount = await api.call({
-        block,
-        target: GOLDLINK_GMX_VAULT_ADDRESS,
-        abi: abi.totalAssets,
-        chain: 'arbitrum'
-      })
-
-    api.add(USDC_TOKEN_ADDRESS, totalAmount);
-}
-
-async function getBorrows(api, block) {
-  const totalAmount = await api.call({
-    block,
-    target: GOLDLINK_GMX_VAULT_ADDRESS,
-    abi: abi.utilizedAssets_,
-    chain: 'arbitrum'
-  })
-
-  api.add(USDC_TOKEN_ADDRESS, totalAmount);
-}
-
-async function borrowed(timestamp, ethBlock, {arbitrum: block}, {api}) {
-  return await getBorrows(api, block)
-}
-
-
-async function tvl(timestamp, ethBlock, {arbitrum: block}, {api}) {
-  return await getTotalDeposits(api, block)
-}
-
+const { sumERC4626VaultsExport } = require('../helper/erc4626')
 
 module.exports = {
-    methodology: 'Delta neutral farming in GMX Vault',
-    timetravel: true,
-    start: 1716638498,
-    arbitrum: {
-      tvl: tvl,
-      borrowed: borrowed,
-    },
+  methodology: 'Delta neutral farming in GMX Vault',
+  start: 1716638498,
+  doublecounted: true,
+  arbitrum: {
+    tvl: sumERC4626VaultsExport({ vaults: ['0xd8dd54df1a7d2ea022b983756d8a481eea2a382a',], isOG4626: true, }),
+  },
 }
