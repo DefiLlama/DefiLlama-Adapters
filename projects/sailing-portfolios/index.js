18e9d01d370d5c9ceeaf3090d240b0f1d5c9553c
Julio <julioagbarrios@gmail.com>
2024-07-22 15:31:34 -0300

    added function for +50k pools

--- projects/sailing-portfolios/index.js
+++ projects/sailing-portfolios/index.js
@@ -10,6 +10,8 @@ const usdtAddresses = {
   kava: '0x919C1c267BC06a7039e03fcc2eF738525769109c',
 };
 
+const feeTiers = [500, 1000, 3000];
+
 function calculatePrice(tokenValue, sqrtPriceX96) {
   const priceInUsdt = (Number(sqrtPriceX96) ** 2) / (2 ** 192);
   return Math.floor(Number(tokenValue) * priceInUsdt);
@@ -26,11 +28,11 @@ async function fetchPortfolioAssets(api, portfolios) {
   return await api.multiCall({ abi: 'address[]:getPortfolioAssets', calls: portfolios });
 }
 
-async function fetchUniswapPoolAddress(api, uniswapV3Factory, tokenAddress, usdtAddress) {
+async function fetchUniswapPoolAddress(api, uniswapV3Factory, tokenAddress, usdtAddress, fee) {
   return await api.call({
     target: uniswapV3Factory,
     abi: 'function getPool(address, address, uint24) view returns (address)',
-    params: [tokenAddress, usdtAddress, 500],
+    params: [tokenAddress, usdtAddress, fee],
   });
 }
 
@@ -39,6 +41,51 @@ async function fetchSlot0(api, poolAddress) {
     target: poolAddress,
     abi: 'function slot0() view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, uint8 feeProtocol, bool unlocked)',
   });
+}
+
+async function fetchTokenBalance(api, tokenAddress, poolAddress) {
+  return await api.call({
+      target: tokenAddress,
+      abi: 'function balanceOf(address) view returns (uint256)',
+      params: [poolAddress],
+  });
+}
+
+async function getPools(api, tokenAddress, usdtAddress) {
+  const uniswapV3Factory = uniswapV3Factories[api.chain];
+  const pools = [];
+
+  for (const fee of feeTiers) {
+    const poolAddress = await fetchUniswapPoolAddress(api, uniswapV3Factory, tokenAddress, usdtAddress, fee);
+    if (poolAddress !== '0x0000000000000000000000000000000000000000') {
+      pools.push(poolAddress);
+    }
+  }
+
+  return pools;
+}
+
+async function findBestPoolWithLiquidity(api, pools, tokenAddress, usdtAddress, minLiquidity) {
+  let bestPool = null;
+  let bestLiquidity = 0;
+
+  for (const poolAddress of pools) {
+    const slot0 = await fetchSlot0(api, poolAddress);
+    const sqrtPriceX96 = slot0.sqrtPriceX96;
+
+    const balanceOfToken0 = await fetchTokenBalance(api, tokenAddress, poolAddress);
+    const balanceOfToken1 = await fetchTokenBalance(api, usdtAddress, poolAddress);
+    const pricePerTokenInUsdt = (Number(sqrtPriceX96) ** 2) / (2 ** 192);
+
+    const resultOfTVLPoolInUsd = (balanceOfToken0 * pricePerTokenInUsdt) + balanceOfToken1;
+
+    if (resultOfTVLPoolInUsd >= minLiquidity && resultOfTVLPoolInUsd > bestLiquidity) {
+      bestPool = poolAddress;
+      bestLiquidity = resultOfTVLPoolInUsd;
+    }
+  }
+  
+  return bestPool;
 }
 
 async function tvl(api) {
@@ -50,10 +97,14 @@ async function tvl(api) {
   const sum = await api.sumTokens({ ownerTokens });
 
   const tokenAddress = Object.keys(sum).length > 0 ? Object.keys(sum)[0].split(':')[1] : null;
-
-  const uniswapV3Factory = uniswapV3Factories[api.chain];
   const usdtAddress = usdtAddresses[api.chain];
-  const poolAddress = await fetchUniswapPoolAddress(api, uniswapV3Factory, tokenAddress, usdtAddress);
+
+  const pools = await getPools(api, tokenAddress, usdtAddress);
+  const poolAddress = await findBestPoolWithLiquidity(api, pools, tokenAddress, usdtAddress, 50000);
+
+  if (!poolAddress) {
+    return {};
+  }
 
   const slot0 = await fetchSlot0(api, poolAddress);
 
