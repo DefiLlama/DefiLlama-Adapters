188a9c53831a6fad46f152788058f3be92dcd95f
Justyn Spooner <justyn@dovu.io>
2024-07-22 17:22:51 +0100

    Use staking key for hedera usd

--- projects/dovu/index.js
+++ projects/dovu/index.js
@@ -1,12 +1,17 @@
 const { fetchURL } = require("../helper/utils")
 
-async function fetch() {
+async function staking() {
   const res = await fetchURL("https://api.dovu.market/api/v1/staking-info");
-  return res.data.total_value_locked.usd;
+  return {
+    usd: res.data.total_value_locked.usd
+  };
 }
 
 module.exports = {
   timetravel: false,
   methodology: 'The calculated TVL is the current USD sum of all accounts staked in the DOVU staking pool.',
-  fetch
+  hedera: {
+    staking,
+    tvl: () => ({}),
+  }
 }
