da2140ab07cd4159aa9cbd6cb602b504d4bff31d
Edgar Almeida <edgar@romeblockchain.com>
2024-07-22 16:28:05 -0300

    feat: Kinetic protocol

--- projects/kinetic/index.js
+++ projects/kinetic/index.js
@@ -1,5 +1,5 @@
 const { compoundExports2 } = require('../helper/compound')
 
 module.exports = {
-  flare: compoundExports2({ comptroller: '0x8041680Fb73E1Fe5F851e76233DCDfA0f2D2D7c8' }),
+  flare: compoundExports2({ comptroller: '0x8041680Fb73E1Fe5F851e76233DCDfA0f2D2D7c8', cether: '0x1D80c49BbBCd1C0911346656B529DF9E5c2F783d' }),
 }
