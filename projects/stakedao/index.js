9919fd2975f9611f06525a6cd316334240602f44
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-22 17:47:31 +0300

    stakedao: bsc use multicall

--- projects/stakedao/index.js
+++ projects/stakedao/index.js
@@ -1,5 +1,3 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const sdk = require('@defillama/sdk');
 const abi = require('./abi.json')
 const { sumTokens2, PANCAKE_NFT_ADDRESS } = require('../helper/unwrapLPs')
 const { getConfig } = require('../helper/cache');
@@ -10,20 +8,20 @@ const LOCKERS_ENDPOINT = 'https://api.stakedao.org/api/lockers';
 const PANCAKESWAP_MASTERCHEF_V3 = '0x556B9306565093C855AEA9AE92A594704c2Cd59e'
 
 const LOCKERS = {
-  curve : {
-    1 : '0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6',
-    42161 : '0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6'
+  curve: {
+    1: '0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6',
+    42161: '0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6'
   },
-  balancer : {
-    1 : '0xea79d1A83Da6DB43a85942767C389fE0ACf336A5'
+  balancer: {
+    1: '0xea79d1A83Da6DB43a85942767C389fE0ACf336A5'
   },
-  pendle : {
-    1 : '0xD8fa8dC5aDeC503AcC5e026a98F32Ca5C1Fa289A'
+  pendle: {
+    1: '0xD8fa8dC5aDeC503AcC5e026a98F32Ca5C1Fa289A'
   },
-  yearn : {
-    1 : '0xF750162fD81F9a436d74d737EF6eE8FC08e98220'
+  yearn: {
+    1: '0xF750162fD81F9a436d74d737EF6eE8FC08e98220'
   },
-  pancakeswap : {
+  pancakeswap: {
     1: '0xB7F79090190c297F59A2b7D51D3AEF7AAd0e9Af3',
     56: '0x1e6f87a9ddf744af31157d8daa1e3025648d042d',
     42161: '0xE5244b1A263ce45CF1E51DfA97469711E9bAD68d',
@@ -34,10 +32,10 @@ const LOCKERS = {
 
 async function getLPStrategiesMainnet(timestamp, block) {
   const resp = await Promise.all([
-    get(`${STRATEGIES_ENDPOINT}/curve/1.json`),
-    get(`${STRATEGIES_ENDPOINT}/balancer/1.json`),
-    get(`${STRATEGIES_ENDPOINT}/pendle/1.json`),
-    get(`${STRATEGIES_ENDPOINT}/yearn/1.json`),
+    getConfig('sakedao/eth-curve', `${STRATEGIES_ENDPOINT}/curve/1.json`),
+    getConfig('sakedao/eth-balancer', `${STRATEGIES_ENDPOINT}/balancer/1.json`),
+    getConfig('sakedao/eth-pendle', `${STRATEGIES_ENDPOINT}/pendle/1.json`),
+    getConfig('sakedao/eth-yearn', `${STRATEGIES_ENDPOINT}/yearn/1.json`),
   ]);
 
   const curveStrats = resp[0].deployed.map((strat) => [strat.gaugeAddress, LOCKERS.curve[1]])
@@ -49,48 +47,18 @@ async function getLPStrategiesMainnet(timestamp, block) {
 }
 
 async function tvl(api) {
-  let balances = {}
   /////////////////////////////////////////////////////////////////////
   // --- STRATEGIES V2 
   /////////////////////////////////////////////////////////////////////
-  // ==== Addresses ==== //
-  const crv3_vault_v2 = {
-    contract: '0xB17640796e4c27a39AF51887aff3F8DC0daF9567',
-    token: '0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490',
-  }
-  const eurs_vault_v2 = {
-    contract: '0xCD6997334867728ba14d7922f72c893fcee70e84',
-    token: '0x194eBd173F6cDacE046C53eACcE9B953F28411d1',
-  }
-  const frax_vault_v2 = {
-    contract: '0x5af15DA84A4a6EDf2d9FA6720De921E1026E37b7',
-    token: '0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B',
-  }
-  const frax_vault2_v2 = {
-    contract: '0x99780beAdd209cc3c7282536883Ef58f4ff4E52F',
-    token: '0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B',
-  }
-  const eth_vault_v2 = {
-    contract: '0xa2761B0539374EB7AF2155f76eb09864af075250',
-    token: '0xA3D87FffcE63B53E0d54fAa1cc983B7eB0b74A9c',
-  }
-  const steth_vault_v2 = {
-    contract: '0xbC10c4F7B9FE0B305e8639B04c536633A3dB7065',
-    token: '0x06325440D014e39736583c165C2963BA99fAf14E',
-  }
   let vaults = [
-    crv3_vault_v2,
-    eurs_vault_v2,
-    frax_vault_v2,
-    frax_vault2_v2,
-    eth_vault_v2,
-    steth_vault_v2
+    '0xB17640796e4c27a39AF51887aff3F8DC0daF9567', , // crv3_vault_v2 
+    '0xCD6997334867728ba14d7922f72c893fcee70e84', , // eurs_vault_v2 
+    '0x5af15DA84A4a6EDf2d9FA6720De921E1026E37b7', , // frax_vault_v2 
+    '0x99780beAdd209cc3c7282536883Ef58f4ff4E52F', , // frax_vault2_v2 
+    '0xa2761B0539374EB7AF2155f76eb09864af075250', , // eth_vault_v2 
+    '0xbC10c4F7B9FE0B305e8639B04c536633A3dB7065', , // steth_vault_v2 
   ]
-  const vaultBals = await api.multiCall({
-    abi: abi['balance'],
-    calls: vaults.map(i => i.contract),
-  })
-  vaultBals.forEach((bal, i) => sdk.util.sumSingleBalance(balances, vaults[i].token, bal))
+  await api.erc4626Sum({ calls: vaults })
 
   /////////////////////////////////////////////////////////////////////
   // --- LP Strategies
@@ -134,7 +102,7 @@ async function tvl(api) {
   let lockerMAVBal = []
 
   for (const { contract, veToken } of callsMAV) {
-    const count = await api.call({  abi: 'function lockupCount(address) view returns (uint256)', target: veToken, params: contract })
+    const count = await api.call({ abi: 'function lockupCount(address) view returns (uint256)', target: veToken, params: contract })
     let balance = 0
     for (let i = 0; i < count; i++) {
       const lockup = await api.call({ abi: 'function lockups(address,uint256) view returns (uint256 amount, uint256 end, uint256 points)', target: veToken, params: [contract, i] })
@@ -149,100 +117,78 @@ async function tvl(api) {
       amount = lockerPendleBal.shift().amount
     } else if (lockersInfos[i].veToken.toLowerCase() == veMAV) {
       amount = lockerMAVBal.shift().amount
-    }  else {
+    } else {
       amount = lockerBals.shift().amount
     }
-    sdk.util.sumSingleBalance(balances, lockersInfos[i].token, amount)
+    api.add(lockersInfos[i].token, amount)
   }
 
-
-  return sumTokens2({ 
-    api, 
-    tokensAndOwners: strategies, 
-    balances, 
+  return sumTokens2({
+    api,
+    tokensAndOwners: strategies,
     uniV3nftsAndOwners: [[PANCAKE_NFT_ADDRESS, LOCKERS.pancakeswap[1]]],
     uniV3ExtraConfig: { nftIdFetcher: PANCAKESWAP_MASTERCHEF_V3 }
   })
 }
 
-async function staking(timestamp, block) {
+async function staking(api) {
   const sanctuary = '0xaC14864ce5A98aF3248Ffbf549441b04421247D3'
   const arbStrat = '0x20D1b558Ef44a6e23D9BF4bf8Db1653626e642c3'
   const veSdt = '0x0C30476f66034E11782938DF8e4384970B6c9e8a'
   const sdtToken = '0x73968b9a57c6E53d41345FD57a6E6ae27d6CDB2F'
   return sumTokens2({
+    api,
     owners: [sanctuary, arbStrat, veSdt,],
     tokens: [sdtToken]
   })
 }
 
-// ----------------------------------- POLYGON ----------------------------------- //
-
 async function polygon(api) {
-  return getBalances(api, [{contract: '0x7d60F21072b585351dFd5E8b17109458D97ec120'}])
-}
-
-// ----------------------------------- AVALANCHE ----------------------------------- //
-
-async function getBalances(api, vaults, { balances = {} } = {}) {
-  const tokens = await api.multiCall({ abi: 'address:token', calls: vaults.map(i => i.contract) })
-  const bals = await api.multiCall({ abi: 'uint256:balance', calls: vaults.map(i => i.contract) })
-  tokens.forEach((token, i) => sdk.util.sumSingleBalance(balances, token, bals[i], api.chain))
-  return balances
+  return api.erc4626Sum({ calls: ['0x7d60F21072b585351dFd5E8b17109458D97ec120'] })
 }
 
 async function avax(api) {
-  const crv_3crv_vault_avalanche = {
-    contract: '0x0665eF3556520B21368754Fb644eD3ebF1993AD4',
-  }
-
-  const vaultsAvalanche = [
-    crv_3crv_vault_avalanche
-  ]
-  return getBalances(api, vaultsAvalanche)
+  return api.erc4626Sum({ calls: ['0x0665eF3556520B21368754Fb644eD3ebF1993AD4'] })
 }
-
-// ----------------------------------- BINANCE SMART CHAIN -----------------------------------
 
 async function addPancakeSwapLPStrategiesBsc(api) {
   const resp = await Promise.all([
-    get(`${STRATEGIES_ENDPOINT}/pancakeswap/56.json`),
+    getConfig('stakedao/bsc-cake', `${STRATEGIES_ENDPOINT}/pancakeswap/56.json`),
   ]);
 
   const strats = resp[0].deployed.filter((strat) => strat.version !== '3')
-  for (const strat of strats){
-    const deposits = await api.call({abi : 'function totalSupply() view returns (uint256)', target: strat.sdGauge.address})
-
+  const deposits = await api.multiCall({ abi: 'uint256:totalSupply', calls: strats.map((strat) => strat.sdGauge.address) })
+  const stableStrats = []
+  const defaultStrats = []
+  strats.forEach((strat, i) => {
+    strat.deposits = deposits[i]
     switch (strat.version) {
-      case 'stable': {
-        const token_balances = await api.multiCall({ abi: 'function balances(uint256) view returns(uint256)', calls: [0, 1], target: strat.pool })
-        const totalSupply = await api.call({ abi: 'function totalSupply() view returns (uint256)', target: strat.lpToken.address, })
-        
-        api.add(strat.coins[0].address, deposits * token_balances[0] / totalSupply)
-        api.add(strat.coins[1].address, deposits * token_balances[1] / totalSupply)
-        break
-      }
-      case '2': {
-        const balances = await api.call({ abi: 'function getReserves() view returns(uint112 _reserve0 ,uint112 _reserve1,uint32 _blockTimestampLast)', target: strat.lpToken.address, })
-        const totalSupply = await api.call({ abi: 'function totalSupply() view returns (uint256)', target: strat.lpToken.address, })
-
-        api.add(strat.coins[0].address, deposits * balances._reserve0 / totalSupply)
-        api.add(strat.coins[1].address, deposits * balances._reserve1 / totalSupply)
-        break
-      }
-      default : {
-        const adapterAddress = await api.call({ abi: 'function adapterAddr() view returns(address)', target: strat.gaugeAddress, })
-        const tokenPerShare = await api.call({ abi : 'function tokenPerShare() view returns(uint256 _token0PerShare, uint256 _token1PerShare)', target : adapterAddress})
-        const totalSupply = await api.call({ abi: 'function totalSupply() view returns (uint256)', target: strat.lpToken.address, })
-
-        api.add(strat.coins[0].address, deposits * tokenPerShare._token0PerShare / totalSupply)
-        api.add(strat.coins[1].address, deposits * tokenPerShare._token1PerShare / totalSupply)
-        break
-      }
+      case 'stable': stableStrats.push(strat); break
+      case '2': api.add(strat.lpToken.address, deposits[i]); break
+      default: defaultStrats.push(strat)
     }
-    
-    api.add(strat.lpToken.address, deposits)
-  }
+  })
+
+  const stableLPsupplies = await api.multiCall({ abi: 'uint256:totalSupply', calls: stableStrats.map((strat) => strat.lpToken.address) })
+  const stableToken0Bals = await api.multiCall({ abi: 'function balances(uint256) view returns(uint256)', calls: stableStrats.map((strat) => ({ target: strat.pool, params: 0})) })
+  const stableToken1Bals = await api.multiCall({ abi: 'function balances(uint256) view returns(uint256)', calls: stableStrats.map((strat) => ({ target: strat.pool, params: 1})) })
+  stableStrats.forEach((strat, i) => {
+    const ratio =  strat.deposits / stableLPsupplies[i]
+    api.add(strat.coins[0].address, ratio * stableToken0Bals[i])
+    api.add(strat.coins[1].address, ratio * stableToken1Bals[i])
+  })
+
+  const dGauges = defaultStrats.map(i => i.gaugeAddress)
+  const dLpTokens = defaultStrats.map(i => i.lpToken.address)
+  const adapterAddress = await api.multiCall({  abi: 'address:adapterAddr', calls: dGauges})
+  const tokenPerShares = await api.multiCall({ abi: 'function tokenPerShare() view returns(uint256 _token0PerShare, uint256 _token1PerShare)', calls: adapterAddress })
+  const dLpSupplies = await api.multiCall({ abi: 'uint256:totalSupply', calls: dLpTokens })
+
+  defaultStrats.forEach((strat, i) => {
+    const ratio = strat.deposits / dLpSupplies[i]
+    api.add(strat.coins[0].address, ratio * tokenPerShares[i]._token0PerShare)
+    api.add(strat.coins[1].address, ratio * tokenPerShares[i]._token1PerShare)
+  })
 }
 
 async function bsc(api) {
@@ -250,27 +196,24 @@ async function bsc(api) {
   const VE_CAKE = '0x5692DB8177a81A6c6afc8084C2976C9933EC1bAB'
 
   const cakeLock = await api.multiCall({ abi: abi.locks, calls: [{ target: VE_CAKE, params: LOCKERS.pancakeswap[56] }] })
-  const cake = Number(cakeLock[0].amount) / 1e18
+  api.add('0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', cakeLock[0].amount)
 
-  // PANCAKE NFT STRATEGIES
-  const pcsStratsTvl = await sumTokens2({ 
-    api,
-    uniV3nftsAndOwners: [[PANCAKE_NFT_ADDRESS, LOCKERS.pancakeswap[56]]],
-    uniV3ExtraConfig: { nftIdFetcher: PANCAKESWAP_MASTERCHEF_V3 }
-  })
 
   // PANCAKE LP STRATEGIES
   await addPancakeSwapLPStrategiesBsc(api)
-  return {
-    'pancakeswap-token': cake, ...pcsStratsTvl
-  }
-}
 
-// ----------------------------------- ARBITRUM ----------------------------------- //
+  // PANCAKE NFT STRATEGIES
+  return sumTokens2({
+    api,
+    uniV3nftsAndOwners: [[PANCAKE_NFT_ADDRESS, LOCKERS.pancakeswap[56]]],
+    uniV3ExtraConfig: { nftIdFetcher: PANCAKESWAP_MASTERCHEF_V3 },
+    resolveLP: true,
+  })
+}
 
 async function getLPStrategiesArbitrum(timestamp, block) {
   const resp = await Promise.all([
-    get(`${STRATEGIES_ENDPOINT}/curve/42161.json`),
+    getConfig('sakedao/arb-curve', `${STRATEGIES_ENDPOINT}/curve/42161.json`),
   ]);
 
   const stratsCurve = resp[0].deployed.map((strat) => [strat.gaugeAddress, LOCKERS.curve[42161]])
@@ -281,15 +224,14 @@ async function getLPStrategiesArbitrum(timestamp, block) {
 async function arbitrum(api) {
   const strategies = await getLPStrategiesArbitrum()
   const PANCAKESWAP_MASTERCHEF_V3_ARBITRUM = '0x5e09ACf80C0296740eC5d6F643005a4ef8DaA694'
-  return sumTokens2({ 
-    api, 
-    tokensAndOwners: strategies, 
+  return sumTokens2({
+    api,
+    tokensAndOwners: strategies,
     uniV3nftsAndOwners: [[PANCAKE_NFT_ADDRESS, LOCKERS.pancakeswap[42161]]],
     uniV3ExtraConfig: { nftIdFetcher: PANCAKESWAP_MASTERCHEF_V3_ARBITRUM }
   })
 }
 
-// node test.js projects/stakedao/index.js
 module.exports = {
   misrepresentedTokens: true,
   ethereum: {
