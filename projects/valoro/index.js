1fc30cb1bdd077d791671abdf2ea4b3d5bf406e5
Kostas Tzoumpas <tzoumpas.ks@gmail.com>
2024-07-22 19:42:41 +0300

    More transparent TVL calculation

--- projects/valoro/index.js
+++ projects/valoro/index.js
@@ -1,17 +1,20 @@
-const { call } = require("../helper/chain/elrond");
+const { sumTokens } = require("../helper/chain/elrond");
+const { get } = require("../helper/http");
 
-const proxyScAddress =
+const fundsOwnerScAddress =
     "erd1qqqqqqqqqqqqqpgqqvj2zrdfv4lsc38p8cvh4e0yd4av6njfu7zsj7ztzl";
-const usdc = "USDC-c76f1f";
 
-const tvl = async (api) => {
-    const cash = await call({
-        target: proxyScAddress,
-        abi: "getTotalNetAssetsValue",
-        responseTypes: ["number"],
-    });
-    api.addTokens([usdc], [cash.toString()]);
-};
+async function getFundScs(address) {
+    const fundsScs = await get(`https://api.multiversx.com/accounts/${address}/contracts`);
+    return fundsScs
+}
+
+async function tvl() {
+    const fundScs = await getFundScs(fundsOwnerScAddress);
+    const fundsAddresses = [...fundScs.map(fund => fund.address)];
+    
+    return sumTokens({ fundsAddresses });
+}
 
 module.exports = {
     timetravel: false,
