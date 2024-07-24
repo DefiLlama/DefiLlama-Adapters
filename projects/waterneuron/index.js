94195d5472e47a69c518c742005eaf5683f737de
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-07-24 21:35:46 +0530

    Update index.js

--- projects/waterneuron/index.js
+++ projects/waterneuron/index.js
@@ -1,18 +1,13 @@
-const BigNumber = require("bignumber.js");
 const { get } = require("../helper/http");
 
 const url = 'https://tsbvt-pyaaa-aaaar-qafva-cai.raw.icp0.io/api/metadata';
 
-async function tvl(_timestamp, _block) {
-  const metadata = (await get(url));
-  const staked_icp = metadata.tracked_6m_stake + metadata.neuron_8y_stake_e8s;
-  if (staked_icp === undefined) {
-      // API didn't return a tvl number to work with
-      throw new Error("Unknown");
-  }
+async function tvl() {
+  const metadata = await get(url)
+  const staked_icp = metadata.tracked_6m_stake + metadata.neuron_8y_stake_e8s
   return {
-    "coingecko:internet-computer": BigNumber(staked_icp).div(1e8).toFixed(0),
-  };
+    "coingecko:internet-computer": staked_icp/ 1e8
+  }
 }
 
 module.exports = {
@@ -21,4 +16,4 @@ module.exports = {
   icp: {
     tvl,
   },
-};
+}
