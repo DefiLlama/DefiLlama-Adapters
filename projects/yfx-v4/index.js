6a61f5cf377e85bf7e40d1efebd4a6daa47d6310
YFX <73627416+yfxcom@users.noreply.github.com>
2024-07-24 15:21:22 +0800

    add YFX V4 info on Base

--- projects/yfx-v4/index.js
+++ projects/yfx-v4/index.js
@@ -1,10 +1,11 @@
-const manager = '0x29bd0372A8A087e4d34d4098259Cd298d554BAc4';
-const vault = '0x50b516a9DB620aB67A33d895DAF4Bd1c294b9517';
-
+const addrs = {
+	"arbitrum": {"manager": "0xFE1ca968afbadEd3BF2CB685451C858Deb46Ce31", "vault": "0x50b516a9DB620aB67A33d895DAF4Bd1c294b9517"},
+	"base": {"manager":"0xfD38EaD11eaa5D566dc331848fF495B3D5cfb79F", "vault": "0xfcDaC5524EC86223216643384f5c59C65560d673"}
+}
 async function tvl(api) {
-  let pools = await api.call({ target: manager, abi: "address[]:getAllPools", });
+  let pools = await api.call({ target: addrs[api.chain].manager, abi: "address[]:getAllPools", });
   const tokens = await api.multiCall({ abi: 'address:getBaseAsset', calls: pools })
-  return api.sumTokens({ owner: vault, tokens })
+  return api.sumTokens({ owner: addrs[api.chain].vault, tokens})
 }
 
 module.exports = {
@@ -12,4 +13,7 @@ module.exports = {
   arbitrum: {
     tvl,
   },
+  base: {
+	  tvl,
+  }
 }
