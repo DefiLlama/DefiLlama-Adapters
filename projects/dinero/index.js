aa3449b6b2bf6392268406ee86ad8f26486e958c
rafael albquerque bezerra <bzrra.rafael@gmail.com>
2024-07-25 11:32:29 -0400

    feat: dinero protocol - remove redacted and rename old dinero to pirex-eth

--- projects/dinero/index.js
+++ projects/dinero/index.js
@@ -1,13 +1,17 @@
-const { nullAddress } = require("../helper/unwrapLPs");
+const dinero = '0x6DF0E641FC9847c0c6Fde39bE6253045440c14d3';
+const sDinero = '0x55769490c825CCb09b2A6Ae955203FaBF04857fd';
+const sDineroAbi = {
+    totalAssets: "function totalAssets() view returns (uint256)"
+}
 
 module.exports = {
-  ethereum: {
-    tvl: async (api) => {
-      const totalSupply = await api.call({ target: '0x04C154b66CB340F3Ae24111CC767e0184Ed00Cc6', abi: 'uint256:totalSupply'});
+    ethereum: {
+      tvl: async (api) => {
+        const totalSupply = await api.call({ target: sDinero, abi: sDineroAbi.totalAssets});
       
-      return {
-        [nullAddress]: totalSupply
-      };
+        return {
+          [dinero]: totalSupply
+        };
+      }
     }
-  }
-};
+  };
