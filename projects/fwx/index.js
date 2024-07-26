ac25f75a69f2a52225280162407dbcdddf8981a0
ideapacito <hattayaporn@fwx.finance>
2024-07-26 18:18:00 +0700

    update tvl(3)

--- projects/fwx/index.js
+++ projects/fwx/index.js
@@ -1,9 +1,13 @@
-const { sumTokens2 } = require("../helper/unwrapLPs");
-const ADDRESSES = require("../helper/coreAssets.json");
-const VECTOR = require("../vector/vectorContracts.json");
+const { getLogs2 } = require('../helper/cache/getLogs')
 const axios = require('axios')
 
 const xliplessDex = "0x82E90fB94fd9a5C19Bf38648DD2C9639Bde67c74";
+
+module.exports = {
+  avax: {
+    tvl,
+  },
+};
 
 async function tvl(api) {
   const getAssetRes = await axios.get("https://app.fwx.finance/api/v2/assets?chain_id=43114")
@@ -19,11 +23,16 @@ async function tvl(api) {
     );
   }
 
-  return sumTokens2({ api, tokensAndOwners })
+  const FACTORY_SUB_MODULE_PROXY = '0x54b048eB204B7CbBb469901fdb5BbfB80d0F0CD1'
+  const logs = await getLogs2({
+    api,
+    factory: FACTORY_SUB_MODULE_PROXY,
+    eventAbi: "event CreateMarket(address indexed creator, address core, address collateralPool, address collateralToken, address underlyingPool, address underlyingToken, bytes32 pairbytes)",
+    fromBlock: 46125548,
+  })
+  logs.forEach(i => {
+    tokensAndOwners.push([i.collateralToken, i.collateralPool])
+    tokensAndOwners.push([i.underlyingToken, i.underlyingPool])
+  })
+  return api.sumTokens({ tokensAndOwners })
 }
-
-module.exports = {
-  avax: {
-    tvl,
-  },
-};
