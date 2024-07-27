458616cd92f8e231b0e0b9c61badde5e6f2d7f37
j-vp <jasper@arcadia.finance>
2024-07-27 13:21:48 +0200

    undo changes in adaptor

--- projects/arcadia-finance-v2/index.js
+++ projects/arcadia-finance-v2/index.js
@@ -42,7 +42,7 @@ async function unwrapArcadiaAeroLP({ api, ownerIds, }) {
       }
     }
   }
-  
+
   const wrappedData = await api.multiCall({ abi: abi.wrappedAeroPositionState, calls: wAERONFTIds, target: wAeroNFT, });
   const stakedData = await api.multiCall({ abi: abi.stakedAeroPositionState, calls: sAERONFTIds, target: sAeroNFT, });
   wrappedData.forEach((data) => api.add(data.pool, data.amountWrapped));
