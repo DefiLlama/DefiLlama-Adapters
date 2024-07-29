9dba2989c6c19890926f1e0e2a18411519b1bb26
Lawson Graham <lawson.graham8@gmail.com>
2024-07-29 01:23:33 -0700

    update thala lsd logic

--- projects/thala-lsd/index.js
+++ projects/thala-lsd/index.js
@@ -1,4 +1,4 @@
-const { aQuery } = require('../helper/chain/aptos')
+const { function_view } = require('../helper/chain/aptos')
 
 // Note: thala-lsd is under the umbrella of Thala protocols
 
@@ -6,13 +6,29 @@ const LSD_ACCOUNT = "0xfaf4e633ae9eb31366c9ca24214231760926576c7b625313b3688b5e9
 
 module.exports = {
   timetravel: false,
-  methodology: "Aggregates thAPT backing Thala LSD's APT delegation.",
+  methodology: "Aggregates APT in delegation pools backing Thala LSD.",
   aptos: {
     tvl: async () => {
-      const { data: { supply } } = await aQuery(`/v1/accounts/${LSD_ACCOUNT}/resource/0x1::coin::CoinInfo%3C${LSD_ACCOUNT}::staking::ThalaAPT%3E`)
+      const delegation_pools = (await function_view({
+        functionStr: `${LSD_ACCOUNT}::pool_router::get_pools`,
+        type_arguments: [],
+        args: []
+      }))[0];
+      let total_apt = 0;
+  
+      for (let i = 0; i < delegation_pools.length; i++) {
+        const [active, inactive, pending_active, pending_inactive] = await function_view({
+          functionStr: `0x1::delegation_pool::get_delegation_pool_stake`,
+          type_arguments: [],
+          args: [delegation_pools[i]]
+        });
+        total_apt += Number(active) + Number(inactive) + Number(pending_active) + Number(pending_inactive);
+      }
+          
       return {
-        aptos: supply.vec[0].integer.vec[0].value / 1e8
+        aptos: total_apt / 1e8
       }
     }
   }
 }
+
