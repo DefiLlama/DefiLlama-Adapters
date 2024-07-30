ae36b01dff50a131d6cd27a9dfd3a22aa664162e
Define101 <93603962+Define101@users.noreply.github.com>
2024-07-30 16:06:13 +0100

    divide etherfi (#11154)

--- projects/ether-fi/index.js
+++ projects/ether-fi/index.js
@@ -1,11 +1,9 @@
-const { staking } = require("../helper/staking");
 const { nullAddress } = require("../helper/unwrapLPs");
 const sdk = require('@defillama/sdk')
 
 module.exports = {
   doublecounted: true,
   ethereum: {
-    staking: staking("0x86B5780b606940Eb59A062aA85a07959518c0161", "0xFe0c30065B384F05761f15d0CC899D4F9F9Cc0eB"),
     tvl: async ({ timestamp }) => {
       const api = new sdk.ChainApi({ timestamp, chain: 'optimism' })
       await api.getBlock()
@@ -14,7 +12,4 @@ module.exports = {
       }
     }
   },
-  arbitrum:{
-    staking: staking("0x86B5780b606940Eb59A062aA85a07959518c0161", "0x7189fb5b6504bbff6a852b13b7b82a3c118fdc27")
-  }
 }
