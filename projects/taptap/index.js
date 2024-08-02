177cfc43b1b7e14c2850b7ea1c36eb878b3cd430
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-02 17:33:09 +0300

    minor fix

--- projects/taptap/index.js
+++ projects/taptap/index.js
@@ -8,6 +8,7 @@ module.exports = {
               "0x36B275207e53273e2EF0a24471FBa0ac8b97D532",
               "0x29263dcedbc27644ccfc51819c6c4bed127859c9"
         ], tokens: [
+            ADDRESSES.null,
             ADDRESSES.taiko.USDC,
             ADDRESSES.taiko.WETH,
         ]}),
