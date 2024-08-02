6f642391a925c19b65f1f1bcac077ce61de96981
Alice0nly <abel@0xcapital.org>
2024-08-02 18:50:21 +0800

    feat: add taptap tvl

---
+++ projects/taptap/index.js
@@ -0,0 +1,15 @@
+const ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokensExport } = require('../helper/unwrapLPs')
+
+module.exports = {
+    taiko: {
+        tvl: sumTokensExport({ owners: 
+            [
+              "0x36B275207e53273e2EF0a24471FBa0ac8b97D532",
+              "0x29263dcedbc27644ccfc51819c6c4bed127859c9"
+        ], tokens: [
+            ADDRESSES.taiko.USDC,
+            ADDRESSES.taiko.WETH,
+        ]}),
+    }
+}; 
