883c0f591ee634f05de68bfce6466dddcc5a4315
dylenfu <dylenfu@126.com>
2024-08-05 10:50:24 +0800

    get tvl on chain

--- projects/Fil-Liquid/index.js
+++ projects/Fil-Liquid/index.js
@@ -1,17 +1,15 @@
-const axios = require("axios");
+const abi = require('./abi.json');
+const { Web3 } = require('web3');
+const web3 = new Web3(`https://api.node.glif.io`);
+const ADDRESSES = require('../helper/coreAssets.json')
+const contract = "0x857b27968f522afA5038F01b1e1f9EdfA3cc631d";
 
-async function getTVL() {
-  const { data } = await axios.get(
-    "https://dapp.filliquid.io/rpc/getTVL"
-  );
-
-  const divisor = BigInt(1e18);
-  const number = BigInt(data.tvl);
-  const integerPart = number / divisor;
-  const fractionalPart = number % divisor;
-  const fractionalString = fractionalPart.toString().padStart(18, "0");
-
-  return { usd: `${integerPart}.${fractionalString}` };
+async function getTVL(api) {
+  var instance = new web3.eth.Contract(abi, contract)
+  var data = await instance.methods.getTVL().call();
+  console.log("minerNum %d, collaterizedMinerNum %d, totalLockedMinerBalance %d, totalFILLiquidity %d, availableFILLiquidity %d", data[0], data[1], data[2], data[3], data[4]);
+  
+  api.add(ADDRESSES.null, data[5]);
 }
 
 module.exports = {
