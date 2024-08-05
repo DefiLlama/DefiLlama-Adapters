cba044dc8ee23cd6c3724f1c03844c5b421e82ed
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-05 12:45:16 +0200

    test fix

--- projects/predx/index.js
+++ projects/predx/index.js
@@ -1,10 +1,10 @@
-const { sumTokensExport } = require('../helper/sumTokens');
+const { sumTokensExport } = require('../helper/sumTokens')
 
 module.exports = {
-    zklink: {
-      tvl: sumTokensExport({
-        owners: ["0x986Ca3A4F05AA7EA5733d81Da6649043f43cB9A8"],
-        tokens: ["0x2F8A25ac62179B31D62D7F80884AE57464699059"]
-      }),
-    },
-};
+  zklink: {
+    tvl: sumTokensExport({
+      owners: ["0x986Ca3A4F05AA7EA5733d81Da6649043f43cB9A8"],
+      tokens: ["0x2F8A25ac62179B31D62D7F80884AE57464699059"]
+    }),
+  },
+}
