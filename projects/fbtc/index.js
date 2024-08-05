a5c84ad286acd071f064662e43a838524b60ecee
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-05 19:50:29 +0200

    update fbtc

--- projects/fbtc/index.js
+++ projects/fbtc/index.js
@@ -1,13 +1,15 @@
-const FBTC = '0xC96dE26018A54D51c097160568752c4E3BD6C364';
-
-async function tvl(api) {
-  return {
-    [FBTC]: await api.call({ target: FBTC, abi: 'erc20:totalSupply' }),
-  };
-}
+const { sumTokensExport } = require('../helper/sumTokens')
 
 module.exports = {
-  ethereum: { tvl },
-  mantle: { tvl },
-  bsc: { tvl },
-};
+  bitcoin: {
+    tvl: sumTokensExport({
+      owners: [
+        // https://docs.fbtc.com/security/bitcoin-reserve-address
+        'bc1q7jgulg69frc8zuzy0ng8d5208kae7t0twyfjwm',
+        'bc1q6c3c0t3zvnphce37ufr4yz9veaqvew2wg0shwr',
+        '3HjNJWcn2ayFLikzmKRaFtcbLufYChQo3T',
+        '374vhN24WryvNWUUZR2uDAnL4oNP5EW4qR',
+      ],
+    }),
+  }
+}
