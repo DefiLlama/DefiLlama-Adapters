fd6e38f5806fc3e8b486231f07dc3d386305c239
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-06 17:39:37 +0300

    minor fix

--- projects/strkfarm/erc4626.js
+++ projects/strkfarm/erc4626.js
@@ -1,42 +1,43 @@
-const ERC4626Abi = [   
-    {
-        "name": "asset",
-        "type": "function",
-        "inputs": [],
-        "outputs": [
-          {
-            "type": "core::starknet::contract_address::ContractAddress"
-          }
-        ],
-        "state_mutability": "view"
-    },
-    {
-        "name": "balanceOf",
-        "type": "function",
-        "inputs": [
-          {
-            "name": "account",
-            "type": "core::starknet::contract_address::ContractAddress"
-          }
-        ],
-        "outputs": [
-          {
-            "type": "core::integer::u256"
-          }
-        ],
-        "state_mutability": "view"
-    },
-    {
-        "name": "total_assets",
-        "type": "function",
-        "inputs": [],
-        "outputs": [
-          {
-            "type": "core::integer::u256"
-          }
-        ],
-        "state_mutability": "view"
-    }
+const ERC4626Abi = [
+  {
+    "name": "asset",
+    "type": "function",
+    "inputs": [],
+    "outputs": [
+      {
+        "type": "core::starknet::contract_address::ContractAddress"
+      }
+    ],
+    "state_mutability": "view"
+  },
+  {
+    "name": "balanceOf",
+    "type": "function",
+    "inputs": [
+      {
+        "name": "account",
+        "type": "core::starknet::contract_address::ContractAddress"
+      }
+    ],
+    "outputs": [
+      {
+        "type": "core::integer::u256"
+      }
+    ],
+    "state_mutability": "view",
+    "customInput": 'address',
+  },
+  {
+    "name": "total_assets",
+    "type": "function",
+    "inputs": [],
+    "outputs": [
+      {
+        "type": "core::integer::u256"
+      }
+    ],
+    "state_mutability": "view"
+  }
 ]
 
 const ERC4626AbiMap = {}
