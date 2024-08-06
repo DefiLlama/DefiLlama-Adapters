fb22a7311fb0013ef6990d67c4d77d402979e722
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-06 16:31:27 +0300

    minor fix

--- projects/balancer-cow-amm/index.js
+++ projects/balancer-cow-amm/index.js
@@ -1,19 +1,19 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const { v1Tvl } = require('../helper/balancer')
+const sdk = require('@defillama/sdk')
 
-module.exports = {
-  ethereum: {
-    tvl: async () => {
-      const tvl1 = await v1Tvl('0xf76c421bAb7df8548604E60deCCcE50477C10462', 20432455)() || 0
-      const tvl2 = await v1Tvl('0x23fcC2166F991B8946D195de53745E1b804C91B7', 20391510)() || 0
-      return tvl1 + tvl2
-    }
-  },
-  xdai: {
-    tvl: async () => {
-      const tvl1 = await v1Tvl('0x703Bd8115E6F21a37BB5Df97f78614ca72Ad7624', 35259725)() || 0
-      const tvl2 = await v1Tvl('0x7573B99BC09c11Dc0427fb9c6662bc603E008304', 35163914)() || 0
-      return tvl1 + tvl2
-    }
-  }
+const config = {
+  ethereum: [
+    ['0xf76c421bAb7df8548604E60deCCcE50477C10462', 20432455],
+    ['0x23fcC2166F991B8946D195de53745E1b804C91B7', 20391510],
+  ],
+  xdai: [
+    ['0x703Bd8115E6F21a37BB5Df97f78614ca72Ad7624', 35259725],
+    ['0x7573B99BC09c11Dc0427fb9c6662bc603E008304', 35163914],
+  ],
 }
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = {
+    tvl: sdk.util.sumChainTvls(config[chain].map(c => v1Tvl(...c)))
+  }
+})
