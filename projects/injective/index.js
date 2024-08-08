448736ecd12f75c17de9ee9b7a400df1d5b1d549
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2024-08-08 19:10:35 +0200

    fix: bridge TVL not counted towards Injective TVL

--- projects/injective/index.js
+++ projects/injective/index.js
@@ -1,6 +1,9 @@
 const { sumTokensExport } = require('../helper/unwrapLPs')
 
+const bridgeTvlWithoutInj = () => sumTokensExport({ owner: '0xf955c57f9ea9dc8781965feae0b6a2ace2bad6f3', fetchCoValentTokens: true, blacklistedTokens: ['0xe28b3B32B6c345A34Ff64674606124Dd5Aceca30'], logCalls: true })
+
 module.exports = {
-  methodology: 'TVL accounts for all liquidity on the Injective chain, using the chain\'s bank module as the source.',
-  ethereum: { tvl: sumTokensExport({ owner: '0xf955c57f9ea9dc8781965feae0b6a2ace2bad6f3', fetchCoValentTokens: true, blacklistedTokens: ['0xe28b3B32B6c345A34Ff64674606124Dd5Aceca30'], logCalls: true }) },
+  methodology: 'Sum of all tokens locked in the Peggy Bridge contract, excluding INJ.',
+  ethereum: { tvl: bridgeTvlWithoutInj() },
+  injective: { tvl: bridgeTvlWithoutInj() },
 };
