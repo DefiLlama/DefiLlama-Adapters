b3878e83c04d83d07a5a4f3c18f372d8139f2512
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-09 11:57:05 +0200

    code refactor

--- projects/pancake-stryke/index.js
+++ projects/pancake-stryke/index.js
@@ -1,17 +1,21 @@
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
-const ADDRESSES = require('../helper/coreAssets.json')
-
-const WETHUSDC = "0x501B03BdB431154b8Df17BF1c00756E3a8F21744"
-const WBTCUSDC = "0x550e7E236912DaA302F7d5D0d6e5D7b6EF191f04"
-const ARBUSDC = "0x4eed3A2b797Bf5630517EcCe2e31C1438A76bb92"
-
-async function tvl(api) {
-  return sumTokens2({ tokens: [nullAddress, ADDRESSES.arbitrum.WETH, ADDRESSES.arbitrum.USDC, ADDRESSES.arbitrum.WBTC, ADDRESSES.arbitrum.ARB], owners: [WETHUSDC, WBTCUSDC, ARBUSDC], api })
-}
-
-module.exports = {
-  methodology: `We count the Tokens on ${WETHUSDC, WBTCUSDC, ARBUSDC}`,
+const config = {
   arbitrum: {
-    tvl
-  }
+    pools: [
+      "0x501B03BdB431154b8Df17BF1c00756E3a8F21744", // WETHUSDC
+      "0x550e7E236912DaA302F7d5D0d6e5D7b6EF191f04", // WBTCUSDC
+      "0x4eed3A2b797Bf5630517EcCe2e31C1438A76bb92", // ARBUSDC
+    ],
+  },
 }
+
+Object.keys(config).forEach(chain => {
+  const { pools } = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const callAssets = await api.multiCall({ abi: 'address:callAsset', calls: pools })
+      const putAssets = await api.multiCall({ abi: 'address:putAsset', calls: pools })
+
+      return api.sumTokens({ tokensAndOwners2: [callAssets.concat(putAssets), pools.concat(pools)]})
+    }
+  }
+})
