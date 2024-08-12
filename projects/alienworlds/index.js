5da892892881d44010c1c83ece3cf1ad3e25f54d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-12 08:58:51 +0200

    minor fix

--- projects/alienworlds/index.js
+++ projects/alienworlds/index.js
@@ -1,12 +1,18 @@
 const { get_account_tvl } = require("../helper/chain/eos");
 
+const accounts = ["nftmt.worlds", "stake.worlds", "lore.worlds", "arena.worlds", "boost.worlds"]
+
 // AlienWorlds
 // https://alienworlds.io/
 async function wax() {
-  const accounts = ["nftmt.worlds", "stake.worlds", "lore.worlds", "arena.worlds", "boost.worlds"];
   const tokens = [
       ["eosio.token", "WAX", "wax"],
-      ["alien.worlds", "TLM", "alien-worlds"]
+  ];
+  return await get_account_tvl(accounts, tokens, "wax");
+}
+async function staking() {
+  const tokens = [
+      ["alien.worlds", "TLM", "alien-worlds"],
   ];
   return await get_account_tvl(accounts, tokens, "wax");
 }
@@ -14,6 +20,7 @@ async function wax() {
 module.exports = {
   methodology: `AlienWorlds TVL is achieved by querying token balances from Staking, Voting & Gaming smart contract(s).`,
   wax: {
-    tvl: wax
+    tvl: wax,
+    staking,
   },
 }
