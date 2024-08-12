ac67227b26f145663c62807e49159de5622e5d9b
Dominik Szopa <dominik@anfata.com>
2024-08-12 11:57:25 +0200

    Refactor TVL calculation for Arrland on Polygon
    - Remove sumTokensExport helper for TVL calculation
    - Set TVL to return 0
    - Update methodology to reflect the simplified TVL calculation

--- projects/arrland/index.js
+++ projects/arrland/index.js
@@ -1,7 +1,6 @@
 
 const { stakings } = require("../helper/staking");
 const { pool2s } = require("../helper/pool2");
-const { sumTokensExport } = require('../helper/unwrapLPs')
 
 const stakingAddresses = ["0x111e34bA90D1dE9a2A57f987b5711C71FA4c0Fa0"];
 const stakingToken = "0x14e5386f47466a463f85d151653e1736c0c50fc3";
@@ -10,9 +9,9 @@ const lpTokens = ["0x6495b61b8088a0a82de737ffb142136119b016e6", "0x464b7eb1c66d6
 
 module.exports = {
   polygon: {
-    tvl: sumTokensExport({ owners: ['0xEAf6E84B4E4C2Ce70a80fd781B6A7f177E7b60F5', '0xb94380939A15C574FE22B04FC95Ec5CCaAD783b8'], tokens: ['0x14e5386f47466a463f85d151653e1736c0c50fc3'] }),
+    tvl: () => 0,
     staking: stakings(stakingAddresses, stakingToken, "polygon"),
     pool2: pool2s(pool2StakingAddresses, lpTokens, "polygon")
   },
-  methodology: "Counts total value locked as $RUM tokens locked in LP staking, the $RUM token staking, LP token staking",
+  methodology: "Counts total value locked as the $RUM token staking, LP token staking",
 }
