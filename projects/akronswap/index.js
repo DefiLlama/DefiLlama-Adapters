b2d9695c9522d0b1b7272c635fd059757c7bb23a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-12 08:49:22 +0200

    minor fix

--- projects/akronswap/index.js
+++ projects/akronswap/index.js
@@ -1,17 +1,11 @@
 const { getUniTVL } = require('../helper/unknownTokens');
-  
+
+const tvl = getUniTVL({ factory: '0xAf39606bec181887951Ab6912Ac7EA216Bd6E4B4', useDefaultCoreAssets: true, fetchBalances: true, })
+
 module.exports = {
   misrepresentedTokens: true,
-  ethereum:{
-    tvl: getUniTVL({ factory: '0xAf39606bec181887951Ab6912Ac7EA216Bd6E4B4', useDefaultCoreAssets: true, fetchBalances: true, }),
-  },
-  arbitrum:{
-    tvl: getUniTVL({ factory: '0xAf39606bec181887951Ab6912Ac7EA216Bd6E4B4', useDefaultCoreAssets: true, fetchBalances: true, }),
-  },
-  base:{
-    tvl: getUniTVL({ factory: '0xAf39606bec181887951Ab6912Ac7EA216Bd6E4B4', useDefaultCoreAssets: true, fetchBalances: true, }),
-  },
-  bsc:{
-    tvl: getUniTVL({ factory: '0xAf39606bec181887951Ab6912Ac7EA216Bd6E4B4', useDefaultCoreAssets: true, fetchBalances: true, }),
-  },      
+  ethereum: { tvl, },
+  arbitrum: { tvl, },
+  base: { tvl, },
+  bsc: { tvl, },
 }
