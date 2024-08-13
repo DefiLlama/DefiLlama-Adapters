4e54f34d2e664043fc1a34e90fbbb1024fac5272
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-13 09:15:54 +0200

    fix maha treasury

--- projects/treasury/mahaxyz.js
+++ projects/treasury/mahaxyz.js
@@ -7,6 +7,10 @@ module.exports = treasuryExports({
     owners: [treasury],
     ownTokens: [
       "0x745407c86DF8DB893011912d3aB28e68B62E49B0", // MAHA
-    ]
+    ],
+    blacklistedTokens: [
+      "0xB4d930279552397bbA2ee473229f89Ec245bc365", // MAHA
+      "0x6b7127a638eDC7Db04bEde220c7c49930fdB4160" // MAHAETH
+    ],
   },
 })
