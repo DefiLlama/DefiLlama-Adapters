d57b6e3c23f0f6e276105fb564a358481ca9f97c
Sakai Vault <hi@paxe.io>
2024-08-14 20:57:19 +0300

    paxe tvl adapters update

--- projects/paxe/index.js
+++ projects/paxe/index.js
@@ -1,22 +1,29 @@
 const { staking } = require("../helper/staking")
 
 
-const PAXE = "0xbA576f5ecbA5182a20f010089107dFb00502241f"
-const SAKAI = "0x43b35e89d15b91162dea1c51133c4c93bdd1c4af"
+const PAXE_TOKEN = "0xd2A3eec06719D5Ac66248003B5488E02165dd2fa"
 const PAXE_FARMING_CONTRACT = '0xbA576f5ecbA5182a20f010089107dFb00502241f';
 const RESTAKING_POOL = '0x269e1ceb128ccCD5684BbAFF9906D69eD1e9e9C8';
+async function tvl(api) {
+  const paxeBalanceinFarm = await api.call({
+    abi: 'erc20:balanceOf',
+    target: PAXE_TOKEN,
+    params: [PAXE_FARMING_CONTRACT],
+  });
+      const paxeBalanceinRestake = await api.call({
+    abi: 'erc20:balanceOf',
+    target: PAXE_TOKEN,
+    params: [RESTAKING_POOL],
+  });
 
-const TOKEN_CONTRACTS = [
-     PAXE,SAKAI
-]
+      api.add(PAXE_TOKEN, paxeBalanceinFarm)
+      api.add(PAXE_TOKEN, paxeBalanceinRestake)
+}
 
 module.exports = {
+      methodology: 'We count the TVL on the PAXE token in the farming contract and the restaking pool',
+    
   bsc: {
-             tvl: sumTokensExport({ 
-          owner: PAXE_FARMING_CONTRACT , 
-                   restake: RESTAKING_POOL,
-          
-        }),
-            
-  },
-}
+    tvl,
+  }
+}; 
