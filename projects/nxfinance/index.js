58549eb20ba5461a8843178b4b0684f064275de4
LidoKing <55234791+LidoKing@users.noreply.github.com>
2024-08-15 18:20:32 +0800

    add tvl from v2 program (#11302)

--- projects/nxfinance/index.js
+++ projects/nxfinance/index.js
@@ -1,16 +1,25 @@
 const { Program } = require("@project-serum/anchor");
 const { getProvider, } = require("../helper/solana");
-const nxIdl = require("./nx-idl.json");
+const nxIdlV1 = require("./nx-idl-v1.json");
+const nxIdlV2 = require("./nx-idl-v2.json");
 
-const NX_PROGRAM_ADDR = "EHBN9YKtMmrZhj8JZqyBQRGqyyeHw5xUB1Q5eAHszuMt";
+const NX_PROGRAM_ADDR_V1 = "EHBN9YKtMmrZhj8JZqyBQRGqyyeHw5xUB1Q5eAHszuMt";
+const NX_PROGRAM_ADDR_V2 = "NXFiKimQN3QSL3CDhCXddyVmLfrai8HK36bHKaAzK7g";
 
 async function tvl(api) {
 	const provider = getProvider();
-	const nx_program = new Program(nxIdl, NX_PROGRAM_ADDR, provider);
-	const accounts = await nx_program.account.marginPool.all()
 
-	for (let { account: pool } of accounts)
-		api.add(pool.tokenMint.toBase58(), pool.depositTokens.toString())
+	const nx_program_v1 = new Program(nxIdlV1, NX_PROGRAM_ADDR_V1, provider)
+	const nx_program_v2 = new Program(nxIdlV2, NX_PROGRAM_ADDR_V2, provider)
+	const res = await Promise.all([
+		nx_program_v1.account.marginPool.all(),
+		nx_program_v2.account.collateralPool.all(),
+		nx_program_v2.account.lendingPool.all()
+	])
+
+	for (let value of res)
+		for (let { account: pool } of value) 
+			api.add(pool.tokenMint.toBase58(), pool.depositTokens.toString())
 }
 
 module.exports = {
