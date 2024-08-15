dcd1cf3747d0209762a84210a5985013a19111ec
T <42140277+ThienLK1@users.noreply.github.com>
2024-08-15 16:03:57 +0700

    Update index.js

--- projects/halotrade/index.js
+++ projects/halotrade/index.js
@@ -8,7 +8,7 @@ module.exports = {
     aura: {
         tvl: getFactoryTvl("aura18qwll06qjgkfl5s5ym4rtpz8jy2tl849ghgx402tm4w9v55wu5asn0mqhv")
     },
-    evm: {
+    ethereum: {
         tvl: getUniTVL({ factory: '0xa51DDB350e768052B7e44e1087D08fe2DD32b1df', useDefaultCoreAssets: true }),
     },
 } // node test.js projects/hallotrade/index.js
