753dad0ee7c5339dbe8734b13778d1421ce1379b
Pavel Shuvalov <pavel.s@ton.org>
2024-08-15 15:36:25 +0400

    Add SCALE staking for DeDust

--- projects/dedust/index.js
+++ projects/dedust/index.js
@@ -1,5 +1,9 @@
 const { get } = require('../helper/http')
 const { transformDexBalances } = require('../helper/portedTokens')
+const ADDRESSES = require("../helper/coreAssets.json");
+
+
+const SCALE_STAKING_ADDRESS = 'EQBNZB91JJX9Ub7KMEAUoQNVcQlCsob5e_WMFbvsML_UoAKD'
 
 module.exports = {
   misrepresentedTokens: true,
@@ -17,6 +21,15 @@ module.exports = {
           token1Bal: i.right_token_reserve,
         }))
       })
+    },
+    staking: async (api) => {
+      const get_staking_data = await get(`https://tonapi.io/v2/blockchain/accounts/${SCALE_STAKING_ADDRESS}/methods/get_staking_data`)
+      if (!get_staking_data.success) {
+        throw new Error(`Failed to get staking data: ${get_staking_data}`)
+      }
+      const price_response = await get(`https://tonapi.io/v2/rates?tokens=${ADDRESSES.ton.SCALE}&currencies=ton`)
+      const scale_price = price_response.rates[ADDRESSES.ton.SCALE].prices.TON
+      return api.add(ADDRESSES.ton.TON, parseInt(get_staking_data.stack[3].num) * scale_price)
     }
   }
 }
