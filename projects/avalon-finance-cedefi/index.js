92934039be0188e92adf3db36b3ce44a58bde588
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-15 16:32:38 +0200

    code refactor

--- projects/avalon-finance-cedefi/index.js
+++ projects/avalon-finance-cedefi/index.js
@@ -1,42 +1,31 @@
-// Defillama Adapter for Avalon Finance CeDeFi Market
-const { fetchMarketData } = require('./markets');
-const { avalonCeDefi, lfbtc } = require("./addresses");
-const coreAssets  = require('../helper/coreAssets.json');
+const ADDRESSES = require('../helper/coreAssets.json')
+
+const abi = {
+  "getPoolManagerReserveInformation": "function getPoolManagerReserveInformation() view returns (tuple(uint256 userAmount, uint256 collateral, uint256 debt, uint256 claimableUSDT, uint256 claimableBTC) poolManagerReserveInfor)"
+}
+
+const config = {
+  ethereum: { poolAddress: "0x02feDCff97942fe28e8936Cdc3D7A480fdD248f0", lfbtcAddress: "0x3119a1AD5B63A000aB9CA3F2470611eB997B93B9", usdtAddress: ADDRESSES.ethereum.USDT, },
+}
 
 // @dev getMetrics: call to get the collateral and debt of the Avalon CeDefi pool contract.
-const getMetrics = async (poolAddress, lfbtcAddress, usdtAddress, api, borrowed) => {
-    try {
-        const marketData = await fetchMarketData(poolAddress, api);
-        const balanceOfCollateral = marketData.collateral;
-        const balanceOfDebt = marketData.debt;
+const getMetrics = async (api, borrowed) => {
+  const { poolAddress, lfbtcAddress, usdtAddress } = config[api.chain]
+  const marketData = await api.call({ abi: abi.getPoolManagerReserveInformation, target: poolAddress, });
+  const balanceOfCollateral = marketData.collateral;
+  const balanceOfDebt = marketData.debt;
 
-        api.add(lfbtcAddress, balanceOfCollateral);
-        if (borrowed) {
-            api.add(usdtAddress, balanceOfDebt);
-        }
-    } catch (error) {
-        console.error("Error in getMetrics:", error);
-        throw error;
-    }
-};
-
-const ethereum = function (borrowed) {
-    const poolAddress = avalonCeDefi.ethereum
-    const lfbtcAddress = lfbtc.ethereum
-    const usdtAddress = coreAssets.ethereum.USDT
-    return async (api) => {
-        return getMetrics(poolAddress, lfbtcAddress, usdtAddress, api, borrowed);
-    }
-};
+  if (borrowed)
+    api.add(usdtAddress, balanceOfDebt);
+  else
+    api.add(lfbtcAddress, balanceOfCollateral);
+}
 
 module.exports = {
-    methodology: `lfbtc collateral and USDT debt of Avalon CeDefi pool contract`,
-    doublecounted: false,
-    ethereum: {
-        tvl: ethereum(false),
-        borrowed: ethereum(true),
-    }
-};
-
-// export LLAMA_DEBUG_MODE="true"
-// node test.js projects/avalon-finance-cedefi/index.js
+  methodology: `lfbtc collateral and USDT debt of Avalon CeDefi pool contract`,
+  doublecounted: false,
+  ethereum: {
+    tvl: (api) => getMetrics(api),
+    borrowed: (api) => getMetrics(api, true),
+  }
+}
