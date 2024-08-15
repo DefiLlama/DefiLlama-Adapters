3f023588438084382a5da8f912e81cee4775924d
0xngmi <0xngmi@protonmail.com>
2024-08-15 09:26:23 +0100

    add error for >1bn

--- projects/demeter/index.js
+++ projects/demeter/index.js
@@ -2,6 +2,9 @@ const { get } = require('../helper/http')
 
 async function fetch() {
   let results = await get('https://api.deotoken.com/api/demeter/supply-data');
+  if(results.tvl > 1e9){
+    throw new Error(">1bn")
+  }
   return results.tvl;
 }
 
