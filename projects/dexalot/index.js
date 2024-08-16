0c56b81ed20e04ac842e09ce15b19bef6391f53b
Jaimi Patel <jaimip34@gmail.com>
2024-08-16 15:14:56 +0100

    refactor tvl via sumTokens2 for avax,arb+base to helper file

--- projects/dexalot/index.js
+++ projects/dexalot/index.js
@@ -1,18 +1,9 @@
-const { sumTokens2 } = require('../helper/unwrapLPs')
 const { get } = require("../helper/http");
+const { exportDexalotTVL } = require("../helper/dexalot");
 
 const dexalotAPI = "https://api.dexalot.com"
 
-function getTVL(env) {
-  return async (api) => {
-    const mainnetRFQs = await get(`${dexalotAPI}/privapi/trading/deployment?contracttype=MainnetRFQ&env=${env}`)
-    const allTokens = await get(`${dexalotAPI}/privapi/trading/tokens`)
-    const tokens = allTokens.filter((t) => !t.isvirtual && t.env === env).map((t) => t.address)
-    return sumTokens2({ api, owner: mainnetRFQs[0].address, tokens })
-  }
-}
-
-async function getDexalotTVL(_) {
+async function getDexalotChainLockedOrders(_) {
   const lockedTokens = await get(`${dexalotAPI}/api/stats/orderstvl`)
   const res = {}
   for (const {coingecko_id, amount} of lockedTokens) {
@@ -27,16 +18,8 @@ async function getDexalotTVL(_) {
 
 module.exports = {
   methodology: "Dexalot TVL is comprised of the token balances locked in the MainnetRFQ (swap) contracts and the value locked in open orders on the Dexalot chain.",
-  arbitrum: {
-    tvl: getTVL("production-multi-arb"),
-  },
-  avax: {
-    tvl: getTVL("production-multi-avax"),
-  },
-  base: {
-    tvl: getTVL("production-multi-base"),
-  },
   dexalot: {
-    tvl: getDexalotTVL,
+    tvl: getDexalotChainLockedOrders,
   },
+  ...exportDexalotTVL("MainnetRFQ")
 }
