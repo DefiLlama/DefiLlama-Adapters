a7fb8223a55246ce978b03f09bdfd5b8de7328fe
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-16 09:28:27 +0200

    code refactor

--- projects/clave/index.js
+++ projects/clave/index.js
@@ -1,16 +1,12 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const ZtakeV1Address = "0x9248F1Ee8cBD029F3D22A92EB270333a39846fB2"
 
 async function tvl(api) {
-  const totalSupply = await api.call({ abi: 'uint256:totalSupply', target: ZtakeV1Address });
-
-  api.add(ADDRESSES.era.ZK, totalSupply)
+  const ZK = await api.call({  abi: 'address:ZK', target: ZtakeV1Address})
+  return api.sumTokens({ owner: ZtakeV1Address, tokens: [ZK] })
 }
 
-
 module.exports = {
-  methodology: 'Calls the totalSupply function from the staking contract',
   era: {
     tvl,
   }
-};
+}
