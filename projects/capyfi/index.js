d03fa4db3a24202adc767ff6063d125d87c4b56c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-16 21:18:18 +0200

    code refactor

--- projects/capyfi/index.js
+++ projects/capyfi/index.js
@@ -1,22 +1,6 @@
-const { usdCompoundExports } = require('../helper/compound')
-
-const capyfiConfig = {
-  comptroller: '0x123Abe3A273FDBCeC7fc0EBedc05AaeF4eE63060',
-  chain: 'lac',
-  nativeTokenMarket: '0x465ebFCeB3953e2922B686F2B4006173664D16cE',
-  options: { blacklist: ['0x25f38518dc45f6b20f716e508b8f468c97b2d4a9'] } 
-}
-
-const capyfiTVL = usdCompoundExports(
-    capyfiConfig.comptroller,
-    capyfiConfig.chain,
-    capyfiConfig.nativeTokenMarket,
-    undefined,
-    capyfiConfig.options  
-)
-
+const { compoundExports2, methodology } = require('../helper/compound')
 
 module.exports = {
-  methodology: 'The TVL is calculated by adding the value of total deposits in all markets minus the total borrows.',
-  lac: { ...capyfiTVL }
+  methodology,
+  lac: compoundExports2({ comptroller: '0x123Abe3A273FDBCeC7fc0EBedc05AaeF4eE63060', cether: '0x465ebfceb3953e2922b686f2b4006173664d16ce' })
 }
--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -92,6 +92,12 @@ const fixBalancesTokens = {
   base: {
     "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2": { coingeckoId: "tether", decimals: 6 },
   },
+  lac: {
+    [ADDRESSES.null]: { coingeckoId: "la-coin", decimals: 18 },
+    '0x2911a1AB18546cb501628Be8625C7503a2A7DB54': { coingeckoId: "la-coin", decimals: 18 },
+    '0x42c8c9c0f0a98720dacdaeac0c319cb272b00d7e': { coingeckoId: "ethereum", decimals: 18 },
+    '0xf54b8cb8eeee3823a55dddf5540ceaddf9724626': { coingeckoId: "wrapped-bitcoin", decimals: 8 },
+  }
 }
 
 ibcChains.forEach(chain => fixBalancesTokens[chain] = { ...ibcMappings, ...(fixBalancesTokens[chain] || {}) })
