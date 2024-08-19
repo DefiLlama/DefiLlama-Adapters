f8ced819ebdc1b5931ecbb6b7f3484e6a9fda42c
johnnyonline <johnnyonline.eth@gmail.com>
2024-08-19 13:04:53 +0300

    feat: add ynLSDe

--- projects/yieldnest/index.js
+++ projects/yieldnest/index.js
@@ -1,13 +1,18 @@
 
 const ADDRESSES = require('../helper/coreAssets.json')
 
-const token = '0x09db87a538bd693e9d08544577d5ccfaa6373a48'
+const YN_ETH = '0x09db87a538bd693e9d08544577d5ccfaa6373a48'
+const YN_LSDE = '0x35Ec69A77B79c255e5d47D5A3BdbEFEfE342630c'
 
 module.exports = {
   ethereum: {
     tvl: async (api) => {
-      const ethBalance = await api.call({  abi: 'uint256:totalAssets', target: token })
-      api.add(ADDRESSES.null, ethBalance)
+      const ynethBalance = await api.call({  abi: 'uint256:totalAssets', target: YN_ETH })
+      api.add(ADDRESSES.null, ynethBalance)
+
+      const ynlsdeBalance = await api.call({  abi: 'uint256:totalAssets', target: YN_LSDE })
+      api.add(ADDRESSES.null, ynlsdeBalance)
+
       return api.getBalances()
     }
   },
