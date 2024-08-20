78d6a0ca0b303fc04e58bac789d36a18b8ce1318
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-20 15:06:53 +0200

    cronos zk: add multicall

--- projects/h2-finance/index.js
+++ projects/h2-finance/index.js
@@ -1,12 +1,3 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports={
-    misrepresentedTokens: true,
-    methodology: "Factory address (0x50704Ac00064be03CEEd817f41E0Aa61F52ef4DC) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
-    cronos_zkevm: {
-        tvl: getUniTVL({
-            factory: '0x50704Ac00064be03CEEd817f41E0Aa61F52ef4DC',
-            useDefaultCoreAssets: true,
-        })
-    }
-}
+module.exports= uniTvlExport('cronos_zkevm', '0x50704Ac00064be03CEEd817f41E0Aa61F52ef4DC')
