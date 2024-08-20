6009f6785ad43470837519f656e8795efce240f6
Aliaksei <milutkin.work@gmail.com>
2024-08-21 00:24:04 +0300

    update tvl

--- projects/blastup/index.js
+++ projects/blastup/index.js
@@ -1,13 +1,67 @@
-const utils = require("../helper/utils");
 const { stakings } = require("../helper/staking");
-const { toUSDTBalances } = require("../helper/balances");
+const { transformBalances } = require("../helper/portedTokens");
+
+const sdk = require("@defillama/sdk");
+const ADDRESSES = require("../helper/coreAssets.json");
+const { sumTokensAndLPsSharedOwners } = require("../helper/unwrapLPs");
 
 const stakingpool = "0x0E84461a00C661A18e00Cab8888d146FDe10Da8D";
 
-const ADDRESSES = require("../helper/coreAssets.json");
+const blastup = "0x59c159e5a4f4d1c86f7abdc94b7907b7473477f6";
+const lockedBlastup = "0xf8a5d147a3a3416ab151758d969eff15c27ab743";
+
+async function stakingBlastUpTvl(_, block, _cb, { api }) {
+  const balances = {};
+
+  const stakingContracts = [
+    "0x115ebda9489cf250ff0e8ea9f473c96c222a874c", // LockedBlastUPStaking 6%
+    "0x2e36e05e7ecd36164ada93752a9a82c1efaa9582", // LockedBlastUPStaking 12%
+    "0xf399110e921d25dd1ad6a0eef020991df3dd0cd3", // LockedBlastUPStaking 18%
+    "0xb0d7902685a4f4d916a21a0ed721298d590cd9cd", // BlastUPStaking 6%
+    "0x520bf8e72f9e808102eb421fb03764624d1984e9", // BlastUPStaking 12%
+    "0xc3524c6fdce9e60c1a1ddce54953973264097542", // BlastUPStaking 18%
+  ];
+
+  const tokens = [
+    lockedBlastup,
+    lockedBlastup,
+    lockedBlastup,
+    blastup,
+    blastup,
+    blastup,
+  ];
+
+  await sumTokensAndLPsSharedOwners(
+    balances,
+    tokens.map((t) => [t, false]),
+    stakingContracts,
+    block,
+    "blast",
+    () => blastup
+  );
+
+  return transformBalances("blast", balances);
+}
+
+async function tvl(...args) {
+  const blastUpStakingBalances = await stakingBlastUpTvl(...args);
+  const stakingBalances = await stakings(
+    [stakingpool],
+    [ADDRESSES.blast.USDB, ADDRESSES.blast.WETH]
+  )(...args);
+
+  sdk.util.mergeBalances(stakingBalances, blastUpStakingBalances);
+
+  return stakingBalances;
+}
 
 module.exports = {
   blast: {
-    tvl: stakings([stakingpool], [ADDRESSES.blast.USDB, ADDRESSES.blast.WETH]),
+    tvl,
   },
+  hallmarks: [
+    [1717575654, "IDO Farming Launch"],
+    [1718871654, "Staking LockedBlastUP Launch"],
+    [1724142054, "Staking BlastUP Launch"],
+  ],
 };
