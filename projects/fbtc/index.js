be18b21a597b092f6d13acea40ef661950d4c3b5
lyfmantle <161433628+lyfmantle@users.noreply.github.com>
2024-08-21 18:55:12 +0800

    feat: update fbtc static addresses (#11360)

--- projects/fbtc/index.js
+++ projects/fbtc/index.js
@@ -2,8 +2,13 @@ const { sumTokens } = require('../helper/chain/bitcoin')
 const sdk = require('@defillama/sdk')
 
 const staticAddresses = [
-  // https://docs.fbtc.com/security/bitcoin-reserve-address
   'bc1qr5nz7n8ulcdz0w3s6fska80fawxhvqlh273qypm3rkjequ9wpmhs65ppw7',
+  'bc1qhu98nf6ddz6ja73rn72encdr8ezsyhexwpdzap0vcs7lg2wpmrnq5ygfsl',
+  'bc1qg6asmzjr7nr5f5upg3xqyrdxl2tq8ef58hha7t0s82mzzx6zjxesyccp4h',
+  'bc1qxe3md4lehg8gmrlx3e8xqju5mytt266l4hcy8khl6tm5mahghmeqtxlgqq',
+  'bc1qy48h0kuv0r3e330wjfs6r74sk49pkzumnm907t5mhqjxml22r3ss2ucuxc',
+  '32DgQPVHSV6FSxLnw68nggvchp3ZNKquxA',
+  // https://docs.fbtc.com/security/bitcoin-reserve-address
   'bc1q7jgulg69frc8zuzy0ng8d5208kae7t0twyfjwm',
   'bc1q6c3c0t3zvnphce37ufr4yz9veaqvew2wg0shwr',
   '3HjNJWcn2ayFLikzmKRaFtcbLufYChQo3T',
