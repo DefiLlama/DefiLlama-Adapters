d97d878f160f6686e0232d38afecbe83c6ec16b8
Define101 <93603962+Define101@users.noreply.github.com>
2024-08-21 13:15:14 +0100

    add new wallet pstake btc (#11368)

--- projects/pstake-btc/index.js
+++ projects/pstake-btc/index.js
@@ -1,7 +1,7 @@
 const { sumTokensExport } = require("../helper/sumTokens");
 const sdk = require("@defillama/sdk");
 
-const owner = "bc1qajcp935tuvqakut95f0sc9qm09hxjj6egexl9d";
+const owner = ["bc1qajcp935tuvqakut95f0sc9qm09hxjj6egexl9d", "bc1pzq0ve6e7j6jt4ckx8uzdjyddrfda9ew8dxvrjmkxmfnj9yz68zeqgqh9cl"];
 
 module.exports = {
   methodology: `Total amount of BTC in ${owner}.`,
