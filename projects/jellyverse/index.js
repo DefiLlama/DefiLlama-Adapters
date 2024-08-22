d63ae7d28317fa17c2895b9a7d3b823848e36320
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-22 13:55:10 +0200

    fix jellyverse

--- projects/jellyverse/index.js
+++ projects/jellyverse/index.js
@@ -1,4 +1,3 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const { cachedGraphQuery } = require('../helper/cache')
 
 module.exports = {
@@ -8,15 +7,24 @@ module.exports = {
 }
 
 async function tvl(api) {
-  let { tokens } = await cachedGraphQuery('jellyverse', 'https://graph.mainnet.jellyverse.org/subgraphs/name/jelly/verse', `{
-  tokens {
-    address
-    symbol
-    pool { id }
+  let { balancers } = await cachedGraphQuery('jellyverse', 'https://graph.mainnet.jellyverse.org/subgraphs/name/jelly/verse', `{
+  balancers {
+    pools {
+      address
+      tokens {
+        symbol
+        address
+      }
+    }
   }
 }`)
   const vault = "0xFB43069f6d0473B85686a85F4Ce4Fc1FD8F00875"
-  tokens = tokens.filter(t => !t.pool).map(t => t.address)
-  tokens.push(ADDRESSES.sei.WSEI)
-  return api.sumTokens({ owner: vault, tokens })
+  const blacklistedTokens = []
+  const tokens = balancers.map(i => {
+    blacklistedTokens.push(...i.pools.map(j => j.address))
+    return i.pools.map(j => {
+      return j.tokens.map(k => k.address)
+    }).flat()
+  }).flat()
+  return api.sumTokens({ owner: vault, tokens, blacklistedTokens, })
 }
