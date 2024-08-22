4a448d711b46f481a619247abf902a7cd2b3f9c4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-22 13:35:57 +0200

    bugfix xrpl

--- projects/xrpl-dex/index.js
+++ projects/xrpl-dex/index.js
@@ -14,13 +14,14 @@ function getTimeNow() {
   return Math.floor(Date.now() / 1000);
 }
 
-async function tvl() {
+async function tvl(api) {
   const timeNow = getTimeNow()
   const aDayInSeconds = 60 * 60 * 24;
   const projectKey = 'xrpl-dex'
   const cacheKey = 'cache'
   let { lastDataUpdate, tvl } = await getCache(projectKey, cacheKey)
-  if (!lastDataUpdate || timeNow - lastDataUpdate > aDayInSeconds || !Object.keys(tvl).length) 
+  const val = tvl?.XRP
+  if (!lastDataUpdate || timeNow - lastDataUpdate > aDayInSeconds || !val) 
     throw new Error("stale/missing tvl data");
-  return tvl
+  api.addCGToken('ripple', val/1e6)
 }
