ba97ab44d7ea91e8d716c4980e51e8d1f6f6ebc3
turingczz <920886811@163.com>
2024-08-23 11:58:16 +0800

    feat: mod bitmap-game index.js

--- projects/bitmap-game/index.js
+++ projects/bitmap-game/index.js
@@ -1,12 +1,14 @@
-const { staking } = require('../helper/staking')
-const STAKE_MERL_CONTRACT = '0xb311c4b8091aff30Bb928b17Cc59Ce5D8775b13A'
-const MERL_TOKEN = '0x5c46bFF4B38dc1EAE09C5BAc65872a1D8bc87378'
-const STAKE_BITMAP_TOKEN_CONTRACT = '0x8567bD39b8870990a2cA14Df3102a00A7d72f7E3'
-const BITMAP_TOKEN = '0x7b0400231Cddf8a7ACa78D8c0483890cd0c6fFD6'
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
 module.exports = {
   merlin: {
-    tvl: staking(STAKE_MERL_CONTRACT, MERL_TOKEN),
-    staking: staking([STAKE_BITMAP_TOKEN_CONTRACT, STAKE_MERL_CONTRACT,], BITMAP_TOKEN),
+    tvl: sumTokensExport({
+      tokensAndOwners: [
+        // [tokenAddress, ownerContractAddress]
+        ['0x5c46bFF4B38dc1EAE09C5BAc65872a1D8bc87378', '0xb311c4b8091aff30Bb928b17Cc59Ce5D8775b13A'], //merl, stake
+        ['0x7b0400231Cddf8a7ACa78D8c0483890cd0c6fFD6', '0x8567bD39b8870990a2cA14Df3102a00A7d72f7E3'], //bitmap, stake 1
+        ['0x7b0400231Cddf8a7ACa78D8c0483890cd0c6fFD6', '0xb311c4b8091aff30Bb928b17Cc59Ce5D8775b13A'], //bitmap, stake 2
+      ],
+    }),
   }
-}
+};
