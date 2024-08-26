c0143601665e12ff136cd5bd16345c5189ad65dd
Jason <feiyingxyz@qq.com>
2024-08-26 09:26:28 +0800

    update calculating method

--- projects/pumpbtc/index.js
+++ projects/pumpbtc/index.js
@@ -4,14 +4,19 @@ module.exports = {
   methodology: 'TVL for pumpBTC is calculated based on the total amount of pumpBTC.',
 }
 const config = {
-  ethereum: { owners: [nullAddress], tokens: ['0xf469fbd2abcd6b9de8e169d128226c0fc90a012e'], },
-  bsc: { owners: [nullAddress], tokens: ['0xf9C4FF105803A77eCB5DAE300871Ad76c2794fa4'], },
-  mantle: { owners: [nullAddress], tokens: ['0xC75D7767F2EdFbc6a5b18Fc1fA5d51ffB57c2B37'], },
+  ethereum: { token: '0xf469fbd2abcd6b9de8e169d128226c0fc90a012e' },
+  bsc: { token: '0xf9C4FF105803A77eCB5DAE300871Ad76c2794fa4', },
+  mantle: { token: '0xC75D7767F2EdFbc6a5b18Fc1fA5d51ffB57c2B37', },
 }
 
 Object.keys(config).forEach(chain => {
-  const { owners, tokens, } = config[chain]
+  const { token, } = config[chain]
   module.exports[chain] = {
-    tvl: async (api) => api.sumTokens({ owners, tokens })
+    tvl: async (api) => {
+      const supply = await api.call({  abi: 'erc20:totalSupply', target: token })
+      //console.log('>>s', tokens[0], supply)
+      //return supply
+      api.add(token, supply)
+    }
   }
 })
