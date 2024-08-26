e4d2f33279a3b7bbaecb95298652cb52790f1390
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-26 10:59:23 +0200

    update euler-v2

--- projects/euler-v2/index.js
+++ projects/euler-v2/index.js
@@ -1,27 +1,17 @@
-const { getLogs2 } = require('../helper/cache/getLogs')
-
 module.exports = {
   methodology: `TVL is supply balance minus borrows the euler contract.`,
 }
 
 const config = {
-  ethereum: { factory: '0x29a56a1b8214D9Cf7c5561811750D5cBDb45CC8e', fromBlock: 20529225 }
+  ethereum: { factory: '0x29a56a1b8214D9Cf7c5561811750D5cBDb45CC8e', }
 }
 
 Object.keys(config).forEach(chain => {
-  const { factory, fromBlock, } = config[chain]
+  const { factory, } = config[chain]
 
   async function getVaults(api) {
-    const logs = await getLogs2({
-      api,
-      factory,
-      eventAbi: 'event ProxyCreated(address indexed proxy, bool upgradeable, address implementation, bytes trailingData)',
-      fromBlock,
-    })
-
-    const vaults = logs.map(log => log.proxy)
-    const tokens = await api.multiCall({  abi: 'address:asset', calls: vaults })
-
+    const vaults = await api.fetchList({ lengthAbi: 'getProxyListLength', itemAbi: 'proxyList', target: factory })
+    const tokens = await api.multiCall({ abi: 'address:asset', calls: vaults })
     return { vaults, tokens }
   }
 
@@ -32,7 +22,7 @@ Object.keys(config).forEach(chain => {
     },
     borrowed: async (api) => {
       const { vaults, tokens } = await getVaults(api)
-      const borrows = await api.multiCall({  abi: 'uint256:totalBorrows', calls: vaults})
+      const borrows = await api.multiCall({ abi: 'uint256:totalBorrows', calls: vaults })
       api.add(tokens, borrows)
     },
   }
