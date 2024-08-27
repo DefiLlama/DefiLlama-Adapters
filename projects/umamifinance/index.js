0f33ee2da2730c1f736586fbbde62d37207a041a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-27 19:06:13 +0200

    Update index.js

--- projects/umamifinance/index.js
+++ projects/umamifinance/index.js
@@ -1,12 +1,16 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const abi = require("./abi.json");
+const { stakings } = require("../helper/staking");
+const { sumERC4626VaultsExport } = require("../helper/erc4626");
 
-// glpUSDC vault is now deprecated
-const glpUSDC = "0x2e2153fd13459eba1f277ab9acd624f045d676ce";
-const USDC = ADDRESSES.arbitrum.USDC;
+const UMAMI = "0x1622bf67e6e5747b81866fe0b85178a93c7f86e3";
+// UMAMI staking for protocol revenue in WETH
+const mUMAMI = "0x2adabd6e8ce3e82f52d9998a7f64a90d294a92a4";
+// UMAMI staking from when it was still ohm fork with rebasing mechanics.
+// There's still some staked tokens that are yet not unstaked and migrated.
+const OHM_STAKING_sUMAMI = "0xc9ecFeF2fac1E38b951B8C5f59294a8366Dfbd81";
 
 // glp vaults are now deprecated as well
 const glpVaults = [
+  "0x2e2153fd13459eba1f277ab9acd624f045d676ce", 
   "0x727eD4eF04bB2a96Ec77e44C1a91dbB01B605e42",
   "0xbb84D79159D6bBE1DE148Dc82640CaA677e06126",
   "0x6a89FaF99587a12E6bB0351F2fA9006c6Cd12257",
@@ -30,22 +34,10 @@ module.exports = {
   doublecounted: true,
   start: 1657027865, // UMAMI deployment block ts
   arbitrum: {
-    tvl: async (api) => {
-
-      const totalAssets = await api.call({ abi: abi.totalAssets, target: glpUSDC, });
-      api.add(USDC, totalAssets);
-
-      const assets = await api.multiCall({ abi: 'address:asset', calls: [...glpVaults, ...gmVaultsArbitrum] });
-      const bals = await api.multiCall({ abi: 'uint256:totalAssets', calls: [...glpVaults, ...gmVaultsArbitrum] });
-      api.add(assets, bals)
-    },
+    staking: stakings([mUMAMI, OHM_STAKING_sUMAMI], UMAMI),
+    tvl: sumERC4626VaultsExport({ vaults: glpVaults.concat(gmVaultsArbitrum), isOG4626: true }),
   },
   avax: {
-    tvl: async (api) => {
-
-      const assets = await api.multiCall({ abi: 'address:asset', calls: gmVaultsAvax });
-      const bals = await api.multiCall({ abi: 'uint256:totalAssets', calls: gmVaultsAvax });
-      api.add(assets, bals)
-    },
+    tvl: sumERC4626VaultsExport({ vaults:gmVaultsAvax, isOG4626: true }),
   }
-};
+}
