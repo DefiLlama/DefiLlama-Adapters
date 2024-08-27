3a8fffe60d8d144097ac51d841afa34ceddec34a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-27 07:59:38 +0200

    code refactor

--- projects/deltatrade.js
+++ projects/deltatrade.js
@@ -1,95 +1,25 @@
-const { getTokenBalance } = require('./helper/chain/near');
+const { sumTokens } = require('./helper/chain/near');
 
 const GRID_CONTRACT_ID = 'grid.deltatrade.near';
 const DCA_CONTRACT_ID = 'dca.deltatrade.near';
 
-const tokenMapping = {
-  'wrap.near': {
-    name: 'near',
-    decimals: 24,
-  },
-  '17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1': {
-    name: 'usd-coin',
-    decimals: 6,
-  },
-  'token.v2.ref-finance.near': {
-    name: 'ref-finance',
-    decimals: 18,
-  },
-  'token.burrow.near': {
-    name: 'burrow',
-    decimals: 18,
-  },
-  'token.lonkingnearbackto2024.near': {
-    name: 'lonk-on-near',
-    decimals: 8,
-  },
-  'blackdragon.tkn.near': {
-    name: 'black-dragon',
-    decimals: 24,
-  },
-  'ftv2.nekotoken.near': {
-    name: 'neko',
-    decimals: 24,
-  },
-  // "dragonsoultoken.near": {
-  //     "decimals": 18
-  // },
-  'gear.enleap.near': {
-    name: 'near-tinker-union-gear',
-    decimals: 18,
-  },
-  // "nearnvidia.near": {
-  //     "decimals": 8
-  // },
-  // "bean.tkn.near": {
-  //     "decimals": 18
-  // },
-  'token.0xshitzu.near': {
-    name: 'shitzu',
-    decimals: 18,
-  },
-  //   "slush.tkn.near": {
-  //       "decimals": 18
-  //   },
-  'marmaj.tkn.near': {
-    name: 'marmaj',
-    decimals: 18,
-  },
-  'edge-fast.near': {
-    name: 'edge-video-ai',
-    decimals: 24,
-  },
-  '802d89b6e511b335f05024a65161bce7efc3f311.factory.bridge.near': {
-    name: 'linear-protocol-lnr',
-    decimals: 18,
-  },
-  //   "hat.tkn.near": {
-  //       "decimals": 18
-  //   }
-};
-
-async function tvl() {
-  const balances = {};
-
-  for (const token of Object.keys(tokenMapping)) {
-    const tokenName = tokenMapping[token].name;
-    if (!tokenName) continue;
-    const [grid, dca] = await Promise.all([
-      getTokenBalance(token, GRID_CONTRACT_ID),
-      getTokenBalance(token, DCA_CONTRACT_ID),
-    ]);
-    const decimals = tokenMapping[token].decimals;
-    const balance = grid / 10 ** decimals + dca / 10 ** decimals;
-    balances[tokenName] = balance;
-  }
-
-  return balances;
-}
+const tokens = [
+  'wrap.near',
+  '17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1',
+  'token.v2.ref-finance.near',
+  'token.burrow.near',
+  'token.lonkingnearbackto2024.near',
+  'blackdragon.tkn.near',
+  'ftv2.nekotoken.near',
+  'gear.enleap.near',
+  'token.0xshitzu.near',
+  'edge-fast.near',
+  '802d89b6e511b335f05024a65161bce7efc3f311.factory.bridge.near',
+]
 
 module.exports = {
   timetravel: false,
   near: {
-    tvl,
+    tvl: () => sumTokens({ tokens, owners: [GRID_CONTRACT_ID, DCA_CONTRACT_ID] }),
   },
-};
+}
