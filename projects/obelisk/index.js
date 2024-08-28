8ec130f6feef1fceec9efdaf6da097a940331f2f
zhenhao goh <kenneth.gzhao@outlook.com>
2024-08-28 16:06:26 +0800

    initial commit for project obelisk

---
+++ projects/obelisk/index.js
@@ -0,0 +1,16 @@
+const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+const sdk = require("@defillama/sdk");
+
+module.exports = {
+  bitcoin: {
+    tvl: async (api, block) => {
+    const MINT_OBTC_TOKEN_CONTRACT = '0xB1e12802831Da99D2d47b6a55049D69bf7De0e3C';
+        let result = await sdk.api.erc20.totalSupply({ target: MINT_OBTC_TOKEN_CONTRACT, block: block });
+        console.log(result);
+        return {
+            [MINT_OBTC_TOKEN_CONTRACT]: (result.output || 0),
+        }
+    }
+  },
+  methodology: `TVL for oBTC consists of the BTC deposits in custody that were used to mint oBTC`,
+}
