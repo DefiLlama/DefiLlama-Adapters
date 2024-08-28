affb4fde4b8b77e4ace9e2fcaa229dd5d07d8bc7
IguanaDEX <karageorge@proton.me>
2024-08-28 17:51:17 +0100

    add: iguanadex and iguanadex v2

--- projects/iguanadex-v2/index.js
+++ projects/iguanadex-v2/index.js
@@ -2,5 +2,5 @@ const { getUniTVL, } = require('../helper/unknownTokens')
 
 module.exports = {
   misrepresentedTokens: true,
-  etherlink: { tvl: getUniTVL({ factory: '0x3eebf549D2d8839E387B63796327eE2C8f64A0C4', useDefaultCoreAssets: true, fetchBalances: true }) }
+  etherlink: { tvl: getUniTVL({ factory: '0x3eebf549D2d8839E387B63796327eE2C8f64A0C4', useDefaultCoreAssets: true }) }
 };
