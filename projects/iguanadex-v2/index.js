5f55c5975105444f0986b03889fadea90490a593
styliann-eth <ns2808@protonmail.com>
2024-08-28 17:48:47 +0100

    add: iguanadex and iguanadex v2

---
+++ projects/iguanadex-v2/index.js
@@ -0,0 +1,6 @@
+const { getUniTVL, } = require('../helper/unknownTokens')
+
+module.exports = {
+  misrepresentedTokens: true,
+  etherlink: { tvl: getUniTVL({ factory: '0x3eebf549D2d8839E387B63796327eE2C8f64A0C4', useDefaultCoreAssets: true, fetchBalances: true }) }
+};
