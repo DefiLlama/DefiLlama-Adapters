84b3cadb5387eeb4f9cb79e0acf5b8f59cf8f085
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-28 19:36:54 +0200

    code refactor

--- projects/iguanadex-v2/index.js
+++ projects/iguanadex-v2/index.js
@@ -1,6 +1,4 @@
-const { getUniTVL, } = require('../helper/unknownTokens')
-
-module.exports = {
-  misrepresentedTokens: true,
-  etherlink: { tvl: getUniTVL({ factory: '0x3eebf549D2d8839E387B63796327eE2C8f64A0C4', useDefaultCoreAssets: true }) }
-};
+const { uniTvlExports } = require('../helper/unknownTokens')
+module.exports = uniTvlExports({
+  'etlk': '0x3eebf549D2d8839E387B63796327eE2C8f64A0C4'
+})
--- projects/iguanadex/index.js
+++
@@ -1,5 +0,0 @@
-const { uniV3Export } = require("../helper/uniswapV3");
-
-module.exports = uniV3Export({
-  etherlink: { factory: "0x093cCBAEcb0E0006c8BfFca92E9929d117fEC583", fromBlock: 1960368 },
-});
