f3bc420585c50c6e2cd0b87796a910baeaafdd2b
g1ntoki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-29 11:20:15 +0200

    use graph

--- projects/iguanadex-v3/index.js
+++
@@ -1,5 +0,0 @@
-const { uniV3Export } = require("../helper/uniswapV3");
-
-module.exports = uniV3Export({
-  etlk: { factory: "0x093cCBAEcb0E0006c8BfFca92E9929d117fEC583", fromBlock: 1960368 },
-});
