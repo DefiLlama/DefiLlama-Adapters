1046064545950273a40c2ed9fb424d266078b580
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-02 10:59:23 +0200

    code refactor

--- projects/LairFinance/index.js
+++ projects/LairFinance/index.js
@@ -1,17 +1,11 @@
-const sdk = require('@defillama/sdk');
-
 const contracts = {
   stKAIA: '0x42952B873ed6f7f0A7E4992E2a9818E3A9001995',
   node: '0x7949597f453592B782EC9036Af27d63Ed9774b2d',
 }
 
 async function tvl(api) {
-  const chain = 'klaytn'
-  const tvl = await sdk.api.abi.call({ chain, target: contracts.node, abi: "uint256:getTotalStakingAmount" });
-
-  return {
-    [`${chain}:0x0000000000000000000000000000000000000000`]: tvl.output
-  };
+  const tvl = await api.call({ target: contracts.node, abi: "uint256:getTotalStakingAmount" })
+  api.addGasToken(tvl)
 }
 
 module.exports = {
