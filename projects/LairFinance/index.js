43f211294086596f1661d234141530b74610705c
0xyongyong <yongyong@bugcity.io>
2024-09-02 02:18:04 +0000

    add LairFinance project

---
+++ projects/LairFinance/index.js
@@ -0,0 +1,19 @@
+const sdk = require('@defillama/sdk');
+
+const contracts = {
+  stKAIA: '0x42952B873ed6f7f0A7E4992E2a9818E3A9001995',
+  node: '0x7949597f453592B782EC9036Af27d63Ed9774b2d',
+}
+
+async function tvl(api) {
+  const chain = 'klaytn'
+  const tvl = await sdk.api.abi.call({ chain, target: contracts.node, abi: "uint256:getTotalStakingAmount" });
+console.log('123',tvl.output)
+  return {
+    [`${chain}:0x0000000000000000000000000000000000000000`]: tvl.output
+  };
+}
+
+module.exports = {
+	klaytn: { tvl }
+}
