460eb7e59b7241fccd39b35cd9166dffbe515ab2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-02 10:39:20 +0200

    track convex balance

--- projects/originether/index.js
+++ projects/originether/index.js
@@ -11,6 +11,18 @@ const ethTvl = async (api) => {
   const stakingBalance2 = await api.call({ abi: abi.checkBalance, target: nativeStaking2, params: ADDRESSES.ethereum.WETH })
   api.add(ADDRESSES.ethereum.WETH, stakingBalance2)
 
+  // add ETH part of curve LP
+  const convexStrategy = '0x1827F9eA98E0bf96550b2FC20F7233277FcD7E63'
+  const lp = await api.call({  abi: 'address:curvePool', target: convexStrategy})
+  const ethIndex = await api.call({  abi: 'uint128:ethCoinIndex', target: convexStrategy})
+  const lpBalance = await api.call({  abi: abi.checkBalance, target: convexStrategy, params: ADDRESSES.ethereum.WETH})
+  const lpSupply = await api.call({  abi: 'erc20:totalSupply', target: lp})
+  const ethInPool = await api.call({  abi: 'function balances(uint256) view returns (uint256)', target: lp, params: ethIndex})
+  const ethLPBalance = (lpBalance / lpSupply) * ethInPool
+  console.log(lpBalance, lpSupply, ethInPool, ethLPBalance)
+  api.add(ADDRESSES.ethereum.WETH, ethLPBalance)
+
+
   return api.sumTokens({ owner: vault, tokens: [ADDRESSES.ethereum.WETH] })
 }
 
