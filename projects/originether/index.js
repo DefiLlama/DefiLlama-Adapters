ee266cff815446d22f9410db8b1df05ccc1a0d15
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-02 10:23:40 +0200

    refactor oETH

--- projects/originether/index.js
+++ projects/originether/index.js
@@ -1,38 +1,35 @@
-const sdk = require("@defillama/sdk");
+const ADDRESSES = require('../helper/coreAssets.json')
 const abi = require("../origindollar/abi.json");
 
-const vault = "0x39254033945aa2e4809cc2977e7087bee48bd7ab";
 
-const ethTvl = async (timestamp, ethBlock) => {
-  const balances = {};
+const ethTvl = async (api) => {
+  const vault = "0x39254033945aa2e4809cc2977e7087bee48bd7ab";
+  const nativeStaking = '0x34eDb2ee25751eE67F68A45813B22811687C0238'
+  const nativeStaking2 = '0x4685dB8bF2Df743c861d71E6cFb5347222992076'
+  const stakingBalance1 = await api.call({ abi: abi.checkBalance, target: nativeStaking, params: ADDRESSES.ethereum.WETH })
+  api.add(ADDRESSES.ethereum.WETH, stakingBalance1)
+  const stakingBalance2 = await api.call({ abi: abi.checkBalance, target: nativeStaking2, params: ADDRESSES.ethereum.WETH })
+  api.add(ADDRESSES.ethereum.WETH, stakingBalance2)
 
-  // Account WETH, rETH, frxETH and stETH backing up the minted OUSD
-  const backingAssets = (
-    await sdk.api.abi.call({
-      abi: abi.getAllAssets,
-      target: vault,
-      block: ethBlock,
-    })
-  ).output;
+  return api.sumTokens({ owner: vault, tokens: [ADDRESSES.ethereum.WETH] })
+}
 
-  for (let i = 0; i < backingAssets.length; i++) {
-    const backingAssetBalance = (
-      await sdk.api.abi.call({
-        abi: abi.checkBalance,
-        target: vault,
-        params: backingAssets[i],
-        block: ethBlock,
-      })
-    ).output;
+async function baseTvl(api) {
+  const vault = '0x98a0CbeF61bD2D21435f433bE4CD42B56B38CC93'
+  const aeroAMO = '0xF611cC500eEE7E4e4763A05FE623E2363c86d2Af'
+  const dripper = await api.call({ target: vault, abi: 'function dripper() view returns (address)' })
+  const [amountWeth, _amountOETH] = await api.call({ abi: 'function getPositionPrincipal() view returns (uint256, uint256)', target: aeroAMO })
+  api.add(ADDRESSES.base.WETH, amountWeth)
+  return api.sumTokens({ owners: [vault], tokens: [ADDRESSES.base.WETH] })
 
-    sdk.util.sumSingleBalance(balances, backingAssets[i], backingAssetBalance);
-  }
+}
 
-  return balances;
-};
 
 module.exports = {
   ethereum: {
     tvl: ethTvl,
+  },
+  base: {
+    tvl: baseTvl,
   },
 };
