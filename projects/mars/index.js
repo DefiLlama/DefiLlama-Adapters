0f9fbebd0329b90e368441cd7e9ad8ccc9c200d9
Linkie Link <linkielink.dev@gmail.com>
2024-09-03 09:30:31 +0200

    fix: remove debt from deposits

--- projects/mars/index.js
+++ projects/mars/index.js
@@ -3,9 +3,17 @@ const sdk = require('@defillama/sdk');
 const { queryContract} = require('../helper/chain/cosmos');
 const { getChainTransform } = require('../helper/portedTokens');
 
-const paramsContractAddresses = {
-  'osmosis': 'osmo1nlmdxt9ctql2jr47qd4fpgzg84cjswxyw6q99u4y4u4q6c2f5ksq7ysent',
-  'neutron':'neutron1x4rgd7ry23v2n49y7xdzje0743c5tgrnqrqsvwyya2h6m48tz4jqqex06x'
+const BigNumber = require("bignumber.js");
+
+const contractAddresses = {
+  osmosis: {
+    params: 'osmo1nlmdxt9ctql2jr47qd4fpgzg84cjswxyw6q99u4y4u4q6c2f5ksq7ysent',
+    redBank: 'osmo1c3ljch9dfw5kf52nfwpxd2zmj2ese7agnx0p9tenkrryasrle5sqf3ftpg',
+  },
+  neutron: {
+    params: 'osmo1nlmdxt9ctql2jr47qd4fpgzg84cjswxyw6q99u4y4u4q6c2f5ksq7ysent',
+    redBank: 'neutron1n97wnm7q6d2hrcna3rqlnyqw2we6k0l8uqvmyqq6gsml92epdu7quugyph',
+  },
 }
 
 
@@ -13,7 +21,7 @@ const paramsContractAddresses = {
 
 async function osmosisTVL() {
   let balances = {};
-  await fetchDeposits(balances, 'osmosis');
+  await fetchDepositsMinusDebts(balances, 'osmosis');
   return balances;
 }
 
@@ -21,22 +29,23 @@ async function osmosisTVL() {
 
 async function neutronTVL() {
   let balances = {};
-  await fetchDeposits(balances, 'neutron');
+  await fetchDepositsMinusDebts(balances, 'neutron');
   return balances;
 }
 
 // HELPERS
 
-async function fetchDeposits(balances, chain) {
+async function fetchDepositsMinusDebts(balances, chain) {
   let coins = [];
   let assetParamsPagesRemaining = true;
+  let redBankParamsPagesRemaining = true;
   let startAfter = null;
   const pageLimit = 5;
   const denomTransform = await getChainTransform(chain);
 
   while (assetParamsPagesRemaining) {
     const assetParams = await queryContract({
-      contract: paramsContractAddresses[chain],
+      contract: contractAddresses[chain].params,
       chain,
       data: { 'all_asset_params': { limit: pageLimit, 'start_after': startAfter } } 
     });
@@ -51,6 +60,25 @@ async function fetchDeposits(balances, chain) {
     await addCoinsFromAssetParams(coins, assetParams, chain);
   }
 
+
+
+  while (redBankParamsPagesRemaining) {
+    const markets = await queryContract({
+      contract: contractAddresses[chain].redBank,
+      chain,
+      data: { 'markets': { limit: pageLimit, 'start_after': startAfter } } 
+    });
+
+    if(markets.length === pageLimit) {
+      startAfter = markets[markets.length - 1].denom;
+      redBankParamsPagesRemaining = true
+    } else {
+      redBankParamsPagesRemaining = false;
+    }
+
+    await deductCoinsFromMarkets(coins, markets, chain);
+  }
+
   coins.forEach(coin =>  {
     sdk.util.sumSingleBalance(balances, denomTransform(coin.denom), coin.amount);
   })
@@ -62,13 +90,34 @@ async function addCoinsFromAssetParams(coins, assetParams, chain) {
   // query the deposited amount for each asset and add it to the coins array
   await Promise.all(assetDenoms.map(async denom => {
     let totalDepositInfo = await queryContract({
-      contract: paramsContractAddresses[chain],
+      contract: contractAddresses[chain].params,
       chain,
       data: { 'total_deposit': {
         'denom': denom,
       } } 
     });
     coins.push({denom, amount: totalDepositInfo.amount});
+  }));
+}
+
+async function deductCoinsFromMarkets(coins, markets, chain) {
+
+  // query the underlying debt amount from the debt_total_scaled
+  await Promise.all(markets.map(async market => {
+    let totalDebt = await queryContract({
+      contract: contractAddresses[chain].redBank,
+      chain,
+      data: { 'underlying_debt_amount': {
+        'denom': market.denom,
+        'amount_scaled': market.debt_total_scaled
+      } } 
+    });
+    coins.forEach(coin => {
+      if(coin.denom === market.denom) {
+        coin.amount = new BigNumber(coin.amount).minus(totalDebt).toString();
+      }
+    });
+    
   }));
 }
 
