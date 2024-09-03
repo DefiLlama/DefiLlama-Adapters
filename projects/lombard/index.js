709c6ef79b585b39b321c6eaa8f4287177a32101
James Stevens <jsteve1@stanford.edu>
2024-09-03 15:32:26 -0400

    fix: lombard btc addresses

--- projects/lombard/index.js
+++ projects/lombard/index.js
@@ -1,21 +1,41 @@
-const sdk = require("@defillama/sdk");
+const { sumTokens } = require('../helper/chain/bitcoin')
+const { get } = require('../helper/http')
 
-async function tvl(timestamp, block) {
-  const lbtcAddress = "0x8236a87084f8B84306f72007F36F2618A5634494";
-  
-  const totalSupply = await sdk.api.erc20.totalSupply({
-    target: lbtcAddress,
-    block
-  });
+const API_URL = 'https://mainnet.prod.lombard.finance/api/v1/addresses'
+const BATCH_SIZE = 1000
 
-  return {
-    bitcoin: totalSupply.output / 1e8
-  };
+async function getAllAddresses() {
+  let allAddresses = []
+  let offset = 0
+  let hasMore = true
+  let batchNumber = 1
+
+  while (hasMore) {
+    const response = await get(`${API_URL}?limit=${BATCH_SIZE}&offset=${offset}`)
+    const data = response.addresses
+
+    const newAddresses = data.map(a => a.btc_address)
+    allAddresses = allAddresses.concat(newAddresses)
+    
+    console.log(`Batch ${batchNumber} completed: ${newAddresses.length} addresses`)
+    
+    hasMore = response.has_more
+    offset += BATCH_SIZE
+    batchNumber++
+  }
+
+  return allAddresses
+}
+
+async function tvl() {
+  const addresses = await getAllAddresses()
+  console.log("Summing tokens for addresses:", addresses)
+  return sumTokens({ owners: addresses })
 }
 
 module.exports = {
+  timetravel: false,
   bitcoin: {
-    tvl,
-  },
-  methodology: `TVL for LBTC consists of the total supply of the LBTC ERC-20 token on Ethereum, representing locked Bitcoin`,
-};
+    tvl
+  }
+}
