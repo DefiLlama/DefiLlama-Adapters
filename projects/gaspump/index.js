fb77a54819bff580aece13c106dec474f22aafd8
g1ntoki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-04 13:44:03 +0200

    fix gaspump #11499

--- projects/gaspump/index.js
+++ projects/gaspump/index.js
@@ -4,7 +4,7 @@ const { fetchURL } = require('../helper/utils');
 async function fetchTvl(api) {
   const response = await fetchURL("https://api.gas111.com/api/v1/internal/tokens/balances-list") 
   for (const pool of response.data) {
-    api.add(ADDRESSES.ton.TON, pool.bonding_curve_balance/2) // we include only the TON part of the pool
+    api.add(ADDRESSES.ton.TON, pool.bonding_curve_balance)
   }
 }
 
