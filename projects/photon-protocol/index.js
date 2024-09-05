37b22cd6d1b0d7f6efbafeee0e00ae3a43c7a29d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-05 13:19:40 +0200

    code refactor

--- projects/photon-protocol/index.js
+++ projects/photon-protocol/index.js
@@ -1,24 +1,15 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { stakingUnknownPricedLP } = require("../helper/staking");
-const { sumTokensAndLPsSharedOwners } = require("../helper/unwrapLPs");
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
 const photon = "0x2F1305F0619ADa14688A4291953bd7d284f9C2a5";
 const stakingContract = "0x81177472Ce36A9b1AB573804CD215C72cEb76F36";
 const photonbusd = "0xEBAa17dA7D5C616441290669E9D2c982c8B1Da25"; //PHOTON-BUSD
 const treasury = "0x0f90591b01DE6F832e8B8E4ec3525efD423BCaD1";
-const treasuryTokens = [
-    [ADDRESSES.bsc.BUSD, false], // BUSD
-];
-
-async function tvl (timestamp, block, chainBlocks) {
-    let balances = {};
-    await sumTokensAndLPsSharedOwners(balances, treasuryTokens, [treasury], chainBlocks.bsc, "bsc", addr=>`bsc:${addr}`);
-    return balances;
-}
 
 module.exports = {
-    bsc: {
-        tvl,
-        staking: stakingUnknownPricedLP(stakingContract, photon, "bsc", photonbusd, addr=>`bsc:${addr}`)
-    }
+  bsc: {
+    tvl: sumTokensExport({ tokens: [ADDRESSES.bsc.BUSD], owner: treasury }),
+    staking: stakingUnknownPricedLP(stakingContract, photon, "bsc", photonbusd, addr => `bsc:${addr}`)
+  }
 }
