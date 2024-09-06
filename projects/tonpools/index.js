9a36bea218c519ad740f8b410c7b18dea08c2a0c
Kunal Sangwan <kunalsangwan1@gmail.com>
2024-09-06 18:57:20 +0530

    Updated code to fetch and use contract balance.

--- projects/tonpools/index.js
+++ projects/tonpools/index.js
@@ -1,11 +1,11 @@
-const { call } = require("../helper/chain/ton");
+const { sumTokensExport } = require('../helper/chain/ton')
+const ADDRESSES = require("../helper/coreAssets.json");
+
+const tonpoolsContractAddress = "EQA7y9QkiP4xtX_BhOpY4xgVlLM7LPcYUA4QhBHhFZeL4fTa";
 
 module.exports = {
-  timetravel: false,
+  methodology: "Ton Pools's TVL includes all deposited supported assets",
   ton: {
-    tvl: async () => {
-      const result = await call({ target: "EQA7y9QkiP4xtX_BhOpY4xgVlLM7LPcYUA4QhBHhFZeL4fTa", abi: "getActiveContest"});
-      return { "coingecko:the-open-network": result[0]/1e9 };
-    },
+    tvl: sumTokensExport({ owner: tonpoolsContractAddress, tokens: [ADDRESSES.null] }),
   },
 }
