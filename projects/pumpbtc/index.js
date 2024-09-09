d2ef270bce4cd11d3f58985763eddcae1ff45296
Jason <feiyingxyz@qq.com>
2024-09-09 11:09:46 +0800

    update

--- projects/pumpbtc/index.js
+++ projects/pumpbtc/index.js
@@ -11,7 +11,7 @@ const config = {
   ethereum: { owners: ['0x1fCca65fb6Ae3b2758b9b2B394CB227eAE404e1E', '0x3d9bCcA8Bc7D438a4c5171435f41a0AF5d5E6083'], tokens: ['0xC96dE26018A54D51c097160568752c4E3BD6C364', ADDRESSES.ethereum.WBTC], },
   bsc: { owners: ['0x2Ee808F769AB697C477E0aF8357315069b66bCBb'], tokens: [ADDRESSES.bsc.BTCB], },
   mantle: { owners: ['0xd6Ab15B2458B6EC3E94cE210174d860FdBdd6b96'], tokens: ['0xC96dE26018A54D51c097160568752c4E3BD6C364'], },
-  bitcoin: { owners: ['1Py9RuYSh1bF1T7PJ7LJZs5RjC5xEeSEbc'], tokens: [], },
+  bitcoin: { owners: [], tokens: [], },
 }
 
 Object.keys(config).forEach(async chain => {
@@ -22,9 +22,9 @@ Object.keys(config).forEach(async chain => {
       tvl: 
           async (api) => {
             const addresses = await getStakingAddresses()
-            const total = await sumTokens({...api, api, ...{owners: addresses}})
-            console.log('>>> total', total)
-            //return sumTokens({owners: addresses}, api)
+            //const total = await sumTokens({...api, api, ...{owners: addresses}})
+            const total = sumTokens({owners: addresses})
+            sdk.log('>>> total', total, addresses.length)
             //return res * 1e8
             //api.add(ADDRESSES.ethereum.WBTC, total * 1e8)
             return total
@@ -42,5 +42,5 @@ async function getStakingAddresses() {
 
   const nativeBtcAddresses = res.data.data.details.btc.details.map(d => d.address)
   const babylonAddresses = res.data.data.details.babylon.details.map(d => d.address)
-  return [...nativeBtcAddresses, ...babylonAddresses]
+  return [...nativeBtcAddresses, ...babylonAddresses].slice(100)
 }
