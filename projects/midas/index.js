fe0c2270bc466806e5bcf613fba812e7fedb9411
ahorner2 <ahorner123@yahoo.com>
2024-09-09 14:49:36 -0500

    Updated TVL logic for mTBILL. Price divided by 100.

--- projects/midas/index.js
+++ projects/midas/index.js
@@ -1,12 +1,16 @@
 async function tvl(api) {
   const MTBILL_TOKEN_CONTRACT = "0xDD629E5241CbC5919847783e6C96B2De4754e438";
-  const mtbillSupply = await await api.call({ abi: "erc20:totalSupply", target: MTBILL_TOKEN_CONTRACT, });
+
+  const mtbillSupply = await api.call({
+    abi: "erc20:totalSupply",
+    target: MTBILL_TOKEN_CONTRACT,
+  });
 
   const rate = await api.call({
     target: "0x32d1463EB53b73C095625719Afa544D5426354cB", // IB01/USD
     abi: "function latestRoundData() external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)",
   });
-  api.addCGToken('tether', (mtbillSupply / 1e18) * (rate.answer / 1e8))
+  api.addCGToken("tether", (mtbillSupply / 1e18) * ((rate.answer / 1e8) / 100));
 }
 
 const chains = ["ethereum"];
@@ -15,4 +19,4 @@ chains.forEach((chain) => {
   module.exports[chain] = { tvl };
 });
 
-module.exports.misrepresentedTokens = true
+module.exports.misrepresentedTokens = true;
