29bcfc7e8016f6472c962678063c8bbbcdf1b019
realdealshaman <realdealshaman11@gmail.com>
2024-09-09 04:27:02 +0100

    add hallmark

--- projects/dough-finance.js
+++ projects/dough-finance.js
@@ -14,6 +14,9 @@ async function getVaults(api) {
 }
 
 module.exports = {
+    hallmarks: [
+        [1720742400, "Protocol Exploit"]
+    ],
     ethereum: {
         tvl: async (api) => {
             const { vaults, tokens } = await getVaults(api)
