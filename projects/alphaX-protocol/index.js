91d151a12b543403413de4ffc9f5a1c20368bb1d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-09 10:17:51 +0200

    track alphaX treasury

--- projects/alphaX-protocol/index.js
+++ projects/alphaX-protocol/index.js
@@ -1,37 +1,18 @@
-const ADDRESSES = require('../helper/coreAssets.json');
-const { sumTokensExport } = require('../helper/unwrapLPs');
-
-const tokens = [
-  ADDRESSES.ethereum.USDT,
-  ADDRESSES.arbitrum.USDT
-];
-
-const owners = [
-  '0x95e2A6458419b7A38193CB853F45fD7329443A90',
-  '0x70eF5649c0b51162C62CE38d807b8aD61Ac2560d',
-
-];
-
+const ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 const walletAddresses = {
- ethereum: ['0xA61a6E696B7C566DA42B80dA27d96e7104bcec99','0x95e2A6458419b7A38193CB853F45fD7329443A90'],
- arbitrum: ['0x552E7A55802f3350C707a243E402aa50Eda9D286']
- 
-};
+  ethereum: ['0xA61a6E696B7C566DA42B80dA27d96e7104bcec99'],
+  arbitrum: ['0x552E7A55802f3350C707a243E402aa50Eda9D286']
+}
 
 const tokenAddress = {
   ethereum: [ADDRESSES.ethereum.USDT],
   arbitrum: [ADDRESSES.arbitrum.USDT],
-
- 
 }
 
-module.exports = {
-  ethereum: {
-    tvl: sumTokensExport({ owners: walletAddresses.ethereum, tokens: tokenAddress.ethereum }),
-  },
-  arbitrum: {
-    tvl: sumTokensExport({ owners: walletAddresses.arbitrum, tokens: tokenAddress.arbitrum }),
-  },
- 
-};
+Object.keys(walletAddresses).forEach(chain => {
+  module.exports[chain] = {
+    tvl: sumTokensExport({ owners: walletAddresses[chain], tokenAddresses: tokenAddress[chain], })
+  }
+})
