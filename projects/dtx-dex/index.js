87719beb06f59323336b7fc2165a7e62fd88030f
dtx-devs <abel@dtx.trade>
2024-09-12 17:24:04 +0800

    feat: add v3 tvl

--- projects/dtx-dex/index.js
+++ projects/dtx-dex/index.js
@@ -1,4 +1,20 @@
 const { uniTvlExports } = require('../helper/unknownTokens')
-module.exports = uniTvlExports({
+const { uniV3Export } = require("../helper/uniswapV3");
+const sdk = require('@defillama/sdk')
+
+const uniV2Exports = uniTvlExports({
   taiko: '0x2EA9051d5a48eA2350b26306f2b959D262cf67e1'
 })
+
+const uniV3Exports = uniV3Export({
+  taiko: {
+    factory: "0xfCA1AEf282A99390B62Ca8416a68F5747716260c",
+    fromBlock: 105000,
+  },
+});
+
+module.exports = {
+  taiko: {
+    tvl: sdk.util.sumChainTvls([uniV2Exports.taiko.tvl, uniV3Exports.taiko.tvl])
+  }
+}
