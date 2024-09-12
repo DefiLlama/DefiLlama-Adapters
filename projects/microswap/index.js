ffe6c12a5972c70a236d14172d5e9235f435c38b
g1ntoki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-12 09:07:33 +0200

    code refactor

--- projects/microswap/index.js
+++ projects/microswap/index.js
@@ -1,68 +1,22 @@
-const {unwrapUniswapLPs} = require("../helper/unwrapLPs");
-const {getChainTransform} = require("../helper/portedTokens");
-
-const FARM_MASTER = "0x19A497E9c034c0D66952366F46f0e4e8b27465a8";
-const blacklistedTokens = []
+const { yieldHelper } = require("../helper/yieldHelper");
 
 const abis = {
-    name: "string:name",
-    wantLockedTotal: "uint256:wantLockedTotal",
-    poolInfo: "function poolInfo(uint256) view returns (address want, uint256 allocPoint, uint256 lastRewardTime, address strategy, uint256 earlyWithdrawFee, uint256 earlyWithdrawTime)",
+  name: "string:name",
+  wantLockedTotal: "uint256:wantLockedTotal",
+  poolInfo: "function poolInfo(uint256) view returns (address want, uint256 allocPoint, uint256 lastRewardTime, address strategy, uint256 earlyWithdrawFee, uint256 earlyWithdrawTime)",
 }
 
-module.exports = {
-    start: 1724346000,
-    cronos_zkevm: {
-        tvl,
-    }
+async function getTokenBalances ({ api, poolInfos, }) {
+  const strategies = poolInfos.map(i => i.strategy)
+  return api.multiCall({ abi: abis.wantLockedTotal, calls: strategies })
 }
 
-async function tvl(api, _b, chainBlocks) {
-    const balances = {};
-    const chainLocal = "cronos_zkevm";
-
-    const poolInfos = await api.fetchList({lengthAbi: 'poolLength', itemAbi: abis.poolInfo, target: FARM_MASTER})
-    const allPoolInfos = poolInfos.filter(el => !blacklistedTokens.includes(el.toLowerCase))
-    const transformAddress = await getChainTransform(chainLocal);
-
-    const wantedAddresses = allPoolInfos.map(i => i.want)
-
-    const wrapTarget = v => ({
-        target: v
-    });
-
-    const wantedLocked = (await api.multiCall({
-        abi: abis.wantLockedTotal,
-        calls: allPoolInfos.map(i => wrapTarget(i.strategy)),
-        permitFailure: true
-    }))
-    const vaultName = (await api.multiCall({
-        abi: abis.name,
-        calls: allPoolInfos.map(i => wrapTarget(i.want)),
-        permitFailure: true
-    })).map(i => i ?? "")
-    const lpPositions = [];
-    for (let k = 0; k < wantedLocked.length; k++) {
-        if (vaultName[k].toLowerCase().endsWith(" lps") || vaultName[k].toLowerCase().endsWith(" lp")) {
-            lpPositions.push({
-                token: wantedAddresses[k],
-                balance: wantedLocked[k],
-            });
-        } else {
-            api.sumSingleBalance(
-                balances,
-                `${chainLocal}:${wantedAddresses[k]}`,
-                wantedLocked[k]
-            );
-        }
-    }
-    await unwrapUniswapLPs(
-        balances,
-        lpPositions,
-        chainBlocks[chainLocal],
-        chainLocal,
-        transformAddress
-    );
-
-    return balances;
-}
+module.exports = yieldHelper({
+  project: 'microswap',
+  chain: 'cronos_zkevm',
+  masterchef: '0x19A497E9c034c0D66952366F46f0e4e8b27465a8',
+  nativeToken: '',
+  abis,
+  getTokens: ({ poolInfos, }) => poolInfos.map(i => i.want),
+  getTokenBalances,
+})
