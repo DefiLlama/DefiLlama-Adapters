352d620b71717a5606b4eb48097f93a103b47eda
realdealshaman <realdealshaman11@gmail.com>
2024-09-12 05:15:54 +0100

    update borrows export

--- projects/evolve/index.js
+++ projects/evolve/index.js
@@ -1,6 +1,7 @@
 const { tarotHelper } = require("../tarot/tarotHelper");
 
 const config = {
+  deadFrom: '2023-02-27',
   cronos: {
     factories: ["0xb8b48e97cd037987de138b978df265d873333a3b"],
   },
@@ -9,3 +10,5 @@ const config = {
 module.exports = {}
 
 tarotHelper(module.exports, config, { tarotSymbol: 'vEvolve'})
+
+module.exports.cronos.borrowed = () => ({}) // bad debt
