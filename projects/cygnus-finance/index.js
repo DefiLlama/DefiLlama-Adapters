fe9cef893be6dd0cadb07fe23c2328aa8de40f4d
greatpie <greatpie@live.com>
2024-09-14 01:50:36 +0800

    feat: add clTON TVL

--- projects/cygnus-finance/index.js
+++ projects/cygnus-finance/index.js
@@ -18,29 +18,57 @@ async function getJettonBalance(address, jettonMasterAddress) {
   return res.balance
 }
 
-async function tonTvl() {
-  const MINTER_ADDRESS = "EQCfvQW-thWpqKgyqtXCFbYayDlHqS0-frkyP6VD70paLFZa"
+async function getCgUsdtTvl() {
+  const CGUSDT_MINTER_ADDRESS = "EQCfvQW-thWpqKgyqtXCFbYayDlHqS0-frkyP6VD70paLFZa"
   const CGUSDT_ADDRESS = 'EQBIBw3mF_TDMJqWAZihVsyUBMWpWw_deftZLiCxTmrCUOKy'
 
-  const minterResult = await call({ target: MINTER_ADDRESS, abi: "get_minter_data", stack: [] })
+  const cgusdtMinterResult = await call({ target: CGUSDT_MINTER_ADDRESS, abi: "get_minter_data", stack: [] })
   // exchange rate from cgUSDT to USDT: decimal 9
-  const cgusdtTousdt = (minterResult[5]) / 10 ** 9
+  const cgusdtToUsdt = (cgusdtMinterResult[5]) / 10 ** 9
 
   // cgUSDT total supply: decimal 6
   const jettonResult = await getJettonMetadata(CGUSDT_ADDRESS)
   const cgUsdtTotalSupply = jettonResult['total_supply']
 
   // subtract the amount of cgUSDT in the withdrawal vault
-  const withdrawVaultBalance = await getJettonBalance(MINTER_ADDRESS, CGUSDT_ADDRESS)
+  const withdrawVaultBalance = await getJettonBalance(CGUSDT_MINTER_ADDRESS, CGUSDT_ADDRESS)
 
   // caculate tvl
-  const tvl = (cgUsdtTotalSupply - withdrawVaultBalance) / 10 ** 6 * cgusdtTousdt
-  return { "coingecko:tether": tvl }
+  const tvl = (cgUsdtTotalSupply - withdrawVaultBalance) / 10 ** 6 * cgusdtToUsdt
+  return tvl
+}
+
+async function getClTonTvl() {
+  const CLTON_MINTER_ADDRESS = 'EQDz48al4FfPnapvXYJOfkBOIj3xvNZ0t5vSpQN-Qukqwm7W'
+  const CLTON_TOKEN_ADDRESS = 'EQCxd6SJQ8KiLkEpN3OoBfUIHqPE3yp0j80UnPysQqcTikNF'
+
+  const cltonMinterResult = await call({ target: CLTON_MINTER_ADDRESS, abi: "get_minter_data", stack: [] })
+  // exchange rate from clTON to TON: decimal 9
+
+  const cltonToTon = cltonMinterResult[4] / 10 ** 9
+  const cltonMetadata = await getJettonMetadata(CLTON_TOKEN_ADDRESS)
+
+  // Now the tokens in the withdrawal vault will be correctly destroyed, so the TOTAL SUPPLY can be a true indication of tvl
+  const cltonTotalSupply = cltonMetadata['total_supply']
+
+  // 
+  const cltonTvl = cltonTotalSupply / 10 ** 9 * cltonToTon
+
+  return cltonTvl
+
+}
+
+async function tonTvl() {
+  const cgusdtTvl = await getCgUsdtTvl()
+  const cltonTvl = await getClTonTvl()
+
+  return { "coingecko:tether": cgusdtTvl, "coingecko:the-open-network": cltonTvl }
+
 }
 
 
 module.exports = {
-  methodology: "Calculates the total cgUSD and cgUSDT Supply",
+  methodology: "Calculates the total cgUSD,cgUSDT and clTON Supply",
   start: START_TIME,
   base: {
     tvl: baseTvl,
