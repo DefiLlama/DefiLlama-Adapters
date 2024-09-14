bad4443027334290230e7f71476de0aae680c88a
Deadshot Ryker <ryker@zerolend.xyz>
2024-09-14 10:22:15 +0400

    added bitcoin lrt pools for zerolend

--- projects/zerolend/index.js
+++ projects/zerolend/index.js
@@ -1,14 +1,22 @@
 const { aaveExports } = require("../helper/aave");
 const { staking } = require("../helper/staking");
 const { pool2s } = require("../helper/pool2");
+const { mergeBalances } = require("@defillama/sdk/build/generalUtil");
 
 const config = {
-  era: "0xB73550bC1393207960A385fC8b34790e5133175E",
-  manta: "0x67f93d36792c49a4493652B91ad4bD59f428AD15",
-  blast: "0xc6DF4ddDBFaCb866e78Dcc01b813A41C15A08C10",
-  linea: "0x67f93d36792c49a4493652B91ad4bD59f428AD15",
-  xlayer: "0x97e59722318F1324008484ACA9C343863792cBf6",
-  ethereum: "0x47223D4eA966a93b2cC96FFB4D42c22651FADFcf",
+  era: ["0xB73550bC1393207960A385fC8b34790e5133175E"],
+  manta: ["0x67f93d36792c49a4493652B91ad4bD59f428AD15"],
+  blast: ["0xc6DF4ddDBFaCb866e78Dcc01b813A41C15A08C10"],
+  linea: [
+    "0x67f93d36792c49a4493652B91ad4bD59f428AD15", // main linea market
+    '0x9aFB91a3cfB9aBc8Cbc8429aB57b6593FE36E173', // croak linea market
+    '0xEe9ec60657B714E3FAC5255a5443AC5EC7Ba5bB0', // foxy linea market
+  ],
+  xlayer: ["0x97e59722318F1324008484ACA9C343863792cBf6"],
+  ethereum: [
+    "0x47223D4eA966a93b2cC96FFB4D42c22651FADFcf", // ethereum lrt market
+    '0x31063F7CA8ef4089Db0dEdf8D6e35690B468A611' // bitcoin lrt market
+  ],
 };
 
 const linea = {
@@ -23,7 +31,34 @@ const linea = {
 
 const data = {};
 Object.keys(config).forEach((chain) => {
-  data[chain] = aaveExports(chain, undefined, undefined, [config[chain]]);
+  // iterate over each chain's pool for tvl
+  const tvl = async (api) => {
+    const balances = {}
+    const pools = config[chain]
+    await Promise.all(pools.map(async (address) => {
+      const data = aaveExports(chain, undefined, undefined, [address]);
+      const balance = await data.tvl(api)
+      mergeBalances(balances, balance)
+    }))
+    return balances
+  };
+
+  // iterate over each chain's pool for borrowed
+  const borrowed = async (api) => {
+    const balances = {}
+    const pools = config[chain]
+    await Promise.all(pools.map(async (address) => {
+      const data = aaveExports(chain, undefined, undefined, [address]);
+      const balance = await data.borrowed(api)
+      mergeBalances(balances, balance)
+    }))
+    return balances
+  };
+
+  data[chain] = {
+    tvl,
+    borrowed
+  }
 });
 
 data.linea.staking = staking(linea.zeroLocker, linea.zero, "linea");
