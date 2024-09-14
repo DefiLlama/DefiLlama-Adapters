65c64534eb591f599038724052aef461a6655e3c
Abhishek <wizzard.coder@gmail.com>
2024-09-14 17:39:35 +0400

    (EddyFinance)Added Ultiverse stableSwap pool (#11633)

--- projects/eddyfinance/index.js
+++ projects/eddyfinance/index.js
@@ -7,7 +7,8 @@ const getReserves = 'function getReserves() view returns (uint112 _reserve0, uin
 const stablePools = [
   '0x448028804461e8e5a8877c228F3adFd58c3Da6B6', // 4 asset pool
   '0x01a9cd602c6c3f05ea9a3a55184c2181bd43e4b8', // 2 asset pool accumulated finance
-  '0xee1629de70afaf3ae3592a9d6d859949750aa697' // 2 asset pool zearn
+  '0xee1629de70afaf3ae3592a9d6d859949750aa697', // 2 asset pool zearn
+  '0x89cb3fA2A7910A268e9f7F619108aFADBD7587c4' // 2 asset UltiVerse pool
 ]
 
 async function stableSwapTvl(api) {
