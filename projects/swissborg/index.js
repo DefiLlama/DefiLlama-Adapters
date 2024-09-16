39b63740e84746ff261d251b36d928163c83abbf
Antoine <67124021+antoineborg@users.noreply.github.com>
2024-09-16 10:37:58 +0200

    Add new SwissBorg wallet (#11647)

--- projects/swissborg/index.js
+++ projects/swissborg/index.js
@@ -17,6 +17,7 @@ const config = {
       '0x67FE3293FC4e877F3CDc3F0ed93721a600f72BdE',
       '0x178Fb204c1ff2Ed7d0651C522A3a5B15480Eb76d',
       '0xFbA64167e4f091Ca625FA79aa6f83665856f8Bf2',
+      '0x8F0d8b27bF808976Fa94f03e2230b4bca95bf3C4',
     ],
   },
   bitcoin: {
@@ -106,6 +107,11 @@ const config = {
   cosmos: {
     owners: [
         'cosmos10dfzd2wpnpeuy2lgan35ah8dg5p4l298v0n8e8',
+    ]
+  },
+  arbitrum: {
+    owners: [
+      '0x8F0d8b27bF808976Fa94f03e2230b4bca95bf3C4',
     ]
   }
 }
