74a197331e48267f8673896988a547a46d3d9f2c
g1ntoki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-16 09:18:38 +0200

    code refactor

--- projects/sperax-demeter-v2/index.js
+++ projects/sperax-demeter-v2/index.js
@@ -1,34 +1,9 @@
-const { sumTokens2 } = require('../helper/unwrapLPs')
-
 async function tvl(api) {
-  const farms = await api.call({
-    target: '0x45bC6B44107837E7aBB21E2CaCbe7612Fce222e0',
-    abi: 'function getFarmList() view returns (address[])',
-  })
-  const rewardTokens = await api.multiCall({
-    calls: farms.map(farm => ({ target: farm })),
-    abi: 'function getTokenAmounts() view returns (address[], uint256[])',
-  })
-  // console.log(rewardTokens)
-  const toa = []
-  const tob = []
-  rewardTokens.forEach((tokens, i) => {
-    tokens[0].forEach((token,j ) => {
-      toa.push([token, farms[i]])
-    })
-  })
-  rewardTokens.forEach((amounts, i) => {
-    amounts[1].forEach((amount, j) => {
-      tob.push([amount, farms[i]])
-    })
-  })
-
-  await sumTokens2({ api, owners: farms, tokenAmounts: tob })
-  
-
-  return sumTokens2({ api, tokensAndOwners: toa })
+  const farms = await api.call({ target: '0x45bC6B44107837E7aBB21E2CaCbe7612Fce222e0', abi: 'address[]:getFarmList', })
+  const tokensAndBals = await api.multiCall({ calls: farms, abi: 'function getTokenAmounts() view returns (address[], uint256[])', })
+  tokensAndBals.forEach(([t, b]) => api.add(t, b))
 }
 
 module.exports = {
-  arbitrum:{tvl},
+  arbitrum: { tvl },
 }
