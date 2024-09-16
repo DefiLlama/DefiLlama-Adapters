36e5bf54b51212827b8e75f1409e932b7e979f24
g1ntoki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-16 09:09:22 +0200

    minor fix

--- projects/typus-safu/index.js
+++ projects/typus-safu/index.js
@@ -6,22 +6,11 @@ async function tvl(api) {
   const fields = await sui.getDynamicFieldObjects({
     parent: SAFU_REGISTRY,
   });
-  // console.log(fields);
   const safuVaults = fields.filter((item) => item.type.includes("Vault"));
 
   safuVaults.forEach(({ fields }) => {
-    // console.log(fields);
     const deposit_token = "0x" + fields.deposit_token.fields.name;
-
-    const share_supply = fields.share_supply;
-    // deposit_token
-    const total_share =
-      Number(share_supply[0]) +
-      Number(share_supply[1]) +
-      Number(share_supply[2]) +
-      Number(share_supply[3]);
-
-    api.add(deposit_token, total_share);
+    api.add(deposit_token, fields.share_supply.slice(0, 4));
   });
 }
 
