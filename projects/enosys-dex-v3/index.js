0bc54fc54035af298c5b132dfc8a47ba07a694f1
TJakubek <39831436+TJakubek@users.noreply.github.com>
2024-09-17 15:18:37 +0200

    Enosys - add songbird v3 dex (#11653)

---
+++ projects/enosys-dex-v3/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+  songbird: { factory: "0x416F1CcBc55033Ae0133DA96F9096Fe8c2c17E7d", fromBlock: 69857654 },
+});
