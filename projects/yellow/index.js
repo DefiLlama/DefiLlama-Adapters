d3200cd723995ce1e6c047325941a6cca63be428
Dmytro Steblyna <dsteblyna@openware.center>
2024-09-17 14:14:25 +0100

    feat: add complete adapter

--- projects/yellow/index.js
+++ projects/yellow/index.js
@@ -1,54 +1,64 @@
-const { getLogs } = require('../helper/cache/getLogs')
+const { sumTokens2 } = require('../helper/unwrapLPs');
 
-const config = {
-  polygon: {
-    vault: '0xFD1353baBf86387FcB6D009C7b74c1aB2178B304',
-    dailyClaim: '0xFD1353baBf86387FcB6D009C7b74c1aB2178B304',
-    fromBlock: 29080112,
-    tokens: {}
+const vaultAddress = '0xb5F3a9dD92270f55e55B7Ac7247639953538A261';
+
+const vaults = {
+  ethereum: {
+    vault: vaultAddress,
+    tokens: [
+      '0x0000000000000000000000000000000000000000', // ETH
+      '0xdAC17F958D2ee523a2206206994597C13D831ec7', // USDT
+      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
+      '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', // WBTC
+      '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // WETH
+      '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
+    ],
   },
-  // linea: {
-  //   vault: '0xFD1353baBf86387FcB6D009C7b74c1aB2178B304',
-  //   dailyClaim: '0xFD1353baBf86387FcB6D009C7b74c1aB2178B304',
-  //   fromBlock: 29080112,
-  //   tokens: {}
-  // },
-  // ethereum: {
-  //   vault: '0xFD1353baBf86387FcB6D009C7b74c1aB2178B304',
-  //   dailyClaim: '0xFD1353baBf86387FcB6D009C7b74c1aB2178B304',
-  //   fromBlock: 29080112,
-  //   tokens: {}
-  // },
-}
+  linea: {
+    vault: vaultAddress,
+    tokens: [
+      '0x0000000000000000000000000000000000000000', // ETH
+      '0xA219439258ca9da29E9Cc4cE5596924745e12B93', // USDT
+      '0x176211869cA2b568f2A7D4EE941E073a821EE1ff', // USDC
+      '0x3aAB2285ddcDdaD8edf438C1bAB47e1a9D05a9b4', // WBTC
+      '0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f', // WETH
+    ],
+  },
+  polygon: {
+    vault: vaultAddress,
+    tokens: [
+      '0x0000000000000000000000000000000000000000', // MATIC
+      '0xc2132D05D31c914a87C6611C10748AEb04B58e8F', // USDT
+      '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359', // USDC
+      '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // USDC (PoS)
+      '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6', // WBTC
+      '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', // WETH
+    ],
+  },
+};
 
-Object.keys(config).forEach(chain => {
-  const { vault, fromBlock } = config[chain]
+module.exports = {
+  methodology: 'The total amount of assets locked in the Yellow Wallet.',
+};
+
+Object.keys(vaults).forEach((chain) => {
+  const { vault, tokens } = vaults[chain];
+
   module.exports[chain] = {
-    tvl: async (api) => {
-      // TODO: Refine logic. Iterate over list of tokens allowed for staking.
-      // Feth locked amount and append to the responce.
+    tvl: async (timestamp, ethBlock, chainBlocks) => {
+      const balances = {};
+      const block = chainBlocks[chain];
 
-      const logs = await getLogs({
-        api,
-        target: vault,
-        eventAbi: 'event FundCreated (address indexed newFund, address comptroller, address shareToken, address vault)',
-        onlyArgs: true,
-        fromBlock,
-      })
-      const tokens = await api.multiCall({ abi: 'address[]:getAssetList', calls: logs.map(l => l.newFund) })
-      const ownerTokens = tokens.map((t, i) => [t, logs[i].vault])
-      return api.sumTokens({ ownerTokens })
-      // TVL EXPECTED RETURN EXAMPLE (TEMP)
-      // return {
-      //   '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': '5000000000000', // 5,000,000 USDC (since USDC has 6 decimals)
-      //   '0x0000000000000000000000000000000000000000': '2000000000000000000', // 2 ETH (in wei)
-      // };
+      await sumTokens2({
+        balances,
+        tokens,
+        owners: [vault],
+        block,
+        chain,
+        includeNativeTokens: true,
+      });
+
+      return balances;
     },
-    // Custom metric.
-    daily_claims: async (api) => {
-      // logs := FilterLogs(DailyClaim, StartFrom: block24hBefore)
-      // return len(logs)
-      return 1000 // Temp placeholder.
-    }
-  }
-})
+  };
+});
