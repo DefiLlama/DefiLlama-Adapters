aaaa45a85a5e027bce668a36d0b0fa8a83a9ca25
eukadish <eugene.kadish@gmail.com>
2024-09-17 08:16:25 -0400

    use Ethereum USDC for tvl price

--- projects/reservoir-protocol/index.js
+++ projects/reservoir-protocol/index.js
@@ -1,26 +1,22 @@
+const ADDRESSES = require('../helper/coreAssets.json');
+
 const rUSD = '0x09D4214C03D01F49544C0448DBE3A27f768F2b34';
 const srUSD = '0x738d1115B90efa71AE468F1287fc864775e23a31';
-const trUSD = '0x128D86A9e854a709Df06b884f81EeE7240F6cCf7';
+const termIssuer = '0x128D86A9e854a709Df06b884f81EeE7240F6cCf7';
 
 const tvl = async (api) => {
 
-    let tvl;
-    let target;
+    const trUSDTotalDebt = await api.call({ target: termIssuer, abi: 'function totalDebt() external view returns (uint256)' });
 
-    target = trUSD;
-    tvl = await api.call({ target, abi: 'function totalDebt() external view returns (uint256)' });
+    api.add(ADDRESSES['ethereum'].USDC, trUSDTotalDebt / 1e12);
 
-    api.add(target, tvl);
+    const rUSDTotalSupply = await api.call({ target: rUSD, abi: 'function totalSupply() external view returns (uint256)' });
 
-    target = rUSD;
-    tvl = await api.call({ target, abi: 'function totalSupply() external view returns (uint256)' });
+    api.add(ADDRESSES['ethereum'].USDC, rUSDTotalSupply / 1e12);
 
-    api.add(target, tvl);
+    const srUSDTotalSupply = await api.call({ target: srUSD, abi: 'function totalSupply() external view returns (uint256)' });
 
-    target = srUSD;
-    tvl = await api.call({ target, abi: 'function totalSupply() external view returns (uint256)' });
-
-    api.add(target, tvl);
+    api.add(ADDRESSES['ethereum'].USDC, srUSDTotalSupply / 1e12);
 }
 
 module.exports = {
