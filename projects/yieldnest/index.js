467c2fbd0fe618dd15d9c56f1b0423364000d03e
johnnyonline <103320838+johnnyonline@users.noreply.github.com>
2024-09-18 13:55:36 +0700

    fix: ynLSDe TVL (#11664)

--- projects/yieldnest/index.js
+++ projects/yieldnest/index.js
@@ -2,7 +2,6 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const YN_ETH = '0x09db87a538bd693e9d08544577d5ccfaa6373a48'
-const YN_LSDE = '0x35Ec69A77B79c255e5d47D5A3BdbEFEfE342630c'
 
 module.exports = {
   ethereum: {
@@ -11,7 +10,7 @@ module.exports = {
       api.add(ADDRESSES.null, ynethBalance)
       const lsdRegistry = '0x323C933df2523D5b0C756210446eeE0fB84270fd'
       const lsds = await api.call({ abi: 'address[]:getAssets', target: lsdRegistry })
-      const bals = await api.call({ abi: 'function assetBalances(address[]) view returns (uint256[])', target: YN_LSDE, params: [lsds] })
+      const bals = await api.call({ abi: 'function getAllAssetBalances() view returns (uint256[])', target: lsdRegistry })
       api.add(lsds, bals)
     }
   },
