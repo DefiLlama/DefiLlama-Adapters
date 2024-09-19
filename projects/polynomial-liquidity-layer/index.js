a42fab803e654444112be6cc7b65823e60d1e247
aniket965 <aniket965.as@gmail.com>
2024-09-19 12:08:23 +0530

    feat: change to onchain calc

--- projects/polynomial-liquidity-layer/index.js
+++ projects/polynomial-liquidity-layer/index.js
@@ -1,18 +1,9 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const { get } = require('../helper/http');
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
-// api
-const BASE_URL = 'https://perps-api-mainnet.polynomial.finance/stats'
-
-async function tvl(timestamp, ethBlock) {
-  const tvlApi = await get(BASE_URL);
-  return {
-    [`ethereum:${ADDRESSES.ethereum.USDC}`]: tvlApi.tvl * 1e6
-  };
-}
 
 module.exports = {
   polynomial: {
-    tvl: tvl
+    tvl: sumTokensExport({ owner: '0xc133983D6d9140923b5eaE52664221d9099cf119', tokens: [ADDRESSES.polynomial.USDC,ADDRESSES.polynomial.SDAI, ADDRESSES.polynomial.sUSDe]})
   }
 }
