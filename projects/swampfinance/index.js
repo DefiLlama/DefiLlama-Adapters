ff13ca37f9adb4c469dd80565e172cf113535884
realdealshaman <realdealshaman11@gmail.com>
2024-09-19 18:54:33 +0100

    fix tvl spike

--- projects/swampfinance/index.js
+++ projects/swampfinance/index.js
@@ -6,5 +6,5 @@ module.exports = yieldHelper({
   chain: 'bsc',
   masterchef: '0x33AdBf5f1ec364a4ea3a5CA8f310B597B8aFDee3',
   nativeToken: '0xc5A49b4CBe004b6FD55B30Ba1dE6AC360FF9765d',
-  blacklistedTokens: [ADDRESSES.bsc.BTCB]
+  blacklistedTokens: [ADDRESSES.bsc.BTCB, ADDRESSES.bsc.ETH]
 })
