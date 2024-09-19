a5286250121dfdee5e76c1063f08274449d33097
0xpeluche <0xpeluche@proton.me>
2024-09-19 10:54:21 +0200

    fix timestamp

--- projects/cswap/index.js
+++ projects/cswap/index.js
@@ -2,8 +2,8 @@ const { transformDexBalances } = require('../helper/portedTokens')
 const { get } = require('../helper/http')
 
 module.exports = {
-  deadFrom: '2024-09-01',
-  hallmarks: [[1719446400,'Sunset of cSwap']],
+  deadFrom: '2024-09-17',
+  hallmarks: [[1726531200,'Sunset of cSwap']],
   timetravel: false,
   misrepresentedTokens: true,
   comdex: {
