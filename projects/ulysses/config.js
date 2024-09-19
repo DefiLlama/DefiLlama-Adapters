c93faac31badb9959755abadcb2624b061ff6d0b
0xLightt <0xlightt@protonmail.com>
2024-09-19 16:41:12 +0100

    add ulysses adapter

---
+++ projects/ulysses/config.js
@@ -0,0 +1,66 @@
+// In Arbitrum
+const RootPort = "0x5399Eee5073bC1018233796a291Ffd6a78E26cbb";
+
+/**
+ * UI supported chains list
+ */
+const CHAINS = [
+  "ethereum",
+  "optimism",
+  "base",
+  "polygon",
+  "bsc",
+  "avax",
+  "metis",
+];
+
+/**
+ * Holds the layerzero chain ids to chain for the UI supported chains
+ */
+const EVM_CHAIN_ID_FROM_LZ_CHAIN_ID = {
+  [101]: "ethereum",
+  [111]: "optimism",
+  [184]: "base",
+  [137]: "polygon",
+  [102]: "bsc",
+  [106]: "avax",
+  [151]: "metis",
+};
+
+const nativeTokenPerChain = {
+  ["0xec32aAd0e8fc6851f4bA024B33dE09607190Ce9b"]: {
+    underlyingAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    tokenChain: "ethereum",
+  },
+  ["0xab57C3f001A749B340686E6DB3532792A76D7715"]: {
+    underlyingAddress: "0x4200000000000000000000000000000000000006",
+    tokenChain: "optimism",
+  },
+  ["0xa168bE53a546163f36210f3c440fbDC41BE99EE2"]: {
+    underlyingAddress: "0x4200000000000000000000000000000000000006",
+    tokenChain: "base",
+  },
+  ["0xabF1b68bD9d4672235AdD840F5C46544785f3fac"]: {
+    underlyingAddress: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+    tokenChain: "polygon",
+  },
+  ["0xa98b83e9529A5a49780ad07fbC6DA48101f41c17"]: {
+    underlyingAddress: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    tokenChain: "bsc",
+  },
+  ["0x53f671e1a31f803Dcf8070F70D60C17a4FBf3c72"]: {
+    underlyingAddress: "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
+    tokenChain: "avax",
+  },
+  ["0x9E60eB63Eaad46759A170B9eE5cC27E5Dd68C7e1"]: {
+    underlyingAddress: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
+    tokenChain: "metis",
+  },
+};
+
+exports.config = {
+  RootPort,
+  CHAINS,
+  EVM_CHAIN_ID_FROM_LZ_CHAIN_ID,
+  nativeTokenPerChain,
+};
---
+++ projects/ulysses/index.js
@@ -0,0 +1,107 @@
+const sdk = require("@defillama/sdk");
+const { getLogs } = require("../helper/cache/getLogs");
+const {
+  config: {
+    RootPort,
+    EVM_CHAIN_ID_FROM_LZ_CHAIN_ID,
+    nativeTokenPerChain,
+    CHAINS,
+  },
+} = require("./config");
+
+async function fetchBalancesPerBranch(api, block) {
+  const data = await getLogs({
+    api,
+    target: RootPort,
+    topics: [
+      "0xbc68f8c6bc7725cd153211b66209e3c2bc0706ec10427059be524f96db68247c",
+    ],
+    fromBlock: 241817312,
+    toBlock: block,
+    eventAbi:
+      "event LocalTokenAdded(address indexed underlyingAddress, address indexed localAddress, address indexed globalAddress, uint256 chainId)",
+  });
+
+  const branchTokensPerChain = data.reduce((memo, log) => {
+    const tokenChain = EVM_CHAIN_ID_FROM_LZ_CHAIN_ID[Number(log.args[3])];
+
+    if (!tokenChain) return memo;
+
+    const underlyingAddress = log.args[0];
+    const globalAddress = log.args[2];
+
+    return {
+      ...memo,
+      [globalAddress]: { underlyingAddress, tokenChain },
+    };
+  }, nativeTokenPerChain);
+
+  const globalAddresses = Object.keys(branchTokensPerChain);
+
+  const balances = await Promise.all(
+    globalAddresses.map(
+      async (target) =>
+        (
+          await sdk.api.erc20.totalSupply({
+            target,
+            block,
+            chain: api.chain,
+          })
+        ).output
+    )
+  );
+
+  return balances.reduce((memo, balance, i) => {
+    const globalAddress = globalAddresses[i];
+    const { underlyingAddress, tokenChain } =
+      branchTokensPerChain[globalAddress];
+
+    const chainBalances = memo[tokenChain] ?? {};
+
+    return {
+      ...memo,
+      [tokenChain]: {
+        ...chainBalances,
+        [tokenChain + ":" + underlyingAddress]: balance,
+      },
+    };
+  }, {});
+}
+
+let balancesPerBranch;
+let lastFetchedBlock;
+let isFetchingBalances = false;
+
+async function tvl(branchChain) {
+  const api = new sdk.ChainApi({
+    chain: "arbitrum",
+    timestamp: sdk.api.timestamp,
+  });
+  await api.getBlock();
+
+  const block = lastFetchedBlock;
+
+  if (!isFetchingBalances && (!block || block !== api.block)) {
+    lastFetchedBlock = api.block;
+    isFetchingBalances = true;
+    balancesPerBranch = await fetchBalancesPerBranch(api, api.block);
+    isFetchingBalances = false;
+  }
+
+  await new Promise((resolve) => {
+    if (isFetchingBalances) {
+      setTimeout(() => resolve(), 1000);
+    } else {
+      resolve();
+    }
+  });
+
+  return balancesPerBranch?.[branchChain];
+}
+
+CHAINS.forEach(
+  async (chain) =>
+    (module.exports[chain] = {
+      tvl: () => tvl(chain),
+    })
+);
