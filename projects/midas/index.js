497aa06d294bb9b932b2b06df0002e8feaf995c0
ahorner2 <ahorner123@yahoo.com>
2024-09-20 09:59:29 -0500

    Adding mBASIS token to TVL

--- projects/midas/index.js
+++ projects/midas/index.js
@@ -1,16 +1,33 @@
 async function tvl(api) {
-  const MTBILL_TOKEN_CONTRACT = "0xDD629E5241CbC5919847783e6C96B2De4754e438";
+  const contracts = [
+    {
+      token: "0xDD629E5241CbC5919847783e6C96B2De4754e438", // mTBILL contract
+      oracle: "0x056339C044055819E8Db84E71f5f2E1F536b2E5b", // mTBILL oracle
+    },
+    {
+      token: "0x2a8c22E3b10036f3AEF5875d04f8441d4188b656", // mBASIS contract
+      oracle: "0xE4f2AE539442e1D3Fb40F03ceEbF4A372a390d24", // mBASIS oracle
+    },
+  ];
 
-  const mtbillSupply = await api.call({
-    abi: "erc20:totalSupply",
-    target: MTBILL_TOKEN_CONTRACT,
-  });
+  let totalTvl = 0;
 
-  const rate = await api.call({
-    target: "0x056339C044055819E8Db84E71f5f2E1F536b2E5b", // mTBILL Oracle
-    abi: "function latestRoundData() external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)",
-  });
-  api.addCGToken("tether", (mtbillSupply / 1e18) * ((rate.answer / 1e8)));
+  for (const { token, oracle } of contracts) {
+    const supply = await api.call({
+      abi: "erc20:totalSupply",
+      target: token,
+    });
+
+    const rate = await api.call({
+      target: oracle,
+      abi: "function latestRoundData() external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)",
+    });
+
+    const tokenTvl = (supply / 1e18) * (rate.answer / 1e8);
+    totalTvl += tokenTvl;
+  }
+
+  api.addCGToken("tether", totalTvl);
 }
 
 const chains = ["ethereum"];
