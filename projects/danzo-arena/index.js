14a4d4cdb6d326052f5276ece3fb56e385424958
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-23 14:26:49 +0200

    update danzo #11712

--- projects/danzo-arena/index.js
+++ projects/danzo-arena/index.js
@@ -2,6 +2,7 @@ const { sumTokensExport } = require("../helper/chain/cardano");
 
 const scriptAdresses = [
   "addr1vx4caaf06swdn7cdfc7pu3sw235zvuhd02tw2np4uac9mwcp2vt4y", //Danzo Tokens locked inside the address for the DeFi game DANZO ARENA: https://cardanzoada.com/arena/
+  "addr1v858vfzl7hdqduqqa4vsj58nfy9njtw5q98q8tzzds58uncqjezd7", //Danzo Tokens locked inside the address for the DeFi game DANZO Game: https://cardanzoada.com/app/#/games
 ];
 
 module.exports = {
