b80af3c038292e515d0c0fe897cd233aca262635
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-23 11:41:39 +0200

    code refactor

--- projects/stakestone-btc/index.js
+++ projects/stakestone-btc/index.js
@@ -1,21 +1,17 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-
 const vaultABI = {
-    "getDepositAmounts": "function getDepositAmounts() view returns (address[], uint256[])"
+  "getDepositAmounts": "function getDepositAmounts() view returns (address[], uint256[])"
 }
 
 const VaultBSC = '0xc6f830BB162e6CFb7b4Bac242B0E43cF1984c853';
 
 const bscTvl = async (api) => {
-    const btclist = await api.call({ abi: vaultABI.getDepositAmounts, target: VaultBSC })
-    api.add(ADDRESSES.bsc.BTCB, btclist[1][0] / 10**18)
-    return api.sumTokens({ owner: VaultBSC, tokens: [ADDRESSES.bsc.BTCB] })
+  const [btclist] = await api.call({ abi: vaultABI.getDepositAmounts, target: VaultBSC })
+  return api.sumTokens({ owner: VaultBSC, tokens: btclist })
 }
 
-
 module.exports = {
-    start: 42326440,
-    bsc: {
-        tvl: bscTvl,
-    }
+  start: 42326440,
+  bsc: {
+    tvl: bscTvl,
+  }
 }
