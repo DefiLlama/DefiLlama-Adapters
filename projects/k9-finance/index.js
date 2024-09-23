b0c0a9a60c7cb7abe144ff3443e1a8bb3e8d7193
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-23 14:22:20 +0200

    refactor

--- projects/k9-finance/index.js
+++ projects/k9-finance/index.js
@@ -1,6 +1,6 @@
-const { stakings } = require("../helper/staking");
-const vestingSettingsAbi = require("./vesting-settings-abi.json");
-const BigNumber = require("bignumber.js");
+const { sumUnknownTokens } = require("../helper/unknownTokens");
+
+const vestingSettingsAbi = "function settings() view returns (bool lockedOnly, uint64 backingRatio, uint64 vestingRatio, uint64 vestingPeriod)"
 
 const BONE_TOKEN = "0x9813037ee2218799597d83D4a5B6F3b6778218d9";
 const BONE_CONTRACT = "0x3358FCA51d7C0408750FBbE7777012E0b67C027F";
@@ -13,68 +13,36 @@ const FARMING_FABRIC = "0x8ed1A7c4736b5835560b0f9E961B8E3581774D42";
 const VESTING = "0xf7384ba80A51979eC8cc0F17a843089ffD706f0a";
 
 async function tvl(api) {
-  const bonesBalance = await api.call({
-    abi: "uint256:getTotalPooledBONE",
-    target: BONE_CONTRACT,
-  });
-
-  api.addToken(BONE_TOKEN, bonesBalance);
+  const bonesBalance = await api.call({ abi: "uint256:getTotalPooledBONE", target: BONE_CONTRACT, });
+  // api.addToken(BONE_TOKEN, bonesBalance);
+  api.addCGToken('bone-shibaswap', bonesBalance / 1e18)
 }
 
 async function pool2(api) {
-  const poolsAddresses = await api.call({
-    abi: "address[]:getAllCreatedPools",
-    target: FARMING_FABRIC,
-  });
+  const poolsAddresses = await api.call({ abi: "address[]:getAllCreatedPools", target: FARMING_FABRIC, });
 
-  const poolsTokensPromise = api.multiCall({
-    abi: "address:pool",
-    calls: poolsAddresses.map((address) => ({ target: address })),
-  });
-
-  const poolsLiquidityPromise = api.multiCall({
-    abi: "uint256:intermediate",
-    calls: poolsAddresses.map((address) => ({ target: address })),
-  });
-
-  const [poolsTokens, poolsLiquidity] = await Promise.all([
-    poolsTokensPromise,
-    poolsLiquidityPromise,
-  ]);
-
-  api.addTokens(poolsTokens, poolsLiquidity);
+  const tokens = await api.multiCall({ abi: "address:pool", calls: poolsAddresses, });
+  const bals = await api.multiCall({ abi: "uint256:intermediate", calls: poolsAddresses, });
+  api.add(tokens, bals)
+  return sumUnknownTokens({ api, lps: tokens, useDefaultCoreAssets: true, resolveLP: true, })
 }
 
 async function vesting(api) {
-  const vestingSettingsPromise = api.call({
-    abi: vestingSettingsAbi,
-    target: VESTING,
-  });
+  const vestingSettings = await api.call({ abi: vestingSettingsAbi, target: VESTING, });
+  const esKNINEAddress = await api.call({ abi: "address:esKNINE", target: VESTING, });
 
-  const esKNINEAddressPromise = api.call({
-    abi: "address:esKNINE",
-    target: VESTING,
-  });
-
-  const [vestingSettings, esKNINEAddress] = await Promise.all([
-    vestingSettingsPromise,
-    esKNINEAddressPromise,
-  ]);
-
-  const esKNINEBalance = await api.call({
-    abi: "erc20:balanceOf",
-    target: esKNINEAddress,
-    params: [VESTING],
-  });
-
+  const esKNINEBalance = await api.call({ abi: "erc20:balanceOf", target: esKNINEAddress, params: VESTING, });
   const vestingRatio = vestingSettings[2];
 
-  const KNINEAmount = BigNumber(esKNINEBalance)
-    .multipliedBy(vestingRatio)
-    .div(100)
-    .toFixed(0);
-
+  const KNINEAmount = esKNINEBalance * vestingRatio / 100
   api.addToken(KNINE_TOKEN, KNINEAmount);
+  return sumUnknownTokens({ api, useDefaultCoreAssets: true, lps: ['0xF3452bCdEcaDa5E08ce56EC3D2BF8e35ECFFFF91'], })
+}
+async function staking(api) {
+  const bal = await api.call({ abi: "erc20:balanceOf", target: KNINE_TOKEN, params: REAL_YIELD_STAKING, });
+
+  api.addToken(KNINE_TOKEN, bal);
+  return sumUnknownTokens({ api, useDefaultCoreAssets: true, lps: ['0xF3452bCdEcaDa5E08ce56EC3D2BF8e35ECFFFF91'],})
 }
 
 module.exports = {
@@ -84,7 +52,7 @@ module.exports = {
     tvl,
   },
   shibarium: {
-    staking: stakings([REAL_YIELD_STAKING], KNINE_TOKEN),
+    staking,
     pool2,
     vesting,
   },
