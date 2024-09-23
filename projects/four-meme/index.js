375ed4a5712b2716e52364794c11891951c876a0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-23 21:13:10 +0200

    refactor

--- projects/four-meme/index.js
+++ projects/four-meme/index.js
@@ -7,10 +7,7 @@ const config = {
             tokenManager: "0xEC4549caDcE5DA21Df6E6422d448034B5233bFbC",
             tokenManager2: "0x5c952063c7fc8610FFDB798152D69F0B9550762b",
         }),
-        tokens: [ADDRESSES.null, ADDRESSES.bsc.USDT,
-            '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
-            '0x9eC02756A559700d8D9e79ECe56809f7bcC5dC27',
-            '0x5b1f874d0b0C5ee17a495CbB70AB8bf64107A3BD'],
+        tokens: [ADDRESSES.null, ADDRESSES.bsc.USDT,],
     },
 };
 
