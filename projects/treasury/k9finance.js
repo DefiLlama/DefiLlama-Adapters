acab4f520143354606e22f98b3db723eda17e7f5
realdealshaman <realdealshaman11@gmail.com>
2024-09-23 20:25:25 +0100

    add nulladdress

--- projects/treasury/k9finance.js
+++ projects/treasury/k9finance.js
@@ -6,11 +6,12 @@ const shibtreasury = "0x5C3d21D406226F17a06510F1CB9157BD9e751416"
 
 module.exports = treasuryExports({
   ethereum: {
+    tokens: [nullAddress],
     owners: [treasury],
     ownTokens: ["0x91fbb2503ac69702061f1ac6885759fc853e6eae"]
   },
   shibarium: {
-    tokens: ["0x839fdb6cc98342b428e074c1573adf6d48ca3bfd"],
+    tokens: [nullAddress,"0x839fdb6cc98342b428e074c1573adf6d48ca3bfd"],
     owners: [shibtreasury],
     ownTokens: ["0x91fbB2503AC69702061f1AC6885759Fc853e6EaE"]
   },
