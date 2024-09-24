d37e7f8a08ff3dbeb2d40e62f76098c0d7de769c
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2024-09-24 11:50:57 +0200

    Fix: Darkcrypto (masterChef logic) (#11729)

--- projects/darkcrypto/index.js
+++ projects/darkcrypto/index.js
@@ -1,34 +1,10 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const { stakingUnknownPricedLP } = require("../helper/staking");
-const farmUtils = require("./farm-utils");
 const vaultUtils = require("./vault-utils")
+const { pool2Balances } = require("./farm-utils");
 
-const sdk = require("@defillama/sdk");
 const sky = "0x9D3BBb0e988D9Fb2d55d07Fe471Be2266AD9c81c";
 const boardroom = "0x2e7d17ABCb9a2a40ec482B2ac9a9F811c12Bf630";
-
-
-async function pool2(timestamp, block, chainBlocks) {
-  // SKY POOL
-  const farmTvl = await farmUtils.farmLocked(chainBlocks["cronos"]);
-  let balances = {};
-
-  //add CRO balance in LP pool
-  sdk.util.sumSingleBalance(
-    balances,
-    "cronos:" + ADDRESSES.cronos.WCRO_1,
-    farmTvl["cronos:" + ADDRESSES.cronos.WCRO_1]
-  );
-
-  //add Dark and Sky balance in LP pool
-  sdk.util.sumSingleBalance(
-    balances,
-    "cronos:" + ADDRESSES.cronos.WCRO_1,
-    farmTvl["cronos:" + ADDRESSES.cronos.WCRO_1],
-  );
-
-  return balances;
-}
+const masterChef = "0x42B652A523367e7407Fb4BF2fA1F430781e7db8C"
 
 async function vault(api){
   return vaultUtils.vaultLocked(api)
@@ -38,13 +14,12 @@ module.exports = {
   doublecounted: true,
   cronos: {
     tvl:vault,
-    pool2,
+    pool2: (api) => pool2Balances(api, masterChef),
     staking: stakingUnknownPricedLP(
       boardroom,
       sky,
       "cronos",
       "0xaA0845EE17e4f1D4F3A8c22cB1e8102baCf56a77"
     ),
-    
   },
 };
