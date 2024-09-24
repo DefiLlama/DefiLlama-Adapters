3e0bed8f10d9bd814cb304f6472480c05569aa3b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-24 11:47:37 +0200

    Multibit (#11644)
    Co-authored-by: pipi-multibit <pipi@multibit.exchange>

--- projects/multibit/index.js
+++ projects/multibit/index.js
@@ -7,9 +7,10 @@ const BRIDGE_TOKENS = "https://api.multibit.exchange/support/token";
 
 const config = {
   // https://app.multibit.exchange/staking
-  ethereum: { stakingPool: "0x2EDfFbc62C3dfFD2a8FbAE3cd83A986B5bbB5495", tokens: [ADDRESSES.null, ADDRESSES.ethereum.USDT] },
+  ethereum: { stakingPool: "0x2EDfFbc62C3dfFD2a8FbAE3cd83A986B5bbB5495", tokens: [ADDRESSES.null, ADDRESSES.ethereum.USDT], chainKey: 'eth' },
   bsc: {},
   polygon: {},
+  bouncebit: { chainKey: 'bb'},
 }
 module.exports = {
   methodology: `Tokens bridged via MultiBit are counted as TVL`,
@@ -20,18 +21,17 @@ module.exports = {
         'bc1pyyms2ssr0hagy5j50r5n689e6ye0626v3c98j5fw0jk6tz3vrgts7nt56g',  // cold wallet
         'bc1qmcrpqanjnrw58y0fvq08fqchgxv5aylctew7vxlkalfns3rpedxsx4hxpu',  // cold wallet
       ],
-      blacklistedTokens: ['MUBI', 'BSSB']
+      blacklistedTokens: ['MUBI', 'BSSB', 'savm'] // more SAVM is bridged than circulating supply according to coingecko & etherscan
     }),
 
   },
 }
 Object.keys(config).forEach(chain => {
-  const { stakingPool, tokens = [ADDRESSES.null] } = config[chain]
+  const { stakingPool, tokens = [ADDRESSES.null], chainKey = chain } = config[chain]
   module.exports[chain] = {
     tvl: async (api) => {
       const data = await getConfig('multibit', BRIDGE_TOKENS)
-      const key = chain === 'ethereum' ? 'eth': chain
-      const owner = data.find(v => v.chain === key)?.real?.contract
+      const owner = data.find(v => v.chain === chainKey)?.real?.contract
       if (!owner) return {}
       return api.sumTokens({ owner, tokens, })
     }
