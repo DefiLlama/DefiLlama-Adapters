0796b810c99079031f225527e9bc8fccfe09e477
rajeebkm <rkm2541997@gmail.com>
2024-09-24 15:15:40 +0530

    TVL adapter updated

--- projects/FilamentFinance/index.js
+++ projects/FilamentFinance/index.js
@@ -2,19 +2,20 @@ const USDC_CONTRACT = '0x3894085Ef7Ff0f0aeDf52E2A2704928d1Ec074F1';
 const FILAMENT_VAULT_CONTRACT = '0xbeB6A6273c073815eBe288d2dF4e5E8bc027DA11';
 
 async function tvl(api) {
-  const usdcBalanceInFilamentVault = await api.call({
-    abi: 'erc20:balanceOf',
-    target: USDC_CONTRACT,
-    params: [FILAMENT_VAULT_CONTRACT],
-  });
+    const tvlUSDC = await api.call({ abi: 'uint256:getUsdBalance', target: FILAMENT_VAULT_CONTRACT });
+    const totalBorrowedUsd = await api.call({ abi: 'uint256:totalBorrowedUSD', target: FILAMENT_VAULT_CONTRACT });
+    
+    // Divide the values by 10^12 to get the correct decimal representation
+    const tvlUSDCInDecimals = tvlUSDC / (10 ** 12);
+    const totalBorrowedUsdInDecimals = totalBorrowedUsd / (10 ** 12);
+    const tvl = tvlUSDCInDecimals + totalBorrowedUsdInDecimals;
+    api.add(USDC_CONTRACT, tvl);
 
-  api.add(USDC_CONTRACT, usdcBalanceInFilamentVault)
 }
 
 module.exports = {
-  methodology: 'Get the current USDC balance available in the pool.',
-  start: 100601155,
-  sei: {
-    tvl
-  }
-};
+    methodology: "TVL is being calculated as the sum of available COMB Pool balance in the vault and total borrowed USDC from the pool by users.",
+    sei:{
+        tvl,
+    },
+}
