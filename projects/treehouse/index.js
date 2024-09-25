edf6f6f42d555a7a95454298f3ef97719b33027c
irlpotato <166589843+irlpotato@users.noreply.github.com>
2024-09-25 11:13:25 +0800

    feat: update treehouse adapters

--- projects/treehouse/index.js
+++ projects/treehouse/index.js
@@ -1,9 +1,21 @@
-const ADDRESSES = require('../helper/coreAssets.json');
-const { sumTokensExport } = require('../helper/unwrapLPs');
+const ADDRESSES = require('../helper/coreAssets.json')
+
+const IAU_tETH = '0x1B6238E95bBCABEE58997c99BaDD4154ad68BA92'
+
+async function ethTvl(api) {
+    const iauSupply = await api.call({
+      abi: 'erc20:totalSupply',
+      target:IAU_tETH,
+    });
+  
+    api.add(ADDRESSES.ethereum.WSTETH, iauSupply)
+  }
+  
 
 module.exports = {
-  start: 1725926400, // Tuesday, September 10, 2024 12:00:00 AM
-  ethereum: {
-    tvl: sumTokensExport({ owner: '0x551d155760ae96050439AD24Ae98A96c765d761B', tokens: [ADDRESSES.ethereum.WSTETH], }),
-  }
-} 
+    methodology: 'Staked tokens are counted as TVL based on the chain that they are staked on and where the liquidity tokens are issued.',
+    start: 1725926400, // Tuesday, September 10, 2024 12:00:00 AM
+    ethereum: {
+      tvl: ethTvl,
+    }
+  }; 
