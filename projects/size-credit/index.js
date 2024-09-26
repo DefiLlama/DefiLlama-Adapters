60816f2f37e014ad5eda9f973c2f273372091a94
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-09-26 10:23:57 -0300

    Prepare adapter for new markets

--- projects/size-credit/index.js
+++ projects/size-credit/index.js
@@ -1,23 +1,27 @@
 const { sumTokensExport } = require('../helper/unwrapLPs')
 const ADDRESSES = require('../helper/coreAssets.json')
 
-const AUSDC_CONTRACT = '0x4e65fe4dba92790696d040ac24aa414708f5c0ab'
-const SZDEBT_CONTRACT = '0xb0a00c4b3d77c896f46dc6b204695e22de7a185d'
-const SIZE_PROXY_CONTRACT = '0xC2a429681CAd7C1ce36442fbf7A4a68B11eFF940'
-const tokens = [
-  ADDRESSES.base.WETH,
-  AUSDC_CONTRACT,
-]
-
-async function borrowed(api) {
-  const totalDebt = await api.call({ abi: 'erc20:totalSupply', target: SZDEBT_CONTRACT, });
-
-  return api.add(ADDRESSES.base.USDbC, totalDebt)
-}
-
-module.exports = {
+const config = {
   base: {
-    tvl: sumTokensExport({ tokens, owner: SIZE_PROXY_CONTRACT }),
-    borrowed
+    WETH_USDC: {
+      Size: '0xC2a429681CAd7C1ce36442fbf7A4a68B11eFF940',
+      CollateralToken: ADDRESSES.base.WETH,
+      BorrowToken: ADDRESSES.base.USDbC,
+      BorrowAToken: '0x4e65fe4dba92790696d040ac24aa414708f5c0ab',
+      DebtToken: '0xb0a00c4b3d77c896f46dc6b204695e22de7a185d'
+    },
   }
 }
+
+async function getBorrowed(api, size, borrowToken) {
+  const totalDebt = await api.call({ abi: 'erc20:totalSupply', target: size });
+  return api.add(borrowToken, totalDebt)
+}
+
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = {
+    tvl: sumTokensExport({ tokens: [config[chain].CollateralToken, config[chain].BorrowAToken], owner: config[chain].Size }),
+    borrowed: (api) => getBorrowed(api, config[chain].BorrowToken)
+  }
+})
