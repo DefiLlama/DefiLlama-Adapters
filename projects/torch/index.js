a68abc1f213db6df924716c732d67e223825d696
grace0950 <pinyan0950@gmail.com>
2024-09-26 19:54:27 +0800

    Refactor torch/index.js to update TVL calculation

--- projects/torch/index.js
+++ projects/torch/index.js
@@ -1,14 +1,30 @@
 const ADDRESSES = require("../helper/coreAssets.json")
-const { sumTokensExport } = require('../helper/chain/ton')
+const { sumTokens } = require('../helper/chain/ton')
+const { get } = require("../helper/http")
 
-const ROUTER = 'EQCaEOMOR2SRcXTVSolw--rY62ghCoCRjn4Is3bBdnqYwIVZ'
+const DEX = 'EQCaEOMOR2SRcXTVSolw--rY62ghCoCRjn4Is3bBdnqYwIVZ'
+const YIELD_ROUTER = 'EQDYU6gxBxFT3arWR-N9RFTfqAoQKyWk4JMgTBNOM2KL18Dw'
+
+async function tvl(api) {
+  const tokenTvl = await get('https://yield-api.torch.finance/tvl')
+  
+  const tvl = tokenTvl.reduce((acc, data) => {
+    acc += Number(data.tvl) / 1e9
+    return acc
+  }, 0)
+
+  await sumTokens({ 
+    api, 
+    owners: [DEX, YIELD_ROUTER],
+    tokens: [ADDRESSES.ton.TON], 
+  });
+
+  api.add(ADDRESSES.ton.USDT, tvl * 1e6)
+}
 
 module.exports = {
   timetravel: false,
   ton: {
-    tvl: sumTokensExport({
-      owners: [ROUTER],
-      tokens: [ADDRESSES.ton.TON],
-    })
+    tvl: tvl
   },
 }
