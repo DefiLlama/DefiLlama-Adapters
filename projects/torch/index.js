e93ddb82797475272c4d20b6da0a4eaf9ac5dc47
grace0950 <pinyan0950@gmail.com>
2024-09-27 01:46:47 +0800

    Add methodology in torch tvl

--- projects/torch/index.js
+++ projects/torch/index.js
@@ -1,6 +1,7 @@
 const ADDRESSES = require("../helper/coreAssets.json")
 const { sumTokens } = require('../helper/chain/ton')
 const { get } = require("../helper/http")
+const { methodology } = require("../parraton")
 
 const DEX = 'EQCaEOMOR2SRcXTVSolw--rY62ghCoCRjn4Is3bBdnqYwIVZ'
 const YIELD_ROUTER = 'EQDYU6gxBxFT3arWR-N9RFTfqAoQKyWk4JMgTBNOM2KL18Dw'
@@ -24,6 +25,10 @@ async function tvl(api) {
 
 module.exports = {
   timetravel: false,
+  methodology: `The TVL calculation for Torch includes both the general jettons 
+  balance and LP jettons balance. The price of general jettons is obtained via tonApi, 
+  while the price of LP jettons is calculated using on-chain data (reserve0, reserve1, 
+  and total LP supply). The LP jettons price calculation is handled on the Torch API side.`.trim(),
   ton: {
     tvl: tvl
   },
