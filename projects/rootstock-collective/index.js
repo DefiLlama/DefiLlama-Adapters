69149eff0fdf186a0beb391043346017a58c18fa
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-27 09:44:36 +0200

    mark it as staking

--- projects/rootstock-collective/index.js
+++ projects/rootstock-collective/index.js
@@ -2,6 +2,7 @@ const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
   rsk: {
-    tvl: sumTokensExport({ owner: '0x5db91e24BD32059584bbDb831A901f1199f3d459', tokens: ['0x2aCc95758f8b5F583470bA265Eb685a8f45fC9D5']})
+    tvl: () => ({}),
+    staking: sumTokensExport({ owner: '0x5db91e24BD32059584bbDb831A901f1199f3d459', tokens: ['0x2aCc95758f8b5F583470bA265Eb685a8f45fC9D5']})
   }
 }
