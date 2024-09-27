ecd9c98bf21e8c40a89855e84b1ad9afec4678cb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-27 10:29:29 +0200

    Revert "Fix: hpdex (outdated)" (#11773)

--- projects/hpdex/index.js
+++ projects/hpdex/index.js
@@ -1,7 +1,6 @@
 const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
-  deadFrom: '2024-09-22',
   misrepresentedTokens: true,
   methodology: "Factory address (0xE1d563BcFD4E2a5A9ce355CC8631421186521aAA) is used to find the LP pairs. TVL is equal to the liquidity on the AMM.",
   hpb: {
