e886c33206eeb8fc3c1c965345f26dc24138ba3d
crispyfisherman <hsu86110@gmail.com>
2024-10-01 15:29:29 +0700

    Update TVL data (#11792)

--- projects/doubleup/index.js
+++ projects/doubleup/index.js
@@ -1,8 +1,6 @@
-const ADDRESSES = require("../helper/coreAssets.json");
 const sui = require("../helper/chain/sui");
 
 const UNI_HOUSE_OBJ_ID = "0x75c63644536b1a7155d20d62d9f88bf794dc847ea296288ddaf306aa320168ab"
-const SUI = ADDRESSES.sui.SUI;
 
 async function tvl(api) {
   const unihouseDynamicFields = await sui.getDynamicFieldObjects({
@@ -15,15 +13,22 @@ async function tvl(api) {
 
   const unihouseIdList = unihouseList.map((house) => house.fields.id.id);
 
-  // console.log(unihouseIdList);
-
   for (const id of unihouseIdList) {
     const house = await sui.getObject(id);
 
-    const pipeDebt = house?.fields?.pipe_debt?.fields?.value;
-    const totalSui = Number(pipeDebt) + Number(house?.fields?.pool);
+    const houseType = house?.type;
+    const coinType = houseType.split("<")[1].split(">")[0];
 
-    api.add(SUI, totalSui);
+    const housePipeDebt = house?.fields?.house_pipe_debt?.fields?.value;
+    const housePool = house?.fields?.house_pool;
+    const houseTvl = Number(housePipeDebt) + Number(housePool);
+
+    api.add(coinType, houseTvl);
+
+    const pipeDebt = house?.fields?.pipe_debt?.fields?.value;
+    const _tvl = Number(pipeDebt) + Number(house?.fields?.pool);
+
+    api.add(coinType, _tvl);
   }
 }
 
