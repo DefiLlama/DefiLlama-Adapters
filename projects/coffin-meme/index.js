bbd406a0a53d1c74f3e938d563e719d1782d545f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-03 20:36:07 +0200

    minor fix

--- projects/coffin-meme/index.js
+++ projects/coffin-meme/index.js
@@ -6,6 +6,6 @@ const coffinScAddr = "EQBozwKVDya9IL3Kw4mR5AQph4yo15EuMdyX8nLljeaUxrpM"
 module.exports = {
     methodology: 'Counts Coffin.Meme smartcontract TON & jettons balance as TVL.',
     ton: {
-        tvl: sumTokensExport({ owner: coffinScAddr, tokens: [ADDRESSES.null], onlyWhitelistedTokens: true, }),
+        tvl: sumTokensExport({ owner: coffinScAddr, tokens: [ADDRESSES.null], }),
     }
 }
