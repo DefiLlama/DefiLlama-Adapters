ea5451eb9fe1ed17370a8b0836b10350462576ad
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-03 17:48:52 +0200

    minor fix

--- projects/kittypunch/index.js
+++ projects/kittypunch/index.js
@@ -1,12 +1,5 @@
-const { uniTvlExport } = require("../helper/calculateUniTvl")
+const { uniTvlExports } = require("../helper/unknownTokens")
 
-// const PUNCH_SWAP_V2_ROUTER = "0xf45AFe28fd5519d5f8C1d4787a4D5f724C0eFa4d"
-const PUNCH_SWAP_V2_FACTORY = "0x29372c22459a4e373851798bFd6808e71EA34A71"
-
-module.exports = {
-  misrepresentedTokens: true,
-  methodology: "KittenSwap is a Uniswap V2-like DEX on Flow.",
-  flow: {
-    tvl: uniTvlExport(PUNCH_SWAP_V2_FACTORY, "flow", true)
-  }
-}
+module.exports = uniTvlExports({
+  flow: "0x29372c22459a4e373851798bFd6808e71EA34A71"
+})
