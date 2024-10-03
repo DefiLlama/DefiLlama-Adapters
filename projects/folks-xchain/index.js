d747b61472a44611d57d23c48247eef599347dcc
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-03 10:29:25 +0200

    code refactor

--- projects/folks-xchain/index.js
+++ projects/folks-xchain/index.js
@@ -1,47 +1,35 @@
-const { multiCall } = require("@defillama/sdk/build/abi/abi2");
-const { HubPools, HubPoolAbi, HubChainId } = require("./constants");
+const sdk = require('@defillama/sdk')
+const { HubPools, HubPoolAbi, } = require("./constants");
 
 async function tvl(api) {
-  const HubPoolsChain = HubPools[api.chain]
-
-  const depositsData = await multiCall({
-    calls: HubPoolsChain.map(pool => ({
-      target: pool.poolAddress,
-    })),
-    abi: HubPoolAbi.getDepositData,
-    chain: HubChainId
-  });
-
-  HubPoolsChain.forEach((pool, idx) => {
-    api.add(pool.tokenAddress, Number(depositsData[idx][1]))
-  })
+  const tokensAndOwners = HubPools[api.chain].map(pool => [pool.tokenAddress, pool.chainPoolAddress ?? pool.poolAddress])
+  return api.sumTokens({ tokensAndOwners })
 }
 
 async function borrowed(api) {
   const HubPoolsChain = HubPools[api.chain]
-  const targets = HubPoolsChain.map(pool => ({ target: pool.poolAddress }))
+  let chainApi = api
+  if (api.chain !== 'avax') {
+    chainApi = new sdk.ChainApi({ chain: 'avax', timestamp: api.timestamp })
+    await chainApi.getBlock()
+  }
+  const targets = HubPoolsChain.map(pool => pool.poolAddress)
 
   const [varBorrowsData, stableBorrowsData] = await Promise.all([
-    await multiCall({
-      calls: targets,
-      abi: HubPoolAbi.getVariableBorrowData,
-      chain: HubChainId
-    }),
-    await multiCall({
-      calls: targets,
-      abi: HubPoolAbi.getStableBorrowData,
-      chain: HubChainId
-    })
+    await chainApi.multiCall({ calls: targets, abi: HubPoolAbi.getVariableBorrowData, }),
+    await chainApi.multiCall({ calls: targets, abi: HubPoolAbi.getStableBorrowData, })
   ]);
 
   HubPoolsChain.forEach((pool, idx) => {
     api.add(pool.tokenAddress, Number(varBorrowsData[idx][3]) + Number(stableBorrowsData[idx][8]))
   })
+  return api.getBalances()
 }
 
 module.exports = {
   methodology: "The Folks Finance xChain states are saved in the Hub chain contracts i.e. Avalanche; TVL counts deposited total amount values for each pool, borrowed counts variable and stable borrowed total amount values for each pool",
-  avax: { tvl, borrowed },
-  ethereum: { tvl, borrowed },
-  base: { tvl, borrowed },
-};
+}
+
+Object.keys(HubPools).forEach(chain => {
+  module.exports[chain] = { tvl, borrowed, }
+})
