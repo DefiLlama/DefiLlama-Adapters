b6ea2758cf03cc41a9238bdded32a04d60d6d7dc
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-04 13:45:14 +0200

    code refactor

--- projects/yay-staking/index.js
+++ projects/yay-staking/index.js
@@ -1,85 +1,16 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-
-const getHistoricalPricePerStoneABI = {
-    "inputs": [
-        {
-            "internalType": "uint256",
-            "name": "round",
-            "type": "uint256"
-        }
-    ],
-    "name": "getHistoricalPricePerStone",
-    "outputs": [
-        {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-};
-
-const getExpectedETHAmountfromAgETHABI = {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "getExpectedETHAmountfromAgETH",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-};
-
 const yayStoneAddress = '0xe86142af1321eaac4270422081c1EdA31eEcFf0c'
 const yayAgETHAddress = '0x0341d2c2CE65B62aF8887E905245B8CfEA2a3b97'
 
-const yayStoneTvl = async (api) => {
-    const yayStoneAmount = await api.call({ abi: 'erc20:totalSupply', target: yayStoneAddress })
-    const currentRound = await api.call({ abi: 'uint256:getCurrentRound', target: yayStoneAddress })
-    const price = await api.call({
-        abi: getHistoricalPricePerStoneABI,
-        target: yayStoneAddress,
-        params: [currentRound-1],
-      });
-
-    const ethAmount = yayStoneAmount * price / 1e18
-
-    return ethAmount
-}
-
-const yayAgETHTvl = async (api) => {
-    const yayAgETHAmount = await api.call({ abi: 'erc20:totalSupply', target: yayAgETHAddress })
-    const price = await api.call({
-        abi: getExpectedETHAmountfromAgETHABI,
-        target: yayAgETHAddress,
-        params: [BigInt("1000000000000000000")],
-      });
-
-    const ethAmount = yayAgETHAmount * price / 1e18
-
-    return ethAmount
-}
-
-const totalTvl = async (api) => {
-    const yayStoneTvlResult = await yayStoneTvl(api);
-    const yayAgETHTvlResult = await yayAgETHTvl(api);
-    
-    return api.add(ADDRESSES.null, yayStoneTvlResult + yayAgETHTvlResult);
+const tvl = async (api) => {
+  return api.sumTokens({
+    tokensAndOwners: [
+      ['0x7122985656e38bdc0302db86685bb972b145bd3c', yayStoneAddress],
+      ['0xe1b4d34e8754600962cd944b535180bd758e6c2e', yayAgETHAddress],
+    ]
+  })
 }
 
 module.exports = {
-    start: 1722488340,
-    ethereum: {
-        tvl: totalTvl
-    },
+  start: 1722488340,
+  ethereum: { tvl },
 }
