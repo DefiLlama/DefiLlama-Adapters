b168e25e24e312f27654ca215e42a657bc4bf111
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-05 16:32:19 +0200

    code refactor

--- projects/mswap/index.js
+++ projects/mswap/index.js
@@ -1,19 +1,5 @@
-const { getUniTVL } = require('../helper/unknownTokens');
-const sdk = require('@defillama/sdk');
+const { uniTvlExports } = require('../helper/unknownTokens');
 
-const factory = '0x338bCC4efd3cA000D123d7352b362Fc6D5B3D829';
-const chain = 'matchain';
-
-const ammTvl = getUniTVL({
-  chain,
-  factory,
-  useDefaultCoreAssets: false,
-  fetchBalances: true,
-});
-
-module.exports = {
-  matchain: {
-    tvl: ammTvl,
-  },
-  misrepresentedTokens: true,
-};
+module.exports = uniTvlExports({
+  matchain: '0x338bCC4efd3cA000D123d7352b362Fc6D5B3D829'
+})
