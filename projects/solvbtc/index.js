e61bffa8e9f385d7184d7d31be3bccf10e0f2883
charq <73696209+buchaoqun@users.noreply.github.com>
2024-10-05 22:16:01 +0800

    Add Bitcoin Address (#11830)

--- projects/solvbtc/index.js
+++ projects/solvbtc/index.js
@@ -1,18 +1,6 @@
-const abi = require("./abi.json");
-const sdk = require('@defillama/sdk');
 const { getConfig } = require("../helper/cache");
-const { cachedGraphQuery } = require("../helper/cache");
 const { sumTokens2, } = require("../helper/unwrapLPs");
 const { sumTokensExport, } = require('../helper/sumTokens');
-
-// The Graph
-const graphUrlList = {
-  ethereum: 'https://api.studio.thegraph.com/query/40045/solv-payable-factory-prod/version/latest',
-  bsc: 'https://api.studio.thegraph.com/query/40045/solv-payable-factory-bsc/version/latest',
-  arbitrum: 'https://api.studio.thegraph.com/query/40045/solv-payable-factory-arbitrum/version/latest',
-  mantle: 'https://api.0xgraph.xyz/api/public/65c5cf65-bd77-4da0-b41c-cb6d237e7e2f/subgraphs/solv-payable-factory-mantle/-/gn',
-  merlin: 'http://solv-subgraph-server-alb-694489734.us-west-1.elb.amazonaws.com:8000/subgraphs/name/solv-payable-factory-merlin',
-}
 
 const solvbtcListUrl = 'https://raw.githubusercontent.com/solv-finance-dev/slov-protocol-defillama/main/solvbtc.json';
 
@@ -20,7 +8,8 @@ const bitcionOwners = [
   'bc1pjrxeuc9f3zqtx92s3mnf6202894jzufswur957l6s04rjns6dumsyh6u89',
   'bc1qdpwl80flfh3k6h6sumzwgws3ephkrmx307hk64',
   'bc1q5pzsptd5whcljevzyztavuqru0hugd5ymgx5ezksdqug3ztrjvmqauys2q',
-  'bc1q437jw8wqph854vf9dwxy4c2u6daveupjm5dqptj469gxw6vcpp0qfpr0mh'
+  'bc1q437jw8wqph854vf9dwxy4c2u6daveupjm5dqptj469gxw6vcpp0qfpr0mh',
+  'bc1q47ur7u0xh943s44kktvhr602sm29exylzn43ru'
 ]
 
 async function tvl(api) {
