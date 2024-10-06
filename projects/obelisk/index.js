01d0bea68c43804729296b7a4fffe15cb52ee6a6
zhenhao goh <kenneth.gzhao@outlook.com>
2024-10-06 16:00:22 +0800

    updated BTC address and evidences for oBTC

--- projects/obelisk/index.js
+++ projects/obelisk/index.js
@@ -1,16 +1,33 @@
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
-const sdk = require("@defillama/sdk");
+const { sumTokens } = require('../helper/chain/bitcoin')
+const sdk = require('@defillama/sdk')
+
+const staticAddresses = [
+    'bc1p0tr3dgulgpx43dkktjxy8z2azz6yvx4j7s0lelj67tlwct0wnqtqeakfer',
+    '14ejzLtUSMsjZE8Pp2LUhX3Pf7BbXPeZyP'
+]
+
+const abi = {
+    getCustodyAddrInfo: "function getCustodyAddrInfo() view returns (tuple(string mark, string btcAddr)[])"
+}
+
+async function tvl() {
+    const api = new sdk.ChainApi({ chain: 'ethereum' })
+
+    const addrInfos = await api.call({
+        abi: abi.getCustodyAddrInfo,
+        target: '0x9F836f8A27F1579258388BFab16ab16E278B1a2C'
+    })
+
+    const btcAddresses = addrInfos.map(info => info.btcAddr)
+
+    btcAddresses.forEach(addr => staticAddresses.push(addr))
+
+    return sumTokens({ owners: staticAddresses })
+}
 
 module.exports = {
-  bitcoin: {
-    tvl: async (api, block) => {
-    const MINT_OBTC_TOKEN_CONTRACT = '0xB1e12802831Da99D2d47b6a55049D69bf7De0e3C';
-        let result = await sdk.api.erc20.totalSupply({ target: MINT_OBTC_TOKEN_CONTRACT, block: block });
-        console.log(result);
-        return {
-            [MINT_OBTC_TOKEN_CONTRACT]: ((result && result.output) || 0),
-        }
+    timetravel: false,
+    bitcoin: {
+        tvl,
     }
-  },
-  methodology: `TVL for oBTC consists of the BTC deposits in custody that were used to mint oBTC`,
 }
