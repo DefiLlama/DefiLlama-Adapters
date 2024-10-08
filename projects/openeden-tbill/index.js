4f7273d4a2dd971504596d53ed13c73458a2e94f
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2024-10-08 10:09:59 +0200

    fix: Openeden xrpl (#11843)

--- projects/openeden-tbill/index.js
+++ projects/openeden-tbill/index.js
@@ -1,12 +1,14 @@
 const { getCache, } = require("../helper/cache");
 const ADDRESSES = require('../helper/coreAssets.json')
 
+const tbill = "0xdd50C053C096CB04A3e3362E2b622529EC5f2e8a"
+
 function getTimeNow() {
   return Math.floor(Date.now() / 1000);
 }
 
 async function tvl(api) {
-  let contract = '0xdd50C053C096CB04A3e3362E2b622529EC5f2e8a'
+  let contract = tbill
   if (api.chain === 'arbitrum') contract = '0xF84D28A8D28292842dD73D1c5F99476A80b6666A'
   const [bal, token] = await api.batchCall([
     { abi: 'uint256:totalAssets', target: contract },
@@ -23,7 +25,7 @@ async function ripplTvl (api) {
   let { lastDataUpdate, tvl } = await getCache(projectKey, cacheKey)
   if (!lastDataUpdate || timeNow - lastDataUpdate > aDayInSeconds || !tvl) 
     throw new Error("stale/missing tvl data");
-  api.add(ADDRESSES.ethereum.USDC, tvl * 10 ** 6, { skipChain: true })
+  api.add(tbill, tvl * 10 ** 6, { skipChain: true })
 }
 
 module.exports = {
