7a5a208e6192cb3d8144df1512ff88ebf1e57229
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-11 23:30:56 +0200

    fix usdx

--- projects/usdx/index.js
+++ projects/usdx/index.js
@@ -1,19 +1,11 @@
-const sdk = require("@defillama/sdk");
 const usdxAddress = "0xf3527ef8dE265eAa3716FB312c12847bFBA66Cef";
 const chains = ["ethereum", "bsc", "arbitrum"];
 
 chains.forEach((chain) => {
   module.exports[chain] = {
-    tvl: async (api, block) => {
-      const res = await sdk.api.erc20.totalSupply({
-        target: usdxAddress,
-        block,
-        chain: chain,
-        decimals: 18,
-      });
-      return {
-        "usdx-money-usdx": res.output,
-      };
+    tvl: async (api) => {
+      const supply = await api.call({  abi: 'erc20:totalSupply', target: usdxAddress})
+      api.addCGToken('usdx-money-usdx', supply/1e18)
     },
-  };
-});
+  }
+})
