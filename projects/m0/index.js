4a7734489ebedaba49f826a9584c91b9cee17dfe
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-15 01:45:13 +0200

    minor fix

--- projects/m0/index.js
+++ projects/m0/index.js
@@ -2,13 +2,13 @@ const { getLogs2 } = require('../helper/cache/getLogs')
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const minterGateway = '0xf7f9638cb444d65e5a40bf5ff98ebe4ff319f04e'
-const eventAbi = 'event CollateralUpdated(address indexed minter, uint240 collateral, uint240 totalResolvedCollateralRetrieval, bytes32 indexed metadataHash, uint40 timestamp)'
+const eventAbi = "event MinterActivated(address indexed minter, address indexed caller)"
 const abi = 'function collateralOf(address minter_) view returns (uint240)'
 
 const tvl = async (api) => {
-  const logs = await getLogs2({ api, target: minterGateway, eventAbi, fromBlock: 19818447 })
-  const minters = [...new Set(logs.map(log => log[0]))];
-  const collateralOf = await api.multiCall({ calls: minters.map((m) => ({ target: minterGateway, params: [m]})), abi })
+  const logs = await getLogs2({ api, target: minterGateway, eventAbi, fromBlock: 19818447, extraKey: 'MinterActivated' })
+  const minters = [...new Set(logs.map(log => log.minter))];
+  const collateralOf = await api.multiCall({ target: minterGateway, calls: minters, abi })
   api.add(ADDRESSES.ethereum.USDC, collateralOf)
 }
 
