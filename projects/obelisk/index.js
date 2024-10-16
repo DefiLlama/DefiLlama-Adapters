1fadbdeb607d772192e44469761eb7a5d25152ea
chainupk <121842069+chainupk@users.noreply.github.com>
2024-10-16 21:53:45 +0800

    Included an additional address in our TVL (#11928)
    Co-authored-by: zhenhao goh <kenneth.gzhao@outlook.com>
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/obelisk/index.js
+++ projects/obelisk/index.js
@@ -3,7 +3,8 @@ const sdk = require('@defillama/sdk')
 
 const staticAddresses = [
     'bc1p0tr3dgulgpx43dkktjxy8z2azz6yvx4j7s0lelj67tlwct0wnqtqeakfer',
-    '14ejzLtUSMsjZE8Pp2LUhX3Pf7BbXPeZyP'
+    '14ejzLtUSMsjZE8Pp2LUhX3Pf7BbXPeZyP',
+    'bc1q0hapsvdtqfeyw9fjacey9350k8zu552p0khyhj'
 ]
 
 const abi = {
