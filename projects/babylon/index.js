4409ed1a6698eb85bd3cd21f617ad2d2aca996d7
domodata <137194840+domodata@users.noreply.github.com>
2024-10-16 11:42:19 -0600

    Update index.js

--- projects/babylon/index.js
+++ projects/babylon/index.js
@@ -1,20 +1,28 @@
-// Babylon Chain TVL Adapter
-
 const axios = require('axios');
 
 async function tvl(api) {
-  // Call the Babylon staking API to get the TVL data
-  const response = await axios.get('https://staking-api.babylonlabs.io/v1/stats');
-  const activeTvl = response.data.data.active_tvl;
+  try {
+    const response = await axios.get('https://staking-api.babylonlabs.io/v1/stats');
+    const activeTvl = response.data.data.active_tvl;
 
-  // Add the active TVL to the balances
-  api.add('bitcoin', activeTvl);
+    
+    console.log('Active TVL (in satoshis):', activeTvl);
+
+    
+    if (activeTvl) {
+      api.add('bitcoin', activeTvl);
+    } else {
+      console.error('No active TVL returned from API.');
+    }
+  } catch (error) {
+    console.error('Error fetching TVL:', error);
+  }
 }
 
 module.exports = {
   methodology: 'The TVL is calculated by taking the total Bitcoin (in satoshis) locked in the Babylon Chain staking protocol.',
   start: 1729000000,
-  timetravel: false,  // Disallow backfill as the API doesn't support timestamps
+  timetravel: false, 
   misrepresentedTokens: false,
   bitcoin: {
     tvl,
