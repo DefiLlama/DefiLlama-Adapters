5f476b875f0f70851e664b2f3b53a5f0f9bba617
g1ntoki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-17 03:08:56 +0200

    minor refactor

--- projects/seer/index.js
+++ projects/seer/index.js
@@ -1,74 +1,61 @@
 const config = {
   'ethereum': {
-      marketFactory: ['0x1F728c2fD6a3008935c1446a965a313E657b7904'],
-      marketView: '0xAb797C4C6022A401c31543E316D3cd04c67a87fC',
-      collateralToken: '0x83F20F44975D03b1b09e64809B757c47f942BEeA'
+    marketFactory: ['0x1F728c2fD6a3008935c1446a965a313E657b7904'],
+    marketView: '0xAb797C4C6022A401c31543E316D3cd04c67a87fC',
+    collateralToken: '0x83F20F44975D03b1b09e64809B757c47f942BEeA'
   },
-  'gnosis': {
-      marketFactory: ['0x83183DA839Ce8228E31Ae41222EaD9EDBb5cDcf1'],
-      marketView: '0x995dC9c89B6605a1E8cc028B37cb8e568e27626f',
-      collateralToken: '0xaf204776c7245bf4147c2612bf6e5972ee483701'
+  'xdai': {
+    marketFactory: ['0x83183DA839Ce8228E31Ae41222EaD9EDBb5cDcf1'],
+    marketView: '0x995dC9c89B6605a1E8cc028B37cb8e568e27626f',
+    collateralToken: '0xaf204776c7245bf4147c2612bf6e5972ee483701'
   },
 }
 
 const MARKET_VIEW_ABI =
   'function getMarket(address marketFactory, address market) public view returns (tuple(address id, string marketName, string[] outcomes, address parentMarket, uint256 parentOutcome, address[] wrappedTokens, uint256 outcomesSupply, uint256 lowerBound, uint256 upperBound, bytes32 parentCollectionId, bytes32 conditionId, bytes32 questionId, uint256 templateId, tuple(bytes32 content_hash, address arbitrator, uint32 opening_ts, uint32 timeout, uint32 finalize_ts, bool is_pending_arbitration, uint256 bounty, bytes32 best_answer, bytes32 history_hash, uint256 bond, uint256 min_bond)[] questions, bytes32[] questionsIds, string[] encodedQuestions,bool payoutReported) memory)'
 
-function tvl(chain) {
-  return async (api) => {
-    // get all markets
-    const marketsByFactory = (await Promise.all(config[chain].marketFactory.map(factoryAddress => api.call({
-      abi: "function allMarkets() external view returns (address[] memory)",
-      target: factoryAddress,
-      chain,
-    }))));
+async function tvl(api) {
+  const { marketFactory, marketView, collateralToken } = config[api.chain]
+  // get all markets
+  const markets = await api.multiCall({ abi: 'address[]:allMarkets', calls: marketFactory })
+  const dataCalls = markets.map((v, i) => {
+    return v.map(val => ({ params: [marketFactory[i], val] }))
+  }).flat()
 
-    /*
-     * marketsData is an array of objects with the following structure:
-     *  - id
-     *  - parentMarket
-     *  - parentOutcome
-     *  - wrappedTokens
-     *  - outcomesSupply
-     */
-    const marketsData = (await Promise.all(marketsByFactory.map(async (markets, factoryIndex) => {
-        return await api.multiCall({
-            calls: markets.map(market => ({
-                target: config[chain].marketView,
-                params: [config[chain].marketFactory[factoryIndex], market
-                ]
-            })),
-            abi: MARKET_VIEW_ABI,
-            withMetadata: true,
-        })
-    }))).map(markets => {
-        return markets.map(market => ({
-            id: market.output.id,
-            parentMarket: market.output.parentMarket,
-            parentOutcome: market.output.parentOutcome,
-            wrappedTokens: market.output.wrappedTokens,
-            outcomesSupply: market.output.wrappedTokens.map(_ => 0n),
-        }))
-    }).flat();
+  /*
+  * marketsData is an array of objects with the following structure:
+  *  - id
+  *  - parentMarket
+  *  - parentOutcome
+  *  - wrappedTokens
+  *  - outcomesSupply
+  */
+  const marketsData = (await api.multiCall({ abi: MARKET_VIEW_ABI, calls: dataCalls, target: marketView })).map(market => ({
+    id: market.id,
+    parentMarket: market.parentMarket,
+    parentOutcome: market.parentOutcome,
+    wrappedTokens: market.wrappedTokens,
+    outcomesSupply: (market.wrappedTokens ?? []).map(_ => 0),
+  }))
 
-    /*
-     * outcomeTokensSupplyByMarket is an array of totalSupply[] of each outcome token of a market.
-     */
-    const outcomeTokensSupplyByMarket = (await Promise.all(marketsData.map(marketData => {
-        return api.multiCall({
-            calls: marketData.wrappedTokens.map(outcomeToken => ({
-                target: outcomeToken,
-            })),
-            abi: 'erc20:totalSupply',
-            withMetadata: true,
-        })
-    }))).map(totalSupplyByMarket => totalSupplyByMarket.map(totalSupllyByOutcome => BigInt(
-        totalSupllyByOutcome.output)));
+  const idIndexMapping = []
+  const supplyCalls = []
+  marketsData.forEach((marketData, index) => {
+    marketData.wrappedTokens.forEach((outcomeToken, idx2) => {
+      idIndexMapping.push([index, idx2])
+      supplyCalls.push(outcomeToken)
+    })
+  })
 
-    const totalSupply = calculateTotalSupply(marketsData, outcomeTokensSupplyByMarket);
+  const supplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: supplyCalls })
+  supplies.forEach((supply, i) => {
+    const [marketIndex, outcomeIndex] = idIndexMapping[i]
+    marketsData[marketIndex].outcomesSupply[outcomeIndex] = +supply
+  })
 
-    api.add(config[chain].collateralToken, totalSupply);
-  }
+  const totalSupply = calculateTotalSupply(marketsData);
+
+  api.add(collateralToken, totalSupply);
 }
 
 /**
@@ -76,15 +63,21 @@ function tvl(chain) {
  * This function calculates the total supply of parent markets by merging child market supplies into parent markets
  * and summing up the unique supplies. These unique supplies represent the TVL of sDAI backing the parent markets.
  */
-function calculateTotalSupply(marketsData, outcomeTokensSupplyByMarket) {
+function calculateTotalSupply(marketsData) {
   const marketSupplies = new Map();
   const processedTokens = new Set();
 
   marketsData.forEach((market, index) => {
-    const supply = outcomeTokensSupplyByMarket[index];
-    const uniqueSupply = supply.filter((_, i) => !processedTokens.has(market.wrappedTokens[i]));
-
-    uniqueSupply.forEach((_, i) => processedTokens.add(market.wrappedTokens[i]));
+    const supply = market.outcomesSupply
+    const uniqueSupply = []
+    let i = 0
+    for (const token of market.wrappedTokens) {
+      if (!processedTokens.has(token)) {
+        uniqueSupply.push(supply[i])
+        processedTokens.add(token)
+      }
+      i++
+    }
 
     marketSupplies.set(market.id, uniqueSupply);
   });
@@ -97,19 +90,19 @@ function calculateTotalSupply(marketsData, outcomeTokensSupplyByMarket) {
 
       if (parentSupply && childSupply) {
         // Add child market supply to the corresponding parent outcome
-        parentSupply[market.parentOutcome] = (parentSupply[market.parentOutcome] || 0n) + childSupply.reduce((a, b) => a > b ? a : b, 0n);
+        parentSupply[market.parentOutcome] = (parentSupply[market.parentOutcome] || 0) + childSupply.reduce((a, b) => a > b ? a : b, 0);
         marketSupplies.set(market.parentMarket, parentSupply);
       }
     }
   });
 
   // Calculate total supply of parent markets (parent markets are backed by sDAI)
-  let totalSupply = 0n;
+  let totalSupply = 0;
   marketsData.forEach((market) => {
     if (market.parentMarket === '0x0000000000000000000000000000000000000000') {
       const marketSupply = marketSupplies.get(market.id);
       if (marketSupply) {
-        totalSupply += marketSupply.reduce((a, b) => a > b ? a : b, 0n);
+        totalSupply += marketSupply.reduce((a, b) => a > b ? a : b, 0);
       }
     }
   });
@@ -118,11 +111,7 @@ function calculateTotalSupply(marketsData, outcomeTokensSupplyByMarket) {
 }
 
 module.exports = {
-  ethereum: {
-      tvl: tvl('ethereum'),
-  },
-  xdai: {
-      tvl: tvl('gnosis'),
-  },
+  ethereum: { tvl },
+  xdai: { tvl },
   methodology: 'TVL represents the total quantity of sDAI held in the conditional tokens contract. The sDAI is withdrawn when the participants merge or redeem their tokens.',
 }
