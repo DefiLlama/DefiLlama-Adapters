92e45a87786f3398f91832dde2dd22d817cd2349
Aslan Tashtanov <aslan.tashtanov@mystenlabs.com>
2024-10-22 14:34:07 -0400

    update url, use timestamp

--- projects/deepbook-v3/index.js
+++ projects/deepbook-v3/index.js
@@ -6,18 +6,17 @@ const usdcType = "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f9
 const bethType = "0xd0e89b2af5e4910726fbcd8b8dd37bb79b29e5f83f7491bca830e94f7f226d29::eth::ETH"
 const wusdtType = "0xc060006111016b8a020ad5b33834984a437aaa7d3c74c18e09a95d48aceab08c::coin::COIN"
 const wusdcType = "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN"
-const endpointUrl = "http://localhost:9008/"
+const endpointUrl = "https://deepbook-indexer.mainnet.mystenlabs.com/"
 const endpointName = "get_net_deposits/"
 
-async function tvl(api) {
+const tvl = async (api) => {
     let url = endpointUrl + endpointName + 
         deepType + "," +
         suiType + "," +
         usdcType + "," +
         bethType + "," +
         wusdtType + "," +
-        wusdcType
-    console.log(url)
+        wusdcType + "/" + api.timestamp
     const data = await get(url)
 
     api.add(deepType, data[deepType])
@@ -30,6 +29,7 @@ async function tvl(api) {
 
 module.exports = {
     methodology: "All deposits into all BalanceManagers minutes all withdrawals from all BalanceManagers",
+    start: 1728858752,
     sui: {
         tvl,
     }
