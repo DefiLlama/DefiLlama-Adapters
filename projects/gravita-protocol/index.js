53108ed42f62cdd4c37e822ba91b023e0b37d412
gbirckan <gbirckan@gmail.com>
2024-10-22 16:05:04 -0300

    feat: add constellation tvl

--- projects/gravita-protocol/index.js
+++ projects/gravita-protocol/index.js
@@ -1,3 +1,4 @@
+const BigNumber = require("bignumber.js");
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
 const ADMIN_ADDRESSES = {
@@ -9,6 +10,11 @@ const ADMIN_ADDRESSES = {
   polygon_zkevm: "0x6b42581aC12F442503Dfb3dff2bC75ed83850637",
 };
 
+const ETH_WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".toLowerCase();
+const ETH_RPL = "0xD33526068D116cE69F19A9ee46F0bd304F21A51f".toLowerCase();
+const ETH_CONSTELLATION_WETH_VAULT = "0xBB22d59B73D7a6F3A8a83A214BECc67Eb3b511fE";
+const ETH_CONSTELLATION_RPL_VAULT = "0x1DB1Afd9552eeB28e2e36597082440598B7F1320";
+
 async function tvl(_, _1, _2, { api }) {
   const adminContract = ADMIN_ADDRESSES[api.chain];
   const collAddresses = await api.call({
@@ -19,8 +25,24 @@ async function tvl(_, _1, _2, { api }) {
     abi: "address:activePool",
     target: adminContract,
   });
-  const balances = await sumTokens2({ api, tokens: collAddresses, owner: activePool });
-  return balances
+  const balances = await sumTokens2({
+    api,
+    tokens: collAddresses,
+    owner: activePool,
+  });
+  if (api.chain == "ethereum") {
+    const wethOnConstallation = await api.call({
+      abi: "uint256:totalAssets",
+      target: ETH_CONSTELLATION_WETH_VAULT,
+    });
+    balances[`ethereum:${ETH_WETH}`] = BigNumber(balances[`ethereum:${ETH_WETH}`] || 0).plus(wethOnConstallation).toString()
+    const rplOnConstellation = await api.call({
+      abi: "uint256:totalAssets",
+      target: ETH_CONSTELLATION_RPL_VAULT,
+    });
+    balances[`ethereum:${ETH_RPL}`] = rplOnConstellation
+  }
+  return balances;
 }
 
 module.exports = {
