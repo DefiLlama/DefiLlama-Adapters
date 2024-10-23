60bb24c53649581f694fee32ef173331a36fda30
0xpeluche <0xpeluche@proton.me>
2024-10-23 12:01:47 +0200

    Update, add solana chain

--- projects/openeden-tbill/index.js
+++ projects/openeden-tbill/index.js
@@ -1,4 +1,5 @@
 const { getCache, } = require("../helper/cache");
+const { getTokenSupply } = require('../helper/solana')
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const tbill = "0xdd50C053C096CB04A3e3362E2b622529EC5f2e8a"
@@ -7,7 +8,7 @@ function getTimeNow() {
   return Math.floor(Date.now() / 1000);
 }
 
-async function tvl(api) {
+async function evmTvl(api) {
   let contract = tbill
   if (api.chain === 'arbitrum') contract = '0xF84D28A8D28292842dD73D1c5F99476A80b6666A'
   const [bal, token] = await api.batchCall([
@@ -15,6 +16,11 @@ async function tvl(api) {
     { abi: 'address:underlying', target: contract },
   ])
   api.add(token, bal)
+}
+
+async function solTvl (api) {
+  const tvl = (await getTokenSupply('4MmJVdwYN8LwvbGeCowYjSx7KoEi6BJWg8XXnW4fDDp6') * 10 ** 6)
+  api.add(tbill, tvl)
 }
 
 async function ripplTvl (api) {
@@ -29,7 +35,8 @@ async function ripplTvl (api) {
 }
 
 module.exports = {
-  ethereum: { tvl },
-  arbitrum: { tvl },
-  ripple: { tvl: ripplTvl }
+  ethereum: { tvl: evmTvl },
+  arbitrum: { tvl: evmTvl },
+  ripple: { tvl: ripplTvl },
+  solana: { tvl: solTvl }
 }
