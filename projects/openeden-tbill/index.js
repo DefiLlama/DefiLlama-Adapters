f70620159d16e990e5dbcd7ba87b8e3164bf592a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-23 13:03:44 +0200

    add support for rpc proxy

--- projects/openeden-tbill/index.js
+++ projects/openeden-tbill/index.js
@@ -1,13 +1,10 @@
 const { getCache, } = require("../helper/cache");
 const { getTokenSupply } = require('../helper/solana')
+const { ripple } = require('../helper/chain/rpcProxy')
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const tbill = "0xdd50C053C096CB04A3e3362E2b622529EC5f2e8a"
 const solTbill = '4MmJVdwYN8LwvbGeCowYjSx7KoEi6BJWg8XXnW4fDDp6'
-
-function getTimeNow() {
-  return Math.floor(Date.now() / 1000);
-}
 
 async function evmTvl(api) {
   let contract = tbill
@@ -25,14 +22,11 @@ async function solTvl (api) {
 }
 
 async function ripplTvl (api) {
-  const timeNow = getTimeNow()
-  const aDayInSeconds = 60 * 60 * 24;
-  const projectKey = 'openeden-tbill'
-  const cacheKey = 'cache'
-  let { lastDataUpdate, tvl } = await getCache(projectKey, cacheKey)
-  if (!lastDataUpdate || timeNow - lastDataUpdate > aDayInSeconds || !tvl) 
-    throw new Error("stale/missing tvl data");
-  api.add(tbill, tvl * 10 ** 6, { skipChain: true })
+  const issuerAddress = "rJNE2NNz83GJYtWVLwMvchDWEon3huWnFn";
+  const subscriptionOperatorAddress = "rB56JZWRKvpWNeyqM3QYfZwW4fS9YEyPWM";
+
+  const data = await ripple.gatewayBalances({ account: issuerAddress, hotwallet: subscriptionOperatorAddress })
+  api.add(tbill, Number(data.obligations?.TBL) * 1e6, { skipChain: true })
 }
 
 module.exports = {
