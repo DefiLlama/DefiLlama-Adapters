ccbef07a3292926ae47b010a235454c90589b3c8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-23 07:58:58 +0200

    update babylon

--- projects/babylon/index.js
+++ projects/babylon/index.js
@@ -1,28 +1,20 @@
 const axios = require('axios');
 
-async function tvl() {
-  try {
-    const response = await axios.get('https://staking-api.babylonlabs.io/v1/stats');
-    const activeTvlSatoshis = response.data.data.active_tvl;
-    const activeTvlBitcoin = activeTvlSatoshis / 1e8;
+async function tvl(api) {
+  const response = await axios.get('https://staking-api.babylonlabs.io/v1/stats');
+  const activeTvlSatoshis = response.data.data.active_tvl;
+  const activeTvlBitcoin = activeTvlSatoshis / 1e8;
 
-    if (activeTvlBitcoin && activeTvlBitcoin > 0) {
-      return {
-        bitcoin: activeTvlBitcoin,
-      };
-    } else {
-      return {};
-    }
-  } catch (error) {
-    return {};
-  }
+  if (activeTvlBitcoin > 0)
+    api.addCGToken('bitcoin', activeTvlBitcoin)
+  else
+    throw new Error('Babylon: Invalid TVL value');
 }
 
 module.exports = {
   methodology: 'TVL is fetched from Babylon’s Staking API and represents the total Bitcoin locked in the Babylon staking protocol.',
-  start: 1724351485, 
+  start: 1724351485,
   timetravel: false,
-  misrepresentedTokens: false,
   bitcoin: {
     tvl,
   },
