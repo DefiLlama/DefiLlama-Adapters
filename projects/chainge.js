e9b83fb85318c2b27b0c62999e98432139cbdadd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-24 08:01:21 +0200

    minor fix

--- projects/chainge.js
+++ projects/chainge.js
@@ -17,6 +17,7 @@ const config = {
 
 module.exports = {
     methodology: "assets in liquidity are counted as TVL + balances of all tokens (USDC, USDT, WBTC, WETH, WSYS, and native SYS) held in the Chainge treasury address on the Rollux network. These tokens are used to provide liquidity for cross-chain swaps.",
+    fusion: { tvl: () => ({})}
 }
 
 Object.keys(config).forEach(chain => {
