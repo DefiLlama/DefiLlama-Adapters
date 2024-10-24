66748bccec770871cad183065426f339e201713d
0xpeluche <0xpeluche@proton.me>
2024-10-24 16:57:34 +0200

    Fix: Univ1

--- projects/uniswap-v1/index.js
+++ projects/uniswap-v1/index.js
@@ -1,13 +1,31 @@
-const { getChainTvl } = require('../helper/getUniSubgraphTvl');
+const { nullAddress } = require('../helper/unwrapLPs')
+const sdk = require("@defillama/sdk")
 
-const v1graph = getChainTvl({
-  ethereum: 'ESnjgAG9NjfmHypk4Huu4PVvz55fUwpyrRqHF21thoLJ'
-}, "uniswaps", "totalLiquidityUSD")
+const uniFactory = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
+
+const abi = {
+  getExchange: "function getExchange(address token) view returns (address)",
+  getTokenWithId: "function getTokenWithId(uint256 token_id) view returns (address)",
+  tokenCount: "function tokenCount() view returns (uint256)",
+}
+
+const tvl = async (api) => {
+  const tokens = (await api.fetchList({ lengthAbi: abi.tokenCount, itemAbi: abi.getTokenWithId, target: uniFactory })).filter((token) => token !== nullAddress)
+  const pools = await api.multiCall({ calls: tokens.map((token) => ({ target: uniFactory, params: [token] })), abi: abi.getExchange })
+  const balanceOfs = await api.multiCall({ calls: pools.map((pool, i) => ({ target: tokens[i], params: [pool] })), abi: 'erc20:balanceOf', permitFailure: true })
+
+  await Promise.all(
+    pools.map(async (pool, i) => {
+      const token = tokens[i]
+      const balance = balanceOfs[i]
+      if (!token || !balance || balance == 0) return // to ensure that there is still collateral in place
+      api.add(nullAddress, (await sdk.api.eth.getBalance({ target: pool })).output * 2) // Since Uniswap V1 only allowed swaps against ETH, it's enough to know the amount of ETH and multiply it by two to determine the pool's value without needing to know the price of the collateral in question
+    })
+  )
+}
 
 module.exports = {
   misrepresentedTokens: true,
-  methodology: `Counts the tokens locked on AMM pools, pulling the data from the 'ianlapham/uniswapv2' subgraph`,
-  ethereum: {
-    tvl: v1graph("ethereum"),
-  }
+  methodology: `Counts the tokens locked on AMM pools, pulling`,
+  ethereum: { tvl }
 }
