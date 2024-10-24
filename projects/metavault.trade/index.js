9a63014300acd262564b5c6907f7673f281d9243
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-24 07:58:51 +0200

    Metavault (#12022)
    Co-authored-by: 0xR <0xr@metavault.trade>

--- projects/metavault.trade/index.js
+++ projects/metavault.trade/index.js
@@ -4,6 +4,7 @@ const { staking } = require("../helper/staking");
 const polygonVault = "0x32848E2d3aeCFA7364595609FB050A301050A6B4";
 const polygonStaking = "0xE8e2E78D8cA52f238CAf69f020fA961f8A7632e9"; // Staked MVX, sMVX
 const polygonMVX = "0x2760e46d9bb43dafcbecaad1f64b93207f9f0ed7";
+const polygonMvxVester = "0x543e07eb4a260e10310fbcf2403e97c762a8db0b" // New Vester
 
 const polygonTVL = async (api) => {
   const tokens = await api.fetchList({  lengthAbi: abi.allWhitelistedTokensLength, itemAbi: abi.allWhitelistedTokens, target: polygonVault})
@@ -12,7 +13,7 @@ const polygonTVL = async (api) => {
 
 module.exports = {
   polygon: {
-    staking: staking(polygonStaking, polygonMVX),
+    staking: staking([polygonStaking, polygonMvxVester], polygonMVX, ),
     tvl: polygonTVL,
   },
 };
