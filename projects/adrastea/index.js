5a3b38510d30832f9302a7323ab091f6b07d22a0
Kay ( Andrew ) <chauanhtuan185@gmail.com>
2024-10-25 21:58:50 +0700

    chore : update TVL for adrastea

--- projects/adrastea/index.js
+++ projects/adrastea/index.js
@@ -1,6 +1,6 @@
 const { sumTokensExport } = require('../helper/solana')
 module.exports = {
   solana: {
-    tvl: sumTokensExport({ owners: ['Ec5tJ1H24iVSM2L8Yd7SHf7bjtD7FUWDiYSeESpFYynM'] })
+    tvl: sumTokensExport({ owners: ['DSsS7nzPv1AhX5UKYMWYSioyd3E8w9QjnTjNA7kyQzVA'] })
   }
 }
