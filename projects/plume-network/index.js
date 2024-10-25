4d3a6b8f760da6b9f219d9cbd2aca9235032c906
ungaro <alp.guneysel@gmail.com>
2024-10-24 21:16:51 -0400

    change sbtc address

--- projects/plume-network/index.js
+++ projects/plume-network/index.js
@@ -3,7 +3,7 @@ const { sumTokens2 } = require('../helper/unwrapLPs')
 
 const RWA_STAKING = "0xdbd03d676e1cf3c3b656972f88ed21784372acab"
 const RESERVE_STAKING = "0xba0ae7069f94643853fce3b8af7f55acbc11e397"
-const SBTC = "0xfe18be6b3bd88a2d2a7f928d00292e7a9963cfc6"
+const SBTC = "0x094c0e36210634c3CfA25DC11B96b562E0b07624"
 const STONE = "0x7122985656e38bdc0302db86685bb972b145bd3c"
 
 async function tvl(timestamp, block, chainBlocks, { api }) {
