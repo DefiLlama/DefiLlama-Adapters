fbf73f2fd7a7db59c5fbffb6ac8d1d0bebc321f5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-25 07:46:38 +0200

    code refactor

--- projects/originarm/index.js
+++ projects/originarm/index.js
@@ -4,13 +4,8 @@ module.exports = {
   ethereum: {
     tvl: async (api) => {
       const originLidoArm = "0x85b78aca6deae198fbf201c82daf6ca21942acc6";
-      const [weth, steth, outstandingSteth] = await Promise.all([
-        api.call({abi: 'erc20:balanceOf', target: originLidoArm, params: ADDRESSES.ethereum.WETH }),
-        api.call({abi: 'erc20:balanceOf', target: originLidoArm, params: ADDRESSES.ethereum.STETH }),
-        api.call({abi: 'uint256:lidoWithdrawalQueueAmount', target: originLidoArm }),
-      ])
-      api.add(ADDRESSES.ethereum.WETH, weth)
-      api.add(ADDRESSES.ethereum.STETH, steth + outstandingSteth)
+      const outstandingSteth = await api.call({abi: 'uint256:lidoWithdrawalQueueAmount', target: originLidoArm })
+      api.add(ADDRESSES.ethereum.STETH, outstandingSteth)
       return api.sumTokens({ owner: originLidoArm, tokens: [ADDRESSES.ethereum.WETH, ADDRESSES.ethereum.STETH] })
     },
   },
