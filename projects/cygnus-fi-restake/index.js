17c3c3ff9ff70694414ca59a6e1d7074b9fc64ef
pie <greatpie@live.com>
2024-10-25 18:16:29 +0000

    feat: add unibtc

--- projects/cygnus-fi-restake/index.js
+++ projects/cygnus-fi-restake/index.js
@@ -13,6 +13,10 @@ const CYGNUS_POOL_CONFIG = {
         STBTC: {
             depositToken: '0xf6718b2701D4a6498eF77D7c152b2137Ab28b8A3',
             vault: '0x0Ce45dd53affbb011884EF1866E0738f58AB7969'
+        },
+        UNIBTC:{
+            depositToken: '0x93919784C523f39CACaa98Ee0a9d96c3F32b593e',
+            vault: '0xBc323bA4bbf2559417C3Ca47A75e2Ea341Cf8320'
         }
     }
 }
