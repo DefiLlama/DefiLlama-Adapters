a2d24430df36becea9d842e350c62a1fa1b2a719
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-25 20:40:35 +0200

    track adrastea LRT

--- projects/helper/chain/sui.js
+++ projects/helper/chain/sui.js
@@ -73,8 +73,9 @@ async function getDynamicFieldObjects({ parent, cursor = null, limit = 48, items
 async function call(method, params, { withMetadata = false } = {}) {
   if (!Array.isArray(params)) params = [params]
   const {
-    result
+    result, error
   } = await http.post(endpoint(), { jsonrpc: "2.0", id: 1, method, params, })
+  if (!result && error) throw new Error(`[sui] ${error.message}`)
   if (['suix_getAllBalances'].includes(method)) return result
   return withMetadata ? result : result.data
 }
