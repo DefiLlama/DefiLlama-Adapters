8ea598d521566a1b8266300eb201314e93fb4a0b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-25 08:25:38 +0200

    code refactor

--- projects/uniswap-v1/index.js
+++ projects/uniswap-v1/index.js
@@ -1,5 +1,4 @@
 const { nullAddress } = require('../helper/unwrapLPs')
-const sdk = require("@defillama/sdk")
 
 const uniFactory = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
 
@@ -10,18 +9,11 @@ const abi = {
 }
 
 const tvl = async (api) => {
-  const tokens = (await api.fetchList({ lengthAbi: abi.tokenCount, itemAbi: abi.getTokenWithId, target: uniFactory })).filter((token) => token !== nullAddress)
-  const pools = await api.multiCall({ calls: tokens.map((token) => ({ target: uniFactory, params: [token] })), abi: abi.getExchange })
-  const balanceOfs = await api.multiCall({ calls: pools.map((pool, i) => ({ target: tokens[i], params: [pool] })), abi: 'erc20:balanceOf', permitFailure: true })
-
-  await Promise.all(
-    pools.map(async (pool, i) => {
-      const token = tokens[i]
-      const balance = balanceOfs[i]
-      if (!token || !balance || balance == 0) return // to ensure that there is still collateral in place
-      api.add(nullAddress, (await sdk.api.eth.getBalance({ target: pool })).output * 2) // Since Uniswap V1 only allowed swaps against ETH, it's enough to know the amount of ETH and multiply it by two to determine the pool's value without needing to know the price of the collateral in question
-    })
-  )
+  let pools = await api.fetchList({ lengthAbi: abi.tokenCount, itemAbi: abi.getTokenWithId, target: uniFactory, itemAbi2: abi.getExchange, })
+  pools = pools.filter(i => i !== nullAddress)
+  await api.sumTokens({ owners: pools, tokens: [nullAddress] })
+  const balancesV2 = api.getBalancesV2()
+  return balancesV2.clone(2).getBalances() // // Since Uniswap V1 only allowed swaps against ETH, it's enough to know the amount of ETH and multiply it by two to determine the pool's value without needing to know the price of the collateral in question
 }
 
 module.exports = {
