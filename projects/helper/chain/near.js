b0aed3c63ccd0cca22cdb10f4213626088e8e8b7
deep-path <476044723@qq.com>
2024-10-25 15:22:56 +0800

    feat: change near rpc

--- projects/helper/chain/near.js
+++ projects/helper/chain/near.js
@@ -12,7 +12,7 @@ function transformAddress(addr) {
   return addr
 }
 
-const endpoint = "https://rpc.mainnet.near.org"
+const endpoint = "https://near.lava.build"
 
 const tokenMapping = {
   'wrap.near': { name: 'near', decimals: 24, },
