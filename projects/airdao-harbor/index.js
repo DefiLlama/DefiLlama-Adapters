11f31dff2285b283da4f61d34571dca7a5d77e52
SigismundSchlomo <o.kuznietsov@inc4.net>
2024-10-25 10:25:25 +0300

    Update airdao harbor stats collection

--- projects/airdao-harbor/index.js
+++ projects/airdao-harbor/index.js
@@ -1,10 +1,10 @@
 async function tvl(api) {
   const totalStake = await api.call({ abi: 'uint256:getTotalStAmb', target: '0xBda7cf631Db4535A500ED16Dd98099C04e66F1d5' })
-  api.addGasToken(totalStake)
+  api.addCGToken('amber', totalStake/1e18)
 }
 
 module.exports = {
-  methodology: `TVL counts deposits made to Harbor liquid staking on AirDAO.`,
+  methodology: `TVL counts deposits made to Harbor on AirDAO.`,
   airdao: {
     tvl
   }
