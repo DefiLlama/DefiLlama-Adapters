ee725d16871a590441b0972722120064b46ac257
santinoYoung <santino.bai@desyn.io>
2024-10-25 18:22:44 +0800

    update get pool tokens

--- projects/desyn-farm2/index.js
+++ projects/desyn-farm2/index.js
@@ -1,8 +1,11 @@
+const { sumTokens2 } = require('../helper/unwrapLPs');
+
 const corePool = '0x5e69d826D3663094321E2cf3C387b7F9Dd7b44Bb'
 
 async function tvl(api) {
     const tokens = await api.multiCall({  abi: 'address[]:getCurrentTokens', calls: corePool})
-    return api.sumTokens({ ownerTokens: tokens.map((tokens, i) => [tokens, pools[i]])})
+    const ownerTokens = tokens.map((v, i) => [v, pools[i]])
+    return sumTokens2({ ownerTokens, api })
 }
 
 module.exports = {
