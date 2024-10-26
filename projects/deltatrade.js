22e52082c99d39ce9c94d8a098a5e4220e93052c
deep-path <54880778+deep-path@users.noreply.github.com>
2024-10-26 19:59:31 +0800

    remove anchor for add solana tvl (#12053)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/deltatrade.js
+++ projects/deltatrade.js
@@ -1,5 +1,14 @@
 const ADDRESSES = require('./helper/coreAssets.json')
-const { sumTokens } = require('./helper/chain/near');
+const { sumTokens } = require('./helper/chain/near')
+const { getConfig } = require('./helper/cache')
+const { get } = require('./helper/http')
+
+const { PublicKey } = require('@solana/web3.js');
+const { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, } = require("@solana/spl-token");
+const { sumTokens2 } = require('./helper/solana')
+
+const state = new PublicKey('FRcbUFpGHQppvXAyJrNYLKME1BQfowh4xKZB2vt9j6yn');
+const programId = new PublicKey('CNLGhYQgNwjyDfHZTEjHfk1MPkqwP96qZahWN82UfcLM');
 
 const GRID_CONTRACT_ID = 'grid.deltatrade.near';
 const DCA_CONTRACT_ID = 'dca.deltatrade.near';
@@ -23,4 +32,37 @@ module.exports = {
   near: {
     tvl: () => sumTokens({ tokens, owners: [GRID_CONTRACT_ID, DCA_CONTRACT_ID] }),
   },
+  solana: {
+    tvl: solanaTvl,
+  }
+}
+
+async function getTokens() {
+  return getConfig('deltatrade/sol-pairs', undefined, {
+    fetcher: async () => {
+      const { data } = await get('https://solapi.deltatrade.ai/api/bot/grid/pairs');
+      const tokenSet = new Set()
+      data.forEach(pair => {
+        tokenSet.add(pair.base_token.code)
+        tokenSet.add(pair.quote_token.code)
+      })
+      const arry = Array.from(tokenSet)
+      arry.sort()
+      return arry
+    }
+  })
+}
+
+function getGlobalBalanceUser(token) {
+  const [globalBalPda] = PublicKey.findProgramAddressSync([Buffer.from("global_balance_user"), state.toBuffer(), new PublicKey(token).toBuffer()], programId);
+
+  // const globalBalTokenAccount = await Token.getAssociatedTokenAddress(ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, new PublicKey(token), new PublicKey(globalBalPda.toString()), true);
+  const [tokenAccount] = PublicKey.findProgramAddressSync([new PublicKey(globalBalPda.toString()), TOKEN_PROGRAM_ID, new PublicKey(token)].map(i => i.toBuffer()), ASSOCIATED_TOKEN_PROGRAM_ID)
+  return tokenAccount.toString();
+}
+
+async function solanaTvl() {
+  const tokens = await getTokens();
+  const tokenAccounts = tokens.map(getGlobalBalanceUser)
+  return sumTokens2({ tokenAccounts })
 }
