ee982feb7f3ec04a9a94d232bddd9458b6ff8008
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-27 10:26:46 +0100

    turn on chain

--- projects/bonzo/index.js
+++ projects/bonzo/index.js
@@ -1,51 +1,7 @@
-const { fetchURL } = require("../helper/utils")
-const BigNumber = require("bignumber.js");
-
-const tvl = async () => {
-  const res = await fetchURL("https://bonzo-data-api-eceac9d8a2aa.herokuapp.com/stats");
-  const result = {};
-  const reserves = res.data.reserves;  
-  for(let i = 0 ; i < reserves.length; i ++) {
-    const tokenId = reserves[i].coingecko_id;
-    const tinytoken = reserves[i].available_liquidity.tiny_token;
-    const decimals = reserves[i] .decimals;
-    result[tokenId] = BigNumber(tinytoken).shiftedBy(-decimals);
-  }
-  return result;
-
-  // Alternate: Just return rollup in hbar
-  // return { "hedera-hashgraph": BigNumber(res.data.total_liquidity_value.hbar_tinybar).shiftedBy(-8) };
-}
-
-const borrowed = async () => {
-  const res = await fetchURL("https://bonzo-data-api-eceac9d8a2aa.herokuapp.com/stats");
-  const result = {};
-  const reserves = res.data.reserves;  
-  for(let i = 0 ; i < reserves.length; i ++) {
-    const tokenId = reserves[i].coingecko_id;
-    // Note: for new we only suppport variable debt
-    const tinytoken = reserves[i].total_variable_debt.tiny_token;
-    const decimals = reserves[i] .decimals;
-    result[tokenId] = BigNumber(tinytoken).shiftedBy(-decimals);
-  }
-  return result;
-
-  // Alternate: Just return rollup in hbar
-  // return { "hedera-hashgraph": BigNumber(res.data.total_borrowed_value.hbar_tinybar).shiftedBy(-8) };
-}
+const { aaveV2Export, methodology } = require("../helper/aave")
 
 module.exports = {
   timetravel: false,
-  methodology: 'The calculated TVL is the current USD sum of deposits minus borrows for all all reserves found under https://app.bonzo.finance/',
-  hedera: {
-    tvl,
-    borrowed
-  },
-  external: [
-    {
-      website: 'https://bonzo.finance/',
-      twitter: 'https://twitter.com/bonzo_finance',
-      discord: 'https://bonzo.finance/discord',
-    },
-  ],
+  methodology,
+  hedera: aaveV2Export('0x236897c518996163E7b313aD21D1C9fCC7BA1afc'),
 }
