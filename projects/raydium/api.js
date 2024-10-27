702785a3753d4d6d82c775c80365b5e0da50d3a3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-27 06:52:40 +0100

    exclude SOS from raydium

--- projects/raydium/api.js
+++ projects/raydium/api.js
@@ -16,7 +16,7 @@ async function tvlWithCheck(api) {
   const balances = await tvl(api)
   api.addBalances(balances)
   api.removeTokenBalance('DS4QiZfkp39PsHXYCRV3NkyDUKV9SpTczp2qnAUg6Nt6') // ZMB
-  // api.removeTokenBalance('HDa3zJc12ahykSsBRvgiWzr6WLEByf36yzKKbVvy4gnF') // SOS
+  api.removeTokenBalance('HDa3zJc12ahykSsBRvgiWzr6WLEByf36yzKKbVvy4gnF') // SOS
   const usdValue = await api.getUSDValue()
   // for some godforsaken reason, the TVL is sometimes reported as 60M, we fail in that case rather than report a wrong number
   if (usdValue < 2e8) throw new Error('TVL is too low :' + usdValue / 1e6 + 'M')
