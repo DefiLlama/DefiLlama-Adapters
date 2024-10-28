2f5e4c066d7fef1823e6494abd45b353a0ddbdaf
nuhbye <kunalsangwan1@gmail.com>
2024-10-28 22:31:48 +0530

    Added another pool to tonpools (#12079)

--- projects/tonpools/index.js
+++ projects/tonpools/index.js
@@ -1,11 +1,19 @@
-const { sumTokensExport } = require('../helper/chain/ton')
+const { sumTokensExport } = require("../helper/chain/ton");
 const ADDRESSES = require("../helper/coreAssets.json");
 
-const tonpoolsContractAddress = "EQA7y9QkiP4xtX_BhOpY4xgVlLM7LPcYUA4QhBHhFZeL4fTa";
+const tonpoolsContractAddress =
+  "EQA7y9QkiP4xtX_BhOpY4xgVlLM7LPcYUA4QhBHhFZeL4fTa";
+
+const tonpoolUSDTContractAddress =
+  "EQDrSz9W4tjwnqy1F4z-O4Vkdp8g2YP94cmh12X5RbYpejCw";
 
 module.exports = {
   methodology: "Ton Pools's TVL includes all deposited supported assets",
   ton: {
-    tvl: sumTokensExport({ owner: tonpoolsContractAddress, tokens: [ADDRESSES.null] }),
+    tvl: sumTokensExport({
+      owners: [tonpoolsContractAddress, tonpoolUSDTContractAddress],
+      tokens: [ADDRESSES.null],
+    }),
   },
-}
+};
+
