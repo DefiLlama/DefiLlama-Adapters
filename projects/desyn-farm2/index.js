1b03486813a29ec1dffb477d0d3581980abdf575
santinoYoung <santino.bai@desyn.io>
2024-10-28 16:11:15 +0800

    update sumTokens

--- projects/desyn-farm2/index.js
+++ projects/desyn-farm2/index.js
@@ -1,11 +1,10 @@
 const { getLogs } = require('../helper/cache/getLogs');
-const { sumTokens2 } = require('../helper/unwrapLPs');
 
 const corePool = '0x5e69d826D3663094321E2cf3C387b7F9Dd7b44Bb'
 
 async function tvl(api) {
     let poolLogs = (await Promise.all([
-        getLogs({ api, target: '0x5e69d826D3663094321E2cf3C387b7F9Dd7b44Bb', topic: 'LOG_NEW_POOL(address,address)', fromBlock: 18756776, }),
+        getLogs({ api, target: corePool, topic: 'LOG_NEW_POOL(address,address)', fromBlock: 18756776, }),
     ])).flat()
 
     let pools = poolLogs.map((poolLog) => `0x${poolLog.topics[2].slice(26)}`)
@@ -13,7 +12,7 @@ async function tvl(api) {
     const poolTokenData = await api.multiCall({ calls: pools, abi: "address[]:getCurrentTokens", })
     const ownerTokens = poolTokenData.map((v, i) => [v, pools[i]])
     console.log('ownerTokens', ownerTokens)
-    await sumTokens2({ ownerTokens, api })
+    await api.sumTokens({ ownerTokens, api })
     return api.getBalances()
 }
 
