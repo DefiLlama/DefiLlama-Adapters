ddd65701ca6cb34e0ee864e446a566b3e770d1db
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-28 14:13:59 +0100

    minor fix

--- projects/bsx-exchange/index.js
+++ projects/bsx-exchange/index.js
@@ -1,3 +1,5 @@
+const { nullAddress } = require("../helper/tokenMapping")
+
 const config = {
   base: '0x26a54955a5fb9472d3edfeac9b8e4c0ab5779ed3',
 }
@@ -7,6 +9,7 @@ Object.keys(config).forEach(chain => {
   module.exports[chain] = {
     tvl: async (api) => {
       const tokens = await api.call({ abi: 'address[]:getSupportedTokenList', target: exchange })
+      tokens.push(nullAddress)
       return api.sumTokens({ owner: exchange, tokens, blacklistedTokens: ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'] })
     }
   }
