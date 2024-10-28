3a655b8c780086796ff1824773c06f419f4fde60
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-28 15:04:17 +0100

    bugfix

--- projects/desyn/index.js
+++ projects/desyn/index.js
@@ -31,17 +31,9 @@ Object.keys(config).forEach(chain => {
         fromBlock,
       })
 
-      const balances = {}
-
       const pools = logs.map(i=>i.pool)
       const tokens = await api.multiCall({  abi: 'address[]:getCurrentTokens', calls: pools})
-      tokens.forEach(async (token, index) => {
-        const accountBalances = (await api.multiCall({  abi: 'address:getBalance', calls: token})).output
-        sdk.util.sumSingleBalance(balances, tokens[index].toString(), accountBalances)
-      })
-    
-      return balances
-
+      return api.sumTokens({ ownerTokens: tokens.map((tokens, i) => [tokens, pools[i]])})
     }
   }
 })
