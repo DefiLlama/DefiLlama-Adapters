07ec47ca0eeb5266984978aafb5a4b93682fba97
santinoYoung <santino.bai@desyn.io>
2024-10-28 21:27:46 +0800

    update getBalance

--- projects/desyn-farm1/index.js
+++ projects/desyn-farm1/index.js
@@ -33,7 +33,7 @@ Object.keys(config).forEach(chain => {
       const tokens = await api.multiCall({  abi: 'address[]:getCurrentTokens', calls: pools})
 
       tokens.forEach(async (token, index) => {
-        const accountBalances =  await api.multiCall({  abi: abi['getBalance'], calls: token,})
+        const accountBalances =  await api.multiCall({  abi: abi['getBalance'], calls: token})
         sdk.util.sumSingleBalance(balances, tokens[index].toString(), accountBalances)
       })
    
