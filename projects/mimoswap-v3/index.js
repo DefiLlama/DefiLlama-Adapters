f369d6edd2ebce6ce85116ec4e2d5025d59706b6
DLHTX <949729789@qq.com>
2024-10-28 10:52:49 +0800

    Add mimoswap V3 DEX

---
+++ projects/mimoswap-v3/index.js
@@ -0,0 +1,8 @@
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+    iotex: { 
+      factory: "0xF36788bF206f75F29f99Aa9d418fD8164b3B8198",
+      fromBlock: 27707694
+    }
+})
