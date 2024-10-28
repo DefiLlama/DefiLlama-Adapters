0c0e1a264d02d0dfa283e987b0a766db2243d0d6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-28 17:17:33 +0100

    minor fix #12066

--- projects/injective-orderbook-spot/index.js
+++ projects/injective-orderbook-spot/index.js
@@ -3,6 +3,6 @@ const { getOrderBookTvl, TYPES } = require('../injective-orderbook/util')
 module.exports = {
   timetravel: false,
   injective: {
-    tvl: getOrderBookTvl(TYPES.DERIVATIVES),
+    tvl: getOrderBookTvl(TYPES.SPOT),
   }
 }
