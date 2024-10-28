363359500faf4bca1343df973f52a96d75bbc92e
santinoYoung <santino.bai@desyn.io>
2024-10-28 20:44:02 +0800

    update multicall

--- projects/desyn-farm1/index.js
+++ projects/desyn-farm1/index.js
@@ -26,7 +26,7 @@ Object.keys(config).forEach(chain => {
 
       const balances = {}
       const pools = logs.map(i=>i.pool)
-      const tokens = await api.multiCall({  abi: 'address[]:getCurrentTokens', calls: pools})
+      const tokens = await api.multiCall({  abi: 'address[]:getCurrentTokens', target: pools, params: any})
 
       tokens.forEach(async (token, index) => {
         const accountBalances =  await api.multiCall({  abi: 'address:getBalance', calls: token})
