6179e94af931463f0eaed7a1e6eb9371816d07e4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-29 08:59:00 +0100

    update surge trade

--- projects/surge-trade/index.js
+++ projects/surge-trade/index.js
@@ -1,5 +1,14 @@
-const { sumTokensExport } = require('../helper/chain/radixdlt')
+const { queryAddresses } = require('../helper/chain/radixdlt')
 
 module.exports = {
-  radixdlt: { tvl: sumTokensExport({ owner: 'component_rdx1crqujzfeukp9d565sh8usdnjsd8k30saa00h540vnwja7se8qqp8g7'}) },
+  radixdlt: { tvl },
+}
+
+async function tvl(api) {
+  const [{ details: { state }, fungible_resources }] = await queryAddresses({ addresses: ['component_rdx1crezrpxw9ypg6v2panqjqwevnwplg94yeej0rhqq9k7p4kgnltrc9g'], miscQuery: { "aggregation_level": "Vault" } })
+  const stateObj = {}
+  state.fields.map(i => stateObj[i.field_name] = +i.value)
+  const poolAmount = parseFloat(fungible_resources.items[0].vaults.items[0].amount)
+  const tvl_usd = poolAmount + stateObj.virtual_balance + stateObj.unrealized_pool_funding + stateObj.pnl_snap
+  api.add('resource_rdx1t4upr78guuapv5ept7d7ptekk9mqhy605zgms33mcszen8l9fac8vf', tvl_usd)
 }
