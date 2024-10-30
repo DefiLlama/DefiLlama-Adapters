cd84db40229dc0d340c1564fa69e23843b847198
santinoYoung <santino.bai@desyn.io>
2024-10-30 10:30:28 +0800

    reset hallmarks

--- projects/desyn/index.js
+++ projects/desyn/index.js
@@ -4,6 +4,8 @@ module.exports = {
   doublecounted: true,
   hallmarks: [
     [1719734400, "Launched on Merlin Chain"],
+    [1718092800, "DeSyn KelpDAO Restaking Fund Launched"],
+    [1713340800, "Restaking Fund Series Launched"]
   ],
   methodology: 'Liquid restaking strategy',
 }
