4e5ad12435b6373a8fa9b0fa583a82ac76e809f5
yellow <8539006+yellowBirdy@users.noreply.github.com>
2024-10-30 23:02:45 +0100

    tvl v1

--- projects/nabla/index.js
+++ projects/nabla/index.js
@@ -10,74 +10,64 @@ const abis = {
   }
 }
 
-const ARB_PORTAL = "0xcB94Eee869a2041F3B44da423F78134aFb6b676B";
 const AA = {
-  ARB : {
+  ARB : [{
     BSP : "0x337B03C2a7482c6eb29d8047eA073119dc68a29A",
     WETH: "0x272dF896f4D0c97F65e787f861bb6e882776a155",
     USDC: "0x058a0875DB2168AF97bbf01043C3e8F751cCd9A8",
     WBTC: "0x411eF79fE9Df8Ba82A09c7e93FdE85AF5732BF12",
     ARB:  "0xE70292D6054B753214D555930e0F11CD7206Efeb",
-  }
+    swapPools: [
+      "0x272dF896f4D0c97F65e787f861bb6e882776a155",
+      "0x058a0875DB2168AF97bbf01043C3e8F751cCd9A8",
+      "0x411eF79fE9Df8Ba82A09c7e93FdE85AF5732BF12",
+      "0xE70292D6054B753214D555930e0F11CD7206Efeb",
+    ]
+  }],
+  BASE : [{
+    BSP : "0x50841f086891fe57829ee0a809f8B10174892b69",
+    WETH: "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB",
+    CBBTC: "0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85",
+
+    swapPools: [
+      "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB",
+      "0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85",
+    ]
+  }]
 }
 
 
 
-async function tvl(api) {
-  const BSPToken = await api.call({
-    abi: abis.backstopPool.asset, 
-    target: AA.ARB.BSP
-  });
-  const BSPReserve = await api.call({
-    abi: 'erc20:balanceOf',
-    target: BSPToken,
-    params: [AA.ARB.BSP],
-  });
+const tvlFactory = (pools) => async function tvl(api) {
 
-  const WETHToken = await api.call({
-    abi: abis.swapPool.asset, 
-    target: AA.ARB.WETH
-  });
-  const WETHReserve = await api.call({
-    abi: abis.swapPool.reserve,
-    target: AA.ARB.WETH
-  });
-  const USDCReserve = await api.call({
-    abi: abis.swapPool.reserve,
-    target: AA.ARB.USDC
-  });
-  const WBTCReserve = await api.call({
-    abi: abis.swapPool.reserve,
-    target: AA.ARB.WBTC
-  });
-  const ARBReserve = await api.call({
-    abi: abis.swapPool.reserve,
-    target: AA.ARB.ARB
-  });
-  const USDCToken = await api.call({
-    abi: abis.swapPool.asset, 
-    target: AA.ARB.USDC
-  });
-  const WBTC = await api.call({
-    abi: abis.swapPool.asset, 
-    target: AA.ARB.WBTC
-  });
-  const ARB = await api.call({
-    abi: abis.swapPool.asset, 
-    target: AA.ARB.ARB
-  });
+  let swapPools = []
+  for (const ensable of pools) {
+    const BSPToken = await api.call({
+      abi: abis.backstopPool.asset, 
+      target: ensable.BSP
+    });
+    const BSPReserve = await api.call({
+      abi: 'erc20:balanceOf',
+      target: BSPToken,
+      params: [ensable.BSP],
+    });
+
+    api.add(BSPToken, BSPReserve);
+
+    swapPools = [...swapPools, ...ensable.swapPools];
+  }
 
 
 
+  const reserves =  await api.multiCall({  abi: abis.swapPool.reserve, calls: pools[0].swapPools})
+  const tokens =  await api.multiCall({  abi: abis.swapPool.asset, calls: pools[0].swapPools})
 
-  api.add(BSPToken, BSPReserve);
-  api.add(WETHToken, WETHReserve);
-  api.add(USDCToken, USDCReserve);
-  api.add(WBTC, WBTCReserve);
-  api.add(ARB, ARBReserve);
+
+  api.add(tokens, reserves);
+
 }
 
 module.exports = {
-    arbitrum: { tvl },
-    // base: { tvl },
+    arbitrum: { tvl: tvlFactory(AA.ARB) },
+    base: { tvl: tvlFactory(AA.BASE) },
 }
