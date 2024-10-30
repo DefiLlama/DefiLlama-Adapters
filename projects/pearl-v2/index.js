52e4c6e94f62d0c3b3b1214ec8f429f99acad08b
Chase Brown <54334583+chasebrownn@users.noreply.github.com>
2024-10-29 21:15:15 -0700

    Removing blacklisted tokens (#12104)

--- projects/pearl-v2/index.js
+++ projects/pearl-v2/index.js
@@ -1,12 +1,6 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { uniV3Export } = require('../helper/uniswapV3')
 
-const ownTokens = [
-  '0xaec9e50e3397f9ddc635c6c429c8c7eca418a143',
-  ADDRESSES.real.RWA,
-  '0x25ea98ac87a38142561ea70143fd44c4772a16b6',
-]
-
 module.exports = uniV3Export({
-  'real': { factory: '0xeF0b0a33815146b599A8D4d3215B18447F2A8101', fromBlock: 33062, blacklistedTokens: ownTokens,}
+  'real': { factory: '0xeF0b0a33815146b599A8D4d3215B18447F2A8101', fromBlock: 33062,}
 })
