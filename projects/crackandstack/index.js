8d9f7fe45c7d744a7b01d731824b946a8d000c7a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-31 08:30:52 +0100

    code refactor

--- projects/crackandstack/index.js
+++ projects/crackandstack/index.js
@@ -1,7 +1,6 @@
-const { sumTokensExport } = require("../helper/unwrapLPs");
-const coreAssets = require("../helper/coreAssets.json");
 
-const TAIKO_TOKEN = ADDRESSES.taiko.TAIKO;
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const ADDRESSES = require("../helper/coreAssets.json");
 
 const CONTRACTS = ["0x2c301eBfB0bb42Af519377578099b63E921515B7", "0xD8F7cd7d919c5266777FB83542F956dD30E80187", "0x12689b6ddE632E69fBAA70d066f86aC9fDd33dd1"];
 
@@ -12,7 +11,7 @@ module.exports = {
   taiko: {
     tvl: sumTokensExport({
       owners: CONTRACTS,
-      tokens: [coreAssets.null, coreAssets.taiko.USDT, coreAssets.taiko.USDC_e, coreAssets.taiko.USDC, TAIKO_TOKEN]
+      tokens: [ADDRESSES.null, ADDRESSES.taiko.USDT, ADDRESSES.taiko.USDC_e, ADDRESSES.taiko.USDC, ADDRESSES.taiko.TAIKO]
     }),
   },
 }
