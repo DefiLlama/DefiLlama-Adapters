e7150134b887f7c6e6a0868193ee7fd6141b55f5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-31 18:29:42 +0100

    code refactor

--- projects/nabla/index.js
+++ projects/nabla/index.js
@@ -1,73 +1,24 @@
 
+const { getLogs2 } = require('../helper/cache/getLogs')
 
-const abis = {
-  backstopPool: {
-    asset: "function asset() external view returns (address)",
-  },
-  swapPool: {
-    asset: "function asset() external view returns (address)", 
-    reserve: "function reserve() external view returns (uint256)",
+const config = {
+  base: { factory: '0x791Fee7b66ABeF59630943194aF17B029c6F487B', fromBlock: 19980311 },
+  arbitrum: { factory: '0x7bcFc8b8ff61456ad7C5E2be8517D01df006d18d', fromBlock: 240797440 },
+}
+
+Object.keys(config).forEach(chain => {
+  const { factory, fromBlock } = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const logs = await getLogs2({ api, factory, eventAbi: "event SwapPoolRegistered(address indexed sender, address pool, address asset)", fromBlock, })
+      const pools = logs.map(log => log.pool)
+      const tokensAndOwners = logs.map(i => [i.asset, i.pool])
+      let backstops = await api.multiCall({ abi: 'address:backstop', calls: pools })
+      backstops = [...new Set(backstops)]
+      const bTokens = await api.multiCall({ abi: 'address:asset', calls: backstops })
+      backstops.forEach((backstop, i) => tokensAndOwners.push([bTokens[i], backstop]))
+      console.log(tokensAndOwners)
+      return api.sumTokens({ tokensAndOwners })
+    }
   }
-}
-
-const AA = {
-  ARB : [{
-    BSP : "0x337B03C2a7482c6eb29d8047eA073119dc68a29A",
-    WETH: "0x272dF896f4D0c97F65e787f861bb6e882776a155",
-    USDC: "0x058a0875DB2168AF97bbf01043C3e8F751cCd9A8",
-    WBTC: "0x411eF79fE9Df8Ba82A09c7e93FdE85AF5732BF12",
-    ARB:  "0xE70292D6054B753214D555930e0F11CD7206Efeb",
-    swapPools: [
-      "0x272dF896f4D0c97F65e787f861bb6e882776a155",
-      "0x058a0875DB2168AF97bbf01043C3e8F751cCd9A8",
-      "0x411eF79fE9Df8Ba82A09c7e93FdE85AF5732BF12",
-      "0xE70292D6054B753214D555930e0F11CD7206Efeb",
-    ]
-  }],
-  BASE : [{
-    BSP : "0x50841f086891fe57829ee0a809f8B10174892b69",
-    WETH: "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB",
-    CBBTC: "0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85",
-
-    swapPools: [
-      "0x123456C6C27bb57013F4b943A0f032a0ab9c12eB",
-      "0xa83a20F4dCaB1a63a9118E9E432932c8BEB39b85",
-    ]
-  }]
-}
-
-
-
-const tvlFactory = (pools) => async function tvl(api) {
-
-  let swapPools = []
-  for (const ensable of pools) {
-    const BSPToken = await api.call({
-      abi: abis.backstopPool.asset, 
-      target: ensable.BSP
-    });
-    const BSPReserve = await api.call({
-      abi: 'erc20:balanceOf',
-      target: BSPToken,
-      params: [ensable.BSP],
-    });
-
-    api.add(BSPToken, BSPReserve);
-
-    swapPools = [...swapPools, ...ensable.swapPools];
-  }
-
-
-
-  const reserves =  await api.multiCall({  abi: abis.swapPool.reserve, calls: pools[0].swapPools})
-  const tokens =  await api.multiCall({  abi: abis.swapPool.asset, calls: pools[0].swapPools})
-
-
-  api.add(tokens, reserves);
-
-}
-
-module.exports = {
-    arbitrum: { tvl: tvlFactory(AA.ARB) },
-    base: { tvl: tvlFactory(AA.BASE) },
-}
+})
