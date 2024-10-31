4670cacbc9ef5ed093cf7b1af2725c2333209b82
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-10-31 08:36:04 +0100

    minor fix

--- projects/pearl-v1-5/index.js
+++ projects/pearl-v1-5/index.js
@@ -1,6 +1,7 @@
 const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
+  misrepresentedTokens: true,
   'real': {
     tvl: getUniTVL({
       factory: '0xAed0A784f357BE9C3f8113BB227a7517a3444Efe', useDefaultCoreAssets: true,
