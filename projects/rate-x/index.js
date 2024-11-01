5ac4e44c0dd745738e330adef77a5535a4f8ffbb
ratex-tony <tony.y@rate-x.io>
2024-11-01 21:56:51 +0800

    fix: update bbsol earn margin market vault address (#12141)

--- projects/rate-x/index.js
+++ projects/rate-x/index.js
@@ -7,7 +7,7 @@ async function tvl(api) {
       'DY3Rw6BZwf6epvWnVo8DSV6kYptEdCh7HbYmFRpdPxuH',
       '3CppdkMFxuz7ASS27pB35EDbwgfUhwrarFYuWDBWWwHB',
       'Grk7mshVug1TafphUvuYBrzwRqadtmCcf7GGPoPKkgs6',
-      '5mB83ye3jdjneDJDx15HAvYxT7SCsGZF5urpVMzuLr1t',
+      'BpYbhwDZGpPvcKw3cSh5f9UqRaHfuxgz3avW9g324LUz',
       '4nyfJ4JBsRJLij7VGCVUeHwKSLAAku66ptJamoodY29L',
       '6opMSfkHgWsvG5KmZo8y2DuShaDHwXfB6VUuTx6W4Age',
       '4Ejjk5w7HAWvmXYT57s5uwn8rs7i61nbpcTRQ9ABB11M'
