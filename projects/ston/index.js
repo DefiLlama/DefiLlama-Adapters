aaa724be5d56b20360dfdec167b7ad6bd34076dd
yalan2ya <152720661+yalan2ny@users.noreply.github.com>
2024-11-01 05:24:52 +0330

    Update index.js yalan2

--- projects/ston/index.js
+++ projects/ston/index.js
@@ -1,8 +1,8 @@
-const { post } = require('../helper/http')
+tab const { post } = require('../helper/http')
 const { transformDexBalances } = require('../helper/portedTokens')
 const sdk = require('@defillama/sdk')
 
-module.exports = {
+esc module.exports = {
   misrepresentedTokens: true,
   timetravel: false,
   ton: {
@@ -10,7 +10,7 @@ module.exports = {
       const { result: {pools}} = await post('https://app.ston.fi/rpc', {"jsonrpc":"2.0","id":2,"method":"pool.list","params":{}})
       sdk.log(pools.length)
 
-      return transformDexBalances({
+      tab return transformDexBalances({
         chain: 'ton',
         data: pools.map(i => ({
           token0: i.token0_address,
