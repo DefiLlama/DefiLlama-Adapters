5612fbadb2fd182e0866d53588d43baa494abd97
jan-magpie <155635960+jan-magpie@users.noreply.github.com>
2024-11-01 19:31:20 +0530

    added zircuit chain (#12138)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/eigenpie/config.js
+++ projects/eigenpie/config.js
@@ -1,6 +1,13 @@
 module.exports = {
-    ethereum: {
-      eigenConfig: "0x20b70E4A1883b81429533FeD944d7957121c7CAB",
-      eigenStaking: "0x24db6717dB1C75B9Db6eA47164D8730B63875dB7",
-    }
-  };
+  ethereum: {
+    eigenConfig: "0x20b70E4A1883b81429533FeD944d7957121c7CAB",
+    eigenStaking: "0x24db6717dB1C75B9Db6eA47164D8730B63875dB7",
+  },
+  zircuit:{
+    msteth: "0x1C1Fb35334290b5ff1bF7B4c09130885b10Fc0f4",
+    egeth: "0x4bcc7c793534246BC18acD3737aA4897FF23B458",
+    wsteth: "0xf0e673Bc224A8Ca3ff67a61605814666b1234833",
+    weth: "0x4200000000000000000000000000000000000006",
+  }
+
+};
