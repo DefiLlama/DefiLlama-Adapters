beb5e3b8fc6d2dec4b6e5771f575efde53c492db
wangjj250wo <wangjj250wo@qq.com>
2024-11-02 10:06:13 +0800

    fix loxodrome vault address

--- projects/loxodrome/index.js
+++ projects/loxodrome/index.js
@@ -3,7 +3,7 @@ const { mergeExports } = require('../helper/utils')
 const { gmxExports } = require('../helper/gmx')
 const exports1 = uniTvlExport('iotex', '0x92bfa051bf12a0aef9a5e1ac8b2aa7dc1b05a406', { hasStablePools: true, })
 const exports2 = uniTvlExport('iotex', '0x9442E8d017bb3dC2Ba35d75204211e60f86fF0F8', { hasStablePools: true, })
-const perp = gmxExports({ vault: '0x22e239513f03C5cc890DAad846Bb279C4837d242' })
+const perp = gmxExports({ vault: '0x13904291B7d3e87d23070d22Bc34FA514F99Db18' })
 const perpTvl = {
     iotex: {
         tvl: perp,
