9f847e099a6b549fe657148f7d9d9ca54f35415d
santinoYoung <santino.bai@desyn.io>
2024-11-04 17:54:26 +0800

    update tvl

--- projects/desyn-farm/index.js
+++ projects/desyn-farm/index.js
@@ -14,16 +14,6 @@ const APIs = {
   farm: 'https://api.desyn.io/etf/defillama/get_pool_list?strategy_type=StrategyType1',
 }
 
-const chains = {
-  ethereum: 'ethereum',
-  arbitrum: 'arbitrum',
-  btr: 'btr',
-  mode: 'mode',
-  zklink: 'zklink',
-  core: 'core',
-  ailayer: 'ailayer',
-}
-
 const abi = {
   getBalance: "function getBalance(address) view returns (uint256)"
 }
@@ -31,28 +21,28 @@ const abi = {
 async function tvl(api) {
       const {data} = await axios.get(APIs.farm)
       const config = data.data.config
-
+      console.log(config)
       Object.keys(config).forEach(async chain => {
         const { safePools } = config[chain]
         const pools = safePools
-
-        const tokens = await api.multiCall({  abi: 'address[]:getCurrentTokens', calls: pools})
-        const calls = []
-        const allTokens = []
-        let i = 0
-        for (const pool of pools) {
-          for (const token of tokens[i]) {
-            calls.push({ target: pool, params: token })
-            allTokens.push(token)
+        module.exports[chain] = {
+          tvl: async (api) => {
+            const tokens = await api.multiCall({  abi: 'address[]:getCurrentTokens', calls: pools})
+            const calls = []
+            const allTokens = []
+            let i = 0
+            for (const pool of pools) {
+              for (const token of tokens[i]) {
+                calls.push({ target: pool, params: token })
+                allTokens.push(token)
+              }
+              i++
+            }
+            const allBals = await api.multiCall({ abi: abi.getBalance, calls })
+            api.add(allTokens, allBals)
           }
-          i++
         }
-        const allBals = await api.multiCall({ abi: abi.getBalance, calls })
-        api.add(allTokens, allBals)
       })
 }
 
-
-Object.keys(chains).forEach(chain => {
-  module.exports[chain] = { tvl }
-})
+tvl()
