3ca566f2f38c03a0fdeb34bb8ad1312a140a1686
MethLab <167086493+MethLabxyz@users.noreply.github.com>
2024-11-06 13:58:33 +0530

    added new methlab contracts

--- projects/methlab-xyz/index.js
+++ projects/methlab-xyz/index.js
@@ -1,48 +1,70 @@
 const { getLogs } = require('../helper/cache/getLogs')
 const { mergeExports } = require('../helper/utils')
 const { uniV3Export } = require('../helper/uniswapV3')
+
+
+const abi = {
+  evt_Strategy: 'event StrategyStatusChange (address indexed strategy, bool status)',
+  evt_VaultAdded: 'event VaultAdded (address indexed vault)',
+  evt_LoanAdded: 'event LoanAdded (address indexed vault, address indexed loan, address loanImplUsed)',
+  strategy_collToken: 'address:collToken',
+  strategy_borrowToken: 'address:borrowToken'
+}
+
 const config = {
   mantle: {
-    factories: [{ target: '0x6Cc0c2D8F9533dFd2276337DdEaBBCEE9dd0747F', fromBlock: 51253051 },],
+    factories: [{ target: '0xB375DfF90F8dafeA50E2EA7a0666B426Ed786C5D', fromBlock: 69177970 },],
   }
 }
 
 Object.keys(config).forEach(chain => {
-  const { factories } = config[chain]
+  const { factories } = config[chain];
+
   module.exports[chain] = {
     tvl: async (api) => {
-      const vaults = []
-      for (const { target, fromBlock, } of factories) {
-        const logs = await getLogs({ api, target, fromBlock, onlyArgs: true, eventAbi: 'event VaultAdded (address indexed vault)' })
-        vaults.push(...logs.map(log => log.vault))
-      }
-      const counters = await api.multiCall({ abi: abi.collectionCounter, calls: vaults })
-      const callOwners = []
-      const calls = []
-      for (let i = 0; i < vaults.length; i++) {
-        const counter = counters[i]
-        for (let j = 0; j < +counter; j++) {
-          calls.push({ target: vaults[i], params: j })
-          callOwners.push(vaults[i])
+      const { target, fromBlock } = factories[0];
+
+      const strategyChanges = await getLogs(
+        {
+          api,
+          target,
+          fromBlock,
+          onlyArgs: true,
+          eventAbi: abi.evt_Strategy
         }
-      }
-      const results = await api.multiCall({ abi: abi.collection, calls })
-      const tokenSet = new Set()
-      results.forEach((result, i) => {
-        tokenSet.add(result.collToken)
-        tokenSet.add(result.borrowToken)
-        // tokensAndOwners.push([result.collToken, callOwners[i]])
-        // tokensAndOwners.push([result.borrowToken, callOwners[i]])
-      })
-      return api.sumTokens({ owners: vaults, tokens: Array.from(tokenSet), blacklistedTokens: ['0x401307732d732dd3b05ac1138b8661c0f55830ea'] })
+      );
+
+
+      const vaults = (await getLogs(
+        {
+          api,
+          target,
+          fromBlock,
+          onlyArgs: true,
+          eventAbi: abi.evt_VaultAdded
+        }
+      )).map(log => log.vault);
+
+
+      const loans = (await getLogs({
+        api,
+        target,
+        fromBlock,
+        onlyArgs: true,
+        eventAbi: abi.evt_LoanAdded
+      })).map(log => log.loan);
+
+      const strategies = [...new Set(strategyChanges.map(log => log.strategy))];
+      const collTokens = await api.multiCall({ abi: abi.strategy_collToken, calls: strategies });
+      const borrowTokens = await api.multiCall({ abi: abi.strategy_borrowToken, calls: strategies });
+
+      const tokens = [...new Set([...collTokens, ...borrowTokens])];
+
+
+      return api.sumTokens({ tokens, owners: [...vaults, ...loans] });
     }
   }
-})
-
-const abi = {
-  "collection": "function collection(uint256 collectionId) view returns ((address collToken, address borrowToken, uint256 minSingleLoanAmt, uint256 maxSingleLoanAmt, uint256 expiresAt, bool isEnabled, (uint256 strikePrice, uint256 interestRate, uint256 duration)[] intents))",
-  "collectionCounter": "uint256:collectionCounter",
-}
+});
 
 module.exports = mergeExports([
   module.exports,
