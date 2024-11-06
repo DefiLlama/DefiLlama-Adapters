77cba4675765a196092e5e63329a5f2907c6d73a
Giuseppe Palazzolo <38705839+palace22@users.noreply.github.com>
2024-11-06 11:19:49 +0100

    Add ARB, ETH Arb and SolvBTC assets on Folks Finance xChain (#12182)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/folks-xchain/constants.js
+++ projects/folks-xchain/constants.js
@@ -30,9 +30,14 @@ const HubPools = {
       tokenAddress: ADDRESSES.avax.BTC_b,
       chainPoolAddress: '0xef7a6EBEDe2ad558DB8c36Df65365b209E5d57dC',
     },
+    { // SolvBTC      
+      poolAddress: "0x307bCEC89624660Ed06C97033EDb7eF49Ab0EB2D",
+      tokenAddress: '0xbc78D84Ba0c46dFe32cf2895a19939c86b81a777',
+    },
   ],
   'ethereum': [
     // excluding USDC cause bridged
+    // excluding SolvBTC cause bridged
     { // ETH_eth      
       poolAddress: "0xB6DF8914C084242A19A4C7fb15368be244Da3c75",
       tokenAddress: ADDRESSES.null,
@@ -46,7 +51,8 @@ const HubPools = {
   ],
   'base': [
     // excluding USDC cause bridged
-    { // ETH_base      
+    // excluding SolvBTC cause bridged
+    { // ETH_base
       poolAddress: "0x51958ed7B96F57142CE63BB223bbd9ce23DA7125",
       tokenAddress: ADDRESSES.null,
       chainPoolAddress: '0xe3B0e4Db870aA58A24f87d895c62D3dc5CD05883',
@@ -58,7 +64,8 @@ const HubPools = {
     },
   ],
   'bsc': [
-    { // BNB      
+    // excluding SolvBTC cause bridged
+    { // BNB
       poolAddress: "0x89970d3662614a5A4C9857Fcc9D9C3FA03824fe3",
       tokenAddress: ADDRESSES.null,
       chainPoolAddress: '0x5f2F4771B7dc7e2F7E9c1308B154E1e8957ecAB0',
@@ -72,6 +79,20 @@ const HubPools = {
       poolAddress: "0xC2FD40D9Ec4Ae7e71068652209EB75258809e131",
       tokenAddress: ADDRESSES.bsc.BTCB,
       chainPoolAddress: '0x12Db9758c4D9902334C523b94e436258EB54156f',
+    },
+  ],
+  'arbitrum': [
+    // excluding USDC cause bridged
+    // excluding SolvBTC cause bridged
+    { // ETH_arb
+      poolAddress: "0x44E0d0809AF8Ee37BFb1A4e75D5EF5B96F6346A3",
+      tokenAddress: ADDRESSES.null,
+      chainPoolAddress: '0x37d761883a01e9F0B0d7fe59EEC8c21D94393CDD',
+    },
+    { // ARB      
+      poolAddress: "0x1177A3c2CccDb9c50D52Fc2D30a13b2c3C40BCF4",
+      tokenAddress: ADDRESSES.arbitrum.ARB,
+      chainPoolAddress: '0x1b2a8d56967d00700DD5C94E27B1a116a1deF8Df',
     },
   ]
 }
