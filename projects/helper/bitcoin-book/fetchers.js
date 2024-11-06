0be91ee9009e48b751228fe58d4cd28c1a6ff4b6
0xpeluche <0xpeluche@proton.me>
2024-11-06 11:46:26 +0100

    add exsatBridge

--- projects/helper/bitcoin-book/fetchers.js
+++ projects/helper/bitcoin-book/fetchers.js
@@ -86,5 +86,42 @@ module.exports = {
     const API_URL = 'https://api.threshold.network/tbtc/wallets/pof'
     const { wallets } = await getConfig('tbtc/wallets', API_URL)
     return wallets.filter(i => +i.walletBitcoinBalance > 0).map(wallet => wallet.walletBitcoinAddress)
+  },
+  exsatBridge: async () => {
+    const API_URL = 'https://raw.githubusercontent.com/exsat-network/exsat-defillama/refs/heads/main/bridge-bitcoin.json'
+    const API2_URL = 'https://rpc-us.exsat.network/v1/chain/get_table_rows'
+
+    const config = await getConfig('exsat', API_URL)
+    const custody_addresses = config['custody_addresses'];
+    const custody_ids = config['custody_ids'];
+    const owners = [...custody_addresses];
+
+    for (let custody_id of custody_ids) {
+      let lower_bound = null;
+      let hasMore = true;
+
+      while (hasMore) {
+          const { data: response } = await axios.post(API2_URL, {
+              "json": true,
+              "code": "brdgmng.xsat",
+              "scope": custody_id,
+              "table": "addrmappings",
+              "lower_bound": lower_bound,
+              "upper_bound": null,
+              "index_position": 1,
+              "key_type": "",
+              "limit": "100",
+              "reverse": false,
+              "show_payer": true
+          });
+
+          const addrs = response.rows.map(row => row.data.btc_address);
+          owners.push(...addrs);
+
+          hasMore = response.more;
+          lower_bound = response.next_key;
+      }
+    }
+    return owners
   }
 }
