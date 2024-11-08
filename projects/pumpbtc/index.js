fb6927ebc31219c0685bf2d12de424ca8079efab
Tony <feiyingxyz@qq.com>
2024-11-08 14:35:48 +0800

    feat: udpate pumpbtc adapter logic (#12147)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/pumpbtc/index.js
+++ projects/pumpbtc/index.js
@@ -1,30 +1,46 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokens } = require('../helper/sumTokens');
-const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
+const utils = require('../helper/utils');
+const { getConfig } = require('../helper/cache');
+const bitcoinBook = require('../helper/bitcoin-book');
 
 module.exports = {
   methodology: 'TVL for pumpBTC is calculated based on the total value of WBTC, FBTC, BTCB held in the contract that were utilized in the minting process of pumpBTC.',
 }
-const config = {
-  ethereum: { owners: ['0x1fCca65fb6Ae3b2758b9b2B394CB227eAE404e1E', '0x3d9bCcA8Bc7D438a4c5171435f41a0AF5d5E6083', '0xAC364d14020f1da0044699691a91f06ca6131Fe3', '0x1fCca65fb6Ae3b2758b9b2B394CB227eAE404e1E'], tokens: ['0xC96dE26018A54D51c097160568752c4E3BD6C364', ADDRESSES.ethereum.WBTC], },
-  bsc: { owners: ['0x2Ee808F769AB697C477E0aF8357315069b66bCBb', '0x80922aD2771c5Ea9C14bA5FF4a903EC6B0f7e7C9', '0x2b4B9047C9fEA54705218388bFC7Aa7bADA4BB5E', '0x8A0727B87fa1027c419c3aa2caf56C799d5Bd8c5'], tokens: [ADDRESSES.bsc.BTCB, '0xC96dE26018A54D51c097160568752c4E3BD6C364'], },
-  mantle: { owners: ['0xd6Ab15B2458B6EC3E94cE210174d860FdBdd6b96'], tokens: ['0xC96dE26018A54D51c097160568752c4E3BD6C364'], },
-  bitcoin: {},
-  base: { owners: ['0x1fCca65fb6Ae3b2758b9b2B394CB227eAE404e1E', '0x4913D495cBA3e1380218d2258126F22Ea5dE5f8B', '0xC7DA129335F8815d62fBd3ca7183A3b2791CdB5e', '0xca873913BBf124441857d32Bb23f723b68433465', '0xF1D06Be8dF2F7Ed4Cdc9ac05915EA2b618FFA3Fb'], tokens: ['0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf'], },
+
+async function getStakingAddresses() {
+  let res = await utils.fetchURL('https://dashboard.pumpbtc.xyz/api/dashboard/asset/tokenowners')
+
+  const btcAddresses = res.data.data || {}
+  //console.log('>>', btcAddresses.length) 
+  return btcAddresses
 }
 
-Object.keys(config).forEach(chain => {
-  const { owners, tokens, } = config[chain]
+async function bitcoinTvl(api) {
+  const owners = await bitcoinBook.pumpBTC()
+  return sumTokens({ api, owners })
+}
 
-  if (chain === 'bitcoin') {
+async function otherTvl(api) {
+  const addresses = await getConfig('pumpbtc/v2', undefined, { fetcher: getStakingAddresses })
+
+  if (!addresses[api.chain]) {
+    return;
+  }
+
+  const { owners, tokens } = addresses[api.chain]
+  return api.sumTokens({ owners, tokens })
+}
+
+module.exports.isHeavyProtocol = true;
+
+['bitcoin', 'ethereum', 'bsc', 'mantle', 'base'].forEach(chain => {
+  if (chain == 'bitcoin') {
     module.exports[chain] = {
-      tvl: async (api) => { return sumTokens({ api, owners: await bitcoinAddressBook.pumpBTC() }) }
+      tvl: bitcoinTvl,
     }
   } else {
     module.exports[chain] = {
-      tvl: async (api) => api.sumTokens({ owners, tokens })
+      tvl: otherTvl
     }
   }
 })
-
-module.exports.isHeavyProtocol = true
