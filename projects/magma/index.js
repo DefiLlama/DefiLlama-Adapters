25e0795277d93c6bfed13c3a844660bebcd5a3c6
Si Yushin <siyushin@hotmail.com>
2024-11-09 20:02:05 +0800

    Magma v3 (#12216)
    Co-authored-by: magmastone <152827065+magmastone@users.noreply.github.com>

--- projects/magma/index.js
+++ projects/magma/index.js
@@ -5,8 +5,8 @@ const sdk = require('@defillama/sdk')
 module.exports = {
   iotex: {
     tvl: sdk.util.sumChainTvls([
-      getLiquityTvl('0x21d81DABF6985587CE64C2E8EB12F69DF2178fe2'),
-      getLiquityTvl('0xAeB0B38040aDdc4a2b520919f13944D9bC944435')
+      getLiquityTvl('0x4315BcE6076953571caf1903d15D682727FBD935'),
+      getLiquityTvl('0xFF5F4bA96586EDae7E7D838D8770dFB3376Ec245', { nonNativeCollateralToken: true, collateralToken: "0x236f8c0a61dA474dB21B693fB2ea7AAB0c803894" })
     ]),
     // staking: staking('0x037a2e9a464fbA409D0E55600836864B410d6Dd8', '0x6C0bf4b53696b5434A0D21C7D13Aa3cbF754913E'), // disabled as backing of WEN is already counted towards tvl
   },
