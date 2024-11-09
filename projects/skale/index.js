d3fdf763bd698d0eb439d340cdcfeec812171d26
TheGreatAxios <sawyer@dirtroad.dev>
2024-11-08 16:25:50 -0800

    Add SKALE IMA Bridge Tracking

--- projects/skale/index.js
+++ projects/skale/index.js
@@ -23,14 +23,14 @@ async function tvl(_, block, _c) {
         '0x50de6856358cc35f3a9a57eaaa34bd4cb707d2cd', // Razor
         '0x8e870d67f660d95d5be530380d0ec0bd388289e1', // USDP
         '0x02de007d412266a2e0fa9287c103474170f06560', // Exorde
-        '0x8fB1A35bB6fB9c47Fb5065BE5062cB8dC1687669', // Human Token
+        '0xd1ba9BAC957322D6e8c07a160a3A8dA11A0d2867', // Human Token
         '0x6307B25A665Efc992EC1C1bC403c38F3dDd7c661', // GCR
         '0x3300b02efa180c99a2f61f4731665b51e4e254c4', // Hitmakr
         '0x2d94aa3e47d9d5024503ca8491fce9a2fb4da198', // BANK
         '0x0954906da0Bf32d5479e25f46056d22f08464cab', // index
         '0x0E186357c323c806C1efdad36D217F7a54b63D18', // CGT2.0
-        '0x8fB1A35bB6fB9c47Fb5065BE5062cB8dC1687669', // PEPE
-        '0x8fB1A35bB6fB9c47Fb5065BE5062cB8dC1687669', // CSC
+        '0x6982508145454ce325ddbe47a25d4ec3d2311933', // PEPE
+        '0xfdcdfa378818ac358739621ddfa8582e6ac1adcb', // CSC
     ];
 
     erc20Tokens.map(i => {
