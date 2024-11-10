828488d16aabb85f61952d05f93118019ccff464
ungaro <alp.guneysel@gmail.com>
2024-11-10 11:32:01 -0500

    fix comments

--- projects/nest-staking/index.js
+++ projects/nest-staking/index.js
@@ -1,8 +1,8 @@
 const RWA_STAKING = "0xdbd03d676e1cf3c3b656972f88ed21784372acab"
 const RESERVE_STAKING = "0xba0ae7069f94643853fce3b8af7f55acbc11e397"
-const SBTC = "0x094c0e36210634c3cfa25dc11b96b562e0b07624"  // Our SBTC
+const SBTC = "0x094c0e36210634c3cfa25dc11b96b562e0b07624" 
 const STONE = "0x7122985656e38bdc0302db86685bb972b145bd3c"
-const SBTC_PRICE = "0xfe18be6b3bd88a2d2a7f928d00292e7a9963cfc6"  // sBTC for price
+const SBTC_PRICE = "0xfe18be6b3bd88a2d2a7f928d00292e7a9963cfc6"
 
 async function tvl(api) {
 
@@ -30,6 +30,6 @@ async function tvl(api) {
 }
 
 module.exports = {
-  methodology: "Counts total value locked in both RWA Staking (stablecoins) and Reserve Staking (SBTC and STONE) contracts. SBTC price is derived from sBTC token.",
+  methodology: "Counts total value locked in both RWA Staking (stablecoins) and Reserve Staking (SBTC and STONE) contracts.",
   ethereum: { tvl }
 }
