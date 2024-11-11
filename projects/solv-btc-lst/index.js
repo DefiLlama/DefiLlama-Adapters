88c37e53e72a52329bdfc3b6853e9725015f7a7f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-11 07:31:37 +0100

    update solv lst #12228

--- projects/solv-btc-lst/index.js
+++ projects/solv-btc-lst/index.js
@@ -5,8 +5,22 @@ async function tvl() {
   return sumTokens({ owners: await bitcoinAddressBook.solvBTC() })
 }
 
+
+const solvbtclstListUrl = 'https://raw.githubusercontent.com/solv-finance-dev/slov-protocol-defillama/main/solvbtc-lst.json';
+
+async function evmTVL(api) {
+  let solvbtclst = await getConfig('solv-protocol/solv-btc-lst', solvbtclstListUrl)
+  let { depositAddress: owners, tokens } = solvbtclst[api.chain]?.otherDeposit ?? {}
+  api.sumTokens({ owners, tokens })
+}
+
 module.exports = {
   methodology: 'Staked tokens via Babylon and Core are counted towards TVL, as they represent the underlying BTC assets securing their respective networks.',
-  doublecounted:true,
+  doublecounted: true,
   bitcoin: { tvl }
 }
+
+const chains = ['ethereum']
+chains.forEach(chain => {
+  module.exports[chain] = { tvl: evmTVL }
+})
