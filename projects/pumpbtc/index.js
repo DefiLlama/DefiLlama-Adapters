633e3c73f2f2aa8721450a38ed6e961c5a0c8f74
David Zhang <125990317+zhangdav@users.noreply.github.com>
2024-11-12 01:47:50 +0700

    Add support for Arbitrum chain in PumpBTC (#12239)

--- projects/pumpbtc/index.js
+++ projects/pumpbtc/index.js
@@ -11,7 +11,7 @@ async function getStakingAddresses() {
   let res = await utils.fetchURL('https://dashboard.pumpbtc.xyz/api/dashboard/asset/tokenowners')
 
   const btcAddresses = res.data.data || {}
-  //console.log('>>', btcAddresses.length) 
+  //console.log('>>', btcAddresses.length)
   return btcAddresses
 }
 
@@ -33,7 +33,7 @@ async function otherTvl(api) {
 
 module.exports.isHeavyProtocol = true;
 
-['bitcoin', 'ethereum', 'bsc', 'mantle', 'base'].forEach(chain => {
+['bitcoin', 'ethereum', 'bsc', 'mantle', 'base', 'arbitrum'].forEach(chain => {
   if (chain == 'bitcoin') {
     module.exports[chain] = {
       tvl: bitcoinTvl,
