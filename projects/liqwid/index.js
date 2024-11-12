2d3e64e26b87af7f282f4a8e20f94c938717d739
rcheiko <cheikororo69005@hotmail.fr>
2024-11-13 00:29:49 +0100

    fix:🐛add USDC/USDT to include them in the TVL

--- projects/liqwid/index.js
+++ projects/liqwid/index.js
@@ -61,8 +61,10 @@ const query = `query($input: MarketsInput)  {
 const tokenMapping = {
   Ada: 'lovelace',
   DJED: '8db269c3ec630e06ae29f74bc39edd1f87c819f1056206e879a1cd61446a65644d6963726f555344',
+  USDM: 'c48cbb3d5e57ed56e276bc45f99ab39abe94e6cd7ac39fb402da47ad0014df105553444d',
   DAI: 'dai',
-  USDM: 'c48cbb3d5e57ed56e276bc45f99ab39abe94e6cd7ac39fb402da47ad0014df105553444d'
+  USDC: 'usd-coin',
+  USDT: 'tether',
 }
 
 const getToken = (market) => tokenMapping[market.id] ?? market.asset.currencySymbol + market.asset.name
