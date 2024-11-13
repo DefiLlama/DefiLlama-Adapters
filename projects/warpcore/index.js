1444c8e2a4bb838f2e7a76f5e8f6af4137177949
clcost <188273260+clcost33@users.noreply.github.com>
2024-11-12 19:43:19 -0600

    Added adapter for Warpcore

---
+++ projects/warpcore/index.js
@@ -0,0 +1,24 @@
+// Contract addresses for token, vaults, and other relevant CAs
+const CONTRACT_ADDRESSES = {
+    WARPSPEED_VELVETCAPITAL_VAULT: '0x8177ec2734d0f32dab7ab7269cbbe7d7232d1932',
+    WARPCORE_TOKEN: '0xE8E286B378254c4913c0C6964361636384b9D018'
+  };
+
+// Calculate TVL of Warpcore (WARP)
+async function tvl(api) {
+    const warpcoreCollateral = await api.call({
+        abi: 'erc20:balanceOf',
+        target: CONTRACT_ADDRESSES.WARPCORE_TOKEN,
+        params: [CONTRACT_ADDRESSES.WARPSPEED_VELVETCAPITAL_VAULT],
+    });
+
+    api.add(CONTRACT_ADDRESSES.WARPCORE_TOKEN, warpcoreCollateral);
+
+}
+
+module.exports = {
+    base: {
+        tvl,
+    },
+    methodology: 'The TVL encompasses the total value of Warpcore ($CORE) that are in the Velvet.Capital Warpspeed Vault on Base.'
+}
