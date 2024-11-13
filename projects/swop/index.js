02b0395707b609a56cfac2b25af5573b57aa1fa1
Vladislav Petushkov <vpetushkov@web3tech.ru>
2024-11-13 23:58:14 +0400

    add protocol to new network unit0

--- projects/swop/index.js
+++ projects/swop/index.js
@@ -1,10 +1,12 @@
 const { sumTokens, call } = require('../helper/chain/waves');
 const { getConfig } = require("../helper/cache");
+const { request, gql } = require("graphql-request");
+const {toUSDTBalances} = require("../helper/balances");
 // const { get } = require("../helper/http");
-// const { toUSDTBalances } = require("../helper/balances");
 
 
 const swopfiBackendEndpoint = "https://backend.swop.fi";
+const unit0Endpoint = "http://graphql-node-htz-fsn1-1.wvservices.com:8000/subgraphs/name/swopfi/swopfi-units"
 
 const getSwopFiTVL = async (api) => {
   // const poolsStats = await get(`${swopfiBackendEndpoint}/pools`);
@@ -16,10 +18,26 @@ const getSwopFiTVL = async (api) => {
   }
 }
 
+const getSwopFiUnit0TVL = async() => {
+  const graphQuery = gql`
+query get_tvl($block: Int) {
+  swopfiFactories(first: 1) {
+    totalLiquidityUSD
+  }
+}
+`
+  const swopfiFactories = (await request(unit0Endpoint, graphQuery))['swopfiFactories']
+  const usdTvl = Number(swopfiFactories[0]['totalLiquidityUSD'])
+  return toUSDTBalances(usdTvl)
+}
+
 
 module.exports = {
   timetravel: false, // Waves blockchain,
   methodology: "Counts the tokens locked on AMM pools",
+  hallmarks: [
+      [1730299107, "Unit0 Protocol Lunch"]
+  ],
   waves: {
     tvl: getSwopFiTVL,
     staking: async () => {
@@ -28,5 +46,8 @@ module.exports = {
       const res = await call({ target: '3PLHVWCqA9DJPDbadUofTohnCULLauiDWhS', key: 'total_GSwop_amount' })
       return { swop: res / 1e8 }
     }
+  },
+  unit0: {
+    tvl: getSwopFiUnit0TVL
   }
 };
