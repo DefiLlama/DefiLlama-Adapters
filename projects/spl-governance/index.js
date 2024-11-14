9e68120589cf88b7faeeea552b7c77e786c1bced
Utkarsh <83659045+0xShuk@users.noreply.github.com>
2024-11-14 20:20:19 +0530

    add token-wise tvl data

--- projects/spl-governance/index.js
+++ projects/spl-governance/index.js
@@ -3,11 +3,20 @@ const url = 'https://realms-tvl.vercel.app/tvl/latest'
 
 async function tvl() {
   const {totalValueUsd} = await getCache(url)
-  return {usd:  parseFloat(totalValueUsd)}
+  const {tokens} = totalValueUsd
+
+  const returnTokens = {}
+
+  for (const token of tokens) {
+    console.log("Treasury addresses holding", token.token_symbol, ": ", token.holders)
+    returnTokens[token.token_symbol] = token.balance
+  }
+
+  return returnTokens
 }
 
 module.exports = {
-  methodology: 'SOL, SPL tokens and stables held in the contracts are counted under tvl', 
+  methodology: 'SOL, SPL tokens and stables held in the treasury by the DAOs are counted under tvl', 
   timetravel: false,
   solana: { tvl }
 }
