2036249e36abe62246061821d365c83f649b7050
basedjosh <basedai@google.com>
2024-11-14 22:19:14 +0300

    mistake

--- projects/basedai/index.js
+++ projects/basedai/index.js
@@ -18,5 +18,5 @@ module.exports = {
       TOKENS
     )
   },
-  methodology: "Currently, the TVL is considered as the amount of Pepecoin and basedAI tokens held in the farming contract at '0xa6b816010ab51e088c4f19c71aba87e54b422e14'."
+  methodology: "Currently, the TVL is considered as the amount of Pepecoin and basedAI tokens held in the farming contracts."
 };
