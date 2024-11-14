15ca1a75858dcaad8584d4779b04cb571e064b68
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-14 13:11:24 +0100

    Bob (#12294)
    Co-authored-by: Brendon Votteler <bvotteler@users.noreply.github.com>

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -79,9 +79,9 @@ const fixBalancesTokens = {
     '0xac485391eb2d7d88253a7f1ef18c37f4242d1a24': { coingeckoId: 'lisk', decimals: 18 },
   },
   bob: {
-    "0x541fd749419ca806a8bc7da8ac23d346f2df8b77": { coingeckoId: "solv-btc", decimals: 18 },
-    "0xcc0966d8418d412c599a6421b760a847eb169a8c": { coingeckoId: "solv-btc", decimals: 18 },
-    "0x236f8c0a61da474db21b693fb2ea7aab0c803894": { coingeckoId: "universal-btc", decimals: 8 },
+    [ADDRESSES.bob.SolvBTC]: { coingeckoId: "solv-btc", decimals: 18 },
+    [ADDRESSES.bob.SolvBTC_BBN]: { coingeckoId: "solv-protocol-solvbtc-bbn", decimals: 18 },
+    [ADDRESSES.bob.uniBTC]: { coingeckoId: "universal-btc", decimals: 8 }
   },
   flow: {
     '0xd3bF53DAC106A0290B0483EcBC89d40FcC961f3e': { coingeckoId: 'flow', decimals: 18 },
