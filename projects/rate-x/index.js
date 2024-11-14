5b6d38981dba23365783643ce19800c45331cf87
ratex-tony <tony.y@rate-x.io>
2024-11-15 02:18:44 +0800

    feat: add usde vaults (#12298)

--- projects/rate-x/index.js
+++ projects/rate-x/index.js
@@ -12,7 +12,10 @@ async function tvl(api) {
       '6opMSfkHgWsvG5KmZo8y2DuShaDHwXfB6VUuTx6W4Age',
       '4Ejjk5w7HAWvmXYT57s5uwn8rs7i61nbpcTRQ9ABB11M',
       '4xq7VjrJCU2Smk5JcJToik5hiEJ8RCvECReePP8Jg6q8',
-      'B2YeVM6Kf3SKYLuH2nfucCmZwy8KJcQpd9e9JEuwv9mt'
+      'B2YeVM6Kf3SKYLuH2nfucCmZwy8KJcQpd9e9JEuwv9mt',
+      '8DeQth4AWPXauRfgAEUy9WpHuyKKyYuNNsH76C5v1Hv7',
+      'FS7TTuJejy7zjkdJXD9BjeLFZ44ipxxr2qmMMUKMZv6y',
+      '6K8yrdpm2dVaLSLpqoRJKv7SNuP54xmbv5KULcJzKTHc'
     ],
     balances: api.getBalances()
   })
