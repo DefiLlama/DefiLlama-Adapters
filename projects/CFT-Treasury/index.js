1daea9d560b77e9ea65b4d951302b25e5653eac6
ClaimTRX <167689685+ClaimTRX@users.noreply.github.com>
2024-11-14 14:07:25 +0100

    Update index.js

--- projects/CFT-Treasury/index.js
+++ projects/CFT-Treasury/index.js
@@ -6,7 +6,6 @@ module.exports = {
     tvl: sumTokensExport({
       tokensAndOwners: [
         [nullAddress, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
-         [ADDRESSES.tron.TRX, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
         [ADDRESSES.tron.JST, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
         [ADDRESSES.tron.SUN, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
         [ADDRESSES.tron.JM, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
