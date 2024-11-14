99da3b593b34dfea1481fc0773b27c896fe60177
ClaimTRX <167689685+ClaimTRX@users.noreply.github.com>
2024-11-14 12:59:18 +0100

    Update index.js

--- projects/CFT-Treasury/index.js
+++ projects/CFT-Treasury/index.js
@@ -1,14 +1,13 @@
-const { sumTokensExport} = require('../helper/unwrapLPs')
-const ADDRESSES = require('../helper/coreAssets.json');
+const ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokensExport, nullAddress, } = require('../helper/sumTokens')
 
 module.exports = {
-    timetravel: false,
-    misrepresentedTokens:false,
-   
-        tvl: sumTokensExport({ 
-          tokensAndOwners: [
-            [ADDRESSES.tron, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq']
-          ],
-        }),
-    }
+  tron: {
+    tvl: sumTokensExport({
+      tokensAndOwners: [
+        [nullAddress, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
+        
+      ]
+    }),
+  },
 };
