a16baf4ee33f5118e580c240f24cb0437804be77
clcost <188273260+clcost33@users.noreply.github.com>
2024-11-13 19:10:23 -0600

    Added Speed / Core LP to Pool count

--- projects/warpcore/index.js
+++ projects/warpcore/index.js
@@ -1,10 +1,13 @@
-// Contract addresses for token, vaults, and other relevant CAs
+const { pool2 } = require("../helper/pool2");
+
+// Contract addresses for token, vaults, and pools
 const CONTRACT_ADDRESSES = {
     WARPSPEED_VELVETCAPITAL_VAULT: '0x8177ec2734d0f32dab7ab7269cbbe7d7232d1932',
-    WARPCORE_TOKEN: '0xE8E286B378254c4913c0C6964361636384b9D018'
-  };
+    WARPCORE_TOKEN: '0xE8E286B378254c4913c0C6964361636384b9D018',
+    UNISWAP_PAIR: '0x5B4Cd326A33467532a0f4C3BF947561bcE1FAE92'
+};
 
-// Calculate TVL of Warpcore (WARP)
+// Calculate TVL of Warpcore (WARP) on Velvet.Capital
 async function tvl(api) {
     const warpcoreCollateral = await api.call({
         abi: 'erc20:balanceOf',
@@ -18,7 +21,8 @@ async function tvl(api) {
 
 module.exports = {
     base: {
-        tvl,
+        tvl: tvl,
+        pool2: pool2(CONTRACT_ADDRESSES.UNISWAP_PAIR, CONTRACT_ADDRESSES.WARPCORE_TOKEN, 'base')
     },
     methodology: 'The TVL encompasses the total value of WarpCore ($Core) that are in the Velvet.Capital Warpspeed Vault on Base.'
 }
