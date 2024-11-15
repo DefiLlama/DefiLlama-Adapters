c23ade3d516320187fe9e816cc6714e79d0696c3
ClaimTRX <167689685+ClaimTRX@users.noreply.github.com>
2024-11-15 14:49:14 +0100

    Update index.js

--- projects/CFT-Treasury/index.js
+++ projects/CFT-Treasury/index.js
@@ -1,15 +1,21 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport, nullAddress, } = require('../helper/sumTokens')
+const { get, post, } = require('../http')
+
+async function getStakedTron(account) {
+  const data = await get(`https://apilist.tronscan.org/api/vote?candidate=${account}`)
+  return data.totalVotes
+}
+
+async function getTrxBalance(account) {
+  const data = await post('https://api.trongrid.io/wallet/getaccount', {
+    address: TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq,
+    visible: true,
+  })
+  return data.balance + (data.frozen?.reduce((t, { frozen_balance }) => t + frozen_balance, 0) ?? 0)
+}
 
 module.exports = {
-  tron: {
-    tvl: sumTokensExport({
-      tokensAndOwners: [
-        [nullAddress, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
-        [ADDRESSES.tron.JST, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
-        [ADDRESSES.tron.SUN, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
-        [ADDRESSES.tron.JM, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
-      ]
-    }),
-  },
-};
+  getStakedTron,
+  getTrxBalance,
+}
