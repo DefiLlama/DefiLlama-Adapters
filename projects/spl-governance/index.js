99ee84d918400fbec950307c76162fcb3847b9d3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-15 11:11:26 +0100

    use usd value of tokens

--- projects/spl-governance/index.js
+++ projects/spl-governance/index.js
@@ -8,11 +8,12 @@ const url = 'https://realms-tvl.vercel.app/tvl/latest'
 const isSolOrStable = (token) => ['sol', 'usd', 'btc', 'eth'].some(i => token.token_symbol.toLowerCase().includes(i))
 
 async function tvl(api, isStaking = false) {
-  const connnection = getConnection()
+  // const connnection = getConnection()
   let { totalValueUsd: { tokens } } = await get(url)
   const filterFn = isStaking ? i => !isSolOrStable(i) : isSolOrStable
   tokens = tokens.filter(filterFn)
-  const decimalsMap = {}
+  tokens.forEach(i => api.addUSDValue(i.value))
+  /* const decimalsMap = {}
   const _tokens = tokens.map(i => i.token)
   const chunks = sliceIntoChunks(_tokens, 99)
 
@@ -24,10 +25,11 @@ async function tvl(api, isStaking = false) {
   }
 
   for (const token of tokens)
-    api.add(token.token, +token.balance * 10 ** decimalsMap[token.token])
+    api.add(token.token, +token.balance * 10 ** decimalsMap[token.token]) */
 }
 
 module.exports = {
+  misrepresentedTokens: true,
   methodology: 'SOL token and stables held in the contracts are counted under tvl, gov tokens are counted under staking',
   timetravel: false,
   solana: { tvl: api => tvl(api), staking: api => tvl(api, true), }
