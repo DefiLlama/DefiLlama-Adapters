5f81037c6a15a0519da97ba33791a4cc1e0596cc
ClaimTRX <167689685+ClaimTRX@users.noreply.github.com>
2024-11-15 14:52:53 +0100

    Update index.js

--- projects/CFT-Treasury/index.js
+++ projects/CFT-Treasury/index.js
@@ -1,18 +1,15 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport, nullAddress, } = require('../helper/sumTokens')
 
-
-
-
-async function getTrxBalance(account) {
-  const data = await post('https://api.trongrid.io/wallet/getaccount', {
-    address: TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq,
-    visible: true,
-  })
-  return data.balance + (data.frozen?.reduce((t, { frozen_balance }) => t + frozen_balance, 0) ?? 0)
-}
-
 module.exports = {
- 
-  getTrxBalance,
-}
+  tron: {
+    tvl: sumTokensExport({
+      tokensAndOwners: [
+        [nullAddress, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
+        [ADDRESSES.tron.JST, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
+        [ADDRESSES.tron.SUN, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
+        [ADDRESSES.tron.JM, 'TFvHNqDqttkXSS8ZTdC4c4W7q97SFW3iKq'],
+      ]
+    }),
+  },
+};
