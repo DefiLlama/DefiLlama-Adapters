a442cde5818dfe41da201cca90d7be0cd3b07313
Antoine <67124021+antoineborg@users.noreply.github.com>
2024-11-15 14:24:11 +0100

    SwissBorg: Add new wallets (#12309)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/swissborg/index.js
+++ projects/swissborg/index.js
@@ -47,6 +47,7 @@ const config = {
       '9qoUcyhKSWMbk6tqGUYQUpeosPcdUnJszG4eQKwfe4gL',
       'Fe7SEekiKygziaEGKxsDsgLVzrCfNvVBvAYsaJBwFA8s',
       'AR2ecEWY2vfsXmd4fUxc196LhbX5p8TnhvJg8t3fgYUN',
+      '7Sng9GTnkjjb8WTF2kYX8JWqGHHwJGk5Ke9639zREUAR',
     ],
   },
   polkadot: {
@@ -109,7 +110,12 @@ const config = {
     owners: [
       '0x8F0d8b27bF808976Fa94f03e2230b4bca95bf3C4',
     ]
-  }
+  },
+  // injective: {
+  //   owners: [
+  //     'inj1wvhk7xhzf9kus9a4tpa6v8vhuqvm265rz7zd6n',
+  //   ]
+  // }
 }
 
 module.exports = cexExports(config)
