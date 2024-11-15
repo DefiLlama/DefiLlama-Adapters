07722a5219d9fafb0b3cfdaf1eab8dde14be219e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-15 11:06:39 +0100

    split gov tokens from tvl

--- projects/spl-governance/index.js
+++ projects/spl-governance/index.js
@@ -1,22 +1,34 @@
-const { getCache } = require('../helper/http')
+const { PublicKey } = require('@solana/web3.js')
+const { get } = require('../helper/http')
+const { getConnection } = require('../helper/solana')
+const { sliceIntoChunks } = require('../helper/utils')
+
 const url = 'https://realms-tvl.vercel.app/tvl/latest'
 
-async function tvl() {
-  const {totalValueUsd} = await getCache(url)
-  const {tokens} = totalValueUsd
+const isSolOrStable = (token) => ['sol', 'usd', 'btc', 'eth'].some(i => token.token_symbol.toLowerCase().includes(i))
 
-  const returnTokens = {}
+async function tvl(api, isStaking = false) {
+  const connnection = getConnection()
+  let { totalValueUsd: { tokens } } = await get(url)
+  const filterFn = isStaking ? i => !isSolOrStable(i) : isSolOrStable
+  tokens = tokens.filter(filterFn)
+  const decimalsMap = {}
+  const _tokens = tokens.map(i => i.token)
+  const chunks = sliceIntoChunks(_tokens, 99)
 
-  for (const token of tokens) {
-    console.log("Treasury addresses holding", token.token_symbol, ": ", token.holders)
-    returnTokens[token.token_symbol] = token.balance
+  for (const chunk of chunks) {
+    const { value } = await connnection.getMultipleParsedAccounts(chunk.map(i => new PublicKey(i)))
+    value.forEach((val, i) => {
+      decimalsMap[chunk[i]] = val.data.parsed.info.decimals
+    })
   }
 
-  return returnTokens
+  for (const token of tokens)
+    api.add(token.token, +token.balance * 10 ** decimalsMap[token.token])
 }
 
 module.exports = {
-  methodology: 'SOL, SPL tokens and stables held in the treasury by the DAOs are counted under tvl', 
+  methodology: 'SOL token and stables held in the contracts are counted under tvl, gov tokens are counted under staking',
   timetravel: false,
-  solana: { tvl }
+  solana: { tvl: api => tvl(api), staking: api => tvl(api, true), }
 }
