001749bf30492a0762d3df09ebc502117265a0f2
0xpeluche <0xpeluche@proton.me>
2024-11-18 12:57:35 +0100

    add trustedNodes as RPL staking

--- projects/rocketpool/index.js
+++ projects/rocketpool/index.js
@@ -5,6 +5,7 @@ const RPL = '0xd33526068d116ce69f19a9ee46f0bd304f21a51f'
 const rocketNodeManager = '0x2b52479F6ea009907e46fc43e91064D1b92Fdc86'
 const rocketVault = '0x3bDC69C4E5e13E52A65f5583c23EFB9636b469d6'
 const rocketRewardsPool = '0xEE4d2A71cF479e0D3d0c3c2C923dbfEB57E73111'
+const trustedNodeManager = '0xb8e783882b11Ff4f6Cef3C501EA0f4b960152cc9'
 
 const abi = {
   getNodeCount: "function getNodeCount() view returns (uint256)",
@@ -12,7 +13,10 @@ const abi = {
   getNodeDetails: "function getNodeDetails(address _nodeAddress) view returns ((bool exists, uint256 registrationTime, string timezoneLocation, bool feeDistributorInitialised, address feeDistributorAddress, uint256 rewardNetwork, uint256 rplStake, uint256 effectiveRPLStake, uint256 minimumRPLStake, uint256 maximumRPLStake, uint256 ethMatched, uint256 ethMatchedLimit, uint256 minipoolCount, uint256 balanceETH, uint256 balanceRETH, uint256 balanceRPL, uint256 balanceOldRPL, uint256 depositCreditBalance, uint256 distributorBalanceUserETH, uint256 distributorBalanceNodeETH, address withdrawalAddress, address pendingWithdrawalAddress, bool smoothingPoolRegistrationState, uint256 smoothingPoolRegistrationChanged, address nodeAddress))",
   getPendingETHRewards: "function getPendingETHRewards() view returns (uint256)",
   balanceOf: "function balanceOf(string _networkContractName) view returns (uint256)",
-  balanceOfToken: "function balanceOfToken(string _networkContractName, address _tokenAddress) view returns (uint256)"
+  balanceOfToken: "function balanceOfToken(string _networkContractName, address _tokenAddress) view returns (uint256)",
+  getMemberAt: "function getMemberAt(uint256 _index) view returns (address)",
+  getMemberCount: "function getMemberCount() view returns (uint256)",
+  getMemberRPLBondAmount: "function getMemberRPLBondAmount(address _nodeAddress) view returns (uint256)"
 };
 
 const tvl = async (api) => {
@@ -47,15 +51,16 @@ const tvl = async (api) => {
     { minipoolCount: 0, ethMatched: 0 }
   );
 
-  api.add(ETH, pendingETHRewards)
-  api.add(ETH, depositPoolBalance)
-  api.add(ETH, ethMatched)
+  api.add(ETH, [pendingETHRewards, depositPoolBalance, ethMatched])
+}
+
+const staking = async (api) => {
+  const trustedNodes = await api.fetchList({ target: trustedNodeManager, lengthAbi: abi.getMemberCount, itemAbi: abi.getMemberAt  })
+  api.add(RPL, await api.multiCall({ calls: trustedNodes.map((node) => ({ target: trustedNodeManager, params: [node] })), abi: abi.getMemberRPLBondAmount }))
+  return api.sumTokens({ owner: rocketVault, tokens: [RPL] })
 }
 
 module.exports = {
   methodology: 'TVL represents the total ETH from the minipools as well as the staking rewards pending distribution',
-  ethereum: {
-    tvl,
-    staking: (api) => api.sumTokens({ owner: rocketVault, tokens: [RPL] })
-  }
+  ethereum: { tvl, staking }
 }
