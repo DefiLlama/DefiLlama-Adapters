b9ef54d0dc1816bf498cb1236713c9e5ca75b8af
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-19 19:26:09 +0500

    feat: add BeetrootFinance adapter

--- projects/BeetrootFinance/index.js
+++ projects/BeetrootFinance/index.js
@@ -1,1 +1,17 @@
 
+const { get } = require('../helper/http')
+const ADDRESSES = require("../helper/coreAssets.json");
+
+async function tvl(api) {
+  const data = await get('https://api.beetroot.finance/v1/tvl')
+  api.add(ADDRESSES.ton.USDT, data.tvl)
+}
+
+module.exports = {
+  misrepresentedTokens: true,
+  timetravel: false,
+  methodology: `TVL calculation methodology consists of the delta between onchain USDT deposits and withdrawals`.trim(),
+  ton: {
+    tvl
+  }
+}
