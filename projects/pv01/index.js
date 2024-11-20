08912ef4be6f9eb9f38a0d1c2fcd37e5af492736
Kevin <kevin@pv0.one>
2024-11-20 12:11:16 +0000

    PV01 TVL adaptor

---
+++ projects/pv01/index.js
@@ -0,0 +1,36 @@
+const PV01_VAULT_FACTORY_ETHEREUM = '0x7eB37F9326E2474D5178Fd5224bc35E30A5398B5';
+const abi = {
+  getVaults: "function getVaults(string type_) view returns (address[])"
+};
+
+async function tvl(api) {
+  // Fetch the list of vaults from the factory
+  const listOfVaults = await api.call({
+    abi: abi.getVaults,
+    target: PV01_VAULT_FACTORY_ETHEREUM,
+    params: ['BondPerpetualVault']
+  });
+
+  // Get total supply for each vault
+  for (const vaultAddress of listOfVaults) {
+    const symbol = await api.call({
+      abi: 'erc20:symbol',
+      target: vaultAddress
+    });
+    if (symbol.startsWith("rTBL")) {
+      const collateralBalance = await api.call({
+        abi: 'erc20:totalSupply',
+        target: vaultAddress
+      });
+      api.add(vaultAddress, collateralBalance);
+    }
+  }
+}
+
+module.exports = {
+  methodology: 'Counts the number of rTBL (Rolling T-bill) tokens issued by the PV01 perpetual bond vaults. TBL (T-bill) tokens not in a vault are not counted.',
+  start: 20377028,
+  ethereum: {
+    tvl,
+  }
+};
