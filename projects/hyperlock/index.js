6c672d263e8f28c3dd575d978c8ed9c38a2155e4
0xGlider <conwayglider@protonmail.com>
2024-11-20 21:25:01 +0100

    hyperlock tvl

--- projects/hyperlock/index.js
+++ projects/hyperlock/index.js
@@ -1,9 +1,9 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokens2 } = require("../helper/unwrapLPs");
-const { cachedGraphQuery } = require('../helper/cache')
 
-const v2Deposits = "0xC3EcaDB7a5faB07c72af6BcFbD588b7818c4a40e";
-const v3Deposits = "0xc28EffdfEF75448243c1d9bA972b97e32dF60d06";
+const abi = require("./abi.json")
+const { sumTokens2 } = require("../helper/unwrapLPs")
+
+const BOOSTER = "0x08d46dC9E455c9B97E671b6291a54ba5668B94AC";
+const VOTER_PROXY = "0x70A8075C73A9Ff9616CB5aF6BB09c04844718F27";
 
 // https://docs.hyperlock.finance/developers/hyperlock-contracts
 module.exports = {
@@ -13,18 +13,10 @@ module.exports = {
   },
 }
 
-const query = `{
-  pools(where:{type: V2}) { id type }
-}`
-
 
 async function tvl(api) {
-  const { pools } = await cachedGraphQuery('hyperlock/v2', 'https://graph.hyperlock.finance/subgraphs/name/hyperlock/points-blast-mainnet', query)
-  const tokens = pools.map(i => i.id)
-  await sumTokens2({ api, owner: v2Deposits, tokens: pools.map(i => i.id), resolveLP: true, })
-  await sumTokens2({ api, tokensAndOwners: [
-    [ADDRESSES.blast.USDB, '0x390b781BAf1e6Db546cF4e3354b81446947838d2'],
-    [ADDRESSES.blast.WETH, '0x1856c7e0b559e9d7287473cb4b4786398db4032a'],
-  ] })
-  return sumTokens2({ api, owner: v3Deposits, resolveUniV3: true })
+  const pools = await api.fetchList({ target: BOOSTER, itemAbi: abi.poolInfo, lengthAbi: abi.poolLength, })
+  pools.shift(); // remove the first pool, which is hyperTHRUST/THRUST stable pool
+  await sumTokens2({ tokens: pools.map(p => p.lptoken), owners: pools.map(p => p.gauge), resolveLP: true, api});
+  return sumTokens2({ api, owners: [VOTER_PROXY], resolveUniV3: true });
 }
