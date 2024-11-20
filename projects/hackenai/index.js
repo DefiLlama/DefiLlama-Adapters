daf56cacdcb9049e169c23866695e6f85e8c2bb5
Oleksii Zhuk <o.zhuk@hacken.io>
2024-11-20 20:05:23 +0100

    removed USDC/HAI liquidity to tvl

--- projects/hackenai/index.js
+++ projects/hackenai/index.js
@@ -1,7 +1,6 @@
 const {get} = require("../helper/http");
 
 const HAI_base = '0x73e2a6320314883ff8cc08b53f1460a5f4c47f2c';
-const HAI_USDC_pool = '0xb6050F3317b8709d0F3feC259B4FE319d383b135';
 
 const getPools = async (chainId) => {
     const response = await get('https://api.atomica.org/srm-production-v2/v2/pool/list');
@@ -15,7 +14,7 @@ const getPools = async (chainId) => {
 
 const getTvl = async (api) => {
     const pools = await getPools(api.chainId);
-    const token_owners = [[HAI_base, HAI_USDC_pool], ...pools.map(address => [HAI_base, address])];
+    const token_owners = pools.map(address => [HAI_base, address]);
     return api.sumTokens({tokensAndOwners: token_owners})
 }
 
@@ -23,5 +22,5 @@ module.exports = {
     base: {
         tvl: getTvl,
     },
-    methodology: 'We count the HAI staked in the Flash Pools contracts and USDC/HAI liquidity pool on Uniswap v3'
+    methodology: 'We count the HAI staked in the Flash Pools contracts'
 }
