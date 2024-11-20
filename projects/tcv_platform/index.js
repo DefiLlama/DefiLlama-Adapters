84e0ac2ea0f84df53d62125a534d1faf227a8bd0
Trịnh Tiến Đạt <97428594+Miyukishirogane@users.noreply.github.com>
2024-11-20 22:09:10 +0700

    Add adapter for tcv_platform (#12363)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ projects/tcv_platform/index.js
@@ -0,0 +1,18 @@
+
+
+const { sumTokens2 } = require('../helper/unwrapLPs');
+
+async function tvl(api) {
+  const tcvFactory = "0xCa2396933E02Fb7636126a914aE5f5512ab31077";
+  const index = await api.call({ target: tcvFactory, abi: 'uint256:numVaults', });
+  const vaults = await api.call({ target: tcvFactory, abi: 'function vaults(uint256,uint256) returns (address[])', params: ["0", String(index)], });
+  await sumTokens2({ api, resolveUniV3: true, owners: vaults })
+}
+
+module.exports = {
+  methodology: "Calculates total liquidity from all NFT ranges in the given pools.",
+  start: 1717239410,
+  arbitrum: {
+    tvl,
+  },
+};
