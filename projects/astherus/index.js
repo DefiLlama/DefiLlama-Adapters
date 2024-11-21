0b7a23ebc9097b6ad8d1540ffad0ea36b3f459d0
paulccccc <paul_ccc@outlook.com>
2024-11-21 11:13:43 +0800

    feat: add asBTC TVL

--- projects/astherus/index.js
+++ projects/astherus/index.js
@@ -1,13 +1,21 @@
 const { getConfig } = require('../helper/cache')
 const { sumTokens2 } = require('../helper/solana')
+const ethers = require('ethers')
 
 const config = {
   bsc: '0x128463A60784c4D3f46c23Af3f65Ed859Ba87974',
   ethereum: '0x604DD02d620633Ae427888d41bfd15e38483736E'
 }
+const asBTCContract = '0x184b72289c0992BDf96751354680985a7C4825d6'
 
 module.exports = {
   start: 1706716800, // 02/01/2024 @ 00:00:00pm (UTC)
+}
+
+const getAsBTCTVL = async () => {
+  const { data: asTokensPrice } = await getConfig(`astherus/token-price`, `https://www.astherus.finance/bapi/futures/v1/public/future/earn/getAssTokenPrice`)
+  const { assTokenTotalSupply} = asTokensPrice?.find(token => token.assTokenName === 'asBTC') ?? {}
+  return assTokenTotalSupply
 }
 
 Object.keys(config).forEach(chain => {
@@ -16,8 +24,13 @@ Object.keys(config).forEach(chain => {
     tvl: async (api) => {
       const { data } = await getConfig(`astherus/${api.chain}`, `https://astherus.finance/bapi/futures/v1/public/future/web3/ae-deposit-asset?chainId=${api.chainId}`)
       const tokens = data.map(i => i.contractAddress)
-      return api.sumTokens({ owner: vault, tokens })
-    }
+      if (chain === 'bsc') {
+        const asBTCTVL = await getAsBTCTVL()
+        api.addToken(asBTCContract, ethers.parseEther(asBTCTVL.toString()).toString())
+      }
+      await api.sumTokens({ owner: vault, tokens })
+      return api.getBalances()
+    },
   }
 })
 
