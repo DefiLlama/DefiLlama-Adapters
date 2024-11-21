afbfe81468fab008181f90d46bb5586b9eef19ca
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-21 16:19:49 +0100

    bitlayter bridge: code refactor

--- projects/bitlayer-btc/index.js
+++ projects/bitlayer-btc/index.js
@@ -1,22 +1,8 @@
-const sdk = require("@defillama/sdk");
 const { sumTokensExport } = require("../helper/sumTokens");
-
-const owners = [
-  "132Cka5Vdw9FcFX3eb28xikKAMvhuMJGwi",
-  "3JszuRdFFEz1WbQKam4XjAtnZsYX62RLMX",
-  "33uhW878fbvVxZuZ216q2XvgdajLdG4rN4",
-  "32a7u8tM45GPSdjzy7PbRvuncEvHFxnYR4",
-  "3D3a4fPJ5HKw43Qd6QHtoJ6RvMUL7UBDT5",
-  "19M7Z1E8Bs4pkXmHJrrJtngmVMUmgFu4K2",
-  "bc1p87c2auxxj372evzhd5f5huddtrladtkn3z4p94ew32qvwcsgujestjj45r",
-  "bc1puqn6dw6etk6yg8zruvf2s94cmhxkfncsaumwhtfhu5qy3e6m94sq37eq66",
-  "bc1pvnh3zy48ml3nhzhqrtc7endhj9rrtrv5puy2775p3jwka8y99aqsz78uu2",
-  "bc1pxpp82hc4t4flkyqtjdnzr3q72qh9st78gfge50vzlrjtp9c6yn4s5zq5vk",
-  "bc1p7agkadaau66jtva9n8k5pg3lsctuyqur8a2l5y9hzwqkh5nlmd0skuhws3",
-];
+const { bitlayerBridge } = require("../helper/bitcoin-book");
 
 module.exports = {
   bitcoin: {
-    tvl: sdk.util.sumChainTvls([sumTokensExport({ owners: owners })]),
+    tvl: sumTokensExport({ owners: bitlayerBridge })
   },
 };
