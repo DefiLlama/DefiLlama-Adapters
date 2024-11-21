94abf4e7fb27213bbb62dc05e323ac1b27911600
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-21 07:55:21 +0100

    code refactor

--- projects/astherus/index.js
+++ projects/astherus/index.js
@@ -1,21 +1,15 @@
 const { getConfig } = require('../helper/cache')
 const { sumTokens2 } = require('../helper/solana')
-const ethers = require('ethers')
 
 const config = {
   bsc: '0x128463A60784c4D3f46c23Af3f65Ed859Ba87974',
-  ethereum: '0x604DD02d620633Ae427888d41bfd15e38483736E'
+  ethereum: '0x604DD02d620633Ae427888d41bfd15e38483736E',
+  scroll: '0x7BE980E327692Cf11E793A0d141D534779AF8Ef4',
 }
 const asBTCContract = '0x184b72289c0992BDf96751354680985a7C4825d6'
 
 module.exports = {
   start: 1706716800, // 02/01/2024 @ 00:00:00pm (UTC)
-}
-
-const getAsBTCTVL = async () => {
-  const { data: asTokensPrice } = await getConfig(`astherus/token-price`, `https://www.astherus.finance/bapi/futures/v1/public/future/earn/getAssTokenPrice`)
-  const { assTokenTotalSupply} = asTokensPrice?.find(token => token.assTokenName === 'asBTC') ?? {}
-  return assTokenTotalSupply
 }
 
 Object.keys(config).forEach(chain => {
@@ -25,18 +19,17 @@ Object.keys(config).forEach(chain => {
       const { data } = await getConfig(`astherus/${api.chain}`, `https://astherus.finance/bapi/futures/v1/public/future/web3/ae-deposit-asset?chainId=${api.chainId}`)
       const tokens = data.map(i => i.contractAddress)
       if (chain === 'bsc') {
-        const asBTCTVL = await getAsBTCTVL()
-        api.addToken(asBTCContract, ethers.parseEther(asBTCTVL.toString()).toString())
+        const asBTCTVL = await api.call({ abi: 'erc20:totalSupply', target: asBTCContract })
+        api.addToken(asBTCContract, asBTCTVL)
       }
-      await api.sumTokens({ owner: vault, tokens })
-      return api.getBalances()
+      return api.sumTokens({ owner: vault, tokens })
     },
   }
 })
 
 module.exports['solana'] = {
-  tvl: async function (...rest) {
+  tvl: async function () {
     const { data: tokens } = await getConfig(`astherus/solana`, `https://astherus.finance/bapi/futures/v1/public/future/web3/ae-deposit-asset?network=SOL`)
-    return sumTokens2({ tokenAccounts: tokens.map(({ tokenVault }) => [tokenVault]).flat() })
+    return sumTokens2({ tokenAccounts: tokens.map(({ tokenVault }) => tokenVault) })
   }
 }
