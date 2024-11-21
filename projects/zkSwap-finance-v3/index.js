6df5d0ae6974d5370ca91af248d65b9730ffef83
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-21 08:17:01 +0100

    code refactor

--- projects/zkSwap-finance-v3/index.js
+++ projects/zkSwap-finance-v3/index.js
@@ -1,8 +1,5 @@
 const { uniV3Export } = require('../helper/uniswapV3')
 
-module.exports = {
-  misrepresentedTokens: true,
-  ...uniV3Export({
-    era: { factory: '0x88ADD6a7e3C221e02f978B388a092c9FD8cd7850', fromBlock: 49205949},
-  })
-};
+module.exports = uniV3Export({
+  era: { factory: '0x88ADD6a7e3C221e02f978B388a092c9FD8cd7850', fromBlock: 49205949 },
+})
