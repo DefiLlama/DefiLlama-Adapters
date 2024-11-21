816635638ec2f0336ba85de90ba174d29d20fd1f
Diyahir <32445955+diyahir@users.noreply.github.com>
2024-11-21 12:26:57 -0600

    Update index.js

--- projects/fluid-protocol/index.js
+++ projects/fluid-protocol/index.js
@@ -1,9 +1,8 @@
 const { sumTokens } = require("../helper/chain/fuel")
 
 async function tvl(api) {
-  const contractId = '0x7fca28897ed946740225f3091e20c64e0b2cac57931e90e477e39ed2622785f7'
-  // let sumTotals = await sumTokens({ api, owner: contractId })
-  // console.log(sumTotals)
+  const contractId = '0xb18340bfe68c0b3a4fbd3a3ae2c014be94c16569b7f360cf53efe1b7023e545e'
+
   return sumTokens({ api, owner: contractId })
 }
 
