fbae322e085a3cd9228f8f3ef24b7a4d91f34c4b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-21 21:52:03 +0100

    update pumpbtc #12379

--- projects/pumpbtc/index.js
+++ projects/pumpbtc/index.js
@@ -2,6 +2,7 @@ const { sumTokens } = require('../helper/sumTokens');
 const utils = require('../helper/utils');
 const { getConfig } = require('../helper/cache');
 const bitcoinBook = require('../helper/bitcoin-book');
+const { sumTokens2 } = require('../helper/unwrapLPs');
 
 module.exports = {
   methodology: 'TVL for pumpBTC is calculated based on the total value of WBTC, FBTC, BTCB held in the contract that were utilized in the minting process of pumpBTC.',
@@ -11,7 +12,6 @@ async function getStakingAddresses() {
   let res = await utils.fetchURL('https://dashboard.pumpbtc.xyz/api/dashboard/asset/tokenowners')
 
   const btcAddresses = res.data.data || {}
-  //console.log('>>', btcAddresses.length)
   return btcAddresses
 }
 
@@ -28,7 +28,7 @@ async function otherTvl(api) {
   }
 
   const { owners, tokens } = addresses[api.chain]
-  return api.sumTokens({ owners, tokens })
+  return sumTokens2({ api, owners, tokens })
 }
 
 module.exports.isHeavyProtocol = true;
