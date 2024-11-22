cf69c2b553fd1bb61397cbf487c6b73439f80f6a
Catton <166699866+Cattontech@users.noreply.github.com>
2024-11-22 16:54:33 +0700

    update logic calculator tvl

--- projects/catton/index.js
+++ projects/catton/index.js
@@ -1,31 +1,35 @@
-const { call } = require("../helper/chain/ton");
-const { Cell } = require("@ton/core")
+const { call, getJettonBalances, getTonBalance } = require("../helper/chain/ton");
 const { sleep } = require('../helper/utils')
 
+const TON_POOL_ADDRESS = "EQAze1aSZHY1yUGz1BFndH62k-VYpXYeDiYofCXTRZClF8Qr"
+const USD_POOL_ADDRESS = "EQBy7pjr6IBzqW8vuVCZ780evtnkiIF3jZSRRDxeqScfZoU9"
+const CTON_ADDRESS = "0:86cf8401d283627a87b58c367b440cad933ab8aa7b383419e8ff7d1a00c945f8"
+const CTUSDT_ADDRESS = "0:9fb449ce8fb43d0f682c713c01d9d8357c7cd0d4a49dd64dd585926990174a4e"
+const USDT_ADDRESS = "0:b113a994b5024a16719f69139328eb759596c38a25f59028b146fecdc3621dfe"
+
 async function getTvlInPoolAndVault() {
+
   const vault = await call({ target: "UQDFlyZ5zsWyowbZvZjZwIW_Vzm-1uvf8z_PUfvQtHrV14dp", abi: "get_vault_data"})
   let tonTVL = BigInt(vault[1] + vault[8]);
   await sleep(1000 * (3 * Math.random() + 3))
 
-  const result = await call({ target: "EQAze1aSZHY1yUGz1BFndH62k-VYpXYeDiYofCXTRZClF8Qr", abi: "get_reserves" })
-  const data = Cell.fromBase64(result[1][1].bytes)
-  const ds = data.beginParse()
-  while (ds.remainingBits) {
-    tonTVL += BigInt(ds.loadCoins())
-  }
-  const ton = Number(tonTVL / BigInt(1e9))
+  const tonBalanceInTonPool = await getTonBalance(TON_POOL_ADDRESS)
+  tonTVL += BigInt(tonBalanceInTonPool)
   await sleep(1000 * (3 * Math.random() + 3))
+  const ctonInPool = await getJettonBalances(TON_POOL_ADDRESS)
+  const cTonValue = BigInt(ctonInPool[CTON_ADDRESS].balance)
+  tonTVL += cTonValue;
+  await sleep(1000 * (3 * Math.random() + 3))
+  const usdInPool = await getJettonBalances(USD_POOL_ADDRESS)
+  const usdtValue = BigInt(usdInPool[USDT_ADDRESS].balance)
+  const ctUsdtValue = BigInt(usdInPool[CTUSDT_ADDRESS].balance)
 
-  const resultU = await call({ target: "EQBy7pjr6IBzqW8vuVCZ780evtnkiIF3jZSRRDxeqScfZoU9", abi: "get_reserves" })
-  const dataU = Cell.fromBase64(resultU[1][1].bytes)
-  const dsu = dataU.beginParse()
-  let usdTVL = 0n;
-  while (dsu.remainingBits) {
-    usdTVL += dsu.loadCoins()
+  const usdTVL = usdtValue + ctUsdtValue;
+  
+  return { 
+    ton: Number(tonTVL / BigInt(1e9)),
+    usd: Number(usdTVL / BigInt(1e6))
   }
-  const usd = Number(usdTVL / BigInt(1e6))
-  console.log({ ton, usd })
-  return { ton, usd }
 }
 
 module.exports = {
