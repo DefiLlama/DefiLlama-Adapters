ce06bb4c7c2efbcde50977dd09519d9237d77652
0xngmi <80541789+0xngmi@users.noreply.github.com>
2024-11-22 21:31:27 +0000

    Update index.js

--- projects/ibtc/index.js
+++ projects/ibtc/index.js
@@ -4,7 +4,7 @@ const ibtcContract = "0x8154Aaf094c2f03Ad550B6890E1d4264B5DdaD9A";
 
 async function tvl(api) {
     // 1iBTC=1XBTC=1BTC(exsat)=1BTC
-    const ibtcTotalSupply = await api.abi.call({
+    const ibtcTotalSupply = await api.call({
         target: ibtcContract,
         abi: "uint256:totalSupply"
     })
