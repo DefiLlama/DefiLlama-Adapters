2f2bedc5600bddd8104d164f5c21516fb2ee1bed
0xngmi <80541789+0xngmi@users.noreply.github.com>
2024-11-22 21:28:21 +0000

    test

--- projects/ibtc/index.js
+++ projects/ibtc/index.js
@@ -2,15 +2,16 @@ const sdk = require('@defillama/sdk')
 
 const ibtcContract = "0x8154Aaf094c2f03Ad550B6890E1d4264B5DdaD9A";
 
-async function tvl(timestamp, ethBlock, chainBlocks) {
+async function tvl(api) {
     // 1iBTC=1XBTC=1BTC(exsat)=1BTC
-    const ibtcTotalSupply = await sdk.api.abi.call({
-        block: ethBlock,
+    const ibtcTotalSupply = await api.abi.call({
         target: ibtcContract,
         abi: "uint256:totalSupply"
     })
 
-    return ibtcTotalSupply
+    return {
+        "coingecko:bitcoin": ibtcTotalSupply
+    }
 }
 
 module.exports = {
