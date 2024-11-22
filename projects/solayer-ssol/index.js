49d264393869f9f57e63a613f3aa6ec9543557f8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-22 11:31:27 +0100

    minor fix

--- projects/solayer-ssol/index.js
+++ projects/solayer-ssol/index.js
@@ -1,8 +1,6 @@
-const { sumTokens2, getConnection, } = require("../helper/solana");
+const { sumTokens2, getConnection, getAssociatedTokenAddress, } = require("../helper/solana");
 const { PublicKey } = require("@solana/web3.js");
 const ADDRESSES = require('../helper/coreAssets.json')
-const TOKEN_PROGRAM_ID = new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
-const SPL_ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
 
 async function tvl(api) {
   const connection = getConnection();
@@ -21,30 +19,22 @@ async function tvl(api) {
     const offset = 8
     const lstMint = new PublicKey(i.account.data.slice(offset + 0, offset + 32));
     const vaultPubkey = new PublicKey(i.pubkey.toString());
-    
+
     // Derive the ATA for this LST and vault
-    const ata = getAssociatedTokenAddress(
-      lstMint,
-      vaultPubkey
-    );
+    const ata = getAssociatedTokenAddress(lstMint, vaultPubkey);
 
     return ata.toString()
   });
 
 
   return sumTokens2({
-    balances: api.getBalances(), 
-    tokenAccounts, 
+    api,
+    tokenAccounts,
     blacklistedTokens: [
       'sSo1wxKKr6zW2hqf5hZrp2CawLibcwi1pMBqk5bg2G4',
       'testqcAoCvfFpuFNtdmrBnBMSfFoXKkSTJ3ky6cPKjx',
     ]
   })
-}
-
-function getAssociatedTokenAddress(mint, owner,) {
-  const [associatedTokenAddress] = PublicKey.findProgramAddressSync([owner.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), mint.toBuffer()], SPL_ASSOCIATED_TOKEN_PROGRAM_ID);
-  return associatedTokenAddress;
 }
 
 module.exports = {
