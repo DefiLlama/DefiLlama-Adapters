57c240129668448f26364e75515fb2f26c99872a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-22 12:08:07 +0100

    exclude synth tokens

--- projects/catton/index.js
+++ projects/catton/index.js
@@ -1,5 +1,6 @@
-const { call, getJettonBalances, getTonBalance } = require("../helper/chain/ton");
+const { call, getJettonBalances, getTonBalance, sumTokens } = require("../helper/chain/ton");
 const { sleep } = require('../helper/utils')
+const ADDRESSES = require('../helper/coreAssets.json')
 
 const TON_POOL_ADDRESS = "EQAze1aSZHY1yUGz1BFndH62k-VYpXYeDiYofCXTRZClF8Qr"
 const USD_POOL_ADDRESS = "EQBy7pjr6IBzqW8vuVCZ780evtnkiIF3jZSRRDxeqScfZoU9"
@@ -7,41 +8,35 @@ const CTON_ADDRESS = "0:86cf8401d283627a87b58c367b440cad933ab8aa7b383419e8ff7d1a
 const CTUSDT_ADDRESS = "0:9fb449ce8fb43d0f682c713c01d9d8357c7cd0d4a49dd64dd585926990174a4e"
 const USDT_ADDRESS = "0:b113a994b5024a16719f69139328eb759596c38a25f59028b146fecdc3621dfe"
 
-async function getTvlInPoolAndVault() {
+async function tvl(api) {
 
-  const vault = await call({ target: "UQDFlyZ5zsWyowbZvZjZwIW_Vzm-1uvf8z_PUfvQtHrV14dp", abi: "get_vault_data"})
-  let tonTVL = BigInt(vault[1] + vault[8]);
+ /*  const vault = await call({ target: "UQDFlyZ5zsWyowbZvZjZwIW_Vzm-1uvf8z_PUfvQtHrV14dp", abi: "get_vault_data" })
+  let tonTVL = +vault[1] + +vault[8]
+  console.log('tonTVL', tonTVL, vault[1], vault[8])
   await sleep(1000 * (3 * Math.random() + 3))
 
   const tonBalanceInTonPool = await getTonBalance(TON_POOL_ADDRESS)
-  tonTVL += BigInt(tonBalanceInTonPool)
+  console.log('tonBalanceInTonPool', tonBalanceInTonPool)
+  tonTVL += +tonBalanceInTonPool
   await sleep(1000 * (3 * Math.random() + 3))
   const ctonInPool = await getJettonBalances(TON_POOL_ADDRESS)
-  const cTonValue = BigInt(ctonInPool[CTON_ADDRESS].balance)
-  tonTVL += cTonValue;
+  const cTonValue = ctonInPool[CTON_ADDRESS]?.balance ?? 0
+  tonTVL += +cTonValue;
   await sleep(1000 * (3 * Math.random() + 3))
   const usdInPool = await getJettonBalances(USD_POOL_ADDRESS)
-  const usdtValue = BigInt(usdInPool[USDT_ADDRESS].balance)
-  const ctUsdtValue = BigInt(usdInPool[CTUSDT_ADDRESS].balance)
+  const usdtValue = +usdInPool[USDT_ADDRESS]?.balance ?? 0
+  const ctUsdtValue = +usdInPool[CTUSDT_ADDRESS]?.balance ?? 0
 
-  const usdTVL = usdtValue + ctUsdtValue;
-  
-  return { 
-    ton: Number(tonTVL / BigInt(1e9)),
-    usd: Number(usdTVL / BigInt(1e6))
-  }
+  api.addCGToken('the-open-network', tonTVL / 1e9)
+  api.addCGToken('tether', (usdtValue + ctUsdtValue) / 1e6) */
+
+  return sumTokens({ owners: [TON_POOL_ADDRESS, USD_POOL_ADDRESS, "UQDFlyZ5zsWyowbZvZjZwIW_Vzm-1uvf8z_PUfvQtHrV14dp"], tokens: [ADDRESSES.null, USDT_ADDRESS], api, onlyWhitelistedTokens: true })
 }
 
 module.exports = {
   methodology: 'Total amount of collateral locked in the Catton Protocol',
   timetravel: false,
   ton: {
-    tvl: async () => {
-      const { ton, usd } = await getTvlInPoolAndVault();
-        return { 
-          "coingecko:the-open-network": ton,
-          "coingecko:tether": usd,
-         };
-      }
+    tvl
   }
 }
