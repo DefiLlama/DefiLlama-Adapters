420a0a8cf9a0b5a3a4c650ae31b40411796599c6
Rajeeb Kumar Malik <rkm2541997@gmail.com>
2024-11-23 23:04:46 +0530

    Update index.js for Filament TVL update. (#12412)

--- projects/filament/index.js
+++ projects/filament/index.js
@@ -2,9 +2,10 @@ const { sumTokensExport } = require("../helper/unwrapLPs");
 
 const USDC_CONTRACT = '0x3894085Ef7Ff0f0aeDf52E2A2704928d1Ec074F1';
 const FILAMENT_VAULT_CONTRACT = '0xbeB6A6273c073815eBe288d2dF4e5E8bc027DA11';
+const FILAMENT_DEPOSIT_CONTRACT = '0x894DE0011C0d69BCa2e19c21CD246C17A8A4252e';
 
 module.exports = {
   sei: {
-    tvl: sumTokensExport({ owner: FILAMENT_VAULT_CONTRACT, tokens: [USDC_CONTRACT] }),
+    tvl: sumTokensExport({ owners: [FILAMENT_VAULT_CONTRACT, FILAMENT_DEPOSIT_CONTRACT], tokens: [USDC_CONTRACT] }),
   },
 }
