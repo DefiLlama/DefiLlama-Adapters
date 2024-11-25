31cf6979d6eb42c36686c95ef8b245624567f599
Kunal Sangwan <kunalsangwan1@gmail.com>
2024-11-25 16:59:33 +0530

    Include Evaa staked balance via USDT pool in TVL

--- projects/tonpools/index.js
+++ projects/tonpools/index.js
@@ -1,4 +1,6 @@
+const { call } = require('../helper/chain/ton')
 const { sumTokensExport } = require("../helper/chain/ton");
+const sdk = require('@defillama/sdk')
 const ADDRESSES = require("../helper/coreAssets.json");
 
 const tonpoolsContractAddress =
@@ -7,13 +9,26 @@ const tonpoolsContractAddress =
 const tonpoolUSDTContractAddress =
   "EQDrSz9W4tjwnqy1F4z-O4Vkdp8g2YP94cmh12X5RbYpejCw";
 
+async function tvl(api) {
+  const result = await call({ target: tonpoolUSDTContractAddress, abi: 'poolBalances' })
+  const balances = {
+    'coingecko:tether': result[1] / 1e6,
+  }
+  
+  const tonpoolsContractBalances = await sumTokensExport({
+    owners: [tonpoolsContractAddress],
+    tokens: [ADDRESSES.null],
+  })(api);
+
+  sdk.util.mergeBalances(balances, tonpoolsContractBalances);
+
+  return balances
+}
+
 module.exports = {
   methodology: "Ton Pools's TVL includes all deposited supported assets",
   ton: {
-    tvl: sumTokensExport({
-      owners: [tonpoolsContractAddress, tonpoolUSDTContractAddress],
-      tokens: [ADDRESSES.null],
-    }),
+    tvl: (api) => tvl(api)
   },
 };
 
