61c73bef21a86ad6d9c6ed88b25d70368fc791a7
Alien hunter no. 42 <alienHunterOnMars@protonmail.com>
2024-11-25 19:00:04 +0530

    adding DegenHive adapter

---
+++ projects/DegenHive/index.js
@@ -0,0 +1,73 @@
+const sui = require("../helper/chain/sui");
+const { GraphQLClient } = require("graphql-request");
+const {TransactionBlock} = require("@mysten/sui.js/transactions");
+  
+const graphQLClientMainnet = new GraphQLClient(
+    "https://5lox8etck8.execute-api.eu-central-1.amazonaws.com/api/v1"
+  );
+
+const GET_SUI_POOLS = `query GetSuiPools {
+  getSuiPools {
+    pools {
+      poolId
+    }
+  }
+}`
+
+const GET_SUI_MEME_POOLS = `query GetSuiMemePools {
+  getSuiMemePools {
+    pools {
+      meme_pool_addr
+    }
+  }
+}`
+
+ 
+
+async function tvl(api) {
+    const sui_pools = await graphQLClientMainnet.request(GET_SUI_POOLS, {})
+    const sui_meme_pools = await graphQLClientMainnet.request(GET_SUI_MEME_POOLS, {})
+
+    // Extract arrays of pool IDs
+    const poolIds = sui_pools.getSuiPools.pools.map(pool => pool.poolId);
+    const memePoolAddrs = sui_meme_pools.getSuiMemePools.pools.map(pool => pool.meme_pool_addr);
+
+    const suiPoolsData = await sui.getObjects(poolIds)
+    const suiMemePoolsData = await sui.getObjects(memePoolAddrs)
+
+    // Add TVL for AMM Pools 
+    for (const { type, fields } of suiPoolsData) {
+        if ( fields.coin_x_reserve == 0 && fields.coin_y_reserve == 0) continue;
+        let coin_x_type = type.split('<')[1].split(',')[0].trim()
+        let coin_y_type = type.split('<')[1].split(',')[1].trim()
+        
+        api.add(  coin_x_type, fields.coin_x_reserve / 10 ** fields.coin_x_decimals)
+        api.add( coin_y_type, fields.coin_y_reserve / 10 ** fields.coin_y_decimals)
+
+        if (fields.coin_z_reserve && fields.coin_z_reserve > 0) {
+            let coin_z_type = type.split('<')[1].split(',')[2].trim()
+            api.add( coin_z_type, fields.coin_z_reserve / 10 ** fields.coin_z_decimals)
+        }
+    }
+
+    // Add TVL for MEME Pools 
+    for (const { fields } of suiMemePoolsData) {
+        if ( fields.sui_available == 0) continue;
+        api.add( "0x2::sui::SUI", fields.sui_available / 10 ** 9) 
+    }
+
+    // Add TVL from SUI liquid staking
+    const dsui_vault = await sui.getObject("0x85aaf87a770b4a09822e7ca3de7f9424a4f58688cfa120f55b294a98d599d402");
+    let sui_staked = dsui_vault.fields.dsui_supply * dsui_vault.fields.sui_claimable_per_dsui / 1e9 + dsui_vault.fields.sui_to_stake;
+
+    api.add( "0x2::sui::SUI", sui_staked / 1e9) 
+
+}
+   
+module.exports = {
+  timetravel: false,
+  sui: {
+    tvl,
+  },
+  methodology: "TVL consists of the liquidity in the AMM pools, SUI present in Meme coin launchpad pools, and the SUI staked in the liquid staking pools."
+};
