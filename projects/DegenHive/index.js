9ed9b99f98c5ca57d5ee37aaab0476b476f1a3c8
Alien hunter no. 42 <alienHunterOnMars@protonmail.com>
2024-11-25 19:10:22 +0530

    fix

--- projects/DegenHive/index.js
+++ projects/DegenHive/index.js
@@ -57,7 +57,7 @@ async function tvl(api) {
 
     // Add TVL from SUI liquid staking
     const dsui_vault = await sui.getObject("0x85aaf87a770b4a09822e7ca3de7f9424a4f58688cfa120f55b294a98d599d402");
-    let sui_staked = dsui_vault.fields.dsui_supply * dsui_vault.fields.sui_claimable_per_dsui / 1e9 + dsui_vault.fields.sui_to_stake;
+    let sui_staked = Number(Number(dsui_vault.fields.dsui_supply * dsui_vault.fields.sui_claimable_per_dsui / 1e9 + dsui_vault.fields.sui_to_stake).toFixed(0));
 
     api.add( "0x2::sui::SUI", sui_staked) 
 
