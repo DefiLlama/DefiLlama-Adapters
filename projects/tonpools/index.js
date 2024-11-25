5d17628ac8f8add0023bc089382651ca3be2669b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-25 12:57:11 +0100

    minor fix

--- projects/tonpools/index.js
+++ projects/tonpools/index.js
@@ -1,34 +1,21 @@
 const { call } = require('../helper/chain/ton')
-const { sumTokensExport } = require("../helper/chain/ton");
-const sdk = require('@defillama/sdk')
+const { sumTokens } = require("../helper/chain/ton");
 const ADDRESSES = require("../helper/coreAssets.json");
 
-const tonpoolsContractAddress =
-  "EQA7y9QkiP4xtX_BhOpY4xgVlLM7LPcYUA4QhBHhFZeL4fTa";
+const tonpoolsContractAddress = "EQA7y9QkiP4xtX_BhOpY4xgVlLM7LPcYUA4QhBHhFZeL4fTa";
 
-const tonpoolUSDTContractAddress =
-  "EQDrSz9W4tjwnqy1F4z-O4Vkdp8g2YP94cmh12X5RbYpejCw";
+const tonpoolUSDTContractAddress = "EQDrSz9W4tjwnqy1F4z-O4Vkdp8g2YP94cmh12X5RbYpejCw";
 
 async function tvl(api) {
   const result = await call({ target: tonpoolUSDTContractAddress, abi: 'poolBalances' })
-  const balances = {
-    'coingecko:tether': result[1] / 1e6,
-  }
-  
-  const tonpoolsContractBalances = await sumTokensExport({
-    owners: [tonpoolsContractAddress],
-    tokens: [ADDRESSES.null],
-  })(api);
-
-  sdk.util.mergeBalances(balances, tonpoolsContractBalances);
-
-  return balances
+  api.add(ADDRESSES.ton.USDT, result[1])
+  await sumTokens({ api, owners: [tonpoolsContractAddress], tokens: [ADDRESSES.null], })
 }
 
 module.exports = {
   methodology: "Ton Pools's TVL includes all deposited supported assets",
   ton: {
-    tvl: (api) => tvl(api)
+    tvl,
   },
 };
 
