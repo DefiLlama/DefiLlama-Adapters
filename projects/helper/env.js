2198e3ddf731d8a08e0a47165e9593d98b9213d8
Derek Silva <derek.silva@gmail.com>
2024-11-26 15:23:25 -0500

    Update env.js
    Using Subsquid's GraphQL instead since it supports http connections

--- projects/helper/env.js
+++ projects/helper/env.js
@@ -30,7 +30,7 @@ const DEFAULTS = {
   RPC_PROXY_URL: "https://rpc-proxy.llama.fi",
   UNIT0_RPC: "https://rpc.unit0.dev",
   RBN_RPC: "https://governors.mainnet.redbelly.network",
-  REEF_RPC: "wss://rpc.reefscan.com/ws",
+  REEF_RPC: "https://v2.archive.subsquid.io/network/reef",
 }
 
 const ENV_KEYS = [
