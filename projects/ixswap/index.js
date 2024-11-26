e290b807a4a73621cfd718a81c23f43a9874dd9c
thi-investax <118243127+thi-investax@users.noreply.github.com>
2024-11-26 20:07:12 +0700

    Update IX swap tvl on Base (#12437)

--- projects/ixswap/index.js
+++ projects/ixswap/index.js
@@ -1,18 +1,19 @@
-const { getUniTVL } = require("../helper/unknownTokens");
-const { staking } = require('../helper/staking')
+const {getUniTVL} = require("../helper/unknownTokens");
+const {staking} = require('../helper/staking')
 
 const IXS_POLYGON = "0x1BA17C639BdaeCd8DC4AAc37df062d17ee43a1b8"
 const IXS_BASE = "0xfe550bffb51eb645ea3b324d772a19ac449e92c5"
 const STAKING_CONTRACTS = [
-    "0xad644F3cC768bc6dceF97096790e2210D5191cec", // stake bank
+  "0xad644F3cC768bc6dceF97096790e2210D5191cec", // stake bank
 ]
 
 module.exports = {
-  polygon:{
-    tvl: getUniTVL({ factory: '0xc2D0e0bc81494adB71Ce9Aa350cC875DaE12D81D', blacklistedTokens: [IXS_POLYGON] }),
+  polygon: {
+    tvl: getUniTVL({factory: '0xc2D0e0bc81494adB71Ce9Aa350cC875DaE12D81D', blacklistedTokens: [IXS_POLYGON]}),
     staking: staking(STAKING_CONTRACTS, IXS_POLYGON),
   },
-  base:{
+  base: {
+    tvl: getUniTVL({factory: '0x2eE28d1Bbc2EcB1fFDB83E8055d585E9F0fb757f'}),
     staking: staking(['0x44F07B446e14127136f3554A16014b49BC67D9E6'], IXS_BASE),
   },
 }
