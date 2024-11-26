5cc8a68475445a07b683f543ea95f398d98e4be6
yasir_ejaz <yasir@seed.im>
2024-11-27 03:27:26 +0500

    removed try catch

--- projects/bluefin-amm/index.js
+++ projects/bluefin-amm/index.js
@@ -2,13 +2,9 @@ const axios = require('axios');
 const ADDRESSES = require('../helper/coreAssets.json')
 
 async function suiTvl(api) {
-    try {
-        const tvl = (await axios.get('https://swap.api.sui-prod.bluefin.io/api/v1/info')).data.tvl;
-        api.add(ADDRESSES.sui.USDC, Number(tvl)*1e6);
-        return api.getBalances();
-    } catch (error) {
-        throw error;
-    }
+    const tvl = (await axios.get('https://swap.api.sui-prod.bluefin.io/api/v1/info')).data.tvl;
+    api.add(ADDRESSES.sui.USDC, Number(tvl)*1e6);
+    return api.getBalances();
 }
 
 
