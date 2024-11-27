a5dabd99530f6a27968e26170a61e646d54c7268
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-27 13:00:54 +0100

    code refactor

--- projects/bluefin-amm/index.js
+++ projects/bluefin-amm/index.js
@@ -1,46 +1,19 @@
-const axios = require('axios');
-const ADDRESSES = require('../helper/coreAssets.json')
 const sui = require("../helper/chain/sui");
+const { getConfig } = require('../helper/cache');
 
 async function suiTvl(api) {
-    const pools = (await axios.get('https://swap.api.sui-prod.bluefin.io/api/v1/pools/info')).data;
-    let tokenAddresses={};
-    for (const pool of pools) {
-        tokenAddresses[pool.tokenA.info.address] = {
-            decimals: pool.tokenA.info.decimals
-        };
-        tokenAddresses[pool.tokenB.info.address] = {
-            decimals: pool.tokenB.info.decimals
-        };
-    }
-
-    const addresses = Object.keys(tokenAddresses);
-
-    const tokenPriceQuery = addresses.join(',');
-    const tokenPrices = (await axios.get(`https://swap.api.sui-prod.bluefin.io/api/v1/tokens/price?tokens=${tokenPriceQuery}`)).data;
-    tokenPrices.map(token => {
-        tokenAddresses[token.address].price = token.price;
-    });
-
-    let tvl = 0;
-    for (const pool of pools) {
-        const tokenA = pool.tokenA.info.address;
-        const tokenB = pool.tokenB.info.address;
-        const onChainPoolObject = await sui.getObject(pool.address);
-        const poolTokenAAmount = onChainPoolObject.fields.coin_a;
-        const poolTokenBAmount = onChainPoolObject.fields.coin_b;
-
-        const tokenAValue = (Number(poolTokenAAmount) / 10 ** tokenAddresses[tokenA].decimals) * (Number(tokenAddresses[tokenA].price) || 0);
-        const tokenBValue = (Number(poolTokenBAmount) / 10 ** tokenAddresses[tokenB].decimals) * (Number(tokenAddresses[tokenB].price) || 0);
-        tvl += tokenAValue + tokenBValue
-    }
-    api.add(ADDRESSES.sui.USDC, tvl * 1e6);
-    return api.getBalances();
+  const pools = (await getConfig('bluefin/amm-sui', 'https://swap.api.sui-prod.bluefin.io/api/v1/pools/info')).map(i => i.address)
+  const res = await sui.getObjects(pools)
+  res.forEach((i) => {
+    const [coinA, coinB] = i.type.split('<')[1].split('>')[0].split(', ')
+    api.add(coinA, i.fields.coin_a)
+    api.add(coinB, i.fields.coin_b)
+  })
 }
 
 
 module.exports = {
-    sui: {
-        tvl: suiTvl
-    },
+  sui: {
+    tvl: suiTvl
+  },
 }
