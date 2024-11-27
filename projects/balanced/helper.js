8afba6d350f3f864351ec08d436809627399182e
usr-icon-foundation <111794534+usr-icon-foundation@users.noreply.github.com>
2024-11-27 12:33:12 +0100

    Add SOL and XLM tracking for Balanced (#12450)

--- projects/balanced/helper.js
+++ projects/balanced/helper.js
@@ -43,6 +43,8 @@ const networkIdentifiers = {
   "0xa4b1.arbitrum": "arbitrum",
   "0x2105.base": "base",
   "sui/0000000000000000000000000000000000000000000000000000000000000002::sui::SUI": "sui",
+  "solana/11111111111111111111111111111111" : "solana",
+  "stellar/CAS3J7GYLGXMF6TDJBBYYSE3HQ6BBSMLNUQ34T6TZMYMW2EVH34XOWMA" : "stellar"
 }
 
 // Get decimals of cross-chain Balanced assets in balancedAssetManagerContract
