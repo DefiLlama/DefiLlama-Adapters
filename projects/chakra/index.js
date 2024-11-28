17c4f8cf1008568a0870652c1f41e7daab336421
CodingPeter <42434849+zhaowei666@users.noreply.github.com>
2024-11-29 00:11:05 +0800

    feat: Add Chakra chain (#12476)

---
+++ projects/chakra/index.js
@@ -0,0 +1,10 @@
+const {sumTokensExport} = require("../helper/sumTokens");
+const sdk = require("@defillama/sdk");
+const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
+
+
+module.exports = {
+    // methodology: `Total amount of BTC in ${bitcoinAddressBook.chakra.join(", ")}. Restaked on babylon`,
+    doublecounted: true,
+    bitcoin: {tvl: sdk.util.sumChainTvls([sumTokensExport({owners: bitcoinAddressBook.chakra})])},
+};
