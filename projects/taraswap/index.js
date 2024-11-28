500b56975dd76b908911ff1bbc48f9d9250d7333
DevZi1la <developeranon85@gmail.com>
2024-11-28 13:34:39 +0100

    adds taraswap + taraxa integration

--- projects/taraswap/index.js
+++ projects/taraswap/index.js
@@ -1,4 +1,4 @@
-const { uniV3Export } = require('../helper/uniswapV3');
+const { getChainTvl } = require('../helper/getUniSubgraphTvl');
 
 module.exports = uniV3Export({
   tara: { factory: '0x5EFAc029721023DD6859AFc8300d536a2d6d4c82', fromBlock: 10674828 },
