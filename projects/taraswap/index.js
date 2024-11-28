e0d931290b05bfa8a82a9df0984932e764cc7255
DevZi1la <developeranon85@gmail.com>
2024-11-28 13:56:04 +0100

    renames chain to tara

--- projects/taraswap/index.js
+++ projects/taraswap/index.js
@@ -1,7 +1,7 @@
 const { getChainTvl } = require('../helper/getUniSubgraphTvl');
 
 const graphUrls = {
-  taraxa: 'https://indexer.lswap.app/subgraphs/name/taraxa/uniswap-v3'
+  tara: 'https://indexer.lswap.app/subgraphs/name/taraxa/uniswap-v3'
 }
 
 const factoriesName = 'factories'
@@ -14,7 +14,7 @@ const v2graph = getChainTvl(graphUrls, factoriesName, tvlName)
 module.exports = {
   misrepresentedTokens: true,
   methodology: `Counts the tokens locked on AMM pools, pulling the data from the 'taraxa/uniswap-v3' subgraph`,
-  taraxa: {
-    tvl: v2graph('taraxa'),
+  tara: {
+    tvl: v2graph('tara'),
   },
 }
