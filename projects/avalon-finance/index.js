55ad0917a34c8f896fd31fc8b409cae48f58a14d
tpktpj <jasmin@tprotocol.io>
2024-11-28 15:04:36 -0700

    Fix: remove iotex-usad on DeFi lending.

--- projects/avalon-finance/index.js
+++ projects/avalon-finance/index.js
@@ -77,7 +77,6 @@ const listaMarkets = {
 
 // @note USDa Defi Lending Markets, not USDaLend
 const usdaDefiLendingMarkets = {
-	iotex: aaveExports('', '', undefined, ['0xDB52DD393e3a5e95d3B7C7e1C42cC06bb807A369'], { v3: true }),
 	taiko: aaveExports('', '', undefined, ['0x5EcDC2432ED77cD8E2cE6183712c5cc712c40ec0'], { v3: true }),
 }
 
