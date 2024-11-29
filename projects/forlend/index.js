fd607e1c5d79d4da647089c5fd792117a2a5eb4c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-29 11:10:42 +0100

    fix forlend

--- projects/forlend/index.js
+++ projects/forlend/index.js
@@ -1,5 +1,5 @@
 const { compoundExports2 } = require('../helper/compound')
 
 module.exports = {
-  findora: compoundExports2({ comptroller: '0x3b056De20d662B09f73bDb28Ea6fa7b7aC82259C', cether: '0xbd4eeda5062605f3c3b86039c5f2c5880f9ecd95', blacklistedTokens: ['0x0000000000000000000000000000000000000000']}),
+  findora: compoundExports2({ comptroller: '0x3b056De20d662B09f73bDb28Ea6fa7b7aC82259C', }),
 }
