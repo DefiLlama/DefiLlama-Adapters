5a09c3f6438845c5d64aa0a0a99ca1058659d337
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-29 14:23:48 +0100

    Update index.js

--- projects/nexton/index.js
+++ projects/nexton/index.js
@@ -1,27 +1,14 @@
-
-const { call, getTokenRates } = require("../helper/chain/ton");
+const { call } = require("../helper/chain/ton");
 
 const contractAddress = "EQBED_4VLQC-SRdrVCtmcmwO7PtnvdLSRFRYNaGIZBqpaQHQ";
-const nxton = {
-    token: "0:9d123d5d121efaf9069c737f19447d9e1adab2a7ee747cedffd3004a17c1ca4f",
-    name: "NxTON",
-    decimals: 9,
-};
 
-const getTVL = async (api)=> {
-    const tonStaked = (await call({
-        target: contractAddress,
-        abi: "tonStaked"
-    }))[0];
-
-    const tokens = ["TON", nxton.token];
-    const priceRates = await getTokenRates({tokens});
-    
-    api.addUSDValue(tonStaked * (priceRates["TON"]/1e9 || 0));
+const tvl = async (api) => {
+  const tonStaked = (await call({ target: contractAddress, abi: "tonStaked" }))[0];
+  api.addGasToken(tonStaked)
 }
 
 module.exports = {
-    ton: {
-        tvl : getTVL
-    }
+  ton: {
+    tvl
+  }
 }
