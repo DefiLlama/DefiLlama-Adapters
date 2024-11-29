0f4c5f9ece215df3a008a02572c7bb092fdb5124
Sergey Kozhevnikov <sk@blum.io>
2024-11-29 16:55:15 +0100

    add blum

---
+++ projects/blum/index.js
@@ -0,0 +1,16 @@
+const ADDRESSES = require("../helper/coreAssets.json")
+
+const { fetchURL } = require('../helper/utils')
+const { sumTokensExport } = require("../helper/sumTokens")
+
+async function fetchTvl(api) {
+  const res = await fetchURL('https://tonfunstats-eqnd7.ondigitalocean.app/api/v1/getServiceTokens?service=blum')
+  await sumTokensExport({ tokens: [ADDRESSES.ton.TON], owners: res.data })(api)
+}
+
+module.exports = {
+  timetravel: false,
+  ton: {
+    tvl: fetchTvl
+  }
+}
