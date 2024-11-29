ed36fbe1c381e6527f9315fff8fed2c9308fb5f5
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-11-29 20:41:32 +0100

    Update Goat Protocol

--- projects/goat-protocol/index.js
+++ projects/goat-protocol/index.js
@@ -2,23 +2,33 @@ const { yieldHelper } = require("../helper/unknownTokens");
 const { getConfig } = require('../helper/cache');
 const { staking } = require('../helper/staking')
 
-const chain = "arbitrum";
-const tokenAPI = "address:want";
+const tokenAPI = "address:asset";
+const balanceAPI = "uint256:totalAssets"
 const GOA_TOKEN_CONTRACT = '0x8c6Bd546fB8B53fE371654a0E54D7a5bD484b319';
 const REWARD_POOL_CONTRACT = '0xAD9CE8580a1Cd887038405275cB02443E8fb88aC';
 
+const chains = {
+    arbitrum: 42161
+}
+
 module.exports = {
-  timetravel: false,
+    timetravel: false,
     doublecounted: true,
-  [chain]: {
-    tvl: async (_, _b, { [chain]: block }) => {
-      const pools = await getConfig('goat-protocol', 'https://raw.githubusercontent.com/goatfi/goat-address-book/main/vault-registry/arbitrum.json');
-      const vaults = [];
-      for(var i = 0; i < pools.length; i++)
-        if(pools[i].platformId != 'goatfi')
-          vaults.push(pools[i].earnedTokenAddress);
-      return yieldHelper({ vaults, chain, block, tokenAPI, useDefaultCoreAssets: true, })
-    },
-    staking: staking(REWARD_POOL_CONTRACT, GOA_TOKEN_CONTRACT)
-  }
+    hallmarks: [
+        [1732186800, "Multistrategies Launch"]
+    ],
+    ...Object.keys(chains).reduce((acc, chain) => {
+        acc[chain] = {
+            tvl: async (_, _b, { [chain]: block }) => {
+                const multistrategies = await getConfig('goat-protocol',`https://api.goat.fi/multistrategies`);
+                const vaults = multistrategies.filter(multistrategy => multistrategy.chain === chain).map(multistrategy => multistrategy.address);
+
+                return yieldHelper({vaults, chain, block, tokenAPI, balanceAPI, useDefaultCoreAssets: true});
+            },
+            ...(chain === 'arbitrum' && {
+                staking: staking(REWARD_POOL_CONTRACT, GOA_TOKEN_CONTRACT),
+            }),
+        };
+        return acc;
+    }, {}),
 }
