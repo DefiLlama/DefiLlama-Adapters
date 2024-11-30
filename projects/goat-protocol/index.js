513988c46355668df32820216da6d9d300f081f8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-30 20:55:31 +0100

    code refactor

--- projects/goat-protocol/index.js
+++ projects/goat-protocol/index.js
@@ -1,34 +1,28 @@
-const { yieldHelper } = require("../helper/unknownTokens");
 const { getConfig } = require('../helper/cache');
 const { staking } = require('../helper/staking')
 
-const tokenAPI = "address:asset";
-const balanceAPI = "uint256:totalAssets"
 const GOA_TOKEN_CONTRACT = '0x8c6Bd546fB8B53fE371654a0E54D7a5bD484b319';
 const REWARD_POOL_CONTRACT = '0xAD9CE8580a1Cd887038405275cB02443E8fb88aC';
 
-const chains = {
-    arbitrum: 42161
+const config = {
+  arbitrum: {}
 }
 
 module.exports = {
-    timetravel: false,
-    doublecounted: true,
-    hallmarks: [
-        [1732186800, "Multistrategies Launch"]
-    ],
-    ...Object.keys(chains).reduce((acc, chain) => {
-        acc[chain] = {
-            tvl: async (_, _b, { [chain]: block }) => {
-                const multistrategies = await getConfig('goat-protocol',`https://api.goat.fi/multistrategies`);
-                const vaults = multistrategies.filter(multistrategy => multistrategy.chain === chain).map(multistrategy => multistrategy.address);
-
-                return yieldHelper({vaults, chain, block, tokenAPI, balanceAPI, useDefaultCoreAssets: true});
-            },
-            ...(chain === 'arbitrum' && {
-                staking: staking(REWARD_POOL_CONTRACT, GOA_TOKEN_CONTRACT),
-            }),
-        };
-        return acc;
-    }, {}),
+  doublecounted: true,
+  hallmarks: [
+    [1732186800, "Multistrategies Launch"]
+  ],
 }
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const multistrategies = await getConfig('goat-protocol', `https://api.goat.fi/multistrategies`);
+      const calls = multistrategies.filter(multistrategy => multistrategy.chain === api.chain).map(multistrategy => multistrategy.address);
+      return api.erc4626Sum({ calls, isOG4626: true, })
+    }
+  }
+})
+
+module.exports.arbitrum.staking = staking(REWARD_POOL_CONTRACT, GOA_TOKEN_CONTRACT)
