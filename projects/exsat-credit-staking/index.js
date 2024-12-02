505d9d86f698b4d5647a1bac73f16c2d1469bae2
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-02 17:16:05 +0100

    mark exsat as doublecounted

--- projects/exsat-credit-staking/index.js
+++ projects/exsat-credit-staking/index.js
@@ -1,12 +1,13 @@
 const { sumTokens } = require("../helper/chain/bitcoin");
-const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
+const bitcoinAddressBook = require('../helper/bitcoin-book/index.js');
 
 async function tvl() {
-    return sumTokens({ owners: await bitcoinAddressBook.exsatCreditStaking() })
-  }
+  return sumTokens({ owners: await bitcoinAddressBook.exsatCreditStaking() })
+}
 
 module.exports = {
-    methodology: 'TVL is based on Bitcoin addresses in the exSat credit staking contract, summing their associated Bitcoin balances.',
-    start: '2024-10-23',
-    bitcoin: { tvl },
+  doublecounted: true,
+  methodology: 'TVL is based on Bitcoin addresses in the exSat credit staking contract, summing their associated Bitcoin balances.',
+  start: '2024-10-23',
+  bitcoin: { tvl },
 };
