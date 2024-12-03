76b7b9347bf366a478f2131c53796980d7fd3a8c
tpktpj <jasmin@tprotocol.io>
2024-12-02 18:09:19 -0700

    List USDaLend

--- projects/usdalend/index.js
+++ projects/usdalend/index.js
@@ -6,8 +6,9 @@ const { mergeExports } = require('../helper/utils')
 //@note Main & Innovative Markets
 const mainMarket = {
 	iotex: aaveExports('', '', undefined, ['0xDB52DD393e3a5e95d3B7C7e1C42cC06bb807A369'], { v3: true }),
+	taiko: aaveExports('', '', undefined, ['0x5EcDC2432ED77cD8E2cE6183712c5cc712c40ec0'], { v3: true }),
+	zircuit: aaveExports('', '', undefined, ['0x5EcDC2432ED77cD8E2cE6183712c5cc712c40ec0'], { v3: true }),
 }
-
 
 module.exports = mergeExports(
 	mainMarket,
