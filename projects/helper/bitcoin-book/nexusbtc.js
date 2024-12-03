5abbea6c40dae63f31a4b9000a206fa40c5848b1
Define101 <93603962+Define101@users.noreply.github.com>
2024-12-03 11:52:40 +0000

    Fix nexus btc (#12538)

--- projects/helper/bitcoin-book/nexusbtc.js
+++ projects/helper/bitcoin-book/nexusbtc.js
@@ -1,10 +1,4 @@
 module.exports = [
     '31oxjGsmepoq2cipeGQ2zKZRRBCf1m3kAC',
-    'bc1pe9sgyavfjphc7pgxfc9kvjfx5hkkjtzfsz2yqtfgddekcgngx0cqx8lt7m',
-    'bc1pq8nu54gwcwgkdnav6gpwylcyhpmnza4tks5cagx6t0730g8jecaqu308s8',
-    'bc1padqwaxlk6gfw2ceeq3caxrn95ygp7fs64x8fxmj829vahw22qu7sql0ga8',
-    'bc1p5xukglzfcelarf7jtegfwxzsfquc4wt8dpulrgwhtmqtpmwg8hxq0t7ett',
-    'bc1pgwfwpkx8rqp2p4lpcztkweqrhnyk973crm5a3c8hw4d2mdpksq3syhpldc',
-    'bc1pu9pzaaywpggum9024zdqzer3u8cuc7pcunrsj0sgskqf2dd4fw4q0k9g38'
   ]
   
