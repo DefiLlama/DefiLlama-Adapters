a989a2a4837c7ebfc5fcf3e45a8afb0a7d2163ad
Define101 <Lovelovenow@protonmail.com>
2024-12-03 12:08:22 +0000

    Divide Resolv products

--- projects/resolv-usr/index.js
+++ projects/resolv-usr/index.js
@@ -1,11 +1,11 @@
-const RLP = "0x4956b52aE2fF65D74CA2d61207523288e4528f96";
+const USR = "0x66a1E37c9b0eAddca17d3662D6c05F4DECf3e110";
 
 const erc20 = {
   "totalSupply": "uint256:totalSupply"
 };
 
 async function ethTvl(api) {
-  api.add(RLP, await api.call({ abi: erc20.totalSupply, target: RLP }));
+  api.add(USR, await api.call({ abi: erc20.totalSupply, target: USR }));
 }
 
 module.exports = {
