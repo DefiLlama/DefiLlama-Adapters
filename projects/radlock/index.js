6ee70500573f8247bdee8bb414c15690a0b412c1
beemdvp <ibrahimpmahmood@gmail.com>
2024-12-05 14:39:50 +0000

    feat: include lp only

--- projects/radlock/index.js
+++ projects/radlock/index.js
@@ -1,7 +1,7 @@
 const { get } = require('../helper/http')
 
 async function tvl(api) {
-  const tokensLockedInXrdList = await get('https://api.radlock.io/token/locked?format=list')
+  const tokensLockedInXrdList = await get('https://api.radlock.io/token/locked?format=list&lpOnly=true')
   return {
     'radix': tokensLockedInXrdList.reduce((acc, token) => acc + +token.xrd, 0)
   }
