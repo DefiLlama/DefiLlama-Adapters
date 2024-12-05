71f6fda230afd73b208e5d5853cbe2fc725a9557
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-05 23:08:52 +0100

    code refactor

--- projects/hermes-v2/index.js
+++ projects/hermes-v2/index.js
@@ -1,31 +1,13 @@
-const { sumTokens2 } = require('../helper/unwrapLPs')
-const tokens = require('./tokens.json')
+const { sumTokensExport } = require('../helper/unwrapLPs')
+const { staking } = require('../helper/staking');
 
-async function stakedLPsTVL() {
-  const stakerAddress = '0x54De3b7b5D1993Db4B2a93C897b5272FBd60e99E'
-  return await sumTokens2({
-    owner: stakerAddress,
-    chain: "arbitrum",
-    resolveUniV3: true,
-    tokens,
-  })
-}
-
-async function burntHermesTVL() {
-  const bHermesAddress = '0x3A0000000000E1007cEb00351F65a1806eCd937C'
-  return await sumTokens2({
-    owner: bHermesAddress,
-    chain: "arbitrum",
-    resolveUniV3: true,
-    tokens: ['0x45940000009600102A1c002F0097C4A500fa00AB']
-  })
-}
+const stakerAddress = '0x54De3b7b5D1993Db4B2a93C897b5272FBd60e99E'
 
 module.exports = {
-  misrepresentedTokens: true,
+  doublecounted: true,
   methodology: 'The sum of All staked Uniswap V3 NFTs and burnt Hermes for staked TVL.',
   arbitrum: {
-    tvl: stakedLPsTVL,
-    staking: burntHermesTVL,
+    tvl: sumTokensExport({ owner: stakerAddress, resolveUniV3: true}),
+    staking: staking('0x3A0000000000E1007cEb00351F65a1806eCd937C', '0x45940000009600102A1c002F0097C4A500fa00AB'),
   },
 };
