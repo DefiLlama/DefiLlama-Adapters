159baf38554cbef93d4083c5f7c865ee6bd1ff0e
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2024-12-06 14:43:33 +0100

    Update: Hashnote (add Near) (#12590)

--- projects/hashnote/index.js
+++ projects/hashnote/index.js
@@ -1,29 +1,25 @@
-const ADDRESSES = require('../helper/coreAssets.json')
+const { call } = require('../helper/chain/near')
 
+const NEAR = '0x136471a34f6ef19fE571EFFC1CA711fdb8E49f2b'
 
-const contracts = {
-  canto:{
-    token: '0xfb8255f0de21acebf490f1df6f0bdd48cc1df03b',
-    oracle: '0x1d18c02bc80b1921255e71cf2939c03258d75470'
-  },
-  ethereum:{
-    token: '0x136471a34f6ef19fE571EFFC1CA711fdb8E49f2b',
-    oracle: '0x4c48bcb2160F8e0aDbf9D4F3B034f1e36d1f8b3e'
-  }
+const CONFIG = {
+  canto: '0xfb8255f0de21acebf490f1df6f0bdd48cc1df03b',
+  ethereum: NEAR,
+  near: 'usyc.near'
 }
 
-const tvl = async (api) => {
-  const totalSupply = await api.call({ target: contracts[api.chain].token, abi: 'uint256:totalSupply'});
-  const rate = await api.call({ target: contracts[api.chain].oracle, abi: 'function latestRoundData() external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)'});
+const evmTvl = async (api, token) => {
+  const supply = await api.call({ target: token, abi: 'erc20:totalSupply' })
+  api.add(NEAR, supply, { skipChain: true })
+}
 
-  return {
-    [ADDRESSES.ethereum.USDC]: (totalSupply * rate.answer)/1e8
+const nonEvmTvl = async (api, token) => {
+  const supply = await call(token, 'ft_total_supply', {});
+  api.add(NEAR, supply, { skipChain: true })
+}
+
+Object.entries(CONFIG).forEach(([chain, address]) => {
+  module.exports[chain] = {
+    tvl: async (api) => (chain === 'near' ? nonEvmTvl(api, address) : evmTvl(api, address))
   };
-}
-
-module.exports = {
-  canto: {
-    tvl
-  },
-  ethereum:{tvl}
-};
+});
