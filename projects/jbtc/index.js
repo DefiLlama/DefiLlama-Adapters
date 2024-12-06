795ea892a1d7ea3cc92386a99d669456fe31f1e6
nexeranet <nuxkill@gmail.com>
2024-12-06 19:15:23 +0200

    update start date

--- projects/jbtc/index.js
+++ projects/jbtc/index.js
@@ -5,7 +5,7 @@ const JBTC_ADDRESS = 'EQCbkRdpnrWZfm07dP5n_wjGx6llNkHNIdmZ-gpIQcDP-J-5'
 
 module.exports = {
   methodology: 'Total amount of BTC locked in smart contract EQCbkRdpnrWZfm07dP5n_wjGx6llNkHNIdmZ-gpIQcDP-J-5.',
-  start: '2024.12.05',
+  start: '2024-12-05',
   ton: {
     tvl: sumTokensExport({ owner: JBTC_ADDRESS, tokens: [ADDRESSES.null]}),
   }
