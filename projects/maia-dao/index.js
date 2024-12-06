98890be1a71aeb629d3908015e3fa04a5845d155
0xLightt <0xlightt@protonmail.com>
2024-12-06 16:35:49 +0000

    also add metis tvl and staking history

--- projects/maia-dao/index.js
+++ projects/maia-dao/index.js
@@ -1,6 +1,10 @@
 const { staking } = require('../helper/staking')
 
 module.exports = {
+  metis: {
+    tvl: () => ({}),
+    staking: () => ({}),
+  },
   arbitrum: {
     tvl: () => 0,
     staking: staking('0x000000f0C01c6200354f240000b7003668B4D080', '0x00000000ea00F3F4000e7Ed5Ed91965b19f1009B'), // v2
