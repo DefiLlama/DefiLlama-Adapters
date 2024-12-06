65ca1ef6493696dfff8f7a37f5dac77fe667579f
0xLightt <0xlightt@protonmail.com>
2024-12-06 15:26:10 +0000

    keep hermes tvl history intact

--- projects/hermes-protocol/index.js
+++ projects/hermes-protocol/index.js
@@ -4,5 +4,6 @@ module.exports = {
   misrepresentedTokens: true,
   metis:{
     tvl: uniTvlExport("0x633a093C9e94f64500FC8fCBB48e90dd52F6668F", "metis", undefined, undefined, { hasStablePools: true, useDefaultCoreAssets: true, }),
+    staking: () => ({}),
   },
 }
