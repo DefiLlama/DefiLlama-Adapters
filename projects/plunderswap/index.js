fd3b08740f75c6bbfc29d8f290e7c6d0116ea349
DarthGus <darthgus@gmail.com>
2024-12-07 09:08:50 +0800

    update Plunderswap Univ3

--- projects/plunderswap/index.js
+++ projects/plunderswap/index.js
@@ -1,3 +1,6 @@
 const { uniTvlExport, } = require('../helper/unknownTokens')
+const { uniV3Export } = require('../helper/uniswapV3')
 
 module.exports = uniTvlExport('zilliqa', '0xf42d1058f233329185A36B04B7f96105afa1adD2')
+module.exports = uniV3Export('zilliqa', '0x000A3ED861B2cC98Cc5f1C0Eb4d1B53904c0c93a')
+
