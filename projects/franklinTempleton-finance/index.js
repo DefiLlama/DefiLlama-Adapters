edf5ea8fd1e82875a2aaf855d67fad1c226f7cae
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2024-12-07 09:26:08 +0100

    Update: Franklin-Templeton (add Ethereum) (#12592)

--- projects/franklinTempleton-finance/index.js
+++ projects/franklinTempleton-finance/index.js
@@ -5,7 +5,8 @@ const BENJI = {
   arbitrum: '0xb9e4765bce2609bc1949592059b17ea72fee6c6a',
   polygon: '0x408a634b8a8f0de729b48574a3a7ec3fe820b00a',
   avax: '0xe08b4c1005603427420e64252a8b120cace4d122',
-  base: '0x60cfc2b186a4cf647486e42c42b11cc6d571d1e4'
+  base: '0x60cfc2b186a4cf647486e42c42b11cc6d571d1e4',
+  ethereum: '0x3ddc84940ab509c11b20b76b466933f40b750dc9'
 }
 
 const stellarTvl = async (api) => {
