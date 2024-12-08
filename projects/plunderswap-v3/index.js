06a75b388224b6b57bb8a09ba44dedd79dd58470
DarthGus <darthgus@gmail.com>
2024-12-08 15:35:52 +0800

    Update plunderswap v2 to use same method as v3, add some more token mappings, include kUSD.

--- projects/plunderswap-v3/index.js
+++ projects/plunderswap-v3/index.js
@@ -1,12 +1,32 @@
 const { getConfig } = require('../helper/cache')
 const { sumTokens2 } = require('../helper/unwrapLPs')
 
+const KUSD = '0xE9DF5B4B1134a3AaDf693DB999786699B016239e'
+const zUSDT = '0x2274005778063684FBB1BFa96A2B725Dc37D75F9'
+
 async function tvl(api) {
   const data = await getConfig('plunderswap', 'https://static.plunderswap.com/PlunderswapPoolPrices.json')
   const v3Pairs = data.filter(pair => pair.version === 'V3').map(pair => pair.address)
   const token0s = await api.multiCall({ abi: 'address:token0', calls: v3Pairs })
   const token1s = await api.multiCall({ abi: 'address:token1', calls: v3Pairs })
-  return sumTokens2({ api, tokensAndOwners2: [token0s.concat(token1s), v3Pairs.concat(v3Pairs)] })
+
+  // Get kUSD balance from all v3 pairs - kUSD is represented as zUSDT (1:1 backed stablecoin)
+  const kUsdBalances = await api.multiCall({
+    abi: 'erc20:balanceOf',
+    target: KUSD,
+    calls: v3Pairs
+  })
+
+  // Sum up balances
+  const totalKusd = kUsdBalances.reduce((a, b) => Number(a) + Number(b), 0)
+  
+  // Add kUSD value to api balances as zUSDT
+  api.add(zUSDT, totalKusd)
+
+  return sumTokens2({ 
+    api, 
+    tokensAndOwners2: [token0s.concat(token1s), v3Pairs.concat(v3Pairs)]
+  })
 }
 
 module.exports = {
