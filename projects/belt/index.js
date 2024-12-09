8292afd778ab0471c2e2335842259a37198644aa
0xpeluche <0xpeluche@proton.me>
2024-12-09 19:28:21 +0100

    no staking on heco

--- projects/belt/index.js
+++ projects/belt/index.js
@@ -1,9 +1,6 @@
 const CONFIG = require('./config')
 
-const beltView = {
-  bsc: '0xB543248F75fd9f64D10c247b5a57F142EFF88Aac',
-  heco: '0x4A971318e2b1ea1A08bC713eEFF9118Cd2F85803'
-}
+const beltView = '0xB543248F75fd9f64D10c247b5a57F142EFF88Aac'
 
 const abi = {
   calcPoolValueInToken: "uint256:calcPoolValueInToken",
@@ -18,7 +15,7 @@ const tvl = async (api) => {
 }
 
 const staking = async (api) => {
-  const { token, pricePerFullShare, beltTokenTotalSupply } = await api.call({ target: beltView[api.chain], abi: abi.getStakingStat })
+  const { token, pricePerFullShare, beltTokenTotalSupply } = await api.call({ target: beltView, abi: abi.getStakingStat })
   api.add(token, beltTokenTotalSupply * pricePerFullShare / 1e18)
 }
 
@@ -26,6 +23,6 @@ module.exports = {
   timetravel: false,
   methodology: 'TVL includes the liquidity of all the Vaults, 3Tether LP and staking counts the BELT that has been staked in BSC.',
   bsc: { tvl, staking },
-  heco: { tvl, staking },
+  heco: { tvl },
   klaytn: { tvl }
 }
