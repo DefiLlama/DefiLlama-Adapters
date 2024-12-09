b342311561caa8e629f943a6c06e71858e1b8295
GemsGame <a.masanovets@gmail.com>
2024-12-09 19:27:05 +0300

    ref: methodology

--- projects/rats-game/index.js
+++ projects/rats-game/index.js
@@ -1,10 +1,10 @@
 const { sumTokensExport } = require('../helper/chain/ton');
-const ADDRESSES = require('../helper/coreAssets.json')
+const ADDRESSES = require('../helper/coreAssets.json');
 
 module.exports = {
-  methodology: 'Counts the donation wallet size as the TVL.',
+  methodology: 'Treasury (donations) amount',
   timetravel: false,
   ton: {
-    tvl: sumTokensExport({ owner: 'UQCwN6wUedlW5GJX6ATYNcBaUhpYt5eCWBRA6kK6mfMtIl90', tokens: [ADDRESSES.null]}),
+    treasury: sumTokensExport({ owner: 'UQCwN6wUedlW5GJX6ATYNcBaUhpYt5eCWBRA6kK6mfMtIl90', tokens: [ADDRESSES.null]}),
   }
 }
