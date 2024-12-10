e2f0774b1e9afeedeaf5883b57697d06b6c50d9c
nexeranet <nuxkill@gmail.com>
2024-12-10 12:57:48 +0200

    update bitcoin addresses

--- projects/jbtc/index.js
+++ projects/jbtc/index.js
@@ -1,12 +1,9 @@
 const { sumTokensExport } = require('../helper/sumTokens')
-const ADDRESSES = require("../helper/coreAssets.json");
-
-const JBTC_ADDRESS = 'EQCbkRdpnrWZfm07dP5n_wjGx6llNkHNIdmZ-gpIQcDP-J-5'
+const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
 
 module.exports = {
-  methodology: 'Total amount of BTC locked in smart contract EQCbkRdpnrWZfm07dP5n_wjGx6llNkHNIdmZ-gpIQcDP-J-5.',
-  start: '2024-12-05',
-  ton: {
-    tvl: sumTokensExport({ owner: JBTC_ADDRESS, tokens: [ADDRESSES.null]}),
+  methodology: 'Total amount of BTC locked in Bitcoin network on wallet bc1qmukuv7j57umsd5tgg9fw88eqap57rzphkfckyp'
+  bitcoin: {
+    tvl: sumTokensExport({ owner: bitcoinAddressBook.jbtc }),
   }
 }
