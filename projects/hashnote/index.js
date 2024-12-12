aefefa0a3d0dfd2801fa8c449bdea8fb5b1badaa
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-12 16:47:13 +0100

    minor fix

--- projects/hashnote/index.js
+++ projects/hashnote/index.js
@@ -1,28 +1,28 @@
 const { call } = require('../helper/chain/near')
 const { get } = require("../helper/http");
 
-const NEAR = '0x136471a34f6ef19fE571EFFC1CA711fdb8E49f2b'
+const USYC = '0x136471a34f6ef19fE571EFFC1CA711fdb8E49f2b'
 
 const CONFIG = {
   canto: '0xfb8255f0de21acebf490f1df6f0bdd48cc1df03b',
-  ethereum: NEAR,
+  ethereum: USYC,
   near: 'usyc.near',
   noble: 'uusyc'
 }
 
 const evmTvl = async (api, token) => {
   const supply = await api.call({ target: token, abi: 'erc20:totalSupply' })
-  api.add(NEAR, supply, { skipChain: true })
+  api.add(USYC, supply, { skipChain: true })
 }
 
 const nonEvmTvl = async (api, token) => {
   const supply = await call(token, 'ft_total_supply', {});
-  api.add(NEAR, supply, { skipChain: true });
+  api.add(USYC, supply, { skipChain: true });
 }
 
 const nobleTvl = async (api, token) => {
   const res = await get(`https://rest.cosmos.directory/noble/cosmos/bank/v1beta1/supply/by_denom?denom=${token}`);
-  api.add(CONFIG.noble, parseInt(res.amount.amount), { skipChain: true });
+  api.add(USYC, parseInt(res.amount.amount), { skipChain: true });
 }
 
 Object.entries(CONFIG).forEach(([chain, address]) => {
