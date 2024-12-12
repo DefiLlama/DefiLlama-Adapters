b02dfe0f426199ad4d37b6c0232958a4156b5269
KafkaUnderCurrent <kafka@undercurrent.tech>
2024-12-12 21:51:49 +0700

    Fix calculate token amount

--- projects/wansui/index.js
+++ projects/wansui/index.js
@@ -1,7 +1,12 @@
 const sui = require("../helper/chain/sui");
+const BigNumber = require('bignumber.js');
 
 const CREATED_EVENT = "0xc4bb66da17fec7444b7b3e2ac750e35ea6225f5cca936c423fad2c78245d987c::suipump::Created";
 
+/** @typedef {import("@defillama/sdk").ChainApi} ChainApi */
+/**
+ * @param {ChainApi} api 
+ */
 async function tvl(api) {
     const createdTokens = await sui.queryEvents({
         eventType: CREATED_EVENT,
@@ -13,24 +18,29 @@ async function tvl(api) {
 
             return {
                 token: tokenAddr,
-                owner: e.created_by
+                bondingCurve: e.bonding_curve
             }
         }
     })
 
-    const tokens = createdTokens.map(e => e.token)
-    const owners = createdTokens.map(e => e.owner)
+    const bondingCurveData = await sui.getObjects(createdTokens.map(t => t.bondingCurve))
 
-    return await sui.sumTokens(
-        {
-            owners,
-            tokens,
-            api
-        }
-    )
+    const suiDecimal = 6
+    const balances = bondingCurveData.map(item => {
+        // When created, bonding curve has 800M tokens to sell
+        const initialAmount = new BigNumber(800_000_000).multipliedBy(10 ** suiDecimal)
+        const tokenReserve = new BigNumber(item.fields.real_token_reserves)
+
+        return initialAmount.minus(tokenReserve)
+    })
+
+    const tokens = createdTokens.map(t => t.token)
+
+    api.addTokens(tokens, balances)
 }
 
 module.exports = {
+    methodology: "We count the tokens that have been created by the bonding curve and subtract the amount of tokens that are still in the bonding curve.",
     sui: {
         tvl,
     },
