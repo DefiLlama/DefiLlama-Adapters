d926e9d983e4c06e130d304bbc5a62042eddae4d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-12 14:53:19 +0100

    code refactor

--- projects/javsphere/index.js
+++ projects/javsphere/index.js
@@ -1,6 +1,4 @@
 const { staking } = require("../helper/staking.js");
-const {ethers} = require("ethers");
-const BigNumber = require("bignumber.js");
 
 const STAKING_BASE = '0xE420BBb4C2454f305a3335BBdCE069326985fb5b'
 const FREEZER_BASE = '0x03e225D2bd32F5ecE539005B57F9B94A743ADBFB'
@@ -8,27 +6,10 @@ const VESTING_BASE = '0x42a40321843220e9811A1385D74d9798436f7002'
 const JAV_BASE = '0xEdC68c4c54228D273ed50Fc450E253F685a2c6b9'
 const LEVERAGEX_BASE_EARN = '0xfd916d70eb2d0e0e1c17a6a68a7fbede3106b852'
 
-const earnAbi = [{
-  "inputs": [],
-  "name": "tvl",
-  "outputs": [
-    {
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-}];
-const provider = new ethers.JsonRpcProvider("https://mainnet.base.org");
-const contractEarn = new ethers.Contract(LEVERAGEX_BASE_EARN, earnAbi, provider);
-
-async function tvl() {
-  const [earnTVL] = await Promise.all([
-    contractEarn.tvl(),
-   ]);
-  return new BigNumber(earnTVL).toNumber() / 1e18;
+async function tvl(api) {
+  const config = await api.fetchList({ lengthAbi: 'tokensCount', itemAbi: "function tokens(uint256) view returns (address asset, bytes32 priceFeed, uint256 targetWeightage, bool isActive)", target: LEVERAGEX_BASE_EARN })
+  const tokens = config.map(i => i.asset)
+  return api.sumTokens({ owner: LEVERAGEX_BASE_EARN, tokens, })
 }
 
 module.exports = {
