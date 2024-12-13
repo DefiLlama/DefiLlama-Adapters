ec5a3770b1ccac633dc361c892eb65eaf2c35122
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-12-13 23:06:01 +0400

    Update index.js

--- projects/bbbpump/index.js
+++ projects/bbbpump/index.js
@@ -1,24 +1,6 @@
-const { sumTokens2 } = require('../helper/unwrapLPs')
-
-const BBBPUMP_CONTRACT = "0x2E24BFdE1EEDa0F1EA3E57Ba7Ff10ac6516ab5Ec";
-
-async function tvl(timestamp, block, chainBlocks, { chain = 'xdc' } = {}) {
-    try {
-        const balances = {};
-        return await sumTokens2({ 
-            balances,
-            owner: BBBPUMP_CONTRACT,
-            chain,
-            block: chainBlocks[chain],
-        });
-    } catch (error) {
-        console.error(`Error in BBBPUMP tvl: ${error.message}`);
-        throw error;
-    }
-}
-
+const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs');
+const BBBPUMP_CONTRACT = '0x2E24BFdE1EEDa0F1EA3E57Ba7Ff10ac6516ab5Ec'
 module.exports = {
-    xdc: {
-        tvl
-    }
+    start: '2024-10-10',
+    xdc: { tvl: sumTokensExport({ owner: BBBPUMP_CONTRACT, tokens: [nullAddress]}) },
 };
