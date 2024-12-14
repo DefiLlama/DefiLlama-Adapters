f6e3ada02c8817d82585a81974bf68d2e4a16622
Linkie Link <linkielink.dev@gmail.com>
2024-12-14 10:19:31 +0100

    add 24h volume metrics to mars-perps

--- projects/mars-perp/index.js
+++ projects/mars-perp/index.js
@@ -1,10 +1,14 @@
 const { queryContract } = require('../helper/chain/cosmos');
+const BigNumber = require('bignumber.js');
+const axios = require('axios');
 
 const contractAddresses = {
   neutron: {
     perps: 'neutron1g3catxyv0fk8zzsra2mjc0v4s69a7xygdjt85t54l7ym3gv0un4q2xhaf6'
   },
 };
+
+
 
 async function tvl(api) {
   const chain = api.chain;
@@ -15,9 +19,30 @@ async function tvl(api) {
   if (perpsVault) api.add(info.base_denom, perpsVault['total_balance']);
 }
 
+async function fetch(api, timestamp) {
+const perpsInfoApi = `https://backend.prod.mars-dev.net/v2/perps_overview?chain=${api.chain}&days=2&response_type=global`
+  const fetchedData = (
+    await axios.get(perpsInfoApi)
+  );
+
+  let last24HourVolume = 0
+  let fetchTimestamp = timestamp
+  // The second element in the array is the last 24 hour volume, while the first element is the current volume of the ongoing day
+  if(fetchedData.data.daily_trading_volume.length > 1) {
+    // Volume is returned in uusd which has 6 decimals
+    last24HourVolume = new BigNumber(fetchedData.data.daily_trading_volume[1].value).shiftedBy(-6)
+    fetchTimestamp = Math.round(new Date(fetchedData.data.daily_trading_volume[1].date).getTime()/1000)
+  }
+
+  return {
+    last24HourVolume,
+    timestamp: fetchTimestamp,
+  };
+}
+
 module.exports = {
   timetravel: false,
   methodology:
     "For each chain, sum token balances by querying the total deposit amount for each asset in the chain's params contract.",
-  neutron: { tvl },
+  neutron: { tvl, fetch },
 }
