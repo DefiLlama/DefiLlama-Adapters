9f943a290e0e7572540a804bc051dd209cb4dc2b
Linkie Link <linkielink.dev@gmail.com>
2024-12-14 10:30:40 +0100

    use axios

--- projects/mars-perp/index.js
+++ projects/mars-perp/index.js
@@ -1,7 +1,6 @@
-
-const fetchURL = require("../utils/fetchURL");
 const { queryContract } = require('../helper/chain/cosmos');
 const BigNumber = require('bignumber.js');
+const axios = require('axios');
 
 const contractAddresses = {
   neutron: {
@@ -22,18 +21,19 @@ async function tvl(api) {
 
 async function fetch(timestamp) {
 const perpsInfoApi = `https://backend.prod.mars-dev.net/v2/perps_overview?chain=neutron&days=2&response_type=global`
-const { global_overview } = (
-  await fetchURL(perpsInfoApi)
+const perpsVolumeData = (
+  await axios(perpsInfoApi)
 );  
 
 
   let last24HourVolume = 0
   let fetchTimestamp = timestamp
+  const globalOverview = perpsVolumeData.data.globalOverview
   // The second element in the array is the last 24 hour volume, while the first element is the current volume of the ongoing day
-  if(global_overview && global_overview.daily_trading_volume.length > 1) {
+  if(globalOverview && globalOverview.daily_trading_volume.length > 1) {
     // Volume is returned in uusd which has 6 decimals
-    last24HourVolume = new BigNumber(global_overview.daily_trading_volume[1].value).shiftedBy(-6)
-    fetchTimestamp = Math.round(new Date(global_overview.daily_trading_volume[1].date).getTime()/1000)
+    last24HourVolume = new BigNumber(globalOverview.daily_trading_volume[1].value).shiftedBy(-6)
+    fetchTimestamp = Math.round(new Date(globalOverview.daily_trading_volume[1].date).getTime()/1000)
   }
 
   return {
