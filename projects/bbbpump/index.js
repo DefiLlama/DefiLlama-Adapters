6ac9afe420434a96def96df8b2415bea726e9792
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-12-14 21:08:03 +0400

    Update index.js

--- projects/bbbpump/index.js
+++ projects/bbbpump/index.js
@@ -1,11 +1,20 @@
-const { sumTokensExport,nullAddress } = require('../helper/unwrapLPs');
+const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs');
+const { stakings } = require("../helper/staking");
+
 module.exports = {
     start: '2024-10-10',
-    xdc: { tvl: sumTokensExport({ owners: [
-        '0x2E24BFdE1EEDa0F1EA3E57Ba7Ff10ac6516ab5Ec', // BBBPump
-        '0x37c00AE5C4b49Ab0F5fD2FFB1033588e9bC33B08', // Megadrop BBB
-      ], tokens: [
-        nullAddress,
-        "0xfa4ddcfa8e3d0475f544d0de469277cf6e0a6fd1"
-      ]}) },
+    xdc: {
+        tvl: sumTokensExport({
+            owners: [
+                '0x2E24BFdE1EEDa0F1EA3E57Ba7Ff10ac6516ab5Ec', // BBBPump
+            ],
+            tokens: [
+                nullAddress
+            ]
+        }),
+        staking: stakings(
+            ['0x37c00AE5C4b49Ab0F5fD2FFB1033588e9bC33B08'], // Megadrop BBB
+            "0xfa4ddcfa8e3d0475f544d0de469277cf6e0a6fd1" ///BBB
+        ),
+    },
 };
