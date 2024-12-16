2120369788e4a9a71cdad23293982c3a011cb081
Emanuele Gaspari <inmarelibero@gmail.com>
2024-12-16 16:41:08 +0100

    updated adapter to call directly KernelDAO's contracts

--- projects/kernel-dao/index.js
+++ projects/kernel-dao/index.js
@@ -1,30 +1,41 @@
-const ASSET_ACCUMULATOR = '0xeDC562F52aa31E74081359901D4f3a371Cf95371';
+/**
+ * Kernel DAO contracts
+ * 
+ * @see https://github.com/Kelp-DAO/kernel-smart-contracts-private/blob/develop/doc/contract-address/Mainnet.md
+ */
+const ASSET_REGISTRY = '0xd0B91Fc0a323bbb726faAF8867CdB1cA98c44ABB';
 
-functionsToCall = [
-    {
-        abi: 'uint256:getTotalBNB',
-        tokenKey: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', // WBNB
-    },
-    {
-        abi: 'uint256:getTotalBTC',
-        tokenKey: '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', // BTCB
-    },
-    {
-        abi: 'uint256:getTotalETH',
-        tokenKey: '0x2170Ed0880ac9A755fd29B2688956BD959F933F8', // WETH
-    },
-]
 
+/**
+ * Get KernelDAO managed assets
+ */
+async function getAssets(api) {
+  return await api.call({
+    abi: 'function getAssets() external view returns (address[] memory)',
+    target: ASSET_REGISTRY,
+    params: [],
+  });
+}
+
+/**
+ * Calculate TVL
+ */
 async function tvl(api) {
-    for (const functionToCall of functionsToCall) {
-        const collateralBalance = await api.call({
-          abi: functionToCall.abi,
-          target: ASSET_ACCUMULATOR,
-          params: [],
-        });
-      
-        api.add(functionToCall.tokenKey, collateralBalance);
-    };
+  // KernelDAO managed assets
+  const assets = await getAssets(api);
+
+  // iterate on KernelDAO managed assets
+  for (const asset of assets) {
+    // get Vault's balance
+    const balance = await api.call({
+      abi: 'function getVaultBalance(address asset) external view returns (uint256)',
+      target: ASSET_REGISTRY,
+      params: [asset],
+    });
+
+    // add balance to TVL
+    api.add(asset, balance);
+  };
 }
 
 module.exports = {
