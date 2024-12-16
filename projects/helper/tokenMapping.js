01a53a239b97ba2423adc59373f461b077a7be9d
tanglenaut87 <90203390+tanglenaut87@users.noreply.github.com>
2024-12-16 10:06:16 +0100

    New DEX: Metropolis Exchange (& new chain: Sonic) (#12715)
    Co-authored-by: tanglenaut <>
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -38,6 +38,9 @@ const ibcMappings = {
 }
 
 const fixBalancesTokens = {
+  sonic: {
+    '0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38': { coingeckoId: "fantom", decimals: 18 },
+  },
   // Sample Code
   ozone: {
     // '0x83048f0bf34feed8ced419455a4320a735a92e9d': { coingeckoId: "ozonechain", decimals: 18 }, // was mapped to wrong chain
