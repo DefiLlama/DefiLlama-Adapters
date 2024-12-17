f480ec808aab8899573dcd3e7572c0b752f38502
Ollie Ottersync <105137269+ollieottersync@users.noreply.github.com>
2024-12-16 19:30:49 -0800

    add Elys Network to env.js

--- projects/helper/env.js
+++ projects/helper/env.js
@@ -10,6 +10,7 @@ const DEFAULTS = {
   COVALENT_KEY: 'ckey_72cd3b74b4a048c9bc671f7c5a6',
   // SOLANA_RPC: 'https://mainnet.helius-rpc.com/?api-key=0109717a-77b4-498a-bc3c-a0b31aa1b3bf',
   SOLANA_RPC: "https://api.mainnet-beta.solana.com",
+  ELYS_RPC: "https://rpc.elys.network",
   ECLIPSE_RPC: 'https://eclipse.lgns.net',
   APTOS_RPC: 'https://fullnode.mainnet.aptoslabs.com',
   SUI_RPC: 'https://fullnode.mainnet.sui.io/',
