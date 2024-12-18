72954af87eab8e36ca36db2cdc5658e24bc5c7ce
GaussETH <GaussETH.xyz@gmail.com>
2024-12-18 14:09:45 +0800

    feat: update FeeFree adpater

--- projects/FeeFree/index.js
+++ projects/FeeFree/index.js
@@ -1,23 +1,25 @@
 const { getLogs2 } = require('../helper/cache/getLogs')
 const { sumTokens2 } = require('../helper/unwrapLPs')
 
-const EVENT_ABI = 'event Initialize(bytes32 id, address indexed currency0, address indexed currency1, uint24 fee, int24 tickSpacing, address hooks)'
+const EVENT_ABI = 'event Initialize(bytes32 indexed id, address indexed currency0, address indexed currency1, uint24 fee, int24 tickSpacing, address hooks, uint160 sqrtPriceX96, int24 tick)'
 
 const config = {
   zora: {
-    pool: "0xB43287b2106BC044F07aE674794f5492E851d3dC",
-    router: "0x0Fee97363deEFBE4De038D437D805A98dbEbA400",
-    fromBlock: 13704184,
+    pool: "0x2BB2DeAeF5D4E62D3798Ce67C3D623da09188AB9",
+    fromBlock: 23864003,
   },
   base: {
-    pool: "0xc08304a5300D9a2310A603b8D7fB8470f752947F",
-    router: "0x0Fee76f15DE74A5211e5Bc2aBF95394d7f50C400",
-    fromBlock: 14089843,
+    pool: "0x60B393a76cEa4a3AFff00e1Fb08d0F63A8F4A314",
+    fromBlock: 23809396,
+  },
+  scroll: {
+    pool: "0xA407e0637b22e1F64395D177C8EAD26C03dC3294",
+    fromBlock: 11975196,
   }
 }
 
 module.exports = {
-  start: '2024-04-25', // Apr 26 2024
+  start: '2024-12-17',
 }
 
 Object.keys(config).forEach(chain => {
