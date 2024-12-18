cbcc13ed4e60123176cfc038d6d8267a087865a0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-18 14:53:06 +0100

    minor fix

--- projects/helper/env.js
+++ projects/helper/env.js
@@ -19,6 +19,7 @@ const DEFAULTS = {
   ANKR_API_KEY: '79258ce7f7ee046decc3b5292a24eb4bf7c910d7e39b691384c7ce0cfb839a01',
   RENEC_RPC: "https://api-mainnet-beta.renec.foundation:8899/",
   RPC_PROXY_URL: "https://rpc-proxy.llama.fi",
+  DUCKCHAIN_RPC: "https://rpc.duckchain.io,https://rpc-hk.duckchain.io",
 }
 
 const ENV_KEYS = [
