a17c27cf8d8cb61708a16344670233de2dbfc93c
Igor Shelkovenkov <is@topiet.de>
2024-12-18 15:01:02 +0100

    Add LeverageX Diamond (traders collateral) side tvl

--- projects/javsphere/index.js
+++ projects/javsphere/index.js
@@ -5,11 +5,12 @@ const FREEZER_BASE = '0x03e225D2bd32F5ecE539005B57F9B94A743ADBFB'
 const VESTING_BASE = '0x42a40321843220e9811A1385D74d9798436f7002'
 const JAV_BASE = '0xEdC68c4c54228D273ed50Fc450E253F685a2c6b9'
 const LEVERAGEX_BASE_EARN = '0xfd916d70eb2d0e0e1c17a6a68a7fbede3106b852'
+const LEVERAGEX_BASE_DIAMOND = '0xBF35e4273db5692777EA475728fDbBa092FFa1B3'
 
 async function tvl(api) {
   const config = await api.fetchList({ lengthAbi: 'tokensCount', itemAbi: "function tokens(uint256) view returns (address asset, bytes32 priceFeed, uint256 targetWeightage, bool isActive)", target: LEVERAGEX_BASE_EARN })
   const tokens = config.map(i => i.asset)
-  return api.sumTokens({ owner: LEVERAGEX_BASE_EARN, tokens, })
+  return api.sumTokens({ owners: [LEVERAGEX_BASE_EARN, LEVERAGEX_BASE_DIAMOND], tokens, })
 }
 
 module.exports = {
