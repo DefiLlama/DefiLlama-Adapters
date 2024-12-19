1517ad655c63dc867a645efbea2eee96a3c02e97
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2024-12-19 13:34:59 +0100

    Fix: Plenty (rateLimit) (#12781)

--- projects/helper/chain/tezos.js
+++ projects/helper/chain/tezos.js
@@ -86,7 +86,7 @@ async function resolveLPPosition({ balances = {}, owner, lpToken, transformAddre
 }
 
 async function sumTokens({ owners = [], balances = {}, includeTezos = false }) {
-  const { errors } = await PromisePool.withConcurrency(10)
+  const { errors } = await PromisePool.withConcurrency(5)
     .for(owners)
     .process(async item => {
       await getTokenBalances(item, includeTezos, { balances })
