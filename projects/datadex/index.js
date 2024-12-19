f5a32fda1cbf62e0eafe32e7f2e8930cf7b3e54d
TheNewAutonomy <darren@ourfamily.io>
2024-12-19 20:04:26 +0000

    Exclude VCAT token that throws balances out

--- projects/datadex/index.js
+++ projects/datadex/index.js
@@ -1,4 +1,9 @@
 const { uniV3Export } = require('../helper/uniswapV3')
 module.exports = uniV3Export({
-  vana: { factory: '0xc2a0d530e57B1275fbce908031DA636f95EA1E38', fromBlock: 763744 },
+  vana: { factory: '0xc2a0d530e57B1275fbce908031DA636f95EA1E38',
+          blacklistedTokens: [
+            '0xbd2d7c728b224961fdb25ccf2a67eb3c25f5ec52', // VCAT decimals incorrect and throws out TVL
+          ],
+          fromBlock: 763744
+        },
 })
