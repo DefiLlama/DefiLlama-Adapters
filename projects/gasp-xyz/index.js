5f502ef6157717e179f98d37e6296d35a1d9d477
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-19 11:13:40 +0100

    track gasp.xyz

---
+++ projects/gasp-xyz/index.js
@@ -0,0 +1,14 @@
+const { sumTokensExport } = require('../helper/unwrapLPs')
+
+const config = {
+  ethereum: '0x79d968d9017B96f202aD4673A2c1BBbdc905A4ca',
+  arbitrum: '0x3aDdEb54ddd43Eb40235eC32DfA7928F28A44bb5',
+  base: '0x308e483afDD225D6cb7bF4d44B8e4a03DFD9c0De',
+}
+
+Object.keys(config).forEach(chain => {
+  const owner = config[chain]
+  module.exports[chain] = {
+    tvl: sumTokensExport({ owner, fetchCoValentTokens: true })
+  }
+})
