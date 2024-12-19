5f5e505678011812375752b1c9cad29bc79ccd40
Andrew-kerslake <kopimldev@gmail.com>
2024-12-19 17:31:34 +0000

    Update index.js
    remove console logs

--- projects/kopi/index.js
+++ projects/kopi/index.js
@@ -1,9 +1,4 @@
-const { JsonRpcProvider } = require('ethers');
 const { get } = require('../helper/http');
-const { totalSupply } = require('@defillama/sdk/build/erc20');
-
-
-
 
 async function getDEXTvl() {
     let total = 0;
@@ -16,16 +11,12 @@ async function getDEXTvl() {
         const price = await get(`${endp}/kopi/dex/price?denom_giving=${fromDen}&denom_receiving=${toDen}`);
         // inj token
         if (toDen == 'ibc/DE63D8AC34B752FB7D4CAA7594145EDE1C9FC256AC6D4043D0F12310EB8FC255') {
-            console.log(((amount / (10 ** 18)) * parseFloat(price.price)))
             return ((amount / (10 ** 18)) * parseFloat(price.price));
         } else {
-            console.log(((amount / (10 ** 6)) * parseFloat(price.price)))
             return ((amount / (10 ** 6)) * parseFloat(price.price));
-
         }
     };
     for (let i = 0; i < denoms.length; i++) {
-        console.log(denoms[i])
         promises.push(getPrices(rest, fromDenom, denoms[i].denom, denoms[i].amount));
     }
     async function getAll() {
@@ -36,18 +27,15 @@ async function getDEXTvl() {
                 for (let i = 0; i < promises.length; i++) {
                     price = price + result[i];
                 }
-                console.log("priceLTV: " + price);
                 total = price;
             });
     }
     await getAll();
-    console.log("total: "+total);
     return total;
 }
 
 
 module.exports = {
-    timetravel: false,
     kopi: {
         tvl: async () => await getDEXTvl()
     }
