3478b70420d0fc698d30bdb7ef5ae9571f042e59
Xavier <ladig1017@gmail.com>
2024-12-19 17:10:05 +0900

    feat: update amm project name

--- projects/sonic-market-amm/index.js
+++ projects/sonic-market-amm/index.js
@@ -1,7 +1,7 @@
 const { getUniTVL } = require("../helper/unknownTokens");
 
 module.exports = {
-  methodology: "TVL consists of assets deposited into the Sonic Market CPMM contract",
+  methodology: "TVL consists of assets deposited into the Sonic Market AMM contract",
   sonic: {
     tvl: getUniTVL({
       factory: "0x01D6747dD2d65dDD90FAEC2C84727c2706ee28E2",
