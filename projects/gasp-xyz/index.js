6558349690f60a654d92a7a35a58ce9825238da1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-19 11:14:36 +0100

    gasp: add comment

--- projects/gasp-xyz/index.js
+++ projects/gasp-xyz/index.js
@@ -1,5 +1,6 @@
 const { sumTokensExport } = require('../helper/unwrapLPs')
 
+// taken from https://gasp-stash-prod-dot-direct-pixel-353917.oa.r.appspot.com/affirmed-network/list
 const config = {
   ethereum: '0x79d968d9017B96f202aD4673A2c1BBbdc905A4ca',
   arbitrum: '0x3aDdEb54ddd43Eb40235eC32DfA7928F28A44bb5',
