50bffe65d4438e305bd6dfcd555bb15f36dd5dd9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-19 11:43:36 +0100

    code refactor

--- projects/sonic-market-amm/index.js
+++ projects/sonic-market-amm/index.js
@@ -1,11 +1,4 @@
-const { getUniTVL } = require("../helper/unknownTokens");
-
-module.exports = {
-  methodology: "TVL consists of assets deposited into the Sonic Market AMM contract",
-  sonic: {
-    tvl: getUniTVL({
-      factory: "0x01D6747dD2d65dDD90FAEC2C84727c2706ee28E2",
-      useDefaultCoreAssets: true,
-    }),
-  }
-};
+const { uniTvlExports } = require('../helper/unknownTokens')
+module.exports = uniTvlExports({
+  'sonic': '0x01D6747dD2d65dDD90FAEC2C84727c2706ee28E2'
+})
