c3d18dd1ff401f97caa61868b24d9d01cf2a44fd
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-19 13:33:13 +0100

    code refactor

--- projects/agoric/index.js
+++ projects/agoric/index.js
@@ -1,74 +1,54 @@
-const sdk = require("@defillama/sdk");
-const { postURL } = require('../helper/utils')
+const { post } = require('../helper/http')
 
-const coinGeckoId = 'inter-stable-token';
-
-const queryData = async (query) => {
-  const url = 'https://api.subquery.network/sq/agoric-labs/agoric-mainnet-v2';
-  try {
-    const response = await postURL(url, { query });
-    return response.data.data;
-  } catch (error) {
-    console.error('Error fetching data from Subquery:', error);
-    throw error;
+const query = `
+query {
+  vaultManagerMetrics {
+    nodes {
+      liquidatingCollateralBrand
+      totalCollateral
+    }
+  }
+  oraclePrices {
+    nodes {
+      typeInAmount
+      typeOutAmount
+      typeInName
+    }
+  }
+  boardAuxes {
+    nodes {
+      allegedName
+      decimalPlaces
+    }
   }
 }
+`
 
-const tvl = async () => {
-  const query = `
-    query {
-      vaultManagerMetrics {
-        nodes {
-          liquidatingCollateralBrand
-          totalCollateral
-        }
-      }
-      oraclePrices {
-        nodes {
-          typeInAmount
-          typeOutAmount
-          typeInName
-        }
-      }
-      boardAuxes {
-        nodes {
-          allegedName
-          decimalPlaces
-        }
-      }
+const coingeckoMapping = {
+  'stATOM': 'stride-staked-atom',
+  'stkATOM': 'stkatom',
+  'ATOM': 'cosmos',
+  'stOSMO': 'stride-staked-osmo',
+  'stTIA': 'stride-staked-tia',
+}
+const decimals = {}
+
+const tvl = async (api) => {
+  const { data: { vaultManagerMetrics, boardAuxes } } = await post('https://api.subquery.network/sq/agoric-labs/agoric-mainnet-v2', { query })
+
+  boardAuxes.nodes.forEach(board => {
+    decimals[board.allegedName] = 10 ** board.decimalPlaces
+  })
+
+  vaultManagerMetrics.nodes.forEach(vault => {
+    const key = vault.liquidatingCollateralBrand
+    if (!coingeckoMapping[key]) {
+      throw new Error(`Unknown coin ${key} (coingecko mapping is missing)`)
     }
-  `;
-  const data = await queryData(query);
-  let totalTVL = BigInt(0);
-
-  const collaterals = data.vaultManagerMetrics.nodes.reduce((acc, curr) => {
-    acc[curr.liquidatingCollateralBrand] = BigInt(curr.totalCollateral);
-    return acc;
-  }, {});
-
-  const oraclePrices = data.oraclePrices.nodes.reduce((acc, curr) => {
-    const typeOutAmount = BigInt(curr.typeOutAmount ?? '1');
-    const typeInAmount = BigInt(curr.typeInAmount ?? '1');
-    acc[curr.typeInName] = { numerator: typeOutAmount, denominator: typeInAmount };
-    return acc;
-  }, {});
-
-  const decimals = data.boardAuxes.nodes.reduce((acc, curr) => {
-    acc[curr.allegedName] = curr.decimalPlaces;
-    return acc;
-  }, {});
-
-  Object.entries(collaterals).forEach(([brand, collateralValue]) => {
-    const oraclePrice = oraclePrices[brand];
-    const decimalValues = BigInt(10) ** BigInt(decimals[brand] ?? 6);
-    const priceNumerator = oraclePrice.numerator * collateralValue;
-    const priceDenominator = oraclePrice.denominator * decimalValues;
-    totalTVL += priceNumerator / priceDenominator;
-  });
-
-  const balances = {};
-  sdk.util.sumSingleBalance(balances, coinGeckoId, totalTVL);
-  return balances;
+    const decimal = decimals[key] ?? 1e6
+    const balance = vault.totalCollateral / decimal
+    api.addCGToken(coingeckoMapping[key], balance)
+  })
 }
 
 module.exports = {
