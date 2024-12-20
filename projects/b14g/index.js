d0a692e2a929d8131e6a2f137cbdef1a71938454
nguyenquangthang1997 <nguyenquangthang1997@gmail.com>
2024-12-20 15:22:34 +0700

    Feat: update new tvl calculation

--- projects/b14g/index.js
+++ projects/b14g/index.js
@@ -1,19 +1,57 @@
-const EARN_RATE_BASE = 1000000
-const Staking = '0x788CC33E9365d8b4F8dEcEF9AeDf5F5fdcDb664B';
-const Earn = '0xf5fA1728bABc3f8D2a617397faC2696c958C3409';
-const apiStat = 'https://api.b14g.xyz/restake/stats'
+const {getLogs} = require("../helper/cache/getLogs");
+const {sumTokens} = require("../helper/chain/bitcoin");
+const {convertToBigInt} = require("@defillama/sdk/build/generalUtil");
+const btcTxHashLockApi = 'https://api.b14g.xyz/restake/marketplace/defillama/btc-tx-hash'
 
+function reserveBytes(txHashTemp) {
+    let txHash = ''
+    if (txHashTemp.length % 2 === 1) {
+        txHashTemp = '0' + txHashTemp
+    }
+    txHashTemp = txHashTemp.split('').reverse().join('')
+    for (let i = 0; i < txHashTemp.length - 1; i += 2) {
+        txHash += txHashTemp[i + 1] + txHashTemp[i]
+    }
+    return txHash
+}
 
 module.exports = {
     core: {
         tvl: async function tvl(api) {
-            let rate = await api.call({abi: "uint256:getCurrentExchangeRate", target: Earn,})
-            let amount = await api.call({abi: "uint256:totalDelegatedStCore", target: Staking,})
-            const response = await fetch(apiStat);
-            const data = await response.json();
-            let amountBTC = Number(data.totalBtc)
-            api.addGasToken(amount * Number(rate) / EARN_RATE_BASE)
-            api.addCGToken('bitcoin', amountBTC)
+            const logs = await getLogs({
+                api,
+                target: "0x04EA61C431F7934d51fEd2aCb2c5F942213f8967",
+                topics: ['0xd2b1d5b132f5d4708209ccf5c5901620429f83988ea4d678603d1f2d57e2400f'],
+                eventAbi: 'event CreateRewardReceiver(address indexed from, address indexed rewardReceiver, uint256 portion, uint256 time)',
+                onlyArgs: true,
+                fromBlock: 19942300,
+            })
+            const receiverRewards = logs.map(i => i[1])
+
+
+            const coreStakeAmounts = (await api.multiCall({
+                abi: 'function totalCoreStake() external view returns (uint256)',
+                calls: receiverRewards
+            })).flat()
+
+
+            const totalCoreStake = coreStakeAmounts.reduce((acc, cur) => convertToBigInt(cur) + acc, BigInt(0))
+            const response = await fetch(btcTxHashLockApi)
+            const listTxHash = await response.json();
+            let listAddressLock = [];
+            for (let txHash of listTxHash.data.result) {
+                let response = await fetch(`https://mempool.space/api/tx/${reserveBytes(txHash.txHash.slice(2))}`)
+                let tx = await response.json()
+                let vinAddress = tx.vin.map(el => el.prevout.scriptpubkey_address);
+                tx.vout.forEach(el => {
+                    if (el.scriptpubkey_type !== "op_return" && !vinAddress.includes(el.scriptpubkey_address)) {
+                        listAddressLock.push(el.scriptpubkey_address)
+                    }
+                })
+            }
+            let amountBTC = await sumTokens({balances: {}, owners: listAddressLock, timestamp: Date.now()})
+            api.addGasToken(totalCoreStake)
+            api.addCGToken('bitcoin', amountBTC.bitcoin)
         }
     },
 }
