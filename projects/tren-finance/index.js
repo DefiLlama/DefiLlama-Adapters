1b6ebd6c46899577378274fdaab72f4480d7786b
Vickler Charles <charlesvikler@gmail.com>
2024-12-23 17:30:57 +0100

    only display tvl

--- projects/tren-finance/index.js
+++ projects/tren-finance/index.js
@@ -2,7 +2,20 @@ const SSL_CONTRACT = '0xDC4a311f0D852934d9b51C0eAc7c7e13EA1DF11b';
 
 async function tvl(api) {
   const totalSupply = await api.call({
-    abi: 'function totalSupply() view returns (uint256)',
+    abi: {
+      constant: true,
+      inputs: [],
+      name: 'totalSupply',
+      outputs: [
+        {
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
     target: SSL_CONTRACT,
   });
 
