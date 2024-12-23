7a688863b7319d97bf8637daaecd9a164cd35ed5
Max Weng <max.m.weng@gmail.com>
2024-12-23 17:59:15 +0800

    chg: add base support

--- projects/union-finance/index.js
+++ projects/union-finance/index.js
@@ -3,18 +3,23 @@ const sdk = require("@defillama/sdk");
 const config = {
   ethereum: {
     userManager: "0x49c910Ba694789B58F53BFF80633f90B8631c195",
-    DAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
-    uDAI: "0x954F20DF58347b71bbC10c94827bE9EbC8706887",
+    underlying: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    uToken: "0x954F20DF58347b71bbC10c94827bE9EbC8706887",
   },
   arbitrum: {
     userManager: "0xb71F3D4342AaE0b8D531E14D2CF2F45d6e458A5F",
-    DAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
-    uDAI: "0x954F20DF58347b71bbC10c94827bE9EbC8706887",
+    underlying: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    uToken: "0x954F20DF58347b71bbC10c94827bE9EbC8706887",
   },
   optimism: {
     userManager: "0x8E195D65b9932185Fcc76dB5144534e0f3597628",
-    DAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
-    uDAI: "0xE478b5e7A423d7CDb224692d0a816CA146A744b2",
+    underlying: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    uToken: "0xE478b5e7A423d7CDb224692d0a816CA146A744b2",
+  },
+  base: {
+    userManager: "0xfd745A1e2A220C6aC327EC55d2Cb404CD939f56b",
+    underlying: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+    uToken: "0xc2447f36FfdA08E278D25D08Ea91D942f0C2d6ea",
   },
 };
 
@@ -71,7 +76,7 @@ function tvl(chain) {
   return async function (_, __, chainBlocks) {
     const chainBlock = chainBlocks[chain];
 
-    const { userManager, uDAI } = config[chain];
+    const { userManager, uToken } = config[chain];
 
     const totalStaked = (
       await sdk.api.abi.call({
@@ -86,7 +91,7 @@ function tvl(chain) {
     const totalRedeemable = (
       await sdk.api.abi.call({
         abi: abi.totalRedeemable,
-        target: uDAI,
+        target: uToken,
         params: [],
         chain,
         block: chainBlock,
@@ -96,7 +101,7 @@ function tvl(chain) {
     const totalReserves = (
       await sdk.api.abi.call({
         abi: abi.totalReserves,
-        target: uDAI,
+        target: uToken,
         params: [],
         chain,
         block: chainBlock,
@@ -109,7 +114,7 @@ function tvl(chain) {
     );
 
     return {
-      [config[chain].DAI]: total,
+      [config[chain].underlying]: total,
     };
   };
 }
@@ -118,12 +123,12 @@ function borrowing(chain) {
   return async function (_, __, chainBlocks) {
     const chainBlock = chainBlocks[chain];
 
-    const { uDAI } = config[chain];
+    const { uToken } = config[chain];
 
     const totalBorrows = (
       await sdk.api.abi.call({
         abi: abi.totalBorrows,
-        target: uDAI,
+        target: uToken,
         params: [],
         chain,
         block: chainBlock,
@@ -131,7 +136,7 @@ function borrowing(chain) {
     ).output;
 
     return {
-      [config[chain].DAI]: totalBorrows,
+      [config[chain].underlying]: totalBorrows,
     };
   };
 }
@@ -152,5 +157,9 @@ module.exports = {
   optimism: {
     tvl: tvl("optimism"),
     borrowed: borrowing("optimism"),
+  },
+  base: {
+    tvl: tvl("base"),
+    borrowed: borrowing("base"),
   },
 };
