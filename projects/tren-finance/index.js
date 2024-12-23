1afdacf25c14a3d4f32caceeac2c00d6cb74f906
Vickler Charles <charlesvikler@gmail.com>
2024-12-23 20:26:07 +0100

    ssl tvl

--- projects/tren-finance/index.js
+++ projects/tren-finance/index.js
@@ -1,27 +1,53 @@
 const sdk = require('@defillama/sdk');
 
-const XY_CONTRACT = '0xD4fe6e1e37dfCf35E9EEb54D4cca149d1c10239f';
-const XY_ETH_POOL = '0xe9766D6aed0A73255f95ACC1F263156e746B70ba';
-const C_POOL = '0x2b2e23b7c1b0de9040011b860cc575650d0817f7';
+const SSL_CONTRACT_1 = '0xDC4a311f0D852934d9b51C0eAc7c7e13EA1DF11b';
+const SSL_CONTRACT_2 = '0xDFF4a68044eb68c60354810E9316B2B6DB88B3eb';
 
 async function tvl(_, _block, chainBlocks) {
   try {
-    const xyInPool = await sdk.api.erc20.balanceOf({
-      target: XY_CONTRACT,
-      owner: XY_ETH_POOL,
-      chain: 'arbitrum',
-      block: chainBlocks['arbitrum'],
-    });
+    const [totalDepositedAmount1, totalDepositedAmount2] = await Promise.all([
+      sdk.api.abi.call({
+        abi: {
+          "constant": true,
+          "inputs": [],
+          "name": "totalDepositedAmount",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        target: SSL_CONTRACT_1,
+        chain: 'arbitrum',
+        block: chainBlocks['arbitrum'],
+      }),
+      sdk.api.abi.call({
+        abi: {
+          "constant": true,
+          "inputs": [],
+          "name": "totalDepositedAmount",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        target: SSL_CONTRACT_2,
+        chain: 'arbitrum',
+        block: chainBlocks['arbitrum'],
+      })
+    ]);
 
-    const xyInCurvePool = await sdk.api.erc20.balanceOf({
-      target: XY_CONTRACT,
-      owner: C_POOL,
-      chain: 'arbitrum',
-      block: chainBlocks['arbitrum'],
-    });
-
-    const totalTVL = (xyInPool.output / 1e18) + (xyInCurvePool.output / 1e18);
-    return { "arbitrum": totalTVL };
+    const totalTVL = (Number(totalDepositedAmount1.output) + Number(totalDepositedAmount2.output)) / 1e18;
+    return { arbitrum: totalTVL };
   } catch (error) {
     console.error('Error fetching balance:', error);
     return { arbitrum: 0 };
