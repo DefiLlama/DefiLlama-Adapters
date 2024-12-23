9bce639e2a2bed452ed3a17ce93379a68fb0231f
m66260 <m66260@protonmail.com>
2024-12-23 11:01:10 -0500

    add base

--- projects/d8x/index.js
+++ projects/d8x/index.js
@@ -5,6 +5,7 @@ const config = {
   polygon_zkevm: { factory: "0xaB7794EcD2c8e9Decc6B577864b40eBf9204720f" },
   xlayer: { factory: "0xb24dB543749277E8625a59C061aE7574C8235475" },
   arbitrum: { factory: "0x8f8BccE4c180B699F81499005281fA89440D1e95" },
+  base : {factory: "0x7F3A4A9e5BB469F0F4977AA390760aF9EFCCd406"}
 };
 
 async function tvl(api) {
