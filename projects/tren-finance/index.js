81db12a24b93f82ead41d45e320ecf9d7ddc7815
Vickler Charles <charlesvikler@gmail.com>
2024-12-23 19:40:43 +0100

    xy liquidity

--- projects/tren-finance/index.js
+++ projects/tren-finance/index.js
@@ -1,19 +1,33 @@
-const SSL_CONTRACT = '0xDC4a311f0D852934d9b51C0eAc7c7e13EA1DF11b'; 
+const sdk = require('@defillama/sdk');
 
-async function tvl(api) {
-  const totalSupply = await api.call({
-    abi: 'function totalDepositedAmount() view returns (uint256)',
-    target: SSL_CONTRACT,
-    chain: 'arbitrum',
-  });
+const XY_CONTRACT = '0xD4fe6e1e37dfCf35E9EEb54D4cca149d1c10239f';
+const XY_ETH_POOL = '0xe9766D6aed0A73255f95ACC1F263156e746B70ba';
+const C_POOL = '0x2b2e23b7c1b0de9040011b860cc575650d0817f7';
 
-  api.add(SSL_CONTRACT, totalSupply);
+async function tvl(_, _block, chainBlocks) {
+  try {
+    const xyInPool = await sdk.api.erc20.balanceOf({
+      target: XY_CONTRACT,
+      owner: XY_ETH_POOL,
+      chain: 'arbitrum',
+      block: chainBlocks['arbitrum'],
+    });
+
+    const xyInCurvePool = await sdk.api.erc20.balanceOf({
+      target: XY_CONTRACT,
+      owner: C_POOL,
+      chain: 'arbitrum',
+      block: chainBlocks['arbitrum'],
+    });
+
+    const totalTVL = (xyInPool.output / 1e18) + (xyInCurvePool.output / 1e18);
+    return { "arbitrum": totalTVL };
+  } catch (error) {
+    console.error('Error fetching balance:', error);
+    return { arbitrum: 0 };
+  }
 }
 
 module.exports = {
-  methodology: 'We count the total supply of the SSL token on Arbitrum.',
-  start: 1000235,
-  arbitrum: {
-    tvl,
-  },
+  arbitrum: { tvl },
 };
