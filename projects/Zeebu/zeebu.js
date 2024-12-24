d170ec20ccc3a37df2ec9c2301cbf9c6ebd38351
dsarvaiya <114975126+dsarvaiya@users.noreply.github.com>
2024-12-24 17:54:17 +0530

    Update zeebu.js
    miscellaneous methods

--- projects/Zeebu/zeebu.js
+++ projects/Zeebu/zeebu.js
@@ -1,1 +1,39 @@
 
+const ABI_TOTAL_SUPPLY_JSON = JSON.stringify([
+  const sdk = require('@defillama/sdk');
+  
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function",
+  },
+]);
+
+async function fetchStakingDetails(api, chain) {
+  try{
+    //console.log("----",await sdk.api);
+    console.log("chain:",chain);
+
+    const currentBlock = await sdk.api.util.getLatestBlock(chain) ; //await api.getBlock(chain);
+    console.log("currentBlock:",currentBlock);
+
+    const totalStaked = await api.abi.call({target: VOTING_ESCROW_ADDRESSES[chain],chain: chain,block : currentBlock.block, abi : 'uint256:supply'});
+    
+    console.log("temp:",totalStaked);
+
+
+      if (!totalStaked) {
+        console.error(`[Error] totalSupply() returned undefined for ${chain}`);
+        return  0 ;
+      }
+      //return totalStaked;
+      return (parseFloat(totalStaked) / 1e18); // Assuming 18 decimals
+      
+    } catch (error) {
+    console.error(`[Error] Failed to fetch staking details for ${chain}:`, error.message);
+    return 0;
+  }
+}
