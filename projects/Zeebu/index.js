242d4fb2342b54fabc0972852006b9b0526e9d3d
dsarvaiya <114975126+dsarvaiya@users.noreply.github.com>
2024-12-26 17:48:15 +0530

    Update index.js
    update pool2 consideration as per defillama team

--- projects/Zeebu/index.js
+++ projects/Zeebu/index.js
@@ -108,13 +108,14 @@ module.exports = {
   base: {
    staking :  stakings([VOTING_ESCROW_ADDRESSES["base"]],ZBU_ADDRESSES["base"]),
     tvl: () => ({}),
-    pool2: async (_, _1, _2, { api }) =>
+    pool2: pool2s(stackingcontract,lpTokens),
+    /*pool2: async (_, _1, _2, { api }) =>
       calculatePool2Tvl({
         api,
         pools: [...POOLS.base.balancer, ...POOLS.base.uniswap, ...POOLS.base.pancakeswap],
         tokens: [ZBU_ADDRESSES.base],
         owner: BALANCER_VAULT_ADDRESS,
-      }),
+      }),*/
     
   },
   bsc: {
