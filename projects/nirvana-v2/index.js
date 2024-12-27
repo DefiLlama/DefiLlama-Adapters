3131917c66970f6dfee682ea338ddbb47a8d0029
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-12-27 17:17:19 +0100

    code refactor

--- projects/nirvana-v2/index.js
+++ projects/nirvana-v2/index.js
@@ -1,44 +1,12 @@
-const axios = require("axios");
-const NIRVANA_API =
-  "https://json-api-prod-548644909672.us-west2.run.app/api/tenants/BcAoCEdkzV2J21gAjCCEokBw5iMnAe96SbYo9F6QmKWV";
+const { sumTokens2 } = require('../helper/solana')
 
-const client = axios.create({
-  baseURL: NIRVANA_API,
-});
-
-async function getMetrics() {
-  try {
-    const response = await client.get("/llama-stats");
-    return {
-      tvlUsd: Number(response.data.tvlUsd),
-      anaStakedUsd: Number(response.data.anaStakedUsd),
-    };
-  } catch (error) {
-    console.error("Error fetching metrics:", error);
-    return { tvlUsd: 0, anaStakedUsd: 0 };
-  }
-}
-
-async function tvl() {
-  const metrics = await getMetrics();
-  return {
-    "usd-coin": metrics.tvlUsd,
-  };
-}
-
-async function staking() {
-  const metrics = await getMetrics();
-  return {
-    "usd-coin": metrics.anaStakedUsd,
-  };
+async function tvl(api) {
+  return sumTokens2({ tokenAccounts: ['FhTJEGXVwj4M6NQ1tPu9jgDZUXWQ9w2hP89ebZHwrJPS'] })
 }
 
 module.exports = {
   timetravel: false,
-  methodology:
-    "TVL - The total value of the Nirvana v2 reserve.  Staking - The total value of ANA tokens locked in the protocol as staking or collateral.",
   solana: {
     tvl,
-    staking,
-  },
-};
+  }
+}
