add17a8daacdd28540661d9c9a8ebc874a5ecaa9
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2024-12-30 20:54:00 -0500

    equalizer sonic

--- projects/equalizer-exchange-sonic/index.js
+++ projects/equalizer-exchange-sonic/index.js
@@ -3,7 +3,7 @@ const { staking } = require('../helper/staking')
 
 module.exports = {
   misrepresentedTokens: true,
-  base:{
+  sonic:{
     tvl: uniTvlExport("0xDDD9845Ba0D8f38d3045f804f67A1a8B9A528FcC", "sonic", undefined, undefined, { hasStablePools: true, useDefaultCoreAssets: true,  }),
     staking: staking("0x3045119766352fF250b3d45312Bd0973CBF7235a", "0xddF26B42C1d903De8962d3F79a74a501420d5F19"),
   },
