0d2b515498075014b0420738b74d04e4fcaa0ecb
xblackbeard <0xblackbeard@protonmail.com>
2024-12-31 22:19:12 -0800

    add blacksail finance tvl adapter

---
+++ projects/blacksail/index.js
@@ -0,0 +1,24 @@
+const utils = require('../helper/utils');
+let _response
+
+
+function fetchChain() {
+    return async () => {
+        if (!_response) _response = utils.fetchURL('https://api.blacksail.finance/stats')
+        const response = await _response;
+
+        let total_tvl = 0;
+        vaults = response["data"]["yield"]
+
+        for (const vault in vaults) {
+            total_tvl += vault["total_usd_staked_in_vault"]
+        }
+        return total_tvl
+    }
+}
+
+module.exports = {
+    sonic: {
+        tvl: fetchChain()
+    }
+}
