bd7913aaba28e89b2aa4070924af72c44d5798ab
0xajing <jing@lorenzo-protocol.xyz>
2025-01-04 23:53:21 +0800

    feat: lorenzo new layout

--- projects/lorenzo-stbtc/index.js
+++ projects/lorenzo-stbtc/index.js
@@ -1,10 +1,8 @@
 const { sumTokensExport } = require("../helper/sumTokens");
 const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
 
-// Lorenzo Protocol Tota TVL
-const allBtcAddressList = [...bitcoinAddressBook.lorenzo, ...bitcoinAddressBook.lorenzo2]
 module.exports = {
   methodology: "Lorenzo, As the Bitcoin Liquidity Finance Layer",
   doublecounted:true,
-  bitcoin: { tvl: sumTokensExport({ owners : allBtcAddressList }) }
+  bitcoin: { tvl: sumTokensExport({ owners : bitcoinAddressBook.lorenzo }) }
 };
