f6f1acfa4e5b59300fa61c76328f3ba546dd07f7
0xvoidmain <hoangtung.utc@gmail.com>
2025-01-06 11:03:57 +0700

    add Staking A8

--- projects/ancient8-bridge/index.js
+++ projects/ancient8-bridge/index.js
@@ -6,8 +6,6 @@ module.exports = {
             const ethApi = new sdk.ChainApi({ chain: 'ethereum' })
             return await ethApi.sumTokens({ 
                 owners: [
-                    '0x12d4e64e1b46d27a00fe392653a894c1dd36fb80', //l1 bridge
-                    '0x639F2AECE398Aa76b07e59eF6abe2cFe32bacb68', //l1 eth holder
                     '0xd42D4417D7f757BAD6A5EA5E40a53649fD50B349' //staking
                 ], 
                 tokens: [
