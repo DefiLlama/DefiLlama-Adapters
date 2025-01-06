4e88024edd8ad72097e12cc755de2f03ad9b56ff
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-06 12:34:40 +0100

    code refactor

--- projects/satlayer/index.js
+++ projects/satlayer/index.js
@@ -1,50 +1,26 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 
-function computeStaked({
-                         owner = "0x42a856dbEBB97AbC1269EAB32f3bb40C15102819",
-                         tokens
-                       }) {
-  return async function tvl(api) {
-    return api.sumTokens({
-      owner: owner,
-      tokens: tokens
-    })
-  }
-}
+
+const { getLogs2 } = require('../helper/cache/getLogs')
+const { getUniqueAddresses } = require('../helper/tokenMapping')
+const factory = '0x42a856dbEBB97AbC1269EAB32f3bb40C15102819'
 
 module.exports = {
-  doublecounted: true,
-  methodology: 'Total amount of BTC staked and restaked on SatLayer.',
-  ethereum: {
-    tvl: computeStaked({
-      tokens: [
-        "0xC96dE26018A54D51c097160568752c4E3BD6C364", // FBTC
-        ADDRESSES.ethereum.WBTC,
-        "0x004E9C3EF86bc1ca1f0bB5C7662861Ee93350568", // uniBTC
-        "0x8236a87084f8B84306f72007F36F2618A5634494", // LBTC
-        "0xd9D920AA40f578ab794426F5C90F6C731D159DEf", // SolvBTC.BBN
-        "0xF469fBD2abcd6B9de8E169d128226C0Fc90a012e", // PumpBTC
-        "0xf6718b2701D4a6498eF77D7c152b2137Ab28b8A3", // stBTC
-      ]
-    }),
-  },
-  bsc: {
-    tvl: computeStaked({
-      tokens: [
-        ADDRESSES.bsc.BTCB,
-        "0xf6718b2701D4a6498eF77D7c152b2137Ab28b8A3", // stBTC
-        "0x1346b618dC92810EC74163e4c27004c921D446a5", // SolvBTC.BBN
-      ]
-    }),
-  },
-  btr: {
-    tvl: computeStaked({
-      owner: "0x2E3c78576735802eD94e52B7e71830e9E44a9a1C",
-      tokens: [
-        "0xf6718b2701d4a6498ef77d7c152b2137ab28b8a3", // stBTC
-        "0x93919784c523f39cacaa98ee0a9d96c3f32b593e", // uniBTC
-        ADDRESSES.btr.WBTC,
-      ]
-    }),
+  methodology: 'Total amount of BTC staked and restaked on SatLayer.'
+}
+
+const config = {
+  ethereum: { factory, fromBlock: 20564864 },
+  bsc: { factory, fromBlock: 42094094 },
+  btr: { factory: "0x2E3c78576735802eD94e52B7e71830e9E44a9a1C", fromBlock: 4532898 },
+}
+
+Object.keys(config).forEach(chain => {
+  const { factory, fromBlock } = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const logs = await getLogs2({ api, factory, eventAbi: "event CapChanged(address token, uint256 cap)", fromBlock, })
+      const tokens = getUniqueAddresses(logs.map(log => log.token))
+      return api.sumTokens({ owner: factory, tokens })
+    }
   }
-};
+})
