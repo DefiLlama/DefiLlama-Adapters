a3bd1a3abf9631bdf1db7e43c774fed8b40572db
fromstupig <tnphong.tech@gmail.com>
2025-01-06 22:12:00 +0700

    feat: add dLEND

---
+++ projects/dlend/index.js
@@ -0,0 +1,10 @@
+const { dLendTvl, dLendBorrowed } = require('../dtrinity/helper');
+
+module.exports = {
+  fraxtal: {
+    tvl: dLendTvl,
+    borrowed: dLendBorrowed,
+  },
+  methodology: "Includes total supplied liquidity plus total debt.",
+  hallmarks: []
+}
