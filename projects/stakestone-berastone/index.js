a01fdae690fcb99c354fc5809cb0a970158d1315
xg <xg@stakestone.io>
2025-01-07 22:04:42 +0800

    add StakeStone SBTCBeraVault

--- projects/stakestone-berastone/index.js
+++ projects/stakestone-berastone/index.js
@@ -1,3 +1,4 @@
+const sdk = require('@defillama/sdk')
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const vaultABI = {
@@ -5,19 +6,25 @@ const vaultABI = {
   "assetsBorrowed": "function assetsBorrowed() external view returns (uint256)"
 }
 
-const Vault = '0x8f88aE3798E8fF3D0e0DE7465A0863C9bbB577f0';
+const ETHVault = '0x8f88aE3798E8fF3D0e0DE7465A0863C9bbB577f0';
+const BTCVault = '0xf401Cc9f467c7046796D9A8b44b0c1348b4DEec7';
 
-const Tvl = async (api) => {
-  const usedTVL = await api.call({ abi: vaultABI.assetsBorrowed, target: Vault })
+const ethTvl = async (api) => {
+  const usedTVL = await api.call({ abi: vaultABI.assetsBorrowed, target: ETHVault })
   api.add(ADDRESSES.ethereum.WETH, usedTVL);
 
-  const underlyings = await api.call({ abi: vaultABI.getUnderlyings, target: Vault })
-  return api.sumTokens({ owner: Vault, tokens: underlyings })
+  const underlyings = await api.call({ abi: vaultABI.getUnderlyings, target: ETHVault })
+  return api.sumTokens({ owner: ETHVault, tokens: underlyings })
+}
+
+const btcTvl = async (api) => {
+  const underlyings = await api.call({ abi: vaultABI.getUnderlyings, target: BTCVault })
+  return api.sumTokens({ owner: BTCVault, tokens: underlyings })
 }
 
 module.exports = {
   doublecounted: true,
   ethereum: {
-    tvl: Tvl,
+    tvl: sdk.util.sumChainTvls([ethTvl, btcTvl]),
   }
 }
