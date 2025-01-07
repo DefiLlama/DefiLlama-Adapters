4aa4d94fd4a649d0a80490f80ad292af335b0418
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-07 10:14:01 +0100

    Update index.js

--- projects/goldstation-dex-v3/index.js
+++ projects/goldstation-dex-v3/index.js
@@ -1,10 +1,6 @@
 const { uniV3Export } = require("../helper/uniswapV3");
 
 
-module.exports= uniV3Export({
-  avax: {
-    factory: "0xF72f4652785a5186EDF7b93a9cfd246FeFc0ef5c",
-    fromBlock: 52760680,
-    isAlgebra: false,
-  },
+module.exports = uniV3Export({
+  avax: { factory: "0xF72f4652785a5186EDF7b93a9cfd246FeFc0ef5c", fromBlock: 52760680, },
 })
