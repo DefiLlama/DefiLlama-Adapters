35db36d36a32643fbab1f94cfe9859ba690db685
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-08 10:33:53 +0100

    bug fixes

--- projects/helper/chain/bittensor.js
+++ projects/helper/chain/bittensor.js
@@ -27,7 +27,6 @@ async function sumTokens({ balances = {}, owners = [] }) {
   let total = 0
   for (const owner of owners) {
     const balance = await getBalance(owner)
-    console.log(owner, balance)
     total += balance
   }
   sdk.util.sumSingleBalance(balances, 'bittensor', total)
--- projects/verus-market/index.js
+++ projects/verus-market/index.js
@@ -31,7 +31,6 @@ async function tvl(api) {
     }
   })
 
-  console.log(data.length)
   return transformDexBalances({ data, api, })
 }
 
