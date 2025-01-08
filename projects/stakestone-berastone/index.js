b971bb60d6768d1975d629acec203c17bc23da03
stakestone-scdev <xg@stakestone.io>
2025-01-08 15:44:24 +0800

    update StakeStone Berachain Vault (#13005)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/stakestone-berastone/index.js
+++ projects/stakestone-berastone/index.js
@@ -5,19 +5,22 @@ const vaultABI = {
   "assetsBorrowed": "function assetsBorrowed() external view returns (uint256)"
 }
 
-const Vault = '0x8f88aE3798E8fF3D0e0DE7465A0863C9bbB577f0';
+const ETHVault = '0x8f88aE3798E8fF3D0e0DE7465A0863C9bbB577f0';
+const BTCVault = '0xf401Cc9f467c7046796D9A8b44b0c1348b4DEec7';
 
-const Tvl = async (api) => {
-  const usedTVL = await api.call({ abi: vaultABI.assetsBorrowed, target: Vault })
-  api.add(ADDRESSES.ethereum.WETH, usedTVL);
+const tvl = async (api) => {
+  // to include assets moved to boyco vault
+  const usedTVL = await api.call({ abi: vaultABI.assetsBorrowed, target: ETHVault })
+  api.add(ADDRESSES.ethereum.STONE, usedTVL);
 
-  const underlyings = await api.call({ abi: vaultABI.getUnderlyings, target: Vault })
-  return api.sumTokens({ owner: Vault, tokens: underlyings })
+  const vaults = [ETHVault, BTCVault];
+  const tokens = await api.multiCall({  abi: vaultABI.getUnderlyings, calls: vaults})
+  return api.sumTokens({ ownerTokens: tokens.map((t, i) => [t, vaults[i]]) })
 }
 
 module.exports = {
   doublecounted: true,
   ethereum: {
-    tvl: Tvl,
+    tvl,
   }
 }
