f2c2934cee271edd67281ffd6a8f20cabebf36b7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-09 22:29:16 +0100

    code refactor

--- projects/cvex/index.js
+++ projects/cvex/index.js
@@ -1,20 +1,11 @@
-const USDC_TOKEN = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831'; // USDC Token Address
+const { sumTokensExport } = require('../helper/unwrapLPs')
+const ADDRESSES = require('../helper/coreAssets.json')
+
 const MAIN_CONTRACT = '0xBe14D34ce8737614331cE1904AA659E26657eE85'; // Main Contract Address (Proxy)
-
-async function tvl(api) {
-  const usdcBalance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: USDC_TOKEN,
-    params: [MAIN_CONTRACT],
-  });
-
-  api.add(USDC_TOKEN, usdcBalance);
-}
 
 module.exports = {
   methodology: 'counts the USDC balance in the main contract on Arbitrum.',
-  start: 292972866, // Replace with the actual start block for Arbitrum
   arbitrum: {
-    tvl,
+    tvl: sumTokensExport({ owner: MAIN_CONTRACT, tokens: [ADDRESSES.arbitrum.USDC_CIRCLE] }),
   },
-};
+}
