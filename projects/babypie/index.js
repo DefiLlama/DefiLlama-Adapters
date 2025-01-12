99cc32cc1722fc221dbf5622a061cfd64e2d4552
jan-magpie <155635960+jan-magpie@users.noreply.github.com>
2025-01-12 12:23:43 +0000

    changing into own tokes as the dex prices are available

--- projects/babypie/index.js
+++ projects/babypie/index.js
@@ -6,17 +6,13 @@ const sdk = require('@defillama/sdk')
 
 
 async function tvl(api) {
-  const { mbtc,wbtc } = config[api.chain];
+  const { mbtc } = config[api.chain];
 
-  // getting total supply of pegged tokesn
+  
    tokenSupply = await api.call({ abi: 'uint256:totalSupply', target: mbtc });
-  //when chain is bsc the decimals of pegged token is 18, but btcb is 8
-  if (api.chain === "bsc") {
-    tokenSupply = tokenSupply * Math.pow(10, 10);
-  }
-    
 
-  api.add(wbtc, tokenSupply);
+
+  api.add(mbtc, tokenSupply);
 }
 
 module.exports = {
