0d0b3412dbce8d6a3c7d98453440fb554d15b2fb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-13 17:11:47 +0100

    split adapter

---
+++ projects/equalizer-sonic/index.js
@@ -0,0 +1,10 @@
+const {uniTvlExport} = require('../helper/calculateUniTvl.js')
+const { staking } = require('../helper/staking')
+
+module.exports = {
+  misrepresentedTokens: true,
+  sonic: {
+    tvl: uniTvlExport("0xDDD9845Ba0D8f38d3045f804f67A1a8B9A528FcC", "fantom", undefined, undefined, { hasStablePools: true, useDefaultCoreAssets: true, stablePoolSymbol: 's-'}),
+    staking: staking("0x3045119766352fF250b3d45312Bd0973CBF7235a", "0xddF26B42C1d903De8962d3F79a74a501420d5F19"),
+  }
+}
