ceae329a6d65ba7e302cd77a71835f6ae3a0b1e9
personal <swordholder@kinza.finance>
2025-01-14 15:27:39 +0800

    add supported networks

--- projects/kinza/index.js
+++ projects/kinza/index.js
@@ -6,4 +6,13 @@ module.exports = {
   bsc: {
     ...aaveExports('bsc', '0x37D7Eb561E189895E5c8601Cd03EEAB67C269189'),
   },
+  opbnb: {
+    ...aaveExports('opbnb', '0xcf46F77cD75a17900d59676fBe4B88aAdcBA9533'),
+  },
+  mantle: {
+    ...aaveExports('mantle', '0xad48812a9d81aCf8De5bfc93c7d6d7165920aBc2'),
+  },
+  ethereum: {
+    ...aaveExports('ethereum', '0x37c9E6eEAbE799878FF9d32984A3a0b91243cbC6'),
+  },
 };
