1478efb176fb45512a50cb67d2b5c4210f2a7a88
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-14 08:24:12 +0100

    code refactor

--- projects/sophon-farm/index.js
+++ projects/sophon-farm/index.js
@@ -1,15 +1,14 @@
-const { sumTokens2 } = require('../helper/unwrapLPs')
-
-const contract = '0x5753fBeC29De6E2b56F73f7d7786e9f0d34897bb'
-
-async function tvl(api) {
-  return sumTokens2({ api, owner: contract, fetchCoValentTokens: true })
-}
+const { sumTokens2 } = require("../helper/unwrapLPs")
 
 module.exports = {
   sophon: {
     tvl,
-    start: 810461,
   },
-  methodology: 'Counts all ERC20 token balances in the Sophon Farm contract'
+}
+
+async function tvl(api) {
+  const farm = '0x5753fBeC29De6E2b56F73f7d7786e9f0d34897bb'
+  const pools = await api.call({  abi: "function getPoolInfo() view returns ((address lpToken, address l2Farm, uint256 amount, uint256 boostAmount, uint256 depositAmount, uint256 allocPoint, uint256 lastRewardBlock, uint256 accPointsPerShare, uint256 totalRewards, string description)[] poolInfos)", target: farm})
+  const tokens = pools.map(i => i.lpToken)
+  return sumTokens2({ owner: farm, tokens, api, })
 }
