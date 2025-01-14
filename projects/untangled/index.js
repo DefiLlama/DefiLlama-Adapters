a71fbeb52a746ef629d3ba8d9f37886f350059e6
tanpx <tanithust1202@gmail.com>
2025-01-14 16:39:19 +0700

    update untangled tvl

--- projects/untangled/index.js
+++ projects/untangled/index.js
@@ -1,23 +1,17 @@
 const Contracts = {
-  SecuritizationManager: "0x4DCC7a839CE7e952Cd90d03d65C70B9CCD6BA4C2",
+  Vault: "0x2A68c98bD43AA24331396F29166aeF2Bfd51343f",
   USDC: "0xcebA9300f2b948710d2653dD7B07f33A8B32118C",
-  SecuritizationPoolValueService: "0x8dA445046281e6FBf2aBAd56952331B9D5Ea8d45"
+  Treasury: "0x53B4dBdbc9861A626C62DE557ED4f1560FFa3822"
 };
 
 async function tvl(api) {
-  const pools = await api.fetchList({  lengthAbi: 'getPoolsLength', itemAbi: 'pools', target: Contracts.SecuritizationManager})
-  const reserves = await api.multiCall({  abi: 'function getReserves() external view returns (uint256, uint256)', calls: pools })
-  api.add(Contracts.USDC, reserves.map(i => i[1]))
+  const reserves = await api.call({  abi: 'function totalAssets() external view returns (uint256)', target: Contracts.Vault })
+  api.add(Contracts.USDC, reserves)
 }
 
-async function borrowed(api) {
-  const pools = await api.fetchList({  lengthAbi: 'getPoolsLength', itemAbi: 'pools', target: Contracts.SecuritizationManager})
-  const poolDebt = await api.multiCall({target: Contracts.SecuritizationPoolValueService, abi: "function getExpectedAssetsValue(address poolAddress) external view returns (uint256)", calls:pools})
-  api.add(Contracts.USDC, poolDebt)
-}
 
 module.exports = {
   celo: {
-    tvl, borrowed,
+    tvl,
   },
 };
