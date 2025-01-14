a45fb664b4205bc49fac571c980f563a87002ba0
EG <37511853+Lefgk@users.noreply.github.com>
2025-01-14 14:54:13 +0000

    owners

--- projects/nexion/index.js
+++ projects/nexion/index.js
@@ -41,13 +41,17 @@ const COLLATERALS = {
 module.exports = {
   methodology: `BuynBurn holds PLS from user deposits. Farms hold PLS-DAI LP from user deposits`,
   pulse: {
-    tvl :sumTokensExport({ owner: [contracts.NEONBuynBurn,contracts.NEONFarm,contracts.OLDNEONFarm],tokens: [COLLATERALS.DAI, COLLATERALS.WPLS,nullAddress],    useDefaultCoreAssets: true,
-      lps: [COLLATERALS.DAIPLS_LP]}),
-    //  staking: sumTokensExport({
+    tvl :sumTokensExport({ owners: [contracts.NEONBuynBurn,contracts.NEONFarm,contracts.OLDNEONFarm],
+      tokens: [COLLATERALS.DAI, COLLATERALS.WPLS,nullAddress],
+      useDefaultCoreAssets: true,
+      lps: [COLLATERALS.DAIPLS_LP]})
+    
+  },
+};
+
+//  staking: sumTokensExport({
     //         owners:[contracts.NEONFarm,contracts.OLDNEONFarm],
     //         tokens: [COLLATERALS.DAI, COLLATERALS.WPLS],
     //         useDefaultCoreAssets: true,
     //         lps: [COLLATERALS.DAIPLS_LP],
     //       }),
-  },
-};
