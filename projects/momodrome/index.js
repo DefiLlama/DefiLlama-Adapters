d00ec57d78d2db7c2bb37782f552ec3b9c5c0001
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2025-01-16 12:09:07 +0100

    fix + small refacto

--- projects/momodrome/index.js
+++ projects/momodrome/index.js
@@ -1,38 +1,50 @@
-const { getLogs } = require('../helper/cache/getLogs')
-const ADDRESSES = require('../helper/coreAssets.json')
+const { getLogs2 } = require('../helper/cache/getLogs')
 
-const config = {
-  morph: { dvmFactory: '0x6694eebf40924e04c952EA8F1626d19E7a656Bb7', fromBlock: 193478, dspFactory: '0xd0de7cA3298fff085E2cb82F8a861a0254256BA0', gspFactory: '0x2235bB894b7600F1a370fc595Ee5477999A30441', dppFactory: '0x297A4885a7da4AaeF340FABEd119e7a6E3f2BCe8' },
- }
-
-Object.keys(config).forEach(chain => {
-  const { dvmFactory, fromBlock, dspFactory, gspFactory, dppFactory, blacklistedTokens, } = config[chain]
-  module.exports[chain] = {
-    tvl: async (api) => {
-      const ownerTokens = []
-      const funcs = [];
-      const builder = (factorys, event) => {
-        if (Array.isArray(factorys)) {
-          factorys.forEach(factory => funcs.push(addLogs(factory, event)));
-        } else {
-          funcs.push(addLogs(factorys, event));
-        }
-      }
-      builder(dvmFactory, 'event NewDVM (address baseToken, address quoteToken, address creator, address pool)');
-      builder(dspFactory, 'event NewDSP(address baseToken, address quoteToken, address creator, address pool)');
-      builder(gspFactory, 'event NewGSP(address baseToken, address quoteToken, address creator, address pool)');
-      builder(dppFactory, 'event NewDPP (address baseToken, address quoteToken, address creator, address pool)');
-
-      await Promise.all(funcs)
-
-      return api.sumTokens({ ownerTokens, blacklistedTokens, permitFailure: true, })
-
-      async function addLogs(target, eventAbi) {
-        if (!target) return;
-        const convert = i => [[i.baseToken, i.quoteToken], i.pool]
-        let logs = await getLogs({ api, target, eventAbi, onlyArgs: true, fromBlock, });
-        ownerTokens.push(...logs.map(convert))
-      }
-    }
+const CONFIG = {
+  dvm: {
+    factory: '0x6694eebf40924e04c952EA8F1626d19E7a656Bb7',
+    eventAbi: 'event NewDVM (address baseToken, address quoteToken, address creator, address pool)',
+    fromBlock: 193478
+  },
+  dsp: {
+    factory: '0xd0de7cA3298fff085E2cb82F8a861a0254256BA0',
+    eventAbi: 'event NewDSP(address baseToken, address quoteToken, address creator, address pool)',
+    fromBlock: 193478
+  },
+  gsp: {
+    factory: '0x2235bB894b7600F1a370fc595Ee5477999A30441',
+    eventAbi: 'event NewGSP(address baseToken, address quoteToken, address creator, address pool)',
+    fromBlock: 193478
+  },
+  dpp: {
+    factory: '0x297A4885a7da4AaeF340FABEd119e7a6E3f2BCe8',
+    eventAbi: 'event NewDPP (address baseToken, address quoteToken, address creator, address pool)',
+    fromBlock: 193478
   }
-})
+}
+
+const getMomoLogs = async (api, configKey, config) => {
+  const { factory: target, eventAbi, fromBlock } = config
+  const extrakey = `momo-${api.chain}-${configKey}`
+  const logs = await getLogs2({ api, extrakey, target, eventAbi, fromBlock, onlyArgs: true })
+  if (!logs.length) return [];
+  return logs.map(log => [[log.baseToken, log.quoteToken], log.pool]);
+}
+
+const tvl = async (api) => {
+  const { dvm, dsp, gsp, dpp} = CONFIG
+
+  const tokens = await Promise.all([
+    getMomoLogs(api, 'dvm', dvm),
+    getMomoLogs(api, 'dsp', dsp),
+    getMomoLogs(api, 'gsp', gsp),
+    getMomoLogs(api, 'dpp', dpp),
+  ])
+
+  const ownerTokens = tokens.filter(tokens => tokens.length > 0).flat()
+  return api.sumTokens({ ownerTokens, permitFailure: true });
+}
+
+module.exports = {
+  morph: { tvl }
+}
