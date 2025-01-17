c1f68d3f7dd4aa9e9f66ec9bcb01e2f92efb5583
Alp Güneysel <alp.guneysel@gmail.com>
2025-01-16 19:56:58 -0500

    [PDE-657] add usdc/usdt to nelixir vault (#13127)

--- projects/nest-staking/index.js
+++ projects/nest-staking/index.js
@@ -7,13 +7,16 @@ const nELIXIR = "0x9fbC367B9Bb966a2A537989817A088AFCaFFDC4c"
 const deUSD = "0x15700B564Ca08D9439C58cA5053166E8317aa138"
 const sdeUSD = "0x5C5b196aBE0d54485975D1Ec29617D42D9198326"
 
+const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+const USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+
 async function tvl(api) {
   // Get allowed stablecoins from RWA Staking
   const stablecoins = await api.call({    target: RWA_STAKING,    abi: 'address[]:getAllowedStablecoins',  })
   const ownerTokens = [
     [stablecoins, RWA_STAKING], 
     [[SBTC, STONE], RESERVE_STAKING],
-    [[deUSD, sdeUSD], nELIXIR],
+    [[deUSD, sdeUSD, USDC, USDT], nELIXIR],
   ]
   return api.sumTokens({ ownerTokens})
 }
