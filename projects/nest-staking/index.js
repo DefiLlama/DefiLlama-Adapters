a3c3845f6166190a1923f7d5f2fc29842c49cdc2
Alp Güneysel <alp.guneysel@gmail.com>
2025-01-18 05:29:17 -0500

    add inElixir to vaults (#13156)

--- projects/nest-staking/index.js
+++ projects/nest-staking/index.js
@@ -5,6 +5,10 @@ const SBTC = "0x094c0e36210634c3CfA25DC11B96b562E0b07624"
 const STONE = "0x7122985656e38bdc0302db86685bb972b145bd3c"
 
 const nELIXIR = "0x9fbC367B9Bb966a2A537989817A088AFCaFFDC4c"
+const inELIXIR = "0xd3bfd6e6187444170a1674c494e55171587b5641"
+
+
+
 const deUSD = "0x15700B564Ca08D9439C58cA5053166E8317aa138"
 const sdeUSD = "0x5C5b196aBE0d54485975D1Ec29617D42D9198326"
 
@@ -18,6 +22,7 @@ async function tvl(api) {
     [stablecoins, RWA_STAKING], 
     [[SBTC, STONE], RESERVE_STAKING],
     [[deUSD, sdeUSD, USDC, USDT], nELIXIR],
+    [[deUSD, sdeUSD, USDC, USDT], inELIXIR],
   ]
   return api.sumTokens({ ownerTokens})
 }
