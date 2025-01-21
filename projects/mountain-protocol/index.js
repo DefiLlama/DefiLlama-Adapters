05d6ef7dcb5c5224e90cc9559f4c7c272f7fd9b5
Guido Peirano <guidopeirano316@gmail.com>
2025-01-21 15:02:25 -0300

    Adding zkSync

--- projects/mountain-protocol/index.js
+++ projects/mountain-protocol/index.js
@@ -1,38 +1,48 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const MOUNTAIN_PROTOCOL_CONTRACT = ADDRESSES.ethereum.USDM;
+const ZKSYNC_MOUNTAIN_PROTOCOL_CONTRACT = ADDRESSES.zksync.USDM;
 
-async function tvl(api) {
-  const totalSupply = await api.call({
-    abi: "erc20:totalSupply",
-    target: MOUNTAIN_PROTOCOL_CONTRACT,
-  });
-
-  const decimals = await api.call({
-    abi: "erc20:decimals",
-    target: MOUNTAIN_PROTOCOL_CONTRACT,
-  });
-
-  return {
-    "usd-coin": totalSupply / 10 ** decimals,
-  };
+function chainTvl(chain){
+  const contractAddress = chain == "zksync" ? ZKSYNC_MOUNTAIN_PROTOCOL_CONTRACT : MOUNTAIN_PROTOCOL_CONTRACT;
+  return async (api) => {
+    const totalSupply = await api.call({
+      abi: "erc20:totalSupply",
+      target: contractAddress,
+    });
+  
+    const decimals = await api.call({
+      abi: "erc20:decimals",
+      target: contractAddress,
+    });
+  
+    return {
+      "usd-coin": totalSupply / 10 ** decimals,
+    };
+  }
 }
 
 module.exports = {
   misrepresentedTokens: true,
   methodology: "Calculates the total USDM Supply",
   ethereum: {
-    tvl,
+    tvl: chainTvl("ethereum"),
   },
   polygon: {
-    tvl,
+    tvl: chainTvl("polygon"),
   },
   optimism: {
-    tvl,
+    tvl: chainTvl("optimism"),
   },
   base: {
-    tvl,
+    tvl: chainTvl("base"),
   },
   arbitrum: {
-    tvl,
+    tvl: chainTvl("arbitrum"),
+  },
+  zksync: {
+    tvl: chainTvl("zksync"),
+  },
+  celo: {
+    tvl: chainTvl("celo"),
   },
 };
