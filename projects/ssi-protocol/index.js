dbe171dfc663552960bb6998207e905c722f4024
terrytoo <terrytong7717@gmail.com>
2025-01-21 10:13:49 +0800

    treasure & ssi

--- projects/ssi-protocol/index.js
+++ projects/ssi-protocol/index.js
@@ -1,6 +1,25 @@
 const { treasuryExports } = require("../helper/treasury");
-const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
+const bitcoinAddressBook = require('../helper/bitcoin-book/index.js');
 
+const abi = {
+    "getAssetIDs": "function getAssetIDs() view returns (uint256[])",
+    "assetTokens": "function assetTokens(uint256 assetID) view returns (address)"
+}
+
+const ASSET_FACTORY_CONTRACT = '0xb04eB6b64137d1673D46731C8f84718092c50B0D';
+const USSI_CONTRACT = '0x3a46ed8FCeb6eF1ADA2E4600A522AE7e24D2Ed18';
+
+async function tvl(api) {
+  const assetIDs = [19];
+  const tokens = await api.batchCall(
+    assetIDs.map(assetID => {
+      return { abi: abi.assetTokens, target: ASSET_FACTORY_CONTRACT, params: [assetID] };
+    })
+  );
+  tokens.push(USSI_CONTRACT);
+  const balances = await api.multiCall({ abi: 'erc20:totalSupply', calls: tokens });
+  api.add(tokens, balances);
+}
 
 // https://github.com/DefiLlama/DefiLlama-Adapters/pull/12893#issuecomment-2577690389s
 module.exports = treasuryExports({
@@ -11,6 +30,15 @@ module.exports = treasuryExports({
   bitcoin: { owners: bitcoinAddressBook.ssiProtocol },
   cardano: { owners: ['addr1v9nkv9p0gz83ha7hx0h6pg6lrte0t0dsj8tyersc8np5gegrwwxpp'] },
   ripple: { owners: ['rp53vxWXuEe9LL6AHcCtzzAvdtynSL1aVM'] },
-})
+});
 
-Object.keys(module.exports).forEach(chain => delete module.exports[chain].ownTokens)
+Object.keys(module.exports).forEach(chain => delete module.exports[chain].ownTokens);
+
+module.exports = {
+  ...module.exports,
+  methodology: 'TVL counts the SSI tokens and USSI token minted.',
+  start: 23863972,
+  base: {
+    tvl,
+  }
+};
