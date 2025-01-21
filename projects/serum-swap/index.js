d1373de6af9a132c90159466e8121f58cce46246
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-21 13:40:32 +0100

    optimize sol adapters

--- projects/serum-swap/index.js
+++ projects/serum-swap/index.js
@@ -2,6 +2,7 @@ const { exportDexTVL } = require('../helper/solana')
 
 module.exports = {
   timetravel: false,
+  isHeavyProtocol: true,
   solana: {
     tvl: exportDexTVL('SwaPpA9LAaLfeLi3a68M4DjnLqgtticKg6CnyNwgAC8')
   }
