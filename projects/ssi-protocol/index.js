9ef12528cdb27443bcd9d377c9e7ba978e85437a
terrytoo <terrytong7717@gmail.com>
2025-01-21 19:11:44 +0800

    del ussi

--- projects/ssi-protocol/index.js
+++ projects/ssi-protocol/index.js
@@ -1,16 +1,37 @@
 const { treasuryExports } = require("../helper/treasury");
-const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
+const bitcoinAddressBook = require('../helper/bitcoin-book/index.js');
 
+const abi = {
+    "getAssetIDs": "function getAssetIDs() view returns (uint256[])",
+    "assetTokens": "function assetTokens(uint256 assetID) view returns (address)"
+}
 
-// https://github.com/DefiLlama/DefiLlama-Adapters/pull/12893#issuecomment-2577690389s
+const ASSET_FACTORY_CONTRACT = '0xb04eB6b64137d1673D46731C8f84718092c50B0D';
+
+async function tvl(api) {
+  const assetIDs = [19];
+  const tokens = await api.batchCall(
+    assetIDs.map(assetID => {
+      return { abi: abi.assetTokens, target: ASSET_FACTORY_CONTRACT, params: [assetID] };
+    })
+  );
+  const balances = await api.multiCall({ abi: 'erc20:totalSupply', calls: tokens });
+  api.add(tokens, balances);
+}
+
+// add SSI Protocol TVL adapter by shirc · Pull Request #12893 · DefiLlama/DefiLlama-Adapters
 module.exports = treasuryExports({
-  ethereum: { owners: ['0x55ac87E54019fa2e2156a0fAf13176DcdDFA16ce', '0x605b50f07f46251a7a39fa18c2247fb612f7452f', '0x3bc6b3146d48fafcfb6be35284295019fbd645e7'], },
-  bsc: { owners: [ '0x3bc6b3146d48fafcfb6be35284295019fbd645e7'], },
-  doge: { owners: [ 'DPQ3EbacSG6gdakZmXwMu7qS6SbpRUjY4a', 'D5gedqfZm198AyTFVg8NqWFUh8bFTdmKj7'], },
-  solana: { owners: ['CeuKmW1XqgKz4E8JNpZxrysMRsvkEz55qUvm9soqhALY', 'GQkn3fPeCV4pH1MGZVHsWPpRdq5ENYnaB8GVwSubjkCZ', '8yVXip9eFwdmrbTxPqHsuCvVR5ktBdLGz7S1bUpSx7j6'] },
-  bitcoin: { owners: bitcoinAddressBook.ssiProtocol },
-  cardano: { owners: ['addr1v9nkv9p0gz83ha7hx0h6pg6lrte0t0dsj8tyersc8np5gegrwwxpp'] },
-  ripple: { owners: ['rp53vxWXuEe9LL6AHcCtzzAvdtynSL1aVM'] },
-})
+  ethereum: { owners: ['0x55ac87E54019fa2e2156a0fAf13176DcdDFA16ce', '0x605b50f07f46251a7a39fa18c2247fb612f7452f'], },
+  doge: { owners: [ 'DPQ3EbacSG6gdakZmXwMu7qS6SbpRUjY4a'], },
+  solana: { owners: ['CeuKmW1XqgKz4E8JNpZxrysMRsvkEz55qUvm9soqhALY', 'GQkn3fPeCV4pH1MGZVHsWPpRdq5ENYnaB8GVwSubjkCZ'] },
+});
+Object.keys(module.exports).forEach(chain => delete module.exports[chain].ownTokens);
 
-Object.keys(module.exports).forEach(chain => delete module.exports[chain].ownTokens)
+module.exports = {
+  ...module.exports,
+  methodology: 'TVL counts the SSI tokens and USSI token minted.',
+  start: 23863972,
+  base: {
+    tvl,
+  }
+};
