1e8de0680865b016db2403a52c457d23987c8ec9
John Adler <john.adler@protonmail.ch>
2025-01-21 21:19:26 -0500

    Add Eclipse TVL to Celestia too.

--- projects/eclipse-bridge/index.js
+++ projects/eclipse-bridge/index.js
@@ -1,13 +1,10 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
+async function tvl(api) {
+  return sumTokens2({ api, owner: '0xd7e4b67e735733ac98a88f13d087d8aac670e644', fetchCoValentTokens: true, permitFailure: true })
+}
+
 module.exports = {
-  ethereum: {
-    tvl: (api) =>
-      sumTokens2({
-        api,
-        owner: '0xd7e4b67e735733ac98a88f13d087d8aac670e644',
-        fetchCoValentTokens: true,
-        permitFailure: true
-      }),
-  },
+  celestia: { tvl },
+  ethereum: { tvl },
 };
