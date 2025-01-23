62a36287d26d0672bbccc50568eea04e24ab25a6
erozanov <evgeny.rozanov@gmail.com>
2025-01-23 21:19:03 +0400

    Artela network

--- projects/accumulated-finance/index.js
+++ projects/accumulated-finance/index.js
@@ -70,6 +70,13 @@ const config = {
       }
     }
   ],
+  "artela": [
+    {
+      "artela": {
+        "LST": "0xcba2aeec821b0b119857a9ab39e09b034249681a"
+      },
+    }
+  ],
   "stETH": [
     {
       "ethereum": {
