994958223bb953736803e660f929cf7d9612f502
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-23 11:57:47 +0100

    code refactor

--- projects/evaa/index.js
+++ projects/evaa/index.js
@@ -1,56 +1,26 @@
-const { call, getTokenRates } = require("../helper/chain/ton");
+
+const ADDRESSES = require('../helper/coreAssets.json')
+const { call, sumTokens } = require("../helper/chain/ton");
 const evaaPoolAssets = require("./evaaPoolAssets");
 
-async function processTVL(api) {
-  const assetPricesMap = new Map();
-
-  for (const [poolName, poolAssets] of Object.entries(evaaPoolAssets)) {
-    for (const { assetId, decimals, cgId, cgListed, address } of poolAssets.assets) {
-      let success = false;
-      let retries = 3;
-      let delay = 1000; // milliseconds
-
-      while (!success && retries > 0) {
-        try {
-          const [totalSupply, totalBorrow] = await call({ target: poolAssets.poolAddress, abi: 'getAssetTotals', params: [["int", assetId]] });
-          const assetTvl = totalSupply / 10 ** decimals;
-          if(cgListed) {
-            api.addCGToken(cgId, assetTvl);
-          } else {
-            if(assetPricesMap.has(address)) {
-              const assetPrice = assetPricesMap.get(address);
-              api.add('tether', assetPrice * assetTvl, { skipChain: true })
-            } else {
-              const tokenRates = await getTokenRates({ tokens: [address] })
-              const tokenPrice = tokenRates[address];
-              assetPricesMap.set(address, tokenPrice);
-              api.add('tether', tokenPrice * assetTvl, { skipChain: true })
-            }
-          }
-          success = true;
-        } catch (error) {
-          if (error) {
-            retries -= 1;
-            if (retries > 0) {
-              await new Promise(resolve => setTimeout(resolve, delay));
-              delay *= 2; // Exponential backoff
-            }
-          } else {
-            throw error; // Rethrow other errors
-          }
-        }
-      }
-
-      if (!success) {
-        throw new Error(`Failed to fetch data after multiple retries for assetId: ${assetId}`);
-      }
+async function borrowed(api) {
+  for (const poolAssets of Object.values(evaaPoolAssets)) {
+    for (const { assetId, address } of poolAssets.assets) {
+      const [_totalSupply, totalBorrow] = await call({ target: poolAssets.poolAddress, abi: 'getAssetTotals', params: [["int", assetId]] });
+      api.add(address, totalBorrow)
     }
   }
+}
+
+async function tvl(api) {
+  const owners = Object.values(evaaPoolAssets).map(pool => pool.poolAddress);
+  return sumTokens({ owners, api, tokens: [ADDRESSES.null], useTonApiForPrices: false })
+
 }
 
 module.exports = {
   methodology: 'Counts the supply of EVAA\'s asset pools as TVL.',
   ton: {
-    tvl: (api) => processTVL(api),
+    tvl, borrowed,
   }
 }
--- projects/helper/chain/ton.js
+++ projects/helper/chain/ton.js
@@ -22,12 +22,16 @@ async function getJettonBalances(addr) {
   return res
 }
 
-async function _sumTokensAccount({ api, addr, tokens = [], onlyWhitelistedTokens = false }) {
+async function _sumTokensAccount({ api, addr, tokens = [], onlyWhitelistedTokens = false, useTonApiForPrices = true, }) {
   if (onlyWhitelistedTokens && tokens.length === 1 && tokens.includes(ADDRESSES.ton.TON)) return;
   const { balances } = await get(`https://tonapi.io/v2/accounts/${addr}/jettons?currencies=usd`)
   await sleep(1000 * (3 * Math.random() + 3))
   balances.forEach(({ balance, price, jetton }) => {
     if (onlyWhitelistedTokens && !tokens.includes(jetton.address)) return;
+    if (!useTonApiForPrices) {
+      api.add(jetton.address, balance)
+      return;
+    }
     const decimals = jetton.decimals
     price = price?.prices?.USD
     if (!decimals || !price) return;
@@ -55,7 +59,7 @@ async function getTokenRates({ tokens = [] }) {
 
 const sumTokensAccount = rateLimited(_sumTokensAccount)
 
-async function sumTokens({ api, tokens, owners = [], owner, onlyWhitelistedTokens = false }) {
+async function sumTokens({ api, tokens, owners = [], owner, onlyWhitelistedTokens = false, useTonApiForPrices = true }) {
   if (!api) throw new Error('api is required')
 
   if (owner) owners.push(owner)
@@ -64,7 +68,7 @@ async function sumTokens({ api, tokens, owners = [], owner, onlyWhitelistedToken
   if (tokens.includes(ADDRESSES.null)) await addTonBalances({ api, addresses: owners })
 
   for (const addr of owners) {
-    await sumTokensAccount({ api, addr, tokens, onlyWhitelistedTokens })
+    await sumTokensAccount({ api, addr, tokens, onlyWhitelistedTokens, useTonApiForPrices })
   }
   return api.getBalances()
 }
@@ -79,7 +83,7 @@ async function call({ target, abi, params = [], rawStack = false, }) {
     "method": abi,
     "stack": params
   }
-  const { ok, result } = await post('https://toncenter.com/api/v2/runGetMethod', requestBody)
+  const { ok, result } = await post('https://ton.drpc.org/rest/runGetMethod', requestBody)
   if (!ok) {
     throw new Error("Unknown");
   }
