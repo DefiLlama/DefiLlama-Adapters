599f63a9a7369c0675a50c1f382ad5752c646745
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-23 13:19:40 +0100

    revert change

--- projects/helper/chain/ton.js
+++ projects/helper/chain/ton.js
@@ -23,13 +23,24 @@ async function getJettonBalances(addr) {
   return res
 }
 
-async function _sumTokensAccount({ api, addr, tokens = [], onlyWhitelistedTokens = false, }) {
+async function _sumTokensAccount({ api, addr, tokens = [], onlyWhitelistedTokens = false, useTonApiForPrices = true, }) {
   if (onlyWhitelistedTokens && tokens.length === 1 && tokens.includes(ADDRESSES.ton.TON)) return;
   const { balances } = await get(`https://tonapi.io/v2/accounts/${addr}/jettons?currencies=usd`)
-  await sleep(1000 * (2 * (Math.random() + 1)))
-  balances.forEach(({ balance, jetton }) => {
+  await sleep(1000 * (3 * Math.random() + 3))
+  balances.forEach(({ balance, price, jetton }) => {
     if (onlyWhitelistedTokens && !tokens.includes(jetton.address)) return;
-    api.add(jetton.address, balance)
+    if (!useTonApiForPrices) {
+      api.add(jetton.address, balance)
+      return;
+    }
+    const decimals = jetton.decimals
+    price = price?.prices?.USD
+    if (!decimals || !price) {
+      api.add(jetton.address, balance)
+      return;
+    }
+    const bal = balance * price / 10 ** decimals
+    api.add('tether', bal, { skipChain: true })
   })
 }
 
@@ -52,7 +63,7 @@ async function getTokenRates({ tokens = [] }) {
 
 const sumTokensAccount = rateLimited(_sumTokensAccount)
 
-async function sumTokens({ api, tokens, owners = [], owner, onlyWhitelistedTokens = false, }) {
+async function sumTokens({ api, tokens, owners = [], owner, onlyWhitelistedTokens = false, useTonApiForPrices = true }) {
   if (!api) throw new Error('api is required')
 
   if (owner) owners.push(owner)
@@ -61,7 +72,7 @@ async function sumTokens({ api, tokens, owners = [], owner, onlyWhitelistedToken
   if (tokens.includes(ADDRESSES.null)) await addTonBalances({ api, addresses: owners })
 
   for (const addr of owners) {
-    await sumTokensAccount({ api, addr, tokens, onlyWhitelistedTokens, })
+    await sumTokensAccount({ api, addr, tokens, onlyWhitelistedTokens, useTonApiForPrices })
   }
   return sumTokens2({ api, })
 }
