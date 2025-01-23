abc95eccababd68e283ea2cf25b404fe99efdaf1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-23 10:35:27 +0100

    code refactor

--- projects/moonlander/index.js
+++ projects/moonlander/index.js
@@ -1,32 +1,16 @@
-const { sumTokens2 } = require("../helper/unwrapLPs");
-
-const CRO_ZKEVM_MOONLANDER_ADDR = '0x02ae2e56bfDF1ee4667405eE7e959CD3fE717A05';
-
-function tvl({moonlander,}) {
-    return async (api) => {
-        const poolTokens = await api.call({
-            abi: abis.totalValue,
-            target: moonlander, 
-        })
-
-        const addresses = [];
-        const valuesUsd = [];
-
-        poolTokens.forEach((poolToken) => {
-            addresses.push(poolToken.tokenAddress);
-            valuesUsd.push(poolToken.valueUsd);
-        });
-
-        return sumTokens2({ api, owner: moonlander, tokens: addresses, balances: valuesUsd})
-    }
-}
-
 const abis = {
-    totalValue: 'function totalValue() view returns (tuple(address tokenAddress, int256 value, uint8 decimals, int256 valueUsd, uint16 targetWeight, uint16 feeBasisPoints, uint16 taxBasisPoints, bool dynamicFee)[])'
+  totalValue: 'function totalValue() view returns (tuple(address tokenAddress, int256 value, uint8 decimals, int256 valueUsd, uint16 targetWeight, uint16 feeBasisPoints, uint16 taxBasisPoints, bool dynamicFee)[])'
+}
+const config = {
+  cronos_zkevm: '0x02ae2e56bfDF1ee4667405eE7e959CD3fE717A05'
 }
 
-module.exports = {
-    cronos_zkevm: {
-      tvl: tvl({ moonlander: CRO_ZKEVM_MOONLANDER_ADDR, }),
-    },
+Object.keys(config).forEach(chain => {
+  const moonlander = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const info = await api.call({  abi: abis.totalValue, target: moonlander})
+      return api.sumTokens({ owner: moonlander, tokens: info.map(i => i.tokenAddress), })
+    }
   }
+})
