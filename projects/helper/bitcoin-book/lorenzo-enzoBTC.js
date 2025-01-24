8deb4554c7abca8bd432aed26f3c120aea5163df
0xajing <jing@lorenzo-protocol.xyz>
2025-01-24 17:33:21 +0800

    feat(lorenzo): update  enzoBTC address list

--- projects/helper/bitcoin-book/lorenzo-enzoBTC.js
+++ projects/helper/bitcoin-book/lorenzo-enzoBTC.js
@@ -1,6 +1,7 @@
 module.exports = [
   'bc1qndzgrwj3y2lhcklme4t72jxq3df2h05vjdgzpp',
-  '19wFRSr3GYHmVQtnmbkx7Wkjw3jZdyYB9a',
   'bc1qa4yfx2meqadqwpmqznlct0t2j6pt5tw5xrpz06',
-  'bc1qnvgmve5gs89ugf4n94jzqgan202dve5dtrj220'
+  'bc1qnvgmve5gs89ugf4n94jzqgan202dve5dtrj220',
+  '19KdJBkptNL7RJYgsWFvGr8BqGaeV9xEpg',
+  '14eK85UFbR74KADFXkPXKyEDrroiVVGmxX'
 ]
