6a88d2e0e919166dc276af289a97a7e80ba3a1f9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-25 12:17:01 +0100

    track metastable meth #13244

--- projects/metastable-musd/index.js
+++ projects/metastable-musd/index.js
@@ -1,31 +1,36 @@
 const sui = require("../helper/chain/sui");
 const http = require('../helper/http');
-const { getEnv } = require('../helper/env')
+const { getEnv } = require('../helper/env');
+const { sumTokens2 } = require("../helper/unwrapLPs");
 
 const endpoint = () => getEnv('SUI_RPC')
 
 async function tvl(api) {
-  const vaultData = await sui.getObjects(["0xb950819c5eba1bb5980f714f2a3b1d8738e3da58a4d9daf5fa21b6c2a7dd1e12"])
+  const vaultData = await sui.getObjects([
+    "0xb950819c5eba1bb5980f714f2a3b1d8738e3da58a4d9daf5fa21b6c2a7dd1e12",
+    "0x2d6e81126336685a28ea0637109b570510f988bba2b589877c9b579d3cb8cad8",
+  ])
 
   for (const { fields: { metadata, } } of vaultData) {
 
     const {
       result: { data: fields }
-    } = await http.post(endpoint(), { jsonrpc: "2.0", id: 1, method: 'suix_getDynamicFields', params: [metadata.fields.id.id , null, 48], })
+    } = await http.post(endpoint(), { jsonrpc: "2.0", id: 1, method: 'suix_getDynamicFields', params: [metadata.fields.id.id, null, 48], })
     const fieldObjects = await sui.getDynamicFieldObjects({
-      parent: metadata.fields.id.id 
+      parent: metadata.fields.id.id
     })
 
     let i = 0
     for (const field of fields) {
-      
+
       const coin = field.name.value.name
       const amount = fieldObjects[i].fields.balance
-      
+
       api.add("0x" + coin, amount)
       i++
     }
   }
+  return sumTokens2({ api })
 }
 
 module.exports = {
