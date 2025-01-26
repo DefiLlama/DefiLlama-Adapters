767109e9759ce9008ec319356cac7a69035f910f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-26 15:57:40 +0100

    Update index.js

--- projects/nexion/index.js
+++ projects/nexion/index.js
@@ -1,9 +1,5 @@
-const sdk = require("@defillama/sdk");
 const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokens2, nullAddress } = require("../helper/unwrapLPs");
-const farmAbi = require("./farm-Abi.json");
 const { sumTokensExport } = require("../helper/unknownTokens.js");
-const { staking } = require('../helper/staking.js');
 
 let contracts = {
   NEONStaking: "0x00149EF1A0a41083bC3996d026a7c0f32fc5cb73",
@@ -17,27 +13,29 @@ let contracts = {
   NEONLPStaking: "0x08e9363DE98F0E2414b6DC7a1081c5a29964319e",
   NEONFarm: "0xdF6ec9b93fa473Cb6772dc47326338ecBa374D39",
   OLDNEONFarm: "0x80020303898695b3Ab8017869B6158B49cD5B6CC",
-  INC:"0x2fa878Ab3F87CC1C9737Fc071108F904c0B0C95d"
+  INC: "0x2fa878Ab3F87CC1C9737Fc071108F904c0B0C95d"
 };
 
 const COLLATERALS = {
-
-    DAI: ADDRESSES.pulse.DAI,
-    WPLS: ADDRESSES.pulse.WPLS,
-    DAIPLS_LP:"0xE56043671df55dE5CDf8459710433C10324DE0aE"
-
+  DAI: ADDRESSES.pulse.DAI,
+  WPLS: ADDRESSES.pulse.WPLS,
+  DAIPLS_LP: "0xE56043671df55dE5CDf8459710433C10324DE0aE"
 };
 
 module.exports = {
   methodology: "NEON can be staked in the protocol, Farms hold PLS-DAI LP from user deposits that can be withdrawn after 500days",
 
   pulse: {
-    staking:staking(contracts.NEONStaking,contracts.NEON),
-    tvl :sumTokensExport({ owners: [contracts.NEONFarm,contracts.OLDNEONFarm],
-      tokens: [COLLATERALS.DAI, COLLATERALS.WPLS,nullAddress],
+    tvl: sumTokensExport({
+      owners: [contracts.NEONFarm, contracts.OLDNEONFarm],
+      tokens: [COLLATERALS.DAI, COLLATERALS.WPLS, ADDRESSES.null],
       useDefaultCoreAssets: true,
       lps: [COLLATERALS.DAIPLS_LP]
-    })
-    
+    }), staking: sumTokensExport({
+      owners: [contracts.NEONStaking],
+      tokens: [contracts.NEON],
+      useDefaultCoreAssets: true,
+      lps: ['0xEd15552508E5200f0A2A693B05dDd3edEF59e624']
+    }),
   },
 };
