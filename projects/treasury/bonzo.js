3481eb83d41a6dfc188d4773cfe098d9bfefe215
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-26 13:49:53 +0100

    refactor(treasury): streamline TVL calculation and improve data fetching

--- projects/treasury/bonzo.js
+++ projects/treasury/bonzo.js
@@ -1,19 +1,17 @@
-const BigNumber = require("bignumber.js");
-const axios = require("axios");
+const { getConfig } = require('../helper/cache')
+const ADDRESSES = require('../helper/coreAssets.json')
 
-const fetch = async () => {
-    const res = await axios.get("https://data.bonzo.finance/market");
-    const result = {};
-    for (const reserve of res.data.reserves) {
-        result[`hedera:${reserve.evm_address}`] = new BigNumber(reserve.total_reserve.tiny_token).toString();
-    }
-    return result;
+const tvl = async (api) => {
+    const { reserves } = await getConfig('bonzo-treasury', "https://data.bonzo.finance/market");
+    const treasury = '0x00000000000000000000000000000000005dc4d4'
+    const tokens = reserves.map(reserve => [reserve.evm_address, reserve.atoken_address	]).flat()
+    tokens.push(ADDRESSES.null)
+    return api.sumTokens({ owner: treasury, tokens })
 };
 
 module.exports = {
-    misrepresentedTokens: true,
     methodology: "The Treasury holds aToken balances, but reports using corresponding HTS token addresses to facilitate proper USD value calculations by DeFi Llama's pricing API.",
     hedera: {
-        tvl: fetch
+        tvl
     }
 };
