b1afca945be627819a25e9a86cb663af8460f4cb
franz <franzns11@gmail.com>
2025-01-27 20:49:11 +0100

    adapt block

--- projects/beethovenx-v3/index.js
+++ projects/beethovenx-v3/index.js
@@ -1,7 +1,7 @@
 const { v3Tvl } = require("../helper/balancer")
 
 const config = {
-  sonic: { vault: '0xbA1333333333a1BA1108E8412f11850A5C319bA9', fromBlock: 368312 },
+  sonic: { vault: '0xbA1333333333a1BA1108E8412f11850A5C319bA9', fromBlock: 368135 },
 }
 
 Object.keys(config).forEach(chain => {
