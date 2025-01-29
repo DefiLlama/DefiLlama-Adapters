77f752cdd2463fe35dc5eeeeeebd79339a1a881a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-29 08:45:29 +0100

    minor fix

--- projects/sulaana/index.js
+++ projects/sulaana/index.js
@@ -1,17 +1,18 @@
-const getStakedSulBalance = require("../helper/chain/pollux");
+const { getStakedSulBalance } = require("../helper/chain/pollux");
 
-const SULAANA_CONTRACT_ADDRESS="PGM9RMkJ3yB5r9vtzHG6yca4hd4DjvweC5";
-const SULMINE_CONTRACT_ADDRESS_ENCODED="000000000000000000000000968cf48070f154927bbe03819db99105e17df975";
-const CALLER_ADDRESS="PSzTrL9rUT9ak5MZbResVqUV9Ztqz83PAA"
+const SULAANA_CONTRACT_ADDRESS = "PGM9RMkJ3yB5r9vtzHG6yca4hd4DjvweC5";
+const SULMINE_CONTRACT_ADDRESS_ENCODED = "000000000000000000000000968cf48070f154927bbe03819db99105e17df975";
+const CALLER_ADDRESS = "PSzTrL9rUT9ak5MZbResVqUV9Ztqz83PAA"
 
-async function tvl() {
-    const totalSulbalance = await getStakedSulBalance(SULAANA_CONTRACT_ADDRESS,SULMINE_CONTRACT_ADDRESS_ENCODED,CALLER_ADDRESS)
-    return totalSulbalance
+async function staking(api) {
+  const totalSulbalance = await getStakedSulBalance(SULAANA_CONTRACT_ADDRESS, SULMINE_CONTRACT_ADDRESS_ENCODED, CALLER_ADDRESS)
+  api.addCGToken('sulaana', totalSulbalance)
 }
 
 module.exports = {
   methodology: 'Counts the Sulaana token present in the contract.',
   pollux: {
-    tvl,
+    tvl: () => ({}),
+    staking
   }
 }; 
