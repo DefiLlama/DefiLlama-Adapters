4e03c9af3bf811bc60125bed8dc4f765bcdc9089
Define101 <93603962+Define101@users.noreply.github.com>
2025-01-29 12:35:43 +0000

    undo last commit (#13297)

--- projects/helper/cex.js
+++ projects/helper/cex.js
@@ -111,7 +111,6 @@ const defaultTokens = {
      "0x4d5f47fa6a74757f35c14fd3a6ef8e3c9bc514e8", // aEthWETH
      "0x98c23e9d8f34fefb1b7bd6a91b7ff122f4e16f5c", // aEthUSDC
      "0x6982508145454ce325ddbe47a25d4ec3d2311933",  // PEPE
-     "0xcd5fe23c85820f7b72d0926fc9b05b43e359b7ee"
   ],
   tron: [
     nullAddress,
