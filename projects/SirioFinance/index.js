e9315e1b8a34f8c0375a3dece01059511bf7d4b1
Jordy <jordy@nefkon.com>
2025-01-29 20:57:08 +0100

    add tvl for Sirio Finance

--- projects/SirioFinance/index.js
+++ projects/SirioFinance/index.js
@@ -1,40 +1,12 @@
-const axios = require("axios");
+const { fetchURL } = require("../helper/utils")
 
-const API_URL = "https://mainnet.service-api.sirio.finance/api/v1/markets/stats"; // Endpoint della tua API
-
-async function tvl(api) {
-    const response = await axios.get(API_URL);
-    const markets = response.data.markets;
-
-    for (let market of markets) {
-    let tokenName = market.token.name;
-
-      if(tokenName === "USDC") {
-        tokenName = "usd-coin";
-      }
-      if(tokenName === "HBAR") {
-        tokenName = "hedera-hashgraph";
-      }
-      if(tokenName === "PACK") {
-        tokenName = "hashpack";
-      }
-      if(tokenName === "SAUCE") {
-        tokenName = "saucerswap";
-      }
-
-      const decimal = market.token.decimal;
-      const supplied = market.supplied / 10 ** (decimal);
-      const borrowed = market.borrowed / 10 ** (decimal);
-
-      const tokenValueUSD = (supplied + borrowed);
-    
-      api.addCGToken(tokenName, tokenValueUSD);
-    }
-    
+async function fetch() {
+    const res = await fetchURL("https://mainnet.service-api.sirio.finance/api/v1/markets/stats");
+    return res.data.total_supply_usd;
 }
 
 module.exports = {
-  methodology: "Sum of all supplied and borrowed tokens from API https://mainnet.service-api.sirio.finance/api/v1/markets/stats",
-  timetravel: false,
-  hedera: { tvl }
-};
+    timetravel: false,
+    methodology: 'The calculated TVL is the current USD sum found under https://app.sirio.finance/analytics',
+    fetch
+}
