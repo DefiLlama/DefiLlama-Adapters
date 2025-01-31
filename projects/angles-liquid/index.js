2a6ebc72f7fa2adcc517d929ee3c22297528ec30
anglesb <derdentyler841@gmail.com>
2025-02-01 00:01:13 +0500

    Angles liquid intro

---
+++ projects/angles-liquid/index.js
@@ -0,0 +1,18 @@
+async function tvl(api) {
+    const registryAddress = '0x9786DB1486A2f67977eF1e3dfbd0Eb01e407Be7b';
+
+    const pools = await api.call({
+        abi: 'function getPoolAddresses() view returns (address[])',
+        target: registryAddress,
+    });
+
+    const tokens = await api.fetchList({ lengthAbi: 'numTokens', itemAbi: 'tokens', calls: pools, groupedByInput: true, })
+    const ownerTokens = pools.map((v, i) => [tokens[i], v])
+    return api.sumTokens({ ownerTokens })
+}
+
+
+module.exports = {
+    start: '2025-01-01',
+    sonic: { tvl }
+};
