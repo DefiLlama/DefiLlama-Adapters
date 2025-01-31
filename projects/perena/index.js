fb49d5d5a2212471bf0728c0cbed7cfe2c0051b8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-31 11:00:02 +0100

    code refactor

--- projects/perena/index.js
+++ projects/perena/index.js
@@ -1,20 +1,29 @@
-const { sumTokens2 } = require("../helper/solana");
+const { getProvider, sumTokens2, } = require('../helper/solana')
+const { Program } = require("@coral-xyz/anchor");
+const { PublicKey } = require('@solana/web3.js');
 
 async function tvl() {
-    return await sumTokens2({
-        tokenAccounts: [
-            '6B8k8At9879r5EsZAWg8W6DEpxzJ798Cwj48twu3pq4b', // USDC
-            'DJfYbGicp4AFXWsragNWv1baugqcdFhw6eFTcK2YWdyK', // USDT
-            '4jV62TqjV4oUyNBioPo9dJZGyjTcgfBDkuG4VbgxfqWC', // PYSUD
-            '3zR26w68Va5qT6J7ZKkbxV6MJVdVxHj5ku7Laogu9VoN', // SUSD
-            '3DFmntWjh9PrzsiFnPQeMW773A9m62uVPE6j9YK32QCa', // USDS
-            'DqNRPbFHeDwM3JAXJmSutPRJNimaz4pdHjKo6vGx6jfg', // USDY
-            'FuSNPqppBECY7vvyFFdDxAkxL67zgoujYEQSpKejfHa2'  // CFUSD
-        ]
-    });
+  const provider = getProvider()
+
+  const programId = new PublicKey('NUMERUNsFCP3kuNmWZuXtm1AaQCPj9uw6Guv2Ekoi5P')
+  const idl = await Program.fetchIdl(programId, provider)
+  const program = new Program(idl, provider)
+
+  // this filter is needed because the call fails, there are some bad pools that fail to decode
+  const poolData = await program.account.stablePool.all([{ dataSize: program.account.stablePool._size }])
+
+  const tokenAccounts = []
+  const blacklistedTokens = []
+  poolData.forEach(({ account: i }) => {
+    blacklistedTokens.push(i.lpMint.toString())
+    i.pairs.forEach(pair => {
+      tokenAccounts.push(pair.xVault.toString())
+    })
+  })
+  return sumTokens2({ tokenAccounts: tokenAccounts.filter(i => i !== '11111111111111111111111111111111'), blacklistedTokens, })
 }
 
 module.exports = {
-    timetravel: false,
-    solana: { tvl, },
+  timetravel: false,
+  solana: { tvl, },
 }
