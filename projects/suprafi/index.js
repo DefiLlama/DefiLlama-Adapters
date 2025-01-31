666a926dba7136cf0ec65819070669d03e969c79
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-31 10:14:18 +0100

    Update index.js

--- projects/suprafi/index.js
+++ projects/suprafi/index.js
@@ -1,23 +1,17 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokens2 } = require("../helper/unwrapLPs");
-
 module.exports = {
   methodology: "Retrieve the total underlying sonic supply",
-};
-
+}
 
 const config = {
   sonic: "0x6ba47940f738175d3f8c22aa8ee8606eaae45eb2",
 }
 
 Object.keys(config).forEach(chain => {
-  const liquidStakingContract = config[chain]
   module.exports[chain] = {
     tvl: async (api) => {
-      const abi = chain === 'fantom' ? 'totalFTMWorth' : 'totalAssets'
-      const supply = await api.call({ abi: "uint256:" + abi, target: liquidStakingContract, });
-      api.addGasToken(supply)
-      return sumTokens2({ api })
+      const vault = config[chain]
+      const bal = await api.call({  abi: 'uint256:totalAssets', target: vault})
+      api.addGasToken(bal)
     }
   }
 })
