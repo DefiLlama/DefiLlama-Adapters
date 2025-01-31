f3acaf0be3bf26e5b337a847aa577eaacc8fcf5d
Define101 <Lovelovenow@protonmail.com>
2025-01-31 10:27:11 +0000

    babypie WIP

--- projects/babypie/index.js
+++ projects/babypie/index.js
@@ -6,5 +6,5 @@ module.exports = {
   bitcoin: {
     tvl: sumTokensExport({ owners: bitcoinAddressBook.magpie }),
   },
-  ethereum: { tvl: sumTokensExport({ owners: ['0xE813FFA7932f2D182F0ae89254acFD0bAa6E2Df3'], tokens: [ADDRESSES.ethereum.WBTC] }) },
+  ethereum: { tvl: sumTokensExport({ owners: ['0xE813FFA7932f2D182F0ae89254acFD0bAa6E2Df3'], tokens: [ADDRESSES.ethereum.WBTC, "0x8BB97A618211695f5a6a889faC3546D1a573ea77"] }) },
 }
