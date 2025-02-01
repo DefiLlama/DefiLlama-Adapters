2a4bbaeae08b56ada21f6b50cff1c65f507e4670
Lakshan Sivananthan <lakshan@MacBook-Air.local>
2025-02-01 01:24:42 -0500

    SXR: Added staking to tvl

--- projects/sharkswap/index.js
+++ projects/sharkswap/index.js
@@ -1,6 +1,19 @@
-const { uniTvlExports } = require('../helper/unknownTokens')
+const { staking } = require('../helper/staking');
+const { getUniTVL } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExports({
-  sx: '0x6A482aC7f61Ed75B4Eb7C26cE8cD8a66bd07B88D',
-  sxr: '0x610CfC3CBb3254fE69933a3Ab19aE1bF2aaaD7C8',
-})
+module.exports = {
+  sx: {
+    tvl: getUniTVL({
+      factory: '0x6A482aC7f61Ed75B4Eb7C26cE8cD8a66bd07B88D',
+      useDefaultCoreAssets: true,
+    })
+  },
+  sxr: {
+    tvl: getUniTVL({
+      factory: '0x610CfC3CBb3254fE69933a3Ab19aE1bF2aaaD7C8',
+      useDefaultCoreAssets: true,
+    }),
+    staking: staking('0x2083eF16cc1749c98F101E41Dba9b9472D4C5702', '0x3E96B0a25d51e3Cc89C557f152797c33B839968f'),
+  }
+}
+
