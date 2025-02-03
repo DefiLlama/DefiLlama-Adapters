992d216f5d0ce97fb4f12753d0a1f64360380428
kohsine <kevoh1516@gmail.com>
2025-02-03 13:21:32 -0500

    fix: add chain

--- projects/solera/index.js
+++ projects/solera/index.js
@@ -3,5 +3,5 @@ const methodologies = require("../helper/methodologies");
 
 module.exports = {
   methodology: methodologies.lendingMarket,
-  plume: aaveExports(undefined, undefined, undefined, ['0x6e2921D5C907f47Fd6A577A992920C9b9Acc7F23'], { v3: true }),
+  plume: aaveExports("plume", undefined, undefined, ['0x6e2921D5C907f47Fd6A577A992920C9b9Acc7F23'], { v3: true }),
 }
