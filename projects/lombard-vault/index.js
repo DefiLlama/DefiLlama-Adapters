e9ed51c36c9bff00a5ab779c20ad4f7e1dd362b3
Define101 <93603962+Define101@users.noreply.github.com>
2025-02-03 13:21:31 +0000

    add more chains lombard vault (#13346)

--- projects/lombard-vault/index.js
+++ projects/lombard-vault/index.js
@@ -1,11 +1,21 @@
 const { sumTokensExport } = require('../helper/unwrapLPs');
 const vaults = [
-  '0x5401b8620E5FB570064CA9114fd1e135fd77D57c',
+  '0x5401b8620E5FB570064CA9114fd1e135fd77D57c', // Lombard DeFi Vault https://www.lombard.finance/app/vault/
 ]
 
 module.exports = {
   doublecounted: true,
   ethereum: {
+    tvl: sumTokensExport({ owners: vaults, fetchCoValentTokens: true, tokenConfig: {
+      onlyWhitelisted: false,
+    }, resolveUniV3: true,}),
+  },
+  base: {
+    tvl: sumTokensExport({ owners: vaults, fetchCoValentTokens: true, tokenConfig: {
+      onlyWhitelisted: false,
+    }, resolveUniV3: true,}),
+  },
+  bsc: {
     tvl: sumTokensExport({ owners: vaults, fetchCoValentTokens: true, tokenConfig: {
       onlyWhitelisted: false,
     }, resolveUniV3: true,}),
