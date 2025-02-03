c4ce294b00958f9fe9ffdbf862dabb75199b72b1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-03 21:25:37 +0100

    sx: remove staking

--- projects/sxbet/index.js
+++ projects/sxbet/index.js
@@ -1,4 +1,3 @@
-const { staking } = require("../helper/staking");
 const { sumTokensExport } = require("../helper/unknownTokens");
 
 const escrowTvl = sumTokensExport({
@@ -6,11 +5,8 @@ const escrowTvl = sumTokensExport({
   tokens: ['0x3E96B0a25d51e3Cc89C557f152797c33B839968f', '0x6629Ce1Cf35Cc1329ebB4F63202F3f197b3F050B'],
 })
 
-const stakingTvl = staking('0x2083eF16cc1749c98F101E41Dba9b9472D4C5702', '0x3E96B0a25d51e3Cc89C557f152797c33B839968f');
-
 module.exports = {
   sxr: {
     tvl: escrowTvl,
-    staking: stakingTvl,
   }
 };
