a2e441d2bb1d7ada2cdd9c28f2aa1e8507c14b70
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-04 12:07:03 +0100

    exclude borrowed from tvl

--- projects/factorial/index.js
+++ projects/factorial/index.js
@@ -1,6 +1,7 @@
 const { call } = require("../helper/chain/ton");
 const { processTVMSliceReadAddress } = require("./utils");
 const ADDRESSES = require("../helper/coreAssets.json");
+const { methodology } = require('../helper/aave')
 
 const factorial_ton = "EQDIKEz2BYLnTRWo5W5a6moZ9PXNtyOVOFF7noi8Ufv3axz_";
 const market_main = "EQCrEHHG4Ff8TD3PzuH5tFSwHBD3zxeXaosz48jGLebwiiNx"
@@ -11,6 +12,7 @@ async function tvl(api) {
   const _kv = 1;
   const assetDicIdx = 13;
   const supplyAmountIdx = 4;
+  const borrowAmountIdx = 5;
   const addressIdx = 15;
 
   const assets = pool_data[assetDicIdx][_kv]["elements"];
@@ -18,12 +20,13 @@ async function tvl(api) {
   assets.forEach((asset) => {
     const assetInfo = asset["tuple"]["elements"];
     const supplied = assetInfo[supplyAmountIdx]["number"]["number"]; 
+    const borrowed = assetInfo[borrowAmountIdx]["number"]["number"]; 
     const address = assetInfo[addressIdx]["slice"]["bytes"];
 
     const assetAddress = processTVMSliceReadAddress(address);
     const addressToAdd = assetAddress === factorial_ton ? ADDRESSES.ton.TON : assetAddress;
     
-    api.add(addressToAdd, supplied);
+    api.add(addressToAdd, supplied - borrowed);
   });
 }
 
@@ -50,7 +53,8 @@ async function borrowed(api) {
 }
 
 module.exports = {
-  methodology: 'Total amount of assets locked in Factorial pool',
+  methodology,
+  timetravel: false,
   ton: {
     tvl, borrowed,
   }
