a24f419704149d38168c7e36c7b515a4f599fe50
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-04 11:19:00 +0100

    Update index.js

--- projects/passdex/index.js
+++ projects/passdex/index.js
@@ -1,18 +1,4 @@
-
-
-
-const { getUniTVL } = require("../helper/unknownTokens");
-
-const config = {
-  soneium: '0x48E07EE0E8450811AD472fFf10aB68B895EA747A',
-  
-}
-
-module.exports = {
-  misrepresentedTokens: true,
-}
-
-Object.keys(config).forEach(chain => {
-  module.exports[chain] = { tvl: getUniTVL({ factory: config[chain], useDefaultCoreAssets: true, }), }
+const { uniTvlExports } = require('../helper/unknownTokens')
+module.exports = uniTvlExports({
+  soneium: '0x48E07EE0E8450811AD472fFf10aB68B895EA747A'
 })
-
