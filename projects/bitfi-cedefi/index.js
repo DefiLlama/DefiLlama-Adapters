88ed06dab4ac65b3169df95e8d38b9d82ca22a51
nstanthony <anthony@bitfi.org>
2025-02-04 15:04:42 -0500

    Update index.js

--- projects/bitfi-cedefi/index.js
+++ projects/bitfi-cedefi/index.js
@@ -1,17 +1,17 @@
 const mapping = {
   btr: {
-    bfbtc: '0xCdFb58c8C859Cb3F62ebe9Cf2767F9e036C7fb15',
+    bfBTC: '0xCdFb58c8C859Cb3F62ebe9Cf2767F9e036C7fb15',
   },
   bsc: {
-    bfbtc: '0x623F2774d9f27B59bc6b954544487532CE79d9DF',
+    bfBTC: '0x623F2774d9f27B59bc6b954544487532CE79d9DF',
   }
 }
 const exportObject = {}
 Object.keys(mapping).forEach(chain => {
   exportObject[chain] = {
     tvl: async (api) => {
-      const supply = await api.call({ abi: 'erc20:totalSupply', target: mapping[chain].bfbtc })
-      api.add(mapping[chain].bfbtc, supply)
+      const supply = await api.call({ abi: 'erc20:totalSupply', target: mapping[chain].bfBTC })
+      api.add(mapping[chain].bfBTC, supply)
     }
   }
 })
