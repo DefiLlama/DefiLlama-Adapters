aeff2b09f81c287e0231709344d1f9a046d4214e
Phil <pbal@fordham.edu>
2025-02-07 13:42:19 -0800

    Remove Bera Vaults from mainnet

--- projects/veda/ethereum_constants.js
+++ projects/veda/ethereum_constants.js
@@ -201,26 +201,6 @@ const boringVaultsV0Ethereum = [
     startBlock: 21363112,
     baseAsset: ADDRESSES.ethereum.USDC,
   },
-  */
-  {
-    name: "Liquid Bera ETH",
-    vault: "0x83599937c2C9bEA0E0E8ac096c6f32e86486b410",
-    accountant: "0x04B8136820598A4e50bEe21b8b6a23fE25Df9Bd8",
-    teller: "0xCbc0D2838256919e55eB302Ce8c46d7eE0E9d807",
-    lens: "0x5232bc0F5999f8dA604c42E1748A13a170F94A1B",
-    startBlock: 20072943,
-    baseAsset: ADDRESSES.ethereum.WETH,
-  },
-  {
-    name: "Liquid Bera BTC",
-    vault: "0xC673ef7791724f0dcca38adB47Fbb3AEF3DB6C80",
-    accountant: "0xF44BD12956a0a87c2C20113DdFe1537A442526B5",
-    teller: "0x07951756b68427e7554AB4c9091344cB8De1Ad5a",
-    lens: "0x5232bc0F5999f8dA604c42E1748A13a170F94A1B",
-    startBlock: 21514285,
-    baseAsset: ADDRESSES.ethereum.WBTC,
-  },
-  /*
   {
     name: "Usual Bera",
     vault: "0x165c62448015d96c920dDA001Ae27733AF2C36c7",
