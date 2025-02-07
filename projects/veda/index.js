a3110cd5f5dd060860c00c6072e5a6edc48a3ac3
Phil <pbal@fordham.edu>
2025-02-07 13:45:04 -0800

    Clean up for initial submission -- remove ethereum vaults

--- projects/veda/index.js
+++ projects/veda/index.js
@@ -1,33 +1,5 @@
-const sdk = require("@defillama/sdk");
-const { legacyVaultsEthereum, boringVaultsV0Ethereum } = require("./ethereum_constants");
 const { boringVaultsV0Berachain } = require("./berachain_constants");
-const { sumLegacyTvl, sumBoringTvl } = require("./helper_methods");
-
-async function ethereum_tvl(api) {
-  const block = await api.getBlock()
-
-  // Legacy vaults
-  const activeLegacyVaultAddresses = filterActiveLegacyVaults(legacyVaultsEthereum, block);
-  if (activeLegacyVaultAddresses.length > 0) {
-    await sumLegacyTvl({
-      api,
-      vaults: activeLegacyVaultAddresses,
-      ownersToDedupe: [...legacyVaultsEthereum, ...boringVaultsV0Ethereum].filter(v => v.id),
-    });
-  }
-
-  // Boring vaults V0
-  const activeBoringVaults = filterActiveBoringVaults(boringVaultsV0Ethereum, block);
-  if (activeBoringVaults.length > 0) {
-    await sumBoringTvl({
-      api,
-      vaults: activeBoringVaults,
-      ownersToDedupe: [...legacyVaultsEthereum, ...boringVaultsV0Ethereum].filter(v => v.id),
-    });
-  }
-
-  return api.getBalances();
-}
+const { sumBoringTvl } = require("./helper_methods");
 
 // Returns list of vault addresses that are deployed based on their start block
 function filterActiveLegacyVaults(vaults, blockHeight) {
@@ -68,6 +40,5 @@ module.exports = {
   misrepresentedTokens: false,
   start: 1710745200,
   doublecounted: true,
-  ["ethereum"]: { tvl: ethereum_tvl },
   ["berachain"]: { tvl: berachain_tvl }
 };
