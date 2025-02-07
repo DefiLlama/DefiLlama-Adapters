34500ea465ecccc90c4b16f2cb26f402677b47f7
Pekaz <afakafak@gmail.com>
2025-02-07 14:55:24 +0900

    Add Circle native USDC as USDC_3. Resource query string changes due to Layerzero USDC -> Native USDC migration.

--- projects/merkle-trade/index.js
+++ projects/merkle-trade/index.js
@@ -3,17 +3,15 @@ const { getResource } = require("../helper/chain/aptos");
 
 const MERKEL_TRADE =
   "0x5ae6789dd2fec1a9ec9cccfb3acaf12e93d432f0a3a42c92fe1a9d490b7bbc06";
-const lzUSDC =
-  ADDRESSES.aptos.USDC_2;
 
-async function tvl(api) {
+  async function tvl(api) {
   const {
     coin_store: { value },
   } = await getResource(
     MERKEL_TRADE,
-    `${MERKEL_TRADE}::vault::Vault<${MERKEL_TRADE}::vault_type::HouseLPVault,${lzUSDC}>`
+    `${MERKEL_TRADE}::vault::Vault<${MERKEL_TRADE}::vault_type::HouseLPVault,${MERKEL_TRADE}::fa_box::W_USDC>`
   );
-  api.add(lzUSDC, value);
+  api.add(ADDRESSES.aptos.USDC_3, value);
 }
 
 module.exports = {
