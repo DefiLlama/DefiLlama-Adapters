c8e2721d9e01ee69fc338c1f2541529d40338b3d
Ryan Turner <rturner8404@gmail.com>
2025-02-10 16:05:01 -0500

    Add logic to track concrete.xyz TVL across chains

--- projects/concrete-xyz/index.js
+++ projects/concrete-xyz/index.js
@@ -1,19 +1,36 @@
 const { sumERC4626VaultsExport } = require("../helper/erc4626");
+const sdk = require('@defillama/sdk');
 
-const config = {
-  berachain: [
-    '0x59E24F42caE1B82c8b2Dc79Ea898F2F8b4986dfC',
-    '0xDa785861aa6fd80D1388F65693Cd62D8a1E2956a',
-    '0xf0d94806e6E5cB54336ED0f8De459659718F149C',
-    '0xAEbeCae444ac70AbA0385feC4cb11eb26a12C92B',
-  ]
+const registryAddresses = {
+  berachain: ['0x34C83440fF0b21a7DaD14c22fB7B1Bb3fc8433E6'],
+  ethereum: ['0x0Ed9E3271B7bD5a94E95d5c36d87321372B2FA14'],
+  morph: ['0x04c60a0468BC0d329A0C04e8391699c41D95D981'],
+  corn: ['0xed497422Eb43d309D63bee71741FF17511bAb577']
 }
 
-Object.keys(config).forEach(chain => {
-  const vaults = config[chain]
+async function getVaultAddresses(chain) {
+  const vaults = [];
+  for (const registry of registryAddresses[chain]) {
+    const vaults1 = await sdk.api.abi.call({
+      target: registry,
+      abi: 'function getAllVaults() view returns (address[])',
+      params: [],
+      chain,
+    })
+    vaults1.output.forEach(vault => vaults.push(vault))
+  }
+  return vaults;
+}
+
+module.exports = {
+  methodology: "TVL includes all deposits made to the protocols vaults.",
+};
+
+Object.keys(registryAddresses).forEach(chain => {
   module.exports[chain] = {
-    tvl: sumERC4626VaultsExport({ vaults, isOG4626: true, })
+    tvl: async (api) => {
+      const vaults = await getVaultAddresses(chain)
+      return sumERC4626VaultsExport({ vaults, isOG4626: true })(api)
+    }
   }
 })
-
-module.exports.doublecounted = true
