32b7eb42381b53613c34c3fdf3d86fa84eb25c1b
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2025-02-10 17:22:44 +0400

    Update tvl adapter for sailfish to use `uniV3GraphExport`

--- projects/sailfish-v3/index.js
+++ projects/sailfish-v3/index.js
@@ -1,5 +1,11 @@
-const { uniV3Export } = require('../helper/uniswapV3')
+const { uniV3Export, uniV3GraphExport } = require('../helper/uniswapV3')
 
-module.exports = uniV3Export({
-  occ: { factory: '0x963A7f4eB46967A9fd3dFbabD354fC294FA2BF5C', fromBlock: 142495 },
-})
+// module.exports = uniV3Export({
+//   occ: { factory: '0x963A7f4eB46967A9fd3dFbabD354fC294FA2BF5C', fromBlock: 142495 },
+// })
+
+module.exports = {
+  occ: {
+    tvl: uniV3GraphExport({ graphURL: 'https://api.goldsky.com/api/public/project_cm5nst0b7iiqy01t6hxww7gao/subgraphs/sailfish-v3-occ-mainnet/1.0.0/gn', name: 'sailfish-v3' }),
+  }
+}
