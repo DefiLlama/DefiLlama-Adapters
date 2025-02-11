b3bb5e2f066deec9b99241f77cba1dcd3a0edef6
liuzeming1 <liuzeming@storswift.com>
2025-02-11 21:58:45 +0800

    update HashKey token address in tokenMapping.js

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -49,7 +49,7 @@ const fixBalancesTokens = {
     '0x891986cf778004c86c5f2d8c18198635f725a5ce': { coingeckoId: "artela-network", decimals: 18 }
   },
   hashkey: {
-    '0xeB0F8d6f3ba574D8C4C9e51aF4c479872C256515': { coingeckoId: "hashkey", decimals: 18 },
+    '0xB210D2120d57b758EE163cFfb43e73728c471Cf1': { coingeckoId: "hashkey", decimals: 18 },
   }
 }
 
