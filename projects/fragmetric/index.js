cbecbb0d937aece43f20efce52bb23653d117754
Tommy Kang <drcotr0623@naver.com>
2025-02-11 16:58:25 +0900

    Add fragJTO fund assets to Fragmetric TVL (#13456)

--- projects/fragmetric/index.js
+++ projects/fragmetric/index.js
@@ -3,18 +3,22 @@ const { sumTokens2 } = require("../helper/solana");
 async function tvl() {
   return sumTokens2({
     tokenAccounts: [
-      // Fund
+      // fragSOL Fund
       "C4EHVbBhbBGdxZNCJEuKWghYTTjwoPibwo1dcu74hvFM",
       "9aCT9JwuyXj9wzoabwRwgtpGBgDHfpmK8ncEzgRpGPM6",
       "4GzQxHyDdGrVPjEj1cz75R5TmrCkXHMCcUw1t9LpyDcK",
       "Fi69jARfww4TGod9ESLWfSqRf9Vq5vfFNy8K4V8uCGSN",
       "5S7qzbgAgWm1AWrm5zUqnoHisc1W72Bsb9CgunLBqu3q",
-      // NTP
+      // fragSOL NTP
       "HSKvv9UFCn4c6Jq3j8iiJfFgXFjRE6dr6QhWX2KD8gGU",
       "3KdpoeWuwaXLuukf56p8e1FtDKjY8pCmtZdmZejUctwP",
       "9grKYUmguSLVC9RHW1xKcLpiAphJrmcDkTVTCg9ebpFz",
       "EbMiibdEBswBKxATPtYEiaRcx7uzJvyWvexVzUJ6iyje",
       "A2WHeVfyxSF9jQSt7uhvwfvBUqFd7SCVMFbkhVf2rHTG",
+      // fragJTO Fund
+      "DvKHN9YNoWERUNcxpEfUvd1UmPwHjoDgyDiwWntMfba4",
+      // Jito JTO Vault
+      "3Fz1WVV7N3h5VScoveHyote6Y129F3Gmc3PcfnBuSbV3",
     ],
     solOwners: [
       "3H22A3T3CMyoGzAURZ4szV5Hmt64Dooo5g9Ns8h1kYy7",
