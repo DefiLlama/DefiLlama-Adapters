87b774b2dad87766ba372cfd4d5f90d8ec66a5f8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-12 11:27:00 +0100

    Refactor owner addresses in index.js for consistency and clarity

--- projects/fastex/index.js
+++ projects/fastex/index.js
@@ -4,12 +4,12 @@ const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
 const config = {
   ethereum: {
     owners: [
-         '0xc21a1d213f64fedea3415737cce2be37eb59be81',
-         '0x199BF7d50A4C00dae8395457A507613ae098fF60',
-         '0x5C133736f0762bD1Bb21455a10a167A8D2500d1F',
-         '0xe851d077836Cc48E4a09B0B4ed984AcBdE358b57',
-         '0x2A747aa880138042de556195262f01779d4CFc91',
-         '0xb25FfAC8F2dd4696a02c3fE312E1E9c907aF74d5',
+      '0xc21a1d213f64fedea3415737cce2be37eb59be81',
+      '0x199BF7d50A4C00dae8395457A507613ae098fF60',
+      '0x5C133736f0762bD1Bb21455a10a167A8D2500d1F',
+      '0xe851d077836Cc48E4a09B0B4ed984AcBdE358b57',
+      '0x2A747aa880138042de556195262f01779d4CFc91',
+      '0xb25FfAC8F2dd4696a02c3fE312E1E9c907aF74d5',
     ],
   },
   bitcoin: {
@@ -17,32 +17,42 @@ const config = {
   },
   tron: {
     owners: [
-        'TPj7TCJ9rxdd243yQ3tc7iJzqcEYtupB4v' ,
-        'TXW8f2umgDJhVarwosuGW1d8Wr4FaPpAEb' ,
-        'TDwRF28KJQhcGV46yRDFXgFdcLbztjxVbs'
+      'TPj7TCJ9rxdd243yQ3tc7iJzqcEYtupB4v',
+      'TXW8f2umgDJhVarwosuGW1d8Wr4FaPpAEb',
+      'TDwRF28KJQhcGV46yRDFXgFdcLbztjxVbs'
     ]
   },
-   ripple: {
-     owners:[
-       'rNxBjsC1FsEga35GThyb1KXAyS3kDx8gyv',
-       'rMnR4pWoDW2kUSZ7hxnpy2it9ojEdQwc6s',
-     ]
-   },
-   cardano: {
-     owners:[
-       'addr1q8yrpu5fw3qw62fjezu72pap8munej98zpgmxgrye6rw6nkc65axaph2qhcn9f08edaujlju8uflqpra9sqyz96w7rpqmn48ww',
-     ]
-   },
-   arbitrum: {
-     owners:[
-       '0xc21A1D213f64FeDEA3415737CCe2BE37Eb59be81'
-     ]
-   },
-   solana: {
-     owners:[
-       'BRvrp5WQkVuda1BAxfaBTpbBW4b73GhNp7AW7NcBs596',
-     ]
-   },
+  ripple: {
+    owners: [
+      'rNxBjsC1FsEga35GThyb1KXAyS3kDx8gyv',
+      'rMnR4pWoDW2kUSZ7hxnpy2it9ojEdQwc6s',
+    ]
+  },
+  cardano: {
+    owners: [
+      'addr1q8yrpu5fw3qw62fjezu72pap8munej98zpgmxgrye6rw6nkc65axaph2qhcn9f08edaujlju8uflqpra9sqyz96w7rpqmn48ww',
+    ]
+  },
+  arbitrum: {
+    owners: [
+      '0xc21A1D213f64FeDEA3415737CCe2BE37Eb59be81'
+    ]
+  },
+  solana: {
+    owners: [
+      'BRvrp5WQkVuda1BAxfaBTpbBW4b73GhNp7AW7NcBs596',
+    ]
+  },
+  ftn: {
+    owners: [
+      '0xc21A1D213f64FeDEA3415737CCe2BE37Eb59be81',
+    ]
+  },
+  litecoin: {
+    owners: [
+      'ltc1qy4400xa5r72lsysd7xvjks08r5lrzr5fu0udx7'
+    ]
+  },
 }
 
 module.exports = cexExports(config)
