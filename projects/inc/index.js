590b9080dead891d219eb373853d2469ce926df8
Lucas Carlson <lucas@carlson.net>
2025-02-12 13:22:48 -0800

    Update methodology description for INC token TVL
    - Remove "staked" from the methodology description
    - Clarify that TVL consists of INC tokens in the protocol on PulseChain

--- projects/inc/index.js
+++ projects/inc/index.js
@@ -2,7 +2,7 @@
 const token = '0x2fa878ab3f87cc1c9737fc071108f904c0b0c95d'
 
 module.exports = {
-  methodology: "TVL consists of INC tokens staked in the protocol on PulseChain",
+  methodology: "TVL consists of INC tokens in the protocol on PulseChain",
   pulse: {
     tvl: async (api) => ({
       ["pulse:" + token]: (await api.call({ target: token, abi: "uint256:totalSupply", chain: 'pulse' }))
