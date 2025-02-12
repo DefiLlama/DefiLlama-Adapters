88624aa25098367973de11328eef83350a6c3178
Lucas Carlson <lucas@carlson.net>
2025-02-12 13:14:41 -0800

    Add INC token TVL calculation for PulseChain
    - Create new file projects/inc/index.js
    - Implement TVL calculation using INC token contract address
    - Add methodology description for INC token staking on PulseChain

---
+++ projects/inc/index.js
@@ -0,0 +1,11 @@
+// INC contract address on PulseChain
+const token = '0x2fa878ab3f87cc1c9737fc071108f904c0b0c95d'
+
+module.exports = {
+  methodology: "TVL consists of INC tokens staked in the protocol on PulseChain",
+  pulse: {
+    tvl: async (api) => ({
+      ["pulse:" + token]: (await api.call({ target: token, abi: "uint256:totalSupply", chain: 'pulse' }))
+    })
+  }
+}; // node test.js projects/inc/index.js
