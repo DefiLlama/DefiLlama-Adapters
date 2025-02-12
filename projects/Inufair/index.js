37932c79aa9ec38097b4d422feb8d9c06a30dd5d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-12 21:43:43 +0100

    feat: add deadFrom property to Inufair module

--- projects/Inufair/index.js
+++ projects/Inufair/index.js
@@ -2,3 +2,4 @@ const { uniTvlExport } = require('../helper/unknownTokens');
 
 module.exports.hallmarks=[[1705708800,"Rug Pull"]];
 module.exports = uniTvlExport('zkfair', '0x3582Ccde3F786229CE6Dbd88c5aDb86bF64DAA31')
+module.exports.deadFrom = '2024-01-20';
