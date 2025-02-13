2f4fee5ba13739e3fef577f7d7300b2a76cf6b98
dwusiq <dwusiq@163.com>
2025-02-13 14:14:13 +0800

    Calculate the tvl of drift vault with rpcProxy

--- projects/nxfinance/index.js
+++ projects/nxfinance/index.js
@@ -2,14 +2,14 @@ const { Program } = require("@project-serum/anchor");
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const { getProvider } = require("../helper/solana");
+const { drift: { vaultTvl } } = require("../helper/chain/rpcProxy")
+
 
 const nxIdlV1 = require("./nx-idl-v1.json");
 const nxIdlV2 = require("./nx-idl-v2.json");
-const driftVaults = require("./drift_vaults.json");
 
 const NX_PROGRAM_ADDR_V1 = "EHBN9YKtMmrZhj8JZqyBQRGqyyeHw5xUB1Q5eAHszuMt";
 const NX_PROGRAM_ADDR_V2 = "NXFiKimQN3QSL3CDhCXddyVmLfrai8HK36bHKaAzK7g";
-const DRIFT_VAULT_PROGRAM_ADDR = "vAuLTsyrvSfZRuRB3XgvkPwNGgYSs9YRYymVebLKoxR";
 const NX_JLP_DELTA_NEUTRAL_VAULT_ADDR = "HYHnL9BB3tqSPxkVbdcAn9CAa4hyqNYUh1FwDc4he7aD";
 
 
@@ -28,10 +28,8 @@ async function tvl(api) {
 		for (let { account: pool } of value) 
 			api.add(pool.tokenMint.toBase58(), pool.depositTokens.toString())
 
-
-	const drift_vault_program = new Program(driftVaults, DRIFT_VAULT_PROGRAM_ADDR, provider)
-	const vault_res = await drift_vault_program.account.vault.fetch(NX_JLP_DELTA_NEUTRAL_VAULT_ADDR);
-	api.add(ADDRESSES.solana.USDC, vault_res.netDeposits.toString())
+	const drift_jlp_vault_tvl = await vaultTvl(NX_JLP_DELTA_NEUTRAL_VAULT_ADDR)
+	api.add(ADDRESSES.solana.USDC, drift_jlp_vault_tvl.toString())
 }
 
 module.exports = {
