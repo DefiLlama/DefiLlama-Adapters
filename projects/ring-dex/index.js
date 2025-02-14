c88d4842cff0ad1996845de90f65dc0787d34fc9
TheLongKing <163384296+TheLongKing@users.noreply.github.com>
2025-02-14 00:26:40 -0800

    Update ring protocol.

--- projects/ring-dex/index.js
+++ projects/ring-dex/index.js
@@ -1,3 +1,16 @@
-    const { uniTvlExport } = require('../helper/unknownTokens')
+const { uniTvlExport } = require('../helper/calculateUniTvl')
 
-    module.exports = uniTvlExport('blast', '0x24F5Ac9A706De0cF795A8193F6AB3966B14ECfE6')
+module.exports = {
+    ethereum: {
+        tvl: uniTvlExport('0xeb2A625B704d73e82946D8d026E1F588Eed06416', 'ethereum', false),
+      },
+    blast: {
+      tvl: uniTvlExport('0x24F5Ac9A706De0cF795A8193F6AB3966B14ECfE6', 'blast', false),
+    },
+    base: {
+      tvl: uniTvlExport('0x9BfFC3B30D6659e3D84754cc38865B3D60B4980E', 'base', false),
+    },
+    arbitrum: {
+      tvl: uniTvlExport('0x1246Fa62467a9AC0892a2d2A9F9aafC2F5609442', 'arbitrum', false),
+    },
+}
