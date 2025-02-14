b75058b0cef21692df3ddec7a78d5fe3e1f8f0b3
Jacob Barshaw <bluederpy420@gmail.com>
2025-02-14 15:14:22 -0500

    add borrowed tvl

--- projects/eggsfinance/index.js
+++ projects/eggsfinance/index.js
@@ -1,15 +1,34 @@
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+const { sumTokens2, nullAddress } = require("../helper/unwrapLPs");
 
 const EGGS_TOKEN_CONTRACT = "0xf26Ff70573ddc8a90Bd7865AF8d7d70B8Ff019bC";
 
-async function tvl(time, ethBlock, _b, {api}) {
-  return sumTokens2({ tokens: [nullAddress], owner: EGGS_TOKEN_CONTRACT, api })
+const abi = [
+  {
+    type: "function",
+    name: "getTotalBorrowed",
+    inputs: [],
+    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+    stateMutability: "view",
+  },
+];
+async function borrowed(api) {
+  const borrowed = await api.call({
+    abi: abi[0],
+    target: EGGS_TOKEN_CONTRACT,
+  });
+
+  api.add("0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38", borrowed);
+}
+
+async function tvl(time, ethBlock, _b, { api }) {
+  return sumTokens2({ tokens: [nullAddress], owner: EGGS_TOKEN_CONTRACT, api });
 }
 
 module.exports = {
   methodology:
-    "Calculates the total backing of S in the Eggs contract",
+    "Calculates the total backing of S in the Eggs contract, and total borrowed in S",
   sonic: {
     tvl,
+    borrowed,
   },
 };
