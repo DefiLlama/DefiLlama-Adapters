5d46fd8dd01a2e94d252f5389008882aea2b4fe2
0xngmi <0xngmi@protonmail.com>
2025-02-14 18:49:35 +0000

    remove borrowing from eggs

--- projects/eggsfinance/index.js
+++ projects/eggsfinance/index.js
@@ -1,26 +1,14 @@
+const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+
 const EGGS_TOKEN_CONTRACT = "0xf26Ff70573ddc8a90Bd7865AF8d7d70B8Ff019bC";
 
-const abi = [
-  {
-    type: "function",
-    name: "getBacking",
-    inputs: [],
-    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-    stateMutability: "view",
-  },
-];
-async function tvl(api) {
-  const backingBalance = await api.call({
-    abi: abi[0],
-    target: EGGS_TOKEN_CONTRACT,
-  });
-
-  api.add("0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38", backingBalance);
+async function tvl(time, ethBlock, _b, {api}) {
+  return sumTokens2({ tokens: [nullAddress], owner: EGGS_TOKEN_CONTRACT, api })
 }
 
 module.exports = {
   methodology:
-    "Calculates the total backing and borrowed amount of S in the Eggs contract",
+    "Calculates the total backing of S in the Eggs contract",
   sonic: {
     tvl,
   },
