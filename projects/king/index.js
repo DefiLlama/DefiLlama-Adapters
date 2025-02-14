638978f76a18e0e4cf5474771614ba79c2432cda
dmitriy-woof-software <dmitriy@woof.software>
2025-02-14 13:00:59 -0600

    add swell chain

--- projects/king/index.js
+++ projects/king/index.js
@@ -1,14 +1,22 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
-async function vaultsTvl(api) {
+async function mainnetTVL(api) {
   const totalSupply = await api.call({ target: ADDRESSES.ethereum.KING, abi: 'uint256:totalSupply' });
   api.add(ADDRESSES.ethereum.KING, totalSupply);
+}
+
+async function swellTVL(api) {
+  const totalSupply = await api.call({ target: ADDRESSES.swellchain.KING, abi: 'uint256:totalSupply' });
+  api.add(ADDRESSES.swellchain.KING, totalSupply);
 }
 
 module.exports = {
   methodology: 'TVL received directly from the KING contract address. TVL is calculated base on the underlying assets',
   start: 20927127,
   ethereum: {
-    tvl: vaultsTvl,
+    tvl: mainnetTVL,
   },
+  swellchain: {
+    tvl: swellTVL
+  }
 }
