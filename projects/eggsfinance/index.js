bcd22dbdaccdaac7bb2c5ded34c2b04a0df05c0a
BLUEDERPY <74120282+BLUEDERPY@users.noreply.github.com>
2025-02-14 15:22:39 -0500

    add borrowed tvl (#13526)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/eggsfinance/index.js
+++ projects/eggsfinance/index.js
@@ -1,15 +1,22 @@
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+const { nullAddress } = require("../helper/unwrapLPs");
 
 const EGGS_TOKEN_CONTRACT = "0xf26Ff70573ddc8a90Bd7865AF8d7d70B8Ff019bC";
 
-async function tvl(time, ethBlock, _b, {api}) {
-  return sumTokens2({ tokens: [nullAddress], owner: EGGS_TOKEN_CONTRACT, api })
+
+async function borrowed(api) {
+  const borrowed = await api.call({ abi: 'uint256:getTotalBorrowed', target: EGGS_TOKEN_CONTRACT, });
+  api.addGasToken(borrowed);
+}
+
+async function tvl(api) {
+  return api.sumTokens({ owner: EGGS_TOKEN_CONTRACT, tokens: [nullAddress] });
 }
 
 module.exports = {
   methodology:
-    "Calculates the total backing of S in the Eggs contract",
+    "Calculates the total backing of S in the Eggs contract, and total borrowed in S",
   sonic: {
     tvl,
+    borrowed,
   },
 };
