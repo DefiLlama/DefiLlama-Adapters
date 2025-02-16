fd8c5770683dd91140abd93c55808549ebcd50f6
Reto Habegger <80355117+GlobeBit@users.noreply.github.com>
2025-02-16 09:15:42 +0100

    Update index.js

--- projects/hliquity/index.js
+++ projects/hliquity/index.js
@@ -1,4 +1,10 @@
+const { staking } = require("../helper/staking.js");
 const { getLiquityTvl } = require("../helper/liquity");
+
+// TroveManager holds total system collateral (deposited in HBAR)
+const TROVE_MANAGER_ADDRESS = "0x00000000000000000000000000000000005c9f66";
+const STAKING_ADDRESS = "";
+const HLQT_ADDRESS = "0x00000000000000000000000000000000005c9f70"
 
 module.exports = {
   methodology: 'the amount of locked hbar in the HLiquity protocol',
