1ebc2ec58104ccad2d5569819b8fa269eb444634
0xRLA <rko.larsson@gmail.com>
2025-02-17 15:51:33 +0100

    feat: :ambulance: minor fix

--- projects/liquidity-house/index.js
+++ projects/liquidity-house/index.js
@@ -3,7 +3,7 @@ const { sumTokensExport } = require("../helper/unwrapLPs");
 
 module.exports = {
   methodology: "TVL is the total quantity of USDC held in the contract",
-  etherlink: {
+  etlk: {
     tvl: sumTokensExport({
       owners: ["0x0c532e1e916219007f244e2d8Ef46f8530Ec75DE"],
       tokens: [ADDRESSES.etlk.USDC],
