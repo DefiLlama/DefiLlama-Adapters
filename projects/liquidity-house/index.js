c2ee8e1ffc58aeb77371abac199cffe2865da262
0xRLA <rko.larsson@gmail.com>
2025-02-17 15:36:43 +0100

    feat: :ambulance: fixed a build error

--- projects/liquidity-house/index.js
+++ projects/liquidity-house/index.js
@@ -1,12 +1,12 @@
 const ADDRESSES = require("../helper/coreAssets.json");
-const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
+const { sumTokensExport } = require("../helper/unwrapLPs");
 
 module.exports = {
   methodology: "TVL is the total quantity of USDC held in the contract",
   etherlink: {
     tvl: sumTokensExport({
       owners: ["0x0c532e1e916219007f244e2d8Ef46f8530Ec75DE"],
-      tokens: [ADDRESSES.etherlink.USDC],
+      tokens: [ADDRESSES.etlk.USDC],
     }),
   },
 };
