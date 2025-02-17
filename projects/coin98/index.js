90bf7e29368b58d23f2a78889993842d19993bb6
khiêm <87562184+khiemsoft@users.noreply.github.com>
2025-02-17 11:07:48 +0700

    fix: fix comment tvl

--- projects/coin98/index.js
+++ projects/coin98/index.js
@@ -1,11 +1,10 @@
 const { staking } = require('../helper/staking');
-const { sumTokensExport } = require('../helper/unwrapLPs')
 
 const POWER_POLL_CONTRACT = '0x39C9dB7C1412041d084fED054Fc9318B9F75AcDb'
 const COIN98_CONTRACT = '0x0Fd0288AAAE91eaF935e2eC14b23486f86516c8C'
 
 module.exports['tomochain'] = {
-    tvl: sumTokensExport({ owner: POWER_POLL_CONTRACT, tokens: [ COIN98_CONTRACT ]}),
+    tvl: () => ({}) ,
     staking: staking(POWER_POLL_CONTRACT, COIN98_CONTRACT)
 }
 
