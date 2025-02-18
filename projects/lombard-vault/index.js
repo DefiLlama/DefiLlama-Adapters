0b12c0e37d5cc8d1e1ca8553f40cab9a9c3d4778
Define101 <93603962+Define101@users.noreply.github.com>
2025-02-18 10:52:20 +0000

    add ETH vault to lombard (#13567)

--- projects/lombard-vault/index.js
+++ projects/lombard-vault/index.js
@@ -1,12 +1,15 @@
 const { sumTokensExport } = require('../helper/unwrapLPs');
+
 const vaults = [
   '0x5401b8620E5FB570064CA9114fd1e135fd77D57c', // Lombard DeFi Vault https://www.lombard.finance/app/vault/
-]
+];
+
+const vault1 = ['0x309f25d839a2fe225e80210e110c99150db98aaf'];
 
 module.exports = {
   doublecounted: true,
   ethereum: {
-    tvl: sumTokensExport({ owners: vaults, fetchCoValentTokens: true, tokenConfig: {
+    tvl: sumTokensExport({ owners: [...vaults, ...vault1], fetchCoValentTokens: true, tokenConfig: {
       onlyWhitelisted: false,
     }, resolveUniV3: true,}),
   },
@@ -20,4 +23,4 @@ module.exports = {
       onlyWhitelisted: false,
     }, resolveUniV3: true,}),
   },
-}
+};
