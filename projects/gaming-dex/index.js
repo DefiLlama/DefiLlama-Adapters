876c5ff26866fa409e1a6e49322ea3650d7203b0
phucuong1112 <phucuong1112@gmail.com>
2025-02-18 15:26:25 +0700

    Add GamingDex TVL in OAS (#13562)
    Co-authored-by: oromp1812 <oromp1812@gmail.com>

--- projects/gaming-dex/index.js
+++ projects/gaming-dex/index.js
@@ -4,4 +4,7 @@ module.exports = {
   defiverse: {
     tvl: onChainTvl('0x2FA699664752B34E90A414A42D62D7A8b2702B85', 87620),
   },
+  oas: {
+    tvl: onChainTvl('0xfb6f8FEdE0Cb63674Ab964affB93D65a4a7D55eA', 4522800),
+  }
 };
