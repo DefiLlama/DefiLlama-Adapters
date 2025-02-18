ca9bc7df562ae1307235c8f70c536de74da0b134
Reto Habegger <80355117+GlobeBit@users.noreply.github.com>
2025-02-18 15:11:55 +0100

    Update index.js
    update comment

--- projects/hliquity/index.js
+++ projects/hliquity/index.js
@@ -1,7 +1,7 @@
 const { staking } = require("../helper/staking.js");
 const { getLiquityTvl } = require("../helper/liquity");
 
-// TroveManager holds total system collateral (deposited in HBAR)
+// TroveManager holds total system collateral (deposited HBAR)
 const TROVE_MANAGER_ADDRESS = "0x00000000000000000000000000000000005c9f66";
 const STAKING_ADDRESS = "0x00000000000000000000000000000000005c9f30";
 const HLQT_ADDRESS = "0x00000000000000000000000000000000005c9f70"
