34b0bde8d98d781e21d2d8d77bedaebc56be23ad
Reto Habegger <80355117+GlobeBit@users.noreply.github.com>
2025-02-18 15:24:24 +0100

    Added staking metrics, HLQT Address and start to HLiquity adapter

--- projects/hliquity/index.js
+++ projects/hliquity/index.js
@@ -7,7 +7,7 @@ const STAKING_ADDRESS = "0x00000000000000000000000000000000005c9f30";
 const HLQT_ADDRESS = "0x00000000000000000000000000000000005c9f70"
 
 module.exports = {
-  start: '2024-06-05',  //start date when protocol went live
+  start: '2024-06-05',  //start date; when protocol went live
   methodology: 'Total deposits of HBAR for borrowed HCHF',
   hedera: {
     tvl: getLiquityTvl(TROVE_MANAGER_ADDRESS),
