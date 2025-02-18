f73e17f277d475ce1a5e97708d1038371ec0fd77
Reto Habegger <80355117+GlobeBit@users.noreply.github.com>
2025-02-18 14:51:40 +0100

    Update index.js
    hlqtStaking contract added

--- projects/hliquity/index.js
+++ projects/hliquity/index.js
@@ -3,7 +3,7 @@ const { getLiquityTvl } = require("../helper/liquity");
 
 // TroveManager holds total system collateral (deposited in HBAR)
 const TROVE_MANAGER_ADDRESS = "0x00000000000000000000000000000000005c9f66";
-const STAKING_ADDRESS = "";
+const STAKING_ADDRESS = "0x00000000000000000000000000000000005c9f30";
 const HLQT_ADDRESS = "0x00000000000000000000000000000000005c9f70"
 
 module.exports = {
