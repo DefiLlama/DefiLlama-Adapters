f1dfa2a4a4fcaf9b1912e7d8a8751691cc27b1f5
Tony Lau <99046505+intrepidcanadian@users.noreply.github.com>
2025-02-18 06:04:32 -0500

    add ginsengswap (#13561)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ projects/ginsengswap/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  conflux: { factory: '0xD6AdEBbD979E5D080e2d5167Bd80eba7E951FA3F', fromBlock: 114865709 },
+})
