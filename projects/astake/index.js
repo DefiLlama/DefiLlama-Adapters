8572d97ac74edfcf3c672c2afb772b3d5a7dbd46
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-20 18:30:04 +0100

    feat: update Astar TVL calculation to use ERC4626 vaults and add token mapping

--- projects/astake/index.js
+++ projects/astake/index.js
@@ -1,37 +1,8 @@
-const sdk = require("@defillama/sdk");
-
-const VAULT_ADDRESS = "0x0DC6E8922ac0ECa8287ba22Db14C9Ac9317ed18F"  
-const ASTR_TOKEN = "0x0000000000000000000000000000000000000000" 
-const CHAIN = "astar"
-
-async function tvl(timestamp, block, chainBlocks) {
-  const totalAssets = await sdk.api.abi.call({
-    target: VAULT_ADDRESS,
-    abi: {
-      "inputs": [],
-      "name": "totalAssets",
-      "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    chain: CHAIN,
-    block: chainBlocks[CHAIN]
-  });
-
-  const balances = {};
-  
-  sdk.util.sumSingleBalance(
-    balances,
-    `${CHAIN}:${ASTR_TOKEN}`, 
-    totalAssets.output
-  );
-
-  return balances;
-}
-
+const { sumERC4626VaultsExport } = require('../helper/erc4626')
+const VAULT_ADDRESS = "0x0DC6E8922ac0ECa8287ba22Db14C9Ac9317ed18F"
 module.exports = {
   methodology: "Calculates the total amount of ASTR tokens deposited in the ERC4626 vault",
   astar: {
-    tvl
+    tvl: sumERC4626VaultsExport({ vaults: [VAULT_ADDRESS], isOG4626: true}),
   },
 }
