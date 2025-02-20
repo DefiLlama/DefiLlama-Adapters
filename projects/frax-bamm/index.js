8ef9a5a14630bb4b87eb4baa64eb8b1c56f656c1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-20 21:31:33 +0100

    track borrowed

--- projects/frax-bamm/index.js
+++ projects/frax-bamm/index.js
@@ -21,6 +21,17 @@ Object.keys(config).forEach(chain => {
       const pairs = await api.multiCall({ abi: 'address:pair', calls: bamms })
       const ownerTokens = bamms.map((v, i) => [[token0s[i], token1s[i], pairs[i]], v])
       return sumUnknownTokens({ api, ownerTokens, useDefaultCoreAssets: true, lps: pairs })
+    },
+    borrowed: async (api) => {
+      const bamms = await api.fetchList({ lengthAbi: 'bammsLength', itemAbi: 'bamms', target: factory })
+      const pairs = await api.multiCall({ abi: 'address:pair', calls: bamms })
+      const sqrtRenteds = await api.multiCall({ abi: 'uint256:sqrtRented', calls: bamms })
+      const rentedMultipliers = await api.multiCall({ abi: 'uint256:rentedMultiplier', calls: bamms })
+      pairs.forEach((pair, i) => {
+        const balance = sqrtRenteds[i] * rentedMultipliers[i] / 1e18
+        api.add(pair, balance)
+      })
+      return sumUnknownTokens({ api, useDefaultCoreAssets: true, lps: pairs, resolveLP: true, })
     }
   }
 })
