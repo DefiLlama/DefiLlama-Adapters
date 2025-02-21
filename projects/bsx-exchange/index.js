13617b7b399b47fc9c8a6c6d38287ab5eb8175a8
khoa <khoa@bsx.exchange>
2025-02-21 04:30:11 -0500

    add: staking tvl to bsx tvl

--- projects/bsx-exchange/index.js
+++ projects/bsx-exchange/index.js
@@ -1,16 +1,22 @@
 const { nullAddress } = require("../helper/tokenMapping")
 
 const config = {
-  base: '0x26a54955a5fb9472d3edfeac9b8e4c0ab5779ed3',
+  base: {
+    'exchange':'0x26a54955a5fb9472d3edfeac9b8e4c0ab5779ed3',
+    'degen':'0x797d6f745F691133cE90438e1Ba3eeEb16e4b5B5',
+    'staking':'0xE5E10Bf64CD32218CdbF501914B7A0d181934930',
+  }
 }
 
 Object.keys(config).forEach(chain => {
-  const exchange = config[chain]
+  const { exchange, degen, staking } = config[chain]
   module.exports[chain] = {
     tvl: async (api) => {
       const tokens = await api.call({ abi: 'address[]:getSupportedTokenList', target: exchange })
       tokens.push(nullAddress)
-      return api.sumTokens({ owner: exchange, tokens, blacklistedTokens: ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'] })
+
+      const owners = [exchange, degen, staking]
+      return api.sumTokens({ owners, tokens, blacklistedTokens: ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'] })
     }
   }
 })
