bdfcf3ee8f1b62077af886b83963d7c844a8c94a
BeratlasLabs <beratlasfinance@gmail.com>
2025-02-21 11:43:22 +0000

    feat: update batlas contract

--- projects/beratlas-finance/index.js
+++ projects/beratlas-finance/index.js
@@ -1,6 +1,6 @@
 const { nullAddress } = require("../helper/unwrapLPs");
 
-const BATLAS_TOKEN_CONTRACT = "";
+const BATLAS_TOKEN_CONTRACT = "0x5E571727c7aD0c062D009F0Fe7c17f93a02da158";
 
 
 async function borrowed(api) {
@@ -15,7 +15,7 @@ async function tvl(api) {
 module.exports = {
   methodology:
     "Calculates the total backing of Bera in the Batlas contract, and total borrowed in Bera",
-  BreakingChangeType: {
+  berachain: {
     tvl,
     borrowed,
   },
