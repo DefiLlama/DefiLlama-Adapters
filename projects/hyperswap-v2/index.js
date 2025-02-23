d8edd2319fde6590648df054030f8ac5c680403b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-23 22:28:20 +0100

    track hyper evm apps #13635

---
+++ projects/hyperswap-v2/index.js
@@ -0,0 +1,4 @@
+const { uniTvlExports } = require('../helper/unknownTokens')
+module.exports = uniTvlExports({
+  'hyperliquid': '0x724412C00059bf7d6ee7d4a1d0D5cd4de3ea1C48'
+})
---
+++ projects/hyperswap-v3/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  hyperliquid: { factory: '0xB1c0fa0B789320044A6F623cFe5eBda9562602E3', fromBlock: 1, },
+})
