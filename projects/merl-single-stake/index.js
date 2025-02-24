63b75bafb3d56b5e1ee9972f7d9a757585bd36ac
turingczz <920886811@163.com>
2025-02-24 16:38:18 +0800

    feat: add merl-single-stake

---
+++ projects/merl-single-stake/index.js
@@ -0,0 +1,9 @@
+const { staking } = require('../helper/staking')
+const MERL_SINGLE_STAKE_CONTRACT = '0x641485BE8efb0a13F151c9C324f3681907f10d59'
+const MERL_TOKEN = '0x5c46bFF4B38dc1EAE09C5BAc65872a1D8bc87378'
+
+module.exports = {
+  merlin: {
+    staking: staking([MERL_SINGLE_STAKE_CONTRACT,], MERL_TOKEN),
+  }
+}
