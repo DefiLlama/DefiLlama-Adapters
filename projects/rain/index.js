577f7e45ff9d3c150536b9c7568b0fe6d0273a2d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-24 12:43:43 +0100

    compress idls

--- projects/rain/index.js
+++ projects/rain/index.js
@@ -5,13 +5,10 @@ const { getProvider } = require("../helper/solana");
 const bankIdl = require('./bank-idl');
 const defiIdl = require('./defi-idl');
 
-const BANK_PROGRAM_ID = 'rain2M5b9GeFCk792swkwUu51ZihHJb3SUQ8uHxSRJf';
-const DEFI_PROGRAM_ID = 'rDeFiHPjHZRLiz4iBzMw3zv6unZs4VwdU6qQcVd3NSK';
-
 async function tvl(api) {
   const provider = getProvider();
-  const bankProgram = new Program(bankIdl, BANK_PROGRAM_ID, provider);
-  const defiProgram = new Program(defiIdl, DEFI_PROGRAM_ID, provider);
+  const bankProgram = new Program(bankIdl, provider);
+  const defiProgram = new Program(defiIdl, provider);
 
   // Get all banks
   const banks = await bankProgram.account.bank.all();
@@ -43,6 +40,41 @@ async function tvl(api) {
   }
 }
 
+async function borrowed(api) {
+  const provider = getProvider();
+  const bankProgram = new Program(bankIdl, provider);
+  const defiProgram = new Program(defiIdl, provider);
+
+  // Get all banks
+  const banks = await bankProgram.account.bank.all();
+
+  // Get all active defi loans
+  const defiLoans = await defiProgram.account.loan.all([
+    {
+      memcmp: {
+        offset: 8 + 1,
+        bytes: bs58.encode(Buffer.from([0])), // active loans only
+      },
+    },
+  ]);
+
+  // Add bank available liquidity to TVL, availableLiquidity = totalLiquidity - borrowedLiquidity
+  for (const bank of banks) {
+    api.add(
+      bank.account.mint.toString(),
+      bank.account.borrowedLiquidity
+    );
+  }
+
+  // Add defi loan collateral to TVL
+  for (const loan of defiLoans) {
+    api.add(
+      loan.account.principal.toString(),
+      loan.account.borrowedAmount
+    );
+  }
+}
+
 module.exports = {
-  solana: { tvl },
+  solana: { tvl, borrowed, },
 }
