aecae5560858230684d42c9a52751f0cb1e5368e
sean <85156828+xzsean666@users.noreply.github.com>
2025-02-24 15:28:06 +0800

    add astar tvl caculation
    add astar tvl caculation

--- projects/astake/index.js
+++ projects/astake/index.js
@@ -10,7 +10,8 @@ const configs = {
     wstASTRTokenAstar: '0x45cCC0DEA1470F1b4213Ab60fd2eBB2a063f1c24',
     wstASTROracleAstar: '0x3e0fcD6f779775Caec47E999F875C040E1a1DD2F',
     wstASTRTokenSoneium: '0x3b0DC2daC9498A024003609031D973B1171dE09E',
-    wstASTROracleSoneium: '0x5A599251a359Cf27A6A42E7baB1b1494d3919083'
+    wstASTROracleSoneium: '0x5A599251a359Cf27A6A42E7baB1b1494d3919083',
+    ccipPoolAstar: '0x80376eE13282E597ad00B55495e408141fe7DDCB'
 };
 // Fetches TVL for wstASTR value to the API.
 async function getSoneiumTvl(api) {
@@ -22,9 +23,29 @@ async function getSoneiumTvl(api) {
     api.add(ADDRESSES.soneium.ASTAR, stakedAmountInASTR);
 }
 
+// Fetches TVL for wstASTR value to the API.
+async function getAstarTvl(api) {
+  // Fetch and calculate TVL for nsASTR
+  const wstASTRSupply = await api.call({ target: configs.wstASTRTokenAstar, abi: 'erc20:totalSupply' });
+  const ccipPoolBalance = await api.call({ 
+    target: configs.wstASTRTokenAstar, 
+    abi: 'erc20:balanceOf',
+    params: [configs.ccipPoolAstar] 
+  });
+  // ccip lock and realse pool ,total supply - ccip pool balance = supply wstASTR
+  const supplyAstar = wstASTRSupply-ccipPoolBalance;
+  const wstASTRRateData = await api.call({ target: configs.wstASTROracleAstar, abi: abis.latestRoundData });
+  const ratio = wstASTRRateData.answer / 1e8; 
+  const stakedAmountInASTR = supplyAstar * ratio ; 
+  api.add(ADDRESSES.astar.ASTR, stakedAmountInASTR);
+}
+
 module.exports = {
     soneium: {
         tvl: getSoneiumTvl
+    },
+    astar: {
+        tvl: getAstarTvl
     },
     methodology: `TVL is calculated by fetching the total supply of wstASTR converting their values into the underlying tokens ASTR using the current conversion rate provided by the respective oracle.`
 };
