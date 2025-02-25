b3f5bc5ce051dafdcfb456e1b80713774d1b571c
Codonyat <48455632+Codonyat@users.noreply.github.com>
2025-02-25 14:04:29 +0100

    SIR Adapter (#13667)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ projects/sir-trading/index.js
@@ -0,0 +1,16 @@
+const VAULT_ADDRESS = '0xB91AE2c8365FD45030abA84a4666C4dB074E53E7';
+const abi = {
+  "paramsById": "function paramsById(uint48 vaultId) view returns ((address debtToken, address collateralToken, int8 leverageTier))"
+}
+
+async function tvl(api) {
+  const res = await api.fetchList({ lengthAbi: 'numberOfVaults', itemAbi: abi.paramsById, target: VAULT_ADDRESS })
+  const tokens = res.map(v => v.collateralToken)
+  return api.sumTokens({ tokens, owner: VAULT_ADDRESS })
+}
+
+module.exports = {
+  methodology: "It queries totalReserves in Vault.sol for each collateral token",
+  start: 21888482,
+  ethereum: { tvl }
+};
