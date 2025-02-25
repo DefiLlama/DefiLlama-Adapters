0fdf333bd5f61f1242934c57689de4760342e6ff
Leo <151592921+lyl150@users.noreply.github.com>
2025-02-25 10:42:59 +0800

    Update index.js

--- projects/savmswap/index.js
+++ projects/savmswap/index.js
@@ -1,4 +1,3 @@
 const { uniTvlExport } = require('../helper/unknownTokens')
 
 module.exports = uniTvlExport('svm', '0x1842c9bD09bCba88b58776c7995A9A9bD220A925')
-module.exports = uniTvlExport('mint', '0xFd24287551068981737F64ab5f4054DA27F01833')
