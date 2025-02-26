f00074e7961d43adb91309efaf27fac1f29460ce
turingczz <920886811@163.com>
2025-02-26 11:38:55 +0800

    feat: check tvl

--- projects/merl-single-staking/index.js
+++ projects/merl-single-staking/index.js
@@ -1,9 +1,8 @@
 const { staking } = require('../helper/staking')
-const MERL_SINGLE_STAKE_CONTRACT = '0x641485BE8efb0a13F151c9C324f3681907f10d59'
+const MERL_SINGLE_CONTRACT = '0x641485BE8efb0a13F151c9C324f3681907f10d59'
 const MERL_TOKEN = '0x5c46bFF4B38dc1EAE09C5BAc65872a1D8bc87378'
 module.exports = {
     merlin: {
-        tvl: staking([MERL_SINGLE_STAKE_CONTRACT,], MERL_TOKEN),
-        staking: staking([MERL_SINGLE_STAKE_CONTRACT,], MERL_TOKEN,),
+        tvl: staking([MERL_SINGLE_CONTRACT,], MERL_TOKEN),
     }
 }
