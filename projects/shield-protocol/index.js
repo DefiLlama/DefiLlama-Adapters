5532d963548f77128cb53836670cce8889e105d3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-26 12:08:58 +0100

    refactor

--- projects/shield-protocol/index.js
+++ projects/shield-protocol/index.js
@@ -1,13 +1,8 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const Abis = require("./abi.json");
-const sdk = require("@defillama/sdk");
-const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
-const { staking } = require("../helper/staking");
-const { sumTokensExport, nullAddress, } = require('../helper/unknownTokens')
- 
-const Contracts = { 
+const { sumTokensExport, } = require('../helper/unknownTokens')
+
+const Contracts = {
   bsc: {
-    wbnb: ADDRESSES.bsc.WBNB,
     shield: "0xD9E90DF21F4229249E8841580cDE7048bF935710",
     bank: "0xcc40896e6d0C8Dd93Bd9DFaF11118B338015EcD4",
     multiFeeDistribution: "0x36D7fa1C701aAA811F8736C40435C50Bb77BF843",
@@ -19,87 +14,15 @@ const Contracts = {
   },
 };
 
-async function calcTvl(timestamp, ethBlock, chainBlocks) {
-  const block = chainBlocks.bsc;
-  const chain = "bsc";
-
-  const bankBalance = await sdk.api.abi.call({
-    target: Contracts.bsc.bank,
-    abi: Abis.bank.usableCollateralBalance,
-    chain: chain,
-    block,
-  });
-
-  return {
-    [`bsc:${Contracts.bsc.wbnb}`]:
-      +bankBalance.output,
-  };
-}
-
-async function calcStakingTvl(timestamp, ethBlock, chainBlocks) {
-  const block = chainBlocks.bsc;
-  const chain = "bsc";
-
-  const stakingData = await sdk.api.abi.call({
-    target: Contracts.bsc.multiFeeDistribution,
-    abi: Abis.multiFeeDistribution.totalSupply,
-    chain: chain,
-    block,
-  });
-
-
-  return {
-    [`bsc:${Contracts.bsc.shield}`]: stakingData.output,
-  };
-}
-
-async function calcPool2(masterchef, lps, block, chain) {
-  let balances = {};
-  const lpBalances = (
-    await sdk.api.abi.multiCall({
-      calls: lps.map((p) => ({
-        target: p,
-        params: masterchef,
-      })),
-      abi: "erc20:balanceOf",
-      block,
-      chain,
-    })
-  ).output;
-  let lpPositions = [];
-  lpBalances.forEach((p) => {
-    lpPositions.push({
-      balance: p.output,
-      token: p.input.target,
-    });
-  });
-  
-  await unwrapUniswapLPs(
-    balances,
-    lpPositions,
-    block,
-    chain,
-    (addr) => `${chain}:${addr}`
-  );
-  return balances;
-}
-
-async function bscPool2(timestamp, block, chainBlocks) {
-  const farm = await calcPool2(
-    Contracts.bsc.chef,
-    Contracts.bsc.lps,
-    chainBlocks.bsc,
-    "bsc"
-  );
-
-
-  return { ...farm };
+async function staking(api) {
+  const bal = await api.call({  abi: 'uint256:totalSupply', target: Contracts.bsc.multiFeeDistribution})
+  api.add(Contracts.bsc.shield, bal)
 }
 
 module.exports = {
   bsc: {
-    tvl: calcTvl,
-    pool2: sumTokensExport({ owner: Contracts.bsc.chef, tokens: Contracts.bsc.lps, useDefaultCoreAssets: true, lps: Contracts.bsc.lps}),
-    staking: calcStakingTvl,
+    tvl: sumTokensExport({ owner: Contracts.bsc.bank, tokens: [ADDRESSES.bsc.WBNB] }),
+    pool2: sumTokensExport({ owner: Contracts.bsc.chef, tokens: Contracts.bsc.lps, useDefaultCoreAssets: true, }),
+    staking,
   },
 };
