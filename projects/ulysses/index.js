80a7994d190dfaff910a411290816b5d8b92dc65
waynebruce0x <waynebruce0x@gmail.com>
2025-02-26 09:35:53 +0000

    Ulysses Sonic Assets (#13673)

--- projects/ulysses/index.js
+++ projects/ulysses/index.js
@@ -18,7 +18,18 @@ Object.keys(ports).forEach(chain => module.exports[chain] = { tvl: sumTokensExpo
 
 const chainsWithoutCovalentSupport = {
   metis: { tokens: Object.values(ADDRESSES.metis).concat([ADDRESSES.null]) },
-  sonic: { tokens: Object.values(ADDRESSES.sonic).concat([ADDRESSES.null, wS]) }
+  sonic: { tokens: Object.values(ADDRESSES.sonic).concat([
+      ADDRESSES.null,
+      wS, 
+      "0x4D85bA8c3918359c78Ed09581E5bc7578ba932ba",
+      "0x455d5f11Fea33A8fa9D3e285930b478B6bF85265",
+      "0xE51EE9868C1f0d6cd968A8B8C8376Dc2991BFE44",
+      "0x9fDbC3f8Abc05Fa8f3Ad3C17D2F806c1230c4564",
+      "0x79bbF4508B1391af3A0F4B30bb5FC4aa9ab0E07C",
+      "0xE5DA20F15420aD15DE0fa650600aFc998bbE3955",
+      "0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38"
+    ]) 
+  }
 }
 
 Object.keys(chainsWithoutCovalentSupport).forEach(chain => {
