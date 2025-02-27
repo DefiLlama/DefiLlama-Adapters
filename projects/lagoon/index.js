2fcd6ca6b4564df488f482e994f452a65d30a9fd
gdupont <gdupont.pro@gmail.com>
2025-02-27 15:59:16 +0100

    add a vault to lagoon protocol (#13694)

--- projects/lagoon/index.js
+++ projects/lagoon/index.js
@@ -6,7 +6,8 @@ const config = {
     lvTokens: {
       totalAssets: [
         "0x07ed467acD4ffd13023046968b0859781cb90D9B", // 9Summits Flagship ETH
-        "0x03D1eC0D01b659b89a87eAbb56e4AF5Cb6e14BFc", 
+        "0x03D1eC0D01b659b89a87eAbb56e4AF5Cb6e14BFc", // 9Summits Flagship USDC
+        "0xB09F761Cb13baCa8eC087Ac476647361b6314F98", // 9Summits & Tulipa Capital cbBTC 
       ],
     },
   },
