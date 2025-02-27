6fe56e25dfb82e986c28a20d5d18a6138e1139d6
fbslo <admin@fbslo.net>
2025-02-27 02:51:02 +0100

    Prepare

--- projects/hyperlend/index.js
+++ projects/hyperlend/index.js
@@ -1,18 +1,5 @@
-const { aaveChainTvl, aaveExports } = require('../helper/aave');
-const methodologies = require('../helper/methodologies');
-const { mergeExports } = require('../helper/utils');
+const { aaveExports } = require('../helper/aave');
 
-function v3(chain) {
-  let params = ["0xF38A8A25DafdCFb5126008ed1f9f2333C3129c93", undefined, ["0x824A4309686C74C3369Ab2273A6f2ced629422e2"]]
-  const section = borrowed => aaveChainTvl(chain, ...params, borrowed, true);
-  return {
-    tvl: section(false),
-    borrowed: section(true)
-  }
-}
-
-module.exports = mergeExports({
-  methodology: methodologies.lendingMarket,
-  arbitrum: v3("arbitrum"),
-});
-// node test.js projects/hyperlend/index.js
+module.exports = {
+  hyperliquid: aaveExports('hyperliquid', 'PoolAddressesProvider', undefined, ["protocolDataProvider"], { v3: true})
+};
