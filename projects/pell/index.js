cba2e636be6cd7b8b60ee663cf9cd87280fb28b7
realdealshaman <realdealshaman11@gmail.com>
2025-02-27 15:49:55 +0000

    fix adapter

--- projects/pell/index.js
+++ projects/pell/index.js
@@ -9,6 +9,9 @@ chains.forEach(chain => {
       if (api.chain === 'corn') {
         api.chainId = 210000
       }
+      if (api.chain === 'hemi') {
+        api.chainId = 43111
+      }
       const { result } = await getConfig(`pell/${api.chain}`, `https://api.pell.network/v1/stakeList?chainId=${api.chainId}`)
       const vaults = result.map(f => f.strategyAddress)
       const tokens = await api.multiCall({ abi: 'address:underlyingToken', calls: vaults })
