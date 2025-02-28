ca6264701b7fb3a3c6995ec5c661a0e1ebe3cb3a
Maxim Cojocari <cojocarigoncearmax@gmail.com>
2025-02-28 11:09:50 +0200

    refine TVL calculations

--- projects/tea-fi/index.js
+++ projects/tea-fi/index.js
@@ -1,34 +1,46 @@
-const { stakings } = require("../helper/staking");
-const coreAssets = require("../helper/coreAssets.json");
+const { SYNTHETIC_TOKENS, SYNTHETIC_STAKING } = require("./constants")
 
-const SYNTHETIC_TOKENS_ETH = [
-  coreAssets.ethereum.tUSDT,
-  coreAssets.ethereum.tPOL,
-  coreAssets.ethereum.tUSDC,
-  coreAssets.ethereum.tSOL,
-  coreAssets.ethereum.tWBTC,
-  coreAssets.ethereum.tWETH
-]
-const SYNTHETIC_TOKENS_POL = [
-  coreAssets.polygon.tUSDT,
-  coreAssets.polygon.tWPOL,
-  coreAssets.polygon.tUSDC,
-  coreAssets.polygon.tSOL,
-  coreAssets.polygon.tWBTC,
-  coreAssets.polygon.tWETH
-]
-
-const SYNTHETIC_STAKING_ETH = '0xd491C1A7E3C6AF6c2A8f8e3EF54E50044F3BA9AA';
-const SYNTHETIC_STAKING_POL = '0xBeB9503dDBDA15aE0D19cf817028C19077f0e99E';
+const stakings = async (api) => {
+  const { chain } = api
+  const synthTokens = Object.values(SYNTHETIC_TOKENS[chain])
+  const synthBalances = await api.multiCall({
+    calls: synthTokens.map(token => ({
+      target: token,
+      params: [SYNTHETIC_STAKING[chain]]
+    })),
+    abi: 'erc20:balanceOf',
+  })
+  const underlyingAssets = await api.multiCall({
+    calls: synthTokens.map(token => ({
+      target: token,
+      params: []
+    })),
+    abi: 'address:underlyingAsset',
+  })
+  const underlyingAssetDecimals = await api.multiCall({
+    calls: underlyingAssets.map(token => ({
+      target: token,
+      params: []
+    })),
+    abi: 'erc20:decimals',
+  })
+  const correctedBalances = synthBalances.map((balance, index) => 
+    BigInt(underlyingAssetDecimals[index]) < 18n ?
+      BigInt(balance) / 10n ** (18n - BigInt(underlyingAssetDecimals[index])) :
+      BigInt(balance) * 10n ** (BigInt(underlyingAssetDecimals[index]) - 18n)
+  )
+  
+  api.add(underlyingAssets, correctedBalances)
+}
 
 module.exports = {
-  methodology: "TVL represents the overall liquidity held across all synthetic asset staking pools on a given chain.",
+  methodology: "TVL is the total value of the assets that back synthetic tokens in staking pools on a specific chain.",
   ethereum: {
     tvl: () => ({}),
-    staking: stakings([SYNTHETIC_STAKING_ETH], SYNTHETIC_TOKENS_ETH, "ethereum"),
+    staking: stakings,
   },
   polygon: {
     tvl: () => ({}),
-    staking: stakings([SYNTHETIC_STAKING_POL], SYNTHETIC_TOKENS_POL, "polygon"),
+    staking: stakings,
   }
 };
