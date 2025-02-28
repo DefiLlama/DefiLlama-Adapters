6069bd7673b695d59ef5e342611cc4ed851f33b2
ONFA Dev <dev@onfa.io>
2025-02-28 18:18:21 +0700

    Delete projects/onfa-usdt/index.js

--- projects/onfa-usdt/index.js
+++
@@ -1,8 +0,0 @@
-const { sumTokensExport } = require('../helper/unwrapLPs')
-const ADDRESSES = require('../helper/coreAssets.json')
-
-module.exports = {
-  onfa: {
-    tvl: sumTokensExport({ owner: '0x2CD63B34B308f379c18852aB294389eE26D6C5FA', tokens: [ADDRESSES.onfa.USDT]})
-  }
-}
