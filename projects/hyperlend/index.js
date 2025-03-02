cca385628d258287f9ae8fb421b7afb5464dc0b6
fbslo <admin@fbslo.net>
2025-03-02 22:41:42 +0100

    feat: add hyperlend mainnet contracts

--- projects/hyperlend/index.js
+++ projects/hyperlend/index.js
@@ -1,5 +1,5 @@
 const { aaveExports } = require('../helper/aave');
 
 module.exports = {
-  hyperliquid: aaveExports('hyperliquid', 'PoolAddressesProvider', undefined, ["protocolDataProvider"], { v3: true})
+  hyperliquid: aaveExports('hyperliquid', '0x0b73a95552cBb12Ce6829Deb9DC734A0Ec3Dbb62', undefined, ["0x9C3f02498ae7150369cD35Fa7786Cb99F029F623"], { v3: true})
 };
