570fbab4738d58cf86d995729c9474515fa7f259
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-03 08:40:25 +0100

    update phemex

--- projects/helper/bitcoin-book/phemex.js
+++ projects/helper/bitcoin-book/phemex.js
@@ -1,3 +1,5 @@
 module.exports = [
-  "3PiGxVdpMjWSsH8X8BypdwcsmPW5cmE4Ta"
+  "3PiGxVdpMjWSsH8X8BypdwcsmPW5cmE4Ta",
+  "bc1qw6mntl39vdtle2fczl8dzds43x02z556yp3249",
+  "39rotuMW6jxjPrZ3Fn7DJJkPZZstWZh6ck",
 ]
