bab723d7e684a83628fb0296af397d37abeedbda
ONFA Dev <dev@onfa.io>
2025-03-03 16:19:14 +0700

    Update index.js

--- projects/onfa/index.js
+++ projects/onfa/index.js
@@ -3,6 +3,6 @@ const ADDRESSES = require('../helper/coreAssets.json')
 
 module.exports = {
   onfa: {
-    tvl: sumTokensExport({ owner: '0x2CD63B34B308f379c18852aB294389eE26D6C5FA', tokens: [ADDRESSES.onfa.USDT]})
+    tvl: sumTokensExport({ owner: '0xd31985Bf78a6Df49d3A063A99361a48eAc83595e', tokens: [ADDRESSES.onfa.OFT]})
   }
 }
