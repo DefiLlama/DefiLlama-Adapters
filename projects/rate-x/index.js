16e6f281dcd551e139a3c2e4169ad95761c5da27
tony.y <tony.y@rate-x.io>
2025-03-04 00:13:39 +0800

    update jto rate

--- projects/rate-x/index.js
+++ projects/rate-x/index.js
@@ -2,6 +2,18 @@ const { sumTokens2 } = require("../helper/solana");
 const { PublicKey } = require("@solana/web3.js");
 const { getConnection } = require("../helper/solana");
 
+async function getJtoRate(connection, key) {
+  const vault = key === 'solana:kyJtowDDACsJDm2jr3VZdpCA6pZcKAaNftQwrJ8KBQP' 
+    ? new PublicKey("ABsoYTwRPBJEf55G7N8hVw7tQnDKBA6GkZCKBVrjTTcf")
+    : new PublicKey("BmJvUzoiiNBRx3v2Gqsix9WvVtw8FaztrfBHQyqpMbTd");
+  
+  const accountInfo = await connection.getAccountInfo(vault);
+  const vrtSupply = Number(accountInfo.data.readBigUInt64LE(104));
+  const tokensDeposited = Number(accountInfo.data.readBigUInt64LE(112));
+  
+  return tokensDeposited / vrtSupply;
+}
+
 async function tvl(api) {
   const connection = getConnection();
   const lookupTableAddress = new PublicKey("eP8LuPmLaF1wavSbaB4gbDAZ8vENqfWCL5KaJ2BRVyV");
@@ -28,10 +40,11 @@ async function tvl(api) {
       key === 'solana:WFRGJnQt5pK8Dv4cDAbrSsgPcmboysrmX3RYhmRRyTR') {
       // kyjto, wfragjto => jto
       const targetKey = 'solana:jtojtomepa8beP8AuQc6eXt5FriJwfFMwQx2v2f9mCL';
+      const jtoRate = await getJtoRate(connection,key)
       if (targetKey in resModified) {
-          resModified[targetKey] = parseInt(resModified[targetKey], 10) + parseInt(res[key], 10);
+          resModified[targetKey] = parseInt(resModified[targetKey], 10) + Math.floor(Number(res[key]) * jtoRate);
       } else {
-          resModified[targetKey] = res[key];
+          resModified[targetKey] = Math.floor(Number(res[key]) * jtoRate);
       }
   }  else if (key === 'solana:BenJy1n3WTx9mTjEvy63e8Q1j4RqUc6E4VBMz3ir4Wo6') {
       // usd* => usdc
