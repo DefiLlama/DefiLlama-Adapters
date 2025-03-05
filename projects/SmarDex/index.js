3e2f11c6a1cecf77843253069541b318d37a77ef
julien <julien.chiapp@gmail.com>
2025-03-05 13:09:45 +0100

    fix: derive USDN TVL with wstETH locked in protocol

--- projects/SmarDex/index.js
+++ projects/SmarDex/index.js
@@ -2,7 +2,6 @@ const sdk = require("@defillama/sdk");
 const { getUniTVL } = require("../helper/unknownTokens");
 const { stakings } = require("../helper/staking");
 const { usdnProtocolAbi, rebalancerAbi } = require("./abis");
-const { fetchURL } = require("../helper/utils");
 
 const config = {
   bsc: "0xA8EF6FEa013034E62E2C4A9Ec1CDb059fE23Af33",
@@ -13,27 +12,11 @@ const config = {
 
 const ethereumFactory = "0xB878DC600550367e14220d4916Ff678fB284214F";
 const USDN_PROTOCOL_ADDRESS = "0x656cB8C6d154Aad29d8771384089be5B5141f01a";
-const USDN_TOKEN_ADDRESS = "0xde17a000ba631c5d7c2bd9fb692efea52d90dee2";
 const WSTETH_TOKEN_ADDRESS = "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0";
 const REBALANCER_ADDRESS = "0xaebcc85a5594e687f6b302405e6e92d616826e03";
 
-const getWstEthPriceAtTimestamp = async (chain, timestamp) => {
-  const prices = (
-    await fetchURL(`https://coins.llama.fi/prices/historical/${timestamp}/${chain}:${WSTETH_TOKEN_ADDRESS}`)
-  ).data.coins;
-  const wstETHResult = prices[`${chain}:${WSTETH_TOKEN_ADDRESS}`];
-  if (!wstETHResult) {
-    throw new Error("No price data found for wstETH");
-  }
-  return wstETHResult.price;
-};
-
 async function fetchUSDNData(api, block) {
-  const { timestamp, chain } = api;
-
-  const wstEthPrice = await getWstEthPriceAtTimestamp(chain, timestamp);
-
-  const formattedWstEthPrice = BigInt(Math.round(wstEthPrice * 10 ** 18));
+  const { chain } = api;
 
   const getBalanceVaultCall = sdk.api.abi.call({
     target: USDN_PROTOCOL_ADDRESS,
@@ -66,17 +49,13 @@ async function fetchUSDNData(api, block) {
     getBalanceVault: BigInt(balanceVault.output),
     getBalanceLong: BigInt(balanceLong.output),
     rebalancerPendingAssets: BigInt(rebalancerCurrentStateData.output[0]),
-    wstEthPrice: formattedWstEthPrice,
   };
 }
 
 const getEthereumTVL = async (api, block, chainBlocks) => {
   const usdnData = await fetchUSDNData(api, block);
 
-  const usdnTVL = Number(
-    ((usdnData.getBalanceLong + usdnData.getBalanceVault + usdnData.rebalancerPendingAssets) * usdnData.wstEthPrice) /
-      BigInt(10 ** 18)
-  ).toExponential();
+  const usdnWstEthLocked = Number(usdnData.getBalanceLong + usdnData.getBalanceVault + usdnData.rebalancerPendingAssets);
 
   const uniTVL = await getUniTVL({ factory: ethereumFactory, fetchBalances: true, useDefaultCoreAssets: false })(
     api,
@@ -86,7 +65,7 @@ const getEthereumTVL = async (api, block, chainBlocks) => {
 
   return {
     ...uniTVL,
-    [`ethereum:${USDN_TOKEN_ADDRESS}`]: usdnTVL,
+    [`ethereum:${WSTETH_TOKEN_ADDRESS}`]: usdnWstEthLocked,
   };
 };
 
