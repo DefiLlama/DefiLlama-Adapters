6b116245e67c1723d42151cea014ad22c4e92d23
Dmitry Green <dmitriy.zelenyi96@gmail.com>
2025-03-05 08:39:17 +0800

    Cleanups in Binaryx Platform adapter

--- projects/binaryx-platform/index.js
+++ projects/binaryx-platform/index.js
@@ -4,7 +4,6 @@ const propertyFactoryAddress = '0x5d618C67674945081824e7473821A79E4ec0970F';
 const priceOracleAddress = '0x551C261eFcf109378D101de9A2741FB8078Abf45';
 const offPlanFactoryAddress = '0x2718fe8eEB091301d1f3D367231aFfE95C2f68Fe';
 const offPlanServiceAddress = '0xe442Aa8dC9D8526d7ccDDF4f3f8369294EAfA9dC';
-const excludedTokens = ['0xC478d5C1E7F19D035Ad330bE09cb84eB9582D7F1', '0xd2198dBB407f5405284d0A00eA6624D087b7098b', '0x228ce2B019B5a54C545E61490E5ba66E40915868'].map(i => i.toLowerCase());
 
 async function tvl(api) {
   await calculateRentalPropertiesTVL(api);
@@ -12,22 +11,9 @@ async function tvl(api) {
 }
 
 async function calculateRentalPropertiesTVL(api) {
-  const rentalAssets = (await api.call({ 
-    target: propertyFactoryAddress, 
-    abi: 'address[]:getAssets'
-  })).filter(address => !excludedTokens.includes(address.toLowerCase()));
-  
-  const rentalSupplies = await api.multiCall({ 
-    abi: 'erc20:totalSupply', 
-    calls: rentalAssets 
-  });
-  
-  const rentalPrices = await api.multiCall({ 
-    abi: 'function latestPrice(address asset) view returns (uint256)', 
-    calls: rentalAssets, 
-    target: priceOracleAddress 
-  });
-  
+  const rentalAssets = (await api.call({ target: propertyFactoryAddress, abi: 'address[]:getAssets' }))
+  const rentalSupplies = await api.multiCall({ abi: 'erc20:totalSupply', calls: rentalAssets });
+  const rentalPrices = await api.multiCall({ abi: 'function latestPrice(address asset) view returns (uint256)', calls: rentalAssets, target: priceOracleAddress });
   rentalSupplies.forEach((supply, i) => {
     const valueInUSDT = (supply / 1e18) * (rentalPrices[i] / 1e6);
     api.add(ADDRESSES.polygon.USDT, valueInUSDT * 1e6);
