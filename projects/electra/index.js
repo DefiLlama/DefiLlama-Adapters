f3e41133c5250d688b28ada5cc4ccce65c9437ea
Denis Serebryansky <denis.serebryansky.eu@gmail.com>
2025-03-10 11:45:16 +0300

    Electra TVL Update

--- projects/electra/index.js
+++ projects/electra/index.js
@@ -4,7 +4,8 @@ const ADDRESSES = require('../helper/coreAssets.json')
 module.exports = {
   timetravel: false,
   bsc: {
-    tvl: sumTokensExport({ owner:'0xfc63831f1C517d196470F03a61afD3d0CC0f7127', tokens: [ADDRESSES.bsc.USDT] })
+    tvl: sumTokensExport({ owner:'0xfc63831f1C517d196470F03a61afD3d0CC0f7127', tokens: [ADDRESSES.bsc.USDT] }) +
+        sumTokensExport({ owner:'0x2212b7DAD2d85176602CaA64F2CBf4a24A6e0a06', tokens: [ADDRESSES.bsc.USDT] }) // ref system
   },
   ethereum: {
     tvl: sumTokensExport({ owner:'0xfc63831f1C517d196470F03a61afD3d0CC0f7127', tokens: [ADDRESSES.ethereum.USDT] })
