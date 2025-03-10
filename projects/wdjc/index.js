c78251ce75514b88c6dfa0b014a788655321cb1d
Artem <artem.v.maluga@gmail.com>
2025-03-10 16:31:34 +0300

    feat: add WDJC project

---
+++ projects/wdjc/index.js
@@ -0,0 +1,21 @@
+const { get_account_tvl } = require("../helper/chain/eos");
+
+// WDJC
+// https://waxdeer.com
+async function wax() {
+  const accounts = ["faw.waxdeers"];
+  const tokens = [
+      ["eosio.token", "WAX", "wax"],
+      ["alien.worlds", "TLM", "alien-worlds"],
+      ["usdt.alcor", "USDT", "usdt-alcor"],
+      ["token.fusion", "LSWAX", "waxfusion-staked-wax"],
+  ];
+  return await get_account_tvl(accounts, tokens, "wax");
+}
+
+module.exports = {
+  methodology: `WDJC TVL is achieved by querying token balances from game smart contract.`,
+  wax: {
+    tvl: wax
+  },
+}
