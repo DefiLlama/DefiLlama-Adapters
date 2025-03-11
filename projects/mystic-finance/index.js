bb3878cdd7b52dc1be918d45967e2d30f7b8b1f3
John Agbanusi <agbanusijohn@gmail.com>
2025-03-11 21:43:27 +0100

    add mystic finance adapter

---
+++ projects/mystic-finance/index.js
@@ -0,0 +1,15 @@
+const {CONFIG } = require("./constants.js")
+const { aaveExports } = require("../helper/aave");
+const methodologies = require("../helper/methodologies");
+
+module.exports.methodology = methodologies.lendingMarket
+
+Object.keys(CONFIG).forEach((chain) => {
+  const poolDatas = CONFIG[chain];
+  module.exports[chain] = aaveExports(chain, undefined, undefined, poolDatas, { v3: true })
+});
+
+
+// module.exports.hallmarks = [
+//   [1773030089, "Start Mystic Rewards"],
+// ]
