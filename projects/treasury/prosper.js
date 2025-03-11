f17f3e9a459fae3658cf6c730dbd53b00f701e14
Prosper <internal@prosper-fi.com>
2025-03-11 12:33:01 +0800

    Update prosper.js

--- projects/treasury/prosper.js
+++ projects/treasury/prosper.js
@@ -1,5 +1,4 @@
 const { nullAddress, treasuryExports } = require("../helper/treasury");
-const { sumTokensExport } = require('../helper/sumTokens');
 const bitcoinAddressBook = require('../helper/bitcoin-book/index.js');
 
 const treasury = "0xb3BbCBd70436c9CAdDf52E2F06732f81DaC1F127";
@@ -8,12 +7,11 @@ const pros_token = "0x915424Ac489433130d92B04096F3b96c82e92a9D"; // PROS token
 
 module.exports = treasuryExports({
   bsc: {
-    tokens: [ 
-        pros_token
-     ],
+    tokens: [ ],
     owners: [treasury],
+    ownTokens: [pros_token],
   },
   bitcoin: {
-    tvl: sumTokensExport({ owners: bitcoinAddressBook.prosper }),
+    owners: bitcoinAddressBook.prosper,
   },
 })
