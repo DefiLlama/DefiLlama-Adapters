4a263d1a8839bebf4f213a676ee07a258c9de61c
Fan Sang <fsang@gatech.edu>
2025-03-12 15:55:36 -0400

    add more enzoBTC TVL addresses

--- projects/helper/bitcoin-book/lorenzo-enzoBTC.js
+++ projects/helper/bitcoin-book/lorenzo-enzoBTC.js
@@ -6,7 +6,9 @@ module.exports = [
   '19KdJBkptNL7RJYgsWFvGr8BqGaeV9xEpg',
   '14eK85UFbR74KADFXkPXKyEDrroiVVGmxX',
 
-  'bc1p8v4wwf883d0f384vcgynrhsyhu6wmx5rtsajxdqjd24x2h22d4kq42cr0z',
-  'bc1px7tzwdf3vy8her3yxeehh7jzps8k4s4w54gfh4uk5klgu7cjk66srh7c2w',
   'bc1pr6pga0d44xm3t8z36qnya6sfznsm8fwkn507x6gqt86xtnvm4h4sj2zqus',
+  'bc1qndzgrwj3y2lhcklme4t72jxq3df2h05vjdgzpp',
+  '19wFRSr3GYHmVQtnmbkx7Wkjw3jZdyYB9a',
+  'bc1q4jnycjnu2322hjk20e56qymu4pwk0kpgds62y0',
+  'bc1qyd4g2r0n0p9u775z7062rz8j88xxy27kmmh5aj'
 ]
