af6bcad86c2655663118c7460f317bf4e18d211e
Austin <austin.jerry@brine.fi>
2025-03-12 15:44:41 +0530

    fix: multipli url

--- projects/multipli/index.js
+++ projects/multipli/index.js
@@ -2,7 +2,7 @@ const axios = require("axios")
 
 
 async function getTvlByChain(chain) {
-    const tvl_response = (await axios.get("https://api.multipli.fi/multipli/v2/external-aggregator/defillama/tvl"))
+    const tvl_response = (await axios.get("https://api.multipli.fi/multipli/v1/external-aggregator/defillama/tvl/"))
     const tvl = tvl_response.data['payload'][chain]
 
     return tvl
