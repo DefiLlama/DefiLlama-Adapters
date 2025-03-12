a75016b7be61e7505cdcfa50f247b2b8c48c8b96
iusserhat <110695840+iusserhat@users.noreply.github.com>
2025-03-12 13:32:54 +0300

    changed token address index.js
    I just realized that we had created our own token on the testnet. Now that we’ve moved to the mainnet, we actually don’t need our own token. I’ve changed the USDT address to the collateral token address. We’ve also updated our smart contracts to work with USDT.

--- projects/credix1/index.js
+++ projects/credix1/index.js
@@ -1,18 +1,19 @@
 const { sumTokens2 } = require('../helper/unwrapLPs');
 const sdk = require("@defillama/sdk");
 
-// Token adresi (Unit0 ağında)
-const token = "0xB13Ae312BC7c45d7120bf8bf317d5f55C36b19dF";
+// USDT token adresi (Unit0 ağında)
+// NOT: Lütfen bu adresi Unit0 ağındaki gerçek USDT adresi ile değiştirin
+const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7"; // Bu Ethereum'daki USDT adresi, Unit0 için güncelleyin
 // Ana dapp kontratı
 const p2p = "0xDCb086519b5776AcBE15EeA5d65FC72498AD110f";
 
 async function tvl(api) {
-  // Token bakiyesini kontrol ediyoruz
-  return sumTokens2({ api, owners: [p2p], tokens: [token] });
+  // USDT bakiyesini kontrol ediyoruz
+  return sumTokens2({ api, owners: [p2p], tokens: [usdt] });
 }
 
 module.exports = {
-  methodology: "TVL is calculated by checking the token balance in the P2P lending contract on Unit0 network.",
+  methodology: "TVL is calculated by checking the USDT balance in the P2P lending contract on Unit0 network.",
   timetravel: false, // Unit0 ağı için timetravel'ı kapatıyoruz
   // start: 1000000, // Projenizin başladığı blok numarasını ekleyebilirsiniz
   unit0: { // Unit0 ağı için
