3b7f9f235b81605408b322edf7a3e24497f4923b
iusserhat <110695840+iusserhat@users.noreply.github.com>
2025-03-12 14:01:38 +0300

    Update index.js
    add usdt address for unit0 network

--- projects/credix1/index.js
+++ projects/credix1/index.js
@@ -2,8 +2,7 @@ const { sumTokens2 } = require('../helper/unwrapLPs');
 const sdk = require("@defillama/sdk");
 
 // USDT token adresi (Unit0 ağında)
-// NOT: Lütfen bu adresi Unit0 ağındaki gerçek USDT adresi ile değiştirin
-const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7"; // Bu Ethereum'daki USDT adresi, Unit0 için güncelleyin
+const usdt = "0xb303d80db8415FD1d3C9FED68A52EEAc9a052671"; // Unit0 ağındaki USDT adresi
 // Ana dapp kontratı
 const p2p = "0xDCb086519b5776AcBE15EeA5d65FC72498AD110f";
 
