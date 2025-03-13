585c04a6c254b8cefd93b6e114709feafdf8114f
alessandroaw <alessandro.ariaw@gmail.com>
2025-03-13 11:58:56 +0700

    feat: calculate tvl based on asset value

--- projects/napier/v2.js
+++ projects/napier/v2.js
@@ -49,50 +49,80 @@ const config = {
 async function tvl(api) {
   const { factory, fromBlock } = config[api.chain];
 
+
   // Get all deployment events
   const deployLogs = await getLogs({
     api,
     target: factory,
+    topic: "0x43a42a2627e2eabb0a72e5fab92586a1ef1fde9b09ca5c0f05b56dc20d977da4",
+    onlyArgs: true,
     eventAbi:
       "event Deployed(address indexed principalToken, address indexed yt, address indexed pool, uint256 expiry, address target)",
     fromBlock,
   });
 
-  const principalTokens = deployLogs.map((l) => l.principalToken);
-  const pools = deployLogs.map((l) => l.pool);
-  const targets = deployLogs.map((l) => l.target);
+  let principalTokens = deployLogs.map((l) => l.principalToken);
+  let pools = deployLogs.map((l) => l.pool);
+  let targets = deployLogs.map((l) => l.target);
 
   // Get underlying assets and balances for PTs
-  const assets = await api.multiCall({
-    abi: "address:i_asset",
-    calls: principalTokens,
-  });
-
-  // Get pool tokens and balances
-  const [coin0s, coin1s] = await Promise.all([
+  const [resolvers, assets] = await Promise.all([
     api.multiCall({
-      abi: "function coins(uint256) view returns (address)",
-      calls: pools.map((p) => ({ target: p, params: [0] })),
+      abi: "address:i_resolver", 
+      calls: principalTokens,
     }),
     api.multiCall({
-      abi: "function coins(uint256) view returns (address)",
-      calls: pools.map((p) => ({ target: p, params: [1] })),
+      abi: "address:i_asset",
+      calls: principalTokens,
     }),
   ]);
+
+  const scales = await api.multiCall({
+    abi: "uint256:scale",
+    calls: resolvers,
+  });
+
+
+  const targetToAsset = targets.reduce((acc, target, i) => {
+    acc[getTokenId(api.chain, target)] = assets[i].toLowerCase();
+    return acc;
+  }, {});
+
+  const targetToScale = targets.reduce((acc, target, i) => {
+    acc[getTokenId(api.chain, target)] = scales[i];
+    return acc;
+  }, {});
+
 
   // Combine all tokens and owners for balance checking
   const tokensAndOwners = [
     // PT vault holdings
     ...principalTokens.map((pt, i) => [targets[i], pt]),
-    ...principalTokens.map((pt, i) => [assets[i], pt]),
-
     // Pool holdings
-    ...pools.map((p, i) => [coin0s[i], p]),
-    ...pools.map((p, i) => [coin1s[i], p]),
+    ...pools.map((p, i) => [targets[i], p]),
   ];
 
-  return sumTokens2({ api, tokensAndOwners });
+  const resInTargets  = await sumTokens2({ api, tokensAndOwners });
+  const resInAsset = {};
+
+  Object.entries(resInTargets).forEach(([targetId, balance]) => {
+    const asset = targetToAsset[targetId];
+    const assetId = getTokenId(api.chain, asset);
+    const scale = targetToScale[targetId];
+    if (Number(balance) > 0) {
+      const adjustedBalance = BigInt(balance) * BigInt(scale) / BigInt(1e18);
+      resInAsset[assetId] = resInAsset[assetId] ?? "0";
+      resInAsset[assetId] = (BigInt(resInAsset[assetId]) + adjustedBalance).toString();
+    } 
+  });
+
+  return resInAsset;
 }
+
+function getTokenId(chain, token) {
+  return `${chain}:${token.toLowerCase()}`;
+}
+
 
 module.exports = {
   methodology:
