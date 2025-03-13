b53ff3418c496dbb414bfff97ff060e804a1ca5d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-13 11:54:58 +0100

    update echelon: #13848

--- projects/echelon/index.js
+++ projects/echelon/index.js
@@ -1,19 +1,24 @@
 const { getResource, } = require("../helper/chain/aptos");
 
-const mainLendingContract = "0xc6bc659f1649553c1a3fa05d9727433dc03843baac29473c817d06d39e7621ba";
+const aptosLendingContract = "0xc6bc659f1649553c1a3fa05d9727433dc03843baac29473c817d06d39e7621ba";
+const movementLendingContract = "0x6a01d5761d43a5b5a0ccbfc42edf2d02c0611464aae99a2ea0e0d4819f0550b5";
+
 const coinAssetType = '300';
-async function getMarketAddresses() {
-  const lending = await getResource(mainLendingContract, `${mainLendingContract}::lending::Lending`);
+
+async function getMarketAddresses(network) {
+  const lendingAddress = network === 'aptos' ? aptosLendingContract : movementLendingContract;
+  const lending = await getResource(lendingAddress, `${lendingAddress}::lending::Lending`, network);
   return lending.market_objects.map(obj => obj.inner);
 }
 
-async function getMarket(marketAddress) {
-  const market = await getResource(marketAddress, `${mainLendingContract}::lending::Market`)
+async function getMarket(network, marketAddress) {
+  const lendingAddress = network === 'aptos' ? aptosLendingContract : movementLendingContract;
+  const market = await getResource(marketAddress, `${lendingAddress}::lending::Market`, network)
   var coinInfo = null;
   if (market.asset_type === coinAssetType) {
-    coinInfo = (await getResource(marketAddress, `${mainLendingContract}::lending::CoinInfo`)).type_name
+    coinInfo = (await getResource(marketAddress, `${lendingAddress}::lending::CoinInfo`, network)).type_name
   } else {
-    coinInfo = (await getResource(marketAddress, `${mainLendingContract}::lending::FungibleAssetInfo`)).metadata.inner; 
+    coinInfo = (await getResource(marketAddress, `${lendingAddress}::lending::FungibleAssetInfo`, network)).metadata.inner; 
   }
   
   
@@ -24,13 +29,23 @@ module.exports = {
   timetravel: false,
   methodology:
     "Aggregate TVL of both the Echelon main pool and its isolated pairs",
-  aptos: {
-    tvl: async (api) => {
-      const marketAddresses = await getMarketAddresses();
-      const markets = await Promise.all(marketAddresses.map(marketAddress => getMarket(marketAddress)));
-      markets.forEach(({ cash, coin }) => {
-        api.add(coin, cash);
-      });
-    }
-  },
+  aptos: {tvl, borrowed,  },
+  move: {tvl, borrowed,  },
 };
+
+async function tvl(api) {
+  
+  const marketAddresses = await getMarketAddresses(api.chain);
+  const markets = await Promise.all(marketAddresses.map(marketAddress => getMarket(api.chain, marketAddress)));
+  markets.forEach(({ cash, coin }) => {
+    api.add(coin, cash);
+  });
+}
+
+async function borrowed(api) {
+  const marketAddresses = await getMarketAddresses(api.chain);
+  const markets = await Promise.all(marketAddresses.map(marketAddress => getMarket(api.chain, marketAddress)));
+  markets.forEach(({ liability, coin }) => {
+    api.add(coin, liability)
+  });
+}
