1c4174417553718321efe5b8e839d4594d76b3a8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-17 21:28:57 +0100

    sophon_ track USDC #13897

--- projects/sophon/index.js
+++ projects/sophon/index.js
@@ -2,6 +2,6 @@ const getTvl = require("../txBridge/util")
 
 module.exports = {
   ethereum: {
-    tvl: async (api) => getTvl(api, { chainId: 50104, }),
+    tvl: async (api) => getTvl(api, { chainId: 50104, additionalBridges: ['0xf553E6D903AA43420ED7e3bc2313bE9286A8F987'] }),
   },
 }
