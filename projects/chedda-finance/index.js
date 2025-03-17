bff1753f664b4a5d3a6018dadc5f84f51f65d3a6
0xpeluche <0xpeluche@proton.me>
2025-03-17 11:08:30 +0100

    Feat: Chedda-finance

--- projects/chedda-finance/index.js
+++ projects/chedda-finance/index.js
@@ -1,54 +1,37 @@
-const { sumTokens2 } = require('../helper/unwrapLPs')
-
 const poolAbi = {
+  activePools: "function activePools() view returns (address[])",
+  collaterals: "function collaterals() view returns (address[])",
+  asset: "function asset() view returns (address)",
   borrowed: "function borrowed() view returns (uint256)"
 };
 
-const cheddaPools = [
-  '0xE5c35103D75a72035B7B21Bb8e3Fd1e06920e5b0',
-  '0x7677DcdaCE362b4185dB2eE47472108156397936'
-];
+const registry = "0xFF11a76cB422642525B751972151841673CB0C57"
 
-const cbAssetPool = "0xE5c35103D75a72035B7B21Bb8e3Fd1e06920e5b0"
-const memecoinPool = "0x7677DcdaCE362b4185dB2eE47472108156397936";
-const USDC = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
-const CBBTC = "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf";
-const CBETH = "0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22";
-const DEGEN = "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed";
-const BRETT = "0x532f27101965dd16442E59d40670FaF5eBB142E4";
-const TOSHI = "0xAC1Bd2486aAf3B5C0fc3Fd868558b082a531B2B4";
+const tvl = async (api) => {
+  const pools = await api.call({ target: registry, abi: poolAbi.activePools })
+  const collateralsTokens = await api.multiCall({ calls: pools, abi: poolAbi.collaterals })
 
-async function tvl({api}) {
-  return sumTokens2({ 
-    tokens: [USDC, CBBTC, CBETH, DEGEN, BRETT, TOSHI], 
-    owners: [cbAssetPool, memecoinPool], api })
+  const taos = pools.flatMap((pool, index) => {
+    const collaterals = collateralsTokens[index]
+    return collaterals.map((coll) => ([coll, pool]));
+  })
+
+  return api.sumTokens({ tokensAndOwners: taos })
 }
 
-const fetchTotalBorrowData = async (api, poolAddresses) => {
-  const poolToToken = {
-    [cbAssetPool]: [USDC, CBBTC, CBETH],
-    [memecoinPool]: [USDC, DEGEN, BRETT, TOSHI]
-  };
+const borrowed = async (api) => {
+  const pools = await api.call({ target: registry, abi: poolAbi.activePools })
+  const assets = await api.multiCall({ calls: pools, abi: poolAbi.asset })
+  const totalBorrows = await api.multiCall({ calls: pools, abi: poolAbi.borrowed });
 
-  const totalBorrows = await api.multiCall({
-    abi: poolAbi.borrowed,
-    calls: poolAddresses
-  });
-  
-  poolAddresses.forEach((poolAddress, i) => {
-    const token = poolToToken[poolAddress][0];
-    api.add(token, totalBorrows[i]);
-  });
-  
-  return api.getBalances();
-};
+  pools.forEach((pool, index) => {
+    const asset = assets[index]
+    const totalBorrow = totalBorrows[index]
+    return api.add(asset, totalBorrow)
+  })
+}
 
 module.exports = {
   methodology: `TVL is comprised of assets and collateral deposited to the pools on https://app.chedda.finance`,
-  base: {
-    tvl,
-    borrowed: async (api) => {
-      return fetchTotalBorrowData(api, cheddaPools);
-    }
-  }
+  base: { tvl, borrowed }
 }
