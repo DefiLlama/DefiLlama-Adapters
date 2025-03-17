ddc9011a4efa6af78b1fd24800a0cd6ddf4bf2f5
0xLennyy <39997507+0xLennyy@users.noreply.github.com>
2025-03-17 16:08:41 +0800

    chore: tvl for address

--- projects/cyclex/index.js
+++ projects/cyclex/index.js
@@ -1,47 +1,32 @@
-const { symbol, decimals } = require('@defillama/sdk/build/erc20');
-const { get } = require('../helper/http')
+const AILAYER_COLLATERAL = {
+    // BFBTC
+    BFBTC: '0xC2236204768456B21eAfEf0d232Ba1FccCe59823',
+    BFBTC_COLLATERAL_ASSET: '0xb3aCcC18A537C394e7D127413B752E1B5404FE05',
+    // ABTC
+    ABTC: '0x1470a4831F76954686BfB4dE8180F7469EA8dE6F',
+    ABTC_COLLATERAL_ASSET: '0x8D3cF47809458879a69661a0270feA62b2495e9E',
 
+}
 
-async function fetch() {
+async function ailayerTVL(api) {
+    const bfbtcCollateralBalance = await api.call({
+        abi: 'erc20:balanceOf',
+        target: AILAYER_COLLATERAL.BFBTC,
+        params: [AILAYER_COLLATERAL.BFBTC_COLLATERAL_ASSET]
+    })
+    api.add(AILAYER_COLLATERAL.BFBTC, bfbtcCollateralBalance)
 
-    const CYCLEX_API_ALL_FUND = 'https://cyclex.cc/api/fundProduct'
-
-    const CAFM = {
-        address: '0x58985dDEda88032e9ABEad5A67d95Ba1e4F60345',
-        name: 'CycleX AILayer Tokenized Fund(M)',
-        symbol: 'CAFM',
-        decimals: 18
-    }
-
-    const CAFR = {
-        address: '0x4c717e5B5B622E5C9F22039eB46762B7AEa9663E',
-        name: 'CycleX AILayer Tokenized Fund(R)',
-        symbol: 'CAFR',
-        decimals: 18
-    }
-    
-    // const CAFMSymbol = await api.call({abi: 'erc20:symbol', target: CAFM})
-    // const CAFMSupply = await api.call({abi: 'erc20:totalSupply', target: CAFM})
-    // const CAFMDecimals = await api.call({abi: 'erc20:decimals', target: CAFM})
-    // CycleX AILayer Tokenized Fund(R):
-    // const CAFR = '0x4c717e5B5B622E5C9F22039eB46762B7AEa9663E'
-
-    const response = await get(CYCLEX_API_ALL_FUND);
-    // console.log(`API Response data: ${JSON.stringify(response.data)}`);
-    const allFund = response.data
-    const symbols = [CAFM.symbol, CAFR.symbol];
-    const funds = allFund.find(item => symbols.includes(item.simple_name));
-    if (funds === undefined || funds === null) {
-        return 0;
-    }
-    const totalTVL = funds.reduce((acc, fund) => acc + (fund.tvl_value * 1e5), 0);
-    return totalTVL;
+    const abtcCollateralBalance = await api.call({
+        abi: 'erc20:balanceOf',
+        target: AILAYER_COLLATERAL.ABTC,
+        params: [AILAYER_COLLATERAL.ABTC_COLLATERAL_ASSET]
+    })
+    api.add(AILAYER_COLLATERAL.ABTC, abtcCollateralBalance)
 }
 
 module.exports = {
-    methodology: "TVL is fetched from CycleX API and includes price data",
+    methodology: "TVL is fetched from CycleX collateral wallet",
     ailayer: {
-        fetch
+        tvl: ailayerTVL
     },
-    fetch
 }
