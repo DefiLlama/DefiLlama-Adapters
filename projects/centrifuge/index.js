f4936c737ebcb16fce458b8364ee58a06999e9ed
Jeroen Offerijns <jeroen@offerijns.com>
2025-03-21 14:36:32 +0100

    Update

--- projects/centrifuge/index.js
+++ projects/centrifuge/index.js
@@ -4,20 +4,23 @@ module.exports = {
   methodology: "TVL corresponds to the total amount of JTRSY minted onchain across Ethereum, Base, and Arbitrum.",
   ethereum: {
     tvl: async (api) => {
+      const decimals = await api.call({ abi: 'erc20:decimals', target: JTRSY })
       const supply = await api.call({ abi: "erc20:totalSupply", target: JTRSY });
-      api.add(JTRSY, supply);
+      api.addUSDValue(supply / 10 ** (decimals));
     },
   },
   base: {
     tvl: async (api) => {
+      const decimals = await api.call({ abi: 'erc20:decimals', target: JTRSY })
       const supply = await api.call({ abi: "erc20:totalSupply", target: JTRSY });
-      api.add(JTRSY, supply);
+      api.addUSDValue(supply / 10 ** (decimals));
     },
   },
   arbitrum: {
     tvl: async (api) => {
+      const decimals = await api.call({ abi: 'erc20:decimals', target: JTRSY })
       const supply = await api.call({ abi: "erc20:totalSupply", target: JTRSY });
-      api.add(JTRSY, supply);
+      api.addUSDValue(supply / 10 ** (decimals));
     },
   },
 };
