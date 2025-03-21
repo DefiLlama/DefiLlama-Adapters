17fc72bcb249def4c7af4df37304d0b9cfab5274
Jeroen Offerijns <jeroen@offerijns.com>
2025-03-21 14:56:24 +0100

    Add USD value

--- projects/centrifuge/index.js
+++ projects/centrifuge/index.js
@@ -1,26 +1,42 @@
 const JTRSY = "0x8c213ee79581Ff4984583C6a801e5263418C4b86";
 
+const config = {
+  "ethereum": [ { "token": JTRSY, "vault": "0x36036fFd9B1C6966ab23209E073c68Eb9A992f50" } ],
+  "base": [ { "token": JTRSY, "vault": "0xF9a6768034280745d7F303D3d8B7f2bF3Cc079eF" } ],
+  "arbitrum": [ { "token": JTRSY, "vault": "0x16C796208c6E2d397Ec49D69D207a9cB7d072f04" } ]
+}
+
+const contractAbis = {
+  totalAssets: "function totalAssets() external view returns (uint256)",
+};
+
 module.exports = {
-  methodology: "TVL corresponds to the total amount of JTRSY minted onchain across Ethereum, Base, and Arbitrum.",
+  methodology: "TVL corresponds to the total USD value of tokens minted on Centrifuge across Ethereum, Base, and Arbitrum.",
   ethereum: {
     tvl: async (api) => {
-      const decimals = await api.call({ abi: 'erc20:decimals', target: JTRSY })
-      const supply = await api.call({ abi: "erc20:totalSupply", target: JTRSY });
-      api.addUSDValue(supply / 10 ** (decimals));
+      for (let i = 0; i < config['ethereum'].length; i++) {
+        const decimals = await api.call({ abi: 'erc20:decimals', target: config['ethereum'][i]['token'] })
+        const value = await api.call({ abi: contractAbis.totalAssets, target: config['ethereum'][i]['vault'] })
+        api.addUSDValue(value / 10 ** (decimals));
+      }
     },
   },
   base: {
     tvl: async (api) => {
-      const decimals = await api.call({ abi: 'erc20:decimals', target: JTRSY })
-      const supply = await api.call({ abi: "erc20:totalSupply", target: JTRSY });
-      api.addUSDValue(supply / 10 ** (decimals));
+      for (let i = 0; i < config['base'].length; i++) {
+        const decimals = await api.call({ abi: 'erc20:decimals', target: config['base'][i]['token'] })
+        const value = await api.call({ abi: contractAbis.totalAssets, target: config['base'][i]['vault'] })
+        api.addUSDValue(value / 10 ** (decimals));
+      }
     },
   },
   arbitrum: {
     tvl: async (api) => {
-      const decimals = await api.call({ abi: 'erc20:decimals', target: JTRSY })
-      const supply = await api.call({ abi: "erc20:totalSupply", target: JTRSY });
-      api.addUSDValue(supply / 10 ** (decimals));
+      for (let i = 0; i < config['arbitrum'].length; i++) {
+        const decimals = await api.call({ abi: 'erc20:decimals', target: config['arbitrum'][i]['token'] })
+        const value = await api.call({ abi: contractAbis.totalAssets, target: config['arbitrum'][i]['vault'] })
+        api.addUSDValue(value / 10 ** (decimals));
+      }
     },
   },
 };
