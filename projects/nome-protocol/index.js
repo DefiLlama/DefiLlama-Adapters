9609744e94cf3fe47360ee57fb28eb0db48e01f6
nome-protocol <nome-protocol>
2025-03-24 03:35:04 +0400

    fix typo

--- projects/nome-protocol/index.js
+++ projects/nome-protocol/index.js
@@ -1,8 +1,8 @@
 const { sumTokensExport } = require("../helper/unwrapLPs")
 
 // POOLS
-const REWARDS_POOLS_NOME_HOMEY_LP = '0xf4399d583d8dad39399ea6a99ca218290595aedc'
-const REWARDS_POOLS_NOME_HOMEY_LP_V2 = '0x5664be48b3575e4bca19d95f75649c0e5c0ac94a'
+const REWARDS_POOLS_NOME_HONEY_LP = '0xf4399d583d8dad39399ea6a99ca218290595aedc'
+const REWARDS_POOLS_NOME_HONEY_LP_V2 = '0x5664be48b3575e4bca19d95f75649c0e5c0ac94a'
 const REWARDS_POOLS_USDbr_HONEY_LP = '0x422bd07acb1f7fa86682fcf211554ca500d0be78'
 const KODIAL_POOL_USDbr_HONEY_LP = '0xf4D9e842a5D388758aD6d48b1e4739224Cd8Fa94'
 const LIQUIDE_BOARDROOM = '0x50caa9627a940bcd4d5f3c3ed231252425af50cf'
@@ -14,7 +14,7 @@ const USDbr_TOKEN = '0x6d4223dae2a8744a85a6d44e97f3f61679f87ee6'
 const HONEY_TOKEN = '0xfcbd14dc51f0a4d49d5e53c2e0950e0bc26d0dce'
 
 // LP POOLS
-const NOME_HOMEY_LP = '0x54270bea720a79db0a34645053b02740ebcbfad5'
+const NOME_HONEY_LP = '0x54270bea720a79db0a34645053b02740ebcbfad5'
 const USDbr_HONEY_LP = '0xb1f0c3a875512191eb718b305f192dc19564f513'
 
 
@@ -28,13 +28,13 @@ module.exports = {
     tvl,
     pool2: sumTokensExport({ 
       owners: [
-        REWARDS_POOLS_NOME_HOMEY_LP, 
+        REWARDS_POOLS_NOME_HONEY_LP, 
         REWARDS_POOLS_USDbr_HONEY_LP, 
         KODIAL_POOL_USDbr_HONEY_LP,
-        REWARDS_POOLS_NOME_HOMEY_LP_V2
+        REWARDS_POOLS_NOME_HONEY_LP_V2
       ], 
       tokens: [
-        NOME_HOMEY_LP, 
+        NOME_HONEY_LP, 
         USDbr_HONEY_LP, 
         HONEY_TOKEN, 
         NOME_TOKEN, 
