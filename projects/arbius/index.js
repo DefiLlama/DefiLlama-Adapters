c5ebac5bab2083bd9069fa22fe39dbe74fd93cd4
beskay <beskay0x@gmail.com>
2025-03-25 10:59:33 +0100

    Comment out pool2

--- projects/arbius/index.js
+++ projects/arbius/index.js
@@ -12,14 +12,14 @@ const WETH_AIUS_UNIV2 = "";
 
 
 module.exports = {
-  misrepresentedTokens: true,
+  misrepresentedTokens: false,
   arbitrum: {
       staking: stakings(stakingContracts, AIUS),
       tvl: async() => ({}),
     },
-    ethereum: {
-        //pool2: stakings(stakingLpContracts, [WETH_AIUS_UNIV2]),
-        //tvl: async() => ({})
-        },
-  methodology: "Counts staked $AIUS in the voting escrow and liquidity mining contract",
+    /* ethereum: {
+        pool2: stakings(stakingLpContracts, [WETH_AIUS_UNIV2]),
+        tvl: async() => ({})
+    }, */
+  methodology: "Counts staked $AIUS in the voting escrow contract",
 };
