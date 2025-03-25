3bf008ecc398f75f4c38beefa1033f54874e7eb9
waynebruce0x <waynebruce0x@gmail.com>
2025-03-25 09:30:37 +0000

    Justbet Refactor  (#14016)

--- projects/justbet/index.js
+++ projects/justbet/index.js
@@ -1,9 +1,6 @@
-const sdk = require('@defillama/sdk')
-
 const { vaultAdapterAbi } = require('./abis')
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
-const WWINR_ADDRESS = "0xbf6fa9d2bf9f681e7b6521b49cf8eccf9ad8d31d";
 const WINR_LOCKV2_CONTRACT = "0x7f3E35B41BDF7DBA6a90661918d0EfDDC6C15c3C";
 const WINR_VAULT_ADAPTER_CONTRACT = "0xc942b79E51fe075c9D8d2c7501A596b4430b9Dd7";
 
@@ -17,38 +14,18 @@ const JUSTBET_BANKROLL_INDEXES = [
 ];
 
 const tvl = async (api) => {
-    const [poolsDetails, lockBalance] = await Promise.all([
-        api.call({
+    const poolsDetails = await api.call({
             abi: vaultAdapterAbi,
             target: WINR_VAULT_ADAPTER_CONTRACT,
             params: [JUSTBET_BANKROLL_INDEXES]
-        }),
-        sdk.api.eth.getBalance({ target: WINR_LOCKV2_CONTRACT, chain: 'winr'}),
-    ])
+        })
 
+    const tokensAndOwners = [
+        ['0x0000000000000000000000000000000000000000', WINR_LOCKV2_CONTRACT], 
+        ...poolsDetails.vaultDetails_.map(d => [d.bankrollTokenAddress, d.vaultAddress])
+    ]
 
-    const [details, amounts] = [poolsDetails[0], poolsDetails[1]];
-    const pools = [];
-
-    details.forEach((detail, index) => {
-        pools.push({
-            detail: detail,
-            amount: amounts[index]
-        });
-    });
-
-    const tokens = pools.map(p => p.detail)
-    const bals = await api.multiCall({  abi: 'erc20:balanceOf', calls: tokens.map(i => ({ target: i.bankrollTokenAddress, params: i.vaultAddress})) })
-    
-    
-    const tokenAddresses = tokens.map(i => i.bankrollTokenAddress.toLowerCase())
-    const winrIndex = tokenAddresses.findIndex(i => i === WWINR_ADDRESS.toLowerCase())
-        
-    bals[winrIndex] = BigInt(bals[winrIndex]) + BigInt(lockBalance.output)
-
-    api.addTokens(tokenAddresses, bals)
-
-    return sumTokens2({ api })
+    return sumTokens2({ api, tokensAndOwners, permitFailure: true })
 }
 
 module.exports = {
