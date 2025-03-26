f6212e2d6fb4f29809485e4239c4dab65b9a0fd8
Aeroth <82216807+gensx-x1@users.noreply.github.com>
2025-03-26 12:53:36 +0100

    Update index.js

--- projects/sonicjumper/index.js
+++ projects/sonicjumper/index.js
@@ -3,11 +3,9 @@ const { fetchURL } = require('../helper/utils');
 
 
 
-async function fetchTvl(api) {
+async function tvl(api) {
     const response = await fetchURL("https://amp81f4tgd.execute-api.eu-north-1.amazonaws.com/api/tvl/146") 
-    const totalTvlConverted = parseFloat(response.data.totalTvl) / Math.pow(10, 18);
-
-    api.add(ADDRESSES.sonic.wS, totalTvlConverted)
+    api.add(ADDRESSES.null, response.data.totalTvl)
 }
 
 
@@ -15,6 +13,6 @@ async function fetchTvl(api) {
 module.exports = {
   timetravel: false,
   sonic: {
-    tvl: fetchTvl
+    tvl
   }
 }
