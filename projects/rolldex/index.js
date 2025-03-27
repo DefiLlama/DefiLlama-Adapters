a1b7bbcd0f4fc335ad64d04c2af123df1cf5c46e
MR_chen <1094448618@qq.com>
2025-03-27 11:09:49 +0800

    feat: add rolldex project

---
+++ projects/rolldex/index.js
@@ -0,0 +1,32 @@
+const { decimals } = require("@defillama/sdk/build/erc20");
+const { ethers, formatUnits } = require("ethers");
+
+const abi = {
+    totalValue: "function totalValue() view returns ((address tokenAddress, int256 value, uint8 decimals, int256 valueUsd, uint16 targetWeight,uint16 feeBasisPoints,uint16 taxBasisPoints, bool dynamicFee)[])",
+};
+
+  const CONFIG = {
+    base: '0xa67998d867cd4b64fe9ecc1549341f1d86389c0f',
+  };
+  
+  const queryTvlData = async (api,chain) => {
+    const tokenBalance = await api.call({
+        abi: abi.totalValue,
+        target: CONFIG[chain],
+        params: [],
+      });
+      const reData = {}
+      tokenBalance.forEach(async item=>{
+        reData[item.tokenAddress] = item.value
+      })
+      return reData
+  }
+  
+  Object.keys(CONFIG).forEach((chain) => {
+      module.exports[chain] = {
+        tvl: (api) => queryTvlData(api, chain),
+      };
+  });
+
+  
+  
