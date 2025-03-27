d9fa0a020480e1b08f40dd49d4bd1ada17c75002
WHYTEWYLL <juanmiblanco1@gmail.com>
2025-03-27 23:55:28 +0200

    fix: decimals

--- projects/resupply/index.js
+++ projects/resupply/index.js
@@ -1,37 +1,37 @@
-const sdk = require('@defillama/sdk')
-const { getConfig } = require('../helper/cache')
+const sdk = require('@defillama/sdk');
+const { getConfig } = require('../helper/cache');
 
 async function tvl(api) {
-    const pairs = await getConfig('resupply/tvl/', 'https://raw.githubusercontent.com/resupplyfi/resupply/main/deployment/contracts.json');
-    
-    const pairsContracts = [];
+  const pairs = await getConfig('resupply/tvl/', 'https://raw.githubusercontent.com/resupplyfi/resupply/main/deployment/contracts.json');
+  const convertToAssetsAbi = 'function convertToAssets(uint256) view returns (uint256)';
 
-    for (const [key, value] of Object.entries(pairs)) {
-        if (!key.endsWith('_DEPRECATED')) {
-            if (key.startsWith('PAIR_CURVELEND') || key.startsWith('PAIR_FRAXLEND')) {
-                pairsContracts.push(value); 
-            }
-        }
-    }
+  const pairsContracts = Object.entries(pairs)
+    .filter(([key]) =>
+      !key.endsWith('_DEPRECATED') &&
+      (key.startsWith('PAIR_CURVELEND') || key.startsWith('PAIR_FRAXLEND'))
+    )
+    .map(([, value]) => value);
 
-    const [balances] = await Promise.all([  
-        api.multiCall({
-            abi: 'uint256:totalCollateral',
-            calls: pairsContracts,
-        })
-    ]);
+  const [balances, tokensCollateral, tokens] = await Promise.all([
+    api.multiCall({ abi: 'uint256:totalCollateral', calls: pairsContracts }),
+    api.multiCall({ abi: 'address:collateral', calls: pairsContracts }),
+    api.multiCall({ abi: 'address:underlying', calls: pairsContracts }),
+  ]);
 
-    const tokens = await api.multiCall({ 
-        abi: 'address:underlying',
-        calls: pairsContracts,
-    });
+  const assetBalances = await api.multiCall({
+    abi: convertToAssetsAbi,
+    calls: balances.map((balance, i) => ({
+      target: tokensCollateral[i],
+      params: [balance],
+    })),
+  });
 
-    api.add(tokens, balances); 
-    }
+  api.add(tokens, assetBalances);
+}
 
 module.exports = {
-    start: '2025-03-15',
-    ethereum: {
-        tvl,
-    }
+  start: '2025-03-15',
+  ethereum: {
+    tvl,
+  },
 };
