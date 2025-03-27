89d40d0477acb3f8bb5bef6c7755c0f86beca898
WHYTEWYLL <juanmiblanco1@gmail.com>
2025-03-27 15:56:15 +0200

    tvl resupply

---
+++ projects/resupply/abi/pairContract.js
@@ -0,0 +1,16 @@
+module.exports = {
+    totalCollateral: {
+      "inputs": [],
+      "name": "totalCollateral",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "_totalCollateralBalance",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  }
+  
---
+++ projects/resupply/index.js
@@ -0,0 +1,47 @@
+const sdk = require('@defillama/sdk')
+const pairContract = require('./abi/pairContract')
+const { get } = require('../helper/http');
+
+// getAllPairAddresses-- 0x607b6d16
+// After for each of the pairs, we do the following:
+// Call totalCollateral-- 0x4ac8eb5f for each
+// 
+
+const BLOCK_START = 22034910;
+
+async function tvl(api) {
+    const pairs = await get('https://raw.githubusercontent.com/resupplyfi/resupply/main/deployment/contracts.json');
+
+    const crv = [];
+    const frx = [];
+  
+    for (const [key, value] of Object.entries(pairs)) {
+      if (!key.endsWith('_DEPRECATED')) {
+        if (key.startsWith('PAIR_CURVELEND')) crv.push(value);
+        else if (key.startsWith('PAIR_FRAXLEND')) frx.push(value);
+      }
+    }
+
+    const [crvTVLs, frxTVLs] = await Promise.all([
+      api.multiCall({
+        abi: pairContract.totalCollateral,
+        calls: crv.map(addr => ({ target: addr })),
+      }),
+      api.multiCall({
+        abi: pairContract.totalCollateral,
+        calls: frx.map(addr => ({ target: addr })),
+      }),
+    ]);
+  
+    const crvTotal = crvTVLs.reduce((sum, bal) => sum + Number(bal) / 1e21, 0);
+    const frxTotal = frxTVLs.reduce((sum, bal) => sum + Number(bal) / 1e18, 0);
+    
+    return {'usd': crvTotal + frxTotal};
+  }
+
+module.exports = {
+    start: BLOCK_START,
+    ethereum: {
+    tvl,
+  }
+}; 
