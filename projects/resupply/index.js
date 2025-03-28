7f1a884c19a316e31b5f8a2304621c72e0935b1e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-28 17:00:34 +0100

    minor fix

--- projects/resupply/index.js
+++ projects/resupply/index.js
@@ -1,37 +1,33 @@
-const sdk = require('@defillama/sdk');
 const { getConfig } = require('../helper/cache');
 
-
 const convertToAssetsAbi = 'function convertToAssets(uint256) view returns (uint256)';
-const coinsAbi = 'function coins(uint256) view returns (address)';
-const balancesAbi = 'function balances(uint256) view returns (uint256)'
 
 async function tvl(api) {
-    const contracts = await getConfig('resupply/tvl', 'https://raw.githubusercontent.com/resupplyfi/resupply/main/deployment/contracts.json');
+  const contracts = await getConfig('resupply/tvl', 'https://raw.githubusercontent.com/resupplyfi/resupply/main/deployment/contracts.json');
 
-    // Lending Market
-    const pairsContracts = Object.entries(contracts)
-        .filter(([key]) =>
-        (key.startsWith('PAIR_CURVELEND') || key.startsWith('PAIR_FRAXLEND'))
-        )
-        .map(([, value]) => value);
+  // Lending Market
+  const pairsContracts = Object.entries(contracts)
+    .filter(([key]) =>
+      (key.startsWith('PAIR_CURVELEND') || key.startsWith('PAIR_FRAXLEND'))
+    )
+    .map(([, value]) => value);
 
-    const [balances, tokensCollateral, tokens] = await Promise.all([
-        api.multiCall({ abi: 'uint256:totalCollateral', calls: pairsContracts }),
-        api.multiCall({ abi: 'address:collateral', calls: pairsContracts }),
-        api.multiCall({ abi: 'address:underlying', calls: pairsContracts }),
-    ]);
+  const [balances, tokensCollateral,] = await Promise.all([
+    api.multiCall({ abi: 'uint256:totalCollateral', calls: pairsContracts }),
+    api.multiCall({ abi: 'address:collateral', calls: pairsContracts }),
+  ]);
 
-    const assetBalances = await api.multiCall({
-        abi: convertToAssetsAbi,
-        calls: balances.map((balance, i) => ({
-        target: tokensCollateral[i],
-        params: [balance],
-        })),
-    });
+  const tokens = await api.multiCall({ abi: 'address:asset', calls: tokensCollateral })
 
-    api.add(tokens, assetBalances);
+  const assetBalances = await api.multiCall({
+    abi: convertToAssetsAbi,
+    calls: balances.map((balance, i) => ({
+      target: tokensCollateral[i],
+      params: [balance],
+    })),
+  });
 
+  api.add(tokens, assetBalances);
 }
 
 module.exports = {
