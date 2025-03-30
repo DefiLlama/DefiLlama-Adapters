458c73826d47190dbffb058f1e850c25e0456272
0xd1esel <0xdiesel123@gmail.com>
2025-03-30 15:17:19 -0700

    use paginated view

--- projects/thalaswap-v2/index.js
+++ projects/thalaswap-v2/index.js
@@ -1,9 +1,10 @@
 const { function_view } = require("../helper/chain/aptos");
 
-const thalaswapLensAddress = "ff1ac437457a839f7d07212d789b85dd77b3df00f59613fcba02388464bfcacb";
+const thalaswapLensAddress = "0xeeadd07bb5e307ea3b3fb312bea4d1876526e63b9d00c3d09acca6d3744eecea";
+const chunkSize = 30;
 
-async function getPools(lensAddress) {
-  return function_view({ functionStr: `${lensAddress}::lens::get_all_pools_info`})
+async function getPools(lensAddress, start, limit) {
+  return function_view({ functionStr: `${lensAddress}::lens::get_pools_paginated`, args: [start, limit], chain: 'aptos'})
 }
 
 module.exports = {
@@ -12,7 +13,18 @@ module.exports = {
     "Aggregates TVL in all pools in Thalaswap, Thala Labs' AMM.",
   aptos: {
     tvl: async (api) => {
-      const poolInfos = await getPools(thalaswapLensAddress)
+      let currStart = 0;
+      let poolInfos = [];
+      while (true) {
+        console.log(`Getting pools from ${currStart} to ${currStart + chunkSize}`)
+        const pools = await getPools(thalaswapLensAddress, currStart, currStart + chunkSize)
+        if (pools.length < chunkSize) {
+          break;
+        }
+        poolInfos.push(...pools);
+        currStart += chunkSize;
+      }
+      console.log(`Found ${poolInfos.length} pools`)
       
       for (const poolInfo of poolInfos) {
         const assets = poolInfo.assets_metadata.map(asset => asset.inner)
