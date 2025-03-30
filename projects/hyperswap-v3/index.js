5cde094c2c48affc662a60a56897bd88091c465b
ethachiSwap <ethachi_eth@proton.me>
2025-03-30 13:34:43 +0300

    Update index.js

--- projects/hyperswap-v3/index.js
+++ projects/hyperswap-v3/index.js
@@ -3,3 +3,5 @@ const { uniV3Export } = require('../helper/uniswapV3')
 module.exports = uniV3Export({
   hyperliquid: { factory: '0xB1c0fa0B789320044A6F623cFe5eBda9562602E3', fromBlock: 1, },
 })
+
+// Please review this issue: `https://github.com/DefiLlama/DefiLlama-Adapters/issues/14004`
