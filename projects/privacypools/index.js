55c02fb76d4e43ecb87b3e2087e0e1b590146bf3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-31 23:32:53 +0200

    refactor privacy pools

--- projects/privacypools/index.js
+++ projects/privacypools/index.js
@@ -1,11 +1,29 @@
-const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs')
+const { sumTokens2 } = require("../helper/unwrapLPs")
 
 const config = {
-  ethereum: { owners: ['0xf241d57c6debae225c0f2e6ea1529373c9a9c9fb'], tokens: [nullAddress] },
+  ethereum: { poolsAndAssets: [
+    { pool: '0x6818809eefce719e480a7526d76bd3e561526b46', assets: ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE']}
+  ]},
 }
 
 Object.keys(config).forEach(chain => {
   module.exports[chain] = {
-    tvl: sumTokensExport(config[chain],)
+    tvl
   }
 })
+
+async function tvl(api) {
+  const { poolsAndAssets } = config[api.chain]
+  const tokens = []
+  const calls = []
+  poolsAndAssets.forEach(({ pool, assets }) => {
+    assets.forEach(asset => {
+      tokens.push(asset)
+      calls.push({ target: pool, params: asset })
+    })
+  })
+  const poolConfig = await api.multiCall({  abi: "function assetConfig(address _asset) view returns (address pool, uint256 minimumDepositAmount, uint256 vettingFeeBPS, uint256 maxRelayFeeBPS)", calls: calls })
+  const owners = poolConfig.map(i => i.pool)
+  return sumTokens2({ api, tokensAndOwners2: [tokens, owners], })
+  
+}
