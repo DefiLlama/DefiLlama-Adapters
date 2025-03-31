25159ac3305b42154adfe1aa2651fc4a2d568cc1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-03-31 22:43:12 +0200

    bugfix

--- projects/treasury/polkadot.js
+++ projects/treasury/polkadot.js
@@ -2,7 +2,7 @@ const { getExports } = require('../helper/heroku-api')
 
 module.exports = {
   timetravel: false,
-  ...getExports("polkadot", ['polkadot'], ['ownTokens', 'tvl']),
+  ...getExports("polkadot", ['polkadot'], ['ownTokens',]),
   methodology:
     "TVL is calculated by summing up the balances across three chains: Relay Chain (DOT), Asset Hub (DOT, USDC, USDT, MYTHOS), and Hydration (DOT, USDC, USDT). The balances are fetched directly from the respective chain's treasury and related addresses.",
 }
