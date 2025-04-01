a849d01bf81a8eafbbd1ed55ff80af7cc9c0096d
Valery <ver.valery99@gmail.com>
2025-04-01 15:33:44 +0200

    Update SAVMSwap factory address in index.js
    Replaced the old factory address with the new one in the uniTvlExport function. This ensures the TVL calculation uses the correct and updated address.

--- projects/savmswap/index.js
+++ projects/savmswap/index.js
@@ -1,3 +1,3 @@
 const { uniTvlExport } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('prom', '0x9CDdb089B2BC334244f9A8845E21b4c955b99Fc7') //factory address
+module.exports = uniTvlExport('prom', '0x3B73a7eDc9dfE4847a20BcCfEf6Eb1c90439f5C9') //factory address
