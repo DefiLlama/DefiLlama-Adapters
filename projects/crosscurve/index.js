77a284a8142bd30aeccffe0754d601a7f4253493
0xpeluche <0xpeluche@proton.me>
2025-04-03 12:16:03 +0200

    small fix mapping

--- projects/crosscurve/index.js
+++ projects/crosscurve/index.js
@@ -217,7 +217,7 @@ module.exports = {
       owner: "0xac8f44ceca92b2a4b30360e5bd3043850a0ffcbe",
       tokens: [
         ADDRESSES.sonic.WETH, // WETH
-        ADDRESSES.sonic.USDC, // usdc.e
+        ADDRESSES.sonic.USDC_e, // usdc.e
       ],
     }),
   },
