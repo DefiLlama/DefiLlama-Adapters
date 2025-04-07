8fd4eaaeaaa392ad3a5d69f725cd45864a4e2bcb
Darruma <umerderr@gmail.com>
2025-04-07 16:03:14 +0100

    feat: update block deployed

--- projects/kittenswap-cl/index.js
+++ projects/kittenswap-cl/index.js
@@ -3,6 +3,6 @@ const { uniV3Export } = require('../helper/uniswapV3')
 module.exports = uniV3Export({
   hyperliquid: { 
     factory: '0x2E08F5Ff603E4343864B14599CAeDb19918BDCaF', 
-    fromBlock: 1, 
+    fromBlock: 2033100, 
   },
 })
