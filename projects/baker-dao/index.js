3d7933476b5d2576119919f1389e511eae55878e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-07 21:16:14 +0200

    code refactor

--- projects/baker-dao/index.js
+++ projects/baker-dao/index.js
@@ -3,6 +3,6 @@ const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
 const BREAD_CONTRACT_ADDRESS = "0x0003eEDFdd020bf60D10cf684ABAc7C4534B7eAd";
 
 module.exports = {
-  methodology: "Measures the total value of BERA and BREAD tokens held in the protocol's contract",
+  methodology: "Measures the total value of BERA held in the protocol's contract",
   berachain: {tvl: sumTokensExport({ owner: BREAD_CONTRACT_ADDRESS, tokens: [nullAddress]})}
 };
