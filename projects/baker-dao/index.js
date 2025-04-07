153af618bdcd9d428278f603a5a94bb6275bdb68
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-07 21:15:58 +0200

    code refactor

--- projects/baker-dao/index.js
+++ projects/baker-dao/index.js
@@ -1,42 +1,8 @@
-const sdk = require("@defillama/sdk");
+const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
 
 const BREAD_CONTRACT_ADDRESS = "0x0003eEDFdd020bf60D10cf684ABAc7C4534B7eAd";
-const BERA_ADDRESS = "0x0000000000000000000000000000000000000000"; // Native token
-
-async function tvl(timestamp, block, chainBlocks) {
-  const balances = {};
-
-  // Get BERA balance of the BREAD contract
-  const beraBalance = await sdk.api.eth.getBalance({
-    target: BREAD_CONTRACT_ADDRESS,
-    chain: "berachain",
-    block: chainBlocks.berachain
-  });
-
-  // Get BREAD token balance (in contract)
-  const breadBalance = await sdk.api.erc20.balanceOf({
-    target: BREAD_CONTRACT_ADDRESS, // BREAD is its own token
-    owner: BREAD_CONTRACT_ADDRESS,  // Checking balance held by the contract itself
-    chain: "berachain",
-    block: chainBlocks.berachain
-  });
-
-  sdk.util.sumSingleBalance(
-    balances,
-    `berachain:${BERA_ADDRESS}`,
-    beraBalance.output
-  );
-
-  sdk.util.sumSingleBalance(
-    balances,
-    `berachain:${BREAD_CONTRACT_ADDRESS}`,
-    breadBalance.output
-  );
-
-  return balances;
-}
 
 module.exports = {
   methodology: "Measures the total value of BERA and BREAD tokens held in the protocol's contract",
-  berachain: {tvl}
+  berachain: {tvl: sumTokensExport({ owner: BREAD_CONTRACT_ADDRESS, tokens: [nullAddress]})}
 };
