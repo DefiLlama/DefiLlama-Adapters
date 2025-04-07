02dd84ad94b901859c9d06dbf272f6e3e7310777
Darruma <30578602+Darruma@users.noreply.github.com>
2025-04-07 17:09:45 +0100

    Kittenswap CL Update (#14224)

---
+++ projects/kittenswap-cl/index.js
@@ -0,0 +1,8 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  hyperliquid: { 
+    factory: '0x2E08F5Ff603E4343864B14599CAeDb19918BDCaF', 
+    fromBlock: 2033100, 
+  },
+})
