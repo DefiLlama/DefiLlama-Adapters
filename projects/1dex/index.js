72c7312ab5473331a46e4dec51786c63f735bb01
Valiant <liuw2285@gmail.com>
2025-04-07 22:10:12 +0800

    feat: update TVL calculation method for 1DEX to use account balance querying

--- projects/1dex/index.js
+++ projects/1dex/index.js
@@ -1,92 +1,19 @@
-const { post } = require("../helper/http");
+const { get_account_tvl } = require("../helper/chain/eos");
 
-const endpoint = 'https://eos.greymass.com/v1'; 
-
-const symbolToCoingeckoId = {
-    'EOS': 'eos',
-    'USDT': 'tether',
-    'BTC': 'bitcoin',
-    'XSAT': 'exsat-network',
-};
-
-async function getContractActions(contract, actionNames) {
-    const allActions = [];
-    const pageSize = 100;
-    let pos = -1;
-    let hasMore = true;
-    
-    while (hasMore) {
-        const response = await post(`${endpoint}/history/get_actions`, {
-            account_name: contract,
-            pos: pos,
-            offset: -pageSize  
-        });
-        
-        const actions = response.actions || [];
-        if (actions.length === 0) break;
-        
-        const filteredActions = actions.filter(action => 
-            Array.isArray(actionNames) 
-                ? actionNames.includes(action.action_trace.act.name)
-                : action.action_trace.act.name === actionNames
-        );
-        
-        allActions.push(...filteredActions);
-
-        const lastAction = actions[actions.length - 1];
-        pos = lastAction.account_action_seq - pageSize + 1;
-        hasMore = actions.length === pageSize;
-    }
-    
-    return allActions;
-}
-
-function calculateBalancesFromActions(actions) {
-    const balances = {};
-    
-    for (const action of actions) {
-        const { name, data } = action.action_trace.act;
-        const { quantity, contract } = data;
-        const [amount, symbol] = quantity.split(' ');
-        
-        if (!balances[symbol]) {
-            balances[symbol] = {
-                amount: 0,
-                contract,
-                symbol
-            };
-        }
-        
-        const parsedAmount = parseFloat(amount);
-        if (name === 'logdeposit1' || name === 'logwithdraw1') {
-            balances[symbol].amount += name === 'logdeposit1' ? parsedAmount : -parsedAmount;
-        }
-    }
-    
-    return balances;
-}
-
-function convertToDefiLlamaFormat(balances) {
-    const result = {};
-    
-    for (const [symbol, data] of Object.entries(balances)) {
-        if (data.amount > 0 && symbolToCoingeckoId[symbol]) {
-            result[symbolToCoingeckoId[symbol]] = data.amount;
-        }
-    }
-    
-    return result;
-}
-
+// 1DEX
+// https://1dex.com
 async function eos() {
-    const actions = await getContractActions("portal.1dex", ["logdeposit1", "logwithdraw1"]);
-    const balances = calculateBalancesFromActions(actions);
-    return convertToDefiLlamaFormat(balances);
+    const accounts = ["dex.velox"];
+    const tokens = [
+        ["eosio.token", "EOS", "eos"],
+        ["usdt.xsat", "USDT", "tether"],
+    ];
+    return await get_account_tvl(accounts, tokens, "eos");
 }
 
 module.exports = {
-    methodology: `1DEX TVL is calculated by tracking deposit and withdrawal actions (logdeposit1 and logwithdraw1) from the portal.1dex contract.`,
+    methodology: `1DEX TVL is achieved by querying token balances from swap smart contract.`,
     eos: {
-        tvl: eos,
+      tvl: eos
     },
 }
