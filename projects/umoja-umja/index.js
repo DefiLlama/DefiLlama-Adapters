f542eef97dbbe8adf39ed03504aa4552c30a8a37
DarkRoku12 <enmarey2012@hotmail.com>
2025-04-09 09:52:29 -0400

    Fetch the UMJA balance locked in UMO Governance.

--- projects/umoja-umja/index.js
+++ projects/umoja-umja/index.js
@@ -1,19 +1,13 @@
-const UMJA = {
-  arbitrum: "0x16A500Aec6c37F84447ef04E66c57cfC6254cF92",
-  base: "0x009E97a080BC7B603257C461598275Dc49B8cd0c",
+const Arbitrum = {
+  token: "0x16A500Aec6c37F84447ef04E66c57cfC6254cF92",
+  umo_governance: "0x256F7b822594a1Bc5cB5a68f0A7d97C8F3F2711C",
 };
 
 module.exports = {
   arbitrum: {
       tvl: async (api) => {
-          const supply = await api.call({ abi: 'erc20:totalSupply', target: UMJA.arbitrum })
-          api.add(UMJA.arbitrum, supply)
+          const lockedUmoGov = await api.call({ abi: 'erc20:balanceOf', target: Arbitrum.token, params: Arbitrum.umo_governance })
+          api.add(Arbitrum.token, lockedUmoGov)
       },
   },
-  base: {
-      tvl: async (api) => {
-          const supply = await api.call({ abi: 'erc20:totalSupply', target: UMJA.base })
-          api.add(UMJA.base, supply)
-      },
-  }
 }
