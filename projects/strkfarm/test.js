e6cfae84abf6c76667ada0a1a8054e3c6b8d12bf
ariyan-hashstack <ariyan_hashstack@hashstack.finance>
2025-04-10 14:59:58 +0530

    [strkfarm] removed redundent file

--- projects/strkfarm/test.js
+++
@@ -1,9 +0,0 @@
-import data from './data.json' assert { type: 'json' };
-
-let totalVolume = 0n;
-
-for (const entry of data.data) {
-    totalVolume += BigInt(entry.volume);
-}
-
-console.log("Total Volume:", totalVolume.toString());
