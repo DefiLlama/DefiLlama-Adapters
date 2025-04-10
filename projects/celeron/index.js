ec21baf5f3b9c2f8539dbb2e328caa9a03867b27
celeron <support@celeron.xyz>
2025-04-10 11:42:35 +0800

    add BURR token info

--- projects/celeron/index.js
+++ projects/celeron/index.js
@@ -8,6 +8,7 @@ const CEL_ADDRESS = "0x8b83ECC4EF8FaEc5c05b7D6EC002B659BE137120";
 const BERACHAIN_FARM_ADDRESS = "0xAbFc9bb50af39D1e6f99836Ff2EeCc39778808a1";
 const BERACHAIN_CEL_ADDRESS = "0xD3415dCFbdA117814e24a4cbaf61128A4D79b860";
 const BERACHAIN_FARM_LP_ADDRESS = "0x5CC7BebF2A05fC4b7F259C8688Ff0d80735E36FE";
+const BERACHAIN_FARM_BURR_ADDRESS = "0x6F581E6D5EF136BE6036398225e60Db8fF3FeE53";
 
 async function getTvl(api, farmAddress) {
   let pools = await api.call({ abi: abiInfo.poolTvls, target: farmAddress });
@@ -25,16 +26,15 @@ async function tvl(api) {
     return await getTvl(api,FARM_IONIC_ADDRESS);
   }else{
     await getTvl(api,BERACHAIN_FARM_ADDRESS);
+    await getTvl(api,BERACHAIN_FARM_BURR_ADDRESS);
     return await getTvl(api,BERACHAIN_FARM_LP_ADDRESS);
   }
 }
 
 async function staking(api) {
-
   let farmAddress = api.chain === 'berachain'?BERACHAIN_FARM_ADDRESS:FARM_ADDRESS
   let celAddress = api.chain === 'berachain'?BERACHAIN_CEL_ADDRESS:CEL_ADDRESS
   
-
   let pools = await api.call({ abi: abiInfo.poolTvls, target: farmAddress });
   let target = pools.find((i) => i.assets === celAddress);
   api.add(celAddress, target.tvl);
