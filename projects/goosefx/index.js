d289c52d0fb4df4034d17e038ef98b92b72eb6df
Shashank Shekhar <shashank@Shashanks-MacBook-Pro.local>
2025-04-11 20:13:07 +0530

    Adds Goosefx tvl to defillama

--- projects/goosefx/index.js
+++ projects/goosefx/index.js
@@ -1,11 +1,11 @@
 const { sumTokens2, } = require('../helper/solana')
 
 async function tvl() {
-  return sumTokens2({ owner: 'ALfS4oPB5684XwTvCjWw7XddFfmyTNdcY7xHxbh2Ui8s'})
+  return sumTokens2({ tokenAccounts: ['ALfS4oPB5684XwTvCjWw7XddFfmyTNdcY7xHxbh2Ui8s']})
 }
 
 async function staking() {
-  return sumTokens2({ tokenAccounts: ['D9DhGq6MctzCzABhFybyuj3KHj3QbeG5H3JzXMGysY3T'] })
+  return sumTokens2({ tokenAccounts: ['24dximAcSUp1aM3uyQ7Cdpsg128ZVpYRwJBz5k4P6HMc'] })
 }
 
 module.exports = {
