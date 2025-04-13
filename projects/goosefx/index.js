84ace8d65bfbb80d0d8c88a4f1c52b06bcafd390
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-13 10:30:42 +0200

    create new adapter for goose fx v2 #14192

--- projects/goosefx/index.js
+++ projects/goosefx/index.js
@@ -1,7 +1,19 @@
 const { sumTokens2, } = require('../helper/solana')
 
 async function tvl() {
-  return sumTokens2({ owner: 'ALfS4oPB5684XwTvCjWw7XddFfmyTNdcY7xHxbh2Ui8s'})
+  // const { api } = arguments[3]
+  // const provider = getProvider()
+  // const idl = await Program.fetchIdl(programs.SSL_PROGRAM_ID, provider)
+  // const program = new Program(idl, programs.SSL_PROGRAM_ID, provider)
+  // // const data = await program.account.liquidityAccount.all()
+  // const data = await program.account.poolRegistry.all()
+  // data.forEach(({ account: { entries }}) => {
+  //   entries.forEach(({ mint, totalLiquidityDeposits, }) => {
+  //     api.add(mint.toString(), totalLiquidityDeposits.toString())
+  //   })
+  // })
+  // return api.getBalances()
+  return sumTokens2({ tokenAccounts: ["8cMk8SxSY6b3a4wGhEF5rgXfsmUuqwPqpqhkH7z1gvXZ","6hDS2kFxSJCHcgX3WYwPULzrQrhSSQWTVDfxAToLKsAv","HJuwvd1bBPWr1fV1EKNnzgR7Kn3znU2WzwiGphb8Mv92", "Af6DjX1eRjfmnF1Lfe99FTgLUMKTVrsyf7CY2Kx9kzbT", "DA2nEQnCZFGhAZSQ5jKUHL4REPG57ixEMeTQAfWuWmJX", "294A1PmDuLHrmgcPKHoQd9GgrJxQ6y2WkpPhSVgbkUoD"] })
 }
 
 async function staking() {
