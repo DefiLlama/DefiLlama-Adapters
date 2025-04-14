b74933460ef5d94f3aacdcddffc7260bf9b433af
Tyler Tsai <tyler@helio.money>
2025-04-14 21:10:22 +0800

    feat: use api instead of fetching by events

--- projects/lista-lending/index.js
+++ projects/lista-lending/index.js
@@ -20,94 +20,103 @@ const eventAbis = {
 module.exports = {
   methodology: "TVL counts the tokens locked in the protocol's vaults based on supply, withdraw and liquidate events",
   start: '2025-04-01',
+  misrepresentedTokens: true,
   bsc: {
     tvl: async (api) => {
-      const { data: { data: vaults } } = await axios.get('https://api.lista.org/api/moolah/vault/list?page=1&pageSize=1000')
+        
+    //   const { data: { data: vaults } } = await axios.get('https://api.lista.org/api/moolah/vault/list?page=1&pageSize=1000')
       
-      const totalAssets = await api.multiCall({
-        abi: abi.totalAssets,
-        calls: vaults.list.map(i => i.address)
-      })
-      const tokensAndOwners = vaults.list.map((vault, i) => {
-        api.add(vault.asset, totalAssets[i])
-        return [vault.asset, vault.address]
-      })
+    //   const totalAssets = await api.multiCall({
+    //     abi: abi.totalAssets,
+    //     calls: vaults.list.map(i => i.address)
+    //   })
+    //   const tokensAndOwners = vaults.list.map((vault, i) => {
+    //     api.add(vault.asset, totalAssets[i])
+    //     return [vault.asset, vault.address]
+    //   })
 
         
     
-      const [supplyLogs
-        , withdrawLogs,
-         liquidateLogs
-    ] = await Promise.all([
-        getLogs2({ 
-          api, 
-          target: config.bsc.vault,
-          eventAbi: eventAbis.supplyCollateral,
-          fromBlock: config.bsc.fromBlock,
-          extraKey: 'SupplyCollateral',
+    //   const [supplyLogs
+    //     , withdrawLogs,
+    //      liquidateLogs
+    // ] = await Promise.all([
+    //     getLogs2({ 
+    //       api, 
+    //       target: config.bsc.vault,
+    //       eventAbi: eventAbis.supplyCollateral,
+    //       fromBlock: config.bsc.fromBlock,
+    //       extraKey: 'SupplyCollateral',
           
-        }),
-        getLogs2({ 
-          api, 
-          target: config.bsc.vault,
-          eventAbi: eventAbis.withdrawCollateral,
-          fromBlock: config.bsc.fromBlock,
-          extraKey: 'WithdrawCollateral'
-        }),
-        getLogs2({ 
-          api, 
-          target: config.bsc.vault,
-          eventAbi: eventAbis.liquidate,
-          fromBlock: config.bsc.fromBlock,
-          extraKey: 'Liquidate'
-        })
-      ])
+    //     }),
+    //     getLogs2({ 
+    //       api, 
+    //       target: config.bsc.vault,
+    //       eventAbi: eventAbis.withdrawCollateral,
+    //       fromBlock: config.bsc.fromBlock,
+    //       extraKey: 'WithdrawCollateral'
+    //     }),
+    //     getLogs2({ 
+    //       api, 
+    //       target: config.bsc.vault,
+    //       eventAbi: eventAbis.liquidate,
+    //       fromBlock: config.bsc.fromBlock,
+    //       extraKey: 'Liquidate'
+    //     })
+    //   ])
 
     
 
-      const tokenBalances = {}
+    //   const tokenBalances = {}
       
-      const uniqueIds = [...new Set(supplyLogs.map(log => log.id))]
+    //   const uniqueIds = [...new Set(supplyLogs.map(log => log.id))]
 
-      const marketParams = await api.multiCall({
-        abi: abi.idToMarketParams,
-        calls: uniqueIds.map(id => ({
-          target: config.bsc.vault,
-          params: [id]
-        }))
-      })
+    //   const marketParams = await api.multiCall({
+    //     abi: abi.idToMarketParams,
+    //     calls: uniqueIds.map(id => ({
+    //       target: config.bsc.vault,
+    //       params: [id]
+    //     }))
+    //   })
 
-      const idToCollateralToken = {}
-      uniqueIds.forEach((id, i) => {
-        idToCollateralToken[id] = marketParams[i].collateralToken
-      })
+    //   const idToCollateralToken = {}
+    //   uniqueIds.forEach((id, i) => {
+    //     idToCollateralToken[id] = marketParams[i].collateralToken
+    //   })
 
-      supplyLogs.forEach(log => {
-        const collateralToken = idToCollateralToken[log.id]
-        const amount = log.assets
-        tokenBalances[collateralToken] = (tokenBalances[collateralToken] || 0) + Number(amount)
-      })
+    //   supplyLogs.forEach(log => {
+    //     const collateralToken = idToCollateralToken[log.id]
+    //     const amount = log.assets
+    //     tokenBalances[collateralToken] = (tokenBalances[collateralToken] || 0) + Number(amount)
+    //   })
 
-      withdrawLogs.forEach(log => {
-        const collateralToken = idToCollateralToken[log.id]
-        const amount = log.assets
-        tokenBalances[collateralToken] = (tokenBalances[collateralToken] || 0) - Number(amount)
-      })
+    //   withdrawLogs.forEach(log => {
+    //     const collateralToken = idToCollateralToken[log.id]
+    //     const amount = log.assets
+    //     tokenBalances[collateralToken] = (tokenBalances[collateralToken] || 0) - Number(amount)
+    //   })
 
-      liquidateLogs.forEach(log => {
-        const collateralToken = idToCollateralToken[log.id]
-        const amount = log.seizedAssets
-        tokenBalances[collateralToken] = (tokenBalances[collateralToken] || 0) - Number(amount)
-      })
+    //   liquidateLogs.forEach(log => {
+    //     const collateralToken = idToCollateralToken[log.id]
+    //     const amount = log.seizedAssets
+    //     tokenBalances[collateralToken] = (tokenBalances[collateralToken] || 0) - Number(amount)
+    //   })
 
-      Object.entries(tokenBalances).forEach(([token, balance]) => {
-        if (balance > 0) {
-          api.add(token, balance)
-        }
-      })
+    //   Object.entries(tokenBalances).forEach(([token, balance]) => {
+    //     if (balance > 0) {
+    //       api.add(token, balance)
+    //     }
+    //   })
 
-      return api.getBalances()
-   
+    //   return api.getBalances()
+    const { data: { data: { totalDeposits, totalCollateral } } } = await axios.get('https://api.lista.org/api/moolah/overall')
+      
+      // Convert string to number and sum up
+      const totalValue = Number(totalDeposits) + Number(totalCollateral)
+      
+      return {
+        'tether': totalValue
+      }
     }
   }
 } 
