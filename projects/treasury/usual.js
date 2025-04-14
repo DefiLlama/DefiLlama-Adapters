8214eb5c0b5ccd80199aab978374a47bc09b3297
Define101 <Lovelovenow@protonmail.com>
2025-04-14 11:18:58 +0100

    Fix Usal Treasury

--- projects/treasury/usual.js
+++ projects/treasury/usual.js
@@ -8,15 +8,13 @@ const tokens = [
 ]
 
 const owners = [
-  '0xdd82875f0840AAD58a455A70B88eEd9F59ceC7c7', // Treasury
   '0x81ad394C0Fa87e99Ca46E1aca093BEe020f203f4', // Yield Treasury
-  '0x4Cbc25559DbBD1272EC5B64c7b5F48a2405e6470',  // USUALM
-  '0x58073531a2809744D1bF311D30FD76B27D662abB' // USUALUSDtb
 ]
 
 module.exports = treasuryExports({
   ethereum: {
     tokens,
-    owners
+    owners,
+    ownTokens: ["0x06B964d96f5dCF7Eae9d7C559B09EDCe244d4B8E", "0xC4441c2BE5d8fA8126822B9929CA0b81Ea0DE38E" ], // USUALX and USUAL 
   }
 })
