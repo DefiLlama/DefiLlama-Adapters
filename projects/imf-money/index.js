671f3b5cf29f6b91b372c6c39290880b02098923
miffy <miffybunnyrabbit@gmail.com>
2025-04-16 08:56:21 +1000

    Updated Wallet Address for Morpho Blue
    IMF is now a co-ordination layer on top of Morpho

--- projects/imf-money/index.js
+++ projects/imf-money/index.js
@@ -1,11 +1,10 @@
 const { sumTokensExport } = require('../helper/unwrapLPs')
-const PEPE_TOKEN_CONTRACT = '0x6982508145454Ce325dDbE47a25d4ec3d2311933';
 const MOG_TOKEN_CONTRACT = '0xaaee1a9723aadb7afa2810263653a34ba2c21c7a';
-const LOCKED_MONEY_CONTRACT = '0x30F75834cB406b7093208Fda7F689938aCBD1EeB'; //wallet that has all the locked money
+const MORPHO_BLUE_CONTRACT = '0xbbbbbbbbbb9cc5e90e3b3af64bdaf62c37eeffcb'; //Morpho Blue wallet address where deposited memes are stored. 
 
 module.exports = {
-  methodology: 'Sums the value of deposited memes',
+  methodology: 'Sums the value of deposited memes in the Morpho Pool',
   ethereum: {
-    tvl: sumTokensExport({ owner: LOCKED_MONEY_CONTRACT, tokens: [PEPE_TOKEN_CONTRACT,MOG_TOKEN_CONTRACT]}),
+    tvl: sumTokensExport({ owner: MORPHO_BLUE_CONTRACT, tokens: [MOG_TOKEN_CONTRACT]}),
   }
 }; 
