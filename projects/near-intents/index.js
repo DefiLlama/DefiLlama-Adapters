1d45b07b5153514459914bc28f67bdc8ec791ad9
Abhishek Anirudhan <22417587+abhishekanirudhan@users.noreply.github.com>
2025-04-16 13:01:32 +0530

    updated near-intents index.js with reference to httpGet helper

--- projects/near-intents/index.js
+++ projects/near-intents/index.js
@@ -1,18 +1,15 @@
-import axios, { AxiosRequestConfig } from "axios"
+import { httpGet } from "../helper/chain/near";
 
 const api_fee = "https://flipsidecrypto.xyz/api/v1/queries/82a54d67-5614-4b3c-b2fa-696396dc5c30/data/latest"
-
 
 const adapter = {
     adapter: {
         "near": {
           start: '2025-04-16',
           fetch: async () => {
-            const fee_result = await httpGet(api_fee);
-            const revenue_result = await httpGet(api_revenue);
+            const result = await httpGet(payload);
             return {
-                dailyFees: fee_result?.data?.total_fee || '0',
-                dailyRevenue: revenue_result?.data.total_revenue || '0',
+                tvl: result?.TOTAL_NET_MINTED_VOLUME || '0',
             }
           }
         }
