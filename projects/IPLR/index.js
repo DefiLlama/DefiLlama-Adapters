ea58a4bfb321423a9dc282f4e13d227874966f5c
Jstrode39 <172228083+Jstrode39@users.noreply.github.com>
2025-04-16 07:35:38 -0400

    Fix: Move TVL to pool2 and return SOL balance only per review

--- projects/IPLR/index.js
+++ projects/IPLR/index.js
@@ -1,32 +1,22 @@
 const { getTokenBalance } = require('../helper/solana')
-const { fetchURL } = require('../helper/utils')
 
-const IPLR_MINT = 'EyzgnBfHGe9hh169B8993muBVcoeURCnSgPbddBeSybo'
 const SOL_MINT = 'So11111111111111111111111111111111111111112'
 const LP_ACCOUNT = 'GpMZbSM2GgvTKHJirzeGfMFoaZ8UR2X7F4v8vHTvxFbL'
 
-async function tvl() {
-  const [iplrBalance, solBalance] = await Promise.all([
-    getTokenBalance(IPLR_MINT, LP_ACCOUNT),
-    getTokenBalance(SOL_MINT, LP_ACCOUNT),
-  ])
-
-  const solPriceData = await fetchURL('https://coins.llama.fi/prices/current/solana:So11111111111111111111111111111111111111112')
-  const solPrice = solPriceData.data.coins['solana:So11111111111111111111111111111111111111112'].price
-
-  const solValueUSD = solBalance * solPrice
-  const estimatedTVL = solValueUSD * 2
+async function pool2() {
+  const solBalance = await getTokenBalance(SOL_MINT, LP_ACCOUNT)
 
   return {
-    tether: estimatedTVL
+    'solana:So11111111111111111111111111111111111111112': solBalance
   }
 }
 
 module.exports = {
   timetravel: false,
   misrepresentedTokens: true,
-  methodology: 'Estimates TVL from the IPLR/SOL LP vault on Solana using real token balances and current SOL price.',
+  methodology: 'Tracks SOL value in the IPLR/SOL Raydium LP vault as pool2 TVL.',
   solana: {
-    tvl,
+    tvl: () => ({}), // Required by DeFiLlama format
+    pool2
   }
 }
