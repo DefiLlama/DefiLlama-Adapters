a5525168bbf7089b497cf120212f8901e664b15f
Abhishek Anirudhan <22417587+abhishekanirudhan@users.noreply.github.com>
2025-04-16 17:51:00 +0530

    updated balances to return array

--- projects/near-intents/index.js
+++ projects/near-intents/index.js
@@ -1,21 +1,20 @@
 const { httpGet } = require("../helper/chain/near")
-
-const api_tvl = "https://flipsidecrypto.xyz/api/v1/queries/82a54d67-5614-4b3c-b2fa-696396dc5c30/data/latest"
-
+const { call, sumSingleBalance } = require('../helper/chain/near')
 
 function tvl() {
     return async () => {
-      const assetsCallResponse = await httpGet(api_tvl);
+        const api_tvl = "https://flipsidecrypto.xyz/api/v1/queries/82a54d67-5614-4b3c-b2fa-696396dc5c30/data/latest";
+        const assetsCallResponse = await httpGet(api_tvl);
   
-      const balances = assetsCallResponse?.TOTAL_NET_MINTED_VOLUME || '0';
+        const balances = {'NEAR': assetsCallResponse[0].TOTAL_NET_MINTED_VOLUME};
   
-      return balances;
+        return balances;
     }
   }
 
 
 module.exports = {
     near: {
-        tvl: tvl()
+        tvl: tvl(),
     }
 }
