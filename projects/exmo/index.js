98afe029f68dd7e1bd468ec0d540b5522962268d
Andre Kidd <andrey@exmo.com>
2025-04-17 11:49:00 +0300

    Fix: Update Bitcoin owners and remove Cosmos configuration

--- projects/exmo/index.js
+++ projects/exmo/index.js
@@ -3,7 +3,8 @@ const { cexExports } = require('../helper/cex')
 const config = {
   bitcoin: {
     owners: [
-      'bc1qxas7gqvqshpwl6dy85fm5rcgf9je2th9xnlwp4',
+      'bc1qa6ztklpcshk048mv5f2f877rudmyv2jaq05n5k',
+      'bc1qgq64fzjjk4rsh008rdq542hve5vmz2uaep7lmx',
     ],
   },
   ethereum: {
@@ -11,11 +12,6 @@ const config = {
       '0x112A52893b96E9679E854934A62841051a679dAA',
       '0x6297C8ec7662c10CdACFb3e9C04B571528d277E2',
     ],
-  },
-  cosmos: {
-    owners: [
-      'cosmos1j60cjs6fmm47xhyl0rtyzruz7aunmu2gxsqq87',
-    ]
   },
   ethereumclassic: {
     owners: [
