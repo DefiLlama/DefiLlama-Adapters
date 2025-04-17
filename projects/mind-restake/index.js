6a15120b047176e90c1ce60f057c2a0df0183b49
joshua <joshua@mindnetwork.xyz>
2025-04-17 12:24:27 +0800

    Update TVL Calculation Method For Mind Network

--- projects/mind-restake/index.js
+++ projects/mind-restake/index.js
@@ -1,9 +1,9 @@
 const config = {
-  bsc: "0x1987d8109638A028BB8bE654531B15642a8708E3",
-  fhe:"0xe8451dC0959469e42F2679b3eC085e58FB212b11"
+  bsc: "0xb080B94052f039eC2CA8BBaF7Ec13329d1926973",
+  fhe:"0xdeD96288c99145da4800f55355A2466f6238fBBE"
 };
 
-// const token = "0xd55C9fB62E176a8Eb6968f32958FeFDD0962727E"
+const token = "0xd55C9fB62E176a8Eb6968f32958FeFDD0962727E"
 
 module.exports = {
   methodology: "Counts the total amount of FHE tokens deposited in Mind Agentic World contracts on BSC and MindChain.",
@@ -13,10 +13,11 @@ Object.keys(config).forEach(chain => {
   const target = config[chain]
   module.exports[chain] = {
     tvl: async (api) => {
-      const tvlAmount = await api.call({
-        abi: "function allHubAssetAmount() view returns (uint256)", 
-        target,
-      })
+      const tvlAmount =  await api.call({
+        abi: 'erc20:balanceOf',
+        target: token,
+        params: [target],
+      }); 
        const bigIntAmount = BigInt(tvlAmount); 
        const amountInToken = bigIntAmount / BigInt(10 ** 18); 
       // api.add(token, tvlAmount,{ priceChain: "bsc" })
