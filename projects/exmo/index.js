7cf9c491abee1d659a70163e681437ef5ce8f3da
Andre Kidd <andrey@exmo.com>
2025-04-17 11:36:11 +0300

    Refactor: Update owners in config for Bitcoin, Cosmos, and Ethereum Classic

--- projects/exmo/index.js
+++ projects/exmo/index.js
@@ -1,19 +1,25 @@
 const { cexExports } = require('../helper/cex')
 
 const config = {
-  ethereum: {
+  bitcoin: {
     owners: [
-        '0x7029B8aaE2399a9f4B82d4516D6D16A35A52ff2e',
+      'bc1qxas7gqvqshpwl6dy85fm5rcgf9je2th9xnlwp4',
     ],
   },
-  doge: {
+  ethereum: {
     owners: [
-        'DC1GBAsLTJMvShigDk4UR3oVnkmiH4xaoE',
+      '0x112A52893b96E9679E854934A62841051a679dAA',
+      '0x6297C8ec7662c10CdACFb3e9C04B571528d277E2',
+    ],
+  },
+  cosmos: {
+    owners: [
+      'cosmos1j60cjs6fmm47xhyl0rtyzruz7aunmu2gxsqq87',
     ]
   },
-  ripple: {
+  ethereumclassic: {
     owners: [
-        'rDfvwaonnG198FJKiugQDn96kVW4Ej6bUX',
+      '0xD936704458E4f8525B6bE7C0ebC5fE268BaB4977',
     ]
   },
 }
