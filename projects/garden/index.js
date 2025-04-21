604a4b6a3e0190fcc5bddc79e28491069c3da750
SATHVIK SVS <svssathvik77@gmail.com>
2025-04-21 10:42:25 +0530

    update garden tvl data

--- projects/garden/index.js
+++ projects/garden/index.js
@@ -2,11 +2,10 @@ const ADDRESSES = require("../helper/coreAssets.json");
 const { sumTokensExport } = require("../helper/sumTokens");
 const sdk = require("@defillama/sdk");
 const bitcoinAddressBook = require('../helper/bitcoin-book/index.js');
-const { methodology } = require("../allinx.js");
 
 const SEED_TOKEN_ADDRESS = "0x86f65121804D2Cdbef79F9f072D4e0c2eEbABC08"; // SEED TOKEN (Garden's Governance Token) Address
 const STAKING_CONTRACT_ADDRESS = "0xe2239938Ce088148b3Ab398b2b77Eedfcd9d1AfC";
-const WBTC_LIQUIDITY_ADDRESSES = ["0x9DD9C2D208B07Bf9A4eF9CA311F36d7185749635","0xEF440AC3D77F906F9F0E1C15B35BCF300B4C427C"];
+const COBI_ADDRESSES = ["0x9DD9C2D208B07Bf9A4eF9CA311F36d7185749635","0xEF440AC3D77F906F9F0E1C15B35BCF300B4C427C", "0x3fDEe07b0756651152BF11c8D170D72d7eBbEc49"];
 
 module.exports = {
   bitcoin: {
@@ -16,18 +15,24 @@ module.exports = {
   },
   ethereum: {
     tvl: sumTokensExport({
-      owners: WBTC_LIQUIDITY_ADDRESSES,
-      tokens: [ADDRESSES.ethereum.WBTC],
+      owners: COBI_ADDRESSES,
+      tokens: [ADDRESSES.ethereum.WBTC, ADDRESSES.ethereum.cbBTC, ADDRESSES.ethereum.USDC],
     }),
   },
   arbitrum: {
     tvl: sumTokensExport({
-      owners: WBTC_LIQUIDITY_ADDRESSES,
-      tokens: [ADDRESSES.arbitrum.WBTC],
+      owners: COBI_ADDRESSES,
+      tokens: [ADDRESSES.arbitrum.WBTC, ADDRESSES.arbitrum.USDC],
     }),
     staking: sumTokensExport({
       owners: [STAKING_CONTRACT_ADDRESS],
       tokens: [SEED_TOKEN_ADDRESS],
     }),
   },
+  base: {
+    tvl: sumTokensExport({
+      owners: COBI_ADDRESSES,
+      tokens: [ADDRESSES.base.USDC, ADDRESSES.base.cbBTC],
+    }),
+  }
 };
