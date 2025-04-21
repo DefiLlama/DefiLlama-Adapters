41b851e54cdc230031dccc1e46f48c90519ea331
joshua <joshua@mindnetwork.xyz>
2025-04-21 09:07:05 +0800

    update tvl to staking

--- projects/mind-restake/index.js
+++ projects/mind-restake/index.js
@@ -1,28 +1,28 @@
 const config = {
-  bsc: "0xb080B94052f039eC2CA8BBaF7Ec13329d1926973",
-  fhe:"0xdeD96288c99145da4800f55355A2466f6238fBBE"
+  bsc: "0xb080B94052f039eC2CA8BBaF7Ec13329d1926973",     // BSC staking contract
+  fhe: "0xdeD96288c99145da4800f55355A2466f6238fBBE", // MindChain staking contract
 };
 
-const token = "0xd55C9fB62E176a8Eb6968f32958FeFDD0962727E"
+const FHE = "0xd55C9fB62E176a8Eb6968f32958FeFDD0962727E"; // FHE token address
 
 module.exports = {
   methodology: "Counts the total amount of FHE tokens deposited in Mind Agentic World contracts on BSC and MindChain.",
-}
+};
 
-Object.keys(config).forEach(chain => {
-  const target = config[chain]
+Object.entries(config).forEach(([chain, stakingContract]) => {
   module.exports[chain] = {
-    tvl: async (api) => {
-      const tvlAmount =  await api.call({
-        abi: 'erc20:balanceOf',
-        target: token,
-        params: [target],
-      }); 
-       const bigIntAmount = BigInt(tvlAmount); 
-       const amountInToken = bigIntAmount / BigInt(10 ** 18); 
-      // api.add(token, tvlAmount,{ priceChain: "bsc" })
-      api.addCGToken("mind-network", amountInToken)
-    }
-  }
-})
-      
+    tvl: async () => ({}), // No external TVL for now
+    staking: async (api) => {
+      const balance = await api.call({
+        abi: "erc20:balanceOf",
+        target: FHE,
+        params: [stakingContract],
+      });
+
+      const bigIntAmount = BigInt(balance); 
+      const amountInToken = bigIntAmount / BigInt(10 ** 18); 
+
+      api.addCGToken("mind-network", amountInToken);
+    },
+  };
+});
