45755591f70bb6a198ec3a9823828331a14ebaa5
liminalAngel <veronikabogdanova268@gmail.com>
2025-04-21 15:31:58 +0400

    move bmTON address to projects/bemo-v2/index.js

--- projects/bemo-v2/index.js
+++ projects/bemo-v2/index.js
@@ -1,4 +1,3 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const { call } = require("../helper/chain/ton");
 
 module.exports = {
@@ -6,7 +5,7 @@ module.exports = {
   methodology: "bmTon",
   ton: {
     tvl: async () => {
-      const result = await call({ target: ADDRESSES.ton.bmTON, abi: "get_full_data" })
+      const result = await call({ target: "EQCSxGZPHqa3TtnODgMan8CEM0jf6HpY-uon_NMeFgjKqkEY", abi: "get_full_data" })
       return { "coingecko:the-open-network": result[1] / 1e9 };
     }
   }
