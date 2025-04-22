e2c20a967981600fc2e8580555b03cac24005451
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-22 11:15:15 +0200

    track magic.fi

---
+++ projects/magic-fi/index.js
@@ -0,0 +1,40 @@
+const ADDRESSES = require('../helper/coreAssets.json')
+const { call, sumTokens } = require('../helper/chain/near');
+const { sumTokensExport } = require('../helper/unwrapLPs');
+const { sumTokens2 } = require('../helper/solana');
+
+const ASSET_MANAGER_CONTRACT = 'asset-manager.orderly-network.near';
+const GET_LISTED_TOKENS_METHOD = 'get_listed_tokens';
+
+const owner = '0x816f722424b49cf1275cc86da9840fbd5a6167e9'
+
+const tokenAddress = {
+  arbitrum: ADDRESSES.arbitrum.USDC_CIRCLE,
+  optimism: ADDRESSES.optimism.USDC_CIRCLE,
+  base: ADDRESSES.base.USDC,
+  mantle: ADDRESSES.mantle.USDC,
+  polygon: ADDRESSES.polygon.USDC_CIRCLE,
+  ethereum: ADDRESSES.ethereum.USDC,
+  sei: ADDRESSES.sei.USDC,
+  berachain: ADDRESSES.berachain.USDC,
+  avax: ADDRESSES.avax.USDC,
+  sonic: ADDRESSES.sonic.USDC_e,
+}
+
+async function tvl() {
+  let ftTokens = await call(ASSET_MANAGER_CONTRACT, GET_LISTED_TOKENS_METHOD, {})
+  return sumTokens({ owners: [ASSET_MANAGER_CONTRACT], tokens: ftTokens });
+}
+
+module.exports = {
+  timetravel: false,
+  near: { tvl, },
+  solana: { tvl: () => sumTokens2({ tokenAccounts: ['77puyQ4K4ov82qzBuda4q9iMh2Ux49YnnBNWqxQkcrXE'] }) },
+  methodology: 'All the tokens deposited into Magic-Fi Network by chain'
+};
+
+Object.keys(tokenAddress).forEach(chain => {
+  module.exports[chain] = {
+    tvl: sumTokensExport({ owner, tokens: [tokenAddress[chain]] })
+  }
+})
