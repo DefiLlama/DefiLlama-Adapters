8ccd53201d4c466920cdd777b6c1a6a8451636a7
SushiMutt <sushimutt@users.noreply.github.com>
2025-04-24 10:07:38 +0700

    Updated to use uniV3Export

--- projects/haven1/index.js
+++ projects/haven1/index.js
@@ -1,3 +1,6 @@
-const {uniTvlExport} = require('../helper/unknownTokens')
 
-module.exports = uniTvlExport('haven1', '0xEf3D1d7d3B10Ff716b1b22C9536bd22Dd0fE60ab')
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+  haven1: { factory: "0xEf3D1d7d3B10Ff716b1b22C9536bd22Dd0fE60ab", fromBlock: 13273, },
+});
