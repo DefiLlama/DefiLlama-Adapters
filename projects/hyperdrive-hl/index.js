e8dee11b1264a0386b767ba63a334e1e9fc1a8e3
Kevin Li <kevinli29033@gmail.com>
2025-04-28 12:22:55 -0700

    update

--- projects/hyperdrive-hl/index.js
+++ projects/hyperdrive-hl/index.js
@@ -1,52 +1,50 @@
-const { sumTokens2 } = require('../helper/unwrapLPs');
-
-const PORTFOLIO = ""; 
-const DEPOSITOR_VAULT = "";
+const MARKET_ROUTER = "FILL";
+const MARKET_LENS = "0x7fB0d63E84D847569ca75A6cdbA283bA1401F9f6";
 
 const ABI = {
-  Portfolio: {
-    getAssets: "function getAssets() view returns (tuple(address token, address custodian, address priceOracle, uint256 maxSupply, tuple(address callback, uint16 options)[] hooks)[])",
+  MarketRouter: {
+    getMarkets: "function getMarkets() view returns (address[])"
   },
-  DepositorVault: {
-    getTotalLiabilities: "function getTotalLiabilities() view returns (uint256)",
-    getUnderlyingAsset: "function getUnderlyingAsset() view returns (address)"
+  MarketLens: {
+    getMarketQuery: "function getMarketQuery(uint256 marketId) view returns (tuple(address marketAsset, string marketAssetSymbol, uint8 marketAssetDecimals, uint256 maxSupply, uint256 totalShares, uint256 totalAssets, uint256 exchangeRate, uint256 totalReserveAssets, uint256 totalLiabilities, uint256 utilization, uint256 borrowRate, uint256 supplyRate))"
   }
 };
 
 async function tvl(api) {
-  const tokensAndOwners = [];
-  
-  const assets = await api.call({
-    abi: ABI.Portfolio.getAssets,
-    target: PORTFOLIO
+  const markets = await api.call({
+    abi: ABI.MarketRouter.getMarkets,
+    target: MARKET_ROUTER
   });
   
-  for (const asset of assets) {
-    tokensAndOwners.push([asset.token, asset.custodian]);
+  for (let i = 0; i < markets.length; i++) {
+    const marketData = await api.call({
+      abi: ABI.MarketLens.getMarketQuery,
+      target: MARKET_LENS,
+      params: [i]
+    });
+    
+    api.add(marketData.marketAsset, marketData.totalAssets);
   }
   
-  const underlyingAsset = await api.call({
-    abi: ABI.DepositorVault.getUnderlyingAsset,
-    target: DEPOSITOR_VAULT
-  });
-  
-  tokensAndOwners.push([underlyingAsset, DEPOSITOR_VAULT]);
-  
-  return sumTokens2({ api, tokensAndOwners });
+  return api.getBalances();
 }
 
 async function borrowed(api) {
-  const underlyingAsset = await api.call({
-    abi: ABI.DepositorVault.getUnderlyingAsset,
-    target: DEPOSITOR_VAULT
+   const markets = await api.call({
+    abi: ABI.MarketRouter.getMarkets,
+    target: MARKET_ROUTER
   });
   
-  const liabilities = await api.call({
-    abi: ABI.DepositorVault.getTotalLiabilities,
-    target: DEPOSITOR_VAULT
-  });
+  for (let i = 0; i < markets.length; i++) {
+    const marketData = await api.call({
+      abi: ABI.MarketLens.getMarketQuery,
+      target: MARKET_LENS,
+      params: [i]
+    });
+    
+    api.add(marketData.marketAsset, marketData.totalLiabilities);
+  }
   
-  api.add(underlyingAsset, liabilities);
   return api.getBalances();
 }
 
