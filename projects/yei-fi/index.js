db34adfa559d7ee19a725ed277274356a11c49d8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-04-28 12:23:51 +0200

    Update index.js

--- projects/yei-fi/index.js
+++ projects/yei-fi/index.js
@@ -1,34 +1,11 @@
 const { aaveExports } = require("../helper/aave");
-const sdk = require("@defillama/sdk");
+const { mergeExports } = require("../helper/utils");
 
-function yeiExports(dataHelpers) {
-  const exports = dataHelpers.map((h) =>
-    aaveExports(undefined, "", undefined, [h])
-  );
-
-  return {
-    tvl: async (api) => {
-      const balances = {};
-      for (const e of exports) {
-        const v = await e.tvl(api);
-        sdk.util.mergeBalances(balances, v);
-      }
-      return balances;
-    },
-    borrowed: async (api) => {
-      const balances = {};
-      for (const e of exports) {
-        const v = await e.borrowed(api);
-        sdk.util.mergeBalances(balances, v);
-      }
-      return balances;
-    },
-  };
-}
-
-module.exports = {
-  sei: yeiExports([
-    "0x60c82a40c57736a9c692c42e87a8849fb407f0d6", // main
-    "0xE77F4334D2Ce16c19F66fD62c653377A39AEFee1", // solv
-  ]),
-};
+module.exports = mergeExports([
+  {
+    sei: aaveExports(undefined, '', undefined, ['0x60c82a40c57736a9c692c42e87a8849fb407f0d6']), // main
+  },
+  {
+    sei: aaveExports(undefined, '', undefined, ['0xE77F4334D2Ce16c19F66fD62c653377A39AEFee1']),  // solv
+  }
+])
