291a61d6e7b6e4242b249200df212d701ef8c141
Adin Lieblein <adinmlieblein@gmail.com>
2025-04-28 19:58:39 -0500

    Turtle Club Treasury & TVL

---
+++ projects/TurtleClub/index.js
@@ -0,0 +1,11 @@
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const { tokens: tokensObj, vaultContracts: owners } = require("./assets");
+
+const tokens = Object.values(tokensObj).map(chain => Object.values(chain)).flat();
+
+module.exports = {
+    // All vaults currently ethereum
+    ethereum: {
+        tvl: sumTokensExport({ owners, tokens, permitFailure: true, onlyWhitelisted: false }),
+    },
+};
