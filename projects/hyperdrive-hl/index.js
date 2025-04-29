3cd716532b9e301c905df44b21649e6406f2cc33
Kevin Li <kevinli29033@gmail.com>
2025-04-29 08:14:06 -0700

    proper get markets

--- projects/hyperdrive-hl/index.js
+++ projects/hyperdrive-hl/index.js
@@ -1,20 +1,44 @@
-const MARKET_ROUTER = "FILL";
+const ethers = require("ethers");
+
 const MARKET_LENS = "0x7fB0d63E84D847569ca75A6cdbA283bA1401F9f6";
+const ADDRESS_REGISTRY = "0x86ccFbEc579D491e9b37354187dD48a1a9C7c1E7"; 
 
 const ABI = {
-  MarketRouter: {
-    getMarkets: "function getMarkets() view returns (address[])"
+  AddressRegistry: {
+    getAddresses: "function getAddresses(bytes32[] keys) view returns (address[])"
   },
   MarketLens: {
     getMarketQuery: "function getMarketQuery(uint256 marketId) view returns (tuple(address marketAsset, string marketAssetSymbol, uint8 marketAssetDecimals, uint256 maxSupply, uint256 totalShares, uint256 totalAssets, uint256 exchangeRate, uint256 totalReserveAssets, uint256 totalLiabilities, uint256 utilization, uint256 borrowRate, uint256 supplyRate))"
   }
 };
 
-async function tvl(api) {
-  const markets = await api.call({
-    abi: ABI.MarketRouter.getMarkets,
-    target: MARKET_ROUTER
+async function lookupAddresses(keys) {
+  return await api.call({
+    abi: ABI.AddressRegistry.getAddresses,
+    target: ADDRESS_REGISTRY,
+    params: [keys.map(key => ethers.keccak256(ethers.toUtf8Bytes(key)))]
   });
+}
+
+async function getAllMarkets() {
+  const markets = [];
+  let i = 1;
+  
+  while (true) {
+    const addresses = await lookupAddresses([`hyperdrive.market.${i}`]);
+    if (addresses[0] !== '0x0000000000000000000000000000000000000000') {
+      markets.push(addresses[0]);
+      i++;
+    } else {
+      break;
+    }
+  }
+  
+  return markets;
+}
+
+async function tvl(api) {
+  const markets = await getAllMarkets();
   
   for (let i = 0; i < markets.length; i++) {
     const marketData = await api.call({
@@ -30,10 +54,7 @@ async function tvl(api) {
 }
 
 async function borrowed(api) {
-   const markets = await api.call({
-    abi: ABI.MarketRouter.getMarkets,
-    target: MARKET_ROUTER
-  });
+  const markets = await getAllMarkets();
   
   for (let i = 0; i < markets.length; i++) {
     const marketData = await api.call({
