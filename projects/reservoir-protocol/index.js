2a36d54c55ee2a4064a7ed9c0a2e4a41619024dd
eukadish <eugene.kadish@gmail.com>
2025-04-30 07:01:12 -0400

    price and tvl calc for kodiak pool

--- projects/reservoir-protocol/index.js
+++ projects/reservoir-protocol/index.js
@@ -20,18 +20,34 @@ Object.keys(config).forEach(chain => {
       const byusdToken = '0x688e72142674041f8f6af4c808a4045ca1d6ac82';
 
       const owner = '0x0db79c0770E1C647b8Bb76D94C22420fAA7Ac181';
+      const owner2 = '0xD6D83e479359766f21A63b20d6AF43A138356EbA';
+      const kodiakPool = '0xD6D83e479359766f21A63b20d6AF43A138356EbA';
+
+      const lpToken2 = '0x7fd165B73775884a38AA8f2B384A53A3Ca7400E6';
+      const rUSDToken = '0x09D4214C03D01F49544C0448DBE3A27f768F2b34';
+
+      const infraredVault = '0x1C5879B75be9E817B1607AFb6f24F632eE6F8820';
+
 
       module.exports[chain] = {
         tvl: async (api) => {
-          const lpBalance = await api.call({ abi: 'address:balanceOf', target: lpToken, params: owner })
-          const honeyBalance = await api.call({ abi: 'address:balanceOf', target: honeyToken, params: owner })
-          const byusdBalance = await api.call({ abi: 'address:balanceOf', target: byusdToken, params: owner })
+          const lpBalance = await api.call({ abi: 'address:balanceOf', target: lpToken, params: owner });
+          const honeyBalance = await api.call({ abi: 'address:balanceOf', target: honeyToken, params: owner });
+          const byusdBalance = await api.call({ abi: 'address:balanceOf', target: byusdToken, params: owner });
+
+          const rUSDBalance = await api.call({ abi: 'address:balanceOf', target: rusdToken, params: owner2 });
+          const honeyBalance2 = await api.call({ abi: 'address:balanceOf', target: honeyToken, params: owner2 });
+          const lpToken2TotalSupply = await api.call({ abi: 'address:TotalSupply', target: lpToken2 });
+
+          const price = (rUSDBalance + honeyBalance) * 1e8 / lpToken2TotalSupply;
+          const balance = await api.call({ abi: 'address:balanceOf', target: infraredVault, params: kodiakPool });
 
           api.add(lpToken, lpBalance);
-          api.add(honeyToken, honeyBalance)
-          api.add(byusdToken, byusdBalance)
+          api.add(honeyToken, honeyBalance);
+          api.add(byusdToken, byusdBalance);
+          api.add(kodiakPool, balance);
 
-          return api.getBalances()
+          return api.getBalances();
         }
       }
     }
