82174d4c5d78d86af554ea50538337fb177e56d8
Wissam Jarjoui <1377465+bosswissam@users.noreply.github.com>
2025-04-30 18:48:45 -0700

    Add support for kodiak islands

--- projects/reservoir-protocol/index.js
+++ projects/reservoir-protocol/index.js
@@ -1,69 +1,147 @@
-const ADDRESSES = require('../helper/coreAssets.json')
+const ADDRESSES = require('../helper/coreAssets.json');
+
+const ASSETS = {
+  rUSD: {
+    address: '0x09D4214C03D01F49544C0448DBE3A27f768F2b34',
+    decimals: 18
+  },
+  HONEY: {
+    address: ADDRESSES.berachain.HONEY,
+    decimals: 18
+  },
+  USDT0: {
+    address: '0x779Ded0c9e1022225f8E0630b35a9b54bE713736',
+    decimals: 6
+  }
+};
 
 const config = {
-    ethereum: [
-        // https://docs.reservoir.xyz/products/proof-of-reserves
-        '0x0c7e4342534e6e8783311dCF17828a2aa0951CC7',
-        '0x9BB2c38F57883E5285b7c296c66B9eEA4769eF80',
-        '0x99A95a9E38e927486fC878f41Ff8b118Eb632b10',
-        '0xE45321525c85fcc418C88E606B96daD8cBcc047f',
-        '0x841DB2cA7E8A8C2fb06128e8c58AA162de0CfCbC',
-        '0x99E8903bdEFB9e44cd6A24B7f6F97dDd071549bc'
-        // '0x31Eae643b679A84b37E3d0B4Bd4f5dA90fB04a61', - exluded RUSD because it is project's own token
+  ethereum: [
+    // https://docs.reservoir.xyz/products/proof-of-reserves
+    '0x0c7e4342534e6e8783311dCF17828a2aa0951CC7',
+    '0x9BB2c38F57883E5285b7c296c66B9eEA4769eF80',
+    '0x99A95a9E38e927486fC878f41Ff8b118Eb632b10',
+    '0xE45321525c85fcc418C88E606B96daD8cBcc047f',
+    '0x841DB2cA7E8A8C2fb06128e8c58AA162de0CfCbC',
+    '0x99E8903bdEFB9e44cd6A24B7f6F97dDd071549bc'
+    // '0x31Eae643b679A84b37E3d0B4Bd4f5dA90fB04a61', - exluded RUSD because it is project's own token
+  ],
+  berachain: {
+    adapters: [
+      '0x0db79c0770E1C647b8Bb76D94C22420fAA7Ac181',
+      '0x6811742721DcCe83942739d44E40f140B5BCee37',
+      '0x8Cc5a546408C6cE3C9eeB99788F9EC3b8FA6b9F3'
     ],
-  berachain: []
+    infraredVaults: [
+      {
+        vault: '0xbbB228B0D7D83F86e23a5eF3B1007D010058161',
+        holder: '0x0db79c0770E1C647b8Bb76D94C22420fAA7Ac181'
+      }
+    ],
+    kodiakIslands: [
+      {
+        name: 'rUSD-HONEY',
+        pool: '0xD6D83e479359766f21A63b20d6AF43A138356EbA',
+        island: '0x7fd165B73775884a38AA8f2B384A53A3Ca7400E6',
+        assets: [ASSETS.rUSD, ASSETS.HONEY],
+        vault: '0x1C5879B75be9E817B1607AFb6f24F632eE6F8820',
+        holder: '0x6811742721DcCe83942739d44E40f140B5BCee37'
+      }
+      /*
+      {
+        name: 'rUSD-USD₮0',
+        pool: '0x8b711A1d31149397845322Dde3514437F6bd2Eb3',
+        island: '0x1fb6c1ade4f9083b2ea42ed3fa9342e41788d4b5',
+        assets: [ASSETS.rUSD, ASSETS.USDT0],
+        vault: '0x1fb6c1ade4f9083b2ea42ed3fa9342e41788d4b5',
+        holder: '0x8Cc5a546408C6cE3C9eeB99788F9EC3b8FA6b9F3'
+      }
+      */
+    ],
+    tokens: [
+      '0xdE04c469Ad658163e2a5E860a03A86B52f6FA8C8',
+      '0x549943e04f40284185054145c6E4e9568C1D3241',
+      ADDRESSES.berachain.HONEY,
+      '0x688e72142674041f8f6af4c808a4045ca1d6ac82',
+      '0x88C4F34B86907b7221c0061f7a0bBeC9391Ea727',
+      '0x7fd165b73775884a38aa8f2b384a53a3ca7400e6',
+      '0x1fb6c1ade4f9083b2ea42ed3fa9342e41788d4b5'
+    ]
+  }
+};
+
+async function calculateKodiakBalances(api, kodiakIsland) {
+  const { pool, island, assets, vault, holder } = kodiakIsland;
+  const balances = await Promise.all(
+    assets.map(async (asset) => {
+      return (
+        (await api.call({
+          abi: 'erc20:balanceOf',
+          target: asset.address,
+          params: [pool]
+        })) *
+        10 ** (18 - asset.decimals)
+      );
+    })
+  );
+
+  const poolBalance = balances.reduce((acc, balance) => {
+    return acc + balance;
+  }, 0);
+
+  const islandTotalSupply = await api.call({
+    abi: 'erc20:totalSupply',
+    target: island
+  });
+
+  const price = poolBalance / islandTotalSupply;
+
+  const balance = await api.call({
+    abi: 'erc20:balanceOf',
+    target: vault,
+    params: [holder]
+  });
+
+  api.add(island, balance * price);
 }
 
-Object.keys(config).forEach(chain => {
-    if (chain === 'berachain') {
-      const byusdHoneyVault = '0xbbB228B0D7D83F86e23a5eF3B1007D0100581613';
-      const byusdHoneyToken = '0xdE04c469Ad658163e2a5E860a03A86B52f6FA8C8';
-      const honeyToken = '0xFCBD14DC51f0A4d49d5E53C2E0950e0bC26d0Dce';
-      const byusdToken = '0x688e72142674041f8f6af4c808a4045ca1d6ac82';
-
-      const owner = '0x0db79c0770E1C647b8Bb76D94C22420fAA7Ac181';
-      const kodiakPool = '0xD6D83e479359766f21A63b20d6AF43A138356EbA';
-
-      const lpToken2 = '0x7fd165B73775884a38AA8f2B384A53A3Ca7400E6';
-      const rUSDToken = '0x09D4214C03D01F49544C0448DBE3A27f768F2b34';
-
-      const infraredVault = '0x1C5879B75be9E817B1607AFb6f24F632eE6F8820';
-
-      module.exports[chain] = {
-        tvl: async (api) => {
-          const lpBalance = await api.call({ abi: 'erc20:balanceOf', target: byusdHoneyVault, params: [owner] });
-          const honeyBalance = await api.call({ abi: 'erc20:balanceOf', target: honeyToken, params: [owner] });
-          const byusdBalance = await api.call({ abi: 'erc20:balanceOf', target: byusdToken, params: [owner] });
-
-          const rUSDBalance = await api.call({ abi: 'erc20:balanceOf', target: rUSDToken, params: [kodiakPool] });
-          const honeyBalance2 = await api.call({ abi: 'erc20:balanceOf', target: honeyToken, params: [kodiakPool] });
-          const lpToken2TotalSupply = await api.call({ abi: 'erc20:totalSupply', target: lpToken2 });
-
-          const price = (rUSDBalance + honeyBalance2) * 1e8 / lpToken2TotalSupply;
-          const balance = await api.call({ abi: 'erc20:balanceOf', target: infraredVault, params: [kodiakPool] });
-
-          api.add(byusdHoneyToken, lpBalance);
-          api.add(honeyToken, honeyBalance);
-          api.add(byusdToken, byusdBalance);
-          api.add(kodiakPool, balance * price / 1e8);
-
-          return api.getBalances();
-        }
+Object.keys(config).forEach((chain) => {
+  if (chain === 'berachain') {
+    module.exports[chain] = {
+      tvl: async (api) => {
+        await Promise.all(
+          config[chain].kodiakIslands.map(
+            async (kodiakIsland) =>
+              await calculateKodiakBalances(api, kodiakIsland)
+          )
+        );
+        console.log(api.getBalances());
+        return api.getBalances();
       }
-    }
-    else if (chain === 'ethereum') {
-      const funds = config[chain]
-      module.exports[chain] = {
-        tvl: async (api) => {
-          const tokens = await api.multiCall({ abi: 'address:underlying', calls: funds })
-          const bals = await api.multiCall({ abi: 'uint256:totalValue', calls: funds })
-          const decimals = await api.multiCall({ abi: 'uint8:decimals', calls: tokens })
-          bals.forEach((v, i) => bals[i] = v * 10 ** (decimals[i] - 18))
-          api.add(tokens, bals)
-          return api.sumTokens({
-            owner: '0x4809010926aec940b550D34a46A52739f996D75D', token: ADDRESSES.ethereum.USDC
-          })
-        }
+    };
+  } else if (chain === 'ethereum') {
+    const funds = config[chain];
+    module.exports[chain] = {
+      tvl: async (api) => {
+        const tokens = await api.multiCall({
+          abi: 'address:underlying',
+          calls: funds
+        });
+        const bals = await api.multiCall({
+          abi: 'uint256:totalValue',
+          calls: funds
+        });
+        const decimals = await api.multiCall({
+          abi: 'uint8:decimals',
+          calls: tokens
+        });
+        bals.forEach((v, i) => (bals[i] = v * 10 ** (decimals[i] - 18)));
+        api.add(tokens, bals);
+        return api.sumTokens({
+          owner: '0x4809010926aec940b550D34a46A52739f996D75D',
+          token: ADDRESSES.ethereum.USDC
+        });
       }
-    }
-})
+    };
+  }
+});
