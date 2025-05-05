13e311cac5400e1732625f96146a975b74ea65a5
EthDropVerse <ethdropverse@gmail.com>
2025-05-05 22:43:48 +0300

    Update index.js

--- projects/projects/ethereum/index.js
+++ projects/projects/ethereum/index.js
@@ -2,11 +2,11 @@ const { sumTokensExport } = require("../helper/unwrapLPs");
 
 module.exports = {
   ethereum: {
-    tvl: sumTokensExport({
-      owner: '0x885af16f25c3673a64e07e58c6c5536ea99c1a97', // Uniswap V3 pool address
+    pool2: sumTokensExport({
+      owner: '0x7541E21e2F98ef07A286eE52a133FbfF3C9F902d', // Uniswap V2 pool address
       tokens: [
         '0xC02aaa39b223FE8D0A0E5C4F27eAD9083C756Cc2', // WETH
-        '0x6507A08A3749A7dcf1a3d7b2B28E6a26E3E8d3b5', // Ethereum (ETH) custom token
+        '0x6507A08A3749A7dcf1a3d7b2B28e6a26E3E8d3b5', // Senin tokenin
       ],
     }),
   },
