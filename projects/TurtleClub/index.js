8c2e0846d6bbf20e460b97005d38ff2ab08cb4a6
Define101 <93603962+Define101@users.noreply.github.com>
2025-05-06 12:11:57 +0100

    add double count true turtle (#14545)

--- projects/TurtleClub/index.js
+++ projects/TurtleClub/index.js
@@ -5,6 +5,7 @@ const tokens = Object.values(tokensObj).map(chain => Object.values(chain)).flat(
 
 module.exports = {
     // All vaults currently ethereum
+    doublecounted: true,
     ethereum: {
         tvl: sumTokensExport({ owners, tokens, permitFailure: true, onlyWhitelisted: false }),
     },
