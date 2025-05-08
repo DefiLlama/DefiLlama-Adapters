0cc52a6aee9aa9e7587e19af365f93afc453db1e
Prasenjit Kaninde <31060173+prasenjit-26@users.noreply.github.com>
2025-05-08 18:30:14 +0530

    vault assets for tvl and removed currentfunds calls for bank contracts

--- projects/beracana/index.js
+++ projects/beracana/index.js
@@ -2,23 +2,19 @@ const { sumTokens2 } = require('../helper/unwrapLPs');
 const defaultAddresses = require('./berachain-contracts.json')
 
 async function getContracts() {
-    const contracts = await fetch('https://api.beracana.com/beracana/getContracts');
+    const contracts = await fetch('https://api.beracana.com/beracana/getContracts2');
     const resp = await contracts.json();
-    if (resp.success && resp.data && resp.data.banks && resp.data.vaults){
-      return { banks: resp.data.banks, vaults: resp.data.vaults }
+    if (resp.success && resp.data && resp.data.vaults){
+      return { vaults: resp.data.vaults }
     } else {
-      return { banks: defaultAddresses.banks, vaults: defaultAddresses.vaults }
+      return { vaults: defaultAddresses.vaults }
     }
 }
 async function getTvl(api) {
-    const { banks, vaults } = await getContracts();
-    const tokens = banks.map(i => i.token)
-    const bankAddresses = banks.map(i => i.bank)
-    const currentFunds = await api.multiCall({  abi: 'function currentFunds() view returns (uint256)', calls: bankAddresses })
+    const { vaults } = await getContracts();
     const vaultAddresses = vaults.map(i => i.vaultAddress)
     const vaultFunds = await api.multiCall({  abi: 'function totalFunds() view returns (uint256)', calls: vaultAddresses })
     const vaultTokens = vaults.map(i => i.priceToken)
-    api.add(tokens, currentFunds)
     api.add(vaultTokens, vaultFunds)
     return sumTokens2({ api, resolveLP: false})
 }
