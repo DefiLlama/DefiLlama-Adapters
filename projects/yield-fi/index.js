1d7b10131ced48f22f5b7e1e418fe5d9f2ceab03
neelra8n <neelratn.official@gmail.com>
2025-05-08 13:18:41 +0530

    subtracted lockbox tvl in eth chain

--- projects/yield-fi/index.js
+++ projects/yield-fi/index.js
@@ -1,3 +1,5 @@
+const { api2 } = require("@defillama/sdk");
+
 const config = {
     ethereum: "0x19Ebd191f7A24ECE672ba13A302212b5eF7F35cb", 
     optimism: '0x4772D2e014F9fC3a820C444e3313968e9a5C8121',
@@ -5,11 +7,27 @@ const config = {
     base: '0x4772D2e014F9fC3a820C444e3313968e9a5C8121',
 }
 
-Object.keys(config).forEach(chain => {
+const lockbox = "0x659b5bc7F2F888dB3D5901b78Cdb34DF270E2231";
+
+const l2Chains = Object.keys(config).filter(chain => chain !== 'ethereum')
+
+l2Chains.forEach(chain => {
     module.exports[chain] = {
         tvl: async (api) => {
             const supply = await api.call({ abi: 'erc20:totalSupply', target: config[chain] })
             api.add(config[chain], supply)
         }
     }
-})
+});
+
+module.exports['ethereum'] = {
+    tvl: async (api) => {
+        const lockboxSupply = await api2.erc20.balanceOf({
+            target: config['ethereum'],
+            owner: lockbox
+        })
+        const ethSupply = await api.call({ abi: 'erc20:totalSupply', target: config['ethereum'] })
+        const supply = (ethSupply - lockboxSupply.output);
+        api.add(config['ethereum'], supply);
+    }
+}
