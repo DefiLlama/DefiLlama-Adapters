0010056f60c7eee9a464ac52f36803e5872007d4
Huy Pham <p.q.huy97@gmail.com>
2025-05-08 22:42:45 +0700

    krystal evm tvl

--- projects/krystal/index.js
+++ projects/krystal/index.js
@@ -1,9 +1,11 @@
 const { tvl: solanaTvl } = require("./solana.js");
+const evmTvl = require("./evm.js");
 
 module.exports = {
   solana: {
     tvl: solanaTvl,
   },
+  ...evmTvl,
   isHeavyProtocol: true,
   methodology: "TVL: Sum of all positions' value in every vaults",
 };
