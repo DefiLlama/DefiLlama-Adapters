9a8c27e2efcb5303b17e92fb170e7e9a3c3e6315
ungaro <alp.guneysel@gmail.com>
2025-05-09 12:03:43 -0400

    add plume ustb

--- projects/superstate/index.js
+++ projects/superstate/index.js
@@ -1,14 +1,28 @@
 const USTB = "0x43415eb6ff9db7e26a15b704e7a3edce97d31c4e";
+const USTB_PLUME = "0xE4fA682f94610cCd170680cc3B045d77D9E528a8";
+
+async function tvl(api) {
+  const totalSupplies = await api.call({
+    target: USTB,
+    abi: "erc20:totalSupply",
+  });
+  api.add(USTB, totalSupplies);
+}
+
+async function tvl_plume(api) {
+  const totalSupplies = await api.call({
+    target: USTB_PLUME,
+    abi: "erc20:totalSupply",
+  });
+  api.add(USTB_PLUME, totalSupplies);
+}
 
 module.exports = {
   methodology: "TVL corresponds to the total amount of USTB & USCC minted onchain, does not include Superstate book-entry AUM",
   ethereum: {
-    tvl: async (api) => {
-      const totalSupplies = await api.multiCall({
-        calls: [USTB],
-        abi: "erc20:totalSupply",
-      });
-      api.addTokens([USTB], totalSupplies);
-    },
+    tvl: tvl
+  },
+  plume_mainnet: {
+    tvl: tvl_plume
   },
 };
