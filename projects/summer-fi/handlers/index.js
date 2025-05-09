ae0307523b019ccc19a6a92446ace4e9f29dba62
Jan Sikora <jan@oazoapps.com>
2025-05-09 15:54:32 +0200

    fix: remove protocols import from handlers/index.js

--- projects/summer-fi/handlers/index.js
+++ projects/summer-fi/handlers/index.js
@@ -1,13 +1,9 @@
 const { automationTvl } = require("./automation-v1");
 const { dpmPositions } = require("./dpm-positions");
 const { makerTvl } = require("./maker-vaults");
-const { aaveV3Tvl, ajnaTvl, morphoBlueTvl } = require("./protocols");
 
 module.exports = {
   automationTvl,
   dpmPositions,
   makerTvl,
-  aaveV3Tvl,
-  ajnaTvl,
-  morphoBlueTvl,
 };
