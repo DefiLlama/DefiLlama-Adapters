c56562061bc87ee2cb1daa99beb906f1587b2477
Lena <elena.mikhaylova0@gmail.com>
2025-05-12 13:09:47 -0400

    fix hallmarks array

--- projects/valence/index.js
+++ projects/valence/index.js
@@ -191,10 +191,10 @@ module.exports = {
     tvl: (api) => getValenceAccountTvl(api, "CosmosCosmwasm", "terra2"), 
   },
   hallmarks: [
-    [1717545600, "Nolus<>Neutron liquidity lending"]
-    [1718064000, "Stargaze<>Neutron liquidity lending"]
-    [1719360000, "Shade<>Neutron liquidity lending"]
-    [1723507200, "Mars<>Neutron liquidity lending"]
+    [1717545600, "Nolus<>Neutron liquidity lending"],
+    [1718064000, "Stargaze<>Neutron liquidity lending"],
+    [1719360000, "Shade<>Neutron liquidity lending"],
+    [1723507200, "Mars<>Neutron liquidity lending"],
     [1743379200, "Neutron dICS deployment"]
   ]
 };
