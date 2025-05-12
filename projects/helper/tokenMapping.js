0593d2d95dfc1f1e31edcf14d333097e4c3a3ecb
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-05-12 14:55:22 +0330

    Add Zanjir chain

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -62,6 +62,13 @@ const fixBalancesTokens = {
     '0x4200000000000000000000000000000000000006': { coingeckoId: 'superseed-bridged-weth-superseed', decimals: 18, },
     '0xc316c8252b5f2176d0135ebb0999e99296998f2e': { coingeckoId: 'superseed-bridged-usdc-superseed', decimals: 6, },
     '0x1217BfE6c773EEC6cc4A38b5Dc45B92292B6E189': { coingeckoId: 'openusdt', decimals: 6, },
+  },
+  zanjir: {
+    '0x9ac37093d6eF6cb5fC0944DCed3AA56eBCE050cb': { coingeckoId: 'tether', decimals: 18, },
+    '0x3B05FB2fA2AE1447f61A0456f102350626A69f0b': { coingeckoId: 'bitcoin', decimals: 18, },
+    '0x681E99A09Db2Be1da8dc54b1504eB81fD6F3724e': { coingeckoId: 'ethereum', decimals: 18, },
+    '0x36E6dc3CF44FDb8C62c5a11B457A28041f4C6eEF': { coingeckoId: 'solana', decimals: 18, },
+    '0xC7d2B19934594c43b6ec678507Df24D49e7e2F69': { coingeckoId: 'dogecoin', decimals: 18, },
   }
 }
 
