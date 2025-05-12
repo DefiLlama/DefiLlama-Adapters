a6e3499c34668dd8b15ecc15205f214cda81e5b6
Roy Mustang <FullmetalHodler@logarithm.fi>
2025-05-12 11:54:51 +0000

    update full code

--- projects/basisos/index.js
+++ projects/basisos/index.js
@@ -1,16 +1,21 @@
+const DATA_PROVIDER_ADDRESS = '0xDD5C8aB2E9F113b397ff2b8528C649bAEf24dF97'
+
 async function tvl(api) {
-    const tvl = await api.call({ target: '0xf44e393dd0796aBC4426B290C5464Cb8cb0cAaB1', abi: 'uint256:getTotalTvl'});
-    const asset = await api.call({ target: '0xf44e393dd0796aBC4426B290C5464Cb8cb0cAaB1', abi: 'address:asset'});
-    const assetDecimals = await api.call({ target: asset, abi: 'uint8:decimals'});
+  // Get all vaults addresses from the data provider contract
+  const vaults = await api.call({
+    target: DATA_PROVIDER_ADDRESS,
+    abi: 'address[]:getAllVaults',
+  })
+
+  // Calculate the TVL for each vault and sum them up
+  const tvl = await api.erc4626Sum({ calls: vaults, tokenAbi: 'address:asset', balanceAbi: 'uint256:totalAssets' })
+
+  return tvl
+}
+
   
-    return {
-      'usd-coin': tvl / 10 ** assetDecimals
-    }
-  }
-  
-    
-  module.exports = {
-    methodology: "TVL is calculated as the aggregated sum of total assets across all deployed vaults.",
-    timetravel: false,
-    arbitrum: { tvl },
-  };
+module.exports = {
+  methodology: "TVL is calculated as the aggregated sum of total assets across all deployed vaults.",
+  timetravel: false,
+  arbitrum: { tvl },
+};
