cae643f43aadaee91e06f252c11ce04738e63952
SeanTuring <sean.turing@exsat.org>
2025-05-13 18:15:45 +0800

    Rename the project ID from exsat-credit-staking to esbtc. (#14619)

--- projects/helper/bitcoin-book/index.js
+++ projects/helper/bitcoin-book/index.js
@@ -101,7 +101,7 @@ const prosper = require('./prosper.js')
 const hotcoin = require('./hotcoin.js')
 const orangex = require('./orangex.js')
 const exmo = require('./exmo.js')
-const ecbtc = require('./ecbtc.js')
+const esbtc = require('./esbtc.js')
 
 const p2pb2b = ['39BFtTzZjj6o2s7eewefFQxqM4617VmhEK']
 const teleswap = [
@@ -258,5 +258,5 @@ module.exports = {
   hotcoin,
   orangex,
   exmo,
-  ecbtc
+  esbtc
 }
