f77f1ced942e6cf42bee4d6ce48409b6f3932152
noah-yala <noah@yala.org>
2025-05-14 18:13:30 +0800

    update start date

--- projects/yala/index.js
+++ projects/yala/index.js
@@ -62,7 +62,7 @@ async function borrowedInSolana(api) {
 module.exports = {
   methodology:
     "The Yala Protocol allows users to lock Bitcoin as collateral to mint YU stablecoins. TVL is calculated by tracking the total supply of YBTC tokens (0x27A70B9F8073efE5A02998D5Cc64aCdc9e0Ba589), which represents Bitcoin locked in the protocol. The borrowed/stablecoin metric tracks the total supply of YU tokens (0xE868084cf08F3c3db11f4B73a95473762d9463f7), which represents the USD-pegged stablecoins minted against the Bitcoin collateral. Both token supplies are converted to their respective underlying asset values using CoinGecko price feeds.",
-  start: "2025-05-13",
+  start: "2025-05-16",
   bitcoin: {
     tvl: tvlInBitcoin,
   },
