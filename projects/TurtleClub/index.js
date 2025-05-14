c19be52f8db8a44e0ecad13c06dcb037d757f002
waynebruce0x <waynebruce0x@gmail.com>
2025-05-14 13:58:33 +0100

    turtleclub doublecount (#14639)

--- projects/TurtleClub/index.js
+++ projects/TurtleClub/index.js
@@ -5,6 +5,7 @@ const plainTokens = Object.values(tokens).map(chain => Object.values(chain)).fla
 
 module.exports = {
     // All vaults currently ethereum
+    doublecounted: true,
     ethereum: {
         tvl: sumTokensExport({
             owners: vaultContracts,
