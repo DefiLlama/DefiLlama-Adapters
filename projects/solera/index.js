a2848e246abb3186c6b78ac6a4b9c9e609d32947
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2025-05-15 18:21:25 -0400

    update solera AaveProtocolDataProvider address

--- projects/solera/index.js
+++ projects/solera/index.js
@@ -3,5 +3,5 @@ const methodologies = require("../helper/methodologies");
 
 module.exports = {
   methodology: methodologies.lendingMarket,
-  plume_mainnet: aaveExports("plume_mainnet", undefined, undefined, ['0x4A6609C56F69836E0Bb53A96494f45f89EafA811'], { v3: true }),
+  plume_mainnet: aaveExports("plume_mainnet", undefined, undefined, ['0xEE343bd811500ca27995Bc83D7ec2bacb63680d0'], { v3: true }),
 }
