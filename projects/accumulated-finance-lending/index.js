a5dd1456866f4c13c8734d91182856386474d91c
erozanov <evgeny.rozanov@gmail.com>
2025-05-16 14:02:56 +0400

    lending - zeta, vlx (new)

--- projects/accumulated-finance-lending/index.js
+++ projects/accumulated-finance-lending/index.js
@@ -9,8 +9,22 @@ const config = {
                 address: "0x570ae1d228d54c2a29c52603f962f45331d66680",
                 collateralToken: "0x8f0ecda9679ad16e30be3d83d183c482821f5325"
             }
+        },
+        {
+            "velas": {
+                address: "0x0acbfff708c9d30684f620b2be23930284af12c5",
+                collateralToken: "0x8f0ecda9679ad16e30be3d83d183c482821f5325"
+            }
         }
-    ]
+    ],
+    "zeta": [
+        {
+            "zeta": {
+                address: '0x9b694d0ed151374989a4ec71d8a14764ae47f89d',
+                collateralToken: '0x7ac168c81f4f3820fa3f22603ce5864d6ab3c547'
+            }
+        }
+    ],
 };
 
 function transformConfig(config) {
