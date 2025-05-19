63ce16c29e8558675ba52ddd381475c8180eb375
WardenJakx <114708157+WardenJakx@users.noreply.github.com>
2025-05-19 09:49:28 -0400

    remove more

--- projects/definitive/index.js
+++ projects/definitive/index.js
@@ -1,7 +1,3 @@
 module.exports = {
   methodology: "For Hyperstaking, an onchain method calculates collateral + balance - debt. For LP, we can just calculate the value of LP tokens held in the vault"
 }
-
-Object.keys(CommunityVaultsLP).forEach(chain => {
-  module.exports[chain] = { }
-})
