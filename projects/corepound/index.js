5cf63bbd09807a0c9e00dfb7f5bd9105f496c44e
corepound <team@corepound.xyz>
2025-05-19 09:50:44 +0800

    add solvBtc.m

--- projects/corepound/index.js
+++ projects/corepound/index.js
@@ -1,10 +1,18 @@
 const { sumTokens2 } = require("../helper/unwrapLPs")
 
 const aggregator = "0xF822226491a93046DA650ecb4049E43386497C7D";
+const solvBtcMFarmAddress = "0x915Be1EC1153F3Eaef16629fE7fb532b777159AC"
 const projectToken = "0x56663F56333717A32Cd91ec41182d6d76D98864e";
 
 async function tvl(api) {
     let pools = await api.call({ abi: abiInfo.pools, target: aggregator });
+  
+    pools
+      .forEach((i) => {
+        api.add(i.assets, i.tvl);
+      });
+
+    pools = await api.call({ abi: abiInfo.pools, target: solvBtcMFarmAddress });
   
     pools
       .forEach((i) => {
