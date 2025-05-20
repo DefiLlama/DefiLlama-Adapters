da849771f5be35bbce65c44120ed3fa584808162
Eden <noat.eth@gmail.com>
2025-05-20 14:32:30 +0700

    clean codes, use simple api

--- projects/helper/curators.js
+++ projects/helper/curators.js
@@ -1,6 +1,3 @@
-const { normalizeAddress } = require("./tokenMapping")
-const { sumTokens2 } = require("./unwrapLPs")
-
 const ABI = {
   ERC4626: {
     asset: 'address:asset',
@@ -16,52 +13,26 @@ const ABI = {
 async function getCuratorTvlErc4626(api, vaults) {
   const assets =  await api.multiCall({ abi: ABI.ERC4626.asset, calls: vaults })
   const totalAssets = await api.multiCall({ abi: ABI.ERC4626.totalAssets, calls: vaults })
-
-  return vaults.map((vault, index) => {
-    return {
-      vault: vault,
-      token: assets[index],
-      balance: totalAssets[index],
-    }
-  })
+  return api.add(assets, totalAssets)
 }
 
 async function getCuratorTvlAeraVault(api, vaults) {
   const assetRegistries =  await api.multiCall({ abi: ABI.aera.assetRegistry, calls: vaults })
   const assets =  await api.multiCall({ abi: ABI.aera.numeraireToken, calls: assetRegistries })
   const values = await api.multiCall({ abi: ABI.aera.value, calls: vaults })
-
-  return vaults.map((vault, index) => {
-    return {
-      vault: vault,
-      token: assets[index],
-      balance: values[index],
-    }
-  })
+  return api.add(assets, values)
 }
 
 async function getCuratorTvl(api, vaults) {
-  let allVaults = []
   if (vaults.morpho) {
-    allVaults = allVaults.concat((await getCuratorTvlErc4626(api, vaults.morpho)))
+    await getCuratorTvlErc4626(api, vaults.morpho) 
   }
   if (vaults.euler) {
-    allVaults = allVaults.concat((await getCuratorTvlErc4626(api, vaults.euler)))
+    await getCuratorTvlErc4626(api, vaults.euler)
   }
   if (vaults.aera) {
-    allVaults = allVaults.concat((await getCuratorTvlAeraVault(api, vaults.aera)))
+    await getCuratorTvlAeraVault(api, vaults.aera)
   }
-
-  const balances = {}
-  for (const vaultBalance of allVaults) {
-    const token = `${api.chain}:${normalizeAddress(vaultBalance.token)}`
-    if (!balances[token]) {
-      balances[token] = 0
-    }
-    balances[token] += Number(vaultBalance.balance)
-  }
-
-  return balances
 }
 
 function getCuratorExport(configs) {
