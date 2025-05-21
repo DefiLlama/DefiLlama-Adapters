506f259990be309ee0f8e5254526690164a9377b
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2025-05-21 14:22:46 +0400

    Update index.js

--- projects/bbbpump/index.js
+++ projects/bbbpump/index.js
@@ -9,15 +9,12 @@ const BBBPump = "0x2E24BFdE1EEDa0F1EA3E57Ba7Ff10ac6516ab5Ec";      // BBBPump
 const Stake="0x2B3Bb9b3265Fcee484e857506fDCf2C0776E9c43";
 const psXDC="0x9B8e12b0BAC165B86967E771d98B520Ec3F665A6";
 const bpsXDC="0x24be372f0915b8BAf17AfA150210FFcB79C88845";
-
+const xdcStake="0x5af754f822CEd42deC729c1F1B3EDb9f13485ba7";
 
 async function bbbPumpTvl(api) {
     const psXDCBalance = await api.call({ abi: 'erc20:balanceOf', target: psXDC,params: [Stake] })
     api.addGasToken(psXDCBalance)
-
-    const bpsXDCSupply = await api.call({ abi: 'erc20:totalSupply', target: bpsXDC })
-    api.addGasToken(bpsXDCSupply)
-    const toa=[[nullAddress,BBBPump]]
+    const toa=[[nullAddress,BBBPump],[nullAddress,xdcStake]]
     return sumTokens2({ tokensAndOwners: toa, api })
  
  
