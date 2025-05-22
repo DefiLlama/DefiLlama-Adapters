5bc0a72d1e140447412f8820255b96130d9e2ee8
Unique Divine <realuniquedivine@gmail.com>
2025-05-22 13:39:59 -0500

    fix(omniswap): reference core assets

--- projects/omniswap/index.js
+++ projects/omniswap/index.js
@@ -1,5 +1,7 @@
 const { getUniTVL } = require('../helper/unknownTokens');
 
+const { nibiru: nibiruCoreAssets } = require('../helper/coreAssets.json')
+
 module.exports = {
   methodology: "TVL consists of liquidity pools created through the factory contract",
   misrepresentedTokens: true,
@@ -7,6 +9,8 @@ module.exports = {
     tvl: getUniTVL({
       factory: "0x2043d6f72CcD82c4Eae36fF331ADAE8C77bA5897",
       chain: "nibiru",
+      useDefaultCoreAssets: true,
+      coreAssets: nibiruCoreAssets,
     }),
   },
 };
