2f4f162b890ab9a6d764b0c94b92e8700e596f36
Eden <noat.eth@gmail.com>
2025-05-22 07:18:37 +0700

    minor fix

--- projects/helper/curators.js
+++ projects/helper/curators.js
@@ -11,8 +11,11 @@ const ABI = {
 }
 
 async function getCuratorTvlErc4626(api, vaults) {
-  const assets =  await api.multiCall({ abi: ABI.ERC4626.asset, calls: vaults, excludeFailed: true, })
-  const totalAssets = await api.multiCall({ abi: ABI.ERC4626.totalAssets, calls: vaults, excludeFailed: true, })
+  const assets =  await api.multiCall({ abi: ABI.ERC4626.asset, calls: vaults, permitFailure: true, excludeFailed: true, })
+  const totalAssets = await api.multiCall({ abi: ABI.ERC4626.totalAssets, calls: vaults, permitFailure: true, excludeFailed: true, })
+
+  console.log({assets, totalAssets})
+
   return api.add(assets, totalAssets)
 }
 
