e1cb6e06542ee588c5344f66d060ec90055d9f47
Luka Levac <luka.levac@gmail.com>
2025-05-25 19:24:09 +0200

    Unihedge new contract

--- projects/unihedge/index.js
+++ projects/unihedge/index.js
@@ -1,8 +1,8 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokens2 } = require('../helper/unwrapLPs');
 
-const contractAddress = '0x28824b535d1F4edaf89a36B558811CB1c0b9Aa47'; // UniHedge contract
-const accountingToken = ADDRESSES.polygon.DAI; // DAI token on Polygon
+const contractAddress = '0xdD90A0504aA3215Dd0E7fb45471A0B133CC3f567'; // UniHedge contract
+const accountingToken = ADDRESSES.polygon.USDC_CIRCLE; // USDC token on Polygon
 const chain = 'polygon';
 
 async function tvl(_, _1, _2, { api }) {
@@ -10,7 +10,7 @@ async function tvl(_, _1, _2, { api }) {
 }
 
 module.exports = {
-  methodology: "TVL is calculated by summing the DAI (accounting token) locked in the UniHedge contract.",
+  methodology: "TVL is calculated by summing the USDC (accounting token) locked in the UniHedge contract.",
   polygon: {
     tvl,
   },
