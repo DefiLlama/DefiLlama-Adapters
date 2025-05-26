9d83fe9d708ad432d1a3f3c15e9938c5675f0fde
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2025-05-26 11:53:15 +0200

    small refacto

--- projects/unihedge/index.js
+++ projects/unihedge/index.js
@@ -1,17 +1,16 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokens2 } = require('../helper/unwrapLPs');
 
-const contractAddress = '0xdD90A0504aA3215Dd0E7fb45471A0B133CC3f567'; // UniHedge contract
-const accountingToken = ADDRESSES.polygon.USDC_CIRCLE; // USDC token on Polygon
-const chain = 'polygon';
+const config = {
+  owner: '0xdD90A0504aA3215Dd0E7fb45471A0B133CC3f567', // UniHedge contract
+  token: ADDRESSES.polygon.USDC_CIRCLE // USDC
+}
 
-async function tvl(_, _1, _2, { api }) {
-  return sumTokens2({ api, tokens: [accountingToken], owners: [contractAddress] });
+const tvl = async (api) => {
+  const { owner, token } = config
+  return api.sumTokens({ token, owner })
 }
 
 module.exports = {
-  methodology: "TVL is calculated by summing the USDC (accounting token) locked in the UniHedge contract.",
-  polygon: {
-    tvl,
-  },
-};
+  methodology: "TVL is calculated by summing the DAI (accounting token) locked in the UniHedge contract.",
+  polygon: { tvl }
+}
