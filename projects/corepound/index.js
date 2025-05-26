1db0c41e6afc41092cdae26ee2b57643f7611a58
Corepound <team@corepound.xyz>
2025-05-26 21:17:39 +0800

    add new farm (#14787)
    Co-authored-by: 0xpeluche <110820448+0xpeluche@users.noreply.github.com>

--- projects/corepound/index.js
+++ projects/corepound/index.js
@@ -4,40 +4,24 @@ const aggregator = "0xF822226491a93046DA650ecb4049E43386497C7D";
 const solvBtcMFarmAddress = "0x915Be1EC1153F3Eaef16629fE7fb532b777159AC"
 const projectToken = "0x56663F56333717A32Cd91ec41182d6d76D98864e";
 
-async function tvl(api) {
-    let pools = await api.call({ abi: abiInfo.pools, target: aggregator });
-  
-    pools
-      .forEach((i) => {
-        api.add(i.assets, i.tvl);
-      });
+const abis = {
+  getTotalTvl: "function getTotalTvl() view returns (tuple(uint256 pid, address assets, uint256 tvl)[])"
+}
 
-    pools = await api.call({ abi: abiInfo.pools, target: solvBtcMFarmAddress });
-  
-    pools
-      .forEach((i) => {
-        api.add(i.assets, i.tvl);
-      });
-  
-    return await sumTokens2({ api, resolveLP: true });
+async function tvl(api) {
+  const pools = (await api.multiCall({ calls: [aggregator, solvBtcMFarmAddress], abi: abis.getTotalTvl })).flat()
+  pools.forEach(({ assets, tvl }) => { api.add(assets, tvl) })
+  await sumTokens2({ api, resolveLP: true })
+  api.removeTokenBalance(projectToken)
   }
   
   async function staking(api) {  
-    let pools = await api.call({ abi: abiInfo.pools, target: aggregator });
-    let target = pools.find((i) => i.assets === projectToken);
+    const pools = await api.call({ abi: abis.getTotalTvl, target: aggregator });
+    const target = pools.find((i) => i.assets === projectToken);
     api.add(projectToken, target.tvl);
     return api.getBalances();
   }
   
   module.exports = {
-    core:{
-      tvl,
-      staking
-    }
+    core: { tvl, staking }
   };
-  
-  const abiInfo = {
-    pools:
-      "function getTotalTvl() view returns (tuple(uint256 pid, address assets, uint256 tvl)[])",
-  };
-  
