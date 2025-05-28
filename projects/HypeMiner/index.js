3b0d11c8c07f372723b98ee3421dde053d6517af
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-05-28 23:15:43 +0200

    minor fix

--- projects/HypeMiner/index.js
+++ projects/HypeMiner/index.js
@@ -1,14 +1,10 @@
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
+const { nullAddress, sumTokensExport } = require('../helper/unwrapLPs')
 
 const contract = "0x2Ac995E7945472da0C1077a0A00E0CF914baE0cC"
-
-async function tvl(time, ethBlock, { sonic: block }) {
-  return sumTokens2({ tokens: [nullAddress], owner: contract, block, chain: 'hyperliquid', })
-}
 
 module.exports = {
   methodology: `We count the HYPE on ${contract}`,
   hyperliquid: {
-    tvl
+    tvl: sumTokensExport({ owner: contract, tokens: [nullAddress], }),
   }
 }
