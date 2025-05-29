029fbc38e6be06948fc0144289045eb8e83515e7
AhrenFullStop <aj_posthumus@yahoo.com>
2025-05-29 12:31:56 +0200

    Updated Listings as per comments https://github.com/DefiLlama/DefiLlama-Adapters/pull/14702#issuecomment-2897292943

--- projects/momint/index.js
+++ projects/momint/index.js
@@ -11,8 +11,7 @@
 // 
 //     Adapter for the Momint marketplace on Lisk/OP stack
 //     This adapter calculates TVL for the Momint marketplace
-//     TVL is calculated as the sum of all listings with valid, 
-//     verifyable on chain sale data
+//     TVL is calculated as the sum of all assets listed in the marketplace (TVL).
 
 // required constants
 const { ethers } = require("ethers");
@@ -33,26 +32,25 @@ async function tvl(timestamp) {
   const marketplace = new ethers.Contract(MARKETPLACE_ADDRESS, MARKETPLACE_ABI, provider);
   const blockNumber = await provider.getBlockNumber(); // Get latest block number
 
-  const marketplaceFilter = marketplace.filters.ListingSold();// filter transactions with valid payments
+  const marketplaceFilter = marketplace.filters.ListingCreated();
   const events = await marketplace.queryFilter(marketplaceFilter, START_BLOCK_LISK, blockNumber).catch(e => {
-    console.error(`Error querying ListingSold events:`, e.message);
+    console.error(`Error querying ListingCreated events:`, e.message);
     return [];
   });
 
   // setup tally variables
-  let totalVolumeInLSK = 0;
-  let totalValidSaleEvents = 0;
+  let totalListingValueInLSK = 0;
+  let totalValidListingEvents = 0;
 
   // loop through the events
   for (const event of events) {
-    console.log(`Processing event: #${totalValidSaleEvents}/${events.length}`);
-    totalValidSaleEvents++; // Tally up the number of valid sales with price data
-    const { listingId, totalPrice } = event.args; // Get metadata from the contract event
-    let listing = await marketplace.listings(listingId); // Get the listing data from the contract
-    const priceInTokens = Number(totalPrice) / Number(BigInt(10) ** BigInt(18)); // LSK has 18 decimals
-    if(listing.paymentToken.toLowerCase() === LISK_TOKEN.toLowerCase()) totalVolumeInLSK += priceInTokens; //ignore other currencies for now
+    console.log(`Processing event: #${totalValidListingEvents}/${events.length}`);
+    totalValidListingEvents++; // Tally up the number of valid listing events
+    const { amount, pricePerToken, paymentToken } = event.args; // Get metadata from the contract event
+    const listingValue = Number(amount) * Number(pricePerToken) / Number(BigInt(10) ** BigInt(18)); // LSK has 18 decimals
+    if(paymentToken.toLowerCase() === LISK_TOKEN.toLowerCase()) totalListingValueInLSK += listingValue; //ignore other currencies for now
   }
-  return { [CHAIN]: totalVolumeInLSK }; //return total LSK volume
+  return { [CHAIN]: totalListingValueInLSK }; //return total LSK listing value
 }
 
 module.exports = {
