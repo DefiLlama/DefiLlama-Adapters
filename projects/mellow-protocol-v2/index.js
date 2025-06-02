16dbf0c97418888cc79d5658c985296e9233b703
Eden <noat.eth@gmail.com>
2025-06-02 21:47:20 +0700

    remove dup vaults (#14888)

--- projects/mellow-protocol-v2/index.js
+++ projects/mellow-protocol-v2/index.js
@@ -9,14 +9,6 @@ const config = {
   },
   lisk: {
     chainId: 1135,
-
-    // assets locked in OFT tokens, bridged to ethereum via layerzero
-    // and do staking on ethereum, we count source assets from lisk, don't count on ethereum staking vaults
-    interopOFTTokens: [
-      '0x1ddBeBd9aaBe4B9660d9Bba5de2949DA1Ae4D229',
-      '0x34B22c672b3dA8396f4A66324703590a945129De',
-      '0xcDf0b12Ef7716f3848F98D77dD842bFBDCF6b857',
-    ],
   }
 }
 
@@ -27,24 +19,9 @@ module.exports = {
 };
 
 Object.keys(config).forEach(chain => {
-  const { mellowLrtVaults, interopOFTTokens } = config[chain]
+  const { mellowLrtVaults } = config[chain]
   module.exports[chain] = {
     tvl: async (api) => {
-      if (interopOFTTokens && interopOFTTokens.length > 0) {
-        const interopTokens = await api.multiCall({ abi: 'function token() public view returns (address)', calls: interopOFTTokens, permitFailure: false })
-        const balances = await api.multiCall({
-          abi: 'function balanceOf(address owner) public view returns (uint256)',
-          calls: interopTokens.map((token, index) => {
-            return {
-              target: token,
-              params: [interopOFTTokens[index]],
-            }
-          }),
-          permitFailure: false,
-        })
-        api.add(interopTokens, balances)
-      }
-
       if (mellowLrtVaults != null && mellowLrtVaults.length > 0) {
         const mellowLrtTvl = await api.multiCall({ abi: 'function underlyingTvl() public view returns (address[] tokens, uint256[] values)', calls: mellowLrtVaults, permitFailure: true })
         mellowLrtTvl.forEach((i) => {
