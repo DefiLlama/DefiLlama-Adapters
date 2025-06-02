4b18ceef3c0d61556cc4a28fcb32049af3ae9819
Alok <alok.orange.25@gmail.com>
2025-06-02 12:47:44 +0800

    Add new berancia vaults

--- projects/berancia/index.js
+++ projects/berancia/index.js
@@ -28,6 +28,28 @@ const BERA_KODIAK_VAULTS = [
     kodiakIsland: "0x5347e5133b22A680Ee94b7e62803E848F8d8C92e",
     token0: "0x7e768f47dfDD5DAe874Aac233f1Bc5817137E453",
     token1: "0x982940eBfC5caa2F5b5a82AAc2Dfa99F18BB7dA4"
+  },
+  {
+    address: "0x585ad5BcD7cC28dAdb3BDd79782D14513171f760",
+    kodiakIsland: "0x9f6cf7aCb2F16f7d906EeeCB0a6020a5Cf91A41d",
+    token0: "0x6969696969696969696969696969696969696969",
+    token1: "0xf3530788DEB3d21E8fA2c3CBBF93317FB38a0D3C"
+  },
+]
+
+const BULLA_VAULTS = [
+  {
+    address: "0xE5A5Cbc42aE849a3780Ec847184Fd9a7ae3F56A5",
+    asset: "0xcfFBFD665BEdB19B47837461A5aBf4388C560D35",
+    token0: "0x6969696969696969696969696969696969696969",
+    token1: "0xac03CABA51e17c86c921E1f6CBFBdC91F8BB2E6b"
+  },
+  // AquaBera vault uses same TVL calculation as bulla vaults
+  {
+    address: "0x978A2FfFB70F40Cf9d64755c80a49f177Ff95e18",
+    asset: "0x04fD6a7B02E2e48caedaD7135420604de5f834f8",
+    token0: "0x6969696969696969696969696969696969696969",
+    token1: "0xb2F776e9c1C926C4b2e54182Fac058dA9Af0B6A5"
   }
 ]
 
@@ -44,6 +66,18 @@ const BURR_BEAR_VAULTS = [
     asset: "0xD10E65A5F8cA6f835F2B1832e37cF150fb955f23",
     poolId: "0xd10e65a5f8ca6f835f2b1832e37cf150fb955f23000000000000000000000004",
     vault: "0xBE09E71BDc7b8a50A05F7291920590505e3C7744"
+  },
+  {
+    address: "0x42dc43875401b2BA1db58be240A06E2e834BA07D",
+    asset: "0xCbeF1B65399065c2DE2C495971e90466ff38f2d0",
+    poolId: "0xcbef1b65399065c2de2c495971e90466ff38f2d000000000000000000000001e",
+    vault: "0xBE09E71BDc7b8a50A05F7291920590505e3C7744"
+  },
+  {
+    address: "0x3960DFdbDb82b67D31CD490aA4d0D7B3DF2087Ea",
+    asset: "0x2A9EA10b60Ab02e9B8960a08b4996e6Bf5eA5EAb",
+    poolId: "0x2a9ea10b60ab02e9b8960a08b4996e6bf5ea5eab000000000000000000000162",
+    vault: "0x4Be03f781C497A489E3cB0287833452cA9B9E80B"
   }
 ]
 
@@ -66,6 +100,41 @@ async function beraTvl(api) {
     // Calculate vault assets based on island assets and vault balance
     const token0Balance = amount0Current * vaultBalance / totalSupply
     const token1Balance = amount1Current * vaultBalance / totalSupply
+
+    api.add(vault.token0, token0Balance)
+    api.add(vault.token1, token1Balance)
+  }
+
+  for (const vault of BULLA_VAULTS) {
+    const vaultBalance = await api.call({
+      abi: "function totalAssets() view returns (uint256 totalAssets)",
+      target: vault.address,
+    })
+    const totalSupply = await api.call({
+      abi: "erc20:totalSupply",
+      target: vault.asset
+    })
+    const {total0, total1} = await api.call({
+      abi: "function getTotalAmounts() view returns (uint256 total0, uint256 total1)",
+      target: vault.asset
+    })
+    const token0AmountRaw = await api.call({
+      abi: "erc20:balanceOf",
+      target: vault.token0,
+      params: [vault.asset]
+    })
+    const token1AmountRaw = await api.call({
+      abi: "erc20:balanceOf",
+      target: vault.token1,
+      params: [vault.asset]
+    })
+    
+    // Calculate vault assets based on island assets and vault balance
+    const poolToken0Total = +total0+(+token0AmountRaw)
+    const poolToken1Total = +total1+(+token1AmountRaw)
+    const beranciaVaultShares = ((+vaultBalance) / (+totalSupply))
+    const token0Balance = poolToken0Total * beranciaVaultShares
+    const token1Balance = poolToken1Total * beranciaVaultShares
 
     api.add(vault.token0, token0Balance)
     api.add(vault.token1, token1Balance)
