c5fe349fe1456295d6b55b10ad7adcbb0f86c7a1
Eden <noat.eth@gmail.com>
2025-06-03 13:30:25 +0700

    remove unused governor address

--- projects/extra-vaults/index.js
+++ projects/extra-vaults/index.js
@@ -2,8 +2,10 @@ const { getMorphoVaultTvl } = require("../helper/morpoho")
 
 const config = {
   base: {
-    governor: "0x8b621804a7637b781e2BbD58e256a591F2dF7d51",
-    morphoVaults: ['0x5A32099837D89E3a794a44fb131CBbAD41f87a8C', '0x23479229e52Ab6aaD312D0B03DF9F33B46753B5e']
+    morphoVaults: [
+      '0x5A32099837D89E3a794a44fb131CBbAD41f87a8C',
+      '0x23479229e52Ab6aaD312D0B03DF9F33B46753B5e',
+    ],
   }
 }
 
