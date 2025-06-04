ef1586eae22fe58f477b24451fb90f1e1c6ad3af
sunnyrk <radadiyasunny970@gmail.com>
2025-06-04 16:53:18 +0530

    Remove commented-out wallet addresses from zyfai index.js

--- projects/zyfai/index.js
+++ projects/zyfai/index.js
@@ -3,11 +3,6 @@ const { siloTvl, aaveTvl, eulerTvl, pendleTvl, beetsTvl, penpieTvl } = require('
 async function tvl(api) {
     const { get } = require('../helper/http');
     const owners = await get('https://api.zyf.ai/api/v1/data/active-wallets?chainId=146');
-    // const owners = [
-    //     '0x952835d17AC55825F198a68DAb2823cD60C8e6bd',
-    //     '0xb50685c25485CA8C520F5286Bbbf1d3F216D6989',
-    //     '0xd61C43c089852e0AB68B967dD1eDe03a18e52223'
-    // ]
     await Promise.all([
         siloTvl(api, owners),
         aaveTvl(api, owners),
