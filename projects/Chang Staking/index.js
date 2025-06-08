2c1af60bb94ffe13296267dc6566d0989b916e25
Chang Coin Foundation <changcoinfoundation@gmail.com>
2025-06-08 18:30:03 +0700

    Create index.js

---
+++ projects/Chang Staking/index.js
@@ -0,0 +1,7 @@
+const CTHStakingProgram = "0x0e6cF7C13aB088fBb216Db1DD768E622f838a382"
+
+module.exports = {
+  methodology: 'counts the number of CTH token in the CTHStakingProgram contract.',
+  ChangChain: {
+       tvl
+  },
