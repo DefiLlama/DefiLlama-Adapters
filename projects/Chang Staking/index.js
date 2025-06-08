25456fa4d1abdac19afb75244770685237fb6b77
Chang Coin Foundation <changcoinfoundation@gmail.com>
2025-06-08 19:43:01 +0700

    Add Chang Staking - Update index.js

--- projects/Chang Staking/index.js
+++ projects/Chang Staking/index.js
@@ -1,4 +1,4 @@
-const CTHStakingProgram = "0x0e6cF7C13aB088fBb216Db1DD768E622f838a382"
+const STAKING_CONTRACT = "0x0e6cF7C13aB088fBb216Db1DD768E622f838a382"
 
 module.exports = {
   methodology: 'counts the number of CTH token in the CTHStakingProgram contract.',
