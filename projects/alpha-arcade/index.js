6c357fd506ed704184e380edaf9865341df7a66d
Jake Loranger <jake.loranger@rockstepsolutions.com>
2025-06-09 15:24:15 -0400

    AlphaArcade TVL

--- projects/alpha-arcade/index.js
+++ projects/alpha-arcade/index.js
@@ -1,13 +1,10 @@
-const { lookupApplications } = require("../helper/chain/algorand");
+const { getApplicationAddress } = require("../helper/chain/algorandUtils/address");
+const { lookupAccountByID } = require("../helper/chain/algorand");
 
-async function getAppState(appId) {
-  const res = await lookupApplications(appId);
-  return res.application.params["global-state"];
-}
+const USDC_ASSET_ID = 31566704; // USDC asset ID on Algorand
 
 async function getAlphaArcadeTvl() {
     // Get all markets
-    // https://g08245wvl7.execute-api.us-east-1.amazonaws.com/api/get-markets?activeOnly=true
     // Loop through each market and add it's marketAppId to an array
     let markets = [];
     let tvl = 0;
@@ -21,15 +18,27 @@ async function getAlphaArcadeTvl() {
             markets.push(market.marketAppId);
         }
     }
-    
+
     for (const marketAppId of markets) {
         // Get application escrow account
         // Get amount of USDC in escrow account
-        // Add amount to total
+        // Add amount to total tvl
+        getApplicationAddress(marketAppId).then((account) => {
+                lookupAccountByID(account).then((accountData) => {
+                    const assets = accountData.account.assets;
+                    if (assets) {
+                        for (const asset of assets) {
+                            if (asset['asset-id'] === USDC_ASSET_ID) {
+                                tvl += asset.amount;
+                            }
+                        }
+                    }
+                });
+            })
+        
     }
 
-    // Return the total as the TVL
-    return tvl;
+    return tvl; // Currently in micro USDC
 }
 
 module.exports = {
