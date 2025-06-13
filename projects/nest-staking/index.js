49dd7a94b2610fd8b1fc9da48809e697b3ba301c
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2025-06-13 18:04:52 -0500

    Add pre-deposit funds back to TVL for Nest Staking

--- projects/nest-staking/index.js
+++ projects/nest-staking/index.js
@@ -1,6 +1,16 @@
 const {getConfig} = require("../helper/cache");
 
+async function tvl_ethereum_predeposit(api) {
+  const RESERVE_STAKING = "0xBa0Ae7069f94643853Fce3B8Af7f55AcBC11e397";
+  const SBTC = "0x094c0e36210634c3CfA25DC11B96b562E0b07624";
+  const STONE = "0x7122985656e38BDC0302Db86685bb972b145bD3C";
+
+  await api.sumTokens({ owner: RESERVE_STAKING, tokens: [SBTC, STONE] });
+}
+
 async function tvl_ethereum(api) {
+  await tvl_ethereum_predeposit(api);
+
   const vaults = await getConfig('nest-vaults', "https://app.nest.credit/api/vaults?includeHidden=true");
   const ethereumVaults = vaults?.map(vault => vault.plume?.contractAddress) ?? [];
   const details = await api.multiCall({ abi: 'erc20:totalSupply', calls: ethereumVaults })
