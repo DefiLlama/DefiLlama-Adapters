1b829633fb9eacbc4cdf2c95ad9fd80655c14593
dtmkeng <phumaisut@gmail.com>
2025-06-15 05:26:39 +0000

    add treasury realms

---
+++ projects/treasury/realms.js
@@ -0,0 +1,17 @@
+const ADDRESSES = require('../helper/coreAssets.json');
+const { treasuryExports } = require("../helper/treasury");
+
+const treasury = "0xa8e6efaf015d424c626cf3c23546fcb3bd2c9f1a";
+
+module.exports = treasuryExports({
+  ethereum: {
+    tokens: [
+        ADDRESSES.null,
+        ADDRESSES.ethereum.WETH
+     ],
+    blacklistedTokens: [
+        '0x686f2404e77ab0d9070a46cdfb0b7fecdd2318b0' // LORDS it is governance token
+    ],
+    owners: [treasury]
+  },
+})
