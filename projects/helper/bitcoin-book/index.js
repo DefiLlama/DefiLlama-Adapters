2f8fd338ce2eee8e0df918ade65980b4ac95b6d7
Define101 <93603962+Define101@users.noreply.github.com>
2025-06-16 09:57:04 +0100

    tezos btc bridge (#15089)

--- projects/helper/bitcoin-book/index.js
+++ projects/helper/bitcoin-book/index.js
@@ -103,6 +103,7 @@ const orangex = require('./orangex.js')
 const exmo = require('./exmo.js')
 const esbtc = require('./esbtc.js')
 const bimaCdp = require('./bima-cdp.js')
+const tzbtc = require('./tezos-btc.js')
 
 const p2pb2b = ['39BFtTzZjj6o2s7eewefFQxqM4617VmhEK']
 const teleswap = [
@@ -260,5 +261,6 @@ module.exports = {
   hotcoin,
   orangex,
   exmo,
-  esbtc
+  esbtc,
+  tzbtc
 }
