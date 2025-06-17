53b98c04a8e384a3c93695dfe186453a8ef46f3d
sunnyrk <radadiyasunny970@gmail.com>
2025-06-17 15:34:44 +0530

    base protocol integration for zyfai

--- projects/zyfai/index.js
+++ projects/zyfai/index.js
@@ -1,7 +1,10 @@
 const { siloTvl, aaveTvl, eulerTvl, pendleTvl, beetsTvl, penpieTvl } = require('./helpers');
+const { aaveTvl: aaveTvlBase, fluidTvl, morphoTvl, sparkTvl } = require('./base');
 
 async function tvl(api) {
     const { get } = require('../helper/http');
+
+    // For Sonic chain
     const owners = await get('https://api.zyf.ai/api/v1/data/active-wallets?chainId=146');
     await Promise.all([
         siloTvl(api, owners),
@@ -11,6 +14,19 @@ async function tvl(api) {
         beetsTvl(api, owners),
         penpieTvl(api, owners)
     ]);
+
+    // For Base chain
+    const baseOwners = [
+        '0xb50685c25485CA8C520F5286Bbbf1d3F216D6989', 
+        '0x952835d17AC55825F198a68DAb2823cD60C8e6bd'
+    ]; // we will change later dynamic owners, i.e. owners api
+    await Promise.all([ 
+        aaveTvlBase(api, baseOwners),
+        fluidTvl(api, baseOwners),
+        morphoTvl(api, baseOwners),
+        sparkTvl(api, baseOwners)
+    ]);
+
 }
 
 module.exports = {
