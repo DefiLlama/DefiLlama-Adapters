f6af90521658b2b3f6a738dca29ea8e5c739934f
Marian-Daniel Rolník <marian-danielrolnik@Marian-Daniels-MacBook-Pro.local>
2025-06-19 15:56:51 +0200

    Add right backend API

--- projects/OnRe/index.js
+++ projects/OnRe/index.js
@@ -12,12 +12,13 @@ const { getLowestOfferPrice, getOfferTokenAccounts } = require('./offerUtils');
 const MINT = '5Y8NV33Vv7WbnLfq3zBcKSdYPrk7g2KoiQoe7M2tcxp5';
 const BOSS = '45YnzauhsBM8CpUz96Djf8UG5vqq2Dua62wuW9H3jaJ5';
 const programId = new PublicKey('onreuGhHHgVzMWSkj2oQDLDtvvGvoepBPkqyaubFcwe');
-const onreOffchain = 'https://onre-api-dev.ew.r.appspot.com/offers';
+const onreOffchain = 'https://onre-api-prod.ew.r.appspot.com/offers';
 
 
 async function tvl(api) {
     // --- Dynamic offers implementation ---
     const offers = (await axios.get(onreOffchain)).data;
+    console.log(offers);
     const lowestPrice = getLowestOfferPrice(offers, ADDRESSES.solana.USDC);
     const allTokenAccounts = getOfferTokenAccounts(offers, MINT, programId);
     const bossTokenAccount = getAssociatedTokenAddress(MINT, BOSS);
@@ -29,34 +30,6 @@ async function tvl(api) {
     const tokenSupply = await getTokenSupplies([MINT]);
     const circulatingSupply = +tokenSupply[MINT] - (+bossAndOffers[`solana:${MINT}`]);
     api.add(ADDRESSES.solana.USDC, BigInt(circulatingSupply) * BigInt(lowestPrice) / BigInt(1e9));
-
-    // --- Original static offer implementation (kept for reference) ---
-    // const offerId = new BN(1);
-    // const [offerAuthority] = PublicKey.findProgramAddressSync([
-    //     Buffer.from('offer_authority'), offerId.toArrayLike(Buffer, 'le', 8)
-    // ], programId);
-    //
-    // const offer = {
-    //     sellTokenStartAmount: BigInt('201525740000'),
-    //     sellTokenEndAmount: BigInt('201525740000'),
-    //     buyToken1Amount: BigInt('200000000000000'),
-    //     startDate: new Date('2025-05-10T00:00:00Z'),
-    //     endDate: new Date('2025-05-25T00:00:00Z'),
-    //     sellToken: ADDRESSES.solana.USDC,
-    //     buyToken1: MINT
-    // }
-    // const price = offer.sellToken === ADDRESSES.solana.USDC ? calculateSellTokenPerBuyTokenNow(offer) : calculateBuyTokenPerSellTokenNow(offer);
-    //
-    // const offerTokenAccount = getAssociatedTokenAddress(MINT, offerAuthority.toBase58())
-    // const bossTokenAccount = getAssociatedTokenAddress(MINT, BOSS)
-    //
-    // console.log(offerAuthority.toBase58())
-    // const bossAndOffers = await sumTokens2({
-    //     tokenAccounts: [offerTokenAccount, bossTokenAccount],
-    // });
-    // const tokenSupply = await getTokenSupplies([MINT]);
-    // const circulatingSupply = +tokenSupply[MINT] - (+bossAndOffers[`solana:${MINT}`])
-    // api.add(ADDRESSES.solana.USDC, BigInt(circulatingSupply) * BigInt(price) / BigInt(1e9))
 }
 
 module.exports = {
