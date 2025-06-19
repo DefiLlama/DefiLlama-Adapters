8c8ada937fb56e1f05e38b310b3020b9822ce238
Nas J <nas@altitude.fi>
2025-06-19 16:14:37 +0100

    Refactorings to remove some code duplication

--- projects/altitude-fi/index.js
+++ projects/altitude-fi/index.js
@@ -1,6 +1,38 @@
 const sdk = require('@defillama/sdk');
 const config = require('./addresses')
 
+async function calculateBorrowAmounts(api, config) {
+  const vaultBorrowCalls = config.vaults.map(vault => ({
+    target: vault.debtToken,
+    params: [vault.address]
+  }))
+
+  const totalBorrowCalls = config.vaults.map(vault => ({
+    target: vault.debtToken,
+  }))
+
+  const [vaultBorrowBalances, totalBorrow] = await Promise.all([
+    api.multiCall({
+      abi: 'erc20:balanceOf',
+      calls: vaultBorrowCalls,
+    }),
+    api.multiCall({
+      abi: 'erc20:totalSupply',
+      calls: totalBorrowCalls,
+    })
+  ])
+
+  return config.vaults.map((vault, i) => {
+    const vaultBorrowBalance = vaultBorrowBalances[i]
+    const totalBorrowBalance = totalBorrow[i]
+    const usersBorrow = totalBorrowBalance - vaultBorrowBalance
+    return {
+      vault,
+      usersBorrow,
+      underlyingBorrowToken: vault.underlyingBorrowToken
+    }
+  })
+}
 
 async function ethereum(includeBorrowed = false) {
   const api = new sdk.ChainApi({ chain: 'ethereum' })
@@ -15,44 +47,23 @@ async function ethereum(includeBorrowed = false) {
     calls,
   })
 
-  const vaultBorrowCalls = config.vaults.map(vault => ({
-    target: vault.debtToken,
-    params: [vault.address]
-  }))
-
-  const totalBorrowCalls = config.vaults.map(vault => ({
-    target: vault.debtToken,
-  }))
-
-  const vaultBorrowBalances = await api.multiCall({
-    abi: 'erc20:balanceOf',
-    calls: vaultBorrowCalls,
-  })
-
-  const totalBorrow = await api.multiCall({
-    abi: 'erc20:totalSupply',
-    calls: totalBorrowCalls,
-  })
-
   if (!includeBorrowed) {
+    // Add supply token balances
     config.vaults.forEach((vault, i) => {
       const balance = balances[i]
       api.add(vault.underlyingSupplyToken, balance)
     })
 
     // Subtract borrowed amounts from TVL
-    config.vaults.forEach((vault, i) => {
-      const vaultBorrowBalance = vaultBorrowBalances[i]
-      const totalBorrowBalance = totalBorrow[i]
-      const netBorrow = totalBorrowBalance - vaultBorrowBalance
-      api.add(vault.underlyingBorrowToken, -netBorrow)  // Subtract from TVL
+    const borrowData = await calculateBorrowAmounts(api, config)
+    borrowData.forEach(({ usersBorrow, underlyingBorrowToken }) => {
+      api.add(underlyingBorrowToken, -usersBorrow)
     })
   } else {
-    config.vaults.forEach((vault, i) => {
-      const vaultBorrowBalance = vaultBorrowBalances[i]
-      const totalBorrowBalance = totalBorrow[i]
-      const usersBorrow = totalBorrowBalance - vaultBorrowBalance
-      api.add(vault.underlyingBorrowToken, usersBorrow)  // Only USDC
+    // Only add borrowed amounts by user
+    const borrowData = await calculateBorrowAmounts(api, config)
+    borrowData.forEach(({ usersBorrow, underlyingBorrowToken }) => {
+      api.add(underlyingBorrowToken, usersBorrow)
     })
   }
 
@@ -65,5 +76,5 @@ module.exports = {
     borrowed: () => ethereum(true),
   },
   doublecounted: true,
-  methodology: 'TVL is calculated by summing the total value of tokens locked across all Altitude Finance vaults minus user borrows.'
+  methodology: 'TVL is calculated by counting the tokens locked in the contracts to be used as collateral to borrow or to earn yield. User borrows are not counted towards the TVL.'
 } 
