372262a9f6a59e3a323b0ec2564bab4aaddc3141
realshaman <realdealshaman11@gmail.com>
2025-06-20 10:20:02 -0600

    add unitas treasury adapter

---
+++ projects/treasury/unitas.js
@@ -0,0 +1,12 @@
+const { treasuryExports } = require("../helper/treasury");
+
+module.exports = treasuryExports({
+  solana: {
+    owners: [
+      'EjwCRUh3HhBaR7vaTrFzuNpDAnTX9h3ddZuiQgKqCadz',
+    ],
+    ownTokens: [
+      '', 
+    ]
+  },
+})
