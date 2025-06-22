f4a11606be52e8d79829df91dc329062b74e5606
miffy <miffybunnyrabbit@gmail.com>
2025-06-22 15:41:19 +1000

    Update index.js
    Revised TVL calculation for IMF v2

--- projects/imf-money/index.js
+++ projects/imf-money/index.js
@@ -1,11 +1,9 @@
-const { sumTokensExport } = require('../helper/unwrapLPs')
-const PEPE_TOKEN_CONTRACT = '0x6982508145454Ce325dDbE47a25d4ec3d2311933';
-const MOG_TOKEN_CONTRACT = '0xaaee1a9723aadb7afa2810263653a34ba2c21c7a';
-const LOCKED_MONEY_CONTRACT = '0x30F75834cB406b7093208Fda7F689938aCBD1EeB'; //wallet that has all the locked money
+const configs = {
+  methodology: 'Counts all assets that are deposited in the IMF vault.',
+  blockchains: {
+    ethereum: {
+      morphoVaultOwners: [
+        '0xdef1fce2df6270fdf7e1214343bebbab8583d43d',
+      ]
 
-module.exports = {
-  methodology: 'Sums the value of deposited memes',
-  ethereum: {
-    tvl: sumTokensExport({ owner: LOCKED_MONEY_CONTRACT, tokens: [PEPE_TOKEN_CONTRACT,MOG_TOKEN_CONTRACT]}),
-  }
-}; 
+module.exports = getCuratorExport(configs)
