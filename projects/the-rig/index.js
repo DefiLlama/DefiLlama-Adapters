fe96090f5815497495e59b1507d3ffeca542d170
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-06-26 13:03:44 +0200

    refactor

--- projects/the-rig/index.js
+++ projects/the-rig/index.js
@@ -1,18 +1,16 @@
 const { query } = require("../helper/chain/fuel")
 const { rigAbi } = require('./abi');
-const BigNumber = require("bignumber.js");
 
 const rigContract = "0x2181f1b8e00756672515807cab7de10c70a9b472a4a9b1b6ca921435b0a1f49b";
 const stFuelAssetId = { bits: "0x5505d0f58bea82a052bc51d2f67ab82e9735f0a98ca5d064ecb964b8fd30c474" };
-const stFuelAssetDecimalPlaces = BigNumber(10).pow(9);
 
 const fuelAssetId = "0x1d5d97005e41cae2187a895fd8eab0506111e0e2f3331cd3912c15c24e3c1d82";
 
 async function tvl(api) {
-  const redemptionRate = await query({ contractId: rigContract, abi: rigAbi, method: 'get_sanitized_price' });
+  let redemptionRate = await query({ contractId: rigContract, abi: rigAbi, method: 'get_sanitized_price' });
   const totalSupply = await query({ contractId: rigContract, abi: rigAbi, method: 'total_supply', params: [stFuelAssetId] });
 
-  const fuelStaked = BigNumber(totalSupply).multipliedBy(BigNumber(redemptionRate)).dividedBy(stFuelAssetDecimalPlaces);
+  const fuelStaked = totalSupply * redemptionRate / 1e9
   api.add(fuelAssetId, fuelStaked);
 }
 
