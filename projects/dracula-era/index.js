5790124bbedbc5363674cef88c27e102a7235105
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-06-26 14:33:01 +0200

    refactor uni adapters

--- projects/dracula-era/index.js
+++ projects/dracula-era/index.js
@@ -1,4 +1,6 @@
 const { uniTvlExports } = require('../helper/unknownTokens')
 module.exports = uniTvlExports({
-  'etn': '0x203D550ed6fA9dAB8A4190720CF9F65138abd15B'
-})
+  'era': '0x68e03D7B8B3F9669750C1282AD6d36988f4FE18e'
+}, { hasStablePools: true, })
+
+
