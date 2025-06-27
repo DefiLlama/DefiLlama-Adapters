5ec2d0fb19d4b934c63443a57163885cb40726c5
Tigran Arakelyan <traqel@gmail.com>
2025-06-27 16:46:21 +0400

    add TVL for jigsaw

--- projects/jigsaw/index.js
+++ projects/jigsaw/index.js
@@ -1,11 +1,20 @@
-const { sumERC4626VaultsExport } = require("../helper/erc4626");
+const axios = require('axios');
+
+const tvl = async (api) => {
+    const apiUrl = "https://api.jigsawdev.org/api/protocol/tvl";
+    try {
+        const {data} = await axios.get(apiUrl);
+        const totalValueLocked = data ? parseFloat(data.totalValueLocked) : 0;
+        api.addUSDValue(totalValueLocked)
+    } catch (error) {
+        console.error('Error fetching TVL:', error);
+        api.addUSDValue(0);
+    }
+}
 
 module.exports = {
-    methodology: 'ETH and LSTs in vaults',
+    methodology: "Data is retrieved from the api at https://api.jigsaw.org",
     ethereum: {
-        tvl: sumERC4626VaultsExport({ 
-            vaults: ["0x4412e5492C689CF13D585dCdb010B3b8b12dF16a", "0x9F5e6E972D76d4501900f4484622f9413E5Cc302", "0x3A10a803958e837599348621Edb42eF73c79aC22"],
-            tokenAbi: 'tokenIn', balanceAbi: 'totalSupply'
-        })
+        tvl: tvl
     }
-}; 
+};
