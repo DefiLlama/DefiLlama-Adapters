5ac8ac07c773a98ed9c7293ece5cfc1428206e69
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2025-06-27 14:35:37 +0200

    try with more sleeptime (#15261)

--- projects/gate-io/index.js
+++ projects/gate-io/index.js
@@ -1671,5 +1671,5 @@ unsupportedChains.forEach(chain => delete config[chain]);
 
 module.exports = mergeExports([
   cexExports(config),
-  { ethereum: { tvl: getStakedEthTVL({ withdrawalAddresses: ['0x287a66c7d9cba7504e90fa638911d74c4dc6a147', '0xbcf03ce48091e6b820a7c33e166e5d0109d8e712', '0x7a3f9b7120386249528c93e5eb373b78e54d5ba9'], sleepTime: 10_000, size: 200, proxy: true }) } },
+  { ethereum: { tvl: getStakedEthTVL({ withdrawalAddresses: ['0x287a66c7d9cba7504e90fa638911d74c4dc6a147', '0xbcf03ce48091e6b820a7c33e166e5d0109d8e712', '0x7a3f9b7120386249528c93e5eb373b78e54d5ba9'], sleepTime: 20_000, size: 200, proxy: true }) } },
 ]);
