d8561ff978a77ff96da11b6bca7de6a6d0ac78aa
Tigran Arakelyan <traqel@gmail.com>
2025-06-27 17:32:25 +0400

    code cleanup

--- projects/jigsaw/index.js
+++ projects/jigsaw/index.js
@@ -1,16 +1,15 @@
-const axios = require('axios');
-
 const tvl = async (api) => {
-    const apiUrl = "https://api.jigsaw.finance/api/protocol/tvl";
+    const apiUrl = "https://api.jigsawdev.org/api/protocol/tvl";
     try {
-        const {data} = await axios.get(apiUrl);
+        const response = await fetch(apiUrl);
+        const data = await response.json();
         const totalValueLocked = data ? parseFloat(data.totalValueLocked) : 0;
-        api.addUSDValue(totalValueLocked)
+        api.addUSDValue(totalValueLocked);
     } catch (error) {
         console.error('Error fetching TVL:', error);
         api.addUSDValue(0);
     }
-}
+};
 
 module.exports = {
     methodology: "Data is retrieved from the api at https://api.jigsaw.org",
