8293972e06f35692262bf4688d43856778c7703a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-06-29 19:24:02 +0200

    minor fix

--- projects/byreal/index.js
+++ projects/byreal/index.js
@@ -3,30 +3,25 @@ const { PublicKey } = require('@solana/web3.js')
 
 const CLMM = 'REALQqNEomY6cQGZJUGwywTBD2UmDT32rZcNnfxQ5N2'
 
-async function tvlCLMM() {
-    const connection = getConnection()
-    const accounts = await connection.getProgramAccounts(new PublicKey(CLMM), {
-      filters: [{
-        dataSize: 1544
-      }]
-    })
-    
-    const data = accounts.map(i => decodeAccount('byrealCLMM', i.account))
-    
-    const tokenAccounts = data.map(i => [i.vaultA, i.vaultB]).flat().map(i => i.toString())
+async function tv() {
+  const connection = getConnection()
+  const accounts = await connection.getProgramAccounts(new PublicKey(CLMM), {
+    filters: [{
+      dataSize: 1544
+    }]
+  })
 
-    return sumTokens2({ tokenAccounts })
-  }
+  const data = accounts.map(i => decodeAccount('byrealCLMM', i.account))
 
-async function combinedTvl(api) {
-    const balancesCLMM = await tvlCLMM()
-    api.addBalances(balancesCLMM);
-    return api.getBalances();
+  const tokenAccounts = data.map(i => [i.vaultA, i.vaultB]).flat().map(i => i.toString())
+
+  return sumTokens2({ tokenAccounts })
 }
+
 
 module.exports = {
   timetravel: false,
-  solana: { tvl: combinedTvl },
-  
+  solana: { tv },
+
 };
 
