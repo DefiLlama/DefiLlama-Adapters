7bd3365081d6f7311fc6374cdf3b0b12cd7b11b2
0xpeluche <0xpeluche@proton.me>
2025-06-30 10:28:41 +0200

    .

--- projects/catsluck/index.js
+++ projects/catsluck/index.js
@@ -53,5 +53,5 @@ module.exports = {
     deadFrom: '2025-06-01',
     misrepresentedTokens: true,
     methodology: "Total value of non-native tokens staked in prediction pools is counted towards tvl metric.",
-    smartbch: { tvl: tvl },
+    smartbch: { tvl },
 }
