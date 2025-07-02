da34c5398bbfcebadd1421add7594d11f214aac7
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-07-02 23:18:01 +0200

    refactor vechain projects

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -42,6 +42,10 @@ const fixBalancesTokens = {
   ozone: {
     // '0x83048f0bf34feed8ced419455a4320a735a92e9d': { coingeckoId: "ozonechain", decimals: 18 }, // was mapped to wrong chain
   },
+  vechain: {
+    '0xd8ccdd85abdbf68dfec95f06c973e87b1b5a9997': { coingeckoId: 'vechain', decimals: 18 },
+    '0x0000000000000000000000000000456e65726779': { coingeckoId: 'vethor-token', decimals: 18 },
+  }
 }
 
 ibcChains.forEach(chain => fixBalancesTokens[chain] = { ...ibcMappings, ...(fixBalancesTokens[chain] || {}) })
