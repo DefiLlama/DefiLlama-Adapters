6548b0216b892b3b1327adc5e405f216770936f4
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2025-07-02 11:42:49 +0200

    Fix hyperiondex linter (#15324)

--- projects/hyperiondex/index.js
+++ projects/hyperiondex/index.js
@@ -2,33 +2,28 @@ const { queryContract, sumTokens } = require("../helper/chain/cosmos");
 
 const HYPERION_FACTORY_ADDRESS = "titan1g4xlpqy29m50j5y69reguae328tc9y83l4299pf2wmjn0xczq5jsd2hhhu";
 
-async function tvl(api) {
+async function _getPairs(offset = 0, limit = 100, api) {
+	const { pairs } = await queryContract({ contract: HYPERION_FACTORY_ADDRESS, data: { pairs: { limit, offset } }, api });
+	return pairs.map((pair) => pair.contract_addr);
+}
+
+const tvl = async (api) => {
 	let offset = 0;
-	let limit = 100;
+	const limit = 100;
 	let pairs = [];
-	do {
-		const data = await _getPairs(offset, limit);
-		pairs = pairs.concat(data);
-		if (data.length < limit) break; // Exit if no more pairs are found
-		offset += limit; // Increment offset for next batch
-	} while (true)
+	let hasMore = true;
 
-	return sumTokens({ owners: pairs, api, })
-
-	async function _getPairs(offset = 0, limit = 100) {
-		const { pairs } = await queryContract({
-			contract: HYPERION_FACTORY_ADDRESS,
-			data: { pairs: { limit, offset, }, },
-			api,
-		});
-
-		return pairs.map((pair) => pair.contract_addr);
+	while (hasMore) {
+		const data = await _getPairs(offset, limit, api);
+		pairs.push(...data);
+		if (data.length < limit) break;
+		offset += limit;
 	}
+
+	return sumTokens({ owners: pairs, api });
 }
 
 module.exports = {
 	methodology: "Counts the total liquidity in all pools on Hyperion.",
-	titan: {
-		tvl,
-	},
-};
+	titan: { tvl }
+}
