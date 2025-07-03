b4d567c3e2acf417a6f373c9a19a24af14dc21e1
haiifeng <haiifenger@gmail.com>
2025-07-03 11:04:47 +0800

    feat: add bitlayer ybtc.b

---
+++ projects/bitlayer-ybtc/index.js
@@ -0,0 +1,12 @@
+const ybtc = '0x2cd3cdb3bd68eea0d3be81da707bc0c8743d7335'
+
+async function tvl(api) {
+  const supply = await api.call({  abi: 'erc20:totalSupply', target: ybtc })
+  api.add(ybtc, supply)
+}
+
+module.exports = {
+  timetravel: false,
+  btr: { tvl },
+  avax: { tvl },
+}
