cb61781ace2b0898fe902516dafb25f2c29803e7
haiifeng <haiifenger@gmail.com>
2025-07-03 19:11:16 +0800

    feat: change tvl on avax

--- projects/bitlayer-ybtc_b/index.js
+++ projects/bitlayer-ybtc_b/index.js
@@ -1,26 +1,19 @@
 const { sumTokensExport, nullAddress } = require("../helper/sumTokens");
 
-const ybtc = '0x2cd3cdb3bd68eea0d3be81da707bc0c8743d7335'
+const ybtc_b = '0x2cd3cdb3bd68eea0d3be81da707bc0c8743d7335'
 
-// async function tvl(api) {
-//   const supply = await api.call({  abi: 'erc20:totalSupply', target: ybtc })
-//   api.add(ybtc, supply)
-// }
+async function tvl(api) {
+  const supply = await api.call({  abi: 'erc20:totalSupply', target: ybtc_b })
+  api.add(ybtc_b, supply)
+}
 
 module.exports = {
   timetravel: false,
-  // btr: { tvl },
-  // avax: { tvl },
   btr: { tvl: sumTokensExport({
-    owners: [ybtc],
+    owners: [ybtc_b],
     tokens: [
       nullAddress,
     ],
   }), },
-  avax: { tvl: sumTokensExport({
-    owners: [ybtc],
-    tokens: [
-      nullAddress,
-    ],
-  }), },
+  avax: { tvl },
 }
