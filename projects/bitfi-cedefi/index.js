eead269e40d88469ac633534982a145abec7c596
nstanthony <anthony@bitfi.org>
2025-07-03 16:56:04 -0400

    add bfBTC address on goat network

--- projects/bitfi-cedefi/index.js
+++ projects/bitfi-cedefi/index.js
@@ -16,7 +16,10 @@ const mapping = {
   },
   core: {
     bfBTC: '0xCdFb58c8C859Cb3F62ebe9Cf2767F9e036C7fb15',
-  }
+  },
+  goat: {
+    bfBTC: '0x623F2774d9f27B59bc6b954544487532CE79d9DF',
+  },
 }
 const exportObject = {}
 Object.keys(mapping).forEach(chain => {
