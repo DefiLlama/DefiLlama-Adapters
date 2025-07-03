f16b87d04590026f412f5a74c505cb3b101201af
Alik <alik110.72@gmail.com>
2025-07-03 22:01:15 +0330

    Updated the wstkscUSD holder address
    from 0x13b435b9efe154ffc39eea7fe41dd6ded1fd96e6 to 0x4f22f13828c27b6d8fdeac116155d1df15f31875

--- projects/stablejack-wstkscUSD/index.js
+++ projects/stablejack-wstkscUSD/index.js
@@ -5,8 +5,8 @@ module.exports = {
   sonic: {
     tvl: sumTokensExport({
       tokensAndOwners: [
-        ['0x8e1e17343b8e4f5e1baa868500163212e00366cc', '0x13b435b9efe154ffc39eea7fe41dd6ded1fd96e6'], // Y-wstkscUSD
-        ['0xe8dcbc94a1a852e7f53713a0e927ef16d980b278', '0x13b435b9efe154ffc39eea7fe41dd6ded1fd96e6'], // P-wstkscUSD
+        ['0x8e1e17343b8e4f5e1baa868500163212e00366cc', '0x4f22f13828c27b6d8fdeac116155d1df15f31875'], // Y-wstkscUSD
+        ['0xe8dcbc94a1a852e7f53713a0e927ef16d980b278', '0x4f22f13828c27b6d8fdeac116155d1df15f31875'], // P-wstkscUSD
       ],
     }),
   },
