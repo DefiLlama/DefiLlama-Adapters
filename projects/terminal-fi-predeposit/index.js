598270ef0aae29e86165475c1df429327658ba18
Terminal dev <dev@terminal.fi>
2025-07-04 17:22:34 +0200

    fix: compute terminal-fi-predeposit tvl based on receipt tokens

--- projects/terminal-fi-predeposit/index.js
+++ projects/terminal-fi-predeposit/index.js
@@ -1,14 +1,47 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokensExport } = require('../helper/unwrapLPs');
+const { sumSingleBalance } = require('@defillama/sdk/build/generalUtil');
+const ADDRESSES = require('../helper/coreAssets.json');
+
+const receiptTokens = {
+  // tUSDe → USDe (Ethena USD)
+  '0xA01227A26A7710bc75071286539E47AdB6DEa417': ADDRESSES.ethereum.USDe,
+
+  // tETH → WETH (Wrapped Ether)
+  '0xa1150cd4A014e06F5E0A6ec9453fE0208dA5adAb': ADDRESSES.ethereum.WETH,
+
+  // tBTC → WBTC (Wrapped Bitcoin)
+  '0x6b6b870C7f449266a9F40F94eCa5A6fF9b0857E4': ADDRESSES.ethereum.WBTC,
+};
 
 module.exports = {
   ethereum: {
-    tvl: sumTokensExport({
-      tokensAndOwners: [
-        [ADDRESSES.ethereum.sUSDe, '0xFaAE52c6A6d477f859a740a76B29c33559ace18c'],
-        [ADDRESSES.ethereum.WEETH, '0xe042678e6c6871fa279e037c11e390f31334ba0b'],
-        [ADDRESSES.ethereum.WBTC, '0x2db1ec186acdeaf7d0fc78bffe335560b0fe0085'],
-      ]
-    }),
-  }
-}
+    tvl: async (api) => {
+      const balances = {};
+
+      for (const [receiptToken, underlyingToken] of Object.entries(receiptTokens)) {
+        const [receiptDecimals, underlyingDecimals, totalSupplyRaw] = await Promise.all([
+          api.call({ abi: 'erc20:decimals', target: receiptToken }),
+          api.call({ abi: 'erc20:decimals', target: underlyingToken }),
+          api.call({ abi: 'erc20:totalSupply', target: receiptToken }),
+        ]);
+
+        const totalSupply = BigInt(totalSupplyRaw.toString());
+        const scaleDiff = receiptDecimals - underlyingDecimals;
+
+        let adjustedSupply = totalSupply;
+
+        // Receipt token has more decimals than underlying token
+        if (scaleDiff > 0) {
+          adjustedSupply = totalSupply / BigInt(10 ** scaleDiff);
+        }
+        // Receipt token has fewer decimals than underlying token
+        else if (scaleDiff < 0) {
+          adjustedSupply = totalSupply * BigInt(10 ** scaleDiff);
+        }
+
+        sumSingleBalance(balances, underlyingToken, adjustedSupply.toString());
+      }
+
+      return balances;
+    },
+  },
+};
