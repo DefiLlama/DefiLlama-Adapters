f8b7f294ae187eb06fbd938a732a73be223a82b4
Tigran Arakelyan <traqel@gmail.com>
2025-07-08 17:03:04 +0400

    add TVL calculation

--- projects/jigsaw/index.js
+++ projects/jigsaw/index.js
@@ -1,19 +1,47 @@
-const tvl = async (api) => {
-    const apiUrl = "https://api.jigsawdev.org/api/protocol/tvl";
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        const totalValueLocked = data ? parseFloat(data.totalValueLocked) : 0;
-        api.addUSDValue(totalValueLocked);
-    } catch (error) {
-        console.error('Error fetching TVL:', error);
-        api.addUSDValue(0);
+const {request, gql} = require("graphql-request")
+
+const BASE_MAINNET_SUBGRAPH_URL = 'https://subgraph.satsuma-prod.com/5b49793e7bcd/jigsaw--557177/jigsaw-protocol/version/v0.0.1/api'
+
+async function fetchAllSubgraphResults({url, query, field, variables = {}}) {
+    let skip = 0;
+    let allResults = [];
+    let hasMore = true;
+    while (hasMore) {
+        const data = await request(url, query, {...variables, skip});
+        const results = data[field];
+        allResults = [...allResults, ...results];
+        if (results.length < 1000) {
+            hasMore = false;
+        } else {
+            skip += 1000;
+        }
     }
-};
+    return allResults;
+}
+
+async function collaterals(_, _1, _2, {api}) {
+    const query = gql`
+        query GetCollaterals {
+            collaterals { id, amount }
+        }
+    `;
+    const allCollaterals = await fetchAllSubgraphResults({
+        url: BASE_MAINNET_SUBGRAPH_URL,
+        query,
+        field: "collaterals",
+    });
+
+    for (const collateral of allCollaterals) {
+        const underlyingAddress = collateral.id;
+        if (!underlyingAddress) continue;
+        api.add(underlyingAddress, collateral.amount);
+    }
+    return api.getBalances();
+}
 
 module.exports = {
-    methodology: "Data is retrieved from the api at https://api.jigsaw.org",
+    methodology: "TVL includes all collateral locked in the protocol",
     ethereum: {
-        tvl: tvl
-    }
-};
+        tvl: collaterals,
+    },
+}
