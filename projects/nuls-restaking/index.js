d9047e743db3aed10d6ba712db7a1618fd230f48
MIMIEYES <pierreluohao@gmail.com>
2025-07-09 14:45:13 +0800

    add nuls-restaking

---
+++ projects/nuls-restaking/index.js
@@ -0,0 +1,14 @@
+const utils = require('../helper/utils');
+
+const url = 'https://assets.nabox.io/api/naistake/tvl'
+
+const tvl = async (api) => {
+  const data = await utils.fetchURL(url)
+  return api.addUSDValue(Math.round(data))
+}
+
+module.exports = {
+  misrepresentedTokens: true,
+  methodology: "A NULS AI Asset Re-Stake Platform.",
+  nuls: { tvl },
+}
