b329778fa99727209ade36a10d445f5b3d200285
Senn <sennhanami@163.com>
2025-07-09 10:20:01 +0800

    remove soon-staking project

--- projects/soon-staking/index.js
+++
@@ -1,8 +0,0 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokensExport } = require('../helper/sumTokens')
-
-module.exports = {
-	bsc: {
-		tvl: sumTokensExport({ owner: '0xcC48B55F6c16d4248EC6D78c11Ba19c1183Fe0F7', tokens: [ADDRESSES.bsc.SOON] }),
-	}
-}
