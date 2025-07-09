44f7f2a53018e5eff87e938d330ee89146e5118b
MIMIEYES <pierreluohao@gmail.com>
2025-07-09 17:59:07 +0800

    fixed data fetch

--- projects/nuls-restaking/index.js
+++ projects/nuls-restaking/index.js
@@ -3,8 +3,8 @@ const utils = require('../helper/utils');
 const url = 'https://assets.nabox.io/api/naistake/tvl'
 
 const tvl = async (api) => {
-  const data = await utils.fetchURL(url)
-  return api.addUSDValue(Math.round(data))
+  const data = await utils.fetchURL(url)  
+  return api.addUSDValue(Math.round(data.data))
 }
 
 module.exports = {
