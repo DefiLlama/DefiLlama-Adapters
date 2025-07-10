bda1d0a663c7cfbe3190f2ffe28f8c1af34b9d87
Senn <sennhanami@163.com>
2025-07-10 16:27:41 +0800

    swtich to soon

--- projects/cobaltX/index.js
+++ projects/cobaltX/index.js
@@ -2,8 +2,9 @@
 const { getConnection, sumTokens2 } = require("../helper/solana.js");
 
 module.exports = {
-  bsc: {
+  soon: {
     tvl: async (api) => {
+      console.log(api)
       const balances = await sumTokens2({api, tokenAccounts:['9XHNuy6oux6wu5FxMmqp5CWkigTy47MectU6QWPYegD3']})
       return balances
     },
