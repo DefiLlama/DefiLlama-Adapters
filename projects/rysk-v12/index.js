ae864f66e05f1efca8e8427988d0074d54aed9d1
Ashlyn <ashlyn.barry1@gmail.com>
2025-07-10 12:20:01 +1000

    use token label

--- projects/rysk-v12/index.js
+++ projects/rysk-v12/index.js
@@ -1,11 +1,12 @@
-const { sumTokensExport } = require("../helper/unwrapLPs")
+const ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
   hyperliquid: {
     tvl: sumTokensExport({
-      owner: '0x24a44f1dc25540c62c1196FfC297dFC951C91aB4',
+      owner: '0x24a44f1dc25540c62c1196FfC297dFC951C91aB4', // marginPool
       tokens: [
-        '0x5555555555555555555555555555555555555555', // WHYPE
+        ADDRESSES.hyperliquid.WHYPE,
         '0x9FDBdA0A5e284c32744D2f17Ee5c74B284993463', // UBTC
         '0xBe6727B535545C67d5cAa73dEa54865B92CF7907', // UETH
       ],
