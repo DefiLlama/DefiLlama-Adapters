893a5e6b590e78fe14ff2f910882f84b9c39a007
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2025-07-11 16:00:40 +0200

    Fix yearn (#15448)

--- projects/yearn/index.js
+++ projects/yearn/index.js
@@ -70,7 +70,7 @@ async function tvl(api) {
     bals = bals.map((bal, i) => bal * ratio[i] / 1e18)
     api.addTokens(tokens, bals)
   }
-  return sumTokens2({ api, resolveLP: true, })
+  return sumTokens2({ api, resolveLP: true, blacklistedLPs: ['0x917aa69d539d6518440dd0bea2eaac142a8d5610'] })
 }
 
 
