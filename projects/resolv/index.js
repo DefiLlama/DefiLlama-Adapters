3078f9c3338db1f615503798fdaa2f0c44cc2348
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2025-07-11 21:55:06 +0530

    added USR tokens on base and bsc chains for resolv tvl

--- projects/resolv/index.js
+++ projects/resolv/index.js
@@ -1,8 +1,13 @@
-const tokens = [
+const ethereumTokens = [
   '0x66a1E37c9b0eAddca17d3662D6c05F4DECf3e110', // USR
   '0x4956b52aE2fF65D74CA2d61207523288e4528f96'  // RLP
 ]
+const baseTokens=['0x35e5db674d8e93a03d814fa0ada70731efe8a4b9'] // USR
+
+const bscTokens = ['0x2492d0006411af6c8bbb1c8afc1b0197350a79e9']  // USR
 
 module.exports = {
-  ethereum: { tvl: async (api) =>  api.add(tokens, await api.multiCall({ calls: tokens, abi: 'erc20:totalSupply' })) }
+  ethereum: { tvl: async (api) =>  api.add(ethereumTokens, await api.multiCall({ calls: ethereumTokens, abi: 'erc20:totalSupply' })) },
+  base: { tvl: async (api) =>  api.add(baseTokens, await api.multiCall({ calls: baseTokens, abi: 'erc20:totalSupply' })) },
+  bsc: { tvl: async (api) =>  api.add(bscTokens, await api.multiCall({ calls: bscTokens, abi: 'erc20:totalSupply' })) }
 }
