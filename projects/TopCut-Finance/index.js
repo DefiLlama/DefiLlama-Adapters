1d9c35fc1fa8a33a0d3ecbe5fdf3b7dd79b5f449
James Osunkiyesi <jaydhales@gmail.com>
2025-07-13 22:40:59 +0100

    Add TVL for TopCut Vault

--- projects/TopCut-Finance/index.js
+++ projects/TopCut-Finance/index.js
@@ -1,25 +1,19 @@
-const portalsContractAddress = "0x24b7d3034C711497c81ed5f70BEE2280907Ea1Fa";
-const portalsV2 = {
-  USDC: "0x9167CFf02D6f55912011d6f498D98454227F4e16",
-  USDCE: "0xE8EfFf304D01aC2D9BA256b602D736dB81f20984",
-  ETH: "0xe771545aaDF6feC3815B982fe2294F7230C9c55b",
-  WBTC: "0x919B37b5f2f1DEd2a1f6230Bf41790e27b016609",
-  ARB: "0x523a93037c47Ba173E9080FE8EBAeae834c24082",
-  LINK: "0x51623b54753E07Ba9B3144Ba8bAB969D427982b6",
-};
+const { sumTokens2 } = require('../helper/unwrapLPs')
+
+const TOPCUT_VAULT = "0x3cfc3CBA1B4aAF969057F590D23efe46848F4270";
+const ETH = "0x0000000000000000000000000000000000000000"
+
 
 async function tvl(api) {
-  const vaults = [portalsContractAddress, ...Object.values(portalsV2)]
-  const bals = await api.multiCall({  abi: 'uint256:totalPrincipalStaked', calls: vaults})
-  const tokens = await api.multiCall({  abi: 'address:PRINCIPAL_TOKEN_ADDRESS', calls: vaults})
-  api.add(tokens, bals)
+  return sumTokens2({
+    owner: TOPCUT_VAULT,
+    tokens: [ETH],
+    api
+  })
 }
 
 module.exports = {
   methodology:
     "TVL is calculated based on the amount of ETH in the TopCut Vault.",
   arbitrum: { tvl },
-  hallmarks: [
-    [1715776637, "Portals V2 Launch"],
-  ],
 };
