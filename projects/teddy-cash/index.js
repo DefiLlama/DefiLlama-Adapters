163eb3dcad53079d2d1f1fcb100b4ee81c7cca1f
bghullu <ghullu.bijay@gmail.com>
2025-07-14 00:29:26 +0545

    Fix chainnameto avax for Teddy Cash adapter

--- projects/teddy-cash/index.js
+++ projects/teddy-cash/index.js
@@ -17,7 +17,7 @@ module.exports = {
   methodology:
     "Counts total AVAX collateral locked in Teddy Cash troves as TVL, " +
     "plus TEDDY tokens staked in the staking contract, and liquidity pool tokens staked in the WAVAX-TSD LP pool on Avalanche.",
-  avalanche: {
+  avax: {
     tvl: getLiquityTvl(TROVE_MANAGER_ADDRESS),
     staking: staking(STAKING_CONTRACT, TEDDY_TOKEN),
     pool2: pool2(POOL2_STAKING_CONTRACT, WAVAX_TSD_LP_TOKEN),
