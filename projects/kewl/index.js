ad88cd3b69b644991da714daf010199ea0926342
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2025-07-14 12:32:04 +0700

    add bsc and base to KEWL

--- projects/kewl/index.js
+++ projects/kewl/index.js
@@ -7,7 +7,7 @@ module.exports = {
   chz: {
     tvl: getUniTVL({
       factory: "0xA0BB8f9865f732C277d0C162249A4F6c157ae9D0",
-      useDefaultCoreAssets: true,
+      useDefaultCoreAssets: false,
     }),
   },
   avax: {
@@ -25,6 +25,18 @@ module.exports = {
   sonic: {
     tvl: getUniTVL({
       factory: "0xA0BB8f9865f732C277d0C162249A4F6c157ae9D0",
+      useDefaultCoreAssets: true,
+    }),
+  },
+  base: {
+    tvl: getUniTVL({
+      factory: "0x5636A64B835F4E3821C798fdA16E0bA106357646",
+      useDefaultCoreAssets: true,
+    }),
+  },
+  bsc: {
+    tvl: getUniTVL({
+      factory: "0x5636A64B835F4E3821C798fdA16E0bA106357646",
       useDefaultCoreAssets: true,
     }),
   },
