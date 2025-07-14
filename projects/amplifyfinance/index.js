e2d94eef9c9da9f7a59def4be4d621963d8684f7
bigbangcookie <88002666+bigbangcookie@users.noreply.github.com>
2025-07-14 19:20:51 +0200

    direct abi calls, pass array directly

--- projects/amplifyfinance/index.js
+++ projects/amplifyfinance/index.js
@@ -1,32 +1,10 @@
+const ADDRESSES = require('../helper/coreAssets.json');
 const AMPLIFY_VAULT = '0x89678540206E7d6964a4e22aE5Cf4ac55926651A'; //AmplifyFinance Amplified ETH vault
-const WSTETH_TOKEN = '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0'; // wstETH on Ethereum
-
-// ABI for the vault function to retrieve all strategies in the vault
-const VAULT_ABI = [
-  {
-    "stateMutability": "view",
-    "type": "function",
-    "name": "get_default_queue",
-    "inputs": [],
-    "outputs": [{"name": "", "type": "address[]"}]
-  }
-];
-
-// ABI for strategy balanceOfCollateral function
-const STRATEGY_ABI = [
-  {
-    "stateMutability": "view",
-    "type": "function",
-    "name": "balanceOfCollateral",
-    "inputs": [],
-    "outputs": [{"name": "", "type": "uint256"}]
-  }
-];
 
 async function tvl(api) {
   // Get all strategies inside the vault
   const strategies = await api.call({
-    abi: VAULT_ABI.find(f => f.name === 'get_default_queue'),
+    abi: "address[]:get_default_queue",
     target: AMPLIFY_VAULT,
   });
 
@@ -37,20 +15,12 @@ async function tvl(api) {
 
   // Get collateral balance for each strategy
   const collateralBalances = await api.multiCall({
-    abi: STRATEGY_ABI.find(f => f.name === 'balanceOfCollateral'),
+    abi: "uint256:balanceOfCollateral",
     calls: strategies.map(strategy => ({ target: strategy })),
   });
 
-  // Sum up all collateral balances
-  let totalCollateral = 0n;
-  for (const balance of collateralBalances) {
-    if (balance) {
-      totalCollateral += BigInt(balance);
-    }
-  }
-
-  // Add the total collateral as wstETH to the balances
-  api.add(WSTETH_TOKEN, totalCollateral.toString());
+  // Add the collateral balances as wstETH to the balances
+  api.add(ADDRESSES.ethereum.WSTETH, collateralBalances);
 }
 
 module.exports = {
