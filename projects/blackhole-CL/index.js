dc428ce4ade7659e0a3c9d5ca4229ac5d069dbc9
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-07-14 16:08:53 +0200

    minor fix

--- projects/blackhole-CL/index.js
+++ projects/blackhole-CL/index.js
@@ -1,16 +1,16 @@
-const { getUniTVL } = require('../helper/unknownTokens.js')
 const config = {
-  avax: { factory: '0x512eb749541B7cf294be882D636218c84a5e9E5F', blacklistedTokens: [''] },
+  avax: { factory: '0x512eb749541B7cf294be882D636218c84a5e9E5F', },
 }
 
 Object.keys(config).forEach(chain => {
-  const { factory, blacklistedTokens, } = config[chain]
-  module.exports[chain] = {
-    tvl: getUniTVL({
-      factory, blacklistedTokens, fetchBalances: true, abis: {
-        allPairsLength: 'uint256:allPairsLength',
-        allPairs: "function allPairs(uint) view returns (address)",
-      }
-    })
+  const { factory, } = config[chain]
+  module.exports[chain] = { tvl }
+
+  async function tvl(api) {
+    const pools = await api.fetchList({ lengthAbi: 'allPairsLength', itemAbi: 'allPairs', target: factory })
+    const token0s = await api.multiCall({ abi: 'address:token0', calls: pools })
+    const token1s = await api.multiCall({ abi: 'address:token1', calls: pools })
+    const tokensAndOwners2 = [token0s.concat(token1s), pools.concat(pools)]
+    return api.sumTokens({ tokensAndOwners2, })
   }
 })
--- projects/blackhole/index.js
+++ projects/blackhole/index.js
@@ -2,10 +2,4 @@ const { uniTvlExports } = require('../helper/unknownTokens')
 
 module.exports = uniTvlExports({
   'avax': '0xfE926062Fb99CA5653080d6C14fE945Ad68c265C'
-}, {
-  abis: {
-
-    allPairsLength: 'uint256:allPairsLength',
-    allPairs: 'function allPairs(uint256) view returns (address)',
-  }, hasStablePools: true,
-})
+}, { hasStablePools: true, })
