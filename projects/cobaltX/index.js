ca233dceced45d71b68dc043b01200bcc5642838
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-07-14 03:42:28 +0200

    Update index.js

--- projects/cobaltX/index.js
+++ projects/cobaltX/index.js
@@ -1,13 +1,10 @@
 
-const { getConnection, sumTokens2 } = require("../helper/solana.js");
-
 module.exports = {
-  soon: {
+  bsc: {
     tvl: async (api) => {
-      console.log(api)
-      const balances = await sumTokens2({api, tokenAccounts:['9XHNuy6oux6wu5FxMmqp5CWkigTy47MectU6QWPYegD3']})
-      return balances
+      const token = await api.call({ abi: 'address:asset', target: '0xcC48B55F6c16d4248EC6D78c11Ba19c1183Fe0F7' })
+      const bal = await api.call({ abi: 'uint256:totalAssets', target: '0xcC48B55F6c16d4248EC6D78c11Ba19c1183Fe0F7' })
+      api.add(token, bal)
     },
   },
-
 }
