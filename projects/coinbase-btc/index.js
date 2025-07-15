f36d32cd8b40e31a8b4f9397d3f44c2a0600dd92
Eden <noat.eth@gmail.com>
2025-07-15 18:54:14 +0700

    comment old codes

--- projects/coinbase-btc/index.js
+++ projects/coinbase-btc/index.js
@@ -14,3 +14,16 @@ module.exports = {
     },
   },
 };
+
+// const sdk = require('@defillama/sdk');
+// const { sumTokensExport } = require('../helper/sumTokens');
+// const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
+
+// module.exports = {
+//   methodology: "BTC collateral backing CBBTC. https://www.coinbase.com/cbbtc/proof-of-reserves",
+//   bitcoin: {
+//     tvl: sdk.util.sumChainTvls([
+//       sumTokensExport({ owners: bitcoinAddressBook.coinbasebtc }),
+//     ]),
+//   },
+// };
