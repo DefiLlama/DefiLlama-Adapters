63ab1eb3a0acae55b1484ed2bd936cad45154e2f
Eden <noat.eth@gmail.com>
2025-07-15 21:10:58 +0700

    use bitcoin wallets (#15495)

--- projects/coinbase-btc/index.js
+++ projects/coinbase-btc/index.js
@@ -1,29 +1,12 @@
 const sdk = require('@defillama/sdk');
+const { sumTokensExport } = require('../helper/sumTokens');
+const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
 
-// https://www.coinbase.com/cbbtc/proof-of-reserves
 module.exports = {
-  methodology: "BTC collateral backing CBBTC reported by ChainLink Smart Data on Base 0x0F8E057D1D7b282EF968D26E9cB432617dF52519",
+  methodology: "BTC collateral backing CBBTC. https://www.coinbase.com/cbbtc/proof-of-reserves",
   bitcoin: {
-    tvl: async function (api) {
-      const latestRoundData = await sdk.api2.abi.call({
-        chain: 'base',
-        target: '0x0F8E057D1D7b282EF968D26E9cB432617dF52519',
-        abi: 'function latestRoundData() view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)',
-      });
-      api.addCGToken('bitcoin', Number(latestRoundData.answer) / 1e8)
-    },
+    tvl: sdk.util.sumChainTvls([
+      sumTokensExport({ owners: bitcoinAddressBook.coinbasebtc }),
+    ]),
   },
 };
-
-// const sdk = require('@defillama/sdk');
-// const { sumTokensExport } = require('../helper/sumTokens');
-// const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
-
-// module.exports = {
-//   methodology: "BTC collateral backing CBBTC. https://www.coinbase.com/cbbtc/proof-of-reserves",
-//   bitcoin: {
-//     tvl: sdk.util.sumChainTvls([
-//       sumTokensExport({ owners: bitcoinAddressBook.coinbasebtc }),
-//     ]),
-//   },
-// };
