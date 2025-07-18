6b0bc7fe588dcd02e663ba9360a83f06fe0c7871
rockluckycat <rockluckycat@gmail.com>
2025-07-19 00:38:33 +0800

    feature: add new TVL source

--- projects/doubleup/index.js
+++ projects/doubleup/index.js
@@ -2,7 +2,11 @@ const sui = require("../helper/chain/sui");
 
 const UNI_HOUSE_OBJ_ID = "0x75c63644536b1a7155d20d62d9f88bf794dc847ea296288ddaf306aa320168ab"
 
+const UPUSD = "0x5de877a152233bdd59c7269e2b710376ca271671e9dd11076b1ff261b2fd113c::up_usd::UP_USD"
+const UPUSD_FACTORY = "0x35507cec814a779e23393aa8e17746c51e38fb5efa18afdc8434a670da4d3338"
+
 async function tvl(api) {
+  //Unihouse
   const unihouseDynamicFields = await sui.getDynamicFieldObjects({
     parent: UNI_HOUSE_OBJ_ID,
   });
@@ -23,13 +27,31 @@ async function tvl(api) {
     const housePool = house?.fields?.house_pool;
     const houseTvl = Number(housePipeDebt) + Number(housePool);
 
-    api.add(coinType, houseTvl);
+    // api.add(coinType, houseTvl);
 
     const pipeDebt = house?.fields?.pipe_debt?.fields?.value;
     const _tvl = Number(pipeDebt) + Number(house?.fields?.pool);
 
-    api.add(coinType, _tvl);
+
+    if(coinType === UPUSD || coinType === "0x49f123d62df1db5a735463f3817dcd53aa084173cbe4593db6b30647b9801cb8::unihouse::FeeTag, 0x49f123d62df1db5a735463f3817dcd53aa084173cbe4593db6b30647b9801cb8::unihouse::HouseFeeConfig"){
+      continue;
+    }else {
+      console.log(`${coinType}`,houseTvl + _tvl);
+
+      api.add(coinType, houseTvl+_tvl);
+    }
+    
   }
+
+  // UPUSD
+  const upusd_factory = await sui.getObject(UPUSD_FACTORY)
+
+  const upusd_tvl = upusd_factory?.fields?.basic_supply?.fields?.supply;
+  
+  console.log(`${UPUSD}`,upusd_tvl);
+
+  api.add("0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC", upusd_tvl);
+
 }
 
 module.exports = {
