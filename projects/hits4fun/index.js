a8bde97b1767776c047b4e66c7d59a28095e7bb7
Senor Scriptito <SenorScriptito@proton.me>
2025-07-18 18:32:19 +0200

    fix: update methodology description for clarity in hits4fun adapter

--- projects/hits4fun/index.js
+++ projects/hits4fun/index.js
@@ -5,7 +5,7 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const VAULT_ADDRESS = '0xDcC2BDbEE24813a4000Afd58252a462ff226bFA1'
 
 module.exports = {
-  methodology: 'Counts the number of ETH in the vault',
+  methodology: 'Counts the amount of ETH in the vault',
   start: 1751443965, // Wed Jul 02 2025 08:12:45 GMT+0000
   base: {
     tvl: async (api) => {
