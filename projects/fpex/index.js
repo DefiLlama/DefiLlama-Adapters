dc0d9266359e99f7f9eca99eb642033569f9f10c
MCNB <benjamin.mcnaughton@zoppas.com>
2025-07-20 11:07:52 +0200

    added fpex to /projects

---
+++ projects/fpex/index.js
@@ -0,0 +1,6 @@
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+  flare: { factory: '0xb06df504137c3f393f0f089ebcad5ae7db592c6f', fromBlock: 
+    44097841, },
+})
