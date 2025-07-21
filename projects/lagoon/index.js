ed71b09bc83f4d2ff40c8cc885a0a0be6b9f1551
Guillaume Dupont <gdupont.pro@gmail.com>
2025-07-21 12:32:38 +0200

    un comment

--- projects/lagoon/index.js
+++ projects/lagoon/index.js
@@ -39,102 +39,102 @@ const config = {
       fromBlock: 29100401
     },
   },
-  // berachain: {
-  //   vaults: [],
-  //   optinProxyFactory:{
-  //     address: "0x245d1C095a0fFa6f1Af0f7Df81818DeFc9Cfc69D",
-  //     fromBlock: 7858746
-  //   },
-  //   beaconFactory: {
-  //     address: "0x7CF8cF276450BD568187fDC0b0959D30eC599853",
-  //     fromBlock: 4061769
-  //   },
-  // },
-  // ethereum: {
-  //   vaults:
-  //     [
-  //       "0x07ed467acD4ffd13023046968b0859781cb90D9B", // 9Summits Flagship ETH
-  //       "0x03D1eC0D01b659b89a87eAbb56e4AF5Cb6e14BFc", // 9Summits Flagship USDC
-  //       "0xB09F761Cb13baCa8eC087Ac476647361b6314F98", // 9Summits & Tulipa Capital cbBTC 
-  //       "0x8092cA384D44260ea4feaf7457B629B8DC6f88F0", // Usual Invested USD0++ in stUSR
-  //       "0x66dCB62da5430D800a1c807822C25be17138fDA8", // Unity Trust
-  //       "0x71652D4898DE9A7DD35e472a5fe4577eC69d82f2", // Trinity Trust
-  //       "0x7895a046b26cc07272b022a0c9bafc046e6f6396", // Noon tacUSN
-  //       "0x8245FD9Ae99A482dFe76576dd4298f799c041D61", // Usual Invested USD0++ in USCC & USTB
-  //       "0xaf87b90e8a3035905697e07bb813d2d59d2b0951", // Usual Invested USD0++ in TAC
-  //     ],
-  //   optinProxyFactory:{
-  //     address: "0x8D6f5479B14348186faE9BC7E636e947c260f9B1",
-  //     fromBlock: 22940919
-  //   },
-  //   beaconFactory: {
-  //     address: "0x09C8803f7Dc251f9FaAE5f56E3B91f8A6d0b70ee",
-  //     fromBlock: 22218451
-  //   },
-  // },
+  berachain: {
+    vaults: [],
+    optinProxyFactory:{
+      address: "0x245d1C095a0fFa6f1Af0f7Df81818DeFc9Cfc69D",
+      fromBlock: 7858746
+    },
+    beaconFactory: {
+      address: "0x7CF8cF276450BD568187fDC0b0959D30eC599853",
+      fromBlock: 4061769
+    },
+  },
+  ethereum: {
+    vaults:
+      [
+        "0x07ed467acD4ffd13023046968b0859781cb90D9B", // 9Summits Flagship ETH
+        "0x03D1eC0D01b659b89a87eAbb56e4AF5Cb6e14BFc", // 9Summits Flagship USDC
+        "0xB09F761Cb13baCa8eC087Ac476647361b6314F98", // 9Summits & Tulipa Capital cbBTC 
+        "0x8092cA384D44260ea4feaf7457B629B8DC6f88F0", // Usual Invested USD0++ in stUSR
+        "0x66dCB62da5430D800a1c807822C25be17138fDA8", // Unity Trust
+        "0x71652D4898DE9A7DD35e472a5fe4577eC69d82f2", // Trinity Trust
+        "0x7895a046b26cc07272b022a0c9bafc046e6f6396", // Noon tacUSN
+        "0x8245FD9Ae99A482dFe76576dd4298f799c041D61", // Usual Invested USD0++ in USCC & USTB
+        "0xaf87b90e8a3035905697e07bb813d2d59d2b0951", // Usual Invested USD0++ in TAC
+      ],
+    optinProxyFactory:{
+      address: "0x8D6f5479B14348186faE9BC7E636e947c260f9B1",
+      fromBlock: 22940919
+    },
+    beaconFactory: {
+      address: "0x09C8803f7Dc251f9FaAE5f56E3B91f8A6d0b70ee",
+      fromBlock: 22218451
+    },
+  },
   
-  // katana: {
-  //   vaults: [],
-  //   optinProxyFactory:{
-  //     address: "0xC094C224ce0406BC338E00837B96aD2e265F7287",
-  //     fromBlock: 6025911
-  //   },
-  //   beaconFactory: {
-  //     address: "0x37F4B3f0102FDC1ff0C7eF644751052fb276dc6e",
-  //     fromBlock: 4544281
-  //   },
-  // },
-  // mantle:{
-  //   optinProxyFactory:{
-  //     address: "0xc094c224ce0406bc338e00837b96ad2e265f7287",
-  //     fromBlock: 82320704
-  //   },
-  //   beaconFactory: {
-  //     address: "0x57D969B556C6AebB3Ac8f54c98CF3a3f921d5659",
-  //     fromBlock: 79901742,
-  //   },
-  // }, 
-  // sonic: {
-  //   optinProxyFactory:{
-  //     address: "0x6FC0F2320483fa03FBFdF626DDbAE2CC4B112b51",
-  //     fromBlock: 38968917
-  //   },
-  //   beaconFactory: {
-  //     address: "0x8846189A4E46997Dd30Fd9e8bE48C1fA1B846920",
-  //     fromBlock: 21645993,
-  //   }
-  // },
-  // tac: {
-  //   optinProxyFactory:{
-  //     address: "0x66Ab87A9282dF99E38C148114F815a9C073ECA8D",
-  //     fromBlock: 2334460
-  //   },
-  //   beaconFactory: {
-  //     address: "0x3e39E287B4c94aC18831A63E5a6183Aa42cd85c3",
-  //     fromBlock: 333643,
-  //   },
-  // },
-  // unichain: {
-  //   vaults: [],
-  //   optinProxyFactory:{
-  //     address: "0x6FC0F2320483fa03FBFdF626DDbAE2CC4B112b51",
-  //     fromBlock: 22021431
-  //   },
-  //   beaconFactory: {
-  //     address: "0xaba1A2e157Dae248f8630cA550bd826725Ff745c",
-  //     fromBlock: 14576623
-  //   },
-  // },
-  // wc: {
-  //   optinProxyFactory:{
-  //     address: "0xC094C224ce0406BC338E00837B96aD2e265F7287",
-  //     fromBlock: 16717424
-  //   },
-  //   beaconFactory: {
-  //     address: "0x600fA26581771F56221FC9847A834B3E5fd34AF7",
-  //     fromBlock: 14294168
-  //   },
-  // },
+  katana: {
+    vaults: [],
+    optinProxyFactory:{
+      address: "0xC094C224ce0406BC338E00837B96aD2e265F7287",
+      fromBlock: 6025911
+    },
+    beaconFactory: {
+      address: "0x37F4B3f0102FDC1ff0C7eF644751052fb276dc6e",
+      fromBlock: 4544281
+    },
+  },
+  mantle:{
+    optinProxyFactory:{
+      address: "0xc094c224ce0406bc338e00837b96ad2e265f7287",
+      fromBlock: 82320704
+    },
+    beaconFactory: {
+      address: "0x57D969B556C6AebB3Ac8f54c98CF3a3f921d5659",
+      fromBlock: 79901742,
+    },
+  }, 
+  sonic: {
+    optinProxyFactory:{
+      address: "0x6FC0F2320483fa03FBFdF626DDbAE2CC4B112b51",
+      fromBlock: 38968917
+    },
+    beaconFactory: {
+      address: "0x8846189A4E46997Dd30Fd9e8bE48C1fA1B846920",
+      fromBlock: 21645993,
+    }
+  },
+  tac: {
+    optinProxyFactory:{
+      address: "0x66Ab87A9282dF99E38C148114F815a9C073ECA8D",
+      fromBlock: 2334460
+    },
+    beaconFactory: {
+      address: "0x3e39E287B4c94aC18831A63E5a6183Aa42cd85c3",
+      fromBlock: 333643,
+    },
+  },
+  unichain: {
+    vaults: [],
+    optinProxyFactory:{
+      address: "0x6FC0F2320483fa03FBFdF626DDbAE2CC4B112b51",
+      fromBlock: 22021431
+    },
+    beaconFactory: {
+      address: "0xaba1A2e157Dae248f8630cA550bd826725Ff745c",
+      fromBlock: 14576623
+    },
+  },
+  wc: {
+    optinProxyFactory:{
+      address: "0xC094C224ce0406BC338E00837B96aD2e265F7287",
+      fromBlock: 16717424
+    },
+    beaconFactory: {
+      address: "0x600fA26581771F56221FC9847A834B3E5fd34AF7",
+      fromBlock: 14294168
+    },
+  },
 };
 
 const vaultsBlacklist = [
