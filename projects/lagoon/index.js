ebda38212ea11aea457763507b03e7af847674de
Guillaume Dupont <gdupont.pro@gmail.com>
2025-07-22 16:31:03 +0200

    add tac 9s eth blacklist on tac

--- projects/lagoon/index.js
+++ projects/lagoon/index.js
@@ -139,7 +139,8 @@ const config = {
 };
 
 const vaultsBlacklist = [
-  "0xDe7CFf032D453Ce6B0a796043E75d380Df258812", // vault tac 9S, used mostly by another vault: 9s flagship
+  "0xDe7CFf032D453Ce6B0a796043E75d380Df258812", // vault tac 9S, used mostly by another vault: 9s flagship, on Eth mainnet
+  "0xd6DaBAf70977a867Fa884844FC5DCb21DE81c498", // vault tac 9s. but on TAC chain
   "0xd730f24d993398d29dbaa537b6e1bd71a55df775", // test vault with fake totalAssets 
 ]
 
