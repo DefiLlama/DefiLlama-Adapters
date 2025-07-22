a500ea5c3d482bc9816b4e2afac3c5fdf6832864
Alik <alik110.72@gmail.com>
2025-07-22 23:44:39 +0330

    Track the TVL of artBTC in Stablejack V2 (Goat chain)

--- projects/stablejack-scusd/index.js
+++ projects/stablejack-scusd/index.js
@@ -7,16 +7,14 @@ module.exports = {
     "TVL includes scUSD, STS, wOS, and wstkscUSD on Sonic and Avalanche chains. Also includes artBTC held by SJ-wartBTC wrapper contract on Goat chain.",
 
   sonic: {
-    tvl: async (api) => {
-      const tokensAndOwners = [
-        [ADDRESSES.sonic.scUSD, '0xf41ECda82C54745aF075B79b6b31a18dD986BA4c'], // scUSD
-        [ADDRESSES.sonic.STS, '0x682D7F02BC57Bc64bfb36078454601Ba0Efbe155'], // stS
+    tvl: sumTokensExport({
+      tokensAndOwners: [
+        [ADDRESSES.sonic.scUSD, '0xf41ECda82C54745aF075B79b6b31a18dD986BA4c'],
+        [ADDRESSES.sonic.STS, '0x682D7F02BC57Bc64bfb36078454601Ba0Efbe155'],
         ['0x9f0df7799f6fdad409300080cff680f5a23df4b1', '0x0A6F4c98D087445Ef92b589c6f39D22C4373615F'], // wOS
         ['0x9fb76f7ce5FCeAA2C42887ff441D46095E494206', '0xb27f555175e67783ba16f11de3168f87693e3c8f'], // wstkscUSD
-      ];
-
-      return api.sumTokens({ tokensAndOwners });
-    },
+      ],
+    }),
   },
 
   avax: {
@@ -29,18 +27,10 @@ module.exports = {
   },
 
   goat: {
-    tvl: async (api) => {
-      const artBTC = '0x02F294cC9Ceb2c80FbA3fD779e17FE191Cc360C4'; // artBTC
-      const sjWartBTC = '0x0238E736166e07D6F857A0E322dAd4e7C1AFF4F3'; // SJ-wartBTC (wrapper contract)
-
-      const balance = await api.call({
-        abi: 'erc20:balanceOf',
-        target: artBTC,
-        params: [sjWartBTC],
-      });
-
-      api.add(artBTC, balance);
-      return api.getBalances();
-    },
+    tvl: sumTokensExport({
+      tokensAndOwners: [
+        ['0x02F294cC9Ceb2c80FbA3fD779e17FE191Cc360C4', '0x0238E736166e07D6F857A0E322dAd4e7C1AFF4F3'], // artBTC in SJ-wartBTC
+      ],
+    }),
   },
 };
