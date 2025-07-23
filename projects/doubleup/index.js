b11312dc0151d03a6b00204a6548e2c1a0058814
rockluckycat <rockluckycat@gmail.com>
2025-07-23 20:45:58 +0800

    feature: modified TVL source

--- projects/doubleup/index.js
+++ projects/doubleup/index.js
@@ -2,6 +2,7 @@ const sui = require("../helper/chain/sui");
 
 const UNI_HOUSE_OBJ_ID = "0x75c63644536b1a7155d20d62d9f88bf794dc847ea296288ddaf306aa320168ab"
 
+const BUCK = "0xce7ff77a83ea0cb6fd39bd8748e2ec89a3f41e8efdc3f4eb123e0ca37b184db2::buck::BUCK"
 const UPUSD = "0x5de877a152233bdd59c7269e2b710376ca271671e9dd11076b1ff261b2fd113c::up_usd::UP_USD"
 const UPUSD_FACTORY = "0x35507cec814a779e23393aa8e17746c51e38fb5efa18afdc8434a670da4d3338"
 
@@ -43,11 +44,11 @@ async function tvl(api) {
   // UPUSD
   const upusd_factory = await sui.getObject(UPUSD_FACTORY)
 
-  const upusd_tvl = upusd_factory?.fields?.basic_supply?.fields?.supply;
+  const buck_tvl = upusd_factory?.fields?.underlying_balance;
   
-  console.log(`${UPUSD}`,upusd_tvl);
+  console.log(`${BUCK}`,buck_tvl);
 
-  api.add("0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC", upusd_tvl);
+  api.add(BUCK, buck_tvl);
 
 }
 
