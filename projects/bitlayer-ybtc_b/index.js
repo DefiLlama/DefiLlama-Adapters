0a77900697435c470369f2334675a07440047571
haiifeng <haiifenger@gmail.com>
2025-07-24 11:24:18 +0800

    feat: only btr

--- projects/bitlayer-ybtc_b/index.js
+++ projects/bitlayer-ybtc_b/index.js
@@ -2,21 +2,11 @@ const { sumTokensExport, nullAddress } = require("../helper/sumTokens");
 
 const ybtc_b = '0x2cd3cdb3bd68eea0d3be81da707bc0c8743d7335'
 
-async function tvl(api) {
-  const supply = await api.call({  abi: 'erc20:totalSupply', target: ybtc_b })
-  api.add(ybtc_b, supply)
-}
-
 module.exports = {
-  timetravel: false,
   btr: { tvl: sumTokensExport({
     owners: [ybtc_b],
     tokens: [
       nullAddress,
     ],
   }), },
-  avax: { tvl },
-  ethereum: { tvl },
-  bsc: { tvl },
-  plume_mainnet: { tvl },
 }
