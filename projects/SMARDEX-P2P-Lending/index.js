d182b83a21f378e2a8a2924adbadb14fb9db8931
Alfred GAILLARD <a.gaillard@ra2.tech>
2025-07-24 15:19:49 +0200

    feat: add multichain support

--- projects/SMARDEX-P2P-Lending/index.js
+++ projects/SMARDEX-P2P-Lending/index.js
@@ -4,17 +4,22 @@ const headers = {
   "x-api-key": process.env.SMARDEX_SUBGRAPH_API_KEY,
 };
 
-const subgraphUrl = "https://subgraph.smardex.io/ethereum/spro";
+const ethereumSubgraphUrl = "https://subgraph.smardex.io/ethereum/spro";
+const arbitrumSubgraphUrl = "https://subgraph.smardex.io/arbitrum/spro";
+const bscSubgraphUrl = "https://subgraph.smardex.io/bsc/spro";
+const baseSubgraphUrl = "https://subgraph.smardex.io/base/spro";
+const polygonPosSubgraphUrl = "https://subgraph.smardex.io/polygon/spro";
 
-const getTokenMetrics = async () => {
-  const tokenMetricsQuery = `{
-    tokenMetrics_collection {
-      id
-      totalCollateralAmount
-      totalBorrowedAmount
-    }
-  }`;
 
+const tokenMetricsQuery = `{
+  tokenMetrics_collection {
+    id
+    totalCollateralAmount
+    totalBorrowedAmount
+  }
+}`;
+
+const getTokenMetrics = async (subgraphUrl) => {
   const result = await fetch(subgraphUrl, {
     method: "POST",
     headers,
@@ -25,25 +30,38 @@ const getTokenMetrics = async () => {
   return result?.data?.tokenMetrics_collection || [];
 };
 
-async function getP2pData(isBorrowed = false) {
-  const tokenMetrics = await getTokenMetrics();
 
+async function getP2pData(subgraphUrl, isBorrowed = false) {
+  const tokenMetrics = await getTokenMetrics(subgraphUrl);
   return tokenMetrics.reduce((acc, token) => {
     const totalBorrowedAmount = parseFloat(token.totalBorrowedAmount);
-
     return {
       ...acc,
-      [token.id]:
-        // We only need to add the total collateral amount if it's not borrowed
-        totalBorrowedAmount + parseFloat(isBorrowed ? 0 : token.totalCollateralAmount),
+      [token.id]: totalBorrowedAmount + parseFloat(isBorrowed ? 0 : token.totalCollateralAmount),
     };
   }, {});
 }
 
 module.exports = {
   ethereum: {
-    tvl: () => getP2pData(),
-    borrowed: () => getP2pData(true),
+    tvl: () => getP2pData(ethereumSubgraphUrl),
+    borrowed: () => getP2pData(ethereumSubgraphUrl, true),
+  },
+  arbitrum: {
+    tvl: () => getP2pData(arbitrumSubgraphUrl),
+    borrowed: () => getP2pData(arbitrumSubgraphUrl, true),
+  },
+  bsc: {
+    tvl: () => getP2pData(bscSubgraphUrl),
+    borrowed: () => getP2pData(bscSubgraphUrl, true),
+  },
+  base: {
+    tvl: () => getP2pData(baseSubgraphUrl),
+    borrowed: () => getP2pData(baseSubgraphUrl, true),
+  },
+  polygon: {
+    tvl: () => getP2pData(polygonPosSubgraphUrl),
+    borrowed: () => getP2pData(polygonPosSubgraphUrl, true),
   },
 };
 // node test.js projects/p2p-lending/index.js
