f5205f7f90fd09df50dbc5112beab9e32951b8be
Eden <noat.eth@gmail.com>
2025-07-28 00:38:35 +0700

    query angstrom history tvl

--- projects/angstrom/index.js
+++ projects/angstrom/index.js
@@ -1,31 +1,55 @@
 
 const subgraphEndpoint = 'https://api.goldsky.com/api/public/project_cm97dvfxxyivn01xe2sda93ka/subgraphs/angstrom-mainnet/1.3.3/gn'
-const subgraphQuery = {
-  query: `
-    {
-      pools(first: 1000) {
-        token0 {
-          id
-          decimals
-        }
-        token1 {
-          id
-          decimals
-        }
-        totalValueLockedToken0
-        totalValueLockedToken1
-      }    
-    }
+const subgraphQuery = (blockNumber) => {
+  return {
+    query: `
+      {
+        pools(first: 1000, block: {number: ${blockNumber}}) {
+          token0 {
+            id
+            decimals
+          }
+          token1 {
+            id
+            decimals
+          }
+          totalValueLockedToken0
+          totalValueLockedToken1
+        }    
+      }
 
-  `
+    `
+  }
 };
+
+async function getGraphBlock() {
+  const query = {
+    query: `
+      {
+        _meta {
+          block {
+            number
+          }
+        }
+      }
+    `
+  }
+  const response = await fetch(subgraphEndpoint, {
+    "body": JSON.stringify(query),
+    "method": "POST"
+  })
+  const data = await response.json()
+
+  return Number(data.data._meta.block.number)
+}
 
 module.exports.ethereum = {
   tvl: async (api) => {
     const block = await api.getBlock(api.timestamp)
+    const graphBlock = await getGraphBlock()
 
     const response = await fetch(subgraphEndpoint, {
-      "body": JSON.stringify(subgraphQuery),
+      "body": JSON.stringify(subgraphQuery(block > graphBlock ? graphBlock : block)),
       "method": "POST"
     })
     const data = await response.json()
