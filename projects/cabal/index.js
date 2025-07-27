14e830abde2fc9e5b9219ef5fdc7fa48fc6b521b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-07-27 10:54:01 +0200

    refactor

--- projects/cabal/index.js
+++ projects/cabal/index.js
@@ -6,9 +6,9 @@ const INIT_METADATA_ADDRESS = '0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996
 const USDC_INIT_LP_METADATA_ADDRESS = '0x543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1'
 
 function toNum(str) {
-    const clean = String(str).replace(/[^\d.]/g, '');
-    return parseFloat(clean);
-  }
+  const clean = String(str).replace(/[^\d.]/g, '');
+  return parseFloat(clean);
+}
 
 async function fetchView(functionName, moduleName, args) {
   const response = await post(REST_URL, {
@@ -26,22 +26,12 @@ async function tvl(api) {
     fetchView('get_real_total_stakes', 'pool_router', [`"${INIT_METADATA_ADDRESS}"`]),
     fetchView('get_real_total_stakes', 'pool_router', [`"${USDC_INIT_LP_METADATA_ADDRESS}"`])
   ])
-
-  const [initPrice, lpPrice] = await Promise.all([
-    fetchView('get_token_price_in_usd', 'utils', [`"${INIT_METADATA_ADDRESS}"`]),
-    fetchView('get_lp_token_value_in_usd', 'utils', [`"${USDC_INIT_LP_METADATA_ADDRESS}"`, '"1000000"'])
-  ])
-  
-  const initValueUsd = toNum(initStakes) * toNum(initPrice)
-  const lpValueUsd = toNum(lpStakes) * toNum(lpPrice)
-  const totalTvl = (initValueUsd + lpValueUsd) / 10 ** 6
-
-  api.addUSDValue(totalTvl)
+  api.add(INIT_METADATA_ADDRESS, toNum(initStakes))
+  api.add(USDC_INIT_LP_METADATA_ADDRESS, toNum(lpStakes))
 }
 
 module.exports = {
   timetravel: false,
-  misrepresentedTokens: true,
   methodology: 'TVL is calculated as the sum of INIT token stakes and USDC-INIT LP token stakes, each multiplied by their on-chain USD value.',
   initia: {
     tvl
