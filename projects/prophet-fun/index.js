964b864fa2f33bab0ca6441ae1208df38d10d557
MrJeleika <den932005@gmail.com>
2025-07-28 20:49:43 +0300

    fix prophet.fun to fetch data from chain

--- projects/prophet-fun/index.js
+++ projects/prophet-fun/index.js
@@ -1,10 +1,46 @@
 const axios = require('axios');
+const { PublicKey } = require("@solana/web3.js");
+const { sumTokens2, ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID } = require('../helper/solana');
+const BN = require("bn.js");
+
+const programId = new PublicKey('ProPh6ruVL41JR3XXPuy6hN6TPH1ERqpWkZ9dp9YSEe')
+const globalState = new PublicKey('6PZKJowZMUAgxLxAJmHsrvzEL8PdXNymqYSJDwPRgh6V')
+const token = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v')
+
+const getAta = (owner) => {
+  const [ata] = PublicKey.findProgramAddressSync(
+    [
+      owner.toBuffer(),
+      TOKEN_PROGRAM_ID.toBuffer(),
+      token.toBuffer(),
+    ],
+    ASSOCIATED_TOKEN_PROGRAM_ID, 
+  );
+  return ata;
+}
+
+function getPredictionMarketAddress(marketId) {
+  const [predictionState] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from('pred_market_state'),
+      new BN(marketId.toString()).toBuffer('le', 8),
+      globalState.toBuffer(),
+    ],
+    programId,
+  );
+  return predictionState;
+}
 
 async function tvl(api) {
-  const response = await axios.get('https://backend.prophet.fun/business-metrics/tvl?timestamp=' + api.timestamp);
-  const data = response.data;
+  const lastRoundId = await axios.get("https://backend.prophet.fun/business-metrics/last-round-id")
+  const tokenAccounts = []
 
-  api.add('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v', data);
+  Array.from({ length: lastRoundId.data.roundId }, (_, i) => i + 1).forEach((roundId) => {
+    const marketAddress = getPredictionMarketAddress(roundId)
+    const marketAta = getAta(marketAddress)
+    tokenAccounts.push(marketAta)
+  })
+  await sumTokens2({ tokenAccounts, api, allowError: true })
 }
 
 
