2cb26f364f749fabd3c29455aff3165784e4e6ff
Your Name <sd0w@protonmail.com>
2025-07-29 15:47:29 +0100

    fix

--- projects/ramses-hl-cl/index.js
+++
@@ -1,5 +0,0 @@
-const { uniV3Export } = require('../helper/uniswapV3')
-
-module.exports = uniV3Export({
-  hyperliquid: { factory: '0xA87c8308722237F6442Ef4762B7287afB84fB191', fromBlock: 6947040, },
-})
