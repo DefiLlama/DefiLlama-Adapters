20bb7ca1d7f3dfbf05a848ab4c5d52b19e040ba4
dave-indx <87349828+mav-brewlabs@users.noreply.github.com>
2025-07-30 21:28:51 +1000

    Update index.js
    added additional v3 contract

--- projects/indx/index.js
+++ projects/indx/index.js
@@ -62,7 +62,9 @@ const V3_ADDRESSES = [
   "0x871fbd1d0cbd80917d4f6fc5b1353d45b3ba68e4",
   "0xd860dcd2f8b31dff84030b36c3160295a0d9cb39",
   "0xbec4f4b9913f1e4e2fb37597cc61f393530502fd",
-  "0x3d169a9ceda1ba8cbedbbbd5a8abf976aa5a0f45"
+  "0x3d169a9ceda1ba8cbedbbbd5a8abf976aa5a0f45",
+  "0xfa064ba05fef454a74c39c2dfbe13f4728b480f7"
+
 ];
 
 async function tvl(api) {
