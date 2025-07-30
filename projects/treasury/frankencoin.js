9d8e33a2f0541c10a93d1409b7598bbeecdae416
samclassix <96245183+samclassix@users.noreply.github.com>
2025-07-30 13:07:07 +0200

    removed duplicated asset token

--- projects/treasury/frankencoin.js
+++ projects/treasury/frankencoin.js
@@ -1,12 +1,11 @@
 const { treasuryExports } = require('../helper/treasury');
 
-const equityContract = '0x1bA26788dfDe592fec8bcB0Eaff472a42BE341B2'; // Equity contract address from ChainAddressMap.ethereum.equity
+const equity = '0x1bA26788dfDe592fec8bcB0Eaff472a42BE341B2'; // Equity contract address from ChainAddressMap.ethereum.equity
 const frankencoin = '0xB58E61C3098d85632Df34EecfB899A1Ed80921cB'; // Frankencoin token address from ChainAddressMap.ethereum.frankencoin
 
 module.exports = treasuryExports({
   ethereum: {
-    tokens: [frankencoin],
-    owners: [equityContract],
+    owners: [equity],
     ownTokens: [frankencoin],
   },
 });
