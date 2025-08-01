97db37007db09b5f2f0ce32cc43400838fc75203
SergeevDmitry <sergeevdms@gmail.com>
2025-08-01 15:08:56 +0200

    Updated methodology texts

--- projects/stratis-masternode-liquid-staking/index.js
+++ projects/stratis-masternode-liquid-staking/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  methodology: "Tvl data that staked on Stratis Masternode Liquid staking protocol by community.",
+  methodology: "TVL represents the total amount of STRAX tokens locked by STRAX Token Holders in the Stratis Liquid Staking Protocol.",
   stratis: {
     tvl: async (api) => {
       const totalSupply = await api.call({ target: '0x81c3fd1c1aD16e31bf75F4aF25a2f9390608C609', abi: 'uint256:totalSupply' })
--- projects/stratis-masternode-staking/index.js
+++ projects/stratis-masternode-staking/index.js
@@ -1,7 +1,7 @@
 const abi = "function totalCollateralAmount() view returns (uint256)"
 
 module.exports = {
-  methodology: "Tvl data that staked on Stratis Masternode staking contract by community.",
+  methodology: "TVL represents the total amount of STRAX tokens locked by STRAX Token Holders in the Stratis Masternode Staking contract.",
   stratis: {
     tvl: async (api) => {
       const totalCollateralAmount = await api.call({ target: '0x0000000000000000000000000000000000001002', abi })
