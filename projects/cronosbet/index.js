db85c08472795e47c0b30e7340cae50a5f6eba2f
pierre48su <pierre48su@gmail.com>
2025-08-03 01:25:33 +0100

    Edit CronosBet TVL adapter

--- projects/cronosbet/index.js
+++ projects/cronosbet/index.js
@@ -6,27 +6,46 @@
  *   as the central treasury for all games
  */
 
-const GAME_POOL_PROXY = '0x9Cf9Bb2526e23783002bd5d774987268c2Ddc115'
+const GAME_POOL = '0xdF697B906AE26a5dB263517c3d1CAf52d19bD8Ac'
 
 async function tvl(api) {
-  const tokens = await api.call({
-    target: GAME_POOL_PROXY,
-    abi: 'function getSupportedTokens() view returns (address[])',
-  })
+    // Get supported ERC-20 tokens from the tokenList array
+    const tokens = await api.fetchList({
+        lengthAbi: 'getSupportedTokensLength',
+        itemAbi: 'function tokenList(uint256) view returns (address)',
+        target: GAME_POOL,
+    })
 
-  return api.sumTokens({
-    owner: GAME_POOL_PROXY,
-    tokens: tokens,
-    nativeToken: true,
-  })
+    // Get native CRO balance from contract
+    const nativeBalance = await api.call({
+        target: GAME_POOL,
+        abi: 'function getPoolBalance() view returns (uint256)'
+    })
+
+    // Add native CRO balance
+    api.add('0x0000000000000000000000000000000000000000', nativeBalance)
+
+    // Add supported token balances
+    for (const token of tokens) {
+        const balance = await api.call({
+            target: token,
+            abi: 'erc20:balanceOf',
+            params: [GAME_POOL]
+        })
+        if (balance > 0) {
+            api.add(token, balance)
+        }
+    }
+
+    return api.getBalances()
 }
 
 module.exports = {
-  cronos: {
-    tvl,
-  },
-  methodology:
-    'TVL is calculated by summing the balances of all supported native CRO and ERC-20 tokens held by the GamePoolUpgradeable contract, which acts as the central treasury for all CronosBet games.',
-  start: '2025-06-28',
-  timetravel: true,
+    cronos: {
+        tvl,
+    },
+    methodology:
+        'TVL is calculated by summing the balances of native CRO and all supported ERC-20 tokens (tracked in the tokenList array) held by the GamePool contract, which acts as the central treasury for all CronosBet games.',
+    start: '2025-06-28',
+    timetravel: true,
 }
