7bda9194611e432ffa1b99ca95c43489852f3dec
Bank Tha Don <warpfinanceeth@gmail.com>
2025-08-03 22:57:51 -0700

    Add index

---
+++ projects/warpfinance/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  ethereum: { factory: '0x312a6be6bD9850D6a2F793b8425c39eFC01C3585', fromBlock: 'block when factory contract was deployed' },
+})
