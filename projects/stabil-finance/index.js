6208d54e35db0006f57ed52c8e67f3d71ed11568
schism595 <schism.fairest33@icloud.com>
2025-08-05 22:02:20 +0200

    Set tvl

--- projects/stabil-finance/index.js
+++ projects/stabil-finance/index.js
@@ -1,6 +1,9 @@
 module.exports = {
   start: 1639350000,
   cronos: {
-    tvl: () => ({}),
+    tvl: getUniTVL({
+      factory: "0x5d29Dc483D4D0A709DBD9EBb5f3acd41c131B472",
+      useDefaultCoreAssets: true,
+    }),
   }
 };
