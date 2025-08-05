7d7273670dea06da08be101e777ed5d3c1c06583
samgaib <sam.huang@gaib.ai>
2025-08-05 14:39:36 +0200

    refactor: remove sei and sty support

--- projects/gaib/index.js
+++ projects/gaib/index.js
@@ -33,22 +33,6 @@ const mainnetContracts = {
             poolToken: '0xd5255Cc08EBAf6D54ac9448822a18d8A3da29A42' // AIDollarAlphaUSDC Pool
         }
     ],
-    sei: [
-        {
-            token: '0x3894085Ef7Ff0f0aeDf52E2A2704928d1Ec074F1', // USDC
-            poolToken: '0xd5255Cc08EBAf6D54ac9448822a18d8A3da29A42' // AIDollarAlphaUSDC Pool
-        },
-        {
-            token: '0x9151434b16b9763660705744891fA906F660EcC5', // USDT
-            poolToken: '0xDc45e7027A0489FE6C2E4A0735097d8E6952A340' // AIDollarAlphaUSDT Pool
-        }
-    ],
-    sty: [
-        {
-            token: '0xF1815bd50389c46847f0Bda824eC8da914045D14', // USDC
-            poolToken: '0xd5255Cc08EBAf6D54ac9448822a18d8A3da29A42' // AIDollarAlphaUSDC Pool
-        }
-    ]
 };
 
 const totalAssetsABI = {
@@ -93,7 +77,7 @@ async function tvl(api) {
 }
 
 module.exports = {
-    methodology: 'Counts the total underlying assets (e.g., USDC, USDT, USR, CUSDO, PUSD) reported by GAIB protocol pool contracts using their `totalAssets()` function across supported mainnet chains.',
+    methodology: 'Counts the total underlying assets (e.g., USDC, USDT, USR, CUSDO) reported by GAIB protocol pool contracts using their `totalAssets()` function across supported mainnet chains.',
     start: 1715490671, 
     timetravel: true, 
     misrepresentedTokens: false, 
@@ -105,12 +89,6 @@ module.exports = {
         tvl,
     },
     base: {
-        tvl,
-    },
-    sei: {
-        tvl,
-    },
-    sty: {
         tvl,
     },
 };
