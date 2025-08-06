c79996f8b46b3741fe815681d99a2c5159ccaa6a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-06 15:39:35 +0200

    code refactor

--- projects/apollon/index.js
+++ projects/apollon/index.js
@@ -1,148 +1,29 @@
-const storagePoolAbi = [{
-  "constant": true,
-  "inputs": [],
-  "name": "checkRecoveryMode",
-  "outputs": [
-    { "name": "isInRecoveryMode", "type": "bool" },
-    { "name": "TCR",               "type": "uint256" },
-    { "name": "entireSystemColl",  "type": "uint256" },
-    { "name": "entireSystemDebt",  "type": "uint256" }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-}]
-
-const swapOperationsAbi = {
-  allPairsLength: {
-    "inputs": [],
-    "name": "allPairsLength",
-    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  allPairs: {
-    "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "name": "allPairs",
-    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "stateMutability": "view",
-    "type": "function"
-  }
+const config = {
+  sei: { troveManager: '0x097639bD7AD4e665f7bac198CF82914e60e604e2', swapOperations: '0xf69D9cACc0140e699C6b545d166C973CB59b8E87' }
 }
 
-const pairAbi = {
-  token0: {
-    "inputs": [],
-    "name": "token0",
-    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  token1: {
-    "inputs": [],
-    "name": "token1",
-    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  getReserves: {
-    "inputs": [],
-    "name": "getReserves",
-    "outputs": [
-      { "internalType": "uint112", "name": "reserve0", "type": "uint112" },
-      { "internalType": "uint112", "name": "reserve1", "type": "uint112" },
-      { "internalType": "uint32",  "name": "blockTimestampLast", "type": "uint32" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
+Object.keys(config).forEach(chain => {
+  const { troveManager, swapOperations } = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const tokenManager = await api.call({ abi: 'address:tokenManager', target: troveManager })
+      const storagePool = await api.call({ abi: 'address:storagePool', target: troveManager })
+      const tokens = await api.call({ abi: 'address[]:getCollTokenAddresses', target: tokenManager })
+      const debtTokens = await api.call({ abi: 'address[]:getDebtTokenAddresses', target: tokenManager })
+      const blacklistedTokens = debtTokens.concat([ // exclude minted tokens whose backing is already counted towards tvl
+        '0x5b8203e65aa5be3f1cf53fd7fa21b91ba4038ecc', // APO - projects own token +  more locked here than mcap on cg + very low liquidity
+        '0x372b2dc06478aa2c8182eee0f12ea0e9a15e2913', // GEM - more locked here than mcap on cg + very low liquidity
+      ])
+
+      const pairs = await api.fetchList({ lengthAbi: 'allPairsLength', itemAbi: 'allPairs', target: swapOperations })
+      const tokens0 = await api.multiCall({ abi: 'address:token0', calls: pairs })
+      const tokens1 = await api.multiCall({ abi: 'address:token1', calls: pairs })
+      await api.sumTokens({ tokensAndOwners2: [tokens0.concat(tokens1), pairs.concat(pairs)], blacklistedTokens })
+
+      return api.sumTokens({ owner: storagePool, tokens, blacklistedTokens })
+    }
   }
-}
+})
 
-const priceFeedAbi = {
-  getUSDValue: {
-    "inputs": [
-      { "internalType": "address", "name": "_tokenAddress", "type": "address" },
-      { "internalType": "uint256", "name": "_amount", "type": "uint256" }
-    ],
-    "name": "getUSDValue",
-    "outputs": [{ "internalType": "uint256", "name": "usdValue", "type": "uint256" }],
-    "stateMutability": "view",
-    "type": "function"
-  }
-}
 
-// -------------------- contract addresses --------------------
-const STORAGE_POOL_ADDRESS   = '0x20eD855d2c4e61F148cf90F4C5Ff84CFBCB5D99F';
-const SWAP_OPERATIONS_ADDRESS = '0xf69D9cACc0140e699C6b545d166C973CB59b8E87';
-const PRICE_FEED_ADDRESS      = '0xeeE9Bfdb368d22Bd0560d4713F6B585Ab1172b3C';
-const DEPLOY_BLOCK            = 133986313;
-
-module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  start: DEPLOY_BLOCK,
-
-  sei: {
-    tvl,
-  },
-};
-
-/**
- * TVL = System collateral (from StoragePool) + value of all reserves inside every SwapPair.
- */
-async function tvl(api) {
-  // ---------------------------------------------------------------------------
-  // 1. System-wide collateral from the StoragePool
-  // ---------------------------------------------------------------------------
-  const storageRes = await api.call({ abi: storagePoolAbi[0], target: STORAGE_POOL_ADDRESS })
-  const systemCollUSD = BigInt(storageRes.entireSystemColl)   // 1e18 already USD-denominated
-
-  // ---------------------------------------------------------------------------
-  // 2. Gather liquidity-pool reserves
-  // ---------------------------------------------------------------------------
-  // 2.1 how many pools?
-  const poolCount = await api.call({ abi: swapOperationsAbi.allPairsLength, target: SWAP_OPERATIONS_ADDRESS })
-  const idxArray  = [...Array(Number(poolCount)).keys()]
-
-  // 2.2 fetch all pair addresses
-  const pairAddresses = await api.multiCall({
-    abi: swapOperationsAbi.allPairs,
-    calls: idxArray.map(i => ({ target: SWAP_OPERATIONS_ADDRESS, params: [i] }))
-  })
-
-  // 2.3 fetch token0, token1 & reserves for every pair in parallel
-  const [tokens0, tokens1, reserves] = await Promise.all([
-    api.multiCall({ abi: pairAbi.token0, calls: pairAddresses }),
-    api.multiCall({ abi: pairAbi.token1, calls: pairAddresses }),
-    api.multiCall({ abi: pairAbi.getReserves, calls: pairAddresses }),
-  ])
-
-  // accumulate amounts per token
-  const tokenBalances = {}
-  for (let i = 0; i < pairAddresses.length; i++) {
-    const [reserve0, reserve1] = reserves[i]
-    const t0 = tokens0[i]
-    const t1 = tokens1[i]
-
-    tokenBalances[t0] = (tokenBalances[t0] || 0n) + BigInt(reserve0)
-    tokenBalances[t1] = (tokenBalances[t1] || 0n) + BigInt(reserve1)
-  }
-
-  // ---------------------------------------------------------------------------
-  // 3. Convert every token amount to USD via on-chain PriceFeed
-  // ---------------------------------------------------------------------------
-  const tokenList = Object.keys(tokenBalances)
-  const usdPerToken = await api.multiCall({
-    abi: priceFeedAbi.getUSDValue,
-    calls: tokenList.map(t => ({ target: PRICE_FEED_ADDRESS, params: [t, tokenBalances[t].toString()] }))
-  })
-
-  let poolsUSD = 0n
-  usdPerToken.forEach(val => { poolsUSD += BigInt(val) })
-
-  // ---------------------------------------------------------------------------
-  // 4. Total TVL in USD (1e18 precision).  Return as `usd-coin` so DL interprets
-  //    it as already-priced USD.
-  // ---------------------------------------------------------------------------
-  const totalUSD = systemCollUSD + poolsUSD
-  return { 'usd-coin': Number(totalUSD) / 1e18 }
-}
+module.exports.methodology = 'Value of tokens backing the minted synth tokens, project related tokens are excluded from the tvl'
