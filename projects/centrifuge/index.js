f7cee9d9bbbbc6c7f9e374517f15bb14625c0f4f
0xpeluche <0xpeluche@proton.me>
2025-08-06 09:34:43 +0200

    Fix: Centrifuge

--- projects/centrifuge/index.js
+++ projects/centrifuge/index.js
@@ -82,8 +82,8 @@ const tvl = async (api) => {
   const { factories, assets: { USDC } } = CONFIG[chain]
   const tokens = await getTokens(api, block, factories)
   if (!tokens) return;
-  const vaults = (await api.multiCall({ calls: tokens.map((t) => ({ target: t, params: [USDC] })), abi: abis.getVault })).filter(addr => addr.toLowerCase() !== nullAddress)
-  await api.erc4626Sum({ calls: vaults, tokenAbi: 'address:asset', balanceAbi: 'uint256:totalAssets' })
+  const vaults = (await api.multiCall({ calls: tokens.map((t) => ({ target: t, params: [USDC] })), abi: abis.getVault, permitFailure: true })).filter(addr => addr.toLowerCase() !== nullAddress)
+  await api.erc4626Sum({ calls: vaults, tokenAbi: 'address:asset', balanceAbi: 'uint256:totalAssets', permitFailure: true })
 }
 
 module.exports.methodology = `TVL corresponds to the total USD value of tokens minted on Centrifuge across Ethereum, Base, and Arbitrum.`
