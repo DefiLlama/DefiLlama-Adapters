1422fd16a44e65052d28dba8d11457e9620b238a
Chang Coin Foundation <changcoinfoundation@gmail.com>
2025-08-07 16:21:06 +0800

    Update index.js

--- projects/changStake/index.js
+++ projects/changStake/index.js
@@ -4,8 +4,7 @@ const { staking } = require('../helper/unknownTokens')
 module.exports = {
   misrepresentedTokens: true,
   changchain: {
-    tvl: () => ({}),
-    staking: staking({
+    tvl: staking({
       owner: '0x0e6cF7C13aB088fBb216Db1DD768E622f838a382', 
       tokens: ['0x0000000000000000000000000000000000000000'], 
       chain: 'changchain'
