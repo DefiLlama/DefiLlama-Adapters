abffe9676506413ec26711e045be5663b78a3946
NeutralTrade <tradeneutral@gmail.com>
2025-08-08 16:18:22 +0800

    rm try catch

--- projects/neutral-trade/utils/hyperliquid.js
+++ projects/neutral-trade/utils/hyperliquid.js
@@ -1,7 +1,6 @@
 const { post } = require('../../helper/http');
 
 async function getTvl(vaultAddress) {
-  try {
     const data = await post(
       "https://api.hyperliquid.xyz/info",
       {
@@ -17,13 +16,9 @@ async function getTvl(vaultAddress) {
     const accountValueHistory = data.portfolio[0][1].accountValueHistory;
     const dp = 1e6;
     return Number(accountValueHistory[accountValueHistory.length - 1][1]) * dp;
-  } catch (error) {
-    console.error("Error in getTvl:", error.message);
-    console.error("Full error:", error);
-    return 0;
-  }
 }
 
 module.exports = {
   getTvl
 };
+
