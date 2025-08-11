4d9b839dc0469b6aae171eb075287f55d3c1eaca
Joshua J. Bouw <dev@joshuajbouw.com>
2025-08-11 18:11:54 +0400

    fix: update start time to v1 contract start time

--- projects/templarfi/index.js
+++ projects/templarfi/index.js
@@ -396,7 +396,7 @@ async function tvl() {
 
 module.exports = {
     methodology: 'TVL is calculated by summing the net borrow asset liquidity (deposits minus outstanding loans) and full collateral deposits for each market deployment.',
-    start: 1747976677, // Fri May 23 2025 05:04:37 TODO: update this to the actual start date of Templar Protocol post-alpha launch
+    start: 1754902109, // August 11, 2025 08:48:29+UTC
     near: {
         tvl,
     },
