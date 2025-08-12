c4fcd8dbe5bf6c6a62fd00d603f44342cf316566
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-12 10:19:04 +0200

    feat: add flare NFT address and update token configuration in flrbank

--- projects/treasury/flrbank.js
+++ projects/treasury/flrbank.js
@@ -5,7 +5,10 @@ module.exports = {
   flare: {
     tvl: sumTokensExport({
       owners: ['0xaA68BC4BAb9a63958466f49f5a58c54A412D4906'],
-      tokens: [ADDRESSES.flare.WFLR, ]
+      tokens: [ADDRESSES.flare.WFLR, '0x0F574Fc895c1abF82AefF334fA9d8bA43F866111' ],
+      resolveLP: true,
+      blacklistedTokens: ['0x194726f6c2ae988f1ab5e1c943c17e591a6f6059'],
+      resolveUniV3: true,
     })
   },
 }
