88bf109c82b073c3fa7d464e451356b7568aac94
Bank Tha Don <warpfinanceeth@gmail.com>
2025-08-11 21:26:31 -0700

    update

--- projects/warpfinance/index.js
+++ projects/warpfinance/index.js
@@ -1,5 +1,21 @@
-const { uniV3Export } = require('../helper/uniswapV3')
+const ADDRESSES = require('../helper/coreAssets.json')
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
-module.exports = uniV3Export({
-  ethereum: { factory: '0x86A74536f7C5548EFE9e935863eA7bE333380288', fromBlock: 'block when factory contract was deployed' },
+const config = {
+  ethereum: {
+    contract: '0x86A74536f7C5548EFE9e935863eA7bE333380288', whitelistedTokens: [ADDRESSES.ethereum.WETH]
+  }
+}
+
+Object.keys(config).forEach(chain => {
+  const {contract, whitelistedTokens,} = config[chain]
+  module.exports[chain] = {
+    tvl: async (api) => {
+      return sumTokens2({ api, owner: contract, uniV3WhitelistedTokens: whitelistedTokens, resolveUniV3: true, })
+    }
+  }
 })
+
+module.exports.methodology = 'Count the value of base tokens in the locked uni-v3 nfts'
+module.exports.doublecounted = true
+main
