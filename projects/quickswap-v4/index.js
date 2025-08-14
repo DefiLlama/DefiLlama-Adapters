da41ba82486f67423265bb1572636c882c6fdecf
Henry Palacios <henrydpalacios@gmail.com>
2025-08-13 21:54:11 -0300

    Adding TVL for Quickswap v4 (Algebra Integral) on Base

---
+++ projects/quickswap-v4/index.js
@@ -0,0 +1,9 @@
+import { uniV3Export } from '../helper/uniswapV3'
+
+export default uniV3Export({
+  base: {
+    factory: '0xC5396866754799B9720125B104AE01d935Ab9C7b',
+    isAlgebra: true,
+    fromBlock: 30736835,
+  }
+})
