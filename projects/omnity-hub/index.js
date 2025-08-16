02bea71dc2334ebcfb27f3071e860ddd1320c498
chezhe <chezhe@hey.com>
2025-08-16 11:04:56 +0800

    update omnity hub endpoint

--- projects/omnity-hub/index.js
+++ projects/omnity-hub/index.js
@@ -1,18 +1,28 @@
+import { BigNumber } from "bignumber.js";
 const { get } = require("../helper/http");
 
 module.exports = {
-  methodology: `TVL data is pulled from the our indexer and calculated, our own explorer can be found here https://explorer.omnity.network/`,
+  methodology: `TVL data is pulled from the our canister`,
   misrepresentedTokens: true,
-  icp: { tvl: icpTVL },
-  osmosis: { tvl: osmosisTVL },
+  bitcoin: { tvl },
 };
 
-async function icpTVL(api) {
-  let result = await get("https://api.omnity.network/api/tvl");
-  return api.addUSDValue(result.icp.tvl ?? 0)
+async function tvl(api) {
+  const runes = await get("https://7rvjr-3qaaa-aaaar-qaeyq-cai.raw.icp0.io/locked_runes");
+
+  // source code: https://github.com/octopus-network/api.omnity.network/blob/main/src/app/api/price/tokens/route.ts
+  const prices = await get("https://api.omnity.network/api/price/tokens")
+
+  const tvl = runes.reduce(
+    (
+      acc,
+      rune
+    ) => {
+      const amount = new BigNumber(rune.amount).div(10 ** rune.decimals)
+      return acc.plus(amount.times(prices[rune.token_id] ?? 0))
+    },
+    new BigNumber(0)
+  )
+  return api.addUSDValue(tvl.toNumber())
 }
 
-async function osmosisTVL(api) {
-  let result = await get("https://api.omnity.network/api/tvl");
-  return api.addUSDValue(result.osmosis.tvl ?? 0)
-}
