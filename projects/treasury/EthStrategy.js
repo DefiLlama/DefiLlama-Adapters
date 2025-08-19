49af3299a050c46f3aa4d686b1d433ed8eadd6fe
beanintern <100470716+beanintern@users.noreply.github.com>
2025-08-19 03:38:12 -0300

    Update EthStrategy.js

--- projects/treasury/EthStrategy.js
+++ projects/treasury/EthStrategy.js
@@ -10,12 +10,6 @@ const OWNERS = [
 module.exports = {
   ethereum: {
     tvl: async (api) => {
-      const OWNERS = [
-        "0xC53CCed6332D06972A7eaEDc64FDF6d4aF5220b8", // ETH Strategy Main
-        "0x75eFa088E34DA03966a5D2b84fA16C77fF25Adfa", // Puttable Warrant (4200 ETH)
-        "0xF89f49e21A2Bd1fb24332462cB21dc1378aA25e1", // Staking Multisig
-      ];
-
       const treasuryModule = treasuryExports({
         ethereum: {
           owners: OWNERS,
@@ -23,8 +17,7 @@ module.exports = {
           ownTokens: ["0x14cF922aa1512Adfc34409b63e18D391e4a86A2f"],         // no protocol token to separate
         },
       });
-
-      }
+      return treasuryModule.ethereum.tvl(api);
     }
   }
-;
+};
