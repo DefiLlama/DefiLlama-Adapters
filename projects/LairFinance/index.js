999c5cb09f9e830d4b27b87104a536e8a059c762
0xyongyong <yongyong@bugcity.io>
2025-08-19 08:23:15 +0000

    add berachain restake

--- projects/LairFinance/index.js
+++ projects/LairFinance/index.js
@@ -1,21 +1,79 @@
 const contracts = {
-  stKAIA: '0x42952B873ed6f7f0A7E4992E2a9818E3A9001995',
-  rstKAIA: '0xd05fCB2d4c427232C3b36a33E21a4F23810A298F',
-  LAIR: '0xD70C7D511560493C79DF607076fB863f5c8A50b0',
-  nodeController: '0x7949597f453592B782EC9036Af27d63Ed9774b2d',
-  restakingManager: '0x66611Ba2aa5deB46e6138aD21a202b40ecE5b6AB'
+  KAIA : {
+    stKAIA: '0x42952B873ed6f7f0A7E4992E2a9818E3A9001995',
+    rstKAIA: '0xd05fCB2d4c427232C3b36a33E21a4F23810A298F',
+    LAIR: '0xD70C7D511560493C79DF607076fB863f5c8A50b0',
+    nodeController: '0x7949597f453592B782EC9036Af27d63Ed9774b2d',
+    restakingManager: '0x66611Ba2aa5deB46e6138aD21a202b40ecE5b6AB',
+  },
+  BERA: {
+    WBERA: '0x6969696969696969696969696969696969696969',
+    LAIR: '0xf3530788DEB3d21E8fA2c3CBBF93317FB38a0D3C',
+    LrBGT: '0x66611Ba2aa5deB46e6138aD21a202b40ecE5b6AB',
+    iBGT: '0xac03caba51e17c86c921e1f6cbfbdc91f8bb2e6b',
+    lairBGTManager: '0x381E9DC031D2667BB61d4Ab61e64D520Bbd7bFfC',
+    lairBGTManagerHelper: '0xfc3Da0822fE3127334B95Ea3661060B957379c82',
+    infraredIBGTVault: '0x75F3Be06b02E235f6d0E7EF2D462b29739168301',
+    infraredWBERALAIRVault: '0x6583e71778A3d275B8A27f1252A125f7a6F875D1',
+    kodiakBERALAIRLp: '0x9f6cf7aCb2F16f7d906EeeCB0a6020a5Cf91A41d'
+  }
 }
 
 async function kaia_tvl(api) {
-  const tvl = await api.call({ target: contracts.nodeController, abi: "uint256:getTotalClaimable" })
-  const rstKAIATotalSupply = await api.call({ target: contracts.rstKAIA, abi: "erc20:totalSupply" })
-  const lairPairRate = await api.call({ target: contracts.restakingManager, abi: "uint256:pairRate" })
+  const tvl = await api.call({ target: contracts.KAIA.nodeController, abi: "uint256:getTotalClaimable" })
+  const rstKAIATotalSupply = await api.call({ target: contracts.KAIA.rstKAIA, abi: "erc20:totalSupply" })
+  const lairPairRate = await api.call({ target: contracts.KAIA.restakingManager, abi: "uint256:pairRate" })
   const restakedLairAmount = rstKAIATotalSupply * lairPairRate / (10 ** 18)
 
   api.addGasToken(tvl)
-  api.add(contracts.LAIR, restakedLairAmount)
+  api.add(contracts.KAIA.LAIR, restakedLairAmount)
+}
+
+async function bera_tvl(api) {
+  const lrBGTTotalSupply = await api.call({ target: contracts.BERA.LrBGT, abi: "erc20:totalSupply" })
+  const tokenContracts = [
+    {
+      token: contracts.BERA.LrBGT,
+      manager: contracts.BERA.lairBGTManager,
+      helper: contracts.BERA.lairBGTManagerHelper,
+      bgtVault: contracts.BERA.infraredIBGTVault,
+      lpVault: contracts.BERA.infraredWBERALAIRVault,
+      bgtToken: contracts.BERA.iBGT,
+      lpToken: contracts.BERA.kodiakBERALAIRLp,
+      token0: contracts.BERA.WBERA,
+      token1: contracts.BERA.LAIR,
+    }
+  ]
+
+  for (const item of tokenContracts) {
+    const [bgtAmount, lpAmount] = await api.call({
+      target: contracts.BERA.lairBGTManagerHelper,
+      abi: "function predictedUnStakeAmount(address,address,address,address,address,address,address,address,uint256) view returns (uint256,uint256)",
+      params: [
+        item.manager,
+        item.token,
+        item.bgtVault,
+        item.lpVault,
+        item.bgtToken,
+        item.lpToken,
+        item.token0,
+        item.token1,
+        lrBGTTotalSupply,
+      ]
+    })
+    const [token0Amount, token1Amount] = await api.call({
+      target: contracts.BERA.lairBGTManagerHelper,
+      abi: "function lpPairTokenAmount(address,uint256) view returns (uint256,uint256)",
+      params: [item.lpToken, lpAmount]
+    })
+
+    api.add(item.bgtToken, bgtAmount)
+    api.add(item.token0, token0Amount)
+    api.add(item.token1, token1Amount)
+  }
 }
 
 module.exports = {
-	klaytn: { tvl: kaia_tvl }
+	klaytn: { tvl: kaia_tvl },
+  berachain: { tvl: bera_tvl }
 }
