a39ebf216f7836d1afb9d305ea413a3715da06a3
Jun Tsai <jcai@ganshane.com>
2025-08-20 22:16:22 +0800

    query blockchain data

--- projects/CatFee/index.js
+++ projects/CatFee/index.js
@@ -1,21 +1,69 @@
-const {
-	fetchURL
-} = require('../helper/utils');
+const ADDRESSES = require("../helper/coreAssets.json");
+const { post } = require('../helper/http');
 
-async function getCurrentStake() {
-	let result = await fetchURL('https://catfee.io/api/stake/public/project/stat'); 
-	let trx = parseInt(result.data.data.total_staked_amount_sun)/ 1000_000;
-	return trx;
+const { getEnv } = require('../helper/env')
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
+
+const addresses = [
+	'TKoffapJgw2E586kp9taJvjqdfpiKrWKWG',
+	'TSmrfRFesHf9of94mNKLgrunbFzrPYGiPZ',
+	'TYEMUMKBmkTRMa1CNh7xkNYZ2mTVFw9f1a',
+	'TXtt43tfe6SUL8HU6KNutXNm4Ybuj1Ro25',
+	'TXtt43tfe6SUL8HU6KNutXNm4Ybuj1Ro25',
+	'TGinDyFgTLYcnoJtFatxictWB1dfN6no26'
+]
+
+async function callApi(api, body) {
+	return await post(getEnv('TRON_RPC') + api, body)
 }
-
-async function tvl() {
-	return {
-		"tron": await getCurrentStake()
+async function getDelegatedAmountSun(from, to) {
+	const data = await callApi('/wallet/getdelegatedresourcev2', {
+		fromAddress: from,
+		toAddress: to,
+		visible: true
+	})
+	const list = data.delegatedResource || [];
+	let bw = 0, en = 0;
+	for (const it of list) {
+		bw += Number(it.frozen_balance_for_bandwidth || 0);
+		en += Number(it.frozen_balance_for_energy || 0);
 	}
+	return bw + en
 }
+
+async function sumOneAddress(to) {
+	const response = await callApi('/wallet/getdelegatedresourceaccountindexv2', {
+		value: to,
+		visible: true
+	})
+	const perRequestDelayMs = 500
+	// console.log(response)
+	let totalSun = 0;
+	for (const from of response.fromAccounts) {
+		const a = await getDelegatedAmountSun(from, to);
+		// console.log("sum from to ",from,to,a)
+		totalSun += a || 0;
+		if (perRequestDelayMs > 0) await sleep(perRequestDelayMs);
+	}
+	// console.log("totalSun:",totalSun)
+	return totalSun
+}
+async function getStakedTron() {
+	let total = 0
+	for (const address of addresses) {
+		total += await sumOneAddress(address)
+	}
+	return total / 1000_000
+}
+
 
 module.exports = {
 	tron: {
-		tvl,
+		tvl: async () => {
+			return {
+				tron: await getStakedTron(),
+			};
+		},
 	},
-}
+	timetravel: false,
+};
