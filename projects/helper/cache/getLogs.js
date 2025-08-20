dcac74c46dcc7bc227cb1186c198df8a23c96e80
Eden <noat.eth@gmail.com>
2025-08-20 22:13:07 +0700

    remove tuple from topic sig

--- projects/helper/cache/getLogs.js
+++ projects/helper/cache/getLogs.js
@@ -64,6 +64,10 @@ async function getLogs({ target,
     cache.fromBlock = fromBlock
     fromBlock = cache.toBlock ?? fromBlock
 
+    // remove tuple baseType from type
+    // ex: CreateMarket(bytes32,tuple(address,address,address,address,uint256)) -> CreateMarket(bytes32,(address,address,address,address,uint256))
+    const fragment = iface.fragments[0]
+    topic = `${fragment.name}(${fragment.inputs.map(i => i.baseType === 'tuple' ? i.type.replace('tuple', '') : i.type).join(',')})`
     let logs = (await sdk.api.util.getLogs({
       chain, target, topic, keys, topics, fromBlock, toBlock,
     })).output
