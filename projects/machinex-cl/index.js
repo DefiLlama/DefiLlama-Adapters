471a74c985018fcc894dbb3133b512394aca8f71
0xshad0w <sd0w@protonmail.com>
2025-08-22 09:13:35 +0100

    .

--- projects/machinex-cl/index.js
+++ projects/machinex-cl/index.js
@@ -1,5 +1,5 @@
 const { uniV3Export } = require('../helper/uniswapV3')
 
 module.exports = uniV3Export({
-  peaq: { factory: '0x2646DCbE025D21A2925fdaCeB639e998E17d6060', fromBlock: 5247765, },
+  peaq: { factory: '0x2646DCbE025D21A2925fdaCeB639e998E17d6060', fromBlock: 6047765, },
 })
