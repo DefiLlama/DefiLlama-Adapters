8bbaf2ba6fa8924dad79752d0362a335754b2a3d
John Agbanusi <agbanusijohn@gmail.com>
2025-08-22 10:03:57 +0100

    removing staking tvl from main index

--- projects/mystic-finance/index.js
+++ projects/mystic-finance/index.js
@@ -1,5 +1,4 @@
 const { getMorphoVaultTvl } = require('../helper/morpoho');
-const ADDRESSES = require('../helper/coreAssets.json')
 
 const config = {
   plume_mainnet: {
@@ -8,13 +7,7 @@ const config = {
       '0x0b14D0bdAf647c541d3887c5b1A4bd64068fCDA7',
       '0xBB748a1346820560875CB7a9cD6B46c203230E07'
     ],
-    stakingToken: '0x5c982097b505A3940823a11E6157e9C86aF08987'
   },
-}
-
-async function stakingTvl(api) {
-  const supply = await api.call({ abi: 'uint256:totalSupply', target: config.plume_mainnet.stakingToken, });
-  api.add(ADDRESSES.null, supply)
 }
 
 module.exports = {
@@ -24,6 +17,5 @@ module.exports = {
     vaults: config.plume_mainnet.morphoVaults,
     morphoFactory: "0x2525D453D9BA13921D5aB5D8c12F9202b0e19456",
     }), 
-    staking: stakingTvl
   },
 }
