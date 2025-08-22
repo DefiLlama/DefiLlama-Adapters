fdbdd568bdd5728e3d51f002f1bb6494ed59df50
Przemysław Maranowski <przemek.maranowski@gmail.com>
2025-08-22 13:17:09 +0200

    Add RedStone Oracles adapter

--- projects/redstone-oracles/index.js
+++ projects/redstone-oracles/index.js
@@ -5,8 +5,6 @@ const STRATEGY_MANAGER_ETH = "0x903a1FF023a35EFeD333ee9D6bF30629A098B9ed";
 
 module.exports = {
   timetravel: true,
-  methodology:
-    "Counts RED staked in EigenLayer by reading RED held at the StrategyManager.",
   ethereum: {
     tvl: async () => ({}),
     staking: sumTokensExport({
