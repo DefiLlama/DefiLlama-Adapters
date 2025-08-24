dab77b1170e251419c4f70a6f069ea368d634596
realshaman <realdealshaman11@gmail.com>
2025-08-24 16:45:02 -0600

    fix paycashswap

--- projects/paycashswap/index.js
+++ projects/paycashswap/index.js
@@ -3,20 +3,23 @@ const utils = require("../helper/utils");
 // https://paycashswap.com
 // https://api.paycashswap.com
 async function eos(api) {
-  const data = {
+  // Get total pool liquidity which represents the actual AMM pool TVL
+  const liquidityQuery = {
     operationName: "TotalLiquidity",
     variables: {},
-    query: "query TotalLiquidity { totalLiquidityChart { value24h } token(id:100) {liquidity} }"
+    query: "query TotalLiquidity { totalLiquidityChart { value24h } }"
   }
-  const { data: { data: { totalLiquidityChart: { value24h }, token: { liquidity } } } } = await utils.postURL("https://api.paycashswap.com/", data);
-  // exclude self-issued tokens
-  const tvlValue = Number(value24h) - Number(liquidity)
-  api.addUSDValue(tvlValue > 0 ? tvlValue : 0)
+  
+  const { data: { data: { totalLiquidityChart: { value24h } } } } = await utils.postURL("https://api.paycashswap.com/", liquidityQuery);
+  
+  // Use the pool liquidity value directly
+  const poolTvl = Number(value24h);
+  api.addUSDValue(poolTvl);
 }
 
 module.exports = {
   misrepresentedTokens: true,
-  methodology: `PayCash Swap TVL is achieved by making a call to its PayCash Swap API.`,
+  methodology: `PayCash Swap TVL is achieved by making a call to its PayCash Swap API and using the totalLiquidityChart.value24h which represents the actual AMM pool liquidity.`,
   eos: {
     tvl: eos
   },
