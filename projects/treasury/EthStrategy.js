f92064ae544eccd337efe2041876fe605f6c4fd0
beanintern <100470716+beanintern@users.noreply.github.com>
2025-08-25 07:42:57 -0300

    Update EthStrategy.js (#16058)

--- projects/treasury/EthStrategy.js
+++ projects/treasury/EthStrategy.js
@@ -14,7 +14,7 @@ module.exports = {
       const treasuryModule = treasuryExports({
         ethereum: {
           owners: OWNERS,
-          tokens: [nullAddress, "0xbf5495Efe5DB9ce00f80364C8B423567e58d2110", "0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84", "0x8c1BEd5b9a0928467c9B1341Da1D7BD5e10b6549","0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"], // native ETH only
+          tokens: [nullAddress, "0xbf5495Efe5DB9ce00f80364C8B423567e58d2110", "0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84", "0x8c1BEd5b9a0928467c9B1341Da1D7BD5e10b6549","0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2","0x856c4efb76c1d1ae02e20ceb03a2a6a08b0b8dc3","0x04c154b66cb340f3ae24111cc767e0184ed00cc6"], // native ETH only
           ownTokens: ["0x14cF922aa1512Adfc34409b63e18D391e4a86A2f"],         // no protocol token to separate
         },
       });
