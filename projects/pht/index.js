8843844f6e49fea66edeb98ff93ebaa47fb884d7
sean-apacx <sean@9dfin88.com>
2025-08-25 20:21:16 +0800

    Update index.js

--- projects/pht/index.js
+++ projects/pht/index.js
@@ -1,1 +1,8 @@
-.
+const { onChainTvl } = require('../helper/balancer')
+
+module.exports = {
+  methodology: "Sum of all the PHT tokens locked in TanukiX vault",
+  pht: {
+    tvl: onChainTvl('0xe75220cB014Dfb2D354bb59be26d7458bB8d0706', 72431824)
+  },
+}
