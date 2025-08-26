d5b1adc46b9a5dbb13ddceb3f873eac476442fe2
joshhchun <jchun2@nd.edu>
2025-08-26 10:38:22 -0400

    mainnet thbill total TVL

--- projects/theo-network-thbill/index.js
+++ projects/theo-network-thbill/index.js
@@ -1,38 +1,11 @@
-const { getConfig } = require('../helper/cache')
+// thBill token address
+THBILL_ETH = "0x5FA487BCa6158c64046B2813623e20755091DA0b"
 
-// thBill token addresses
-const THBILL = {
-  ethereum: "0x5FA487BCa6158c64046B2813623e20755091DA0b",
-  arbitrum: "0xfdd22ce6d1f66bc0ec89b20bf16ccb6670f55a5a",
-  base: "0xfdd22ce6d1f66bc0ec89b20bf16ccb6670f55a5a",
-  hyperliquid: "0xfdd22ce6d1f66bc0ec89b20bf16ccb6670f55a5a"
-}
-const ETH_ADAPTER = "0xfdd22ce6d1f66bc0ec89b20bf16ccb6670f55a5a"
-
-const config = {
-  ethereum: {},
-  arbitrum: {},
-  base: {},
-  hyperliquid: {}
-}
-
-Object.keys(config).forEach(chain => {
-  module.exports[chain] = {
+module.exports = {
+  ethereum: {
     tvl: async (api) => {
-      const addr = THBILL[chain]
-
-      // for mainnet, subtract balance of adapter to avoid double counting
-      if (chain === "ethereum") {
-        const [totalSupply, locked] = await Promise.all([
-          api.call({ target: addr, abi: 'erc20:totalSupply' }),
-          api.call({ target: addr, abi: 'erc20:balanceOf', params: [ETH_ADAPTER] }),
-        ])
-        const effective = totalSupply - locked
-        if (effective > 0n) api.add(addr, effective)
-      } else {
-        const supply = await api.call({ target: addr, abi: 'erc20:totalSupply' })
-        api.add(addr, supply)
-      }
-    }
-  }
-})
+      const supply = await api.call({ target: THBILL_ETH, abi: 'erc20:totalSupply' })
+      api.add(THBILL_ETH, supply)
+    },
+  },
+}
