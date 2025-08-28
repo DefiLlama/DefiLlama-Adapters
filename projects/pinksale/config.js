b7466524a56aabe27d3aea7fa762b31e4c31b7c4
Eden <noat.eth@gmail.com>
2025-08-28 15:04:38 +0700

    fix pinksale, morpho blue

--- projects/pinksale/config.js
+++ projects/pinksale/config.js
@@ -18,6 +18,7 @@ module.exports = {
       '0x4aee9d30893c5c73e5a5b8637a10d9537497f1c8',
       '0x9FBff386a9405b4C98329824418ec02b5C20976b',
       '0xa0b3c4f174f4bb51039adaecf2af0e4ef5925f7d',
+      '0xde65be98bfc08a3150a491dfa84df8b02ba6c6ac',
     ],
     log_coreAssetPrices: [
       300/ 1e18,
@@ -27,38 +28,38 @@ module.exports = {
     ],
     log_minTokenValue: 1e6,
   },
-  ethereum: {
-    vaults: [
-      '0x33d4cC8716Beb13F814F538Ad3b2de3b036f5e2A',
-      '0x71B5759d73262FBb223956913ecF4ecC51057641',
-    ],
-    blacklist: [
-      '0xd626661e2d4f93a1c4122d386fa9ea0f62b5ab0b',
-    ],
-  },
-  polygon: {
-    vaults: [
-      '0x5fb71Dbf7248a01bf96cE2AB2DA34EEAbE58c261',
-      '0x6C9A0D8B1c7a95a323d744dE30cf027694710633',
-    ],
-  },
-  avax: {
-    vaults: [
-      '0x4DffB05d1Bc222A2852799e2076e956acb589322',
-      '0x9479C6484a392113bB829A15E7c9E033C9e70D30',
-    ],
-  },
-  fantom: {
-    vaults: [
-      '0xdd6e31a046b828cbbafb939c2a394629aff8bbdc',
-      '0x0E1757b9d6501e60B2e4Ca0D000e49532948CF6c',
-    ],
-  },
-  cronos: {
-    vaults: [
-      '0xdD6E31A046b828CbBAfb939C2a394629aff8BBdC',
-      '0x102137A9F278B013419332f82aCEA429D944Fc34',
-    ],
-  },
+  // ethereum: {
+  //   vaults: [
+  //     '0x33d4cC8716Beb13F814F538Ad3b2de3b036f5e2A',
+  //     '0x71B5759d73262FBb223956913ecF4ecC51057641',
+  //   ],
+  //   blacklist: [
+  //     '0xd626661e2d4f93a1c4122d386fa9ea0f62b5ab0b',
+  //   ],
+  // },
+  // polygon: {
+  //   vaults: [
+  //     '0x5fb71Dbf7248a01bf96cE2AB2DA34EEAbE58c261',
+  //     '0x6C9A0D8B1c7a95a323d744dE30cf027694710633',
+  //   ],
+  // },
+  // avax: {
+  //   vaults: [
+  //     '0x4DffB05d1Bc222A2852799e2076e956acb589322',
+  //     '0x9479C6484a392113bB829A15E7c9E033C9e70D30',
+  //   ],
+  // },
+  // fantom: {
+  //   vaults: [
+  //     '0xdd6e31a046b828cbbafb939c2a394629aff8bbdc',
+  //     '0x0E1757b9d6501e60B2e4Ca0D000e49532948CF6c',
+  //   ],
+  // },
+  // cronos: {
+  //   vaults: [
+  //     '0xdD6E31A046b828CbBAfb939C2a394629aff8BBdC',
+  //     '0x102137A9F278B013419332f82aCEA429D944Fc34',
+  //   ],
+  // },
   
 }
