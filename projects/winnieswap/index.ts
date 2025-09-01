a4ea023ba432dbae8de97e40388a129f8fd58269
Zdeadex <51711900+zdeadex@users.noreply.github.com>
2025-09-01 15:29:47 +0200

    Create index.ts

---
+++ projects/winnieswap/index.ts
@@ -0,0 +1,34 @@
+const winnieFactory = '0x76fD9D07d5e4D889CAbED96884F15f7ebdcd6B63'
+
+const abis = {
+  token0: 'address:token0',
+  token1: 'address:token1',
+  getDeployers: 'address[]:getDeployers',
+  getStickyVault: 'function getStickyVault(address deployer) view returns (address[])',
+  getUnderlyingBalances: 'function getUnderlyingBalances() view returns (uint256 amount0Current, uint256 amount1Current)'
+}
+
+const tvl = async (api) => {
+  const deployers = await api.call({ target: winnieFactory, abi: abis.getDeployers })
+  const stickyvaults = (await api.multiCall({ calls: deployers.map((d) => ({ target: winnieFactory, params: [d] })), abi: abis.getStickyVault })).flat()
+
+  const [token0s, token1s, balances] = await Promise.all([
+    api.multiCall({ calls: stickyvault, abi: abis.token0, permitFailure: true }),
+    api.multiCall({ calls: stickyvault, abi: abis.token1, permitFailure: true }),
+    api.multiCall({ calls: stickyvault, abi: abis.getUnderlyingBalances, permitFailure: true })
+  ])
+
+  stickyvault.forEach((_, i) => {
+    const token0 = token0s[i]
+    const token1 = token1s[i]
+    const balance = balances[i]
+    if (!token0  !token1  !balance) return
+    const { amount0Current, amount1Current } = balance
+    api.add(token0, amount0Current)
+    api.add(token1, amount1Current)
+  })
+}
+
+module.exports = {
+  berachain: { tvl }
+}
