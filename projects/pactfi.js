9e755d9fe3bdde49410b0c5a8ebca8f0850a9bfb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-01 09:11:51 +0200

    fix pact

--- projects/pactfi.js
+++ projects/pactfi.js
@@ -1,10 +1,18 @@
 const { get } = require('./helper/http')
 const { getConfig } = require('./helper/cache')
 
+const blacklistedTokens = new Set(['3178895177'])
+
 async function tvl(api) {
   await getConfig('pact-fi', 'https://api.pact.fi/api/internal/pools_details/all')
-  const global_data = await get("https://api.pact.fi/api/global_stats")
-  return api.addUSDValue(Math.round(global_data.tvl_usd))
+  const global_data = await get("https://api.pact.fi/api/internal/pools_details/all")
+  let usdValue = 0
+  for (const pool of global_data) {
+    if (!pool.assets.some(i => blacklistedTokens.has(i.on_chain_id))) {
+      usdValue += +pool.tvl_usd
+    }
+  }
+  return api.addUSDValue(Math.round(usdValue))
 }
 
 module.exports = {
