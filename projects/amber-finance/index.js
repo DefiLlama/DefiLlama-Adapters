9df86c80487e8514a9cb7d78f9b8b798da78e677
Linkie Link <linkielink.dev@gmail.com>
2025-09-01 12:56:54 +0200

    simplify by using markets_v2

--- projects/amber-finance/index.js
+++ projects/amber-finance/index.js
@@ -1,84 +1,53 @@
 const { queryContract } = require('../helper/chain/cosmos');
-const axios = require('axios');
 const BigNumber = require('bignumber.js');
 
-const contractAddresses = {
-  params: 'neutron1y2hjwse8sq77gvsmcy8gm6kjye6a3g9ksyxdjg99ceg3rmlpq5usyv5n07',
-  redBank: 'neutron1k8xyccg9nvfavagqjsqngh66w4z286utqweswl4txtnewaymkc9ss5f5e8',
-}
+const redBank = 'neutron1k8xyccg9nvfavagqjsqngh66w4z286utqweswl4txtnewaymkc9ss5f5e8'
 
-
-async function tvl(api) {
-  const chain = api.chain;
-  const { params, redBank } = contractAddresses
+async function getMarkets(chain, redBank) {
   let startAfter = null;
   const pageLimit = 5;
-
-  do {
-    const assetParams = await queryContract({
-      contract: params,
-      chain,
-      data: { all_asset_params: { limit: pageLimit, start_after: startAfter } },
-    });
-
-    if (assetParams.length === pageLimit) startAfter = assetParams[assetParams.length - 1].denom;
-    else startAfter = null;
-
-    await addCoinsFromAssetParams(assetParams);
-  } while (startAfter);
+  const allMarkets = [];
 
   do {
     const markets = await queryContract({
       contract: redBank,
       chain,
-      data: { 'markets': { 'limit': pageLimit, 'start_after': startAfter } },
+      data: { 'markets_v2': { 'limit': pageLimit, 'start_after': startAfter } },
     });
+
+    allMarkets.push(...markets.data);
 
     if (markets.length === pageLimit) startAfter = markets[markets.length - 1].denom;
     else startAfter = null;
-
-    await deductCoinsFromMarkets(markets);
   } while (startAfter);
-  async function addCoinsFromAssetParams(assetParams) {
-    const assetDenoms = assetParams.map((asset) => asset.denom);
 
-    // query the deposited amount for each asset and add it to the depositCoins array
-    await Promise.all(
-      assetDenoms.map(async (denom) => {
-        const totalDepositInfo = await queryContract({
-          contract: params,
-          chain,
-          data: { 'total_deposit': { 'denom': denom } },
-        });
-          api.add(denom, totalDepositInfo.amount);
-        }),
-    );
-  }
+  return allMarkets;
+}
 
-  async function deductCoinsFromMarkets(markets) {
-    // query the underlying debt amount from the debt_total_scaled
-    await Promise.all(
-      markets.map(async (market) => {
-        const totalDebt = await queryContract({
-          contract: redBank,
-          chain,
-          data: {
-            'underlying_debt_amount': {
-              'denom': market.denom,
-              'amount_scaled': market['debt_total_scaled'],
-            },
-          },
-        });
-        api.add(market.denom, totalDebt * -1);
-      }),
-    );
-  }
+async function tvl(api) {
+  const markets = await getMarkets(api.chain, redBank);
+  
+  markets.forEach(market => {
+    const netAmount = BigNumber(market.collateral_total_amount).minus(market.debt_total_amount);
+    api.add(market.denom, netAmount.toString());
+  });
+}
+
+async function borrowed(api) {
+  const markets = await getMarkets(api.chain, redBank);
+  
+  markets.forEach(market => {
+    api.add(market.denom, market.debt_total_amount);
+  });
 }
 
 module.exports = {
   timetravel: false,
   methodology: "Sum token balances by querying the total deposit amount for each asset from the market stats api.",
-  neutron: { tvl },
+  neutron: { 
+    tvl,
+    borrowed
+   },
   hallmarks: [
     [1756303200, 'Launch on Neutron'],
   ],
