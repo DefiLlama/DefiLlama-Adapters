15e21fb5bb76adc59f02b8cf92a6f25d3daa0c68
Linkie Link <linkielink.dev@gmail.com>
2025-09-01 11:57:30 +0200

    update from API to on-chain queries

--- projects/amber-finance/index.js
+++ projects/amber-finance/index.js
@@ -1,67 +1,78 @@
+const { queryContract } = require('../helper/chain/cosmos');
 const axios = require('axios');
 const BigNumber = require('bignumber.js');
 
-const apiUrl = 'https://amberfi-backend.prod.mars-dev.net/v2/redbank_markets_data?chain=neutron&days=1';
-
-const assets = [
-  {
-    denom:
-      "ibc/2EB30350120BBAFC168F55D0E65551A27A724175E8FBCC7B37F9A71618FE136B",
-    symbol: "FBTC",
-    decimals: 8,
-  },
-  {
-    denom:
-      "ibc/B7BF60BB54433071B49D586F54BD4DED5E20BEFBBA91958E87488A761115106B",
-    symbol: "LBTC",
-    decimals: 8,
-  },
-  {
-    denom:
-      "ibc/C0F284F165E6152F6DDDA900537C1BC8DA1EA00F03B9C9EC1841FA7E004EF7A3",
-    symbol: "solvBTC",
-    decimals: 18,
-  },
-  {
-    denom:
-      "ibc/E2A000FD3EDD91C9429B473995CE2C7C555BCC8CFC1D0A3D02F514392B7A80E8",
-    symbol: "eBTC",
-    decimals: 8,
-  },
-  {
-    denom:
-      "ibc/1075520501498E008B02FD414CD8079C0A2BAF9657278F8FB8F7D37A857ED668",
-    symbol: "pumpBTC",
-    decimals: 8,
-  },
-  {
-    denom:
-      "ibc/3F1D988D9EEA19EB0F3950B4C19664218031D8BCE68CE7DE30F187D5ACEA0463",
-    symbol: "uniBTC",
-    decimals: 8,
-  },
-  {
-    denom:
-      "ibc/0E293A7622DC9A6439DB60E6D234B5AF446962E27CA3AB44D0590603DFF6968E",
-    symbol: "wBTC",
-    decimals: 8,
-  },
-]
+const contractAddresses = {
+  params: 'neutron1y2hjwse8sq77gvsmcy8gm6kjye6a3g9ksyxdjg99ceg3rmlpq5usyv5n07',
+  redBank: 'neutron1k8xyccg9nvfavagqjsqngh66w4z286utqweswl4txtnewaymkc9ss5f5e8',
+}
 
 
 async function tvl(api) {
-  const marketStatsData = await axios.get(apiUrl);
-  if(marketStatsData.status !== 200) return;
+  const chain = api.chain;
+  const { params, redBank } = contractAddresses
+  let startAfter = null;
+  const pageLimit = 5;
 
-  const marketStats = marketStatsData.data.data[0].markets ?? [];
-  if(marketStats.length === 0) return;
-  marketStats.forEach((market) => {
-    const asset = assets.find((asset) => asset.denom === market.denom);
-    if(!asset) return;
-    const depositAmount = BigNumber(market.deposit_amount).shiftedBy(asset.decimals);
-    const borrowAmount = BigNumber(market.borrow_amount).shiftedBy(asset.decimals);
-    api.add(market.denom,  depositAmount.minus(borrowAmount).integerValue(BigNumber.ROUND_DOWN).toString());
-  });
+  do {
+    const assetParams = await queryContract({
+      contract: params,
+      chain,
+      data: { all_asset_params: { limit: pageLimit, start_after: startAfter } },
+    });
+
+    if (assetParams.length === pageLimit) startAfter = assetParams[assetParams.length - 1].denom;
+    else startAfter = null;
+
+    await addCoinsFromAssetParams(assetParams);
+  } while (startAfter);
+
+  do {
+    const markets = await queryContract({
+      contract: redBank,
+      chain,
+      data: { 'markets': { 'limit': pageLimit, 'start_after': startAfter } },
+    });
+
+    if (markets.length === pageLimit) startAfter = markets[markets.length - 1].denom;
+    else startAfter = null;
+
+    await deductCoinsFromMarkets(markets);
+  } while (startAfter);
+  async function addCoinsFromAssetParams(assetParams) {
+    const assetDenoms = assetParams.map((asset) => asset.denom);
+
+    // query the deposited amount for each asset and add it to the depositCoins array
+    await Promise.all(
+      assetDenoms.map(async (denom) => {
+        const totalDepositInfo = await queryContract({
+          contract: params,
+          chain,
+          data: { 'total_deposit': { 'denom': denom } },
+        });
+          api.add(denom, totalDepositInfo.amount);
+        }),
+    );
+  }
+
+  async function deductCoinsFromMarkets(markets) {
+    // query the underlying debt amount from the debt_total_scaled
+    await Promise.all(
+      markets.map(async (market) => {
+        const totalDebt = await queryContract({
+          contract: redBank,
+          chain,
+          data: {
+            'underlying_debt_amount': {
+              'denom': market.denom,
+              'amount_scaled': market['debt_total_scaled'],
+            },
+          },
+        });
+        api.add(market.denom, totalDebt * -1);
+      }),
+    );
+  }
 }
 
 module.exports = {
