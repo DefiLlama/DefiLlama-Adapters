d2a72e014a9b2b81aac882321a0e5495035bb21b
nordwinder <therandomchoice@proton.me>
2025-09-02 16:44:11 +0400

    DefiTuna Lending

---
+++ projects/defituna_lending.js
@@ -0,0 +1,133 @@
+const { getProvider  } = require("./helper/solana");
+const { Program } = require("@project-serum/anchor");
+
+async function tvl(api) {
+  const provider = getProvider(api.chain);
+  const program = new Program(tunaIDL, tunaIDL.address, provider);
+  const vaults = await program.account.vault.all();
+  vaults.forEach(vault => api.add(vault.account.mint.toBase58(), vault.account.depositedFunds));
+}
+
+async function borrowed(api) {
+  const provider = getProvider(api.chain);
+  const program = new Program(tunaIDL, tunaIDL.address, provider);
+  const vaults = await program.account.vault.all();
+  vaults.forEach(vault => api.add(vault.account.mint.toBase58(), vault.account.borrowedFunds));
+}
+
+module.exports = {
+  timetravel: false,
+  solana: { tvl, borrowed },
+  methodology: "TVL is calculated based on the amount of deposited tokens.",
+  start: "2024-11-29",
+  hallmarks: [
+    [1753297176, 'TUNA token launched']
+  ],
+};
+
+const tunaIDL = {
+    address: "tuna4uSQZncNeeiAMKbstuxA9CUkHH6HmC64wgmnogD",
+    metadata: {
+        name: "tuna",
+        version: "2.0.11",
+        spec: "0.1.0",
+        description: "DefiTuna"
+    },
+  instructions: [],
+  accounts: [
+    {
+      name: "Vault",
+      type: {
+        kind: "struct",
+        fields: [
+          {
+            name: "version",
+            docs: ["Struct version"],
+            type: "u16",
+          },
+          {
+            name: "bump",
+            docs: ["Bump seed for the vault account"],
+            type: {
+              array: ["u8", 1],
+            },
+          },
+          {
+            name: "mint",
+            docs: ["The mint of the token that this vault holds"],
+            type: "publicKey",
+          },
+          {
+            name: "deposited_funds",
+            docs: [
+              "The amount of funds deposited in the vault - takes into account accrued interest",
+            ],
+            type: "u64",
+          },
+          {
+            name: "deposited_shares",
+            docs: ["The amount of shares deposited in the vault"],
+            type: "u64",
+          },
+          {
+            name: "borrowed_funds",
+            docs: [
+              "The amount of funds borrowed from the vault - takes into account accrued interest",
+            ],
+            type: "u64",
+          },
+          {
+            name: "borrowed_shares",
+            docs: ["The amount of shares borrowed from the vault"],
+            type: "u64",
+          },
+          {
+            name: "unpaid_debt_shares",
+            docs: [
+              "Bad dept may appear on a position liquidation if not enough funds to repay the debt to a lending pool.",
+            ],
+            type: "u64",
+          },
+          {
+            name: "interest_rate",
+            docs: [
+              "The interest rate of the vault per seconds. (1<<60) / 31536000 = 1152921504606846976 / 31536000 = 100% annually.",
+            ],
+            type: "u64",
+          },
+          {
+            name: "last_update_timestamp",
+            docs: ["The last time the vault was updated."],
+            type: "u64",
+          },
+          {
+            name: "supply_limit",
+            docs: [
+              "The maximum allowed supply for this pool. The default value 0 is unlimited supply.",
+            ],
+            type: "u64",
+          },
+          {
+            name: "pyth_oracle_price_update",
+            docs: ["Pyth oracle price update account."],
+            type: "publicKey",
+          },
+          {
+            name: "pyth_oracle_feed_id",
+            docs: ["Pyth oracle price feed id."],
+            type: "publicKey",
+          },
+          {
+            name: "reserved",
+            docs: ["Reserved"],
+            type: {
+              array: ["u8", 184],
+            },
+          },
+        ],
+      },
+    },
+  ],
+  errors: [],
+  types: [],
+};
