760b22f0cb158f3383174171fecd49f2c515d828
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-04 11:03:53 +0200

    Refactor TVL calculation and update exports

--- projects/winnieswap/index.js
+++ projects/winnieswap/index.js
@@ -1,34 +1,29 @@
-const winnieFactory = '0x18B9ABf2E821E2fE7A08Dc255d5a7e77fFc0b844'
+const stickyFactory = '0x18B9ABf2E821E2fE7A08Dc255d5a7e77fFc0b844'
 
 const abis = {
-  token0: 'address:token0',
-  token1: 'address:token1',
   getDeployers: 'address[]:getDeployers',
-  getStickyVault: 'function getStickyVault(address deployer) view returns (address[])',
-  getUnderlyingBalances: 'function getUnderlyingBalances() view returns (uint256 amount0Current, uint256 amount1Current)'
+  getStickyVault: 'function getStickyVaults(address deployer) view returns (address[])',
 }
 
-const tvl = async (api) => {
-  const deployers = await api.call({ target: winnieFactory, abi: abis.getDeployers })
-  const stickyvaults = (await api.multiCall({ calls: deployers.map((d) => ({ target: winnieFactory, params: [d] })), abi: abis.getStickyVault })).flat()
+const akkarisTVL = async (api) => {
+  const deployers = await api.call({ target: stickyFactory, abi: abis.getDeployers })
+  const stickyVaults = (await api.multiCall({ calls: deployers, target: stickyFactory, abi: abis.getStickyVault })).flat()
 
-  const [token0s, token1s, balances] = await Promise.all([
-    api.multiCall({ calls: stickyvaults, abi: abis.token0, permitFailure: true }),
-    api.multiCall({ calls: stickyvaults, abi: abis.token1, permitFailure: true }),
-    api.multiCall({ calls: stickyvaults, abi: abis.getUnderlyingBalances, permitFailure: true })
+  const [token0s, token1s,] = await Promise.all([
+    api.multiCall({ calls: stickyVaults, abi: 'address:token0', }),
+    api.multiCall({ calls: stickyVaults, abi: 'address:token1', }),
   ])
-
-  stickyvaults.forEach((_, i) => {
-    const token0 = token0s[i]
-    const token1 = token1s[i]
-    const balance = balances[i]
-    if (!token0 || !token1 || !balance) return
-    const { amount0Current, amount1Current } = balance
-    api.add(token0, amount0Current)
-    api.add(token1, amount1Current)
-  })
+  const tokensAndOwners2 = [token0s.concat(token1s), stickyVaults.concat(stickyVaults)]
+  return api.sumTokens({ tokensAndOwners2 })
 }
 
-module.exports = {
-  berachain: { tvl }
-}
+const { uniV3Export } = require('../helper/uniswapV3');
+const { mergeExports } = require('../helper/utils')
+
+const uniExports = uniV3Export({
+  berachain: { factory: '0x76fD9D07d5e4D889CAbED96884F15f7ebdcd6B63', fromBlock: 7402487 },
+})
+
+module.exports = mergeExports([{
+  berachain: { tvl: akkarisTVL }
+}, uniExports])
