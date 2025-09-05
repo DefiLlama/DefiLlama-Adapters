f34f57a455e261b9f74f94fd22f07e6ebd60267a
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2025-09-05 17:06:17 +0530

    tracking remora markets (#16153)

---
+++ projects/remora-markets/index.js
@@ -0,0 +1,34 @@
+const { getTokenSupplies } = require('../helper/solana')
+
+const data = [
+    {
+        "name": "Circle Internet Group rStock",
+        "address": "5fKr9joRHpioriGmMgRVFdmZge8EVUTbrWyxDVdSrcuG"
+    },
+    {
+        "name": "MicroStrategy Inc rStock",
+        "address": "B8GKqTDGYc7F6udTHjYeazZ4dFCRkrwK2mBQNS4igqTv"
+    },
+    {
+        "name": "NVIDIA Corporation rStock",
+        "address": "ALTP6gug9wv5mFtx2tSU1YYZ1NrEc2chDdMPoJA8f8pu"
+    },
+    {
+        "name": "S&P500 rStock",
+        "address": "AVw2QGVkXJPRPRjLAceXVoLqU5DVtJ53mdgMXp14yGit"
+    },
+    {
+        "name": "Tesla, Inc. rStock",
+        "address": "FJug3z58gssSTDhVNkTse5fP8GRZzuidf9SRtfB2RhDe"
+    },
+];
+
+async function tvl(api) {
+    const tokens = data.map(stock => stock.address).filter(Boolean);
+    const supplies = await getTokenSupplies(tokens);
+    api.add(tokens, tokens.map((token) => supplies[token] || '0'));
+}
+
+module.exports = {
+    solana: { tvl }
+}
