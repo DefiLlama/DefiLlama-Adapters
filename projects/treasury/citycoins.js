f29f04508fc6cde0b86e257ebf8afc0b0b784d8d
friedger <mail@friedger.de>
2025-09-06 16:10:50 +0200

    upgrade mia to v3, add mia rewards as treasury

--- projects/treasury/citycoins.js
+++ projects/treasury/citycoins.js
@@ -1,6 +1,6 @@
 const { sumTokensExport, nullAddress } = require('../helper/sumTokens')
 
-const MIAMI_DAO_TREASURY = 'SP8A9HZ3PKST0S42VM9523Z9NV42SZ026V4K39WH.ccd002-treasury-mia-mining-v2'
+const MIAMI_DAO_TREASURY = 'SP8A9HZ3PKST0S42VM9523Z9NV42SZ026V4K39WH.ccd002-treasury-mia-mining-v3'
 const NYC_DAO_TREASURY = 'SP8A9HZ3PKST0S42VM9523Z9NV42SZ026V4K39WH.ccd002-treasury-nyc-mining-v2'
 
 module.exports = {
