f92f501f28139a0bc553a1d715f32ec06e3c647d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-09 18:47:27 +0200

    minor fix

--- projects/lendle-vault-allocator/index.js
+++ projects/lendle-vault-allocator/index.js
@@ -1,25 +1,13 @@
-async function tvl(api) {
-    const abis = {
-      asset: "function asset() view returns (address)",
-      balance: "function totalAssets() external view returns (uint256)",
-    };
-  
-    const vaults = [
-      "0xE12EED61E7cC36E4CF3304B8220b433f1fD6e254",
-      "0x5A285484126D4e1985AC2cE0E1869D6384027727",
-      "0xf36a57369362eB1553f24C8ad50873723E6e1173"
-    ];
-  
-    const assets = await api.multiCall({ abi: abis.asset, calls: vaults });
-    const balances = await api.multiCall({ abi: abis.balance, calls: vaults });
-    console.log(assets, balances);
-    vaults.forEach((v, i) => {
-      api.add(assets[i], balances[i]);
-    });
-  }
-  
-  module.exports = {
-    doublecounted: true,
-    methodology: 'TVL accounts for all assets deposited into the Vaults.',
-    mantle: { tvl },
-  };
+const { sumERC4626VaultsExport2 } = require('../helper/erc4626')
+
+const vaults = [
+  "0xE12EED61E7cC36E4CF3304B8220b433f1fD6e254",
+  "0x5A285484126D4e1985AC2cE0E1869D6384027727",
+  "0xf36a57369362eB1553f24C8ad50873723E6e1173"
+];
+
+module.exports = {
+  doublecounted: true,
+  methodology: 'TVL accounts for all assets deposited into the Vaults.',
+  mantle: { tvl: sumERC4626VaultsExport2({ vaults }) },
+};
