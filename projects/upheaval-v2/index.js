4c3181f86d0ecac0666e9efbc7d24578be384aa0
upheaval-defi <upheavaldefi@gmail.com>
2025-09-10 12:48:25 +0530

    added upheaval dex tvl (#16056)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/upheaval-v2/index.js
+++ projects/upheaval-v2/index.js
@@ -1,10 +1,4 @@
-const { getUniTVL } = require('../helper/unknownTokens')
-
-module.exports = {
-    hyperliquid: {
-        tvl: getUniTVL({
-            factory: '0x98e19A533FadB2C9853983772E4e7aa09a1478e0',
-            chain: 'hyperliquid',
-        })
-    }
-}
+const { uniTvlExports } = require('../helper/unknownTokens')
+module.exports = uniTvlExports({
+  'hyperliquid': '0x98e19A533FadB2C9853983772E4e7aa09a1478e0'
+})
