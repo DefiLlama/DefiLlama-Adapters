9b64abb03e21f5d2634a552c7f2057c45a8fbda2
aleeeexcool <aleeeexcool@gmail.com>
2025-09-11 11:31:53 +0300

    fix: remove unused vaults

--- projects/brink/index.js
+++ projects/brink/index.js
@@ -11,9 +11,6 @@ const mantleVaults = [
   "0xE12EED61E7cC36E4CF3304B8220b433f1fD6e254", // lendle
   "0x5A285484126D4e1985AC2cE0E1869D6384027727", // lendle
   "0xf36a57369362eB1553f24C8ad50873723E6e1173", // lendle
-  "0x6E435EC692eE22bbd797b88c199C89FC68b4dE11", // lendle + init
-  "0xBD0f0F53c9B2d314C8399e624382e924cb7aa5eD", // lendle + init
-  "0x40c91DA86FC8F20D049Fa95Ab38c1134030dbF84", // lendle + init
 ];
 
 async function baseTvl(api) {
