6756f16bf7d63a901f77a84188832d8b96e71348
allin <992899491@qq.com>
2025-09-13 10:31:51 +0800

    update Somnex Dexs

--- projects/somnex-xyz/index.js
+++ projects/somnex-xyz/index.js
@@ -1,5 +1,39 @@
-const { uniTvlExports } = require('../helper/unknownTokens')
+// const { uniTvlExports } = require('../helper/unknownTokens')
 
-module.exports = uniTvlExports({
-  somnia: '0xaFd71143Fb155058e96527B07695D93223747ed1'
-})
+// module.exports = uniTvlExports({
+//   somnia: '0xaFd71143Fb155058e96527B07695D93223747ed1'
+// })
+
+const { request, gql } = require('graphql-request')
+const { toUSDTBalances } = require('../helper/balances');
+
+const endpoint = 'https://api.subgraph.somnia.network/api/public/962dcbf6-75ff-4e54-b778-6b5816c05e7d/subgraphs/somnia-swap/v1.0.0/gn'
+
+const tvlQuery = gql`
+query {
+  pairFactory(id: "0") {
+    totalLiquidityUSD
+  }
+  factories {
+    totalValueLockedUSD
+  }
+}
+`
+
+// ① 把逻辑包成「链」维度函数
+async function tvl() {
+  const data = await request(endpoint, tvlQuery)
+
+  const factoryTvl  = parseFloat(data.pairFactory?.totalLiquidityUSD || 0)
+  const factoriesTvl = parseFloat(data.factories?.[0]?.totalValueLockedUSD || 0)
+
+  const total = factoryTvl + factoriesTvl
+
+  return toUSDTBalances(total);
+}
+
+module.exports = {
+  timetravel: false,
+  methodology: 'TVL from subgraph totalLiquidityUSD + totalValueLockedUSD',
+  somnia: { tvl }
+}
