79b0155d41efe7197158792e9084b2a604c78a90
Eteria <132741663+Eteriaio@users.noreply.github.com>
2025-09-15 13:55:49 +0800

    Update index.js

--- projects/arbitrum/index.js
+++ projects/arbitrum/index.js
@@ -1,15 +1,12 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
 module.exports = {
-  ethereum: {
+  eteria: {
     tvl: (api) =>
       sumTokens2({
         api,
         owners: [
-          "0xa3A7B6F88361F48403514059F1F16C8E78d60EeC",
-          "0x8315177aB297bA92A06054cE80a67Ed4DBd7ed3a",
-          "0xcEe284F754E854890e311e3280b767F80797180d",
-          "0xA10c7CE4b876998858b1a9E12b10092229539400",
+          "0x79d0FF4D5b05f05D97227F3601C0Ee85937B0513",
         ],
         fetchCoValentTokens: true,
         permitFailure: true
