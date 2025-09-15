fae0ac07951574f7b33357d8273da9004ed1d978
Dre Cronye <dre@rezerve.money>
2025-09-16 00:47:53 +0300

    Update index.js

--- projects/rezerve-lending/index.js
+++ projects/rezerve-lending/index.js
@@ -3,10 +3,6 @@ const { getCuratorTvl } = require("../helper/curators");
 const configs = {
   methodology: 'Count all assets deposited into vaults curated by Rezerve',
   ethereum: {
-    eulerVaultOwners: [
-      '0x1f4817b5F2B2e71658251d905634C62FAb8e2033', // gnosis safe
-      '0x5f5a6E0F769BBb9232d2F6EDA84790296b288974' // deployer wallet
-    ],
     euler: [
       '0xc42d337861878baa4dc820d9e6b6c667c2b57e8a', // USDC
       '0xBAfC1A885e25C6F594e06F12edaeB46858547724', // frxUSD
