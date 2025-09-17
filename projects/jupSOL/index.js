a0caf054c6622df048eca18d9cb2087582ad5fd5
Eden <noat.eth@gmail.com>
2025-09-17 22:54:22 +0700

    add Jupiter Labs Perpetuals Vault to jupSOL

--- projects/jupSOL/index.js
+++ projects/jupSOL/index.js
@@ -1,7 +1,11 @@
-const { getSolBalanceFromStakePool } = require('../helper/solana')
+const { getStakedSol, getSolBalanceFromStakePool } = require('../helper/solana')
 
 async function tvl(api) {
+  // jupSOL
   await getSolBalanceFromStakePool('8VpRhuxa7sUUepdY3kQiTmX9rS5vx4WgaXiAnXq4KCtr', api)
+
+  // Jupiter Labs Perpetuals Vault
+  await getStakedSol('AVzP2GeRmqGphJsMxWoqjpUifPpCret7LqWhD8NWQK49', api)
 }
 
 module.exports = {
