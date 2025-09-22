bb040cf57a46065545984f866045f940ebeda9a6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-09-22 11:03:44 +0200

    move meteora-dlmm to heavy adapters

--- projects/meteora-dlmm/index.js
+++ projects/meteora-dlmm/index.js
@@ -4,13 +4,15 @@ const idl = require('./idl.json')
 
 // https://docs.meteora.ag/dlmm/dlmm-integration/dlmm-sdk
 async function tvl() {
+  if (!getEnv('IS_RUN_FROM_CUSTOM_JOB')) throw new Error('This job is not meant to be run directly, please use the custom job feature')
+
   const provider = getProvider()
   const programId = 'LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo'
   // const idl = await Program.fetchIdl(programId, provider)
   const program = new Program(idl, programId, provider)
   const pools = await program.account.lbPair.all()
-  const tokenAccounts = pools.map(({ account: { reserveX, reserveY }}) => [reserveX, reserveY]).flat()
-  return sumTokens2({ tokenAccounts})
+  const tokenAccounts = pools.map(({ account: { reserveX, reserveY } }) => [reserveX, reserveY]).flat()
+  return sumTokens2({ tokenAccounts })
 }
 
 module.exports = {
