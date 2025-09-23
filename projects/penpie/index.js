e788158c42216d6f616e493708467dcf3b88dc46
bhomSinghMagpie <bhom@magpiexyz.io>
2025-09-23 23:21:34 +0530

    fixed: penpie tvl issue

--- projects/penpie/index.js
+++ projects/penpie/index.js
@@ -3,8 +3,9 @@ const config = require("./config");
 const { staking } = require('../helper/staking')
 
 async function tvl(api) {
-  const { masterPenpie, vlPNP, pendleStaking, mPENDLE, } = config[api.chain];
+  const { masterPenpie, vlPNP, pendleStaking, mPENDLE, PNP } = config[api.chain];
 
+  // Get main protocol TVL (Pendle staking + mPENDLE)
   const poolTokens = await api.fetchList({
     lengthAbi: MasterMagpieAbi.poolLength,
     itemAbi: MasterMagpieAbi.registeredToken,
@@ -13,16 +14,19 @@ async function tvl(api) {
   const blacklistedTokens = []
   if (vlPNP) blacklistedTokens.push(vlPNP)
   if (mPENDLE && masterPenpie) await api.sumTokens({ tokens: [mPENDLE], owner: masterPenpie })
+  
+  // Add staking TVL if PNP and vlPNP exist
+  if (PNP && vlPNP) {
+    await api.sumTokens({ tokens: [PNP], owner: vlPNP })
+  }
+  
   return api.sumTokens({ tokens: poolTokens, owner: pendleStaking, blacklistedTokens })
 }
 
 Object.keys(config).forEach((chain) => {
-  const { PNP, vlPNP, } = config[chain];
-
   module.exports[chain] = {
     tvl,
   };
-  if (PNP && vlPNP) module.exports[chain].staking = staking(vlPNP, PNP)
 });
 
 module.exports.doublecounted = true
