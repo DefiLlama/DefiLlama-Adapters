d87119667b330c8a766039b3749cb9d1daffd9c5
weilifeng <weilifeng@huobi.com>
2025-09-24 21:15:34 +0800

    fix volume

--- projects/sunperp/index.js
+++ projects/sunperp/index.js
@@ -10,7 +10,22 @@ async function tvl(api) {
         params: [chefContract],
     });
 
+    const wallet1 = await api.call({
+        abi: 'function balanceOf(address account) external view returns (uint256)',
+        target: ADDRESSES.tron.USDT,
+        params: ["TK8v6HPniRKYixj6Egby712nr5R3xrYYCT"],
+    });
+
+    const wallet2 = await api.call({
+        abi: 'function balanceOf(address account) external view returns (uint256)',
+        target: ADDRESSES.tron.USDT,
+        params: ["TYRmcmmKvjL1SFQbvPQtJU1YcaXEnH8dvT"],
+    });
+
     api.add(ADDRESSES.tron.USDT, usdtBalance);
+    api.add(ADDRESSES.tron.USDT, wallet1);
+    api.add(ADDRESSES.tron.USDT, wallet2);
+
 }
 
 module.exports = {
