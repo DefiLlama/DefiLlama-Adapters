08d8faa98bd78cfd36d42f1b74f6b7d2e593d34c
prajwalghate <prajwalghate18@gmail.com>
2025-09-25 17:12:39 +0530

    Added stCollateral TVL

--- projects/solido/index.js
+++ projects/solido/index.js
@@ -1,34 +1,38 @@
-const sdk = require("@defillama/sdk");
 const {
   invokeViewFunction,
 } = require("../helper/chain/supra");
-const { transformBalances } = require("../helper/portedTokens");
-const coreTokensAll = require("../helper/coreAssets.json");
 
+const { transformBalances } = require("../helper/portedTokens");
 const CDP_GET_TOTAL_STATS_FUNCTION_TYPE =
   "0x9176f70f125199a3e3d5549ce795a8e906eed75901d535ded623802f15ae3637::cdp_multi::get_total_stats";
-  
+const CONVERT_TO_ASSETS_FUNCTION_TYPE="0x81846514536430ea934c7270f86cf5b067e2a2faef0e91379b4f284e91c7f53c::vault_core::convert_to_assets";
 const calculateSolidoTVL = async (api) => {
   const chain = api.chain;
-  const coreTokens = Object.values(coreTokensAll[chain] ?? {});
+  const collateralTokens =[
+    '0x1::supra_coin::SupraCoin',
+    '0x81846514536430ea934c7270f86cf5b067e2a2faef0e91379b4f284e91c7f53c::vault_core::VaultShare'
+  ]
   let balances = {};
-
-  // For each supported collateral type, get total stats
-  for (const coinType of coreTokens) {
-    const [totalCollateral, totalDebt] = await invokeViewFunction(
-      CDP_GET_TOTAL_STATS_FUNCTION_TYPE,
-      [coinType],
-      []
-    );
-    
-    if (totalCollateral > 0) {
-      sdk.util.sumSingleBalance(balances, coinType, totalCollateral);
-    }
-  }
-
+  let totalCollateral_in_supra=0;
+  const [SUPRA_TVL,SUPRA_DEBT] = await invokeViewFunction(
+    CDP_GET_TOTAL_STATS_FUNCTION_TYPE,
+    [collateralTokens[0]],
+    []
+  );
+  const [stSUPRA_TVL,stSUPRA_DEBT] = await invokeViewFunction(
+    CDP_GET_TOTAL_STATS_FUNCTION_TYPE,
+    [collateralTokens[1]],
+    []
+  );
+  const stSUPRA_TVL_in_supra = await invokeViewFunction(
+    CONVERT_TO_ASSETS_FUNCTION_TYPE,
+    [collateralTokens[0]],
+    [stSUPRA_TVL]
+  );
+  totalCollateral_in_supra = BigInt(SUPRA_TVL) + BigInt(stSUPRA_TVL_in_supra[0]);
+  balances={ '0x1::supra_coin::SupraCoin': totalCollateral_in_supra }
   return transformBalances(chain, balances);
 };
-
 module.exports = {
   timetravel: false,
   misrepresentedTokens: true,
@@ -36,3 +40,4 @@ module.exports = {
     tvl: calculateSolidoTVL,
   },
 };
+
