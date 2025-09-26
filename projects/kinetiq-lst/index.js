911529a9947e30f1755d34b259b659b6ad9c5c25
0xpeluche <0xpeluche@proton.me>
2025-09-26 15:42:21 +0200

    Update Kinetiq

---
+++ projects/kinetiq-lst/index.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const sdk = require('@defillama/sdk')
+
+const HYPERLIQUID_MAINNET_RPC_URL = 'https://api.hyperliquid.xyz';
+
+const managers = [
+  '0x393D0B87Ed38fc779FD9611144aE649BA6082109', // kHype
+  '0xaD492f9CADcccE9c3c213edd8aE55c152cD3A3ad', // hiHype
+  '0xfdd35c5179E8594E237031dd945E0584Af29572b', // flowHype
+  '0x0c5d890Cf52973aE4A7b10fA7EE18e146d13D87B', // asxnHype
+]
+
+const getUserStakingSummary = async (user) => {
+  const url = `${HYPERLIQUID_MAINNET_RPC_URL}/info`;
+  const response = await axios.post(url, { type: 'delegatorSummary', user });
+  return response.data;
+};
+
+const tvl = async (api) => {
+  const [_hypeBalances, _delegatedBalances] = await Promise.all([
+    Promise.all(managers.map((m) => sdk.api.eth.getBalance({ target: m, chain: 'hyperliquid' }))),
+    Promise.all(managers.map((m) => getUserStakingSummary(m)))
+  ]);
+
+  managers.forEach((_, index) => {
+    const hypeBalance = _hypeBalances[index].output
+    const _delegatedBalance = _delegatedBalances[index]
+    const { delegated, undelegated, totalPendingWithdrawal } = _delegatedBalance
+    const delegatedBalance = (+delegated + +undelegated - +totalPendingWithdrawal) * 1e18
+    api.addGasToken(+hypeBalance + delegatedBalance)
+  })
+}
+
+module.exports = {
+  timetravel: false,
+  hyperliquid: { tvl }
+}
