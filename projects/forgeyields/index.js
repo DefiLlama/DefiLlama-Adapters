54a68e979579d0501ca2b9604fc7f6d91b02c18b
0xSacha <90143060+0xSacha@users.noreply.github.com>
2025-09-27 17:06:17 +0100

    Update to on chain tvl calculation

--- projects/forgeyields/index.js
+++ projects/forgeyields/index.js
@@ -1,6 +1,26 @@
 const { getConfig } = require('../helper/cache')
 const { get } = require('../helper/http')
 const ADDRESSES = require('../helper/coreAssets.json');
+const { call } = require('../helper/chain/starknet');
+
+const totalAssetsABI = {
+  "type": "function",
+  "name": "total_assets",
+  "inputs": [
+      {
+          "name": "token_gateway",
+          "type": "core::starknet::contract_address::ContractAddress"
+      }
+  ],
+  "outputs": [
+      {
+          "type": "core::integer::u256"
+      }
+  ],
+  "state_mutability": "view",
+  "customInput": "address"
+
+}
 
 
 async function fetcher() {
@@ -8,13 +28,22 @@ async function fetcher() {
   return get(apiUrl)
 }
 
+const totalAssetsProvider = "0x2d0ee5bf4445712c414d58544c9d522a537e4292fa3c3ad36e68bd177a378b8"
+
 async function tvl(api) {
   const strategies = await getConfig('forgeyields', undefined, { fetcher })
   for (let index = 0; index < strategies.length; index++) {
     const strategyInfo = strategies[index];
-    console.log(strategyInfo.tvl)
+    const tokenGateway = strategyInfo.token_gateway_per_domain
+    .find(domain => domain.domain === 'starknet')
+    ?.token_gateway;
+    const totalAssets = await call({
+      abi: totalAssetsABI,
+      target: totalAssetsProvider,
+      params: [tokenGateway]
+    });
     const underlying = ADDRESSES.starknet[strategyInfo.underlyingSymbol]
-    api.add(underlying, strategyInfo.tvl * 10 ** strategyInfo.decimals)
+    api.add(underlying, totalAssets)
   }
 }
 module.exports = {
