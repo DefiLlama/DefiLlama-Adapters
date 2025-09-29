ed374a3a5daf57c2088830fc68728028c7671d91
BlazingApp <admin@blazingbot.com>
2025-09-29 15:15:08 +0000

    BlazingApp: add adapter scaffold; fees=blazingbot

---
+++ projects/blazingapp/index.js
@@ -0,0 +1,24 @@
+const { getLogs } = require("../helpers/cache/getLogs");
+const { ARBITRUM, AVAX, BASE, BSC, ETHEREUM, BERACHAIN, SONIC } = require("../helpers/chains");
+
+// Minimal placeholder adapter: reports zero TVL while we wire real contracts.
+// This lets us submit the PR and iterate addresses quickly with maintainers.
+
+async function tvl() {
+  return {};
+}
+
+module.exports = {
+  timetravel: false,
+  methodology: "Placeholder adapter. We will compute fees and TVL from on-chain deployed routers listed in the PR description.",
+  start: 1731946483,
+  ethereum: { tvl },
+  base: { tvl },
+  bsc: { tvl },
+  avalanche: { tvl },
+  berachain: { tvl },
+  sonic: { tvl },
+  solana: { tvl },
+};
+
+
