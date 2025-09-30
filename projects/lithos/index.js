09536755d1e3b24e78eb26a9d0a4253755ee9b42
Rahul Sethuram <rahul.eth@icloud.com>
2025-09-30 11:31:03 +0100

    use correct method

--- projects/lithos/index.js
+++ projects/lithos/index.js
@@ -1,10 +1,5 @@
-const { getUniTVL } = require("../helper/unknownTokens");
+const { uniTvlExport } = require('../helper/unknownTokens')
+const chain = 'plasma'
+const factory = '0x71a870D1c935C2146b87644DF3B5316e8756aE18...' // v2 factory address
 
-module.exports = {
-  plasma: {
-    tvl: getUniTVL({
-      factory: "0x71a870D1c935C2146b87644DF3B5316e8756aE18",
-      useDefaultCoreAssets: true,
-    }),
-  },
-};
+module.exports = uniTvlExport(chain, factory)
