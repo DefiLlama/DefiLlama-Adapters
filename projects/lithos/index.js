c4a8e30d2d6922e2d2041d0486f2da93e612688b
Rahul Sethuram <rahul.eth@icloud.com>
2025-09-30 11:19:19 +0100

    Add Lithos project.

---
+++ projects/lithos/index.js
@@ -0,0 +1,10 @@
+const { getUniTVL } = require("../helper/unknownTokens");
+
+module.exports = {
+  plasma: {
+    tvl: getUniTVL({
+      factory: "0x71a870D1c935C2146b87644DF3B5316e8756aE18",
+      useDefaultCoreAssets: true,
+    }),
+  },
+};
