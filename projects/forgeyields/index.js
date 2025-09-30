589f49a710429f97bfe6d22f1f4e71a96ff66ec7
0xSacha <90143060+0xSacha@users.noreply.github.com>
2025-09-30 16:08:17 +0100

    Update index.js

--- projects/forgeyields/index.js
+++ projects/forgeyields/index.js
@@ -1,7 +1,7 @@
 const { getConfig } = require('../helper/cache')
 const { get } = require('../helper/http')
 const ADDRESSES = require('../helper/coreAssets.json');
-const { call } = require('../helper/chain/starknet');
+const { multiCall } = require('../helper/chain/starknet');
 
 const totalAssetsABI = {
   "type": "function",
@@ -32,18 +32,33 @@ const totalAssetsProvider = "0x2d0ee5bf4445712c414d58544c9d522a537e4292fa3c3ad36
 
 async function tvl(api) {
   const strategies = await getConfig('forgeyields', undefined, { fetcher })
-  for (let index = 0; index < strategies.length; index++) {
-    const strategyInfo = strategies[index];
+  
+  const calls = strategies
+    .map(strategyInfo => {
+      const tokenGateway = strategyInfo.token_gateway_per_domain
+        .find(domain => domain.domain === 'starknet')
+        ?.token_gateway;
+      return {
+        abi: totalAssetsABI,
+        target: totalAssetsProvider,
+        params: [tokenGateway]
+      }
+    })
+    .filter(call => call.params[0])
+
+  const totalAssets = await multiCall({ calls })
+  
+  let callIndex = 0
+  for (const strategyInfo of strategies) {
     const tokenGateway = strategyInfo.token_gateway_per_domain
-    .find(domain => domain.domain === 'starknet')
-    ?.token_gateway;
-    const totalAssets = await call({
-      abi: totalAssetsABI,
-      target: totalAssetsProvider,
-      params: [tokenGateway]
-    });
-    const underlying = ADDRESSES.starknet[strategyInfo.underlyingSymbol]
-    api.add(underlying, totalAssets)
+      .find(domain => domain.domain === 'starknet')
+      ?.token_gateway;
+    
+    if (tokenGateway) {
+      const underlying = ADDRESSES.starknet[strategyInfo.underlyingSymbol]
+      api.add(underlying, totalAssets[callIndex])
+      callIndex++
+    }
   }
 }
 module.exports = {
