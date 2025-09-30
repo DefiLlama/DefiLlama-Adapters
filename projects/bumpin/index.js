d626e9d1375c55f64582f8180bbec4d8bac92a92
jax <>
2025-09-30 16:55:09 +0800

    Fix: Bumpin, Borrow TVL

--- projects/bumpin/index.js
+++ projects/bumpin/index.js
@@ -1,6 +1,6 @@
-const { getProvider, sumTokens2, } = require('../helper/solana')
-const { Program } = require("@coral-xyz/anchor");
-const { PublicKey } = require('@solana/web3.js');
+const {getProvider, sumTokens2,} = require('../helper/solana')
+const {Program} = require("@coral-xyz/anchor");
+const {PublicKey} = require('@solana/web3.js');
 
 async function tvl() {
     const provider = getProvider()
@@ -13,20 +13,39 @@ async function tvl() {
 
     const poolData = await program.account.pool.all()
     const tokenData = await program.account.tradeToken.all()
+    const tokenVault = await program.account.tokenVault.all();
     const tokenAccounts = []
     const blacklistedTokens = []
-    poolData.forEach(({ account: i }) => {
+    
+    poolData.forEach(({account: i}) => {
         blacklistedTokens.push(i.launcherMint.toString())
         tokenAccounts.push(i.poolVaultKey.toString())
     });
-    tokenData.forEach(({ account: i }) => {
+    tokenData.forEach(({account: i}) => {
+        tokenAccounts.push(i.vaultKey.toString())
+    });
+    tokenVault.forEach(({account: i}) => {
         tokenAccounts.push(i.vaultKey.toString())
     });
 
-    return sumTokens2({ tokenAccounts: tokenAccounts.filter(i => i !== '11111111111111111111111111111111'), blacklistedTokens, })
+    // 使用 Set 去重，避免重复计数
+    const uniqueTokenAccounts = [...new Set(tokenAccounts)];
+
+    return sumTokens2({
+        tokenAccounts: uniqueTokenAccounts.filter(i => i !== '11111111111111111111111111111111'),
+        blacklistedTokens, 
+        api: api => {
+            // 添加已借出的金额到 TVL
+            // 因为 vaultKey 中的余额已经扣除了借出的部分
+            tokenVault.forEach((data) => {
+                const mint = data.account.mintKey.toString()
+                api.add(mint, data.account.totalBorrowed)
+            })
+        }
+    })
 }
 
 module.exports = {
     timetravel: false,
-    solana: { tvl, },
+    solana: {tvl,},
 }
