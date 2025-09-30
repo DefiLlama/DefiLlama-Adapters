14db05624d85c226fe14000bbdc0612559f1c9d5
Paulo Reis <prsc.reis@gmail.com>
2025-09-30 18:45:33 -0400

    Additional categories

--- projects/ripe/index.js
+++ projects/ripe/index.js
@@ -5,10 +5,13 @@ const USDC_CONTRACT = '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913'
 const GREEN_LP_CONTRACT = '0xd6c283655b42fa0eb2685f7ab819784f071459dc'
 const RIPE_CONTRACT = '0x2A0a59d6B975828e781EcaC125dBA40d7ee5dDC0'
 const RIPE_GREEN_LP_CONTRACT = '0x2aEf3eE3Eb64B7EC0B4ef57BB7E004747FE87eFc'
+const RIPE_WETH_LP_CONTRACT = '0x765824aD2eD0ECB70ECc25B0Cf285832b335d6A9'
 const ENDAOMENT_CONTRACT = '0x14F4f1CD5F4197DB7cB536B282fe6c59eACfE40d'
+const RIPE_GOV_CONTRACT = '0xe42b3dC546527EB70D741B185Dc57226cA01839D'
+const LEDGER_CONTRACT = '0x365256e322a47Aa2015F6724783F326e9B24fA47'
 
 const ONE_HUNDRED = BigInt(10000)
-
+const RIPE_MAX_SUPPLY = BigInt(1000000000) * BigInt(10) ** BigInt(18)
 
 async function getGreenLpValues(api, address){
   const [usdcBalance, greenBalance, greenLpSupply, addressLp] = await Promise.all([
@@ -52,9 +55,11 @@ async function getPairs(){
   const nonSpAssets = assetsArray.filter(a => a.vaultId > 2)
 
   const assetVaultPairs = [
-    ...assetsArray.map(({ tokenAddress, vaultAddress }) => [tokenAddress, vaultAddress]), 
-    ...nonSpAssets.map(({ tokenAddress}) => [tokenAddress, stabilityPoolAddress]),
-    [GREEN_CONTRACT, SGREEN_CONTRACT]
+    ...nonSpAssets.map(({ tokenAddress, vaultAddress }) => [tokenAddress, vaultAddress]), 
+    ...nonSpAssets.map(({ tokenAddress }) => [tokenAddress, stabilityPoolAddress]),
+    [GREEN_CONTRACT, SGREEN_CONTRACT],
+    [USDC_CONTRACT, ENDAOMENT_CONTRACT],
+    [GREEN_CONTRACT, ENDAOMENT_CONTRACT]
   ];
 
   return {assetVaultPairs, stabilityPoolAddress};
@@ -75,7 +80,7 @@ async function tvl(api) {
   
   const underlyingCalls = pairs.map(([token], index) => {
     return api.call({
-        abi: "function getUnderlyingData(address, uint256) view returns (tuple(address asset, uint256 amount, uint256 usdValue, uint256 legId, address legoAddr, string legoDesc))",
+      abi: "function getUnderlyingData(address, uint256) view returns (tuple(address asset, uint256 amount, uint256 usdValue, uint256 legId, address legoAddr, string legoDesc))",
       target: LEGO_TOOLS_CONTRACT,
       params: [token, balances[index]],
         
@@ -90,44 +95,70 @@ async function tvl(api) {
   });
 
   await getGreenLpValues(api, stabilityPoolAddress)
+  await getGreenLpValues(api, ENDAOMENT_CONTRACT)
 }
 
-async function staking(api){
-  await getGreenLpValues(api, ENDAOMENT_CONTRACT)
-
-  const [endaoRipe, endaoRipeGreenLp, usdcEndao, greenEndao] = await Promise.all([
+async function vesting(api){
+  const [endaoRipe, totalSupply] = await Promise.all([
     api.call({
       abi: 'erc20:balanceOf',
       target: RIPE_CONTRACT,
       params: [ENDAOMENT_CONTRACT]
     }),
     api.call({
+      abi: 'erc20:totalSupply',
+      target: RIPE_CONTRACT
+    })
+  ])
+
+  const total = RIPE_MAX_SUPPLY + BigInt(endaoRipe) - BigInt(totalSupply)
+  api.add(RIPE_CONTRACT, total)
+}
+
+async function staking(api){
+  const ripeStaked = await api.call({
+      abi: 'erc20:balanceOf',
+      target: RIPE_CONTRACT,
+      params: [RIPE_GOV_CONTRACT]
+    })
+  
+  api.add(RIPE_CONTRACT, ripeStaked)
+}
+
+async function pool2(api){
+  const [ripeWethLp, endaoRipeGreenLp] = await Promise.all([
+    api.call({
+      abi: 'erc20:balanceOf',
+      target: RIPE_WETH_LP_CONTRACT,
+      params: [RIPE_GOV_CONTRACT]
+    }),
+    api.call({
       abi: 'erc20:balanceOf',
       target: RIPE_GREEN_LP_CONTRACT,
       params: [ENDAOMENT_CONTRACT]
     }),
-    api.call({
-      abi: 'erc20:balanceOf',
-      target: USDC_CONTRACT,
-      params: [ENDAOMENT_CONTRACT]
-    }),
-    api.call({
-      abi: 'erc20:balanceOf',
-      target: GREEN_CONTRACT,
-      params: [ENDAOMENT_CONTRACT]
-    })
   ])
   
-  api.add(RIPE_CONTRACT, endaoRipe)
+  api.add(RIPE_WETH_LP_CONTRACT, ripeWethLp)
   api.add(RIPE_GREEN_LP_CONTRACT, endaoRipeGreenLp)
-  api.add(USDC_CONTRACT, usdcEndao)
-  api.add(GREEN_CONTRACT, greenEndao)
+}
+
+async function borrowed(api){
+  const borrowed = await api.call({
+    abi: "function totalDebt() view returns (uint256)",
+    target: LEDGER_CONTRACT,
+  })
+
+  api.add(GREEN_CONTRACT, borrowed)
 }
 
 module.exports = {
-  methodology: 'RIPE TVL',
-  start: 1750961079,
+  methodology: 'Ripe Protocol',
+  start: 1754006400,
   base: {
     tvl,
+    pool2,
+    vesting,
     staking,
+    borrowed
 }}; 
