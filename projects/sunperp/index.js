3262a9453629ba99c949301726ab7c8b3373d433
waynebruce0x <waynebruce0x@gmail.com>
2025-10-01 10:24:31 +0100

    sunperp exclude cold wallets (#16503)

--- projects/sunperp/index.js
+++ projects/sunperp/index.js
@@ -10,21 +10,21 @@ async function tvl(api) {
         params: [chefContract],
     });
 
-    const wallet1 = await api.call({
-        abi: 'function balanceOf(address account) external view returns (uint256)',
-        target: ADDRESSES.tron.USDT,
-        params: ["TK8v6HPniRKYixj6Egby712nr5R3xrYYCT"],
-    });
+    // const wallet1 = await api.call({
+    //     abi: 'function balanceOf(address account) external view returns (uint256)',
+    //     target: ADDRESSES.tron.USDT,
+    //     params: ["TK8v6HPniRKYixj6Egby712nr5R3xrYYCT"],
+    // });
 
-    const wallet2 = await api.call({
-        abi: 'function balanceOf(address account) external view returns (uint256)',
-        target: ADDRESSES.tron.USDT,
-        params: ["TYRmcmmKvjL1SFQbvPQtJU1YcaXEnH8dvT"],
-    });
+    // const wallet2 = await api.call({
+    //     abi: 'function balanceOf(address account) external view returns (uint256)',
+    //     target: ADDRESSES.tron.USDT,
+    //     params: ["TYRmcmmKvjL1SFQbvPQtJU1YcaXEnH8dvT"],
+    // });
 
     api.add(ADDRESSES.tron.USDT, usdtBalance);
-    api.add(ADDRESSES.tron.USDT, wallet1);
-    api.add(ADDRESSES.tron.USDT, wallet2);
+    // api.add(ADDRESSES.tron.USDT, wallet1);
+    // api.add(ADDRESSES.tron.USDT, wallet2);
 
 }
 
