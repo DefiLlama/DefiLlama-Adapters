4eb153a9ed1537a9863e486426a8354f4c17104e
Rahul Sethuram <rahul.eth@icloud.com>
2025-10-01 14:27:03 +0400

    refactor: update TVL export structure for plasma chain

--- projects/lithos/index.js
+++ projects/lithos/index.js
@@ -1,5 +1,11 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
-const chain = 'plasma'
-const factory = '0x71a870D1c935C2146b87644DF3B5316e8756aE18...' // v2 factory address
+const { getUniTVL } = require("../helper/unknownTokens");
 
-module.exports = uniTvlExport(chain, factory)
+module.exports = {
+  misrepresentedTokens: true,
+  plasma: {
+    tvl: getUniTVL({
+      factory: "0x71a870D1c935C2146b87644DF3B5316e8756aE18",
+      useDefaultCoreAssets: true,
+    }),
+  },
+};
