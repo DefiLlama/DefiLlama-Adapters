b9f5537bddd6bb9aa62c3222f87d725a09737071
jax <>
2025-10-04 20:28:13 +0800

    Fix: Bumpin, Borrow TVL

--- projects/bumpin/index.js
+++ projects/bumpin/index.js
@@ -1,22 +1,20 @@
 const {getProvider, sumTokens2,} = require('../helper/solana')
 const {Program} = require("@coral-xyz/anchor");
 const {PublicKey} = require('@solana/web3.js');
+const idl = require('./bumpin_trade.json');
 
-async function tvl() {
+async function tvl(api) {
     const provider = getProvider()
-
-    const programId = new PublicKey('bumpinX5wdLt59DBR3eetmV6xB2W3rNGxMoTSc58ah2')
-    const idl = await Program.fetchIdl(programId, provider)
-    // idl's address maybe wrong, force update idl address
-    idl.address = "bumpinX5wdLt59DBR3eetmV6xB2W3rNGxMoTSc58ah2";
     const program = new Program(idl, provider)
 
     const poolData = await program.account.pool.all()
     const tokenData = await program.account.tradeToken.all()
-    const tokenVault = await program.account.tokenVault.all();
+    
+    // 安全检查是否存在 tokenVault 账户
+    const tokenVault = program.account.tokenVault ? await program.account.tokenVault.all() : [];
     const tokenAccounts = []
     const blacklistedTokens = []
-    
+
     poolData.forEach(({account: i}) => {
         blacklistedTokens.push(i.launcherMint.toString())
         tokenAccounts.push(i.poolVaultKey.toString())
@@ -29,15 +27,18 @@ async function tvl() {
     });
 
     const uniqueTokenAccounts = [...new Set(tokenAccounts)];
+    if (tokenVault && tokenVault.length > 0) {
+        tokenVault.forEach((data) => {
+            const mint = data.account.mintKey.toString()
+            api.add(mint, data.account.totalBorrowed)
+        })
+    }
+
     return sumTokens2({
         tokenAccounts: uniqueTokenAccounts.filter(i => i !== '11111111111111111111111111111111'),
         blacklistedTokens, 
-        api: api => {
-            tokenVault.forEach((data) => {
-                const mint = data.account.mintKey.toString()
-                api.add(mint, data.account.totalBorrowed)
-            })
-        }
+        allowError: true,
+        api
     })
 }
 
