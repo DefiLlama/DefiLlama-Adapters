203a157b5eeabf41b9b380433f58beb9885efd26
ZNSCTO <znscto@proton.me>
2025-10-06 14:23:22 +0200

    Update index.js

--- projects/gmcheap/index.js
+++ projects/gmcheap/index.js
@@ -2,10 +2,9 @@ const CHAINS = [
   'ethereum', 'base', 'optimism', 'arbitrum', 'polygon', 'avalanche', 'bsc',
   'scroll', 'mantle', 'linea', 'zksync', 'taiko', 'blast', 'mode', 'zora', 'metis',
   'cronos', 'celo', 'gnosis', 'conflux', 'ronin', 'lisk',
-  'berachain', 'kaia', 'x-layer', 'core', 'redstone', 'cyber', 'story', 'morph', 'zircuit',
-  'apechain', 'ancient8', 'degen', 'botanix', 'mezo', 'bob', 'world-chain',
+  'berachain', 'kaia', 'core', 'redstone', 'cyber', 'story', 'morph', 'zircuit',
+  'apechain', 'ancient8', 'degen', 'botanix', 'mezo', 'bob',
   'abstract', 'soneium', 'ink', 'unichain', 'plume', 'gravity', 'sonic', 'manta',
-  'polygon-zkevm',
 ];
 
 const chainExports = Object.fromEntries(CHAINS.map((c) => [c, { tvl: () => ({}) }]));
