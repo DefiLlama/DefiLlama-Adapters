2a7cf731e9c421dcb697ddb30062a3c4e6aa8c12
jim <muskdong00@gmail.com>
2025-10-07 20:43:45 +0800

    Fixed known issues

--- projects/Fil-Liquid/index.js
+++ projects/Fil-Liquid/index.js
@@ -6,14 +6,6 @@ const fil_fig_stake_contract = "0xD44bfE4523f1B2703DDE9C7dBc010Ad39EF668f7";
 const bsc_fig_stake_contract = "0xAF5dD88328d73916231a9e6a64bf0029354f5812";
 const FIL_FIG_TOKEN = "0xc87FAb479B450993E8A7b498C631AdF81f3ca5B4";
 const BSC_FIG_TOKEN = "0x2f3E415F56BA07b444bc68ED037377dE1a668D6B";
-// function tvl(api) {
-//   return api.call({
-//     abi: 'function getTVL() external view returns (uint)',
-//     target: static_contract,
-//   }).then(balance => {
-//     api.add(ADDRESSES.null, balance);
-//   });
-// }
 
 async function fil_tvl(api) {
   const filBalance = await api.call({
@@ -21,33 +13,26 @@ async function fil_tvl(api) {
     target: static_contract,
   });
   api.add(ADDRESSES.null, filBalance);
-
-  const stakeList = await api.call({
-    abi: 'function getTotalStakeList() view returns (uint256[])',
-    target: fil_fig_stake_contract,
-  });
-
-  const totalFIG = stakeList.reduce((a, b) => a + BigInt(b), 0n);
-  api.add(FIL_FIG_TOKEN, totalFIG);
 }
 
-async function tvl_bsc(api) {
+async function getFigStake(api, contract, token) {
   const stakeList = await api.call({
     abi: 'function getTotalStakeList() view returns (uint256[])',
-    target: bsc_fig_stake_contract,
+    target: contract,
   });
   const totalFIG = stakeList.reduce((a, b) => a + BigInt(b), 0n);
-  api.add(BSC_FIG_TOKEN, totalFIG);
+  api.add(token, totalFIG);
 }
 
 module.exports = {
   timetravel: false,
-  methodology: 'Get the total amount of pledge and account balance of fil in the statistical contract',
+  methodology: 'TVL counts FIL locked in the statistical contract. Staking counts FIG staked on Filecoin and BSC.',
   filecoin: {
-    tvl:fil_tvl
+    tvl:fil_tvl,
+    staking:(api) => getFigStake(api, fil_fig_stake_contract, FIL_FIG_TOKEN)
   },
   bsc: {
-    tvl:tvl_bsc
+    staking:(api) => getFigStake(api, bsc_fig_stake_contract, BSC_FIG_TOKEN)
   }
 };
 /* eslint-enable no-async-promise-executor */
