b01852d0ba4d52bf77c3c1caf187cb2e73e0c0c4
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-10-07 10:19:09 +0200

    refactor

--- projects/bracket-vaults/index.js
+++ projects/bracket-vaults/index.js
@@ -1,39 +1,20 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const CONFIG = {
-  brUSDC: '0xb8ca40E2c5d77F0Bc1Aa88B2689dddB279F7a5eb', //  USDC+ Vault
-  brETH: '0x3588e6Cb5DCa99E35bA2E2a5D42cdDb46365e71B', // ETH+ Vault
-  bravUSDC: '0x9f96E4B65059b0398B922792d3fF9F10B4567533', // Avant+ Vault
+  vaults: {
+    brUSDC: '0xb8ca40E2c5d77F0Bc1Aa88B2689dddB279F7a5eb', //  USDC+ Vault
+    brETH: '0x3588e6Cb5DCa99E35bA2E2a5D42cdDb46365e71B', // ETH+ Vault
+    bravUSDC: '0x9f96E4B65059b0398B922792d3fF9F10B4567533', // Avant+ Vault
+  },
   bracketLens: '0xcdc3a8374532Ddb762c76604f30F6a9FDd29082c',
-  WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-  USDC: ADDRESSES.ethereum.USDC
 }
 
 const tvl = async (api) => {
-  const { brUSDC, brETH, bravUSDC, bracketLens, WETH, USDC } = CONFIG
+  let { vaults, bracketLens } = CONFIG
+  vaults = Object.values(vaults)
 
-  const brUSDCTvl = await api.call({
-    abi: 'function getTVL(address) external view returns (uint256)',
-    target: bracketLens,
-    params: [brUSDC],
-  });
+  const tokens = await api.multiCall({ abi: 'address:token', calls: vaults })
+  const bals = await api.multiCall({ abi: 'function getTVL(address) view returns (uint256)', calls: vaults, target: bracketLens })
 
-  api.add(USDC, brUSDCTvl)
-
-  const brETHTvl = await api.call({
-    abi: 'function getTVL(address) external view returns (uint256)',
-    target: bracketLens,
-    params: [brETH],
-  });
-
-  api.add(WETH, brETHTvl)
-
-  const bravUSDCVtl = await api.call({
-    abi: 'function getTVL(address) external view returns (uint256)',
-    target: bracketLens,
-    params: [bravUSDC],
-  });
-
-  api.add(USDC, bravUSDCVtl)
+  api.add(tokens, bals)
 }
 
 module.exports = {
