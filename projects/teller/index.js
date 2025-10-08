c7bbfc57ce9ae0b785b8177a675761bdb7a180f5
andy <admazzola@gmail.com>
2025-10-08 10:49:31 -0400

    adding both methods to calculate collateral

--- projects/teller/index.js
+++ projects/teller/index.js
@@ -1,9 +1,24 @@
 const { gql, request } = require("graphql-request");
  
+ const { getLogs } = require('../helper/cache/getLogs')
+
 const { cachedGraphQuery } = require('../helper/cache')
 
 const sdk = require('@defillama/sdk')
 
+const { sumTokens2 } = require('../helper/unwrapLPs')
+
+
+
+const ogconfig = {
+  ethereum: { factory: '0x2551a099129ad9b0b1fec16f34d9cb73c237be8b', fromBlock: 16472616, tellerV2: '0x00182FdB0B880eE24D428e3Cc39383717677C37e', },
+  polygon: { factory: '0x76888a882a4ff57455b5e74b791dd19df3ba51bb', fromBlock: 38446227, tellerV2: '0xD3D79A066F2cD471841C047D372F218252Dbf8Ed', },
+  arbitrum: { factory: '0x71B04a8569914bCb99D5F95644CF6b089c826024', fromBlock: 108629315, tellerV2: '0x5cfD3aeD08a444Be32839bD911Ebecd688861164', },
+  base: { factory: '0x71B04a8569914bCb99D5F95644CF6b089c826024', fromBlock: 2935376, tellerV2: '0x5cfD3aeD08a444Be32839bD911Ebecd688861164', },
+  katana: { factory: '0x3D495036Dfeb1bBfCCabAC74e90e01cDD5C8E578', fromBlock: 6541271, tellerV2: '0xf7B14778035fEAF44540A0bC1D4ED859bCB28229', },
+}
+
+const blacklistedTokens = ['0x8f9bbbb0282699921372a134b63799a48c7d17fc', '0xd4416b13d2b3a9abae7acd5d6c2bbdbe25686401']
 
 
 
@@ -105,6 +120,82 @@ const poolsGraphQuery = gql`
   }
 `
 
+ 
+
+async function ogBidCollateral(api) {
+  const { factory, fromBlock, tellerV2 } = ogconfig[api.chain]
+
+  const collateralDepositedLogs = await getLogs({
+    api,
+    target: factory,
+    topics: ['0x1a7f128dbc559fb97831b7681dee32957c2917e95d1c5070da20fb89e91f9d7a'],
+    eventAbi: 'event CollateralDeposited (uint256 _bidId, uint8 _type, address _collateralAddress, uint256 _amount, uint256 _tokenId)',
+    onlyArgs: true,
+    extraKey: 'CollateralDeposited',
+    fromBlock,
+  })
+
+  const escrowLogs = await getLogs({
+    api,
+    target: factory,
+    topics: ['0xc201bfb915e3eed80ff17e013f3d88db1c51ac7fc12728fce91a2afc659128ef'],
+    eventAbi: 'event CollateralEscrowDeployed (uint256 _bidId, address _collateralEscrow)',
+    onlyArgs: true,
+    extraKey: 'CollateralEscrowDeployed',
+    fromBlock,
+  })
+
+  const repaidLogs = await getLogs({
+    api,
+    target: tellerV2,
+    topic: 'LoanRepaid(uint256)',
+    eventAbi: 'event LoanRepaid(uint256 indexed bidId)',
+    extraKey: 'LoanRepaid',
+    onlyArgs: true,
+    fromBlock,
+  })
+
+  const liquidatedLogs = await getLogs({
+    api,
+    target: tellerV2,
+    topic: 'LoanLiquidated(uint256,address)',
+    eventAbi: 'event LoanLiquidated(uint256 indexed bidId, address indexed liquidator)',
+    onlyArgs: true,
+    extraKey: 'LoanLiquidated',
+    fromBlock,
+  })
+
+  // Build set of closed bids
+  const closedBidSet = new Set()
+  repaidLogs.forEach(i => closedBidSet.add(Number(i.bidId)))
+  liquidatedLogs.forEach(i => closedBidSet.add(Number(i.bidId)))
+
+  // Build escrow map
+  const escrowMap = {}
+  escrowLogs.forEach(i => {
+    const bidId = Number(i._bidId)
+    if (closedBidSet.has(bidId)) return
+    escrowMap[bidId] = {
+      owner: i._collateralEscrow,
+      tokens: [],
+    }
+  })
+
+  // Add collateral tokens to each escrow
+  collateralDepositedLogs.forEach(i => {
+    const bidId = Number(i._bidId)
+    if (closedBidSet.has(bidId)) return
+    if (escrowMap[bidId]) {
+      escrowMap[bidId].tokens.push(i._collateralAddress)
+    }
+  })
+
+  // Sum tokens from all active escrows
+  const ownerTokens = Object.values(escrowMap).map(escrow => [escrow.tokens, escrow.owner])
+  await sumTokens2({ api, ownerTokens, blacklistedTokens, permitFailure: true })
+}
+
+
 
 
 async function bidCollateralTvl(api) {
@@ -127,10 +218,10 @@ async function bidCollateralTvl(api) {
     }
 
     // Filter for active loans only (status: Accepted)
-    const activeCollaterals = allCollaterals.filter(c => c.bid.status === 'Accepted')
+  //  const activeCollaterals = allCollaterals.filter(c => c.bid.status === 'Accepted')
 
     // Sum collateral by token
-    activeCollaterals.forEach(collateral => {
+    allCollaterals.forEach(collateral => {
       api.add(collateral.token.address, collateral.amount)
     })
 
@@ -185,17 +276,24 @@ async function poolsLenderTvl( api ) {
 async function tvl(api) {
 
 
-  //add OG collateral data
+  //add OG collateral data  
 
-    await bidCollateralTvl ( api )
+    // await ogBidCollateral ( api )
 
-    await poolsLenderTvl( api)
+     await bidCollateralTvl ( api )
+
+      await poolsLenderTvl( api)
 
 
 }
 
-async function borrowed(api) {
-  let allPools = []
+
+
+
+
+async function poolsBorrowed( api) {
+
+     let allPools = []
 
   // Fetch from v1 pools if available
   if (pools_graph_config[api.chain]) {
@@ -218,6 +316,32 @@ async function borrowed(api) {
       api.add(pool.principal_token_address, netBorrowed.toString())
     }
   })
+
+
+}
+
+async function bidBorrowed(api){
+  const tellerGraphUrl = teller_graph_config[api.chain]
+
+  const { tokenVolumes } = await cachedGraphQuery(
+    `teller/${api.chain}`,
+    tellerGraphUrl,
+    tellerGraphQuery
+  );
+
+  // Borrowed is the outstandingCapital for each principal token
+  // Use only protocol-level entries (id starts with "protocol-v2-") to avoid duplicates
+  tokenVolumes.forEach(volume => {
+    if (volume.token && volume.outstandingCapital && volume.id.startsWith('protocol-v2-') && !volume.collateralToken) {
+      api.add(volume.token.address, volume.outstandingCapital)
+    }
+  });
+
+}
+
+async function borrowed(api) {
+     await poolsBorrowed ( api )
+
 }
 
 // All chains that have either v1 or v2 pools
