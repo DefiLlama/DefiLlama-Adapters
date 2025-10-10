b56bef12988b03f0fb4b96aa34d35b7a38a9c52b
Aryioshin <aryoshin.cronos@gmail.com>
2025-10-10 08:01:21 +0200

    Update index.js

--- projects/zenfinance/index.js
+++ projects/zenfinance/index.js
@@ -1,21 +1,22 @@
 const { stakings } = require("../helper/staking");
 
+const ZENSWAP_CONTRACT = "0xEB401e50e30E770222bDeA6CA6938B237De1f3f9";
 const ZENSTAKE_CONTRACT = "0x620B2367E630430C615ccF5CA02084c11995Fe25";
 const ZENRECHARGE_CONTRACT = "0xD39e62C0FFb6653BDE0f8f456E9624BF64216126";
 const ARY_TOKEN = "0x41bc026dABe978bc2FAfeA1850456511ca4B01bc";
 
 async function tvl(api) {
   await api.sumTokens({ 
-    owners: [ZENRECHARGE_CONTRACT],
+    owners: [ZENRECHARGE_CONTRACT, ZENSWAP_CONTRACT],
     resolveLP: true,
   });
 }
 
 module.exports = {
-  methodology: "ZenFinance TVL includes ARY staked in ZenStake and ZenRecharge contracts, plus other tokens in ZenRecharge",
+  methodology: "ZenFinance TVL includes ARY staked in ZenStake, ARY staked in ZenRecharge, tokens in ZenRecharge, and CRO in ZenSwap competitions",
   start: 1726238297, // Sep 13, 2025 - ZenStake deployment date
   cronos: {
     tvl,
-    staking: stakings([ZENSTAKE_CONTRACT, ZENRECHARGE_CONTRACT], ARY_TOKEN),
+    staking: stakings(ZENSTAKE_CONTRACT, ARY_TOKEN),
   }
 };
