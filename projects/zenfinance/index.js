a542bd89246786d9e9b6628eef8120ec259550f0
Aryioshin <aryoshin.cronos@gmail.com>
2025-10-10 08:32:09 +0200

    Modify TVL function to include ZenRecharge contract
    Updated TVL calculation to include ZenRecharge and removed staking reference.

--- projects/zenfinance/index.js
+++ projects/zenfinance/index.js
@@ -1,14 +1,21 @@
-const { stakings } = require("../helper/staking");
-
 const ZENSTAKE_CONTRACT = "0x620B2367E630430C615ccF5CA02084c11995Fe25";
 const ZENRECHARGE_CONTRACT = "0xD39e62C0FFb6653BDE0f8f456E9624BF64216126";
 const ARY_TOKEN = "0x41bc026dABe978bc2FAfeA1850456511ca4B01bc";
-
 const ZENSWAP_CONTRACT = "0xEB401e50e30E770222bDeA6CA6938B237De1f3f9";
 
 async function tvl(api) {
+  // Get ARY staked in ZenStake
+  const zenStakeBalance = await api.call({
+    abi: "uint256:totalStaked",
+    target: ZENSTAKE_CONTRACT,
+  });
+  
+  // Add staked ARY from ZenStake
+  api.add(ARY_TOKEN, zenStakeBalance);
+  
+  // Add all tokens from ZenRecharge and ZenSwap
   await api.sumTokens({ 
-    owners: [ZENSWAP_CONTRACT],
+    owners: [ZENRECHARGE_CONTRACT, ZENSWAP_CONTRACT],
     resolveLP: true,
   });
 }
@@ -18,6 +25,5 @@ module.exports = {
   start: 1726238297, // Sep 13, 2025 - ZenStake deployment date
   cronos: {
     tvl,
-    staking: stakings([ZENSTAKE_CONTRACT, ZENRECHARGE_CONTRACT], ARY_TOKEN),
   }
 };
