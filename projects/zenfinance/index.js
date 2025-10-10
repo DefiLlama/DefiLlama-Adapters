1f2846a034942d091a16e32e2392271c630c8037
Aryioshin <aryoshin.cronos@gmail.com>
2025-10-10 08:16:58 +0200

    Refactor methodology and staking parameters

--- projects/zenfinance/index.js
+++ projects/zenfinance/index.js
@@ -5,6 +5,7 @@ const ZENSTAKE_CONTRACT = "0x620B2367E630430C615ccF5CA02084c11995Fe25";
 const ZENRECHARGE_CONTRACT = "0xD39e62C0FFb6653BDE0f8f456E9624BF64216126";
 const ARY_TOKEN = "0x41bc026dABe978bc2FAfeA1850456511ca4B01bc";
 
+
 async function tvl(api) {
   await api.sumTokens({ 
     owners: [ZENRECHARGE_CONTRACT, ZENSWAP_CONTRACT],
@@ -13,10 +14,10 @@ async function tvl(api) {
 }
 
 module.exports = {
-  methodology: "ZenFinance TVL includes ARY staked in ZenStake, ARY staked in ZenRecharge, tokens in ZenRecharge, and CRO in ZenSwap competitions",
+  methodology: "ZenFinance TVL includes ARY staked in ZenStake and ZenRecharge contracts, plus other tokens held by protocol contracts",
   start: 1726238297, // Sep 13, 2025 - ZenStake deployment date
   cronos: {
     tvl,
-    staking: stakings(ZENSTAKE_CONTRACT, ARY_TOKEN),
+    staking: stakings([ZENSTAKE_CONTRACT, ZENRECHARGE_CONTRACT], ARY_TOKEN),
   }
 };
