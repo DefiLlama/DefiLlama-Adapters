a1a73d00641363abaa95280029fe49e7e27926bf
murmky <184486394+murmky@users.noreply.github.com>
2025-10-13 15:56:55 +0200

    Update Kyros

--- projects/kyros/index.js
+++ projects/kyros/index.js
@@ -5,14 +5,21 @@ async function tvl() {
       tokenAccounts: [
         "CRFtzwkekKorgdTRSdvsYeqL1vEuVvwGRvweuWCyaRt3", // jitoSOL token account in kySOL Vault address
         "HzwDsHJBtuSTRx3VV6bz1R8yrLywxKgfGte7FASXU8Gd", // JTO token account in kyJTO Vault address
-        "Ct8QS77TMFF98gvN1ZXrNjGqUmdkJQACi5Xi2sCTSC7D", // KYROS account in kyKYROS Vault address
       ]
     })
+}
+
+async function staking() {
+  return sumTokens2({
+    tokenAccounts: [
+      "Ct8QS77TMFF98gvN1ZXrNjGqUmdkJQACi5Xi2sCTSC7D", // KYROS account in kyKYROS Vault address
+    ]
+  })
 }
 
 module.exports = {
     timetravel: false,
     doublecounted: true,
-    solana: { tvl },
+    solana: { tvl, staking },
     methodology: 'The TVL is calculated by summing all restaked assets.',
 };
