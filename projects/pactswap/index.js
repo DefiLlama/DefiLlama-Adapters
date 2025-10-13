ea5379335e9c48d807052c674d15643d921d8a02
odiinnn <38395645+odiinnn@users.noreply.github.com>
2025-10-13 20:05:15 +0300

    Implement Coinweb native chain integration instead of api usage

--- projects/pactswap/index.js
+++ projects/pactswap/index.js
@@ -3,7 +3,70 @@ const getPactswapConfig = async () => {
   return data
 }
 
-const PACTSWAP_API_URL = 'https://api.pactswap.io/pactswap_cm'
+const COINWEB_API_URL = 'https://api-cloud.coinweb.io/wallet'
+
+const fetchClaimsQuery = `query($contractId: String!, $firstPart: JSON!, $secondPart: JSON) {
+  shard(name: BNB) {
+    fetchClaimsStable(
+      claimFilters: {
+        keyFirstPart: $firstPart
+        keySecondPart: $secondPart
+        issuer: { FromSmartContract: $contractId }
+      }
+      offset: "0x0"
+      maxClaimsToReturn: "0x64"
+    ) {
+      claim {
+        content {
+          key
+          body
+        }
+      }
+    }
+  }
+}
+`
+
+const fetchClaims = async (contractId, firstPart, secondPart) => {
+  const data = await fetch(COINWEB_API_URL, {
+    method: 'POST',
+    body: JSON.stringify({ query: fetchClaimsQuery, variables: { contractId, firstPart, secondPart } })
+  })
+  const json = await data.json()
+  return json.data.shard.fetchClaimsStable
+}
+
+const getAllC2Orders = async (contractId) => {
+  const claims = await fetchClaims(contractId, ["ORDER_ACTIVE_INDEX"], null)
+  const orderIds = claims.map(claim => claim.claim.content.key.second_part[1])
+
+  const orders = await Promise.all(orderIds.map(async (orderId) => {
+    const order = await fetchClaims(contractId, ["ORDER_STATE"], [orderId])
+    return Array.isArray(order) ? order[0] : order
+  }))
+
+  return orders.map(order => {
+    if (!order?.claim?.content?.body) {
+      return null
+    }
+    return order.claim.content.body
+  }).filter(Boolean)
+}
+
+const getAllC1Orders = async (contractId) => {
+  const claims = await fetchClaims(contractId, ["ACTIVE_INDEX"], null)
+  const orderIds = claims.map(claim => claim.claim.content.key.second_part[1])
+  const orders = await Promise.all(orderIds.map(async (orderId) => {
+    const order = await fetchClaims(contractId, ["STATE"], [orderId])
+    return Array.isArray(order) ? order[0] : order
+  }))
+  return orders.map(order => {
+    if (!order?.claim?.content?.body) {
+      return null
+    }
+    return order.claim.content.body
+  }).filter(Boolean)  
+}
 
 const chainsConfig = {
     ethereum: 'ETH',
@@ -31,7 +94,7 @@ const tvl = async (api) => {
   for (const token of tokens) {
     const c2ContractAddress = psConfig[token].L2_CONTRACT_ADDRESS_MAKER.id;
 
-    const c2Orders = await fetch(`${PACTSWAP_API_URL}/getAllC2Orders?contractAddress=${c2ContractAddress}`).then(res => res.json())
+    const c2Orders = await getAllC2Orders(c2ContractAddress)
 
     let value = 0;
 
@@ -71,10 +134,10 @@ const tvlCoinweb = async (api) => {
     }
   }
   const allC1Orders = await Promise.all(c1OContractsIds.map(async (contractId) => {
-    return fetch(`${PACTSWAP_API_URL}/getAllC1Orders?contractAddress=${contractId}`).then(res => res.json())
+    return getAllC1Orders(contractId)
   }))
   const allC2Orders = await Promise.all(c2OContractsIds.map(async (contractId) => {
-    return fetch(`${PACTSWAP_API_URL}/getAllC2Orders?contractAddress=${contractId}`).then(res => res.json())
+    return getAllC2Orders(contractId)
   }))
   for (const order of allC1Orders.flat()) {
     cwebValue += Number(order.funds)
