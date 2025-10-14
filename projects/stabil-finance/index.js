6143967dd87959412e0c4fa81e2496e75ff047d2
schism595 <schism.fairest33@icloud.com>
2025-10-14 14:01:04 +0200

    Add missing array

--- projects/stabil-finance/index.js
+++ projects/stabil-finance/index.js
@@ -1,7 +1,7 @@
 const { getCuratorTvl } = require("../helper/curators");
 const { getUniTVL } = require("../helper/unknownTokens")
 
-const morphoVaultOwners = "0x64d7c7632C30C5a0f37a9a9970B6E42470Cb933D";
+const morphoVaultOwners = ["0x64d7c7632C30C5a0f37a9a9970B6E42470Cb933D"];
 
 module.exports = {
   methodology: "Count all assets are deposited in all vaults curated by Stabil Finance.",
