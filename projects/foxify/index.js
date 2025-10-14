e7783778b43a51ec1c5634eff4adb69e15c9a2e1
Hoc Vu <hocvv@twendeesoft.com>
2025-10-14 09:43:31 +0700

    refactor: update Foxify TVL calculations to utilize staking helper and remove unused functions

--- projects/foxify/index.js
+++ projects/foxify/index.js
@@ -1,4 +1,4 @@
-const { sumTokens2 } = require('../helper/unwrapLPs');
+const { stakings } = require('../helper/staking');
 
 // Sonic
 const contracts = [
@@ -6,43 +6,13 @@ const contracts = [
   '0x3725B740b33E75898e4e2E616E9BB519884edd37', // FoxMaxi
 ];
 
-// Arbitrum
-const arbitrumKitsuneVaultContract = '0xe5a4f22fcb8893ba0831babf9a15558b5e83446f';
-
-async function sonicTvl(api) {
-  // Fetch total supplies from both contracts
-  const supplies = await api.multiCall({
-    abi: 'erc20:totalSupply',
-    calls: contracts,
-  });
-
-  // Sum total supplies from both contracts
-  const staking = supplies.reduce((sum, supply) => sum + BigInt(supply), 0n);
-
-  api.add("coingecko:foxify", staking);
-
-  // Return TVL as a balance object
-  api.addUSDValue(staking);
-  return api.getBalances() / 1e18;
-}
-
-async function arbitrumTvl(api) {
-  // Fetch totalAssets from the Arbitrum Kitsune Vault contract
-  const totalAssets = await api.call({
-    abi: 'uint256:totalAssets',
-    target: arbitrumKitsuneVaultContract,
-  });
-
-  api.addUSDValue(Number(totalAssets));
-  return api.getBalances() / 1e6;
-}
-
 module.exports = {
   methodology: 'Counts the totalSupply of Foxify protocol tokens and totalAssets from Arbitrum contract',
   sonic: {
-    tvl: sonicTvl,
+    tvl: () => ({}),
+    staking: stakings(contracts, '0x261dfa2528dfa19011f10b168c856e02baaf0eb6')
   },
   arbitrum: {
-    tvl: arbitrumTvl,
+    tvl: stakings(['0xe5a4f22fcb8893ba0831babf9a15558b5e83446f'], '0xaf88d065e77c8cC2239327C5EDb3A432268e5831')
   },
 };
