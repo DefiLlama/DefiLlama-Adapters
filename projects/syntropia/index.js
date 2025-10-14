bda09c68f266a0e5e91b2e4981896ce0ce0138db
Internal Msg <lildev@protonmail.com>
2025-10-14 15:03:36 +0100

    Use erc4626 helper to get Syntropia TVL

--- projects/syntropia/index.js
+++ projects/syntropia/index.js
@@ -1,11 +1,11 @@
-const { get } = require("../helper/http");
-const ADDRESSES = require("../helper/coreAssets.json");
-
-async function tvl(api) {
-  const data = await get("https://syntropia.ai/api/balances/aum");
-  const tvl = data.aum;
-
-  api.add(ADDRESSES.ethereum.USDC, tvl * 1e6);
+const config = {
+  ethereum: {
+    vaults: [
+      "0x1b2cb79a4564206f53ba80b4d780f251b4ae6765",
+      "0xd17049ed25d8f99fe3bfd10cef2263da9995cfd8",
+      "0x8df3deba711ae4a9af16cbca5e4fbb1402f036d5"
+    ]
+  }
 }
 
 module.exports = {
@@ -14,7 +14,11 @@ module.exports = {
   methodology: `
 The methodology for calculating TVL includes value of all assets in Syntropia strategies expressed in USDC
   `.trim(),
-  ethereum: {
-    tvl,
-  },
 };
+
+
+Object.keys(config).forEach((chain) => {
+  module.exports[chain] = {
+    tvl: (api) => api.erc4626Sum({ calls: config[chain].vaults, tokenAbi: 'asset', balanceAbi: 'totalAssets' })
+  }
+})
