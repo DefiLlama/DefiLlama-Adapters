b3645cb844a17218ed93c01e08ee21a40e7cda3f
JaceLabs <alienchaintoken@gmail.com>
2025-10-15 12:41:08 +0800

    Update index.js

--- projects/GateSwap/index.js
+++ projects/GateSwap/index.js
@@ -1,3 +1,15 @@
-const { uniTvlExport } = require('../helper/unknownTokens')
+const { getUniTVL } = require("../helper/unknownTokens")
 
-module.exports = uniTvlExport('gatelayer', '0xaD8d59f3e026c02Aed0DAdFB46Ceca127030DFa2', {  hasStablePools: false‌ })
+const config = {
+  gatelayer: '0xaD8d59f3e026c02Aed0DAdFB46Ceca127030DFa2'
+}
+
+module.exports = {
+  misrepresentedTokens: true,
+}
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = {
+    tvl: getUniTVL({ factory: config[chain], useDefaultCoreAssets: true }),
+  }
+})
