63e616478bfc936eca14f134c9d590e043dcce2a
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-10-15 21:06:16 +0200

    Change import path for sumTokensExport

--- projects/hemi-locking/index.js
+++ projects/hemi-locking/index.js
@@ -1,4 +1,4 @@
-const { sumTokensExport } = require('@defillama/sdk/build/generalUtil')
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 const HEMI_TOKEN = '0x99e3dE3817F6081B2568208337ef83295b7f591D'
 const VE_ESCROW  = '0x371d3718D5b7F75EAb050FAe6Da7DF3092031c89'
