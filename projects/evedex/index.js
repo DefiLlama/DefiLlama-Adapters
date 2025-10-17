4347bd890be661fe1a5a3f93d6769bdaf560a857
shcube <shandin.va@gmail.com>
2025-10-17 14:54:39 +0300

    fix: recalc using contracts

--- projects/evedex/index.js
+++ projects/evedex/index.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const ADDRESSES = require('../helper/coreAssets.json')
+const ADDRESSES = require('../helper/coreAssets.json');
+const { sumTokens2 } = require('../helper/unwrapLPs');
 
-const tvl = async (api) => {
-    const target = ADDRESSES.eventum.USDT;
-    const totalSupply = await api.call({
-        abi: 'erc20:totalSupply',
-        target,
-    });
-    api.add(target, totalSupply);
-    return api.getBalances();
-};
+const tokens = [ADDRESSES.eventum.USDT];
+const owners = [
+  '0x1DC14e4261eCd7747Cbf6D2C8538a73371405D76', 
+  '0x5e023c31E1d3dCd08a1B3e8c96f6EF8Aa8FcaCd1', 
+  '0x77075c627e51145d54e4EDD54Afa169DA7ff8A17', 
+  '0x026968b5cED079ECCD6CC78f35a5Dfddc13F9Af8', 
+  '0x0a9591c64Fd9e8C1f9A81DB1B668a5f211b5735A'
+];
 
 module.exports = {
-  eventum: { tvl },
+    eventum: {
+        tvl: (api) => sumTokens2({ api, tokens, owners })
+    }
 }
