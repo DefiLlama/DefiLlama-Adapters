fc55530a3aae2dc01a263b44d1f830634c1d0876
Bearetta Bera <bearetta@berachain.com>
2025-10-20 18:37:30 +0200

    feat(berachain): add bend protocol

---
+++ projects/berachain-bend/index.js
@@ -0,0 +1,64 @@
+const { berachain } = require("../beracana");
+const abi = require("../helper/abis/berachain-bend.json");
+const { sumTokens2 } = require("../helper/unwrapLPs");
+
+
+const MORPHO_BLUE_ADDRESS = "0x24147243f9c08d835C218Cda1e135f8dFD0517D0";
+
+const NULL_ADDRESS = "0x0000000000000000000000000000000000000000"
+
+const getWhitelistedVaults = async (api) => {
+    return ["0x30BbA9CD9Eb8c95824aa42Faa1Bb397b07545bc1"]
+}
+
+const getMarketFromWhitelistedVaults = async (api) => {
+    const whitelistedVaults = await getWhitelistedVaults();
+
+
+    const withdrawQueueLengths = await api.multiCall({ calls: whitelistedVaults, abi: abi.metaMorphoFunctions.withdrawQueueLength })
+
+    const marketResults = await Promise.all(
+        withdrawQueueLengths.map(async (len, idx) => {
+            const withdrawQueueIdxs = [...Array(Number(len)).keys()];
+
+            const a = await api.multiCall({
+                target: whitelistedVaults[idx],
+                calls: withdrawQueueIdxs,
+                abi: abi.metaMorphoFunctions.withdrawQueue
+            });
+
+            return a;
+        })
+    );
+
+    const flattened = marketResults.flat();
+    const markets = [...new Set(flattened)];
+
+    return markets;
+
+}
+
+const tvl = async (api) => {
+    const markets = await getMarketFromWhitelistedVaults(api)
+    const marketInfos = await api.multiCall({ target: MORPHO_BLUE_ADDRESS, calls: markets, abi: abi.morphoBlueFunctions.idToMarketParams })
+    const tokens = [...new Set(marketInfos.flatMap(({ collateralToken, loanToken }) => [collateralToken, loanToken]))].filter((token => token != NULL_ADDRESS))
+    return sumTokens2({ api, owner: MORPHO_BLUE_ADDRESS, tokens, chain: berachain })
+}
+
+const borrowed = async (api) => {
+    const markets = await getMarketFromWhitelistedVaults(api)
+    const marketInfos = await api.multiCall({ target: MORPHO_BLUE_ADDRESS, calls: markets, abi: abi.morphoBlueFunctions.idToMarketParams })
+    const marketDatas = await api.multiCall({ target: MORPHO_BLUE_ADDRESS, calls: markets, abi: abi.morphoBlueFunctions.market })
+
+    marketDatas.forEach((data, idx) => {
+        const { _, loanToken } = marketInfos[idx];
+        api.add(loanToken, data.totalBorrowAssets);
+    });
+}
+
+
+module.exports = {
+    berachain: {
+        tvl, borrowed
+    }
+};
