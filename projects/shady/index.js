220d48fa25a263f772a629dfeed8b4e1f2c110f1
shadylabs <shadylabsprivacy@gmail.com>
2025-10-21 21:02:49 +0700

    fix: update Shady adapter (set timetravel false & export pool2)

--- projects/shady/index.js
+++ projects/shady/index.js
@@ -3,18 +3,18 @@ const { sumTokens2 } = require('../helper/solana')
 const owner = '6WEsL1dvUQbTwjtMvKvZZKqv4GwG6b9qfCQSsa4Bpump' // Shady program ID
 const pair = 'DndG3sqpGs6s5yJDaubCna6hu877D6HfQCwcdKUuTacC' // Raydium pair
 
+async function pool2(api) {
+  return sumTokens2({
+    api,
+    owner,
+    tokens: [
+      'So11111111111111111111111111111111111111112', // SOL
+      pair, // Raydium LP
+    ],
+  })
+}
+
 module.exports = {
-  timetravel: true,
-  solana: {
-    tvl: async (api) => {
-      return sumTokens2({
-        api,
-        owner,
-        tokens: [
-          'So11111111111111111111111111111111111111112', // SOL
-          pair, // Raydium liquidity pool
-        ],
-      })
-    },
-  },
+  timetravel: false,
+  solana: { pool2 },
 }
