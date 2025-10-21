64383c4d20c4c7b64439c3c3999464b6d2c3e40a
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2025-10-21 14:14:44 +0200

    Update index.js

--- projects/yieldbricks/index.js
+++ projects/yieldbricks/index.js
@@ -5,17 +5,17 @@ const { sumTokensExport } = require('../helper/unwrapLPs')
 
 const arbConfig = {
     chain: 'arbitrum',
-    contracts: [ADDRESSES.arbitrum.YBR],
+    contracts: ['0x11920f139a3121c2836e01551d43f95b3c31159c'],
     stakingContracts: ['0x7436750e80bB956C6488A879D573cA417D6712A2', '0x80EF7E080EfC299cd6a7Ed8341273d935252c896'],
     lpContracts: ['0xb18e2e8B2f6C4f3f2e5afc1229d9d7654B0DdAA3'],
-    lpAddresses: [ADDRESSES.arbitrum.YBR, ADDRESSES.arbitrum.USDT]
+    lpAddresses: ['0x11920f139a3121c2836e01551d43f95b3c31159c', ADDRESSES.arbitrum.USDT]
 };
 
 const ethConfig = {
     chain: 'ethereum',
-    contracts: [ADDRESSES.ethereum.YBR],
+    contracts: ['0x9d9535Dae62F5f12aB83F1183DCa1eAd244b0DB3'],
     lpContracts: ['0xe81940eCdEFc9464082B51ACE7ADeD83a1dC1EFc'],
-    lpAddresses: [ADDRESSES.ethereum.YBR, ADDRESSES.ethereum.WETH]
+    lpAddresses: ['0x9d9535Dae62F5f12aB83F1183DCa1eAd244b0DB3', ADDRESSES.ethereum.WETH]
 };
 
 const tvl = (config) => async (api) => {
