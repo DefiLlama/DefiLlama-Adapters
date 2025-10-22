d184cd66f2de4d704d9b96773f37a4547c3b5ecb
Irfandi D Vendy <dlacia.vendy@gmail.com>
2025-10-22 16:18:28 +0700

    feat: add union chain, use sdk query, new tvl method

--- projects/escher/index.js
+++ projects/escher/index.js
@@ -1,13 +1,15 @@
-const { queryContract } = require("../helper/chain/cosmos");
+const { queryContract, queryV1Beta1 } = require("../helper/chain/cosmos");
 const CORE_ASSETS = require('../helper/coreAssets.json');
 
-const consts = {
+const config = {
   EBABY_CONTRACT: 'bbn1m7zr5jw4k9z22r9ajggf4ucalwy7uxvu9gkw6tnsmv42lvjpkwasagek5g',
+  U_DELEGATOR: 'union19ydrfy0d80vgpvs6p0cljlahgxwrkz54ps8455q7jfdfape7ld7quaq69v',
+  U_COINGECKO: 'coingecko:union-2'
 }
 
 async function eBabyTVL(api) {
   const data = await queryContract({
-    contract: consts.EBABY_CONTRACT,
+    contract: config.EBABY_CONTRACT,
     chain: api.chain,
     data: { staking_liquidity: {} },
   });
@@ -20,20 +22,19 @@ async function eBabyTVL(api) {
 }
 
 async function eUTVL(api) {
-  const lcd = 'https://rest.union.build';
-  const delegator = 'union19ydrfy0d80vgpvs6p0cljlahgxwrkz54ps8455q7jfdfape7ld7quaq69v';
-
-  const res = await fetch(`${lcd}/cosmos/staking/v1beta1/delegations/${delegator}`);
-  const json = await res.json();
-  const delegations = Array.isArray(json?.delegation_responses) ? json.delegation_responses : [];
+  const data = await queryV1Beta1({
+    chain: api.chain,
+    url: `staking/v1beta1/delegations/${config.U_DELEGATOR}`
+  });
+  const delegations = Array.isArray(data?.delegation_responses) ? data.delegation_responses : [];
 
   let tvlAtomic = 0;
   for (const d of delegations) {
     tvlAtomic += Number(d?.balance?.amount || 0);
   }
+  tvlAtomic /= 10 ** 18;
 
-  const token = CORE_ASSETS.ethereum.U;
-  api.add(token, tvlAtomic);
+  api.add(config.U_COINGECKO, tvlAtomic, { skipChain: true });
 }
 
 module.exports = {
@@ -41,7 +42,7 @@ module.exports = {
     methodology: 'TVL counts the tokens that are locked in the Escher staking hub',
     tvl: eBabyTVL,
   },
-  ethereum: {
+  union: {
     methodology: 'TVL counts the token of each delegator',
     tvl: eUTVL,
   },
