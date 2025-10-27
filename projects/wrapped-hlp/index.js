754b082b7b78d251f5987ef70254e6c912b4b35f
kamilstakingrewards <kamil@stakingrewards.com>
2025-10-27 10:56:06 +0100

    WHLP TVL export token update no.2

--- projects/wrapped-hlp/index.js
+++ projects/wrapped-hlp/index.js
@@ -2,7 +2,6 @@ const { getConfig } = require("../helper/cache")
 
 const WHLP_VAULT = '0x1359b05241cA5076c9F59605214f4F84114c0dE8'
 const CHAIN_ID_STR = '999'
-const WHYPE_TOKEN = '0x5555555555555555555555555555555555555555'
 
 const tvl = async (api) => {
   const data = await getConfig(
@@ -15,19 +14,17 @@ const tvl = async (api) => {
   let totalUsd = 0
   for (const pos of Object.values(strat)) totalUsd += Number(pos.valueInBase || 0)
 
-  const priceData = await fetch(
-    `https://coins.llama.fi/prices/current/hyperliquid:${WHYPE_TOKEN}`
-  ).then(r => r.json())
-  const whypePrice = priceData?.coins?.[`hyperliquid:${WHYPE_TOKEN}`]?.price
-  if (!whypePrice) return
+  const priceData = await fetch(`https://coins.llama.fi/prices/current/hyperliquid:${WHLP_VAULT}`).then(r => r.json())
+  const whlpPrice = priceData?.coins?.[`hyperliquid:${WHLP_VAULT}`]?.price
+  if (!whlpPrice || whlpPrice <= 0) return
 
-  const whypeAmount = BigInt(Math.round((totalUsd / whypePrice) * 1e18))
-  api.add(WHYPE_TOKEN, whypeAmount)
+  const whlpAmount = BigInt(Math.round((totalUsd / whlpPrice) * 1e6))
+  api.add(WHLP_VAULT, whlpAmount)
 }
 
 module.exports = {
   hyperliquid: { tvl },
   misrepresentedTokens: true,
   methodology:
-    "The total USD value of assets in strategy positions held by the WHLP vault.",
+    "The total USD value of assets in strategy positions held by the wHLP vault.",
 }
