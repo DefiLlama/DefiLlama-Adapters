3e159804bd72cc8e80ba99097a4f53af0c2dd853
iris112 <nglayi99@gmail.com>
2025-10-27 20:57:30 +0900

    feat: TaoFi uni v3 pool's TVL adapter

--- projects/taofi/index.js
+++ projects/taofi/index.js
@@ -1,26 +1,5 @@
-const USDC_TOKEN_CONTRACT = '0xB833E8137FEDf80de7E908dc6fea43a029142F20';
-const WTAO_TOKEN_CONTRACT = '0x9Dc08C6e2BF0F1eeD1E00670f80Df39145529F81';
-const POOL = '0x6647dcbeb030dc8E227D8B1A2Cb6A49F3C887E3c'
+const { uniV3Export } = require("../helper/uniswapV3");
 
-async function tvl(api) {
-  const usdcBalance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: USDC_TOKEN_CONTRACT,
-    params: [POOL],
-  });
-  const wtaoBalance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: WTAO_TOKEN_CONTRACT,
-    params: [POOL],
-  });
-
-  api.add(USDC_TOKEN_CONTRACT, usdcBalance)
-  api.add(WTAO_TOKEN_CONTRACT, wtaoBalance)
-}
-
-module.exports = {
-  methodology: 'counts the number of USDC/sTAO tokens of the uni v3 pool.',
-  "bittensor_evm_mainnet": {
-    tvl,
-  },
-}; 
+module.exports = uniV3Export({
+  bittensor_evm_mainnet: { factory: "0x20D0Cdf9004bf56BCa52A25C9288AAd0EbB97D59", fromBlock: 5618527, },
+}); 
