386ee7ac678b4dc1e879234de597174dd8f88227
Eden <noat.eth@gmail.com>
2025-10-29 20:50:44 +0700

    remove loop address tvl

--- projects/stream/index.js
+++ projects/stream/index.js
@@ -1,4 +1,6 @@
 const abi = require("./abi.js");
+const { getLogs2 } = require('../helper/cache/getLogs');
+
 const vaults = Object.values({
   STREAMUSD_WRAPPER_CONTRACT: "0x6eAf19b2FC24552925dB245F9Ff613157a7dbb4C",
   STREAMBTC_WRAPPER_CONTRACT: "0x05F47d7CbB0F3d7f988E442E8C1401685D2CAbE0",
@@ -6,12 +8,52 @@ const vaults = Object.values({
   STREAMEUR_WRAPPER_CONTRACT: "0xDCFd98A5681722DF0d93fc11b9205f757576a427",
 })
 
+const xTokens = Object.values({
+  xUSD: "0xE2Fc85BfB48C4cF147921fBE110cf92Ef9f26F94",
+  xBTC: "0x12fd502e2052CaFB41eccC5B596023d9978057d6",
+  xETH: "0x7E586fBaF3084C0be7aB5C82C04FfD7592723153",
+  xEUR: "0xc15697f61170Fc3Bb4e99Eb7913b4C7893F64F13",
+})
 
+const StakeEvent = 'event Stake(address indexed account, uint256 amount, uint256 round)';
+const UnStakeEvent = 'event Unstake(address indexed account, uint256 amount, uint256 round)';
+
+// this address from team did loop to manipulate tvl, we will remove all deposit from this address
+// these wallets deposit some initial USDC, WBTC, ETH on ethereum to mint xUSD, xBTC, xETH
+// they bridge xUSD, xBTC, xETH to other chains
+// they used xUSD, xBTC, xETH as collateral to borrow USDT, USDC, deUSD
+// they use swap all borrowed tokens to USDC, WBTC, ETH, deposit back to xUSD, xBTC, xETH
+const IgnoreWallets = [
+  String('0x1597E4B7cF6D2877A1d690b6088668afDb045763').toLowerCase(),
+];
+const FromBlock = 21870476;
 
 async function tvlEth(_, _1, _2, { api }) {
   const bals = await api.multiCall({  abi: abi.totalSupply, calls: vaults})
-  const tokens = await api.multiCall({  abi: abi.asset, calls: vaults})
-  api.addTokens(tokens, bals)
+  const assets = await api.multiCall({  abi: abi.asset, calls: vaults})
+  
+  for (let i = 0; i < vaults.length; i++) {
+    const totalBalance = Number(bals[i]);
+    
+    // remove deposit from team wallets
+    let teamDeposit = 0;
+    const stakeEvents = await getLogs2({ api, target: xTokens[i], fromBlock: FromBlock, eventAbi: StakeEvent, skipCache: true });
+    const unstakeEvents = await getLogs2({ api, target: xTokens[i], fromBlock: FromBlock, eventAbi: UnStakeEvent, skipCache: true  });
+    for (const log of stakeEvents) {
+      if (IgnoreWallets.includes(String(log.account).toLowerCase())) {
+        teamDeposit += Number(log.amount);
+      }
+    }
+    for (const log of unstakeEvents) {
+      if (IgnoreWallets.includes(String(log.account).toLowerCase())) {
+        teamDeposit -= Number(log.amount);
+      }
+    }
+    
+    const balance = teamDeposit > 0 ? totalBalance - teamDeposit : totalBalance;
+    
+    api.addToken(assets[i], balance)
+  }
 }
 
 
