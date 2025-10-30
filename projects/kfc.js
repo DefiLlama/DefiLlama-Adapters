ac42e8f92824d221d2589ac3924c5e564432d398
muhammad abubakar <81152955+muhammadabubakar7526@users.noreply.github.com>
2025-10-30 15:04:56 +0500

    Fix Kaspa Finance (KFC) adapter: correct TVL, add name & logo

--- projects/kfc.js
+++ projects/kfc.js
@@ -1,7 +1,31 @@
-const { uniV3Export } = require("./helper/uniswapV3")
+const { post } = require("./helper/http") 
 
-const factory = '0x09df701f1f5df83a3bbef7da4e74bb075199d6a4' // same on all chains
+// KaspaFinance Mainnet Subgraph endpoint
+const SUBGRAPH_URL = 'https://graph.kaspafinance.io/subgraphs/name/mainnet-kaspa-finance-exchange-v3'
+const QUERY = `
+{
+  factory(id: "0x09Df701f1f5df83A3BbEf7DA4e74Bb075199d6A4") {
+    totalValueLockedUSD
+  }
+}
+`
+async function tvl() {
+  try {
+    const res = await post(SUBGRAPH_URL, { query: QUERY })
+    const tvlUsd = Number(res.data?.factory?.totalValueLockedUSD ?? 0)
+    return { tether: tvlUsd } 
+  } catch (e) {
+    console.error('Subgraph TVL fetch failed:', e)
+    return {}
+  }
+}
 
-module.exports = uniV3Export({
-  kasplex: { factory, fromBlock: 484634, }
-})
+module.exports = {
+  kasplex: {
+    tvl,
+    name: 'Kaspa Finance V3 (KFC)',
+    logo: 'https://app.kaspafinance.io/images/chains/0xf87e587ab945f7b111329a6ace6dc497d34f098b.png',
+  },
+  methodology:
+    'TVL is fetched directly from KaspaFinance’s official subgraph, which aggregates the total USD value of all liquidity in Uniswap V3-style pools deployed by the factory.',
+}
