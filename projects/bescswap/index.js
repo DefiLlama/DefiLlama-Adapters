673a4507f0aa6da82ceb7181a07218b16b843d96
BESC LLC <support@besc.services>
2025-10-31 20:26:18 -0400

    Update index.js

--- projects/bescswap/index.js
+++ projects/bescswap/index.js
@@ -4,5 +4,4 @@ const { uniTvlExport } = require('../helper/unknownTokens')
 const FACTORY_ADDRESS = '0x20EE72D1B7E36e97566f31761dfF14eDc35Fbf22'
 const ROUTER_ADDRESS  = '0x2600E57E2044d62277775A925709af0047c28Eb7'
 
-// Export standard Uniswap-style TVL helper for BESC Hyperchain
 module.exports = uniTvlExport('besc', FACTORY_ADDRESS)
