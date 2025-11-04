7b956789a57ebbfe92a120d3596dcecda1bd32f4
Martin PM <martin@hemi.cash>
2025-11-04 13:04:41 +0300

    Cleaned the codebase a bit more

--- projects/luckypeaches/index.js
+++ projects/luckypeaches/index.js
@@ -1,17 +1,10 @@
-const sdk = require('@defillama/sdk')
-const { aaveExports } = require("../helper/aave");
+const { aaveExports } = require('../helper/aave')
 
-const config = {
-  hemi: ["0x986b04d0a228b8cB81E236F9Add85e43758F21B2"],
+module.exports = {
+  hemi: aaveExports(
+    'hemi',
+    undefined,
+    undefined,
+    ['0x986b04d0a228b8cB81E236F9Add85e43758F21B2']
+  ),
 }
-
-const data = {};
-Object.keys(config).forEach((chain) => {
-  const chainExports = config[chain].map((address) => aaveExports(chain, undefined, undefined, [address]))
-  data[chain] = {
-    tvl: sdk.util.sumChainTvls(chainExports.map(i => i.tvl)),
-    borrowed: sdk.util.sumChainTvls(chainExports.map(i => i.borrowed))
-  }
-});
-
-module.exports = data;
