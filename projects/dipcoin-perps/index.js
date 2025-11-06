2317409dbacd1b9e70c15de88309a22baaa60c34
alan <every_ding@hotmail.com>
2025-11-06 12:17:44 +0800

    add tvl、volume、fee three meters for dipcoin spot and dipcoin perps. and combine dipcoin spot and dipcoin perps into one dipcoin group.

--- projects/dipcoin-perps/index.js
+++ projects/dipcoin-perps/index.js
@@ -1,22 +1,25 @@
-const { get } = require("../helper/http");
-const { getConfig } = require('../helper/cache');
-const ADDRESSES = require('../helper/coreAssets.json');
+const ADDRESSES = require('../helper/coreAssets.json')
+const sui = require("../helper/chain/sui");
+
+const SUI_BANK_ID =
+  "0x3cc2bfbe6b9dc346f3f27a47b4b0c9eaaf0143c0c704726a1513a1e8c5d9a4c1";
 
 async function suiTvl(api) {
-  const tvl = (await getConfig('dipcoin/perps-sui', 'https://gray-api.dipcoin.io/api/perp-market-api/tvl'))?.data?.tvl;
-  api.add(ADDRESSES.sui.USDC, tvl/1e12);
+  const object = await sui.getObject(SUI_BANK_ID);
+  const usdcAmount = object.fields.coin_balance;
+
+  // div by 1e6 as usdc coin has 6 precision
+  api.add(ADDRESSES.sui.USDC, usdcAmount);
 }
 
-
 module.exports = {
+  sui: {
+    tvl: suiTvl
+  },
   hallmarks: [
     ['2025-10-15', "Launched the Perpetual Mainnet (v1.0)."],
     ['2025-10-16', "Listed perpetual contracts for BTC, ETH, and SUI."],
     ['2025-10-29', "Upgraded to Mainnet v1.1, introducing Take Profit/Stop Loss and margin management features."],
     ['2025-10-31', "Listed perpetual contracts for SOL, BNB, and XRP."],
   ],
-  sui: {
-    tvl: suiTvl
-  },
 }
-
