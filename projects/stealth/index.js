c7cbd4a05a31afecf3714aa92d6b3f13762adf5f
frostyNinja <35054303+frostyNinja@users.noreply.github.com>
2025-11-06 21:39:42 +0500

    Update index.js

--- projects/stealth/index.js
+++ projects/stealth/index.js
@@ -1,9 +1,11 @@
-const { getUniTVL } = require('../helper/getUniSubgraphTvl')
+const { getUniTVL } = require('../helper/unknownTokens')
+
+const factory = '0x6ff746cf0ab3f0a3643f4bf9cd900837f8c6f7b5'
 
 module.exports = {
-  methodology: `AVAX, USDC, USDT, allocated in LP`,
+  methodology: `AVAX, USDC, USDT allocated in LP`,
   misrepresentedTokens: true,
-  avax:{
-    tvl: getChainTvl("https://dex.stealth.live/data"),
+  metis:{
+    tvl: getUniTVL({ factory, useDefaultCoreAssets: true }),
   }
 }
