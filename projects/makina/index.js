466b227d30e1d68b1178e451b339c7a17533407b
Hugo Masclet <hugo@makina.finance>
2025-11-06 09:32:29 +0100

    Rename Makina Finance to Makina

--- projects/makina/index.js
+++ projects/makina/index.js
@@ -1,7 +1,7 @@
 // https://docs.makina.finance/
 // https://app.makina.finance/
 
-// Makina Finance TVL Adapter - External Pricing Version
+// Makina TVL Adapter - External Pricing Version
 const DUSD_TOKEN = '0x1e33e98af620f1d563fcd3cfd3c75ace841204ef';
 const DETH_TOKEN = '0x871ab8e36cae9af35c6a3488b049965233deb7ed';
 const DBIT_TOKEN = '0x972966bcc17f7d818de4f27dc146ef539c231bdf';
