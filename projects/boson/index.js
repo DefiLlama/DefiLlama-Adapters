9f25392b5846f8bf660a329f75675636039f9b25
zajck <klemen@impressieve.com>
2025-11-07 13:03:27 +0100

    Calculate TVL across all chains

--- projects/boson/index.js
+++ projects/boson/index.js
@@ -1,17 +1,109 @@
 const { staking } = require("../helper/staking");
+const { MaxUint256 } = require("ethers");
+
+const {
+    getVoidedOffers,
+    getOffers,
+    getCommits,
+    getReservedRanges,
+    getEncumberedFunds,
+} = require("./utils");
+
+async function tvl(api) {
+  // Currently Encumbered funds. 
+  // Includes all buyer commitments, seller deposits and dispute resolution fees for active exchanges for both Fixed Price and Price Discovery offers.
+  // It also includes all additional commits as result of Sequential Commitments
+  const tokenBalances = await getEncumberedFunds(api);
+
+  // Listed offers with uncommitted quantity
+  const voidedOffers = await getVoidedOffers(api);
+  console.log("#of voided offers:", voidedOffers.length);
+
+  const activeOffers = await getOffers(api, voidedOffers);
+  console.log("#of active offers:", activeOffers.length);
+
+  // Ignore Price Discovery offers. Their price is unknown until commitment. Its contribution to TVL is included in `getEncumberedFunds`.
+  // Work only with Fixed Price offers from here on.
+  const fixedPriceOffers = activeOffers.filter((i) => i.offer?.priceType !== 1n); // priceType 1 = Price Discovery
+  console.log("#of fixed price offers:", fixedPriceOffers.length);
+
+  // Get the existing commits to avoid double counting
+  const commitsByOffer = await getCommits(api);
+
+  // Handle first the offers with upfront known quantityAvailable
+  const limitedOffers = fixedPriceOffers.filter((i) => i.offer?.quantityAvailable !== MaxUint256); // MaxUint256 indicates unlimited quantityAvailable
+  for (const offer of limitedOffers) {
+    const offerId = BigInt(offer.offerId);
+    // Uncommitted quantity is total quantityAvailable minus already committed quantity
+    // No need to check for reserved ranges since they do not affect the quantityAvailable
+    const uncommittedQuantity = BigInt(offer.offer.quantityAvailable) - BigInt(commitsByOffer[offerId]?.length || 0);
+
+    addUncommittedQuantityToTokenBalances(offer, uncommittedQuantity, tokenBalances);
+  }
+
+  // Handle the offers with unlimited quantityAvailable
+  // Their uncommitted quantity is determined by the reserved range minus the number of commits that fall within its reserved range
+  // If there are any additional commits outside the reserved range, belonging to the same offer, they are included in TVL in `getEncumberedFunds` already
+  const unlimitedOffers = fixedPriceOffers.filter((i) => i.offer?.quantityAvailable === MaxUint256);
+  const reservedRanges = await getReservedRanges(api);
+  for (const offer of unlimitedOffers) {
+    const offerId = BigInt(offer.offerId);
+    const reservedRange = reservedRanges[offerId];
+    if (reservedRange) {
+      const { startExchangeId, endExchangeId } = reservedRange;
+      // Check if any of the commits fall within the reserved range
+      // Range is inclusive of both startExchangeId and endExchangeId
+      const commitsInRange =
+        commitsByOffer[offerId]?.filter((exchangeId) => {
+          const exId = BigInt(exchangeId);
+          return exId >= startExchangeId && exId <= endExchangeId;
+        }).length || 0n;
+      const uncommittedQuantity = endExchangeId - startExchangeId + 1n - commitsInRange;
+
+      addUncommittedQuantityToTokenBalances(offer, uncommittedQuantity, tokenBalances);
+    }
+  }
+
+  console.table(tokenBalances);
+
+  api.addTokens(Object.keys(tokenBalances), Object.values(tokenBalances));
+}
+
+function addUncommittedQuantityToTokenBalances(offer, uncommittedQuantity, tokenBalances) {
+  const exchangeToken = offer.offer.exchangeToken;
+  const price = BigInt(offer.offer.price);
+  const sellerDeposit = BigInt(offer.offer.sellerDeposit);
+  const drFeeAmount = BigInt(offer.disputeResolutionTerms.feeAmount);
+  const lockedPerExchange = price + sellerDeposit + drFeeAmount;
+  const totalLocked = lockedPerExchange * uncommittedQuantity;
+
+  tokenBalances[exchangeToken] = (tokenBalances[exchangeToken] || 0n) + totalLocked;
+}
 
 module.exports = {
   ethereum: {
-    tvl: () => ({}),
-    staking: staking(
-      [
-        "0x6244bc0d4b661526c0c62c3610571cd1ac9df2dd",
-        "0xbacc083795846a67b0782327a96622447ddafe6c",
-        "0x081a52f02e51978ad419dd7894d7ae3555f8bc26",
-        "0x3ed0c99c8e8eb94438837cc8a08ca3bb187424cf",
-        "0x3810d9d6685812af6ef4257de0542ecdba9bfd95"
-      ],
-      "0xC477D038d5420C6A9e0b031712f61c5120090de9"
-    ),
+    tvl,
+    // staking: staking(
+    //   [
+    //     "0x6244bc0d4b661526c0c62c3610571cd1ac9df2dd",
+    //     "0xbacc083795846a67b0782327a96622447ddafe6c",
+    //     "0x081a52f02e51978ad419dd7894d7ae3555f8bc26",
+    //     "0x3ed0c99c8e8eb94438837cc8a08ca3bb187424cf",
+    //     "0x3810d9d6685812af6ef4257de0542ecdba9bfd95"
+    //   ],
+    //   "0xC477D038d5420C6A9e0b031712f61c5120090de9"
+    // ),
+  },
+  polygon: {
+    tvl,
+  },
+  base: {
+    tvl,
+  },
+  optimism: {
+    tvl,
+  },
+  arbitrum: {
+    tvl,
   },
 };
---
+++ projects/boson/utils.js
@@ -0,0 +1,179 @@
+const ethers = require("ethers");
+const { getLogs } = require("../helper/cache/getLogs");
+
+const {
+  protocolDiamondAddress,
+  protocolDeploymentBlock,
+  fundsEncumberedTopic,
+  fundsReleasedTopic,
+  protocolFeeCollectedTopic,
+  offerCreatedTopic_v2_0_0,
+  offerCreatedTopic_v2_3_0,
+  offerCreatedTopic_v2_4_0,
+  offerCreatedTopic_v2_5_0,
+  offerVoidedTopic,
+  rangeReservedTopic,
+  buyerCommittedTopic_v2_0_0,
+  buyerCommittedTopic_v2_5_0,
+  OfferCreatedEvent_v2_0_0,
+  OfferCreatedEvent_v2_3_0,
+  OfferCreatedEvent_v2_4_0,
+  OfferCreatedEvent_v2_5_0,
+  OfferVoidedEvent,
+  RangeReservedEvent,
+  FundsEncumberedEvent,
+  FundsReleasedEvent,
+  ProtocolFeeCollectedEvent,
+  BuyerCommittedEvent_v2_0_0,
+  BuyerCommittedEvent_v2_5_0,
+} = require("./constants");
+
+async function getVoidedOffers(api) {
+  const logs = await getLogs({
+    api,
+    target: protocolDiamondAddress,
+    topics: [offerVoidedTopic],
+    fromBlock: protocolDeploymentBlock[api.chain],
+    eventAbi: OfferVoidedEvent,
+    onlyArgs: true,
+    extraKey: "offerVoided",
+  });
+
+  return logs.map((i) => BigInt(i.offerId));
+}
+
+async function getOffers(api, voidedOffers = []) {
+  const response = await getLogs({
+    api,
+    target: protocolDiamondAddress,
+    topics: [[offerCreatedTopic_v2_0_0, offerCreatedTopic_v2_3_0, offerCreatedTopic_v2_4_0, offerCreatedTopic_v2_5_0]],
+    fromBlock: protocolDeploymentBlock[api.chain],
+    extraKey: "offerCreated",
+  });
+
+  // Combine all versions of the OfferCreated event
+  const iface = new ethers.Interface([
+    OfferCreatedEvent_v2_0_0,
+    OfferCreatedEvent_v2_3_0,
+    OfferCreatedEvent_v2_4_0,
+    OfferCreatedEvent_v2_5_0,
+  ]);
+
+  const parsedLogs = response.map((log) => {
+    return iface.parseLog(log)?.args;
+  });
+
+  console.log("Total offers fetched:", parsedLogs.length);
+
+  const blockTimestamp = BigInt(api.timestamp);
+
+  return parsedLogs.filter(
+    (i) =>
+      !voidedOffers.includes(BigInt(i.offerId)) &&
+      BigInt(i?.offerDates?.validUntil) >= blockTimestamp &&
+      BigInt(i?.offerDates?.validFrom) < blockTimestamp
+  );
+}
+
+async function getCommits(api) {
+  const response = await getLogs({
+    api,
+    target: protocolDiamondAddress,
+    topics: [[buyerCommittedTopic_v2_0_0, buyerCommittedTopic_v2_5_0]],
+    fromBlock: protocolDeploymentBlock[api.chain],
+    extraKey: "buyerCommitted",
+  });
+
+  // Combine all versions of the BuyerCommitted event
+  const iface = new ethers.Interface([BuyerCommittedEvent_v2_0_0, BuyerCommittedEvent_v2_5_0]);
+
+  const parsedLogs = response.map((log) => {
+    return iface.parseLog(log)?.args;
+  });
+
+  console.log("Total # of commits:", parsedLogs.length);
+
+  const commitsByOffer = {};
+
+  for (const commit of parsedLogs) {
+    const offerId = BigInt(commit.offerId);
+    if (!commitsByOffer[offerId]) {
+      commitsByOffer[offerId] = [commit.exchangeId];
+    } else {
+      commitsByOffer[offerId].push(commit.exchangeId);
+    }
+  }
+
+  return commitsByOffer;
+}
+
+async function getReservedRanges(api) {
+  const logs = await getLogs({
+    api,
+    target: protocolDiamondAddress,
+    topics: [rangeReservedTopic],
+    fromBlock: protocolDeploymentBlock[api.chain],
+    extraKey: "reservedRanges",
+    eventAbi: RangeReservedEvent,
+    onlyArgs: true,
+  });
+
+  console.log("Total # of reserved ranges:", logs.length);
+
+  const reservedRangeByOffer = {};
+
+  for (const log of logs) {
+    const offerId = BigInt(log.offerId);
+    const startExchangeId = BigInt(log.startExchangeId);
+    const endExchangeId = BigInt(log.endExchangeId);
+
+    reservedRangeByOffer[offerId] = { startExchangeId, endExchangeId }; // there can be only one reserved range per offer
+  }
+
+  return reservedRangeByOffer;
+}
+
+async function getEncumberedFunds(api) {
+  const FundsEncumberedLogs = await getLogs({
+    api,
+    target: protocolDiamondAddress,
+    topics: [fundsEncumberedTopic],
+    fromBlock: protocolDeploymentBlock[api.chain],
+    eventAbi: FundsEncumberedEvent,
+    onlyArgs: true,
+    extraKey: "fundsEncumbered",
+  });
+
+  const encumberedByToken = {};
+  for (const log of FundsEncumberedLogs) {
+    const token = log.exchangeToken;
+    const amount = BigInt(log.amount);
+    encumberedByToken[token] = (encumberedByToken[token] || 0n) + amount;
+  }
+
+  const FundsReleasedLogs = await getLogs({
+    api,
+    target: protocolDiamondAddress,
+    topics: [fundsReleasedTopic],
+    fromBlock: protocolDeploymentBlock[api.chain],
+    eventAbi: FundsReleasedEvent,
+    onlyArgs: true,
+    extraKey: "fundsReleased",
+  });
+
+  for (const log of FundsReleasedLogs) {
+    const token = log.exchangeToken;
+    const amount = BigInt(log.amount);
+    encumberedByToken[token] -= amount;
+  }
+
+  return encumberedByToken;
+}
+
+module.exports = {
+  getVoidedOffers,
+  getOffers,
+  getCommits,
+  getReservedRanges,
+  getEncumberedFunds,
+};
