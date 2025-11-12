5d2cf9bb6443075208ad4d51f8f0557b3738f08b
Your Name <you@example.com>
2025-11-12 12:06:56 +0800

    add projects fufuture-v2 fufuture-contract

--- projects/fufuture-v2/index.js
+++ projects/fufuture-v2/index.js
@@ -37,22 +37,46 @@ module.exports = {
 
 const config = {
     bsc: {
-        factory: '0x0CB5274a8Ff86b7b750933B09aba8B5eb3660977',
-        fromBlock: 33366630,
-        optionsTrade: '0x1e933E0957e6236E519e64CD13f967146Fcb4755'
+        factory: '0x88f29D34c9602130B21Be4E1EaBEB7797177f259',
+        fromBlock: 46616446,
+        optionsTrade: '0x4e10384e98F2f170E00bfB1C3363D6f108a9ee68'
     },
-    arbitrum: {
-        factory: '0x0CB5274a8Ff86b7b750933B09aba8B5eb3660977',
-        fromBlock: 162984841,
-        optionsTrade: '0x1e933E0957e6236E519e64CD13f967146Fcb4755'
+    acala:{
+        factory: '0xec676347105e7e898e0d8e24f524989cbff23d55',
+        fromBlock: 7253471,
+        optionsTrade: '0x5e413ed5e35506488779c5188ea58628955c83b8'
+    },
+    // bitlayer:{
+    //     factory: '0xb711ba37fa74b99e70c27e82def0b15a50ac7a9e',
+    //     fromBlock: 4328431,
+    //     optionsTrade: '0x482390a90a49d17d119f5c7dfe5e5bedb149fd9d'
+    // },
+    op_bnb:{
+        factory: '0x91ABFdd51c0959016c54AbDfb012E81Ef82008d1',
+        fromBlock: 51301555,
+        optionsTrade: '0x91e644C8Fd5d5B799e0D238F31D9b46a91b8Ca03'
     },
     // aia:{
     //     factory: '0x0e1e15b77de5924650da76f9e1ae46b07f493aec',
-    //     fromBlock: 34771199,
+    //     fromBlock: 34771150,
     //     optionsTrade: '0x599bcd62cfa06c7e566da0ce62b376d0359d4a24'
-    // }
+    // },
+    base:{
+        factory: '0x344d02cEBb36ABBA6e55A9cefC703A75cA09E4E0',
+        fromBlock: 28996468,
+        optionsTrade: '0x43F323AcA25b57182bDc87564410ea7056975265'
+    },
+    // avalanche:{
+    //     factory: '0x5874ee8e6e96bFB17212962537e5Bf359630a268',
+    //     fromBlock: 	57296254,
+    //     optionsTrade: '0x344d02cEBb36ABBA6e55A9cefC703A75cA09E4E0'
+    // },
+    conflux:{
+        factory: '0xdc8091d097ca47afbbcc1ec216f3c5c26c587d19',
+        fromBlock: 117895905,
+        optionsTrade: '0x34bfe094c1275026475d37df3bb63a8d0adbc558'
+    }
 }
-
 
 Object.keys(config).forEach(chain => {
     module.exports[chain] = {tvl}
