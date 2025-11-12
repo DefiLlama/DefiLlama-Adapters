7b9f3ada6502e9c00fe46d696a050944d6497e15
0xShad0w <sd0w@protonmail.com>
2025-11-12 13:38:36 +0000

    add ramses hyperliquid cl and legacy (#17013)

---
+++ projects/ramses-hl-cl/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  hyperliquid: { factory: '0xA87c8308722237F6442Ef4762B7287afB84fB191', fromBlock: 18149975, },
+})
