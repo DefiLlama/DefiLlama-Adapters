da53d4b18df7eee1fd4323eee7472a1924d48122
0xpeluche <0xpeluche@proton.me>
2025-11-13 11:01:05 +0100

    Fix: notional-v3

--- projects/notional-v3/index.js
+++ projects/notional-v3/index.js
@@ -1,36 +1,45 @@
 const { sumTokens2 } = require('../helper/unwrapLPs');
-const abi = require('../notional/abi');
 const { cachedGraphQuery } = require('../helper/cache')
 
-const SUBGRAPHS = {
-  arbitrum: 'DnghsCNvJ4xmp4czX8Qn7UpkJ8HyHjy7cFN4wcH91Nrx',
-  ethereum: '4oVxkMtN4cFepbiYrSKz1u6HWnJym435k5DQRAFt2vHW'
-};
-const vaultsQuery = `{ vaultConfigurations { id } }`
-
-const CONTRACTS = {
-  arbitrum: "0x1344A36A1B56144C3Bc62E7757377D288fDE0369",
-  ethereum: "0x6e7058c91F85E0F6db4fc9da2CA41241f5e4263f"
+const CONFIG = {
+  ethereum: { endpoint: '4oVxkMtN4cFepbiYrSKz1u6HWnJym435k5DQRAFt2vHW', contract: '0x6e7058c91F85E0F6db4fc9da2CA41241f5e4263f'},
+  arbitrum: { endpoint: 'DnghsCNvJ4xmp4czX8Qn7UpkJ8HyHjy7cFN4wcH91Nrx', contract: '0x1344A36A1B56144C3Bc62E7757377D288fDE0369'},
 }
 
-async function addVaultTvl(api) {
-  let { vaultConfigurations } = await cachedGraphQuery(`notional-v3/${api.chain}`, SUBGRAPHS[api.chain], vaultsQuery)
+const payload = `{ vaultConfigurations { id } }`
+
+const abi = {
+  getMaxCurrencyId: "function getMaxCurrencyId() view returns (uint16)",
+  getCurrency: "function getCurrency(uint16) view returns ((address,bool,int256,uint8,uint256),(address,bool,int256,uint8,uint256))",
+  getPrimeCashHoldingsOracle: "function getPrimeCashHoldingsOracle(uint16) view returns (address)",
+  currencyIdToAddress: "function currencyIdToAddress(uint16) view returns (address)"
+}
+
+async function addVaultTvl(api, endpoint) {
+  const { vaultConfigurations } = await cachedGraphQuery(`notional-v3/${api.chain}`, endpoint, payload)
   const vaults = vaultConfigurations.map(i => i.id)
   const abi = "function getStrategyVaultInfo() view returns ((address pool, uint8 singleSidedTokenIndex, uint256 totalLPTokens, uint256 totalVaultShares, uint256 maxPoolShare, uint256 oraclePriceDeviationLimitPercent))"
   const data = await api.multiCall({ abi, calls: vaults, permitFailure: true })
   data.forEach(i => i && api.add(i.pool, i.totalLPTokens))
 }
 
-async function tvl(api) {
-  let oracles = await api.fetchList({ lengthAbi: abi.getMaxCurrencyId, itemAbi: abi.getPrimeCashHoldingsOracle, target: CONTRACTS[api.chain], startFromOne: true, })
-  let underlying = await api.multiCall({ abi: 'address:underlying', calls: oracles.map((o) => ({ target: o })) })
-  let holdings = await api.multiCall({ abi: 'address[]:holdings', calls: oracles.map((o) => ({ target: o })) })
-  let tokens = underlying.concat(holdings.flatMap((_) => _))
-  await addVaultTvl(api)
-  return sumTokens2({ tokens, owner: CONTRACTS[api.chain], api })
+const getArbTvl = async (api, contract) => {
+  const tokens = await api.fetchList({ itemCount: 15, itemAbi: abi.currencyIdToAddress, target: contract, startFromOne: true })
+  return sumTokens2({ tokens, owner: contract, api })
 }
 
-module.exports = {
-  arbitrum: { tvl },
-  ethereum: { tvl }
-};
+const tvl = async (api) => {
+  const chain = api.chain
+  const { contract, endpoint } = CONFIG[chain]
+  if (chain === 'arbitrum') return getArbTvl(api, contract)
+  const oracles = await api.fetchList({ lengthAbi: abi.getMaxCurrencyId, itemAbi: abi.getPrimeCashHoldingsOracle, target: contract, startFromOne: true, })
+  const underlying = await api.multiCall({ abi: 'address:underlying', calls: oracles.map((o) => ({ target: o })), permitFailure: true })
+  const holdings = await api.multiCall({ abi: 'address[]:holdings', calls: oracles.map((o) => ({ target: o })), permitFailure: true })
+  const tokens = underlying.concat(holdings.flatMap((_) => _))
+  await addVaultTvl(api, endpoint)
+  return sumTokens2({ tokens, owner: contract, api })
+}
+
+Object.keys(CONFIG).forEach((chain) => {
+  module.exports[chain] = { tvl }
+})
