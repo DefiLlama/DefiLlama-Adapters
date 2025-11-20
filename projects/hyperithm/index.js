dd731cd00e9b42db864d08fd054f138f541748c8
Dolchan <bchandol2@gmail.com>
2025-11-20 13:26:15 +0900

    fix : mToken tvl calculation

--- projects/hyperithm/index.js
+++ projects/hyperithm/index.js
@@ -1,48 +1,6 @@
-const ADDRESSES = require('../helper/coreAssets.json');
 const { getCuratorExport } = require("../helper/curators");
 
-// MIDAS CONTRACTS CONFIGURATION
-const MIDAS_MHYPER = {
-  ethereum: {
-    token: '0x9b5528528656DBC094765E2abB79F293c21191B9',
-    priceFeed: '0x43881b05c3be68b2d33eb70addf9f666c5005f68',
-  },
-  plasma: {
-    token: '0xb31bea5c2a43f942a3800558b1aa25978da75f8a',
-    priceFeed: '0xfc3e47c4da8f3a01ac76c3c5ecfbfc302e1a08f0',
-  }
-};
-
-const MIDAS_MXRP = {
-  bsc: {
-    token: '0xc8739fbBd54C587a2ad43b50CbcC30ae34FE9e34',
-    priceFeed: '0x3BdE0b7B59769Ec00c44C77090D88feB4516E731',
-  },
-  xrplevm: {
-    token: '0x06e0B0F1A644Bb9881f675Ef266CeC15a63a3d47',
-    priceFeed: '0xFF64785Ee22D764F8E79812102d3Fa7f2d3437Af',
-  }
-};
-
-// MIDAS TVL CALCULATION
-async function getMidasTvl(api, config) {
-  const totalSupply = await api.call({
-    target: config.token,
-    abi: 'uint256:totalSupply',
-  });
-
-  const rate = await api.call({
-    target: config.priceFeed,
-    abi: 'int256:lastAnswer',
-  });
-
-  const supply = Number(totalSupply) / 1e18;
-  const sharePrice = Number(rate) / 1e8;
-  return supply * sharePrice;
-}
-
-// VAULT CONFIGURATIONS BY BLOCKCHAIN
-const configs = {
+const vaultConfigs = {
   methodology: 'Count all assets are deposited in all vaults curated by Hyperithm.',
   blockchains: {
     ethereum: {
@@ -72,39 +30,33 @@ const configs = {
   }
 };
 
-const adapterExport = getCuratorExport(configs);
-
-const ethereumTvl = adapterExport.ethereum.tvl;
-adapterExport.ethereum.tvl = async (api) => {
-  await ethereumTvl(api);
-
-  const midasTvl = await getMidasTvl(api, MIDAS_MHYPER.ethereum);
-  api.add(ADDRESSES.ethereum.USDC, midasTvl * 1e6);
-};
-
-const plasmaTvl = adapterExport.plasma.tvl;
-adapterExport.plasma.tvl = async (api) => {
-  await plasmaTvl(api);
-  const midasTvl = await getMidasTvl(api, MIDAS_MHYPER.plasma);
-  api.add(ADDRESSES.plasma.USDT0, midasTvl * 1e6);
-};
-
-const bscTvl = adapterExport.bsc?.tvl;
-adapterExport.bsc = {
-  tvl: async (api) => {
-    if (bscTvl) await bscTvl(api);
-    const xrpAmount = await getMidasTvl(api, MIDAS_MXRP.bsc);
-    api.add("0x1D2F0da169ceB9fC7B3144628dB156f3F6c60dBE", xrpAmount * 1e18);
+const mtokenConfig = {
+  ethereum: {
+    mHYPER: '0x9b5528528656DBC094765E2abB79F293c21191B9',
+  },
+  plasma: {
+    mHYPER: '0xb31bea5c2a43f942a3800558b1aa25978da75f8a',
+  },
+  bsc: {
+    mXRP: '0xc8739fbBd54C587a2ad43b50CbcC30ae34FE9e34',
+  },
+  xrplevm: {
+    mXRP: '0x06e0B0F1A644Bb9881f675Ef266CeC15a63a3d47',
   }
 };
 
-const xrplevmTvl = adapterExport.xrplevm?.tvl;
-adapterExport.xrplevm = {
-  tvl: async (api) => {
-    if (xrplevmTvl) await xrplevmTvl(api);
-    const xrpAmount = await getMidasTvl(api, MIDAS_MXRP.xrplevm);
-    api.add(ADDRESSES.xrplevm.XRP, xrpAmount * 1e18);
-  }
-};
+const adapterExport = getCuratorExport(vaultConfigs);
+
+Object.keys(mtokenConfig).forEach(chain => {
+  const existingTvl = adapterExport[chain]?.tvl;
+  adapterExport[chain] = {
+    tvl: async (api) => {
+      await existingTvl?.(api);
+      const tokens = Object.values(mtokenConfig[api.chain]);
+      const bals = await api.multiCall({ abi: 'uint256:totalSupply', calls: tokens });
+      api.add(tokens, bals);
+    }
+  };
+});
 
 module.exports = adapterExport;
