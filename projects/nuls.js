ce4b68a106eb524f934bc7d98de7e5796dfada44
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2025-11-21 11:47:24 +0100

    Fix: nuls (#17115)

--- projects/nuls.js
+++ projects/nuls.js
@@ -4,6 +4,7 @@ const url = "https://public1.nuls.io/nuls/tvl"
 
 const tvl = async (api) => {
   const { data } = await utils.fetchURL(url)
+  if (data.tvl > 10_000_000) throw new Error("abnormally high TVL")
   return api.addUSDValue(Math.floor(data.tvl))
 }
 
