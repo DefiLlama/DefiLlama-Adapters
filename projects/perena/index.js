6a89b7b8fb9c589906c3c5333c55359a20bcf355
Chelioso <cheliosooo26@gmail.com>
2025-11-21 14:50:13 -0500

    perena data update

--- projects/perena/index.js
+++ projects/perena/index.js
@@ -2,7 +2,7 @@ const { getProvider, sumTokens2, } = require('../helper/solana')
 const { Program } = require("@coral-xyz/anchor");
 const { PublicKey } = require('@solana/web3.js');
 
-async function tvl() {
+async function numeraireTvlData() {
   const provider = getProvider()
 
   const programId = new PublicKey('NUMERUNsFCP3kuNmWZuXtm1AaQCPj9uw6Guv2Ekoi5P')
@@ -20,10 +20,47 @@ async function tvl() {
       tokenAccounts.push(pair.xVault.toString())
     })
   })
-  return sumTokens2({ tokenAccounts: tokenAccounts.filter(i => i !== '11111111111111111111111111111111'), blacklistedTokens, })
+
+  return { tokenAccounts: tokenAccounts.filter(i => i !== '11111111111111111111111111111111'), blacklistedTokens };
+}
+
+async function usdStarTvlData() {
+  const provider = getProvider()
+
+  const programId = new PublicKey('save8RQVPMWNTzU18t3GBvBkN9hT7jsGjiCQ28FpD9H')
+  const idl = await Program.fetchIdl(programId, provider)
+  const program = new Program(idl, provider)
+
+  const usdStarVaults = (await program.account.vaultGenState.all()).filter(
+    (x) =>
+      x.account.config.bank.equals(
+        new PublicKey("sM6P4mh53CnG4faN4Fo3seY7wMSAiHdy8o6gKjwQF7A") // USD* bank
+      )
+  );
+
+  const balances = {};
+  for (let i = 0; i < usdStarVaults.length; i++) {
+    const vault = usdStarVaults[i];
+    const tvl = vault.account.accounting.yieldingTvl.toNumber() / 10 ** 6;
+    const key = "solana:" + vault.account.config.yieldingTokenMint.toString();
+
+    if (!(key in balances)) {
+      balances[key] = tvl;
+    } else {
+      balances[key] = balances[key] + tvl;
+    }
+  }
+
+  return { balances };
+}
+
+async function tvl() {
+  const { tokenAccounts, blacklistedTokens } = await numeraireTvlData();
+  const { balances } = await usdStarTvlData();
+  return sumTokens2({ balances, tokenAccounts, blacklistedTokens });
 }
 
 module.exports = {
   timetravel: false,
-  solana: { tvl, },
+  solana: { tvl },
 }
