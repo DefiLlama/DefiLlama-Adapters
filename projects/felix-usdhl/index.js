c2b09024cce21c3c91af1823415547aaa0784db1
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-11-24 22:27:50 +0100

    minor fix

--- projects/felix-usdhl/index.js
+++ projects/felix-usdhl/index.js
@@ -1,19 +1,10 @@
-async function tvl(api) {
-  const mToken = '0x866a2bf4e572cbcf37d5071a7a58503bfb36be1b';
-  const holder = '0xb50a96253abdf803d85efcdce07ad8becbc52bd5';  
-  const balance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: mToken,
-    params: [holder]
-  })
-  
-  const usdValue = balance / 1e6;
-  
-  return { 'usd-coin': usdValue }
-}
+const { staking } = require("../helper/staking");
 
 module.exports = {
+  ethereum: {
+    tvl: staking('0x36f586A30502AE3afb555b8aA4dCc05d233c2ecE', '0x866a2bf4e572cbcf37d5071a7a58503bfb36be1b')
+  },
   hyperliquid: {
-    tvl,
+    tvl: staking('0xb50a96253abdf803d85efcdce07ad8becbc52bd5', '0x866a2bf4e572cbcf37d5071a7a58503bfb36be1b')
   },
 }
