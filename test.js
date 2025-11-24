42a4e68fbc926a2eefa290c53b87c096395a521d
Vukašin Manojlović <vmanojlovic@monad.foundation>
2025-11-24 15:17:10 +0100

    Add Uniswap v4 on Monad (#17135)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- test.js
+++ test.js
@@ -121,11 +121,11 @@ function validateHallmarks(hallmark) {
 
 (async () => {
 
-  const moduleArg = process.argv[2]
+  const moduleArg = process.argv[2].replace('/index.js', '').split('/').pop()
 
   // throw error if module doesnt start with lowercase letters
   if (!/^[a-z]/.test(moduleArg)) {
-    throw new Error("Module name should start with a lowercase letter");
+    throw new Error("Module name should start with a lowercase letter: " + moduleArg);
   }
 
   let module = {};
