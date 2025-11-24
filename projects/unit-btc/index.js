d09279c5e8a7c12363e2dfe1a3f9423042580c48
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-11-24 21:55:00 +0100

    unit: track monad

--- projects/unit-btc/index.js
+++ projects/unit-btc/index.js
@@ -4,22 +4,11 @@ const ADDRESSES = require('../helper/coreAssets.json');
 
 // https://docs.hyperunit.xyz/developers/key-addresses
 
-const tvl = async (api) => {
-  return api.sumTokens({ token: ADDRESSES.null, owner: '0x8e88826F42A0f5f199a9c91C3798c626326730b4' })
-}
-
 module.exports = {
   methodology: 'HyperUnit Hot wallets For BTC/ETH/SOL',
   bitcoin: {
     tvl: sumTokensExport({ owners: bitcoinAddressBook.unitbtc }),
   },
-  ethereum: {
-    tvl: sumTokensExport({
-      owners: ["0xBEa9f7FD27f4EE20066F18DEF0bc586eC221055A"],
-      tokens: [ADDRESSES.ethereum.WETH, ADDRESSES.null],
-    }),
-  },
-  plasma: { tvl },
   solana: {
     tvl: sumTokensExport({
       solOwners: ['9SLPTL41SPsYkgdsMzdfJsxymEANKr5bYoBsQzJyKpKS'],
@@ -28,3 +17,16 @@ module.exports = {
     })
   }
 };
+
+const config = {
+  ethereum: { owner: '0xBEa9f7FD27f4EE20066F18DEF0bc586eC221055A'},
+  monad: { owner: '0x4213de5c3C01eB3D757e271D4BEBc999F996E3D5'},
+  plasma: { owner: '0x8e88826F42A0f5f199a9c91C3798c626326730b4'},
+}
+
+Object.keys(config).forEach(chain => {
+  if (!config[chain].tokens) config[chain].tokens = [ADDRESSES.null]
+  module.exports[chain] = {
+    tvl: sumTokensExport(config[chain])
+  }
+})
