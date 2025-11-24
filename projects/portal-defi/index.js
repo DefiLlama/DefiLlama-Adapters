383eb31ea7f9e77c90936c26a18435c1724417a3
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-11-24 22:15:46 +0100

    refactor

--- projects/portal-defi/index.js
+++ projects/portal-defi/index.js
@@ -8,7 +8,7 @@ const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 const cfg = {
   ethereum: {
       owners: [ETHEREUM_STAKING_CONTRACT],
-      tokens: [ETH, PTB, USDC],
+      tokens: [ETH, USDC],
   },
 }
 
