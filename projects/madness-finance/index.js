5ad0ffaef03b9510254d0cd94ded963537b1d895
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-11-24 15:21:16 +0100

    minor fix

--- projects/madness-finance/index.js
+++ projects/madness-finance/index.js
@@ -1,14 +1,4 @@
-const { getUniTVL } = require("../helper/unknownTokens");
-
-const config = {
+const { uniTvlExports } = require('../helper/unknownTokens')
+module.exports = uniTvlExports({
   monad: '0x93d71152A93619c0b10A2EFc856AC46120FD01Ab',
-}
-
-module.exports = {
-  misrepresentedTokens: true,
-}
-
-Object.keys(config).forEach(chain => {
-  module.exports[chain] = { tvl: getUniTVL({ factory: config[chain], useDefaultCoreAssets: true, }), }
 })
-
