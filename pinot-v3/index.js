147d91ff0964edc680108b812d67fc03d85acb60
pinotfinance <noir@pinot.finance>
2025-11-24 19:21:56 +0900

    Create index.js

---
+++ pinot-v3/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  monad: { factory: '0x7716F310d62Aee3d009fd94067c627fe7E2f2aA9', fromBlock: 36151690 },
+})
