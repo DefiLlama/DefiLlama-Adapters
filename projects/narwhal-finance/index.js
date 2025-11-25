c77e020b2541919b93d9ec68d01e51f1432df10b
Thanh Nguyen <thanh@narwhal.finance>
2025-11-25 21:41:19 +0700

    fix: add Narwhal mainnet TVL

--- projects/narwhal-finance/index.js
+++ projects/narwhal-finance/index.js
@@ -2,7 +2,7 @@ const sdk = require("@defillama/sdk");
 const { request, gql } = require("graphql-request");
 const BigNumber = require("bignumber.js");
 
-const graphUrl = sdk.graph.modifyEndpoint(`2hcrVuaRPwMgnyejcNRYENsAtiSmRxWyoCPMVjTAG5Sg`);
+const graphUrl = sdk.graph.modifyEndpoint(`2wvUVLQbwNoV1vWxLuyUz5D2LmtHeusCwoqiHnWhxHvL`);
 const graphQuery = gql`
 {
   markets(first: 10) {
@@ -18,8 +18,6 @@ const graphQuery = gql`
 }
 `;
 
-const ADDRESS_aprMON = '0xb2f82D0f38dc453D596Ad40A37799446Cc89274A';
-
 async function tvl(api) {
   const res = await request(graphUrl, graphQuery);
 
@@ -30,19 +28,11 @@ async function tvl(api) {
   }
 
   // vaultLock is the TVL in the vault (in wei/18 decimals)
-  const vaultLockAprMON = new BigNumber(market.vaultLock)
+  const vaultLockUSDC = new BigNumber(market.vaultLock)
     .dividedBy(new BigNumber(10).pow(18));
-  const ratio_aprMON_MON_raw = await api.call({
-    target: ADDRESS_aprMON,
-    abi: 'function convertToAssets(uint256) returns (uint256)',
-    params: ['1000000000000000000'],
-  });
-  const ratio_aprMON_MON = new BigNumber(ratio_aprMON_MON_raw)
-    .dividedBy(new BigNumber(10).pow(18));
-  const vaultLockMON = vaultLockAprMON.multipliedBy(ratio_aprMON_MON);
 
   return {
-    "coingecko:tether": vaultLockMON.toFixed(0),
+    "coingecko:tether": vaultLockUSDC.toFixed(0),
   };
 }
 
