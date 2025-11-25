111d939c9fe974ed900c96fc6afe70ddf9a3488e
SuperPingu <0xbluepingu@gmail.com>
2025-11-25 10:02:08 +0100

    add mon to pingu.exchange

--- projects/pingu/index.js
+++ projects/pingu/index.js
@@ -1,14 +1,23 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport, nullAddress } = require('../helper/unwrapLPs')
 
-const fundStore = "0x7Cc41ee3Cba9a1D2C978c37A18A0d6b59c340224"; // FundStore
-const PINGU = "0x83E60B9F7f4DB5cDb0877659b1740E73c662c55B"; // PINGU
-const assets = [nullAddress, ADDRESSES.arbitrum.USDC_CIRCLE] // ETH, USDC
+const fundStore_arb = "0x7Cc41ee3Cba9a1D2C978c37A18A0d6b59c340224"; // fundStore_arb
+const PINGU_arb = "0x83E60B9F7f4DB5cDb0877659b1740E73c662c55B"; // PINGU_arb
+const assets_ARB = [nullAddress, ADDRESSES.arbitrum.USDC_CIRCLE] // ETH, USDC
+
+const fundStore_monad = "0x576d51fB872065DC4Af6f83902fd4078eBCc2f03"; // fundStore_monad
+const PINGU_monad = "0xA2426cD97583939E79Cfc12aC6E9121e37D0904d"; // PINGU_monad
+const USDC_monad = "0x754704Bc059F8C67012fEd69BC8A327a5aafb603"; // USDC_monad
+const assets_MONAD = [nullAddress, USDC_monad] // MON, USDC
 
 module.exports = {
 	start: '2024-01-10',
 	arbitrum: {
-		tvl: sumTokensExport({ owners: [fundStore], tokens: assets }),
-		staking: sumTokensExport({ owners: [fundStore], tokens: [PINGU] }),
+		tvl: sumTokensExport({ owners: [fundStore_arb], tokens: assets_ARB }),
+		staking: sumTokensExport({ owners: [fundStore_arb], tokens: [PINGU_arb] }),
+	},
+	monad: {
+		tvl: sumTokensExport({ owners: [fundStore_monad], tokens: assets_MONAD }),
+		staking: sumTokensExport({ owners: [fundStore_monad], tokens: [PINGU_monad] }),
 	},
 }
