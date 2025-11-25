22228d11d8a288aa7d49856156cd4fae19f93b29
graykode <nlkey2022@gmail.com>
2025-11-25 11:46:53 +0900

    refactor: remove unused base configuration from index.js

--- projects/clober-liquidity-vault/index.js
+++ projects/clober-liquidity-vault/index.js
@@ -7,12 +7,6 @@ const abi = {
 }
 
 const config = {
-  base: {
-    rebalancer: '0xeA0E19fbca0D9D707f3dA10Ef846cC255B0aAdf3',
-    bookManager: '0x382CCccbD3b142D7DA063bF68cd0c89634767F76',
-    blacklistedTokens: ['0x000000000000bb1b11e5ac8099e92e366b64c133'],
-    fromBlock: 21715410,
-  },
   monad: {
     rebalancer: '0xB09684f5486d1af80699BbC27f14dd5A905da873',
     bookManager: '0x6657d192273731C3cAc646cc82D5F28D0CBE8CCC',
@@ -33,5 +27,4 @@ async function tvl(api) {
 module.exports = {
   methodology: "TVL includes all assets deposited into the Clober Liquidity Vault contract, specifically allocated for liquidity provision and market-making within the Clober ecosystem",
   monad: { tvl },
-  base: { tvl },
 }
