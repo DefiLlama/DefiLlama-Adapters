33b724dfba4d56ccf687b3e68c5872ed6a54ad91
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-11-25 22:04:59 +0100

    code refactor

--- projects/curvance/index.js
+++ projects/curvance/index.js
@@ -1,32 +1,29 @@
-const ADDRESSES = require('./contracts.json');
-const native = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
-
-async function sumAndPrice(api, tokenAddress, wrapped_native) {
-    const supply = await api.call({ abi: 'uint256:totalAssets', target: tokenAddress });
-    let asset = await api.call({ abi: 'address:asset', target: tokenAddress });
-
-    try {
-        const vaultAsset = await api.call({ abi: 'address:asset', target: asset });
-        asset = native == String(vaultAsset).toLowerCase() ? wrapped_native : vaultAsset; 
-    } catch (e) {}
-
-
-    return api.add(asset, supply);
-};
-
-const monadTvl = async (api) => {
-    let promises = [];
-    const wrapped_native = "0x3bd359C1119dA7Da1D913D1C4D2B7c461115433A";
-    for(const market of Object.values(ADDRESSES.monad)) {
-        for(const address of Object.values(market)) {
-            promises.push(sumAndPrice(api, address, wrapped_native));
-        }
-    }
-    await Promise.all(promises);
+const { sumTokens2 } = require('../helper/unwrapLPs')
+const config = {
+  monad: '0x1310f352f1389969Ece6741671c4B919523912fF'
 }
 
-module.exports = {
-    monad: {
-        tvl: monadTvl
+Object.keys(config).forEach(chain => {
+  const centralRegistry = config[chain]
+
+  async function getMarkets(api) {
+    const managers = await api.call({ abi: 'address[]:marketManagers', target: centralRegistry })
+    const markets = await api.multiCall({ abi: 'address[]:queryTokensListed', calls: managers })
+    return markets.flat()
+  }
+
+  module.exports[chain] = {
+    tvl: async (api) => {
+      const contracts = await getMarkets(api)
+      const tokens = await api.multiCall({ abi: 'address:asset', calls: contracts })
+      return sumTokens2({ api, tokensAndOwners2: [tokens, contracts] })
+    },
+    borrowed: async (api) => {
+      const contracts = await getMarkets(api)
+      const tokens = await api.multiCall({ abi: 'address:asset', calls: contracts })
+      const bals = await api.multiCall({ abi: 'uint256:marketOutstandingDebt', calls: contracts })
+      api.add(tokens, bals)
+      return sumTokens2({ api, })
     }
-}
+  }
+})
