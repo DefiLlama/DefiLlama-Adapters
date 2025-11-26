b434a35ae4f3d1ad0642ba78d9ddbbf1b9a54ff2
jiajames <jia.james1@gmail.com>
2025-11-26 14:03:22 -0500

    Add capricorn TVL adapter

--- projects/capricorn/index.js
+++ projects/capricorn/index.js
@@ -1,12 +1,5 @@
-const { getUniTVL } = require('../helper/unknownTokens')
+const { uniV3Export } = require("../helper/uniswapV3");
 
-module.exports = {
-  misrepresentedTokens: true,
-  cube: {
-    /* tvl: getUniTVL({
-      useDefaultCoreAssets: true,
-      factory: '0x33CB4150f3ADFCD92fbFA3309823A2a242bF280f',
-    }) */
-    tvl: () => ({})
-  }
-};
+module.exports = uniV3Export({
+  monad: { factory: "0x6B5F564339DbAD6b780249827f2198a841FEB7F3", fromBlock: 36628679 },
+});
