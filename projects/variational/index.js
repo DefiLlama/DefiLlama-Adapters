db72942bdf3c36b68fe28e58b3454b8fae59e688
PatelPrinci <principatel@lampros.tech>
2025-11-27 13:56:44 +0530

    Fix: use direct SDK call instead of helper

--- projects/variational/index.js
+++ projects/variational/index.js
@@ -1,14 +1,10 @@
 const sdk = require('@defillama/sdk');
-const { sumTokens2 } = require('../helper/unwrapUniswapV2.js');
 
 const OLP_VAULT = '0x74bbbb0e7f0bad6938509dd4b556a39a4db1f2cd';
 
 async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
-  return sumTokens2({ 
-    api, 
-    owner: OLP_VAULT,
-    fetchBalances: true 
-  });
+  const tokens = await sdk.api.erc20.countTokens({ chain: 'arbitrum', owner: OLP_VAULT });
+  return tokens;
 }
 
 module.exports = {
