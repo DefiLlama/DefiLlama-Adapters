3d04b52576c8a19042612885c7cb8c00c59ed4d9
PatelPrinci <principatel@lampros.tech>
2025-11-27 14:09:14 +0530

    Fix Variational adapter import

--- projects/variational/index.js
+++ projects/variational/index.js
@@ -3,11 +3,11 @@ const { sumTokens2 } = require('../helper/unwrapLPs');
 const OLP_VAULT = '0x74bbbb0e7f0bad6938509dd4b556a39a4db1f2cd';
 
 async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
-  return sumTokens2({ 
-    api, 
+  return sumTokens2({
+    api,
     owner: OLP_VAULT,
-    fetchBalances: true 
-  });
+    fetchCoValentTokens: true,
+  })
 }
 
 module.exports = {
