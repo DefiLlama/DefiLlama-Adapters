961817d4da28048e53e7875c76cb9913de706550
OriginalSK <83238566+skgreazy@users.noreply.github.com>
2025-11-27 21:46:48 +0200

    Update index.js

--- projects/aave-horizon/index.js
+++ projects/aave-horizon/index.js
@@ -1,7 +1,5 @@
 const { aaveV3Export } = require("../helper/aave");
-
-module.exports = aaveV3Export({
-  ethereum: { 
-    pool: '0x53519c32f73fE1797d10210c4950fFeBa3b21504',
-  },
-});
+const CONFIG = {
+  ethereum: ['0x53519c32f73fE1797d10210c4950fFeBa3b21504'],
+};
+module.exports = aaveV3Export(CONFIG)
