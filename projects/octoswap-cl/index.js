0aeb35594d4a35ba65867c73be07cf80a119316f
Sudzpel <114531512+Sudzpel@users.noreply.github.com>
2025-11-30 12:29:52 +0300

    add OctoSwap CL & OctoSwap Classic adapters on Monad (#17233)

---
+++ projects/octoswap-cl/index.js
@@ -0,0 +1,8 @@
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+  monad: {
+    factory: "0x30Db57A29ACf3641dfc3885AF2e5f1F5A408D9CB",
+    fromBlock:33818287,
+  },
+});
