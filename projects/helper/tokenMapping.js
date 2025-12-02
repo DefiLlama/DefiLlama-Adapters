83479d4fe2a6236fc464ec1ff58864e177a1398a
Ruslan <ruslan.k@ylem.company>
2025-12-02 13:53:52 +0100

    feat: add tac-liquid-staking adapter - gTAC

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -40,6 +40,9 @@ const ibcMappings = {
 }
 
 const fixBalancesTokens = {
+  tac: {
+    '0x0000000000000000000000000000000000000000': { coingeckoId: 'tac', decimals: 18 },
+  },
   ozone: {
     // '0x83048f0bf34feed8ced419455a4320a735a92e9d': { coingeckoId: "ozonechain", decimals: 18 }, // was mapped to wrong chain
   },
