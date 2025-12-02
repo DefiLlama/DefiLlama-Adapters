d5fdba4ddf37a8d5d3a8d8712bb10b2da553c379
Gloom0x0 🦾 <122207760+Gloom0x0@users.noreply.github.com>
2025-12-02 09:48:06 +0100

    Update sc for second vault
    Replaces the second base address in the configuration file with a new value for shift-protocol. Ensures the application uses the correct contract address.

--- projects/shift-protocol/config.js
+++ projects/shift-protocol/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   base: [
     "0xaf69Bf9ea9E0166498c0502aF5B5945980Ed1E0E",
-    "0x8d8d4643F6B3fF6F953Aae428306aD91fE872327",
+    "0x4cE3ec1b7B4FFb33A0B70c64a0560A3F341AA2E1",
   ],
 };
