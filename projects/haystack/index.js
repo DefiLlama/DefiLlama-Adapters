2b748359ce5e3c9c3baec806f62b4d1fe14155d4
John Mizz <johnmizzoni6@gmail.com>
2025-12-03 14:28:48 -0500

    refactor(haystack): move HAY token tracking to staking

--- projects/haystack/index.js
+++ projects/haystack/index.js
@@ -6,7 +6,7 @@ const HAY_ASA_ID = '3160000000'
 // Single staking escrow account that holds all staked HAY
 const STAKING_ESCROW = 'OLSICPA5V6IPWORUVWQKCJTSFKLP7P5JORZBICKU6CH7W7EVMDALLWD7SQ'
 
-async function tvl() {
+async function staking() {
   return sumTokens({
     owner: STAKING_ESCROW,
     tokens: [HAY_ASA_ID],
@@ -17,7 +17,8 @@ module.exports = {
   timetravel: false,
   methodology: 'Counts the total HAY tokens held in the Haystack staking escrow account on Algorand.',
   algorand: {
-    tvl,
+    tvl: () => ({}),
+    staking,
   },
 }
 
