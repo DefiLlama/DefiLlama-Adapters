394a82af1c8f1e301acfc415d9a61ea4901dd8df
0xpeluche <0xpeluche@proton.me>
2025-12-05 14:21:33 +0100

    Fear: Add usx.capital

---
+++ projects/usx.capital/index.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+  scroll: { tvl: async (api) =>  await api.erc4626Sum({ calls: ['0xcB14BcdF6cD483665D10dfD6f87d908996C7F922'], tokenAbi: 'address:asset', balanceAbi: 'uint256:totalAssets' }) }  
+}
