af339490125a3264603e8e02d2cb69430bd91d3f
adiraviraj <adi@interchain.io>
2025-12-09 12:56:09 +0100

    export token balance instead of USD

--- projects/noble-points-vault.js
+++ projects/noble-points-vault.js
@@ -4,14 +4,15 @@ const ENDPOINT = 'https://api.noble.xyz/noble/dollar/vaults/v1/stats';
 
 async function tvl() {
   const data = await get(ENDPOINT);
-  const tvlUsd = Number(data.staked_total_principal) / 1e6;
-  return { tether: tvlUsd };
+  const usdnAmount = Number(data.staked_total_principal) / 1e6;
+  return { 'noble-dollar-usdn': usdnAmount };
 }
 
 module.exports = {
   timetravel: false,
-  methodology: 'TVL is the total principal deposited in the Noble USDN points vault.',
+  methodology: 'TVL is the total USDN deposited in the Noble points vault.',
   noble: {
     tvl,
   },
-}
+};
+
