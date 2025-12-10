d6d3877b0b9a91ff6de601e09a414a3d8d061ae1
dwea <dwarkeshp@etherauthority.io>
2025-12-10 13:03:14 +0530

    added grxswap adapter

---
+++ projects/grxswap/index.js
@@ -0,0 +1,9 @@
+const { uniTvlExport } = require('../helper/unknownTokens')
+
+module.exports = uniTvlExport(
+  'grx',
+  '0xc7316818841f355c5107753a3f3fdea799bd25f6', // Factory address
+  {
+    provider: 'https://rpc.grxchain.io', // explicit RPC
+  }
+)
---
+++ projects/helper/chain/grx.js
@@ -0,0 +1,7 @@
+const RPC_ENDPOINTS = {
+    'grx': 'https://rpc.grxchain.io',
+}
+
+module.exports = {
+    RPC_ENDPOINTS,
+}
