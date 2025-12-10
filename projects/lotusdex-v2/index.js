21b9476c7f220d6ff148f673b54c2f7776b8f072
lotusdex_dev <contact@lotusdex.io>
2025-12-10 20:32:02 +0700

    fix: update 'mantra' to 'mantrachain' in TVL exports for lotusdex v2 and v3

--- projects/lotusdex-v2/index.js
+++ projects/lotusdex-v2/index.js
@@ -1,4 +1,4 @@
 const { uniTvlExports } = require('../helper/unknownTokens')
 module.exports = uniTvlExports({
-  'mantra': '0xf8f54C91E868c7CF87774C388B6793807921BAa1'
+  'mantrachain': '0xf8f54C91E868c7CF87774C388B6793807921BAa1'
 })
--- projects/lotusdex-v3/index.js
+++ projects/lotusdex-v3/index.js
@@ -1,4 +1,4 @@
 const { uniV3Export } = require('../helper/uniswapV3')
 module.exports = uniV3Export({
-  'mantra': { factory: '0xf091964417222474f1835B1bF096995d66303d96', fromBlock: 1 }
+  'mantrachain': { factory: '0xf091964417222474f1835B1bF096995d66303d96', fromBlock: 9868586 }
 })
