a017d3acd3b7ee1da5f17a9eb47d26ef272ebdc2
cjmario89 <mario89cheng@gmail.com>
2025-12-11 18:30:04 +0800

    refactor: change mappingTokenAsset to a standard function declaration in lake protocol

--- projects/lake-protocol/index.js
+++ projects/lake-protocol/index.js
@@ -9,7 +9,7 @@ const TOKEN = {
   USDC: "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC",
 }
 
-async function mappingTokenAsset(type){
+function mappingTokenAsset(type){
   switch (type) {
     case TOKEN.lakeUSDC:
       return TOKEN.USDC;
