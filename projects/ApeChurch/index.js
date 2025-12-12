384d5699b5235d7d84a4a9403f60fdf12b0c40c0
Christian Rhodes <78001767+christianrhodes18@users.noreply.github.com>
2025-12-11 17:50:34 -0700

    Update index.js

--- projects/ApeChurch/index.js
+++ projects/ApeChurch/index.js
@@ -1,3 +1,19 @@
-const USER_INFO_CONTRACT = '0x6ea76f01aa615112ab7de1409efbd80a13bfcc84';
-const HOUSE_CONTRACT = '0x2054709f89f18a4ccac6132ace7b812e32608469';
+const { sumTokens2 } = require('../helper/unwrapLPs');
+const { nullAddress } = require('../helper/tokenMapping');
 
+module.exports = {
+  methodology: `Ape Church TVL is achieved by tracking the balance of The House smart contract. Volume is achieved by tracking the Transfer events from the UserInfoTracker smart contract.`,
+  apechain: {
+    volume: async (api) => {
+        return await api.call({
+            target: "0x6EA76F01Aa615112AB7de1409EFBD80a13BfCC84",
+            params: [],
+            abi: 'erc20:totalSupply'
+            // fromBlock: 23427001,
+        })
+    },
+    tvl: async (api) => {
+        return sumTokens2({ owner: "0x2054709F89F18a4CCAC6132acE7b812E32608469", tokens: [nullAddress], api, })
+    }
+  }
+}
