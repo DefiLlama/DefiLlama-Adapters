cc8368c21dc937646c02778da8dca5c4ef7679c0
v7m <v7m@example.com>
2025-12-12 01:39:15 +0100

    Update StakeNova project adapter

--- projects/stakenova/index.js
+++ projects/stakenova/index.js
@@ -1,15 +1,16 @@
 const { sumTokens2 } = require('../helper/solana');
 
-const TOKEN_ACCOUNT = '7MBk8DXFnZCpKDiSwEMFwtyDNqxXMdUSgm52fQ3Chit4';
+const INF_VAULT = '7MBk8DXFnZCpKDiSwEMFwtyDNqxXMdUSgm52fQ3Chit4';
+const USDC_VAULT = 'HVpnjWTJCDvHL2pL2LeE5Y3mLEE4hP4MwpZpBvBLHQuA';
 
 async function tvl() {
   return sumTokens2({
-    tokenAccounts: [TOKEN_ACCOUNT],
+    tokenAccounts: [INF_VAULT, USDC_VAULT],
   });
 }
 
 module.exports = {
-  methodology: 'StakeNova is a staking protocol on Solana. TVL includes all user-deposited SOL that generates yield rewards.',
+  methodology: 'TVL represents all user-deposited SOL plus unclaimed USDC rewards. Calculated by reading vault token balances directly on-chain.',
   solana: {
     tvl,
   },
