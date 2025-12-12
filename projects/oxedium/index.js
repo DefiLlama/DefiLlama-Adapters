8f18c7cc7af18b657a6515f6fc61ef67eac1e0cb
Arj-Lab <arj.lemited@gmail.com>
2025-12-13 01:18:17 +0300

    add oxedium

---
+++ projects/oxedium/index.js
@@ -0,0 +1,33 @@
+const { PublicKey } = require('@solana/web3.js');
+const { getConnection, sumTokens2 } = require('../helper/solana');
+
+
+async function tvl() {
+  // const programId = new PublicKey('program_id');
+  // const connection = getConnection();
+
+  // const OXEDIUM_SEED = Buffer.from("oxedium-seed");
+  // const TREASURY_SEED = Buffer.from("treasury-seed");
+
+  // const [treasuryAccount] = PublicKey.findProgramAddressSync(
+  //   [OXEDIUM_SEED, TREASURY_SEED],
+  //   programId
+  // );
+
+  // const tokenAccountsData = await connection.getParsedTokenAccountsByOwner(
+  //   treasuryAccount,
+  //   { programId: new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA") } 
+  // );
+
+  // const tokenAccounts = tokenAccountsData.value.map(account => account.pubkey.toBase58());
+  
+  // return sumTokens2({ tokenAccounts, solOwners: [treasuryAccount.toBase58()], nativeToken: 'solana' });
+  return 0;
+}
+
+module.exports = {
+  timetravel: false,
+  solana: { tvl },
+};
+
+
