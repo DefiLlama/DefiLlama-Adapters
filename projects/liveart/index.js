26923a9cc354f31a3c0ee6cb76dddb5c66b73b54
Alex <67423307+aleeeexcool@users.noreply.github.com>
2025-12-15 14:25:37 +0200

    feat: add few new tokens and chain for fetching

--- projects/liveart/index.js
+++ projects/liveart/index.js
@@ -4,14 +4,29 @@ async function computeMarketCap(api, assets, token) {
   const priceFeedAbi = 'function latestRoundData() view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)'
   const assetList = Array.isArray(assets) ? assets : [assets]
 
-  await Promise.all(assetList.map(async (asset) => {
-    const { answer: price } = await api.call({ abi: priceFeedAbi, target: asset.priceFeed })
-    const totalSupply = await api.call({ abi: 'erc20:totalSupply', target: asset.assetId })
-    const assetDecimals = asset.decimals ?? 18
-    const priceDecimals = asset.priceDecimals ?? 8
-    const marketCap = (Number(totalSupply) / 10 ** assetDecimals) * (Number(price) / 10 ** priceDecimals)
+  const priceCalls = []
+  const supplyCalls = []
+  for (const asset of assetList) {
+    priceCalls.push({ target: asset.priceFeed })
+    supplyCalls.push({ target: asset.assetId })
+  }
+
+  const prices = await api.multiCall({
+    abi: priceFeedAbi,
+    calls: priceCalls,
+  })
+
+  const supplies = await api.multiCall({
+    abi: 'erc20:totalSupply',
+    calls: supplyCalls,
+  })
+
+  for (let i = 0; i < assetList.length; i++) {
+    const { answer: price } = prices[i]
+    const totalSupply = supplies[i]
+    const marketCap = (Number(totalSupply) / 1e18) * (Number(price) / 1e8)
     sdk.util.sumSingleBalance(api.getBalances(), token, marketCap)
-  }))
+  }
 
   return api.getBalances()
 }
@@ -24,6 +39,12 @@ module.exports = {
     }, {
       assetId: '0x25a0e808A7Ed833EA9e9C0B643Eb8B6832af3749', // MURFLO Token
       priceFeed: '0x6913A66f54DAe935fD489f34728D20aeEB43A6bB',
+    }, {
+      assetId: '0x1bb8da2A094078DdD91A1425afb29B0F5381b990', // BANGIR Token
+      priceFeed: '0x5FD035D057f27fA6872E0D83F3F9C31665019b7A',
+    }, {
+      assetId: '0xf3b70e15743a9d94FE679891a4BfdA61183FD9f5', // KUSPUM Token
+      priceFeed: '0x488Cd7e0D3CAE1fe5f52895DC0D255e9185F0f39',
     }], 'coingecko:usd-coin'),
   },
   bsc: {
@@ -33,12 +54,33 @@ module.exports = {
     }, {
       assetId: '0x2024739806E4DEb480481150C45340fB800cB8fe', // MURFLO Token
       priceFeed: '0x102a2a121F6d54cCc137978642c6294a73c971d7',
-    }], 'coingecko:tether')
+    }, {
+      assetId: '0xafE03Cf0833401c83C639566448fC4d754a52829', // HARPLA Token
+      priceFeed: '0xAd49a5C7D8d8241588Da87821a51c83198cC3cd5',
+    }, {
+      assetId: '0x1bb8da2A094078DdD91A1425afb29B0F5381b990', // BANGIR Token
+      priceFeed: '0x8eB682CdC4E7F913414274a54db1ca35bcC0c4bC',
+    }, {
+      assetId: '0xf3b70e15743a9d94FE679891a4BfdA61183FD9f5', // KUSPUM Token
+      priceFeed: '0x8eB682CdC4E7F913414274a54db1ca35bcC0c4bC',
+    },], 'coingecko:tether')
   },
   berachain: {
     tvl: async (api) => computeMarketCap(api, [{
       assetId: '0x25a0e808A7Ed833EA9e9C0B643Eb8B6832af3749', // MURFLO Token
       priceFeed: '0x0C0ccB0d5de18E0a208225585f0de4288D8dD2ff',
+    }, {
+      assetId: '0x1bb8da2A094078DdD91A1425afb29B0F5381b990', // BANGIR Token
+      priceFeed: '0x638c575b1a374c84f0D11DaCeD349B2cC78E2f48',
+    }, {
+      assetId: '0xf3b70e15743a9d94FE679891a4BfdA61183FD9f5', // KUSPUM Token
+      priceFeed: '0x638c575b1a374c84f0D11DaCeD349B2cC78E2f48',
     }], 'coingecko:tether')
+  },
+  linea: {
+    tvl: async (api) => computeMarketCap(api, [{
+      assetId: '0xf3b70e15743a9d94FE679891a4BfdA61183FD9f5', // KUSPUM Token
+      priceFeed: '0xFB64a717319EBbB64f2dfC93d8E4316723eB8417',
+    }], 'coingecko:usd-coin')
   },
 };
