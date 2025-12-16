19f45077dbaf2e2a4cb729b3481c3ab8786a7024
noah-nexus <noah@to.nexus>
2025-12-16 10:40:49 +0900

    fix issues from PR review

--- projects/cross-rewards/index.js
+++ projects/cross-rewards/index.js
@@ -1,23 +1,15 @@
 const { addCrossToken } = require('../helper/chain/cross');
 
-const STAKING_CONTRACTS = ['0x8bD742488CdD51658cF871b737Ce24e20FDdd711'];
+const DEPOSIT_CONTRACTS = ['0x8bD742488CdD51658cF871b737Ce24e20FDdd711'];
 const CROSS_TOKEN_ADDRESS = '0x642060e8B44C8f2d6D2974a71a0ca8F995cAfBdA';
 
-// getTotalDeposited ABI
-const ABI = {
-  "type": "function",
-  "name": "getTotalDeposited",
-  "inputs": [{"type": "address", "name": "token"}],
-  "outputs": [{"type": "uint256"}],
-  "stateMutability": "view"
-};
 
-async function tvl(api) {
+async function staking(api) {
   const balances = api.getBalances();
   
-  for (const contractAddress of STAKING_CONTRACTS) {
+  for (const contractAddress of DEPOSIT_CONTRACTS) {
     const deposit = await api.call({
-      abi: ABI,
+      abi: "function getTotalDeposited(address) external view returns (uint256)",
       target: contractAddress,
       params: [CROSS_TOKEN_ADDRESS],
     });
@@ -27,8 +19,9 @@ async function tvl(api) {
 }
 
 module.exports = {
-  methodology: 'TVL is the total amount of CROSS tokens staked in the Cross Rewards contract.',
+  methodology: 'CROSS amount deposisted to CROSS Rewards contract',
   cross: {
-    tvl,
+    tvl: () => ({}),
+    staking,
   },
 };
