6ca34a594789334ffd09ecd9760a7772e4c5d3db
dwinea <154954349+dwinea@users.noreply.github.com>
2025-12-17 12:09:21 +0530

    Remove explicit RPC provider from grxswap

--- projects/grxswap/index.js
+++ projects/grxswap/index.js
@@ -2,8 +2,5 @@ const { uniTvlExport } = require('../helper/unknownTokens')
 
 module.exports = uniTvlExport(
   'grx',
-  '0xc7316818841f355c5107753a3f3fdea799bd25f6', // Factory address
-  {
-    provider: 'https://rpc.grxchain.io', // explicit RPC
-  }
+  '0xc7316818841f355c5107753a3f3fdea799bd25f6' // Factory address
 )
