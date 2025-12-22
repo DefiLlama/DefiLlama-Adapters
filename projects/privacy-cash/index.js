322ebedf81d75e73b4578205e3c55fafa928a7d8
dashboardsdata <dunedatadash@gmail.com>
2025-12-22 12:34:29 +0100

    fix: Privacy Cash TVL to Include USDC (#17460)

--- projects/privacy-cash/index.js
+++ projects/privacy-cash/index.js
@@ -1,6 +1,11 @@
 const { sumTokensExport } = require('../helper/solana')
+const ADDRESSES = require('../helper/coreAssets.json')
 
 module.exports = {
-  methodology: 'Value of SOL in the privacy cash pool',
-  solana: { tvl: sumTokensExport({ solOwners: ['4AV2Qzp3N4c9RfzyEbNZs2wqWfW4EwKnnxFAZCndvfGh'] }) },
+  methodology: 'Value of SOL + USDC in the privacy cash pool',
+  solana: { tvl: sumTokensExport({
+    owners: ['2vV7xhCMWRrcLiwGoTaTRgvx98ku98TRJKPXhsS8jvBV'],
+    tokens: [ADDRESSES.solana.USDC],
+    solOwners: ['4AV2Qzp3N4c9RfzyEbNZs2wqWfW4EwKnnxFAZCndvfGh']
+  }) },
 }
