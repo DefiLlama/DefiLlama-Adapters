c3f6f5aabd8a2b17de9697cb5d70f667df8ea1c6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-12-29 14:25:26 +0100

    temp fix: saga rpc

--- projects/helper/env.js
+++ projects/helper/env.js
@@ -27,20 +27,16 @@ const DEFAULTS = {
   ANKR_API_KEY: '79258ce7f7ee046decc3b5292a24eb4bf7c910d7e39b691384c7ce0cfb839a01',
   SUBSCAN_API_KEY: 'ca3ba5ed1ff44b689c5f81dfc6b1644b',
   RENEC_RPC: "https://api-mainnet-beta.renec.foundation:8899/",
-  FLOW_RPC: 'https://rest-mainnet.onflow.org',
   FOGO_RPC: 'https://mainnet.fogo.io',
-  CAMP_RPC: 'https://rpc.camp.raas.gelato.cloud',
   LULO_API_KEY: '',
   TRON_RPC: 'https://api.trongrid.io',
   MOVE_RPC: 'https://mainnet.movementnetwork.xyz',
   SUPRA_RPC: 'https://rpc-mainnet.supra.com',
   IOTA_RPC: "https://api.mainnet.iota.cafe",
-  MANTRA_RPC: "https://evm.mantrachain.io",
   KASPLEX_RPC: "https://evmrpc.kasplex.org",
-  MANTRA_RPC_MULTICALL: "0xca11bde05977b3631167028862be2a173976ca11",
+  SAGA_RPC: "https://sagaevm.jsonrpc.sagarpc.io",
   BIFROST_P_RPC: "wss://api-bifrost-polkadot.n.dwellir.com/" + _yek,
   BIFROST_K_RPC: "wss://api-bifrost-kusama.n.dwellir.com/" + _yek,
-  VIRBICOIN_RPC: "https://rpc.digitalregion.jp",
 }
 
 const ENV_KEYS = [
