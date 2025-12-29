58d9aae2817e7a16e7e4ad8a6d9cb61f2530e314
jayasuryaw <blok.kjede.smidur@gmail.com>
2025-12-29 17:52:11 +0530

    ../helper/staking used in the index.js

--- projects/openLedger/index.js
+++ projects/openLedger/index.js
@@ -1,23 +1,11 @@
-const sdk = require("@defillama/sdk");
+const { staking } = require('../helper/staking')
 
 const OCTO = "0xC4B6A514449375eD2208E050540dBDf0dCAdA619";
-const TOKENS = [
-  "0xA227Cc36938f0c9E09CE0e64dfab226cad739447"
-];
-
-async function tvl(_, _b, _cb, { api }) {
-  for (const token of TOKENS) {
-    const bal = await api.call({
-      abi: "erc20:balanceOf",
-      target: token,
-      params: [OCTO],
-    });
-    api.add(token, bal);
-  }
-}
+const TOKEN = "0xA227Cc36938f0c9E09CE0e64dfab226cad739447";
 
 module.exports = {
   ethereum: {
-    tvl,
+    tvl: () => ({}),
+    staking: staking(OCTO, TOKEN),
   },
 };
