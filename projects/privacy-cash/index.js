0f97ae85ea9d5d756739959590fb1a8f90df507f
SocialFi Panda <zhew1991@gmail.com>
2025-12-31 17:13:37 -0500

    Add new token to Solana TVL calculation

--- projects/privacy-cash/index.js
+++ projects/privacy-cash/index.js
@@ -5,7 +5,7 @@ module.exports = {
   methodology: 'Value of SOL + USDC in the privacy cash pool',
   solana: { tvl: sumTokensExport({
     owners: ['2vV7xhCMWRrcLiwGoTaTRgvx98ku98TRJKPXhsS8jvBV'],
-    tokens: [ADDRESSES.solana.USDC, ADDRESSES.solana.USDT, 'A7bdiYdS5GjqGFtxf17ppRHtDKPkkRqbKtR27dxvQXaS'],
+    tokens: [ADDRESSES.solana.USDC, ADDRESSES.solana.USDT, 'A7bdiYdS5GjqGFtxf17ppRHtDKPkkRqbKtR27dxvQXaS', 'oreoU2P8bN6jkk3jbaiVxYnG1dCXcYxwhwyK9jSybcp'],
     solOwners: ['4AV2Qzp3N4c9RfzyEbNZs2wqWfW4EwKnnxFAZCndvfGh']
   }) },
 }
