f919a1e72d5c67087e6ac9cb23abeedf6febcdcc
Smriti-08 <smriti@lampros.tech>
2026-01-01 11:08:32 +0530

    Refactor TVL function and add staking support

--- projects/earnm/index.js
+++ projects/earnm/index.js
@@ -1,27 +1,15 @@
-const sdk = require("@defillama/sdk");
+const { staking } = require('../helper/staking')
+
+
+const TOKEN = "0x3e62fED35c97145e6B445704B8CE74B2544776A9"
+const STAKING_CONTRACT = "0xA9F4ee72439afC704db48dc049CbFb7E914aD300"
 
 module.exports = {
   arbitrum: {
-    tvl: async (api) => {
-      const tokenAddress = "0x3e62fED35c97145e6B445704B8CE74B2544776A9";
-      const ownerAddress = "0xA9F4ee72439afC704db48dc049CbFb7E914aD300";
-
-      const balance = await api.call({
-        target: tokenAddress,
-        abi: 'erc20:balanceOf',
-        params: [ownerAddress]
-      });
-
-      console.log("Raw Balance:", balance);
-      console.log("Human Readable Balance:", Number(balance) / 1e18);
-      
-      // The api.addCGToken already handles decimals automatically
-      // But if it's not working, you can divide manually:
-      const normalizedBalance = BigInt(balance) / BigInt(1e18);
-      
-      api.addCGToken("earnm", normalizedBalance.toString());
-      
-      return api.getBalances();
-    },
+    tvl: () => ({}),
+    staking: staking(
+      STAKING_CONTRACT,
+      TOKEN,
+    ),
   },
-};
+}
