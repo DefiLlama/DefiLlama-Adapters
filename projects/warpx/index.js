3229aae594502b0f5cfff92059313dd07d7af1b0
vnaysngh <singhvinay2525@gmail.com>
2026-01-01 23:29:50 +0700

    feat: add Warpx V2 Mainnet adapter

---
+++ projects/warpx/index.js
@@ -0,0 +1,16 @@
+const { getUniTVL } = require('../helper/unknownTokens')
+
+const FACTORY = "0xB3Ae00A68F09E8b8a003B7669e2E84544cC4a385"
+const WMEGA = "0x4200000000000000000000000000000000000006"
+
+module.exports = {
+  misrepresentedTokens: true,
+  megaeth: {
+    tvl: getUniTVL({
+      factory: FACTORY, 
+      useDefaultCoreAssets: true,
+      fetchBalances: true,
+      coreAssets: [WMEGA],
+    })
+  }
+}
