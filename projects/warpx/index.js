6d6f0e1fc20906626e156c52ec5ae1ec1f9ab596
vnaysngh <singhvinay2525@gmail.com>
2026-01-02 01:19:56 +0700

    fix: rename WMEGA to WETH in Warpx adapter

--- projects/warpx/index.js
+++ projects/warpx/index.js
@@ -1,7 +1,7 @@
 const { getUniTVL } = require('../helper/unknownTokens')
 
 const FACTORY = "0xB3Ae00A68F09E8b8a003B7669e2E84544cC4a385"
-const WMEGA = "0x4200000000000000000000000000000000000006"
+const WETH = "0x4200000000000000000000000000000000000006"
 
 module.exports = {
   misrepresentedTokens: true,
@@ -10,7 +10,7 @@ module.exports = {
       factory: FACTORY, 
       useDefaultCoreAssets: true,
       fetchBalances: true,
-      coreAssets: [WMEGA],
+      coreAssets: [WETH],
     })
   }
 }
