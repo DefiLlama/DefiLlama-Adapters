ea34ccbb642755877e59d9e002e15eaec967b744
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-01-02 18:52:19 +0100

    update megaeth

--- projects/megaeth-bridge/index.js
+++ projects/megaeth-bridge/index.js
@@ -7,6 +7,7 @@ module.exports = {
         api,
         owners: [
           "0x7f82f57F0Dd546519324392e408b01fcC7D709e8",
+          "0x0ca3a2fbc3d770b578223fbb6b062fa875a2ee75",
         ],
         fetchCoValentTokens: true,
         permitFailure: true
