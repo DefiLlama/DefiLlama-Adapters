99dc52a6857f1fe7a012a4f6550f281f3dc7f38e
blok.kjede.smidur <108473400+jayasuryaw@users.noreply.github.com>
2026-01-05 14:59:37 +0530

    Update OneLedger OCTO adapter with multi-chain staking support (#17539)

--- projects/openLedger/index.js
+++ projects/openLedger/index.js
@@ -1,11 +1,19 @@
 const { staking } = require('../helper/staking')
 
-const OCTO = "0xC4B6A514449375eD2208E050540dBDf0dCAdA619";
-const TOKEN = "0xA227Cc36938f0c9E09CE0e64dfab226cad739447";
+const TOKEN = "0xA227Cc36938f0c9E09CE0e64dfab226cad739447"
+const OCTO = "0xC4B6A514449375eD2208E050540dBDf0dCAdA619"
+
+//same hex, different chains
+const ETH_STAKING = "0xaDB85bDF08E492E9B62B0d0F705113d1E379ED85"
+const BNB_STAKING = "0xaDB85bDF08E492E9B62B0d0F705113d1E379ED85"
 
 module.exports = {
   ethereum: {
     tvl: () => ({}),
-    staking: staking(OCTO, TOKEN),
+    staking: staking([OCTO, ETH_STAKING], TOKEN),
   },
-};
+  bsc: {
+    tvl: () => ({}),
+    staking: staking(BNB_STAKING, TOKEN),
+  },
+}
