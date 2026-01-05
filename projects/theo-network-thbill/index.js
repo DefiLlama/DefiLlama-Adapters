71285212f1531abff3b219daaf8c95916becf27e
joshhchun <jchun2@nd.edu>
2026-01-05 15:14:05 -0500

    add solana

--- projects/theo-network-thbill/index.js
+++ projects/theo-network-thbill/index.js
@@ -3,9 +3,12 @@ const { sumTokensExport } = require("../helper/unwrapLPs")
 
 module.exports = {
   ethereum: {
-    tvl: sumTokensExport({ owner: '0xAECCa546baFB16735b273702632C8Cbb83509d8F', tokens: ['0x50293DD8889B931EB3441d2664dce8396640B419', ADDRESSES.ethereum.USDC,]})
+    tvl: sumTokensExport({ owner: '0xAECCa546baFB16735b273702632C8Cbb83509d8F', tokens: ['0x50293DD8889B931EB3441d2664dce8396640B419', ADDRESSES.ethereum.USDC,] })
   },
   arbitrum: {
-    tvl: sumTokensExport({ owner: '0xAECCa546baFB16735b273702632C8Cbb83509d8F', tokens: ['0xc26af85ede9cc25d449bcebef866bb85afd5d346', ]})
+    tvl: sumTokensExport({ owner: '0xAECCa546baFB16735b273702632C8Cbb83509d8F', tokens: ['0xc26af85ede9cc25d449bcebef866bb85afd5d346',] })
+  },
+  solana: {
+    tvl: sumTokensExport({ owner: 'HzENMcyK2jP5P5o38pVz1kDmBH8kAvjCHRMQXkfgV8rW', tokens: ['9DRPPWYud8i6CaSsDsFESs1xyVr8dBCMtjPZji2xiZEa',] })
   },
 }
