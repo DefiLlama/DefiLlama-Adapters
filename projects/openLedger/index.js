0c77e549f05f1fa872ca674f803fa0e043e20487
jayasuryaw <blok.kjede.smidur@gmail.com>
2026-01-05 23:34:36 +0530

    removed unused line

--- projects/openLedger/index.js
+++ projects/openLedger/index.js
@@ -1,8 +1,5 @@
 const { staking } = require('../helper/staking')
-const { sumTokensExport } = require('../helper/unwrapLPs')
 const ADDRESSES = require('../helper/coreAssets.json')
-
-
 
 const TOKEN = "0xA227Cc36938f0c9E09CE0e64dfab226cad739447"
 const OCTO = "0xC4B6A514449375eD2208E050540dBDf0dCAdA619"
