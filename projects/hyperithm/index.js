c3b2fbe465a5f23b632ce13208d124e0ff18b8f5
Dolchan <159571699+chandoling@users.noreply.github.com>
2026-01-05 19:35:00 +0900

    Added Hyperithm's Midas mTokens (#17555)

--- projects/hyperithm/index.js
+++ projects/hyperithm/index.js
@@ -33,7 +33,18 @@ const vaultConfigs = {
 const mtokenConfig = {
   ethereum: {
     mHYPER: '0x9b5528528656DBC094765E2abB79F293c21191B9',
-  }
+    mHyperETH: '0x5a42864b14C0C8241EF5ab62Dae975b163a2E0C1',
+    mHyperBTC: '0xC8495EAFf71D3A563b906295fCF2f685b1783085',
+  },
+  xrplevm: {
+    mXRP: '0x06e0B0F1A644Bb9881f675Ef266CeC15a63a3d47',
+  },
+  plasma: {
+    mHYPER: '0xb31BeA5c2a43f942a3800558B1aa25978da75F8a',
+  },
+  bsc: {
+    mXRP: '0xc8739fbBd54C587a2ad43b50CbcC30ae34FE9e34',
+  },
 };
 
 const adapterExport = getCuratorExport(vaultConfigs);
