48b8c9efb372193428e8433dbd89aa3cc2d7d4ba
joshhchun <jchun2@nd.edu>
2026-01-06 11:29:41 -0500

    Fix TVL for underlying ULTRA on Solana

--- projects/theo-network-thbill/index.js
+++ projects/theo-network-thbill/index.js
@@ -1,5 +1,19 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport } = require("../helper/unwrapLPs")
+const { getConnection } = require("../helper/solana")
+const { PublicKey } = require("@solana/web3.js")
+
+const ULTRA_TOKEN_ACCOUNT = '6fk5UwZXF1Zs327zV5Fbmay2xTYCqg7eM5QeNQyyu7ae'
+const ULTRA_ETH = '0x50293DD8889B931EB3441d2664dce8396640B419'
+
+async function solanaTvl(api) {
+  const connection = getConnection()
+  const accountInfo = await connection.getAccountInfo(new PublicKey(ULTRA_TOKEN_ACCOUNT))
+  if (accountInfo) {
+    const balance = Number(accountInfo.data.readBigUInt64LE(64))
+    api.add('ethereum:' + ULTRA_ETH, balance, { skipChain: true })
+  }
+}
 
 module.exports = {
   ethereum: {
@@ -9,6 +23,6 @@ module.exports = {
     tvl: sumTokensExport({ owner: '0xAECCa546baFB16735b273702632C8Cbb83509d8F', tokens: ['0xc26af85ede9cc25d449bcebef866bb85afd5d346',] })
   },
   solana: {
-    tvl: sumTokensExport({ owner: 'HzENMcyK2jP5P5o38pVz1kDmBH8kAvjCHRMQXkfgV8rW', tokens: ['9DRPPWYud8i6CaSsDsFESs1xyVr8dBCMtjPZji2xiZEa',] })
+    tvl: solanaTvl
   },
 }
