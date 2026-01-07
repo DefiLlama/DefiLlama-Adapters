da07735029a7916816472a8fc2860cbfec0c2828
DocPis <docpis@tutamail.com>
2026-01-07 22:11:24 +0100

    Update WETH address in index.js

--- projects/currentx/index.js
+++ projects/currentx/index.js
@@ -4,7 +4,7 @@ const sdk = require('@defillama/sdk')
 const uniTvl = getUniTVL({ factory: '0x1F49127E87A1B925694a67C437dd2252641B3875', useDefaultCoreAssets: true })
 const masterchef  = '0x0e59533B28df0537bc28D05618a2c4f20EBE07a0'
 const crx = '0x128D4F902eC739339F05bbCE778Fb474ba7617b0'
-const weth = ADDRESSES.megaeth.WETH
+const weth = '0x4200000000000000000000000000000000000006'
 
 module.exports = {
   misrepresentedTokens: true,
@@ -18,4 +18,5 @@ module.exports = {
       tokens: [crx],
     })
   }
+
 };
