2eb0e80e39b4ffe999bff9f00d0d01a73137d59a
Leon <leondo123@gmail.com>
2026-01-12 11:29:27 -0600

    Add PEPU Chain and DEX

---
+++ projects/pepu/index.js
@@ -0,0 +1,8 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  pepu: {
+    factory: '0x5984B8BF2d4dB9C0aCB1d7924762e4474D80C807',
+    fromBlock: 20,
+  },
+})
