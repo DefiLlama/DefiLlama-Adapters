8c4eae39e8d01d72212769f6913d6cd36b6a8530
Sheikh Wasif <wasif28@hotmail.com>
2026-01-13 17:00:08 +0500

    Rain dynamic token support

--- projects/rain-one/index.js
+++ projects/rain-one/index.js
@@ -1,10 +1,21 @@
+const { sumTokens2 } = require('../helper/unwrapLPs')
 const { getLogs2 } = require('../helper/cache/getLogs')
 const ADDRESSES = require('../helper/coreAssets.json')
 
+const RAIN_TOKEN = "0x25118290e6a5f4139381d072181157035864099d"
 const RAIN_PROTOCOL_FACTORY = "0xccCB3C03D9355B01883779EF15C1Be09cf3623F1"
 
-async function rainProtocolTvl(api) {
+const NON_CIRCULATING_ADDRESSES = [
+    "0xc41A5462362f6AB4A878AE0Aac91b7259f7176A5", // Claim Presale Contract
+]
 
+// ---- Treasury TVL ----
+async function vesting(api) {
+    return api.sumTokens({ owners: NON_CIRCULATING_ADDRESSES, tokens: [RAIN_TOKEN] })
+}
+
+async function rainProtocolTvl(api) {
+    // Get all pools
     const logs = await getLogs2({
         api,
         target: RAIN_PROTOCOL_FACTORY,
@@ -12,17 +23,30 @@ async function rainProtocolTvl(api) {
         fromBlock: 307025521,
     })
 
-    const poolAddresses = logs.map(log => log.poolAddress)
-    if (poolAddresses.length === 0) return {}
+    const poolAddresses = logs.map(l => l.poolAddress)
+    if (!poolAddresses.length) return {}
 
-    // Sum USDT balances in all pools
-    return api.sumTokens({ owners: logs.map(log => log.poolAddress), tokens: [ADDRESSES.arbitrum.USDT] })
+    // Get baseToken for each pool
+    const tokensAndOwners = await Promise.all(
+        poolAddresses.map(async pool => {
+            let token = await api.call({
+                target: pool,
+                abi: 'function baseToken() view returns (address)',
+            }).catch(() => ADDRESSES.arbitrum.USDT) // fallback if call fails
+            token = token || ADDRESSES.arbitrum.USDT
+            return [token.toLowerCase(), pool.toLowerCase()]
+        })
+    )
+
+    // Use sumTokens2 to include LP unwrapping if needed
+    return sumTokens2({ api, tokensAndOwners, resolveLP: true })
 }
 
 module.exports = {
     methodology:
-        "TVL includes pools created on rain.one platform.",
+        "TVL includes RAIN tokens held in Rain.one’s vesting/presale contracts and pools created on rain.one platform.",
     arbitrum: {
         tvl: rainProtocolTvl,
+        vesting,
     },
 }
