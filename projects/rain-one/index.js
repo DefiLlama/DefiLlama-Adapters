d3236b91ca33a1732e5cd1a30650ad2ff647e309
Sheikh Wasif <wasif28@hotmail.com>
2026-01-15 16:29:31 +0500

    Update index.js

--- projects/rain-one/index.js
+++ projects/rain-one/index.js
@@ -1,18 +1,7 @@
 const { sumTokens2 } = require('../helper/unwrapLPs')
 const { getLogs2 } = require('../helper/cache/getLogs')
 const ADDRESSES = require('../helper/coreAssets.json')
-
-const RAIN_TOKEN = "0x25118290e6a5f4139381d072181157035864099d"
 const RAIN_PROTOCOL_FACTORY = "0xccCB3C03D9355B01883779EF15C1Be09cf3623F1"
-
-const NON_CIRCULATING_ADDRESSES = [
-    "0xc41A5462362f6AB4A878AE0Aac91b7259f7176A5", // Claim Presale Contract
-]
-
-// ---- Treasury TVL ----
-async function vesting(api) {
-    return api.sumTokens({ owners: NON_CIRCULATING_ADDRESSES, tokens: [RAIN_TOKEN] })
-}
 
 async function rainProtocolTvl(api) {
     // Get all pools
@@ -44,9 +33,8 @@ async function rainProtocolTvl(api) {
 
 module.exports = {
     methodology:
-        "TVL includes RAIN tokens held in Rain.one’s vesting/presale contracts and pools created on rain.one platform.",
+        "TVL includes pools created on rain.one platform.",
     arbitrum: {
         tvl: rainProtocolTvl,
-        vesting,
     },
 }
