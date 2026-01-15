507d7a9978e5aadda2b592cac3f89fc9188c4a10
0xBugsy <0xbuggy2@proton.me>
2026-01-15 20:26:14 +0000

    Fix output nesting issue

--- projects/hermes-protocol/index.js
+++ projects/hermes-protocol/index.js
@@ -1,8 +1,10 @@
 const { uniTvlExports } = require('../helper/unknownTokens')
 
+const tvlExports = uniTvlExports({'metis': '0x633a093C9e94f64500FC8fCBB48e90dd52F6668F'}, { hasStablePools: true, })
+
 module.exports = {
   metis: {
-    tvl: uniTvlExports({'metis': '0x633a093C9e94f64500FC8fCBB48e90dd52F6668F'}, { hasStablePools: true, }),
+    ...tvlExports.metis,
     staking: () => ({})
   },
   hallmarks: [
