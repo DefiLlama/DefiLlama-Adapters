91ccd97d747ad9e986a7487b97d6fe17b6aa235a
Jarryd Allison <jallison@figure.com>
2026-01-15 12:59:49 -0700

    feat: update hastra adapter

--- projects/hastra/index.js
+++ projects/hastra/index.js
@@ -1,21 +1,32 @@
-const { getTokenAccountBalances, getTokenSupplies } = require('../helper/solana')
-const { sumTokens2 } = require('../helper/unwrapLPs');
+const { getTokenAccountBalances } = require('../helper/solana')
 
-// const PRIMEAccount = '3b8X44fLF9ooXaUm3hhSgjpmVs6rZZ3pPoGnGahc3Uu7'
 const wYLDSVaultAccount = "FvkbfMm98jefJWrqkvXvsSZ9RFaRBae8k6c1jaYA5vY3"
 const RedeemVaultAccount = "HH1hSzaBKvBDf7GWD1mw557Q8LwBPHHE63WEu6BURS8X"
 
 async function solanaTvl(api) {
-    const wYLDSVaultBalance = await getTokenAccountBalances([wYLDSVaultAccount])
-    Object.keys(wYLDSVaultBalance).map(token => api.add(token, wYLDSVaultBalance[token]))
-    const redeemVaultBalance = await getTokenAccountBalances([RedeemVaultAccount])
-    Object.keys(redeemVaultBalance).map(token => api.add(token, redeemVaultBalance[token]))
-    return api.getBalances()
+  const balances = await getTokenAccountBalances([wYLDSVaultAccount, RedeemVaultAccount])
+  Object.entries(balances).forEach(([token, balance]) => {
+      api.add(token, balance);
+  });
+
+  return api.getBalances();
+}
+
+async function staking(api) {
+  const balances = await getTokenAccountBalances([wYLDSVaultAccount])
+  Object.entries(balances).forEach(([token, balance]) => {
+      api.add(token, balance);
+  });
+
+  return api.getBalances();
 }
 
 module.exports = {
   doublecounted: true,
   timetravel: true,
   methodology: 'Hastra TVL consists of the amount of vaulted wYLDS, plus the amount of unredeemed tokens in the redemption vault.',
-  solana: { tvl: solanaTvl },
+  solana: { 
+    tvl: solanaTvl,
+    staking: staking 
+  },
 };
