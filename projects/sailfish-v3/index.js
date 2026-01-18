c8184e64e3372f67606cd08b4f026c6b3119cad4
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2026-01-18 19:12:30 +0400

    Update Sailfish V3 subgraph to active URL

--- projects/sailfish-v3/index.js
+++ projects/sailfish-v3/index.js
@@ -6,6 +6,6 @@ const { uniV3Export, uniV3GraphExport } = require('../helper/uniswapV3')
 
 module.exports = {
   occ: {
-    tvl: uniV3GraphExport({ graphURL: 'https://api.goldsky.com/api/public/project_cm5nst0b7iiqy01t6hxww7gao/subgraphs/sailfish-v3-occ-mainnet/1.0.0/gn', name: 'sailfish-v3' }),
+    tvl: uniV3GraphExport({ graphURL: 'https://api.goldsky.com/api/public/project_cm1s79wa2tlb701tbchmeaflf/subgraphs/sailfish-v3-occ-mainnet/1.0.3/gn', name: 'sailfish-v3' }),
   }
 }
