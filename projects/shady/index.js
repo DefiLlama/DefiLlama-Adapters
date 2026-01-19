c9e8d217e42cc93b2ea3ecbf827d37175ec180b3
rohannero <rohanzarathustra@gmail.com>
2026-01-19 16:53:27 -0500

    fix

---
+++ projects/shady/index.js
@@ -0,0 +1,19 @@
+const { sumTokens2 } = require('../helper/solana')
+
+const owner = '6WEsL1dvUQbTwjtMvKvZZKqv4GwG6b9qfCQSsa4Bpump' // Shady program ID
+
+async function tvl(api) {
+    return sumTokens2({
+        api,
+        solOwners: [owner],
+        tokenAccounts: [
+            '7DjuVG7uiwcvjtVz23mwyBGqfMRS12nGhrvT459S36kx', // Shady token account
+            '3nnSedpFLRGa16CAyqQMj5EcxGGgbsBz4fq37T3MosBQ', // WSOL token account
+        ]
+    })
+}
+
+module.exports = {
+    timetravel: false,
+    solana: { tvl },
+}
