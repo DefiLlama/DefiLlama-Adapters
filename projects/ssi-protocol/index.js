ebe4cc56e5addf6777010a3eb4f41b10416827eb
urvi-bala <urvibala@lampros.tech>
2026-01-20 17:08:16 +0530

    Fixed the adapter

--- projects/ssi-protocol/index.js
+++ projects/ssi-protocol/index.js
@@ -1,56 +1,61 @@
 const sdk = require('@defillama/sdk')
 const { getBlock } = require('../helper/http');
 
-const abi = {
-  getBasket: 'function getBasket() view returns ((string chain, string symbol, string addr, uint8 decimals, uint256 amount)[])'
-}
-
 const ssi_tokens = [
-  // MAG7.ssi
-  '0x9E6A46f294bB67c20F1D1E7AfB0bBEf614403B55',
-  // DEFI.ssi
-  '0x164ffdaE2fe3891714bc2968f1875ca4fA1079D0',
-  // MEME.ssi
-  '0xdd3acDBDc7b358Df453a6CB6bCA56C92aA5743aA'
+  '0x9E6A46f294bB67c20F1D1E7AfB0bBEf614403B55', // MAG7.ssi
+  '0x164ffdaE2fe3891714bc2968f1875ca4fA1079D0', // DEFI.ssi
+  '0xdd3acDBDc7b358Df453a6CB6bCA56C92aA5743aA', // MEME.ssi
 ]
 
+const abi = {
+  getBasket: 'function getBasket() view returns (tuple(string chain, string symbol, string addr, uint8 decimals, uint256 amount)[])'
+}
+
 function underlyingExports(chains) {
-  const exportObj = {};
+  const exportObj = {}
+  
   Object.entries(chains).forEach(([chain, [chain_name, native_token]]) => {
     exportObj[chain] = {
       tvl: async (api, ethBlock, chainBlocks) => {
-        const balances = {};
-        const base_block = await getBlock(api.timestamp, 'base', chainBlocks);
+        const balances = {}
+        const base_block = await getBlock(api.timestamp, 'base', chainBlocks)
+        
         const baskets = await Promise.all(ssi_tokens.map(async (ssi_token) => {
           const res = await sdk.api.abi.call({
             abi: abi.getBasket,
             target: ssi_token,
             chain: 'base',
             block: base_block,
-          });
-          return res.output;
-        }));
+          })
+          return res.output || []
+        }))
+        
         baskets.forEach(basket => {
           basket.forEach(token => {
-            if (token.chain == chain_name) {
-              let token_addr;
-              let token_amount;
-              if (token.addr != '') {
-                token_addr = chain + ':' + token.addr;
-                token_amount = token.amount;
+            if (token.chain === chain_name) {
+              let token_addr, token_amount
+              
+              if (!token.addr || token.addr === '') {
+                // Native token
+                token_addr = native_token
+                token_amount = token.amount / 10 ** token.decimals
               } else {
-                token_addr = native_token;
-                token_amount = token.amount / 10 ** token.decimals;
+                // ERC20/Token with address
+                token_addr = `${chain}:${token.addr}`
+                token_amount = token.amount
               }
-              sdk.util.sumSingleBalance(balances, token_addr, token_amount);
+              
+              sdk.util.sumSingleBalance(balances, token_addr, token_amount)
             }
-          });
-        });
-        return balances;
+          })
+        })
+        
+        return balances
       }
     }
-  });
-  return exportObj;
+  })
+  
+  return exportObj
 }
 
 module.exports = {
@@ -64,4 +69,4 @@ module.exports = {
     cardano: ['ADA', 'cardano'],
     ripple: ['XRP', 'ripple'],
   })
-};
+}
