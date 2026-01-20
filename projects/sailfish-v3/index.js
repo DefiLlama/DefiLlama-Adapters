3e214337d6c5d80ec8d0f2b18d4c901a9c6948ff
Olu <oluwafemialofe@yahoo.com>
2026-01-20 22:46:03 +0400

    Refactor index.js to consolidate exports

--- projects/sailfish-v3/index.js
+++ projects/sailfish-v3/index.js
@@ -5,7 +5,3 @@ module.exports = {
     tvl: uniV3GraphExport({ graphURL: 'https://api.goldsky.com/api/public/project_cm1s79wa2tlb701tbchmeaflf/subgraphs/sailfish-v3-occ-mainnet/1.0.3/gn', name: 'sailfish-v3' }),
   }
 }
-
-module.exports = uniV3Export({
-  occ: { factory: '0x963A7f4eB46967A9fd3dFbabD354fC294FA2BF5C', fromBlock: 142495, },
-})
