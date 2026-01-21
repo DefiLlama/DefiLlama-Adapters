9363edd1953db362bf043cd5c928adf38479d055
Straucoin <straucoin@gmail.com>
2026-01-21 11:53:47 -0300

    fix

--- projects/pagcrypto/config.js
+++ projects/pagcrypto/config.js
@@ -13,11 +13,6 @@ module.exports = {
             wallet: "5XvzUs92L7G4picBJchfatM25RcR93oE3h8xGRZe7462",
         },
 
-        xrpl: {
-            status: "active",
-            wallet: "r421rbtEZxdniuMmKbq7qzFNnxxHssVYn6",
-        },
-
         tron: {
             status: "active",
             wallet: "TA9Xywe3xb6GPeBFYDdTkdT43DktDPnyDT",
@@ -48,9 +43,16 @@ module.exports = {
             wallet: "0xC8e3BC38C3e4D768f83a1a064BdE4045aFf3158C",
         },
 
+        xrpl: {
+            status: "active",
+            wallet: "r421rbtEZxdniuMmKbq7qzFNnxxHssVYn6",
+            unsupported_here: true
+        },
+
         toncoin: {
             status: "active",
             wallet: "UQBYm7IGk0XmqcwRDHm9vtJWu05im1P3zU7pJYCnG8rymMxf",
+            unsupported_here: true
         },
     },
 
