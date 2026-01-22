985b93c5c1f6af5346d304001e3fa0ab80d80842
T0xicPotato <iamlovewhenyouscream@protonmail.com>
2026-01-22 19:25:12 +0800

    fix merge

--- projects/sharwa-finance/index.js
+++ projects/sharwa-finance/index.js
@@ -1,16 +1,16 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
+
 const legacy_pools = {
-const pools = {
   WETH: '0xc2c105e981F46a011D19511F8a118991663B136b',
   WBTC: '0x20D96638DA7B7e8FD7B78427EA49048d4A847946',
   USDC: '0x02434cD23972C82FbAbf610D157b41bFB45A45a3',
 }
 
 const pools = {
-USDC: '0xA4113Ac6Cc41141B819f34d81F6ccdabcA07AecF',
-WETH: '0x2839851d52e4dce3c714D199716b5f0fc9dbbaAa',
-WBTC: '0x6302e6683422cAFdC48fBA98309b559AABa95386',
+  USDC: '0xA4113Ac6Cc41141B819f34d81F6ccdabcA07AecF',
+  WETH: '0x2839851d52e4dce3c714D199716b5f0fc9dbbaAa',
+  WBTC: '0x6302e6683422cAFdC48fBA98309b559AABa95386',
 }
 
 const legacy_margin_account = '0x5c479762C8Fe57B6D874893a4B4932B40F612580'
@@ -32,20 +32,8 @@ async function tvl(api) {
     [ADDRESSES.arbitrum.USDC, margin_account],
     [ADDRESSES.arbitrum.WETH, margin_account],
     [ADDRESSES.arbitrum.WBTC, margin_account],
-const marginAccount = '0x5c479762C8Fe57B6D874893a4B4932B40F612580'
-
-async function tvl(api) {
-  const tokensAndOwners = [
-    [ADDRESSES.arbitrum.WETH, pools.WETH],
-    [ADDRESSES.arbitrum.WBTC, pools.WBTC],
-    [ADDRESSES.arbitrum.USDC, pools.USDC],
-    [ADDRESSES.arbitrum.USDC_CIRCLE, pools.USDC],
-    [ADDRESSES.arbitrum.USDC, marginAccount],
-    [ADDRESSES.arbitrum.USDC_CIRCLE, marginAccount],
-    [ADDRESSES.arbitrum.WETH, marginAccount],
-    [ADDRESSES.arbitrum.WBTC, marginAccount],
   ]
-  
+
   return api.sumTokens({ tokensAndOwners })
 }
 
