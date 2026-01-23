ec0c8c62ea207dfaddaae5eeb7d5aef86ca910bd
burhanuddin03 <burhanuddinmshabbir@gmail.com>
2026-01-23 19:01:52 +0500

    add ember ethereum tvl

--- projects/ember-protocol/index.js
+++ projects/ember-protocol/index.js
@@ -1,8 +1,12 @@
 const { sui } = require("../helper/chain/rpcProxy");
 const { getConfig } = require("../helper/cache");
+const {sumERC4626Vaults} = require("../helper/erc4626");
 
-const PACKAGE_ID =
+const SUI_PACKAGE_ID =
   "0xc83d5406fd355f34d3ce87b35ab2c0b099af9d309ba96c17e40309502a49976f";
+
+const SUI_CHAIN_IDENTIFIER = "sui";
+const ETHEREUM_CHAIN_IDENTIFIER = "ethereum";
 
 // there are only one deposit address
 const blacklistedVaults = [
@@ -11,22 +15,46 @@ const blacklistedVaults = [
 
 async function suiTvl(api) {
   const vaults = (
-    await getConfig('ember-protocol/vaults', `https://vaults.api.sui-prod.bluefin.io/api/v1/vaults`)
+    await getConfig('ember-protocol/vaults', `https://vaults.api.sui-prod.bluefin.io/api/v2/vaults`)
   );
-  for (const vault of Object.values(vaults).filter(v => !blacklistedVaults.includes(v.id))) {
+  for (const vault of Object.values(vaults)) {
+    
+    const suiVault = vault.detailsByChain[SUI_CHAIN_IDENTIFIER];
+    if (!suiVault || blacklistedVaults.includes(suiVault.address)) {
+      continue;
+    }
+    console.log(suiVault);
     const vaultTvl = await sui.query({
-      target: `${PACKAGE_ID}::vault::get_vault_tvl`,
-      contractId: vault.address,
-      typeArguments: [vault.depositCoin.address, vault.receiptCoin.address],
+      target: `${SUI_PACKAGE_ID}::vault::get_vault_tvl`,
+      contractId: suiVault.address,
+      typeArguments: [suiVault.baseDepositCoin.address, suiVault.receiptCoin.address],
       sender:
         "0xbaef681eafe323b507b76bdaf397731c26f46a311e5f3520ebb1bde091fff295",
     });
-    api.add(vault.depositCoin.address, vaultTvl[0]);
+    api.add(suiVault.baseDepositCoin.address, vaultTvl);
   }
+}
+
+async function ethereumTvl(api) {
+  const vaults = (
+    await getConfig('ember-protocol/vaults', `https://vaults.api.sui-prod.bluefin.io/api/v2/vaults`)
+  );
+  const ethereumVaultAddresses = []
+  for (const vault of Object.values(vaults)) {
+      const ethereumVault = vault.detailsByChain[ETHEREUM_CHAIN_IDENTIFIER];
+      if (!ethereumVault) {
+        continue;
+      }
+      ethereumVaultAddresses.push(ethereumVault.address);
+  }
+  await sumERC4626Vaults({ api, calls: ethereumVaultAddresses, });
 }
 
 module.exports = {
   sui: {
     tvl: suiTvl,
+  },
+  ethereum: {
+    tvl: ethereumTvl,
   },
 };
