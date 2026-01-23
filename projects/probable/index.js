dbde445ad47750569a57b3aca3ded0aeba386a99
defioasis <defioasis.liu@gmail.com>
2026-01-23 22:25:19 +0800

    Add Probable TVL adapter (BSC)

---
+++ projects/probable/index.js
@@ -0,0 +1,10 @@
+const { sumTokensExport } = require("../helper/unwrapLPs");
+
+const TREASURY = "0x364d05055614B506e2b9A287E4ac34167204cA83";
+const USDT = "0x55d398326f99059fF775485246999027B3197955";
+
+module.exports = {
+  bsc: {
+    tvl: sumTokensExport({ tokens: [[USDT, TREASURY]] }),
+  },
+};
