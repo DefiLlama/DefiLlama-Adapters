cae1ed4c588e41993141867c66da4088a522e0fc
Joeh <joeh@sobal.fi>
2026-01-23 18:22:12 +0800

    feat: use indexed data for accurate tvl

--- projects/kumbaya/index.js
+++ projects/kumbaya/index.js
@@ -1,4 +1,25 @@
-const { uniV3Export } = require('../helper/uniswapV3')
-module.exports = uniV3Export({
-  megaeth: { factory: '0x68b34591f662508076927803c567Cc8006988a09', fromBlock: 3520272 },
-})
+const { graphQuery } = require("../helper/http");
+
+const ENVIO_GRAPHQL_URL = "https://kby-hasura.up.railway.app/v1/graphql";
+
+const query = `
+  query {
+    UniswapDayData(
+      where: { chainId: { _eq: 4326 } }
+      order_by: { date: desc }
+      limit: 1
+    ) {
+      tvlUSD
+    }
+  }
+`;
+
+async function tvl() {
+  const data = await graphQuery(ENVIO_GRAPHQL_URL, query);
+  const tvlUSD = data?.UniswapDayData?.[0]?.tvlUSD || 0;
+  return { tether: parseFloat(tvlUSD) };
+}
+
+module.exports = {
+  megaeth: { tvl },
+};
