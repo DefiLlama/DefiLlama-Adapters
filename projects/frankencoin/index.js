86a56a368cce795ec027f5b46d35f81547e00dc0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-01-23 23:22:45 +0100

    break frankencoin

--- projects/frankencoin/index.js
+++ projects/frankencoin/index.js
@@ -5,6 +5,7 @@ const XCHFBridge = ["0xb4272071ecadd69d933adcd19ca99fe80664fc08", "0x7bbe8F18040
 const VCHFBridge = ["0x79d4f0232A66c4c91b89c76362016A1707CFBF4f", "0x3b71ba73299f925a837836160c3e1fec74340403"];
 
 async function tvl(api) {
+  throw new Error("Disabled for now till BOSS token is priced correctly");
   // @dev: register bridged token as collateral for backing and TVL
   const tokensAndOwners = [XCHFBridge, VCHFBridge];
 
