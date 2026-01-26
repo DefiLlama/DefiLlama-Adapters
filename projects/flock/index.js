e3c174a2aede7aa69599ca9704f40416ca1b6a54
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-01-26 21:07:36 +0100

    minor fix

--- projects/flock/index.js
+++ projects/flock/index.js
@@ -1,20 +1,13 @@
+const { staking } = require("../helper/staking");
+
 const FLOCK_TOKEN_CONTRACT = '0x5aB3D4c385B400F3aBB49e80DE2fAF6a88A7B691';
 const GM_EXCHANGE_CONTRACT = '0xe1Fa4592b7a35Ff6Cef65FDEC5e13A1F48fC6123';
-
-async function tvl(api) {
-  const stakedBalance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: FLOCK_TOKEN_CONTRACT,
-    params: [GM_EXCHANGE_CONTRACT],
-  });
-
-  api.add(FLOCK_TOKEN_CONTRACT, stakedBalance)
-}
 
 module.exports = {
   methodology: 'counts the number of FLOCK tokens in the FLOCK GMExchange contract.',
   start: 1747913430,
   base: {
-    tvl,
+    tvl: () => ({}),
+    staking: staking(GM_EXCHANGE_CONTRACT, FLOCK_TOKEN_CONTRACT),
   }
 }; 
