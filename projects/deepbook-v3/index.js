181d3d8df1fc9f70a5c50ad45ec17989244662af
Tony Lee <chunglee2018@u.northwestern.edu>
2026-01-27 14:03:59 -0500

    margin pool tvl

--- projects/deepbook-v3/index.js
+++ projects/deepbook-v3/index.js
@@ -2,17 +2,28 @@ const ADDRESSES = require('../helper/coreAssets.json')
 const { get } = require("../helper/http");
 
 const assetsUrl = "https://deepbook-indexer.mainnet.mystenlabs.com/assets";
-const endpointUrl = "https://deepbook-indexer.mainnet.mystenlabs.com"
-const endpointName = "get_net_deposits"
+const endpointUrl = "https://deepbook-indexer.mainnet.mystenlabs.com";
+const endpointName = "get_net_deposits";
+const marginSupplyEndpoint = "margin_supply";
 
 const tvl = async (api) => {
   const assets = await get(assetsUrl);
   const coins = Object.values(assets).map(a => a.asset_type);
   const url = `${endpointUrl}/${endpointName}/${coins.join(',')}/${api.timestamp}`
-  // console.log(url);
   const data = await get(url)
   coins.forEach(coin => {
     api.add(coin, data[coin])
+  })
+
+  // Add margin supply
+  const symbolToCoin = {}
+  Object.entries(assets).forEach(([symbol, info]) => {
+    symbolToCoin[symbol] = info.asset_type
+  })
+  const marginSupply = await get(`${endpointUrl}/${marginSupplyEndpoint}`)
+  Object.entries(marginSupply).forEach(([symbol, amount]) => {
+    const coin = symbolToCoin[symbol]
+    if (coin) api.add(coin, amount)
   })
 
   const { usdTvl } = await api.getUSDJSONs();
