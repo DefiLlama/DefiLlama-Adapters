699869388449e0b3794ce76e4b1902972017463a
rohannero <rohanzarathustra@gmail.com>
2026-01-27 13:17:55 -0500

    fix

--- projects/fira/index.js
+++ projects/fira/index.js
@@ -1,6 +1,5 @@
 const { sumTokensExport } = require("../helper/unwrapLPs")
 
-const bUSD0Token = "0x35D8949372D46B7a3D5A56006AE77B215fc69bC0";
 const USD0Token = "0x73A15FeD60Bf67631dC6cd7Bc5B6e8da8190aCF5";
 const UZRLendingMarket = "0xa428723eE8ffD87088C36121d72100B43F11fb6A"; // UZR Lending Market (MetaMorpho vault)
 const UZRMarketId = "0xa597b5a36f6cc0ede718ba58b2e23f5c747da810bf8e299022d88123ab03340e";
@@ -15,9 +14,8 @@ module.exports = {
     methodology: "Fira TVL is bonded USD0 (bUSD0) collateral deposited into the UZR vault",
     ethereum: {
         tvl: sumTokensExport({
-            tokens: [bUSD0Token],
+            tokens: [USD0Token],
             owners: [UZRLendingMarket],
-            resolveUniV3: false,
         }),
         borrowed,
     },
