0e4fb0de81526aa86b711e62d8491d50f425f96b
Jolly Walker <wz.lim.00@gmail.com>
2026-01-28 10:58:24 +0800

    feat: added new dew vaults

--- projects/dew.js
+++ projects/dew.js
@@ -1,23 +1,40 @@
+const { default: BigNumber } = require('bignumber.js')
 const { sumSingleBalance, call } = require('./helper/chain/near')
 
-const VAULT_CONTRACT = "dew-rneardefi-vault.near"
+const rNEAR = "lst.rhealab.near";
+const USDC = "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1";
+const USDT = "usdt.tether-token.near";
+const VAULTS = [
+    { 
+        contract_id: "dew-rneardefi-vault.near",
+        asset: { FungibleToken: { contract_id: rNEAR }}
+    },
+    {
+        contract_id: "usdc.meteor-vaults.near",
+        asset: { FungibleToken: { contract_id: USDC }}
+    },
+    {
+        contract_id: "usdt.meteor-vaults.near",
+        asset: { FungibleToken: { contract_id: USDT }}
+    }
+]
 
 async function tvl() {
     const balances = {}
 
-    const totalShares = await call(VAULT_CONTRACT, 'ft_total_supply', {})
-    const exchangeRateScale = await call(VAULT_CONTRACT, 'get_exchange_rate_scale', {})
-    const rNearExchangeRate = await call(VAULT_CONTRACT, 'get_exchange_rate', {
-        asset: {
-            FungibleToken: {
-                contract_id: "lst.rhealab.near"
-            }
-        }
-    })
+    await Promise.all(VAULTS.map(async (vault) => {
+        const isDewNear = vault.contract_id === "dew-rneardefi-vault.near"
+        const sharePriceMethod = isDewNear ? "get_exchange_rate" : "get_share_price_in_asset"
+        const sharePriceScaleMethod = isDewNear ? "get_exchange_rate_scale" : "get_share_price_scale"
 
-    const rNearSupplied = BigInt(totalShares) * BigInt(rNearExchangeRate) / BigInt(exchangeRateScale)
+        const totalShares = await call(vault.contract_id, 'ft_total_supply', {})
+        const exchangeRateScale = await call(vault.contract_id, sharePriceScaleMethod, {})
+        const extraDecimalScale = await call(vault.contract_id, "get_extra_decimal_scale", {})
+        const exchangeRate = await call(vault.contract_id, sharePriceMethod, { asset: vault.asset })
 
-    sumSingleBalance(balances, "lst.rhealab.near", rNearSupplied)
+        const supplied = BigNumber(totalShares) * BigNumber(exchangeRate) / BigNumber(exchangeRateScale) / BigNumber(extraDecimalScale);
+        sumSingleBalance(balances, vault.asset.FungibleToken.contract_id, supplied)
+    }))
 
     return balances
 }
