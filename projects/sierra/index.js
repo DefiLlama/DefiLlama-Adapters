23644c26f4680c23c2ac4e0dd2a14265c5539498
SierraIsMoney <mitchell@sierralabs.xyz>
2026-01-29 10:42:41 -0500

    Update index.js

--- projects/sierra/index.js
+++ projects/sierra/index.js
@@ -11,7 +11,7 @@ async function tvlAvalanche(api) {
       '0x09Ca60Ca323a6313aE144778c3EbDfCCFBB5e5D2', // OpenTrade XMMF Vault
       '0x3458F1Cab06cdf7C9323d8FffB04093F9D8380b6', // OpenTrade xMorphoGPUSDC-Base Vault
       '0x4a8094F20906a453a4A74769aa74c4012B0d5Df6', // OpenTrade xAaveUSDC-ETH Vault
-      '0x4c8eaBA17c3b30295f442A6415d495e8410a5693', // OpenTrade xWildcatWMTUSDC-ETH Vault
+      '0x4c8eaBA17c3b30295f442A6415d495e8410a5693' // OpenTrade xWildcatWMTUSDC-ETH Vault
     ],
   });
 }
