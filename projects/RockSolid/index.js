15c3fef02eec03014e77723206db260fa4f74284
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-02-02 08:46:17 +0100

    fixes #17877

--- projects/RockSolid/index.js
+++ projects/RockSolid/index.js
@@ -1,15 +1,15 @@
-const vault = '0x936facdf10c8c36294e7b9d28345255539d81bc7' // RockSolid rock.rETH
-
-async function tvl(_, _b, _cb, { api }) {
-  // Use erc4626Sum helper to automatically get underlying asset and totalAssets
-  return api.erc4626Sum({ calls: [vault], tokenAbi: 'asset', balanceAbi: 'totalAssets' })
-}
+const { sumERC4626VaultsExport2 } = require("../helper/erc4626");
 
 module.exports = {
   methodology:
-    'Calls totalAssets() on the RockSolid rock.rETH vault to get the total amount of rETH managed by the vault.',
+    'Calls totalAssets() on the RockSolid rock.rETH and rock.loopedETH vaults to get the total amount of rETH and ETH managed by the vaults.',
   start: 1756339201, //  vault launch timestamp
-  ethereum: { tvl },
-  timetravel: true,
-  misrepresentedTokens: false,
+  ethereum: {
+    tvl: sumERC4626VaultsExport2({
+      vaults: [
+        '0x936facdf10c8c36294e7b9d28345255539d81bc7', // RockSolid rock.rETH
+        '0x7a12D4B719F5aA479eCD60dEfED909fb2A37e428', // RockSolid rock.loopedETH
+      ],
+    }),
+  },
 }
