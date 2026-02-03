e7a7a9bc59115504750f05d515e77560e10ec1a5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-02-03 22:09:28 +0100

    track morpho vault

--- projects/sentora/index.js
+++ projects/sentora/index.js
@@ -8,6 +8,9 @@ const configs = {
         '0x5aB5FE7d04CFDeFb9daf61f6f569a58A53D05eE1',
         '0xe78C246ea973389F55BAEADF71e04750D50417d1'
       ],
+      morphoVaultOwners: [
+        '0x13DE0cEE0B83562CBfD46682e10FfA4E3c5090e1',
+      ],
     },
     solana: {
       kaminoLendVaultAdmins: ['7fLxEftpppneavpueYgP2s7HhSGbWpj2jTCmAEwwqonY'],
