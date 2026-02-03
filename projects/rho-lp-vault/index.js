cfb20e6cf83731f8c32f6bf52e864238942020ee
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-02-03 10:23:40 +0100

    Refactor TVL calculation using sumERC4626VaultsExport2

--- projects/rho-lp-vault/index.js
+++ projects/rho-lp-vault/index.js
@@ -1,20 +1,10 @@
+const { sumERC4626VaultsExport2 } = require("../helper/erc4626");
 
-const ADDRESSES = require('../helper/coreAssets.json')
 const VAULT = "0x4cb280e63251b9ab24a54def74bf5995d82ff398";
-const totalAssetsAbi = "uint256:totalAssets";
-
-async function tvl(api) {
-  const totalAssets = await api.call({
-    target: VAULT,
-    abi: totalAssetsAbi,
-  });
-  
-  api.add(ADDRESSES.ethereum.USDT, totalAssets);
-  return api.getBalances();
-}
 
 module.exports = {
-  ethereum: { tvl },
-  methodology:
-    "",
-};
+  doublecounted: true,
+  ethereum: {
+    tvl: sumERC4626VaultsExport2({ vaults: [VAULT], }),
+  },
+}
