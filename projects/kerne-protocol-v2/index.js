c0449c44ad2d0212d6bfd4175e9cfc8b0fb98ae7
enerzy17 <157988761+enerzy17@users.noreply.github.com>
2026-02-03 12:24:32 -0700

    feat: update kerne-protocol vault address to production

--- projects/kerne-protocol-v2/index.js
+++ projects/kerne-protocol-v2/index.js
@@ -3,7 +3,7 @@ const sdk = require('@defillama/sdk');
 // Kerne Protocol TVL Adapter
 // ERC-4626 Vault on Base
 
-const VAULT_ADDRESS = "0x5FD0F7eA40984a6a8E9c6f6BDfd297e7dB4448Bd";
+const VAULT_ADDRESS = "0xDF9a2f5152c533F7fcc3bAdEd41e157C9563C695";
 const WETH_ADDRESS = "0x4200000000000000000000000000000000000006";
 
 async function tvl(api) {
