7770c57086ae6cca28e27eed3d1d2376dd8fdc51
Alex Walch <awalch23@gmail.com>
2026-02-03 19:10:58 -0500

    Add Snuggle adapter - Uniswap V3 position manager on Base
    Snuggle is an automated concentrated liquidity manager for Uniswap V3 on Base.
    TVL is calculated by unwrapping all UniV3 NFT positions held by the vault.
    
    Vault address: 0x43Ca8D329d91ADF0aa471aC7587Aac1B2743F043
    Website: https://snuggle.fi

---
+++ projects/snuggle/index.js
@@ -0,0 +1,21 @@
+const { sumTokens2 } = require('../helper/unwrapLPs')
+
+// Snuggle Vault on Base - holds Uniswap V3 NFT positions
+const SNUGGLE_VAULT = '0x43Ca8D329d91ADF0aa471aC7587Aac1B2743F043'
+
+async function tvl(api) {
+  return sumTokens2({
+    api,
+    owners: [SNUGGLE_VAULT],
+    resolveUniV3: true,
+    permitFailure: true,
+  })
+}
+
+module.exports = {
+  methodology: 'TVL is calculated by summing the value of all Uniswap V3 concentrated liquidity positions held by the Snuggle vault contract.',
+  start: 1704067200, // January 1, 2024 (approximate launch)
+  base: {
+    tvl,
+  },
+}
