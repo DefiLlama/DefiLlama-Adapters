53d3f6c9d17f15187d5774f328d78972a1c54b9f
Alex Walch <awalch23@gmail.com>
2026-02-03 19:18:48 -0500

    Add doublecounted flag for accurate aggregate TVL

--- projects/snuggle/index.js
+++ projects/snuggle/index.js
@@ -15,6 +15,7 @@ async function tvl(api) {
 module.exports = {
   methodology: 'TVL is calculated by summing the value of all Uniswap V3 concentrated liquidity positions held by the Snuggle vault contract.',
   start: 1704067200, // January 1, 2024 (approximate launch)
+  doublecounted: true,
   base: {
     tvl,
   },
