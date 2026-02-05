0abba7e4e582228d10c11b6fe17c0828543a98c6
SierraIsMoney <mitchell@sierralabs.xyz>
2026-02-05 03:26:58 -0500

    Update index.js (#17927)

--- projects/sierra-money/index.js
+++ projects/sierra-money/index.js
@@ -6,7 +6,9 @@ const openTradeVaults = [
   '0x09Ca60Ca323a6313aE144778c3EbDfCCFBB5e5D2', // OpenTrade XMMF Vault
   '0x3458F1Cab06cdf7C9323d8FffB04093F9D8380b6', // OpenTrade xMorphoGPUSDC-Base Vault
   '0x4a8094F20906a453a4A74769aa74c4012B0d5Df6', // OpenTrade xAaveUSDC-ETH Vault
-  '0x4c8eaBA17c3b30295f442A6415d495e8410a5693' // OpenTrade xWildcatWMTUSDC-ETH Vault
+  '0x4c8eaBA17c3b30295f442A6415d495e8410a5693', // OpenTrade xWildcatWMTUSDC-ETH Vault
+  '0x27C3c978e3cBF383ed715D313A2e97c052FbbEe0', // OpenTrade xSOLY Vault
+  '0x0E57DFcF4A53cd1A19256Bd2aF8bC28a9AC7EBE8' // OpenTrade xIGCP Vault
 ];
 
 // Owner of reserves backing SIERRA, see https://debank.com/profile/0xBC7C0b1b9C61f35068561077FbaA163707128597 or https://docs.sierra.money/reserves-management/reserve-strategy
