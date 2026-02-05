d2fcdce0a03b2b2a716854e31587e81f00477d1f
yinchuan <18872117825@163.com>
2026-02-05 11:16:01 +0800

    add base, arbitrum and bsc assets

--- projects/picwe/index.js
+++ projects/picwe/index.js
@@ -1,5 +1,6 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { function_view } = require("../helper/chain/aptos");
+const { sumTokens2 } = require('../helper/unwrapLPs')
 
 const CONSTANTS = {
   PROTOCOL: {
@@ -13,6 +14,22 @@ const CONSTANTS = {
   MAX_RETRIES: 3,
   RETRY_DELAY: 1000, // ms
 };
+
+const picwe = {
+  abi: {
+    totalSupply: "uint256:totalSupply",
+    c: "uint256:c",
+  },
+  marketAddress: {
+    ihp: "0x90bd884ca808173de0b605aa4fabfeb105933d08",
+    ucar: "0x608376b122322582d22eeb6e51dd0265cb922b14",
+  },
+  rwaToken: {
+    ihp: "0x3fed4274cce069f6e5b21c643aee874d2205f610",
+    ucar: "0x5bdc43507550a414e6970d6e43531f8031e34471",
+  }
+}
+const mintRedeem = '0x5D54f1092FD1750A3ab96972dc1867c5B23eF22C';
 
 /**
  * Fetches total reserves from the WEUSD protocol
@@ -45,6 +62,28 @@ module.exports = {
       const reserves = await getProtocolReserves();
       api.add(CONSTANTS.TOKENS.TOKEN_A, reserves[0]);
       api.add(CONSTANTS.TOKENS.TOKEN_B, reserves[1]);
+    }
+  },
+  base: {
+    tvl: async (api) => {
+      const baseUsdc = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913';
+      return sumTokens2({owner: mintRedeem, tokens: [baseUsdc], api: api})
+    }
+  },
+  arbitrum: {
+    tvl: async (api) => {
+      const arbitrumUsdc = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831';
+      return sumTokens2({owner: mintRedeem, tokens: [arbitrumUsdc], api: api})
+    }
+  },
+  bsc: {
+    tvl: async (api) => {
+      const bscUsdc = '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d';
+      await sumTokens2({owner: mintRedeem, tokens: [bscUsdc], api: api});
+      const tokens = [picwe.rwaToken.ihp, picwe.rwaToken.ucar];
+      const totalSupplies = await api.multiCall({abi: picwe.abi.totalSupply, calls: tokens});
+      const cs = await api.multiCall({abi: picwe.abi.c, calls: [picwe.marketAddress.ihp, picwe.marketAddress.ucar]});
+      api.add(bscUsdc, (totalSupplies[0] * cs[0] + totalSupplies[1] * cs[1]) / 1e18);
     }
   }
 };
