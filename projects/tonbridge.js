a6a1c8ea650359152256ee70ef626d7327bafe57
rohannero <rohanzarathustra@gmail.com>
2026-02-06 16:48:28 -0500

    refactor

--- projects/tonbridge.js
+++ projects/tonbridge.js
@@ -1,73 +1,101 @@
-const sdk = require("@defillama/sdk");
-const axios = require('axios');
-const burl = 'https://token-indexer.broxus.com/v1/root_contract/root_address/0:';
-const tokenMap = {
-    'eb2ccad2020d9af9cec137d3146dde067039965c13a27d97293c931dae22b2b9': 'dai',
-    'a519f99bb5d6d51ef958ed24d337ad75a1c770885dcd42d51d6663f9fcdacfb2': 'tether',
-    'c37b3fafca5bf7d3704b081fde7df54f298736ee059bf6d32fac25f5e6085bf6': 'usd-coin',
-    '2ba32b75870d572e255809b7b423f30f36dd5dea075bd5f026863fceb81f2bcf': 'wrapped-bitcoin',
-    '59b6b64ac6798aacf385ae9910008a525a84fc6dcf9f942ae81f8e8485fe160d': 'weth',
-    //'1e6e1b3674b54753864af7b15072566ce632965bd83bab431a8ff86d68cf1657': 'UNIV2-usdt-wton',
-    'efed9f9a7e6c455ee60829fd003b2f42edda513c6f19a484f916b055e9aa58d2': 'frax',
-    'c14e2f026feaae0f99b92c04ee421051a782fff60156ac8a586a12f63d7facef': 'frax-share',
-    //'f48054939064d686a9ad68d96d9ab79e409b095557c06ab7f073097dade7057f': 'dartflex',
-    //'00ca16398f314a9b3bed582dc69582515d866ededb6c4e18190f63b305cedf91': NA
-    'a49cd4e158a9a15555e624759e2e4e766d22600b7800d891e46f9291f044a93d': 'everscale',
-    // 'b2e341c01da068d43cfa0eae6dae36b12b476e55cf2c3eeb002689f44b9ddef9': 'aave',
-    // 'bc77ba7f3cbbebcca393e85ed479ef44df63cdee4fb572c3e0f904fb9fc63e25': 'compound-governance-token',
-    // '7dd7ae82835848dc6b490515ec4034968a8ceff893a6d5f31ab3cdfcfb79bbb6': 'curve-dao-token',
-    // '6b2baa777b89da66dddaf9f1602142987b13ca565bbb170da929ea945f5ce9fb': 'stasis-eurs',
-    //'387609364f765017fa3fa5815e08d420e054c88a86426cd6d5aaf2a1ee46ff5a': 'torn',
-    // 'e114f1f7d21ac6566d988c983315e0cdd5bee7b43c08918537d1117dea7e4534': 'yearn-finance',
-    // '3c66e3e0ce0a909ce8779b31509db773e544132d8fa6f6641c00bce257c79d9c': '1inch',
-    //'bf1c7c0e8a187d9d5ba6069bf768b69a982df8b22ef8430b31dcc4f97263507e': 'dartflex'
-};
+const { sumTokensExport } = require('./helper/unwrapLPs')
+const ADDRESSES = require('./helper/coreAssets.json')
 
-async function tvl() {
-    const balances = {};
-    const tokenAddresses = Object.keys(tokenMap);
-    for (let i = 0; i < tokenAddresses.length; i++) {
-        const supply = (await axios.get(burl + tokenAddresses[i])).data.totalSupply;
-        balances[tokenMap[tokenAddresses[i]]] = supply
-    }
-    return balances;
-}
-
-function evm(chain, target) {
-    return async (timestamp, block, chainBlocks) => {
-        return { everscale: (await sdk.api.abi.call({
-            target,
-            abi: 'erc20:totalSupply',
-            block: chainBlocks[chain],
-            chain,
-        })).output / 10 ** 9 };
-    };
+const config = {
+    avax: {
+        owners: [
+            "0x54c55369a6900731d22eacb0df7c0253cf19dfff",
+            "0x299956ae1fbd9485689395b6b01d7f7d1ab7f776",
+            "0xbdfd06befeb8cd629b31d89a315092d10435be06",
+            "0x48a02b26f544b00f896e5548004d6d80191b774a",
+            "0x0930179958a0f34ffab00dd8feb16b7b250652e0",
+            "0x4582ae9a5d051a8bb09b416c946cbae9a7817aa6",
+        ],
+        tokens: [
+            ADDRESSES.avax.DAI,
+            ADDRESSES.avax.USDT_e,
+            ADDRESSES.avax.USDC,
+            ADDRESSES.avax.WBTC_e,
+            ADDRESSES.avax.WETH_e,
+        ],
+    },
+    bsc: {
+        owners: [
+            "0x54c55369a6900731d22eacb0df7c0253cf19dfff",
+            "0xad4c25634e3818d674ddc07b98135ed6db7ef307",
+            "0x5d767d4e250b5c8640cb2bf7e7cd3acaeb7768e1",
+            "0x65950dd2a3d8316c197bda1a353aed046035b1c9",
+            "0x0cc7096480e78409aec14795a96efeaf3e0b4b38",
+            "0x5b1e3e9f24455debd6f3a0c4b8bc6b46ca57f68c",
+            "0x295f917cd26a708463b61730226316040418352c",
+        ],
+        tokens: [
+            ADDRESSES.bsc.DAI,
+            ADDRESSES.bsc.USDC,
+            ADDRESSES.bsc.USDT,
+            ADDRESSES.bsc.BTCB,
+            ADDRESSES.bsc.ETH,
+        ],
+    },
+    ethereum: {
+        owners: [
+            "0x54c55369a6900731d22eacb0df7c0253cf19dfff",
+            "0xf8a0d53ddc6c92c3c59824f380c0f3d2a3cf521c",
+            "0xf67d8b970a0a955b5ff2a80b8dfd6aff21567633",
+            "0x55046f53eb9fa069286969d73432b769f068e1fc",
+            "0x8d589f403d5232e37bd30e02260ea6b6ad061f3f",
+            "0xf2403a61c7a97a1a1b94a225173f6dd03614b907",
+            "0x4dee4b939371a9957f5f36a26341f40a88eed0cc",
+            "0x9c217efa90a6a2bb5f0543a0be7d682ce6cf5275",
+        ],
+        tokens: [
+            ADDRESSES.ethereum.DAI,
+            ADDRESSES.ethereum.USDC,
+            ADDRESSES.ethereum.USDT,
+            ADDRESSES.ethereum.WBTC,
+            ADDRESSES.ethereum.WETH,
+            ADDRESSES.ethereum.FRAX,
+            ADDRESSES.ethereum.FXS,
+        ],
+    },
+    fantom: {
+        owners: [
+            "0x54c55369a6900731d22eacb0df7c0253cf19dfff",
+            "0x334d7e33f3b0ac04309b17ca56bcb0f0fa3d0efd",
+            "0xb05a3640132642e6297980376129354ee21a9fc6",
+            "0x8f9d8cfd0b018b1939bb24e2ce48a9e4040fb68a",
+            "0x5115cd7e0dd0886c11e28e54ad2422f61544f314",
+        ],
+        tokens: [
+            ADDRESSES.fantom.DAI,
+            ADDRESSES.fantom.USDC,
+            ADDRESSES.fantom.WBTC,
+        ],
+    },
+    polygon: {
+        owners: [
+            "0x54c55369a6900731d22eacb0df7c0253cf19dfff",
+            "0xced734f47613e2484fd9ee6f76afcb866bc4d6fa",
+            "0xd33492080f2d3a89ae500a3b3bc0e076713a3cbb",
+            "0xf504e9a7511f1af03f8e8c6800a05fb9d43481f2",
+            "0x1fa28c9cb44d2853afd0d932c3805221fab95a8b",
+            "0x356b37e007564fd37b957f946a246871bf827ea2",
+        ],
+        tokens: [
+            ADDRESSES.polygon.DAI,
+            ADDRESSES.polygon.USDC,
+            ADDRESSES.polygon.USDT,
+            ADDRESSES.polygon.WBTC,
+            ADDRESSES.polygon.WETH_1,
+        ],
+    },
 }
 
 module.exports = {
-    timetravel: false,
-    everscale: {
-        tvl
-    },
-    bsc: {
-        tvl: evm('bsc', '0x0A7e7D210C45c4abBA183C1D0551B53AD1756ecA')
-    },
-    ethereum: {
-        tvl: evm('ethereum', '0x29d578CEc46B50Fa5C88a99C6A4B70184C062953')
-    },
-    avax:{
-        tvl: evm('avax', '0x1ffefd8036409cb6d652bd610de465933b226917')
-    },
-    milkomeda: {
-        tvl: evm('milkomeda', '0x1ffEFD8036409Cb6d652bd610DE465933b226917')
-    },
-    polygon: {
-        tvl: evm('polygon', '0x1ffEFD8036409Cb6d652bd610DE465933b226917')
-    },
-    fantom: {
-        tvl: evm('fantom', '0x1ffEFD8036409Cb6d652bd610DE465933b226917')
-    },
-    hallmarks:[
-        [1651881600, "UST depeg"],
-      ],
-};
+    hallmarks: [["2023-05-07", "UST depeg"]],
+    deadFrom: "2024-08-29",
+    ...Object.entries(config).reduce((acc, [chain, { owners, tokens }]) => {
+        acc[chain] = { tvl: sumTokensExport({ owners, tokens }) }
+        return acc
+    }, {}),
+}
