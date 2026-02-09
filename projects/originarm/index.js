20e2ccc02164c05aa8d02bd73e1a0973d76d7c52
Christopher Jacobs <apexearth@gmail.com>
2026-02-09 11:12:26 -0800

    move OETH and OS tokens out of coreAssets.json

--- projects/originarm/index.js
+++ projects/originarm/index.js
@@ -3,6 +3,8 @@ const ADDRESSES = require('../helper/coreAssets.json')
 module.exports = {
   ethereum: {
     tvl: async (api) => {
+      const OETH = "0x856c4Efb76C1D1AE02e20CEB03A2A6a08b0b8dC3"
+
       const lidoArm = "0x85b78aca6deae198fbf201c82daf6ca21942acc6";
       const oethArm = "0x6bac785889A4127dB0e0CeFEE88E0a9F1Aaf3cC7";
       const etherfiArm = "0xfB0A3CF9B019BFd8827443d131b235B3E0FC58d2";
@@ -12,7 +14,7 @@ module.exports = {
       api.add(ADDRESSES.ethereum.STETH, outstanding_STETH);
 
       const outstanding_OETH = await api.call({ abi: 'uint256:vaultWithdrawalAmount', target: oethArm });
-      api.add(ADDRESSES.ethereum.OETH, outstanding_OETH);
+      api.add(OETH, outstanding_OETH);
 
       const outstanding_EETH = await api.call({ abi: 'uint256:etherfiWithdrawalQueueAmount', target: etherfiArm });
       api.add(ADDRESSES.ethereum.EETH, outstanding_EETH);
@@ -26,7 +28,7 @@ module.exports = {
         [ADDRESSES.ethereum.STETH, lidoArm],
         // OETH ARM
         [ADDRESSES.ethereum.WETH, oethArm],
-        [ADDRESSES.ethereum.OETH, oethArm],     
+        [OETH, oethArm],     
         // Etherfi ARM
         [ADDRESSES.ethereum.WETH, etherfiArm],
         [ADDRESSES.ethereum.EETH, etherfiArm],
@@ -38,14 +40,15 @@ module.exports = {
   },
   sonic: {
     tvl: async (api) => {
+      const OS = "0xb1e25689D55734FD3ffFc939c4C3Eb52DFf8A794"
       // OS ARM
       const osArm = "0x2F872623d1E1Af5835b08b0E49aAd2d81d649D30";
       const outstandingOs = await api.call({ abi: 'uint256:vaultWithdrawalAmount', target: osArm });
-      api.add(ADDRESSES.sonic.OS, outstandingOs);
+      api.add(OS, outstandingOs);
       return api.sumTokens({ tokensAndOwners: [
         // OS ARM
         [ADDRESSES.sonic.wS, osArm],
-        [ADDRESSES.sonic.OS, osArm],
+        [OS, osArm],
       ]});
     },
   },
