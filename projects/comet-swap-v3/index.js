5488dc75eb87cfa61b63a2585756e73baf73e1c9
d3tAdam <141658334+d3t-adam8023@users.noreply.github.com>
2026-02-09 23:57:59 +0800

    Add Comet Swap TVL adapter (#17965)
    Co-authored-by: adam <liyi8023@icloud.com>
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

---
+++ projects/comet-swap-v3/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  astar: { factory: '0x2C1EEf5f87F4F3194FdAAfa20aE536b1bA49863b', fromBlock: 12168518, },
+})
