3c00d78486d0f36047fd2a1e6686534ff98dc43b
DocPis <docpis@tutamail.com>
2026-02-10 03:04:46 +0100

    Add fromBlock parameter to uniTvlExport call

--- projects/currentx/index.ts
+++ projects/currentx/index.ts
@@ -1,3 +1,4 @@
 const { uniTvlExport } = require("../helper/unknownTokens");
 
-module.exports = uniTvlExport('megaeth', '0xc60940f182f7699522970517f6d753a560546937')
+
+module.exports = uniTvlExport('megaeth', '0xc60940f182f7699522970517f6d753a560546937', fromBlock: 7349750 },
