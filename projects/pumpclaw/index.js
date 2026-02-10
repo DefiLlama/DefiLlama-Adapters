5eee7e5fc77eb531dd5d43c9100145cefb121eb6
Clawd <t.800.y.1984@gmail.com>
2026-02-10 20:08:51 +0900

    Add PumpClaw adapter (Base token launcher with Uniswap V4)

---
+++ projects/pumpclaw/index.js
@@ -0,0 +1,29 @@
+const { getLogs2 } = require('../helper/cache/getLogs')
+
+const FACTORY = '0xe5bCa0eDe9208f7Ee7FCAFa0415Ca3DC03e16a90'
+const POOL_MANAGER = '0x498581fF718922c3f8e6A244956aF099B2652b2b'
+
+const eventAbi = 'event TokenCreated(address indexed token, address indexed creator, string name, string symbol, uint256 positionId, uint256 totalSupply, uint256 initialFdv, int24 tickLower, int24 tickUpper)'
+
+async function tvl(api) {
+  // Get all tokens created by PumpClaw factory
+  const logs = await getLogs2({
+    api,
+    target: FACTORY,
+    fromBlock: 25973600,
+    eventAbi,
+  })
+
+  const tokens = logs.map(log => log.token)
+
+  // Sum token balances held in Uniswap V4 PoolManager
+  // Each PumpClaw token is exclusively paired with ETH in a V4 pool
+  // LP positions are locked forever in PumpClaw's LPLocker contract
+  return api.sumTokens({ owner: POOL_MANAGER, tokens })
+}
+
+module.exports = {
+  methodology: 'Counts the value of tokens locked in Uniswap V4 pools created by the PumpClaw factory. All LP positions are permanently locked.',
+  start: 1769932905,
+  base: { tvl },
+}
