8c38237529f3e2b1657b8371230473c16cff9c1f
DocPis <docpis@tutamail.com>
2026-02-10 03:10:07 +0100

    Refactor TVL export to use getUniTVL function

--- projects/currentx/index.ts
+++ projects/currentx/index.ts
@@ -1,4 +1,17 @@
-const { uniTvlExport } = require("../helper/unknownTokens");
+const { getUniTVL } = require('../helper/unknownTokens')
 
+const FACTORY = "0xC60940F182F7699522970517f6d753A560546937"
+const WETH = "0x4200000000000000000000000000000000000006"
 
-module.exports = uniTvlExport('megaeth', '0xc60940f182f7699522970517f6d753a560546937', fromBlock: 7349750 },
+module.exports = {
+  misrepresentedTokens: true,
+  megaeth: {
+    tvl: getUniTVL({
+      factory: FACTORY, 
+      useDefaultCoreAssets: true,
+      fetchBalances: true,
+      coreAssets: [WETH],
+    })
+  }
+}
+
