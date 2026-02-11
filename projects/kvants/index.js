5d5c2ca6e1a2b93b568bed4a5720117204a58f74
robby3082 <lazar.roby30@gmail.com>
2026-02-11 22:57:05 +0200

    update kvants with enzyme vault

--- projects/kvants/index.js
+++ projects/kvants/index.js
@@ -17,6 +17,14 @@ const DRIFT_VAULTS = [
   },
 ];
 
+const ENZYME_VAULTS = [
+  {
+    name: "BTC Quant Trend Following",
+    address: "0xfb48ae6a8e7bd05f9ef542708dd632bc8517539e",
+    valueAsset: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf", // cbBTC
+  },
+];
+
 
 async function drift_vaults_tvl(api) {
   await getDriftVaultTvl(
@@ -25,13 +33,26 @@ async function drift_vaults_tvl(api) {
   );
 }
 
-async function tvl(api) {
-  await drift_vaults_tvl(api);
+async function enzymeTvl(api) {
+  const addresses = ENZYME_VAULTS.map((v) => v.address);
+  const valueAssets = ENZYME_VAULTS.map((v) => v.valueAsset);
+  const [shareValues, totalSupplies, decimals] = await Promise.all([
+    api.multiCall({ calls: addresses, abi: 'uint256:shareValue' }),
+    api.multiCall({ calls: addresses, abi: 'uint256:totalSupply' }),
+    api.multiCall({ calls: valueAssets, abi: 'erc20:decimals' }),
+  ]);
+  for (let i = 0; i < ENZYME_VAULTS.length; i++) {
+    // shareValue is in 18-decimal precision, convert to value asset's decimals
+    const nav = BigInt(shareValues[i]) * BigInt(totalSupplies[i]) / (10n ** BigInt(36 - decimals[i]));
+    api.add(ENZYME_VAULTS[i].valueAsset, nav.toString());
+  }
 }
+
 
 module.exports = {
   timetravel: false,
   doublecounted: true,
   methodology: "The combined TVL of all vaults.",
-  solana: { tvl },
+  solana: { tvl: drift_vaults_tvl },
+  base: { tvl: enzymeTvl },
 };
