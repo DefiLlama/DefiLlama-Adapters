29adbdc7840da3c358bfca9aa1f28b787a966bf2
jasmin-avalon <122794657+jasmin-avalon@users.noreply.github.com>
2026-02-12 12:51:33 -0700

    Fix: movement tvl code logic

--- projects/avalon-finance-superearn/index.js
+++ projects/avalon-finance-superearn/index.js
@@ -11,19 +11,19 @@ const evmConfig = {
 }
 
 const moveConfig = {
-    move: {
-        vaultAddress: '0x38a958445901773dab8977471366701b4f53ae5f72967d47da3a98a3e09e5ab8',
-        // USDC
-        vaultStableTokenAddress: '0x83121c9f9b0527d1f056e21a950d6bf3b9e9e2e8353d0e95ccea726713cbea39',
-    },
+  move: {
+    vaultAddress: '0x38a958445901773dab8977471366701b4f53ae5f72967d47da3a98a3e09e5ab8',
+    // USDC
+    vaultStableTokenAddress: '0x83121c9f9b0527d1f056e21a950d6bf3b9e9e2e8353d0e95ccea726713cbea39',
+  },
 }
 
-const getMovementTvl = async (api) => {
+const getMovementTvl = async (api, { vaultAddress, vaultStableTokenAddress }) => {
   const primary_fungible_asset_balance = "0x1::primary_fungible_store::balance"
   const tvl = await function_view({
     functionStr: primary_fungible_asset_balance,
     type_arguments: ["0x1::fungible_asset::Metadata"],
-    args: [moveConfig.vaultAddress, moveConfig.vaultStableTokenAddress],
+    args: [vaultAddress, vaultStableTokenAddress],
     chain: "move"
   })
   return tvl
@@ -35,15 +35,17 @@ module.exports = {
 }
 
 
-// Movement
-Object.keys(moveConfig).forEach(chain => {
-  module.exports[chain] = {
-    tvl: async (api) => {
-      const tvl = await getMovementTvl(api)
-      api.add(moveConfig.vaultStableTokenAddress, tvl)
-    },
-  }
-})
+ // Movement
+ Object.keys(moveConfig).forEach(chain => {
+  const { vaultAddress, vaultStableTokenAddress } = moveConfig[chain]
+   module.exports[chain] = {
+     tvl: async (api) => {
+
+      const tvl = await getMovementTvl(api, { vaultAddress, vaultStableTokenAddress })
+      api.add(vaultStableTokenAddress, tvl)
+     },
+   }
+ })
 
 // EVM
 Object.keys(evmConfig).forEach(chain => {
