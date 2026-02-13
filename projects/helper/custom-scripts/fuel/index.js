7eb16aa37e1110c18d527a24f66c5af99a26ba1d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-02-13 21:36:02 +0100

    update fuel

---
+++ projects/helper/custom-scripts/fuel/index.js
@@ -0,0 +1,39 @@
+const sdk = require('@defillama/sdk')
+const { Provider, Contract } = require('fuels')
+
+
+let provider
+async function getProvider() {
+  if (!provider) provider = new Provider(process.env.FUEL_CUSTOM_RPC ?? 'https://mainnet.fuel.network/v1/graphql')
+  return provider
+}
+
+async function query1({ contractId, abi, method, params = [] }) {
+
+  const contract = new Contract(contractId, abi, await getProvider())
+  const { value } = await contract.functions[method](...params).get()
+  return value
+}
+
+async function main() {
+
+  const api = new sdk.ChainApi({ chain: 'fuel' })
+  const balances = await require('../../../o2/api').fuel.tvl(api, query1)
+
+  console.log("o2 TVL:", balances);
+
+  await sdk.elastic.writeLog('custom-scripts', {
+    metadata: {
+      type: 'tvl',
+    },
+    chain: 'fuel',
+    project: 'o2',
+    balances,
+    timestamp: Math.floor(Date.now() / 1000),
+  });
+}
+
+main().catch((err) => {
+  console.error("Error computing o2 TVL:", err);
+  process.exit(1);
+}).then(() => process.exit(0));
