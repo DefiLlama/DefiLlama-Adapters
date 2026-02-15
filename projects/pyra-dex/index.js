ed628a98d2fe6cc458aa06e60480d7798784a5c4
Nancock <nancock@mail.ru>
2026-02-15 13:58:49 +0200

    usd-pyra

--- projects/pyra-dex/index.js
+++ projects/pyra-dex/index.js
@@ -1,18 +1,36 @@
-const { getProvider, sumTokens2 } = require("../helper/solana");
+const { getProvider, getTokenAccountBalances } = require("../helper/solana");
 const { Program } = require("@project-serum/anchor");
+const { get } = require("../helper/http");
 const idl = require("./idl.json");
 
 const PROGRAM_ID = "94jkbjHAz6oVCsbsDKpeBRZZYvhm2Hg2epNDihLmb4nN";
+const PYRA_MINT = "9pan9bMn5HatX4EJdBwg9VgCa7Uz5HL8N1m5D3NdXejP";
+const PYRA_SOLANA_MINT = "8iHqAtn2z4yi9qrEpm25Qd6NvhoqDmfx19kyz9jCpump";
+const PYRA_DECIMALS = 9;
 
 async function tvl(api) {
   const provider = getProvider(api.chain);
   const program = new Program(idl, PROGRAM_ID, provider);
   const pools = await program.account.pool.all();
-  const tokenAccounts = pools.flatMap(({ account }) => [
-    account.tokenAVault,
-    account.tokenBVault,
-  ]);
-  return sumTokens2({ tokenAccounts, api });
+
+  const pyraVaults = [];
+  for (const { account } of pools) {
+    const isPyraA = account.tokenAMint.toString() === PYRA_MINT;
+    pyraVaults.push(isPyraA ? account.tokenAVault : account.tokenBVault);
+  }
+
+  const balances = await getTokenAccountBalances(pyraVaults, { individual: true, chain: api.chain });
+  let totalPyra = 0;
+  for (const { amount } of balances) {
+    totalPyra += Number(amount);
+  }
+  totalPyra /= 10 ** PYRA_DECIMALS;
+
+  const priceData = await get(`https://lite-api.jup.ag/price/v3?ids=${PYRA_SOLANA_MINT}`);
+  const pyraPrice = priceData[PYRA_SOLANA_MINT].usdPrice;
+
+  // TVL ≈ 2 * PYRA_balance * price (both sides of AMM pool are roughly equal in value)
+  api.addUSDValue(totalPyra * pyraPrice * 2);
 }
 
 module.exports = {
@@ -20,5 +38,5 @@ module.exports = {
   isHeavyProtocol: true,
   pyra: { tvl },
   methodology:
-    "TVL is calculated by summing token balances in all Pyra DEX (DAMM v2) liquidity pool vaults.",
+    "TVL is calculated by summing PYRA token balances in all DEX pool vaults and deriving USD value using Jupiter price feed.",
 };
--- projects/pyra-launchpad/index.js
+++ projects/pyra-launchpad/index.js
@@ -1,19 +1,36 @@
-const { getProvider, sumTokens2 } = require("../helper/solana");
+const { getProvider, getTokenAccountBalances } = require("../helper/solana");
 const { Program } = require("@project-serum/anchor");
+const { get } = require("../helper/http");
 const idl = require("./idl.json");
 
 const PROGRAM_ID = "4nvcyXwTMAqM1ZoZbJWvcPXtg8dNXVbt2CFaXVwaPbT6";
+const PYRA_MINT = "9pan9bMn5HatX4EJdBwg9VgCa7Uz5HL8N1m5D3NdXejP";
+const PYRA_SOLANA_MINT = "8iHqAtn2z4yi9qrEpm25Qd6NvhoqDmfx19kyz9jCpump";
+const PYRA_DECIMALS = 9;
 
 async function tvl(api) {
   const provider = getProvider(api.chain);
   const program = new Program(idl, PROGRAM_ID, provider);
   const pools = await program.account.virtualPool.all();
-  const tokenAccounts = [];
+
+  const quoteVaults = [];
   for (const { account } of pools) {
     if (account.isMigrated) continue;
-    tokenAccounts.push(account.baseVault, account.quoteVault);
+    quoteVaults.push(account.quoteVault);
   }
-  return sumTokens2({ tokenAccounts, api });
+
+  const balances = await getTokenAccountBalances(quoteVaults, { individual: true, chain: api.chain });
+  let totalPyra = 0;
+  for (const { amount } of balances) {
+    totalPyra += Number(amount);
+  }
+  totalPyra /= 10 ** PYRA_DECIMALS;
+
+  const priceData = await get(`https://lite-api.jup.ag/price/v3?ids=${PYRA_SOLANA_MINT}`);
+  const pyraPrice = priceData[PYRA_SOLANA_MINT].usdPrice;
+
+  // For bonding curves: count PYRA in quote vaults * 2 (base side has roughly equal value)
+  api.addUSDValue(totalPyra * pyraPrice * 2);
 }
 
 module.exports = {
@@ -21,5 +38,5 @@ module.exports = {
   isHeavyProtocol: true,
   pyra: { tvl },
   methodology:
-    "TVL is calculated by summing token balances in active (non-migrated) Pyra Launchpad bonding curve pools.",
+    "TVL is calculated by summing PYRA token balances in active bonding curve pools and deriving USD value using Jupiter price feed.",
 };
