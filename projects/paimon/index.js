9837492d8bc248c9586c05fefed2aa28bc0b0ca5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-02-17 11:32:11 +0100

    refactor

--- projects/paimon/index.js
+++ projects/paimon/index.js
@@ -1,25 +1,7 @@
-const ADDRESSES = require("../helper/coreAssets.json");
-
-const PP = "0x8505c32631034A7cE8800239c08547e0434EdaD9";
-const USDT = ADDRESSES.bsc.USDT;
-
-const abi = {
-  totalSupply: "uint256:totalSupply",
-  sharePrice: "uint256:sharePrice",
-};
-
-async function tvl(api) {
-  const totalSupply = await api.call({ target: PP, abi: abi.totalSupply });
-
-  const sharePrice = await api.call({ target: PP, abi: abi.sharePrice });
-
-  const tvl = (totalSupply * sharePrice) / 1e18;
-  api.add(USDT, tvl);
-}
+const { sumERC4626VaultsExport2 } = require('../helper/erc4626')
 
 module.exports = {
   bsc: {
-    tvl,
+    tvl: sumERC4626VaultsExport2({ vaults: ['0x8505c32631034A7cE8800239c08547e0434EdaD9']}),
   },
-  methodology: "TVL is calculated as the total assets held in the Paimon (PP) ERC-4626 vault contract",
 };
