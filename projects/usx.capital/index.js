3c77aef91111f7157bc93ef11a8414180602c257
rohannero <rohanzarathustra@gmail.com>
2026-02-18 19:14:50 -0500

    fix

--- projects/usx.capital/index.js
+++ projects/usx.capital/index.js
@@ -1,8 +1,9 @@
-const USDC = '0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4'
+const ADDRESSES = require('../helper/coreAssets.json')
+const treasury = '0x9F3d4b0C9E930Ca3957eCD3DEdb7417f8e0e4c35'
 
 async function tvl(api) {
   const assetManagerUSDC = await api.call({ abi: 'uint256:assetManagerUSDC', target: treasury })
-  api.add(USDC, assetManagerUSDC)
+  api.add(ADDRESSES.scroll.USDC, assetManagerUSDC)
 }
 
 module.exports = {
