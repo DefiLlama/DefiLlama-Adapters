1d6b5196599d38d7b35b605fdb009c3e447867f8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-02-19 08:17:24 +0100

    consolidate erc4626 adapters

--- projects/paimon/index.js
+++
@@ -1,7 +0,0 @@
-const { sumERC4626VaultsExport2 } = require('../helper/erc4626')
-
-module.exports = {
-  bsc: {
-    tvl: sumERC4626VaultsExport2({ vaults: ['0x8505c32631034A7cE8800239c08547e0434EdaD9']}),
-  },
-};
