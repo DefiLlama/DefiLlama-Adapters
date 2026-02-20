53f3bb937874dd7c35000da41dbe8ce4d7ffd30d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-02-20 22:46:20 +0100

    consolidate aave forks

--- projects/zentra/index.js
+++
@@ -1,8 +0,0 @@
-const { aaveV3Export } = require("../helper/aave");
-
-// Zentra Protocol - AAVE V3 lending markets on Citrea
-// Markets: USDC, WcBTC, ctUSD
-// https://zentra.fi
-module.exports = aaveV3Export({
-  citrea: '0x0FC811fE6bD0Be53717f9ca722E30a7bc4B90C31',
-})
