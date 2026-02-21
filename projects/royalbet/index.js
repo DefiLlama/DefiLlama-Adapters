89fbf2a0b4c60431a8a2eeb047d2714dc5c325e9
SuperBet Finance <262697902+SUP3RB3T-Project@users.noreply.github.com>
2026-02-21 14:41:20 +0100

    Update index.js

--- projects/royalbet/index.js
+++ projects/royalbet/index.js
@@ -1,7 +1,7 @@
 const { sumTokens2 } = require("../helper/solana");
 
 module.exports = {
-  methodology: "TVL is the SOL held in the manager (escrow) wallet for active bets.",
+  methodology: "TVL is the SOL held in the TREASURY.",
   solana: {
     tvl: async () => {
       return sumTokens2({
