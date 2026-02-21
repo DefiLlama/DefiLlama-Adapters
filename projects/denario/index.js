d1fcddd10a109b54616d98ff675869e6a193b0a5
Almost21 <185598204+Almost21@users.noreply.github.com>
2026-02-21 17:23:14 +0530

    refactor(denario): simplify TVL logic and rely on server pricing

--- projects/denario/index.js
+++ projects/denario/index.js
@@ -1,27 +1,13 @@
-// Denario Silver Coin is 100% backed by physical silver.
-// Price is determined by the price of silver on the spot market plus premium.
-// The price is stored in the price oracle and updated every minute.
+// Denario Silver Coin and GOld Coin is 100% backed by physical silver and Gold.
+// Denario tokens are priced by DefiLlama's server-side RWA adapter.
+// This adapter only returns raw token supplies.
 
 const sdk = require('@defillama/sdk');
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const silverAddress = ADDRESSES.polygon.DSC
 const goldAddress = ADDRESSES.polygon.DGC
-const priceOracle = '0x9be09fa9205e8f6b200d3c71a958ac146913662e'
 
-
-const priceOracleABI = [
-  {
-    "inputs": [{ "name": "key", "type": "string" }],
-    "name": "getValue",
-    "outputs": [
-      { "name": "", "type": "uint128" },
-      { "name": "", "type": "uint128" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  }
-]
 
 const firstBlock = 62270459
 
@@ -38,28 +24,7 @@ async function tvl(api) {
 	})
   ])
 
-	const [silverPriceRaw, goldPriceRaw] = await Promise.all([
-    api.call({
-		target: priceOracle,
-		params: 'silvercoin/latest/USD',
-		abi: priceOracleABI[0]
-	}).then(res => res[0]),
-
-	 api.call({
-      target: priceOracle,
-      params: 'goldcoin/latest/USD',
-      abi: priceOracleABI[0]
-    }).then(res => res[0])
-  ])
-  
-// Oracle price has 8 decimals; tokens have 18 decimals; USDC has 6 decimals.
-// Scale factor: price(8d) * supply(18d) / 1e18 * 1e6 / 1e8 = supply * price / 1e20
-const SCALE = BigInt('100000000000000000000') // 1e20
-const silverValueUSDC = BigInt(totalSilverSupply) * BigInt(silverPriceRaw) / SCALE
-const goldValueUSDC = BigInt(totalGoldSupply) * BigInt(goldPriceRaw) / SCALE
-const totalValueUSDC = silverValueUSDC + goldValueUSDC
-
-api.add(ADDRESSES.polygon.USDC, totalValueUSDC.toString()) // USDC has 6 decimals
+	
 	return {
 		[silverAddress]: totalSilverSupply,
 		[goldAddress]: totalGoldSupply,
@@ -67,7 +32,6 @@ api.add(ADDRESSES.polygon.USDC, totalValueUSDC.toString()) // USDC has 6 decimal
 }
 
 module.exports = {
-	misrepresentedTokens: true,
 	methodology: 'TVL corresponds to the total amount of token minted, which is 100% backed by physical metal.',
 	start: firstBlock,
 	polygon: {
