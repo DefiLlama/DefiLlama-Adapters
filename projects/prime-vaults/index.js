b583c9912bfa884ccd45d9b58ca6b770ce5e8fe0
ducmanh1101 <jake@beraji.io>
2026-02-24 10:49:50 +0700

    Remove cross-chain logs and export berachain TVL
    Drop cross-chain bridging logic and event log handling: remove getLogs import, eventBridgeExecuted ABI, crossChain config and crossTvl. Rename coreTvl to tvl and simplify module exports to only provide berachain: { tvl }. Core multicall supply logic remains unchanged.

--- projects/prime-vaults/index.js
+++ projects/prime-vaults/index.js
@@ -1,19 +1,10 @@
 const config = require("./config");
-const { getLogs } = require('../helper/cache/getLogs');
 
 const abi = {
     getAccountantState: 'function getAccountantState() view returns (tuple(address payoutAddress, uint96 exchangeRate, uint128 feesOwedInBase, uint128 totalSharesLastUpdate, uint64 lastUpdateTimestamp, uint16 platformFee))',
-    eventBridgeExecuted: 'event BridgeExecuted(address indexed sender, address indexed receiver, address token, uint256 amount, uint256 timestamp)'
 }
 
-const crossChain = {
-    ethereum: { executor: '0xb2f865041e3F7De4576FB5B30ac8e9fbDA82e29d', fromBlock: 24090329 },
-    arbitrum: { executor: '0xb2f865041e3F7De4576FB5B30ac8e9fbDA82e29d', fromBlock: 428462141 },
-    bsc: { executor: '0x6b5a6B402F984FCd4175C43b642800920873cbC5', fromBlock: 74944045 },
-    core: { executor: '0xb2f865041e3F7De4576FB5B30ac8e9fbDA82e29d', fromBlock: 31251416 }
-}
-
-async function coreTvl(api) {
+async function tvl(api) {
     const contracts =Object.values(config.vaults)
 
     const supplies = await api.multiCall({
@@ -40,27 +31,7 @@ async function coreTvl(api) {
     })
 }
 
-async function crossTvl(api, executor, fromBlock) {
-    const logs = await getLogs({
-        api,
-        target: executor,
-        eventAbi: abi.eventBridgeExecuted,
-        onlyArgs: true,
-        fromBlock,
-    })
-    logs.forEach((log) => {
-        api.add(log.token, log.amount)
-    })
-}
-
 module.exports = {
-    berachain: {
-        tvl: coreTvl
-    },
+    doublecounted: true,
+    berachain: { tvl }
 }
-
-Object.keys(crossChain).forEach(chain => {
-    module.exports[chain] = {
-        tvl: (api) => crossTvl(api, crossChain[chain].executor, crossChain[chain].fromBlock)
-    }
-})
