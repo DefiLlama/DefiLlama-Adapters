6a7935b63ee1c61a0b5d98e3e32b87da7c14c3c5
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-02-28 09:11:27 +0100

    blackhold: exclude GSCORE from the tvl

--- projects/supernova-CL/index.js
+++ projects/supernova-CL/index.js
@@ -11,6 +11,8 @@ Object.keys(config).forEach(chain => {
     const token0s = await api.multiCall({ abi: 'address:token0', calls: pools })
     const token1s = await api.multiCall({ abi: 'address:token1', calls: pools })
     const tokensAndOwners2 = [token0s.concat(token1s), pools.concat(pools)]
-    return api.sumTokens({ tokensAndOwners2, })
+    return api.sumTokens({ tokensAndOwners2, blacklistedTokens: [
+      '0x0fd4a527a4422aca27f48cd79e4093867544a616', // G-SCORE -  - token minter holder all of the supply?
+    ] })
   }
 })
