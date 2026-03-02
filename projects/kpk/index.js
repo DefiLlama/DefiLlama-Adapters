fe8d6ae3d776fe293ecc6a23f2d2042f96b5291b
kpk-giel <giel@karpatkey.com>
2026-03-02 17:17:22 +0000

    Add kpk eulerVaultOwners to index.js
    Add eulerVaultOwners to index.js

--- projects/kpk/index.js
+++ projects/kpk/index.js
@@ -42,11 +42,17 @@ const configs = {
         "0x9178eBE0691593184c1D785a864B62a326cc3509", //Morpho v1 USDC Yield
       ],
 
+      eulerVaultOwners: [
+        "0x1572063377a9a4f8065BD7bA0D7fa135cd13051F", //Euler kpk Curation Safe
+        "0x354C92aF243d53A24feb3dFF20372Af7b7c47478" // kpk Security Council Safe
+      ],
+      
       // Other ERC-4626 vaults (non-Morpho)
       erc4626: [
         "0x9396dcbf78fc526bb003665337c5e73b699571ef", //Gearbox ETH
         "0xA9d17f6D3285208280a1Fd9B94479c62e0AABa64", //Gearbox wstETH
       ],
+      
 
       // NEW: Gearbox v3.1 Market Configurator (legacy configurator) to crawl
       gearboxMarketConfigurator: "0x1b265b97eb169fb6668e3258007c3b0242c7bdbe",
