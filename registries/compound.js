9a964569e942784e6d572aa3a8673afad1a2ce0f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-03-02 11:36:10 +0100

    fix quantus

--- registries/compound.js
+++ registries/compound.js
@@ -38,8 +38,8 @@ const configs = {
   },
   'quantus': {
     methodology: "Counts the tokens locked in the contracts to be used as collateral to borrow or to earn yield. Borrowed coins are not counted towards the TVL, so only the coins actually locked in the contracts are counted. There's multiple reasons behind this but one of the main ones is to avoid inflating the TVL through cycled lending.",
-    monad: { comptroller: '0xFc57bF0733e5e65d8549fc2922919Cfb97e62D5f' },
-    megaeth: { comptroller: '0x1F1416EbbeAb7a13fC5B6111A1E77696Be600413' },
+    monad: { comptroller: '0xFc57bF0733e5e65d8549fc2922919Cfb97e62D5f', isInsolvent: true },
+    megaeth: { comptroller: '0x1F1416EbbeAb7a13fC5B6111A1E77696Be600413', isInsolvent: true },
   },
   'takara': {
     methodology,
