bb38033c7ddd0df7aec1bd590b5ab781671d62d1
Define101 <93603962+Define101@users.noreply.github.com>
2026-03-03 10:00:16 +0000

    Update index.js

--- projects/grovex/index.js
+++ projects/grovex/index.js
@@ -8,6 +8,31 @@ const config = {
       "0x7ee04cd7187D9EDb18646e58168bAbB9CEF75923"
     ],
   },
+  ethereum: {
+    owners: [
+      "0x79b5a2d395db1711a6e6c42a95eb48710276f666"
+    ],
+  },
+  avax: {
+    owners: [
+      "0x79b5a2d395db1711a6e6c42a95eb48710276f666"
+    ],
+  },
+  bsc: {
+    owners: [
+      "0x79b5a2d395db1711a6e6c42a95eb48710276f666"
+    ],
+  },
+  polygon: {
+    owners: [
+      "0x79b5a2d395db1711a6e6c42a95eb48710276f666"
+    ],
+  },
+  base: {
+    owners: [
+      "0x79b5a2d395db1711a6e6c42a95eb48710276f666"
+    ],
+  },
 };
 
 module.exports = cexExports(config);
