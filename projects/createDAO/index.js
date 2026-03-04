56a971b4ab6d5244d6af6f208ad95e3946ae0fcf
dikobay <dikobay@createdao.org>
2026-03-04 13:22:36 +0500

    fix: changes according to coderabbitai

--- projects/createDAO/index.js
+++ projects/createDAO/index.js
@@ -1,20 +1,8 @@
 const { sumTokens2 } = require('../helper/unwrapLPs');
 const { getLogs } = require('../helper/cache/getLogs');
-const ADDRESSES = require('../helper/coreAssets.json');
 const config = require('./config');
 
 const DAO_CREATED_EVENT = 'event DAOCreated(address indexed creator, address indexed token, address indexed governor, address timelock, string daoName, string tokenName, string tokenSymbol, uint256 totalSupply)';
-
-// Common tokens that DAO treasuries may hold
-const COMMON_TOKENS = [
-  ADDRESSES.null,               // Native ETH
-  ADDRESSES.ethereum.USDC,      // USDC
-  ADDRESSES.ethereum.USDT,      // USDT
-  ADDRESSES.ethereum.DAI,       // DAI
-  ADDRESSES.ethereum.WETH,      // WETH
-  ADDRESSES.ethereum.WBTC,      // WBTC
-  ADDRESSES.ethereum.WSTETH,    // wstETH
-];
 
 async function tvl(api) {
   const { factoryAddress, fromBlock } = config[api.chain];
@@ -34,7 +22,7 @@ async function tvl(api) {
   return sumTokens2({
     api,
     owners: timelockAddresses,
-    tokens: COMMON_TOKENS,
+    fetchCoValentTokens: true,
   });
 }
 
