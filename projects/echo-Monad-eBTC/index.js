79c928e2cec8423f0e83e157a76e129da324f0fb
Yaron1902 <Yaron1902@proton.me>
2026-03-06 10:50:08 +0800

    Add Monad TVL to echo bridge

--- projects/echo-Monad-eBTC/index.js
+++
@@ -1,7 +0,0 @@
-const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
-const { sumTokensExport } = require("../helper/sumTokens.js");
-
-module.exports = {
-    methodology: "Echo is a Bitcoin liquidity aggregation and yield infrastructure layer",
-    bitcoin: { tvl: sumTokensExport({ owners: bitcoinAddressBook.echoMonadEBTC }) }
-};
--- projects/echo-bridge/index.js
+++ projects/echo-bridge/index.js
@@ -1,8 +1,10 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const { sumTokensExport } = require('../helper/sumTokens')
+const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
 
 module.exports = {
   bsquared: {
     tvl: sumTokensExport({ owner: '0x5b1399B8b97fBC3601D8B60Cc0F535844C411Bd5', tokens: [ADDRESSES.bsquared.UBTC]})
-  }
+  },
+  bitcoin: { tvl: sumTokensExport({ owners: bitcoinAddressBook.echoMonadEBTC }) }
 }
