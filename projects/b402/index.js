5ee050fa3fcfc86a2b19534eeab3085f56dbea55
Mayur Chougule <mayurchougule1@gmail.com>
2026-03-06 03:26:34 -0500

    Add B402 protocol listing (Base)

---
+++ projects/b402/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  methodology: "B402 does not have locked TVL. It is a privacy transaction protocol where funds flow through shielded USDC transfers on Base.",
+  base: {
+    tvl: async () => ({}),
+  },
+};
