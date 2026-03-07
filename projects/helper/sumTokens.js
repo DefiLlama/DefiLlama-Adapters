836b4f462f48f998b48db79275adfa98e5a02c66
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-03-07 18:51:32 +0100

    fix backpack

--- projects/helper/sumTokens.js
+++ projects/helper/sumTokens.js
@@ -1,6 +1,7 @@
 const { ibcChains, getUniqueAddresses } = require('./tokenMapping')
 const { get, post, } = require('./http')
 const { sumTokens2: sumTokensEVM, nullAddress, } = require('./unwrapLPs')
+const { svmChains, svmChainsSet, } = require('./svmChainConfig')
 const sdk = require('@defillama/sdk')
 const { RateLimiter } = require("limiter");
 
@@ -41,6 +42,10 @@ const helpers = {
   "radixdlt": require("./chain/radixdlt"),
   "stellar": require("./chain/stellar"),
 }
+
+svmChains.forEach(chain => {
+  helpers[chain] = helpers.solana
+})
 
 
 // some chains support both evm & non-evm, this is to handle if address provided is not evm
@@ -117,9 +122,13 @@ async function sumTokens(options) {
   if (ibcChains.includes(chain) && nonEvmOwnerFound) helper = helpers.cosmos
 
   if (helper) {
+
+    if (svmChainsSet.has(chain)) {
+      return helper.sumTokens2(options)
+    }
+
     switch (chain) {
       case 'cardano':
-      case 'solana': return helper.sumTokens2(options)
       case 'eos': return helper.get_account_tvl(owners, tokens, 'eos')
       case 'tezos': options.includeTezos = true; break;
     }
