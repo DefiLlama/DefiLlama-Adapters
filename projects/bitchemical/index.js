894b41b9196fea5a01265c232b38a1fa5cc74d56
blackrock81 <35052576+blackrock81@users.noreply.github.com>
2026-03-09 02:43:57 +0300

    Update index.js

--- projects/bitchemical/index.js
+++ projects/bitchemical/index.js
@@ -1,62 +1,13 @@
-const BCHEM = '0x9102E0A76a5e2823073Ed763a32Ba8ca8521b1F3'
-const USDT = '0x55d398326f99059fF775485246999027B3197955'
+const { staking } = require("../helper/staking");
 
-
-const STAKING = '0x01F82039810f18F703F4c8b943940ce04Fa00C78'
-
-const UNISWAP_V4_POOL_KEYS = {
-  USDT: '0xa41edf5a64964f2dea4b59b3d5a19cbf2198ef0e20b5309fdbb265d3a234c390',
-
-}
-
-
-const TVL_HOLDER_ADDRESSES = [
-  '0x28e2Ea090877bF75740558f6BFB36A5ffeE9e9dF',
-
-]
-
-const erc20BalanceOfAbi = 'function balanceOf(address) view returns (uint256)'
-const totalStakedAbi = 'function totalStaked() view returns (uint256)'
-const rewardVaultAbi = 'function rewardVault() view returns (uint256)'
-
-
-async function liquidityPoolTvl(api) {
-  const calls = []
-  for (const holder of TVL_HOLDER_ADDRESSES) {
-    calls.push({ target: BCHEM, params: [holder] })
-    calls.push({ target: USDT, params: [holder] })
-  }
-
-  const balances = await api.multiCall({
-    abi: erc20BalanceOfAbi,
-    calls,
-  })
-
-  for (let i = 0; i < balances.length; i += 2) {
-    api.add(BCHEM, balances[i])
-    api.add(USDT, balances[i + 1])
-  }
-}
-
-/**
- * Staking TVL:
- * Staking kontratındaki kullanıcı stake'i + henüz dağıtılmamış reward kasası.
- */
-async function stakingTvl(api) {
-  const [totalStaked, rewardVault] = await Promise.all([
-    api.call({ target: STAKING, abi: totalStakedAbi }),
-    api.call({ target: STAKING, abi: rewardVaultAbi }),
-  ])
-
-  api.add(BCHEM, totalStaked)
-  api.add(BCHEM, rewardVault)
-}
+const BCHEM = "0x9102E0A76a5e2823073Ed763a32Ba8ca8521b1F3";
+const STAKING_CONTRACT = "0x01F82039810f18F703F4c8b943940ce04Fa00C78";
 
 module.exports = {
   methodology:
-    `TVL, BSC üzerinde on-chain olarak hesaplanır. Uniswap v4 pool key'leri: USDT=${UNISWAP_V4_POOL_KEYS.USDT} . Swap/Liquidity TVL: ilgili pool'lara ait holder adreslerindeki BCHEM/USDT/USDC bakiyeleri toplamı. Staking TVL: staking kontratındaki totalStaked + rewardVault BCHEM.`,
+    "Counts BCHEM tokens locked in Bitchemical staking on BNB Chain.",
+  start: 1735689600,
   bsc: {
-    tvl: liquidityPoolTvl,
-    staking: stakingTvl,
+    staking: staking(STAKING_CONTRACT, BCHEM),
   },
-}
+};
