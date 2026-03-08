033cfbdacb0dfaaa6afb09a8912df5bd56b3cf1a
blackrock81 <35052576+blackrock81@users.noreply.github.com>
2026-03-09 02:56:15 +0300

    Update index.js

--- projects/bitchemical/index.js
+++ projects/bitchemical/index.js
@@ -4,9 +4,13 @@ const USDT = '0x55d398326f99059fF775485246999027B3197955'
 
 const STAKING = '0x01F82039810f18F703F4c8b943940ce04Fa00C78'
 
+const UNISWAP_V4_POOL_KEYS = {
+  USDT: '0xa41edf5a64964f2dea4b59b3d5a19cbf2198ef0e20b5309fdbb265d3a234c390',
+ 
+}
 
-const UNISWAP_V4_POOL_ADDRESSES = [
-  '0xa41edf5a64964f2dea4b59b3d5a19cbf2198ef0e20b5309fdbb265d3a234c390', // BCHEM/USDT pool
+const TVL_HOLDER_ADDRESSES = [
+  '0x28e2Ea090877bF75740558f6BFB36A5ffeE9e9dF',
 
 ]
 
@@ -14,12 +18,12 @@ const erc20BalanceOfAbi = 'function balanceOf(address) view returns (uint256)'
 const totalStakedAbi = 'function totalStaked() view returns (uint256)'
 const rewardVaultAbi = 'function rewardVault() view returns (uint256)'
 
-
 async function liquidityPoolTvl(api) {
   const calls = []
-  for (const pool of UNISWAP_V4_POOL_ADDRESSES) {
-    calls.push({ target: BCHEM, params: [pool] })
-    calls.push({ target: USDT, params: [pool] })
+  for (const holder of TVL_HOLDER_ADDRESSES) {
+    calls.push({ target: BCHEM, params: [holder] })
+    calls.push({ target: USDT, params: [holder] })
+   
   }
 
   const balances = await api.multiCall({
@@ -30,11 +34,14 @@ async function liquidityPoolTvl(api) {
   for (let i = 0; i < balances.length; i += 2) {
     api.add(BCHEM, balances[i])
     api.add(USDT, balances[i + 1])
-
+  
   }
 }
 
-
+/**
+ * Staking TVL:
+ * Staking kontratındaki kullanıcı stake'i + henüz dağıtılmamış reward kasası.
+ */
 async function stakingTvl(api) {
   const [totalStaked, rewardVault] = await Promise.all([
     api.call({ target: STAKING, abi: totalStakedAbi }),
@@ -47,7 +54,7 @@ async function stakingTvl(api) {
 
 module.exports = {
   methodology:
-    ,
+   USDT=${UNISWAP_V4_POOL_KEYS.USDT}. 
   bsc: {
     tvl: liquidityPoolTvl,
     staking: stakingTvl,
