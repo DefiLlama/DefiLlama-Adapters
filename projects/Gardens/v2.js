b7da33c3a7e778d6aae4b542a269493830a9d880
Corantin <corantin.noll@gmail.com>
2026-03-07 22:40:12 -0500

    refactor: Gardens use startegy getPoolAmount that consider super token balance

--- projects/Gardens/v2.js
+++ projects/Gardens/v2.js
@@ -1,10 +1,15 @@
 const {  getAddress } = require('ethers');
 const { gql } = require('graphql-request');
 
+const CV_STRATEGY_ABI = {
+  getPoolAmount: 'function getPoolAmount() view returns (uint256)',
+};
+const COMMON_POOL_STRATEGY = '0x96dee4849a6ab9e51ae36712794318001515dc91';
+
 const subgraphs = {
   polygon: 'https://api.studio.thegraph.com/query/102093/gardens-v2---polygon/version/latest/',
   xdai: 'https://api.studio.thegraph.com/query/102093/gardens-v2---gnosis/version/latest/',
-  arbitrum: 'https://api.studio.thegraph.com/query/102093/gardens-v2---arbitrum/version/latest/',
+  arbitrum: 'https://api.studio.thegraph.com/query/102093/gardens-v2---arbitrum/latest/',
   base: 'https://api.studio.thegraph.com/query/102093/gardens-v2---base/version/latest/',
   celo: 'https://api.studio.thegraph.com/query/102093/gardens-v2---celo/version/latest/',
   optimism: 'https://api.studio.thegraph.com/query/102093/gardens-v2---optimism/version/latest/',
@@ -47,28 +52,47 @@ async function fetchStrategiesAndCommunities(api) {
 async function tvl(api) {
   const { strategies, communities } = await fetchStrategiesAndCommunities(api);
 
-  // Multi calls 
-  const calls = strategies.map(s => ({ target: s.token, params: [s.id] }));
-  communities.forEach(c => {
-    if (c.garden?.id) {
-      calls.push({ target: c.garden.id, params: [c.id] });
+  // Strategy balances should use getPoolAmount() to include wrapped token accounting.
+  const strategyCalls = strategies.map(s => ({ target: s.id }));
+  const strategyBalances = await api.multiCall({ abi: CV_STRATEGY_ABI.getPoolAmount, calls: strategyCalls, permitFailure: true });
+  if (strategyBalances.length !== strategyCalls.length) {
+    throw new Error(`Mismatched strategy balances and calls length on ${api.chain}`);
+  }
+
+  strategyBalances.forEach((balance, i) => {
+    if (BigInt(balance || 0) > 0n) {
+      const token = getAddress(strategies[i].token);
+      api.add(token, balance);
     }
   });
 
-  // Multicall
-  const balances = await api.multiCall({ abi: 'erc20:balanceOf', calls, permitFailure: true });
-
-  // If not same length, something went wrong
-  if (balances.length !== calls.length) {
-    throw new Error(`Mismatched balances and calls length on ${api.chain}`);
+  const commonPoolIndex = api.chain === 'xdai'
+    ? strategies.findIndex(s => s.id?.toLowerCase() === COMMON_POOL_STRATEGY)
+    : -1;
+  if (commonPoolIndex !== -1) {
+    const { id, token } = strategies[commonPoolIndex];
+    const [tokenBalance] = await api.multiCall({
+      abi: 'erc20:balanceOf',
+      calls: [{ target: token, params: [id] }],
+      permitFailure: true,
+    });
+    console.log(`[Gardens-v2][xdai] Common pool ${id} token=${token} balanceOf=${tokenBalance || 0} getPoolAmount=${strategyBalances[commonPoolIndex] || 0}`);
   }
 
-  balances.forEach((balance, i) => {
+  const communityCalls = communities
+    .filter(c => c.garden?.id)
+    .map(c => ({ target: c.garden.id, params: [c.id] }));
+  const communityBalances = await api.multiCall({ abi: 'erc20:balanceOf', calls: communityCalls, permitFailure: true });
+  if (communityBalances.length !== communityCalls.length) {
+    throw new Error(`Mismatched community balances and calls length on ${api.chain}`);
+  }
+
+  communityBalances.forEach((balance, i) => {
     if (BigInt(balance || 0) > 0n) {
-      const token = getAddress(calls[i].target);
+      const token = getAddress(communityCalls[i].target);
       api.add(token, balance);
     }
-  });  
+  });
 }
 
 module.exports = {
