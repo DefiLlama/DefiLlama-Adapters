fda6d8b414252ee78250d8b70ca507c7984ba0f6
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2026-03-11 17:36:31 +0100

    Update Safe Treasury (#18402)

--- projects/treasury/safe.js
+++ projects/treasury/safe.js
@@ -1,27 +1,34 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const {  nullAddress,treasuryExports } = require("../helper/treasury");
+const { nullAddress, treasuryExports } = require("../helper/treasury");
+const { mergeExports } = require("../helper/utils");
 
-const treasury = "0x3EDf6868d7c42863E44072DaEcC16eCA2804Dea1" //
-const SAFE = ADDRESSES.ethereum.SAFE
-const safe_foundation_treasury = "0x1d4f25bc16b68c50b78e1040bc430a8097fd6f45"
-const safe_dao_2 = "0x0b00b3227a5f3df3484f03990a87e02ebad2f888"
-const safe_gnosisdao_joint_treasury = "0xd28b432f06cb64692379758B88B5fCDFC4F56922"
+const SAFE = ADDRESSES.ethereum.SAFE;
+const VESTING_ID = "0xF7EED36776C9DC083FA957E459DD07F5ABF4D77A97ED1E3356C553368C174762"
+const VESTING_ADDRESS = '0x96b71e2551915d98d22c448b040a3bc4801ea4ff'
+const VESTING_ABI = "function vestings(bytes32) view returns (address account, uint8 curveType, bool managed, uint16 durationWeeks, uint64 startDate, uint128 amount, uint128 amountClaimed, uint64 pausingDate, bool cancelled)"
 
+const vesting = async (api) => {
+  const vestingData = await api.call({ target: VESTING_ADDRESS, params: [VESTING_ID], abi: VESTING_ABI })
+  api.add(SAFE, vestingData.amount - vestingData.amountClaimed)
+}
 
-module.exports = treasuryExports({
-  optimism: {
-    tokens: [
-        nullAddress,
-        ADDRESSES.optimism.OP,
-     ],
-    owners: [treasury],
-  },
-  ethereum: {
-    tokens: [
-        nullAddress,
-     ],
-    owners: [safe_foundation_treasury, safe_dao_2,safe_gnosisdao_joint_treasury],
-    ownTokens: [SAFE],
-  },
-
-})
+module.exports = mergeExports(
+  treasuryExports({
+    optimism: {
+      tokens: [nullAddress, ADDRESSES.optimism.OP],
+      owners: ["0x3EDf6868d7c42863E44072DaEcC16eCA2804Dea1"],
+    },
+    ethereum: {
+      tokens: [nullAddress],
+      owners: [
+        "0x1d4f25bc16b68c50b78e1040bc430a8097fd6f45",
+        "0x0b00b3227a5f3df3484f03990a87e02ebad2f888",
+        "0xd28b432f06cb64692379758B88B5fCDFC4F56922",
+      ],
+      ownTokens: [SAFE],
+    },
+  }),
+  {
+    ethereum: { vesting },
+  }
+)
