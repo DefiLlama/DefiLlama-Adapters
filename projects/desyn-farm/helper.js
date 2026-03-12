17d078591266d14951d2cfc25165245f065739a7
santinoYoung <santino.bai@desyn.io>
2026-03-12 14:19:43 +0800

    desyn update chains

--- projects/desyn-farm/helper.js
+++ projects/desyn-farm/helper.js
@@ -1,6 +1,6 @@
 const { getConfig } = require('../helper/cache')
 
-const chains = ["arbitrum", "btr", "mode", "zklink", "core", "ailayer", "linea", "merlin", "scroll", "bsquared", "hemi", "bsc", "xsat", "goat", "plume_mainnet", "hsk", "abstract"];
+const chains = ["arbitrum", "btr", "zklink", "merlin", "abstract"];
 
 const blacklistChains = ["ethereum", "mode", "core", "ailayer", "bsquared", "hemi", "bsc", "goat", "plume_mainnet",];
 
