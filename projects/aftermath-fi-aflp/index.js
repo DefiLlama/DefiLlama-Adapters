84d98e4b4d6afbba3caa74cd5f41f5e4fa077974
Andrey Minin <am@aftermath.finance>
2026-03-12 12:49:11 +0100

    update AFLP vault ID to reflect the latest on-chain value

--- projects/aftermath-fi-aflp/index.js
+++ projects/aftermath-fi-aflp/index.js
@@ -1,11 +1,11 @@
 const sui = require("../helper/chain/sui");
 
-// Example type: 0x384ab5f72d0ef84e3b7e0f63b595e1f6d6ff6fb991160ae803458534c0795457::vault::Vault<
-//  0x2609a5d99772af4df0775c6167f9d72b428ce621a289fe5e5de2cb887ded6f4c::af_lp::AF_LP,
-//  0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC
+// Example type: 0x9e20798d97c110f6b36b7b3d8543aa9246322ef2fd8d83ad79ef3325d473bc2f::vault::Vault<
+//  0x08b18262b85423f64b60f279f0f1d935bb03b7cc9eebd8018e20bb1575f9d39a::af_lp::AF_LP,
+//  0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC>
 // >
 const AFLP_VAULT_ID =
-    "0xb0932e1b22b1e9b43ff6d8389b2c2a0b3f2db2a0d7b8a7981deacfda3fc59023";
+    "0x1f75030f3e687f824cce9354d96701cd72ab0e6f86a495fcca1ba70cabc13aea";
 
 async function tvl(api) {
     const vault = await sui.getObject(AFLP_VAULT_ID);
