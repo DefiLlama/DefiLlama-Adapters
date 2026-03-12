a658b9712f5e054cdde82f1946b9dc593b5029cc
John Agbanusi <agbanusijohn@gmail.com>
2026-03-12 18:19:19 +0100

    update methodology for myPlume tvl

--- projects/mystic-finance-myplume/index.js
+++ projects/mystic-finance-myplume/index.js
@@ -1,13 +1,17 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const token = '0xc2387E0feA344D1edEC3E93Bf2124f909f74938C'
+const feedContract = '0xFbb53aa72c10680e822e255aC70D10f8bb957D64'
 
 async function tvl(api) {
-  const supply = await api.call({ abi: 'uint256:totalSupply', target: token, });
+  const [supply, withheld] = await Promise.all([
+    api.call({ abi: 'uint256:getPlumeStakedAmount', target: feedContract, }),
+    api.call({ abi: 'uint256:getCurrentWithheldETH', target: feedContract, })
+  ]);
   api.add(ADDRESSES.null, supply)
+  api.add(ADDRESSES.null, withheld)
 }
 
 module.exports = {
-  methodology: 'Retrieve the total myPLUME supply. myPLUME is a liquid staking token for PLUME, the native token on the Plume network. The TVL is calculated by summing the total supply of myPLUME.',
+  methodology: 'Retrieve the total amount of PLUME staked via the Plume feed contract and current withheld ETH (insurance/withheld fund for instant withdrawals) to track underlying collateral.',
   plume_mainnet: {
     tvl
   }
