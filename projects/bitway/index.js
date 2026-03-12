1ad37f09466c46a04e74cbedb12a6e8d6cb92aad
Doug <dongdong@side.one>
2026-03-12 08:51:33 +0800

    Iterate STRATEGIES instead of hardcoding

--- projects/bitway/index.js
+++ projects/bitway/index.js
@@ -1,34 +1,35 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const STRATEGIES = {
-  ABSOLUTE_RETURN: {
+const STRATEGIES = [
+  {
+    name: "ABSOLUTE_RETURN",
     vault: '0x5C4a6903732532eeB3AE0803e062d8AE25d52BD1',
     tokens: [ADDRESSES.bsc.USDT]
   },
-  CORE_ALPHA: {
+  {
+    name: "CORE_ALPHA",
     vault: '0xb82E32062C773c7748776C06FdB11B92EDAE3B63',
     tokens: [ADDRESSES.bsc.USDT]
   }
-}
+]
 
 const abis = {
   getTVL: "function getTVL(address _token) external view returns (uint256)"
 }
 
 const tvl = async (api) => {
-  // for strategy absolute_return
-  let vault = STRATEGIES.ABSOLUTE_RETURN.vault
-  let tokens = STRATEGIES.ABSOLUTE_RETURN.tokens
-  let balances = await api.multiCall({ abi: abis.getTVL, calls: tokens.map((t) => ({ target: vault, params: [t]})) })
-  tokens.forEach((t, i) => { api.add(t, balances[i]) })
-
-  // for strategy core_alpha
-  vault = STRATEGIES.CORE_ALPHA.vault
-  tokens = STRATEGIES.CORE_ALPHA.tokens
-  balances = await api.multiCall({ abi: abis.getTVL, calls: tokens.map((t) => ({ target: vault, params: [t]})) })
-  tokens.forEach((t, i) => { api.add(t, balances[i]) })
+  for (const { vault, tokens } of STRATEGIES) {
+    const calls = tokens.map(token => ({ target: vault, params: [token] }));
+    const balances = await api.multiCall({
+      abi: abis.getTVL,
+      calls
+    });
+    tokens.forEach((token, i) => {
+      api.add(token, balances[i]);
+    });
+  }
 }
 
 module.exports = {
-  methodology: 'For each strategy and token: call getTVL(token) on its vault and sum per-token amounts',
+  methodology: 'For each strategy: call getTVL(token) on its vault and sum per-token amounts',
   bsc: { tvl }
 }
