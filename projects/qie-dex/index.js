9de7449b218b0669d5502b704795759168708c50
qieadmin <info@qiewallet.me>
2026-03-13 12:55:23 +0530

    Fix chain key to qiev3 for QIE DEX adapter

--- projects/qie-dex/index.js
+++ projects/qie-dex/index.js
@@ -1,7 +1,7 @@
 const { getUniTVL } = require('../helper/unknownTokens')
 
 module.exports = {
-  qie: {
+  qiev3: {
     tvl: getUniTVL({
       factory: '0x8E23128a5511223bE6c0d64106e2D4508C08398C',
       chain: 'qiev3',
