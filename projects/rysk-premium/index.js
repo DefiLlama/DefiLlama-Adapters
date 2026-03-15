9db0fa71af33d98ae6065265a325d03f53f07a45
ManInBlackie <carlo.parisi01234@gmail.com>
2026-03-15 18:51:46 +0100

    all addresses are lowercase now

--- projects/rysk-premium/index.js
+++ projects/rysk-premium/index.js
@@ -5,8 +5,8 @@ const vaultRegistryHyperliquid = '0x425ffAB71CEFc7aB96CBFbb75282e731234C1885'
 
 async function tvlHyperliquid(api) {
   const vaults = await api.call({ abi: 'function getAllVaults() view returns (address[])', target: vaultRegistryHyperliquid })
-  const collaterals = await api.multiCall({ abi: 'function collateralAsset() view returns (address)', calls: vaults })
-  const uniqueCollaterals = [...new Set(collaterals.map(c => c.toLowerCase()))]
+  const collaterals = (await api.multiCall({ abi: 'function collateralAsset() view returns (address)', calls: vaults })).map(c => c.toLowerCase())
+  const uniqueCollaterals = [...new Set(collaterals)]
 
   const vaultCalls = vaults.map((vault, i) => ({ target: collaterals[i], params: vault }))
   const marginCalls = marginPools.flatMap(pool => uniqueCollaterals.map(token => ({ target: token, params: pool })))
