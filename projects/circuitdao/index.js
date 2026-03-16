5bfe2e90ffc9c3b53f3a864262b18201027d1602
nimcon <32712736+nimcon@users.noreply.github.com>
2026-03-16 11:09:46 +0300

    Add docstrings to CircuitDAO TVL adapter

--- projects/circuitdao/index.js
+++ projects/circuitdao/index.js
@@ -4,6 +4,10 @@ const STATS_API = "https://api.circuitdao.com/protocol/stats";
 const MOJOS_PER_XCH = 1e12;
 const MBYC_PER_BYC = 1e3;
 
+/**
+ * TVL: USD value of total XCH locked in Circuit collateral vaults.
+ * `collateral` is returned in mojos (1 XCH = 1e12 mojos).
+ */
 async function tvl(api) {
   const data = await get(STATS_API);
   // stats array is oldest-first; take the most recent entry
@@ -12,10 +16,14 @@ async function tvl(api) {
   api.addCGToken("chia", xchAmount);
 }
 
+/**
+ * Borrowed: total BYC stablecoin in circulation (amount minted against XCH collateral, less amount melted).
+ * `byc_in_circulation` is in mBYC (milli-BYC); BYC is pegged 1:1 to USD,
+ * so it is reported as USDT to give DefiLlama a known $1 price.
+ */
 async function borrowed(api) {
   const data = await get(STATS_API);
   const latest = data.stats[data.stats.length - 1];
-  // byc_in_circulation is in mBYC; BYC is a USD stablecoin pegged 1:1
   const bycAmount = latest.byc_in_circulation / MBYC_PER_BYC;
   api.add("tether", bycAmount, { skipChain: true });
 }
