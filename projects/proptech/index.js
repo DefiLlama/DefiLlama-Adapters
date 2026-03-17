8a17f1978a2cb1a9923f9b488dae8d3be91457a0
FurqaanAli <Furqaanali@ismiletechnologies.com>
2026-03-18 00:54:06 +0530

    fix(proptech): remove unused import and add methodology clarification

--- projects/proptech/index.js
+++ projects/proptech/index.js
@@ -1,17 +1,16 @@
 // projects/proptech/index.js
-const { sumTokens2 } = require("../helper/unwrapLPs");
 const sdk = require('@defillama/sdk');
 
 const GENESIS_ADDRESS = '0x243AC97f37040A7f64a11B84c818cE222A8d3ab7';
 
 async function tvl(timestamp, block, chainBlocks, { api }) {
-  // Get native token balance
+  // Get native token balance from genesis address
   const balance = await sdk.api.eth.getBalance({
     target: GENESIS_ADDRESS,
     chain: 'proptech',
     block: chainBlocks?.proptech,
   });
-  
+
   // Add to TVL with CoinGecko ID
   api.addCGToken('proptech', balance.output);
   
@@ -19,7 +18,7 @@ async function tvl(timestamp, block, chainBlocks, { api }) {
 }
 
 module.exports = {
-  methodology: 'PTEK native token held in genesis miner address (0x243AC97f37040A7f64a11B84c818cE222A8d3ab7)',
+  methodology: 'PTEK native token held in genesis miner address (0x243AC97f37040A7f64a11B84c818cE222A8d3ab7). This address received 21M PTEK at genesis and represents the chain\'s initial token distribution.',
   start: 0,
   proptech: { tvl }
 };
