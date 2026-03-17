b3a87337f9f2dc4b83667ccfd925fa207a7cf460
FurqaanAli <Furqaanali@ismiletechnologies.com>
2026-03-18 00:47:42 +0530

    Add PropTech Mainnet TVL adapter (Chain ID 88788)

--- projects/proptech/index.js
+++ projects/proptech/index.js
@@ -1,12 +1,25 @@
+// projects/proptech/index.js
+const { sumTokens2 } = require("../helper/unwrapLPs");
+const sdk = require('@defillama/sdk');
+
 const GENESIS_ADDRESS = '0x243AC97f37040A7f64a11B84c818cE222A8d3ab7';
 
-async function tvl(api) {
-  const balance = await api.getBalance(GENESIS_ADDRESS);
-  api.addCGToken('native', balance);
+async function tvl(timestamp, block, chainBlocks, { api }) {
+  // Get native token balance
+  const balance = await sdk.api.eth.getBalance({
+    target: GENESIS_ADDRESS,
+    chain: 'proptech',
+    block: chainBlocks?.proptech,
+  });
+  
+  // Add to TVL with CoinGecko ID
+  api.addCGToken('proptech', balance.output);
+  
+  return api.getBalances();
 }
 
 module.exports = {
-  methodology: 'Tracks PTEK native token balance in the genesis miner address (0x243AC97f37040A7f64a11B84c818cE222A8d3ab7) which received 21M PTEK at genesis.',
+  methodology: 'PTEK native token held in genesis miner address (0x243AC97f37040A7f64a11B84c818cE222A8d3ab7)',
   start: 0,
-  proptech: { tvl },
+  proptech: { tvl }
 };
