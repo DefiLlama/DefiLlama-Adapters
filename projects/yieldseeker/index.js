9ee9231e534b14ace54d82fbb3c65f516dee6a0a
Krishan Patel <krishan711@gmail.com>
2026-03-17 15:58:39 +0000

    .

--- projects/yieldseeker/index.js
+++ projects/yieldseeker/index.js
@@ -4,6 +4,7 @@ module.exports = {
   methodology: 'Counts the tokens held across all YieldSeeker agent wallets. Wallet addresses are obtained from the YieldSeeker API, and token balances are read on-chain.',
   // NOTE(krishan711): Can't time travel yet cos the agent list is obtained via API. re-enable once agent list is enumerable onchain
   timetravel: false,
+  doublecounted: true,
   base: {
     tvl: yieldAgentsTvl,
   },
