fe1b7862f8b0e1d0d8ccd49156eac43bcf9fc89a
neelra8n <neelratn.official@gmail.com>
2026-03-19 00:23:29 +0530

    updated chain

--- projects/goevolve/index.js
+++ projects/goevolve/index.js
@@ -2,22 +2,18 @@ const eusd = {
     sei: "0xf2282e641cd3ceeafd4e24663d409fcb68edc1df",
 }
 
-const l2Chains = Object.keys(eusd).filter(chain => chain !== 'ethereum')
-
-l2Chains.forEach(chain => {
-    module.exports[chain] = {
+module.exports['sei'] = {
         tvl: async (api) => {
             const calls = [
-                eusd[chain],
+                eusd['sei'],
             ].filter(Boolean)
             const supply = await api.multiCall({ calls, abi: 'erc20:totalSupply' })
             api.addTokens(calls, supply, {
                 name: 'eUSD',
                 symbol: 'eUSD',
                 decimals: 18,
-                chain: chain,
-                address: eusd[chain],
+                chain: 'sei',
+                address: eusd['sei'],
             });
         }
     }
-});
