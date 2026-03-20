7f91da89ee7dad66e69b899dcd741b5c1e630a10
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-03-20 09:21:05 -0400

    add uniswap on tempo (#18486)
    Co-authored-by: g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>

--- projects/helper/env.js
+++ projects/helper/env.js
@@ -44,6 +44,7 @@ const DEFAULTS = {
   FUEL_CUSTOM_RPC: 'https://mainnet.fuel.network/v1/graphql',
   TATUM_PUBLIC_API_KEY: "t-" + "698992414f6f4e3435d62161" + "-3d94ca2d70024efdaf3ca6fd",
   KEETA_RPC: "https://rep1.main.network.api.keeta.com/api",
+  TEMPO_RPC_MULTICALL: "0xcA11bde05977b3631167028862bE2a173976CA11",
 }
 
 const ENV_KEYS = [
