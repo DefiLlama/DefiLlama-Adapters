aa683f9e4eacdbdbe277191cef1aaaa6c59d2c28
rohannero <rohanzarathustra@gmail.com>
2026-03-23 16:55:55 -0400

    fix prjx

--- registries/uniswapV3.js
+++ registries/uniswapV3.js
@@ -1084,6 +1084,7 @@ const uniV3Configs = {
     hyperliquid: {
       factory: '0xff7b3e8c00e57ea31477c32a5b52a58eea47b072',
       fromBlock: 7876741,
+      permitFailure: true,
     },
   },
   'quickswap-v3': {
