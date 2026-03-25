e934a2b36f5b64409296dc87334a8ebc47bd8d71
zgorizzo69 <benjamin.mateo@protonmail.com>
2026-03-25 13:10:08 +0100

    fix owner

--- projects/treasury/fira.js
+++ projects/treasury/fira.js
@@ -1,11 +1,7 @@
 const { addFiraTreasuryPositions } = require("../fira/treasuryHelper");
 
 const owners = [
-  '0xF3D913De4B23ddB9CfdFAF955BAC5634CbAE95F4',
-  '0xc32e2a2F03d41768095e67b62C9c739f2C2Bc4aA',
-  '0x81ad394C0Fa87e99Ca46E1aca093BEe020f203f4',
-  '0xe3FD5A2cA538904A9e967CBd9e64518369e5a03f',
-  '0xcbf85D44178c01765Ab32Af72D5E291dcd39A06B',
+  '0x97fD565B913b439Ff16774Da152B1f71F05A82F1' 
 ]
 
 async function tvl(api) {
