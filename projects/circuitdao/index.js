f14eeef56dbe02e5ec19773f2de3139bf8e1c21f
nimcon <32712736+nimcon@users.noreply.github.com>
2026-03-25 18:10:03 +0300

    Remove borrowed tracking

--- projects/circuitdao/index.js
+++ projects/circuitdao/index.js
@@ -2,7 +2,6 @@ const { get } = require("../helper/http");
 
 const STATS_API = "https://api.circuitdao.com/protocol/stats";
 const MOJOS_PER_XCH = 1e12;
-const MBYC_PER_BYC = 1e3;
 
 /**
  * TVL: USD value of total XCH locked in Circuit collateral vaults.
@@ -16,21 +15,8 @@ async function tvl(api) {
   api.addCGToken("chia", xchAmount);
 }
 
-/**
- * Borrowed: total BYC stablecoin in circulation (amount minted against XCH collateral, less amount melted).
- * `byc_in_circulation` is in mBYC (milli-BYC); BYC is pegged 1:1 to USD,
- * so it is reported as USDT to give DefiLlama a known $1 price.
- */
-async function borrowed(api) {
-  const data = await get(STATS_API);
-  const latest = data.stats[data.stats.length - 1];
-  const bycAmount = latest.byc_in_circulation / MBYC_PER_BYC;
-  api.add("tether", bycAmount, { skipChain: true });
-}
-
 module.exports = {
   chia: {
     tvl,
-    borrowed,
   },
 };
