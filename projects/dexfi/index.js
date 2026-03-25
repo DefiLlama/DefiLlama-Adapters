2709b91d04b64c8236d1b97c9efda49e4bcd1f08
badgerdf <badger@gordian.agency>
2026-03-25 09:33:19 +0300

    upd

--- projects/dexfi/index.js
+++ projects/dexfi/index.js
@@ -4,28 +4,45 @@ const { default: BigNumber } = require('bignumber.js')
 const CONFIG = {
   sonic: {
     factory: "0x095d35c49d2d0ea2eba3e2f9e377966db35af7e2",
-    nativeToken: "0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38", // wS
+    nativeToken: "0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38",
+    start: '2025-05-03',
   },
   avax: {
     factory: "0x5764dad2fd4b6918949c6ae86081819ca8c19749",
-    nativeToken: "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7", // WAVAX
+    nativeToken: "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
+    start: '2025-08-12',
   },
   bsc: {
     factory: "0xc9dc65aed28bdb016726d32d0f8c2cd5c9461961",
-    nativeToken: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", // WBNB
+    nativeToken: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    start: '2025-10-03',
   },
   ethereum: {
     factory: "0x4c1a8a04577286ce58d0723b1a90160f380e550a",
-    nativeToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // WETH
+    nativeToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    start: '2026-01-29',
   },
   base: {
     factory: "0xcb34f261a5284554bb9fea8aa12a0578c4ba3fc6",
-    nativeToken: "0x4200000000000000000000000000000000000006", // WETH on Base
+    nativeToken: "0x4200000000000000000000000000000000000006",
+    start: '2025-04-12',
   },
   arbitrum: {
     factory: "0x061f8132b344cb2a32d3895eb3ebc2ff87455f79",
-    nativeToken: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", // WETH on Arbitrum
+    nativeToken: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+    start: '2026-02-17',
   },
+};
+
+const TARGET_AMOUNT_MULTIPLIER = 50
+function estimatedAmountTo(tokensInAmount) {
+  const adjustedBalance = BigNumber(tokensInAmount).div(TARGET_AMOUNT_MULTIPLIER).toFixed(0);
+  return adjustedBalance;
+};
+
+function estimatedAmountFrom(tokensOutAmount) {
+  const adjustedBalance = BigNumber(tokensOutAmount).times(TARGET_AMOUNT_MULTIPLIER).toFixed(0);
+  return adjustedBalance;
 };
 
 const getVaults = async (api, factory) => {
@@ -61,8 +78,9 @@ const tvl = async (api) => {
   }));
   const liquidities = await api.multiCall({ calls: liquidityCalls, abi: abi.vault.liquidity, permitFailure: true });
 
+  // call to estimate a swapping a `1/TARGET_AMOUNT_MULTIPLIER` of locked tvl to native
   const nativeAmountCalls = vaultFarmsWithConnectors.map(({ connector }, i) => ({
-    target: connector, params: [targetAmountTo(liquidities[i]) || '0'],
+    target: connector, params: [liquidities[i] ? estimatedAmountTo(liquidities[i]) : '0'],
   }));
   const nativeAmounts = await api.multiCall({
     calls: nativeAmountCalls,
@@ -71,23 +89,12 @@ const tvl = async (api) => {
   });
 
   const totalNativeAmount = nativeAmounts.reduce((acc, amount) => {
-    if (amount && amount !== '0') return acc.plus(targetAmountFrom(amount));
+    if (amount && amount !== '0') return acc.plus(estimatedAmountFrom(amount));
     return acc;
   }, BigNumber(0)).toFixed(0);
   api.add(nativeToken, totalNativeAmount);
 };
 
 Object.keys(CONFIG).forEach((chain) => {
-  module.exports[chain] = { tvl };
+  module.exports[chain] = { tvl, start: CONFIG[chain].start };
 })
-
-const TARGET_AMOUNT_MULTIPLIER = 30
-function targetAmountTo(tokensInAmount) {
-  const adjustedBalance = BigNumber(tokensInAmount).div(TARGET_AMOUNT_MULTIPLIER).toFixed(0);
-  return adjustedBalance;
-};
-
-function targetAmountFrom(tokensOutAmount) {
-  const adjustedBalance = BigNumber(tokensOutAmount).times(TARGET_AMOUNT_MULTIPLIER).toFixed(0);
-  return adjustedBalance;
-};
