bfff6bdf211c810a88e0063e85e72f32c396359c
bheluga <anonymous_cat_llama@protonmail.com>
2026-03-25 23:35:53 +0530

    move virtus-cl to uni3 registry

--- projects/virtus-protocol-cl/index.js
+++
@@ -1,9 +0,0 @@
-const { uniV3Export } = require("../helper/uniswapV3")
-
-module.exports = uniV3Export({
-    base: {
-        factory: '0x0e5Ab24beBdA7e5Bb3961f7E9b3532a83aE86B48',
-        fromBlock: 42960000,
-        eventAbi: 'event PoolCreated(address indexed token0, address indexed token1, int24 indexed tickSpacing, address pool)',
-      },
-})
