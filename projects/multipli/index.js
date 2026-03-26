497088a7e889927ff31350899efce77068778595
gauthampharish <gautham.harish@brine.fi>
2026-03-26 16:53:55 +0530

    feat: add TVL tracking for arbitrum chain

--- projects/multipli/index.js
+++ projects/multipli/index.js
@@ -4,7 +4,6 @@ const axios = require("axios")
 async function getTvlByChain(chain) {
     const tvl_response = (await axios.get("https://api.multipli.fi/multipli/v1/external-aggregator/defillama/tvl/"))
     const tvl = tvl_response.data['payload'][chain]
-    console.log(chain, tvl)
     return tvl
 }
 
