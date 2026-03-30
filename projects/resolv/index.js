330a1de1259b124db9bb092745a6071c3091e93d
wayne <waynebruce0x@gmail.com>
2026-03-30 13:09:01 +0100

    resolv

--- projects/resolv/index.js
+++ projects/resolv/index.js
@@ -14,10 +14,11 @@ module.exports = {
       if (api.timestamp > 1777106021) throw new Error('Resolv error - has the hack been resolved?')
       if (api.timestamp > 1774137600) {
         api.add(ADDRESSES.ethereum.USDC, (await api.call({ target: tokens[0], abi: 'erc20:totalSupply' })) / 10 **12)
-        // Subtract 80M after hack 
+        // Subtract 80M after hack => adjusted to 34M
         // https://etherscan.io/tx/0x41b6b9376d174165cbd54ba576c8f6675ff966f17609a7b80d27d8652db1f18f
         // https://etherscan.io/tx/0xfe37f25efd67d0a4da4afe48509b258df48757b97810b28ce4c649658dc33743
-        api.add(ADDRESSES.ethereum.USDC, -8e13)
+        // https://x.com/ResolvLabs/status/2037110361711870214
+        api.add(ADDRESSES.ethereum.USDC, -34e12)
         api.add(tokens[1], await api.call({ target: tokens[1], abi: 'erc20:totalSupply' }))
       } else {
         api.add(tokens, await api.multiCall({ calls: tokens, abi: 'erc20:totalSupply' }))
