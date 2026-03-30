8d403dabd5b10ea8983c89c5810e73f9e0374c06
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-03-30 16:42:58 +0200

    minor fix

--- projects/aave-v4/index.js
+++ projects/aave-v4/index.js
@@ -1,10 +1,15 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
+const methodologies = require("../helper/methodologies");
 
-const HUBS = [
-  "0xCca852Bc40e560adC3b1Cc58CA5b55638ce826c9", // Core Hub
-  "0x06002e9c4412CB7814a791eA3666D905871E536A", // Plus Hub
-  "0x943827DCA022D0F354a8a8c332dA1e5Eb9f9F931", // Prime Hub
-];
+const config = {
+  ethereum: {
+    HUBS: [
+      "0xCca852Bc40e560adC3b1Cc58CA5b55638ce826c9", // Core Hub
+      "0x06002e9c4412CB7814a791eA3666D905871E536A", // Plus Hub
+      "0x943827DCA022D0F354a8a8c332dA1e5Eb9f9F931", // Prime Hub
+    ],
+  }
+}
 
 const abis = {
   getAssetUnderlyingAndDecimals: "function getAssetUnderlyingAndDecimals(uint256) view returns (address, uint8)",
@@ -13,8 +18,8 @@ const abis = {
 };
 
 async function getHubAssets(api) {
-  const assetCounts = await api.multiCall({ abi: abis.getAssetCount, calls: HUBS });
-  const calls = HUBS.flatMap((hub, i) =>
+  const assetCounts = await api.multiCall({ abi: abis.getAssetCount, calls: config[api.chain].HUBS });
+  const calls = config[api.chain].HUBS.flatMap((hub, i) =>
     Array.from({ length: assetCounts[i] }, (_, assetId) => ({ target: hub, params: [assetId] }))
   );
   const assets = await api.multiCall({ abi: abis.getAssetUnderlyingAndDecimals, calls });
@@ -35,7 +40,8 @@ async function borrowed(api) {
   owed.forEach((amount, i) => api.add(hubAssets[i].underlying, amount));
 }
 
-module.exports = {
-  methodology: "Counts the tokens held in Aave V4 Hub contracts.",
-  ethereum: { tvl, borrowed },
-};
+module.exports = { methodology: methodologies.lendingMarket, }
+
+Object.keys(config).forEach(chain => {
+  module.exports[chain] = { tvl, borrowed, }
+})
