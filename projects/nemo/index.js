017c4319d3b6686c4f4ad9e6ce1cb0244fea63f2
rohannero <rohanzarathustra@gmail.com>
2026-03-31 17:20:08 -0400

    add fixes

--- projects/nemo/index.js
+++ projects/nemo/index.js
@@ -1,4 +1,3 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const sui = require("../helper/chain/sui");
 const BigNumber = require("bignumber.js");
 const {COIN_CONFIG} = require("./coinConfig.js");
