680deb2bc6ad39abef3839405c7e159f6b46ffb9
rohannero <rohanzarathustra@gmail.com>
2026-04-01 19:09:14 -0400

    minor fix

--- projects/cabal/index.js
+++ projects/cabal/index.js
@@ -1,6 +1,6 @@
 const { post } = require('../helper/http')
 
-const INITIA_REST_RPC = 'https://rest.initia.xyz/initia/move/v1/view/json'
+const REST_RPC = 'https://rest.initia.xyz/initia/move/v1/view/json'
 const CABAL_MODULE_ADDRESS = '0x53c3f5d8e11844ba3747ebaec1b2d25051574ffbeedc69d72068395991e3ea28'
 const INIT_METADATA_ADDRESS = '0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9'
 const USDC_INIT_LP_METADATA_ADDRESS = '0x543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1'
@@ -15,7 +15,7 @@ function toNum(str) {
 }
 
 async function fetchView(functionName, moduleName, args) {
-  const response = await post(INITIA_REST_RPC, {
+  const response = await post(REST_RPC, {
     address: CABAL_MODULE_ADDRESS,
     module_name: moduleName,
     function_name: functionName,
@@ -25,10 +25,10 @@ async function fetchView(functionName, moduleName, args) {
   return response.data
 }
 
-async function initiaTvl(api) {
+async function tvl(api) {
   const [initStakes, lpStakes] = await Promise.all([
     fetchView('get_real_total_stakes', 'pool_router', [`"${INIT_METADATA_ADDRESS}"`]),
-    fetchView('get_real_total_stakes', 'pool_router', [`"${USDC_INIT_LP_METADATA_ADDRESS}"`]),
+    fetchView('get_real_total_stakes', 'pool_router', [`"${USDC_INIT_LP_METADATA_ADDRESS}"`])
   ])
   api.add(INIT_METADATA_ADDRESS, toNum(initStakes))
   api.add(USDC_INIT_LP_METADATA_ADDRESS, toNum(lpStakes))
@@ -37,6 +37,6 @@ async function initiaTvl(api) {
 module.exports = {
   timetravel: false,
   methodology: 'TVL is calculated as the sum of: INIT token stakes (sxINIT vault), USDC-INIT LP token stakes (cbl LP vault) on Initia, and iUSD holdings across the Cabal iUSD and Delta Neutral INIT EVM vaults on the cabal-1 L2.',
-  initia: { tvl: initiaTvl },
+  initia: { tvl },
   cabal: { tvl: async (api) => api.erc4626Sum2({ calls: CABAL_VAULTS }) },
 }
