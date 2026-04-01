aa0965a704e7d650b3b9ef2fcf741bbc9bf6ab97
rohannero <rohanzarathustra@gmail.com>
2026-04-01 19:10:31 -0400

    fix

--- projects/cabal/index.js
+++ projects/cabal/index.js
@@ -1,6 +1,6 @@
 const { post } = require('../helper/http')
 
-const REST_RPC = 'https://rest.initia.xyz/initia/move/v1/view/json'
+const REST_URL = 'https://rest.initia.xyz/initia/move/v1/view/json'
 const CABAL_MODULE_ADDRESS = '0x53c3f5d8e11844ba3747ebaec1b2d25051574ffbeedc69d72068395991e3ea28'
 const INIT_METADATA_ADDRESS = '0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9'
 const USDC_INIT_LP_METADATA_ADDRESS = '0x543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1'
@@ -15,7 +15,7 @@ function toNum(str) {
 }
 
 async function fetchView(functionName, moduleName, args) {
-  const response = await post(REST_RPC, {
+  const response = await post(REST_URL, {
     address: CABAL_MODULE_ADDRESS,
     module_name: moduleName,
     function_name: functionName,
