a886998b97ca5d984c602c779c7f40031a8e0b11
rohannero <rohanzarathustra@gmail.com>
2026-04-01 18:38:07 -0400

    refactor

--- projects/cabal/index.js
+++ projects/cabal/index.js
@@ -1,9 +1,13 @@
 const { post } = require('../helper/http')
 
-const REST_URL = 'https://rest.initia.xyz/initia/move/v1/view/json'
+const INITIA_REST_RPC = 'https://rest.initia.xyz/initia/move/v1/view/json'
 const CABAL_MODULE_ADDRESS = '0x53c3f5d8e11844ba3747ebaec1b2d25051574ffbeedc69d72068395991e3ea28'
 const INIT_METADATA_ADDRESS = '0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9'
 const USDC_INIT_LP_METADATA_ADDRESS = '0x543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1'
+const CABAL_VAULTS = [
+  '0x5Eb1A2e8218a0140151ce3445A3799d6C4433f46', // iUSD vault
+  '0x69fdf919612Ef40e89e56282C6891aca41640204', // Delta Neutral vault
+]
 
 function toNum(str) {
   const clean = String(str).replace(/[^\d.]/g, '');
@@ -11,7 +15,7 @@ function toNum(str) {
 }
 
 async function fetchView(functionName, moduleName, args) {
-  const response = await post(REST_URL, {
+  const response = await post(INITIA_REST_RPC, {
     address: CABAL_MODULE_ADDRESS,
     module_name: moduleName,
     function_name: functionName,
@@ -21,10 +25,10 @@ async function fetchView(functionName, moduleName, args) {
   return response.data
 }
 
-async function tvl(api) {
+async function initiaTvl(api) {
   const [initStakes, lpStakes] = await Promise.all([
     fetchView('get_real_total_stakes', 'pool_router', [`"${INIT_METADATA_ADDRESS}"`]),
-    fetchView('get_real_total_stakes', 'pool_router', [`"${USDC_INIT_LP_METADATA_ADDRESS}"`])
+    fetchView('get_real_total_stakes', 'pool_router', [`"${USDC_INIT_LP_METADATA_ADDRESS}"`]),
   ])
   api.add(INIT_METADATA_ADDRESS, toNum(initStakes))
   api.add(USDC_INIT_LP_METADATA_ADDRESS, toNum(lpStakes))
@@ -32,8 +36,7 @@ async function tvl(api) {
 
 module.exports = {
   timetravel: false,
-  methodology: 'TVL is calculated as the sum of INIT token stakes and USDC-INIT LP token stakes, each multiplied by their on-chain USD value.',
-  initia: {
-    tvl
-  }
+  methodology: 'TVL is calculated as the sum of: INIT token stakes (sxINIT vault), USDC-INIT LP token stakes (cbl LP vault) on Initia, and iUSD holdings across the Cabal iUSD and Delta Neutral INIT EVM vaults on the cabal-1 L2.',
+  initia: { tvl: initiaTvl },
+  cabal: { tvl: async (api) => api.erc4626Sum2({ calls: CABAL_VAULTS }) },
 }
