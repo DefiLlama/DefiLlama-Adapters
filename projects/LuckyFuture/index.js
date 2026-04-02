f8f1b77c66876e898f836a24a481bf1cd39e5953
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-04-02 18:09:27 -0400

    add hm

--- projects/LuckyFuture/index.js
+++ projects/LuckyFuture/index.js
@@ -1,6 +1,9 @@
 // Drift was hacked - all TVL marked down to 0
 module.exports = {
   timetravel: false,
+  hallmarks: [
+    ["2026-04-01", "Drift hack"]
+  ],
   doublecounted: true,
   methodology: "Calculate sum of spot positions in vaults with unrealized profit and loss",
   solana: {
