6eac075f0f93b37bfb6cae91e96f2cdb707734f6
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-04-02 18:07:36 -0400

    add hallmark

--- projects/vectis/index.js
+++ projects/vectis/index.js
@@ -12,6 +12,9 @@ const { post } = require('../helper/http');
 
 module.exports = {
   timetravel: false,
+  hallmarks: [
+    ["2026-04-01", "Drift hack"]
+  ],
   doublecounted: true,
   methodology: "Calculate sum of spot positions in vaults with unrealized profit and loss",
   solana: {
