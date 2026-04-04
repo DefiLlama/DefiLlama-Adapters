63d22f7b7b5de4efe7a3b46e50c231ad2796234a
Eden <noat.eth@gmail.com>
2026-04-04 23:49:31 +0700

    bl eBTC on curvance

--- projects/curvance/index.js
+++ projects/curvance/index.js
@@ -1,20 +1,29 @@
 const { sumTokens2 } = require('../helper/unwrapLPs')
 const config = {
-  monad: '0x1310f352f1389969Ece6741671c4B919523912fF'
+  monad: {
+    centralRegistry: '0x1310f352f1389969Ece6741671c4B919523912fF',
+    blcklistedMarkets: [
+      // single source of deposit and no borrowers
+      // from this address: 0x8da30b68b1e11761e1307eb10564a39e01ae4480 created multiple Safe wallets and made deposits
+      '0x2840772E14fFbe337aB966727B7D1Dd09BDc76E4',
+    ],
+  },
 }
 
 Object.keys(config).forEach(chain => {
-  const centralRegistry = config[chain]
+  const centralRegistry = config[chain].centralRegistry
+  const blcklistedMarkets = config[chain].blcklistedMarkets.map(m => String(m).toLowerCase())
 
   async function getMarkets(api) {
     const managers = await api.call({ abi: 'address[]:marketManagers', target: centralRegistry })
     const markets = await api.multiCall({ abi: 'address[]:queryTokensListed', calls: managers })
-    return markets.flat()
+    return markets.flat().filter(m => !blcklistedMarkets.includes(String(m).toLowerCase()))
   }
 
   module.exports[chain] = {
     tvl: async (api) => {
       const contracts = await getMarkets(api)
+      console.log(contracts)
       const tokens = await api.multiCall({ abi: 'address:asset', calls: contracts })
       return sumTokens2({ api, tokensAndOwners2: [tokens, contracts] })
     },
