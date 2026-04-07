ef1d0ebe60eef1e6c53147bd6e28fe7fed996a92
Kozharina <120336880+Kozharina@users.noreply.github.com>
2026-04-07 18:47:27 +0300

    Remove metadata keys from adapter

--- projects/perpstats/index.js
+++ projects/perpstats/index.js
@@ -1,12 +1,6 @@
 module.exports = {
-  name: "PerpStats",
-  category: "Portfolio Tracker",
-  logo: "perpstats.png",
-  url: "https://perpstats.com/portfolio",
-  twitter: "perpstats",
-  description: "Free DeFi analytics for 224+ perpetual DEXes. Track APY yields, TVL, trading volume and Open Interest across Hyperliquid, GMX, dYdX, Jupiter Perps.",
   start: 1700000000,
   ethereum: {
-    tvl: false,
+    tvl: false
   }
 };
