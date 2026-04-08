8c9eb57d3c6712bff9042522632a7b33079d2cec
Xpertfinancedev <xpertfinance@proton.me>
2026-04-08 21:36:22 +0800

    Create index.js

---
+++ projects/xpert/index.js
@@ -0,0 +1,6 @@
+const { compoundExports2, methodology } = require('../helper/compound')
+
+module.exports = {
+  ink: compoundExports2({ comptroller: '0x4f3b08B7FE4E14f728d084850A7B9CFF2E759Eb7' }),
+  methodology,
+}
