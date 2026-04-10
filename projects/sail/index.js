2c7e5a2a23a0f412a72c3c3fa009ed93fa6a89f7
jmad-fungi <jmastorgadiz@gmail.com>
2026-04-10 14:44:26 +0200

    Include endpoint call for fetching all active wallet. Integrate real tokens addresses

--- projects/sail/index.js
+++ projects/sail/index.js
@@ -2,20 +2,20 @@ const { getConfig } = require('../helper/cache');
 const { allPoolTokens: basePoolTokens } = require('./base');
 const { allPoolTokens: arbitrumPoolTokens } = require('./arbitrum');
 
+async function getOwners() {
+    const data = await getConfig('sail/active-wallets', 'https://app.sail.money/api/v1/projects/sail/pages/institutions/custom/active_wallets');
+    const owners = Array.isArray(data) ? data : data.result;
+    return owners.filter(o => o !== '');
+}
+
 async function baseTvl(api) {
-    const owners = ["0x5F01488eD803b3f48BbF4020504344711787bfAE"]; //await getConfig('sail/' + api.chain, 'https://api.sail.xyz/api/v1/data/smart-accounts?chainId=8453');
-    return api.sumTokens({
-        tokens: basePoolTokens,
-        owners: owners.filter(o => o !== ''),
-    });
+    const owners = await getOwners();
+    return api.sumTokens({ tokens: basePoolTokens, owners });
 }
 
 async function arbitrumTvl(api) {
-    const owners = ["0x5F01488eD803b3f48BbF4020504344711787bfAE"]; //await getConfig('sail/' + api.chain, 'https://api.sail.xyz/api/v1/data/smart-accounts?chainId=42161');
-    return api.sumTokens({
-        tokens: arbitrumPoolTokens,
-        owners: owners.filter(o => o !== ''),
-    });
+    const owners = await getOwners();
+    return api.sumTokens({ tokens: arbitrumPoolTokens, owners });
 }
 
 module.exports = {
