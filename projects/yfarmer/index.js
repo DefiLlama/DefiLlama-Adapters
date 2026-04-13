f7cbbe2c554e611e80f2ac9dbf4d4bbbd0ee5e9a
Armand Morin <72650161+Armand-Morin@users.noreply.github.com>
2026-04-13 20:26:19 +0200

    Update index.js

--- projects/yfarmer/index.js
+++ projects/yfarmer/index.js
@@ -1,10 +1,11 @@
 const USDC = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+const VAULT = "0x71c298a6eb10e7958ce25a450a706330a4c946c0"
 
 module.exports = {
   base: {
     tvl: async (api) => {
       const totalAssets = await api.call({
-        target: "0x71c298a6eb10e7958ce25a450a706330a4c946c0",
+        target: VAULT,
         abi: "function totalAssets() view returns (uint256)",
       })
 
