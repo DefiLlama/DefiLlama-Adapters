2938a5a7ce062b730e6e76e04a2f90a9d2f6251a
OpSecChainFailure <110779103+JazzBashara@users.noreply.github.com>
2026-04-14 22:19:27 +0200

    Add Ferdy on Mega

--- projects/ferdyflip/index.js
+++ projects/ferdyflip/index.js
@@ -4,7 +4,7 @@ const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs");
 // https://docs.ferdyflip.xyz/developers/contracts/platform
 const config = {
   avax: {
-    tokens: [ADDRESSES.avax.WAVAX, nullAddress], owners: ['0x20AfbaC35B333dA4fE7230CC60946F88ee87aAA3'],
+    tokens: [ADDRESSES.avax.WAVAX, nullAddress], owners: ['0xe0a69f4d29c891b9c5c7368b591ed3109bcb80f7'],
   },
   base: {
     tokens: [ADDRESSES.base.WETH, nullAddress], owners: ['0x3b6014e4b38791444a352D687022D6d6d79Eb99c'],
@@ -12,11 +12,14 @@ const config = {
   mantle: {
     tokens: [ADDRESSES.mantle.WMNT, nullAddress], owners: ['0x559036D9466C93d8Ca3c2232626548e62ceBC07c'],
   },
+  megaeth: {
+    tokens: [ADDRESSES.megaeth.ETH, nullAddress], owners: ['0x58Daa362b6732d224a618149C5872b4942947681'],
+  },
 };
 
 Object.keys(config).forEach((chain) => {
   module.exports[chain] = {
-    start: 1675962000, //Fri Feb 10 2023
+    start: '2023-02-09', //Fri Feb 10 2023
     tvl: sumTokensExport(config[chain]),
   };
 });
