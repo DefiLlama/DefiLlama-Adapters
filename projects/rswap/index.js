0b2e8587d19f9f51f4912a3aeb15617eb7377c5c
Timan Rebel <timan@trebel.nl>
2026-04-17 11:19:08 +0200

    add rswap

---
+++ projects/rswap/index.js
@@ -0,0 +1,33 @@
+const sdk = require("@defillama/sdk");
+const { sumTokens, queryAddresses } = require('../helper/chain/radixdlt');
+const { getConfig } = require('../helper/cache');
+const { get } = require('../helper/http');
+
+module.exports = {
+   radixdlt: {
+      tvl: async (api) => {
+         const pools = await getConfig('rswap', null, {
+            fetcher: async () => {
+               let items = [];
+               let cursor = 0;
+               do {
+                  const { data, next_cursor } = await get(`https://dex.reddicks.meme/api/pairs-defillama?cursor=${cursor}&limit=100`);
+                  items.push(...data);
+                  sdk.log(`Fetched ${items.length} pools`, data.length, next_cursor);
+                  cursor = next_cursor;
+               } while (items.length % 100 === 0 && cursor !== 0);
+
+               return items;
+            }
+         });
+         const data = await queryAddresses({ addresses: pools.map((i) => i.address) });
+         const owners = [];
+         data.forEach((c) => {
+            owners.push(c.details.state.fields.find((i) => i.field_name === 'base_pool').value);
+            owners.push(c.details.state.fields.find((i) => i.field_name === 'quote_pool').value);
+         });
+         return sumTokens({ owners, api });
+      },
+   },
+   timetravel: false,
+};
