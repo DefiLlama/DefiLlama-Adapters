945a51718f068875f7890d5375e10ba0cc000211
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-04-19 23:03:15 +0200

    refactor

--- projects/soulbound-finance/index.js
+++ projects/soulbound-finance/index.js
@@ -1,22 +1,9 @@
-const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokens2, nullAddress } = require('../helper/unwrapLPs')
-
 const DEPOSIT_POOL = '0xe8D294F3fff2A5CB34D15eCdEF34A53b01f5A462'
 const CLAIM_POOL = '0x5F5a08fFFE8071410f3ECe2FCC40D81B457b28ce'
 
-const tokens = [
-  nullAddress,                        // ETH
-  ADDRESSES.arbitrum.USDC_CIRCLE,     // USDC (native Circle)
-  ADDRESSES.arbitrum.DAI,             // DAI
-  ADDRESSES.arbitrum.WBTC,            // WBTC
-]
-
 async function tvl(api) {
-  return sumTokens2({
-    api,
-    owners: [DEPOSIT_POOL, CLAIM_POOL],
-    tokens,
-  })
+  const tokens = await api.call({  abi: 'address[]:getTokenList', target: DEPOSIT_POOL})
+  return api.sumTokens({ tokens, owners: [DEPOSIT_POOL, CLAIM_POOL] })
 }
 
 module.exports = {
