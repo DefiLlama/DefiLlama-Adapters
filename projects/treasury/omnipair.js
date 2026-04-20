d78d95b97d6321c7e3c99ba90081c892d64e4c1e
Henrystats <65398020+henrystats@users.noreply.github.com>
2026-04-20 23:52:31 +0100

    update omnipair treasury address

--- projects/treasury/omnipair.js
+++ projects/treasury/omnipair.js
@@ -2,6 +2,6 @@ const { treasuryExports } = require("../helper/treasury")
 
 module.exports = treasuryExports({
   solana: {
-    owners: ["4JpPs9Mi11qoj6GthQPiTjUc4gXq8BAoSs3AD6NVjQUZ"],
+    owners: ["8s6Jdoh7tgUqmU3D2EmpNJHSvuN5U4NybpLAdsiMitwB"],
   },
 })
