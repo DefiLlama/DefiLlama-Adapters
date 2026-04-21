e34016519fdc8a9086e273c132576d7b955eb9ff
springx <>
2026-04-21 09:44:08 +0800

    add springx

---
+++ projects/springx/index.js
@@ -0,0 +1,18 @@
+const { sumTokens2 } = require("../helper/unwrapLPs")
+
+const FARM_ADDRESS = "0xafF15Ca201C08F05f65d4d0A9d9C368C8356f796"
+async function tvl(api){
+    let pools = await api.call({
+        abi:ABI.getPoolTotalTvl,
+        target:FARM_ADDRESS,
+    })
+    pools.forEach(({ assets, tvl }) => { api.add(assets, tvl) })
+}
+
+module.exports = {
+    plasma: { tvl }
+  };
+
+const ABI = {
+    getPoolTotalTvl:"function getPoolTotalTvl() view returns (tuple(uint256 pid, address assets, uint256 tvl)[])"
+}
