a1cd8d2539de2eaa1ef5c9e78af403a44caca5f4
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-04-20 23:05:49 -0400

    add omfg to ownTokens

--- projects/treasury/omnipair.js
+++ projects/treasury/omnipair.js
@@ -3,5 +3,6 @@ const { treasuryExports } = require("../helper/treasury")
 module.exports = treasuryExports({
   solana: {
     owners: ["8s6Jdoh7tgUqmU3D2EmpNJHSvuN5U4NybpLAdsiMitwB"],
+    ownTokens: ["omfgRBnxHsNJh6YeGbGAmWenNkenzsXyBXm3WDhmeta"]
   },
 })
