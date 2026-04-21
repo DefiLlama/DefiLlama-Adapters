f4a55d9be2419cbacd9a75df15fecb5b358ce176
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-04-21 20:37:34 +0200

    Update index.js

--- projects/chisino/index.js
+++ projects/chisino/index.js
@@ -3,7 +3,7 @@ const { sumTokensExport } = require('../helper/unwrapLPs')
 module.exports = {
   megaeth: {
     tvl: sumTokensExport({
-      owner: '0x4fd02a1A80923cE1D7E70A8719421431Ea286941',
+      owner: '0x5d3712fF54226A55ab8fE866678A87a73cBC49b7',
       token: '0x2eA493384F42d7Ea78564F3EF4C86986eAB4a890', // USDmY
     }),
   }
