559195a98a667b0ba440daa30719d502d3d78ce6
rohannero <rohanzarathustra@gmail.com>
2026-04-21 21:46:02 -0400

    refactor treasury

--- projects/treasury/monstro-fun.js
+++ projects/treasury/monstro-fun.js
@@ -1,10 +1,8 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const { sumTokens2 } = require('../helper/unwrapLPs')
 const { treasuryExports } = require('../helper/treasury')
 
 const MONSTRO = '0x1d3bE1CC80cA89DDbabe5b5C254AF63200e708f7'
 const ALB = '0x1dd2d631c92b1aCdFCDd51A0F7145A50130050C4'
-const ES_ALB = '0x365c6d588e8611125De3bEA5B9280C304FA54113'
 
 const DAO_WALLETS = [
   '0x4713b3ab36C9759043694757E6Cb8123915a8dd0', // Treasury
@@ -14,6 +12,8 @@ const DAO_WALLETS = [
 ]
 
 module.exports = treasuryExports({
+  isComplex: true,
+  complexOwners: ['0x4713b3ab36C9759043694757E6Cb8123915a8dd0'],
   base: {
     owners: DAO_WALLETS,
     ownTokens: [MONSTRO],
