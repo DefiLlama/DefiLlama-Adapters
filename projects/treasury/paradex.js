e7dc15880fd783ed7d27caef484405d8e6c13e22
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2026-04-22 15:38:07 -0300

    paradex: add treasury

---
+++ projects/treasury/paradex.js
@@ -0,0 +1,18 @@
+const { treasuryExports, nullAddress } = require("../helper/treasury");
+const ADDRESSES = require('../helper/coreAssets.json')
+
+//https://app.paradex.trade/dime/overview
+const PARADEX_TREASURY = '0xF1f82188E7D3B54b8872986869D4207F8A78A4F1';
+const DIME_TOKEN = "0xb32E10022FFBeDfE10bc818a1C7e67D9d87e0fa7";
+
+module.exports = treasuryExports({
+    ethereum: {
+        owners: [PARADEX_TREASURY],
+        tokens: [
+            nullAddress,
+            ADDRESSES.ethereum.USDC,
+            DIME_TOKEN
+        ],
+        ownTokens: [DIME_TOKEN]
+    }
+})
