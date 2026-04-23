47f23531575bea922e72aea41e8ea7d6659dcb83
rohannero <rohanzarathustra@gmail.com>
2026-04-22 22:11:22 -0400

    fix

--- projects/monstro-fun/index.js
+++ projects/monstro-fun/index.js
@@ -1,10 +1,24 @@
+const { sumTokensExport } = require('../helper/unwrapLPs')
+
+const MONSTRO = '0x1d3bE1CC80cA89DDbabe5b5C254AF63200e708f7'
+const STAKING_CONTRACT = '0x99741758A3BCD7A95B80845E124C5C499DF4742b'
+
+async function stakingTvl(api) {
+  const totalStaked = await api.call({
+    target: STAKING_CONTRACT,
+    abi: 'function totalStaked() view returns (uint256)',
+  })
+  api.add(MONSTRO, totalStaked)
+}
+
 module.exports = {
+  methodology: 'Staking counts MONSTRO tokens staked by users via totalStaked(), excluding unreleased emissions.',
   hallmarks: [
-    ['2024-12-21', 'BSC->Base ecosystem migration'],
-    ['2026-01-21', 'Legacy contracts deprecated, migrated to DAO'],
+    ['2026-01-21', 'Legacy contracts deprecated'],
+    ['2026-02-01', 'DAO launch with staking'],
   ],
-  deadFrom: '2026-01-21',
   base: {
     tvl: () => ({}),
+    staking: stakingTvl,
   },
 }
