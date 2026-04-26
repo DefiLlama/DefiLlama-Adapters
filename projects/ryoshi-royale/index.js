d7f7d0d6ed02d9e55d4acb1b0b667b15871ef104
Pandalaki <153791120+Pandalaki@users.noreply.github.com>
2026-04-26 16:35:29 -0400

    Refactor TVL function to use SDK for balance retrieval

--- projects/ryoshi-royale/index.js
+++ projects/ryoshi-royale/index.js
@@ -1,11 +1,18 @@
+const sdk = require('@defillama/sdk');
+
 const BATTLE_REWARDS = "0x15b7d7402441DEA17637D5Edb373Ea773f135EDe";
 const VAULTS = "0x200246E9c5E80496EaD817632d543B869A4537cC";
 const RYOSHI_TOKEN = "0x055c517654d72A45B0d64Dc8733f8A38E27Fd49C";
 
 async function tvl(api) {
-  const croBalance = await api.getBalance({ target: BATTLE_REWARDS });
-  api.add("crypto-com-chain", croBalance);
+  // Native CRO locked in Battle Rewards contract
+  const croBalance = await sdk.api.eth.getBalance({ 
+    target: BATTLE_REWARDS, 
+    chain: 'cronos' 
+  });
+  api.add("crypto-com-chain", croBalance.output);
 
+  // RYOSHI tokens locked in Vaults
   await api.sumTokens({ owner: VAULTS, tokens: [RYOSHI_TOKEN] });
 }
 
