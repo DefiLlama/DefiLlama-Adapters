3abb37545d7cea48d441d788139ea6ce4b612e34
den <dzianis.smalonski@protofire.io>
2026-04-27 10:05:00 +0200

    adds shapeswap-v3 and shapeswap-v2 TVL registry modules.

--- registries/uniswapV2.js
+++ registries/uniswapV2.js
@@ -1595,8 +1595,10 @@ const uniV2Configs = {
   'reservoir-tools-v2': {
     abstract: '0x566d7510dEE58360a64C9827257cF6D0Dc43985E',
     zero_network: '0x1B4427e212475B12e62f0f142b8AfEf3BC18B559',
-    shape: '0xb411eaf2f2070822b26e372e3ea63c5060ba45e6',
     ink: '0xfe57a6ba1951f69ae2ed4abe23e0f095df500c04',
+  },
+  'shapeswap-v2': {
+    shape: '0xb411eaf2f2070822b26e372e3ea63c5060ba45e6',
   },
   'revoswap': {
     xlayer: '0xa38498983e7b31DE851e36090bc9D1D8fB96BE5E',
--- registries/uniswapV3.js
+++ registries/uniswapV3.js
@@ -1163,10 +1163,6 @@ const uniV3Configs = {
       factory: '0xA1160e73B63F322ae88cC2d8E700833e71D0b2a1',
       fromBlock: 1,
     },
-    shape: {
-      factory: '0xeCf9288395797Da137f663a7DD0F0CDF918776F8',
-      fromBlock: 1,
-    },
     redstone: {
       factory: '0xece75613Aa9b1680f0421E5B2eF376DF68aa83Bb',
       fromBlock: 1,
@@ -1175,6 +1171,12 @@ const uniV3Configs = {
       factory: '0x640887A9ba3A9C53Ed27D0F7e8246A4F933f3424',
       fromBlock: 1,
       blacklistedTokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'],
+    },
+  },
+  'shapeswap-v3': {
+    shape: {
+      factory: '0xeCf9288395797Da137f663a7DD0F0CDF918776F8',
+      fromBlock: 1,
     },
   },
   'retro': {
