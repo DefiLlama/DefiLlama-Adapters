78e5217a041f7b61ade8035d7fb051954480e385
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-04-27 10:41:24 +0200

    remove support brc20 tokens

--- projects/xrgb/index.js
+++ projects/xrgb/index.js
@@ -3,5 +3,6 @@ const bitcoinAddressBook = require('../helper/bitcoin-book/index.js')
 
 module.exports = {
   methodology: "XRGB as bridge,Unlock ERC404 on all chains",
+  deadFrom: '2024-06-10',
   bitcoin: {  tvl: sumTokensExport({ owners: bitcoinAddressBook.xrgb, blacklistedTokens: ['XRGB'] }) }
 };
