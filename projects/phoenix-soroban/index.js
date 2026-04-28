64b14fbd0d80af44c4f1eb60b458911fbcd6db7d
Pablo Ramirez <pablo@matrixcr.ai>
2026-04-28 13:34:24 -0600

    fix(phoenix): correct methodologies import path

--- projects/phoenix-soroban/index.js
+++ projects/phoenix-soroban/index.js
@@ -5,7 +5,7 @@
 'use strict'
 
 const { sorobanFactoryTvl } = require('../helper/chain/sorobanFactory')
-const methodologies = require('../../helper/methodologies')
+const methodologies = require('../helper/methodologies')
 
 const PHOENIX_FACTORY = 'CB4SVAWJA6TSRNOJZ7W2AWFW46D5VR4ZMFZKDIKXEINZCZEGZCJZCKMI'
 
