5455cfb3ae31fa6336aca414ecf67cb576795053
bheluga <anonymous_cat_llama@protonmail.com>
2026-04-28 19:54:18 +0530

    remove from reservior tools

--- registries/uniswapV3.js
+++ registries/uniswapV3.js
@@ -1163,10 +1163,6 @@ const uniV3Configs = {
       factory: '0xA1160e73B63F322ae88cC2d8E700833e71D0b2a1',
       fromBlock: 1,
     },
-    shape: {
-      factory: '0xeCf9288395797Da137f663a7DD0F0CDF918776F8',
-      fromBlock: 1,
-    },
     redstone: {
       factory: '0xece75613Aa9b1680f0421E5B2eF376DF68aa83Bb',
       fromBlock: 1,
