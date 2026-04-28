f5a3821ab640c5f8bc56af56e1fc668e4467b735
rgevis-rocksolid <richard@rocksolid.network>
2026-04-28 11:20:05 -0500

    Update index.js

--- projects/rocksolid/index.js
+++ projects/rocksolid/index.js
@@ -1,7 +1,7 @@
 const vaults = [
-  '0x936facdf10c8c36294e7b9d28345255539d81bc7', // RockSolid rock.rETH
-  '0x7a12D4B719F5aA479eCD60dEfED909fb2A37e428', // RockSolid rock.loopedETH
-  '0xba71097e426983d840569edfa1a01396b56d86ad', // RockSolid MegaETH USDm
+  '0x936facdf10c8c36294e7b9d28345255539d81bc7', // RockSolid rock.rETH, curated by Tulipa Capital
+  '0x7a12D4B719F5aA479eCD60dEfED909fb2A37e428', // RockSolid rock.loopedETH, curated by Tulipa Capital
+  '0xba71097e426983d840569edfa1a01396b56d86ad', // RockSolid MegaETH USDm, curated by Tulipa Capital
 ]
 async function tvl(_, _b, _cb, { api }) {
   // Use erc4626Sum helper to automatically get underlying asset and totalAssets
