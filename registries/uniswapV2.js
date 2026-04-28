e7155b1f6b539b9f374d1fbf27dcbb2729c4c072
bheluga <bheluga@defillama.com>
2026-04-28 20:00:15 +0530

    track shapeswap v2 and v3 tvl (#18931)

--- registries/uniswapV2.js
+++ registries/uniswapV2.js
@@ -1595,7 +1595,6 @@ const uniV2Configs = {
   'reservoir-tools-v2': {
     abstract: '0x566d7510dEE58360a64C9827257cF6D0Dc43985E',
     zero_network: '0x1B4427e212475B12e62f0f142b8AfEf3BC18B559',
-    shape: '0xb411eaf2f2070822b26e372e3ea63c5060ba45e6',
     ink: '0xfe57a6ba1951f69ae2ed4abe23e0f095df500c04',
   },
   'revoswap': {
@@ -1694,6 +1693,9 @@ const uniV2Configs = {
   },
   'shadowswap': {
     core: '0x326Ee96748E7DcC04BE1Ef8f4E4F6bdd54048932',
+  },
+  'shapeswap-v2': {
+    shape: '0xb411eaf2f2070822b26e372e3ea63c5060ba45e6',
   },
   'sharelock': {
     era: '0xea51CE8A1f9C1Cbbf5B89D9B1dA4A94fB1557866',
