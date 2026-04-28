edc5f897d30feac448b020c382a7d4e7c0f01eee
luka dodelia <luka@lnh.ge>
2026-04-28 10:51:22 +0400

    fixes: 18908
    https://github.com/DefiLlama/DefiLlama-Adapters/issues/18908

--- projects/MorpheusAI/index.js
+++ projects/MorpheusAI/index.js
@@ -1,6 +1,8 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 const PROJECT_CONTRACT = '0x47176B2Af9885dC6C4575d4eFd63895f7Aaa4790';
 const PROJECT_CONTRACT_V2 = '0xDf1AC1AC255d91F5f4B1E3B4Aef57c5350F64C7A';
+const MOR_ARBITRUM = '0x092bAaDB7DEf4C3981454dD9c0A0D7FF07bCFc86';
+const BUILDERS_ARBITRUM = '0xC0eD68f163d44B6e9985F0041fDf6f67c6BCFF3f';
 
 async function tvl(api) {
   const v2Deployment = +new Date('2025-09-17') / 1e3
@@ -18,6 +20,10 @@ async function tvl(api) {
 
 }
 
+async function arbitrumTvl(api) {
+  return api.sumTokens({ owner: BUILDERS_ARBITRUM, tokens: [MOR_ARBITRUM] })
+}
+
 const abi = {
   "getAllATokens": "function getAllATokens() view returns ((string symbol, address tokenAddress)[])",
   "getAllReservesTokens": "function getAllReservesTokens() view returns ((string symbol, address tokenAddress)[])",
@@ -26,9 +32,10 @@ const abi = {
 module.exports = {
   timetravel: true,
   misrepresentedTokens: false,
-  methodology: 'Calculates TVL based on stETH deposits in the project contract.',
+  methodology: 'Calculates TVL based on Ethereum deposits in the project contracts and MOR deposits in the Arbitrum Builders contract.',
   start: '2024-02-08',  // Feb-08-2024 07:33:35 AM UTC in Unix timestamp
   ethereum: { tvl },
+  arbitrum: { tvl: arbitrumTvl },
   hallmarks: [
     ['2024-04-06', "MOR token launch"],  // May-08-2024 12:00:00 AM UTC in Unix timestamp
   ],
