b68b73b822c74bf6390a87f69fac6a1fe21eb4fd
0xpeluche <0xpeluche@proton.me>
2026-05-01 10:11:51 +0200

    Fix: Bulbaswap (try cache)

--- registries/uniswapV3.js
+++ registries/uniswapV3.js
@@ -255,6 +255,7 @@ const uniV3Configs = {
     morph: {
       factory: '0xFf8578C2949148A6F19b7958aE86CAAb2779CDDD',
       fromBlock: 25159,
+      onlyUseExistingCache: true,
       blacklistedTokens: [
         '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a',
         '0xe3C0FF176eF92FC225096C6d1788cCB818808b35',
