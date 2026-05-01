ce90d9e4bc74c2b1c397822c50f77c3f680b33a0
gordy-ftw <mjpeters1987@gmail.com>
2026-05-01 12:21:04 +0000

    Add Jade protocol listing

---
+++ projects/jade/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  methodology:
+    "Jade is a Solana-native, fair-launch store-of-value protocol forked from ORE. No TVL is currently tracked on-chain; protocol activity is captured via the fees dimension (10% of SOL deployed on Jade boards accumulates in the Treasury PDA F4Uwd5sQT8go5r6iiejrVc2iurYSc2dA4RKvX3cLB8e3 each round-reset, with 99% routed to JADE buyback-and-burn).",
+  timetravel: false,
+  misrepresentedTokens: false,
+};
