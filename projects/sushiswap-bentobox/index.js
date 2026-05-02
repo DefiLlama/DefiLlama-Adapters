2de8258b875f358f34281d5644150d0ce88186e5
pdubey7 <rdpushp1@gmail.com>
2026-05-02 22:34:00 +0530

    fix: use >= for deadFrom boundary check

--- projects/sushiswap-bentobox/index.js
+++ projects/sushiswap-bentobox/index.js
@@ -21,7 +21,7 @@ const DEAD_FROM = new Date('2024-06-12');
 bentobox_chains.forEach((chain) => {
   module.exports[chain] = {
     tvl: chain === "fantom" || chain === "avax" ? () => ({}) : async (api) => {
-      if (new Date(api.timestamp * 1000) > DEAD_FROM) return {};
+      if (new Date(api.timestamp * 1000) >= DEAD_FROM) return {};
       return bentobox(api);
     },
   };
