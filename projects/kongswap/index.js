23ab8d1f511e823eac3812e6bf32dedc31249d04
bheluga <bheluga@defillama.com>
2026-05-02 15:26:43 +0530

    Update index.js

--- projects/kongswap/index.js
+++ projects/kongswap/index.js
@@ -1,14 +1,11 @@
 const { get } = require('../helper/http')
 
 module.exports = {
-  deadFrom: '2026-04-06',
   misrepresentedTokens: true,
   icp: { tvl },
 }
 
 async function tvl(api) {
-  if (api.timestamp * 1000 > new Date('2026-04-06')) return {}
-
-  let { total_tvl } = await get('https://api.kongswap.io/api/pools/totals')
+  let { total_tvl } = await get('https://api2.kongswap.io/pools/totals')
   api.addCGToken('tether', Math.round(total_tvl))
 }
