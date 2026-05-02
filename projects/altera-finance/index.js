6838faef94d591fcadbb09d8ec95c283f0534eca
terabyte39 <JohnPhilip7727@outlook.com>
2026-05-02 11:24:44 -0700

    tvl address update

--- projects/altera-finance/index.js
+++ projects/altera-finance/index.js
@@ -1,18 +1,13 @@
-const { sumTokens2 } = require('../helper/unwrapLPs')
-
-const POOLINFO = '0x744EaA81544E940F054AC1931025f1B6D26f2FDE'
-const PM = '0x1dc0d79dab5a4a630d2af3f9dc973af97f390427'
+const POOLINFO = '0x5072fF50B5ad5ED1Fe87b934cbFdB679394E1B1a';
 
 async function tvl(api) {
-  const pools = await api.call({
-    target: POOLINFO,
-    abi: 'function getAllPoolsTVL() view returns ((address token, uint8 decimals, uint256 amount)[])',
-  })
-  const tokens = pools.map(p => p.token)
-  return sumTokens2({ api, tokens, owner: PM, resolveUniV3: true })
+  const res = await api.call({ target: POOLINFO, abi:  "function getAllPoolsTVL() view returns ((address token, uint8 decimals, uint256 amount)[])"})
+  res.forEach(i => api.add(i.token, i.amount))
 }
 
 module.exports = {
-  start: 1776849595,
-  ethereum: { tvl },
+  start: 1777746061,
+  ethereum: {
+    tvl
+  }
 }
