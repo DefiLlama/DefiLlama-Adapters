5ada07606a37b45b5c998e22c1a778924008e312
alxn787 <alenabraham787@gmail.com>
2026-05-03 20:26:17 +0530

    fix(resonatefinance): mark adapter dead

--- projects/resonatefinance/index.js
+++ projects/resonatefinance/index.js
@@ -1,22 +1,14 @@
 const { get } = require('../helper/http');
 
 
-//Full Calculations for computing TVL for Resonate Finance is done internally by Resonate and exposed via API
-//API Endpoint: https://api.resonate.finance/{chainId}/tvl
-//https://github.com/Revest-Finance/railway-monorepo/blob/main/src/api.ts",
 async function tvl(api) {
-  let usdValue = 0;
-  do {
-      const data = await get(`https://api.resonate.finance/${api.chainId}/tvl`);
-      usdValue = data;
-  } while (usdValue == 0)
-  
-  return { 
-    tether: usdValue,
-  };
+  if ((api.timestamp ?? Date.now() / 1000) * 1000 > new Date('2025-08-01')) return {}
+
+  api.addUSDValue(await get(`https://api.resonate.finance/${api.chainId}/tvl`));
 }
 
 module.exports = {
+    deadFrom: '2025-08-01',
     methodology: "We sum all the tokens deposited as principal and any unclaimed interest accrued on it.",
     hallmarks: [
       ['2023-04-25', "Launch of Regen Portal"],
