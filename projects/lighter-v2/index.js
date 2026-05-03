e591966adcddb0c151a93412ceb65a1ce3006f5b
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-05-03 01:08:15 -0400

    use labels

--- projects/lighter-v2/index.js
+++ projects/lighter-v2/index.js
@@ -11,11 +11,12 @@ const config = {
     tokens: [
       ADDRESSES.ethereum.USDC,
       ADDRESSES.null,
-      '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
-      '0x514910771af9ca656af840dff83e8264ecf986ca',
-      '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
-      '0x5a98fcbea516cf06857215779fd812ca3bef1b32',
+      ADDRESSES.ethereum.AAVE,
+      ADDRESSES.ethereum.LINK,
+      ADDRESSES.ethereum.UNI,
+      ADDRESSES.ethereum.LIDO,
       '0x56072C95FAA701256059aa122697B133aDEd9279',
+      '0xA27EC0006e59f245217Ff08CD52A7E8b169E62D2',
     ],
   },
 };
