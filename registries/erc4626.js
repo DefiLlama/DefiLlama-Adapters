cb31dcec819df47d3f6035fadb388137c303c61e
rohannero <rohanzarathustra@gmail.com>
2026-05-03 01:31:23 -0400

    add vault

--- registries/erc4626.js
+++ registries/erc4626.js
@@ -145,11 +145,12 @@ const configs = {
     ],
   },
   'RockSolid': {
-    methodology: 'Calls totalAssets() on the RockSolid rock.rETH and rock.loopedETH vaults to get the total amount of rETH and ETH managed by the vaults.',
+    methodology: 'Calls totalAssets() on the RockSolid rock.rETH, rock.loopedETH, and rockUSDm vaults to get the total amount of rETH and ETH managed by the vaults.',
     start: 1756339201,
     ethereum: [
       '0x936facdf10c8c36294e7b9d28345255539d81bc7',
       '0x7a12D4B719F5aA479eCD60dEfED909fb2A37e428',
+      '0xba71097e426983d840569edfa1a01396b56d86ad'
     ],
   },
   'avon': {
