0ac2debcec405fefa1468c6556874f73fabf8a5b
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-05-03 18:05:32 +0200

    fix sushi trident

---
+++ projects/sushiswap-kashi/index.js
@@ -0,0 +1,26 @@
+const _f = () => ({})
+
+module.exports = {
+  // "deadFrom": "2025-01-01",
+  "misrepresentedTokens": true,
+  "ethereum": {
+    "tvl": _f,
+    "borrowed": _f
+  },
+  "polygon": {
+    "tvl": _f,
+    "borrowed": _f
+  },
+  "bsc": {
+    "tvl": _f,
+    "borrowed": _f
+  },
+  "avax": {
+    "tvl": _f,
+    "borrowed": _f
+  },
+  "arbitrum": {
+    "tvl": _f,
+    "borrowed": _f
+  }
+}
