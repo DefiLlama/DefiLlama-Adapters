e066ebd3acf98dbefeedbb8c1d2fb5956cbb9960
El-Mavericko <michaelbands@yahoo.com>
2026-05-04 23:08:07 +0100

    fix: remove Titan chain from powerflow-bridge and cosmos endpoints - fixes #19113
    Titan chain LCD infrastructure is fully offline (all titanlab.io endpoints
    return NXDOMAIN, titanlab.io itself returns HTTP 530). Removed the titan
    LCD entry from cosmos.js and stripped titan TVL from the powerflow-bridge
    adapter. The adapter continues to report TVL for Ethereum and Solana.

--- projects/powerflow-bridge/index.js
+++ projects/powerflow-bridge/index.js
@@ -1,70 +1,8 @@
 const BigNumber = require("bignumber.js");
 const ADDRESSES = require("../helper/coreAssets.json");
-const { getBalance2 } = require("../helper/chain/cosmos.js");
 const { sumTokensExport } = require("../helper/unwrapLPs.js");
 const { getConnection, sumTokens2 } = require("../helper/solana.js");
 const { PublicKey, LAMPORTS_PER_SOL } = require("@solana/web3.js");
-
-const config = {
-  titan: {
-    atkx: {
-      coinGeckoId: "tokenize-xchange",
-      decimals: 18,
-    },
-    [`${ADDRESSES.titan.factory}/titan1pvrwmjuusn9wh34j7y520g8gumuy9xtl3gvprlljfdpwju3x7ucsgehpjy/usdc`]: {
-        coinGeckoId: "usdc",
-        decimals: 6,
-      },
-    [`${ADDRESSES.titan.factory}/titan1pvrwmjuusn9wh34j7y520g8gumuy9xtl3gvprlljfdpwju3x7ucsgehpjy/usdt`]:
-      {
-        coinGeckoId: "tether",
-        decimals: 6,
-      },
-    [`${ADDRESSES.titan.factory}/titan1pvrwmjuusn9wh34j7y520g8gumuy9xtl3gvprlljfdpwju3x7ucsgehpjy/eth`]:
-      {
-        coinGeckoId: "ethereum",
-        decimals: 18,
-      },
-    [`${ADDRESSES.titan.factory}/titan1pvrwmjuusn9wh34j7y520g8gumuy9xtl3gvprlljfdpwju3x7ucsgehpjy/sol`]:
-      {
-        coinGeckoId: "solana",
-        decimals: 9,
-      },
-    "factory/titan1pvrwmjuusn9wh34j7y520g8gumuy9xtl3gvprlljfdpwju3x7ucsgehpjy/meow":
-      {
-        coinGeckoId: "meow-2",
-        decimals: 8,
-      },
-    "factory/titan1pvrwmjuusn9wh34j7y520g8gumuy9xtl3gvprlljfdpwju3x7ucsgehpjy/oracler":
-      {
-        coinGeckoId: "oracler-ai",
-        decimals: 6,
-      },
-    "factory/titan1pvrwmjuusn9wh34j7y520g8gumuy9xtl3gvprlljfdpwju3x7ucsgehpjy/monkeys":
-      {
-        coinGeckoId: "monkeys-2",
-        decimals: 6,
-      },
-  },
-};
-
-async function titanTvl(api) {
-  const bals = await getBalance2({
-    chain: "titan",
-    owner: "titan1pvrwmjuusn9wh34j7y520g8gumuy9xtl3gvprlljfdpwju3x7ucsgehpjy",
-  });
-
-  Object.entries(bals).forEach(([denom, amount]) => {
-    const token = config.titan[denom];
-    if (!token) return;
-
-    const tokenAmount = new BigNumber(amount)
-      .dividedBy(new BigNumber(10).pow(token.decimals))
-      .toNumber();
-
-    api.addCGToken(token.coinGeckoId, tokenAmount);
-  });
-}
 
 const erc20Contracts = [
   ADDRESSES.ethereum.USDC,
@@ -106,9 +44,6 @@ async function solanaTvl() {
 
 module.exports = {
   methodology: "Counts the tokens locked in the PowerFlow Bridge contracts.",
-  titan: {
-    tvl: titanTvl,
-  },
   ethereum: {
     tvl: ethereumTvl,
   },
