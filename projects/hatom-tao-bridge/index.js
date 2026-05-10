2dc29ef7fbf8f1e3eb2b42de03a00a0eff222c4b
vipineth <vipineth@gmail.com>
2026-05-10 16:17:41 +0530

    fix hatom tao bridge tvl

--- projects/hatom-tao-bridge/index.js
+++ projects/hatom-tao-bridge/index.js
@@ -1,6 +1,14 @@
-const { getExports } = require('../helper/heroku-api')
+const { get } = require('../helper/http')
+
+const WTAO = 'WTAO-4f5363'
+
+const tvl = async (api) => {
+  const { minted, burnt } = await get(`https://api.multiversx.com/tokens/${WTAO}/supply`)
+  const supply = Number(BigInt(minted) - BigInt(burnt)) / 1e9
+  api.addCGToken('bittensor', supply)
+}
 
 module.exports = {
-  methodology: 'Value of tao locked in the bridge contract',
-  ...getExports("hatom-tao-bridge", ['bittensor']),
+  methodology: 'TVL is the circulating supply of Wrapped TAO (WTAO-4f5363) on MultiversX, which is minted 1:1 against TAO held in the Hatom Bittensor bridge custody.',
+  bittensor: { tvl },
 }
