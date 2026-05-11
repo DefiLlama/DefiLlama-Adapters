d4f1e282bb2322b8aa3a2c6e7b26abff93ced945
amanraj1608 <archanaamanraj@gmail.com>
2026-05-11 22:28:30 +0530

    parallelize calls and guard NAV oracle

--- projects/coinshift-uspc/index.js
+++ projects/coinshift-uspc/index.js
@@ -4,9 +4,12 @@ const IUSPC = '0xdc807c3a618B6B1248481783def7ED76700B9eC6'
 const USPC_NAV_FEED = '0x02ae69C812DD749c32afb4F1723F6833EeF3d7a3'
 
 async function tvl(api) {
-  const supply = await api.call({ target: IUSPC, abi: 'erc20:totalSupply' })
-  const price = await api.call({ target: USPC_NAV_FEED, abi: 'int256:latestAnswer' })
-  const priceDecimals = await api.call({ target: USPC_NAV_FEED, abi: 'uint8:decimals' })
+  const [supply, price, priceDecimals] = await Promise.all([
+    api.call({ target: IUSPC, abi: 'erc20:totalSupply' }),
+    api.call({ target: USPC_NAV_FEED, abi: 'int256:latestAnswer' }),
+    api.call({ target: USPC_NAV_FEED, abi: 'uint8:decimals' }),
+  ])
+  if (BigInt(price) <= 0n) throw new Error('Invalid USPC NAV from Chainlink feed')
   const value = (BigInt(supply) * BigInt(price)) / 10n ** BigInt(priceDecimals)
   api.add(ADDRESSES.ethereum.USDC, value.toString())
 }
