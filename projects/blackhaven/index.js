82851358158b2f57c08734ca7f070f3eefe477f0
blackhavenxyz <dev@blackhaven.xyz>
2026-05-11 14:07:25 +0200

    Update index.js

--- projects/blackhaven/index.js
+++ projects/blackhaven/index.js
@@ -1,9 +1,11 @@
 const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
-  methodology: "Counts the RBT tokens staked in the bond contract.",
+  methodology: "USDM and Aave USDM held by the treasury, ensuring the backing of the RBT token",
   megaeth: {
-    tvl: () => ({}),
-    staking: sumTokensExport({ owner: '0x40f5F3654Db5F7F56cCe33caF0F7a0CAaaE57EAc', token: '0x8F77A685bDe702E6d32A103e9AeB41906317D7e5' }),
-  },
+    tvl: sumTokensExport({
+        owner: "0xb59126f8a13F907f63e67CFc248160698cE41918",
+        tokens: ["0xFAfDdbb3FC7688494971a79cc65DCa3EF82079E7", "0x5dF82810CB4B8f3e0Da3c031cCc9208ee9cF9500"]
+      })
+  }
 }
