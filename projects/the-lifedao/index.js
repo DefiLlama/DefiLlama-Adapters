3bbe65d06cdf6ce127a6a1ade793e07084ebaf58
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-05-12 01:34:50 -0400

    refactor

--- projects/the-lifedao/index.js
+++ projects/the-lifedao/index.js
@@ -1,33 +1,9 @@
-const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
-const { getChainTransform } = require("../helper/portedTokens");
-const sdk = require("@defillama/sdk");
+const { sumTokens2 } = require("../helper/unwrapLPs");
 
 const VAULT_ADDRESS = "0x6d1bD94Cd572e51Aa018aE6Bf909BA61F8F8d4f9";
-const CHAIN = "arbitrum";
 
-async function tvl(timestamp, block, chainBlocks) {
-  const balances = {};
-  const transform = await getChainTransform(CHAIN);
-
-  // Fetch LP token balance held by the Savings Vault
-  const lpTokenCall = await sdk.api.abi.call({
-    target: VAULT_ADDRESS,
-    abi: "erc20:balanceOf",
-    params: [VAULT_ADDRESS],
-    chain: CHAIN,
-    block: chainBlocks[CHAIN],
-  });
-
-  // Unwrap the Uniswap V3 LP position into underlying USDC + USDT amounts
-  await unwrapUniswapLPs(
-    balances,
-    [{ token: VAULT_ADDRESS, balance: lpTokenCall.output }],
-    chainBlocks[CHAIN],
-    CHAIN,
-    transform
-  );
-
-  return balances;
+async function tvl(api) {
+  await sumTokens2({ api, owner: VAULT_ADDRESS, resolveUniV3: true })
 }
 
 module.exports = {
