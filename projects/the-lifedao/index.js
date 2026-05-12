804bd9d1a52f59ae77ffd4ec948c58ee9f805885
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-05-12 17:38:04 +0200

    minor fix

--- projects/the-lifedao/index.js
+++ projects/the-lifedao/index.js
@@ -1,9 +1,10 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
+const ADDRESSES = require('../helper/coreAssets.json')
 
 const VAULT_ADDRESS = "0x6d1bD94Cd572e51Aa018aE6Bf909BA61F8F8d4f9";
 
 async function tvl(api) {
-  await sumTokens2({ api, owner: VAULT_ADDRESS, resolveUniV3: true })
+  await sumTokens2({ api, owner: VAULT_ADDRESS, resolveUniV3: true, tokens: [ADDRESSES.arbitrum.USDC] })
 }
 
 module.exports = {
