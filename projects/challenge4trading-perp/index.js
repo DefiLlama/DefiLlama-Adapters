770d2d6f6f756ed0512c2dd683a84061799d29f4
dbehar753 <80523024+dbehar753@users.noreply.github.com>
2026-05-14 10:23:43 +0300

    Fix Challenge4Trading Perp (C4T) TVL adapter export

--- projects/challenge4trading-perp/index.js
+++ projects/challenge4trading-perp/index.js
@@ -5,10 +5,11 @@ const USDC_ADDRESS = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
 
 module.exports = {
   methodology: "TVL for Challenge4Trading Perp (C4T) is the onchain USDC balance held by the perp contract on Arbitrum.",
-  arbitrum: async (api) => {
-    return sumTokens2({
-      api,
-      tokensAndOwners: [[USDC_ADDRESS, PERP_ADDRESS]],
-    });
+  arbitrum: {
+    tvl: async (api) =>
+      sumTokens2({
+        api,
+        tokensAndOwners: [[USDC_ADDRESS, PERP_ADDRESS]],
+      }),
   },
 };
