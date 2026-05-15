12d87a3f5504bc1459dd639295449e191d9f9261
Daniel_Dean <danieldean.mist@gmail.com>
2026-05-14 20:33:11 -0400

    yieldz project created

---
+++ projects/yieldz/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  methodology: "Yieldz is a non-custodial DeFi utility app that specializes in one-click leverage and bundled transactions. Yieldz is only a frontend and has NO new smart contracts. Only existing deployed and audited contracts from different apps are used.",
+  arbitrum: {
+    tvl: () => ({})
+  },
+  base: {
+    tvl: () => ({})
+  },
+  bsc: {
+    tvl: () => ({})
+  },
+  ethereum: {
+    tvl: () => ({})
+  },
+  hyperliquid: {
+    tvl: () => ({})
+  },
+  katana: {
+    tvl: () => ({})
+  },
+  monad: {
+    tvl: () => ({})
+  },
+  optimism: {
+    tvl: () => ({})
+  },
+  tempo: {
+    tvl: () => ({})
+  },
+}
