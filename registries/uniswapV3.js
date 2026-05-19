2d8e1b79b176f002dae079fa4640a60058ff0bfd
rohannero <rohanzarathustra@gmail.com>
2026-05-19 01:03:37 -0400

    refactor

--- registries/uniswapV3.js
+++ registries/uniswapV3.js
@@ -1718,6 +1718,14 @@ const uniV3Configs = {
   'fluxflow-v3': {
     methodology: 'TVL counts liquidity locked in FluxFlow V3 pools on Fluent. PoolCreated events are read from the factory and reserves of each pool are summed.',
     fluent: { factory: '0x69Be606be7Fd2d27C8f9821329c748c77d24FF4f', fromBlock: 2189672}
+  },
+  'topaz-cl': {
+    bsc: { 
+      factory: '0x73DC984D9490286E735548f61dfCCec67Af82ed9',
+      fromBlock: 98756164,
+      eventAbi: 'event PoolCreated(address indexed token0, address indexed token1, int24 indexed tickSpacing, address pool)',
+      topics: ['0xab0d57f0df537bb25e80245ef7748fa62353808c54d6e528a9dd20887aed9ac2']
+    }
   }
 }
 
