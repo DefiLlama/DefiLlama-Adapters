3668821d50a79c2294f80719b83725ff511bbd75
rellyxx <rellycheng@163.com>
2026-05-19 20:22:17 +0800

    Add OpenFi adapter (Pharos, BSC)

--- projects/openfi/index.js
+++ projects/openfi/index.js
@@ -4,6 +4,9 @@ const CONFIG = {
   pharos: {
     poolDatas: ['0x3EF4724f0f2fabfA0ba96AfC711D64e6BE3367Fb'],
   },
+  bsc: {
+    poolDatas: ['0xeFD74781D9C468FFE6a4d8CA7720E8EF0223744E'],
+  },
 };
 
 module.exports = aaveV3Export(CONFIG);
