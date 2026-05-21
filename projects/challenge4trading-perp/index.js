9ea041a3460cf45c6c15e6e7eeae68c140b610d4
Felix Bruguera <felixbruguera0@gmail.com>
2026-05-21 15:50:26 -0400

    use coreAssets

--- projects/challenge4trading-perp/index.js
+++ projects/challenge4trading-perp/index.js
@@ -1,7 +1,7 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
+const ADDRESSES = require("../helper/coreAssets.json")
 
 const PERP_ADDRESS = "0x54A62D550e1754f3bB34ad80501A63815297Fccc";
-const USDC_ADDRESS = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
 
 module.exports = {
   methodology: "TVL for Challenge4Trading Perp (C4T) is the onchain USDC balance held by the perp contract on Arbitrum.",
@@ -9,7 +9,7 @@ module.exports = {
     tvl: async (api) =>
       sumTokens2({
         api,
-        tokensAndOwners: [[USDC_ADDRESS, PERP_ADDRESS]],
+        tokensAndOwners: [[ADDRESSES.arbitrum.USDC_CIRCLE, PERP_ADDRESS]],
       }),
   },
 };
