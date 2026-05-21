d30eb4fe64258291eca9773e090004ec4db2a027
Deepak <deepakrider0725@gmail.com>
2026-05-21 15:56:00 +0530

    fix kaching: query fee wallet directly, remove unused program ID lookup

--- projects/kaching/index.js
+++ projects/kaching/index.js
@@ -1,31 +1,20 @@
 const ADDRESSES = require('../helper/coreAssets.json');
 const { getConnection } = require('../helper/solana');
 const { PublicKey } = require('@solana/web3.js');
-const PROGRAM_ID = 'Dr9ci9hAYPWd5tmDQysroPp74BcMLh84uh34ZUjVrh35';
+
 const FEE_WALLET = 'Eu59ybbXJ2m5TUUqgCAZ5aVdF1U2ar1c76mcQ2xDfGEA';
 const USDC = ADDRESSES.solana.USDC;
 
-
-async function getUsdcBalance(connection, owner) {
-  const res = await connection.getParsedTokenAccountsByOwner(
-    new PublicKey(owner),
-    { mint: new PublicKey(USDC) }
-  );
-  return res.value.reduce((sum, { account }) => {
-    return sum + BigInt(account.data.parsed.info.tokenAmount.amount);
-  }, BigInt(0));
-}
-
 async function tvl(api) {
   const connection = getConnection();
-
-  // On-chain balances already cover all USDC held by the protocol
-  const [programBalance, feeWalletBalance] = await Promise.all([
-    getUsdcBalance(connection, PROGRAM_ID),
-    getUsdcBalance(connection, FEE_WALLET),
-  ]);
-
-  api.add(USDC, (programBalance + feeWalletBalance).toString());
+  const res = await connection.getParsedTokenAccountsByOwner(
+    new PublicKey(FEE_WALLET),
+    { mint: new PublicKey(USDC) }
+  );
+  const total = res.value.reduce((sum, { account }) => {
+    return sum + BigInt(account.data.parsed.info.tokenAmount.amount);
+  }, BigInt(0));
+  api.add(USDC, total.toString());
 }
 
 module.exports = {
