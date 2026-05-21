5f4e1d0f6bb65082a85386b497fc35628a2b326f
Felix Bruguera <felixbruguera0@gmail.com>
2026-05-21 11:12:03 -0400

    use morpho helper

--- projects/galaxy/index.js
+++ projects/galaxy/index.js
@@ -1,25 +1,17 @@
-const { getMorphoVaultTvl } = require("../helper/morpoho");
+const { getCuratorExport } = require("../helper/curators");
 
-const ethereumConfig = {
-  vaults: [
-    "0x71ffB6a81786eC285D429d531Cf655107B9D878d",
-    "0x91600E31fBeDc72433d4a57F16639cfe661Be7d8",
-    "0x1878805799273d10aE96a58201A6f5254CF9824F"
-  ]
-}
+const owners = ["0x42D510eDeb9257f8D920d5B9f5109D95cB22419d"]
 
-const baseConfig = {
-  vaults: [
-    "0x1e9e47583f15D45a10Df48c0b1846E0492c795D7"
-  ]
-}
-
-module.exports = {
-  methodology: "TVL is calculated by summing the assets deposited in Galaxy's Morpho vaults on each supported chain, using the ERC4626 balances of the listed vault contracts.",
-  ethereum: {
-    tvl: getMorphoVaultTvl(undefined, {vaults: ethereumConfig.vaults})
+const configs = {
+  methodology: "TVL is calculated by summing the assets deposited in all Morpho vaults curated by Galaxy on each supported chain.",
+  blockchains: {
+    ethereum: {
+      morphoVaultOwners: owners,
+    },
+    base: {
+      morphoVaultOwners: owners,
+    },
   },
-  base: {
-    tvl: getMorphoVaultTvl(undefined, {vaults: baseConfig.vaults})
-  }
 }
+
+module.exports = getCuratorExport(configs)
