3769d35a2e09e0a4c9d9e33523922e14bec06963
0xjosaphat <jo.saphat.metr@proton.me>
2026-05-22 21:03:12 +0200

    added additional chains: ethereum, base

--- projects/sectorone-dlmm/index.js
+++ projects/sectorone-dlmm/index.js
@@ -7,13 +7,13 @@ const {joeV2Export} = require('../helper/traderJoeV2')
  * @property {string} megaeth.factory - Factory contract address on MegaETH, Ethereum, Base
  */
 module.exports = joeV2Export({
-    megaeth: {
-        factory: '0x304BaEB300dD71CD76f771343E74612C2237a320',
-    },
     ethereum: {
         factory: '0x9d8688043150c2B2A4cdCE2eD03eB40b6cCd2c57',
     },
     base: {
         factory: '0x3357f02fB3aA78fc86D3Bccdc5Edf039D4b952B5',
+    },
+    megaeth: {
+        factory: '0x304BaEB300dD71CD76f771343E74612C2237a320',
     },
 })
