80dd2c2c0bd8d7c81ab7c282150e1cceb295a81e
FelixBruguera <felixbruguera0gmail.com>
2026-05-22 11:18:14 -0400

    simplify collateral fetch

--- projects/stockfi/index.js
+++ projects/stockfi/index.js
@@ -1,7 +1,4 @@
 const { getLogs2 } = require("../helper/cache/getLogs");
-const { ethers } = require("ethers");
-const { PromisePool } = require("@supercharge/promise-pool");
-const { treasuryExports } = require("../helper/treasury");
 
 const VAULT_REGISTRY = "0x9732A52eB6BAc678BbC95F6C06Ba70a5b2071379";
 const EVENT_ABI = "event VaultRegistered(address indexed vault, address indexed baseToken)";
@@ -14,23 +11,13 @@ async function tvl(api) {
         eventAbi: EVENT_ABI,
         fromBlock: 80743203,
     });
-
-    const pools = [];
-    for (const vaultData of vaults) {
-        // Collateral token is NOT the baseToken emitted by VaultRegistered
-        // Reading directly from storage        
-        const collateralToken = await api.provider.getStorage(vaultData.vault, "0x78");
-        if (!collateralToken || collateralToken === ethers.ZeroHash) {
-            return null;
-        } else {
-            const token = ethers.getAddress("0x" + collateralToken.slice(-40));
-            pools.push({ vault: vaultData.vault, collateral: token });
-        };
-    };
-
-    const validPools = pools.filter(Boolean);
+    const vaultAddresses = vaults.map(v => v.vault);
+    const collaterals = await api.multiCall({
+        abi: 'address:collateralToken',
+        calls: vaultAddresses,
+    });
     await api.sumTokens({ 
-        tokensAndOwners: validPools.map(i => [i.collateral, i.vault])
+        tokensAndOwners: vaultAddresses.map((vault, i) => [collaterals[i], vault])
     });
 };
 
