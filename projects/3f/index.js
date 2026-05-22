a11804e5a1a41bf976265d634666cc3d24ae8b0a
belbazanas <belbazanas@gmail.com>
2026-05-22 17:41:06 +0200

    feat(tvl-adapter): remove symbols from mapping
    Signed-off-by: belbazanas <belbazanas@gmail.com>

--- projects/3f/index.js
+++ projects/3f/index.js
@@ -4,18 +4,17 @@
 // Add new assets here as more leveraged vaults launch.
 
 const WRAPPED_ASSETS = [
-  { symbol: "wJAAA", address: "0x86b495e4Cb00AB18Ad94BFD7920479cC79E8eBFE" },
-  { symbol: "wUSCC", address: "0xF458Ad24B1dE7c653e8471efB0b87710b316b7D9" },
-  { symbol: "wFalconX", address: "0x4614F7A56A3Eb83b2Ff9fA4B4b9575B28Fb68644" },
+  "0x86b495e4Cb00AB18Ad94BFD7920479cC79E8eBFE", // wJAAA
+  "0xF458Ad24B1dE7c653e8471efB0b87710b316b7D9", // wUSCC
+  "0x4614F7A56A3Eb83b2Ff9fA4B4b9575B28Fb68644", // wFalconX
 ];
 
 async function tvl(api) {
-  const addresses = WRAPPED_ASSETS.map((a) => a.address);
   const supplies = await api.multiCall({
     abi: "erc20:totalSupply",
-    calls: addresses,
+    calls: WRAPPED_ASSETS,
   });
-  api.add(addresses, supplies);
+  api.add(WRAPPED_ASSETS, supplies);
 }
 
 module.exports = {
