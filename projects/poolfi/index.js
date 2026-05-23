d64419059c31245a098b54880ea3250163c6f835
fiscofacilsuporte-ctrl <fiscofacilsuporte@gmail.com>
2026-05-23 15:03:56 +0100

    Add Poolfi index.js for DeFi APY tracking

---
+++ projects/poolfi/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  methodology: "Poolfi is a real-time DeFi APY tracker and aggregator. It does not have any smart contracts with locked TVL. It provides informational data for users to compare yields across various protocols.",
+  miscellaneous: {
+    tvl: () => ({}),
+  },
+};
