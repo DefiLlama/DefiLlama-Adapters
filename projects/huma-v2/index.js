5e92cb4d9f0db5490a795ec0d58b186ea49e37e8
rohannero <rohanzarathustra@gmail.com>
2026-05-26 00:07:21 -0400

    use pst supply

--- projects/huma-v2/index.js
+++ projects/huma-v2/index.js
@@ -1,7 +1,14 @@
-const { sumTokensExport } = require('../helper/solana');
+const { getTokenSupplies } = require('../helper/solana');
+
+const PST = '59obFNBzyTBGowrkif5uK7ojS58vsuWz3ZCvg6tfZAGw';
+
+async function tvl(api) {
+    const supplies = await getTokenSupplies([PST]);
+    api.add(PST, supplies[PST]);
+}
 
 module.exports = {
-    methodology: "Tracks all tokens inside Huma Finance pool authority token accounts",
     doublecounted: true,
-    solana: { tvl: sumTokensExport({ owner: '9936VFvgRmW1STvdgeyPQaKHDx5DwBtbhZkT3HcdL3QK', blacklistedTokens: ['59obFNBzyTBGowrkif5uK7ojS58vsuWz3ZCvg6tfZAGw']}) }
+    methodology: "TVL counts the total supply of PST, which is backed by externally deployed USDC deposits.",
+    solana: { tvl }
 }
