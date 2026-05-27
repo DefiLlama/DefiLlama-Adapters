2f999a659141da455508bdd2bf4c9539e274ec87
rohannero <rohanzarathustra@gmail.com>
2026-05-27 12:43:04 -0400

    wip: fix

--- projects/treasury/ens.js
+++ projects/treasury/ens.js
@@ -1,5 +1,5 @@
-const { karpatKeyTvl } = require('../helper/karpatkey');
-const { sumTokensExport } = require('../helper/unknownTokens');
+const ADDRESSES = require('../helper/coreAssets.json')
+const { treasuryExports } = require("../helper/treasury");
 
 const treasury = "0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7";
 const vestingAddress = "0xd7a029db2585553978190db5e85ec724aa4df23f"
@@ -7,12 +7,15 @@ const treasury2 = "0x690f0581ececcf8389c223170778cd9d029606f2"
 
 const ENS= "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72";
 
-module.exports={
-  ethereum:{
-    tvl: async (api)=>karpatKeyTvl(api, "ENS DAO", "ENS"),
-    ownTokens: sumTokensExport({
-      tokens: [ENS],
-      owners: [treasury, vestingAddress, treasury2],
-    })
-  }
-}
+module.exports = treasuryExports({
+  ethereum: {
+    tokens: [ 
+        ADDRESSES.null,
+        ADDRESSES.ethereum.USDC,
+        ADDRESSES.ethereum.sUSDS,
+        ADDRESSES.ethereum.LINK
+     ],
+     ownTokens: [ENS],
+    owners: [treasury, vestingAddress, treasury2],
+  },
+})
