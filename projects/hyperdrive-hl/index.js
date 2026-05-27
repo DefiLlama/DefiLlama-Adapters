c7622eff1a17f479f25915eb8477c6bb21f89333
Eden <noat.eth@gmail.com>
2026-05-28 04:13:11 +0700

    split lending and vault products

--- projects/hyperdrive-hl/index.js
+++ projects/hyperdrive-hl/index.js
@@ -18,23 +18,6 @@ const ABI = {
     },
 };
 
-const VAULTS = [
-    "0x9271A5C684330B2a6775e96B3C140FC1dC3C89be",
-    "0xaEAAD6d9B096829E5F3804a747C9FDD6677d78f0",
-    "0x72EE42bd660e4f676106C3718b00af06257c9d35",
-    "0x7f2b789Ac6D93521FAe86Cbc838eFcfc4F2b004B",
-    "0x5743AeC1f06E896544D1638E0FEBd15098855CB5",
-    "0x4d0fF6a0DD9f7316b674Fb37993A3Ce28BEA340e",
-];
-
-async function lookupAddresses(api, keys) {
-    return await api.call({
-        abi: ABI.AddressRegistry.getAddresses,
-        target: ADDRESS_REGISTRY,
-        params: [keys.map((key) => ethers.keccak256(ethers.toUtf8Bytes(key)))],
-    });
-}
-
 async function getAllMarkets(api) {
     const keys = [];
     for (let i = 0; i < 100; i++) {
@@ -90,9 +73,7 @@ async function tvl(api) {
         // api.add(datum.marketAsset, totalCollateral);
     });
   
-    await sumTokens2({ api, tokensAndOwners })
-
-    return api.erc4626Sum({ calls: VAULTS, permitFailure: true });
+    return await sumTokens2({ api, tokensAndOwners })
 }
 
 async function borrowed(api) {
