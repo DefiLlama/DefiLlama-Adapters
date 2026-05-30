99679025585acb59cb5ea405860707811ad30544
Soumya Mukhija <soumya@pred.app>
2026-05-30 23:22:29 +0530

    fix: update contracts

--- projects/pred/index.js
+++ projects/pred/index.js
@@ -1,8 +1,8 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const PROTOCOL_CONTRACTS = [
-  "0x4e9608d5cf77f22b2d5543b6c65a8c5417e8122c", // wrappedCollateral
-  "0xc83C5Cca746D213D9Cf63FE668E7EB8dEe35314B", // conditionalTokens
+  "0xC7E015B63d8226444A028c57fcaa1d30bfC3178C", // wrappedCollateral
+  "0xb365835F194E383354367572D0eB9d2Dce46b693", // conditionalTokens
 ];
 
 async function tvl(api) {
