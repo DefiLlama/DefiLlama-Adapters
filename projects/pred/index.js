59bf649af62829c760dd03231edbb677b12b812e
Felix Bruguera <felixbruguera0@gmail.com>
2026-05-30 16:23:38 -0400

    keep olders contracts for refills

--- projects/pred/index.js
+++ projects/pred/index.js
@@ -1,8 +1,12 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
 const PROTOCOL_CONTRACTS = [
-  "0xC7E015B63d8226444A028c57fcaa1d30bfC3178C", // wrappedCollateral
-  "0xb365835F194E383354367572D0eB9d2Dce46b693", // conditionalTokens
+  // wrappedCollateral
+  "0x4e9608d5cf77f22b2d5543b6c65a8c5417e8122c",
+  "0xC7E015B63d8226444A028c57fcaa1d30bfC3178C",
+  // conditionalTokens
+  "0xc83C5Cca746D213D9Cf63FE668E7EB8dEe35314B",
+  "0xb365835F194E383354367572D0eB9d2Dce46b693", 
 ];
 
 async function tvl(api) {
