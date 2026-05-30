798e67a47e19682902be56c20c41ea23bd6c5c6f
Felix Bruguera <felixbruguera0@gmail.com>
2026-05-30 17:36:56 -0400

    fix

--- projects/cassa/index.js
+++ projects/cassa/index.js
@@ -4,9 +4,8 @@ const configs = {
   methodology: 'Count all assets are deposited in all vaults curated by Cassa.',
   blockchains: {
     ethereum: {
-      eulerVaultOwners: [
-        '0x34a0d3333FF6028b5Fad62879A9d24d3C071E343',
-        '0xeeF44faf5fE0cc1FAB04Bb052Bff750Cf37A7fC4',
+      euler: [
+        '0xBc79C4DA0452152D2C329ADE328C79705a964CEE'
       ],
     },
   },
