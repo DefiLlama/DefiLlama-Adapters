8c3228fb6278d2b2f1e0e754ca03b6703b79396c
eyusufatik <esadyusufatik@gmail.com>
2026-06-02 16:00:48 +0300

    feat: add Citrea Staking

---
+++ projects/citrea-staking/index.js
@@ -0,0 +1,11 @@
+const { sumTokensExport } = require("../helper/unwrapLPs");
+const ADDRESSES = require("../helper/coreAssets.json");
+
+const XCTR_CONTRACT = "0x2015F35030A8Ff2C0CA161a865414996F8E80AA4";
+
+module.exports = {
+  methodology: "Sum of CTR locked by users in the xCTR (Staked CTR) staking contract on Citrea, which grants governance voting power.",
+  citrea: {
+    tvl: sumTokensExport({ tokensAndOwners: [[ADDRESSES.citrea.CTR, XCTR_CONTRACT]] }),
+  },
+};
