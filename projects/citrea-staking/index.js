6fbabb5b5d58494e3e47fc544d828263c2fde51f
eyusufatik <esadyusufatik@gmail.com>
2026-06-02 16:32:57 +0300

    fix nitpick from coderabbit

--- projects/citrea-staking/index.js
+++ projects/citrea-staking/index.js
@@ -1,4 +1,4 @@
-const { sumTokensExport } = require("../helper/unwrapLPs");
+const { sumTokensExport } = require("../helper/sumTokens");
 const ADDRESSES = require("../helper/coreAssets.json");
 
 const XCTR_CONTRACT = "0x2015F35030A8Ff2C0CA161a865414996F8E80AA4";
