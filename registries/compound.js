137616222d765dd5c9911ea2d7448e0c1ce0a3bf
rohannero <rohanzarathustra@gmail.com>
2026-06-03 16:43:11 -0400

    minor fixes

--- registries/compound.js
+++ registries/compound.js
@@ -457,7 +457,7 @@ const configs = {
       cether: '0xec0d3f28d37a3393cf09ee3ad446c485b6afdaa3',
       staking: [['0x4f2ED52bC4CbdE54e2b3547D3758474A21598D7c', '0x024510151204DeC56Cc4D54ed064f62efAC264d5', '0x2EacD2D7cF5Cba9dA031C0a9C5d7FDeDc056216C', '0x8c9886Aca8B6984c10F988078C5e1D91976dFD16', '0x63D21dBD5A30940C605d77882D065736e8fffC94'], '0x4576E6825B462b6916D2a41E187626E9090A92c6'],
     },
-    klaytn: { comptroller: '0x35dc04eE1D6E600C0d13B21FdfB5C83D022CEF25', cether: '0xacc72a0ca4e85f79876ed4c5e6ea29be1cd26c2e' },
+    klaytn: { comptroller: '0x35dc04eE1D6E600C0d13B21FdfB5C83D022CEF25', cether: '0xacc72a0ca4e85f79876ed4c5e6ea29be1cd26c2e', isInsolvent: true },
   },
   'onyx': {
     ethereum: {
