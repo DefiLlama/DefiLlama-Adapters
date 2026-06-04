ab48c368c98eb40cc7f106611f3ae6174f895d21
Ioannis Kokkinis <info@upg.gr>
2026-06-04 11:33:32 +0300

    chore(ada-harvest): vault address update (security redeploy 4b11840d)

--- projects/ada-harvest/index.js
+++ projects/ada-harvest/index.js
@@ -2,10 +2,10 @@ const { sumTokensExport } = require('../helper/chain/cardano');
 
 // ADA Harvest - Cardano PlutusV3 flash loan vault + yield aggregator
 // Vault address (base address, delegates to a stake pool):
-//   addr1zx0es78sfa3cl47pmu9tcwqc7mx9wacu270u76yrk4duk2ggucktr6zta3rtuqlqam4395sry3vl5c7ss08m50qtnj6s4zlejx
+//   addr1z993rpqdxcgnncqxjyy37kjy27f94gyvwcatg7x86wk8xjcgucktr6zta3rtuqlqam4395sry3vl5c7ss08m50qtnj6slx9svv
 // Docs: https://ada-harvest.com
 
-const VAULT_ADDRESS = 'addr1zx0es78sfa3cl47pmu9tcwqc7mx9wacu270u76yrk4duk2ggucktr6zta3rtuqlqam4395sry3vl5c7ss08m50qtnj6s4zlejx';
+const VAULT_ADDRESS = 'addr1z993rpqdxcgnncqxjyy37kjy27f94gyvwcatg7x86wk8xjcgucktr6zta3rtuqlqam4395sry3vl5c7ss08m50qtnj6slx9svv';
 
 module.exports = {
   // Vault funds may later be deployed into Minswap/Liqwid, which have their own
