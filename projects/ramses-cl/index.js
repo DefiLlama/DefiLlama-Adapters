a2b8d5d54fd35253ebb0aa644a55df9891333670
Ivan Widjaja <60992624+imarshallwidjaja@users.noreply.github.com>
2026-06-06 10:36:39 +1000

    use shared tvl summing for ramses

--- projects/ramses-cl/index.js
+++ projects/ramses-cl/index.js
@@ -1,20 +1,5 @@
-const BigNumber = require('bignumber.js')
+const sdk = require('@defillama/sdk')
 const { uniV3Export } = require('../helper/uniswapV3')
-
-function mergeBalances(target, source = {}) {
-  Object.entries(source).forEach(([token, amount]) => {
-    target[token] = new BigNumber(target[token] || 0).plus(amount).toFixed(0)
-  })
-  return target
-}
-
-function sumTvls(...tvls) {
-  return async (api) => {
-    const balances = {}
-    for (const tvl of tvls) mergeBalances(balances, await tvl(api))
-    return balances
-  }
-}
 
 const arbitrumLegacyCl = uniV3Export({
   arbitrum: { factory: '0xaa2cd7477c451e703f3b9ba5663334914763edf8', fromBlock: 90593047 },
@@ -26,7 +11,7 @@ const arbitrumRamsesXCl = uniV3Export({
 
 module.exports = {
   arbitrum: {
-    tvl: sumTvls(arbitrumLegacyCl, arbitrumRamsesXCl),
+    tvl: sdk.util.sumChainTvls([arbitrumLegacyCl, arbitrumRamsesXCl]),
   },
   ...uniV3Export({
     hyperliquid: { factory: '0x07E60782535752be279929e2DFfDd136Db2e6b45', fromBlock: 18149975 },
