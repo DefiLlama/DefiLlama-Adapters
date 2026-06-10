d2c6eb53131e1332e672f7ea21c205a63afedf12
BexChain <bexchain.team@gmail.com>
2026-06-10 21:12:40 +0700

    Update index.js

--- projects/bexchain/index.js
+++ projects/bexchain/index.js
@@ -1,9 +1,3 @@
 const { uniTvlExport } = require('../helper/unknownTokens');
 
-module.exports = {
-  misrepresentedTokens: true,
-  timetravel: false,
-  bexchain: {
-    tvl: uniTvlExport('bexchain', '0xe6ade1cf5b60d9f135e1d8c003b1e4bf9a897fd2'),
-  }
-};
+module.exports = uniTvlExport('bexchain', '0xe6ade1cf5b60d9f135e1d8c003b1e4bf9a897fd2');
