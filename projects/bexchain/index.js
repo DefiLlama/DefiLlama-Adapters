d6cbf2410bbff29193c1e66ed4c9975bf22c07c1
BexChain <bexchain.team@gmail.com>
2026-06-10 20:15:40 +0700

    Update index.js

--- projects/bexchain/index.js
+++ projects/bexchain/index.js
@@ -2,9 +2,9 @@ const { uniTvlExport } = require('../helper/unknownTokens');
 
 module.exports = {
   misrepresentedTokens: true,
-  timetravel: false, // Menghindari eror sinkronisasi waktu blokir baru
+  timetravel: false,
   bexchain: {
     tvl: uniTvlExport('0xe6ade1cf5b60d9f135e1d8c003b1e4bf9a897fd2', 'bexchain'),
   },
-  tvl: uniTvlExport('0xe6ade1cf5b60d9f135e1d8c003b1e4bf9a897fd2', 'bexchain'), // Perbaikan utama untuk mendefinisikan total TVL global
+  tvl: uniTvlExport('0xe6ade1cf5b60d9f135e1d8c003b1e4bf9a897fd2', 'bexchain'),
 };
