0e2b6c65149564a8e027c840b06f9ee11422d71f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-06-10 10:27:03 +0200

    update apechurch

--- projects/ape-church/index.js
+++ projects/ape-church/index.js
@@ -1,16 +1,13 @@
-const { sumTokens2 } = require('../helper/unwrapLPs');
+const { sumTokensExport } = require('../helper/unwrapLPs');
 const { nullAddress } = require('../helper/tokenMapping');
 
 const HOUSE_CONTRACT = '0x2054709F89F18a4CCAC6132acE7b812E32608469';
-
-async function tvl(api) {
-    return sumTokens2({ owner: HOUSE_CONTRACT, tokens: [nullAddress], api, })
-}
+const HOUSE_CONTRACT_V2 = '0xB7EcD1F3fA462d2c6c65F55357E8c16c614CC2f1';
 
 module.exports = {
   methodology: `Ape Church TVL is achieved by tracking the balance of The House smart contract. Volume is achieved by tracking the Transfer events from the UserInfoTracker smart contract.`,
   start: 1757586225,
   apechain: {
-    tvl
+    tvl: sumTokensExport({ owners: [HOUSE_CONTRACT, HOUSE_CONTRACT_V2], tokens: [nullAddress], }),
   }
 }
