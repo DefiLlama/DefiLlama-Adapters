5ddb53bdb1c3880431635e5e89a0dde208a9190e
rohannero <rohanzarathustra@gmail.com>
2026-06-10 16:34:27 -0400

    fix

--- projects/metric/index.js
+++ projects/metric/index.js
@@ -1,5 +1,4 @@
 const { uniV3Export } = require('../helper/uniswapV3');
-const { getConfig } = require('../helper/cache')
 
 const factory = '0xe22F9fc0f04486dE25ed6CF1800a4a47aFD82e0C';
 const eventAbi = 'event PoolCreated(address indexed token0, address indexed token1, address indexed manager, address pool, bytes32 poolKey)';
@@ -13,17 +12,7 @@ const fromBlocks = {
   polygon: 83380134,
   megaeth: 9083666,
   hyperliquid: 30774348,
-  // monad: 64807339,
-}
-
-// getLogs2 is failing on monad
-async function tvl(api) {
-    // const API_BASE = 'https://api.metric.xyz';
-    const API_BASE = 'http://54.199.103.16:8080';
-    const pools = await getConfig(`metric.xyz-pools-${api.chain}`, `${API_BASE}/${api.chain}/metadata`);
-    const ownerTokens = pools.map((p) => [[p.token0, p.token1], p.poolAddress])
-
-    await api.sumTokens({ownerTokens})
+  monad: 64807339,
 }
 
 module.exports = uniV3Export(
@@ -31,5 +20,3 @@ module.exports = uniV3Export(
     Object.entries(fromBlocks).map(([chain, fromBlock]) => [chain, { factory, eventAbi, fromBlock }])
   )
 )
-
-module.exports.monad = {tvl}
