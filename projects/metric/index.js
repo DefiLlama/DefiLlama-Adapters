a00dd1b95d1f6fd5657a7019ab03198f59b4a4d8
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-06-11 11:20:05 +0200

    fix metric

--- projects/metric/index.js
+++ projects/metric/index.js
@@ -1,22 +1,23 @@
-const { uniV3Export } = require('../helper/uniswapV3');
+const { getConfig } = require('../helper/cache')
 
-const factory = '0xe22F9fc0f04486dE25ed6CF1800a4a47aFD82e0C';
-const eventAbi = 'event PoolCreated(address indexed token0, address indexed token1, address indexed manager, address pool, bytes32 poolKey)';
+// const API_BASE = "https://api.metric.xyz";
+const API_BASE = "http://54.199.103.16:8080";
+const chainConfig = {
+  ethereum: "ethereum",
+  base: 'base',
+  arbitrum: "arbitrum",
+  bsc: "bsc",
+  avax: "avax",
+  polygon: "polygon",
+  megaeth: "megaeth",
+  hyperliquid: "hyperevm",
+  monad: "monad",
+};
 
-const fromBlocks = {
-  ethereum: 24521185,
-  base: 42540736,
-  arbitrum: 435210755,
-  bsc: 82964761,
-  avax: 78822864,
-  polygon: 83380134,
-  megaeth: 9083666,
-  hyperliquid: 30774348,
-  monad: 64807339,
+Object.keys(chainConfig).forEach((key) => module.exports[key] = { tvl })
+
+async function tvl(api) {
+  const pools = await getConfig(`metric-pools/${api.chain}`, `${API_BASE}/${chainConfig[api.chain]}/metadata`)
+  const ownerTokens = pools.map(i => [[i.token0, i.token1], i.poolAddress])
+  return api.sumTokens({ ownerTokens })
 }
-
-module.exports = uniV3Export(
-  Object.fromEntries(
-    Object.entries(fromBlocks).map(([chain, fromBlock]) => [chain, { factory, eventAbi, fromBlock }])
-  )
-)
