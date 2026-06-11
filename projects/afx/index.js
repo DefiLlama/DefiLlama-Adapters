f221de62970696c8568bceb36d99a5bd732e84a9
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-06-11 01:03:53 -0400

    refactor

--- projects/afx/index.js
+++ projects/afx/index.js
@@ -1,21 +1,7 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-
-const AFX_BRIDGE = '0xCb3B9A3E5668AFE84DC7A864B36b845dCE062e67'
-
-async function tvl(api) {
-  const usdc = ADDRESSES.arbitrum.USDC_CIRCLE
-  const balance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: usdc,
-    params: [AFX_BRIDGE],
-  })
-
-  api.add(usdc, balance)
-}
+const { sumTokensExport } = require('../helper/unwrapLPs')
 
 module.exports = {
   methodology: 'Counts USDC deposited through Arbitrum and locked in the AFX bridge contract.',
-  arbitrum: {
-    tvl,
-  },
+  arbitrum: { tvl: sumTokensExport({owner: '0xCb3B9A3E5668AFE84DC7A864B36b845dCE062e67', tokens: [ADDRESSES.arbitrum.USDC_CIRCLE]}) },
 }
