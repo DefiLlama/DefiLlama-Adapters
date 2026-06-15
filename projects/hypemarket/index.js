655e4762cf62591e1438a7e1f1972067416f22d2
rohannero <rohanzarathustra@gmail.com>
2026-06-15 13:36:42 -0400

    use getConfig

--- projects/hypemarket/index.js
+++ projects/hypemarket/index.js
@@ -1,6 +1,6 @@
 const sdk = require('@defillama/sdk')
-const { get } = require('../helper/http')
 const { getResourceData } = require('../helper/chain/supra')
+const { getConfig } = require('../helper/cache')
 
 const CONTRACT = '0xbe08b388cc1a21ba8d8a514c1857369f4908d4b478bb8d66a369bfb9ca5a2da8'
 const MARKETS_URL = 'https://api.hypemarket.trade/api/markets'
@@ -9,7 +9,7 @@ async function fetchAllMarketAddresses() {
   const addresses = []
   let page = 1
   while (true) {
-    const res = await get(`${MARKETS_URL}?limit=100&page=${page}`)
+    const res = await getConfig('hypemarket', `${MARKETS_URL}?limit=100&page=${page}`)
     const items = res?.data?.items || []
     addresses.push(...items.map(i => i.market))
     if (items.length === 0 || addresses.length >= (res?.data?.total || 0) || page > 100) break
