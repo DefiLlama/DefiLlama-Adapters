be02ed3f28194ffeb1d8d30c557e6db5ef458bad
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-06-15 15:06:18 -0400

    Delete projects/hyperdrome/index.js

--- projects/hyperdrome/index.js
+++
@@ -1,3 +0,0 @@
-const { uniTvlExport } = require('../helper/unknownTokens.js')
-
-module.exports = uniTvlExport('hyperliquid', '0x2fd54310cE91919c2F328e7C9028A854De40673E', { hasStablePools: true })
