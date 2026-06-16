5123a493befc14b163b4d8bc89cb0a0c0245a604
Rightruth <rightruth1202@gmail.com>
2026-06-16 13:18:43 +0900

    Add Kayen Finance v3 (Chiliz)

---
+++ projects/kayen-finance-v3/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  chz: { factory: '0x4bC8BDF50843Cc503E196Be4A9560134d358bb3C', fromBlock: 32422500, permitFailure: true },
+})
