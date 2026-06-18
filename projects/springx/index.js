d61d475dc70dc2f5d450907cd1ded2c75cb9e70c
SpringX <TheofficialSpringX@protonmail.com>
2026-06-18 13:36:56 +0800

    add Springx TVL adapter for monad (#19709)
    Co-authored-by: springx <>

--- projects/springx/index.js
+++ projects/springx/index.js
@@ -2,13 +2,15 @@ const { sumTokens2 } = require("../helper/unwrapLPs")
 
 const FARM_ADDRESS = "0xafF15Ca201C08F05f65d4d0A9d9C368C8356f796"
 async function tvl(api){
-    let pools = await api.call({abi:ABI.getPoolTotalTvl,target:FARM_ADDRESS,})
+    const pools = await api.call({abi:ABI.getPoolTotalTvl,target:FARM_ADDRESS,})
     pools.forEach(({ assets, tvl }) => { api.add(assets, tvl) })
     return sumTokens2({ api, resolveLP: true })
 }
 
+
 module.exports = {
-    plasma: { tvl }
+    plasma: { tvl},
+    monad:{tvl}
   };
 
 const ABI = {
