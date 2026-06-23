e860cbd68125cb09915f1b8c7d3d76cd5e254df1
rohannero <rohanzarathustra@gmail.com>
2026-06-22 23:43:31 -0400

    fix

--- .github/workflows/getFileList.js
+++ .github/workflows/getFileList.js
@@ -8,7 +8,7 @@ const fileSet = new Set();
   const [root, dir] = file.split('/')
   if (dir === 'treasury' || dir === 'entities') fileSet.add(file)
   else if (root === 'projects' && dir !=='helper' && dir !== 'config') fileSet.add(root + '/' + dir)
-  else if (root === 'registries' && file.endsWith('.js') && dir !== 'index.js' && dir !== 'utils.js') {
+  else if (root === 'registries' && /^registries\/[a-zA-Z0-9_-]+\.js$/.test(file) && dir !== 'index.js' && dir !== 'utils.js') {
     let protocolNames = new Set()
     try { protocolNames = new Set(Object.keys(require('../../' + file))) } catch (e) {}
     const diffBase = process.env.DIFF_BASE || 'HEAD~1'
