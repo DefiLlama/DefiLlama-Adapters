59602cdf97a8fcd91e79641f40f0fe4d63f661af
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-06-24 11:04:42 +0200

    fix broken

--- projects/helper/deadChains.js
+++ projects/helper/deadChains.js
@@ -29,4 +29,6 @@ module.exports =  [
   'ham', // block explorer and rpcs are dead: https://explorer.ham.fun/
   'onus', // block explorer and rpcs are dead (rug?): https://explorer.onuschain.io
   'planq', // block explorer and rpcs are dead: https://evm.planq.network & https://explorer.planq.network
+  'functionx', // sunset, migrating to Ethereum: https://medium.com/pundiai/pundi-aifx-omnilayer-sunsetting-migration-to-ethereum-begins-584f6ea70b27
+  'genesys', // shutdown 2026-03-01: https://x.com/GenesysChain/status/2024935948228985085
 ]
