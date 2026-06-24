e6bbdcc7570284b577687725e184657d33170419
rohannero <rohanzarathustra@gmail.com>
2026-06-23 23:38:51 -0400

    track tori vault

--- projects/tori-finance/index.js
+++ projects/tori-finance/index.js
@@ -1,15 +1,7 @@
-const ADDRESSES = require('../helper/coreAssets.json');
-
-const minter = '0xe16b821B9F6B0a6f06a2C4a61D5d1f71cFa53F1E';
-const getCustodiansAbi = 'address[]:getCustodianAddresses';
-
-async function tvl(api) {
-    const owners = await api.call({abi: getCustodiansAbi, target: minter})
-
-    await api.sumTokens({owners, tokens: [ADDRESSES.ethereum.USDC, ADDRESSES.ethereum.USDT]})
-}
+const { sumERC4626VaultsExport } = require('../helper/erc4626');
 
 module.exports = {
-    methodology: 'Counts all tokens backing trUSD.',
-    ethereum: { tvl }
+    methodology: 'Counts total value of assets staked into the Upshift Tori ecosystem vault.',
+    doublecounted: true,
+    ethereum: { tvl: sumERC4626VaultsExport({vaults: ['0xcd69123b3FBBfC666E1f6a501da27B564C00De54'], tokenAbi: 'address:asset', balanceAbi: 'uint:getTotalAssets'}) }
 }
