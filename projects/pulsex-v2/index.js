c5a50a91a970dded54c5dd5eb63a5583d70333c1
DaniiRix <danielrix01@protonmail.com>
2026-06-25 12:39:21 +0530

    fix: pulsex-v2 adapter to use onchain data

--- projects/pulsex-v2/index.js
+++ projects/pulsex-v2/index.js
@@ -1,19 +1,14 @@
-const { graphQuery } = require('../helper/http')
+const { getUniTVL } = require('../helper/unknownTokens');
 
-const SUBGRAPH = 'https://graph.pulsechain.com/subgraphs/name/pulsechain/pulsexv2'
-
-async function tvl(api) {
-  const { pulseXFactories } = await graphQuery(SUBGRAPH, `{
-    pulseXFactories {
-      totalLiquidityUSD
-    }
-  }`)
-  api.addUSDValue(+pulseXFactories[0].totalLiquidityUSD)
-}
+// PulseX V2 is a standard Uniswap V2 fork on PulseChain.
+// Factory exposes allPairsLength()/allPairs(uint) and pairs expose getReserves()/token0()/token1().
+// https://otter.pulsechain.com/address/0x29eA7545DEf87022BAdc76323F373EA1e707C523
+const factory = '0x29eA7545DEf87022BAdc76323F373EA1e707C523';
 
 module.exports = {
-  misrepresentedTokens: true,
+  methodology: 'Enumerates all PulseX V2 pairs on-chain via the factory and sums the reserves of known (core) assets held in the liquidity pools.',
+  isHeavyProtocol: true,
   pulse: {
-    tvl,
+    tvl: getUniTVL({ factory, useDefaultCoreAssets: true }),
   },
 };
