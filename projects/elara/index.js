2f452352621f23c15dd8bf1f44395bd3bda1ed3e
Vlad <vlad@truefi.io>
2026-07-09 16:59:55 +0200

    fix: use elusd supply for elara tvl

--- projects/elara/index.js
+++ projects/elara/index.js
@@ -1,20 +1,18 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
-const TREASURY_WALLETS = [
-  '0xFa92ED1ab198759045B5a9a35c3Ac7b63252A047', // depositWallet
-  '0x52D0238F31D30334b88c227F72EbDb9d02E094c0', // withdrawWallet
-]
+// elUSD token proxy: https://etherscan.io/address/0x65Fb0f9b196d524De0C4F3BAF572F0a79eb21194
+const ELUSD = '0x65Fb0f9b196d524De0C4F3BAF572F0a79eb21194'
 
-const COLLATERAL_TOKENS = [
-  ADDRESSES.ethereum.USDC,
-  ADDRESSES.ethereum.USDT,
-  ADDRESSES.ethereum.USDe,
-]
+const tvl = async (api) => {
+  const supply = await api.call({ target: ELUSD, abi: 'erc20:totalSupply' })
+  api.add(ADDRESSES.ethereum.USDC, supply / 1e12)
+}
 
 module.exports = {
   start: '2026-06-16',
-  methodology: 'TVL counts supported collateral assets (USDC, USDT, and USDe) held in Elara treasury deposit and withdraw wallets on Ethereum.',
+  methodology: 'TVL uses elUSD total supply as a proxy for protocol assets backing issued elUSD, mapped 1:1 to USDC for pricing.',
   ethereum: {
-    tvl: (api) => api.sumTokens({ owners: TREASURY_WALLETS, tokens: COLLATERAL_TOKENS }),
+    misrepresentedTokens: true,
+    tvl,
   },
 }
