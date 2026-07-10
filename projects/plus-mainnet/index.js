eb18a149639e0aedab086c01d10f27c84175bb24
waassa621-png <waassa621@gmail.com>
2026-07-10 15:30:44 +0900

    Update fetch function to use toUSDTBalances
    Modify fetch function to return TVL in USDT format.

--- projects/plus-mainnet/index.js
+++ projects/plus-mainnet/index.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
+const { toUSDTBalances } = require('../helper/balances');
 async function fetch() {
   const response = await axios.get("https://plusmain.net/api/defillama/tvl");
-  return response.data.tvl;
+  // 단순 숫자가 아닌 USDT(달러) 단위임을 명시하여 리턴합니다.
+  return toUSDTBalances(response.data.tvl);
 }
 module.exports = {
   timetravel: false,
