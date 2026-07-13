b5f2b0770787d300d691a69c77b67b1502b0adcb
rohannero <rohanzarathustra@gmail.com>
2026-07-13 18:13:33 -0400

    add wikicious

--- registries/sumTokens.js
+++ registries/sumTokens.js
@@ -28852,6 +28852,44 @@ const configs = {
       ]
     },
   },
+  "wikicious": {
+    "arbitrum": {
+      "tvl": {
+        "owners": [
+          "0x4533E181FdF5b0C66e0816992F38c23d57e42Df8",  
+          "0x723f653a3DEFC45FB934BBF81f1411883a977468", 
+          "0x9C63c27B8A73A990a2D89141622A639a2363b88A",
+          "0x5e73fa11c2Fa157dbE59E7B8F7f1b3101c5c6004",
+          "0x95F3Cf765b479478c44D0EE932f17444ADA6A9a1",
+          "0xf2cD47C16CCA38aC77e6ab344E04e7E97C400748",
+          "0x8897A8Ae133b0DD71ef6E28B1A8efB42f1Ef78d4",
+          "0x53b6A9bE66C68090c26d4BE74f6eB916578F3A0B",
+          "0x74635CFa33EEAe220367fF10C598e098a29e9246",
+          "0x9e09dF7E84aBf818882a259Ef897a55f25CE1163",
+          "0xE019e13abdd7160f8467D55E3e190022295dEdc1",
+          "0x42DB4776FFB45f2cc5663407e7953935f63fd40E",
+          "0x08FC8f870Df09A7265D1D06a7A95C41cEf98d9E6", 
+        ],
+        "tokens": [
+          ADDRESSES.arbitrum.USDC_CIRCLE,
+          ADDRESSES.arbitrum.WETH,
+          ADDRESSES.arbitrum.WBTC,
+          ADDRESSES.arbitrum.ARB,
+          ADDRESSES.arbitrum.USDT,
+          ADDRESSES.arbitrum.WSTETH,
+          '0xEC70Dcb4A1EFa46b8F2D97C310C9c4790ba5ffA8', // rETH
+        ]
+      },
+      "staking": {
+        "owners": [
+          "0xDD551D705fAbD4380D2C95F7345b671cE3310bd2",
+          "0x6ac54F360315E0B3Dae455ad371A06d154b410B2",
+          "0x016886FF6fdab890Dd03aE7a1D6535ef57f06F92",
+        ],
+        "token": "0xa681Bf6f0449ABc4E98DCa3468488Fe1b24FdD0F"
+      }
+    }
+  },
   "windfall": {
     "methodology": "Counts the number of tokens in the Windfall contract.",
     "canto": {
