ab21994b24843e033d748293183ca0e9895560d5
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-07-14 01:03:10 -0400

    minor refactor

--- projects/siloam/index.js
+++ projects/siloam/index.js
@@ -1,4 +1,4 @@
-const { getLogs } = require('../helper/cache/getLogs')
+const { getLogs2 } = require('../helper/cache/getLogs')
 const { sumTokens2 } = require('../helper/unwrapLPs')
 
 const FACTORY = '0x6e1Bb585ca754db1202Cf555f1dE6A12fe5aCeec'
@@ -11,16 +11,15 @@ module.exports = {
     'is a sovereign contract denominated in exactly one ERC-20.',
   base: {
     tvl: async (api) => {
-      const logs = await getLogs({
+      const logs = await getLogs2({
         api,
         target: FACTORY,
         eventAbi:
           'event PoolCreated(address indexed pool, address indexed creator, address indexed depositToken, address yieldAdapter, bytes32 salt, bool isWhitelisted)',
-        onlyArgs: true,
         fromBlock: DEPLOY_BLOCK,
       })
-      const ownerTokens = logs.map((l) => [[l.depositToken], l.pool])
-      return sumTokens2({ api, ownerTokens })
+      const tokensAndOwners = logs.map((l) => [l.depositToken, l.pool])
+      await sumTokens2({ api, tokensAndOwners })
     },
   },
 }
