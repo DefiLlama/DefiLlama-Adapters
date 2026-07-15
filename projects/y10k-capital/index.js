06850a0384d7209aafca7fb812db942e7ba16eed
Simão Pinto <87367287+Sk1mer12@users.noreply.github.com>
2026-07-15 11:57:36 +0000

    Add support to sei asset

--- projects/y10k-capital/index.js
+++ projects/y10k-capital/index.js
@@ -1,12 +1,21 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
+//ETH Assets
 const eY10K = '0x953972ea0C1703c58F09FB6fD2477Fdcf0FEe074'
+
+//SEI Assets
+const y10kPYUSD = '0x6137dcfdd3c83fe2922b1cba4105d2e92b327a06'
 
 module.exports = {
   doublecounted: true,
   ethereum: {
     tvl: async (api) => {
       return api.erc4626Sum({ calls: [eY10K], isOG4626: true})
+    }
+  },
+  sei: {
+    tvl: async (api) => {
+      return api.erc4626Sum({ calls: [y10kPYUSD], isOG4626: true})
     }
   },
 }
