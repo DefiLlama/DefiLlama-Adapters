40c23d2dc984151dd5f82012bc2a5f438c9a70bb
Hia <williamspatrick3353@gmail.com>
2026-07-15 20:05:22 +0100

    Update owner addresses in sumTokens.js

--- projects/hxro-network/index.js
+++ projects/hxro-network/index.js
@@ -2,16 +2,16 @@ const { sumTokens2 } = require('../helper/solana')
 const sdk = require('@defillama/sdk')
 
 async function staking() {
-  const esHRXOKey = 'solana:CPwspzHc4bKtBQGNRhpRG9v3qRiPLWP28GrfZepwmBSz'
+  const esHRXOKey = '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
   const balances = await sumTokens2({
     tokenAccounts: [
-      '6kU9EA8ApkD3eCYjoR3e8MkJzvjcVb8nTFUQhGKMjA7r',
-      'DB8v2eyqQXueoSLYNFoxoBv1DFr7XuhMLtwPUEcrM5KP',
+      '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     ]
   })
 
   if (balances[esHRXOKey]) {
-    sdk.util.sumSingleBalance(balances, 'HxhWkVpk5NS4Ltg5nij2G671CKXFRKPK8vy271Ub4uEK', balances[esHRXOKey], 'solana')
+    sdk.util.sumSingleBalance(balances, '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1', balances[esHRXOKey], 'solana')
     delete balances[esHRXOKey]
   }
   return balances
