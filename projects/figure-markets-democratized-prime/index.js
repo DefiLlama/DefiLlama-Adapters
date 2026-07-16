cf44e631c9f8a96289376341fc61de8f922364fb
Jarryd Allison <jallison@figure.com>
2026-07-16 16:47:37 -0600

    feat: update to correctly calculate tvl as liquidity minus borrowed

--- projects/figure-markets-democratized-prime/index.js
+++ projects/figure-markets-democratized-prime/index.js
@@ -1,5 +1,7 @@
 const { queryV1Beta1, queryContract } = require('../helper/chain/cosmos.js');
 
+// node test.js projects/figure-markets-democratized-prime/index.js
+
 const demoPrimeContracts = [
     "pb1gqw3m5ftuu0hdcj646ppgmrp7ual3kkjj2wq6usqsfe0ntfmsc7s8fh70c", // AUTO
     "pb1lgdznp6dyljdq40xvcknkzcgelh2es0udwnx9rzn7c5q55435l3sx6v5a6", // Home Equity
@@ -10,10 +12,11 @@ const getBalances = async (api, isBorrowed) => {
         const state = await queryContract({ contract: pool, chain: 'provenance', data: {get_state: {}}})
         const liquidity = Math.trunc(state.reserve?.total_liquidity)
         const borrowed = Math.trunc(state.reserve?.total_borrow)
+        const collateral = liquidity - borrowed
         const token = state?.contract?.ld?.n
 
         if (token && liquidity) {
-            api.add(token, isBorrowed ? borrowed : liquidity)
+            api.add(token, isBorrowed ? borrowed : collateral)
         }
     }))
 }
