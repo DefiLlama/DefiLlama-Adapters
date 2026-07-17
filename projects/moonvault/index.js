bd682e78ba9be65d0c0bcb204b67436bf854a2cb
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-07-17 12:36:09 -0400

    minor refactor

--- projects/moonvault/index.js
+++ projects/moonvault/index.js
@@ -1,15 +1,12 @@
 const { sumTokens2 } = require('../helper/unwrapLPs')
-const Staking = "0xF21469471b5b89762ec7F017cF9FB607B0E25Ee7";
 
-const abis = {
-  lpToken: 'function stakingToken(uint256) public view returns (address)',
-  poolLength: 'function poolLength() public view returns (uint256)',
-}
+const Staking = "0xF21469471b5b89762ec7F017cF9FB607B0E25Ee7";
+const lpTokenAbi = 'function stakingToken(uint256) public view returns (address)'
 
 module.exports = {
   robinhood: {
     tvl: async (api) => {
-      const vaults = await api.fetchList({ target: Staking, lengthAbi: abis.poolLength, itemAbi: abis.lpToken, })
+      const vaults = await api.fetchList({ target: Staking, lengthAbi: 'uint256:poolLength', itemAbi: lpTokenAbi, })
       const tokens = await api.multiCall({ abi: 'address:want', calls: vaults })
       const bals = await api.multiCall({ abi: 'uint256:balance', calls: vaults })
       tokens.forEach((token, i) => {
