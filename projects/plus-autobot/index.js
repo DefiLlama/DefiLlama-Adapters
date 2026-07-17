f5a0594aa952715a16e693fb072dc73c839c3bce
waassa621-png <waassa621@gmail.com>
2026-07-17 20:53:26 +0900

    Refactor fetch function to return TVL under plus

--- projects/plus-autobot/index.js
+++ projects/plus-autobot/index.js
@@ -2,7 +2,6 @@ const { fetchURL } = require("../helper/utils");
 
 async function fetch() {
   const response = await fetchURL("https://bot.plusmain.net/api/defillama/tvl");
-  // axios의 response 구조에 맞게 데이터를 추출하여 리턴합니다.
   return response.data.tvl;
 }
 
@@ -10,6 +9,7 @@ module.exports = {
   timetravel: false,
   misrepresentedTokens: true,
   methodology: "TVL includes Base Genesis Node Staking, Ecosystem Treasury Live User Deposits managed off-chain.",
-  // API를 통해 통합 TVL 단순 숫자를 가져올 때는 루트 레벨에 fetch를 위치시켜야 합니다.
-  fetch: fetch
+  plus: {
+    tvl: fetch
+  }
 };
