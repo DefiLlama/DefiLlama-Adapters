125f22e7a8c31b515f70cdf6b74e3f3fcf512130
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-07-22 14:36:31 +0200

    fix tori fi

--- projects/tori-finance/index.js
+++ projects/tori-finance/index.js
@@ -1,13 +1,11 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
 async function tvl(api) {
-    const assets = await api.call({target: '0xcd69123b3FBBfC666E1f6a501da27B564C00De54', abi: 'uint:getTotalAssets'});
-    api.add(ADDRESSES.ethereum.USDC, assets / 1e12);
+  const assets = await api.call({ target: '0xd0580192E98eA6CEB9c7b6191Ed2E27560911697', abi: 'uint256:totalSupply' });
+  api.add('0xd0580192E98eA6CEB9c7b6191Ed2E27560911697', assets)
 }
 
 module.exports = {
-    methodology: 'Counts total value of assets staked into the Upshift Tori ecosystem vault.',
-    doublecounted: true,
-    ethereum: { tvl } // temp until trUSD is priced on dex
-    // ethereum: { tvl: sumERC4626VaultsExport({vaults: ['0xcd69123b3FBBfC666E1f6a501da27B564C00De54'], tokenAbi: 'address:asset', balanceAbi: 'uint:getTotalAssets'}) }
+  methodology: 'Supply of trUSD',
+  ethereum: { tvl }
 }
