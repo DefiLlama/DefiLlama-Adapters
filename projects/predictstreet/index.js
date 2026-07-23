662f02b7173c5ffa601fd5c5386c641cf0ebc214
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-07-23 16:28:26 +0200

    minor refactor

--- projects/predictstreet/index.js
+++ projects/predictstreet/index.js
@@ -1,7 +1,6 @@
 const { getLogs2 } = require('../helper/cache/getLogs')
 const ADDRESSES = require('../helper/coreAssets.json')
 
-const USDC_E = ADDRESSES.adi.USDC_e
 const VAULT_FACTORY = '0xc16B8b190064451c2FeEb2e77c4B2aC4c7009552'
 const CONDITIONAL_TOKENS = '0xD28B8295Cd57F205e4d080Ff4c6d23C06a9EEe3a' // locks collateral backing open positions
 const NEG_RISK_ADAPTER = '0x29e58C3916d1fD235f3d1e7fCF640fd1fA8BDb1e'
@@ -23,8 +22,9 @@ async function tvl(api) {
   const wcols = await api.multiCall({ abi: 'function wcolOf(bytes32) view returns (address)', calls: marketLogs.map(l => ({ target: NEG_RISK_ADAPTER, params: [l.marketId] })) })
   const clones = wcols.filter(a => a && a !== ADDRESSES.null)
 
+  const owners = [CONDITIONAL_TOKENS].concat(vaults, clones)
   // sum idle USDC.e in all vaults, ConditionalTokens, and neg-risk WrappedCollateral clones
-  await api.sumTokens({ owners: [CONDITIONAL_TOKENS, ...vaults, ...clones], tokens: [USDC_E] })
+  await api.sumTokens({ owners, tokens: ['0x9cb8142aEBBcdc60AF7c97Af897A67A8f3CA71C2'] }) // USDC.e
 }
 
 module.exports = {
