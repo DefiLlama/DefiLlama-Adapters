2f9bf67d5dd8bd29c34e0c1ef7ed79f9d11ff011
kozak26hu <kozak26@gmail.com>
2026-07-24 12:59:22 +0200

    Update staking contract address in index.js
    Replace the old staking contract with the new verified staking contract following a security review.

--- projects/paperhands/index.js
+++ projects/paperhands/index.js
@@ -1,7 +1,7 @@
 const { staking } = require('../helper/staking');
 
 const PHANDS_TOKEN = '0x11157da1fc6dcfd58b50ed79082183b2c6176245';
-const STAKING_CONTRACT = '0x0f4e761F2DcFD509eccd18004b89e329D25903B7';
+const STAKING_CONTRACT = '0x62fe22a9b954bc84fc6a74d889324fb40d13dce4';
 
 module.exports = {
   methodology: 'TVL includes the total volume of PHANDS tokens locked in the official staking smart contract.',
