fbe450af5b2d7ff664241481747aedcd5bc29e96
Daniel_Dean <danieldean.mist@gmail.com>
2026-07-25 19:36:42 -0400

    renamed folder to yieldzio and updated description

---
+++ projects/yieldzio/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  methodology: "Yieldz.io is a non-custodial DeFi utility app that specializes in one-click leverage and bundled transactions. Yieldz.io is only a frontend that orchestrates on-chain calls and has NO new smart contracts.",
+  ethereum: {
+    tvl: () => ({})
+  },
+}
