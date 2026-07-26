cb6188ed5b1d3805a2bba2752cecafd827bc90c1
June <june.zhu@unipay.fi>
2026-07-26 22:24:58 +0800

    fix(unitas): include xGLD supply in TVL
    Merge the unitas-xgld adapter into Unitas and add xGLD TVL on BSC, Base, and Ethereum.

--- projects/unitas/index.js
+++ projects/unitas/index.js
@@ -2,6 +2,11 @@ const {getTokenSupplies} = require('../helper/solana');
 
 const SOLANA_TOKEN_ADDRESS = '9ckR7pPPvyPadACDTzLwK2ZAEeUJ3qGSnzPs8bVaHrSy';
 const BSC_TOKEN_ADDRESS = "0xeA953eA6634d55dAC6697C436B1e81A679Db5882"
+const XGLD_TOKEN_ADDRESS = {
+    bsc: "0xe60106a5cAb7e7C64830919d36Ab20CaAf50Ac91",
+    base: "0xeA953eA6634d55dAC6697C436B1e81A679Db5882",
+    ethereum: "0x77a31A47E8a1dCe18Cb1772ae1C2157Fa080CFde",
+}
 
 async function solanaTvl() {
     const supply = await getTokenSupplies([SOLANA_TOKEN_ADDRESS]);
@@ -11,10 +16,19 @@ async function solanaTvl() {
 }
 
 async function bscTvl(api) {
-    const supply = await api.call({abi: 'erc20:totalSupply', target: BSC_TOKEN_ADDRESS})
-    return {
-        'usd-coin': supply / 1e18
-    }
+    const [usduSupply, xgldSupply] = await Promise.all([
+        api.call({abi: 'erc20:totalSupply', target: BSC_TOKEN_ADDRESS}),
+        api.call({abi: 'erc20:totalSupply', target: XGLD_TOKEN_ADDRESS.bsc}),
+    ])
+
+    api.addCGToken('usd-coin', usduSupply / 1e18)
+    api.add(XGLD_TOKEN_ADDRESS.bsc, xgldSupply)
+}
+
+async function xgldTvl(api) {
+    const supply = await api.call({abi: 'erc20:totalSupply', target: XGLD_TOKEN_ADDRESS[api.chain]})
+
+    api.add(XGLD_TOKEN_ADDRESS[api.chain], supply)
 }
 
 module.exports = {
@@ -22,11 +36,17 @@ module.exports = {
         ['2025-05-19', "solana unitas launch"]
     ],
     timetravel: false,
-    methodology: "Currently, tvl is composed of minted USDu",
+    methodology: "TVL is composed of minted USDu and xGLD supply.",
     solana: {
         tvl: solanaTvl
     },
     bsc: {
         tvl: bscTvl
+    },
+    base: {
+        tvl: xgldTvl
+    },
+    ethereum: {
+        tvl: xgldTvl
     }
 }
