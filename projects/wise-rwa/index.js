c2ad729833ab4d32893e771d7473bba6e8de13a4
Byron Kim Barra <byronbarra09@gmail.com>
2026-07-27 19:57:10 +0800

    fix: update wise-rwa vault addresses

--- projects/wise-rwa/index.js
+++ projects/wise-rwa/index.js
@@ -1,11 +1,11 @@
 const contracts = {
   ethereum: [
-    { token: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', vault: '0x11cEeE394842d9492f2C97050f66dE0e3f89D3A6' }, // USDC forwarding deposit
-    { token: '0xdAC17F958D2ee523a2206206994597C13D831ec7', vault: '0x3Ed1f16BbE0eE2C58119c13517a88fe9ccedfd45' }, // USDT forwarding deposit
+    { token: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', vault: '0x7e1EFF4301defc24936470B30bd1c686D2a295dc' }, // USDC forwarding deposit
+    { token: '0xdAC17F958D2ee523a2206206994597C13D831ec7', vault: '0x7e1EBE1D25367C6D3bC0aA72A1f00fC5320a05d7' }, // USDT forwarding deposit
   ],
   arbitrum: [
-    { token: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', vault: '0x025421D3e98D3bB7A33d6814Dd576eD8B9090077' }, // USDC forwarding deposit
-    { token: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', vault: '0xD69670d0eCaf032Ea8b1A6925E59dBacAA20f43A' }, // USDT forwarding deposit
+    { token: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', vault: '0x7e1EFF4301defc24936470B30bd1c686D2a295dc' }, // USDC forwarding deposit
+    { token: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', vault: '0x7e1EBE1D25367C6D3bC0aA72A1f00fC5320a05d7' }, // USDT forwarding deposit
   ],
 };
 
