ca8b6e58040b3bdd34d5922b7a80b5c25dbcb46a
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-07-29 16:04:38 -0400

    minor refactor

--- projects/byzanlink/index.js
+++ projects/byzanlink/index.js
@@ -1,43 +1,25 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const { sumERC4626VaultsExport2 } = require('../helper/erc4626')
-const { sumTokens2 } = require('../helper/solana')
+const { sumTokensExport } = require('../helper/solana')
 
-const vaultsByChain = {
+const vaults = {
   hedera: [
-    { name: 'Credible Payfi Vault', address: '0x6b8dfA6aa5f803a886Beb2492eF3307EC0Ee16FB' },
+    "0x6b8dfA6aa5f803a886Beb2492eF3307EC0Ee16FB", // Credible Payfi Vault
   ],
   ethereum: [
-    { name: 'Byzanlink SyrupUSDC Vault', address: '0xA5cDEE01aA7A5E0620df5f27F26E552fdf7f5F20' },
+    "0xA5cDEE01aA7A5E0620df5f27F26E552fdf7f5F20", // Byzanlink SyrupUSDC Vault
   ],
 }
 
-const solanaVault = {
-  name: 'Credible Payfi Vault',
-  tokenAccounts: [
-    'H77JK9eoCRPyEty5yXkm6hXMadn6isfYmDqaB3t5m4RM',
-    'EjwW5YHMCdHcymXw3awB3dVrcda6y2PKUfsqUkEsN6RK',
-  ],
-  payfiReceiptToken: 'EnGJvwX84JwV91Nzci5wrKFsxefwFtyFDJMaEi3wsbGz',
-}
-
-async function solanaTvl(api) {
-  await sumTokens2({ api, tokenAccounts: solanaVault.tokenAccounts })
-  const balances = api.getBalances()
-  const receiptKey = `solana:${solanaVault.payfiReceiptToken}`
-  if (balances[receiptKey]) {
-    api.add(ADDRESSES.solana.USDC, balances[receiptKey])
-    delete balances[receiptKey]
-  }
-}
+const solanaTokenAccounts = [
+  'H77JK9eoCRPyEty5yXkm6hXMadn6isfYmDqaB3t5m4RM',
+  'EjwW5YHMCdHcymXw3awB3dVrcda6y2PKUfsqUkEsN6RK',
+]
 
 module.exports = {
-  methodology:
-    'Total value of assets deposited in the Byzanlink vaults, read on-chain and valued in USD.',
-  solana: { tvl: solanaTvl },
-  ...Object.fromEntries(
-    Object.entries(vaultsByChain).map(([chain, vaults]) => [
-      chain,
-      { tvl: sumERC4626VaultsExport2({ vaults: vaults.map((v) => v.address) }) },
-    ])
-  ),
+  methodology: 'Total value of assets deposited in the Byzanlink vaults, read on-chain and valued in USD.',
+  solana: { tvl: sumTokensExport({ tokenAccounts: solanaTokenAccounts }) },
 }
+
+Object.keys(vaults).forEach(chain => {
+  module.exports[chain] = { tvl: sumERC4626VaultsExport2({ vaults: vaults[chain] }) }
+})
