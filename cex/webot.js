da9ef7e30fd88186673d1bee3705af3b1c8e1e9d
Zayn9c <286531131+Zayn9c@users.noreply.github.com>
2026-07-31 01:03:09 +0800

    Add Webot CEX (#20298)

---
+++ cex/webot.js
@@ -0,0 +1,15 @@
+module.exports = {
+  bitcoin: 'webot',
+  ethereum: {
+    owners: ['0x40127a34a92ab1f32467d6e438410893589db4b2'],
+  },
+  hyperliquid: {
+    owners: ['0x40127a34a92ab1f32467d6e438410893589db4b2'],
+  },
+  solana: {
+    owners: ['9f1McBacd6pJUCM2TQ982qQ9noY3rWEU5mhcGiDydbyo'],
+  },
+  tron: {
+    owners: ['TFozPAaqBd2rXVw2VvRUNGkv6iaJqFYWFy'],
+  },
+}
