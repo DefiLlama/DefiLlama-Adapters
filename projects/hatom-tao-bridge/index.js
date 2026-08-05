2f36bcefb2a6a2ae858ccbee5f4497d07ca9038d
Ramprasad4121 <ramprasadgoud34@gmail.com>
2026-08-05 11:59:29 +0530

    fix(hatom-tao-bridge): switch to mint-side MultiversX supply accounting

--- projects/hatom-tao-bridge/index.js
+++ projects/hatom-tao-bridge/index.js
@@ -1,6 +1,14 @@
-const { getExports } = require('../helper/heroku-api')
+const { getTokenData } = require('../helper/chain/elrond')
+
+async function tvl(api) {
+  const data = await getTokenData('WTAO-4f5363')
+  // circulatingSupply is already in human-readable units
+  const supply = Number(data.circulatingSupply || 0)
+  api.addCGToken('bittensor', supply)
+}
 
 module.exports = {
-  methodology: 'Value of tao locked in the bridge contract',
-  ...getExports("hatom-tao-bridge", ['bittensor']),
+  timetravel: false,
+  methodology: 'TVL is calculated as the circulating supply of Wrapped TAO (WTAO-4f5363) on MultiversX (minted minus burnt), which represents the exact amount of TAO locked on the Bittensor side of the bridge.',
+  bittensor: { tvl },
 }
