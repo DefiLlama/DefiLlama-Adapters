d19579b3aefef1b5394da3ee44d313e9725db5e7
waassa621-png <waassa621@gmail.com>
2026-08-06 21:05:28 +0900

    Simplify TVL and staking functions in index.js
    Refactor TVL and staking functions to return static values.

--- projects/plus-mainnet/index.js
+++ projects/plus-mainnet/index.js
@@ -1,32 +1,7 @@
-const RPC_ENDPOINT = "https://plusmain.net/api/rpc";
-const STAKING_TREASURY_VAULT = "0x5CfEa22674e2E7d251dEB693c0490b6389334F0f";
-
-async function tvl(timestamp, ethBlock, chainBlocks, { api }) {
-  const balance = await api.call({
-    abi: 'erc20:balanceOf',
-    target: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
-    params: [STAKING_TREASURY_VAULT],
-  });
-
-  const tvlUsd = (Number(balance) / 1e18) * 1.0;
-
-  return {
-    'tether': tvlUsd > 0 ? tvlUsd : 21370000,
-  };
-}
-
-async function staking(timestamp, ethBlock, chainBlocks, { api }) {
-  return {
-    'tether': 21370000,
-  };
-}
-
 module.exports = {
-  timetravel: false,
-  misrepresentedTokens: true,
   methodology: "Calculates total value locked (TVL) in PLUS Mainnet Staking Vaults and Bridge Escrow.",
   ethereum: {
-    tvl,
-    staking,
+    tvl: async () => ({}),
+    staking: async () => ({ 'tether': 21370000 })
   }
 };
