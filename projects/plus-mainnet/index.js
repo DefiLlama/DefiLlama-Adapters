44b9042598782b6965b97d4883fc41f97f72550f
waassa621-png <waassa621@gmail.com>
2026-08-07 00:44:29 +0900

    Refactor TVL calculation methodology in index.js
    Updated the methodology for calculating total value locked in PLUS Mainnet Staking Vaults to reflect dynamic on-chain RPC balances.

--- projects/plus-mainnet/index.js
+++ projects/plus-mainnet/index.js
@@ -1,7 +1,17 @@
+const STAKING_VAULTS = [
+  '0x87a3A5E8383A31D6d07146b1a457dBd8d50de58D',
+  '0x5CfEa22674e2E7d251dEB693c0490b6389334F0f'
+];
+const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+
+async function staking(api) {
+  await api.sumTokens({ owners: STAKING_VAULTS, tokens: [USDT] });
+}
+
 module.exports = {
-  methodology: "Calculates total value locked (TVL) in PLUS Mainnet Staking Vaults and Bridge Escrow.",
+  methodology: "Calculates total value locked in PLUS Mainnet Staking Vaults dynamically from on-chain RPC balances.",
   ethereum: {
     tvl: async () => ({}),
-    staking: async () => ({ 'tether': 21370000 })
+    staking
   }
 };
