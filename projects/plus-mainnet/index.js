7144f6a22414e592c5491e4e506e21c194b47488
waassa621-png <waassa621@gmail.com>
2026-08-07 00:51:17 +0900

    Refactor staking function to use dynamic USDT address

--- projects/plus-mainnet/index.js
+++ projects/plus-mainnet/index.js
@@ -1,15 +1,21 @@
+const ADDRESSES = require('../helper/coreAssets.json');
+const { sumTokens2 } = require('../helper/unwrapLPs');
+
 const STAKING_VAULTS = [
   '0x87a3A5E8383A31D6d07146b1a457dBd8d50de58D',
   '0x5CfEa22674e2E7d251dEB693c0490b6389334F0f'
 ];
-const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
 
 async function staking(api) {
-  await api.sumTokens({ owners: STAKING_VAULTS, tokens: [USDT] });
+  return sumTokens2({
+    api,
+    owners: STAKING_VAULTS,
+    tokens: [ADDRESSES.ethereum.USDT]
+  });
 }
 
 module.exports = {
-  methodology: "Calculates total value locked in PLUS Mainnet Staking Vaults dynamically from on-chain RPC balances.",
+  methodology: "Calculates total value locked in PLUS Mainnet Staking Vaults dynamically from on-chain balances.",
   ethereum: {
     tvl: async () => ({}),
     staking
