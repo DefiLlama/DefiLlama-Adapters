dfe1ce7a817141f6ac322a0cf79d28a4215c1c9b
Cory Dissinger <cdissinger@figure.com>
2026-08-07 12:55:17 -0400

    chore: Replace Math.trunc calcs with decimal safe values

--- projects/figure-markets-democratized-prime-cbl/index.js
+++ projects/figure-markets-democratized-prime-cbl/index.js
@@ -1,4 +1,5 @@
 const { queryContract } = require('../helper/chain/cosmos.js');
+const BigNumber = require('bignumber.js');
 
 // node test.js projects/figure-markets-democratized-prime-cbl/index.js
 
@@ -6,13 +7,13 @@ const pool = "pb1msvy4f0vcdm9kx4x56lrk5ctlnh88ag84k4ggskywzwvf2nzlmtqnm5ged"
 
 const getBalances = async (api, isBorrowed) => {
     const state = await queryContract({ contract: pool, chain: 'provenance', data: {get_state: {}}})
-    const liquidity = Math.trunc(state.reserve?.total_liquidity)
-    const borrowed = Math.trunc(state.reserve?.total_borrow)
-    const collateral = liquidity - borrowed
+    const liquidity = new BigNumber(state.reserve?.total_liquidity || 0).integerValue(BigNumber.ROUND_DOWN)
+    const borrowed = new BigNumber(state.reserve?.total_borrow || 0).integerValue(BigNumber.ROUND_DOWN)
+    const collateral = liquidity.minus(borrowed)
     const token = state?.contract?.ld?.n
 
-    if (token && liquidity) {
-        api.add(token, isBorrowed ? borrowed : collateral)
+    if (token && !liquidity.isZero()) {
+        api.add(token, (isBorrowed ? borrowed : collateral).toFixed(0))
     }
 }
 
