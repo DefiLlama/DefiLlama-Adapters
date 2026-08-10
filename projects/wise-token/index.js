7e866134c415f2afa96f2c3a6cef7decaeb4e95b
Byron Kim Barra <byronbarra09@gmail.com>
2026-08-11 03:42:44 +0800

    rewrite wise-token tvl as on-chain ownerless ETH share of WISE/ETH Uniswap LP

--- projects/wise-token/index.js
+++ projects/wise-token/index.js
@@ -1,41 +1,25 @@
-const vaults = {
-  ethereum: {
-    wtnUSDC: '0x7e1EFF4301defc24936470B30bd1c686D2a295dc',
-    wtnUSDT: '0x7e1EBE1D25367C6D3bC0aA72A1f00fC5320a05d7',
-    USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
-  },
-  arbitrum: {
-    wtnUSDC: '0x7e1EFF4301defc24936470B30bd1c686D2a295dc',
-    wtnUSDT: '0x7e1EBE1D25367C6D3bC0aA72A1f00fC5320a05d7',
-    USDC: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
-    USDT: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
-  },
-  base: {
-    wtnUSDC: '0x7e1EFF4301defc24936470B30bd1c686D2a295dc',
-    wtnUSDT: '0x7e1EBE1D25367C6D3bC0aA72A1f00fC5320a05d7',
-    USDC: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
-    USDT: '0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2',
-  },
-}
+const WISE_ETH_PAIR_ADDR = '0x21b8065d10f73EE2e260e5B47D3344d3Ced7596E';
+const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
+const WETH_ADDR = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 
 async function tvl(api) {
-  const chainVaults = vaults[api.chain]
-  if (!chainVaults) return
+  const [unownedUniLP, totalUniLP, reserves] = await Promise.all([
+    api.call({ target: WISE_ETH_PAIR_ADDR, abi: 'erc20:balanceOf', params: [ZERO_ADDR] }),
+    api.call({ target: WISE_ETH_PAIR_ADDR, abi: 'erc20:totalSupply' }),
+    api.call({ target: WISE_ETH_PAIR_ADDR, abi: 'function getReserves() view returns (uint112,uint112,uint32)' })
+  ]);
 
-  const supplies = await api.multiCall({
-    abi: 'erc20:totalSupply',
-    calls: [chainVaults.wtnUSDC, chainVaults.wtnUSDT]
-  })
+  const ethResWise = BigInt(reserves[1]); // ETH is token1
+  const unowned = BigInt(unownedUniLP);
+  const total = BigInt(totalUniLP);
 
-  api.add(chainVaults.USDC, supplies[0])
-  api.add(chainVaults.USDT, supplies[1])
+  const uniswapOwnerlessEth = total === 0n ? 0n : (ethResWise * unowned) / total;
+
+  api.add(WETH_ADDR, uniswapOwnerlessEth);
 }
 
 module.exports = {
   misrepresentedTokens: true,
-  methodology: 'RWA TVL = totalSupply of wtnUSDC + wtnUSDT receipt tokens (1:1 with deposited stables). Principal is forwarded to World Mobile for AirNodes; vaults only hold claimable interest.',
-  ethereum: { tvl },
-  arbitrum: { tvl },
-  base: { tvl },
-}
+  methodology: 'TVL = ownerless (unclaimed) share of ETH in the WISE/ETH Uniswap LP',
+  ethereum: { tvl }
+};
