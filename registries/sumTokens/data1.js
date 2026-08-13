b66f35807021c2cce122a49cf90f90978692eb3e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-08-13 19:25:08 +0200

    update hylo

--- registries/sumTokens/data1.js
+++ registries/sumTokens/data1.js
@@ -54,17 +54,6 @@ module.exports = {
       ]
     }
   },
-  "hylo": {
-    "timetravel": false,
-    "doublecounted": true,
-    "methodology": "TVL is calculated by summing all LSTs locked in Hylo protocol.",
-    "solana": {
-      "tokenAccounts": [
-        "2Y3TLkdGoJwbdizxqrZmQwNLYJyGKTgzC4tbetbkvQ43", // jitoSOL
-        "7VNBQCDKt4cxLWW51suV8a6VAYC4R66CfyySiYJek7Rj" // hyloSOL
-      ]
-    }
-  },
   "arrowpad-fun": {
     // ArrowPad.fun launchpad on Robinhood Chain: every launch seeds the full
     // token supply as single-sided Uniswap V3 liquidity and the LP NFT is locked
