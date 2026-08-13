7aec72e16c38c70f809d7edf530626af4a9fce59
Crypto RECEH <cryptoreceh.com@gmail.com>
2026-08-13 14:25:58 +0700

    Update index.js

--- projects/receh-staking/index.js
+++ projects/receh-staking/index.js
@@ -1,10 +1,7 @@
 const { sumTokensExport } = require("../helper/unwrapLPs");
 
-const STAKING_CONTRACT =
-  "0xc94320525237422185c498BCae62949839B8330f";
-
-const RECEH =
-  "0x4c9C431Fa7fD104c0E7230d20E1623E62019A1C5";
+const STAKING_CONTRACT = "0xc94320525237422185c498BCae62949839B8330f";
+const RECEH = "0x4c9C431Fa7fD104c0E7230d20E1623E62019A1C5";
 
 module.exports = {
   bsc: {
