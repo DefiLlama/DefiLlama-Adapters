fce6136558720c2eff8a6a53c2277042ca2c9e92
Fundmanager1 <146046259+Fundmanager1@users.noreply.github.com>
2026-08-15 11:22:43 -0400

    Update index.js
    Add farms TVL to Qom X adapter

--- projects/qomx/index.js
+++ projects/qomx/index.js
@@ -1,11 +1,36 @@
 const { getUniTVL } = require('../helper/unknownTokens')
+const { sumTokens2 } = require('../helper/unwrapLPs')
+
+const FARM_FACTORY = '0x951AFf794ffD122e4EA90B8BcFeE722c05f7133D'
+const DEX_FACTORY = '0x356037CbC77B3A2B36E0484d96DF0De247e66785'
+
+async function farms(api) {
+  const farms = await api.call({
+    target: FARM_FACTORY,
+    abi: 'function getAllFarms() view returns (address[])'
+  })
+
+  const lpTokens = await api.multiCall({
+    abi: 'address:lpToken',
+    calls: farms
+  })
+
+  const balances = await api.multiCall({
+    abi: 'uint256:totalStaked',
+    calls: farms
+  })
+
+  const tokensAndOwners = lpTokens.map((lp, i) => [lp, farms[i]])
+  return sumTokens2({ api, tokensAndOwners })
+}
 
 module.exports = {
-  methodology: 'TVL is the value of tokens locked in Qom X DEX liquidity pools on BSC (calculated from the factory).',
+  methodology: 'TVL includes tokens locked in Qom X DEX liquidity pools + tokens staked in Qom X farms on BSC.',
   bsc: {
     tvl: getUniTVL({
-      factory: '0x356037CbC77B3A2B36E0484d96DF0De247e66785',
+      factory: DEX_FACTORY,
       useDefaultCoreAssets: true,
-    })
+    }),
+    staking: farms,   // this will show as "Staking" TVL on DefiLlama
   }
 }
