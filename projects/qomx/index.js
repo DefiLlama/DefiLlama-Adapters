1fc5f1de4bd4aea2acf02de36668f4710624696c
Fundmanager1 <146046259+Fundmanager1@users.noreply.github.com>
2026-08-15 10:01:38 -0400

    Update index.js
    Add Qom X DEX TVL adapter

--- projects/qomx/index.js
+++ projects/qomx/index.js
@@ -1,6 +1,11 @@
+const { getUniTVL } = require('../helper/unknownTokens')
+
 module.exports = {
-  methodology: 'QomX DEX pools and farms on BSC',
+  methodology: 'TVL is the value of tokens locked in Qom X DEX liquidity pools on BSC (calculated from the factory).',
   bsc: {
-    tvl: async () => ({})
+    tvl: getUniTVL({
+      factory: '0x356037CbC77B3A2B36E0484d96DF0De247e66785',
+      useDefaultCoreAssets: true,
+    })
   }
 }
