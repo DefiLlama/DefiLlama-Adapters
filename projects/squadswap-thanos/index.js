3a99adc9b5a193153a98b946adc3be473a18e613
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-08-17 13:30:11 -0400

    small fix

--- projects/squadswap-thanos/index.js
+++ projects/squadswap-thanos/index.js
@@ -1,6 +1,5 @@
 const { getLogs2 } = require('../helper/cache/getLogs');
 const { sumTokens2 } = require('../helper/unwrapLPs');
-const { sliceIntoChunks } = require('../helper/utils');
 
 const config = {
   bsc: { vault: '0x3754Bd79D88e89F397ed1BfFAD8cdf3E0FDCC37E', clPoolManager: '0x9d3b119EfF69Cd81d324f654062b6fFA3Dd7f405', binPoolManager: '0xd7a5A9Df1719Ee83a4d10749019CaABf137DeBAC', fromBlock: 74380131 },
@@ -14,12 +13,6 @@ Object.keys(config).forEach(chain => {
   const { vault, clPoolManager, binPoolManager, fromBlock } = config[chain]
   module.exports[chain] = {
     tvl: async (api) => {
-
-      const fiveHoursAgo = Math.floor(Date.now() / 1000) - 5 * 3600
-      if (api.timestamp > fiveHoursAgo) {
-        api.block = undefined
-      }
-
       const [clLogs, binLogs] = await Promise.all([
         getLogs2({ api, target: clPoolManager, fromBlock, eventAbi: clEventAbi }),
         getLogs2({ api, target: binPoolManager, fromBlock, eventAbi: binEventAbi }),
@@ -34,11 +27,7 @@ Object.keys(config).forEach(chain => {
 
       const tokens = Array.from(tokenSet)
 
-      const tokenChunks = sliceIntoChunks(tokens, 1000)
-
-      for (const tokens of tokenChunks) {
-        await sumTokens2({ api, tokens, owner: vault, permitFailure: true })
-      }
+      return sumTokens2({ api, tokens, owner: vault })
     }
   }
 })
