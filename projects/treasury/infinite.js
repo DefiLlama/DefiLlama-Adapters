6b78ce7b7e8bbcc65932fd1ba37e4933bbc9514e
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-08-17 15:47:58 -0400

    small fixes (#20542)

--- projects/treasury/infinite.js
+++ projects/treasury/infinite.js
@@ -4,11 +4,20 @@ const { mergeExports } = require("../helper/utils");
 const TREASURY = "0xb5dB6e5a301E595B76F40319896a8dbDc277CEfB"
 const owners = [TREASURY, "0x1E2cD0E5905AFB73a67c497D82be271Cc65302Eb"]
 
+const VELODROME_RELAYER = "0x865e21A07d0915b72488860c3f3961f25e2c9347"
+const ITP_VELO_LP = "0xC04754F8027aBBFe9EeA492C9cC78b66946a07D1"
+
 const DHEDGE_FACTORY = {
   optimism: "0x5e61a079A178f0E5784107a4963baAe0c5a680c6",
   arbitrum: "0xffFb5fB14606EB3a548C113026355020dDF27535",
   polygon: "0xfdc7b8bFe0DD3513Cc669bB8d601Cb83e2F69cB0",
   base: "0x49Afe3abCf66CF09Fab86cb1139D8811C8afe56F",
+}
+
+async function relayerTvl(api) {
+  const daoShares = await api.call({ target: VELODROME_RELAYER, abi: 'erc20:balanceOf', params: [TREASURY] })
+  api.add(ITP_VELO_LP, daoShares)
+  return api.getBalances()
 }
 
 async function dhedgeTvl(api) {
@@ -38,6 +47,6 @@ const dhedgeExport = {}
 Object.keys(DHEDGE_FACTORY).forEach(chain => { dhedgeExport[chain] = { tvl: dhedgeTvl } })
 
 module.exports = {
-  ...mergeExports([treasuryExports(config), dhedgeExport]),
+  ...mergeExports([treasuryExports(config), dhedgeExport, { optimism: { tvl: relayerTvl } }]),
   methodology: "Tracks Infinite Trading treasury holdings across multiple chains including token balances in multisig wallets and the treasury's share of dHEDGE vaults it manages.",
 }
