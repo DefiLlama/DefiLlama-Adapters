73178b4599e3d04b336220261116384d321f15e3
rohannero <rohanzarathustra@gmail.com>
2026-08-17 15:14:02 -0400

    fixes

---
+++ cex/niza.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ethereum: {
+    owners: [
+        '0x8374F37B298420ae13ccD3cbE7dC07895290676d'
+    ],
+  },
+  bitcoin: {
+    owners: [
+        'bc1qx2p4hx3s60cg69rt3j78l2vskelgcjj95s5ty3'
+    ]
+  },
+}
