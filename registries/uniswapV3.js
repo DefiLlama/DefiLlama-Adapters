604ac43fe2fd9f1860cb03d6ad45de53df0d82a8
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-08-19 15:24:32 -0400

    fix nest cl (#20574)

--- registries/uniswapV3.js
+++ registries/uniswapV3.js
@@ -940,6 +940,7 @@ const uniV3Configs = {
       factory: '0xF77Bd082c627aA54591cF2f2EaA811fd1AB3b1F3',
       fromBlock: 17877130,
       isAlgebra: true,
+      blacklistedTokens: ['0x9d0e8f5b25384c7310cb8c6ae32c8fbeb645d083'],
       blacklistedOwners: ['0xbAd2fB864FBD3f8b9bCC81512D7C8Ee1Aa0a8D6C'],
     },
   },
