bd37d8ab03bbd0f4470c79048c1fdb9becdeaeee
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-08-20 12:31:45 -0400

    small fix

--- projects/etherfi-cash-liquid/index.js
+++ projects/etherfi-cash-liquid/index.js
@@ -11,14 +11,12 @@ const commonVaults = [
 const vaults = {
   optimism: [
     ...commonVaults,
-    '0x86B5780b606940Eb59A062aA85a07959518c0161', // sETHFI
     '0xca5921DF65E2e1b0B98Ae91c0187BA80D4124898', // liquidRESERVE
     '0xcC476B1a49bcDf5192561e87b6Fb8ea78aa28C13', // weEUR
     '0x17bC8Ffd82b8a36e737Ca1141C025089589B915e', // liquidRWA
   ],
   scroll: [
     ...commonVaults,
-    '0x86B5780b606940Eb59A062aA85a07959518c0161', // sETHFI
     '0xb7Fb3768CAAC98354EaDF514b48f28F2fE822bF0', // liquidRESERVE
   ],
 }
@@ -28,7 +26,6 @@ async function tvl(api) {
   const totalSupplies = await api.multiCall({
     abi: 'erc20:totalSupply',
     calls: chainVaults,
-    permitFailure: true,
   })
 
   totalSupplies.forEach((totalSupply, index) => {
