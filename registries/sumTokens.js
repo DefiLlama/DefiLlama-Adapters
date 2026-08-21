a39bad59c99c559de79fea3d983d64d589b3995c
rohannero <rohanzarathustra@gmail.com>
2026-08-21 17:41:07 -0400

    remove duplicates

--- registries/sumTokens.js
+++ registries/sumTokens.js
@@ -17823,16 +17823,6 @@ const configs = {
       ]
     },
   },
-  "manta-myield": {
-    "manta": {
-      "owner": "0x1B9bcc6644CC9b5e1F89aBaAb66904F5a562d4a1",
-      "tokens": [
-        "0x1468177DbCb2a772F3d182d2F1358d442B553089",
-        "0xACCBC418a994a27a75644d8d591afC22FaBA594e",
-        "0x649d4524897cE85A864DC2a2D5A11Adb3044f44a"
-      ]
-    },
-  },
   "marspoolin": {
     "ethereum": {
       "tokensAndOwners2": [
