050a897701fabb1eca4a0de29adf9e32917e154c
Kyros Finance <KyrosFinance@proton.me>
2026-08-22 12:11:26 +0800

    Add index.js for Kyros project with exports

---
+++ projects/kyros/index.js
@@ -0,0 +1,6 @@
+const {compoundExports2,methodology} = require('../helper/compound')
+
+module.exports = {
+  robinhood: compoundExports2({comptroller:'0x5f3Da5a8bCF26011e95CA2D03aFc6E0C54D4F0c9'}),
+  methodology
+}
