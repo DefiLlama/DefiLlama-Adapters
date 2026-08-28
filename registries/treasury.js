f2164b013f4c52a765990886b96147868ab86d86
rohannero <rohanzarathustra@gmail.com>
2026-08-27 23:45:10 -0400

    fix

--- registries/treasury.js
+++ registries/treasury.js
@@ -3,7 +3,6 @@ const ADDRESSES = require('../projects/helper/coreAssets.json')
 const { nullAddress } = require('../projects/helper/treasury')
 const { nullAddress: tokenMappingNullAddress } = require('../projects/helper/tokenMapping')
 const bitcoinAddressBook = require('../projects/helper/bitcoin-book/index.js')
-const { robinhood } = require('../projects/merryforge/index.js')
 
 // Treasury registry.
 //
