20c50ae3f6c65caa4a7973bcd11a6fe4e16d0802
Zofiacoin <zooqofficialcoin@gmail.com>
2026-08-29 21:14:53 +0300

    Add professional profile README for ZOFIA project
    Added JSDoc documentation for the tvl function and updated the start timestamp to the correct deployment time (1784819550) for accurate historical backfill.

--- projects/zofia/index.js
+++ projects/zofia/index.js
@@ -1,6 +1,11 @@
 const ZOFIA_TOKEN = '0x2e6576c64b27aed687556a4ef39b1547534429ad';
 const POOL_CONTRACT = '0x23190194f76A7c93a4e64a9DFb38C2c45721d587';
 
+/**
+ * Calculates the Total Value Locked (TVL) for ZOFIA by querying the ZOFIA balance
+ * in the QuickSwap ZOFIA/USDC pool on Polygon.
+ * @param {Object} api - The DeFiLlama SDK API object.
+ */
 async function tvl(api) {
   const balance = await api.call({
     abi: 'erc20:balanceOf',
@@ -12,7 +17,7 @@ async function tvl(api) {
 
 module.exports = {
   methodology: 'TVL is calculated as the total ZOFIA tokens locked in the QuickSwap ZOFIA/USDC liquidity pool.',
-  start: 1784905950,
+  start: 1784819550,
   polygon: {
     tvl,
   },
