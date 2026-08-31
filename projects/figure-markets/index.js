693b5b57162adbe50be3ec452230a07897cf11d6
Eden <noat.eth@gmail.com>
2026-08-31 14:21:57 +0700

    exclude trading commitment

--- projects/figure-markets/index.js
+++ projects/figure-markets/index.js
@@ -1,13 +1,14 @@
-const { sumTokens2 } = require('../helper/unwrapLPs');
-const { queryV1Beta1V2 } = require('../helper/chain/cosmos.js');
+const { sumTokens2 } = require('../helper/unwrapLPs')
+const { queryV1Beta1V2 } = require('../helper/chain/cosmos.js')
 
 const figureMarketsExchangeID = '1'
 
-const collateralizedAssets = [
+const excludedDenoms = new Set([
+    'uusd.trading',
     'pm.sale.pool.3dxq3fk9llvhrqqwhodiap', // YLDS HELOCs
     'pm.pool.asset.1y3flutqcyuf8duew1vj2g', // YLDS CBLs
-    'pm.pool.asset.3hjz8rcr3pejdc3msntlvy' // YLDS HELOC+
-]
+    'pm.pool.asset.3hjz8rcr3pejdc3msntlvy', // YLDS HELOC+
+])
 
 const getLockedTokens = async (api) => {
     const commitments = await queryV1Beta1V2({
@@ -15,14 +16,15 @@ const getLockedTokens = async (api) => {
         url: `exchange/v1/market/${figureMarketsExchangeID}/commitments`,
         limit: 1000,
     })
-    for (const c of commitments) {
-        for (const a of c.amount) {
-            if (!collateralizedAssets.includes(a.denom)) {
-                api.add(a.denom, a.amount)
+
+    for (const commitment of commitments) {
+        for (const coin of commitment.amount) {
+            if (!excludedDenoms.has(coin.denom)) {
+                api.add(coin.denom, coin.amount)
             }
         }
     }
-};
+}
 
 const tvl = async (api) => {
     await getLockedTokens(api)
@@ -32,6 +34,6 @@ const tvl = async (api) => {
 module.exports = {
     timetravel: true,
     misrepresentedTokens: true,
-    methodology: "Figure Markets TVL is the sum of all tokens locked within the Figure Markets protocol contract.",
+    methodology: "Figure Markets TVL is the sum of non-USD.TRADING assets committed to the Figure Markets exchange. USD.TRADING commitments are excluded.",
     provenance: { tvl },
 }
