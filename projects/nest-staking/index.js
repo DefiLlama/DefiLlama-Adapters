cb45d0a3713804fbd62606dcbef20b9e955fc584
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-08-31 11:24:29 -0400

    refactor

--- projects/nest-staking/index.js
+++ projects/nest-staking/index.js
@@ -1,11 +1,8 @@
-const sdk = require("@defillama/sdk");
 const { getConfig } = require("../helper/cache");
 const { getTokenSupplies } = require("../helper/solana");
-const { fetchURL } = require("../helper/utils");
 
 const minTvl = 10_000;
 const includedStatuses = ["active", "hidden"];
-let vaultContextPromise;
 
 async function getIncludedVaults() {
   const responses = await Promise.all(
@@ -21,101 +18,23 @@ async function getIncludedVaults() {
     .filter(vault => vault.tvl > minTvl);
 }
 
-function getCanonicalCoin(vault) {
-  return `plume_mainnet:${vault.vaultAddress}`;
-}
-
-async function getVaultContext() {
-  if (!vaultContextPromise) {
-    vaultContextPromise = (async () => {
-      const vaults = await getIncludedVaults();
-      const plumeApi = new sdk.ChainApi({ chain: "plume_mainnet" });
-      const addresses = vaults.map(vault => vault.vaultAddress);
-      const accountants = vaults.map(vault => vault.nestAccountant.address);
-      const coins = vaults.map(getCanonicalCoin).join(",");
-
-      const [priceResponse, decimals, rates, rateDecimals] = await Promise.all([
-        fetchURL(`https://coins.llama.fi/prices/current/${coins}`),
-        plumeApi.multiCall({ abi: "erc20:decimals", calls: addresses }),
-        plumeApi.multiCall({ abi: "uint256:getRate", calls: accountants }),
-        plumeApi.multiCall({ abi: "uint8:baseDecimals", calls: accountants }),
-      ]);
-
-      const pricedCoins = new Set(Object.keys(priceResponse.data.coins ?? {}));
-      const pricing = new Map(vaults.map((vault, index) => [
-        vault.vaultAddress,
-        {
-          decimals: Number(decimals[index]),
-          rate: Number(rates[index]) / 10 ** Number(rateDecimals[index]),
-        },
-      ]));
-
-      return { vaults, pricedCoins, pricing };
-    })().catch(error => {
-      vaultContextPromise = undefined;
-      throw error;
-    });
-  }
-
-  return vaultContextPromise;
-}
-
-function addVaultTvl(api, vault, supply, supplyDecimals, context) {
-  const canonicalCoin = getCanonicalCoin(vault);
-  const { decimals, rate } = context.pricing.get(vault.vaultAddress);
-
-  if (context.pricedCoins.has(canonicalCoin)) {
-    const canonicalSupply = BigInt(supply) * 10n ** BigInt(decimals) /
-      10n ** BigInt(supplyDecimals);
-    api.add(canonicalCoin, canonicalSupply.toString(), { skipChain: true });
-  } else {
-    const shares = Number(supply) / 10 ** Number(supplyDecimals);
-    api.addCGToken("tether", shares * rate, { label: vault.symbol });
-  }
-}
-
 function evmTvl(chain) {
   return async function tvl(api) {
-    const context = await getVaultContext();
-    const chainVaults = context.vaults.filter(vault => vault.chain?.[chain]);
-    const addresses = chainVaults.map(vault => vault.vaultAddress);
-
-    const [supplies, decimals] = await Promise.all([
-      api.multiCall({ abi: "erc20:totalSupply", calls: addresses }),
-      api.multiCall({ abi: "erc20:decimals", calls: addresses }),
-    ]);
-
-    chainVaults.forEach((vault, index) => {
-      addVaultTvl(api, vault, supplies[index], decimals[index], context);
-    });
+    const vaults = await getIncludedVaults();
+    const addresses = vaults.filter(vault => vault.chain?.[chain]).map(vault => vault.vaultAddress);
+    const supplies = await api.multiCall({ abi: "erc20:totalSupply", calls: addresses });
+    api.add(addresses, supplies);
   }
 }
 
 async function tvl_solana(api) {
-  const context = await getVaultContext();
-  const vaults = context.vaults
-    .filter(vault => vault.solana?.mintAddress);
-  const mints = vaults.map(vault => vault.solana.mintAddress);
-  const supplies = await getTokenSupplies(mints);
-
-  vaults.forEach(vault => {
-    const supply = supplies[vault.solana.mintAddress];
-    if (supply === undefined) return;
-
-    addVaultTvl(
-      api,
-      vault,
-      supply,
-      vault.solana.decimals,
-      context
-    );
-  });
+  const vaults = await getIncludedVaults();
+  const mints = vaults.filter(vault => vault.solana?.mintAddress).map(vault => vault.solana.mintAddress);
+  await getTokenSupplies(mints, { api });
 }
 
 module.exports = {
-  methodology: "TVL sums the onchain supplies of active and hidden vaults above $10k across supported chains. Canonical Plume tokens are used when priced, with Plume accountant rates reported as USDT for unpriced tokens.",
-  misrepresentedTokens: true,
-  timetravel: false,
+  methodology: "TVL is calculated from the value of Nest tokens, which represent user shares in vaults backed by yield-generating assets.",
   ethereum: { tvl: evmTvl("mainnet") },
   plume_mainnet: { tvl: evmTvl("plume") },
   bsc: { tvl: evmTvl("bsc") },
