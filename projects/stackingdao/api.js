9fdb13bbfcb2d1c0198ec9178b8656772814c95e
y0s0n <eeeedev@gmail.com>
2026-09-03 11:22:34 +0300

    enable timetravel for historical backfill

--- projects/stackingdao/api.js
+++ projects/stackingdao/api.js
@@ -3,15 +3,18 @@ const { call } = require('../helper/chain/stacks-api')
 const D = 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG'
 
 module.exports = {
-  timetravel: false,
+  timetravel: true,
   stacks: { tvl },
 }
 
 async function tvl(api) {
+  const block = api.block
+
   // stSTX backing: total STX in stx-reserve-v2 (liquid balance + staked in PoX-5)
   const stxReserve = await call({
     target: `${D}.stx-reserve-v2`,
     abi: 'get-total-stx',
+    block,
   })
   api.add('blockstack', stxReserve.value)
 
@@ -19,6 +22,7 @@ async function tvl(api) {
   const sbtcReserve = await call({
     target: `${D}.stbtc-reserve`,
     abi: 'get-total-sbtc',
+    block,
   })
   api.add('sBTC', sbtcReserve.value)
 }
