0356765da39147697781ecbf78e9b03163536be0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-09-07 14:09:01 +0200

    minor fix

--- projects/helper/unwrapLPs.js
+++ projects/helper/unwrapLPs.js
@@ -10,7 +10,7 @@ const { isLP, log, sliceIntoChunks, isICHIVaultToken, createIncrementArray, slee
 const { sumArtBlocks, whitelistedNFTs, } = require('./nft')
 const uniV3ABI = require('./abis/uniV3.json');
 const slipstreamNftABI = require('../arcadia-finance-v2/slipstreamNftABI.json');
-const { covalentGetTokens, blockscoutGetTokens, } = require("./token");
+const { covalentGetTokens, } = require("./token");
 const SOLIDLY_VE_NFT_ABI = require('./abis/solidlyVeNft.json');
 const { tickToPrice } = require('./utils/tick');
 const { queryAllium } = require('./allium');
@@ -968,7 +968,6 @@ async function sumTokens2({
   resolveVlCVX = false,
   permitFailure = false,
   fetchCoValentTokens = false,
-  fetchBlockscoutTokens = false,
   tokenConfig = {
     // onlyWhitelisted
     // onlyUseExistingCache
@@ -1054,11 +1053,6 @@ group by
   if (fetchCoValentTokens && useCurrentBalances) {
     const cTokens = (await Promise.all(owners.map(i => covalentGetTokens(i, api, tokenConfig))))
     cTokens.forEach((tokens, i) => ownerTokens.push([tokens, owners[i]]))
-  }
-
-  if (fetchBlockscoutTokens) {
-    const bTokens = await Promise.all(owners.map(i => blockscoutGetTokens(i, api, tokenConfig)))
-    bTokens.forEach((tokens, i) => ownerTokens.push([tokens, owners[i]]))
   }
 
   if (resolveNFTs) {
