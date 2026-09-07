b6f5e0ea0cef872faed0b91f9f71f38d3aadf219
RED TIGER <RedTigerAuditor@gmail.com>
2026-09-07 20:23:53 +0000

    expand Keyring TVL adapter

--- registries/curators.js
+++ registries/curators.js
@@ -732,6 +732,11 @@ const configs = {
     config: {
       methodology: 'Count all assets are deposited in all vaults curated by Keyring Network.',
       blockchains: {
+        ethereum: {
+          eulerVaultOwners: [
+            '0x0B50beaE6aac0425e31d5a29080F2A7Dec22754a',
+          ],
+        },
         avax: {
           eulerVaultOwners: [
             '0x0B50beaE6aac0425e31d5a29080F2A7Dec22754a',
