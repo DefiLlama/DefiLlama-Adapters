440c362ce5985c7faddbf23c3f57cd6bbc6df0fc
RohanNero <100052099+RohanNero@users.noreply.github.com>
2026-09-07 16:36:14 -0400

    small fix

--- registries/curators.js
+++ registries/curators.js
@@ -778,7 +778,7 @@ const configs = {
   },
   "monarq": {
     config: {
-      methodology: 'Count FXRP managed by the Monarq XRP Yield Vault through its on-chain getTotalAssets value. .',
+      methodology: 'Count FXRP managed by the Monarq XRP Yield Vault through its on-chain getTotalAssets value.',
       blockchains: {
         flare: {
           upshiftV2: ['0x2439D4bb753A0f3777d4C9011AFacc475ba6B951'],
