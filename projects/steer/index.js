e14f6d10fba29bbe8a35052981cee195c994183a
Deepak <ideepak.jsd@gmail.com>
2026-09-08 21:38:19 +0530

    fixed some const usage

--- projects/steer/index.js
+++ projects/steer/index.js
@@ -1,4 +1,5 @@
 const sdk = require("@defillama/sdk");
+const ADDRESSES = require('../helper/coreAssets.json')
 const { cachedGraphQuery } = require('../helper/cache')
 const { stakings } = require("../helper/staking");
 
@@ -413,7 +414,7 @@ function addBalances(api, balances, token0s, token1s, [balance0, balance1], fall
 function getV4MetadataToken(metadata, index) {
   if (typeof metadata !== 'string' || !/^0x[\da-f]{128,}$/i.test(metadata)) return
   const token = `0x${metadata.slice(2 + index * 64 + 24, 2 + (index + 1) * 64)}`
-  return token === '0x0000000000000000000000000000000000000000' ? undefined : token
+  return token === ADDRESSES.null ? undefined : token
 }
 
 supportedChains.forEach(chain => {
