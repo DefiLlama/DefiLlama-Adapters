1045a8d28df98d4045345475935e7975bd2e032d
root <root@vps-f17b9bee.vps.ovh.net>
2026-09-09 13:43:03 +0200

    Fix chain-specific DropSwap TVL calculation

--- projects/dropswap/index.js
+++ projects/dropswap/index.js
@@ -2,7 +2,8 @@ const { getUniTVL } = require('../helper/unknownTokens')
 
 const FACTORY = '0xDCed5445409398dc609C2f87849B44bc9479664A'
 
-const tvl = getUniTVL({
+const getTvl = chain => getUniTVL({
+  chain,
   factory: FACTORY,
   useDefaultCoreAssets: true,
 })
@@ -12,10 +13,10 @@ module.exports = {
     'TVL is the value of tokens held in DropSwap V2 liquidity pools. Pools are discovered on-chain from the DropSwap V2 Factory using allPairsLength() and allPairs(uint256).',
 
   arbitrum: {
-    tvl,
+    tvl: getTvl('arbitrum'),
   },
 
   robinhood: {
-    tvl,
+    tvl: getTvl('robinhood'),
   },
 }
