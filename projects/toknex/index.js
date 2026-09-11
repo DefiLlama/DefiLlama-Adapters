dadd78b113477e419fcc75a457477f179b0a4e9a
SaadJamil19 <saadjamil196@gmail.com>
2026-09-11 14:56:30 +0500

    add Toknex TVL adapter

--- projects/toknex/index.js
+++ projects/toknex/index.js
@@ -1,12 +1,19 @@
-const vaults = [
-  '0x44f5D969769335B895fEd06fa6408aB56a891213',
-  '0xBa78aaB35f820c29627420D8413b106dd798Cb09',
-]
+// The same two vault addresses are intentionally deployed on both Ethereum and BSC.
+const vaults = {
+  ethereum: [
+    '0x44f5D969769335B895fEd06fa6408aB56a891213',
+    '0xBa78aaB35f820c29627420D8413b106dd798Cb09',
+  ],
+  bsc: [
+    '0x44f5D969769335B895fEd06fa6408aB56a891213',
+    '0xBa78aaB35f820c29627420D8413b106dd798Cb09',
+  ],
+}
 
 const vaultStatsAbi = 'function vaultStats() view returns ((uint256 totalShares, uint256 activeAUM, uint256 pendingLiabilities, uint256 vaultBalance, uint256 pricePerShare, uint256 pendingCount, bool isPaused, address fundsManager, address asset) stats)'
 
 async function tvl(api) {
-  const stats = await api.multiCall({ abi: vaultStatsAbi, calls: vaults })
+  const stats = await api.multiCall({ abi: vaultStatsAbi, calls: vaults[api.chain] })
   stats.forEach(({ asset, activeAUM, pendingLiabilities }) => api.add(asset, (BigInt(activeAUM) + BigInt(pendingLiabilities)).toString()))
 }
 
