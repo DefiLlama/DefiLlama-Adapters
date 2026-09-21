cf6d1aa5a9d8bdaa9510085efaa26039de514f8d
Buglefamily <buglefamily@proton.me>
2026-09-21 13:07:24 +0000

    Add Bugle Family Arc adapter

---
+++ projects/buglefamily/index.js
@@ -0,0 +1,8 @@
+const { uniV3Export } = require('../helper/uniswapV3')
+
+module.exports = uniV3Export({
+  arc: {
+    factory: '0xB09f790A1907a1db006e88F14C4f0168fBee9598',
+    fromBlock: 1946500,
+  },
+})
