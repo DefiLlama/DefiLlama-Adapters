302078d0130bcebba82f51576da271646caff68d
FelixBruguera <felixbruguera0@gmail.com>
2026-09-21 09:46:26 -0400

    move to registry

--- projects/buglefamily/index.js
+++
@@ -1,8 +0,0 @@
-const { uniV3Export } = require('../helper/uniswapV3')
-
-module.exports = uniV3Export({
-  arc: {
-    factory: '0xB09f790A1907a1db006e88F14C4f0168fBee9598',
-    fromBlock: 1946500,
-  },
-})
