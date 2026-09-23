1d089e0fa912b491242c7d8c8abad2a2d24f9bc6
Victoria Horbunova <vimirabo@gmail.com>
2026-09-23 16:25:52 +0200

    docs: add JSDoc docstrings for staking and tvl

--- projects/origyn/index.js
+++ projects/origyn/index.js
@@ -1,6 +1,10 @@
 const { toUSDTBalances } = require("../helper/balances");
 const { get } = require('../helper/http')
 
+/**
+ * Calculates staking TVL for ORIGYN by fetching total staked OGY in governance neurons.
+ * @param {Object} api - DefiLlama API helper object
+ */
 async function staking(api) {
   // Get governance staked OGY (in e8s)
   // Ref: https://gateway.origyn.com/docs/#/Tokens/get_origyn_governance_stats
@@ -9,6 +13,10 @@ async function staking(api) {
   api.addCGToken('origyn-foundation', Number(total_staked) / 1e8)
 }
 
+/**
+ * Calculates TVL for ORIGYN by fetching the total value locked of ORIGYN Certificates.
+ * @returns {Promise<Object>} Balances object with total value locked in USDT
+ */
 async function tvl() {
   // Get the total value of ORIGYN Certificates
   // Ref: https://github.com/ORIGYN-SA/origyn-sns/blob/master/backend/canisters/collection_index/impl/src/queries/http_request.rs#L29
