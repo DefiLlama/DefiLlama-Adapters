75f81cddbf7a4a19991cbfdc055fb35f691e8094
sabaaa1 <sdeli23@freeuni.edu.ge>
2026-09-25 11:18:51 +0400

    hinkal: update pools, add robinhood and arc, sync registry tokens

--- projects/hinkal/index.js
+++ projects/hinkal/index.js
@@ -8,8 +8,8 @@ const SHARED_OWNERS = [
   "0x25e5e82f5702A27C3466fE68f14abDbbAdFca826",
   "0x7cb60446d7635C68EDf1c568cac74A1f98c1Cfa4",
 ];
-const TRON_VAULT = "TKFUxULu53pSfDkSZwF85PFuKBw1K9axaw";
-const SOLANA_VAULT = "HrcpUS1oFVqeNVZxwHZP2fHSiXJWpv4DTN6qyQX4tAJa";
+const TRON_VAULTS = ["TDybyktjKwcuLdcuroUJABT7pQE5WDYeqa", "TKFUxULu53pSfDkSZwF85PFuKBw1K9axaw"];
+const SOLANA_VAULTS = ["8WMX4EvePEjLnzkcrDRhHFftLLAH7gRK2XUwMJeYNhpn", "HrcpUS1oFVqeNVZxwHZP2fHSiXJWpv4DTN6qyQX4tAJa"];
 
 const tvl = async (api) => {
   const chain = api.chain;
@@ -34,10 +34,11 @@ module.exports = {
   ethereum: { tvl },
   base: { tvl },
   arbitrum: { tvl },
-  optimism: { tvl },
   polygon: { tvl },
   bsc: { tvl },
   tempo: { tvl },
-  tron: { tvl: sumTokensExport({ owners: [TRON_VAULT], tokens: [ADDRESSES.null, ...registryTokensByChain.tron] }) },
-  solana: { tvl: sumSolanaTokensExport({ solOwners: [SOLANA_VAULT], computeTokenAccount: true, allowError: true }) },
+  robinhood: { tvl },
+  arc: { tvl },
+  tron: { tvl: sumTokensExport({ owners: TRON_VAULTS, tokens: [ADDRESSES.null, ...registryTokensByChain.tron] }) },
+  solana: { tvl: sumSolanaTokensExport({ owners: SOLANA_VAULTS, tokens: registryTokensByChain.solana, solOwners: SOLANA_VAULTS, computeTokenAccount: true, allowError: true }) },
 };
